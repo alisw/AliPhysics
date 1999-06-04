4337c6a9ed9654d1b6b3606a7cd56e1c89e632b7
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
1999-06-04 05:55:30 +0000

    Removed version 4 in still in FORTRAN from the Makefile and ITSLinkDef.h. The files are still there, just in case.

--- ITS/ITSLinkDef.h
+++ ITS/ITSLinkDef.h
@@ -8,7 +8,8 @@
 #pragma link C++ class  AliITSv0;
 #pragma link C++ class  AliITSv1;
 #pragma link C++ class  AliITSv3;
-#pragma link C++ class  AliITSv4;
+// Probably to be removed, this is the non-turbo version
+//#pragma link C++ class  AliITSv4;
 #pragma link C++ class  AliITSv5;
 #pragma link C++ class  AliITShit;
 #pragma link C++ class  AliITSdigit;
