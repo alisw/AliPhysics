3b932593a0f1d9b087c4ae4e3f5284bd20afee2f
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
1999-06-11 17:28:29 +0000

    Drawing macros added.

---
+++ TRD/ViewTRD2.C
@@ -0,0 +1,69 @@
+//void ViewTRD()
+{
+
+   geant3->Gsatt("TRD ","seen",0);
+   geant3->Gsatt("UTRS","seen",0);
+   geant3->Gsatt("UTRI","seen",0);
+   
+   geant3->Gsatt("UCI1","seen",0);
+   geant3->Gsatt("UCI2","seen",0);
+   geant3->Gsatt("UCI3","seen",0);
+   geant3->Gsatt("UCI4","seen",0);
+   geant3->Gsatt("UCI5","seen",0);
+   geant3->Gsatt("UCI6","seen",0);
+   geant3->Gsatt("UCN1","seen",0);
+   geant3->Gsatt("UCN2","seen",0);
+   geant3->Gsatt("UCN3","seen",0);
+   geant3->Gsatt("UCN4","seen",0);
+   geant3->Gsatt("UCN5","seen",0);
+   geant3->Gsatt("UCN6","seen",0);
+   geant3->Gsatt("UCO1","seen",0);
+   geant3->Gsatt("UCO2","seen",0);
+   geant3->Gsatt("UCO3","seen",0);
+   geant3->Gsatt("UCO4","seen",0);
+   geant3->Gsatt("UCO5","seen",0);
+   geant3->Gsatt("UCO6","seen",0);
+
+   geant3->Gsatt("UII1","seen",0);
+   geant3->Gsatt("UII2","seen",0);
+   geant3->Gsatt("UII3","seen",0);
+   geant3->Gsatt("UII4","seen",0);
+   geant3->Gsatt("UII5","seen",0);
+   geant3->Gsatt("UII6","seen",0);
+   geant3->Gsatt("UIN1","seen",0);
+   geant3->Gsatt("UIN2","seen",0);
+   geant3->Gsatt("UIN3","seen",0);
+   geant3->Gsatt("UIN4","seen",0);
+   geant3->Gsatt("UIN5","seen",0);
+   geant3->Gsatt("UIN6","seen",0);
+   geant3->Gsatt("UIO1","seen",0);
+   geant3->Gsatt("UIO2","seen",0);
+   geant3->Gsatt("UIO3","seen",0);
+   geant3->Gsatt("UIO4","seen",0);
+   geant3->Gsatt("UIO5","seen",0);
+   geant3->Gsatt("UIO6","seen",0);
+
+   geant3->Gsatt("UT1I","seen",1);
+   geant3->Gsatt("UT1N","seen",1);
+   geant3->Gsatt("UT1O","seen",1);
+
+   geant3->Gsatt("UXI1","seen",1);
+   geant3->Gsatt("UXI2","seen",1);
+   geant3->Gsatt("UXI3","seen",1);
+   geant3->Gsatt("UXI4","seen",1);
+   geant3->Gsatt("UXI5","seen",1);
+   geant3->Gsatt("UXI6","seen",1);
+   geant3->Gsatt("UXN1","seen",1);
+   geant3->Gsatt("UXN2","seen",1);
+   geant3->Gsatt("UXN3","seen",1);
+   geant3->Gsatt("UXN4","seen",1);
+   geant3->Gsatt("UXN5","seen",1);
+   geant3->Gsatt("UXN6","seen",1);
+   geant3->Gsatt("UXO1","seen",1);
+   geant3->Gsatt("UXO2","seen",1);
+   geant3->Gsatt("UXO3","seen",1);
+   geant3->Gsatt("UXO4","seen",1);
+   geant3->Gsatt("UXO5","seen",1);
+   geant3->Gsatt("UXO6","seen",1);
+
+}
