1825051ce06e1d3bb7ad3fcf339d83617e3e2143
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
1999-07-28 16:36:07 +0000

    Use directly TMath::Pi() and not kPI from TVector.h

--- STEER/AliConst.h
+++ STEER/AliConst.h
@@ -3,8 +3,8 @@
  
 #include <TMath.h> 
   
-static const Double_t k2PI    = 2*kPI;
-static const Double_t kDegrad = kPI/180;
-static const Double_t kRaddeg = 180/kPI;
+static const Double_t k2PI    = 2*TMath::Pi();
+static const Double_t kDegrad = TMath::Pi()/180;
+static const Double_t kRaddeg = 180/TMath::Pi();
 
 #endif 
