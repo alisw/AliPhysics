ced48fbdca99eef042527cad2b35119dad0a5d6d
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
1999-08-03 19:50:59 +0000

    Introduce routines of GEANE

--- GEANT321/comad/gcadd.cxx
+++ GEANT321/comad/gcadd.cxx
@@ -1,18 +1,24 @@
 #if defined(CERNLIB_WINNT)
   #define gcaddc GCADDC
   #define gcaddf GCADDF
+  #define gcaddd GCADDD
   #define gcaddi GCADDI
   #define gcaddl GCADDL
   #define type_of_call _stdcall
 #else
   #define gcaddc gcaddc_
   #define gcaddf gcaddf_
+  #define gcaddd gcaddd_
   #define gcaddi gcaddi_
   #define gcaddl gcaddl_
   #define type_of_call
 #endif
 
 extern "C" char* type_of_call gcaddc(char *arg)
+{
+  return arg;
+}
+extern "C" double* type_of_call gcaddd(double *arg)
 {
   return arg;
 }
