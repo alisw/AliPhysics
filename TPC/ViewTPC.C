bbc39960159e7678b78671ad93209364eea25409
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
1999-11-04 17:03:50 +0000

    Update for the new version of the TPC

--- TPC/ViewTPC.C
+++ TPC/ViewTPC.C
@@ -1,28 +1,32 @@
 //void ViewTPC()
 {
   gMC->Gsatt("TPC","SEEN",0);
-  gMC->Gsatt("TGAS","SEEN",0);
-  gMC->Gsatt("TPSG","SEEN",0);
-  gMC->Gsatt("TPHV","SEEN",1);
-  gMC->Gsatt("TPMH","SEEN",1);
   gMC->Gsatt("TPEC","SEEN",0);
+  gMC->Gsatt("TGAS","SEEN",0);
+  gMC->Gsatt("TPMH","SEEN",1);
+  gMC->Gsatt("TPHV","SEEN",1);
+  gMC->Gsatt("TPSG","SEEN",0);
   gMC->Gsatt("TRCS","SEEN",1);
   gMC->Gsatt("TRCL","SEEN",1);
-  gMC->Gsatt("TPWL","SEEN",1);
+  gMC->Gsatt("TSGA","SEEN",0);
+  gMC->Gsatt("TSST","SEEN",0);
+  gMC->Gsatt("TLGA","SEEN",0);
+  gMC->Gsatt("TLST","SEEN",0);
   gMC->Gsatt("TPWI","SEEN",1);
+  gMC->Gsatt("TPWL","SEEN",0);
+  gMC->Gsatt("TPCO","SEEN",1);
+  gMC->Gsatt("TPOV","SEEN",0);
+  gMC->Gsatt("TPG1","SEEN",0);
   gMC->Gsatt("TPWS","SEEN",1);
   gMC->Gsatt("TPW1","SEEN",1);
-  gMC->Gsatt("TPS1","SEEN",1);
+  gMC->Gsatt("TPS1","SEEN",0);
   gMC->Gsatt("TPS2","SEEN",1);
-  gMC->Gsatt("TPG1","SEEN",1);
   gMC->Gsatt("TPG2","SEEN",1);
   gMC->Gsatt("TPWC","SEEN",1);
-  gMC->Gsatt("TPSI","SEEN",1); 
-  gMC->Gsatt("TPSO","SEEN",1);
-  gMC->Gsatt("TPCO","SEEN",1);
-  gMC->Gsatt("TPOV","SEEN",1);
+  gMC->Gsatt("TPSE","SEEN",1);
+  gMC->Gsatt("TPSI","SEEN",0);
+  gMC->Gsatt("TPSO","SEEN",0);
   gMC->Gsatt("TPVD","SEEN",1);
-  gMC->Gsatt("TLGA","SEEN",-1);
-  gMC->Gsatt("TSGA","SEEN",-1);
-
+  gMC->Gsatt("TIVC","SEEN",0);
+  gMC->Gsatt("TIVB","SEEN",0);
 }
