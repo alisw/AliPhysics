4a671e6cea74d44164a9204fa664b49685d006f5
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-03-02 14:36:17 +0000

    Derives from AliPHOSFastRecParticle now

--- PHOS/AliPHOSRecParticle.cxx
+++ PHOS/AliPHOSRecParticle.cxx
@@ -36,13 +36,13 @@ ClassImp(AliPHOSRecParticle)
   // ctor
  
   fPHOSTrackSegment = new AliPHOSTrackSegment(*ts) ; 
-  fE                = ts->GetEnergy() ; 
+  Float_t kenergy   = ts->GetEnergy() ; 
   TVector3 momdir   = ts->GetMomentumDirection() ;
-  fPx               = fE * momdir.X() ; 
-  fPy               = fE * momdir.Y() ; 
-  fPz               = fE * momdir.Z() ; 
+  fPx               = kenergy * momdir.X() ; 
+  fPy               = kenergy * momdir.Y() ; 
+  fPz               = kenergy * momdir.Z() ; 
   fType             = kUNDEFINED ;  
-                           
+  fE                = kenergy ;    // !!! all particles have mass = 0 
 }
 
 //____________________________________________________________________________
@@ -78,133 +78,4 @@ ClassImp(AliPHOSRecParticle)
     delete fPHOSTrackSegment ;
     fPHOSTrackSegment = 0 ; 
   } 
-}
-
-//____________________________________________________________________________
-Int_t AliPHOSRecParticle::DistancetoPrimitive(Int_t px, Int_t py)
-{
-  //  Compute distance from point px,py to a AliPHOSRecParticle considered as a Tmarker
-  //  Compute the closest distance of approach from point px,py to this marker.
-  //  The distance is computed in pixels units.
-
-  Double_t kRADDEG = 180. / TMath::Pi() ; 
-  Coord_t x = Phi() * kRADDEG     ;
-  Coord_t y = Theta() * kRADDEG     ;
-  const Int_t kMaxDiff = 10;
-  Int_t pxm  = gPad->XtoAbsPixel(x);
-  Int_t pym  = gPad->YtoAbsPixel(y);
-  Int_t dist = (px-pxm)*(px-pxm) + (py-pym)*(py-pym);
-  
-  if (dist > kMaxDiff) return 9999;
-  return dist;
-}
-
-//___________________________________________________________________________
- void AliPHOSRecParticle::Draw(Option_t *option)
- {
-   // Draw this AliPHOSRecParticle with its current attributes
-    
-   AppendPad(option);
- }
-
-//______________________________________________________________________________
-void AliPHOSRecParticle::ExecuteEvent(Int_t event, Int_t px, Int_t py)
-{
-  //  Execute action corresponding to one event
-  //  This member function is called when a AliPHOSRecParticle is clicked with the locator
-  //
-    
-  if (!gPad->IsEditable()) 
-    return ;
-
-  static TPaveText * clustertext = 0 ; 
-
-  switch (event) {
-    
-  case kButton1Down: {
-    Double_t kRADDEG = 180. / TMath::Pi() ; 
-    Coord_t x = Phi() * kRADDEG     ;
-    Coord_t y = Theta() * kRADDEG     ;
-    clustertext = new TPaveText(x-1, y+1, x+5, y+3, "") ;
-    Text_t  line1[40] ;
-    Text_t  line2[40] ;
-    sprintf( line1, "PID: %s ", (const char*)Name() ) ;
-    sprintf( line2, "ENERGY: %f ", Energy() ) ;
-    clustertext ->AddText(line1) ;
-    clustertext ->AddText(line2) ;
-    clustertext ->Draw("");   
-    gPad->Update() ; 
-    break ;
-  }
-  
-  case kButton1Up: {
-    delete clustertext ; 
-    clustertext = 0 ; 
-    gPad->Update() ; 
-   break ;
-  }
-  
-  }
-
-}
-//____________________________________________________________________________
-TString AliPHOSRecParticle::Name()
-{
-  TString  name ; 
-  switch (fType) {
-  case kGAMMA:
-    name = "PHOTON" ;
-    break ; 
-   case kELECTRON:
-     name = "ELECTRON" ;
-    break ; 
-  case kNEUTRAL:
-    name = "NEUTRAL" ;
-    break ; 
-   case kCHARGEDHADRON:
-    name = "CHARGED HADRON" ;
-    break ; 
-  case kNEUTRALHADRON:
-    name = "NEUTRAL HADRON" ; 
-    break ; 
-  case kNEUTRALEM:
-    name = "NEUTRAL EM" ; 
-    break ; 
-  case kGAMMAHADRON:
-    name = "PHOTON HADRON" ; 
-    break ; 
-
-  }
-  return name ; 
-}
-
-//______________________________________________________________________________
-void AliPHOSRecParticle::Paint(Option_t *)
-{
-// Paint this ALiRecParticle in theta,phi coordinate as a TMarker  with its current attributes
-
-  Double_t kRADDEG = 180. / TMath::Pi() ; 
-   Coord_t x = Phi() * kRADDEG     ;
-   Coord_t y = Theta() * kRADDEG     ;
-   Color_t markercolor = 1 ;
-   Size_t  markersize  = 1. ;
-   Style_t markerstyle = 5 ;
-
-   if (!gPad->IsBatch()) {
-     gVirtualX->SetMarkerColor(markercolor) ;
-     gVirtualX->SetMarkerSize (markersize)  ;
-     gVirtualX->SetMarkerStyle(markerstyle) ;
-   }
-   gPad->SetAttMarkerPS(markercolor,markerstyle,markersize) ;
-   gPad->PaintPolyMarker(1,&x,&y,"") ;
-}
-
-//____________________________________________________________________________
-void AliPHOSRecParticle::Print()
-{
-  cout << "AliPHOSRecParticle > " << "type is  " << Name() << endl 
-       << "                     " << "Energy = " << fE << endl 
-       << "                     " << "Px     = " << fPx << endl 
-       << "                     " << "Py     = " << fPy << endl 
-       << "                     " << "Pz     = " << fPz << endl ; 
 }
--- PHOS/AliPHOSRecParticle.h
+++ PHOS/AliPHOSRecParticle.h
@@ -22,43 +22,22 @@
 // --- AliRoot header files ---
 
 #include "AliPHOSTrackSegment.h"
-#include <cassert>
+#include "AliPHOSFastRecParticle.h"
 
-const static Int_t kUNDEFINED     = -1; 
-const static Int_t kGAMMA         = 0 ; 
-const static Int_t kELECTRON      = 1 ;
-const static Int_t kNEUTRAL       = 2 ;  
-const static Int_t kCHARGED       = 3 ;  
-const static Int_t kCHARGEDHADRON = 4 ;  
-const static Int_t kNEUTRALHADRON = 5 ;  
-const static Int_t kNEUTRALEM     = 6 ;  
-const static Int_t kGAMMAHADRON   = 7 ; 
-
-class AliPHOSRecParticle : public TParticle {
+class AliPHOSRecParticle : public AliPHOSFastRecParticle {
 
 public:
   
   AliPHOSRecParticle() {};          // ctor
   AliPHOSRecParticle(AliPHOSTrackSegment * ts) ;  // ctor
   AliPHOSRecParticle(const AliPHOSRecParticle & rp) ;  // ctor
-  AliPHOSRecParticle& operator= (const AliPHOSRecParticle& rp) { assert(0==1) ; } // forbidden
-
   virtual ~AliPHOSRecParticle() ; // dtor
 
-  virtual Int_t DistancetoPrimitive(Int_t px, Int_t py) ; 
-  virtual void Draw(Option_t *option) ;  
-  virtual void ExecuteEvent(Int_t event, Int_t px, Int_t py) ; 
   AliPHOSTrackSegment * GetPHOSTrackSegment() const { return fPHOSTrackSegment ; } 
-  Int_t GetType() { return fType ; } 
-  TString Name() ; 
-  virtual void Paint(Option_t * option="");
-  void Print() ; 
-  void SetType(Int_t type) { fType = type ; } 
 
 private:
 
   AliPHOSTrackSegment * fPHOSTrackSegment ; // pointer to the associated track segment in PHOS  
-  Int_t fType ;                             // identified particle type
 
   ClassDef(AliPHOSRecParticle,1)  // Reconstructed Particle, version 1
 
