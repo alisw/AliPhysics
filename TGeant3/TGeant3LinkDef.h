d1c15d11dfae08ca49cbbd88b7bc56de0ccd8851
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-03-21 11:42:35 +0000

    Adding GEANT3 common blocks to known structures for CINT

--- TGeant3/TGeant3LinkDef.h
+++ TGeant3/TGeant3LinkDef.h
@@ -17,4 +17,31 @@
 #pragma link C++ class  AliGeant3GeometryGUI-;
 #pragma link C++ class  AliGUIMaterial-;
 #pragma link C++ class  AliGUIMedium-;
+
+#pragma link C++ class Quest_t; 
+#pragma link C++ class Gcbank_t;
+#pragma link C++ class Gclink_t; 
+#pragma link C++ class Gcflag_t; 
+#pragma link C++ class Gckine_t; 
+#pragma link C++ class Gcking_t; 
+#pragma link C++ class Gckin2_t;
+#pragma link C++ class Gckin3_t;
+#pragma link C++ class Gcmate_t; 
+#pragma link C++ class Gctmed_t; 
+#pragma link C++ class Gctrak_t; 
+#pragma link C++ class Gcvolu_t; 
+#pragma link C++ class Gcsets_t; 
+#pragma link C++ class Gcnum_t; 
+#pragma link C++ class Gccuts_t; 
+#pragma link C++ class Gcmulo_t;
+#pragma link C++ class Gcphys_t; 
+#pragma link C++ class Gcopti_t; 
+#pragma link C++ class Gctlit_t; 
+#pragma link C++ class Gcvdma_t; 
+#pragma link C++ class Gctpol_t; 
+#pragma link C++ class Ertrio_t;
+#pragma link C++ class Eropts_t;
+#pragma link C++ class Eroptc_t;
+#pragma link C++ class Erwork_t;
+
 #endif
