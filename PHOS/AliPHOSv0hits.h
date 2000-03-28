30651ceed38be56bc348dfb98a3b6f7dc2d2aa34
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-03-28 14:19:02 +0000

    Adding new classe AliPHOSv0hits (new name for coding conventions

---
+++ PHOS/AliPHOSv0hits.h
@@ -0,0 +1,35 @@
+#ifndef ALIPHOSV0HITS_H
+#define ALIPHOSV0HITS_H
+/* Copyright(c) 1998-1999-2000, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+//_________________________________________________________________________
+// Version of AliPHOSv0 which allows for keeping all hits in TreeH
+//  This version is NOT recommended for Reconstruction analysis
+//                  
+//*-- Author: Gines MARTINEZ (SUBATECH)
+
+// --- ROOT system ---
+#include "TClonesArray.h"
+
+// --- AliRoot header files ---
+#include "AliPHOSv0.h"
+#include "AliPHOSReconstructioner.h"
+
+class AliPHOSv0hits : public AliPHOSv0 {
+
+public:
+
+  AliPHOSv0hits(void) ;
+  AliPHOSv0hits(const char *name, const char *title="") ;
+  virtual ~AliPHOSv0hits(void) ;
+
+  virtual void   AddHit( Int_t primary, Int_t id, Float_t *hits ) ; 
+// adds a hit to the hit tree (any pre=digitalization is done here (so large root file !!) 
+  void           FinishEvent(void) ;          // makes the digits from the hits 
+protected:
+
+  ClassDef(AliPHOSv0hits,1)  // Class AliPHOSv0 which allows to write ond disk al the information of the hits. 
+
+};
+
+#endif // AliPHOSV0HITS_H
