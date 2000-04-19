18739b8ed8cae16bb5f1b652bb64682b3e666de8
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-04-19 13:44:28 +0000

    Implements all the new classes (JB, AM)

--- RICH/RICHLinkDef.h
+++ RICH/RICHLinkDef.h
@@ -1,30 +1,32 @@
 #ifdef __CINT__
-/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- * See cxx source for full Copyright notice                               */
-
-/* $Id$ */
-
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ enum   Response_t;
 #pragma link C++ class  AliRICH-;
 #pragma link C++ class  AliRICHv0;
-#pragma link C++ class  AliRICHhit;
+#pragma link C++ class  AliRICHHit;
+#pragma link C++ class  AliRICHPadHit;
+#pragma link C++ class  AliRICHDigit;
+#pragma link C++ class  AliRICHTransientDigit;
+#pragma link C++ class  AliRICHRawCluster;
+#pragma link C++ class  AliRICHRecHit;
 #pragma link C++ class  AliRICHCerenkov;
-#pragma link C++ class  AliRICHdigit;
-#pragma link C++ class  AliRICHlist;
-#pragma link C++ class  AliRICHcluster;
-#pragma link C++ class  AliRICHreccluster;
-#pragma link C++ class  AliRICHRecCluster;
-#pragma link C++ class  AliRICHsegmentation;
-#pragma link C++ class  AliRICHresponse;
-#pragma link C++ class  AliRICHsegmentationV0;
-#pragma link C++ class  AliRICHresponseV0;
-#pragma link C++ class  AliRICHresponseCkv;
-#pragma link C++ class  AliRICHchamber;
-#pragma link C++ class  AliRICHpoints;
-#pragma link C++ class  AliRICHdisplay;
+#pragma link C++ class  AliRICHSegmentation;
+#pragma link C++ class  AliRICHSegmentationV0;
+#pragma link C++ class  AliRICHSegmentationV1;
+#pragma link C++ class  AliRICHResponse;
+#pragma link C++ class  AliRICHResponseV0;
+#pragma link C++ class  AliRICHGeometry;
+#pragma link C++ class  AliRICHGeometryV0;
+#pragma link C++ class  AliRICHChamber;
+#pragma link C++ class  AliRICHPoints;
+#pragma link C++ class  AliRICHDisplay;
+#pragma link C++ class  AliRICHEllipse;
+#pragma link C++ class  AliRICHDetect;
 #pragma link C++ class  AliRICHHitMap;
 #pragma link C++ class  AliRICHHitMapA1;
+#pragma link C++ class  AliRICHClusterFinder;
 #endif
+
+
