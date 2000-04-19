256f1ea357dd019a54008f399e51aeb120ab4b03
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-04-19 13:03:18 +0000

    Updated RICH constants.

--- RICH/AliRICHConst.h
+++ RICH/AliRICHConst.h
@@ -1,10 +1,5 @@
 #ifndef RICHConst_H
 #define RICHConst_H
-/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- * See cxx source for full Copyright notice                               */
-
-/* $Id$ */
-
 /////////////////////////////////////////////////////////////////////////////
 //
 //---------------------------------------------------------------------
@@ -22,8 +17,9 @@ const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber
 //
 //
 
-const Float_t adc_satm  = 1024; // dynamic range (10 bits)
+const Float_t adc_satm  = 4096; // dynamic range (10 bits)
 const Float_t zero_supm = 6.; // zero suppression
 const Float_t sig_noise = 500.; // electronics noise (no. of electrons)
 
+const Int_t kMaxNeighbours = 24; // max number of neighbours
 #endif
