5a5abf3c7dfd6ef00d54de155a2aa5e78186330f
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-05-02 07:47:50 +0000

    Precision parameter for pT sampling plus corresponding getter introduced.

--- EVGEN/AliGenParam.h
+++ EVGEN/AliGenParam.h
@@ -1,28 +1,53 @@
-#ifndef AliGenParam_H
-#define AliGenParam_H
+#ifndef ALIGENPARAM_H
+#define ALIGENPARAM_H
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
 /* $Id$ */
 
 #include "AliGenerator.h"
-#include "AliPythia.h"
-#include "TNamed.h"
 #include "TF1.h"
-#include "TArrayF.h"
 #include "TArrayI.h"
-#include "TTree.h"
-#include "TParticle.h"
-
+#include "AliPythia.h"
+#include "GenTypeDefs.h"
 //-------------------------------------------------------------
 class AliGenParam : public AliGenerator
 {
-protected:
-  Double_t (*fPtParaFunc)(Double_t*, Double_t*); //! Pointer to Pt parametrisation function
-  Double_t (*fYParaFunc )(Double_t*, Double_t*); //! Pointer to Y parametrisation function
-  Int_t    (*fIpParaFunc )();    //! Pointer to particle type parametrisation function
-    TF1* fPtPara;
-    TF1* fYPara;
+ public:
+    AliGenParam();
+    AliGenParam(Int_t npart, Param_t param);
+    AliGenParam(Int_t npart, Param_t param,
+		Double_t (*PtPara)(Double_t*, Double_t*),
+		Double_t (*YPara )(Double_t*, Double_t*),
+		Int_t    (*IpPara)()                      ); 
+    virtual ~AliGenParam();
+    virtual void Generate();
+    virtual void Init();
+    // select particle type
+    virtual void SetParam(Param_t param=jpsi_p) {fParam=param;}
+    // force decay type
+    virtual void SetForceDecay(Decay_t decay=dimuon) {fForceDecay=decay;}
+    virtual void SetWeighting(Weighting_t flag=analog) {fAnalog=flag;}	
+    virtual void SetCutOnChild(Int_t flag=0) {fCutOnChild=flag;}
+    virtual void SetChildMomentumRange(Float_t pmin=0, Float_t pmax=1.e10)
+	{fChildPMin = pmin; fChildPMax = pmax;}
+    virtual void SetChildPtRange(Float_t ptmin=0, Float_t ptmax=20.)
+	{fChildPtMin = ptmin; fChildPtMax = ptmax;}
+    virtual void SetChildPhiRange(Float_t phimin=-180., Float_t phimax=180)
+	{fChildPhiMin = TMath::Pi()*phimin/180;
+	fChildPhiMax = TMath::Pi()*phimax/180;}
+    virtual void SetChildThetaRange(Float_t thetamin=0, Float_t thetamax=180)
+	{fChildThetaMin = TMath::Pi()*thetamin/180;
+	fChildThetaMax = TMath::Pi()*thetamax/180;}
+    virtual void SetDeltaPt(Float_t delta=0.01) {fDeltaPt=delta;}
+    
+	    
+ protected:
+    Double_t (*fPtParaFunc)(Double_t*, Double_t*); //! Pointer to Pt parametrisation function
+    Double_t (*fYParaFunc )(Double_t*, Double_t*); //! Pointer to Y parametrisation function
+    Int_t    (*fIpParaFunc )();    //! Pointer to particle type parametrisation function
+    TF1* fPtPara;              // Transverse momentum parameterisation
+    TF1* fYPara;               // Rapidity parameterisation
     Param_t     fParam;        // Parameterisation type 
     Float_t     fdNdy0;        // central multiplicity per event
     Float_t     fYWgt;         // Y-weight
@@ -40,39 +65,15 @@ protected:
     Float_t     fChildPhiMax;  // Children maximum phi
     Float_t     fChildThetaMin;// Children minimum theta
     Float_t     fChildThetaMax;// Children maximum theta
-    TArrayI   fChildSelect;
-    AliPythia *fPythia;
+    Float_t     fDeltaPt;      // pT sampling in steps of fDeltaPt
+    TArrayI     fChildSelect;  // Children to be selected from decay products
+    AliPythia   *fPythia;      // Pointer to pythia object for decays
  private:
     // check if particle is selected as child
     Bool_t ChildSelected(Int_t ip);
     // all kinematic selection goes here
     Bool_t KinematicSelection(TParticle *particle);
- public:
-  AliGenParam();
-  AliGenParam(Int_t npart, Param_t param);
-  AliGenParam(Int_t npart, Param_t param,
-		   Double_t (*PtPara)(Double_t*, Double_t*),
-		   Double_t (*YPara )(Double_t*, Double_t*),
-		   Int_t    (*IpPara)()                      ); 
-  virtual ~AliGenParam();
-  virtual void Generate();
-  virtual void Init();
-  // select particle type
-  virtual void SetParam(Param_t param=jpsi_p) {fParam=param;}
-  // force decay type
-  virtual void SetForceDecay(Decay_t decay=dimuon) {fForceDecay=decay;}
-  virtual void SetWeighting(Weighting_t flag=analog) {fAnalog=flag;}	
-  virtual void SetCutOnChild(Int_t flag=0) {fCutOnChild=flag;}
-  virtual void SetChildMomentumRange(Float_t pmin=0, Float_t pmax=1.e10)
-      {fChildPMin = pmin; fChildPMax = pmax;}
-  virtual void SetChildPtRange(Float_t ptmin=0, Float_t ptmax=20.)
-      {fChildPtMin = ptmin; fChildPtMax = ptmax;}
-  virtual void SetChildPhiRange(Float_t phimin=-180., Float_t phimax=180)
-      {fChildPhiMin = TMath::Pi()*phimin/180;
-      fChildPhiMax = TMath::Pi()*phimax/180;}
-  virtual void SetChildThetaRange(Float_t thetamin=0, Float_t thetamax=180)
-      {fChildThetaMin = TMath::Pi()*thetamin/180;
-      fChildThetaMax = TMath::Pi()*thetamax/180;}
+
   ClassDef(AliGenParam,1) // Generator using parameterised pt- and y-distribution
 };
 #endif
