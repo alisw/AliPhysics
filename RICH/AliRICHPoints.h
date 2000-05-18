f06eabd737d6b778649aaffa8ea699e96c6764b1
jbarbosa <jbarbosa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-05-18 13:46:26 +0000

    Added ShowRing function.

--- RICH/AliRICHPoints.h
+++ RICH/AliRICHPoints.h
@@ -13,10 +13,11 @@
 
 class AliRICHPoints : public AliPoints {
 protected:
-   Int_t            fHitIndex;         // Link to hit number 
-   Int_t            fTrackIndex;       // Link to track number 
-   Int_t            fDigitIndex;       // Link to digit 
-   TMarker3DBox     *fMarker[3];        // pointer to  associated 3D-marker
+  Int_t            fIndex;            // Particle number in AliRun::fParticles
+  Int_t            fHitIndex;         // Link to hit number 
+  Int_t            fTrackIndex;       // Link to track number 
+  Int_t            fDigitIndex;       // Link to digit 
+  TMarker3DBox     *fMarker[3];       // pointer to  associated 3D-marker
 public:
   AliRICHPoints();
   AliRICHPoints(Int_t npoints);
@@ -25,20 +26,24 @@ public:
   Int_t                 GetHitIndex() {return fHitIndex;}
   Int_t                 GetTrackIndex(); // *MENU*
   Int_t                 GetDigitIndex() {return fDigitIndex;}
+  Int_t                 GetIndex() {return fIndex;}
   AliRICHHit           *GetHit() const;
   AliRICHDigit         *GetDigit() const;
   virtual const Text_t *GetName() const;
   virtual Text_t       *GetObjectInfo(Int_t px, Int_t py);
   TMarker3DBox         *GetMarker(Int_t i) {return fMarker[i];}
+  TParticle            *GetParticle() const;
   virtual void          InspectHit(); // *MENU*
   virtual void          DumpHit(); // *MENU*
   virtual void          InspectDigit(); // *MENU*
   virtual void          DumpDigit(); // *MENU*
   virtual void          GetCenterOfGravity(); // *MENU*
+  virtual void          ShowRing(Int_t highlight); // *MENU*
   virtual void          SetHitIndex(Int_t hitindex) {fHitIndex = hitindex;}
   virtual void          SetTrackIndex(Int_t trackindex) {fTrackIndex = trackindex;}
   virtual void          SetDigitIndex(Int_t digitindex) {fDigitIndex = digitindex;}
   virtual void          Set3DMarker(Int_t i,TMarker3DBox *marker) {fMarker[i] = marker;}  
+  virtual void          SetParticle(Int_t index) {fIndex = index;}
   ClassDef(AliRICHPoints,1) //Class to draw detector clusters (is PolyMarker3D)
 };
 #endif
