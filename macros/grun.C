45681ac3a9f003bcfd6e3304a747792458cd1b79
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-06-08 07:40:09 +0000

    Workaround for a scoping problem of Cint

--- macros/grun.C
+++ macros/grun.C
@@ -1,8 +1,10 @@
+Int_t ievent;
 void grun (Int_t nevent=1, const char *config="Config.C")
 {
   //
   // Simple macro to run aliroot in a batch mode
   //
+  ievent=nevent;
   gAlice->Init(config);
-  gAlice->Run(nevent);
+  gAlice->Run(ievent);
 }
