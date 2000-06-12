f6362a1736192ceae80a20d05e56a378d91cab8b
nilsen <nilsen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-06-12 23:52:10 +0000

    Updated for new ITS code.

--- ITS/ITSLinkDef.h
+++ ITS/ITSLinkDef.h
@@ -7,6 +7,8 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+
+// Standard ITS classes 
  
 #pragma link C++ class  AliITS-;
 #pragma link C++ class  AliITSv1-;
@@ -14,16 +16,58 @@
 #pragma link C++ class  AliITSv5-;
 #pragma link C++ class  AliITShit-;
 #pragma link C++ class  AliITSdigit;
+#pragma link C++ class  AliITSdigitSPD;
+#pragma link C++ class  AliITSdigitSDD;
+#pragma link C++ class  AliITSdigitSSD;
+#pragma link C++ class  AliITSTransientDigit;
 #pragma link C++ class  AliITSgeom-;
 #pragma link C++ class  AliITSgeomSPD;
-#pragma link C++ class  AliITSgeomSPD300-;
-#pragma link C++ class  AliITSgeomSPD425-;
 #pragma link C++ class  AliITSgeomSDD;
 #pragma link C++ class  AliITSgeomSSD;
-#pragma link C++ class  AliITSmodule-;
-#pragma link C++ class  AliITSmoduleSPD;
-#pragma link C++ class  AliITSmoduleSDD;
-#pragma link C++ class  AliITSmoduleSSD;
-#pragma link C++ class  AliITSdigitSSD;
+
+#pragma link C++ class AliITSgeomSPD300-;
+#pragma link C++ class AliITSgeomSPD425-;
+
+#pragma link C++ class  AliITSmodule;
+#pragma link C++ class  AliITSRecPoint;
+#pragma link C++ class  AliITSRawCluster;
+#pragma link C++ class  AliITSRawClusterSPD;
+#pragma link C++ class  AliITSRawClusterSDD;
+#pragma link C++ class  AliITSRawClusterSSD;
+#pragma link C++ class  AliITSMap;
+#pragma link C++ class  AliITSMapA1-;
+#pragma link C++ class  AliITSMapA2-;
+#pragma link C++ class  AliITSsegmentation;
+#pragma link C++ class  AliITSsegmentationSPD;
+#pragma link C++ class  AliITSsegmentationSDD;
+#pragma link C++ class  AliITSsegmentationSSD;
+#pragma link C++ class  AliITSresponse;
+#pragma link C++ class  AliITSresponseSPD;
+#pragma link C++ class  AliITSresponseSDD-;
+#pragma link C++ class  AliITSresponseSSD-;
+#pragma link C++ class  AliITSsimulation;
+#pragma link C++ class  AliITSsimulationSPD;
+#pragma link C++ class  AliITSsimulationSDD-;
+#pragma link C++ class  AliITSsimulationSSD;
+#pragma link C++ class  AliITSsimulationFastPoints;
+#pragma link C++ class  AliITSClusterFinder;
+#pragma link C++ class  AliITSClusterFinderSPD;
+#pragma link C++ class  AliITSClusterFinderSDD;
+#pragma link C++ class  AliITSClusterFinderSSD;
+#pragma link C++ class  AliITSDetType;
+// SDD simulation
+#pragma link C++ class  AliITSRawData;
+#pragma link C++ class  AliITSInStream-;
+#pragma link C++ class  AliITSOutStream-;
+#pragma link C++ class  AliITSHNode;
+#pragma link C++ class  AliITSHTable-;
+#pragma link C++ class  AliITSetfSDD;
+// SSD simulation and reconstruction
+#pragma link C++ class  AliITSdictSSD;
+#pragma link C++ class  AliITSdcsSSD;
+#pragma link C++ class  AliITSclusterSSD;
+#pragma link C++ class  AliITSpackageSSD;
+#pragma link C++ class  AliITSstatistics-;
+#pragma link C++ class  AliITSstatistics2-;
 
 #endif
