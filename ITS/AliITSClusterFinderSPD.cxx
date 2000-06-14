4ee323f43e3c5cfe7c05e109f0638a26be32b56a
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-06-14 17:43:38 +0000

    Another problem with HP fixed.

--- ITS/AliITSClusterFinderSPD.cxx
+++ ITS/AliITSClusterFinderSPD.cxx
@@ -222,7 +222,7 @@ void  AliITSClusterFinderSPD::GroupClusters()
   AliITSRawClusterSPD *clusterI;
   AliITSRawClusterSPD *clusterJ;
   
-  Int_t *label=new Int_t[nofClusters];  // activate this for DEC machines
+  Int_t *label = new Int_t [nofClusters];  // activate this for DEC machines
   Int_t i,j;
   for(i=0; i<nofClusters; i++) label[i] = 0;
   for(i=0; i<nofClusters; i++) {
