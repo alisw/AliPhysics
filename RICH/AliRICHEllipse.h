1cd97ac8d0eab963418ae525d7a330b527640839
dibari <dibari@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-06-30 16:35:53 +0000

    New drawing routine from Nico and Daniela

--- RICH/AliRICHEllipse.h
+++ RICH/AliRICHEllipse.h
@@ -12,10 +12,10 @@ class AliRICHEllipse :  public TPolyMarker3D {
 
  public:
     AliRICHEllipse();
-    AliRICHEllipse(Float_t cx, Float_t cy, Float_t omega, Float_t theta, Float_t phi);
+    AliRICHEllipse(Float_t cx, Float_t cy, Float_t omega, Float_t theta, Float_t phi, Float_t emiss);
     
     virtual          ~AliRICHEllipse();
-    virtual void CreatePoints(Int_t chamber);
+    virtual void CerenkovRingDrawing(Int_t chamber, Int_t track);
  private:
     Float_t fOmega;                    //Cherenkov angle 
     Float_t fTheta;                    //Incidence angle (dip angle)
@@ -23,6 +23,9 @@ class AliRICHEllipse :  public TPolyMarker3D {
     Float_t fCx;                       //Hit coordinate-x
     Float_t fCy;                       //Hit coordinate-y
     Float_t fh;                        //Distance from radiator to pads
+    Float_t fEmissPoint;               //Emission point
+    Float_t fCoordEllipse[2][100];     // Ellipse points to draw
+    
 
     ClassDef(AliRICHEllipse, 1)   //Utility class to draw an ellipse
 };
