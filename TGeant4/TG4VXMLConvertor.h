a93e8347a6dc33b8f23f5def4c9f8a866184e8ab
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-09-05 09:24:10 +0000

    added methods: Open/CloseMaterials(), WriteRotation(), Increase/DecreaseIndention()

--- TGeant4/TG4VXMLConvertor.h
+++ TGeant4/TG4VXMLConvertor.h
@@ -22,20 +22,26 @@ class TG4VXMLConvertor
     virtual ~TG4VXMLConvertor();
 
     // methods
+    virtual void OpenMaterials(const G4String& version, const G4String& date, 
+            const G4String& author, const G4String dtdVersion) = 0; 
     virtual void OpenSection(const G4String& name, const G4String& version,
 	    const G4String& date, const G4String& author,
             const G4String& topVolume) = 0;
     virtual void OpenComposition(const G4String& name) = 0;
+    virtual void CloseMaterials() = 0;
     virtual void CloseSection() = 0;
     virtual void CloseComposition() = 0;
 
     virtual void WriteMaterial(const G4Material* material) = 0; 
     virtual void WriteSolid(const G4VSolid* solid, G4String materialName) = 0; 
+    virtual void WriteRotation(const G4RotationMatrix* rotation) = 0; 
     virtual void WritePosition(G4String solidName, G4ThreeVector position) = 0; 
     virtual void WritePositionWithRotation(
                                G4String solidName, G4ThreeVector position,
                                const G4RotationMatrix* rotation) = 0; 
     virtual void WriteEmptyLine() = 0;
+    virtual void IncreaseIndention() = 0;
+    virtual void DecreaseIndention() = 0;
 };
 
 #endif //TG4_V_XML_CONVERTOR_H
