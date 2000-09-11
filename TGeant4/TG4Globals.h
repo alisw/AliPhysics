d5d031f67010affc5b6ba64fb0b70e3a7cabaced
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-09-11 11:50:43 +0000

    added typedef TG4RotationMatrixVector, AppendNumberToString() method

--- TGeant4/TG4Globals.h
+++ TGeant4/TG4Globals.h
@@ -15,12 +15,14 @@
 #include <g4std/set>
 #include <g4rw/tvordvec.h>
 #include <g4rw/tpordvec.h>
+#include <G4RotationMatrix.hh>
 
 class G4Material;
 class G4Element;
 
 typedef G4std::vector<G4bool>  TG4boolVector;
 typedef G4std::vector<G4int>   TG4intVector;
+typedef G4std::vector<const G4RotationMatrix*> TG4RotationMatrixVector;
 typedef G4std::set <G4String, G4std::less<G4String> > TG4StringSet; 
 typedef G4RWTValOrderedVector<G4double> TG4doubleVector;
 typedef G4RWTValOrderedVector<G4String> TG4StringVector;
@@ -41,6 +43,7 @@ class TG4Globals
       // program - according to G4Exception.cc
     static void Warning(const char* string=0);
       // Global warning function prints string to cerr
+    static void AppendNumberToString(G4String& string, G4int number);
 
   protected:
     TG4Globals();  
