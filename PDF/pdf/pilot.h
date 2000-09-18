889ed42f9748a90496af30950cdfb6c04a484b42
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-09-18 09:33:55 +0000

    PDF version 8 added

---
+++ PDF/pdf/pilot.h
@@ -0,0 +1,9 @@
+#if 0
+*CMZ :          16/03/95  22.26.48  by  H. Plothow-Besch
+*Modified by IML
+#endif
+#if !defined(CERNLIB_SINGLE)
+#ifndef CERNLIB_DOUBLE
+#define CERNLIB_DOUBLE
+#endif
+#endif
