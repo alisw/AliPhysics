3437556d9f4740c959b9439c81df333c85f7220a
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-09-20 10:31:28 +0000

    START added

--- macros/DrawTrees.C
+++ macros/DrawTrees.C
@@ -16,8 +16,12 @@ void DrawTrees()
    menu->AddButton("PMD tree",     "((TGeant3*)gMC)->Gdtree(\"DPMD\")","Shows the Geant tree for the PMD");
    menu->AddButton("RICH tree",    "((TGeant3*)gMC)->Gdtree(\"rich\")","Shows the Geant tree for the RICH");
    menu->AddButton("SHIL tree",    "((TGeant3*)gMC)->Gdtree(\"YMOT\")","Shows the Geant tree for the SHIL");
+   menu->AddButton("START tree",    "((TGeant3*)gMC)->Gdtree(\"STRT\")","Shows the Geant tree for the START");
    menu->AddButton("TOF tree",     "((TGeant3*)gMC)->Gdtree(\"FBAR\")","Shows the Geant tree for the TOF");
    menu->AddButton("TRD tree",     "((TGeant3*)gMC)->Gdtree(\"trd\")","Shows the Geant tree for the TRD");
    menu->AddButton("ZDC tree",     "((TGeant3*)gMC)->Gdtree(\"zdc\")","Shows the Geant tree for the ZDC");
    menu->Show();
 }
+
+
+
