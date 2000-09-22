178debfb0b945dfd14b5b8a7fe9542bfc803a537
nilsen <nilsen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-09-22 12:49:18 +0000

    New ITS test macro. This will run AliRoot to create the hits, run AliITSHits2Digits.C to create digits and AliITSFindClusters.C to fill the clusters and RecPoints classes.

---
+++ ITS/AliITStest.C
@@ -0,0 +1,32 @@
+Int_t AliITStest() {
+   Int_t rc=0;
+
+//Test ITS simulation
+   gROOT->LoadMacro("$(ALICE_ROOT)/macros/grun.C");
+   grun();
+
+   Int_t ver=gAlice->GetDetector("ITS")->IsVersion();
+   delete gAlice; gAlice=0;
+
+   if (ver!=5) {
+      cerr<<"Invalid ITS version: "<<ver<<" ! (must be 5 for the moment)\n";
+      return 12345;
+   }
+
+   if (ver==5) {
+     gROOT->LoadMacro("$(ALICE_ROOT)/ITS/AliITSHits2Digits.C");
+     if (rc=AliITSHits2Digits()) return rc;
+
+   }
+
+   printf("start reconstruction\n");
+
+//Test ITS reconstruction
+   gROOT->LoadMacro("$(ALICE_ROOT)/ITS/AliITSFindClusters.C");
+   if (rc=AliITSFindClusters()) return rc;
+
+   //gROOT->LoadMacro("$(ALICE_ROOT)/ITS/AliITSgraphycs.C");
+   //if (rc=AliITSgraphycs()) return rc;
+
+   return rc;
+}
