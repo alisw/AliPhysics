4e8295cee06313517d9d1c6d1c9532d1a6c157f7
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-10-02 21:54:48 +0000

    add NaN trap to linux version

--- ALIROOT/dummies.c
+++ ALIROOT/dummies.c
@@ -1,8 +1,14 @@
+#ifdef __linux
+#include <fpu_control.h>
+void __attribute__ ((constructor))
+trapfpe () {
+  (void) __setfpucw (_FPU_DEFAULT &
+                     ~(_FPU_MASK_IM | _FPU_MASK_ZM | _FPU_MASK_OM));
+}
+void MAIN__()  {}
+#endif
 void izrtoc_() {}
 void igmess_() {}
 void igloc2_() {}
 void igpxmp_() {}
 void izitoc_() {}
-#ifdef __linux
-void MAIN__()  {}
-#endif
