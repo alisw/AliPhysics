25d91f5319b86795a00943002b47ffc90e8be4dd
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-10-06 23:09:12 +0000

    New coarse geometry (symmetric services

---
+++ ITS/AliITSvPPRcoarsesymm.h
@@ -0,0 +1,37 @@
+#ifndef ALIITSVPPRCOARSESYMM_H
+#define ALIITSVPPRCOARSESYMM_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+/////////////////////////////////////////////////////////
+//  Manager and hits classes for set: ITS version 7    //
+/////////////////////////////////////////////////////////
+ 
+#include "AliITS.h"
+ 
+class AliITSvPPRcoarsesymm : public AliITS {
+
+ public:
+    AliITSvPPRcoarsesymm();
+    AliITSvPPRcoarsesymm(const char *name, const char *title);
+    AliITSvPPRcoarsesymm(const AliITSvPPRcoarsesymm &source); // copy constructor
+    AliITSvPPRcoarsesymm& operator=(const AliITSvPPRcoarsesymm &source); // assignment operator
+    virtual       ~AliITSvPPRcoarsesymm() ;
+    virtual void   BuildGeometry();
+    virtual void   CreateGeometry();
+    virtual void   CreateMaterials();
+    virtual void   Init(); 
+    virtual Int_t  IsVersion() const {
+      // returns the ITS version number 
+      return 7;
+    } 
+    virtual void   DrawModule();
+    virtual void   StepManager();
+
+    ClassDef(AliITSvPPRcoarsesymm,1)  //Hits manager for set:ITS version 7 
+                                       // PPR coarse Geometry symmetric
+};
+ 
+#endif
