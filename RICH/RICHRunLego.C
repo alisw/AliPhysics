d57aa8cb52e6afb4901fa8844d33051ef49eb3a8
jbarbosa <jbarbosa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-10-24 13:19:41 +0000

    Corrected RunLego() calls.

--- RICH/RICHRunLego.C
+++ RICH/RICHRunLego.C
@@ -2,10 +2,10 @@
 RICHRunLego()
 { 
    TControlBar *menu = new TControlBar("vertical","RICH Lego");
-   menu->AddButton("Center chamber (40 bins)","gAlice->RunLego(\"Config.C\",40,80,100,40,80,100,0,500,600)","Fast run");
-   menu->AddButton("All chambers (120 bins)","gAlice->RunLego(\"Config.C\",120,60,120,120,60,120,0,500,600)","Fast run");
-   menu->AddButton("Center chamber (160 bins)","gAlice->RunLego(\"Config.C\",1600,80,100,160,80,100,0,500,600)","Slow run");
-   menu->AddButton("All chambers hi (200 bins)","gAlice->RunLego(\"Config.C\",200,60,120,200,60,120,0,500,600)","Very slow run");
+   menu->AddButton("Center chamber (40 bins)","gAlice->RunLego(\"Config.C\",40,80,100,40,80,100,0,600,500)","Fast run");
+   menu->AddButton("All chambers (120 bins)","gAlice->RunLego(\"Config.C\",120,60,120,120,60,120,0,600,500)","Fast run");
+   menu->AddButton("Center chamber (80 bins)","gAlice->RunLego(\"Config.C\",80,80,100,80,80,100,0,600,500)","Slow run");
+   menu->AddButton("All chambers hi (200 bins)","gAlice->RunLego(\"Config.C\",200,60,120,200,60,120,0,600,500)","Very slow run");
    menu->Show();
 }
 
