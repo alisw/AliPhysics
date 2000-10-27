1625219fdb57a59244b9a41c10d8812007670e41
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-10-27 08:13:02 +0000

    Lego generator for phi-z binning.

---
+++ STEER/AliLegoGeneratorPhiZ.h
@@ -0,0 +1,33 @@
+#ifndef ALILEGOGENERATORPHIZ_H
+#define ALILEGOGENERATORPHIZ_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//                                                                           //
+//    Utility class to compute and draw Radiation Length Map                 //
+//                                                                           //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "AliLegoGenerator.h"
+
+class AliLegoGeneratorPhiZ : public AliLegoGenerator
+{
+
+ public:
+    virtual void    Generate();
+    ClassDef(AliLegoGeneratorPhiZ,1) //Lego GeneratorPhiZ
+};
+#endif
+
+
+
+
+
+
+
+
