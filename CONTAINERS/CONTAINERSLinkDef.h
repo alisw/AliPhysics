0840130dc47c73056a53c448558d028b389b6dde
kowal2 <kowal2@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-11-01 16:09:37 +0000

    new file

---
+++ CONTAINERS/CONTAINERSLinkDef.h
@@ -0,0 +1,30 @@
+#ifdef __CINT__
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+
+#pragma link C++ class AliClassInfo;
+#pragma link C++ class AliDataType;
+
+#pragma link C++ class AliMemArray;
+#pragma link C++ class AliObjectArray-;
+#pragma link C++ class TArrayOfArray;
+
+#pragma link C++ class TArrayOfArray_vStack;
+#pragma link C++ class TArrayOfArray_vList;
+
+
+#pragma link C++ class AliArraySubBranch;
+#pragma link C++ class AliArrayBranch-;
+#pragma link C++ class AliObjectBranch;
+#pragma link C++ class AliTree;
+
+#pragma link C++ class AliSegmentID;
+#pragma link C++ class AliSegmentArray-;
+#pragma link C++ class AliDigits;
+#pragma link C++ class AliArrayI; 
+#pragma link C++ class AliArrayS; 
+
+#endif
