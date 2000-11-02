26ee205d6a8b786b6a788c18d90534719cb7a0dc
kowal2 <kowal2@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-11-02 10:22:50 +0000

    Logs added

--- TPC/AliTPCTrackHits.h
+++ TPC/AliTPCTrackHits.h
@@ -3,7 +3,7 @@
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
-/* $Id */
+/* $Id$ */
 ////////////////////////////////////////////////
 //  Manager class for TPC   clusters                   //
 ////////////////////////////////////////////////
