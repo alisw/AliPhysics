a372b300e53130781fe4099b8b02cbecdb090390
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2000-12-18 07:38:43 +0000

    added mapping between G4 process names and AliMCProcess codes (fProcessMap, FillProcessMap()); added FillG3Cut/FlagNameVector() methods

--- TGeant4/TG4PhysicsManager.h
+++ TGeant4/TG4PhysicsManager.h
@@ -14,9 +14,11 @@
 #include "TG3Flag.h"
 #include "TG3ParticleWSP.h"
 
+#include <Rtypes.h>
+#include "AliMCProcess.h"
+
 #include <globals.hh>
 
-#include <Rtypes.h>
 
 class TG4CutVector;
 class TG4FlagVector;
@@ -55,6 +57,7 @@ class TG4PhysicsManager
     void Lock();     
     void SetProcessActivation();  
     G4int GetPDGEncodingFast(G4ParticleDefinition* particle);
+    AliMCProcess GetMCProcess(const G4String& g4ProcessName);
     G4bool CheckCutWithCutVector(
              G4String name, G4double value, TG3Cut& cut);   
     G4bool CheckFlagWithFlagVector(
@@ -89,6 +92,9 @@ class TG4PhysicsManager
   private:
     // methods
     void LockException() const;
+    void FillG3CutNameVector();
+    void FillG3FlagNameVector();
+    void FillProcessMap();
     G4int GetPDGEncoding(G4ParticleDefinition* particle);
     G4int GetPDGEncoding(G4String particleName);
 
@@ -119,6 +125,8 @@ class TG4PhysicsManager
                                         //and TDatabasePDG names 
     TG4IntMap        fParticlePDGMap;   //the mapping between G4 particle names
                                         //and TDatabasePDG codes
+    TG4IntMap        fProcessMap;       //the mapping between G4 process names
+                                        //and AliMCProcess codes
 };
 
 // inline methods
