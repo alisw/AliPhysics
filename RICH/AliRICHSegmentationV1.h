12ecede784626fa1eb9117e98b738109a71ba47b
jbarbosa <jbarbosa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-01-22 21:36:18 +0000

    added dead zone size to data members

--- RICH/AliRICHSegmentationV1.h
+++ RICH/AliRICHSegmentationV1.h
@@ -13,22 +13,30 @@ class AliRICHSegmentationV1 : public AliRICHSegmentationV0 {
  public:
     AliRICHSegmentationV1();
     virtual ~AliRICHSegmentationV1();
+
     // current sector
     virtual Int_t  ISector() {return fSector;}
+
     // calculate sector from x-y coordinates
     virtual Int_t  Sector(Float_t x, Float_t y);
 
     // Transform from pad to real coordinates
     virtual void    GetPadI(Float_t x ,Float_t y ,Int_t   &ix,Int_t   &iy);
     virtual void    GetPadI(Float_t x, Float_t y , Float_t z, Int_t &ix, Int_t &iy)  
-	{GetPadI(x, y, ix, iy);}
+      {GetPadI(x, y, ix, iy);}
+    
     // Transform from real to pad coordinates
     virtual void    GetPadC(Int_t   ix,Int_t   iy,Float_t &x ,Float_t &y );
     virtual void    GetPadC(Int_t ix, Int_t iy, Float_t &x, Float_t &y, Float_t &z) 
-	{z=0; GetPadC(ix, iy, x , y);}
+      {z=0; GetPadC(ix, iy, x , y);}
+    
     // Current integration limits
-    virtual void IntegrationLimits
-	(Float_t& x1, Float_t& x2, Float_t& y1, Float_t& y2);
+    virtual void IntegrationLimits (Float_t& x1, Float_t& x2, Float_t& y1, Float_t& y2);
+    
+ protected:
+
+    Float_t fDeadZone;               //width of dead zones between CsI photocathodes
+    
  private:
     ClassDef(AliRICHSegmentationV1,1)
 };
