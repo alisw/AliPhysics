69ff218e3a13caba61d26f822a3b56475312b9ab
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-03-07 12:23:05 +0000

    New improved version with error calculation

--- ITS/ITSPlotTracks.C
+++ ITS/ITSPlotTracks.C
@@ -13,10 +13,10 @@ void ITSPlotTracks(){
   TH1F *hp=new TH1F("hp","PHI resolution",50,-15.,15.); hp->SetFillColor(4);     
   TH1F *hl=new TH1F("hl","LAMBDA resolution",50,-15.,15.); hl->SetFillColor(4);  
   
- //TH1F *hp=new TH1F("hp","PHI resolution",100,-50.,50.); hp->SetFillColor(4);     
- // TH1F *hl=new TH1F("hl","LAMBDA resolution",100,-50.,50.); hl->SetFillColor(4); 
+  // TH1F *hp=new TH1F("hp","PHI resolution",50,-5.,5.); hp->SetFillColor(4);     
+  //TH1F *hl=new TH1F("hl","LAMBDA resolution",50,-5.,5.); hl->SetFillColor(4); 
       
- // TH1F *hpt=new TH1F("hpt","Relative Pt resolution",40,-20.,20.);
+ // TH1F *hpt=new TH1F("hpt","Relative Pt resolution",50,-5.,5.);
  TH1F *hpt=new TH1F("hpt","Relative Pt resolution",40,-10.,10.); 
   hpt->SetFillColor(2); 
   TH1F *hd=new TH1F("hd","Impact parameter distribution ",100,0,2000); 
@@ -35,12 +35,15 @@ void ITSPlotTracks(){
   */ 
 
   TH1F *hgood=new TH1F("hgood","Good tracks",10,0,2);
-  TH1F *hfound=new TH1F("hfound","Found tracks",10,0,2); 
-  TH1F *hfake=new TH1F("hfake","Fake tracks",10,0,2);    
+  hgood->Sumw2();  // aggiunto il 15-01-01
+  TH1F *hfound=new TH1F("hfound","Found tracks",10,0,2);
+  hfound->Sumw2();  // aggiunto il 15-01-01
+  TH1F *hfake=new TH1F("hfake","Fake tracks",10,0,2);
+  hfake->Sumw2();    
   TH1F *hg=new TH1F("hg","",10,0,2); //efficiency for good tracks  
   hg->SetLineColor(4); hg->SetLineWidth(2);
   TH1F *hf=new TH1F("hf","Efficiency for fake tracks",10,0,2);
-  hf->SetFillColor(1); hf->SetFillStyle(3013); hf->SetLineWidth(2);
+  /*hf->SetFillColor(1); hf->SetFillStyle(3013);*/ hf->SetLineColor(4); hf->SetLineWidth(2);
 
   /////////////////////////////////////////////////////////////////////////////////////////////////// 
 
@@ -52,7 +55,8 @@ void ITSPlotTracks(){
     hgood->Fill(ptg);
   }
   in1.close();
-    
+   
+  Int_t neglabs=0;	 // provvisoria
   for (;;){    
     for (int r=0; r<9; r++) in>>DataOut(r);
     if( in.eof() ) break;
@@ -61,6 +65,7 @@ void ITSPlotTracks(){
     Double_t deltalam=DataOut(4); Double_t deltaphi=DataOut(5);
     Double_t Dtot=DataOut(6); Double_t Dr=DataOut(7); Double_t Dz=DataOut(8);	
 
+     if(labITS<0) neglabs++;    // provvisoria
 	  if(labITS>=0) hfound->Fill(ptg); else 	{   hfake->Fill(ptg);}
 
 	  if(labITS>=0 ) {	  
@@ -72,7 +77,7 @@ void ITSPlotTracks(){
       hdz->Fill(Dz);
    }    
   }
- 
+  
 
   in.close();
   Stat_t ngood=hgood->GetEntries(); cerr<<"Good tracks "<<ngood<<endl;  
@@ -91,27 +96,32 @@ void ITSPlotTracks(){
   p4->cd(); hd->Draw(); c1->cd();
    
   TCanvas *c3=new TCanvas("c3","",200,200,800,500);
+  hfound->Print("all");  // aggiunto il 16-01-01
+  hgood->Print("all");  // aggiunto il 16-01-01
   TPad *p7=new TPad("p7","",0,0,0.333,1); p7->Draw(); p7->cd(); hfound->Draw(); c3->cd(); 
   TPad *p8=new TPad("p8","",0.333,0,0.666,1); p8->Draw(); p8->cd(); hfake->Draw(); c3->cd();
   TPad *p9=new TPad("p9","",0.666,0,1,1); p9->Draw(); p9->cd(); hgood->Draw(); c3->cd();
      
   TCanvas *c4=new TCanvas("c4","",300,300,800,500);
-  hg->Divide(hfound,hgood,1,1.,"b");
-  hf->Divide(hfake,hgood,1,1.,"b");
+  hg->Divide(hfound,hgood,1.,1.); //,"b");
+  hf->Divide(hfake,hgood,1.,1.); //,"b");
   hg->SetMaximum(1.4);
   hg->SetYTitle("Tracking efficiency");
   hg->SetXTitle("Pt (GeV/c)");
-  hg->Draw();
+  hg->Print("all");  // aggiunto il 16-01-01
+  hg->Draw();  // to not plot the erro bars    hg->Draw("histo");
   TLine *line1 = new TLine(0,1.0,2,1.0); line1->SetLineStyle(4);
   line1->Draw("same");
   TLine *line2 = new TLine(0,0.9,2,0.9); line2->SetLineStyle(4);
-  line2->Draw("same");
+  line2->Draw("histosame");
+  
    
   hf->SetFillColor(1);
   hf->SetFillStyle(3013);
   hf->SetLineColor(2);
   hf->SetLineWidth(2);
-  hf->Draw("histsame");
+  hf->Draw("same");  // to not plot the error bars  hf->Draw("histosame");
+  
   TText *text = new TText(0.461176,0.248448,"Fake tracks");
   text->SetTextSize(0.05);
   text->Draw();
@@ -124,5 +134,6 @@ void ITSPlotTracks(){
   p5->cd(); hdr->Draw();  hdr->Fit("gaus"); c2->cd();
   TPad *p6=new TPad("p6","",0.5,0,1,1); p6->Draw(); hdz->SetXTitle("(micron)");
   p6->cd(); hdz->Draw(); hdz->Fit("gaus"); c2->cd();
-   
+  
+  cout<<"neglabs = "<<neglabs<<"\n";  // provvisoria
 }
