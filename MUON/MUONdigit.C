45504e222508d15cc7c8b1ac8187b3e824255bb0
gosset <gosset@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-03-30 09:38:42 +0000

    Comments put back

--- MUON/MUONdigit.C
+++ MUON/MUONdigit.C
@@ -1,5 +1,47 @@
 void MUONdigit (Int_t evNumber1=0, Int_t evNumber2=0, Int_t ibg=0, Int_t bgr=10) 
 {
+  //////////////////////////////////////
+  //                                  //
+  // ROOT macro for ALICE Dimuon Arm: //
+  // Digitization                     //
+  //                                  //
+  //////////////////////////////////////
+  //
+  // Adds the tree TD for digits (charges deposited on pads)
+  // to the ROOT file "galice.root"
+  // containing the signal hit coordinates from simulation (tree TH).
+  // Eventually (argument "ibg"), background hits are also taken into account,
+  // from the ROOT file "bg.root".
+  //
+  // Arguments:
+  //   evNumber1 = first event number to digitize in file "galice.root"
+  //   evNumber2 = last event number to digitize in file "galice.root"
+  //   ibg       = 0 if no background hits to be taken into account;
+  //             = 1 if  background hits to be taken into account
+  //                     in file "bg.root"
+  //   bgr       : used only if "ibg" = 1
+  //             = number of events in the background file "bg.root";
+  //               the signal events are divided into "bgr" successive parts,
+  //               all events of each part are associated
+  //               with the same background event,
+  //               starting with event number 0,
+  //               incrementing it by 1 for the next part.
+  //               Strictly speaking, "bgr" can be smaller than
+  //               the number of events in the background file,
+  //               in which case one will only use
+  //               the first "bgr" events of the background file.
+  //               But it SHOULD NOT BE LARGER THAN
+  //               THE NUMBER OF EVENTS IN THE BACKGROUND FILE.
+  //
+  // Input file(s):
+  //   "galice.root" for signal
+  //   "bg.root" for background (used only if "ibg" = 1)
+  //
+  // Output file:
+  //   "galice.root"
+  //
+  //__________________________________________________________________________
+
 // Dynamically link some shared libs
 
    if (gClassTable->GetID("AliRun") < 0) {
