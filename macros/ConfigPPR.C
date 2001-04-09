7d6da3f906886d5c33dfd482cf4700926453d2c1
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-04-09 08:21:34 +0000

    New set-up and comments for ITS. (R. Barbera)

--- macros/ConfigPPR.C
+++ macros/ConfigPPR.C
@@ -144,19 +144,20 @@ void Config()
 
 
   if(iITS) {
+
     //=================== ITS parameters ============================
     //
     // As the innermost detector in ALICE, the Inner Tracking System "impacts" on
-    // almost all other detectors. This involves the fact that the ITS geometry 
-    // still has several options to be followed in parallel in order to determine 
+    // almost all other detectors. This involves the fact that the ITS geometry
+    // still has several options to be followed in parallel in order to determine
     // the best set-up which minimizes the induced background. All the geometries
-    // available to date are described in the following. Read carefully the comments 
+    // available to date are described in the following. Read carefully the comments
     // and use the default version (the only one uncommented) unless you are making
     // comparisons and you know what you are doing. In this case just uncomment the
-    // ITS geometry you want to use and run Aliroot. 
+    // ITS geometry you want to use and run Aliroot.
     //
-    // Detailed geometries:
-    // ====================
+    // Detailed geometries:         
+
     //
     //
     //AliITS *ITS  = new AliITSv3("ITS","Old ITS detailed version as of the ALICE TP");
@@ -167,27 +168,56 @@ void Config()
     //
     AliITS *ITS  = new AliITSv5asymm("ITS","Updates ITS TDR detailed version with asymmetric services");
     //
+    //AliITSvPPRasymm *ITS  = new AliITSvPPRasymm("ITS","New ITS PPR detailed version with asymmetric services");
+    //ITS->SetMinorVersion(2);
+    //ITS->SetReadDet(kFALSE);
+    //ITS->SetWriteDet("$ALICE_ROOT/ITS/ITSgeometry_vPPRasymm2.det");
+    //ITS->SetThicknessDet1(300.);   // detector thickness on layer 1 must be in the range [100,300]
+    //ITS->SetThicknessDet2(300.);   // detector thickness on layer 2 must be in the range [100,300]
+    //ITS->SetThicknessChip1(300.);  // chip thickness on layer 1 must be in the range [150,300]
+    //ITS->SetThicknessChip2(300.);  // chip thickness on layer 2 must be in the range [150,300]
+    //ITS->SetRails(1);              // 1 --> rails in ; 0 --> rails out
+    //ITS->SetCoolingFluid(1);       // 1 --> water ; 0 --> freon
     //
-    // Coarse geometries (warning: no hits are produced with these coarse geometries and they unuseful for reconstruction !):
-    // ======================================================================================================================
+    //AliITSvPPRsymm *ITS  = new AliITSvPPRsymm("ITS","New ITS PPR detailed version with symmetric services");
+    //ITS->SetMinorVersion(2);                                  
+    
+    //ITS->SetReadDet(kFALSE);
+    //ITS->SetWriteDet("$ALICE_ROOT/ITS/ITSgeometry_vPPRsymm2.det");
+    //ITS->SetThicknessDet1(300.);   // detector thickness on layer 1 must be in the range [100,300]
+    //ITS->SetThicknessDet2(300.);   // detector thickness on layer 2 must be in the range [100,300]
+    //ITS->SetThicknessChip1(300.);  // chip thickness on layer 1 must be in the range [150,300]
+    //ITS->SetThicknessChip2(300.);  // chip thickness on layer 2 must be in the range [150,300]
+    //ITS->SetRails(1);              // 1 --> rails in ; 0 --> rails out
+    //ITS->SetCoolingFluid(1);       // 1 --> water ; 0 --> freon
+    //
+    //
+    // Coarse geometries (warning: no hits are produced with these coarse geometries and they unuseful 
+    // for reconstruction !):
+    //                                                     
     //
     //
     //AliITS *ITS  = new AliITSv1("ITS","Old ITS coarse version as of the ALICE TP");
     //
-    //AliITS *ITS  = new AliITSvPPRcoarseasymm("ITS","New ITS coarse version with asymmetric services");
+    //AliITSvPPRcoarseasymm *ITS  = new AliITSvPPRcoarseasymm("ITS","New ITS coarse version with asymmetric services");
+    //ITS->SetRails(1);                // 1 --> rails in ; 0 --> rails out
+    //ITS->SetSupportMaterial(0);      // 0 --> Copper ; 1 --> Aluminum ; 2 --> Carbon
     //
     //AliITS *ITS  = new AliITSvPPRcoarsesymm("ITS","New ITS coarse version with symmetric services");
+    //ITS->SetRails(1);                // 1 --> rails in ; 0 --> rails out
+    //ITS->SetSupportMaterial(0);      // 0 --> Copper ; 1 --> Aluminum ; 2 --> Carbon
+    //                      
     //
     //
     // Geant3 <-> EUCLID conversion
     // ============================
     //
-    // SetEUCLID is a flag to output (=1) or not to output (=0) both geometry and 
-    // media to two ASCII files (called by default ITSgeometry.euc and 
-    // ITSgeometry.tme) in a format understandable to the CAD system EUCLID. 
+    // SetEUCLID is a flag to output (=1) or not to output (=0) both geometry and
+    // media to two ASCII files (called by default ITSgeometry.euc and
+    // ITSgeometry.tme) in a format understandable to the CAD system EUCLID.
     // The default (=0) means that you dont want to use this facility.
     //
-    ITS->SetEUCLID(0);
+    ITS->SetEUCLID(0);  
   }
 
 
