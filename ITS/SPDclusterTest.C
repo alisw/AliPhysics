a32dd78a55607c44eb2eb8e06db1bfc2cc38ad9f
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-04-18 13:26:00 +0000

    New version of the macros from Boris Batyunya

--- ITS/SPDclusterTest.C
+++ ITS/SPDclusterTest.C
@@ -29,12 +29,92 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
       if (gAlice) printf("AliRun object found on file\n");
       if (!gAlice) gAlice = new AliRun("gAlice","Alice test program");
    }
- 
+
+	  // ------------ Cluster and point analysis histogramms ------------
+
+	  TH1F *Nxpix1 = new TH1F("Nxpix1","Cluster size in x(r*phi) direction for layer 1",20,0.,20.);
+	  TH1F *Nxpix2 = new TH1F("Nxpix2","Cluster size in x(r*phi) direction for layer 2",20,0.,20.);
+	  TH1F *Nzpix1 = new TH1F("Nzpix1","Cluster size in z direction for layer 1",15,0.,15.);
+	  TH1F *Nzpix2 = new TH1F("Nzpix2","Cluster size in z direction for layer 2",15,0.,15.);
+	  TH1F *Xpix1 = new TH1F("Xpix1","Local x coordinate (mm) for layer 1",20,-2.,18.);
+	  TH1F *Xpix2 = new TH1F("Xpix2","Local x coordinate (mm) for layer 2",20,-2.,18.);
+	  TH1F *Zpix1 = new TH1F("Zpix1","Local z coordinate (mm) for layer 1",90,-2.,88.);
+	  TH1F *Zpix2 = new TH1F("Zpix2","Lolac z coordinate (mm) for layer 2",90,-2.,88.);
+
+	  TH1F *Xres1 = new TH1F("Xres1","Xrec and Xgen difference (micr) for layers 1",100,-200.,200.);
+	  TH1F *Xres2 = new TH1F("Xres2","Xrec and Xgen difference (micr) for layers 2",100,-200.,200.);
+	  TH1F *Zres1 = new TH1F("Zres1","Zrec and Zgen difference (micr) for layers 1",100,-800.,800.);
+	  TH1F *Zres2 = new TH1F("Zres2","Zrec and Zgen difference (micr) for layers 2",100,-800.,800.);
+
+	  // -------------- Create ntuples --------------------
+	  //  ntuple structures:
+
+	  struct {
+	    Int_t lay;
+	    Int_t nx;
+	    Int_t nz;
+	    Int_t hitprim;
+	    Int_t partcode;
+	    Int_t ntrover;
+	    Float_t dx;
+	    Float_t dz;
+	    Float_t pmod;
+	  } ntuple_st;
+
+	  struct {
+	    Int_t lay;
+	    Int_t lad;
+	    Int_t det;
+	    Int_t nx;
+	    Int_t nz;
+	    Int_t ntrover;
+	    Int_t noverlaps;
+	    Int_t noverprim;
+	    Float_t qcl;
+	    Float_t dx;
+	    Float_t dz;
+	  } ntuple1_st;
+
+	  struct {
+	    //	    Int_t lay;
+	    Int_t nx;
+	    Int_t nz;
+	  } ntuple2_st;
+
+	  ntuple = new TTree("ntuple","Demo ntuple");
+	  ntuple->Branch("lay",&ntuple_st.lay,"lay/I");
+	  ntuple->Branch("nx",&ntuple_st.nx,"nx/I");
+	  ntuple->Branch("nz",&ntuple_st.nz,"nz/I");
+	  ntuple->Branch("hitprim",&ntuple_st.hitprim,"hitprim/I");
+	  ntuple->Branch("partcode",&ntuple_st.partcode,"partcode/I");
+	  ntuple->Branch("ntrover",&ntuple_st.ntrover,"ntrover/I");
+	  ntuple->Branch("dx",&ntuple_st.dx,"dx/F");
+	  ntuple->Branch("dz",&ntuple_st.dz,"dz/F");
+	  ntuple->Branch("pmod",&ntuple_st.pmod,"pmod/F");
+
+	  ntuple1 = new TTree("ntuple1","Demo ntuple1");
+	  ntuple1->Branch("lay",&ntuple1_st.lay,"lay/I");
+	  ntuple1->Branch("lad",&ntuple1_st.lad,"lad/I");
+	  ntuple1->Branch("det",&ntuple1_st.det,"det/I");
+	  ntuple1->Branch("nx",&ntuple1_st.nx,"nx/I");
+	  ntuple1->Branch("nz",&ntuple1_st.nz,"nz/I");
+	  ntuple1->Branch("qcl",&ntuple1_st.qcl,"qcl/F");
+	  ntuple1->Branch("ntrover",&ntuple1_st.ntrover,"ntrover/I");
+	  ntuple1->Branch("noverlaps",&ntuple1_st.noverlaps,"noverlaps/I");
+	  ntuple1->Branch("noverprim",&ntuple1_st.noverprim,"noverprim/I");
+	  ntuple1->Branch("dx",&ntuple1_st.dx,"dx/F");
+	  ntuple1->Branch("dz",&ntuple1_st.dz,"dz/F");
+
+
+	  ntuple2 = new TTree("ntuple2","Demo ntuple2");
+	  //	  ntuple2->Branch("lay",&ntuple2_st.lay,"lay/I");
+	  ntuple2->Branch("nx",&ntuple2_st.nx,"nx/I");
+	  ntuple2->Branch("nz",&ntuple2_st.nz,"nz/I");
+
+// ------------------------------------------------------------------------
 //
 //   Loop over events 
 //
-   Int_t Nh=0;
-   Int_t Nh1=0;
    for (int nev=0; nev<= evNumber2; nev++) {
      Int_t nparticles = gAlice->GetEvent(nev);
      cout << "nev         " << nev <<endl;
@@ -45,10 +125,6 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
      TTree *TH = gAlice->TreeH();
      Int_t ntracks = TH->GetEntries();
      cout<<"ntracks "<<ntracks<<endl;
-
-   Int_t nbytes = 0;
-
-   AliITSRawClusterSPD  *ITSclust;
 
 // Get pointers to Alice detectors and Digits containers
    AliITS *ITS  = (AliITS*)gAlice->GetModule("ITS");
@@ -77,99 +153,12 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
 
           if (idettype != 0) continue;
 
-
-	  // ------------ Cluster and point analysis histogramms ------------
-
-	  TH1F *Nxpix1 = new TH1F("Nxpix1","Cluster size in x(r*phi) direction for layer 1",20,0.,20.);
-	  TH1F *Nxpix2 = new TH1F("Nxpix2","Cluster size in x(r*phi) direction for layer 2",20,0.,20.);
-	  TH1F *Nzpix1 = new TH1F("Nzpix1","Cluster size in z direction for layer 1",15,0.,15.);
-	  TH1F *Nzpix2 = new TH1F("Nzpix2","Cluster size in z direction for layer 2",15,0.,15.);
-	  TH1F *Xpix1 = new TH1F("Xpix1","Local x coordinate (mm) for layer 1",20,-2.,18.);
-	  TH1F *Xpix2 = new TH1F("Xpix2","Local x coordinate (mm) for layer 2",20,-2.,18.);
-	  TH1F *Zpix1 = new TH1F("Zpix1","Local z coordinate (mm) for layer 1",90,-2.,88.);
-	  TH1F *Zpix2 = new TH1F("Zpix2","Lolac z coordinate (mm) for layer 2",90,-2.,88.);
-
-	  TH1F *Xres1 = new TH1F("Xres1","Xrec and Xgen difference (micr) for layers 1",100,-200.,200.);
-	  TH1F *Xres2 = new TH1F("Xres2","Xrec and Xgen difference (micr) for layers 2",100,-200.,200.);
-	  TH1F *Zres1 = new TH1F("Zres1","Zrec and Zgen difference (micr) for layers 1",100,-800.,800.);
-	  TH1F *Zres2 = new TH1F("Zres2","Zrec and Zgen difference (micr) for layers 2",100,-800.,800.);
-
-
-	  // -------------- Create ntuples --------------------
-
-	  //  ntuple structures:
-
-	  struct {
-	    Int_t lay;
-	    Int_t nx;
-	    Int_t nz;
-	    Int_t hitprim;
-	    Int_t partcode;
-	    Float_t dx;
-	    Float_t dz;
-	    Float_t pmod;
-	  } ntuple_st;
-
-	  struct {
-	    Int_t lay;
-	    Int_t lad;
-	    Int_t det;
-	    Int_t nx;
-	    Int_t nz;
-	    Int_t ntrover;
-	    Int_t noverlaps;
-	    Int_t noverprim;
-	    Float_t qcl;
-	    Float_t dx;
-	    Float_t dz;
-	  } ntuple1_st;
-
-
-	  struct {
-	    //	    Int_t lay;
-	    Int_t nx;
-	    Int_t nz;
-	  } ntuple2_st;
-
-
-	  ntuple = new TTree("ntuple","Demo ntuple");
-	  ntuple->Branch("lay",&ntuple_st.lay,"lay/I");
-	  ntuple->Branch("nx",&ntuple_st.nx,"nx/I");
-	  ntuple->Branch("nz",&ntuple_st.nz,"nz/I");
-	  ntuple->Branch("hitprim",&ntuple_st.hitprim,"hitprim/I");
-	  ntuple->Branch("partcode",&ntuple_st.partcode,"partcode/I");
-	  ntuple->Branch("dx",&ntuple_st.dx,"dx/F");
-	  ntuple->Branch("dz",&ntuple_st.dz,"dz/F");
-	  ntuple->Branch("pmod",&ntuple_st.pmod,"pmod/F");
-
-	  ntuple1 = new TTree("ntuple1","Demo ntuple1");
-	  ntuple1->Branch("lay",&ntuple1_st.lay,"lay/I");
-	  ntuple1->Branch("lad",&ntuple1_st.lad,"lad/I");
-	  ntuple1->Branch("det",&ntuple1_st.det,"det/I");
-	  ntuple1->Branch("nx",&ntuple1_st.nx,"nx/I");
-	  ntuple1->Branch("nz",&ntuple1_st.nz,"nz/I");
-	  ntuple1->Branch("qcl",&ntuple1_st.qcl,"qcl/F");
-	  ntuple1->Branch("ntrover",&ntuple1_st.ntrover,"ntrover/I");
-	  ntuple1->Branch("noverlaps",&ntuple1_st.noverlaps,"noverlaps/I");
-	  ntuple1->Branch("noverprim",&ntuple1_st.noverprim,"noverprim/I");
-	  ntuple1->Branch("dx",&ntuple1_st.dx,"dx/F");
-	  ntuple1->Branch("dz",&ntuple1_st.dz,"dz/F");
-
-
-	  ntuple2 = new TTree("ntuple2","Demo ntuple2");
-	  //	  ntuple2->Branch("lay",&ntuple2_st.lay,"lay/I");
-	  ntuple2->Branch("nx",&ntuple2_st.nx,"nx/I");
-	  ntuple2->Branch("nz",&ntuple2_st.nz,"nz/I");
-
-// ------------------------------------------------------------------------
-
 	  // Module loop
-
 	  for (Int_t mod=0; mod<nent; mod++) {
 	      AliITSmodule *itsModule = (AliITSmodule*)ITSmodules->At(mod);
 
 	      Int_t nhits = itsModule->GetNhits();
-              if(nhits) printf("module nhits %d %d\n",mod,nhits);
+              //if(nhits) printf("module nhits %d %d\n",mod,nhits);
 	      if(!nhits) continue;
      
               ITS->ResetClusters();
@@ -210,15 +199,6 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
 		Float_t dxprimlast = 10.e+6;
 		Float_t dzprimlast = 10.e+6;
 
-		if(clustersizex>2&&clustersizez>1) {
-		//        if(module > 217 && module <  226) {
-	      if (nclust) printf("Found %d clust for module %d in det type %d \n",nclust,mod,idettype);
-		cout<<"mod,nclust,clu,Nxpix,Nzpix ="<<mod<<","<<nclust<<","<<clu<<","<<clustersizex<<","<<clustersizez<<endl;
-		// cout<<"clusx,clusz ="<<clusterx<<","<<clusterz<<endl;
-		cout<<"XStartf,XStopf,Zend,Zstart,Zstop,Q ="<<xstart<<","<<xstop<<","<<zend<<","<<zstart<<","<<zstop<<","<<clusterQ<<endl;
-		}
-		
-
         	Float_t SPDlength = 83600;	
         	Float_t SPDwidth = 12800;	
                 Float_t xhit0 = 1e+5;
@@ -240,7 +220,7 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
 		  Int_t clusterladder= hitladder;
 		  Int_t clusterdetector = hitdet;
 
-		  Int_t track = itsHit->fTrack;
+		  Int_t track = itsHit->GetTrack();
 		  Int_t dray = 0;
 		  Int_t hitstat = itsHit->GetTrackStatus();
 
@@ -284,7 +264,6 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
 		  Float_t xdif = xmed - clusterx;
 		  Float_t zdif = zmed - clusterz;
 
-		  // cout<<"clu,hit,xmed,fxstart,fxstop,zmed,zstart,zstop ="<<clu<<","<<hit<<","<<xmed<<","<<fxstart<<","<<fxstop<<","<<zmed<<","<<zstart<<","<<zstop<<endl;
 
         // Consider the hits inside of cluster region only
 
@@ -302,13 +281,15 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
 //  partcode (pdgCode): 11 - e-, 13 - mu-, 22 - gamma, 111 - pi0, 211 - pi+
 //                      310 - K0s, 321 - K+, 2112 - n, 2212 - p, 3122 - lambda
 
+	Float_t pmod = itsHit->GetParticle()->P(); // the momentum at the
+	                                           // vertex
+        pmod *= 1.0e+3;
 
-        Float_t px = itsHit->GetPXL();
+	/*
+        Float_t px = itsHit->GetPXL();  // the momenta at this GEANT point
         Float_t py = itsHit->GetPYL();
         Float_t pz = itsHit->GetPZL();
-        Float_t pmod = 1000*sqrt(px*px+py*py+pz*pz);
-
-	// cout<<"track,partcode,pmod,parent ="<<track<<","<<partcode<<","<<pmod<<","<<parent<<endl;
+	*/
 
         Int_t hitprim = 0;
 
@@ -338,6 +319,7 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
          ntuple_st.nz = clustersizez;
          ntuple_st.hitprim = hitprim;
          ntuple_st.partcode = partcode;
+         ntuple_st.ntrover = ntrover;
          ntuple_st.dx = xdif;
          ntuple_st.dz = zdif;
          ntuple_st.pmod = pmod;
@@ -348,12 +330,10 @@ void SPDclusterTest (Int_t evNumber1=0,Int_t evNumber2=0)
 
       if(hitprim > 0) {   // for primary particles
         if(hitlayer == 1) {
-	  // cout<<"!!!!!! lay,hitprim,xdif,zdif ="<<hitlayer<<","<<hitprim<<","<<xdif<<","<<zdif<<endl;
            Xres1->Fill(xdif);
            Zres1->Fill(zdif);
         }
         if(hitlayer == 2) {
-	  // cout<<"!!!!!! lay,hitprim,xdif,zdif ="<<hitlayer<<","<<hitprim<<","<<xdif<<","<<zdif<<endl;
            Xres2->Fill(xdif);
            Zres2->Fill(zdif);
         }
