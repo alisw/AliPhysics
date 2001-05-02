0ffa659233c4a0394f8ff6568d10052c5ad27a76
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-05-02 11:53:35 +0000

    AliPIPEv0 is again default pipe version.

--- macros/Config.C
+++ macros/Config.C
@@ -144,7 +144,7 @@ void Config()
   if(iPIPE) {
     //=================== PIPE parameters ============================
 
-    AliPIPE *PIPE  = new AliPIPEvTemp("PIPE","Beam Pipe");
+    AliPIPE *PIPE  = new AliPIPEv0("PIPE","Beam Pipe");
   }
 
 
