c8a27b8db4cd095162c8eb1867f1f29cec17c4d0
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-05-03 18:33:39 +0000

    TG4GUI renamed to TG4MainFrame; major update of geometry browser classes by D.Adamova; volumes and material properties panels added

---
+++ TGeant4/TG4MainFrame.h
@@ -0,0 +1,78 @@
+// $Id$
+// Category: interfaces
+//
+// Author: D. Adamova
+//
+//======================================================
+//
+//------------TG4MainFrame.h--------------------------------//
+//---------Main Window for the AG4 Geometry Browser---//
+//
+//=======================================================
+
+#ifndef TG4_MAIN_FRAME_H
+#define TG4_MAIN_FRAME_H
+
+#include <TGFrame.h>
+
+class TG4VolumesFrames;
+class TG4MaterialsFrames;
+class TObject;
+class TGListTreeItem;
+class TGPicture;
+class TGListTree;
+class TGTab;
+class TGMenuBar;
+class TGPopupMenu;
+class TGCanvas;
+
+
+class TG4MainFrame : public TGMainFrame {
+
+public:   
+    
+    TG4MainFrame(const TGWindow *p, UInt_t w, UInt_t h);
+    ~TG4MainFrame();
+ 
+    TGListTreeItem*
+        AddItem(TObject* obj, TGListTreeItem* parent,const char* name,
+                const TGPicture* open, const TGPicture* closed);
+		
+     TG4VolumesFrames* GetVolumesFrames() const;
+     TG4MaterialsFrames* GetMaterialsFrames() const;
+     void CloseWindow();		    			    
+     Bool_t ProcessMessage(Long_t msg, Long_t parm1, Long_t parm2);
+//---------------------------------------------------------------------------
+
+protected:
+
+    TG4MainFrame& operator=(const TG4MainFrame& mf);
+    TG4MainFrame(const TG4MainFrame& mf); 		    
+//----------------------------------------------------
+
+private:
+
+    TGMenuBar*          fMenuBar;          // main menu bar   
+
+    TGPopupMenu*        fPopupMenu;     // popup for window manipulations
+    TGPopupMenu*        fPopupMenuTest; // popup for test messages
+    TGPopupMenu*        fPopupMenuHelp; // popup for help messages
+
+    TGLayoutHints*      fMenuBarItemLayout;// layout left
+    TGLayoutHints*      fMenuBarHelpLayout;// layout right
+    TGLayoutHints*      fMenuBarLayout;    // main bar layout 
+    
+    TGTab*      fTab;           // tab widget
+    TGCanvas*   fCanvasWindow;  // Canvas window for the list tree
+    
+    TGListTree*     fVolumesListTree;     // volumes list tree 
+    
+    TG4VolumesFrames*   fvolumesFrames;  // service class for adding vols subframes 
+    TG4MaterialsFrames*   fmaterialsFrames;  // service class for adding mats subframes 
+			
+    ClassDef(TG4MainFrame,0)  // the main frame for the TG4 Browser  
+  };
+  
+//
+
+#endif
