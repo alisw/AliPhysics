434a26be37ba12210dfc38c783f5cdbee296f940
jbarbosa <jbarbosa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-05-10 12:27:15 +0000

    Added several more functions (memory management mainly).

--- RICH/AliRICHDetect.h
+++ RICH/AliRICHDetect.h
@@ -11,6 +11,7 @@
 /////////////////////////////////////////////////////////
 
 #include "AliRICH.h"
+#include "TCanvas.h"
 
 class AliRICHDetect;
 
@@ -19,15 +20,22 @@ class AliRICHDetect : public TObject {
  public:
   AliRICHDetect();
   AliRICHDetect(const char *name, const char *title);
-  virtual       ~AliRICHDetect() {}
-  void   Detect();
+  virtual       ~AliRICHDetect();
+  void   Detect(Int_t nev);
   float Area(float theta,float OMEGA);
-  //virtual void AddRecHit(const AliRICHRecHit);
-  //virtual int  ***i3tensor(long nrl, long nrh, long ncl, long nch, long ndl, long ndh);
+  Int_t  ***i3tensor(long nrl, long nrh, long ncl, long nch, long ndl, long ndh);
+  void free_i3tensor(int ***t, long nrl, long nrh, long ncl, long nch,long ndl, long ndh);
+  Float_t SnellAngle(Float_t iangle);
+  Float_t InvSnellAngle(Float_t rangle);
+  void CreatePoints(Float_t theta, Float_t phi, Float_t omega, Float_t h);
+  
+ public:
+
+  TCanvas *fc1;                   //Online reconstruction data
+  TCanvas *fc2;                   //Online SPOT reconstruction data 
+  TCanvas *fc3;                   //Online digits' coordinates data
+
   ClassDef(AliRICHDetect,1)  //Reconstruction module for :RICH version 0
-      
-      
-	
 	};
 
 
