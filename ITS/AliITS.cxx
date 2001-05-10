8c9572ed43a295c2df9e748d93ab7ea74d1fd7d1
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-05-10 18:14:25 +0000

    A typo corrected

--- ITS/AliITS.cxx
+++ ITS/AliITS.cxx
@@ -15,6 +15,9 @@
 
 /*
 $Log$
+Revision 1.47  2001/05/10 17:55:59  barbera
+Modified to create rec points also for PPR geometries
+
 Revision 1.46  2001/05/10 00:05:28  nilsen
 Allowed for HitsToDigits function to work with versions 5, 7, 8, and 9. This
 should probably be cleaned up to only check to make sure that fITSgeom has
@@ -1206,8 +1209,7 @@ void AliITS::DigitsToRecPoints(Int_t evNumber,Int_t lastentry,Option_t *opt)
    // initialised for all versions - for the moment it is only for v5 !
    // 7 is the SDD beam test version  
    Int_t ver = this->IsVersion(); 
-   if(ver!=5 && ver!=6 && ver!=8 && ver!=9) return;
-   //if(ver!=5) return; 
+   if(ver!=5 && ver!=8 && ver!=9) return;
 
    const char *all = strstr(opt,"All");
    const char *det[3] = {strstr(opt,"SPD"),strstr(opt,"SDD"),strstr(opt,"SSD")};
@@ -1292,7 +1294,7 @@ Option_t *option,Option_t *opt,Text_t *filename)
    // the condition below will disappear when the geom class will be
    // initialised for all versions - for the moment it is only for v5 !  
    Int_t ver = this->IsVersion(); 
-   if(ver!=5 && ver!=6 && ver!=8 && ver!=9) return;
+   if(ver!=5 && ver!=8 && ver!=9) return;
    //if(ver!=5) return; 
 
    const char *all = strstr(opt,"All");
