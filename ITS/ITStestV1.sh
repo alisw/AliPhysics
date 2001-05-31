41047f559224426076c76c7964a2ac11f5fca290
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-05-31 11:59:38 +0000

    Updated version to work with the current versions of TPC and ITS tracking (R.Barbera)

--- ITS/ITStestV1.sh
+++ ITS/ITStestV1.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
+# delete eventual old files from the last run
 DeleteOldFiles
-# do simulation, digitization, reconstruction and tracking in TPC
-aliroot -q AliTPCtracks.C
-# do digitization in ITS
-aliroot -q ITSHitsToDigits.C
-# do reconstruction in ITS
-aliroot -q ITSDigitsToClusters.C
-# do tracking in ITS
-aliroot -q ITStracks.C
-aliroot -q ITStracking.C
+# run the hit generation
+aliroot -q -b grun.C
+# digitize TPC
+aliroot -q -b AliTPCHits2Digits.C
+# do tracking in TPC
+aliroot -q -b AliTPCtracknew.C
+# prepare TPC tracks for matching with the ITS
+aliroot -q -b TPCtracks.C
+# digitize ITS
+aliroot -q -b ITSHitsToDigits.C
+# create reconstruct point for the ITS
+aliroot -q -b ITSDigitsToClusters.C
+# prepare for tracking
+aliroot -q -b ITStracks.C
+# do the tracking
+aliroot -q -b ITStracking.C
+#
+# after all of the above you can run ITSPlotTracks.C macro under aliroot to
+# see plots of the efficiency and track parameter resolution
