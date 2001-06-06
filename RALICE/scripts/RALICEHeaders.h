3f19e6851003a01ba2d12104aa91085116451f57
nick <nick@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-06-06 13:33:46 +0000

    Subdirectory "scripts" created in RALICE. This subdirectory contains some scripts to install the RALICE physics analysis tools in standalone mode and also contains the files evtwrite.cc and evtread.cc to run some tests. The files evtwrite.log and evtread.log contain the output of successfully run tests of these macros in order to check proper functioning. As can be seen from the .log files, the tests run properly under ROOT 3.00/06 using the old schema evolution.

---
+++ RALICE/scripts/RALICEHeaders.h
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+// All headers of RALICE
+// This header list is used to create the RALICE dictionary via rootcint
+// in the automatic installation of the ROOT loadable library.
+//
+// Note : Class names have also to be entered into the list in the file
+//        RALICELinkDef.h
+//
+//--- NvE 12-apr-1998 UU-SAP Utrecht
+////////////////////////////////////////////////////////////////////////////////
+ 
+#include "AliMath.h"
+#include "AliRandom.h"
+#include "AliSample.h"
+#include "Ali3Vector.h"
+#include "Ali3VectorObj.h"
+#include "Ali4Vector.h"
+#include "Ali4VectorObj.h"
+#include "AliBoost.h"
+#include "AliPosition.h"
+#include "AliPositionObj.h"
+#include "AliSignal.h"
+#include "AliCalorimeter.h"
+#include "AliCalmodule.h"
+#include "AliCalcluster.h"
+#include "AliTrack.h"
+#include "AliJet.h"
+#include "AliVertex.h"
+#include "AliInvmass.h"
+#include "AliEvent.h"
