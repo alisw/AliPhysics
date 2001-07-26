b6c768374d2f7e6c7183f6ce5d5b09f5838bb4f9
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-07-26 13:22:17 +0000

    Added to constants

--- HLT/hough/AliL3Defs.h
+++ HLT/hough/AliL3Defs.h
@@ -8,6 +8,7 @@ const Int_t NRows[5][2] = {{ 0, 45},{46,77},{78,109},{110,141},{142,173}};
 //const Int_t NRows[5][2] = {{ 0, 31},{46,173},{78,109},{110,141},{142,173}};
 const Double_t Pi = 3.14159265358979323846;
 const Double_t ToRad = Pi/180.;
-
+const Int_t MaxNPads = 256;
+const Int_t MaxNTimeBins = 512;
 
 #endif /* _ALIL3DEFS_H_ */
