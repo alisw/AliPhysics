c2fb1f2ddc8216b45bfde0d142251fd8338fc7bf
jchudoba <jchudoba@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-07-27 13:02:12 +0000

    ABC for detector digits merging/digitization

---
+++ STEER/AliDigitizer.h
@@ -0,0 +1,37 @@
+#ifndef ALIDIGITIZER_H
+#define ALIDIGITIZER_H
+/* Copyright(c) 1998-2001, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id */
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Pure Virtual Base Class for Detector specific Merging/Digitization   
+//                  
+//  Author: Jiri Chudoba (CERN)
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "TNamed.h"
+class AliRunDigitizer;
+
+class AliDigitizer: public TNamed {
+
+ public:
+    AliDigitizer();                               // default ctor - dummy
+    AliDigitizer(AliRunDigitizer *manager);       // ctor to be used          
+    virtual ~AliDigitizer();
+    virtual Bool_t Init() = 0;
+    virtual void Digitize() = 0;
+    Int_t GetDebug() {return fDebug;}             // get debug level
+    void SetDebug(Int_t level){fDebug = level;}   // set debug level
+    
+private:
+
+    Int_t fDebug;                                 // debug level
+
+    ClassDef(AliDigitizer,1)
+};
+
+#endif // ALIDIGITIZER_H
