a4183ec7ee0469cc4ad3fbc845d5a6d1e3159563
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-08-09 12:22:11 +0000

    attribute fSDManager removed; methods CreateSensitiveDetectors/1/2() moved to AliSDConstruction; comment lines separating methods added

--- AliGeant4/AliMoreModulesConstruction.cxx
+++ AliGeant4/AliMoreModulesConstruction.cxx
@@ -5,22 +5,21 @@
 
 #include "AliMoreModulesConstruction.h"
 #include "AliSingleModuleConstruction.h"
-#include "AliSDManager.h"
 #include "AliModule.h"
 #include "AliGlobals.h"
 #include "AliFiles.h"
 
 #include "TG4GeometryManager.h"
 
-#include <G4SDManager.hh>
 #include <G4LogicalVolume.hh>
 #include <G4LogicalVolumeStore.hh>
 
+//_____________________________________________________________________________
 AliMoreModulesConstruction::AliMoreModulesConstruction() {
 //
-  fSDManager = AliSDManager::Instance();
 }
 
+//_____________________________________________________________________________
 AliMoreModulesConstruction::AliMoreModulesConstruction(
                                const AliMoreModulesConstruction& right)
 {
@@ -29,6 +28,7 @@ AliMoreModulesConstruction::AliMoreModulesConstruction(
 }  
   			       
 
+//_____________________________________________________________________________
 AliMoreModulesConstruction::~AliMoreModulesConstruction()
 {
   // delete module constructions
@@ -38,6 +38,7 @@ AliMoreModulesConstruction::~AliMoreModulesConstruction()
 
 // operators
 
+//_____________________________________________________________________________
 AliMoreModulesConstruction& 
 AliMoreModulesConstruction::operator=(const AliMoreModulesConstruction& right)
 {
@@ -57,69 +58,12 @@ AliMoreModulesConstruction::operator=(const AliMoreModulesConstruction& right)
     AddModule(name, version, type);
   }  
 
-  fSDManager = right.fSDManager;
-
   return *this;  
 }    
           
-// private methods
-
-void AliMoreModulesConstruction::CreateSensitiveDetectors(
-                                   G4bool allLVSensitive)
-{
-// Creates sensitive detectors.
-// ---
-
-  if (allLVSensitive)
-    CreateSensitiveDetectors1();
-  else
-    CreateSensitiveDetectors2();
-
-  // set static number of logical volumes already processed
-  G4LogicalVolumeStore* pLVStore = G4LogicalVolumeStore::GetInstance();
-  fSDManager->SetNofLVWithSD(pLVStore->size());  
-}    
-
-void AliMoreModulesConstruction::CreateSensitiveDetectors1()
-{ 
-// Creates sensitive detectors.
-// Sensitive detectors are set to all logical volumes
-// ---
-
-  G4LogicalVolumeStore* pLVStore = G4LogicalVolumeStore::GetInstance();
-  G4int nofLV = pLVStore->size();
-  
-  G4int nofLVWithSD = fSDManager->GetNofLVWithSD();
-  for (G4int i=nofLVWithSD; i<nofLV; i++) {
-    G4LogicalVolume* lv = (*pLVStore)[i];
-    AliModule* module = fSDManager->FindAliModule(lv);
-    fSDManager->CreateSD(lv, module);
-  }
-}
-
-void AliMoreModulesConstruction::CreateSensitiveDetectors2()
-{ 
-// Creates sensitive detectors.
-// Sensitive detectors are set only to logical volumes
-// in G3SensVolVector.
-// ---
-
-  TG4GeometryManager* pGeometryManager = TG4GeometryManager::Instance();
-
-  G3SensVolVector pSVVector
-    = pGeometryManager->GetG3SensVolVector();
-
-  G4int nofSV = pSVVector.entries();
-  if (nofSV>0)
-    for (G4int isv=0; isv<nofSV; isv++) {
-      G4LogicalVolume* lv = pSVVector[isv];
-      AliModule* module = fSDManager->FindAliModule(lv);    
-      fSDManager->CreateSD(lv, module);
-    } 
-}
-
 // public methods
 
+//_____________________________________________________________________________
 void AliMoreModulesConstruction::AddModule(G4String moduleName, G4int version,
                                            AliModuleType moduleType)
 {					   
@@ -134,6 +78,7 @@ void AliMoreModulesConstruction::AddModule(G4String moduleName, G4int version,
   fModuleConstructionVector.push_back(moduleConstruction);
 }  
   					   
+//_____________________________________________________________________________
 void AliMoreModulesConstruction::Configure(const AliFiles& files)
 { 
 // Executes the detectors setup Root macros
@@ -153,6 +98,7 @@ void AliMoreModulesConstruction::Configure(const AliFiles& files)
       fModuleConstructionVector[i]->Configure(files);
 }      
 
+//_____________________________________________________________________________
 void AliMoreModulesConstruction::Construct()
 { 
 // Constructs geometry.
@@ -170,7 +116,6 @@ void AliMoreModulesConstruction::Construct()
     // get geometry manager
     TG4GeometryManager* pGeometryManager = TG4GeometryManager::Instance();
 
-    G4bool allLVSensitive = false;    
     G4int i;
     for (i=0; i<nofModules; i++) {
 
@@ -214,11 +159,6 @@ void AliMoreModulesConstruction::Construct()
         if (writeGeometry) 
           pGeometryManager->CloseOutFile();
       }	
-
-      // all logical volumes will be made sensitive if any
-      // module requires this
-      if (fModuleConstructionVector[i]->GetAllLVSensitive()) 
-         allLVSensitive = true;
     }  
   
     // construct G4 geometry
@@ -226,28 +166,18 @@ void AliMoreModulesConstruction::Construct()
 
     // print name map
     // pGeometryManager->PrintNameMap();
-    
-    // create sensitive detectors
-    CreateSensitiveDetectors(allLVSensitive);
-  
+
     for (i=0; i<nofModules; i++) {
       // set the detector frame (envelope)
       // (without warning output if enevelope is not defined)
       fModuleConstructionVector[i]->SetDetFrame(false);
 
-      // build sensitive detectors table
-      fModuleConstructionVector[i]->GetAliModule()->Init();
-
       // construct geometry for display
       fModuleConstructionVector[i]->GetAliModule()->BuildGeometry();
-    }  
+    }
 
     // reset TG4GeometryManager 
     pGeometryManager->ClearG3Tables();
-  
-    // print current total number of logical volumes
-    G4cout << "Current total number of sensitive volumes: "
-           << pGeometryManager->NofVolumes() << G4endl;
 
 #ifdef ALICE_VISUALIZE
     // set visualization attributes
