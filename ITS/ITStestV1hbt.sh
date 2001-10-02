964439fd74bbebf0b1d4864c9f03e6d2e794b0c9
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-10-02 06:52:07 +0000

    Test suite to run tracking V1 for HBT analysis.

---
+++ ITS/ITStestV1hbt.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# delete eventual old files from the last run
+./DeleteOldFiles
+# run the hit generation
+aliroot -q -b "$ALICE_ROOT/macros/grunHBT.C($1)"
+# digitize TPC
+aliroot -q -b "$ALICE_ROOT/TPC/AliTPCHits2Digits.C($1)"
+# prepare TPC tracks for matching with the ITS
+aliroot -q -b "MyAliTPCComparison.C($1)"
+# digitize ITS
+aliroot -q -b "$ALICE_ROOT/ITS/ITSHitsToDigits.C(0,$[$1-1])"
+# create reconstruct point for the ITS
+aliroot -q -b "$ALICE_ROOT/ITS/ITSDigitsToClusters.C(0,$[$1-1])"
+# prepare for tracking
+aliroot -q -b "ITStracks.C(0,$[$1-1])"
+# do the tracking
+aliroot -q -b "ITStracking.C(0,$[$1-1])"
+# do the comparison
+aliroot -q -b "ITSComparisonV1.C(0,$[$1-1])"
+#
+# after all of the above you can run ITSPlotTracks.C macro under aliroot to
+# see plots of the efficiency and track parameter resolution
