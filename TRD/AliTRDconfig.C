28c11bc1721b126f253c5882a2eecf9d0e00322a
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-10-06 02:41:29 +0000

    Minor correction

--- TRD/AliTRDconfig.C
+++ TRD/AliTRDconfig.C
@@ -14,7 +14,7 @@ void Config()
 
   // Set external decayer
   AliDecayer* decayer = new AliDecayerPythia();
-  decayer->SetForceDecay(all);
+  decayer->SetForceDecay(kAll);
   //decayer->SetForceDecay(kAll);
   decayer->Init();
   gMC->SetExternalDecayer(decayer);
