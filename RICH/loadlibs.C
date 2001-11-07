447c6351a0a562aeb98171ef6ce2770eb728c020
dibari <dibari@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-07 20:22:03 +0000

    Initial import

---
+++ RICH/loadlibs.C
@@ -0,0 +1,36 @@
+void loadlibs () 
+{
+  if (gClassTable->GetID("AliRun") >= 0) return;
+   cout<<"RICH private loadlibs.C ...";
+  gSystem->Load("libminicern");
+  gSystem->Load("libPhysics");
+  gSystem->Load("libEG");
+  gSystem->Load("libSTEER");
+  gSystem->Load("libTGeant3Dummy");
+  gSystem->Load("libdummyhijing");
+  gSystem->Load("libTHijing");
+  gSystem->Load("libdummypythia6");
+  gSystem->Load("libEGPythia6");
+  gSystem->Load("libCONTAINERS");
+  gSystem->Load("libdummyHBTP");
+  gSystem->Load("libTHbtp");
+  gSystem->Load("libdummymevsim");
+  gSystem->Load("libTMevSim");
+  gSystem->Load("libEVGEN");
+  gSystem->Load("libHBTP"); 
+  gSystem->Load("libRALICE");
+  gSystem->Load("libFMD");
+  gSystem->Load("libMUON");
+  gSystem->Load("libPHOS");
+  gSystem->Load("libPMD");
+  gSystem->Load("libRICH");
+  gSystem->Load("libSTRUCT");
+  gSystem->Load("libTOF");
+  gSystem->Load("libTPC");
+  gSystem->Load("libTRD");
+  gSystem->Load("libZDC");
+  gSystem->Load("libITS");
+  gSystem->Load("libCASTOR");
+  gSystem->Load("libSTART");
+  cout<<"done\n";
+}
