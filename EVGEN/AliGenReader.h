3d7fc6392944e8c6b5d8429282d0e672f4bbbf8a
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-09 09:09:59 +0000

    Base class with responsibility to read events and particles from a file. To be used with AliGenExtFile.

---
+++ EVGEN/AliGenReader.h
@@ -0,0 +1,42 @@
+#ifndef ALIGENREADER_H
+#define ALIGENREADER_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+#include "TObject.h"
+
+class TParticle;
+
+class AliGenReader : public TObject
+{
+ public:
+    AliGenReader(){;}
+    AliGenReader(const AliGenReader &reader){;}
+    
+    virtual ~AliGenReader(){;}
+    // Initialise 
+    virtual void Init() {}
+    // set file name of data file
+    virtual void SetFileName(const Text_t *filname) {fFileName=filname;}
+    // Read
+    virtual Int_t NextEvent(){return 0;}
+    enum Code_t {kPDG, kGEANT3};
+    void SetParticleCode(Code_t code) {fCode = code;}
+    virtual TParticle* NextParticle(){return NULL;}
+    
+    AliGenReader & operator=(const AliGenReader & rhs);
+ protected:
+    const Text_t *fFileName;      // Name of file to read from
+    Code_t        fCode;          // Particle code type
+    
+    ClassDef(AliGenReader,1) //Generate particles from external file
+};
+#endif
+
+
+
+
+
+
