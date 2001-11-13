9d9fd0ad01d52be1ea54e4467e47c880e3713c17
dibari <dibari@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-13 15:01:36 +0000

    rules checked

--- RICH/AliRICHSegmentationV1.h
+++ RICH/AliRICHSegmentationV1.h
@@ -10,51 +10,44 @@
 #include <iostream.h>
 
 class AliRICHSegmentationV1 : public AliRICHSegmentationV0 
-{
-    
+{    
 public:
-            AliRICHSegmentationV1();
-   virtual ~AliRICHSegmentationV1() {}
-   inline void Print();
+// ctor $ dtor:      
+            AliRICHSegmentationV1();      // default ctor
+   virtual ~AliRICHSegmentationV1() {}    // dtor
+// The following staff is defined in AliRICHSegmentation.cxx:
+   virtual void   Init(Int_t id); // Recalculates all the values after some of them have been changed
 
-    // Initialisation
-    virtual void Init(Int_t id);
+   virtual Int_t  Sector(Float_t x, Float_t y);    // calculate sector from x-y coordinates
 
-    // current sector
-    virtual Int_t  ISector() {return fSector;}
-
-    // calculate sector from x-y coordinates
-    virtual Int_t  Sector(Float_t x, Float_t y);
-
-    // Transform from pad to real coordinates
-    virtual void    GetPadI(Float_t x ,Float_t y ,Int_t   &ix,Int_t   &iy);
-    virtual void    GetPadI(Float_t x, Float_t y , Float_t z, Int_t &ix, Int_t &iy)  
-      {GetPadI(x, y, ix, iy);}
+   virtual void    GetPadI(Float_t x ,Float_t y ,Int_t   &ix,Int_t   &iy);         // Transform from pad to real coordinates
+   virtual void    GetPadI(Float_t x, Float_t y , Float_t z, Int_t &ix, Int_t &iy)  {GetPadI(x, y, ix, iy);}
     
-    // Transform from real to pad coordinates
-    virtual void    GetPadC(Int_t   ix,Int_t   iy,Float_t &x ,Float_t &y );
-    virtual void    GetPadC(Int_t ix, Int_t iy, Float_t &x, Float_t &y, Float_t &z) 
-      {z=0; GetPadC(ix, iy, x , y);}
+   virtual void    GetPadC(Int_t   ix,Int_t   iy,Float_t &x ,Float_t &y );    // Transform from real to pad coordinates
+   virtual void    GetPadC(Int_t ix, Int_t iy, Float_t &x, Float_t &y, Float_t &z) {z=0; GetPadC(ix, iy, x , y);}
     
-    // Current integration limits
-    virtual void IntegrationLimits (Float_t& x1, Float_t& x2, Float_t& y1, Float_t& y2);
+   virtual void IntegrationLimits (Float_t& x1, Float_t& x2, Float_t& y1, Float_t& y2);    // Current integration limits
+// inline methods:
+   virtual Int_t  ISector() const{return fSector;}   // Get current sector
+   
+   inline virtual void Print(Option_t *option)const; // Prints debug information
     
- private:
+private:
     ClassDef(AliRICHSegmentationV1,1)
 };
 
-inline void AliRICHSegmentationV1::Print()
+inline void AliRICHSegmentationV1::Print(Option_t *option)const
 {
    TObject::Print();
-   cout<<"Pad width in cm:         "<<fDpx<<endl;
-   cout<<"Pad heights in cm:       "<<fDpy<<endl;
-   cout<<"Pad number along x:      "<<fNpx<<endl;
-   cout<<"Pad number along y:      "<<fNpy<<endl;
-   cout<<"Sector:                  "<<fSector<<endl;
-   cout<<"Wire pitch:              "<<fWireD<<endl; 
-   cout<<"Dead zone in cm:         "<<fDeadZone<<endl;
-   cout<<"Pad plane width in cm:   "<<fPadPlane_Width<<endl;
-   cout<<"Pad plane heights in cm: "<<fPadPlane_Length<<endl;
-}//void AliRICHSegmentationV1::Print()
+   cout<<"Pad width in cm:         "<<fDpx                 <<endl;
+   cout<<"Pad heights in cm:       "<<fDpy                 <<endl;
+   cout<<"Pad number along x:      "<<fNpx                 <<endl;
+   cout<<"Pad number along y:      "<<fNpy                 <<endl;
+   cout<<"Sector:                  "<<fSector              <<endl;
+   cout<<"Wire pitch:              "<<fWireD               <<endl; 
+   cout<<"Dead zone in cm:         "<<fDeadZone            <<endl;
+   cout<<"Pad plane width in cm:   "<<fPadPlane_Width      <<endl;
+   cout<<"Pad plane heights in cm: "<<fPadPlane_Length     <<endl;
+}//void AliRICHSegmentationV1::Print(Option_t *option)const
 	
 #endif//AliRICHSegmentationV1_h
