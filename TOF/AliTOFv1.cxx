fd6f539190014ee47ec29d1bb237aba2411c50cf
vicinanz <vicinanz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-13 14:36:40 +0000

    Updated check for ppad[1] range

--- TOF/AliTOFv1.cxx
+++ TOF/AliTOFv1.cxx
@@ -1047,6 +1047,7 @@ void AliTOFv1::StepManager()
     gMC->Gmtod(xm,xpad,1);
     gMC->Gmtod(pm,ppad,2);
     if(ppad[1]>1.) ppad[1]=1.;
+    if(ppad[1]<-1.) ppad[1]=-1.;
     incidenceAngle = TMath::ACos(ppad[1])*kRaddeg;
 
     z = pos[2];
