3cba1a19bdaa970eeef581263d5beeec1ec1325d
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-13 16:45:07 +0000

    switch statement corrected

--- AliGeant4/macro/ZDC/Config.C
+++ AliGeant4/macro/ZDC/Config.C
@@ -5,7 +5,7 @@ void Config(Int_t version)
   AliZDC* ZDC = 0;
   switch (version) {
     case 1: ZDC  = new AliZDCv1("ZDC","ZDCv1 detector"); break;
-    case 1: ZDC  = new AliZDCv2("ZDC","normal ZDC"); break;
+    case 2: ZDC  = new AliZDCv2("ZDC","normal ZDC"); break;
   }  
 
 //=================== ZDC parameters ============================
