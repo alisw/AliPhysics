2cc2eccd7ccaba3f506ca292f95c64e28d562976
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-16 08:02:35 +0000

    added AliSHILvF version

--- AliGeant4/macro/STRUCT/ConfigSHIL.C
+++ AliGeant4/macro/STRUCT/ConfigSHIL.C
@@ -3,6 +3,7 @@ void Config(Int_t version)
   AliSHIL* SHIL = 0;
   switch (version) {
     case 0: SHIL  = new AliSHILv0("SHIL","Shielding"); break;
+    case 1: SHIL  = new AliSHILvF("SHIL","Shielding"); break;
   }  
 
 //=================== SHIL parameters ============================
