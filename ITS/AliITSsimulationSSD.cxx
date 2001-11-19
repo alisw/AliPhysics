8a10604ddefcd147c742426d9585956ac62a36ab
barbera <barbera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-19 10:06:25 +0000

    fMapA2->Clear() and FillMapFrompList(pList) removed in digitization routine to avoid double counting of the charge

--- ITS/AliITSsimulationSSD.cxx
+++ ITS/AliITSsimulationSSD.cxx
@@ -134,9 +134,9 @@ void AliITSsimulationSSD::SDigitiseModule(AliITSmodule *mod,Int_t dummy0,
 //______________________________________________________________________
 void AliITSsimulationSSD::SDigitToDigit(Int_t module,AliITSpList *pList){
     // Takes the pList and finishes the digitization.
-
-    fMapA2->Clear();          // added to avoid double counting of the charge                                
-    FillMapFrompList(pList);  
+                               
+//    FillMapFrompList(pList);  //commented out to avoid double counting of the
+                                //charge
 
     ApplyNoise(pList,module);
     ApplyCoupling(pList,module);
