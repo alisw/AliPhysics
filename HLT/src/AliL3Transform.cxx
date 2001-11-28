043fb91f1f29ba43a07841dffd51013393ffe836
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-11-28 19:24:07 +0000

    Bugfix in raw2local

--- HLT/src/AliL3Transform.cxx
+++ HLT/src/AliL3Transform.cxx
@@ -511,12 +511,18 @@ void AliL3Transform::Raw2Local(Float_t *xyz,Int_t sector,Int_t row,Float_t pad,F
     xyz[1]=(pad-0.5*(npads-1))*fPadPitchWidthUp;
   xyz[2]=fZWidth*time-3.*fZSigma;
   Int_t sign=-1;
-  Int_t nis=fNSectorLow;
-  Int_t nos=fNSectorUp;
-  
-  if((sector<nis)/2 || ((sector-nis)<nos/2)) sign=1;
+    
+  if(slice < 18)
+    sign = 1;
+  else
+    sign = -1;
   xyz[2]=sign*(250.-xyz[2]);
-
+  
+  
+  //Int_t nis=fNSectorLow;
+  //Int_t nos=fNSectorUp;
+  //if((sector<nis)/2 || ((sector-nis)<nos/2)) sign=1;
+  
 }
 
 void AliL3Transform::Local2Global(Float_t *xyz,Int_t sector,Int_t row)
