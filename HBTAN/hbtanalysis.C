f7a87a146288a3e15e63ecc935058638aa56e429
skowron <skowron@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-12-03 16:03:51 +0000

    Data read option implemented

--- HBTAN/hbtanalysis.C
+++ HBTAN/hbtanalysis.C
@@ -1,7 +1,21 @@
-void hbtanalysis(Int_t first = -1,Int_t last = -1, char *outfile = "hbtanalysis.root")
+void hbtanalysis(Option_t* datatype, Option_t* processopt="TracksAndParticles", 
+                Int_t first = -1,Int_t last = -1, 
+                char *outfile = "hbtanalysis.root")
  {
 //HBT Anlysis Macro
 //Anlyzes TPC recontructed tracks and simulated particles that corresponds to them
+
+//datatype defines type of data to be read
+//  Kine  - analyzes Kine Tree: simulated particles
+//  TPC   - analyzes TPC   tracking + particles corresponding to these tracks
+//  ITSv1 - analyzes ITSv1 ----------------------//--------------------------
+//  ITSv2 - analyzes ITSv2 ----------------------//--------------------------
+
+//processopt defines option passed to AliHBTAnlysis::Process method
+// default: TracksAndParticles - process both recontructed tracks and sim. particles corresponding to them 
+//          Tracks - process only recontructed tracks
+//          Particles - process only simulated particles
+
 //Reads data from diroctories from first to last(including)
 // For examples if first=3 and last=5 it reads from
 //  ./3/
@@ -29,29 +43,53 @@ void hbtanalysis(Int_t first = -1,Int_t last = -1, char *outfile = "hbtanalysis.
   AliHBTAnalysis * analysis = new AliHBTAnalysis();
   
   AliHBTReader* reader;
+  Int_t kine = strcmp(datatype,"Kine");
+  Int_t TPC = strcmp(datatype,"TPC");
+  Int_t ITSv1 = strcmp(datatype,"ITSv1");
+  Int_t ITSv2 = strcmp(datatype,"ITSv2");
+
+  if(!kine)
+   {
+    reader = new AliHBTReaderKineTree();
+    processopt="Particles"; //this reader by definition reads only simulated particles
+   }
+  else if(!TPC)
+   {
+    reader = new AliHBTReaderTPC();
+   }
+  else if(!ITSv1)
+   {
+    reader = new AliHBTReaderITSv1();
+   }
+  else if(!ITSv2)
+   {
+    reader = new AliHBTReaderITSv2();
+   }
+  else
+   {
+    cerr<<"Option "<<datatype<<"  not recognized. Exiting"<<endl;
+    return;
+   }
+
   TObjArray* dirs=0;
-  if ( (first < 0) || (last<0) || ( (last-first)<0 ) )
-    {//read from current directory
-     reader = new AliHBTReaderTPC();
+  if ( (first >= 0) && (last>=0) && ( (last-first)>=0 ) )
+   {//read from many dirs dirs
+     char buff[50];
+     dirs = new TObjArray(last-first+1);
+     for (Int_t i = first; i<=last; i++)
+      {
+        sprintf(buff,"%s/%s/%s/%d",basedir,field,serie,i);
+        TObjString *odir= new TObjString(buff);
+        dirs->Add(odir);
+      }
     }
-   else
-    {//read from many dirs dirs
-      char buff[50];
-      dirs = new TObjArray(last-first+1);
-      for (Int_t i = first; i<=last; i++)
-       {
-         sprintf(buff,"%s/%s/%s/%d",basedir,field,serie,i);
-         TObjString *odir= new TObjString(buff);
-         dirs->Add(odir);
-       }
-      reader = new AliHBTReaderTPC(dirs);
-    }
+   reader->SetDirs(dirs);
    
   /***********************************************************/    
   
   //we want have only low pt pi+ so set a cut to reader
   AliHBTParticleCut* readerpartcut= new AliHBTParticleCut();
-  readerpartcut->SetPtRange(0.0,3.0);
+//  readerpartcut->SetPtRange(0.0,1.5);
   readerpartcut->SetPID(kPiPlus);
   reader->AddParticleCut(readerpartcut);//read this particle type with this cut
   
@@ -59,16 +97,16 @@ void hbtanalysis(Int_t first = -1,Int_t last = -1, char *outfile = "hbtanalysis.
   /************************************************************/
   
   AliHBTPairCut *paircut = new AliHBTPairCut();
-  paircut->SetQInvRange(0.0,0.15);  
+  paircut->SetQInvRange(0.0,0.20);  
   analysis->SetGlobalPairCut(paircut);
 
-  AliHBTQInvCorrelFctn * qinvcfT= new AliHBTQInvCorrelFctn();
-  AliHBTQInvCorrelFctn * qinvcfP= new AliHBTQInvCorrelFctn();
+  AliHBTQInvCorrelFctn * qinvcfT= new AliHBTQInvCorrelFctn(500,3.);
+  AliHBTQInvCorrelFctn * qinvcfP= new AliHBTQInvCorrelFctn(500,3.);
   
   analysis->AddTrackFunction(qinvcfT);
   analysis->AddParticleFunction(qinvcfP);
   
-  analysis->Process();
+  analysis->Process(processopt);
   
   qinvcfP->Rename("qinvcfP","Particle (simulated) Qinv CF \\pi^{+} \\pi^{+}");
   qinvcfT->Rename("qinvcfT","Track (recontructed) Qinv CF \\pi^{+} \\pi^{+}");
