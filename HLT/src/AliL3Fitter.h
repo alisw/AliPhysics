9d8b2b0de7a4959ef2271f9dd6d780a5bb51510e
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-12-10 14:04:40 +0000

    Refitting tracks after tracking

---
+++ HLT/src/AliL3Fitter.h
@@ -0,0 +1,33 @@
+#ifndef ALIL3_Fitter
+#define ALIL3_Fitter
+
+#include "AliL3RootTypes.h"
+
+class AliL3Track;
+class AliL3Vertex;
+class AliL3SpacePointData;
+
+class AliL3Fitter {
+
+ private:
+  AliL3Track *fTrack; //!
+  AliL3Vertex *fVertex; //!
+  Double_t BFACT;
+  Bool_t fVertexConstraint;
+  AliL3SpacePointData *fClusters[36][6]; //!
+  UInt_t fNcl[36][6];
+
+ public:
+  AliL3Fitter(AliL3Vertex *vertex);
+  virtual ~AliL3Fitter() {};
+  
+  void LoadClusters(Char_t *path);
+  Int_t FitHelix(AliL3Track *track);
+  Int_t FitCircle();
+  Int_t FitLine();
+  void NoVertex() {fVertexConstraint=kFALSE;}
+  
+  ClassDef(AliL3Fitter,1) //HLT fit class
+};
+
+#endif
