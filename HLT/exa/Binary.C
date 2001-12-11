2d8b13575c5b04ec9ca04e1c88c4ec21a4b9c16d
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-12-11 10:17:11 +0000

    possible to only do 1 slice

--- HLT/exa/Binary.C
+++ HLT/exa/Binary.C
@@ -2,12 +2,12 @@ Binary(char* in,int first, int last,char *path=""){
   char name[256];
   AliL3FileHandler *fFileHandler = new AliL3FileHandler(); 
   fFileHandler->SetAliInput(in);
-//  Int_t row[5][2] = {{ 0, 45},{46,77},{78,109},{110,141},{142,173}};
   const Int_t npatch = 6;
   Int_t row[npatch][2] = {{0,31},{32,63},{64,91},{92,119},{120,143},{144,175}};
   for(int slice=first; slice<=last; slice++){
     for(int patch=0;patch<npatch;patch++){
       cerr<<"reading slice: "<<slice<<" patch: "<<patch;
+      fFileHandler->Free();
       fFileHandler->Init(slice,patch,row[patch]);      
       sprintf(name,"%sdigits_%d_%d.raw",path,slice,patch);
       fFileHandler->SetBinaryOutput(name);
@@ -18,3 +18,36 @@ Binary(char* in,int first, int last,char *path=""){
   }
   fFileHandler->CloseAliInput();
 }
+ 
+void singlepatch(char* in,int first, int last,char *path="",int event=0)
+{
+  AliL3Logger l;
+  //  l.UnSet(AliL3Logger::kDebug);
+  //  l.UnSet(AliL3Logger::kAll);
+  //  l.Set(AliL3Logger::kInformational);
+  //l.UseStdout();
+  l.UseStream();
+  
+  char fname[100];
+  sprintf(fname,"%sevent_%d/",path,event);
+  char name[256];
+  AliL3FileHandler *fFileHandler = new AliL3FileHandler(); 
+  fFileHandler->SetAliInput(in);
+  Int_t srow[2] = {0,175};
+  int patch=0;
+  for(int slice=first; slice<=last; slice++)
+    {
+      cerr<<"reading slice: "<<slice;
+      fFileHandler->Free();
+      fFileHandler->Init(slice,patch,srow);
+      sprintf(name,"%sdigits_%d_%d.raw",fname,slice,patch);
+      fFileHandler->SetBinaryOutput(name);
+      fFileHandler->AliDigits2CompBinary(event);
+      fFileHandler->CloseBinaryOutput();      
+      cerr<<" done"<<endl;
+    }
+  fFileHandler->CloseAliInput();
+  
+}
+
+
