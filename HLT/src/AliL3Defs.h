d2d9fb610aee73197f5813ad0eb4135b26743136
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-01-07 16:47:53 +0000

    Changed to normal bfield

--- HLT/src/AliL3Defs.h
+++ HLT/src/AliL3Defs.h
@@ -6,6 +6,6 @@
 //const Int_t NRows[5][2] = {{ 0, 45},{46,77},{78,109},{110,141},{142,175}};
 const Double_t Pi = 3.14159265358979323846;
 const Double_t ToRad = Pi/180.;
-const Double_t BField = 0.4;
+const Double_t BField = 0.2;
 
 #endif /* _ALIL3DEFS_H_ */
