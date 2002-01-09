7f65824d01dadfef44ea73ddbb9898501f466e55
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-01-09 12:13:37 +0000

    Added maikes classes

---
+++ HLT/sim/AliL3Generator.h
@@ -0,0 +1,23 @@
+#ifndef ALIL3GENERATOR_H
+#define ALIL3GENERATOR_H
+
+#include "AliL3RootTypes.h"
+#include "AliGenerator.h"
+
+class AliL3Generator : public AliGenerator {
+
+ private:
+  
+  
+  
+
+ public:
+  AliL3Generator();
+  virtual ~AliL3Generator();
+  
+  void Generate();
+  void Init();
+  void ReadParticles(TClonesArray *particles);
+
+  ClassDef(AliL3Generator,1)
+};
