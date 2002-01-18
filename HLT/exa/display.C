0f3f4f4e638ccf23b5c7b047aed928998e0b83d2
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-01-18 10:23:33 +0000

    Minor changes

--- HLT/exa/display.C
+++ HLT/exa/display.C
@@ -1,22 +1,24 @@
-void display(char *file="")
+void display(int minslice,int maxslice,char *file="")
 {
   AliL3Logger l;
 //  l.UnSet(AliL3Logger::kDebug);
 //  l.UnSet(AliL3Logger::kAll);
 //  l.Set(AliL3Logger::kInformational);
-   l.UseStdout();
+  //l.UseStdout();
    l.UseStream();
    
   gStyle->SetOptStat(0);
 
-  int slice[2] = {0,35};
+  int slice[2] = {minslice,maxslice};
 
   a = new AliL3Display(slice);
   a->Setup("tracks.raw","./");
-  a->DisplayAll();
-  //a->DisplayTracks();
+  //a->Setup("/prog/alice/data/Rawdata/1_patch/pp/recon_6/tracks.raw","/prog/alice/data/Rawdata/1_patch/pp/recon_6/");
+  //a->DisplayAll();
+  a->DisplayTracks();
   //a->DisplayClusters();
   //a->DisplayClusterRow(1,151,file,"colz");
 
 }
+
 
--- HLT/exa/eval.C
+++ HLT/exa/eval.C
@@ -1,23 +1,75 @@
-Int_t eval(){
-
-  AliL3Logger l;
-  l.UseStdout();
-  l.UseStream();
-
-  Char_t *mcfile = "/heim/franken/data/V3.04/fast/hg_8k_v0_s1-3_e0.root";
-//  Char_t *mcfile = "/heim/franken/data/V3.04/slow/hg_8k_v0_s1-3_e0.root";
-  Char_t *mcClusterfile= "hg_8k_v0_s1-3_e0_cl.root";
-  Char_t *trackfile ="tracks.raw";
+void eval(char *inFile)
+{
 
 
+  // Connect the Root Galice file containing Geometry, Kine and Hits
 
-  Int_t slice[2]={1,3};
-  AliL3Evaluate *eval = new AliL3Evaluate(mcfile,slice);
-  eval->SetupFast(trackfile,mcClusterfile);
-//  eval->SetupSlow(trackfile);
-//  eval->EvaluateSlice(1,10,10);
-  eval->EvaluateGlobal(1,1);
-  eval->Write2File("eval.root");
+  file = new TFile(inFile);
   
-  return 0;
+  gAlice = (AliRun*)file->Get("gAlice");
+  if (gAlice) printf("AliRun object found on file\n");
+  if (!gAlice) gAlice = new AliRun("gAlice","Alice test program");
+  
+  gAlice->GetEvent(0);
+  AliTPC *TPC = (AliTPC*)gAlice->GetDetector("TPC");      
+  
+  AliTPCParam *param = (AliTPCParam*)file->Get("75x40_100x60");
+  
+  TFile *nfile = new TFile("/prog/alice/data/exact_clusters.root","RECREATE");
+  nfile->cd();
+
+  //setup AliTPCClustersArray
+  AliTPCClustersArray * arr=new AliTPCClustersArray;
+  arr->SetClusterType("AliComplexCluster");
+  arr->Setup(param);
+  TPC->SetParam(param);
+  arr->MakeTree();
+
+  TPC->SetClustersArray(arr); 
+  TPC->Hits2ExactClustersSector(1);
+  TPC->Hits2ExactClustersSector(37);
+    
+
+  //write results
+  char treeName[100];
+  sprintf(treeName,"TreeCExact_%s",param->GetTitle());
+  TPC->GetClustersArray()->GetTree()->Write(treeName);
+  param->Write(param->GetTitle());
+  file->Close();
+  nfile->Close();
+  
+  return;
+}
+
+void calc(char *rootfile,char *digitsfile,char *cfile)
+{
+
+  int slice[2] = {1,1};
+  a = new AliL3Evaluate(rootfile,digitsfile,slice);
+  a->SetupSlow("tracks.raw","./");
+  ntuppel = (TNtuple*)a->EvaluatePoints(cfile);
+
+  f = new TFile("results.root","RECREATE");
+  f->cd();
+  ntuppel->Write();
+  f->Close();
+
+}
+
+void plot(char *file)
+{
+  gStyle->SetOptFit(0110);
+  f = new TFile(file);
+  
+  TCanvas *c1 = new TCanvas("c1","",2);
+  TH1F *hist = new TH1F("hist","",100,-2,2);
+  
+  ntuppel->Draw("resy>>hist","pt>1");
+
+  float rms = hist->GetRMS();
+  hist->SetXTitle("#delta_{T} [cm]");
+
+  TF1 *f1 = new TF1("f1","gaus",-rms,rms);
+  hist->Fit("f1","R");
+
 }
