77b4e6db97d608b411e8313046f5d7a7b7553c97
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-01-22 10:30:07 +0000

    Addition methods and member data for bg mixing.

--- EMCAL/AliEMCALJetFinder.h
+++ EMCAL/AliEMCALJetFinder.h
@@ -21,13 +21,13 @@ class AliEMCALJetFinder : public TTask {
     AliEMCALJetFinder();
     AliEMCALJetFinder(const char* name, const char *title);
     virtual ~AliEMCALJetFinder();
-    virtual void Find(Int_t ncell, Int_t ncell_tot, Float_t etc[30000], 
+    virtual void  Find(Int_t ncell, Int_t ncell_tot, Float_t etc[30000], 
 		      Float_t etac[30000], Float_t phic[30000],
 		      Float_t min_move, Float_t max_move, Int_t mode,
 		      Float_t prec_bg, Int_t ierror);
-    virtual void Find();
-    virtual void FindTracksInJetCone();
-    virtual void Test();
+    virtual void  Find();
+    virtual void  FindTracksInJetCone();
+    virtual void  Test();
     virtual void  BuildTrackFlagTable();
     virtual Int_t SetTrackFlag(Float_t radius, Int_t pdgCode, Double_t charge);
     // Geometry
@@ -47,7 +47,7 @@ class AliEMCALJetFinder : public TTask {
     virtual void SetHadronCorrector(AliEMCALHadronCorrection* corr)
 	{fHadronCorrector = corr;}
     virtual void SetHadronCorrection(Int_t flag = 1) {fHCorrection = flag;}
-    // Results
+    // Access to Results
     virtual Int_t   Njets();
     virtual Float_t JetEnergy(Int_t);
     virtual Float_t JetPhiL(Int_t);
@@ -60,6 +60,8 @@ class AliEMCALJetFinder : public TTask {
     virtual void FillFromHitFlaggedTracks(Int_t flag = 0);
     virtual void FillFromDigits(Int_t flag = 0);
     virtual void FillFromTracks(Int_t flag = 0, Int_t ich = 0);
+    virtual void SaveBackgroundEvent();
+    virtual void InitFromBackground();
     virtual void AddJet(const AliEMCALJet& jet);
     virtual void WriteJets();
     virtual void ResetJets();
@@ -72,16 +74,18 @@ class AliEMCALJetFinder : public TTask {
  protected:
     TClonesArray*                  fJets;            //! List of Jets
     TH2F*                          fLego;            //! Lego Histo
+    TH2F*                          fLegoB;           //! Lego Histo Backg    
     AliEMCALJet*                   fJetT[10];        //! Jet temporary storage
     AliEMCALHadronCorrection*      fHadronCorrector; //! Pointer to hadronic correction
     Int_t                          fHCorrection;     //  Hadron correction flag
     Int_t                          fDebug;           //! Debug flag
+    Int_t                          fBackground;      //! Background flag
     Float_t                        fConeRadius;      //  Cone radius
     Float_t                        fPtCut;           //  Pt cut on charged tracks
     Float_t                        fEtSeed;          //  Min. Et for seed
     Float_t                        fMinJetEt;        //  Min Et of jet
     Float_t                        fMinCellEt;       //  Min Et in one cell
-    Float_t                        fSamplingF;
+    Float_t                        fSamplingF;       //  Sampling Fraction
     Bool_t                         fSmear;           //  Flag for momentum smearing
     Bool_t                         fEffic;           //  Flag for efficiency simulation
     Bool_t                         fK0N;             //  Flag for efficiency simulation
@@ -96,13 +100,24 @@ class AliEMCALJetFinder : public TTask {
     Float_t                        fEtaCell[30000];  //! Cell eta
     Float_t                        fPhiCell[30000];  //! Cell phi
     Int_t*                         fTrackList;       //! List of selected tracks
+    Int_t*                         fTrackListB;      //! List of selected tracks in Bg
     Int_t                          fNt;              //! number of tracks
-    Float_t*                       fPtT;             //! Pt   of tracks in jet cone
-    Float_t*                       fEtaT;            //! Eta  of tracks in jet cone
-    Float_t*                       fPhiT;            //! Phi  of tracks in jet cone
+    Int_t                          fNtS;             //! number of tracks selected
+    Float_t*                       fPtT;             //! Pt   of tracks 
+    Float_t*                       fEtaT;            //! Eta  of tracks
+    Float_t*                       fPhiT;            //! Phi  of tracks
+    Int_t                          fNtB;             //! number of tracks in Bg
+    Float_t*                       fPtB;             //! Pt   of tracks in Bg
+    Float_t*                       fEtaB;            //! Eta  of tracks in Bg
+    Float_t*                       fPhiB;            //! Phi  of tracks in Bg
+
     ClassDef(AliEMCALJetFinder,2)        // JetFinder for EMCAL
 }
 ;
 #endif // ALIEMCALJetFinder_H
+
+
+
+
 
 
