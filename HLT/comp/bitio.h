b53a5f7c3f9b00059954a4e73ef2f9351e11944f
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-01-31 17:46:09 +0000

    Functions for bitio. Taken as is from The Data Compression Book

---
+++ HLT/comp/bitio.h
@@ -0,0 +1,45 @@
+/************************** Start of BITIO.H *************************/
+
+#ifndef _BITIO_H
+#define _BITIO_H
+
+#include <stdio.h>
+
+typedef struct bit_file {
+    FILE *file;
+    unsigned char mask;
+    int rack;
+    int pacifier_counter;
+} BIT_FILE;
+
+//#ifdef __STDC__
+
+BIT_FILE     *OpenInputBitFile( char *name );
+BIT_FILE     *OpenOutputBitFile( char *name );
+void          OutputBit( BIT_FILE *bit_file, int bit );
+void          OutputBits( BIT_FILE *bit_file,
+                          unsigned long code, int count );
+int           InputBit( BIT_FILE *bit_file );
+unsigned long InputBits( BIT_FILE *bit_file, int bit_count );
+void          CloseInputBitFile( BIT_FILE *bit_file );
+void          CloseOutputBitFile( BIT_FILE *bit_file );
+void          FilePrintBinary( FILE *file, unsigned int code, int bits );
+/*
+#else   
+
+BIT_FILE     *OpenInputBitFile();
+BIT_FILE     *OpenOutputBitFile();
+void          OutputBit();
+void          OutputBits();
+int           InputBit();
+unsigned long InputBits();
+void          CloseInputBitFile();
+void          CloseOutputBitFile();
+void          FilePrintBinary();
+
+#endif  
+*/
+#endif  /* _BITIO_H */
+
+/*************************** End of BITIO.H **************************/
+
---
+++ HLT/comp/errhand.h
@@ -0,0 +1,18 @@
+/************************* Start of ERRHAND.H ************************/
+
+#ifndef _ERRHAND_H
+#define _ERRHAND_H
+
+//#ifdef __STDC__
+
+void fatal_error( char *fmt, ... );
+/*
+#else
+
+void fatal_error();
+
+#endif
+*/
+#endif  /* _ERRHAND_H */
+
+/************************** End of ERRHAND.H *************************/
