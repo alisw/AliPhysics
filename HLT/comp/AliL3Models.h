a8c9da95eef160e2eb254a325d8366c9be4bca96
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-02-01 15:52:07 +0000

    Data structures for track and clusters

---
+++ HLT/comp/AliL3Models.h
@@ -0,0 +1,31 @@
+#ifndef AliL3Models_H
+#define AliL3Models_H
+
+#include "AliL3RootTypes.h"
+
+const Int_t MaxNClusters = 32;
+
+struct AliL3ClusterModel {
+  Bool_t fPresent;
+  Float_t fDTime;
+  Float_t fDPad;
+  Float_t fDCharge;
+  Float_t fDSigmaY2;
+  Float_t fDSigmaZ2;
+};
+typedef struct AliL3ClusterModel AliL3ClusterModel;
+
+struct AliL3TrackModel {
+  Float_t fKappa;
+  Float_t fFirstPointX;
+  Float_t fFirstPointY;
+  Float_t fFirstPointZ;
+  Float_t fTgl;
+  Float_t fPsi;
+  Short_t fLength;
+  Short_t fClusterCharge;
+  Char_t fNClusters;
+};
+typedef struct AliL3TrackModel AliL3TrackModel;
+
+#endif
