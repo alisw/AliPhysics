ed6ecec51f04b9236e65eb7148afc10728dd03c8
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-02-11 16:46:47 +0000

    Reintroduce the hit flags

--- TRD/AliTRDhit.h
+++ TRD/AliTRDhit.h
@@ -29,7 +29,24 @@ class AliTRDhit : public AliHit {
           void   SetDetector(Int_t det)      { fDetector = det; }
           void   SetQ(Int_t q)               { fQ        =   q; }
 
+          Bool_t FromDrift() const           { return TestBit(kDrift);         };
+          Bool_t FromAmplification() const   { return TestBit(kAmplification); };
+          Bool_t FromTRphoton() const        { return TestBit(kTRphoton);      };
+          Bool_t FromTest() const            { return TestBit(kTest);          };
+
+          void   SetDrift()                  { SetBit(kDrift);         };
+          void   SetAmplification()          { SetBit(kAmplification); };
+          void   SetTRphoton()               { SetBit(kTRphoton);      };
+          void   SetTest()                   { SetBit(kTest);          }; 
+
  protected:
+
+  enum {
+    kDrift         = 0x00000001,    // Hit is from the drift region
+    kAmplification = 0x00000002,    // Hit is from the amplification region
+    kTRphoton      = 0x00000004,    // Hit is from a TR photon
+    kTest          = 0x00000008     // Hit is a special test hit
+  };
 
   UShort_t     fDetector;           // TRD detector number
   Short_t      fQ;                  // Charge created by a hit. TR signals are negative.
