0903da2109a00178bff3ac1c4a97a1d55b3c26be
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-02-11 14:27:11 +0000

    New pad plane design, new TRF+PRF, tail cancelation, cross talk

--- TRD/AliTRDdigitizer.h
+++ TRD/AliTRDdigitizer.h
@@ -35,7 +35,8 @@ class AliTRDdigitizer : public AliDigitizer {
   virtual Bool_t       Init();
   virtual Bool_t       InitDetector();
   virtual Bool_t       ReInit();
-  virtual Bool_t       Open(const Char_t *name, Int_t nEvent = 0);
+  virtual void         Exec(Option_t* option = 0);  
+  virtual Bool_t       Open(const Char_t *file, Int_t nEvent = 0);
   virtual Bool_t       MakeBranch(const Char_t *file = 0);
   virtual Bool_t       MakeDigits();
   virtual void         AddSDigitsManager(AliTRDdigitsManager *manager);
@@ -57,12 +58,17 @@ class AliTRDdigitizer : public AliDigitizer {
   virtual void         SetPadResponse(Int_t prfOn = 1)      { fPRFOn          = prfOn;    };
   virtual void         SetTimeResponse(Int_t trfOn = 1)     { fTRFOn          = trfOn;   
                                                               ReInit();                   };
+  virtual void         SetCrossTalk(Int_t ctOn = 1)         { fCTOn           = ctOn;   
+                                                              ReInit();                   };
+  virtual void         SetTailCancelation(Int_t tcOn = 1)   { fTCOn           = tcOn;     };
+  virtual void         SetNexponential(Int_t nexp)          { fTCnexp         = nexp;     };
   virtual void         SetDriftVelocity(Float_t v)          { fDriftVelocity  = v;       
                                                               ReInit();                   };
   virtual void         SetPadCoupling(Float_t v)            { fPadCoupling    = v;        };
   virtual void         SetTimeCoupling(Float_t v)           { fTimeCoupling   = v;        };
+  virtual void         SetTiltingAngle(Float_t v);
   virtual void         SetCompress(Int_t c = 1)             { fCompress       = c;        };
-  virtual void         SetVerbose(Int_t v = 1)              { fVerbose        = v;        };
+  virtual void         SetDebug(Int_t v = 1)                { fDebug          = v;        };
   virtual void         SetSDigits(Int_t v = 1)              { fSDigits        = v;        };
   virtual void         SetSDigitsScale(Float_t s)           { fSDigitsScale   = s;        };
   virtual void         SetEvent(Int_t v = 0)                { fEvent          = v;        };
@@ -88,6 +94,7 @@ class AliTRDdigitizer : public AliDigitizer {
           Bool_t       GetSDigits() const                   { return fSDigits;            };
           Float_t      GetSDigitsScale() const              { return fSDigitsScale;       };
           Float_t      GetTimeBinWidth() const              { return fTimeBinWidth;       };
+          Float_t      GetTiltingAngle() const;
   virtual Float_t      GetDiffusionL(Float_t vd, Float_t b);
   virtual Float_t      GetDiffusionT(Float_t vd, Float_t b);
   virtual Float_t      GetOmegaTau(Float_t vd, Float_t b);
@@ -102,6 +109,8 @@ class AliTRDdigitizer : public AliDigitizer {
   AliTRDgeometry      *fGeo;                //! TRD geometry
   
   Int_t                fEvent;              //! Event number
+
+  Int_t               *fMasks;              //! Masks for the merging
 
   Float_t              fField;              //  Magnetic field
   Float_t              fGasGain;            //  Gas gain
@@ -131,21 +140,29 @@ class AliTRDdigitizer : public AliDigitizer {
   Float_t              fTRFlo;              //  Lower boundary of the TRF
   Float_t              fTRFhi;              //  Higher boundary of the TRF
   Float_t              fTRFwid;             //  Bin width of the integrated TRF
+  Int_t                fCTOn;               //  Switch for cross talk
+  Float_t             *fCTsmp;              //! Integrated cross talk
+  Int_t                fTCOn;               //  Switch for the tail cancelation
+  Int_t                fTCnexp;             //  Number of exponential of the digital filter
   Float_t              fDriftVelocity;      //  Drift velocity (cm / mus)
   Float_t              fTimeBinWidth;       //  Time bin width in ns
   Float_t              fPadCoupling;        //  Pad coupling factor
   Float_t              fTimeCoupling;       //  Time coupling factor (image charge of moving ions)
+  Float_t              fTiltingAngle;       //  Tilting angle of the readout pads
   Bool_t               fCompress;           //  Switch to keep only compressed data in memory
-  Int_t                fVerbose;            //  Sets the verbose level
+  Int_t                fDebug;              //  Sets the debug level
   Bool_t               fSDigits;            //  Switch for the summable digits
   Float_t              fSDigitsScale;       //  Scale factor for the summable digits 
 
  private:
 
+  virtual Float_t      Col0Tilted(Float_t col0, Float_t rowOffset, Int_t plane);
+  virtual Float_t      CrossTalk(Float_t time); 
   virtual Int_t        Diffusion(Float_t driftlength, Float_t *xyz);
   virtual Int_t        ExB(Float_t driftlength, Float_t *xyz);  
-  virtual Int_t        PadResponse(Float_t signal, Float_t dist, Float_t *pad);
+  virtual Int_t        PadResponse(Float_t signal, Float_t dist, Int_t plane, Float_t *pad);
   virtual Float_t      TimeResponse(Float_t time);  
+  virtual void         DeConvExp(Double_t *source, Double_t *target, Int_t n, Int_t nexp);
   virtual Bool_t       CheckDetector(Int_t plane, Int_t chamber, Int_t sector);
   virtual void         SamplePRF();
   virtual void         SampleTRF();
