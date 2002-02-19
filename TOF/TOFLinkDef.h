8f48a8eb881cb1786581a00c08cc47bd74721d89
vicinanz <vicinanz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-02-19 15:07:38 +0000

    Updated Linkdef and libTOF.pkg

--- TOF/TOFLinkDef.h
+++ TOF/TOFLinkDef.h
@@ -9,14 +9,18 @@
 #pragma link off all functions;
  
 #pragma link C++ class  AliTOF+;
+#pragma link C++ class  AliTOFT0+;
+#pragma link C++ class  AliTOFPID+;
 #pragma link C++ class  AliTOFv0+;
 #pragma link C++ class  AliTOFv1+;
 #pragma link C++ class  AliTOFv2+;
 #pragma link C++ class  AliTOFv3+;
 #pragma link C++ class  AliTOFv4+;
+#pragma link C++ class  AliTOFv4T0+;
 #pragma link C++ class  AliTOFv2FHoles+;
 
 #pragma link C++ class  AliTOFhit+;
+#pragma link C++ class  AliTOFhitT0+;
 
 #pragma link C++ class  AliTOFdigit+;
 #pragma link C++ class  AliTOFRoc+;
