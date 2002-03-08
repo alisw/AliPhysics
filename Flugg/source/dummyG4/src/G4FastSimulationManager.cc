68e15568c0fff295dea9d6e92027192afd3a2975
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-03-08 12:16:28 +0000

    Ininial import

---
+++ Flugg/source/dummyG4/src/G4FastSimulationManager.cc
@@ -0,0 +1,15 @@
+//---------------------------------------------------------------
+//
+//  dummy G4FastSimulationManager.cc
+//
+//---------------------------------------------------------------
+
+#include "G4FastSimulationManager.hh"
+#include "G4LogicalVolume.hh"
+
+
+G4FastSimulationManager::G4FastSimulationManager(G4LogicalVolume* logVol)
+{;}
+
+G4FastSimulationManager::~G4FastSimulationManager()
+{;}
