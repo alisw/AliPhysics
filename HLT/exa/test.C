82f9ea8576a76a7c092d6ad200d43780caeb66db
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-04-18 14:52:58 +0000

    Incorporated latest changes in AliLevel3 class.

--- HLT/exa/test.C
+++ HLT/exa/test.C
@@ -1,32 +1,23 @@
-void test(int minslice,int maxslice,int nevent=1,char *clufile="")
+void test(int minslice,int maxslice,char *rootfile=0,int nevent=1)
 {
   AliL3Logger l;
-  //  l.UnSet(AliL3Logger::kDebug);
-  //  l.UnSet(AliL3Logger::kAll);
   l.Set(AliL3Logger::kAll);
-  //l.UseStdout();
+  l.UseStdout();
   l.UseStream();
-  
-  TFile * in = new TFile(clufile,"read");
-  TFile * out= new TFile("dummy.root","recreate");
-  
-  //a = new AliLevel3(in,out); 
-  
   
   for(Int_t ev=0; ev<nevent; ev++)
     {
-      a = new AliLevel3(in,out);
-      // a->UseBinaryInput("/heim/franken/binary/onepatch/");
-      //a->UseBinaryInput("/prog/alice/data/Rawdata/6_patch/20tracks_s2/");
-      //a->UseBinaryInput("/prog/alice/data/Rawdata/6_patch/hg_84210_s1-3/");
-      //a->UseBinaryInput("/prog/alice/data/Rawdata/1_patch/pp/event_0/");
-      /*
-	char fname[256];
-	sprintf(fname,"/prog/alice/data/Rawdata/1_patch/pp/event_%d/",ev);
-	a->UseBinaryInput(fname);
-	sprintf(fname,"/prog/alice/data/Rawdata/1_patch/pp/recon_%d/",ev);
-	a->WriteFiles(fname);
-      */
+      if(!rootfile)
+	a = new AliLevel3();
+      else
+	a = new AliLevel3(rootfile);
+
+      //a->Init("/usr/local/anders/data/hg_42105_s1-3/");
+      a->Init("/prog/alice/data/Rawdata/6_patch/hg_1000_s1-3/");
+      //a->Init("/prog/alice/data/Rawdata/6_patch/1track_s1/");
+      //a->Init("/prog/alice/data/Rawdata/6_patch/pp/event_0/");
+      //a->Init("/prog/alice/data/Rawdata/1_patch/pp/pileups/event_1/");
+
       //  a->DoRoi();
       //a->DoMc();
       
@@ -37,8 +28,8 @@ void test(int minslice,int maxslice,int nevent=1,char *clufile="")
       
       phi_segments = 50;//50;
       eta_segments = 100;//100;
-      trackletlength = 5;
-      tracklength = 10;
+      trackletlength = 3;
+      tracklength = 5;
       rowscopetracklet = 2;
       rowscopetrack = 2;
       min_pt_fit = 0;
@@ -46,42 +37,20 @@ void test(int minslice,int maxslice,int nevent=1,char *clufile="")
       goodDist = 5;
       maxphi=100;
       maxeta=100;
-      hitChi2Cut = 200;
-      goodHitChi2 = 50;
-      trackChi2Cut = 100;
-      /*
-      //main vertex tracking parameters:
-      a->SetTrackerParam(phi_segments,eta_segments,trackletlength,tracklength,
-      rowscopetracklet,rowscopetrack,
-      min_pt_fit,maxangle,goodDist,hitChi2Cut,
-      goodHitChi2,trackChi2Cut,50,maxphi,maxeta,kTRUE);
-      */
-      a->SetTrackerParam();
-      
-      //non vertex tracking parameters:
-      trackletlength = 2;
-      tracklength = 10;
-      rowscopetracklet = 2;
-      rowscopetrack = 2;
-      min_pt_fit = 0;
-      maxangle = 1.31;
-      goodDist = 0;
       hitChi2Cut = 100;
-      goodHitChi2 = 5;
-      trackChi2Cut = 80;
-      
-      a->SetTrackerParam(phi_segments,eta_segments,trackletlength,tracklength,
+      goodHitChi2 = 20;
+      trackChi2Cut = 50;
+       
+      //main vertex tracking parameters:
+       a->SetTrackerParam(phi_segments,eta_segments,trackletlength,tracklength,
 			 rowscopetracklet,rowscopetrack,
 			 min_pt_fit,maxangle,goodDist,hitChi2Cut,
-			 goodHitChi2,trackChi2Cut,50,maxphi,maxeta,kFALSE);
+			 goodHitChi2,trackChi2Cut,50,maxphi,maxeta,kTRUE);
       
-      // a->SetTrackerParam();
-      //a->DoNonVertexTracking();
-      a->WriteFiles();
-      //a->ProcessEvent(0,2);
-      a->ProcessEvent(minslice,maxslice);
-      //a->DoBench("benchmark_0");
-      
-      delete a;
+       a->WriteFiles();
+       a->ProcessEvent(minslice,maxslice);
+       //a->DoBench("benchmark_0");
+       delete a;
     }
+  
 }
