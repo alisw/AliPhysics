775851b0096919ed0daa39e8a3ea20fd8156efbc
vestbo <vestbo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-04-18 14:55:03 +0000

    Loading all present HLT libaries

--- HLT/exa/rootlogon.C
+++ HLT/exa/rootlogon.C
@@ -2,19 +2,23 @@
 {
   printf("\nWELCOME to the magic world of Level3\n\n"); 
 
-  //load library neccesary for TPC alone 
   gSystem->Load("$(ROOTSYS)/lib/libPhysics");
   gSystem->Load("$(ROOTSYS)/lib/libEG");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTEER");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libCONTAINERS");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTPC");
-  gSystem->Load("$(LEVEL3)/kip/MLUC/lib/linux-i386/libMLUC.so");
-  gSystem->Load("$(LEVEL3)/lib_$(USERNAME)/libAliL3.so");
-  gSystem->Load("libExa.so");
-//  gROOT->LoadMacro("../../dev/XFunct.C");
-  
-  gStyle->SetStatBorderSize(1);
-  gStyle->SetTitleBorderSize(0);
-  printf("TPC libraries loaded\n");
-  printf("Level3 libraries loaded\n");
+
+  if(1)
+    {
+      gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTEER");
+      gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libCONTAINERS");
+      gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTPC");
+      cout<<"TPC libraries loaded"<<endl;
+    }
+  if(1)
+    {
+      gSystem->Load("$(LEVEL3)/kip/MLUC/lib/linux-i386/libMLUC.so");
+      gSystem->Load("$(LEVEL3)/lib_$(USERNAME)/libAliL3");
+      gSystem->Load("$(LEVEL3)/lib_$(USERNAME)/libAliL3Hough");
+      gSystem->Load("$(LEVEL3)/lib_$(USERNAME)/libAliL3Comp");
+      cout<<"HLT libraries loaded"<<endl;
+    }
+
 }
