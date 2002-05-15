a7476a6eee72d6bec3c9a4a48de80c63813b850e
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-05-15 08:59:36 +0000

    First commit.

---
+++ EVGEN/AliGenBeamGas.h
@@ -0,0 +1,39 @@
+#ifndef ALIGENBEAMGAS_H
+#define ALIGENBEAMGAS_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+
+#include "AliGenExtFile.h"
+#include "AliGenReader.h"
+
+class TTree;
+
+class AliGenBeamGas : public AliGenExtFile
+{
+ public:
+    AliGenBeamGas();
+    AliGenBeamGas(const AliGenBeamGas &ext);
+    virtual ~AliGenBeamGas();
+    //
+    virtual void SetNumberOfInteractions(Int_t n) 
+	{fInteractions = n;}
+    // Initialise 
+    virtual void Init();
+    // generate event
+    virtual void Generate();
+ protected:
+    Int_t fInteractions;
+    
+    ClassDef(AliGenBeamGas,1) //Generate particles from external file
+	
+};
+#endif
+
+
+
+
+
+
