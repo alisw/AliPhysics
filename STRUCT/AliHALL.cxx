7749fae9e901a41eb3ef05f03db291b34c5fd88a
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-06-12 08:28:28 +0000

    HBMT positioning commented out.

--- STRUCT/AliHALL.cxx
+++ STRUCT/AliHALL.cxx
@@ -15,6 +15,9 @@
 
 /*
 $Log$
+Revision 1.15  2002/05/28 08:47:38  morsch
+Reduces size of pillars to avoid overlap with DIPO.
+
 Revision 1.14  2001/11/16 12:38:26  morsch
 Reverse z-ordering in HMBS. (I. Hrivnacova)
 
@@ -322,9 +325,9 @@ void AliHALL::CreateGeometry()
   gMC->Gsvolu("HMBU", "PGON", idtmed[1954], ppgon, 10);
 
   gMC->Gspos("HMBU", 1, "HMBT", 0., -70., 0., 0, "ONLY");
-*/
+
   gMC->Gspos("HMBT", 1, "ALIC", 0.,  70., 0., 0, "ONLY");
-  
+*/  
 }
 
 //_____________________________________________________________________________
