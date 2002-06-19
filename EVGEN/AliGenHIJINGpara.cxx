3f4c4c30934a95c28b3a4ad0632bf4bda27d736c
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-06-19 06:56:34 +0000

    Memory leak corrected

--- EVGEN/AliGenHIJINGpara.cxx
+++ EVGEN/AliGenHIJINGpara.cxx
@@ -15,6 +15,9 @@
 
 /*
 $Log$
+Revision 1.11  2002/03/20 10:21:13  hristov
+Set fPtMax to 15 GeV in order to avoid some numerical problems
+
 Revision 1.10  2001/10/15 16:44:46  morsch
 - Possibility for vertex distribution truncation.
 - Write mc header with vertex position.
@@ -246,27 +249,27 @@ void AliGenHIJINGpara::Init()
     fETApic = new TF1("etapic",&etapic,etaMin,etaMax,0);
     fETAkac = new TF1("etakac",&etakac,etaMin,etaMax,0);
 
-    TF1 *etaPic0 = new TF1("etapic",&etapic,-7,7,0);
-    TF1 *etaKac0 = new TF1("etakac",&etakac,-7,7,0);
+    TF1 etaPic0("etapic",&etapic,-7,7,0);
+    TF1 etaKac0("etakac",&etakac,-7,7,0);
 
-    TF1 *ptPic0  = new TF1("ptpi",&ptpi,0.,15.,0);
-    TF1 *ptKac0  = new TF1("ptka",&ptka,0.,15.,0);
+    TF1 ptPic0("ptpi",&ptpi,0.,15.,0);
+    TF1 ptKac0("ptka",&ptka,0.,15.,0);
 
-    Float_t intETApi  = etaPic0->Integral(-0.5, 0.5);
-    Float_t intETAka  = etaKac0->Integral(-0.5, 0.5);
+    Float_t intETApi  = etaPic0.Integral(-0.5, 0.5);
+    Float_t intETAka  = etaKac0.Integral(-0.5, 0.5);
     Float_t scalePi   = 7316/(intETApi/1.5);
     Float_t scaleKa   =  684/(intETAka/2.0);
 
 //  Fraction of events corresponding to the selected pt-range    
-    Float_t intPt    = (0.877*ptPic0->Integral(0, 15)+
-			0.123*ptKac0->Integral(0, 15));
-    Float_t intPtSel = (0.877*ptPic0->Integral(fPtMin, fPtMax)+
-			0.123*ptKac0->Integral(fPtMin, fPtMax));
+    Float_t intPt    = (0.877*ptPic0.Integral(0, 15)+
+			0.123*ptKac0.Integral(0, 15));
+    Float_t intPtSel = (0.877*ptPic0.Integral(fPtMin, fPtMax)+
+			0.123*ptKac0.Integral(fPtMin, fPtMax));
     Float_t ptFrac   = intPtSel/intPt;
 
 //  Fraction of events corresponding to the selected eta-range    
-    Float_t intETASel  = (scalePi*etaPic0->Integral(etaMin, etaMax)+
-			  scaleKa*etaKac0->Integral(etaMin, etaMax));
+    Float_t intETASel  = (scalePi*etaPic0.Integral(etaMin, etaMax)+
+			  scaleKa*etaKac0.Integral(etaMin, etaMax));
 //  Fraction of events corresponding to the selected phi-range    
     Float_t phiFrac    = (fPhiMax-fPhiMin)/2/TMath::Pi();
 
