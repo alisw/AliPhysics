e426e6c8ef1a4a388cf83abdc3ddce18e1e2b193
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-07-15 17:11:30 +0000

    First commit.

---
+++ EVGEN/AliStructFuncType.h
@@ -0,0 +1,24 @@
+#ifndef ALISTRUCFUNCTYPE_H
+#define ALISTRUCFUNCTYPE_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+typedef enum
+{
+    kDOSet1     = 1006,
+    kGRVLO      = 5005,
+    kGRVHO      = 5006,
+    kMRSDminus  = 3031,
+    kMRSD0      = 3030,
+    kMRSG       = 3041,
+    kCTEQ2pM    = 4024,
+    kCTEQ4L     = 4032,
+    kCTEQ4M     = 4034,
+    kMRSTcgLO   = 3072,
+    kCTEQ5L     = 4046,
+    kGRVLO98    = 5012
+}
+StrucFunc_t;
+#endif
