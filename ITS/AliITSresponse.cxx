fbed33f021ba68f42e973e61af544d67c6cab8db
nilsen <nilsen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-08-21 22:08:13 +0000

    Cleaned up code and added some general Silicon diffusion functions and parameters.

--- ITS/AliITSresponse.cxx
+++ ITS/AliITSresponse.cxx
@@ -2,8 +2,232 @@
 //  Response class for set:ITS                //
 ////////////////////////////////////////////////
 
+#include <TMath.h>
 #include <TF1.h>
 #include <TString.h>
 #include "AliITSresponse.h"
 
 ClassImp(AliITSresponse)
+
+//______________________________________________________________________
+AliITSresponse::AliITSresponse(){
+    // Default Constructor
+
+    fdv = 0.000375;  // 300 microns and 80 volts.
+    fN  = 0.0;
+    fT  = 300.0;
+}
+//______________________________________________________________________
+AliITSresponse::AliITSresponse(Double_t thickness){
+    // Default Constructor
+
+    fdv = thickness/80.0;   // 80 volts.
+    fN  = 0.0;
+    fT  = 300.0;
+}
+//______________________________________________________________________
+Double_t AliITSresponse::MobilityElectronSiEmp(){
+    // Computes the electron mobility in cm^2/volt-sec. Taken from SILVACO
+    // International ATLAS II, 2D Device Simulation Framework, User Manual 
+    // Chapter 5 Equation 5-6. An empirical function for low-field mobiliity 
+    // in silicon at different tempeatures.
+    // Inputs:
+    //    none.
+    // Output:
+    //    none.
+    // Return:
+    //    The Mobility of electrons in Si at a give temprature and impurity
+    //    concentration. [cm^2/Volt-sec]
+    const Double_t m0  = 55.24; // cm^2/Volt-sec
+    const Double_t m1  = 7.12E+08; // cm^2 (degree K)^2.3 / Volt-sec
+    const Double_t N0  = 1.072E17; // #/cm^3
+    const Double_t T0  = 300.; // degree K.
+    const Double_t eT0 = -2.3; // Power of Temp.
+    const Double_t eT1 = -3.8; // Power of Temp.
+    const Double_t eN  = 0.73; // Power of Dopent Consentrations
+    Double_t m;
+    Double_t T = fT,N = fN;
+
+    if(N<=0.0){ // Simple case.
+	if(T==300.) return 1350.0; // From Table 5-1 at consentration 1.0E14.
+	m = m1*TMath::Power(T,eT0);
+	return m;
+    } // if N<=0.0
+    m = m1*TMath::Power(T,eT0) - m0;
+    m /= 1.0 + TMath::Power(T/T0,eT1)*TMath::Power(N/N0,eN);
+    m += m0;
+    return m;
+}
+//______________________________________________________________________
+Double_t AliITSresponse::MobilityHoleSiEmp(){
+    // Computes the Hole mobility in cm^2/volt-sec. Taken from SILVACO
+    // International ATLAS II, 2D Device Simulation Framework, User Manual 
+    // Chapter 5 Equation 5-7 An empirical function for low-field mobiliity 
+    // in silicon at different tempeatures.
+    // Inputs:
+    //    none.
+    // Output:
+    //    none.
+    // Return:
+    //    The Mobility of Hole in Si at a give temprature and impurity
+    //    concentration. [cm^2/Volt-sec]
+    const Double_t m0a = 49.74; // cm^2/Volt-sec
+    const Double_t m0b = 49.70; // cm^2/Volt-sec
+    const Double_t m1  = 1.35E+08; // cm^2 (degree K)^2.3 / Volt-sec
+    const Double_t N0  = 1.606E17; // #/cm^3
+    const Double_t T0  = 300.; // degree K.
+    const Double_t eT0 = -2.2; // Power of Temp.
+    const Double_t eT1 = -3.7; // Power of Temp.
+    const Double_t eN  = 0.70; // Power of Dopent Consentrations
+    Double_t m;
+    Double_t T = fT,N = fN;
+
+    if(N<=0.0){ // Simple case.
+	if(T==300.) return 495.0; // From Table 5-1 at consentration 1.0E14.
+	m = m1*TMath::Power(T,eT0) + m0a-m0b;
+	return m;
+    } // if N<=0.0
+    m = m1*TMath::Power(T,eT0) - m0b;
+    m /= 1.0 + TMath::Power(T/T0,eT1)*TMath::Power(N/N0,eN);
+    m += m0a;
+    return m;
+}
+//______________________________________________________________________
+Double_t AliITSresponse::DiffusionCoefficientElectron(){
+    // Computes the Diffusion coefficient for electrons in cm^2/sec. Taken 
+    // from SILVACO International ATLAS II, 2D Device Simulation Framework, 
+    // User Manual Chapter 5 Equation 5-53. Einstein relations for diffusion 
+    // coefficient. Note: 1 cm^2/sec = 10 microns^2/nanosec.
+    // Inputs:
+    //    none.
+    // Output:
+    //    none.
+    // Return:
+    //    The Diffusion Coefficient of electrons in Si at a give temprature
+    //    and impurity concentration. [cm^2/sec]
+    // const Double_t kb = 1.3806503E-23; // Joules/degree K
+    // const Double_t qe = 1.60217646E-19; // Coulumbs.
+    const Double_t kbqe = 8.617342312E-5; // Volt/degree K
+    Double_t m = MobilityElectronSiEmp();
+    Double_t T = fT;
+
+    return m*kbqe*T;  // [cm^2/sec]
+}
+//______________________________________________________________________
+Double_t AliITSresponse::DiffusionCoefficientHole(){
+    // Computes the Diffusion coefficient for Holes in cm^2/sec. Taken 
+    // from SILVACO International ATLAS II, 2D Device Simulation Framework, 
+    // User Manual Chapter 5 Equation 5-53. Einstein relations for diffusion 
+    // coefficient. Note: 1 cm^2/sec = 10 microns^2/nanosec.
+    // Inputs:
+    //    none.
+    // Output:
+    //    none.
+    // Return:
+    //    The Defusion Coefficient of Hole in Si at a give temprature and 
+    //    impurity concentration. [cm^2/sec]
+    //    and impurity concentration. [cm^2/sec]
+    // const Double_t kb = 1.3806503E-23; // Joules/degree K
+    // const Double_t qe = 1.60217646E-19; // Coulumbs.
+    const Double_t kbqe = 8.617342312E-5; // Volt/degree K
+    Double_t m = MobilityHoleSiEmp();
+    Double_t T = fT;
+
+    return m*kbqe*T;  // [cm^2/sec]
+}
+//______________________________________________________________________
+Double_t AliITSresponse::SpeedElectron(){
+    // Computes the average speed for electrons in Si under the low-field 
+    // approximation. [cm/sec].
+    // Inputs:
+    //    none.
+    // Output:
+    //    none.
+    // Return:
+    //    The speed the holes are traveling at due to the low field applied. 
+    //    [cm/sec]
+    Double_t m = MobilityElectronSiEmp();
+
+    return m/fdv;  // [cm/sec]
+}
+//______________________________________________________________________
+Double_t AliITSresponse::SpeedHole(){
+    // Computes the average speed for Holes in Si under the low-field 
+    // approximation.[cm/sec].
+    // Inputs:
+    //    none.
+    // Output:
+    //    none.
+    // Return:
+    //    The speed the holes are traveling at due to the low field applied. 
+    //    [cm/sec]
+    Double_t m = MobilityHoleSiEmp();
+
+    return m/fdv;  // [cm/sec]
+}
+//______________________________________________________________________
+Double_t AliITSresponse::SigmaDiffusion3D(Double_t l){
+    // Returns the Gaussian sigma^2 == <x^2+y^2+z^2> [cm^2] due to the defusion 
+    // of electrons or holes through a distance l [cm] caused by an applied
+    // voltage v [volt] through a distance d [cm] in any material at a
+    // temperature T [degree K]. The sigma diffusion when expressed in terms
+    // of the distance over which the diffusion occures, l=time/speed, is 
+    // independent of the mobility and therefore the properties of the
+    // material. The charge distributions is given by 
+    // n = exp(-r^2/4Dt)/(4piDt)^1.5. From this <r^2> = 6Dt where D=mkT/e
+    // (m==mobility, k==Boltzman's constant, T==temparature, e==electric 
+    // charge. and vel=m*v/d. consiquently sigma^2=6kTdl/ev.
+    // Inputs:
+    //    Double_t l   Distance the charge has to travel.
+    // Output:
+    //    none.
+    // Return:
+    //    The Sigma due to the diffution of electrons. [cm]
+    const Double_t con = 5.17040258E-04; // == 6k/e [J/col or volts]
+
+    return TMath::Sqrt(con*fT*fdv*l);  // [cm]
+}
+//______________________________________________________________________
+Double_t AliITSresponse::SigmaDiffusion2D(Double_t l){
+    // Returns the Gaussian sigma^2 == <x^2+z^2> [cm^2] due to the defusion 
+    // of electrons or holes through a distance l [cm] caused by an applied
+    // voltage v [volt] through a distance d [cm] in any material at a
+    // temperature T [degree K]. The sigma diffusion when expressed in terms
+    // of the distance over which the diffusion occures, l=time/speed, is 
+    // independent of the mobility and therefore the properties of the
+    // material. The charge distributions is given by 
+    // n = exp(-r^2/4Dt)/(4piDt)^1.5. From this <x^2+z^2> = 4Dt where D=mkT/e
+    // (m==mobility, k==Boltzman's constant, T==temparature, e==electric 
+    // charge. and vel=m*v/d. consiquently sigma^2=4kTdl/ev.
+    // Inputs:
+    //    Double_t l   Distance the charge has to travel.
+    // Output:
+    //    none.
+    // Return:
+    //    The Sigma due to the diffution of electrons. [cm]
+    const Double_t con = 3.446935053E-04; // == 4k/e [J/col or volts]
+
+    return TMath::Sqrt(con*fT*fdv*l);  // [cm]
+}
+//______________________________________________________________________
+Double_t AliITSresponse::SigmaDiffusion1D(Double_t l){
+    // Returns the Gaussian sigma^2 == <x^2> [cm^2] due to the defusion 
+    // of electrons or holes through a distance l [cm] caused by an applied
+    // voltage v [volt] through a distance d [cm] in any material at a
+    // temperature T [degree K]. The sigma diffusion when expressed in terms
+    // of the distance over which the diffusion occures, l=time/speed, is 
+    // independent of the mobility and therefore the properties of the
+    // material. The charge distributions is given by 
+    // n = exp(-r^2/4Dt)/(4piDt)^1.5. From this <r^2> = 2Dt where D=mkT/e
+    // (m==mobility, k==Boltzman's constant, T==temparature, e==electric 
+    // charge. and vel=m*v/d. consiquently sigma^2=2kTdl/ev.
+    // Inputs:
+    //    Double_t l   Distance the charge has to travel.
+    // Output:
+    //    none.
+    // Return:
+    //    The Sigma due to the diffution of electrons. [cm]
+    const Double_t con = 1.723467527E-04; // == 2k/e [J/col or volts]
+
+    return TMath::Sqrt(con*fT*fdv*l);  // [cm]
+}
--- ITS/AliITSresponse.h
+++ ITS/AliITSresponse.h
@@ -1,7 +1,6 @@
 #ifndef ALIITSRESPONSE_H
 #define ALIITSRESPONSE_H
 
-
 #include <TObject.h>
 
 #include "AliITSsegmentation.h"
@@ -14,11 +13,13 @@ class AliITSgeom;
 //
 // ITS response virtual base class
 //
-class AliITSresponse :
-public TObject {
+class AliITSresponse : public TObject {
  public:
-
-
+    // Default Constructor
+    AliITSresponse();
+    // Standard Constructor
+    AliITSresponse(Double_t Thickness);
+    // Destructor.
     virtual ~AliITSresponse() {}
     //
     // Configuration methods
@@ -27,40 +28,49 @@ public TObject {
     // Set Electronics
     virtual void    SetElectronics(Int_t p1) {}
     // Get Electronics
-    virtual Int_t Electronics()  {return 0;}                       
+    virtual Int_t Electronics()  {return 0;}
 
     // Set maximum Adc-count value
     virtual void    SetMaxAdc(Float_t p1) {}
     // Get maximum Adc-count value
-    virtual Float_t MaxAdc()  {return 0.;}                       
+    virtual Float_t MaxAdc()  {return 0.;}
 
     // Set maximum Adc-top value
     virtual void    SetDynamicRange(Float_t p1) {}
     // Get maximum Adc-top value
-    virtual Float_t DynamicRange()  {return 0.0;}                       
+    virtual Float_t DynamicRange()  {return 0.0;}
 
     // Set Charge Loss Linear Coefficient
     virtual void    SetChargeLoss(Float_t p1) {}
     // Get Charge Loss Linear Coefficient
-    virtual Float_t ChargeLoss()  {return 0.0;}                       
+    virtual Float_t ChargeLoss()  {return 0.0;}
 
     // Diffusion coefficient
     virtual void    SetDiffCoeff(Float_t, Float_t) {}
     // Get diffusion coefficients
     virtual void    DiffCoeff(Float_t &,Float_t &) {}
 
-    // Temperature
-    virtual void    SetTemperature(Float_t) {}
-    // Get temperature
-    virtual Float_t Temperature() {return 0.;}  
+    // Temperature in [degree K]
+    virtual void    SetTemperature(Float_t t=300.0) {fT = t;}
+    // Get temperature [degree K]
+    virtual Float_t Temperature() {return fT;}
     // Type of data - real or simulated
     virtual void    SetDataType(const char *data) {}
+    // Set the impurity concentrations in [#/cm^3]
+    virtual void SetImpurity(Double_t n=0.0){fN = n;}
+    // Returns the impurity consentration in [#/cm^3]
+    virtual Double_t Impurity(){return fN;}
+    // Sets the applied ratio distance/voltage [cm/volt]
+    virtual void SetDistanceOverVoltage(Double_t d,Double_t v){fdv = d/v;}
+    // Sets the applied ration distance/voltage [cm/volt]. Default value
+    // is 300E-4cm/80 volts = 0.000375 cm/volts
+    virtual void SetDistanceOverVoltage(Double_t dv=0.000375){fdv = dv;}
+    // Returns the ration distance/voltage
+    virtual Double_t DistanceOverVoltage(){return fdv;}
     // Get data type
-    virtual const char  *DataType() const {return "";} 
-
-
+    virtual const char  *DataType() const {return "";}
  
-    // Set parameters options: "same" or read from "file" or "SetInvalid" or ...
+    // Set parameters options: "same" or read from "file" or "SetInvalid" or...
     virtual void   SetParamOptions(const char* opt1,const char* opt2) {}
     // Set noise parameters 
     virtual void   SetNoiseParam(Float_t, Float_t) {}
@@ -72,10 +82,10 @@ public TObject {
     // Parameters options
     virtual void   ParamOptions(char *,char*) {}
     virtual Int_t  NDetParam() {return 0;}
-    virtual void   GetDetParam(Float_t *) {} 
+    virtual void   GetDetParam(Float_t *) {}
     virtual void   GetNoiseParam(Float_t&, Float_t&) {}
 
-    // Zero-suppression option - could be 1D, 2D or non-ZeroSuppressed 
+    // Zero-suppression option - could be 1D, 2D or non-ZeroSuppressed
     virtual void   SetZeroSupp(const char* opt) {}
     // Get zero-suppression option
     virtual const char *ZeroSuppOption() const {return "";}
@@ -87,17 +97,16 @@ public TObject {
     virtual Int_t  MinVal() {return 0;};
 
     // Set filenames
-    virtual void   SetFilenames(const char *f1,const char *f2,const char *f3) {}
+    virtual void SetFilenames(const char *f1,const char *f2,const char *f3) {}
     // Filenames
     virtual void   Filenames(char*,char*,char*) {}
-
 
     virtual Float_t DriftSpeed() {return 0.;}
     virtual Bool_t  OutputOption() {return kFALSE;}
     virtual Bool_t  Do10to8() {return kTRUE;}
     virtual void    GiveCompressParam(Int_t *x) {}
 
-    //  
+    //
     // Detector type response methods
     // Set number of sigmas over which cluster disintegration is performed
     virtual void    SetNSigmaIntegration(Float_t p1) {}
@@ -111,22 +120,52 @@ public TObject {
     virtual Float_t GausLookUp(Int_t) {return 0.;}
     // Set sigmas of the charge spread function
     virtual void    SetSigmaSpread(Float_t p1, Float_t p2) {}
-    // Get sigmas for the charge spread 
+    // Get sigmas for the charge spread
     virtual void    SigmaSpread(Float_t &s1, Float_t &s2) {}
-
 
     // Pulse height from scored quantity (eloss)
     virtual Float_t IntPH(Float_t eloss) {return 0.;}
-    // Charge disintegration 
+    // Charge disintegration
     virtual Float_t IntXZ(AliITSsegmentation *) {return 0.;}
+    // Electron mobility in Si. [cm^2/(Volt Sec)]. T in degree K, N in #/cm^3
+    virtual Double_t MobilityElectronSiEmp();
+    // Hole mobility in Si. [cm^2/(Volt Sec)]  T in degree K, N in #/cm^3
+    virtual Double_t MobilityHoleSiEmp();
+    // Einstein relation for Diffusion Coefficient of Electrons. [cm^2/sec]
+    //  T in degree K, N in #/cm^3
+    virtual Double_t DiffusionCoefficientElectron();
+    // Einstein relation for Diffusion Coefficient of Holes. [cm^2/sec]
+    //  T in [degree K], N in [#/cm^3]
+    virtual Double_t DiffusionCoefficientHole();
+    // Electron <speed> under an applied electric field E=Volts/cm. [cm/sec]
+    // d distance-thickness in [cm], v in [volts], T in [degree K],
+    // N in [#/cm^3]
+    virtual Double_t SpeedElectron();
+    // Holes <speed> under an applied electric field E=Volts/cm. [cm/sec]
+    // d distance-thickness in [cm], v in [volts], T in [degree K],
+    // N in [#/cm^3]
+    virtual Double_t SpeedHole();
+    // Returns the Gaussian sigma == <x^2+z^2> [cm^2] due to the defusion of
+    // electrons or holes through a distance l [cm] caused by an applied
+    // voltage v [volt] through a distance d [cm] in any material at a
+    // temperature T [degree K].
+    virtual Double_t SigmaDiffusion3D(Double_t l);
+    // Returns the Gaussian sigma == <x^2 +y^2+z^2> [cm^2] due to the
+    // defusion of electrons or holes through a distance l [cm] caused by an
+    // applied voltage v [volt] through a distance d [cm] in any material at a
+    // temperature T [degree K].
+    virtual Double_t SigmaDiffusion2D(Double_t l);
+    // Returns the Gaussian sigma == <x^2+z^2> [cm^2] due to the defusion of
+    // electrons or holes through a distance l [cm] caused by an applied
+    // voltage v [volt] through a distance d [cm] in any material at a
+    // temperature T [degree K].
+    virtual Double_t SigmaDiffusion1D(Double_t l);
+ private:
+    Double_t fdv;  // The parameter d/v where d is the disance over which the
+                   // the potential v is applied d/v [cm/volts]
+    Double_t fN;   // the impurity consentration of the material in #/cm^3
+    Double_t fT;   // The temperature of the Si in Degree K.
 
-    ClassDef(AliITSresponse,1) // Detector type response virtual base class 
+    ClassDef(AliITSresponse,2) // Detector type response virtual base class 
 };
 #endif
-
-
-
-
-
-
-
