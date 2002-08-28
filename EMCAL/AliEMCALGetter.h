443c345d07dd7a7c6cff17ff6432f73a931705a2
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-08-28 14:57:27 +0000

    Removing ^M at the end of lines

--- EMCAL/AliEMCALGetter.h
+++ EMCAL/AliEMCALGetter.h
@@ -1,255 +1,510 @@
 #ifndef ALIEMCALGETTER_H
+
 #define ALIEMCALGETTER_H
+
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+
  * See cxx source for full Copyright notice                               */
+
+
 
 /* $Id$ */
 
+
+
 //_________________________________________________________________________
+
 //  A singleton that returns various objects 
+
 //  Should be used on the analysis stage to avoid confusing between different
+
 //  branches of reconstruction tree: e.g. reading RecPoints and TS made from 
+
 //  another set of RecPoints.
+
 // 
+
 //  The objects are retrived from folders.  
+
 //*-- Author: Yves Schutz (SUBATECH) & Dmitri Peressounko (RRC KI & SUBATECH)
+
 //    
 
+
+
 // Modif: 
+
 //  August 2002 Yves Schutz: clone PHOS as closely as possible and intoduction
+
 //                           of new  IO (à la PHOS)
+
  
+
 // --- ROOT system ---
+
 #include "TClonesArray.h"
+
 #include "TFolder.h"  
+
 #include "TTree.h"
+
 #include "TFile.h"
+
 class TString ;
+
 class TParticle ;
+
 class TTask ;
 
+
+
 // --- Standard library ---
+
 #include <stdlib.h>
+
 #include <iostream.h>
+
+
 
 // --- AliRoot header files ---
 
+
+
 #include "AliRun.h"
+
 #include "AliEMCAL.h" 
+
 #include "AliEMCALHit.h" 
+
 #include "AliEMCALDigit.h" 
+
 #include "AliEMCALTowerRecPoint.h"
+
 //class AliEMCALTrackSegment ;
+
 //class AliEMCALRecParticle ;
+
 class AliEMCALGeometry ;
+
 #include "AliEMCALDigitizer.h" 
+
 #include "AliEMCALSDigitizer.h"
+
 class AliEMCALClusterizer ;
+
 //class AliEMCALTrackSegmentMaker ;
+
 //class AliEMCALPID ;
 
+
+
 class AliEMCALGetter : public TObject {
+
   
+
  public:
+
   
+
   AliEMCALGetter(){  // ctor: this is a singleton, the ctor should never be called but cint needs it as public
+
     cerr << "ERROR: AliEMCALGetter is a singleton default ctor not callable" << endl ;
+
     abort() ; 
+
   } 
+
   AliEMCALGetter(const AliEMCALGetter & obj) {
+
     // cpy ctor requested by Coding Convention 
+
     // but not yet needed
+
     abort() ; 
+
   } 
+
   
+
   AliEMCALGetter & operator = (const AliEMCALGetter & ) {
+
     // assignement operator requested by coding convention, but not needed
+
     abort() ;
+
     return *this ; 
+
   }
+
   virtual ~AliEMCALGetter() ; 
+
   
+
   //=========== Instantiators ================  
+
   static AliEMCALGetter * GetInstance(const char* headerFile,
+
 				      const char* branchTitle = "Default", 
+
 				      const Bool_t toSplit = kFALSE ) ; 
+
   static AliEMCALGetter * GetInstance() ; 
+
  
+
   //=========== General information about run ============== 
+
   const Int_t  MaxEvent() const    { return static_cast<Int_t>(gAlice->TreeE()->GetEntries()) ; }
+
   const Int_t  EventNumber() const { return static_cast<Int_t>(gAlice->GetEvNumber()) ; }
+
   const Bool_t BranchExists(const TString recName) const ;
 
+
+
   //========== EMCALGeometry and EMCAL ============= 
+
   const AliEMCAL *   EMCAL() ;  
+
   AliEMCALGeometry * EMCALGeometry() ; 
 
+
+
   //========== Methods to read something from file ==========
+
   void   Event(const Int_t event, const char * opt = "HSDRP") ;    
+
   void   Track(const Int_t itrack) ;
+
   void   ReadTreeS(TTree * treeS,Int_t input) ; //Method to be used when 
+
                                                 //digitizing is under the conytrol of AliRunDigitizer, 
+
                                                 //which opens all files etc.
+
   //========== Alarms ======================
+
   //TFolder * Alarms() const { return dynamic_cast<TFolder*>(ReturnO("Alarms", 0)) ; }
+
   //const TObjArray *  Alarms(const char * name ) const { return dynamic_cast<const TObjArray*>(ReturnO("Alarms", name)) ; }
+
   //const TTask * QATasks(const char * name = 0) const { return ReturnT("QATasks", name) ; }
+
+
 
   //-----------------now getter's data--------------------------------------
 
+
+
   //=========== Primaries ============
+
   TTree *           TreeK(TString filename="") ; 
+
   TClonesArray *    Primaries(void) const { return dynamic_cast<TClonesArray*>(ReturnO("Primaries")) ; }
+
   const TParticle * Primary(Int_t index) const;
+
   const Int_t       NPrimaries()const { return fNPrimaries; }
+
   const TParticle * Secondary(TParticle * p, Int_t index=1) const ;  
+
  
+
   //=========== Hits =================
+
   TTree *               TreeH(TString filename="") ; 
+
   const TClonesArray *  Hits(void) { return dynamic_cast<const TClonesArray*>(ReturnO("Hits")) ; }
+
   const AliEMCALHit *   Hit(Int_t index) { return dynamic_cast<const AliEMCALHit*>(Hits()->At(index) );}
 
+
+
   //=========== SDigits ==============
+
   TTree *         TreeS(TString filename="") ; 
+
   TClonesArray *  SDigits(const char * name = 0, const char * file=0) { 
+
     return dynamic_cast<TClonesArray*>(ReturnO("SDigits", name, file)) ; }
+
   const AliEMCALDigit *  SDigit(Int_t index) { return static_cast<const AliEMCALDigit*>(SDigits()->At(index)) ;}
+
   const AliEMCALSDigitizer *  SDigitizer(const char * name =0) const { 
+
     return (const AliEMCALSDigitizer*)(ReturnT("SDigitizer", name)) ; // here static or dynamic cast does not work ! why ?
+
   }
+
+
 
   //========== Digits ================
+
   TTree *         TreeD(TString filename="") ; 
+
   TClonesArray *  Digits(const char * name = 0)const  { 
+
     return dynamic_cast<TClonesArray*>(ReturnO("Digits", name)) ; }
+
   const AliEMCALDigit *  Digit(Int_t index) { return static_cast<const AliEMCALDigit *>(Digits()->At(index)) ;}
+
   const AliEMCALDigitizer *  Digitizer(const char * name =0) const { 
+
     return (const AliEMCALDigitizer*)(ReturnT("Digitizer", name)) ; }
+
       
+
   //========== RecPoints =============
+
   TObjArray * TowerRecPoints(const char * name = 0) const { 
+
     return (dynamic_cast<TObjArray*>(ReturnO("TowerRecPoints", name))) ; }
+
   const AliEMCALTowerRecPoint *  TowerRecPoint(Int_t index) { return static_cast<const AliEMCALTowerRecPoint *>(TowerRecPoints()->At(index)) ;}
+
   TObjArray * PreShowerRecPoints(const char * name = 0) const { 
+
     return (dynamic_cast<TObjArray*>(ReturnO("PreShowerRecPoints", name))) ; }
+
   const AliEMCALClusterizer * Clusterizer (const char * name =0) const { 
+
     return (const AliEMCALClusterizer*)(ReturnT("Clusterizer", name)) ;// here static or dynamic cast does not work ! why ?
+
   }
 
+
+
   //========== TrackSegments ==========
+
   //TClonesArray * TrackSegments(const char * name = 0) const { 
+
   // return (dynamic_cast<TClonesArray*>(ReturnO("TrackSegments", name))) ; }
+
   //const AliEMCALTrackSegmentMaker * TrackSegmentMaker (const char * name =0) const {
+
   // return (const AliEMCALTrackSegmentMaker*)(ReturnT("TrackSegmentMaker", name)) ;// here static or dynamic cast does not work ! why }
 
+
+
   //========== RecParticles ===========
+
   //TClonesArray * RecParticles(const char * name = 0) const { 
+
   // return (dynamic_cast<TClonesArray*>(ReturnO("RecParticles", name))) ; }
+
   //const AliEMCALPID * PID(const char * name =0) const {
+
   // return (const AliEMCALPID*)(ReturnT("PID", name)) ;// here static or dynamic cast does not work ! why }
 
+
+
   //-----------------Auxiliary methods: cleaners-----------------
+
   void RemoveTask(TString opt, TString name) const ;
+
   void RemoveObjects(TString opt, TString name) const ; 
+
   void RemoveSDigits() const ; 
 
+
+
  //-----------------Auxiliary methods: miscellana-----------------
+
   void CloseFile() ;  
+
   const TFolder * Folder(const TString what) const ;
+
   const Bool_t HasFailed(void) const {return fFailed ;} 
+
   void ListBranches(Int_t event=0) const ;
+
   void NewBranch(TString name, Int_t event = 0) ; 
+
   Bool_t NewFile(TString name) ;
+
   TFolder * SDigitsFolder() { return dynamic_cast<TFolder*>(fSDigitsFolder->FindObject("EMCAL")) ; }
+
   void SetDebug(Int_t level) {fDebug = level;} // Set debug level
+
   void SetRecParticlesTitle(const TString title) { fRecParticlesTitle = title ; }
 
+
+
   //------------Auxiliary methods: Posters--------------------
+
   const Bool_t PostPrimaries(void ) const ;  
+
   const Bool_t PostHits(void ) const ;  
+
   const Bool_t PostSDigits(      const char * name,  const char * file = 0) const ;  
+
   const Bool_t PostDigits(       const char * name ) const ;  
+
   const Bool_t PostRecPoints(    const char * name ) const ;  
+
   //const Bool_t PostTrackSegments(const char * name) const ;  
+
   //const Bool_t PostRecParticles( const char * name) const ;  
+
   const Bool_t PostClusterizer( const char * name) const ;  
+
   const Bool_t PostClusterizer(AliEMCALClusterizer * clu) const ;  
+
   const Bool_t PostSDigitizer (AliEMCALSDigitizer * sdigitizer) const ;  
+
   const Bool_t PostSDigitizer ( const char * name, const char * file ) const ;  
+
   const Bool_t PostDigitizer (AliEMCALDigitizer * digitizer) const ;  
+
   const Bool_t PostDigitizer  ( const char * name) const ;  
+
   //const Bool_t PostTrackSegmentMaker(AliEMCALTrackSegmentMaker * tsm) const ;  
+
   //const Bool_t PostTrackSegmentMaker(const char * name ) const ;  
+
   //const Bool_t PostPID  (AliEMCALPID * pid) const ;  
+
   //const Bool_t PostPID  (const char * name ) const ;  
+
   //const Bool_t PostQA   (void) const ;
+
   
+
  private:
+
+
 
   AliEMCALGetter(const char* headerFile, const char* branchTitle ="Default", const Bool_t toSplit = kFALSE) ; 
+
   TObject * ReturnO(TString what, TString name=0, TString file=0) const ; 
+
   const TTask * ReturnT(TString what,TString name=0) const ; 
+
   void DefineBranchTitles(char* branch, char* branchTitle) ;
+
   Int_t ReadTreeD(const Int_t event) ;
+
   Int_t ReadTreeH(void) ;
+
   Int_t ReadTreeR(const Int_t event) ;
+
   Int_t ReadTreeS(const Int_t event) ;
+
   void ReadTreeQA(void) ;
+
   void ReadPrimaries(void) ;
+
   void CleanWhiteBoard(void) ;
+
   void CloseSplitFiles(void) ;
+
   void SetTitle(const char * title) ;
 
+
+
   TObject ** PrimariesRef(void) const ;
+
   TObject ** HitsRef(void) const ;
+
   TObject ** SDigitsRef(const char * name, const char * file = 0 ) const;
+
   TObject ** DigitsRef (const char * name)   const ;
+
   TObject ** TowerRecPointsRef (const char * name) const ;
+
   TObject ** PreShowerRecPointsRef (const char * name) const ;
+
   //TObject ** TrackSegmentsRef(const char * name)   const ;
+
   //TObject ** RecParticlesRef (const char * name)   const ;
+
   //TObject ** AlarmsRef (void)   const ;
 
+
+
   TObject ** SDigitizerRef (const char * name) const ; 
+
   TObject ** DigitizerRef  (const char * name) const ; 
+
   TObject ** ClusterizerRef(const char * name) const ; 
+
   //TObject ** TSMakerRef    (const char * name) const ; 
+
   //TObject ** PIDRef        (const char * name) const ; 
+
+
 
  private:
 
+
+
   static TFile * fFile ;              //!
+
   Bool_t         fToSplit ;           //! Do we work in the split mode
+
   TString        fHeaderFile ;        //! File in which gAlice lives
+
   TString        fBranchTitle ;       //!
+
   TString        fTrackSegmentsTitle ;//! 
+
   TString        fTrackSegmentsFileName ;//! 
+
   TString        fRecPointsTitle ;    //!
+
   TString        fRecPointsFileName ;    //!
+
   TString        fRecParticlesTitle ; //!
+
   TString        fRecParticlesFileName ; //!
+
   TString        fDigitsTitle ;       //!TDirectory tempo(gDirectory) 
+
   TString        fDigitsFileName ;    //! TDirectory tempo(gDirectory)  
+
   TString        fSDigitsTitle ;      //!
+
   TString        fSDigitsFileName ;      //!
+
   Bool_t         fFailed ;            //! true if file is not opened and/or galice not found
+
   Int_t          fDebug ;             // Debug level
+
   AliRun *       fAlice ;             //! needed to read TreeK if in an other file than fHeaderFile
+
   Int_t          fNPrimaries ;        //! # of primaries 
+
   TObjArray *    fPrimaries ;         //! list of lists of primaries-for the case of mixing
+
   TFolder *      fModuleFolder ;      //!Folder that contains the modules 
+
   TFolder *      fPrimariesFolder ;   //!Folder that contains the Primary Particles 
+
   TFolder *      fHitsFolder ;        //!Folder that contains the Hits 
+
   TFolder *      fSDigitsFolder ;     //!Folder that contains the SDigits 
+
   TFolder *      fDigitsFolder ;      //!Folder that contains the Digits 
+
   TFolder *      fRecoFolder ;        //!Folder that contains the reconstructed objects (RecPoints, TrackSegments, RecParticles) 
+
   TFolder *      fQAFolder ;          //!Folder that contains the QA objects  
+
   TFolder *      fTasksFolder ;       //!Folder that contains the Tasks (sdigitizer, digitizer, reconstructioner)
+
  
+
   static AliEMCALGetter * fgObjGetter; // pointer to the unique instance of the singleton 
+
+
 
   ClassDef(AliEMCALGetter,1)  // Algorithm class that provides methods to retrieve objects from a list knowing the index 
 
+
+
 };
 
+
+
 #endif // AliEMCALGETTER_H
+
--- EMCAL/AliEMCALv1.cxx
+++ EMCAL/AliEMCALv1.cxx
@@ -1,222 +1,444 @@
 /**************************************************************************
+
  * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+
  *                                                                        *
+
  * Author: The ALICE Off-line Project.                                    *
+
  * Contributors are mentioned in the code where appropriate.              *
+
  *                                                                        *
+
  * Permission to use, copy, modify and distribute this software and its   *
+
  * documentation strictly for non-commercial purposes is hereby granted   *
+
  * without fee, provided that the above copyright notice appears in all   *
+
  * copies and that both the copyright notice and this permission notice   *
+
  * appear in the supporting documentation. The authors make no claims     *
+
  * about the suitability of this software for any purpose. It is          *
+
  * provided "as is" without express or implied warranty.                  *
+
  **************************************************************************/
+
+
 
 /* $Id$ */
 
+
+
 //_________________________________________________________________________
+
 // Implementation version v1 of EMCAL Manager class 
+
 // An object of this class does not produce digits
+
 // It is the one to use if you do want to produce outputs in TREEH 
+
 //                  
+
 //*-- Author: Sahal Yacoob (LBL /UCT)
+
 //*--       : Jennifer Klay (LBL)
 
+
+
 // This Class not stores information on all particles prior to EMCAL entry - in order to facilitate analysis.
+
 // This is done by setting fIShunt =2, and flagging all parents of particles entering the EMCAL.
 
+
+
 // 15/02/2002 .... Yves Schutz
+
 //  1. fSamplingFraction and fLayerToPreshowerRatio have been removed
+
 //  2. Timing signal is collected and added to hit
 
+
+
 // --- ROOT system ---
+
 #include "TPGON.h"
+
 #include "TTUBS.h"
+
 #include "TNode.h"
+
 #include "TRandom.h"
+
 #include "TTree.h"
+
 #include "TGeometry.h"
+
 #include "TParticle.h"
+
+
 
 // --- Standard library ---
 
+
+
 #include <stdio.h>
+
 #include <string.h>
+
 #include <stdlib.h>
+
 #include <strstream.h>
+
 #include <iostream.h>
+
 #include <math.h>
+
 // --- AliRoot header files ---
 
+
+
 #include "AliEMCALv1.h"
+
 #include "AliEMCALHit.h"
+
 #include "AliEMCALGeometry.h"
+
 #include "AliConst.h"
+
 #include "AliRun.h"
+
 #include "AliMC.h"
+
+
 
 ClassImp(AliEMCALv1)
 
 
-//______________________________________________________________________
-AliEMCALv1::AliEMCALv1():AliEMCALv0(){
-  // ctor
-    fLightYieldMean = 0 ; 
-    fIntrinsicAPDEfficiency = fLightFactor = fLightYieldAttenuation =   fAPDFactor = fAPDGain = fRecalibrationFactor = fAPDFactor = 0. ; 
-}
+
+
 
 //______________________________________________________________________
+
+AliEMCALv1::AliEMCALv1():AliEMCALv0(){
+
+  // ctor
+
+    fLightYieldMean = 0 ; 
+
+    fIntrinsicAPDEfficiency = fLightFactor = fLightYieldAttenuation =   fAPDFactor = fAPDGain = fRecalibrationFactor = fAPDFactor = 0. ; 
+
+}
+
+
+
+//______________________________________________________________________
+
 AliEMCALv1::AliEMCALv1(const char *name, const char *title):
+
     AliEMCALv0(name,title){
+
     // Standard Creator.
 
+
+
     fHits= new TClonesArray("AliEMCALHit",1000);
+
     gAlice->AddHitList(fHits);
 
+
+
     fNhits = 0;
+
     fIshunt     =  2; // All hits are associated with particles entering the calorimeter
 
+
+
     //Photoelectron statistics:
+
     // The light yield is a poissonian distribution of the number of
+
     // photons created in a plastic layer, calculated using following formula
+
     // NumberOfPhotons = EnergyLost * LightYieldMean* APDEfficiency *
+
     //              exp (-LightYieldAttenuation * DistanceToPINdiodeFromTheHit)
+
     // LightYieldMean is parameter calculated to be over 100000 photons per GeV (a guess)
+
     // APDEfficiency is 0.02655
+
     // fLightYieldAttenuation is 0.0045 a guess
+
     // TO BE FIXED
+
     //***** Need a method in geometry to retrieve the fiber length corresponding to each layer
+
     //***** See the step manager for the light attenuation calculation 
+
     // The number of electrons created in the APD is
+
     // NumberOfElectrons = APDGain * LightYield
+
     // The APD Gain is 300
+
     
+
     fLightYieldMean         = 10000000.;  // This is a guess
+
     fIntrinsicAPDEfficiency = 0.02655 ;
+
     fLightFactor            = fLightYieldMean * fIntrinsicAPDEfficiency ; 
+
     fLightYieldAttenuation  = 0.0045 ; // an other guess 
+
     fAPDGain                = 300. ;
+
     fRecalibrationFactor    = 13.418/ fLightYieldMean ;
+
     fAPDFactor              = (fRecalibrationFactor/100.) * fAPDGain ; 
 
+
+
 }
 
+
+
 //______________________________________________________________________
+
 AliEMCALv1::~AliEMCALv1(){
+
     // dtor
 
+
+
     if ( fHits) {
+
 	fHits->Delete();
+
 	delete fHits;
+
 	fHits = 0;
+
     }
+
 }
+
 //______________________________________________________________________
+
 void AliEMCALv1::AddHit(Int_t shunt, Int_t primary, Int_t tracknumber, Int_t iparent, Float_t ienergy, 
+
 			Int_t id, Float_t * hits,Float_t * p){
+
     // Add a hit to the hit list.
+
     // An EMCAL hit is the sum of all hits in a single segment 
+
     //   originating from the same enterring particle 
+
     Int_t hitCounter;
+
     
+
     AliEMCALHit *newHit;
+
     AliEMCALHit *curHit;
+
     Bool_t deja = kFALSE;
 
+
+
     newHit = new AliEMCALHit(shunt, primary, tracknumber, iparent, ienergy, id, hits, p);
+
     for ( hitCounter = fNhits-1; hitCounter >= 0 && !deja; hitCounter-- ) {
+
 	curHit = (AliEMCALHit*) (*fHits)[hitCounter];
+
 	// We add hits with the same tracknumber, while GEANT treats
+
 	// primaries succesively
+
 	if(curHit->GetPrimary() != primary) break;
+
 	if( *curHit == *newHit ) {
+
 	    *curHit = *curHit + *newHit;
+
 	    deja = kTRUE;
+
 	} // end if
+
     } // end for hitCounter
 
+
+
     if ( !deja ) {
+
 	new((*fHits)[fNhits]) AliEMCALHit(*newHit);
+
 	fNhits++;
+
     } // end if
 
+
+
     delete newHit;
+
 }
+
 //______________________________________________________________________
+
 void AliEMCALv1::StepManager(void){
+
   // Accumulates hits as long as the track stays in a single
+
   // crystal or PPSD gas Cell
 
+
+
   Int_t          id[2];           // (layer, phi, Eta) indices
+
   Int_t          absid;
+
   // position wrt MRS and energy deposited
+
   Float_t        xyzte[5]={0.,0.,0.,0.,0.};// position wrt MRS, time and energy deposited
+
   Float_t        pmom[4]={0.,0.,0.,0.};
+
   TLorentzVector pos; // Lorentz vector of the track current position.
+
   TLorentzVector mom; // Lorentz vector of the track current momentum.
+
   Int_t tracknumber =  gAlice->CurrentTrack();
+
   Int_t primary = 0;
+
   static Int_t iparent = 0;
+
   static Float_t ienergy = 0;
+
   Int_t copy = 0;
+
   
+
   AliEMCALGeometry * geom = GetGeometry() ; 
 
+
+
   if(gMC->IsTrackEntering() && (strcmp(gMC->CurrentVolName(),"XALU") == 0)){ // This Particle in enterring the Calorimeter
+
     gMC->TrackPosition(pos) ;
+
     xyzte[0] = pos[0] ;
+
     xyzte[1] = pos[1] ;
+
     xyzte[2] = pos[2] ;
+
     if ( (xyzte[0]*xyzte[0] + xyzte[1]*xyzte[1])  
+
 	 <  (geom->GetEnvelop(0)+geom->GetGap2Active()+1.5 )*(geom->GetEnvelop(0)+geom->GetGap2Active()+1.5 ) ) {
+
       iparent = tracknumber;
+
       gMC->TrackMomentum(mom);
+
       ienergy = mom[3]; 
+
       TParticle * part = 0 ;
+
       Int_t parent = iparent ;
+
       while ( parent != -1 ) { // <------------- flags this particle to be kept and
+
 	//all the ancestors of this particle
+
 	part = gAlice->Particle(parent) ;
+
 	part->SetBit(kKeepBit);
+
 	parent = part->GetFirstMother() ;
+
       }
+
     }
+
   }
+
   if(gMC->CurrentVolID(copy) == gMC->VolId("XPHI") ) { // We are in a Scintillator Layer 
+
     
+
     Float_t depositedEnergy ; 
+
     
+
     if( (depositedEnergy = gMC->Edep()) > 0.){// Track is inside a scintillator and deposits some energy
+
       
+
       gMC->TrackPosition(pos);
+
       xyzte[0] = pos[0];
+
       xyzte[1] = pos[1];
+
       xyzte[2] = pos[2];
+
       xyzte[3] = gMC->TrackTime() ;       
+
       
+
       gMC->TrackMomentum(mom);
+
       pmom[0] = mom[0];
+
       pmom[1] = mom[1];
+
       pmom[2] = mom[2];
+
       pmom[3] = mom[3];
+
       
+
       gMC->CurrentVolOffID(1, id[0]); // get the POLY copy number;
+
       gMC->CurrentVolID(id[1]); // get the phi number inside the layer
+
       absid = (id[0]-1)*(geom->GetNPhi()) + id[1];
+
       
+
       //Calculates the light yield, the number of photons produced in the
+
       //plastic layer 
+
       // Here we need to know the fiber lebgth to calculate the attenuation
+
      
+
       Float_t lengthOfFiber = 0. ;// should be retrieved from the geometry
 
+
+
       Float_t lightYield = gRandom->Poisson(fLightFactor * depositedEnergy *
+
 					    exp(-fLightYieldAttenuation * lengthOfFiber)) ;
+
       xyzte[4] = fAPDFactor * lightYield  ;
+
    
+
       primary = gAlice->GetPrimary(tracknumber);
+
       AddHit(fIshunt, primary,tracknumber, iparent, ienergy, absid, xyzte, pmom);
+
     } // there is deposited energy
+
   }
+
 }
+
