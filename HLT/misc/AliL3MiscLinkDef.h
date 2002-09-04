8351a9f3b99c88596dcec26ddb3d0c8c8490441b
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-09-04 13:56:42 +0000

    Added AliL3Transbit entry.

--- HLT/misc/AliL3MiscLinkDef.h
+++ HLT/misc/AliL3MiscLinkDef.h
@@ -4,9 +4,9 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class AliTransBit; 
-#pragma link C++ class AliTransBit_v1; 
-#pragma link C++ class AliTransBit_v2; 
+#pragma link C++ class AliL3TransBit; 
+#pragma link C++ class AliL3TransBit_v1; 
+#pragma link C++ class AliL3TransBit_v2; 
 #pragma link C++ class AliL3AltroMemHandler;
 #pragma link C++ class AliL3DataHandler;
 #pragma link C++ class AliL3VHDLClusterFinder;
