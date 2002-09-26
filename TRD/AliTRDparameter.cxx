fba5a5694e414c9f9b6a454b7dc65548f616ed10
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-09-26 09:26:31 +0000

    Bug fix in LUT

--- TRD/AliTRDparameter.cxx
+++ TRD/AliTRDparameter.cxx
@@ -15,6 +15,9 @@
 
 /*
 $Log$
+Revision 1.6  2002/06/12 09:54:35  cblume
+Update of tracking code provided by Sergei
+
 Revision 1.5  2002/04/30 08:30:40  cblume
 gAlice now only read by AliRunDigitizer. Therefore it is just deleted in AliTRDmerge.C
 
@@ -1209,7 +1212,7 @@ void AliTRDparameter::FillLUT()
 
   for (Int_t iplan = 0; iplan < kNplan; iplan++) {
     for (Int_t ilut  = 0; ilut  < kNlut; ilut++) {
-      fLUT[iplan*kNplan+ilut] = lut[iplan][ilut];
+      fLUT[iplan*kNlut+ilut] = lut[iplan][ilut];
     }
   }
 
@@ -1332,6 +1335,7 @@ Double_t AliTRDparameter::LUTposition(Int_t iplane, Double_t ampL
   //
 
   const Int_t kNplan = AliTRDgeometry::kNplan;
+  const Int_t kNlut  = 128;
 
   Double_t pos;
   Double_t x = 0.0;
@@ -1368,7 +1372,7 @@ Double_t AliTRDparameter::LUTposition(Int_t iplane, Double_t ampL
     } 
     else {
       ix  = (Int_t) ((x - xmin) / xwid);
-      pos = side * fLUT[iplane*kNplan+ix];
+      pos = side * fLUT[iplane*kNlut+ix];
     }
        
   } 
