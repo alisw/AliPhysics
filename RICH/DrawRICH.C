45fd0d880428840d245c0532dfd6b5bae389a742
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-10-29 15:06:19 +0000

    Corrected for rotated RICH. (J. Barbosa)

--- RICH/DrawRICH.C
+++ RICH/DrawRICH.C
@@ -4,13 +4,13 @@ void DrawRICH()
    gMC->Gsatt("alic", "seen", 0);
    gROOT->LoadMacro("ViewRICH.C");
    gInterpreter->ProcessLine("ViewRICH()");
-   gMC->Gdopt("hide", "on");
-   gMC->Gdopt("shad", "on");
-   gMC->Gsatt("*", "fill", 7);
+   gMC->Gdopt("hide", "off");
+   gMC->Gdopt("shad", "off");
+   gMC->Gsatt("*", "fill", 1);
    gMC->SetClipBox(".");
-   gMC->SetClipBox("*", 0, 3000, -3000, 3000, -6000, 6000);
+   gMC->SetClipBox("*", 0, 2000, 0, 2000, 0, 2000);
    gMC->DefaultRange();
-   gMC->Gdraw("alic", 40, 120, 0, 10, 0, .03, .03);
+   gMC->Gdraw("alic", 30, 40, 0, -2, 4, .03, .03);
    gMC->Gdhead(1111, "Ring Imaging Cherenkov");
    gMC->Gdman(16, 6, "MAN");
 }
