b53a130bd0b790d3aee44815a12c073e949d4eb0
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-11-09 19:19:20 +0000

    Removing AliGeant4, now it is in a separate module aligeant4

--- AliGeant4/macro/STRUCT/ConfigABSO.C
+++
@@ -1,9 +0,0 @@
-void Config(Int_t version)
-{
-  AliABSO* ABSO = 0;
-  switch (version) {
-    case 0: ABSO  = new AliABSOv0("ABSO","Muon Absorber"); break;
-  }
-
-//=================== ABSO parameters ============================
-}  
--- AliGeant4/macro/STRUCT/ConfigHALL.C
+++
@@ -1,9 +0,0 @@
-void Config(Int_t version)
-{
-  AliHALL* HALL = 0;
-  switch (version) {
-    case 0: HALL  = new AliHALL("HALL","Alice Hall"); break;
-  }  
-
-//=================== HALL parameters ============================
-}  
