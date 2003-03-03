49a0da6b49d3c3d7c1edd24986e9d389e554f7fc
kowal2 <kowal2@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-03-03 16:38:25 +0000

    Updated by J. Chudoba

--- TPC/AliTPCTracking.C
+++ TPC/AliTPCTracking.C
@@ -12,14 +12,18 @@
 // input parameters: 
 //        Int_t nEvents      ... nr of events to process
 //        Int_t firstEventNr ... first event number (starts from 0)
-//        TString fileNameHits ... name of file with hits
-//        TString fileNameDigits .. name of file with TPC digits
-//        TString fileNameClusters .. name of file with TPC clusters (output)
-//        TString fileNameTracks .. name of file with TPC tracks (output)
+//        Char_t* fileNameHits ... name of file with hits
+//        Char_t* fileNameDigits .. name of file with TPC digits
+//        Char_t* fileNameClusters .. name of file with TPC clusters (output)
+//        Char_t* fileNameTracks .. name of file with TPC tracks (output)
 //
 //        default file names correspond to pp production (2002-04)
 //
 // History:
+//
+//     03.03.2003 ... SetFieldFactor moved to AliTracker class and
+//                    LoadTPCParam moved to AliTPC class
+//                    TString replaced by Char_t*
 //
 //     20.11.2002 ... Changes due to a changed interface of AliTPCtracker. 
 //                    Use Riostream.h instead of iostream.h
@@ -36,49 +40,47 @@
 #include "TPC/alles.h"
 #include "TPC/AliTPCtracker.h"
 #include "TPC/AliTPCclusterer.h"
+#include "TPC/AliTPC.h"
 #include "STEER/AliRun.h"
 #include "STEER/AliHeader.h"
 #include "STEER/AliGenEventHeader.h"
 #include "STEER/AliMagF.h"
+#include "STEER/AliTracker.h"
 
 #endif
 
 Int_t gDEBUG = 2;
 
 Int_t TPCFindClusters(Int_t nEvents=1, Int_t firstEvent=0,
-		      TString fileNameDigits="rfio:galiceSDR.root", 
-		      TString fileNameClusters="tpc.clusters.root");
+		      Char_t* fileNameDigits="rfio:galiceSDR.root", 
+		      Char_t* fileNameClusters="tpc.clusters.root");
 Int_t TPCFindClusters(Int_t nEvents, Int_t firstEvent,
 		      TFile* fileDigits, TFile* fileClusters, 
 		      AliTPCParam* paramTPC=0);
 Int_t TPCFindTracks(Int_t nEvents=1, Int_t firstEvent=0,
-		    TString fileNameClusters="tpc.clusters.root",
-		    TString fileNameTracks="tpc.tracks.root");
+		    Char_t* fileNameClusters="tpc.clusters.root",
+		    Char_t* fileNameTracks="tpc.tracks.root");
 Int_t TPCFindTracks(Int_t nEvents, Int_t firstEvent,
 		    TFile* fileClusters, TFile* fileTracks,
 		    AliTPCParam* paramTPC=0);
 
-AliTPCParam* LoadTPCParam(TFile *file);
 void FindVertex(Int_t iEvent, Double_t *vertex);
 void PrintVertex(TArrayF &primaryVertex);
-Int_t SetFieldFactor(TString fileName, Bool_t closeFile = kTRUE);
-Int_t SetFieldFactor(TFile* file, Bool_t deletegAlice = kTRUE);
-Int_t SetFieldFactor();
 
 Int_t AliTPCTracking(Int_t nEvents=1, Int_t firstEvent=0,
-		     TString fileNameHits="rfio:galice.root",
-		     TString fileNameDigits="rfio:galiceSDR.root",
-		     TString fileNameClusters="tpc.clusters.root",
-		     TString fileNameTracks="tpc.tracks.root");
+		     Char_t* fileNameHits="rfio:galice.root",
+		     Char_t* fileNameDigits="rfio:galiceSDR.root",
+		     Char_t* fileNameClusters="tpc.clusters.root",
+		     Char_t* fileNameTracks="tpc.tracks.root");
 
 ////////////////////////////////////////////////////////////////////////
 Int_t AliTPCTracking( Int_t nEvents, Int_t firstEvent,
-		      TString fileNameHits,
-		      TString fileNameDigits,
-		      TString fileNameClusters,
-		      TString fileNameTracks) {
+		      Char_t* fileNameHits,
+		      Char_t* fileNameDigits,
+		      Char_t* fileNameClusters,
+		      Char_t* fileNameTracks) {
 
-  SetFieldFactor(fileNameHits,kFALSE);
+  AliTracker::SetFieldFactor(fileNameHits,kFALSE);
 
 // ********** Find TPC clusters *********** //
   if (TPCFindClusters(nEvents,firstEvent,fileNameDigits,fileNameClusters)) {
@@ -97,7 +99,7 @@ Int_t AliTPCTracking( Int_t nEvents, Int_t firstEvent,
 
 ////////////////////////////////////////////////////////////////////////
 Int_t TPCFindClusters(Int_t nEvents, Int_t firstEvent,
-		      TString fileNameDigits, TString fileNameClusters) {
+		      Char_t* fileNameDigits, Char_t* fileNameClusters) {
   
   Int_t rc;
   const Char_t *name="TPCFindClusters";
@@ -131,7 +133,7 @@ Int_t TPCFindClusters(Int_t nEvents, Int_t firstEvent,
 		      AliTPCParam* paramTPC) {
 
   fileDigits->cd();
-  if (!paramTPC) paramTPC = LoadTPCParam(fileDigits);
+  if (!paramTPC) paramTPC = AliTPC::LoadTPCParam(fileDigits);
   if (!paramTPC) return 1;
 
   for (Int_t iEvent = firstEvent; iEvent < firstEvent+nEvents; iEvent++){
@@ -142,7 +144,7 @@ Int_t TPCFindClusters(Int_t nEvents, Int_t firstEvent,
 }
 ////////////////////////////////////////////////////////////////////////
 Int_t TPCFindTracks(Int_t nEvents, Int_t firstEvent,
-		    TString fileNameClusters, TString fileNameTracks) {
+		    Char_t* fileNameClusters, Char_t* fileNameTracks) {
 
   Int_t rc = 0;
   const Char_t *name="TPCFindTracks";
@@ -168,7 +170,7 @@ Int_t TPCFindTracks(Int_t nEvents, Int_t firstEvent,
 		    AliTPCParam* paramTPC) {
 
   Int_t rc = 0;
-  if (!paramTPC) paramTPC = LoadTPCParam(fileClusters);
+  if (!paramTPC) paramTPC = AliTPC::LoadTPCParam(fileClusters);
   if (!paramTPC) return 1;
 
   for (Int_t iEvent = firstEvent; iEvent < firstEvent+nEvents; iEvent++){
@@ -183,77 +185,6 @@ Int_t TPCFindTracks(Int_t nEvents, Int_t firstEvent,
     delete tracker;
   }
   return rc;
-}
-
-////////////////////////////////////////////////////////////////////////
-Int_t SetFieldFactor() {
-
-   AliKalmanTrack::SetConvConst(1000/0.299792458/gAlice->Field()->SolenoidField());
-   if (gDEBUG > 2) cout<<"Magnetic field in kGauss: "<<gAlice->Field()->SolenoidField()<<endl;
-   return 0;
-}
-////////////////////////////////////////////////////////////////////////
-Int_t SetFieldFactor(TFile *file, Bool_t deletegAlice) {
-
-  if (!(gAlice=(AliRun*)file->Get("gAlice"))) {
-    cerr<<"gAlice has not been found in file "<<file->GetName();
-    return 1;
-  }   
-  Int_t rc = SetFieldFactor();
-  if (deletegAlice) {
-    delete gAlice;  
-    gAlice = 0;
-  }
-  return rc;
-}
-////////////////////////////////////////////////////////////////////////
-Int_t SetFieldFactor(TString fileName, Bool_t closeFile) {
-
-   TFile *file=TFile::Open(fileName);
-   if (!file->IsOpen()) {cerr<<"Cannnot open "<<fileName<<" !\n"; return 1;}
-   Int_t rc = SetFieldFactor(file, closeFile) ;
-   if (closeFile) file->Close();
-   return rc;
-}
-////////////////////////////////////////////////////////////////////////
-AliTPCParam* LoadTPCParam(TFile *file) {
-
-  char paramName[50];
-  sprintf(paramName,"75x40_100x60_150x60");
-  AliTPCParam *paramTPC=(AliTPCParam*)file->Get(paramName);
-  if (paramTPC) {
-    if (gDEBUG > 1) cout<<"TPC parameters "<<paramName<<" found."<<endl;
-  } else {
-    cerr<<"TPC parameters not found. Create new (they may be incorrect)."
-	<<endl;    
-    paramTPC = new AliTPCParamSR;
-  }
-  return paramTPC;
-
-// the older version of parameters can be accessed with this code.
-// In some cases, we have old parameters saved in the file but 
-// digits were created with new parameters, it can be distinguish 
-// by the name of TPC TreeD. The code here is just for the case 
-// we would need to compare with old data, uncomment it if needed.
-//
-//  char paramName[50];
-//  sprintf(paramName,"75x40_100x60");
-//  AliTPCParam *paramTPC=(AliTPCParam*)in->Get(paramName);
-//  if (paramTPC) {
-//    cout<<"TPC parameters "<<paramName<<" found."<<endl;
-//  } else {
-//    sprintf(paramName,"75x40_100x60_150x60");
-//    paramTPC=(AliTPCParam*)in->Get(paramName);
-//    if (paramTPC) {
-//	cout<<"TPC parameters "<<paramName<<" found."<<endl;
-//    } else {
-//	cerr<<"TPC parameters not found. Create new (they may be incorrect)."
-//	    <<endl;    
-//	paramTPC = new AliTPCParamSR;
-//    }
-//  }
-//  return paramTPC;
-
 }
 
 ////////////////////////////////////////////////////////////////////////
