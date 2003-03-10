e0fc8ff7d5f9f52e6cb1cf1692892c71fc49c041
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-03-10 15:01:52 +0000

    Corrections to obey the coding conventions

--- HBTAN/AliHBTLLWeightsPID.h
+++ HBTAN/AliHBTLLWeightsPID.h
@@ -10,45 +10,49 @@
 //
 /////////////////////////////////////////////////////////////
 
-#include "TH1.h"                                                                                  
-#include "TH2.h"                                                                                  
-#include <TF1.h>                                                                                  
-#include "TCanvas.h"                                                                              
-#include "TPad.h"                                                                                 
-
 #include <TObject.h>
-
+class TF1;
+class TH1;
 class AliHBTPair;
 class AliHBTLLWeightsPID: public TObject
  {
-   public:
-     AliHBTLLWeightsPID();
-     virtual ~AliHBTLLWeightsPID(){;}
-     static AliHBTLLWeightsPID* Instance();
-
-     Double_t GetWeightPID(const AliHBTPair* trackpair); //get weight calculated Batyunia's  algorithm
-     Float_t efficTPC1,efficTPC2,efficTOF1,efficTOF2;     
-    
-   protected:
-                                                                                             
-   static AliHBTLLWeightsPID *fWeightsPID;// pointer to wrapper of Fortran Lednicky code   
-   TH1 *ptK;   //comment?
-   TH1 *ptKefftpc;//comment?
-   TH1 *ptKefftpcboth;//comment?
-   TF1 *effic1pol;//comment?
-   TF1 *effic2pol;//comment?
-   TF1 *effic3pol;//comment?
-   TF1 *effic4pol;//comment?
+ public:
+   AliHBTLLWeightsPID();
+   AliHBTLLWeightsPID(const AliHBTLLWeightsPID &source) {
+     //Copy ctor needed by the coding conventions but not used
+     Fatal("AliHBTLLWeightsPID","copy ctor not implemented");
+   }
+   AliHBTLLWeightsPID & operator=(const AliHBTLLWeightsPID &source) {
+     //Assignment operator needed by the coding conventions but not used
+     Fatal("AliHBTLLWeightsPID","assignment operator not implemented");
+     return * this;
+   }
+   virtual ~AliHBTLLWeightsPID(){;}
+   static AliHBTLLWeightsPID* Instance();
    
-   TF1 *effic1polTOF;//comment?
-   TF1 *effic2polTOF;//comment?
-   TF1 *effic3polTOF;//comment?
-   TF1 *effic4polTOF;//comment?
-	   
-   private:
-
-   public:
-     ClassDef(AliHBTLLWeightsPID,1)
+   Double_t GetWeightPID(const AliHBTPair* trackpair); //get weight calculated Batyunia's  algorithm
+   
+ protected:
+   Float_t fEfficTPC1; // ...?
+   Float_t fEfficTPC2; // ...?
+   Float_t fEfficTOF1; // ...?
+   Float_t fEfficTOF2; // ...?
+   
+   static AliHBTLLWeightsPID *fgWeightsPID;// pointer to wrapper of Fortran Lednicky code   
+   TH1 *fPtK;   //comment?
+   TH1 *fPtKefftpc;//comment?
+   TH1 *fPtKefftpcboth;//comment?
+   TF1 *fEffic1pol;//comment?
+   TF1 *fEffic2pol;//comment?
+   TF1 *fEffic3pol;//comment?
+   TF1 *fEffic4pol;//comment?
+   
+   TF1 *fEffic1polTOF;//comment?
+   TF1 *fEffic2polTOF;//comment?
+   TF1 *fEffic3polTOF;//comment?
+   TF1 *fEffic4polTOF;//comment?
+   
+   ClassDef(AliHBTLLWeightsPID,1)
  };
 
 #endif  
