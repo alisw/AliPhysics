b121daf7800de866ffc2fe559d269bd87513fc52
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-06-10 07:45:11 +0000

    Don't call stepping.

--- TFluka/endraw.cxx
+++ TFluka/endraw.cxx
@@ -10,14 +10,13 @@ extern "C" {
 void endraw(Int_t& icode, Int_t& mreg, Double_t& rull, Double_t& xsco, Double_t& ysco, Double_t& zsco)
 {
   ((TFluka*) gMC)->SetIcode(icode);
-  ((TFluka*) gMC)->SetMreg(mreg);
+//  ((TFluka*) gMC)->SetMreg(mreg);
   ((TFluka*) gMC)->SetRull(rull);
   ((TFluka*) gMC)->SetXsco(xsco);
   ((TFluka*) gMC)->SetYsco(ysco);
   ((TFluka*) gMC)->SetZsco(zsco);
-  cout << endl << " !!! I am in endraw - calling Stepping()" << endl;
   ((TFluka*) gMC)->FutoTest();
-  (TVirtualMCApplication::Instance())->Stepping();
+//  (TVirtualMCApplication::Instance())->Stepping();
 } // end of endraw
 } // end of extern "C"
 
