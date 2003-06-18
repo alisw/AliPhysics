180744745cec250fda655f661451c9ce2b5ff146
skowron <skowron@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-06-18 19:08:52 +0000

    bug correction (unnecessary const in GetValue)

--- HBTAN/AliHBTLLWeightTheorFctn.h
+++ HBTAN/AliHBTLLWeightTheorFctn.h
@@ -2,33 +2,30 @@
 #define ALIHBTLLWEIGHTTHEORFCTN_H
 /* $Id$ */
 
-//Author: Ludmila Malinina, JINR (malinina@sunhe.jinr.ru)
+//This function allows to obtain Q_inv correlation function with weights
+//calculated by Lednicky's alghorithm.
+//Numerator is filled with weighted events. Weights are attributed to simulated particles.
+//Weights are calculated with corresponding simulated particles momenta.
+//Denominator is filled with mixing unweighted simulated particles.
+//One needs only simulated pairs, so 
+//this function is of class AliHBTOnePairFctn1D.
+//Author Ludmila Malinina JINR (malinina@sunhe.jinr.ru)
 
 #include "AliHBTFunction.h"
-
 
 class AliHBTLLWeights;
 
 class AliHBTLLWeightTheorQInvFctn: public AliHBTOnePairFctn1D
 {
-  //This function allows to obtain Q_inv correlation function with weights
-  //calculated by Lednicky's alghorithm.
-  //Numerator is filled with weighted events. Weights are attributed to simulated particles.
-  //Weights are calculated with corresponding simulated particles momenta.
-  //Denominator is filled with mixing unweighted simulated particles.
-  //One needs only simulated pairs, so 
-  //this function is of class AliHBTOnePairFctn1D.
-  //Author Ludmila Malinina JINR (malinina@sunhe.jinr.ru)
 
   public:
   AliHBTLLWeightTheorQInvFctn(Int_t nbins = 100, Double_t maxXval = 0.15, Double_t minXval = 0.0);
-  
-  virtual  ~AliHBTLLWeightTheorQInvFctn(){};
-  TH1* GetResult(); 
+  virtual  ~AliHBTLLWeightTheorQInvFctn(){}
 
+  TH1* GetResult(); 
   void   ProcessSameEventParticles(AliHBTPair* partpair);
       
-  Double_t GetValue(AliHBTPair* partpair) const
+  Double_t GetValue(AliHBTPair* partpair)
     { return partpair->GetQInv();} //isn't used
 
   ClassDef(AliHBTLLWeightTheorQInvFctn,1)
