449ef90807765c96d4c9c33c5ff595880641bdf6
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-07-13 11:07:20 +0000

    Transition to NewIO

--- PHOS/PHOSLinkDef.h
+++ PHOS/PHOSLinkDef.h
@@ -52,7 +52,6 @@
 #pragma link C++ class AliPHOSClusterizerv2+;
 #pragma link C++ class AliPHOSEvalRecPoint+;
 #pragma link C++ class AliPHOSPIDv1+;
-#pragma link C++ class AliPHOSMemoryWatcher+;
 #pragma link C++ class AliPHOSRaw2Digits+;
 #pragma link C++ class AliPHOSBeamTestEvent+;
 #pragma link C++ class AliPHOSCalibrationDB+;
