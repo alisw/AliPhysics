9a9c7c5d4c67fcf8244d5231018fc43a3ea90ce9
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-07-13 11:00:31 +0000

    Transition to NewIO

---
+++ PHOS/AliPHOSCPVBaseGeometry.cxx
@@ -0,0 +1,41 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/* 
+   $Log$
+   Revision 1.2  2001/05/31 22:46:32  schutz
+   Coding rule violation corrected
+
+   Revision 1.1  2000/11/03 16:27:47  schutz
+   CPV base geometry class to correct previous name-convetion rule violation
+
+*/
+
+//_________________________________________________________________________
+// Geometry base class  for PHOS : CPV (Charged particle veto)
+//                  
+//*-- Author   : Yuri Kharlov (IHEP, Protvino) 18 September 2000
+//
+// --- ROOT system ---
+
+// --- Standard library ---
+
+// --- AliRoot header files ---
+
+#include "AliPHOSCPVBaseGeometry.h"
+
+ClassImp(AliPHOSCPVBaseGeometry) ;
+
+//____________________________________________________________________________
