38aa69dc4726743c2adb98a7889df922bd7a1d0d
kir <kir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-07-16 14:26:49 +0000

    dimentions and degress defined

--- RICH/AliRICHConst.h
+++ RICH/AliRICHConst.h
@@ -1,12 +1,20 @@
-#ifndef RICHConst_H
-#define RICHConst_H
+#ifndef AliRICHConst_h
+#define AliRICHConst_h
+#include <TMath.h>
+const Double_t d2r=TMath::Pi()/180;
+const Double_t r2d=57.2957795130823229;
+const Double_t deg=TMath::Pi()/180;
+const Double_t rad=1;
+const Double_t mm=0.1;
+const Double_t cm=1;
+const Double_t m=100;
 /////////////////////////////////////////////////////////////////////////////
 //
 //---------------------------------------------------------------------
 //   ALICE RICH chambers geometry
 //--------------------------------------------------------------------
 //
-const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber
+const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber???????
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -15,7 +23,7 @@ const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber
 //    ALICE RICH source debugging level
 //-------------------------------------------------------------------------
 //
-//static const Int_t kRICHDebugSource = 0;
+//static const Int_t kRICHDebugSource = 0;??????????
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -27,8 +35,8 @@ const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber
 //
 
 const Float_t adc_satm  = 4096; // dynamic range (10 bits)
-const Float_t zero_supm = 6.; // zero suppression
-const Float_t sig_noise = 500.; // electronics noise (no. of electrons)
+const Float_t zero_supm = 6.; // zero suppression?????????????
+const Float_t sig_noise = 500.; // electronics noise (no. of electrons)?????????
 
 const Int_t kMaxNeighbours = 24; // max number of neighbours
 #endif
