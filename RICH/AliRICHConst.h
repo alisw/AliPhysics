7c4fba1c18b28c56df63a9aef5aae4250f207de6
kir <kir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-07-25 18:03:25 +0000

    Commented out unneeded globals

--- RICH/AliRICHConst.h
+++ RICH/AliRICHConst.h
@@ -8,35 +8,11 @@ const Double_t rad=1;
 const Double_t mm=0.1;
 const Double_t cm=1;
 const Double_t m=100;
-/////////////////////////////////////////////////////////////////////////////
-//
-//---------------------------------------------------------------------
-//   ALICE RICH chambers geometry
-//--------------------------------------------------------------------
-//
-const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber???????
 
-
-//////////////////////////////////////////////////////////////////////////////
-//
-//--------------------------------------------------------------------------
-//    ALICE RICH source debugging level
-//-------------------------------------------------------------------------
-//
-//static const Int_t kRICHDebugSource = 0;??????????
-
-
-/////////////////////////////////////////////////////////////////////////////
-//
-//---------------------------------------------------------------------
-//   ALICE RICH  Electronics Parameters
-//--------------------------------------------------------------------
-//
-//
+//const Float_t zend = 511.+0.15-2*0.001; // z-out position of first chamber???????
+//const Float_t zero_supm = 6.; // zero suppression?????????????
+//const Float_t sig_noise = 500.; // electronics noise (no. of electrons)?????????
 
 const Float_t adc_satm  = 4096; // dynamic range (10 bits)
-const Float_t zero_supm = 6.; // zero suppression?????????????
-const Float_t sig_noise = 500.; // electronics noise (no. of electrons)?????????
-
 const Int_t kMaxNeighbours = 24; // max number of neighbours
 #endif
