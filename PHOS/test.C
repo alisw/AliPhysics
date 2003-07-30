289de74a6c4a88f313d35f01af01e2f3411dc632
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-07-30 12:24:51 +0000

    Remove old test suite replaced by TesSuite.csh

--- PHOS/test.C
+++
@@ -1,6 +0,0 @@
-void test (Int_t nevent=1) 
-{
- gSystem->SetIncludePath("-I$ROOTSYS/include -I$ALICE_ROOT/include -I.");
- gROOT->ProcessLine(".x testsim.C(nevent)") ; 
- gROOT->ProcessLine(".x testsimglobal.C++") ; 
-}
