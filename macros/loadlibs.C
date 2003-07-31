57e7d77f02a78160e56d6a7a79e12a8b489ec582
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-07-31 10:13:06 +0000

    Introducing comments for Darwin

--- macros/loadlibs.C
+++ macros/loadlibs.C
@@ -1,36 +1,40 @@
 void loadlibs () 
 {
+  gSystem->Load("libPhysics");
+
+  // Uncomment the following line for Darwin
+  // Waiting for a better solution
+  // gSystem->Load("libg2c_sh");
+  gSystem->Load("libmicrocern");
+  gSystem->Load("libpdf");
+  gSystem->Load("libpythia6");
+
+  gSystem->Load("libEG");
   gSystem->Load("libGeom");
   gSystem->Load("libVMC");
 
-  // libraries required by EVGEN
-  // (commented libraries are already loaded in prevoius sequence)
-
-  gSystem->Load("libmicrocern");
-  gSystem->Load("libEG"); 
+  gSystem->Load("libEGPythia6");
   gSystem->Load("libSTEER");
   gSystem->Load("libEVGEN");
-  gSystem->Load("libEGPythia6");
-  gSystem->Load("libpdf");
-  gSystem->Load("libpythia6");
   gSystem->Load("libAliPythia6");
 
-  gSystem->Load("libPhysics");
-
-  gSystem->Load("libCONTAINERS");
-  gSystem->Load("libFMD");
-  gSystem->Load("libMUON");
+  gSystem->Load("libSTRUCT");
   gSystem->Load("libPHOS");
+  gSystem->Load("libMUON");
+  gSystem->Load("libFMD");
   gSystem->Load("libPMD");
   gSystem->Load("libRICH");
-  gSystem->Load("libSTRUCT");
+  gSystem->Load("libSTART");
+  gSystem->Load("libZDC");
+  gSystem->Load("libCRT");
+  gSystem->Load("libVZERO");
+  //  gSystem->Load("libEMCAL");
+  gSystem->Load("libCONTAINERS");
+
+  // The following lines have to be commented on Darwin
+  // for the moment due to cross dependencies
   gSystem->Load("libTPC");
   gSystem->Load("libTRD");
   gSystem->Load("libTOF");
-  gSystem->Load("libZDC");
   gSystem->Load("libITS");
-  gSystem->Load("libCRT");
-  gSystem->Load("libSTART");
-//  gSystem->Load("libEMCAL");
-  gSystem->Load("libVZERO");
 }
