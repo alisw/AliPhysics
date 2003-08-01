351d7ae1b0e4252295fa0d443cb03089374fcad0
masera <masera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-08-01 15:43:19 +0000

    macro moved to oldmacros directory. It has to be updated

--- ITS/AliITSStoreFindableTracks.C
+++
@@ -1,8 +0,0 @@
-Int_t AliITSStoreFindableTracks
-(Int_t nMinClusters = 5, const Text_t *evname = "galice", Int_t evnum = 0)
-{
-	gSystem->SetIncludePath("-I- -I$ALICE_ROOT/ITS -I$ALICE_ROOT/STEER");
-
-	gROOT->LoadMacro("$ALICE_ROOT/ITS/AliITSStoreFindableTracksCompiled.C+");
-	AliITSStoreFindableTracksCompiled(nMinClusters, evname, evnum);
-}
