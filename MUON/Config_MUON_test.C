eaaf7659f00b589306e08aec505c85cc22a92440
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-08-11 10:12:12 +0000

    Updating Config_MUON_test.C from macros/Config.C

--- MUON/Config_MUON_test.C
+++ MUON/Config_MUON_test.C
@@ -1,4 +1,3 @@
-
 // Config file test for MUON spectormeter
 // Remember to define the directory and option
 // gAlice->SetConfigFunction("Config('$HOME','box');");
@@ -7,22 +6,21 @@ void Config(char directory[100]="", char option[6]="box")
 {
   //
   // Config file for MUON test
-  // Gines MARITNEZ, Subatech, mai 2003
+  // Gines MARITNEZ, Subatech, mai 2003, august 2003
   // 
 
   //=====================================================================
   //  Libraries required by geant321
   gSystem->Load("libgeant321.so");
-
   new TGeant3("C++ Interface to Geant3");
-
   //=======================================================================
   //  Create the output file    
   Text_t filename[100];
   sprintf(filename,"%sgalice.root",directory);
   cout << ">>> Output file is " << filename << endl;   
   cout << ">>> Config_MUON_test.C: Creating Run Loader ..."<<endl;
-  AliRunLoader* rl = AliRunLoader::Open(
+  AliRunLoader* rl=0x0;
+  rl = AliRunLoader::Open(
 	filename, AliConfig::fgkDefaultEventFolderName, "recreate");
   if (rl == 0x0) {
     gAlice->Fatal("Config_MUON_test.C","Can not instatiate the Run Loader");
@@ -32,13 +30,10 @@ void Config(char directory[100]="", char option[6]="box")
   rl->SetNumberOfEventsPerFile(100);
   gAlice->SetRunLoader(rl);
 
-  //=======================================================================
-  // MonteCarlo
-  TGeant3 *geant3 = (TGeant3*)gMC;
-
+  
   //=======================================================================
   // Set External decayer
-  AliDecayer* decayer = new AliDecayerPythia();
+  TVirtualMCDecayer *decayer = new AliDecayerPythia();
   decayer->SetForceDecay(kAll);
   decayer->Init();
   gMC->SetExternalDecayer(decayer);
@@ -46,33 +41,35 @@ void Config(char directory[100]="", char option[6]="box")
   //
   //=======================================================================
   // ******* GEANT STEERING parameters FOR ALICE SIMULATION *******
-  geant3->SetTRIG(1); //Number of events to be processed 
-  geant3->SetSWIT(4,10);
-  geant3->SetDEBU(0,0,1);
-  //geant3->SetSWIT(2,2);
-  geant3->SetDCAY(1);
-  geant3->SetPAIR(1);
-  geant3->SetCOMP(1);
-  geant3->SetPHOT(1);
-  geant3->SetPFIS(0);
-  geant3->SetDRAY(0);
-  geant3->SetANNI(1);
-  geant3->SetBREM(1);
-  geant3->SetMUNU(1);
-  geant3->SetCKOV(1);
-  geant3->SetHADR(1); //Select pure GEANH (HADR 1) or GEANH/NUCRIN (HADR 3)
-  geant3->SetLOSS(2);
-  geant3->SetSTRA(0); // Simulation des collisions dans the thin layers
-  geant3->SetMULS(1);
-  geant3->SetRAYL(1);
-  geant3->SetAUTO(1); //Select automatic STMIN etc... calc. (AUTO 1) or manual (AUTO 0)
-  geant3->SetABAN(0); //Restore 3.16 behaviour for abandoned tracks
-  geant3->SetOPTI(2); //Select optimisation level for GEANT geometry searches (0,1,2)
-  geant3->SetERAN(5.e-7);
-  Float_t cut    = 1.e-3; // 1MeV cut by default
-  Float_t tofmax = 1.e10;
-  //             GAM ELEC NHAD CHAD MUON EBREM MUHAB EDEL MUDEL MUPA TOFMAX
-  geant3->SetCUTS(cut,cut, cut, cut, cut, cut,  cut,  cut, cut,  cut, tofmax);
+    gMC->SetProcess("DCAY",1);
+    gMC->SetProcess("PAIR",1);
+    gMC->SetProcess("COMP",1);
+    gMC->SetProcess("PHOT",1);
+    gMC->SetProcess("PFIS",0);
+    gMC->SetProcess("DRAY",0);
+    gMC->SetProcess("ANNI",1);
+    gMC->SetProcess("BREM",1);
+    gMC->SetProcess("MUNU",1);
+    gMC->SetProcess("CKOV",1);
+    gMC->SetProcess("HADR",1);
+    gMC->SetProcess("LOSS",2);
+    gMC->SetProcess("MULS",1);
+    gMC->SetProcess("RAYL",1);
+
+    Float_t cut = 1.e-3;        // 1MeV cut by default
+    Float_t tofmax = 1.e10;
+
+    gMC->SetCut("CUTGAM", cut);
+    gMC->SetCut("CUTELE", cut);
+    gMC->SetCut("CUTNEU", cut);
+    gMC->SetCut("CUTHAD", cut);
+    gMC->SetCut("CUTMUO", cut);
+    gMC->SetCut("BCUTE",  cut); 
+    gMC->SetCut("BCUTM",  cut); 
+    gMC->SetCut("DCUTE",  cut); 
+    gMC->SetCut("DCUTM",  cut); 
+    gMC->SetCut("PPCUTM", cut);
+    gMC->SetCut("TOFMAX", tofmax); 
   //
   //=======================================================================
   // ************* STEERING parameters FOR ALICE SIMULATION **************
@@ -124,8 +121,9 @@ void Config(char directory[100]="", char option[6]="box")
   }
      
   //=============================================================
-  //Specify maximum magnetic field in Tesla (neg. ==> default field)
-  gAlice->SetField(2,1) ;  //(-999,2);    
+  //Specify maximum magnetic field in Tesla (neg. ==> default field)   
+  AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 1., 10., 1);
+  gAlice->SetField(field); 
 
   //=================== Alice BODY parameters =============================
   AliBODY *BODY = new AliBODY("BODY","Alice envelop");
