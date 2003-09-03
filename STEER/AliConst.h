938b23b37b7471c1743abf2d495dfb846708eb12
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-03 16:07:44 +0000

    Correcting for disappearance of kPI

--- STEER/AliConst.h
+++ STEER/AliConst.h
@@ -7,8 +7,9 @@
 
 #include <TMath.h> 
   
-static const Double_t k2PI    = 2*TMath::Pi();
-static const Double_t kDegrad = TMath::Pi()/180;
-static const Double_t kRaddeg = 180/TMath::Pi();
+static const Double_t kPI      = TMath::Pi();
+static const Double_t k2PI     = TMath::TwoPi();
+static const Double_t kDegrad  = TMath::DegToRad();
+static const Double_t kRaddeg  = TMath::RadToDeg();
 
 #endif 
