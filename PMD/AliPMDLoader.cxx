394c456248a6bd94e7e49a62db0072a035137658
bnandi <bnandi@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-13 21:48:41 +0000

    PMD loader

---
+++ PMD/AliPMDLoader.cxx
@@ -0,0 +1,27 @@
+#include "AliPMDLoader.h"
+
+const TString AliPMDLoader::fgkDefaultHitsFileName      = "PMD.Hits.root";
+const TString AliPMDLoader::fgkDefaultSDigitsFileName   = "PMD.SDigits.root";
+const TString AliPMDLoader::fgkDefaultDigitsFileName    = "PMD.Digits.root";
+const TString AliPMDLoader::fgkDefaultRecPointsFileName = "PMD.RecPoints.root";
+const TString AliPMDLoader::fgkDefaultTracksFileName    = "PMD.Tracks.root";
+
+
+ClassImp(AliPMDLoader)
+AliPMDLoader::AliPMDLoader()
+ {
+ }
+/*****************************************************************************/ 
+AliPMDLoader::AliPMDLoader(const Char_t *name,const Char_t *topfoldername)
+ :AliLoader(name,topfoldername)
+{
+  if (GetDebug()) 
+   Info("AliPMDLoader"," name = %s; topfolder = %s",name,topfoldername);
+}
+/*****************************************************************************/ 
+
+AliPMDLoader::AliPMDLoader(const Char_t *name,TFolder *topfolder)
+ :AliLoader(name,topfolder)
+ {
+ }
+
