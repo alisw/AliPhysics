fe6e9f16f941850b9c361ad59802a784d799b330
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-15 15:10:07 +0000

    warning GetNtracks()

--- MUON/AliMUONData.h
+++ MUON/AliMUONData.h
@@ -54,7 +54,7 @@ class AliMUONData : public TNamed {
     TClonesArray*  RecTracks() {return fRecTracks;}
 
     void           GetTrack(Int_t it) {fLoader->TreeH()->GetEvent(it);}
-    int            GetNtracks()       {return fLoader->TreeH()->GetEntries();}
+    Int_t          GetNtracks()       {return (Int_t) fLoader->TreeH()->GetEntries();}
     void           GetCathode(Int_t ic) {fLoader->TreeD()->GetEvent(ic);}
     void           GetRawClusters() {fLoader->TreeR()->GetEvent(0);}
     void           GetTrigger() {fLoader->TreeR()->GetEvent(0);}
