6f134072423c9f6c0e1cbcabffb0bab5af7676e4
kir <kir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-17 13:43:57 +0000

    some new global consts

--- RICH/AliRICHConst.h
+++ RICH/AliRICHConst.h
@@ -1,14 +1,22 @@
 #ifndef AliRICHConst_h
 #define AliRICHConst_h
 #include <TMath.h>
-static const Double_t d2r=TMath::Pi()/180;
-static const Double_t r2d=57.2957795130823229;
-static const Double_t deg=TMath::Pi()/180;
-static const Double_t rad=1;
-static const Double_t mm=0.1;
-static const Double_t cm=1;
-static const Double_t m=100;
+const Double_t d2r=TMath::Pi()/180;
+const Double_t r2d=57.2957795130823229;
+const Double_t deg=TMath::Pi()/180;
+const Double_t rad=1;
+const Double_t cm=1;
+const Double_t mm=0.1;
+const Double_t m=100;
+const Double_t nm=1e-7;
+const Double_t fm=1e-13;
+const Double_t fermi=1e-13;
+const Int_t    kBad=-101;        //useful const to mark initial (uninitalised) values
 
-static const Float_t adc_satm  = 4096; // dynamic range (10 bits)
-static const Int_t kMaxNeighbours = 24; // max number of neighbours
+
+const int kNCH=7;                //number of RICH chambers 
+const Float_t adc_satm  = 4096;  //dynamic range (10 bits)
+const Int_t kMaxNeighbours = 24; //max number of neighbours
+const Int_t kCerenkov=50000050;  //??? go to something more general like TPDGCode
+const Int_t kFeedback=50000051;  //??? go to something more general like TPDGCode
 #endif
