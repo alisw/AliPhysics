f3b658914e2a1f88bab1a8cb7909c92e1150d382
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-18 12:46:17 +0000

    Tracker for newIO. TClonesArray of AliMUONTrack in TreeT in MUON.Tracks.root

--- MUON/MUONTracker.C
+++ MUON/MUONTracker.C
@@ -1,7 +1,26 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/* $Id$ */
+
 // Macro MUONTracker.C (TO BE COMPILED)
 // for testing the C++ reconstruction code
 // Output is using aliroot standard output MUON.Tracks.root
 // The output is a TClonesArray of AliMUONTracks.
+// Gines MARTINEZ, Subatech, sep 2003
+
 #include <TClonesArray.h>
 
 #include "AliRun.h"
@@ -78,5 +97,6 @@ void MUONTracker (Text_t *FileName = "galice.root", Int_t FirstEvent = 0, Int_t 
     MUONLoader->WriteTracks("OVERWRITE");
     muondata->ResetRecTracks();
   } // Event loop
+  MUONLoader->UnloadHits();
   MUONLoader->UnloadRecPoints();
 }
