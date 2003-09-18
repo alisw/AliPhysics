e7baa2446426f801c372e771969f41b98c39e0bc
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-18 10:32:30 +0000

    Update for station2:    Initial version    (I.Hrivnacova)

---
+++ MUON/mapping/AliMpStationType.h
@@ -0,0 +1,19 @@
+// $Id$
+// Category: sector
+//
+// Enum AliMpStationType
+// ---------------------
+// Enumeration for refering to bending and non-bending planes.
+//
+// Authors: David Guez, Ivana Hrivnacova; IPN Orsay
+ 
+#ifndef ALI_MP_STATION_TYPE_H
+#define ALI_MP_STATION_TYPE_H
+ 
+enum AliMpStationType
+{
+  kStation1, // station 1
+  kStation2, // station 2
+};
+
+#endif //ALI_MP_STATION_TYPE_H
