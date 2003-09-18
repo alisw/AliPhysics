828ff045b5ac8c44db847acf81308ef480d2f48e
mhorner <mhorner@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-09-18 16:17:11 +0000

    Stored additional energy information

--- EMCAL/AliEMCALJet.h
+++ EMCAL/AliEMCALJet.h
@@ -16,7 +16,9 @@ class AliEMCALJet : public TObject {
   virtual ~AliEMCALJet();
   void SetEnergy(Float_t val) {fEnergy = val;}
   void SetEMCALEnergy(Float_t val) {fEMCALEnergy = val;}
+  void SetEMCALEnergyBGSub(Float_t val){fEMCALEnergyBGSub = val;}
   void SetTrackEnergy(Float_t val) {fTrackEnergy = val;}
+  void SetTrackEnergyPtCut(Float_t val){fTrackEnergyPtCut = val;}
   void SetHCEnergy(Float_t val) {fHCEnergy = val;}
   void SetPhi(Float_t val)    {fPhi    = val;}  
   void SetEta(Float_t val)    {fEta    = val;}    
@@ -24,7 +26,9 @@ class AliEMCALJet : public TObject {
   void SetTrackList(Int_t val, Float_t* pt, Float_t* eta, Float_t* phi, Int_t* pdg);
   Float_t Energy()  {return fEnergy;}
   Float_t EMCALEnergy()  {return fEMCALEnergy;}
+  Float_t EMCALEnergyBGSub()  {return fEMCALEnergyBGSub;}
   Float_t TrackEnergy()  {return fTrackEnergy;}
+  Float_t TrackEnergyPtCut()  {return fTrackEnergyPtCut;}
   Float_t HCEnergy()  {return fHCEnergy;}
   Float_t Phi()     {return fPhi;}
   Float_t Eta()     {return fEta;}
@@ -33,8 +37,10 @@ class AliEMCALJet : public TObject {
   
 protected:
   Float_t  fEnergy;      // Jet Energy
-  Float_t  fEMCALEnergy; // EMCAL component of Energy inside Jet cone
-  Float_t  fTrackEnergy; // Charge tracks component of Energy inside Jet cone
+  Float_t  fEMCALEnergy; // EMCAL component of Energy inside Jet cone before BG subtraction
+  Float_t  fEMCALEnergyBGSub; // EMCAL component of Energy inside Jet cone after BG subtraction
+  Float_t  fTrackEnergy; // Charge tracks component of Energy inside Jet cone with no pT cut
+  Float_t  fTrackEnergyPtCut; // Charge tracks component of Energy inside Jet cone after pT cut
   Float_t  fHCEnergy;    // HC  component of Energy inside Jet cone
   Bool_t   fIsWeightedEnergy; // Store flag regarding energy calculation
   Float_t  fEta;      // Jet Phi
@@ -44,7 +50,7 @@ protected:
   Float_t  fEtaT[1000]; // Track eta
   Float_t  fPhiT[1000]; // Track phi
   Int_t    fPdgT[1000]; // Track pdg code
-  ClassDef(AliEMCALJet,5) // Jet for EMCAL
+  ClassDef(AliEMCALJet,6) // Jet for EMCAL
 
 } ;
 
