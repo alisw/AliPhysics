93c50cba61a2648ac79ec5426707b029ce434c46
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-10-02 14:47:55 +0000

    Dummy methods to make it work with new TVirtualMC added.

--- TRD/AliTRDsimpleMC.h
+++ TRD/AliTRDsimpleMC.h
@@ -217,6 +217,13 @@ class AliTRDsimpleMC : public TVirtualMC {
     virtual void InitLego() {}
     virtual void Gfpart(Int_t, char*, Int_t&, Float_t&, Float_t&, Float_t&) {} 
     virtual void Gspart(Int_t, const char*, Int_t, Double_t, Double_t, Double_t) {} 
+    virtual void DefineParticle(int, const char*, TMCParticleType, double, double, double);
+    virtual void DefineIon(const char*, int, int, int, double, double);
+    virtual TString  ParticleName(int) const {return "";}
+    virtual Double_t ParticleMass(int) const {return 0.;}
+    virtual Double_t ParticleCharge(int) const {return 0.;}
+    virtual Double_t ParticleLifeTime(int) const {return 0.;}
+    virtual TMCParticleType ParticleMCType(int) const {return (TMCParticleType) 0;}
 
   // Control Methods
   virtual void          Init()                                                            { };
