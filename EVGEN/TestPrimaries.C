8b816df2f63c3118abd02b217cbc28e4fd44d485
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-10-24 13:11:15 +0000

    Works with newio now.

--- EVGEN/TestPrimaries.C
+++ EVGEN/TestPrimaries.C
@@ -6,28 +6,9 @@ void TestPrimaries(Int_t evNumber1=0, Int_t evNumber2=0)
 	loadlibs();
     }
 // Connect the Root Galice file containing Geometry, Kine and Hits
-
-    TFile *file = (TFile*)gROOT->GetListOfFiles()->FindObject("galice.root");
-    
-    if (!file) {
-	printf("\n Creating galice.root \n");
-	file = new TFile("galice.root");
-    } else {
-	printf("\n galice.root found in file list");
-    }
-    file->ls();
+    AliRunLoader* rl = AliRunLoader::Open("galice.root");
 //
-    TDatabasePDG*  DataBase = new TDatabasePDG();
-    
-// Get AliRun object from file or create it if not on file
-    if (!gAlice) {
-	gAlice = (AliRun*)(file->Get("gAlice"));
-	if (gAlice) printf("AliRun object found on file\n");
-	if (!gAlice) {
-	    printf("\n create new gAlice object");
-	    gAlice = new AliRun("gAlice","Alice test program");
-	}
-    }
+    TDatabasePDG*  DataBase = TDatabasePDG::Instance();
     
 //  Create some histograms
 
@@ -41,27 +22,24 @@ void TestPrimaries(Int_t evNumber1=0, Int_t evNumber2=0)
 //
 //   Loop over events 
 //
+    rl->LoadKinematics();
+    rl->LoadHeader();    
     
     for (Int_t nev=0; nev<= evNumber2; nev++) {
-
-	Int_t nparticles = gAlice->GetEvent(nev);
+	rl->GetEvent(nev);
+	AliStack* stack = rl->Stack();
+	Int_t npart = stack->GetNprimary();
 	if (nev < evNumber1) continue;
-	if (nparticles <= 0) return;
-	
-	TObjArray*  parray = gAlice->Particles();
-	Int_t npart = (gAlice->TreeK())->GetEntries();
 //
 // Loop over primary particles (jpsi. upsilon, ...)
 //       
 	
 	for (Int_t part=0; part<npart; part++) {
-	    TParticle *MPart = gAlice->Particle(part);
+	    TParticle *MPart = stack->Particle(part);
 	    Int_t mpart  = MPart->GetPdgCode();
 	    Int_t child1 = MPart->GetFirstDaughter();
 	    Int_t child2 = MPart->GetLastDaughter();	
 	    Int_t mother = MPart->GetFirstMother();	   
-	    printf("\n  %d %d %d %d ", mpart, child1, child2, mother);
-	    
 	    Float_t Pt = MPart->Pt();
 	    Float_t E  = MPart->Energy();
 	    Float_t Pz = MPart->Pz();
@@ -74,7 +52,7 @@ void TestPrimaries(Int_t evNumber1=0, Int_t evNumber2=0)
 	    Float_t y     = 0.5*TMath::Log((E+Pz+1.e-13)/(E-Pz+1.e-13));
 	    
 	    if (child1 >= 0) continue;
-	    if (mpart == kPi0 || mpart == kGamma) continue;
+	    if (DataBase->GetParticle(mpart)->Charge() == 0) continue;
 	    Float_t wgt = 1./(Float_t ((evNumber2-evNumber1)+1.));
 	    thetaH->Fill(theta*180./TMath::Pi(),wgt);
 	    phiH->Fill(phi*180./TMath::Pi(),wgt);
@@ -92,33 +70,21 @@ void TestPrimaries(Int_t evNumber1=0, Int_t evNumber2=0)
     c1->cd(1); ptH->Draw();
     c1->cd(2); etaH->Draw();
     c1->cd(3); yH->Draw();
-    c1->cd(4
-
-
-
-); eH->Draw();
+    c1->cd(4); eH->Draw();
 
     TCanvas *c2 = new TCanvas("c2","Canvas 1",400,10,600,700);
     c2->Divide(2,2);
     c2->cd(1); phiH->Draw();
     c2->cd(2); thetaH->Draw();
     c2->cd(3); eetaH->Draw();
-    Float_t i0,i1,i2,i3,i4,i5;
-    Float_t ne = Float_t ((evNumber2-evNumber1)+1.);
-    
-    i0=eetaH->Integral(01,25)/1000./ne;
-    i1=eetaH->Integral(25,40)/1000./ne;
-    i2=eetaH->Integral(40,50)/1000./ne;
-    i3=eetaH->Integral(50,60)/1000./ne;
-    i4=eetaH->Integral(60,70)/1000./ne;
-    i5=eetaH->Integral(70,120)/1000./ne;
 
-    printf("  0 < eta < 2.5 %f \n",i0);
-    printf("2.5 < eta < 4.0 %f \n",i1);
-    printf("4.0 < eta < 5.0 %f \n",i2);
-    printf("5.0 < eta < 6.0 %f \n",i3);
-    printf("6.0 < eta < 7.0 %f \n",i4);
-    printf("7.0 < eta <12.0 %f \n",i5);
+    TFile* f = new TFile("kineH.root", "update");
+    ptH->Write();
+    etaH->Write();
+    yH->Write();
+    phiH->Write();
+    f->Close();
+    
 
 }
 
