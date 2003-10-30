414114f6143c0bc0143f676d13422fe33214b0d1
cheynis <cheynis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-10-30 15:46:02 +0000

    New coordinate system

--- VZERO/DrawVZERO.C
+++ VZERO/DrawVZERO.C
@@ -11,17 +11,12 @@ void DrawVZERO()
    gMC->SetClipBox(".");
    gMC->DefaultRange();
    
-//   Vues de la partie gauche :
-
-//   gMC->Gdraw("alic", 30, 0, 0, -25, 10, 0.2, 0.2);
-
-//   Vues de la partie droite :
+//  right part view  :
 
 //  gMC->Gdraw("alic", 0, 0, 0, 10, 10, 0.2, 0.2);
 
-
-    gMC->Gdraw("alic", 90, 0, 0, 90, 9, 0.92, 0.2 );
-  
+    gMC->Gdraw("alic", 90, 0, 0, -70, 10, 0.9, 0.2 );
+    
     gMC->Gdhead(1111, "VZERO Detector");
 
 
