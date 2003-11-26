e26a399353e6696a6f2d530ec4033c0983ba3ccf
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-11-26 18:15:07 +0000

    Extrusion of PEMC to PHOS corrected

--- PHOS/AliPHOSGeometry.cxx
+++ PHOS/AliPHOSGeometry.cxx
@@ -95,8 +95,8 @@ void AliPHOSGeometry::Init(void)
   Float_t * emcParams = fGeometryEMCA->GetEMCParams() ;
   
   fPHOSParams[0] =  TMath::Max((Double_t)fGeometryCPV->GetCPVBoxSize(0)/2., 
-			       (Double_t)(emcParams[0]*(fGeometryCPV->GetCPVBoxSize(1)+emcParams[3]) - 
-				emcParams[1]* fGeometryCPV->GetCPVBoxSize(1))/emcParams[3] ) ;
+ 			       (Double_t)(emcParams[0] - (emcParams[1]-emcParams[0])*
+					  fGeometryCPV->GetCPVBoxSize(1)/2/emcParams[3]));
   fPHOSParams[1] = emcParams[1] ;
   fPHOSParams[2] = TMath::Max((Double_t)emcParams[2], (Double_t)fGeometryCPV->GetCPVBoxSize(2)/2.);
   fPHOSParams[3] = emcParams[3] + fGeometryCPV->GetCPVBoxSize(1)/2. ;
