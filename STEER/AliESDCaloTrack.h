a145099468745097100799279343dbc2d0c6f4fc
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-01-12 11:29:47 +0000

    Temporary solution waiting for the correct implementation

--- STEER/AliESDCaloTrack.h
+++ STEER/AliESDCaloTrack.h
@@ -3,7 +3,17 @@
 
 #include "TObject.h"
 
+class AliPHOSRecParticle;
+
 class AliESDCaloTrack : public TObject {
+ public:
+  AliESDCaloTrack(AliPHOSRecParticle * part = 0x0) {
+    fPHOSRecParticle = part;
+  }
+  virtual ~AliESDCaloTrack (){}
+
+ private:
+  AliPHOSRecParticle * fPHOSRecParticle; // pointer to PHOS particle
   ClassDef(AliESDCaloTrack,1)  //ESD calorimeter track class 
 };
 
