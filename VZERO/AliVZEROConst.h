597b124113a69eeb7f24df4b88c9e4d375bd9872
cheynis <cheynis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-01-12 14:53:48 +0000

    New detector geometry

---
+++ VZERO/AliVZEROConst.h
@@ -0,0 +1,12 @@
+#ifndef ALIVZEROCONST_H
+#define ALIVZEROCONST_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. */
+
+const    Float_t kC     =  1e-11;
+const    Float_t kthau  =  2.1*1e-9;
+const    Float_t ktheta =  50.0 * kC;
+const    Float_t kQe    =  1.6e-19;
+
+#endif
+
