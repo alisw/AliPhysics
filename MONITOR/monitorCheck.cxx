9102a560c60a3bbb8c1cfc147f653232c2975653
tkuhr <tkuhr@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-01-22 17:30:01 +0000

    data consistency check monitoring added

--- MONITOR/monitorCheck.cxx
+++ MONITOR/monitorCheck.cxx
@@ -17,7 +17,8 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
-// this program performs local monitoring on a GDC by running the HLT code   //
+// this program performs local monitoring on a GDC and checks the            //
+// consistency of the data                                                   //
 //                                                                           //
 // If an argument is given, this is taken as the name of a date file which   //
 // is used instead of the local node.                                        //
@@ -34,12 +35,6 @@
 #include "AliRawReaderDate.h"
 #include "event.h"
 #include "monitor.h"
-#ifdef ALI_HLT
-#include <AliLevel3.h>
-#include <AliL3Transform.h>
-#include <AliL3MemHandler.h>
-#include <AliL3TrackArray.h>
-#endif
 #endif
 
 
@@ -68,14 +63,6 @@ int main(int argc, char** argv)
   // set ROOT in batch mode
   gROOT->SetBatch();   
 
-  // open a log file
-#ifdef ALI_HLT
-  FILE* file = fopen("monitorGDC.log", "w");
-#else
-  FILE* file = NULL;
-#endif
-  TDatime time;
-
   // get data from a file or online from this node
   Int_t status = 0;
   if (argc > 1) {
@@ -91,15 +78,8 @@ int main(int argc, char** argv)
   if (status) ::Fatal("monitorDeclareTable", monitorDecodeError(status));
 
   // declare this monitoring program to DATE
-  status = monitorDeclareMp("GDC physics monitoring");
+  status = monitorDeclareMp("data consistency check");
   if (status) ::Fatal("monitorDeclareMp", monitorDecodeError(status));
-
-#ifdef ALI_HLT
-  // initialize HLT transformations
-  if (!AliL3Transform::Init("./", kFALSE)) {
-    ::Fatal("AliL3Transform::Init", "HLT initialization failed");
-  }
-#endif
 
   // create the signal handler
   AliGDCInterruptHandler* handler = new AliGDCInterruptHandler;
@@ -119,99 +99,33 @@ int main(int argc, char** argv)
       continue;
     }
 
-#ifdef ALI_HLT
+    // check the data
     AliRawReaderDate rawReader(ptr);
-    if ((rawReader.GetAttributes()[0] & 0x02) != 0) {
-
-      // run the HLT tracker
-      AliLevel3* hlt = new AliLevel3((Char_t*)ptr);
-      hlt->Init("./", AliLevel3::kDate, 1);
-
-      hlt->SetClusterFinderParam(-1, -1, kTRUE);
-  
-      Int_t phiSegments = 50;
-      Int_t etaSegments = 100;
-      Int_t trackletlength = 3;
-      Int_t tracklength = 20;//40 or 5
-      Int_t rowscopetracklet = 2;
-      Int_t rowscopetrack = 10;
-      Double_t minPtFit = 0;
-      Double_t maxangle = 0.1745;
-      Double_t goodDist = 5;
-      Double_t maxphi = 0.1;
-      Double_t maxeta = 0.1;
-      Double_t hitChi2Cut = 15;//100 or 15
-      Double_t goodHitChi2 = 5;//20 or 5
-      Double_t trackChi2Cut = 10;//50 or 10
-      hlt->SetTrackerParam(phiSegments, etaSegments, 
-			   trackletlength, tracklength,
-			   rowscopetracklet, rowscopetrack,
-			   minPtFit, maxangle, goodDist, hitChi2Cut,
-			   goodHitChi2, trackChi2Cut, 50, maxphi, maxeta, 
-			   kTRUE);
-  
-      gSystem->Exec("rm -rf hlt");
-      gSystem->MakeDirectory("hlt");
-      hlt->WriteFiles("./hlt/");
-      hlt->ProcessEvent(0, 35, 0);
-
-      time.Set();
-      if (file) fprintf(file, "%s\n", time.AsString());
-      if (file) fprintf(file, "run: %d  event: %d %d\n", 
-			rawReader.GetRunNumber(), 
-			rawReader.GetEventId()[0], rawReader.GetEventId()[1]);
-
-      AliL3MemHandler memHandler;
-      if (!memHandler.SetBinaryInput("hlt/tracks_0.raw")) {
-	if (file) fprintf(file, "no HLT tracks\n");
-	continue;
-      }
-      AliL3TrackArray* tracks = new AliL3TrackArray;
-      memHandler.Binary2TrackArray(tracks);
-      if (file) fprintf(file, "HLT found %d tracks\n", tracks->GetNTracks());
-      delete tracks;
-      memHandler.CloseBinaryInput();
-
-      hlt->DoBench("hlt");
-      if (file) {
-	FILE* bench = fopen("hlt.dat", "r");
-	while (bench && !feof(bench)) {
-	  char buffer[256];
-	  if (!fgets(buffer, 256, bench)) break;
-	  fprintf(file, "%s", buffer);
-	}
-	fclose(bench);
-	fprintf(file, "\n");
-      }
-
-      gSystem->Exec("rm -rf hlt");
-      delete hlt;
+    Int_t errorCode = rawReader.CheckData();
+    if ((errorCode != 0) && (errorCode != AliRawReader::kErrSize)) {
+      TDatime time;
+      printf("\n%s\n", time.AsString());
+      const char* errMsg[6] = {"no error", "wrong magic word in sub event",
+			       "no mini header", 
+			       "wrong magic word in mini header",
+			       "inconsistent size in sub event and mini header",
+			       "access to data out of bounds"};
+      printf("Error: %s\n", errMsg[errorCode]);
+      printf("run: %d  event: %d %d\n", rawReader.GetRunNumber(), 
+	     rawReader.GetEventId()[0], rawReader.GetEventId()[1]);
+      printf("trigger: %08x %08x   detector: %08x\n",
+	     rawReader.GetTriggerPattern()[0], 
+	     rawReader.GetTriggerPattern()[1],
+	     rawReader.GetDetectorPattern()[0]);
+      printf("attributes: %08x %08x %08x\n", rawReader.GetAttributes()[0], 
+	     rawReader.GetAttributes()[1], rawReader.GetAttributes()[2]);
     }
 
-#else
-    // read run, event, detector, DDL numbers and data size
-    AliRawReaderDate rawReader(ptr);
-    time.Set();
-    printf("\n%s\n", time.AsString());
-    printf("run: %d  event: %d %d\n", rawReader.GetRunNumber(), 
-	   rawReader.GetEventId()[0], rawReader.GetEventId()[1]);
-    while (rawReader.ReadMiniHeader()) {
-      printf(" detector: %d   DDL: %d  size: %d\n", 
-	     rawReader.GetDetectorID(), rawReader.GetDDLID(), 
-	     rawReader.GetDataSize());
-    }
-
-#endif
-
-    gSystem->Sleep(100);   // sleep for 0.1 second
     free(ptr);
-
     gSystem->ProcessEvents();
-    if (file) fflush(file);
   }
 
   gSystem->RemoveSignalHandler(handler);
-  if (file) fclose(file);
 
   return 0;
 }
