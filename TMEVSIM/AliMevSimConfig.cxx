4d9a979b550ec619567fb4afa2f87b9e012fd3ee
skowron <skowron@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-01-23 14:16:07 +0000

    Comments from fortran source file copied as a class description

--- TMEVSIM/AliMevSimConfig.cxx
+++ TMEVSIM/AliMevSimConfig.cxx
@@ -23,6 +23,66 @@
 // --------------------------------------------
 // author: radomski@if.pw.edu.pl
 //
+//    (3) model_type - equals 1,2,3,4,5 or 6 so far.  See comments in
+//                     Function dNdpty to see what is calculated.
+//                     The models included are:
+//                   = 1, Factorized mt exponential, Gaussian rapidity model
+//                   = 2, Pratt non-expanding, spherical thermal source model
+//                   = 3, Bertsch non-expanding spherical thermal source model
+//                   = 4, Pratt spherically expanding, thermally equilibrated
+//                        source model.
+//                   = 5, Factorized pt and eta distributions input bin-by-bin.
+//                   = 6, Fully 2D pt,eta distributions input bin-by-bin.
+//                        NOTE: model_type = 1-4 are functions of (pt,y)
+//                              model_type = 5,6 are functions of (pt,eta)
+//    (4) reac_plane_cntrl - Can be either 1,2,3 or 4 where:
+//                         = 1 to ignore reaction plane and anisotropic flow,
+//                             all distributions will be azimuthally symm.
+//                         = 2 to use a fixed reaction plane angle for all
+//                             events in the run.
+//                         = 3 to assume a randomly varying reaction plane
+//                             angle for each event as determined by a
+//                             Gaussian distribution.
+//                         = 4 to assume a randomly varying reaction plane
+//                             for each event in the run as determined by
+//                             a uniform distribution from 0 to 360 deg.
+//    (5) PSIr_mean, PSIr_stdev - Reaction plane angle mean and Gaussian
+//                                std.dev. (both are in degrees) for cases
+//                                with reac_plane_cntrl = 2 (use mean value)
+//                                and 3.  Note: these are read in regardless
+//                                of the value of reac_plane_cntrl.
+//    (6) MultFac_mean, MultFac_stdev - Overall multiplicity scaling factor
+//                                      for all PID types; mean and std.dev.;
+//                                      for trigger fluctuations event-to-evt.
+//    (7) pt_cut_min,pt_cut_max - Range of transverse momentum in GeV/c.
+//    (8) eta_cut_min,eta_cut_max - Pseudorapidity range
+//    (9) phi_cut_min,phi_cut_max - Azimuthal angular range in degrees.
+//   (10) n_stdev_mult - Number of standard deviations about the mean value
+//                       of multiplicity to include in the random event-to-
+//                       event selection process.  The maximum number of
+//                       steps that can be covered is determined by
+//                       parameter n_mult_max_steps in the accompanying
+//                       include file 'Parameter_values.inc' which is
+//                       presently set at 1000, but the true upper limit for
+//                       this is n_mult_max_steps - 1 = 999.
+//   (11) n_stdev_temp - Same, except for the "Temperature" parameter.
+//   (12) n_stdev_sigma- Same, except for the rapidity width parameter.
+//   (13) n_stdev_expvel - Same, except for the expansion velocity parameter.
+//   (14) n_stdev_PSIr   - Same, except for the reaction plane angle
+//   (15) n_stdev_Vn     - Same, except for the anisotropy coefficients, Vn.
+//   (16) n_stdev_MultFac - Same, except for the multiplicity scaling factor.
+//   (17) n_integ_pts - Number of mesh points to use in the random model
+//                      parameter selection process.  The upper limit is
+//                      set by parameter nmax_integ in the accompanying
+//                      include file 'Parameter_values.inc' which is presently
+//                      set at 100, but the true upper limit for n_integ_pts
+//                      is nmax_integ - 1 = 99.
+//   (18) n_scan_pts  - Number of mesh points to use to scan the (pt,y)
+//                      dependence of the model distributions looking for
+//                      the maximum value.  The 2-D grid has
+//                      n_scan_pts * n_scan_pts points; no limit to size of
+//                      n_scan_pts.
+//
 ////////////////////////////////////////////////////////////////////
 
 #include "AliMevSimConfig.h"
--- TMEVSIM/AliMevSimConfig.h
+++ TMEVSIM/AliMevSimConfig.h
@@ -17,6 +17,68 @@
 // --------------------------------------------
 // author: radomski@if.pw.edu.pl
 //
+//Comments taken from MevSim fortran source file (by Lanny Ray):
+//
+//    (3) model_type - equals 1,2,3,4,5 or 6 so far.  See comments in
+//                     Function dNdpty to see what is calculated.
+//                     The models included are:
+//                   = 1, Factorized mt exponential, Gaussian rapidity model
+//                   = 2, Pratt non-expanding, spherical thermal source model
+//                   = 3, Bertsch non-expanding spherical thermal source model
+//                   = 4, Pratt spherically expanding, thermally equilibrated
+//                        source model.
+//                   = 5, Factorized pt and eta distributions input bin-by-bin.
+//                   = 6, Fully 2D pt,eta distributions input bin-by-bin.
+//                        NOTE: model_type = 1-4 are functions of (pt,y)
+//                              model_type = 5,6 are functions of (pt,eta)
+//    (4) reac_plane_cntrl - Can be either 1,2,3 or 4 where:
+//                         = 1 to ignore reaction plane and anisotropic flow,
+//                             all distributions will be azimuthally symm.
+//                         = 2 to use a fixed reaction plane angle for all
+//                             events in the run.
+//                         = 3 to assume a randomly varying reaction plane
+//                             angle for each event as determined by a
+//                             Gaussian distribution.
+//                         = 4 to assume a randomly varying reaction plane
+//                             for each event in the run as determined by
+//                             a uniform distribution from 0 to 360 deg.
+//    (5) PSIr_mean, PSIr_stdev - Reaction plane angle mean and Gaussian
+//                                std.dev. (both are in degrees) for cases
+//                                with reac_plane_cntrl = 2 (use mean value)
+//                                and 3.  Note: these are read in regardless
+//                                of the value of reac_plane_cntrl.
+//    (6) MultFac_mean, MultFac_stdev - Overall multiplicity scaling factor
+//                                      for all PID types; mean and std.dev.;
+//                                      for trigger fluctuations event-to-evt.
+//    (7) pt_cut_min,pt_cut_max - Range of transverse momentum in GeV/c.
+//    (8) eta_cut_min,eta_cut_max - Pseudorapidity range
+//    (9) phi_cut_min,phi_cut_max - Azimuthal angular range in degrees.
+//   (10) n_stdev_mult - Number of standard deviations about the mean value
+//                       of multiplicity to include in the random event-to-
+//                       event selection process.  The maximum number of
+//                       steps that can be covered is determined by
+//                       parameter n_mult_max_steps in the accompanying
+//                       include file 'Parameter_values.inc' which is
+//                       presently set at 1000, but the true upper limit for
+//                       this is n_mult_max_steps - 1 = 999.
+//   (11) n_stdev_temp - Same, except for the "Temperature" parameter.
+//   (12) n_stdev_sigma- Same, except for the rapidity width parameter.
+//   (13) n_stdev_expvel - Same, except for the expansion velocity parameter.
+//   (14) n_stdev_PSIr   - Same, except for the reaction plane angle
+//   (15) n_stdev_Vn     - Same, except for the anisotropy coefficients, Vn.
+//   (16) n_stdev_MultFac - Same, except for the multiplicity scaling factor.
+//   (17) n_integ_pts - Number of mesh points to use in the random model
+//                      parameter selection process.  The upper limit is
+//                      set by parameter nmax_integ in the accompanying
+//                      include file 'Parameter_values.inc' which is presently
+//                      set at 100, but the true upper limit for n_integ_pts
+//                      is nmax_integ - 1 = 99.
+//   (18) n_scan_pts  - Number of mesh points to use to scan the (pt,y)
+//                      dependence of the model distributions looking for
+//                      the maximum value.  The 2-D grid has
+//                      n_scan_pts * n_scan_pts points; no limit to size of
+//                      n_scan_pts.
+//
 ////////////////////////////////////////////////////////////////////
 
 #include "TObject.h"
@@ -39,16 +101,16 @@ class AliMevSimConfig : public TObject {
   Float_t fMultFacMean;//fMultFacMean Mean multiplicity
   Float_t fMultFacStDev;//fMultFacStDev multiplicity variance
 
-  Float_t fNStDevMult;//fNStDevMult
-  Float_t fNStDevTemp;//fNStDevTemp
-  Float_t fNStDevSigma;//fNStDevSigma
-  Float_t fNStDevExpVel;//fNStDevExpVel
-  Float_t fNStdDevPSIr;//fNStdDevPSIr
-  Float_t fNStDevVn;//fNStDevVn
-  Float_t fNStDevMultFac;//fNStDevMultFac
+  Float_t fNStDevMult;//see (10) n_stdev_mult
+  Float_t fNStDevTemp;//(11) n_stdev_temp
+  Float_t fNStDevSigma;//see (12) n_stdev_sigma
+  Float_t fNStDevExpVel;//see (13) n_stdev_expvel
+  Float_t fNStdDevPSIr;//see (14) n_stdev_PSIr
+  Float_t fNStDevVn;//see (15) n_stdev_Vn
+  Float_t fNStDevMultFac;//see (16) n_stdev_MultFac
 
-  Int_t fNIntegPts;//fNIntegPts
-  Int_t fNScanPts;//fNScanPts
+  Int_t fNIntegPts;//see (17) n_integ_pts
+  Int_t fNScanPts;//see (18) n_scan_pts
 
   void Init();
 
