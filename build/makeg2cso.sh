9018512db275ee1d928bb5f14c0c8d8eb34f0b4b
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-03-11 12:39:57 +0000

    Update for Mac OS X 10.3

--- build/makeg2cso.sh
+++ build/makeg2cso.sh
@@ -2,9 +2,10 @@
 out=$ALICE_ROOT/lib/tgt_$ALICE_TARGET/libg2c_sh
 cd /tmp
 rm -rf g2c ; mkdir g2c ; cd g2c
-ar -x /usr/lib/gcc/darwin/default/libg2c.a
-export MACOSX_DEPLOYMENT_TARGET=10.2
+ar -x /sw/lib/libg2c.a
+export MACOSX_DEPLOYMENT_TARGET=10.3
+unset LD_PREBIND
 rm -f ${out}.so ${out}.dylib
-g++ -bundle -flat_namespace -undefined suppress -o ${out}.so `ls *.o | grep -v buggy`
-g++ -dynamiclib -flat_namespace -undefined suppress -single_module -o ${out}.dylib `ls *.o | grep -v buggy`
+g++ -bundle -flat_namespace -undefined dynamic_lookup -o ${out}.so `ls *.o | grep -v buggy`
+g++ -dynamiclib -flat_namespace -undefined dynamic_lookup -single_module -o ${out}.dylib `ls *.o | grep -v buggy`
 
