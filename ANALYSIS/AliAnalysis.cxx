a3e0f1b0ea91a9730d12ea4f977bef67bb9333cf
skowron <skowron@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-04-01 09:00:47 +0000

    Compiler warning removed

--- ANALYSIS/AliAnalysis.cxx
+++ ANALYSIS/AliAnalysis.cxx
@@ -18,17 +18,20 @@ Int_t AliAnalysis::fgkDebug = 0;
 
 AliAnalysis::AliAnalysis()
 {
-
+ //ctor
 }
 /*********************************************************/
 
-AliAnalysis::AliAnalysis(const char* name,const char* title)
+AliAnalysis::AliAnalysis(const char* name,const char* title):
+ TTask(name,title)
 {
+ //ctor
 }
 /*********************************************************/
 
 AliAnalysis::~AliAnalysis()
 {
+ //dtor
 }
 /*********************************************************/
 
