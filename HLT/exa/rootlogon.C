40d33d50fc84fd2df89ceb542b76fa5a72832d7d
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-07-05 08:48:00 +0000

    Changes to get it work with HEAD and v4-01-Release. Removed older env variables used in HLT standalone.

--- HLT/exa/rootlogon.C
+++ HLT/exa/rootlogon.C
@@ -12,64 +12,38 @@
     gSystem->Load("$(ROOTSYS)/lib/libGeom");
     gSystem->Load("$(ROOTSYS)/lib/libVMC");
   }
-  gErrorIgnoreLevel=saveErrIgLevel;
 
   if(1)
     {
-      if(getenv("ALICE_TARGET")) {
-        gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTEER");
-        gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libCONTAINERS");
-        gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTPC");
-      } else {
-        gSystem->Load("$(ALICE_ROOT)/lib/libSTEER");
-        gSystem->Load("$(ALICE_ROOT)/lib/libCONTAINERS");
-        gSystem->Load("$(ALICE_ROOT)/lib/libTPC");
+      gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTEER");
+      gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libCONTAINERS");
+      if(gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTPC")!=0){
+	gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTCPbase");
+	gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTCPrec");
+	gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTCPsim");
+	gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTCPfast");
       }
       cout<<"TPC libraries loaded"<<endl;
     }
 
   if(1)
     {
-      if(getenv("ALIHLT_MLUCDIR")) {
-        if(strcmp("false",getenv("ALIHLT_NOLOGGING"))==0) gSystem->Load("$(ALIHLT_MLUCDIR/lib/libMLUC");
-        gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Src");
-        gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Misc");
-        gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Hough");
-        gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Comp");
-      } else {
-        if(strcmp("false",getenv("ALIHLT_NOLOGGING"))==0) gSystem->Load("$(ALIHLT_BASEDIR)/kip/MLUC/lib/linux-i386/libMLUC.so");
-        gSystem->Load("$(ALIHLT_BASEDIR)/lib_$(USER)/libAliL3Src");
-        gSystem->Load("$(ALIHLT_BASEDIR)/lib_$(USER)/libAliL3Misc");
-        gSystem->Load("$(ALIHLT_BASEDIR)/lib_$(USER)/libAliL3Hough");
-        gSystem->Load("$(ALIHLT_BASEDIR)/lib_$(USER)/libAliL3Comp");
-      }
+      gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Src");
+      gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Misc");
+      gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Hough");
+      gSystem->Load("$(ALIHLT_LIBDIR)/libAliL3Comp");
       cout<<"HLT libraries loaded"<<endl;
-
-      if(strcmp("false",getenv("ALIHLT_NOLOGGING"))==0){
-	AliL3Logger gLogger;
-	gLogger.UseStream();
-      }
-
-      if(getenv("ALIHLT_TRANSFORMFILE")){
-	cout << "Loading config \"" << getenv("ALIHLT_TRANSFORMFILE") << "\": " << flush;
-	if(AliL3Transform::Init(getenv("ALIHLT_TRANSFORMFILE")))
-	  cout << "Ok!" << endl;
-	else cout << "Failed!" << endl;
-      }
-
-      Int_t saveErrIgLevel=gErrorIgnoreLevel;
-      gErrorIgnoreLevel=kFatal; //dont report errors
-      gSystem->Load("MakePileup_C.so");
-      gSystem->Load("Read_C.so");
-      gSystem->Load("runhough_C.so");
-      gSystem->Load("runrowhough_C.so");
-      gSystem->Load("deconvclusters_C.so");
-      gSystem->Load("runtracker_C.so");
-      gErrorIgnoreLevel=saveErrIgLevel;
-
-      if(strcmp("true",getenv("ALIHLT_DOMC"))==0) gSystem->SetIncludePath(" -Ddo_mc");
-      gSystem->SetIncludePath(" -I$ALIHLT_TOPDIR/hough -I$ALIHLT_TOPDIR/src -I$ALIHLT_TOPDIR/comp -I$ALIHLT_TOPDIR/misc -I$ALICE_ROOT/include/ -I$ALICE_ROOT/TPC -I$ALICE_ROOT/CONTAINERS -I$ALICE_ROOT/STEER ");
     }
+
+  gSystem->Load("MakePileup_C.so");
+  gSystem->Load("Read_C.so");
+  gSystem->Load("runhough_C.so");
+  gSystem->Load("runrowhough_C.so");
+  gSystem->Load("deconvclusters_C.so");
+  gSystem->Load("runtracker_C.so");
+  gErrorIgnoreLevel=saveErrIgLevel;
+
+  gSystem->SetIncludePath(" -D do_mc -I$ALIHLT_TOPDIR/hough -I$ALIHLT_TOPDIR/src -I$ALIHLT_TOPDIR/comp -I$ALIHLT_TOPDIR/misc -I$ALICE_ROOT/include/ -I$ALICE_ROOT/TPC -I$ALICE_ROOT/CONTAINERS -I$ALICE_ROOT/STEER ");
 
   gStyle->SetStatColor(10);
   gStyle->SetStatBorderSize(1);
