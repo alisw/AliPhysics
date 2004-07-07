de086e18a57bbf80edd5aa79a86f9eacf60ecb48
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-07-07 16:22:45 +0000

    Only with TGeo now.

--- TFluka/scripts/runflukageo.sh
+++ TFluka/scripts/runflukageo.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 CURDIR=`pwd`
 cd $ALICE_ROOT
-if [ -f $ALICE_ROOT/lib/tgt_$ALICE_TARGET/libTFluka.so ]; then
-   COMPROOT=`nm -s $ALICE_ROOT/lib/tgt_$ALICE_TARGET/libTFluka.so | grep gGeoManager`
-   if [ -z "${COMPROOT}" ]; then
-      echo 'TFluka was compiled with FLUGG -> Recompilation...'
-      make clean-TFluka
-   else
-      echo 'TFluka was compiled with TGeo -> OK'
-   fi      
-fi   
-export WITH_ROOT=1
 echo 'Making sure that TFluka is up to date...'
 make all-TFluka
 cd $CURDIR
@@ -19,6 +9,7 @@ rm -rf tmp
 # Make a new temporary directory and move to it
 mkdir tmp
 cd tmp
+mkdir peg
 
 # Link here some special Fluka files needed
 ln -s $FLUPRO/xnloan.dat .
