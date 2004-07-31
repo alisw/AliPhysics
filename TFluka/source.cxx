a35ca026e42bacfaf9c110ee20fab4adbe38aac5
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-07-31 13:00:25 +0000

    Use OPPHST for optical photons.

--- TFluka/source.cxx
+++ TFluka/source.cxx
@@ -10,6 +10,7 @@
 #include "Fbeam.h"    //(BEAM)   fluka common
 #include "Fpaprop.h"  //(PAPROP) fluka common
 #include "Fltclcm.h"  //(LTCLCM) fluka common
+#include "Fopphst.h"  //(OPPHST) fluka common
 //#include "Fcaslim.h"  //(CASLIM) fluka common
 
 //Virutal MC
@@ -153,132 +154,176 @@ extern "C" {
     /* Lstack is the stack counter: of course any time source is called it
      * must be =0
      */
-    
-    STACK.lstack++;
-
-    /* Wt is the weight of the particle*/
-    STACK.wt[STACK.lstack] = oneone;
-    STARS.weipri += STACK.wt[STACK.lstack];
-
-    /* Particle type (1=proton.....). Ijbeam is the type set by the BEAM
-     * card
-       */
-
-    //STACK.ilo[STACK.lstack] = BEAM.ijbeam;
-    if (pdg == 50000050 ||  pdg ==  50000051) {
-	STACK.ilo[STACK.lstack] = fluka-> IdFromPDG(22);
-    } else {
-	STACK.ilo[STACK.lstack] = fluka-> IdFromPDG(pdg);
-    }
-    
-    
-	    
-
-    /* From this point .....
-     * Particle generation (1 for primaries)
-    */
-    STACK.lo[STACK.lstack] = 1;
-
-    /* User dependent flag:*/
-    STACK.louse[STACK.lstack] = 0;
-
-    /* User dependent spare variables:*/
-    Int_t ispr = 0;
-    for (ispr = 0; ispr < mkbmx1; ispr++)
-      STACK.sparek[STACK.lstack][ispr] = zerzer;
-
-    /* User dependent spare flags:*/
-    for (ispr = 0; ispr < mkbmx2; ispr++)
-	STACK.ispark[STACK.lstack][ispr] = 0;
-
-    /* Save the track number of the stack particle:*/
-    STACK.ispark[STACK.lstack][mkbmx2-1] = itrack;
-    STACK.nparma++;
-    STACK.numpar[STACK.lstack] = STACK.nparma;
-    STACK.nevent[STACK.lstack] = 0;
-    STACK.dfnear[STACK.lstack] = +zerzer;
-    
-    /* Particle age (s)*/
-    STACK.agestk[STACK.lstack] = +zerzer;
-    STACK.aknshr[STACK.lstack] = -twotwo;
-
-    /* Group number for "low" energy neutrons, set to 0 anyway*/
-    STACK.igroup[STACK.lstack] = 0;
-    
-    /* Kinetic energy */
-    if (pdg == 50000050 ||  pdg ==  50000051) {
-	// 
-	// Special case for optical photons
-	STACK.tke[STACK.lstack] = particle->Energy();
-    } else {
-	STACK.tke[STACK.lstack] = particle->Energy() - particle->GetMass();
-    }
-    
-    
-    /* Particle momentum*/
-    STACK.pmom [STACK.lstack] = particle->P();
-    
     /* Cosines (tx,ty,tz)*/
     Double_t cosx = particle->Px()/particle->P();
     Double_t cosy = particle->Py()/particle->P();
     Double_t cosz = TMath::Sqrt(oneone - cosx*cosx - cosy*cosy);
-    if (particle->Pz() < 0.) cosz = -cosz;
-    STACK.tx [STACK.lstack] = cosx;
-    STACK.ty [STACK.lstack] = cosy;
-    STACK.tz [STACK.lstack] = cosz;
-    
-    /* Polarization cosines:*/
-    if (polarisation.Mag()) {
-	Double_t cospolx = polarisation.Px()/polarisation.Mag();
-	Double_t cospoly = polarisation.Py()/polarisation.Mag();
-	Double_t cospolz = sqrt(oneone - cospolx*cospolx - cospoly*cospoly);
-	STACK.tx [STACK.lstack] = cospolx;
-	STACK.ty [STACK.lstack] = cospoly;
-	STACK.tz [STACK.lstack] = cospolz;
-    }
-    else {
-	STACK.txpol [STACK.lstack] = -twotwo;
-	STACK.typol [STACK.lstack] = +zerzer;
-	STACK.tzpol [STACK.lstack] = +zerzer;
-    }
-    
-    /* Particle coordinates*/
-    // Vertext coordinates;
-    STACK.xa [STACK.lstack] = particle->Vx();
-    STACK.ya [STACK.lstack] = particle->Vy();
-    STACK.za [STACK.lstack] = particle->Vz();
-    
-    /*  Calculate the total kinetic energy of the primaries: don't change*/
-    Int_t st_ilo =  STACK.ilo[STACK.lstack];
-    if ( st_ilo != 0 )
-	EPISOR.tkesum += 
-	    ((STACK.tke[STACK.lstack] + PAPROP.amdisc[st_ilo+6])
-	     * STACK.wt[STACK.lstack]);
-    else
-	EPISOR.tkesum += (STACK.tke[STACK.lstack] * STACK.wt[STACK.lstack]);
-    
-    /*  Here we ask for the region number of the hitting point.
-     *     NREG (LSTACK) = ...
-     *  The following line makes the starting region search much more
-     *  robust if particles are starting very close to a boundary:
-     */
-    geocrs( STACK.tx[STACK.lstack], 
-	    STACK.ty[STACK.lstack], 
-	    STACK.tz[STACK.lstack] );
-    
-    Int_t idisc;
+    if (particle->Pz() < 0.) cosz = -cosz;    
 
-    georeg ( STACK.xa[STACK.lstack], 
-	     STACK.ya[STACK.lstack], 
-	     STACK.za[STACK.lstack],
-	     STACK.nreg[STACK.lstack], 
-	     idisc);//<-- dummy return variable not used
-    /*  Do not change these cards:*/
-    Int_t igeohsm1 = 1;
-    Int_t igeohsm2 = -11;
-    geohsm ( STACK.nhspnt[STACK.lstack], igeohsm1, igeohsm2, LTCLCM.mlattc );
-    STACK.nlattc[STACK.lstack] = LTCLCM.mlattc;
-    soevsv();
+    //STACK.ilo[STACK.lstack] = BEAM.ijbeam;
+    if (pdg != 50000050 &&  pdg !=  50000051) {
+	STACK.lstack++;
+
+	STACK.ilo[STACK.lstack] = fluka-> IdFromPDG(pdg);
+	
+	/* Wt is the weight of the particle*/
+	STACK.wt[STACK.lstack] = oneone;
+	STARS.weipri += STACK.wt[STACK.lstack];
+	
+	STACK.lo[STACK.lstack] = 1;
+	
+	/* User dependent flag:*/
+	STACK.louse[STACK.lstack] = 0;
+
+	/* User dependent spare variables:*/
+	Int_t ispr = 0;
+	for (ispr = 0; ispr < mkbmx1; ispr++)
+	    STACK.sparek[STACK.lstack][ispr] = zerzer;
+
+	/* User dependent spare flags:*/
+	for (ispr = 0; ispr < mkbmx2; ispr++)
+	    STACK.ispark[STACK.lstack][ispr] = 0;
+
+	/* Save the track number of the stack particle:*/
+	STACK.ispark[STACK.lstack][mkbmx2-1] = itrack;
+	STACK.nparma++;
+	STACK.numpar[STACK.lstack] = STACK.nparma;
+	STACK.nevent[STACK.lstack] = 0;
+	STACK.dfnear[STACK.lstack] = +zerzer;
+    
+	/* Particle age (s)*/
+	STACK.agestk[STACK.lstack] = +zerzer;
+	STACK.aknshr[STACK.lstack] = -twotwo;
+
+	/* Group number for "low" energy neutrons, set to 0 anyway*/
+	STACK.igroup[STACK.lstack] = 0;
+	
+	/* Kinetic energy */
+	STACK.tke[STACK.lstack] = particle->Energy() - particle->GetMass();
+	/* Particle momentum*/
+	STACK.pmom [STACK.lstack] = particle->P();
+    
+
+	STACK.tx [STACK.lstack] = cosx;
+	STACK.ty [STACK.lstack] = cosy;
+	STACK.tz [STACK.lstack] = cosz;
+    
+	/* Polarization cosines:*/
+	if (polarisation.Mag()) {
+	    Double_t cospolx = polarisation.Px() / polarisation.Mag();
+	    Double_t cospoly = polarisation.Py() / polarisation.Mag();
+	    Double_t cospolz = sqrt(oneone - cospolx * cospolx - cospoly * cospoly);
+	    STACK.txpol [STACK.lstack] = cospolx;
+	    STACK.typol [STACK.lstack] = cospoly;
+	    STACK.tzpol [STACK.lstack] = cospolz;
+	}
+	else {
+	    STACK.txpol [STACK.lstack] = -twotwo;
+	    STACK.typol [STACK.lstack] = +zerzer;
+	    STACK.tzpol [STACK.lstack] = +zerzer;
+	}
+	
+	/* Particle coordinates*/
+	// Vertext coordinates;
+	STACK.xa [STACK.lstack] = particle->Vx();
+	STACK.ya [STACK.lstack] = particle->Vy();
+	STACK.za [STACK.lstack] = particle->Vz();
+    
+	/*  Calculate the total kinetic energy of the primaries: don't change*/
+	Int_t st_ilo =  STACK.ilo[STACK.lstack];
+	if ( st_ilo != 0 )
+	    EPISOR.tkesum += 
+		((STACK.tke[STACK.lstack] + PAPROP.amdisc[st_ilo+6])
+		 * STACK.wt[STACK.lstack]);
+	else
+	    EPISOR.tkesum += (STACK.tke[STACK.lstack] * STACK.wt[STACK.lstack]);
+	
+	/*  Here we ask for the region number of the hitting point.
+	 *     NREG (LSTACK) = ...
+	 *  The following line makes the starting region search much more
+	 *  robust if particles are starting very close to a boundary:
+	 */
+	geocrs( STACK.tx[STACK.lstack], 
+		STACK.ty[STACK.lstack], 
+		STACK.tz[STACK.lstack] );
+    
+	Int_t idisc;
+
+	georeg ( STACK.xa[STACK.lstack], 
+		 STACK.ya[STACK.lstack], 
+		 STACK.za[STACK.lstack],
+		 STACK.nreg[STACK.lstack], 
+		 idisc);//<-- dummy return variable not used
+	/*  Do not change these cards:*/
+	Int_t igeohsm1 = 1;
+	Int_t igeohsm2 = -11;
+	geohsm ( STACK.nhspnt[STACK.lstack], igeohsm1, igeohsm2, LTCLCM.mlattc );
+	STACK.nlattc[STACK.lstack] = LTCLCM.mlattc;
+	soevsv();
+    } else {
+	// Next particle is optical photon
+	OPPHST.lstopp++;
+	OPPHST.donear [OPPHST.lstopp - 1] = 0.;
+
+	OPPHST.xoptph [OPPHST.lstopp - 1] = particle->Vx();
+	OPPHST.yoptph [OPPHST.lstopp - 1] = particle->Vy();
+	OPPHST.zoptph [OPPHST.lstopp - 1] = particle->Vz();
+
+	OPPHST.txopph [OPPHST.lstopp - 1] = cosx;
+	OPPHST.tyopph [OPPHST.lstopp - 1] = cosy;
+	OPPHST.tzopph [OPPHST.lstopp - 1] = cosz;
+
+
+	if (polarisation.Mag()) {
+	    Double_t cospolx = polarisation.Px() / polarisation.Mag();
+	    Double_t cospoly = polarisation.Py() / polarisation.Mag();
+	    Double_t cospolz = sqrt(oneone - cospolx * cospolx - cospoly * cospoly);
+	    OPPHST.txpopp [OPPHST.lstopp - 1] = cospolx;
+	    OPPHST.typopp [OPPHST.lstopp - 1] = cospoly;
+	    OPPHST.tzpopp [OPPHST.lstopp - 1] = cospolz;
+	}
+	else {
+	    OPPHST.txpopp [OPPHST.lstopp - 1] = -twotwo;
+	    OPPHST.typopp [OPPHST.lstopp - 1] = +zerzer;
+	    OPPHST.tzpopp [OPPHST.lstopp - 1] = +zerzer;
+	}
+	
+	geocrs( OPPHST.txopph[OPPHST.lstopp - 1], 
+		OPPHST.tyopph[OPPHST.lstopp - 1], 
+		OPPHST.tzopph[OPPHST.lstopp - 1] );
+    
+	Int_t idisc;
+
+	georeg ( OPPHST.xoptph[OPPHST.lstopp - 1], 
+		 OPPHST.yoptph[OPPHST.lstopp - 1], 
+		 OPPHST.zoptph[OPPHST.lstopp - 1],
+		 OPPHST.nregop[OPPHST.lstopp - 1], 
+		 idisc);//<-- dummy return variable not used
+	
+	OPPHST.wtopph [OPPHST.lstopp - 1] = particle->GetWeight();
+	OPPHST.poptph [OPPHST.lstopp - 1] = particle->P();
+	OPPHST.agopph [OPPHST.lstopp - 1] = particle->T();	
+	OPPHST.cmpopp [OPPHST.lstopp - 1] = 0.;
+	OPPHST.loopph [OPPHST.lstopp - 1] = 0;
+//	Int_t mother = particle->GetFirstMother();
+	OPPHST.louopp [OPPHST.lstopp - 1] = itrack;
+/*
+	if (mother > -1) {
+	    TParticle* mparticle = cppstack->Particle(mother);
+	    OPPHST.apropp [OPPHST.lstopp - 1] = mparticle->T();
+	    Int_t mpdg = mparticle->GetPdgCode();
+	    if (mpdg == 50000050 || mpdg 50000051) {
+		OPPHST.ipropp [OPPHST.lstopp - 1] = -1;	    
+		OPPHST.tpropp [OPPHST.lstopp - 1] = mparticle->Energy();
+	    } else {
+		OPPHST.ipropp [OPPHST.lstopp - 1] = mpdg;
+		OPPHST.tpropp [OPPHST.lstopp - 1] = mparticle->Energy() - mparticle->GetMass();
+	    }
+
+	}
+*/
+    }
+    
 //
 //  Pre-track actions at for primary tracks
 //
