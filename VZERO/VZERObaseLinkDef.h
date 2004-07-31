b2cd7725afcefb7966f197b565414b6f547ab743
cheynis <cheynis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-07-31 18:00:39 +0000

    Splitting of libraries

---
+++ VZERO/VZERObaseLinkDef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class  AliVZEROLoader+;
+#pragma link C++ class  AliVZEROdigit+;
+
+#endif
---
+++ VZERO/VZEROsimLinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class  AliVZERO+;
+#pragma link C++ class  AliVZEROv2+;
+#pragma link C++ class  AliVZEROv3+;
+#pragma link C++ class  AliVZEROv4+;
+#pragma link C++ class  AliVZEROv5+;
+#pragma link C++ class  AliVZEROhit+;
+#pragma link C++ class  AliVZERODigitizer+;
+#pragma link C++ class  AliVZEROBuffer+;
+
+#endif
