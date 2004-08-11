eecf3a071cf899884ebe5f8057f4535823107411
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-08-11 12:59:07 +0000

    Standard header added.

--- TFluka/TFlukaCerenkov.cxx
+++ TFluka/TFlukaCerenkov.cxx
@@ -1,3 +1,32 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/* $Id$*/
+
+//
+// Stores user defined cerenkov properties of media like 
+// absorption coefficient, refraction index and quantum efficiency. 
+// The properties are stored in arrays. The array index corresponds to discrete 
+// optical photon energies defined in fEnergy.
+// Access to the properties is provided by interpolation.
+// 
+// Author:
+// A. Morsch 
+// andreas.morsch@cern.ch
+//
+
 #include "TFlukaCerenkov.h"
 
 Double_t TFlukaCerenkov::fgGlobalMaximumEfficiency = 0.;
