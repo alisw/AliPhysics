2e43f4a8ae4c18ff01033b04789d29c4fad676f0
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-08-25 15:27:59 +0000

    cosmetics

--- PHOS/AliPHOSRawStream.h
+++ PHOS/AliPHOSRawStream.h
@@ -24,17 +24,17 @@ public :
   
   AliPHOSRawStream(AliRawReader* rawReader);
   
-  Int_t            GetColumn() const {return fPad;};
-  Int_t            GetModule() const {return fSector;};
-  Int_t            GetPrevColumn() const {return fPrevPad;};
-  Int_t            GetPrevModule() const {return fPrevSector;};
-  Int_t            GetPrevRow() const {return fPrevRow;};
-  Int_t            GetRow() const {return fRow;};
-  Int_t            GetSignal() const {return fSignal;};
-  Int_t            GetTime() const {return fTime;};
-  Bool_t           IsNewColumn() const {return (GetColumn() != GetPrevColumn()) || IsNewRow();};
-  Bool_t           IsNewModule() const {return GetModule() != GetPrevModule();};
-  Bool_t           IsNewRow() const {return (GetRow() != GetPrevRow()) || IsNewModule();};
+  Int_t            GetColumn() const {return fPad;}
+  Int_t            GetModule() const {return fSector;}
+  Int_t            GetPrevColumn() const {return fPrevPad;}
+  Int_t            GetPrevModule() const {return fPrevSector;}
+  Int_t            GetPrevRow() const {return fPrevRow;}
+  Int_t            GetRow() const {return fRow;}
+  Int_t            GetSignal() const {return fSignal;}
+  Int_t            GetTime() const {return fTime;}
+  Bool_t           IsNewColumn() const {return (GetColumn() != GetPrevColumn()) || IsNewRow();}
+  Bool_t           IsNewModule() const {return GetModule() != GetPrevModule();}
+  Bool_t           IsNewRow() const {return (GetRow() != GetPrevRow()) || IsNewModule();}
   
   ClassDef(AliPHOSRawStream, 0)   // class for reading PHOS raw digits
     };
