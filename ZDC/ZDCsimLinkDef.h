bd9774eed93e38fc1b1aef3519507ccdf11a34ab
coppedis <coppedis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-10-22 10:42:12 +0000

    Splitted libraries

---
+++ ZDC/ZDCsimLinkDef.h
@@ -0,0 +1,24 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class  AliZDC+;
+#pragma link C++ class  AliZDCv1+;
+#pragma link C++ class  AliZDCv2+;
+
+#pragma link C++ class  AliZDCHit+;
+#pragma link C++ class  AliZDCSDigit+;
+#pragma link C++ class  AliZDCFragment+;
+#pragma link C++ class  AliZDCDigitizer+;
+
+#pragma link C++ class  AliGenZDC+;
+#pragma link C++ class  AliZDCMergedHit+;
+#pragma link C++ class  AliZDCMerger+;
+
+#endif
