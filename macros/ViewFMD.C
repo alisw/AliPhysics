d22f577d8296bb4cea3f48309cb5376dd23fd4e3
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-10-29 10:20:02 +0000

    Updated FMD visualization (C.Holm)

--- macros/ViewFMD.C
+++ macros/ViewFMD.C
@@ -1,7 +1,18 @@
 void ViewFMD()
 {
-   gMC->Gsatt("FMD0","seen",1);
-   gMC->Gsatt("FMD1","seen",1);
-   gMC->Gsatt("FMD2","seen",1);
-   gMC->Gsatt("FMD3","seen",1);
+   gMC->Gsatt("FMD1","seen",0);
+   gMC->Gsatt("FMD2","seen",0);
+   gMC->Gsatt("FMD3","seen",0);
+   gMC->Gsatt("FRGI","seen",0);
+   gMC->Gsatt("FRGO","seen",0);
+   gMC->Gsatt("FVFI","seen",0);
+   gMC->Gsatt("FVFO","seen",0);
+   gMC->Gsatt("FVBI","seen",0);
+   gMC->Gsatt("FVBO","seen",0);
+   gMC->Gsatt("FACI","seen",1);
+   gMC->Gsatt("FACO","seen",1);
+   gMC->Gsatt("FPTI","seen",1);
+   gMC->Gsatt("FPTO","seen",1);
+   gMC->Gsatt("FPBI","seen",1);
+   gMC->Gsatt("FPBO","seen",1);
 }
