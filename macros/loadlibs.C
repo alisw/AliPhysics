de093c01a15b7d1a6b29c4ee4b09cbb412f893c8
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-01-22 19:59:43 +0000

    Adapted to the new set of libraries

--- macros/loadlibs.C
+++ macros/loadlibs.C
@@ -1,5 +1,14 @@
 void loadlibs () 
 {
+  // Macro which loads the libraries needed for simulation and reconstruction
+  // Possible usage: In a Root session (no AliRoot) one does
+  // root [0] .x loadlibs.C
+  // root [1] gAlice = new AliRun("gAlice","test")           
+  // root [2] AliSimulation sim
+  // root [3] sim.Run()
+  // root [4] AliReconstruction rec
+  // root [5] rec.Run()
+
   gSystem->Load("libPhysics");
   gSystem->Load("libMinuit");
 
@@ -23,21 +32,32 @@ void loadlibs ()
   gSystem->Load("libEVGEN");
   gSystem->Load("libFASTSIM");
   gSystem->Load("libAliPythia6");
+
+  gSystem->Load("libhijing");
+  gSystem->Load("libTHijing");// AliGenHijingEventHeader needed by libZDCsim.so
+
   gSystem->Load("libSTRUCT");
   gSystem->Load("libPHOS");
   gSystem->Load("libMUONmapping");
-  gSystem->Load("libMUON");
+  gSystem->Load("libMUONgeometry");
+  gSystem->Load("libMUONbase");
+  gSystem->Load("libMUONsim");
+  gSystem->Load("libMUONrec");
   gSystem->Load("libFMDbase");
   gSystem->Load("libFMDsim");
   gSystem->Load("libFMDrec");
   gSystem->Load("libPMDbase");
   gSystem->Load("libPMDsim");
   gSystem->Load("libPMDrec");
-  gSystem->Load("libRICH");
+  gSystem->Load("libRICHbase");
+  gSystem->Load("libRICHsim");
+  gSystem->Load("libRICHrec");
   gSystem->Load("libSTARTbase");
   gSystem->Load("libSTARTsim");
   gSystem->Load("libSTARTrec");
-  gSystem->Load("libZDC");
+  gSystem->Load("libZDCbase");
+  gSystem->Load("libZDCsim");
+  gSystem->Load("libZDCrec");
   gSystem->Load("libCRT");
   gSystem->Load("libVZERObase");
   gSystem->Load("libVZEROsim");
@@ -56,5 +76,14 @@ void loadlibs ()
   gSystem->Load("libTRDsim");
   gSystem->Load("libTRDrec");
   gSystem->Load("libTRDfast");
-  gSystem->Load("libTOF");
+  gSystem->Load("libTOFbase");
+  gSystem->Load("libTOFsim");
+  gSystem->Load("libTOFrec");
+
+  gSystem->Load("libAliL3Src");
+  gSystem->Load("libAliL3Misc");
+  gSystem->Load("libAliL3Comp");
+  gSystem->Load("libThread");
+  gSystem->Load("libAliL3Hough");
+  gSystem->Load("libAliL3ITS");
 }
