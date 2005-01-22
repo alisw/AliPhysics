6c07ee6436eeca865c2a1581601f5ac211647f0c
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-01-22 19:58:12 +0000

    Changes to avoid circular dependences between the libraries

--- RICH/RICHbaseLinkDef.h
+++ RICH/RICHbaseLinkDef.h
@@ -4,9 +4,4 @@
 #pragma link off all functions;
 #pragma link C++ class  AliRICHParam+;
 #pragma link C++ class  AliRICHChamber+;
-#pragma link C++ class  AliRICH+;
-#pragma link C++ class  AliRICHhit+;
-#pragma link C++ class  AliRICHdigit+;
-#pragma link C++ class  AliRICHcluster+;
-#pragma link C++ class  AliRICHDisplFast+;
 #endif
--- RICH/RICHsimLinkDef.h
+++ RICH/RICHsimLinkDef.h
@@ -2,9 +2,14 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+#pragma link C++ class  AliRICH+;
 #pragma link C++ class  AliRICHv0+;
 #pragma link C++ class  AliRICHv1+;
 #pragma link C++ class  AliRICHDigitizer+;
 #pragma link C++ class  AliGenRadioactive+;
+#pragma link C++ class  AliRICHhit+;
+#pragma link C++ class  AliRICHdigit+;
+#pragma link C++ class  AliRICHcluster+;
+#pragma link C++ class  AliRICHDisplFast+;
 #pragma link C++ enum   RadioNuclei_t;
 #endif
