d8c2e9c80b85a44f56b708374e44bf10c45f287f
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-04-14 04:06:47 +0000

    Bug fix (alpha)

--- MUON/AliMUONFactoryV2.cxx
+++ MUON/AliMUONFactoryV2.cxx
@@ -849,7 +849,7 @@ void AliMUONFactoryV2::BuildStation6()
 
       // chamber 8 7 1 0 cathode 1
       for (Int_t i=0; i<6; i++) nStrip[i]=8;
-      nStrip[7]=16;
+      nStrip[6]=16;
       for (Int_t i=0; i<7; i++) stripYsize[i]=stripLength[3];  
       for (Int_t i=0; i<7; i++) stripXsize[i]=stripWidth[2];
       trigSegY[8]->Init(0,nStrip,stripYsize,stripXsize,0.);  
