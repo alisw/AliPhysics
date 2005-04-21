24837f662eaeea16d2c0e8d9d5c100c9f7674323
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-04-21 08:21:11 +0000

    Get rid of old geometry

--- TRD/AliTRDv2.cxx
+++ TRD/AliTRDv2.cxx
@@ -29,7 +29,7 @@
 
 #include "AliConst.h"
 #include "AliRun.h"
-#include "AliTRDgeometryDetail.h"
+#include "AliTRDgeometryFull.h"
 #include "AliTRDv2.h"
 
 ClassImp(AliTRDv2)
@@ -62,7 +62,7 @@ AliTRDv2::AliTRDv2(const char *name, const char *title)
   if (frame->IsVersion() == 1) {
     // Detailed geometry without hole
     if (fGeometry) delete fGeometry;
-    fGeometry = new AliTRDgeometryDetail();
+    fGeometry = new AliTRDgeometryFull();
   }
   else {
     Error("Ctor","Could not find valid FRAME version 1\n");
