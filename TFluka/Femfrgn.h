06f538ebf33bdb22d755064d8f2bc9c093f92bba
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-05-26 09:49:21 +0000

    EMFRGN common block added.

---
+++ TFluka/Femfrgn.h
@@ -0,0 +1,42 @@
+#ifndef FEMFRGN_H
+#define FEMFRGN_H 1
+
+#include "cfortran.h"
+#include "Rtypes.h"
+extern "C" {
+  //*$ CREATE EMFRGN.ADD
+  //*COPY EMFRGN
+  //*
+  //*----------------------------------------------------------------------*
+  //*                                                                      *
+  //*     Common Emfrgn for EMF                                            *
+  //*                                                                      *
+  //*----------------------------------------------------------------------*
+  //*
+  //    COMMON / EFMRGN / RHOR   (MXXRGN), ECUT   (MXXRGN), PCUT (MXXRGN),
+  //   &                  EMREJE, EMSAMP, EMSNGL, VACDST,
+  //   &                  MEDEMF (MXXRGN), IRAYLR (MXXRGN), NRGEMF,
+  //   &                  NOSCAT, NOLLDA
+  //*D === Obsolete variable names === *
+  //*D     DIMENSION MED (MXXRGN)
+  //*D     EQUIVALENCE ( MED (1), MEDEMF (1) )
+typedef struct {
+  Double_t rhor[mxxrgn];
+  Double_t ecut[mxxrgn];
+  Double_t pcut[mxxrgn];
+  Double_t emreje;
+  Double_t emsamp;
+  Double_t emsngl;
+  Double_t vacdst;
+  Int_t    medemf[mxxrgn];
+  Int_t    iraylr[mxxrgn];
+  Int_t    nrgemf;
+  Int_t    noscat;
+  Int_t    nollda;
+} emfrgnCommon;
+
+#define EFMRGN COMMON_BLOCK(EFMRGN,efmrgn)
+COMMON_BLOCK_DEF(emfrgnCommon,EFMRGN);
+}
+
+#endif
