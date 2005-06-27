4f42401ba3ba18f1044657a123188e3c9f4cf362
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-06-27 07:34:52 +0000

    Removing memory leak

--- FMD/AliFMDReconstructor.cxx
+++ FMD/AliFMDReconstructor.cxx
@@ -235,11 +235,11 @@ AliFMDReconstructor::Reconstruct(TTree* digitsTree,
   AliDebug(1, "Reconstructing from digits in a tree");
   
   TBranch *digitBranch = digitsTree->GetBranch("FMD");
-  TClonesArray* digits = new TClonesArray("AliFMDDigit");
   if (!digitBranch) {
     Error("Exec", "No digit branch for the FMD found");
     return;
   }
+  TClonesArray* digits = new TClonesArray("AliFMDDigit");
   digitBranch->SetAddress(&digits);
 
   TIter next(&fAlgorithms);
@@ -255,6 +255,8 @@ AliFMDReconstructor::Reconstruct(TTree* digitsTree,
   while ((algorithm = static_cast<AliFMDMultAlgorithm*>(next()))) 
     algorithm->PostEvent();
   clusterTree->Fill();
+  digits->Delete();
+  delete digits;
 }
  
 //____________________________________________________________________
