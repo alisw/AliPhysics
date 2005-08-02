bb698e6eda5ad4d43f4d0f713eb5fd4fb1a94f35
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-08-02 12:33:09 +0000

    Corrected argument

--- EMCAL/AliEMCALJetMicroDst.h
+++ EMCAL/AliEMCALJetMicroDst.h
@@ -81,7 +81,7 @@ class AliEMCALJetMicroDst: public TNamed {
   // service routine
   static TList *MoveHistsToList(const char* name="ListOfHists", Bool_t putToBrowser=kTRUE);
   static void FillH1(TList *l=0, Int_t ind=0, Double_t x=-99999., Double_t w=1.);
-  static void FillH2(TList *l=0, Int_t ind=0, Double_t x=-99999., Double_t w=-99999., Double_t w=1.);
+  static void FillH2(TList *l=0, Int_t ind=0, Double_t x=-99999., Double_t y=-99999., Double_t w=1.);
   static int  SaveListOfHists(TList *list=0, const char* name="test", Bool_t kSingleKey=kFALSE,
   const char* opt="RECREATE");
 
