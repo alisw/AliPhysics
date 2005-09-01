754d4f725b2162d2c7462399853e56291272e5fc
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-09-01 13:40:43 +0000

    episor replaced by sourcm

--- TFluka/Fsourcm.h
+++ TFluka/Fsourcm.h
@@ -1,17 +1,17 @@
-#ifndef FEPISOR_H
-#define FEPISOR_H 1
+#ifndef FSOURCM_H
+#define FSOURCM_H 1
 
 #include "Rtypes.h"
 #include "cfortran.h"
 extern "C" {
-//*$ create episor.add
-//*copy episor
+//*$ create sourcm.add
+//*copy sourcm
 //*
-//*=== episor ===========================================================*
+//*=== Sourcm ===========================================================*
 //*
 //*----------------------------------------------------------------------*
 //*                                                                      *
-//*     Include file: episor                                             *
+//*     Include file: sourcm                                             *
 //*                                                                      *
 //*     version       march 1996     by   Alfredo Ferrari, INFN - Milan  *
 //*                                                                      *
@@ -20,14 +20,13 @@ extern "C" {
 //*     Included in the following subroutines or functions:              *
 //*                                                                      *
 //*            BDNOPT                                                    *
-//*            EPILOG                                                    *
+//*            FLKEND                                                    *
 //*            EVTDAT                                                    *
 //*            FEEDER                                                    *
 //*            FLUKAM                                                    *
 //*            MGDRAW                                                    *
 //*            SOURCE                                                    *
 //*                                                                      *
-//*     Description of the common block(s) and variable(s)               *
 //*                                                                      *
 //*      Whasou(1-18) = user variables                                   *
 //*            Tkesum = total kinetic energy of the primaries of the     *
@@ -45,15 +44,15 @@ typedef struct {
    Double_t tkesum;
    Int_t    lussrc;
    Int_t    lsouit;
-} episorCommon;
-#define EPISOR COMMON_BLOCK(EPISOR,episor)
-COMMON_BLOCK_DEF(episorCommon,EPISOR);
+} sourcmCommon;
+#define SOURCM COMMON_BLOCK(SOURCM,sourcm)
+COMMON_BLOCK_DEF(sourcmCommon,SOURCM);
 
 typedef struct {
    Char_t   sdusou[8];
-} chepsrCommon;
-#define CHEPSR COMMON_BLOCK(CHEPSR,chepsr)
-COMMON_BLOCK_DEF(chepsrCommon,CHEPSR);
+} chsocmCommon;
+#define CHSOCM COMMON_BLOCK(CHSOCM,chsocm)
+COMMON_BLOCK_DEF(chsocmCommon,CHSOCM);
 
 typedef struct {
    Double_t xn[3];
@@ -64,6 +63,31 @@ typedef struct {
 } norlatCommon;
 #define NORLAT COMMON_BLOCK(NORLAT,norlat)
 COMMON_BLOCK_DEF(norlatCommon,NORLAT);   
+
+typedef struct {
+   Double_t xb[3];
+   Double_t wb[3];
+   Double_t wp[3];
+   Double_t xp[3];
+   Double_t rin;
+   Double_t rout;
+   Double_t pinf;
+   Double_t dist;
+   Int_t    ir;
+   Int_t    idbg;
+   Int_t    irprim;
+   Int_t    nasc;
+   Int_t    lsurf;
+   Int_t    nbo;
+   Int_t    lri;
+   Int_t    lro;
+   Int_t    kloop;
+   Int_t    loop;
+   Int_t    itype;
+   Int_t    n0a;
+} paremCommon;
+#define PAREM COMMON_BLOCK(PAREM,parem)
+COMMON_BLOCK_DEF(paremCommon,PAREM);   
 }
 
 #endif
