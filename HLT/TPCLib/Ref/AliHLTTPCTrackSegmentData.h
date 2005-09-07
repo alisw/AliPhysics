7bb807bb6815178300feadc5f6aa4402318b3695
timms <timms@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-09-07 13:37:22 +0000

    Added data members to the track segment data structure to make it similarly sized and layouted as the one from the "normal" (one directory up) HLT TPC component library.

--- HLT/TPCLib/Ref/AliHLTTPCTrackSegmentData.h
+++ HLT/TPCLib/Ref/AliHLTTPCTrackSegmentData.h
@@ -16,9 +16,31 @@ struct AliHLTTPCTrackSegmentData
 	Double_t fPt;
 	Double_t fPsi;
         Double_t fTgl;
+	Double_t fPterr;
+	Double_t fPsierr;
+        Double_t fTglerr;
         Int_t fCharge;
+#ifdef INCLUDE_TPC_HOUGH
+#ifdef ROWHOUGHPARAMS
+      /* needed for PDC */
+        UInt_t  fWeight;
+        Int_t  fTrackID;
+        Int_t  fRowRange1;
+        Int_t  fRowRange2;
+        Int_t  fSector;
+        Float_t  fPID;
+        Float_t  fBinX;
+        Float_t  fBinY;
+        Float_t  fBinXSize;
+        Float_t  fBinYSize;
+#endif
+#endif // INCLUDE_TPC_HOUGH
 	UInt_t  fNPoints;
+#if defined(__HP_aCC) || defined(__DECCXX) || defined(__SUNPRO_CC)
+	UInt_t  fPointIDs[1];
+#else
 	UInt_t  fPointIDs[0];
+#endif
     };
 
 typedef struct AliHLTTPCTrackSegmentData AliHLTTPCTrackSegmentData;
