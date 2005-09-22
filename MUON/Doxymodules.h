930c98c800ad07aa1be8f373e37af2c01d23b076
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-09-22 11:10:33 +0000

    Doxygen configuration files (Initial version)

---
+++ MUON/Doxymodules.h
@@ -0,0 +1,70 @@
+// $Id$
+// The class categories definitions for Doxygen
+
+/** @defgroup base MUON_base
+ *  Category of basic classes
+ *  @{
+ */
+/** @} */
+
+/** @defgroup sim MUON_sim
+ *  Category of classes for simulation
+ *  @{
+ */
+/** @} */
+
+/** @defgroup rec MUON_rec
+ *  Category of classes for reconstruction
+ *  @{
+ */
+/** @} */
+
+/** @defgroup geometry MUON_geometry
+ *  Category of geometry framework classes
+ *  @{
+ */
+/** @} */
+
+/** @defgroup mapping MUON_mapping
+ *  Category of mapping classes
+ *  @{
+ */
+
+/** @defgroup basic MUON_mapping_basic
+ *  @ingroup mapping
+ *  Category of basic classes in mapping
+ *  @{
+ */
+/** @} */
+
+/** @defgroup motif MUON_mapping_motif
+ *  @ingroup mapping
+ *  Category of motif classes in mapping
+ *  @{
+ */
+/** @} */
+
+/** @defgroup sector MUON_mapping_sector
+ *  @ingroup mapping
+ *  Category of classes for quadrants in mapping
+ *  @{
+ */
+/** @} */
+
+/** @defgroup slat MUON_mapping_slat
+ *  @ingroup mapping
+ *  Category of classes for slats in mapping
+ *  @{
+ */
+/** @} */
+
+/** @defgroup graphics MUON_mapping_graphics
+ *  @ingroup mapping
+ *  Category of classes for visualization in mapping
+ *  @{
+ */
+/** @} */
+
+
+
+
---
+++ MUON/Doxymodules_STEER.h
@@ -0,0 +1,103 @@
+// $Id$
+// The class categories definitions for Doxygen
+
+/** @defgroup STEER STEER
+ *  Category of AliRoot steering classes
+ *  @{
+ */
+    class AliRun {};
+    class AliRunLoader {}; 
+    class AliLoader {}; 
+    class AliDataLoader {};
+    class AliObjectLoader {}; 
+    class AliBaseLoader {}; 
+    class AliTreeLoader {}; 
+    class AliTaskLoader {};
+    class AliDetector {};	
+    class AliHeader {};	
+    class AliMagF {}; 
+    class AliDigit {};	
+    class AliHit {};	
+    class AliPoints {};
+    class AliDisplay {};	
+    class AliRun {}; 
+    class AliGenerator {}; 
+    class AliVertexGenerator {};
+    class AliLego {};	
+    class AliModule {};	
+    class AliDigitNew {}; 
+    class AliGeometry {};	
+    class AliRecPoint {};	
+    class AliSegmentation {};
+    class AliHitMap {};	
+    class AliMagFC {};	
+    class AliMagFCM {};
+    class AliMagFDM {};	
+    class AliLegoGenerator {}; 
+    class AliLegoGeneratorXYZ {};
+    class AliLegoGeneratorPhiZ {}; 
+    class AliLegoGeneratorEta {};
+    class AliRndm {};
+    class AliCluster {}; 
+    class AliTracker {};
+    class AliMCQA {}; 
+    class AliPDG {}; 
+    class AliDebugVolume {};
+    class AliGenEventHeader {}; 
+    class AliStack {}; 
+    class AliConfig {};
+    class AliRunDigitizer {}; 
+    class AliDigitizer {}; 
+    class AliStream {}; 
+    class AliMergeCombi {}; 
+    class AliMagFMaps {}; 
+    class AliFieldMap {}; 
+    class AliGausCorr {}; 
+    class AliTrackReference {};
+    class AliTrackMap {}; 
+    class AliTrackMapper {}; 
+    class AliCollisionGeometry {}; 
+    class AliMemoryWatcher {}; 
+    class AliVertexer {}; 
+    class AliMC {}; 
+    class AliSimulation {}; 
+    class AliReconstruction {}; 
+    class AliVertexGenFile {}; 
+    class AliRunTag {}; 
+    class AliLHCTag {}; 
+    class AliDetectorTag {}; 
+    class AliEventTag {}; 
+    class AliCDBMetaData {}; 
+    class AliCDBEntry {}; 
+    class AliCDBStorage {}; 
+    class AliCDBDump {}; 
+    class AliCDBLocal {};
+    class AliReconstructor {}; 
+    class AliCDBMetaDataSelect {};
+    class AliDetectorEventHeader {}; 
+    class TTreeStream {};
+/** @} */
+
+/** @defgroup STEER_ESD STEER_ESD
+ *  Category of AliRoot event sumary data classes
+ *  @{
+ */
+    class AliESD {};
+    class AliESDtrack {};
+    class AliESDMuonTrack {}; 
+    class AliESDPmdTrack {}; 
+    class AliESDHLTtrack {};
+    class AliESDv0 {}; 
+    class AliESDcascade {}; 
+    class AliESDVertex {};
+    class AliESDpid {}; 
+    class AliESDkink {}; 
+    class AliESDV0MI {};
+    class AliKalmanTrack {}; 
+    class AliHelix {}; 
+    class AliExternalTrackParam {}; 
+    class AliTrackParam {}; 
+    class AliLog {}; 
+    class AliPID {}; 
+    class AliESDV0MIParams {};
+/** @} */
