ea88b8d963faf98e30b6358b004810b820292f6b
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-09-26 07:34:59 +0000

    Added setting path to mapping data

--- MUON/mapping/macros/mlibs.C
+++ MUON/mapping/macros/mlibs.C
@@ -1,23 +1,25 @@
 // $Id$
 //
-// Macro for loading libraries for mapping in AliRoot
+// Macro for loading libraries for mapping in AliRoot.
+// It sets also the environment variable used to find mapping data.
 
 void mlibs() 
 {
   // add include path
   // gSystem->SetIncludePath(" -I$MINSTALL/include");
   
-  // set path to mapping
-  if (! gSystem->Getenv("MINSTALL")) {    
-    TString dirPath = gSystem->Getenv("ALICE_ROOT");
-    dirPath += "/MUON/mapping"; 
-    gSystem->Setenv("MINSTALL", dirPath.Data());
-    //cout << "AliMpFiles top path set to " << dirPath << endl;	  
-  }
-
   // load Root libraries
   gSystem->Load("libPhysics"); 
   
   // load mapping library
   gSystem->Load("libMUONmapping"); 
+
+  // set path to mapping data
+  if (! gSystem->Getenv("MINSTALL")) {    
+    TString dirPath = gSystem->Getenv("ALICE_ROOT");
+    dirPath += "/MUON/mapping"; 
+    AliMpFiles::Instance()->SetTopPath(dirPath);
+    gSystem->Setenv("MINSTALL", dirPath.Data());
+    // cout << "AliMpFiles top path set to " << dirPath << endl;	  
+  }  
 }  
