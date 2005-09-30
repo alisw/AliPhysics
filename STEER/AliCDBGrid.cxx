804e1391753be10cf14ce74946175ef3a31e4612
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-09-30 09:26:45 +0000

    Small bugfix (A.Colla)

--- STEER/AliCDBGrid.cxx
+++ STEER/AliCDBGrid.cxx
@@ -206,9 +206,9 @@ Bool_t AliCDBGrid::PrepareId(AliCDBId& id) {
 
 	TString lastStorage = id.GetLastStorage();
 	if(lastStorage.Contains(TString("new"), TString::kIgnoreCase) && id.GetVersion() > 1 ){
-		AliWarning(Form("*** WARNING! a NEW object is being stored with version v%d_s%d",
+		AliWarning(Form("*** WARNING! a NEW object is being stored with version %d",
 					id.GetVersion()));
-		AliWarning(Form("and it will hide previously stored object with v%d_s%d!",
+		AliWarning(Form("and it will hide previously stored object with version %d!",
 					id.GetVersion()-1));
 	}
 
