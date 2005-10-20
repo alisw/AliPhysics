f3f616b1e524b6ce1d06eb20ed54a927d8466deb
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-10-20 16:03:01 +0000

    Obsolete code removed (fca).

--- TFluka/scripts/runflukageo.sh
+++ TFluka/scripts/runflukageo.sh
@@ -1,32 +1,18 @@
-#!/bin/bash
-CURDIR=`pwd`
-cd $ALICE_ROOT
-echo 'Making sure that TFluka is up to date...'
-make all-TFluka
-cd $CURDIR
+#!/bin/sh
 # Remove the previous temporary directory
 rm -rf tmp
 # Make a new temporary directory and move to it
 mkdir tmp
 cd tmp
-mkdir peg
 
 # Link here some special Fluka files needed
-ln -s $FLUPRO/xnloan.dat .
-ln -s $FLUPRO/sigmapi.bin .
-ln -s $FLUPRO/nuclear.bin .
 ln -s $FLUPRO/neuxsc_72.bin neuxsc.bin
-ln -s $FLUPRO/fluodt.dat .
-ln -s $FLUPRO/elasct.bin .
 
 # Copy the random seed
 cp $FLUPRO/random.dat old.seed
 
 # Give some meaningfull name to the output
 ln -s fluka.out fort.11
-
-# Link the pemf and input file for alice
-ln -s $ALICE_ROOT/TFluka/input/FlukaVmc.pemf .
 
 #Link FlukaConfig.C as Config.C
 ln -fs $ALICE_ROOT/TFluka/macro/FlukaConfig.C Config.C
