58f00f52de054b44bec79497e33805f57d8bc8e5
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-10-28 07:02:50 +0000

    Test case for B0 -> mu

--- EVGEN/AliDecayer.h
+++ EVGEN/AliDecayer.h
@@ -18,7 +18,7 @@ typedef enum
     kBPsiPrimeDiMuon, kBPsiPrimeDiElectron, kPiToMu, kKaToMu, 
     kNoDecay, kHadronicD, kOmega, kPhiKK, 
     kAll, kNoDecayHeavy, kHardMuons, kBJpsi,
-    kWToMuon,kWToCharm, kWToCharmToMuon
+    kWToMuon,kWToCharm, kWToCharmToMuon, kNewTest
 } Decay_t;
 #endif
 
