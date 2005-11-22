b96a1ad1069a40fbddf65faaf97108ad0f1e993e
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-11-22 13:59:06 +0000

    - Removing AliMUONVGeometryDEIndexing class; - Adding AliMUONGeometryModuleTransformer, AliMUONGeometryTransformer,   AliMUONGeometry, AliMUONSegmentation, AliMUONGeometryDEIndexing classes

--- MUON/MUONLinkDefGeometry.h
+++ MUON/MUONLinkDefGeometry.h
@@ -10,13 +10,17 @@
 #pragma link C++ class  AliMUONGeometryEnvelopeStore+;
 #pragma link C++ class  AliMUONGeometryDetElement+;
 #pragma link C++ class  AliMUONGeometrySVMap+;
+#pragma link C++ class  AliMUONGeometryModuleTransformer+;
 #pragma link C++ class  AliMUONGeometryModule+;
-#pragma link C++ class  AliMUONVGeometryDEIndexing+;
+#pragma link C++ class  AliMUONGeometryTransformer+;
+#pragma link C++ class  AliMUONGeometry+;
 #pragma link C++ class  AliMUONVGeometryBuilder+;
 #pragma link C++ class  AliMUONGeometryBuilder+;
 #pragma link C++ class  AliMUONGeometryStore+;
 #pragma link C++ class  AliMUONVGeometryDESegmentation+;
 #pragma link C++ class  AliMUONGeometrySegmentation+;
+#pragma link C++ class  AliMUONSegmentation+;
+#pragma link C++ class  AliMUONGeometryDEIndexing+;
 
 #pragma link C++ enum   AliMUONGeometryDirection;
 #endif
