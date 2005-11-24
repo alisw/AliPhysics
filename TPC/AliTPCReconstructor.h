3cf3dd10e9b74b85b584e9f37ad002e20627bd13
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-11-24 14:38:46 +0000

    Correct overloading of the methods from AliReconstructor

--- TPC/AliTPCReconstructor.h
+++ TPC/AliTPCReconstructor.h
@@ -18,8 +18,26 @@ public:
   virtual void         Reconstruct(AliRunLoader* runLoader) const;
   virtual void         Reconstruct(AliRunLoader* runLoader,
 				   AliRawReader* rawReader) const;
+  virtual void         Reconstruct(TTree* digitsTree, TTree* clustersTree) const {
+    AliReconstructor::Reconstruct(digitsTree,clustersTree);
+  }
+  virtual void         Reconstruct(AliRawReader* rawReader, TTree* clustersTree) const {
+    AliReconstructor::Reconstruct(rawReader,clustersTree);
+  }
   virtual AliTracker*  CreateTracker(AliRunLoader* runLoader) const;
   virtual void         FillESD(AliRunLoader* runLoader, AliESD* esd) const;
+  virtual void         FillESD(TTree* digitsTree, TTree* clustersTree, 
+			       AliESD* esd) const {
+    AliReconstructor::FillESD(digitsTree,clustersTree,esd);
+  }
+  virtual void         FillESD(AliRawReader* rawReader, TTree* clustersTree, 
+			       AliESD* esd) const {
+    AliReconstructor::FillESD(rawReader,clustersTree,esd);
+  }
+  virtual void         FillESD(AliRunLoader* runLoader, 
+			       AliRawReader* rawReader, AliESD* esd) const {
+    AliReconstructor::FillESD(runLoader,rawReader,esd);
+  }
 
   static void SetCtgRange(Double_t ctgRange = 1.05) {fgCtgRange = ctgRange;}
   static Double_t GetCtgRange(){ return fgCtgRange;}
