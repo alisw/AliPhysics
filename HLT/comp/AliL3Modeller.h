6c64655cd347bf6b889b511a1ab9108bf49d67be
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-12-09 08:34:32 +0000

    Compiler warnings fixes.

--- HLT/comp/AliL3Modeller.h
+++ HLT/comp/AliL3Modeller.h
@@ -51,9 +51,9 @@ class AliL3Modeller {
   void SetOuterSearchRange(Int_t p,Int_t t) {fOuterPadSearch=p; fOuterTimeSearch=t;}
   void SetMaxClusterRange(Int_t p,Int_t t) {fMaxPads=p; fMaxTimebins=t;}
   void Debug() {fDebug=kTRUE;}
-  virtual void SetFitRange(Int_t /*p*/,Int_t /*t*/) {return;}
+  virtual Bool_t SetFitRange(AliL3ModelTrack *track,Int_t */*p*/,Int_t */*t*/) {return kFALSE;}
   virtual void SetNmaxOverlaps(Int_t /*i*/) {return;}
-  virtual void SetChiSqMax(Float_t /*f*/) {return;}
+  virtual void SetChiSqMax(Float_t /*f*/,Int_t /*p*/) {return;}
   
   AliL3TrackArray *GetTracks() {return fTracks;}
     
