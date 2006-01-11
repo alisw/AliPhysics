79c5ed0abf31473f7643e77732b3a4d3e89e1b22
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-01-11 09:05:17 +0000

    Removed kStationInvalid value

--- MUON/mapping/AliMpStationType.h
+++ MUON/mapping/AliMpStationType.h
@@ -2,7 +2,7 @@
  * See cxx source for full Copyright notice                               */
 
 // $Id$
-// $MpId: AliMpStationType.h,v 1.5 2005/10/28 15:05:04 ivana Exp $
+// $MpId: AliMpStationType.h,v 1.6 2006/01/11 10:07:18 ivana Exp $
 
 /// \ingroup basic
 /// \enum AliMpStationType
@@ -12,40 +12,32 @@
  
 #ifndef ALI_MP_STATION_TYPE_H
 #define ALI_MP_STATION_TYPE_H
+
+#include <TString.h>
+
+#include "AliLog.h"
  
 enum AliMpStationType
 {
-  kStationInvalid = -1,///< invalid station
-  kStation1 = 0,       ///< station 1 (quadrants)
+  kStation1,           ///< station 1 (quadrants)
   kStation2,           ///< station 2 (quadrants)
   kStation345,         ///< station 3,4,5 (slats)
   kStationTrigger      ///< trigger stations (slats)
-
 };
 
 inline 
-const char* StationTypeName(AliMpStationType stationType)
+TString StationTypeName(AliMpStationType stationType)
 {
-  switch ( stationType )
-  {
-    case kStation1:
-      return "st1";
-      break;
-    case kStation2:
-      return "st2";
-      break;
-    case kStation345:
-      return "slat";
-      break;
-    case kStationTrigger:
-      return "trigger";
-      break;
-    case kStationInvalid:
-    default:
-      return "invalid";
-      break;
+  switch ( stationType ) {
+    case kStation1:       return "st1";     break;
+    case kStation2:       return "st2";     break;
+    case kStation345:     return "slat";    break;
+    case kStationTrigger: return "trigger"; break;
   }
-  return "unknown";
+  
+  // Cannot reach this line
+  AliFatalGeneral("AliMpStationType.h", "Unknown station type"); 
+  return "invalidStation";
 }
 
 #endif //ALI_MP_STATION_TYPE_H
