2813e996d45a77586bb03edc83a3800bf1ecc366
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-01-24 11:08:22 +0000

    Adding AliVZEROv6

--- VZERO/VZEROsimLinkDef.h
+++ VZERO/VZEROsimLinkDef.h
@@ -11,6 +11,7 @@
 #pragma link C++ class  AliVZEROv3+;
 #pragma link C++ class  AliVZEROv4+;
 #pragma link C++ class  AliVZEROv5+;
+#pragma link C++ class  AliVZEROv6+;
 #pragma link C++ class  AliVZEROhit+;
 #pragma link C++ class  AliVZERODigitizer+;
 #pragma link C++ class  AliVZEROBuffer+;
