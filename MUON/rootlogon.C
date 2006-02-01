01a22692e1bfd82226be5c9bd89195fde0449295
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-02-01 13:03:25 +0000

    Added for setting include paths

---
+++ MUON/rootlogon.C
@@ -0,0 +1,10 @@
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+/// By Laurent Aphecetche
+
+{
+  gSystem->SetIncludePath("-I${ALICE_ROOT}/include -I${ALICE_ROOT}/MUON -I${ALICE_ROOT}/MUON/mapping");
+}
