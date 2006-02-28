e21ff8a797210b560e4a558d0d9d38d05f5ce3c8
jklay <jklay@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-02-28 22:29:05 +0000

    add package files for jetfinder code

---
+++ EMCAL/EMCALLinkDefjet.h
@@ -0,0 +1,34 @@
+#ifdef __CINT__
+// for libEMCALrec.pkg
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ enum  AliEMCALJetFinderAlgoBGCalcType_t;
+#pragma link C++ enum  AliEMCALJetFinderResetType_t;
+#pragma link C++ enum  AliEMCALJetFinderTrackType_t;
+#pragma link C++ enum  AliEMCALJetFinderSmearingType_t;
+#pragma link C++ enum  AliEMCALJetFinderEMCALType_t;
+#pragma link C++ enum  AliEMCALJetFinderFileType_t;
+#pragma link C++ enum  AliEMCALJetFinderAlgoUA1UnitFlagType_t;
+#pragma link C++ enum  AliEMCALJetFinderAlgoUA1FillUnitFlagType_t;
+// -
+#pragma link C++ class AliEMCALGeneratorFactory+;
+#pragma link C++ class AliEMCALJet+;
+#pragma link C++ class AliEMCALParton+;
+#pragma link C++ class AliEMCALJetFinder+;
+#pragma link C++ class AliEMCALJetFinderInput+;
+#pragma link C++ class AliEMCALJetFinderInputPrep+;
+#pragma link C++ class AliEMCALJetFinderInputSimPrep+;
+#pragma link C++ class AliEMCALJetFinderOutput+;
+#pragma link C++ class AliEMCALJetFinderAlgo+;
+#pragma link C++ class AliEMCALJetFinderAlgoOmni+;
+#pragma link C++ class AliEMCALJetFinderAlgoUA1Unit+;
+#pragma link C++ class AliEMCALJetFinderPlots+;
+#pragma link C++ class AliEMCALJetMicroDst+;
+#pragma link C++ class AliEMCALHadronCorrection+;		 
+#pragma link C++ class AliEMCALHadronCorrectionv0+;
+#pragma link C++ class AliEMCALHadronCorrectionv1+;		 
+#pragma link C++ class AliEMCALFast+;
+
+#endif
