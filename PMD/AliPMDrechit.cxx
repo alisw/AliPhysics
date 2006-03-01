2ef8b1d50a3332034838363c4de2a3b82b340135
bnandi <bnandi@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-03-01 14:47:29 +0000

    stores hit info of a cluster

---
+++ PMD/AliPMDrechit.cxx
@@ -0,0 +1,70 @@
+/***************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+//-----------------------------------------------------//
+//                                                     //
+//  Date   : February 26 2006                          //
+//                                                     //
+//  Store cellhits associated to a cluster             //
+//                                                     //
+//-----------------------------------------------------//
+
+#include "Riostream.h"
+#include "Rtypes.h"
+#include "AliPMDrechit.h"
+
+ClassImp(AliPMDrechit)
+
+AliPMDrechit::AliPMDrechit():
+  fXcell(0),
+  fYcell(0)
+{
+  // Standard constructor
+}
+
+AliPMDrechit::AliPMDrechit(Int_t cellx, Int_t celly)
+{
+  // Constructor
+  fXcell     = cellx;
+  fYcell     = celly;
+}
+
+AliPMDrechit::AliPMDrechit(const AliPMDrechit& source):TObject(source) {
+  //Copy Constructor 
+  if(&source == this) return;
+  this->fXcell = source.fXcell;
+  this->fYcell = source.fYcell;
+  return;
+}
+
+AliPMDrechit& AliPMDrechit::operator=(const AliPMDrechit& source) {
+  //Copy Constructor 
+  if(&source == this) return *this;
+  this->fXcell = source.fXcell;
+  this->fYcell = source.fYcell;
+  return *this;
+}
+
+AliPMDrechit::~AliPMDrechit()
+{
+  // Default destructor
+}
+Int_t AliPMDrechit::GetCellX() const
+{
+  return fXcell;
+}
+Int_t AliPMDrechit::GetCellY() const
+{
+  return fYcell;
+}
---
+++ PMD/AliPMDrechit.h
@@ -0,0 +1,37 @@
+#ifndef ALIPMDRECHIT_H
+#define ALIPMDRECHIT_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+//-----------------------------------------------------//
+//                                                     //
+//  Date   : February 26 2006                          //
+//                                                     //
+//  Store cellhits associated to a cluster             //
+//                                                     //
+//-----------------------------------------------------//
+
+#include "TObject.h"
+class TClonesArray;
+
+class AliPMDrechit : public TObject
+{
+ public:
+  AliPMDrechit();
+  AliPMDrechit(Int_t cellx, Int_t celly);
+  AliPMDrechit(AliPMDrechit *pmdrechit) {*this = *pmdrechit;}
+  AliPMDrechit (const AliPMDrechit &alipmdrechit);  // copy constructor
+  AliPMDrechit &operator=(const AliPMDrechit &alipmdrechit); // assignment op
+
+  virtual ~AliPMDrechit();
+
+  Int_t   GetCellX() const;
+  Int_t   GetCellY() const;
+  
+ protected:
+  Int_t   fXcell;         // x-position of the cell
+  Int_t   fYcell;         // y-position of the cell
+  
+  ClassDef(AliPMDrechit,1) // To keep cell hit information
+};
+
+#endif
