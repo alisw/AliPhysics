21e2c3a2dbb071dedc0ccf233a8196d78a580558
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-03-07 19:22:27 +0000

    Remove CDB setting from the script. It is passed via env.variable

--- PHOS/macros/AlignmentDB/AliPHOSDisalign.C
+++ PHOS/macros/AlignmentDB/AliPHOSDisalign.C
@@ -1,32 +1,18 @@
 /* $Id$ */
 
-// Run simulations with PHOS alignment parameters read from CDB
-// located in the local file
-// InitAlignDB/PHOS/Align/IdealGeometry2007/RunXXX.root
+// Run simulations with PHOS alignment parameters read from CDB.
+// CDB storage should be defined by environment variable CDB_PATH before 
+// the aliroot session, otherwise the default hard-coded geometry will be used
+// The alignment object is located in the local file
+// $CDB_PATH/PHOS/Alignment/Geometry/RunXXX.root
 //
 // Author: Yuri Kharlov
-// 4 March 2006
+// 7 March 2006
 
-void AliPHOSDisalign(char *path="InitAlignDB", Int_t nevents=1)
+void AliPHOSDisalign(Int_t nevents=1)
 {
-
-  //Load alignment database into aliroot session
-  
-  TString localPath="local://";
-  localPath += path;
-  AliPHOSAlignData* alignda  = (AliPHOSAlignData*)(AliCDBManager::Instance()
-    ->GetStorage(localPath)->Get("PHOS/Alignment/Geometry",0)
-    ->GetObject());
-  alignda->Print();
-
-  // Create PHOS geometry instance with alignment object read from CDB
-
-  AliPHOSGeometry* geom = AliPHOSGeometry::GetInstance("IHEP","",alignda);
-
-  // Run simulations
-
   AliSimulation sim ; 
-//   sim.SetMakeSDigits("PHOS") ;
-//   sim.SetMakeDigits("PHOS") ;
+  sim.SetMakeSDigits("PHOS") ;
+  sim.SetMakeDigits("PHOS") ;
   sim.Run(nevents) ;  
 }
