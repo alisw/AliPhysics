6ce8518a7e9716ec8d08de4468963bb3df05d673
bnandi <bnandi@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-03-14 15:30:39 +0000

    calibration macros

---
+++ PMD/AliPMDPutCalibration.C
@@ -0,0 +1,12 @@
+//
+// This macro puts the gains in the database
+//
+void AliPMDPutCalibration()
+{
+  AliPMDCalibData *calibda=new AliPMDCalibData();
+  AliCDBManager *man = AliCDBManager::Instance();
+  man->SetDefaultStorage("local://$ALICE_ROOT");
+  AliCDBId id("PMD/Calib/Data",0,0);
+  AliCDBMetaData *md=new AliCDBMetaData();
+  man->GetDefaultStorage()->Put(calibda,id,md);
+}
