e556de6d581b3080ea90f9cf4b5e951c0cd77753
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-03-17 16:54:24 +0000

    Let the CDB storage alone. It is now handled by AliSimulation and AliReconstruction (Laurent)

--- MUON/AlirootRun_MUONtest.sh
+++ MUON/AlirootRun_MUONtest.sh
@@ -13,31 +13,10 @@ FULLPATH="$CURDIR/$OUTDIR"
 NEVENTS=100
 SEED=1234567
 
-CDBDIRECTORY="$ALICE_ROOT/MUON/CDB/Default";
-CDB="local://$CDBDIRECTORY";
-
-if [ ! -d $CDBDIRECTORY"/MUON" ]; then
-
-echo "Generating Condition Database in directory $CDBDIRECTORY. This may take a while, so please be patient..."
-
-aliroot -b >& testGenerateCalibrations.out << EOF
-.L $ALICE_ROOT/MUON/MUONCDB.C++
-gRandom->SetSeed($SEED);
-generateCalibrations("$CDB",true);
-.q
-EOF
-
-else
-
-echo "Condition Database found in directory $CDBDIRECTORY. Will use it if needed."
-
-fi
-
 echo "Running simulation  ..."
 
 aliroot -b  >& testSim.out << EOF 
 gRandom->SetSeed($SEED);
-AliCDBManager::Instance()->SetDefaultStorage("$CDB");
 AliSimulation MuonSim("$ALICE_ROOT/MUON/Config.C");
 MuonSim.SetWriteRawData("MUON");
 MuonSim.Run($NEVENTS)
@@ -48,7 +27,6 @@ echo "Running reconstruction  ..."
 
 aliroot -b >& testReco.out << EOF
 gRandom->SetSeed($SEED);
-AliCDBManager::Instance()->SetDefaultStorage("$CDB");
 AliReconstruction MuonRec("galice.root")
 MuonRec.SetInput("$FULLPATH/");
 MuonRec.SetRunTracking("")
@@ -79,7 +57,7 @@ echo "Running dumps ..."
 
 if [ "$NEVENTS" -le 20 ]; then
 
-aliroot -b > /dev/null << EOF
+aliroot -b << EOF
 .L $ALICE_ROOT/MUON/MUONCheck.C++
 MUONdigits(); > check.digits
 MUONrecpoints(); > check.recpoints
