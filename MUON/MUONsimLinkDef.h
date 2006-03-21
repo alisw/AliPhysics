79051d9dccdbea9080692db9facf7b9f4c4ab3eb
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-03-21 10:04:55 +0000

    Removing not used St1 classes

--- MUON/MUONsimLinkDef.h
+++ MUON/MUONsimLinkDef.h
@@ -15,18 +15,9 @@
 #pragma link C++ class AliMUONSDigitizerV2+;  
 #pragma link C++ class AliMUONTriggerDecision+; 
 #pragma link C++ class AliMUONTriggerDecisionV1+; 
-
-
 #pragma link C++ class AliMUONTest+; 
 
 // response
-#pragma link C++ class AliMUONSt1Response+;
-#pragma link C++ class AliMUONSt1ElectronicElement+; 
-#pragma link C++ class AliMUONSt1SpecialMotif+; 
-#pragma link C++ class AliMUONSt1ResponseParameter+; 
-#pragma link C++ class AliMUONSt1ResponseRule+; 
-#pragma link C++ class AliMUONSt1IniReader+; 
-#pragma link C++ namespace decoder; 
 #pragma link C++ class AliMUONResponse+; 
 #pragma link C++ class AliMUONResponseV0+;
 #pragma link C++ class AliMUONResponseTrigger+; 
