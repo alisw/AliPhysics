d65d075ce21e353bc76b90e5105a8047199267eb
arcelli <arcelli@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-03-28 15:11:15 +0000

    calib+alignment classes moved/added to base libs

--- TOF/TOFbaseLinkDef.h
+++ TOF/TOFbaseLinkDef.h
@@ -13,5 +13,17 @@
 #pragma link C++ class  AliTOFGeometryV5+;
 #pragma link C++ class  AliTOFdigit+;
 #pragma link C++ class  AliTOFRawStream+;
+#pragma link C++ class  AliTOFChannel+;
+#pragma link C++ class  AliTOFChSim+;
+#pragma link C++ class  AliTOFcalibESD+;
+#pragma link C++ class  AliTOFcalib+;
+#pragma link C++ class  AliTOFCalPadZ+;
+#pragma link C++ class  AliTOFCalStrip+;
+#pragma link C++ class  AliTOFCalPlateA+;
+#pragma link C++ class  AliTOFCalPlateB+;
+#pragma link C++ class  AliTOFCalPlateC+;
+#pragma link C++ class  AliTOFCalSector+;
+#pragma link C++ class  AliTOFCal+;
+#pragma link C++ class  AliTOFAlignment+;
 
 #endif
