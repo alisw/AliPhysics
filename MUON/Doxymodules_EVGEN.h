9633dac25acfeef68aa07c50765054ad11ac6d16
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-04-24 17:41:09 +0000

    Doxygen configuration file: initial version (Ivana)

---
+++ MUON/Doxymodules_EVGEN.h
@@ -0,0 +1,60 @@
+// $Id$
+// The class categories definitions for Doxygen
+
+/** @defgroup EVGEN EVGEN
+ *  Category of AliRoot event generator classes
+ *  @{
+ */
+
+    class  AliGenHIJINGpara {};
+    class  AliGenHIJINGparaBa {};
+    class  AliGenFixed {};
+    class  AliGenBox {};
+    class  AliGenThetaSlice {};
+    class  AliGenParam {};
+    class  AliGenCocktail {};
+    class  AliGenMUONCocktail {};
+    class  AliGenMUONCocktailpp {};
+    class  AliGenCocktailAfterBurner {};
+    class  AliGenCocktailEntry {};
+    class  AliGenExtFile {};
+    class  AliGenScan {};
+    class  AliGenDoubleScan {};
+    class  AliGenLib {};
+    class  AliGenMUONlib {};
+    class  AliGenRICHlib {};
+    class  AliGenFLUKAsource {};
+    class  AliGenHalo {};
+    class  AliGenHaloProtvino {};
+    class  AliDimuCombinator {};
+    class  AliGenPHOSlib {};
+    class  AliGenGSIlib {};
+    class  AliGenPMDlib {};
+    class  AliGenSTRANGElib {};
+    class  AliGenMC {};
+    class  AliGenReader {};
+    class  AliGenReaderCwn+ {};
+    class  AliGenReaderTreeK+ {};
+    class  AliGenReaderEcalHijing+ {};
+    class  AliGenReaderEcalJets+ {};
+    class  AliGenGeVSim {};
+    class  AliGenGeVSimEventHeader {};
+    class  AliGeVSimParticle {};
+    class  AliGenBeamGas {};
+    class  AliGenAfterBurnerFlow {};
+    class  AliStructFuncType {};
+    class  AliGenSlowNucleons {};
+    class  AliSlowNucleonModel {};
+    class  AliSlowNucleonModelExp {};
+    class  AliGenHBTosl {};
+    class  AliGenCocktailEventHeader {};
+    class  AliGenReaderEMD {};
+    class  AliDecayerPolarized {};
+
+/** @} */
+
+
+
+
+
+
