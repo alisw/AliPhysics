fd19d9872ef8279b97e6ce87ff13ea41b9cc5418
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-05-06 18:57:40 +0000

    Removing obsolete TRD calls

--- macros/ConfigHBT.C
+++ macros/ConfigHBT.C
@@ -411,12 +411,6 @@ void Config()
         //=================== TRD parameters ============================
 
         AliTRD *TRD = new AliTRDv1("TRD", "TRD slow simulator");
-
-        // Select the gas mixture (0: 97% Xe + 3% isobutane, 1: 90% Xe + 10% CO2)
-        TRD->SetGasMix(1);
-
-        // Switch on TR
-        AliTRDsim *TRDsim = TRD->CreateTR();
     }
 
     if (iFMD)
