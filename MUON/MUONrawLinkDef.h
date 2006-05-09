e66801fb9512aeda2f10567b141041aeb6a83d25
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-05-09 15:18:11 +0000

    Library for raw data reading & decoding (Christian)

---
+++ MUON/MUONrawLinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// raw data
+#pragma link C++ class AliMUONDDLTrigger+;
+#pragma link C++ class AliMUONDarcHeader+;
+#pragma link C++ class AliMUONRegHeader+;
+#pragma link C++ class AliMUONLocalStruct+;
+#pragma link C++ class AliMUONRawStreamTrigger+;
+#pragma link C++ class AliMUONDDLTracker+;
+#pragma link C++ class AliMUONDspHeader+;
+#pragma link C++ class AliMUONBlockHeader+;
+#pragma link C++ class AliMUONBusStruct+;
+#pragma link C++ class AliMUONRawStreamTracker+;
+
+#endif
+
+
