fea02987ad6272de64d83a7bf475b121dd735f5b
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-05-17 10:36:33 +0000

    This commit was generated by cvs2svn to compensate for changes in r13866, which included commits to RCS files with non-trunk default branches.

---
+++ HLT/PHOS/old/AliPHOSFitTest.cxx
@@ -0,0 +1,8 @@
+#include "AliPHOSFitGui.h"
+#include <stdio.h>
+
+AliPHOSFitGui::AliPHOSFitGui()
+{
+  printf("\nthis is a dummy fitgui\n");
+}
+    
---
+++ HLT/PHOS/old/AliPHOSFitTest.h
@@ -0,0 +1,10 @@
+#ifndef ALIPHOSFITTEST_H
+#define ALIPHOSFITTEST_H
+
+class AliPHOSFitGui
+  {
+  public:
+    AliPHOSFitGui();
+  };
+
+#endif
