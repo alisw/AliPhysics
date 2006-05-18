7d3e7201f8d3856b441b54ac38568d9b87c6bb31
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-05-18 10:09:15 +0000

    Added selector for dNdEtaAnalysis, use with testAnalysis2.C

---
+++ PWG0/dNdEta/AlidNdEtaAnalysisSelector.h
@@ -0,0 +1,35 @@
+#ifndef ALIDNDETAANALYSISSELECTOR_H
+#define ALIDNDETAANALYSISSELECTOR_H
+
+#include "../AliSelector.h"
+
+class AliESDtrackCuts;
+class dNdEtaCorrection;
+class dNdEtaAnalysis;
+
+class TParticle;
+
+class AlidNdEtaAnalysisSelector : public AliSelector {
+  public:
+    AlidNdEtaAnalysisSelector(TTree *tree=0);
+    virtual ~AlidNdEtaAnalysisSelector();
+
+    virtual void    SlaveBegin(TTree *tree);
+    virtual Bool_t  Process(Long64_t entry);
+    virtual void    SlaveTerminate();
+    virtual void    Terminate();
+
+ protected:
+  AliESDtrackCuts*  fEsdTrackCuts;          // Object containing the parameters of the esd track cuts
+
+  dNdEtaAnalysis* fdNdEtaAnalysis;      // contains the intermediate histograms (on each slave)
+
+  dNdEtaCorrection* fdNdEtaCorrection; // correction map
+  dNdEtaAnalysis* fdNdEtaAnalysisFinal; // contains the final histograms
+
+ private:
+
+  ClassDef(AlidNdEtaAnalysisSelector, 0);
+};
+
+#endif
---
+++ PWG0/dNdEta/testAnalysis2.C
@@ -0,0 +1,39 @@
+//
+// Script to test the dN/dEta analysis using the dNdEtaAnalysis and
+// dNdEtaCorrection classes. Note that there is a cut on the events,
+// so the measurement will be biassed.
+//
+// implementation with TSelector
+//
+
+#include "../CreateESDChain.C"
+
+testAnalysis2(Char_t* dataDir, Int_t nRuns=20)
+{
+  gSystem->Load("libPWG0base");
+
+  TChain* chain = CreateESDChain(dataDir, nRuns);
+
+  // ########################################################
+  // selection of esd tracks
+  AliESDtrackCuts* esdTrackCuts = new AliESDtrackCuts();
+  esdTrackCuts->DefineHistograms(1);
+
+  esdTrackCuts->SetMinNClustersTPC(50);
+  esdTrackCuts->SetMaxChi2PerClusterTPC(3.5);
+  esdTrackCuts->SetMaxCovDiagonalElements(2,2,0.5,0.5,2);
+  esdTrackCuts->SetRequireTPCRefit(kTRUE);
+
+  esdTrackCuts->SetMinNsigmaToVertex(3);
+  esdTrackCuts->SetAcceptKingDaughters(kFALSE);
+
+  chain->GetUserInfo()->Add(esdTrackCuts);
+
+  dNdEtaCorrection* dNdEtaCorrection = new dNdEtaCorrection();
+  dNdEtaCorrection->LoadHistograms("correction_map.root","dndeta_correction");
+  dNdEtaCorrection->RemoveEdges(2,0,2);
+
+  chain->GetUserInfo()->Add(dNdEtaCorrection);
+
+  chain->Process("AlidNdEtaAnalysisSelector.cxx+");
+}
