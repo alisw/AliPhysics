6aefa51182678a3b2c49041b3874792d4df2d3a4
dibari <dibari@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-05-24 11:54:03 +0000

    Right values for misalignement randomly distributed according to a gaussian

--- RICH/RichAlign.C
+++ RICH/RichAlign.C
@@ -1,6 +1,6 @@
-void RichAlign(Float_t sigmaTrans=0, Float_t sigmaRot=0)
+void RichAlign(Float_t sigmaTrans=0.1, Float_t sigmaRot=0.001)
 {
-  Float_t dX, dY, dX;          Float_t dPsi, dTheta, dPhi;   //displacements
+  Float_t dX, dY, dZ;          Float_t dPsi, dTheta, dPhi;   //displacements
 
   TClonesArray *pCA = new TClonesArray("AliAlignObjMatrix",10000);
   
@@ -10,8 +10,8 @@ void RichAlign(Float_t sigmaTrans=0, Float_t sigmaRot=0)
  
   Int_t idRICH =  AliAlignObj::kRICH;
   for (Int_t iCh = 0; iCh < 7; iCh++) {
-    dX     = (pRnd->Uniform()-0.5)*sigmaTrans;    dY     = (pRnd->Uniform()-0.5)*sigmaTrans;    dZ     = (pRnd->Uniform()-0.5)*sigmaTrans;
-    dPsi   = (pRnd->Uniform()-0.5)*sigmaRot;    dTheta = (pRnd->Uniform()-0.5)*sigmaRot;    dPhi   = (pRnd->Uniform()-0.5)*sigmaRot;
+    dX     = pRnd->Gaus(0,sigmaTrans);    dY     = pRnd->Gaus(0,sigmaTrans);    dZ     = pRnd->Gaus(0,sigmaTrans);
+    dPsi   = pRnd->Gaus(0,sigmaRot);      dTheta = pRnd->Gaus(0,sigmaRot);      dPhi   = pRnd->Gaus(0,sigmaRot);
     new((*pCA)[iCh]) AliAlignObjMatrix(AliAlignObj::GetVolPath(idRICH,iCh), AliAlignObj::LayerToVolUID(idRICH,iCh),dX,dY,dZ,dPsi,dTheta,dPhi);
   }
 
