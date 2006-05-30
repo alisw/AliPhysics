fad5c2bace2d86a1d314743f1dcfab905d18a543
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-05-30 22:32:24 +0000

    adding vertex recon eff selector

---
+++ PWG0/dNdEta/AlidNdEtaVertexRecEffSelector.h
@@ -0,0 +1,38 @@
+/* $Id$ */
+
+#ifndef ALIDNDETAVERTEXRECEFFSELECTOR_H
+#define ALIDNDETAVERTEXRECEFFSELECTOR_H
+
+// This class plots the vertex reconstruction efficiency
+
+#include "AliSelector.h"
+
+class TH1F;
+
+class AlidNdEtaVertexRecEffSelector : public AliSelector {
+  public:
+    AlidNdEtaVertexRecEffSelector();
+    virtual ~AlidNdEtaVertexRecEffSelector();
+
+    virtual void    SlaveBegin(TTree *tree);
+    virtual Bool_t  Process(Long64_t entry);
+    virtual void    SlaveTerminate();
+    virtual void    Terminate();
+
+ protected:
+    static const Float_t fkEtaRange;
+
+    Bool_t CheckVertex();
+
+    TH1F* fdNGen;  //! generated multiplicity
+    TH1F* fdNRec;  //! generated multiplicity of events with reconstructed vertex
+
+    TH1F* fVtxGen;  //! generated vertex z 
+    TH1F* fVtxRec;  //! generated vertex z of events with reconstructed vertex
+
+ private:
+
+  ClassDef(AlidNdEtaVertexRecEffSelector, 0);
+};
+
+#endif
