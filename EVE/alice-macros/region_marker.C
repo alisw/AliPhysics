b12189f2c301aa863d098af8d5fe9ecbf3cd7827
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-06-06 18:34:35 +0000

    Added separate y, z box dimensions.

--- EVE/alice-macros/region_marker.C
+++ EVE/alice-macros/region_marker.C
@@ -1,20 +1,21 @@
-// $Header
+// $Header$
 
-void region_marker(Float_t a=10, Float_t x=0, Float_t y=0, Float_t z=0)
+void region_marker(Float_t a=10, Float_t b=10, Float_t c=20,
+		   Float_t x=0, Float_t y=0, Float_t z=0)
 {
   TPolyMarker3D* mark = new TPolyMarker3D(8);
   mark->SetName("Origin marker");
   mark->SetMarkerStyle(3);
   mark->SetMarkerColor(6);
-  mark->SetPoint(0, x+a, y+a, z+a);
-  mark->SetPoint(1, x+a, y-a, z+a);
-  mark->SetPoint(2, x-a, y-a, z+a);
-  mark->SetPoint(3, x-a, y+a, z+a);
+  mark->SetPoint(0, x+a, y+b, z+c);
+  mark->SetPoint(1, x+a, y-b, z+c);
+  mark->SetPoint(2, x-a, y-b, z+c);
+  mark->SetPoint(3, x-a, y+b, z+c);
 
-  mark->SetPoint(4, x+a, y+a, z-a);
-  mark->SetPoint(5, x+a, y-a, z-a);
-  mark->SetPoint(6, x-a, y+a, z-a);
-  mark->SetPoint(7, x-a, y-a, z-a);
+  mark->SetPoint(4, x+a, y+b, z-c);
+  mark->SetPoint(5, x+a, y-b, z-c);
+  mark->SetPoint(6, x-a, y+b, z-c);
+  mark->SetPoint(7, x-a, y-b, z-c);
 
   using namespace Reve;
   Color_t* colp = FindColorVar(mark, "fMarkerColor");
