f9e82959ee16d446660d03877b112f7e6a8df4ee
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-06-12 19:30:36 +0000

    Take track-parameters at inner TPC radius if extrapolation to ITS failed (controlled by global Bool_t gkFixFailedITSExtr); reformat / group common behaviour.

--- EVE/alice-macros/esd_tracks.C
+++ EVE/alice-macros/esd_tracks.C
@@ -1,12 +1,38 @@
 // $Id$
 
+Reve::Track* esd_make_track(Reve::TrackRnrStyle* rnrStyle,
+			    AliESDtrack* at,
+			    AliExternalTrackParam* tp=0)
+{
+  Double_t        pbuf[3], vbuf[3];
+  Reve::RecTrack  rt;
+
+  if(tp == 0) tp = at;
+
+  rt.label  =         at->GetLabel();
+  rt.status = (Int_t) at->GetStatus();
+  rt.sign   = tp->GetSign();
+  tp->GetXYZ(vbuf);
+  rt.V.Set(vbuf);
+  tp->GetPxPyPz(pbuf);
+  rt.P.Set(pbuf);
+  Double_t ep = at->GetP(), mc = at->GetMass();
+  rt.beta = ep/TMath::Sqrt(ep*ep + mc*mc);
+ 
+  Reve::Track* track = new Reve::Track(&rt, rnrStyle);
+  track->SetName(Form("ESDTrack %d", rt.label));
+  track->SetTitle(Form("pT=%.3f, pZ=%.3f; V=(%.3f, %.3f, %.3f)",
+		       rt.sign*TMath::Hypot(rt.P.x, rt.P.y), rt.P.z,
+		       rt.V.x, rt.V.y, rt.V.z));
+  return track;
+}
+
+Bool_t gkFixFailedITSExtr = kTRUE;
+
 Reve::TrackList* esd_tracks(Double_t min_pt=0.1, Double_t max_pt=100)
 {
   AliESD* esd = Alieve::Event::AssertESD();
-
-  AliESDtrack*    at;
-  Reve::RecTrack  rt;
-  Double_t        pbuf[3], vbuf[3];
+  AliPID  pid;
 
   Double_t minptsq = min_pt*min_pt;
   Double_t maxptsq = max_pt*max_pt;
@@ -17,12 +43,15 @@ Reve::TrackList* esd_tracks(Double_t min_pt=0.1, Double_t max_pt=100)
 
   Reve::TrackList* cont = new Reve::TrackList("ESD Tracks"); 
   cont->SetMainColor(Color_t(6));
+  Reve::TrackRnrStyle* rnrStyle = cont->GetRnrStyle();
+  rnrStyle->SetMagField( esd->GetMagneticField() );
 
   TGListTreeItem *holder = gReve->AddRenderElement(cont);
 
-  Int_t count = 0;
+  Int_t    count = 0;
+  Double_t pbuf[3];
   for (Int_t n=0; n<esd->GetNumberOfTracks(); n++) {
-    at = esd->GetTrack(n);
+    AliESDtrack* at = esd->GetTrack(n);
 
     // Here would be sweet to have TObjectFormula.
     at->GetPxPyPz(pbuf);
@@ -32,21 +61,14 @@ Reve::TrackList* esd_tracks(Double_t min_pt=0.1, Double_t max_pt=100)
 
     ++count;
 
-    rt.label  = at->GetLabel();
-    rt.status = (Int_t) at->GetStatus();
-    rt.sign   = at->GetSign();
-    at->GetXYZ(vbuf);
-    rt.V.Set(vbuf);
-    rt.P.Set(pbuf);
-    Double_t ep = at->GetP(), mc = TMath::C()*at->GetMass();
-    rt.beta = ep/TMath::Sqrt(ep*ep + mc*mc);
+    // If ITS refit failed, take track parameters at inner TPC radius.
+    AliExternalTrackParam* tp = at;
+    if (gkFixFailedITSExtr && !at->IsOn(AliESDtrack::kITSrefit)) {
+       tp = at->GetInnerParam();
+    }
 
-    Reve::Track* track = new Reve::Track(&rt, cont->GetRnrStyle());
-    track->SetName(Form("ESDTrack %d", rt.label));
-    track->SetTitle(Form("pT=%.2f; V=(%.3f, %.3f, %.3f)",
-			TMath::Sqrt(ptsq), rt.V.x, rt.V.y, rt.V.z));
+    Reve::Track* track = esd_make_track(rnrStyle, at, tp);
     cont->AddElement(track);
-
     gReve->AddRenderElement(holder, track);
   }
 
@@ -61,28 +83,27 @@ Reve::TrackList* esd_tracks(Double_t min_pt=0.1, Double_t max_pt=100)
   return cont;
 }
 
-Reve::TrackList* esd_tracks_from_array(TCollection* col)
+Reve::TrackList* esd_tracks_from_array(TCollection* col, AliESD* esd=0)
 {
   // Retrieves AliESDTrack's from collection.
   // See example usage with AliAnalysisTrackCuts in the next function.
+
+  if(esd == 0) esd = Alieve::Event::AssertESD();
+  AliPID  pid;
 
   TGListTree*     l_tree = gReve->GetListTree();
   TGListTreeItem* parent = gReve->GetEventTreeItem();
 
   Reve::TrackList* cont = new Reve::TrackList("ESD Tracks"); 
   cont->SetMainColor(Color_t(6));
+  Reve::TrackRnrStyle* rnrStyle = cont->GetRnrStyle();
+  rnrStyle->SetMagField( esd->GetMagneticField() );
 
   TGListTreeItem *holder = gReve->AddRenderElement(cont);
-
-
-  AliESDtrack*    at;
-  Reve::RecTrack  rt;
-  Double_t        pbuf[3], vbuf[3];
 
   Int_t    count = 0;
   TIter    next(col);
   TObject *obj;
-
   while((obj = next()) != 0) {
     if(obj->IsA()->InheritsFrom("AliESDtrack") == kFALSE) {
       Warning("Object '%s', '%s' is not an AliESDtrack.",
@@ -91,23 +112,10 @@ Reve::TrackList* esd_tracks_from_array(TCollection* col)
     }
 
     ++count;
-    at = (AliESDtrack*) obj;
+    AliESDtrack* at = (AliESDtrack*) obj;
 
-    rt.label  = at->GetLabel();
-    rt.status = (Int_t) at->GetStatus();
-    rt.sign   = at->GetSign();
-    at->GetXYZ(vbuf);     rt.V.Set(vbuf);
-    at->GetPxPyPz(pbuf);  rt.P.Set(pbuf);
-    Double_t ep = at->GetP();
-    Double_t mc = TMath::C()*at->GetMass();
-    rt.beta = ep/TMath::Sqrt(ep*ep + mc*mc);
-
-    Reve::Track* track = new Reve::Track(&rt, cont->GetRnrStyle());
-    track->SetName(Form("ESDTrack %d", rt.label));
-    track->SetTitle(Form("P=(%.3f, %.3f, %.3f); V=(%.3f, %.3f, %.3f)",
-			 rt.P.x, rt.P.y, rt.P.z, rt.V.x, rt.V.y, rt.V.z));
+    Reve::Track* track = esd_make_track(rnrStyle, at);
     cont->AddElement(track);
-
     gReve->AddRenderElement(holder, track);
   }
 
@@ -125,10 +133,11 @@ Reve::TrackList* esd_tracks_from_array(TCollection* col)
 void esd_tracks_alianalcuts_demo()
 {
   AliESD* esd = Alieve::Event::AssertESD();
+  gSystem->Load("libANALYSIS");
 
   AliAnalysisTrackCuts atc;
   atc.SetPtRange(0.1, 5);
   atc.SetRapRange(-1, 1);
 
-  esd_tracks_from_array(atc.GetAcceptedParticles(esd));
+  esd_tracks_from_array(atc.GetAcceptedParticles(esd), esd);
 }
