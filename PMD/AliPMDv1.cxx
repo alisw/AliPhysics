c44ee1118fe3e548f8f4c575f6fb32f816447e1c
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-06-19 13:01:14 +0000

    Remove deprecated function

--- PMD/AliPMDv1.cxx
+++ PMD/AliPMDv1.cxx
@@ -1503,7 +1503,7 @@ void AliPMDv1::StepManager()
   Int_t   vol[10];
   //  const char *namep;
   
-  if(gMC->GetMedium() == fMedSens && (destep = gMC->Edep())) {
+  if(gMC->CurrentMedium() == fMedSens && (destep = gMC->Edep())) {
   
     gMC->CurrentVolID(copy);
     //     namep=gMC->CurrentVolName();
