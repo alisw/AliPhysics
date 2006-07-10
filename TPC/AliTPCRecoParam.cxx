0105d7e2cb77e7396f7fcf6527e4fc62a1d0d582
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-07-10 09:48:59 +0000

    Parameterization of reconstruction algorithm. Default parameters for low flux and high flux enevironment + default parameters for Cosmic and Laser tests of  TPC (Marian)

---
+++ TPC/AliTPCRecoParam.cxx
@@ -0,0 +1,112 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// Class with TPC reconstruction parameters                                  //
+//                                                                           //  
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "AliTPCRecoParam.h"
+
+ClassImp(AliTPCRecoParam)
+
+
+
+
+//_____________________________________________________________________________
+AliTPCRecoParam::AliTPCRecoParam():
+  fCtgRange(1.05),       
+  fMaxSnpTracker(0.95),
+  fMaxSnpTrack(0.999),
+  fFirstBin(0),
+  fLastBin(-1),
+  fBCalcPedestal(kFALSE),
+  fBDoUnfold(kTRUE),
+  fDumpAmplitudeMin(100),
+  fMaxNoise(3.),
+  fBKinkFinder(kTRUE)
+{
+  //
+  // constructor
+  //
+}
+
+//_____________________________________________________________________________
+AliTPCRecoParam::~AliTPCRecoParam() 
+{
+  //
+  // destructor
+  //  
+}
+
+
+
+
+AliTPCRecoParam *AliTPCRecoParam::GetLowFluxParam(){
+  //
+  // make default reconstruction  parameters for low  flux env.
+  //
+  AliTPCRecoParam *param = new AliTPCRecoParam;
+  param->fCtgRange = 10;
+  param->fFirstBin = 0;
+  param->fLastBin  = 1000;
+  return param;
+}
+
+AliTPCRecoParam *AliTPCRecoParam::GetHighFluxParam(){
+  //
+  // make reco parameters for high flux env.
+  //
+  AliTPCRecoParam *param = new AliTPCRecoParam;
+  param->fCtgRange = 1.05;
+  param->fFirstBin = 0;
+  param->fLastBin  = 1000;
+  return param;
+}
+
+AliTPCRecoParam *AliTPCRecoParam::GetLaserTestParam(Bool_t bPedestal){
+  //
+  // special setting for laser
+  //
+  AliTPCRecoParam *param = new AliTPCRecoParam;
+  param->fCtgRange = 10.05;
+  param->fFirstBin = 0;
+  param->fLastBin  = 1000;
+  param->fBCalcPedestal = bPedestal;
+  param->fBDoUnfold     = kFALSE;
+  param->fBKinkFinder   = kFALSE;
+  return param;
+}
+
+AliTPCRecoParam *AliTPCRecoParam::GetCosmicTestParam(Bool_t bPedestal){
+  //
+  // special setting for cosmic 
+  // 
+  AliTPCRecoParam *param = new AliTPCRecoParam;
+  param->fCtgRange = 10.05;    // full TPC
+  param->fFirstBin = 60;
+  param->fLastBin  = 1000;
+  param->fBCalcPedestal = bPedestal;
+  param->fBDoUnfold     = kFALSE;
+  param->fBKinkFinder   = kFALSE;
+  return param;
+}
+
+
+
---
+++ TPC/AliTPCRecoParam.h
@@ -0,0 +1,58 @@
+#ifndef ALITPCRECOPARAM_H
+#define ALITPCRECOPARAM_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// Class with TPC reconstruction parameters                                  //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+
+#include "TObject.h"
+
+class AliTPCRecoParam : public TObject
+{
+ public: 
+  AliTPCRecoParam();
+  virtual ~AliTPCRecoParam();
+  Double_t GetCtgRange() const     { return fCtgRange;}
+  Double_t GetMaxSnpTracker() const{ return fMaxSnpTracker;}
+  Double_t GetMaxSnpTrack() const  { return fMaxSnpTrack;}
+  //
+  Int_t    GetFirstBin() const     { return fFirstBin;}
+  Int_t    GetLastBin() const      { return fLastBin;}
+  void     SetTimeBinRange(Int_t first, Int_t last){ fFirstBin = first; fLastBin = last;}
+  Bool_t   GetCalcPedestal() const { return fBCalcPedestal;}
+  Bool_t   GetDoUnfold() const     { return fBDoUnfold;}
+  Float_t  GetDumpAmplitudeMin() const  { return fDumpAmplitudeMin;}
+  Float_t  GetMaxNoise() const     { return fMaxNoise;}
+  //
+  Bool_t   GetDoKinks() const      { return fBKinkFinder;}
+  static   AliTPCRecoParam *GetLowFluxParam();        // make reco parameters for low  flux env.
+  static   AliTPCRecoParam *GetHighFluxParam();       // make reco parameters for high flux env. 
+  static   AliTPCRecoParam *GetLaserTestParam(Bool_t bPedestal);  // special setting for laser 
+  static   AliTPCRecoParam *GetCosmicTestParam(Bool_t bPedestal); // special setting for cosmic  
+  //
+ protected:
+  Double_t fCtgRange;        // +-fCtgRange is the ctg(Theta) window used for clusterization and tracking (MI) 
+  Double_t fMaxSnpTracker;   // max sin of local angle  - for TPC tracker
+  Double_t fMaxSnpTrack;     // max sin of local angle  - for track 
+  //
+  //   clusterer parameters
+  //
+  Int_t    fFirstBin;        // first time bin used by cluster finder
+  Int_t    fLastBin;         // last time bin  used by cluster finder 
+  Bool_t   fBCalcPedestal;   // calculate Pedestal
+  Bool_t   fBDoUnfold;       // do unfolding of clusters
+  Float_t  fDumpAmplitudeMin; // minimal amplitude of signal to be dumped 
+  Float_t  fMaxNoise;        // maximal noise sigma on pad to be used in cluster finder
+  //
+  //
+  Bool_t   fBKinkFinder;     // do kink finder reconstruction
+  ClassDef(AliTPCRecoParam, 1)
+};
+
+
+#endif
