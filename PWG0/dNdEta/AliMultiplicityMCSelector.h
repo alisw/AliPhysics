131959ea8ac111cc242cfe92fb2f5a293c84e282
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-07-26 16:37:55 +0000

    adding multipicity from mc, and creation of correlation map for the corrections

---
+++ PWG0/dNdEta/AliMultiplicityMCSelector.h
@@ -0,0 +1,38 @@
+/* $Id$ */
+
+#ifndef ALIMULTIPLICITYMCSELECTOR_H
+#define ALIMULTIPLICITYMCSELECTOR_H
+
+#include "AliSelectorRL.h"
+
+class AliESDtrackCuts;
+class TH1F;
+class TH2F;
+
+class AliMultiplicityMCSelector : public AliSelectorRL {
+  public:
+    AliMultiplicityMCSelector();
+    virtual ~AliMultiplicityMCSelector();
+
+    virtual void    Begin(TTree* tree);
+    virtual void    SlaveBegin(TTree *tree);
+    virtual Bool_t  Process(Long64_t entry);
+    virtual void    SlaveTerminate();
+    virtual void    Terminate();
+
+ protected:
+    void ReadUserObjects(TTree* tree);
+
+    TH1F* fMultiplicityESD; // multiplicity histogram
+    TH1F* fMultiplicityMC; // multiplicity histogram
+
+    TH2F* fCorrelation; // (gene multiplicity) vs (meas multiplicity)
+
+    AliESDtrackCuts*  fEsdTrackCuts;     // Object containing the parameters of the esd track cuts
+
+ private:
+
+  ClassDef(AliMultiplicityMCSelector, 0);
+};
+
+#endif
