66c1ca8da55c26d255033128077d4d9257da882f
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-07-27 08:29:48 +0000

    adding support for PWG0dep.par PROOF archive

--- PWG0/PROOF-INF.PWG0base/SETUP.C
+++ PWG0/PROOF-INF.PWG0base/SETUP.C
@@ -1,8 +1,17 @@
 void SETUP()
 {
+   // Load some ROOT libraries
+   gSystem->Load("libEG");
+   gSystem->Load("libGeom");
+
+   // Load the ESD library
+   gSystem->Load("libESD");
+
    gSystem->Load("libPWG0base");
 
-   // Set the Inlucde paths
-   gSystem->SetIncludePath("-I$ROOTSYS/include -IPWG0base");
+   // Set the include paths
    gROOT->ProcessLine(".include PWG0base");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("PWG0base_INCLUDE", "PWG0base");
 }
