7b996c462226085e2885177a0c4183bf0263ea7e
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-08-15 15:49:29 +0000

    Removing ALIFAST

--- ALIFAST/user.C
+++
@@ -1,9 +0,0 @@
-void user() {
-  // change AliFast default parameters
-
-   gAliFast->SetSmearing(1);
-   gAliFast->SetTrackFinding(1);
-   gAliFast->MCMaker()->Save(2);     //give 2 to save all particles
-   gAliFast->TrackMaker()->Save(1);  // give 1 to save tracks
-}
-
