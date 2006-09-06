b46603107c0a4918c7deeca77762cc8cc4706d2a
ekman <ekman@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-09-06 09:30:49 +0000

    Added selector to check esd track cuts. Made AliESDtrackCuts inherit from TNamed (needed to identify the objects in the input list (PROOF)). Changed testESDtrackCuts to use the selector

---
+++ PWG0/esdTrackCuts/AliTestESDtrackCutsSelector.h
@@ -0,0 +1,35 @@
+/* $Id$ */
+
+#ifndef ALIMULTIPLICITYESDSELECTOR_H
+#define ALIMULTIPLICITYESDSELECTOR_H
+
+#include "AliSelectorRL.h"
+
+class AliESDtrackCuts;
+
+class AliTestESDtrackCutsSelector : public AliSelectorRL {
+  public:
+    AliTestESDtrackCutsSelector();
+    virtual ~AliTestESDtrackCutsSelector();
+
+    virtual void    Begin(TTree* tree);
+    virtual void    SlaveBegin(TTree *tree);
+    virtual Bool_t  Process(Long64_t entry);
+    virtual void    SlaveTerminate();
+    virtual void    Terminate();
+
+ protected:
+    void ReadUserObjects(TTree* tree);
+
+    AliESDtrackCuts*  fEsdTrackCutsAll;  // esd track cuts for all tracks   
+    AliESDtrackCuts*  fEsdTrackCutsPri;  // cuts for tracks from primary particles 
+    AliESDtrackCuts*  fEsdTrackCutsSec;  // cuts for tracks from secondary particles 
+
+ private:
+    AliTestESDtrackCutsSelector(const AliTestESDtrackCutsSelector&);
+    AliTestESDtrackCutsSelector& operator=(const AliTestESDtrackCutsSelector&);
+
+  ClassDef(AliTestESDtrackCutsSelector, 0);
+};
+
+#endif
