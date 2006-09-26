d005e04c6e3ce4d4cbdf23793ad3d87c8b1f7c9a
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-09-26 18:54:45 +0000

    Properly update items of render-elements; call gReve::Redraw3D() instead of pad modified/update.

--- EVE/Reve/RGEditor.cxx
+++ EVE/Reve/RGEditor.cxx
@@ -1,6 +1,8 @@
 // $Header$
 
 #include "RGEditor.h"
+#include "RenderElement.h"
+#include "RGTopFrame.h"
 
 #include <TGedFrame.h>
 #include <TGCanvas.h>
@@ -17,17 +19,26 @@ ClassImp(RGEditor)
 RGEditor::RGEditor(TCanvas* canvas) : TGedEditor(canvas)
 {}
 
+void RGEditor::DisplayRenderElement(RenderElement* re)
+{
+  fRnrElement = re;
+  TObject* obj = fRnrElement ? fRnrElement->GetObject() : 0;
+  SetModel(fPad, obj, kButton1Down);
+}
+
 void RGEditor::DisplayObject(TObject* obj)
 {
+  fRnrElement = 0;
   SetModel(fPad, obj, kButton1Down);
 }
 
 void RGEditor::Update(TGedFrame* /*gframe*/)
 {
-  // Copy from TGeEditor ... need to do something better now.
-  if (fPad) {
-      fPad->Modified();
-      fPad->Update();
-   }
+  // Virtual method from TGedEditor ... called on every change.
 
+  if (fRnrElement) {
+    fRnrElement->UpdateItems();
+  }
+
+  gReve->Redraw3D();
 }
--- EVE/Reve/RGEditor.h
+++ EVE/Reve/RGEditor.h
@@ -7,17 +7,21 @@
 
 namespace Reve {
 
+class RenderElement;
+
 class RGEditor : public TGedEditor
 {
 protected:
+  RenderElement* fRnrElement;
 
 public:
   RGEditor(TCanvas* canvas=0);
   virtual ~RGEditor() {}
   
+  void DisplayRenderElement(RenderElement* re);
   void DisplayObject(TObject* obj);
 
-  virtual void Update(TGedFrame* gframe);
+  virtual void Update(TGedFrame* gframe=0);
 
   ClassDef(RGEditor, 1);
 }; // endclass RGEditor
