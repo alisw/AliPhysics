6e5710cd32402ecc3e463e132090a0f61423be53
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-10-04 20:51:15 +0000

    Example of PHOS shuttle usage (B.Polichtchouk)

---
+++ PHOS/macros/Shuttle/AliPHOSCalibHistoProducer.C
@@ -0,0 +1,16 @@
+void AliPHOSCalibHistoProducer(const char* file="2006run2211.root") 
+{
+  // Script is to be run at DAQ computers (LDC, GDC, HLT or MOOD);
+  // it fills the histograms with amplitudes per channel.
+  // These histograms will be processed in the Shuttle preprocessor
+  // e.g., to extract mean amplitudes needed for relative channel calibration
+  //
+  // This example assumes that the input data is supplied from the
+  // raw data file in the ROOT format.
+  //
+  // Author: Boris Polichtchouk, 4 October 2006
+
+  AliRawReaderRoot* rf = new AliRawReaderRoot(file);
+  AliPHOSCalibHistoProducer hp(rf);
+  hp.Run();
+}
