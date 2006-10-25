ed761d6e7b9d733f44412d1218894a14dffc732c
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-10-25 16:21:16 +0000

    Added function 'make_vertex_boxes()' that creates boxes around vertex region to be used for visual determination of multiplicity.

--- EVE/alice-macros/primary_vertex.C
+++ EVE/alice-macros/primary_vertex.C
@@ -9,11 +9,34 @@ TPolyMarker3D* make_vertex_marker(AliESDVertex* v, const Text_t* name)
   printf("%s: %f %f %f   -   %f %f %f\n", name,
 	 x[0], x[1], x[2], e[0], e[1], e[2]);
 
-  TPolyMarker3D* m = new TPolyMarker3D(8);
+  TPolyMarker3D* m = new TPolyMarker3D(1);
   m->SetName(name);
   m->SetPoint(0, x[0], x[1], x[2]);
 
   return m;
+}
+
+Reve::BoxSet* make_vertex_boxes(AliESDVertex* v)
+{
+  Double_t x[3], e[3];
+  v->GetTruePos(x);
+  v->GetSigmaXYZ(e);
+
+  Reve::BoxSet* bs;
+
+  bs = new BoxSet("+- 10 x 10 x 20mm");
+  bs->SetRenderMode(Reve::BoxSet::RM_Line);
+  bs->AddBox(Reve::Box(-1, x[0], x[1], x[2], 1, 1, 2));
+  bs->SetMainColor((Color_t) 2);
+  gReve->AddRenderElement(bs);
+
+  bs = new BoxSet("+- 30 sigma_r x 10 sigma_z");
+  bs->SetRenderMode(Reve::BoxSet::RM_Line);
+  bs->AddBox(Reve::Box(-1, x[0], x[1], x[2], 30*e[0], 30*e[1], 10*e[2]));
+  bs->SetMainColor((Color_t) 3);
+  gReve->AddRenderElement(bs);
+
+  gReve->Redraw3D();
 }
 
 void register_vertex_marker(TPolyMarker3D* m)
@@ -25,7 +48,7 @@ void register_vertex_marker(TPolyMarker3D* m)
   gReve->Redraw3D();
 }
 
-void primary_vertex(Bool_t showSPD=kTRUE)
+void primary_vertex(Bool_t showSPD=kTRUE, Bool_t showBoxes=kFALSE)
 {
   AliESD* esd = Alieve::Event::AssertESD();
 
@@ -42,4 +65,7 @@ void primary_vertex(Bool_t showSPD=kTRUE)
     spdvm->SetMarkerColor(7);
     register_vertex_marker(spdvm);
   }
+
+  if(showBoxes)
+    make_vertex_boxes(pv);
 }
