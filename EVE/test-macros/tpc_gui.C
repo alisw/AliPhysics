6aa0783e6635b7a278262d5d3158d70940e513f7
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-10-26 13:24:33 +0000

    Added short usgae notice.

--- EVE/test-macros/tpc_gui.C
+++ EVE/test-macros/tpc_gui.C
@@ -1,6 +1,13 @@
 // $Header$
 
 // Function to spawn a gui for reading rootified raw-data from TPC sector test.
+//
+// To use:
+// a) select TPCLoader entry in the list-tree view;
+//    you'll get a dialog to steer the data-loading process in an adjacent window
+// b) to select a ROOT file containing the raw-data double-click on 'File:'
+//    text entry to spawn a file-dialog or type in the name
+// c) click open to actually open the file and load an event
 
 #ifdef __CINT__
 
