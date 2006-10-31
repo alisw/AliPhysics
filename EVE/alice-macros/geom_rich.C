2259b898f0dca0272b805b704e83939bf1775709
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-10-31 09:22:43 +0000

    Put all modules in common container.

--- EVE/alice-macros/geom_rich.C
+++ EVE/alice-macros/geom_rich.C
@@ -6,12 +6,15 @@ void geom_rich()
 
   gGeoManager = gReve->GetGeometry("$REVESYS/alice-data/alice_fullgeo.root");
 
+  Reve::RenderElementList* list = new Reve::RenderElementList("RICH");
+  gReve->AddGlobalRenderElement(list);
+
   for(Int_t i=1; i<=7; ++i) {
     TGeoNode* node = gGeoManager->GetTopVolume()->FindNode(Form("RICH_%d", i));
 
     Reve::GeoTopNodeRnrEl* re = new Reve::GeoTopNodeRnrEl(gGeoManager, node);
     re->SetUseNodeTrans(kTRUE);
-    gReve->AddGlobalRenderElement(re);
+    gReve->AddGlobalRenderElement(list, re);
   }
 
   gReve->Redraw3D();
