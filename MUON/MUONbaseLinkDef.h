c54b7e0ed3664712973304cba5518b5910719777
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-03 15:24:46 +0000

    Removed AliMUONSt345SlatSegmentationV2 class

--- MUON/MUONbaseLinkDef.h
+++ MUON/MUONbaseLinkDef.h
@@ -10,7 +10,6 @@
 #pragma link C++ class AliMUONSegFactory+;
 #pragma link C++ class AliMUONSt12QuadrantSegmentation+; 
 #pragma link C++ class AliMUONSt345SlatSegmentation+;
-#pragma link C++ class AliMUONSt345SlatSegmentationV2+;
 #pragma link C++ class AliMUONTriggerSegmentation+;
 
 // geometry 
