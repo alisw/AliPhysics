864a026d7ba9b1b098623677485cdf27785a15be
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-06 08:27:17 +0000

    added histograms for dndeta from triggered, triggered + vertex event sample

--- PWG0/dNdEta/AlidNdEtaAnalysisMCSelector.h
+++ PWG0/dNdEta/AlidNdEtaAnalysisMCSelector.h
@@ -23,12 +23,13 @@ class AlidNdEtaAnalysisMCSelector : public AliSelectorRL {
  protected:
 
  private:
-    dNdEtaAnalysis* fdNdEtaAnalysis;      // contains the intermediate histograms (on each slave)
+    dNdEtaAnalysis* fdNdEtaAnalysis;      // contains the dndeta from the full sample
+    dNdEtaAnalysis* fdNdEtaAnalysisTr;      // contains the dndeta from the triggered events
+    dNdEtaAnalysis* fdNdEtaAnalysisTrVtx;      // contains the dndeta from the triggered events with vertex
 
     TH3F* fVertex;  //! vertex of counted particles
-    TH1F* fPartEta; //! counted particles as function of eta
+    TH1F* fPartEta[3]; //! counted particles as function of eta (full vertex range, below 0 range, above 0 range)
     TH1F* fPartPt; //! counted particles as function of pt
-    Int_t fEvents;  //! number of processed events
 
     AlidNdEtaAnalysisMCSelector(const AlidNdEtaAnalysisMCSelector&);
     AlidNdEtaAnalysisMCSelector& operator=(const AlidNdEtaAnalysisMCSelector&);
