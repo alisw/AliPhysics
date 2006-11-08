8ad70a890ea19430eceb92a4900fb3648b5d1ff4
ekman <ekman@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-08 14:59:42 +0000

    updated ROC ESD analysis classes (new hist + functionality)

--- PWG0/TPC/AliTPCClusterHistograms.cxx
+++ PWG0/TPC/AliTPCClusterHistograms.cxx
@@ -1,10 +1,12 @@
 /* $Id$ */
 
-
+#include <TStyle.h>
 #include <TFile.h>
 #include <TCanvas.h>
 #include <TH2F.h>
 #include <TProfile2D.h>
+#include <TLatex.h>
+
 
 #include <../TPC/AliTPCclusterMI.h>
 
@@ -25,14 +27,21 @@ AliTPCClusterHistograms::AliTPCClusterHistograms()
   fhQmaxProfileYVsRow(0), 
   fhQtotProfileYVsRow(0),
   fhSigmaYProfileYVsRow(0),
-  fhSigmaZProfileYVsRow(0)
+  fhSigmaZProfileYVsRow(0),
+  fhQmaxProfileZVsRow(0), 
+  fhQtotProfileZVsRow(0),
+  fhSigmaYProfileZVsRow(0),
+  fhSigmaZProfileZVsRow(0),
+  fhQtotVsTime(0),  
+  fhQmaxVsTime(0)
 {
   // default constructor
 }
 
 //____________________________________________________________________
-AliTPCClusterHistograms::AliTPCClusterHistograms(const Char_t* name, const Char_t* title) 
-  : TNamed(name, title),
+//AliTPCClusterHistograms::AliTPCClusterHistograms(const Char_t* name, const Char_t* title) 
+AliTPCClusterHistograms::AliTPCClusterHistograms(Int_t detector, const Char_t* comment, Int_t timeStart, Int_t timeStop)
+  : TNamed(),
   fhQmaxVsRow(0),          
   fhQtotVsRow(0),          
   fhSigmaYVsRow(0),        
@@ -40,23 +49,79 @@ AliTPCClusterHistograms::AliTPCClusterHistograms(const Char_t* name, const Char_
   fhQmaxProfileYVsRow(0), 
   fhQtotProfileYVsRow(0),
   fhSigmaYProfileYVsRow(0),
-  fhSigmaZProfileYVsRow(0)
+  fhSigmaZProfileYVsRow(0),
+  fhQmaxProfileZVsRow(0), 
+  fhQtotProfileZVsRow(0),
+  fhSigmaYProfileZVsRow(0),
+  fhSigmaZProfileZVsRow(0),
+  fhQtotVsTime(0),  
+  fhQmaxVsTime(0)
 {
-  // constructor initializing tnamed
+  // constructor 
 
-  #define BINNING_Y 100, -50, 50
+  // make name and title
 
-  fhQmaxVsRow  = new TH2F("QmaxVsPadRow", "Qmax vs. pad row;Pad row;Qmax", 91, -0.5, 90.5, 301, -0.5, 300.5);
-  fhQtotVsRow  = new TH2F("QtotVsPadRow", "Qtot vs. pad row;Pad row;Qtot", 91, -0.5, 90.5, 100,  0,  1000);
+  if (detector < 0 || detector >= 72) {
+    AliDebug(AliLog::kError, Form("Detector %d does not exist", detector));
+    return;
+  }
+      
+  Int_t sector = detector%18;
+  TString side;
+  TString inout;
+  if (detector<18 || ( detector>=36 && detector<54))
+    side.Form("A");
+  else 
+    side.Form("B");
   
-  fhSigmaYVsRow = new TH2F("SigmaYVsPadRow", "Sigma Y vs. pad row;Pad row;#sigma_{Y}", 91, -0.5, 90.5, 100,  0,  0.5);
-  fhSigmaZVsRow = new TH2F("SigmaZVsPadRow", "Sigma Z vs. pad row;Pad row;#sigma_{Z}", 91, -0.5, 90.5, 100,  0,  0.5);
-  
-  fhQmaxProfileYVsRow = new TProfile2D("QmaxMeanYVsPadRow","Mean Qmax, y vs pad row;Pad row;y",91,-0.5,90.5, BINNING_Y);
-  fhQtotProfileYVsRow = new TProfile2D("QtotMeanYVsPadRow","Mean Qtot, y vs pad row;Pad row;y",91,-0.5,90.5, BINNING_Y);
-  fhSigmaYProfileYVsRow = new TProfile2D("SigmaYMeanYVsPadRow","Mean Sigma y, x vs pad row;Pad row;y",91,-0.5,90.5, BINNING_Y);
-  fhSigmaZProfileYVsRow = new TProfile2D("SigmaZMeanYVsPadRow","Mean Sigma y, x vs pad row;Pad row;y",91,-0.5,90.5, BINNING_Y);
+  if (detector<36)
+    inout.Form("IROC");
+  else 
+    inout.Form("OROC");
 
+  TString name;
+  name.Form("sector_%s%d_%s", side.Data(), sector, inout.Data());
+  
+  SetName(name);
+  SetTitle(Form("%s (detector %d)",name.Data(), detector));
+
+  fTimeStart = timeStart;
+  fTimeStop  = timeStop;
+  
+  #define BINNING_Z 250, 0, 250
+  
+  Float_t yRange   = 45;
+  Int_t nPadRows   = 96;
+  
+  if (TString(name).Contains("IROC")) {
+    yRange = 25;
+    nPadRows = 63;
+  }
+  
+  // 1 bin for each 0.5 cm
+  Int_t nBinsY = Int_t(4*yRange);
+
+  fhQmaxVsRow  = new TH2F("QmaxVsPadRow", "Qmax vs. pad row;Pad row;Qmax", nPadRows+2, -1.5, nPadRows+0.5, 301, -0.5, 300.5);
+  fhQtotVsRow  = new TH2F("QtotVsPadRow", "Qtot vs. pad row;Pad row;Qtot", nPadRows+2, -1.5, nPadRows+0.5, 400,  0,  4000);
+  
+  fhSigmaYVsRow = new TH2F("SigmaYVsPadRow", "Sigma Y vs. pad row;Pad row;#sigma_{Y}", nPadRows+2, -1.5, nPadRows+0.5, 100,  0,  0.5);
+  fhSigmaZVsRow = new TH2F("SigmaZVsPadRow", "Sigma Z vs. pad row;Pad row;#sigma_{Z}", nPadRows+2, -1.5, nPadRows+0.5, 100,  0,  0.5);
+  
+  fhQmaxProfileYVsRow = new TProfile2D("MeanQmaxYVsPadRow","Mean Qmax, y vs pad row;Pad row;y",nPadRows+2, -1.5, nPadRows+0.5, nBinsY, -yRange, yRange);
+  fhQtotProfileYVsRow = new TProfile2D("MeanQtotYVsPadRow","Mean Qtot, y vs pad row;Pad row;y",nPadRows+2, -1.5, nPadRows+0.5, nBinsY, -yRange, yRange);
+  fhSigmaYProfileYVsRow = new TProfile2D("MeanSigmaYVsPadRow","Mean Sigma y, y vs pad row;Pad row;y",nPadRows+2, -1.5, nPadRows+0.5, nBinsY, -yRange, yRange);
+  fhSigmaZProfileYVsRow = new TProfile2D("MeanSigmaYVsPadRow","Mean Sigma y, y vs pad row;Pad row;y",nPadRows+2, -1.5, nPadRows+0.5, nBinsY, -yRange, yRange);
+
+  fhQmaxProfileZVsRow = new TProfile2D("MeanQmaxZVsPadRow","Mean Qmax, z vs pad row;Pad row;z",nPadRows+2, -1.5, nPadRows+0.5, BINNING_Z);
+  fhQtotProfileZVsRow = new TProfile2D("MeanQtotZVsPadRow","Mean Qtot, z vs pad row;Pad row;z",nPadRows+2, -1.5, nPadRows+0.5, BINNING_Z);
+  fhSigmaYProfileZVsRow = new TProfile2D("MeanSigmaZVsPadRow","Mean Sigma y, z vs pad row;Pad row;z",nPadRows+2, -1.5, nPadRows+0.5, BINNING_Z);
+  fhSigmaZProfileZVsRow = new TProfile2D("MeanSigmaZVsPadRow","Mean Sigma y, z vs pad row;Pad row;z",nPadRows+2, -1.5, nPadRows+0.5, BINNING_Z);
+
+  Int_t nTimeBins  = 100;
+  
+  fhQtotVsTime = new TProfile("MeanQtotVsTime", "Mean Qtot vs. event time stamp; time; Qtot",nTimeBins, fTimeStart, fTimeStop);
+  fhQmaxVsTime = new TProfile("MeanQmaxVsTime", "Mean Qmax vs. event time stamp; time; Qmax",nTimeBins, fTimeStart, fTimeStop);
+  
 }
 
 //____________________________________________________________________
@@ -105,7 +170,31 @@ AliTPCClusterHistograms::~AliTPCClusterHistograms()
     delete fhSigmaZProfileYVsRow;
     fhSigmaZProfileYVsRow = 0;
   }
+  if (fhQmaxProfileZVsRow) {
+    delete fhQmaxProfileZVsRow;
+    fhQmaxProfileZVsRow = 0;
+  }
+  if (fhQtotProfileZVsRow) {
+    delete fhQtotProfileZVsRow;
+    fhQtotProfileZVsRow = 0;
+  }
+  if (fhSigmaYProfileZVsRow) {
+    delete fhSigmaYProfileZVsRow;
+    fhSigmaYProfileZVsRow = 0;
+  }
+  if (fhSigmaZProfileZVsRow) {
+    delete fhSigmaZProfileZVsRow;
+    fhSigmaZProfileZVsRow = 0;
+  }
 
+  if (fhQtotVsTime) {
+    delete fhQtotVsTime;
+    fhQtotVsTime = 0;
+  }
+  if (fhQmaxVsTime) {
+    delete fhQmaxVsTime;
+    fhQmaxVsTime = 0;
+  }
 }
 
 //____________________________________________________________________
@@ -147,13 +236,20 @@ Long64_t AliTPCClusterHistograms::Merge(TCollection* list)
   TList* collectionSigmaYProfileYVsRow  = new TList;
   TList* collectionSigmaZProfileYVsRow  = new TList;
 
+  TList* collectionQmaxProfileZVsRow    = new TList;
+  TList* collectionQtotProfileZVsRow    = new TList;
+  TList* collectionSigmaYProfileZVsRow  = new TList;
+  TList* collectionSigmaZProfileZVsRow  = new TList;
+
+  TList* collectionQtotVsTime  = new TList;
+  TList* collectionQmaxVsTime  = new TList;
+
    Int_t count = 0;
    while ((obj = iter->Next())) {
     
      AliTPCClusterHistograms* entry = dynamic_cast<AliTPCClusterHistograms*> (obj);
      if (entry == 0) 
        continue;
-
 
      collectionQmaxVsRow          ->Add(entry->fhQmaxVsRow	   );
      collectionQtotVsRow	  ->Add(entry->fhQtotVsRow	   );
@@ -164,6 +260,15 @@ Long64_t AliTPCClusterHistograms::Merge(TCollection* list)
      collectionQtotProfileYVsRow  ->Add(entry->fhQtotProfileYVsRow );
      collectionSigmaYProfileYVsRow->Add(entry->fhSigmaYProfileYVsRow);
      collectionSigmaZProfileYVsRow->Add(entry->fhSigmaZProfileYVsRow);
+
+     collectionQmaxProfileZVsRow  ->Add(entry->fhQmaxProfileZVsRow );
+     collectionQtotProfileZVsRow  ->Add(entry->fhQtotProfileZVsRow );
+     collectionSigmaYProfileZVsRow->Add(entry->fhSigmaYProfileZVsRow);
+     collectionSigmaZProfileZVsRow->Add(entry->fhSigmaZProfileZVsRow);
+
+     collectionQtotVsTime->Add(entry->fhQtotVsTime);
+     collectionQmaxVsTime->Add(entry->fhQmaxVsTime);
+
 
      count++;
    }
@@ -178,6 +283,14 @@ Long64_t AliTPCClusterHistograms::Merge(TCollection* list)
    fhSigmaYProfileYVsRow->Merge(collectionSigmaYProfileYVsRow);
    fhSigmaZProfileYVsRow->Merge(collectionSigmaZProfileYVsRow);
 
+   fhQmaxProfileZVsRow  ->Merge(collectionQmaxProfileZVsRow  ); 
+   fhQtotProfileZVsRow  ->Merge(collectionQtotProfileZVsRow  );
+   fhSigmaYProfileZVsRow->Merge(collectionSigmaYProfileZVsRow);
+   fhSigmaZProfileZVsRow->Merge(collectionSigmaZProfileZVsRow);
+
+   fhQtotVsTime->Merge(collectionQtotVsTime);
+   fhQmaxVsTime->Merge(collectionQmaxVsTime);
+
    delete collectionQmaxVsRow;          
    delete collectionQtotVsRow;  
    delete collectionSigmaYVsRow;	  
@@ -188,11 +301,19 @@ Long64_t AliTPCClusterHistograms::Merge(TCollection* list)
    delete collectionSigmaYProfileYVsRow;
    delete collectionSigmaZProfileYVsRow;
 
+   delete collectionQmaxProfileZVsRow;  
+   delete collectionQtotProfileZVsRow;  
+   delete collectionSigmaYProfileZVsRow;
+   delete collectionSigmaZProfileZVsRow;
+
+   delete collectionQtotVsTime;
+   delete collectionQmaxVsTime;
+
   return count+1;
 }
 
 //____________________________________________________________________
-void AliTPCClusterHistograms::FillCluster(AliTPCclusterMI* cluster) {
+void AliTPCClusterHistograms::FillCluster(AliTPCclusterMI* cluster, Int_t time) {
   //
   //
   //
@@ -203,6 +324,7 @@ void AliTPCClusterHistograms::FillCluster(AliTPCclusterMI* cluster) {
   Float_t sigmaY = cluster->GetSigmaY2();
   Float_t sigmaZ = cluster->GetSigmaZ2();
   Float_t y      = cluster->GetY();
+  Float_t z      = cluster->GetZ();
 
   fhQmaxVsRow           ->Fill(padRow, qMax);
   fhQtotVsRow           ->Fill(padRow, qTot);
@@ -214,6 +336,19 @@ void AliTPCClusterHistograms::FillCluster(AliTPCclusterMI* cluster) {
   fhQtotProfileYVsRow   ->Fill(padRow, y, qTot); 
   fhSigmaYProfileYVsRow ->Fill(padRow, y, sigmaY);
   fhSigmaZProfileYVsRow ->Fill(padRow, y, sigmaZ);
+
+  fhQmaxProfileZVsRow   ->Fill(padRow, z, qMax);
+  fhQtotProfileZVsRow   ->Fill(padRow, z, qTot); 
+  fhSigmaYProfileZVsRow ->Fill(padRow, z, sigmaY);
+  fhSigmaZProfileZVsRow ->Fill(padRow, z, sigmaZ);
+
+  if (time>0 & fTimeStart>0 & fTimeStop>0 & time>fTimeStart) {
+    //Float_t timeFraction = (time - fTimeStart)/(fTimeStop-fTimeStart); 
+
+    fhQtotVsTime->Fill(time,qTot);
+    fhQmaxVsTime->Fill(time,qMax);
+  }
+
 }
 
 
@@ -238,7 +373,68 @@ void AliTPCClusterHistograms::SaveHistograms()
   fhSigmaYProfileYVsRow ->Write();
   fhSigmaZProfileYVsRow ->Write();
 
+  fhQmaxProfileZVsRow   ->Write();
+  fhQtotProfileZVsRow   ->Write();
+  fhSigmaYProfileZVsRow ->Write();
+  fhSigmaZProfileZVsRow ->Write();
+
+  if (fhQtotVsTime->GetEntries()>0)
+    fhQtotVsTime->Write();
+
+  if (fhQmaxVsTime->GetEntries()>0)
+    fhQmaxVsTime->Write();
+
   gDirectory->cd("../");
 
 }
 
+//____________________________________________________________________
+TCanvas* AliTPCClusterHistograms::DrawHistograms(const Char_t* opt) {
+
+  TCanvas* c = new TCanvas(Form("plots_%s",fName.Data()), fName.Data(), 1200, 1000);
+
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(0);
+
+  gStyle->SetPadLeftMargin(0.05);
+
+  c->Divide(3,3);
+
+  c->Draw();  
+
+  c->cd(1);
+  
+  // this is not really a nice way to do it...
+  c->GetPad(1)->Delete();
+  
+  TLatex* name = new TLatex(0.1,0.8,fName.Data());
+  name->SetTextSize(0.02);
+  name->DrawClone();
+
+  c->cd(2);
+  fhQmaxVsRow->Draw("colz");
+
+  c->cd(3);
+  fhQtotVsRow->Draw("colz");         
+  			
+  c->cd(4);
+  fhQmaxProfileYVsRow   ->Draw("colz");
+  c->cd(5);
+  fhQtotProfileYVsRow   ->Draw("colz");
+  c->cd(6);
+  fhQmaxProfileZVsRow   ->Draw("colz");
+  c->cd(7);
+  fhQtotProfileZVsRow   ->Draw("colz");
+  c->cd(8);
+  fhSigmaYVsRow         ->Draw("colz");
+
+  c->cd(9);
+  fhSigmaZVsRow         ->Draw("colz");
+  			
+  //fhSigmaYProfileYVsRow ->Draw("colz");
+  //fhSigmaZProfileYVsRow ->Draw("colz");
+
+  //fhSigmaYProfileZVsRow ->Draw("colz");
+  //fhSigmaZProfileZVsRow ->Draw("colz");
+  return c;
+}
--- PWG0/TPC/AliTPCClusterHistograms.h
+++ PWG0/TPC/AliTPCClusterHistograms.h
@@ -9,7 +9,10 @@
 
 class TH3;
 class TH2F;
+class TCanvas;
+class TProfile;
 class TProfile2D;
+class TString;
 class AliTPCclusterMI;
 
 class AliTPCClusterHistograms : public TNamed
@@ -17,7 +20,9 @@ class AliTPCClusterHistograms : public TNamed
 public:
 
   AliTPCClusterHistograms();
-  AliTPCClusterHistograms(const Char_t* name, const Char_t* title);
+  //AliTPCClusterHistograms(const Char_t* name, const Char_t* title);
+  AliTPCClusterHistograms(Int_t detector, const Char_t* comment="", Int_t timeStart=-1, Int_t timeStop=-1);
+  
   AliTPCClusterHistograms(const AliTPCClusterHistograms& c);
   virtual ~AliTPCClusterHistograms();
   AliTPCClusterHistograms& operator=(const AliTPCClusterHistograms& corrMatrix);
@@ -26,9 +31,14 @@ public:
 
   virtual void SaveHistograms();
 
-  void FillCluster(AliTPCclusterMI* clusterMI);
+  void FillCluster(AliTPCclusterMI* clusterMI, Int_t time=-1);
+
+  TCanvas* DrawHistograms(const Char_t* opt="");
 
 protected:
+
+  Int_t       fTimeStart;
+  Int_t       fTimeStop;
 
   TH2F*       fhQmaxVsRow;
   TH2F*       fhQtotVsRow;
@@ -40,6 +50,13 @@ protected:
   TProfile2D* fhSigmaYProfileYVsRow;
   TProfile2D* fhSigmaZProfileYVsRow;
 
+  TProfile2D* fhQmaxProfileZVsRow;
+  TProfile2D* fhQtotProfileZVsRow;
+  TProfile2D* fhSigmaYProfileZVsRow;
+  TProfile2D* fhSigmaZProfileZVsRow;
+
+  TProfile*   fhQtotVsTime;
+  TProfile*   fhQmaxVsTime;
 
   ClassDef(AliTPCClusterHistograms,1)
 };
