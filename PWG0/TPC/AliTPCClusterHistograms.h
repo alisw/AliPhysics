94e6cfc765b8543c4025df0d001e2f5155d49d12
ekman <ekman@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-09 13:56:04 +0000

    forgot this...

--- PWG0/TPC/AliTPCClusterHistograms.h
+++ PWG0/TPC/AliTPCClusterHistograms.h
@@ -3,9 +3,12 @@
 
 /* $Id$ */
 
+//
+// This class contains a number of histograms for diagnostics of a TPC
+// read out chamber from the reconstructed clusters.
+//
 
-#include <TNamed.h>
-//#include <../TPC/AliTPCclusterMI.h>
+#include "TNamed.h"
 
 class TH3;
 class TH2F;
@@ -13,6 +16,7 @@ class TCanvas;
 class TProfile;
 class TProfile2D;
 class TString;
+
 class AliTPCclusterMI;
 
 class AliTPCClusterHistograms : public TNamed
@@ -20,7 +24,6 @@ class AliTPCClusterHistograms : public TNamed
 public:
 
   AliTPCClusterHistograms();
-  //AliTPCClusterHistograms(const Char_t* name, const Char_t* title);
   AliTPCClusterHistograms(Int_t detector, const Char_t* comment="", Int_t timeStart=-1, Int_t timeStop=-1);
   
   AliTPCClusterHistograms(const AliTPCClusterHistograms& c);
@@ -32,31 +35,32 @@ public:
   virtual void SaveHistograms();
 
   void FillCluster(AliTPCclusterMI* clusterMI, Int_t time=-1);
+  //  void FillCluster(AliTPCclusterMI* clusterMI, Int_t time=-1, Float_t trackangle);
 
   TCanvas* DrawHistograms(const Char_t* opt="");
 
 protected:
 
-  Int_t       fTimeStart;
-  Int_t       fTimeStop;
+  Int_t       fTimeStart;               // begin time of run(s)
+  Int_t       fTimeStop;                // end time of runs(s)
 
-  TH2F*       fhQmaxVsRow;
-  TH2F*       fhQtotVsRow;
-  TH2F*       fhSigmaYVsRow;
-  TH2F*       fhSigmaZVsRow;
+  TH2F*       fhQmaxVsRow;              //
+  TH2F*       fhQtotVsRow;              //
+  TH2F*       fhSigmaYVsRow;            //
+  TH2F*       fhSigmaZVsRow;            //
 
-  TProfile2D* fhQmaxProfileYVsRow;
-  TProfile2D* fhQtotProfileYVsRow;
-  TProfile2D* fhSigmaYProfileYVsRow;
-  TProfile2D* fhSigmaZProfileYVsRow;
+  TProfile2D* fhQmaxProfileYVsRow;      //
+  TProfile2D* fhQtotProfileYVsRow;      // 
+  TProfile2D* fhSigmaYProfileYVsRow;    //
+  TProfile2D* fhSigmaZProfileYVsRow;    //
 
-  TProfile2D* fhQmaxProfileZVsRow;
-  TProfile2D* fhQtotProfileZVsRow;
-  TProfile2D* fhSigmaYProfileZVsRow;
-  TProfile2D* fhSigmaZProfileZVsRow;
+  TProfile2D* fhQmaxProfileZVsRow;      //
+  TProfile2D* fhQtotProfileZVsRow;      //
+  TProfile2D* fhSigmaYProfileZVsRow;    //
+  TProfile2D* fhSigmaZProfileZVsRow;    //
 
-  TProfile*   fhQtotVsTime;
-  TProfile*   fhQmaxVsTime;
+  TProfile*   fhQtotVsTime;             //
+  TProfile*   fhQmaxVsTime;             //
 
   ClassDef(AliTPCClusterHistograms,1)
 };
