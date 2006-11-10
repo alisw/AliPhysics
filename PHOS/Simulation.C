7c10f0e486e6d3aef54fc58cbfb48ac436d2d052
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-10 02:00:42 +0000

    added cdb stuff

--- PHOS/Simulation.C
+++ PHOS/Simulation.C
@@ -28,11 +28,10 @@
 //_________________________________________________________________________
 #include "AliSimulation.h"
 #include "TString.h"
-#include "AliPHOSGetter.h"
-#include "AliEMCALGetter.h"
 #include "Riostream.h"
+#include "TStopwatch.h"
 
-void simu(Int_t nevents=1, TString opt="GSD", TString name="all") 
+void simu(Int_t nevents=1, TString opt="GSD", TString name="all", TString cdb="") 
 {
   AliSimulation sim ; 
   // Generation and simulation
@@ -41,8 +40,13 @@ void simu(Int_t nevents=1, TString opt="GSD", TString name="all")
   // Making SDigits 
   if ( !opt.Contains("S") )
     sim.SetMakeSDigits("") ; 
-  else 
-    sim.SetMakeSDigits(name.Data()) ;
+  else {
+   sim.SetMakeSDigits(name.Data()) ;
+    if (name.Contains("ITS") || name.Contains("all"))
+     sim.SetMakeDigitsFromHits("ITS ") ; 
+    if (name.Contains("TPC") || name.Contains("all"))
+     sim.SetMakeDigitsFromHits("TPC") ;
+  }  
   // Making Digits 
   if ( !opt.Contains("D") )
     sim.SetMakeDigits("") ; 
@@ -54,6 +58,14 @@ void simu(Int_t nevents=1, TString opt="GSD", TString name="all")
   //Region of Interest
   sim.SetRegionOfInterest(kFALSE) ; 
   if ( opt.Contains("W") )
-     sim.SetWriteRawData(name.Data()) ;
+     sim.SetWriteRawData(name.Data(),"raw.root",kTRUE) ;
+  if ( !cdb.IsNull() ) 
+     sim.SetDefaultStorage(cdb.Data()) ; 
+
+  TStopwatch timer;
+  timer.Start();
   sim.Run(nevents) ;    
+  timer.Stop();
+  timer.Print();
+
 }
