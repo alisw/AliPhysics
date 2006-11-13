4e8289e9b41b22ef688484c0baf533706f520868
panos <panos@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-13 13:02:09 +0000

    Adding the AlienGUILinkDef.

---
+++ ANALYSIS/Aliengui/AlienGUILinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliAnalysisGUI+;
+#pragma link C++ class AliLoginFrame+;
+#pragma link C++ class AliFileListFrame+;
+#pragma link C++ class AliPackageFrame+;
+#pragma link C++ class AliAlienBrowser+;
+#pragma link C++ class AliTagFrame+;
+#pragma link C++ class AliTagAnalysisFrame+;
+#pragma link C++ class AliSelectorFrame+;
+
+#endif
