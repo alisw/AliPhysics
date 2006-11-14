31a58866cc24fce8e30875c388100432edc88141
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-14 16:27:44 +0000

    New files: container with chunked memory allocation (to avoid frequent re-allocation on creation time) and ability to refit itself into a contiguous array.

---
+++ EVE/Reve/Plex.cxx
@@ -0,0 +1,79 @@
+// $Header$
+
+#include "Plex.h"
+
+using namespace Reve;
+
+//______________________________________________________________________
+// Plex
+//
+// A group of containers with chunked memory allocation.
+//
+
+//______________________________________________________________________
+// VoidCPlex
+//
+
+void VoidCPlex::ReleaseChunks()
+{
+  for (Int_t i=0; i<fVecSize; ++i)
+    delete fChunks[i];
+  fChunks.clear();
+}
+
+VoidCPlex::VoidCPlex() : 
+  fS(0), fN(0),
+  fSize(0), fVecSize(0), fCapacity(0)
+{}
+
+VoidCPlex::VoidCPlex(Int_t atom_size, Int_t chunk_size) :
+  fS(atom_size), fN(chunk_size),
+  fSize(0), fVecSize(0), fCapacity(0)
+{}
+
+VoidCPlex::~VoidCPlex()
+{
+  ReleaseChunks();
+}
+
+/**************************************************************************/
+
+void VoidCPlex::Reset(Int_t atom_size, Int_t chunk_size)
+{
+  ReleaseChunks();
+  fS = atom_size;
+  fN = chunk_size;
+  fSize = fVecSize = fCapacity = 0;
+}
+
+void VoidCPlex::Refit()
+{
+  if (fSize == 0 || (fVecSize == 1 && fSize == fCapacity))
+    return;
+
+  TArrayC* one = new TArrayC(fS*fSize);
+  Char_t*  pos = one->fArray;
+  for (Int_t i=0; i<fVecSize; ++i)
+  {
+    Int_t size = fS * NAtoms(i);
+    memcpy(pos, fChunks[i]->fArray, size);
+    pos += size;
+  }
+  ReleaseChunks();
+  fN = fCapacity = fSize;
+  fVecSize = 1;
+  fChunks.push_back(one);
+}
+
+/**************************************************************************/
+
+Char_t* VoidCPlex::NewChunk()
+{
+  fChunks.push_back(new TArrayC(fS*fN));
+  ++fVecSize;
+  fCapacity += fN;
+  return fChunks.back()->fArray;
+}
+
+/**************************************************************************/
+/**************************************************************************/
---
+++ EVE/Reve/Plex.h
@@ -0,0 +1,96 @@
+// $Header$
+
+#ifndef REVE_PLEX_H
+#define REVE_PLEX_H
+
+#include <Reve/Reve.h>
+
+#include <TObject.h>
+#include <TArrayC.h>
+
+#include <vector>
+
+namespace Reve {
+
+/**************************************************************************/
+// VoidCPlex
+/**************************************************************************/
+
+class VoidCPlex
+{
+private:
+  VoidCPlex(const VoidCPlex&);            // Not implemented
+  VoidCPlex& operator=(const VoidCPlex&); // Not implemented
+
+protected:
+  Int_t fS; // Size of atom
+  Int_t fN; // Number of atoms in a chunk
+
+  Int_t fSize;     // Size of container, number of atoms
+  Int_t fVecSize;  // Number of allocated chunks
+  Int_t fCapacity; // Available capacity within the chunks
+
+  std::vector<TArrayC*> fChunks; // Memory blocks
+
+  void ReleaseChunks();
+
+public:
+  VoidCPlex();
+  VoidCPlex(Int_t atom_size, Int_t chunk_size);
+  virtual ~VoidCPlex();
+
+  void Reset(Int_t atom_size, Int_t chunk_size);
+  void Refit();
+
+  Int_t    S() const { return fS; }
+  Int_t    N() const { return fN; }
+  
+  Int_t    Size()     const { return fSize; }
+  Int_t    VecSize()  const { return fVecSize; }
+  Int_t    Capacity() const { return fCapacity; }
+
+  Char_t* Atom(Int_t idx)   { return fChunks[idx/fN]->fArray + idx%fN*fS; }
+  Char_t* Chunk(Int_t chk)  { return fChunks[chk]->fArray; }
+  Int_t   NAtoms(Int_t chk) { return (chk < fVecSize-1) ? fN : (fSize-1)%fN + 1; }
+
+  Char_t* NewAtom();
+  Char_t* NewChunk();
+
+  ClassDef(VoidCPlex, 1)
+};
+
+inline Char_t* VoidCPlex::NewAtom()
+{
+  Char_t *a = (fSize >= fCapacity) ? NewChunk() : Atom(fSize);
+  ++fSize;
+  return a;
+}
+
+
+/**************************************************************************/
+// Some-class CPlex
+/**************************************************************************/
+
+template<class T>
+class CPlex : public VoidCPlex
+{
+private:
+  CPlex(const CPlex&);            // Not implemented
+  CPlex& operator=(const CPlex&); // Not implemented
+
+public:
+  CPlex()                 : VoidCPlex() {}
+  CPlex(Int_t chunk_size) : VoidCPlex(sizeof(T), chunk_size) {}
+  virtual ~CPlex() {}
+
+  void Reset(Int_t chunk_size) { Reset(sizeof(T), chunk_size); }
+
+  T* At(Int_t idx)  { return reinterpret_cast<T*>(Atom(ids)); }
+  T& Ref(Int_t idx) { return *At(idx); }
+
+  ClassDef(CPlex, 1)
+}; // endclass CPlex
+
+}
+
+#endif
