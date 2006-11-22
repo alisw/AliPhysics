a9cbdecb1bb5b983162e38707d4b702ad3c77275
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-22 09:27:08 +0000

    Library for jet analysis using MC truth.

---
+++ JETAN/JETANMCLinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+  
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliJetKineReader;
+#pragma	link C++ class AliJetKineReaderHeader;
+#pragma	link C++ class AliJetMCReader;
+#pragma	link C++ class AliJetMCReaderHeader;
+#pragma link C++ class AliJetESDmcReader;
+
+#endif
