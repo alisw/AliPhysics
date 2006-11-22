4900e0d29b8acd5ef0bdbb6b2493cc17e85d59f0
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-22 09:23:08 +0000

    ESD jet analysis.

---
+++ JETAN/JETANLinkDef.h
@@ -0,0 +1,23 @@
+#ifdef __CINT__
+  
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+  
+#pragma link C++ class AliJet+;
+#pragma link C++ class AliJetHeader+;
+#pragma link C++ class AliUA1JetHeader+;
+#pragma link C++ class AliJetFinder+;
+#pragma link C++ class AliJetReaderHeader+;
+#pragma link C++ class AliJetESDReaderHeader+;
+#pragma link C++ class AliJetReader+;
+#pragma link C++ class AliJetESDReader+;
+#pragma link C++ class AliJetControlPlots+;
+#pragma link C++ class AliLeading+;
+#pragma link C++ class AliJetProductionData+;
+#pragma link C++ class AliJetProductionDataPDC2004+;
+#pragma link C++ class AliJetAnalysis+;
+#pragma link C++ class AliJetDistributions+;
+#pragma link C++ class AliUA1JetFinderV1+;
+#pragma link C++ class AliUA1JetHeaderV1+;
+#endif
