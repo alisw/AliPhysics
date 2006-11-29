b77e7e42d88e6c5c6de3d30d7d203c4db99177d7
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-11-29 09:54:53 +0000

    RICH becomes HMPID, the old module RICH is removed

--- RICH/RICHrecLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ class  AliRICHReconstructor+;
-#pragma link C++ class  AliRICHTracker+;
-#pragma link C++ class  AliRICHRecon+;
-#pragma link C++ class  AliRICHPreprocessor+;
-#endif
