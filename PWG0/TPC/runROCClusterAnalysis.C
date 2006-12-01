47fd8b132e1d19e62fa449d31be7eea846c510ac
ekman <ekman@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-12-01 13:34:29 +0000

    Added classes to analyse TPC data from reconstructed clusters (not on tracks) and modified the AliTPCClusterHistogram class accordingly.

---
+++ PWG0/TPC/runROCClusterAnalysis.C
@@ -0,0 +1,42 @@
+/* $Id$ */
+
+//
+// 
+//
+
+#include "../CreateESDChain.C"
+#include "../PWG0Helper.C"
+
+void runROCClusterAnalysis(Char_t* data, Int_t nRuns=20, Int_t offset=0, Bool_t aDebug = kFALSE, Int_t runNumber=0, Bool_t aProof = kFALSE, 
+    const char* option = "", const char* proofServer = "jgrosseo@lxb6046")
+{
+  if (aProof)
+    connectProof(proofServer);
+
+  TString libraries("libEG;libGeom;libESD;libPWG0base;libPWG0dep");
+  TString packages("PWG0base;PWG0dep");
+
+  if (!prepareQuery(libraries, packages, 2))
+    return;
+  
+  TChain* chain = CreateESDChain(data, nRuns, offset, kTRUE, kTRUE);
+
+  TList inputList;
+
+  //  TNamed* comment = new TNamed("comment",runStr.Data());
+  if (runNumber!=0)
+    inputList.Add(new TNamed("comment",Form("%d",runNumber)));
+
+  TString selectorName = "AliROCClusterAnalysisSelector";
+  AliLog::SetClassDebugLevel(selectorName, AliLog::kInfo);
+
+  if (aDebug != kFALSE)
+  {
+    AliLog::SetClassDebugLevel(selectorName, AliLog::kDebug);
+    selectorName += ".cxx++g";
+  }
+  else
+    selectorName += ".cxx+";
+
+  Int_t result = executeQuery(chain, &inputList, selectorName, option);
+}
