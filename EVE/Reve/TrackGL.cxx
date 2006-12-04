c81b5c90e52106b304c9212771568a20502828f4
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-12-04 11:26:38 +0000

    In TrackGL::ProcessSelection(): make model-track emit Clicked signal.

--- EVE/Reve/TrackGL.cxx
+++ EVE/Reve/TrackGL.cxx
@@ -29,15 +29,25 @@ TrackGL::~TrackGL()
 
 /**************************************************************************/
 
-void TrackGL::ProcessSelection(UInt_t* ptr, TGLViewer* v, TGLScene* s)
+void TrackGL::ProcessSelection(UInt_t* ptr, TGLViewer*, TGLScene*)
 {
-   // Processes secondary selection from TGLViewer.
-   // Calls TPointSet3D::PointSelected(Int_t) with index of selected
-   // point as an argument.
-  TGLLogicalShape::ProcessSelection(ptr, v, s);
+  // Processes secondary selection from TGLViewer.
+  // Calls TPointSet3D::PointSelected(Int_t) with index of selected
+  // point as an argument.
+
+  Int_t n = ptr[0];
+  printf("TrackGL::ProcessSelection %d names on the stack (z1=%g, z2=%g).\n",
+	 n, Float_t(ptr[1])/0x7fffffff, Float_t(ptr[2])/0x7fffffff);
+  ptr += 3;
+  printf("  Names: ");
+  for (Int_t j=0; j<n; ++j, ++ptr) printf ("%d ", *ptr);
+  printf("\n");
+
+  ((Track*)fM)->CtrlClicked((Track*)fM);
+
   /*
-  if (ptr[0] < 2) return;
-  TPointSet3D& q = * (TPointSet3D*) fExternalObj;
-  q.PointSelected(ptr[4]);
+    if (ptr[0] < 2) return;
+    TPointSet3D& q = * (TPointSet3D*) fExternalObj;
+    q.PointSelected(ptr[4]);
   */
 }
