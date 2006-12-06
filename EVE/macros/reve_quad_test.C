1a52703e08aa027f25b19434bafc676e785eea7c
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-12-06 19:53:05 +0000

    Added test function for hexagons.

--- EVE/macros/reve_quad_test.C
+++ EVE/macros/reve_quad_test.C
@@ -6,7 +6,7 @@ Reve::QuadSet* reve_quad_test(Float_t x=0, Float_t y=0, Float_t z=0)
 
   gStyle->SetPalette(1, 0);
 
-  Reve::QuadSet* q = new Reve::QuadSet("Pepe");
+  Reve::QuadSet* q = new Reve::QuadSet("RectangleXY");
   q->Reset(Reve::QuadSet::QT_RectangleXY, kFALSE, 32);
   for (Int_t i=0; i<100; ++i) {
     q->AddQuad(r.Uniform(-10, 10), r.Uniform(-10, 10), r.Uniform(-10, 10),
@@ -47,6 +47,49 @@ Reve::QuadSet* reve_quad_test_circ()
 
   gReve->AddRenderElement(q);
   gReve->Redraw3D();
+
+  return q;
+}
+
+Reve::QuadSet* reve_quad_test_hex(Float_t x=0, Float_t y=0, Float_t z=0)
+{
+  TRandom r(0);
+
+  gStyle->SetPalette(1, 0);
+
+  {
+    Reve::QuadSet* q = new Reve::QuadSet("HexagonXY");
+    q->Reset(Reve::QuadSet::QT_HexagonXY, kFALSE, 32);
+    for (Int_t i=0; i<100; ++i) {
+      q->AddHexagon(r.Uniform(-10, 10), r.Uniform(-10, 10), r.Uniform(-10, 10),
+		    r.Uniform(0.2, 1));
+      q->QuadValue(r.Uniform(0, 120));
+    }
+    q->RefitPlex();
+
+    Reve::ZTrans& t = q->RefHMTrans();
+    t.SetPos(x, y, z);
+
+    gReve->AddRenderElement(q);
+    gReve->Redraw3D();
+  }
+
+  {
+    Reve::QuadSet* q = new Reve::QuadSet("HexagonYX");
+    q->Reset(Reve::QuadSet::QT_HexagonYX, kFALSE, 32);
+    for (Int_t i=0; i<100; ++i) {
+      q->AddHexagon(r.Uniform(-10, 10), r.Uniform(-10, 10), r.Uniform(-10, 10),
+		    r.Uniform(0.2, 1));
+      q->QuadValue(r.Uniform(0, 120));
+    }
+    q->RefitPlex();
+
+    Reve::ZTrans& t = q->RefHMTrans();
+    t.SetPos(x, y, z);
+
+    gReve->AddRenderElement(q);
+    gReve->Redraw3D();
+  }
 
   return q;
 }
