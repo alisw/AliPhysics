f7d71e77845a1f1e37fa11b74db7e741a69f3de0
cheynis <cheynis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-12-10 08:25:47 +0000

    Reconstruction of ESD data implemented

--- VZERO/AliVZEROReconstructor.cxx
+++ VZERO/AliVZEROReconstructor.cxx
@@ -21,19 +21,23 @@
 ///                                                                          //
 ///////////////////////////////////////////////////////////////////////////////
 
-
+#include "AliRunLoader.h"
 #include "AliVZEROReconstructor.h"
+#include "AliESD.h"
 
 ClassImp(AliVZEROReconstructor)
 
 //_____________________________________________________________________________
 AliVZEROReconstructor:: AliVZEROReconstructor(): AliReconstructor(),
+   fESDVZERO(0x0),
+   fESD(0x0),
+   fRunLoader(0x0),
    fCalibData(GetCalibData())
 {
   // Default constructor  
   // Get calibration data
   
-//  fCalibData = GetCalibData(); 
+  // fCalibData = GetCalibData(); 
 }
 
 
@@ -51,10 +55,119 @@ AliVZEROReconstructor& AliVZEROReconstructor::operator =
 AliVZEROReconstructor::~AliVZEROReconstructor()
 {
 // destructor
-
+   delete fESDVZERO; 
+   
 }
 
+//_____________________________________________________________________________
+void AliVZEROReconstructor::Init(AliRunLoader* runLoader)
+{
+/// initializer
+
+  fRunLoader = runLoader;
+  fESDVZERO  = new AliESDVZERO;
+}
+
+//______________________________________________________________________
+void AliVZEROReconstructor::Reconstruct(AliRunLoader* runLoader) const
+{
+
+  AliVZEROLoader* loader = (AliVZEROLoader* )runLoader->GetLoader( "VZEROLoader" );
+//  AliVZEROLoader* loader = (AliVZEROLoader* )fRunLoader->GetLoader( "VZEROLoader" );
+ 
+  loader->LoadDigits("READ");
+  TTree* vzeroDigitsTree = loader->TreeD();
+  if (!vzeroDigitsTree) return;
+
+  TClonesArray* vzeroDigits = new TClonesArray("AliVZEROdigit",1000);
+  TBranch* digitBranch = vzeroDigitsTree->GetBranch("VZERODigit");
+  digitBranch->SetAddress(&vzeroDigits);
+
+  Int_t   NbPMV0A = 0;
+  Int_t   NbPMV0C = 0;
+  Int_t   MTotV0A = 0;
+  Int_t   MTotV0C = 0;
+  Float_t ADCV0A  = 0.0;
+  Float_t ADCV0C  = 0.0;
+  Float_t MultV0A[4];
+  Float_t MultV0C[4];
+  Int_t   MRingV0A[4];
+  Int_t   MRingV0C[4];
   
+  Int_t   ADC[64]; 
+  Float_t MIP[64];
+  for (Int_t i=0; i<64; i++){
+       ADC[i] = 0;
+       MIP[i] = 110.0;}
+  for (Int_t j=0; j<4; j++){
+       MultV0A[j]  = 0.0;
+       MultV0C[j]  = 0.0;
+       MRingV0A[j] = 0;
+       MRingV0C[j] = 0;}
+     
+  // loop over VZERO entries
+  Int_t nEntries = (Int_t)vzeroDigitsTree->GetEntries();
+  for (Int_t e=0; e<nEntries; e++) {
+    vzeroDigitsTree->GetEvent(e);
+
+    Int_t nDigits = vzeroDigits->GetEntriesFast();
+    
+    for (Int_t d=0; d<nDigits; d++) {    
+      AliVZEROdigit* digit = (AliVZEROdigit*)vzeroDigits->At(d);      
+      Int_t  PMNumber      = digit->PMNumber();  
+      ADC[PMNumber] = digit->ADC();  
+      if (PMNumber<=31) {
+        if (PMNumber<=7) MultV0C[0]=MultV0C[0]+ float(ADC[PMNumber])/MIP[PMNumber];
+	if (PMNumber>=8  && PMNumber<=15) MultV0C[1]=MultV0C[1]+ float(ADC[PMNumber])/MIP[PMNumber];
+	if (PMNumber>=16 && PMNumber<=23) MultV0C[2]=MultV0C[2]+ float(ADC[PMNumber])/MIP[PMNumber];
+	if (PMNumber>=24 && PMNumber<=31) MultV0C[3]=MultV0C[3]+ float(ADC[PMNumber])/MIP[PMNumber];
+        ADCV0C = ADCV0C + float(ADC[PMNumber])/MIP[PMNumber];
+	if(ADC[PMNumber] > 4) NbPMV0C++;
+      }	
+      if (PMNumber>=32) {
+        if (PMNumber>=32 && PMNumber<=39) MultV0A[0]=MultV0A[0]+ float(ADC[PMNumber])/MIP[PMNumber];
+	if (PMNumber>=40 && PMNumber<=47) MultV0A[1]=MultV0A[1]+ float(ADC[PMNumber])/MIP[PMNumber];
+	if (PMNumber>=48 && PMNumber<=55) MultV0A[2]=MultV0A[2]+ float(ADC[PMNumber])/MIP[PMNumber];
+	if (PMNumber>=56 && PMNumber<=63) MultV0A[3]=MultV0A[3]+ float(ADC[PMNumber])/MIP[PMNumber];
+        ADCV0A = ADCV0A + float(ADC[PMNumber])/MIP[PMNumber];
+	if(ADC[PMNumber] > 4) NbPMV0A++;
+      }
+    } // end of loop over digits
+    
+  } // end of loop over events in digits tree
+  
+  MTotV0A = int(ADCV0A + 0.5);
+  MTotV0C = int(ADCV0C + 0.5);
+  for (Int_t j=0; j<4; j++){       
+       MRingV0A[j] = int(MultV0A[j] + 0.5);
+       MRingV0C[j] = int(MultV0C[j] + 0.5);}
+     
+  AliDebug(1,Form("VZERO multiplicities : %d (V0A) %d (V0C)", MTotV0A, MTotV0C));
+  AliDebug(1,Form("Number of PMs fired  : %d (V0A) %d (V0C)", NbPMV0A, NbPMV0C));
+
+  fESDVZERO->SetNbPMV0A(NbPMV0A);
+  fESDVZERO->SetNbPMV0C(NbPMV0C);
+  fESDVZERO->SetMTotV0A(MTotV0A);
+  fESDVZERO->SetMTotV0C(MTotV0C);
+  fESDVZERO->SetMRingV0A(MRingV0A);
+  fESDVZERO->SetMRingV0C(MRingV0C);
+  
+}
+
+
+//_____________________________________________________________________________
+void AliVZEROReconstructor::FillESD(AliRunLoader* /*runLoader*/, 
+				    AliESD* esd) const
+{
+// fill ESD 
+
+   if (esd) { 
+      AliDebug(1, Form("Writing VZERO data to ESD tree"));
+      esd->SetVZEROData(fESDVZERO);
+   }
+  
+}
+
 //_____________________________________________________________________________
 AliCDBStorage* AliVZEROReconstructor::SetStorage(const char *uri) 
 {
@@ -111,4 +224,3 @@ AliVZEROCalibData* AliVZEROReconstructor::GetCalibData() const
 
   return calibdata;
 }
-
--- VZERO/AliVZEROReconstructor.h
+++ VZERO/AliVZEROReconstructor.h
@@ -1,40 +1,66 @@
 #ifndef ALIVZERORECONSTRUCTOR_H
 #define ALIVZERORECONSTRUCTOR_H
-/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- * See cxx source for full Copyright notice                               */
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved.*/
+/* See cxx source for full Copyright notice                              */
+/* $Id$  */
 
-/* $Id$ */
-
-///////////////////////////////////////////////////////////////////////////////
-///                                                                          //
-/// class for VZERO reconstruction                                           //
-///                                                                          //
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+///                                                                      //
+/// class for VZERO reconstruction                                       //
+///                                                                      //
+///////////////////////////////////////////////////////////////////////////
 
 #include "AliReconstructor.h"
 #include "AliCDBManager.h"
 #include "AliCDBStorage.h"
-#include "AliVZEROCalibData.h"
 #include "AliCDBEntry.h"
 
+#include "AliVZEROCalibData.h"
+#include "AliLog.h"
+#include "AliESDVZERO.h"
+
 class AliLoader;
+class AliESD;
 
 class AliVZEROReconstructor: public AliReconstructor {
 public:
   AliVZEROReconstructor();
   virtual ~AliVZEROReconstructor();
-
+  virtual void   Init(AliRunLoader* runLoader);
+  virtual void   Reconstruct(AliRunLoader* runLoader) const;
+  
+  virtual void   Reconstruct(AliRawReader* /*rawReader*/, 
+		             TTree* /*clustersTree*/) const {return;};
+  virtual void   Reconstruct(AliRunLoader* /*runLoader*/, 
+                             AliRawReader* /*rawReader*/) const {return;};
+  virtual void   Reconstruct(TTree*, TTree*) const {return;};
+  
+  virtual void   FillESD(AliRunLoader* /*runLoader*/, AliESD* /*esd*/) const;
+  
+  virtual void   FillESD(TTree* /*digitsTree*/, TTree* /*clustersTree*/, 
+			         AliESD* /*esd*/) const {return;};
+  virtual void   FillESD(AliRawReader* /*rawReader*/, TTree* /*clustersTree*/, 
+			 AliESD* /*esd*/) const {return;};
+  
+  virtual void   FillESD(AliRunLoader* /*runLoader*/, 
+			 AliRawReader* /*rawReader*/, AliESD* /*esd*/) const {return;};
+  
   AliCDBStorage     *SetStorage(const char* uri);
   AliVZEROCalibData *GetCalibData() const; 
 
 private:
-
   AliVZEROReconstructor(const AliVZEROReconstructor& reconstructor);
   AliVZEROReconstructor& operator = (const AliVZEROReconstructor& reconstructor);
   
-  AliVZEROCalibData *fCalibData;       //! calibration data
+protected:
+  AliESDVZERO*        fESDVZERO;      // ESD output object  
+  AliESD*                  fESD;      // ESD object
+  
+private:
+  AliRunLoader*      fRunLoader;      //! pointer to runloader    
+  AliVZEROCalibData* fCalibData;      //! calibration data
  
-  ClassDef(AliVZEROReconstructor, 0)   // class for the VZERO reconstruction
+  ClassDef(AliVZEROReconstructor, 0)  // class for the VZERO reconstruction
 };
 
 #endif
