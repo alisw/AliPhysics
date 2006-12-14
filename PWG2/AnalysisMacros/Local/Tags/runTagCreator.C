2cf0a1d45035e9f8022b4664734ca0ec9d8652ac
panos <panos@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-12-14 15:18:37 +0000

    Adding the files to create tags for the locally stored ESDs.

---
+++ PWG2/AnalysisMacros/Local/Tags/runTagCreator.C
@@ -0,0 +1,51 @@
+void runTagCreator() {
+  TStopwatch timer;
+  timer.Start();
+
+  //____________________________________________________//
+  //_____________Setting up ESD.par_____________________//
+  //____________________________________________________//
+  const char* pararchivename1 = "ESD";
+  //////////////////////////////////////////
+  // Libraries required to load
+  //////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////
+  // Setup PAR File
+  if (pararchivename1) {
+    char processline[1024];
+    sprintf(processline,".! tar xvzf %s.par",pararchivename1);
+    gROOT->ProcessLine(processline);
+    const char* ocwd = gSystem->WorkingDirectory();
+    gSystem->ChangeDirectory(pararchivename1);
+
+    // check for BUILD.sh and execute
+    if (!gSystem->AccessPathName("PROOF-INF/BUILD.sh")) {
+      printf("*******************************\n");
+      printf("*** Building PAR archive    ***\n");
+      printf("*******************************\n");
+
+      if (gSystem->Exec("PROOF-INF/BUILD.sh")) {
+        Error("runProcess","Cannot Build the PAR Archive! - Abort!");
+        return -1;
+      }
+    }
+    // check for SETUP.C and execute
+    if (!gSystem->AccessPathName("PROOF-INF/SETUP.C")) {
+      printf("*******************************\n");
+      printf("*** Setup PAR archive       ***\n");
+      printf("*******************************\n");
+      gROOT->Macro("PROOF-INF/SETUP.C");
+    }
+    
+    gSystem->ChangeDirectory("../");
+  }
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libESD.so");
+
+  gROOT->LoadMacro("CreateTags.C");  
+  CreateTags();
+
+  timer.Stop();
+  timer.Print();
+}
