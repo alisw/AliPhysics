9ecc70104713850e39ee681a56ba0c3ffaa28da4
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-12-20 08:51:05 +0000

    adding new

---
+++ ESDCheck/PROOF-INF.AnalysisCheck/SETUP.C
@@ -0,0 +1,13 @@
+void SETUP()
+{
+
+   // Load the ESD library
+   gSystem->Load("libAnalysisCheck");
+
+   // Set the Include paths
+   gSystem->SetIncludePath("-I$ROOTSYS/include -IAnalysisCheck");
+   gROOT->ProcessLine(".include AnalysisCheck");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("AnalysisCheck_INCLUDE", "AnalysisCheck");
+}
