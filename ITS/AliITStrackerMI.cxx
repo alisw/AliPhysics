80794997fc07342f00dfe96d898c15729443e39c
masera <masera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-01-18 16:44:33 +0000

    Method GetTrackPoint now works with mis-aligned geometry (L. Gaudichet)

--- ITS/AliITStrackerMI.cxx
+++ ITS/AliITStrackerMI.cxx
@@ -569,6 +569,10 @@ AliCluster *AliITStrackerMI::GetCluster(Int_t index) const {
   return fgLayers[l].GetCluster(c);
 }
 
+#include "TGeoManager.h"
+#include "TGeoMatrix.h"
+#include "TGeoPhysicalNode.h"
+
 Bool_t AliITStrackerMI::GetTrackPoint(Int_t index, AliTrackPoint& p) const {
   //
   // Get track space point with index i
@@ -577,21 +581,49 @@ Bool_t AliITStrackerMI::GetTrackPoint(Int_t index, AliTrackPoint& p) const {
   Int_t c=(index & 0x0fffffff) >> 00;
   AliITSRecPoint *cl = fgLayers[l].GetCluster(c);
   Int_t idet = cl->GetDetectorIndex();
-  const AliITSdetector &det = fgLayers[l].GetDetector(idet);
-  Float_t phi = det.GetPhi();
-  Float_t r = det.GetR();
-  Float_t cp=TMath::Cos(phi), sp=TMath::Sin(phi);
-  Float_t xyz[3];
-  xyz[0] = r*cp - cl->GetY()*sp;
-  xyz[1] = r*sp + cl->GetY()*cp;
-  xyz[2] = cl->GetZ();
-  Float_t cov[6];
-  cov[0] = sp*sp*cl->GetSigmaY2();
-  cov[1] = -sp*cp*cl->GetSigmaY2();
-  cov[2] = 0.;
-  cov[3] = cp*cp*cl->GetSigmaY2();
-  cov[4] = 0.;
-  cov[5] = cl->GetSigmaZ2();
+
+  const char* name = AliAlignObj::SymName((AliAlignObj::ELayerID)
+					  (l+AliAlignObj::kFirstLayer), idet);
+  TGeoPNEntry *mapPN = gGeoManager->GetAlignableEntry(name);
+
+  if (!mapPN) return kFALSE;
+  TGeoPhysicalNode *node = mapPN->GetPhysicalNode();
+  if (!node) {
+    gGeoManager->MakeAlignablePN(name);
+    node = mapPN->GetPhysicalNode();
+  }
+  if (!node) return kFALSE;
+  TGeoHMatrix* matrix = node->GetMatrix();
+  if (!matrix) return kFALSE;
+
+  //
+  // Calculate the global coordinates
+  //
+  Double_t localCoord[]  = {cl->GetDetLocalX(), 0, cl->GetDetLocalZ()};
+  // LG  AliAlignObj makes life simple but keep in mind that alignable
+  // LG  volume doesn't mean sensitive volume. There might be a shift
+  // LG  between the 2, has it is here for the SPD :
+  if (l<2) localCoord[1] = 0.01;
+  // LG   !!!   Check for this when the new geometry comes   !!!
+  Double_t globalCoord[3] = {0};
+  matrix->LocalToMaster(localCoord, globalCoord);
+  Float_t xyz[3]= {globalCoord[0], globalCoord[1], globalCoord[2]};
+
+  //
+  // Calculate the cov matrix
+  //
+  TGeoRotation rotMatrix(*matrix);
+  TGeoRotation rotMatrixTr(rotMatrix.Inverse());
+  Double_t sigmaArray[] = {cl->GetSigmaY2(),0,0, 0,0,0, 0,0,cl->GetSigmaZ2()};
+  TGeoRotation sigmMatrix;
+  sigmMatrix.SetMatrix( sigmaArray );
+  sigmMatrix.MultiplyBy(&rotMatrixTr, kFALSE);
+  sigmMatrix.MultiplyBy(&rotMatrix, kTRUE);
+  const Double_t *globalSigma =  sigmMatrix.GetRotationMatrix();
+  Float_t cov[6]= { globalSigma[0], globalSigma[1], globalSigma[2],
+		                    globalSigma[4], globalSigma[5],
+		                                    globalSigma[8] };
+
   p.SetXYZ(xyz[0],xyz[1],xyz[2],cov);
   AliAlignObj::ELayerID iLayer = AliAlignObj::kInvalidLayer; 
   switch (l) {
