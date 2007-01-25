87a35c5fe0e088afb6ead76150840db09596a84f
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-01-25 16:56:07 +0000

    New definition and implementation of bus patch class, now representing a bus patch and not bus patch manager.

--- MUON/mapping/AliMpBusPatch.cxx
+++ MUON/mapping/AliMpBusPatch.cxx
@@ -14,52 +14,29 @@
  **************************************************************************/
 
 // $Id$
-// $MpId: AliMpBusPatch.cxx,v 1.5 2006/05/24 13:58:34 ivana Exp $
-// Category: management
-
-// Class AliMpBusPatch
-// ---------------
-// Class that manages the maps buspatch<>DDL<>DE 
-// for the mapping
-// Calculates also the maximum DSP and buspatch numbers for a given DDL
-// Create a bus Iterator for DDL, needed especially for station 3
-// Implementing a Sort method for Iterator, not really needed for the moment
+// $MpId: AliMpBusPatch.cxx,v 1.4 2006/05/24 13:58:34 ivana Exp $
 //
-// Author: Ch. Finck; Subatech Nantes
+// --------------------
+// Class AliMpBusPatch
+// --------------------
+// The class defines the properties of BusPatch
+// Author: Ivana Hrivnacova, IPN Orsay
 
 #include "AliMpBusPatch.h"
-#include "AliMpFiles.h"
-#include "AliMpHelper.h"
+
 #include "AliLog.h"
-#include "TArrayI.h"
-#include "Riostream.h"
+
+#include <Riostream.h>
 
 /// \cond CLASSIMP
 ClassImp(AliMpBusPatch)
 /// \endcond
 
 const Int_t  AliMpBusPatch::fgkOffset = 100;
+//
+// static methods
+//
 
-//_____________________________________________________________________________
-AliMpBusPatch::AliMpBusPatch()
-  : TObject(),
-    fDetElemIdToBusPatch(300),
-    fBusPatchToDetElem(300),
-    fBusPatchToDDL(300)
-{
-/// Default constructor
-}
-
-//_____________________________________________________________________________
-AliMpBusPatch::~AliMpBusPatch() 
-{
-/// Destructor
-
-  fDetElemIdToBusPatch.Delete();
-  fBusPatchToDetElem.Delete();
-  fBusPatchToDDL.Delete();
-
-}
 //____________________________________________________________________
 Int_t AliMpBusPatch::GetGlobalBusID(Int_t localID, Int_t ddlID)
 {
@@ -76,219 +53,78 @@ Int_t AliMpBusPatch::GetLocalBusID(Int_t globalID, Int_t ddlID)
   return globalID - ddlID*fgkOffset;
 
 }
-//____________________________________________________________________
-Int_t AliMpBusPatch::GetDEfromBus(Int_t busPatchId)
-{
- /// getting DE id from bus patch
-  Long_t it = fBusPatchToDetElem.GetValue(busPatchId);
 
- if ( it ) 
-   return (Int_t)it;
- else 
-   return -1;
+//______________________________________________________________________________
+AliMpBusPatch::AliMpBusPatch(Int_t id, Int_t detElemId, Int_t ddlId)
+  : TObject(),
+    fId(id),
+    fDEId(detElemId),
+    fDdlId(ddlId),
+    fManus()
+{
+/// Standard constructor
 }
 
-//____________________________________________________________________
-TArrayI*  AliMpBusPatch::GetBusfromDE(Int_t idDE)
+//______________________________________________________________________________
+AliMpBusPatch::AliMpBusPatch(TRootIOCtor* /*ioCtor*/)
+  : TObject(),
+    fId(),
+    fDEId(),
+    fDdlId(),
+    fManus()
 {
-/// getting bus patch from DE id 
-
-  return (TArrayI*)fDetElemIdToBusPatch.GetValue(idDE);
-}
-//____________________________________________________________________
-Int_t AliMpBusPatch::GetDDLfromBus(Int_t busPatchId)
-{
-/// getting DE id from bus patch
-  Long_t it = fBusPatchToDDL.GetValue(busPatchId);
-
- if ( it ) 
-   return (Int_t)it;
- else 
-   return -1;
+/// Root IO constructor
 }
 
-//____________________________________________________________________
-void AliMpBusPatch::GetDspInfo(Int_t iDDL, Int_t& iDspMax, Int_t* iBusPerDSP) 
-const
+//______________________________________________________________________________
+AliMpBusPatch::~AliMpBusPatch()
 {
-/// calculates the number of DSP & buspatch per block
-
-  Int_t iBusPerBlk = fBusInDDL[iDDL].GetSize()/2; //per block
-
-  iDspMax =  iBusPerBlk/5; //number max of DSP per block
-  if (iBusPerBlk % 5 != 0)
-    iDspMax += 1;
-  
-  for (Int_t i = 0; i < iDspMax; i++) {
-    if ((iBusPerBlk -= 5) > 0) 
-      iBusPerDSP[i] = 5;
-    else 
-      iBusPerDSP[i] = iBusPerBlk + 5;
-  }
-  
-}
-//____________________________________________________________________
-void AliMpBusPatch::ReadBusPatchFile()
-{
-/// idDE <> buspatch <> iDDL map's
-  
-   TString infile = AliMpFiles::BusPatchFilePath();
-
-   ifstream in(infile, ios::in);
-   if (!in) AliError("DetElemIdToBusPatch.dat not found.");
-       
-   char line[80];
-
-   while ( in.getline(line,80) ) {
-
-      if ( line[0] == '#' ) continue;
-
-      TString tmp(AliMpHelper::Normalize(line));
-
-      Int_t blankPos  = tmp.First(' ');
-      Int_t blankPos1 = tmp.Last(' ');
-
-      TString sDE(tmp(0, blankPos));
-
-      Int_t idDE = atoi(sDE.Data());
-
-      TString sDDL(tmp(blankPos1 + 1, tmp.Length()-blankPos1));
-
-      Int_t iDDL = atoi(sDDL.Data());
-
-      // always working local DDL number... for the moment.
-
-      // not really needed remove for stand alone purpose (Ch.F)
-      //      if (iDDL >= AliDAQ::DdlIDOffset("MUONTRK"))
-      // 	iDDL -= AliDAQ::DdlIDOffset("MUONTRK");
-
-
-      TString busPatch(tmp(blankPos + 1,blankPos1-blankPos-1));
-      AliDebug(3,Form("idDE %d buspatch %s iDDL %d\n", idDE, busPatch.Data(), iDDL));
-      AddDetElem(iDDL, idDE);
-
-      TArrayI busPatchList;
-      // decoding range of buspatch
-      AliMpHelper::DecodeName(busPatch,';',busPatchList);
-      
-      // filling buspatch -> idDE
-
-      for (Int_t i = 0; i < busPatchList.GetSize(); i++) {
-	fBusPatchToDetElem.Add((Long_t)busPatchList[i],(Long_t)idDE);
-	fBusPatchToDDL.Add((Long_t)busPatchList[i],(Long_t)iDDL);
-	AddBus(iDDL, busPatchList[i]);
-      }
-   
-      // filling idDE -> buspatch list (vector)
-      fDetElemIdToBusPatch.Add((Long_t)idDE, (Long_t)(new TArrayI(busPatchList))); 
-
-    }
-   
-  in.close();
-
-}
-//____________________________________________________________________
-void AliMpBusPatch::AddBus(Int_t iDDL, Int_t busPatch)
-{
-/// add bus patch number per DDL
-
-  fBusInDDL[iDDL].Set(fBusInDDL[iDDL].GetSize() + 1);
-  fBusInDDL[iDDL].AddAt(busPatch, fBusInDDL[iDDL].GetSize() - 1);
-
+/// Destructor
 }
 
-//____________________________________________________________________
-void AliMpBusPatch::AddDetElem(Int_t iDDL, Int_t detElem)
+//
+// public methods
+//
+
+//______________________________________________________________________________
+Bool_t AliMpBusPatch::AddManu(Int_t manuId)
 {
-/// add DE per DDL
+/// Add detection element with given detElemId.
+/// Return true if the detection element was added
 
-  fDeInDDL[iDDL].Set(fDeInDDL[iDDL].GetSize() + 1);
-  fDeInDDL[iDDL].AddAt(detElem, fDeInDDL[iDDL].GetSize() - 1);
+  if ( HasManu(manuId) ) {
+    AliWarningStream() 
+      << "Manu with manuId=" << manuId << " already present."
+      << endl;
+    return false;
+  }    
 
+  fManus.Add(manuId);
+  return true;
+}   
+
+//______________________________________________________________________________
+Int_t AliMpBusPatch::GetNofManus() const
+{  
+/// Return the number of detection elements connected to this DDL
+
+  return fManus.GetSize(); 
 }
-//____________________________________________________________________
-Int_t AliMpBusPatch::NextBusInDDL(Int_t iDDL)
-{
-/// Next bus patch number in DDL
 
-  if (fBusItr[iDDL] >= fBusInDDL[iDDL].GetSize())
-    return -1;
+//______________________________________________________________________________
+Int_t  AliMpBusPatch::GetManuId(Int_t index) const
+{  
+/// Return the detection element by index (in loop)
 
-  return fBusInDDL[iDDL].At(fBusItr[iDDL]++);
+  return fManus.GetValue(index); 
+}
 
+//______________________________________________________________________________
+Bool_t  AliMpBusPatch::HasManu(Int_t manuId) const
+{  
+/// Return true if bus patch has manu with given manuId
+
+  return fManus.HasValue(manuId); 
 }
 
 
-//____________________________________________________________________
-void AliMpBusPatch::ResetBusItr(Int_t iDDL)
-{
-/// reset bus iterator for the given DDL
-
-  fBusItr[iDDL] = 0;
-}
-
-//____________________________________________________________________
-void AliMpBusPatch::Sort()
-{
-/// sort bus patch number for all DDL
-
-  // put it hardware wise
-  // this method is not used for the moment.
-  Int_t numberOfDdls = 20;
-
-  for (Int_t j = 0; j < numberOfDdls; j++) {
-    Sort(fBusInDDL[j], 0, fBusInDDL[j].GetSize() - 1);
-
-    if (AliLog::GetGlobalDebugLevel() == 1) {
-      printf("DDL %d\n",j);
-      for (Int_t i = 0; i <  fBusInDDL[j].GetSize(); i++)
-	printf("buspatch %d index %d\n",fBusInDDL[j].At(i), i);
-    }
-  } 
-
-}
-
-//____________________________________________________________________
-void AliMpBusPatch::Sort(TArrayI& arr, Int_t start, Int_t end)
-{
-/// sort bus patch number per DDL
-/// not really needed, but for future developments ?
-/// quicksort method, not in Root ?
-
-  Int_t pivot;
-  Int_t starth;
-  Int_t endh; // store pivot # keep start & end in memory for split
-
-  starth = start;
-  endh = end;
-  pivot = arr[start];
-
-  while(start < end) {
-      while((arr[end] >= pivot) && (start < end))
-	end--;
-
-      if (start != end) {
-	  arr[start] = arr[end];
-	  start++;
-      }
-      while ((arr[start] <= pivot) && (start < end))
-	start++;
-
-      if (start != end) {
-	  arr[end] = arr[start];
-	  end--;
-      }
-  }
-
-  arr[start] = pivot;
-  pivot = start;
-  start = starth;
-  end   = endh;
-
-  if(start < pivot)
-    Sort(arr, start, pivot-1);
-
-  if(end > pivot)
-    Sort(arr, pivot+1, end);
-
-}
--- MUON/mapping/AliMpBusPatch.h
+++ MUON/mapping/AliMpBusPatch.h
@@ -2,74 +2,87 @@
  * See cxx source for full Copyright notice                               */
 
 // $Id$
-// $MpId: AliMpBusPatch.h,v 1.5 2006/05/24 13:58:16 ivana Exp $
+// $MpId: $ 
 
 /// \ingroup management
 /// \class AliMpBusPatch
-/// \brief Class that manages the maps buspatch<>DDL<>DE 
+/// \brief The class defines the properties of BusPatch
 ///
-/// Calculates also the maximum DSP and buspatch numbers for a given DE
-///
-/// \author Ch. Finck; Subatech Nantes
+/// \author Ivana Hrivnacova, IPN Orsay
 
-#ifndef ALI_MP_BUSPATCH_H
-#define ALI_MP_BUSPATCH_H
+#ifndef ALI_MP_BUS_PATCH_H
+#define ALI_MP_BUS_PATCH_H
 
 #include <TObject.h>
 
-#include <TExMap.h>
-#include <TArrayI.h>
+#include "AliMpArrayI.h"
 
-class TExMapIter;
+class AliMpBusPatch : public  TObject {
 
-class AliMpBusPatch : public TObject
-{
+  public:
+    AliMpBusPatch(Int_t id, Int_t deId, Int_t ddlId);
+    AliMpBusPatch(TRootIOCtor* /*ioCtor*/);
+    virtual ~AliMpBusPatch();
 
- public:
+    // static methods
+    static Int_t GetGlobalBusID(Int_t localID, Int_t ddlID);
+    static Int_t GetLocalBusID(Int_t globalID, Int_t ddlID);
 
-  AliMpBusPatch();
-  virtual ~AliMpBusPatch();
-  
-  // static methods
-  static Int_t GetGlobalBusID(Int_t localID, Int_t ddlID);
-  static Int_t GetLocalBusID(Int_t globalID, Int_t ddlID);
- 
+    // methods 
+    Bool_t AddManu(Int_t manuId);
 
-  // methods
-  void ReadBusPatchFile();
-  void GetDspInfo(Int_t iDDL, Int_t& iDspMax, Int_t* iBusPerDSP) const;
+    // get methods
+    Int_t  GetId() const;
+    Int_t  GetDEId() const;
+    Int_t  GetDdlId() const;
+    Int_t  GetNofManus() const;
+    Int_t  GetManuId(Int_t index) const;
+    Bool_t HasManu(Int_t manuId) const;
+    
 
-  Int_t    GetDEfromBus(Int_t busPatchId);
-  TArrayI* GetBusfromDE(Int_t idDE);
-  Int_t    GetDDLfromBus(Int_t busPatchId);
-  void     AddBus(Int_t nDDL, Int_t busPatch);
-  void     AddDetElem(Int_t nDDL, Int_t detElem);
-  void     Sort();
-  TArrayI  GetBusInDDL(Int_t nDDL) {return fBusInDDL[nDDL];}
-  TArrayI  GetDeInDDL(Int_t nDDL) {return fDeInDDL[nDDL];}
-  Int_t    NextBusInDDL(Int_t iDDL);
-  void     ResetBusItr(Int_t iDDL);
-  TExMapIter   GetBusItr() {return TExMapIter(&fBusPatchToDDL);}
+  private:
+    AliMpBusPatch();
+    AliMpBusPatch(const AliMpBusPatch& rhs);
+    AliMpBusPatch& operator=(const AliMpBusPatch& rhs);
 
- private:
-  AliMpBusPatch(const AliMpBusPatch& src);
-  AliMpBusPatch& operator = (const AliMpBusPatch& src) ;
+    // static data members	
+    static const Int_t  fgkOffset; //< Offset for conversion global/local ID  
 
-  static const Int_t  fgkOffset;     //< Offset for conversion global/local ID  
-
-  TExMap fDetElemIdToBusPatch;       //!< Map from idDE to BusPatch   
-  TExMap fBusPatchToDetElem;         //!< Map from BusPatch to idDE
-  TExMap fBusPatchToDDL;             //!< Map from BusPatch to iDDL
-
-  TArrayI fBusInDDL[20];             //!< buspatch array per DDL
-  Int_t   fBusItr[20];               //!< buspatch in DDL iterator
-  TArrayI fDeInDDL[20];              //!< detElem array per DDL
-
-
-  void Sort(TArrayI& arr, Int_t start, Int_t end);
-
-  ClassDef(AliMpBusPatch,1) //utility class for the motif type
+    // data members	
+    Int_t        fId;    ///< Identifier (unique)
+    Int_t        fDEId;  ///< Detection element to which this bus patch is connected
+    Int_t        fDdlId; ///< DDL to which this bus patch is connected
+    AliMpArrayI  fManus; ///< Manu Ids connected to this bus patch
+     
+  ClassDef(AliMpBusPatch,1)  // The class collectiong electronics properties of DDL
 };
 
+// inline functions
 
-#endif //ALI_MP_BUSPATCH_H
+/// Return the unique Id
+inline Int_t AliMpBusPatch::GetId() const
+{  return fId; }
+
+/// Return the Detection element Id
+inline Int_t AliMpBusPatch::GetDEId() const
+{  return fDEId; }
+
+/// Return the Ddl  Id
+inline Int_t AliMpBusPatch::GetDdlId() const
+{  return fDdlId; }
+
+#endif //ALI_BUS_PATCH_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
