9ecf34089a290572bf7c156aec27dc85eb7a1997
alla <alla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-01-30 12:32:52 +0000

    for compile lookup

--- T0/T0baseLinkDef.h
+++ T0/T0baseLinkDef.h
@@ -8,16 +8,13 @@
 #pragma link off all classes;
 #pragma link off all functions;
  
-#pragma link C++ typedef  AliSTARTdigit;
-#pragma link C++ typedef  AliSTARTLoader;
-#pragma link C++ typedef  AliSTARTTrigger;
-#pragma link C++ typedef  AliSTARTCalibData;
-#pragma link C++ typedef  AliSTARTParameters;
  
 #pragma link C++ class  AliT0digit+;
 #pragma link C++ class  AliT0Loader+;
 #pragma link C++ class  AliT0Trigger+;
 #pragma link C++ class  AliT0CalibData+;
 #pragma link C++ class  AliT0Parameters+;
+#pragma link C++ class  AliT0LookUpValue+;
+#pragma link C++ class  AliT0LookUpKey+;
 
 #endif
