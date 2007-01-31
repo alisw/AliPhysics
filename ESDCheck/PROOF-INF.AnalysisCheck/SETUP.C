39f1d9b01694dbe6115423e21eb9fa09492677d2
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-01-31 17:50:23 +0000

    This was completely wrong

--- ESDCheck/PROOF-INF.AnalysisCheck/SETUP.C
+++ ESDCheck/PROOF-INF.AnalysisCheck/SETUP.C
@@ -2,12 +2,12 @@ void SETUP()
 {
 
    // Load the ESD library
-   gSystem->Load("libPWG4Gamma");
+   gSystem->Load("libAnalysisCheck");
 
    // Set the Include paths
-   gSystem->SetIncludePath("-I$ROOTSYS/include -IPWG4");
+   gSystem->SetIncludePath("-I$ROOTSYS/include -IAnalysisCheck");
    gROOT->ProcessLine(".include PWG4");
 
    // Set our location, so that other packages can find us
-   gSystem->Setenv("PWG4Gamma_INCLUDE", "PWG4");
+   gSystem->Setenv("AnalysisCheck_INCLUDE", "AnalysisCheck");
 }
