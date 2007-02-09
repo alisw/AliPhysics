c20fdfc7de2c60aa8814cf235cbd5e4620af253b
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-09 13:57:26 +0000

    New definitions

--- HLT/PHOS/AliHLTPHOSCommonDefs.h
+++ HLT/PHOS/AliHLTPHOSCommonDefs.h
@@ -2,18 +2,30 @@
 #define ALIPHOSCOMMONDEFS_H
 
 //Hardware constants
-#define N_RCUS             4   /**<Number of RCUs per Module*/
-#define N_BRANCHES         2   /**<Number of branches per RCU*/
-#define N_FEECS           14   /**<Number of Frontend cards per branch*/
-#define N_ALTROS           4   /**<Number of ALTROs per frontend card*/
-#define N_ALTROCHANNELS   16   /**<Number of readout channles per ALTRO*/
+#define N_RCUS             4      /**<Number of RCUs per Module*/
+#define N_BRANCHES         2      /**<Number of branches per RCU*/
+#define N_FEECS           14      /**<Number of Frontend cards per branch*/
+#define N_ALTROS           4      /**<Number of ALTROs per frontend card*/
+#define N_ALTROCHANNELS   16      /**<Number of readout channles per ALTRO*/
 
 //Geometry constants
-#define N_MODULES          5   /**<Number of modules of the PHOS detector*/
-#define N_ROWS_MOD         64  /**<Number of rows per module*/       
-#define N_COLUMNS_MOD      56  /**<Number of columns per module*/ 
-#define N_ROWS_RCU         32  /**<Number of rows per module*/       
-#define N_COLUMNS_RCU      28  /**<Number of columns per module*/ 
-#define N_GAINS            2   /**<Number of gains per ALTRO channel*/
+#define N_MODULES          5      /**<Number of modules of the PHOS detector*/
+#define N_ROWS_MOD         64     /**<Number of rows per module*/       
+#define N_COLUMNS_MOD      56     /**<Number of columns per module*/ 
+#define N_ROWS_RCU         32     /**<Number of rows per module*/       
+#define N_COLUMNS_RCU      28     /**<Number of columns per module*/ 
+#define N_GAINS            2      /**<Number of gains per ALTRO channel*/
+
+#define N_DATATYPES        10 
+
+//peakfinder constatnts
+#define PF_MAX_PATH_LENGTH 256
+#define PF_VECTOR_DIR "/HLT/PHOS/PFVectors"
+#define PF_DEFAULT_N_SAMPLES 70
+#define PF_DEFAULT_STARTINDEX 0
+
+//general altro signal constatnts
+#define DEFAULT_TAU 2    /**<Assume that the signal rise time of the altrp pulses is 2 us (nominal value of the electronics)*/
+#define DEFAULT_FS  10   /**<Assume that the signal is samples with 10 MHZ samle rate*/
 
 #endif
