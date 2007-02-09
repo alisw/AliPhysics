9836bc1560977632ed4320d90f0a46c230d1de85
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-09 15:38:13 +0000

    Added copyright notice

--- HLT/PHOS/AliHLTPHOSModuleCellEnergyDataStruct.h
+++ HLT/PHOS/AliHLTPHOSModuleCellEnergyDataStruct.h
@@ -1,8 +1,23 @@
 #ifndef ALIHLTPHOSNODULECELLENERGYDATASTRUCT_H
 #define ALIHLTPHOSNODULECELLENERGYDATASTRUCT_H
 
+/***************************************************************************
+ * Copyright(c) 2007, ALICE Experiment at CERN, All rights reserved.       *
+ *                                                                         *
+ * Author: Per Thomas Hille <perthi@fys.uio.no> for the ALICE HLT Project. *
+ * Contributors are mentioned in the code where appropriate.               *
+ *                                                                         *
+ * Permission to use, copy, modify and distribute this software and its    *
+ * documentation strictly for non-commercial purposes is hereby granted    *
+ * without fee, provided that the above copyright notice appears in all    *
+ * copies and that both the copyright notice and this permission notice    *
+ * appear in the supporting documentation. The authors make no claims      *
+ * about the suitability of this software for any purpose. It is           *
+ * provided "as is" without express or implied warranty.                   *
+ **************************************************************************/
+
 #include "AliHLTPHOSCommonDefs.h"
-#include "AliHLTPHOSValiCellDataStruct.h"
+#include "AliHLTPHOSValidCellDataStruct.h"
 
 struct AliHLTPHOSModuleCellEnergyDataStruct
 {
@@ -10,7 +25,7 @@ struct AliHLTPHOSModuleCellEnergyDataStruct
   AliHLTUInt16_t fCnt;
   //  AliHLTUInt16_t fValidData[N_ROWS_MOD*N_COLUMNS_MOD*N_GAINS];
   
-  AliHLTPHOSValidCellDataStruct fValidData[N_RCUS]; 
+  AliHLTPHOSValidCellDataStruct fValidData[N_MODULES*N_ROWS_MOD*N_COLUMNS_MOD*N_GAINS]; 
 
   unsigned long cellEnergies[N_ROWS_MOD][N_COLUMNS_MOD][N_GAINS];
 };
