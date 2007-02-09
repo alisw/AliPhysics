fa5f3e73674ff45b32f0e8c9e19c56fa2096db91
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-09 15:44:19 +0000

    added copyright note

--- HLT/PHOS/AliHLTPHOSValidCellDataStruct.h
+++ HLT/PHOS/AliHLTPHOSValidCellDataStruct.h
@@ -1,15 +1,29 @@
 #ifndef ALIHLTPHOSVALIDCELLDATASTRUCT_H
 #define ALIHLTPHOSVALIDCELLDATASTRUCT_H 
 
-#include "AliHLTPHOSCommonDefs.h"
-//#include "AliHLTDataTypes.h"
+/***************************************************************************
+ * Copyright(c) 2007, ALICE Experiment at CERN, All rights reserved.       *
+ *                                                                         *
+ * Author: Per Thomas Hille <perthi@fys.uio.no> for the ALICE HLT Project. *
+ * Contributors are mentioned in the code where appropriate.               *
+ *                                                                         *
+ * Permission to use, copy, modify and distribute this software and its    *
+ * documentation strictly for non-commercial purposes is hereby granted    *
+ * without fee, provided that the above copyright notice appears in all    *
+ * copies and that both the copyright notice and this permission notice    *
+ * appear in the supporting documentation. The authors make no claims      *
+ * about the suitability of this software for any purpose. It is           *
+ * provided "as is" without express or implied warranty.                   *
+ **************************************************************************/
+
+//#include "AliHLTPHOSCommonDefs.h"
+#include "AliHLTDataTypes.h"
 //#include "Rtypes.h"
+
+
 
 struct AliHLTPHOSValidCellDataStruct
 {
-  //  AliHLTUInt16_t fRcuX;
-  //  AliHLTUInt16_t fRcuZ;
-  //  AliHLTUInt16_t fModuleID;
   AliHLTUInt16_t fRow;
   AliHLTUInt16_t fCol;
   AliHLTUInt16_t fGain;
