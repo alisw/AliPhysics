b0a5fcd5f79600a6f4f1729f9672a039cabd0d27
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-14 15:40:57 +0000

    The multidimensional array holding celle energies for was removed.

--- HLT/PHOS/AliHLTPHOSRcuCellEnergyDataStruct.h
+++ HLT/PHOS/AliHLTPHOSRcuCellEnergyDataStruct.h
@@ -20,12 +20,11 @@
 
 struct AliHLTPHOSRcuCellEnergyDataStruct
 {
+  AliHLTUInt16_t fModuleID;
   AliHLTUInt16_t fRcuX;
   AliHLTUInt16_t fRcuZ;
-  AliHLTUInt16_t fModuleID;
   AliHLTUInt16_t fCnt;
   AliHLTPHOSValidCellDataStruct fValidData[N_ROWS_RCU*N_COLUMNS_RCU*N_GAINS];
-  Double_t fCellEnergies[N_ROWS_RCU][N_COLUMNS_RCU][N_GAINS];
   
 };
 
