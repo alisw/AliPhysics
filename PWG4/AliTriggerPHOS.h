5cfb173bac634bea5ea616486cc985ccdec4dee6
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-22 17:33:39 +0000

    New class

---
+++ PWG4/AliTriggerPHOS.h
@@ -0,0 +1,42 @@
+#ifndef ALIPHOSQATASK_H
+#define ALIPHOSQATASK_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice     */
+//______________________________________________________________________________
+// An analysis task to check the PHOS photon data in simulated data
+//
+//*-- Yves Schutz 
+//////////////////////////////////////////////////////////////////////////////
+
+#include <TTree.h> 
+#include "AliAnalysisTask.h"  
+
+class AliESD ; 
+class TNtuple ;
+class TH1D ; 
+class TH1I ; 
+
+class AliTriggerPHOS : public AliAnalysisTask {
+
+public:
+  AliTriggerPHOS(const char *name) ;
+  virtual ~AliTriggerPHOS() ;
+   
+  virtual void Exec(Option_t * opt = "") ;
+  virtual void ConnectInputData(Option_t *);
+  virtual void CreateOutputObjects();
+  virtual void Terminate(Option_t * opt = "") ;
+
+private:
+  TTree   * fChain ;            //!pointer to the analyzed TTree or TChain
+  AliESD  * fESD ;              //! Declaration of leave types
+
+  TObjArray * fOutputContainer ; //! output data container
+
+  // Histograms
+  TNtuple * fNtTrigger22 ;
+  TNtuple * fNtTriggerNN ;
+
+  ClassDef(AliTriggerPHOS, 0); // a PHOS photon analysis task 
+};
+#endif // ALIPHOSQATASK_H
