70a2ff61b0886874bda96ee1898704889e7fd757
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-25 22:27:32 +0000

    New defintitons

--- HLT/PHOS/AliHLTPHOSCommonDefs.h
+++ HLT/PHOS/AliHLTPHOSCommonDefs.h
@@ -2,20 +2,42 @@
 #define ALIPHOSCOMMONDEFS_H
 
 //Hardware constants
-#define N_RCUS             4      /**<Number of RCUs per Module*/
-#define N_BRANCHES         2      /**<Number of branches per RCU*/
-#define N_FEECS           14      /**<Number of Frontend cards per branch*/
-#define N_ALTROS           4      /**<Number of ALTROs per frontend card*/
-#define N_ALTROCHANNELS   16      /**<Number of readout channles per ALTRO*/
-#define ALTRO_MAX_SAMPLES 1008    /**<The maximum number of samples of the ALTRO*/
+#define N_MODULES          5                             /**<Number of modules of the PHOS detector*/
+#define N_RCUS             4                             /**<Number of RCUs per Module*/
+#define N_RCUS_PER_MODULE  4                             /**<Number of RCUs per Module*/
+#define N_RCUS_PER_TOTAL   N_MODULES*N_RCUS_PER_MODULE   /**<Total number of RCUs for PHOS*/
+#define N_BRANCHES         2                             /**<Number of branches per RCU*/
+#define N_FEECS           14                             /**<Number of Frontend cards per branch*/
+#define N_ALTROS           4                             /**<Number of ALTROs per frontend card*/
+#define N_ALTROCHANNELS   16                             /**<Number of readout channles per ALTRO*/
+#define ALTRO_MAX_SAMPLES 1008                           /**<The maximum number of samples of the ALTRO*/
 
 //Geometry constants
-#define N_MODULES          5      /**<Number of modules of the PHOS detector*/
-#define N_ROWS_MOD         64     /**<Number of rows per module*/       
-#define N_COLUMNS_MOD      56     /**<Number of columns per module*/ 
-#define N_ROWS_RCU         32     /**<Number of rows per module*/       
-#define N_COLUMNS_RCU      28     /**<Number of columns per module*/ 
-#define N_GAINS            2      /**<Number of gains per ALTRO channel*/
+//#define N_MODULES          5                           /**<Number of modules of the PHOS detector*/
+
+//#define N_ROWS_MOD         64                          /**<Number of rows per module*/       
+//#define N_COLUMNS_MOD      56                          /**<Number of columns per module*/ 
+
+#define N_ROWS_MOD         56                            /**<Number of rows per module*/       
+#define N_COLUMNS_MOD      64                            /**<Number of columns per module*/ 
+
+#define N_ROWS_RCU         28                            /**<Number of rows per module*/       
+#define N_COLUMNS_RCU      32   
+#define N_ZROWS_RCU        N_ROWS_RCU                                 /**<Number of rows per module*/       
+#define N_XCOLUMNS_RCU     N_COLUMNS_RCU 
+
+#define N_ZROWS_MOD        N_ROWS_MOD                            /**<Number of rows per module*/       
+#define N_XCOLUMNS_MOD     N_COLUMNS_MOD                            /**<Number of columns per module*/ 
+
+//#define N_ROWS_RCU         32                            /**<Number of rows per module*/       
+//#define N_COLUMNS_RCU      28                            /**<Number of columns per module*/ 
+
+
+
+//#define N_ZROWS_RCU        N_ROWS_RCU                                 /**<Number of rows per module*/       
+//#define N_XCOLUMNS_RCU     N_COLUMNS_RCU 
+
+#define N_GAINS            2                             /**<Number of gains per ALTRO channel*/
 
 #define N_DATATYPES        10 
 
