35f9e34824f7236f71df21616e00addea62fca77
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-02-26 17:12:09 +0000

    Set path marks in track container.

--- EVE/alice-macros/kine_daughter_tracks.C
+++ EVE/alice-macros/kine_daughter_tracks.C
@@ -9,10 +9,8 @@ kine_daughter_tracks(Int_t label, Reve::TrackList* cont = 0)
   if (label < 0) {
     Warning("kine_daughter_tracks", "label not set.");
     return 0;
-  }
- 
-  //Bool_t create_cont;
-  //create_cont = cont ? 0:1;
+  }  
+
   AliRunLoader* rl =  Alieve::Event::AssertRunLoader();
   rl->LoadKinematics();
   AliStack* stack = rl->Stack();
@@ -22,9 +20,8 @@ kine_daughter_tracks(Int_t label, Reve::TrackList* cont = 0)
   {
     if (cont == 0) 
     {
-      printf("create container \n");
-      cont =  
-	new TrackList(Form("%d kine_daughter_tracks %d", p->GetNDaughters(), label));
+      cont = new TrackList(Form("%d kine_daughter_tracks %d",
+				p->GetNDaughters(), label));
 
       Reve::TrackRnrStyle* rnrStyle = cont->GetRnrStyle();
       // !!! Watch the '-', apparently different sign convention then for ESD.
@@ -33,6 +30,7 @@ kine_daughter_tracks(Int_t label, Reve::TrackList* cont = 0)
       sprintf(tooltip,"%d, mother %d", p->GetNDaughters(), label);
       cont->SetTitle(tooltip);  
       cont->SelectByPt(0, 100);
+      cont->SetEditPathMarks(kTRUE);
       rnrStyle->fColor = 8;  
       rnrStyle->fMaxOrbs = 8;  
       gReve->AddRenderElement(cont);
@@ -48,6 +46,13 @@ kine_daughter_tracks(Int_t label, Reve::TrackList* cont = 0)
       gReve->AddRenderElement(cont, track);
     }
 
+    // set path marks
+    if(cont->GetEditPathMarks()) {
+      Alieve::KineTools kt; 
+      rl->LoadTrackRefs();
+      kt.SetPathMarks(cont,stack, rl->TreeTR());
+    }
+
     cont->UpdateItems(); // update list tree
     cont->MakeTracks();
     cont->MakeMarkers();
@@ -56,7 +61,7 @@ kine_daughter_tracks(Int_t label, Reve::TrackList* cont = 0)
   }
   else 
   {
-    printf("Particle %d does not have daughters.", label);
+    Warning("kine_daughters_tracks", Form("Particle %d does not have daughters %d.", label));
     return 0;
   }
 
