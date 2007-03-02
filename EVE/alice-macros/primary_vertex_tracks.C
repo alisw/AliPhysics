c2458d2b762fc2ec7a6091b399a00ee1dde25e0a
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-02 10:33:39 +0000

    New file: import tracks that were used for primary vertex fit (asked for by Andrea Dainese).

---
+++ EVE/alice-macros/primary_vertex_tracks.C
@@ -0,0 +1,28 @@
+Reve::TrackList* primary_vertex_tracks()
+{
+  Reve::LoadMacro("esd_tracks.C");
+  AliESD       *esd = Alieve::Event::AssertESD();
+  AliESDVertex *pv  = esd->GetPrimaryVertex();
+
+  Reve::TrackList* cont = new Reve::TrackList("Tracks for Primary Vertex"); 
+  cont->SetMainColor(Color_t(7));
+  Reve::TrackRnrStyle* rnrStyle = cont->GetRnrStyle();
+  rnrStyle->SetMagField( esd->GetMagneticField() );
+
+  gReve->AddRenderElement(cont);
+
+  for (Int_t n=0; n<pv->GetNIndices(); n++)
+  {
+    AliESDtrack* at = esd->GetTrack(pv->GetIndices()[n]);
+    Reve::Track* track = esd_make_track(rnrStyle, n, at, at);
+    track->SetLineWidth(4);
+    track->SetLineStyle(7);
+    gReve->AddRenderElement(cont, track);
+  }
+
+  cont->MakeTracks();
+  cont->MakeMarkers();
+  gReve->Redraw3D();
+
+  return cont;
+}
