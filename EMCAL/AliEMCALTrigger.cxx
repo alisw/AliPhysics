96124a9954134f7bab5ce16f8a9e797a54c9b007
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-05 17:16:04 +0000

    Removing some warnings

--- EMCAL/AliEMCALTrigger.cxx
+++ EMCAL/AliEMCALTrigger.cxx
@@ -13,10 +13,6 @@
  * provided "as is" without express or implied warranty.                  *
  **************************************************************************/
 /* $Id$ */
-/* $Log$ */
-/* Revision 1.7  2007/03/04 14:23:50  gustavo */
-/* Trigger maximum amplitude patch value and postion stored in ESDs, possibility to isolate patch in SuperModule or TRU acceptance implemented */
-/* */
 
 //_________________________________________________________________________  
 //
