fe8da8f3a7bea411a18ffb1ea2ad798802726800
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-07 18:12:59 +0000

    THis file was once more the wrong one (it is the one from PWG4), how can this happen ?

--- ESDCheck/PROOF-INF.AnalysisCheck/SETUP.C
+++ ESDCheck/PROOF-INF.AnalysisCheck/SETUP.C
@@ -2,12 +2,12 @@ void SETUP()
 {
 
    // Load the ESD library
-   gSystem->Load("libPWG4Gamma");
+   gSystem->Load("libAnalysisCheck");
 
    // Set the Include paths
-   gSystem->SetIncludePath("-I$ROOTSYS/include -IPWG4Gamma");
-   gROOT->ProcessLine(".include PWG4Gamma");
+   gSystem->SetIncludePath("-I$ROOTSYS/include -IAnalysisCheck");
+   gROOT->ProcessLine(".include AnalysisCheck");
 
    // Set our location, so that other packages can find us
-   gSystem->Setenv("PWG4Gamma_INCLUDE", "PWG4Gamma");
+   gSystem->Setenv("AnalysisCheck_INCLUDE", "AnalysisCheck");
 }
