2fb0300c4fd487bc0227b74c4bea5fd8921a6836
panos <panos@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-08 12:57:05 +0000

    New analysis frameowrk using PROOF - Adding the needed macros.

---
+++ PWG2/AnalysisMacros/Proof/runAnalysis.C
@@ -0,0 +1,39 @@
+void runAnalysis() {
+  TStopwatch timer;
+  timer.Start();
+  
+  runProofESD("AliAnalysisTaskPt.cxx+");
+
+  timer.Stop();
+  timer.Print();
+}
+
+//==========================================//
+void runProofESD(const char *selectorfile) {
+  //the next line should point to the local $ALICE_ROOT
+  //that contains the latest ANALYSIS developments
+  gSystem->AddIncludePath("-I\"$ALICE_ROOT/include\"");
+  printf("****** Connect to PROOF *******\n");
+  TProof::Open("proof://<username>@lxb6046.cern.ch"); 
+
+  // Enable the Analysis Package
+  gProof->UploadPackage("ESD.par");
+  gProof->EnablePackage("ESD");
+  gProof->UploadPackage("ANALYSIS.par");
+  gProof->EnablePackage("ANALYSIS");
+  
+  // You should get this macro and the txt file from:
+  // http://aliceinfo.cern.ch/Offline/Analysis/CAF/
+  gROOT->LoadMacro("CreateESDChain.C");
+  TChain* chain = 0x0;
+  chain = CreateESDChain("ESD100_110_v2.txt",10);
+
+  gROOT->LoadMacro(selectorfile);
+  gROOT->LoadMacro("demoCAF.C");
+  demoCAF(chain,"proof");
+ 
+  gSystem->Exec("rm -rf ESD ANALYSIS");
+}
+
+
+                                                                                                                                               
