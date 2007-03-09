b9ac9c349be22c2c3a7de63759ed3863e39ffd32
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-09 17:22:20 +0000

    Adding comment

--- MUON/AliMUONTrackerPreprocessor.cxx
+++ MUON/AliMUONTrackerPreprocessor.cxx
@@ -63,6 +63,8 @@ AliMUONTrackerPreprocessor::~AliMUONTrackerPreprocessor()
 void
 AliMUONTrackerPreprocessor::Initialize(Int_t run, UInt_t startTime, UInt_t endTime)
 {
+  /// Re-register the subprocessor(s) depnding on the actual runTYpe
+
   ClearSubprocessors();
   
   TString runType = GetRunType();
