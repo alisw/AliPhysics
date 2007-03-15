85b45b2357e0e139eaafa9da21a5aeafe3111a71
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-15 14:39:38 +0000

    From Bogdan: updates macros for MUON display.

--- EVE/alice-macros/MUON_geom.C
+++ EVE/alice-macros/MUON_geom.C
@@ -4,13 +4,9 @@ void MUON_geom()
 {
   using namespace std;
 
-  AliRunLoader* rl =  Alieve::Event::AssertRunLoader();
-  TString fileName = rl->GetFileName();
-  Int_t length = fileName.Length();
-  fileName.Resize(length-11);
-  fileName.Append("geometry.root");
-
-  gGeoManager = gReve->GetGeometry(fileName.Data());
+  TString dataPath = TString(Alieve::gEvent->GetTitle());
+  dataPath.Append("/geometry.root");
+  gGeoManager = gReve->GetGeometry(dataPath.Data());
 
   TGeoNode *node1 = gGeoManager->GetTopVolume()->FindNode("DDIP_1");
   TGeoNode *node2 = gGeoManager->GetTopVolume()->FindNode("YOUT1_1");
@@ -28,6 +24,6 @@ void MUON_geom()
   re3->SetUseNodeTrans(kTRUE);
   gReve->AddGlobalRenderElement(re3);
 
-  gReve->Redraw3D();
+  gReve->Redraw3D(kTRUE);
 
 }
