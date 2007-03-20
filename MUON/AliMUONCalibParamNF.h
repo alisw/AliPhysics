e33719dc5620d4691792a4be55b3e83c2af0b2d6
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-20 15:11:34 +0000

    Adding new calibration parameter class, a little bit more generic than before (Laurent)

---
+++ MUON/AliMUONCalibParamNF.h
@@ -0,0 +1,58 @@
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+* See cxx source for full Copyright notice                               */
+
+// $Id$
+
+/// \ingroup base
+/// \class AliMUONCalibParamNF
+/// \brief Implementation of AliMUONVCalibParam for tuples of floats
+/// 
+//  Author Laurent Aphecetche
+
+#ifndef ALIMUONCALIBPARAMNF_H
+#define ALIMUONCALIBPARAMNF_H
+
+#ifndef ALIMUONVCALIBPARAM_H
+#  include "AliMUONVCalibParam.h"
+#endif
+
+class AliMUONCalibParamNF : public AliMUONVCalibParam
+{
+public:
+  AliMUONCalibParamNF();
+  AliMUONCalibParamNF(Int_t dimension, Int_t theSize, Float_t fillWithValue=0);
+  AliMUONCalibParamNF(const AliMUONCalibParamNF& other);
+  AliMUONCalibParamNF& operator=(const AliMUONCalibParamNF& other);
+  
+  virtual ~AliMUONCalibParamNF();
+
+  /// Return dimension
+  virtual Int_t Dimension() const { return fDimension; }
+  
+  virtual void Print(Option_t* opt="") const;
+  
+  virtual void SetValueAsFloat(Int_t i, Int_t j, Float_t value);
+  virtual void SetValueAsInt(Int_t i, Int_t j, Int_t value);
+  
+  /// Return size - the number of float pair we hold
+  virtual Int_t Size() const { return fSize; } 
+
+  virtual Float_t ValueAsFloat(Int_t i, Int_t j=0) const;
+  virtual Int_t ValueAsInt(Int_t i, Int_t j=0) const;
+     
+private:
+  void CopyTo(AliMUONCalibParamNF& destination) const;
+  Int_t Index(Int_t i, Int_t j) const;  
+    
+private:
+  Int_t fDimension; ///< dimension of this object
+  Int_t fSize; ///< The number of float pair we hold
+  Int_t fN;    ///< The total number of floats we hold (fDimension*fSize)
+
+  /// The values array
+  Float_t* fValues; //[fN] The values array
+  
+  ClassDef(AliMUONCalibParamNF,1) // Container for calibration parameters
+};
+
+#endif
