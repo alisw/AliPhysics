e5feb743f48a7c496fc6f51ab668d0bb41d71cd1
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-22 15:24:08 +0000

    Fill ITSModuleStepper with a mix of modules from SPD, SDD and SSD.

--- EVE/alice-macros/its_module_stepper.C
+++ EVE/alice-macros/its_module_stepper.C
@@ -1,12 +1,13 @@
 #include "TGLViewer.h"
 
-namespace Alieve {
+namespace Alieve
+{
 class ITSModuleStepper;
 }
 
 Alieve::ITSModuleStepper* stepper = 0;
 
-void its_module_stepper(Int_t col = 4 , Int_t row = 3)
+void its_module_stepper(Int_t col = 4 , Int_t row = 4)
 {
   AliRunLoader* rl =  Alieve::Event::AssertRunLoader();
   rl->LoadDigits("ITS");
@@ -18,20 +19,31 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 3)
   gReve->DisableRedraw();
 
   Alieve::ITSModuleStepper* store = new Alieve::ITSModuleStepper(di);
-  store->SetStepper(col, row, 10, 10);
+  store->SetStepper(col, row, 10, 8);
   store->SetMainColor((Color_t)2);
   gReve->AddRenderElement(store);
   stepper = store;
   
   TRandom r(0);
   Int_t module;
+  // SPD
   for (Int_t i=0; i<40; ++i) {
-    module = r.Integer(51);
+    module = r.Integer(240);
+    store->AddToList(module);
+  }
+  // SDD
+  for (Int_t i=0; i<40; ++i) {
+    module = 240 + r.Integer(260);
+    store->AddToList(module);
+  }
+  // SSD
+  for (Int_t i=0; i<40; ++i) {
+    module = 500 + r.Integer(1600);
     store->AddToList(module);
   }
   store->Start();
-  gReve->EnableRedraw();
 
+  gReve->EnableRedraw();
   TGLViewer * v = (TGLViewer *)gPad->GetViewer3D();
   v->SetCurrentCamera(TGLViewer::kCameraOrthoXOY);
 }
