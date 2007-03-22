1e437be581a3d2e1176a66f4e2420ce7f37ead37
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-22 09:41:27 +0000

    Add a comment behind ClassDef

--- TRD/AliTRDPreprocessor.h
+++ TRD/AliTRDPreprocessor.h
@@ -29,7 +29,7 @@ class AliTRDPreprocessor : public AliPreprocessor
 
   private:
     
-    ClassDef(AliTRDPreprocessor,0);
+    ClassDef(AliTRDPreprocessor,0) // The SHUTTLE preprocessor for TRD
 
 };
 
