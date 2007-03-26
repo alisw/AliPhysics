02a7bf6e03ebfa00e9efe7537f4826edb3d7e97b
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-26 17:43:31 +0000

    Modify dimesions of stepper cell.

--- EVE/alice-macros/its_module_stepper.C
+++ EVE/alice-macros/its_module_stepper.C
@@ -16,22 +16,22 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 4)
   gReve->DisableRedraw();
 
 
-  Int_t sw = 10;
-  Int_t sh = 8;
-  Float_t CW = col*sw;
-  Float_t CH = col*sh;
-   
-  Float_t off_x = CW*0.07;
-  Float_t off_y = CH*0.1;
+  Float_t CW = 50;
+  Float_t CH = 40;
+  Float_t off_x = CW*0.3;
+  Float_t off_y = CH*0.31;
 
   Reve::ZTrans* mx;
-
   Bool_t rnrFrame = kTRUE;
-  Color_t wcol = 41;  
-  
+  Color_t wcol    = kCyan-9;  
+
+  Int_t col, row;
   // SPD  
+  col = 3; row = 4;
   Alieve::ITSModuleStepper* spd_lay1 = new Alieve::ITSModuleStepper(di);
-  spd_lay1->SetStepper(col, row, sw, sh);
+  spd_lay1->SetStepper(col, row, CW/col, CH*0.6/row);
+  mx = spd_lay1->PtrMainHMTrans();
+  mx->SetPos(0, 10, 0);
   spd_lay1->DisplayDet(0, 1);
   spd_lay1->SetName("SPD 1");
   spd_lay1->SetRnrFrame(rnrFrame);
@@ -39,18 +39,19 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 4)
   gReve->AddRenderElement(spd_lay1);
 
   Alieve::ITSModuleStepper* spd_lay2 = new Alieve::ITSModuleStepper(di);
-  spd_lay2->SetStepper(col, row, sw, sh);
+  spd_lay2->SetStepper(col, row, CW/col, CH*0.6/row);
   mx = spd_lay2->PtrMainHMTrans();
-  mx->SetPos(CW+off_x, 0, 0);
+  mx->SetPos(CW+off_x, 10, 0);
   spd_lay2->DisplayDet(0, 2);
   spd_lay2->SetName("SPD 2");
   spd_lay2->SetRnrFrame(rnrFrame);
   spd_lay2->SetWColor(wcol);
   gReve->AddRenderElement(spd_lay2);
-  
+
   // SDD  
+  col = 4; row = 3;
   Alieve::ITSModuleStepper* sdd_lay1 = new Alieve::ITSModuleStepper(di);
-  sdd_lay1->SetStepper(col, row, 10, 8);
+  sdd_lay1->SetStepper(col, row,  CW/col, CH/row);
   mx = sdd_lay1->PtrMainHMTrans();
   mx->SetPos(0, CH+off_y, 0);
   sdd_lay1->DisplayDet(1, 3);
@@ -59,8 +60,9 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 4)
   sdd_lay1->SetWColor(wcol);
   gReve->AddRenderElement(sdd_lay1);
 
+
   Alieve::ITSModuleStepper* sdd_lay2 = new Alieve::ITSModuleStepper(di);
-  sdd_lay2->SetStepper(col, row, 10, 8); 
+  sdd_lay2->SetStepper(col, row, CW/col, CH/row); 
   mx = sdd_lay2->PtrMainHMTrans();
   mx->SetPos(CW+off_x, CH+off_y, 0);
   sdd_lay2->DisplayDet(1, 4);
@@ -68,11 +70,12 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 4)
   sdd_lay2->SetRnrFrame(rnrFrame);
   sdd_lay2->SetWColor(wcol);
   gReve->AddRenderElement(sdd_lay2);
-  
 
-  // SSD  
+
+  // SSD 
+  col = 5; row = 3;
   Alieve::ITSModuleStepper* ssd_lay1 = new Alieve::ITSModuleStepper(di);
-  ssd_lay1->SetStepper(col, row, 10, 8);
+  ssd_lay1->SetStepper(col, row,  CW/col, CH/row);
   mx = ssd_lay1->PtrMainHMTrans();
   mx->SetPos(0, 2*(CH+off_y), 0);
   ssd_lay1->DisplayDet(2, 5);
@@ -81,8 +84,9 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 4)
   ssd_lay1->SetWColor(wcol);
   gReve->AddRenderElement(ssd_lay1);
 
+
   Alieve::ITSModuleStepper* ssd_lay2 = new Alieve::ITSModuleStepper(di);
-  ssd_lay2->SetStepper(col, row, 10, 8); 
+  ssd_lay2->SetStepper(col, row,  CW/col, CH/row); 
   mx = ssd_lay2->PtrMainHMTrans();
   mx->SetPos(CW+off_x, 2*(CH+off_y), 0);
   ssd_lay2->DisplayDet(2, 6);
@@ -90,9 +94,6 @@ void its_module_stepper(Int_t col = 4 , Int_t row = 4)
   ssd_lay2->SetRnrFrame(rnrFrame);
   ssd_lay2->SetWColor(wcol);
   gReve->AddRenderElement(ssd_lay2);
-
-  Alieve::DigitScaleInfo* si =  Alieve::ITSScaledModule::fgDigitScaleInfo;
-  si->ScaleChanged(2);
 
   gReve->EnableRedraw();
   TGLViewer * v = (TGLViewer *)gPad->GetViewer3D();
