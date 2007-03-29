f2affcb335f98fc9cfc44111a6e92bd4b172178b
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-29 16:48:52 +0000

    VZERO volumes now placed in common mother.

--- EVE/alice-macros/geom_vzero.C
+++ EVE/alice-macros/geom_vzero.C
@@ -4,20 +4,37 @@ void geom_vzero()
 {
   using namespace std;
 
+  static const Reve::Exc_t eH("geom_vzero() ");
+
   gGeoManager = gReve->GetGeometry("$REVESYS/alice-data/alice_fullgeo.root");
 
   Reve::RenderElementList* list = new Reve::RenderElementList("VZero");
   gReve->AddGlobalRenderElement(list);
 
-  TGeoNode* node;
+  TGeoNode* node = 0;
   Reve::GeoTopNodeRnrEl* re;
 
-  node = gGeoManager->GetTopVolume()->FindNode("V0RI_1");
+  TGeoNode* mnode = gGeoManager->GetTopVolume()->FindNode("VZERO_1");
+  if (!mnode) {
+    Error(eH, "mother node not found.");
+    return;
+  }
+
+  node = mnode->GetVolume()->FindNode("V0RI_1");
+  printf("opofoih %p\n", node);
+  if (!node) {
+    Error(eH, "V0R not found.");
+    return;
+  }
   re = new Reve::GeoTopNodeRnrEl(gGeoManager, node);
   re->UseNodeTrans();
   gReve->AddGlobalRenderElement(list, re);
 
-  node = gGeoManager->GetTopVolume()->FindNode("V0LE_1");
+  node = mnode->GetVolume()->FindNode("V0LE_1");
+  if (!node) {
+    Error(eH, "V0L not found.");
+    return;
+  }
   re = new Reve::GeoTopNodeRnrEl(gGeoManager, node);
   re->UseNodeTrans();
   gReve->AddGlobalRenderElement(list, re);
