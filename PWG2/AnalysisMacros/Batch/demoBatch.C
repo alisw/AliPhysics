e68ded7ec2d9ad4293520d44de687e32ce926aaf
panos <panos@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-29 07:31:29 +0000

    removing the old macro

--- PWG2/AnalysisMacros/Batch/demoBatch.C
+++ PWG2/AnalysisMacros/Batch/demoBatch.C
@@ -1,42 +1,39 @@
 //________________________________________________________________________
-void demoBatch(const char* collectionfile) {
-  //____________________________________________//
-  // Open a collection
-  TAlienCollection* coll = TAlienCollection::Open(collectionfile);
-  if (!coll) Error("demoBatch", Form("Cannot create an AliEn collection from %s", collectionfile));
+void demoBatch() {
+  const char *collectionfile = "wn.xml";
 
   //____________________________________________//
-  // Create an esd chain
-  const char *chainname="esdTree";
-  TChain* chain1 = new TChain(chainname);
-  
-  //____________________________________________//
-  // Convert the collection and fill tha chain
-  coll->Reset();
-  while ( coll->Next() ) {
-    Info("demoBatch", Form("Adding %s", coll->GetTURL("")));
-    chain1->Add(coll->GetTURL(""));
-  }
+  //Usage of event tags
+  AliTagAnalysis *analysis = new AliTagAnalysis();
+  TChain *chain = 0x0;
+  chain = analysis->GetChainFromCollection(collectionfile,"esdTree");
 
+  //____________________________________________//
+  //Usage of a collection file - no tags
+  /*TChain *chain = new TChain("esdTree");
+  TAlienCollection *collection = TAlienCollection::Open(collectionfile);
+  TGridResult *result = collection->GetGridResult("",0,0);
+  for(Int_t i = 0; i < resukt->GetEntries(); i++)  chain->Add(result->GetKey(i,"turl"));*/
+   
   //____________________________________________//
   // Make the analysis manager
-  AliAnalysisManager *mgr = new AliAnalysisManager();
+  AliAnalysisManager *mgr = new AliAnalysisManager("TestManager");
   //____________________________________________//
   // 1st Pt task
-  AliAnalysisTask *task1 = new AliAnalysisTaskPt("TaskPt");
+  AliAnalysisTaskPt *task1 = new AliAnalysisTaskPt("TaskPt");
   mgr->AddTask(task1);
   // Create containers for input/output
   AliAnalysisDataContainer *cinput1 = mgr->CreateContainer("cchain1",TChain::Class(),AliAnalysisManager::kInputContainer);
-  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("chist1", TH1::Class(),AliAnalysisManager::kOutputContainer);
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("chist1", TH1::Class(),AliAnalysisManager::kOutputContainer,"Pt.ESD.root");
   
   //____________________________________________//
   mgr->ConnectInput(task1,0,cinput1);
   mgr->ConnectOutput(task1,0,coutput1);
-  cinput1->SetData(chain1);
+  cinput1->SetData(chain);
   
   if (mgr->InitAnalysis()) {
     mgr->PrintStatus();
-    chain1->Process(mgr);
+    mgr->StartAnalysis("grid",chain);
   }
 }                         
                       
