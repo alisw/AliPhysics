df7ad2bcbe0c2e89972f870c9bb35d67a908a426
acolla <acolla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-03-30 17:05:52 +0000

    correct streamer

--- STEER/CDBLinkDef.h
+++ STEER/CDBLinkDef.h
@@ -8,34 +8,34 @@
 #pragma link off all classes;
 #pragma link off all functions;
  
-#pragma link C++ class AliCDBPath;
-#pragma link C++ class AliCDBRunRange;
-#pragma link C++ class AliCDBId;
-#pragma link C++ class AliCDBMetaData;
-#pragma link C++ class AliCDBEntry;
-#pragma link C++ class AliCDBStorage;
-#pragma link C++ class AliCDBStorageFactory;
-#pragma link C++ class AliCDBManager;
-#pragma link C++ class AliCDBParam;
-#pragma link C++ class AliCDBLocal;
-#pragma link C++ class AliCDBLocalFactory;
-#pragma link C++ class AliCDBLocalParam;
-#pragma link C++ class AliCDBDump;
-#pragma link C++ class AliCDBDumpFactory;
-#pragma link C++ class AliCDBDumpParam; 
-#pragma link C++ class AliCDBGrid;
-#pragma link C++ class AliCDBGridFactory;
-#pragma link C++ class AliCDBGridParam;
+#pragma link C++ class AliCDBPath+;
+#pragma link C++ class AliCDBRunRange+;
+#pragma link C++ class AliCDBId+;
+#pragma link C++ class AliCDBMetaData+;
+#pragma link C++ class AliCDBEntry+;
+#pragma link C++ class AliCDBStorage+;
+#pragma link C++ class AliCDBStorageFactory+;
+#pragma link C++ class AliCDBManager+;
+#pragma link C++ class AliCDBParam+;
+#pragma link C++ class AliCDBLocal+;
+#pragma link C++ class AliCDBLocalFactory+;
+#pragma link C++ class AliCDBLocalParam+;
+#pragma link C++ class AliCDBDump+;
+#pragma link C++ class AliCDBDumpFactory+;
+#pragma link C++ class AliCDBDumpParam+; 
+#pragma link C++ class AliCDBGrid+;
+#pragma link C++ class AliCDBGridFactory+;
+#pragma link C++ class AliCDBGridParam+;
 
-#pragma link C++ class AliDCSValue;
+#pragma link C++ class AliDCSValue+;
 #pragma link C++ class AliDCSSensor+;
 #pragma link C++ class AliDCSSensorArray+;
 
-#pragma link C++ class AliPreprocessor;
+#pragma link C++ class AliPreprocessor+;
 
-#pragma link C++ class AliShuttleInterface;
+#pragma link C++ class AliShuttleInterface+;
 
-#pragma link C++ class AliGRPPreprocessor;
-#pragma link C++ class AliGRPDCS;
+#pragma link C++ class AliGRPPreprocessor+;
+#pragma link C++ class AliGRPDCS+;
 
 #endif
