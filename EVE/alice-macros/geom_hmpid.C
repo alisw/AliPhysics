681ffa81e4f4b5c39e99adbdbb6bcf70ce6d3774
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-11 16:13:01 +0000

    Rename rich to hmpid etc.

--- EVE/alice-macros/geom_hmpid.C
+++ EVE/alice-macros/geom_hmpid.C
@@ -1,21 +1,21 @@
 // $Id$
 
-void geom_rich()
+void geom_hmpid()
 {
   using namespace std;
 
   gGeoManager = gReve->GetGeometry("$REVESYS/alice-data/alice_fullgeo.root");
 
-  Reve::RenderElementList* list = new Reve::RenderElementList("RICH");
+  Reve::RenderElementList* list = new Reve::RenderElementList("HMPID");
   gReve->AddGlobalRenderElement(list);
 
-  for(Int_t i=1; i<=7; ++i) {
+  for(Int_t i=0; i<7; ++i) {
     //PH The line below is replaced waiting for a fix in Root
     //PH which permits to use variable siza arguments in CINT
     //PH on some platforms (alphalinuxgcc, solariscc5, etc.)
     //PH TGeoNode* node = gGeoManager->GetTopVolume()->FindNode(Form("HMPID_%d", i));
     char form[1000];
-    sprintf(form,"RICH_%d", i);
+    sprintf(form, "HMPID_%d", i);
     TGeoNode* node = gGeoManager->GetTopVolume()->FindNode(form);
 
     Reve::GeoTopNodeRnrEl* re = new Reve::GeoTopNodeRnrEl(gGeoManager, node);
