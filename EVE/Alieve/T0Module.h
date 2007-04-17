f1db3c8bdca1a07ec5c474315920b936d49cc2c2
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-17 12:18:12 +0000

    Alexey: new files for T0 visualization.

---
+++ EVE/Alieve/T0Module.h
@@ -0,0 +1,53 @@
+#ifndef ALIEVE_T0Module_H
+#define ALIEVE_T0Module_H
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// The main AliEVE drawing module for the T0 detector                   //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+#include <Reve/QuadSet.h>
+#include <AliT0digit.h>
+#include <AliT0RawReader.h>
+
+namespace Alieve {
+ 
+class T0Module : public Reve::QuadSet
+{
+ 
+  T0Module(const T0Module&);
+  T0Module& operator=(const T0Module&);
+
+public:
+ 
+  T0Module(const Text_t* n="T0Module", Int_t sigType=0, AliT0digit *digits=0,AliT0RawReader *start=0);
+  virtual ~T0Module();
+
+
+  virtual void QuadSelected(Int_t idx);
+  static void MakeModules(AliT0digit *digits);
+  void LoadRaw(TString fileName, Int_t ievt);
+
+protected:
+
+ Int_t fSigType; // 0 ~ ADC, 1 ~ TDC
+ AliT0digit * fDigits;
+ AliT0RawReader *fStart;
+   ClassDef(T0Module,1);
+ 
+};
+
+/*
+ class T0ModuleTDC : public T0Module
+ {
+ public:
+   // constructor
+
+    virtual void QuadSelected(Int_t idx);
+ };
+*/
+
+}
+#endif
