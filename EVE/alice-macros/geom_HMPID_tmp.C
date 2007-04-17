4defcd0b942dd5926f0df1fc3035740f8e56eefa
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-17 13:39:02 +0000

    MacOSX does not like files that differ only by case

---
+++ EVE/alice-macros/geom_HMPID_tmp.C
@@ -0,0 +1,18 @@
+// $Id$
+
+void geom_rich()
+{
+  using namespace std;
+
+  gGeoManager = gReve->GetGeometry("$REVESYS/alice-data/alice_fullgeo.root");
+
+  for(Int_t i=1; i<=7; ++i) {
+    TGeoNode* node = gGeoManager->GetTopVolume()->FindNode(Form("HMPID_%d", i));
+
+    Reve::GeoTopNodeRnrEl* re = new Reve::GeoTopNodeRnrEl(gGeoManager, node);
+    re->SetUseNodeTrans(kTRUE);
+    gReve->AddGlobalRenderElement(re);
+  }
+
+  gReve->Redraw3D();
+}
