ed639b77772a64490aa2938d347669a2a38a658a
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-18 06:47:21 +0000

    Enlarged arrays

--- PMD/AliPMDClusteringV1.cxx
+++ PMD/AliPMDClusteringV1.cxx
@@ -384,13 +384,13 @@ void AliPMDClusteringV1::RefClust(Int_t incr, Double_t edepcell[])
 
   AliPMDcludata *pmdcludata = 0;
 
-  Int_t *cellCount;
-  Int_t **cellXY;
+  Int_t *cellCount = 0x0;
+  Int_t **cellXY = 0x0;
   const Int_t kdim = 4500;
 
   Int_t    i, j, k, i1, i2, id, icl,  itest,ihld, ig, nsupcl,clno;
   Int_t    t[kdim];
-  Int_t    ncl[kdim], iord[kdim], lev1[20], lev2[20];
+  Int_t    ncl[kdim], iord[kdim], lev1[kdim], lev2[kdim];
   Int_t    clxy[15];
   Float_t  clusdata[6];
   Double_t x1, y1, z1, x2, y2, z2, dist,rr,sum;
