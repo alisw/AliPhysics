4f53430132c478694b78344681c63b46e366e33a
masera <masera@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-19 11:00:45 +0000

    SDD preprocessor for baselines, noise and gains. Test FXS files read by the preprocessor available in /afs/usr/cern.ch/f/frprino/public

--- ITS/AliITSPreprocessorSDD.cxx
+++ ITS/AliITSPreprocessorSDD.cxx
@@ -10,9 +10,13 @@
 #include "AliCDBMetaData.h"
 #include "TObjArray.h"
 #include "AliLog.h"
+#include <TObjString.h>
+#include <TSystem.h>
+#include <TList.h>
 
 const Int_t AliITSPreprocessorSDD::fgkNumberOfSDD = 260;
 const Int_t AliITSPreprocessorSDD::fgkNumberOfChannels = 512;
+const Int_t AliITSPreprocessorSDD::fgkNumberOfChannelsPerChip = 64;
 const TString AliITSPreprocessorSDD::fgkNameHistoPedestals = "hpedestal";
 const TString AliITSPreprocessorSDD::fgkNameHistoNoise = "hnoise";
 ClassImp(AliITSPreprocessorSDD)
@@ -22,38 +26,49 @@ UInt_t AliITSPreprocessorSDD::Process(TMap*/* dcsAliasMap*/){
 
   //preprocessing. 
 
-
-  AliCDBMetaData *md1= new AliCDBMetaData(); // metaData describing the object
-  md1->SetObjectClassName("AliITSCalibration");
-  md1->SetResponsible("Elisabetta Crescio, Francesco Prino");
-  md1->SetBeamPeriod(0);
-  md1->SetAliRootVersion("head 5 April 2007"); //root version
-  md1->SetComment("This is a test");
-
   TObjArray respSDD(fgkNumberOfSDD);
   respSDD.SetOwner(kFALSE);
-  
-  Char_t filid[20];
   Float_t baseline,rawnoise,cmn,gain;
   Int_t isgoodan,i,im,is,isgoodmod;
   Int_t numOfBadChannels[fgkNumberOfSDD];
+  //TString pwd = gSystem->pwd();
+  const Char_t* tempDir=AliShuttleInterface::GetShuttleTempDir();
+  
+  TList* sourceList = GetFileSources(kDAQ, "SDD_Calib");
+  if (!sourceList){ 
+    Log("Error: no sources found for SDD_Calib");
+    return 2;
+  }
+
+  Int_t ind = 0;
+  while (sourceList->At(ind)!=NULL) {
+    TObjString* tarId = (TObjString*) sourceList->At(ind);
+    TString tarName = GetFile(kDAQ, "SDD_Calib", tarId->GetString().Data());
+    gSystem->cd(tempDir);
+    Char_t command[100];
+    sprintf(command,"tar -xf %s",tarName.Data());
+    gSystem->Exec(command);
+    ind++;
+  }
+  
+  delete sourceList;
+
   for(Int_t imod=0;imod<fgkNumberOfSDD;imod++){
     AliITSCalibrationSDD *cal = new AliITSCalibrationSDD("simulated");
     numOfBadChannels[imod]=0;
     Int_t badch[fgkNumberOfChannels];
     for(Int_t isid=0;isid<=1;isid++){
-      sprintf(filid,"DAQDAm%03ds%d",imod,isid);
-      const char* filenamed= GetFile(kDAQ,filid,"GDC");
-      FILE* filed = fopen(filenamed,"read");
-      if (filed == 0) {
-	AliWarning("File not found"); 
-	continue;
-      }
-      
-      fscanf(filed,"%d %d %d\n",&im,&is,&isgoodmod);
+      Char_t basFileName[100];
+      sprintf(basFileName,"%s/SDDbase_mod%03d_sid%d.data",tempDir,imod,isid);
+      FILE* basFil = fopen(basFileName,"read");
+      if (basFil == 0) {
+	Log(Form("File %s not found.",basFileName));
+	return 2;
+      }      
+      fscanf(basFil,"%d %d %d\n",&im,&is,&isgoodmod);
       if(!isgoodmod) cal->SetDead();
       for(Int_t ian=0;ian<(fgkNumberOfChannels/2);ian++){
-	fscanf(filed,"%d %d %f %f %f %f\n",&i,&isgoodan,&baseline,&rawnoise,&cmn,&gain);
+	fscanf(basFil,"%d %d %f %f %f %f\n",&i,&isgoodan,&baseline,&rawnoise,&cmn,&gain);
 	Int_t ich=ian;
 	if(isid==1) ich+=256;
 	if(!isgoodan){ 
@@ -62,20 +77,28 @@ UInt_t AliITSPreprocessorSDD::Process(TMap*/* dcsAliasMap*/){
 	  badch[ibad]=ich;
 	}
 	cal->SetBaseline(ich,baseline);
-	cal->SetNoiseAfterElectronics(ich,rawnoise);	
-	//	cal->SetGain(gain,isid,ian/4,ian%4);
+	cal->SetNoiseAfterElectronics(ich,rawnoise);
+	Int_t iChip=ian/fgkNumberOfChannelsPerChip;
+	Int_t iChInChip=ian%fgkNumberOfChannelsPerChip;
+	cal->SetGain(gain,isid,iChip,iChInChip);
       }
       cal->SetDeadChannels(numOfBadChannels[imod]);
       for(Int_t ibad=0;ibad<numOfBadChannels[imod];ibad++){
 	cal->SetBadChannel(ibad,badch[ibad]);
       }
-      fclose(filed);
+      fclose(basFil);
     }
     respSDD.Add(cal);
   }
 
-  Bool_t retcode = Store("Calib","Data",&respSDD,md1);
+  AliCDBMetaData *md1= new AliCDBMetaData(); // metaData describing the object
+  md1->SetObjectClassName("AliITSCalibration");
+  md1->SetResponsible("Elisabetta Crescio, Francesco Prino");
+  md1->SetBeamPeriod(0);
+  md1->SetAliRootVersion("head 5 April 2007"); //root version
+  md1->SetComment("This is a test");
 
+  Bool_t retcode = Store("Calib","CalibSDD",&respSDD,md1, 0, kTRUE);
 
   if(retcode) return 0;
   else return 1;
