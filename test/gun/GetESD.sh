28c098a70a963ae4d083c8cf08bc8b7918772a23
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-20 08:21:50 +0000

    Adding reconstruction of raw data

--- test/gun/GetESD.sh
+++ test/gun/GetESD.sh
@@ -13,9 +13,15 @@
 # Geant3
 # export LD_LIBRARY_PATH=$ALICE/geant3/lib/tgt_${ALICE_TARGET}:$LD_LIBRARY_PATH
 
-rm -rf *.root *.dat *.log fort* hlt hough *raw*
+rm -rf *.root *.dat *.log fort* hlt hough raw* recraw/*.root recraw/*.log
 aliroot -b -q sim.C      2>&1 | tee sim.log
 aliroot -b -q rec.C      2>&1 | tee rec.log
+aliroot -b -q ${ALICE_ROOT}/STEER/CheckESD.C 2>&1 | tee check.log
+aliroot -b -q ${ALICE_ROOT}/STEER/CreateAODfromESD.C 2>&1 | tee aod.log
 
-
+cd recraw
+ln -s ../geometry.root
+ln -s ../raw.root
+aliroot -b -q rec.C      2>&1 | tee rec.log
+aliroot -b -q ${ALICE_ROOT}/STEER/CreateAODfromESD.C 2>&1 | tee aod.log
 
--- test/gun/sim.C
+++ test/gun/sim.C
@@ -2,7 +2,7 @@ void sim(Int_t nev=4) {
   AliSimulation simulator;
   simulator.SetMakeSDigits("TRD TOF PHOS HMPID EMCAL MUON FMD ZDC PMD T0 VZERO");
   simulator.SetMakeDigitsFromHits("ITS TPC");
-  //  simulator.SetWriteRawData("ALL","raw.root",kTRUE);
+  simulator.SetWriteRawData("ALL","raw.root",kTRUE);
 
   TStopwatch timer;
   timer.Start();
