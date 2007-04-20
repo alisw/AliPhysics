d148c3e566113db5e5bb54aadafc5c3f15645aa2
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-20 09:48:58 +0000

    added D0toKpi and BtoJPSItoEle

--- PWG3/PWG3baseLinkDef.h
+++ PWG3/PWG3baseLinkDef.h
@@ -1,4 +1,5 @@
 #ifdef __CINT__
+
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
@@ -6,6 +7,13 @@
 #pragma link C++ class AliQuarkoniaAcceptance+; 
 #pragma link C++ class AliQuarkoniaEfficiency+; 
 #pragma link C++ class AliPWG3TrackExtrapInterface+; 
+
+#pragma link C++ class AliD0toKpi+;
+#pragma link C++ class AliD0toKpiAnalysis+;
+#pragma link C++ class AliBtoJPSItoEle+;
+#pragma link C++ class AliBtoJPSItoEleAnalysis+;
+
+
 #endif
 
 
