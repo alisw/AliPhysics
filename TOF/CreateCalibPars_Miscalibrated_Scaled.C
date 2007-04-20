fc9a7c9fe39a89c5ea2cdd98d2d11ee5e39f1743
arcelli <arcelli@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-20 17:41:48 +0000

    New Macro for Miscalibrated digits (ToT spectrum is scaled by 10 ns)

---
+++ TOF/CreateCalibPars_Miscalibrated_Scaled.C
@@ -0,0 +1,42 @@
+void CreateCalibPars_Miscalibrated_Scaled(){
+  // Create TOF Calibration Object for miscalibrated detector
+  // and write it on CDB
+  AliTOFGeometry *geom = new AliTOFGeometryV5(); 
+  AliTOFcalib *tofcalib = new AliTOFcalib(geom);
+  AliTOFCal *tofCal= new AliTOFCal(geom);
+  tofCal->CreateArray();
+
+  // Input data for decalibration
+
+  TFile f("$ALICE_ROOT/TOF/data/spectrumScaled.root","READ");  
+
+  TH1F *hTimeToTFit=  (TH1F*)f.Get("hTimeToTScaled");
+  TF1  *fit=hTimeToTFit->GetFunction("pol5");
+  
+  // Slewing parameters (same for all channels)
+
+  Float_t par[6] = {0.,0.,0.,0.,0.,0.};
+  for(Int_t i =0;i<6;i++){
+    par[i]=fit->GetParameter(i);
+    cout << " Slewing parameters=" << par[i] << endl;
+  }
+
+  // Global time offset 3 ns
+  Float_t delay=3.;
+
+  // ToT spectrum 
+  TH1F *hToT=  (TH1F*)f.Get("hToTScaled");
+
+  // Fill the Sim calibration object
+
+  //  TRandom *rnd   = new TRandom(4357);
+  for (Int_t ipad = 0 ; ipad<tofCal->NPads(); ipad++){
+    AliTOFChannel *calChannel = tofCal->GetChannel(ipad);
+    calChannel->SetSlewPar(par);
+    calChannel->SetDelay(delay);
+  }
+  tofcalib->WriteSimParOnCDB("TOF/Calib",0,0,tofCal,hToT);
+  f.Close();
+}
+
+
