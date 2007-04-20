4cb3bc0e7ee241feb25cf848e6185d16f61fcf65
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-20 13:57:30 +0000

    Add algebraic functions (Marian, Jens)

--- TPC/AliTPCCalPad.cxx
+++ TPC/AliTPCCalPad.cxx
@@ -124,6 +124,73 @@ void AliTPCCalPad::Copy(TObject &c) const
   TObject::Copy(c);
 }
 
+//_____________________________________________________________________________
+void AliTPCCalPad::Add(Float_t c1)
+{
+    //
+    // add constant for all channels of all ROCs
+    //
+
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+	if (fROC[isec]){
+	    fROC[isec]->Add(c1);
+	}
+    }
+}
+
+//_____________________________________________________________________________
+void AliTPCCalPad::Multiply(Float_t c1)
+{
+    //
+    // multiply constant for all channels of all ROCs
+    //
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+	if (fROC[isec]){
+	    fROC[isec]->Multiply(c1);
+	}
+    }
+}
+
+//_____________________________________________________________________________
+void AliTPCCalPad::Add(const AliTPCCalPad * pad, Double_t c1)
+{
+    //
+    // add calpad channel by channel multiplied by c1 - all ROCs
+    //
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+	if (fROC[isec]){
+	    fROC[isec]->Add(pad->GetCalROC(isec),c1);
+	}
+    }
+}
+
+//_____________________________________________________________________________
+void AliTPCCalPad::Multiply(const AliTPCCalPad * pad)
+{
+    //
+    // multiply calpad channel by channel - all ROCs
+    //
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+	if (fROC[isec]){
+	    fROC[isec]->Multiply(pad->GetCalROC(isec));
+	}
+    }
+}
+
+//_____________________________________________________________________________
+void AliTPCCalPad::Divide(const AliTPCCalPad * pad)
+{
+    //
+    // divide calpad channel by channel - all ROCs
+    //
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+	if (fROC[isec]){
+	    fROC[isec]->Divide(pad->GetCalROC(isec));
+	}
+    }
+}
+
+//_____________________________________________________________________________
 TGraph  *  AliTPCCalPad::MakeGraph(Int_t type, Float_t ratio){
   //
   //   type=1 - mean
@@ -158,6 +225,161 @@ TGraph  *  AliTPCCalPad::MakeGraph(Int_t type, Float_t ratio){
   return graph;
 }
 
+//_____________________________________________________________________________
+Double_t AliTPCCalPad::GetMeanRMS(Double_t &rms)
+{
+    //
+    // Calculate mean an RMS of all rocs
+    //
+    Double_t sum = 0, sum2 = 0, n=0, val=0;
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+        AliTPCCalROC *calRoc = fROC[isec];
+	if ( calRoc ){
+	    for (UInt_t irow=0; irow<calRoc->GetNrows(); irow++){
+		for (UInt_t ipad=0; ipad<calRoc->GetNPads(irow); ipad++){
+		    val = calRoc->GetValue(irow,ipad);
+		    sum+=val;
+		    sum2+=val*val;
+                    n++;
+		}
+	    }
+
+	}
+    }
+    Double_t n1 = 1./n;
+    Double_t mean = sum*n1;
+    rms  = TMath::Sqrt(TMath::Abs(sum2*n1-mean*mean));
+    return mean;
+}
+
+
+//_____________________________________________________________________________
+Double_t AliTPCCalPad::GetMean()
+{
+    //
+    // return mean of the mean of all ROCs
+    //
+    Double_t arr[kNsec];
+    Int_t n=0;
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+        AliTPCCalROC *calRoc = fROC[isec];
+	if ( calRoc ){
+	    arr[n] = calRoc->GetMean();
+            n++;
+	}
+    }
+    return TMath::Mean(n,arr);
+}
+
+//_____________________________________________________________________________
+Double_t AliTPCCalPad::GetRMS()
+{
+    //
+    // return mean of the RMS of all ROCs
+    //
+    Double_t arr[kNsec];
+    Int_t n=0;
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+        AliTPCCalROC *calRoc = fROC[isec];
+	if ( calRoc ){
+	    arr[n] = calRoc->GetRMS();
+            n++;
+	}
+    }
+    return TMath::Mean(n,arr);
+}
+
+//_____________________________________________________________________________
+Double_t AliTPCCalPad::GetMedian()
+{
+    //
+    // return mean of the median of all ROCs
+    //
+    Double_t arr[kNsec];
+    Int_t n=0;
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+        AliTPCCalROC *calRoc = fROC[isec];
+	if ( calRoc ){
+	    arr[n] = calRoc->GetMedian();
+            n++;
+	}
+    }
+    return TMath::Mean(n,arr);
+}
+
+//_____________________________________________________________________________
+Double_t AliTPCCalPad::GetLTM(Double_t *sigma, Double_t fraction)
+{
+    //
+    // return mean of the LTM and sigma of all ROCs
+    //
+    Double_t arrm[kNsec];
+    Double_t arrs[kNsec];
+    Double_t *sTemp=0x0;
+    Int_t n=0;
+
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+        AliTPCCalROC *calRoc = fROC[isec];
+	if ( calRoc ){
+	    if ( sigma ) sTemp=arrs+n;
+	    arrm[n] = calRoc->GetLTM(sTemp,fraction);
+            n++;
+	}
+    }
+    if ( sigma ) *sigma = TMath::Mean(n,arrs);
+    return TMath::Mean(n,arrm);
+}
+
+//_____________________________________________________________________________
+TH1F * AliTPCCalPad::MakeHisto1D(Float_t min, Float_t max,Int_t type){
+  //
+  // make 1D histo
+  // type -1 = user defined range
+  //       0 = nsigma cut nsigma=min
+  if (type>=0){
+    if (type==0){
+      // nsigma range
+      Float_t mean  = GetMean();
+      Float_t sigma = GetRMS();
+      Float_t nsigma = TMath::Abs(min);
+      min = mean-nsigma*sigma;
+      max = mean+nsigma*sigma;
+    }
+    if (type==1){
+      // fixed range
+      Float_t mean   = GetMedian();
+      Float_t  delta = min;
+      min = mean-delta;
+      max = mean+delta;
+    }
+    if (type==2){
+      //
+      // LTM mean +- nsigma
+      //
+      Double_t sigma;
+      Float_t mean  = GetLTM(&sigma,max);
+      sigma*=min;
+      min = mean-sigma;
+      max = mean+sigma;
+    }
+  }
+  char  name[1000];
+  sprintf(name,"%s Pad 1D",GetTitle());
+  TH1F * his = new TH1F(name,name,100, min,max);
+    for (Int_t isec = 0; isec < kNsec; isec++) {
+	if (fROC[isec]){
+	    for (UInt_t irow=0; irow<fROC[isec]->GetNrows(); irow++){
+		UInt_t npads = (Int_t)fROC[isec]->GetNPads(irow);
+		for (UInt_t ipad=0; ipad<npads; ipad++){
+		    his->Fill(fROC[isec]->GetValue(irow,ipad));
+		}
+	    }
+	}
+    }
+  return his;
+}
+
+//_____________________________________________________________________________
 TH2F *AliTPCCalPad::MakeHisto2D(Int_t side){
   //
   // Make 2D graph
--- TPC/AliTPCCalPad.h
+++ TPC/AliTPCCalPad.h
@@ -18,6 +18,7 @@ class AliTPCCalDet;
 class TObjArray;
 class TGraph;
 class TH2F;
+class TH1F;
 
 class AliTPCCalPad : public TNamed {
  public:
@@ -31,9 +32,21 @@ class AliTPCCalPad : public TNamed {
   virtual void     Copy(TObject &c) const;
   AliTPCCalROC *GetCalROC(Int_t sector) const { return fROC[sector]; };  
   //
+  // algebra
+  void Add(Float_t c1);
+  void Multiply(Float_t c1);
+  void Add(const AliTPCCalPad * roc, Double_t c1 = 1);
+  void Multiply(const AliTPCCalPad * pad);
+  void Divide(const AliTPCCalPad * pad);
   //
-  TGraph       *MakeGraph(Int_t type=0, Float_t ratio=0.7);
-  TH2F     *MakeHisto2D(Int_t side=0);
+  Double_t GetMeanRMS(Double_t &rms);
+  Double_t GetMean();
+  Double_t GetRMS() ;
+  Double_t GetMedian() ;
+  Double_t GetLTM(Double_t *sigma=0, Double_t fraction=0.9);
+  TGraph  *MakeGraph(Int_t type=0, Float_t ratio=0.7);
+  TH2F    *MakeHisto2D(Int_t side=0);
+  TH1F    *MakeHisto1D(Float_t min=4, Float_t max=-4, Int_t type=0);
  protected:
   AliTPCCalROC *fROC[kNsec];                    //  Array of ROC objects which contain the values per pad
   ClassDef(AliTPCCalPad,1)                      //  TPC calibration class for parameters which are saved per pad
