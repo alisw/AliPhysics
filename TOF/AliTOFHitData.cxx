f35bd5e3e83aaf87ad4d604eda3d0bf678255d50
arcelli <arcelli@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-04-27 11:09:55 +0000

    object for storing TOF decoded RAW data

---
+++ TOF/AliTOFHitData.cxx
@@ -0,0 +1,88 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/*
+$Log$
+ authors: Roberto Preghenella, preghenella@bo.infn.it
+          with contribution from Chiara Zampolli, zampolli@bo.infn.it 
+*/
+
+////////////////////////////////////////////////////////////////////////
+//                                                                    //
+//     This class provides the basic object to store just-decoded     //
+//     raw data                                                       //
+//                                                                    //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+#include "AliTOFHitData.h"
+
+ClassImp(AliTOFHitData)
+
+AliTOFHitData::AliTOFHitData():
+  TObject(),fDDLID(-1),fSlotID(-1),fACQ(-1),fChain(-1),fPS(-1),fTDC(-1),fChan(-1),fTime(-1),fTimeBin(-1),fTOT(-1),fTOTBin(-1)
+{
+  //ctor
+}
+//-----------------------------------------------------------------------------
+
+AliTOFHitData::AliTOFHitData(const AliTOFHitData &source):
+  TObject(),fDDLID(-1),fSlotID(-1),fACQ(-1),fChain(-1),fPS(-1),fTDC(-1),fChan(-1),fTime(-1),fTimeBin(-1),fTOT(-1),fTOTBin(-1){ 
+  // copy constructor 
+  for (Int_t i = 0; i < 5; i++) this->fVolume[i]=source.fVolume[i];
+  this->fDDLID=source.fDDLID;
+  this->fSlotID=source.fSlotID;
+  this->fACQ=source.fACQ;
+  this->fChain=source.fChain;
+  this->fPS=source.fPS;
+  this->fTDC=source.fTDC;
+  this->fTOT=source.fTOT;
+  this->fTOTBin=source.fTOTBin;
+  this->fChan=source.fChan;
+  this->fTime=source.fTime;
+  this->fTimeBin=source.fTimeBin;
+}
+
+//-----------------------------------------------------------------------------
+AliTOFHitData& AliTOFHitData::operator=(const AliTOFHitData & source) { 
+// assignment operator
+  for (Int_t i = 0; i < 5; i++)
+    this->fVolume[i]=source.fVolume[i];
+  this->fDDLID=source.fDDLID;
+  this->fSlotID= source.fSlotID;
+  this->fACQ= source.fACQ;
+  this->fChain= source.fChain;
+  this->fPS= source.fPS;
+  this->fTDC= source.fTDC;
+  this->fChan= source.fChan;
+  this->fTime= source.fTime;
+  this->fTimeBin= source.fTimeBin;
+  this->fTOT= source.fTOT;
+  this->fTOTBin= source.fTOTBin;
+  return *this;
+}
+//----------------------------------------------------------------------------
+void AliTOFHitData::SetVolume(Int_t *Volume)
+{
+  // setting the TOF volume index 
+  fVolume[0]=Volume[0];
+  fVolume[1]=Volume[1];
+  fVolume[2]=Volume[2];
+  fVolume[3]=Volume[3];
+  fVolume[4]=Volume[4];
+}
+
+
+  
---
+++ TOF/AliTOFHitData.h
@@ -0,0 +1,71 @@
+#ifndef ALITOFHITDATA_H
+#define ALITOFHITDATA_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+///////////////////////////////////////////////////////////////
+//                                                           //
+//   This class provides the key-reading for TOF raw data.   //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+#include "TObject.h"
+
+class AliTOFHitData : public TObject{
+ public:
+  AliTOFHitData();
+  ~AliTOFHitData()
+    {};
+  
+  
+  AliTOFHitData(const AliTOFHitData &source);
+  
+  AliTOFHitData& operator=(const AliTOFHitData & source); //ass. operator
+  
+  Int_t  *GetVolume() {return fVolume;};
+  Short_t GetDDLID() const {return fDDLID;};
+  Short_t GetSlotID() const {return fSlotID;};
+  Short_t GetACQ() const {return fACQ;};
+  Short_t GetChain() const {return fChain;};
+  Short_t GetPS() const {return fPS;};
+  Short_t GetTDC() const {return fTDC;};
+  Short_t GetChan() const {return fChan;};
+  Float_t GetTime() const {return fTime;};
+  Int_t GetTimeBin() const {return fTimeBin;};
+  Float_t GetTOT() const {return fTOT;};
+  Int_t GetTOTBin() const {return fTOTBin;};
+
+  void SetVolume(Int_t *Volume);
+
+  void SetDDLID(Short_t DDLID)    { fDDLID=DDLID;};
+  void SetSlotID(Short_t slotID)  { fSlotID=slotID;};
+  void SetACQ(Short_t ACQ)        { fACQ=ACQ;};
+  void SetChain(Short_t chain)    { fChain=chain;};
+  void SetPS(Short_t PS)          { fPS=PS;};
+  void SetTDC(Short_t TDC)        { fTDC=TDC;};
+  void SetChan(Short_t chan)      { fChan=chan;};
+  void SetTime(Float_t time)      { fTime=time;};
+  void SetTimeBin(Int_t timeBin) {fTimeBin=timeBin;};
+  void SetTOT(Float_t TOT)        { fTOT=TOT;};
+  void SetTOTBin(Int_t TOTBin) {fTOTBin=TOTBin;};
+  
+ private:
+  Int_t   fVolume[5];  // TOF volume index
+  Short_t fDDLID;      // DDL index
+  Short_t fSlotID;     // slot index
+  Short_t fACQ;        // ACQ flag
+  Short_t fChain;      // chain index
+  Short_t fPS;         // PS bit
+  Short_t fTDC;        // TDC index
+  Short_t fChan;       // channel index
+  Float_t fTime;      // time [ns]
+  Int_t fTimeBin;      // time [TDC bin = 24.4ps]
+  Float_t fTOT;       // tot [ns]
+  Int_t fTOTBin;       // TOT [TOT bin = 48.4ps]
+
+  ClassDef(AliTOFHitData, 1);
+};
+
+#endif
