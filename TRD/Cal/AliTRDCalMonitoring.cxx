9055a038d7a37b794167658f6486731ad0eb59d2
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-02 17:16:53 +0000

    Corrected initialization of arrays (Opteron)

--- TRD/Cal/AliTRDCalMonitoring.cxx
+++ TRD/Cal/AliTRDCalMonitoring.cxx
@@ -35,7 +35,21 @@ AliTRDCalMonitoring::AliTRDCalMonitoring()
   //
   // AliTRDCalMonitoring default constructor
   //
-
+  
+  for (Int_t i=0; i<540; ++i) {
+    fAnodeCurrentsMin[i] = 0;
+    fAnodeCurrentsMax[i] = 0;
+    fDriftCurrentsMin[i] = 0;
+    fDriftCurrentsMax[i] = 0;
+    fAnodeVoltagesMin[i] = 0;
+    fAnodeVoltagesMax[i] = 0;
+    fDriftVoltagesMin[i] = 0;
+    fDriftVoltagesMax[i] = 0;
+  }
+  for (Int_t i=0; i<360; ++i) {
+    fLVVoltage[i] = 0;
+    fLVCurrent[i] = 0;
+  }
 }
 
 //_____________________________________________________________________________
@@ -49,5 +63,19 @@ AliTRDCalMonitoring::AliTRDCalMonitoring(const Text_t *name, const Text_t *title
   // AliTRDCalMonitoring constructor
   //
 
+  for (Int_t i=0; i<540; ++i) {
+    fAnodeCurrentsMin[i] = 0;
+    fAnodeCurrentsMax[i] = 0;
+    fDriftCurrentsMin[i] = 0;
+    fDriftCurrentsMax[i] = 0;
+    fAnodeVoltagesMin[i] = 0;
+    fAnodeVoltagesMax[i] = 0;
+    fDriftVoltagesMin[i] = 0;
+    fDriftVoltagesMax[i] = 0;
+  }
+  for (Int_t i=0; i<360; ++i) {
+    fLVVoltage[i] = 0;
+    fLVCurrent[i] = 0;
+  }
 }
 
