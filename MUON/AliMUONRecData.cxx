1b828622ae83d657ab5a3058190b8b918435154e
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-09 08:45:07 +0000

    In DumpTracks(): do not loop over chambers (this gives the same tracks ten times) (Laurent)

--- MUON/AliMUONRecData.cxx
+++ MUON/AliMUONRecData.cxx
@@ -657,22 +657,17 @@ AliMUONRecData::DumpTracks(Int_t event2Check, Option_t* opt)
     // Getting event ievent
     fRunLoader->GetEvent(ievent);
     
-    Int_t nchambers = AliMUONConstants::NTrackingCh();
     SetTreeAddress("RT"); 
     GetRecTracks();
     
-    // Loop on chambers
-    for (Int_t ichamber=0; ichamber<nchambers; ++ichamber) 
-    {
-      // Loop on tracks
-      TClonesArray* tracks = RecTracks();
-      Int_t ntracks = (Int_t) tracks->GetEntriesFast();
+    // Loop on tracks
+    TClonesArray* tracks = RecTracks();
+    Int_t ntracks = (Int_t) tracks->GetEntriesFast();
 
-      for (Int_t i=0; i<ntracks; ++i) 
-      {
-        AliMUONTrack* mTrack = static_cast<AliMUONTrack*>(tracks->At(i));
-        mTrack->Print(opt);
-      }
+    for (Int_t i=0; i<ntracks; ++i) 
+    {
+      AliMUONTrack* mTrack = static_cast<AliMUONTrack*>(tracks->At(i));
+      mTrack->Print(opt);
     }
     ResetRecTracks();
     if (event2Check!=0) ievent=nevents;
