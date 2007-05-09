3c466baf7813383d194ccfdcdef8cf0855cd925b
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-09 02:22:38 +0000

    New online anysis classes

---
+++ HLT/PHOS/AliHLTPHOSRecPointDataStruct.h
@@ -0,0 +1,28 @@
+#ifndef ALIHLTPHOSRECPOINTDATASTRUCT
+#define ALIHLTPHOSRECPOINTDATASTRUCT
+
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Authors: Øystein Djuvsland <oysteind@ift.uib.no>                       *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+struct AliHLTPHOSRecPointDataStruct
+{
+  AliHLTUInt8_t fPHOSModule;
+  AliHLTUInt8_t fMultiplicity;
+  AliHLTUInt8_t fCoordinatesPtr[2]; 
+  Float_t fX;
+  Float_t fZ;
+  Float_t* fEnergiesListPtr;
+};
+
+#endif
