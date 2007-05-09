dc1b73a8155f91a2c19930f742cd12d03f98c804
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-09 02:17:36 +0000

    New datatypes

--- HLT/PHOS/AliHLTPHOSDefinitions.h
+++ HLT/PHOS/AliHLTPHOSDefinitions.h
@@ -7,11 +7,8 @@
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
-/* AliHLTPHOSDefinitions
- */
 
 #include "AliHLTDataTypes.h"
-//#include "Rtypes.h"
 
 class AliHLTPHOSDefinitions
     {
