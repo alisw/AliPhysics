a31d23df18568ff3f46a0d586b307364df6ca02a
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-16 10:22:13 +0000

    Making the directory structure of AliFemto flat. All files go into one common directory

---
+++ PWG2/FEMTOSCOPY/AliFemto/AliFemtoEventCut.h
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+/// AliFemtoEventCut - the pure virtual base class for the event cut         ///
+/// All event cuts must inherit from this one                                ///
+////////////////////////////////////////////////////////////////////////////////
+#ifndef AliFemtoEventCut_hh
+#define AliFemtoEventCut_hh
+
+class AliFemtoEvent;
+class AliFemtoBaseAnalysis;
+
+#include "AliFemtoCutMonitorHandler.h"
+#include "AliFemtoString.h"
+
+class AliFemtoEventCut : public AliFemtoCutMonitorHandler {
+
+  friend class AliFemtoBaseAnalysis;
+
+public:
+
+  AliFemtoEventCut();                // default constructor. - Users should write their own
+  AliFemtoEventCut(const AliFemtoEventCut& c); // copy constructor
+  virtual ~AliFemtoEventCut(){/* no-op */};       // destructor
+  AliFemtoEventCut& operator=(const AliFemtoEventCut& aCut);
+
+  virtual bool Pass(const AliFemtoEvent* event) =0;  // true if passes, false if not
+
+  virtual AliFemtoString Report() =0;    // user-written method to return string describing cuts
+  virtual AliFemtoEventCut* Clone() { return 0;}
+
+
+  AliFemtoBaseAnalysis* HbtAnalysis(){return fyAnalysis;};
+  void SetAnalysis(AliFemtoBaseAnalysis* aAnalysis);
+
+protected:
+  AliFemtoBaseAnalysis* fyAnalysis;
+
+#ifdef __ROOT__
+  ClassDef(AliFemtoEventCut, 0)
+#endif
+};
+
+inline AliFemtoEventCut::AliFemtoEventCut(const AliFemtoEventCut& c) : AliFemtoCutMonitorHandler(), fyAnalysis(0) { }
+inline void AliFemtoEventCut::SetAnalysis(AliFemtoBaseAnalysis* analysis) { fyAnalysis = analysis; }
+inline AliFemtoEventCut::AliFemtoEventCut(): AliFemtoCutMonitorHandler(), fyAnalysis(0){};                // default constructor. - Users should write their own
+inline AliFemtoEventCut& AliFemtoEventCut::operator=(const AliFemtoEventCut& aCut) { if (this == &aCut) return *this; fyAnalysis = aCut.fyAnalysis; return *this; }
+#endif
---
+++ PWG2/FEMTOSCOPY/AliFemto/AliFemtoParticleCut.h
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////
+/// AliFemtoParticleCut - the pure virtual base class for the particle cut   ///
+/// All particle cuts must inherit from this one                             ///
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef AliFemtoParticleCut_hh
+#define AliFemtoParticleCut_hh
+
+#include "AliFemtoTypes.h"
+#include "AliFemtoCutMonitorHandler.h"
+
+class AliFemtoBaseAnalysis;
+
+class AliFemtoParticleCut : public AliFemtoCutMonitorHandler {
+
+  friend class AliFemtoBaseAnalysis;
+
+public:
+  AliFemtoParticleCut();   // default constructor. - Users should write their own
+  AliFemtoParticleCut(const AliFemtoParticleCut&); // copy constructor
+  virtual ~AliFemtoParticleCut(){/* no-op */};  // destructor
+  AliFemtoParticleCut& operator=(const AliFemtoParticleCut& aCut);
+
+  virtual AliFemtoString Report() =0;    // user-written method to return string describing cuts
+
+  double Mass(){return fMass;};       // mass of the particle being selected
+  virtual void SetMass(const double& mass) {fMass = mass;};
+
+  virtual void EventBegin(const AliFemtoEvent* aEvent) { /* no-op */ }
+  virtual void EventEnd(const AliFemtoEvent* aEvent) { /* no-op */ }
+  virtual AliFemtoParticleCut* Clone() { return 0;}
+
+  virtual AliFemtoParticleType Type()=0;
+
+  // the following allows "back-pointing" from the CorrFctn to the "parent" Analysis
+  AliFemtoBaseAnalysis* HbtAnalysis(){return fyAnalysis;};
+  void SetAnalysis(AliFemtoBaseAnalysis*);
+
+protected:
+  AliFemtoBaseAnalysis* fyAnalysis; // Link to the base analysis class
+  double fMass;
+
+#ifdef __ROOT__
+  ClassDef(AliFemtoParticleCut, 0)
+#endif
+};
+
+inline AliFemtoParticleCut::AliFemtoParticleCut(): AliFemtoCutMonitorHandler(), fyAnalysis(0), fMass(0){};   // default constructor. - Users should write their own
+inline AliFemtoParticleCut::AliFemtoParticleCut(const AliFemtoParticleCut& c): AliFemtoCutMonitorHandler(), fyAnalysis(0), fMass(0) { 
+  fMass = c.fMass; fyAnalysis = 0; 
+#ifdef STHBTDEBUG
+  cout << " AliFemtoParticleCut::AliFemtoParticleCut(const AliFemtoParticleCut& c) - fMass: " << fMass << endl;
+#endif
+}
+inline void AliFemtoParticleCut::SetAnalysis(AliFemtoBaseAnalysis* analysis) { fyAnalysis = analysis; }
+inline AliFemtoParticleCut& AliFemtoParticleCut::operator=(const AliFemtoParticleCut& aCut) { if (this == &aCut) return *this; fyAnalysis = aCut.fyAnalysis; fMass=aCut.fMass; return *this; }
+#endif
