d1851eacbae43f510c69e3b12e4e0075533cc7a8
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-31 13:08:10 +0000

    AliTRDDataDCS has its own streamer

--- TRD/AliTRDDataDCS.cxx
+++ TRD/AliTRDDataDCS.cxx
@@ -1,3 +1,28 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/* $Id$ */
+
+////////////////////////////////////////////////////////////////////////////
+//                                                                        //
+// This class collects the DCS information and does an AliSplineFit       //
+//                                                                        //
+// Author:                                                                //
+//   W. Monange   (wilfried.monange@free.fr)                              //
+//                                                                        //
+////////////////////////////////////////////////////////////////////////////
 
 
 #include <TGraph.h>
@@ -12,7 +37,6 @@
 #include "AliSplineFit.h"
 
 ClassImp(AliTRDDataDCS)
-
 
 /*
 Typical use :
--- TRD/AliTRDDataDCS.h
+++ TRD/AliTRDDataDCS.h
@@ -1,6 +1,17 @@
 #ifndef AliTRDDataDCS_H
 #define AliTRDDAtaDCS_H
 
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+////////////////////////////////////////////////////////////////////////////
+//                                                                        //
+// Extracts the DCS information                                           //
+//                                                                        //
+////////////////////////////////////////////////////////////////////////////
+
 #include <TNamed.h>
 #include <TObjArray.h>
 #include <TString.h>
