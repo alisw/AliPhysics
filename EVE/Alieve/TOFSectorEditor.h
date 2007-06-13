cc2e3d30152fb6ec7fad8a0aef3567b657871382
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-06-13 14:29:34 +0000

    From Annalisa: new files for TOF visualization.

---
+++ EVE/Alieve/TOFSectorEditor.h
@@ -0,0 +1,79 @@
+// $Header$
+
+#ifndef ALIEVE_TOFSectorEditor_H
+#define ALIEVE_TOFSectorEditor_H
+
+#include <TGedFrame.h>
+
+class TGCheckButton;
+class TGNumberEntry;
+class TGColorSelect;
+class TGDoubleHSlider;
+
+class TGHSlider;
+
+namespace Reve {
+  class RGValuator;
+  class RGDoubleValuator;
+  class ZTransSubEditor;
+}
+
+namespace Alieve {
+  
+  class TOFSector;
+  
+  class TOFSectorEditor : public TGedFrame
+  {
+    //private:
+    TOFSectorEditor(const TOFSectorEditor&);            // Not implemented
+    TOFSectorEditor& operator=(const TOFSectorEditor&); // Not implemented
+
+  protected:
+    TOFSector* fM; // fModel dynamic-casted to TOFSectorEditor
+    
+    Reve::RGValuator* fSectorID;
+    
+    TGCheckButton*    fAutoTrans;
+
+    TGCheckButton**    fPlate;
+
+    TGCheckButton*    fPlate0;
+    TGCheckButton*    fPlate1;
+    TGCheckButton*    fPlate2;
+    TGCheckButton*    fPlate3;
+    TGCheckButton*    fPlate4;
+
+    Reve::RGValuator* fThreshold;
+    Reve::RGValuator* fMaxVal;   
+
+
+    // Declare widgets
+    // TGSomeWidget*   fXYZZ;
+    
+  public:
+    TOFSectorEditor(const TGWindow* p=0, Int_t width=170, Int_t height=30, UInt_t options = kChildFrame, Pixel_t back=GetDefaultFrameBackground());
+    virtual ~TOFSectorEditor();
+    
+    virtual void SetModel(TObject* obj);
+    void DoSectorID();
+    void DoAutoTrans(); 
+    void DoPlate0();
+    void DoPlate1();
+    void DoPlate2();
+    void DoPlate3();
+    void DoPlate4();
+
+    void DoPlate(Int_t nPlate);
+    void DoThreshold();
+    void DoMaxVal();
+
+
+    // Declare callback/slot methods
+    // void DoXYZZ();
+    
+    ClassDef(TOFSectorEditor, 0); // Editor for TOFSector
+  }; // endclass TOFSectorEditor
+  
+}
+
+#endif
---
+++ EVE/Alieve/TOFStrip.h
@@ -0,0 +1,63 @@
+#ifndef ALIEVE_TOFStrip_H
+#define ALIEVE_TOFStrip_H
+
+#include <Reve/QuadSet.h>
+#include <Reve/RenderElement.h>
+
+#include <Reve/RGBAPalette.h>
+#include <Reve/FrameBox.h>
+
+#include <TGeoManager.h>
+#include <TClonesArray.h>
+
+#include <AliTOFGeometry.h>
+#include <AliTOFGeometryV5.h>
+
+namespace Alieve {
+
+class TOFStrip : public Reve::QuadSet
+{
+  TOFStrip(const TOFStrip&);            // Not implemented
+  TOFStrip& operator=(const TOFStrip&); // Not implemented
+
+private:
+  void LoadQuads();
+  
+protected:
+  virtual void InitModule();
+  virtual void SetTrans();
+
+  AliTOFGeometry* fTOFgeometry;
+
+  TClonesArray *fTOFarray;
+
+  Int_t fSector;
+  Int_t fPlate;
+  Int_t fStrip;
+
+  Float_t  fDx;
+  Float_t  fDz;
+
+  TGeoManager *fGeoManager;
+
+public:
+  TOFStrip(const Text_t* n="TOFStrip", const Text_t* t=0);
+  TOFStrip(TGeoManager *localGeoManager,
+	    Int_t nSector, Int_t nPlate, Int_t nStrip);
+
+  TOFStrip(TGeoManager *localGeoManager,
+	    Int_t nSector, Int_t nPlate, Int_t nStrip,
+	    TClonesArray *tofArray);
+  virtual ~TOFStrip();
+
+  static Bool_t    fgStaticInitDone;
+  static void      InitStatics();
+
+  static Reve::FrameBox* fgTOFstripFrameBox;
+
+  static Reve::RGBAPalette* fgTOFstripPalette;
+
+  ClassDef(TOFStrip, 1);
+}; 
+}
+#endif
