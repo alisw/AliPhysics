bebae8ba86f32d700ac259472d7d02a48527c864
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-06-17 20:59:25 +0000

    Output directory is now suffixed with the number of events, and simulated files moved in a separate directory before reconstruction, so we can have both simulated and reconstructed digits, for instance (Laurent)

--- MUON/AlirootRun_MUONtest.sh
+++ MUON/AlirootRun_MUONtest.sh
@@ -2,22 +2,30 @@
 # $Id$
 # with galice.root, galice_sim.root 
 
+if [ $# -ne 1 ]; then
+  NEVENTS=3000
+  echo "Number of events not specified. Using $NEVENTS"
+else
+  NEVENTS=$1
+fi
+
 CURDIR=`pwd`
-OUTDIR=test_out
+OUTDIR=test_out.$NEVENTS
 
 rm -fr $OUTDIR
 mkdir $OUTDIR
 cp $ALICE_ROOT/MUON/.rootrc $ALICE_ROOT/MUON/rootlogon.C $OUTDIR
 cd $OUTDIR
 
+DUMPEVENT=5
 RUN=0
 FULLPATH="$CURDIR/$OUTDIR"
-NEVENTS=100
 SEED=1234567
+SIMDIR="generated"
 
 echo "Running simulation  ..."
 
-aliroot -b  >& testSim.out << EOF 
+aliroot -b >& testSim.out << EOF 
 // Uncoment following lines to run simulation with local residual mis-alignment
 // (generated via MUONGenerateGeometryData.C macro)
 // AliCDBManager* man = AliCDBManager::Instance();
@@ -27,15 +35,14 @@ gRandom->SetSeed($SEED);
 AliCDBManager::Instance()->SetRun($RUN);
 AliSimulation MuonSim("$ALICE_ROOT/MUON/Config.C");
 MuonSim.SetMakeTrigger("MUON");
-MuonSim.SetWriteRawData("MUON","raw.root", kTRUE);
+MuonSim.SetWriteRawData("MUON","raw.root",kTRUE);
 MuonSim.Run($NEVENTS);
 .q
 EOF
 
-echo "Removing Digits files ..."
-mkdir MUON.Digits
-mv MUON.Digits*.root MUON.Digits/ 
-mv galice.root MUON.Digits/ 
+echo "Moving generated files to $SIMDIR"
+mkdir $SIMDIR
+mv MUON*.root Kinematics*.root galice.root TrackRefs*.root $SIMDIR
 
 echo "Running reconstruction  ..."
 
@@ -44,40 +51,25 @@ AliCDBManager::Instance()->SetRun($RUN);
 gRandom->SetSeed($SEED);
 AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 1, 1., 10., AliMagFMaps::k5kG);
 AliTracker::SetFieldMap(field, kFALSE);
-AliReconstruction MuonRec("galice.root");
+AliReconstruction* MuonRec = new AliReconstruction("galice.root");
 MuonRec.SetInput("$FULLPATH/raw.root");
 MuonRec.SetRunVertexFinder(kFALSE);
 MuonRec.SetRunLocalReconstruction("MUON");
 MuonRec.SetRunTracking("MUON");
-MuonRec.SetFillESD("MUON");
+MuonRec.SetFillESD("");
 MuonRec.SetLoadAlignData("MUON");
-MuonRec.SetNumberOfEventsPerFile($NEVENTS);
-// Uncoment following line to run reconstruction with the orginal tracking method
-// instead of the kalman one (default)
-//MuonRec.SetOption("MUON","Original");
-// Use the following to change clustering method
-//MuonRec.SetOption("MUON","MLEM"); // new scheme AZs clustering
-//MuonRec.SetOption("MUON","SIMPLEFIT"); // new scheme simple fitting
-//MuonRec.SetOption("MUON","COG"); // new scheme basic center-of-gravity only
-// Use the following to disconnect the status map creation (only for debug!)
-// as this speeds up startup a bit...
-//MuonRec.SetOption("MUON","NOSTATUSMAP");
-// Use the following to write to disk the digits (from raw data)
-//MuonRec.SetOption("MUON","SAVEDIGITS");
+MuonRec.SetNumberOfEventsPerFile(1000);
+MuonRec.SetOption("MUON","SAVEDIGITS");
+MuonRec.SetOption("MUON","NOLOCALRECONSTRUCTION");
 MuonRec.Run();
+delete MuonRec;
 .q
 EOF
-
-if [ ! -e MUON.Digits.root ]; then
-    echo "Moving Digits files back ..."
-    mv MUON.Digits/MUON.Digits.root .
-fi 
-mv MUON.Digits/galice.root ./galice_sim.root 
 
 echo "Running Trigger efficiency  ..."
 aliroot -b >& testTriggerResults.out << EOF
 .L $ALICE_ROOT/MUON/MUONTriggerEfficiency.C+
-MUONTriggerEfficiency("galice_sim.root", "galice.root", 1);
+MUONTriggerEfficiency("$SIMDIR/galice.root", "galice.root", 1);
 .q
 EOF
 
@@ -86,34 +78,40 @@ echo "Running efficiency  ..."
 aliroot -b >& testResults.out << EOF
 .L $ALICE_ROOT/MUON/MUONefficiency.C+
 // no argument assumes Upsilon but MUONefficiency(443) works on Jpsi
-MUONefficiency("galice_sim.root");
+MUONefficiency("$SIMDIR/galice.root");
 .q
 EOF
 
 echo "Running check ..."
-
 aliroot -b >& testCheck.out << EOF
 gSystem->Load("libMUONevaluation");
 .L $ALICE_ROOT/MUON/MUONCheck.C+
-MUONCheck(0, 9, "galice_sim.root", "galice.root", "AliESDs.root"); 
+MUONCheck(0, $NEVENTS-1, "generated/galice.root", "galice.root", "AliESDs.root"); 
 .q
 EOF
 
-echo "Running dumps for selected event (5) ..."
+
+echo "Running dumps for selected event ($DUMPEVENT) ..."
 
 aliroot -b  << EOF
-AliMUONSimData simData("galice_sim.root");
-simData.DumpKine(5);       > dump.kine
-simData.DumpHits(5);       > dump.hits
-simData.DumpDigits(5);     > dump.digits
-simData.DumpSDigits(5);    > dump.sdigits
+AliMUONMCDataInterface mcdSim("$SIMDIR/galice.root");
+mcdSim.DumpKine($DUMPEVENT);       > dump.kine
+mcdSim.DumpHits($DUMPEVENT);       > dump.hits
+mcdSim.DumpTrackRefs($DUMPEVENT);  > dump.trackrefs
 
-AliMUONRecData recData("galice.root");
-recData.DumpRecPoints(5);  > dump.recpoints
-recData.DumpTracks(5);     > dump.tracks
-recData.DumpRecTrigger(5); > dump.rectrigger
+AliMUONDataInterface diSim("$SIMDIR/galice.root");
+diSim.DumpDigits($DUMPEVENT,true);     > dump.simdigits
+diSim.DumpSDigits($DUMPEVENT,true);    > dump.sdigits
+
+AliMUONDataInterface dRec("galice.root");
+dRec.DumpRecPoints($DUMPEVENT);  > dump.recpoints
+dRec.DumpTracks($DUMPEVENT);     > dump.tracks
+dRec.DumpTriggerTracks($DUMPEVENT); > dump.triggertracks
+dRec.DumpTrigger($DUMPEVENT); > dump.trigger
+dRec.DumpDigits($DUMPEVENT,true); > dump.recdigits
 .q
 EOF
+
 
 echo "Finished"  
 echo "... see results in test_out"
