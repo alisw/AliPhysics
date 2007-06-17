ab59373095f7d63ad3f2c82eb2b7d588272b61a0
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-06-17 20:35:42 +0000

    New interface of a VStore to hold digits (Laurent)

---
+++ MUON/AliMUONVDigitStore.cxx
@@ -0,0 +1,118 @@
+/**************************************************************************
+* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+*                                                                        *
+* Author: The ALICE Off-line Project.                                    *
+* Contributors are mentioned in the code where appropriate.              *
+*                                                                        *
+* Permission to use, copy, modify and distribute this software and its   *
+* documentation strictly for non-commercial purposes is hereby granted   *
+* without fee, provided that the above copyright notice appears in all   *
+* copies and that both the copyright notice and this permission notice   *
+* appear in the supporting documentation. The authors make no claims     *
+* about the suitability of this software for any purpose. It is          *
+* provided "as is" without express or implied warranty.                  *
+**************************************************************************/
+
+// $Id$
+
+/// \class AliMUONVDigitStore
+///
+/// Interface for a digit (or sdigit) container
+///
+/// It offers methods to Add, Find and Remove single elements, and
+/// can create iterators to loop over (part of) the elements.
+///
+/// \author Laurent Aphecetche, Subatech
+
+#include "AliMUONVDigitStore.h"
+
+#include "AliLog.h"
+#include "AliMUONVDigit.h"
+#include <TString.h>
+#include <TTree.h>
+
+/// \cond CLASSIMP
+ClassImp(AliMUONVDigitStore)
+/// \endcond
+
+//_____________________________________________________________________________
+AliMUONVDigitStore::AliMUONVDigitStore()
+{
+  /// ctor
+}
+
+//_____________________________________________________________________________
+AliMUONVDigitStore::~AliMUONVDigitStore()
+{
+  /// dtor
+}
+
+//_____________________________________________________________________________
+Bool_t 
+AliMUONVDigitStore::Add(TObject* object)
+{
+  /// Add an object, if it is of type AliMUONVDigit
+  if (object)
+  {
+    AliMUONVDigit* digit = dynamic_cast<AliMUONVDigit*>(object);
+    if (digit)
+    {
+      AliMUONVDigit* added = Add(*digit,AliMUONVDigitStore::kIgnore);
+      if (!added)
+      {
+        AliError("Could not add digit through Add(TObject*) method");
+      }
+      else
+      {
+        return kTRUE;
+      }
+    }
+  }
+  return kFALSE;
+}
+
+//_____________________________________________________________________________
+AliMUONVDigit* 
+AliMUONVDigitStore::Add(Int_t detElemId, 
+                       Int_t manuId,
+                       Int_t manuChannel,
+                       Int_t cathode,
+                       EReplacePolicy replace)
+{
+  /// Add a digit and return it
+  AliMUONVDigit* digit = CreateDigit(detElemId,manuId,manuChannel,cathode);
+  if (digit)
+  {
+    AliMUONVDigit* d = Add(*digit,replace);
+    delete digit;
+    return d;
+  }
+  return 0x0;
+}
+
+//_____________________________________________________________________________
+AliMUONVDigitStore*
+AliMUONVDigitStore::Create(TTree& tree)
+{
+  /// Create store from the given tree (if possible).
+  TString dataType = ( strcmp(tree.GetName(),"TreeD") == 0 ? "Digit" : 
+                       (strcmp(tree.GetName(),"TreeS")== 9 ? "SDigit" : "")
+                       );
+  return static_cast<AliMUONVDigitStore*>(AliMUONVStore::Create(tree,dataType.Data()));
+}
+
+//_____________________________________________________________________________
+Int_t 
+AliMUONVDigitStore::GetSize(Int_t detElemId, Int_t cathode) const
+{
+  /// Return the number of digits we have for a given detection element
+  TIter next(CreateIterator(detElemId,detElemId,cathode));
+  AliMUONVDigit* digit;
+  Int_t n(0);
+  while ( ( digit = static_cast<AliMUONVDigit*>(next()) ) )
+  {
+    ++n;
+  }
+  return n;
+}
+
