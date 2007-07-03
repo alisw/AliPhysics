d7df26bf7b10a34118efc6d715cb3761bce251ab
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-07-03 14:50:18 +0000

    Updated macro to visualize phos clusters, finally a good one (Matevz+Cvetan)

--- EVE/alice-macros/phos_clusters.C
+++ EVE/alice-macros/phos_clusters.C
@@ -11,22 +11,28 @@ Reve::PointSet* phos_clusters(RenderElement* cont=0)
   Reve::PointSet* clusters = new Reve::PointSet(10000);
   clusters->SetOwnIds(kTRUE);
 
-  AliPHOSEmcRecPoint *cl=NULL;
+  TObjArray *arr=NULL;
   TBranch *branch=cTree->GetBranch("PHOSEmcRP");
-  branch->SetAddress(&cl);
+  branch->SetAddress(&arr);
 
   Int_t nentr=(Int_t)branch->GetEntries();
-  Warning("phos_clusters",Form(" %d"),nentr);
   for (Int_t i=0; i<nentr; i++) {
     if (!branch->GetEvent(i)) continue;
 
-    Float_t g[3]; //global coordinates
-    cl->GetGlobalXYZ(g);
+    Int_t ncl=arr->GetEntriesFast();
+    while (ncl--) {
+      AliCluster *cl=(AliCluster*)arr->UncheckedAt(ncl);
 
-    clusters->SetNextPoint(g[0], g[1], g[2]);
-    AliCluster *atp = new AliCluster(*cl);
-    clusters->SetPointId(atp);
+      Float_t g[3]; //global coordinates
+      cl->GetGlobalXYZ(g);
+
+      AliCluster *atp = new AliCluster(*cl);
+      clusters->SetNextPoint(g[0], g[1], g[2]);
+      clusters->SetPointId(atp);
+    }
   }
+
+  Warning("phos_clusters"," %d",clusters->Size());
 
   if(clusters->Size() == 0 && gReve->GetKeepEmptyCont() == kFALSE) {
     Warning("phos_clusters", "No PHOS clusters");
