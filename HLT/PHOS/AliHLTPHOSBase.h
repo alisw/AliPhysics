6ee1c4e9bd50c7215188705c8109fd0e56a083c8
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-07-15 15:05:00 +0000

    New base for all classes in HLT/PHOS

---
+++ HLT/PHOS/AliHLTPHOSBase.h
@@ -0,0 +1,70 @@
+#ifndef ALIHLTPHOSBASE_H
+#define ALIHLTPHOSBASE_H
+
+/**************************************************************************
+ * This file is property of and copyright by the Experimental Nuclear     *
+ * Physics Group, Dep. of Physics                                         *
+ * University of Oslo, Norway, 2007                                       *
+ *                                                                        *
+ * Author: Per Thomas Hille <perthi@fys.uio.no> for the ALICE HLT Project.*
+ * Contributors are mentioned in the code where appropriate.              *
+ * Please report bugs to perthi@fys.uio.no                                *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+#include <iostream>
+#include <Rtypes.h>
+#include "TString.h"
+#include "AliHLTDataTypes.h"
+#include "AliHLTPHOSDefinitions.h"
+#include "AliHLTPHOSConstants.h"
+#include "AliHLTPHOSCommonDefs.h"
+
+using namespace PhosHLTConst;
+using namespace std;
+
+class  AliHLTPHOSBase
+{
+ public:
+  AliHLTPHOSBase();
+  virtual ~AliHLTPHOSBase();
+
+  template<typename T> 
+    void  DumpData(T *array, int N, int nPerLine)
+    {
+      cout <<   "DumpData N=  " << N <<endl;
+      for(int i= 0; i< N; i++)
+	{
+	  if((i%nPerLine == 0)  &&  (i != 0))
+	    {
+	      printf("\n");
+	    }
+
+	  cout << array[i]<< "\t";
+
+	}
+    }
+
+  template<typename T> 
+    void  Reset(T *array, int N)
+    {
+      for(int i= 0; i< N; i++)
+	{
+	  array[i] = 0;
+	}
+    }
+ 
+ private:
+  AliHLTPHOSBase(const AliHLTPHOSBase & );
+  AliHLTPHOSBase & operator = (const AliHLTPHOSBase &);
+
+};
+
+#endif
