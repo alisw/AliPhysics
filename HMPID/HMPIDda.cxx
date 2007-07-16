50977200d43fc0d6d228d9d3b259a052a99dd02f
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-07-16 13:14:00 +0000

    Updated version of the HMPID DA. To be checked on real data by the experts and then run in the pit

--- HMPID/HMPIDda.cxx
+++ HMPID/HMPIDda.cxx
@@ -34,50 +34,46 @@ int main(int argc, char **argv){
 
   int status;
 
+  /* log start of process */
+  printf("HMPID DA program started\n");  
+
   /* check that we got some arguments = list of files */
   if (argc<2) {
     printf("Wrong number of arguments\n");
     return -1;
   }
 
-  /* copy locally a file from daq detector config db */
+  /* copy locally a file from daq detector config db
   status=daqDA_DB_getFile("myconfig","./myconfig.txt");
   if (status) {
     printf("Failed to get config file : %d\n",status);
     return -1;
   }
-  /* and possibly use it */
-
-  /* open result file */
-  FILE *fp=NULL;
-  fp=fopen("./result.txt","a");
-  if (fp==NULL) {
-    printf("Failed to open file\n");
-    return -1;
-  }
+  and possibly use it */
 
   /* report progress */
   daqDA_progressReport(10);
 
 
   /* init some counters */
-  //int nevents_physics=0;
-  //int nevents_total=0;
+  Float_t SummQ[14][48][11][25], SummQ2[14][48][11][25];
+  Bool_t  isDDLOn[14];
+  for(Int_t ddl=0;ddl<=13;ddl++) {
+    isDDLOn[ddl] = kFALSE;
+    for(Int_t row=1;row<=24;row++)
+      for(Int_t dil=1;dil<=10;dil++)
+	for(Int_t adr=0;adr<=47;adr++)
+	  {
+	    SummQ[ddl][adr][dil][row]=0;
+	    SummQ2[ddl][adr][dil][row]=0;
+	  }
+  }
 
-    Float_t SummQ[14][48][11][25], Mean[14][48][11][25], SummQ2[14][48][11][25], Sigma[14][48][11][25];
-
+  /* init event counter */
   Int_t iEvtNcal=0;
 
-  ofstream out;
-
-  for(Int_t ddl=0;ddl<=13;ddl++) for(Int_t row=1;row<=24;row++) for(Int_t dil=1;dil<=10;dil++) for(Int_t adr=0;adr<=47;adr++)
-
-    {
-      SummQ[ddl][adr][dil][row]=0; SummQ2[ddl][adr][dil][row]=0; Mean[ddl][adr][dil][row]=0; Sigma[ddl][adr][dil][row]=0;
-    }
-
-    int n;
-    for (n=1;n<argc;n++) {
+  int n;
+  for (n=1;n<argc;n++) {
 
     status=monitorSetDataSource( argv[n] );
     if (status!=0) {
@@ -89,9 +85,7 @@ int main(int argc, char **argv){
     /* in this example, indexed on the number of files */
     daqDA_progressReport(10+80*n/argc);
 
-  for(;;) // infinite loop 
-
-    {
+    for(;;) { // infinite loop 
       struct eventHeaderStruct *event;
       eventTypeType eventT;
 
@@ -132,9 +126,8 @@ int main(int argc, char **argv){
 	    Int_t q = AliBitPacking::UnpackWord(RawDataWord, 0,11); assert(0<=q&&q<=4095); // 0000 0rrr rrdd ddaa aaaa qqqq qqqq qqqq Qdc        (0..4095)
 
 	    SummQ[ddl][a][d][r]+=q;
- 
             SummQ2[ddl][a][d][r]+=(q*q);
-	  
+	    isDDLOn[ddl] = kTRUE;
 	  }//raw records loop
 	
 	delete pRR;
@@ -148,52 +141,50 @@ int main(int argc, char **argv){
     
       } // events loop   
 
-     free(event);
- }
- 
-} 
-  /* close result file */
-  fclose(fp);
+      free(event);
+    }
+
+  }
+
+  /* write report */
+  printf("Run #%s, received %d calibration events\n",getenv("DATE_RUN_NUMBER"),iEvtNcal);
+
+  if (!iEvtNcal) {
+    printf("No calibration events have been read. Exiting\n");
+    return -1;
+  }
 
   /* report progress */
   daqDA_progressReport(90);
 
-  /* store the result file on FES */
-  status=daqDA_FES_storeFile("./result.txt","DAcase1_results");
-  if (status) {
-    printf("Failed to export file : %d\n",status);
-    return -1;
+  for(Int_t ddl=0; ddl < 14; ddl++) {
+    if (!isDDLOn[ddl]) continue;
+
+    ofstream out;
+    out.open(Form("./HmpidPedDdl%02i.txt",ddl));
+
+    for(Int_t row=1; row < 25; row++)
+      for(Int_t dil=1; dil < 11; dil++)
+	for(Int_t adr=0; adr < 48; adr++) {
+
+	  Float_t mean = SummQ[ddl][adr][dil][row]/iEvtNcal;
+          Float_t sigma = TMath::Sqrt(SummQ2[ddl][adr][dil][row]/iEvtNcal - (SummQ[ddl][adr][dil][row]/iEvtNcal)*(SummQ[ddl][adr][dil][row]/iEvtNcal));
+	  Int_t inhard=((Int_t(mean))<<9)+Int_t(mean+3*sigma);
+
+	  out << Form("%2i %2i %2i %5.2f %5.2f %x\n",row,dil,adr,mean,sigma,inhard);
+	}
+
+    /* store the result file on FES */
+    status=daqDA_FES_storeFile(Form("./HmpidPedDdl%02i.txt",ddl),Form("HMPID_DA_Pedestals_ddl=%02i",ddl));
+    if (status) {
+      printf("Failed to export file : %d\n",status);
+      return -1;
+    }
   }
+
 
   /* report progress */
   daqDA_progressReport(100);
 
   return status;
-
-      for(Int_t ddl=0;ddl<=13;ddl++){
-
-        out.open(Form("HmpidPedDdl%02i.txt",ddl));
-
-        for(Int_t row=1;row<=24;row++)
-
-          for(Int_t dil=1;dil<=10;dil++)
-
-            for(Int_t adr=0;adr<=47;adr++){
-
-              Mean[ddl][adr][dil][row] = SummQ[ddl][adr][dil][row]/iEvtNcal;
-
-              Sigma[ddl][adr][dil][row] = TMath::Sqrt(SummQ2[ddl][adr][dil][row]/iEvtNcal - (SummQ[ddl][adr][dil][row]/iEvtNcal)*(SummQ[ddl][adr][dil][row]/iEvtNcal));
-
-                Int_t inhard=((Int_t(Mean[ddl][adr][dil][row]))<<9)+Int_t(Mean[ddl][adr][dil][row]+3*Sigma[ddl][adr][dil][row]);
-
-                out << Form("%2i %2i %2i %5.2f %5.2f %x\n",row,dil,adr,Mean[ddl][adr][dil][row],Sigma[ddl][adr][dil][row],inhard);
-
-	    }
-      }
 }
-
-//}
-
-
-
-
