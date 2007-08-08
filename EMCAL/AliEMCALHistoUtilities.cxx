a365481f5d6830d32e7d1324319fa623bd74db1f
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-08-08 15:56:57 +0000

    Additional functionality (Aleksei)

--- EMCAL/AliEMCALHistoUtilities.cxx
+++ EMCAL/AliEMCALHistoUtilities.cxx
@@ -25,15 +25,30 @@
 
 #include "AliEMCALHistoUtilities.h"
 
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include <TROOT.h>
+#include <TPad.h>
 #include <TFile.h>
 #include <TH1.h>
 #include <TH2.h>
+#include <TF1.h>
+#include <TLatex.h>
+#include <TChain.h>
 #include <TList.h>
 #include <TObjArray.h>
 #include <TObjString.h>
-#include <TROOT.h>
 #include <TRegexp.h>
 #include <TString.h>
+#include <TLorentzVector.h>
+#include <Gtypes.h> // color, line style and so on
+
+#include "AliESDCaloCluster.h"
+#include "AliEMCALRecPoint.h"
+
+using namespace std;
 
 ClassImp(AliEMCALHistoUtilities)
 
@@ -59,6 +74,7 @@ TList* AliEMCALHistoUtilities::MoveHistsToList(const char* name, Bool_t putToBro
     if (!objHist->InheritsFrom("TH1")) continue;
     ((TH1*)objHist)->SetDirectory(0); // Remove from gROOT
     list->Add(objHist);
+    gDirectory->GetList()->Remove(objHist);
   }
   if(putToBrowser) gROOT->GetListOfBrowsables()->Add((TObject*)list);
   return list;
@@ -144,6 +160,45 @@ void AliEMCALHistoUtilities::AddToNameAndTitleToList(TList *l, const char *name,
   }
 }
 
+TLatex *AliEMCALHistoUtilities::lat(const char *text, Float_t x,Float_t y, Int_t align, Float_t tsize, short tcolor)
+{ 
+  double y1=y;
+  TLatex *latex = new TLatex;
+  latex->SetTextAlign(align);
+  latex->SetTextSize(tsize);
+  latex->SetTextColor(tcolor);
+  latex->DrawLatex(x, y1, text);
+  printf("<I> AliEMCALHistoUtilities::lat() -> %s gPad->GetLogy() %i\n", 
+  text, gPad->GetLogy());
+  return latex;
+}
+
+void AliEMCALHistoUtilities::InitChain(TChain *chain, const char* nameListOfFiles, Int_t nFileMax)
+{
+  // Read name of files from text file with nameListOfFiles and added to the chain
+  if(chain==0 || nameListOfFiles==0) return;
+ 
+  ifstream in;
+  in.open(nameListOfFiles);
+  if (!in) {
+    cout << "Input file "<<nameListOfFiles<<" cannot be opened.\n";
+    return;
+  }
+
+  Int_t nfiles = 0; // number of files in chain
+  char nf[200];     // name of current file
+  while (!in.getline(nf,200).eof()) {
+    if(!in.good()) break;
+    chain->Add(nf);
+    nfiles++;
+    cout<<nfiles<<" "<<nf<<endl;
+    if(nFileMax && nfiles>=nFileMax) break;
+  }
+  cout << " \n ********** <I> Accepted file "<< nfiles << "********* \n"<<endl;
+  //  chainEsd->Print();
+  //  chain->Lookup();
+}
+
 int AliEMCALHistoUtilities::ParseString(const TString &topt, TObjArray &Opt)
 { 
   // Moved from AliEMCALGeometry
@@ -162,3 +217,139 @@ int AliEMCALHistoUtilities::ParseString(const TString &topt, TObjArray &Opt)
   }
   return Opt.GetEntries();
 }
+// Analysis utilites
+Bool_t AliEMCALHistoUtilities::GetLorentzVectorFromESDCluster(TLorentzVector &v, const AliESDCaloCluster* cl)
+{
+  // May 8, 2007
+  static Double_t e=0.0;
+  static Float_t pos[3];
+  static TVector3 gpos;
+  if(cl==0) return kFALSE;
+  
+  e = Double_t(cl->E());
+  if(e<=0.0) {
+    printf(" negative cluster energy : %f \n", e);
+    return kFALSE;
+  }
+  cl->GetPosition(pos);
+  gpos.SetXYZ(Double_t(pos[0]), Double_t(pos[1]), Double_t(pos[2]));
+  gpos.SetMag(e);
+  v.SetVectM(gpos, 0.0);
+
+  return kTRUE;
+}
+
+Bool_t AliEMCALHistoUtilities::GetLorentzVectorFromRecPoint(TLorentzVector &v, const AliEMCALRecPoint  *rp)
+{
+  // Jun 20, 2007
+  static Double_t e=0.0;
+  static TVector3 gpos;
+  if(rp==0) return kFALSE;
+  
+  e = Double_t(rp->GetPointEnergy());
+  if(e<=0.0) {
+    printf(" negative rec.point energy : %f \n", e);
+    return kFALSE;
+  }
+  rp->GetGlobalPosition(gpos);
+  gpos.SetMag(e);
+  v.SetVectM(gpos, 0.0);
+
+  return kTRUE;
+}
+//
+//// Drawing:
+//
+void AliEMCALHistoUtilities::DrawHist(TH1* hid,int lineWidth,int lineColor,const char* opt, int lineStyle)
+{
+  TString OPT;
+  OPT = opt;
+  if(!hid) return;
+  printf(" Hist. %s : option |%s| \n", hid->GetName(), opt);
+  if(hid && hid->GetEntries()>=1.) {
+    if(lineWidth) hid->SetLineWidth(lineWidth);
+    if(lineColor) hid->SetLineColor(lineColor);
+    if(lineStyle) hid->SetLineStyle(lineStyle);
+    if(OPT.Contains("stat",TString::kIgnoreCase)) hid->SetStats(kTRUE);
+    hid->Draw(opt);
+  } else {
+    if   (strcmp(opt,"empty")==0) hid->Draw();
+    else printf(" has fewer entries %i or hid is zero\n", (int) hid->GetEntries());
+  }
+}
+
+//
+//// Fitting:
+//
+TF1* AliEMCALHistoUtilities::Gausi(const char *addName,double xmi,double xma,double N,double mean,double sig,double width)
+{ // Fit by gaus where first parameter is the number of events under ga
+  TString name("gi");
+  name += addName;
+  TF1 *F = new TF1(name.Data(), Gi, xmi, xma, 4); 
+  F->SetParNames("INTEGRAL","MEAN","SIGMA","WIDTH");
+
+  F->SetParameter(0,N);
+  F->SetParameter(1,mean);
+  F->SetParameter(2,sig);
+
+  F->FixParameter(3,width); // width of histogramm bin
+  return F;
+}
+
+TF1* AliEMCALHistoUtilities::Gausi(const char *addName, double xmi, double xma, TH1 *h) 
+{
+  // You can change parameters after that if you don't like this assumption
+  if(h) return Gausi(addName, xmi, xma, h->Integral(), h->GetMean(),h->GetRMS(), h->GetBinWidth(1));
+  else  return 0; 
+}
+
+TF1* AliEMCALHistoUtilities::GausiPol2(const char *addName,double xmi,double xma, TF1 *g, TF1* bg)
+{ // Fit by gaus where first parameter is the number of events under ga
+  TString name("giPol2");
+  name += addName;
+  TF1 *F = new TF1(name.Data(), GiPol2, xmi, xma, 7); 
+  F->SetParNames("INTEGRAL","MEAN","SIGMA","WIDTH","a0","a1","a2");
+
+  if(g) {
+    for(int i=0; i<4; i++) F->SetParameter(i, g->GetParameter(i));
+    F->FixParameter(3,g->GetParameter(3));
+  }
+
+  if(bg) {
+    for(int i=4; i<7; i++) F->SetParameter(i, bg->GetParameter(i+4));
+  }
+  F->SetLineColor(kRed);
+  return F;
+}
+
+Double_t AliEMCALHistoUtilities::Gi(Double_t *x, Double_t *par)
+{ 
+  // First parameter is integral (number events under gaus)
+  // Forth parameter (par[3]) is width of histogram bin 
+  // gaus(0) is a substitute for [0]*exp(-0.5*((x-[1])/[2])**2)
+
+  static Double_t C = TMath::Sqrt(2.*TMath::Pi()), y=0.0, f=0.0; // sqrt(2.*pi)
+
+  y  = (x[0]-par[1])/par[2];
+  f  = par[0]*par[3]/(par[2]*C) * TMath::Exp(-0.5*y*y);
+
+  return f;
+}
+
+Double_t AliEMCALHistoUtilities::GiPol2(Double_t *x, Double_t *par)
+{ 
+  // First parameter is integral (number events under gaus)
+  // Forth parameter (par[3]) is width of histogram bin 
+  // gaus(0) is a substitute for [0]*exp(-0.5*((x-[1])/[2])**2)
+  // + pol2 -> 7 parameters
+  static Double_t C = TMath::Sqrt(2.*TMath::Pi()), y=0.0, f=0.0; // sqrt(2.*pi)
+
+  y  = (x[0]-par[1])/par[2];
+  f  = par[0]*par[3]/(par[2]*C) * TMath::Exp(-0.5*y*y);
+
+  f += par[4] + par[5]*x[0] + par[6]*x[0]*x[0];
+
+  return f;
+}
+
+
--- EMCAL/AliEMCALHistoUtilities.h
+++ EMCAL/AliEMCALHistoUtilities.h
@@ -14,8 +14,16 @@
 //*-- Authors: J.L. Klay (LLNL) & Aleksei Pavlinov (WSU)
 
 #include <TNamed.h>
+
 class TList;
 class TH1;
+class TF1;
+class TLatex;
+class TChain;
+class TLorentzVector;
+
+class AliESDCaloCluster;
+class AliEMCALRecPoint;
 
 class AliEMCALHistoUtilities: public TNamed {
   public:
@@ -33,8 +41,25 @@ class AliEMCALHistoUtilities: public TNamed {
   const char* opt="RECREATE");
   static void AddToNameAndTitle(TH1   *h=0, const char *name=0, const char *title=0);
   static void AddToNameAndTitleToList(TList *l=0, const char *name=0, const char *title=0);
+  static TLatex *lat(const char *text="", Float_t x=0.0,Float_t y=0.0, Int_t align=12, Float_t tsize=0.05, short tcolor = 1); 
+  // TChain
+  static void InitChain(TChain *chain=0, const char* nameListOfFiles=0, Int_t nFileMax=0); 
   // 
   static int ParseString(const TString &topt, TObjArray &Opt); 
+  // Analysis utilites
+  static Bool_t GetLorentzVectorFromESDCluster(TLorentzVector &v, const AliESDCaloCluster *cl);
+  static Bool_t GetLorentzVectorFromRecPoint(TLorentzVector &v, const AliEMCALRecPoint  *rp);
+  // Drawing 
+  static void DrawHist(TH1* hid=0,int lineWidth=1,int lineColor=1,const char* opt="",int lineStyle=1);
+  // Fitting:
+  static TF1* Gausi(const char *addName, double xmi, double xma, double N, double mean, double sig, 
+  double width);
+  static TF1* Gausi(const char *addName, double xmi, double xma, TH1 *h);
+
+  static TF1* GausiPol2(const char *addName, double xmi, double xma, TF1 *g, TF1* bg);
+  //
+  static Double_t Gi(Double_t *x, Double_t *par);
+  static Double_t GiPol2(Double_t *x, Double_t *par);
 
   AliEMCALHistoUtilities & operator = (const AliEMCALHistoUtilities &) {
     Fatal("operator =", "not implemented") ; return *this ; }
