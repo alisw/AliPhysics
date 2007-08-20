8d298daec14579ef65459fe7db3846c7a96dac7f
dibari <dibari@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-08-20 07:53:14 +0000

    New order to take into account general mods in libs

--- HMPID/Hlib.C
+++ HMPID/Hlib.C
@@ -2,22 +2,31 @@ void Hlib()
 {
   gSystem->Load("libPhysics.so");
   gSystem->Load("libMinuit.so");
-  gSystem->Load("libTree.so");
-  gSystem->Load("libVMC.so");
-  gSystem->Load("libESD.so");
-  gSystem->Load("libSTEER.so");
-  gSystem->Load("libCDB.so");
-
-  gSystem->Load("libEVGEN.so");
-  gSystem->Load("libSTRUCT.so");
   
-  gSystem->Load("libANALYSIS.so");
+  gSystem->Load("libEG");
+  gSystem->Load("libGeom");
+  gSystem->Load("libVMC");
 
-  gSystem->Load("libRAWDatasim.so");
-  
+  gSystem->Load("libEGPythia6");
+
+  gSystem->Load("libNet");
+  gSystem->Load("libTree");
+ 
+  gSystem->Load("libSTEERBase");
+  gSystem->Load("libESD");
+  gSystem->Load("libCDB");
+  gSystem->Load("libSTEER");
+  gSystem->Load("libRAWDatabase");
+  gSystem->Load("libRAWDatarec");
+  gSystem->Load("libRAWDatasim");
+
   gSystem->Load("libHMPIDbase.so");
   gSystem->Load("libHMPIDsim.so");
   gSystem->Load("libHMPIDrec.so");
 
   gSystem->AddIncludePath("-I$ALICE_ROOT/include -I$ALICE_ROOT/HMPID"); 
 }
+
+
+
+
