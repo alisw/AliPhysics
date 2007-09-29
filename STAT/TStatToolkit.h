49088cd7cd16f86d9e3abc6ca02921dcbaa69bd6
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-09-29 10:04:45 +0000

    Adding a TStatToolkit Some frequently used statistical functions
    Operation on arrays
    Histograms
    Trees
    
    
    Marian

---
+++ STAT/TStatToolkit.h
@@ -0,0 +1,58 @@
+#ifndef ALIMATHBASE_H
+#define TSTATTOOLKIT_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+
+ 
+#include "TObject.h"
+#include "TVectorD.h"
+#include "TMatrixD.h"
+#include "TGraph2D.h"
+#include "TGraph.h"
+
+class TH1F;
+class TH3;
+class TString;
+class TTree;
+ 
+class TStatToolkit : public TObject
+{
+ public:
+  TStatToolkit();
+  virtual ~TStatToolkit();
+  //
+  //
+  //
+  static void    EvaluateUni(Int_t nvectors, Double_t *data, Double_t &mean, Double_t &sigma, Int_t hh);
+  static void    EvaluateUniExternal(Int_t nvectors, Double_t *data, Double_t &mean, Double_t &sigma, Int_t hh, Float_t externalfactor=1);
+  static Int_t  Freq(Int_t n, const Int_t *inlist, Int_t *outlist, Bool_t down);    
+  //
+  // HISTOGRAMS TOOLS
+  //
+  static  void TruncatedMean(TH1F * his, TVectorD *param, Float_t down=0, Float_t up=1.0, Bool_t verbose=kFALSE);
+  static void LTM(TH1F * his, TVectorD *param=0 , Float_t fraction=1,  Bool_t verbose=kFALSE);
+  static Double_t  FitGaus(TH1F* his, TVectorD *param=0, TMatrixD *matrix=0, Float_t xmin=0, Float_t xmax=0,  Bool_t verbose=kFALSE);
+  static Double_t  FitGaus(Float_t *arr, Int_t nBins, Float_t xMin, Float_t xMax, TVectorD *param=0, TMatrixD *matrix=0, Bool_t verbose=kFALSE);
+  static Float_t  GetCOG(Short_t *arr, Int_t nBins, Float_t xMin, Float_t xMax, Float_t *rms=0, Float_t *sum=0);
+
+  static TGraph2D *  MakeStat2D(TH3 * his, Int_t delta0, Int_t delta1, Int_t type);
+  static TGraph *  MakeStat1D(TH3 * his, Int_t delta1, Int_t type);
+
+  //
+  // Fitting function
+  //
+  static TString* FitPlane(TTree * tree, const char* drawCommand, const char* formula, const char* cuts, Double_t & chi2, Int_t &npoints,  TVectorD &fitParam, TMatrixD &covMatrix, Int_t start=0, Int_t stop=10000000);
+
+
+
+
+  //
+  // TestFunctions:
+  //
+ static  void TestGausFit(Int_t nhistos=5000);
+    
+ ClassDef(TStatToolkit,0) // Various mathematical tools for physics analysis - which are not included in ROOT TMath
+ 
+};
+#endif
