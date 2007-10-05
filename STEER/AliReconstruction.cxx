f388cc178618eeb193a0ba260ebc6bb8c4b462e0
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-05 15:40:21 +0000

    Bugfix

--- STEER/AliReconstruction.cxx
+++ STEER/AliReconstruction.cxx
@@ -2710,7 +2710,7 @@ AliQualAssDataMaker * AliReconstruction::GetQualAssDataMaker(Int_t iDet)
   }
   if (qadm) {
     AliInfo(Form("Initializing quality assurance data maker for %s", fgkDetectorName[iDet]));
-    qadm->Init(AliQualAss::kRECPOINTS, fRawReader->GetRunNumber(), GetQACycles(fgkDetectorName[iDet]));
+    qadm->Init(AliQualAss::kRECPOINTS, fRunLoader->GetHeader()->GetRun(), GetQACycles(fgkDetectorName[iDet]));
     qadm->StartOfCycle(AliQualAss::kRECPOINTS);
     qadm->StartOfCycle(AliQualAss::kESDS, "same") ; 	
     fQualAssDataMaker[iDet] = qadm;
