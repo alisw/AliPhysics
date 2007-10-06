2076a6e1c7cf18a4cd4803f51260f875dd414971
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-06 22:15:04 +0000

    clean-up and removal of TPC legacy code

--- HLT/exa/runMakePileup.C
+++
@@ -1,8 +0,0 @@
-//$Id$
-
-void runMakePileup(Char_t *path,Int_t startev,Int_t endev,Char_t *pileupdir="pileup",Int_t npiles=25,Int_t triggerevent=-1,Char_t *gfile=0)
-{
-  gSystem->Load("MakePileup_C.so");
-
-  MakePileup(path,startev,endev,pileupdir,npiles,triggerevent,gfile);
-}
