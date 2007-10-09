253e935317b20d5130df4389908957727672dee5
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-09 07:37:54 +0000

    From Eleazar: one module was missing. Load geometry from current directory.

--- EVE/alice-macros/geom_acorde.C
+++ EVE/alice-macros/geom_acorde.C
@@ -4,20 +4,20 @@ void geom_acorde()
 {
   using namespace std;
 
-  //  gGeoManager = gReve->GetGeometry("$REVESYS/alice-data/alice_fullgeo.root");
-  Reve::RenderElementList* list = new Reve::RenderElementList("ACORDE1_");
+  gGeoManager = gReve->GetGeometry("geometry.root");
+
+  Reve::RenderElementList* list = new Reve::RenderElementList("ACORDE");
   gReve->AddGlobalRenderElement(list);
 
-
-  for(Int_t i=1; i<60; ++i){
+  for(Int_t i=1; i<61; ++i) {
     char form[10000];
     sprintf(form, "ACORDE1_%d", i);
     TGeoNode* node = gGeoManager->GetTopVolume()->FindNode(form);       
     Reve::GeoTopNodeRnrEl* re =  new Reve::GeoTopNodeRnrEl(gGeoManager, node);
     re->UseNodeTrans();
     gReve->AddGlobalRenderElement(list, re);
+    // gReve->AddGlobalRenderElement(re, list); // For EVE-dev
   }
   
-  gReve->Redraw3D();
-  
+  gReve->Redraw3D();  
 }
