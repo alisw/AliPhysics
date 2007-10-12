4fb43ed074484a2b7eb6f84b34164f1d7f306549
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-12 13:35:45 +0000

    rootcint sees ALI_DATE and not DATE_ROOT

--- TPC/TPCmonLinkDef.h
+++ TPC/TPCmonLinkDef.h
@@ -3,6 +3,9 @@
  * See cxx source for full Copyright notice                               */
 
 /* $Log$
+/* Revision 1.2  2007/09/17 16:34:54  cvetan
+/* The package was overwriting the rootcint flags. This was fixed by applying the necessary changes in the DATE-dependent parts of the code
+/*
 /* Revision 1.1  2007/09/17 10:23:31  cvetan
 /* New TPC monitoring package from Stefan Kniege. The monitoring package can be started by running TPCMonitor.C macro located in macros folder.
 /* */
@@ -17,7 +20,7 @@
 #pragma link C++ class  AliTPCMonitorFFT+; 
 #pragma link C++ class  AliTPCMonitorMappingHandler+;
 
-#ifdef DATE_ROOT
+#ifdef ALI_DATE
 #pragma link C++ class  AliTPCMonitorDateMonitor+;
 #endif
 #pragma link C++ class  AliTPCMonitorDateFile+;  
