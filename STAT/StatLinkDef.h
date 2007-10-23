9c9f0978ea56456809795fcc9881c080e1e26aa7
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-23 10:04:35 +0000

    library name changed

---
+++ STAT/StatLinkDef.h
@@ -0,0 +1,22 @@
+#ifdef __CINT__
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TKDTree<Int_t, Float_t>+;
+#pragma link C++ class TKDTree<Int_t, Double_t>+;
+#pragma link C++ typedef TKDTreeID;
+#pragma link C++ typedef TKDTreeIF;
+
+
+#pragma link C++ class TKDInterpolator+;
+#pragma link C++ class TKDInterpolator::TKDNodeInfo+;
+#pragma link C++ class TKDSpline+;
+
+
+#endif
+
