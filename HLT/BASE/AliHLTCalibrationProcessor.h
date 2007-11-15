1557824af774ada431d237f5bad9b97c9f03ce56
szostak <szostak@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-11-15 16:40:10 +0000

    The ProcessCalibration methods should be protected, not private, because classes need to inherit them.

--- HLT/BASE/AliHLTCalibrationProcessor.h
+++ HLT/BASE/AliHLTCalibrationProcessor.h
@@ -83,6 +83,41 @@ class AliHLTCalibrationProcessor : public AliHLTProcessor {
   static const AliHLTUInt32_t fgkFXSProtocolHeaderSize;
   static const AliHLTUInt32_t fgkFXSProtocolHeaderVersion;
 
+  /*
+   * ######################## ProcessCalibaration #####################
+   */
+
+  /**
+   * The low-level data processing method for the component,
+   * called for every data event. This is the custom processing 
+   * method and can be overloaded by the component.
+   * @param evtData       event data structure
+   * @param blocks        input data block descriptors
+   * @param trigData	  trigger data structure
+   * @param outputPtr	  pointer to target buffer
+   * @param size	  <i>input</i>: size of target buffer
+   *            	  <i>output</i>:size of produced data
+   * @param outputBlocks  list to receive output block descriptors
+   * @return neg. error code if failed
+   */
+  virtual Int_t ProcessCalibration(const AliHLTComponent_EventData& evtData,
+			  const AliHLTComponent_BlockData* blocks,
+			  AliHLTComponent_TriggerData& trigData, AliHLTUInt8_t* outputPtr,
+			  AliHLTUInt32_t& size,
+			  vector<AliHLTComponent_BlockData>& outputBlocks);
+ 
+  /**  
+   * The high-level data processing method for the component,
+   * called for every data event. This is the custom processing 
+   * method and can be overloaded by the component.
+   * This is the default processing method; the method is called
+   * if no low level @ref ProcessCalibration method is overloaded by the component.
+   * @param evtData       event data structure
+   * @param trigData	  trigger data structure
+   * @return neg. error code if failed
+   */
+  virtual Int_t ProcessCalibration( const AliHLTComponentEventData& evtData, AliHLTComponentTriggerData& trigData);
+
  private:
 
   /*
@@ -159,41 +194,6 @@ class AliHLTCalibrationProcessor : public AliHLTProcessor {
 		       AliHLTUInt8_t* outputPtr, 
 		       AliHLTUInt32_t& size,
 		       vector<AliHLTComponentBlockData>& outputBlocks );
-
-  /*
-   * ######################## ProcessCalibaration #####################
-   */
-
-  /**
-   * The low-level data processing method for the component,
-   * called for every data event. This is the custom processing 
-   * method and can be overloaded by the component.
-   * @param evtData       event data structure
-   * @param blocks        input data block descriptors
-   * @param trigData	  trigger data structure
-   * @param outputPtr	  pointer to target buffer
-   * @param size	  <i>input</i>: size of target buffer
-   *            	  <i>output</i>:size of produced data
-   * @param outputBlocks  list to receive output block descriptors
-   * @return neg. error code if failed
-   */
-  virtual Int_t ProcessCalibration(const AliHLTComponent_EventData& evtData,
-			  const AliHLTComponent_BlockData* blocks,
-			  AliHLTComponent_TriggerData& trigData, AliHLTUInt8_t* outputPtr,
-			  AliHLTUInt32_t& size,
-			  vector<AliHLTComponent_BlockData>& outputBlocks);
- 
-  /**  
-   * The high-level data processing method for the component,
-   * called for every data event. This is the custom processing 
-   * method and can be overloaded by the component.
-   * This is the default processing method; the method is called
-   * if no low level @ref ProcessCalibration method is overloaded by the component.
-   * @param evtData       event data structure
-   * @param trigData	  trigger data structure
-   * @return neg. error code if failed
-   */
-  virtual Int_t ProcessCalibration( const AliHLTComponentEventData& evtData, AliHLTComponentTriggerData& trigData);
 
   /*
    * ######################## ShipDataToFXS #####################
