40bb03c6b1de563d0486e6958aedd5636d77fe79
alla <alla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-11-15 09:27:18 +0000

    neede for T0 QA classes

--- T0/AliT0digit.h
+++ T0/AliT0digit.h
@@ -15,16 +15,18 @@ class AliT0digit: public TObject {
   AliT0digit();
   virtual ~AliT0digit();
 
-  Int_t BestTimeA() {return fBestTimeA;}
-  Int_t BestTimeC() {return fBestTimeC;}
-  Int_t MeanTime() {return fTimeAverage;}
-  Int_t TimeDiff() {return fTimeDiff;}
-  Int_t SumMult() {return fSumMult;}
+  Int_t BestTimeA()   {return fBestTimeA;}
+  Int_t BestTimeC()   {return fBestTimeC;}
+  Int_t MeanTime()    {return fTimeAverage;}
+  Int_t TimeDiff()    {return fTimeDiff;}
+  Int_t SumMult()     {return fSumMult;}
+  Int_t RefPoint()    {return fRefPoint;}
   void SetTimeBestA( Int_t time) {fBestTimeA = time;}
   void SetTimeBestC( Int_t time) {fBestTimeC = time;}
   void SetMeanTime(Int_t time) {fTimeAverage=time;}
   void SetDiffTime(Int_t time) {fTimeDiff=time;}
   void SetSumMult(Int_t time) {fSumMult=time;}
+  void SetRefPoint(Int_t time) {fRefPoint=time;}
   
   virtual void SetTimeCFD (TArrayI &o);
   virtual void GetTimeCFD (TArrayI &o);
@@ -47,10 +49,11 @@ class AliT0digit: public TObject {
   Int_t fBestTimeA;        // TOF first particle on the right 
   Int_t fBestTimeC;         // TOF first particle on the left
   Int_t fSumMult;   //multiplisity
+  Int_t fRefPoint; // reference point
   AliT0digit( const AliT0digit& );
   AliT0digit& operator=(const AliT0digit&); 
   
-  ClassDef(AliT0digit,5)  //Digit (Header) object for set:T0
+  ClassDef(AliT0digit,6)  //Digit (Header) object for set:T0
 };
 
 
