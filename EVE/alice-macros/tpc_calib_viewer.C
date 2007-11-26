3bc8538fcf4f3e6000c65bf4f2e5a8e8935f2c87
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-11-26 12:26:15 +0000

    From Lars Bozyk: create TPC callibration viewer in accordance with ROOT browser standards.

--- EVE/alice-macros/tpc_calib_viewer.C
+++ EVE/alice-macros/tpc_calib_viewer.C
@@ -1,8 +1,19 @@
 void tpc_calib_viewer(const char* file="CalibTree.root")
 {
-  Reve::RGBrowser* b = gReve->GetBrowser();
-  b->StartEmbedding(1);
-  AliTPCCalibViewerGUI::ShowGUI(file);
-  b->StopEmbedding();
-  b->SetTabTitle("TPC Calib", 1);
+   Reve::RGBrowser* b = gReve->GetBrowser();
+   b->StartEmbedding(1);
+
+   TGMainFrame* frmMain = new TGMainFrame(gClient->GetRoot(), 1000, 600);
+   frmMain->SetWindowName("AliTPCCalibViewer GUI");
+   frmMain->SetCleanup(kDeepCleanup);
+
+   AliTPCCalibViewerGUI* calibViewer1 = new AliTPCCalibViewerGUI(frmMain, 1000, 600, (char*)file);
+   frmMain->AddFrame(calibViewer1, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 0, 0, 0, 0));
+
+   frmMain->MapSubwindows();
+   frmMain->Resize();
+   frmMain->MapWindow();
+
+   b->StopEmbedding();
+   b->SetTabTitle("TPC CalibViewer", 1);
 }
