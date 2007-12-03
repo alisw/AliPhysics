0fe2963b1dd9625a469186945f69a9329f45be96
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-12-03 14:31:14 +0000

    Making histograms browsable (Marian)

--- PWG1/AliComparisonDraw.h
+++ PWG1/AliComparisonDraw.h
@@ -17,7 +17,9 @@ class TProfile2D;
 class TGraph2D;
 class TGraph; 
 
-class AliComparisonDraw : public TObject {
+#include "TNamed.h"
+
+class AliComparisonDraw : public TNamed {
 public :
   AliComparisonDraw(); 
   ~AliComparisonDraw();
@@ -46,54 +48,54 @@ protected:
   // efficiency 
   //
   static    Bool_t    fgBDraw;         //option draw temporary results
-  TProfile* fEffTPCPt;      // TPC efficiency as function of Pt (tan+-1)
-  TProfile* fEffTPCPtMC;    // MC -TPC efficiency as function of Pt (tan+-1)
-  TProfile* fEffTPCPtF;     // efficiency for findable tracks
+  TProfile* fEffTPCPt;      //->TPC efficiency as function of Pt (tan+-1)
+  TProfile* fEffTPCPtMC;    //->MC -TPC efficiency as function of Pt (tan+-1)
+  TProfile* fEffTPCPtF;     //->efficiency for findable tracks
   //
-  TProfile* fEffTPCTan;   // TPC efficiency as function of Tan (pt>0.15
-  TProfile* fEffTPCTanMC; // MC -TPC efficiency as function of Tan (pt>0.15)
-  TProfile* fEffTPCTanF;  // efficiency for findable tracks Tan (pt>0.15)
+  TProfile* fEffTPCTan;   //->TPC efficiency as function of Tan (pt>0.15
+  TProfile* fEffTPCTanMC; //->MC -TPC efficiency as function of Tan (pt>0.15)
+  TProfile* fEffTPCTanF;  //->efficiency for findable tracks Tan (pt>0.15)
   //
-  TProfile2D* fEffTPCPtTan;    // TPC efficiency as function of Pt and tan
-  TProfile2D* fEffTPCPtTanMC;  // MC -TPC efficiency as function of Pt and tan
-  TProfile2D* fEffTPCPtTanF;  // TPC efficiency as function of Pt and tan
+  TProfile2D* fEffTPCPtTan;    //->TPC efficiency as function of Pt and tan
+  TProfile2D* fEffTPCPtTanMC;  //->MC -TPC efficiency as function of Pt and tan
+  TProfile2D* fEffTPCPtTanF;  //-> TPC efficiency as function of Pt and tan
   //
   // dEdx resolution
   //
-  TH2F* fTPCSignalNormTan; // tpc signal normalized to the mean signal - MC
-  TH2F* fTPCSignalNormSPhi;   // tpc signal normalized to the mean signal - MC
-  TH2F* fTPCSignalNormTPhi;   // tpc signal normalized to the mean signal - MC
+  TH2F* fTPCSignalNormTan; //-> tpc signal normalized to the mean signal - MC
+  TH2F* fTPCSignalNormSPhi;   //-> tpc signal normalized to the mean signal - MC
+  TH2F* fTPCSignalNormTPhi;   //-> tpc signal normalized to the mean signal - MC
   //
-  TH3F* fTPCSignalNormTanSPhi;   // tpc signal normalized to the mean signal - MC
-  TH3F* fTPCSignalNormTanTPhi;   // tpc signal normalized to the mean signal - MC
-  TH3F* fTPCSignalNormTanSPt;   // tpc signal normalized to the mean signal - MC
+  TH3F* fTPCSignalNormTanSPhi;   //-> tpc signal normalized to the mean signal - MC
+  TH3F* fTPCSignalNormTanTPhi;   //-> tpc signal normalized to the mean signal - MC
+  TH3F* fTPCSignalNormTanSPt;   //->tpc signal normalized to the mean signal - MC
 
 
   //
   //
-  TH2F* fPtResolLPT;        // pt resolution - low pt
-  TH2F* fPtResolHPT;        // pt resolution - high pt 
-  TH2F* fPtPullLPT;         // pt resolution - low pt
-  TH2F* fPtPullHPT;         // pt resolution - high pt 
+  TH2F* fPtResolLPT;        //-> pt resolution - low pt
+  TH2F* fPtResolHPT;        //-> pt resolution - high pt 
+  TH2F* fPtPullLPT;         //-> pt resolution - low pt
+  TH2F* fPtPullHPT;         //-> pt resolution - high pt 
   //
   // Resolution constrained param
   //
-  TH2F   *fCPhiResolTan;   // angular resolution -  constrained
-  TH2F   *fCTanResolTan;   // angular resolution -  constrained
-  TH2F   *fCPtResolTan;    // pt resolution      -  constrained
-  TH2F   *fCPhiPullTan;   // angular resolution -  constrained
-  TH2F   *fCTanPullTan;   // angular resolution -  constrained
-  TH2F   *fCPtPullTan;    // pt resolution      -  constrained
+  TH2F   *fCPhiResolTan;   //-> angular resolution -  constrained
+  TH2F   *fCTanResolTan;   //-> angular resolution -  constrained
+  TH2F   *fCPtResolTan;    //-> pt resolution      -  constrained
+  TH2F   *fCPhiPullTan;   //-> angular resolution -  constrained
+  TH2F   *fCTanPullTan;   //-> angular resolution -  constrained
+  TH2F   *fCPtPullTan;    //-> pt resolution      -  constrained
   //
   // DCA resolution
   //
-  TH3F  *fD0TanSPtB1;   // distance to vertex y  
-  TH3F  *fD1TanSPtB1;   // distance to vertex z  
-  TH3F  *fD0TanSPtL1;   // distance to vertex y  
-  TH3F  *fD1TanSPtL1;   // distance to vertex z  
+  TH3F  *fD0TanSPtB1;   //-> distance to vertex y  
+  TH3F  *fD1TanSPtB1;   //-> distance to vertex z  
+  TH3F  *fD0TanSPtL1;   //-> distance to vertex y  
+  TH3F  *fD1TanSPtL1;   //-> distance to vertex z  
 
 protected:
-   ClassDef(AliComparisonDraw,1);
+   ClassDef(AliComparisonDraw,2);
 };
 
 
