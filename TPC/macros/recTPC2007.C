1f06363383f2ae3775b2cba063a1456c784931fa
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-12-16 20:37:00 +0000

    Changing parameters (Marian)

--- TPC/macros/recTPC2007.C
+++ TPC/macros/recTPC2007.C
@@ -1,7 +1,11 @@
 void recTPC2007(Int_t type, const char *filename="data.root")
 {
+  //  .L $ALICE_ROOT/TPC/macros/recTPC2007.C
+  
+  // recTPC2007(0,"rfio:/castor/cern.ch/alice/data/2007/12/16/11/07000013151011.20.root")
+  //recTPC2007(0,"alien:///alice/data/2007/LHC07w_TPC/000012674/raw/07000012674011.90.root")
+  //  recTPC2007(0,"/data/test2007/07000012674011/07000012674011.90.root")
 
-  // .x recTPC(0,"rfio:/castor/cern.ch/alice/tpc/2007/12/13/00/07000011524011.980.root")
   //
   // Set path to calibration data
   //
@@ -10,7 +14,8 @@ void recTPC2007(Int_t type, const char *filename="data.root")
   //
   // Set reconstruction parameters
   ////
-  gSystem->Load("/afs/cern.ch/alice/tpctest/root/HEAD/lib/libRAliEn.so");  
+  //gSystem->Load("/afs/cern.ch/alice/tpctest/root/HEAD/lib/libRAliEn.so");  
+  //gSystem->Load("$ROOTSYS/lib/libXrdClient.so") 
   TGrid::Connect("alien://");
   //
 
@@ -18,10 +23,20 @@ void recTPC2007(Int_t type, const char *filename="data.root")
   AliTPCRecoParam * tpcRecoParam = 0;
   if (type==0)  tpcRecoParam = AliTPCRecoParam::GetCosmicTestParam(kTRUE);
   if (type>0)  tpcRecoParam = AliTPCRecoParam::GetLaserTestParam(kTRUE);
-  tpcRecoParam->SetTimeInterval(60,1000);
+  tpcRecoParam->SetTimeInterval(60,940);
   tpcRecoParam->Dump();
+  //
+  //
+ //  tpcRecoParam->SetMinMaxCutAbs(4.);
+//   tpcRecoParam->SetMinLeftRightCutAbs(6.);
+//   tpcRecoParam->SetMinUpDownCutAbs(7.);
+//   tpcRecoParam->SetMinMaxCutSigma(4.);
+//   tpcRecoParam->SetMinLeftRightCutSigma(6.);
+//   tpcRecoParam->SetMinUpDownCutSigma(7.);
+  //
+  //
   AliTPCReconstructor::SetRecoParam(tpcRecoParam);
-  AliTPCReconstructor::SetStreamLevel(1);
+  AliTPCReconstructor::SetStreamLevel(100);
   AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 1., 10., 1);
   AliTracker::SetFieldMap(field,1);
   //
@@ -37,6 +52,7 @@ void recTPC2007(Int_t type, const char *filename="data.root")
   rec.SetFillESD("TPC");
   rec.SetFillTriggerESD(kFALSE);
   rec.SetRunVertexFinder(kFALSE);
+  rec.SetCleanESD(kFALSE);
   rec.Run();
 }
 
