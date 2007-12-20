009ffc78de5e213a0e54e781ffa8f0c4d5d07ee6
bhippoly <bhippoly@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-12-20 16:31:05 +0000

    Couple of Updates from H.Ricaud

--- PWG2/SPECTRA/AliAnalysisTaskESDStrangeMC.h
+++ PWG2/SPECTRA/AliAnalysisTaskESDStrangeMC.h
@@ -29,6 +29,7 @@ class AliAnalysisTaskESDStrangeMC : public AliAnalysisTask {
 
   // MC histograms
   TH1F        *fHistPtMC; //Pt spectrum
+  TH1F        *fHistMCMultiplicity;
 
   TH2F        *fHistMCPtVsYK0s;
   TH2F        *fHistMCPtVsYLambda;
@@ -38,12 +39,20 @@ class AliAnalysisTaskESDStrangeMC : public AliAnalysisTask {
   TH1F        *fHistTrackPerEvent;
   TH1F        *fHistMCDaughterTrack;
 
+  TH1F        *fHistPrimaryVertexX;
+  TH1F        *fHistPrimaryVertexY;
+  TH1F        *fHistPrimaryVertexZ;
+
   TH2F        *fHistDcaPosToPrimVertex;
   TH2F        *fHistDcaNegToPrimVertex;
+  TH2F        *fHistDcaPosToPrimVertexZoom;
+  TH2F        *fHistDcaNegToPrimVertexZoom;
   TH2F        *fHistRadiusV0;
   TH2F        *fHistDecayLengthV0;
   TH2F        *fHistDcaV0Daughters;
   TH2F        *fHistChi2;
+  TH2F        *fHistCosPointAngle;
+  TH2F        *fHistCosPointAngleZoom;
   TH2F        *fHistPtVsYK0s;
   TH2F        *fHistPtVsYK0sMI;
   TH2F        *fHistPtVsYLambda;
@@ -58,6 +67,16 @@ class AliAnalysisTaskESDStrangeMC : public AliAnalysisTask {
   TH1F        *fHistMassAntiLambda;
   TH1F        *fHistMassAntiLambdaMI;
 
+  TH2F        *fHistMassVsRadiusK0;
+  TH2F        *fHistMassVsRadiusK0MI;
+  TH2F        *fHistMassVsRadiusLambda;
+  TH2F        *fHistMassVsRadiusLambdaMI;
+  TH2F        *fHistMassVsRadiusAntiLambda;
+  TH2F        *fHistMassVsRadiusAntiLambdaMI;
+
+  TH2F        *fHistArmenterosPodolanski;
+  TH2F        *fHistArmenterosPodolanskiMI;
+
 
   // Associated particles histograms
   TH1F        *fHistAsMcPtK0;
@@ -66,6 +85,10 @@ class AliAnalysisTaskESDStrangeMC : public AliAnalysisTask {
   TH1F        *fHistAsMcPtLambdaMI;
   TH1F        *fHistAsMcPtAntiLambda;
   TH1F        *fHistAsMcPtAntiLambdaMI;
+  TH1F        *fHistAsMcPtZoomK0;
+  TH1F        *fHistAsMcPtZoomK0MI;
+  TH1F        *fHistAsMcPtZoomLambda;
+  TH1F        *fHistAsMcPtZoomLambdaMI;
   TH1F        *fHistPidMcMassK0;
   TH1F        *fHistPidMcMassK0MI;
   TH1F        *fHistPidMcMassLambda;
@@ -78,7 +101,57 @@ class AliAnalysisTaskESDStrangeMC : public AliAnalysisTask {
   TH1F        *fHistAsMcMassLambdaMI;
   TH1F        *fHistAsMcMassAntiLambda;
   TH1F        *fHistAsMcMassAntiLambdaMI;
-   
+
+  TH2F        *fHistAsMcMassVsRadiusK0;
+  TH2F        *fHistAsMcMassVsRadiusK0MI;
+  TH2F        *fHistAsMcMassVsRadiusLambda;
+  TH2F        *fHistAsMcMassVsRadiusLambdaMI;
+  TH2F        *fHistAsMcMassVsRadiusAntiLambda;
+  TH2F        *fHistAsMcMassVsRadiusAntiLambdaMI;
+
+  TH1F        *fHistAsMcResxK0;
+  TH1F        *fHistAsMcResyK0;
+  TH1F        *fHistAsMcReszK0;
+
+  TH2F        *fHistAsMcResrVsRadiusK0;
+  TH2F        *fHistAsMcReszVsRadiusK0;
+
+  TH1F        *fHistAsMcResxK0MI;
+  TH1F        *fHistAsMcResyK0MI;
+  TH1F        *fHistAsMcReszK0MI;
+
+  TH2F        *fHistAsMcResrVsRadiusK0MI;
+  TH2F        *fHistAsMcReszVsRadiusK0MI;
+
+  TH1F        *fHistAsMcResxLambda;
+  TH1F        *fHistAsMcResyLambda;
+  TH1F        *fHistAsMcReszLambda;
+
+  TH2F        *fHistAsMcResrVsRadiusLambda;
+  TH2F        *fHistAsMcReszVsRadiusLambda;
+    
+  TH1F        *fHistAsMcResxLambdaMI;
+  TH1F        *fHistAsMcResyLambdaMI;
+  TH1F        *fHistAsMcReszLambdaMI;
+
+  TH2F        *fHistAsMcResrVsRadiusLambdaMI;
+  TH2F        *fHistAsMcReszVsRadiusLambdaMI;
+
+  TH1F        *fHistAsMcResxAntiLambda;
+  TH1F        *fHistAsMcResyAntiLambda;
+  TH1F        *fHistAsMcReszAntiLambda;
+
+  TH2F        *fHistAsMcResrVsRadiusAntiLambda;
+  TH2F        *fHistAsMcReszVsRadiusAntiLambda;
+    
+  TH1F        *fHistAsMcResxAntiLambdaMI;
+  TH1F        *fHistAsMcResyAntiLambdaMI;
+  TH1F        *fHistAsMcReszAntiLambdaMI;
+
+  TH2F        *fHistAsMcResrVsRadiusAntiLambdaMI;
+  TH2F        *fHistAsMcReszVsRadiusAntiLambdaMI;
+
+
   AliAnalysisTaskESDStrangeMC(const AliAnalysisTaskESDStrangeMC&); 
   AliAnalysisTaskESDStrangeMC& operator=(const AliAnalysisTaskESDStrangeMC&); 
 
