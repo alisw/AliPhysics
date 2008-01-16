8f30e79546de70e598fb531e2111cf52573c2d56
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-01-16 10:39:03 +0000

    Implemented methods: MakeHits(TTree*), MakeSDigits(TTree*), MakeDigits(TTree*) (Christian)

--- MUON/AliMUONQADataMakerSim.h
+++ MUON/AliMUONQADataMakerSim.h
@@ -3,21 +3,20 @@
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
+/* $Id$ */
+
 /// \ingroup sim
 /// \class AliMUONQADataMakerSim
 /// \brief MUON Quality assurance data maker
 ///
 //  Author Christian Finck
 
-// dummy function for simulation part
-// to avoid circular dependencie
-
 // --- ROOT system ---
 class TObjArray; 
 
 // --- AliRoot header files ---
-class AliMUONVClusterStore;
-class AliMUONVTrackStore;
+class AliMUONVHitStore;
+class AliMUONVDigitStore;
 
 #include "AliQADataMakerSim.h"
 
@@ -31,29 +30,32 @@ public:
   
 private:
   virtual void   StartOfDetectorCycle(); 
-      /// init hits QA from Array (not implemented)
-  virtual void   InitHits() {return;} 
-     /// init SDigits QA from Array (not implemented)
-  virtual void   InitSDigits() {return;}  
-     /// init SDigits QA from Array (not implemented)
-  virtual void   InitDigits() {return;} 
+      /// init hits QA from Array 
+  virtual void   InitHits(); 
+     /// init SDigits QA from Array 
+  virtual void   InitSDigits();  
+     /// init SDigits QA from Array
+  virtual void   InitDigits();
     
     /// make hits QA from Array (not implemented)
   virtual void   MakeHits(TClonesArray* /*hits*/) {return;}
-    /// make hits QA from tree (not implemented)
-  virtual void   MakeHits(TTree* /*hits*/)        {return;}
+    /// make hits QA from tree
+  virtual void   MakeHits(TTree* hitsTree);
     /// make SDigits QA from Array (not implemented)
   virtual void   MakeSDigits(TClonesArray* /*sigits*/) {return;} 
-    /// make SDigits QA from Tree (not implemented)
-  virtual void   MakeSDigits(TTree* /*sigits*/)        {return;} 
+    /// make SDigits QA from Tree
+  virtual void   MakeSDigits(TTree* sigitsTree);
      /// make Digits QA from Array (not implemented)
   virtual void   MakeDigits(TClonesArray* /*digits*/)  {return;}
-      /// make SDigits QA from Tree (not implemented)
-  virtual void   MakeDigits(TTree* /*digits*/)         {return;}
+      /// make SDigits QA from Tree
+  virtual void   MakeDigits(TTree* digitsTree);
   
-    virtual void   EndOfDetectorCycle(AliQA::TASKINDEX, TObjArray* list);
+  virtual void   EndOfDetectorCycle(AliQA::TASKINDEX, TObjArray* list);
 
-  ClassDef(AliMUONQADataMakerSim,1)  // MUON Quality assurance data maker
+  AliMUONVHitStore*   fHitStore;   //!< pointer to hit store
+  AliMUONVDigitStore* fDigitStore; //!< pointer to digit store
+                                    
+  ClassDef(AliMUONQADataMakerSim,2)  // MUON Quality assurance data maker
 
 };
 #endif
