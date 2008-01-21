2d30a10b007b2a6727c3e93901a52a98d0a62d79
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-01-21 12:57:14 +0000

    Remove obsolete macro

--- macros/GLfca.C
+++
@@ -1,7 +0,0 @@
-void GLfca()
-{
-   gSystem.Load("/usr/X11R6/lib/libXmu.so");
-   gSystem.Load("/soft/Mesa-3.0/lib/libMesaGL.so");
-   gSystem.Load("/soft/Mesa-3.0/lib/libMesaGLU.so");
-   gSystem.Load("$ROOTSYS/lib/libRGL.so");
-}
