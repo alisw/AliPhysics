82095805fcc8377399daefe5037938e0b907e14f
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-01-22 14:10:30 +0000

    AliCutTask uses global cut initialization

--- PWG0/esdTrackCuts/run.C
+++ PWG0/esdTrackCuts/run.C
@@ -34,15 +34,6 @@ void run(Char_t* data, Int_t nRuns=20, Int_t offset=0, Bool_t aDebug = kFALSE, B
   // Create the analysis manager
   mgr = new AliAnalysisManager("testAnalysis");
 
-  // selection of esd tracks
-  gROOT->ProcessLine(".L CreateCuts.C");
-  AliESDtrackCuts* esdTrackCuts = CreateTrackCuts();
-  if (!esdTrackCuts)
-  {
-    printf("ERROR: esdTrackCuts could not be created\n");
-    return;
-  }
-
   TString taskName("AliCutTask.cxx+");
   if (aDebug)
     taskName += "+g";
@@ -54,7 +45,24 @@ void run(Char_t* data, Int_t nRuns=20, Int_t offset=0, Bool_t aDebug = kFALSE, B
     gROOT->Macro(taskName);
 
   task = new AliCutTask;
-  task->SetTrackCuts(esdTrackCuts);
+
+  AliPWG0Helper::AnalysisMode analysisMode = AliPWG0Helper::kTPC;
+  task->SetAnalysisMode(analysisMode);
+
+  if (analysisMode != AliPWG0Helper::kSPD)
+  {
+    // selection of esd tracks
+    gROOT->ProcessLine(".L ../CreateStandardCuts.C");
+    AliESDtrackCuts* esdTrackCuts = CreateTrackCuts(analysisMode);
+    if (!esdTrackCuts)
+    {
+      printf("ERROR: esdTrackCuts could not be created\n");
+      return;
+    }
+
+    task->SetTrackCuts(esdTrackCuts);
+  }
+
   mgr->AddTask(task);
 
   // Add ESD handler
