27943f44a4d49f961169b357470d0ec5b17a4559
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-01-23 23:03:58 +0000

    adding missing header file

---
+++ HLT/BASE/AliHLTMisc.h
@@ -0,0 +1,50 @@
+// @(#) $Id: AliHLTMisc.h 23396 2008-01-18 12:45:35Z richterm $
+
+#ifndef ALIHLTMISC_H
+#define ALIHLTMISC_H
+//* This file is property of and copyright by the ALICE HLT Project        * 
+//* ALICE Experiment at CERN, All rights reserved.                         *
+//* See cxx source for full Copyright notice                               */
+
+/** @file   AliHLTMisc.h
+    @author Matthias Richter
+    @date   
+    @brief  Definition of various glue functions implemented in dynamically
+            loaded libraries
+*/
+
+#define ALIHLTMISC_LIBRARY "libHLTrec.so"
+#define ALIHLTMISC_INIT_CDB "AliHLTMiscInitCDB"
+#define ALIHLTMISC_SET_CDB_RUNNO "AliHLTMiscSetCDBRunNo"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /**
+   * Init the CDB access for the running instance.
+   * The method is used from the C wrapper interface utilized by the  on-line
+   * framework. The path of the (H)CDB is set to the specified path.<br>
+   * When running from AliRoot, the CDB path is set in the startup of the
+   * reconstruction.
+   * @param cdbpath     path to the CDB
+   * @return neg. error code if failed
+   * @note function implemented in libHLTrec
+   */
+  int AliHLTMiscInitCDB(const char* cdbpath);
+  typedef int (*AliHLTMiscInitCDB_t)(const char* cdbpath);
+
+
+  /**
+   * Init the Run no for the CDB access.
+   * @param cdbpath     path to the CDB
+   * @return neg. error code if failed
+   * @note function implemented in libHLTrec
+   */
+  int AliHLTMiscSetCDBRunNo(int runNo);
+  typedef int (*AliHLTMiscSetCDBRunNo_t)(int runNo);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //ALIHLTMISC_H
