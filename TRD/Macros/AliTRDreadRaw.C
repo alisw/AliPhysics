ee6053f33d9913597a5e0f10180e8b504acbecab
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-01-25 15:37:43 +0000

    Introduction of raw stream base class

---
+++ TRD/Macros/AliTRDreadRaw.C
@@ -0,0 +1,40 @@
+void AliTRDreadRaw(const char *fname = "raw.root")
+{
+  AliCDBManager *cdb = AliCDBManager::Instance();
+  cdb->SetDefaultStorage("local://$ALICE_ROOT");
+  cdb->SetRun(0);
+  
+  AliTRDdigitsManager manR;
+  manR.CreateArrays();
+
+  AliRawReaderRoot reader(fname, 0);
+  reader.Select("TRD");
+
+  while (reader.NextEvent())
+    {
+      AliTRDrawStreamBase *pstream = AliTRDrawStreamBase::GetRawStream(&reader);
+      AliTRDrawStreamBase &stream = *pstream;
+      
+      Int_t ichambers = 0;
+      while (stream.NextChamber(&manR) >= 0)
+	ichambers++;
+      
+      cout << "Chambers loaded " << stream.IsA()->GetName() << " " 
+	   << ichambers << endl;
+      
+      delete pstream
+    }
+}
+
+void readRaw2(const char *fname = "raw.root")
+{
+  AliCDBManager *cdb = AliCDBManager::Instance();
+  cdb->SetDefaultStorage("local://$ALICE_ROOT");
+  cdb->SetRun(0);
+
+  AliRawReaderRoot reader(fname, 0);
+  reader.Select("TRD");
+
+  AliTRDrawData rawData;
+  AliTRDdigitsManager *man = rawData.Raw2Digits(&reader);
+}
