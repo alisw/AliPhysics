55884a2240ecbcd8c3e3948bb813e6f88514b3c3
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-02-15 08:57:49 +0000

    New QA gui by Sylwester

---
+++ TRD/TRDguiLinkDef.h
@@ -0,0 +1,18 @@
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliTRDqaGuiMain+;
+#pragma link C++ class AliTRDqaGuiClusters+;
+#pragma link C++ class AliTRDqaGuiClustersSM+; 
+#pragma link C++ class AliTRDqaGuiClustersStack+;
+#pragma link C++ class AliTRDqaGuiESDs+;
+
+#pragma link C++ class AliTRDqaGuiMainBlack+;
+#pragma link C++ class AliTRDqaGuiBlackSM+;
+#pragma link C++ class AliTRDqaGuiBlackChamber+;
+
+#endif
