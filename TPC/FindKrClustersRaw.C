e9b47b5dd49cb56fb6810b6c37e506a8859c99ed
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-02-15 11:28:03 +0000

    Avoid using of RunLoaders (Marian)

--- TPC/FindKrClustersRaw.C
+++ TPC/FindKrClustersRaw.C
@@ -1,7 +1,7 @@
 Int_t FindKrClustersRaw(const char *fileName="data.root"){
 
   //define tree
-  TFile *hfile=new TFile("adc.root","RECREATE","ADC file");
+  TFile *hfile=new TFile("KryptonCl.root","RECREATE","ADC file");
   // Create a ROOT Tree
   TTree *mytree = new TTree("Kr","Krypton cluster tree");
 
@@ -22,24 +22,21 @@ Int_t FindKrClustersRaw(const char *fileName="data.root"){
   clusters->SetRecoParam(0);
 
   //only for geometry parameters loading - temporarly
-  AliRunLoader* rl = AliRunLoader::Open("galice.root");
-  AliTPCParam *param=(AliTPCParamSR *)gDirectory->Get("75x40_100x60_150x60");
+  //AliRunLoader* rl = AliRunLoader::Open("galice.root");
+  //AliTPCParam *param=(AliTPCParamSR *)gDirectory->Get("75x40_100x60_150x60");
+  AliTPCParam *param=new AliTPCParam;
+  param->SetTSample(1.00000002337219485e-07);
+  param->Update();
   //if (!param) {cerr<<"TPC parameters have not been found !\n"; return 4;}
   clusters->SetParam(param);
-
+  
 
   Int_t evtnr=0;
   while (reader->NextEvent()) {
     //output for each event
-    //  AliTPCclustererKr *clusters = new AliTPCclustererKr();
-    //  clusters->SetOutput(mytree);
-
-    //if(evtnr++<35)continue;
     cout<<"Evt = "<<evtnr<<endl;
     clusters->finderIO(reader);
     evtnr++;
-
-
   }
 
   //mytree->Print();//print rootuple summary 
