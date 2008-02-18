d6f5f4f78a091c9c4fda2903be5b604a25235de0
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-02-18 10:54:49 +0000

    Adding the PWG2 AOD code Creates a new library with a PWG2 specific information that needs to be added to the AOD as an additional track information, which has a TRef pointing back at the original AliAODTrack Adding a filter task which creates the PWG2 AOD

---
+++ PWG2/AOD/AliAnalysisTaskPWG2ESDfilter.h
@@ -0,0 +1,56 @@
+//-------------------------------------------------------------------------
+//     Task for the Analysis Framework 
+// Creates a PWG2 specific AOD, from the ESD, adding information required
+// for PWG2 analysis. 
+//  - Puts the per-track information into the AliPWG2AODTrack container, 
+//    together with the link to the original AliAODTrack
+//
+//     Author: Adam Kisiel, OSU, Adam.Kisiel@cern.ch
+//-------------------------------------------------------------------------
+#ifndef ALIANALYSISTASKPWG2ESDFILTER_H
+#define ALIANALYSISTASKPWG2ESDFILTER_H
+ 
+#include <AliAnalysisTask.h>
+
+class AliESDEvent;
+class TChain;
+class AliAODEvent;
+class AliAnalysisFilter;
+class TTree;
+class TClonesArray;
+class TList;
+
+class AliAnalysisTaskPWG2ESDfilter : public AliAnalysisTask
+{
+ public:
+    AliAnalysisTaskPWG2ESDfilter();
+    AliAnalysisTaskPWG2ESDfilter(const char* name);
+    virtual ~AliAnalysisTaskPWG2ESDfilter() {;}
+    // Implementation of interface methods
+    virtual void ConnectInputData(Option_t *option = "");
+    virtual void CreateOutputObjects();
+    virtual void Init();
+    virtual void LocalInit() {Init();}
+    virtual void Exec(Option_t *option);
+    virtual void Terminate(Option_t *option);
+    // Setters
+    virtual void SetTrackFilter(AliAnalysisFilter* trackF) {fTrackFilter = trackF;}
+    virtual void SetKinkFilter (AliAnalysisFilter*  KinkF) {fKinkFilter  =  KinkF;}
+    virtual void SetV0Filter   (AliAnalysisFilter*    V0F) {fV0Filter    =    V0F;}
+    virtual void SetDebugLevel(Int_t level) {fDebug = level;}
+    
+ private:
+    Int_t              fDebug;         //  Debug flag
+    TTree*             fTree;          //! chained files
+    AliESDEvent*       fESD;           //! ESD
+    AliAODEvent*       fAOD;           //! AOD event 
+    TTree*             fTreeA;         //! AOD tree
+    AliAnalysisFilter* fTrackFilter;   //  Track Filter
+    AliAnalysisFilter* fKinkFilter;    //  Kink  Filter
+    AliAnalysisFilter* fV0Filter;      //  V0    Filter    
+    TClonesArray*      fPWG2AODTracks; //! container for PWG2 specific information
+
+    ClassDef(AliAnalysisTaskPWG2ESDfilter, 1); // Analysis task for standard ESD filtering
+};
+ 
+#endif
---
+++ PWG2/PWG2AODLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+
+#pragma link off all glols;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  AliPWG2AODTrack+;
+#pragma link C++ class  AliAnalysisTaskPWG2ESDfilter+;
+
+#endif
