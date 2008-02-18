750047ca1214c977ddd0b46575d0c27b056fe523
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-02-18 10:54:49 +0000

    Adding the PWG2 AOD code Creates a new library with a PWG2 specific information that needs to be added to the AOD as an additional track information, which has a TRef pointing back at the original AliAODTrack Adding a filter task which creates the PWG2 AOD

---
+++ PWG2/PROOF-INF.PWG2AOD/SETUP.C
@@ -0,0 +1,32 @@
+void SETUP() {
+  // Load some ROOT libraries
+  CheckLoadLibrary("libEG");
+  CheckLoadLibrary("libGeom");
+  
+  // Load the ESD library
+  CheckLoadLibrary("libESD");
+
+  // Load the ESD library
+  CheckLoadLibrary("libAOD");
+
+  // Load the ESD library
+  CheckLoadLibrary("libANALYSIS");
+
+  // Load the PWG2 library
+  CheckLoadLibrary("libPWG2AOD");
+
+  // Set the include paths
+  gROOT->ProcessLine(".include PWG2AOD");
+
+  // Set our location, so that other packages can find us
+  gSystem->Setenv("PWG2AOD_INCLUDE", "PWG2/AOD");
+}
+
+Int_t CheckLoadLibrary(const char* library) {
+  // checks if a library is already loaded, if not loads the library
+  
+  if (strlen(gSystem->GetLibraries(Form("%s.so", library), "", kFALSE)) > 0)
+    return 1;
+  
+  return gSystem->Load(library);
+}
