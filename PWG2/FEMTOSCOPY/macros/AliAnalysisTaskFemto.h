9be56f4302458d3afc8716a6f58c8041d7b08af5
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-02-27 13:47:00 +0000

    Updating the AliAnalysisFemtoTask to work with both the ESD and AOD output

--- PWG2/FEMTOSCOPY/macros/AliAnalysisTaskFemto.h
+++ PWG2/FEMTOSCOPY/macros/AliAnalysisTaskFemto.h
@@ -1,17 +1,31 @@
+//------------------------------------------------------
+// AliAnalysisTaskFemto - A task for the analysis framework
+// from the FEMTOSCOPY analysis of PWG2. Creates the necessary
+// connection between the ESD or AOD input and the femtoscopic
+// code.
+// Author: Adam Kisiel, OSU; Adam.Kisiel@cern.ch
+//------------------------------------------------------
+#ifndef ALIANALYSISTASKFEMTO_H
+#define ALIANALYSISTASKFEMTO_H
+
 #include "TH1.h"
 
+#include "AliESDInputHandler.h"
+#include "AliAODInputHandler.h"
 #include "AliESDEvent.h"
+#include "AliAODEvent.h"
 
 #include "AliAnalysisTask.h"
 #include "AliAnalysisManager.h"
 #include "AliAnalysisDataContainer.h"
 
 #include "AliFemtoEventReaderESDChain.h"
+#include "AliFemtoEventReaderAODChain.h"
 #include "AliFemtoManager.h"
 
 class AliAnalysisTaskFemto : public AliAnalysisTask {
  public:
-  AliAnalysisTaskFemto() : AliAnalysisTask(), fESD(0), fHistPt(0) {}
+  AliAnalysisTaskFemto() : AliAnalysisTask(), fESD(0), fAOD(0), fOutputList(0), fReaderESD(0x0), fReaderAOD(0x0), fManager(0x0), fAnalysisType(0) {}
   AliAnalysisTaskFemto(const char *name);
   virtual ~AliAnalysisTaskFemto() {}
   
@@ -20,15 +34,20 @@ class AliAnalysisTaskFemto : public AliAnalysisTask {
   virtual void   Exec(Option_t *option);
   virtual void   Terminate(Option_t *);
 
-  void SetFemtoReader(AliFemtoEventReaderESDChain *aReader);
+  void SetFemtoReaderESD(AliFemtoEventReaderESDChain *aReader);
+  void SetFemtoReaderAOD(AliFemtoEventReaderAODChain *aReader);
   void SetFemtoManager(AliFemtoManager *aManager);
-  //  void SetFriendAddress(AliESDfriend **aFriendAddress);
 
  private:
-  AliESDEvent   *fESD;       // ESD object
-  TH1F          *fHistPt;    // Pt spectrum
-  AliFemtoEventReaderESDChain *fReader;   // AliFemto reader for ESD given by the chain
-  AliFemtoManager             *fManager;  // AliFemto top-level manager 
-
-  ClassDef(AliAnalysisTaskFemto, 1); // example of analysis
+  AliESDEvent                 *fESD;          //! ESD object
+  AliAODEvent                 *fAOD;          //! AOD object
+  TList                       *fOutputList;   //  AliFemto results list
+  AliFemtoEventReaderESDChain *fReaderESD;    //! AliFemto reader for ESD given by the chain
+  AliFemtoEventReaderAODChain *fReaderAOD;    //! AliFemto reader for AOD given by the chain
+  AliFemtoManager             *fManager;      //! AliFemto top-level manager 
+  int                          fAnalysisType; //  Mark ESD of AOD analysis
+ 
+  ClassDef(AliAnalysisTaskFemto, 2); // example of analysis
 };
+
+#endif
