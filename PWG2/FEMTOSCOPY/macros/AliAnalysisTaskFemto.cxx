dcedaef99a2f4b4177970cf46d9265334d723207
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-02-27 13:47:00 +0000

    Updating the AliAnalysisFemtoTask to work with both the ESD and AOD output

--- PWG2/FEMTOSCOPY/macros/AliAnalysisTaskFemto.cxx
+++ PWG2/FEMTOSCOPY/macros/AliAnalysisTaskFemto.cxx
@@ -1,4 +1,10 @@
-#define AliAnalysisTaskFemto_cxx
+//------------------------------------------------------
+// AliAnalysisTaskFemto - A task for the analysis framework
+// from the FEMTOSCOPY analysis of PWG2. Creates the necessary
+// connection between the ESD or AOD input and the femtoscopic
+// code.
+// Author: Adam Kisiel, OSU; Adam.Kisiel@cern.ch
+//------------------------------------------------------
 #include "TROOT.h"
 #include "TChain.h"
 #include "TH1.h"
@@ -10,126 +16,145 @@
 
 #include "AliESDEvent.h"
 
+#include "AliFemtoAnalysis.h"
 #include "AliAnalysisTaskFemto.h"
 
 ClassImp(AliAnalysisTaskFemto)
 
 //________________________________________________________________________
-  AliAnalysisTaskFemto::AliAnalysisTaskFemto(const char *name) :AliAnalysisTask(name,""), fESD(0), fHistPt(0) {
+  AliAnalysisTaskFemto::AliAnalysisTaskFemto(const char *name): 
+    AliAnalysisTask(name,""), 
+    fESD(0), 
+    fAOD(0),
+    fOutputList(0), 
+    fReaderESD(0x0),
+    fReaderAOD(0x0),
+    fManager(0x0),
+    fAnalysisType(0)
+{
   // Constructor.
   // Input slot #0 works with an Ntuple
   DefineInput(0, TChain::Class());
-  //  DefineInput(1, TChain::Class());
   // Output slot #0 writes into a TH1 container
-  DefineOutput(0, TH1F::Class());
+  DefineOutput(0, TList::Class());
 }
 
 //________________________________________________________________________
 void AliAnalysisTaskFemto::ConnectInputData(Option_t *) {
   printf("   ConnectInputData %s\n", GetName());
 
-//   char ** address = (char **)GetBranchAddress(0, "ESD");
-//   cout << "Got tree address " << address << endl;
-//   if (address) {
-//     fESD = (AliESDEvent*)(*address);
-//   }
-//   else  {
-//     fESD = new AliESDEvent();
-//     fESD->
-//     //    SetBranchAddress(0, "ESD", &fESD);
-//   }
-  TChain *inpchain = (TChain *) GetInputData(0);
-  fESD = new AliESDEvent();
-  fESD->ReadFromTree(inpchain);
+  fESD = 0;
+  fAOD = 0;
+  fAnalysisType = 0;
+
+  TTree* tree = dynamic_cast<TTree*> (GetInputData(0));
+  if (!tree) {
+    Printf("ERROR: Could not read chain from input slot 0");
+  } 
+  else {
+    AliESDInputHandler *esdH = dynamic_cast<AliESDInputHandler*> (AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler());
+    
+    if(esdH) {
+      cout << "Selected ESD analysis" << endl;
+      fAnalysisType = 1;
+      
+      if (!esdH) {
+	Printf("ERROR: Could not get ESDInputHandler");
+      } 
+      else {
+	fESD = esdH->GetEvent();
+      }
+    }
+    else {
+      AliAODInputHandler *aodH = dynamic_cast<AliAODInputHandler*> (AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler());
+      
+      if (!aodH) {
+	Printf("ERROR: Could not get AODInputHandler");
+      } 
+      else {
+	cout << "Selected AOD analysis" << endl;
+	fAnalysisType = 2;
+
+	fAOD = aodH->GetEvent();
+      }
+    }
+    if ((!fAOD) && (!fESD)) {
+      Printf("Wrong analysis type: Only ESD and AOD types are allowed!");
+    }
+  }
   
   
-
-//   char ** addressfriend = (char **)GetBranchAddress(1, "ESDfriend");
-//   cout << "Got friend address " << addressfriend << endl;
-//   if (addressfriend) {
-//     fESDfriend = (AliESDfriend*)(*address);
-//   }
-//   else  {
-//     fESDfriend = new AliESDfriend();
-//     SetBranchAddress(1, "ESDfriend", &fESDfriend);
-//   }
-
-
-//   TString fileName(chain->GetCurrentFile()->GetName());
-//   fileName.ReplaceAll("AliESDs", "AliESDfriends");
-//   cout << "Reading friend " << fileName.Data() << endl;;
-//   chain->AddFriend("esdFriendTree",fileName.Data());
-//   SetBranchAddress(0, "ESDfriend",&fESDfriend);
-
 }
 
 //________________________________________________________________________
 void AliAnalysisTaskFemto::CreateOutputObjects() {
-  if (!fHistPt) {
-    fHistPt = new TH1F("fHistPt","This is the Pt distribution",15,0.1,3.1);
-    fHistPt->SetStats(kTRUE);
-    fHistPt->GetXaxis()->SetTitle("P_{T} [GeV]");
-    fHistPt->GetYaxis()->SetTitle("#frac{dN}{dP_{T}}");
-    fHistPt->GetXaxis()->SetTitleColor(1);
-    fHistPt->SetMarkerStyle(kFullCircle);
-  }
 }
 
 //________________________________________________________________________
 void AliAnalysisTaskFemto::Exec(Option_t *) {
-  // Task making a pt distribution.
-  // Get input data
-  TChain *chain = (TChain*)GetInputData(0);
-  Long64_t ientry = chain->GetReadEntry();
-  if (!fESD) return;
-  //  if (!chain->GetCurrentFile())
-    //    return 0;
-    
-  printf("Tracks: %d \n",fESD->GetNumberOfTracks());
+  // Task making a femtoscopic analysis.
+  if (fAnalysisType==1) {
+    if (!fESD) {
+      Printf("ERROR: fESD not available");
+      return;
+    }
+
+    printf("Tracks in ESD: %d \n",fESD->GetNumberOfTracks());
   
-  if (fESD->GetNumberOfTracks() > 0) {
-    cout << "Chain has " << chain->GetListOfFriends() << " friends" << endl;
-    cout << "First track is " << fESD->GetTrack(0)->Get1P() << " momentum" << endl;
+    if (fESD->GetNumberOfTracks() >= 0) {
     
-    fReader->SetESDSource(fESD);
-    //    fReader->SetESDfriendSource(*fESDfriend);
-    
-    fManager->ProcessEvent();
+      if (!fReaderESD) {
+	printf("ERROR: No ESD reader for ESD analysis !\n");
+      }
+      else {
+	fReaderESD->SetESDSource(fESD);
+	fManager->ProcessEvent();
+      }
+    } 
+    fOutputList = fManager->Analysis(0)->GetOutputList();
+    PostData(0, fOutputList);
+  }
   
-    // Post final data. It will be written to a file with option "RECREATE"
-    PostData(0, fHistPt);
+  if (fAnalysisType==2) {    
+    if (!fAOD) {
+      Printf("ERROR: fAOD not available");
+      return;
+    }
+    printf("Tracks in AOD: %d \n",fAOD->GetNumberOfTracks());
+    
+    if (fAOD->GetNumberOfTracks() > 0) {
+      Double_t pxyz[3];
+      
+      if (!fReaderAOD) {
+	printf("ERROR: No AOD reader for AOD analysis! \n");
+      }
+      else {
+	fReaderAOD->SetAODSource(fAOD);
+	fManager->ProcessEvent();
+      }
+    } 
+    fOutputList = fManager->Analysis(0)->GetOutputList();
+    PostData(0, fOutputList);
   }
 }      
 
 //________________________________________________________________________
 void AliAnalysisTaskFemto::Terminate(Option_t *) {
-  // Draw some histogram at the end.
-//   if (!gROOT->IsBatch()) {
-//     TCanvas *c1 = new TCanvas("c1","Pt",10,10,310,310);
-//     c1->SetFillColor(10);
-//     c1->SetHighLightColor(10);
-    
-//     c1->cd(1)->SetLeftMargin(0.15);
-//     c1->cd(1)->SetBottomMargin(0.15);  
-//     c1->cd(1)->SetLogy();
-//     //fHistPt = (TH1F*)GetOutputData(0);
-//     if (fHistPt) fHistPt->DrawCopy("E");
-//   }
-  
+  // Do the final processing
 }
 //________________________________________________________________________
-void AliAnalysisTaskFemto::SetFemtoReader(AliFemtoEventReaderESDChain *aReader)
+void AliAnalysisTaskFemto::SetFemtoReaderESD(AliFemtoEventReaderESDChain *aReader)
 {
-  fReader = aReader;
+  fReaderESD = aReader;
+}
+//________________________________________________________________________
+void AliAnalysisTaskFemto::SetFemtoReaderAOD(AliFemtoEventReaderAODChain *aReader)
+{
+  fReaderAOD = aReader;
 }
 //________________________________________________________________________
 void AliAnalysisTaskFemto::SetFemtoManager(AliFemtoManager *aManager)
 {
   fManager = aManager;
 }
-//________________________________________________________________________
-// void AliAnalysisTaskFemto::SetFriendAddress(AliESDfriend **aFriendAddress)
-// {
-//   fESDfriend = aFriendAddress;
-// }
 
