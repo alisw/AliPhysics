dfa32829bd2624deae56fb2557e652b194a574ec
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-03 17:11:40 +0000

    Macros for raw-data reco and QA at P2

---
+++ test/cosmic/rawqa.C
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <fstream>
+
+#include <TAlienCollection.h>
+#include <TFile.h>
+#include <TGrid.h>
+#include <TGridResult.h>
+#include <TROOT.h>
+#include <TString.h>
+#include <TSystem.h>
+
+#include "AliCDBManager.h"
+#include "AliLog.h"
+#include "AliQA.h"
+#include "AliQADataMakerSteer.h"
+#include "AliRawReader.h"
+#include "AliRawReaderRoot.h"
+#include "AliDAQ.h"
+
+TString ClassName() { return "rawqa" ; } 
+
+//________________________________qa______________________________________
+void rawqa(const Int_t runNumber, const char* year = "08") 
+{	
+
+	AliLog::SetGlobalDebugLevel(0) ; 
+	// connect to the grid 
+	TGrid * grid = 0x0 ; 
+       	grid = TGrid::Connect("alien://") ; 
+		
+	Bool_t detIn[AliDAQ::kNDetectors] = {kFALSE} ;
+	char * detNameOff[AliDAQ::kNDetectors] = {"ITS", "ITS", "ITS", "TPC", "TRD", "TOF", "HMPID", "PHOS", "PHOS", "PMD", "MUON", "MUON", "FMD", "T0", "VZERO", "ZDC", "ACORDE", "TRG", "EMCAL", "DAQ_TEST", "HLT"} ; 
+	// make the file name pattern year and run number
+	TString pattern;
+	pattern.Form("%9d",runNumber);
+	pattern.ReplaceAll(" ", "0") ; 
+	pattern.Prepend(year);
+	pattern.Append("*0.root");
+
+	// find the files associated to this run
+	TGridResult * result = 0x0 ; 
+	const UInt_t kMaxFiles = 10 ; 
+	Bool_t local = kFALSE ; 
+	if (grid) { // get the list of files from AliEn directly 
+	  TString baseDir; 
+	  baseDir.Form("/alice/data/20%s/",year);
+	  result = grid->Query(baseDir, pattern) ;  
+	} else {
+	  TString collectionFile(pattern) ; 
+	  collectionFile.Append(".xml") ; 
+	  if ( gSystem->AccessPathName(collectionFile) == 0 ) { // get the list of files from an a-priori created collection file
+	    TAlienCollection collection(collectionFile.Data(), kMaxFiles) ; 
+	    result = collection.GetGridResult("", 0, 0); 
+	  } else { // get the list of files from the local current directory 
+	    local = kTRUE ; 
+	    char line[100] ; 
+	    sprintf(line, ".! ls %s*.root > tempo.txt", pattern.Data()) ; 
+	    gROOT->ProcessLine(line) ;
+	  }
+	}
+	AliLog::Flush();
+	ifstream in ; 
+	if (local) 
+		in.open("tempo.txt", ifstream::in) ; 
+
+	AliCDBManager* man = AliCDBManager::Instance();
+	man->SetDefaultStorage("alien://folder=/alice/data/2008/LHC08a/OCDB/"); // AliQA::GetQARefDefaultStorage()
+	AliQA::SetQARefStorage("local://../TestCDB") ; 
+	man->SetSpecificStorage(Form("%s/*", AliQA::GetQAOCDBDirName()), AliQA::GetQARefStorage());
+	AliQADataMakerSteer qas ; 
+	TString detectors  = ""; 
+	TString detectorsW = ""; 
+	UShort_t file = 0 ; 
+	for ( file = 0 ; file < kMaxFiles ; file++) {
+		TString fileName ; 
+		if ( local) {
+			in >> fileName ;
+		} 
+		else 
+			fileName = result->GetKey(file, "turl");
+	       	if ( fileName == "" )  
+		        break ;
+		if ( fileName.Contains("tag") )
+			continue; 
+		char input[200] ; 
+		if (local) 
+			sprintf(input, "%s", fileName.Data()) ; 
+		else 
+			sprintf(input, "%s", result->GetKey(file, "turl")); 
+		AliInfo(Form("Proccessing file # %d --> %s", file, input)) ;
+		AliLog::Flush();
+		// check which detectors are present 
+		AliRawReader * rawReader = new AliRawReaderRoot(input);
+		rawReader->NextEvent() ; 
+		man->SetRun(rawReader->GetRunNumber());
+		UChar_t * data ; 
+		while (rawReader->ReadNextData(data)) {
+			Int_t detID = rawReader->GetDetectorID();
+			if (detID < 0 || detID >= AliDAQ::kNDetectors) {
+			  AliError("Wrong detector ID! Skipping payload...");
+			  continue;
+			}
+			detIn[detID] = kTRUE ; 
+		}
+		for (Int_t detID = 0; detID < AliDAQ::kNDetectors ; detID++) {
+			if (detIn[detID]) {
+				if ( ! detectors.Contains(detNameOff[detID]) ) {
+					detectors.Append(detNameOff[detID]) ;
+					detectors.Append(" ") ;
+				}
+			}
+		}
+		// TEMPORARY REMOVAL OF TRD!!!
+		detectors.ReplaceAll("TRD","");
+		// TEMPORARY REMOVAL OF TRD!!!
+		if ( !detectors.IsNull() ) {
+		  detectorsW = qas.Run(detectors, rawReader) ;
+		  qas.Reset() ;
+		} else {
+		  AliError("No valid detectors found") ; 
+		} 
+		delete rawReader;
+	}
+	AliLog::Flush();
+	qas.Merge(runNumber) ; 
+	
+	AliLog::Flush();
+	// The summary 
+	AliInfo(Form("\n\n********** Summary for run %d **********", runNumber)) ; 
+	printf("     detectors present in the run        : %s\n", detectors.Data()) ; 
+	printf("     detectors present in the run with QA: %s\n", detectorsW.Data()) ; 
+	printf("     number of files processed           : %d\n", file) ; 
+	TFile * qaResult = TFile::Open(AliQA::GetQAResultFileName()) ; 
+	AliQA * qa = dynamic_cast<AliQA *>(qaResult->Get("QA")) ; 
+	for (Int_t index = 0 ; index < AliQA::kNDET ; index++)
+		if (detectorsW.Contains(AliQA::GetDetName(AliQA::DETECTORINDEX(index)))) 
+			qa->ShowStatus(AliQA::DETECTORINDEX(index)) ;
+}
---
+++ test/cosmic/rec.C
@@ -0,0 +1,166 @@
+void rec(Int_t runNumber = 0, const char* year = "08", const char *localFileName = NULL)
+{
+  // Offline shifter reconstruction macro
+
+  cout << "Going to run the reconstruction for run: " << runNumber << endl;
+
+  // connect to the grid 
+  TGrid * grid = 0x0 ; 
+  grid = TGrid::Connect("alien://") ; 
+		
+  // make the file name pattern year and run number
+  TString pattern;
+  pattern.Form("%9d",runNumber);
+  pattern.ReplaceAll(" ", "0") ; 
+  pattern.Prepend(year);
+  pattern.Append("*0.root");
+
+  // find the files associated to this run
+  // get the list of files from AliEn directly 
+  TString baseDir; 
+  baseDir.Form("/alice/data/20%s/",year);
+
+  cout << "Looking for raw-data files with pattern " << pattern << " in folder " << baseDir << endl;
+
+  TGridResult *result = grid->Query(baseDir, pattern);
+
+  TList *fileList = result->GetFileInfoList();
+
+  cout << fileList->GetEntries() << " raw-data files found" << endl;
+  if ( fileList->GetEntries() == 0) {
+    cout << "Exiting..." << endl;
+    return;
+  }
+
+  // Take the first (or last?) file...
+  TFileInfo *fi =  (TFileInfo *)fileList->At(0); 
+  //  TFileInfo *fi =  (TFileInfo *)fileList->At(fileList->GetEntries()-1); 
+
+  cout << "Getting the file:" << fi->GetCurrentUrl()->GetUrl() << endl;
+  fi->Dump();
+
+  TString filename(fi->GetCurrentUrl()->GetUrl());
+
+  AliLog::Flush();
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // First version of the reconstruction
+  // script for the FDR'08
+
+  // Set the CDB storage location
+  // AliLog::SetModuleDebugLevel("STEER",2);
+  AliCDBManager * man = AliCDBManager::Instance();
+  //  man->SetDefaultStorage("local://LocalCDB");
+  man->SetDefaultStorage("alien://folder=/alice/data/2008/LHC08a/OCDB/");
+  
+  // Files that we can not read from alien...
+  man->SetSpecificStorage("ITS/Calib/MapsAnodeSDD","local://$ALICE_ROOT");
+  man->SetSpecificStorage("ITS/Calib/MapsTimeSDD","local://$ALICE_ROOT");
+  man->SetSpecificStorage("TPC/Calib/ExB","local://$ALICE_ROOT");
+
+  // Objects not found if using LHC07w database
+  //  man->SetSpecificStorage("ITS/Calib/MapsAnodeSDD","local:///afs/cern.ch/user/c/cheshkov/public/OCDB");
+  // man->SetSpecificStorage("GRP/GRP/Data","local://$ALICE_ROOT");
+  // man->SetSpecificStorage("ITS/Calib/DDLMapSDD","local://$ALICE_ROOT");
+  // man->SetSpecificStorage("MUON/Calib/Mapping","local://$ALICE_ROOT");
+  // man->SetSpecificStorage("MUON/Calib/DDLStore","local://$ALICE_ROOT");
+
+  // ITS settings
+  AliITSRecoParam * itsRecoParam = AliITSRecoParam::GetCosmicTestParam();
+  itsRecoParam->SetClusterErrorsParam(2);
+  itsRecoParam->SetFindV0s(kFALSE);
+  itsRecoParam->SetAddVirtualClustersInDeadZone(kFALSE);
+  itsRecoParam->SetUseAmplitudeInfo(kFALSE);
+  // In case we want to switch off a layer
+  //  itsRecoParam->SetLayerToSkip(<N>);
+  itsRecoParam->SetLayerToSkip(4);
+  itsRecoParam->SetLayerToSkip(5);
+  itsRecoParam->SetLayerToSkip(2);
+  itsRecoParam->SetLayerToSkip(3);
+  AliITSReconstructor::SetRecoParam(itsRecoParam);
+
+  // TPC settings
+  AliLog::SetClassDebugLevel("AliTPCclustererMI",2);
+  AliTPCRecoParam * tpcRecoParam = AliTPCRecoParam::GetCosmicTestParam(kTRUE);
+  tpcRecoParam->SetTimeInterval(60,940);
+  tpcRecoParam->Dump();
+  AliTPCReconstructor::SetRecoParam(tpcRecoParam);
+  AliTPCReconstructor::SetStreamLevel(1);
+
+  // PHOS settings
+  AliPHOSRecoParam* recEmc = new AliPHOSRecoParamEmc();
+  recEmc->SetSubtractPedestals(kTRUE);
+  recEmc->SetMinE(0.05);
+  recEmc->SetClusteringThreshold(0.10);
+  AliPHOSReconstructor::SetRecoParamEmc(recEmc);
+
+  // T0 settings
+  AliLog::SetModuleDebugLevel("T0", 10);
+
+  // MUON settings
+  AliLog::SetClassDebugLevel("AliMUONRawStreamTracker",3);
+  AliMUONRecoParam *muonRecoParam = AliMUONRecoParam::GetLowFluxParam();
+  muonRecoParam->CombineClusterTrackReco(kTRUE);
+  muonRecoParam->SetCalibrationMode("NOGAIN");
+  //muonRecoParam->SetClusteringMode("PEAKFIT");
+  //muonRecoParam->SetClusteringMode("PEAKCOG");
+  muonRecoParam->Print("FULL");
+  AliRecoParam::Instance()->RegisterRecoParam(muonRecoParam);
+ 
+  // Tracking settings
+  //  AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 1., 10., 1);
+  AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 0., 10., 2);
+  AliTracker::SetFieldMap(field,1);
+  Double_t mostProbPt=0.35;
+  AliExternalTrackParam::SetMostProbablePt(mostProbPt);
+
+  // AliReconstruction settings
+  AliReconstruction rec;
+  rec.SetUniformFieldTracking(kFALSE);
+  rec.SetWriteESDfriend(kTRUE);
+  rec.SetWriteAlignmentData();
+  rec.SetInput(filename.Data());
+  rec.SetRunReconstruction("ALL");
+  rec.SetUseTrackingErrorsForAlignment("ITS");
+
+  // In case some detectors have to be switched off...
+  //  rec.SetRunLocalReconstruction("ALL");
+  //  rec.SetRunTracking("ALL");
+  //  rec.SetFillESD("ALL");
+  // Disable vertex finder for the moment
+  rec.SetRunVertexFinder(kFALSE);
+
+  // To be enabled if some equipment IDs are not set correctly by DAQ
+  //  rec.SetEquipmentIdMap("EquipmentIdMap.data");
+
+  // Detector options if any
+  rec.SetOption("ITS","cosmics,onlyITS");
+  rec.SetOption("MUON","SAVEDIGITS");
+  rec.SetOption("TPC","OldRCUFormat");
+  rec.SetOption("PHOS","OldRCUFormat");
+
+  // To be enabled when CTP readout starts
+  rec.SetFillTriggerESD(kFALSE);
+
+  // all events in one single file
+  rec.SetNumberOfEventsPerFile(-1);
+
+  // switch off cleanESD
+  rec.SetCleanESD(kFALSE);
+
+  // rec.SetEventRange(0,15);
+  // AliLog::SetGlobalDebugLevel(2);
+
+  rec.SetRunQA(kFALSE);
+  AliLog::Flush();
+  rec.Run();
+
+  //cout << "-----------------------------------------------------------------" << endl;
+  //cout << "-----------------------------------------------------------------" << endl;
+  //cout << "--------- Reconstruction Completed. Start merging QAs -----------" << endl;
+  //cout << "-----------------------------------------------------------------" << endl;
+  //cout << "-----------------------------------------------------------------" << endl;
+  //AliQADataMakerSteer qas;
+  //qas.Merge();
+}
