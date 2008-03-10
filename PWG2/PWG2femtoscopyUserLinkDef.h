e85aca4986864493c1694055aeac50d2cb2c7015
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-10 09:54:22 +0000

    Adding new classes to compilation

--- PWG2/PWG2femtoscopyUserLinkDef.h
+++ PWG2/PWG2femtoscopyUserLinkDef.h
@@ -30,4 +30,7 @@
 //#pragma link C++ class AliStHbtTrack;
 #pragma link C++ class AliFemtoEventReaderESDKine;
 #pragma link C++ class AliFemtoModelCorrFctnTrueQ;
+#pragma link C++ class AliFemtoModelCorrFctnNonIdDR;
+#pragma link C++ class AliFemtoModelCorrFctn3DSpherical;
+#pragma link C++ class AliFemtoModelCorrFctn3DLCMSSpherical;
 #pragma link C++ class AliFemtoCutMonitorParticleMomRes;
