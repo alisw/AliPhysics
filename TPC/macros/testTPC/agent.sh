441863a8c5ebb4eed5dfc356085b2028c4aa3c4d
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-11 14:24:31 +0000

    Adding extended documentation (Marian)

--- TPC/macros/testTPC/agent.sh
+++ TPC/macros/testTPC/agent.sh
@@ -1,20 +1,38 @@
 #!/usr/local/bin/bash
-
 #aguments
 #1 working directory
+
+
 # Example usage:
 # $ALICE_ROOT/TPC/macros/testTPC/agent.sh /afs/cern.ch/user/m/miranov/public/test2008/reckr191xx
 
+#
+#  AGENT to run some action:
+#  3 components to be present
+#
+
+#  0. CREATE ACTION LIST ( see $ALICE_ROOT/TPC/macros/testTPC/AlienToolkit.cxx)
+#
+
+#  1. WORKING DIRECTORY SETUP (in prev. example /afs/cern.ch/user/m/miranov/public/test2008/reckr191xx)
+#    1.a. action.list
+#    1.b  empty out directory
+#    1.c  macros directory -with  <action>.C  - e.g rec.C  FindKrClustersRaw.C
 
 
-# Copy the script  to setup aliroot root to the  ~/.bagentsetup
-# Copy certificates to default place -  ~/.agentauth/
-# cp /tmp/*$UID* ~/.agentauth/
-# Set envirnment variables needed OCDB_PATH and INPUTTYPE
-# CERN SETUP
+# 2. ENVIRONMENT SETUP:
+#
+# 2.a  Copy the script  to setup aliroot root to the  ~/.bagentsetup
+# 2.b  Copy certificates to default place -  ~/.agentauth/
+#     cp /tmp/*$UID* ~/.agentauth/
+# 2.c  Set envirnment variables needed OCDB_PATH and INPUTTYPE
 
+
+#
+#     CERN SETUP - THIS WILL BE DONE IN USER ENV in future
+#
 export     AGENTINPUTTYPE=2 
-#use  0-XRD  1-ALIEN  2-CASTOR
+#use       0-XRD  1-ALIEN  2-CASTOR
 export     OCDB_PATH=local:///afs/cern.ch/alice/tpctest/OCDB     
 #
 
