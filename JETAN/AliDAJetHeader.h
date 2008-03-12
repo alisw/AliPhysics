0c553e9d66719133aa8cc0b0ffa681e4dc671b39
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-12 20:47:03 +0000

    Parameter for minimum jet energy added (D. Perrino)

--- JETAN/AliDAJetHeader.h
+++ JETAN/AliDAJetHeader.h
@@ -27,6 +27,7 @@ class AliDAJetHeader : public AliJetHeader
 	void ChargedOnly	(Bool_t charged ) { fChgOnly=charged; }
 	void SelectJets		(Bool_t seljets ) { fSelectJets=seljets; }
 	void SetNclust		(Int_t ncl      ) { fNclustMax=ncl ; fFixedCl=kTRUE; }
+	void SetEtMin		(Float_t etmin  ) { fEtMin =etmin;  }
 
 	Char_t*  GetDirectory()	const { return fDirectory; }
 	Char_t*  GetFileOut()	const { return fFileOut; }
@@ -37,19 +38,21 @@ class AliDAJetHeader : public AliJetHeader
 	Bool_t   GetSelJets()	const { return fSelectJets; }
 	Int_t    GetNclustMax() const { return fNclustMax; }
 	Bool_t   GetFixedCl()	const { return fFixedCl; }
+	Float_t  GetEtMin()		const { return fEtMin;   }
 
   protected:
 	AliDAJetHeader(const AliDAJetHeader &jh);
 	AliDAJetHeader& operator=(const AliDAJetHeader &jh);
 	Char_t	   *fDirectory;                                 //directory name 
 	Char_t	   *fFileOut;                                   //output file name 
-	Bool_t		fPytOnly;                               // 
-	Double_t	fPtCut;					//cut on transverse momentum
-	Double_t	fEtaCut;				//cut on absolute eta
-	Bool_t		fChgOnly;				//flag on charged particles
-	Bool_t		fSelectJets;			        //choose if to select jets between clusters
-	Int_t		fNclustMax;				//number of clusters when to stop annealing
-	Bool_t		fFixedCl;                               //  
+	Bool_t		fPytOnly;						//use only data from PYTHIA
+	Double_t	fPtCut;							//cut on transverse momentum
+	Double_t	fEtaCut;						//cut on absolute eta
+	Bool_t		fChgOnly;						//flag on charged particles
+	Bool_t		fSelectJets;					//select jets among clusters
+	Int_t		fNclustMax;						//number of clusters when to stop annealing
+	Bool_t		fFixedCl;						//use a fixed fNclustMax
+	Float_t		fEtMin;							//minimum energy for found jets
 
 	ClassDef(AliDAJetHeader,1)
 };
