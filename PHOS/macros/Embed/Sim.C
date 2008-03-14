252f9e4156ed048ae73e392ebf7c36d0b7da523e
prsnko <prsnko@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-14 18:24:40 +0000

    Add example of mixing with RAW

--- PHOS/macros/Embed/Sim.C
+++ PHOS/macros/Embed/Sim.C
@@ -5,6 +5,8 @@ void Sim(const Int_t nevents=10,
   //with previouly reconstructed Digits from 
   //raw event. We assume they are in dir "Bg"
 
+  AliPHOSSimParam * phosSimParam = AliPHOSSimParam::GetInstance() ;
+
   AliLog::SetGlobalDebugLevel(debLevel);
   AliSimulation sim;
   sim.SetMakeSDigits("PHOS") ;
@@ -14,6 +16,8 @@ void Sim(const Int_t nevents=10,
   //Set path to reconstricted raw digits
   //and set number of simulated events per one raw
   sim.MergeWith("Bg/galice.root",1) ;
+  phosSimParam->SetStreamDigits(1) ; //Mark this stream as digits and not SDigits
+
   sim.Run(nevents);
 
 
