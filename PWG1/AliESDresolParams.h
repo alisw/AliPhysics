de8c23fd8990a2437254ea015ff3b006cdc8b6e6
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-16 12:31:56 +0000

    Adding track paramater resolution parameterization TO BE PART OF STEERbase in future (~ 2 weeks)
    (Marian Ivanov)

---
+++ PWG1/AliESDresolParams.h
@@ -0,0 +1,53 @@
+#ifndef ALIESDRESOLPARAMS_H
+#define ALIESDRESOLPARAMS_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+//-------------------------------------------------------------------------
+//   ESD tracks and V0 resolution  parameterization
+//
+//    Origin: Marian Ivanov marian.ivanov@cern.ch
+//-------------------------------------------------------------------------
+
+#include "TObject.h"
+#include <TVectorD.h>
+class TTree;
+
+class AliESDresolParams : public TObject{
+ public:
+  AliESDresolParams();
+  void SetInstance(AliESDresolParams *param){fgInstance = param;}
+  //
+  Double_t GetResolPrim(Int_t param, Float_t onept, Float_t tanth);
+  Double_t GetResolR(Int_t param, Float_t onept, Float_t radius);
+  static Double_t  SGetResolPrim(Int_t sdim, Float_t onept, Float_t tanth){ return fgInstance->GetResolPrim(sdim,onept,tanth);}
+  static Double_t  SGetResolR(Int_t param, Float_t onept, Float_t radius){ return fgInstance->GetResolR(param,onept,radius);}
+
+  void MakeParamPrimFast(TTree * tree, Float_t fraction=-1, Int_t entries=100000);
+  void MakeParamRFast(TTree * tree, Float_t fraction=-1, Int_t entries=100000);
+  // protected:
+ public:
+  //Resolution at prim vertex
+  TVectorD *fResolDCAyy;            // resolution Y parameterization
+  TVectorD *fResolDCAzz;            // resolution Z parameterization 
+  TVectorD *fResolDCAphi;           // resolution phi parameterization - pt-theta
+  TVectorD *fResolDCAth;            // resolution theta parameterization -pt-theta
+  TVectorD *fResolDCA1pt;           // resolution 1/pt parameterization - pt-theta
+  //
+  // Resolution at V0 - radial dependent
+  //
+  TVectorD *fResolCyy;              // resolution Y parameterization - r-pt
+  TVectorD *fResolCzz;              // resolution Z parameterization - r-pt
+  TVectorD *fResolCphi;             // resolution phi parameterization - r-pt
+  TVectorD *fResolCth;              // resolution theta parameterization - r-pt
+  TVectorD *fResolC1pt;             // resolution 1/pt parameterization - r-pt
+  
+  // 
+  static AliESDresolParams*   fgInstance; //! Instance of this class (singleton implementation)
+  ClassDef(AliESDresolParams,1)      // ESD resolution parametereization
+};
+
+
+
+#endif
