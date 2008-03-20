c165372f1878679e184ca0974c289c08f28d30f6
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-20 10:26:21 +0000

    EffC++ warnings corrected.

--- TDPMjet/AliIonPDGCodes.cxx
+++ TDPMjet/AliIonPDGCodes.cxx
@@ -28,15 +28,16 @@
 ClassImp(AliIonPDGCodes)
  
 //______________________________________________________________________________
-AliIonPDGCodes::AliIonPDGCodes()
+    AliIonPDGCodes::AliIonPDGCodes():
+	TObject(),
+	fNIon(200)
 { 
-  fNIon = 200;
-  for(Int_t i=0; i<fNIon; i++) fPDGCode[i]=0;
-
+    for(Int_t i=0; i<fNIon; i++) fPDGCode[i]=0;
 }
 
 AliIonPDGCodes::AliIonPDGCodes(const AliIonPDGCodes &/*PDGCodes*/)
-    :TObject()
+    :TObject(),
+     fNIon(200)
 {
 }
 
