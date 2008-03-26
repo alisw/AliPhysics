eb047f462e447ee1715b6aa13e6927de963feb9b
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-26 11:50:06 +0000

    This is the pedestal detector algorithm for the FMD. It produces csv files that are the input for the AliFMDPreprocessor.

---
+++ FMD/AliFMDPedestalDA.h
@@ -0,0 +1,41 @@
+#ifndef ALIFMDPEDESTALDA_H
+#define ALIFMDPEDESTALDA_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights
+ * reserved. 
+ *
+ * See cxx source for full Copyright notice                               
+ */
+// This class implements the pedestal detector algorithm (DA) for the FMD.
+// It uses 51200 TH1S histograms to store the data for each channel of the FMD.
+// The mean and standard deviation of a histogram define the pedestal and 
+// the noise for that channel.
+
+
+#include "AliFMDBaseDA.h"
+#include "TH1.h"
+#include "TObjArray.h"
+
+class AliFMDPedestalDA: public AliFMDBaseDA {
+  
+ public:
+  AliFMDPedestalDA() ;
+  AliFMDPedestalDA(const AliFMDPedestalDA & pedDA) ;
+  AliFMDPedestalDA& operator = (const AliFMDPedestalDA & pedDA) ; 
+  virtual ~AliFMDPedestalDA();
+  void Init();
+ 
+ protected:
+ 
+  void AddChannelContainer(TObjArray* sectorArray, UShort_t det, Char_t ring, UShort_t sec, UShort_t strip);
+  void FillChannels(AliFMDDigit* digit);
+  void Analyse(UShort_t det, Char_t ring, UShort_t sec, UShort_t strip);
+  void WriteHeaderToFile();
+  void FinishEvent() {}
+ private:
+  TH1S* GetChannel(UShort_t det, Char_t ring, UShort_t sec, UShort_t strip);
+    
+  ClassDef(AliFMDPedestalDA,0)
+
+};
+#endif
