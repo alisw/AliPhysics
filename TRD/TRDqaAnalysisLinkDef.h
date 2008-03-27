56343095d3129a98762be19968dfba0cba80f58f
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-03-27 12:36:42 +0000

    Add QA analysis classes

---
+++ TRD/TRDqaAnalysisLinkDef.h
@@ -0,0 +1,16 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id: TRDrecLinkDef.h 24196 2008-02-28 11:53:37Z cblume $ */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  AliTRDqaElectronSpectra+;
+#pragma link C++ class  AliTRDqaESDFriends+;
+#pragma link C++ class  AliTRDqaEnergyDeposit+;
+#pragma link C++ class  AliTRDqaAT+;
+
+#endif
