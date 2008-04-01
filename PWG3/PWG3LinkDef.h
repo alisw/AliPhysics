847d68b622b46fa558ddade13a1b04f9cf9a24b9
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-01 09:20:56 +0000

    Move AliD0toKpi* and AliBtoJPSI* from libPWG3base to libPWG3 (Andrea)

--- PWG3/PWG3LinkDef.h
+++ PWG3/PWG3LinkDef.h
@@ -4,6 +4,10 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class AliD0toKpi+;
+#pragma link C++ class AliD0toKpiAnalysis+;
+#pragma link C++ class AliBtoJPSItoEle+;
+#pragma link C++ class AliBtoJPSItoEleAnalysis+;
 
 
 #endif
