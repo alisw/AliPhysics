fb609760cbf2c14e8448384a5a50758b22068d4b
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-03 18:24:31 +0000

    Updated settings (Andrea)

--- PWG3/ConfigVertexingHF.C
+++ PWG3/ConfigVertexingHF.C
@@ -3,17 +3,39 @@ AliAnalysisVertexingHF* ConfigVertexingHF() {
   printf("Call to AliAnalysisVertexingHF parameters setting :\n");
   vHF = new AliAnalysisVertexingHF();
  
+  vHF->SetUseTRef(); // only if you write on the AOD
+  //--- switch-off candidates finding (default: all on)
+  //vHF->SetD0toKpiOff();
   //vHF->SetJPSItoEleOff();
-  vHF->Set3ProngOff();
+  //vHF->Set3ProngOff();
   vHF->Set4ProngOff();
+  //--- secondary vertex with KF?
+  //vHF->SetSecVtxWithKF();
+  //--- set cuts for single-track selection
   vHF->SetITSrefitRequired();
   vHF->SetBothSPDNotRequired();
   vHF->SetMinITSCls(5);
-  vHF->SetMinPtCut(0.5);
+  vHF->SetMinPtCut(0.3);
   vHF->SetMind0Cut(0.);
-  vHF->SetD0toKpiCuts(0.2,999999.,1.1,0.,0.,999999.,999999.,999999.,0.3);
-  vHF->SetBtoJPSICuts(0.350);
+  //--- set cuts for candidates selection
+  vHF->SetD0toKpiCuts(0.6,999999.,1.1,0.,0.,999999.,999999.,999999.,-1);
+  vHF->SetBtoJPSICuts(0.6);
   vHF->SetDplusCuts(0.2,0.,0.,0.,0.,0.01,0.06,0.,0.,0.8);
+  vHF->SetDsCuts(0.2);
+  vHF->SetLcCuts(0.2);
+  //--- set this if you want to reconstruct primary vertex candidate by
+  //    candidate using other tracks in the event (for pp, broad 
+  //    interaction region)
+  //vHF->SetRecoPrimVtxSkippingTrks();
+  //--- OR set this if you want to remove the candidate daughters from 
+  //    the primary vertex, without recostructing it from scratch
+  //vHF->SetRmTrksFromPrimVtx();
+
+  //--- check the settings
+  vHF->PrintStatus();
+  //--- verbose
+  vHF->SetDebug(1);
+
  
   return vHF;
 }
