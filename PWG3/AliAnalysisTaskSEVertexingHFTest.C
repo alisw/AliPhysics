6932f9a954ba00a66ef1ed2dcfa15ded1a7d89ac
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-03 18:19:34 +0000

    New AliAnalysisTaskSE for vertexing HF (Andrea)

---
+++ PWG3/AliAnalysisTaskSEVertexingHFTest.C
@@ -0,0 +1,90 @@
+void AliAnalysisTaskSEVertexingHFTest()
+{
+  //
+  // Test macro for the AliAnalysisTaskSE for heavy-flavour vertexing
+  // A.Dainese, andrea.dainese@lnl.infn.it
+  //
+
+  gSystem->Load("libTree.so");
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libPhysics.so");
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libSTEERBase.so");
+  gSystem->Load("libESD.so");
+  gSystem->Load("libAOD.so"); 
+  gSystem->Load("libANALYSIS.so");
+  gSystem->Load("libANALYSISalice.so");
+  gSystem->Load("libPWG3base.so");
+
+
+  // Local files 
+  TChain *chain = new TChain("esdTree");
+  chain->Add("./AliESDs.root");
+
+  // or:
+  /*
+  //Fetch files with AliEn :
+  const char *collectionfile = "CollectionTags.xml";
+  TGrid::Connect("alien://") ;
+  //Create an AliRunTagCuts and an AliEventTagCuts Object and impose some selection criteria
+  AliRunTagCuts      *runCuts   = new AliRunTagCuts();
+  AliEventTagCuts    *eventCuts = new AliEventTagCuts();
+  AliLHCTagCuts      *lhcCuts   = new AliLHCTagCuts();
+  AliDetectorTagCuts *detCuts   = new AliDetectorTagCuts();
+  eventCuts->SetMultiplicityRange(0,20000);
+  //Create an AliTagAnalysis Object and chain the tags
+  AliTagAnalysis   *tagAna = new AliTagAnalysis();
+  tagAna->SetType("ESD");
+  TAlienCollection *coll   = TAlienCollection::Open(collectionfile);
+  TGridResult      *tagResult = coll->GetGridResult("",0,0);
+  tagResult->Print();
+  tagAna->ChainGridTags(tagResult);
+  //Create a new esd chain and assign the chain that is returned by querying the tags
+  TChain* chain = tagAna->QueryTags(runCuts,lhcCuts,detCuts,eventCuts);
+  */
+
+
+  // Create the analysis manager
+  AliAnalysisManager *mgr  = new AliAnalysisManager("My Manager","My Manager");
+  mgr->SetDebugLevel(10);
+  
+  // Input Handler
+  //AliAODInputHandler *inputHandler = new AliAODInputHandler();
+  AliESDInputHandler *inputHandler = new AliESDInputHandler();
+  inputHandler->SetInactiveBranches("FMD CaloCluster");
+  mgr->SetInputEventHandler(inputHandler);
+  
+  // Output 
+  AliAODHandler *aodHandler = new AliAODHandler();
+  aodHandler->SetOutputFileName("AliAOD.VertexingHF.root");
+  aodHandler->SetCreateNonStandardAOD();
+  mgr->SetOutputEventHandler(aodHandler);
+  
+  // Vertexing analysis task    
+  AliAnalysisTaskSEVertexingHF *hfTask = new AliAnalysisTaskSEVertexingHF("VertexingHFAnalysis");
+  hfTask->SetDebugLevel(2);
+  
+  mgr->AddTask(hfTask);
+  
+  //
+  // Create containers for input/output
+  AliAnalysisDataContainer *cinput1 = mgr->CreateContainer("cchain",TChain::Class(), 
+							   AliAnalysisManager::kInputContainer);
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("tree", TTree::Class(),
+							    AliAnalysisManager::kOutputContainer, 
+							    "default");
+  mgr->ConnectInput(hfTask,0,cinput1);
+  mgr->ConnectOutput(hfTask,0,coutput1);
+
+  //
+  // Run the analysis
+  //    
+  printf("CHAIN HAS %d ENTRIES\n",(Int_t)chain->GetEntries());
+  if(mgr->InitAnalysis()) {
+    mgr->PrintStatus();
+    mgr->StartAnalysis("local",chain);
+    //mgr->StartAnalysis("grid",chain);
+  }
+
+  return;
+}
