81cf38f6011626bb32c4b4f86a2343a630dee113
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-04 13:43:03 +0000

    Trigger Lut generation analysis task and how to (Bogdan)

---
+++ PWG3/analysisTaskLUTNT.C
@@ -0,0 +1,55 @@
+//________________________________________________________________________
+void analysisTaskLUTNT() {
+
+  TChain* chain = new TChain("esdTree");
+
+  TAlienCollection* coll = TAlienCollection::Open("wn.xml");
+
+  TGridResult* result = coll->GetGridResult("",0,0);
+  Int_t nFiles = 0;
+  for(Int_t i = 0; i < result->GetEntries(); i++) {
+    printf("TURL = %s \n",result->GetKey(i,"turl"));
+    chain->Add(result->GetKey(i,"turl"));
+    nFiles++;
+    //if (nFiles == 10) break;
+  }
+
+  //____________________________________________//
+  // Make the analysis manager
+  AliAnalysisManager *mgr = new AliAnalysisManager("AnalysisManager");
+
+  //____________________________________________//
+  // ntuple task
+  AliAnalysisTaskLUT *task = new AliAnalysisTaskLUT("TaskLUT");
+  mgr->AddTask(task);
+
+  // Create containers for input/output
+
+  // input
+  AliAnalysisDataContainer *cinput = mgr->CreateContainer("cchain",TChain::Class(),AliAnalysisManager::kInputContainer);
+
+  Char_t text[256];
+  sprintf(text,"Ntuple.LUT.root");
+  printf("Analysis output in %s \n",text);
+
+  // output
+  AliAnalysisDataContainer *coutput = mgr->CreateContainer("cntuple", TNtuple::Class(),AliAnalysisManager::kOutputContainer,text);
+
+  //____________________________________________//
+  mgr->ConnectInput(task,0,cinput);
+  mgr->ConnectOutput(task,0,coutput);
+
+  if (mgr->InitAnalysis()) {
+    mgr->PrintStatus();
+
+    TStopwatch timer;
+    timer.Start();
+
+    mgr->StartAnalysis("local",chain);
+
+    timer.Stop();
+    timer.Print();
+
+  }
+}                         
+                      
