e744de97594d8c1cef0c1a7f191b556663b363c6
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-06 21:48:52 +0000

    TPC scaled resolution (Jacek Otwinowski)

--- PWG1/AliComparisonRes.cxx
+++ PWG1/AliComparisonRes.cxx
@@ -216,84 +216,84 @@ void AliComparisonRes::InitHisto(){
   // Parametrisation histograms
   // 
 
-  f1Pt2Resol1PtTPC = new TH2F("f1Pt2Resol1PtTPC","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.020,0.020);  
+  f1Pt2Resol1PtTPC = new TH2F("f1Pt2Resol1PtTPC","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.010,0.010);  
   f1Pt2Resol1PtTPC->SetXTitle("1/mcp_{t}");
   f1Pt2Resol1PtTPC->SetYTitle("(1/mcp_{t}-1/p_{t})/(1+1/mcp_{t})^2)");
 
-  f1Pt2Resol1PtTPCITS = new TH2F("f1Pt2Resol1PtTPCITS","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.020,0.020);  
+  f1Pt2Resol1PtTPCITS = new TH2F("f1Pt2Resol1PtTPCITS","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.010,0.010);  
   f1Pt2Resol1PtTPCITS->SetXTitle("1/mcp_{t}");
   f1Pt2Resol1PtTPCITS->SetYTitle("(1/mcp_{t}-1/p_{t})/(1+1/mcp_{t})^2)");
 
-  fYResol1PtTPC = new TH2F("fYResol1PtTPC","fYResol1PtTPC",100, 0,10,200,-0.010,0.010);   
+  fYResol1PtTPC = new TH2F("fYResol1PtTPC","fYResol1PtTPC",100, 0,10,200,-1.0,1.0);   
   fYResol1PtTPC->SetXTitle("1/mcpt");
   fYResol1PtTPC->SetYTitle("#DeltaY");
 
-  fYResol1PtTPCITS = new TH2F("fYResol1PtTPCITS","fYResol1PtTPCITS",100, 0,10,200,-0.010,0.010);   
+  fYResol1PtTPCITS = new TH2F("fYResol1PtTPCITS","fYResol1PtTPCITS",100, 0,10,200,-0.05,0.05);   
   fYResol1PtTPCITS->SetXTitle("1/mcpt");
   fYResol1PtTPCITS->SetYTitle("#DeltaY");
 
-  fZResol1PtTPC = new TH2F("fZResol1PtTPC","fZResol1PtTPC",50, 0,10,200,-0.020,0.020);   
+  fZResol1PtTPC = new TH2F("fZResol1PtTPC","fZResol1PtTPC",100, 0,10,200,-1.0,1.0);   
   fZResol1PtTPC->SetXTitle("1/mcpt");
   fZResol1PtTPC->SetYTitle("#DeltaZ");
 
-  fZResol1PtTPCITS = new TH2F("fZResol1PtTPCITS","fZResol1PtTPCITS",50, 0,10,200,-0.020,0.020);   
+  fZResol1PtTPCITS = new TH2F("fZResol1PtTPCITS","fZResol1PtTPCITS",100, 0,10,200,-0.05,0.05);   
   fZResol1PtTPCITS->SetXTitle("1/mcpt");
   fZResol1PtTPCITS->SetYTitle("#DeltaZ");
 
-  fPhiResol1PtTPC = new TH2F("fPhiResol1PtTPC","fPhiResol1PtTPC",50, 0,10,200,-0.005,0.005);   
+  fPhiResol1PtTPC = new TH2F("fPhiResol1PtTPC","fPhiResol1PtTPC",100, 0,10,200,-0.025,0.025);   
   fPhiResol1PtTPC->SetXTitle("1/mcpt");
   fPhiResol1PtTPC->SetYTitle("#Delta#phi");
 
-  fPhiResol1PtTPCITS = new TH2F("fPhiResol1PtTPCITS","fPhiResol1PtTPCITS",50, 0,10,200,-0.005,0.005);   
+  fPhiResol1PtTPCITS = new TH2F("fPhiResol1PtTPCITS","fPhiResol1PtTPCITS",100, 0,10,200,-0.01,0.01);   
   fPhiResol1PtTPCITS->SetXTitle("1/mcpt");
   fPhiResol1PtTPCITS->SetYTitle("#Delta#phi");
 
-  fThetaResol1PtTPC = new TH2F("fThetaResol1PtTPC","fThetaResol1PtTPC",50, 0,10,200,-0.005,0.005);   
+  fThetaResol1PtTPC = new TH2F("fThetaResol1PtTPC","fThetaResol1PtTPC",100, 0,10,200,-0.025,0.025);   
   fThetaResol1PtTPC->SetXTitle("1/mcpt");
   fThetaResol1PtTPC->SetYTitle("#Delta#theta");
 
-  fThetaResol1PtTPCITS = new TH2F("fThetaResol1PtTPCITS","fThetaResol1PtTPCITS",50, 0,10,200,-0.005,0.005);   
+  fThetaResol1PtTPCITS = new TH2F("fThetaResol1PtTPCITS","fThetaResol1PtTPCITS",100, 0,10,200,-0.01,0.01);   
   fThetaResol1PtTPCITS->SetXTitle("1/mcpt");
   fThetaResol1PtTPCITS->SetYTitle("#Delta#theta");
   
   // constrained
-  fC1Pt2Resol1PtTPC = new TH2F("fC1Pt2Resol1PtTPC","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.020,0.020);  
+  fC1Pt2Resol1PtTPC = new TH2F("fC1Pt2Resol1PtTPC","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.010,0.010);  
   fC1Pt2Resol1PtTPC->SetXTitle("1/mcp_{t}");
   fC1Pt2Resol1PtTPC->SetYTitle("(1/mcp_{t}-1/p_{t})/(1+1/mcp_{t})^2)");
 
-  fC1Pt2Resol1PtTPCITS = new TH2F("fC1Pt2Resol1PtTPCITS","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.020,0.020);  
+  fC1Pt2Resol1PtTPCITS = new TH2F("fC1Pt2Resol1PtTPCITS","(1/mcpt-1/pt)/(1+1/mcpt)^2 vs 1/pt)",100,0,10,200,-0.010,0.010);  
   fC1Pt2Resol1PtTPCITS->SetXTitle("1/mcp_{t}");
   fC1Pt2Resol1PtTPCITS->SetYTitle("(1/mcp_{t}-1/p_{t})/(1+1/mcp_{t})^2)");
 
-  fCYResol1PtTPC = new TH2F("fCYResol1PtTPC","fCYResol1PtTPC",100, 0,10,200,-0.010,0.010);   
+  fCYResol1PtTPC = new TH2F("fCYResol1PtTPC","fCYResol1PtTPC",100, 0,10,200,-1.0,1.0);   
   fCYResol1PtTPC->SetXTitle("1/mcpt");
   fCYResol1PtTPC->SetYTitle("#DeltaY");
 
-  fCYResol1PtTPCITS = new TH2F("fCYResol1PtTPCITS","fCYResol1PtTPCITS",100, 0,10,200,-0.010,0.010);   
+  fCYResol1PtTPCITS = new TH2F("fCYResol1PtTPCITS","fCYResol1PtTPCITS",100, 0,10,200,-0.05,0.05);   
   fCYResol1PtTPCITS->SetXTitle("1/mcpt");
   fCYResol1PtTPCITS->SetYTitle("#DeltaY");
 
-  fCZResol1PtTPC = new TH2F("fCZResol1PtTPC","fCZResol1PtTPC",50, 0,10,200,-0.020,0.020);   
+  fCZResol1PtTPC = new TH2F("fCZResol1PtTPC","fCZResol1PtTPC",100, 0,10,200,-1.0,1.0);   
   fCZResol1PtTPC->SetXTitle("1/mcpt");
   fCZResol1PtTPC->SetYTitle("#DeltaZ");
 
-  fCZResol1PtTPCITS = new TH2F("fCZResol1PtTPCITS","fCZResol1PtTPCITS",50, 0,10,200,-0.020,0.020);   
+  fCZResol1PtTPCITS = new TH2F("fCZResol1PtTPCITS","fCZResol1PtTPCITS",100, 0,10,200,-0.05,0.05);   
   fCZResol1PtTPCITS->SetXTitle("1/mcpt");
   fCZResol1PtTPCITS->SetYTitle("#DeltaZ");
 
-  fCPhiResol1PtTPC = new TH2F("fCPhiResol1PtTPC","fCPhiResol1PtTPC",50, 0,10,200,-0.005,0.005);   
+  fCPhiResol1PtTPC = new TH2F("fCPhiResol1PtTPC","fCPhiResol1PtTPC",100, 0,10,200,-0.025,0.025);   
   fCPhiResol1PtTPC->SetXTitle("1/mcpt");
   fCPhiResol1PtTPC->SetYTitle("#Delta#phi");
 
-  fCPhiResol1PtTPCITS = new TH2F("fCPhiResol1PtTPCITS","fCPhiResol1PtTPCITS",50, 0,10,200,-0.005,0.005);   
+  fCPhiResol1PtTPCITS = new TH2F("fCPhiResol1PtTPCITS","fCPhiResol1PtTPCITS",100, 0,10,200,-0.01,0.01);   
   fCPhiResol1PtTPCITS->SetXTitle("1/mcpt");
   fCPhiResol1PtTPCITS->SetYTitle("#Delta#phi");
 
-  fCThetaResol1PtTPC = new TH2F("fCThetaResol1PtTPC","fCThetaResol1PtTPC",50, 0,10,200,-0.005,0.005);   
+  fCThetaResol1PtTPC = new TH2F("fCThetaResol1PtTPC","fCThetaResol1PtTPC",100, 0,10,200,-0.025,0.025);   
   fCThetaResol1PtTPC->SetXTitle("1/mcpt");
   fCThetaResol1PtTPC->SetYTitle("#Delta#theta");
 
-  fCThetaResol1PtTPCITS = new TH2F("fCThetaResol1PtTPCITS","fCThetaResol1PtTPCITS",50, 0,10,200,-0.005,0.005);   
+  fCThetaResol1PtTPCITS = new TH2F("fCThetaResol1PtTPCITS","fCThetaResol1PtTPCITS",100, 0,10,200,-0.01,0.01);   
   fCThetaResol1PtTPCITS->SetXTitle("1/mcpt");
   fCThetaResol1PtTPCITS->SetYTitle("#Delta#theta");
 }
@@ -321,6 +321,9 @@ void AliComparisonRes::Process(AliMCInfo* infoMC, AliESDRecInfo *infoRC)
   Int_t clusterITS[200];
   Double_t dca[2], cov[3]; // dca_xy, dca_z, sigma_xy, sigma_xy_z, sigma_z
 
+  Float_t deltaPt, pullPt, deltaPhi, deltaTan, delta1Pt2, deltaY1Pt, deltaZ1Pt, deltaPhi1Pt, deltaTheta1Pt; 
+  Float_t deltaPtTPC, pullPtTPC, deltaPhiTPC, deltaTanTPC, delta1Pt2TPC, deltaY1PtTPC, deltaZ1PtTPC, deltaPhi1PtTPC, deltaTheta1PtTPC; 
+
   Float_t mcpt = infoMC->GetParticle().Pt();
 
   // distance to Prim. vertex 
@@ -340,23 +343,30 @@ void AliComparisonRes::Process(AliMCInfo* infoMC, AliESDRecInfo *infoRC)
   fVertex->SetYv( infoMC->GetParticle().Vy() - dv[1] );
   fVertex->SetZv( infoMC->GetParticle().Vz() - dv[2] );
 
-  Float_t deltaPt= (mcpt-infoRC->GetESDtrack()->Pt())/mcpt;  
-  Float_t pullPt= (1/mcpt-infoRC->GetESDtrack()->OneOverPt())/TMath::Sqrt(infoRC->GetESDtrack()->GetSigma1Pt2());  
-  Float_t deltaPhi = TMath::ATan2(infoRC->GetESDtrack()->Py(),infoRC->GetESDtrack()->Px())-
+  deltaPt= (mcpt-infoRC->GetESDtrack()->Pt())/mcpt;  
+  pullPt= (1/mcpt-infoRC->GetESDtrack()->OneOverPt())/TMath::Sqrt(infoRC->GetESDtrack()->GetSigma1Pt2());  
+  deltaPhi = TMath::ATan2(infoRC->GetESDtrack()->Py(),infoRC->GetESDtrack()->Px())-
                      TMath::ATan2(infoMC->GetParticle().Py(),infoMC->GetParticle().Px());
 
-  Float_t deltaTan = TMath::ATan2(infoRC->GetESDtrack()->Pz(),infoRC->GetESDtrack()->Pt())-
+  deltaTan = TMath::ATan2(infoRC->GetESDtrack()->Pz(),infoRC->GetESDtrack()->Pt())-
                      TMath::ATan2(infoMC->GetParticle().Pz(),infoMC->GetParticle().Pt());
 
-  Float_t delta1Pt2 = (1/mcpt-infoRC->GetESDtrack()->OneOverPt())/TMath::Power(1+1/mcpt,2);       
+  delta1Pt2 = (1/mcpt-infoRC->GetESDtrack()->OneOverPt())/TMath::Power(1+1/mcpt,2);       
+  deltaY1Pt = (infoMC->GetParticle().Vy()-infoRC->GetESDtrack()->GetY()) / (0.2+1/mcpt);
+  deltaZ1Pt = (infoMC->GetParticle().Vz()-infoRC->GetESDtrack()->GetZ()) / (0.2+1/mcpt);
+  deltaPhi1Pt = deltaPhi   / (0.1+1/mcpt);
+  deltaTheta1Pt = deltaTan / (0.1+1/mcpt);
 
-  Float_t deltaY1Pt = (infoMC->GetParticle().Vy()-infoRC->GetESDtrack()->GetY()) / (0.2+1/mcpt);
-  Float_t deltaZ1Pt = (infoMC->GetParticle().Vz()-infoRC->GetESDtrack()->GetZ()) / (0.2+1/mcpt);
-  Float_t deltaPhi1Pt = deltaPhi   / (0.1+1/mcpt);
-  Float_t deltaTheta1Pt = deltaTan / (0.1+1/mcpt);
+  //track parameters at the first measured point (TPC) 
+  //Double_t param[5],x,alpha; // [0]-Y [cm],[1]-Z [cm],[2]-sin(phi),[3]-tan(theta),[4]-1/pt [1/GeV]   
+  //infoRC->GetESDtrack()->GetInnerExternalParameters(alpha,x,param);
+  //const AliExternalTrackParam *innerTPC =  infoRC->GetESDtrack()->GetInnerParam(); 
+  //const AliExternalTrackParam *innerTPC =  infoRC->GetESDtrack()->GetTPCInnerParam(); 
+
 
   // calculate track parameters at vertex
-  if (infoRC->GetESDtrack()->GetTPCInnerParam())
+  const AliExternalTrackParam *innerTPC =  0;
+  if ((innerTPC = infoRC->GetESDtrack()->GetTPCInnerParam()) != 0)
   {
     if ((track = new AliExternalTrackParam(*infoRC->GetESDtrack()->GetTPCInnerParam())) != 0 )
     {
@@ -366,17 +376,31 @@ void AliComparisonRes::Process(AliMCInfo* infoMC, AliESDRecInfo *infoRC)
       // Fill parametrisation histograms (only TPC track)
       if(bStatus && bDCAStatus) 
 	  {
-        f1Pt2Resol1PtTPC->Fill(1/mcpt,delta1Pt2);
-        fYResol1PtTPC->Fill(1/mcpt,deltaY1Pt);
-        fZResol1PtTPC->Fill(1/mcpt,deltaZ1Pt);
-        fPhiResol1PtTPC->Fill(1/mcpt,deltaPhi1Pt);
-        fThetaResol1PtTPC->Fill(1/mcpt,deltaTheta1Pt);
+			deltaPtTPC= (mcpt-innerTPC->Pt())/mcpt;  
+			pullPtTPC= (1/mcpt-innerTPC->OneOverPt())/TMath::Sqrt(innerTPC->GetSigma1Pt2());  
+			deltaPhiTPC = TMath::ATan2(innerTPC->Py(),innerTPC->Px())-
+								TMath::ATan2(infoMC->GetParticle().Py(),infoMC->GetParticle().Px());
+
+			deltaTanTPC = TMath::ATan2(innerTPC->Pz(),innerTPC->Pt())-
+								TMath::ATan2(infoMC->GetParticle().Pz(),infoMC->GetParticle().Pt());
+
+			delta1Pt2TPC = (1/mcpt-innerTPC->OneOverPt())/TMath::Power(1+1/mcpt,2);       
+			deltaY1PtTPC= (infoMC->GetParticle().Vy()-innerTPC->GetY()) / (0.2+1/mcpt);
+			deltaZ1PtTPC = (infoMC->GetParticle().Vz()-innerTPC->GetZ()) / (0.2+1/mcpt);
+			deltaPhi1PtTPC = deltaPhiTPC   / (0.1+1/mcpt);
+			deltaTheta1PtTPC = deltaTanTPC / (0.1+1/mcpt);
+
+			f1Pt2Resol1PtTPC->Fill(1/mcpt,delta1Pt2TPC);
+			fYResol1PtTPC->Fill(1/mcpt,deltaY1PtTPC);
+			fZResol1PtTPC->Fill(1/mcpt,deltaZ1PtTPC);
+			fPhiResol1PtTPC->Fill(1/mcpt,deltaPhi1PtTPC);
+			fThetaResol1PtTPC->Fill(1/mcpt,deltaTheta1PtTPC);
 	  }
 	  delete track;
     }
   }
 
- // TPC and ITS (nb. of clusters >2) in the system
+  // TPC and ITS (nb. of clusters >2) in the system
   if(infoRC->GetESDtrack()->GetITSclusters(clusterITS)>2) 
   {
       f1Pt2Resol1PtTPCITS->Fill(1/mcpt,delta1Pt2);
@@ -407,6 +431,9 @@ void AliComparisonRes::ProcessConstrained(AliMCInfo* infoMC, AliESDRecInfo *info
   Int_t clusterITS[200];
   Double_t dca[2], cov[3]; // dca_xy, dca_z, sigma_xy, sigma_xy_z, sigma_z
  
+  Float_t deltaPt, pullPt, deltaPhi, pullPhi, deltaTan, pullTan, delta1Pt2, deltaY1Pt, deltaZ1Pt, deltaPhi1Pt, deltaTheta1Pt; 
+  Float_t deltaPtTPC, pullPtTPC, deltaPhiTPC, deltaTanTPC, delta1Pt2TPC, deltaY1PtTPC, deltaZ1PtTPC, deltaPhi1PtTPC, deltaTheta1PtTPC; 
+
   Float_t mcpt = infoMC->GetParticle().Pt();
   Float_t tantheta = TMath::Tan(infoMC->GetParticle().Theta()-TMath::Pi()*0.5);
 
@@ -429,24 +456,25 @@ void AliComparisonRes::ProcessConstrained(AliMCInfo* infoMC, AliESDRecInfo *info
 
   // constrained parameters resolution
   const AliExternalTrackParam * cparam = infoRC->GetESDtrack()->GetConstrainedParam();
-  Float_t deltaPt= (mcpt-cparam->Pt())/mcpt;  
-  Float_t pullPt= (1/mcpt-cparam->OneOverPt())/TMath::Sqrt(cparam->GetSigma1Pt2());          
-  Float_t deltaPhi = TMath::ATan2(cparam->Py(),cparam->Px())-
+  deltaPt= (mcpt-cparam->Pt())/mcpt;  
+  pullPt= (1/mcpt-cparam->OneOverPt())/TMath::Sqrt(cparam->GetSigma1Pt2());          
+  deltaPhi = TMath::ATan2(cparam->Py(),cparam->Px())-
                      TMath::ATan2(infoMC->GetParticle().Py(),infoMC->GetParticle().Px());
-  Float_t pullPhi = deltaPhi/TMath::Sqrt(cparam->GetSigmaSnp2()); 
-  Float_t deltaTan = TMath::ATan2(cparam->Pz(),cparam->Pt())-TMath::ATan2(infoMC->GetParticle().Pz(),infoMC->GetParticle().Pt());
-  Float_t pullTan = deltaPhi/TMath::Sqrt(cparam->GetSigmaSnp2()); 
+  pullPhi = deltaPhi/TMath::Sqrt(cparam->GetSigmaSnp2()); 
+  deltaTan = TMath::ATan2(cparam->Pz(),cparam->Pt())-TMath::ATan2(infoMC->GetParticle().Pz(),infoMC->GetParticle().Pt());
+  pullTan = deltaPhi/TMath::Sqrt(cparam->GetSigmaSnp2()); 
 
 
-  Float_t delta1Pt2 = (1/mcpt-cparam->OneOverPt())/TMath::Power(1+1/mcpt,2);       
+  delta1Pt2 = (1/mcpt-cparam->OneOverPt())/TMath::Power(1+1/mcpt,2);       
 
-  Float_t deltaY1Pt = (infoMC->GetParticle().Vy()-cparam->GetY()) / (0.2+1/mcpt);
-  Float_t deltaZ1Pt = (infoMC->GetParticle().Vz()-cparam->GetZ()) / (0.2+1/mcpt);
-  Float_t deltaPhi1Pt = deltaPhi   / (0.1+1/mcpt);
-  Float_t deltaTheta1Pt = deltaTan / (0.1+1/mcpt);
+  deltaY1Pt = (infoMC->GetParticle().Vy()-cparam->GetY()) / (0.2+1/mcpt);
+  deltaZ1Pt = (infoMC->GetParticle().Vz()-cparam->GetZ()) / (0.2+1/mcpt);
+  deltaPhi1Pt = deltaPhi   / (0.1+1/mcpt);
+  deltaTheta1Pt = deltaTan / (0.1+1/mcpt);
 
   // calculate track parameters at vertex
-  if (infoRC->GetESDtrack()->GetTPCInnerParam())
+  const AliExternalTrackParam *innerTPC =  0;
+  if ((innerTPC = infoRC->GetESDtrack()->GetTPCInnerParam()) != 0)
   {
     if ((track = new AliExternalTrackParam(*infoRC->GetESDtrack()->GetTPCInnerParam())) != 0 )
     {
@@ -456,11 +484,25 @@ void AliComparisonRes::ProcessConstrained(AliMCInfo* infoMC, AliESDRecInfo *info
       // Fill parametrisation histograms (only TPC track)
       if(bStatus && bDCAStatus) 
 	  {
-        fC1Pt2Resol1PtTPC->Fill(1/mcpt,delta1Pt2);
-        fCYResol1PtTPC->Fill(1/mcpt,deltaY1Pt);
-        fCZResol1PtTPC->Fill(1/mcpt,deltaZ1Pt);
-        fCPhiResol1PtTPC->Fill(1/mcpt,deltaPhi1Pt);
-        fCThetaResol1PtTPC->Fill(1/mcpt,deltaTheta1Pt);
+		  deltaPtTPC= (mcpt-innerTPC->Pt())/mcpt;  
+		  pullPtTPC= (1/mcpt-innerTPC->OneOverPt())/TMath::Sqrt(innerTPC->GetSigma1Pt2());  
+		  deltaPhiTPC = TMath::ATan2(innerTPC->Py(),innerTPC->Px())-
+								TMath::ATan2(infoMC->GetParticle().Py(),infoMC->GetParticle().Px());
+
+		  deltaTanTPC = TMath::ATan2(innerTPC->Pz(),innerTPC->Pt())-
+								TMath::ATan2(infoMC->GetParticle().Pz(),infoMC->GetParticle().Pt());
+
+		  delta1Pt2TPC = (1/mcpt-innerTPC->OneOverPt())/TMath::Power(1+1/mcpt,2);       
+		  deltaY1PtTPC= (infoMC->GetParticle().Vy()-innerTPC->GetY()) / (0.2+1/mcpt);
+		  deltaZ1PtTPC = (infoMC->GetParticle().Vz()-innerTPC->GetZ()) / (0.2+1/mcpt);
+		  deltaPhi1PtTPC = deltaPhiTPC   / (0.1+1/mcpt);
+		  deltaTheta1PtTPC = deltaTanTPC / (0.1+1/mcpt);
+
+          fC1Pt2Resol1PtTPC->Fill(1/mcpt,delta1Pt2TPC);
+          fCYResol1PtTPC->Fill(1/mcpt,deltaY1PtTPC);
+          fCZResol1PtTPC->Fill(1/mcpt,deltaZ1PtTPC);
+          fCPhiResol1PtTPC->Fill(1/mcpt,deltaPhi1PtTPC);
+          fCThetaResol1PtTPC->Fill(1/mcpt,deltaTheta1PtTPC);
 	  }
 	  delete track;
     }
