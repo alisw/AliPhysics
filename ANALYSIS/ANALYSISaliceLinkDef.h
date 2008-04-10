856e635759db40a7fd7492fa18f7bade50d2c8f1
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-10 09:53:22 +0000

    Moved from PWG0

--- ANALYSIS/ANALYSISaliceLinkDef.h
+++ ANALYSIS/ANALYSISaliceLinkDef.h
@@ -8,6 +8,7 @@
 #pragma link C++ class  AliAnalysisTaskESDfilter+;
 #pragma link C++ class  AliAnalysisTaskKineFilter+;
 #pragma link C++ class  AliKineTrackCuts+;
+#pragma link C++ class  AliESDtrackCuts+;
 #ifdef WITHXML
 #pragma link C++ class AliTagAnalysis+;
 #pragma link C++ class AliXMLCollection+;
