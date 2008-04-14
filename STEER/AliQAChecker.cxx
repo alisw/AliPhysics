1b742506ddab09efce457afbb821fa50400e1c9c
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-14 11:35:21 +0000

    set the default CDB storage only id not ser previously

--- STEER/AliQAChecker.cxx
+++ STEER/AliQAChecker.cxx
@@ -154,12 +154,12 @@ void AliQAChecker::GetRefSubDir(const char * det, const char * task, TDirectory 
 		}  
 	} else if (refStorage.Contains(AliQA::GetLabLocalOCDB()) || refStorage.Contains(AliQA::GetLabAliEnOCDB())) {	
 		AliCDBManager* man = AliCDBManager::Instance() ; 
-		if ( man->IsDefaultStorageSet() ) { 
-			TString temp(AliQA::GetQARefStorage()) ;  
-			if ( ! temp.Contains(man->GetDefaultStorage()->GetBaseFolder() ) ) {
+		if ( ! man->IsDefaultStorageSet() ) //{ 
+//		TString temp(AliQA::GetQARefStorage()) ;  
+//			if ( ! temp.Contains(man->GetDefaultStorage()->GetBaseFolder() ) ) {
 				man->SetDefaultStorage(AliQA::GetQARefStorage()) ; 
-				man->SetSpecificStorage("*", AliQA::GetQARefStorage()) ;
-			}
+		man->SetSpecificStorage("*", AliQA::GetQARefStorage()) ;
+//			}
 		}
 		char detOCDBDir[20] ; 
 		sprintf(detOCDBDir, "%s/%s/%s", det, AliQA::GetRefOCDBDirName(), AliQA::GetRefDataDirName()) ; 
