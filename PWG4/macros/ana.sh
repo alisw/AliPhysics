abbd10a0550ff7dc895f6fb712e0e207843a5710
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-15 12:47:35 +0000

    Some comentaries and options added

--- PWG4/macros/ana.sh
+++ PWG4/macros/ana.sh
@@ -6,10 +6,22 @@
 # Created by schutz on 09.04.08.
 # Copyright 2008 ALICE. All rights reserved.
 
-export INDIR=$1
+#Analysis mode, 0 local, 1 localCAF, 2 PROOF, 3 GRID
+export MODE=$1
+
+#Local mode parameters
+#Data root directory
+export INDIR=$2
+#Pattern of data directory name
 export PATTERN=Run
-export NEVENT=$2
-export MODE=$3
+#Number of files to analyze
+export NEVENT=$3
+
+#GRID mode parameters
+#name of xml file
+export XML=$2
+
+#Do the analysis
 alienroot -b  <<EOF
 .L anaGammaAnalysis.C
 anaGammaAnalysis($MODE)
