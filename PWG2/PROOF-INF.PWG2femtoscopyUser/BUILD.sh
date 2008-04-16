348cfa1881ca985cb5baefe379056b876f1a4643
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-16 14:06:55 +0000

    Fixing the PWG2femtoscopyUser PROOF-INF directory

---
+++ PWG2/PROOF-INF.PWG2femtoscopyUser/BUILD.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+touch libPWG2femtoscopyUser.pkg
+
+make libPWG2femtoscopyUser.so
---
+++ PWG2/PROOF-INF.PWG2femtoscopyUser/SETUP.C
@@ -0,0 +1,18 @@
+void SETUP() {
+  CheckLoadLibrary("libPWG2femtoscopyUser");
+  
+  // Set the include paths
+  gROOT->ProcessLine(".include PWG2femtoscopyUser");
+
+  // Set our location, so that other packages can find us
+  gSystem->Setenv("PWG2femtoscopyUser_INCLUDE", "PWG2femtoscopyUser/FEMTOSCOPY/AliFemtoUser");
+}
+
+Int_t CheckLoadLibrary(const char* library) {
+  // checks if a library is already loaded, if not loads the library
+
+  if (strlen(gSystem->GetLibraries(Form("%s.so", library), "", kFALSE)) > 0)
+    return 1;
+  
+  return gSystem->Load(library);
+}
