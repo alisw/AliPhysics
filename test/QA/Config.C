67f73b42b010b2c07819bd5b686ab77b7b42963e
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-21 11:33:05 +0000

    change the order in loading the libraries

--- test/QA/Config.C
+++ test/QA/Config.C
@@ -187,13 +187,13 @@ void Config()
 
   // libraries required by geant321
 #if defined(__CINT__)
-  gSystem->Load("liblhapdf.so");      // Parton density functions
-  gSystem->Load("libEGPythia6.so");   // TGenerator interface
-  gSystem->Load("libpythia6.so");     // Pythia
-  gSystem->Load("libAliPythia6.so");  // ALICE specific implementations
-  gSystem->Load("libgeant321");
+	gSystem->Load("liblhapdf.so");      // Parton density functions
+	gSystem->Load("libpythia6.so");     // Pythia
+	gSystem->Load("libgeant321");
+	gSystem->Load("libEG");
+	gSystem->Load("libEGPythia6.so");   // TGenerator interface
+	gSystem->Load("libAliPythia6.so");  // ALICE specific implementations
 #endif
-
   new TGeant3TGeo("C++ Interface to Geant3");
 
   // Output every 100 tracks
