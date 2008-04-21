5061977e23ff11b2dc5a88d3f3302731c700506e
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-21 14:39:17 +0000

    tag files are skipped, screwing up the file counter for rawqa

--- test/QA/simrun.sh
+++ test/QA/simrun.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 cd $WORK/QATest
 rm -f *.root *.C *.log
 ln -si $ALICE_ROOT/test/QA/Config.C Config.C
@@ -8,11 +8,12 @@ aliroot -b -q $ALICE_ROOT/test/QA/rec.C > rec.log 2>&1
 alienroot -b -q $ALICE_ROOT/test/QA/recqa.C > recqa.log 2>&1
 alienroot -b -q $ALICE_ROOT/test/QA/tag.C > tag.log 2>&1
 cd $WORK/QATest/data
+rm -f *.root
 cp  $ALICE_ROOT/test/QA/rawqa.C .
-aliroot -b -q  > rawqa.log 2>&1 << EOF
+alienroot -b > rawqa.log 2>&1 << EOF
 .x  $ALICE_ROOT/test/QA/rootlogon.C
 .L rawqa.C++
-rawqa(21950, 1)
+rawqa(21950, 10)
 EOF
 rm -f rawqa.C
 exit
