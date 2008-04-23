085854f29d09b9e6d8bb1a216be460a4afd0a8b0
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-23 13:14:00 +0000

    Do the correct unit conversion

--- TTherminator/TTherminator.cxx
+++ TTherminator/TTherminator.cxx
@@ -35,6 +35,7 @@
 #include <TDatabasePDG.h>
 #include <TParticle.h>
 #include <TClonesArray.h>
+#include <TClass.h>
 
 ReadPar *sRPInstance;
 STR      sRPFileName;
@@ -233,7 +234,7 @@ Int_t       TTherminator::ImportParticles(TClonesArray *particles, Option_t */*o
   Int_t nump = 0;
   if (!fEvent) return 0;
   Int_t numpart = fEvent->GetParticleCount();
-  printf("\n TTherminator: Therminator stack contains %d particles.", numpart);
+  printf("\n TTherminator: Therminator stack contains %d particles.\n", numpart);
   for (Int_t iPart=0; iPart<numpart; iPart++) {
     Particle *tPart = fEvent->GetParticleOfCount(iPart);
     Int_t tFather;
@@ -248,9 +249,9 @@ Int_t       TTherminator::ImportParticles(TClonesArray *particles, Option_t */*o
     nump++;
     //    printf("Putting %d %d %lf %d\n", tPart->GetParticleType()->GetPDGCode(), iPart, tPart->px, tPart);
     new (particlesR[iPart]) TParticle(tPart->GetParticleType()->GetPDGCode(), tPart->HadDecayed(),
-				      tFather, -1, -1, -1,
-				      tPart->px, tPart->py, tPart->pz, tPart->GetEnergy() ,
-				      tPart->rx*1.e-13*kFmToGev, tPart->ry*1.e-13*kFmToGev, tPart->rz*1.e-13*kFmToGev, tPart->rt*1.e-13*kFmToGev/3e10);
+ 				      tFather, -1, -1, -1,
+ 				      tPart->px, tPart->py, tPart->pz, tPart->GetEnergy() ,
+ 				      tPart->rx*1.e-13*kFmToGev, tPart->ry*1.e-13*kFmToGev, tPart->rz*1.e-13*kFmToGev, tPart->rt*1.e-13*kFmToGev/3e10);
     particlesR[iPart]->SetUniqueID(iPart);
   }
   
