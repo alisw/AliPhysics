0a7425d9117d3ca9aead91e766addbe37a1dc909
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-24 12:50:45 +0000

    Remove compilation warnings (Marian)

--- TPC/AliTPCcalibBase.h
+++ TPC/AliTPCcalibBase.h
@@ -10,16 +10,16 @@
 
 #include "TNamed.h"
 class AliTPCseed;
-class AliESDEvent;
+class AliESDevent;
 class TCollection;
 
 class AliTPCcalibBase:public TNamed {
 public:
-  AliTPCcalibBase();
+  AliTPCcalibBase(); 
   virtual ~AliTPCcalibBase();
-  virtual void     Process(AliESDEvent *event){return;}
-  virtual void     Process(AliTPCseed *track){return;}
-  virtual Long64_t Merge(TCollection *li){return 0;}
+  virtual void     Process(AliESDevent */*event*/){return;}
+  virtual void     Process(AliTPCseed */*track*/){return;}
+  virtual Long64_t Merge(TCollection */*li*/){return 0;}
   virtual void     Analyze(){return;}
  private: 
   ClassDef(AliTPCcalibBase,1)
