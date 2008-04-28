07c5b189090a12d31f7184cb352d064f42e1158a
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-28 13:49:21 +0000

    Changed call to TClonesArray::GetEntries() to TClonesArray::GetEntriesFast() for performance

--- FMD/AliFMDRawReader.cxx
+++ FMD/AliFMDRawReader.cxx
@@ -90,7 +90,7 @@ AliFMDRawReader::Exec(Option_t*)
   ReadAdcs(array);
   Int_t nWrite = fTree->Fill();
   AliFMDDebug(1, ("Got a grand total of %d digits, wrote %d bytes to tree", 
-		   array->GetEntries(), nWrite));
+		   array->GetEntriesFast(), nWrite));
 }
 
 
