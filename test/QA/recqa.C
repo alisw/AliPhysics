f7c63e01cb54b6d56b6e146c45a0380307fa0dc3
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-28 17:13:16 +0000

    cleaning

--- test/QA/recqa.C
+++ test/QA/recqa.C
@@ -1,13 +1,15 @@
 void recqa()
 {
-  const char * kYear = "08" ; 
-  AliGeomManager::LoadGeometry("geometry.root");	
-  AliCDBManager * man = AliCDBManager::Instance();
-  //man->SetDefaultStorage("alien://Folder=/alice/simulation/2007/PDC07_v4-09-Rev-00/Ideal/CDB/");
-  man->SetDefaultStorage("local://$ALICE_ROOT");
-  TString detectors("ITS TPC TRD TOF PHOS HMPID EMCAL MUON FMD ZDC PMD T0 VZERO");
-  AliQA::SetQARefStorage(Form("%s%s/", AliQA::GetQARefDefaultStorage(), kYear)) ;
-  AliQA::SetQARefDataDirName("Sim") ; //Data, Pedestals, BlackEvent, .....
+	const char * kYear = "08" ; 
+ 	gEnv->SetValue("Root.Stacktrace","no");
+	gEnv->SetValue("Root.Stacktrace","no");
+	AliCDBManager * man = AliCDBManager::Instance();
+	//man->SetDefaultStorage("alien://Folder=/alice/simulation/2007/PDC07_v4-09-Rev-00/Ideal/CDB/");
+	man->SetDefaultStorage("local://$ALICE_ROOT");
+	TString detectors("ITS TPC TRD TOF PHOS HMPID EMCAL MUON FMD ZDC PMD T0 VZERO");
+	
+	AliQA::SetQARefStorage(Form("%s%s/", AliQA::GetQARefDefaultStorage(), kYear)) ;
+	AliQA::SetQARefDataDirName("Sim") ; //Data, Pedestals, BlackEvent, .....
    
   AliQADataMakerSteer qas ; 
   qas.Run(detectors.Data(), AliQA::kRECPOINTS);
--- test/QA/simqa.C
+++ test/QA/simqa.C
@@ -1,15 +1,19 @@
 void simqa()
 {
-  const char * kYear = "08" ; 
-  gEnv->SetValue("Root.Stacktrace","no");
-  gEnv->SetValue("Root.Stacktrace","no");
-  AliQA::SetQARefStorage(Form("%s%s/", AliQA::GetQARefDefaultStorage(), kYear)) ;
-  AliQA::SetQARefDataDirName("Sim") ; //Data, Pedestals, BlackEvent, .....
+	const char * kYear = "08" ; 
+	gEnv->SetValue("Root.Stacktrace","no");
+	gEnv->SetValue("Root.Stacktrace","no");
+	AliCDBManager * man = AliCDBManager::Instance();
+	//man->SetDefaultStorage("alien://Folder=/alice/simulation/2007/PDC07_v4-09-Rev-00/Ideal/CDB/");
+	man->SetDefaultStorage("local://$ALICE_ROOT");
+	TString detectors("ITS TPC TRD TOF PHOS HMPID EMCAL MUON FMD PMD ZDC T0 VZERO"); 
+	
+	AliQA::SetQARefStorage(Form("%s%s/", AliQA::GetQARefDefaultStorage(), kYear)) ;
+	AliQA::SetQARefDataDirName("Sim") ; //Data, Pedestals, BlackEvent, .....
   
-  TString detectors("ITS TPC TRD TOF PHOS HMPID EMCAL MUON FMD PMD ZDC T0 VZERO"); 
 
-  AliQADataMakerSteer qas ; 
-  qas.Run(detectors.Data(), AliQA::kHITS);
-  qas.Run(detectors.Data(), AliQA::kSDIGITS);
-  qas.Run(detectors.Data(), AliQA::kDIGITS);
+	AliQADataMakerSteer qas ; 
+	qas.Run(detectors.Data(), AliQA::kHITS);
+	qas.Run(detectors.Data(), AliQA::kSDIGITS);
+	qas.Run(detectors.Data(), AliQA::kDIGITS);
 }
