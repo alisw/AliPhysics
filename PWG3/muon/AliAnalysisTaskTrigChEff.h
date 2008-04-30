a08f16c8a8eebc3c7ea311adeb7038b601b3858e
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-30 07:59:02 +0000

    Added AliAnalysisTaskTrigChEff: analysis task for trigger chamber efficiency determination. Added AnalysisTrigChEff.C: macro to run the analysis task (locally or on grid) (Digeo)

---
+++ PWG3/muon/AliAnalysisTaskTrigChEff.h
@@ -0,0 +1,69 @@
+#include "TH1.h"
+#include "TList.h"
+
+#include "AliESDEvent.h"
+#include "AliAODEvent.h"
+
+class AliAnalysisTaskTrigChEff : public AliAnalysisTask {
+ public:
+  AliAnalysisTaskTrigChEff(const char *name = "AliAnalysisTaskTrigChEff");
+  virtual ~AliAnalysisTaskTrigChEff() {}
+  
+  virtual void   ConnectInputData(Option_t *);
+  virtual void   CreateOutputObjects();
+  virtual void   Exec(Option_t *option);
+  virtual void   Terminate(Option_t *);
+
+  void SetType(const char* type) {fAnalysisType = type;}
+
+protected:
+  void ResetHistos();
+  
+  /// Getting flag telling which efficiency is performable for current track
+  Int_t GetEffFlag(UShort_t pattern) { return (pattern >> 8) & 0x03; }
+  
+  /// Getting crossed slat
+  Int_t GetSlat(UShort_t pattern) { return (pattern >> 10) & 0x1F; }
+  
+  Int_t IsChInefficient(UShort_t pattern, Int_t cathode);
+
+private:
+  AliESDEvent* fESD; //!< ESDevent object
+  AliAODEvent* fAOD; //!< AODevent object
+  TString fAnalysisType; //"ESD" or "AOD"
+
+  TList*  fList; //TList output object
+
+  enum {
+    kNcathodes = 2,  ///< Number of cathodes
+    kNchambers = 4,  ///< Number of chambers
+    kNplanes   = 8,  ///< Number of planes
+    kNslats    = 18 ///< Number of slats
+  };
+
+  enum {kAllChEff, kChNonEff, kNcounts};
+
+  enum {
+    kNoEff,
+    kChEff,
+    kSlatEff,
+    kBoardEff
+  };
+
+  enum {
+    kHtracksInSlat  = 0,  ///< Tracks in slat histogram index
+    kHtracksInBoard = 1,  ///< Tracks in board histogram index
+    kHchamberAllEff = 2,  ///< N44 per cathode histogram index
+    kHchamberNonEff = 4,  ///< N33 per cathode histogram index
+    kHslatAllEff    = 6,  ///< N44 per slat histogram index
+    kHslatNonEff    = 14, ///< N33 per slat histogram index
+    kHboardAllEff   = 22, ///< N44 per board histogram index
+    kHboardNonEff   = 30  ///< N33 per board histogram index
+  };
+  
+  /// Given cathode and chamber, return plane number
+  Int_t GetPlane(Int_t cathode, Int_t chamber) { return kNchambers*cathode + chamber; }
+
+  ClassDef(AliAnalysisTaskTrigChEff, 0); // Single muon analysis
+};
+
---
+++ PWG3/muon/AnalysisTrigChEff.C
@@ -0,0 +1,159 @@
+//--------------------------------------------------------------------------
+// Base macro for submitting trigger chamber efficiency determination.
+// 
+// In case it is not run with full aliroot, it needs to have in the working directory:
+//  - STEERBase.par
+//  - ESD.par
+//  - AOD.par
+//  - ANALYSIS.par
+//  - ANALYSISalice.par
+// 
+// The macro reads ESDs and outputs file:
+// - MUON.TriggerEfficiencyMap.root
+//
+// To display the trigger chamber efficiency:
+// > aliroot
+// > AliMUONTriggerEfficiencyCells effCells("MUON.TriggerEfficiencyMap.root")
+// > effCells.DisplayEfficiency()
+//--------------------------------------------------------------------------
+
+enum anaModes {kMlocal, kMgridLocal, kMgrid};
+TString modeName[3] = {"local", "local", "grid"};
+const TString kDefaultLocalInputDir="$ALICE_ROOT/MUON/test_out.100";
+const TString kDefaultXmlFile="wn.xml";
+
+void AnalysisTrigChEff(Int_t mode=kMlocal)
+{
+// Example of running analysis train
+  TStopwatch timer;
+  timer.Start();
+
+  gSystem->Load("libTree.so");
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libPhysics.so");
+
+  // Common packages
+  SetupPar("STEERBase");
+  SetupPar("ESD");
+  SetupPar("AOD");
+  SetupPar("ANALYSIS");
+  SetupPar("ANALYSISalice");
+
+  // Analysis using standard AliRoot libraries
+  gSystem->Load("libSTEERBase.so");
+  gSystem->Load("libESD.so");
+  gSystem->Load("libAOD.so");
+  gSystem->Load("libANALYSIS.so");
+  gSystem->Load("libANALYSISalice.so");
+
+  
+  // A task can be compiled dynamically with AClic
+  gROOT->ProcessLine(".L AliAnalysisTaskTrigChEff.cxx+");
+
+  //
+  // Connect to alien
+  //
+  if(mode==kMgridLocal || mode==kMgrid)
+    TGrid::Connect("alien://"); 
+
+  //
+  // Create the chain
+  //
+  TChain* chain = CreateChain(mode);
+
+  TString outFileName("MUON.TriggerEfficiencyMap.root");
+  //
+  //
+  // Make the analysis manager
+  AliAnalysisManager *mgr = new AliAnalysisManager("Analysis Train", "A test setup for the analysis train");
+  // ESD input handler
+  AliESDInputHandler *esdHandler = new AliESDInputHandler();
+  //esdHandler->SetInactiveBranches("FMD CaloCluster");
+  // Monte Carlo handler
+  //AliMCEventHandler* mcHandler = new AliMCEventHandler();
+
+  mgr->SetInputEventHandler(esdHandler);
+  //mgr->SetMCtruthEventHandler(mcHandler);
+  
+  // Trigger chamber efficiency analysis
+  AliAnalysisTaskTrigChEff* taskTrigChEff = new AliAnalysisTaskTrigChEff("TaskTrigChEff");
+  mgr->AddTask(taskTrigChEff);
+  
+  // Create containers for input/output
+  // Top container for ESD input 
+  AliAnalysisDataContainer *cin_esd = mgr->CreateContainer("cESD",TChain::Class(), 
+							   AliAnalysisManager::kInputContainer);
+
+  // Output histograms list for single muons analysis
+  AliAnalysisDataContainer *cout_trigChEff = mgr->CreateContainer("triggerChamberEff", TList::Class(),
+								 AliAnalysisManager::kOutputContainer, outFileName.Data());
+
+  mgr->ConnectInput  (taskTrigChEff, 0, cin_esd);
+  mgr->ConnectOutput (taskTrigChEff, 0, cout_trigChEff);
+
+  //
+  // Run the analysis
+  //
+  if (mgr->InitAnalysis()) {
+    mgr->PrintStatus();
+    mgr->StartAnalysis(modeName[mode].Data(), chain);
+  }
+  timer.Stop();
+  timer.Print();
+}
+
+//______________________________________________________________________________
+TChain* CreateChain(Int_t mode)
+{
+  printf("*******************************\n");
+  printf("*** Getting the Chain       ***\n");
+  printf("*******************************\n");
+  TChain *chain = 0x0;
+  if(mode==kMgridLocal || mode==kMgrid){
+    AliTagAnalysis *analysis = new AliTagAnalysis();
+    chain = analysis->GetChainFromCollection(kDefaultXmlFile.Data(),"esdTree");
+  }
+  else{
+    chain = new TChain("esdTree");
+    TString inFileName("AliESDs.root");
+    inFileName.Prepend(Form("%s/",kDefaultLocalInputDir.Data()));
+    chain->Add(inFileName.Data());
+  }
+  if (chain) chain->ls();
+  return chain;
+}
+
+//______________________________________________________________________________
+void SetupPar(char* pararchivename)
+{
+  if (pararchivename) {
+    char processline[1024];
+    sprintf(processline,".! tar xvzf %s.par",pararchivename);
+    gROOT->ProcessLine(processline);
+    const char* ocwd = gSystem->WorkingDirectory();
+    gSystem->ChangeDirectory(pararchivename);
+    printf("Current directory = %s\n",gSystem->pwd());
+
+    // check for BUILD.sh and execute
+    if (!gSystem->AccessPathName("PROOF-INF/BUILD.sh")) {
+      printf("*******************************\n");
+      printf("*** Building PAR archive    ***\n");
+      printf("*******************************\n");
+
+      if (gSystem->Exec("PROOF-INF/BUILD.sh")) {
+	Error("runProcess","Cannot Build the PAR Archive! - Abort!");
+	return -1;
+      }
+    }
+    // check for SETUP.C and execute
+    if (!gSystem->AccessPathName("PROOF-INF/SETUP.C")) {
+      printf("*******************************\n");
+      printf("*** Setup PAR archive       ***\n");
+      printf("*******************************\n");
+      gROOT->Macro("PROOF-INF/SETUP.C");
+    }
+	
+    gSystem->ChangeDirectory("../");
+  } 
+}
