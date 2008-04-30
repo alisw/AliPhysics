d7169f1533d5cb1b9d19df4cc7e3c6148d3048e6
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-04-30 12:51:31 +0000

    update configuration

--- PWG3/vertexingHF/ConfigVertexingHF.C
+++ PWG3/vertexingHF/ConfigVertexingHF.C
@@ -13,20 +13,20 @@ AliAnalysisVertexingHF* ConfigVertexingHF() {
   //vHF->SetSecVtxWithKF();
   //--- set cuts for single-track selection
   vHF->SetITSrefitRequired();
-  vHF->SetBothSPDNotRequired();
+  vHF->SetBothSPDRequired();
   vHF->SetMinITSCls(5);
   vHF->SetMinPtCut(0.3);
   vHF->SetMind0Cut(0.);
   //--- set cuts for candidates selection
-  vHF->SetD0toKpiCuts(0.6,999999.,1.1,0.,0.,999999.,999999.,999999.,-1);
-  vHF->SetBtoJPSICuts(0.6);
+  vHF->SetD0toKpiCuts(0.2,999999.,1.1,0.,0.,999999.,999999.,999999.,0.3);
+  vHF->SetBtoJPSICuts(0.350);
   vHF->SetDplusCuts(0.2,0.,0.,0.,0.,0.01,0.06,0.,0.,0.8);
-  vHF->SetDsCuts(0.2);
-  vHF->SetLcCuts(0.2);
+  vHF->SetDsCuts(0.2,0.,0.,0.,0.,0.01,0.06,0.,0.,0.8);
+  vHF->SetLcCuts(0.2,0.,0.,0.,0.,0.01,0.06,0.,0.,0.8);
   //--- set this if you want to reconstruct primary vertex candidate by
   //    candidate using other tracks in the event (for pp, broad 
   //    interaction region)
-  //vHF->SetRecoPrimVtxSkippingTrks();
+  vHF->SetRecoPrimVtxSkippingTrks();
   //--- OR set this if you want to remove the candidate daughters from 
   //    the primary vertex, without recostructing it from scratch
   //vHF->SetRmTrksFromPrimVtx();
