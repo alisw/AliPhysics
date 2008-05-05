a752f983ff0279368dbb032342ec176cd1217de2
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-05 09:01:39 +0000

    PAR library for resonance package

---
+++ PWG2/PROOF-INF.PWG2resonances/SETUP.C
@@ -0,0 +1,19 @@
+void SETUP() {
+  CheckLoadLibrary("libANALYSISalice");
+  CheckLoadLibrary("libPWG2resonances");
+
+  // Set the include paths
+  gROOT->ProcessLine(".include PWG2resonances");
+
+  // Set our location, so that other packages can find us
+  gSystem->Setenv("PWG2resonances_INCLUDE", "PWG2resonances");
+}
+
+Int_t CheckLoadLibrary(const char* library) {
+  // checks if a library is already loaded, if not loads the library
+
+  if (strlen(gSystem->GetLibraries(Form("%s.so", library), "", kFALSE)) > 0)
+    return 1;
+  
+  return gSystem->Load(library);
+}
