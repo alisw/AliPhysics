152dc92ba8e17e11a0436f0b089e214bd2f1f4aa
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-06 21:52:08 +0000

    Pattern class for validation of electronics

---
+++ HLT/PHOS/AliHLTPHOSPattern.h
@@ -0,0 +1,43 @@
+#ifndef ALIHLTPHOSPATTERN_H
+#define ALIHLTPHOSPATTERN_H 
+
+/* Copyright(c) 2006, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice  */ 
+
+
+//#include "AliHLTPHOSConstants.h"
+//using  namespace PhosHLTConst;
+#include "AliHLTPHOSBase.h"
+
+
+class AliHLTPHOSPattern : public AliHLTPHOSBase
+{
+ public:
+  AliHLTPHOSPattern(const int *pattern, const int length);
+  ~AliHLTPHOSPattern();
+  const int AddPattern(const int *readbackpattern,  const int nsamples =  ALTRO_MAX_SAMPLES, const int nPresamples = 0);
+  const bool CheckDoExistPattern(const int *readbackpattern,  const int nsamples =  ALTRO_MAX_SAMPLES, const int nPresamples = 0);
+  const AliHLTPHOSPattern *GetNextPtr() const {return fPattern;}; 
+  const int  GetPattern(int *pattern,  const int maxlengths =  ALTRO_MAX_SAMPLES) const;
+  const int GetPatternLength() const {return  fPatternLength;};
+  const int ValidatePattern(const int *readbackpattern,  const int nsamples =  ALTRO_MAX_SAMPLES, const int nPresamples = 0) const;
+  void PrintPattern(const int nPerLine = ALTRO_MAX_SAMPLES);
+ 
+ 
+ private:
+  AliHLTPHOSPattern(const AliHLTPHOSPattern &);
+  AliHLTPHOSPattern & operator = (const AliHLTPHOSPattern &); 
+  AliHLTPHOSPattern();
+
+  const int DoComparePattern(const int *inputPattern1, const int *inputPattern2, const int nsamples) const;
+  const bool CheckPatternLength(const int nsamples, const int nPresamples) const;
+  void SetPattern(const int *pattern, const int length);
+  int fVal[ALTRO_MAX_SAMPLES];
+  int fPatternLength;
+  int fN; /**<The number of detected patterns equal to fPattern*/ 
+  int fCnt; 
+  AliHLTPHOSPattern *fPattern;
+ };
+
+#endif
+
