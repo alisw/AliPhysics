94674a58376e555cb832036b7eeefea77fea61b2
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-06 15:50:33 +0000

    adding script to retrieve all datapoints for testing purposes

---
+++ SHUTTLE/schema/checkAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DETS="ACO EMC FMD HMP MCH MTR PHS CPV PMD SPD SDD SSD TOF TPC TRD T00 V00 ZDC GRP"
+
+for I in $DETS
+do
+        echo $I
+	aliroot -b -q TestDPs.C\(\"$I\",1210083077,1210083078\) > $I.out 2>&1
+done
+
