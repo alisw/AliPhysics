681fa27cfdf07d22c9058515780d84219d9246e0
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-06 22:12:01 +0000

    Adding new components to the linkdef file

--- HLT/PHOS/HLTPHOSLinkDef.h
+++ HLT/PHOS/HLTPHOSLinkDef.h
@@ -5,24 +5,25 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class AliHLTPHOSClusterizer; 
-#pragma link C++ class AliHLTPHOSEmcCalibData+; 
-#pragma link C++ class AliHLTPHOSPhysicsAnalyzer+; 
-#pragma link C++ class AliHLTPHOSPhysicsAnalyzerPeakFitter+; 
-#pragma link C++ class AliHLTPHOSPhysicsAnalyzerSpectrum+; 
-#pragma link C++ class AliHLTPHOSPulseGenerator+; 
-#pragma link C++ class AliHLTPHOSRawAnalyzerChiSquareFit+; 
-#pragma link C++ class AliHLTPHOSRawAnalyzerCrude+; 
-#pragma link C++ class AliHLTPHOSRawAnalyzerKLevel+; 
-#pragma link C++ class AliHLTPHOSRawAnalyzerLMS+; 
-#pragma link C++ class AliHLTPHOSRawAnalyzerPeakFinder+; 
-#pragma link C++ class AliHLTPHOSDigit+;
-#pragma link C++ class AliHLTPHOSDigitMaker+;
-#pragma link C++ class AliHLTPHOSBaseline+;
-#pragma link C++ class AliHLTPHOSSanityInspector+;
-#pragma link C++ class AliHLTPHOSBaselineAnalyzer+;
-#pragma link C++ class AliHLTPHOSESDMaker+;
-#pragma link C++ class AliHLTPHOSClusterAnalyser+;
+#pragma link C++ class AliHLTPHOSClusterizer;  
+#pragma link C++ class AliHLTPHOSEmcCalibData+;  
+#pragma link C++ class AliHLTPHOSPhysicsAnalyzer+;  
+#pragma link C++ class AliHLTPHOSPhysicsAnalyzerPeakFitter+;  
+#pragma link C++ class AliHLTPHOSPhysicsAnalyzerSpectrum+;  
+#pragma link C++ class AliHLTPHOSPulseGenerator+;  
+#pragma link C++ class AliHLTPHOSRawAnalyzerChiSquareFit+;  
+#pragma link C++ class AliHLTPHOSRawAnalyzerCrude+;  
+#pragma link C++ class AliHLTPHOSRawAnalyzerKLevel+;  
+#pragma link C++ class AliHLTPHOSRawAnalyzerLMS+;  
+#pragma link C++ class AliHLTPHOSRawAnalyzerPeakFinder+;  
+#pragma link C++ class AliHLTPHOSDigit+; 
+#pragma link C++ class AliHLTPHOSDigitMaker+; 
+#pragma link C++ class AliHLTPHOSBaseline+; 
+#pragma link C++ class AliHLTPHOSSanityInspector+; 
+#pragma link C++ class AliHLTPHOSBaselineAnalyzer+; 
+#pragma link C++ class AliHLTPHOSESDMaker+; 
+#pragma link C++ class AliHLTPHOSClusterAnalyser+; 
+
 //#pragma link C++ class AliHLTPHOSChannelBadness+;
 
 
