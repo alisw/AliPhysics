84cffeff8239d948e51af03342d49e09875fe35e
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-07 09:33:17 +0000

    Update by Sylwester

--- TRD/AliTRDqaBlackEvents.cxx
+++ TRD/AliTRDqaBlackEvents.cxx
@@ -26,7 +26,8 @@
 
 #include "TH1D.h"
 #include "TH2D.h"
-#include "TH3D.h"
+#include "TH2S.h"
+#include "TH3F.h"
 #include "TF1.h"
 #include "TFile.h"
 #include "TCanvas.h"
@@ -40,13 +41,17 @@
 
 ClassImp(AliTRDqaBlackEvents)
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 AliTRDqaBlackEvents::AliTRDqaBlackEvents() 
   :TObject() 
   ,fnEvents(0)
+  ,fCreateFull(0)
+  ,fThresh(0)
+  ,fCount(0)
   ,fOccupancy(0)
+  ,fDetRob(0)
+  ,fTBEvent(0)
   ,fFitType(0)
   ,fMinNoise(0.5)
   ,fMaxNoise(2) 
@@ -62,7 +67,12 @@ AliTRDqaBlackEvents::AliTRDqaBlackEvents()
 AliTRDqaBlackEvents::AliTRDqaBlackEvents(const AliTRDqaBlackEvents &qa) 
   :TObject(qa) 
   ,fnEvents(0)
+  ,fCreateFull(0)
+  ,fThresh(0)
+  ,fCount(0)
   ,fOccupancy(0)
+  ,fDetRob(0)
+  ,fTBEvent(0)
   ,fFitType(0)
   ,fMinNoise(0.5)
   ,fMaxNoise(2) 
@@ -86,9 +96,10 @@ void AliTRDqaBlackEvents::Init()
 
   fnEvents = 0;
 
-  for(Int_t i=0; i<540; i++) {
+  // histograms for chambers
+  for(Int_t i=0; i<kDET; i++) {
     fNPoint[i]  = new TH2D(Form("entries_%d", i), "",  16, -0.5, 15.5, 144, -0.5, 143.5);
-    fData[i]    = new TH3D(Form("data_%d", i), "", 16, -0.5, 15.5, 144, -0.5, 143.5, 50, -0.5, 49.5);
+    fData[i]    = new TH3F(Form("data_%d", i), "", 16, -0.5, 15.5, 144, -0.5, 143.5, 50, -0.5, 49.5);
     fChPed[i]   = new TH2D(Form("ped_%d", i), "", 16, -0.5, 15.5, 144, -0.5, 143.5);
     fChNoise[i] = new TH2D(Form("noise_%d", i), "", 16, -0.5, 15.5, 144, -0.5, 143.5);
     fPed[i]     = new TH1D(Form("pedDist_%d", i), ";pedestals (ADC counts)", 100, 5, 15);
@@ -99,7 +110,14 @@ void AliTRDqaBlackEvents::Init()
     fnEntriesRM[i] = new TH2D(Form("entriesRM_%d", i), ";ROB,MCM", 8, -0.5, 7.5, 16, -0.5, 15.5);
   }
 
+  // histogram for each MCM
+  for(Int_t i=0; i < kDET * kROB * kMCM; i++)
+    fFullCounter[i] = 0;
+
+  // histograms from the whole detector
   fOccupancy = new TH1D("occupancy", "", 20, -0.5, 19.5);
+  fDetRob    = new TH2D("DetRob", ";detector;ROB", kDET, -0.5, 539.5, 8, -0.5, 7.5);
+  fTBEvent   = new TH2D("tbEvent", ";event ID;time bin", 100, -0.5, 99.5, 30, -0.5, 29.5);
 
   //Info("Init", "Done");
 }
@@ -113,7 +131,7 @@ void AliTRDqaBlackEvents::Reset()
   // Resets the histograms
   //
 
-  for(Int_t i=0; i<540; i++) {
+  for(Int_t i=0; i<kDET; i++) {
     fData[i]->Reset();
     fChPed[i]->Reset();
     fChNoise[i]->Reset();
@@ -128,16 +146,17 @@ Int_t AliTRDqaBlackEvents::AddEvent(AliTRDrawStreamTB *data)
   // Add an event
   //
 
+  // structure to keep track if particular chanel is used
+  Char_t isUsed[kDET][kCOL][kPAD]; 
+  for(Int_t i=0; i<kDET; i++)
+    for(Int_t j=0; j<kCOL; j++)
+      for(Int_t k=0; k<kPAD; k++)
+   	isUsed[i][j][k] = 0;
   
-  Char_t isUsed[540][16][144]; 
-  for(Int_t i=0; i<540; i++)
-    for(Int_t j=0; j<16; j++)
-      for(Int_t k=0; k<144; k++)
-  	isUsed[i][j][k] = 0;
- 
   Int_t nb = 0;
   Int_t rob_last = -1;
   Int_t mcm_last = -1;
+  Int_t sm_01 = -1;
 
   while (data->Next()) {
 
@@ -148,43 +167,76 @@ Int_t AliTRDqaBlackEvents::AddEvent(AliTRDrawStreamTB *data)
 
     Int_t rob = data->GetROB();
     Int_t mcm = data->GetMCM();
+    Int_t adc = data->GetADC();
 
     Int_t *sig = data->GetSignals();
     nb++;
 
-    //printf("det = %d\n", det);
+    // ugly hook
+    if (det == 0 && row == 0 && col == 0) {
+      if (sm_01 > -1) printf("\t\t\t!!! !!! second data set !!! !!!\n");
+      sm_01++; 
+    }
+
+    det += sm_01 * 30;   /// ugly
+
+    // memory coruption protection
+    if (det<0 || det>=kDET) continue;
     
-    if (det<0 || det>=540) continue;
+    // check the ROBs
+    fDetRob->Fill(det, rob, 1./(kMCM*18));
 
     isUsed[det][row][col]++;
 
+    // check if mcm signal is continuus
     if ((rob_last != rob) || (mcm_last != mcm)) {
       rob_last = rob;
       mcm_last = mcm;
       fnEntriesRM[det]->Fill(rob,mcm);
     }
-
-    // if (!isUsed[det][data->GetRow()][data->GetCol()]) {
-    //  isUsed[det][data->GetRow()][data->GetCol()] = 1;
-    //  continue;
-    // }
-
+    
+    // number of entries for each channels
     fNPoint[det]->Fill(row, col);
     
-    for(Int_t k=0; k<30; k++) { /// to be corrected
+
+    // create a structure for an MCM if needed
+    Int_t mcmIndex = det * (kMCM * kROB) + rob * kMCM + mcm;
+    if (fCreateFull && !fFullSignal[mcmIndex])
+      fFullSignal[mcmIndex] = new TH2S(Form("mcm_%d_%d_%d", det, rob, mcm), 
+				       Form("mcm-%d-%d-%d;ADC;time bin", det, rob,mcm),
+				       21, -0.5, 20.5, 30, -0.5, 29.5);
+    
+
+    // loop over Time Bins and fill histograms
+    for(Int_t k=0; k<kTB; k++) { /// to be corrected
+
+      //if (sig[k] < 1) 
+      //printf("det = %d rob = %d mcm = %d adc = %d k = %d S = %d\n", det, rob, mcm, adc, k, sig[k]);
+      
       fSignal[det]->Fill(sig[k]);
       fData[det]->Fill(row, col, sig[k]);
+      
+      // check if data strange enought
+      if (fCreateFull && fFullSignal[mcmIndex]) {
+	if (sig[k] > fThresh || sig[k] < 1) fFullCounter[mcmIndex]++;
+	fFullSignal[mcmIndex]->Fill(adc, k, sig[k]);
+      }
+      
+      // noisy chamber
+      if (det == 29) {
+	fTBEvent->Fill(fnEvents, k, sig[k]);
+      }
+
     }
   }
   
-  for(Int_t i=0; i<540; i++) {
-    if (i != 0 && i != 8) continue;
-    for(Int_t j=0; j<16; j++)
-      for(Int_t k=0; k<144; k++)
+  // is the dead-alive status changing during the run
+  for(Int_t i=0; i<kDET; i++) {
+    for(Int_t j=0; j<kCOL; j++)
+      for(Int_t k=0; k<kPAD; k++)
   	fOccupancy->Fill(isUsed[i][j][k]);
   }
 
-  
   fnEvents++;
   return nb;
 }
@@ -197,13 +249,13 @@ void AliTRDqaBlackEvents::Process(const char *filename)
   // Process something
   //
 
-  Int_t map[540];
+  Int_t map[kDET];
   
   TH1D *hist = new TH1D("fitSignal", "", 50, -0.5, 49.5);
   TF1 *fit = new TF1("fit", "gaus(0)", 0, 20);
   fit->SetParameters(1e3, 10, 1);
     
-  for(Int_t i=0; i<540; i++) {
+  for(Int_t i=0; i<kDET; i++) {
     
     map[i] = 0;
     if (fData[i]->GetSum() < 10) continue;
@@ -261,7 +313,7 @@ void AliTRDqaBlackEvents::Process(const char *filename)
 
   // normalize number of entries histos
   Int_t max = 0;
-  for(Int_t i=0; i<540; i++) { 
+  for(Int_t i=0; i<kDET; i++) { 
     if (!map[i]) continue;
     for(Int_t j=0; j<fNPoint[i]->GetXaxis()->GetNbins(); j++) {
       for(Int_t k=0; k<fNPoint[i]->GetYaxis()->GetNbins(); k++) {
@@ -272,11 +324,14 @@ void AliTRDqaBlackEvents::Process(const char *filename)
     }
   }
   
-  for(Int_t i=0; i<540; i++) {
+  char entriesDistName[100];
+  
+  for(Int_t i=0; i<kDET; i++) {
     
     if (!map[i]) continue;
     
-    fNPointDist[i] = new TH1D(Form("entriesDist_%d", i), ";number of events", max+2, -0.5, max+1.5);
+    sprintf(entriesDistName, "entriesDist_%d", i);
+    fNPointDist[i] = new TH1D(entriesDistName, ";number of events", max+2, -0.5, max+1.5);
     
     for(Int_t j=0; j<fNPoint[i]->GetXaxis()->GetNbins(); j++) {
       for(Int_t k=0; k<fNPoint[i]->GetYaxis()->GetNbins(); k++) {
@@ -291,12 +346,14 @@ void AliTRDqaBlackEvents::Process(const char *filename)
   }
   
 
-  for(Int_t i=0; i<540; i++) {
+  for(Int_t i=0; i<kDET; i++) {
     fnEntriesRM[i]->SetMaximum(fnEvents * 1.5);
   }
 
-  TFile *file = new TFile(filename, "UPDATE");
-  for(Int_t i=0; i<540; i++) {
+  // save histograms
+
+  TFile *file = new TFile(filename, "recreate");
+  for(Int_t i=0; i<kDET; i++) {
     if (!map[i]) continue; 
     fChPed[i]->Write();
     fChNoise[i]->Write();
@@ -307,7 +364,20 @@ void AliTRDqaBlackEvents::Process(const char *filename)
     fSignal[i]->Write();
     fnEntriesRM[i]->Write();
   }
+
+  Int_t nMcm = 0;
+  for(Int_t i=0; i < kDET * kROB * kMCM; i++) {
+    if (fFullSignal[i] && fFullCounter[i] > fCount) {
+      fFullSignal[i]->Write();
+      nMcm++;
+    }
+  }
+
+  printf("Number of saved MCMs = %d\n", nMcm);
+
   fOccupancy->Write();
+  fDetRob->Write();
+  fTBEvent->Write();
   file->Close();
   delete file;
 }
--- TRD/AliTRDqaBlackEvents.h
+++ TRD/AliTRDqaBlackEvents.h
@@ -18,7 +18,8 @@
 
 class TH1D;
 class TH2D;
-class TH3D;
+class TH2S;
+class TH3F;
 class AliTRDrawStreamTB;
 
 class AliTRDqaBlackEvents : public TObject {
@@ -48,22 +49,53 @@ class AliTRDqaBlackEvents : public TObject {
   //void ScanChamber(const char *filename, Int_t first, Int_t last);
   void DrawSm(const char *filename, Int_t sm, Int_t w=900, Int_t h=700);
 
+  void SetFullView(Int_t v, Int_t thresh, Int_t count) {
+    fCreateFull = v;
+    fThresh = thresh;
+    fCount = count;
+  }
+
  private:
   
   Int_t fnEvents;         // number of events processed  
+  Int_t fCreateFull;      // flag if to create a full view
+  Int_t fThresh;          // threshold to analyze MCM data
+  Int_t fCount;           // minimum number of entries above threshold
+  
+  // geometry constants 
+  enum {
+    kDET = 540,
+    kROB = 8,
+    kMCM = 16,
+    kADC = 21,
+    kTB  = 30,
+    kCOL = 16,
+    kPAD = 144
+  };
+
+  // histograms per detector
 
   TH1D *fOccupancy;       // how many times is a pad present in data
+  TH2D *fDetRob;          // detector -- read out board
 
-  TH1D *fPed[540];        // reconstructed pedestals distribution (on hist per chamber)
-  TH1D *fNoise[540];      // reconstructed noise distribution (on hist per chamber)
-  TH1D *fNPointDist[540]; // distributin of the number of points
-  TH2D *fChPed[540];      // Some histograms
-  TH2D *fChNoise[540];    // Some histograms
-  TH2D *fNPoint[540];     // number of data points
-  TH3D *fData[540];       // Some histograms
-  TH1D *fSignal[540];     // Some histograms
-  TH2D *fnEntriesRM[540];     // number of entries for ROB - MCM
-  TH1D *fnEntriesRMDist[540]; // distribtion of number of entries per ROB-MCM
+  // histograms per chamber
+
+  TH1D *fPed[kDET];        // reconstructed pedestals distribution (on hist per chamber)
+  TH1D *fNoise[kDET];      // reconstructed noise distribution (on hist per chamber)
+  TH1D *fNPointDist[kDET]; // distributin of the number of points
+  TH2D *fChPed[kDET];      // Some histograms
+  TH2D *fChNoise[kDET];    // Some histograms
+  TH2D *fNPoint[kDET];     // number of data points
+  TH3F *fData[kDET];       // Some histograms
+  TH1D *fSignal[kDET];     // Some histograms
+  TH2D *fnEntriesRM[kDET];     // number of entries for ROB - MCM
+  TH1D *fnEntriesRMDist[kDET]; // distribtion of number of entries per ROB-MCM
+
+  TH2S *fFullSignal[kDET*kROB*kMCM];     // one histogram per MCM  
+  Short_t fFullCounter[kDET*kROB*kMCM];  // counts a number of entries with high signal
+  
+  TH2D *fTBEvent;    // coherent noise
+
 
   Int_t fFitType;
 
