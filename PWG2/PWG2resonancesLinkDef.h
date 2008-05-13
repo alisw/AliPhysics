a7b4884ddef3e659da411a223a532d9aff1de12b
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-13 16:15:18 +0000

    Changes in code and adding task

--- PWG2/PWG2resonancesLinkDef.h
+++ PWG2/PWG2resonancesLinkDef.h
@@ -4,18 +4,26 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class AliRsnParticle+;
 #pragma link C++ class AliRsnDaughter+;
+
+#pragma link C++ class AliRsnPID+;
 
 #pragma link C++ class AliRsnDaughterCut+;
 #pragma link C++ class AliRsnDaughterCutPt+;
+#pragma link C++ class AliRsnDaughterCutVt+;
 
 #pragma link C++ class AliRsnDaughterCutPair+;
 #pragma link C++ class AliRsnDaughterCutPairPt+;
+#pragma link C++ class AliRsnDaughterCutPairAngle+;
 #pragma link C++ class AliRsnDaughterCutPairArmenteros+;
 
 #pragma link C++ class AliRsnEvent+;
 #pragma link C++ class AliRsnReader+;
-#pragma link C++ class AliRsnSelectorRL+;
+#pragma link C++ class AliRsnReaderTask+;
+
+#pragma link C++ class AliRsnPair+;
+#pragma link C++ class AliRsnManager+;
 #pragma link C++ class AliRsnAnalysis+;
 
 #endif
