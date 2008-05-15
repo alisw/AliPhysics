a3f4150f6b7c8dbb8deccb0940e3d0291dfab1d7
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-15 07:13:23 +0000

    Moved to STEERBase

--- TDPMjet/TDPMjetLinkDef.h
+++ TDPMjet/TDPMjetLinkDef.h
@@ -7,7 +7,6 @@
 #pragma link C++ enum  DpmProcess_t;
 #pragma link C++ class TDPMjet+;
 #pragma link C++ class AliGenDPMjet+;
-#pragma link C++ class AliGenDPMjetEventHeader+;
 #pragma link C++ class AliIonPDGCodes+;
 
 #endif
