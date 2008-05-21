2d51ea07a263ca447d8f26c1e9998aef06912651
decaro <decaro@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-21 10:22:56 +0000

    Unused parameter: warning suppression

--- TOF/AliTOFQAChecker.h
+++ TOF/AliTOFQAChecker.h
@@ -28,8 +28,10 @@ public:
 
  protected:
 
-  virtual const Double_t Check(AliQA::ALITASK_t index, TObjArray * list) ;
-  virtual const Double_t Check(AliQA::ALITASK_t index) {return 0.;} ;
+  virtual const Double_t Check(AliQA::ALITASK_t /*index*/, TObjArray * list) ;
+  virtual const Double_t Check(AliQA::ALITASK_t /*index*/) {return 0.;} ;
+  virtual const Double_t Check() {return 0.;} ;
+  virtual const Double_t Check(TObjArray*) {return 0.;} ;
 
   
   ClassDef(AliTOFQAChecker,2)  // description 
