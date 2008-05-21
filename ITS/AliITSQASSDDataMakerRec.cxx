cead7787ab5984a144705f3dbd5fe769a133056e
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-21 11:20:19 +0000

    Bug fix.

--- ITS/AliITSQASSDDataMakerRec.cxx
+++ ITS/AliITSQASSDDataMakerRec.cxx
@@ -359,7 +359,7 @@ void AliITSQASSDDataMakerRec::MakeRaws(AliRawReader* rawReader) {
   for(Int_t i = 0; i < fgkNumOfLDCs; i++) {
     if(fSizePerLDC[i] > 0) {
       (fAliITSQADataMakerRec->GetRawsData(fGenOffset+21))->Fill(i+6);
-      (fAliITSQADataMakerRec->GetRawsData(fGenOffset+23+i))->Fill(TMath::Log10(fSizePerLDC[0]));
+      (fAliITSQADataMakerRec->GetRawsData(fGenOffset+23+i))->Fill(TMath::Log10(fSizePerLDC[i]));
     }
     (fAliITSQADataMakerRec->GetRawsData(fGenOffset+22))->Fill(i+6,fSizePerLDC[i]/1e+06);
   }
