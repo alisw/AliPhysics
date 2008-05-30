2e447e961cb45d348c270eb7c98b2be01cd109ba
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-30 09:56:34 +0000

    Modified to be used in trunk (Cynthia H.)

--- EMCAL/beamtest07/Rec.C
+++ EMCAL/beamtest07/Rec.C
@@ -1,26 +1,36 @@
-void Rec(TString file="/Users/jklay/Projects/LHC/alice/work/beamtest07/Period_LHC07a_EMCAL.Run_000000518.Host_001.Seq_10.root")
+void Rec(TString file="/scratch/alicehp2/commun/testbeam07/LHC07a_EMCAL/000000190/raw/07000000190001.10.root")
 {
+
+  // Modified for aliroot v4-13
+  // Cynthia Hadjidakis, 30 May 2008
   // Reconstruction of RAW data from the input file raw.root
   // Boris Polichtchouk, 31 Aug 2007
 
+
   AliCDBManager::Instance()->SetDefaultStorage("local://$ALICE_ROOT");
-  AliCDBManager::Instance()->SetSpecificStorage("EMCAL/Calib/Data","local://");
+  AliCDBManager::Instance()->SetSpecificStorage("EMCAL/Calib/Data","local://$ALICE_ROOT/EMCAL/beamtest07/");
 
   //AliLog::SetGlobalDebugLevel(2);
+  AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 1., 10., 1);
+  AliTracker::SetFieldMap(field,kFALSE);
 
   AliReconstruction rec ;
   rec.SetOption("EMCAL","OldRCUFormat");
+  rec.SetRunQA(kFALSE);       // bug with QA
   rec.SetRunTracking("") ;
-  rec.SetRunVertexFinder(kFALSE) ; 
+  rec.SetRunVertexFinder(kFALSE) ;
   rec.SetRunLocalReconstruction("EMCAL") ;
   rec.SetFillESD("EMCAL") ;
-
+  
   rec.SetInput(file.Data());  // read RAW data
 
-  rec.SetEventRange(0,10);
+  rec.SetEventRange(0,-1);
   rec.SetNumberOfEventsPerFile(-1);
-
   rec.Run();
 
+  delete rec;
+  gObjectTable->Print();
 
 }
+
+
