0c577884ce6bd844e05068cfb022ba3a53bbd58d
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-06-13 20:16:17 +0000

    rotation object replaced by pointer

--- TPC/AliTPCv2.cxx
+++ TPC/AliTPCv2.cxx
@@ -894,8 +894,8 @@ void AliTPCv2::CreateGeometry()
   //
   // resistor rods assembly
   //
-   TGeoRotation rotr("rotr");
-   rotr.RotateZ(-21.);
+   TGeoRotation* rotr = new TGeoRotation("rotr");
+   rotr->RotateZ(-21.);
   //
   TGeoTube *rri = new TGeoTube(1.8,2.2,126.64);//inner
   TGeoTube *rro = new TGeoTube(1.8,2.2,126.54);//inner
@@ -926,8 +926,8 @@ void AliTPCv2::CreateGeometry()
     z= 126.96;
     //
     if(i==3){
-      v9->AddNode(rrin,1,new TGeoCombiTrans(x,y,z,&rotr)); //A
-      v9->AddNode(rrin,2,new TGeoCombiTrans(x,y,-z,&rotr)); //C      
+	v9->AddNode(rrin,1,new TGeoCombiTrans(x,y, z, rotr)); //A
+	v9->AddNode(rrin,2,new TGeoCombiTrans(x,y,-z, rotr)); //C      
     } 
     else { 
       gGeoManager->Node("TPC_Rod",i+1,"TPC_Drift",x,y,z,0,kTRUE,upar,3);//shaft
@@ -943,12 +943,12 @@ void AliTPCv2::CreateGeometry()
     z=127.06;
     //
     if(i==15){
-      v9->AddNode(hvrv,1,new TGeoTranslation(x,y,z));//A-side only
-      gGeoManager->Node("TPC_Rod",i+55,"TPC_Drift",x,y,-z,0,kTRUE,upar,3);
+	v9->AddNode(hvrv,1,new TGeoTranslation(x,y,z));//A-side only
+	gGeoManager->Node("TPC_Rod",i+55,"TPC_Drift",x,y,-z,0,kTRUE,upar,3);
     }
     else if(i==11){
-      v9->AddNode(rrou,1,new TGeoCombiTrans(x,y,z,&rotr)); //A
-      v9->AddNode(rrou,2,new TGeoCombiTrans(x,y,-z,&rotr)); //C
+	v9->AddNode(rrou,1,new TGeoCombiTrans(x,y, z, rotr));  //A
+	v9->AddNode(rrou,2,new TGeoCombiTrans(x,y,-z, rotr));  //C
     }
     else{
     //
