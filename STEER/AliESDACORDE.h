5b7bb277f2922f1f2d873a38b69b171cb722e666
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-06-17 14:52:10 +0000

    Getters added.

--- STEER/AliESDACORDE.h
+++ STEER/AliESDACORDE.h
@@ -2,7 +2,9 @@
 #define AliESDACORDE_H
 
 #include <TObject.h>
-
+// Send comments to:
+// Mario Rodriguez <mrodrigu@mail.cern.ch>
+// Luciano Diaz <luciano.diaz@nucleares.unam.mx>
 class AliESDACORDE : public TObject
 {
 
@@ -12,11 +14,17 @@ class AliESDACORDE : public TObject
   AliESDACORDE(Int_t *ACORDESingleMuon,Int_t *ACORDEMultiMuon); 
   virtual ~AliESDACORDE() {};
   virtual void Copy(TObject &) const;
+
+ // We define the "setters" for ACORDE
+
   void SetACORDEMultiMuon(Bool_t ACORDEMultiMuon[60]){for(Int_t i=0;i<60;i++){fACORDEMultiMuon[i]=ACORDEMultiMuon[i];}}
   
   void SetACORDESingleMuon(Bool_t ACORDESingleMuon[60]){for(Int_t i=0;i<60;i++){fACORDESingleMuon[i]=ACORDESingleMuon[i];}} 
   
-	
+ // Getters  	
+
+  Int_t* GetACORDESingleMuon() const{return (int*) fACORDESingleMuon;}
+  Int_t* GetACORDEMultiMuon()  const{return (int*) fACORDEMultiMuon;}
   AliESDACORDE &operator=(const AliESDACORDE& source);
   
  protected:
@@ -24,7 +32,7 @@ class AliESDACORDE : public TObject
   Bool_t	fACORDESingleMuon[60];	// array with the Single Muon hits in the 60 Acorde's Modules 
   Bool_t	fACORDEMultiMuon[60];   // array with the Multi Muon hits in the 60 Acorde's Modules
 
-  ClassDef(AliESDACORDE,2)
+  ClassDef(AliESDACORDE, 2)
 
 };
 
