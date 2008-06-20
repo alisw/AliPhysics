79fdad8124d2f5f63be9c70059444f6983680f00
dibari <dibari@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-06-20 07:32:40 +0000

    Just clean-up

--- HMPID/AliHMPIDRecon.cxx
+++ HMPID/AliHMPIDRecon.cxx
@@ -28,27 +28,6 @@
 #include <TClonesArray.h>    //CkovAngle()
 #include <AliESDtrack.h>     //CkovAngle()
 
-  Int_t     fPhotCnt;                           // counter of photons candidate
-  Int_t    *fPhotFlag;                          // flags of photon candidates
-  Double_t *fPhotCkov;                          // Ckov angles of photon candidates, [rad]
-  Double_t *fPhotPhi;                           // phis of photons candidates, [rad]
-  Double_t *fPhotWei;                           // weigths of photon candidates
-  Double_t  fCkovSigma2;                        // sigma2 of the reconstructed ring
-
-  Bool_t    fIsWEIGHT;                          // flag to consider weight procedure
-  Float_t   fDTheta;                            // Step for sliding window
-  Float_t   fWindowWidth;                       // Hough width of sliding window
-  
-  Double_t  fRingArea;                          // area of a given ring
-  Double_t  fRingAcc;                           // fraction of the ring accepted by geometry
-  TVector3  fTrkDir;                            // track direction in LORS at RAD
-  TVector2  fTrkPos;                            // track positon in LORS at RAD
-  TVector2  fMipPos;                            // mip positon for a given track
-  TVector2  fPc;                                // track position at PC
-  
-  AliHMPIDParam *fParam;                        // Pointer to AliHMPIDParam
-
-
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 AliHMPIDRecon::AliHMPIDRecon():
   TTask("RichRec","RichPat"),
