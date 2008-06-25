88e210a051b8a9277e92af30022d315e7250bf08
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-06-25 09:59:19 +0000

    adding initialization of magnetic field (now required in AliReconstruction)

--- HLT/TPCLib/macros/rec-hlt-tpc.C
+++ HLT/TPCLib/macros/rec-hlt-tpc.C
@@ -4,13 +4,19 @@
  * AliRoot reconstruction. The reconstruction is done from the TPC raw
  * data.
  *
- * aliroot -b -q rec-hlt-tpc.C | tee rec-hlt-tpc.log
+ * Usage:
+ * <pre>
+ *   aliroot -b -q rec-hlt-tpc.C | tee rec-hlt-tpc.log
+ * </pre>
  *
  * The chain to be run is defined by the macro given to the parameter
  * 'config='
  *
- * The makro asumes that raw data is available in the rawx folders, either
- * simulated or real data.
+ * The macro asumes raw data to be available in the rawx folders, either
+ * simulated or real data. A different input can be specified as parameter
+ * <pre>
+ *   aliroot -b -q rec-hlt-tpc.C'("input.root")'
+ * </pre>
  *
  * In the first section, an analysis chain is defined. The scale of the
  * chain can be defined by choosing the range of sectors and partitions.
@@ -18,9 +24,16 @@
  * The reconstruction is steered by the AliReconstruction object in the
  * usual way.
  *
- * Matthias.Richter@ift.uib.no
+ * @ingroup alihlt_tpc
+ * @author Matthias.Richter@ift.uib.no
  */
+void rec_hlt_tpc(const char* input="./")
 {
+  if (!input) {
+    cerr << "please specify input or run without arguments" << endl;
+    return;
+  }
+
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   //
   // init the HLT system in order to define the analysis chain below
@@ -82,12 +95,14 @@
   // All but HLT reconstructio is switched off
   //
   AliReconstruction rec;
-  rec.SetInput("./");
+  rec.SetInput(input);
   rec.SetRunVertexFinder(kFALSE);
   rec.SetRunLocalReconstruction("HLT");
-  rec.SetRunReconstruction("");
   rec.SetRunTracking("");
   rec.SetLoadAlignFromCDB(0);
+  rec.SetRunQA(kFALSE);
+  AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 1., 10., AliMagFMaps::k5kG);
+  AliTracker::SetFieldMap(field,kTRUE);
   rec.SetFillESD("HLT");
   rec.SetOption("HLT", "libAliHLTUtil.so libAliHLTRCU.so libAliHLTTPC.so loglevel=0x7c chains=sink1");
   rec.Run();
