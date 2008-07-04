1a921af6e70d51c126345edd8d24c4e32c6a5c58
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-07-04 07:39:10 +0000

    Include cstdlib (gcc 4.3.0)

--- TRD/AliTRDSaxHandler.cxx
+++ TRD/AliTRDSaxHandler.cxx
@@ -24,6 +24,7 @@
 //                                                                        //
 ////////////////////////////////////////////////////////////////////////////
 
+#include <cstdlib>
 #include <Riostream.h>
 #include <TList.h>
 #include <TXMLAttr.h>
