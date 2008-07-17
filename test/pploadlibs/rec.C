2217f4451b35e1da53fe52d3115a9bb46529866b
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-07-17 10:12:23 +0000

    Bug fixes

--- test/pploadlibs/rec.C
+++ test/pploadlibs/rec.C
@@ -1,10 +1,8 @@
 void rec() {
-  if (!strcmp(gSystem->GetBuildArch(),"win32gcc")) {
-    gSystem->Load("libProof");
-    gSystem->Load("libGui");
-    gROOT->Macro("loadlibsrec.C");
-    new AliRun("gAlice","The ALICE Off-line Simulation Framework");
-  }
+  gSystem->Load("libProof");
+  gSystem->Load("libGui");
+  gROOT->Macro("loadlibsrec.C");
+  new AliRun("gAlice","The ALICE Off-line Simulation Framework");
 
   AliReconstruction reco;
 
