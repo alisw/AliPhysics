fefd9690c76d55cb60931d3208c7d7ea2daafe62
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-07-22 11:11:54 +0000

    Initial structure for PWG4JetTasks (for PWG4 Task Force) incl. steering macro and modified Makefile. First Task: Underlying Event studies

---
+++ PWG4/PROOF-INF.PWG4JetTasks/SETUP.C
@@ -0,0 +1,13 @@
+void SETUP()
+{
+
+   // Load the JET-Tasks library
+   gSystem->Load("libPWG4JetTasks");
+
+   // Set the Include paths
+   gSystem->AddIncludePath("-I$ROOTSYS/include -IPWG4");
+   gROOT->ProcessLine(".include PWG4");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("PWG4JetTasks_INCLUDE", "PWG4");
+}
