ae5870d177a3d71884c045929d89cce8ac2031f3
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-07-24 13:53:35 +0000

    corrected test macro according to r27483; extended test

--- HLT/BASE/test/testAliHLT_C_Component_WrapperInterface.C
+++ HLT/BASE/test/testAliHLT_C_Component_WrapperInterface.C
@@ -30,6 +30,7 @@
 const char* gDummy="dummy";
 AliHLTUInt32_t gRunNo=kAliHLTVoidRunNo;
 const char* gChainId="<void>";
+AliHLTComponentDataType gInputDt=kAliHLTVoidDataType;
 
 class TestProcessor : public AliHLTProcessor
 {
@@ -60,8 +61,19 @@ private:
     return 0;
   }
 
-  int DoEvent( const AliHLTComponentEventData& evtData, AliHLTComponentTriggerData& trigData) {
-    return 0;
+  int DoEvent( const AliHLTComponentEventData& /*evtData*/, AliHLTComponentTriggerData& /*trigData*/) {
+    int iResult=0;
+    HLTInfo("processing event");
+    for (const AliHLTComponentBlockData* pBlock=GetFirstInputBlock();
+	 pBlock!=NULL; 
+	 pBlock=GetNextInputBlock()) {
+      gInputDt=pBlock->fDataType;
+      AliHLTComponentDataType dt;
+      SetDataType(dt, "-OUTPUT-", "MYCO");
+      iResult=PushBack(pBlock->fPtr, pBlock->fSize/2, dt, ~pBlock->fSpecification);
+      Forward();
+    }
+    return iResult;
   }
 };
 
@@ -90,7 +102,7 @@ public:
 
 TestAgent gAgent;
 
-int Logging( void* param, 
+int Logging( void* /*param*/, 
 	     AliHLTComponentLogSeverity severity,
 	     const char* origin,
 	     const char* keyword,
@@ -98,6 +110,16 @@ int Logging( void* param,
 {
   cout << "Logging: "<< severity << " " << origin << " " << keyword << " " << message << endl;
   return 0;
+}
+
+void* AllocMemory( void* param, unsigned long size )
+{
+  if (param!=&gDummy) {
+    cerr << "AllocMemoryFunc callback with wrong parameter " << endl;
+    abort();
+  }
+  if (size==0) return NULL;
+  return new AliHLTUInt8_t[size];
 }
 
 const char* GetComponentDescription(void* param)
@@ -115,15 +137,16 @@ int main(int /*argc*/, const char** /*argv*/)
   AliHLTComponentEnvironment environment;
   memset(&environment, 0, sizeof(environment));
   environment.fStructSize=sizeof(environment);
+  environment.fAllocMemoryFunc=AllocMemory;
   environment.fLoggingFunc=Logging;
-  environment.fGetComponentDescription=GetComponentDescription;
+  //environment.fGetComponentDescription=GetComponentDescription;
   if ((iResult=AliHLT_C_Component_InitSystem( &environment ))<0) {
-    cerr << "AliHLT_C_Component_InitSystem failed with " << iResult << endl;
+    cerr << "error: AliHLT_C_Component_InitSystem failed with " << iResult << endl;
     return iResult;
   }
 
   if ((iResult=AliHLT_C_Component_LoadLibrary("libAliHLTUtil.so"))<0) {
-    cerr << "AliHLT_C_Component_LoadLibrary failed with " << iResult << endl;
+    cerr << "error: AliHLT_C_Component_LoadLibrary failed with " << iResult << endl;
     return iResult;
   }
 
@@ -132,25 +155,105 @@ int main(int /*argc*/, const char** /*argv*/)
   desc.fStructSize=sizeof(desc);
   desc.fRunNo=0xbeef;
   if ((iResult=AliHLT_C_SetRunDescription(&desc, "dummy run"))<0) {
-    cerr << "AliHLT_C_Component_SetRunDescription failed with " << iResult << endl;
+    cerr << "error: AliHLT_C_Component_SetRunDescription failed with " << iResult << endl;
     return iResult;
   }
 
   AliHLTComponentHandle handle;
   if ((iResult=AliHLT_C_CreateComponent("TestProcessor", &gDummy, 0, NULL, &handle ))<0) {
-    cerr << "AliHLT_C_Component_CreateComponent failed with " << iResult << endl;
+    cerr << "error: AliHLT_C_Component_CreateComponent failed with " << iResult << endl;
     return iResult;
   }
 
   if (gRunNo!=0xbeef) {
-    cerr << "propagation of run number failed " << hex << gRunNo << " vs. 0xbeef" << endl;
+    cerr << "error: propagation of run number failed " << hex << gRunNo << " vs. 0xbeef" << endl;
     return -1;
   }
 
-  if (strcmp(gChainId, "test")) {
-    cerr << "propagation of chain id failed: '" << gChainId << "' vs. test" << endl;
+  // can be used in the new interface again
+//   if (strcmp(gChainId, "test")) {
+//     cerr << "propagation of chain id failed: '" << gChainId << "' vs. test" << endl;
+//     return -1;
+//   }
+
+
+  const char* inputData="some data to be copied";
+  AliHLTComponentBlockData inputBlock;
+  memset(&inputBlock, 0, sizeof(inputBlock));
+  inputBlock.fStructSize=sizeof(inputBlock);
+  inputBlock.fPtr=(void*)inputData;
+  inputBlock.fSize=strlen(inputData);
+  inputBlock.fDataType=kAliHLTDataTypeDDLRaw;
+  inputBlock.fSpecification=0xdead;
+
+  AliHLTComponentEventData evtData;
+  memset(&evtData, 0, sizeof(evtData));
+  evtData.fStructSize=sizeof(evtData);
+  evtData.fBlockCnt=1;
+
+  AliHLTComponentTriggerData trigData;
+  memset(&trigData, 0, sizeof(trigData));
+  trigData.fStructSize=sizeof(trigData);
+
+  AliHLTUInt8_t outputPtr[100];
+  AliHLTUInt32_t size=sizeof(outputPtr);
+
+  AliHLTUInt32_t outputBlockCnt=0;
+  AliHLTComponentBlockData* outputBlocks=NULL;
+  AliHLTComponentEventDoneData* edd=NULL;
+
+  if ((iResult=AliHLT_C_ProcessEvent( handle, &evtData, &inputBlock, &trigData, outputPtr,
+				      &size, &outputBlockCnt, &outputBlocks, &edd ))<0) {
+    cerr << "error: AliHLT_C_Component_ProcessEvent failed with " << iResult << endl;
+    return iResult;
+  }
+
+  if (outputBlockCnt<2) {
+    cerr << "error: mismatch in output block count, expecting >2, got " << outputBlockCnt << endl;
     return -1;
   }
+
+  if (outputBlocks==NULL) {
+    cerr << "error: did not get output block array " << endl;
+    return -1;
+  }
+
+  AliHLTComponentDataType outdt;
+  AliHLTComponent::SetDataType(outdt, "-OUTPUT-", "MYCO");
+  bool bHaveForwarded=false;
+  bool bHaveCopied=false;
+  for (unsigned int i=0; i<outputBlockCnt; i++) {
+    if (outputBlocks[i].fDataType==kAliHLTDataTypeDDLRaw) {
+      if (outputBlocks[i].fPtr!=inputData ||
+	  outputBlocks[i].fSize!=strlen(inputData)) {
+	cerr << "error: failed comparing forwarded input block" << endl;
+	return -1;
+      }
+      bHaveForwarded=true;
+    } else if (outputBlocks[i].fDataType==outdt) {
+      if (outputBlocks[i].fSize!=strlen(inputData)/2) {
+	cerr << "error: wrong size of copied block" << endl;
+	return -1;
+      }
+      if (memcmp(inputData, outputPtr+outputBlocks[i].fOffset, outputBlocks[i].fSize)) {
+	cerr << "error: failed comparing copied block" << endl;
+	return -1;
+      }
+      bHaveCopied=true;
+    }
+  }
+
+  if (!bHaveForwarded) {
+    cerr << "error: did not get forwarded data block" << endl;
+    return -1;
+  }
+
+  if (!bHaveCopied) {
+    cerr << "error: did not get copied data block" << endl;
+    return -1;
+  }
+
+  AliHLT_C_DestroyComponent(handle);
 
   if ((iResult=AliHLT_C_Component_DeinitSystem( ))<0) {
     cerr << "AliHLT_C_Component_DeinitSystem failed with " << iResult << endl;
