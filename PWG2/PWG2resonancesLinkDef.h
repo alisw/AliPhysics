c7cbe50d86df5be2c0e641d2dd1505dbf3eea527
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-07-30 17:56:44 +0000

    Updated accordingly the module and linkDef files

--- PWG2/PWG2resonancesLinkDef.h
+++ PWG2/PWG2resonancesLinkDef.h
@@ -5,28 +5,31 @@
 #pragma link C++ class AliRsnPairParticle+;
 
 #pragma link C++ class AliRsnCut+;
-#pragma link C++ class AliRsnVariableExpression+;
-#pragma link C++ class AliRsnExpression+;
 #pragma link C++ class AliRsnCutSet+;
 #pragma link C++ class AliRsnCutMgr+;
+#pragma link C++ class AliRsnExpression+;
+#pragma link C++ class AliRsnVariableExpression+;
 
 #pragma link C++ class AliRsnPID+;
-#pragma link C++ class AliRsnPIDWeightsMgr+;
 #pragma link C++ class AliRsnPIDIndex+;
+#pragma link C++ class AliRsnPIDWeightsMgr+;
 
 #pragma link C++ class AliRsnEvent+;
+#pragma link C++ class AliRsnEventBuffer+;
+
 #pragma link C++ class AliRsnReader+;
 #pragma link C++ class AliRsnReaderTaskSE+;
 
 #pragma link C++ class AliRsnPairDef+;
-#pragma link C++ class AliRsnPairSimple+;
+#pragma link C++ class AliRsnHistoDef+;
 
-#pragma link C++ class AliRsnEventBuffer+;
-#pragma link C++ class AliRsnAnalysisSimple+;
-#pragma link C++ class AliRsnAnalyzerSimple+;
-#pragma link C++ class AliRsnAnalysisSimpleTask+;
+#pragma link C++ class AliRsnSimpleFunction+;
+#pragma link C++ class AliRsnSimpleFcnInvMass+;
+#pragma link C++ class AliRsnSimpleFcnResolution+;
 
-//#pragma link C++ class AliRsnPair+;
-//#pragma link C++ class AliRsnBaseAT+;
-//#pragma link C++ class AliRsnAnalysisAT+;
+#pragma link C++ class AliRsnSimpleAnalysis+;
+#pragma link C++ class AliRsnSimpleAnalyzer+;
+#pragma link C++ class AliRsnSimpleAnalysisTask+;
+#pragma link C++ class AliRsnSimpleAnalysisTaskSE+;
+
 #endif
