89676e3a79ee8c1170780483d4b0910daf5b3276
aszostak <aszostak@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-08-12 16:32:58 +0000

    Replacing very old (and deprecated) class with first version of a HLT trigger base class.

--- HLT/trigger/AliHLTTrigger.cxx
+++ HLT/trigger/AliHLTTrigger.cxx
@@ -1,173 +1,73 @@
+/**************************************************************************
+ * This file is property of and copyright by the ALICE HLT Project        *
+ * ALICE Experiment at CERN, All rights reserved.                         *
+ *                                                                        *
+ * Primary Authors: Artur Szostak <artursz@iafrica.com>                   *
+ *                  for The ALICE HLT Project.                            *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
 
 #include "AliHLTTrigger.h"
-#include "AliHLTTrackArray.h"
-#include "AliHLTTransform.h"
-#include "AliHLTVertex.h"
-#include "AliHLTDefs.h"
-#include "AliHLTDigitData.h"
-#include "AliHLTLogging.h"
-#include "AliHLTTrack.h"
-#include "AliHLTMemHandler.h"
 
 ClassImp(AliHLTTrigger)
 
-AliHLTTrigger::AliHLTTrigger()
+
+AliHLTTrigger::AliHLTTrigger() :
+	AliHLTProcessor(),
+	fEventData(NULL),
+	fTriggerData(NULL),
+	fDecisionMade(false),
+	fTriggerEventResult(0)
 {
-  fDataSize=0;
-  fTracks=0;
-  fDigitRowData=0;
-  fOutput=0;
-  fVertex=0;
+  // Default constructor sets pointers to NULL.
 }
+
 
 AliHLTTrigger::~AliHLTTrigger()
 {
-  if(fTracks)
-    delete fTracks;
+  // Default destructor.
 }
 
-void AliHLTTrigger::InitTrigger()
-{
-  if(fTracks)
-    delete fTracks;
-  fTracks = new AliHLTTrackArray();
-}
 
-void AliHLTTrigger::InitPatch(Int_t slice,Int_t patch)
+int AliHLTTrigger::DoEvent(const AliHLTComponentEventData& evtData, AliHLTComponentTriggerData& trigData)
 {
-  fSlice=slice;
-  fPatch=patch;
-  fTracks->Reset();
-}
+  /// Sets the pointers to the evtData and trigData, then calls the DoTrigger to
+  /// execute the actual trigger algorithm.
 
-void AliHLTTrigger::FillTracks(Int_t ntracks,AliHLTTrackSegmentData *tr)
-{
-  fTracks->FillTracks(ntracks,tr);
-}
-
-void AliHLTTrigger::FillData(AliHLTDigitRowData *data)
-{
-  fDigitRowData = data;
-}
-
-void AliHLTTrigger::SetParameters(Float_t zcut,Int_t timematch,Int_t padmatch)
-{
-  fZcut=zcut;
-  fTimeMatch=timematch;
-  fPadMatch=padmatch;
-}
-
-void AliHLTTrigger::SetOutputData(AliHLTDigitRowData *ptr)
-{
-  fOutput=ptr;
-}
-
-void AliHLTTrigger::RemovePileupTracks()
-{
-  Double_t xc,yc,zc;
-  for(Int_t i=0; i<fTracks->GetNTracks(); i++)
-    {
-      AliHLTTrack *track = fTracks->GetCheckedTrack(i);
-      if(!track) continue;
-      track->Rotate(fSlice,kTRUE);
-      track->CalculateHelix();
-      track->GetClosestPoint(fVertex,xc,yc,zc);
-      if(fabs(zc) > fZcut)
-	{
-	  fTracks->Remove(i);
-	  continue;
-	}
-    }
-  fTracks->Compress();
-}
-
-void AliHLTTrigger::RemovePileupData()
-{
-  Float_t hit[3];
-  Int_t sector,row;
-  struct rowhit {Int_t pad; Int_t time;};
-  rowhit row_cross[(fTracks->GetNTracks())];
-  Int_t digitcount[(NumRows[fPatch])];
-  Int_t totalcount=0;
-  AliHLTDigitRowData *rowPt = fDigitRowData;
-  for(Int_t i=NRows[fPatch][0]; i<=NRows[fPatch][1]; i++)
-    {
-      digitcount[(i-NRows[fPatch][0])]=0;
-      for(Int_t j=0; j<fTracks->GetNTracks(); j++)
-	{
-	  AliHLTTrack *track = fTracks->GetCheckedTrack(j);
-	  if(!track) continue;
-	  track->GetCrossingPoint(i,hit);
-	  AliHLTTransform::Slice2Sector(fSlice,i,sector,row);
-	  AliHLTTransform::Local2Raw(hit,sector,row);
-	  row_cross[j].pad = (Int_t)rint(hit[1]);
-	  row_cross[j].time = (Int_t)rint(hit[2]);
-	}
-      AliHLTDigitData *digPt = (AliHLTDigitData*)rowPt->fDigitData;
-      Bool_t mark;
-      for(Int_t k=0; k<rowPt->fNDigit; k++)
-	{
-	  mark = kFALSE;
-	  for(Int_t l=0; l<fTracks->GetNTracks(); l++)
-	    {
-	      if(abs((Int_t)digPt[k].fPad-row_cross[l].pad) < fPadMatch &&
-		 abs((Int_t)digPt[k].fTime-row_cross[l].time) < fTimeMatch)
-		{
-		  digitcount[(i-NRows[fPatch][0])]++;
-		  totalcount++;
-		  mark=kTRUE;
-		  break;
-		}
-	    }
-	  if(mark==kTRUE)
-	    digPt[k].fCharge=0;
-	}
-      AliHLTMemHandler::UpdateRowPointer(rowPt);
-    }
+  fEventData = &evtData;
+  fTriggerData = &trigData;
+  fDecisionMade = false;
+  fTriggerEventResult = 0;
   
-  Int_t size = totalcount*sizeof(AliHLTDigitData) + NumRows[fPatch]*sizeof(AliHLTDigitRowData);
-  fDataSize = size;
-  LOG(AliHLTLog::kDebug,"AliHLTTrigger::RemovePileupData","Memory")
-    <<"Allocating "<<size<<" bytes of data for trigger event"<<ENDLOG;
-  Byte_t *data = new Byte_t[size];
-  memset(data,0,size);
-  AliHLTDigitRowData *tempPt = (AliHLTDigitRowData*)data;
-  rowPt = fDigitRowData;
+  int result = DoTrigger();
+  if (result != 0) return result;
   
-  Int_t localcount;
-  for(Int_t i=NRows[fPatch][0]; i<=NRows[fPatch][1]; i++)
-    {
-      tempPt->fRow = i;
-      tempPt->fNDigit = digitcount[(i-NRows[fPatch][0])];
-      AliHLTDigitData *digPt = (AliHLTDigitData*)rowPt->fDigitData;
-      localcount=0;
-      for(Int_t j=0; j<rowPt->fNDigit; j++)
-	{
-	  if(digPt[j].fCharge==0) continue;
-	  if(localcount >= digitcount[(i-NRows[fPatch][0])])
-	    {
-	      LOG(AliHLTLog::kFatal,"AliL§Trigger::RemovePileupData","Array")
-		<<"Mismatch in digitcount: "<<localcount<<" "<<digitcount[(i-NRows[fPatch][0])]<<ENDLOG;
-	      return;
-	    }
-	  tempPt->fDigitData[localcount].fCharge=digPt[j].fCharge;
-	  tempPt->fDigitData[localcount].fPad=digPt[j].fPad;
-	  tempPt->fDigitData[localcount].fTime=digPt[j].fTime;
-	  localcount++;
-	}
-      if(digitcount[(i-NRows[fPatch][0])]!=localcount)
-	{
-	  LOG(AliHLTLog::kFatal,"AliL§Trigger::RemovePileupData","Array")
-	    <<"Mismatch in digitcount: "<<localcount<<" "<<digitcount[(i-NRows[fPatch][0])]<<ENDLOG;
-	}
-      AliHLTMemHandler::UpdateRowPointer(rowPt);
-      Byte_t *tmp = (Byte_t*)tempPt;
-      Int_t size = sizeof(AliHLTDigitRowData)+digitcount[(i-NRows[fPatch][0])]*sizeof(AliHLTDigitData);
-      tmp += size;
-      tempPt = (AliHLTDigitRowData*)tmp;
-    }
-  
-  fOutput=(AliHLTDigitRowData*)data;
+  // Fill in a default decision of false if none was made.
+  if (not fDecisionMade)
+  {
+    TriggerEvent(false);
+  }
+  // Cleanup
+  fEventData = NULL;
+  fTriggerData = NULL;
+  return fTriggerEventResult;
 }
 
+
+void AliHLTTrigger::TriggerEvent(bool value)
+{
+  /// Sets the trigger decision for the current event.
+  
+  if (fTriggerEventResult != 0) return;  // Do not do anything if a previous call failed.
+  TObjString triggerResult(GetTriggerName());
+  triggerResult.SetBit(BIT(14), value);  // Use bit 14 for the boolean decision.
+  fTriggerEventResult = PushBack(triggerResult, kAliHLTDataTypeTObject|kAliHLTDataOriginOut);
+}
 
--- HLT/trigger/AliHLTTrigger.h
+++ HLT/trigger/AliHLTTrigger.h
@@ -1,50 +1,139 @@
-// @(#) $Id$
+#ifndef ALIHLTTRIGGER_H
+#define ALIHLTTRIGGER_H
+/* This file is property of and copyright by the ALICE HLT Project        * 
+ * ALICE Experiment at CERN, All rights reserved.                         *
+ * See cxx source for full Copyright notice                               */
 
-#ifndef AliHLT_Trigger
-#define AliHLT_Trigger
+#include "AliHLTProcessor.h"
 
-#include "AliHLTRootTypes.h"
-
-class AliHLTTrackSegmentData;
-class AliHLTDigitRowData;
-class AliHLTTrackArray;
-class AliHLTVertex;
-
-class AliHLTTrigger {
- 
- private:
-  AliHLTTrackArray *fTracks; //!
-  AliHLTDigitRowData *fDigitRowData; //!
-  AliHLTDigitRowData *fOutput; //!
-  AliHLTVertex *fVertex; //!
-  Int_t fDataSize;
-
-  Float_t fZcut;
-  Int_t fTimeMatch;
-  Int_t fPadMatch;
-  Int_t fSlice;
-  Int_t fPatch;
-
+class AliHLTTrigger : public AliHLTProcessor
+{
  public:
+ 
   AliHLTTrigger();
   virtual ~AliHLTTrigger();
+
+  /**
+   * Returns the name of the trigger. This must be unique across the system.
+   * This method is pure virtual and must be implemented by child classes.
+   * @return string containing the trigger name.
+   */
+  virtual const char* GetTriggerName() const = 0;
   
-  void InitTrigger();
-  void InitPatch(Int_t slice,Int_t patch);
-  void FillTracks(Int_t ntracks,AliHLTTrackSegmentData *tr);
-  void FillData(AliHLTDigitRowData *data);
-  void SetOutputData(AliHLTDigitRowData *ptr);
-  void SetVertex(AliHLTVertex *vertex) {fVertex = vertex;}
-  void SetParameters(Float_t zcut,Int_t timematch,Int_t padmatch);
-  void RemovePileupTracks();
-  void RemovePileupData();
+  /**
+   * Inherited from AliHLTComponent. Returns the name of the trigger by default.
+   * @return string containing the trigger name as the component ID.
+   */
+  virtual const char* GetComponentID() { return GetTriggerName(); };
+
+  /**
+   * Get the input data types of the component.
+   * This method returns kAliHLTAnyDataType by default.
+   * @param list <i>[out]</i>: The list of data types to be filled.
+   */
+  virtual void GetInputDataTypes(AliHLTComponentDataTypeList& list) const
+  {
+    list.push_back(kAliHLTAnyDataType);
+  }
+
+  /**
+   * Returns extra output data types this trigger generates.
+   * This returns an empty list by default.
+   * @param list <i>[out]</i>: The list of data types to be filled.
+   */
+  virtual void GetOutputDataTypes(AliHLTComponentDataTypeList& list) const
+  {
+    list.push_back(kAliHLTDataTypeTObject);
+  }
+
+  /**
+   * Get a ratio by how much the data volume is shrinked or enhanced.
+   * The method returns a size proporional to the trigger name string length
+   * for constBase, and 1 for inputMultiplier.
+   * @param constBase        <i>[out]</i>: additive part, independent of the
+   *                                   input data volume  
+   * @param inputMultiplier  <i>[out]</i>: multiplication ratio
+   */
+  virtual void GetOutputDataSize(unsigned long& constBase, double& inputMultiplier)
+  {
+    constBase = strlen(GetTriggerName()) + sizeof(TObjString) + 1;
+    inputMultiplier = 1;
+  }
+
+ protected:
+
+  /**
+   * This method needs to be implemented by child classes to implement the actual
+   * trigger algorithm. A possitive trigger decision is made by calling the TriggerEvent
+   * method with TriggerEvent(true), or TriggerEvent(false) for a negative result
+   * (no trigger).
+   * If the AliHLTComponentEventData structure is needed for the current event being
+   * processed then the GetEventData method can be used to fetch it.
+   * Similarly, the AliHLTComponentTriggerData structure can be fetched with a call
+   * to GetTriggerData.
+   * @return Zero should be returned on success and a negative error code, which is
+   *     compatible with the AliHLTSystem framework, on failure.
+   */
+  virtual int DoTrigger() = 0;
   
-  Int_t GetDataSize() {return fDataSize;}
+  /**
+   * Fills the output with the trigger decision. This should be called in the DoTrigger
+   * method when a trigger decision has been made.
+   * @param value  The trigger decision value. True for positive trigger and false
+   *     for a negative result. (true by default)
+   */
+  void TriggerEvent(bool value = true);
   
-  ClassDef(AliHLTTrigger,1) 
+  /**
+   * Returns the event data structure for the current event.
+   * NULL is returned if this method is not called inside the DoTrigger method.
+   */
+  const AliHLTComponentEventData* GetEventData() const { return fEventData; }
+  
+  /**
+   * Returns the trigger data structure for the current event.
+   * NULL is returned if this method is not called inside the DoTrigger method.
+   */
+  AliHLTComponentTriggerData* GetTriggerData() const { return fTriggerData; }
+
+ private:
+ 
+  /**
+   * Inherited from AliHLTComponent. This method will clear the fDecisionMade flag,
+   * remember the evtData and trigData pointers, then call DoTrigger to invoke the
+   * actual trigger algorithm.
+   */
+  virtual int DoEvent(const AliHLTComponentEventData& evtData, AliHLTComponentTriggerData& trigData);
+  
+  using AliHLTProcessor::DoEvent;
+ 
+  /**
+   * Inherited from AliHLTComponent. This method is deprecated so hide it.
+   * @return kAliHLTMultipleDataType is returned.
+   */
+  virtual AliHLTComponentDataType GetOutputDataType() { return kAliHLTMultipleDataType; };
+
+  /**
+   * Inherited from AliHLTComponent. This method is replaced with one that is
+   * symmetric to GetInputDataTypes that returns void, so we make this method
+   * private.
+   * @param list     list to receive the output data types.
+   * @return the number of elements in the list.
+   */
+  virtual int GetOutputDataTypes(AliHLTComponentDataTypeList& list)
+  {
+    GetOutputDataTypes(list);
+    return list.size();
+  }
+  
+  const AliHLTComponentEventData* fEventData; ///! Event data for the current event. Only valid inside DoTrigger.
+  AliHLTComponentTriggerData* fTriggerData; ///! Trigger data for the current event. Only valid inside DoTrigger.
+  bool fDecisionMade;  ///! Flag indicating if the trigger decision has been made for this trigger yet.
+  int fTriggerEventResult;  ///! Result returned by PushBack method in the TriggerEvent method.
+  
+  ClassDef(AliHLTTrigger, 0) // Base class for HLT triggers.
 
 };
 
-typedef AliHLTTrigger AliL3Trigger; // for backward compatibility
+#endif // ALIHLTTRIGGER_H
 
-#endif
