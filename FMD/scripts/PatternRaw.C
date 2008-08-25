466b76a16c5ea382cfb3cc9bcf307f7095ec5adc
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-08-25 14:47:45 +0000

    Added the specialised FMD display for raws (cholm)

---
+++ FMD/scripts/PatternRaw.C
@@ -0,0 +1,31 @@
+//____________________________________________________________________
+//
+// $Id: PatternDigits.C 28055 2008-08-18 00:33:20Z cholm $
+//
+// Draw hits in the specialised FMD event display 
+//
+/** Display hits 
+    @ingroup FMD_script
+ */
+void
+PatternRaw(const char* file="raw.root")
+{
+  AliLog::SetModuleDebugLevel("FMD", 3);
+  AliCDBManager* cdb = AliCDBManager::Instance();
+  cdb->SetDefaultStorage("local://$ALICE_ROOT");
+  cdb->SetRun(0);
+  AliFMDParameters::Instance()->Init();
+  gSystem->Load("libFMDutil.so");
+  AliFMDPattern* d = new AliFMDPattern;
+  d->AddLoad(AliFMDInput::kRaw);
+  d->SetRawFile(file);
+  d->SetName("raw");
+  d->SetTitle("Raw");
+  // d->AddLoad(AliFMDInput::kKinematics);
+  d->Run();
+}
+
+//____________________________________________________________________
+//
+// EOF
+//
