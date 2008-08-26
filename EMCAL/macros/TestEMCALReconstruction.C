aaf7d011d667c2abd59a73c7e7560f75c6afeee5
jklay <jklay@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-08-26 16:56:57 +0000

    minor updates to rec macro

--- EMCAL/macros/TestEMCALReconstruction.C
+++ EMCAL/macros/TestEMCALReconstruction.C
@@ -8,17 +8,26 @@ LLNL
 */
 
 
-void TestEMCALReconstruction(Int_t nev =10) {
+void TestEMCALReconstruction(Int_t nev =-1) {
+
+  //  AliLog::SetModuleDebugLevel("EMCAL",100);
 
   AliReconstruction rec;
 
   rec.SetRunTracking("");
   rec.SetRunVertexFinder(kFALSE);
+
   //calls local reconstruction of EMCAL and filling of ESD
   rec.SetRunLocalReconstruction("EMCAL");  //only do emcal
   rec.SetFillESD("EMCAL");
   rec.SetEventRange(0,nev);
-  rec.SetInput("raw.root");
+  //rec.SetInput("raw.root");
+  //rec.SetRunQA(":");
+
+  // **** The field map settings must be the same as in Config.C !
+  AliMagFMaps *field=new AliMagFMaps("Maps","Maps",2,1.,10.,AliMagFMaps::k5kG);
+  Bool_t uniform=kFALSE;
+  AliTracker::SetFieldMap(field,uniform);
 
   TStopwatch timer;
   timer.Start();
@@ -26,5 +35,6 @@ void TestEMCALReconstruction(Int_t nev =10) {
   rec.Run();
   timer.Stop();
   timer.Print();
+  gObjectTable->Print();
 
 }
