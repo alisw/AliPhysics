e4ed99b114741ea78c703f0f09588bd80229f81e
cheynis <cheynis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-08-27 13:28:03 +0000

    Conversion of DCS mapping to aliroot mapping

--- VZERO/AliVZERODataDCS.h
+++ VZERO/AliVZERODataDCS.h
@@ -42,8 +42,22 @@ public:
   
   void Draw(const Option_t* option);
   
-  Float_t* GetMeanHV()   const {return (float*)fMeanHV;}
+  Float_t* GetMeanHV()    const {return (float*)fMeanHV;}
   Float_t* GetWidthHV()   const {return (float*)fWidthHV;}
+
+// Getter of Offline Channel number as used in aliroot (defined by aliroot 
+// numbering convention) from DCS Channel number
+
+    Int_t      GetOfflineChannel(Int_t channel)  const
+      { Int_t  fOfflineChannel[64] = {32, 33, 34, 35, 36, 37, 38, 39, 
+                                      40, 41, 42, 43, 44, 45, 46, 47, 
+			              48, 49, 50, 51, 52, 53, 54, 55, 
+			              56, 57, 58, 59, 60, 61, 62, 63,
+			               0,  1,  2,  3,  4,  5,  6,  7, 
+			               8,  9, 10, 11, 12, 13, 14, 15,
+			              16, 17, 18, 19, 20, 21, 22, 23, 
+			              24, 25, 26, 27, 28, 29, 30, 31};
+               return fOfflineChannel[channel]; }
 
 private:
   AliVZERODataDCS(const AliVZERODataDCS&); // Not implemented
