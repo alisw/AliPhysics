d89abd0d661d37cae6f32e91e86686696b1a9d0a
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-09-02 08:09:06 +0000

    Class for pi0 (two-photon invariant mass) analysis. Yuri Kharlov <Yuri.Kharlov@cern.ch>, Boris Polishchuk <boris.polichtchouk@cern.ch>

--- PWG4/PWG4PartCorrLinkDef.h
+++ PWG4/PWG4PartCorrLinkDef.h
@@ -16,6 +16,8 @@
 #pragma link C++ class AliAnaPartCorrMaker+;
 #pragma link C++ class AliAnaPartCorrBaseClass+;
 #pragma link C++ class AliAnaExample+;
+#pragma link C++ class AliAnaPi0+;
+#pragma link C++ class AliCaloPhoton+;
 #pragma link C++ class AliAnaGammaDirect+;
 #pragma link C++ class AliAnaParticlePartonCorrelation+;
 #pragma link C++ class AliAnaParticleHadronCorrelation+;
