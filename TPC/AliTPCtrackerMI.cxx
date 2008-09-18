8ae761498caee95b367b6c90d6af8831fca124d0
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-09-18 12:30:18 +0000

    Use debug streamer only if requested

--- TPC/AliTPCtrackerMI.cxx
+++ TPC/AliTPCtrackerMI.cxx
@@ -2662,13 +2662,15 @@ Int_t AliTPCtrackerMI::RefitInward(AliESDEvent *event)
       AliExternalTrackParam paramIn;
       AliExternalTrackParam paramOut;
       Int_t ncl = seed->RefitTrack(seed,&paramIn,&paramOut);
-      (*fDebugStreamer)<<"RecoverIn"<<
-	"seed.="<<seed<<
-	"esd.="<<esd<<
-	"pin.="<<&paramIn<<
-	"pout.="<<&paramOut<<
-	"ncl="<<ncl<<
-	"\n";
+      if (AliTPCReconstructor::StreamLevel()>0) {
+	(*fDebugStreamer)<<"RecoverIn"<<
+	  "seed.="<<seed<<
+	  "esd.="<<esd<<
+	  "pin.="<<&paramIn<<
+	  "pout.="<<&paramOut<<
+	  "ncl="<<ncl<<
+	  "\n";
+      }
       if (ncl>15) {
 	seed->Set(paramIn.GetX(),paramIn.GetAlpha(),paramIn.GetParameter(),paramIn.GetCovariance());
 	seed->SetNumberOfClusters(ncl);
