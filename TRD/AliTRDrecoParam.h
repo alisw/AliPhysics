78561e37606424a117a09afd2aac2553f6e0f8e0
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-02 11:25:36 +0000

    modify AliTRDrecoParam class version and update database

--- TRD/AliTRDrecoParam.h
+++ TRD/AliTRDrecoParam.h
@@ -139,7 +139,7 @@ private:
   Int_t     fNumberOfPresamples;     // number of presamples 
   Int_t     fNumberOfPostsamples;     // number of postsamples 
 
-  ClassDef(AliTRDrecoParam, 5)       // Reconstruction parameters for TRD detector
+  ClassDef(AliTRDrecoParam, 6)       // Reconstruction parameters for TRD detector
 
 };
 
