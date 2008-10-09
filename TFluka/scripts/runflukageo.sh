f68c8cabd8e3426d943c47e0a66ef4685f8ca448
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-09 11:54:42 +0000

    Updates for 2008.3

--- TFluka/scripts/runflukageo.sh
+++ TFluka/scripts/runflukageo.sh
@@ -6,7 +6,7 @@ mkdir tmp
 cd tmp
 
 # Link here some special Fluka files needed
-ln -s $FLUPRO/neuxsc_72.bin neuxsc.bin
+ln -s $FLUPRO/neuxsc-ind_260.bin neuxsc.bin
 ln -s $FLUPRO/random.dat random.dat
 # Copy the random seed
 cp $FLUPRO/random.dat old.seed
