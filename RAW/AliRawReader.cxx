090ef0a752518d35e1fa5b0bd156f45d3b45a8ab
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-14 14:12:44 +0000

    Compilation warning fixed

--- RAW/AliRawReader.cxx
+++ RAW/AliRawReader.cxx
@@ -493,7 +493,7 @@ Bool_t AliRawReader::ReadNextChar(UChar_t& data)
   return kTRUE;
 }
 
-Bool_t  AliRawReader::GotoEvent(Int_t event)
+Bool_t  AliRawReader::GotoEvent(Int_t /*event*/)
 {
   Error("GotoEvent","Method not implemented! Nothing done");
   return kFALSE;
