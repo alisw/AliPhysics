b9c63c38cd3f8465d93687a2457884049cf7cff2
mrodrigu <mrodrigu@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-19 17:05:50 +0000

    Crash Solved

--- ACORDE/AliACORDEQAChecker.h
+++ ACORDE/AliACORDEQAChecker.h
@@ -34,7 +34,7 @@ public:
   virtual const Double_t Check(AliQA::ALITASK_t /*index*/) {return 0.;} ;
   virtual const Double_t Check() {return 0.;} ;
   virtual const Double_t Check(TObjArray*) {return 0.;} ;
-  virtual const Double_t Check(AliQA::ALITASK_t, TNtupleD*);
+//  virtual const Double_t Check(AliQA::ALITASK_t, TNtupleD*);
 //  Double_t CheckEntries(TObjArray * list) const ;/*--> to be implemented*/
 
 
