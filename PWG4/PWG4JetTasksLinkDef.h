208a0e6ad21524aea7d6d46f4132f74fcbbb955e
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-24 16:52:29 +0000

    correction of warning, link def forgot in previous commit

---
+++ PWG4/PWG4JetTasksLinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliAnalysisTaskUE+;
+#pragma link C++ class AliAnalysisTaskJetSpectrum+;
+#pragma link C++ class AliAnalysisHelperJetTasks+;
+#pragma link C++ class AliAnaESDSpectraQA+;
+
+
+#endif
