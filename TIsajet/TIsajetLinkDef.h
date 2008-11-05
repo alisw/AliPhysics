1f342b1afa29e77665d2f89aeb93e396a0d3c2a8
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-11-05 10:03:35 +0000

    Removed until updated.

--- TIsajet/TIsajetLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
- 
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
- 
-#pragma link C++ class TIsajet+;
-#pragma link C++ class AliGenIsajet+;
-#endif
