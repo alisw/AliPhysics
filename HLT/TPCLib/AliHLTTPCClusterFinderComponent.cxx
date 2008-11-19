75c46ec3686a64fcc7ab6be81fc690e07c127d1f
kaamodt <kaamodt@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-11-19 23:43:14 +0000

    Corrected bug, ClusterFinderUnpacked not cleaned properly. ClusterFinderUnpacked is now included again until a proper cleanup is done.

--- HLT/TPCLib/AliHLTTPCClusterFinderComponent.cxx
+++ HLT/TPCLib/AliHLTTPCClusterFinderComponent.cxx
@@ -86,6 +86,9 @@ const char* AliHLTTPCClusterFinderComponent::GetComponentID()
   case kClusterFinderPacked:
     return "TPCClusterFinderPacked";
     break;
+  case kClusterFinderUnpacked: 	 
+    return "TPCClusterFinderUnpacked"; 	 
+    break;
   case kClusterFinderDecoder:
     return "TPCClusterFinderDecoder";
     break;
@@ -101,6 +104,9 @@ void AliHLTTPCClusterFinderComponent::GetInputDataTypes( vector<AliHLTComponentD
   switch(fModeSwitch){
   case kClusterFinderPacked:
     list.push_back( kAliHLTDataTypeDDLRaw | kAliHLTDataOriginTPC );
+    break;
+  case kClusterFinderUnpacked: 	 
+    list.push_back( AliHLTTPCDefinitions::fgkUnpackedRawDataType ); 	 
     break;
   case kClusterFinderDecoder:
     list.push_back( kAliHLTDataTypeDDLRaw | kAliHLTDataOriginTPC );
