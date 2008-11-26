dd93afd182dd459c58df462fe8958d23d042941c
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-11-26 16:24:34 +0000

    not needed anymore

--- EMCAL/mood/TMPHS.cxx
+++
@@ -1,9 +0,0 @@
-#include "TMPHS.h"
-
-ClassImp(TMPHS)
-
-//_____________________________________________________________________________
-TMPHS::TMPHS(const TGWindow *p, UInt_t w, UInt_t h) : 
-  TMCal(p, w, h, AliCaloCalibPedestal::kPhos)
-{
-}
