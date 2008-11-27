9a2b4b6fd81f2ec13f12510cb63ab8092aadfd38
cheynis <cheynis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-11-27 13:08:37 +0000

    Coding conventions...

--- VZERO/AliVZEROQADataMakerRec.h
+++ VZERO/AliVZEROQADataMakerRec.h
@@ -1,13 +1,13 @@
-#ifndef ALIVZEROQADataMakerRec_H
-#define ALIVZEROQADataMakerRec_H
+#ifndef ALIVZEROQADATAMAKERREC_H
+#define ALIVZEROQADATAMAKERREC_H
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
-/*
-  Produces the data needed to calculate the quality assurance. 
-  All data must be mergeable objects.
-*/
 
+//  Produces the data needed to calculate the quality assurance  
+//  All data must be mergeable objects
+//  Handles ESDs and RAWs
+//  Histos will be used for Raw Data control and monitoring
 
 // --- ROOT system ---
 class TH1F; 
@@ -26,30 +26,30 @@ class AliVZEROCalibData;
 class AliVZEROQADataMakerRec: public AliQADataMakerRec {
 
 public:
-  //Histograms for Raw data control
-  enum HRawType_t {kPedestal_Int0,kPedestal_Int1,kPedestal_Cycle_Int0,kPedestal_Cycle_Int1,kPedestal_Time_Int0,kPedestal_Time_Int1
-  			,kChargeEoI_Int0,kChargeEoI_Int1,kChargeEoI_Time_Int0,kChargeEoI_Time_Int1,kChargeEoI_Cycle_Int0,kChargeEoI_Cycle_Int1
-			,kChargeEoI_BB_Int0,kChargeEoI_BB_Int1,kChargeEoI_BG_Int0,kChargeEoI_BG_Int1,kChargeVsClock_Int0,kChargeVsClock_Int1
-			,kChargeMB_BB0_BG0_Int0,kChargeMB_BB0_BG1_Int0,kChargeMB_BB1_BG0_Int0,kChargeMB_BB1_BG1_Int0
-			,kChargeMB_BB0_BG0_Int1,kChargeMB_BB0_BG1_Int1,kChargeMB_BB1_BG0_Int1,kChargeMB_BB1_BG1_Int1
-			,kWidth,kWidth_BB,kWidth_BG,kHPTDCTime,kHPTDCTime_BB,kHPTDCTime_BG,kBBFlagVsClock,kBGFlagVsClock
-			,kMultiV0A,kMultiV0C,kChargeV0A,kChargeV0C,kChargeV0 
-			,kV0ATime,kV0CTime,kDiffTime
-			,kRawMIPV0A,kRawMIPV0C,kRawMIPV0,kRawMIPChannel} ;
+  // Histograms for Raw data control
+  enum HRawType_t {kPedestalInt0,kPedestalInt1,kPedestalCycleInt0,kPedestalCycleInt1,kPedestalTimeInt0,kPedestalTimeInt1
+  		  ,kChargeEoIInt0,kChargeEoIInt1,kChargeEoITimeInt0,kChargeEoITimeInt1,kChargeEoICycleInt0,kChargeEoICycleInt1
+		  ,kChargeEoIBBInt0,kChargeEoIBBInt1,kChargeEoIBGInt0,kChargeEoIBGInt1,kChargeVsClockInt0,kChargeVsClockInt1
+		  ,kChargeMBBB0BG0Int0,kChargeMBBB0BG1Int0,kChargeMBBB1BG0Int0,kChargeMBBB1BG1Int0
+		  ,kChargeMBBB0BG0Int1,kChargeMBBB0BG1Int1,kChargeMBBB1BG0Int1,kChargeMBBB1BG1Int1
+		  ,kWidth,kWidthBB,kWidthBG,kHPTDCTime,kHPTDCTimeBB,kHPTDCTimeBG,kBBFlagVsClock,kBGFlagVsClock
+		  ,kMultiV0A,kMultiV0C,kChargeV0A,kChargeV0C,kChargeV0 
+		  ,kV0ATime,kV0CTime,kDiffTime
+		  ,kRawMIPV0A,kRawMIPV0C,kRawMIPV0,kRawMIPChannel} ;
 	
-	enum HESDType_t {kCellMultiV0A,kCellMultiV0C,kMIPMultiV0A,kMIPMultiV0C,kMIPMultiChannel
-			,kBBFlag,kBGFlag,kChargeChannel,kTimeChannel
-			,kESDV0ATime,kESDV0CTime,kESDDiffTime};
+ enum HESDType_t {kCellMultiV0A,kCellMultiV0C,kMIPMultiV0A,kMIPMultiV0C,kMIPMultiChannel
+		  ,kBBFlag,kBGFlag,kChargeChannel,kTimeChannel
+		  ,kESDV0ATime,kESDV0CTime,kESDDiffTime};
 
 public:
-  AliVZEROQADataMakerRec() ;           // constructor
+  AliVZEROQADataMakerRec() ;            // constructor
   AliVZEROQADataMakerRec(const AliVZEROQADataMakerRec& qadm) ;   
   AliVZEROQADataMakerRec& operator = (const AliVZEROQADataMakerRec& qadm) ;
   virtual ~AliVZEROQADataMakerRec() {;} // destructor
   AliVZEROCalibData *GetCalibData() const;
   
 protected: 
-  AliVZEROCalibData *fCalibData;  //! calibration data
+  AliVZEROCalibData *fCalibData;        //! calibration data
    
 private:
   virtual void   EndOfDetectorCycle(AliQA::TASKINDEX_t, TObjArray * list) ;
@@ -62,10 +62,10 @@ private:
   Int_t   fEvent;
   Int_t   fEven[64];
   Int_t   fOdd[64];
-  Float_t fADC_Mean[128];
+  Float_t fADCmean[128];
 
   ClassDef(AliVZEROQADataMakerRec,1)  // description 
 
 };
 
-#endif // AliVZEROQADataMakerRec_H
+#endif // AliVZEROQADATAMAKERREC_H
