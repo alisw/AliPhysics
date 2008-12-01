693dd6016698c71b19c0c8c1fd9d52c88afa8382
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-12-01 16:09:35 +0000

    Changes to use a collection of AODs as input (Renu)

--- PWG3/vertexingHF/AliAnalysisTaskSEVertexingHFTest.C
+++ PWG3/vertexingHF/AliAnalysisTaskSEVertexingHFTest.C
@@ -5,7 +5,7 @@ void AliAnalysisTaskSEVertexingHFTest()
   // A.Dainese, andrea.dainese@lnl.infn.it
   //
   Bool_t inputAOD=kTRUE; // otherwise, ESD
-
+  TString mode="local"; // otherwise, "grid" 
 
   gSystem->Load("libTree.so");
   gSystem->Load("libGeom.so");
@@ -19,40 +19,49 @@ void AliAnalysisTaskSEVertexingHFTest()
   gSystem->Load("libPWG3base.so");
   gSystem->Load("libPWG3vertexingHF.so");
 
+  TChain *chain = 0;
 
-  // Local files 
-  TString treeName,fileName;
-  if(inputAOD) {
-    treeName="aodTree"; 
-    fileName="AliAOD.root";
+  if(mode=="local") {
+    // Local files 
+    TString treeName,fileName;
+    if(inputAOD) {
+      treeName="aodTree"; 
+      fileName="AliAOD.root";
+    } else {
+      treeName="esdTree"; 
+      fileName="AliESDs.root";
+    }
+    chain = new TChain(treeName.Data());
+    chain->Add(fileName.Data());
+
+  } else if (mode=="grid") {
+    //Fetch files with AliEn :
+    const char *collectionfile = "Collection.xml";
+    TGrid::Connect("alien://") ;
+    TAlienCollection *coll   = TAlienCollection::Open(collectionfile);
+    if(inputAOD) { // input AOD
+      chain = new TChain("aodTree");
+      while(coll->Next()) chain->Add(coll->GetTURL(""));
+    } else { // input ESD
+      //Create an AliRunTagCuts and an AliEventTagCuts Object and impose some selection criteria
+      AliRunTagCuts      *runCuts   = new AliRunTagCuts();
+      AliEventTagCuts    *eventCuts = new AliEventTagCuts();
+      AliLHCTagCuts      *lhcCuts   = new AliLHCTagCuts();
+      AliDetectorTagCuts *detCuts   = new AliDetectorTagCuts();
+      eventCuts->SetMultiplicityRange(0,20000);
+      //Create an AliTagAnalysis Object and chain the tags
+      AliTagAnalysis   *tagAna = new AliTagAnalysis();
+      tagAna->SetType("ESD");
+      TGridResult      *tagResult = coll->GetGridResult("",0,0);
+      tagResult->Print();
+      tagAna->ChainGridTags(tagResult);
+      //Create a new esd chain and assign the chain that is returned by querying the tags
+      chain = tagAna->QueryTags(runCuts,lhcCuts,detCuts,eventCuts);
+    }
   } else {
-    treeName="esdTree"; 
-    fileName="AliESDs.root";
+    printf("ERROR: mode has to be \"local\" or \"grid\" \n");
+    return;
   }
-  TChain *chain = new TChain(treeName.Data());
-  chain->Add(fileName.Data());
-
-  // or:
-  /*
-  //Fetch files with AliEn :
-  const char *collectionfile = "CollectionTags.xml";
-  TGrid::Connect("alien://") ;
-  //Create an AliRunTagCuts and an AliEventTagCuts Object and impose some selection criteria
-  AliRunTagCuts      *runCuts   = new AliRunTagCuts();
-  AliEventTagCuts    *eventCuts = new AliEventTagCuts();
-  AliLHCTagCuts      *lhcCuts   = new AliLHCTagCuts();
-  AliDetectorTagCuts *detCuts   = new AliDetectorTagCuts();
-  eventCuts->SetMultiplicityRange(0,20000);
-  //Create an AliTagAnalysis Object and chain the tags
-  AliTagAnalysis   *tagAna = new AliTagAnalysis();
-  tagAna->SetType("ESD");
-  TAlienCollection *coll   = TAlienCollection::Open(collectionfile);
-  TGridResult      *tagResult = coll->GetGridResult("",0,0);
-  tagResult->Print();
-  tagAna->ChainGridTags(tagResult);
-  //Create a new esd chain and assign the chain that is returned by querying the tags
-  TChain* chain = tagAna->QueryTags(runCuts,lhcCuts,detCuts,eventCuts);
-  */
 
 
   // Create the analysis manager
@@ -94,11 +103,11 @@ void AliAnalysisTaskSEVertexingHFTest()
   // Run the analysis
   //    
   printf("CHAIN HAS %d ENTRIES\n",(Int_t)chain->GetEntries());
-  if(mgr->InitAnalysis()) {
-    mgr->PrintStatus();
-    mgr->StartAnalysis("local",chain);
-    //mgr->StartAnalysis("grid",chain);
-  }
+  if(!mgr->InitAnalysis()) return;
+
+  mgr->PrintStatus();
+
+  mgr->StartAnalysis(mode.Data(),chain);
 
   return;
 }
