940748bd6c414ccc533fb30cef87fdcec1d40df6
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-12-04 07:49:15 +0000

    Updated list of libraries to be loaded

--- test/fpprod/check.C
+++ test/fpprod/check.C
@@ -6,6 +6,10 @@ void check(){
     new AliRun("gAlice","The ALICE Off-line Simulation Framework");
   }
 
+  gSystem->Load("liblhapdf");      // Parton density functions
+  gSystem->Load("libEGPythia6");   // TGenerator interface
+  gSystem->Load("libpythia6");     // Pythia
+  gSystem->Load("libAliPythia6");  // ALICE specific implementations
   gSystem->Load("libdpmjet");
   gSystem->Load("libTDPMjet");
   gROOT->Macro("$ALICE_ROOT/STEER/CheckESD.C");
