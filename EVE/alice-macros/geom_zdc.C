8e5837615086b8d5cd93c59d1dbdf6f72f3e2dc5
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-12-15 13:05:50 +0000

    Replace the default geometry for geometry visualization with the latest one as produced by test/ppbench/Config.C - this also includes ZDC.
    Add macro for showing ZDC geometry

---
+++ EVE/alice-macros/geom_zdc.C
@@ -0,0 +1,31 @@
+// $Id: geom_t0.C 27052 2008-07-02 17:56:46Z mtadel $
+// Main authors: Matevz Tadel & Alja Mrak-Tadel: 2006, 2007
+
+/**************************************************************************
+ * Copyright(c) 1998-2008, ALICE Experiment at CERN, all rights reserved. *
+ * See http://aliceinfo.cern.ch/Offline/AliRoot/License.html for          *
+ * full copyright notice.                                                 *
+ **************************************************************************/
+
+void geom_zdc()
+{
+  gGeoManager = gEve->GetDefaultGeometry();
+
+  TEveElementList* list = new TEveElementList("ZDC");
+  gEve->AddGlobalElement(list);
+
+  TGeoNode* node;
+  TEveGeoTopNode* re;
+
+  node = gGeoManager->GetTopVolume()->FindNode("ZDCA_1");
+  re = new TEveGeoTopNode(gGeoManager, node);
+  re->UseNodeTrans();
+  gEve->AddGlobalElement(re, list);
+
+  node = gGeoManager->GetTopVolume()->FindNode("ZDCC_1");
+  re = new TEveGeoTopNode(gGeoManager, node);
+  re->UseNodeTrans();
+  gEve->AddGlobalElement(re, list);
+
+  gEve->Redraw3D();
+}
