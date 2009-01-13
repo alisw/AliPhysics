de87d87b79a38ac9523db3f44b3fe5533a3e7014
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-01-13 19:46:00 +0000

    Removing the setup of the OCDB from the SetupCalibTask User specific ConfigOCDB should be called instead e.g: gROOT->Macro(Form("$ALICE_ROOT/TPC/macros/ConfigOCDB.C(%f)",5));

--- TPC/macros/CalibrateTPC.C
+++ TPC/macros/CalibrateTPC.C
@@ -3,10 +3,7 @@
   //
   //0. Setup memory chcecker if you want 
   //
-  gSystem->Load("$ROOTSYS/lib/libGui.so");
-  gSystem->Load("$ROOTSYS/lib/libTree.so");
-  gSystem->Load("$MEMSTAT/libMemStat.so");
-  TMemStat *memstat = new TMemStat(100000000,10000000,kTRUE);
+  TMemStat *memstat = new TMemStat("new,gnubuildin");
   AliSysInfo::AddCallBack(TMemStatManager::GetInstance()->fStampCallBack);
 
   AliSysInfo::AddStamp("Start");  
@@ -17,19 +14,21 @@
   // Setup analysis manager
   //
   .L $ALICE_ROOT/TPC/macros/CalibrateTPC.C
-  AliAnalysisManager * mgr = SetupCalibTask("/V6/");
+  AliAnalysisManager * mgr = ( AliAnalysisManager *)SetupCalibTask("/V2/");  
+  gROOT->Macro(Form("$ALICE_ROOT/TPC/macros/ConfigOCDB.C(%f)",5));
+
   //
   // Process data - chain
   //
   gSystem->AddIncludePath("-I$ALICE_ROOT/TPC/macros");
   gROOT->LoadMacro("$ALICE_ROOT/TPC/macros/AliXRDPROOFtoolkit.cxx+")
   AliXRDPROOFtoolkit tool; 
-  TChain * chain = tool.MakeChain("esd.txt","esdTree",0,100);
+  TChain * chain = tool.MakeChainRandom("esd.txt","esdTree",0,5);
   chain->Lookup();
   // memory
-  mgr->SetNSysInfo(100); 
+  mgr->SetNSysInfo(1000); 
   //
-  mgr->SetDebugLevel(1);
+  mgr->SetDebugLevel(10);
   mgr->StartAnalysis("proof",chain);
   //mgr->StartAnalysis("local",chain);
   // delete manager
@@ -40,37 +39,17 @@
   // analyze memstat report
   //
   delete memstat;
-  TMemStat draw("memstat.root");
-  draw.MakeReport(0,0,"order 0 sortstat 3 sortstamp 0 sortdeep 10 stackdeep 15 maxlength 50")   
+  TMemStat mem;
+  mem.MakeReport(0,0,"order 0 sortstat 3 sortstamp 0 sortdeep 10 stackdeep 15 maxlength 50")   
 */
 
-
-AliAnalysisManager * SetupCalibTask(char * prefix ="/V12/") {
+TObject  * SetupCalibTask(char * prefix ="/V12/") {
   //
   //
   //
   TStopwatch stopwatch;
   stopwatch.Start();
   //
-  // set magnetic field form the cosmos - it should be provided by framework
-  AliMagFMaps* field = new AliMagFMaps("Maps","Maps", 2, 1., 10., 2);
-  AliTracker::SetFieldMap(field,0);
-  TGeoManager::Import("/u/miranov/proof/geometry.root");
-  //
-  TFile f("/u/miranov/GainMap.root");
-  AliTPCCalPad *gainMap = f.Get("GainMap");
-  //
-  // OCDB setup
-  //
-  AliCDBManager::Instance()->SetRun(1);
-  AliCDBManager::Instance()->SetDefaultStorage("local://$ALICE_ROOT");
-  AliTPCcalibDB::Instance()->SetExBField(0);
-  AliTPCClusterParam * param = AliTPCcalibDB::Instance()->GetClusterParam();
-  AliTPCClusterParam::SetInstance(param);
-  AliTPCcalibDB::Instance()->SetExBField(0);
-  //
-
-
   //
   AliAnalysisManager *mgr=new AliAnalysisManager("TestManager");
 
@@ -82,7 +61,6 @@ AliAnalysisManager * SetupCalibTask(char * prefix ="/V12/") {
   AliCDBManager::Instance()->SetRun(1) ;
   AliCDBManager::Instance()->SetDefaultStorage("local://$ALICE_ROOT");
   AliTPCClusterParam * clusterParam = AliTPCcalibDB::Instance()->GetClusterParam();
-
   AliTPCAnalysisTaskcalib *task1=new AliTPCAnalysisTaskcalib("TPC calibration task");
   
   AliTPCcalibTracksCuts *cuts = new AliTPCcalibTracksCuts(40, 0.4, 0.5, 0.13, 0.018);
@@ -93,40 +71,66 @@ AliAnalysisManager * SetupCalibTask(char * prefix ="/V12/") {
 
   AliTPCcalibTracksGain *calibTracksGain =  new AliTPCcalibTracksGain("calibTracksGain","Gain calibration using tracks",cuts); 
   AliTPCcalibAlign *calibAlign = new AliTPCcalibAlign("alignTPC","Alignment of the TPC sectors");
+  AliTPCcalibAlign *calibAlignAll = new AliTPCcalibAlign("alignTPCAll","Alignment of the TPC sectors- All");
   AliTPCcalibLaser *calibLaser = new AliTPCcalibLaser("laserTPC","laserTPC");
   AliTPCcalibCosmic *calibCosmic = new AliTPCcalibCosmic("cosmicTPC","cosmicTPC");
   AliTPCcalibCalib *calibCalib = new AliTPCcalibCalib("calibTPC","calibTPC");
   TTimeStamp startTime(2008,9,0,0,0,0);
   TTimeStamp stopTime(2008,11,0,0,0,0);
-  AliTPCcalibTime *calibTime = new AliTPCcalibTime("cosmicTime","cosmicTime",0, startTime.GetSec(), stopTime.GetSec(), 5*60, 5*60);
+  AliTPCcalibTime *calibTime = new AliTPCcalibTime("calibTime","calibTime", startTime.GetSec(), stopTime.GetSec(), 5*60, 5*60);
 
-  calibCosmic->SetGainMap(gainMap);
-  calibTracksGain->SetGainMap(gainMap);
+  AliTPCcalibUnlinearity *calibUnlinearity = new AliTPCcalibUnlinearity("calibUnlinearity","calibUnlinearity");
+  AliTPCcalibUnlinearity *calibUnlinearityAll = new AliTPCcalibUnlinearity("calibUnlinearityAll","calibUnlinearityAll");
+
   //
-  calibTracks->SetDebugLevel(20);
+  calibTracks->SetDebugLevel(2);
   calibTracks->SetStreamLevel(20);
   calibTracksGain->SetDebugLevel(2);
   calibTracksGain->SetStreamLevel(20);
   calibAlign->SetDebugLevel(20);
   calibAlign->SetStreamLevel(10);
-  calibLaser->SetDebugLevel(20);
-  calibLaser->SetStreamLevel(20);
+  calibAlignAll->SetDebugLevel(20);
+  calibAlignAll->SetStreamLevel(10);
+  calibLaser->SetDebugLevel(0);
+  calibLaser->SetStreamLevel(0);
   calibCosmic->SetDebugLevel(20);
   calibCosmic->SetStreamLevel(2);
-  calibCalib->SetDebugLevel(20);
-  calibCalib->SetStreamLevel(10);
+  calibCalib->SetDebugLevel(0);
+  calibCalib->SetStreamLevel(0);
+  calibTime->SetDebugLevel(20);
+  calibTime->SetStreamLevel(10);
+  //
+  calibUnlinearity->SetDebugLevel(20);
+  calibUnlinearity->SetStreamLevel(10);
+  calibUnlinearityAll->SetDebugLevel(20);
+  calibUnlinearityAll->SetStreamLevel(10);
+  
+  calibCalib->SetTriggerMask(-1,-1,kFALSE);       //accept everything 
+  calibTracks->SetTriggerMask(-1,16,kTRUE);      //reject laser trigger, accept everything else
+  calibTracksGain->SetTriggerMask(-1,16,kTRUE);  //reject laser trigger, accept everything else
+  calibAlign->SetTriggerMask(-1,-1,kTRUE);       //accept everything 
+  calibAlignAll->SetTriggerMask(-1,-1,kFALSE);   //accept everything 
+  calibLaser->SetTriggerMask(-1,-1,kFALSE);       //accept only laser trigger
+  calibCosmic->SetTriggerMask(-1,-1,kTRUE);      //reject laser trigger, accept everything else
+  calibTime->SetTriggerMask(-1,-1,kFALSE);        //accept everything 
+  calibUnlinearity->SetTriggerMask(-1,-1,kTRUE);   //reject laser 
+  calibUnlinearityAll->SetTriggerMask(-1,-1,kFALSE);   //non reject laser 
+
 
   //
  // ---*---*-----*-*-----*----------*---
   // ADD CALIB JOBS HERE!!!!!!!!!!!!!!!!
   task1->AddJob(calibCalib);
-  task1->AddJob(calibAlign);
-  task1->AddJob(calibLaser);
-  task1->AddJob(calibCosmic);
+  //task1->AddJob(calibAlign);
+  //task1->AddJob(calibAlignAll);
+  //task1->AddJob(calibLaser);
+  //task1->AddJob(calibCosmic);
   task1->AddJob(calibTime);
 
-  task1->AddJob(calibTracksGain);
-  task1->AddJob(calibTracks);
+  //task1->AddJob(calibTracksGain);
+  //task1->AddJob(calibTracks);
+  //task1->AddJob(calibUnlinearity);
+  //task1->AddJob(calibUnlinearityAll);
   // task1->AddJob(new AliTPCcalibV0);
   // -*----*----*---*-*------*-------**--
   // -------*--*---------*-----*-------*-
@@ -145,14 +149,14 @@ AliAnalysisManager * SetupCalibTask(char * prefix ="/V12/") {
 			  AliAnalysisManager::kOutputContainer,
 			  "CalibObjects.root");
   
-  coutput1->SetSpecialOutput(kTRUE);
+  //coutput1->SetSpecialOutput(kTRUE);
   //coutput1->SetFileName("CalibObjectFile.root");
   mgr->ConnectInput(task1,0,cinput1);
   mgr->ConnectOutput(task1,0,coutput1);
   //
   //mgr->SetSpecialOutputLocation(path->Data());
 
-  if (!mgr->InitAnalysis()) return;
+  if (!mgr->InitAnalysis()) return 0;
   mgr->PrintStatus(); 
   
   stopwatch.Stop();
@@ -161,24 +165,31 @@ AliAnalysisManager * SetupCalibTask(char * prefix ="/V12/") {
 }
 
 
-void Merge(){
-  fstream finput;
-  TString currentFile("");
-  finput.open("mergelist.txt", ios_base::in);
-  TFileMerger merger;
-  merger.OutputFile("result.root");  
-  finput >> currentFile;
-  merger->AddFile(currentFile->Data());
-  merger.Merge();
-  TFile::Cp("result.root","last.root");
+
+
+
+
+void CalibrateTPC(Float_t magf, Int_t first, Int_t last){
+  gSystem->Load("libANALYSIS");
+  gSystem->Load("libTPCcalib");
+  gROOT->Macro(Form("$ALICE_ROOT/TPC/macros/ConfigOCDB.C(%f)",magf));
   //
-  while(finput.good()) {
-    TFileMerger  merger2;
-    merger2.OutputFile("result.root");
-    finput >> currentFile;    
-    merger2.AddFile("last.root");
-    merger2.AddFile(currentFile->Data());    
-    merger2.Merge(); 
-    TFile::Cp("result.root","last.root");
-  }
+  // Setup analysis manager
+  //
+  //.L $ALICE_ROOT/TPC/macros/CalibrateTPC.C
+  AliAnalysisManager * mgr = (AliAnalysisManager*)SetupCalibTask("/V3/");
+  //
+  // Process data - chain
+  //
+  gSystem->AddIncludePath("-I$ALICE_ROOT/TPC/macros");
+  gROOT->LoadMacro("$ALICE_ROOT/TPC/macros/AliXRDPROOFtoolkit.cxx+");
+  AliXRDPROOFtoolkit tool; 
+  TChain * chain = tool.MakeChain("esd.txt","esdTree",0,last-first,last);
+  chain->Lookup();
+  // memory
+  mgr->SetNSysInfo(5000); 
+  //
+  mgr->SetDebugLevel(1);
+  mgr->StartAnalysis("local",chain);
+  
 }
