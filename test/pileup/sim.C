a6feec7900be8d1349c646976126f8a57ea7272e
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-01-20 17:10:13 +0000

    Test suite for beam-beam pileup simulation. The meaning of the settings and some examples are documented in the config file

---
+++ test/pileup/sim.C
@@ -0,0 +1,25 @@
+void sim(Int_t nev=5) {
+  if (!strcmp(gSystem->GetBuildArch(),"win32gcc")) {
+    gSystem->Load("libProof");
+    gSystem->Load("libGui");
+    gROOT->Macro("loadlibssim.C");
+    new AliRun("gAlice","The ALICE Off-line Simulation Framework");
+  }
+
+  AliSimulation simulator;
+  simulator.SetMakeSDigits("TRD TOF PHOS HMPID EMCAL MUON FMD ZDC PMD T0 VZERO");
+  simulator.SetMakeDigitsFromHits("ITS TPC");
+  if (strcmp(gSystem->GetBuildArch(),"win32gcc")) {
+    simulator.SetWriteRawData("ALL","raw.root",kTRUE);
+  }
+
+  simulator.SetDefaultStorage("local://$ALICE_ROOT");
+  simulator.SetSpecificStorage("GRP/GRP/Data",
+			       Form("local://%s",gSystem->pwd()));
+ 
+  TStopwatch timer;
+  timer.Start();
+  simulator.Run(nev);
+  timer.Stop();
+  timer.Print();
+}
