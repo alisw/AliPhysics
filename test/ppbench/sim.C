1160f57aedfcb5dc0ebc457288f3d033ea6df98a
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-01-28 13:34:33 +0000

    allow to set the number of events throub an env variable

--- test/ppbench/sim.C
+++ test/ppbench/sim.C
@@ -1,4 +1,4 @@
-void sim(Int_t nev=20) {
+void sim(Int_t nev=1) {
   AliSimulation simulator;
   simulator.SetMakeSDigits("TRD TOF PHOS HMPID EMCAL MUON FMD ZDC PMD T0 VZERO");
   simulator.SetMakeDigitsFromHits("ITS TPC");
