cc753c9650c9248e98ed27536da1e8853cbb1282
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-01-29 17:53:07 +0000

    Make the JF similar to PYCELL, increase fiducial region

--- JETAN/ConfigJetAnalysisMC.C
+++ JETAN/ConfigJetAnalysisMC.C
@@ -10,6 +10,7 @@ AliJetFinder*  ConfigJetAnalysis()
     jrh->SetFastSimTPC(kFALSE);
     jrh->SetFastSimEMCAL(kFALSE);
     jrh->SetPtCut(0.);
+    jrh->SetFiducialEta(-2.1,2.1); // to take all MC particles default is 0.9
 
     // Define reader and set its header
     AliJetKineReader *er = new AliJetKineReader();
@@ -18,16 +19,19 @@ AliJetFinder*  ConfigJetAnalysis()
  
     // Define jet header
     AliUA1JetHeaderV1 *jh=new AliUA1JetHeaderV1();
-    jh->SetComment("UA1 jet code with default parameters");
+    jh->SetComment("UA1 jet code with  parameters similar to PYCELL during sim.");
     jh->BackgMode(0);
     jh->SetRadius(1.0);
-    jh->SetEtSeed(2.);
-    jh->SetLegoNbinPhi(420.);
-    jh->SetLegoNbinEta(120.);
-    jh->SetLegoEtaMin(-1.9);
-    jh->SetLegoEtaMax(+1.9);  
-    jh->SetMinJetEt(5.);
+    jh->SetEtSeed(4.);
+    jh->SetLegoNbinPhi(432);
+    jh->SetLegoNbinEta(274);
+    jh->SetLegoEtaMin(-2);
+    jh->SetLegoEtaMax(+2);  
+    jh->SetJetEtaMax(0.5);  
+    jh->SetJetEtaMin(-0.5);  
+    jh->SetMinJetEt(10.);
     
+
     // Define jet finder. Set its header and reader
     jetFinder = new AliUA1JetFinderV1();
     jetFinder->SetJetHeader(jh);
