ba47225e473cecfbb374e1897aff07a88afcdcaa
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-12 10:22:38 +0000

    remove Fluka related Gstpar parameters. Waiting for new tests.

--- TRD/AliTRD.cxx
+++ TRD/AliTRD.cxx
@@ -530,8 +530,9 @@ void AliTRD::Init()
   gMC->Gstpar((* fIdtmed)[9],"DRAY"    , 1.0);
   gMC->Gstpar((* fIdtmed)[9],"STRA"    , 1.0); 
   gMC->Gstpar((* fIdtmed)[9],"LOSS"    ,13.0);      // Specific energy loss
-  gMC->Gstpar((* fIdtmed)[9],"PRIMIO_E",23.53);     // 1st ionisation potential
-  gMC->Gstpar((* fIdtmed)[9],"PRIMIO_N",19.344431); // Number of primaries
+  // Parameters specific to Fluka
+  //gMC->Gstpar((* fIdtmed)[9],"PRIMIO_E",23.53);     // 1st ionisation potential
+  //gMC->Gstpar((* fIdtmed)[9],"PRIMIO_N",19.344431); // Number of primaries
 
 }
 
