5173a234ff89224e580dcd6bb6e1f6367e3f10e2
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-19 14:15:40 +0000

    added the option to use functions as weights

--- PWG2/FLOW/macros/makeWeights.C
+++ PWG2/FLOW/macros/makeWeights.C
@@ -14,22 +14,32 @@
 
 void makeWeights(TString type="ESD", TString method="GFC", TString cumulantOrder="4th")
 {
- //load needed libraries:
+ // load needed libraries:
  gSystem->AddIncludePath("-I$ROOTSYS/include");
  gSystem->Load("libTree.so");
 
- //for AliRoot
+ // for AliRoot
  gSystem->AddIncludePath("-I$ALICE_ROOT/include");
  gSystem->Load("libANALYSIS.so");
  gSystem->Load("libPWG2flowCommon.so");
  cerr<<"libPWG2flowCommon.so loaded ..."<<endl;
 
- //open the output file from the first run of the specified method:
+ // open the output file from the first run of the specified method:
  TString inputFileName = "output";
  TFile* file = NULL;
  file = TFile::Open(((((inputFileName.Append(method.Data())).Append("analysis")).Append(type.Data())).Append(".root")).Data(), "READ"); 
-   
- //accessing the results:
+ 
+ // using pt weights linear or quadratic in pt:
+ Bool_t useLinearPt    = kFALSE;
+ Bool_t useQuadraticPt = kTRUE;
+ if(useLinearPt && useQuadraticPt)
+ {
+  cout<<" WARNING: you can use pt weights linear or quadratic in pt, but not at the same time. "<<endl;
+  exit(0);
+ }
+ Bool_t useFunctionOfPt = useLinearPt||useQuadraticPt; 
+ 
+ // accessing the results:
  TString cobj = "cobj";
  TString afc  = "AliFlowCommonHist";
  TString afcr = "AliFlowCommonHistResults";
@@ -67,12 +77,13 @@ void makeWeights(TString type="ESD", TString method="GFC", TString cumulantOrder
  Double_t nParticlesPerBin = 0; // average number of particles per phi bin
  Double_t nParticles = 0;       // number of particles in all phi bins 
  Double_t wPhi = 0.;  
- //common control histos:
+ 
+ // common control histos:
  if(commonHist)
  {
-  //azimuthal acceptance:
+  // azimuthal acceptance:
   (commonHist->GetHistPhiInt())->SetName("phi_weights");
-  (commonHist->GetHistPhiInt())->SetTitle("Phi_weights: correction for non-uniform acceptance");
+  (commonHist->GetHistPhiInt())->SetTitle("phi_weights: correction for non-uniform acceptance");
   (commonHist->GetHistPhiInt())->SetYTitle("weights");
   (commonHist->GetHistPhiInt())->SetXTitle("#phi"); 
   nBinsPhi = (commonHist->GetHistPhiInt())->GetNbinsX();
@@ -88,17 +99,60 @@ void makeWeights(TString type="ESD", TString method="GFC", TString cumulantOrder
   }
   listWeights->Add(commonHist->GetHistPhiInt());
  }else{cout<<" WARNING: the common control histos from the 1st run were not accessed."<<endl;} 
- //common results histos:
+ 
+ // common results histos:
  if(commonHistRes)
  {
-  //diff. flow (pt):
+  // diff. flow (pt):
   (commonHistRes->GetHistDiffFlowPtPOI())->SetName("pt_weights");
-  listWeights->Add(commonHistRes->GetHistDiffFlowPtPOI());
-  //diff. flow (eta):
+  if(!useFunctionOfPt) listWeights->Add(commonHistRes->GetHistDiffFlowPtPOI());
+  // diff. flow (eta):
   (commonHistRes->GetHistDiffFlowEtaPOI())->SetName("eta_weights");
   listWeights->Add(commonHistRes->GetHistDiffFlowEtaPOI());
  }else{cout<<" WARNING: the common results histos from the 1st run were not accessed."<<endl;}  
  
+ // pt weights linear and quadratic in pt:
+ if(useFunctionOfPt)
+ {
+  Double_t ptMin = AliFlowCommonConstants::GetPtMin();
+  Double_t ptMax = AliFlowCommonConstants::GetPtMax();
+  Int_t nBinsPt  = AliFlowCommonConstants::GetNbinsPt();
+  Double_t ptCutOff = 4.0; // for pt > ptCutOff use constant weights 
+  if(nBinsPt==0) 
+  { 
+   cout<<" WARNING: number of pt bins is 0. "<<endl;
+   exit(0);
+  } 
+  Double_t ptBinWidth = 1.*(ptMax-ptMin)/nBinsPt;
+ 
+  TH1D *ptFunction = new TH1D("ptFunction", "ptFunction", nBinsPt, ptMin, ptMax);
+  ptFunction->SetName("pt_weights");
+  ptFunction->SetXTitle("p_{t} [GeV]");
+  ptFunction->SetYTitle("weights");
+  Int_t power = 0;
+  if(useLinearPt) 
+  {
+   power = 1;
+   ptFunction->SetTitle("p_{t} weights: #propto p_{t}");
+  } 
+  if(useQuadraticPt)
+  { 
+   power = 2;
+   ptFunction->SetTitle("p_{t} weights: #propto p_{t}^{2}");
+  }   
+  for(Int_t b=1;b<nBinsPt+1;b++)
+  {
+   if(ptMin+b*ptBinWidth < ptCutOff)
+   {
+    ptFunction->SetBinContent(b, pow(ptMin+b*ptBinWidth, power)); 
+   }else
+    {
+     ptFunction->SetBinContent(b, pow(ptCutOff, power)); 
+    }  
+  } 
+  listWeights->Add(ptFunction); 
+ }   
+        
  outputFile->WriteObject(listWeights,"weights","SingleKey");
 
  delete listWeights;
