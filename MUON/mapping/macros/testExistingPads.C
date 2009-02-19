4537049ac3984d509f3f92bb2306a02d888790d0
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-19 11:26:09 +0000

    In mapping/macros: Make testExistingPads.C, testExistingPads2.C macros working again and added AliMpFastSegmentation in test.

--- MUON/mapping/macros/testExistingPads.C
+++ MUON/mapping/macros/testExistingPads.C
@@ -1,26 +1,46 @@
 // $Id$
 // $MpId: testExistingPads.C,v 1.12 2005/10/28 15:36:07 ivana Exp $
 //
-// Test macro for testing which pad is seen as "existing" by AliMpSector.
+// Test macro for testing which pad is seen as "existing" by 
+// by AliMpSectorSegmentation and AliMpFastSegmentation
+// To run macro:
+// root [0] .L testExistingPads.C+
+// root [1] testExistingPads();
 
-void testExistingPads(AliMp::StationType station = AliMp::kStation1,
-                      AliMp::PlaneType plane = AliMp::kBendingPlane, 
-		      Bool_t rootInput = false)
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include "AliMpStation12Type.h"
+#include "AliMpPlaneType.h"
+#include "AliMpDataProcessor.h"
+#include "AliMpDataMap.h"
+#include "AliMpDataStreams.h"
+#include "AliMpSector.h"
+#include "AliMpSectorReader.h"
+#include "AliMpSectorSegmentation.h"
+#include "AliMpFastSegmentation.h"
+#include "AliMpArea.h"
+#include "AliMpVPadIterator.h"
+#include "AliMpVPainter.h"
+
+#include <Riostream.h>
+#include <TCanvas.h>
+#include <TH2.h>
+
+#include <iomanip>
+#include <fstream>
+
+#endif
+
+void testExistingPads(AliMq::Station12Type station = AliMq::kStation1,
+                      AliMp::PlaneType plane = AliMp::kBendingPlane)
 {
-  AliMpSector *sector = 0;
-  if (!rootInput) {
-    AliMpSectorReader r(station, plane);
-    sector=r.BuildSector();
-  }
-  else  {
-    TString filePath = AliMpFiles::SectorFilePath(station,plane);
-    filePath.ReplaceAll("zones.dat", "sector.root"); 
+  AliMpDataProcessor mp;
+  AliMpDataMap* dataMap = mp.CreateDataMap("data");
+  AliMpDataStreams dataStreams(dataMap);
 
-    TFile f(filePath.Data(), "READ");
-    sector = (AliMpSector*)f.Get("Sector");
-  }  
-
-  AliMpSectorSegmentation segmentation(sector);
+  AliMpSectorReader r(dataStreams, station, plane);
+  AliMpSector* sector = r.BuildSector();
+  AliMpSectorSegmentation* segmentation = new AliMpSectorSegmentation(sector);
   AliMpVPainter* painter = AliMpVPainter::CreatePainter(sector);
 
   TCanvas* c1 = new TCanvas("view",
@@ -28,8 +48,8 @@ void testExistingPads(AliMp::StationType station = AliMp::kStation1,
   painter->Draw("ZSSMP");
   c1->Update();
 
-  Int_t maxPadIndexX = segmentation.MaxPadIndexX();
-  Int_t maxPadIndexY = segmentation.MaxPadIndexY();
+  Int_t maxPadIndexX = segmentation->MaxPadIndexX();
+  Int_t maxPadIndexY = segmentation->MaxPadIndexY();
   
   // Define histogram limits
   Int_t nx = (maxPadIndexX/10 + 1)*10;
@@ -39,15 +59,34 @@ void testExistingPads(AliMp::StationType station = AliMp::kStation1,
 
   TCanvas* c2 = new TCanvas("c2","Only existing pads are filled");
 
-  AliMpSectorSegmentation segmentation(sector);
   for (Int_t i=0; i<maxPadIndexX+1;i++){
     for (Int_t j=0;j<maxPadIndexY+1;++j){
 
       AliMpIntPair indices(i,j);
-      if (segmentation.HasPad(indices)) histo->Fill(i,j);
+      if (segmentation->HasPad(indices)) histo->Fill(i,j);
     }
   }
 
   c2->cd();
   histo->Draw("box");
+
+  // the same plot with fast segmentation
+  TH2C* histo2 = new TH2C("histo2","Existing pads2", 
+                          nx, -0.5, nx-0.5, ny, -0.5, ny-0.5);
+
+  TCanvas* c3 = new TCanvas("c3","Only existing pads are filled");
+
+  AliMpFastSegmentation* fast = new AliMpFastSegmentation(segmentation);
+  for (Int_t i=0; i<maxPadIndexX+1;i++){
+    for (Int_t j=0;j<maxPadIndexY+1;++j){
+
+      AliMpIntPair indices(i,j);
+      if (fast->HasPad(indices)) histo2->Fill(i,j);
+    }
+  }
+
+  c3->cd();
+  histo2->Draw("box");
+  
+  delete fast;
 }
--- MUON/mapping/macros/testExistingPads2.C
+++ MUON/mapping/macros/testExistingPads2.C
@@ -2,54 +2,91 @@
 // $MpId: testExistingPads2.C,v 1.3 2005/08/24 08:53:27 ivana Exp $
 //
 // Extended testExistingPads macro for testing which pad is seen as 
-// "existing" by AliMpSector.
+// "existing" by AliMpSectorSegmentation or AliMpFastSegmentation.
 // The loop over pads is performed in two ways:
 // 1) via sector area pad iterator
 // 2) via indices
+// To run macro:
+// root [0] .L testExistingPads2.C+
+// root [1] testExistingPads2();
+
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include "AliMpStation12Type.h"
+#include "AliMpPlaneType.h"
+#include "AliMpDataProcessor.h"
+#include "AliMpDataMap.h"
+#include "AliMpDataStreams.h"
+#include "AliMpSector.h"
+#include "AliMpSectorReader.h"
+#include "AliMpSectorSegmentation.h"
+#include "AliMpFastSegmentation.h"
+#include "AliMpArea.h"
+#include "AliMpVPadIterator.h"
+
+#include <Riostream.h>
 
 #include <iomanip>
+#include <fstream>
 
-void testExistingPads2(AliMp::StationType station = AliMp::kStation1,
+#endif
+
+void testExistingPads2(Bool_t useFastSegmentation = kTRUE,
+                       AliMq::Station12Type station = AliMq::kStation1,
                        AliMp::PlaneType plane = AliMp::kBendingPlane) 
 {
-  AliMpSectorReader r(station, plane);
+  // Read data
+  AliMpDataProcessor mp;
+  AliMpDataMap* dataMap = mp.CreateDataMap("data");
+  AliMpDataStreams dataStreams(dataMap);
+  AliMpSectorReader r(dataStreams, station, plane);
   AliMpSector* sector = r.BuildSector();
-  AliMpSectorSegmentation segmentation(sector);
-  
-  ofstream out1("testExistingPads2.ixiy.out", ios::out);
-  ofstream out2("testExistingPads2.iter.out", ios::out);
+
+  // Segmentation
+  AliMpVSegmentation* segmentation;
+  if ( useFastSegmentation)
+    segmentation = new AliMpFastSegmentation(new AliMpSectorSegmentation(sector));
+  else
+    segmentation = new AliMpSectorSegmentation(sector);
+    
+  // Output files 
+  ofstream out1("testExistingPads.ixiy.out", ios::out);
+  ofstream out2("testExistingPads.iter.out", ios::out);
   
   // First loop over indices
   cout << "Iterating via indices ..." << endl;
   Int_t counter1 = 0;
   if ( sector->GetDirection() == AliMp::kX )
-    for (Int_t i=1; i<segmentation.MaxPadIndexX()+1; i++) {
-      for (Int_t j=1; j<segmentation.MaxPadIndexY()+1; j++) {
+    for (Int_t i=1; i<segmentation->MaxPadIndexX()+1; i++) {
+      for (Int_t j=1; j<segmentation->MaxPadIndexY()+1; j++) {
         AliMpIntPair indices(i,j);
-        if ( segmentation.HasPad(indices) ) 
+        if ( segmentation->HasPad(indices) ) 
           out1 << std::setw(4) << ++counter1 << "  "
-	       << segmentation.PadByIndices(indices) << endl;;
+	       << segmentation->PadByIndices(indices) << endl;;
       }
     }
 
   if ( sector->GetDirection() == AliMp::kY )
-    for (Int_t j=1; j<segmentation.MaxPadIndexY()+1; j++) {
-      for (Int_t i=1; i<segmentation.MaxPadIndexX()+1; i++) {
+    for (Int_t j=1; j<segmentation->MaxPadIndexY()+1; j++) {
+      for (Int_t i=1; i<segmentation->MaxPadIndexX()+1; i++) {
         AliMpIntPair indices(i,j);
-        if ( segmentation.HasPad(indices) ) 
+        if ( segmentation->HasPad(indices) ) 
           out1 << std::setw(4) << ++counter1 << "  "
-	       << segmentation.PadByIndices(indices) << endl;;
+	       << segmentation->PadByIndices(indices) << endl;;
       }
     }
   
   // Second loop via pad area iterator
   cout << "Iterating via iterator ..." << endl;
   AliMpArea area(TVector2(60.,60.), TVector2(60.,60.) );
-  AliMpVPadIterator* it = segmentation.CreateIterator(area);
+  AliMpVPadIterator* it = segmentation->CreateIterator(area);
   Int_t counter2 = 0;
   for (it->First(); ! it->IsDone(); it->Next()){
    
     out2 << std::setw(4) << ++counter2 << "  "
 	 << it->CurrentItem() << endl;
   }  
+  
+  delete segmentation;
 }
