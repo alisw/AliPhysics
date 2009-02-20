6ca01e799d7de420d58084b604e46f27e3b3ac69
rgrosso <rgrosso@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-20 14:34:24 +0000

    Base class (AliMisaligner); each detector will provide its derived class, AliDETMisaligner, substituting the macros MakeDET<Zero,Res,Full>MisAlignment.C
    AliZDCMisaligner and others will soon be committed and can be used as examples.
    
    This will allow to have a common set of methods to produce alignment objects
    without repeating several things as it is done presently in the detectors' macros.
    
    
    New macro (MakeAlignmentObjs.C) for producing simulated alignment objects
    for all detectors or a subset of them, for the three misalignment scenarios
    (ideal, residual, full). When all detectors have substituted their macros 
    MakeDET<Zero,Res,Full>MisAlignment.C into the derived class AliDETMisAligner,
    this macro will substitute the three current macros MakeAllDETs<Zero,Res,Full>MisAlignment.

---
+++ STEER/AliMisAligner.h
@@ -0,0 +1,38 @@
+#ifndef ALI_MISALIGNER_H
+#define ALI_MISALIGNER_H
+
+#include "TObject.h"
+#include "TString.h"
+#include "AliCDBMetaData.h"
+
+class TClonesArray;
+class AliCDBManager;
+
+// Base class for creating a TClonesArray of simulated misalignment objects
+// for a given subdetector of type ideal,residual or full
+//
+
+class AliMisAligner : public TObject {
+
+  public:
+    AliMisAligner();
+    virtual TClonesArray* MakeAlObjsArray() const =0;
+    virtual AliCDBMetaData* GetCDBMetaData() const =0;
+    void SetMisalType(const char* misalType)
+    {
+      fMisalType=misalType;
+    }
+    const char* GetMisalType() const
+    {
+      return fMisalType.Data();
+    }
+
+  protected:
+    TString fMisalType;
+
+  private:
+    ClassDef(AliMisAligner,0);
+};
+
+#endif
+
