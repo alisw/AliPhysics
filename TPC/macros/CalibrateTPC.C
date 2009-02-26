105b769e1bfeeb6030a3eecc5b9bdea988f023db
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-26 09:17:39 +0000

    Modification related to the new AliAnalysisTask common input (Marian)
    + Analysis manager and AliTPCAnalysisTaskCalib setup in different functions
    (Marian)

--- TPC/macros/CalibrateTPC.C
+++ TPC/macros/CalibrateTPC.C
@@ -13,9 +13,11 @@
   //
   // Setup analysis manager
   //
-  .L $ALICE_ROOT/TPC/macros/CalibrateTPC.C
-  AliAnalysisManager * mgr = ( AliAnalysisManager *)SetupCalibTask("/V2/");  
-  gROOT->Macro(Form("$ALICE_ROOT/TPC/macros/ConfigOCDB.C(%f)",5));
+  TString path=gSystem->pwd();
+  gROOT->Macro(Form("%s/ConfigOCDB.C",path->Data()));
+
+  .L CalibrateTPC.C
+  AliAnalysisManager * mgr = ( AliAnalysisManager *)SetupCalibTask("/V6/");  
 
   //
   // Process data - chain
@@ -23,14 +25,14 @@
   gSystem->AddIncludePath("-I$ALICE_ROOT/TPC/macros");
   gROOT->LoadMacro("$ALICE_ROOT/TPC/macros/AliXRDPROOFtoolkit.cxx+")
   AliXRDPROOFtoolkit tool; 
-  TChain * chain = tool.MakeChainRandom("esd.txt","esdTree",0,5);
-  chain->Lookup();
+  TChain * chain = tool.MakeChain("esd.txt","esdTree",0,10);
+  //chain->Lookup();
   // memory
   mgr->SetNSysInfo(1000); 
   //
   mgr->SetDebugLevel(10);
   mgr->StartAnalysis("proof",chain);
-  //mgr->StartAnalysis("local",chain);
+  //mgr->StartAnalysis("local",chain);//
   // delete manager
   //
   delete mgr;
@@ -43,28 +45,49 @@
   mem.MakeReport(0,0,"order 0 sortstat 3 sortstamp 0 sortdeep 10 stackdeep 15 maxlength 50")   
 */
 
-TObject  * SetupCalibTask(char * prefix ="/V12/") {
+char * prefix = "/V6/";
+void SetupCalibTask(AliTPCAnalysisTaskcalib* task1);
+
+TObject  * SetupCalibTask(char * tprefix ="/V12/") {
   //
   //
   //
+  prefix=tprefix;
   TStopwatch stopwatch;
   stopwatch.Start();
-  //
-  //
   AliAnalysisManager *mgr=new AliAnalysisManager("TestManager");
-
   AliESDInputHandler* esdH=new AliESDInputHandler;
   esdH->SetActiveBranches("ESDfriend");
   mgr->SetInputEventHandler(esdH);  
-  //
-  //
-  AliCDBManager::Instance()->SetRun(1) ;
-  AliCDBManager::Instance()->SetDefaultStorage("local://$ALICE_ROOT/OCDB");
-  AliTPCClusterParam * clusterParam = AliTPCcalibDB::Instance()->GetClusterParam();
   AliTPCAnalysisTaskcalib *task1=new AliTPCAnalysisTaskcalib("TPC calibration task");
+  //
+  SetupCalibTask(task1);
+  //
+  mgr->AddTask(task1);
+  AliAnalysisDataContainer *cinput1 = mgr->GetCommonInputContainer();
   
-  AliTPCcalibTracksCuts *cuts = new AliTPCcalibTracksCuts(40, 0.4, 0.5, 0.13, 0.018);
+  if (!cinput1) cinput1 = mgr->CreateContainer("cchain",TChain::Class(), 
+                                      AliAnalysisManager::kInputContainer);
 
+  AliAnalysisDataContainer *coutput1 =mgr->CreateContainer("TPCCalib",TObjArray::Class(), AliAnalysisManager::kOutputContainer, "CalibObjects.root");  
+  mgr->ConnectInput(task1,0,cinput1);
+  mgr->ConnectOutput(task1,0,coutput1);
+  //
+  if (!mgr->InitAnalysis()) return 0;
+  mgr->PrintStatus();   
+  stopwatch.Stop();
+  stopwatch.Print();
+  return mgr;
+}
+
+
+
+void SetupCalibTask(AliTPCAnalysisTaskcalib* task1){
+  //
+  // Configure calibration task
+  //
+  AliTPCClusterParam * clusterParam = AliTPCcalibDB::Instance()->GetClusterParam();  
+  AliTPCcalibTracksCuts *cuts = new AliTPCcalibTracksCuts(40, 0.4, 0.5, 0.13, 0.018);  
   //
   AliTPCcalibTracks *calibTracks =  new AliTPCcalibTracks("calibTracks", "Resolution calibration object for tracks", clusterParam, cuts); 
 
@@ -81,7 +104,30 @@ TObject  * SetupCalibTask(char * prefix ="/V12/") {
 
   AliTPCcalibUnlinearity *calibUnlinearity = new AliTPCcalibUnlinearity("calibUnlinearity","calibUnlinearity");
   AliTPCcalibUnlinearity *calibUnlinearityAll = new AliTPCcalibUnlinearity("calibUnlinearityAll","calibUnlinearityAll");
-
+  //
+  //
+  //
+  AliExternalComparison *compCosmic = new AliExternalComparison("compCosmic","compCosmic");
+  compCosmic->SetDefaultRange(1,160,20);
+  //
+  compCosmic->SetResolRange(0,-1.5,1.5,100);
+  compCosmic->SetResolRange(1,-1,1,100);
+  compCosmic->SetResolRange(2,-0.02,0.02,100);
+  compCosmic->SetResolRange(3,-0.02,0.02,100);
+  compCosmic->SetResolRange(4,-0.15,0.15,100);
+  //
+  compCosmic->SetParameterRange(0,-30,30,10);
+  compCosmic->SetParameterRange(1,-250,250,10);
+  compCosmic->SetParameterRange(2,-1,1,10);
+  compCosmic->SetParameterRange(3,-1.5,1.5,20);
+  compCosmic->SetParameterRange(4,-2,2,20);
+  compCosmic->SetParameterRange(5,0,90,10);
+  compCosmic->SetParameterRange(6,-3.14,3.14,20);
+  //
+  compCosmic->SetDistCut(3,8,0.06,0.06,0.2);
+  compCosmic->SetPullDistCut(10,20,20,20,10);
+  calibCosmic->SetComparison(compCosmic);
+  //
   //
   calibTracks->SetDebugLevel(2);
   calibTracks->SetStreamLevel(20);
@@ -91,12 +137,12 @@ TObject  * SetupCalibTask(char * prefix ="/V12/") {
   calibAlign->SetStreamLevel(10);
   calibAlignAll->SetDebugLevel(20);
   calibAlignAll->SetStreamLevel(10);
-  calibLaser->SetDebugLevel(0);
-  calibLaser->SetStreamLevel(0);
+  calibLaser->SetDebugLevel(20);
+  calibLaser->SetStreamLevel(20);
   calibCosmic->SetDebugLevel(20);
-  calibCosmic->SetStreamLevel(2);
-  calibCalib->SetDebugLevel(0);
-  calibCalib->SetStreamLevel(0);
+  calibCosmic->SetStreamLevel(20);
+  calibCalib->SetDebugLevel(20);
+  calibCalib->SetStreamLevel(20);
   calibTime->SetDebugLevel(20);
   calibTime->SetStreamLevel(10);
   //
@@ -105,7 +151,8 @@ TObject  * SetupCalibTask(char * prefix ="/V12/") {
   calibUnlinearityAll->SetDebugLevel(20);
   calibUnlinearityAll->SetStreamLevel(10);
   
-  calibCalib->SetTriggerMask(-1,-1,kFALSE);       //accept everything 
+  //  calibCalib->SetTriggerMask(-1,-1,kFALSE);       //accept everything 
+  calibCalib->SetTriggerMask(-1,-1,kTRUE);       //accept everything  - except laser
   calibTracks->SetTriggerMask(-1,16,kTRUE);      //reject laser trigger, accept everything else
   calibTracksGain->SetTriggerMask(-1,16,kTRUE);  //reject laser trigger, accept everything else
   calibAlign->SetTriggerMask(-1,-1,kTRUE);       //accept everything 
@@ -121,11 +168,11 @@ TObject  * SetupCalibTask(char * prefix ="/V12/") {
  // ---*---*-----*-*-----*----------*---
   // ADD CALIB JOBS HERE!!!!!!!!!!!!!!!!
   task1->AddJob(calibCalib);
-  //task1->AddJob(calibAlign);
+  task1->AddJob(calibAlign);
   //task1->AddJob(calibAlignAll);
   //task1->AddJob(calibLaser);
-  //task1->AddJob(calibCosmic);
-  task1->AddJob(calibTime);
+  task1->AddJob(calibCosmic);
+  //task1->AddJob(calibTime);
 
   //task1->AddJob(calibTracksGain);
   //task1->AddJob(calibTracks);
@@ -139,31 +186,7 @@ TObject  * SetupCalibTask(char * prefix ="/V12/") {
   gSystem->mkdir(path);
   task1->SetDebugOuputhPath(path.Data());
 
-  mgr->AddTask(task1);
-
-  AliAnalysisDataContainer *cinput1
-    =mgr->GetCommonInputContainer();
-  AliAnalysisDataContainer *coutput1
-    =mgr->CreateContainer("TPCCalib",TObjArray::Class(),
-			  AliAnalysisManager::kOutputContainer,
-			  "CalibObjects.root");
-  
-  //coutput1->SetSpecialOutput(kTRUE);
-  //coutput1->SetFileName("CalibObjectFile.root");
-  mgr->ConnectInput(task1,0,cinput1);
-  mgr->ConnectOutput(task1,0,coutput1);
-  //
-  //mgr->SetSpecialOutputLocation(path->Data());
-
-  if (!mgr->InitAnalysis()) return 0;
-  mgr->PrintStatus(); 
-  
-  stopwatch.Stop();
-  stopwatch.Print();
-  return mgr;
 }
-
-
 
 
 
