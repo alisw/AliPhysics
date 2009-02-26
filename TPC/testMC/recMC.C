35c92a87779d4d4d9ac48c83040f94e656dcb617
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-26 11:22:58 +0000

    Adding updated version of test recMC.C

---
+++ TPC/testMC/recMC.C
@@ -0,0 +1,32 @@
+void recMC(const char* tpcDBpath="local://$ALICE_ROOT"){
+  AliTPCRecoParam * tpcRecoParam = AliTPCRecoParam::GetLowFluxParam();  
+  AliTPCReconstructor::SetStreamLevel(10);
+  //
+
+  AliReconstruction rec;
+  rec.SetSpecificStorage("TPC/*/*",tpcDBpath);
+  rec.SetRecoParam("TPC",tpcRecoParam);
+  //
+  rec.SetLoadAlignData("");
+  rec.SetWriteESDfriend(kTRUE);
+  rec.SetFillTriggerESD(kFALSE);
+  rec.SetRunVertexFinder(kTRUE);
+  rec.SetWriteAlignmentData(kTRUE);
+  rec.SetRunReconstruction("ITS TPC TRD TOF");
+  //rec.SetRunReconstruction("ITS TPC");
+  rec.SetCleanESD(kFALSE);
+  //disable HLT and QA
+  //
+  rec.SetRunHLTTracking(kFALSE);
+  rec.SetUseHLTData(" ");
+  rec.SetRunQA(kFALSE);
+  //
+  rec.SetWriteESDfriend(kTRUE);
+  rec.SetWriteAlignmentData(kTRUE);
+  rec.SetUniformFieldTracking(kFALSE);
+  rec.SetWriteAlignmentData(kTRUE);
+  //
+
+  //
+  rec.Run();
+}
