e53176c3deda5082790b29545aebc786c4e3f9ca
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-03-03 13:52:19 +0000

    recraw.C removed, using recraw/rec.C instead. Restoring the original test layout

--- test/ppbench/recraw/rec.C
+++ test/ppbench/recraw/rec.C
@@ -14,6 +14,15 @@ void rec() {
   reco.SetSpecificStorage("GRP/GRP/Data",
 			  Form("local://%s/..",gSystem->pwd()));
 
+  reco.SetRunQA("ALL:ALL") ;
+  
+  AliQA::SetQARefStorage("local://$ALICE_ROOT/QAref") ;
+  
+  for (Int_t det = 0 ; det < AliQA::kNDET ; det++) {
+    reco.SetQACycles(det, 999) ;
+    reco.SetQAWriteExpert(det) ; 
+  }
+
   TStopwatch timer;
   timer.Start();
   reco.Run();
