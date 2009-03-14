96bea73e963aac74e248894731ae37f1e7824ca8
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-03-14 09:43:35 +0000

    naming

--- PWG2/FLOW/AliFlowCommon/AliFlowTrackSimple.h
+++ PWG2/FLOW/AliFlowCommon/AliFlowTrackSimple.h
@@ -28,18 +28,14 @@ class AliFlowTrackSimple: public TObject {
   Double_t Pt()  const; 
   Double_t Phi() const; 
   TBits    GetFlowBits() const;
-  //Bool_t   UseForIntegratedFlow() const;
-  //Bool_t   UseForDifferentialFlow() const;
-  Bool_t   InRPSelection() const; //will replace UseForIntegratedFlow()
-  Bool_t   InPOISelection() const; //will replace UseForDifferentialFlow()
-  
+  Bool_t   InRPSelection() const; 
+  Bool_t   InPOISelection() const; 
+    
   void SetEta(Double_t eta);
   void SetPt(Double_t pt); 
   void SetPhi(Double_t phi);
-  void SetForIntegratedFlow(Bool_t b);
-  void SetForDifferentialFlow(Bool_t b);
-  void SetForRPSelection(Bool_t b); //will replace SetForIntegratedFlow
-  void SetForPOISelection(Bool_t b); //will replace SetForDifferentialFlow
+  void SetForRPSelection(Bool_t b); 
+  void SetForPOISelection(Bool_t b); 
   
  private:
   Double_t fEta;    // eta
@@ -61,10 +57,6 @@ inline Double_t AliFlowTrackSimple::Phi() const {
   return this->fPhi; }
 inline TBits AliFlowTrackSimple::GetFlowBits() const { 
   return this->fFlowBits; }
-//inline Bool_t AliFlowTrackSimple::UseForIntegratedFlow() const { 
-//  return this->fFlowBits.TestBitNumber(0); }
-//inline Bool_t AliFlowTrackSimple::UseForDifferentialFlow() const { 
-//  return this->fFlowBits.TestBitNumber(1); }
 inline Bool_t AliFlowTrackSimple::InRPSelection() const { 
   return this->fFlowBits.TestBitNumber(0); }
 inline Bool_t AliFlowTrackSimple::InPOISelection() const { 
@@ -77,10 +69,6 @@ inline void AliFlowTrackSimple::SetPt(Double_t val) {
   //  cout << "pt set to:" << fPt << endl;}
 inline void AliFlowTrackSimple::SetPhi(Double_t val) {
   fPhi = val; }
-//inline void AliFlowTrackSimple::SetForIntegratedFlow(Bool_t val) { //replace
-//  fFlowBits.SetBitNumber(0,val); }
-//inline void AliFlowTrackSimple::SetForDifferentialFlow(Bool_t val) { //replace
-//  fFlowBits.SetBitNumber(1,val); }
 inline void AliFlowTrackSimple::SetForRPSelection(Bool_t val) {
   fFlowBits.SetBitNumber(0,val); }
 inline void AliFlowTrackSimple::SetForPOISelection(Bool_t val) {
