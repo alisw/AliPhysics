cedc5fbee083ee0484fa48a740f7ca6e0847a9c6
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-04-02 09:41:03 +0000

    Adding the proton task in the SPECTRA dir and in the corresponding library (request from Mihaela)

--- PWG2/PWG2spectraLinkDef.h
+++ PWG2/PWG2spectraLinkDef.h
@@ -4,8 +4,10 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class AliProtonAnalysisBase+;
 #pragma link C++ class AliProtonAnalysis+;
 #pragma link C++ class AliProtonQAAnalysis+;
+#pragma link C++ class AliAnalysisTaskProtons+;
 #pragma link C++ class AliAnalysisTaskCheckV0+;
 #pragma link C++ class AliAnalysisTaskCheckCascade+;
 #pragma link C++ class AliAnalysisTaskStrange+;
