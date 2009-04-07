ea61fc5d812247872d5c3596dc1104be899f3964
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-04-07 08:56:52 +0000

    oving all the proton analysis related macros to the proper directory

---
+++ PWG2/SPECTRA/macros/AddTaskProtons.C
@@ -0,0 +1,42 @@
+AliAnalysisTaskProtons *AddTaskProtons(const char *analysisType="Hybrid",
+				       const char *pidMode="Bayesian")
+{
+  // Creates a proton analysis task and adds it to the analysis manager.
+  
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    Error("AddTaskESDFilter", "No analysis manager to connect to.");
+    return NULL;
+  }   
+  
+  // Check the analysis type using the event handlers connected to the analysis manager.
+  //==============================================================================
+  if (!mgr->GetInputEventHandler()) {
+    ::Error("AddTaskProtons", "This task requires an input event handler");
+    return NULL;
+  }   
+  TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+  gROOT->LoadMacro("$ALICE_ROOT/PWG2/SPECTRA/macros/configProtonAnalysis.C");
+  AliProtonAnalysis *pa = 0;
+  if (type=="ESD") pa = GetProtonAnalysisObject("ESD", analysisType, pidMode);
+  else if (type=="AOD") pa = GetProtonAnalysisObject("AOD", analysisType, pidMode);
+  else pa = GetProtonAnalysisObject("MC", analysisType, pidMode);
+
+  // Create the task, add it to manager and configure it.
+  //===========================================================================
+  AliAnalysisTaskProtons *taskproton = new AliAnalysisTaskProtons("TaskProtons");
+  mgr->AddTask(taskproton);
+  taskproton->SetAnalysisObject(pa);
+
+  // Create ONLY the output containers for the data produced by the task.
+  // Get and connect other common input/output containers via the manager as below
+  //==============================================================================
+  AliAnalysisDataContainer *cout_proton = mgr->CreateContainer("protonhist", TList::Class(),AliAnalysisManager::kOutputContainer,"protonhist.root");                              
+  mgr->ConnectInput(taskproton, 0, mgr->GetCommonInputContainer());
+  mgr->ConnectOutput(taskproton, 0, cout_proton);
+  
+  // Return task pointer at the end
+  return taskproton;
+}
