1bd45300b24375e6bebd0add99839a8250e9753a
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-04-28 21:28:21 +0000

    New test macro for HF mass fitter (Chiara)

---
+++ PWG3/vertexingHF/AliHFMassFitterTest.C
@@ -0,0 +1,30 @@
+// hinvMass -> invariant mass histogram to fit
+// typeb -> type of fit function for background. Can be 0, 1, 2, 3 (see AliHFMassFitter.cxx for details)
+// types -> type of fit function for signal. Can be 0, 1 (see AliHFMassFitter.cxx for details)
+// factor4refl -> sigmaRefl=factor4refl*sigmaSgn (have a look to AliHFMassFitter.cxx for details). Set it if types=1
+
+void AliHFMassFitterTest(TH1F *hinvMass, Int_t typeb=0, Int_t types=0, Int_t factor4refl=1){
+  
+  Int_t nbin=hinvMass->GetNbinsX();
+  Double_t min=hinvMass->GetBinLowEdge(1),max=hinvMass->GetBinLowEdge(nbin)+hinvMass->GetBinWidth(nbin);
+  //TH1F *hMass=new TH1F("hMass","Invariant Mass",nbin,min,max);
+  
+  AliHFMassFitter *fitter=new AliHFMassFitter(hinvMass,min, max,1,typeb,types);
+  
+  fitter->SetReflectionSigmaFactor(factor4refl);
+  //fitter->SetRangeFit(min,max);
+  fitter->MassFitter();
+  
+  TNtuple *ntu=fitter->NtuParamOneShot("ntuInvMass");
+  Double_t err1,err3,err6;
+  Double_t significance1sigma,significance3sigma,significance6sigma;
+  fitter->Significance(1,significance1sigma,err1);
+  fitter->Significance(3,significance3sigma,err3);
+  fitter->Significance(6,significance6sigma,err6);
+
+  cout<<"N sigma \t significance\n";
+  cout<<"1\t"<<significance1sigma<<endl;
+  cout<<"3\t"<<significance3sigma<<endl;
+  cout<<"6\t"<<significance6sigma<<endl;
+
+}
