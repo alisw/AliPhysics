0cd9169956c473239e40eabffc66a084ccd70a0b
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-05-04 09:04:29 +0000

    Compilation warnings fixed. Functionality updated.

--- PHOS/AliPHOSFastAltroFit.cxx
+++ PHOS/AliPHOSFastAltroFit.cxx
@@ -34,6 +34,12 @@
 // --- AliRoot header files ---
 #include "AliPHOSFastAltroFit.h"
 
+#include <TH1.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TGraphErrors.h>
+#include <TMath.h>
+
 #include <math.h>
 
 ClassImp(AliPHOSFastAltroFit)
@@ -41,49 +47,129 @@ ClassImp(AliPHOSFastAltroFit)
 //____________________________________________________________________________
   AliPHOSFastAltroFit:: AliPHOSFastAltroFit() 
 : TNamed(), 
-  fSig(0),fTau(0),fN(0), fAmp(0),fAmpErr(0),fT0(0),fT0Err(0),fChi2(0.),fNDF(0) 
+  fSig(0),fTau(0),fN(0),fPed(0), fAmp(0),fAmpErr(0),fT0(0),fT0Err(0),fChi2(0.),fNDF(0)
+,fNfit(0),fTfit(0),fAmpfit(0), fStdFun(0)
 {
 }
 
 //____________________________________________________________________________
 AliPHOSFastAltroFit::AliPHOSFastAltroFit(const char* name, const char* title, const Double_t tau)
   : TNamed(name, title), 
-  fSig(0),fTau(0),fN(2), fAmp(0),fAmpErr(0),fT0(0),fT0Err(0),fChi2(0.),fNDF(0) 
+  fSig(0),fTau(tau),fN(2),fPed(0), fAmp(0),fAmpErr(0),fT0(0),fT0Err(0),fChi2(0.),fNDF(0) 
+ ,fNfit(0),fTfit(0),fAmpfit(0), fStdFun(0)
 {
   if(strlen(name)==0) SetName("FastAltroFit");
 }
 
 //____________________________________________________________________________
+AliPHOSFastAltroFit::AliPHOSFastAltroFit(const AliPHOSFastAltroFit &obj)
+  : TNamed(obj), 
+  fSig(0),fTau(0),fN(2),fPed(0), fAmp(0),fAmpErr(0),fT0(0),fT0Err(0),fChi2(0.),fNDF(0) 
+ ,fNfit(0),fTfit(0),fAmpfit(0), fStdFun(0)
+{
+}
+
+//____________________________________________________________________________
 AliPHOSFastAltroFit::~AliPHOSFastAltroFit() 
 {  
+  if(fTfit) delete [] fTfit;
+  if(fAmpfit) delete [] fAmpfit;
+}
+
+//____________________________________________________________________________
+AliPHOSFastAltroFit& AliPHOSFastAltroFit::operator= (const AliPHOSFastAltroFit &/*obj*/)
+{
+  // Not implemented yet
+  return *this;
+}
+
+//____________________________________________________________________________
+void AliPHOSFastAltroFit::FastFit(TH1F* h, Double_t sig, Double_t tau, Double_t ped)
+{
+  // Service method for convinience only  
+  Reset();
+
+  if(h==0) return;
+  Int_t n = h->GetNbinsX();
+  if(n<=0) return;
+
+  Int_t* t = new Int_t[n];
+  Int_t* y = new Int_t[n];
+
+  for(Int_t i=0; i<n; i++) {
+    t[i] = Int_t(h->GetBinCenter(i+1));
+    y[i] = Int_t(h->GetBinContent(i+1));
+  }
+  FastFit(t,y,n, sig,tau,ped);
+
+  delete [] t;
+  delete [] y;
 }
 
 void AliPHOSFastAltroFit::FastFit(Int_t* t, Int_t* y, Int_t n, Double_t sig, Double_t tau, Double_t ped)
 {
+  Reset();
+
   fSig = sig;
   fTau = tau;
+  fPed = ped;
 
-  Double_t* tn = new Double_t[n]; 
-  Double_t* yn = new Double_t[n]; 
-  Int_t nn=0;
+  if(fTfit) delete [] fTfit;
+  if(fAmpfit) delete [] fAmpfit;
 
-  DeductPedestal(t,y,n,  tau,ped,  tn,yn,nn);
-  //  printf(" n %i : nn %i : ped %f \n", n, nn, ped);
-  // for(int i=0; i<nn; i++) 
-  // printf(" i %i : yn %7.2f : tn %7.2f \n", i, yn[i], tn[i]); 
+  fNfit   = 0;
+  fTfit   = new Double_t[n]; 
+  fAmpfit = new Double_t[n]; 
 
-  FastFit(tn,yn,nn,sig,tau, fAmp,fAmpErr, fT0,fT0Err,fChi2);
+  DeductPedestal(t,y,n,  tau,ped,  fTfit,fAmpfit,fNfit);
+  //  printf(" n %i : fNfit %i : ped %f \n", n, fNfit, ped);
+  // for(int i=0; i<fNfit; i++) 
+  // printf(" i %i : fAmpfit %7.2f : fTfit %7.2f \n", i, fAmpfit[i], fTfit[i]); 
 
-  if(fChi2> 0.0) fNDF = nn - 2;
-  else           fNDF = 0; 
+  if(fNfit>=2) {
+    FastFit(fTfit,fAmpfit,fNfit,sig,tau, fAmp,fAmpErr, fT0,fT0Err,fChi2);
 
-  delete [] tn;
-  delete [] yn;
+    if(fChi2> 0.0) fNDF = fNfit - 2;
+    else           fNDF = 0; 
+  } else if(fNfit==1){
+    Reset(); // What to do here => fT0 = fTfit[0]; fAmp = fAmpFit[0] ??
+  } else {
+    Reset();
+  }
+}
+
+void AliPHOSFastAltroFit::Reset()
+{
+  fSig  = fTau = 0.0;
+  fAmp  = fAmpErr = fT0 = fT0Err = 0.0;
+  fChi2 = -1.;
+  fNDF  = fNfit = 0;
+  fTfit = fAmpfit = 0;
+}
+
+
+void AliPHOSFastAltroFit::GetFitResult(Double_t &amp,Double_t &eamp,Double_t &t0,Double_t &et0, 
+Double_t &chi2, Int_t &ndf) 
+{
+  amp  = fAmp;
+  eamp = fAmpErr;
+  t0   = fT0;
+  et0  = fT0Err;
+  chi2 = fChi2;
+  ndf  = fNDF;
+}
+
+void AliPHOSFastAltroFit::GetFittedPoints(Int_t &nfit, Double_t* ar[2])
+{
+  nfit  = fNfit;
+  ar[0] = fTfit;
+  ar[1] = fAmpfit;
 }
 
 void AliPHOSFastAltroFit::DeductPedestal(Int_t* t, Int_t* y, Int_t n, Double_t tau, Double_t ped, 
   Double_t* tn, Double_t* yn, Int_t &nn)
 {
+  static Double_t yMinUnderPed=2.; // should be tune
   Int_t ymax=0, nmax=0;
   for(Int_t i=0; i<n; i++){
     if(y[i]>ymax) {
@@ -92,19 +178,41 @@ void AliPHOSFastAltroFit::DeductPedestal(Int_t* t, Int_t* y, Int_t n, Double_t t
     }
   }
   Int_t i1 = nmax - Int_t(tau);
-  Int_t i2 = n; // No rejection of points from right - should correct for EMCAL
+  //i1 = 0;
   i1 = i1<0?0:i1;
+  Int_t i2 = n;
 
   nn = 0;
-  Double_t yd=0.0;
+  Double_t yd=0.0, tdiff=0.0;;
   for(Int_t i=i1; i<i2; i++) {
-    yd = Double_t(y[i]) - ped;
-    if(yd>1.) {
-      yn[nn] = yd;
-      tn[nn] = Double_t(t[i]);
-      nn++;
+    if(ped>0.0) {
+      yd = Double_t(y[i]) - ped;
+    } else {
+      yd = Double_t(y[i]);
     }
+    if(yd < yMinUnderPed) continue;
+
+    if(i>i1 && nn>0){
+      tdiff = t[i] - tn[nn-1];
+      //      printf(" i %i : nn %i : tdiff %6.2f : tn[nn] %6.2f \n", i,nn, tdiff, tn[nn-1]);
+      if(tdiff>1.) {
+     // discard previous points if its are before maximum point and with gap>1
+        if(i<nmax ) {
+          nn = 0; // nn--;
+     // if point with gap after maximum - finish selection
+        } else if(i>=nmax ) {
+          break;
+        }
+      }
+     // Far away from maximum
+     //if(i-nmax > Int_t(5*tau))              break;
+    }
+    tn[nn] = Double_t(t[i]);
+    yn[nn] = yd;
+    //printf("i %i : nn %i : tn %6.2f : yn %6.2f \n", i, nn, tn[nn], yn[nn]); 
+    nn++;
   }
+  //printf(" nmax %i : n %i : nn %i i1 %i \n", nmax, n, nn, i1);
 }
 
 void AliPHOSFastAltroFit::FastFit(Double_t* t, Double_t* y, Int_t n, Double_t sig, Double_t tau,
@@ -119,11 +227,12 @@ Double_t &amp, Double_t &eamp, Double_t &t0, Double_t &et0, Double_t &chi2)
   //   tau   - filter time response (in timebin units)
   // Output:
   //       amp - amplitude at t0;
-  //        t0 - timeof max amplitude; 
+  //        t0 - time of max amplitude; 
   static Double_t xx; // t/tau
   static Double_t a, b, c;
   static Double_t f02, f12, f22;    // functions
   static Double_t f02d, f12d, f22d; // functions derivations
+
   chi2 = -1;
   if(n<=0) {
     printf("<I> FastFit : n<=0 \n"); 
@@ -166,6 +275,10 @@ Double_t &amp, Double_t &eamp, Double_t &t0, Double_t &et0, Double_t &chi2)
       t0   = t0_2; 
       chi2 = chi2_2; 
     }
+    if(tau<3.) { // EMCAL case : small tau 
+      t0 += -0.03; 
+      Amplitude(t,y,n, sig, tau, t0, amp, chi2);
+    }
     CalculateParsErrors(t, y, n, sig, tau, amp, t0, eamp, et0);
 
     // Fill1();
@@ -186,7 +299,7 @@ Bool_t AliPHOSFastAltroFit::QuadraticRoots(Double_t a, Double_t b, Double_t c, D
   dtmp = b*b - 4.*a*c;
 
   if(dtmp>=-1.0e-7 && dtmp<0.0) {
-    printf("QuadraticRoots : small negative square : dtmp %f ", dtmp);
+    printf("QuadraticRoots : small negative square : dtmp %f \n", dtmp);
     dtmp = 0.0;
   }
   if(dtmp>=0.0) {
@@ -197,7 +310,7 @@ Bool_t AliPHOSFastAltroFit::QuadraticRoots(Double_t a, Double_t b, Double_t c, D
     //    printf(" x1 %f : x2 %f \n", x1, x2);
     return kTRUE;
   } else {
-    printf("QuadraticRoots : negative square : dtmp %f ", dtmp);
+    printf("QuadraticRoots : negative square : dtmp %f \n", dtmp);
     return kFALSE;
   }
 }
@@ -216,7 +329,7 @@ Double_t t0, Double_t &amp, Double_t &chi2)
     amp += f*y[i];
     den += f*f;
   }
-  amp /= den;
+  if(den>0.0) amp /= den;
   //
   // chi2 calculation
   //
@@ -228,16 +341,18 @@ Double_t t0, Double_t &amp, Double_t &chi2)
     f    = amp*x*x*f02;
     dy   = y[i]-f;
     chi2 += dy*dy;
-    //printf(" %i : y %f -> f %f : dy %f \n", i, y[i], f, dy); 
+    //    printf(" %i : y %f -> f %f : dy %f \n", i, y[i], f, dy); 
   }
   chi2 /= (sig*sig);
 }
 
-void AliPHOSFastAltroFit::CalculateParsErrors(Double_t* t, Double_t* y, Int_t n, Double_t sig, Double_t tau,
-Double_t &amp, Double_t &t0, Double_t &eamp, Double_t &et0)
+void AliPHOSFastAltroFit::CalculateParsErrors(Double_t* t, Double_t* /*y*/, Int_t n, Double_t sig, 
+					      Double_t tau, Double_t &amp, Double_t &t0, 
+					      Double_t &eamp, Double_t &et0)
 {
   // fmax_nk = (n/k)**n*exp(-n) => n=k=2 => exp(-n) = exp(-2.)
   static Double_t cc = exp(-2.);
+  //   static Double_t cc = exp(-fN); // mean(N)~1.5 ??
 
   Double_t sumf2=0.0, sumfd2=0.0, x, f02, f12, f22, f22d;
 
@@ -255,4 +370,91 @@ Double_t &amp, Double_t &t0, Double_t &eamp, Double_t &et0)
   et0  = sig/amp/sqrt(sumfd2);
   amp *= cc;
   eamp = sig*cc/sqrt(sumf2); 
+}
+
+//
+// Drawing
+//
+TCanvas* AliPHOSFastAltroFit::DrawFastFunction()
+{
+  // QA of fitting
+  if(fNfit<=0) return 0; // no points
+
+  static TCanvas *c = 0;
+  if(c==0) {
+    c =  new TCanvas("fastFun","fastFun",800,600);
+  }
+
+  c->cd();
+  
+  Double_t* eamp = new Double_t[fNfit];
+  Double_t* et   = new Double_t[fNfit];
+
+  for(Int_t i=0; i<fNfit; i++) {
+    eamp[i] = fSig;
+    et[i]   = 0.0;
+  }
+
+  TGraphErrors *gr = new TGraphErrors(fNfit, fTfit,fAmpfit, et,eamp);
+  gr->Draw("Ap");
+  gr->SetTitle(Form("Fast Fit : #chi^{2}/ndf = %8.2f / %i", GetChi2(), GetNDF()));
+  gr->GetHistogram()->SetXTitle(" time bin ");
+  gr->GetHistogram()->SetYTitle(" amplitude ");
+
+  if(fStdFun==0) {
+     fStdFun = new TF1("stdFun", StdResponseFunction, 0., fTfit[fNfit-1]+2., 5);    
+     fStdFun->SetParNames("amp","t0","tau","N","ped");
+  }
+  fStdFun->SetParameter(0, GetEnergy());
+  fStdFun->SetParameter(1, GetTime() + GetTau());
+  fStdFun->SetParameter(2, GetTau());  // 
+  fStdFun->SetParameter(3, GetN());    // 2
+  fStdFun->SetParameter(4, 0.);  // 
+  
+  fStdFun->SetLineColor(kBlue);
+  fStdFun->SetLineWidth(1);
+
+  fStdFun->Draw("same");
+
+  delete [] eamp;
+  delete [] et;
+
+  c->Update();
+
+  return c;
+}
+
+Double_t AliPHOSFastAltroFit::StdResponseFunction(Double_t *x, Double_t *par)
+{
+  // Standard Response Function : 
+  // look to Double_t AliEMCALRawUtils::RawResponseFunction(Double_t *x, Double_t *par)
+  // Using for drawing only.
+  // 
+  // Shape of the electronics raw reponse:
+  // It is a semi-gaussian, 2nd order Gamma function (n=2) of the general form
+  //
+  // t' = (t - t0 + tau) / tau
+  // F = A * t**N * exp( N * ( 1 - t) )   for t >= 0
+  // F = 0                                for t < 0 
+  //
+  // parameters:
+  // A:   par[0]   // Amplitude = peak value
+  // t0:  par[1]
+  // tau: par[2]
+  // N:   par[3]
+  // ped: par[4]
+  //
+  static Double_t signal , tau, n, ped, xx;
+
+  tau = par[2];
+  n   = par[3];
+  ped = par[4];
+  xx = ( x[0] - par[1] + tau ) / tau ;
+
+  if (xx <= 0) 
+    signal = ped ;  
+  else {  
+    signal = ped + par[0] * TMath::Power(xx , n) * TMath::Exp(n * (1 - xx )) ; 
+  }
+  return signal ;  
 }
--- PHOS/AliPHOSFastAltroFit.h
+++ PHOS/AliPHOSFastAltroFit.h
@@ -17,6 +17,12 @@
 #include <TNamed.h>
 #include <TArrayD.h>
 // --- ROOT system ---
+class TCanvas;
+class TVirtualPad;
+class TF1;
+class TH1F;
+//class TLegend;
+//class TPaveStats;
 
 class AliPHOSFastAltroFit : public TNamed {
 
@@ -28,19 +34,27 @@ public:
 
   void SetTau(const Double_t tau) {fTau = tau;}
 
+  void FastFit(TH1F* h, Double_t sig, Double_t tau, Double_t ped);
   void FastFit(Int_t* t, Int_t* y, Int_t n, Double_t sig, Double_t tau, Double_t ped);
+  void Reset();
 
-  void GetFitResult(Double_t &amp, Double_t &eamp, Double_t &t0, Double_t &et0, Double_t &chi2,
-		    Int_t &fNDF) {};
+  void GetFitResult(Double_t &amp, Double_t &eamp, Double_t &t0, Double_t &et0, 
+                    Double_t &chi2,Int_t &ndf);
   Double_t GetSig()    const {return fSig;}
   Double_t GetTau()    const {return fTau;}
   Double_t GetN()      const {return fN;}
-  Double_t GetAmp()    const {return fAmp;}
+  Double_t GetPed()    const {return fPed;}
+
+  Double_t GetEnergy() const {return fAmp;}
+  Double_t GetAmp()    const {return GetEnergy();}
   Double_t GetAmpErr() const {return fAmpErr;}
-  Double_t GetT0()     const {return fT0;}
+  Double_t GetTime()   const {return fT0;}
+  Double_t GetT0()     const {return GetTime();}
   Double_t GetT0Err()  const {return fT0Err;}
   Double_t GetChi2()   const {return fChi2;}
-  Double_t GetNDF()    const {return fNDF;}
+  Int_t    GetNDF()    const {return fNDF;}
+  Int_t    GetNfit()   const {return fNfit;}
+  void     GetFittedPoints(Int_t &nfit, Double_t* ar[2]);
 
   static void DeductPedestal(Int_t* t, Int_t* y, Int_t n, Double_t ped, Double_t tau, 
   Double_t* tn, Double_t* yn, Int_t &nn);
@@ -52,17 +66,33 @@ public:
   static void CalculateParsErrors(Double_t* t, Double_t* y, Int_t n, Double_t sig, Double_t tau,
 				 Double_t &amp, Double_t &t0, Double_t &eamp, Double_t &et0);
 
- protected:
+  // Drawing for QA
+  TCanvas* DrawFastFunction(); // *MENU*
+  static Double_t StdResponseFunction(Double_t *x, Double_t *par); 
+
+private:
+  AliPHOSFastAltroFit(const AliPHOSFastAltroFit &obj);
+  AliPHOSFastAltroFit& operator= (const AliPHOSFastAltroFit &obj);
+
+protected:
   Double_t fSig;
   Double_t fTau; // filter time response
   Double_t fN;   // order of function (equal 2)
+  Double_t fPed; // pedestal
 
   Double_t fAmp;
-  Double_t fAmpErr;
+  Double_t fAmpErr; 
   Double_t fT0;
   Double_t fT0Err;
   Double_t fChi2;
   Int_t    fNDF;
+
+  // Working variable
+  Int_t     fNfit;   //! number points for fit
+  Double_t* fTfit;   //! points for fit after selection
+  Double_t* fAmpfit; //!
+  // 
+  TF1*      fStdFun; //! function for drawing
 
   ClassDef(AliPHOSFastAltroFit,1) // Class for fast altro fitting
 };
