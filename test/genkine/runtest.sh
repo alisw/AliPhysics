e751220a0cd3443cd3c6375c22db113f57664b13
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-05-11 10:20:30 +0000

    Revert changes from rev. 30849

--- test/genkine/runtest.sh
+++ test/genkine/runtest.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -rf */*.root */*.log */*.dat
 cd ./gen
-aliroot -b -q rungen.C\(1\) 2>&1 | tee gen.log
+aliroot -b -q rungen.C\(5\) 2>&1 | tee gen.log
 chmod a-w *.root
 cd ../sim
-aliroot -b -q sim.C\(1\) 2>&1 | tee sim.log
-#aliroot -b -q rec.C      2>&1 | tee rec.log
+aliroot -b -q sim.C\(5\) 2>&1 | tee sim.log
+aliroot -b -q rec.C      2>&1 | tee rec.log
 aliroot -b -q ${ALICE_ROOT}/STEER/CheckESD.C 2>&1 | tee check.log
 aliroot -b -q ${ALICE_ROOT}/STEER/CreateAODfromESD.C 2>&1 | tee aod.log
 
