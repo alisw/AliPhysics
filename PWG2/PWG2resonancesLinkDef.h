03262fc3fc2126dc9ab3433f0105b92672a4fe0c
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-05-15 09:36:56 +0000

    modified compilation files to comply with changes

--- PWG2/PWG2resonancesLinkDef.h
+++ PWG2/PWG2resonancesLinkDef.h
@@ -1,25 +1,20 @@
 #ifdef __CINT__
 
-#pragma link C++ class AliRsnMCInfo+;
 #pragma link C++ class AliRsnDaughter+;
 #pragma link C++ class AliRsnPairParticle+;
 
 #pragma link C++ class AliRsnCut+;
+#pragma link C++ class AliRsnCutStd+;
+#pragma link C++ class AliRsnCutBetheBloch+;
 #pragma link C++ class AliRsnCutSet+;
 #pragma link C++ class AliRsnCutMgr+;
 #pragma link C++ class AliRsnExpression+;
 #pragma link C++ class AliRsnVariableExpression+;
 
-#pragma link C++ class AliRsnPID+;
 #pragma link C++ class AliRsnPIDIndex+;
 #pragma link C++ class AliRsnPIDDefESD+;
 
 #pragma link C++ class AliRsnEvent+;
-#pragma link C++ class AliRsnEventBuffer+;
-
-#pragma link C++ class AliRsnReader+;
-#pragma link C++ class AliRsnReaderTask+;
-#pragma link C++ class AliRsnReaderTaskSE+;
 
 #pragma link C++ class AliRsnPairDef+;
 #pragma link C++ class AliRsnHistoDef+;
@@ -27,10 +22,15 @@
 #pragma link C++ class AliRsnFunction+;
 
 #pragma link C++ class AliRsnPair+;
-#pragma link C++ class AliRsnPairMgr+;
 
-#pragma link C++ class AliRsnAnalysisTaskSEBase+;
-#pragma link C++ class AliRsnAnalysisTaskBase+;
+#pragma link C++ class AliRsnVATProcessInfo+;
+#pragma link C++ class AliRsnVAnalysisTaskSE+;
+#pragma link C++ class AliRsnVAnalysisTaskME+;
+#pragma link C++ class AliRsnVManager+;
+
+#pragma link C++ class AliRsnPairManager+;
+#pragma link C++ class AliRsnAnalysisManager+;
 #pragma link C++ class AliRsnAnalysisSE+;
+#pragma link C++ class AliRsnAnalysisME+;
 
 #endif
