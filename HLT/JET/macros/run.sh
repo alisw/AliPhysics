959971783534ac8a9d84d8b331228caa0c58d5ba
jthaeder <jthaeder@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-05-29 08:28:56 +0000

    updated run script

--- HLT/JET/macros/run.sh
+++ HLT/JET/macros/run.sh
@@ -1,21 +1,28 @@
 #/bin/bash
 
 
+N_EVENTS=100
+
 if [ ! -d ./analysis ] ; then
     mkdir analysis
 fi
 
 ROOTFILES=`find . -maxdepth 1 -name "*.root" `
 
+if [ -n "${ROOTFILES}" ] ; then
+    rm ${ROOTFILES}
+fi
+
+
+aliroot -b -l -q 'HLTJetReconstruction.C(10000,0,kTRUE)' 2>&1 | tee log.log
+
+ROOTFILES=`find . -maxdepth 1 -name "*.root" `
 
 if [ -n "${ROOTFILES}" ] ; then
     rm ${ROOTFILES}
 fi
 
-aliroot -b -l -q 'HLTJetReconstruction.C(100,0,kTRUE)'
+aliroot -l 'readJets.C("./analysis/EOR_analyze_10000_kPythia6Jets104_125.root")'
 
 
-
-
-
-#valgrind --error-limit=no --leak-check=full --show-reachable=yes aliroot -b -l -q 'HLTJetReconstruction.C(10)' 2>&1 | tee valgrind.log
+#valgrind --error-limit=no --leak-check=full --show-reachable=yes aliroot -b -l -q 'HLTJetReconstruction.C(10)' 
