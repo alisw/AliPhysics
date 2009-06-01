6893d407ed7c854103f33f3e5899b3ed288fe984
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-01 13:06:51 +0000

    First version of class for alignment/calibration
    Marian

---
+++ TPC/AliTPCkalmanFit.h
@@ -0,0 +1,75 @@
+#ifndef ALITPCKALMANFIT_H
+#define ALITPCKALMANFIT_H
+
+
+
+#include "TNamed.h"
+#include "TMatrixD.h"
+#include "TVectorD.h"
+#include "TObjArray.h"
+class TTreeSRedirector;
+class AliTrackPointArray;
+class AliTrackPoint;
+class TFormula;
+class TBits;
+class THnSparse;
+//
+
+class AliTPCkalmanFit: public TNamed{
+public:
+  AliTPCkalmanFit();
+  void Init();
+  void InitTransformation();
+  void  AddCalibration(AliTPCTransformation * calib);
+  AliTPCTransformation * GetTransformation(Int_t i){return (fCalibration)? (AliTPCTransformation *)fCalibration->At(i):0;}
+  //
+  void FitTrackLinear(AliTrackPointArray& points, Int_t step=1,  TTreeSRedirector *debug=0);
+  void DumpTrackLinear(AliTrackPointArray& points, TTreeSRedirector *debug);
+  void UpdateLinear(AliTrackPoint &point, TTreeSRedirector *debug=0);
+
+  void Propagate(TTreeSRedirector *debug=0);
+  void PropagateTime(Int_t time);
+
+  static AliTrackPointArray * MakePointArrayLinear(Double_t alpha, Double_t y0, Double_t z0, Double_t ky, Double_t kz, Double_t err=0.02); 
+  void  ApplyCalibration(AliTrackPointArray *array, Double_t csign);
+  Bool_t  CheckCovariance(TMatrixD &covar, Float_t maxEl);
+  Bool_t DumpCorelation(Double_t threshold);
+  Bool_t DumpCalib();
+  //
+  Double_t GetTPCDeltaXYZ(Int_t coord, Int_t volID, Double_t x, Double_t y, Double_t z);
+  static Double_t SGetTPCDeltaXYZ(Int_t coord, Int_t volID, Double_t x, Double_t y, Double_t z);
+  AliTPCkalmanFit *Test(Int_t ntracks);
+ public:
+  //
+  // Calibration parameters
+  //
+  TObjArray *fCalibration;  // array of calibrations
+  TMatrixD  *fCalibParam;   // calibration parameters 
+  TMatrixD  *fCalibCovar;   // calibration parameters 
+  //
+  // Linear track
+  //
+  TMatrixD  *fLinearParam;      // linear parameters
+  TMatrixD  *fLinearCovar;      // linear covariance
+  THnSparse *fLinearTrackDelta[8];   // linear tracks matching residuals - delta 
+  THnSparse *fLinearTrackPull[8];    // linear tracks matching residuals  - pull
+  //
+  //
+  //
+  Int_t      fLastTimeStamp; // last time stamp - used for propagation of parameters
+  //static AliTPCkalmanFit* Instance();
+  void SetInstance(AliTPCkalmanFit*param){fgInstance = param;}
+  static AliTPCkalmanFit*   fgInstance; //! Instance of this class (singleton implementation)
+ private:  
+  Double_t   fCurrentAlpha; //! current rotation frame
+  Double_t   fCA;           //! cosine of current angle
+  Double_t   fSA;           //! sinus of current angle  
+//   AliTPCkalmanFit&  operator=(const AliTPCkalmanFit&){;}// not implemented
+//   AliTPCkalmanFit(const AliTPCkalmanFit&){;} //not implemented
+  ClassDef(AliTPCkalmanFit,1);
+};
+
+
+
+#endif
+
