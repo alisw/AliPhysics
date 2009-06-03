12c7babe00dcd95922250b49f90fb1fb16b3d7f3
bhippoly <bhippoly@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-03 19:45:34 +0000

    Bug fix: proper place to reinitialise the generic MC histos in the switch -> no crash for the events containing more than one generated cascade of the same type (A.Maire)

--- PWG2/SPECTRA/AliAnalysisTaskCheckPerformanceCascade.h
+++ PWG2/SPECTRA/AliAnalysisTaskCheckPerformanceCascade.h
@@ -55,7 +55,7 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	
 	// - Plots for |y(MC)| < 1
 	TH1F	*fHistYGenCascMidRapXiMinus;		//! MC rapidity of Xi- generated within |y(MC)| < 1
-	TH1F	*fHistEtaGenCascMidRapXiMinus;	//! MC Eta of Xi-      generated within |y(MC)| < 1
+	TH1F	*fHistEtaGenCascMidRapXiMinus;		//! MC Eta of Xi-      generated within |y(MC)| < 1
 	TH1F	*fHistThetaGenCascMidRapXiMinus;	//! MC theta of Xi-    generated within |y(MC)| < 1
 	TH1F	*fHistPtGenCascMidRapXiMinus;		//! MC Pt of Xi-       generated within |y(MC)| < 1
 	
@@ -65,12 +65,12 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	TH1F	*fHistPtFdblGenCascXiMinus;		//! MC Pt of the cascade, Xi-
 	
 	TH1F	*fHistThetaLambdaXiMinus;		//! MC Theta angle of the Lambda daughter of the generated Xi-
-	TH1F	*fHistThetaBachXiMinus;		//! MC Theta angle of the Bachelor (pi-)
+	TH1F	*fHistThetaBachXiMinus;			//! MC Theta angle of the Bachelor (pi-)
 	
 	TH1F	*fHistThetaMesDghterXiMinus;		//! MC Theta angle of the mesonic  V0 daughter in the generated cascade, pi-
 	TH1F	*fHistThetaBarDghterXiMinus;		//! MC Theta angle of the baryonic V0 daughter in the generated cascade, p+
 	
-	TH1F	*fHistPtBachXiMinus;   		//! MC Pt of the Bachelor (pi-)                   (Control Plot)
+	TH1F	*fHistPtBachXiMinus;   			//! MC Pt of the Bachelor (pi-)                         (Control Plot)
 	TH1F	*fHistPtMesDghterXiMinus;		//! MC Pt of the meson daughter of the 'Lambda0', pi-   (Control Plot)
 	TH1F	*fHistPtBarDghterXiMinus;		//! MC Pt of the baryon daughter of the 'Lambda0', p+   (Control Plot)
 	
@@ -83,7 +83,7 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	// - Plots for |y(MC)| < 1
 	TH1F	*fHistYGenCascMidRapXiPlus;		//! MC rapidity of Xi+ generated within |y(MC)| < 1
 	TH1F	*fHistEtaGenCascMidRapXiPlus;		//! MC Eta of Xi+      generated within |y(MC)| < 1
-	TH1F	*fHistThetaGenCascMidRapXiPlus;	//! MC theta of Xi+    generated within |y(MC)| < 1
+	TH1F	*fHistThetaGenCascMidRapXiPlus;		//! MC theta of Xi+    generated within |y(MC)| < 1
 	TH1F	*fHistPtGenCascMidRapXiPlus;		//! MC Pt of Xi+       generated within |y(MC)| < 1
 		
 	// - Histos planned for Xi+ emitted within the acceptance (cuts in theta + pt of daughters)
@@ -91,12 +91,12 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	TH1F	*fHistPtFdblGenCascXiPlus;		//! MC Pt of the cascade, Xi+
 	
 	TH1F	*fHistThetaLambdaXiPlus;		//! MC Theta angle of the anti-Lambda daughter of the generated Xi+
-	TH1F	*fHistThetaBachXiPlus;		//! MC Theta angle of the Bachelor (pi+)
+	TH1F	*fHistThetaBachXiPlus;			//! MC Theta angle of the Bachelor (pi+)
 	
 	TH1F	*fHistThetaMesDghterXiPlus;		//! MC Theta angle of the mesonic  V0 daughter in the generated cascade, pi+
 	TH1F	*fHistThetaBarDghterXiPlus;		//! MC Theta angle of the baryonic V0 daughter in the generated cascade, p-
 	
-	TH1F	*fHistPtBachXiPlus;   		//! MC Pt of the Bachelor (pi+)                   (Control Plot)
+	TH1F	*fHistPtBachXiPlus;   			//! MC Pt of the Bachelor (pi+)                       (Control Plot)
 	TH1F	*fHistPtMesDghterXiPlus;		//! MC Pt of the meson daughter of the 'Lambda0', pi+ (Control Plot)
 	TH1F	*fHistPtBarDghterXiPlus;		//! MC Pt of the baryon daughter of the 'Lambda0', p- (Control Plot)
 	
@@ -104,23 +104,23 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	
 	//--------------
 	// Omega-
-	TH1F	*fHistEtaGenCascOmegaMinus;   	//! MC Pseudo-rapidity of any generated Omega- (no cuts in acceptance)
+	TH1F	*fHistEtaGenCascOmegaMinus;   		//! MC Pseudo-rapidity of any generated Omega- (no cuts in acceptance)
 	
 	// - Plots for |y(MC)| < 1
-	TH1F	*fHistYGenCascMidRapOmegaMinus;	//! MC rapidity of Omega- generated within |y(MC)| < 1
+	TH1F	*fHistYGenCascMidRapOmegaMinus;		//! MC rapidity of Omega- generated within |y(MC)| < 1
 	TH1F	*fHistEtaGenCascMidRapOmegaMinus;	//! MC Eta of Omega-      generated within |y(MC)| < 1
 	TH1F	*fHistThetaGenCascMidRapOmegaMinus;	//! MC theta of Omega-    generated within |y(MC)| < 1
 	TH1F	*fHistPtGenCascMidRapOmegaMinus;	//! MC Pt of Omega-       generated within |y(MC)| < 1
 	
 	// - Histos planned for Omega- emitted within the acceptance (cuts in theta + pt of daughters)
 	TH1F	*fHistThetaGenCascOmegaMinus;		//! MC Theta angle of the generated Omega-
-	TH1F	*fHistPtFdblGenCascOmegaMinus;	//! MC Pt of the cascade, Omega-
+	TH1F	*fHistPtFdblGenCascOmegaMinus;		//! MC Pt of the cascade, Omega-
 	
 	TH1F	*fHistThetaLambdaOmegaMinus;		//! MC Theta angle of the Lambda daughter of the generated Omega-
 	TH1F	*fHistThetaBachOmegaMinus;		//! MC Theta angle of the Bachelor (K-)
 	
-	TH1F	*fHistThetaMesDghterOmegaMinus;	//! MC Theta angle of the mesonic  V0 daughter in the generated cascade, pi-
-	TH1F	*fHistThetaBarDghterOmegaMinus;	//! MC Theta angle of the baryonic V0 daughter in the generated cascade, p+
+	TH1F	*fHistThetaMesDghterOmegaMinus;		//! MC Theta angle of the mesonic  V0 daughter in the generated cascade, pi-
+	TH1F	*fHistThetaBarDghterOmegaMinus;		//! MC Theta angle of the baryonic V0 daughter in the generated cascade, p+
 	
 	TH1F	*fHistPtBachOmegaMinus;   		//! MC Pt of the Bachelor (K-)                   (Control Plot)
 	TH1F	*fHistPtMesDghterOmegaMinus;		//! MC Pt of the meson daughter of the 'Lambda0', pi- (Control Plot)
@@ -133,10 +133,10 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	TH1F	*fHistEtaGenCascOmegaPlus;   		//! MC Pseudo-rapidity of any generated Omega+ (no cuts in acceptance)
 		
 	// - Plots for |y(MC)| < 1
-	TH1F	*fHistYGenCascMidRapOmegaPlus;	//! MC rapidity of Omega+ generated within |y(MC)| < 1
+	TH1F	*fHistYGenCascMidRapOmegaPlus;		//! MC rapidity of Omega+ generated within |y(MC)| < 1
 	TH1F	*fHistEtaGenCascMidRapOmegaPlus;	//! MC Eta of Omega+      generated within |y(MC)| < 1
 	TH1F	*fHistThetaGenCascMidRapOmegaPlus;	//! MC theta of Omega+    generated within |y(MC)| < 1
-	TH1F	*fHistPtGenCascMidRapOmegaPlus;	//! MC Pt of Omega+       generated within |y(MC)| < 1
+	TH1F	*fHistPtGenCascMidRapOmegaPlus;		//! MC Pt of Omega+       generated within |y(MC)| < 1
 	
 	// - Histos planned for Omega+ emitted within the acceptance (cuts in theta + pt of daughters)
 	TH1F	*fHistThetaGenCascOmegaPlus;		//! MC Theta angle of the generated Omega+
@@ -145,8 +145,8 @@ class AliAnalysisTaskCheckPerformanceCascade : public AliAnalysisTaskSE {
 	TH1F	*fHistThetaLambdaOmegaPlus;		//! MC Theta angle of the anti-Lambda daughter of the generated Omega+
 	TH1F	*fHistThetaBachOmegaPlus;		//! MC Theta angle of the Bachelor (K+)
 	
-	TH1F	*fHistThetaMesDghterOmegaPlus;	//! MC Theta angle of the mesonic  V0 daughter in the generated cascade, pi+
-	TH1F	*fHistThetaBarDghterOmegaPlus;	//! MC Theta angle of the baryonic V0 daughter in the generated cascade, p-
+	TH1F	*fHistThetaMesDghterOmegaPlus;		//! MC Theta angle of the mesonic  V0 daughter in the generated cascade, pi+
+	TH1F	*fHistThetaBarDghterOmegaPlus;		//! MC Theta angle of the baryonic V0 daughter in the generated cascade, p-
 	
 	TH1F	*fHistPtBachOmegaPlus;   		//! MC Pt of the Bachelor (K+) (Control Plot)
 	TH1F	*fHistPtMesDghterOmegaPlus;		//! MC Pt of the meson daughter of the 'Lambda0', pi+ (Control Plot)
