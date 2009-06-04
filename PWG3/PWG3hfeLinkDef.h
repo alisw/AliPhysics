53351c73005b446688577cbf4bb3f7d1c0b1fe87
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-04 09:01:37 +0000

    New package for heavy flavour electrons analysis (M.Fasel)

---
+++ PWG3/PWG3hfeLinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  AliHFEcollection+;
+
+#pragma link C++ class  AliHFEpid+;
+#pragma link C++ class  AliHFEpidBase+;
+#pragma link C++ class  AliHFEpidTPC+;
+#pragma link C++ class  AliHFEpidTRD+;
+#pragma link C++ class  AliHFEpidTOF+;
+#pragma link C++ class  AliHFEpidMC+;
+
+#pragma link C++ class  AliHFEcuts+;
+#pragma link C++ class  AliHFEextraCuts+;
+#endif
