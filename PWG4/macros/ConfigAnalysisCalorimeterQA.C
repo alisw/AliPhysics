869b9325368c238e6b22a4bc28958ff1531468c2
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-07 20:36:43 +0000

    New analysis added, Neutral meson invariant mass measurement: pi0, eta, omega, and temporal class for Calorimeters QA analysis, to be moved to QA frame.

---
+++ PWG4/macros/ConfigAnalysisCalorimeterQA.C
@@ -0,0 +1,92 @@
+/* $Id: $ */
+
+//------------------------------------
+// Configuration macro example:
+//
+// Calorimeters QA
+//
+// Author : Gustavo Conesa Balbastre (INFN-LNF)
+//------------------------------------
+
+AliAnaPartCorrMaker*  ConfigAnalysis()
+{
+	//
+	// Configuration goes here
+	// 
+	printf("======================== \n");
+	printf("ConfigAnalysis() \n");
+	printf("======================== \n");
+	
+	
+	
+	//-----------------------------------------------------------  
+	// Reader
+	//-----------------------------------------------------------
+	AliCaloTrackESDReader *reader = new AliCaloTrackESDReader();
+	reader->SetDebug(-1);
+	
+	//Switch on or off the detectors information that you want
+	reader->SwitchOnEMCAL();
+	reader->SwitchOffCTS();
+	reader->SwitchOnPHOS();
+	reader->SwitchOnEMCALCells();
+	reader->SwitchOnPHOSCells();
+	
+	//Min particle pT
+	reader->SetEMCALPtMin(0.); 
+	reader->SetPHOSPtMin(0.);
+	
+	
+	reader->Print("");
+	
+	
+	//---------------------------------------------------------------------
+	// Analysis algorithm
+	//---------------------------------------------------------------------
+	
+	AliFidutialCut * fidCut = new AliFidutialCut();
+	fidCut->DoCTSFidutialCut(kFALSE) ;
+	fidCut->DoEMCALFidutialCut(kTRUE) ;
+	fidCut->DoPHOSFidutialCut(kTRUE) ;
+	
+	
+  	AliAnaCalorimeterQA *anaEMCAL = new AliAnaCalorimeterQA();
+	anaEMCAL->SetDebug(-1); //10 for lots of messages
+	anaEMCAL->SetCalorimeter("EMCAL");
+	anaEMCAL->SwitchOnDataMC() ;//Access MC stack and fill more histograms
+	//anaEMCAL->SetStyleMacro("$MACROS/style.C") ;
+	anaEMCAL->AddToHistogramsName("AnaCaloQA_EMCAL_");
+	anaEMCAL->SetFidutialCut(fidCut);
+	anaEMCAL->SwitchOnFidutialCut();
+	anaEMCAL->Print("");
+	
+	AliAnaCalorimeterQA *anaPHOS = new AliAnaCalorimeterQA();
+	anaPHOS->SetDebug(-1); //10 for lots of messages
+	anaPHOS->SetCalorimeter("PHOS");
+	anaPHOS->SwitchOnDataMC() ;//Access MC stack and fill more histograms
+	//anaPHOS->SetStyleMacro("$MACROS/style.C") ;
+	anaPHOS->AddToHistogramsName("AnaCaloQA_PHOS_");
+	anaPHOS->SetFidutialCut(fidCut);
+	anaPHOS->SwitchOnFidutialCut();
+	anaPHOS->Print("");
+	
+	
+	//---------------------------------------------------------------------
+	// Set  analysis algorithm and reader
+	//---------------------------------------------------------------------
+	maker = new AliAnaPartCorrMaker();
+	maker->SetReader(reader);//pointer to reader
+	maker->AddAnalysis(anaEMCAL,1);
+	maker->AddAnalysis(anaPHOS,0);
+	//maker->SetAnaDebug(0)  ;
+	maker->SwitchOnHistogramsMaker()  ;
+	maker->SwitchOffAODsMaker()  ;
+	
+	maker->Print("");
+	//
+	printf("======================== \n");
+	printf("END ConfigAnalysis() \n");
+	printf("======================== \n");
+	return maker ;
+}
+
