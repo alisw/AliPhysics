e2941b1597606e43f9cd054361afd1cf10a15bfc
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-09 07:56:42 +0000

    Add macro for the LRC (Andrey Ivanov)

--- PWG2/EBYE/macros/runLRCAnalysis.C
+++ PWG2/EBYE/macros/runLRCAnalysis.C
@@ -1,13 +1,16 @@
-void runLRCAnalysis(const char* mode = "Local", 
-		    const char* inputName= "ESDs.lst") {
+void runLRCAnalysis(const char* mode = "GRID", const char* inputName= "wn.xml") {
 // This macro runs AliAnalysisTaskLRC in three modes : "Local" , "Interactive", "PROOF", "GRID" 
 // ESD-only
 // inputName refers to :
 // "Local" - file with plane-text list of filenames 
 // "Intaractive","GRID" - XML collection name
 // "PROOF" - dataset name 
-//  
-//
+//  ---------------------------------------------------------
+// This macro needs AliRoot-v4-17-03 or later 
+// and macro AddTaskLRC.C in workdir
+
+// For PROOF run PARS(ANALYSISalice.par  ANALYSIS.par  AOD.par  ESD.par  PWG2ebye.par  
+// STEERBase.par ) from AliRoot-v4-17-03 or later are to be in workdir.
 
 if(mode!="Local" && mode!="Interactive" && mode!="PROOF" && mode!="GRID")
 {
@@ -47,32 +50,21 @@ void runLRCLocal(const char* inputName= "ESDs.lst") {
   
   //___________Compile analysis task using AClic____________//
  
-  gROOT->ProcessLine(".include $ALICE_ROOT/include");
-  cout<<"  # Compiling analysis task\n";
+  //gROOT->ProcessLine(".include $ALICE_ROOT/include");
+  //cout<<"  # Compiling analysis task\n";
   //gROOT->LoadMacro("AliAnalysisTaskLRC.cxx+g");
   gROOT->LoadMacro("$ALICE_ROOT/PWG0/CreateESDChain.C");
-
+  gROOT->LoadMacro("AddTaskLRC.C");
   TChain* chain = CreateESDChain(inputName);
   
   //____________________________________________//
   // Make the analysis manager
-  AliAnalysisManager *mgr = new AliAnalysisManager("TestManager");
+  AliAnalysisManager *mgr = new AliAnalysisManager("TestLRCManagerLocal");
   AliESDInputHandler* esdH = new AliESDInputHandler();
   mgr->SetInputEventHandler(esdH);  
   
+  AddLRCTaskSet();
   
-  AliAnalysisTaskLRC *task1 = new AliAnalysisTaskLRC("TaskLRC");
-  mgr->AddTask(task1);
-  
-  task1->SetETAWindows(-0.7,0.0,0.3,1.0);
-  
-  // Create containers for input/output
-  AliAnalysisDataContainer *cinput1 = mgr->CreateContainer("cchain1",TChain::Class(),AliAnalysisManager::kInputContainer);
-  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("chist1", TList::Class(),AliAnalysisManager::kOutputContainer,"LRC.ESD.testLocal.root");
-  
-  //____________________________________________//
-  mgr->ConnectInput(task1,0,cinput1);
-  mgr->ConnectOutput(task1,0,coutput1);
   if (!mgr->InitAnalysis()) return;
   mgr->PrintStatus();
   mgr->StartAnalysis("local",chain);
@@ -95,45 +87,43 @@ void runLRCProof(const char* inputName= "/COMMON/COMMON/tutorial_small")
 	
   cout<<"  # Loadnig libs...\n";
 		
-	gProof->UploadPackage("AF-v4-16");
-	gProof->EnablePackage("AF-v4-16");
 
-	  
+gProof->UploadPackage("STEERBase");
+gProof->UploadPackage("ESD");
+gProof->UploadPackage("AOD");
+gProof->UploadPackage("ANALYSIS");
+gProof->UploadPackage("ANALYSISalice");
+gProof->UploadPackage("PWG2ebye");
+
+
+gProof->EnablePackage("STEERBase");
+gProof->EnablePackage("ESD");
+gProof->EnablePackage("AOD");
+gProof->EnablePackage("ANALYSIS");
+gProof->EnablePackage("ANALYSISalice");
+gProof->EnablePackage("PWG2ebye");
+
+ gROOT->LoadMacro("AddTaskLRC.C");  
   
   // Use AliRoot includes to compile our task
-  gROOT->ProcessLine(".include $ALICE_ROOT/include");
+  //gROOT->ProcessLine(".include $ALICE_ROOT/include");
 
-  cout<<"  # Compiling analysis task\n";
+  //cout<<"  # Compiling analysis task\n";
   
   
   //gProof->Load("AliAnalysisTaskLRC.cxx++g");   
-  task = new AliAnalysisTaskLRC("TestLRC");
-  
-  task->SetETAWindows(-0.7,0.0,0.3,1.0);
-
-    // Create the analysis manager
-  mgr = new AliAnalysisManager("testAnalysis");
-  
-  
-  mgr->AddTask(task);
-
+  // Create the analysis manager
+  mgr = new AliAnalysisManager("TestLRCManagerProof");
+   
   AliESDInputHandler* esdH = new AliESDInputHandler();
   mgr->SetInputEventHandler(esdH);
 
-
-  // Create containers for input/output
-  cinput = mgr->CreateContainer("cchain", TChain::Class(), AliAnalysisManager::kInputContainer);
-  coutput = mgr->CreateContainer("chist", TList::Class(),    AliAnalysisManager::kOutputContainer, "LRC.ESD.PROOFtest.root");
- 
-  // Connect input/output
-  mgr->ConnectInput(task, 0, cinput);
-  mgr->ConnectOutput(task, 0, coutput);
- 
- 
-  mgr->InitAnalysis();
+  AddLRCTaskSet();
+   
+  if (!mgr->InitAnalysis()) return;
   mgr->PrintStatus();
 
- mgr->StartAnalysis("proof", inputName);
+  mgr->StartAnalysis("proof", inputName);
 
   
 
@@ -142,7 +132,7 @@ void runLRCProof(const char* inputName= "/COMMON/COMMON/tutorial_small")
 void runLRCInteractive(const char* inputName= "wn.xml") {
   
   printf("  ------------------------------------------\n");
-  printf(" # LRC local-interactive run manager \n");
+  printf(" # LRC local-interactive/GRID run manager \n");
   cout<<"  # Task from :"<<gSystem->pwd()<<"\n";
   cout<<"  # Collection :"<<inputName<<"\n";
 
@@ -165,12 +155,12 @@ void runLRCInteractive(const char* inputName= "wn.xml") {
   
   gSystem->Load("libPWG2ebye.so");
   
-  gROOT->ProcessLine(".include $ALICE_ROOT/include");
+  //gROOT->ProcessLine(".include $ALICE_ROOT/include");
   
   
  //___________Compile analysis task using AClic____________//
  cout<<"  # Compiling analysis task\n";
- //gROOT->LoadMacro("AliAnalysisTaskLRC.cxx+g");
+ // gROOT->LoadMacro("AliAnalysisTaskLRC.cxx+g");
   
   
    
@@ -194,26 +184,62 @@ void runLRCInteractive(const char* inputName= "wn.xml") {
   
   AliAnalysisManager *mgr = new AliAnalysisManager("Local-Interactive_LRC_Manager");
   AliESDInputHandler* esdH = new AliESDInputHandler();
-  
   mgr->SetInputEventHandler(esdH);  
   //____________________________________________//
  
-  AliAnalysisTaskLRC *task1 = new AliAnalysisTaskLRC("TaskLRC");
-  mgr->AddTask(task1);
+  gROOT->LoadMacro("AddTaskLRC.C");
+   
+  AddLRCTaskSet();
   
-  task1->SetETAWindows(-0.7,0.0,0.3,1.0);
-  
-  // Create containers for input/output
-  AliAnalysisDataContainer *cinput1 = mgr->CreateContainer("cchain1",TChain::Class(),AliAnalysisManager::kInputContainer);
-  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("chist1", TList::Class(),AliAnalysisManager::kOutputContainer,"LRC.ESD.Local-Interactive.root");
-  
-  //____________________________________________//
-  mgr->ConnectInput(task1,0,cinput1);
-  mgr->ConnectOutput(task1,0,coutput1);
   if (!mgr->InitAnalysis()) return;
   mgr->PrintStatus();
   mgr->StartAnalysis("local",chain);
 
   timer.Stop();
   timer.Print();
+}
+
+Int_t SetupPar(const char* pararchivename) {
+  ///////////////////
+  // Setup PAR File//
+  ///////////////////
+  if (pararchivename) {
+    char processline[1024];
+    TString base = gSystem->BaseName(pararchivename);
+    TString dir  = gSystem->DirName(pararchivename);
+    TString ocwd = gSystem->WorkingDirectory();
+    // Move to dir where the par files are and unpack
+    gSystem->ChangeDirectory(dir.Data());
+    sprintf(processline,".! tar xvzf %s.par",base.Data());
+    gROOT->ProcessLine(processline);
+    // Move to par folder
+    gSystem->ChangeDirectory(base.Data());
+                                                                                                                                               
+    // check for BUILD.sh and execute
+    if (!gSystem->AccessPathName("PROOF-INF/BUILD.sh")) {
+      printf("*******************************\n");
+      printf("*** Building PAR archive    ***\n");
+      printf("*******************************\n");
+                                                                                                                                               
+      if (gSystem->Exec("PROOF-INF/BUILD.sh")) {
+        Error("runAnalysis","Cannot Build the PAR Archive! - Abort!");
+        return -1;
+      }
+    }
+    // check for SETUP.C and execute
+    if (!gSystem->AccessPathName("PROOF-INF/SETUP.C")) {
+      printf("*******************************\n");
+      printf("*** Setup PAR archive       ***\n");
+      printf("*******************************\n");
+      // If dir not empty, set the full include path
+      if (dir.Length()) {
+         sprintf(processline, ".include %s", pararchivename);
+         gROOT->ProcessLine(processline); 
+      }   
+      gROOT->Macro("PROOF-INF/SETUP.C");
+    }
+                                                                                                                                               
+    gSystem->ChangeDirectory(ocwd.Data());
+  }                                                                                                                                               
+  return 1;
 }
