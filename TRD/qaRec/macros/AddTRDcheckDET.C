bca8f94efd9283c005a0b08b3147da51924ce043
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-10 11:37:32 +0000

    Update by Raphaelle

--- TRD/qaRec/macros/AddTRDcheckDET.C
+++ TRD/qaRec/macros/AddTRDcheckDET.C
@@ -26,13 +26,21 @@ void AddTRDcheckDET(AliAnalysisManager *mgr, Char_t *trd, AliAnalysisDataContain
   if(!(TSTBIT(map, kCalibration))) return;
   AliTRDcalibration *ctask = 0x0;
   mgr->AddTask(ctask = new AliTRDcalibration());
+  ctask->SetHisto2d(kTRUE);
+  ctask->SetVector2d(kTRUE);
+  ctask->SetVdriftLinear(kTRUE);
+  ctask->SetNz(0,0);
+  ctask->SetNrphi(0,0);
+  ctask->SetNz(0,1);
+  ctask->SetNrphi(0,1);
+  ctask->SetNz(0,2);
+  ctask->SetNrphi(0,2);
   ctask->SetLow(0);
   ctask->SetHigh(30);
   ctask->SetFillZero(kFALSE);
-  ctask->SetDebugLevel(0);
+  ctask->SetDebugLevel(1);
 
   // Create containers for input/output
-  mgr->ConnectInput(task, 0, ci[0]);
-  mgr->ConnectOutput(task, 0, mgr->CreateContainer(ctask->GetName(), TObjArray::Class(), AliAnalysisManager::kOutputContainer, Form("TRD.Task%s.root", ctask->GetName())));
+  mgr->ConnectInput(ctask, 0, ci[0]);
+  mgr->ConnectOutput(ctask, 0, mgr->CreateContainer(ctask->GetName(), TObjArray::Class(), AliAnalysisManager::kOutputContainer, Form("TRD.Task%s.root", ctask->GetName())));
 }
-
