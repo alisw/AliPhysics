b678cbf45fad192a2c5b0a3ea575540a5e738d8d
basanta <basanta@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-10 10:15:36 +0000

    default directory is set correctly

--- PMD/AliPMDPutHot.C
+++ PMD/AliPMDPutHot.C
@@ -5,8 +5,8 @@ void AliPMDPutHot()
 {
   AliPMDHotData *hotda=new AliPMDHotData();
   AliCDBManager *man = AliCDBManager::Instance();
-  man->SetDefaultStorage("local://$ALICE_ROOT");
-  AliCDBId id("PMD/Calib/Hot",0,0);
+  man->SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+  AliCDBId id("PMD/Calib/Hot",0,AliCDBRunRange::Infinity());
   AliCDBMetaData *md=new AliCDBMetaData();
   man->GetDefaultStorage()->Put(hotda,id,md);
 }
