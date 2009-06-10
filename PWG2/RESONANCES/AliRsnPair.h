c85ac0acdebb8e568afb10417363eadbc6b4414b
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-10 07:52:57 +0000

    removed obsolete commented methods

--- PWG2/RESONANCES/AliRsnPair.h
+++ PWG2/RESONANCES/AliRsnPair.h
@@ -21,7 +21,6 @@
 #include "AliRsnPairParticle.h"
 #include "AliRsnEvent.h"
 #include "AliRsnCutMgr.h"
-#include "AliRsnHistoDef.h"
 #include "AliRsnPIDIndex.h"
 
 class AliRsnFunction;
@@ -46,7 +45,6 @@ class AliRsnPair : public TObject
     void    LoopPair(TArrayI *a1, TArrayI *a2, AliRsnEvent *ev1, AliRsnEvent *ev2 = 0);
     void    SetCutMgr(AliRsnCutMgr* theValue) { fCutMgr = theValue; }
     void    AddFunction(AliRsnFunction *fcn);
-    //void    AddFunction(AliRsnFunctionDef *fcn);
     TList*  GenerateHistograms(TString prefix = "");
     void    GenerateHistograms(TString prefix, TList *tgt);
 
@@ -58,8 +56,6 @@ class AliRsnPair : public TObject
     TString GetPairName() const;
     TString GetPairHistName(AliRsnFunction *fcn, TString text = "") const;
     TString GetPairHistTitle(AliRsnFunction *fcn, TString text = "") const;
-    //TString GetPairHistName(AliRsnFunctionNew *fcn, TString text = "") const;
-    //TString GetPairHistTitle(AliRsnFunctionNew *fcn, TString text = "") const;
 
   private:
 
