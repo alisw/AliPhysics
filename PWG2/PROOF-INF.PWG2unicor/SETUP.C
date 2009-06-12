f886c8f370b6446915b435d03ce35898d5010efe
misko <misko@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-12 13:15:19 +0000

    Preparing UNICOR for running on PWG2 train etc.

---
+++ PWG2/PROOF-INF.PWG2unicor/SETUP.C
@@ -0,0 +1,24 @@
+/=============================================================================
+void SETUP() {
+  CheckLoadLibrary("libPhysics.so");
+  CheckLoadLibrary("libEG.so");
+  CheckLoadLibrary("libTree.so");
+  CheckLoadLibrary("libVMC.so"); 
+  CheckLoadLibrary("libSTEERBase.so");
+  CheckLoadLibrary("libESD.so");
+  CheckLoadLibrary("libANALYSIS");
+  CheckLoadLibrary("libANALYSISalice");
+  CheckLoadLibrary("libPWG2unicor");
+
+  gROOT->ProcessLine(".include PWG2unicor/UNICOR");
+  gSystem->Setenv("PWG2unicor_INCLUDE", "PWG2unicor/UNICOR");
+}
+/=============================================================================
+Int_t CheckLoadLibrary(const char* library) {
+
+  // load library if not yet done
+
+  if (strlen(gSystem->GetLibraries(Form("%s.so", library), "", kFALSE)) > 0) return 1;  
+  return gSystem->Load(library);
+}
+/=============================================================================
