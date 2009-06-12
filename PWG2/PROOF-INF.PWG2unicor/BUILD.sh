4031fa4a6b09883eb205588261183441040d832d
misko <misko@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-12 13:15:19 +0000

    Preparing UNICOR for running on PWG2 train etc.

---
+++ PWG2/PROOF-INF.PWG2unicor/BUILD.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+touch libPWG2unicor.pkg
+
+make libPWG2unicor.so
---
+++ PWG2/UNICOR/AddTaskUnicor.C
@@ -0,0 +1,18 @@
+AliAnalysisTaskUnicor *AddTaskUnicor()
+{
+  // Creates a unicor analysis task and adds it to the analysis manager 
+
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {Error("AddTaskUnicor", "no analysis manager"); return NULL;}  
+  if (!mgr->GetInputEventHandler()) {Error("AddTaskUnicor", "no input event handler"); return NULL;}
+  TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+  if (type!="ESD") {Error("AddTaskUnicor","handler not of ESD type"); return NULL;}
+  AliAnalysisTaskUnicor *mytask = new AliAnalysisTaskUnicor();
+  mgr->AddTask(mytask);
+  mgr->ConnectInput (mytask,0,mgr->GetCommonInputContainer());
+  AliAnalysisDataContainer *coutpt = mgr->CreateContainer("unilis", TList::Class(),
+							  AliAnalysisManager::kOutputContainer,
+							  "unicor-result-as-list.root");
+  mgr->ConnectOutput(mytask,1,coutpt);
+  return mytask;
+}
