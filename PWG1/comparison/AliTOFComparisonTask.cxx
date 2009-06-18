642a15534fb138c7635ec003256794293cea3812
belikov <belikov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-18 14:41:41 +0000

    Fixing the coding conventions.

--- PWG1/comparison/AliTOFComparisonTask.cxx
+++ PWG1/comparison/AliTOFComparisonTask.cxx
@@ -1,3 +1,10 @@
+//-------------------------------------------------------------------------
+//
+// This is the PROOF-enabled version of TOF/AliTOFComparison.C macro.
+// Origin:  Andrei.Zalite@cern.ch
+//
+//-------------------------------------------------------------------------
+
 #include "TChain.h"
 #include "TTree.h"
 #include "TH1F.h"
@@ -9,10 +16,6 @@
 #include "TLine.h"
 #include "TText.h"
 #include "TFile.h"
-#include "TBenchmark.h"
-
-#include "AliAnalysisTask.h"
-#include "AliAnalysisManager.h"
 
 #include "AliLog.h"
 #include "AliVEvent.h"
@@ -26,17 +29,19 @@
 
 ClassImp(AliTOFComparisonTask)
 
+extern TStyle *gStyle;
+
 AliTOFComparisonTask::AliTOFComparisonTask()
   : AliAnalysisTaskSE("AliTOFComparisonTask"),
     fListOfHistos(0),
-    hgood(0),
-    hfound(0),
-    hfake(0),
-    hgoodPhi(0),
-    hfoundPhi(0),
-    hgoodl(0),
-    hfakel(0),
-    hfoundl(0)
+    fGood(0),
+    fFound(0),
+    fFake(0),
+    fGoodPhi(0),
+    fFoundPhi(0),
+    fGoodl(0),
+    fFakel(0),
+    fFoundl(0)
 {
   // Default constructor
   AliInfo("Defaulut Constructor AliTOFComparisonTask");
@@ -50,14 +55,14 @@ AliTOFComparisonTask::AliTOFComparisonTask()
 AliTOFComparisonTask::AliTOFComparisonTask(const char* name)
   : AliAnalysisTaskSE(name),
     fListOfHistos(0),
-    hgood(0),
-    hfound(0),
-    hfake(0),
-    hgoodPhi(0),
-    hfoundPhi(0),
-    hgoodl(0),
-    hfakel(0),
-    hfoundl(0)
+    fGood(0),
+    fFound(0),
+    fFake(0),
+    fGoodPhi(0),
+    fFoundPhi(0),
+    fGoodl(0),
+    fFakel(0),
+    fFoundl(0)
 {
   // Constructor
   AliInfo("Constructor AliTOFComparisonTask");
@@ -78,23 +83,23 @@ void AliTOFComparisonTask::UserCreateOutputObjects()
   // Create output container
   fListOfHistos = new TList();
   
-  hgood = new TH1F("hgood", "Pt for good tracks", 34, 0.2, 7.0);
-  hfound = new TH1F("hfound", "Pt for found tracks", 34, 0.2, 7.0);
-  hfake = new TH1F("hfake",  "Pt for fake tracks", 34, 0.2, 7.0);
-  hgoodPhi = new TH1F("hgoodPhi", "Phi for Good tracks", 90, 0., 2.*TMath::Pi());
-  hfoundPhi = new TH1F("hfoundPhi", "Phi for Found tracks", 90, 0., 2.*TMath::Pi());
-  hgoodl = new TH1F("hgoodl", "Good tracks", 30, -1., 1.);
-  hfakel = new TH1F("hfakel", "Mismatched tracks", 30, -1., 1.);
-  hfoundl = new TH1F("hfoundl", "Matched tracks", 30, -1., 1.);
+  fGood = new TH1F("fGood", "Pt for good tracks", 34, 0.2, 7.0);
+  fFound = new TH1F("fFound", "Pt for found tracks", 34, 0.2, 7.0);
+  fFake = new TH1F("fFake",  "Pt for fake tracks", 34, 0.2, 7.0);
+  fGoodPhi = new TH1F("fGoodPhi", "Phi for Good tracks", 90, 0., 2.*TMath::Pi());
+  fFoundPhi = new TH1F("fFoundPhi", "Phi for Found tracks", 90, 0., 2.*TMath::Pi());
+  fGoodl = new TH1F("fGoodl", "Good tracks", 30, -1., 1.);
+  fFakel = new TH1F("fFakel", "Mismatched tracks", 30, -1., 1.);
+  fFoundl = new TH1F("fFoundl", "Matched tracks", 30, -1., 1.);
   
-  fListOfHistos->Add(hgood);
-  fListOfHistos->Add(hfound);
-  fListOfHistos->Add(hfake);
-  fListOfHistos->Add(hgoodPhi);
-  fListOfHistos->Add(hfoundPhi);
-  fListOfHistos->Add(hgoodl);
-  fListOfHistos->Add(hfakel);
-  fListOfHistos->Add(hfoundl);
+  fListOfHistos->Add(fGood);
+  fListOfHistos->Add(fFound);
+  fListOfHistos->Add(fFake);
+  fListOfHistos->Add(fGoodPhi);
+  fListOfHistos->Add(fFoundPhi);
+  fListOfHistos->Add(fGoodl);
+  fListOfHistos->Add(fFakel);
+  fListOfHistos->Add(fFoundl);
 }
 
 
@@ -136,10 +141,10 @@ void AliTOFComparisonTask::UserExec(Option_t *)
 	
 	
     // Loop over Track References
-    Bool_t LabelTPC = kFALSE;
-    Bool_t LabelTOF = kFALSE;
+    Bool_t labelTPC = kFALSE;
+    Bool_t labelTOF = kFALSE;
     AliTrackReference* trackRef = 0;
-    UInt_t ITSLayerMap = 0;
+    UInt_t iTSLayerMap = 0;
     for (Int_t iTrackRef = 0; iTrackRef  < track->GetNumberOfTrackReferences(); iTrackRef++) {
       trackRef = track->GetTrackReference(iTrackRef);
       if(trackRef) {
@@ -147,17 +152,17 @@ void AliTOFComparisonTask::UserExec(Option_t *)
 	if (detectorId == AliTrackReference::kITS) {
           Float_t Radius = trackRef->R();
 	  if (Radius > 2.5 && Radius < 5.5)
-            ITSLayerMap |= 1;
+            iTSLayerMap |= 1;
           else if (Radius > 5.5 && Radius < 8.5)
-            ITSLayerMap |= 1 << 1;
+            iTSLayerMap |= 1 << 1;
           else if (Radius > 13.5 && Radius < 16.5)
-            ITSLayerMap |= 1 << 2;
+            iTSLayerMap |= 1 << 2;
           else if (Radius > 22. && Radius < 26.)
-            ITSLayerMap |= 1 << 3;
+            iTSLayerMap |= 1 << 3;
           else if (Radius > 36. && Radius < 41.)
-            ITSLayerMap |= 1 << 4;
+            iTSLayerMap |= 1 << 4;
           else if (Radius > 42. && Radius < 46.)
-            ITSLayerMap |= 1 << 5;
+            iTSLayerMap |= 1 << 5;
           else {
             Printf("Wrong radius %f ", Radius);
             return;
@@ -165,19 +170,19 @@ void AliTOFComparisonTask::UserExec(Option_t *)
 
         }
 	if (detectorId == AliTrackReference::kTPC) {	    
-	  LabelTPC = kTRUE;
+	  labelTPC = kTRUE;
 	}
 	if (detectorId == AliTrackReference::kTOF) {
-	  LabelTOF = kTRUE;
+	  labelTOF = kTRUE;
 	}
       }      
     }    // track references loop   
     
     // Skip tracks that passed not all ITS layers 
-    if (ITSLayerMap != 0x3F) continue;
+    if (iTSLayerMap != 0x3F) continue;
     
     // "Good" tracks
-    if (LabelTPC && LabelTOF) {
+    if (labelTPC && labelTOF) {
       AliMCComparisonTrack* ref = new((*refs)[nt]) AliMCComparisonTrack();
       ref->SetLabel(iTracks);
       TParticle* particle = track->Particle();
@@ -186,12 +191,12 @@ void AliTOFComparisonTask::UserExec(Option_t *)
       ref->SetPz(track->Pz());
       Float_t Pt = track->Pt();
       ref->SetPt(Pt);
-      hgood->Fill(Pt);
+      fGood->Fill(Pt);
       Float_t phig = track->Phi();
       ref->SetPhi(phig);
-      hgoodPhi->Fill(phig);
+      fGoodPhi->Fill(phig);
       Double_t tgl = track->Pz()/Pt;
-      hgoodl->Fill(tgl);
+      fGoodl->Fill(tgl);
       nt++;  
     }      
   }    // track loop 
@@ -210,10 +215,10 @@ void AliTOFComparisonTask::UserExec(Option_t *)
   Int_t nfake = 0;
   Int_t nlost = 0;
   
-  Int_t MCgoods = refs->GetEntriesFast();
+  Int_t mcGoods = refs->GetEntriesFast();
     
   // Loop over all "good" MC tracks
-  for (Int_t k = 0; k < MCgoods; k++) {
+  for (Int_t k = 0; k < mcGoods; k++) {
     AliMCComparisonTrack* ref = (AliMCComparisonTrack*)refs->UncheckedAt(k); 
     if (!ref) continue;
     Int_t MCLabel = ref->GetLabel();
@@ -240,14 +245,14 @@ void AliTOFComparisonTask::UserExec(Option_t *)
       if (TOFLabel[0] != MCLabel)
         if (TOFLabel[1] != MCLabel)
 	  if (TOFLabel[2] != MCLabel) {
-	    hfake->Fill(ptg); 
-	    hfakel->Fill(tgl);
+	    fFake->Fill(ptg); 
+	    fFakel->Fill(tgl);
 	    nfake++;
 	    break;
 	  }
-      hfound->Fill(ptg); 
-      hfoundl->Fill(tgl);
-      hfoundPhi->Fill(Phig);
+      fFound->Fill(ptg); 
+      fFoundl->Fill(tgl);
+      fFoundPhi->Fill(Phig);
       nfound++;	  
       break;      
     } 
@@ -287,14 +292,14 @@ void AliTOFComparisonTask::Terminate(Option_t *)
     return;
   }
 
-  hgood = dynamic_cast<TH1F*>(fListOfHistos->At(0));
-  hfound = dynamic_cast<TH1F*>(fListOfHistos->At(1));
-  hfake = dynamic_cast<TH1F*>(fListOfHistos->At(2));  
-  hgoodPhi = dynamic_cast<TH1F*>(fListOfHistos->At(3));
-  hfoundPhi = dynamic_cast<TH1F*>(fListOfHistos->At(4));
-  hgoodl = dynamic_cast<TH1F*>(fListOfHistos->At(5));
-  hfakel = dynamic_cast<TH1F*>(fListOfHistos->At(6));
-  hfoundl = dynamic_cast<TH1F*>(fListOfHistos->At(7));
+  fGood = dynamic_cast<TH1F*>(fListOfHistos->At(0));
+  fFound = dynamic_cast<TH1F*>(fListOfHistos->At(1));
+  fFake = dynamic_cast<TH1F*>(fListOfHistos->At(2));  
+  fGoodPhi = dynamic_cast<TH1F*>(fListOfHistos->At(3));
+  fFoundPhi = dynamic_cast<TH1F*>(fListOfHistos->At(4));
+  fGoodl = dynamic_cast<TH1F*>(fListOfHistos->At(5));
+  fFakel = dynamic_cast<TH1F*>(fListOfHistos->At(6));
+  fFoundl = dynamic_cast<TH1F*>(fListOfHistos->At(7));
   
   gStyle->SetOptStat(111110);
   gStyle->SetOptFit(1); 
@@ -304,11 +309,11 @@ void AliTOFComparisonTask::Terminate(Option_t *)
   TPad* p1 = new TPad("p1", "", 0., 0.5, 1., 1.); p1->Draw();  
   p1->cd(); p1->SetFillColor(42); p1->SetFrameFillColor(10);
   
-  hfound->Sumw2(); hgood->Sumw2(); hfake->Sumw2();
+  fFound->Sumw2(); fGood->Sumw2(); fFake->Sumw2();
   TH1F* hgp = new TH1F("hgp", " ", 34, 0.2, 7.0);
   TH1F* hfp = new TH1F("hfp", "Probability of mismatching", 34, 0.2, 7.0);
-  hgp->Divide(hfound, hgood, 1., 1., "b");
-  hfp->Divide(hfake, hgood, 1., 1., "b");
+  hgp->Divide(fFound, fGood, 1., 1., "b");
+  hfp->Divide(fFake, fGood, 1., 1., "b");
   hgp->SetLineColor(4); hgp->SetLineWidth(2);
   hgp->SetMaximum(1.4);
   hgp->SetYTitle("Tracking efficiency");
@@ -327,11 +332,11 @@ void AliTOFComparisonTask::Terminate(Option_t *)
   TPad* p2 = new TPad("p2", "", 0., 0., 1., 0.5); p2->Draw();
   p2->cd(); p2->SetFillColor(42); p2->SetFrameFillColor(10);  
   
-  hfoundl->Sumw2(); hgoodl->Sumw2(); hfakel->Sumw2();
+  fFoundl->Sumw2(); fGoodl->Sumw2(); fFakel->Sumw2();
   TH1F* hgl = new TH1F("hgl", "", 30, -1., 1.);
   TH1F* hfl = new TH1F("hfl", "Probability of mismatching", 30, -1., 1.);
-  hgl->Divide(hfoundl, hgoodl, 1., 1., "b");
-  hfl->Divide(hfakel, hgoodl, 1., 1., "b");
+  hgl->Divide(fFoundl, fGoodl, 1., 1., "b");
+  hfl->Divide(fFakel, fGoodl, 1., 1., "b");
   hgl->SetLineColor(4); hgl->SetLineWidth(2);
   hgl->SetMaximum(1.4);
   hgl->SetYTitle("Tracking efficiency");
@@ -350,10 +355,10 @@ void AliTOFComparisonTask::Terminate(Option_t *)
   
   TCanvas* c2 = new TCanvas("c2", "", 10, 10, 510, 510);
   c2->SetFillColor(42); c2->SetFrameFillColor(10);
-  hfoundPhi->Sumw2(); hgoodPhi->Sumw2();
+  fFoundPhi->Sumw2(); fGoodPhi->Sumw2();
   TH1F* hgphi = new TH1F("hgphi", "Efficiency for good tracks (Phi)",
 			 90, 0., 2.*TMath::Pi());
-  hgphi->Divide(hfoundPhi, hgoodPhi, 1., 1., "B");
+  hgphi->Divide(fFoundPhi, fGoodPhi, 1., 1., "B");
   hgphi->SetYTitle("Tracking efficiency");
   hgphi->SetXTitle("Phi (rad)");
   hgphi->Draw();
