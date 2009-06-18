b8b98742b8fd44bb52ad2f3366e9beafecf58436
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-18 16:28:07 +0000

    Initial version of the analysis task containing the forward detectors QA.

---
+++ PWG1/AliAnaFwdDetsQA.h
@@ -0,0 +1,54 @@
+#ifndef ALIANAFWDDETSQA_H
+#define ALIANAFWDDETSQA_H
+
+//------------------------------
+// Analysis task for quality-assurance
+// of forward detectors ESD
+//
+// 12/06/2009 cvetan.cheshkov@cern.ch
+//------------------------------
+
+#include "AliAnalysisTaskSE.h"
+
+class AliAnaFwdDetsQA : public AliAnalysisTaskSE
+{
+ public:	 
+  AliAnaFwdDetsQA();
+  AliAnaFwdDetsQA(const char *name);
+  virtual ~AliAnaFwdDetsQA() {}
+
+  virtual void   UserCreateOutputObjects();
+  virtual void   UserExec(Option_t *option);
+  virtual void   Terminate(Option_t *);
+
+  TH1F* CreateHisto(const char* name, const char* title, Int_t nBins, Double_t xMin, 
+			Double_t xMax, const char* xLabel = NULL, const char* yLabel = NULL);
+  TH1F* CreateEffHisto(const TH1F* hGen, const TH1F* hRec);
+  Bool_t FitHisto(TH1* histo, Double_t& res, Double_t& resError);
+  
+ private:
+
+  TList* fListOfHistos;
+
+  TH1F* fT0vtxRec;
+  TH2F* fT0vtxRecGen;
+  TH1F* fT0time;
+  TH1F* fT0mult;
+  TH1F* fT0vtxRes;
+
+  TH1F* fV0a;
+  TH1F* fV0c;
+  TH1F* fV0multA;
+  TH1F* fV0multC;
+  TH2F* fV0multAcorr;
+  TH2F* fV0multCcorr;
+  TH2F* fV0Acorr;
+  TH2F* fV0Ccorr;
+
+  AliAnaFwdDetsQA(const AliAnaFwdDetsQA&); // not implemented
+  AliAnaFwdDetsQA& operator=(const AliAnaFwdDetsQA&); // not implemented
+
+  ClassDef(AliAnaFwdDetsQA, 1) // example of analysis
+};
+
+#endif
