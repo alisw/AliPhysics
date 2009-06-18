e9d1ffd54dec6a578924c09a3baef4a911986d16
belikov <belikov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-18 14:22:15 +0000

    Fixing the coding conventions.

--- PWG1/comparison/AliTPCComparisonTask.h
+++ PWG1/comparison/AliTPCComparisonTask.h
@@ -1,5 +1,12 @@
-#ifndef AliTPCComparisonTask_cxx
-#define AliTPCComparisonTask_cxx
+#ifndef ALITPCCOMPARISONTASK_H
+#define ALITPCCOMPARISONTASK_H
+
+//-------------------------------------------------------------------------
+//
+// This is the PROOF-enabled version of TPC/AliTPCComparison.C macro.
+// Origin:  Andrei.Zalite@cern.ch
+//
+//-------------------------------------------------------------------------
 
 class TList;
 class TH1F;
@@ -24,19 +31,19 @@ class AliTPCComparisonTask: public AliAnalysisTaskSE
     virtual void Terminate(Option_t *);
   
   private:
-    TList* fListOfHistos;
+    TList* fListOfHistos; // the list of the output histos
     
-    TH1F* hgood;
-    TH1F* hfound;
-    TH1F* hfake;
-    TH1F* hp;
-    TH1F* hl;
-    TH1F* hpt;
-    TH1F* hmpt;
-    TH1F* he;
-    TH2F* hep;
-    TH1F* hgoodPhi;
-    TH1F* hfoundPhi;
+    TH1F* fGood;          // good tracks
+    TH1F* fFound;         // found tracks
+    TH1F* fFake;          // fake tracks
+    TH1F* fP;             // phi resolution 
+    TH1F* fL;             // lambda resolution
+    TH1F* fPt;            // pt resolution
+    TH1F* fHmpt;          // high-pt resolution
+    TH1F* fE;             // dE/dx for MIP
+    TH2F* fEp;            // dE/dx vs momentum
+    TH1F* fGoodPhi;       // phi for good tracks
+    TH1F* fFoundPhi;      // phi for found tracks
     
     AliTPCComparisonTask(const AliTPCComparisonTask&); // not implemented
     AliTPCComparisonTask& operator=(const AliTPCComparisonTask&); // not implemented
