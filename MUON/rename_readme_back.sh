85a2d40d6414b4f8772c2e3af6c1ff226620c634
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-06-29 11:10:31 +0000

    Adding READMEeve.txt page

--- MUON/rename_readme_back.sh
+++ MUON/rename_readme_back.sh
@@ -6,17 +6,18 @@
 # by Doxygen, in the same order as on the main Web page. 
 
 mv  p03_READMEbase.txt         READMEbase.txt             
-mv  p12_READMEcalib.txt        READMEcalib.txt
-mv  p06_READMEcosmics.txt      READMEcosmics.txt
-mv  p05_READMEevaluation.txt   READMEevaluation.txt
-mv  p07_READMEfast.txt         READMEfast.txt
-mv  p13_READMEgeometry.txt     READMEgeometry.txt
-mv  p09_READMEmapping.txt      READMEmapping.txt
-mv  p10_READMEmchda.txt        READMEmchda.txt
+mv  p13_READMEcalib.txt        READMEcalib.txt
+mv  p07_READMEcosmics.txt      READMEcosmics.txt
+mv  p06_READMEevaluation.txt   READMEevaluation.txt
+mv  p05_READMEeve.txt          READMEeve.txt
+mv  p08_READMEfast.txt         READMEfast.txt
+mv  p14_READMEgeometry.txt     READMEgeometry.txt
+mv  p10_READMEmapping.txt      READMEmapping.txt
+mv  p11_READMEmchda.txt        READMEmchda.txt
 mv  p04_READMEmchview.txt      READMEmchview.txt
-mv  p11_READMEmtrda.txt        READMEmtrda.txt
-mv  p08_READMEraw.txt          READMEraw.txt
+mv  p12_READMEmtrda.txt        READMEmtrda.txt
+mv  p09_READMEraw.txt          READMEraw.txt
 mv  p02_READMErec.txt          READMErec.txt
-mv  p15_READMEshuttle.txt      READMEshuttle.txt
+mv  p16_READMEshuttle.txt      READMEshuttle.txt
 mv  p01_READMEsim.txt          READMEsim.txt
-mv  p14_READMEtrigger.txt      READMEtrigger.txt
+mv  p15_READMEtrigger.txt      READMEtrigger.txt
--- MUON/rename_readme_for_refman.sh
+++ MUON/rename_readme_for_refman.sh
@@ -6,17 +6,18 @@
 # by Doxygen, in the same order as on the main Web page. 
 
 mv READMEbase.txt         p03_READMEbase.txt
-mv READMEcalib.txt        p12_READMEcalib.txt
-mv READMEcosmics.txt      p06_READMEcosmics.txt
-mv READMEevaluation.txt   p05_READMEevaluation.txt
-mv READMEfast.txt         p07_READMEfast.txt
-mv READMEgeometry.txt     p13_READMEgeometry.txt
-mv READMEmapping.txt      p09_READMEmapping.txt
-mv READMEmchda.txt        p10_READMEmchda.txt
+mv READMEcalib.txt        p13_READMEcalib.txt
+mv READMEcosmics.txt      p07_READMEcosmics.txt
+mv READMEevaluation.txt   p06_READMEevaluation.txt
+mv READMEeve.txt          p05_READMEeve.txt
+mv READMEfast.txt         p08_READMEfast.txt
+mv READMEgeometry.txt     p14_READMEgeometry.txt
+mv READMEmapping.txt      p10_READMEmapping.txt
+mv READMEmchda.txt        p11_READMEmchda.txt
 mv READMEmchview.txt      p04_READMEmchview.txt
-mv READMEmtrda.txt        p11_READMEmtrda.txt
-mv READMEraw.txt          p08_READMEraw.txt
+mv READMEmtrda.txt        p12_READMEmtrda.txt
+mv READMEraw.txt          p09_READMEraw.txt
 mv READMErec.txt          p02_READMErec.txt
-mv READMEshuttle.txt      p15_READMEshuttle.txt
+mv READMEshuttle.txt      p16_READMEshuttle.txt
 mv READMEsim.txt          p01_READMEsim.txt
-mv READMEtrigger.txt      p14_READMEtrigger.txt
+mv READMEtrigger.txt      p15_READMEtrigger.txt
