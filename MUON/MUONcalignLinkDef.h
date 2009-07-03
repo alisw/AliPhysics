937dbd3662995801afe7b710fc191f9cd265a7f0
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-07-03 11:17:01 +0000

    - Adding new MUONcalign library:   now adding files missing in previous commit

---
+++ MUON/MUONcalignLinkDef.h
@@ -0,0 +1,22 @@
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+// $Id$
+
+/// \file MUONcalign.h
+/// \brief The CINT link definitions for \ref calign 
+
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliMUONClusterInfo+;
+#pragma link C++ class AliMUONPadInfo+;
+#pragma link C++ class AliMUONAlignmentTask+;
+#pragma link C++ class AliMUONReAlignTask+;
+#pragma link C++ class AliMUONChamberCalibrationTask+;
+
+#endif
+
