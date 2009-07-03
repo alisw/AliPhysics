bed0b2dae214d1ef451dfa7366707e2cb6b331f8
ivana <ivana@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-07-03 11:49:02 +0000

    Separating mapping graphics classes in a new library, MUONmpgraphics. - This library is not linked with aliroot - This makes mapping library independent from Root GUI classes

--- MUON/MUONmappingLinkDef.h
+++ MUON/MUONmappingLinkDef.h
@@ -6,6 +6,11 @@
 /// \file MUONmappingLinkDef.h
 /// \brief The CINT link definitions for \ref mapping 
 
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
 #pragma link C++ nestedclasses;
 #pragma link C++ nestedtypedef;
 
@@ -124,20 +129,4 @@
 #pragma link C++ class  AliMpFastSegmentation+;
 #pragma link C++ class  AliMpUID+;
 
-/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- * See cxx source for full Copyright notice                               */
-
-// $MpId: graphicsLinkDefIn.h,v 1.4 2005/08/24 09:42:12 ivana Exp $ 
-
-#pragma link C++ class  AliMpGraphContext+;
-#pragma link C++ class  AliMpVPainter+;
-#pragma link C++ class  AliMpMotifPainter+;
-#pragma link C++ class  AliMpRowPainter+;
-#pragma link C++ class  AliMpRowSegmentPainter+;
-#pragma link C++ class  AliMpSectorPainter+;
-#pragma link C++ class  AliMpSubZonePainter+;
-#pragma link C++ class  AliMpZonePainter+;
-#pragma link C++ class  AliMpSlatPainter+;
-#pragma link C++ class  AliMpPCBPainter+;
-#pragma link C++ class  AliMpDEVisu+;
-#pragma link C++ class  AliMpIteratorPainter+;
+#endif
