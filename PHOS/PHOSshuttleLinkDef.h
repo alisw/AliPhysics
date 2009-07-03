c644106e4348800a69d969fa5cba352102ebfda0
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-07-03 17:26:28 +0000

    Files reverted, were deleted by fault

---
+++ PHOS/PHOSshuttleLinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliPHOSPreprocessor+;
+#pragma link C++ class AliPHOSDA1+;
+#pragma link C++ class AliPHOSDA2+;
+#pragma link C++ class AliPHOSCpvDA1+;
+#pragma link C++ class AliPHOSCpvPreprocessor+;
+#pragma link C++ class AliPHOSRcuDA1+;
+
+#endif
