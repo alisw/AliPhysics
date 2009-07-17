80664648a9dec84065245bcf586f5b51740e9177
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-07-17 14:02:52 +0000

    Modifications in the balance function code + addition of the very first version of the macro and the task to run the balance function

--- PWG2/EBYE/AliBalance.h
+++ PWG2/EBYE/AliBalance.h
@@ -16,18 +16,16 @@
 
 #define MAXIMUM_NUMBER_OF_STEPS	1024
 
-class TLorentzVector;
 class TGraphErrors;
+class TObjArray;
 
-class AliBalance : public TObject
-{
+class AliBalance : public TObject {
  public:
   AliBalance();
   AliBalance(Double_t p2Start, Double_t p2Stop, Int_t p2Steps);
   AliBalance(const AliBalance& balance);
   ~AliBalance();
   
-  void SetParticles(TLorentzVector *P, Double_t *charge, Int_t dim);
   void SetNumberOfBins(Int_t ibins);
   void SetAnalysisType(Int_t iType);
   void SetInterval(Double_t p2Start, Double_t p2Stop);
@@ -47,7 +45,7 @@ class AliBalance : public TObject
   Double_t GetNpp(Int_t p2) const { return 1.0*fNpp[p2]; }
   Double_t GetNpn(Int_t p2) const { return 1.0*fNpn[p2]; }
  
-  void CalculateBalance();
+  void CalculateBalance(TObjArray *gTrackArray);
   
   Double_t GetBalance(Int_t p2);
   Double_t GetError(Int_t p2);
@@ -58,11 +56,6 @@ class AliBalance : public TObject
   void Merge(AliBalance *b);
 
  private:
-  Double_t *fCharge; //matrix with the charge of each track
-  Int_t fNtrack; //number of tracks to compute the BF
-
-  TLorentzVector *fV; //4-momentum vector - info for tracks used to compute the BF
-  
   Int_t fNumberOfBins; //number of bins of the analyzed interval
   Int_t fAnalysisType; //0:y - 1:eta - 2:Qlong - 3:Qout - 4:Qside - 5:Qinv - 6:phi
   Int_t fAnalyzedEvents; //number of events that have been analyzed
