7feb4f8335c3af5d95ff14455109166a143e6a6b
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-07-29 09:38:28 +0000

    adding running options for the ITS cluster finding macro: running only spd, sdd, ssd or all; adding macro documentation and comments (Gaute)

--- HLT/ITS/macros/rec-its-cluster.C
+++ HLT/ITS/macros/rec-its-cluster.C
@@ -1,7 +1,37 @@
 // $Id$
-//aliroot -b -q rec-spd-cluster.C | tee rec-spd-cluster.log
+/*
+ * Example macro to run the Offline ITS Cluster Finding in HLT.
+ * The reconstruction is done from the ITS raw data.
+ *
+ * Usage:
+ * <pre>
+ *   aliroot -b -q rec-its-cluster.C | tee rec-its-cluster.log
+ *   aliroot -b -q rec-its-cluster.C'("./","spd")' | tee rec-its-cluster.log
+ * </pre>
+ *
+ * The macro asumes raw data to be available in the rawx folders, either
+ * simulated or real data. A different input can be specified as parameter
+ * <pre>
+ *   aliroot -b -q rec-its-cluster.C'("input.root")'
+ * </pre>
+ *
+ * In the second parameter you can change the clusterfinders to run only
+ * SPD, SDD, SSD or All:
+ *    - SPD runs only Silicon Pixels
+ *    - SDD runs only Silicon Drift
+ *    - SSD runs only Silicon Stips
+ *    - All will run the full ITS
+ *
+ * All is default. The chain ends in a histogram component. 
+ *
+ * The reconstruction is steered by the AliReconstruction object in the
+ * usual way.
+ *
+ * @ingroup alihlt_its
+ * @author st05886@ift.uib.no
+ */
 
-void rec_its_cluster(const char* input="./", char* opt="")
+void rec_its_cluster(const char* input="./", char* opt="All")
 {
 
   if(!gSystem->AccessPathName("galice.root")){
@@ -25,7 +55,39 @@ void rec_its_cluster(const char* input="./", char* opt="")
   // Setting up which output to give
   //
   TString option="libAliHLTUtil.so libAliHLTRCU.so libAliHLTITS.so libAliHLTSample.so loglevel=0x7c chains=";
-
+  Bool_t runspd=kFALSE, runsdd=kFALSE, runssd=kFALSE;
+  TString allArgs=opt;
+  TString argument;
+  TObjArray* pTokens=allArgs.Tokenize(" ");
+  if (pTokens) {
+    for (int i=0; i<pTokens->GetEntries(); i++) {
+      argument=((TObjString*)pTokens->At(i))->GetString();
+      if (argument.IsNull()) continue;
+ 
+      if (argument.CompareTo("spd", TString::kIgnoreCase)==0) {
+	runspd=kTRUE;
+	continue;
+      } 
+      if (argument.CompareTo("sdd", TString::kIgnoreCase)==0) {
+	runsdd=kTRUE;
+	continue;
+      }
+      if (argument.CompareTo("ssd",TString::kIgnoreCase)==0) {
+	runssd=kTRUE;
+	continue;
+      }
+      if (argument.CompareTo("all",TString::kIgnoreCase)==0) {
+	runspd=kTRUE;
+	runsdd=kTRUE;
+	runssd=kTRUE;
+	continue;
+      }
+      else {
+	break;
+      }
+    }
+  }
+  
   ///////////////////////////////////////////////////////////////////////////////////////////////////
   //
   // define the analysis chain to be run
@@ -39,64 +101,69 @@ void rec_its_cluster(const char* input="./", char* opt="")
   int maxddl=19;         //max ddl number for SPD
   int spec=0x1;          //spec for ddl's
   int ddlno=0;
-
   TString dummyInput="";
-  for(ddlno=minddl;ddlno<=maxddl;ddlno++){  
-    TString arg, publisher, cf;
+
+  if(runspd){
+    for(ddlno=minddl;ddlno<=maxddl;ddlno++){  
+      TString arg, publisher, cf;
+      
+      arg.Form("-minid %d -datatype 'DDL_RAW ' 'ISPD ' -dataspec 0x%08x -verbose",ddlno, spec);
+      publisher.Form("DP_%d", ddlno);
+      AliHLTConfiguration pubconf(publisher.Data(), "AliRawReaderPublisher", NULL , arg.Data());
+      
+      cf.Form("CF_%d",ddlno);
+      AliHLTConfiguration cfconf(cf.Data(), "ITSClusterFinderSPD", publisher.Data(), "");
+      
+      if (dummyInput.Length()>0) dummyInput+=" ";
+      dummyInput+=cf;
+      
+      spec=spec<<1;
+    }
+  }
   
-    arg.Form("-minid %d -datatype 'DDL_RAW ' 'ISPD ' -dataspec 0x%08x -verbose",ddlno, spec);
-    publisher.Form("DP_%d", ddlno);
-    AliHLTConfiguration pubconf(publisher.Data(), "AliRawReaderPublisher", NULL , arg.Data());
+  if(runsdd){
+    minddl=256;        //min ddl number for SDD    
+    maxddl=279;        //max ddl number for SDD
+    spec=0x1;          //spec for ddl's
     
-    cf.Form("CF_%d",ddlno);
-    AliHLTConfiguration cfconf(cf.Data(), "ITSClusterFinderSPD", publisher.Data(), "");
-
-    if (dummyInput.Length()>0) dummyInput+=" ";
-    dummyInput+=cf;
-
-    spec=spec<<1;
+    for(ddlno=minddl;ddlno<=maxddl;ddlno++){  
+      TString arg, publisher, cf;
+      
+      arg.Form("-minid %d -datatype 'DDL_RAW ' 'ISDD ' -dataspec 0x%08x -verbose",ddlno, spec); 
+      publisher.Form("DP_%d", ddlno);
+      AliHLTConfiguration pubconf(publisher.Data(), "AliRawReaderPublisher", NULL , arg.Data());
+      
+      cf.Form("CF_%d",ddlno);
+      AliHLTConfiguration cfconf(cf.Data(), "ITSClusterFinderSDD", publisher.Data(), "");
+      
+      if (dummyInput.Length()>0) dummyInput+=" ";
+      dummyInput+=cf;
+      
+      spec=spec<<1;
+    }
   }
 
-  minddl=256;        //min ddl number for SDD    
-  maxddl=279;        //max ddl number for SDD
-  spec=0x1;          //spec for ddl's
- 
-  for(ddlno=minddl;ddlno<=maxddl;ddlno++){  
-    TString arg, publisher, cf;
+  if(runssd){
+    minddl=512;      //min ddl number for SSD     
+    maxddl=527;      //max ddl number for SSD
+    spec=0x1;        //spec for ddl's
     
-    arg.Form("-minid %d -datatype 'DDL_RAW ' 'ISDD ' -dataspec 0x%08x -verbose",ddlno, spec); 
-    publisher.Form("DP_%d", ddlno);
-    AliHLTConfiguration pubconf(publisher.Data(), "AliRawReaderPublisher", NULL , arg.Data());
-    
-    cf.Form("CF_%d",ddlno);
-    AliHLTConfiguration cfconf(cf.Data(), "ITSClusterFinderSDD", publisher.Data(), "");
-
-    if (dummyInput.Length()>0) dummyInput+=" ";
-    dummyInput+=cf;
-
-    spec=spec<<1;
+    for(ddlno=minddl;ddlno<=maxddl;ddlno++){  
+      TString arg, publisher, cf;
+      
+      arg.Form("-minid %d -datatype 'DDL_RAW ' 'ISSD ' -dataspec 0x%08x -verbose",ddlno, spec);
+      publisher.Form("DP_%d", ddlno);
+      AliHLTConfiguration pubconf(publisher.Data(), "AliRawReaderPublisher", NULL , arg.Data());
+      
+      cf.Form("CF_%d",ddlno);
+      AliHLTConfiguration cfconf(cf.Data(), "ITSClusterFinderSSD", publisher.Data(), "");
+      
+      if (dummyInput.Length()>0) dummyInput+=" ";
+      dummyInput+=cf;
+      
+      spec=spec<<1;
+    }
   }
-
-  minddl=512;      //min ddl number for SSD     
-  maxddl=527;      //max ddl number for SSD
-  spec=0x1;        //spec for ddl's
-  
-  for(ddlno=minddl;ddlno<=maxddl;ddlno++){  
-    TString arg, publisher, cf;
-    
-    arg.Form("-minid %d -datatype 'DDL_RAW ' 'ISSD ' -dataspec 0x%08x -verbose",ddlno, spec);
-    publisher.Form("DP_%d", ddlno);
-    AliHLTConfiguration pubconf(publisher.Data(), "AliRawReaderPublisher", NULL , arg.Data());
-    
-    cf.Form("CF_%d",ddlno);
-    AliHLTConfiguration cfconf(cf.Data(), "ITSClusterFinderSSD", publisher.Data(), "");
-
-    if (dummyInput.Length()>0) dummyInput+=" ";
-    dummyInput+=cf;
-
-    spec=spec<<1;
-  }
-
 
   AliHLTConfiguration cfconf("clusterHisto","ITSClusterHisto",dummyInput.Data(),"");
   AliHLTConfiguration fwconf("histFile","ROOTFileWriter", "clusterHisto","-datafile ClusterHisto -concatenate-events -overwrite");
