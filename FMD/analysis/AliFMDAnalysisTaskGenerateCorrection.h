e6a81482e99fc8fc56ae3737c3de09384538d575
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-08-05 10:46:52 +0000

    classes missed in earlier commit

---
+++ FMD/analysis/AliFMDAnalysisTaskGenerateCorrection.h
@@ -0,0 +1,61 @@
+#ifndef ALIFMDANALYSISTASKGENERATECORRECTION_H
+#define ALIFMDANALYSISTASKGENERATECORRECTION_H
+
+#include "AliAnalysisTaskSE.h"
+#include "TList.h"
+#include "AliFMDFloatMap.h"
+#include "TH1F.h"
+
+class AliFMDAnaCalibBackgroundCorrection;
+class AliFMDAnaCalibTriggerEfficiency;
+class AliFMDAnaCalibEventSelectionEfficiency;
+
+class AliFMDAnalysisTaskGenerateCorrection : public AliAnalysisTaskSE
+{
+ public:
+  AliFMDAnalysisTaskGenerateCorrection();
+    AliFMDAnalysisTaskGenerateCorrection(const char* name);
+    ~AliFMDAnalysisTaskGenerateCorrection() {;}
+ AliFMDAnalysisTaskGenerateCorrection(const AliFMDAnalysisTaskGenerateCorrection& o) : AliAnalysisTaskSE(), 
+      fListOfHits(), 
+      fListOfPrimaries(),
+      fListOfCorrection(),
+      fVertexBins(o.fVertexBins),
+      fLastTrackByStrip(o.fLastTrackByStrip),
+      fHitsByStrip(o.fHitsByStrip),
+      fZvtxCut(o.fZvtxCut),
+      fNvtxBins(o.fNvtxBins),
+      fNbinsEta(o.fNbinsEta),
+      fBackground(o.fBackground)
+      {}
+    AliFMDAnalysisTaskGenerateCorrection& operator=(const AliFMDAnalysisTaskGenerateCorrection&) { return *this; }
+    
+    virtual void Init();
+    virtual void UserCreateOutputObjects();
+    virtual void UserExec(Option_t* /*option*/);
+    void  Terminate(Option_t */*option*/);
+    void SetZvtxCut(Float_t vtxcut) {fZvtxCut = vtxcut;}
+    void SetNvtxBins(Int_t nvtxbins) {fNvtxBins = nvtxbins;}
+    void SetNbinsEta(Int_t netabins) {fNbinsEta = netabins;}
+    void ReadFromFile(const Char_t* filename = "background.root", Bool_t storeInOCDB = kFALSE, Int_t runNo=0);
+ private:
+    
+    void GenerateCorrection();
+    
+    TList fListOfHits;
+    TList fListOfPrimaries;
+    TList fListOfCorrection;
+    TH1F  fVertexBins;
+    AliFMDFloatMap fLastTrackByStrip;
+    AliFMDFloatMap fHitsByStrip;
+    Float_t fZvtxCut;
+    Int_t fNvtxBins;
+    Int_t fNbinsEta;
+    AliFMDAnaCalibBackgroundCorrection* fBackground;
+    AliFMDAnaCalibTriggerEfficiency*    fTriggerEff;
+    AliFMDAnaCalibEventSelectionEfficiency*     fEventSelectionEff;
+    
+    ClassDef(AliFMDAnalysisTaskGenerateCorrection, 1);
+
+};
+#endif
