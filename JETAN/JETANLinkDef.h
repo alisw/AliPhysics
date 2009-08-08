3651e300dd6b861d258b598fdd6bc84ff757d5ab
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-08-08 11:12:42 +0000

    Obsolete code removed.

--- JETAN/JETANLinkDef.h
+++ JETAN/JETANLinkDef.h
@@ -4,7 +4,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
   
-#pragma link C++ class AliJet+;
 #pragma link C++ class AliJetHeader+;
 #pragma link C++ class AliUA1JetHeader+;
 #pragma link C++ class AliJetFinder+;
@@ -12,12 +11,8 @@
 #pragma link C++ class AliJetESDReaderHeader+;
 #pragma link C++ class AliJetReader+;
 #pragma link C++ class AliJetESDReader+;
-#pragma link C++ class AliJetControlPlots+;
-#pragma link C++ class AliLeading+;
 #pragma link C++ class AliJetProductionData+;
 #pragma link C++ class AliJetProductionDataPDC2004+;
-#pragma link C++ class AliJetAnalysis+;
-#pragma link C++ class AliJetDistributions+;
 #pragma link C++ class AliUA1JetFinderV1+;
 #pragma link C++ class AliUA1JetFinderV2+;
 #pragma link C++ class AliUA1JetHeaderV1+;
