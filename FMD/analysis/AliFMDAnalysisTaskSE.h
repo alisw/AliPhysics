e2f0cf03a42fab5d248418f7222b1e6eeffbd471
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-08-11 09:32:57 +0000

    small corrections

--- FMD/analysis/AliFMDAnalysisTaskSE.h
+++ FMD/analysis/AliFMDAnalysisTaskSE.h
@@ -32,32 +32,27 @@ class AliFMDAnalysisTaskSE : public AliAnalysisTaskSE
     AliFMDAnalysisTaskSE(const char* name);
     virtual ~AliFMDAnalysisTaskSE() {;}
  AliFMDAnalysisTaskSE(const AliFMDAnalysisTaskSE& o) : AliAnalysisTaskSE(),
-      fListOfHistos(o.fListOfHistos),
-      fSharing(o.fSharing),
-      fDensity(o.fDensity),
-      fBackground(o.fBackground),
-      fDndeta(o.fDndeta)
-      //      fPrimary(o.fPrimary),
-      // fRecordHits(o.fRecordHits) 
-	{}
+						       fListOfHistos(o.fListOfHistos),
+						       fSharing(o.fSharing),
+						       fDensity(o.fDensity),
+						       fBackground(o.fBackground),
+						       fDndeta(o.fDndeta)   {}
     AliFMDAnalysisTaskSE& operator=(const AliFMDAnalysisTaskSE&) { return *this; }
-    
-    virtual void Init();
-    virtual void UserCreateOutputObjects();
-    virtual void UserExec(Option_t* /*option*/);
-    void  Terminate(Option_t */*option*/);
-    //   void  SetAnalyzePrimary(Bool_t prim) {fPrimary = prim;}
-    //   void  SetRecordHits(Bool_t recordhits) {fRecordHits = recordhits;}
- private:
-    
-    TList* fListOfHistos;
-    AliFMDAnalysisTaskSharing              fSharing;
-    AliFMDAnalysisTaskDensity              fDensity;
-    AliFMDAnalysisTaskBackgroundCorrection fBackground;
-    AliFMDAnalysisTaskDndeta               fDndeta;
-    //   Bool_t                                 fPrimary;
-    //  Bool_t                                 fRecordHits;
-    ClassDef(AliFMDAnalysisTaskSE, 1);
+  
+  virtual void Init();
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t* /*option*/);
+  void  Terminate(Option_t */*option*/);
+  
+private:
+  
+  TList* fListOfHistos;
+  AliFMDAnalysisTaskSharing              fSharing;
+  AliFMDAnalysisTaskDensity              fDensity;
+  AliFMDAnalysisTaskBackgroundCorrection fBackground;
+  AliFMDAnalysisTaskDndeta               fDndeta;
+  
+  ClassDef(AliFMDAnalysisTaskSE, 1);
 
 };
 #endif
