c8c585700c50bbe0788ee2b465bea70aada224dc
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-08-19 10:53:18 +0000

    Needed for FASTJETAN parfiles

---
+++ JETAN/PROOF-INF.FASTJETAN/SETUP.C
@@ -0,0 +1,12 @@
+void SETUP()
+{
+   // Load some ROOT libraries
+   gSystem->Load("libFASTJETAN");
+
+   // Set the Inlucde paths
+   gSystem->SetIncludePath("-I$ROOTSYS/include -IJETAN");
+   gROOT->ProcessLine(".include JETAN");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("JETAN_INCLUDE", "JETAN");
+}
