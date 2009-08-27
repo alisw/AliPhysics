11cad610b778e481b0bc57281f7448570964b2c4
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-08-27 17:09:39 +0000

    Remove deletion of s-digits manager

--- TRD/AliTRDdigitizer.cxx
+++ TRD/AliTRDdigitizer.cxx
@@ -192,7 +192,7 @@ AliTRDdigitizer::~AliTRDdigitizer()
   }
 
   if (fSDigitsManager) {
-    delete fSDigitsManager;
+    // s-digitsmanager will be deleted via list
     fSDigitsManager     = 0;
   }
 
