0a55a909bb49302523d704373a3b34078cad66de
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-09-07 14:17:30 +0000

    At the start of the online GUI sterring window we will read the list of already active physics runs from the DAQ logbook. Once started the runs appear and disappear using the DIM service mechanism.

--- MONITOR/alionlinemonitor.cxx
+++ MONITOR/alionlinemonitor.cxx
@@ -3,6 +3,12 @@
 
 #include <TRint.h>
 
+#include <TSQLServer.h>
+#include <TSQLResult.h>
+#include <TSQLRow.h>
+
+#include <TTimeStamp.h>
+
 int main(int argc, char **argv)
 {
   AliDimIntNotifier::SetMainThreadId();
@@ -24,6 +30,42 @@ int main(int argc, char **argv)
     win->GetSOR()->infoHandlerTest(2244);
 
     printf("win = (AliOnlineReco*) 0x%lx\n", (unsigned long)win);
+  }
+  else {
+
+    const char *dbHost = "aldaqdb";
+    Int_t dbPort = 3306;
+    const char *dbName = "LOGBOOK";
+    const char *user = "logbook";
+    const char *password = "alice";
+
+    TSQLServer* server = TSQLServer::Connect(Form("mysql://%s:%d/%s", dbHost, dbPort, dbName), user, password);
+    if (!server) {
+      printf("ERROR: Could not connect to DAQ Logbook");
+      return 0;
+    }
+    TString sqlQuery;
+    TTimeStamp ts;
+    sqlQuery.Form("SELECT run FROM logbook WHERE DAQ_time_start > %u AND DAQ_time_end IS NULL AND partition = 'PHYSICS'",
+		  ts.GetSec()-86400);
+    TSQLResult* result = server->Query(sqlQuery);
+    if (!result) {
+      printf("ERROR: Can't execute query <%s>!", sqlQuery.Data());
+      return 0;
+    }
+    if (result->GetRowCount() == 0) {
+      printf("No active physics runs found");
+    }
+    else {
+      for (Int_t iRow = 0; iRow < result->GetRowCount(); iRow++) {
+	TSQLRow* row = result->Next();
+	TString runStr = row->GetField(0);
+	if (runStr.IsDigit())
+	  win->StartOfRun(runStr.Atoi());
+	delete row;
+      }
+    }
+    delete result;
   }
 
   app.Run(kTRUE);
