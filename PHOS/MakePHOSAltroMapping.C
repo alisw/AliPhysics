792fd53864c66ded461e4898260ac4be12439e5b
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-09-10 11:27:10 +0000

    Modifications to create 20 different mapping objects, one per module per RCU.

--- PHOS/MakePHOSAltroMapping.C
+++ PHOS/MakePHOSAltroMapping.C
@@ -1,20 +1,36 @@
 void MakePHOSAltroMapping(){
-  // Create TObjArray of PHOS altro mapping objects and
-  // store it in the CDB
+  // Create TObjArray of PHOS altro mapping objects.
+  // Set the environment variables in order to store it in the OCDB:
+  // export TOCDB=kTRUE
+  // export STORAGE=local://$ALICE_ROOT/OCDB
+  // Then the newly created root file $ALICE_ROOT/OCDB/PHOS/Calib/Mapping
+  // should be committed to SVN and submitted to AliEn production manager
   //
+  // Yuri Kharlov. 10 September 2009
+
+
   const char* macroname = "MakePHOSAltroMapping.C";
 
-  TObjArray mappingsArray(6);
+  TObjArray mappingsArray(20);
   
+  nModules = 5;
+  nRCU     = 4;
   TString path = gSystem->Getenv("ALICE_ROOT");
-  path += "/PHOS/mapping/RCU";
-  TString path2;
-  for(Int_t i = 0; i < 4; i++) {
-    path2 = path;
-    path2 += i;
-    path2 += ".data";
-    AliAltroMapping *mapping = new AliCaloAltroMapping(path2.Data());
-    mappingsArray.Add(mapping);
+  path += "/PHOS/mapping/";
+  TString path1, path2;
+  for(Int_t m = 0; m < nModules; m++) {
+    path1 = path;
+    path1 += "Mod";
+    path1 += m;
+    path1 += "RCU";
+    for(Int_t i = 0; i < nRCU; i++) {
+      path2 = path1;
+      path2 += i;
+      path2 += ".data";
+      Info(macroname,"Mapping file: %s",path2.Data());
+      AliAltroMapping *mapping = new AliCaloAltroMapping(path2.Data());
+      mappingsArray.Add(mapping);
+    }
   }
 
   if( TString(gSystem->Getenv("TOCDB")) != TString("kTRUE") ){
@@ -46,7 +62,7 @@ void MakePHOSAltroMapping(){
     }
     AliCDBMetaData* md = new AliCDBMetaData();
     md->SetResponsible("Yuri Kharlov");
-    md->SetComment("Default ALTRO mapping for PHOS");
+    md->SetComment("Default ALTRO mapping for PHOS: 20 mapping objects, one per modules per RCU");
     md->SetAliRootVersion(gSystem->Getenv("ARVERSION"));
     AliCDBId id("PHOS/Calib/Mapping",0,AliCDBRunRange::Infinity());
     storage->Put(&mappingsArray,id,md);
