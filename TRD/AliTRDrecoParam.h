53e1999917d8ac680ac286e2fe6e5a66aec3e5d1
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-09-10 08:18:58 +0000

    Incremented class version (Markus)

--- TRD/AliTRDrecoParam.h
+++ TRD/AliTRDrecoParam.h
@@ -126,7 +126,7 @@ private:
   Int_t     fNumberOfPresamples;     // number of presamples 
   Int_t     fNumberOfPostsamples;     // number of postsamples 
 
-  ClassDef(AliTRDrecoParam, 7)       // Reconstruction parameters for TRD detector
+  ClassDef(AliTRDrecoParam, 8)       // Reconstruction parameters for TRD detector
 
 };
 
