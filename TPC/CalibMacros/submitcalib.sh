90ac009faec3b3785f4b632f38f0dda62de191c0
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-09-17 16:27:13 +0000

    Adding script to run TPC calibration train at GSI (Marian)

---
+++ TPC/CalibMacros/submitcalib.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# 1 argument      - start file
+# 2 argument      - number of files
+# 3 argument      - input file list
+# 4 argument      - run number used for ConfigOCDB
+
+#myvar=0;
+#while [ $myvar -ne 640 ] ; do bsub -q alice-t3_8h  submitcalib.sh $myvar $(($myvar+1))  /lustre_alpha/alice/miranov/rec/october2008/all/esdzip.txt; myvar=$(( $myvar +2 )) ; echo $myvar ; done
+
+echo $1   $2  $3
+source /u/miranov/.balice64HEAD0108
+
+
+mkdir $1_$2
+cp *.C $1_$2 
+cd $1_$2
+cp $3 esd.txt
+
+fstart=$1
+fend=$2
+cdbrun=$4
+echo start aliroot
+echo command aliroot -q -b CalibrateTPC.C\($fstart,$fend,$cdbrun\)
+command aliroot  -q -b "CalibrateTPC.C($fstart,$fend,$cdbrun)"
+rm list.txt
+
+
