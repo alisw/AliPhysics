5dd185712098150b61ee6b712fbbc295a85c71c8
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-09-23 02:26:35 +0000

    Removing obsolete and unused classes in order to facilitated the refectoring to a common CALO library for EMCAL and PHOS

--- HLT/PHOS/AliHLTPHOSLinkDef.h
+++ HLT/PHOS/AliHLTPHOSLinkDef.h
@@ -12,11 +12,11 @@
 #pragma link C++ class AliHLTPHOSClusterizer+;  
 #pragma link C++ class AliHLTPHOSEmcCalibData+;  
 #pragma link C++ class AliHLTPHOSPhysicsAnalyzer+;  
-#pragma link C++ class AliHLTPHOSPulseGenerator+;  
+//#pragma link C++ class AliHLTPHOSPulseGenerator+;  
 #pragma link C++ class AliHLTPHOSRawAnalyzerChiSquareFit+;  
 #pragma link C++ class AliHLTPHOSRawAnalyzerCrude+;  
-#pragma link C++ class AliHLTPHOSRawAnalyzerKLevel+;  
-#pragma link C++ class AliHLTPHOSRawAnalyzerLMS+;  
+//#pragma link C++ class AliHLTPHOSRawAnalyzerKLevel+;  
+//#pragma link C++ class AliHLTPHOSRawAnalyzerLMS+;  
 #pragma link C++ class AliHLTPHOSRawAnalyzerPeakFinder+;  
 #pragma link C++ class AliHLTPHOSDigitMaker+; 
 #pragma link C++ class AliHLTPHOSSanityInspector+; 
