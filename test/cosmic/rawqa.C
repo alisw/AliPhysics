9ba547dc29e901626c752751f8f57b774b59359e
schutz <schutz@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-09-28 10:27:15 +0000

    update to adapt to QA framework modifications

--- test/cosmic/rawqa.C
+++ test/cosmic/rawqa.C
@@ -1,76 +1,60 @@
-//#include <iostream>
-//#include <fstream>
-//
-//#include <TAlienCollection.h>
-//#include <TFile.h>
-//#include <TGrid.h>
-//#include <TGridResult.h>
-//#include <TMath.h>
-//#include <TROOT.h>
-//#include <TString.h>
-//#include <TSystem.h>
-//
-//#include "AliCDBManager.h"
-//#include "AliDAQ.h"
-//#include "AliLog.h"
-//#include "AliQA.h"
-//#include "AliQADataMakerSteer.h"
-//#include "AliRawReader.h"
-//#include "AliRawReaderRoot.h"
+#include <iostream>
+#include <fstream>
+
+#include <TFile.h>
+#include <TGrid.h>
+#include <TGridResult.h>
+#include <TMath.h>
+#include <TROOT.h>
+#include <TString.h>
+#include <TSystem.h>
+
+#include "AliCDBManager.h"
+#include "AliDAQ.h"
+#include "AliLog.h"
+#include "AliQAv1.h"
+#include "AliQAManager.h"
+#include "AliRawReader.h"
+#include "AliRawReaderRoot.h"
 //#include "AliTRDrawStreamBase.h"
-//#include "AliGeomManager.h"
+#include "AliGeomManager.h"
 
 TString ClassName() { return "rawqa" ; } 
 
 //________________________________qa______________________________________
-void rawqa(const char * filename) 
+void rawqa(Int_t run) 
 {	
-  // retrieve evironment variables
-  const char * year = gSystem->Getenv("YEAR") ; 
-  const TString baseDir(gSystem->Getenv("BASEDIR")) ;
-  const Int_t runNumber = atoi(gSystem->Getenv("RUNNUM")) ; 
-
-  // build the default storage of OCDB 
-  TString sfilename(filename) ; 
-  sfilename = sfilename.Strip() ; 
-  sfilename = sfilename.Strip(TString::kLeading) ;
-  sfilename.Prepend("alien://") ; 
-
-  baseDir.Append("/") ; 
-  TString temp(filename) ;
-  temp = temp.ReplaceAll(baseDir, "") ; 
-  temp = temp.Strip(TString::kLeading, '/') ; 
-  baseDir.Append(temp(0, temp.Index("/"))) ;  
-  char * kDefaultOCDBStorage = Form("alien://folder=%s/OCDB/", baseDir.Data()) ; 
-
+  const Char_t * filename = "raw.root" ; 
+  // Set the CDB storage location
+  AliCDBManager * man = AliCDBManager::Instance();
+  //man->SetDefaultStorage("local://cdb2");
+  man->SetDefaultStorage("raw://");
+  
   // set the location of reference data 
-  //AliQA::SetQARefStorage(Form("%s%s/", AliQA::GetQARefDefaultStorage(), year)) ;  
-  AliQA::SetQARefStorage("local://$ALICE_ROOT/OCDB") ;
+  //AliQAv1::SetQARefStorage(Form("%s%s/", AliQAv1::GetQARefDefaultStorage(), year)) ;  
+  AliQAv1::SetQARefStorage("local://$ALICE_ROOT/QAref") ;
 	
   AliLog::SetGlobalDebugLevel(0) ; 
 	
-  // connect to the grid 
-  TGrid * grid = 0x0 ; 
-  grid = TGrid::Connect("alien://") ; 
-	
+  	
   Bool_t detIn[AliDAQ::kNDetectors] = {kFALSE} ;
-  char * detNameOff[AliDAQ::kNDetectors] = {"ITS", "ITS", "ITS", "TPC", "TRD", "TOF", "HMPID", "PHOS", "PHOS", 
+  const char * detNameOff[AliDAQ::kNDetectors] = {"ITS", "ITS", "ITS", "TPC", "TRD", "TOF", "HMPID", "PHOS", "PHOS", 
           "PMD", "MUON", "MUON", "FMD", "T0", "VZERO", "ZDC", "ACORDE", "TRG", 
           "EMCAL", "DAQ_TEST", "HLT"} ; 
 	
-  AliQADataMakerSteer qas("rec") ; 
+  AliQAManager * qam = AliQAManager::QAManager(AliQAv1::kRECMODE) ; 
+  qam->SetEventSpecie(AliRecoParam::kCosmic) ; 
+  AliQAv1::Instance()->SetEventSpecie(AliRecoParam::kCosmic) ; 
   TString detectors  = ""; 
   TString detectorsW = ""; 
   UShort_t eventsProcessed = 0 ; 
   UShort_t filesProcessed  = 1 ; 
-  AliCDBManager* man = AliCDBManager::Instance();
-  man->SetDefaultStorage(kDefaultOCDBStorage) ;  
-  man->SetRun(runNumber) ; 
+  man->SetRun(run);
   AliGeomManager::LoadGeometry();
   printf("INFO: Proccessing file %s\n", filename) ;
   // check which detectors are present 
-  AliRawReader * rawReader = new AliRawReaderRoot(sfilename);
-  AliTRDrawStreamBase::SetRawStreamVersion("TB");
+  AliRawReader * rawReader = new AliRawReaderRoot(filename);
+  //AliTRDrawStreamBase::SetRawStreamVersion("TB");
   while ( rawReader->NextEvent() ) {
     man->SetRun(rawReader->GetRunNumber());
     AliLog::Flush();
@@ -95,34 +79,36 @@ void rawqa(const char * filename)
       break ; 
   }
   if ( !detectors.IsNull() ) {
-	  //qas->SetMaxEvents(1) ; 
-    qas.SetTasks(Form("%d", AliQA::kRAWS)); 			
-    detectorsW = qas.Run(detectors, rawReader) ;
-    qas.Reset() ;
+	  qam->SetMaxEvents(-1) ; 
+    qam->SetTasks(Form("%d", AliQAv1::kRAWS)); 			
+    detectorsW = qam->Run(detectors, rawReader) ;
+    qam->Reset() ;
   } else {
        	  printf("ERROR: No valid detectors found") ; 
   } 
   delete rawReader ;
-  eventsProcessed += qas.GetCurrentEvent() ; 
-	//qas.Merge(runNumber) ; 
+  eventsProcessed += qam->GetCurrentEvent() ; 
+	//qam->Merge(run) ; 
 	
 	// The summary 
-  printf("\n\n********** Summary for run %d **********", runNumber) ; 
+  printf("\n\n********** Summary for run %d **********", run) ; 
   printf("     data file                           : %s\n", filename);
   printf("     detectors present in the run        : %s\n", detectors.Data()) ; 
   printf("     detectors present in the run with QA: %s\n", detectorsW.Data()) ; 
   printf("     number of files/events processed    : %d/%d\n", filesProcessed, eventsProcessed) ; 
-  TFile * qaResult = TFile::Open(AliQA::GetQAResultFileName()) ; 
+  //const char * qaFileName = AliQAv1::GetQAResultFileName() ;
+  //  TFile * qaResult = TFile::Open(qaFileName) ; 
+  TFile * qaResult = TFile::Open("QA.root") ; 
   if ( qaResult ) {
-    AliQA * qa = dynamic_cast<AliQA *>(qaResult->Get(AliQA::GetQAName())) ; 
+    AliQAv1 * qa = dynamic_cast<AliQAv1 *>(qaResult->Get(AliQAv1::GetQAName())) ; 
     if ( qa) {
-      for (Int_t index = 0 ; index < AliQA::kNDET ; index++)
-        if (detectorsW.Contains(AliQA::GetDetName(index))) 
-          qa->Show((AliQA::GetDetIndex(AliQA::GetDetName(index)))) ;
+      for (Int_t index = 0 ; index < AliQAv1::kNDET ; index++)
+        if (detectorsW.Contains(AliQAv1::GetDetName(index))) 
+          qa->Show((AliQAv1::GetDetIndex(AliQAv1::GetDetName(index)))) ;
     } else {
-      printf("ERROR: %s not found in %s !", AliQA::GetQAName(), AliQA::GetQAResultFileName()) ; 
+      printf("ERROR: %s not found in %s !", AliQAv1::GetQAName(), AliQAv1::GetQAResultFileName()) ; 
     }
   } else {
-    printf("ERROR: %s has not been produced !", AliQA::GetQAResultFileName()) ; 
+    printf("ERROR: %s has not been produced !", AliQAv1::GetQAResultFileName()) ; 
   }
 }
