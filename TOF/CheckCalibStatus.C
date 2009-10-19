b5fe7bd32cf1eeacb7c80273530a0b1301b8e2dd
rpreghen <rpreghen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-10-19 16:09:33 +0000

    added to CheckCalibStatus macro two histograms in order to display, on the basis of detector configuration stored in OCDB, enabled and noisy channels in a user-friendly fashion

--- TOF/CheckCalibStatus.C
+++ TOF/CheckCalibStatus.C
@@ -5,10 +5,31 @@ CheckCalibStatus(const Char_t *fileName)
   AliCDBEntry *cdbe = (AliCDBEntry *)file->Get("AliCDBEntry");
   AliTOFChannelOnlineStatusArray *array = (AliTOFChannelOnlineStatusArray *)cdbe->GetObject();
 
-  TH1F *h = new TH1F("h", "Channel status;index;status", array->GetSize(), 0., array->GetSize(););
-  for (Int_t i = 0; i <  array->GetSize(); i++)
-    h->SetBinContent(i + 1, array->GetStatus(i));
-  
-  h->Draw();
+  TH1F *hStatus = new TH1F("hStatus", "Channel status;index;status", array->GetSize(), 0., array->GetSize(););
+  TH2F *hNoiseMap = new TH2F("hNoiseMap", "Noise map;sector;strip", 72, 0., 18., 91, 0., 91.);
+  TH2F *hEnableMap = new TH2F("hEnableMap", "Enable map;sector;strip", 72, 0., 18., 91, 0., 91.);
+
+  AliTOFcalibHisto calib;
+  calib.LoadCalibHisto();
+
+  Int_t sector, sectorStrip, padx, fea;
+  Float_t hitmapx, hitmapy;
+  for (Int_t i = 0; i <  array->GetSize(); i++) {
+    hStatus->SetBinContent(i + 1, array->GetStatus(i));
+    sector = calib.GetCalibMap(AliTOFcalibHisto::kSector, i);
+    sectorStrip = calib.GetCalibMap(AliTOFcalibHisto::kSectorStrip, i);
+    padx = calib.GetCalibMap(AliTOFcalibHisto::kPadX, i);
+    fea = padx / 12;
+    hitmapx = sector + ((Double_t)(3 - fea) + 0.5) / 4.;
+    hitmapy = sectorStrip;
+    if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk) hEnableMap->Fill(hitmapx, hitmapy);
+    if (array->GetNoiseStatus(i) == AliTOFChannelOnlineStatusArray::kTOFNoiseBad) hNoiseMap->Fill(hitmapx, hitmapy);
+  }
+
+  TFile *fout = TFile::Open("CheckCalibStatus.root", "RECREATE");
+  hStatus->Write();
+  hNoiseMap->Write();
+  hEnableMap->Write();
+  fout->Close();
 
 }
