2041ccea5caedda928a98deae4cdbf27704250ac
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-10-21 06:55:05 +0000

    tempory to fix compilation problems

--- TRD/qaRec/AliTRDpidRefMakerLQ.cxx
+++ TRD/qaRec/AliTRDpidRefMakerLQ.cxx
@@ -46,6 +46,7 @@
 #include "../../STAT/TKDPDF.h"
 #include "AliTRDpidRefMakerLQ.h"
 #include "../Cal/AliTRDCalPID.h"
+#include "../Cal/AliTRDCalPIDLQ.h"
 #include "AliTRDseedV1.h"
 #include "AliTRDcalibDB.h"
 #include "AliTRDgeometry.h"
@@ -54,10 +55,8 @@ ClassImp(AliTRDpidRefMakerLQ)
 
 //__________________________________________________________________
 AliTRDpidRefMakerLQ::AliTRDpidRefMakerLQ()
-  :AliTRDpidRefMaker("pidRefMakerLQ", "PID(LQ) Reference Maker")
-  ,fPbin(-1)
-  ,fSbin(-1)
-  ,fResults(0x0)
+  :AliTRDpidRefMaker("PIDrefMakerLQ", "PID(LQ) Reference Maker")
+  ,fPIDbin(-1)
 {
   //
   // AliTRDpidRefMakerLQ default constructor
@@ -70,10 +69,6 @@ AliTRDpidRefMakerLQ::~AliTRDpidRefMakerLQ()
   //
   // AliTRDCalPIDQRef destructor
   //
-  if(fResults) {
-    fResults->Delete();
-    delete fResults;
-  }
 }
 
 //________________________________________________________________________
@@ -90,7 +85,7 @@ void AliTRDpidRefMakerLQ::CreateOutputObjects()
     TObjArray *arr = new TObjArray(AliPID::kSPECIES);
     arr->SetName(Form("Pbin%02d", ip));
     for(Int_t is=AliPID::kSPECIES; is--;) {
-      h2 = new TH2I(Form("h%s%d", AliPID::ParticleShortName(is), ip), Form("%s ref. dEdx @ Pbin[%d]", AliPID::ParticleName(is), ip), 50, 5., 10., 50, 5., 10.);
+      h2 = new TH2D(Form("h%s%d", AliPID::ParticleShortName(is), ip), Form("%s ref. dEdx @ Pbin[%d]", AliPID::ParticleName(is), ip), 50, 5., 10., 50, 5., 10.);
       h2->GetXaxis()->SetTitle("log(dE/dx_{am}) [au]");
       h2->GetYaxis()->SetTitle("log(dE/dx_{dr}) [au]");
       h2->GetZaxis()->SetTitle("#");
@@ -100,8 +95,7 @@ void AliTRDpidRefMakerLQ::CreateOutputObjects()
   }
 
   fData = new TTree(GetName(), Form("Reference data for %s", GetName()));
-  fData->Branch("s", &fSbin, "l/b");
-  fData->Branch("p", &fPbin, "p/b");
+  fData->Branch("ps", &fPIDbin, "p/b");
   fData->Branch("dEdx" , fdEdx , Form("dEdx[%d]/F", GetNslices()));
 }
 
@@ -120,8 +114,6 @@ Float_t* AliTRDpidRefMakerLQ::CookdEdx(AliTRDseedV1 *trklt)
   fdEdx[1] = TMath::Log(dedx[2]);
   return fdEdx;
 }
-
-#include "../Cal/AliTRDCalPIDLQ.h"
 
 //__________________________________________________________________
 TObject* AliTRDpidRefMakerLQ::GetOCDBEntry(Option_t *opt)
@@ -145,16 +137,12 @@ Bool_t AliTRDpidRefMakerLQ::GetRefFigure(Int_t ifig)
     AliError("Ref fig requested outside definition.");
     return kFALSE;
   }
-  if(!fResults){ 
-    AliError("No results processed.");
-    return kFALSE;
-  }
   if(!gPad){
     AliWarning("Please provide a canvas to draw results.");
     return kFALSE;
   }
 
-  TObjArray *arr = (TObjArray*)fResults->At(ifig);
+  TObjArray *arr = (TObjArray*)fContainer->At(ifig);
   gPad->Divide(3, 2, 1.e-5, 1.e-5); 
   TList *l=gPad->GetListOfPrimitives(); 
   for(Int_t is=0; is<AliPID::kSPECIES; is++){
@@ -170,9 +158,10 @@ Bool_t AliTRDpidRefMakerLQ::GetRefFigure(Int_t ifig)
 void AliTRDpidRefMakerLQ::Fill()
 {
   // get particle type
-  fSbin = TMath::LocMax(AliPID::kSPECIES, fPID);
+  Int_t fSbin = TMath::LocMax(AliPID::kSPECIES, fPID);
   // get momentum bin
-  fPbin = AliTRDpidUtil::GetMomentumBin(fP);
+  Int_t fPbin = AliTRDpidUtil::GetMomentumBin(fP);
+  fPIDbin = ((fSbin&0xf)<<4)|(fPbin&0xf);
   // fill data
   fData->Fill();
   // fill monitor
@@ -196,40 +185,32 @@ Bool_t AliTRDpidRefMakerLQ::PostProcess()
   TFile *fCalib = TFile::Open(Form("TRD.Calib%s.root", GetName()), "update");
   fData = dynamic_cast<TTree*>(gFile->Get(GetName()));
   if (!fData) {
-    AliError("Tree not available");
+    AliError("Calibration data not available");
     return kFALSE;
   }
+  TObjArray *o = 0x0;
+  if(!(o = (TObjArray*)gFile->Get(Form("Moni%s", GetName())))){
+    AliWarning("Missing monitoring container.");
+    return kFALSE;
+  }
+  fContainer = (TObjArray*)o->Clone("monitor");
+
   TDatime d;
   TDirectoryFile *pdfs = new TDirectoryFile(Form("PDF_%d", d.GetDate()), "PDFs for LQ TRD-PID", "", gFile);
   pdfs->Write();
   AliDebug(2, Form("Data[%d]", fData->GetEntries()));
-
-  // save a volatile PDF representation
-  gROOT->cd();
-  TH2 *h2 = 0x0;
-  fResults = new TObjArray(AliTRDCalPID::kNMom);
-  for(Int_t ip=AliTRDCalPID::kNMom; ip--;){ 
-    TObjArray *arr = new TObjArray(AliPID::kSPECIES);
-    arr->SetName(Form("Pbin%02d", ip));
-    for(Int_t is=AliPID::kSPECIES; is--;) {
-      h2 = new TH2D(Form("i%s%d", AliPID::ParticleShortName(is), ip), Form("Interpolated %s dEdx @ Pbin[%d]", AliPID::ParticleName(is), ip), 50, -3., 3., 50, -3., 3.);
-      h2->GetXaxis()->SetTitle("log(dE/dx^{*}_{am}) [au]");
-      h2->GetYaxis()->SetTitle("log(dE/dx^{*}_{dr}) [au]");
-      h2->GetZaxis()->SetTitle("#");
-      arr->AddAt(h2, is);
-    }
-    fResults->AddAt(arr, ip);
-  }
   pdfs->cd();
 
   TCanvas *cc = new TCanvas("cc", "", 500, 500);
 
   Float_t *data[] = {0x0, 0x0};
-  TPrincipal principal(2, "ND");
-  for(Int_t ip=AliTRDCalPID::kNMom; ip--; ){ 
+//  TPrincipal principal(2, "ND");
+//  for(Int_t ip=AliTRDCalPID::kNMom; ip--; ){ 
+  for(Int_t ip=5; ip>0; ip-=10){ 
     for(Int_t is=AliPID::kSPECIES; is--;) {
-      principal.Clear();
-      Int_t n = fData->Draw("dEdx[0]:dEdx[1]", Form("p==%d&&s==%d", ip, is), "goff");
+//      principal.Clear();
+      Int_t bin = (is<<4) | ip;
+      Int_t n = fData->Draw("dEdx[0]:dEdx[1]", Form("ps==%d", bin), "goff");
 
       // estimate bucket statistics
       Int_t nb(kMinBuckets), // number of buckets
@@ -238,61 +219,47 @@ Bool_t AliTRDpidRefMakerLQ::PostProcess()
 // if(Float_t(n)/nb < 220.) ns = 200; // 7% stat error
 //       else if(Float_t(n)/nb < 420.) ns = 400; // 5% stat error
 
-      AliDebug(2, Form("pBin[%d] sBin[%d] N[%d] n[%d] nb[%d]", ip, is, n, ns, nb));
+      AliDebug(2, Form("pBin[%d] sBin[%d] n[%d] ns[%d] nb[%d]", ip, is, n, ns, nb));
       if(ns<Int_t(kMinStat)){
-        AliWarning(Form("Not enough entries [%d] for %s[%d].", ns, AliPID::ParticleShortName(is), ip));
+        AliWarning(Form("Not enough entries [%d] for %s[%d].", n, AliPID::ParticleShortName(is), ip));
         continue;
       }
       // allocate storage
       data[0] = new Float_t[n];data[1] = new Float_t[n];
-
-      // fill and make principal
-      Double_t *v1 = fData->GetV1(), *v2 = fData->GetV2();
-      while(n--){
-        Double_t dedx[] = {v1[n], v2[n]};
-        principal.AddRow(dedx);
-      }
-      principal.MakePrincipals();
-
-//       // calculate covariance ellipse
-//       eValues  = principal.GetEigenValues();
-//       x0  = 0.;
-//       y0  = 0.;
-//       rx  = 3.5*sqrt((*eValues)[0]);
-//       ry  = 3.5*sqrt((*eValues)[1]);
-
-      // rotate to principal axis
-      const Double_t *xx; Double_t rxy[2];
-      while((xx = principal.GetRow(++n))){
-        principal.X2P(xx, rxy);
-        data[0][n]=rxy[0]; data[1][n]=rxy[1];
+      // fill
+      Double_t *v0 = fData->GetV1(), *v1 = fData->GetV2();
+      for(Int_t in=n; in--;){
+        data[0][in]=v0[in]; data[1][in]=v1[in];
       }
 
       // build PDF
       TKDPDF pdf(n, 2, ns, data);
+      pdf.SetCOG(kFALSE);
+      pdf.SetWeights();
       pdf.SetStore();
       pdf.SetAlpha(5.);
-      //pdf.GetStatus();
-      Float_t *c, v, ve; Double_t r, e;
+      pdf.GetStatus();
+      Float_t *c, v, ve; Double_t r, e, rxy[2];
       for(Int_t in=pdf.GetNTNodes(); in--;){
         pdf.GetCOGPoint(in, c, v, ve);
         rxy[0] = (Double_t)c[0];rxy[1] = (Double_t)c[1];
         pdf.Eval(rxy, r, e, kTRUE);
       }
-      // visual on-line monitoring
-      pdf.DrawBins(0,1,-5.,5.,-8.,8.);cc->Modified(); cc->Update(); cc->SaveAs(Form("pdf_%s%02d.gif", AliPID::ParticleShortName(is), ip));
-      cc->SaveAs(Form("%s_%s%02d.gif", GetName(), AliPID::ParticleShortName(is), ip));
+//       // visual on-line monitoring
+//       pdf.DrawProjection();cc->Modified(); cc->Update(); cc->SaveAs(Form("pdf_%s%02d.gif", AliPID::ParticleShortName(is), ip));
+//       cc->SaveAs(Form("%s_%s%02d.gif", GetName(), AliPID::ParticleShortName(is), ip));
 
       // save a discretization of the PDF for result monitoring
-      TH2 *h2s = (TH2D*)((TObjArray*)fResults->At(ip))->At(is);
+      TH2 *h2s = (TH2D*)((TObjArray*)fContainer->At(ip))->At(is);
       TAxis *ax = h2s->GetXaxis(), *ay = h2s->GetYaxis();
+      h2s->Clear();
       for(int ix=1; ix<=ax->GetNbins(); ix++){
         rxy[0] = ax->GetBinCenter(ix);
         for(int iy=1; iy<=ay->GetNbins(); iy++){
           rxy[1] = ay->GetBinCenter(iy);
       
           Double_t rr,ee;
-          pdf.Eval(rxy, rr, ee);
+          pdf.Eval(rxy, rr, ee, kFALSE);
           if(rr<0. || ee/rr>.15) continue; // 15% relative error
           //printf("x[%2d] x[%2d] r[%f] e[%f]\n", ix, iy, rr, ee);
           h2s->SetBinContent(ix, iy, rr);
@@ -300,6 +267,7 @@ Bool_t AliTRDpidRefMakerLQ::PostProcess()
       }
 
       // write results to output array
+      //pdf.GetStatus();
       pdf.Write(Form("%s[%d]", AliPID::ParticleShortName(is), ip));
 
       delete [] data[0]; delete [] data[1];
