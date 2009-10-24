93ffc6a75bab14086944e80ceb4618b1ae6630ce
sgorbuno <sgorbuno@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-10-24 04:25:47 +0000

    swap of the input parameters in Propagate()

--- HLT/TPCLib/AliHLTTPCdEdxComponent.cxx
+++ HLT/TPCLib/AliHLTTPCdEdxComponent.cxx
@@ -458,7 +458,7 @@ int AliHLTTPCdEdxComponent::DoEvent
 	tTPC.SetClusterPointer( row, c );
 	
 	AliTPCTrackerPoint &point = *( tTPC.GetTrackPoint( row ) );
-	tTPC.Propagate( TMath::DegToRad()*(sec%18*20.+10.), fSolenoidBz, c->GetX() );
+	tTPC.Propagate( TMath::DegToRad()*(sec%18*20.+10.), c->GetX(), fSolenoidBz );
 	Double_t angle2 = tTPC.GetSnp()*tTPC.GetSnp();
 	angle2 = (angle2<1) ?TMath::Sqrt(angle2/(1-angle2)) :10.; 
 	point.SetAngleY( angle2 );
