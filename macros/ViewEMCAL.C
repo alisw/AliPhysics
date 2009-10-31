108b67a8a2cf7785ebaf9990c66b7a27deb5ab3c
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-10-31 10:45:54 +0000

    Obsolete macros

--- macros/ViewEMCAL.C
+++
@@ -1,7 +0,0 @@
-void ViewEMCAL(){
-    gMC->Gsatt("XEN1","seen",0);  // EMCAL envelope/mother volume
-    gMC->Gsatt("XALU","seen",1);  //
-    gMC->Gsatt("XPST","seen",0);
-    gMC->Gsatt("XPBX","seen",0);
-    
-}
