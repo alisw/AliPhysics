719e4f261c16b29a6e3d4d23429995148f87bdec
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-01 21:04:37 +0000

    New reconstruction parameters for all event species: Default, Cosmic, lowMult, highMult

--- PHOS/MakePHOSRecoParam.C
+++ PHOS/MakePHOSRecoParam.C
@@ -5,11 +5,12 @@ void MakePHOSRecoParam(AliRecoParam::EventSpecie_t default=AliRecoParam::kDefaul
   // Steering macro for PHOS reconstruction parameters
   //
   // Author: Yuri Kharlov
-  // 13.08.2008
+  // 01.11.2009
   //========================================================================
-
+  // $Id$ */
 
   const char* macroname = "MakePHOSRecoParam.C";
+  const Int_t firstRun = 85779;
 
   // Activate CDB storage and load geometry from CDB
   AliCDBManager* cdb = AliCDBManager::Instance();
@@ -18,21 +19,56 @@ void MakePHOSRecoParam(AliRecoParam::EventSpecie_t default=AliRecoParam::kDefaul
   TObjArray *recoParamArray = new TObjArray();
 
   {
+    // Default reconstruction parameters
     AliPHOSRecoParam * phosRecoParam = AliPHOSRecoParam::GetDefaultParameters();
     phosRecoParam->SetEventSpecie(AliRecoParam::kDefault);
     phosRecoParam->SetEMCSubtractPedestals(kFALSE);
     phosRecoParam->SetEMCRawDigitThreshold(2);
     phosRecoParam->SetEMCMinE(0.012);
     phosRecoParam->SetEMCClusteringThreshold(0.20);
-    phosRecoParam->SetEMCDecoderVersion("v1");
+    phosRecoParam->SetEMCFitterVersion("v0");
     phosRecoParam->SetEMCSampleQualityCut(10.);
+    phosRecoParam->SetName("Default");
     recoParamArray->AddLast(phosRecoParam);
   }
   {
+    // Reconstruction parameters for cosmic run 2009
     AliPHOSRecoParam * phosRecoParam = AliPHOSRecoParam::GetDefaultParameters();
-    recoParamArray->AddLast(phosRecoParam);
-    phosRecoParam->SetEMCClusteringThreshold(0.02008);
     phosRecoParam->SetEventSpecie(AliRecoParam::kCosmic);
+    phosRecoParam->SetEMCSubtractPedestals(kFALSE);
+    phosRecoParam->SetEMCMinE(0.025);
+    phosRecoParam->SetEMCClusteringThreshold(0.05);
+    phosRecoParam->SetEMCFitterVersion("v0");
+    phosRecoParam->SetEMCUnfolding(kFALSE);
+    phosRecoParam->SetEMCEnergyCorrectionOn(kFALSE);
+    phosRecoParam->SetName("Cosmic2009");
+    recoParamArray->AddLast(phosRecoParam);
+  }
+  {
+    // Reconstruction parameters for the first pp run 2009
+    AliPHOSRecoParam * phosRecoParam = AliPHOSRecoParam::GetDefaultParameters();
+    phosRecoParam->SetEventSpecie(AliRecoParam::kLowMult);
+    phosRecoParam->SetEMCSubtractPedestals(kFALSE);
+    phosRecoParam->SetEMCRawDigitThreshold(2);
+    phosRecoParam->SetEMCMinE(0.012);
+    phosRecoParam->SetEMCClusteringThreshold(0.22);
+    phosRecoParam->SetEMCFitterVersion("v0");
+    phosRecoParam->SetEMCUnfolding(kTRUE);
+    phosRecoParam->SetName("LowMult2009");
+    recoParamArray->AddLast(phosRecoParam);
+  }
+  {
+    // Reconstruction parameters for the first PbPb run 2010
+    AliPHOSRecoParam * phosRecoParam = AliPHOSRecoParam::GetDefaultParameters();
+    phosRecoParam->SetEventSpecie(AliRecoParam::kHighMult);
+    phosRecoParam->SetEMCSubtractPedestals(kFALSE);
+    phosRecoParam->SetEMCRawDigitThreshold(2);
+    phosRecoParam->SetEMCMinE(0.015);
+    phosRecoParam->SetEMCClusteringThreshold(0.40);
+    phosRecoParam->SetEMCFitterVersion("v0");
+    phosRecoParam->SetEMCUnfolding(kTRUE);
+    phosRecoParam->SetName("HighMult2009");
+    recoParamArray->AddLast(phosRecoParam);
   }
 
   // Set the default
@@ -54,12 +90,11 @@ void MakePHOSRecoParam(AliRecoParam::EventSpecie_t default=AliRecoParam::kDefaul
   // save in CDB storage
   AliCDBMetaData *md= new AliCDBMetaData();
   md->SetResponsible("Yuri Kharlov");
-  md->SetComment("Reconstruction parameters PHOS");
+  md->SetComment("Reconstruction parameters PHOS for cosmic, lowMult and highMult");
   md->SetAliRootVersion(gSystem->Getenv("ARVERSION"));
   md->SetBeamPeriod(0);
-  AliCDBId id("PHOS/Calib/RecoParam",0,AliCDBRunRange::Infinity());
+  AliCDBId id("PHOS/Calib/RecoParam",firstRun,AliCDBRunRange::Infinity());
   cdb->GetDefaultStorage()->Put(recoParamArray,id, md);
 
   return;
 }
-
