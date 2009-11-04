a078f1b000190f641191c2dd47d19352dddfa1b5
odjuvsla <odjuvsla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-04 02:00:01 +0000

    - adding digit readers to assist with the linked list of digits

---
+++ HLT/PHOS/AliHLTPHOSDigitReader.cxx
@@ -0,0 +1,62 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Authors: Oystein Djuvsland <oysteind@ift.uib.no>                       *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+
+#include "AliHLTPHOSDigitReader.h"
+#include "AliHLTPHOSDigitDataStruct.h"
+
+
+AliHLTPHOSDigitReader::AliHLTPHOSDigitReader() :
+  fDigitHeader(0)
+{
+  // See header file for documentation
+}
+
+AliHLTPHOSDigitReader::~AliHLTPHOSDigitReader()
+{
+  // See header file for documentation
+}
+
+
+AliHLTPHOSDigitDataStruct* AliHLTPHOSDigitReader::NextDigit()
+{
+
+  fPrevDigit = fCurrentDigit;
+  fCurrentDigit = fNextDigit;
+
+  if(fCurrentDigit->fMemOffsetNext != 0)
+    {
+      fNextDigit = reinterpret_cast<AliHLTPHOSDigitDataStruct*>(reinterpret_cast<Int_t>(fCurrentDigit) + fCurrentDigit->fMemOffsetNext);
+    }
+  else
+    {
+      fNextDigit = 0;
+    }
+
+  return fCurrentDigit;
+}
+
+void AliHLTPHOSDigitReader::DropDigit()
+{
+  if(fCurrentDigit == fFirstDigit)
+    {
+      fFirstDigit = reinterpret_cast<AliHLTPHOSDigitDataStruct*>(reinterpret_cast<Int_t>(fFirstDigit) + fFirstDigit->fMemOffsetNext);
+      fDigitHeader->fFirstDigitOffset += fCurrentDigit->fMemOffsetNext;
+    }
+  else if(fCurrentDigit != 0)
+    {
+      fPrevDigit->fMemOffsetNext = fPrevDigit->fMemOffsetNext + fCurrentDigit->fMemOffsetNext;
+    }
+  fCurrentDigit = fPrevDigit;
+}
---
+++ HLT/PHOS/AliHLTPHOSDigitReader.h
@@ -0,0 +1,86 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Authors: Oystein Djuvsland <oysteind@ift.uib.no>                       *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+#ifndef ALIHLTPHOSDIGITREADER_H
+#define ALIHLTPHOSDIGITREADER_H
+
+
+/** @file   AliHLTPHOSClusterizerComponent.cxx
+    @author Oystein Djuvsland
+    @date   
+    @brief  A clusterizer component for PHOS HLT
+*/
+
+// see header file for class documentation
+// or
+// refer to README to build package
+// or
+// visit http://web.ift.uib.no/~kjeks/doc/alice-hlt
+
+
+class AliHLTPHOSDigitDataStruct;
+
+/** 
+ * @class AliHLTPHOSDigitReader
+ * Class takes as input a AliHLTPHOSDigitHeaderStruct and iterates through 
+ * the list of digits following the header
+ * @ingroup alihlt_phos
+ */
+class AliHLTPHOSDigitReader
+{
+public:
+  AliHLTPHOSDigitReader();			
+  virtual ~AliHLTPHOSDigitReader();
+
+  void SetDigitHeader(AliHLTPHOSDigitHeaderStruct *digitHeader) 
+  { 
+    fDigitHeader = digitHeader; 
+    if(fDigitHeader->fNDigits != 0)
+      {
+	fFirstDigit = reinterpret_cast<AliHLTPHOSDigitDataStruct>(reinterpret_cast<Int_t>(fDigitHeader) + sizeof(AliHLTPHOSDigitHeaderStruct) + fDigitHeader->fFirstDigitOffset);
+      }
+    else 
+      {
+	fFirstDigit = 0;
+      }
+    fNextDigit = fFirstDigit;
+  }
+    
+  void SetCurrentDigit(AliHLTPHOSDigitDataStruct *currentDigit) { fCurrentDigit = currentDigit; }
+
+  AliHLTPHOSDigitDataStruct* NextDigit();
+
+  void DropDigit();
+
+  void Rewind() { fCurrentDigit = fFirstDigit; }
+
+  Int_t GetCurrentDigitOffset() { return reinterpret_cast<Int_t>(fCurrentDigit) - reinterpret_cast<Int_t>(fDigitHeader) + sizeof(AliHLTPHOSDigitHeaderStruct);
+
+private:
+  
+  /** Pointer to the digit header */
+  AliHLTPHOSDigitHeaderStruct *fDigitHeader;    //COMMENT
+
+  /** Pointer to the current digit */
+  AliHLTPHOSDigitDataStruct *fCurrentDigit;    //COMMENT
+
+  /** Pointer to the current digit */
+  AliHLTPHOSDigitDataStruct *fPrevDigit;    //COMMENT
+
+  /** Pointer to the first digit */
+  AliHLTPHOSDigitDataStruct *fFirstDigit;    //COMMENT
+};
+
+
+#endif
