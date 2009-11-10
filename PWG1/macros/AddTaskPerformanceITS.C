4448b631385b61e3521af45c951b211500198fc9
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-10 23:59:10 +0000

    New task for ITS tracking check

---
+++ PWG1/macros/AddTaskPerformanceITS.C
@@ -0,0 +1,38 @@
+AliAnalysisTaskITSTrackingCheck *AddTaskPerformanceITS(Bool_t readMC=kTRUE) 
+{
+  //
+  // Task for check of ITS tracking
+  //
+  // andrea.dainese@pd.infn.it
+  //
+
+
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTask", "No analysis manager to connect to.");
+    return NULL;
+  }   
+
+  // Create the task
+  AliAnalysisTaskITSTrackingCheck *taskITS = new AliAnalysisTaskITSTrackingCheck("ITStracking");
+  taskITS->SetReadMC(readMC);
+  AliLog::SetClassDebugLevel("AliAnalysisTaskITSTrackingCheck",10);
+  // Add to the manager
+  mgr->AddTask(taskITS);
+
+  //
+  // Create containers for input/output
+  AliAnalysisDataContainer *cInputITS = mgr->CreateContainer("cInputITS",TChain::Class(),AliAnalysisManager::kInputContainer);
+
+  AliAnalysisDataContainer *cOutputITS = mgr->CreateContainer("cOutputITS",TList::Class(),AliAnalysisManager::kOutputContainer,"ITS.Performance.root");
+
+
+  // Attach input
+  mgr->ConnectInput(taskITS,0,mgr->GetCommonInputContainer());
+  // Attach output
+  mgr->ConnectOutput(taskITS,0,cOutputITS);
+  
+  return taskITS;
+}
