15c616e1796adbed903873ec45de84821da36417
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-11 18:23:05 +0000

    document a few settings

--- PWG2/FLOW/Tools/glauberMC/runGlauberMCexample.C
+++ PWG2/FLOW/Tools/glauberMC/runGlauberMCexample.C
@@ -2,7 +2,16 @@
   //load libraries
   gROOT->Macro("initGlauberMC.C");
 
-  //run the example code:
-  //AliGlauberMC::runAndSaveNucleons(10000,"Pb","Pb",72);
-  AliGlauberMC::runAndSaveNtuple(10000,"Pb","Pb",72);
+  Int_t nevents = 10000; // number of events to simulate 
+  // supported systems are e.g. "p", "d", "Si", "Au", "Pb", "U" 
+  Option_t *sysA="Pb"; 
+  Option_t *sysB="Pb";
+  Double_t signn=72; // inelastic nucleon nucleon cross section
+  const char *fname="GlauberMC_PbPb_ntuple.root"; // name output file
+
+  // run the code to produce an ntuple:
+  //  AliGlauberMC::runAndSaveNucleons(10000,"Pb","Pb",72);
+  Double_t mind=0.4;
+  AliGlauberMC::runAndSaveNtuple(nevents,sysA,sysB,signn,mind,fname);
+
 }
