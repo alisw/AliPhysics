659101dd6a78bdb59a21cc5b611f8e3c10f7069a
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-11 15:47:34 +0000

    Sector numbering corrected in stepmanager (has changed in the geometry after previous commit)

--- VZERO/AliVZEROv7.cxx
+++ VZERO/AliVZEROv7.cxx
@@ -2918,25 +2918,41 @@ void AliVZEROv7::StepManager()
   vol[2]    = gMC->CurrentVolID(copy);
   vol[3]    = copy;
   static Int_t idV0R1 = gMC->VolId("V0R1");
-  static Int_t idV0L1 = gMC->VolId("V0L1");
+
+  static Int_t idV0L11 = gMC->VolId("V0L1Sec1");
+  static Int_t idV0L12 = gMC->VolId("V0L1Sec2");
+  static Int_t idV0L13 = gMC->VolId("V0L1Sec3");
+  static Int_t idV0L14 = gMC->VolId("V0L1Sec4");
   static Int_t idV0L15 = gMC->VolId("V0L15");
   static Int_t idV0L16 = gMC->VolId("V0L16");
   static Int_t idV0L17 = gMC->VolId("V0L17");
   static Int_t idV0L18 = gMC->VolId("V0L18");  
   static Int_t idV0R2 = gMC->VolId("V0R2");
-  static Int_t idV0L2 = gMC->VolId("V0L2");
+
+  static Int_t idV0L21 = gMC->VolId("V0L2Sec1");
+  static Int_t idV0L22 = gMC->VolId("V0L2Sec2");
+  static Int_t idV0L23 = gMC->VolId("V0L2Sec3");
+  static Int_t idV0L24 = gMC->VolId("V0L2Sec4");
   static Int_t idV0L25 = gMC->VolId("V0L25");
   static Int_t idV0L26 = gMC->VolId("V0L26");
   static Int_t idV0L27 = gMC->VolId("V0L27");
   static Int_t idV0L28 = gMC->VolId("V0L28");
   static Int_t idV0R3 = gMC->VolId("V0R3");
-  static Int_t idV0L3 = gMC->VolId("V0L3");
+
+  static Int_t idV0L31 = gMC->VolId("V0L3Sec1");
+  static Int_t idV0L32 = gMC->VolId("V0L3Sec2");
+  static Int_t idV0L33 = gMC->VolId("V0L3Sec3");
+  static Int_t idV0L34 = gMC->VolId("V0L3Sec4");
   static Int_t idV0L35 = gMC->VolId("V0L35");
   static Int_t idV0L36 = gMC->VolId("V0L36");
   static Int_t idV0L37 = gMC->VolId("V0L37");
   static Int_t idV0L38 = gMC->VolId("V0L38");
   static Int_t idV0R4 = gMC->VolId("V0R4");
-  static Int_t idV0L4 = gMC->VolId("V0L4");
+
+  static Int_t idV0L41 = gMC->VolId("V0L4Sec1");
+  static Int_t idV0L42 = gMC->VolId("V0L4Sec2");
+  static Int_t idV0L43 = gMC->VolId("V0L4Sec3");
+  static Int_t idV0L44 = gMC->VolId("V0L4Sec4");
   static Int_t idV0L45 = gMC->VolId("V0L45");
   static Int_t idV0L46 = gMC->VolId("V0L46");
   static Int_t idV0L47 = gMC->VolId("V0L47");
@@ -2948,18 +2964,93 @@ void AliVZEROv7::StepManager()
   double lightAttenuation;
   double nMeters; 
   double fibToPhot;
-  if      ( gMC->CurrentVolID(copy) == idV0R1 || gMC->CurrentVolID(copy) == idV0L1 || gMC->CurrentVolID(copy) == idV0L15 ||  gMC->CurrentVolID(copy) == idV0L16  || gMC->CurrentVolID(copy) == idV0L17  || gMC->CurrentVolID(copy) == idV0L18  )
-    ringNumber = 1;
-  else if ( gMC->CurrentVolID(copy) == idV0R2 || gMC->CurrentVolID(copy) == idV0L2 || gMC->CurrentVolID(copy) == idV0L25 || gMC->CurrentVolID(copy) == idV0L26 || gMC->CurrentVolID(copy) == idV0L27 || gMC->CurrentVolID(copy) == idV0L28 )
-    ringNumber = 2;  
-  else if ( gMC->CurrentVolID(copy) == idV0R3 || gMC->CurrentVolID(copy) == idV0R4
-	    || gMC->CurrentVolID(copy) == idV0L3 || gMC->CurrentVolID(copy) == idV0L35 || gMC->CurrentVolID(copy) == idV0L36 || gMC->CurrentVolID(copy) == idV0L37 || gMC->CurrentVolID(copy) == idV0L38 ) ringNumber = 3;
-  else if ( gMC->CurrentVolID(copy) == idV0R5 || gMC->CurrentVolID(copy) == idV0R6
-	    || gMC->CurrentVolID(copy) == idV0L4 || gMC->CurrentVolID(copy) == idV0L45 || gMC->CurrentVolID(copy) == idV0L46 || gMC->CurrentVolID(copy) == idV0L47 || gMC->CurrentVolID(copy) == idV0L48 ) ringNumber = 4;	       
+  if      ( gMC->CurrentVolID(copy) == idV0R1   || 
+	    gMC->CurrentVolID(copy) == idV0L11  || 
+	    gMC->CurrentVolID(copy) == idV0L12  || 
+	    gMC->CurrentVolID(copy) == idV0L13  || 
+	    gMC->CurrentVolID(copy) == idV0L14  || 
+	    gMC->CurrentVolID(copy) == idV0L15  ||  
+	    gMC->CurrentVolID(copy) == idV0L16  || 
+	    gMC->CurrentVolID(copy) == idV0L17  || 
+	    gMC->CurrentVolID(copy) == idV0L18  
+      )
+      ringNumber = 1;
+  
+  else if ( gMC->CurrentVolID(copy) == idV0R2  || 
+	    gMC->CurrentVolID(copy) == idV0L21 || 
+	    gMC->CurrentVolID(copy) == idV0L22 || 
+	    gMC->CurrentVolID(copy) == idV0L23 || 
+	    gMC->CurrentVolID(copy) == idV0L24 || 
+	    gMC->CurrentVolID(copy) == idV0L25 || 
+	    gMC->CurrentVolID(copy) == idV0L26 || 
+	    gMC->CurrentVolID(copy) == idV0L27 || 
+	    gMC->CurrentVolID(copy) == idV0L28 
+      )
+      ringNumber = 2; 
+  
+  else if ( gMC->CurrentVolID(copy) == idV0R3  || 
+	    gMC->CurrentVolID(copy) == idV0R4  || 
+	    gMC->CurrentVolID(copy) == idV0L31 || 
+	    gMC->CurrentVolID(copy) == idV0L32 || 
+	    gMC->CurrentVolID(copy) == idV0L33 || 
+	    gMC->CurrentVolID(copy) == idV0L34 || 
+	    gMC->CurrentVolID(copy) == idV0L35 || 
+	    gMC->CurrentVolID(copy) == idV0L36 || 
+	    gMC->CurrentVolID(copy) == idV0L37 || 
+	    gMC->CurrentVolID(copy) == idV0L38 
+      ) 
+      ringNumber = 3;
+  else if ( gMC->CurrentVolID(copy) == idV0R5  || 
+	    gMC->CurrentVolID(copy) == idV0R6  || 
+	    gMC->CurrentVolID(copy) == idV0L41 || 
+	    gMC->CurrentVolID(copy) == idV0L42 || 
+	    gMC->CurrentVolID(copy) == idV0L43 || 
+	    gMC->CurrentVolID(copy) == idV0L44 || 
+	    gMC->CurrentVolID(copy) == idV0L45 || 
+	    gMC->CurrentVolID(copy) == idV0L46 || 
+	    gMC->CurrentVolID(copy) == idV0L47 || 
+	    gMC->CurrentVolID(copy) == idV0L48 
+      ) ringNumber = 4;	       
+  
   else ringNumber = 0;
+  
   if  (ringNumber) {
-    if (gMC->CurrentVolID(copy) == idV0L1 || gMC->CurrentVolID(copy) == idV0L15  || gMC->CurrentVolID(copy) == idV0L16  || gMC->CurrentVolID(copy) == idV0L17 || gMC->CurrentVolID(copy) == idV0L18 || gMC->CurrentVolID(copy) == idV0L2 || gMC->CurrentVolID(copy) == idV0L25 || gMC->CurrentVolID(copy) == idV0L26  || gMC->CurrentVolID(copy) == idV0L27  || gMC->CurrentVolID(copy) == idV0L28 || gMC->CurrentVolID(copy) == idV0L3 || gMC->CurrentVolID(copy) == idV0L35 || gMC->CurrentVolID(copy) == idV0L36 || gMC->CurrentVolID(copy) == idV0L37 || gMC->CurrentVolID(copy) == idV0L38 || gMC->CurrentVolID(copy) == idV0L4 || gMC->CurrentVolID(copy) == idV0L45 || gMC->CurrentVolID(copy) == idV0L46 || gMC->CurrentVolID(copy) == idV0L47 || gMC->CurrentVolID(copy) == idV0L48)
-      hitOnV0C = false;
+      if (
+	  gMC->CurrentVolID(copy) == idV0L11 || 
+	  gMC->CurrentVolID(copy) == idV0L12 || 
+	  gMC->CurrentVolID(copy) == idV0L13 || 
+	  gMC->CurrentVolID(copy) == idV0L14 || 
+	  gMC->CurrentVolID(copy) == idV0L15 || 
+	  gMC->CurrentVolID(copy) == idV0L16 || 
+	  gMC->CurrentVolID(copy) == idV0L17 || 
+	  gMC->CurrentVolID(copy) == idV0L18 || 
+	  gMC->CurrentVolID(copy) == idV0L21 || 
+	  gMC->CurrentVolID(copy) == idV0L22 || 
+	  gMC->CurrentVolID(copy) == idV0L23 || 
+	  gMC->CurrentVolID(copy) == idV0L24 || 
+	  gMC->CurrentVolID(copy) == idV0L25 || 
+	  gMC->CurrentVolID(copy) == idV0L26 || 
+	  gMC->CurrentVolID(copy) == idV0L27 || 
+	  gMC->CurrentVolID(copy) == idV0L28 || 
+	  gMC->CurrentVolID(copy) == idV0L31 || 
+	  gMC->CurrentVolID(copy) == idV0L32 || 
+	  gMC->CurrentVolID(copy) == idV0L33 || 
+	  gMC->CurrentVolID(copy) == idV0L34 || 
+	  gMC->CurrentVolID(copy) == idV0L35 || 
+	  gMC->CurrentVolID(copy) == idV0L36 || 
+	  gMC->CurrentVolID(copy) == idV0L37 || 
+	  gMC->CurrentVolID(copy) == idV0L38 || 
+	  gMC->CurrentVolID(copy) == idV0L41 || 
+	  gMC->CurrentVolID(copy) == idV0L42 || 
+	  gMC->CurrentVolID(copy) == idV0L43 || 
+	  gMC->CurrentVolID(copy) == idV0L44 || 
+	  gMC->CurrentVolID(copy) == idV0L45 || 
+	  gMC->CurrentVolID(copy) == idV0L46 || 
+	  gMC->CurrentVolID(copy) == idV0L47 || 
+	  gMC->CurrentVolID(copy) == idV0L48
+	  )
+	  hitOnV0C = false;
+
     destep = gMC->Edep();
     step   = gMC->TrackStep();
     if (hitOnV0C) {
@@ -3108,26 +3199,45 @@ Int_t AliVZEROv7::GetCellId(Int_t *vol, Float_t *hits)
     //	 << " called " << gMC->VolName(vol[0]) << endl;
     // cout << " vol[2] = " << vol[2] << " copy : " << vol[3] 
     //	 << " called " << gMC->VolName(vol[2]) << endl;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L1")) fCellId = vol[1]+47;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L2")) fCellId = 8+vol[1]+47;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L3")) fCellId = 16+vol[1]+47;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L4")) fCellId = 24+vol[1]+47;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L15")) fCellId = 48+4;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L16")) fCellId = 48+5;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L17")) fCellId = 48+6;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L18")) fCellId = 48+7;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L25")) fCellId = 8+48+4;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L26")) fCellId = 8+48+5;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L27")) fCellId = 8+48+6;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L28")) fCellId = 8+48+7;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L35")) fCellId = 16+48+4;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L36")) fCellId = 16+48+5;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L37")) fCellId = 16+48+6;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L38")) fCellId = 16+48+7;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L45")) fCellId = 24+48+4;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L46")) fCellId = 24+48+5;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L47")) fCellId = 24+48+6;
-    if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L48")) fCellId = 24+48+7;
+    // upper half
+
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L1Sec1")) fCellId =  47 + 1;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L1Sec2")) fCellId =  47 + 2;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L1Sec3")) fCellId =  47 + 3;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L1Sec4")) fCellId =  47 + 4;
+
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L2Sec1")) fCellId =  47 +  9;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L2Sec2")) fCellId =  47 + 10;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L2Sec3")) fCellId =  47 + 11;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L2Sec4")) fCellId =  47 + 12;
+
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L3Sec1")) fCellId =  47 + 17;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L3Sec2")) fCellId =  47 + 18;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L3Sec3")) fCellId =  47 + 19;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L3Sec4")) fCellId =  47 + 20;
+
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L4Sec1")) fCellId =  47 + 25;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L4Sec2")) fCellId =  47 + 26;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L4Sec3")) fCellId =  47 + 27;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L4Sec4")) fCellId =  47 + 28;
+
+    // lower half 
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L15")) fCellId = 48+4;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L16")) fCellId = 48+5;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L17")) fCellId = 48+6;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L18")) fCellId = 48+7;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L25")) fCellId = 8+48+4;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L26")) fCellId = 8+48+5;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L27")) fCellId = 8+48+6;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L28")) fCellId = 8+48+7;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L35")) fCellId = 16+48+4;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L36")) fCellId = 16+48+5;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L37")) fCellId = 16+48+6;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L38")) fCellId = 16+48+7;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L45")) fCellId = 24+48+4;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L46")) fCellId = 24+48+5;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L47")) fCellId = 24+48+6;
+      if (gMC->CurrentVolID(vol[2]) == gMC->VolId("V0L48")) fCellId = 24+48+7;
   }
 
   return fCellId;
