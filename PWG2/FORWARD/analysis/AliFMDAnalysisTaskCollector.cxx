3a60cf4b60eaaada53d661f9fa1f30be94a0957f
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-12 21:25:49 +0000

    Removing redundant includes

--- PWG2/FORWARD/analysis/AliFMDAnalysisTaskCollector.cxx
+++ PWG2/FORWARD/analysis/AliFMDAnalysisTaskCollector.cxx
@@ -1,11 +1,32 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/* $Id:$ */
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//  Here some comments on what this task does                                //
  
-#include <TROOT.h>
-#include <TSystem.h>
-#include <TInterpreter.h>
 #include <TChain.h>
+#include <TF1.h>
 #include <TFile.h>
+#include <TInterpreter.h>
 #include <TList.h>
 #include <TMath.h>
+#include <TROOT.h>
+#include <TSystem.h>
 #include <iostream>
 
 #include "AliFMDAnalysisTaskCollector.h"
