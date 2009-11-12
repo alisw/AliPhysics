e4542afaf81dc89bbbc986026462e4a07111d8c5
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-12 13:51:10 +0000

    fix to run also in root under windows

--- PWG2/FLOW/Tools/glauberMC/initGlauberMC.C
+++
@@ -1,7 +0,0 @@
-{
-  gSystem->AddIncludePath("-I$ALICE_ROOT/include");
-  gSystem->AddIncludePath("-I$PWD");
-  gROOT->LoadMacro("AliGlauberNucleon.cxx++");
-  gROOT->LoadMacro("AliGlauberNucleus.cxx++");
-  gROOT->LoadMacro("AliGlauberMC.cxx++");
-}
