b1829325e48f46b4778245e611cbae858d5d891b
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-16 09:54:50 +0000

    cover case for AOD analysis

--- PWG4/macros/AddTaskJetSpectrum2.C
+++ PWG4/macros/AddTaskJetSpectrum2.C
@@ -1,4 +1,4 @@
-AliAnalysisTaskJetSpectrum2 *AddTaskJetSpectrum2()
+AliAnalysisTaskJetSpectrum2 *AddTaskJetSpectrum2(char* bRec = "jets",char* bGen = "jetsAODMC_UA104",UInt_t filterMask = 16)
 {
 // Creates a jet fider task, configures it and adds it to the analysis manager.
 
@@ -13,30 +13,40 @@ AliAnalysisTaskJetSpectrum2 *AddTaskJetSpectrum2()
    // Check the analysis type using the event handlers connected to the analysis manager.
    //==============================================================================
    if (!mgr->GetInputEventHandler()) {
-      ::Error("AddTaskJetSpectrum2", "This task requires an input event handler");
+     ::Error("AddTaskJetSpectrum2", "This task requires an input event handler");
       return NULL;
    }
 
+   TString type = mgr->GetInputEventHandler()->GetDataType();
+   TString typeMC(bGen);
    // Create the task and configure it.
    //===========================================================================
    
-   AliAnalysisTaskJetSpectrum2* pwg4spec = new  AliAnalysisTaskJetSpectrum2("Jet Spectrum");
+   AliAnalysisTaskJetSpectrum2* pwg4spec = new  AliAnalysisTaskJetSpectrum2(Form("Jet Spectrum %s %s",bRec,bGen));
       
    // or a config file
    pwg4spec->SetAnalysisType(AliAnalysisTaskJetSpectrum2::kAnaMC);
-   //      if(iAODanalysis)pwg4spec->SetAODInput(kTRUE);
+   // if(iAODanalysis)pwg4spec->SetAODInput(kTRUE);
    // pwg4spec->SetDebugLevel(11); 
-   pwg4spec->SetBranchGen("jetsMC"); 
-   pwg4spec->SetBranchRec("jetsAOD"); 
+   pwg4spec->SetBranchRec(bRec); 
+   pwg4spec->SetBranchGen(bGen); 
+   pwg4spec->SetFilterMask(filterMask); 
+
+   if(type == "AOD"){
+     pwg4spec->SetTrackTypeRec(AliAnalysisTaskJetSpectrum2::kTrackAODIn);
+     pwg4spec->SetAODInput(kTRUE);
+   }
+   else pwg4spec->SetTrackTypeRec(AliAnalysisTaskJetSpectrum2::kTrackAODOut);
+
+   if(typeMC.Contains("AODMC2"))pwg4spec->SetTrackTypeGen(AliAnalysisTaskJetSpectrum2::kTrackAODMCCharged);
+   else pwg4spec->SetTrackTypeGen(AliAnalysisTaskJetSpectrum2::kTrackAODMCAll);
+   
    mgr->AddTask(pwg4spec);
-
-
-
-      
+     
    // Create ONLY the output containers for the data produced by the task.
    // Get and connect other common input/output containers via the manager as below
    //==============================================================================
-   AliAnalysisDataContainer *coutput1_Spec = mgr->CreateContainer("pwg4spec2", TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:PWG4_spec2",AliAnalysisManager::GetCommonFileName()));
+   AliAnalysisDataContainer *coutput1_Spec = mgr->CreateContainer("pwg4spec2", TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:PWG4_spec2_%s_%s",AliAnalysisManager::GetCommonFileName(),bRec,bGen));
 
    mgr->ConnectInput  (pwg4spec, 0, mgr->GetCommonInputContainer());
    mgr->ConnectOutput (pwg4spec, 0, mgr->GetCommonOutputContainer());
