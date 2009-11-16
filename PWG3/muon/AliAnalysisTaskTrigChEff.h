a0bd9212f369d63675ae7838652e683fd5c3a153
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-16 08:08:08 +0000

    Fixing algorithm for efficiency calculation (Diego)

--- PWG3/muon/AliAnalysisTaskTrigChEff.h
+++ PWG3/muon/AliAnalysisTaskTrigChEff.h
@@ -1,19 +1,21 @@
-#include "TH1.h"
-#include "TList.h"
+/// \ingroup "PWG3muon"
+/// \class AliAnalysisTaskTrigChEff
+/// \brief Analysis task for trigger chamber efficiency determination
+///
+//  Author Diego Stocco
 
-#include "AliESDEvent.h"
-#include "AliAODEvent.h"
+class TList;
 
-class AliAnalysisTaskTrigChEff : public AliAnalysisTask {
+class AliAnalysisTaskTrigChEff : public AliAnalysisTaskSE {
  public:
   AliAnalysisTaskTrigChEff(const char *name = "AliAnalysisTaskTrigChEff");
   virtual ~AliAnalysisTaskTrigChEff() {}
-  
-  virtual void   ConnectInputData(Option_t *);
-  virtual void   CreateOutputObjects();
-  virtual void   Exec(Option_t *option);
+
+  virtual void   UserCreateOutputObjects();
+  virtual void   UserExec(Option_t *option);
   virtual void   Terminate(Option_t *);
 
+  /// Use ghost tracks in calculations
   void SetUseGhostTracks(Bool_t useGhosts = kTRUE) { fUseGhosts = useGhosts; }
 
 protected:
@@ -25,10 +27,9 @@ private:
   /// Not implemented
   AliAnalysisTaskTrigChEff& operator = (const AliAnalysisTaskTrigChEff& rhs);
     
-  AliESDEvent* fESD; //!< ESDevent object
-  Bool_t fUseGhosts; //!< Flag to use also the trigger tracks not matching the tracker in eff. calculation
+  Bool_t fUseGhosts; ///< Flag to use also the trigger tracks not matching the tracker in eff. calculation
 
-  TList*  fList; //TList output object
+  TList*  fList; ///<TList output object
 
   enum {
     kNcathodes = 2,  ///< Number of cathodes
@@ -37,16 +38,16 @@ private:
     kNslats    = 18 ///< Number of slats
   };
 
-  enum {kAllChEff, kChNonEff, kNcounts};
+  enum {kChHit, kChNonHit, kNcounts};
 
   enum {
     kHtracksInSlat  = 0,  ///< Tracks in slat histogram index
     kHtracksInBoard = 1,  ///< Tracks in board histogram index
-    kHchamberAllEff = 2,  ///< N44 per cathode histogram index
+    kHchamberEff    = 2,  ///< N44 per cathode histogram index
     kHchamberNonEff = 4,  ///< N33 per cathode histogram index
-    kHslatAllEff    = 6,  ///< N44 per slat histogram index
+    kHslatEff       = 6,  ///< N44 per slat histogram index
     kHslatNonEff    = 14, ///< N33 per slat histogram index
-    kHboardAllEff   = 22, ///< N44 per board histogram index
+    kHboardEff      = 22, ///< N44 per board histogram index
     kHboardNonEff   = 30, ///< N33 per board histogram index
     kHthetaX        = 38, ///< Angular distribution theta_x
     kHthetaY        = 39  ///< Angular distribution theta_y
@@ -55,6 +56,6 @@ private:
   /// Given cathode and chamber, return plane number
   Int_t GetPlane(Int_t cathode, Int_t chamber) { return kNchambers*cathode + chamber; }
 
-  ClassDef(AliAnalysisTaskTrigChEff, 0); // Single muon analysis
+  ClassDef(AliAnalysisTaskTrigChEff, 1); // Trigger chamber efficiency analysis
 };
 
