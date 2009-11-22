f7df083a0e12da43b2f3165414de859f16145fa1
shahoian <shahoian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-22 01:46:51 +0000

    link for new mp2 tracker

--- ITS/ITSrecLinkDef.h
+++ ITS/ITSrecLinkDef.h
@@ -122,6 +122,7 @@
 #pragma link C++ class AliITSAlignMille2Constraint+;
 #pragma link C++ class AliITSAlignMille2ConstrArray+;
 #pragma link C++ class AliITSAlignMilleData+;
+#pragma link C++ class AliITSTPArrayFit+;
 #pragma link C++ class AliITSResidualsAnalysis+;
 #pragma link C++ class AliITSRealignTracks+;
 // Classes for QA
