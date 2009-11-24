f130678316ff2f7e9839314497f119ba993e2065
agheata <agheata@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-24 12:53:49 +0000

    Root does not give precedence anymore to current directory in the dynamic library search path. In case of usage of par files, this leads to loading the libraries in aliroot libdir instead of the compiled ones. This fix adds . in the search path at the profit of all libs loaded after STEERBase.

--- STEER/PROOF-INF.STEERBase/SETUP.C
+++ STEER/PROOF-INF.STEERBase/SETUP.C
@@ -7,6 +7,9 @@ void SETUP()
    gSystem->Load("libPhysics");
 
    // Load the STEERBase library
+   TString dypath = gSystem->GetDynamicPath();
+   dypath.Prepend(".:");
+   gSystem->SetDynamicPath(dypath);
    gSystem->Load("libSTEERBase");
 
    // Set the include paths
