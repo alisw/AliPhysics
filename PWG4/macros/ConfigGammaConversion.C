33f7b21182c8b3f6a02047b0541451392caa20e7
kaamodt <kaamodt@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-26 17:34:45 +0000

    Added contact information

--- PWG4/macros/ConfigGammaConversion.C
+++ PWG4/macros/ConfigGammaConversion.C
@@ -1,5 +1,21 @@
 /** VERSION NUMBER 1.1 */
 
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: Ana Marin, Kathrin Koch, Kenneth Aamodt
+ * Contact: kenneth.aamodt@cern.ch
+ * Version 1.1                                                            *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
 class AliAnalysisDataContainer;
 class AliGammaConversionHistograms;
 class AliAnalysisTaskGammaConversion;
