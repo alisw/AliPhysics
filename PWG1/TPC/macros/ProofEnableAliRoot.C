3948223417fec133d7feb749996fcfbde6736944
jotwinow <jotwinow@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-29 19:55:50 +0000

    TPC related macros

---
+++ PWG1/TPC/macros/ProofEnableAliRoot.C
@@ -0,0 +1,18 @@
+
+void ProofEnableAliRoot(const char* location = "/usr/local/grid/AliRoot/v4-05-Release")
+{
+  // enables a locally deployed AliRoot in a PROOF cluster
+  gProof->Exec(Form("TString str(gSystem->ExpandPathName(\"%s\")); gSystem->Setenv(\"ALICE_ROOT\", str);", location), kTRUE);
+
+  gProof->AddIncludePath(Form("%s/include", location));
+  gProof->AddIncludePath(Form("%s/TPC", location));
+  gProof->AddIncludePath(Form("%s/PWG1", location));
+  gProof->AddIncludePath(Form("%s/ANALYSIS", location));
+
+  gProof->AddDynamicPath(Form("%s/lib/tgt_linuxx8664gcc", location));
+
+  // load all libraries
+  gProof->Exec("gROOT->Macro(\"$ALICE_ROOT/PWG1/TPC/macros/LoadMyLibs.C\")",kTRUE);
+}
+
+ 
