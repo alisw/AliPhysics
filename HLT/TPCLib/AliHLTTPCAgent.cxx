b08dd9ba810882054762d1ef89088ef649b3add7
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-30 08:48:32 +0000

    using '-release-memory' option for TPC clusterfinder allows reduced memory consumption for HLT chains in AliRoot

--- HLT/TPCLib/AliHLTTPCAgent.cxx
+++ HLT/TPCLib/AliHLTTPCAgent.cxx
@@ -140,13 +140,13 @@ int AliHLTTPCAgent::CreateConfigurations(AliHLTConfigurationHandler* handler,
 
 	// cluster finder components
 	cf.Form("TPC-CF_%02d_%d", slice, part);
-	arg="";
+	arg="-release-memory";
 	if (!rawReader && runloader) {
 	  arg+=" -do-mc";
 	  handler->CreateConfiguration(cf.Data(), "TPCClusterFinderUnpacked", publisher.Data(), arg.Data());
 	} else {
 #ifndef HAVE_NOT_ALTRORAWSTREAMV3
-	  handler->CreateConfiguration(cf.Data(), "TPCClusterFinder32Bit", publisher.Data(),"");
+	  handler->CreateConfiguration(cf.Data(), "TPCClusterFinder32Bit", publisher.Data(),arg.Data());
 #else
 	  // using the AltroDecoder if the official V3 decoder is not
 	  // available in the offline code
