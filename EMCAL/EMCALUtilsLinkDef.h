8d73749e67fd74c1812ddb8e6a269f7e6af956a8
dsilverm <dsilverm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-01 10:22:09 +0000

    adding AliEMCALGeoParams in Utils lib

--- EMCAL/EMCALUtilsLinkDef.h
+++ EMCAL/EMCALUtilsLinkDef.h
@@ -4,6 +4,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class AliEMCALGeoParams+;
 #pragma link C++ class AliEMCALGeoUtils+;
 #pragma link C++ class AliEMCALEMCGeometry+;
 #pragma link C++ class AliEMCALShishKebabTrd1Module+;
