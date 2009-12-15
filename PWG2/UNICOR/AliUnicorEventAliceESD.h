b465a7417b748f340263f30c0f137efc25caf8da
misko <misko@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-15 08:53:02 +0000

    Adapted to pp collision data.

--- PWG2/UNICOR/AliUnicorEventAliceESD.h
+++ PWG2/UNICOR/AliUnicorEventAliceESD.h
@@ -27,17 +27,17 @@ class AliUnicorEventAliceESD : public AliUnicorEvent {
   Double_t    Centrality() const {return 0.9999*exp(-NParticles()/20.0);} // OK for pp
   void        RP(Double_t &qx, Double_t &qy) const {AliUnicorEvent::RP(qx,qy,2);}
   Double_t    RPphi() const {Double_t qx,qy; RP(qx,qy); return atan2(qy,qx);}
-  Double_t    Zver() const {return fESD->GetPrimaryVertex()->GetZv()/10.0;}
+  Double_t    Zver() const {return fESD->GetPrimaryVertexTPC()->GetZv()/10.0;}
   Int_t       NParticles() const {return fESD->GetNumberOfTracks();}
 
   Bool_t      ParticleGood(Int_t i, Int_t pidi=0) const;
   Double_t    ParticleP(Int_t i)     const {return fESD->GetTrack(i)->GetTPCInnerParam()->P();}
   Double_t    ParticleTheta(Int_t i) const {return fESD->GetTrack(i)->GetTPCInnerParam()->Theta();}
   Double_t    ParticlePhi(Int_t i)   const {return TVector2::Phi_mpi_pi(fESD->GetTrack(i)->GetTPCInnerParam()->Phi());}
-  Double_t    ParticleDedx(Int_t i)  const {return fESD->GetTrack(i)->GetTPCsignal()/47.0;}
+  Double_t    ParticleDedx(Int_t i)  const {return fESD->GetTrack(i)->GetTPCsignal()/50.0;}
   Bool_t      PairGood(Double_t p0, Double_t the0, Double_t phi0, 
 		       Double_t p1, Double_t the1, Double_t phi1) const;
-  // alternative: GetTPCInnerParam, GetConstrainedParam
+  // alternative: GetConstrainedParam, GetInnerParam, GetTPCInnerParam 
   void        SetESD(AliESDEvent * const esd) {fESD = esd;}
   AliESDEvent *GetESD() const {return fESD;}
 
