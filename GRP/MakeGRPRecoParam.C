867d89fa586aa1e4c4c6cce8fdeca0ee1f6d7b9e
rgrosso <rgrosso@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-16 19:07:37 +0000

    primary vertex constraints enabled for the 2nd pass reconstruction (A.Dainese)

--- GRP/MakeGRPRecoParam.C
+++ GRP/MakeGRPRecoParam.C
@@ -18,16 +18,36 @@ void MakeGRPRecoParam(AliRecoParam::EventSpecie_t default=AliRecoParam::kLowMult
   {
     AliGRPRecoParam * param = AliGRPRecoParam::GetCosmicTestParam();
     param->SetEventSpecie(AliRecoParam::kCosmic);
+    param->SetVertexerTracksConstraintITS(kFALSE);
+    param->SetVertexerTracksConstraintTPC(kFALSE);
     recoParamArray->AddLast(param);
   }
   {
+    // new settings for pass 2reco of Dec09 pp data
     AliGRPRecoParam * param = AliGRPRecoParam::GetLowFluxParam();
     param->SetEventSpecie(AliRecoParam::kLowMult);
+    param->SetVertexerTracksConstraintITS(kTRUE);
+    Double_t cutsITS[12]={0.1,
+                          0.1,
+			  0.5,
+			  4, // minimum 4 clusters (default was 5)
+                          1,
+                          3.,
+                          100.,
+                          1000.,
+                          3.,
+                          30.,
+                          1,
+                          4};
+    param->SetVertexerTracksCutsITS(12,cutsITS);
+    param->SetVertexerTracksConstraintTPC(kTRUE);
     recoParamArray->AddLast(param);
   }
   {
     AliGRPRecoParam * param = AliGRPRecoParam::GetHighFluxParam();
     param->SetEventSpecie(AliRecoParam::kHighMult);
+    param->SetVertexerTracksConstraintITS(kFALSE);
+    param->SetVertexerTracksConstraintTPC(kFALSE);
     recoParamArray->AddLast(param);
   }
 
