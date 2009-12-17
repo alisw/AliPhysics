7122701769cdb6919a7b1aa69b6a3d6d111b5e94
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-17 14:42:52 +0000

    displaying bg histograms (Michele)

--- PWG0/eventStats/show.C
+++ PWG0/eventStats/show.C
@@ -19,4 +19,28 @@ void show(const char* fileName = "event_stat.root")
   hist->SetStats(0);
   hist->Draw("TEXT");
   c->SaveAs("bc.png");
+
+  // BG
+  gStyle->SetStatX(0.87);
+  gStyle->SetStatY(0.93);
+
+  TH2F * hBB  = (TH2F*) gDirectory->Get("physics_selection/background_identification/h_CINT1B-ABCE-NOPF-ALL");
+  TH2F * hBE  = (TH2F*) gDirectory->Get("physics_selection/background_identification/h_CINT1C-ABCE-NOPF-ALL");
+  TH2F * hBEA = (TH2F*) gDirectory->Get("physics_selection/background_identification/h_CINT1A-ABCE-NOPF-ALL");
+
+  if(!hBB || !hBE || !hBEA) {
+    printf("WARNING: no BG histos\n");
+  }
+  else
+  {
+    hBE->Add(hBEA);
+    hBE->SetTitle("CINT1A-ABCE-NOPF-ALL + CINT1C-ABCE-NOPF-ALL");
+    c = new TCanvas("cbg","cbg",800,400);
+    c->Divide(2,1);
+    c->cd(1);
+    hBB->Draw("colz");
+    c->cd(2);
+    hBE->Draw("colz");
+    c->Print("bg.png");
+  }
 }
