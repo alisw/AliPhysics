928215a8b2c716a46a617612aff6eae4b9ef15b3
dperrino <dperrino@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-17 09:28:04 +0000

    Added an output container to the DiJet task

--- PWG4/macros/AddTaskDiJets.C
+++ PWG4/macros/AddTaskDiJets.C
@@ -1,6 +1,6 @@
 AliAnalysisTaskDiJets *AddTaskDiJets()
 {
-// Creates a jet fider task, configures it and adds it to the analysis manager.
+// Creates a dijet task, configures it and adds it to the analysis manager.
 
    // Get the pointer to the existing analysis manager via the static access method.
    //==============================================================================
@@ -8,8 +8,8 @@ AliAnalysisTaskDiJets *AddTaskDiJets()
    if (!mgr) {
       ::Error("AddTaskJets", "No analysis manager to connect to.");
       return NULL;
-   }  
-   
+   }
+
    // Check the analysis type using the event handlers connected to the analysis manager.
    //==============================================================================
    if (!mgr->GetInputEventHandler()) {
@@ -19,16 +19,20 @@ AliAnalysisTaskDiJets *AddTaskDiJets()
 
    // Create the task and configure it.
    //===========================================================================
-   
+
    AliAnalysisTaskDiJets *dijetana = new AliAnalysisTaskDiJets("DiJetAnalysis");
-   
+   dijetana->SetDebugLevel(10);
    mgr->AddTask(dijetana);
-      
+
+   AliAnalysisDataContainer *cout_dijet = mgr->CreateContainer("DiJet", TList::Class(),AliAnalysisManager::kOutputContainer,
+     Form("%s:PWG4_DiJet",AliAnalysisManager::GetCommonFileName()));
+
    // Create ONLY the output containers for the data produced by the task.
    // Get and connect other common input/output containers via the manager as below
    //==============================================================================
    mgr->ConnectInput  (dijetana, 0, mgr->GetCommonInputContainer());
    mgr->ConnectOutput (dijetana, 0, mgr->GetCommonOutputContainer());
-   
+   mgr->ConnectOutput (dijetana, 1, cout_dijet);
+
    return dijetana;
 }
