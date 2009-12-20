d63987cc98337d50a9a515f5805ca6ea329be33c
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-20 23:03:31 +0000

    New base class.

---
+++ STEER/AliVCuts.h
@@ -0,0 +1,27 @@
+#ifndef AliVCuts_H
+#define AliVCuts_H
+/* Copyright(c) 1998-2007, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+//-------------------------------------------------------------------------
+//     Event cuts base class
+//     Author: Andreas Morsch, CERN
+//-------------------------------------------------------------------------
+
+#include <TNamed.h>
+
+class AliVCuts : public TNamed {
+
+ public :
+  AliVCuts(); 
+  AliVCuts(const char* name, const char* title); 
+  virtual ~AliVCuts() { };
+  AliVCuts(const AliVCuts& evt); 
+  AliVCuts& operator=(const AliVCuts& evt);
+  virtual Bool_t IsSelected(TObject* /* obj  */)  = 0;
+  ClassDef(AliVCuts,1);
+};
+
+#endif
