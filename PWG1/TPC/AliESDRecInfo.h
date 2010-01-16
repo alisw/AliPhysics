1fc39188b99986b3685a23719ec38ed0e79cc950
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-01-16 10:11:46 +0000

    Forward declaration (Stefan)

--- PWG1/TPC/AliESDRecInfo.h
+++ PWG1/TPC/AliESDRecInfo.h
@@ -24,6 +24,7 @@ class AliITStrackMI;
 class AliTRDtrackV1;
 class AliTPCParam;
 class AliTPCseed;
+class AliMCInfo;
 
 /////////////////////////////////////////////////////////////////////////
 class AliESDRecInfo: public TObject {
