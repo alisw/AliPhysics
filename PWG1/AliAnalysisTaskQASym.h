c3a00a7e55c4e75a1ffb265c668f72b9beee3c4e
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-01-16 11:37:01 +0000

    Coding rule violations corrected.

--- PWG1/AliAnalysisTaskQASym.h
+++ PWG1/AliAnalysisTaskQASym.h
@@ -1,6 +1,15 @@
-#ifndef AliAnalysisTaskQASym_cxx
-#define AliAnalysisTaskQASym_cxx
+#ifndef ALIANALYSISTASKQASYM_H
+#define ALIANALYSISTASKQASYM_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id:$ */
  
+//------------------------------
+// Analysis task for quality-assurance of central tracking
+// mainly based on fundamental symmetries 
+//
+// eva.sicking@cern.ch
 
 class TH1F;
 class TH2F;
@@ -13,8 +22,6 @@ class AliESDtrackCuts;
 
 
 #include "AliAnalysisTaskSE.h"
-#include "TFile.h"
-#include "TNtuple.h"
 
 class AliAnalysisTaskQASym : public AliAnalysisTaskSE {
  public:
@@ -35,7 +42,7 @@ class AliAnalysisTaskQASym : public AliAnalysisTaskSE {
  private:
 
  
-  Bool_t      fFieldOn;
+  Bool_t      fFieldOn;         // field flag
 
   TList       *fHists;          // List of histos
 
@@ -68,80 +75,66 @@ class AliAnalysisTaskQASym : public AliAnalysisTaskSE {
   TH1F        *fRecDPos;       // impact parameter of neg. particle
   TH1F        *fRecDNeg;       // impact parameter of neg. particle
 
-
-
-
   // two sides of TPC -> Eta/Theta
-  TH1F        *fRecQPtPosEta; 
-  TH1F        *fRecQPtNegEta;
-  TH1F        *fRecPtPosEta;  
-  TH1F        *fRecPtNegEta; 
-  TH1F        *fRecPhiPosEta;
-  TH1F        *fRecPhiNegEta;
-  TH1F        *fRecDcaPosEta;
-  TH1F        *fRecDcaNegEta;
-  TH1F        *fRecDPosEta;
-  TH1F        *fRecDNegEta;
+  TH1F        *fRecQPtPosEta;  // Q x Pt for pos. eta
+  TH1F        *fRecQPtNegEta;  // Q x Pt for neg. eta
+  TH1F        *fRecPtPosEta;   //     Pt for pos. eta
+  TH1F        *fRecPtNegEta;   //     Pt for neg. eta
+  TH1F        *fRecPhiPosEta;  // phi for pos. eta
+  TH1F        *fRecPhiNegEta;  // phi for neg. eta 
+  TH1F        *fRecDcaPosEta;  // dca for pos. eta 
+  TH1F        *fRecDcaNegEta;  // dca for neg. eta
+  TH1F        *fRecDPosEta;    // d   for pos. eta
+  TH1F        *fRecDNegEta;    // d   for neg. eta
 
-
- 
   // sectors of TPC (with pt>xGeV?), TODO: extent to TPC standalone tracks
-  TH1F        *fRecPtTpcSector[18];
-  TH1F        *fRecEtaTpcSector[18];
-  TH1F        *fRecQPtTpcSector[18];
-  TH1F        *fRecEtaPtTpcSector[18];
-  TH1F        *fSignedDcaTpcSector[18];
-
+  TH1F        *fRecPtTpcSector[18];      // pt per sector
+  TH1F        *fRecEtaTpcSector[18];     // eta per sector
+  TH1F        *fRecQPtTpcSector[18];     // Qxpt per sector
+  TH1F        *fRecEtaPtTpcSector[18];   // eta x pt per sector
+  TH1F        *fSignedDcaTpcSector[18];  // dca per sector
 
   // 7 different case of hit in ITS ladders
-  TH1F        *fSignDcaPos[7];
-  TH1F        *fSignDcaNeg[7];
-  TH1F        *fSignDcaNegInv[7];
-  TH1F        *fPtSigmaPos[7];
-  TH1F        *fPtSigmaNeg[7];
-  TH1F        *fRecPtPosLadder[7];
-  TH1F        *fRecPtNegLadder[7];
-  TH1F        *fRecPhiPosLadder[7];
-  TH1F        *fRecPhiNegLadder[7];
-  TH1F        *fRecEtaPosLadder[7];
-  TH1F        *fRecEtaNegLadder[7];
-
+  TH1F        *fSignDcaPos[7];           // dca for pos. charge       
+  TH1F        *fSignDcaNeg[7];           // dca for neg. charge
+  TH1F        *fSignDcaNegInv[7];        // dca for neg. charge
+  TH1F        *fPtSigmaPos[7];           // sigma pt for pos. charge
+  TH1F        *fPtSigmaNeg[7];           // sigma pt for neg. charge
+  TH1F        *fRecPtPosLadder[7];       // pt for pos. charge
+  TH1F        *fRecPtNegLadder[7];       // pt for neg. charge
+  TH1F        *fRecPhiPosLadder[7];      // phi for pos. charge
+  TH1F        *fRecPhiNegLadder[7];      // phi for neg. charge
+  TH1F        *fRecEtaPosLadder[7];      // eta for pos. charge
+  TH1F        *fRecEtaNegLadder[7];      // eta for neg. charge
 
   // 2D: all measures as function of z of vertex
-  TH2F        *fRecPtPosVz;
-  TH2F        *fRecPtNegVz;
-  TH2F        *fRecEtaPosVz;
-  TH2F        *fRecEtaNegVz;
-  TH2F        *fRecPhiPosVz;
-  TH2F        *fRecPhiNegVz;
-  TH2F        *fSignedDcaPosVz;
-  TH2F        *fSignedDcaNegVz;
-  TH2F        *fRecQPtPosEtaVz;
-  TH2F        *fRecQPtNegEtaVz;
-  TH2F        *fRecEtaPtPosVz;
-  TH2F        *fRecEtaPtNegVz;
-
+  TH2F        *fRecPtPosVz;              // pt for pos. charge
+  TH2F        *fRecPtNegVz;              // pt for neg. charge
+  TH2F        *fRecEtaPosVz;             // eta for pos. charge
+  TH2F        *fRecEtaNegVz;             // eta for neg. charge
+  TH2F        *fRecPhiPosVz;             // phi for pos. charge
+  TH2F        *fRecPhiNegVz;             // phi for neg. charge
+  TH2F        *fSignedDcaPosVz;          // dca for pos. charge
+  TH2F        *fSignedDcaNegVz;          // dca for neg. charge
+  TH2F        *fRecQPtPosEtaVz;          // qxpt for pos. charge
+  TH2F        *fRecQPtNegEtaVz;          // qxpt for neg. charge
+  TH2F        *fRecEtaPtPosVz;           // etaxpt for pos. charge
+  TH2F        *fRecEtaPtNegVz;           // etaxpt for neg. charge
 
   //high
-  TH1F * fDeltaPhiAll;
-  TH2F * fDeltaPhiLeading;
-  TH1F * fDiffDcaD;
+  TH1F * fDeltaPhiAll;                   // dphi
+  TH2F * fDeltaPhiLeading;               // dphi rel. to leading
+  TH1F * fDiffDcaD;                      // delta dca
 
   //sim
-  TH1F * fPhiRec;
-  TH1F * fThetaRec;
-  TH1F * fNumber;
-  TH1F * fVx;
-  TH1F * fVy;
-  TH1F * fVz;
-  TNtuple * test;
+  TH1F * fPhiRec;                        // phi
+  TH1F * fThetaRec;                      // theta
+  TH1F * fNumber;                        // n
+  TH1F * fVx;                            // vx
+  TH1F * fVy;                            // vy
+  TH1F * fVz;                            // vz
 
-
-
-  Double_t  sdca_tr;
-  Float_t xy, z;
-
-  AliESDtrackCuts* fCuts;                      // List of cuts
+  AliESDtrackCuts* fCuts;                // List of cuts
   AliAnalysisTaskQASym(const AliAnalysisTaskQASym&); // not implemented
   AliAnalysisTaskQASym& operator=(const AliAnalysisTaskQASym&); // not implemented
   
