ff4ef540f6bf05875f1c7a0f8eed35c0bba18a6a
rpreghen <rpreghen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-01-19 17:19:52 +0000

    updated hardcoded status information: disabled not connected MRPC strips

--- TOF/CheckCalibStatus.C
+++ TOF/CheckCalibStatus.C
@@ -29,12 +29,14 @@ CheckCalibStatus(AliCDBEntry *cdbe)
   AliTOFChannelOnlineStatusArray *array = (AliTOFChannelOnlineStatusArray *)cdbe->GetObject();
 
   TH1F *hStatus = new TH1F("hStatus", "Channel status;index;status", array->GetSize(), 0., array->GetSize(););
+  TH1F *hChEnabled = new TH1F("hChEnabled", "Channel enabled;index;enabled", array->GetSize(), 0., array->GetSize(););
   TH1F *hChNoisy = new TH1F("hChNoisy", "Channel noise flag;index;noise flag", array->GetSize(), 0., array->GetSize(););
   TH2F *hNoiseMap = new TH2F("hNoiseMap", "Noise map;sector;strip", 72, 0., 18., 91, 0., 91.);
   TH2F *hEnableMap = new TH2F("hEnableMap", "Enable map;sector;strip", 72, 0., 18., 91, 0., 91.);
 
   AliTOFcalibHisto calib;
   calib.LoadCalibHisto();
+  calib.LoadCalibStat(); /* temp */
 
   Int_t sector, sectorStrip, padx, fea;
   Float_t hitmapx, hitmapy;
@@ -46,16 +48,24 @@ CheckCalibStatus(AliCDBEntry *cdbe)
     fea = padx / 12;
     hitmapx = sector + ((Double_t)(3 - fea) + 0.5) / 4.;
     hitmapy = sectorStrip;
-    if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk) hEnableMap->Fill(hitmapx, hitmapy);
+    if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk &&
+	calib.GetCalibStat(AliTOFcalibHisto::kStripStat, i) == 1) hEnableMap->Fill(hitmapx, hitmapy);
     if (array->GetNoiseStatus(i) == AliTOFChannelOnlineStatusArray::kTOFNoiseBad) {
       hChNoisy->SetBinContent(i + 1, 1);
       hNoiseMap->Fill(hitmapx, hitmapy);
     }
+    if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk &&
+	array->GetNoiseStatus(i) != AliTOFChannelOnlineStatusArray::kTOFNoiseBad &&
+	calib.GetCalibStat(AliTOFcalibHisto::kStripStat, i) == 1) {
+      hChEnabled->SetBinContent(i + 1, 1);
+    }
+   
   }
 
   TFile *fout = TFile::Open("CheckCalibStatus.root", "RECREATE");
   hStatus->Write();
   hChNoisy->Write();
+  hChEnabled->Write();
   hNoiseMap->Write();
   hEnableMap->Write();
   fout->Close();
