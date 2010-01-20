b9bf65231cdd265c6c71d5de243472811a77d264
rpreghen <rpreghen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-01-20 16:06:18 +0000

    EnableMap histogram now shows channels enable in FEE readout. added status histogram to display hardcoded status of MRPC strips

--- TOF/CheckCalibStatus.C
+++ TOF/CheckCalibStatus.C
@@ -33,6 +33,7 @@ CheckCalibStatus(AliCDBEntry *cdbe)
   TH1F *hChNoisy = new TH1F("hChNoisy", "Channel noise flag;index;noise flag", array->GetSize(), 0., array->GetSize(););
   TH2F *hNoiseMap = new TH2F("hNoiseMap", "Noise map;sector;strip", 72, 0., 18., 91, 0., 91.);
   TH2F *hEnableMap = new TH2F("hEnableMap", "Enable map;sector;strip", 72, 0., 18., 91, 0., 91.);
+  TH2F *hStatusMap = new TH2F("hStatusMap", "Status map;sector;strip", 72, 0., 18., 91, 0., 91.);
 
   AliTOFcalibHisto calib;
   calib.LoadCalibHisto();
@@ -48,12 +49,16 @@ CheckCalibStatus(AliCDBEntry *cdbe)
     fea = padx / 12;
     hitmapx = sector + ((Double_t)(3 - fea) + 0.5) / 4.;
     hitmapy = sectorStrip;
-    if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk &&
-	calib.GetCalibStat(AliTOFcalibHisto::kStripStat, i) == 1) hEnableMap->Fill(hitmapx, hitmapy);
+
+    if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk) 
+      hEnableMap->Fill(hitmapx, hitmapy);
+    if (calib.GetCalibStat(AliTOFcalibHisto::kStripStat, i) == 1) 
+      hStatusMap->Fill(hitmapx, hitmapy);
     if (array->GetNoiseStatus(i) == AliTOFChannelOnlineStatusArray::kTOFNoiseBad) {
       hChNoisy->SetBinContent(i + 1, 1);
       hNoiseMap->Fill(hitmapx, hitmapy);
     }
+
     if (array->GetHWStatus(i) == AliTOFChannelOnlineStatusArray::kTOFHWOk &&
 	array->GetNoiseStatus(i) != AliTOFChannelOnlineStatusArray::kTOFNoiseBad &&
 	calib.GetCalibStat(AliTOFcalibHisto::kStripStat, i) == 1) {
@@ -68,6 +73,7 @@ CheckCalibStatus(AliCDBEntry *cdbe)
   hChEnabled->Write();
   hNoiseMap->Write();
   hEnableMap->Write();
+  hStatusMap->Write();
   fout->Close();
 
 }
