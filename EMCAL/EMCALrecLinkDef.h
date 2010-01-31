43a60164df269a6d7c92afc61d050d28d200d84a
dsilverm <dsilverm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-01-31 21:44:21 +0000

    from Per Thomas - add new raw ana classes in LinkDef also

--- EMCAL/EMCALrecLinkDef.h
+++ EMCAL/EMCALrecLinkDef.h
@@ -3,7 +3,6 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-
 #pragma link C++ class AliEMCALReconstructor+;
 #pragma link C++ class AliEMCALClusterizerv1+;
 #pragma link C++ class AliEMCALTrack+;
@@ -12,5 +11,12 @@
 #pragma link C++ class AliEMCALQADataMakerRec+;
 #pragma link C++ class AliEMCALAodCluster+;
 #pragma link C++ class AliCaloNeuralFit+;
+#pragma link C++ class AliCaloRawAnalyzer+;
+#pragma link C++ class AliCaloRawAnalyzerCrude+;
+#pragma link C++ class AliCaloRawAnalyzerNN+;
+#pragma link C++ class AliCaloRawAnalyzerLMS+;
+#pragma link C++ class AliCaloRawAnalyzerPeakFinder+;
+#pragma link C++ class AliCaloRawAnalyzerFastFit+;
+
 
 #endif
