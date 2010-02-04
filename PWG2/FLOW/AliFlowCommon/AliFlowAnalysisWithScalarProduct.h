36e3968aa1219b13f7a654cd8a722e051d93d419
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-02-04 16:55:51 +0000

    improved avarages caculation supressing multiplicty fluctuations

--- PWG2/FLOW/AliFlowCommon/AliFlowAnalysisWithScalarProduct.h
+++ PWG2/FLOW/AliFlowCommon/AliFlowAnalysisWithScalarProduct.h
@@ -19,18 +19,17 @@ class TList;
 class TFile;
 class Riostream;
 
-
+/////////////////////////////////////////////////////////////////////////////
 // Description: Maker to analyze Flow from the Scalar Product method.
 //               
-// author: N. van der Kolk (kolk@nikhef.nl)              
-
+// authors: N. van der Kolk (kolk@nikhef.nl), A. Bilandzic (anteb@nikhef.nl)              
+/////////////////////////////////////////////////////////////////////////////
  
 class AliFlowAnalysisWithScalarProduct {
 
  public:
  
    AliFlowAnalysisWithScalarProduct();            //default constructor
- 
    virtual  ~AliFlowAnalysisWithScalarProduct();  //destructor
  
    void    Init();                                          //defines variables and histograms
@@ -44,6 +43,13 @@ class AliFlowAnalysisWithScalarProduct {
    void    SetDebug(Bool_t kt)   { this->fDebug = kt ; }
    Bool_t  GetDebug() const      { return this->fDebug ; }
 
+   Double_t CalculateStatisticalError(Int_t bin, 
+				      Double_t aStatErrorQaQb,
+				      TProfile* aHistProUQ, 
+				      TProfile* aHistProUQQaQb, 
+				      TH1D** aHistSumOfWeights);
+
+
    //phi weights
    void    SetWeightsList(TList* const aWeightsList)  {this->fWeightsList = (TList*)aWeightsList->Clone();}
    TList*  GetWeightsList() const                     {return this->fWeightsList;}  
@@ -52,28 +58,61 @@ class AliFlowAnalysisWithScalarProduct {
 
    // Output 
    TList*    GetHistList() const    { return this->fHistList ; }     // Gets output histogram list
-  
-   TProfile* GetHistProUQetaRP() const {return this->fHistProUQetaRP;}   
-   void      SetHistProUQetaRP(TProfile* const aHistProUQetaRP) {this->fHistProUQetaRP = aHistProUQetaRP;}
+   //histogram getters
+   TProfile* GetHistProUQetaRP() const  {return this->fHistProUQetaRP;} 
    TProfile* GetHistProUQetaPOI() const {return this->fHistProUQetaPOI;}
-   void      SetHistProUQetaPOI(TProfile* const aHistProUQetaPOI) {this->fHistProUQetaPOI = aHistProUQetaPOI;}
-   TProfile* GetHistProUQPtRP() const {return this->fHistProUQPtRP;} 
-   void      SetHistProUQPtRP(TProfile* const aHistProUQPtRP) {this->fHistProUQPtRP = aHistProUQPtRP;}
-   TProfile* GetHistProUQPtPOI() const {return this->fHistProUQPtPOI;}
-   void      SetHistProUQPtPOI(TProfile* const aHistProUQPtPOI) {this->fHistProUQPtPOI = aHistProUQPtPOI;}
-   TProfile* GetHistProQaQb() const {return this->fHistProQaQb;}
-   void      SetHistProQaQb(TProfile* const aHistProQaQb) {this->fHistProQaQb = aHistProQaQb;}
-   TProfile* GetHistProM() const {return this->fHistProM;}
-   void      SetHistProM(TProfile* const aHistProM) {this->fHistProM = aHistProM;}
-   TH1D*     GetHistM() const {return this->fHistM;}
-   void      SetHistM(TH1D* const aHistM) {this->fHistM = aHistM;}
-
-   AliFlowCommonHist* GetCommonHists() const {return this->fCommonHists; }
-   void SetCommonHists(AliFlowCommonHist* const someCommonHists) {this->fCommonHists = someCommonHists; }
+   TProfile* GetHistProUQPtRP() const   {return this->fHistProUQPtRP;} 
+   TProfile* GetHistProUQPtPOI() const  {return this->fHistProUQPtPOI;}
+   TProfile* GetHistProQaQb() const     {return this->fHistProQaQb;}
+   TH1D*     GetHistSumOfLinearWeights() const    {return this->fHistSumOfLinearWeights;}
+   TH1D*     GetHistSumOfQuadraticWeights() const {return this->fHistSumOfQuadraticWeights;}
+   TProfile* GetHistProUQQaQbPtRP() const         {return this->fHistProUQQaQbPtRP;}   
+   TProfile* GetHistProUQQaQbEtaRP() const        {return this->fHistProUQQaQbEtaRP;}   
+   TProfile* GetHistProUQQaQbPtPOI() const        {return this->fHistProUQQaQbPtPOI;}   
+   TProfile* GetHistProUQQaQbEtaPOI() const            {return this->fHistProUQQaQbEtaPOI;}   
+   TH1D*     GetHistSumOfWeightsPtRP(Int_t i) const    {return this->fHistSumOfWeightsPtRP[i];}
+   TH1D*     GetHistSumOfWeightsEtaRP(Int_t i) const   {return this->fHistSumOfWeightsEtaRP[i];}
+   TH1D*     GetHistSumOfWeightsPtPOI(Int_t i) const   {return this->fHistSumOfWeightsPtPOI[i];}
+   TH1D*     GetHistSumOfWeightsEtaPOI(Int_t i) const  {return this->fHistSumOfWeightsEtaPOI[i];}
+   AliFlowCommonHist*        GetCommonHists() const    {return this->fCommonHists; }
    AliFlowCommonHistResults* GetCommonHistsRes() const {return this->fCommonHistsRes; }
-   void SetCommonHistsRes(AliFlowCommonHistResults* const someCommonHistsRes) {this->fCommonHistsRes = someCommonHistsRes; }
-   
 
+   //histogram setters
+   void SetHistProUQetaRP(TProfile* const aHistProUQetaRP)   
+     {this->fHistProUQetaRP = aHistProUQetaRP;}
+   void SetHistProUQetaPOI(TProfile* const aHistProUQetaPOI) 
+     {this->fHistProUQetaPOI = aHistProUQetaPOI;}
+   void SetHistProUQPtRP(TProfile* const aHistProUQPtRP)     
+     {this->fHistProUQPtRP = aHistProUQPtRP;}
+   void SetHistProUQPtPOI(TProfile* const aHistProUQPtPOI)   
+     {this->fHistProUQPtPOI = aHistProUQPtPOI;}
+   void SetHistProQaQb(TProfile* const aHistProQaQb)         
+     {this->fHistProQaQb = aHistProQaQb;}
+   void SetHistSumOfLinearWeights(TH1D* const aHistSumOfLinearWeights) 
+     {this->fHistSumOfLinearWeights = aHistSumOfLinearWeights;}
+   void SetHistSumOfQuadraticWeights(TH1D* const aHistSumOfQuadraticWeights) 
+     {this->fHistSumOfQuadraticWeights = aHistSumOfQuadraticWeights;}
+   void SetHistProUQQaQbPtRP(TProfile* const aHistProUQQaQbPtRP)     
+     {this->fHistProUQQaQbPtRP = aHistProUQQaQbPtRP;}   
+   void SetHistProUQQaQbEtaRP(TProfile* const aHistProUQQaQbEtaRP)   
+     {this->fHistProUQQaQbEtaRP = aHistProUQQaQbEtaRP;}   
+   void SetHistProUQQaQbPtPOI(TProfile* const aHistProUQQaQbPtPOI)   
+     {this->fHistProUQQaQbPtPOI = aHistProUQQaQbPtPOI;}   
+   void SetHistProUQQaQbEtaPOI(TProfile* const aHistProUQQaQbEtaPOI) 
+     {this->fHistProUQQaQbEtaPOI = aHistProUQQaQbEtaPOI;}
+   void SetHistSumOfWeightsPtRP(TH1D* const aHistSumOfWeightsPtRP, Int_t const i) 
+     {this->fHistSumOfWeightsPtRP[i] = aHistSumOfWeightsPtRP;}   
+   void SetHistSumOfWeightsEtaRP(TH1D* const aHistSumOfWeightsEtaRP, Int_t const i) 
+     {this->fHistSumOfWeightsEtaRP[i] = aHistSumOfWeightsEtaRP;}   
+   void SetHistSumOfWeightsPtPOI(TH1D* const aHistSumOfWeightsPtPOI, Int_t const i) 
+     {this->fHistSumOfWeightsPtPOI[i] = aHistSumOfWeightsPtPOI;}  
+   void SetHistSumOfWeightsEtaPOI(TH1D* const aHistSumOfWeightsEtaPOI, Int_t const i) 
+     {this->fHistSumOfWeightsEtaPOI[i] = aHistSumOfWeightsEtaPOI;}   
+   void SetCommonHists(AliFlowCommonHist* const someCommonHists)              
+     {this->fCommonHists = someCommonHists; }
+   void SetCommonHistsRes(AliFlowCommonHistResults* const someCommonHistsRes) 
+     {this->fCommonHistsRes = someCommonHistsRes; }
+   
 
  private:
    AliFlowAnalysisWithScalarProduct(const AliFlowAnalysisWithScalarProduct& anAnalysis);            //copy constructor
@@ -92,8 +131,18 @@ class AliFlowAnalysisWithScalarProduct {
    TProfile*  fHistProUQPtRP;    //uQ(pt) for RP
    TProfile*  fHistProUQPtPOI;   //uQ(pt) for POI
    TProfile*  fHistProQaQb;      //average of QaQb (for event plane resolution)
-   TProfile*  fHistProM;         //holds avarage of M-1 and Ma*Mb
-   TH1D*      fHistM;            //holds sum of M-1 and Ma*Mb
+   TH1D*      fHistSumOfLinearWeights;     //holds sum of Ma*Mb
+   TH1D*      fHistSumOfQuadraticWeights;  //holds sum of (Ma*Mb)^2
+   
+   TProfile*  fHistProUQQaQbPtRP;         //holds weighted average of <QuQaQb>
+   TProfile*  fHistProUQQaQbEtaRP;        //holds weighted average of <QuQaQb>
+   TProfile*  fHistProUQQaQbPtPOI;        //holds weighted average of <QuQaQb>
+   TProfile*  fHistProUQQaQbEtaPOI;       //holds weighted average of <QuQaQb>
+   TH1D*      fHistSumOfWeightsPtRP[3];   //holds sums of 0: Mq-1, 1: (Mq-1)^2, 2: (Mq-1)*Ma*Mb for each bin
+   TH1D*      fHistSumOfWeightsEtaRP[3];  //holds sums of 0: Mq-1, 1: (Mq-1)^2, 2: (Mq-1)*Ma*Mb for each bin
+   TH1D*      fHistSumOfWeightsPtPOI[3];  //holds sums of 0: Mq-1, 1: (Mq-1)^2, 2: (Mq-1)*Ma*Mb for each bin
+   TH1D*      fHistSumOfWeightsEtaPOI[3]; //holds sums of 0: Mq-1, 1: (Mq-1)^2, 2: (Mq-1)*Ma*Mb for each bin
+   
    AliFlowCommonHist*        fCommonHists;    //control histograms
    AliFlowCommonHistResults* fCommonHistsRes; //results histograms
 
