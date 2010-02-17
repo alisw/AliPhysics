c3ef9bd585a630199ef1aa4ebf727b4640b1b153
alla <alla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-02-17 12:22:25 +0000

    Reco Param with Latency and Vertex shift

--- T0/AliT0RecoParam.cxx
+++ T0/AliT0RecoParam.cxx
@@ -42,7 +42,10 @@ ClassImp(AliT0RecoParam)
 AliT0RecoParam::AliT0RecoParam():
   AliDetectorRecoParam(),
    fRefAmp(7),
-    fRefPoint(0)
+   fRefPoint(0),
+   fLatencyL1(0),  
+   fLatencyHPTDC(0),      
+   fVertexShift(0)   
 {
   //
   // constructor
@@ -63,8 +66,11 @@ AliT0RecoParam::~AliT0RecoParam()
 
 AliT0RecoParam::AliT0RecoParam(const AliT0RecoParam &p):
   AliDetectorRecoParam(p),       
-   fRefAmp(p.fRefAmp),
-   fRefPoint(p.fRefPoint)
+  fRefAmp(p.fRefAmp),
+  fRefPoint(p.fRefPoint),
+  fLatencyL1(p.fLatencyL1),  
+  fLatencyHPTDC(p.fLatencyHPTDC),      
+  fVertexShift(p.fVertexShift)   
 {
  
  //copy constructor
@@ -84,6 +90,9 @@ AliT0RecoParam& AliT0RecoParam:: operator=(const AliT0RecoParam &p)
   AliDetectorRecoParam::operator=(p);
   fRefAmp = p.fRefAmp;
   fRefPoint = p.fRefPoint;
+  fLatencyL1 = p.fLatencyL1;
+  fLatencyHPTDC = p.fLatencyHPTDC;
+  fVertexShift = p.fVertexShift;
   return *this;
 
 }
@@ -97,6 +106,9 @@ AliT0RecoParam *AliT0RecoParam::GetLowFluxParam()
   AliT0RecoParam *param = new AliT0RecoParam();
   param->fRefAmp = 1;
   param->fRefPoint = 0;
+  param->fLatencyL1 = 7782.01;
+  param->fLatencyHPTDC = 22000;
+  param->fVertexShift = 4.6;
   for (Int_t i=0; i<500; i++)
     {
      param-> fLow[i]=0.;
@@ -118,10 +130,13 @@ AliT0RecoParam *AliT0RecoParam::GetHighFluxParam()
   AliT0RecoParam *param = new AliT0RecoParam();
   param->fRefAmp = 5;
   param->fRefPoint = 0;
+  param->fLatencyL1 = 7782.01;
+  param->fLatencyHPTDC = 22000;
+  param->fVertexShift = 4.6;
   for (Int_t i=0; i<500; i++)
     {
-     param-> fLow[i]=0.;
-     param-> fHigh[i]=20000.;
+      param-> fLow[i]=0.;
+      param-> fHigh[i]=20000.;
     }
   //
   param->SetName("High Flux");
@@ -140,6 +155,9 @@ AliT0RecoParam *AliT0RecoParam::GetLaserTestParam()
   AliT0RecoParam *param = new AliT0RecoParam();
   param->fRefAmp = 1;
   param->fRefPoint = 1;
+  param->fLatencyL1 = 7782.01;
+  param->fLatencyHPTDC = 22000;
+  param->fVertexShift = 0;
   for (Int_t i=0; i<500; i++)
     {
      param-> fLow[i]=0.;
@@ -159,6 +177,8 @@ void AliT0RecoParam::PrintParameters() const
   //
   AliInfo(Form(" Reference amplitude for walk corerection : %f", fRefAmp));
   AliInfo(Form(" Reference point in channel  : %i", fRefPoint));
+  AliInfo(Form(" Current latency  : %i ns", fLatencyL1));
+  AliInfo(Form(" HPTDC latency  : %i ns", fLatencyHPTDC));
   cout<<" AliT0RecoParam::PrintParameters() "<<endl;
   for (Int_t i=0; i<500; i++) cout<<i<<" "<<fLow[i]<<" "<<fHigh[i]<<endl; 
 }
--- T0/AliT0RecoParam.h
+++ T0/AliT0RecoParam.h
@@ -19,8 +19,7 @@ class AliT0RecoParam : public AliDetectorRecoParam
   AliT0RecoParam(const AliT0RecoParam &p); //copy constructor
   AliT0RecoParam& operator=(const AliT0RecoParam &p);
   virtual ~AliT0RecoParam();
- 
-  
+   
  static   AliT0RecoParam *GetLowFluxParam();        // make reco parameters for low  flux env
   static   AliT0RecoParam *GetHighFluxParam();       // make reco parameters for high flux env 
   static   AliT0RecoParam *GetLaserTestParam();  // special setting for laser SetLaserTestParam 
@@ -31,7 +30,7 @@ class AliT0RecoParam : public AliDetectorRecoParam
   void    SetRefAmp(Float_t amp)   { fRefAmp = amp;}
   Int_t   GetRefPoint() const {return fRefPoint;}
   void    SetRefPoint(Int_t ref) {fRefPoint = ref;}
- 
+
   Float_t   GetLow(Int_t numhist) const {return fLow[numhist];}
   //  Float_t   GetLow() {return *fLow;}
   void      SetLow(Int_t numhist, Float_t low) {fLow[numhist] = low;}
@@ -40,17 +39,25 @@ class AliT0RecoParam : public AliDetectorRecoParam
   //  Float_t   GetHigh()  {return *fHigh;}
   void      SetHigh(Int_t numhist, Float_t high) {fHigh[numhist] = high;}
   
+  Float_t   GetLatencyL1() const {return fLatencyL1;}
+  void      SetLatencyL1(Float_t lat) {fLatencyL1 = lat;}
+  Float_t   GetLatencyHPTDC() const {return fLatencyHPTDC;}
+  void      SetLatencyHPTDC(Float_t lat) {fLatencyHPTDC = lat;}
+  Float_t   GetVertexShift() const {return fVertexShift;}
+  void      SetVertexShift(Float_t sh) {fVertexShift = sh;}
+
   void PrintParameters() const;
   
  protected:
-  Float_t fRefAmp;
-  Int_t   fRefPoint;   
-  Float_t   fLow[500];
-  Float_t   fHigh[500];
+  Float_t   fRefAmp;            // for slewing correcton
+  Int_t     fRefPoint;          // #channel for RefPoint
+  Float_t   fLow[500];          //low limit of monitoring histograms
+  Float_t   fHigh[500];         //high limit of monitoring histograms
+  Float_t   fLatencyL1;         //Latency L1
+  Float_t   fLatencyHPTDC;      //Latency HPTDC
+  Float_t   fVertexShift;       // for slewing correcton
 
-
-
-  ClassDef(AliT0RecoParam, 2);
+  ClassDef(AliT0RecoParam, 4);
 
 };
 #endif
