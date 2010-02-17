48563e9749f867024ff7de30352ff0b5bb357a1c
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-02-17 15:39:24 +0000

    Updates. (M. Gheata)

--- PWG1/PilotTrain/PilotAnalysis.C
+++ PWG1/PilotTrain/PilotAnalysis.C
@@ -1,3 +1,4 @@
+#include "Riostream.h"
 void LoadLibraries();
 void AddAnalysisTasks(); 
 class AliAnalysisAlien;                                                                                                                    
@@ -12,6 +13,17 @@ Bool_t doTPC          = 1;   // output ok
 Bool_t doEventStat    = 1;   // output ok
 Bool_t doSDD          = 1;   // outout ok needs RP
 
+TString     train_name         = "QA001_PASS4";
+TString     job_tag            = "QA001: PWG1 QA train";
+TString     root_version       = "v5-26-00b";
+TString     aliroot_version    = "v4-19-04-AN";
+TString     grid_datadir       = "/alice/data/2009/LHC09d";
+TString     data_pattern       = "*ESDs/pass4/*ESDs.root";
+//TString     alien_outdir       = "";
+TString     alien_outdir       = "/alice/data/2009/LHC09d/analysis/QA001_PASS4";
+
+Bool_t useProductionMode       = kTRUE;
+
 void PilotAnalysis(const char *plugin_mode = "full")
 {
   TGrid::Connect("alien://");
@@ -19,6 +31,30 @@ void PilotAnalysis(const char *plugin_mode = "full")
     ::Error("PilotAnalysis", "No grid connection");
     return;
   }
+  // Write configuration
+  TString cdir = gSystem->WorkingDirectory();
+  gSystem->MakeDirectory(train_name);
+  gSystem->ChangeDirectory(train_name);
+  ofstream out;
+  out.open(Form("%sConfig.C",train_name.Data()), ios::out);
+  out << "{" << endl;
+  out << "   train_name      = " << "\"" << train_name.Data() << "\";" << endl;
+  out << "   root_version    = " << "\"" << root_version.Data() << "\";" << endl;
+  out << "   aliroot_version = " << "\"" << aliroot_version.Data() << "\";" << endl;
+  out << "   grid_datadir   = " << "\"" << grid_datadir.Data() << "\";" << endl;
+  if (!alien_outdir.Length()) alien_outdir = Form("output_%s",train_name.Data());
+  out << "   alien_outdir    = " << "\"" << alien_outdir.Data() << "\";" << endl;
+  out << "   doQAsim         = " << doQAsym << ";" << endl;
+  out << "   doVZERO         = " << doVZERO << ";" << endl;
+  out << "   doVertex        = " << doVertex << ";" << endl;
+  out << "   doSPD           = " << doSPD << ";" << endl;
+  out << "   doSDD           = " << doSDD << ";" << endl;
+  out << "   doFMD           = " << doFMD << ";" << endl;
+  out << "   doTPC           = " << doTPC << ";" << endl;
+  out << "   doEventStat     = " << doEventStat << ";" << endl;
+  out << "}" << endl;
+  out.close();
+  
   // Load libraries
   gSystem->SetIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT/include -I$ALICE_ROOT/ITS");
   LoadLibraries();
@@ -31,15 +67,24 @@ void PilotAnalysis(const char *plugin_mode = "full")
   mgr->SetInputEventHandler(esdHandler);
 
   mgr->SetDebugLevel(3);
-
+  mgr->SetSaveCanvases(kTRUE);
+  
   // AnalysisTasks
   AddAnalysisTasks();
   // Grid handler
-  AliAnalysisGrid *alienHandler = CreateAlienHandler(plugin_mode);
+  AliAnalysisAlien *alienHandler = CreateAlienHandler(plugin_mode);
   mgr->SetGridHandler(alienHandler);
   if (mgr->InitAnalysis()) {                                                                                                              
     mgr->PrintStatus(); 
     mgr->StartAnalysis("grid");
+    TString alien_workdir = gGrid->GetHomeDirectory();
+    alien_workdir += "analysisDATA";
+    TString configName = Form("%s/%sConfig.C", alien_workdir.Data(), train_name.Data());
+    if (strcmp(plugin_mode, "test")) {
+      printf("=== Registering configuration file <%s>===\n", configName.Data());
+      if (AliAnalysisAlien::FileExists(configName.Data())) gGrid->Rm(configName.Data());                                                     
+      TFile::Cp(Form("file:%sConfig.C",train_name.Data()), Form("alien://%s", configName.Data()));  
+    }  
   }
 }
 
@@ -56,14 +101,14 @@ void LoadLibraries()
   gSystem->Load("libPWG2forward.so");
 
   if (doSPD) {   
-    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/PilotTrain/AliAnalysisTaskSPD.cxx"), "AliAnalysisTaskSPD.cxx");
-    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/PilotTrain/AliAnalysisTaskSPD.h"), "AliAnalysisTaskSPD.h");
+    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/ITS/AliAnalysisTaskSPD.cxx"), "AliAnalysisTaskSPD.cxx");
+    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/ITS/AliAnalysisTaskSPD.h"), "AliAnalysisTaskSPD.h");
     gROOT->LoadMacro("AliAnalysisTaskSPD.cxx++g");
   }
   if (doSDD) {  
-    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/PilotTrain/AliAnalysisTaskSDDRP.cxx"), "AliAnalysisTaskSDDRP.cxx");
-    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/PilotTrain/AliAnalysisTaskSDDRP.h"), "AliAnalysisTaskSDDRP.h");
-    gROOT->LoadMacro("AliAnalysisTaskSDDRP.cxx++g");
+    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/ITS/AliAnalysisTaskSDDRP.cxx"), "AliAnalysisTaskSDDRP.cxx");
+    TFile::Cp(gSystem->ExpandPathName("$ALICE_ROOT/PWG1/ITS/AliAnalysisTaskSDDRP.h"), "AliAnalysisTaskSDDRP.h");
+//    gROOT->LoadMacro("AliAnalysisTaskSDDRP.cxx++g");
   }  
 }
 
@@ -131,6 +176,7 @@ void AddAnalysisTasks()
       AliPhysicsSelectionTask* physSelTask = AddTaskPhysicsSelection();
       AliPhysicsSelection* physSel = physSelTask->GetPhysicsSelection();
       physSel->AddBackgroundIdentification(new AliBackgroundSelection());
+      AliAnalysisManager::GetAnalysisManager()->RegisterExtraFile("event_stat.root");
   }    
 }
 
@@ -144,20 +190,19 @@ AliAnalysisAlien* CreateAlienHandler(const char *plugin_mode)
    AliAnalysisAlien *plugin = new AliAnalysisAlien();
 // Set the run mode (can be "full", "test", "offline", "submit" or "terminate")
    plugin->SetRunMode(plugin_mode);
-//   if (useProductionMode) plugin->SetProductionMode();
-   plugin->SetJobTag("Pilot analysis train");
+   if (useProductionMode) plugin->SetProductionMode();
+   plugin->SetJobTag(job_tag);
    plugin->SetNtestFiles(1);
 // Set versions of used packages
-//   plugin->SetAPIVersion("V2.4");
    plugin->SetAPIVersion("V1.1x");
-   plugin->SetROOTVersion("v5-25-04-3");
-   plugin->SetAliROOTVersion("v4-18-14-AN-1");
+   plugin->SetROOTVersion(root_version);
+   plugin->SetAliROOTVersion(aliroot_version);
 // Declare input data to be processed.
 // Method 1: Create automatically XML collections using alien 'find' command.
 // Define production directory LFN
-   plugin->SetGridDataDir("/alice/data/2009/LHC09d");
+   plugin->SetGridDataDir(grid_datadir);
 // Set data search pattern
-   plugin->SetDataPattern("*ESD.tag.root");
+   plugin->SetDataPattern(data_pattern);
 // ...then add run numbers to be considered
 //   if (!iAODanalysis) plugin->SetRunRange(run_range[0], run_range[1]);
    plugin->SetRunPrefix("000");
@@ -172,7 +217,8 @@ AliAnalysisAlien* CreateAlienHandler(const char *plugin_mode)
 // Define alien work directory where all files will be copied. Relative to alien $HOME.
    plugin->SetGridWorkingDir("analysisDATA");
 // Declare alien output directory. Relative to working directory.
-   plugin->SetGridOutputDir("pilotAnalysis2");
+   if (alien_outdir.IsNull()) alien_outdir = Form("output_%s",train_name.Data());
+   plugin->SetGridOutputDir(alien_outdir);
 
 //   plugin->EnablePackage("");
 
@@ -213,30 +259,32 @@ AliAnalysisAlien* CreateAlienHandler(const char *plugin_mode)
       }   
    }
    if (mgr->GetExtraFiles().Length()) {
-      if (listaods.Length()) listaods += ",";
-      listaods += mgr->GetExtraFiles();
-      listaods.ReplaceAll(" ", ",");
+      if (listhists.Length()) listhists += ",";
+      listhists += mgr->GetExtraFiles();
+      listhists.ReplaceAll(" ", ",");
    }
    if (listhists.Length()) listhists = Form("hist_archive.zip:%s", listhists.Data());
    if (listaods.Length())  listaods  = Form("aod_archive.zip:%s", listaods.Data());
    if (!listhists.Length()) {
       ::Fatal("AnalysisTrainNew", "No task output !");
    }
-   TString outputArchive = "log_archive.zip:stdout,stderr";
+   TString outputArchive = "log_archive.zip:stdout,stderr@disk=4";
    if (listaods.Length()) {
       outputArchive += " ";
       outputArchive += listaods;
+      outputArchive += "@disk=4";
    }   
    if (listhists.Length()) {
       outputArchive += " ";
       outputArchive += listhists;
+      outputArchive += "@disk=4";
    }   
 // Set friends
 //   if (iAODanalysis && iPWG3d2h) 
 //      plugin->SetFriendChainName("AliAOD.VertexingHF.root");
    plugin->SetOutputArchive(outputArchive);
 // Optionally set a name for the generated analysis macro (default MyAnalysis.C)
-   plugin->SetAnalysisMacro("pilotAnalysis002.C");
+   plugin->SetAnalysisMacro(Form("%s.C", train_name.Data()));
 // Optionally set maximum number of input files/subjob (default 100, put 0 to ignore)
    plugin->SetSplitMaxInputFileNumber(1000);
 // Optionally set number of failed jobs that will trigger killing waiting sub-jobs.
@@ -248,9 +296,9 @@ AliAnalysisAlien* CreateAlienHandler(const char *plugin_mode)
 // Optionally set input format (default xml-single)
    plugin->SetInputFormat("xml-single");
 // Optionally modify the name of the generated JDL (default analysis.jdl)
-   plugin->SetJDLName("pilotAnalysis002.jdl");
+   plugin->SetJDLName(Form("%s.jdl", train_name.Data()));
 // Optionally modify the executable name (default analysis.sh)
-   plugin->SetExecutable("pilotAnalysis002.sh");
+   plugin->SetExecutable(Form("%s.sh", train_name.Data()));
 // Optionally modify job price (default 1)
    plugin->SetPrice(1);      
 // Optionally modify split mode (default 'se')    
