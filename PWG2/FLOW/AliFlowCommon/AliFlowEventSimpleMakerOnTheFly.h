5657f20ad85affd32f32495bc310df007b8a2144
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-02-23 10:37:43 +0000

    additions to the toy model to test the methods with eta dependence and an extra check before sqrt()

--- PWG2/FLOW/AliFlowCommon/AliFlowEventSimpleMakerOnTheFly.h
+++ PWG2/FLOW/AliFlowCommon/AliFlowEventSimpleMakerOnTheFly.h
@@ -57,8 +57,11 @@ class AliFlowEventSimpleMakerOnTheFly {
   void SetTemperatureOfRP(Double_t temperatureRP) {this->fTemperatureOfRP = temperatureRP;}
   Double_t GetTemperatureOfRP() const {return this->fTemperatureOfRP;} 
   
-  void SetUseConstantHarmonics(Bool_t const uch) {this->fUseConstantHarmonics = uch;};
-  Bool_t GetUseConstantHarmonics() const {return this->fUseConstantHarmonics;};
+  void SetPtDependentHarmonics(Bool_t const pdh) {this->fPtDependentHarmonics = pdh;};
+  Bool_t GetPtDependentHarmonics() const {return this->fPtDependentHarmonics;};
+  
+  void SetEtaDependentHarmonics(Bool_t const edh) {this->fEtaDependentHarmonics = edh;};
+  Bool_t GetEtaDependentHarmonics() const {return this->fEtaDependentHarmonics;};
   
   // constant harmonics:  
   void SetV1RP(Double_t dV1RP) {this->fV1RP = dV1RP;}
@@ -67,8 +70,8 @@ class AliFlowEventSimpleMakerOnTheFly {
   void SetV1SpreadRP(Double_t dV1SpreadRP) {this->fV1SpreadRP = dV1SpreadRP;}
   Double_t GetV1SpreadRP() const {return this->fV1SpreadRP;} 
   
-  void SetV2DistrOfRPsIsGauss(Bool_t const v2dorig) {this->fV2DistrOfRPsIsGauss = v2dorig;};
-  Bool_t GetV2DistrOfRPsIsGauss() const {return this->fV2DistrOfRPsIsGauss;};
+  void SetConstantV2IsSampledFromGauss(Bool_t const cV2isfg) {this->fConstantV2IsSampledFromGauss = cV2isfg;};
+  Bool_t GetConstantV2IsSampledFromGauss() const {return this->fConstantV2IsSampledFromGauss;};
   
   void SetV2RP(Double_t dV2RP) {this->fV2RP = dV2RP;}
   Double_t GetV2RP() const {return this->fV2RP;} 
@@ -89,11 +92,17 @@ class AliFlowEventSimpleMakerOnTheFly {
   Double_t GetV4SpreadRP() const {return this->fV4SpreadRP;} 
   
   // (pt,eta) dependent harmonics:
-  void SetV2RPMax(Double_t dV2RPMax) {this->fV2RPMax = dV2RPMax;}
-  Double_t GetV2RPMax() const {return this->fV2RPMax;} 
+  void SetV2RPMax(Double_t dV2RPMax) {this->fV2vsPtEtaMax = dV2RPMax;}
+  Double_t GetV2RPMax() const {return this->fV2vsPtEtaMax;} 
+  
+  void SetV2vsPtEtaMax(Double_t dV2vsPtEtaMax) {this->fV2vsPtEtaMax = dV2vsPtEtaMax;}
+  Double_t GetV2vsPtEtaMax() const {return this->fV2vsPtEtaMax;} 
   
   void SetPtCutOff(Double_t dPtCutOff) {this->fPtCutOff = dPtCutOff;}
   Double_t GetPtCutOff() const {return this->fPtCutOff;} 
+  
+  void SetV2vsEtaSpread(Double_t dV2vsEtaSpread) {this->fV2vsEtaSpread = dV2vsEtaSpread;}
+  Double_t GetV2vsEtaSpread() const {return this->fV2vsEtaSpread;} 
   
   void SetFirstSectorPhiMin(Double_t dPhiMin1) {this->fPhiMin1 = dPhiMin1;}
   Double_t GetFirstSectorPhiMin() const {return this->fPhiMin1;} 
@@ -116,6 +125,8 @@ class AliFlowEventSimpleMakerOnTheFly {
   
   void SetNoOfLoops(Int_t noofl) {this->fNoOfLoops = noofl;}
   Int_t GetNoOfLoops() const {return this->fNoOfLoops;} 
+  void SetPhiRange(Double_t phr) {this->fPhiRange = phr;}
+  Double_t GetPhiRange() const {return this->fPhiRange;}   
   void SetPtRange(Double_t pr) {this->fPtRange = pr;}
   Double_t GetPtRange() const {return this->fPtRange;}   
   void SetEtaRange(Double_t er) {this->fEtaRange = er;}
@@ -139,15 +150,16 @@ class AliFlowEventSimpleMakerOnTheFly {
   Int_t     fMinMultOfRP;            // minimal multiplicity of RPs (if sampled uniformly)
   Int_t     fMaxMultOfRP;            // maximum multiplicity of RPs (if sampled uniformly)
   Double_t  fTemperatureOfRP;        // "temperature" of RPs in GeV/c (increase this parameter to get more high pt RPs) 
-  Bool_t    fUseConstantHarmonics;   // harmonics V1, V2, V4... are constant (kTRUE) or functions of pt and eta (kFALSE)     
+  Bool_t    fPtDependentHarmonics;   // harmonic V2 is a function of pt     
+  Bool_t    fEtaDependentHarmonics;  // harmonic V2 is a function of eta     
   // constant harmonics: 
   Double_t  fV1RP;                   // directed flow of RPs
   Double_t  fV1SpreadRP;             // directed flow spread of RPs
   
-  Bool_t    fV2DistrOfRPsIsGauss;    // 1.) if kTRUE  = elliptic flow of RPs is sampled e-b-e from Gaussian distribution with
-                                     //                 mean = fV2RP and spread = fV2SpreadRP
-                                     // 2.) if kFALSE = elliptic flow of RPs is sampled e-b-e uniformly from 
-                                     //                 interval [fMinV2RP,fMaxV2RP]
+  Bool_t    fConstantV2IsSampledFromGauss; // 1.) if kTRUE  = elliptic flow of RPs is sampled e-b-e from Gaussian distribution with
+                                           //                 mean = fV2RP and spread = fV2SpreadRP
+                                           // 2.) if kFALSE = elliptic flow of RPs is sampled e-b-e uniformly from 
+                                           //                 interval [fMinV2RP,fMaxV2RP]
   Double_t  fV2RP;                   // mean elliptic flow of RPs (if sampled from Gaussian)
   Double_t  fV2SpreadRP;             // elliptic flow spread of RPs (if sampled from Gaussian)
   Double_t  fMinV2RP;                // minimal elliptic flow of RPs (if sampled uniformly)
@@ -156,8 +168,9 @@ class AliFlowEventSimpleMakerOnTheFly {
   Double_t  fV4RP;                   // harmonic V4 of RPs
   Double_t  fV4SpreadRP;             // harmonic V4's spread of RPs
   // (pt,eta) dependent harmonics:
-  Double_t  fV2RPMax;                // elliptic flow of RPs
-  Double_t  fPtCutOff;               // elliptic flow spread of RPs
+  Double_t  fV2vsPtEtaMax;           // max value of (pt,eta) dependent V2
+  Double_t  fPtCutOff;               // V2(pt) is linear up to pt = dPtCutOff and for pt > dPtCutOff it is constant, V2(pt) = dV2vsPtEtaMax:
+  Double_t  fV2vsEtaSpread;          // V2(eta) is Gaussian centered at midrapidity (eta=0) with spread = dV2vsEtaSpread
   // non-uniform acceptance:
   Double_t  fPhiMin1;                // first non-uniform sector starts at azimuth fPhiMin1
   Double_t  fPhiMax1;                // first non-uniform sector ends at azimuth fPhiMax1
@@ -176,8 +189,9 @@ class AliFlowEventSimpleMakerOnTheFly {
   TRandom3* fMyTRandom3; // our TRandom3 generator
   Int_t     fCount;      // count number of events 
   Int_t     fNoOfLoops;  // number of times to use the same particle (nonflow)
-  Double_t  fPtRange;    // splitted track pt range for uniform sampling
-  Double_t  fEtaRange;   // splitted track eta range for uniform sampling
+  Double_t  fPhiRange;   // splitted track phi range (+/- from original track's phi) for uniform sampling
+  Double_t  fPtRange;    // splitted track pt range (+/- from original track's pt) for uniform sampling
+  Double_t  fEtaRange;   // splitted track eta range (+/- from original track's eta) for uniform sampling
   Double_t  fEtaMinA;    // minimum eta of subevent A eta range
   Double_t  fEtaMaxA;    // maximum eta of subevent A eta range
   Double_t  fEtaMinB;    // minimum eta of subevent B eta range
