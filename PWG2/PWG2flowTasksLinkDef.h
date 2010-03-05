3a38c760dfe6e0696f55b8d084503857c7d43401
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-03-05 18:44:03 +0000

    task matching the new classes

--- PWG2/PWG2flowTasksLinkDef.h
+++ PWG2/PWG2flowTasksLinkDef.h
@@ -15,5 +15,7 @@
 #pragma link C++ class AliAnalysisTaskFittingQDistribution+;
 #pragma link C++ class AliAnalysisTaskFlowEvent+;
 #pragma link C++ class AliAnalysisTaskFlowEventforRP+;
+#pragma link C++ class AliAnalysisTaskMixedHarmonics+;
+#pragma link C++ class AliAnalysisTaskNestedLoops+;
 
 #endif
