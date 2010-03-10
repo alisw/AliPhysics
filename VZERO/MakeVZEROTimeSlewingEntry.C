04c51f0a1d95261bf923075bca8492073e7f98d7
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-03-10 14:58:40 +0000

    Adding OCDB entry for time slewing + a macro to create it.

---
+++ VZERO/MakeVZEROTimeSlewingEntry.C
@@ -0,0 +1,29 @@
+void MakeVZEROTimeSlewingEntry()
+{
+
+  AliCDBManager *man = AliCDBManager::Instance();
+  man->SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+
+  // Creation of the time slewing OCDB object
+  TF1 *slew = new TF1("TimeSlewing","[0]*TMath::Power(x,[1])",1,1024);
+  slew->SetParameter(0,1.57345e1);
+  slew->SetParameter(1,-4.25603e-1);
+	
+  TObjString str("VZERO Time-slewing correction");
+
+  AliCDBMetaData *md= new AliCDBMetaData(); // metaData describing the object
+  md->SetResponsible("Brigitte Cheynis");
+  md->SetBeamPeriod(0);
+  md->SetAliRootVersion(gSystem->Getenv("ARVERSION"));
+  md->SetComment("Time-slewing correction used in reconstruction and MC simulation");
+  md->PrintMetaData();
+
+  AliCDBStorage *storLoc = man->GetDefaultStorage();
+  AliCDBId id("VZERO/Calib/TimeSlewing",0,AliCDBRunRange::Infinity());
+
+  storLoc->Put(slew, id, md);
+
+  storLoc->Delete();
+  delete md;
+
+}
