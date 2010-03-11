b90dbfcdf57e5819eaafe4e37e82d19f002aede4
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-03-11 15:30:04 +0000

    Moving the OCDBscan scripts to the subdirectory Alien implementation not yet done. For the moment local and batch farm implementation.
    (Marian Ivanov)

---
+++ TPC/scripts/OCDBscan/makeCalibTree.sh
@@ -0,0 +1,27 @@
+# Aruments
+# 1   -  run list
+# 2   -  start run
+# 3   -  end run
+
+runList=$1
+startRun=$2
+endRun=$3
+echo runList=$runList
+echo startRun=$startRun
+echo endRun=$endRun
+#
+workdir=${GUI_OUTDIR}/tmp/tmp${startRun}-${endRun}
+backupdir=`pwd`/
+mkdirhier $workdir
+cd $workdir
+source guiEnv.sh
+source halloWorld.sh
+#
+aliroot -q -b $SCRIPTDIR/ConfigOCDB.C  $ALICE_ROOT/TPC/CalibMacros/CalibEnv.C+\(\"$runList\",$startRun,$endRun\)
+echo End of job:
+echo pwd=`pwd`
+echo ls=
+ls -alrt
+echo cp dcsTime.root $GUI_OUTDIR/time/calibTreeTime_$startRun_$endRun.root
+cp dcsTime.root $GUI_OUTDIR/time/calibTreeTime_$startRun_$endRun.root
+cd $backupdir
