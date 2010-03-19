148b1163607f35463b12037383df7033487b8b0f
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-03-19 14:56:35 +0000

    fixing coding violations from FC

--- PWG2/FORWARD/analysis/AliFMDDndeta.h
+++ PWG2/FORWARD/analysis/AliFMDDndeta.h
@@ -4,6 +4,12 @@
 #include "TObject.h"
 #include "TList.h"
 #include "TString.h"
+class TH1F;
+
+//
+// This class creates dN/deta for the FMD from the analysis objects.
+// The contents of this class should probably go into a task at some point
+//
 
 class AliFMDDndeta : public TObject
 {
@@ -15,7 +21,8 @@ class AliFMDDndeta : public TObject
     fList(0),
     fMultList(),
     fNbinsToCut(o.fNbinsToCut),
-    fVtxCut(o.fVtxCut),
+    fVtxCut1(o.fVtxCut1),
+    fVtxCut2(o.fVtxCut2),
     fIsInit(o.fIsInit),
     fIsGenerated(),
     fPrimEvents(o.fPrimEvents),
@@ -32,26 +39,31 @@ class AliFMDDndeta : public TObject
   
   enum Analysis {kHits, kHitsTrVtx, kMult, kMultTrVtx};
   
-  void Init(const Char_t* filename); 
+  void Init(const Char_t* filename);
+  void Init(TList* list);
   void GenerateMult(Analysis what);
-  void DrawDndeta(Analysis what, Int_t rebin = 1);
+  void DrawDndeta(Analysis what, Int_t rebin = 1, Bool_t realdata = kFALSE);
   void SetNbinsToCut(Int_t nbins) {fNbinsToCut = nbins;}
-  void SetVtxCut(Int_t vtxcut) {fVtxCut = vtxcut;}
+  void SetVtxCut1(Int_t vtxcut) {fVtxCut1 = vtxcut;}
+  void SetVtxCut2(Int_t vtxcut) {fVtxCut2 = vtxcut;}
   void CreateSharingEfficiency(const Char_t* filename, Bool_t store = kFALSE);
+  TList* GetMultList() {return &fMultList;}
  private:
-  void GenerateHits();
+  void GenerateHits(Analysis what);
   void SetNames(Analysis what);
   const char* GetAnalysisName(Analysis what, UShort_t det, Char_t ring, Int_t vtxbin);
   const char* GetPrimName(Analysis what, UShort_t det, Char_t ring, Int_t vtxbin);
-  TList* fList;
-  TList  fMultList;
-  Int_t  fNbinsToCut;
-  Int_t  fVtxCut;
-  Bool_t fIsInit;
-  Bool_t fIsGenerated[3];
-  TString fPrimEvents;
-  TString fEvents;
-  TString fPrimdNdeta;
+  void   RebinHistogram(TH1F* hist, Int_t rebin);
+  TList* fList;                          // A list of input histograms
+  TList  fMultList;                      // A list of mult histograms 
+  Int_t  fNbinsToCut;                    // The number of bins to cut
+  Int_t  fVtxCut1;                       // Vtx low
+  Int_t  fVtxCut2;                       // Vtx high
+  Bool_t fIsInit;                        // Are we init ? 
+  Bool_t fIsGenerated[4];                // Have we generated ?
+  TString fPrimEvents;                   // Number of prim events
+  TString fEvents;                       // Number of events
+  TString fPrimdNdeta;                   // the primary dNdeta from MC
   
   ClassDef(AliFMDDndeta,2);
 };
