5f167f0b7298b863c71e71d4c4ba3281b5b1a1e1
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-03-25 13:02:42 +0000

    Technical fix: possibility to use partial EMCAL geometry

--- EVE/alice-macros/emcal_digits.C
+++ EVE/alice-macros/emcal_digits.C
@@ -29,42 +29,43 @@
 
 void emcal_digits()
 {
-  AliRunLoader* rl =  AliEveEventManager::AssertRunLoader();
-
-  rl->LoadDigits("EMCAL");
-  TTree* dt = rl->GetTreeD("EMCAL", kFALSE);
-  if (!dt) return;
-
   AliEveEventManager::AssertGeometry();
 
   TGeoNode* node = gGeoManager->GetTopVolume()->FindNode("XEN1_1");
+  if (!node) return;
 
-  TGeoBBox* bbbox = (TGeoBBox*) node->GetDaughter(0) ->GetVolume()->GetShape();
-  TGeoBBox* sbbox = (TGeoBBox*) node->GetDaughter(10)->GetVolume()->GetShape();
+  Int_t nModules = node->GetNdaughters();
 
   TEveElementList* l = new TEveElementList("EMCAL");
   l->SetTitle("Tooltip");
   gEve->AddElement(l);
 
+  TGeoBBox* bbbox = (TGeoBBox*) node->GetDaughter(0) ->GetVolume()->GetShape();
   TEveFrameBox* frame_big = new TEveFrameBox();
   frame_big->SetFrameColorRGBA(200,200,0,50);
   frame_big->SetAABoxCenterHalfSize(0, 0, 0, bbbox->GetDX(), bbbox->GetDY(), bbbox->GetDZ());
 
-  TEveFrameBox* frame_sml = new TEveFrameBox();
-  frame_sml->SetFrameColorRGBA(200,200,0,50);
-  frame_sml->SetAABoxCenterHalfSize(0, 0, 0, sbbox->GetDX(), sbbox->GetDY(), sbbox->GetDZ());
+  TEveFrameBox* frame_sml = 0x0;
+
+  if (nModules==12) {
+    TGeoBBox* sbbox = (TGeoBBox*) node->GetDaughter(10)->GetVolume()->GetShape();
+    frame_sml = new TEveFrameBox();
+    frame_sml->SetFrameColorRGBA(200,200,0,50);
+    frame_sml->SetAABoxCenterHalfSize(0, 0, 0, sbbox->GetDX(), sbbox->GetDY(), sbbox->GetDZ());
+  }
 
   gStyle->SetPalette(1, 0);
   TEveRGBAPalette* pal = new TEveRGBAPalette(0, 512);
   pal->SetLimits(0, 1024);
 
   TEveQuadSet* smodules[12];
+  memset(smodules,0,12*sizeof(TEveQuadSet*));
 
 
   AliEMCALGeometry * geom  = AliEMCALGeometry::GetInstance();  
   if (!geom) geom = AliEMCALGeometry::GetInstance("","");
 
-  for (Int_t sm=0; sm<12; ++sm)
+  for (Int_t sm=0; sm<nModules; ++sm)
   {
     TEveQuadSet* q = new TEveQuadSet(Form("SM %d", sm+1));
     q->SetOwnIds(kTRUE);
@@ -80,6 +81,12 @@ void emcal_digits()
     gEve->AddElement(q, l);
     smodules[sm] = q;
   }
+
+  AliRunLoader* rl =  AliEveEventManager::AssertRunLoader();
+
+  rl->LoadDigits("EMCAL");
+  TTree* dt = rl->GetTreeD("EMCAL", kFALSE);
+  if (!dt) return;
 
   TClonesArray *digits = 0;
   dt->SetBranchAddress("EMCAL", &digits);
@@ -124,9 +131,11 @@ void emcal_digits()
       cout << x <<" "<< y <<" "<< z <<endl;
 
       TEveQuadSet* q = smodules[iSupMod];
-      q->AddQuad(y, z);
-      q->QuadValue(TMath::Nint(amp));
-      q->QuadId(new AliEMCALDigit(*dig));
+      if (q) {
+	q->AddQuad(y, z);
+	q->QuadValue(TMath::Nint(amp));
+	q->QuadId(new AliEMCALDigit(*dig));
+      }
     } else {
       cout<<"Digit pointer 0x0"<<endl;
     }
@@ -135,7 +144,7 @@ void emcal_digits()
   rl->UnloadDigits("EMCAL");
 
 
-  for (Int_t sm = 0; sm < 12; ++sm)
+  for (Int_t sm = 0; sm < nModules; ++sm)
   {
     smodules[iSupMod]->RefitPlex();
   }
