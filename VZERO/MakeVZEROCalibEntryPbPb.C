df77afb31f2a7e1c36b6599dbd73013b9396024d
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-04-01 19:12:35 +0000

    Corrected HV values for PbPb. MIP at 2 ADC.

--- VZERO/MakeVZEROCalibEntryPbPb.C
+++ VZERO/MakeVZEROCalibEntryPbPb.C
@@ -10,7 +10,15 @@ void MakeVZEROCalibEntryPbPb(Int_t run,const char *inputCDB = "raw://"){
   AliVZEROCalibData *calibdaorg = (AliVZEROCalibData*)entry->GetObject();
   AliVZEROCalibData *calibda = new AliVZEROCalibData(*calibdaorg);
 
-  const Float_t scale = 70.;
+  Float_t p0[64] = {
+    7.094891, 7.124938, 7.089708, 7.098169, 7.094482, 7.147250, 7.170978, 7.183392, 
+    7.145760, 7.148096, 7.153840, 7.143544, 7.186069, 7.194580, 7.203516, 7.195176, 
+    7.188333, 7.198607, 7.209412, 7.226565, 7.221695, 7.205132, 7.191238, 7.227724, 
+    7.232810, 7.252655, 7.230309, 7.140891, 7.273518, 7.242969, 7.252859, 7.252655, 
+    7.026802, 7.079913, 7.134147, 7.092387, 7.079561, 7.072848, 7.123192, 7.003141, 
+    7.024667, 7.124784, 7.123442, 7.129744, 7.110671, 7.143031, 7.139439, 7.178109, 
+    7.247803, 7.139396, 7.293809, 7.094454, 6.992198, 7.206448, 7.244765, 7.056197, 
+    7.263595, 7.138569, 7.089582, 7.215683, 7.266183, 7.165123, 7.243276, 7.235135 };
   Float_t p1[64] = {
     0.135569, 0.146405, 0.142425, 0.144278, 0.142307, 0.141648, 0.128477, 0.138239, 
     0.144173, 0.143419, 0.143572, 0.144482, 0.138024, 0.136542, 0.135955, 0.138537, 
@@ -21,10 +29,10 @@ void MakeVZEROCalibEntryPbPb(Int_t run,const char *inputCDB = "raw://"){
     0.132394, 0.142849, 0.140477, 0.144592, 0.141558, 0.157646, 0.143758, 0.173385, 
     0.146489, 0.143279, 0.145230, 0.147203, 0.147333, 0.144979, 0.148597, 0.138985 };
 
+  Float_t mipperadc = 0.5;
   for (Int_t i = 0; i < 64; ++i) {
-    Float_t hvorg = calibdaorg->GetMeanHV(i);
-    Float_t hvnew = hvorg/(TMath::Power(scale,p1[i]));
-    calibda->SetMeanHV(hvnew,i);
+    Float_t hv = TMath::Exp(p1[i]*TMath::Log(0.6/mipperadc)+p0[i]);
+    calibda->SetMeanHV(hv,i);
   }
 
   // Creation of the object VZERO Calibration as a MetaData
