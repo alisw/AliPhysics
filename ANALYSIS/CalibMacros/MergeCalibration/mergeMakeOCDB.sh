340090dad97c5cc352d168be7c783fcc71ef4160
zampolli <zampolli@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-04-08 16:23:12 +0000

    Updating jdls (arguments correctly passed and used), and script to perform merging+OCDB filling.

--- ANALYSIS/CalibMacros/MergeCalibration/mergeMakeOCDB.sh
+++ ANALYSIS/CalibMacros/MergeCalibration/mergeMakeOCDB.sh
@@ -3,26 +3,22 @@
 # Script to merge objects coming out of the calibration train:
 # Arguments:
 #    1 - directory on which to look for the files to be merged 
-#    2 - OCDB output path
+#    2 - run number
+#    3 - OCDB output path
+
 # example:
-# mergeMakeOCDB.sh /alice/cern.ch/user/z/zampolli/CalibTrain/output/
+# mergeMakeOCDB.sh /alice/cern.ch/user/z/zampolli/CalibTrain/output/ 000114798 alien://folder=/alice/cern.ch/user/z/zampolli/MergeCalib/OCDB
 
 #ALIEN setting
 # $1 = directory where to perform the find 
-#runnum=$1
-#ocdbStorage=$2
-#echo Directory to look into = $1
-
-echo Run path $1
-echo OCDB output path $2
-runNum=`basename $1`
-echo RunNumber $runNum
+# $2 = run number
+# $3 = OCDB path
 
 echo ">>>>>>> Running AliRoot to merge calib objects found in $1"
 aliroot -l -b -q merge.C\(\"$1\"\) 2>&1 | tee merge.log
 
-echo ">>>>>>> Extract OCDB entries"
-aliroot -l -b -q makeOCDB.C\(\"$runNum\",\"$2\"\) 2>&1 | tee ocdb.log
+echo ">>>>>>> Extract OCDB entries for run = $2, to be stored in $3"
+aliroot -l -b -q makeOCDB.C\(\"$2\",\"$3\"\) 2>&1 | tee ocdb.log
 
 
 
