336d17d7520b3e992c3d39418e8e8c31c0d28063
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-04-12 13:06:49 +0000

    acorde_raw.C --> update of the macro used to visualize acorde data from raw data file. acorde_esd.C --> new macro created to visualize the acorde data from AliESDs file.
    Mario

--- EVE/alice-macros/acorde_esd.C
+++ EVE/alice-macros/acorde_esd.C
@@ -1,5 +1,3 @@
-// $Id$
-// Main authors: Matevz Tadel & Alja Mrak-Tadel: 2006, 2007
 
 /**************************************************************************
  * Copyright(c) 1998-2008, ALICE Experiment at CERN, all rights reserved. *
@@ -9,40 +7,29 @@
 
 TString acorde_module_path(Int_t module);
 
-Color_t g_acorde_raw_color_on  = kRed;
-Color_t g_acorde_raw_color_off = kBlue;
+Color_t g_acorde_esd_color_on  = kRed;
+Color_t g_acorde_esd_color_off = kBlue;
 
-UChar_t g_acorde_raw_transp_on  = 30;
-UChar_t g_acorde_raw_transp_off = 60;
+UChar_t g_acorde_esd_transp_on  = 30;
+UChar_t g_acorde_esd_transp_off = 60;
 
-void acorde_raw()
+void acorde_esd()
 {
 
-  // From Mario RC
+  // From Mario RC:
 
   AliEveEventManager::AssertGeometry();
+  AliESDEvent* fESD = new AliESDEvent();
+  AliESDACORDE *acordeESD = AliEveEventManager::AssertESD()->GetACORDEData();
 
-  AliRawReader       * reader = AliEveEventManager::AssertRawReader();
-  AliACORDERawStream * stream = new AliACORDERawStream(reader);
-
-  stream->Reset();
-  stream->Next();
-
-  UInt_t dy[4];
-  dy[0] = stream->GetWord(0);
-  dy[1] = stream->GetWord(1);
-  dy[2] = stream->GetWord(2);
-  dy[3] = stream->GetWord(3);
-
-  printf ("ACORDE event 0x%08x 0x%08x 0x%08x 0x%08x\n", dy[0], dy[1], dy[2], dy[3]);
 
   if (acorde_module_path(0).IsNull())
   {
-    Warning("acorde_raw", "Missing / wrong ACORDE module geometry.");
+    Warning("acorde_esd", "Missing / wrong ACORDE module geometry.");
     return;
   }
 
-  TEveElementList* acorde = new TEveElementList("ACORDE_Raw");
+  TEveElementList* acorde = new TEveElementList("ACORDE_Esd");
 
   gEve->AddElement(acorde);
 
@@ -52,20 +39,17 @@ void acorde_raw()
 
     if ( ! gGeoManager->cd(path))
     {
-      Warning("acorde_raw", "Module id=%d, path='%s' not found.", module, path.Data());
+      Warning("acorde_esd", "Module id=%d, path='%s' not found.", module, path.Data());
       continue;
     }
 
+    Bool_t val = kFALSE;
+    if (acordeESD->GetHitChannel(module)) val=kTRUE;  
     // From Matevz:
-    // Here check state and assign color, I do it partially for now.
-    Int_t  word_idx = module / 30;
-    Int_t  bit_idx  = module % 30;
-    Bool_t val      = (dy[word_idx] & (1 << bit_idx)) != 0; 
-
     TEveGeoShape* eg_shape = new TEveGeoShape(TString::Format("Module %d", module),
                                               TString::Format("Module %d, %s", module, val ? "Fired" : "Not fired"));
-    eg_shape->SetMainColor       (val ? g_acorde_raw_color_on  : g_acorde_raw_color_off);
-    eg_shape->SetMainTransparency(val ? g_acorde_raw_transp_on : g_acorde_raw_transp_off);
+    eg_shape->SetMainColor       (val ? g_acorde_esd_color_on  : g_acorde_esd_color_off);
+    eg_shape->SetMainTransparency(val ? g_acorde_esd_transp_on : g_acorde_esd_transp_off);
     eg_shape->SetPickable(kTRUE);
     eg_shape->RefMainTrans().SetFrom(*gGeoManager->GetCurrentMatrix());
     eg_shape->SetShape((TGeoShape*) gGeoManager->GetCurrentVolume()->GetShape()->Clone());
@@ -73,7 +57,6 @@ void acorde_raw()
     acorde->AddElement(eg_shape);
   }
 
-  delete stream;
   gEve->Redraw3D();
 }
 
@@ -91,6 +74,6 @@ TString acorde_module_path(Int_t module)
   TGeoPNEntry* pne = gGeoManager->GetAlignableEntry(Form("ACORDE/Array%d", module));
   if (!pne) return "";
 
-  return Form("%s/ACORDESCINTILLATORMODULE_6", pne->GetTitle());
+  return Form("%s/ACORDESCINTILLATORMODULE_6", pne->GetTitle()); 
 
 }
