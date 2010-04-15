7b0c3fceac80a3cdf9ab4fc73199ed36bd95b068
kkanaki <kkanaki@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-04-15 09:06:04 +0000

    - macros for running the analysis tasks for the HLT-offline ESD tree comparison on the GRID

---
+++ HLT/QA/tasks/macros/CreateAlienHandler.C
@@ -0,0 +1,133 @@
+// $Id$
+/*
+ * Alien handler macro to run an HLT analysis task for comparing the offline
+ * with the HLT esd tree on the GRID.
+ *
+ * It is loaded in compare_HLT_offline_grid.C.
+ *
+ * @ingroup alihlt_tpc
+ * @author Kalliopi.Kanaki@ift.uib.no
+ */
+
+AliAnalysisGrid *CreateAlienHandler(const char* mode = "full", const char* detectorTask="global"){
+  
+  // Check if user has a valid token, otherwise make one. This has limitations.
+  // One can always follow the standard procedure of calling alien-token-init then
+  // source /tmp/gclient_env_$UID in the current shell.
+  
+  if(!AliAnalysisGrid::CreateToken()) return NULL;
+  AliAnalysisAlien *plugin = new AliAnalysisAlien();
+
+  // Set the run mode (can be "full", "test", "offline", "submit" or "terminate")
+  plugin->SetRunMode(mode);
+  
+  // check the versions available on alien with the command 'packages'
+  plugin->SetAPIVersion("V1.1x");
+  plugin->SetROOTVersion("v5-26-00b");
+  plugin->SetAliROOTVersion("v4-19-10-AN");
+
+  // data alien directory
+  plugin->SetGridDataDir("/alice/data/2010/LHC10b/");
+  
+  // Set data search pattern
+  plugin->SetDataPattern("*ESD.tag.root"); 
+  
+  plugin->AddRunNumber("000116112"); 
+  //plugin->SetRunRange(xxx,yyy);
+ 
+  // define working and output directories
+  plugin->SetGridWorkingDir("ESDcomparison"); // relative to $HOME
+  plugin->SetGridOutputDir("output");         // relative to working dir
+  
+  
+  Bool_t bTPC=kFALSE, bPHOS=kFALSE, bITS=kFALSE, bGLOBAL=kFALSE;
+ 
+  TString allArgs = detectorTask;
+  TString argument;
+ 
+  TObjArray *pTokens = allArgs.Tokenize(" ");
+  if(pTokens){
+     for(int i=0; i<pTokens->GetEntries(); i++){
+         argument=((TObjString*)pTokens->At(i))->GetString();
+         if(argument.IsNull()) continue;
+
+         if(argument.CompareTo("tpc", TString::kIgnoreCase)==0){
+	    bTPC = kTRUE;
+	    continue;
+         }        
+         if(argument.CompareTo("phos", TString::kIgnoreCase)==0){
+  	    bPHOS = kTRUE;
+	    continue;
+         }         
+	 if(argument.CompareTo("its", TString::kIgnoreCase)==0){
+  	    bITS = kTRUE;
+	    continue;
+         }	
+	 if(argument.CompareTo("global", TString::kIgnoreCase)==0){
+  	    bGLOBAL = kTRUE;
+	    continue;
+         }        
+	 if(argument.CompareTo("all",TString::kIgnoreCase)==0){
+	    bTPC    = kTRUE;
+	    bPHOS   = kTRUE;
+	    bITS    = kTRUE;
+	    bGLOBAL = kTRUE;
+	    continue;
+         }
+         else break;
+    }
+  }
+    
+  // libs, seperate by blanks
+  if(bTPC){  
+    plugin->SetAnalysisSource("AliAnalysisTaskHLTTPC.cxx");  
+    plugin->SetAdditionalLibs("AliAnalysisTaskHLTTPC.h AliAnalysisTaskHLTTPC.cxx");
+    plugin->SetOutputFiles("HLT-OFFLINE-TPC-comparison.root");    
+  }
+  if(bITS){  
+    plugin->SetAnalysisSource("AliAnalysisTaskHLTITS.cxx");  
+    plugin->SetAdditionalLibs("AliAnalysisTaskHLTITS.h AliAnalysisTaskHLTITS.cxx");
+    plugin->SetOutputFiles("HLT-OFFLINE-ITS-comparison.root");    
+  }
+  if(bPHOS){  
+    plugin->SetAnalysisSource("AliAnalysisTaskHLTPHOS.cxx");  
+    plugin->SetOutputFiles("HLT-OFFLINE-GLOBAL-comparison.root");
+    plugin->SetOutputFiles("HLT-OFFLINE-PHOS-comparison.root");    
+  }
+  if(bGLOBAL){  
+    plugin->SetAnalysisSource("AliAnalysisTaskHLT.cxx");  
+    plugin->SetAdditionalLibs("AliAnalysisTaskHLT.h AliAnalysisTaskHLT.cxx"); 
+    plugin->SetOutputFiles("HLT-OFFLINE-GLOBAL-comparison.root");
+  }
+
+  // Optionally define the files to be archived.
+  plugin->SetOutputArchive("log_archive.zip:stdout,stderr");
+  
+  // Optionally set a name for the generated analysis macro (default MyAnalysis.C)
+  plugin->SetAnalysisMacro("runComparison.C");
+  plugin->SetExecutable("comparison.sh");
+
+  plugin->SetSplitMaxInputFileNumber(100);
+  
+  // Optionally set number of failed jobs that will trigger killing waiting sub-jobs.
+  plugin->SetMaxInitFailed(10);
+  
+  // Optionally resubmit threshold.
+  plugin->SetMasterResubmitThreshold(90); // in %
+
+  plugin->SetTTL(30000);// in sec
+  
+  // Optionally set input format (default xml-single)
+  plugin->SetInputFormat("xml-single");
+ 
+  // Optionally modify the name of the generated JDL (default analysis.jdl)
+  plugin->SetJDLName("analysis.jdl");
+ 
+  // Optionally modify job price (default 1)
+  plugin->SetPrice(1);
+  
+  // Optionally modify split mode (default 'se')
+  plugin->SetSplitMode("se");
+  
+  return plugin;
+}
---
+++ HLT/QA/tasks/macros/compare-HLT-offline-grid.C
@@ -0,0 +1,148 @@
+// $Id$
+/*
+ * Example macro to run an HLT analysis task for comparing the offline
+ * with the HLT esd tree on the GRID.
+ *
+ * The output is a root file containing the histograms defined in the
+ * analysis task. There is one output file per detector.
+ *
+ * Usage:
+ * <pre>
+ *   aliroot -b -q -l compare_HLT_offline_grid.C'("full","phos")' 2>&1 | tee task.log
+ *   aliroot -b -q -l compare_HLT_offline_grid.C'("full","global")' 2>&1 | tee task.log
+ *   aliroot -b -q -l compare_HLT_offline_grid.C 2>&1 | tee task.log
+ * </pre>
+ *
+ * If no argument is specified, ALL detector tasks are run. The GRID mode is by default 'full'.
+ *
+ * @ingroup alihlt_tpc
+ * @author zbyin@mail.ccnu.edu.cn, Kalliopi.Kanaki@ift.uib.no
+ */
+
+void compare_HLT_offline_grid(const char* mode = "full", const char* detectorTask="global"){
+ 
+  TStopwatch timer;
+  timer.Start();
+
+  gSystem->Load("libTree.so");
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libPhysics.so");
+ 
+  //----------- Loading the required libraries ---------//
+
+  gSystem->Load("libSTEERBase.so");
+  gSystem->Load("libESD.so");
+  gSystem->Load("libAOD.so");
+  gSystem->Load("libANALYSIS.so");
+  gSystem->Load("libANALYSISalice.so");
+  gSystem->Load("libHLTbase.so");
+  gROOT->ProcessLine(".include $ALICE_ROOT/include");
+
+  
+  Bool_t bAll=kFALSE, bTPC=kFALSE, bPHOS=kFALSE, bITS=kFALSE, bGLOBAL=kFALSE;
+ 
+  TString allArgs = detectorTask;
+  TString argument;
+ 
+  TObjArray *pTokens = allArgs.Tokenize(" ");
+  if(pTokens){
+     for(int i=0; i<pTokens->GetEntries(); i++){
+         argument=((TObjString*)pTokens->At(i))->GetString();
+         if(argument.IsNull()) continue;
+
+         if(argument.CompareTo("tpc", TString::kIgnoreCase)==0){
+	    bTPC = kTRUE;
+	    continue;
+         }        
+         if(argument.CompareTo("phos", TString::kIgnoreCase)==0){
+  	    bPHOS = kTRUE;
+	    continue;
+         }         
+	 if(argument.CompareTo("its", TString::kIgnoreCase)==0){
+  	    bITS = kTRUE;
+	    continue;
+         }	
+	 if(argument.CompareTo("global", TString::kIgnoreCase)==0){
+  	    bGLOBAL = kTRUE;
+	    continue;
+         }        
+	 if(argument.CompareTo("all",TString::kIgnoreCase)==0){
+	    bTPC    = kTRUE;
+	    bPHOS   = kTRUE;
+	    bITS    = kTRUE;
+	    bGLOBAL = kTRUE;
+	    bAll    = kTRUE;
+	    continue;
+         }
+         else break;
+    }
+  }
+    
+  //-------- Make the analysis manager ---------------//
+ 
+  AliAnalysisManager *mgr  = new AliAnalysisManager("TestManager");
+  AliESDInputHandler *esdH = new AliESDInputHandler;
+  esdH->SetReadHLT();
+  mgr->SetInputEventHandler(esdH);  
+  mgr->SetNSysInfo(1000);
+  
+  // Create and configure the alien handler plugin
+  gROOT->LoadMacro("CreateAlienHandler.C");
+  AliAnalysisGrid *alienHandler = CreateAlienHandler(mode, detectorTask);
+  if (!alienHandler) return;
+  
+  // Connect plugin to the analysis manager
+  mgr->SetGridHandler(alienHandler);
+ 
+  //-------------- Compile the analysis tasks ---------- //
+  if(bTPC)    gROOT->LoadMacro("AliAnalysisTaskHLTTPC.cxx+"); 
+  if(bPHOS)   gROOT->LoadMacro("AliAnalysisTaskHLTPHOS.cxx+"); 
+  if(bITS)    gROOT->LoadMacro("AliAnalysisTaskHLTITS.cxx+");
+  if(bGLOBAL) gROOT->LoadMacro("AliAnalysisTaskHLT.cxx+");
+
+ 
+  //-------------- define the tasks ------------//
+  
+  if(bTPC){ 
+     AliAnalysisTaskHLTTPC *taskTPC = new AliAnalysisTaskHLTTPC("offhlt_comparison_TPC");
+     mgr->AddTask(taskTPC);
+     AliAnalysisDataContainer *coutput1 =  mgr->CreateContainer("tpc_histograms", TList::Class(), AliAnalysisManager::kOutputContainer, "HLT-OFFLINE-TPC-comparison.root");  
+     mgr->ConnectInput(taskTPC,0,mgr->GetCommonInputContainer());
+     mgr->ConnectOutput(taskTPC,1,coutput1);
+  }
+
+  if(bPHOS){
+     AliAnalysisTaskHLTPHOS *taskPHOS = new AliAnalysisTaskHLTPHOS("offhlt_comparison_PHOS");
+     mgr->AddTask(taskPHOS);
+     AliAnalysisDataContainer *coutput2 =  mgr->CreateContainer("phos_histograms",TList::Class(), AliAnalysisManager::kOutputContainer, "HLT-OFFLINE-PHOS-comparison.root");  
+     mgr->ConnectInput(taskPHOS,0,mgr->GetCommonInputContainer());
+     mgr->ConnectOutput(taskPHOS,1,coutput2);
+  }
+  
+  if(bITS){
+     AliAnalysisTaskHLTITS *taskITS = new AliAnalysisTaskHLTITS("offhlt_comparison_ITS");
+     mgr->AddTask(taskITS);
+     AliAnalysisDataContainer *coutput3 =  mgr->CreateContainer("its_histograms",TList::Class(), AliAnalysisManager::kOutputContainer, "HLT-OFFLINE-ITS-comparison.root");  
+     mgr->ConnectInput(taskITS,0,mgr->GetCommonInputContainer());
+     mgr->ConnectOutput(taskITS,1,coutput3);
+  }
+ 
+  if(bGLOBAL){
+     AliAnalysisTaskHLT *taskGLOBAL = new AliAnalysisTaskHLT("offhlt_comparison_GLOBAL");
+     mgr->AddTask(taskGLOBAL);
+     AliAnalysisDataContainer *coutput4 =  mgr->CreateContainer("global_histograms",TList::Class(), AliAnalysisManager::kOutputContainer, "HLT-OFFLINE-GLOBAL-comparison.root");  
+     mgr->ConnectInput(taskGLOBAL,0,mgr->GetCommonInputContainer());
+     mgr->ConnectOutput(taskGLOBAL,1,coutput4);
+  }
+  
+  // Enable debug printouts
+  mgr->SetDebugLevel(2);
+
+  if (!mgr->InitAnalysis()) return;
+  mgr->PrintStatus();
+  mgr->StartAnalysis("grid");
+
+  timer.Stop();
+  timer.Print();
+}
