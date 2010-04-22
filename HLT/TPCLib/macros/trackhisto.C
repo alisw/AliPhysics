5e071ba1d606aabb9a74aa0fff69ac6172dca80c
kkanaki <kkanaki@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-04-22 21:24:25 +0000

    - added options to access the input file and/or the OCDB entries from the grid

--- HLT/TPCLib/macros/trackhisto.C
+++ HLT/TPCLib/macros/trackhisto.C
@@ -9,7 +9,9 @@
  * Usage:
  * <pre>
  *
- * aliroot -b -q -l trackhisto.C'("file", minEvent, maxEvent)'
+ * aliroot -b -q -l trackhisto.C'("file", "cdbURI", minEvent, maxEvent)' 
+ * aliroot -b -q -l trackhisto.C'("raw://run115322", 0, 100)'
+ * aliroot -b -q -l trackhisto.C'("raw.root", "local://$ALICE_ROOT/OCDB/", 0, 100)'
  * </pre>
  *
  * The reconstruction is steered by the AliReconstruction object in the
@@ -19,7 +21,7 @@
  * @author Kalliopi.Kanaki@ift.uib.no
  */
 
-void trackhisto(const char *filename, int minEvent=-1, int maxEvent=-1){
+void trackhisto(const char *filename, const char *cdbURI, int minEvent=-1, int maxEvent=-1){
 
   if(!gSystem->AccessPathName("galice.root")){
     cerr << "Remove galice.root or run in a different folder." << endl;
@@ -40,16 +42,35 @@ void trackhisto(const char *filename, int minEvent=-1, int maxEvent=-1){
   AliHLTSystem *gHLT = AliHLTPluginBase::GetInstance();
 
   AliHLTConfiguration trhiconf("trhi", "TPCTrackHisto", "TPC-clusters TPC-globalmerger", "");
-  AliHLTConfiguration rwfconf("rfw", "ROOTFileWriter", "trhi", "-datafile trackhisto -concatenate-events -overwrite");
+  AliHLTConfiguration rwfconf("rfw", "ROOTFileWriter", "trhi", "-datafile TrackHisto -concatenate-events -overwrite");
 
 
   AliReconstruction rec;
 
   rec.SetRunQA(":");
   rec.SetInput(filename);
+  
+  // connect to the GRID if we use a file or OCDB from the GRID
+  TString struri  = cdbURI;
+  TString strfile = filename;
+  if(struri.BeginsWith("raw://") ||  strfile.Contains("://") && !strfile.Contains("local://")){
+     TGrid::Connect("alien");
+  }
+  
+  AliCDBManager *man = AliCDBManager::Instance();
+  man->SetDefaultStorage(cdbURI);
 
-  rec.SetDefaultStorage("local://$ALICE_ROOT/OCDB/");  
-  rec.SetSpecificStorage("GRP/GRP/Data",Form("local://%s",gSystem->pwd()));
+  if(struri.BeginsWith("local://")) {
+     // set specific storage for GRP entry
+     // search in the working directory and one level above, the latter
+     // follows the standard simulation setup like e.g. in test/ppbench
+     if(!gSystem->AccessPathName("GRP/GRP/Data")) {
+       man->SetSpecificStorage("GRP/GRP/Data", "local://$PWD");
+     } 
+     else if (!gSystem->AccessPathName("../GRP/GRP/Data")) {
+          man->SetSpecificStorage("GRP/GRP/Data", "local://$PWD/..");
+     }
+  }
 
   if(minEvent>=0 || maxEvent>minEvent){
      if(minEvent<0) minEvent=0;
@@ -65,10 +86,15 @@ void trackhisto(const char *filename, int minEvent=-1, int maxEvent=-1){
 
 }
 
+void trackhisto(const char *filename, int minEvent=-1, int maxEvent=-1){
+
+  trackhisto(filename, "raw://", minEvent, maxEvent);
+}
+
 void trackhisto(){
 
   cout << "trackhisto: Run HLT TPC tracking and fill histograms" << endl;
-  cout << " Usage: aliroot -b -q -l trackhisto.C'(\"file\", minEvent, maxEvent)'" << endl;
+  cout << " Usage: aliroot -b -q -l trackhisto.C'(\"file\", \"cdb\", minEvent, maxEvent)'" << endl;
   cout << "" << endl;
 }
 
