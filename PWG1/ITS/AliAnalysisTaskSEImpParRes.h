5dfce5e1dc8aa60632f99aa9f94c76954b7c4413
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-04-23 21:05:15 +0000

    Added d0 vs phi and cluster type

--- PWG1/ITS/AliAnalysisTaskSEImpParRes.h
+++ PWG1/ITS/AliAnalysisTaskSEImpParRes.h
@@ -32,7 +32,7 @@ class AliAnalysisTaskSEImpParRes : public AliAnalysisTaskSE {
   virtual void Terminate(Option_t *option);
   void SetReadMC(Bool_t readMC) { fReadMC=readMC; return; }
   void SetSelectedPdg(Int_t pdg) { fSelectedPdg=pdg; return; }
-  void SetUseDiamond(Bool_t use=kTRUE) { fUseDiamond=use; return; }
+  void SetUseDiamond(Bool_t use=kFALSE) { fUseDiamond=use; return; }
 
  private:
   
@@ -42,6 +42,8 @@ class AliAnalysisTaskSEImpParRes : public AliAnalysisTaskSE {
   Int_t PtBin(Double_t pt) const;
   Int_t SinThetaBin(Double_t sintheta) const;
   Double_t Getd0HistRange(Int_t i) const;
+  Int_t PhiBin(Double_t phi) const;
+  Int_t ClusterTypeOnITSLayer(AliESDtrack *t,Int_t layer) const;
   Bool_t fReadMC;       // flag used to switch on/off MC reading
   Int_t  fSelectedPdg;  // only for a given particle species (-1 takes all tracks)
   Bool_t fUseDiamond;   // use diamond constraint in primary vertex
@@ -63,6 +65,19 @@ class AliAnalysisTaskSEImpParRes : public AliAnalysisTaskSE {
   TList *fOutputOnlyRefitSkip; //! ITS+TPC: any ITScls, w/o track in vtx
   TList *fOutputSinThetaRec;              //! ITS+TPC: TH2F(pt,sintheta), with track in vtx
   TList *fOutputSinThetaSkip;             //! ITS+TPC: TH2F(pt,sintheta), w/o track in vtx
+  TList *fOutputallPointTrue;    //!
+  TList *fOutputpostvTracTrue;   //!
+  TList *fOutputnegtvTracTrue;   //!
+  TList *fOutputpullAllpointTrue;  //!
+  TList *fOutputphiAllpointSkip;  //!
+  TList *fOutputphiPostvtracSkip;  //!
+  TList *fOutputphiNegtvtracSkip;  //!
+  TList *fOutputclusterTypeSPD01Skip;  //!
+  TList *fOutputclusterTypeSPD02Skip;  //!
+  TList *fOutputclusterTypeSPD03Skip;  //!
+  TList *fOutputclusterTypeSPD11Skip;  //!
+  TList *fOutputclusterTypeSPD12Skip;  //!
+  TList *fOutputclusterTypeSPD13Skip;  //!
   TList *fOutputPt;     //!           
   TH1F  *fNentries;   //! histogram of number of events
   TH1F  *fEstimVtx;   //! vertex resolution
