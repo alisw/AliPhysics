76b177a303353646881fe38ab693dd7c309a45cf
phille <phille@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-05-05 03:00:07 +0000

    Cosmetics

--- HLT/PHOS/AliHLTPHOSConstants.h
+++ HLT/PHOS/AliHLTPHOSConstants.h
@@ -26,90 +26,55 @@ public:
 
   Int_t GetMAXHOSTS() const { return fkMAXHOSTS;}				
   Int_t GetDEFAULTEVENTPORT() const { return fkDEFAULTEVENTPORT; }		
-  //  Int_t GetMAXBINVALUE() const { return fkMAXBINVALUE; } 			
-
-  //  Int_t GetHIGHGAIN() const { return fkHIGHGAIN;} 				
-  //  Int_t GetLOWGAIN() const { return fkLOWGAIN;} 				
-  										
-  //  Int_t GetALTROMAXSAMPLES() const {return fkALTROMAXSAMPLES;} 			
-  //  Int_t GetALTROMAXPRESAMPLES() const { return fkALTROMAXPRESAMPLES;} 		
- 										
   Int_t GetNZROWSRCU() const { return fkNZROWSRCU;}				
   Int_t GetNXCOLUMNSRCU() const { return fkNXCOLUMNSRCU;} 			
   Int_t GetNZROWSMOD() const { return fkNZROWSMOD;} 				
   Int_t GetNXCOLUMNSMOD() const { return fkNXCOLUMNSMOD;} 			
-  //  Int_t GetNGAINS() const { return fkNGAINS;} 					
   Int_t GetNDATATYPES() const { return fkNDATATYPES;} 				
- 										
   Int_t GetPFMAXPATHLENGTH() const { return fkPFMAXPATHLENGTH;} 		
- 										
   Int_t GetPFDEFAULTNSAMPLES() const{ return fkPFDEFAULTNSAMPLES;} 		
   Int_t GetPFDEFAULTSTARTINDEX() const { return fkPFDEFAULTSTARTINDEX;} 	
- 										
   Double_t GetDEFAULTTAU() const { return fkDEFAULTTAU;} 			
   Int_t GetDEFAULTFS() const { return fkDEFAULTFS;} 				
- 										
   Int_t GetMODULE0() const { return fkMODULE0;} 				
   Int_t GetMODULE1() const { return fkMODULE1;} 				
   Int_t GetMODULE2() const { return fkMODULE2;} 				
   Int_t GetMODULE3() const { return fkMODULE3;} 				
   Int_t GetMODULE4() const { return fkMODULE4;} 				
- 										
   Int_t GetCSPSPERFEE() const { return fkCSPSPERFEE;} 				
   Int_t GetRCU0() const { return fkRCU0;} 					
   Int_t GetRCU1() const { return fkRCU1;} 					
   Int_t GetRCU2() const { return fkRCU2;} 					
   Int_t GetRCU3() const { return fkRCU3;} 					
- 										
   Int_t GetZ0() const { return fkZ0;} 						
   Int_t GetZ1() const { return fkZ1;} 						
   Int_t GetX0() const { return fkX0;} 						
   Int_t GetX1() const { return fkX1;} 						
- 										
   Int_t GetNMODULES() const { return fkNMODULES;} 				
   Int_t GetNRCUS() const { return fkNRCUS;} 					
-										
   Int_t GetNRCUSPERMODULE() const { return fkNRCUSPERMODULE;} 			
   Int_t GetNRCUSPERTOTAL() const { return fkNRCUSPERTOTAL;} 			
   Int_t GetNFEECS() const { return fkNFEECS;} 					
   Int_t GetNALTROS() const { return fkNALTROS;} 				
   Int_t GetNALTROCHANNELS() const { return fkNALTROCHANNELS;} 			
   Int_t GetNBRANCHES() const { return fkNBRANCHES;} 				
-										
-  Float_t GetCELLSTEP() const { return fkCELLSTEP; } 					
-  Int_t GetNRCUSPERSECTOR() const { return fkNRCUSPERSECTOR; } 					
-  
-  Int_t GetDDLOFFSET() const { return fkDDLOFFSET; }
-  
+  Float_t GetCELLSTEP() const { return fkCELLSTEP; } 				  Int_t GetNRCUSPERSECTOR() const { return fkNRCUSPERSECTOR; } 			  Int_t GetDDLOFFSET() const { return fkDDLOFFSET; }
   TString GetDETNAME() const { return fkDETNAME; }
 
 private:
-
   /** Constant members */
   const Int_t fkMAXHOSTS;                  //Constant
   const Int_t fkDEFAULTEVENTPORT;          //Constant
-  //   const Int_t fkMAXBINVALUE;          //Constant
-  //  const Int_t fkHIGHGAIN;          //Constant
-  //  const Int_t fkLOWGAIN;          //Constant
-
-  //  const Int_t fkALTROMAXSAMPLES; /**<The maximum number of samples of the ALTRO*/
-  //   const Int_t fkALTROMAXPRESAMPLES;           //Constant
-
   const Int_t fkNZROWSRCU; /**<Number of rows per module*/ 
   const Int_t fkNXCOLUMNSRCU;          //Constant
   const Int_t fkNZROWSMOD;  /**<Number of rows per module*/ 
   const Int_t fkNXCOLUMNSMOD;  /**<Number of columns per module*/ 
-  //   const Int_t fkNGAINS;  /**<Number of gains per ALTRO channel*/
   const Int_t fkNDATATYPES;          //Constant
-
   const Int_t fkPFMAXPATHLENGTH;          //Constant
-
   const Int_t fkPFDEFAULTNSAMPLES;          //Constant
   const Int_t fkPFDEFAULTSTARTINDEX;          //Constant
-
   const Double_t fkDEFAULTTAU; /**<Assume that the signal rise time of the altrp pulses is 2 us (nominal value of the electronics)*/
   const Int_t fkDEFAULTFS; /**<Assume that the signal is samples with 10 MHZ samle rate*/
-
   const Int_t fkMODULE0;          //Constant
   const Int_t fkMODULE1;          //Constant
   const Int_t fkMODULE2;          //Constant
@@ -139,9 +104,7 @@ private:
 
   const Float_t fkCELLSTEP;  //Constant
   const Int_t fkNRCUSPERSECTOR;  //Constant
-  
   const Int_t fkDDLOFFSET;  //Constant
-
   const TString fkDETNAME;  //Constant
    
   ClassDef(AliHLTPHOSConstants, 1);
