6d303ad016b9a6f821b07b22ad58be18d6d79518
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-05-08 14:22:38 +0000

    Addition of Lc->V0+bachelor analysis (Zaida)

--- PWG3/PWG3vertexingHFLinkDef.h
+++ PWG3/PWG3vertexingHFLinkDef.h
@@ -17,6 +17,7 @@
 #pragma link C++ class AliRDHFCutsDplustoKpipi+;
 #pragma link C++ class AliRDHFCutsDstoKKpi+;
 #pragma link C++ class AliRDHFCutsLctopKpi+;
+#pragma link C++ class AliRDHFCutsLctoV0+;
 #pragma link C++ class AliRDHFCutsD0toKpipipi+;
 #pragma link C++ class AliAnalysisVertexingHF+;
 #pragma link C++ class AliAnalysisTaskSEVertexingHF+;
