3797367f55be46346be67e380e18d886c1cbe115
odjuvsla <odjuvsla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-05-15 12:53:05 +0000

    - removing obsolete files

--- HLT/CALO/macros/runPhos.C
+++
@@ -1,9 +0,0 @@
-
-int runPhos(const char *data="./", const char *grp="./")
-{
-  gSystem->Load("libAliHLTCalo");
-  gROOT->LoadMacro("rec_hlt_calo_phos.C");
-  gROOT->LoadMacro("read_HLT_ESDs.C");
-  rec_hlt_calo_phos();
-  read_HLT_ESDs();
-}
