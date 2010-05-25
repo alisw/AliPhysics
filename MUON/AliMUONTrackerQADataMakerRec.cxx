3b43fb3eab556e8692af5e51d624f252be4cefa8
laphecet <laphecet@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-05-25 11:48:45 +0000

    Fixing a double delete

--- MUON/AliMUONTrackerQADataMakerRec.cxx
+++ MUON/AliMUONTrackerQADataMakerRec.cxx
@@ -1636,7 +1636,6 @@ TObjArray* AliMUONTrackerQADataMakerRec::GetArray(TObjArray*& array, Bool_t crea
     if ( create ) 
     {
       array = new TObjArray(AliRecoParam::kNSpecies);
-      array->SetOwner(kTRUE);
     }
   }
   
