43f33cd78e38a556a23c465fe2f35f971f79aa0b
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-06-25 10:17:00 +0000

    New task to check the quality of the muon reconstruction and macros to run it. New generic class to handle collection of counters (Philippe. P.)

---
+++ PWG3/muon/AliAnalysisTaskMuonQA.h
@@ -0,0 +1,90 @@
+#ifndef ALIANALYSISTASKMUONQA_H
+#define ALIANALYSISTASKMUONQA_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/// \ingroup muondep
+/// \class AliAnalysisTaskMuonQA
+/// \brief Quality assurance of MUON ESDs
+//Author: Philippe Pillot - SUBATECH Nantes
+
+class TObjArray;
+class AliCounterCollection;
+
+class AliAnalysisTaskMuonQA : public AliAnalysisTaskSE {
+public:
+  
+  AliAnalysisTaskMuonQA(const char *name = "MuonQA");
+  virtual ~AliAnalysisTaskMuonQA();
+  
+  virtual void   UserCreateOutputObjects();
+  virtual void   UserExec(Option_t *);
+  virtual void   Terminate(Option_t *);
+  
+  void SelectCharge(Short_t charge = 0) {fSelectCharge = charge;}
+  void SelectPhysics(Bool_t flag = kTRUE) {fSelectPhysics = flag;}
+  
+private:
+  
+  /// Not implemented
+  AliAnalysisTaskMuonQA(const AliAnalysisTaskMuonQA& rhs);
+  /// Not implemented
+  AliAnalysisTaskMuonQA& operator = (const AliAnalysisTaskMuonQA& rhs);
+  
+  Double_t ChangeThetaRange(Double_t theta);
+  
+private:
+  
+  enum EESD { 
+    kNTracks                 = 0,  ///< number of tracks
+    kMatchTrig               = 1,  ///< number of tracks matched with trigger
+    kSign                    = 2,  ///< track sign
+    kDCA                     = 3,  ///< DCA distribution
+    kP                       = 4,  ///< P distribution
+    kPt                      = 5,  ///< Pt distribution
+    kRapidity                = 6,  ///< rapidity distribution
+    kThetaX                  = 7,  ///< thetaX distribution
+    kThetaY                  = 8,  ///< thetaY distribution
+    kChi2                    = 9,  ///< normalized chi2 distribution
+    kProbChi2                = 10, ///< distribution of probability of chi2
+    
+    kNClustersPerTrack       = 11, ///< number of clusters per track
+    kNChamberHitPerTrack     = 12, ///< number of chamber hit per track
+    kNClustersPerCh          = 13, ///< number of clusters per chamber per track
+    kNClustersPerDE          = 14, ///< number of clusters per DE per track
+    kClusterHitMapInCh       = 15, ///< cluster position distribution in chamber i
+    kClusterChargeInCh       = 25, ///< cluster charge distribution in chamber i
+    kClusterChargePerChMean  = 35, ///< cluster charge per Ch: mean
+    kClusterChargePerChSigma = 36, ///< cluster charge per Ch: dispersion
+    kClusterChargePerDE      = 37, ///< cluster charge distribution per DE
+    kClusterChargePerDEMean  = 38, ///< cluster charge per DE: mean
+    kClusterChargePerDESigma = 39, ///< cluster charge per DE: dispersion
+    kClusterSizeInCh         = 40, ///< cluster size distribution in chamber i
+    kClusterSizePerChMean    = 50, ///< cluster size per Ch: mean
+    kClusterSizePerChSigma   = 51, ///< cluster size per Ch: dispersion
+    kClusterSizePerDE        = 52, ///< cluster size distribution per DE
+    kClusterSizePerDEMean    = 53, ///< cluster size per DE: mean
+    kClusterSizePerDESigma   = 54  ///< cluster size per DE: dispersion
+  };
+  
+  TObjArray*  fList;       //!< List of output object for everybody
+  TObjArray*  fListExpert; //!< List of output object for experts
+  
+  AliCounterCollection* fTrackCounters; //!< track statistics
+  AliCounterCollection* fEventCounters; //!< event statistics
+  
+  Short_t fSelectCharge;  ///< Fill histograms only with negative/position tracks (0=all)
+  Bool_t  fSelectPhysics; ///< Fill histograms only with track passing the physics selection
+  
+  static const Int_t nCh;       ///< number of tracking chambers
+  static const Int_t nDE;       ///< number of DE
+  static const Float_t dMax[5]; ///< maximum diameter of each station
+  static const Int_t fgkNTriggerClass;        ///< number of trigger class we consider
+  static const char* fgkTriggerClass[10];     ///< full trigger class name
+  static const char* fgkTriggerShortName[11]; ///< short trigger class name for counters
+  
+  ClassDef(AliAnalysisTaskMuonQA, 1);
+};
+
+#endif
+
---
+++ PWG3/muon/AliCounterCollection.h
@@ -0,0 +1,117 @@
+#ifndef ALICOUNTERCOLLECTION_H
+#define ALICOUNTERCOLLECTION_H
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/// \ingroup PWG3muon
+/// \class AliCounterCollection
+/// \brief generic class to handle a collection of counters
+// Author: Philippe Pillot
+
+#include <TNamed.h>
+
+class TString;
+class TObjArray;
+class THnSparse;
+class THashList;
+class TArrayI;
+class TH1D;
+class TH2D;
+class TCollection;
+
+class AliCounterCollection : public TNamed {
+public:
+  
+  AliCounterCollection(const char* name = "counters");
+  virtual ~AliCounterCollection();
+  
+  virtual void Clear(Option_t* = "");
+  
+  // Add a new rubric with the complete list of related key words separated by "/"
+  void AddRubric(TString name, TString listOfKeyWords);
+  // Add a new rubric containing at maximum maxNKeyWords key words
+  void AddRubric(TString name, Int_t maxNKeyWords);
+  // Initialize the internal counters from the added rubrics
+  void Init();
+  
+  // Add "value" to the counter referenced by "externalKey"
+  void Count(TString externalKey, Int_t value = 1);
+  
+  // Print every individual counters if opt=="", else call "Print(TString rubrics=opt, TString selections="")"
+  virtual void Print(const Option_t* opt = "") const;
+  // Print the full list of key words
+  void PrintKeyWords() const;
+  // Print value of selected counter
+  void PrintValue(TString selections);
+  // Print desired rubrics for the given selection
+  void Print(TString rubrics, TString selections);
+  // Print the overall statistics under the given rubric for the given selection
+  void PrintSum(TString rubric, TString selections = "");
+  
+  /// Overload TObject::Draw(Option_t*): Call "Draw(TString rubric1=opt, TString selections="")"
+  virtual void Draw(Option_t* opt = "") {Draw(opt, "");}
+  // Draw counters of the rubric "rubric1" for the given "selection"
+  TH1D* Draw(TString rubric1, TString selections);
+  // Draw counters of the "rubric1" vs "rubric2" for the given "selection"
+  TH2D* Draw(TString rubric1, TString rubric2, TString selections);
+  
+  // Add the given AliCounterCollections to this
+  void Add(const AliCounterCollection* counter);
+  
+  // Merge this with a list of AliCounterCollections
+  Long64_t Merge(TCollection* list);
+  
+  // Sort rubrics defined without a list of authorized key words or all rubrics if opt=="all"
+  void Sort(Option_t* opt = "", Bool_t asInt = kFALSE);
+  /// Sort only that rubric. If asInt=kTRUE, key words are ordered as interger instead of alphabetically
+  void SortRubric(TString rubric, Bool_t asInt = kFALSE);
+  
+private:
+  
+  /// Not implemented
+  AliCounterCollection(const AliCounterCollection& rhs);
+  /// Not implemented
+  AliCounterCollection& operator = (const AliCounterCollection& rhs);
+  
+  // Return the corresponding bins ordered by rubric or 0x0 if externalKey is not valid
+  const Int_t* FindBins(const TString& externalKey, Bool_t allocate, Int_t& nEmptySlots);
+  // Return the dimension corresponding to that rubric (or -1)
+  Int_t FindDim(const TString& rubricName) const;
+  // Return the bin number corresponding to that key word (or -1)
+  Int_t FindBin(Int_t dim, const TString& keyWord, Bool_t allocate);
+  
+  // Make sure all strings appear only once in this list
+  void CleanListOfStrings(TObjArray* list);
+  
+  // Print the content of 1D histogram as a list
+  void PrintList(const TH1D* hist) const;
+  // Print the content of 2D histogram as an array
+  void PrintArray(const TH2D* hist) const;
+  // Print the content of nD histogram as a list of arrays
+  void PrintListOfArrays(const THnSparse* hist) const;
+  
+  // Return the number of characters of the longest label
+  Int_t GetMaxLabelSize(THashList* labels) const;
+  
+  // Return desired "data" for the given "selection" stored in a new histogram or 0x0
+  TObject* Projection(const TObjArray& data, const TString& selections);
+  
+  // Consistency check of the two counter collections
+  Int_t* CheckConsistency(const AliCounterCollection* c);
+  
+  // Sort labels (alphabetically or as integer) in each rubric flagged in "rubricsToSort"
+  void Sort(const Bool_t* rubricsToSort, Bool_t asInt);
+  /// Return a list (not owner) of labels sorted assuming they are integers
+  THashList* SortAsInt(const THashList* labels);
+  
+private:
+  
+  THashList* fRubrics;        ///< list of rubrics with associated key words
+  TArrayI*   fRubricsSize;    ///< maximum number of key words in the corresponding rubric
+  THnSparse* fCounters;       ///< histogram of nRubrics dimensions used as n-dimensional counter
+  
+  ClassDef(AliCounterCollection, 1); // collection of mergeable counters
+};
+
+#endif
+
