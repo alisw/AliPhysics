5ac715d9bf5dd6c9e9d1aeae14c2f0ddf77dc7a3
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-06 13:39:35 +0000

    Changes related to PID mode and parameters configuration (Rossella, Francesco)

--- PWG3/vertexingHF/AliAODPidHF.h
+++ PWG3/vertexingHF/AliAODPidHF.h
@@ -27,8 +27,15 @@ class AliAODPidHF : public AliAODPid{
  void SetSigma(Int_t idet,Double_t sigma){fnSigma[idet]=sigma;return;}
  void SetTofSigma(Double_t sigma){fTOFSigma=sigma;return;}
  void SetPriors(Double_t *priors){fPriors=priors;return;}
- void SetPLimit(Double_t *plim){fPLimit=plim;return;}
+// void SetPLimit(Double_t *plim){fPLimit=plim;return;}
+ void SetPLimit(Double_t *plim){for(Int_t i=0;i<2;i++){fPLimit[i]=plim[i];}return;}
  void SetAsym(Bool_t asym){fAsym=asym;return;}
+ void SetTPC(Bool_t tpc){fTPC=tpc;return;}
+ void SetTOF(Bool_t tof){fTOF=tof;return;}
+ void SetITS(Bool_t its){fITS=its;return;}
+ void SetTRD(Bool_t trd){fTRD=trd;return;}
+ void SetMatch(Bool_t match){fMatch=match;return;}
+ void SetCompat(Bool_t comp){fCompat=comp;return;}
  
 
  Int_t RawSignalPID (AliAODTrack *track, TString detector) const;
@@ -36,12 +43,14 @@ class AliAODPidHF : public AliAODPid{
  Bool_t IsPionRaw (AliAODTrack *track, TString detector) const;
  Bool_t IsProtonRaw (AliAODTrack *track, TString detector) const;
  Bool_t IsElectronRaw (AliAODTrack *track, TString detector) const;
- void BayesianProbability(AliAODTrack *track,TString detectors,Double_t *pid) const;
+ void BayesianProbability(AliAODTrack *track,Double_t *pid) const;
  void CombinedProbability(AliAODTrack *track,Bool_t *type) const; //0 = pion, 1 = kaon, 2 = proton
  Bool_t CheckStatus(AliAODTrack *track,TString detectors) const;
 
  Bool_t TPCRawAsym(AliAODTrack* track,Int_t specie) const;
  Int_t MatchTPCTOF(AliAODTrack *track,Int_t mode,Int_t specie,Bool_t compat);
+
+ Int_t MakeRawPid(AliAODTrack *track,Int_t specie); //general method to perform PID using raw signals
 
 
 
@@ -56,19 +65,24 @@ class AliAODPidHF : public AliAODPid{
  void BayesianProbabilityTRD(AliAODTrack *track,Double_t *prob) const;
 
  private:
-
- Int_t fnNSigma; // size of the nsigma array
- Double_t *fnSigma; //[fnNSigma] sigma for the raw signal PID: 0-2 for TPC, 3 for TOF, 4 for ITS 
+ Int_t fnNSigma; // number of sigmas
+ Double_t *fnSigma; // [fnNSigma], sigma for the raw signal PID: 0-2 for TPC, 3 for TOF, 4 for ITS 
  Double_t fTOFSigma; // TOF precision 
- Int_t fnPriors;    // size of the priors array
- Double_t *fPriors; //[fnPriors] set of priors
- Int_t fnPLimit;  // size of the plimit array
- Double_t *fPLimit; //[fnPLimit] limit of p intervals for asimmetric PID: fPLimit<p[0], fPLimit[0]<p<fPLimit[1], p>fPLimit[1]
+ Int_t fnPriors; //number of priors
+ Double_t *fPriors; // [fnPriors], set of priors
+ Int_t fnPLimit; //number of Plimit
+ Double_t *fPLimit; // [fnPLimit], limit of p intervals for asimmetric PID: fPLimit<p[0], fPLimit[0]<p<fPLimit[1], p>fPLimit[1]
  Bool_t fAsym; // asimmetric PID required
+ Bool_t fTPC; //switch to include or exclude TPC 
+ Bool_t fTOF; // switch to include or exclude TOF
+ Bool_t fITS; //switch to include or exclude ITS
+ Bool_t fTRD; // switch to include or exclude TRD
+ Bool_t fMatch; //switch to combine the info from more detectors: 1 = || , 2 = &, 3 = p region
+ Bool_t fCompat; // compatibility region : useful only if fMatch=1
  
 
 
- ClassDef(AliAODPidHF,4) // AliAODPid for heavy flavor PID
+ ClassDef(AliAODPidHF,3) // AliAODPid for heavy flavor PID
 
 };
 
