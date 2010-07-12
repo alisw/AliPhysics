aabf833fed661c7f545c9cbcca4f3b493c628399
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-12 15:04:02 +0000

    Dplus task used AliAODPidHF via AliRDHFCutsDplustoKpipi (Renu, Francesco)

--- PWG3/vertexingHF/AliRDHFCutsDplustoKpipi.h
+++ PWG3/vertexingHF/AliRDHFCutsDplustoKpipi.h
@@ -14,15 +14,14 @@
 
 
 #include "AliRDHFCuts.h"
-
+#include "AliAODPidHF.h"
 class AliRDHFCutsDplustoKpipi : public AliRDHFCuts 
 {
  public:
 
   AliRDHFCutsDplustoKpipi(const char* name="CutsDplustoKpipi");
   
-  virtual ~AliRDHFCutsDplustoKpipi(){}
-
+  virtual ~AliRDHFCutsDplustoKpipi(){};
   AliRDHFCutsDplustoKpipi(const AliRDHFCutsDplustoKpipi& source);
   AliRDHFCutsDplustoKpipi& operator=(const AliRDHFCutsDplustoKpipi& source); 
  
@@ -30,12 +29,13 @@ class AliRDHFCutsDplustoKpipi : public AliRDHFCuts
 
   using AliRDHFCuts::IsSelected;
   virtual Int_t IsSelected(TObject* obj,Int_t selectionLevel);
-  
+  virtual Int_t IsSelectedPID(AliAODRecoDecayHF *rd) const;
+
+
   Float_t GetMassCut(Int_t iPtBin=0) const { return (GetCuts() ? fCutsRD[GetGlobalIndex(0,iPtBin)] : 1.e6);}
   Float_t GetDCACut(Int_t iPtBin=0) const { return (GetCuts() ? fCutsRD[GetGlobalIndex(1,iPtBin)] : 1.e6);}
   
  protected:
-  
 
   ClassDef(AliRDHFCutsDplustoKpipi,1);  // class for cuts on AOD reconstructed 
                                    // D+->Kpipi
