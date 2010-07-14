f294f0cbbffcf2570874a48cb000a5a1555ee817
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-14 15:03:41 +0000

    Missing file

---
+++ test/generators/herwig/rec.C
@@ -0,0 +1,16 @@
+void rec() {
+  AliReconstruction reco;
+
+  reco.SetWriteESDfriend();
+  reco.SetWriteAlignmentData();
+
+  reco.SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+  reco.SetSpecificStorage("GRP/GRP/Data",
+			  Form("local://%s",gSystem->pwd()));
+
+  TStopwatch timer;
+  timer.Start();
+  reco.Run();
+  timer.Stop();
+  timer.Print();
+}
