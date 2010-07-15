25234e1d5fa9cbd139ab2df2643f76227e41c08d
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-15 15:25:44 +0000

    Fix for report #69877: High memory consumption for simulation of HIJING central PbPb@2760 GeV

--- test/PbPbbench/rec.C
+++ test/PbPbbench/rec.C
@@ -19,6 +19,12 @@ void rec() {
     reco.SetQAWriteExpert(det) ; 
   }
 
+  // High multiplicity settings
+  Double_t cuts[]={33, 0.1, 0.1, 0.05, 0.99, 0.9, 100}; 
+  AliV0vertexer::SetDefaultCuts(cuts); 
+  Double_t cts[]={33., 0.05, 0.008, 0.035, 0.1, 0.9985, 0.9,100}; 
+  AliCascadeVertexer::SetDefaultCuts(cts); 
+
   TStopwatch timer;
   timer.Start();
   reco.Run();
--- test/PbPbbench/recraw/rec.C
+++ test/PbPbbench/recraw/rec.C
@@ -25,6 +25,12 @@ void rec() {
 
   reco.SetNumberOfEventsPerFile(-1); // all events in one single file
 
+  // High multiplicity settings
+  Double_t cuts[]={33, 0.1, 0.1, 0.05, 0.99, 0.9, 100}; 
+  AliV0vertexer::SetDefaultCuts(cuts); 
+  Double_t cts[]={33., 0.05, 0.008, 0.035, 0.1, 0.9985, 0.9,100}; 
+  AliCascadeVertexer::SetDefaultCuts(cts); 
+
   TStopwatch timer;
   timer.Start();
   reco.Run();
