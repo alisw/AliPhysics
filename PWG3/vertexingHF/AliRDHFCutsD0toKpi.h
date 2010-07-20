c73bb3fed1b2afbed2fb8cad1c9b4028b5cb01b1
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-20 17:42:48 +0000

    Implmented IsSelectedPID (non yet with AliAODPidHF), additional cuts and removal of daughters from primary vertex (Andrea R, Chiara B)

--- PWG3/vertexingHF/AliRDHFCutsD0toKpi.h
+++ PWG3/vertexingHF/AliRDHFCutsD0toKpi.h
@@ -11,6 +11,9 @@
 
 #include "AliRDHFCuts.h"
 
+class AliAODEvent;
+class AliAODRecoDecayHF;
+
 class AliRDHFCutsD0toKpi : public AliRDHFCuts 
 {
  public:
@@ -25,20 +28,28 @@ class AliRDHFCutsD0toKpi : public AliRDHFCuts
   virtual void GetCutVarsForOpt(AliAODRecoDecayHF *d,Float_t *vars,Int_t nvars,Int_t *pdgdaughters);
 
   using AliRDHFCuts::IsSelected;
-  virtual Int_t IsSelected(TObject* obj,Int_t selectionLevel);
-  
+  virtual Int_t IsSelected(TObject* obj,Int_t selectionLevel) 
+                         {return IsSelected(obj,selectionLevel,0);}
+  virtual Int_t IsSelected(TObject* obj,Int_t selectionLevel,AliAODEvent* aod);
+
   Float_t GetMassCut(Int_t iPtBin=0) const { return (GetCuts() ? fCutsRD[GetGlobalIndex(0,iPtBin)] : 1.e6);}
   Float_t GetDCACut(Int_t iPtBin=0) const { return (GetCuts() ? fCutsRD[GetGlobalIndex(1,iPtBin)] : 1.e6);}
-
+  Int_t CombineSelectionLevels(Int_t selectionvalTrack,Int_t selectionvalCand,Int_t selectionvalPID)const;
   virtual Bool_t IsInFiducialAcceptance(Double_t pt,Double_t y) const;
 
-  virtual Int_t IsSelectedPID(AliAODRecoDecayHF *rd) const;
-
-
+  virtual Int_t IsSelectedPID(AliAODRecoDecayHF *rd);
+  Int_t IsSelectedSpecialCuts(AliAODRecoDecayHF *d) const;
+  void SetUseSpecialCuts(Bool_t useSpecialCuts) {fUseSpecialCuts=useSpecialCuts;}
+  void SetRemoveDaughtersFromPrim(Bool_t removeDaughtersPrim) {fRemoveDaughtersFromPrimary=removeDaughtersPrim;}
+  Bool_t GetUseSpecialCuts() const {return fUseSpecialCuts;}
+  Bool_t GetIsPrimaryWithoutDaughters() const {return fRemoveDaughtersFromPrimary;}
+  
+  
  protected:
+  Bool_t fRemoveDaughtersFromPrimary;      // flag to switch on the removal of duaghters from the primary vertex computation
+  Bool_t fUseSpecialCuts;           // flag to switch on/off special cuts
 
-
-  ClassDef(AliRDHFCutsD0toKpi,1);  // class for cuts on AOD reconstructed D0->Kpi
+  ClassDef(AliRDHFCutsD0toKpi,2);  // class for cuts on AOD reconstructed D0->Kpi
 };
 
 #endif
