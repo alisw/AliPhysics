3c720097f7ed5d45606bbef82b14b628c20f156d
aszostak <aszostak@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-28 14:17:51 +0000

    Fixing warnings

--- HLT/TPCLib/AliHLTTPCModels.h
+++ HLT/TPCLib/AliHLTTPCModels.h
@@ -1,34 +1,44 @@
 // @(#) $Id$
 // Original: AliHLTModels.h,v 1.11 2004/05/17 16:37:19 hristov 
 
+//* This file is property of and copyright by the ALICE HLT Project        * 
+//* ALICE Experiment at CERN, All rights reserved.                         *
+//* See cxx source for full Copyright notice                               *
+
 #ifndef AliHLTTPCModels_H
 #define AliHLTTPCModels_H
 
+/// \author Anders Vestbo or Constantin Loizides
+/// \date 7 Sep 2005
+/// \brief Data structures used for TPC clusters.
+///
+/// Basic structures used by the AliHLTTPCModelTrack class.
+
 #include "AliHLTTPCRootTypes.h"
 
-const Int_t MaxNClusters = 32;
+const Int_t kMaxNClusters = 32;
 
 struct AliHLTTPCClusterModel {
-  Byte_t fPresent;
-  Float_t fDTime;
-  Float_t fDPad;
-  Float_t fDCharge;
-  Float_t fDSigmaY;
-  Float_t fDSigmaZ;
-  UInt_t fNPads;
-  Short_t fSlice;
+  Byte_t fPresent;  // Boolean flag, is the cluster present or not?
+  Float_t fDTime;   // Digit time.
+  Float_t fDPad;    // Digit pad.
+  Float_t fDCharge; // Digit charge.
+  Float_t fDSigmaY; // Sigma in Y.
+  Float_t fDSigmaZ; // Sigma in X.
+  UInt_t fNPads;    // Total number of pafs.
+  Short_t fSlice;   // TPC slice.
 #ifdef do_mc
-  Int_t fTrackID[3];
+  Int_t fTrackID[3];  // Track IDs from MC.
 #endif
 };
 typedef struct AliHLTTPCClusterModel AliHLTTPCClusterModel;
 
 struct AliHLTTPCRemainingCluster {
-  Float_t fPad;   
-  Float_t fTime;  
-  Float_t fSigmaY2;  
-  Float_t fSigmaZ2;  
-  UShort_t fCharge;
+  Float_t fPad;     // Pad value.
+  Float_t fTime;    // Time value.
+  Float_t fSigmaY2; // Square of sigma in Y.
+  Float_t fSigmaZ2; // Square of sigma in X.
+  UShort_t fCharge; // Charge.
 };
 typedef struct AliHLTTPCRemainingCluster AliHLTTPCRemainingCluster;
 
@@ -36,9 +46,9 @@ struct AliHLTTPCRemainingRow {
   Byte_t fPadRow;       //1 byte
   UShort_t fNClusters;  //2 bytes
 #if defined(__HP_aCC) || defined(__DECCXX) || defined(__SUNPRO_CC)
-  AliHLTTPCRemainingCluster fClusters[1];
+  AliHLTTPCRemainingCluster fClusters[1];  // Array of clusters.
 #else
-  AliHLTTPCRemainingCluster fClusters[0];
+  AliHLTTPCRemainingCluster fClusters[0];  // Array of clusters.
 #endif
 };
 typedef struct AliHLTTPCRemainingRow AliHLTTPCRemainingRow;
