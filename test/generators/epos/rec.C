c68728dddb7daa299a981804522031ec67ac1044
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-07-30 13:05:30 +0000

    High multiplicity settings

--- test/generators/epos/rec.C
+++ test/generators/epos/rec.C
@@ -8,6 +8,12 @@ void rec() {
   reco.SetSpecificStorage("GRP/GRP/Data",
 			  Form("local://%s",gSystem->pwd()));
 
+  // High multiplicity settings
+  Double_t cuts[]={33, 0.1, 0.1, 0.05, 0.99, 0.9, 100}; 
+  AliV0vertexer::SetDefaultCuts(cuts); 
+  Double_t cts[]={33., 0.05, 0.008, 0.035, 0.1, 0.9985, 0.9,100}; 
+  AliCascadeVertexer::SetDefaultCuts(cts); 
+
   TStopwatch timer;
   timer.Start();
   reco.Run();
