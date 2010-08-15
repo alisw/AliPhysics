af3537d07242ff44ed0566bd89ad5fa1cd254abe
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-08-15 21:51:12 +0000

    Update for D* task (Alessandro)

--- PWG3/vertexingHF/AliAnalysisTaskSEDStarSpectra.h
+++ PWG3/vertexingHF/AliAnalysisTaskSEDStarSpectra.h
@@ -18,30 +18,19 @@
 #include <TH2F.h>
 #include "TROOT.h"
 #include "TSystem.h"
+
 #include "AliAnalysisTaskSE.h"
-#include "AliAnalysisVertexingHF.h"
 #include "AliAODEvent.h"
 #include "AliPID.h"
+#include "AliRDHFCutsDStartoKpipi.h"
 
-
-class TH2F;
-class TH1I;
-class TParticle;
-class TFile;
-class TClonesArray;
-class AliCFManager;
-class AliAODRecoDecay;
-class AliAODRecoDecayHF2Prong;
-class AliAODMCParticle;
 
 class AliAnalysisTaskSEDStarSpectra : public AliAnalysisTaskSE {
   
  public:
   
   AliAnalysisTaskSEDStarSpectra();
-  AliAnalysisTaskSEDStarSpectra(const Char_t* name);
-  AliAnalysisTaskSEDStarSpectra& operator= (const AliAnalysisTaskSEDStarSpectra& c);
-  AliAnalysisTaskSEDStarSpectra(const AliAnalysisTaskSEDStarSpectra& c);
+  AliAnalysisTaskSEDStarSpectra(const Char_t* name,AliRDHFCutsDStartoKpipi* cuts);
   virtual ~AliAnalysisTaskSEDStarSpectra();
   
   virtual void UserCreateOutputObjects();
@@ -51,24 +40,19 @@ class AliAnalysisTaskSEDStarSpectra : public AliAnalysisTaskSE {
   virtual void Terminate(Option_t *option);
  
   //Background simulation
-  void     SideBandBackground(Int_t ptbin, AliAODRecoCascadeHF *part, Bool_t PIDon, Int_t nSigma, AliAnalysisVertexingHF *vhf, TList *listout);
-  void     WrongSignForDStar(Int_t ptbin, AliAODRecoCascadeHF *part, Bool_t PIDon, Int_t nSigma, AliAnalysisVertexingHF *vhf, TList *listout);
+  void     SideBandBackground(AliAODRecoCascadeHF *part, Bool_t PIDon, Int_t nSigma, AliRDHFCutsDStartoKpipi *cuts, TList *listout);
+  void     WrongSignForDStar(AliAODRecoCascadeHF *part, Bool_t PIDon, Int_t nSigma, AliRDHFCutsDStartoKpipi *cuts, TList *listout);
   //cuts
-  Bool_t   SingleTrackSelections(AliAODRecoDecayHF2Prong* theD0particle, AliAODTrack *track0, AliAODTrack *track1, AliAODTrack *track2);
-  Bool_t   SetSelections(Double_t pt);  
-  Bool_t   SelectPID(AliAODTrack *track, AliPID::EParticleType pid, Double_t nsig);
+  Bool_t   SingleTrackSelections(const AliAODRecoDecayHF2Prong* theD0particle, const AliAODTrack *track2);
+  Bool_t   SelectPID(const AliAODTrack *track, AliPID::EParticleType pid, Double_t nsig);
+  Bool_t   SelectTOFPID(const AliAODRecoDecayHF2Prong* d, const AliAODTrack *tracksoft);
   // histos
-  void   FillSpectrum(Int_t ptbin, AliAODRecoCascadeHF *part, Int_t isDStar, Bool_t PIDon, Int_t nSigma, AliAnalysisVertexingHF *vhf, TList *listout);
+  void   FillSpectrum(AliAODRecoCascadeHF *part, Int_t isDStar, Bool_t PIDon, Int_t nSigma, AliRDHFCutsDStartoKpipi *cuts, TList *listout);
   void     DefineHistograms();
   // set analysis type
   void     SetAnalysisType(Int_t anaType) {fAnalysis = anaType;}
   void     PrintAnalysisType() {printf("Analysis type: %d\n(0: Heidelberg\t1: Utrecht)",fAnalysis);}
-  // set minimum ITS clusters for the analysis
-  void     SetMinITSClusters(Int_t minITSClusters) {fMinITSClusters = minITSClusters;}
-  Int_t    GetMinITSClusters() const {return fMinITSClusters;}
-  // set minimum for soft pion pt
-  void     SetMinITSClustersSoft(Int_t minITSClustersSoft) {fMinITSClustersSoft = minITSClustersSoft;}
-  Int_t    GetMinITSClustersSoft() const {return fMinITSClustersSoft;}
+
   // kaon PID
   void     SetPID(Bool_t usePID) {fPID = usePID;}
   Int_t    GetPID() const {return fPID;}
@@ -83,28 +67,23 @@ class AliAnalysisTaskSEDStarSpectra : public AliAnalysisTaskSE {
   
   Int_t  fEvents;                //  n. of events
   Int_t  fAnalysis;		 //  0: HD;	1: UU;
-  AliAnalysisVertexingHF *fVHF;  //  Set the cuts
-  AliAnalysisVertexingHF *fVHFloose;  //  Set the cuts
   Double_t fD0Window;		 //  select width on D0Mass
   Double_t fPeakWindow;          //  select width on DstarMass
-  Int_t  fMinITSClusters;        //  min n. of ITS clusters for RecoDecay
-  Int_t  fMinITSClustersSoft;    //  min n. of ITS clusters for RecoDecay soft pions
   Bool_t fUseMCInfo;             //  Use MC info
   TList *fOutput;                //!  User output
-  TList *fOutputSpectrum;
-  TList *fOutputAll;
-  TList *fOutputPID3;
-  TList *fOutputPID2;
-  TList *fOutputPID1;
+  TList *fOutputSpectrum;        //!  User output1
+  TList *fOutputAll;             //!  User output2
+  TList *fOutputPID3;            //!  User output3
+  TList *fOutputPID2;            //!  User output4
+  TList *fOutputPID1;            //!  User output5
   Int_t  fNSigma;                //  n sigma for kaon PID
   Bool_t fPID;                   //  PID flag
-  AliAODTrack* fAODTrack;        //!
-  
+  AliRDHFCutsDStartoKpipi *fCuts; // Cuts - sent to output slot 7
   // define the histograms
   TH1F *fCEvents;             //!
   TH2F *fTrueDiff2;           //!
  
-  ClassDef(AliAnalysisTaskSEDStarSpectra,6); // class for D* spectra
+  ClassDef(AliAnalysisTaskSEDStarSpectra,7); // class for D* spectra
 };
 
 #endif
