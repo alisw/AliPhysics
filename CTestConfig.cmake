4620b6c454e7fda2ec03c917356282a949d0ab42
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-08-17 18:10:00 +0000

    Adding tests (Johny)

---
+++ CTestConfig.cmake
@@ -0,0 +1,6 @@
+set(CTEST_PROJECT_NAME "AliRoot")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "alirootbuildcmake.cern.ch")
+set(CTEST_DROP_LOCATION "/~johny/submit.php?project=AliRoot")
+set(CTEST_DROP_SITE_CDASH TRUE)
