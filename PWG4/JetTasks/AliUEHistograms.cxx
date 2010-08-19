348cb2ea5fb92a88a97d65e28b73f87d859db513
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-08-19 07:29:57 +0000

    changed phi calculation

--- PWG4/JetTasks/AliUEHistograms.cxx
+++ PWG4/JetTasks/AliUEHistograms.cxx
@@ -254,7 +254,9 @@ void AliUEHistograms::FillRegion(AliUEHist::Region region, AliUEHist::CFStep ste
     vars[2] = leading->Pt();
     vars[3] = multiplicity;
     vars[4] = leading->Phi() - particle->Phi();
-    
+    if (vars[4]>TMath::Pi()/2.)vars[4]-=TMath::TwoPi();
+    if (vars[4]< - 3.*TMath::Pi()/2.)vars[4]+=TMath::TwoPi();
+
     fNumberDensitypT->GetTrackHist(region)->Fill(vars, step);
     fSumpT->GetTrackHist(region)->Fill(vars, step, particle->Pt());
     
