9371b8da8c1435b5d99f1acd377993dbce4ec075
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-08-20 07:48:45 +0000

    Start translation of the macros helper.C and AliTRDperformanceTrain.h into a class AliTRDPWG1helper. (Markus)
    Reason is the following: Whenever I want to run
    the train with a different configuration (like now with the new trigger)
    and I copy the add macros to a test directory, rootcint doesn't find the
    two macros unless I copy them to the same path. This is something I
    don't like because I have to do the same on the grid. It is better to
    have this compiled inside a class (as static members) so one only has to
    load the library PWG1 and the simply use it.

---
+++ PWG1/TRD/AliTRDpwg1Helper.h
@@ -0,0 +1,48 @@
+#ifndef ALITRDPWG1HELPER_H
+#define ALITRDPWG1HELPER_H
+
+class AliTRDpwg1Helper{
+  public:
+    enum ETRDinfoGenOutSlots {
+      kEventInfo     = 1
+      ,kTracksBarrel  = 2
+      ,kTracksSA      = 3
+      ,kTracksKink    = 4
+      ,kV0List        = 5
+      ,kMonitor       = 6
+      ,kNOutSlots     = 7
+    };
+
+    enum ETRDrecoTasks{
+      kCheckESD      = 0
+      ,kInfoGen       = 1
+      ,kCheckDET      = 2
+      ,kEfficiency    = 3
+      ,kResolution    = 4
+      ,kCheckPID      = 5
+      ,kV0Monitor     = 6 
+      ,kCalibration   = 7
+      ,kEfficiencyMC  = 8
+      ,kAlignment     = 9
+      ,kPIDRefMaker   =10
+      ,kClErrParam    =11
+      ,kMultiplicity  =12
+    };
+
+    enum{
+      kNTRDQATASKS = 7,
+      kNTRDCALIBTASKS = 6,
+      kNTRDTASKS = kNTRDQATASKS + kNTRDCALIBTASKS
+    };
+    static const Char_t * fgkTRDtaskClassName[kNTRDTASKS];
+    static const Char_t * fgkTRDtaskOpt[kNTRDTASKS + 1];
+
+    static Int_t ParseOptions(Char_t *trd);
+    static void MergeProd(const Char_t *mark, const Char_t *files, Int_t nBatch);
+    static Bool_t HasReadMCData(Char_t *opt);
+    static Bool_t HasReadFriendData(Char_t *opt);
+
+    AliTRDpwg1Helper();
+    ~AliTRDpwg1Helper();
+};
+#endif
