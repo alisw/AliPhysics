f706e295e4c6a02b517892775947a6bfa58ccae6
rpreghen <rpreghen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-09-08 09:00:51 +0000

    minor change in macro

--- TOF/MakeCDBEntryRunParams.C
+++ TOF/MakeCDBEntryRunParams.C
@@ -1,11 +1,11 @@
-MakeCDBEntryRunParams(Int_t startRun = 0, Int_t endRun = AliCDBRunRange::Infinity())
+MakeCDBEntryRunParams(Float_t time0, Float_t reso, Float_t spread = -1., Int_t startRun = 0, Int_t endRun = AliCDBRunRange::Infinity())
 {
 
   const Int_t nPoints = 1;
   UInt_t timestamp[nPoints] = {0.};
-  Float_t t0[nPoints] = {0.};
-  Float_t tofReso[nPoints] = {80.};
-  Float_t t0Spread[nPoints] = {0.};
+  Float_t t0[nPoints] = {time0};
+  Float_t tofReso[nPoints] = {reso};
+  Float_t t0Spread[nPoints] = {spread};
 
   /* create object */
   AliTOFRunParams *obj = new AliTOFRunParams(1);
