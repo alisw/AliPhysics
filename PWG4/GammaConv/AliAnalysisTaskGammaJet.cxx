ca8e5ecb0c4f4e4339dbfece8a91bfe5b4608e59
slindal <slindal@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-09-09 13:06:52 +0000

    Updated gamma jet task

--- PWG4/GammaConv/AliAnalysisTaskGammaJet.cxx
+++ PWG4/GammaConv/AliAnalysisTaskGammaJet.cxx
@@ -20,22 +20,43 @@
 
 #include <iostream>
 
-// example of an analysis task creating a p_t spectrum
-// Authors: Panos Cristakoglou, Jan Fiete Grosse-Oetringhaus, Christian Klein-Boesing
-// Reviewed: A.Gheata (19/02/10)
+// Gamma - jet correlation analysis task
+// Authors: Svein Lindal
+
 
 using namespace std;
 
 ClassImp(AliAnalysisTaskGammaJet)
 
 //________________________________________________________________________
+AliAnalysisTaskGammaJet::AliAnalysisTaskGammaJet() : AliAnalysisTaskSE(), 
+  fOutputList(NULL), 
+  fHistPt(NULL),
+  fHistPtPhos(NULL),
+  fHistPtEmcal(NULL),
+  fHistPtJets(NULL),
+  fHistGammaJets(NULL),
+  fDeltaAODFileName("")
+{
+  // Constructor
+
+  // Define input and output slots here
+  // Input slot #0 works with a TChain
+  //DefineInput(0, TChain::Class());
+  // Output slot #0 id reserved by the base class for AOD
+  // Output slot #1 writes into a TH1 container
+  //DefineOutput(1, TList::Class());
+}
+
+
+//________________________________________________________________________
 AliAnalysisTaskGammaJet::AliAnalysisTaskGammaJet(const char *name) : AliAnalysisTaskSE(name), 
-  fESD(0), 
   fOutputList(0), 
   fHistPt(0),
   fHistPtPhos(0),
   fHistPtEmcal(0),
   fHistPtJets(0),
+  fHistGammaJets(NULL),
   fDeltaAODFileName("")
 {
   // Constructor
@@ -76,11 +97,14 @@ void AliAnalysisTaskGammaJet::UserCreateOutputObjects()
   fHistPtJets->GetXaxis()->SetTitle("P_{T} (GeV/c)");
   fHistPtJets->GetYaxis()->SetTitle("dN/dP_{T} (c/GeV)");
   fHistPtJets->SetMarkerStyle(kFullCircle);
+
+  fHistGammaJets = new TH1F("fHistGammaJets", "fHistGammaJets", 200, -TMath::Pi(), 2*TMath::Pi());
   
   fOutputList->Add(fHistPt);
   fOutputList->Add(fHistPtPhos);
   fOutputList->Add(fHistPtEmcal);
   fOutputList->Add(fHistPtJets);
+  fOutputList->Add(fHistGammaJets);
 }
 
 //________________________________________________________________________
@@ -90,66 +114,98 @@ void AliAnalysisTaskGammaJet::UserExec(Option_t *)
   // Called for each event
 
 
+  printf("in userexec \n");
 
-  fESD = dynamic_cast<AliESDEvent*>(InputEvent());
-  if (!fESD) {
-    printf("ERROR: fESD not available\n");
+  // fESD = dynamic_cast<AliESDEvent*>(InputEvent());
+  // if (!fESD) {
+  //   printf("ERROR: fESD not available\n");
+  //   //return;
+  //   AliAODEvent * fAOD = dynamic_cast<AliAODEvent*>(InputEvent());
+
+  // AliAODEvent * aodEvent = AODEvent();
+
+  //AliESDEvent * esdEvent = dynamic_cast<AliESDEvent*>(InputEvent());
+  
+  
+  AliAODEvent * aodEvent = GetAODEvent();
+  if(!aodEvent) {
+    cout << "No AOD event!!"<<endl;
+    return;
+  }
+  
+  //Get Conversion gamma branch of AOD. 
+  TClonesArray * convGamma = dynamic_cast<TClonesArray*>(aodEvent->FindListObject("GammaConv"));
+  if(!convGamma)
+    convGamma = GetConversionGammas();
+
+  if(!convGamma) {
+    printf("No convgamma");
     return;
   }
 
-  TClonesArray * convGamma = NULL;
-  AliAODEvent * aodEvent = AODEvent();
+  TClonesArray * jets = aodEvent->GetJets();
   
-  if(!aodEvent) return;
-
-  //Get Conversion gamma branch of AOD. 
-  if(fDeltaAODFileName.Length() > 0) 
-    convGamma = GetConversionGammas();
-  else
-    convGamma = dynamic_cast<TClonesArray*>(aodEvent->FindListObject("GammaConv"));
-
-  if(convGamma) {
-    for (Int_t iPhot = 0; iPhot < convGamma->GetEntriesFast(); iPhot++) {
-      AliGammaConversionAODObject * aodO = dynamic_cast<AliGammaConversionAODObject*>(convGamma->At(iPhot));
-      if (!aodO) {
-	//printf("ERROR: Could not receive ga %d\n", iPhot);
-	continue;
-      }
-      fHistPt->Fill(aodO->E());
-    } 
-  }
-
-  if(fESD) {
-    for(int icl = 0; icl < fESD->GetNumberOfCaloClusters(); icl++) {
-      AliESDCaloCluster * cluster = fESD->GetCaloCluster(icl);
-      if(cluster->GetEmcCpvDistance() > 10) {
-	if (cluster->IsPHOS()) fHistPtPhos->Fill(cluster->E());
-	if (cluster->IsEMCAL()) fHistPtEmcal->Fill(cluster->E());
-      }
+  for (Int_t iPhot = 0; iPhot < convGamma->GetEntriesFast(); iPhot++) {
+    AliGammaConversionAODObject * aodO = dynamic_cast<AliGammaConversionAODObject*>(convGamma->At(iPhot));
+    if (!aodO) {
+      printf("ERROR: Could not receive ga %d\n", iPhot);
+      continue;
     }
-  }
-
-  if(aodEvent) {
-    TClonesArray * jets = aodEvent->GetJets();
+    
+    //if(aodO->E() < 0.2) continue;
+    
     if (jets) {
       for(int ij = 0; ij < jets->GetEntriesFast(); ij++) {
 	AliAODJet * jet = aodEvent->GetJet(ij);
 	if(jet) {
+	  //cout << jet->E() << endl;
 	  fHistPtJets->Fill(jet->E());
+	  cout << jet->Phi() << " " << aodO->Phi() << endl;
+	  fHistGammaJets->Fill(jet->Phi() - aodO->Phi());
+	  
 	}
       }
     }
-  }
     
-  PostData(1, fOutputList);
-}      
+    
+    fHistPt->Fill(aodO->E());
+  } 
 
+
+  // if(esdEvent) {
+  //   for(int icl = 0; icl < fESD->GetNumberOfCaloClusters(); icl++) {
+  //     AliESDCaloCluster * cluster = fESD->GetCaloCluster(icl);
+  //     if(cluster->GetEmcCpvDistance() > 10) {
+  // 	if (cluster->IsPHOS()) fHistPtPhos->Fill(cluster->E());
+  // 	if (cluster->IsEMCAL()) fHistPtEmcal->Fill(cluster->E());
+  //     }
+  //   }
+  // }
+
+
+  
+  PostData(1, fOutputList);
+        
+}
 //________________________________________________________________________
 void AliAnalysisTaskGammaJet::Terminate(Option_t *) 
 {
   // Draw result to the screen
   // Called once at the end of the query
 }
+
+//________________________________________________________________________
+AliAODEvent * AliAnalysisTaskGammaJet::GetAODEvent() {
+  //Get the AOD event from whereever it might be
+  AliAODEvent * aodEvent = dynamic_cast<AliAODEvent*>(InputEvent());
+  if(!aodEvent) {
+    aodEvent = AODEvent();
+  }
+
+  return aodEvent;
+
+}
+
 
 //________________________________________________________________________
 TClonesArray * AliAnalysisTaskGammaJet::GetConversionGammas() {
