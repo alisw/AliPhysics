d05285d945f148f388980627130735742cbfc9c0
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-09-14 10:45:28 +0000

    adding small script to merge grid files

---
+++ PWG2/FORWARD/analysis/mergescript.C
@@ -0,0 +1,42 @@
+alienmerge(const char* path, const char* pattern, const char* outfile=0, Int_t nFiles = 10000){
+  //gSystem->Load("libVMC.so");
+  
+  TGrid::Connect("alien://",0,0,"t");
+  TGridResult* result = gGrid->Query(path,pattern);
+  result->Print();
+  
+  TFileMerger m;
+  
+   if (outfile) m.OutputFile(outfile);
+  Int_t i=0;
+  
+  TObjArray* outArray = new TObjArray();
+  //TList list;
+  
+  while (result->GetKey(i,"turl") && i<nFiles) {
+    // TAlienFile* file = TAlienFile::Open(result->GetKey(i,"turl"),"READ");
+    //file->ls();
+    // TList* list = (TList*)file->Get("listOfhists");
+
+    
+    // std::cout << list;// << std::endl;
+    
+    //
+    //    TObjArray* tmp = (TObjArray*)file.Get("FMD");
+    // list.Add(tmp);
+    
+    //    if(i!=blacklist1 && i!=blacklist2) {
+      m.AddFile(result->GetKey(i,"turl"));
+      cout<<i<<"   "<<result->GetKey(i,"turl")<<endl;
+      
+      // }
+      i++;
+  }
+  if (i)
+    m.Merge();
+  
+  //outArray->Merge(list);
+  //TFile fout("MergeTest.root","RECREATE");
+  //outArray->Write();
+  //fout.Close();
+}
