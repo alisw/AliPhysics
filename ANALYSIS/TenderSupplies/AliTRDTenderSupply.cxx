71f5992d38ef073c412612a8ee47d074cabf56a0
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-09-16 19:02:07 +0000

    - recalibration of the chamber gain - take out chambers which the user declares as bad

--- ANALYSIS/TenderSupplies/AliTRDTenderSupply.cxx
+++ ANALYSIS/TenderSupplies/AliTRDTenderSupply.cxx
@@ -20,6 +20,16 @@
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <TChain.h>
+#include <TList.h>
+#include <TObjString.h>
+#include <TTree.h>
+#include <TString.h>
+
+#include <AliCDBEntry.h>
+#include <AliCDBId.h>
+#include <AliCDBManager.h>
+#include <AliTRDCalDet.h>
 
 #include <AliLog.h>
 #include <TTree.h>
@@ -36,11 +46,21 @@
 
 #include "AliTRDTenderSupply.h"
 
+ClassImp(AliTRDTenderSupply)
 
+//_____________________________________________________
 AliTRDTenderSupply::AliTRDTenderSupply() :
   AliTenderSupply(),
-  fESDpid(0x0),
-  fPIDmethod(k1DLQpid)
+  fESD(NULL),
+  fESDpid(NULL),
+  fChamberGainOld(NULL),
+  fChamberGainNew(NULL),
+  fChamberVdriftOld(NULL),
+  fChamberVdriftNew(NULL),
+  fPIDmethod(k1DLQpid),
+  fPthreshold(0.8),
+  fNBadChambers(0),
+  fGainCorrection(kTRUE)
 {
   //
   // default ctor
@@ -50,14 +70,24 @@ AliTRDTenderSupply::AliTRDTenderSupply() :
 //_____________________________________________________
 AliTRDTenderSupply::AliTRDTenderSupply(const char *name, const AliTender *tender) :
   AliTenderSupply(name,tender),
-  fESDpid(0x0),
-  fPIDmethod(k1DLQpid)
+  fESD(NULL),
+  fESDpid(NULL),
+  fChamberGainOld(NULL),
+  fChamberGainNew(NULL),
+  fChamberVdriftOld(NULL),
+  fChamberVdriftNew(NULL),
+  fPIDmethod(k1DLQpid),
+  fPthreshold(0.8),
+  fNBadChambers(0),
+  fGainCorrection(kTRUE)
 {
   //
   // named ctor
   //
+  memset(fBadChamberID, 0, sizeof(Int_t) * kNChambers);
 }
 
+//_____________________________________________________
 AliTRDTenderSupply::~AliTRDTenderSupply()
 {
   //
@@ -72,9 +102,6 @@ void AliTRDTenderSupply::Init()
   // Initialise TRD tender
   //
 
-  //
-  // Set event information
-  //
   AliAnalysisManager *mgr=AliAnalysisManager::GetAnalysisManager();
   
   // 1DLQ PID implemented in the AliESD object
@@ -86,13 +113,12 @@ void AliTRDTenderSupply::Init()
   // Set Normalisation Factors
   if(mgr->GetMCtruthEventHandler()){
     // Assume MC
-    fESDpid->GetTRDResponse().SetGainNormalisationFactor(1.8315);
+    SwitchOffGainCorrection();
   }
   else{
     // Assume Data
-    fESDpid->GetTRDResponse().SetGainNormalisationFactor(1.14);
+    SwitchOnGainCorrection();
   }
-  
 }
 
 //_____________________________________________________
@@ -101,21 +127,216 @@ void AliTRDTenderSupply::ProcessEvent()
   //
   // Reapply pid information
   //
+  if (fTender->RunChanged()){
+    AliDebug(0, Form("AliTPCTenderSupply::ProcessEvent - Run Changed (%d)\n",fTender->GetRun()));
+    if (fGainCorrection) SetChamberGain();
+  }
 
-
-  AliESDEvent *event=fTender->GetEvent();
-  if (!event) return;
-  Int_t ntracks=event->GetNumberOfTracks();
-  
+  fESD = fTender->GetEvent();
+  if (!fESD) return;
+  Int_t ntracks=fESD->GetNumberOfTracks();
+   
   //
   // recalculate PID probabilities
   //
-  switch(fPIDmethod){
-    case k1DLQpid:
-      for(Int_t itrack = 0; itrack < ntracks; itrack++) 
-        fESDpid->MakeTRDPID(event->GetTrack(itrack));
-      break;
-    default:
-      AliError("PID Method not implemented (yet)");
+  for(Int_t itrack = 0; itrack < ntracks; itrack++){
+    AliESDtrack *track=fESD->GetTrack(itrack);
+    if(!(track->GetStatus() & AliESDtrack::kTRDout)) continue;
+    if(fGainCorrection) ApplyGainCorrection(track);
+    fESDpid->MakeTRDPID(fESD->GetTrack(itrack));
   }
 }
+
+//_____________________________________________________
+void AliTRDTenderSupply::SetChamberGain(){
+  //
+  // Load Chamber Gain factors into the Tender supply
+  //
+  
+  //find previous entry from the UserInfo
+  TTree *tree=((TChain*)fTender->GetInputData(0))->GetTree();
+  if (!tree) {
+  AliError("Tree not found in ESDhandler");
+    return;
+  }
+ 	 
+  TList *userInfo=(TList*)tree->GetUserInfo();
+  if (!userInfo) {
+    AliError("No UserInfo found in tree");
+    return;
+  }
+
+  TList *cdbList=(TList*)userInfo->FindObject("cdbList");
+  if (!cdbList) {
+    AliError("No cdbList found in UserInfo");
+    if (AliLog::GetGlobalLogLevel()>=AliLog::kError) userInfo->Print();
+    return;
+  }
+ 	
+  TIter nextCDB(cdbList);
+  TObjString *os=0x0;
+  while ( (os=(TObjString*)nextCDB()) ){
+    if(os->GetString().Contains("TRD/Calib/ChamberGainFactor")){
+      // Get Old gain calibration
+      AliCDBId *id=AliCDBId::MakeFromString(os->GetString());
+ 	   
+      AliCDBEntry *entry=fTender->GetCDBManager()->Get(*id);
+      if (!entry) {
+        AliError("No previous gain calibration entry found");
+        return;
+      }
+
+      fChamberGainOld = dynamic_cast<AliTRDCalDet *>(entry->GetObject());
+ 	   
+      AliDebug(1, Form("Used old Gain entry: %s\n",entry->GetId().ToString().Data()));
+    } else if(os->GetString().Contains("TRD/Calib/ChamberVdrift")){
+      // Get Old drift velocity calibration
+      AliCDBId *id=AliCDBId::MakeFromString(os->GetString());
+ 	   
+      AliCDBEntry *entry=fTender->GetCDBManager()->Get(*id);
+      if (!entry) {
+        AliError("No previous drift velocity calibration entry found");
+        return;
+      }
+
+      fChamberVdriftOld = dynamic_cast<AliTRDCalDet *>(entry->GetObject());
+ 	   
+      AliDebug(1, Form("Used old Drift velocity entry: %s\n",entry->GetId().ToString().Data()));
+    
+    }
+  }
+
+  // Get Latest Gain Calib Object
+  AliCDBEntry *entryNew=fTender->GetCDBManager()->Get("TRD/Calib/ChamberGainFactor",fTender->GetRun());
+  if (entryNew) {
+    AliDebug(1, Form("Used new Gain entry: %s\n",entryNew->GetId().ToString().Data()));
+    fChamberGainNew = dynamic_cast<AliTRDCalDet *>(entryNew->GetObject());
+  } else
+    AliError("No new gain calibration entry found");
+  
+  // Also get the latest Drift Velocity calibration object
+  entryNew=fTender->GetCDBManager()->Get("TRD/Calib/ChamberVdrift",fTender->GetRun());
+  if (entryNew) {
+    AliDebug(1, Form("Used new Drift velocity entry: %s\n",entryNew->GetId().ToString().Data()));
+    fChamberVdriftNew = dynamic_cast<AliTRDCalDet *>(entryNew->GetObject());
+  } else
+    AliError("No new drift velocity calibration entry found");
+
+  if(!fChamberGainNew || !fChamberVdriftNew) AliError("No recent calibration found");
+}
+
+//_____________________________________________________
+void AliTRDTenderSupply::ApplyGainCorrection(AliESDtrack * track){
+  //
+  // Apply new gain factors to the track
+  //
+  if(!fChamberGainNew || !fChamberGainOld){
+    AliError("Cannot apply gain correction.");
+    return;
+  }
+ 
+  if(!(track->GetStatus() & AliESDtrack::kTRDout)) return;
+  Double_t p = track->GetOuterParam() ? track->GetOuterParam()->P() : track->P();
+  if(p < fPthreshold) return; // Apply low momentum cutoff
+
+  Bool_t applyCorrectionVdrift = kFALSE;
+  if(fChamberVdriftOld && fChamberVdriftNew) applyCorrectionVdrift = kTRUE;
+
+  Int_t chamberID[kNPlanes]; 
+  for(Int_t il = 0; il < kNPlanes; il++) chamberID[il] = -1;
+  if(!GetTRDchamberID(track, chamberID)) return;
+  Int_t nTrackletsPID = 0, nTracklets = track->GetTRDntracklets();
+  for(Int_t iplane = 0; iplane < kNPlanes; iplane++){
+    if(chamberID[iplane] < 0) continue;
+
+    // Mask out bad chambers
+    Bool_t isMasked = kFALSE;
+    for(UInt_t icam = 0; icam < fNBadChambers; icam++)
+      if(fBadChamberID[icam] == chamberID[iplane]){
+        isMasked = kTRUE;
+        break;
+      }
+    if(isMasked){
+      for(Int_t islice = 0; islice < track->GetNumberOfTRDslices(); islice++){
+        track->SetTRDslice(0, iplane, islice);
+      }
+      continue;
+    }
+
+    // Take old and new gain factor and make ratio
+    Double_t facOld = fChamberGainOld->GetValue(chamberID[iplane]);
+    Double_t facNew = fChamberGainNew->GetValue(chamberID[iplane]); 
+    Double_t correction = facNew/facOld;
+    if(applyCorrectionVdrift){
+      // apply also correction for drift velocity calibration
+      Double_t vDriftOld = fChamberVdriftOld->GetValue(chamberID[iplane]);
+      Double_t vDriftNew = fChamberVdriftNew->GetValue(chamberID[iplane]);
+      correction *= vDriftNew/vDriftOld;
+    }
+    AliDebug(2, Form("Applying correction factor %f\n", correction));
+    Int_t nSlices = 0;
+    for(Int_t islice = 0; islice < track->GetNumberOfTRDslices(); islice++){
+      Double_t qslice = track->GetTRDslice(iplane, islice);
+      if(qslice <= 0.) continue; 
+      track->SetTRDslice(qslice / correction, iplane, islice);
+      nSlices++;
+    }
+    if(nSlices) nTrackletsPID++;
+  }
+  // Use nTrackletsPID to indicate the number of tracklets from good
+  // chambers so they are used for the PID
+  track->SetTRDntracklets(nTrackletsPID | (nTracklets << 3));
+}
+
+//_____________________________________________________
+Bool_t AliTRDTenderSupply::GetTRDchamberID(AliESDtrack * const track, Int_t *detectors) {
+  //
+  // Calculate TRD chamber ID
+  //
+  Double_t xLayer[kNPlanes] = {300.2, 312.8, 325.4, 338., 350.6, 363.2};
+  Double_t etamin[kNStacks] = {0.536, 0.157, -0.145, -0.527,-0.851};
+  Double_t etamax[kNStacks] = {0.851, 0.527, 0.145, -0.157,-0.536};
+  for(Int_t ily = 0; ily < kNPlanes; ily++) detectors[ily] = -1;
+
+  const AliExternalTrackParam *trueparam = NULL;
+  if(track->GetTPCInnerParam()) trueparam = track->GetTPCInnerParam();
+  else if(track->GetOuterParam()) trueparam = track->GetOuterParam();
+  else if(track->GetInnerParam()) trueparam = track->GetInnerParam();
+  if(!trueparam){
+    AliDebug(2, "No Track Params");
+    return kFALSE;
+  }
+
+  AliExternalTrackParam workparam(*trueparam); // Do calculation on working Copy
+  Double_t pos[3];
+  Int_t nDet = 0;
+  for(Int_t ily = 0; ily < kNPlanes; ily++){
+    if(!workparam.PropagateTo(xLayer[ily], fESD->GetMagneticField())) {
+      AliDebug(2, "Propagation failed");
+      break;
+    }
+    workparam.GetXYZ(pos);
+    Double_t trackAlpha = TMath::ATan2(pos[1], pos[0]);
+    if(trackAlpha < 0) trackAlpha = 2 * TMath::Pi() + trackAlpha;
+    Double_t secAlpha = 2 * TMath::Pi() / 18.;
+   
+    Int_t sector = static_cast<Int_t>(trackAlpha/secAlpha);
+    Double_t etaTrack = track->Eta();
+    Int_t stack = -1;
+    for(Int_t istack = 0; istack < 5; istack++){
+      if(etaTrack >= etamin[istack] && etaTrack <= etamax[istack]){
+        stack = istack;
+        break;
+      }
+    }
+    if(stack < 0) {
+      AliDebug(2, "Dead Area");
+      continue;
+    }
+
+    detectors[ily] = sector * kNStacks * kNPlanes + stack * kNPlanes + ily;
+    nDet++;
+  }
+  return nDet ? kTRUE : kFALSE;
+}
+
--- ANALYSIS/TenderSupplies/AliTRDTenderSupply.h
+++ ANALYSIS/TenderSupplies/AliTRDTenderSupply.h
@@ -14,35 +14,59 @@
 
 #include <AliTenderSupply.h>
 
+class AliTRDCalDet;
+
 class AliTRDTenderSupply: public AliTenderSupply {
   
 public:
-  AliTRDTenderSupply();
-  AliTRDTenderSupply(const char *name, const AliTender *tender=NULL);
-  virtual ~AliTRDTenderSupply();
-
-  void SetPIDmethod(Int_t pidMethod) { fPIDmethod = pidMethod; }
-  
-  virtual void              Init();
-  virtual void              ProcessEvent();
-  
-  
-private:
   enum{
     kNNpid = 0,
     k1DLQpid = 1,
     k2DLQpid = 2
   };
-  AliESDpid          *fESDpid;       //! ESD PID object
+  AliTRDTenderSupply();
+  AliTRDTenderSupply(const char *name, const AliTender *tender=NULL);
+  virtual ~AliTRDTenderSupply();
+
+  void SetPIDmethod(Int_t pidMethod) { fPIDmethod = pidMethod; }
+  void SetCalibLowpThreshold(Double_t pmin) { fPthreshold = pmin; };
+  
+  virtual void              Init();
+  virtual void              ProcessEvent();
+  
+  void SwitchOnGainCorrection() { fGainCorrection = kTRUE; }
+  void SwitchOffGainCorrection() { fGainCorrection = kFALSE; }
+  void AddBadChamber(Int_t chamberID){fBadChamberID[fNBadChambers++] = chamberID;};
+  
+private:
+  enum{
+    kNPlanes = 6,
+    kNStacks = 5,
+    kNChambers = 540
+  };
+
+  Bool_t GetTRDchamberID(AliESDtrack * const track, Int_t *detectors);
+  void SetChamberGain();
+  void ApplyGainCorrection(AliESDtrack *track);
+  
+  AliESDEvent           *fESD;       //! the ESD Event
+  AliESDpid             *fESDpid;    //! ESD PID object
+
+  AliTRDCalDet *fChamberGainOld;     // TRD Chamber Gain Factor used for producing the ESD
+  AliTRDCalDet *fChamberGainNew;     // New TRD Chamber Gain Factor
+  AliTRDCalDet *fChamberVdriftOld;   // Old drift velocity calibration
+  AliTRDCalDet *fChamberVdriftNew;   // New drift velocity calibration
 
   Int_t fPIDmethod;                  // PID method
+  Double_t fPthreshold;              // Low Momentum threshold for calibration
+  Int_t fBadChamberID[kNChambers];   // List of Bad Chambers
+  UInt_t fNBadChambers;              // Number of bad chambers
+  Bool_t fGainCorrection;            // Apply gain correction 
   
   AliTRDTenderSupply(const AliTRDTenderSupply&c);
   AliTRDTenderSupply& operator= (const AliTRDTenderSupply&c);
   
   ClassDef(AliTRDTenderSupply, 1);  // TRD tender task
 };
-
-
 #endif
 
