5345ab440f9c58c31f3d5a2ded816ea8ae239aa3
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-09-19 06:18:39 +0000

    Cleanup

--- PWG4/TwoPartCorr/AutoCorr.h
+++ PWG4/TwoPartCorr/AutoCorr.h
@@ -1,23 +1,23 @@
-// $Id:$
+// $Id$
 
 #ifndef AutoCorr_h
 #define AutoCorr_h
 
+#include <Rtypes.h>
 #include <Riostream.h>
-#include <TTree.h>
-#include <TChain.h>
-#include <TCanvas.h>
-#include <TClonesArray.h>
-#include <TH1F.h>
-#include <TH2F.h>
-#include "Rtypes.h"
-#include "TMath.h"
-#include "TFile.h"
-#include "TSystem.h"
-#include "TROOT.h"
+//#include <TTree.h>
+//#include <TChain.h>
+//#include <TCanvas.h>
+//#include <TClonesArray.h>
+//#include <TH1F.h>
+//#include <TH2F.h>
+#include <TMath.h>
+//#include "TFile.h"
+//#include "TSystem.h"
+//#include "TROOT.h"
 #include <vector>
-#include <deque>
-#include "TRandom.h"
+//#include <deque>
+#include <TRandom.h>
 #include "TreeClasses.h"
 #include "EventPool.h"
 
@@ -27,27 +27,35 @@ public:
   AutoCorr(){;}
   ~AutoCorr(){;}
   
-  // RangeMin,Max specify periodic boundaries
-  Double_t DeltaPhi(MyPart* t1, MyPart* t2, 
-		    double rangeMin = -TMath::Pi()/2, 
-		    double rangeMax = 3*TMath::Pi()/2);
-  Double_t DeltaEta(MyPart* t1, MyPart* t2);  
-  Bool_t IsTrackOk(MyPart* t);
-  Bool_t IsPairOk(MyPart* t1, MyPart* t2);
-  Bool_t IsMixedPairOk(MyPart* t1, MyPart* t2);
-//  Bool_t IsEventOk(MyHeader *h);
+  
+  Double_t   DeltaPhi(const MyPart &t1, const MyPart &t2, 
+                      Double_t rangeMin = -TMath::Pi()/2, 
+                      Double_t rangeMax = 3*TMath::Pi()/2)      const;
+  Double_t   DeltaEta(const MyPart &t1, const MyPart &t2)       const;  
+  Bool_t     IsMixedPairOk(const MyPart &t1, const MyPart &t2)  const;
+  Bool_t     IsPairOk(const MyPart &t1, const MyPart &t2)       const;
+  Bool_t     IsTrackOk(const MyPart &t, 
+                       Double_t etaMin, Double_t etaMax)        const;
+  Bool_t     IsTrackOk(const MyPart &t, 
+                     Double_t etaMin, Double_t etaMax,
+                       Double_t ptMin, Double_t ptMax)          const;
+  Double_t   InvMass(const MyPart &p1, const MyPart &p2)        const;
+  Bool_t     IsEventOk(const MyHeader &ev, Int_t minVc, 
+                       Int_t maxNTracklets, Double_t zMin,
+                       Double_t zMax)                           const;
+  Bool_t     InBounds(Double_t val, Double_t min, Double_t max) const;
+  Bool_t     InBounds(Int_t val, Int_t min, Int_t max)          const;
 
-  Int_t InitEventPools(Int_t depth, 
-		       Int_t nmultbins, Double_t multbins[], 
-		       Int_t nzvtxbins, Double_t zvtxbins[]);
+  EventPool* GetEventPool(Int_t iMult, Int_t iZvtx)             const;
+  Int_t      InitEventPools(Int_t depth, 
+                            Int_t nmultbins, Double_t multbins[], 
+                            Int_t nzvtxbins, Double_t zvtxbins[]);
+  Int_t      UpdatePools(int iEvent, MyHeader* ev, TClonesArray* trk);
 
-  Int_t UpdatePools(int iEvent, MyHeader* ev, TClonesArray* trk);
-  EventPool* GetEventPool(int iMult, int iZvtx);// { return fEvPool.at(iMult).at(iZvtx); }
 protected:
-  Int_t fNMultBins;
-  Int_t fNZvtxBins;
-
-  std::vector<std::vector<EventPool*> > fEvPool; // [fMultBin][fZvtxBin]
+  Int_t      fNMultBins;                         // mult bins
+  Int_t      fNZvtxBins;                         // vertex bins
+  std::vector<std::vector<EventPool*> > fEvPool; // pool in bins of [fMultBin][fZvtxBin]
 
   ClassDef(AutoCorr,1)
 };
