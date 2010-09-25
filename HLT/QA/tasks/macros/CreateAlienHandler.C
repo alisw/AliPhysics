e8b5a89f428a56e193b4807ac76eb214b7f80ab5
kkanaki <kkanaki@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-09-25 14:41:56 +0000

    - updated aliroot and root version (Hege)

--- HLT/QA/tasks/macros/CreateAlienHandler.C
+++ HLT/QA/tasks/macros/CreateAlienHandler.C
@@ -23,8 +23,8 @@ AliAnalysisGrid* CreateAlienHandler(TString runNumber, TString dataDir, TString 
   
   // check the versions available on alien with the command 'packages'
   plugin->SetAPIVersion("V1.1x");
-  plugin->SetROOTVersion("v5-26-00b-6");
-  plugin->SetAliROOTVersion("v4-20-01-AN");
+  plugin->SetROOTVersion("v5-27-05-build1");
+  plugin->SetAliROOTVersion("v4-20-09-AN-build1");
 
   //Allow non-default outputs
   plugin->SetDefaultOutputs(kFALSE);
