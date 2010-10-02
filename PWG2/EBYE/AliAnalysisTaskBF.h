da37228528408b28e62dc8490ed37614f3c1ccd8
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-10-02 21:48:12 +0000

    Including the physics selection and moving to AliAnalysisTaskSE

--- PWG2/EBYE/AliAnalysisTaskBF.h
+++ PWG2/EBYE/AliAnalysisTaskBF.h
@@ -4,37 +4,37 @@
 // Analysis task for the BF code
 // Authors: Panos Cristakoglou@cern.ch
 
+class TList;
+class TH1F;
+
 class AliBalance;
 class AliESDEvent;
 class AliAODEvent;
 class AliMCEvent;
 
-#include "AliAnalysisTask.h"
+#include "AliAnalysisTaskSE.h"
 
-class AliAnalysisTaskBF : public AliAnalysisTask {
+class AliAnalysisTaskBF : public AliAnalysisTaskSE {
  public:
   AliAnalysisTaskBF(const char *name = "AliAnalysisTaskBF");
   virtual ~AliAnalysisTaskBF() {}
   
-  virtual void   ConnectInputData(Option_t *);
-  virtual void   CreateOutputObjects();
-  virtual void   Exec(Option_t *option);
+  virtual void   UserCreateOutputObjects();
+  virtual void   UserExec(Option_t *option);
   virtual void   Terminate(Option_t *);
 
   void SetAnalysisObject(AliBalance *const analysis) {
     fBalance = analysis;}
   
  private:
-  AliESDEvent *fESD;    //ESD object
-  AliAODEvent *fAOD;    //AOD object
-  AliMCEvent  *fMC;     //MC object
-
   AliBalance *fBalance; //BF object
-   
+  TList *fList; //fList object
+  TH1F *fHistEventStats; //event stats
+
   AliAnalysisTaskBF(const AliAnalysisTaskBF&); // not implemented
   AliAnalysisTaskBF& operator=(const AliAnalysisTaskBF&); // not implemented
   
-  ClassDef(AliAnalysisTaskBF, 1); // example of analysis
+  ClassDef(AliAnalysisTaskBF, 2); // example of analysis
 };
 
 #endif
