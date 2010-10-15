70726ede254a58fb5fe16d26a5b5d343041b932d
belikov <belikov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-10-15 12:48:27 +0000

    Reserving a status bit to indicate a possible mismatch with the TOF (F. Noferini)

--- STEER/AliESDtrack.h
+++ STEER/AliESDtrack.h
@@ -47,6 +47,7 @@ public:
     kTPCin=0x0010,kTPCout=0x0020,kTPCrefit=0x0040,kTPCpid=0x0080,
     kTRDin=0x0100,kTRDout=0x0200,kTRDrefit=0x0400,kTRDpid=0x0800,
     kTOFin=0x1000,kTOFout=0x2000,kTOFrefit=0x4000,kTOFpid=0x8000,
+    kTOFmismatch=0x100000,
     kHMPIDout=0x10000,kHMPIDpid=0x20000,
     kEMCALmatch=0x40000,
     kPHOSmatch=0x200000,
