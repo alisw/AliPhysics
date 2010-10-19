f56df5e5764d624e4549f2b0cc9daf9bb18999ea
cblume <cblume@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-10-19 11:14:21 +0000

    Coverity (Frederick)

--- TRD/AliTRDSaxHandler.cxx
+++ TRD/AliTRDSaxHandler.cxx
@@ -324,7 +324,7 @@ void AliTRDSaxHandler::OnStartElement(const char *name, const TList *attributes)
   // if there is a new DCS element put it in the correct array
   if (CompareString(tagName, "DCS")) {
     if (fSystem == kInsideFEE) {
-      fDCSFEEObj = new AliTRDCalDCSFEE();
+      fDCSFEEObj = new AliTRDCalDCSFEE(name,dcsTitle);
       fDCSFEEObj->SetDCSid(dcsId);
     }
     if (fSystem == kInsidePTR) {
@@ -422,9 +422,13 @@ void AliTRDSaxHandler::OnEndElement(const char *name)
     if (fLevel1Tag == kInsideGainTable) {
       if (CompareString(tagName, "name"))   fDCSFEEObj->SetGainTableName(fContent);
       if (CompareString(tagName, "desc"))   fDCSFEEObj->SetGainTableDesc(fContent);
-      if (CompareString(tagName, "adcdac")) fDCSFEEObj->SetGainTableAdcdac(fCurrentROB, fCurrentMCM, fContent.Atoi());
-      if (CompareString(tagName, "fgfn"))   fDCSFEEObj->SetGainTableFgfn(fCurrentROB, fCurrentMCM, fCurrentADC, fContent.Atoi());
-      if (CompareString(tagName, "fgan"))   fDCSFEEObj->SetGainTableFgan(fCurrentROB, fCurrentMCM, fCurrentADC, fContent.Atoi());
+      if (fCurrentROB>=0 && fCurrentMCM>=0) {
+	if (CompareString(tagName, "adcdac")) fDCSFEEObj->SetGainTableAdcdac(fCurrentROB, fCurrentMCM, fContent.Atoi());
+	if (fCurrentADC>=0) {
+	  if (CompareString(tagName, "fgfn"))   fDCSFEEObj->SetGainTableFgfn(fCurrentROB, fCurrentMCM, fCurrentADC, fContent.Atoi());
+	  if (CompareString(tagName, "fgan"))   fDCSFEEObj->SetGainTableFgan(fCurrentROB, fCurrentMCM, fCurrentADC, fContent.Atoi());
+	}
+      }
     }
     if (fInsideRstate == 1) {
       if (fCurrentROB>=0 && fCurrentMCM>=0) {
