cc0ee82aa92dd497b9901e5c7a56f644c723fdf3
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-11-01 14:00:04 +0000

    Fix from Kristjan - allow more energies

--- PWG2/FORWARD/analysis/SubmitFMDCorrections.C
+++ PWG2/FORWARD/analysis/SubmitFMDCorrections.C
@@ -9,30 +9,43 @@ void SubmitFMDCorrections(const Char_t* filename, Bool_t store, Float_t energy, 
   
   if(energy == 900)
     pars->SetEnergy(AliFMDAnaParameters::k900);
-  else if(energy == 7000)
-    pars->SetEnergy(AliFMDAnaParameters::k7000);
   else if(energy == 10000)
     pars->SetEnergy(AliFMDAnaParameters::k10000);
   else if(energy == 14000)
     pars->SetEnergy(AliFMDAnaParameters::k14000);
+  else if(energy == 7000)
+    pars->SetEnergy(AliFMDAnaParameters::k7000);
+  else if(energy == 2400)
+    pars->SetEnergy(AliFMDAnaParameters::k2400);
   else if(energy == 5500)
     pars->SetEnergy(AliFMDAnaParameters::k5500);
+  else if(energy == 2750)
+    pars->SetEnergy(AliFMDAnaParameters::k2750);
   
   if(trigger == 0)
     pars->SetTriggerDefinition(AliFMDAnaParameters::kMB1);
   else if(trigger == 1)
     pars->SetTriggerDefinition(AliFMDAnaParameters::kMB2);
+  else if(trigger == 2)
+    pars->SetTriggerDefinition(AliFMDAnaParameters::kSPDFASTOR);
+  else if(trigger == 3)
+    pars->SetTriggerDefinition(AliFMDAnaParameters::kNOCTP);
+  else if(trigger == 4)
+    pars->SetTriggerDefinition(AliFMDAnaParameters::kEMPTY);
+  else if(trigger == 5)
+    pars->SetTriggerDefinition(AliFMDAnaParameters::kNSD);
 
   if(mag==0)
     pars->SetMagField(AliFMDAnaParameters::k0G);
   else if(mag==1)
     pars->SetMagField(AliFMDAnaParameters::k5G);
+  else if(mag==-1)
+    pars->SetMagField(AliFMDAnaParameters::k5Gnegative);
   
   if(collsystem == 0)
     pars->SetCollisionSystem(AliFMDAnaParameters::kPP);
   else if(collsystem == 1)
     pars->SetCollisionSystem(AliFMDAnaParameters::kPbPb);
-  
   
   pars->SetInelGtZero(inelGtZero);
   pars->PrintStatus();
@@ -47,3 +60,9 @@ void SubmitFMDCorrections(const Char_t* filename, Bool_t store, Float_t energy, 
   else
     std::cout<<" - and not stored!"<<std::endl;
 }
+
+
+//
+// EOF
+//
+
