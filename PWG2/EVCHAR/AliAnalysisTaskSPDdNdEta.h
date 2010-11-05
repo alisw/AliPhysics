5703e324b5902aebd1444172fb145dfa27f2baae
enrico <enrico@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-11-05 15:30:20 +0000

    Update for PbPb analysis (Mariella)

--- PWG2/EVCHAR/AliAnalysisTaskSPDdNdEta.h
+++ PWG2/EVCHAR/AliAnalysisTaskSPDdNdEta.h
@@ -9,122 +9,113 @@
 // Contact: Maria.Nicassio@ba.infn.it, Domenico.Elia@ba.infn.it          //
 ///////////////////////////////////////////////////////////////////////////
 
-class TH1F;
+class TH1F; 
 class TH2F;
 class TH3F;
 class AliESDEvent;
+class TList;
 
-#include "AliAnalysisTask.h"
+#include "AliTriggerAnalysis.h"
+#include "AliMCEvent.h"
+#include "AliMCEventHandler.h"
+#include "AliAnalysisTaskSE.h"
 
-class AliAnalysisTaskSPDdNdEta : public AliAnalysisTask {
+class AliAnalysisTaskSPDdNdEta : public AliAnalysisTaskSE {
  public:
   AliAnalysisTaskSPDdNdEta(const char *name = "AliAnalysisTaskSPDdNdEta");
   virtual ~AliAnalysisTaskSPDdNdEta(); 
   
-  virtual void   ConnectInputData(Option_t *);
-  virtual void   CreateOutputObjects();
-  virtual void   Exec(Option_t *option);
+  virtual void   UserCreateOutputObjects();
+  virtual void   UserExec(Option_t *option);
   virtual void   Terminate(Option_t *);
 
-  void SetEvtGen(Bool_t pythia = kTRUE) { fpythia = pythia; }
-  void SetReadMC(Bool_t readmc = kFALSE) { fCorr = readmc; }
-  void SetTrigger(Int_t MBtrigger) { fTrigger = MBtrigger; } 
+  enum MCCentralityBin{kcentral=1,kbin3to6=2,kbin6to9=3,kbin9to12=4,kbin12to15=5,kperipheral=6,kall=7};
+
+  void SetReadMC(Bool_t readmc = kFALSE) { fUseMC = readmc; }
+  void SetTrigger(AliTriggerAnalysis::Trigger trigger) { fTrigger = trigger; }
+  void SetReadPbPb(Bool_t pbpb = kFALSE) { fPbPb = pbpb; } 
+  void SetReadTrackRefs(Bool_t readtr = kFALSE) { fTR = readtr; }
+  void SetRecoTracklets(Bool_t recotracklets = kFALSE) { fRecoTracklets = recotracklets; }
+  void SetMCCentralityBin(MCCentralityBin mccentrbin) {fMCCentralityBin=mccentrbin;}
+  void SetCentralityLowLim(Float_t centrlowlim) {fCentrLowLim=centrlowlim;}
+  void SetCentralityUpLim(Float_t centruplim) {fCentrUpLim=centruplim;}
+  void SetCentralityEst(TString centrest) {fCentrEst=centrest;}
  
+  Bool_t IsDetectablePrimary(Int_t nref, AliMCParticle* mcpart);
+  Bool_t IsDetectedPrimary(Int_t nref, AliMCParticle* mcpart, Int_t Layer);
+
  protected:
-  AliESDEvent *fESD;               //ESD object
-  TList* fOutput;                  //! list send on output slot 0
+  AliESDEvent *fmyESD;             // ! ESD object 
+  TList *fOutput;                  // ! output list send on output slot 1 
   
-  Bool_t fpythia;                  // to identify accordingly process types
-  Bool_t fCorr;                    // flag to enable the correction histo calculation
-  Int_t fTrigger;                  // to set the MBtrigger selection
+  MCCentralityBin fMCCentralityBin; // to select MC centrality bin in which corrections are calculated
+  Float_t fCentrLowLim;             // to select centrality bin on data
+  Float_t fCentrUpLim;              // to select centrality bin on data
+  TString fCentrEst;                // to select centrality estimator
+
+
+  Bool_t fUseMC;                   // flag to enable the calculation of correction histograms
+  Bool_t fPbPb;                    // flag to analyze PbPb data 
+  AliTriggerAnalysis::Trigger fTrigger;  
+  Bool_t fTR;                      // to read track references and calculate factors of track to particle correction 
+  Bool_t fRecoTracklets;           // flag to recostruct tracklets
+
+  TH2F        *fHistSPDRAWMultvsZ;          // ! data to be corrected 
+  TH2F        *fHistSPDRAWMultvsZTriggCentrEvts; // ! data to be corrected
+  TH2F        *fHistSPDRAWMultvsZCentrEvts; // ! data to be corrected
+
+  TH2F        *fHistSPDRAWEtavsZ;           // ! data to be corrected
+
+  TH1F        *fHistSPDmultEtacut;          // ! cluster inner layer and tracklet check histos
+  TH1F        *fHistSPDmult;                // ! cluster inner layer and tracklet check histos
+  TH1F        *fHistSPDmultcl1;
+  TH1F        *fHistSPDeta;                 // ! cluster inner layer and tracklet check histos
+  TH1F        *fHistSPDphi;                 // ! cluster inner layer and tracklet check histos
+  TH1F        *fHistSPDtheta;               // ! cluster inner layer and tracklet check histos
+  TH1F        *fHistSPDdePhi;               // ! cluster inner layer and tracklet check histos
+  TH2F        *fHistSPDphivsSPDeta;         // ! cluster inner layer and tracklet check histos
+  TH1F        *fHistSPDdeTheta;             // ! cluster inner layer and tracklet check histos
+
+  TH1F        *fHistSPDvtxAnalysis;         // ! SPD vertex distributions
+  TH2F        *fHistSPDdePhideTheta;        // ! histogram for combinatorial background studies
+
+  TH2F* fHistBkgCorrDen;             // ! track level correction histograms
+  TH2F* fHistBkgCorrDenPrimGen;      // ! track level correction histograms
+  TH2F* fHistBkgCorrNum;             // ! track level correction histograms
+  TH2F* fHistAlgEffNum;              // ! track level correction histograms
+  TH2F* fHistNonDetectableCorrDen;   // ! track level correction histograms
+
+  TH2F* fHistNonDetectableCorrNum;   // ! track level correction histograms
+  TH2F* fHistAllPrimaries;           // ! track level correction histograms
+  TH2F* fHistTrackCentrEvts;         // ! track level correction histograms
+  TH2F* fHistTrackTrigCentrEvts;     // ! track level correction histograms
  
-  TH2F        *fHistSPDRAWMultvsZ;          // data to be corrected 
-  TH2F        *fHistSPDRAWMultvsZTriggEvts; // data to be corrected
-  TH2F        *fHistSPDRAWEtavsZ;           // data to be corrected
+  TH2F* fHistAllEvts;                // ! event level correction histograms
+  TH2F* fHistCentrEvts;              // ! event level correction histograms
+  TH2F* fHistTrigCentrEvts;          // ! event level correction histograms
+  TH2F* fHistSelEvts;                // ! event level correction histograms
 
-  TH1F        *fHistSPDmultEtacut;          // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDmult;                // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDeta;                 // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDcl1multEtacutLay1;   // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDcl1mult;             // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDcl1eta;              // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDphi;                 // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDcl1phi;              // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDtheta;               // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDcl1theta;            // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDdePhi;               // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDdePhiZ;              // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDdePhi3D;             // cluster inner layer and tracklet check histos
-  TH2F        *fHistSPDphivsSPDeta;         // cluster inner layer and tracklet check histos
-  TH2F        *fHistSPDcl1phivsSPDcl1eta;   // cluster inner layer and tracklet check histos
-  TH1F        *fHistSPDdeTheta;             // cluster inner layer and tracklet check histos
+  TH1F* fHistMCmultEtacut;           // ! MC distributions
+  TH2F* fHistMCmultEtacutvsSPDmultEtacut; // ! MC distributions
 
-  TH1F        *fHistSPDvtxAnalysis;         // SPD vertex distributions
-  TH3F        *fHistSPDvtx3D;               // SPD vertex distributions
-  TH1F        *fHistSPDvtxZ;                // SPD vertex distributions
-  TH2F        *fHistNcontribSPDvtxvsSPDvtx; // SPD vertex distributions
-  TH1F        *fHistNcontribSPDvtx3D;       // SPD vertex distributions
-  TH1F        *fHistNcontribSPDvtxZ;        // SPD vertex distributions
-  TH1F        *fHistNcontribSPDvtxall;      // SPD vertex distributions
+  TH1F* fHistMCvtxx;                 // ! MC vertex
+  TH1F* fHistMCvtxy;                 // ! MC vertex
+  TH1F* fHistMCvtxz;                 // ! MC vertex
 
-  TH2F        *fHistSPDcl1multvsnFiredChipsLay1;              // SPD fired chips distributions
-  TH2F        *fHistSPDmultvsnFiredChipsLay1;                 // SPD fired chips distributions
-  TH2F        *fHistSPDmultvsnFiredChipsLay2;                 // SPD fired chips distributions
-  TH2F        *fHistnFiredChipsLay2vsnFiredChipsLay1;         // SPD fired chips distributions
-  TH2F        *fHistnFiredChipsLay2vsnFiredChipsLay1novtxrec; // SPD fired chips distributions
-  TH1F        *fHistSPDvtxRec;                                // SPD vertex distribution
+  TH2F* fHistRecvsGenImpactPar;      // ! impact parameter correlation (ZDC est vs gen)
+  TH1F* fHistMCNpart;                // ! distribution of number of participants from MC 
 
-  TH2F* fHistBkgCorrNum;             // track level correction histograms
-  TH2F* fHistBkgCorrDen;             // track level correction histograms
-  TH2F* fHistAlgEffNum;              // track level correction histograms
-  TH2F* fHistNonDetectableCorrNum;   // track level correction histograms
-  TH2F* fHistNonDetectableCorrDen;   // track level correction histograms
-  TH2F* fHistTrackTrigVtxCorrNum;    // track level correction histograms
-  TH2F* fHistTrackTrigCorrDen;       // track level correction histograms
-  TH2F* fHistTrackTrigVtxCorrNumNSD; // track level correction histograms
-  TH2F* fHistTrackTrigNSD;           // track level correction histograms 
- 
-  TH2F* fHistTrigVtxCorrNum;       // event level correction histograms
-  TH2F* fHistTrigVtxCorrDen;       // event level correction histograms
+  TH1F* fHistdPhiPP;                 // ! tracklet check histo
+  TH1F* fHistdPhiSS;                 // ! tracklet check histo
+  TH1F* fHistdPhiComb;               // ! tracklet check histo
 
-  TH2F* fHistTrigCorrDen;          // event level correction histograms
-  TH2F* fHistTrigVtxCorrNumNSD;    // event level correction histograms
-  TH2F* fHistEvTrigNSD;            // event level correction histograms
-
-  TH2F* fHistMCEtavsZTriggMCvtxEvts;  // MC distributions
-  TH2F* fHistMCEtavsZTriggESDvtxEvts; // MC distributions
-  TH2F* fHistMCEtavsZ;                // MC distributions 
-
-  TH1F* fHistMCEtaInel;                 // MC distributions
-  TH1F* fHistMCEtaNonDiffractive;       // MC distributions
-  TH1F* fHistMCEtaNonSingleDiffractive; // MC distributions
-  TH1F* fHistoProcessType;              // additional check histo
-  TH1F* fHistoProcessTypeTriggered;     // additional check histo
-
-  TH2F* fHistContributorsvsMCVtx;     // additional check histos
-  TH3F* fHistoDetectableNotr;         // additional check histos
-  TH2F* fHistoDetectabletr;           // additional check histos
-  TH2F* fHistoNonStoppingTracks;      // additional check histos
-  TH2F* fHistoDetectedLay1;           // additional check histos
-  TH2F* fHistoDetectedLay2;           // additional check histos
-  TH1F* fHistoPt;                     // additional check histos 
-  TH1F* fHistoRTRm1;                  // additional check histos
-  TH3F* fHistMCvtx;                   // additional check histos
-
-  TH1F* fHistMultAllNonDiff;          // trigger-vertex efficiencies for non diffractive events
-  TH1F* fHistMultAllSingleDiff;       // trigger-vertex efficiencies for single diffractive events
-  TH1F* fHistMultAllDoubleDiff;       // trigger-vertex efficiencies for double diffractive events
-  TH1F* fHistMultTrVtxNonDiff;        // trigger-vertex efficiencies for non diffractive events
-  TH1F* fHistMultTrVtxSingleDiff;     // trigger-vertex efficiencies for single diffractive events
-  TH1F* fHistMultTrVtxDoubleDiff;     // trigger-vertex efficiencies for double diffractive events
- 
-  TH1F* fHistMCEtaNonSingleDiffractiveLargeBin; // MC pseudorapidity distribution
+  TH2F* fHistDeVtx;                  // ! check histo
 
  private:    
-  AliAnalysisTaskSPDdNdEta(const AliAnalysisTaskSPDdNdEta&); 
-  AliAnalysisTaskSPDdNdEta& operator=(const AliAnalysisTaskSPDdNdEta&); 
+  AliAnalysisTaskSPDdNdEta(const AliAnalysisTaskSPDdNdEta&); // not implemented
+  AliAnalysisTaskSPDdNdEta& operator=(const AliAnalysisTaskSPDdNdEta&); // not implemented 
   
-  ClassDef(AliAnalysisTaskSPDdNdEta, 2); 
+  ClassDef(AliAnalysisTaskSPDdNdEta, 3);  
 };
 
 #endif
