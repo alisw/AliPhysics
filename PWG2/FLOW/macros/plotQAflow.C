efe677d3dc52411a28de795a03200ed7ab572b23
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-11-09 13:56:16 +0000

    bug fix

--- PWG2/FLOW/macros/plotQAflow.C
+++ PWG2/FLOW/macros/plotQAflow.C
@@ -2,7 +2,10 @@
 void plotQAflow(const char* filename="")
 {
  TFile f(filename,"read");
- TObjArray* flowQA = dynamic_cast<TObjArray*>(f.Get("flowQA"));
+ TKey* key = f.GetListOfKeys()->At(0);
+ if (!key) return;
+ TObjArray* flowQA = dynamic_cast<TObjArray*>(f.Get(key->GetName()));
+ if (!flowQA) return;
  TObjArray* before = dynamic_cast<TObjArray*>(flowQA->At(0));
  TObjArray* after = dynamic_cast<TObjArray*>(flowQA->At(1));
  for (Int_t i=0; i<before->GetEntries(); i++)
@@ -11,6 +14,7 @@ void plotQAflow(const char* filename="")
    TH1* hafter = dynamic_cast<TH1*>(after->At(i));
    TCanvas* canvas = new TCanvas(hbefore->GetName(), hbefore->GetTitle());
    TLegend* legend = new TLegend(0.8,0.8,1.0,1.0);
+   hbefore->SetAxisRange(0.1,hbefore->GetBinContent(hbefore->GetMaximumBin()),"Y");
    hbefore->Draw();
    legend->AddEntry(hbefore,"before cuts","l");
    hafter->SetLineColor(2);
