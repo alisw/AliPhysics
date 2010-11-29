5b823ddc53e9cd9baf83113f6a15815489e8843a
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-11-29 21:50:42 +0000

    Added script to run analysis locally, and visualise the result
    Fixed up RunManager for local TChain 
    Fixed up a few things and added comments to the AddTask script

--- PWG2/FORWARD/analysis/AddTaskFMD.C
+++ PWG2/FORWARD/analysis/AddTaskFMD.C
@@ -1,12 +1,15 @@
-//This is the macro to include the FMD analysis in the train.
-//It depends on two libraries: libFMDanalysis.so in AliRoot and 
-//libPhysics.so in ROOT. It has been tested to work with the 
-//example scripts in the ANALYSIS webpages.
-// Author: Hans Hjersing Dalsgaard, hans.dalsgaard@cern,ch
+// This is the macro to include the FMD analysis in the train.  It
+// depends on two libraries: libFMDanalysis.so in AliRoot and
+// libPhysics.so in ROOT. It has been tested to work with the example
+// scripts in the ANALYSIS webpages.  
+//
+// Author: Hans Hjersing Dalsgaard <hans.dalsgaard@cern.ch>
 
 
-AliFMDAnalysisTaskSE* AddTaskFMD() {
-
+AliFMDAnalysisTaskSE* AddTaskFMD(Float_t cmsGeV=900.F, 
+				 const char* col="p-p", 
+				 Float_t bkG=5.F) 
+{
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
     Error("AddTaskESDFilter", "No analysis manager to connect to.");
@@ -14,30 +17,45 @@ AliFMDAnalysisTaskSE* AddTaskFMD() {
   }   
   
   
-  AliFMDAnalysisTaskSE *taskfmd = new AliFMDAnalysisTaskSE("TaskFMD");
-  mgr->AddTask(taskfmd);
-  
+  // --- Generate parameter manager ----------------------------------
   AliFMDAnaParameters* pars = AliFMDAnaParameters::Instance();
-  
+  pars->SetEnergy(cmsGeV);
+  pars->SetCollisionSystem(col);
+  pars->SetMagField(bkG);
   pars->SetProcessPrimary(kFALSE);
   pars->SetProcessHits(kFALSE);
-  
   pars->SetRealData(kTRUE);
-  AliMCEventHandler* eventHandler = dynamic_cast<AliMCEventHandler*> (AliAnalysisManager::GetAnalysisManager()->GetMCtruthEventHandler());
+
+  // --- Check if we have an MC handler ------------------------------
+  AliMCEventHandler* eventHandler = 
+    dynamic_cast<AliMCEventHandler*> (AliAnalysisManager::GetAnalysisManager()
+				      ->GetMCtruthEventHandler());
   if(eventHandler) {
     pars->SetRealData(kFALSE);
     pars->SetProcessPrimary(kTRUE);
     pars->SetProcessHits(kFALSE);
   }
-  
   pars->Init();
+  pars->PrintStatus();
   
-  TString outputfile = AliAnalysisManager::GetCommonFileName();
-  outputfile += Form(":%s",pars->GetDndetaAnalysisName());
+  // --- Make the task -----------------------------------------------
+  AliFMDAnalysisTaskSE *taskfmd = new AliFMDAnalysisTaskSE("TaskFMD");
+  mgr->AddTask(taskfmd);
 
-  AliAnalysisDataContainer *cout_fmd = mgr->CreateContainer("BackgroundCorrected", TList::Class(), AliAnalysisManager::kOutputContainer,outputfile);                             
+  // --- Connect input/output ----------------------------------------
+  TString outputfile = Form("%s:%s", 
+			    AliAnalysisManager::GetCommonFileName(),
+			    pars->GetDndetaAnalysisName());
+
+  AliAnalysisDataContainer *cout_fmd = 
+    mgr->CreateContainer("BackgroundCorrected", TList::Class(), 
+			 AliAnalysisManager::kOutputContainer,outputfile);
   mgr->ConnectInput(taskfmd, 0, mgr->GetCommonInputContainer());
   mgr->ConnectOutput(taskfmd, 1, cout_fmd);
 
   return taskfmd;
 }
+//
+// EOF
+//
+
