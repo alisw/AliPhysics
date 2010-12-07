f10bee27034aad0d7ae32188b9fc6a23f67453c7
odjuvsla <odjuvsla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-12-07 11:55:29 +0000

    - moving code to new base class - adding use of physics selection and centrality

--- PWG4/totEt/AliAnalysisTaskTotEt.h
+++ PWG4/totEt/AliAnalysisTaskTotEt.h
@@ -14,47 +14,29 @@ class AliESDtrackCuts;
 class TH2F;
 class TList;
 
-#include "AliAnalysisTaskSE.h"
+#include "AliAnalysisTaskTransverseEnergy.h"
 
-class AliAnalysisTaskTotEt : public AliAnalysisTaskSE {
+class AliAnalysisTaskTotEt : public AliAnalysisTaskTransverseEnergy {
   
 public:
-  AliAnalysisTaskTotEt(const char *name = "AliAnalysisTaskTotEt");
+  AliAnalysisTaskTotEt(const char *name = "AliAnalysisTaskTotEt", Bool_t isMc = false);
   virtual ~AliAnalysisTaskTotEt();
-  
-public:
   
   //  virtual void   ConnectInputData(Option_t *);
   virtual void   UserCreateOutputObjects();
   virtual void   UserExec(Option_t *option);
   virtual void   Terminate(Option_t *);
     
-
-  AliESDtrackCuts* GetTPCITSTrackCuts(){return (AliESDtrackCuts*) fOutputList->FindObject("fEsdTrackCuts");}
-  AliESDtrackCuts* GetTPCOnlyTrackCuts(){return (AliESDtrackCuts*) fOutputList->FindObject("fEsdTrackCutsTPCOnly");}
-  AliESDtrackCuts* GetITSTrackCuts(){return (AliESDtrackCuts*) fOutputList->FindObject("fEsdTrackCutsITS");}
-
 private:
   
   //Declare private to avoid compilation warning
   AliAnalysisTaskTotEt & operator = (const AliAnalysisTaskTotEt & g) ;//copy assignment
   AliAnalysisTaskTotEt(const AliAnalysisTaskTotEt & g) ; // copy ctor
 
-  TString       fMCConfigFile;        // the name of the ConfigFile
-  TString       fRecoConfigFile;        // the name of the ConfigFile
-
-  TList *fOutputList; //output list
-  
   AliAnalysisEtReconstructed *fRecAnalysis; // Rec 
   AliAnalysisEtMonteCarlo *fMCAnalysis; // MC
   
-  TH2F *fHistEtRecvsEtMC; // Rec vs MC histo
-
-  AliESDtrackCuts* fEsdtrackCutsITSTPC; // track cuts ITS&TPC
-  AliESDtrackCuts* fEsdtrackCutsTPC; // track cuts TPC
-  AliESDtrackCuts* fEsdtrackCutsITS; // track cuts ITS
-  
-  ClassDef(AliAnalysisTaskTotEt, 1); // example of analysis
+  ClassDef(AliAnalysisTaskTotEt, 2) 
 };
 
 #endif
