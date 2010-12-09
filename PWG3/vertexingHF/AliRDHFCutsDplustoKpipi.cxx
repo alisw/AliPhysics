ee19317b674294da3ada828a9471011ddf4cadef
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-12-09 15:48:47 +0000

    Standard cuts (Renu)

--- PWG3/vertexingHF/AliRDHFCutsDplustoKpipi.cxx
+++ PWG3/vertexingHF/AliRDHFCutsDplustoKpipi.cxx
@@ -99,6 +99,14 @@ fUseStrongPid(kFALSE)
 
   
 }
+
+
+
+
+
+
+
+
 //--------------------------------------------------------------------------
 AliRDHFCutsDplustoKpipi::AliRDHFCutsDplustoKpipi(const AliRDHFCutsDplustoKpipi &source) :
   AliRDHFCuts(source),
@@ -289,7 +297,6 @@ Int_t AliRDHFCutsDplustoKpipi::IsSelectedPID(AliAODRecoDecayHF *rd)
 }
 
 
-
 //---------------------------------------------------------------------------
 Int_t AliRDHFCutsDplustoKpipi::IsSelected(TObject* obj,Int_t selectionLevel, AliAODEvent* aod) {
   //
@@ -412,4 +419,356 @@ Int_t AliRDHFCutsDplustoKpipi::IsSelected(TObject* obj,Int_t selectionLevel, Ali
 
   return 3;
 }
+
+
+
+
 //---------------------------------------------------------------------------
+
+
+void AliRDHFCutsDplustoKpipi::SetStandardCutsPP2010() {
+  //
+  //STANDARD CUTS USED FOR 2010 pp analysis 
+  //                                            
+  
+  SetName("DplustoKpipiCutsStandard");
+  SetTitle("Standard Cuts for D+ analysis");
+  
+  // PILE UP REJECTION
+  SetOptPileup(AliRDHFCuts::kRejectPileupEvent);
+
+  // EVENT CUTS
+  SetMinVtxContr(1);
+
+  AliESDtrackCuts* esdTrackCuts=new AliESDtrackCuts();
+  esdTrackCuts->SetRequireSigmaToVertex(kFALSE);
+  //default
+  esdTrackCuts->SetRequireTPCRefit(kTRUE);
+  esdTrackCuts->SetRequireITSRefit(kTRUE);
+  //esdTrackCuts->SetMinNClustersITS(4); // default is 5
+  esdTrackCuts->SetMinNClustersTPC(70);
+  esdTrackCuts->SetClusterRequirementITS(AliESDtrackCuts::kSPD,
+					 AliESDtrackCuts::kAny); 
+  // default is kBoth, otherwise kAny
+  esdTrackCuts->SetMinDCAToVertexXY(0.);
+  esdTrackCuts->SetPtRange(0.3,1.e10);
+  
+  AddTrackCuts(esdTrackCuts);
+  
+ 
+  const Int_t nptbins =13;
+  const Int_t nvars=12;
+  Float_t ptbins[nptbins+1];
+  ptbins[0]=0.;
+  ptbins[1]=1;	
+  ptbins[2]=2.;
+  ptbins[3]=3.;
+  ptbins[4]=4.;
+  ptbins[5]=5.;
+  ptbins[6]=6.;
+  ptbins[7]=8.;
+  ptbins[8]=10.;
+  ptbins[9]=12.;
+  ptbins[10]=14.;
+  ptbins[11]=16.;
+  ptbins[12]=24.;
+  ptbins[13]=99999.;
+      
+    
+  Float_t** anacutsval;
+  anacutsval=new Float_t*[nvars];
+  
+  for(Int_t ic=0;ic<nvars;ic++){anacutsval[ic]=new Float_t[nptbins];}
+  //Double_t cutsDplus[12]={0.2,0.4,0.4,0.,0.,0.01,0.06,0.02,0.,0.85,0.,10000000000.};
+  Int_t ic=0;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.2;
+  }
+  
+  ic=3;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.;
+  }
+  ic=4;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.;
+  }
+  ic=5;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.01;
+  }
+  //ic=6;
+  //for(Int_t ipt=0;ipt<nptbins;ipt++){
+  // anacutsval[ic][ipt]=0.06;
+  
+  ic=11;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=10000000000.;
+  }
+  
+  
+  anacutsval[1][0]=0.3;
+  anacutsval[1][1]=0.3;
+  anacutsval[1][2]=0.4;
+  
+  anacutsval[2][0]=0.3;
+  anacutsval[2][1]=0.3;
+  anacutsval[2][2]=0.4;
+  
+  
+  ic=1;
+  for(Int_t ipt=3;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.4;
+  }
+  ic=2;
+  for(Int_t ipt=3;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.4;
+  }
+  
+  anacutsval[6][0]=0.022100;
+  anacutsval[6][1]=0.022100;
+  anacutsval[6][2]=0.034;
+  anacutsval[6][3]=0.020667;
+  anacutsval[6][4]=0.020667;
+  anacutsval[6][5]=0.023333;
+  anacutsval[6][6]=0.023333;
+  anacutsval[6][7]=0.023333;
+  anacutsval[6][8]=0.023333;
+  anacutsval[6][9]=0.023333;
+  anacutsval[6][10]=0.06;
+  anacutsval[6][11]=0.06;
+  anacutsval[6][12]=0.06;
+  anacutsval[6][13]=0.06;
+  
+  
+  anacutsval[7][0]=0.08;
+  anacutsval[7][1]=0.08;
+  anacutsval[7][2]=0.09;  
+  anacutsval[7][3]=0.095;
+  anacutsval[7][4]=0.095;
+  anacutsval[7][5]=0.115;
+  anacutsval[7][6]=0.115;
+  anacutsval[7][7]=0.115;
+  anacutsval[7][8]=0.115;
+  anacutsval[7][9]=0.115;
+  anacutsval[7][10]=0.02;
+  anacutsval[7][11]=0.02;
+  anacutsval[7][12]=0.02;
+  anacutsval[7][13]=0.02;
+  
+  
+  anacutsval[8][0]=0.5;
+  anacutsval[8][1]=0.5;
+  anacutsval[8][2]=1.0;
+  anacutsval[8][3]=0.5;
+  anacutsval[8][4]=0.5;
+  anacutsval[8][5]=0.5;
+  anacutsval[8][6]=0.5;
+  anacutsval[8][7]=0.5;
+  anacutsval[8][8]=0.5;
+  anacutsval[8][9]=0.5;
+  anacutsval[8][10]=0.2;
+  anacutsval[8][11]=0.2;
+  anacutsval[8][12]=0.2;
+  anacutsval[8][13]=0.2;
+    
+    
+  anacutsval[9][0]=0.95;
+  anacutsval[9][1]=0.95;
+  anacutsval[9][2]=0.95; 
+  anacutsval[9][3]=0.95; 
+  anacutsval[9][4]= 0.95;
+  anacutsval[9][5]=0.92;
+  anacutsval[9][6]=0.92;
+  anacutsval[9][7]=0.92;
+  anacutsval[9][8]=0.92;
+  anacutsval[9][9]=0.90;
+  anacutsval[9][10]=0.90;
+  anacutsval[9][11]=0.90;
+  anacutsval[9][12]=0.90;
+  anacutsval[9][13]=0.90;
+  
+  anacutsval[10][0]=0.0055;
+  anacutsval[10][1]=0.0055;
+  anacutsval[10][2]= 0.0028;
+  anacutsval[10][3]=0.000883;
+  anacutsval[10][4]=0.000883;
+  anacutsval[10][5]=0.000883;
+  anacutsval[10][6]=0.000883;
+  anacutsval[10][7]=0.000883;
+  anacutsval[10][8]=0.000883;
+  anacutsval[10][9]=0.000883;
+  anacutsval[10][10]=0.0;
+  anacutsval[10][11]=0.0;
+  anacutsval[10][12]=0.0;
+  anacutsval[10][13]=0.0;
+  
+  
+  SetPtBins(nptbins+1,ptbins);
+  SetCuts(nvars,nptbins,anacutsval);
+  SetUsePID(kTRUE);
+  SetRemoveDaughtersFromPrim(kTRUE);
+  
+  PrintAll();
+
+  return;
+}
+
+
+void AliRDHFCutsDplustoKpipi::SetStandardCutsPbPb2010() {
+  //
+  //STANDARD CUTS USED FOR 2010 Pb Pb analysis.... not optimized yet 
+  //                                              
+  
+  SetName("DplustoKpipiCutsStandard");
+  SetTitle("Standard Cuts for D+ analysis in PbPb2010 run");
+  
+  // PILE UP REJECTION
+  //SetOptPileup(AliRDHFCuts::kRejectPileupEvent);
+
+  // EVENT CUTS
+  SetMinVtxContr(1);
+
+  
+  AliESDtrackCuts* esdTrackCuts=new AliESDtrackCuts();
+  esdTrackCuts->SetRequireSigmaToVertex(kFALSE);
+  //default
+  esdTrackCuts->SetRequireTPCRefit(kTRUE);
+  esdTrackCuts->SetRequireITSRefit(kTRUE);
+  //esdTrackCuts->SetMinNClustersITS(4); // default is 5
+  esdTrackCuts->SetMinNClustersTPC(70);
+  esdTrackCuts->SetClusterRequirementITS(AliESDtrackCuts::kSPD,
+					 AliESDtrackCuts::kAny); 
+  // default is kBoth, otherwise kAny
+  esdTrackCuts->SetMinDCAToVertexXY(0.);
+  esdTrackCuts->SetPtRange(0.8,1.e10);
+    
+  AddTrackCuts(esdTrackCuts);
+    
+  const Int_t nptbins=10;
+  Float_t* ptbins;
+  ptbins=new Float_t[nptbins+1];
+  ptbins[0]=0.;
+  ptbins[1]=1.;
+  ptbins[2]=2.;
+  ptbins[3]=3.;
+  ptbins[4]=4.;
+  ptbins[5]=5.;
+  ptbins[6]=6.;
+  ptbins[7]=8.;
+  ptbins[8]=12.;
+  ptbins[9]=16.;
+  ptbins[10]=24.;
+  const Int_t nvars=12;
+    
+    
+    
+  Float_t** anacutsval;
+  anacutsval=new Float_t*[nvars];
+  
+  for(Int_t ic=0;ic<nvars;ic++){anacutsval[ic]=new Float_t[nptbins];}
+  //Double_t cutsDplus[12]={0.2,0.4,0.4,0.,0.,0.01,0.06,0.02,0.,0.85,0.,10000000000.};
+  Int_t ic=0;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.2;
+  }
+  
+  ic=3;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.;
+  }
+  ic=4;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.;
+  }
+  ic=5;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.01;
+  }
+  //ic=6;
+  //for(Int_t ipt=0;ipt<nptbins;ipt++){
+  // anacutsval[ic][ipt]=0.06;
+  
+  ic=11;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=10000000000.;
+  }
+  
+  
+  
+  
+  ic=1;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.8;
+  }
+  ic=2;
+  for(Int_t ipt=0;ipt<nptbins;ipt++){
+    anacutsval[ic][ipt]=0.8;
+  }
+  
+    
+  anacutsval[6][0]=0.022100;
+  anacutsval[6][1]=0.022100;
+  anacutsval[6][2]=0.034;
+  anacutsval[6][3]=0.020667;
+  anacutsval[6][4]=0.020667;
+  anacutsval[6][5]=0.023333;
+  anacutsval[6][6]=0.023333;
+  anacutsval[6][7]=0.023333;
+  anacutsval[6][8]=0.023333;
+  anacutsval[6][9]=0.023333;
+  
+  anacutsval[7][0]=0.08;
+  anacutsval[7][1]=0.08;
+  anacutsval[7][2]=0.09;  
+  anacutsval[7][3]=0.095;
+  anacutsval[7][4]=0.095;
+  anacutsval[7][5]=0.115;
+  anacutsval[7][6]=0.115;
+  anacutsval[7][7]=0.115;
+  anacutsval[7][8]=0.115;
+  anacutsval[7][9]=0.115;
+  
+  anacutsval[8][0]=0.5;
+  anacutsval[8][1]=0.5;
+  anacutsval[8][2]=1.0;
+  anacutsval[8][3]=0.5;
+  anacutsval[8][4]=0.5;
+  anacutsval[8][5]=0.5;
+  anacutsval[8][6]=0.5;
+  anacutsval[8][7]=0.5;
+  anacutsval[8][8]=0.5;
+  anacutsval[8][9]=0.5;
+  
+  anacutsval[9][0]=0.995;
+  anacutsval[9][1]=0.995;
+  anacutsval[9][2]=0.997; 
+  anacutsval[9][3]=0.989; 
+  anacutsval[9][4]= 0.989;
+  anacutsval[9][5]=0.997;
+  anacutsval[9][6]=0.997;
+  anacutsval[9][7]=0.997;
+  anacutsval[9][8]=0.997;
+  anacutsval[9][9]=0.997;
+ 
+  anacutsval[10][0]=0.0055;
+  anacutsval[10][1]=0.0055;
+  anacutsval[10][2]= 0.0028;
+  anacutsval[10][3]=0.000883;
+  anacutsval[10][4]=0.000883;
+  anacutsval[10][5]=0.000883;
+  anacutsval[10][6]=0.000883;
+  anacutsval[10][7]=0.000883;
+  anacutsval[10][8]=0.000883;
+  anacutsval[10][9]=0.000883;
+    
+    
+  SetPtBins(nptbins+1,ptbins);
+  SetCuts(nvars,nptbins,anacutsval);
+  SetUsePID(kTRUE);
+  SetRemoveDaughtersFromPrim(kFALSE);
+    
+  PrintAll();
+
+  return;
+}
