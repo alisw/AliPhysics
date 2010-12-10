be9b8a6c57eb74070e2d1886d15fae67ff0e4a16
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-12-10 13:01:37 +0000

    Update of analysis macros

--- PWG2/RESONANCES/macros/train/LHC2010-7TeV-phi/RsnConfigNoSA.C
+++ PWG2/RESONANCES/macros/train/LHC2010-7TeV-phi/RsnConfigNoSA.C
@@ -5,8 +5,8 @@
 // - ITS: not included
 // - dip: not included
 //
-Bool_t RsnConfigNoSA(const char *taskName, const char *options, const char *path)
+Bool_t RsnConfigNoSA(const char *taskName, const char *options, const char *path, Int_t multMin = 0, Int_t multMax = 0)
 {
   gROOT->LoadMacro("$(ALICE_ROOT)/PWG2/RESONANCES/macros/train/LHC2010-7TeV-phi/RsnConfig.C");
-  return RsnConfig(taskName, options, "pid", path);
+  return RsnConfig(taskName, options, "pid", path, multMin, multMax);
 }
