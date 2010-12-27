b3e9c345d317d2635dc722d750a948a5aa407e40
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2010-12-27 06:28:15 +0000

    AddTask macro for fast embedding

---
+++ PWG4/macros/AddTaskFastEmbedding.C
@@ -0,0 +1,76 @@
+
+AliAnalysisTaskFastEmbedding* AddTaskFastEmbedding(){
+
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    if(!mgr){
+	::Error("AddTaskCentralitySelection", "No analysis manager to connect ot.");
+	return NULL;
+    }
+    if(!mgr->GetInputEventHandler()){
+        ::Error("AddTaskCentralitySelection", "This task requires an input event handler.");
+	return NULL;
+    }
+
+
+    AliAnalysisTaskFastEmbedding *task = new AliAnalysisTaskFastEmbedding("FastEmbedding");
+    // ## set embedding mode ##
+    // kAODFull=0, kAODJetTracks, kAODJet4Mom, kToySingle4Mom
+    task->SetEmbedMode(AliAnalysisTaskFastEmbedding::kToyTracks);
+
+    // ## set ranges for toy ##
+    //SetToyTrackRanges(
+    //    Double_t minPt = 50., Double_t maxPt = 50.,
+    //    Double_t minEta = -.5, Double_t maxEta = .5,
+    //    Double_t minPhi = 0., Double_t maxPhi = 2*TMath::Pi())
+    //task->SetToyNumberOfTrackRange(5,700);
+    //task->SetToyTrackRanges(0.15, 300., 5,-.9, .9, 0., 2*TMath::Pi());
+    task->SetToyTrackRanges();
+    task->SetToyFilterMap((1<<32)-1);
+
+    // ## set event selection for events of the addition AOD ##
+    // kEventsAll=0; kEventsJetPt
+    task->SetEvtSelecMode(AliAnalysisTaskFastEmbedding::kEventsJetPt);
+
+    // ## set jet pT range for event selection ##
+    // SetEvtSelJetPtRange(Float_t minPt, Float_t maxPt)
+    task->SetEvtSelJetPtRange(20.,-1.);
+
+    mgr->AddTask(task);
+
+    // ## create the output containers ##
+    AliAnalysisDataContainer *coutputFastEmbedding = mgr->CreateContainer(
+         "fastembedding", TList::Class(), AliAnalysisManager::kOutputContainer,
+         Form("%s:PWG4_FastEmbedding", AliAnalysisManager::GetCommonFileName()));
+
+    mgr->ConnectInput (task, 0, mgr->GetCommonInputContainer());
+    mgr->ConnectOutput(task, 0, mgr->GetCommonOutputContainer());
+    mgr->ConnectOutput(task, 1, coutputFastEmbedding);
+
+
+    return task;
+
+}
+
+AliAnalysisTaskFastEmbedding* AddTaskFastEmbedding(TObjArray* aodarray){
+
+    AliAnalysisTaskFastEmbedding *task = AddTaskFastEmbedding();
+    if(aodarray){
+      task->SetArrayOfAODPaths(aodarray);
+      task->SetEmbedMode(AliAnalysisTaskFastEmbedding::kAODFull);
+    }
+
+    return task;
+}
+
+
+AliAnalysisTaskFastEmbedding* AddTaskFastEmbedding(const char* filepath){
+
+    AliAnalysisTaskFastEmbedding *task = AddTaskFastEmbedding();
+    if(strlen(filepath)){
+       task->SetAODPath(filepath);
+       task->SetEmbedMode(AliAnalysisTaskFastEmbedding::kAODFull);
+    }
+
+    return task;
+}
+
