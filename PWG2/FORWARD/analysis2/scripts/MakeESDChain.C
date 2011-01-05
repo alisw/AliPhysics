12dbbfa41dbf53b8314a0e65555c5b4f31a5ae8c
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-05 11:35:38 +0000

    Also search sub-directories for ESD files

--- PWG2/FORWARD/analysis2/scripts/MakeESDChain.C
+++ PWG2/FORWARD/analysis2/scripts/MakeESDChain.C
@@ -1,5 +1,53 @@
+void
+ScanDirectory(TSystemDirectory* dir, TChain* chain, bool recursive)
+{
+  // gROOT->IndentLevel();
+  // Printf("Scanning %s ...", dir->GetName());
+  // gROOT->IncreaseDirLevel();
+  
+  
+  // Get list of files, and go back to old working directory
+  TString oldDir(gSystem->WorkingDirectory());
+  TList* files = dir->GetListOfFiles();
+  gSystem->ChangeDirectory(oldDir);
+
+  // Sort list of files and check if we should add it 
+  files->Sort();
+  TIter next(files);
+  TSystemFile* file = 0;
+  while ((file = static_cast<TSystemFile*>(next()))) {
+    TString name(file->GetName());
+    
+    // Ignore special links 
+    if (name == "." || name == "..") continue;
+
+    // Check if this is a directory 
+    if (file->IsDirectory()) { 
+      if (recursive) 
+	ScanDirectory(static_cast<TSystemDirectory*>(file),chain,recursive);
+      continue;
+    }
+    
+    // If this is not a root file, ignore 
+    if (!name.EndsWith(".root")) continue;
+
+    // If this file does not contain AliESDs, ignore 
+    if (!name.Contains("AliESDs")) continue;
+    
+    // Get the path 
+    TString esd(Form("%s/%s", file->GetTitle(), name.Data()));
+
+    // Print and add 
+    // gROOT->IndentLevel();
+    // Printf("adding %s", esd.Data());
+    chain->Add(esd);
+
+  }
+  // gROOT->DecreaseDirLevel();
+}
+
 TChain*
-MakeESDChain(const char* esddir)
+MakeESDChain(const char* esddir, bool recursive=false)
 {
   // --- Our data chain ----------------------------------------------
   TChain* chain = new TChain("esdTree");
@@ -8,21 +56,9 @@ MakeESDChain(const char* esddir)
   // Open source directory, and make sure we go back to were we were 
   TString oldDir(gSystem->WorkingDirectory());
   TSystemDirectory d(esddir, esddir);
-  TList* files = d.GetListOfFiles();
-  gSystem->ChangeDirectory(oldDir);
+  ScanDirectory(&d, chain, recursive);
 
-  // Sort list of files and check if we should add it 
-  files->Sort();
-  TIter next(files);
-  TSystemFile* file = 0;
-  while ((file = static_cast<TSystemFile*>(next()))) {
-    if (file->IsDirectory()) continue;
-    TString name(file->GetName());
-    if (!name.EndsWith(".root")) continue;
-    if (!name.Contains("AliESDs")) continue;
-    TString esd(Form("%s/%s", file->GetTitle(), name.Data()));
-    Info("RunManager", "Adding %s to chain", esd.Data());
-    chain->Add(esd);
-  }  
+  // chain->GetListOfFiles()->ls();
+
   return chain;
 }
