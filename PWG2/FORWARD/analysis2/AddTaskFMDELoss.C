aee1ebe5a0ec1d57132b0dd9e81625429571ac06
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-17 16:16:45 +0000

    Macro to add the energy distribution task to the train

---
+++ PWG2/FORWARD/analysis2/AddTaskFMDELoss.C
@@ -0,0 +1,79 @@
+/** 
+ * @defgroup pwg2_forward_scripts Scripts used in the analysis
+ *
+ * @ingroup pwg2_forward
+ */
+/**
+ * @file 
+ * @ingroup pwg2_forward_scripts
+ * 
+ */
+/**
+ * This is the macro to include the FMD energy fitter in a train.  
+ * 
+ * @ingroup pwg2_forward_scripts
+ */
+AliAnalysisTask*
+AddTaskFMDELoss(Bool_t mc)
+{
+  gSystem->Load("libPWG2forward2");
+
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    Error("AddTaskFMD", "No analysis manager to connect to.");
+    return NULL;
+  }   
+
+  // --- Make the task and add it to the manager ---------------------
+  AliFMDEnergyFitterTask* task = new AliFMDEnergyFitterTask("fmdEnergyFitter");
+  mgr->AddTask(task);
+   // --- Set parameters on the algorithms ----------------------------
+  // Set the number of SPD tracklets for which we consider the event a
+  // low flux event
+  task->GetEventInspector().SetLowFluxCut(1000); 
+  // Set the maximum error on v_z [cm]
+  task->GetEventInspector().SetMaxVzErr(0.2);
+  // Set the eta axis to use - note, this overrides whatever is used
+  // by the rest of the algorithms - but only for the energy fitter
+  // algorithm. 
+  task->GetEnergyFitter().SetEtaAxis(200, -4, 6);
+  // Set maximum energy loss to consider 
+  task->GetEnergyFitter().SetMaxE(15); 
+  // Set number of energy loss bins 
+  task->GetEnergyFitter().SetNEbins(100);
+  // Set whether to use increasing bin sizes 
+  task->GetEnergyFitter().SetUseIncreasingBins(true);
+  // Set whether to do fit the energy distributions 
+  task->GetEnergyFitter().SetDoFits(kTRUE);
+  // Set whether to make the correction object 
+  task->GetEnergyFitter().SetDoMakeObject(kTRUE);
+  // Set the low cut used for energy
+  task->GetEnergyFitter().SetLowCut(0.4);
+  // Set the number of bins to subtract from maximum of distributions
+  // to get the lower bound of the fit range
+  task->GetEnergyFitter().SetFitRangeBinWidth(4);
+  // Set the maximum number of landaus to try to fit (max 5)
+  task->GetEnergyFitter().SetNParticles(5);
+  // Set the minimum number of entries in the distribution before
+  // trying to fit to the data
+  task->GetEnergyFitter().SetMinEntries(1000);
+  // --- Set limits on fits the energy -------------------------------
+  // Maximum relative error on parameters 
+  AliFMDCorrELossFit::ELossFit::fgMaxRelError = .12;
+  // Least weight to use 
+  AliFMDCorrELossFit::ELossFit::fgLeastWeight = 1e-5;
+  // Maximum value of reduced chi^2 
+  AliFMDCorrELossFit::ELossFit::fgMaxChi2nu   = 5;
+    
+  // --- Make the output container and connect it --------------------
+  TString outputfile = AliAnalysisManager::GetCommonFileName();
+  // outputfile += ":PWG2forwardDnDeta"; 
+  // Form(":%s",pars->GetDndetaAnalysisName());
+  AliAnalysisDataContainer* histOut = 
+    mgr->CreateContainer("Forward", TList::Class(), 
+			 AliAnalysisManager::kOutputContainer,outputfile);
+  mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+  mgr->ConnectOutput(task, 1, histOut);
+
+  return task;
+}
