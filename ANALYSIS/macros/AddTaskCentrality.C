dcb440622e71e272b3456470b1ec52e8a77bb9c2
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-21 14:09:59 +0000

    reverting previous commit

--- ANALYSIS/macros/AddTaskCentrality.C
+++ ANALYSIS/macros/AddTaskCentrality.C
@@ -24,7 +24,7 @@ AliCentralitySelectionTask *AddTaskCentrality()
   AliAnalysisDataContainer *cinput0 = mgr->GetCommonInputContainer();
   AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("CentralityStat",
                 TList::Class(), AliAnalysisManager::kOutputContainer,
-                "Centrality_temp.root");
+                "EventStat_temp.root");
   
   mgr->ConnectInput(centralityTask, 0, mgr->GetCommonInputContainer());
   mgr->ConnectOutput(centralityTask,1,coutput1);
