7c3fe48c25be874041346c4dae358d28cc90807e
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-25 13:12:11 +0000

    New analysis task objects with the capability to do mixing

---
+++ PWG2/RESONANCES/AliRsnAnalysisTask.h
@@ -0,0 +1,56 @@
+//
+// Class AliRsnAnalysisTask
+//
+// Virtual Class derivated from AliRsnVAnalysisTask which will be base class
+// for all RSN SE tasks
+//
+// authors: Martin Vala (martin.vala@cern.ch)
+//          Alberto Pulvirenti (alberto.pulvirenti@ct.infn.it)
+//
+#ifndef ALIRSNANALYSISTASK_H
+#define ALIRSNANALYSISTASK_H
+
+#include "AliPID.h"
+#include "AliRsnVAnalysisTask.h"
+#include "AliRsnAnalysisManager.h"
+#include "AliRsnEvent.h"
+#include "AliRsnCutSet.h"
+
+class AliRsnPIDDefESD;
+
+class AliRsnAnalysisTask : public AliRsnVAnalysisTask
+{
+  public:
+  
+    AliRsnAnalysisTask(const char *name = "AliRsnAnalysisTask", Bool_t useKine = kFALSE);
+    AliRsnAnalysisTask(const AliRsnAnalysisTask& copy);
+    AliRsnAnalysisTask& operator=(const AliRsnAnalysisTask& copy);
+    virtual ~AliRsnAnalysisTask() {}
+
+    virtual void            RsnUserCreateOutputObjects();
+    virtual void            RsnUserExec(Option_t*);
+    virtual void            RsnUserExecMix(Option_t* );
+    virtual void            RsnTerminate(Option_t*);
+    virtual Bool_t          EventProcess();
+
+    AliRsnCutSet*           GetEventCuts()                           {return &fEventCuts;}
+    AliRsnAnalysisManager*  GetAnalysisManager()                     {return &fRsnAnalysisManager;}
+    void                    SetAnalysisManagerName(const char *name) {fRsnAnalysisManager.SetName(name);}
+
+    Double_t                GetZeroEventPercentWarning() const            {return fZeroEventPercentWarning;}
+    void                    SetZeroEventPercentWarning(Double_t val = 50) {fZeroEventPercentWarning = val;}
+    void                    UseZeroEventWarning(Bool_t b = kTRUE)         {fUseZeroEventWarning = b;}
+
+  private:
+
+    AliRsnAnalysisManager   fRsnAnalysisManager;      // analysis main engine
+    AliRsnCutSet            fEventCuts;               // event cuts
+    TList                  *fOutList;                 // list of output events
+
+    Double_t                fZeroEventPercentWarning; // Percent Number for Zero Event Warning
+    Bool_t                  fUseZeroEventWarning;     // flag if Zero Event Warning is used (default is true)
+
+    ClassDef(AliRsnAnalysisTask, 1)
+};
+
+#endif
---
+++ PWG2/RESONANCES/AliRsnVAnalysisTask.h
@@ -0,0 +1,90 @@
+//
+// Class AliRsnVAnalysisTask
+//
+// Virtual Class derivated from AliAnalysisTaskSE which will be base class
+// for all RSN Multi tasks
+//
+// authors: Martin Vala (martin.vala@cern.ch)
+//          Alberto Pulvirenti (alberto.pulvirenti@ct.infn.it)
+//
+
+#ifndef ALIRSNVANALYSISTASK_H
+#define ALIRSNVANALYSISTASK_H
+
+#include <TH1.h>
+
+#include "AliAnalysisTaskSE.h"
+
+#include "AliRsnEvent.h"
+#include "AliRsnVATProcessInfo.h"
+#include <AliMixInputEventHandler.h>
+
+class AliESDEvent;
+class AliAODEvent;
+class AliMCEvent;
+
+class AliRsnVAnalysisTask : public AliAnalysisTaskSE
+{
+  public:
+  
+    AliRsnVAnalysisTask(const char *name = "AliRsnVAnalysisTask", Bool_t mcOnly = kFALSE);
+    AliRsnVAnalysisTask(const AliRsnVAnalysisTask& copy);
+    AliRsnVAnalysisTask& operator= (const AliRsnVAnalysisTask& /*copy*/) { return *this; }
+    virtual ~AliRsnVAnalysisTask() {/* Does nothing*/;}
+
+    // basic interface methods
+    virtual void    LocalInit();
+    virtual Bool_t  UserNotify();
+    virtual void    ConnectInputData(Option_t *opt);
+    virtual void    UserCreateOutputObjects();
+    virtual void    UserExec(Option_t* opt);
+		virtual void    UserExecMix(Option_t* option = "");
+    virtual void    Terminate(Option_t* opt);
+
+    // customized methods (to be implemented in derived classes)
+    virtual void    RsnUserCreateOutputObjects();
+    virtual void    RsnUserExec(Option_t*);
+    virtual void    RsnUserExecMix(Option_t*);
+    virtual void    RsnTerminate(Option_t*);
+
+    // event pre-processing functions
+    virtual Bool_t  EventProcess();
+
+    // getters
+    AliRsnEvent*           GetRsnEvent(Int_t i=0) {return &fRsnEvent[i];}
+    AliRsnVATProcessInfo*  GetInfo()     {return &fTaskInfo;}
+
+    // setters
+    void SetMCOnly(Bool_t mcOnly = kTRUE)                           {fMCOnly = mcOnly;}
+    void SetLogType(AliLog::EType_t type, const char *classes = "") {fLogType = type; fLogClassesString = classes;}
+    void SetPrintInfoNumber(const Long64_t &num = 100)              {fTaskInfo.SetPrintInfoNumber(num);}
+    
+    void SetMixing(Bool_t doMix = kTRUE) {fIsMixing = doMix;}
+    Bool_t IsMixing() { return fIsMixing;}
+
+  protected:
+
+    AliLog::EType_t         fLogType;          //  log type
+    TString                 fLogClassesString; //  all classes string divided with ":"
+
+    AliESDEvent            *fESDEvent[2];         //  ESD event
+    AliMCEvent             *fMCEvent[2];          //  MC event
+    AliAODEvent            *fAODEventIn[2];       //  AOD event from input
+    AliAODEvent            *fAODEventOut[2];      //  AOD event from output from previous taks
+
+    Bool_t                  fIsMixing;        // flag is using mixing
+    Bool_t                  fMCOnly;           //  use only MC information
+    AliRsnEvent             fRsnEvent[2];         //  interface to event for RSN package
+
+    TList                  *fInfoList;         //! output list for informations
+    AliRsnVATProcessInfo    fTaskInfo;         //  task info
+    
+    AliMixInputEventHandler *fMixedEH;         //! mixed event hadnler
+
+    void                    SetupMixingEvents();
+    void                    SetDebugForAllClasses();
+
+    ClassDef(AliRsnVAnalysisTask, 1)
+};
+
+#endif
