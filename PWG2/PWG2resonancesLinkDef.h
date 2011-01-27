6af2445fae9fdefa48d7ca7b32f66e6f8b142390
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-27 14:34:25 +0000

    Added a utiliy class for TOF correction, extrapolated from all places where it is used

--- PWG2/PWG2resonancesLinkDef.h
+++ PWG2/PWG2resonancesLinkDef.h
@@ -48,6 +48,7 @@
 #pragma link C++ class AliRsnAnalysisMonitorTask+;
 
 #pragma link C++ class AliRsnTOFT0maker+;
+#pragma link C++ class AliRsnTOFCorrectionESD+;
 #pragma link C++ class AliRsnAnalysisPhi900GeV+;
 #pragma link C++ class AliRsnAnalysisPhi7TeV+;
 #pragma link C++ class AliRsnAnalysisPhi7TeVNoPID+;
