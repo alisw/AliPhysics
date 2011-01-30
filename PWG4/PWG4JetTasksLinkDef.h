87a08b6c9792e1356f49fea251676b56beee9725
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-30 19:46:31 +0000

    write also background subtracted jets to the delta AODmacros/AnalysisTrainPWG4Jets.C

--- PWG4/PWG4JetTasksLinkDef.h
+++ PWG4/PWG4JetTasksLinkDef.h
@@ -35,7 +35,6 @@
 #pragma link C++ class AliAnalysisTaskLeadingTrackUE+;
 #pragma link C++ class AliAnalysisTaskJetsTM+;
 #pragma link C++ class AliAnalysisTaskQGSep+;
-#pragma link C++ class AliAnalysisTaskPhiCorrelations+;
 #pragma link C++ class AliAnalysisTaskJetResponse+;
 
 #endif
