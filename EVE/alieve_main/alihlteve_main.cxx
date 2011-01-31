37391aa749eefd62d60a0f016fb99e75115ceeb3
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-01-31 15:12:34 +0000

    Coverity fix

--- EVE/alieve_main/alihlteve_main.cxx
+++ EVE/alieve_main/alihlteve_main.cxx
@@ -81,7 +81,10 @@ int main(int argc, char **argv)
 
   gEve->RegisterGeometryAlias("Default", Form("%s/alice-data/default_geo.root", evedir.Data()));
 
-  AliEveConfigManager::InitializeMaster();
+	try {AliEveConfigManager::InitializeMaster();}
+	catch (TEveException exc) {
+		AliErrorGeneral("alihlteve_main",exc.Data());
+	}
 
   app->Run(kTRUE);
 
