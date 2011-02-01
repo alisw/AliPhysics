16d42f5c371ff6fe1e66bc0e0d6be9ec844d725e
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-01 19:51:30 +0000

    Adding Cpossibility to create CDB.par

---
+++ STEER/PROOF-INF.CDB/SETUP.C
@@ -0,0 +1,20 @@
+void SETUP()
+{
+   // Load some ROOT libraries
+
+   gSystem->Load("libVMC");
+   gSystem->Load("libNet");
+   gSystem->Load("libTree");
+
+   // Load libSTEERBase, CDB depends on it
+   gSystem->Load("libSTEERBase");
+
+   // Load the CDB library
+   gSystem->Load("libCDB");
+
+   // Set the include paths
+   gROOT->ProcessLine(".include CDB");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("CDB_INCLUDE", "CDB");
+}
