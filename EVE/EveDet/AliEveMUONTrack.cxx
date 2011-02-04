3de3a76db15f2fb6e98319f223e570f4867cc352
vulpescu <vulpescu@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-04 07:56:33 +0000

    Coverity again

--- EVE/EveDet/AliEveMUONTrack.cxx
+++ EVE/EveDet/AliEveMUONTrack.cxx
@@ -779,8 +779,8 @@ void AliEveMUONTrack::Propagate(Float_t *xr, Float_t *yr, Float_t *zr, Int_t i1,
   trackParamAtCluster = fTrack->GetTrackParamAtCluster();
 
   if (IsMUONTrack() || IsESDTrack() || IsRefTrack()) {
-    charge = (Int_t)trackParam->GetCharge();
     trackParam = (AliMUONTrackParam*)trackParamAtCluster->At(i1);
+    charge = (Int_t)trackParam->GetCharge();
   } else {
     return;
   }
