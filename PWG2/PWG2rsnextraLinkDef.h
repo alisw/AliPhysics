b332f8ef0c5d7407238d46075538227599c1e6bb
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-08 17:17:20 +0000

    Splitted resonance library into two separate elements: 1) PWG2resonances:    this contains the basic package, with all necessary to set up a resonance analysis    in the old way, using all the objects developed since the introduction of this package    and without any dependence on non-analysis libraries 2) PWG2rsnextra:    all the classes/code/analysisTasks which do not use the basic RSN package    this library can have dependences on non-analysis code (currently, it depends on TOFrec)    but it has to be viewed as a temporary entry due to current situation of the available reconstruction    or to keep track of analysis tasks related to resonances, but not yet fuly integrated in the standard package

---
+++ PWG2/PWG2rsnextraLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+
+#pragma link C++ class AliRsnTOFT0maker+;
+#pragma link C++ class AliRsnTOFCorrectionESD+;
+#pragma link C++ class AliRsnAnalysisPhi900GeV+;
+#pragma link C++ class AliRsnAnalysisPhi7TeV+;
+#pragma link C++ class AliRsnAnalysisPhi7TeVNoPID+;
+#pragma link C++ class AliAnalysisTaskSigma1385+;
+
+#endif
