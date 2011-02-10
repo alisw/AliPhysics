3094ea3c80cb92c74fd64a0e2feb1df107be0aa0
agheata <agheata@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-10 15:24:05 +0000

    corrected default OCDB path

--- ANALYSIS/TenderSupplies/AddTaskTender.C
+++ ANALYSIS/TenderSupplies/AddTaskTender.C
@@ -16,7 +16,8 @@ AliAnalysisTask *AddTaskTender(Bool_t useV0=kFALSE){
   //========= Add tender to the ANALYSIS manager and set default storage =====
   AliTender *tender=new AliTender("AnalysisTender");
   tender->SetCheckEventSelection(checkEvtSelection);
-  tender->SetDefaultCDBStorage("raw://");
+//  tender->SetDefaultCDBStorage("raw://");
+  tender->SetDefaultCDBStorage("alien://?Folder=/alice/data/2010/OCDB");
   mgr->AddTask(tender);
   if (checkEvtSelection) {
      if (mgr->GetTasks()->First() != (TObject*)tender) {
