0b152f04c085a46ede7a98595ed222691161a455
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-11 22:53:30 +0000

    removing obsolete variable

--- HLT/BASE/util/AliHLTRawReaderPublisherComponent.cxx
+++ HLT/BASE/util/AliHLTRawReaderPublisherComponent.cxx
@@ -1,26 +1,26 @@
 // $Id$
 
-/**************************************************************************
- * This file is property of and copyright by the ALICE HLT Project        * 
- * ALICE Experiment at CERN, All rights reserved.                         *
- *                                                                        *
- * Primary Authors: Matthias Richter <Matthias.Richter@ift.uib.no>        *
- *                  for The ALICE HLT Project.                            *
- *                                                                        *
- * Permission to use, copy, modify and distribute this software and its   *
- * documentation strictly for non-commercial purposes is hereby granted   *
- * without fee, provided that the above copyright notice appears in all   *
- * copies and that both the copyright notice and this permission notice   *
- * appear in the supporting documentation. The authors make no claims     *
- * about the suitability of this software for any purpose. It is          *
- * provided "as is" without express or implied warranty.                  *
- **************************************************************************/
+///**************************************************************************
+///* This file is property of and copyright by the ALICE HLT Project        * 
+///* ALICE Experiment at CERN, All rights reserved.                         *
+///*                                                                        *
+///* Primary Authors: Matthias Richter <Matthias.Richter@ift.uib.no>        *
+///*                  for The ALICE HLT Project.                            *
+///*                                                                        *
+///* Permission to use, copy, modify and distribute this software and its   *
+///* documentation strictly for non-commercial purposes is hereby granted   *
+///* without fee, provided that the above copyright notice appears in all   *
+///* copies and that both the copyright notice and this permission notice   *
+///* appear in the supporting documentation. The authors make no claims     *
+///* about the suitability of this software for any purpose. It is          *
+///* provided "as is" without express or implied warranty.                  *
+///**************************************************************************/
 
-/** @file   AliHLTRawReaderPublisherComponent.cxx
-    @author Matthias Richter
-    @date   
-    @brief  A general tree publisher component for the AliRawReader.
-*/
+/// @file   AliHLTRawReaderPublisherComponent.cxx
+/// @author Matthias Richter
+/// @date   
+/// @brief  A general tree publisher component for the AliRawReader.
+///
 
 // see header file for class documentation
 // or
@@ -201,8 +201,7 @@ int AliHLTRawReaderPublisherComponent::DoInit( int argc, const char** argv )
     //return -EINVAL;
   }
 
-  AliRawReader* pRawReader=GetRawReader();
-  if ((pRawReader=GetRawReader())!=NULL) {
+  if (GetRawReader()!=NULL) {
   } else {
     AliErrorStream() << "RawReader instance needed" << endl;
     return -EINVAL;
