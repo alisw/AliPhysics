dd7d6a2a6d622c3d6fab1b6d2083a1ba3766d629
hdalsgaa <hdalsgaa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-13 21:30:12 +0000

    The revisited SPD cluster analysis in the new AOD framework and the corrections for it for 900 GeV p+p

---
+++ PWG2/FORWARD/analysis2/AliCentralMultiplicityTask.cxx
@@ -0,0 +1,210 @@
+//====================================================================
+// 
+// Base class for classes that calculate the multiplicity in the
+// central region event-by-event
+// 
+// Inputs: 
+//   - AliESDEvent 
+//
+// Outputs: 
+//   - AliAODCentralMult 
+// 
+// Histograms 
+//   
+// Corrections used 
+#include "AliCentralMultiplicityTask.h"
+#include "AliForwardCorrectionManager.h"
+#include "AliForwardUtil.h"
+#include "AliLog.h"
+#include "AliAODHandler.h"
+#include "AliInputEventHandler.h"
+#include "AliESDInputHandler.h"
+#include "AliAnalysisManager.h"
+#include "AliESDEvent.h"
+#include "AliMultiplicity.h"
+#include <TROOT.h>
+#include <TFile.h>
+#include <iostream>
+#include <iomanip>
+
+//====================================================================
+AliCentralMultiplicityTask::AliCentralMultiplicityTask(const char* name) 
+  : AliAnalysisTaskSE(name),
+    fData(0),
+    fList(0),
+    fAODCentral(kFALSE),
+    fManager()
+{
+  
+  DefineOutput(1, TList::Class());
+}
+//____________________________________________________________________
+void AliCentralMultiplicityTask::UserCreateOutputObjects() {
+
+  AliAnalysisManager* am = AliAnalysisManager::GetAnalysisManager();
+  AliAODHandler*      ah = 
+    dynamic_cast<AliAODHandler*>(am->GetOutputEventHandler());
+  if (!ah) AliFatal("No AOD output handler set in analysis manager");
+  
+  
+  TObject* obj = &fAODCentral;
+  ah->AddBranch("AliAODCentralMult", &obj);
+  
+  fList = new TList();
+  PostData(1,fList);
+  
+}
+//____________________________________________________________________
+void AliCentralMultiplicityTask::UserExec(Option_t* /*option*/) {
+  
+  AliESDInputHandler* eventHandler = 
+    dynamic_cast<AliESDInputHandler*> (AliAnalysisManager::GetAnalysisManager()
+				       ->GetInputEventHandler());
+  if (!eventHandler) {
+    AliWarning("No inputhandler found for this event!");
+    return;}
+  
+  AliESDEvent* esd = eventHandler->GetEvent();
+  
+  //Selecting only events with |valid vertex| < 10 cm
+  const AliESDVertex* vertex = esd->GetPrimaryVertexSPD();
+  if (!vertex) return;
+  if(!(vertex->GetStatus())) return;
+  if(vertex->GetNContributors() <= 0) return ;
+  if(vertex->GetZRes() > 0.1 ) return;
+  Double_t vertexXYZ[3]={0,0,0};
+  vertex->GetXYZ(vertexXYZ);
+  if(TMath::Abs(vertexXYZ[2]) > 10) return;
+  
+  Double_t delta = 2 ;
+  Double_t vertexBinDouble = (vertexXYZ[2] + 10) / delta;
+  Int_t vtxbin = (Int_t)vertexBinDouble +1 ; //HHD: The vtxbins are 1-10, not 0-9
+  
+  // Make sure AOD is filled
+  AliAnalysisManager* am = AliAnalysisManager::GetAnalysisManager();
+  AliAODHandler*      ah = 
+    dynamic_cast<AliAODHandler*>(am->GetOutputEventHandler());
+  if (!ah)  
+    AliFatal("No AOD output handler set in analysis manager");
+  
+  ah->SetFillAOD(kTRUE);
+  
+  //Doing analysis
+  fAODCentral.Clear("");
+  TH2D *aodHist = &(fAODCentral.GetHistogram());
+  
+  const AliMultiplicity* spdmult = esd->GetMultiplicity();
+  //Filling clusters in layer 1 used for tracklets...
+  for(Int_t j = 0; j< spdmult->GetNumberOfTracklets();j++) {
+    aodHist->Fill(spdmult->GetEta(j),spdmult->GetPhi(j));
+  }
+  //...and then the unused ones in layer 1 
+  for(Int_t j = 0; j< spdmult->GetNumberOfSingleClusters();j++) {
+    aodHist->Fill(-TMath::Log(TMath::Tan(spdmult->GetThetaSingle(j)/2.)),
+		   spdmult->GetPhiSingle(j));
+  }
+  
+  // Corrections
+  
+  TH2D* hSecMap     = fManager.GetSecMapCorrection(vtxbin);
+  TH1D* hAcceptance = fManager.GetAcceptanceCorrection(vtxbin);
+  
+  aodHist->Divide(hSecMap);
+  
+  for(Int_t nx = 1; nx <= aodHist->GetNbinsX(); nx++) {
+    Float_t acccor = hAcceptance->GetBinContent(nx);
+    
+    Bool_t etabinSeen = kFALSE;  
+    for(Int_t ny = 1; ny <= aodHist->GetNbinsY(); ny++) {
+      Float_t aodvalue = aodHist->GetBinContent(nx,ny);
+      Float_t seccor = hSecMap->GetBinContent(nx,ny);
+      if(seccor > 0.5) etabinSeen = kTRUE;
+      if(aodvalue < 0.000001) { aodHist->SetBinContent(nx,ny, 0); continue; }
+      
+      Float_t aodnew   = aodvalue / acccor ;
+      aodHist->SetBinContent(nx,ny, aodnew);
+      Float_t error = aodnew*TMath::Sqrt(TMath::Power(aodHist->GetBinError(nx,ny)/aodvalue,2) +
+					 TMath::Power(hAcceptance->GetBinError(nx)/acccor,2) );
+      aodHist->SetBinError(nx,ny,error);
+      
+    }
+    //Filling underflow bin if we eta bin is in range
+    if(etabinSeen) aodHist->SetBinContent(nx,0, 1.);
+    
+  }  
+
+  PostData(1,fList);
+  
+}
+//____________________________________________________________________
+void AliCentralMultiplicityTask::Terminate(Option_t* /*option*/) {
+
+
+
+}
+//____________________________________________________________________
+void
+AliCentralMultiplicityTask::Print(Option_t* /*option*/) const
+{
+   
+}
+//____________________________________________________________________
+AliCentralMultiplicityTask::Manager::Manager() :
+  fAcceptancePath("$ALICE_ROOT/PWG2/FORWARD/corrections/CentralAcceptance"),
+  fSecMapPath("$ALICE_ROOT/PWG2/FORWARD/corrections/CentralSecMap"),
+  fAcceptance(),
+  fSecmap(),
+  fAcceptanceName("centralacceptance"),
+  fSecMapName("centralsecmap")
+{
+
+
+}
+//____________________________________________________________________
+const char* AliCentralMultiplicityTask::Manager::GetFileName(UShort_t  what ,
+							     UShort_t  sys, 
+							     UShort_t  sNN,
+							     Short_t   field) {
+  
+  TString fname = "";
+  
+  switch(what) {
+  case 0:
+    fname.Append(fSecMapName.Data());
+    break;
+  case 1: 
+    fname.Append(fAcceptanceName.Data());
+    break;
+  default:
+    printf("Invalid indentifier for central object, must be 0 or 1!");
+    break;
+  }
+  fname.Append(Form("_%s_%04dGeV_%c%1dkG.root", 
+		    AliForwardUtil::CollisionSystemString(sys), 
+		    sNN, (field < 0 ? 'm' : 'p'), TMath::Abs(field)));
+  
+  return fname.Data();
+  
+}
+//____________________________________________________________________
+void AliCentralMultiplicityTask::Manager::Init( UShort_t  sys, 
+						UShort_t  sNN,
+						Short_t   field) {
+  
+  TFile fsec(GetFullFileName(0,sys,sNN,field));
+  fSecmap = dynamic_cast<AliCentralCorrSecondaryMap*>(fsec.Get(fSecMapName.Data()));  
+  if(!fSecmap) {
+    printf("no central Secondary Map found!") ;
+    return;
+  }
+  TFile facc(GetFullFileName(1,sys,sNN,field));
+  fAcceptance = dynamic_cast<AliCentralCorrAcceptance*>(facc.Get(fAcceptanceName.Data()));
+if(!fAcceptance) {
+  printf("no central Acceptance found!") ;
+  return;
+ }
+
+}
+//
+// EOF
+//
