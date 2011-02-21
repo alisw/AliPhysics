47fea8fdafbb83b4de233dd5d963a244b81c6a89
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-21 16:09:58 +0000

    Changes to Ds task (Gian Michele)

--- PWG3/vertexingHF/macros/AddTaskDs.C
+++ PWG3/vertexingHF/macros/AddTaskDs.C
@@ -1,98 +1,75 @@
-AliAnalysisTaskSEDs *AddTaskDs(Bool_t readMC=kTRUE)
+AliAnalysisTaskSEDs *AddTaskDs(Bool_t readMC=kFALSE,
+				     TString filename="DstoKKpiCuts.root")
 {
-  //                                                                           
+  //                                                                                                                                    
   // Test macro for the AliAnalysisTaskSE for Ds candidates 
 
+  //Invariant mass histogram and                                                 
+  // association with MC truth (using MC info in AOD)                                                                                   
+  // Origin: R. Bala, bala@to.infn.it         
+  // Modified for Ds meson: G.M. Innocenti innocent@to.infn.it
+  // Get the pointer to the existing analysis manager via the static access method.                                                     
+  //==============================================================================                                                      
 
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
     ::Error("AddTaskDs", "No analysis manager to connect to.");
-    return NULL;
   }
 
 
-
-  //ptbins
-  const Int_t nptbins=4;
-  Float_t ptbins[nptbins+1]={0.,2.,3.,5.,99999.};
-
-  //setting cut values
-  Int_t nvars=14;
-  Float_t** ancutsval;
-  ancutsval=new Float_t*[nvars];
-  for(Int_t ic=0;ic<nvars;ic++){ancutsval[ic]=new Float_t[nptbins];}  
-  for(Int_t ipt=0;ipt<nptbins;ipt++){
-    ancutsval[0][ipt]=0.2;
-    ancutsval[1][ipt]=0.4;
-    ancutsval[2][ipt]=0.4;
-    ancutsval[3][ipt]=0.;
-    ancutsval[4][ipt]=0.;
-    ancutsval[5][ipt]=0.005;
-    ancutsval[6][ipt]=0.038;
-    ancutsval[7][ipt]=0.;
-    ancutsval[8][ipt]=0.;
-    ancutsval[9][ipt]=0.95;
-    ancutsval[10][ipt]=0.;
-    ancutsval[11][ipt]=0.1;
-    ancutsval[12][ipt]=0.004;
-    ancutsval[13][ipt]=0.035;
+  TFile* filecuts=new TFile(filename.Data());
+  if(!filecuts->IsOpen()){
+    cout<<"Error: Input file not found!"<<endl;
   }
-  Float_t** prcutsval;
-  prcutsval=new Float_t*[nvars];
-  for(Int_t ic=0;ic<nvars;ic++){prcutsval[ic]=new Float_t[nptbins];}  
-  for(Int_t ipt=0;ipt<nptbins;ipt++){
-    prcutsval[0][ipt]=0.2;
-    prcutsval[1][ipt]=0.4;
-    prcutsval[2][ipt]=0.4;
-    prcutsval[3][ipt]=0.;
-    prcutsval[4][ipt]=0.;
-    prcutsval[5][ipt]=0.005;
-    prcutsval[6][ipt]=0.06;
-    prcutsval[7][ipt]=0.;
-    prcutsval[8][ipt]=0.;
-    prcutsval[9][ipt]=0.85;
-    prcutsval[10][ipt]=0.;
-    prcutsval[11][ipt]=0.1;
-    prcutsval[12][ipt]=0.1;
-    prcutsval[13][ipt]=0.1;
-  }
+  
+  
+  //Analysis Task
 
-  //Analysis cuts
-  AliRDHFCutsDstoKKpi *analysiscuts = new AliRDHFCutsDstoKKpi();
-  analysiscuts->SetPtBins(nptbins+1,ptbins);
-  analysiscuts->SetCuts(nvars,nptbins,ancutsval);
-  AliRDHFCutsDstoKKpi *prodcuts = new AliRDHFCutsDstoKKpi();
-  prodcuts->SetPtBins(nptbins+1,ptbins);
-  prodcuts->SetCuts(nvars,nptbins,prcutsval);
+  
+  AliRDHFCutsDstoKKpi* analysiscuts=new AliRDHFCutsDstoKKpi();
+  analysiscuts = (AliRDHFCutsDstoKKpi*)filecuts->Get("AnalysisCuts");
 
-  // Analysis task                                                                                                                     
+  AliRDHFCutsDstoKKpi* prodcuts=new AliRDHFCutsDstoKKpi();
+  prodcuts = (AliRDHFCutsDstoKKpi*)filecuts->Get("ProdCuts");
+  
+  //AliRDHFCutsDstoKKpi *prodcuts = (AliRDHFCutsDstoKKpi*)fileCuts->Get("ProdCuts");
+  //AliRDHFCutsDstoKKpi *analysiscuts = (AliRDHFCutsDstoKKpi*)fileCuts->Get("AnalysisCuts");
+
+  
   AliAnalysisTaskSEDs *dsTask = new AliAnalysisTaskSEDs("DsAnalysis",prodcuts,analysiscuts);
   dsTask->SetReadMC(readMC);
-  dsTask->SetDebugLevel(0);
+  //dsTask->SetDoLikeSign(kTRUE);
+  //  dsTask->SetUseTPCpid(kTRUE);
+  //dsTask->SetUseTOFpid(kTRUE);
+  dsTask->SetDebugLevel(10);
+  //dsTask->SetMassLimits(0.2);
   mgr->AddTask(dsTask);
-
-  //                                                                                                                                    
-  // Create containers for input/output                                                                                                 
+  
+  // Create containers for input/output 
+  
   AliAnalysisDataContainer *cinputDs = mgr->CreateContainer("cinputDs",TChain::Class(),
-							    AliAnalysisManager::kInputContainer);
-
- TString outputfile = AliAnalysisManager::GetCommonFileName(); 
- outputfile += ":PWG3_D2H_InvMassDs";
- 
- AliAnalysisDataContainer *coutputDsCuts = mgr->CreateContainer("coutputDsCuts",TList::Class(),
-                                                            AliAnalysisManager::kOutputContainer,
- 								outputfile.Data());
-
- 
+							       AliAnalysisManager::kInputContainer);
+  TString outputfile = AliAnalysisManager::GetCommonFileName();
+  outputfile += ":PWG3_D2H_InvMassDs";
+  
+  AliAnalysisDataContainer *coutputDsCuts = mgr->CreateContainer("coutputDsCuts",TList::Class(),
+								    AliAnalysisManager::kOutputContainer,
+								    outputfile.Data());
+  
   AliAnalysisDataContainer *coutputDs = mgr->CreateContainer("coutputDs",TList::Class(),
-							     AliAnalysisManager::kOutputContainer,
-							     outputfile.Data());
-
-
+								AliAnalysisManager::kOutputContainer,
+								outputfile.Data());
+  AliAnalysisDataContainer *coutputDsNorm = mgr->CreateContainer("coutputDsNorm",AliNormalizationCounter::Class(),
+								AliAnalysisManager::kOutputContainer,
+								outputfile.Data());
+  
   mgr->ConnectInput(dsTask,0,mgr->GetCommonInputContainer());
-
+  
   mgr->ConnectOutput(dsTask,1,coutputDs);
-
+  
   mgr->ConnectOutput(dsTask,2,coutputDsCuts);
+
+  mgr->ConnectOutput(dsTask,3,coutputDsNorm);  
+  
   return dsTask;
 }
