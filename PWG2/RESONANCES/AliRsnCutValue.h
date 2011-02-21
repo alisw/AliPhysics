d70daced2651a9776062a8a9bd4a99b342c6b357
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-21 08:09:52 +0000

    Removed AliRsnPairDef data member. If neede, it must be set as support object in a value computed on pairs

--- PWG2/RESONANCES/AliRsnCutValue.h
+++ PWG2/RESONANCES/AliRsnCutValue.h
@@ -6,7 +6,7 @@
 // Since that value is implemented always as a Double_t one,
 // then this cut operates only with the Double_t data members
 // of the AliRsnCut base class.
-// It allows to cusomize the reference AliRsnValue object by
+// It allows to customize the reference AliRsnValue object by
 // means of a getter that returns a pointer to it.
 // This cut can apply to any kind of object, but the type of
 // target must be one of those for which the chosen value type
@@ -21,21 +21,17 @@
 #include "AliRsnCut.h"
 #include "AliRsnValue.h"
 
-class AliRsnPairDef;
-
 class AliRsnCutValue : public AliRsnCut {
 public:
 
    AliRsnCutValue();
-   AliRsnCutValue(const char *name, AliRsnValue::EValueType type, Double_t min, Double_t max, AliRsnPairDef *pd = 0x0);
+   AliRsnCutValue(const char *name, AliRsnValue::EValueType type, Double_t min, Double_t max);
    AliRsnCutValue(const AliRsnCutValue& copy);
    AliRsnCutValue& operator=(const AliRsnCutValue& copy);
    virtual ~AliRsnCutValue() { }
 
-   void           SetPairDef(AliRsnPairDef *pd) {fPairDef = pd;}
-   AliRsnPairDef* GetPairDef()  {return fPairDef;}
-   Double_t       GetValue()    {return fValue.GetComputedValue();}
-   AliRsnValue*   GetValueObj() {return &fValue;}
+   Double_t       GetComputedValue() {return fValue.GetComputedValue();}
+   AliRsnValue*   GetValueObj()      {return &fValue;}
 
    virtual Bool_t IsSelected(TObject *object);
    virtual void   Print(const Option_t *option = "") const;
@@ -43,7 +39,6 @@ public:
 protected:
 
    AliRsnValue    fValue;
-   AliRsnPairDef *fPairDef;
 
    ClassDef(AliRsnCutValue, 1)
 };
