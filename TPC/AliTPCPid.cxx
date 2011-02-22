c6af91231ff733590f696deaa2967cdd8b6aa115
kowal2 <kowal2@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-22 08:32:56 +0000

    code defect - nonused pointer - corrected

--- TPC/AliTPCPid.cxx
+++ TPC/AliTPCPid.cxx
@@ -489,9 +489,9 @@ AliTPCPid::AliTPCPid(Int_t ntrack):TObject(),
 
     //fCutKa = new TF1("fkaons","[0]/x/x+[1]",0.1,1.2);
     //fCutPr = new TF1("fprotons","[0]/x/x +[1]",0.2,1.2);
-    TF1 *frmska=0;
+    //TF1 *frmska=0;
     
-    frmska = new TF1("x_frmska","1.46-7.82*x+16.78*x^2-15.53*x^3+5.24*x^4 ",
+    TF1 frmska("x_frmska","1.46-7.82*x+16.78*x^2-15.53*x^3+5.24*x^4 ",
 	        0.1,1.2);
     fCutKa = new TF1("fkaons",
 	   "1.25+0.044/x/x+1.25+0.044*x-13.87*x^2+22.37*x^3-10.05*x^4-2.5*x_frmska",
