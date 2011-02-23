af528fe5421a2cb635a92752f847dff2b2010b06
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-02-23 10:13:19 +0000

    Modifications to Run.sh script

--- PWG2/FORWARD/analysis2/Run.sh
+++ PWG2/FORWARD/analysis2/Run.sh
@@ -2,8 +2,6 @@
 
 ana=$ALICE_ROOT/PWG2/FORWARD/analysis2
 nev=-1
-noanal=0
-nodraw=0
 rebin=1
 vzmin=-10
 vzmax=10
@@ -16,8 +14,15 @@ cms=900
 hhd=1
 comp=1
 tit=
-pass1=Pass1.C
-output=forward.root
+dopass1=0
+dopass2=0
+dopass3=0
+pass1=MakeAOD.C
+pass2=MakedNdeta.C
+pass3=DrawdNdeta.C
+output1=forward.root
+output2=forward_dndeta.root
+gdb_script=$ALICE_ROOT/PWG2/FORWARD/analysis2/gdb_cmds
 
 usage()
 {
@@ -28,16 +33,17 @@ Do Pass1 and Pass2 on ESD files in current directory.
 
 Options:
 	-h,--help		This help                  
-	-n,--events N		Number of events           ($nev)
-	-1,--pass1 		Run only pass 1, no draw   ($nodraw)
-	-2,--pass2		Run only pass 2, just draw ($noanal)
-	-v,--vz-min CM          Minimum value of vz        ($vzmin)
-	-V,--vz-max CM          Maximum value of vz        ($vzmax)
-	-t,--trigger TYPE       Select trigger TYPE        ($type)
-	-b,--batch              Do batch processing        ($batch)
-	-P,--proof NWORKERS	Run in PROOF(Lite) mode    ($proof)
-	-M,--mc			Run over MC data           ($mc)
-	-g,--gdb		Run in GDB mode    	   ($gdb)
+	-n,--events N		Number of events            ($nev)
+	-1,--pass1 		Run only pass 1, only AOD   ($dopass2)
+	-2,--pass2		Run only pass 2, only Hists ($dopass1)
+	-D,--draw		Draw results                ($dopass3)
+	-v,--vz-min CM          Minimum value of vz         ($vzmin)
+	-V,--vz-max CM          Maximum value of vz         ($vzmax)
+	-t,--trigger TYPE       Select trigger TYPE         ($type)
+	-b,--batch              Do batch processing         ($batch)
+	-P,--proof NWORKERS	Run in PROOF(Lite) mode     ($proof)
+	-M,--mc			Run over MC data            ($mc)
+	-g,--gdb		Run in GDB mode    	    ($gdb)
 	-E,--eloss		Run energy loss script     
 
 TYPE is a comma or space separated list of 
@@ -58,19 +64,23 @@ toggle()
 
 while test $# -gt 0 ; do
     case $1 in 
-	-h|--help)           usage            ; exit 0;; 
-	-n|--events)         nev=$2           ; shift ;; 
-	-2|--pass2)          noanal=`toggle $noanal`   ;; 
-	-1|--pass1)          nodraw=`toggle $nodraw`   ;; 
-	-b|--batch)          batch=`toggle $batch`   ;; 
-	-P|--proof)          proof=$2	      ; shift ;; 
-	-M|--mc)             mc=`toggle $mc`   ;; 
-	-g|--gdb)            gdb=`toggle $gdb`   ;; 
-	-v|--vz-min)         vzmin=$2         ; shift ;; 
-	-V|--vz-max)         vzmax=$2         ; shift ;; 
-	-E|--eloss)          pass1=MakeELossFits.C ; 
-	                     output=forward_eloss.root ;
-			     nodraw=1 ;;
+	-h|--help)            usage            ; exit 0;; 
+	-n|--events)          nev=$2           ; shift ;; 
+	-3|--pass3|-D|--draw) dopass3=`toggle $dopass3`   ;; 
+	-2|--pass2|-H|--hist) dopass2=`toggle $dopass2`   ;; 
+	-1|--pass1|-A|--aod)  dopass1=`toggle $dopass1`   ;; 
+	-b|--batch)           batch=`toggle $batch`   ;; 
+	-P|--proof)           proof=$2	      ; shift ;; 
+	-M|--mc)              mc=`toggle $mc`   ;; 
+	-g|--gdb)             gdb=`toggle $gdb`   ;; 
+	-v|--vz-min)          vzmin=$2         ; shift ;; 
+	-V|--vz-max)          vzmax=$2         ; shift ;; 
+	-E|--eloss)           pass1=MakeELossFits.C 
+	                      pass2=scripts/ExtractELoss.C
+	                      pass3=scripts/DrawAnaELoss.C 
+	                      output1=forward_eloss.root 
+			      dopass2=1 
+			     ;;
 	-t|--type)           
 	    if test "x$type" = "x" ; then type=$2 ; else type="$type|$2"; fi
 	    shift ;;
@@ -87,7 +97,7 @@ fi
 opts="-l -x"
 opts1=""
 redir=
-if test $nodraw -lt 1 ; then 
+if test $dopass2 -gt 0 ; then 
     opts1="-q" 
 fi
 if test $batch -gt 0 ; then 
@@ -95,12 +105,11 @@ if test $batch -gt 0 ; then
     redir="2>&1 | tee ${base}.log"
     echo "redir=$redir"
 fi 
-if test $noanal -lt 1 ; then 
-    rm -f AnalysisResult.root AliAODs.root
-    rm -f fmdana.png
+if test $dopass1 -gt 0 ; then 
+    rm -f ${output} AliAOD.root
 
     if test $gdb -gt 0 ; then 
-	export PROOF_WRAPPERCMD="gdb -batch -x $ALICE_ROOT/PWG2/FORWARD/analysis2/gdb_cmds --args"
+	export PROOF_WRAPPERCMD="gdb -batch -x ${gdb_script} --args"
     fi
     echo "Running aliroot ${opts} ${opts1} ${ana}/${pass1}\(\".\",$nev,$proof,$mc\) $redir"
     if test $batch -gt 0 ; then 
@@ -109,25 +118,46 @@ if test $noanal -lt 1 ; then
 	aliroot $opts $opts1 ${ana}/${pass1}\(\".\",$nev,$proof,$mc\)
     fi
     fail=$?
-    rm -f outputs_valid \
-	`printf %09d.stat $nev` 
     if  test $fail -gt 0  ; then 
         echo "Return value $fail not 0" ; exit $fail 
     fi
-    if test ! -f ${output} ; then 
-	echo "$output not made" ; exit 1; 
+    if test ! -f ${output1} ; then 
+	echo "$output1 not made" ; exit 1; 
     fi
-    if test ! -f AliAODs.root ; then 
+    if test ! -f AliAOD.root ; then 
 	echo "No AOD creates" ; exit 1;
     fi
-    echo "Analysis done"
+    echo "Pass 1 done"
 fi
 
-if test $nodraw -lt 1 ; then
-    rm -f result.root 
+if test $dopass2 -gt 0 ; then
+    rm -f ${output2}
+    args="\(\".\",$nev,\"$type\",$vzmin,$vzmax,$proof\)"
+    if test "x$pass1" = "xMakeELossFits.C" ; then 
+	args="\(\"${output1}\"\)"
+    fi
+    echo "Running aliroot ${opts} ${opts1} ${ana}/${pass2}.C${args}"
+    aliroot ${opts} ${ana}/${pass2}.C${args}
+
+    fail=$? 
+    if test $fail -gt 0 ; then 
+	echo "Return value $fail not 0" ; exit $fail 
+    fi
+    if test ! -f ${output2} ; then 
+	echo "$output2 not made" ; exit 1; 
+    fi
+    echo "Pass 2 done"
+fi
+
+if test $dopass3 -gt 0 ; then
     tit=`echo $tit | tr ' ' '@'` 
-    echo "Running aliroot ${opts} ${opts1} ${ana}/Pass2.C\(\".\",$nev,\"$type\",$vzmin,$vzmax,$proof\)"
-    aliroot ${opts} ${ana}/Pass2.C\(\".\",$nev,\"$type\",$vzmin,$vzmax,$proof\)
+    args="\(\"${output2}\"\)"
+    if test "x$pass1" = "xMakeELossFits.C" ; then 
+	args="\(\"${output1}\"\)"
+    fi
+    
+    echo "Running aliroot ${opts} ${opts1} ${ana}/${pass3}.C${args}"
+    aliroot ${opts} ${ana}/${pass3}.C${args}
 fi
 
 
