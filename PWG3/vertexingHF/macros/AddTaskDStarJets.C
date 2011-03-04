9b5c9c9249e8d6c5d3251cf1e016eb1fb9a765d1
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-04 15:10:02 +0000

    Update (Alessandro)

--- PWG3/vertexingHF/macros/AddTaskDStarJets.C
+++ PWG3/vertexingHF/macros/AddTaskDStarJets.C
@@ -1,12 +1,5 @@
 //DEFINITION OF A FEW CONSTANTS
-const Int_t    mintrackrefsTPC = 0 ;
-const Int_t    mintrackrefsITS = 3 ;
-const Int_t    PDG = 421; 
-const Int_t    minclustersTPC = 0 ;
-const Int_t    minITSClusters = 4;
-// ANALYSIS TYPE D*+ or D*-
-const Bool_t computeD0 = kFALSE;
-const Bool_t topologicalCut = kFALSE;
+const Int_t    chargeFrCorr = 20;
 //----------------------------------------------------
 
 AliAnalysisTaskSEDStarJets *AddTaskDStarJets(Bool_t theMCon=kTRUE)
@@ -14,20 +7,34 @@ AliAnalysisTaskSEDStarJets *AddTaskDStarJets(Bool_t theMCon=kTRUE)
 
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
-    ::Error("AddTaskDStarJets", "No analysis manager to connect to.");
+    ::Error("AddTaskDStarJets2", "No analysis manager to connect to.");
     return NULL;
-  }  
-  
+  } 
+
+  TFile* filecuts=new TFile("DStartoKpipiCuts.root");
+  if(!filecuts->IsOpen()){
+    cout<<"Input file not found: exit"<<endl;
+    return;
+  }
+
+  AliRDHFCutsDStartoKpipi* RDHFDStartoKpipi=new AliRDHFCutsDStartoKpipi();
+  RDHFDStartoKpipi = (AliRDHFCutsDStartoKpipi*)filecuts->Get("DStartoKpipiCuts");
+  RDHFDStartoKpipi->SetName("DStartoKpipiCuts");
+
+  // mm let's see if everything is ok
+  if(!RDHFDStartoKpipi){
+    cout<<"Specific AliRDHFCuts not found"<<endl;
+    return;
+  } 
+
   //CREATE THE TASK
   printf("CREATE TASK\n");
   // create the task
-  AliAnalysisTaskSEDStarJets *task = new AliAnalysisTaskSEDStarJets("AliAnalysisTaskSEDStarJets");
-  task->SetMinITSClusters(minITSClusters);
-  task->SetAnalType(computeD0);
+  AliAnalysisTaskSEDStarJets *task = new AliAnalysisTaskSEDStarJets("AliAnalysisTaskSEDStarJets",RDHFDStartoKpipi);
   task->SetMC(theMCon);
-  task->SetCutType(topologicalCut);
+  task->SetChargeFractionCorrection(chargeFrCorr);
+
   // Create and connect containers for input/output
-  
   TString outputfile = AliAnalysisManager::GetCommonFileName();
   outputfile += ":PWG3_D2H_DStarJet";
 
@@ -37,13 +44,14 @@ AliAnalysisTaskSEDStarJets *AddTaskDStarJets(Bool_t theMCon=kTRUE)
   // ----- output data -----
   
   // output TH1I for event counting
-  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("chist0", TList::Class(),AliAnalysisManager::kOutputContainer,outputfile.Data());
-  
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("charmJetCorr", TList::Class(),AliAnalysisManager::kOutputContainer,outputfile.Data());
+  AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("cuts",AliRDHFCutsDStartoKpipi::Class(),AliAnalysisManager::kOutputContainer, outputfile.Data()); //cuts
   mgr->AddTask(task);
   
   mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
   mgr->ConnectOutput(task,1,coutput1);
-  
+  mgr->ConnectOutput(task,2,coutput2);
+
   return task ;
 }
 
