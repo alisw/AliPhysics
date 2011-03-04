6746863886ccf96c4d105eeab4a15c6cde2a8666
bhippoly <bhippoly@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-04 12:50:42 +0000

    changes for the PWG2 train (M.Chojnacki)

--- PWG2/SPECTRA/macros/AddTaskChargedHadronSpectraITSTruncatedMean.C
+++ PWG2/SPECTRA/macros/AddTaskChargedHadronSpectraITSTruncatedMean.C
@@ -1,5 +1,6 @@
- AliAnalysisChargedHadronSpectraITSTruncatedMeanTask* AddTaskChargedHadronSpectraITSTruncatedMean(Int_t lowcut=-1,Int_t upcut=-1,Int_t mc=0, TString filename="./configChargedHadronSpectraITSTruncatedMeanTask.C")
+AliAnalysisChargedHadronSpectraITSTruncatedMeanTask* AddTaskChargedHadronSpectraITSTruncatedMean(Int_t lowcut=-1,Int_t upcut=-1,Int_t mc=0,Int_t hi=0 ,TString filename="./configChargedHadronSpectraITSTruncatedMeanTask.C")
 {
+	//pp settings 	
 	AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
 	if (!mgr) 
 	{
@@ -26,59 +27,26 @@
 	gROOT->LoadMacro(filename.Data());
 	AliAnalysisChargedHadronSpectraITSTruncatedMeanTask* task = GetAliAnalysisChargedHadronSpectraITSTruncatedMeanTask(mc);
 	mgr->AddTask(task);
-	Int_t upint=0;
-	Int_t lowint=0;
-	
-	
-	task->SetMultiplicityCut(lowcut,upcut);
-	
 	AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();	
-	AliAnalysisDataContainer *coutput = mgr->CreateContainer(Form("outputlow%dup%dHI0",lowcut,upcut),TList::Class(),AliAnalysisManager::kOutputContainer,outputFileName);
-	mgr->ConnectInput(task, 0, cinput);
- 	mgr->ConnectOutput(task, 1, coutput);
-	
-	return task;
-  }
-
-AliAnalysisChargedHadronSpectraITSTruncatedMeanTask* AddTaskChargedHadronSpectraITSTruncatedMean(Float_t lowcut,Float_t upcut,Int_t mc=0,TString filename="./configChargedHadronSpectraITSTruncatedMeanTask.C")
-{
-	AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-	if (!mgr) 
+	AliAnalysisDataContainer *coutput =0x0;
+	if(hi)
 	{
-		::Error("AddTaskITSsaTracks", "No analysis manager to connect to.");
-		return NULL;
-	}   
-  
-  // Check the analysis type using the event handlers connected to the analysis manager.
-  //==============================================================================
-	if (!mgr->GetInputEventHandler()) 
-	{
-		::Error("AddTaskITSsaTracks", "This task requires an input event handler");
-		return NULL;
-	}   
-  
-  	TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
-	if(type.Contains("AOD"))
-	{
-		::Error("AddTaskITSsaTracks", "This task requires to run on ESD");
-		return NULL;
+		Float_t lowcencut=(Float_t)lowcut;
+		Float_t upcencut=(Float_t)upcut;
+		if(lowcencut<0.0)
+			lowcencut=0.0;
+		if(upcencut<0.0||upcencut>100.0)
+			upcencut=100.0;
+		task->SetCentralityCut(lowcencut,upcencut);
+		task->SetHImode();	
+		coutput =mgr->CreateContainer(Form("outputlow%dup%dHI1",lowcut,upcut),TList::Class(),AliAnalysisManager::kOutputContainer,outputFileName);
+			
 	}
-	TString outputFileName = AliAnalysisManager::GetCommonFileName();
-	outputFileName += ":PWG2SpectraITSTPC";
-	gROOT->LoadMacro(filename.Data());
-	AliAnalysisChargedHadronSpectraITSTruncatedMeanTask* task = GetAliAnalysisChargedHadronSpectraITSTruncatedMeanTask(mc);
-	mgr->AddTask(task);
-	Int_t upint=0;
-	Int_t lowint=0;
-	
-	upint=(Int_t)upcut;
-	lowint=(Int_t)lowcut;
-	
-	task->SetCentralityCut(lowcut,upcut);
-	task->SetHImode();
-	
-	AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();	
-	AliAnalysisDataContainer *coutput = mgr->CreateContainer(Form("outputlow%dup%dHI1",lowint,upint),TList::Class(),AliAnalysisManager::kOutputContainer,outputFileName);
+	else
+	{
+		task->SetMultiplicityCut(lowcut,upcut);
+		coutput =mgr->CreateContainer(Form("outputlow%dup%dHI0",lowcut,upcut),TList::Class(),AliAnalysisManager::kOutputContainer,outputFileName);
+	}
 	mgr->ConnectInput(task, 0, cinput);
  	mgr->ConnectOutput(task, 1, coutput);
 	
