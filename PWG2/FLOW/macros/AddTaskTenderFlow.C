007691f862d8f34d5b63c913af26614e6477bfa6
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-04 18:38:35 +0000

    new macro

---
+++ PWG2/FLOW/macros/AddTaskTenderFlow.C
@@ -0,0 +1,48 @@
+AliAnalysisTask* AddTaskTenderFlow( Float_t tofres = 80,
+                                    Bool_t corrExpTimes=kFALSE,
+                                    Bool_t applyT0=kFALSE)
+{
+  //get the current analysis manager
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    Error("AddTask_tender_Tender", "No analysis manager found.");
+    return 0;
+  }
+  // currently don't accept AOD input
+  if (!mgr->GetInputEventHandler()->InheritsFrom(AliESDInputHandler::Class())) {
+    Error("AddTask_tender_Tender","The analysis tender only works with ESD input!");
+    return 0;
+  }
+
+  //========= Add tender to the ANALYSIS manager and set default storage =====
+  AliTender *tender=new AliTender("AnalysisTender");
+  tender->SetCheckEventSelection(kFALSE);
+  //tender->SetDefaultCDBStorage("raw://");
+  tender->SetDefaultCDBStorage("alien://folder=/alice/data/2010/OCDB");
+  mgr->AddTask(tender);
+  
+  //========= Attach TOF supply ======
+  AliTOFTenderSupply *TOFtender = new AliTOFTenderSupply("TOFtender");
+  TOFtender->SetTOFres(tofres);
+  TOFtender->SetApplyT0(applyT0);
+  TOFtender->SetCorrectExpTimes(corrExpTimes);
+  //TOFtender->SetTheorExpTimes(kTRUE);
+  tender->AddSupply(TOFtender);
+
+  //========= Attach TPC supply ======
+  AliTPCTenderSupply *tpcSupply=new AliTPCTenderSupply("TPCtender");
+  tpcSupply->SetDebugLevel(2);
+  //tpcSupply->SetMip(50.);
+  tender->AddSupply(tpcSupply);
+ 
+  //define output containers, please use 'username'_'somename'
+  AliAnalysisDataContainer *coutput1 =
+      mgr->CreateContainer("tender_event", AliESDEvent::Class(),
+                           AliAnalysisManager::kExchangeContainer,"default_tender");
+ 
+  //connect containers
+  mgr->ConnectInput  (tender,  0, mgr->GetCommonInputContainer() );
+  mgr->ConnectOutput (tender,  1, coutput1);
+ 
+  return tender;
+}
