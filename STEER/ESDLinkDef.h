f7a8b9a16d657b88bc71a910f42a279c5ae4ae62
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-08 13:02:57 +0000

    Fix for #77400 Bad change in AliESDCaloTrigger

--- STEER/ESDLinkDef.h
+++ STEER/ESDLinkDef.h
@@ -20,6 +20,10 @@
 #pragma link C++ class  AliESDHLTDecision+;
 #pragma link C++ class  AliESDZDC+;
 #pragma link C++ class  AliESDCaloTrigger+;
+
+#pragma read sourceClass="AliESDCaloTrigger" targetClass="AliESDCaloTrigger" source="Char_t fTriggerBits[48][64]" version="[2]" \
+  target="fNEntries, fColumn, fRow, fTriggerBits" targetType="Int, Int_t*, Int_t*, Int_t*" code="{fTriggerBits = new Int_t[fNEntries]; for (Int_t i=0; i<fNEntries; ++i) fTriggerBits[i]=onfile.fTriggerBits[fColumn[i]][fRow[i]];}"
+
 #pragma link C++ class  AliESDfriend+;                                                                                                           
 #pragma read sourceClass="AliESDtrack" targetClass="AliESDtrack" source="UChar_t fTRDpidQuality"  version="[-49]" target="fTRDntracklets" targetType="UChar_t" code="{fTRDntracklets = onfile.fTRDpidQuality;}"
 // see http://root.cern.ch/svn/root/trunk/io/doc/DataModelEvolution.txt
