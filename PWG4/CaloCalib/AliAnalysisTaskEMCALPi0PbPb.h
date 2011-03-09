03ee6d0aa5aefdccbc284b4c9e46978428436258
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-09 17:27:00 +0000

    Cleaned and structured version

--- PWG4/CaloCalib/AliAnalysisTaskEMCALPi0PbPb.h
+++ PWG4/CaloCalib/AliAnalysisTaskEMCALPi0PbPb.h
@@ -3,73 +3,57 @@
 
 // $Id$
 
-// ROOT includes
-#include <TList.h>
-#include <TH1.h>
-#include <TH2.h>
-#include <TH3.h>
-#include <TTreeStream.h>
-
-// AliRoot includes
-#include <AliAnalysisTaskSE.h>
-#include <AliESDEvent.h>
-#include <AliAnalysisFilter.h>
-#include <AliStack.h>
-
 class TH1F;
 class TH2F;
-class TH3F;
-class TH1I;
-class AliESDEvent;
-class AliAODEvent;
-class AliPHOSGeometry;
-class AliESDtrackCuts;
-class AliESDCaloCluster;
+class TClonesArray;
+class TObjArray;
+class AliAODCaloCells;
 class AliAODCaloCluster;
+class AliAODEvent;
+class AliESDCaloCells;
+class AliESDCaloCluster;
+class AliESDEvent;
 
 #include "AliAnalysisTaskSE.h"
 
 class AliAnalysisTaskEMCALPi0PbPb : public AliAnalysisTaskSE {
  public:
-//  AliAnalysisTaskEMCALPi0PbPb();
+  AliAnalysisTaskEMCALPi0PbPb();
   AliAnalysisTaskEMCALPi0PbPb(const char *name);
   virtual ~AliAnalysisTaskEMCALPi0PbPb(); 
   
-  virtual void   UserCreateOutputObjects();
-  virtual void   UserExec(Option_t *option);
-  virtual void   Terminate(Option_t *);
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t *option);
+  virtual void Terminate(Option_t *);
 
-  Double_t GetSigmaMax(AliESDCaloCluster * clust, AliESDEvent * evt); 
-  Double_t GetSigmaMax(AliAODCaloCluster * clust, AliAODEvent * evt); 
- 
+  void SetCentrality(const char *name)                { fCentVar = name;            }
+  void SetCentralityRange(Double_t from, Double_t to) { fCentFrom=from; fCentTo=to; }
+  void SetClusName(const char *name)                  { fClusName = name;           }
+  void SetVertexRange(Double_t z1, Double_t z2)       { fVtxZMin=z1; fVtxZMax=z2;   }
+
+ protected:
+    // input members
+  TString                fCentVar;              // variable for centrality determination
+  Double_t               fCentFrom;             // min centrality (def=0)
+  Double_t               fCentTo;               // max centrality (def=100)
+  Double_t               fVtxZMin;              // min primary vertex z (def=-7cm)
+  Double_t               fVtxZMax;              // max primary vertex z (def=+7cm)
+  TString                fClusName;             // cluster branch name (def="")
+    // derived members (ie with ! after //)
+  TList                 *fOutput;               //!container of output histograms
+  AliESDEvent           *fEsdEv;                //!pointer to input esd event
+  AliAODEvent           *fAodEv;                //!pointer to input aod event
+  const TObjArray       *fEsdClusters;          //!pointer to esd clusters
+  const AliESDCaloCells *fEsdCells;             //!pointer to esd cells
+  const TObjArray       *fAodClusters;          //!pointer to aod clusters
+  const AliAODCaloCells *fAodCells;             //!pointer to aod cells
+    // histograms
+
  private:
-  TList       *fOutputDataESD;  //! container of output ESD histograms
-  TH1I *fhNEvents;    //!  Total analysis Event number in Histogram
-  Int_t nModuleEMC;
+  AliAnalysisTaskEMCALPi0PbPb(const AliAnalysisTaskEMCALPi0PbPb&);            // not implemented
+  AliAnalysisTaskEMCALPi0PbPb &operator=(const AliAnalysisTaskEMCALPi0PbPb&); // not implemented
 
-  //Multi Dis. Vs Centrality at different module
-  TH2F **  fhCentVsClusMultEMC;   //!  Cluster multi.  Vs Centrality  at EMCal Module
-  TH2F **  fhCentVsCellMultEMC;   //!  Cell multi.     Vs Centrality  at EMCal Module
-
-  TH2F * fhCentVsClusMultEMCAll;  //! Cluster multi.  Vs Centrality  at EMCal all module
-  TH2F * fhCentVsCellMultEMCAll;  //! Cluster multi.  Vs Centrality  at EMCal all module
-
-  TH3F * fhCentVsInMVsPtEMC;      //! Centrality Vs Invariant mass Vs Pt of two cluster
-  TH3F * fhCentVsInMVsPhiEMC;      //! Centrality Vs Invariant mass Vs Pt of two cluster
-  TH2F * fhAsyVsPt;               //! Asymmetry Vs Pt of two cluster 
-
- // EMCal occupancy and cell energy
-
-  TH3F ** fhCentVsColuRowEMC;            //! Column Vs Row Vs Centrality at EMCal Module 
-  TH3F ** fhCentVsColuRowEnerEMC;        //! Column Vs Row Vs Centrality at EMCal Module Vs energy weight 
-
- // Cluster QA and showershape
-  TH3F * fhMggPtEr;            //! Invariant mass vs Pt vs Energy ratio  
-  TH3F * fhPtEnSg;             //! Pt vs En vs Cluster long axis
-
-  AliAnalysisTaskEMCALPi0PbPb& operator=(const AliAnalysisTaskEMCALPi0PbPb&); // not implemented
- 
-  ClassDef(AliAnalysisTaskEMCALPi0PbPb, 1); // example of analysis
+  ClassDef(AliAnalysisTaskEMCALPi0PbPb, 1); // Analysis task for neutral pions in Pb+Pb
 };
 
 #endif
