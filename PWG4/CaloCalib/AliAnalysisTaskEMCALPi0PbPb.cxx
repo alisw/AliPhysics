9165f511e8686e3df8d40ea08d4035915e767919
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-10 09:53:47 +0000

    Clean way to get input from clusterizer and event

--- PWG4/CaloCalib/AliAnalysisTaskEMCALPi0PbPb.cxx
+++ PWG4/CaloCalib/AliAnalysisTaskEMCALPi0PbPb.cxx
@@ -15,6 +15,7 @@
 #include "AliEMCALGeoUtils.h"
 #include "AliESDEvent.h"
 #include "AliESDVertex.h"
+#include "AliLog.h"
 
 ClassImp(AliAnalysisTaskEMCALPi0PbPb)
 
@@ -26,10 +27,12 @@ AliAnalysisTaskEMCALPi0PbPb::AliAnalysisTaskEMCALPi0PbPb()
     fCentTo(100),
     fVtxZMin(-7),
     fVtxZMax(+7),
+    fUseQualFlag(1),
     fClusName(),
     fOutput(0),
     fEsdEv(0),
     fAodEv(0),
+    fRecPoints(0),
     fEsdClusters(0),
     fEsdCells(0),
     fAodClusters(0),
@@ -49,10 +52,12 @@ AliAnalysisTaskEMCALPi0PbPb::AliAnalysisTaskEMCALPi0PbPb(const char *name)
     fCentTo(100),
     fVtxZMin(-7),
     fVtxZMax(+7),
+    fUseQualFlag(1),
     fClusName(),
     fOutput(0),
     fEsdEv(0),
     fAodEv(0),
+    fRecPoints(0),
     fEsdClusters(0),
     fEsdCells(0),
     fAodClusters(0),
@@ -85,14 +90,15 @@ void AliAnalysisTaskEMCALPi0PbPb::UserCreateOutputObjects()
   fOutput = new TList();
   fOutput->SetOwner();
 
-  fHcuts = new TH1F("hCuts","",3,0.5,3.5);
+  fHcuts = new TH1F("hEventCuts","",4,0.5,4.5);
   fHcuts->GetXaxis()->SetBinLabel(1,"All (PS)");
   fHcuts->GetXaxis()->SetBinLabel(2,Form("%s: %.0f-%.0f",fCentVar.Data(),fCentFrom,fCentTo));
-  fHcuts->GetXaxis()->SetBinLabel(3,Form("zvtx: %.0f-%.0f",fVtxZMin,fVtxZMax));
+  fHcuts->GetXaxis()->SetBinLabel(3,"QFlag");
+  fHcuts->GetXaxis()->SetBinLabel(4,Form("zvtx: %.0f-%.0f",fVtxZMin,fVtxZMax));
   fOutput->Add(fHcuts);
-  fHvertexZ = new TH1F("hVertexZ",";z [cm];",100,-25,25);
+  fHvertexZ = new TH1F("hVertexZBeforeCuts",";z [cm];",100,-25,25);
   fOutput->Add(fHvertexZ);
-  fHcent = new TH1F("hCent",Form(";%s;",fCentVar.Data()),101,-1,100);
+  fHcent = new TH1F("hCentBeforeCuts",Form(";%s;",fCentVar.Data()),101,-1,100);
   fOutput->Add(fHcent);
 
   PostData(1, fOutput); 
@@ -106,6 +112,16 @@ void AliAnalysisTaskEMCALPi0PbPb::UserExec(Option_t *)
   if (!InputEvent())
     return;
 
+  AliAnalysisManager *am = AliAnalysisManager::GetAnalysisManager();
+  fEsdEv = dynamic_cast<AliESDEvent*>(InputEvent());
+  if (fEsdEv) {
+    am->LoadBranch("AliESDRun.");
+    am->LoadBranch("AliESDHeader.");
+  } else {
+    fAodEv = dynamic_cast<AliAODEvent*>(InputEvent());
+    am->LoadBranch("header");
+  }
+
   Int_t cut = 1;
   fHcuts->Fill(cut++);
 
@@ -114,12 +130,24 @@ void AliAnalysisTaskEMCALPi0PbPb::UserExec(Option_t *)
   fHcent->Fill(cent);
   if (cent<fCentFrom||cent>fCentTo)
     return;
+
+  fHcuts->Fill(cut++);
   
-  // todo test quality flag (optional)
+  if (fUseQualFlag) {
+    if (centP->GetQuality()>0)
+      return;
+  }
 
   fHcuts->Fill(cut++);
 
-  const AliVVertex *vertex =  InputEvent()->GetPrimaryVertex();
+  if (fEsdEv) {
+    am->LoadBranch("PrimaryVertex");
+  } else {
+    fAodEv = dynamic_cast<AliAODEvent*>(InputEvent());
+    am->LoadBranch("vertices");
+  }
+
+  const AliVVertex *vertex = InputEvent()->GetPrimaryVertex();
   if (!vertex)
     return;
 
@@ -130,17 +158,28 @@ void AliAnalysisTaskEMCALPi0PbPb::UserExec(Option_t *)
 
   fHcuts->Fill(cut++);
 
-  AliAnalysisManager *am = AliAnalysisManager::GetAnalysisManager();
-  fEsdEv = dynamic_cast<AliESDEvent*>(InputEvent());
-  fAodEv = dynamic_cast<AliAODEvent*>(InputEvent());
+  fRecPoints   = 0; // will be set if fClusName is given and AliAnalysisTaskEMCALClusterizeFast is used
+  fEsdClusters = 0; // will be set if ESD input used and if fRecPoints are not set of if clusters are attached
+  fEsdCells    = 0; // will be set if ESD input used
+  fAodClusters = 0; // will be set if AOD input used and if fRecPoints are not set of if clusters are attached
+                    //             or if fClusName is given and AliAnalysisTaskEMCALClusterizeFast in AOD output mode
+  fAodCells    = 0; // will be set if AOD input used
 
-  fEsdClusters = 0;
-  fEsdCells    = 0;
-  fAodClusters = 0;
-  fAodCells    = 0;
-
-  // deal with AOD output first
-  if (AODEvent() && !fClusName.IsNull()) {
+  // deal with special output from AliAnalysisTaskEMCALClusterizeFast first
+  Bool_t clusattached = 0;
+  Bool_t recalibrated = 0;
+  if (1 && !fClusName.IsNull()) {
+    AliAnalysisTaskEMCALClusterizeFast *cltask = 0;
+    TObjArray *ts = am->GetTasks();
+    cltask = dynamic_cast<AliAnalysisTaskEMCALClusterizeFast*>(ts->FindObject(fClusName));
+    if (cltask && cltask->GetClusters()) {
+      fRecPoints = cltask->GetClusters();
+      clusattached = cltask->GetAttachClusters();
+      if (cltask->GetCalibData()!=0)
+        recalibrated = kTRUE;
+    }
+  }
+  if (1 && AODEvent() && !fClusName.IsNull()) {
     TList *l = AODEvent()->GetList();
     TClonesArray *clus = 0;
     if (l) {
@@ -150,19 +189,9 @@ void AliAnalysisTaskEMCALPi0PbPb::UserExec(Option_t *)
   }
 
   if (fEsdEv) { // ESD input mode
-    if (!fEsdClusters) {
-      AliAnalysisTaskEMCALClusterizeFast *cltask = 0;
-      TObjArray *ts = am->GetTasks();
-      for (Int_t i=0; i<ts->GetEntries(); ++i) {
-        cltask = dynamic_cast<AliAnalysisTaskEMCALClusterizeFast*>(ts->At(i));
-        if (cltask && cltask->GetClusters()) {
-          fEsdClusters = cltask->GetClusters();
-          break;
-        }
-      }
-    }
-    if (!fEsdClusters) {
-      am->LoadBranch("CaloClusters");
+    if (1 && (!fRecPoints||clusattached)) {
+      if (!clusattached)
+        am->LoadBranch("CaloClusters");
       TList *l = fEsdEv->GetList();
       TClonesArray *clus = 0;
       if (l) {
@@ -170,13 +199,15 @@ void AliAnalysisTaskEMCALPi0PbPb::UserExec(Option_t *)
         fEsdClusters = clus;
       }
     }
-    if (!fEsdCells) {
-      am->LoadBranch("EMCALCells.");
+    if (1) {
+      if (!recalibrated)
+        am->LoadBranch("EMCALCells.");
       fEsdCells = fEsdEv->GetEMCALCells();
     }
   } else if (fAodEv) { // AOD input mode
-    if (!fAodClusters) {
-      am->LoadBranch("caloClusters");
+    if (1 && (!fAodClusters || clusattached)) {
+      if (!clusattached)
+        am->LoadBranch("caloClusters");
       TList *l = fAodEv->GetList();
       TClonesArray *clus = 0;
       if (l) {
@@ -184,12 +215,21 @@ void AliAnalysisTaskEMCALPi0PbPb::UserExec(Option_t *)
         fAodClusters = clus;
       }
     }
-    if (!fAodCells) {
-      am->LoadBranch("emcalCells");
+    if (1) {
+      if (!recalibrated)
+        am->LoadBranch("emcalCells");
       fAodCells = fAodEv->GetEMCALCells();
     }
   } else {
     AliFatal("Impossible to not have either pointer to ESD or AOD event");
+  }
+
+  if (1) {
+    AliDebug(2,Form("fRecPoints   set: %p", fRecPoints));
+    AliDebug(2,Form("fEsdClusters set: %p", fEsdClusters));
+    AliDebug(2,Form("fEsdCells    set: %p", fEsdCells));
+    AliDebug(2,Form("fAodClusters set: %p", fAodClusters));
+    AliDebug(2,Form("fAodCells    set: %p", fAodCells));
   }
 
   FillCellHists();
