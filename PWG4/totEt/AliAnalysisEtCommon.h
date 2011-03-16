30ae7bfa0407d3195b9869933464ce612aef5d1e
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-16 18:01:07 +0000

    Adding common variable for current centrality bin

--- PWG4/totEt/AliAnalysisEtCommon.h
+++ PWG4/totEt/AliAnalysisEtCommon.h
@@ -153,6 +153,7 @@ protected:
     Float_t ProtonBaryonEnhancement(Float_t pt);//Function which gives the factor to reweigh a lambda or antilambda so it roughly matches baryon enhancement seen at RHIC
     TString fCentralityMethod;//string specifying the centrality method, see https://twiki.cern.ch/twiki/bin/viewauth/ALICE/CentStudies
     Int_t fNCentBins;//number of centrality bins (10 or 20)
+    Int_t fCentBin;//current centrality bin
 
 
  private:
