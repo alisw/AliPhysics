767a600f0de180da3aab57ecce05ca49d3656d9f
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-18 20:11:03 +0000

    fix mem leak in operator=

--- ANALYSIS/AliAnalysisTaskESDfilter.cxx
+++ ANALYSIS/AliAnalysisTaskESDfilter.cxx
@@ -973,6 +973,11 @@ void AliAnalysisTaskESDfilter::ConvertTPCOnlyTracks(const AliESDEvent& esd)
         delete track;
         continue;
       }
+      Double_t  fSigmaConstrainedMax = 5.;
+      if(track->GetConstrainedChi2TPC() > fSigmaConstrainedMax*fSigmaConstrainedMax) {
+	delete track;
+	continue;
+      }
       track->Set(exParam.GetX(),exParam.GetAlpha(),exParam.GetParameter(),exParam.GetCovariance());
     }
     
