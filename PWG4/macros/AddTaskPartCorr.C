80414012e6ccf7c85396b4f3f71a8550a8817cb2
rvernet <rvernet@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-03-24 16:01:36 +0000

    added all corrections for EMCAL for periods LHC10d and LHC10e

--- PWG4/macros/AddTaskPartCorr.C
+++ PWG4/macros/AddTaskPartCorr.C
@@ -1,5 +1,14 @@
-AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TString calorimeter, Bool_t kPrintSettings = kFALSE,Bool_t kSimulation = kFALSE, Bool_t outputAOD=kFALSE, Bool_t oldAOD=kFALSE)
-{
+AliAnalysisTaskParticleCorrelation *AddTaskPartCorr
+(
+ TString inputDataType, 
+ TString calorimeter, 
+ Bool_t kPrintSettings = kTRUE,
+ Bool_t kSimulation = kFALSE, 
+ Bool_t outputAOD=kFALSE, 
+ Bool_t oldAOD=kFALSE,
+ TString period
+ ) {
+
   // Creates a PartCorr task, configures it and adds it to the analysis manager.
   
   // Get the pointer to the existing analysis manager via the static access method.
@@ -32,15 +41,13 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   //if(!kSimulation) reader->SetFiredTriggerClassName("CINT1B-ABCE-NOPF-ALL");
   if(calorimeter == "EMCAL") {
     reader->SwitchOnEMCALCells();  
-    reader->SwitchOnEMCAL(); 
+    reader->SwitchOnEMCAL();
   }
   if(calorimeter == "PHOS") { 
     reader->SwitchOnPHOSCells();  
     reader->SwitchOnPHOS();
   }
   
-   reader->SwitchOnSuspiciousClustersRemoval();  //EMCAL
-
   // for case inputDataType="deltaAOD", no need to fill the EMCAL/PHOS cluster lists
   if(inputDataType.Contains("delta")){
     reader->SwitchOffEMCAL();
@@ -59,57 +66,192 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
       reader->SwitchOnAODMCParticles(); 
     }
   }
+
+  //-----------------------------------------------------------------
+  // Switch on cluster energy smearing 
+  //  -> SIMULATION
+  //  -> EMCAL
+  //-----------------------------------------------------------------
+
+  if (kSimulation && calorimeter == "EMCAL") {
+    //switch on cluster energy smearing
+    reader->SwitchOnClusterEnergySmearing();
+    reader->SetSmearingParameters(0,0.07);
+    reader->SetSmearingParameters(0,0.00);
+    reader->SetSmearingParameters(0,0.00);
+  }
+
   
+  //-----------------------------------------------------------------
+  // Z vertex cut
   reader->SetZvertexCut(10.);
-  
-  //Min particle pT
+  //-----------------------------------------------------------------
+
+  //-----------------------------------------------------------------
+  // Min particle pT
+  //-----------------------------------------------------------------
   reader->SetEMCALPtMin(0.3); 
   reader->SetPHOSPtMin(0.3);
   reader->SetCTSPtMin(0.1);
-  if(outputAOD)  reader->SwitchOnWriteDeltaAOD()  ;
-  if(oldAOD) reader->SwitchOnOldAODs();
+
+  if(outputAOD)      reader->SwitchOnWriteDeltaAOD()  ;
+  if(oldAOD)         reader->SwitchOnOldAODs();
   if(kPrintSettings) reader->Print("");
   
+
+  //-----------------------------------------------------------------
+  // Bad cluster removal
+  //  -> REAL DATA ONLY
+  //-----------------------------------------------------------------
+  reader->SwitchOnSuspiciousClustersRemoval();
+
+
   // *** Calorimeters Utils	***
   AliCalorimeterUtils *cu = new AliCalorimeterUtils;
-  // Remove clusters close to borders, at least max energy cell is 1 cell away 
+
+  //-----------------------------------------------------------------
+  // Non-linearity corrections 
+  //  -> REAL DATA AND SIMULATION
+  //  -> EMCAL
+  //-----------------------------------------------------------------
+  if (calorimeter == "EMCAL") {
+    cu->SwitchOnCorrectClusterLinearity();
+    if (!kSimulation) {
+      cu->GetEMCALRecoUtils()->SetNonLinearityFunction(AliEMCALRecoUtils::kBeamTestCorrected);
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(0,0.976       ) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(1,9.83529e-01 ) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(2,-1.84235e+02) ; 
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(3,-2.05019e+00) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(4,-5.89423e+00) ;
+    }
+    else              {
+      cu->GetEMCALRecoUtils()->SetNonLinearityFunction(AliEMCALRecoUtils::kPi0MC);
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(0,1.001   ) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(1,-0.01264) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(2,-0.03632) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(3,0.1798  ) ;
+      cu->GetEMCALRecoUtils()->SetNonLinearityParam(4,-0.522  ) ;
+    }
+  }
+  
+  //-----------------------------------------------------------------
+  //  Remove clusters close to borders, 
+  //  at least max energy cell is 1 cell away 
+  //-----------------------------------------------------------------
   cu->SetNumberOfCellsFromEMCALBorder(1);
   cu->SetNumberOfCellsFromPHOSBorder(2);
   
-  // Remove EMCAL hottest channels for first LHC10 periods 	
-  cu->SwitchOnBadChannelsRemoval();
-  // SM0
-  cu->SetEMCALChannelStatus(0,3,13);  cu->SetEMCALChannelStatus(0,44,1); cu->SetEMCALChannelStatus(0,3,13); 
-  cu->SetEMCALChannelStatus(0,20,7);  cu->SetEMCALChannelStatus(0,38,2);   
-  // SM1
-  cu->SetEMCALChannelStatus(1,4,7);   cu->SetEMCALChannelStatus(1,4,13);  cu->SetEMCALChannelStatus(1,9,20); 
-  cu->SetEMCALChannelStatus(1,14,15); cu->SetEMCALChannelStatus(1,23,16); cu->SetEMCALChannelStatus(1,32,23); 
-  cu->SetEMCALChannelStatus(1,37,5);  cu->SetEMCALChannelStatus(1,40,1);  cu->SetEMCALChannelStatus(1,40,2);
-  cu->SetEMCALChannelStatus(1,40,5);  cu->SetEMCALChannelStatus(1,41,0);  cu->SetEMCALChannelStatus(1,41,1);
-  cu->SetEMCALChannelStatus(1,41,2);  cu->SetEMCALChannelStatus(1,41,4);
-  // SM2 	
-  cu->SetEMCALChannelStatus(2,14,15); cu->SetEMCALChannelStatus(2,18,16); cu->SetEMCALChannelStatus(2,18,17); 
-  cu->SetEMCALChannelStatus(2,18,18); cu->SetEMCALChannelStatus(2,18,20); cu->SetEMCALChannelStatus(2,18,21); 
-  cu->SetEMCALChannelStatus(2,18,23); cu->SetEMCALChannelStatus(2,19,16); cu->SetEMCALChannelStatus(2,19,17); 
-  cu->SetEMCALChannelStatus(2,19,19); cu->SetEMCALChannelStatus(2,19,20); cu->SetEMCALChannelStatus(2,19,21); 
-  cu->SetEMCALChannelStatus(2,19,22);
-  //SM3
-  cu->SetEMCALChannelStatus(3,4,7);
+  
+  //-----------------------------------------------------------------
+  // Remove EMCAL hottest channels 
+  //  -> REAL DATA AND SIMULATION
+  //  -> EMCAL
+  // Recover the file from alien  
+  //   /alice/cern.ch/user/g/gconesab/BadChannelsDB
+  //-----------------------------------------------------------------
+  if (calorimeter == "EMCAL") {
+    cu->SwitchOnBadChannelsRemoval();
+    cu->SwitchOnDistToBadChannelRecalculation();
+    TFile * fbad = new TFile("BadChannels.root","read");
+    TH2I * hbad0 = (TH2I*)fbad->Get("EMCALBadChannelMap_Mod0");
+    TH2I * hbad1 = (TH2I*)fbad->Get("EMCALBadChannelMap_Mod1");
+    TH2I * hbad2 = (TH2I*)fbad->Get("EMCALBadChannelMap_Mod2");
+    TH2I * hbad3 = (TH2I*)fbad->Get("EMCALBadChannelMap_Mod3");
+    cu->SetEMCALChannelStatusMap(0,hbad0);
+    cu->SetEMCALChannelStatusMap(1,hbad1);
+    cu->SetEMCALChannelStatusMap(2,hbad2);
+    cu->SetEMCALChannelStatusMap(3,hbad3);
+  }
   
   
-  //Recalibration
-  //cu->SwitchOnRecalibration();
-  //TFile * f = new TFile("RecalibrationFactors.root","read");
-  //cu->SetEMCALChannelRecalibrationFactors(0,(TH2F*)f->Get("EMCALRecalFactors_SM0"));
-  //cu->SetEMCALChannelRecalibrationFactors(1,(TH2F*)f->Get("EMCALRecalFactors_SM1"));
-  //cu->SetEMCALChannelRecalibrationFactors(2,(TH2F*)f->Get("EMCALRecalFactors_SM2"));
-  //cu->SetEMCALChannelRecalibrationFactors(3,(TH2F*)f->Get("EMCALRecalFactors_SM3"));
-  //f->Close();	
+  //-----------------------------------------------------------------
+  // Misalignment + recalculate position 
+  //  -> REAL DATA and SIMULATION
+  //  -> EMCAL
+  //-----------------------------------------------------------------
+  if (calorimeter == "EMCAL") {
+    cu->SetEMCALGeometryName("EMCAL_FIRSTYEARV1");
+    cu->GetEMCALRecoUtils()->SetParticleType(AliEMCALRecoUtils::kPhoton);
+    cu->GetEMCALRecoUtils()->SetW0(4.5);      
+    cu->GetEMCALRecoUtils()->SetPositionAlgorithm(AliEMCALRecoUtils::kPosTowerGlobal);
+    cu->SwitchOnLoadOwnEMCALGeometryMatrices();
+    TGeoHMatrix *matrix[4];
+    
+    double rotationMatrix[4][9] = {-0.014587, -0.999892, -0.002031, 0.999892, -0.014591,  0.001979, -0.002009, -0.002002,  0.999996,
+				   -0.014587,  0.999892,  0.002031, 0.999892,  0.014591, -0.001979, -0.002009,  0.002002, -0.999996,
+				   -0.345864, -0.938278, -0.003412, 0.938276, -0.345874,  0.003010, -0.004004, -0.002161,  0.999990,
+				   -0.345861,  0.938280,  0.003412, 0.938276,  0.345874, -0.003010, -0.004004,  0.002161, -0.999990};
+    
+    double translationMatrix[4][3] = {0.351659,    447.576446,  176.269742,
+				      1.062577,    446.893974, -173.728870,
+				      -154.213287, 419.306156,  176.753692,
+				      -153.018950, 418.623681, -173.243605};
+    for(int j=0; j<4; j++)
+      {
+	matrix[j] = new TGeoHMatrix();
+	matrix[j]->SetRotation(rotationMatrix[j]);
+	matrix[j]->SetTranslation(translationMatrix[j]);
+	matrix[j]->Print();
+	cu->SetEMCALGeometryMatrixInSM(matrix[j],j);
+      }
+    cu->SwitchOnRecalculateClusterTrackMatching();
+  }
   
+  //-----------------------------------------------------------------
+  // Time dependent corrections 
+  //  -> REAL_DATA ONLY
+  //  -> EMCAL
+  //  Recover file from alien  
+  //  /alice/cern.ch/user/g/gconesab/TimeDepCorrectionDB
+  //-----------------------------------------------------------------
+  if (!kSimulation && calorimeter == "EMCAL") {
+    cu->GetEMCALRecoUtils()->SwitchOnTimeDepCorrection();
+    char cmd[200] ;
+    sprintf(cmd, ".!tar xvfz CorrectionFiles.tgz >& /dev/null") ;
+    gROOT->ProcessLine(cmd) ;
+  }
+  
+  
+  //------------------------------------------------------------------------
+  //     Recalibration factors 
+  //       -> REAL DATA ONLY
+  //       -> EMCAL
+  //     Recover the file from alien for LHC10d pass2
+  //     /alice/cern.ch/user/g/gconesab/RecalDB/december2010        -> LHC10d pass2
+  //     /alice/cern.ch/user/g/gconesab/RecalDB/summer_december2010 -> LHC10e pass1
+  //  ******
+  //     For other periods/passes, see
+  //     https://twiki.cern.ch/twiki/bin/view/ALICE/EMCalOffline#Summary_of_Calibration_and_Align
+  //------------------------------------------------------------------------
+  
+  if (calorimeter == "EMCAL") {
+    cu->SwitchOnRecalibration();
+    TFile* f = 0x0 ;
+    if (!kSimulation) {
+      if      (period == "LHC10d") f = new TFile("RecalibrationFactors_LHC10d.root","read");
+      else if (period == "LHC10e") f = new TFile("RecalibrationFactors_LHC10e.root","read");
+      else                         Fatal("AddTaskPartCorr","run period not supported");
+    }
+    else {
+      f = new TFile("DecalibrationFactors.root","read");
+    }
+    if (!f || !f->IsOpen()) Fatal("AddTaskPartCorr","Re(De)-calibration file not found");
+
+    Info("AddTaskPartCorr",Form("Using calibration files for period %s",period.Data()));
+
+    TH2F * h0 = (TH2F*)f->Get("EMCALRecalFactors_SM0");
+    TH2F * h1 = (TH2F*)f->Get("EMCALRecalFactors_SM1");
+    TH2F * h2 = (TH2F*)f->Get("EMCALRecalFactors_SM2");
+    TH2F * h3 = (TH2F*)f->Get("EMCALRecalFactors_SM3");
+    cu->SetEMCALChannelRecalibrationFactors(0,h0);
+    cu->SetEMCALChannelRecalibrationFactors(1,h1);
+    cu->SetEMCALChannelRecalibrationFactors(2,h2);
+    cu->SetEMCALChannelRecalibrationFactors(3,h3);
+  }
+
   cu->SetDebug(-1);
-  if(kPrintSettings) cu->Print("");
-  
-  
+
+
   // ##### Analysis algorithm settings ####
   
   // -------------------------------------------------
@@ -120,19 +262,18 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   anaphoton->SetDebug(-1); //10 for lots of messages
   //settings for different multiplicity analysis
   anaphoton->SwitchOffEventSelection() ;
-  anaphoton->SetMultiplicity(80, 120);
+  //anaphoton->SetMultiplicity(80, 120);
 
   if(calorimeter == "PHOS"){
-    anaphoton->SetNCellCut(2);// At least 3 cells
-    anaphoton->SetMinPt(0.3);
+    anaphoton->SetNCellCut(2);
+    anaphoton->SetMinPt(0.);
     anaphoton->SetMinDistanceToBadChannel(2, 4, 5);
   }
   else {//EMCAL
-    anaphoton->SetNCellCut(1);// At least 2 cells
-    anaphoton->SetMinPt(0.3); 
-    //if(!kUseKinematics) anaphoton->SetTimeCut(400,900);// Time window of [400-900] ns
-    //anaphoton->SetMinDistanceToBadChannel(6, 12, 18);
-    anaphoton->SetMinDistanceToBadChannel(1, 2, 3);//For new releases.
+    anaphoton->SetNCellCut(1);
+    anaphoton->SetMinPt(0.); 
+    if(!kUseKinematics) anaphoton->SetTimeCut(400,900);// Time window of [400-900] ns
+    anaphoton->SetMinDistanceToBadChannel(4, 5, 10);
   }
   anaphoton->SetCalorimeter(calorimeter);
   if(kUseKinematics) anaphoton->SwitchOnDataMC() ;//Access MC stack and fill more histograms
@@ -156,9 +297,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   else anaphoton->SetInputAODName(Form("Photons%s",calorimeter.Data()));
   anaphoton->AddToHistogramsName("AnaPhotonCorr_");
   //Set Histograms bins and ranges
-  anaphoton->SetHistoPtRangeAndNBins(0, 50, 200) ;
-  //      ana->SetHistoPhiRangeAndNBins(0, TMath::TwoPi(), 100) ;
-  //      ana->SetHistoEtaRangeAndNBins(-0.7, 0.7, 100) ;
+  anaphoton->SetHistoPtRangeAndNBins(0, 20, 200) ;
   if(kPrintSettings) anaphoton->Print("");
   
   // -----------------------------------
@@ -190,7 +329,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   else anapi0->SwitchOffDataMC() ;
   if(calorimeter=="PHOS") anapi0->SetNumberOfModules(3); //PHOS first year
   else  anapi0->SetNumberOfModules(4); //EMCAL first year
-  anapi0->SetHistoPtRangeAndNBins(0, 20, 200) ;
+  anapi0->SetHistoPtRangeAndNBins(0, 20, 40) ;
   //anapi0->SetHistoPhiRangeAndNBins(0, TMath::TwoPi(), 100) ;
   //anapi0->SetHistoEtaRangeAndNBins(-0.8, 0.8, 200) ;
   anapi0->SetHistoMassRangeAndNBins(0., 0.9, 300) ;
@@ -205,7 +344,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   
   AliAnaPi0EbE *anapi0ebe = new AliAnaPi0EbE();
   anapi0ebe->SwitchOffEventSelection() ;
-  anapi0ebe->SetMultiplicity(80, 120);
+  //anapi0ebe->SetMultiplicity(80, 120);
   anapi0ebe->SetMultiBin(1);  
   anapi0ebe->SetDebug(-1);//10 for lots of messages
   anapi0ebe->SetAnalysisType(AliAnaPi0EbE::kIMCalo);
@@ -227,7 +366,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   //Set Histrograms bins and ranges
   if(calorimeter=="EMCAL" ){
     nms->SetHistoERangeAndNBins(0, 15, 150) ;  
-    anapi0ebe->SetHistoPtRangeAndNBins(0, 15, 75) ;
+    anapi0ebe->SetHistoPtRangeAndNBins(0, 30, 60) ;
   }
   else{
     nms->SetHistoERangeAndNBins(0, 30, 200) ;  
@@ -419,7 +558,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   anacorrisohadron->SetAODObjArrayName("IsoPhotonHadronCorr"); 
   anacorrisohadron->SetDebug(-1);
   anacorrisohadron->SwitchOffCaloPID();
-    if(kSimulation){
+  if(kSimulation){
     anacorrisohadron->SwitchOnFiducialCut();
     AliFiducialCut * fidCut1stYear = anacorrisohadron->GetFiducialCut();
     fidCut1stYear->DoEMCALFiducialCut(kTRUE) ;
@@ -498,7 +637,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   anacorrhadronisopi0->SetAODObjArrayName("IsoPi0HadronCorr"); 
   anacorrhadronisopi0->SetDebug(-1);
   anacorrhadronisopi0->SwitchOffCaloPID();
-    if(kSimulation){
+  if(kSimulation){
     anacorrhadronisopi0->SwitchOnFiducialCut();
     AliFiducialCut * fidCut1stYear = anacorrhadronisopi0->GetFiducialCut();
     fidCut1stYear->DoEMCALFiducialCut(kTRUE) ;
@@ -535,29 +674,31 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   AliAnaPartCorrMaker * maker = new AliAnaPartCorrMaker();
   maker->SetReader(reader);//pointer to reader
   maker->SetCaloUtils(cu); //pointer to calorimeter utils
+
+
   Int_t n = 0;//Analysis number, order is important
   // Particle selection analysis
   maker->AddAnalysis(anaphoton,n++);
-  maker->AddAnalysis(anapi0,n++);
+  //maker->AddAnalysis(anapi0,n++);
   maker->AddAnalysis(anapi0ebe,n++);
-  maker->AddAnalysis(anaomegaToPi0Gamma,n++);  
+//   maker->AddAnalysis(anaomegaToPi0Gamma,n++);  
   //if(calorimeter=="EMCAL")maker->AddAnalysis(anabtag,n++);   
   // Isolation analysis
   maker->AddAnalysis(anaisol,n++);
-  maker->AddAnalysis(anaisolpi0,n++);
+//   maker->AddAnalysis(anaisolpi0,n++);
   // Correlation analysis
-  //maker->AddAnalysis(anacorrjet,n++);
-  //maker->AddAnalysis(anacorrhadron,n++);
-  //maker->AddAnalysis(anacorrhadronpi0,n++);
-  //maker->AddAnalysis(anacorrisohadron,n++);
-  //maker->AddAnalysis(anacorrhadronisopi0,n);
+//   maker->AddAnalysis(anacorrjet,n++);
+  maker->AddAnalysis(anacorrhadron,n++);
+//   maker->AddAnalysis(anacorrhadronpi0,n++);
+//   maker->AddAnalysis(anacorrisohadron,n++);
+//   maker->AddAnalysis(anacorrhadronisopi0,n);
   maker->SetAnaDebug(0)  ;
   maker->SwitchOnHistogramsMaker()  ;
   if(inputDataType.Contains("delta")) maker->SwitchOffAODsMaker()  ;
-  else                       maker->SwitchOnAODsMaker()  ;
+  else                                maker->SwitchOnAODsMaker()  ;
 	
   if(kPrintSettings) maker->Print("");
-  
+
   printf("======================== \n");
   printf(" End Configuration of PartCorr analysis with detector %s \n",calorimeter.Data());
   printf("======================== \n");
@@ -578,7 +719,8 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   //AliAnalysisDataContainer *cout_pc = mgr->CreateContainer(Form(name),TList::Class(),
   //					   AliAnalysisManager::kOutputContainer, Form("PartCorr_%s.root",calorimeter.Data()));
   
-  TString outputfile = AliAnalysisManager::GetCommonFileName(); 
+  TString outputfile = AliAnalysisManager::GetCommonFileName();
+
   //  AliAnalysisDataContainer *cout_pc = mgr->CreateContainer(Form("PartCorr_%s",calorimeter.Data()),  TList::Class(), AliAnalysisManager::kOutputContainer, Form("%s:PartCorr_%s",outputfile.Data(),calorimeter.Data()));
   AliAnalysisDataContainer *cout_pc   = mgr->CreateContainer(calorimeter.Data(), TList::Class(), 
                                                              AliAnalysisManager::kOutputContainer, 
@@ -587,7 +729,7 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   AliAnalysisDataContainer *cout_cuts = mgr->CreateContainer(Form("%sCuts",calorimeter.Data()), TList::Class(), 
                                                              AliAnalysisManager::kParamContainer, 
                                                              Form("%s:PartCorrCuts",outputfile.Data()));
-	
+  
   // Create ONLY the output containers for the data produced by the task.
   // Get and connect other common input/output containers via the manager as below
   //==============================================================================
@@ -596,8 +738,6 @@ AliAnalysisTaskParticleCorrelation *AddTaskPartCorr(TString inputDataType, TStri
   if(!inputDataType.Contains("delta")   && outputAOD) mgr->ConnectOutput (task, 0, mgr->GetCommonOutputContainer());
   mgr->ConnectOutput (task, 1, cout_pc);
   mgr->ConnectOutput (task, 2, cout_cuts);
-  
+
   return task;
 }
-
-
