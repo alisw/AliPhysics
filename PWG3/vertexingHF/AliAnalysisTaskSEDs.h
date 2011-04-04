67edcda7a4f0d648a7d34d771a87a5c7f398db07
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-04 21:01:00 +0000

    Update (Francesco, Gian Michele)

--- PWG3/vertexingHF/AliAnalysisTaskSEDs.h
+++ PWG3/vertexingHF/AliAnalysisTaskSEDs.h
@@ -51,7 +51,7 @@ class AliAnalysisTaskSEDs : public AliAnalysisTaskSE
   Int_t GetBackgroundHistoIndex(Int_t iPtBin) const { return iPtBin*4+2;}
   Int_t GetReflSignalHistoIndex(Int_t iPtBin) const { return iPtBin*4+3;}
 
-  enum {kMaxPtBins=10};
+  enum {kMaxPtBins=20};
 
   AliAnalysisTaskSEDs(const AliAnalysisTaskSEDs &source);
   AliAnalysisTaskSEDs& operator=(const AliAnalysisTaskSEDs& source); 
