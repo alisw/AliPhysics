c6f706931e342c5ea67f206145f37c80d32b5390
odjuvsla <odjuvsla@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-07 18:08:50 +0000

    - detector passed as argument to config file

--- PWG4/totEt/macros/ConfigEtMonteCarlo.C
+++ PWG4/totEt/macros/ConfigEtMonteCarlo.C
@@ -1,6 +1,6 @@
 
-AliAnalysisEtMonteCarlo * ConfigEtMonteCarlo(){
-  Bool_t EMCAL = true;
+AliAnalysisEtMonteCarlo * ConfigEtMonteCarlo(Bool_t isEmcal = true){
+  Bool_t EMCAL = isEmcal;
   if(EMCAL){
     AliAnalysisEtMonteCarloEmcal *totEtMC = new AliAnalysisEtMonteCarloEmcal();
     //Look at the 2010 p+p data...
