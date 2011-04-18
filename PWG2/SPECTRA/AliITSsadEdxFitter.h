b2c9b5106e8318b39d69d0185b62fa8f49a32edc
prino <prino@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-18 14:07:48 +0000

    Added option for initialization of peak position (Francesco) + new parameterization of mean positions (Melinda) + new containers for fit parameters (Riccardo Russo)

--- PWG2/SPECTRA/AliITSsadEdxFitter.h
+++ PWG2/SPECTRA/AliITSsadEdxFitter.h
@@ -27,15 +27,25 @@ class AliITSsadEdxFitter  : public TObject {
   };
 
   Double_t CalcSigma(Int_t code,Float_t x) const;
-  Int_t CalcMean(Int_t code,Float_t x, Float_t mean0, Float_t &mean1, Float_t &mean2) const;
+  Int_t InitializeMeanPosParam(Int_t code,Float_t x);
+  Int_t InitializeMeanPosBB(Int_t code,Float_t x);
+  void Initialize(TH1F* h,Int_t code,Int_t bin); 
 
   void GetFitPar(Double_t *fitpar, Double_t *fitparerr) const;
+  Double_t GetFitPar(Int_t nparam) const {
+    if(nparam<9) return fFitPars[nparam];
+    return -1.;
+  }
+  Double_t GetFitParError(Int_t nparam) const {
+    if(nparam<9) return fFitParErrors[nparam];
+    return -1.;
+  }
+
   void DoFitTail(TH1F *h, Int_t bin, Int_t code);
   void DoFit(TH1F *h, Int_t bin, Int_t code, TGraph *gres);
   void DoFitProton(TH1F *h, Int_t bin, Int_t code, TGraph *gres);
   void DoFitOnePeak(TH1F *h, Int_t bin, Int_t signedcode);
   void DoFitProtonFirst(TH1F *h, Int_t bin, Int_t signedcode, TGraph *gres);
-  void GetInitialParam(TH1F* h,Int_t code,Int_t bin, Float_t &pt, Float_t &ampl); 
   void FillHisto(TH1F *hsps, Int_t bin, Float_t binsize, Int_t code);
   void FillHistoMC(TH1F *hsps, Int_t bin, Int_t code, TH1F *h);
   Bool_t IsGoodBin(Int_t bin,Int_t code);
@@ -49,6 +59,10 @@ class AliITSsadEdxFitter  : public TObject {
     fIsMC=kFALSE;
     if(fITSpid) delete fITSpid;
     fITSpid=new AliITSPIDResponse(kFALSE);	  
+  }
+
+  void SetInitializationOption(Char_t opt){
+    fOptInit=opt;
   }
 
   void SetRangeStep1(Double_t dxlow=-0.2, Double_t dxup=0.3){
@@ -93,6 +107,7 @@ class AliITSsadEdxFitter  : public TObject {
   }
 
   void PrintAll() const;
+  void PrintInitialValues() const;
   void CalcResidual(TH1F *h,TF1 *fun,TGraph *gres) const;
   Double_t GausPlusTail(const Double_t x, const Double_t mean, Double_t rms, Double_t c, Double_t slope, Double_t cut ) const;  
   Double_t GausOnBackground(const Double_t* x, const Double_t *par) const;
@@ -102,8 +117,10 @@ class AliITSsadEdxFitter  : public TObject {
   AliITSsadEdxFitter(const AliITSsadEdxFitter& s);
   AliITSsadEdxFitter& operator=(const AliITSsadEdxFitter& s);
 
-  Double_t fFitpar[3];     // array with fit parameters
-  Double_t fFitparErr[3];  // array with fit parameter errors 
+  Double_t fFitPars[9];       // array with fit parameters
+  Double_t fFitParErrors[9];  // array with fit parameter errors
+  Double_t fFitpar[3];     // array with fit parameters for sel. peak
+  Double_t fFitparErr[3];  // array with fit parameter errors for sel. peak
   Double_t fRangeStep1[2]; // Range for Step1 (w.r.t pion peak)
   Double_t fRangeStep2[2]; // Range for Step2 (w.r.t kaon/proton peak)
   Double_t fRangeStep3[2]; // Range for Step3 (w.r.t proton/kaon peak)
@@ -115,18 +132,24 @@ class AliITSsadEdxFitter  : public TObject {
   Double_t fExpPionMean;    // expected mean for pion peak
   Double_t fExpKaonMean;    // expected mean for kaon peak
   Double_t fExpProtonMean;  // expected mean for proton peak
+  Double_t fExpPionMeanRange;    // expected range for pion peak mean
+  Double_t fExpKaonMeanRange;    // expected range for kaon peak mean
+  Double_t fExpProtonMeanRange;  // expected range for proton peak mean
   Double_t fExpPionSigma;   // expected sigma for pion peak
   Double_t fExpKaonSigma;   // expected sigma for kaon peak
   Double_t fExpProtonSigma; // expected sigma for proton peak
+  Double_t fExpPionAmpl;    // epxeted area under pion peak
 
   Int_t fBinsUsedPion[2];   // limits on bins used pions
   Int_t fBinsUsedKaon[2];   // limits on bins used kaons
   Int_t fBinsUsedProton[2]; // limits on bins used protons
 
   Bool_t fIsMC;                 // flag MC/data
+  Char_t fOptInit;              // option for initialization
   AliITSPIDResponse* fITSpid;   // ITS pid object
 
-  ClassDef(AliITSsadEdxFitter,3);
+
+  ClassDef(AliITSsadEdxFitter,4);
 };
 
 #endif
