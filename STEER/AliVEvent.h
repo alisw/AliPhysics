08fa7dc6daf326d00d4e554d696de3f29df1acdb
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-21 12:53:29 +0000

    Introduction of CINT5 and CMUS5 triggers (Michele)

--- STEER/AliVEvent.h
+++ STEER/AliVEvent.h
@@ -35,6 +35,8 @@ public:
       kMUON         = BIT(2), // Muon trigger, offline SPD or V0 selection
       kHighMult     = BIT(3), // High-multiplicity trigger (threshold defined online), offline SPD or V0 selection
       kEMC1         = BIT(4), // EMCAL trigger
+      kCINT5        = BIT(5), // Minimum bias trigger without SPD. i.e. interaction trigger, offline V0 selection
+      kCMUS5        = BIT(6), // Muon trigger, offline V0 selection
       kFastOnly     = BIT(30), // The fast cluster fired. This bit is set in to addition another trigger bit, e.g. kMB
       kUserDefined  = BIT(31), // Set when custom trigger classes are set in AliPhysicsSelection, offline SPD or V0 selection
       kAny          = 0xffffffff // to accept any trigger
