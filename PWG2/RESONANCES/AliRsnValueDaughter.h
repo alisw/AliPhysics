3948040e56f9d8eed5bc702d25daa670a35dd225
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-21 14:23:45 +0000

    New values and cuts

---
+++ PWG2/RESONANCES/AliRsnValueDaughter.h
@@ -0,0 +1,47 @@
+#ifndef ALIRSNVALUEDAUGHTER_H
+#define ALIRSNVALUEDAUGHTER_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+ 
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Values which depend on 4-momentum of the daughters.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "AliRsnValue.h"
+
+class AliRsnValueDaughter : public AliRsnValue {
+public:
+
+   enum EType {
+      kP,          // total momentum
+      kPt,         // transverse momentum
+      kPtpc,       // total momentum in the TPC inner wall
+      kEta,        // pseudo-rapidity
+      kITSsignal,  // ITS signal
+      kTPCsignal,  // TPC signal
+      kTOFsignal,  // TOF signal
+      kTypes
+   };
+
+   AliRsnValueDaughter(const char *name = "valDaughter", EType type = kTypes);
+   AliRsnValueDaughter(const AliRsnValueDaughter& copy);
+   AliRsnValueDaughter& operator=(const AliRsnValueDaughter& copy);
+   virtual ~AliRsnValueDaughter() { }
+
+   void             SetType(EType type)  {fType = type;}
+   EType            GetType()     const  {return fType;}
+   const char*      GetTypeName() const;
+
+   virtual Bool_t   Eval(TObject *object);
+
+protected:
+
+   EType           fType;                //  type from enumeration
+
+   ClassDef(AliRsnValueDaughter, 1)  // AliRsnValueDaughter class
+};
+
+#endif
