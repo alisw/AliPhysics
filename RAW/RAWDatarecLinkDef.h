a8917135bd647c94aa16aaf4a5ca10b135e0f3de
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-27 20:37:51 +0000

    removing obsolete classes regarding the fast decoder of 40bit Altro data, all input has been replaced by AliAltroRawStreamV3

--- RAW/RAWDatarecLinkDef.h
+++ RAW/RAWDatarecLinkDef.h
@@ -15,10 +15,6 @@
 #pragma link C++ class AliCaloRawStream+;
 #pragma link C++ class AliCaloRawStreamV3+;
 #pragma link C++ class AliVMERawStream+;
-#pragma link C++ class AliAltroDecoder+;
-#pragma link C++ class AliAltroData+;
-#pragma link C++ class AliAltroBunch+;
-#pragma link C++ class AliAltroRawStreamFast+;
 #pragma link C++ class AliRawHLTManager+;
 #pragma link C++ class AliAltroRawStreamV3+;
 #endif
