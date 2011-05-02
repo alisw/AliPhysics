284f28fe077009b3fb54c20fcb706ffdc5927f42
jotwinow <jotwinow@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-05-02 07:18:37 +0000

    -- modified cut analysis for Pb-Pb M    dNdPt/AlidNdPtCutAnalysisPbPb.cxx M    dNdPt/AlidNdPtCutAnalysisPbPb.h -- create log axis functionality M    dNdPt/AlidNdPt.h M    dNdPt/AlidNdPt.cxx

--- PWG0/dNdPt/AlidNdPtCutAnalysisPbPb.h
+++ PWG0/dNdPt/AlidNdPtCutAnalysisPbPb.h
@@ -53,7 +53,7 @@ public :
   TFolder *CreateFolder(TString folder = "folderdNdPtAnalysis",TString title = "Analysed dNdPt histograms");
 
   // Fill histograms
-  void FillHistograms(AliESDtrack *const esdTrack, AliStack *const stack) const;
+  void FillHistograms(AliESDtrack *const esdTrack, AliStack *const stack, Float_t centralityF) const;
 
   // Getters
   THnSparseF *GetEventCount()   const {return fEventCount;}
@@ -64,6 +64,10 @@ public :
   //
   THnSparseF *GetRecMCTrackHist() const {return fRecMCTrackHist;}
   
+
+  TString GetCentralityEstimator() const {return fCentralityEstimator; }
+  void SetCentralityEstimator(TString centEst="V0M") { fCentralityEstimator = centEst; }
+
 private:
 
   // analysis folder 
@@ -81,12 +85,14 @@ private:
   //
   // THnSparse track histograms
   //
-  THnSparseF *fRecMCTrackHist; //-> nClust:chi2PerClust:nClust/nFindableClust:DCAy:DCAz:eta:phi:pt:hasStrangeMother:isFromMaterial:isPrim:charge
+  //THnSparseF *fRecMCTrackHist; //-> nClust:chi2PerClust:nClust/nFindableClust:DCAy:DCAz:eta:phi:pt:hasStrangeMother:isFromMaterial:isPrim:charge:centr
+  THnSparseF *fRecMCTrackHist; //-> nCrossRows:chi2PerClust:nCrossRows/nFindableClust:fracSharedClust:DCAy:DCAz:eta:phi:pt:hasStrangeMother:isFromMaterial:isPrim:charge:centr
+  TString fCentralityEstimator;     // use centrality can be "VOM" (default), "FMD", "TRK", "TKL", "CL0", "CL1", "V0MvsFMD", "TKLvsV0M", "ZEMvsZDC"
 
   AlidNdPtCutAnalysisPbPb(const AlidNdPtCutAnalysisPbPb&); // not implemented
   AlidNdPtCutAnalysisPbPb& operator=(const AlidNdPtCutAnalysisPbPb&); // not implemented
 
-  ClassDef(AlidNdPtCutAnalysisPbPb,1);
+  ClassDef(AlidNdPtCutAnalysisPbPb,2);
 };
 
 #endif
