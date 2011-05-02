14d6eac23ad671681b35d8f68cef56c82a0fdc56
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-05-02 04:34:29 +0000

    Adding flexibility to submission macros

---
+++ PWG4/totEt/macros/hadEt/ConfigHadEtMonteCarlopp7TeV.C
@@ -0,0 +1,33 @@
+
+AliAnalysisHadEtMonteCarlo * ConfigHadEtMonteCarlo(){
+  //cout<<"Hello I am configuring you"<<endl;
+  cout<<"You are analyzing 7 TeV p+p simulations"<<endl;
+  AliAnalysisHadEtMonteCarlo *hadEtMC = new AliAnalysisHadEtMonteCarlo();
+  //Whether or not to investigate the effects of efficiency, momentum resolution, PID, etc.
+  hadEtMC->InvestigateSmearing(kFALSE);
+
+  //Whether or not to look at Et(sim)-Et(reco) for full acceptance
+  hadEtMC->InvestigateFull(kTRUE);
+
+  //Whether or not to look at Et(sim)-Et(reco) for EMCAL acceptance
+  hadEtMC->InvestigateEMCAL(kFALSE);
+
+  //Whether or not to look at Et(sim)-Et(reco) for PHOS acceptance
+  hadEtMC->InvestigatePHOS(kFALSE);
+
+  //Whether or not to look at Et(sim)-Et(reco) for Pi/K/p in full acceptance (full acceptance must be turned on)
+  hadEtMC->InvestigatePiKP(kFALSE);
+
+  //Look at ITS+TPC tracks
+  hadEtMC->RequireITSHits(kTRUE);
+
+  //Look at the 2010 p+p data...
+  hadEtMC->SetDataSet(2010);
+  hadEtMC->SetV0ScaleDataSet(2010);
+
+  //Turn baryon enhancement on and off
+  hadEtMC->EnhanceBaryons(kTRUE);
+
+  hadEtMC->Init();
+  return hadEtMC;
+}
