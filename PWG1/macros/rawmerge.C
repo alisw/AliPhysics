9a664b9f05e83b7e458a4ce6b9e09bc52a098656
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-05-10 09:53:46 +0000

    Open/Close the file only in case the file is changed reducing the AliEn API calls
    (Marian)

--- PWG1/macros/rawmerge.C
+++ PWG1/macros/rawmerge.C
@@ -4,10 +4,14 @@
 //  eventListFileName - input ascii file with list of chunks and event numbers - two collumns
 //  osplit            - split the file after given threhshold value
 //
+//  TGrid * alien = TGrid::Connect("alien://",0,0,"t");
+
 void rawmerge(const char *eventListFileName,
               const char *outputDirectoryURI,
               Long64_t osplit=-1)
 {
+  TGrid * alien = TGrid::Connect("alien://",0,0,"t");
+  
   Int_t eventNumber;
   FILE *files=fopen(eventListFileName,"r");
   if (!files) {
@@ -15,6 +19,7 @@ void rawmerge(const char *eventListFileName,
     return;
   }
   char iURI[1000];
+  TString  iURIold;
   char oURI[1000];
   TFile *ifile=0;
   TFile *ofile=0;
@@ -24,19 +29,30 @@ void rawmerge(const char *eventListFileName,
   Long64_t oevent;
   Int_t ofilenumber=0;
   Int_t line=0;
+  Int_t eventold=0;
   while (!feof(files)) {
-    delete ifile;ifile=0;
     ++line;
     if (fscanf(files,"%s %d\n",iURI,&ievent)!=2) {
       fprintf(stderr,"warning: corrupted event line (%d) in input file, skipping it...\n",line);
       continue;
-    }
+    } 
     printf("> processing \"%s\" event %d...\n",iURI,ievent);
-    TFile *ifile=TFile::Open(iURI);
-    if (!ifile) {
-      fprintf(stderr,"warning: could not open file for event \"%s\", skipping it...\n",iURI);
-      continue;
+    if (ievent==eventold) { printf("duplicated continue\n"),continue;}
+    //
+    if (iURIold.Contains(iURI)==0){ 
+      printf("NF: %s\n",iURI);
+      delete ifile;ifile=0;
+      ifile=TFile::Open(iURI);
+      if (!ifile) {
+	fprintf(stderr,"warning: could not open file for event \"%s\", skipping it...\n",iURI);
+	continue;
+      }
+      iURIold=iURI;
+    }else{
+      printf("OF: %s\n",iURI);
+      iURIold=iURI; 
     }
+    //
     TTree *itree=dynamic_cast<TTree*>(ifile->Get("RAW"));
     if (!itree) {
       fprintf(stderr,"warning: could not find RAW tree for event \"%s\", skipping it...\n",iURI);
@@ -56,10 +72,11 @@ void rawmerge(const char *eventListFileName,
       }
       otree=itree->CloneTree(0);
     }
-
     // copy event and write to file
     otree->CopyAddresses(itree);
+    if (ievent==eventold) continue;
     itree->GetEntry(ievent);
+    eventold=ievent;    
     otree->Fill();
 //    otree->CopyEntries(itree,Form("Entry$==%d",ievent),1);
     ofile->Write();
