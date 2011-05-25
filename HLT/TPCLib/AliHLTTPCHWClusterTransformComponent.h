b13424c18014cb072cd9de68011b96874f43de85
sgorbuno <sgorbuno@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-05-25 09:20:52 +0000

    move data members to private area

--- HLT/TPCLib/AliHLTTPCHWClusterTransformComponent.h
+++ HLT/TPCLib/AliHLTTPCHWClusterTransformComponent.h
@@ -133,14 +133,10 @@ protected:
 		     AliHLTUInt32_t& size, vector<AliHLTComponentBlockData>& outputBlocks );
   int DoDeinit();
   
-  int Reconfigure(const char* cdbEntry, const char* chainId);
+  int Reconfigure(const char* cdbEntry, const char* chainId);  
 
   using AliHLTProcessor::DoEvent;
-  Bool_t fDataId;
-  UInt_t fChargeThreshold;  //!transient 
-  AliHLTTPCClusterTransformation fTransform;
-  AliHLTComponentBenchmark fBenchmark; // benchmarks
-
+  
 private:
    
   int ScanConfigurationArgument(int argc, const char** argv);
@@ -153,6 +149,11 @@ private:
 
   static const char* fgkOCDBEntryHWTransform; //!transient
   
+  Bool_t fDataId;
+  UInt_t fChargeThreshold;  //!transient 
+  AliHLTTPCClusterTransformation fTransform;
+  AliHLTComponentBenchmark fBenchmark; // benchmarks
+
   ClassDef(AliHLTTPCHWClusterTransformComponent, 0)
 };
 
