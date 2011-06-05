d57f7a4f9c8bd224f62da4ded67b4dac433d1ea4
kharlov <kharlov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-06-05 21:40:19 +0000

    Mostly internal redesign. Now works with alien plugin. Externally visible changes: 1. MOST IMPORTANT: arguments of AddTaskCaloCellsQA() changed; 2. AliAnalysisTaskCaloCellsQA::InitCaloCellsQA() merged into the constructor, constructor arguments changed; 3. default constructor AliCaloCellsQA::AliCaloCellsQA() is now an I/O constructor.

--- PWG4/UserTasks/CaloCellQA/macros/AddTaskCaloCellsQA.C
+++ PWG4/UserTasks/CaloCellQA/macros/AddTaskCaloCellsQA.C
@@ -1,38 +1,45 @@
-AliAnalysisTaskCaloCellsQA* AddTaskCaloCellsQA(Int_t nmods = 10, Int_t det = 0, char* fname = "CellsQA.root",
-                                               Bool_t kFullAnalysis = kFALSE, char* contname = NULL)
+AliAnalysisTaskCaloCellsQA* AddTaskCaloCellsQA(Int_t nmods = 10, Int_t det = 0,
+                                               char* fname = "CellsQA.root", char* contname = NULL)
 {
   // Task to add EMCAL/PHOS cellsQA/runsQA to your analysis.
   //
   // Usage example for EMCAL:
   //
+  //   gSystem->Load("libPWG4UserTasks.so");
   //   gROOT->LoadMacro("$ALICE_ROOT/PWG4/UserTasks/CaloCellQA/macros/AddTaskCaloCellsQA.C");
   //   AliAnalysisTaskCaloCellsQA *taskQA = AddTaskCaloCellsQA(10); // 10 supermodules
   //   taskQA->SelectCollisionCandidates(AliVEvent::kMB); // if necessary
   //   // taskQA->SetAvoidPileup(kFALSE); // some customization
+  //   // taskQA->GetCaloCellsQA()->ActivateFullAnalysis(); // more histograms, not usually necessary
   //   // Int_t badcells[] = {74,103,917};
   //   // taskQA->SetBadCells(badcells, 3); // reject clusters containing any of these cells
   //
   // Usage example for PHOS:
   //
+  //   gSystem->Load("libPWG4UserTasks.so");
   //   gROOT->LoadMacro("$ALICE_ROOT/PWG4/UserTasks/CaloCellQA/macros/AddTaskCaloCellsQA.C");
   //   AliAnalysisTaskCaloCellsQA *taskQA = AddTaskCaloCellsQA(4, 1);
   //   taskQA->SelectCollisionCandidates(AliVEvent::kMB); // if necessary
   //   taskQA->GetCaloCellsQA()->SetClusterEnergyCuts(0.3,0.1); // increase statistics
+  //   // taskQA->SetAvoidPileup(kFALSE); // some customization
+  //   // taskQA->GetCaloCellsQA()->ActivateFullAnalysis(); // more histograms, not usually necessary
+  //   // Int_t badcells[] = {1234};
+  //   // taskQA->SetBadCells(badcells, 1); // reject clusters containing any of these cells
   //
-  // fname -- output file name;
   // nmods -- maximum supermodule number + 1:
   //   use 4 for EMCAL <= 2010;
   //   use 4 for PHOS (PHOS numbers start from 1, not from zero);
   //   use 10 for EMCAL >= 2011;
   // det -- detector, 0/EMCAL, 1/PHOS;
-  // kFullAnalysis -- if true, initialize the analysis to fill more histograms
-  //                  (necessary for the completeness in searching of problematic cells);
+  // fname -- output file name;
+  //   if NULL, the output will be written into mgr->GetCommonFileName() + container;
   // contname -- TObjArray container name in the output file;
-  //   DO NOT USE THIS OPTION UNLESS YOU KNOW WHAT YOU ARE DOING;
-  //   if not NULL, the output will be written into the output container and cannot be later
-  //   merged for different run numbers;
-  //   name must be unique, if you are going to call AddTaskCaloCellsQA() several times
-  //   (AliAnalysisManager limitation).
+  //   if not NULL (or fname = NULL), the output will be written into output container and
+  //   cannot be later merged for different run numbers;
+  //   contname must be unique, if you are going to call AddTaskCaloCellsQA() several times.
+  //
+  // Note that if fname = NULL and contname = NULL, the output will be written into
+  //   file mgr->GetCommonFileName() with container name CellsQAResults.
 
   // get manager instance
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
@@ -50,29 +57,32 @@ AliAnalysisTaskCaloCellsQA* AddTaskCaloCellsQA(Int_t nmods = 10, Int_t det = 0, 
   // Configure analysis
   //===========================================================================
 
+  // detector number
+  Int_t det2 = -1;
+  if       (det == 0) det2 = AliAnalysisTaskCaloCellsQA::kEMCAL;
+  else  if (det == 1) det2 = AliAnalysisTaskCaloCellsQA::kPHOS;
+  else
+  ::Fatal("AddTaskCaloCellsQA", "Wrong detector provided");
+
   AliAnalysisTaskCaloCellsQA* task;
 
-  if (!contname) task = new AliAnalysisTaskCaloCellsQA("AliAnalysisTaskCaloCellsQA", fname);
-  else           task = new AliAnalysisTaskCaloCellsQA("AliAnalysisTaskCaloCellsQA");
+  if (fname && !contname) task = new AliAnalysisTaskCaloCellsQA("AliAnalysisTaskCaloCellsQA", nmods, det2, fname);
+  else                    task = new AliAnalysisTaskCaloCellsQA("AliAnalysisTaskCaloCellsQA", nmods, det2);
   mgr->AddTask(task);
-
-  // initialize analysis instance
-  if (det == 0)// EMCAL
-    task->InitCaloCellsQA(nmods, AliAnalysisTaskCaloCellsQA::kEMCAL);
-  else if (det == 1)// PHOS
-    task->InitCaloCellsQA(nmods, AliAnalysisTaskCaloCellsQA::kPHOS);
-  else
-    ::Fatal("AddTaskCaloCellsQA", "Wrong detector provided");
-
-  // activate filling of all the histograms
-  if (kFullAnalysis)
-    task->GetCaloCellsQA()->ActivateFullAnalysis();
 
   mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
 
-  if (contname)
+  // container output into particular file
+  if (fname && contname)
     mgr->ConnectOutput(task, 1, mgr->CreateContainer(contname,
                        TObjArray::Class(), AliAnalysisManager::kOutputContainer, fname));
+
+  // container output into common file
+  if (!fname) {
+    if (!contname) contname = "CellsQAResults";
+    mgr->ConnectOutput(task, 1, mgr->CreateContainer(contname,
+                       TObjArray::Class(), AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName()));
+  }
 
   return task;
 }
