431ce6e080ec3f374309fd49e774d089b41f268a
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-06-20 18:53:56 +0000

    Fixing minor coding violation

--- PWG4/totEt/AliAnalysisTaskHadEt.h
+++ PWG4/totEt/AliAnalysisTaskHadEt.h
@@ -27,7 +27,7 @@ public:
     virtual void   UserCreateOutputObjects();
     virtual void   UserExec(Option_t *option);
     virtual void   Terminate(Option_t *);
-    void IsSim(){isSim = kTRUE;}
+    void IsSim(){fIsSim = kTRUE;}
 
 private:
 
@@ -37,7 +37,7 @@ private:
 
     AliAnalysisHadEtReconstructed *fRecAnalysis; // Rec
     AliAnalysisHadEtMonteCarlo *fMCAnalysis; // MC
-    Bool_t isSim;//Boolean to keep track of whether or not this is running on simulations
+    Bool_t fIsSim;//Boolean to keep track of whether or not this is running on simulations
 
     ClassDef(AliAnalysisTaskHadEt, 2); // example of analysis
 };
