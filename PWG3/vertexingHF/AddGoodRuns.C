64d461ec10fd44c186251ad78741acc8780c27aa
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-06-29 16:10:53 +0000

    Update for low energy pp (Zaida, Renu)

--- PWG3/vertexingHF/AddGoodRuns.C
+++ PWG3/vertexingHF/AddGoodRuns.C
@@ -93,14 +93,27 @@ Int_t AddGoodRuns(AliAnalysisAlien* plugin,TString lhcPeriod,TString mcprod="") 
   }
 
   if(lhcPeriod=="LHC11a") {
-    nruns=14;
-    Int_t runlist[14]={146860, 146859, 146856, 146824, 146817, 146806, 146805, 146804, 146803, 146802, 146801, 146748, 146747, 146746};  
-   
-    for(Int_t k=0;k<nruns;k++){
-      if(runlist[k]<firstrun || runlist[k]>lastrun) continue;
-      plugin->AddRunNumber(runlist[k]);
-      ngoodruns++;
+    
+    if(mcprod=="") {
+      nruns=15;
+      Int_t runlist[15]={146860, 146859, 146858, 146856, 146824, 146817, 146806, 146805, 146804, 146803, 146802, 146801, 146748, 146747, 146746};  
+      
+      for(Int_t k=0;k<nruns;k++){
+	if(runlist[k]<firstrun || runlist[k]>lastrun) continue;
+	plugin->AddRunNumber(runlist[k]);
+	ngoodruns++;
+      }
+    } else if(mcprod=="LHC11b10b") {
+      nruns=24;
+      Int_t runlist[24]={146860, 146859, 146858, 146857, 146856, 146824, 146817, 146814, 146813, 146812, 146808, 146807, 146806,
+			 146805, 146804, 146803, 146802, 146801, 146748, 146747, 146746, 146689, 146688, 146686};
+      for(Int_t k=0;k<nruns;k++){
+	if(runlist[k]<firstrun || runlist[k]>lastrun) continue;
+	plugin->AddRunNumber(runlist[k]);
+	ngoodruns++;
+      }
     }
+    
     plugin->SetNrunsPerMaster(ngoodruns);
   }
 
