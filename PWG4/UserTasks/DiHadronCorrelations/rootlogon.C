0d37e16aa0c365e3742dc38ee71bbbc584806d60
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-01 10:50:13 +0000

    coding vios

--- PWG4/UserTasks/DiHadronCorrelations/rootlogon.C
+++ PWG4/UserTasks/DiHadronCorrelations/rootlogon.C
@@ -15,8 +15,11 @@
       gRandom = new TRandom3(0);
    }
 
+  gSystem->Exec("ln -s AliPool.cxx.txt AliPool.cxx");
+  gSystem->Exec("ln -s AliPool.h.txt AliPool.h");
+  gROOT->ProcessLine(".L AliPool.cxx+g");
+
   // Include directories
   // gSystem->AddIncludePath("-I\"common\" ");
   // gROOT->LoadMacro("common/Utils.C+g");
-
 }
