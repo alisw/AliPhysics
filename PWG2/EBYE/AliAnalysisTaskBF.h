f7261772240ac520a7e4305d7a2482abf1b96c06
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-13 22:00:59 +0000

    Adding the macro to run the centrality train for the balance function + major updates in the bf code to run on AODs and on a train mode (PbPb) provided by Michael Weber

--- PWG2/EBYE/AliAnalysisTaskBF.h
+++ PWG2/EBYE/AliAnalysisTaskBF.h
@@ -6,19 +6,24 @@
 
 class TList;
 class TH1F;
+class TH2F;
 
 class AliBalance;
 class AliESDtrackCuts;
 
 #include "AliAnalysisTaskSE.h"
+#include "AliBalance.h"
+
 
 class AliAnalysisTaskBF : public AliAnalysisTaskSE {
  public:
   AliAnalysisTaskBF(const char *name = "AliAnalysisTaskBF");
   virtual ~AliAnalysisTaskBF() {}
   
+  
   virtual void   UserCreateOutputObjects();
   virtual void   UserExec(Option_t *option);
+  virtual void   FinishTaskOutput();
   virtual void   Terminate(Option_t *);
 
   void SetAnalysisObject(AliBalance *const analysis) {
@@ -30,18 +35,51 @@ class AliAnalysisTaskBF : public AliAnalysisTaskSE {
     fVyMax = vy;
     fVzMax = vz;
   }
+  void SetKinematicsCutsAOD(Double_t ptmin, Double_t ptmax, Double_t etamin, Double_t etamax){
+    fPtMin  = ptmin;
+    fPtMax  = ptmax;
+    fEtaMin = etamin;
+    fEtaMax = etamax;
+
+  }
+ 
+  //Centrality
+  void SetCentralityEstimator(const char* centralityEstimator) {fCentralityEstimator = centralityEstimator;}
+  const char* GetCentralityEstimator(void)                     {return fCentralityEstimator;}
+  void SetCentralityPercentileRange(Double_t min, Double_t max) { 
+    fCentralityPercentileMin=min;
+    fCentralityPercentileMax=max;
+  }
 
   void UseOfflineTrigger() {fUseOfflineTrigger = kTRUE;}
 
  private:
   AliBalance *fBalance; //BF object
   TList *fList; //fList object
+  TList *fListBF; //fList object
+
   TH1F *fHistEventStats; //event stats
+  TH1F *fHistTrackStats; //Track filter bit stats
   TH1F *fHistVx; //x coordinate of the primary vertex
   TH1F *fHistVy; //y coordinate of the primary vertex
   TH1F *fHistVz; //z coordinate of the primary vertex
 
+  TH2F *fHistClus;
+  TH2F *fHistDCA;
+  TH1F *fHistChi2;
+  TH1F *fHistPt;
+  TH1F *fHistEta;
+  TH1F *fHistPhi;
+  TH2F *fHistV0M;
+
+  TH1F *fHistBF[NUMBER_OF_ANALYSES][3];
+  TH1F *fHistN;
+
   AliESDtrackCuts *fESDtrackCuts; //ESD track cuts
+
+  TString fCentralityEstimator;      //"V0M","TRK","TKL","ZDC","FMD"
+  Double_t fCentralityPercentileMin;
+  Double_t fCentralityPercentileMax;
 
   Bool_t fUseOfflineTrigger;//Usage of the offline trigger selection
 
@@ -49,10 +87,15 @@ class AliAnalysisTaskBF : public AliAnalysisTaskSE {
   Double_t fVyMax;//vymax
   Double_t fVzMax;//vzmax
 
+  Double_t fPtMin;//only used for AODs
+  Double_t fPtMax;//only used for AODs
+  Double_t fEtaMin;//only used for AODs
+  Double_t fEtaMax;//only used for AODs
+
   AliAnalysisTaskBF(const AliAnalysisTaskBF&); // not implemented
   AliAnalysisTaskBF& operator=(const AliAnalysisTaskBF&); // not implemented
   
-  ClassDef(AliAnalysisTaskBF, 3); // example of analysis
+  ClassDef(AliAnalysisTaskBF, 5); // example of analysis
 };
 
 #endif
--- PWG2/EBYE/macros/configBalanceFunctionAnalysis.C
+++ PWG2/EBYE/macros/configBalanceFunctionAnalysis.C
@@ -3,30 +3,55 @@ AliBalance *GetBalanceFunctionObject(const char* analysisLevel = "ESD") {
   //Function to setup the AliProtonAnalysis object and return it
   AliBalance *gBalance = new AliBalance();
   gBalance->SetAnalysisLevel(analysisLevel);
-  gBalance->SetAnalysisType(1);
-  gBalance->SetNumberOfBins(18);
-  gBalance->SetInterval(-0.9,0.9);
 
+  //Set all the same
+  //gBalance->SetNumberOfBins(-1,9);
+  //gBalance->SetInterval(-1,0.0,0.9);
+
+  //Set all analyses separately
+  //Rapidity
+  gBalance->SetNumberOfBins(0,64);
+  gBalance->SetInterval(0,0.0,1.6);
+  
+  //Eta
+  gBalance->SetNumberOfBins(1,64);
+  gBalance->SetInterval(1,0.0,1.6);
+  
+  //Qlong
+  gBalance->SetNumberOfBins(2,64);
+  gBalance->SetInterval(2,0.0,2.0);
+  
+  //Qout
+  gBalance->SetNumberOfBins(3,64);
+  gBalance->SetInterval(3,0.0,2.0);
+  
+  //Qside
+  gBalance->SetNumberOfBins(4,64);
+  gBalance->SetInterval(4,0.0,2.0);
+  
+  //Qinv
+  gBalance->SetNumberOfBins(5,64);
+  gBalance->SetInterval(5,0.0,2.0);
+  
+  //Phi
+  gBalance->SetNumberOfBins(6,64);
+  gBalance->SetInterval(6,-8.0,8.0);
+  
   return gBalance;
 }
 
 //__________________________________________________//
 AliESDtrackCuts *GetTrackCutsObject() {
-  //Function to setup the AliESDtrackCuts object and return it
-  AliESDtrackCuts *cuts = new AliESDtrackCuts("bfTrackCuts","bfTrackCuts");
-  cuts->SetMinNClustersTPC(80);
-  cuts->SetMinNClustersITS(2);
-  cuts->SetMaxChi2PerClusterTPC(4.0);
-  cuts->SetRequireTPCRefit();
-  cuts->SetRequireITSRefit();
-  cuts->SetAcceptKinkDaughters(kFALSE);
-  cuts->SetMaxDCAToVertexXY(0.3);
-  cuts->SetMaxDCAToVertexZ(0.3);
+  // only used for ESDs
+  // Function to setup the AliESDtrackCuts object and return it
+  AliESDtrackCuts *cuts = AliESDtrackCuts::GetStandardTPCOnlyTrackCuts();
+  cuts->SetMinNClustersTPC(70);
   
   cuts->SetPtRange(0.3,1.5);
   cuts->SetEtaRange(-0.8,0.8);
-
-  cuts->SaveHistograms("trackCuts");
+  cuts->DefineHistograms(1);
+  //cuts->SaveHistograms("trackCuts");
 
   return cuts;
 }
+
