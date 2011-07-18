dbed644b64916842232d51435267576f90fa265a
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-18 13:21:22 +0000

    merge.C     - local cache - done with timeout makeOCDB.C  - TRD fix to export the OCDB

--- PWG1/CalibMacros/Pass0/merge.C
+++ PWG1/CalibMacros/Pass0/merge.C
@@ -3,15 +3,16 @@
   using AliFileMerger functionality
 
   Directory with runCalibTrain output: outputDir
-  pattern: AliESDfriends_v1.root 
+  pattern: AliESDfriends_v1.root
   Output file name: CalibObjects.root
 
-  Example: 
+  Example:
   .L $ALICE_ROOT/ANALYSIS/CalibMacros/MergeCalibration/merge.C
   merge("alien:///alice/cern.ch/user/j/jotwinow/CalibTrain/output","AliESDfriends_v1.root");
 */
 
-void merge(const char* outputDir, const char* pattern) {
+void merge(const char* outputDir, const char* pattern)
+{
   //
   // load libraries
   //
@@ -21,13 +22,69 @@ void merge(const char* outputDir, const char* pattern) {
 
   //
   AliFileMerger merger;
-  merger.AddReject("esdFriend"); // do not merge 
+  merger.AddReject("esdFriend"); // do not merge
+  cpTimeOut(outputDir, pattern,10);
 
-  // local 
-  // merger.IterTXT("calib.list","CalibObjects.root",kFALSE);
-	 
-  // alien 
-  merger.IterAlien(outputDir, "CalibObjects.root", pattern);
+  // local
+   merger.IterTXT("calib.list","CalibObjects.root",kFALSE);
+
+  // alien
+  //merger.IterAlien(outputDir, "CalibObjects.root", pattern);
 
 return;
 }
+
+void cpTimeOut(const char * searchdir, const char* pattern, Int_t timeOut=10)
+{
+
+  gSystem->Setenv("XRDCLIENTMAXWAIT",Form("%d",timeOut));
+  gEnv->SetValue("XNet.RequestTimeout", timeOut);
+  gEnv->SetValue("XNet.ConnectTimeout", timeOut);
+  gEnv->SetValue("XNet.TransactionTimeout", timeOut);
+  TFile::SetOpenTimeout(timeOut);
+
+  TGrid::Connect("alien");
+
+  TString filelist;
+  TString command;
+  command = Form("find %s/ %s", searchdir, pattern);
+  cerr<<"command: "<<command<<endl;
+  TGridResult *res = gGrid->Command(command);
+  if (!res) return;
+  TIter nextmap(res);
+  TMap *map = 0;
+
+  ofstream outputFile;
+  outputFile.open(Form("calib.list"));
+  Int_t counter=0;
+
+  while((map=(TMap*)nextmap()))
+  {
+    TObjString *objs = dynamic_cast<TObjString*>(map->GetValue("turl"));
+    if (!objs || !objs->GetString().Length())
+    {
+      delete res;
+      break;
+    }
+
+    TString src=Form("%s",objs->GetString().Data());
+    TString dst=src;
+    dst.ReplaceAll("alien:///","");
+    dst.ReplaceAll("/","_");
+    TTimeStamp s1;
+    Bool_t result = TFile::Cp(src.Data(),dst.Data(),kTRUE);
+    TTimeStamp s2;
+    AliSysInfo::AddStamp(dst.Data(),counter, result);
+    if (result)
+    {
+      counter++;
+      outputFile << dst.Data()<< endl;
+    }
+  }
+  cout<<counter<<" files copied!"<<endl;
+
+  outputFile.close();
+  gSystem->Exec("mv syswatch.log syswatch_copy.log");
+  return;
+}
+
