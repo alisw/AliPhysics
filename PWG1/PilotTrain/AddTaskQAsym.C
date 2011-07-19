30ab1f1e4a2acc0b76fcdc05600fadf2ef59def8
esicking <esicking@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-19 15:51:55 +0000

    Added tasks for Emcal trigger and Muon trigger

--- PWG1/PilotTrain/AddTaskQAsym.C
+++ PWG1/PilotTrain/AddTaskQAsym.C
@@ -1,4 +1,8 @@
-AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, UInt_t maskMB, UInt_t maskHM)
+AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, 
+				    UInt_t maskMB,
+				    UInt_t maskHM,
+				    UInt_t maskEM,
+				    UInt_t maskMU)
 
 {
   // Creates a QA task exploiting simple symmetries phi, eta +/-, charge ...
@@ -33,6 +37,18 @@ AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, UInt_t maskMB, UInt_t maskH
    task0HM->SetTrackType(0);
    task0HM->SelectCollisionCandidates(maskHM);
    task0HM->SetNChargedRange(30,50); 
+   //Task for global tracks (EMCAL trigger)
+   AliAnalysisTaskQASym *task0EM = new AliAnalysisTaskQASym("AliAnalysisTaskQASym_Global_EMCAL");
+   task0EM->SetTrackType(0);
+   task0EM->SelectCollisionCandidates(maskEM); 
+   task0EM->SetNChargedRange(30,50);
+   //Task for global tracks (Muon trigger)
+   AliAnalysisTaskQASym *task0MU = new AliAnalysisTaskQASym("AliAnalysisTaskQASym_Global_MUON");
+   task0MU->SetTrackType(0);
+   task0MU->SelectCollisionCandidates(maskMU); 
+   task0MU->SetNChargedRange(30,50);
+
+
    //Task for ITS tracks 
    AliAnalysisTaskQASym *task1 = new AliAnalysisTaskQASym("AliAnalysisTaskQASym_ITS");
    task1->SetTrackType(1);
@@ -93,18 +109,24 @@ AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, UInt_t maskMB, UInt_t maskH
 
    task0->SetCuts(esdTrackCutsL0);
    task0HM->SetCuts(esdTrackCutsL0);
+   task0EM->SetCuts(esdTrackCutsL0);
+   task0MU->SetCuts(esdTrackCutsL0);
    task1->SetCuts(esdTrackCutsL1);
    task1sa->SetCuts(esdTrackCutsL1sa);
    task2->SetCuts(esdTrackCutsL2);
 
    mgr->AddTask(task0);
    mgr->AddTask(task0HM);
+   mgr->AddTask(task0EM);
+   mgr->AddTask(task0MU);
    mgr->AddTask(task1);
    mgr->AddTask(task1sa);
    mgr->AddTask(task2);
   
    AliAnalysisDataContainer *cout0    = 0;
    AliAnalysisDataContainer *cout0HM  = 0;
+   AliAnalysisDataContainer *cout0EM  = 0;
+   AliAnalysisDataContainer *cout0MU  = 0;
    AliAnalysisDataContainer *cout1    = 0;
    AliAnalysisDataContainer *cout1sa  = 0;
    AliAnalysisDataContainer *cout2    = 0;
@@ -113,6 +135,10 @@ AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, UInt_t maskMB, UInt_t maskH
     cout0 =  mgr->CreateContainer("QAsymHists_Global",TList::Class(),
 				  AliAnalysisManager::kOutputContainer, Form("run%d.root",runNumber));
     cout0HM =  mgr->CreateContainer("QAsymHists_Global_HighMult",TList::Class(),
+				    AliAnalysisManager::kOutputContainer, Form("run%d.root",runNumber));
+    cout0EM =  mgr->CreateContainer("QAsymHists_Global_Emcal",TList::Class(),
+				    AliAnalysisManager::kOutputContainer, Form("run%d.root",runNumber));
+    cout0MU =  mgr->CreateContainer("QAsymHists_Global_Muon",TList::Class(),
 				    AliAnalysisManager::kOutputContainer, Form("run%d.root",runNumber));
     cout1 =  mgr->CreateContainer("QAsymHists_ITS",TList::Class(),
 				  AliAnalysisManager::kOutputContainer, Form("run%d.root",runNumber));
@@ -129,6 +155,12 @@ AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, UInt_t maskMB, UInt_t maskH
       cout0HM = mgr->CreateContainer("QAsymHists_Global_HighMult",TList::Class(),
 				   AliAnalysisManager::kOutputContainer, 
 				   Form("%s:PWG1_QAsymHists",AliAnalysisManager::GetCommonFileName()));
+      cout0EM = mgr->CreateContainer("QAsymHists_Global_Emcal",TList::Class(),
+				     AliAnalysisManager::kOutputContainer, 
+				     Form("%s:PWG1_QAsymHists",AliAnalysisManager::GetCommonFileName()));
+      cout0MU = mgr->CreateContainer("QAsymHists_Global_Muon",TList::Class(),
+				     AliAnalysisManager::kOutputContainer, 
+				     Form("%s:PWG1_QAsymHists",AliAnalysisManager::GetCommonFileName()));
       cout1 = mgr->CreateContainer("QAsymHists_ITS",TList::Class(),
 				   AliAnalysisManager::kOutputContainer, 
 				 Form("%s:PWG1_QAsymHists",AliAnalysisManager::GetCommonFileName()));
@@ -143,12 +175,16 @@ AliAnalysisTaskQASym * AddTaskQAsym(Int_t runNumber, UInt_t maskMB, UInt_t maskH
 
    mgr->ConnectInput  (task0,   0, mgr->GetCommonInputContainer());
    mgr->ConnectInput  (task0HM, 0, mgr->GetCommonInputContainer());
+   mgr->ConnectInput  (task0EM, 0, mgr->GetCommonInputContainer());
+   mgr->ConnectInput  (task0MU, 0, mgr->GetCommonInputContainer());
    mgr->ConnectInput  (task1,   0, mgr->GetCommonInputContainer());
    mgr->ConnectInput  (task1sa, 0, mgr->GetCommonInputContainer());
    mgr->ConnectInput  (task2,   0, mgr->GetCommonInputContainer());
 
    mgr->ConnectOutput (task0,   1, cout0);
    mgr->ConnectOutput (task0HM, 1, cout0HM);
+   mgr->ConnectOutput (task0EM, 1, cout0EM);
+   mgr->ConnectOutput (task0MU, 1, cout0MU);
    mgr->ConnectOutput (task1,   1, cout1);
    mgr->ConnectOutput (task1sa, 1, cout1sa);
    mgr->ConnectOutput (task2,   1, cout2);
