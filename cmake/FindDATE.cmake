47f10b24412a22584bb9d0649e68b67722c96217
fca <fca@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-21 12:40:33 +0000

    Revert of a harmful change (A.Goel)

--- cmake/FindDATE.cmake
+++ cmake/FindDATE.cmake
@@ -3,7 +3,7 @@
 # Author: Johny Jose (johny.jose@cern.ch)
 #         Port of previous Makefile build to cmake
 
-cmake_minimum_required(VERSION 2.8.4 FATAL_ERROR)
+cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
 
 find_program(DATE_CONFIG date-config)
 message(STATUS "Check for Date : ${DATE_CONFIG}")
@@ -13,9 +13,10 @@ if(DATE_CONFIG)
   set(DATE_COMMON_DEFS $ENV{DATE_COMMON_DEFS})
   set(DATE_MONITOR_DIR $ENV{DATE_MONITOR_DIR})
   execute_process(COMMAND date-config --cflags OUTPUT_VARIABLE DATEFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REGEX REPLACE "\"" "'" DATEFLAGS ${DATEFLAGS})
+  set(DATEFLAGS "-DALI_DATE ${DATEFLAGS}")
   execute_process(COMMAND date-config --monitorlibs OUTPUT_VARIABLE DMONLIBS OUTPUT_STRIP_TRAILING_WHITESPACE)
 else()
   set(DATEFLAGS "-D${CMAKE_SYSTEM_NAME} -DDATE_SYS=${CMAKE_SYSTEM_NAME} -Dlong32='int' -Dlong64='long long' -DdatePointer='long'")
 endif(DATE_CONFIG)
-string ( REPLACE "\\" "" DATEFLAGS "${DATEFLAGS}" )
-message(STATUS "DATEFLAGS: ${DATEFLAGS}")
+
