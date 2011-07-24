fb9ce63b536e0125017a6cd5923115020e0933f1
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-24 09:14:51 +0000

    Updated LRC code (Andrey Ivanov): I'm sure that tomorrow I will receive a mail asking me to change the code, the only connection to which is that I commit it, significantly and then within the next 7 days if I don't do so I will be banned

--- PWG2/EBYE/macros/AddTaskLRC.C
+++ PWG2/EBYE/macros/AddTaskLRC.C
@@ -1,4 +1,5 @@
-AliAnalysisTaskLRC *AddTaskLRC(Bool_t RunKine=kFALSE)
+
+AliAnalysisTaskLRC *AddTaskLRC(Bool_t RunKine=kFALSE, TString OutputRootFolder=":PWG2LRC")
 {
 // This macro adds AliAnalysisTaskLRC to existing AnalysisManager
 // RunKine paramiter switch task to kinematics analysis 
@@ -6,79 +7,43 @@ AliAnalysisTaskLRC *AddTaskLRC(Bool_t RunKine=kFALSE)
 // Author : Andrey Ivanov , St.Peterburg State University
 // Email: Andrey.Ivanov@cern.ch
 
-// Version line : 3.5
-// Version 3.5.5
+// Version line : 3.6
+// Version 3.6.7
 
+	gROOT->LoadMacro("configLRCAnalysis.C");
+	
+	//gROOT->LoadMacro("AliAnalysisTaskIA.cxx+g"); 
+	
 
- 
-   // A. Get the pointer to the existing analysis manager via the static access method.
-   //==============================================================================
-   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-   if (!mgr) {
-      Error("AddTaskLRC", "No analysis manager to connect to.");
-      return NULL;
-   }  
+	// A. Get the pointer to the existing analysis manager via the static access method.
+	//==============================================================================
+	AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+	if (!mgr) {
+		Error("AddTaskLRC", "No analysis manager to connect to.");
+		return NULL;
+	}  
+	
+	// B. Check the analysis type using the event handlers connected to the analysis
+	//    manager. The availability of MC handler cann also be checked here.
+	//==============================================================================
+	if (!mgr->GetInputEventHandler()) {
+		Error("AddTaskLRC", "This task requires an input event handler");
+		return NULL;
+	}  
+	TString type = mgr->GetInputEventHandler()->GetDataType(); 
+	cout<<" # TaskLRC - input :"<<type<<"\n";
 
-   // B. Check the analysis type using the event handlers connected to the analysis
-   //    manager. The availability of MC handler cann also be checked here.
-   //==============================================================================
-   if (!mgr->GetInputEventHandler()) {
-      Error("AddTaskLRC", "This task requires an input event handler");
-      return NULL;
-   }  
-   TString type = mgr->GetInputEventHandler()->GetDataType(); 
-   
-   cout<<" # TaskLRC - input :"<<type<<"\n";
-   
-  
+	// MB - Global2
 
-     
-   // C. Create the task, add it to manager.
-   //===========================================================================
-   AliAnalysisTaskLRC *taskLRC = new AliAnalysisTaskLRC("Tak_LRC",RunKine);
-   mgr->AddTask(taskLRC);
+	taskLRC = createLRCtaskSkeleton("Task_LRC_MB_Global2",RunKine);
+	taskLRC->SetTrackCuts(createAliLRCcuts("Global2"));
+	addAliLRCProcessors(taskLRC,kTRUE);
+	taskLRC->SetVtxDiamond(0.4,0.4,7.0);
+	taskLRC->SetMaxPtLimit(1.5);
+	taskLRC->SetMinPtLimit(0.3);
+	mgr->AddTask(taskLRC);
+	configureLRCtaskOutput(taskLRC,":PWG2LRC");
 
-   taskLRC->fMinPtLimit=0.2;    // 200MeV minimal Pt (5GeV max Pt by default)
-      
-   // D. Configure the analysis task. Extra parameters can be used via optional
-   // arguments of the AddTaskXXX() function.
-   //===========================================================================
- 
-   //Adding LRC processors to the task
-   
- //FB
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.2,-0.0,0.0,0.2));
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.4,-0.0,0.0,0.4));
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.6,-0.0,0.0,0.6));
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.8,-0.0,0.0,0.8));
-//0.2 gap
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.4,-0.2,0.2,0.4));
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.6,-0.4,0.4,0.6));
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.8,-0.6,0.6,0.8));
-//0.4 gap
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.6,-0.2,0.2,0.6));
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.8,-0.4,0.4,0.8));
-//0.6 gap
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.8,-0.2,0.2,0.8));
-
-//FULL
-taskLRC->AddLRCProcess(new AliLRCProcess(-0.8,0.8,-0.8,0.8));
-
-TString OutName;
-OutName="LRC_out_"+type;
-
-   
-   // E. Create ONLY the output containers for the data produced by the task.
-   // Get and connect other common input/output containers via the manager as below
-   //==============================================================================
- TString outputFileName = AliAnalysisManager::GetCommonFileName();
- outputFileName += ":PWG2EbyE.LRC.C.root";
-   AliAnalysisDataContainer *cout_LRC = mgr->CreateContainer(OutName, TList::Class(),
-							     AliAnalysisManager::kOutputContainer,outputFileName.Data());
-   mgr->ConnectInput(taskLRC, 0, mgr->GetCommonInputContainer());
-   mgr->ConnectOutput(taskLRC, 1, cout_LRC);
-
-   // Return task pointer at the end
-   return taskLRC;
+return taskLRC;
 }
 
