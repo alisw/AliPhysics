b57659a02f20a8a34c15ad60c5006514614a7f63
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-07-24 09:14:51 +0000

    Updated LRC code (Andrey Ivanov): I'm sure that tomorrow I will receive a mail asking me to change the code, the only connection to which is that I commit it, significantly and then within the next 7 days if I don't do so I will be banned

--- PWG2/EBYE/LRC/AliAnalysisTaskLRC.h
+++ PWG2/EBYE/LRC/AliAnalysisTaskLRC.h
@@ -5,23 +5,22 @@
 // This includes a TList of AliLRCProcess objects that are processing LRC analysis
 // for a given Eta window 
 
-
 // Author : Andrey Ivanov , St.Peterburg State University
 // Email: Andrey.Ivanov@cern.ch
 
-// Version line : 3.5
-// Version 3.5.5
+/*  See cxx source for full Copyright notice */
 
-#include "AliAnalysisTaskSE.h"
-#include "AliLRCProcess.h"
+#include <AliAnalysisTaskSE.h>
+class AliLRCProcess;
+class AliLRCBase;
+class AliESDtrackCuts;
+class TH1D;
+class TH2D;
 
 class AliAnalysisTaskLRC : public AliAnalysisTaskSE {
 
 public:
- 
- 
-  //Constructors
-  
+   //Constructors 
   AliAnalysisTaskLRC(const char *name = "AliAnalysisTaskLRC",Bool_t runKine=kFALSE);
   virtual ~AliAnalysisTaskLRC() {}
   
@@ -30,21 +29,77 @@ public:
   virtual void   UserCreateOutputObjects();
   virtual void   UserExec(Option_t *option);
   virtual void   Terminate(Option_t *);
-    
-  void AddLRCProcess(AliLRCProcess *newProc); //Adds new AliLRCProcess to analysis task
+  //----------------------------------  
   
+  void AddLRCProcess(AliLRCBase *newProc); //Adds new AliLRCProcess to analysis task
+  
+  // Setters
+  
+  void SetMaxPtLimit(Double_t MaxPtLimit);   //Sets  Max Pt filter
+  void SetMinPtLimit(Double_t MinPtLimit);   //Sets  Min Pt filter 
+  void SetCheckForkVtx(Bool_t CheckForkVtx){fCheckForkVtx=CheckForkVtx;} // Accept only events with veretex
+  void SetCheckForVtxPosition(Bool_t CheckForVtxPosition ){fCheckForVtxPosition=CheckForVtxPosition;} //Accept only events with veretex in slected range
+  void SetTrackCuts(AliESDtrackCuts* const cuts)  { fEsdTrackCuts = cuts; }
+  void SetShowEventStats(Bool_t ShowEventStats)  {fShowEventStats= ShowEventStats;}
+  void SetShowPerTrackStats(Bool_t ShowPerTrackStats) {fShowPerTrackStats=ShowPerTrackStats;}
+  void SetVtxDiamond(Double_t Vx, Double_t Vy, Double_t Vz) {fVxMax = Vx;fVyMax =Vy;fVzMax = Vz;}
+  void SetNchCuts(Int_t minNch, Int_t maxNch){fMinAceptedTracksCut=minNch; fMaxAceptedTracksCut=maxNch;}
+  
+// Getters
+  TList* GetListOfProcessors() { return &fLRCproc;} // Returns list of included 
+  AliESDtrackCuts* GetTrackCuts() const                         { return fEsdTrackCuts; }
+  AliLRCBase * Proc(Int_t index);// Get Processor i 
+  
+  
+  
+protected:
+// Track cuts
+
+  AliESDtrackCuts *fEsdTrackCuts;               // esd track cuts
+
+
+
+// Aceptance cuts
+
   Double_t fMaxPtLimit;  //Max Pt filter
   Double_t fMinPtLimit;  // Min Pt filter 
-  Bool_t fDropKineE;     // Force to drop any e+- in Kine
 
-private:
+// Nch cuts
+  Int_t fMinAceptedTracksCut;   //Minimum number of accepted tracks in event
+  Int_t fMaxAceptedTracksCut;   //Maximum number of accepted tracks in event
   
+// Vtx cuts
+  Bool_t fCheckForkVtx;		// Check for vertex
+  Bool_t fCheckForVtxPosition;  // Check if vertex position in range
+  Double_t fVxMax;	// X vrtx max
+  Double_t fVyMax;	// Y vrtx max
+  Double_t fVzMax;	// Z vrtx max
+
+
   TList fLRCproc;       //  AliLRCProcess objects list
-  TList* fOutList;      //! Output data container 
+  TList* fOutList;      //! Task Output data container 
   
   Bool_t fRunKine;      // ESD/AOD  - KINE switch
-  
+  Bool_t fShowEventStats; //  Allows per event debug output (trigger Nch, cuts etc)
+  Bool_t fShowPerTrackStats; // Allows per track debug output
       
+
+// QA histos 
+
+  TH1D *fHistEventCutStats;  //! Event cut statistics
+  TH1D *fHistTrackCutStats;  //! Track cut statistics
+
+  TH1D *fHistVx;  //!Vx hist
+  TH1D *fHistVy;  //!Vy hist
+  TH1D *fHistVz;  //!Vz hist
+
+  TH2D *fHistEtaVsZvCoverage; //! Statistics on tracks Zv and Eta for all tracks
+  TH2D *fHistEtaVsZvCoverageAcepted; //!  Statistics on tracks Zv and Eta for acepted tracks
+
+  TH1D *fHistAceptedMult;   //! Number of acepted tracks histo
+
+
+
   AliAnalysisTaskLRC(const AliAnalysisTaskLRC&); // not implemented
   AliAnalysisTaskLRC& operator=(const AliAnalysisTaskLRC&); // not implemented
   
---
+++ PWG2/EBYE/LRC/AliLRCBase.cxx
@@ -0,0 +1,24 @@
+/**************************************************************************
+ * Author: Andrey Ivanov.                                                 *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+//-------------------------------------------------------------------------
+//    Description: 
+//    This class is abstact (interface) base class for different classes 
+//    used for track-by-track data analysis in AliAnalysisTaskLRC
+//    Origin: Andrey Ivanov (SPbSU-CERN) anivanov@cern.ch, 
+//    Igor Altsebeev (SPbSU-CERN) 
+//-------------------------------------------------------------------------
+
+#include "AliLRCBase.h"
+
+ClassImp(AliLRCBase)
+
