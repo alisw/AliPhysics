358dd1c45aaafd8b2b7bcd0036213f1ba7574f66
pchrist <pchrist@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-08-04 07:43:45 +0000

    Additions in the balance function code, including shuffling

--- PWG2/EBYE/macros/AddTaskBalanceCentralityTrain.C
+++ PWG2/EBYE/macros/AddTaskBalanceCentralityTrain.C
@@ -5,13 +5,35 @@ const char* centralityEstimator = "V0M";
 //_________________________________________________________//
 AliAnalysisTaskBF *AddTaskBalanceCentralityTrain(Double_t centrMin=0.,
 						 Double_t centrMax=100.,
+						 Double_t vertexZ=10.,
+						 Double_t DCAxy=2.4,
+						 Double_t DCAz=3.2,
+						 Double_t ptMin=0.3,
+						 Double_t ptMax=1.5,
+						 Double_t etaMin=-0.8,
+						 Double_t etaMax=0.8,
 						 TString fileNameBase="AnalysisResults") {
+
   // Creates a balance function analysis task and adds it to the analysis manager.
   // Get the pointer to the existing analysis manager via the static access method.
   TString centralityName("");
   centralityName+=Form("%.0f",centrMin);
   centralityName+="-";
   centralityName+=Form("%.0f",centrMax);
+  centralityName+="_";
+  centralityName+=Form("vZ%.1f",vertexZ);
+  centralityName+="_";
+  centralityName+=Form("DCAxy%.1f",DCAxy);
+  centralityName+="_";
+  centralityName+=Form("DCAz%.1f",DCAz);
+  centralityName+="_Pt";
+  centralityName+=Form("%.1f",ptMin);
+  centralityName+="-";
+  centralityName+=Form("%.1f",ptMax);
+  centralityName+="_Eta";
+  centralityName+=Form("%.1f",etaMin);
+  centralityName+="-";
+  centralityName+=Form("%.1f",etaMax);
 
   TString outputFileName(fileNameBase);
   outputFileName.Append(".root");
@@ -31,15 +53,26 @@ AliAnalysisTaskBF *AddTaskBalanceCentralityTrain(Double_t centrMin=0.,
   }
   TString analysisType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
   gROOT->LoadMacro("./configBalanceFunctionAnalysis.C");
-  AliBalance *bf = 0;
-  if (analysisType=="ESD") bf = GetBalanceFunctionObject("ESD");
-  else if (analysisType=="AOD") bf = GetBalanceFunctionObject("AOD");
-  else bf = GetBalanceFunctionObject("MC");
+  AliBalance *bf  = 0;  // Balance Function object
+  AliBalance *bfs = 0;  // shuffled Balance function object
+
+  if (analysisType=="ESD"){
+    bf  = GetBalanceFunctionObject("ESD");
+    bfs = GetBalanceFunctionObject("ESD");
+  }
+  else if (analysisType=="AOD"){
+    bf  = GetBalanceFunctionObject("AOD");
+    bfs = GetBalanceFunctionObject("AOD");
+  }
+  else{
+    bf  = GetBalanceFunctionObject("MC");
+    bfs = GetBalanceFunctionObject("MC");
+  }
 
   // Create the task, add it to manager and configure it.
   //===========================================================================
   AliAnalysisTaskBF *taskBF = new AliAnalysisTaskBF("TaskBF");
-  taskBF->SetAnalysisObject(bf);
+  taskBF->SetAnalysisObject(bf,bfs);
   taskBF->SetCentralityPercentileRange(centrMin,centrMax);
   if(analysisType == "ESD") {
     AliESDtrackCuts *trackCuts = GetTrackCutsObject();
@@ -48,18 +81,20 @@ AliAnalysisTaskBF *AddTaskBalanceCentralityTrain(Double_t centrMin=0.,
   }
   else if(analysisType == "AOD") {
     // pt and eta cut (pt_min, pt_max, eta_min, eta_max)
-    taskBF->SetKinematicsCutsAOD(0.3,1.5,-0.8,0.8);
+    taskBF->SetKinematicsCutsAOD(ptMin,ptMax,etaMin,etaMax);
+    taskBF->SetExtraDCACutsAOD(DCAxy,DCAz);
   }
 
     // vertex cut (x,y,z)
-    taskBF->SetVertexDiamond(.3,.3,10.);
+    taskBF->SetVertexDiamond(.3,.3,vertexZ);
+
     // offline trigger selection (AliVEvent.h)
     // taskBF->UseOfflineTrigger(); // NOT usable 
     // with this only selected events are analyzed
     taskBF->SelectCollisionCandidates(AliVEvent::kMB);
 
-  //bf->PrintAnalysisSettings();
-  mgr->AddTask(taskBF);
+    //bf->PrintAnalysisSettings();
+    mgr->AddTask(taskBF);
 
   // Create ONLY the output containers for the data produced by the task.
   // Get and connect other common input/output containers via the manager as below
@@ -71,8 +106,8 @@ AliAnalysisTaskBF *AddTaskBalanceCentralityTrain(Double_t centrMin=0.,
   AliAnalysisDataContainer *coutBF = mgr->CreateContainer(Form("listBF_%s",centralityName.Data()), TList::Class(),AliAnalysisManager::kOutputContainer,outputFileName.Data());
   mgr->ConnectInput(taskBF, 0, mgr->GetCommonInputContainer());
   //mgr->ConnectOutput(taskBF, 1, coutBalance);
-  mgr->ConnectOutput(taskBF, 2, coutQA);
-  mgr->ConnectOutput(taskBF, 3, coutBF);
+  mgr->ConnectOutput(taskBF, 3, coutQA);
+  mgr->ConnectOutput(taskBF, 4, coutBF);
 
   return taskBF;
 }
