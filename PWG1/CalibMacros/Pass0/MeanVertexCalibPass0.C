82ccf50fcfc3a197d9ca7625d0d53464f5089881
dainese <dainese@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-08-22 11:18:48 +0000

    Update (Davide)

--- PWG1/CalibMacros/Pass0/MeanVertexCalibPass0.C
+++ PWG1/CalibMacros/Pass0/MeanVertexCalibPass0.C
@@ -9,7 +9,7 @@ LoadLibraries()
 
 //___________________________________________________________________________
 
-MakeOCDB(const Char_t *filename = "AliESDfriends_v1.root", const Char_t *dbString = "raw://alice/data/2011/OCDB/", Int_t runNB)
+MakeOCDB(const Char_t *filename = "AliESDfriends_v1.root", const Char_t *dbString = "raw://", Int_t runNB)
 {
   LoadLibraries();
   AliMeanVertexPreprocessorOffline meanVertexCalib;
