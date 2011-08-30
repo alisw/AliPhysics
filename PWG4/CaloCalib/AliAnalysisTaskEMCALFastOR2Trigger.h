eba42d2b3398253c717b8125861dcfe79bbd6a72
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-08-30 14:47:09 +0000

    coding conventions

--- PWG4/CaloCalib/AliAnalysisTaskEMCALFastOR2Trigger.h
+++ PWG4/CaloCalib/AliAnalysisTaskEMCALFastOR2Trigger.h
@@ -19,51 +19,47 @@ public:
   AliAnalysisTaskEMCALFastOR2Trigger();
   AliAnalysisTaskEMCALFastOR2Trigger(const char *name);
   virtual ~AliAnalysisTaskEMCALFastOR2Trigger();
-  
+
+public:  
   virtual void     UserCreateOutputObjects();
   virtual void     UserExec(Option_t *option);
   virtual void     Terminate(Option_t *);
   
-  void SetCheckDeadClusters(Bool_t c) {fCheckDeadClusters = c;}
-  Bool_t GetCheckDeadClusters() {return fCheckDeadClusters;}
+  Bool_t                  GetCheckDeadClusters()                  const { return fCheckDeadClusters;    }
+  AliCaloCalibPedestal*   GetPedestal()                           const { return fPedestal;             }
+  Int_t                   GetnCol()                               const { return fNcol;                 }
+  Int_t                   GetnRow()                               const { return fNrow;                 }
+  Int_t                   GetshiftCol()                           const { return fShiftCol;             }
+  Int_t                   GetshiftRow()                           const { return fShiftRow;             }
+  Int_t                   GetMinL0Time()                          const { return fMinL0Time;            }
+  Int_t                   GetMaxL0Time()                          const { return fMaxL0Time;            }
+  Bool_t                  GetTimeCutOn()                          const { return fTimeCutOn;            }
+  void                    SetCheckDeadClusters(Bool_t c)                { fCheckDeadClusters = c;       }
+  void                    SetPedestal(AliCaloCalibPedestal *pds)        { fPedestal = pds;              }
+  void                    SetnCol(Int_t n)                              { fNcol = n;                    }
+  void                    SetnRow(Int_t n)                              { fNrow = n;                    }
+  void                    SetshiftCol(Int_t n)                          { fShiftCol = n;                }
+  void                    SetshiftRow(Int_t n)                          { fShiftRow = n;                }
+  void                    SetTriggerClustersName(const TString &name)   { fTriggerClustersName = name;  }
+  void                    SetMinL0Time(Int_t t)                         { fMinL0Time = t;               }
+  void                    SetMaxL0Time(Int_t t)                         { fMaxL0Time = t;               }
+  void                    SetTimeCutOn(Bool_t yes)                      { fTimeCutOn = yes;             }
   
-  void SetPedestal(AliCaloCalibPedestal *pds) {fPedestal = pds;}
-  AliCaloCalibPedestal* GetPedestal() {return fPedestal;}
-  
-  void SetnCol(Int_t n) {nCol = n;}
-  Int_t GetnCol() {return nCol;}
-  void SetnRow(Int_t n) {nRow = n;}
-  Int_t GetnRow() {return nRow;}
-  void SetshiftCol(Int_t n) {shiftCol = n;}
-  Int_t GetshiftCol() {return shiftCol;}
-  void SetshiftRow(Int_t n) {shiftRow = n;}
-  Int_t GetshiftRow() {return shiftRow;}
-  
-  void SetTriggerClustersName(TString name) {fTriggerClustersName = name;}
-  
-  Int_t GetMinL0Time() {return fMinL0Time;}
-  void SetMinL0Time(Int_t t) {fMinL0Time = t;}
-  Int_t GetMaxL0Time() {return fMaxL0Time;}
-  void SetMaxL0Time(Int_t t) {fMaxL0Time = t;}
-  
-  Bool_t GetTimeCutOn() {return fTimeCutOn;}
-  
-  void SetTimeCutOn(Bool_t yes) {fTimeCutOn = yes;}
-  
+protected:
+  Int_t                 fNcol;                    // Fixed window number of cells in phi direction
+  Int_t                 fNrow;                    // Fixed window number of cells in eta direction
+  Int_t                 fShiftCol;                // Shifting number of cells in phi direction
+  Int_t                 fShiftRow;                // Shifting number of cells in eta direction
+  TString               fTriggerClustersName;     // Name of the TClonesObject that will contain the computed triggers
+  Bool_t                fTimeCutOn;               // Determines whether or not apply time cuts
+  Int_t                 fMinL0Time;               // Minimum L0 time cut
+  Int_t                 fMaxL0Time;               // Maximum L0 time cut
+  Bool_t                fCheckDeadClusters;       // Determines whether or not check for dead clusters
+  AliCaloCalibPedestal *fPedestal;                //!Pointer to an object containing information about dead clusters
+
 private:
-  Bool_t                fCheckDeadClusters;
-  Int_t                 nCol;
-  Int_t                 nRow;
-  Int_t                 shiftCol;
-  Int_t                 shiftRow;
-  AliCaloCalibPedestal *fPedestal;
-  TString               fTriggerClustersName;
-  Int_t                 fMinL0Time;
-  Int_t                 fMaxL0Time;
-  Bool_t                fTimeCutOn;
-  
-  AliAnalysisTaskEMCALFastOR2Trigger (const AliAnalysisTaskEMCALFastOR2Trigger&); // not implemented
-  AliAnalysisTaskEMCALFastOR2Trigger operator=(const AliAnalysisTaskEMCALFastOR2Trigger&); // not implemented
+  AliAnalysisTaskEMCALFastOR2Trigger (const AliAnalysisTaskEMCALFastOR2Trigger&);           // not implemented
+  AliAnalysisTaskEMCALFastOR2Trigger operator=(const AliAnalysisTaskEMCALFastOR2Trigger&);  // not implemented
   
   ClassDef(AliAnalysisTaskEMCALFastOR2Trigger, 1); 
 };
