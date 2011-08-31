6a53a5ef209ea588203d00bada30c6c55721d0c9
haavard <haavard@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-08-31 12:50:56 +0000

    Corrected UInt_t <-> Int_t conversion

--- TPC/AliTPCSensorTemp.cxx
+++ TPC/AliTPCSensorTemp.cxx
@@ -185,7 +185,7 @@ TClonesArray * AliTPCSensorTemp::ReadTree(TTree *tree,
   Int_t sector=0;
   char  type[100];
   char  side[100];
-  UInt_t num=0;
+  Int_t num=0;
   Int_t echa=0;
   //Double_t x=0;
   //Double_t y=0;
@@ -329,7 +329,7 @@ TClonesArray * AliTPCSensorTemp::ReadTree(TTree *tree,
       temp->SetZ(-240);
       }
 
-    if ( num < (sizeof(kTSz)/sizeof(kTSz[0]))) {
+    if ( num < (Int_t)(sizeof(kTSz)/sizeof(kTSz[0]))) {
       if(temp->GetType()==5 && temp->GetSide()==0) {
          temp->SetZ(kTSz[num]);
       }
