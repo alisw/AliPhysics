dfca21fbd1d5a3beaa386e83970d30e05207bf17
lbarnby <lbarnby@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-01 08:51:20 +0000

    Fitting macro suite now has options for anti-Lambda and also summed Lambda+anti-Lambda and some graphics cleaned up. Default bin-by-bin fit options which work for minbias K0 and summed L+anti-L

--- PWG2/SPECTRA/LambdaK0PbPb/runFit.sh
+++ PWG2/SPECTRA/LambdaK0PbPb/runFit.sh
@@ -1,14 +1,19 @@
 
-cent=04
-hist=2
-suffix=K0MC
+cent=00
+# Set hist equal to 0 for standard pt analysis, other values for QA histograms
+hist=0
+#suffix=K0MC
 #suffix=K0Real
 #suffix=LMC
-#suffix=LReal
-partID=0
+suffix=LReal
+# Set partID = 0 for K0s, 1 for Lambda, 2 for anti-Lambda
+# or 3 for special case of summed anti-Lambda + Lambda 
+partID=1
+# Set data equal to the subdirectory within output[or output10binsNew - check in run.sh] which contains root files from task
 #data=LHC10h_000139172_p2
 #data=LHC10h_pass2
-data=LHC11a10a_final
+#data=LHC11a10a_final
+data=Twiki23Aug
 out=results
 #out=resultsPt
 
@@ -24,4 +29,4 @@ mv Diag* ${outfold}
 mv Masses* ${outfold}
 
 echo "//////////////////////////////////////"
-echo "output directory is: " ${out fold}
+echo "output directory is: " ${outfold}
