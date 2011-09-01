baefcea45c7f1653b04fc55a25df0f55a185ea67
pulvir <pulvir@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-01 11:43:14 +0000

    Monitor: coverity fix (added a missing "break") QA: added separate counting of resonances in events with no vertex and vertex out of range

--- PWG2/RESONANCES/extra/AliAnalysisTaskResonanceQA.h
+++ PWG2/RESONANCES/extra/AliAnalysisTaskResonanceQA.h
@@ -32,6 +32,15 @@ public:
       // this must be last and counter
       kResonances
    };
+   
+   enum EEvtType {
+      kBadNoVertex,
+      kBadVertexOutZ,
+      kGoodEvent,
+      
+      // this must be last and counter
+      kEvtTypes
+   };
 
    AliAnalysisTaskResonanceQA(const char *name = "RsnQA");
    virtual ~AliAnalysisTaskResonanceQA() {}
@@ -44,37 +53,39 @@ public:
    void           SetPrimaryThr(Double_t d) {fPrimaryThr = d;}
    void           SetVz(Double_t vz)        {fVz = vz;}
    
+   const char*    EvtName  (Int_t type) const {return EvtName  ((EEvtType)type);}
    const char*    RsnName  (Int_t type) const {return RsnName  ((ERsn)type);}
    const char*    RsnSymbol(Int_t type) const {return RsnSymbol((ERsn)type);}
    Int_t          RsnPDG   (Int_t type) const {return RsnPDG   ((ERsn)type);}
+   const char*    EvtName  (EEvtType type) const;
    const char*    RsnName  (ERsn type)  const;
    const char*    RsnSymbol(ERsn type)  const;
    Int_t          RsnPDG   (ERsn type)  const;
 
 private:
 
-   AliESDpid::EStartTimeType_t fT0;  // T0 type for TOF computation
-   Double_t fPrimaryThr;             // maximum DCA for selecting primary particles w.r. to gen primary vertex
-   Double_t fVz;                     // maximum VZ for primary vertex (reconstructed)
+   AliESDpid::EStartTimeType_t fT0;       // T0 type for TOF computation
+   Double_t fPrimaryThr;                  // maximum DCA for selecting primary particles w.r. to gen primary vertex
+   Double_t fVz;                          // maximum VZ for primary vertex (reconstructed)
+                                         
+   TList *fOutputList;                    // list with output histograms
    
-   TList *fOutputList;               // list with output histograms
-   
-   TH1I  *fSelectedEvts;  // selected events
-   TH2F  *fdEdxTPC;       // TPC PID QA
-   TH2F  *fdEdxITS;       // ITS PID QA
-   TH2F  *fTOFpid;        // TOF PID QA
-   TH2F  *fDCAXYvsPtBeforeCuts;   // DCA QA r
-   TH2F  *fDCAZvsPtBeforeCuts;    // DCA QA z
-   TH2F  *fNClusterPtBeforeCuts;  // N cluster TPC
+   TH1I  *fSelectedEvts;                  // selected events
+   TH1F  *fEventVz;                       // Vz distribution of events
+   TH2F  *fdEdxTPC;                       // TPC PID QA
+   TH2F  *fdEdxITS;                       // ITS PID QA
+   TH2F  *fTOFpid;                        // TOF PID QA
+   TH2F  *fDCAXYvsPtBeforeCuts;           // DCA QA r
+   TH2F  *fDCAZvsPtBeforeCuts;            // DCA QA z
+   TH2F  *fNClusterPtBeforeCuts;          // N cluster TPC
    TH2F  *fNFindableClusterPtBeforeCuts;  // N findable TPC clusters
    TH2F  *fNCrossedRowsTPCPtBeforeCuts;   // crossed rows
-   TH2F  *fRsnYPt[2][kResonances];        // rapidity vs pt distribution of resonances
-   TH2F  *fRsnYPtCINT1B[2][kResonances];  // number of resonances in selected events
+   TH3F  *fRsnYPt[2][kResonances];        // rapidity vs pt vs event type distribution of resonances
    TH1I  *fProducedParticles;             // synoptic of all resonances
       
-   AliESDEvent      *fESD;          //! temporary object (event)
-   AliESDpid        *fESDpid;       //  temporary object (PID)
-   AliESDtrackCuts  *fTrackCuts;    //  temporary object (quality track cuts)
+   AliESDEvent      *fESD;                //! temporary object (event)
+   AliESDpid        *fESDpid;             //  temporary object (PID)
+   AliESDtrackCuts  *fTrackCuts;          //  temporary object (quality track cuts)
    
    AliAnalysisTaskResonanceQA(const AliAnalysisTaskResonanceQA&);                // disabled
    AliAnalysisTaskResonanceQA& operator=(const AliAnalysisTaskResonanceQA&);     // disabled
