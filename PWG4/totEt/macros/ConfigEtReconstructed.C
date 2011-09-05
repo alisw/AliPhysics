98f1c3583f97dfb87a2bbfce9206d9ca1a446eb4
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-05 15:46:40 +0000

    Configuring AliAnalysisEmEtMonteCarlo and AliAnalysisEmEtReconstructed with config files instead of manually

--- PWG4/totEt/macros/ConfigEtReconstructed.C
+++ PWG4/totEt/macros/ConfigEtReconstructed.C
@@ -1,35 +1,38 @@
 
-AliAnalysisEtReconstructed * ConfigEtReconstructed(Bool_t isEmcal = true){
-  Bool_t EMCAL = isEmcal;
+AliAnalysisEtReconstructed * ConfigEtReconstructed(Bool_t EMCAL = true, Bool_t DETAIL = false){
+  gInterpreter->GenerateDictionary("std::map<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;map")  ;
+  gInterpreter->GenerateDictionary("std::pair<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;utility");
+  //Bool_t EMCAL = true;
+  TFile *infile = new TFile("corrections.root");
+  corrections = (AliAnalysisHadEtCorrections *)infile->Get("hadCorrectionEMCAL");
+  cout<<"Setting the AliAnalysisHadEtCorrections to "<<corrections->GetName()<<endl;
+  cout<<"eta cut is "<<corrections->GetEtaCut()<<endl;
+
   if(EMCAL){
-    AliAnalysisEtReconstructedEmcal *totEtReco = new AliAnalysisEtReconstructedEmcal();
-    totEtReco->SetDataSet(2010);
-    //Set corrections...
-    
-    TFile *infile = new TFile("corrections.root");
-    corrections = (AliAnalysisHadEtCorrections *)infile->Get("hadCorrectionEMCAL");
-    cout<<"Setting the AliAnalysisHadEtCorrections to "<<corrections->GetName()<<endl;
-    cout<<"eta cut is "<<corrections->GetEtaCut()<<endl;
-    totEtReco->SetCorrections(corrections);
-    
-    totEtReco->Init();
-    return totEtReco;
+    if(DETAIL){
+      AliAnalysisEmEtReconstructed *totEtReco = new AliAnalysisEmEtReconstructed();
+      totEtReco->SetDataSet(2010);
+      //Set corrections...
+      totEtReco->SetCorrections(corrections);    
+      totEtReco->Init();
+      return totEtReco;
+    }
+    else{
+      AliAnalysisEtReconstructedEmcal *totEtReco = new AliAnalysisEtReconstructedEmcal();
+      totEtReco->SetDataSet(2010);
+      //Set corrections...
+      totEtReco->SetCorrections(corrections);    
+      totEtReco->Init();
+      return totEtReco;
+    }
   }
   else{
     AliAnalysisEtReconstructedPhos *totEtReco = new AliAnalysisEtReconstructedPhos();
     totEtReco->SetDataSet(2010);
     //Set corrections...
-    
-    TFile *infile = new TFile("corrections.root");
-    corrections = (AliAnalysisHadEtCorrections *)infile->Get("hadCorrectionPHOS");
-    cout<<"Setting the AliAnalysisHadEtCorrections to "<<corrections->GetName()<<endl;
-    cout<<"eta cut is "<<corrections->GetEtaCut()<<endl;
-    totEtReco->SetCorrections(corrections);
-    
+    totEtReco->SetCorrections(corrections);    
     totEtReco->Init();
     return totEtReco;
   }
-  gInterpreter->GenerateDictionary("std::map<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;map")  ;
-  gInterpreter->GenerateDictionary("std::pair<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;utility");
 }
 
