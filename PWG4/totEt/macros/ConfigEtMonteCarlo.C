92eebc3263d0cab2ac638e0d2b0407ab5e470ed8
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-05 15:46:40 +0000

    Configuring AliAnalysisEmEtMonteCarlo and AliAnalysisEmEtReconstructed with config files instead of manually

--- PWG4/totEt/macros/ConfigEtMonteCarlo.C
+++ PWG4/totEt/macros/ConfigEtMonteCarlo.C
@@ -1,19 +1,26 @@
 
-AliAnalysisEtMonteCarlo * ConfigEtMonteCarlo(Bool_t isEmcal = true){
-  Bool_t EMCAL = isEmcal;
+AliAnalysisEtMonteCarlo * ConfigEtMonteCarlo(Bool_t EMCAL = true, Bool_t DETAIL = false){
+  //Bool_t EMCAL = true;
   if(EMCAL){
-    AliAnalysisEtMonteCarloEmcal *totEtMC = new AliAnalysisEtMonteCarloEmcal();
-    //Look at the 2010 p+p data...
-    totEtMC->SetDataSet(2010);
-    
-    totEtMC->Init();
-    return totEtMC;
+    if(DETAIL){
+      AliAnalysisEmEtMonteCarlo *totEtMC = new AliAnalysisEmEtMonteCarlo();
+      //Look at the 2010 p+p data...
+      totEtMC->SetDataSet(2010);
+      totEtMC->Init();
+      return totEtMC;
+    }
+    else{
+      AliAnalysisEtMonteCarloEmcal *totEtMC = new AliAnalysisEtMonteCarloEmcal();
+      //Look at the 2010 p+p data...
+      totEtMC->SetDataSet(2010);
+      totEtMC->Init();
+      return totEtMC;
+    }
   }
   else{
     AliAnalysisEtMonteCarloPhos *totEtMC = new AliAnalysisEtMonteCarloPhos();
     //Look at the 2010 p+p data...
     totEtMC->SetDataSet(2010);
-    
     totEtMC->Init();
     return totEtMC;
   }
