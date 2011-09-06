2d3f9cf95f3e154bc97c9c4f41d37e85beb8f104
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-06 11:42:06 +0000

    Running EMCal Tender supply before EMCal analysis

--- PWG4/totEt/macros/runCaloEt.C
+++ PWG4/totEt/macros/runCaloEt.C
@@ -5,9 +5,9 @@
 //With the argument true this submits jobs to the grid
 //As written this requires an xml script tag.xml in the ~/et directory on the grid to submit jobs
 void runCaloEt(bool submit = false, // true or false 
-	       const char *dataType="sim", // "sim" or "real" etc.
-	       const char *pluginRunMode="full", // "test" or "full" or "terminate"
-	       const char *det = "EMCAL") // "PHOS" or "EMCAL"
+	       const char *dataType="real", // "sim" or "real" etc.
+	       const char *pluginRunMode="test", // "test" or "full" or "terminate"
+	       const char *det = "EMCalDetail") // "PHOS" or "EMCAL"
 {
   TStopwatch timer;
   timer.Start();
@@ -28,6 +28,8 @@ void runCaloEt(bool submit = false, // true or false
   gSystem->Load("libANALYSIS");
   gSystem->Load("libANALYSISalice");
   gSystem->Load("libCORRFW");
+
+
 
   if (!submit) { 
     cout << "local - no submitting" << endl;
@@ -53,9 +55,8 @@ gROOT->ProcessLine(".L AliAnalysisEmEtMonteCarlo.cxx+g");
 gROOT->ProcessLine(".L AliAnalysisEmEtReconstructed.cxx+g");
   gROOT->ProcessLine(".L AliAnalysisTaskTotEt.cxx+g");
 
-   gInterpreter->GenerateDictionary("std::map<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;map")  ;
-   gInterpreter->GenerateDictionary("std::pair<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;utility");
-
+  gInterpreter->GenerateDictionary("std::map<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;map")  ;
+  gInterpreter->GenerateDictionary("std::pair<int, AliPhysicsSelection*>", "AliPhysicsSelection.h;utility");
 
   char *kTreeName = "esdTree" ;
   TChain * chain   = new TChain(kTreeName,"myESDTree") ;
@@ -104,6 +105,7 @@ gROOT->ProcessLine(".L AliAnalysisEmEtReconstructed.cxx+g");
       chain->Add(fileLocation.Data()); // link to local test file
     }
     else { // pp
+      cout<<"adding pp simulation file"<<endl;
       chain->Add("/data/LHC10d15/1821/AliESDs.root");
       //chain->Add("/data/LHC10dpass2/10000126403050.70/AliESDs.root");//data
       //chain->Add("/home/dsilverm/data/E_T/sim/LHC10d1/117222/100/AliESDs.root"); // link to local test file
@@ -113,30 +115,54 @@ gROOT->ProcessLine(".L AliAnalysisEmEtReconstructed.cxx+g");
   }
   else { // real data
     isMc = kFALSE;
-    chain->Add("/home/dsilverm/data/E_T/data/2010/LHC10b/000117222/ESDs/pass2/10000117222021.30/AliESDs.root"); // link to local test file
+    chain->Add("/data/LHC10dpass2/10000126403050.70/AliESDs.root");//data
+    //chain->Add("/home/dsilverm/data/E_T/data/2010/LHC10b/000117222/ESDs/pass2/10000117222021.30/AliESDs.root"); // link to local test file
     cout << " not MC " << endl;
   }
 
+
+  if(!isMc && detStr.Contains("EMC")){
+    cout<<"You are running over EMCal data and using the tender supply"<<endl;
+    gSystem->Load("libTENDER.so");
+    gSystem->Load("libTENDERSupplies.so"); 
+    gROOT->ProcessLine(".include $ALICE_ROOT/Tender/"); 
+    gSystem->AddIncludePath("-I$ALICE_ROOT/ANALYSIS "); 
+    //Tender Supplies
+    gROOT->LoadMacro("CreateEMCALTender.C");
+    AliAnalysisTaskSE *tender = CreateEMCALTender(kTRUE);
+    mgr->AddTask(tender); 
+  }
+
+  if(isMc) cout<<"I am a MC"<<endl;
   gROOT->ProcessLine(".L $ALICE_ROOT/ANALYSIS/macros/AddTaskPhysicsSelection.C");
   
   AliPhysicsSelectionTask *physicsSelectionTask = AddTaskPhysicsSelection(isMc);//isMC is true when processing monte carlo
-
   if(isPb){	 
+    cout<<"Adding centrality selection task"<<endl;
     gROOT->ProcessLine(".L $ALICE_ROOT/ANALYSIS/macros/AddTaskCentrality.C");
     gROOT->ProcessLine(".L AliCentralitySelectionTask.cxx++g");
     AliCentralitySelectionTask *centTask = AddTaskCentrality();
   }
 
+
+
+  AliAnalysisDataContainer *cinput1 = mgr->GetCommonInputContainer();
+
+
+
+
+
   AliAnalysisTaskTotEt *task1 = new AliAnalysisTaskTotEt(taskName);
   task1->SetMcData(isMc);//necessary to tell the task to basically accept all MC events.
   mgr->AddTask(task1);
 
-  AliAnalysisDataContainer *cinput1 = mgr->GetCommonInputContainer();
   AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("out1", TList::Class(), AliAnalysisManager::kOutputContainer, outputName);
   
   //____________________________________________//
   mgr->ConnectInput(task1,0,cinput1);
   mgr->ConnectOutput(task1,1,coutput1);
+
+
   
   mgr->SetDebugLevel(0);
   
