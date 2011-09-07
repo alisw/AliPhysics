be9e26a59c9dad695bcb53502546ea1aa1a2f9ee
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-07 15:18:35 +0000

    Add Psel macro

---
+++ PWG4/UserTasks/EmcalTasks/macros/AddEmcalPhysicsSelection.C
@@ -0,0 +1,37 @@
+// $Id$
+
+void AddEmcalPhysicsSelelection(Bool_t exFOnly, Bool_t rejectBG=kTRUE, Bool_t computeBG=kTRUE)
+{
+  // Add EMCAL physics selection task.
+
+  //get the current analysis manager  
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTask1PhysSel", "No analysis manager found.");
+    return 0;
+  }
+  isMC = (mgr->GetMCtruthEventHandler()) ? kTRUE:kFALSE; 
+
+  AliEmcalPhysicsSelectionTask *pseltask = new AliEmcalPhysicsSelectionTask("PhysSel");
+  //pseltask->SetDoWriteHistos(kFALSE);
+  AliEmcalPhysicsSelection *physSel = pseltask->GetPhysicsSelection();
+  physSel->SetExcludeFastOnly(exFOnly);
+  if (rejectBG) 
+    physSel->AddBackgroundIdentification(new AliBackgroundSelection());
+  if (computeBG)
+    physSel->SetComputeBG(computeBG);
+  if (isMC)      
+    physSel->SetAnalyzeMC();
+  mgr->AddTask(pseltask);
+
+  AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+  mgr->ConnectInput(pseltask, 0, cinput);
+  TString oname("EventStat.root");
+  AliAnalysisDataContainer *co1 = 
+    mgr->CreateContainer("PhysSel",
+                         TList::Class(),
+                         AliAnalysisManager::kOutputContainer,
+                         oname);
+  mgr->ConnectOutput(pseltask,1,co1);
+  cout << " *** AliEmcalPhysicsTask configured *** " << endl;
+}   
