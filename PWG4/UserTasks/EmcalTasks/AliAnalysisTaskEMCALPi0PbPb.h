9388f9143f648e785f1e6b29464c438ff6a225fd
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-09 07:55:37 +0000

    more cells and v0and

--- PWG4/UserTasks/EmcalTasks/AliAnalysisTaskEMCALPi0PbPb.h
+++ PWG4/UserTasks/EmcalTasks/AliAnalysisTaskEMCALPi0PbPb.h
@@ -217,11 +217,12 @@ class AliStaHeader
  public:
   AliStaHeader() : fRun(0), fOrbit(0), fPeriod(0), fBx(0), fL0(0), fL1(0), fL2(0),
                    fTrClassMask(0), fTrCluster(0), fOffTriggers(0), fFiredTriggers(),
-                   fTcls(0), fV0Cent(0), fV0(0), fCl1Cent(0), fCl1(0), fTrCent(0), fTr(0),
+                   fTcls(0), fV0And(0), fV0Cent(0), fV0(0), fCl1Cent(0), fCl1(0), fTrCent(0), fTr(0),
                    fCqual(-1), fPsi(0), fPsiRes(0), fNSelTr(0), fNSelPrimTr(0), fNSelPrimTr1(0),
-                   fNSelPrimTr2(0), fNCells(0), fNCells1(0), fNCells2(0), fNCells5(0), 
-                   fNClus(0), fNClus1(0), fNClus2(0), fNClus5(0), 
+                   fNSelPrimTr2(0), fNCells(0), fNCells0(0), fNCells01(0), fNCells03(0), 
+                   fNCells1(0), fNCells2(0), fNCells5(0), fNClus(0), fNClus1(0), fNClus2(0), fNClus5(0), 
                    fMaxCellE(0), fMaxClusE(0) {;}
+
   ULong64_t     GetEventId() const {
                   return (((ULong64_t)fPeriod << 36) |
                           ((ULong64_t)fOrbit  << 12) |
@@ -242,6 +243,7 @@ class AliStaHeader
   UInt_t        fOffTriggers;    //         fired offline triggers for this event
   TString       fFiredTriggers;  //         string with fired triggers
   UInt_t        fTcls;           //         custom trigger definition
+  Bool_t        fV0And;          //         V0AND (from AliTriggerAnalysis)
   Double32_t    fV0Cent;         //[0,0,16] v0 cent
   Double32_t    fV0;             //[0,0,16] v0 result used for cent 
   Double32_t    fCl1Cent;        //[0,0,16] cl1 cent
@@ -256,9 +258,12 @@ class AliStaHeader
   UShort_t      fNSelPrimTr1;    //         # selected tracks (primary) pt > 1 GeV/c
   UShort_t      fNSelPrimTr2;    //         # selected tracks (primary) pt > 2 GeV/c
   UShort_t      fNCells;         //         # cells
-  UShort_t      fNCells1;        //         # cells > 1 GeV
-  UShort_t      fNCells2;        //         # cells > 2 GeV
-  UShort_t      fNCells5;        //         # cells > 5 GeV
+  UShort_t      fNCells0;        //         # cells > 0.45 GeV
+  UShort_t      fNCells01;       //         # cells > 0.1  GeV
+  UShort_t      fNCells03;       //         # cells > 0.3  GeV
+  UShort_t      fNCells1;        //         # cells > 1    GeV
+  UShort_t      fNCells2;        //         # cells > 2    GeV
+  UShort_t      fNCells5;        //         # cells > 5    GeV
   UShort_t      fNClus;          //         # clus
   UShort_t      fNClus1;         //         # clus > 1 GeV
   UShort_t      fNClus2;         //         # clus > 2 GeV
@@ -266,7 +271,7 @@ class AliStaHeader
   Double32_t    fMaxCellE;       //[0,0,16] maximum cell energy
   Double32_t    fMaxClusE;       //[0,0,16] maximum clus energy
 
-  ClassDef(AliStaHeader,3) // Header class
+  ClassDef(AliStaHeader,4) // Header class
 };
 
 class AliStaVertex
