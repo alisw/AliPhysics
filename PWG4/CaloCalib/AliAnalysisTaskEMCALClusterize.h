496578109c625854c446faeaa61e610d51459fcd
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-11 13:55:01 +0000

    added posibility to change cuts to remove cells with too low energy after unfolding

--- PWG4/CaloCalib/AliAnalysisTaskEMCALClusterize.h
+++ PWG4/CaloCalib/AliAnalysisTaskEMCALClusterize.h
@@ -39,9 +39,9 @@ class AliAnalysisTaskEMCALClusterize : public AliAnalysisTaskSE {
   
   //OCDB
   Bool_t         AccessOCDB();
-  void           SwitchOnAccessOCDB()                           { fAccessOCDB = kTRUE          ; }
-  void           SwitchOffAccessOCDB()                          { fAccessOCDB = kFALSE         ; } 
-  void           SetOCDBPath(const char *path)                  { fOCDBpath = path             ; }
+  void           SwitchOnAccessOCDB()                           { fAccessOCDB       = kTRUE    ; }
+  void           SwitchOffAccessOCDB()                          { fAccessOCDB       = kFALSE   ; } 
+  void           SetOCDBPath(const char *path)                  { fOCDBpath         = path     ; }
   
   //Geometry methods
   void           SetGeometryName(TString &name)                 { fGeomName = name             ; }
@@ -55,28 +55,30 @@ class AliAnalysisTaskEMCALClusterize : public AliAnalysisTaskSE {
   void           FillAODFile(Bool_t yesno)                      { fFillAODFile         = yesno ; }
   void           FillAODCaloCells();
   void           FillAODHeader();
-  void           SwitchOnFillAODHeader()                        { fFillAODHeader    = kTRUE    ; }
-  void           SwitchOffFillAODHeader()                       { fFillAODHeader    = kFALSE   ; } 
-  void           SwitchOnFillAODCaloCells()                     { fFillAODCaloCells = kTRUE    ; }
-  void           SwitchOffFillAODCaloCells()                    { fFillAODCaloCells = kFALSE   ; } 
+  void           SwitchOnFillAODHeader()                        { fFillAODHeader     = kTRUE   ; }
+  void           SwitchOffFillAODHeader()                       { fFillAODHeader     = kFALSE  ; } 
+  void           SwitchOnFillAODCaloCells()                     { fFillAODCaloCells  = kTRUE   ; }
+  void           SwitchOffFillAODCaloCells()                    { fFillAODCaloCells  = kFALSE  ; } 
   
   //Algorithms settings
-  void           JustUnfold(Bool_t yesno)                       { fJustUnfold          = yesno ; }
+  void           JustUnfold(Bool_t yesno)                       { fJustUnfold        = yesno   ; }
   AliEMCALRecParam * GetRecParam()                       const  { return fRecParam             ; }
   void           InitClusterization();
   
-  void           SetEMCALRecoUtils(AliEMCALRecoUtils * ru)      { fRecoUtils           = ru    ; }
+  void           SetEMCALRecoUtils(AliEMCALRecoUtils * ru)      { fRecoUtils         = ru      ; }
   AliEMCALRecoUtils* GetRecoUtils()                      const  { return fRecoUtils            ; }
   
-  void           SetConfigFileName(TString name)                { fConfigName          = name  ; }
-  void           SetMaxEvent(Int_t max)                         { fMaxEvent            = max   ; }
+  void           SetConfigFileName(TString name)                { fConfigName        = name    ; }
+  void           SetMaxEvent(Int_t max)                         { fMaxEvent          = max     ; }
   
-  void           SwitchOnTrackMatching()                        { fDoTrackMatching    = kTRUE  ; }
-  void           SwitchOffTrackMatching()                       { fDoTrackMatching    = kFALSE ; } 
+  void           SwitchOnTrackMatching()                        { fDoTrackMatching   = kTRUE   ; }
+  void           SwitchOffTrackMatching()                       { fDoTrackMatching   = kFALSE  ; } 
 
-  void           SwitchOnCellEnergySelection()                  { fSelectCell         = kTRUE  ; }
-  void           SwitchOffCellEnergySelection()                 { fSelectCell         = kFALSE ; } 
-
+  // Cell selection after unfolding
+  void           SwitchOnCellEnergySelection()                  { fSelectCell        = kTRUE   ; }
+  void           SwitchOffCellEnergySelection()                 { fSelectCell        = kFALSE  ; } 
+  void           SetCellCuts(Float_t e, Float_t frac)           { fSelectCellMinE    = e       ; 
+                                                                  fSelectCellMinFrac = frac    ; }
   
  private:
     
@@ -107,28 +109,31 @@ class AliAnalysisTaskEMCALClusterize : public AliAnalysisTaskSE {
   Bool_t                 fJustUnfold;       // Just unfold, do not recluster
   
   //AOD
-  TClonesArray          *fOutputAODBranch;  //! AOD Branch with output clusters  
-  TString                fOutputAODBranchName;  // New of output AOD branch
-  Bool_t                 fFillAODFile;      // Fill the output AOD file with the new clusters, 
-                                            // if not they will be only available for the event they were generated
-  Bool_t                 fFillAODHeader;    // Copy header to standard branch
-  Bool_t                 fFillAODCaloCells; // Copy calocells to standard branch
+  TClonesArray          *fOutputAODBranch;     //! AOD Branch with output clusters  
+  TString                fOutputAODBranchName; // New of output AOD branch
+  Bool_t                 fFillAODFile;         // Fill the output AOD file with the new clusters, 
+                                               // if not they will be only available for the event they were generated
+  Bool_t                 fFillAODHeader;       // Copy header to standard branch
+  Bool_t                 fFillAODCaloCells;    // Copy calocells to standard branch
 
-  Int_t                  fRun;              //!run number
+  Int_t                  fRun;                 //!run number
   
-  AliEMCALRecoUtils*     fRecoUtils;        // Access to factorized reconstruction algorithms
-  TString                fConfigName;       // Name of analysis configuration file
+  AliEMCALRecoUtils*     fRecoUtils;           // Access to factorized reconstruction algorithms
+  TString                fConfigName;          // Name of analysis configuration file
   
   Int_t                  fCellLabels[12672];       // Array with MC label to be passed to digit. 
   Int_t                  fCellSecondLabels[12672]; // Array with Second MC label to be passed to digit. 
   Double_t               fCellTime[12672];         // Array with cluster time to be passed to digit in case of AODs 
 
-  Int_t                  fMaxEvent;         // Set a maximum event
+  Int_t                  fMaxEvent;            // Set a maximum event
   
-  Bool_t                 fDoTrackMatching;  // On/Off the matching recalulation to speed up analysis in PbPb
-  Bool_t                 fSelectCell;       // Reject cells from cluster if energy is too low and recalculate position/energy and other
+  Bool_t                 fDoTrackMatching;     // On/Off the matching recalulation to speed up analysis in PbPb
+  Bool_t                 fSelectCell;          // Reject cells from cluster if energy is too low and recalculate position/energy and other
+  Float_t                fSelectCellMinE;      // Min energy cell threshold, after unfolding
+  Float_t                fSelectCellMinFrac;   // Min fraction of cell energy after unfolding cut
   
-  ClassDef(AliAnalysisTaskEMCALClusterize, 10);
+  ClassDef(AliAnalysisTaskEMCALClusterize, 11);
+
 };
 
 #endif //ALIANALYSISTASKEMCALCLUSTERIZE_H
