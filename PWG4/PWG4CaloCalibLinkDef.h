f204c67db3f34a963a65e781ab797588102fa65c
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-11 19:43:14 +0000

    remove old and unused code

--- PWG4/PWG4CaloCalibLinkDef.h
+++ PWG4/PWG4CaloCalibLinkDef.h
@@ -5,7 +5,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class AliAnalysisTaskCaloFilter+;
-#pragma link C++ class AliAnalysisTaskPHOSPi0CalibSelection+;
 #pragma link C++ class AliAnalysisTaskEMCALPi0CalibSelection+;
 #pragma link C++ class AliAnalysisTaskEMCALClusterize+;
 
