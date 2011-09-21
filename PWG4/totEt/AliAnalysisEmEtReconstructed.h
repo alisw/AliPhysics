c1467e6fedf58350903e232a8e3f41e9ad53d133
cnattras <cnattras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-09-21 11:37:47 +0000

    Adding more useful comments by Marcelo

--- PWG4/totEt/AliAnalysisEmEtReconstructed.h
+++ PWG4/totEt/AliAnalysisEmEtReconstructed.h
@@ -44,164 +44,93 @@ protected:
 
 protected:
 
-	Double_t fResCut;//Marcelo please add comment
+	Double_t fResCut;//track matching residual cut
 	
-	Double_t fAllRectotETDep;//Marcelo please add comment
-	Double_t fElectronMatchtotETDep;//Marcelo please add comment
-	Double_t fNeutralRectotET;//Marcelo please add comment
-	Double_t fTotEMRectotET;//Marcelo please add comment
-	Double_t fMuonMatchtotETDep, fPionMatchtotETDep, fKaonMatchtotETDep, fProtonMatchtotETDep;//Marcelo please add comment
-	Double_t fTotChargedMatchtotETDep;//Marcelo please add comment
-	Double_t fTotalRectotETDep;//Marcelo please add comment
+	Double_t fAllRectotETDep;//total ET deposited - all particles
+	Double_t fElectronMatchtotETDep;// total Et deposited - matched electrons
+	Double_t fNeutralRectotET;// total ET - neutral particles
+	Double_t fTotEMRectotET;// total electromagnetic ET
+	Double_t fMuonMatchtotETDep, fPionMatchtotETDep, fKaonMatchtotETDep, fProtonMatchtotETDep;// total ET deposited - hadrons
+	Double_t fTotChargedMatchtotETDep;// total Et deposited - matched chagred particles
+	Double_t fTotalRectotETDep;// total ET deposited
 	
-	AliESDEvent *fESD;//Marcelo please add comment
-	AliEMCALGeometry *fGeoUt;//Marcelo please add comment
+	AliESDEvent *fESD;// ESD object
+	AliEMCALGeometry *fGeoUt;// EMCal geometry object
 
 	// *******************
 	// all ET
 	// *******************
-	//TH2F *fHistAllRecEtaEDepETDep;//Marcelo please add comment 
-	//TH2F *fHistAllRecEtaETDep;//Marcelo please add comment 
-	
-	THnSparseD* fHistAllRecETDep;//Marcelo please add comment
-	THnSparseD* fHistAllRec;//Marcelo please add comment
-	TH1F *fHistAllRectotETDep;//Marcelo please add comment
+	THnSparseD* fHistAllRecETDep;// ET deposited - all particles
+	THnSparseD* fHistAllRec;// mutliplicity - all particles
+	TH1F *fHistAllRectotETDep;// total ET deposited - all particles
 	
 	// *******************
 	// electron ET reconstructed in EMCal
 	// *******************
-	/*
-	TH2F *fHistElectronMatchEtaEDepETDep;//Marcelo please add comment 
-	TH2F *fHistElectronMatchEtaPtETDep;//Marcelo please add comment 
-	TH2F *fHistElectronMatchEtaETDep;//Marcelo please add comment 
-	TH2F *fHistElectronMatchEtaPt;//Marcelo please add comment 
+	THnSparseD* fHistElectronRecETDep;// Et deposited - matched electrons
+	THnSparseD* fHistElectronRec;// multiplicity - matched electrons
+	TH1F *fHistElectronMatchtotETDep;// total Et deposited - matched electrons
 	
-	TH2F *fHistElectronRec_ResEDep_ETDep;//Marcelo please add comment 
-	TH2F *fHistElectronRec_ResPt_ETDep;//Marcelo please add comment 
-	TH2F *fHistElectronRec_ResEDep;//Marcelo please add comment 
-	TH2F *fHistElectronRec_ResPt;//Marcelo please add comment 
-	*/
-	
-	THnSparseD* fHistElectronRecETDep;//Marcelo please add comment
-	THnSparseD* fHistElectronRec;//Marcelo please add comment
-	TH1F *fHistElectronMatchtotETDep;//Marcelo please add comment 
-	
-	TH2F *fHistElectronRecdEdxP;//Marcelo please add comment
+	TH2F *fHistElectronRecdEdxP;// electron dEdx vs p
 
 	// *******************
 	// Neutral ET reconstructed in EMCal
 	// *******************
-	/*
-	TH2F *fHistNeutralRec_EtaE_ET;//Marcelo please add comment  
-	TH2F *fHistNeutralRec_EtaPt_ET;//Marcelo please add comment  
-	TH2F *fHistNeutralRec_EtaET;//Marcelo please add comment  
-	TH2F *fHistNeutralRec_EtaE;//Marcelo please add comment  
-	TH2F *fHistNeutralRec_EtaPt;//Marcelo please add comment  
-	*/
-	
-	TH1F *fHistNeutralRectotET;//Marcelo please add comment  
+	TH1F *fHistNeutralRectotET;// total ET - neutral particles 
 
 	// *******************
 	// total EM ET reconstructed in EMCal
 	// *******************
-	TH1F *fHistTotEMRectotET;//Marcelo please add comment
+	TH1F *fHistTotEMRectotET;// total electromagnetic ET
 
 	// *******************
 	// muon ET (+ and -)
 	// *******************
-	/*
-	TH2F *fHistMuonMatchEtaEDepETDep;//Marcelo please add comment 
-	TH2F *fHistMuonMatchEtaPtETDep;//Marcelo please add comment 
-	TH2F *fHistMuonMatchEtaETDep;//Marcelo please add comment 
-	TH2F *fHistMuonMatchEtaPt;//Marcelo please add comment 
-	
-	TH2F *fHistMuonRecResEDepETDep;//Marcelo please add comment 
-	TH2F *fHistMuonRecResPtETDep;//Marcelo please add comment 
-	TH2F *fHistMuonRecResEDep;//Marcelo please add comment 
-	TH2F *fHistMuonRecResPt;//Marcelo please add comment 
-	*/
-	
-	THnSparseD* fHistMuonRecETDep;//Marcelo please add comment
-	THnSparseD* fHistMuonRec;//Marcelo please add comment
-	TH1F *fHistMuonMatchtotETDep;//Marcelo please add comment 
+	THnSparseD* fHistMuonRecETDep;// Et deposited
+	THnSparseD* fHistMuonRec;// multiplicity
+	TH1F *fHistMuonMatchtotETDep;// total Et deposited
 
-	TH2F *fHistMuonRecdEdxP;//Marcelo please add comment
+	TH2F *fHistMuonRecdEdxP;// dEdx vs p
 	
 	// *******************
 	// pion ET (+ and -)
 	// *******************
-	/*
-	TH2F *fHistPionMatchEtaEDepETDep;//Marcelo please add comment 
-	TH2F *fHistPionMatchEtaPtETDep;//Marcelo please add comment 
-	TH2F *fHistPionMatchEtaETDep;//Marcelo please add comment 
-	TH2F *fHistPionMatchEtaPt;//Marcelo please add comment 
-	
-	TH2F *fHistPionRecResEDepETDep;//Marcelo please add comment 
-	TH2F *fHistPionRecResPtETDep;//Marcelo please add comment 
-	TH2F *fHistPionRecResEDep;//Marcelo please add comment 
-	TH2F *fHistPionRecResPt;//Marcelo please add comment 
-	*/
-	
-	THnSparseD* fHistPionRecETDep;//Marcelo please add comment
-	THnSparseD* fHistPionRec;//Marcelo please add comment
-	TH1F *fHistPionMatchtotETDep;//Marcelo please add comment 
+	THnSparseD* fHistPionRecETDep;// Et deposited 
+	THnSparseD* fHistPionRec;// multiplicity
+	TH1F *fHistPionMatchtotETDep;// total Et deposited
 
-	TH2F *fHistPionRecdEdxP;//Marcelo please add comment
+	TH2F *fHistPionRecdEdxP;// dEdx vs p
 
 	// *******************
 	// charged kaon (+ and -) ET
 	// *******************
-	/*
-	TH2F *fHistKaonMatchEtaEDepETDep;//Marcelo please add comment 
-	TH2F *fHistKaonMatchEtaPtETDep;//Marcelo please add comment 
-	TH2F *fHistKaonMatchEtaETDep;//Marcelo please add comment 
-	TH2F *fHistKaonMatchEtaPt;//Marcelo please add comment 
-	
-	TH2F *fHistKaonRecResEDepETDep;//Marcelo please add comment 
-	TH2F *fHistKaonRecResPtETDep;//Marcelo please add comment 	
-	TH2F *fHistKaonRecResEDep;//Marcelo please add comment 
-	TH2F *fHistKaonRecResPt;//Marcelo please add comment 
-	*/
-	
-	THnSparseD* fHistKaonRecETDep;//Marcelo please add comment
-	THnSparseD* fHistKaonRec;//Marcelo please add comment
-	TH1F *fHistKaonMatchtotETDep;//Marcelo please add comment 
+	THnSparseD* fHistKaonRecETDep;// Et deposited
+	THnSparseD* fHistKaonRec;// multiplicity
+	TH1F *fHistKaonMatchtotETDep;// total Et deposited
 
-	TH2F *fHistKaonRecdEdxP;//Marcelo please add comment
+	TH2F *fHistKaonRecdEdxP;// dEdx vs p
 	
 	// *******************
 	// proton (anti) ET
 	// *******************
-	/*
-	TH2F *fHistProtonMatchEtaEDepETDep;//Marcelo please add comment 
-	TH2F *fHistProtonMatchEtaPtETDep;//Marcelo please add comment 
-	TH2F *fHistProtonMatchEtaETDep;//Marcelo please add comment 
-	TH2F *fHistProtonMatchEtaPt;//Marcelo please add comment 
-	
-	TH2F *fHistProtonRecResEDepETDep;//Marcelo please add comment 
-	TH2F *fHistProtonRecResPtETDep;//Marcelo please add comment 
-	TH2F *fHistProtonRecResEDep;//Marcelo please add comment 
-	TH2F *fHistProtonRecResPt;//Marcelo please add comment 
-	*/
-	
-	THnSparseD* fHistProtonRecETDep;//Marcelo please add comment
-	THnSparseD* fHistProtonRec;//Marcelo please add comment
-	TH1F *fHistProtonMatchtotETDep;//Marcelo please add comment 
+	THnSparseD* fHistProtonRecETDep;// Et deposited
+	THnSparseD* fHistProtonRec;// multiplicity
+	TH1F *fHistProtonMatchtotETDep;// total Et deposited
 
-	TH2F *fHistProtonRecdEdxP;//Marcelo please add comment
+	TH2F *fHistProtonRecdEdxP;// dEdx vs p
 	
 	// *******************
 	// total charged ET
 	// *******************
-	TH1F *fHistTotChargedMatchtotETDep;//Marcelo please add comment
+	TH1F *fHistTotChargedMatchtotETDep;// total Et deposited - all charged particles 
 	
 	// *******************
 	// total ET
 	// *******************
-	TH1F *fHistTotalRectotETDep;//Marcelo please add comment
+	TH1F *fHistTotalRectotETDep;// total Et deposited - all particles
 	
 	//few checks
-	TH2F *fHistDeltaRZ;//Marcelo please add comment
+	TH2F *fHistDeltaRZ;// track-cluster matching residual
 	
  private:
   //Declare it private to avoid compilation warning
