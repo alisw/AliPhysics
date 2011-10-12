f6c35c539d86b006d7d2c5bc7a3b8f0d11c0b4a4
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-10-12 08:28:00 +0000

    Compact TH1F fraction histograms into a single TH2F, coverity fix, cosmetics, add Terminate method

--- PWG4/UserTasks/EmcalTasks/AliAnalysisTaskEMCALTriggerQA.h
+++ PWG4/UserTasks/EmcalTasks/AliAnalysisTaskEMCALTriggerQA.h
@@ -37,10 +37,16 @@ public:
   
   void   SetGeometryName(TString name)  { fGeoName = name ; } 
   
-  void   SetTRUTotalSignalHistogramsRange(Int_t nbins,  Float_t max) { fNBinsTRUSignal = nbins; fMaxTRUSignal = max ; }
-  void   SetSTUTotalSignalHistogramsRange(Int_t nbins,  Float_t max) { fNBinsSTUSignal = nbins; fMaxSTUSignal = max ; }
-  void   SetV0TotalSignalHistogramsRange (Int_t nbins,  Float_t max) { fNBinsV0Signal  = nbins; fMaxV0Signal  = max ; }
+  void   Terminate(Option_t *option);
   
+  //Histogram setters
+  
+  void   SetTRUTotalSignalHistogramsRange(Int_t nbins,  Float_t max) { fNBinsTRUSignal   = nbins; fMaxTRUSignal   = max ; }
+  void   SetSTUTotalSignalHistogramsRange(Int_t nbins,  Float_t max) { fNBinsSTUSignal   = nbins; fMaxSTUSignal   = max ; }
+  void   SetV0TotalSignalHistogramsRange (Int_t nbins,  Float_t max) { fNBinsV0Signal    = nbins; fMaxV0Signal    = max ; }
+  void   SetSTUFEERatioHistogramsRange   (Int_t nbins,  Float_t max) { fNBinsSTUFEERatio = nbins; fMaxSTUFEERatio = max ; }
+  void   SetSTUTRURatioHistogramsRange   (Int_t nbins,  Float_t max) { fNBinsSTUTRURatio = nbins; fMaxSTUFEERatio = max ; }
+
   
 private:
   TList            *fOutputList;  //! Output list
@@ -55,28 +61,33 @@ private:
   TH2F             *fhL0Patch;       //! FOR with L0 patch associated
   TH2F             *fhL1GPatch;      //! FOR with L1 Gamma patch associated
   TH2F             *fhL1JPatch;      //! FOR with L1 Jet patch associated
-  TH1F             *fhFEESTU[30];    //! Correlation FEE vs STU
-  TH1F             *fhTRUSTU[30];    //! Correlation TRU vs STU
+  TH2F             *fhFEESTU;        //! Correlation FEE vs STU
+  TH2F             *fhTRUSTU;        //! Correlation TRU vs STU
   TH2I             *fhV0STU;         //! Total signal STU vs V0C+V0S
   TH2I             *fhFullTRUSTU;    //! Total signal STU vs TRU
   TH2I             *fhSTUChecks;     //! Checks STU/TRU link
   
   // Histograms bins
   
-  Int_t             fNBinsSTUSignal ; // Number of bins for STU total signal histograms
-  Float_t           fMaxSTUSignal   ; // Maximum value for TRU total signal histograms
-  Int_t             fNBinsTRUSignal ; // Number of bins for TRU total signal histograms
-  Float_t           fMaxTRUSignal   ; // Maximum value for TRU total signal histograms
-  Int_t             fNBinsV0Signal  ; // Number of bins for V0 total signal histograms
-  Float_t           fMaxV0Signal    ; // Maximum value for V0 total signal histograms
-  
+  Int_t             fNBinsSTUSignal   ; // Number of bins for STU total signal histograms
+  Float_t           fMaxSTUSignal     ; // Maximum value for TRU total signal histograms
+  Int_t             fNBinsTRUSignal   ; // Number of bins for TRU total signal histograms
+  Float_t           fMaxTRUSignal     ; // Maximum value for TRU total signal histograms
+  Int_t             fNBinsV0Signal    ; // Number of bins for V0 total signal histograms
+  Float_t           fMaxV0Signal      ; // Maximum value for V0 total signal histograms
+  Int_t             fNBinsSTUFEERatio ; // Number of bins for STU/FEE ratios histograms
+  Float_t           fMaxSTUFEERatio   ; // Maximum value for STU/FEE ratios histograms
+  Int_t             fNBinsSTUTRURatio ; // Number of bins for STU/TRU ratios histograms
+  Float_t           fMaxSTUTRURatio   ; // Maximum value for STU/TRU ratios histograms
   
   //Constants needed by the class: EMCAL 
-  static const int fgkFALTRORows = AliEMCALGeoParams::fgkEMCALRows*(AliEMCALGeoParams::fgkEMCALModules-7)/2; // total number of fake altro rows    in EMCAL
-  //(ALTRO channels in one SM times 5 SM divided by 2 per FALTRO)
+  static const int  fgkFALTRORows = AliEMCALGeoParams::fgkEMCALRows*(AliEMCALGeoParams::fgkEMCALModules-7)/2; 
+  // total number of fake altro rows    in EMCAL
+  // (ALTRO channels in one SM times 5 SM divided by 2 per FALTRO)
   
-  static const int fgkFALTROCols = AliEMCALGeoParams::fgkEMCALCols;                                          // total number of fake altro columns in EMCAL 
-  //(ALTRO channels in one SM times 2 SM divided by 2 per FALTRO)
+  static const int  fgkFALTROCols = AliEMCALGeoParams::fgkEMCALCols;                                          
+  // total number of fake altro columns in EMCAL 
+  // (ALTRO channels in one SM times 2 SM divided by 2 per FALTRO)
   
   
   AliAnalysisTaskEMCALTriggerQA(const AliAnalysisTaskEMCALTriggerQA&);            //not implemented
