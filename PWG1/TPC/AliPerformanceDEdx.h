a0107db0666aab6e6fce930428abd7bfbcef0af3
jotwinow <jotwinow@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-10-18 09:26:47 +0000

    - changes by Markus Koehler
    - histogram ranges for high pT and new varible (TPC found/findlable clusters) added 
    M    TPC/AliPerformanceDEdx.h
    M    TPC/AliPerformanceDEdx.cxx
    
    - histogram ranges for high pT:
    M    TPC/AliPerformanceTPC.cxx
    M    TPC/AliPerformanceEff.cxx
    M    TPC/AliPerformanceDCA.cxx
    M    TPC/AliPerformanceMatch.cxx
    M    TPC/AliPerformanceMC.cxx
    M    TPC/AliPerformanceRes.cxx

--- PWG1/TPC/AliPerformanceDEdx.h
+++ PWG1/TPC/AliPerformanceDEdx.h
@@ -85,7 +85,7 @@ private:
   static Bool_t fgUseMergeTHnSparse;
   
   // TPC dE/dx 
-  THnSparseF *fDeDxHisto; //-> signal:phi:y:z:snp:tgl:ncls:p
+  THnSparseF *fDeDxHisto; //-> signal:phi:y:z:snp:tgl:ncls:p:nclsDEdx:nclsF
   TObjArray* fFolderObj; // array of analysed histograms
   
   // Selection cuts
