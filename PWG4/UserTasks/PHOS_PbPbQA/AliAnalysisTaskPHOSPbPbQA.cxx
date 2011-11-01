e57b4a9de6b49d3f441bc27fb203b9e47ec9a18b
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-01 15:03:32 +0000

    Changes for #88242 Request to port bugfix in the AliAnalysisTaskPHOSPbPbQA.cxx. From rev. 52400,52427

--- PWG4/UserTasks/PHOS_PbPbQA/AliAnalysisTaskPHOSPbPbQA.cxx
+++ PWG4/UserTasks/PHOS_PbPbQA/AliAnalysisTaskPHOSPbPbQA.cxx
@@ -37,6 +37,20 @@
 ClassImp(AliAnalysisTaskPHOSPbPbQA)
 
 //________________________________________________________________________
+AliAnalysisTaskPHOSPbPbQA::AliAnalysisTaskPHOSPbPbQA() : AliAnalysisTaskSE(),
+  fOutputContainer(0),fPHOSEvent(0),fCentrality(0),fCenBin(0),
+  fPHOSGeo(0),fEventCounter(0)
+{
+  //Default constructor
+  
+  for(Int_t i=0;i<1;i++){
+    for(Int_t j=0;j<2;j++)
+      fPHOSEvents[i][j]=0 ;
+  }
+  
+}
+
+//________________________________________________________________________
 AliAnalysisTaskPHOSPbPbQA::AliAnalysisTaskPHOSPbPbQA(const char *name) 
 : AliAnalysisTaskSE(name),
   fOutputContainer(0),
@@ -247,7 +261,7 @@ void AliAnalysisTaskPHOSPbPbQA::UserExec(Option_t *)
 	FillHistogram(key,p12.M() ,p12.Pt()) ;
 
 	if(sm1==sm2) {
-	  snprintf(key,55,"hPi0AllSM%d_cen%d",sm1,fCenBin) ;
+	  snprintf(key,55,"hMiPi0AllSM%d_cen%d",sm1,fCenBin) ;
 	  FillHistogram(key,p12.M() ,p12.Pt()) ; 
 	}
 
