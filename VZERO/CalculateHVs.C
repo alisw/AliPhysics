d9c806ebaf268bfb680fb8d1053edd6bbc8a656f
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-03 15:48:14 +0000

    Macro to calculate the needed HV values as a function of the required MIP signal.

---
+++ VZERO/CalculateHVs.C
@@ -0,0 +1,19 @@
+void CalculateHVs(Float_t requiredADCperMIP = 2.6, Int_t run = 137366)
+{
+  AliCDBManager *man = AliCDBManager::Instance();
+  man->SetDefaultStorage("raw://");
+  man->SetSpecificStorage("VZERO/Calib/PMGains","local://$ALICE_ROOT/OCDB");
+  man->SetSpecificStorage("VZERO/Calib/LightYields","local://$ALICE_ROOT/OCDB");
+  man->SetRun(run);
+
+  AliCDBEntry *ent = man->Get("VZERO/Calib/Data");
+  AliVZEROCalibData *calData = (AliVZEROCalibData*)ent->GetObject();
+  for(Int_t i = 0; i < 64; ++i) {
+    printf("%d %.0f (%.0f) S%d R%d\n",
+	   i,
+	   calData->GetHV(i,requiredADCperMIP),
+	   calData->GetMeanHV(i),
+	   i%8,
+	   (i<32) ? i/8 : (i-32)/8);
+  }
+}
