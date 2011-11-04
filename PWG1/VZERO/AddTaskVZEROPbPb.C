739fd6443754858da9994db70a7ba06c15d2077a
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-04 21:39:53 +0000

    Next version of Pb-Pb VZERO QA task + add macro (Raphael)

---
+++ PWG1/VZERO/AddTaskVZEROPbPb.C
@@ -0,0 +1,42 @@
+AliAnalysisTaskSE* AddTaskVZEROPbPb(Int_t runNumber)
+{
+  // Creates a PbPb QA task for VZERO
+  
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTaskQAsym", "No analysis manager to connect to.");
+    return NULL;
+  }  
+  
+  // Check the analysis type using the event handlers connected to the analysis manager.
+  //==============================================================================
+  if (!mgr->GetInputEventHandler()) {
+    ::Error("AddTasQAsym", "This task requires an input event handler");
+    return NULL;
+  }
+   TString inputDataType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+  
+   // Configure analysis
+   //===========================================================================
+   
+ 
+ 
+   AliAnaVZEROPbPb* task = new AliAnaVZEROPbPb("AliAnaVZEROPbPb");
+   task->SetClassesNames("CTRUE-,CVLN-,CVHN-,CPBI1-,CPBI2-,C0HWU-");
+   mgr->AddTask(task);
+  
+   AliAnalysisDataContainer *cout  = mgr->CreateContainer("PbPbVZEROHists",TList::Class(),
+							  AliAnalysisManager::kOutputContainer, Form("%s:VZERO_PbPb_Performance", 
+												     mgr->GetCommonFileName()));
+
+   mgr->ConnectInput  (task, 0, mgr->GetCommonInputContainer());
+   mgr->ConnectOutput (task, 1, cout);
+
+   return task;
+   
+  
+}
+
+
