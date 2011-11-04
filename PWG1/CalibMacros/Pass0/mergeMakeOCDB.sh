613f118090ca63b07e7e6c4a6c5e1c986dfa9f6c
rpreghen <rpreghen@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-04 13:52:57 +0000

    update to cpass0 executable scripts. current implementation taken from AliEn dir /alice/cern.ch/user/a/alidaq/bin. changes wrt. to previuos svn version in runPass0.sh -> removed limit to no. of events to be reconstructed

--- PWG1/CalibMacros/Pass0/mergeMakeOCDB.sh
+++ PWG1/CalibMacros/Pass0/mergeMakeOCDB.sh
@@ -3,15 +3,15 @@
 # Script to merge objects coming out of the calibration train:
 # Arguments:
 #    1 - directory on which to look for the files to be merged 
-#    2 - pattern
+#    2 - run number
 #    3 - OCDB output path
 
 # example:
-# mergeMakeOCDB.sh /alice/cern.ch/user/j/jotwinow/Pass0/output/ 000120691 alien://folder=/alice/cern.ch/user/j/jotwinow/Pass0/output
+# mergeMakeOCDB.sh /alice/cern.ch/user/a/aliprod/Pass0/output/ 000120691 alien://folder=/alice/cern.ch/user/a/aliprod/Pass0/output
 
 #ALIEN setting
 # $1 = directory where to perform the find 
-# $2 = pattern
+# $2 = run number
 # $3 = OCDB path
 
 echo ">>>>>>> Running AliRoot to merge calib objects found in $1 with pattern AliESDfriends_v1.root"
