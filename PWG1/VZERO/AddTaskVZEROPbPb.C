69110b0763e38b2f3594677cf4bc416485629387
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-13 12:42:29 +0000

    Update from Raphael.

--- PWG1/VZERO/AddTaskVZEROPbPb.C
+++ PWG1/VZERO/AddTaskVZEROPbPb.C
@@ -24,6 +24,8 @@ AliAnalysisTaskSE* AddTaskVZEROPbPb(Int_t runNumber)
  
  
    AliAnaVZEROPbPb* task = new AliAnaVZEROPbPb("AliAnaVZEROPbPb");
+   task->SetEquaMultRange(800,800.);
+   task->SetSumEquaMultRange(100,15000.,20000.);
 
    if(runNumber>166532)
    		task->SetClassesNames("CTRUE-,C0HWU-,CPBI2WU_B1-,CPBI2_B1-,CPBI1WU-,CPBI1-,CVHNWU-,CVHN-,CVLNWU-,CVLN-");
