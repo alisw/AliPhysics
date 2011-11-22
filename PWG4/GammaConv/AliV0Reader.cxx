9502c4ee42f0f52124e9f4aebeeb52742fa5d1f8
slindal <slindal@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-22 15:14:27 +0000

    fixing warning

--- PWG4/GammaConv/AliV0Reader.cxx
+++ PWG4/GammaConv/AliV0Reader.cxx
@@ -1753,27 +1753,18 @@ Bool_t AliV0Reader::GetHelixCenter(AliESDtrack* track, Double_t b,Int_t charge, 
 	Double_t ypoint =	radius * TMath::Sin(phi);
 
 	if(b<0){
-		if(charge > 0){
-			xpoint = - xpoint;
-			ypoint = - ypoint;
-		}
-
-		if(charge < 0){
-			xpoint =	xpoint;
-			ypoint =	ypoint;
-		}
+	  if(charge > 0){
+		xpoint = - xpoint;
+		ypoint = - ypoint;
+	  }
+	
+	} else if(b>0){
+	  if(charge < 0){
+		xpoint = - xpoint;
+		ypoint = - ypoint;
+	  }
 	}
-	if(b>0){
-		if(charge > 0){
-			xpoint =	xpoint;
-			ypoint =	ypoint;
-		}
 
-		if(charge < 0){
-			xpoint = - xpoint;
-			ypoint = - ypoint;
-		}
-	}
 	center[0] =	xpos + xpoint;
 	center[1] =	ypos + ypoint;
 
