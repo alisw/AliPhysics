67eee443991ffb148d3fdeae68e0492e5953758c
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-24 08:19:01 +0000

    make results scripts compilable for usage in the performance page

--- PWG1/TRD/AliTRDpwg1Helper.h
+++ PWG1/TRD/AliTRDpwg1Helper.h
@@ -50,8 +50,8 @@ public:
 
   static Bool_t DoTask(Int_t idx, Int_t map);
   static Int_t  GetTaskIndex(const Char_t *name);
-  static Bool_t HasReadMCData(Char_t *opt);
-  static Bool_t HasReadFriendData(Char_t *opt);
+  static Bool_t HasReadMCData(const Char_t *opt);
+  static Bool_t HasReadFriendData(const Char_t *opt);
   static const Char_t * TaskOpt(Int_t itask) {return fgkTRDtaskOpt[itask];}
   static const Char_t * TaskClassName(Int_t itask) {return fgkTRDtaskClassName[itask];}
 
@@ -59,7 +59,7 @@ public:
   static const Char_t*  Dirname(const char* filepath);
   static Int_t  MergeBatch(const Char_t *mark, const Char_t *files, const Int_t nfiles=20, const Int_t first=0, Bool_t kSVN=kTRUE, Bool_t kCLEAR=kFALSE);
   static void   MergeProd(const Char_t *mark, const Char_t *files, const Int_t nBatch=20, Int_t level=0);
-  static Int_t  ParseOptions(Char_t *trd);
+  static Int_t  ParseOptions(const Char_t *trd);
 
 private:
   AliTRDpwg1Helper(const AliTRDpwg1Helper& ref);
