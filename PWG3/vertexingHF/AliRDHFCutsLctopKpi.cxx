c7b69cf76f913b6c5a3c68072872ef1def794e5d
prino <prino@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-28 21:54:30 +0000

    Coverity

--- PWG3/vertexingHF/AliRDHFCutsLctopKpi.cxx
+++ PWG3/vertexingHF/AliRDHFCutsLctopKpi.cxx
@@ -724,7 +724,7 @@ void AliRDHFCutsLctopKpi::SetStandardCutsPbPb2010() {
 Bool_t AliRDHFCutsLctopKpi::ReconstructKF(AliAODRecoDecayHF3Prong *d,Int_t *pdgs,Double_t field) const{
 
   const Int_t nprongs=d->GetNProngs();
-  if(nprongs==0) return kFALSE;
+  if(nprongs<=0) return kFALSE;
 
  Int_t iprongs[nprongs];
  for(Int_t i=0;i<nprongs;i++) iprongs[i]=i;
