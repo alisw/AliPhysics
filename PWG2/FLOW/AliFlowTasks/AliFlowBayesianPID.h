eb87f09f27031fb83c8eb094bb782204cc19a4fd
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-12-05 13:29:10 +0000

    coverty

--- PWG2/FLOW/AliFlowTasks/AliFlowBayesianPID.h
+++ PWG2/FLOW/AliFlowTasks/AliFlowBayesianPID.h
@@ -1,10 +1,16 @@
-#ifndef AliFlowBayesianPID_h
-#define AliFlowBayesianPID_h
+/*
+-----------------------------------------------------------------------------------
+ AliFlowBayesianPID class implemented by F. Noferini (noferini@bo.infn.it)
+ needed to use Bayesian probability in the flow package (used in AliFlowTrackCuts)
+-----------------------------------------------------------------------------------
+*/
+#ifndef ALIFLOWBAYESIANPID_H
+#define ALIFLOWBAYESIANPID_H
 
-#include "TObject.h"
 #include "AliESDpid.h"
 #include "AliPIDResponse.h"
 
+class TObject;
 class TDatabasePDG;
 class AliESDEvent;
 class AliESDtrack;
@@ -66,74 +72,72 @@ class AliFlowBayesianPID : public AliPIDResponse {
   virtual Float_t NumberOfSigmasTOF(const AliVParticle *vtrack, AliPID::EParticleType type) const {if(vtrack) printf("Don't call AliFlowBayesianPID::NumberOfSigmasTOF method (%i)\n",type); return 0.0;} // do not use it
 
   // setter
-  void SetMC(Bool_t flag = kTRUE){fIsMC=flag;} // actually do nothing
   void SetDetResponse(AliESDEvent *esd,Float_t centrality=-1.0,EStartTimeType_t flagStart=AliESDpid::kTOF_T0,Bool_t recomputeT0TOF=kFALSE);
   void SetNewTrackParam(Bool_t flag=kTRUE){fNewTrackParam=flag;};
-  void SetDetAND(Int_t idet){if(idet < fNdetectors && idet >= 0) fMaskAND[idet] = kTRUE;};
-  void SetDetOR(Int_t idet){if(idet < fNdetectors && idet >= 0) fMaskOR[idet] = kTRUE;};
-  void ResetDetAND(Int_t idet){if(idet < fNdetectors && idet >= 0) fMaskAND[idet] = kFALSE;};
-  void ResetDetOR(Int_t idet){if(idet < fNdetectors && idet >= 0) fMaskOR[idet] = kFALSE;};
+  void SetDetAND(Int_t idet){if(idet < fgkNdetectors && idet >= 0) fMaskAND[idet] = kTRUE;};
+  void SetDetOR(Int_t idet){if(idet < fgkNdetectors && idet >= 0) fMaskOR[idet] = kTRUE;};
+  void ResetDetAND(Int_t idet){if(idet < fgkNdetectors && idet >= 0) fMaskAND[idet] = kFALSE;};
+  void ResetDetOR(Int_t idet){if(idet < fgkNdetectors && idet >= 0) fMaskOR[idet] = kFALSE;};
 
   // getter
   AliESDpid* GetESDpid(){return fPIDesd;};
-  TH2D *GetHistoPriors(Int_t specie){if(specie >=0 && specie < fNspecies) return hPriors[specie]; else return NULL;};
+  const TH2D *GetHistoPriors(Int_t specie) const {if(specie >=0 && specie < fgkNspecies) return fghPriors[specie]; else return NULL;};
   TSpline3 *GetMismatch();  
-  TF1 *GetTOFprob(){return fTOFResponse;};
-  TF1 *GetTPCprob(){return fTPCResponse;};
-  Float_t *GetWeights(Int_t det){if(det < fNdetectors && det >= 0){return fWeights[det];} else{return NULL;}};
-  Float_t *GetProb(){return fProb;};
-  Float_t GetTOFMismWeight(){return fWTofMism;};
-  Float_t GetTOFMismProb(){return fProbTofMism;};
-  Float_t GetMassOverZ(){return fMassTOF;};
-  Float_t GetZ(){return fZ;};
-  Bool_t GetDetANDstatus(Int_t idet){if(idet < fNdetectors && idet >= 0){return fMaskAND[idet];} else{return kFALSE;} };
-  Bool_t GetDetORstatus(Int_t idet){if(idet < fNdetectors && idet >= 0){return fMaskOR[idet];} else{return kFALSE;} };
-  Bool_t GetCurrentMask(Int_t idet){if(idet < fNdetectors && idet >= 0){return fMaskCurrent[idet];} else{return kFALSE;} };
-  Float_t GetExpDeDx(const AliESDtrack *t,Int_t iS);
+  const TF1 *GetTOFprob() const {return fTOFResponseF;};
+  const TF1 *GetTPCprob() const {return fTPCResponseF;};
+  const Float_t *GetWeights(Int_t det) const {if(det < fgkNdetectors && det >= 0){return fWeights[det];} else{return NULL;}};
+  Float_t *GetProb() {return fProb;};
+  Float_t GetTOFMismWeight() const {return fWTofMism;};
+  Float_t GetTOFMismProb() const {return fProbTofMism;};
+  Float_t GetMassOverZ() const {return fMassTOF;};
+  Float_t GetZ() const {return fZ;};
+  Bool_t GetDetANDstatus(Int_t idet) const {if(idet < fgkNdetectors && idet >= 0){return fMaskAND[idet];} else{return kFALSE;} };
+  Bool_t GetDetORstatus(Int_t idet) const {if(idet < fgkNdetectors && idet >= 0){return fMaskOR[idet];} else{return kFALSE;} };
+  Bool_t GetCurrentMask(Int_t idet) const {if(idet < fgkNdetectors && idet >= 0){return fMaskCurrent[idet];} else{return kFALSE;} };
+  Float_t GetExpDeDx(const AliESDtrack *t,Int_t iS) const;
 
   // methods for Bayesina Combined PID
   void ComputeWeights(const AliESDtrack *t);
   void ComputeProb(const AliESDtrack *t,Float_t); // obsolete method
   void ComputeProb(const AliESDtrack *t){ComputeProb(t,0.0);}; 
 
-  void SetTOFres(Float_t res){fTOFres=res;};
+  void SetTOFres(Float_t res){fTOFresolution=res;};
 
  private: 
   void SetPriors();
 
-  static const Int_t fNdetectors = 2;
-  static const Int_t fNspecies = 8;// 0=el, 1=mu, 2=pi, 3=ka, 4=pr, 5=deuteron, 6=triton, 7=He3 
-  static TH2D* hPriors[fNspecies]; // histo with priors (hardcoded)
-  static TSpline3 *fMism; // function for mismatch
-  static AliTOFGeometry *fTofGeo; // TOF geometry needed to reproduce mismatch shape
+  static const Int_t fgkNdetectors = 2; // Number of detector used for PID
+  static const Int_t fgkNspecies = 8;// 0=el, 1=mu, 2=pi, 3=ka, 4=pr, 5=deuteron, 6=triton, 7=He3 
+  static TH2D* fghPriors[fgkNspecies]; // histo with priors (hardcoded)
+  static TSpline3 *fgMism; // function for mismatch
+  static AliTOFGeometry *fgTofGeo; // TOF geometry needed to reproduce mismatch shape
 
   AliESDpid *fPIDesd;//ESDpid object
   TDatabasePDG *fDB; // Database pdg
-  Double_t fMass[fNspecies]; // mass for el(0),mu(1),pi(2),K(3),p(4)
+  Double_t fMass[fgkNspecies]; // mass for el(0),mu(1),pi(2),K(3),p(4)
 
   Bool_t fNewTrackParam; // switch for new tracking resolution TOF parameterization
-  Bool_t fIsMC; // switch if MC data
-  Float_t fTOFres; // TOF res needed only if T0-TOF should be recomputed
+  Float_t fTOFresolution; // TOF res needed only if T0-TOF should be recomputed
 
   AliFlowBayesianPID(const AliFlowBayesianPID&); // not implemented
   AliFlowBayesianPID& operator=(const AliFlowBayesianPID&); // not implemented 
 
-  TF1 *fTOFResponse; // TOF Gaussian+tail response function (tail at 1.1 sigma)
-  TF1 *fTPCResponse; // TPC Gaussian+tail response function (tail at 1.8 sigma)
+  TF1 *fTOFResponseF; // TOF Gaussian+tail response function (tail at 1.1 sigma)
+  TF1 *fTPCResponseF; // TPC Gaussian+tail response function (tail at 1.8 sigma)
 
   AliTOFT0maker *fTOFmaker; //TOF-T0 maker object
 
-  Float_t fWeights[fNdetectors][fNspecies]; // weights: 0=tpc,1=tof
-  Float_t fProb[fNspecies],fWTofMism,fProbTofMism; // Bayesian Combined PID + mismatch weights and probability 
+  Float_t fWeights[fgkNdetectors][fgkNspecies]; // weights: 0=tpc,1=tof
+  Float_t fProb[fgkNspecies],fWTofMism,fProbTofMism; // Bayesian Combined PID + mismatch weights and probability 
 
   Float_t fZ,fMassTOF; //measured charge(Z) and mass/Z
   TF1 *fBBdata; // Bethe Bloch function (needed to compute the charge of the particle)
 
-  Bool_t fMaskAND[fNdetectors],fMaskOR[fNdetectors],fMaskCurrent[fNdetectors]; // mask detector should be used
+  Bool_t fMaskAND[fgkNdetectors],fMaskOR[fgkNdetectors],fMaskCurrent[fgkNdetectors]; // mask detector should be used
 
   Float_t fCurrCentrality; // Centrality in current event
 
-  ClassDef(AliFlowBayesianPID, 4); // example of analysis
+  ClassDef(AliFlowBayesianPID, 5); // example of analysis
 };
 
 #endif
