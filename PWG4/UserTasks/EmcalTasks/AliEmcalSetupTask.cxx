b112fb29e7816dedbf5d518eaf803cd295ff502c
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-12-12 21:53:40 +0000

    propagator and setup task

---
+++ PWG4/UserTasks/EmcalTasks/AliEmcalSetupTask.cxx
@@ -0,0 +1,134 @@
+// $Id$
+//
+// Task to setup emcal related global objects
+//
+//
+
+#include <TClonesArray.h>
+#include <TGeoManager.h>
+#include <TGeoGlobalMagField.h>
+#include "AliAnalysisManager.h"
+#include "AliCDBManager.h"
+#include "AliEMCALGeometry.h"
+#include "AliESDEvent.h"
+#include "AliEmcalSetupTask.h"
+#include "AliGeomManager.h"
+#include "AliOADBContainer.h"
+
+ClassImp(AliEmcalSetupTask)
+
+//________________________________________________________________________
+AliEmcalSetupTask::AliEmcalSetupTask() : 
+  AliAnalysisTaskSE(),
+  fOcdbPath(),
+  fOadbPath("$ALICE_ROOT/OADB/EMCAL"),
+  fEsdEv(0),
+  fIsInit(kFALSE)
+{
+  // Constructor.
+}
+
+//________________________________________________________________________
+AliEmcalSetupTask::AliEmcalSetupTask(const char *name) : 
+  AliAnalysisTaskSE(name),
+  fOcdbPath(),
+  fOadbPath("$ALICE_ROOT/OADB/EMCAL"),
+  fEsdEv(0),
+  fIsInit(kFALSE)
+{
+  // Constructor.
+  fBranchNames = "ESD:AliESDHeader.,AliESDRun.";
+}
+
+//________________________________________________________________________
+AliEmcalSetupTask::~AliEmcalSetupTask()
+{
+  // Destructor.
+}
+
+//________________________________________________________________________
+void AliEmcalSetupTask::UserExec(Option_t *) 
+{
+  // Main loop, called for each event.
+
+  if (fIsInit)
+    return;
+
+  fEsdEv = dynamic_cast<AliESDEvent*>(InputEvent());
+  if (!fEsdEv) {
+    AliError("Task works only on ESD events, returning");
+    return;
+  }
+
+  AliAnalysisManager *am = AliAnalysisManager::GetAnalysisManager();
+  if (!am) {
+    AliError("Manager zero, returning");
+    return;
+  }
+  am->LoadBranch("AliESDRun.");
+  am->LoadBranch("AliESDHeader.");
+
+  Int_t runno = fEsdEv->GetRunNumber();
+  Bool_t is2010 = kTRUE;
+  if (runno>139517) {
+    is2010 = kFALSE;
+  }
+
+  TString geoname("EMCAL_FIRSTYEARV1");
+  if (!is2010)
+    geoname = "EMCAL_COMPLETEV1";
+  AliEMCALGeometry *geom = AliEMCALGeometry::GetInstance(geoname);
+  if (!geom) {
+    AliFatal(Form("Can not create geometry: %s",geoname.Data()));
+    return;
+  }
+
+  AliCDBManager *man = 0;
+  if (fOcdbPath.Length()>0) {
+    AliInfo(Form("Setting up OCDB"));
+    man = AliCDBManager::Instance();
+    man->SetDefaultStorage(fOcdbPath);
+    man->SetRun(runno);
+  }
+
+  TGeoManager *geo = AliGeomManager::GetGeometry();
+  if (!geo) {
+    TString fname("geometry_2010.root");
+    if (!is2010)
+      fname = "geometry_2011.root";
+    if (gSystem->AccessPathName(fname)!=0)
+      fname = "geometry.root";
+    if (gSystem->AccessPathName(fname)==0) {
+      AliInfo(Form("Loading geometry from %s", fname.Data()));
+      AliGeomManager::LoadGeometry(fname);
+    } else if (man) {
+      AliInfo(Form("Loading geometry from OCDB"));
+      AliGeomManager::LoadGeometry();
+    }
+  }
+  if (geo) {
+    AliGeomManager::ApplyAlignObjsFromCDB("EMCAL");
+    AliInfo(Form("Locking geometry"));
+    geo->LockGeometry();
+  }
+
+  if (!TGeoGlobalMagField::Instance()->GetField()) { // construct field map
+    AliInfo("Constructing field map from ESD run info");
+    fEsdEv->InitMagneticField();
+  }
+
+  if (fOadbPath.Length()>0) {
+    AliOADBContainer emcalgeoCont(Form("emcal"));
+    emcalgeoCont.InitFromFile(Form("%s/EMCALlocal2master.root",fOadbPath.Data()),
+                              Form("AliEMCALgeo"));
+    TObjArray *mobj=dynamic_cast<TObjArray*>(emcalgeoCont.GetObject(runno,"EmcalMatrices"));
+    if (mobj) {
+      for(Int_t mod=0; mod < (geom->GetEMCGeometry())->GetNumberOfSuperModules(); mod++){
+        //AliInfo(Form("Misalignment matrix %d", mod));
+        geom->SetMisalMatrix((TGeoHMatrix*) mobj->At(mod),mod);
+      } 
+    }
+  }
+
+  fIsInit = kTRUE;
+}
---
+++ PWG4/UserTasks/EmcalTasks/AliEmcalTrackPropagatorTask.cxx
@@ -0,0 +1,116 @@
+// $Id$
+//
+// Task to propagate tracks to EMCAL surface.
+//
+//
+
+#include <TClonesArray.h>
+#include "AliAnalysisManager.h"
+#include "AliEMCALRecoUtils.h"
+#include "AliESDEvent.h"
+#include "AliEmcalTrackPropagatorTask.h"
+
+ClassImp(AliEmcalTrackPropagatorTask)
+
+//________________________________________________________________________
+AliEmcalTrackPropagatorTask::AliEmcalTrackPropagatorTask() : 
+  AliAnalysisTaskSE(),
+  fRecoUtils(0),
+  fTracksName(),
+  fDist(430),
+  fMinPtCut(0.5),
+  fEsdEv(0),
+  fTracks(0)
+{
+  // Constructor.
+}
+
+//________________________________________________________________________
+AliEmcalTrackPropagatorTask::AliEmcalTrackPropagatorTask(const char *name) : 
+  AliAnalysisTaskSE(name),
+  fRecoUtils(0),
+  fTracksName("TpcSpdVertexConstrainedTracks"),
+  fDist(430),
+  fMinPtCut(0.5),
+  fEsdEv(0),
+  fTracks(0)
+{
+  // Constructor.
+  fBranchNames = "ESD:AliESDHeader.,Tracks";
+}
+
+//________________________________________________________________________
+AliEmcalTrackPropagatorTask::~AliEmcalTrackPropagatorTask()
+{
+  //Destructor.
+
+  delete fRecoUtils;
+}
+
+//________________________________________________________________________
+void AliEmcalTrackPropagatorTask::UserCreateOutputObjects()
+{
+  // Create histograms.
+
+  if (!fRecoUtils) {
+    fRecoUtils = new AliEMCALRecoUtils;
+    fRecoUtils->SetStep(25);
+    AliInfo("No reoc utils given, creating default utils");
+  }
+}
+
+//________________________________________________________________________
+void AliEmcalTrackPropagatorTask::UserExec(Option_t *) 
+{
+  // Main loop, called for each event.
+
+  fEsdEv = dynamic_cast<AliESDEvent*>(InputEvent());
+  if (!fEsdEv) {
+    AliError("Task works only on ESD events, returning");
+    return;
+  }
+
+  AliAnalysisManager *am = AliAnalysisManager::GetAnalysisManager();
+  if (!am) {
+    AliError("Manager zero, returning");
+    return;
+  }
+
+  // get tracks from event if not yet there
+  if (fTracksName == "Tracks")
+    am->LoadBranch("Tracks");
+  fTracks = dynamic_cast<TClonesArray*>((InputEvent()->FindListObject(fTracksName)));
+  if (!fTracks) {
+    AliError(Form("Could not get tracks %s, returning", fTracksName.Data()));
+    return;
+  }
+
+  // Loop over all tracks
+  const Int_t ntr = fTracks->GetEntries();
+  for (Int_t i=0; i<ntr; ++i) {
+    AliESDtrack *eTrack = static_cast<AliESDtrack*>(fTracks->At(i));
+    if (!eTrack)
+      continue;
+    if(eTrack->Pt()<fMinPtCut) 
+      continue;
+    Double_t phi = eTrack->Phi()*TMath::RadToDeg();
+    if (TMath::Abs(eTrack->Eta())>0.8 || phi <= 20 || phi >= 240) 
+      continue;
+    AliExternalTrackParam *trackParam =  const_cast<AliExternalTrackParam*>(eTrack->GetInnerParam());
+    if(!trackParam) 
+      continue;
+
+    // Extrapolate the track to EMCal surface
+    AliExternalTrackParam emcalParam(*trackParam);
+    Float_t etaout=0, phiout=0;
+    Bool_t ret = fRecoUtils->ExtrapolateTrackToEMCalSurface(&emcalParam, 
+                                                            fDist, 
+                                                            fRecoUtils->GetMass(), 
+                                                            fRecoUtils->GetStepSurface(), 
+                                                            etaout, 
+                                                            phiout);
+    if (!ret)
+      continue;
+    eTrack->SetOuterParam(&emcalParam,AliExternalTrackParam::kMultSec);
+  }
+}
