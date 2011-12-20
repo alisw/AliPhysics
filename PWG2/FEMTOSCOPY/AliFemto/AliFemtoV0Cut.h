44970a00c7e935a0b014b5f4d47e883c3f684d3b
akisiel <akisiel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-12-20 12:49:28 +0000

    Fix Coverity

--- PWG2/FEMTOSCOPY/AliFemto/AliFemtoV0Cut.h
+++ PWG2/FEMTOSCOPY/AliFemto/AliFemtoV0Cut.h
@@ -17,6 +17,7 @@ public:
   AliFemtoV0Cut(){/* no-op */};                             // default constructor. - Users should write their own
   AliFemtoV0Cut(const AliFemtoV0Cut& aCut);                 // copy constructor
   virtual ~AliFemtoV0Cut(){/* no-op */};                    // destructor
+  AliFemtoV0Cut& operator=(const AliFemtoV0Cut& aCut);                 // copy constructor
 
   virtual bool Pass(const AliFemtoV0* aV0)=0;               // true if passes, false if not
 
@@ -29,5 +30,6 @@ public:
 };
 
 inline AliFemtoV0Cut::AliFemtoV0Cut(const AliFemtoV0Cut& c) : AliFemtoParticleCut(c) { /* no-op */ } 
+inline AliFemtoV0Cut& AliFemtoV0Cut::operator=(const AliFemtoV0Cut& c) { if (this != &c) { AliFemtoParticleCut::operator=(c); } return *this; } 
 
 #endif
