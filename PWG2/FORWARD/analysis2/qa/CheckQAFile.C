db5c76c1509d0130d5d1d6f65937df33c2057324
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-06 11:06:46 +0000

    Modified some code

---
+++ PWG2/FORWARD/analysis2/qa/CheckQAFile.C
@@ -0,0 +1,40 @@
+/**
+ * @file   CheckQAFile.C
+ * @author Christian Holm Christensen <cholm@master.hehi.nbi.dk>
+ * @date   Fri Jan  6 11:39:22 2012
+ * 
+ * @brief  Script to check a QA file
+ * 
+ * @ingroup pwg2_forward_qa_scripts
+ */
+/** 
+ * Script to check a QA file.  Note that this terminates the ROOT session. 
+ * 
+ * @param filename File to read 
+ * 
+ * @ingroup pwg2_forward_qa_scripts
+ */
+void CheckQAFile(const char* filename)
+{
+  int    ret  = 0;
+  TFile* file = TFile::Open(filename, "READ");
+  if (!file) { 
+    Error("CheckQAFile", "No such file %s", filename);
+    exit(1);
+  }
+  TObject* forward1 = file->Get("Forward");
+  if (!forward1) {
+    Error("CheckQAFile", "No Forward object found in %s", filename);
+    ret |= 2;
+  } 
+  TObject* forward2 = file->Get("ForwardResults");
+  if (!forward2) {
+    Error("CheckQAFile", "No ForwardResults object found in %s", filename);
+    ret |= 4;
+  } 
+  file->Close();
+  exit(ret);
+}
+//
+// EOF
+//
