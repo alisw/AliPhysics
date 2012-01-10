80e9ad767e5690c549fab456f545b70795f7f2a4
esicking <esicking@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-10 10:08:23 +0000

    move analysis task for minijet analysis from PWG4/JetTasks to PWG4/CorrelationDPhi

--- PWG4/PWG4JetTasksLinkDef.h
+++ PWG4/PWG4JetTasksLinkDef.h
@@ -32,7 +32,6 @@
 #pragma link C++ class AliAnalysisTaskJetChem+;
 #pragma link C++ class AliAnalysisTaskJetChem::AliFragFuncHistosInvMass+;
 #pragma link C++ class AliAnalysisTaskJetChem::AliFragFuncHistosPhiCorrInvMass+;
-#pragma link C++ class AliAnalysisTaskMinijet+;
 #pragma link C++ class AliAnalysisTaskJetsTM+;
 #pragma link C++ class AliAnalysisTaskJetCore+;
 #pragma link C++ class AliAnalysisTaskQGSep+;
