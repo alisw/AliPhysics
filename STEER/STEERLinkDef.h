9767c171be8b214432b6e1ecc7400c9f717b7e35
rgrosso <rgrosso@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-13 15:10:19 +0000

    putting back some classes removed by mistake

--- STEER/STEERLinkDef.h
+++ STEER/STEERLinkDef.h
@@ -142,11 +142,22 @@
 #pragma link C++ class AliParamSolver+;
 
 #pragma link C++ class AliGRPManager+;
+#pragma link C++ class AliDCSArray+; 	 
+#pragma link C++ class AliLHCReader+;
 #pragma link C++ class AliCTPTimeParams+;
 #pragma link C++ class AliCTPInputTimeParams+;
 
+#pragma link C++ class AliLHCDipValT<Double_t>+; 	 
+#pragma link C++ class AliLHCDipValT<Int_t>+; 	 
+#pragma link C++ class AliLHCDipValT<Float_t>+; 	 
+#pragma link C++ class AliLHCDipValT<Char_t>+; 	 
+#pragma link C++ class AliLHCData+;
 #pragma link C++ class AliLHCClockPhase+;
 
 #pragma link C++ class AliLTUConfig+;
 
+#pragma link C++ typedef AliLHCDipValD; 	 
+#pragma link C++ typedef AliLHCDipValI; 	 
+#pragma link C++ typedef AliLHCDipValF; 	 
+#pragma link C++ typedef AliLHCDipValC;
 #endif
