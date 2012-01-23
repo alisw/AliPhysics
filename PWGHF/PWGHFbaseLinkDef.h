0eecf7cf871a730e473f175a9bfc340e981034a8
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-23 19:15:13 +0000

    Transition PWG3 --> PWGHF
        PWG3/vertexingHF --> PWGHF/vertexingHF
        PWG3/hfe --> PWGHF/hfe
        PWG3/muon --> PWG/muon
        PWG3/muondep --> PWG/muondep
        PWG3/base/*PtMoth* --> PWGHF/base
        PWG3/base/AliCounterCollection --> STEERBase

---
+++ PWGHF/PWGHFbaseLinkDef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliAnalysisTaskPtMothFromPtDaugh+;
+#pragma link C++ class AliPtMothFromPtDaugh+;
+#pragma link C++ class AliCounterCollection+;
+#endif
+
+
