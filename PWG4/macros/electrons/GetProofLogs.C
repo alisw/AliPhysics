95c6475dc398f27c5042ad4dce807ba9ae6c11c3
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-24 10:01:03 +0000

    move PWG4 macros to PWGGA corresponding places

--- PWG4/macros/electrons/GetProofLogs.C
+++
@@ -1,7 +0,0 @@
-{
- gEnv->SetValue("XSec.GSI.DelegProxy","2");
- logs=TProof::Mgr("kread@localhost")->GetSessionLogs(0);
- logs->Display();
- //logs->Grep("segmentation violation");
- logs->Save("*","logs.txt");
-}
