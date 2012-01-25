1aa8c10c53a4bace022bd801f6866dee03193b44
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-25 16:22:01 +0000

    moving AliAnalysisTaskDiHadron (Jason) to PWGCF

--- PWG4/PWG4JCORRANLinkDef.h
+++ PWG4/PWG4JCORRANLinkDef.h
@@ -18,5 +18,4 @@
 #pragma link C++ class AliPhJPiZero+;
 #pragma link C++ class AliJCORRANSetup+;
 #pragma link C++ class AliJCORRANTask+;
-#pragma link C++ class AliAnalysisTaskDiHadron+;
 #endif /* __CINT__ */
