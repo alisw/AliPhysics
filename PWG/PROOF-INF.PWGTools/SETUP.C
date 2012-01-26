e4a59074af5f16a17abed2de4d21fc0e4e362779
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-26 19:46:19 +0000

    for Tools Par file

---
+++ PWG/PROOF-INF.PWGTools/SETUP.C
@@ -0,0 +1,26 @@
+void SETUP()
+{
+
+   // Load the JET-Tasks library
+   gSystem->Load("libPWGTools");
+
+   // Set the Include paths
+   gSystem->AddIncludePath("-IPWGTools");
+   gROOT->ProcessLine(".include PWGTools/Tools");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("PWGTools_INCLUDE", "PWGTools/Tools");
+}
+void SETUP()
+{
+
+   // Load the JET-Tasks library
+   gSystem->Load("libPWGTools");
+
+   // Set the Include paths
+   gSystem->AddIncludePath("-IPWGTools");
+   gROOT->ProcessLine(".include PWGTools/Tools");
+
+   // Set our location, so that other packages can find us
+   gSystem->Setenv("PWGTools_INCLUDE", "PWGTools/Tools");
+}
