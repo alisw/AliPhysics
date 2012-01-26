93922e791bfdc3d27e515182799d986ca0436fac
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-26 09:50:39 +0000

    Track cuts for LHC10h are the default one, can switch on 11h

--- PWGJE/macros/ConfigLegoTrainPWGJE.C
+++ PWGJE/macros/ConfigLegoTrainPWGJE.C
@@ -3,8 +3,11 @@ void ConfigLegoTrainPWGJE(int iFlag = 0){
   // Adds the Global Variables depending on the flag
   // 
 
-  if(iFlag==1013){ // 10h
+  if(iFlag==1008){ // 10h
     AliAnalysisManager::SetGlobalStr("kJetRunPeriod","LHC10h");
+  }
+  else if(iFlag==1108){ // 11h
+    AliAnalysisManager::SetGlobalStr("kJetRunPeriod","LHC11h");
   }
 
 }
