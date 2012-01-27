85fc313ea61f1b936fc2d09b9f7de65b69b3f95c
kleinb <kleinb@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-27 11:03:42 +0000

    correct doubled patch

--- PWG/PROOF-INF.PWGTools/SETUP.C
+++ PWG/PROOF-INF.PWGTools/SETUP.C
@@ -11,16 +11,3 @@ void SETUP()
    // Set our location, so that other packages can find us
    gSystem->Setenv("PWGTools_INCLUDE", "PWGTools/Tools");
 }
-void SETUP()
-{
-
-   // Load the JET-Tasks library
-   gSystem->Load("libPWGTools");
-
-   // Set the Include paths
-   gSystem->AddIncludePath("-IPWGTools");
-   gROOT->ProcessLine(".include PWGTools/Tools");
-
-   // Set our location, so that other packages can find us
-   gSystem->Setenv("PWGTools_INCLUDE", "PWGTools/Tools");
-}
