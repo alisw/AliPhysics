dbd495fb6cdf20eb97a6fed7aa07a2f78d9f67c4
fbellini <fbellini@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-27 17:01:30 +0000

    Migration of PWG2/RESONANCES -> PWGLF/RESONANCES

--- PWG2/PWG2rsnextraLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class AliRsnTOFT0maker+;
-#pragma link C++ class AliRsnTOFCorrectionESD+;
-#pragma link C++ class AliRsnAnalysisPhi900GeV+;
-#pragma link C++ class AliRsnAnalysisPhi7TeV+;
-#pragma link C++ class AliAnalysisTaskSigma1385+;
-
-#endif
