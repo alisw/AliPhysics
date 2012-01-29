d7dad67678b3948d8c405d7f127216714780d740
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-01-29 21:59:30 +0000

    commit this version to have it. will be overwritten shortly

--- PWGGA/EMCALTasks/AliEsdSkimTask.cxx
+++ PWGGA/EMCALTasks/AliEsdSkimTask.cxx
@@ -10,21 +10,23 @@
 #include <TTree.h>
 #include "AliAnalysisManager.h"
 #include "AliCentrality.h"
-#include "AliEventplane.h"
 #include "AliESDEvent.h"
 #include "AliESDFMD.h"
 #include "AliESDtrackCuts.h"
 #include "AliEsdTrackExt.h"
+#include "AliEventplane.h"
 #include "AliMultiplicity.h"
+#include "AliVEventHandler.h"
+#include "AliPicoTrack.h"
 
 //_________________________________________________________________________________________________
 AliEsdSkimTask::AliEsdSkimTask(const char *opt) :
   AliAnalysisTaskSE(opt), fEvent(0), fTree(0), fCuts(0),
   fDoZDC(1), fDoV0(1), fDoT0(1), fDoTPCv(1), fDoSPDv(1), fDoPriv(1),
-  fDoEmCs(1), fDoPCs(1), fDoEmT(1), fDoPT(1), fDoTracks(1), fDoFmd(1),
-  fDoMult(1), fDoTof(1), fDoPileup(1), fDoClus(1), fEmcNames(""), 
-  fDoMiniTracks(0), fTracks("Tracks"), fPhosClusOnly(0), fDoSaveBytes(1),
-  fDoCent(1), fDoRP(1), fRemoveCP(0), fResetCov(1)
+  fDoEmCs(1), fDoPCs(0), fDoEmT(1), fDoPT(0), fDoTracks(0), fDoFmd(1),
+  fDoMult(1), fDoTof(0), fDoPileup(1), fDoClus(0), fEmcNames(""), 
+  fDoMiniTracks(0), fTracks("Tracks"), fPhosClusOnly(0), fDoSaveBytes(0),
+  fDoCent(1), fDoRP(1), fRemoveCP(0), fResetCov(1), fDoPicoTracks(0)
 {
   // Constructor.
 
@@ -47,7 +49,7 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
 
   AliAnalysisManager *am = AliAnalysisManager::GetAnalysisManager();
 
-  fEvent->Reset();
+  fEvent->ResetStdContent();
 
   TList* objsin  = esdin->GetList();
   TList* objsout = fEvent->GetList();
@@ -55,7 +57,48 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
   AliESDHeader *header = dynamic_cast<AliESDHeader*>(objsout->FindObject("AliESDHeader"));
   if (header) {
     am->LoadBranch("AliESDHeader.");
-    *header = *esdin->GetHeader();
+    if (!fDoSaveBytes) {
+      *header = *esdin->GetHeader();
+    } else {
+      AliESDHeader *hin = esdin->GetHeader();
+      header->SetTriggerMask(hin->GetTriggerMask());
+      header->SetOrbitNumber(hin->GetOrbitNumber());
+      header->SetTimeStamp(hin->GetTimeStamp());
+      header->SetEventType(hin->GetEventType());
+      header->SetEventSpecie(hin->GetEventSpecie());
+      header->SetEventNumberInFile(hin->GetEventNumberInFile());
+      header->SetBunchCrossNumber(hin->GetBunchCrossNumber());
+      header->SetPeriodNumber(hin->GetPeriodNumber());
+      header->SetTriggerCluster(hin->GetTriggerCluster());
+      header->SetL0TriggerInputs(hin->GetL0TriggerInputs());
+      header->SetL1TriggerInputs(hin->GetL1TriggerInputs());
+      header->SetL2TriggerInputs(hin->GetL2TriggerInputs());
+      for (Int_t i=0;i<24;++i) {
+        const char *name = hin->GetTriggerInputName(i,0);
+        if (name)
+          header->SetActiveTriggerInputs(name,i);
+      }
+      for (Int_t i=0;i<24;++i) {
+        const char *name = hin->GetTriggerInputName(i,1);
+        if (name)
+          header->SetActiveTriggerInputs(name,i+24);
+      }
+      for (Int_t i=0;i<12;++i) {
+        const char *name = hin->GetTriggerInputName(i,2);
+        if (name)
+          header->SetActiveTriggerInputs(name,i+48);
+      }
+    }
+    if (am->GetInputEventHandler()) {
+      TTree *tree = am->GetInputEventHandler()->GetTree();
+      if (tree) {
+        TFile *cfile = tree->GetCurrentFile();
+        if (cfile) {
+          TString cname(cfile->GetName());
+          header->SetTitle(cname);
+        }
+      }
+    }
   }
   AliESDRun *run = dynamic_cast<AliESDRun*>(objsout->FindObject("AliESDRun"));
   if (run) {
@@ -122,11 +165,28 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
     am->LoadBranch("PrimaryVertex.");
     *priv = *esdin->GetPrimaryVertexTracks();
   }
+#if 1
   AliESDCaloCells *ecells = dynamic_cast<AliESDCaloCells*>(objsout->FindObject("EMCALCells"));
   if (ecells) {
     am->LoadBranch("EMCALCells.");
     *ecells = *esdin->GetEMCALCells();
   }
+#else
+  TClonesArray *ecells = dynamic_cast<TClonesArray*>(objsout->FindObject("EMCALCells"));
+  if (ecells) {
+    am->LoadBranch("EMCALCells.");
+    AliESDCaloCells *ein = esdin->GetEMCALCells();
+    ecells->Clear();
+    for(Int_t i=0;i<ein->GetNumberOfCells();++i) {
+      Short_t  cellNumber = 0;
+      Double_t amplitude  = 0; 
+      Double_t time       = 0;
+      ein->GetCell(i, cellNumber, amplitude, time);
+      new ((*ecells)[i]) AliCaloCell(cellNumber, amplitude, time);
+    }
+  }
+#endif
+
   AliESDCaloCells *pcells = dynamic_cast<AliESDCaloCells*>(objsout->FindObject("PHOSCells"));
   if (pcells) {
     am->LoadBranch("PHOSCells.");
@@ -136,6 +196,7 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
   if (etrig) {
     am->LoadBranch("EMCALTrigger.");
     *etrig = *esdin->GetCaloTrigger("EMCAL");
+    etrig->Print("");
   }
   AliESDCaloTrigger *ptrig = dynamic_cast<AliESDCaloTrigger*>(objsout->FindObject("PHOSTrigger"));
   if (ptrig) {
@@ -226,7 +287,9 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
           continue;
         if (c->E()<emin)
           continue;
-        new ((*arrout)[nC++]) AliESDCaloCluster(*c);
+        AliESDCaloCluster *newCluster = new ((*arrout)[nC]) AliESDCaloCluster(*c);
+        newCluster->SetID(nC);
+        ++nC;
       }
     }
     delete namearr;
@@ -246,6 +309,7 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
     const Int_t Ntracks = tracksin->GetEntries();
     Int_t nacc = 0;
     for (Int_t iTracks = 0; iTracks < Ntracks; ++iTracks) {
+      if (iTracks>1500) break;
       AliESDtrack *track = dynamic_cast<AliESDtrack*>(tracksin->At(iTracks));
       if (!track)
         continue;
@@ -253,17 +317,29 @@ void AliEsdSkimTask::UserExec(Option_t */*opt*/)
         if (!fCuts->IsSelected(track))
           continue;
       }
-      AliEsdTrackExt *newtrack = new ((*tracksout)[nacc]) AliEsdTrackExt(*track);
-      if (fDoMiniTracks) {
-        newtrack->MakeMiniTrack(0,fRemoveCP);
-        if (fResetCov)
-          newtrack->ResetCovariance(1);
+      if (fDoPicoTracks) {
+        AliEsdTrackExt newtrack(*track);
+        Double_t etaemc = 0;
+        Double_t phiemc = 0;
+        if (newtrack.IsEMCAL()) {
+          etaemc = newtrack.GetEmcEta();
+          phiemc = newtrack.GetEmcPhi();
+        }
+        new ((*tracksout)[nacc]) AliPicoTrack(newtrack.Pt(), newtrack.Eta(), newtrack.Phi(), 
+                                              newtrack.Charge(), newtrack.GetLabel(), 
+                                              etaemc, phiemc, newtrack.IsEMCAL());
+        ++nacc;
       } else {
-        newtrack->DeleteParams();
+        AliEsdTrackExt *newtrack = new ((*tracksout)[nacc]) AliEsdTrackExt(*track);
+        if (fDoMiniTracks) {
+          newtrack->MakeMiniTrack(0,fRemoveCP);
+          newtrack->ResetCovariance(fResetCov);
+        } else {
+          newtrack->DeleteParams();
+        }
+        newtrack->SetID(nacc);
+        ++nacc;
       }
-      newtrack->SetID(nacc);
-      fEvent->AddTrack(track);
-      ++nacc;
     }
     if (fCuts) 
       AliInfo(Form("Selected %d out of %d \n", nacc, Ntracks));
@@ -276,7 +352,12 @@ void AliEsdSkimTask::UserCreateOutputObjects()
 {
   // Create output objects.
 
+  TFile *file = OpenFile(1);
   fTree = new TTree("esdTree", "Tree with skimmed ESD objects");
+  file->SetCompressionLevel(2);
+  fTree->SetDirectory(file);
+  fTree->SetAutoFlush(-10*1024*1024);
+
   fEvent = new AliESDEvent;
   fEvent->AddObject(new AliESDHeader());
   fEvent->AddObject(new AliESDRun());
@@ -313,6 +394,9 @@ void AliEsdSkimTask::UserCreateOutputObjects()
   }
   if (fDoEmCs) {
     fEvent->AddObject(new AliESDCaloCells("EMCALCells","EMCALCells"));
+    //TClonesArray *arr = new TClonesArray("AliCaloCell",0);
+    //arr->SetName("EMCALCells");
+    //fEvent->AddObject(arr);
   }
   if (fDoPCs) {
     fEvent->AddObject(new AliESDCaloCells("PHOSCells","PHOSCells"));
@@ -364,7 +448,10 @@ void AliEsdSkimTask::UserCreateOutputObjects()
   }
   if (fDoTracks) {
     TClonesArray *arr = 0;
-    if (fDoMiniTracks) {
+    if (fDoPicoTracks) {
+      arr = new TClonesArray("AliPicoTrack",0);
+      arr->SetName("Tracks");
+    } else if (fDoMiniTracks) {
       arr = new TClonesArray("AliEsdTrackExt",0);
       arr->SetName("Tracks");
     } else {
@@ -376,10 +463,5 @@ void AliEsdSkimTask::UserCreateOutputObjects()
   fEvent->GetStdContent();
   fEvent->WriteToTree(fTree);
   fTree->GetUserInfo()->Add(fEvent);
-  TFile *file = OpenFile(1);
-  file->SetCompressionLevel(2);
-  fTree->SetDirectory(file);
-  fTree->SetAutoFlush(-1024*1024*1024);
-  fTree->SetAutoSave(-1024*1024*1024);
   PostData(1,fTree);
 }
