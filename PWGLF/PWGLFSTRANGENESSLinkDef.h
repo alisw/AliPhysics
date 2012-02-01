35bbeb130cf6e74e8e58a744d76dacf85296fb17
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-02-01 11:35:00 +0000

    Transition PWG2/spectra -> PWGLF/STRANGENESS

---
+++ PWGLF/PWGLFSTRANGENESSLinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+
+#pragma link off all glols;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliAnalysisTaskCheckCascade+;
+#pragma link C++ class AliAnalysisTaskCheckCascadePbPb+;
+#pragma link C++ class AliAnalysisTaskCheckPerformanceCascade+;
+#pragma link C++ class AliAnalysisTaskCheckPerformanceCascadePbPb+;
+#pragma link C++ class AliAnalysisCentralitySelector+;
+#pragma link C++ class AliAnalysisTaskCTauPbPb+;
+#pragma link C++ class AliAnalysisTaskCTauPbPbaod+;
+#pragma link C++ class AliAnalysisTaskLukeAOD+;
+#pragma link C++ class AliAnalysisTaskLukeV0+;
+#pragma link C++ class AliAnalysisTaskPerformanceStrange+;
+
+#endif
