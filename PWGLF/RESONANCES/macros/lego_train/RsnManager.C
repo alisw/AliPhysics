f2b33eeec91e95962780da1ab1f99a7e52e728ee
fbellini <fbellini@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-02-08 21:27:48 +0000

    Updated lego_train macros and PWG2 -> PWGLF (M.Vala)

--- PWGLF/RESONANCES/macros/lego_train/RsnManager.C
+++ PWGLF/RESONANCES/macros/lego_train/RsnManager.C
@@ -1,19 +1,23 @@
 TList *RsnManager() {
 
-   Int_t isPP           = 1;
+   Int_t isPP           = 1; // in GRID case it will be overwriten
    Int_t useRsnMini     = 1;
 
    Int_t useMixing      = 1;
    Int_t numMix         = 10;
 
-   Int_t fullOutput     = 0;
+   Int_t fullOutput     = 1;
    Int_t mcMomentum     = 0;
-   
+
    Int_t useEventMixPar = 0;
    Int_t useRsnPar      = 0;
-
-   TString legoTrainPath = "$ALICE_ROOT/PWG2/RESONANCES/macros/lego_train";
-//    legoTrainPath = "$HOME/git/PWG2resonances/PWG2/RESONANCES/macros/lego_train";
+   
+   TString rootver = "v5-30-06";
+   TString alirootver = "";
+//   alirootver = "v5-02-17-AN";
+   
+   TString legoTrainPath = "$ALICE_ROOT/PWGLF/RESONANCES/macros/lego_train";
+//   legoTrainPath = "$HOME/git/PWG2resonances/PWG2/RESONANCES/macros/lego_train";
 
 
    TList *listRsn = new TList();
@@ -24,52 +28,57 @@ TList *RsnManager() {
    // so you can control different cut settings
    // string "<Name>:mon" means that it will add monitoring histograms from cuts
    // Note : for now you have to set gRsnUseMiniPackage = 0 to have mon histograms
-//    listRsn->Add(new TNamed("<Name>:mon","<CutName>:<opt>"));
+   //    listRsn->Add(new TNamed("<Name>:mon","<CutName>:<opt>"));
 
 
-//    listRsn->Add(new TNamed("Phi","Phi2010"));
+   //    listRsn->Add(new TNamed("Phi","Phi2010"));
    listRsn->Add(new TNamed("Phi","Phi2010:mon"));
-//    listRsn->Add(new TNamed("Phi","Phi2010:qualityonly_mon"));
-//     listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma1_mon"));
-//     listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma2_mon"));
-//     listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma3_mon"));
-//     listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma1_mon"));
-//     listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma2_mon"));
-//     listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma3_mon"));
-//     listRsn->Add(new TNamed("Phi","BPID:mon"));
-//
-//    // in case you have MC
-//    listRsn->Add(new TNamed("Phi","PDG:mon"));
-//
-//    listRsn->Add(new TNamed("KStar","KStar2010:mon"));
-//    listRsn->Add(new TNamed("KStar","BPID:mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma1_mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma2_mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma3_mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma1_mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma2_mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma3_mon"));
+//   listRsn->Add(new TNamed("Phi","BPID:mon"));
+//   listRsn->Add(new TNamed("Phi","Phi2010:qualityonly_mon"));
+   //
+   //    // in case you have MC
+   //    listRsn->Add(new TNamed("Phi","PDG:mon"));
+   //
+   //    listRsn->Add(new TNamed("KStar","KStar2010:mon"));
+   //    listRsn->Add(new TNamed("KStar","BPID:mon"));
 
-//    listRsn->Add(new TNamed("KStar","KStar:mon"));
-//    listRsn->Add(new TNamed("KStar","KStar:TPCTOFpidDefaultKstarPP2010_mon"));
-//    listRsn->Add(new TNamed("KStar","KStar:FastTPCpid1point5sigma_mon"));
-//    listRsn->Add(new TNamed("KStar","KStar:FastTPCpid2sigma_mon"));
+   //    listRsn->Add(new TNamed("KStar","KStar:mon"));
+   //    listRsn->Add(new TNamed("KStar","KStar:TPCTOFpidDefaultKstarPP2010_mon"));
+   //    listRsn->Add(new TNamed("KStar","KStar:FastTPCpid1point5sigma_mon"));
+   //    listRsn->Add(new TNamed("KStar","KStar:FastTPCpid2sigma_mon"));
 
-   
+
    //============= ONLY for GRID ====================
-   Int_t idRsnTrain=0;
-
    TString dsConfig;
-   isPP = 0;
-   dsConfig = "datasets-grid/LHC10h_p2_ESD.txt";
-   dsConfig = "datasets-grid/LHC10h_p2_AOD049.txt";
-   dsConfig = "datasets-grid/LHC10h_p2_AOD073.txt";
+
+//   isPP = 0;
+//   dsConfig = "datasets-grid/LHC10h_p2_ESD.txt";
+//   dsConfig = "datasets-grid/LHC10h_p2_AOD049.txt";
+//   dsConfig = "datasets-grid/LHC10h_p2_AOD073.txt";
 
 //   dsConfig = "datasets-grid/LHC11a10b_AOD080.txt";
 
-//   isPP = 1;
-//   dsConfig = "datasets-grid/LHC10b_p2_ESD.txt";
-//   dsConfig = "datasets-grid/LHC10b_p2_AOD038.txt";
+//      isPP = 1;
+//      dsConfig = "datasets-grid/LHC10b_p2_ESD.txt";
+//      dsConfig = "datasets-grid/LHC10b_p2_AOD038.txt";
+
+   isPP = 1;
+   dsConfig = "datasets-grid/LHC11a_AOD072.txt";
+   dsConfig = "datasets-grid/LHC11h5b_AOD079.txt";
+
+
    //================================================
 
    ///////////////////////////////////////////
    // don't edit next lines (EXPERTS ONLY)
    ///////////////////////////////////////////
-   
+
    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
    if (!mgr) { Printf("Error[RsnManager] mgr is null !!!"); return 0; }
 
@@ -110,8 +119,11 @@ TList *RsnManager() {
    AliAnalysisManager::SetGlobalInt("rsnMixPrintRefresh",-1);
 
    // RSN train settings for GRID
-   AliAnalysisManager::SetGlobalInt("rsnTrainID",idRsnTrain);
    AliAnalysisManager::SetGlobalStr("rsnTrainDSConfig",dsConfig.Data());
+
+   // root and aliroot version
+   AliAnalysisManager::SetGlobalStr("rsnLegoTrainROOTversion",rootver.Data());
+   AliAnalysisManager::SetGlobalStr("rsnLegoTrainAliROOTversion",alirootver.Data());
 
    return listRsn;
 }
