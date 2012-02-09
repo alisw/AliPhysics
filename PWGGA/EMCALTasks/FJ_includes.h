b4a4e9be271eb5792464648e2eeea0bdbf9dba1e
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-02-09 19:53:58 +0000

    have jet task and wrapper

---
+++ PWGGA/EMCALTasks/FJ_includes.h
@@ -0,0 +1,17 @@
+#ifndef FJ_includes_H
+#define FJ_includes_H
+
+// $Id$
+
+#if !defined(__CINT__) && !defined(__MAKECINT__)
+#if defined __GNUC__
+#pragma GCC system_header
+#endif
+#include <fastjet/PseudoJet.hh>
+#include <fastjet/JetDefinition.hh>
+#include <fastjet/ClusterSequence.hh>
+#include <fastjet/ClusterSequenceArea.hh>
+#include <fastjet/AreaDefinition.hh>
+#include <fastjet/SISConePlugin.hh>
+#endif
+#endif
