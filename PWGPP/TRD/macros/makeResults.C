a7a2df007a335a0f457202f888bc070c81dfe796
abercuci <abercuci@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-02-14 09:45:47 +0000

    add libPWGmuon.so add skip AliTaskCDBconnect option in run.C

--- PWGPP/TRD/macros/makeResults.C
+++ PWGPP/TRD/macros/makeResults.C
@@ -80,7 +80,7 @@
 #include "PWGPP/TRD/AliTRDinfoGen.h"
 #endif
 
-const Char_t *libs[] = {"libProofPlayer.so", "libANALYSIS.so", "libANALYSISalice.so", "libCORRFW", "libTENDER.so", "libPWGPP.so"};
+const Char_t *libs[] = {"libProofPlayer.so", "libANALYSIS.so", "libANALYSISalice.so", "libCORRFW", "libTENDER.so", "libPWGPP.so", "libPWGmuon.so"};
 // define setup
 TCanvas *c(NULL);
 Bool_t mc(kFALSE), friends(kFALSE);
