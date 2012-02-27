497a86bf4ed636febc0164011b22961e83eb183e
fbellini <fbellini@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-02-27 09:41:40 +0000

    Improved check for label in PDG cut case (M.Vala)

--- PWGLF/RESONANCES/AliRsnDaughterSelector.cxx
+++ PWGLF/RESONANCES/AliRsnDaughterSelector.cxx
@@ -17,7 +17,8 @@ AliRsnDaughterSelector::AliRsnDaughterSelector(const char *name, const char *tit
    fCutSetsC("AliRsnCutSet", 0),
    fEntryListsN("TEntryList", 0),
    fEntryListsP("TEntryList", 0),
-   fEntryListsM("TEntryList", 0)
+   fEntryListsM("TEntryList", 0),
+   fUseLabelCheck(kTRUE)
 {
 //
 // Default constructor.
@@ -34,7 +35,8 @@ AliRsnDaughterSelector::AliRsnDaughterSelector(const AliRsnDaughterSelector &cop
    fCutSetsC(copy.fCutSetsC),
    fEntryListsN(copy.fEntryListsN),
    fEntryListsP(copy.fEntryListsP),
-   fEntryListsM(copy.fEntryListsM)
+   fEntryListsM(copy.fEntryListsM),
+   fUseLabelCheck(copy.fUseLabelCheck)
 {
 //
 // Copy constructor.
@@ -61,6 +63,7 @@ AliRsnDaughterSelector &AliRsnDaughterSelector::operator=(const AliRsnDaughterSe
    fEntryListsN = copy.fEntryListsN;
    fEntryListsP = copy.fEntryListsP;
    fEntryListsM = copy.fEntryListsM;
+   fUseLabelCheck = copy.fUseLabelCheck;
 
    AliDebug(AliLog::kDebug + 10, "->");
 
@@ -255,6 +258,7 @@ void AliRsnDaughterSelector::ScanEvent(AliRsnEvent *ev)
    TClonesArray *cutsArray = 0x0, *entryArray = 0x0;
    TEntryList labelList;
    TEntryList *el = 0;
+   AliRsnCutSet *cuts;
    for (id = 0; id < nTot; id++) {
       ev->SetDaughter(check, id);
       // some checks
@@ -277,20 +281,21 @@ void AliRsnDaughterSelector::ScanEvent(AliRsnEvent *ev)
          cutsArray = &fCutSetsN;
          entryArray = &fEntryListsN;
       }
+
+      if (fUseLabelCheck && labelList.Contains(check.GetLabel())) continue;
+
       // check with all cuts in that charge
       nSel = cutsArray->GetEntries();
       for (is = 0; is < nSel; is++) {
-         AliRsnCutSet *cuts = (AliRsnCutSet *)cutsArray->At(is);
+         cuts = (AliRsnCutSet *)cutsArray->At(is);
          if (cuts->IsSelected(&check)) {
-            if (!labelList.Contains(check.GetLabel())) {
-               el = (TEntryList *)entryArray->At(is);
-               el->Enter(id);
-               labelList.Enter(check.GetLabel());
-            }
-
+            el = (TEntryList *)entryArray->At(is);
+            el->Enter(id);
+            if (fUseLabelCheck) labelList.Enter(check.GetLabel());
          }
       }
    }
 
    //Print();
 }
+
