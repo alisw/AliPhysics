b75171d8bdfab4d21123d8de9e1e3920fcf078e1
pcrochet <pcrochet@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-01 16:02:40 +0000

    take the sigma cut for cluster/trackRef association from recoParam. add a MC label to ghost track. (Philippe Pillot)

--- PWG/muondep/AddTaskESDMCLabelAddition.C
+++ PWG/muondep/AddTaskESDMCLabelAddition.C
@@ -1,40 +1,42 @@
-AliAnalysisTaskESDMCLabelAddition *AddTaskESDMCLabelAddition(Bool_t useKineFilter=kTRUE)
+AliAnalysisTaskESDMCLabelAddition *AddTaskESDMCLabelAddition()
 {
-// Creates a filter task and adds it to the analysis manager.
-// This file allows the creation of MC labels (based on the code of Philippe P.)
+  /// Add AliAnalysisTaskESDMCLabelAddition to the train (Philippe Pillot)
+  
+  // Get the pointer to the existing analysis manager via the static access method.
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if(!mgr) { 
+    Error("AddTaskESDMCLabelAddition","AliAnalysisManager not set!");
+    return NULL;
+  }
+  
+  // This task runs on ESDs
+  TString type = mgr->GetInputEventHandler()->GetDataType();
+  if (!type.Contains("ESD")) {
+    Error("AddTaskESDMCLabelAddition", "ESD input handler needed!");
+    return NULL;
+  }
+  
+  // This task needs MC input
+  AliMCEventHandler *mcH = (AliMCEventHandler*)mgr->GetMCtruthEventHandler();
+  if (!mcH) {
+    Error("AddTaskESDMCLabelAddition", "No MC handler connected!");
+    return NULL;
+  }   
+  
+  // Create and configure task
+  AliAnalysisTaskESDMCLabelAddition *task = new AliAnalysisTaskESDMCLabelAddition("ESD MC Labels addition");
+  if (!task) {
+    Error("AddTaskESDMCLabelAddition", "MClabel addition task cannot be created!");
+    return NULL;
+  }
+  
+  // Add task to analysis manager
+  mgr->AddTask(task);
+  
+  // Connect input container
+  mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+  
+  return task;
+  
+}
 
-   // Get the pointer to the existing analysis manager via the static access method.
-   //==============================================================================
-   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-   if (!mgr) {
-      ::Error("AddTaskESDMCLabelAddition", "No analysis manager to connect to.");
-      return NULL;
-   }   
-   
-   TString type = mgr->GetInputEventHandler()->GetDataType();
-   // Check if MC handler is connected in case kine filter requested
-   AliMCEventHandler *mcH = (AliMCEventHandler*)mgr->GetMCtruthEventHandler();
-   if (!mcH && useKineFilter) {
-      ::Error("AddTaskESDFilter", "No MC handler connected while kine filtering requested");
-      return NULL;
-   }   
-   
-   if (useKineFilter) {
-      AliAnalysisTaskMCParticleFilter *kinefilter = new AliAnalysisTaskMCParticleFilter("Particle Kine Filter");
-      mgr->AddTask(kinefilter);
-   }   
-
-
-   // Create the task, add it to the manager and configure it.
-   //===========================================================================   
-   // Barrel tracks filter
-   AliAnalysisTaskESDMCLabelAddition *ESDMCLabeltask = new AliAnalysisTaskESDMCLabelAddition("ESD MC Labels addition");
-   mgr->AddTask(ESDMCLabeltask);
-   
-   // Create ONLY the output containers for the data produced by the task.
-   // Get and connect other common input/output containers via the manager as below
-   //==============================================================================
-   mgr->ConnectInput  (ESDMCLabeltask,  0, mgr->GetCommonInputContainer());
-
-   return ESDMCLabeltask;
-}   
--- PWG/muondep/AliAnalysisTaskESDMCLabelAddition.h
+++ PWG/muondep/AliAnalysisTaskESDMCLabelAddition.h
@@ -6,44 +6,39 @@
 
 /* $Id$ */ 
 
-#include <TList.h> 
+#include <TString.h>
 #include "AliAnalysisTaskSE.h"
-
-class AliAnalysisFilter;
-class AliStack;
-class AliESDMuonTrack;
-class AliMUONTrack;
-class AliMUONVTrackStore;
 
 class AliAnalysisTaskESDMCLabelAddition : public AliAnalysisTaskSE
 {
   
-  public:
-    AliAnalysisTaskESDMCLabelAddition();
-    AliAnalysisTaskESDMCLabelAddition(const char* name);
-    virtual ~AliAnalysisTaskESDMCLabelAddition() {;}
-    // Implementation of interface methods
-    virtual void UserCreateOutputObjects();
-    virtual void Init();
-    virtual void LocalInit() {Init();}
-    virtual void UserExec(Option_t *option);
-    virtual void Terminate(Option_t *option);
-    
-    
-  private:
-    
-    static const Double_t fgkSigmaCut; // sigma cut applied to match a reconstructed cluster with a trackref
-    
-    AliAnalysisTaskESDMCLabelAddition(const AliAnalysisTaskESDMCLabelAddition&);
-    AliAnalysisTaskESDMCLabelAddition& operator=(const AliAnalysisTaskESDMCLabelAddition&);
-    
-    void AddMCLabel();
-    AliMUONTrack* ESDToMUON(AliESDMuonTrack &esdTrack);
-    AliMUONTrack* MatchWithTrackRef(AliESDMuonTrack &esdTrack, AliMUONVTrackStore &trackRefStore);
-    Bool_t TrackMatched(AliMUONTrack &track, AliMUONTrack &trackRef);
-    
-    ClassDef(AliAnalysisTaskESDMCLabelAddition, 1); // Analysis task for standard ESD filtering
-    
+public:
+  AliAnalysisTaskESDMCLabelAddition();
+  AliAnalysisTaskESDMCLabelAddition(const char* name);
+  virtual ~AliAnalysisTaskESDMCLabelAddition() {;}
+  
+  // Implementation of interface methods
+  virtual void UserCreateOutputObjects();
+  virtual void NotifyRun();
+  virtual void UserExec(Option_t *option);
+  virtual void Terminate(Option_t *option);
+  
+  /// Set location of the default OCDB storage (if not set use "raw://")
+  void SetDefaultStorage(const char* ocdbPath) { fDefaultStorage = ocdbPath; }
+  
+  
+private:
+  
+  AliAnalysisTaskESDMCLabelAddition(const AliAnalysisTaskESDMCLabelAddition&);
+  AliAnalysisTaskESDMCLabelAddition& operator=(const AliAnalysisTaskESDMCLabelAddition&);
+  
+  TString  fDefaultStorage; ///< location of the default OCDB storage
+  Double_t fSigmaCut;       //!< sigma cut to associate clusters with TrackRefs
+  Double_t fSigmaCutTrig;   //!< sigma cut to associate trigger track to triggerable track
+  
+  ClassDef(AliAnalysisTaskESDMCLabelAddition, 2); // Analysis task for standard ESD filtering
+  
 };
 
 #endif
+
