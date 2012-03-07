7a3de3a11bd9d7e2b8b4ea726c7a4240307114d0
snelling <snelling@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-07 17:13:57 +0000

    updated phi flow analysis

--- PWGCF/FLOW/macros/AddTaskPhiFlow.C
+++ PWGCF/FLOW/macros/AddTaskPhiFlow.C
@@ -3,18 +3,28 @@
 // AddTaskPhiFlow macro
 // Author: Redmer A. Bertens, Utrecht University, 2012
 /////////////////////////////////////////////////////////////////////////////////////////////
-AliAnalysisTaskPhiFlow* AddTaskPhiFlow( Float_t centrMin = 30.,
-                                        Float_t centrMax = 40.,
-                                        Float_t vertexZ = 10.,
-                                        Float_t bayesThresholdP = 0.5,
-                                        Float_t minEtaA = -0.8,
-                                        Float_t maxEtaA = 0.,
-                                        Float_t minEtaB = 0.,
-                                        Float_t maxEtaB = 0.8,
-                                        Float_t deltaDip = 0.0,
-                                        Float_t deltaDipMaxPt = 0.0,
-                                        Float_t eventPlanePtCut = 10,
-                                        AliAnalysisTaskPhiFlow::PIDtype pidtype = AliAnalysisTaskPhiFlow::kCombined)
+AliAnalysisTaskPhiFlow* AddTaskPhiFlow(Bool_t SP = kTRUE,
+                                       Bool_t QC = kTRUE,
+                                       Bool_t EP = kTRUE,
+                                       Int_t harmonic = 2,
+                                       Float_t centrMin = 30.,
+                                       Float_t centrMax = 40.,
+                                       Float_t vertexZ = 10.,
+                                       Float_t bayesThresholdP = 0.5,
+                                       Bool_t strictKaonCuts = kTRUE,
+                                       Bool_t TPCStandAloneTracks = kTRUE,
+                                       Float_t RPEtaMinA = -0.8,
+                                       Float_t RPEtaMaxA = 0.0,
+                                       Float_t RPEtaMinB = 0.0,
+                                       Float_t RPEtaMaxB = 0.8,
+                                       Float_t RPEtaMax = 0.8,
+                                       Float_t POIEtaMin = -0.8,
+                                       Float_t POIEtaMax = 0.8,
+                                       Float_t POIPtMin = 0.15,
+                                       Float_t POIPtMax = 10.,
+                                       Float_t deltaDip = 0.0,
+                                       Float_t deltaDipMaxPt = 0.0,
+                                       AliAnalysisTaskPhiFlow::PIDtype pidtype = AliAnalysisTaskPhiFlow::kCombined)
 {
    TString centralityName("");
    centralityName += Form("%.0f", centrMin);
@@ -25,20 +35,27 @@ AliAnalysisTaskPhiFlow* AddTaskPhiFlow( Float_t centrMin = 30.,
    centralityName += "_";
    centralityName += Form("bayP%.1f", bayesThresholdP);
    centralityName += "_";
-   centralityName += "_EtaA";
-   centralityName += Form("%.1f", minEtaA);
+   centralityName += "_POIEta";
+   centralityName += Form("%.1f", POIEtaMin);
    centralityName += "-";
-   centralityName += Form("%.1f", maxEtaA);
-   centralityName += "_EtaB";
-   centralityName += Form("%.1f", minEtaB);
+   centralityName += Form("%.1f", POIEtaMax);
+   centralityName += "_RPEta";
+   centralityName += Form("%.1f", RPEtaMinA);
    centralityName += "-";
-   centralityName += Form("%.1f", maxEtaB);
+   centralityName += Form("%.1f", RPEtaMaxA);
+   centralityName += "-";
+   centralityName += Form("%.1f", RPEtaMinB);
+   centralityName += "-";
+   centralityName += Form("%.1f", RPEtaMaxB);
    centralityName += "-";
    centralityName += Form("dDip%.f", deltaDip);
    centralityName += "-";
    centralityName += Form("dDipPt%.f", deltaDipMaxPt);
-   centralityName += "-";
-   centralityName += Form("epPtCut%.f", eventPlanePtCut);
+   if (strictKaonCuts)
+   {
+      centralityName += "-";
+      centralityName += "StrictKaonCuts";
+   }
 
    TString fileName = AliAnalysisManager::GetCommonFileName();
    fileName += ":PhiV2FlowEvents";
@@ -68,17 +85,8 @@ AliAnalysisTaskPhiFlow* AddTaskPhiFlow( Float_t centrMin = 30.,
    AliFlowTrackCuts* cutsPOI = new AliFlowTrackCuts("GlobalPOI");
    cutsPOI->GetStandardGlobalTrackCuts2010();
 
-   // set and configure kaon track cuts (for the bayesian PID method)
-   // select this method by setting PIDtype to kCombined
-   AliFlowTrackCuts* cutsKaon = new AliFlowTrackCuts("kaon_cuts");
-   AliFlowTrackCuts::PIDsource pid = AliFlowTrackCuts::kTPCbayesian;
-   cutsKaon->SetPID(AliPID::kKaon, pid, bayesThresholdP);
-   cutsKaon->SetAllowTOFmismatchFlag(kTRUE);
-   cutsKaon->SetRequireStrictTOFTPCagreement(kFALSE);
-
    // set some more cuts ...
-   task->EventPlanePtCut(kTRUE); // set to true to enforce pt cut on event plane tracks
-   task->SetEventPlanePtCut(eventPlanePtCut); // set the upper value for Pt
+   task->EventPlanePtCut(kFALSE); // set to true to enforce pt cut on event plane tracks
    task->SetMaxDeltaDipAngleAndPt(deltaDip, deltaDipMaxPt); // set an upper value for d-dip angle and pt of tracks on which this should be applied
    task->SetCentralityParameters(centrMin, centrMax, "V0M");
    task->SetVertexZ(vertexZ);
@@ -86,8 +94,11 @@ AliAnalysisTaskPhiFlow* AddTaskPhiFlow( Float_t centrMin = 30.,
    task->SetPOICuts(cutsPOI);
 
    // set the kaon cuts, and specify the PID procedure which will be used
-   task->SetKaonCuts(cutsKaon);
    task->SetIdentificationType(pidtype);
+   task->SetBayesianProbability(bayesThresholdP);
+   if (strictKaonCuts) task->SetRequireStrictKaonCuts();
+   if (TPCStandAloneTracks) task->SetRequireTPCStandAloneKaons();
+   task->SetCandidateEtaAndPt(POIEtaMin, POIEtaMax, POIPtMin, POIPtMax);
 
    //set RP cuts for flow package analysis
    cutsFlowRP = new AliFlowTrackCuts("rp_cuts_111");
@@ -111,7 +122,7 @@ AliAnalysisTaskPhiFlow* AddTaskPhiFlow( Float_t centrMin = 30.,
       flowBands[1][i] = 0.99 + (i + 1) * 0.0034;
    }
    task->SetMassRanges(flowBands);
-   task->SetEtaRanges(minEtaA, maxEtaA, minEtaB, maxEtaB);
+   task->SetEtaRanges(RPEtaMinA, RPEtaMaxA, RPEtaMinB, RPEtaMaxB);
    task->SetFlowRPCuts(cutsFlowRP);
    mgr->AddTask(task);
 
@@ -128,37 +139,62 @@ AliAnalysisTaskPhiFlow* AddTaskPhiFlow( Float_t centrMin = 30.,
       mgr->ConnectOutput(task, 2 + r, coutputCandidates[r]);
    }
 
-   // Scalar Product
-   AliAnalysisTaskScalarProduct *taskSP[30];
-   AliAnalysisDataContainer *coutputSP[30];
-   for (int r = 0; r != 30; ++r)
+
+   if (SP) // set up scalar product (SP) tasks
    {
-      taskSP[r] = new AliAnalysisTaskScalarProduct(Form("SP_MassBand_%d", r), kFALSE);
-      taskSP[r]->SetRelDiffMsub(1.0);
-      taskSP[r]->SetApplyCorrectionForNUA(kTRUE);
-      mgr->AddTask(taskSP[r]);
-      coutputSP[r] = mgr->CreateContainer(Form("cobjSP_MassBand_%d", r), TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
-      mgr->ConnectInput(taskSP[r], 0, coutputCandidates[r]);
-      mgr->ConnectOutput(taskSP[r], 1, coutputSP[r]);
-   }
-   // Q-Cumulants
-   AliAnalysisTaskQCumulants *taskQC[30];
-   AliAnalysisDataContainer *coutputQC[30];
-   for (int r = 0; r != 30; ++r)
-   {
-      taskQC[r] = new AliAnalysisTaskQCumulants(Form("QC_MassBand_%d", r), kFALSE);
-      taskQC[r]->SetCalculateCumulantsVsM(kFALSE);
-      taskQC[r]->SetnBinsMult(10000);
-      taskQC[r]->SetMinMult(0.);
-      taskQC[r]->SetMaxMult(10000.);
-      taskQC[r]->SetApplyCorrectionForNUA(kTRUE);
-      taskQC[r]->SetFillMultipleControlHistograms(kFALSE);
-      mgr->AddTask(taskQC[r]);
-      coutputQC[r] = mgr->CreateContainer(Form("cobjQC_MassBand_%d", r), TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
-      mgr->ConnectInput(taskQC[r], 0, coutputCandidates[r]);
-      mgr->ConnectOutput(taskQC[r], 1, coutputQC[r]);
+      AliAnalysisTaskScalarProduct *taskSP[30];
+      AliAnalysisDataContainer *coutputSP[30];
+      for (int r = 0; r != 30; ++r)
+      {
+         taskSP[r] = new AliAnalysisTaskScalarProduct(Form("SP_MassBand_%d", r), kFALSE);
+         taskSP[r]->SetHarmonic(harmonic);
+         taskSP[r]->SetRelDiffMsub(1.0);
+         taskSP[r]->SetApplyCorrectionForNUA(kTRUE);
+         mgr->AddTask(taskSP[r]);
+         coutputSP[r] = mgr->CreateContainer(Form("cobjSP_MassBand_%d", r), TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
+         mgr->ConnectInput(taskSP[r], 0, coutputCandidates[r]);
+         mgr->ConnectOutput(taskSP[r], 1, coutputSP[r]);
+      }
    }
 
+   if (EP) // set up event plane (ep) tasks
+   {
+      AliAnalysisTaskScalarProduct *taskEP[30];
+      AliAnalysisDataContainer *coutputEP[30];
+      for (int r = 0; r != 30; ++r)
+      {
+         taskEP[r] = new AliAnalysisTaskScalarProduct(Form("EP_MassBand_%d", r), kFALSE);
+         taskEP[r]->SetBehaveAsEP();
+         taskEP[r]->SetHarmonic(harmonic);
+         taskEP[r]->SetRelDiffMsub(1.0);
+         taskEP[r]->SetApplyCorrectionForNUA(kTRUE);
+         mgr->AddTask(taskEP[r]);
+         coutputEP[r] = mgr->CreateContainer(Form("cobjEP_MassBand_%d", r), TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
+         mgr->ConnectInput(taskEP[r], 0, coutputCandidates[r]);
+         mgr->ConnectOutput(taskEP[r], 1, coutputEP[r]);
+      }
+   }
+
+   if (QC) // set up q-cumulants task
+   {
+      AliAnalysisTaskQCumulants *taskQC[30];
+      AliAnalysisDataContainer *coutputQC[30];
+      for (int r = 0; r != 30; ++r)
+      {
+         taskQC[r] = new AliAnalysisTaskQCumulants(Form("QC_MassBand_%d", r), kFALSE);
+         taskQC[r]->SetHarmonic(harmonic);
+         taskQC[r]->SetCalculateCumulantsVsM(kFALSE);
+         taskQC[r]->SetnBinsMult(10000);
+         taskQC[r]->SetMinMult(0.);
+         taskQC[r]->SetMaxMult(10000.);
+         taskQC[r]->SetApplyCorrectionForNUA(kTRUE);
+         taskQC[r]->SetFillMultipleControlHistograms(kFALSE);
+         mgr->AddTask(taskQC[r]);
+         coutputQC[r] = mgr->CreateContainer(Form("cobjQC_MassBand_%d", r), TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
+         mgr->ConnectInput(taskQC[r], 0, coutputCandidates[r]);
+         mgr->ConnectOutput(taskQC[r], 1, coutputQC[r]);
+      }
+   }
    mgr->ConnectInput(task, 0, cinput);
    mgr->ConnectOutput(task, 1, coutput);
 
