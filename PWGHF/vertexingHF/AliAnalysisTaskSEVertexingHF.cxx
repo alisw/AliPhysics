867cd0813737a88cdc3093cb31be7f1a033eff8d
prino <prino@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-07 22:43:23 +0000

    Fix PWG3->PWGHF (Renu)

--- PWGHF/vertexingHF/AliAnalysisTaskSEVertexingHF.cxx
+++ PWGHF/vertexingHF/AliAnalysisTaskSEVertexingHF.cxx
@@ -108,8 +108,8 @@ void AliAnalysisTaskSEVertexingHF::Init()
   if(fDebug > 1) printf("AnalysisTaskSEVertexingHF::Init() \n");
 
   if(gROOT->LoadMacro("ConfigVertexingHF.C")) {
-    printf("AnalysisTaskSEVertexingHF::Init() \n Using $ALICE_ROOT/PWG3/vertexingHF/ConfigVertexingHF.C\n");
-    gROOT->LoadMacro("$ALICE_ROOT/PWG3/vertexingHF/ConfigVertexingHF.C");
+    printf("AnalysisTaskSEVertexingHF::Init() \n Using $ALICE_ROOT/PWGHF/vertexingHF/ConfigVertexingHF.C\n");
+    gROOT->LoadMacro("$ALICE_ROOT/PWGHF/vertexingHF/ConfigVertexingHF.C");
   }
 
   fVHF = (AliAnalysisVertexingHF*)gROOT->ProcessLine("ConfigVertexingHF()");  
