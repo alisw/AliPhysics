210534ae50fe473a9c0afa7aca6b629004f4e284
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-13 15:05:21 +0000

    Changes for #92466 port to Release AliT0AnalysisTaskQA. From rev. 55075

--- PWGPP/T0/AliT0AnalysisTaskQA.h
+++ PWGPP/T0/AliT0AnalysisTaskQA.h
@@ -2,18 +2,17 @@
 #define AliT0AnalysisTaskQA_cxx
 
 // task determines mean and sigma of T0 signals  ORA, ORC, ORA-ORC, ORA+ORC/2  
-// Authors: FK  
+// Authors: FK  last change 23 Feb 2012 
 
-#define NPMT0 24  //number T0 of photomultipliers
+#define kNPMT0 24  //number T0 of photomultipliers
 
 class TH1F;
 class TObjArray; 
 class AliESDEvent;
 class TH2F;
+class AliESDpid;
 
-#ifndef ALIANALYSISTASKSE_H
 #include "AliAnalysisTaskSE.h"
-#endif
 
 class AliT0AnalysisTaskQA : public AliAnalysisTaskSE {
  public:
@@ -33,23 +32,22 @@ class AliT0AnalysisTaskQA : public AliAnalysisTaskSE {
   TH1F        *fTzeroORC;     //! or C spectrum    
   TH1F        *fResolution;   //! or A minus or C spectrum    
   TH1F        *fTzeroORAplusORC; //! ORA+ORC /2 
+  TH1F        *fTzeroTof;     //! start T0 time from TOF 
   int         fRunNumber;
   TH2F        **fTimeVSAmplitude; //! Time vs. Amplitude
   TH2F        *fCFDVSPmtId;   //! CFDi vs pmt id
   TH2F        *fSPDVertexVST0Vertex; //! SPD vertex vs T0 vertex   
   TH2F        *fOrAvsNtracks; //! T0A vs Ntracks
   TH2F        *fOrCvsNtracks; //! T0C vs Ntracks
-  TH2F        *fT0vsNtracks;  //! T0A vs Ntracks
-  TH1F        *fEffAC;        //T0AC eff
-  TH1F        *fEffA;         //T0A eff
-  TH1F        *fEffC;         //T0C eff
-  TH1F        *ftracksEffSPD;  // ntracks in SPD 
-  
+  TH2F        *fT0vsNtracks; //! T0A vs Ntracks
+  TH2F        *fT0TimevsT0Tof; //! T0 time vs T0 TOF time 
+
+  AliESDpid* fESDpid;  //! esd pid 
  
   AliT0AnalysisTaskQA(const AliT0AnalysisTaskQA&); // not implemented
   AliT0AnalysisTaskQA& operator=(const AliT0AnalysisTaskQA&); // not implemented
   
-  ClassDef(AliT0AnalysisTaskQA, 1); // example of analysis
+  ClassDef(AliT0AnalysisTaskQA, 2); // example of analysis
 };
 
 #endif
