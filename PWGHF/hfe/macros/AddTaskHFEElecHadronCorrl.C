9e8ccbfef36e1fb6bf71409cd58bbabd9f422ea7
ssakai <ssakai@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-18 11:14:03 +0000

    new add task for PbPb

---
+++ PWGHF/hfe/macros/AddTaskHFEElecHadronCorrl.C
@@ -0,0 +1,41 @@
+AliAnalysisTask AddTaskHFEElecHadronCorrl()
+{
+  //get the current analysis manager
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    Error("AddTaskHFEElecHadronCorrl", "No analysis manager found.");
+    return 0;
+  }
+
+  if (!mgr->GetInputEventHandler()) {
+    ::Error("AddTaskHFEElecHadronCorrl", "This task requires an input event handler");
+    return NULL;
+  }
+  TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+  if (type=="AOD"){
+    ::Error("AddTaskHFEElecHadronCorrl", "The tasks exits because AODs are in input");
+    return NULL;
+  }
+  Bool_t MCthere=kTRUE;
+  AliMCEventHandler *mcH = dynamic_cast<AliMCEventHandler*>(mgr->GetMCtruthEventHandler());
+  if(!mcH){
+    MCthere=kFALSE;
+  }
+
+  //analysis task 
+  gROOT->LoadMacro("AliAnalysisTaskElecHadronCorrel2.cxx++g");
+  //gROOT->LoadMacro("ConfigHFEemcalMod.C");
+  gROOT->LoadMacro("$ALICE_ROOT/PWGHF/hfe/macros/configs/PbPb/ConfigHFEElecHadronCorl.C");
+  AliAnalysisTaskElecHadronCorrel2 *taskHFE = ConfigHFEemcalMod(MCthere);
+
+  // output list of histos
+  TString foutputName = "ElecHadronCorrelAna.root";
+  AliAnalysisDataContainer *coutput3 = mgr->CreateContainer("ccontainer0",TList::Class(),AliAnalysisManager::kOutputContainer,foutputName.Data());
+
+  mgr->ConnectInput(taskHFE,0,mgr->GetCommonInputContainer());
+  mgr->ConnectOutput(taskHFE,1,coutput3);
+
+  mgr->AddTask(taskHFE);
+
+  return taskHFE;
+}
