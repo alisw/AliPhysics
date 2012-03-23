e6c62e1262ff8a961ca3201ae486b1e094ff636e
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-23 15:45:39 +0000

    Adding test directory
    mergeMakeOCDBUser.sh -
    Shell script for testing of the mergeMakeOCDB on the grid for user purposes
                          
    
    
    ine, and those below, will be ignored--
    
    A    test
    AM   test/mergeMakeOCDBUser.sh

---
+++ PWGPP/CalibMacros/Pass0/test/mergeMakeOCDBUser.sh
@@ -0,0 +1,63 @@
+# Script for testing of the mergemakeOCDB for user purposes
+# To be used in case the standard production failed or was not automatically validated
+# Output objects are writn to the predefined path:
+
+# 
+# Parameters for macro:
+# 1  -   alien user dir name                 - e.g  /m/miranov/
+# 2  -   input directory with data to merge  - e.g /alice/data/2011/LHC11h/000169926/cpass0_HLT/
+# 3  -   run number                          - e.g run number 169926
+# 4  -   OCDB output path                    - alien://folder=/alice/cern.ch/user/m/miranov/Pass0/169926
+# Example:
+# $ALICE_ROOT/PWGPP/CalibMacros/Pass0/test/mergeMakeOCDBUser.sh /m/miranov/ /alice/data/2011/LHC11h/000169926/cpass0_HLT/  169926 alien://folder=/alice/cern.ch/user/m/miranov/Pass0/169926
+#
+# authors:   marian.ivanov#cern.ch, mikolaj.krzewicki@cern.ch 
+
+AlienName=$1
+InputDataDir=$2
+Run=$3
+OCDBPath=$4
+InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/Pass0/
+
+echo xxxxxxxxxxxxxxxxxxxxxxxxxx
+echo SETUP
+echo AlienName=$1
+echo InputDataDir=$2
+echo Run=$3
+echo OCDBPath=$4
+echo InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/Pass0/
+echo xxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#
+# 1. copy macroses and sh to the predefiend alien directory
+#
+OutputMacros=`echo /alice/cern.ch/user/j/jotwinow/Pass0/MergeCalibration/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
+alien_mkdir  $OutputMacros
+
+for lfile in `ls $InputMacros/{*C,*sh} | grep -v AddTask`; do
+    bname=`basename $lfile`  
+    echo  Copping alien_cp $lfile alien://$OutputMacros/$bname 
+    alien_rm   $OutputMacros/$bname   
+    alien_cp   $lfile alien://$OutputMacros/$bname 
+done
+#
+# 2. Copy shell script and jdl
+#
+OutputBin=`echo  /alice/cern.ch/user/j/jotwinow/bin/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
+alien_rm /$OutputBin/mergeMakeOCDB.sh
+echo alien_cp $InputMacros/mergeMakeOCDB.sh  alien://$OutputBin/mergeMakeOCDB.sh
+alien_cp $InputMacros/mergeMakeOCDB.sh  alien://$OutputBin/mergeMakeOCDB.sh
+#
+# 3. Copy validation switch off return value - job will alway finish
+#
+cat $InputMacros/validationMerging.sh |  sed "s_exit \$error_exit 0_" > validationMerging.sh
+alien_rm  $OutputMacros/validationMerging.sh
+alien_cp  validationMerging.sh  alien:///$OutputMacros/validationMerging.sh
+
+#
+# 4. Submit job
+#
+echo alien_submit alien:///$OutputMacros/mergeMakeOCDB.jdl $InputDataDir $Run $OCDBPath
+alien_submit $OutputMacros/mergeMakeOCDB.jdl $InputDataDir $Run $OCDBPath &
+echo Alien job submitted $!
+return $!
