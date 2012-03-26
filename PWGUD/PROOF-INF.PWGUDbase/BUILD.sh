7971d50add6d4b269ed2f935ad41a56d7fc5aa7d
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-26 13:15:36 +0000

    Fixes for #90303: Fix PROOF-INF/SETUP.C to create PAR files for AliRoot trunk

--- PWGUD/PROOF-INF.PWGUDbase/BUILD.sh
+++ PWGUD/PROOF-INF.PWGUDbase/BUILD.sh
@@ -4,4 +4,15 @@
 # touch libPWGUDselectors.pkg
 # touch libPWGUDdep.pkg
 
+if [ "" = "clean" ]; then
+   make clean
+   exit 0
+fi
+
 make libPWGUDbase.so
+rc=$?
+echo "rc=$?"
+if [ $? != "0" ] ; then
+   exit 1
+fi
+exit 0
