3b4e8cd279fe0b38bf1298060cd67b21f0dc5bad
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-28 11:55:28 +0000

    test/runCPass0User.sh test/mergeMakeOCDBUser.sh   - moving Pass0 -> CPass0 mergeMakeOCDB.jdl           - moving Pass0 -> CPass0 CPass0.jdl                  - moving Pass0 -> CPass0

--- PWGPP/CalibMacros/CPass0/test/mergeMakeOCDBUser.sh
+++ PWGPP/CalibMacros/CPass0/test/mergeMakeOCDBUser.sh
@@ -7,11 +7,11 @@
 # 1  -   alien user dir name                 - e.g  /m/miranov/
 # 2  -   input directory with data to merge  - e.g /alice/data/2011/LHC11h/000169926/cpass0_HLT/
 # 3  -   run number                          - e.g run number 169926
-# 4  -   OCDB output path                    - alien://folder=/alice/cern.ch/user/m/miranov/Pass0/169926
+# 4  -   OCDB output path                    - alien://folder=/alice/cern.ch/user/m/miranov/CPass0/169926
 # Example:
 # run=170572
 # AlienName=/m/miranov/
-# $ALICE_ROOT/PWGPP/CalibMacros/Pass0/test/mergeMakeOCDBUser.sh $AlienName /alice/data/2011/LHC11h/000$Run/cpass0_HLT/  $Run alien://folder=/alice/cern.ch/user/m/miranov/Pass0/$Run
+# $ALICE_ROOT/PWGPP/CalibMacros/CPass0/test/mergeMakeOCDBUser.sh $AlienName /alice/data/2011/LHC11h/000$Run/cpass0_HLT/  $Run alien://folder=/alice/cern.ch/user/m/miranov/CPass0/$Run
 #
 # authors:   marian.ivanov#cern.ch, mikolaj.krzewicki@cern.ch 
 
@@ -19,7 +19,7 @@ AlienName=$1
 InputDataDir=$2
 Run=$3
 OCDBPath=$4
-InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/Pass0/
+InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/CPass0/
 
 echo xxxxxxxxxxxxxxxxxxxxxxxxxx
 echo SETUP
@@ -27,14 +27,14 @@ echo AlienName=$1
 echo InputDataDir=$2
 echo Run=$3
 echo OCDBPath=$4
-echo InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/Pass0/
+echo InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/CPass0/
 echo xxxxxxxxxxxxxxxxxxxxxxxxxx
 
 #
 # 1. copy macroses and sh to the predefiend alien directory
 #
-OutputMacros=`echo /alice/cern.ch/user/j/jotwinow/Pass0/MergeCalibration/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
-alien_mkdir  $OutputMacros
+OutputMacros=`echo /alice/cern.ch/user/j/jotwinow/CPass0/MergeCalibration/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
+alien_mkdir -p  $OutputMacros
 
 for lfile in `ls $InputMacros/{*C,*sh,*jdl} | grep -v AddTask`; do
     bname=`basename $lfile`  
