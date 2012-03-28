0ea7d1917d09bae71e37ac07a28a041a8afd4d0c
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-28 11:08:46 +0000

    1. Renaming Pass0 --> CPass0 2. Renaming PassX --> CPass1
    3. User defined macros to submit alien test jobs
          CPass0/test/runPass0User.sh
          CPass0/test/mergeMakeOCDBUser.sh
    
    4.  ConfigCalibTrain.C and  recPass0.C
          Optional usage of the OCDB snapshot
    
    5.  CPass0/AddTaskTPCCalib.C
          Adding the component for cluster errors
    
    6.  CPass0/runPass0.sh 
          Additional options for  local test

---
+++ PWGPP/CalibMacros/CPass0/ConfigCalibTrain.C
@@ -0,0 +1,42 @@
+/*
+
+ Macro to initialize: 
+ - the OCDB (run number required as input argument)
+ - the geometry (expected to be in the current directory)
+ to run the Calibration train.
+ 
+ Example:
+ .L $ALICE_ROOT/ANALYSIS/CalibMacros/MergeCalibration/ConfigCalibTrain.C
+ ConfigCalibTrain(129160,"raw://");
+
+*/
+
+void ConfigCalibTrain(Int_t run, const char *ocdb="raw://"){
+
+  // OCDB
+  printf("setting run to %d\n",run);
+  AliCDBManager::Instance()->SetDefaultStorage(ocdb);
+  AliCDBManager::Instance()->SetRun(run); 
+  if (gSystem->AccessPathName("OCDB.root", kFileExists)==0) {  
+    AliCDBManager::Instance()->InitFromSnapshot("OCDB.root");
+  }
+  // magnetic field
+  if ( !TGeoGlobalMagField::Instance()->GetField() ) {
+    printf("Loading field map...\n");
+    AliGRPManager grpMan;
+    if( !grpMan.ReadGRPEntry() ) { 
+      printf("Cannot get GRP entry\n"); 
+    }
+    if( !grpMan.SetMagField() ) { 
+      printf("Problem with magnetic field setup\n"); 
+    }
+  }
+
+  // geometry
+  printf("Loading geometry...\n");
+  AliGeomManager::LoadGeometry();
+  if( !AliGeomManager::ApplyAlignObjsFromCDB("GRP ITS TPC") ) {
+    printf("Problem with align objects\n"); 
+  }
+
+}
---
+++ PWGPP/CalibMacros/CPass0/runPass0.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Script to run:
+#    1. reconstruction
+#    2. calibration and friend track filtering
+#
+# Files assumed to be in working directory:
+# recPass0.C          - reconstruction macro
+# runCalibTrain.C     - calibration/filtering macro
+# Arguments (run locally):
+#    1  - raw data file name
+#    2  - number of events to be processed
+#    3  - run number 
+
+# example:
+# runPass0.sh raw.root  50  104892
+
+#ALIEN setting
+# $1 = raw input filename
+runNum=`echo $1 | cut -d "/" -f 6`
+if [ $# -eq 1 ] ; then
+  # alien Setup
+  nEvents=99999999
+  fileName="alien://"$1
+  ocdbPath="raw://"
+fi;
+if [ $# -eq 4 ] ; then
+  # local setup
+  nEvents=$2
+  runNum=$3
+  fileName=$1
+  ocdbPath=$4
+fi
+
+echo xxxxxxxxxxxxxxxxxxxxxxxxxxx
+echo runPass0.sh Input arguments
+echo fileName=$fileName
+echo nEvents=$nEvents
+echo runNum=$runNum
+echo ocdbPath=$ocdbPath
+echo xxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+if [ -f Run0_999999999_v3_s0.root ]; then
+    mkdir -p TPC/Calib/Correction
+    mv Run0_999999999_v3_s0.root TPC/Calib/Correction/
+fi
+
+
+
+echo File to be  processed $1
+echo Number of events to be processed $nEvents
+
+echo ">>>>>>>>> PATH is..."
+echo $PATH
+echo ">>>>>>>>> LD_LIBRARY_PATH is..."
+echo $LD_LIBRARY_PATH
+echo ">>>>>>>>> recPass0.C is..."
+#cat recPass0.C
+echo
+
+echo ">>>>>>> Running AliRoot to reconstruct $1. Run number is $runNum..."
+
+aliroot -l -b -q recPass0.C\(\""$fileName\", $nEvents, \"$ocdbPath"\"\) 2>&1 | tee rec.log
+mv syswatch.log syswatch_rec.log
+
+echo ">>>>>>> Running AliRoot to make calibration..."
+aliroot -l -b -q  runCalibTrain.C\(\""$runNum\",\"AliESDs.root\",\"$ocdbPath"\"\)   2>&1 | tee calib.log
+mv syswatch.log syswatch_calib.log
