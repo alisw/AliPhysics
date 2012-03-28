3836d7afc615ef1ff87f698310c1d9a026657a3a
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-28 10:55:17 +0000

    New event header version. Has to be ported to the releases.

--- STEER/STEER/AliGRPPreprocessor.cxx
+++ STEER/STEER/AliGRPPreprocessor.cxx
@@ -1554,6 +1554,7 @@ UInt_t AliGRPPreprocessor::ProcessDaqFxs()
 	AliRawEventHeaderV3_9::Class()->IgnoreTObjectStreamer(); // to avoid trying reading TObject store in AliRawEventHeaderV3_9 - temporary fix 
 	AliRawEventHeaderV3_11::Class()->IgnoreTObjectStreamer(); // to avoid trying reading TObject store in AliRawEventHeaderV3_11 - temporary fix 
 	AliRawEventHeaderV3_12::Class()->IgnoreTObjectStreamer(); // to avoid trying reading TObject store in AliRawEventHeaderV3_12 - temporary fix 
+	AliRawEventHeaderV3_13::Class()->IgnoreTObjectStreamer(); // to avoid trying reading TObject store in AliRawEventHeaderV3_13 - temporary fix 
 	Log("Processing DAQ FXS");
 	TList* list = GetFileSources(kDAQ);  	
 	if (!list) {
