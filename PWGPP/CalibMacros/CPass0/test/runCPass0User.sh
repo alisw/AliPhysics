5099576e0a516975879ed7560d37b4f3b5bbcb7d
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-28 11:32:19 +0000

    Rename all of macros/jdl from  *Pass0* CPass0

--- PWGPP/CalibMacros/CPass0/test/runCPass0User.sh
+++ PWGPP/CalibMacros/CPass0/test/runCPass0User.sh
@@ -17,21 +17,21 @@
 AlienName=$1
 RawPath=$2
 Run=$3
-InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/Pass0/
+InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/CPass0/
 
 echo xxxxxxxxxxxxxxxxxxxxxxxxxx
 echo SETUP
 echo AlienName=$1
 echo RawPath=$2
 echo Run=$3 
-echo InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/Pass0/
+echo InputMacros=$ALICE_ROOT/PWGPP/CalibMacros/CPass0/
 echo xxxxxxxxxxxxxxxxxxxxxxxxxx
 
 #
 # 1. copy macroses and sh to the predefiend alien directory
 #
-OutputMacros=`echo /alice/cern.ch/user/j/jotwinow/Pass0/CalibMacros/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
-alien_mkdir  $OutputMacros
+OutputMacros=`echo /alice/cern.ch/user/j/jotwinow/CPass0/CalibMacros/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
+alien_mkdir -p $OutputMacros
 
 for lfile in `ls $InputMacros/{*C,*sh} `; do
     bname=`basename $lfile`  
@@ -44,11 +44,11 @@ done
 # 2. Copy shell script and jdl
 #
 OutputBin=`echo  /alice/cern.ch/user/j/jotwinow/bin/ | sed s_\/j\/jotwinow\/_$AlienName\_ `
-echo alien_cp -n $InputMacros/runPass0.sh  alien://$OutputBin/runPass0.sh
-alien_cp -n  $InputMacros/runPass0.sh   alien://$OutputBin/runPass0.sh
-cat $InputMacros/Pass0.jdl | sed "s_/j/jotwinow/_${AlienName}_g" | sed "s_/alice/data/2010/LHC10d/_${RawPath}_g" > Pass0.jdl
-echo alien_cp -n Pass0.jdl alien://$OutputMacros/Pass0.jdl
-alien_cp -n Pass0.jdl alien://$OutputMacros/Pass0.jdl
+echo alien_cp -n $InputMacros/runCPass0.sh  alien://$OutputBin/runCPass0.sh
+alien_cp -n  $InputMacros/runCPass0.sh   alien://$OutputBin/runCPass0.sh
+cat $InputMacros/CPass0.jdl | sed "s_/j/jotwinow/_${AlienName}_g" | sed "s_/alice/data/2010/LHC10d/_${RawPath}_g" > CPass0.jdl
+echo alien_cp -n CPass0.jdl alien://$OutputMacros/CPass0.jdl
+alien_cp -n CPass0.jdl alien://$OutputMacros/CPass0.jdl
 
 #
 # 3. Copy validation switch off return value - job will alway finish
@@ -59,7 +59,7 @@ alien_cp  -n validation.sh  alien:///$OutputMacros/validation.sh
 #
 # 4. Submit job
 #
-echo nohup alien_submit alien:///$OutputMacros/Pass0.jdl $Run  >submitJob$Run.txt
-#nohup alien_submit alien:///$OutputMacros/Pass0.jdl $Run  >submitJob$Run.txt
+echo nohup alien_submit alien:///$OutputMacros/CPass0.jdl $Run  >submitJob$Run.txt
+#nohup alien_submit alien:///$OutputMacros/CPass0.jdl $Run  >submitJob$Run.txt
 #echo Alien job submitted $!
 
