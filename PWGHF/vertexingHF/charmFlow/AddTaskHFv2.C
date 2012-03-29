2d238b64bbb92622592f3f14bb14b6f98cd765d4
prino <prino@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-03-29 14:56:30 +0000

    Modification for lego train

--- PWGHF/vertexingHF/charmFlow/AddTaskHFv2.C
+++ PWGHF/vertexingHF/charmFlow/AddTaskHFv2.C
@@ -1,4 +1,4 @@
-AliAnalysisTaskSEHFv2 *AddTaskHFv2(TString filename="DplustoKpipiCutsPbPb.root",AliAnalysisTaskSEHFv2::DecChannel decCh=AliAnalysisTaskSEHFv2::kDplustoKpipi,TString cutsobjname="AnalysisCuts", Bool_t readMC=kFALSE,TString name="",Int_t flagep=0 /*0=tracks,1=V0*/)
+AliAnalysisTaskSEHFv2 *AddTaskHFv2(TString filename="DplustoKpipiCutsPbPb.root",AliAnalysisTaskSEHFv2::DecChannel decCh=AliAnalysisTaskSEHFv2::kDplustoKpipi,TString cutsobjname="AnalysisCuts", Bool_t readMC=kFALSE, TString suffix="", Int_t flagep=0 /*0=tracks,1=V0*/)
 {
   //
   // Test macro for the AliAnalysisTaskSE for  D 
@@ -15,54 +15,67 @@ AliAnalysisTaskSEHFv2 *AddTaskHFv2(TString filename="DplustoKpipiCutsPbPb.root",
     ::Error("AddTaskHFv2", "No analysis manager to connect to.");
     return NULL;
   }
+
   Bool_t stdcuts=kFALSE;
-  TFile* filecuts=new TFile(filename.Data());
-  if(!filecuts->IsOpen()){
-    cout<<"Input file not found:  using std cut object"<<endl;
-    stdcuts=kTRUE;
+  TFile* filecuts;
+  if( filename.EqualTo("") ) {
+    stdcuts=kTRUE; 
+  } else {
+    filecuts=TFile::Open(filename.Data());
+    if(!filecuts ||(filecuts&& !filecuts->IsOpen())){
+      AliFatal("Input file not found : check your cut object");
+    }
   }
   
   AliRDHFCuts *analysiscuts=0x0;
-  TString suffix="";
-
+  Int_t pdgmes=-1;
   //Analysis cuts
-  if(decCh==AliAnalysisTaskSEHFv2::kDplustoKpipi){
-    analysiscuts = (AliRDHFCutsDplustoKpipi*)filecuts->Get(cutsobjname);
-  }else if(decCh==AliAnalysisTaskSEHFv2::kD0toKpi){
-    analysiscuts = (AliRDHFCutsD0toKpi*)filecuts->Get(cutsobjname);
-  }else if(decCh==AliAnalysisTaskSEHFv2::kDstartoKpipi){
-    analysiscuts = (AliRDHFCutsDStartoKpipi*)filecuts->Get(cutsobjname);
-  }
 
+  if(decCh==AliAnalysisTaskSEHFv2::kDplustoKpipi){
+    if(stdcuts){
+      analysiscuts = new AliRDHFCutsDplustoKpipi();
+      analysiscuts->SetStandardCutsPbPb2011();
+    }else  analysiscuts = (AliRDHFCutsDplustoKpipi*)filecuts->Get(cutsobjname);
+    suffix.Prepend("Dplus");
+    pdgmes=411;
+  }else if(decCh==AliAnalysisTaskSEHFv2::kD0toKpi){
+    if(stdcuts) {
+      analysiscuts = new AliRDHFCutsD0toKpi();
+      analysiscuts->SetStandardCutsPbPb2011();
+    }
+    else analysiscuts = (AliRDHFCutsD0toKpi*)filecuts->Get(cutsobjname);
+    suffix.Prepend("Dzero");
+    pdgmes=421;
+  }else if(decCh==AliAnalysisTaskSEHFv2::kDstartoKpipi){
+    if(stdcuts) {
+      analysiscuts = new AliRDHFCutsDStartoKpipi();
+      analysiscuts->SetStandardCutsPbPb2011();
+    }
+    else analysiscuts = (AliRDHFCutsDStartoKpipi*)filecuts->Get(cutsobjname);
+    suffix.Prepend("Dstar");
+    pdgmes=413;
+  }
+  if(pdgmes==-1){
+    AliFatal("Wrong meson setting");
+  }
   if(!analysiscuts){
     AliFatal("Specific AliRDHFCuts not found");
   }
 
-  analysiscuts->SetUseCentrality(AliRDHFCuts::kCentV0M);
-  analysiscuts->SetRemoveDaughtersFromPrim(kFALSE);
-  analysiscuts->SetTriggerClass("");
-  analysiscuts->ResetMaskAndEnableMBTrigger();
-  analysiscuts->EnableCentralTrigger();
-  analysiscuts->EnableSemiCentralTrigger();
-  analysiscuts->SetMinCentrality(30.);
-  analysiscuts->SetMaxCentrality(80.);
-  ((AliRDHFCutsDplustoKpipi*)analysiscuts)->SetMinPtCandidate(3.);
-  ((AliRDHFCutsDplustoKpipi*)analysiscuts)->SetUseImpParProdCorrCut(kFALSE);
-  AliAODPidHF *pid = analysiscuts->GetPidHF();
-  pid->SetOldPid(kFALSE);
-  analysiscuts->SetPidHF(pid);
   // Analysis task    
   AliAnalysisTaskSEHFv2 *v2Task = new AliAnalysisTaskSEHFv2("HFv2Analysis",analysiscuts,decCh);
-  v2Task->SetReadMC(kFALSE);
+  v2Task->SetReadMC(readMC);
   v2Task->SetEtaGapFeatureForEventplaneFromTracks(kFALSE);
   v2Task->SetNMassBins(104);
-  v2Task->SetMassLimits(0.2,411);
+  v2Task->SetMassLimits(0.2,pdgmes);
   v2Task->SetDebugLevel(0);
   v2Task->SetV0EventPlaneOrder(2);
   if(flagep==0){
     v2Task->SetTPCEP();//SetVZEROEPOnly();
+    suffix+="TPC";
   }else{
     v2Task->SetVZEROEP();
+    suffix+="VZERO";
   }
   mgr->AddTask(v2Task);
 
@@ -72,16 +85,16 @@ AliAnalysisTaskSEHFv2 *AddTaskHFv2(TString filename="DplustoKpipiCutsPbPb.root",
   AliAnalysisDataContainer *cinputv2 = mgr->CreateContainer(contname.Data(),TChain::Class(),AliAnalysisManager::kInputContainer);
 
   TString outputfile = AliAnalysisManager::GetCommonFileName();
-  TString outputhistos = outputfile += ":PWG3_D2H_HFv2"; 
+  TString outputhistos = Form("%s:PWGHF_D2H_HFv2_%s",outputfile.Data(),suffix.Data()); 
 
   contname=Form("hEventsInfo%s",suffix.Data());
-  AliAnalysisDataContainer *coutputstat = mgr->CreateContainer(contname.Data(),TH1F::Class(),AliAnalysisManager::kOutputContainer,outputfile.Data());
+  AliAnalysisDataContainer *coutputstat = mgr->CreateContainer(contname.Data(),TH1F::Class(),AliAnalysisManager::kOutputContainer,outputhistos.Data());
 
   contname=Form("coutputv2%s",suffix.Data());
-  AliAnalysisDataContainer *coutputv2 = mgr->CreateContainer(contname.Data(),TList::Class(),AliAnalysisManager::kOutputContainer,outputfile.Data());
+  AliAnalysisDataContainer *coutputv2 = mgr->CreateContainer(contname.Data(),TList::Class(),AliAnalysisManager::kOutputContainer,outputhistos.Data());
 
   contname=Form("cutobj%s",suffix.Data());
-  AliAnalysisDataContainer *cutobj = mgr->CreateContainer(contname.Data(),AliRDHFCuts::Class(),AliAnalysisManager::kOutputContainer,outputfile.Data());
+  AliAnalysisDataContainer *cutobj = mgr->CreateContainer(contname.Data(),AliRDHFCuts::Class(),AliAnalysisManager::kOutputContainer,outputhistos.Data());
 
   mgr->ConnectInput(v2Task,0,mgr->GetCommonInputContainer());
   
