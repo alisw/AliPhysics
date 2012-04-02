222555a3c3dea5ce3998bf222e1bf5dc4b7d615b
rgrosso <rgrosso@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-02 09:34:01 +0000

    example GRP/CTP/Aliases object and macros to read it

---
+++ STEER/findAliasesForClass.C
@@ -0,0 +1,24 @@
+#include "ARVersion.h"
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <TNamed.h>
+#include <THashList.h>
+#include <TObjArray.h>
+#include <TString.h>
+#include <TObjString.h>
+#endif
+
+TObjArray* findAliasesForClass(THashList &list, const char* className)
+{
+
+    TObjArray* matchingTrAliases = new TObjArray(2);
+    TNamed *n = dynamic_cast<TNamed*>(list.FindObject(className));
+    TString aliasList = n->GetTitle();
+    TObjArray* arrAliases = aliasList.Tokenize(',');
+    Int_t nAliases = arrAliases->GetEntries();
+    for(Int_t i=0; i<nAliases; i++){
+	TObjString *alias = dynamic_cast<TObjString*>(arrAliases->At(i));
+	matchingTrAliases->Add(alias);
+    }
+
+    return matchingTrAliases;
+}
---
+++ STEER/findClassesForAlias.C
@@ -0,0 +1,34 @@
+#include "ARVersion.h"
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include <TNamed.h>
+#include <THashList.h>
+#include <TObjArray.h>
+#include <TString.h>
+#include <TObjString.h>
+#endif
+
+TObjArray* findClassesForAlias(THashList &list, const char* aliasName)
+{
+
+    TObjArray* matchingTrClasses = new TObjArray(2);
+    TIter iter(&list);
+    TNamed *n = 0;
+    iter.Reset();
+    while((n = dynamic_cast<TNamed*>(iter.Next()))){
+	TString aliasList(n->GetTitle());
+	if(aliasList.Contains(aliasName)){
+	    TObjArray* arrAliases = aliasList.Tokenize(',');
+	    Int_t nAliases = arrAliases->GetEntries();
+	    for(Int_t i=0; i<nAliases; i++){
+		TObjString *alias = (TObjString*) arrAliases->At(i);
+		alias->Print(0);
+		if(alias->String()==TString(aliasName)){
+		    TObjString *trClass = new TObjString(n->GetName());
+		    matchingTrClasses->Add(trClass);
+		}
+	    }
+	}
+    }
+
+    return matchingTrClasses;
+}
