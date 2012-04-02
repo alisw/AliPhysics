d5d5e18374bab279a81235530ca2c6591eac3566
prino <prino@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-02 14:06:19 +0000

    Update (Carlos)

--- PWGHF/vertexingHF/charmFlow/AddTaskFlowD2H.C
+++ PWGHF/vertexingHF/charmFlow/AddTaskFlowD2H.C
@@ -4,8 +4,10 @@ class AliFlowEventTrackCuts;
 class AliRDHFCutsD0toKpi;
 class AliRDHFCutsDStartoKpipi; 
 
-void AddTaskFlowD2H(TString fileNameCuts, TString folderName, Int_t nDmeson, Int_t myCentrality, Int_t myHarmonic, 
-		    Bool_t bDoQC, Bool_t bDoSPTPC, Bool_t bDoSPVZERO, Bool_t bDoEPAlso, Int_t ptBinWidth ) {
+void AddTaskFlowD2H(TString fileNameCuts, TString folderName, Int_t nDmeson, Int_t minCent, Int_t maxCent, Int_t myHarmonic, 
+		    Bool_t bDoQC, Bool_t bDoSPTPC, Bool_t bDoSPVZERO, Bool_t bDoEPAlso, Int_t ptBinWidth,
+		    Double_t gapTPC, Double_t etaVZERO1, Double_t etaVZERO2, Double_t etaVZERO3, Double_t etaVZERO4,
+		    Int_t trigger, Bool_t testOnly=kFALSE ) {
   TFile *filecuts = TFile::Open( fileNameCuts.Data() );
   if( (!filecuts) || ( filecuts && !filecuts->IsOpen()) ){
     AliFatal("Could not open cuts file.");
@@ -49,7 +51,10 @@ void AddTaskFlowD2H(TString fileNameCuts, TString folderName, Int_t nDmeson, Int
   // POI FILTER CUTS
   AliFlowTrackSimpleCuts *filterPOIQC[50]; // MASS BANDS
   AliFlowTrackSimpleCuts *filterPOISP[50][2]; // MASS BANDS || ETA || SUBEVENT GAP
-  for(int mb=0; mb!=NumberOfMassBins(nDmeson); ++mb) {
+  int myMassBands = NumberOfMassBins( nDmeson );
+  if(testOnly)
+    myMassBands = 1;
+  for(int mb=0; mb!=myMassBands; ++mb) {
     filterPOISP[mb][0] = new AliFlowTrackSimpleCuts( Form("FilterPOISP_MB%d_ETANEG",mb) );
     filterPOISP[mb][0]->SetEtaMin( -0.8 );
     filterPOISP[mb][0]->SetEtaMax( 0. );
@@ -67,80 +72,85 @@ void AddTaskFlowD2H(TString fileNameCuts, TString folderName, Int_t nDmeson, Int
     filterPOIQC[mb]->SetMassMax( LowMassBin(nDmeson,mb+1) );
   }
 
-  //**********************************************************************
-  // CENTRALITIES
-  Int_t centMin[3] = {0, 15, 30};
-  Int_t centMax[3] = {5, 30, 50};
-  for(int cc=myCentrality;cc!=myCentrality+1;++cc) {
-    int minCC = centMin[cc];
-    int maxCC = centMax[cc];
-    TString ccName = Form("cc%d%d",minCC,maxCC);
-    // * EVENT CUTS ******************************************************
-    AliFlowEventCuts* cutsEvent = new AliFlowEventCuts( Form("EventCuts_%s",ccName.Data()) );
-    cutsEvent->SetCentralityPercentileRange(minCC,maxCC);
-    cutsEvent->SetCentralityPercentileMethod(AliFlowEventCuts::kV0);
-    cutsEvent->SetNContributorsRange(2);
-    cutsEvent->SetPrimaryVertexZrange(-9.,9.);
-    cutsEvent->SetQA(kTRUE);
-    cutsEvent->SetRefMultMethod(AliESDtrackCuts::kTrackletsITSTPC);
-    // * DMESON SELECTOR *************************************************
-    AliAnalysisTaskFlowD2H *taskSel;
-    switch (nDmeson) {
-    case ( AliRDHFCuts::kD0toKpiCuts ):
-      AliRDHFCutsD0toKpi *myCutsD0;
-      myCutsD0 = (AliRDHFCutsD0toKpi*)filecuts->Get("D0toKpiCuts");
-      if(!myCutsD0) {
-	AliFatal("Problems reaching D0toKpiCuts");
-      }
-      taskSel = new AliAnalysisTaskFlowD2H( Form("TaskD0Selector_%s",ccName.Data()), 
-					    cutsEvent, myCutsD0, nDmeson );
-      break;
-    case ( AliRDHFCuts::kDstarCuts ):
-      AliRDHFCutsDStartoKpipi *myCutsDStar;
-      myCutsDStar = (AliRDHFCutsDStartoKpipi*)filecuts->Get("DStartoKpipiCuts");
-      if(!myCutsDStar) {
-	AliFatal("Problems reaching DStarToKpipiCuts");
-      }
-      taskSel = new AliAnalysisTaskFlowD2H( Form("TaskDStarSelector_%s",ccName.Data()),
-					    cutsEvent, myCutsDStar, nDmeson);
-      break;
+  int minCC = minCent;
+  int maxCC = maxCent;
+  TString ccName = Form("cc%d%d",minCC,maxCC);
+  // * EVENT CUTS ******************************************************
+  AliFlowEventCuts* cutsEvent = new AliFlowEventCuts( Form("EventCuts_%s",ccName.Data()) );
+  cutsEvent->SetCentralityPercentileRange(minCC,maxCC);
+  cutsEvent->SetCentralityPercentileMethod(AliFlowEventCuts::kV0);
+  cutsEvent->SetNContributorsRange(2);
+  cutsEvent->SetPrimaryVertexZrange(-9.,9.);
+  cutsEvent->SetQA(kTRUE);
+  cutsEvent->SetRefMultMethod(AliESDtrackCuts::kTrackletsITSTPC);
+  // * DMESON SELECTOR *************************************************
+  AliAnalysisTaskFlowD2H *taskSel;
+  switch (nDmeson) {
+  case ( AliRDHFCuts::kD0toKpiCuts ):
+    AliRDHFCutsD0toKpi *myCutsD0;
+    myCutsD0 = (AliRDHFCutsD0toKpi*)filecuts->Get("D0toKpiCuts");
+    if(!myCutsD0) {
+      AliFatal("Problems reaching D0toKpiCuts");
     }
-    taskSel->SelectCollisionCandidates(AliVEvent::kCentral|AliVEvent::kSemiCentral|AliVEvent::kMB);
-    //taskSel->SetDebug();
-    AliAnalysisDataContainer *coutHist = mgr->CreateContainer( Form("%sSelector_%s",sgn.Data(),ccName.Data()),
-							       TList::Class(),AliAnalysisManager::kOutputContainer,
-							       Form("%s.root:FlowD2H_%s_%s",fileName.Data(),ccName.Data(),thecuts.Data()) );
-    AliAnalysisDataContainer *coutArr = mgr->CreateContainer( Form("TaskSelectorCandidates_%s_%s",ccName.Data(),sgn.Data()),
-							      TObjArray::Class(),AliAnalysisManager::kExchangeContainer);
-    mgr->AddTask(taskSel);
-    mgr->ConnectOutput(taskSel,1,coutHist);
-    mgr->ConnectOutput(taskSel,2,coutArr);
-    mgr->ConnectInput (taskSel,0,cinput1);
-    // * RAW FLOWEVENT TPC *************************************************
-    AliAnalysisDataContainer *exc_TPC = MakeFlowEvent("TPC", ccName.Data(), fileName.Data(), thecuts.Data(), ptBinWidth, cinput1,
-    						      coutArr, cutsEvent, cutsRFPTPC, cutsPOI, true, nDmeson, 1.0);
-    // * RAW FLOWEVENT VZERO ***********************************************
-    AliAnalysisDataContainer *exc_VZE = MakeFlowEvent("VZE", ccName.Data(), fileName.Data(), thecuts.Data(), ptBinWidth, cinput1,
-						      coutArr, cutsEvent, cutsRFPVZE, cutsPOI, false, nDmeson, 5.0);
-    // * HANGING ANALYSIS TASKS ********************************************
-    for(int harm=myHarmonic; harm!=myHarmonic+1; ++harm)
-      for(int mb=0; mb!=NumberOfMassBins(nDmeson); ++mb) {
-	if(bDoQC) {
-	  AddQCmethod( Form("%sQCTPCMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), harm, exc_TPC, filterPOIQC[mb]);
-	}
-	if(bDoSPVZERO) {
-	  AddSPmethod( Form("%sSPVZEMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), -6, -2, +2, +6,
-		       "QaQb", harm, exc_VZE, 0, filterPOIQC[mb], NULL, bDoEPAlso );
-	}
-	if(bDoSPTPC) {
-	  for(int eg=0; eg!=2; ++eg) {
-	    AddSPmethod( Form("%sSPTPCMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), -0.8, -0.4*eg, +0.4*eg, +0.8,
-			 "Qa", harm, exc_TPC, eg, filterPOISP[mb][1], NULL, bDoEPAlso );
-	    AddSPmethod( Form("%sSPTPCMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), -0.8, -0.4*eg, +0.4*eg, +0.8,
-			 "Qb", harm, exc_TPC, eg, filterPOISP[mb][0], NULL, bDoEPAlso );
-	  }
-	}
+    taskSel = new AliAnalysisTaskFlowD2H( Form("TaskD0Selector_%s",ccName.Data()), 
+					  cutsEvent, myCutsD0, nDmeson );
+    break;
+  case ( AliRDHFCuts::kDstarCuts ):
+    AliRDHFCutsDStartoKpipi *myCutsDStar;
+    myCutsDStar = (AliRDHFCutsDStartoKpipi*)filecuts->Get("DStartoKpipiCuts");
+    if(!myCutsDStar) {
+      AliFatal("Problems reaching DStarToKpipiCuts");
+    }
+    taskSel = new AliAnalysisTaskFlowD2H( Form("TaskDStarSelector_%s",ccName.Data()),
+					  cutsEvent, myCutsDStar, nDmeson);
+    break;
+  case (AliRDHFCuts::kDplusCuts):
+    AliRDHFCutsDplustoKpipi *myCutsDplus;
+    myCutsDplus = (AliRDHFCutsDplustoKpipi*)filecuts->Get("AnalysisCuts");
+    if(!myCutsDplus) {
+      AliFatal("Problems reaching AnalysisCuts");
+    }
+    taskSel = new AliAnalysisTaskFlowD2H( Form("TaskDplusSelector_%s",ccName.Data()), 
+					  cutsEvent, myCutsDplus, nDmeson );
+    break;
+  }
+  taskSel->SelectCollisionCandidates(trigger);
+  //taskSel->SetDebug();
+  AliAnalysisDataContainer *coutHist = mgr->CreateContainer( Form("%sSelector_%s",sgn.Data(),ccName.Data()),
+							     TList::Class(),AliAnalysisManager::kOutputContainer,
+							     Form("%s.root:FlowD2H_%s_%s",fileName.Data(),ccName.Data(),thecuts.Data()) );
+  AliAnalysisDataContainer *coutArr = mgr->CreateContainer( Form("TaskSelectorCandidates_%s_%s",ccName.Data(),sgn.Data()),
+							    TObjArray::Class(),AliAnalysisManager::kExchangeContainer);
+  mgr->AddTask(taskSel);
+  mgr->ConnectOutput(taskSel,1,coutHist);
+  mgr->ConnectOutput(taskSel,2,coutArr);
+  mgr->ConnectInput (taskSel,0,cinput1);
+  // * RAW FLOWEVENT TPC *************************************************
+  AliAnalysisDataContainer *exc_TPC = MakeFlowEvent("TPC", ccName.Data(), fileName.Data(), thecuts.Data(), ptBinWidth, cinput1,
+						    coutArr, cutsEvent, cutsRFPTPC, cutsPOI, true, nDmeson, 1.0);
+  // * RAW FLOWEVENT VZERO ***********************************************
+  AliAnalysisDataContainer *exc_VZE = MakeFlowEvent("VZE", ccName.Data(), fileName.Data(), thecuts.Data(), ptBinWidth, cinput1,
+						    coutArr, cutsEvent, cutsRFPVZE, cutsPOI, false, nDmeson, 5.0);
+  // * HANGING ANALYSIS TASKS ********************************************
+  int harm = myHarmonic;
+  for(int mb=0; mb!=myMassBands; ++mb) {
+    if(bDoQC) {
+      AddQCmethod( Form("%sQCTPCMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), harm, exc_TPC, filterPOIQC[mb]);
+    }
+    if(bDoSPVZERO) {
+      AddSPmethod( Form("%sSPVZEMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), etaVZERO1, etaVZERO2, etaVZERO3, etaVZERO4,
+		   "Qa", harm, exc_VZE, 0, filterPOIQC[mb], NULL, bDoEPAlso );
+      AddSPmethod( Form("%sSPVZEMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), etaVZERO1, etaVZERO2, etaVZERO3, etaVZERO4,
+		   "Qb", harm, exc_VZE, 0, filterPOIQC[mb], NULL, bDoEPAlso );
+    }
+    if(bDoSPTPC) {
+      for(int eg=0; eg!=2; ++eg) {
+	AddSPmethod( Form("%sSPTPCMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), -0.8, -gapTPC*eg, +gapTPC*eg, +0.8,
+		     "Qa", harm, exc_TPC, eg, filterPOISP[mb][1], NULL, bDoEPAlso );
+	AddSPmethod( Form("%sSPTPCMB%d",sgn.Data(),mb), ccName.Data(), fileName.Data(), thecuts.Data(), -0.8, -gapTPC*eg, +gapTPC*eg, +0.8,
+		     "Qb", harm, exc_TPC, eg, filterPOISP[mb][0], NULL, bDoEPAlso );
       }
+    }
   }
 }
 
@@ -240,7 +250,7 @@ AliAnalysisDataContainer* MakeFlowEvent(char* RFPName, char* ccName, char *fileN
   fe->SetQAOn(turnOnQA);
   fe->SelectCollisionCandidates(AliVEvent::kCentral|AliVEvent::kSemiCentral|AliVEvent::kMB);
 
-  fe->SetNbinsEta(40); fe->SetEtaMin(-etaMax); fe->SetEtaMax(+etaMax);
+  fe->SetNbinsEta(20); fe->SetEtaMin(-etaMax); fe->SetEtaMax(+etaMax);
 
   int bins = NumberOfMassBins(nDmeson);
   fe->SetNbinsMass( bins );
@@ -249,6 +259,8 @@ AliAnalysisDataContainer* MakeFlowEvent(char* RFPName, char* ccName, char *fileN
 
   fe->SetNbinsPt(24/ptbins); fe->SetPtMin(0); fe->SetPtMax(24);
   fe->SetNbinsMult(2); fe->SetMultMin(2); fe->SetMultMax(10);
+  fe->SetNbinsPhi(20); fe->SetPhiMin(0); fe->SetPhiMax(TMath::TwoPi());
+  fe->SetNbinsQ(10); fe->SetQMin( 0 ); fe->SetQMax( 3 );
 
   mgr->AddTask(fe);
   mgr->ConnectInput (fe,0,input);
@@ -258,7 +270,7 @@ AliAnalysisDataContainer* MakeFlowEvent(char* RFPName, char* ccName, char *fileN
   return exc;
 }
 
-double LowMassBin( int nDmeson, int mb ) {
+double LowMassBin( int nDmeson, int mb) {
   double min,max;
   int bins = NumberOfMassBins( nDmeson );
   if(mb>bins) return 0.0;
