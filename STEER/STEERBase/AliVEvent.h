6246b1985b1123915905e7e91ca9635235dd8c8b
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-05 08:00:15 +0000

    1) Changes 1.a) a bit added on the VEvent for the power interaction trigger 1.b) a comment on the OADBFillingScheme for the ACE case 1.c) The macro attached to create the OADB root file 2) The OADB root file, that should be on "OADB/COMMON/PHYSICSSELECTION/data/"
    Zaida, Michele, Jan Fiete

--- STEER/STEERBase/AliVEvent.h
+++ STEER/STEERBase/AliVEvent.h
@@ -58,11 +58,12 @@ public:
       kSemiCentral  = BIT(17), // PbPb semicentral collision trigger
       kDG5          = BIT(18), // Double gap diffractive
       kZED          = BIT(19), // ZDC electromagnetic dissociation
+      kSPI          = BIT(20), // Power interaction trigger
       kUserDefined  = BIT(27), // Set when custom trigger classes are set in AliPhysicsSelection, offline SPD or V0 selection
       // Bits 28 and above are reserved for FLAGS
       kFastOnly     = BIT(30), // The fast cluster fired. This bit is set in to addition another trigger bit, e.g. kMB
       kAny          = 0xffffffff, // to accept any trigger
-      kAnyINT       = kMB | kINT7 | kCINT5 // to accept any interaction (aka minimum bias) trigger
+      kAnyINT       = kMB | kINT7 | kCINT5 | kSPI // to accept any interaction (aka minimum bias) trigger
   };
 
   AliVEvent() { }
