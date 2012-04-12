71c017712fb49223913c780465263253a199c80c
aadare <aadare@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-12 19:45:32 +0000

    changes from Misha

--- PWGCF/Correlations/macros/AddTaskDiHadronPID.C
+++ PWGCF/Correlations/macros/AddTaskDiHadronPID.C
@@ -1,8 +1,29 @@
-AliAnalysisTaskDiHadronPID *AddTaskDiHadronPID() {
-    
-	// AddTask Macro (v 8.00).
-	// Updated: Mar 2nd. 2012.
-	
+// AddTask Macro (v 8.00).
+// Updated: Apr 12th 2012.
+// Author: Misha Veldhoen (m.veldhoen@cern.ch)
+
+AliAnalysisTaskDiHadronPID *AddTaskDiHadronPID(Int_t verbose = 1,
+                                               Bool_t printbuffersize = kFALSE,
+                                               Bool_t mixedevents = kTRUE,
+                                               TString beamtype = "PbPb",
+                                               Double_t MaxEta = 0.8,
+                                               Double_t MaxPlotEta = 0.8,
+                                               Double_t MaxPt = 10.,
+                                               Int_t NEtaBins = 36,
+                                               Int_t NPhiBins = 36,
+                                               Double_t VertexZMixedEvents = 2.,
+                                               Bool_t zoomed = kFALSE,
+                                               Bool_t DoITSCut = kFALSE,
+                                               Bool_t DoDCACut = kTRUE,
+                                               Bool_t DemandNoMismatch = kTRUE,
+                                               Int_t trigbuffermaxsize=2500,
+                                               Double_t centralitycutmax=0.,
+                                               Double_t centralitycutmin=10.)
+
+                                               
+
+{
+    	
 	// Get the current analysis manager.
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
     if (!mgr) {
@@ -10,12 +31,28 @@ AliAnalysisTaskDiHadronPID *AddTaskDiHadronPID() {
         return 0;
     }
     
-    // Add the task to the manager.
+    // Create an instance of the task.
     AliAnalysisTaskDiHadronPID *task = new AliAnalysisTaskDiHadronPID("DiHadronPID");
     
-    task->SetVerbose(kFALSE);
-    task->SetCalculateMixedEvents();
-	
+    // Configure the task.
+    task->SetVerbose(verbose);
+    task->SetPrintBufferSize(printbuffersize);
+    task->SetCalculateMixedEvents(mixedevents);
+    task->SetBeamType(beamtype);
+    task->SetMaxEta(MaxEta);
+    task->SetMaxPlotEta(MaxPlotEta);
+    task->SetMaxPt(MaxPt);
+    task->SetNEtaBins(NEtaBins);
+    task->SetNPhiBins(NPhiBins);
+    task->SetVertexZMixedEvents(VertexZMixedEvents);
+    task->SetZoomed(zoomed);
+    task->SetDoITSCut(DoITSCut);
+    task->SetDoDCACut(DoDCACut);
+    task->SetDemandNoMismatch(DemandNoMismatch);
+    task->SetTrigBufferMaxSize(trigbuffermaxsize);
+    task->SetCentralityCut(centralitycutmax,centralitycutmin);
+
+    // Add the task.
 	mgr->AddTask(task);
     
 	// Data containers.
