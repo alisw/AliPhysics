4317e1734595b868655b9c3479fbcefb4e3215cf
fbellini <fbellini@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-13 16:53:17 +0000

    Bug fix for MC mother label

--- PWGLF/RESONANCES/AliRsnMiniParticle.h
+++ PWGLF/RESONANCES/AliRsnMiniParticle.h
@@ -31,7 +31,7 @@ public:
    Float_t       &Pz(Bool_t mc)              {return (mc ? fPsim[2] : fPrec[2]);}
    Short_t       &PDG()                      {return fPDG;}
    Short_t        PDGAbs()                   {return TMath::Abs(fPDG);}
-   Short_t       &Mother()                   {return fMother;}
+   Int_t         &Mother()                   {return fMother;}
    Short_t       &MotherPDG()                {return fMotherPDG;}
    UShort_t      &CutBits()                  {return fCutBits;}
    Bool_t         HasCutBit(Int_t i)         {UShort_t bit = 1 << i; return ((fCutBits & bit) != 0);}
@@ -48,7 +48,7 @@ private:
    Float_t   fPsim[3];      // MC momentum of the track
    Float_t   fPrec[3];      // reconstructed momentum of the track
    Short_t   fPDG;          // particle PDG code
-   Short_t   fMother;       // index of mother in its container
+   Int_t     fMother;       // index of mother in its container
    Short_t   fMotherPDG;    // PDG code of mother
    UShort_t  fCutBits;      // list of bits used to know what cuts were passed by this track
 
