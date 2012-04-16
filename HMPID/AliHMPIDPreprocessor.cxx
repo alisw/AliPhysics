283bd3c2c358e42a2c18e2b18daed0b2d4a97436
gvolpe <gvolpe@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-16 09:33:04 +0000

    Coverity fixed

--- HMPID/AliHMPIDPreprocessor.cxx
+++ HMPID/AliHMPIDPreprocessor.cxx
@@ -446,8 +446,8 @@ Double_t AliHMPIDPreprocessor::ProcTrans(TMap* pMap)
     Double_t aN2 = AliHMPIDParam::NMgF2Idx(photEn);
     Double_t aN3 = 1;                              // Argon Idx
 
-    Double_t aR1               = ((aN1 - aN2)*(aN1 - aN2))/((aN1 + aN2)*(aN1 + aN2));
-    Double_t aR2               = ((aN2 - aN3)*(aN2 - aN3))/((aN2 + aN3)*(aN2 + aN3));
+   // Double_t aR1               = ((aN1 - aN2)*(aN1 - aN2))/((aN1 + aN2)*(aN1 + aN2));
+  //  Double_t aR2               = ((aN2 - aN3)*(aN2 - aN3))/((aN2 + aN3)*(aN2 + aN3));
    // Double_t aT1               = (1 - aR1);
    // Double_t aT2               = (1 - aR2);
    // Double_t aCorrFactor       = (aT1*aT1)/(aT2*aT2);
