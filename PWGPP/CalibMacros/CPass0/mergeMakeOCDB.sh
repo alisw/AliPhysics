b994db78df8841c844660770578dbc474388f6f8
marian <marian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-18 16:37:31 +0000

    Default OCDB path as an argument for the macro The default value is raw (as before)

--- PWGPP/CalibMacros/CPass0/mergeMakeOCDB.sh
+++ PWGPP/CalibMacros/CPass0/mergeMakeOCDB.sh
@@ -5,6 +5,7 @@
 #    1 - directory on which to look for the files to be merged 
 #    2 - pattern
 #    3 - OCDB output path
+#    [4 - default OCDB]
 
 # example:
 # mergeMakeOCDB.sh /alice/cern.ch/user/a/aliprod/CPass0/output/ 000120691 alien://folder=/alice/cern.ch/user/a/aliprod/CPass0/output
@@ -13,6 +14,10 @@
 # $1 = directory where to perform the find 
 # $2 = pattern
 # $3 = OCDB path
+
+# if fourth argument given, its the default OCDB, otherwise use the default raw://
+defaultOCDB="raw://"
+[[ $# -eq 4 ]] && defaultOCDB=$4
 
 if [ -f Run0_999999999_v3_s0.root ]; then
     mkdir -p TPC/Calib/Correction
@@ -24,5 +29,5 @@ aliroot -l -b -q merge.C\(\"$1\",\"AliESDfriends_v1.root\"\) 2>&1 | tee merge.lo
 mv syswatch.log syswatch_merge.log
 
 echo ">>>>>>> Extract OCDB entries for run = $2, to be stored in $3"
-aliroot -l -b -q makeOCDB.C\(\"$2\",\"$3\"\) 2>&1 | tee ocdb.log
+aliroot -l -b -q makeOCDB.C\(\"$2\",\"$3\",\"$defaultOCDB\"\) 2>&1 | tee ocdb.log
 mv syswatch.log syswatch_makeOCDB.log
