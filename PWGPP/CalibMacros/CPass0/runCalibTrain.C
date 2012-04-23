e624d41c014151f4aa171ca3891aba941ee16182
zampolli <zampolli@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-04-23 15:14:41 +0000

    Updating calib train steering macro to take into account presence/absence of TPC reading teh detector mask from the OCDB (GRP/GRP/Data) and configuring the ITS task accordingly - Ruben.

--- PWGPP/CalibMacros/CPass0/runCalibTrain.C
+++ PWGPP/CalibMacros/CPass0/runCalibTrain.C
@@ -43,6 +43,14 @@ void runCalibTrain(TString runNumberString, const char *inFileName = "AliESDs.ro
   ConfigCalibTrain(runNumber, ocdb);
   
   //
+  // check the presence of the detectors
+  AliCDBEntry* entry = AliCDBManager::Instance()->Get("GRP/GRP/Data");
+  AliGRPObject* grpData = dynamic_cast<AliGRPObject*>(entry->GetObject()); 
+  if (!grpData) {printf("Failed to get GRP data for run",runNumber); return;}
+  Int_t activeDetectors = grpData->GetDetectorMask(); 
+  TString detStr = AliDAQ::ListOfTriggeredDetectors(activeDetectors);
+  printf("Detectors in the data:\n%s\n",detStr.Data());
+
   // setup analysis
   //
   AliAnalysisManager *mgr  = new AliAnalysisManager("ESD to ESD", "Analysis Manager");
@@ -67,10 +75,13 @@ void runCalibTrain(TString runNumberString, const char *inFileName = "AliESDs.ro
   AliAnalysisTask* tT0 = AddTaskT0Calib(runNumber);
   AliMeanVertexCalibTask *tMeanVtx = AddTaskMeanVertexCalib();
   //
-  Bool_t useTPCcrv  = kTRUE;
+  Bool_t okTPC = detStr.Contains("TPC");
+  Bool_t useTPCcrv=kTRUE;
   Bool_t writeITSTP = kFALSE;
+  if (!okTPC) useTPCcrv = kFALSE;
   AliAnalysisTaskITSAlignQA *itsAlign = AddTaskSDDCalib(0,writeITSTP,useTPCcrv);
-
+  if (!okTPC) itsAlign->SetUseITSstandaloneTracks(kTRUE); 
+  //
   // Run the analysis
   if (!mgr->InitAnalysis()) {
     printf("Analysis cannot be started, returning\n");
