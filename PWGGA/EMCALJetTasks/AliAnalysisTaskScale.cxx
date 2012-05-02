30e4e23005c18625ddd8256ceaa0865812206853
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-02 05:06:24 +0000

    Cosmetics. Centralit and change from E to Et clusters

--- PWGGA/EMCALJetTasks/AliAnalysisTaskScale.cxx
+++ PWGGA/EMCALJetTasks/AliAnalysisTaskScale.cxx
@@ -25,9 +25,8 @@ ClassImp(AliAnalysisTaskScale)
 //________________________________________________________________________
 AliAnalysisTaskScale::AliAnalysisTaskScale(const char *name) 
   : AliAnalysisTaskSE(name), 
-    fTracksName("tracks"),
-    fClustersName("clusters"),
-    fESD(0), 
+    fTracksName("Tracks"),
+    fClustersName("CaloClusters"),
     fOutputList(0), 
     fHistCentrality(0), 
     fHistPtTPCvsCent(0), 
@@ -56,19 +55,17 @@ void AliAnalysisTaskScale::UserCreateOutputObjects()
   fOutputList = new TList();
   fOutputList->SetOwner();
 
-  fHistCentrality = new TH1F("fHistCentrality","Centrality",101,-1,100);
-
-  fHistPtTPCvsCent = new TH2F("fHistPtTPCvsCent","rho vs cent",101,-1,100,500,0,1000);
-  fHistPtEMCALvsCent = new TH2F("fHistPtEMCALvsCent","rho vs cent",101,-1,100,500,0,1000);
-  fHistEtvsCent = new TH2F("fHistEtvsCent","rho vs cent",101,-1,100,500,0,1000);
-  fHistScalevsCent = new TH2F("fHistScalevsCent","rho vs cent",101,-1,100,400,0,4.0);
-  fHistDeltaScalevsCent = new TH2F("fHistDeltaScalevsCent","rho vs cent",101,-1,100,400,-2.0,2.0);
-  
-  fHistPtTPCvsNtrack = new TH2F("fHistPtTPCvsNtrack","rho vs cent",500,0,2500,500,0,1000);
-  fHistPtEMCALvsNtrack = new TH2F("fHistPtEMCALvsNtrack","rho vs cent",500,0,2500,500,0,1000);
-  fHistEtvsNtrack = new TH2F("fHistEtvsNtrack","rho vs cent",500,0,2500,500,0,1000);
-  fHistScalevsNtrack = new TH2F("fHistScalevsNtrack","rho vs cent",500,0,2500,400,0,4.0);
-  fHistDeltaScalevsNtrack = new TH2F("fHistDeltaScalevsNtrack","rho vs cent",500,0,2500,400,-2.0,2.0);
+  fHistCentrality         = new TH1F("fHistCentrality","Centrality",         101, -1, 100);
+  fHistPtTPCvsCent        = new TH2F("fHistPtTPCvsCent","rho vs cent",       101, -1, 100, 500,   0, 1000);
+  fHistPtEMCALvsCent      = new TH2F("fHistPtEMCALvsCent","rho vs cent",     101, -1, 100, 500,   0, 1000);
+  fHistEtvsCent           = new TH2F("fHistEtvsCent","rho vs cent",          101, -1, 100, 500,   0, 1000);
+  fHistScalevsCent        = new TH2F("fHistScalevsCent","rho vs cent",       101, -1, 100, 400,   0, 4);
+  fHistDeltaScalevsCent   = new TH2F("fHistDeltaScalevsCent","rho vs cent",  101, -1, 100, 400,  -2, 2);
+  fHistPtTPCvsNtrack      = new TH2F("fHistPtTPCvsNtrack","rho vs cent",     500,  0, 2500, 500,  0, 1000);
+  fHistPtEMCALvsNtrack    = new TH2F("fHistPtEMCALvsNtrack","rho vs cent",   500,  0, 2500, 500,  0, 1000);
+  fHistEtvsNtrack         = new TH2F("fHistEtvsNtrack","rho vs cent",        500,  0, 2500, 500,  0, 1000);
+  fHistScalevsNtrack      = new TH2F("fHistScalevsNtrack","rho vs cent",     500,  0, 2500, 400,  0, 4);
+  fHistDeltaScalevsNtrack = new TH2F("fHistDeltaScalevsNtrack","rho vs cent",500,  0, 2500, 400, -2, 2);
     
   fOutputList->Add(fHistCentrality);
   fOutputList->Add(fHistPtTPCvsCent);
@@ -90,70 +87,69 @@ void AliAnalysisTaskScale::UserExec(Option_t *)
 {
   // Execute on each event.
 
-  fESD = dynamic_cast<AliESDEvent*>(InputEvent());
-  if (!fESD) {
-    printf("ERROR: fESD not available\n");
-    return;
-  }
-
+  // get input collections
   TClonesArray *tracks = 0;
   TClonesArray *clusters = 0;
-  AliCentrality *centrality = 0;
   TList *l = InputEvent()->GetList();
-
   tracks = dynamic_cast<TClonesArray*>(l->FindObject(fTracksName));
   if (!tracks) {
     AliError(Form("Pointer to tracks %s == 0", fTracksName.Data() ));
     return;
   }
-    
   clusters = dynamic_cast<TClonesArray*>(l->FindObject(fClustersName));
   if (!clusters){
     AliError(Form("Pointer to clusters %s == 0", fClustersName.Data() ));
     return;
   }
 
-  centrality = dynamic_cast<AliCentrality*>(l->FindObject("Centrality"));
-  float fCent = centrality->GetCentralityPercentile("V0M");
-  fHistCentrality->Fill(fCent);
+  // get centrality
+  Double_t cent = -1;
+  AliCentrality *centrality = InputEvent()->GetCentrality();
+  if (centrality)
+    cent = centrality->GetCentralityPercentile("V0M");
+  if (cent<0) {
+    AliError(Form("Centrality negative: %f", cent));
+    return;
+  }
+  fHistCentrality->Fill(cent);
 
-  float scale = 0.000066*fCent*fCent-0.0015*fCent+1.5; //march 25th fit
-  float ptTPC = 0;float ptEMCAL=0;
+  // get vertex
+  Double_t vertex[3] = {0, 0, 0};
+  InputEvent()->GetPrimaryVertex()->GetXYZ(vertex);
+
+  Double_t scale   = 0.000066*cent*cent-0.0015*cent+1.5; //march 25th fit
+  Double_t ptTPC   = 0;
+  Double_t ptEMCAL = 0;
   const Int_t Ntracks = tracks->GetEntries();
   for (Int_t iTracks = 0; iTracks < Ntracks; ++iTracks) {
     AliVTrack *track = static_cast<AliVTrack*>(tracks->At(iTracks));
     if (!track)
       continue;
-    if (fabs(track->Eta())>0.7)
+    if (TMath::Abs(track->Eta())>0.7)
       continue;
     ptTPC+=track->Pt();
     if ((track->Phi()>3.14)||(track->Phi()<1.4))
       continue;
     ptEMCAL+=track->Pt();
-  } //track loop 
+  }
   
-  Double_t vertex[3] = {0, 0, 0};
-  
-  float Et = 0;
-  InputEvent()->GetPrimaryVertex()->GetXYZ(vertex);
+  Double_t Et = 0;
   const Int_t Nclus = clusters->GetEntries();
   for (Int_t iClus = 0; iClus < Nclus; ++iClus) {
-    
     AliVCluster *c = dynamic_cast<AliVCluster*>(clusters->At(iClus));
     if (!c->IsEMCAL())
       continue;
     TLorentzVector nPart;
     c->GetMomentum(nPart, vertex);
-    Et += nPart.P();
+    Et += nPart.Pt();
   }
   
-  float scalecalc = ((Et+ptEMCAL)/2.44)*(8.8/ptTPC); //2.44 = EMCAL Acc, 8.8 = TPC Acc
-  fHistPtTPCvsCent->Fill(fCent,ptTPC);
-  fHistPtEMCALvsCent->Fill(fCent,ptEMCAL);
-  fHistEtvsCent->Fill(fCent,Et);
-  fHistScalevsCent->Fill(fCent,scalecalc);
-  fHistDeltaScalevsCent->Fill(fCent,scalecalc-scale);
-
+  const Double_t scalecalc = ((Et+ptEMCAL)/2.44)*(8.8/ptTPC); //2.44 = EMCAL Acc, 8.8 = TPC Acc
+  fHistPtTPCvsCent->Fill(cent,ptTPC);
+  fHistPtEMCALvsCent->Fill(cent,ptEMCAL);
+  fHistEtvsCent->Fill(cent,Et);
+  fHistScalevsCent->Fill(cent,scalecalc);
+  fHistDeltaScalevsCent->Fill(cent,scalecalc-scale);
   fHistPtTPCvsNtrack->Fill(Ntracks,ptTPC);
   fHistPtEMCALvsNtrack->Fill(Ntracks,ptEMCAL);
   fHistEtvsNtrack->Fill(Ntracks,Et);
