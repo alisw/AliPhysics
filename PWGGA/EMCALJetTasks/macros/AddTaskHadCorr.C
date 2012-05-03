9ff2b04f92158bc370673fc082e7ba05ee18aa03
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-03 06:56:22 +0000

    add phi/eta match

--- PWGGA/EMCALJetTasks/macros/AddTaskHadCorr.C
+++ PWGGA/EMCALJetTasks/macros/AddTaskHadCorr.C
@@ -6,6 +6,8 @@ AliHadCorrTask* AddTaskHadCorr(
   const char *outClusName    = "CaloClustersCorr",
   const Double_t hadcorr     = 1,
   const Double_t minPt       = 0.15,
+  const Double_t phiMatch    = 0.050,
+  const Double_t etaMatch    = 0.025,
   const char *outputname     = "AnalysisResults.root"
 )
 {  
@@ -34,7 +36,9 @@ AliHadCorrTask* AddTaskHadCorr(
   AliHadCorrTask *hcor = new AliHadCorrTask(name);
   hcor->SetTracksName(nTracks);
   hcor->SetClusName(nClusters);
-  hcor->SetOutClusName(outClusName);  
+  hcor->SetOutClusName(outClusName);
+  hcor->SetPhiMatch(phiMatch);
+  hcor->SetEtaMatch(etaMatch);
   hcor->SetHadCorr(hadcorr);
   hcor->SetMinPt(minPt);
 
