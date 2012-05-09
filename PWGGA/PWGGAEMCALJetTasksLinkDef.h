96440ee3f8099bf0e8b87c0d72fadeac0dca29f2
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-09 18:29:17 +0000

    move to jet tasks

--- PWGGA/PWGGAEMCALJetTasksLinkDef.h
+++ PWGGA/PWGGAEMCALJetTasksLinkDef.h
@@ -4,9 +4,11 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class AliAnalysisTaskSAJF+;
 #pragma link C++ class AliAnalysisTaskScale+;
+#pragma link C++ class AliEmcalJet+;
 #pragma link C++ class AliEmcalJetTask+;
-#pragma link C++ class AliJetEmbeddingTask+;
 #pragma link C++ class AliHadCorrTask+;
+#pragma link C++ class AliJetEmbeddingTask+;
 
 #endif
