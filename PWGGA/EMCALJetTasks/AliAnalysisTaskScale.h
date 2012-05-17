c1b3fc31b416aefb5cf3ba6cc9e0c5f84d76f595
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-17 02:45:07 +0000

    update from pdsf

--- PWGGA/EMCALJetTasks/AliAnalysisTaskScale.h
+++ PWGGA/EMCALJetTasks/AliAnalysisTaskScale.h
@@ -6,12 +6,13 @@
 class TList;
 class TH1F;
 class TH2F;
+class TF1;
 
 #include "AliAnalysisTaskSE.h"
 
 class AliAnalysisTaskScale : public AliAnalysisTaskSE {
  public:
-    AliAnalysisTaskScale() : AliAnalysisTaskSE(), fTracksName(), fClustersName(),
+    AliAnalysisTaskScale() : AliAnalysisTaskSE(), fTracksName(), fClustersName(), fScaleFunction(0),
       fOutputList(0), fHistCentrality(0), fHistPtTPCvsCent(0), fHistPtEMCALvsCent(0), fHistEtvsCent(0),  
       fHistScalevsCent(0),  fHistDeltaScalevsCent(0), fHistPtTPCvsNtrack(0), fHistPtEMCALvsNtrack(0), 
       fHistEtvsNtrack(0), fHistScalevsNtrack(0), fHistDeltaScalevsNtrack(0) {}
@@ -21,12 +22,19 @@ class AliAnalysisTaskScale : public AliAnalysisTaskSE {
   virtual void           UserCreateOutputObjects();
   virtual void           UserExec(Option_t *option);
   virtual void           Terminate(Option_t *);
-  virtual void           SetTracksName(const char *n)    { fTracksName   = n; }
-  virtual void           SetClustersName(const char *n)  { fClustersName = n; }
+
+  void                   SetTracksName(const char *n)                          { fTracksName    = n    ; }
+  void                   SetClustersName(const char *n)                        { fClustersName  = n    ; }
+  void                   SetScaleFunction(TF1* sf)                             { fScaleFunction = sf   ; }
   
+ protected:
+  virtual Double_t       GetScaleFactor(Double_t cent);
+
  private:
   TString                fTracksName;             // name of track collection
   TString                fClustersName;           // name of clusters collection
+  TF1                   *fScaleFunction;          // scale factor as a function of centrality
+  TF1                   *fNewScaleFunction;       //!new scale factor as a function of centrality
   TList                 *fOutputList;             //!output list
   TH1F                  *fHistCentrality;         //!output histogram
   TH2F                  *fHistPtTPCvsCent;        //!output histogram
@@ -43,6 +51,6 @@ class AliAnalysisTaskScale : public AliAnalysisTaskSE {
   AliAnalysisTaskScale(const AliAnalysisTaskScale&); // not implemented
   AliAnalysisTaskScale& operator=(const AliAnalysisTaskScale&); // not implemented
   
-  ClassDef(AliAnalysisTaskScale, 2); // example of analysis
+  ClassDef(AliAnalysisTaskScale, 3); // Scale task
 };
 #endif
