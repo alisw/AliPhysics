6c2bb6019e7fdd72e2720b532ab5d4a1b5cf4310
cbaumann <cbaumann@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-17 15:45:36 +0000

    Add macros for first AOD tests

---
+++ PWGDQ/dielectron/macrosLMEE/AddTask_cbaumann_LMEEPbPb2011AOD.C
@@ -0,0 +1,97 @@
+AliAnalysisTask *AddTask_cbaumann_LMEEPbPb2011AOD(Bool_t runAll=kFALSE){
+  //get the current analysis manager
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+	Error("AddTask_cbaumann_LMEEPbPb2011", "No analysis manager found.");
+	return 0;
+  }
+
+  //set config file name
+  TString configFile("$ALICE_ROOT/PWGDQ/dielectron/macrosLMEE/ConfigLMEEPbPb2011AOD.C");
+
+  //AOD Usage not yet testes/avialable-------------------------------------
+
+  if (mgr->GetInputEventHandler()->IsA()==AliAODInputHandler::Class()){
+	::Info("AddTaskLMEEPbPb2011", "no dedicated AOD configuration");
+//	configFile="$TRAIN_ROOT/util/dielectron/dielectron/macros/ConfigLMEEPbPb2011AOD.C";	
+	
+  }
+
+  //Do we have an MC handler?
+  Bool_t hasMC=(AliAnalysisManager::GetAnalysisManager()->GetMCtruthEventHandler()!=0x0);
+
+  const Int_t kNtriggers=1;
+    ULong64_t triggers[]={AliVEvent::kCentral | AliVEvent::kSemiCentral | AliVEvent::kMB };
+	const char* triggerNames[]={"all"};
+
+
+
+//  create task and add it to the manager
+//	gSystem->AddIncludePath("$ALICE_ROOT/PWGDQ/dielectron/macrosLMEE");
+
+	gROOT->LoadMacro("$ALICE_ROOT/PWGDQ/dielectron/macrosLMEE/LMEECutLibAOD.C");
+	gROOT->LoadMacro(configFile.Data());
+  LMEECutLib* cutlib = new LMEECutLib();
+  AliAnalysisTaskMultiDielectron *task=new AliAnalysisTaskMultiDielectron("MultiDiEData");
+  if (!hasMC) task->UsePhysicsSelection();
+  task->SetTriggerMask(triggers[0]);
+  task->SetEventFilter(cutlib->GetEventCuts(LMEECutLib::kPbPb2011TPCandTOF)); //
+	
+
+  //load dielectron configuration file
+
+  //add dielectron analysis with different cuts to the task
+if (runAll) {
+  AliDielectron *lowmass4=ConfigLMEEPbPb2011AOD(4,hasMC);
+  task->AddDielectron(lowmass4);
+  printf("add: %s\n",lowmass4->GetName());
+
+}
+else {
+  AliDielectron *lowmass1=ConfigLMEEPbPb2011AOD(1,hasMC);
+  task->AddDielectron(lowmass1);
+  printf("add: %s\n",lowmass1->GetName())
+}
+
+  mgr->AddTask(task);
+
+  //create output container
+  AliAnalysisDataContainer *coutput1 =
+	mgr->CreateContainer("cbaumann_LMEEPbPb2011_tree",
+		TTree::Class(),
+		AliAnalysisManager::kExchangeContainer,
+		"cbaumann_LMEEPbPb2011_default.root");
+
+  AliAnalysisDataContainer *cOutputHist1 =
+	mgr->CreateContainer("cbaumann_LMEEPbPb2011_out",
+		TList::Class(),
+		AliAnalysisManager::kOutputContainer,
+		"cbaumann_LMEEPbPb2011_out.root");
+
+  /*  AliAnalysisDataContainer *cOutputHist2 =
+	  mgr->CreateContainer("cbaumann_lowmass_CF",
+	  TList::Class(),
+	  AliAnalysisManager::kOutputContainer,
+	  "cbaumann_lowmass_CF.root");
+	  */
+  AliAnalysisDataContainer *cOutputHist2 =
+	mgr->CreateContainer("cbaumann_LMEEPbPb2011_CF",
+		TList::Class(),
+		AliAnalysisManager::kOutputContainer,
+		"cbaumann_LMEEPbPb2011_out.root");
+
+  AliAnalysisDataContainer *cOutputHist3 =
+	mgr->CreateContainer("cbaumann_EventStatPbPb2011",
+		TH1D::Class(),
+		AliAnalysisManager::kOutputContainer,
+		"cbaumann_LMEEPbPb2011_out.root");
+
+
+  mgr->ConnectInput(task,  0, mgr->GetCommonInputContainer());
+  mgr->ConnectOutput(task, 0, coutput1 );
+  mgr->ConnectOutput(task, 1, cOutputHist1);
+  mgr->ConnectOutput(task, 2, cOutputHist2);
+  mgr->ConnectOutput(task, 3, cOutputHist3);
+
+  return task;
+}
