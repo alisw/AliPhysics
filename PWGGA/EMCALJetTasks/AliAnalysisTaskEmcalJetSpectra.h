81f1b5e16ee71e9d54222ded4c4e610e0751b29d
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-17 02:45:49 +0000

    update from pdsf plus tparameter change

--- PWGGA/EMCALJetTasks/AliAnalysisTaskEmcalJetSpectra.h
+++ PWGGA/EMCALJetTasks/AliAnalysisTaskEmcalJetSpectra.h
@@ -18,28 +18,20 @@ class AliAnalysisTaskEmcalJetSpectra : public AliAnalysisTaskSE {
   virtual void   UserCreateOutputObjects();
   virtual void   UserExec(Option_t *option);
   virtual void   Terminate(Option_t *);
-  virtual void   SetTracksName(const char *n)            { fTracksName = n; }
-  virtual void   SetJetsName(const char *n)              { fJetsName = n;   }
-  virtual void   SetRhos1Name(const char *rn)             { fRhos1Name=rn;    }
-  virtual void   SetRhos2Name(const char *rn)             { fRhos2Name=rn;    }
-  virtual void   SetJetPhi(Double_t pmin, Double_t pmax) { phimin = pmin; phimax = pmax; }
-  virtual void   SetJetEta(Double_t emin, Double_t emax) { etamin = emin; etamax = emax; }
-  virtual void SetAreaCut(Double_t a)                    { areacut = a;}
+  virtual void   SetTracksName(const char *n)              { fTracksName = n; }
+  virtual void   SetJetsName(const char *n)                { fJetsName = n;   }
+  virtual void   SetRhos1Name(const char *rn)              { fRhos1Name=rn;    }
+  virtual void   SetRhos2Name(const char *rn)              { fRhos2Name=rn;    }
+  virtual void   SetJetPhi(Double_t pmin, Double_t pmax)   { phimin = pmin; phimax = pmax; }
+  virtual void   SetJetEta(Double_t emin, Double_t emax)   { etamin = emin; etamax = emax; }
+  virtual void   SetAreaCut(Double_t a)                    { areacut = a;}
   
  protected:
   virtual Int_t GetCentBin(Double_t cent) const;
  
   
  private:
-  TString                fTracksName;             // name of track collection
-  TString                fJetsName;             // name of jet collection
-  TString                fRhos1Name;             // name of Rho1 array output
-  TString                fRhos2Name;             // name of Rho2 array output
-  TString                fClustersName;             // name of clusters collection
-  TClonesArray *fArrRhos1;
-  TClonesArray *fArrRhos2;
-
-  AliESDEvent *fESD;    //! ESD object
+   AliESDEvent *fESD;    //! ESD object
   TList       *fOutputList; //! Output list
   TH1F        *fHistCentrality;
   TH1F        *fHistJetArea; 
@@ -63,29 +55,37 @@ class AliAnalysisTaskEmcalJetSpectra : public AliAnalysisTaskSE {
   TH2F        *fHistJetNEFvsNtrack;
   TH2F        *fHistNjetvsNtrack;
 
-  TH2F       *fHistNEFvsPt[6][3];
-  TH2F       *fHistZvsPt[6][3];
-  TH2F       *fHistZchvsPt[6][3];
-  TH2F       *fHistZemvsPt[6][3];
-  TH2F       *fHistAreavsPt[6][3];
-  TH1F       *fHistJetPt[6][3];
-  TH2F       *fHistNconsvsPt[6][3];
-  TH1F       *fHistRawJetPt[6];
-  TH2F       *fHistAreavsRawPt[6];
+  TH2F       *fHistDeltaRho12vsCent;
+  TH2F       *fHistDeltaRho13vsCent;
+  TH2F       *fHistDeltaRho23vsCent;
+  
   TH2F       *fHistRho1vsCent;
   TH2F       *fHistRho2vsCent;
   TH2F       *fHistRho3vsCent;
 
-  TH2F       *fHistDeltaRho12vsCent;
-  TH2F       *fHistDeltaRho13vsCent;
-  TH2F       *fHistDeltaRho23vsCent;
+  TString                fTracksName;             // name of track collection
+  TString                fJetsName;             // name of jet collection
+  TString                fClustersName;             // name of clusters collection
+  TString                fRhos1Name;             // name of Rho1 array output
+  TString                fRhos2Name;             // name of Rho2 array output
+  TString                fRhos3Name;             // name of Rho2 array output
 
   Double_t phimin;
   Double_t phimax;
   Double_t etamin;
   Double_t etamax;
   Double_t areacut;
-  Int_t cSwitch;
+
+ TH2F       *fHistNEFvsPt[6][4];
+  TH2F       *fHistZvsPt[6][4];
+  TH2F       *fHistZchvsPt[6][4];
+  TH2F       *fHistZemvsPt[6][4];
+  TH2F       *fHistAreavsPt[6][4];
+  TH1F       *fHistJetPt[6][4];
+  TH2F       *fHistNconsvsPt[6][4];
+  TH1F       *fHistRawJetPt[6];
+  TH2F       *fHistAreavsRawPt[6];
+
 
    AliAnalysisTaskEmcalJetSpectra(const AliAnalysisTaskEmcalJetSpectra&); // not implemented
   AliAnalysisTaskEmcalJetSpectra& operator=(const AliAnalysisTaskEmcalJetSpectra&); // not implemented
