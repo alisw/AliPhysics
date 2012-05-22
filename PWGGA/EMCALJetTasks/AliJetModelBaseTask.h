db2902cabe2fe31a8d79d3157a2814bb50a95796
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-22 22:52:31 +0000

    random eta/phi

--- PWGGA/EMCALJetTasks/AliJetModelBaseTask.h
+++ PWGGA/EMCALJetTasks/AliJetModelBaseTask.h
@@ -33,14 +33,14 @@ class AliJetModelBaseTask : public AliAnalysisTaskSE {
 
  protected:
 
-  virtual void           Run();                                                               // do jet model action
-  void                   GetRandomCell(Double_t &eta, Double_t &phi, Int_t &absId);           // generate a random cell in the calorimeter
-  Double_t               GetRandomEta();                                                      // generate a random eta value in the given range
-  Double_t               GetRandomPhi();                                                      // generate a random phi value in the given range
-  Double_t               GetRandomPt();                                                       // generate a random pt value in the given range
-  AliVCluster           *AddCluster(Double_t e = -1, Double_t eta = -1, Double_t phi = -1);   // add a cluster; if values are -1 generate random parameters
-  AliVCluster           *AddCluster(Double_t e, Int_t absId);                                 // add a cluster with given energy and position
-  AliPicoTrack          *AddTrack(Double_t pt = -1, Double_t eta = -1, Double_t phi = -1);    // add a track; if values are -1 generate random parameters
+  virtual void           Run();                                                                 // do jet model action
+  void                   GetRandomCell(Double_t &eta, Double_t &phi, Int_t &absId);             // generate a random cell in the calorimeter
+  Double_t               GetRandomEta();                                                        // generate a random eta value in the given range
+  Double_t               GetRandomPhi();                                                        // generate a random phi value in the given range
+  Double_t               GetRandomPt();                                                         // generate a random pt value in the given range
+  AliVCluster           *AddCluster(Double_t e = -1, Double_t eta = -999, Double_t phi = -1);   // add a cluster; if values are -1 generate random parameters
+  AliVCluster           *AddCluster(Double_t e, Int_t absId);                                   // add a cluster with given energy and position
+  AliPicoTrack          *AddTrack(Double_t pt = -1, Double_t eta = -999, Double_t phi = -1);    // add a track; if values are -1 generate random parameters
 
   TString                fGeomName;               // EMCal geometry name
   TString                fTracksName;             // name of track collection
