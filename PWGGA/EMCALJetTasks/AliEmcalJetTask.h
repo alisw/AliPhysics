8d149bd42a10345c0b4504c07928122f824e4fba
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-27 20:08:10 +0000

    Use active ghosts to compute area on emcal surface. Also introduce cut on area and jets that can be given to task.

--- PWGGA/EMCALJetTasks/AliEmcalJetTask.h
+++ PWGGA/EMCALJetTasks/AliEmcalJetTask.h
@@ -23,7 +23,9 @@ class AliEmcalJetTask : public AliAnalysisTaskSE {
   void         SetAlgo(Int_t a)                 { fAlgo          = a;  }
   void         SetClusName(const char *n)       { fCaloName      = n;  }
   void         SetJetsName(const char *n)       { fJetsName      = n;  }
+  void         SetMinJetArea(Double_t a)        { fMinJetArea    = a;  }
   void         SetMinJetClusPt(Double_t min)    { fMinJetClusPt  = min;}
+  void         SetMinJetPt(Double_t j)          { fMinJetPt      = j;  }
   void         SetMinJetTrackPt(Double_t min)   { fMinJetTrackPt = min;}
   void         SetRadius(Double_t r)            { fRadius        = r;  }
   void         SetTracksName(const char *n)     { fTracksName    = n;  }
@@ -38,14 +40,16 @@ class AliEmcalJetTask : public AliAnalysisTaskSE {
   Int_t                  fAlgo;                   // algo (0==kt, 1==antikt)
   Double_t               fRadius;                 // jet radius
   Int_t                  fType;                   // jet type (0=all, 1=ch, 2=neutral)
-  Double_t               fMinJetTrackPt;          // min jet track momentum
-  Double_t               fMinJetClusPt;           // min jet cluster momentum
+  Double_t               fMinJetTrackPt;          // min jet track momentum   (applied before clustering)
+  Double_t               fMinJetClusPt;           // min jet cluster momentum (applied before clustering)
+  Double_t               fMinJetArea;             // min area to keep jet in output
+  Double_t               fMinJetPt;               // min jet pt to keep jet in output
   TClonesArray          *fJets;                   //!jet collection
 
  private:
   AliEmcalJetTask(const AliEmcalJetTask&);            // not implemented
   AliEmcalJetTask &operator=(const AliEmcalJetTask&); // not implemented
 
-  ClassDef(AliEmcalJetTask, 1) // Jet producing task
+  ClassDef(AliEmcalJetTask, 2) // Jet producing task
 };
 #endif
