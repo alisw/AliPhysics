c7434aaec92505598e13cac0121a51e8757f4462
ssakai <ssakai@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-29 19:08:34 +0000

    updated

--- PWGHF/hfe/macros/configs/PbPb/ConfigHFECal.C
+++ PWGHF/hfe/macros/configs/PbPb/ConfigHFECal.C
@@ -18,7 +18,7 @@ AliAnalysisTaskHFECal* ConfigHFECal(Bool_t useMC){
   hfecuts->SetPtRange(2, 50);
   hfecuts->SetMaxImpactParam(1,2);
   
-  AliAnalysisTaskHFECal *task = new AliAnalysisTaskHFECal("HFE v2");
+  AliAnalysisTaskHFECal *task = new AliAnalysisTaskHFECal("HFEanalysisEMCal");
   printf("task ------------------------ %p\n ", task);
   task->SetHFECuts(hfecuts);
   task->SetInvariantMassCut(0.05);
@@ -30,7 +30,7 @@ AliAnalysisTaskHFECal* ConfigHFECal(Bool_t useMC){
   pid->AddDetector("EMCAL", 1);
   // change E/p cuts
   AliHFEpidEMCAL *emcpid = pid->AliHFEpid::GetDetPID(AliHFEpid::kEMCALpid);
-  emcpid->SetEoPMax(1.2);
+  emcpid->SetEoPMax(1.3);
   emcpid->SetEoPMim(0.9);
 
   Double_t params[4];
@@ -38,9 +38,11 @@ AliAnalysisTaskHFECal* ConfigHFECal(Bool_t useMC){
   if(useMC){
 	  // Monte-Carlo needs modelling of the falling mean with momentum at low momentum
 	  // for high momentum it is consistent with a flat -0.94
-	  cutmodel = "[0]*TMath::Exp([1]*x) + [2] + [3]*x";
-	  Double_t paramsMC[4] = {0.7174, -1.588, -0.9395, 0.0246};
-	  for(int ipar = 0; ipar < 4; ipar++) params[ipar] = paramsMC[ipar];
+	  //cutmodel = "[0]*TMath::Exp([1]*x) + [2] + [3]*x";
+	  //Double_t paramsMC[4] = {0.7174, -1.588, -0.9395, 0.0246};
+	  //for(int ipar = 0; ipar < 4; ipar++) params[ipar] = paramsMC[ipar];
+	  cutmodel = "pol0";
+	  params[0] = -1.0; //sigma min
   } else {
 	  // Data is consistent with a flat 0.12
 	  cutmodel = "pol0";
