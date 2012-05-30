7a7a0e22ec42fb2da9908d6ed3c92e5d699e3f92
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-30 05:10:11 +0000

    use emcal jet class for matching

--- PWGGA/EMCALJetTasks/AliJetResponseMaker.h
+++ PWGGA/EMCALJetTasks/AliJetResponseMaker.h
@@ -18,36 +18,38 @@ class AliJetResponseMaker : public AliAnalysisTaskEmcal {
   void                        UserCreateOutputObjects();
   void                        Terminate(Option_t *option);
 
-  void                        SetMCJetsName(const char *n)       { fMCJetsName    = n;  }
-  void                        SetMCTracksName(const char *n)     { fMCTracksName  = n;  }
+  void                        SetMCJetsName(const char *n)       { fMCJetsName    = n; }
+  void                        SetMCTracksName(const char *n)     { fMCTracksName  = n; }
+  void                        SetMaxDistance(Double_t d)         { fMaxDistance   = d; }
 
  protected:
-  void                        DoJetLoop(Int_t &maxJetIndex, TClonesArray *jets, TClonesArray *tracks, TClonesArray *clusters = 0);
+  void                        DoJetLoop(TClonesArray *jets1, TClonesArray *jets2);
   void                        FillHistograms();
   void                        RetrieveEventObjects();
 
   TString                     fMCTracksName;              // name of MC particle collection
   TString                     fMCJetsName;                // name of MC jet collection
+  Double_t                    fMaxDistance;               // maximum distance between matched particle and detector level jets
   TClonesArray               *fMCTracks;                  //!MC particles
   TClonesArray               *fMCJets;                    //!MC jets
   // Particle level jets
   TH2F                       *fHistMCJetPhiEta;           //!phi-eta distribution of jets
   TH1F                       *fHistMCJetsPt;              //!inclusive jet pt spectrum
-  TH1F                       *fHistMCJetsPtTrack;         //!inclusive jet pt spectrum track biased
-  TH1F                       *fHistMCJetsPtClus;          //!inclusive jet pt spectrum cluster biased
   TH1F                       *fHistMCJetsPtNonBias;       //!non biased inclusive jet pt spectrum
-  TH1F                       *fHistMCLeadingJetPt;        //!leading jet pt spectrum
   TH2F                       *fHistMCJetsNEFvsPt;         //!jet neutral energy fraction vs. jet pt
   TH2F                       *fHistMCJetsZvsPt;           //!constituent Pt over Jet Pt ratio vs. jet pt
   // Detector level jets
   TH2F                       *fHistJetPhiEta;             //!phi-eta distribution of jets
   TH1F                       *fHistJetsPt;                //!inclusive jet pt spectrum
-  TH1F                       *fHistJetsPtTrack;           //!inclusive jet pt spectrum track biased
-  TH1F                       *fHistJetsPtClus;            //!inclusive jet pt spectrum cluster biased
   TH1F                       *fHistJetsPtNonBias;         //!non biased inclusive jet pt spectrum
-  TH1F                       *fHistLeadingJetPt;          //!leading jet pt spectrum
   TH2F                       *fHistJetsNEFvsPt;           //!jet neutral energy fraction vs. jet pt
   TH2F                       *fHistJetsZvsPt;             //!constituent Pt over Jet Pt ratio vs. jet pt
+  // Detector-particle level matching
+  TH1F                       *fHistClosestDistance;       //!distance between closest particle to detector level jet
+  TH1F                       *fHistClosestDeltaPhi;       //!delta phi between closest particle to detector level jet
+  TH1F                       *fHistClosestDeltaEta;       //!delta eta between closest particle to detector level jet
+  TH1F                       *fHistClosestDeltaPt;        //!delta pt between closest particle to detector level jet
+  TH2F                       *fHistPartvsDetecPt;         //!particle vs detector level jet pt
 
  private:
   AliJetResponseMaker(const AliJetResponseMaker&);            // not implemented
