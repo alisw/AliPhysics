16cd13588bc94db7c5f79e13213590d2f82675d3
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-05-30 05:01:36 +0000

    added mc info

--- PWGGA/EMCALJetTasks/AliEmcalJet.cxx
+++ PWGGA/EMCALJetTasks/AliEmcalJet.cxx
@@ -9,6 +9,35 @@
 ClassImp(AliEmcalJet)
 
 //__________________________________________________________________________________________________
+AliEmcalJet::AliEmcalJet() : 
+  AliVParticle(), 
+  fPt(0), 
+  fEta(0), 
+  fPhi(0), 
+  fM(0), 
+  fNEF(0),
+  fArea(0),       
+  fAreaEmc(-1), 
+  fAxisInEmcal(0), 
+  fMaxCPt(0), 
+  fMaxNPt(0), 
+  fMCPt(0),
+  fNn(0), 
+  fNch(0),        
+  fClusterIDs(),
+  fTrackIDs(),
+  fMatched(2) 
+{
+  // Constructor.
+
+  fClosestJets[0] = 0;
+  fClosestJets[1] = 0; 
+  fClosestJetsDist[0] = 999; 
+  fClosestJetsDist[1] = 999; 
+  fMatched = 2; 
+}
+
+//__________________________________________________________________________________________________
 AliEmcalJet::AliEmcalJet(Double_t px, Double_t py, Double_t pz) : 
   AliVParticle(), 
   fPt(TMath::Sqrt(px*px+py*py)), 
@@ -21,8 +50,12 @@ AliEmcalJet::AliEmcalJet(Double_t px, Double_t py, Double_t pz) :
   fAxisInEmcal(0),
   fMaxCPt(0), 
   fMaxNPt(0), 
+  fMCPt(0),
+  fNn(0),
+  fNch(0),
   fClusterIDs(), 
-  fTrackIDs()
+  fTrackIDs(),
+  fMatched(2)
 {    
   // Constructor.
 
@@ -31,6 +64,12 @@ AliEmcalJet::AliEmcalJet(Double_t px, Double_t py, Double_t pz) :
     if (fPhi<0.) 
       fPhi += 2. * TMath::Pi();
   }
+
+  fClosestJets[0] = 0; 
+  fClosestJets[1] = 0;
+  fClosestJetsDist[0] = 999; 
+  fClosestJetsDist[1] = 999;
+  fMatched = 2; 
 }
 
 //_________________________________________________________________________________________________
@@ -45,14 +84,24 @@ AliEmcalJet::AliEmcalJet(Double_t pt, Double_t eta, Double_t phi, Double_t m) :
   fAreaEmc(-1), 
   fAxisInEmcal(0),
   fMaxCPt(0), 
-  fMaxNPt(0), 
+  fMaxNPt(0),
+  fMCPt(0),
+  fNn(0),
+  fNch(0), 
   fClusterIDs(), 
-  fTrackIDs()
+  fTrackIDs(),
+  fMatched(2)
 {
   // Constructor.
 
  if (fPhi<0.) 
    fPhi += TMath::TwoPi();
+
+  fClosestJets[0] = 0; 
+  fClosestJets[1] = 0;
+  fClosestJetsDist[0] = 999; 
+  fClosestJetsDist[1] = 999;
+  fMatched = 2; 
 }
 
 //_________________________________________________________________________________________________
@@ -68,6 +117,9 @@ AliEmcalJet::AliEmcalJet(const AliEmcalJet &jet) :
   fAxisInEmcal(jet.fAxisInEmcal),
   fMaxCPt(jet.fMaxCPt), 
   fMaxNPt(jet.fMaxNPt), 
+  fMCPt(jet.fMCPt),
+  fNn(jet.fNn),
+  fNch(jet.fNch),
   fClusterIDs(jet.fClusterIDs), 
   fTrackIDs(jet.fTrackIDs)
 {
@@ -91,6 +143,9 @@ AliEmcalJet &AliEmcalJet::operator=(const AliEmcalJet &jet)
     fAxisInEmcal  = jet.fAxisInEmcal; 
     fMaxCPt       = jet.fMaxCPt; 
     fMaxNPt       = jet.fMaxNPt;
+    fMCPt         = jet.fMCPt;
+    fNn           = jet.fNn;
+    fNch          = jet.fNch;
     fClusterIDs   = jet.fClusterIDs;
     fTrackIDs     = jet.fTrackIDs;
   }
