fdef18a27d6d8eef3193a7bf0328b5e2495e131f
auras <auras@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-06-02 00:23:25 +0000

    Modified support macro AddMisalignmentToClusters.C

--- MFT/AddMisalignmentToClusters.C
+++ MFT/AddMisalignmentToClusters.C
@@ -18,7 +18,7 @@ Double_t misalignmentY[AliMFTConstants::fNMaxPlanes] = {0};
 
 void AddMisalignmentToClusters(Char_t *nameDir=".",
 			       Int_t seed = 12345,
-			       Double_t misalignment = 0.0030) {
+			       Double_t misalignment = 0.0015) {
 
   gRandom -> SetSeed(seed);
 
