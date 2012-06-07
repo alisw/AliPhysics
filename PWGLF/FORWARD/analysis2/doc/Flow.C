6a1293f2927bb059cec587f8e442ebcb42c8d2fe
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-06-07 08:59:04 +0000

    Added sub-directory for documentation 'code'

---
+++ PWGLF/FORWARD/analysis2/doc/Flow.C
@@ -0,0 +1,16 @@
+#error This is not for compilation 
+/** 
+ * @page flow_doc Anisotropic Azimuthal Charge Particle Moments
+ *
+ * Module: @ref pwglf_forward_flow
+ *
+ * @tableofcontents 
+ *
+ * @section flow_intro Introduction 
+ *
+ * @f$v_{n}\{m\}@f$
+ */
+//
+// EOF
+//
+
