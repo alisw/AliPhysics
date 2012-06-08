c964a8b6d3ccc60693a7e79fbfc71b3d837309e5
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-06-08 21:00:15 +0000

    hist switch

--- PWGGA/EMCALTasks/AliAnalysisTaskEmcal.h
+++ PWGGA/EMCALTasks/AliAnalysisTaskEmcal.h
@@ -37,11 +37,11 @@ class AliAnalysisTaskEmcal : public AliAnalysisTaskSE {
   virtual void                UserExec(Option_t *option);
   virtual void                Terminate(Option_t *option);
 
-  void                        SetTracksName(const char *n)                         { fTracksName     = n          ; }
-  void                        SetClusName(const char *n)                           { fCaloName       = n          ; }
   void                        SetAnaType(EmcalAnaType type)                        { fAnaType        = type       ; }
-  void                        SetPtCut(Float_t cut)                                { fPtCut          = cut        ; }
+  void                        SetClusName(const char *n)                           { fCaloName       = n          ; }
   void                        SetHistoBins(Int_t nbins, Float_t min, Float_t max)  { fNbins = nbins; fMinBinPt = min; fMaxBinPt = max; }
+  void                        SetPtCut(Float_t cut)                                { fPtCut          = cut        ; }
+  void                        SetTracksName(const char *n)                         { fTracksName     = n          ; }
 
  protected:
 
@@ -50,28 +50,25 @@ class AliAnalysisTaskEmcal : public AliAnalysisTaskSE {
   Bool_t                      AcceptTrack(AliVTrack            *track, Bool_t acceptMC = kFALSE)       const;
   BeamType                    GetBeamType()                                                                 ;
 
-  virtual Bool_t              FillHistograms()             { return kFALSE; }
+  virtual Bool_t              FillHistograms()             { return fCreateHisto; }
   virtual Bool_t              RetrieveEventObjects();
   virtual Bool_t              Run()                        { return kTRUE ; }
 
   EmcalAnaType                fAnaType;                    // analysis type
   Bool_t                      fInitialized;                // whether or not the task has been already initialized
   Bool_t                      fCreateHisto;                // whether or not create histograms
-
   TString                     fTracksName;                 // name of track collection
   TString                     fCaloName;                   // name of calo cluster collection
   Int_t                       fNbins;                      // no. of pt bins
   Float_t                     fMinBinPt;                   // min pt in histograms
   Float_t                     fMaxBinPt;                   // max pt in histograms
   Float_t                     fPtCut;                      // cut on particle pt
-
   TClonesArray               *fTracks;                     //!tracks
   TClonesArray               *fCaloClusters;               //!clusters
   Float_t                     fCent;                       //!event centrality
   Int_t                       fCentBin;                    //!event centrality bin
   Double_t                    fVertex[3];                  //!event vertex
   BeamType                    fBeamType;                   //!event beam type
-
   TList                      *fOutput;                     //!output list
 
  private:
