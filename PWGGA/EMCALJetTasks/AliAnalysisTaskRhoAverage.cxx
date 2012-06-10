de613999926f8d4ef6a342a9549553fc7963f766
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-06-10 20:13:49 +0000

    improved handling of input collections

--- PWGGA/EMCALJetTasks/AliAnalysisTaskRhoAverage.cxx
+++ PWGGA/EMCALJetTasks/AliAnalysisTaskRhoAverage.cxx
@@ -1,11 +1,13 @@
 // $Id$
 //
-// Calculation of rho, method: sum of all particle pt / full acceptance area
+// Calculation of rho, method: sum of all particle pt / full acceptance area.
 //
 // Authors: S. Aiola
 
-#include <TList.h>
+#include "AliAnalysisTaskRhoAverage.h"
+
 #include <TClonesArray.h>
+#include <TList.h>
 #include <TMath.h>
 
 #include "AliAnalysisManager.h"
@@ -16,23 +18,25 @@
 #include "AliVCluster.h"
 #include "AliVEventHandler.h"
 #include "AliVTrack.h"
-#include "AliAnalysisTaskRhoAverage.h"
 
 ClassImp(AliAnalysisTaskRhoAverage)
 
 //________________________________________________________________________
 AliAnalysisTaskRhoAverage::AliAnalysisTaskRhoAverage() : 
   AliAnalysisTaskRhoBase(),
-  fTracksName("tracks"),
-  fClustersName("caloClusters"),
-  fJetsName("KtJets"),
-  fEtaMin(-0.9),
-  fEtaMax(0.9),
+  fTracksName(),
+  fClustersName(),
+  fJetsName(),
+  fEtaMin(0),
+  fEtaMax(0),
   fPhiMin(0),
-  fPhiMax(2 * TMath::Pi()),
-  fPtMin(0.15)
+  fPhiMax(0),
+  fPtMin(0),
+  fClusters(0),
+  fJets(0),
+  fTracks(0)
 {
-  // Constructor
+  // Default constructor.
 }
 
 //________________________________________________________________________
@@ -45,10 +49,12 @@ AliAnalysisTaskRhoAverage::AliAnalysisTaskRhoAverage(const char *name) :
   fEtaMax(0.9),
   fPhiMin(0),
   fPhiMax(2 * TMath::Pi()),
-  fPtMin(0.15)
+  fPtMin(0.15),
+  fClusters(0),
+  fJets(0),
+  fTracks(0)
 {
-  // Constructor
-
+  // Constructor.
 }
 
 //________________________________________________________________________
@@ -56,57 +62,36 @@ void AliAnalysisTaskRhoAverage::UserExec(Option_t *)
 {
   // Main loop, called for each event.
   
-  AliAnalysisTaskRhoBase::UserExec("");
+  if (!fIsInit) {
+    ExecOnce();
+    fIsInit = 1;
+  }
 
   fRho->SetVal(-1);
 
-  TClonesArray *jets = 0;
-  TClonesArray *tracks = 0;
-  TClonesArray *clusters = 0;
-
-  tracks = dynamic_cast<TClonesArray*>(InputEvent()->FindListObject(fTracksName));
-  if (!tracks) {
-  AliError(Form("Pointer to tracks %s == 0", fTracksName.Data() ));
-   return;
-  }
-
-  if (fClustersName != "") {
-    clusters = dynamic_cast<TClonesArray*>(InputEvent()->FindListObject(fClustersName));
-    if (!clusters) {
-      AliError(Form("Pointer to clusters %s == 0", fClustersName.Data() ));
-      return;
-    }
-  }
-
-  if (fJetsName != "") {
-    jets = dynamic_cast<TClonesArray*>(InputEvent()->FindListObject(fJetsName));
-    if (!jets) {
-      AliError(Form("Pointer to jets %s == 0", fJetsName.Data() ));
-      return;
-    }
-  }
-
   Double_t rho = 0;
   
-  Int_t Ntracks = tracks->GetEntries();
+  Int_t Ntracks = 0;
+  if (fTracks) 
+    Ntracks = fTracks->GetEntriesFast();
 
   Int_t Nclusters = 0;
-  if (clusters)
-    Nclusters = clusters->GetEntries();
+  if (fClusters)
+    Nclusters = fClusters->GetEntriesFast();
 
   Int_t Njets = 0;
-  if (jets)
-    Njets = jets->GetEntries();
+  if (fJets)
+    Njets = fJets->GetEntriesFast();
 
-  Float_t maxJetPt = 0;
+  Double_t maxJetPt = 0;
   Int_t maxJetId = -1;
   AliEmcalJet *maxJet = 0;
   for (Int_t ij = 0; ij < Njets; ij++) {
       
-    AliEmcalJet *jet = static_cast<AliEmcalJet*>(jets->At(ij));
+    AliEmcalJet *jet = static_cast<AliEmcalJet*>(fJets->At(ij));
   
     if (!jet) {
-      AliError(Form("Could not receive jet %d", ij));
+      AliError(Form("%s: Could not receive jet %d", GetName(), ij));
       continue;
     } 
   
@@ -117,14 +102,14 @@ void AliAnalysisTaskRhoAverage::UserExec(Option_t *)
   }
 
   if (maxJetId >= 0)
-    maxJet = static_cast<AliEmcalJet*>(jets->At(maxJetId));
+    maxJet = static_cast<AliEmcalJet*>(fJets->At(maxJetId));
 
-  for (Int_t it = 0; it < Ntracks; it++) {
+  for (Int_t it = 0; it < Ntracks; ++it) {
       
-    AliVTrack *track = static_cast<AliVTrack*>(tracks->At(it));
+    AliVTrack *track = static_cast<AliVTrack*>(fTracks->At(it));
   
     if (!track) {
-      AliError(Form("Could not receive track %d", it));
+      AliError(Form("%s: Could not receive track %d", GetName(), it));
       continue;
     } 
 
@@ -143,12 +128,12 @@ void AliAnalysisTaskRhoAverage::UserExec(Option_t *)
   Double_t vertex[] = {0, 0, 0};
   InputEvent()->GetPrimaryVertex()->GetXYZ(vertex);
 
-  for (Int_t ic = 0; ic < Nclusters; ic++) {
+  for (Int_t ic = 0; ic < Nclusters; ++ic) {
       
-    AliVCluster *cluster = static_cast<AliVCluster*>(clusters->At(ic));
+    AliVCluster *cluster = static_cast<AliVCluster*>(fClusters->At(ic));
   
     if (!cluster) {
-      AliError(Form("Could not receive cluster %d", ic));
+      AliError(Form("%s: Could not receive cluster %d", GetName(), ic));
       continue;
     } 
 
@@ -177,10 +162,45 @@ void AliAnalysisTaskRhoAverage::UserExec(Option_t *)
   if (maxJet)
     area -= maxJet->Area();
 
-  rho /= area;
-
-  fRho->SetVal(rho);
+  if (area>0) {
+    rho /= area;
+    fRho->SetVal(rho);
+  } else {
+    AliError(Form("%s: Area negative %f", GetName(), area));
+  }
 }      
+
+//________________________________________________________________________
+void AliAnalysisTaskRhoAverage::Terminate(Option_t *) 
+{
+  // Called at the end of the analysis.
+}
+
+//________________________________________________________________________
+void AliAnalysisTaskRhoAverage::ExecOnce() 
+{
+  // Initialize some settings that need to be determined in UserExec.
+
+  AliAnalysisTaskRhoBase::ExecOnce();
+
+  fClusters = dynamic_cast<TClonesArray*>(InputEvent()->FindListObject(fClustersName));
+  if (!fClusters) {
+    AliError(Form("%s: Pointer to jets %s == 0", GetName(), fClustersName.Data() ));
+    return;
+  }
+
+  fJets = dynamic_cast<TClonesArray*>(InputEvent()->FindListObject(fJetsName));
+  if (!fJets) {
+    AliError(Form("%s: Pointer to jets %s == 0", GetName(), fJetsName.Data() ));
+    return;
+  }
+
+  fTracks = dynamic_cast<TClonesArray*>(InputEvent()->FindListObject(fTracksName));
+  if (!fTracks) {
+    AliError(Form("%s: Pointer to tracks %s == 0", GetName(), fTracksName.Data() ));
+    return;
+  }
+}
 
 //________________________________________________________________________
 Bool_t AliAnalysisTaskRhoAverage::IsJetTrack(AliEmcalJet* jet, Int_t itrack) const
@@ -206,11 +226,4 @@ Bool_t AliAnalysisTaskRhoAverage::IsJetCluster(AliEmcalJet* jet, Int_t iclus) co
       return kTRUE;
   }
   return kFALSE;
-}
-
-
-//________________________________________________________________________
-void AliAnalysisTaskRhoAverage::Terminate(Option_t *) 
-{
-  // Called at the end of the analysis.
 }
