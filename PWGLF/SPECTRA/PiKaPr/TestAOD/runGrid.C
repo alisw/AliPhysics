bed3d4aa286e69e8aa5e1bf2b11603c51fc5479d
lmilano <lmilano@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-06-19 16:10:28 +0000

    runGrid macro updated

--- PWGLF/SPECTRA/PiKaPr/TestAOD/runGrid.C
+++ PWGLF/SPECTRA/PiKaPr/TestAOD/runGrid.C
@@ -73,8 +73,16 @@ void runGrid(TString mode="test",Bool_t mc=kTRUE,Int_t day=15,Int_t month=6, Int
   Double_t DCA=100000;
   UInt_t minNclsTPC=70;
   for(Int_t icut=0;icut<4;icut++){
-    if(icut!=0)continue;
-    AddTaskSpectraAOD(mc,CentCutMin[icut],CentCutMax[icut],QvecCutMin[icut],QvecCutMax[icut],EtaMin[icut],EtaMax[icut],Nsigmapid,pt,p,y,ptTofMatch,trkbit,trkbitQVector,UseCentPatchAOD049,DCA,minNclsTPC);
+    //if(icut!=0)continue;
+    AliAnalysisTaskSpectraAOD *taskAOD =AddTaskSpectraAOD(mc,CentCutMin[icut],CentCutMax[icut],QvecCutMin[icut],QvecCutMax[icut],EtaMin[icut],EtaMax[icut],Nsigmapid,pt,p,y,ptTofMatch,trkbit,trkbitQVector,UseCentPatchAOD049,DCA,minNclsTPC);
+    
+    taskAOD->GetOutputSlot(1)->GetContainer()->SetName(Form("%s_%s",taskAOD->GetOutputSlot(1)->GetContainer()->GetName(),taskAOD->GetName()));
+    taskAOD->GetOutputSlot(2)->GetContainer()->SetName(Form("%s_%s",taskAOD->GetOutputSlot(2)->GetContainer()->GetName(),taskAOD->GetName()));
+    taskAOD->GetOutputSlot(3)->GetContainer()->SetName(Form("%s_%s",taskAOD->GetOutputSlot(3)->GetContainer()->GetName(),taskAOD->GetName()));
+    taskAOD->GetOutputSlot(4)->GetContainer()->SetName(Form("%s_%s",taskAOD->GetOutputSlot(4)->GetContainer()->GetName(),taskAOD->GetName()));
+    
+    //cout<<"OutSlot :"<<taskAOD->GetOutputSlot(1)->GetContainer()->GetName()<<endl;
+    //return;
   }
   
   mgr->SetDebugLevel(2);
