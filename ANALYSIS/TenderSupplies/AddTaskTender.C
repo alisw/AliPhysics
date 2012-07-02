d20279730dcef27bf836720f97eff2949b8edc3e
shahoian <shahoian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-02 15:44:27 +0000

    Include 1/pt fixing supply to the train (but switched off)

--- ANALYSIS/TenderSupplies/AddTaskTender.C
+++ ANALYSIS/TenderSupplies/AddTaskTender.C
@@ -5,7 +5,8 @@ AliAnalysisTask *AddTaskTender(Bool_t useV0=kFALSE,
                                Bool_t usePID=kTRUE,
                                Bool_t useVTX=kTRUE,
                                Bool_t useT0=kTRUE,
-                               Bool_t useEmc=kFALSE)
+                               Bool_t useEmc=kFALSE,
+			       Bool_t usePtFix=kFALSE)
 {
   if (!(useV0 | useTPC | useTOF | useTRD | usePID | useVTX | useEmc)) {
      ::Error("AddTaskTender", "No supply added to tender, so tender not created");
@@ -55,6 +56,13 @@ AliAnalysisTask *AddTaskTender(Bool_t useV0=kFALSE,
      tpcSupply->SetDebugLevel(2);
      //tpcSupply->SetMip(50.);
      tender->AddSupply(tpcSupply);
+  }   
+
+  //========= Attach track 1/pt correction supply ======
+  if (usePtFix) {
+     AliTrackFixTenderSupply *trfixSupply=new AliTrackFixTenderSupply("PTInvFix");
+     //trfixSupply->SetDebugLevel(2);
+     tender->AddSupply(trfixSupply);
   }   
 
   //========= Attach T0 supply ======
