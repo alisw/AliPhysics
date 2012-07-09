b447b755a9da41e23c299d8b11e10e9d932a7462
mcosenti <mcosenti@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-09 15:20:22 +0000

    changes from mconnors

--- PWGGA/EMCALJetTasks/macros/AddTaskEmcalJetHMEC.C
+++ PWGGA/EMCALJetTasks/macros/AddTaskEmcalJetHMEC.C
@@ -9,7 +9,9 @@ AliAnalysisTaskEmcalJetHMEC* AddTaskEmcalJetHMEC(
    const Double_t minEta      = -0.3,
    const Double_t maxEta      = 0.3,
    const Double_t minArea     = 0.4,
-   const Int_t EvtMix         = 0                                                 
+   const Int_t EvtMix         = 0,
+   const Double_t TrkBias     = 5,
+   const Double_t ClusBias    = 5                                                 
 )
 {  
   // Get the pointer to the existing analysis manager via the static access method.
@@ -41,6 +43,9 @@ AliAnalysisTaskEmcalJetHMEC* AddTaskEmcalJetHMEC(
   correlationtask->SetJetEta(minEta,maxEta);
   correlationtask->SetAreaCut(minArea);
   correlationtask->SetEventMixing(EvtMix);
+  correlationtask->SetTrkBias(TrkBias);
+  correlationtask->SetClusBias(TrkBias);
+
  
   //-------------------------------------------------------
   // Final settings, pass to manager and set the containers
