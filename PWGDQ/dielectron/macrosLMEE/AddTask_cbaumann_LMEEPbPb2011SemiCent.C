b19fdd2a24e655bdbe86cdc9e4d37ae453c384a5
cbaumann <cbaumann@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-09 13:51:46 +0000

    update Tasks

--- PWGDQ/dielectron/macrosLMEE/AddTask_cbaumann_LMEEPbPb2011SemiCent.C
+++ PWGDQ/dielectron/macrosLMEE/AddTask_cbaumann_LMEEPbPb2011SemiCent.C
@@ -1,9 +1,9 @@
-AliAnalysisTask *AddTask_cbaumann_LMEEPbPb2011SemiCent(Bool_t runRejection=kFALSE, Bool_t setMC=kFALSE,Bool_t enableCF=kFALSE){
+AliAnalysisTask *AddTask_cbaumann_LMEEPbPb2011SemiCent(Bool_t runRejection=kFALSE, Bool_t setMC=kFALSE,Bool_t enableCF=kFALSE, Bool_t switchToPhiV=kTRUE){
   //get the current analysis manager
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
-    Error("AddTask_cbaumann_LMEEPbPb2011", "No analysis manager found.");
-    return 0;
+	Error("AddTask_cbaumann_LMEEPbPb2011", "No analysis manager found.");
+	return 0;
   }
 
   //create config File names: TRAIN_ROOT is for running on GSI train, 
@@ -19,20 +19,20 @@ AliAnalysisTask *AddTask_cbaumann_LMEEPbPb2011SemiCent(Bool_t runRejection=kFALS
 
   //AOD Usage currently tested with separate task, to be merged
   if (mgr->GetInputEventHandler()->IsA()==AliAODInputHandler::Class()){
-    ::Info("AddTaskLMEEPbPb2011", "no dedicated AOD configuration");
+	::Info("AddTaskLMEEPbPb2011", "no dedicated AOD configuration");
   }
 
   //Do we have an MC handler?
   Bool_t hasMC=setMC;
   if (AliAnalysisManager::GetAnalysisManager()->GetMCtruthEventHandler()!=0x0)
-    hasMC=kTRUE;
+	hasMC=kTRUE;
 
 
 
   if (!gROOT->GetListOfGlobalFunctions()->FindObject(configLMEECutLib.Data()))
-    gROOT->LoadMacro(configLMEECutLibPath.Data());
+	gROOT->LoadMacro(configLMEECutLibPath.Data());
   if (!gROOT->GetListOfGlobalFunctions()->FindObject(configFile.Data()))
-    gROOT->LoadMacro(configFilePath.Data());
+	gROOT->LoadMacro(configFilePath.Data());
 
   LMEECutLib* cutlib = new LMEECutLib();
   cutlib->SetMCFlag(hasMC);
@@ -47,54 +47,65 @@ AliAnalysisTask *AddTask_cbaumann_LMEEPbPb2011SemiCent(Bool_t runRejection=kFALS
   //add dielectron analysis with different cuts to the task
   if (runRejection) {
 
-    AliDielectron *lowmass4=ConfigLMEEPbPb2011(4,hasMC,enableCF);
-    task->AddDielectron(lowmass4);
-    printf("add: %s\n",lowmass4->GetName());
+	AliDielectron *lowmass4=ConfigLMEEPbPb2011(4,hasMC,enableCF);
+	task->AddDielectron(lowmass4);
+	printf("add: %s\n",lowmass4->GetName());
   }
   else {
 
-    AliDielectron *lowmass3=ConfigLMEEPbPb2011(3,hasMC,enableCF);
-    lowmass3->SetUseKF(kFALSE);
-	task->AddDielectron(lowmass3);
-    printf("add: %s\n",lowmass3->GetName());
+	if (switchToPhiV) {
+
+	  AliDielectron *lowmass10=ConfigLMEEPbPb2011(10,hasMC,enableCF);
+	  lowmass10->SetUseKF(kFALSE);
+	  task->AddDielectron(lowmass10);
+	  printf("add: %s\n",lowmass10->GetName());
+
+
+	}
+	else {
+	  AliDielectron *lowmass2=ConfigLMEEPbPb2011(2,hasMC,enableCF);
+	  lowmass2->SetUseKF(kFALSE);
+	  task->AddDielectron(lowmass2);
+	  printf("add: %s\n",lowmass2->GetName());
+	}
   }
 
   mgr->AddTask(task);
 
   //create output container
   AliAnalysisDataContainer *coutput1 =
-    mgr->CreateContainer("cbaumann_LMEEPbPb2011SemiCent_tree",
-	TTree::Class(),
-	AliAnalysisManager::kExchangeContainer,
-	"cbaumann_LMEEPbPb2011SemiCent_default.root");
+	mgr->CreateContainer("cbaumann_LMEEPbPb2011SemiCent_tree",
+		TTree::Class(),
+		AliAnalysisManager::kExchangeContainer,
+		"cbaumann_LMEEPbPb2011SemiCent_default.root");
 
   AliAnalysisDataContainer *cOutputHist1 =
-    mgr->CreateContainer("cbaumann_LMEEPbPb2011SemiCent_out",
-	TList::Class(),
-	AliAnalysisManager::kOutputContainer,
-	"cbaumann_LMEEPbPb2011SemiCent_out.root");
-  
+	mgr->CreateContainer("cbaumann_LMEEPbPb2011SemiCent_out",
+		TList::Class(),
+		AliAnalysisManager::kOutputContainer,
+		"cbaumann_LMEEPbPb2011SemiCent_out.root");
+
   AliAnalysisDataContainer *cOutputHist2 = 0x0;
   if (enableCF) {
-    cOutputHist2 = 
-      mgr->CreateContainer("cbaumann_LMEEPbPb2011SemiCent_CF",
-	  TList::Class(),
-	  AliAnalysisManager::kOutputContainer,
-	  "cbaumann_LMEEPbPb2011SemiCent_out.root");
+	cOutputHist2 = 
+	  mgr->CreateContainer("cbaumann_LMEEPbPb2011SemiCent_CF",
+		  TList::Class(),
+		  AliAnalysisManager::kOutputContainer,
+		  "cbaumann_LMEEPbPb2011SemiCent_out.root");
 
   }
   AliAnalysisDataContainer *cOutputHist3 =
-    mgr->CreateContainer("cbaumann_EventStatPbPb2011SemiCent",
-	TH1D::Class(),
-	AliAnalysisManager::kOutputContainer,
-	"cbaumann_LMEEPbPb2011SemiCent_out.root");
+	mgr->CreateContainer("cbaumann_EventStatPbPb2011SemiCent",
+		TH1D::Class(),
+		AliAnalysisManager::kOutputContainer,
+		"cbaumann_LMEEPbPb2011SemiCent_out.root");
 
 
   mgr->ConnectInput(task,  0, mgr->GetCommonInputContainer());
   mgr->ConnectOutput(task, 0, coutput1 );
   mgr->ConnectOutput(task, 1, cOutputHist1);
   if (enableCF) {
-    mgr->ConnectOutput(task, 2, cOutputHist2);
+	mgr->ConnectOutput(task, 2, cOutputHist2);
   }
   mgr->ConnectOutput(task, 3, cOutputHist3);
 
