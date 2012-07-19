ff1a0291438674fdcd63ee36c81346808c8f01f7
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-19 21:14:16 +0000

    Update of the flatenning task and macros

--- PWGPP/VZERO/runAnaEPFlatenningProof.C
+++ PWGPP/VZERO/runAnaEPFlatenningProof.C
@@ -16,23 +16,42 @@ void runAnaEPFlatenningProof(Int_t mode = 0, const char *folder = "/alice/data",
     Char_t *alienuser = gSystem->Getenv("alien_API_USER");
     cout<<"==> Your AliEn username is: "<<alienuser<<endl;
 
+    //    TProof::Mgr("alice-caf.cern.ch")->SetROOTVersion("VO_ALICE@ROOT::v5-33-02a");
+
     TProof *p = TProof::Open(alienuser!=0 ? Form("%s@alice-caf.cern.ch",
 						 alienuser) : "alice-caf.cern.ch",
 			     workers>0 ? Form("workers=%d",workers) : "");
 
-    gProof->EnablePackage("VO_ALICE@AliRoot::v5-02-16-AN");
+    gProof->EnablePackage("VO_ALICE@AliRoot::v5-03-23-AN");
+
+    if (1) {
+      gProof->Exec("TGrid::Connect(\"alien://\")",kTRUE);
+      gSystem->Setenv("OADB_PATH","alien:///alice/cern.ch/user/c/cheshkov/OADB");
+      gProof->Exec("gSystem->Setenv(\"OADB_PATH\",\"alien:///alice/cern.ch/user/c/cheshkov/OADB\")",kTRUE);
+    }
   }
 
   // Create the analysis manager
   AliAnalysisManager *mgr = new AliAnalysisManager("EPFlatenningAnalysis");
 
-  AliESDInputHandler* esdH = new AliESDInputHandler();
-  esdH->SetInactiveBranches("FMD AliRawDataErrorLogs CaloClusters Cascades EMCALCells EMCALTrigger ESDfriend.fTracks Kinks MuonTracks TrdTracks");
-  esdH->SetReadFriends(kFALSE);
-  mgr->SetInputEventHandler(esdH);
+  Bool_t esdData = kTRUE;
+  TString tempStr1 = folder;
+  if (tempStr1.Contains("AOD")) esdData = kFALSE;
+  TString tempStr2 = dataset;
+  if (tempStr2.Contains("AOD")) esdData = kFALSE;
+  if (esdData) {
+    AliESDInputHandler* esdH = new AliESDInputHandler();
+    esdH->SetInactiveBranches("FMD AliRawDataErrorLogs CaloClusters Cascades EMCALCells EMCALTrigger ESDfriend.fTracks Kinks MuonTracks TrdTracks");
+    esdH->SetReadFriends(kFALSE);
+    mgr->SetInputEventHandler(esdH);
+  }
+  else {
+    AliAODInputHandler* aodH = new AliAODInputHandler();
+    mgr->SetInputEventHandler(aodH);
+  }
 
   // physics and centrality selection
-  if (usePS) {
+  if (esdData && usePS) {
     gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPhysicsSelection.C");
     AliPhysicsSelectionTask *physicsSelectionTask = AddTaskPhysicsSelection(kFALSE);
   }
@@ -42,6 +61,12 @@ void runAnaEPFlatenningProof(Int_t mode = 0, const char *folder = "/alice/data",
 
   // Create task
   if (mode==0 || mode==2) {
+    if (0) {
+      gProof->Load(Form("%s/STEER/STEERBase/AliEventplane.cxx++g",
+			gSystem->Getenv("ALICE_ROOT")));
+      gProof->Load(Form("%s/ANALYSIS/AliVZEROEPSelectionTask.cxx++g",
+			gSystem->Getenv("ALICE_ROOT")));
+    }
     gProof->Load(Form("%s/AliAnaVZEROEPFlatenning.cxx++g",
 		      gSystem->pwd()));
   }
@@ -49,10 +74,14 @@ void runAnaEPFlatenningProof(Int_t mode = 0, const char *folder = "/alice/data",
     gROOT->LoadMacro(Form("%s/AliAnaVZEROEPFlatenning.cxx++g",
 			  gSystem->pwd()));
   }
+
+  gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskVZEROEPSelection.C");
+  AliVZEROEPSelectionTask *selTask = AddTaskVZEROEPSelection();
+
   AliAnaVZEROEPFlatenning *task = new AliAnaVZEROEPFlatenning("AliAnaVZEROEPFlatenning");
   task->SetMBTrigName(minBias);
   if (usePS) task->SetUsePhysSel(kTRUE);
-  task->SetInput("VZERO.EPFlatenning.PS.LHC11h_000169683_p1_muon_ESDs.root");
+  task->SetInput("/home/cheshkov/alice/AliRoot/PWGPP/VZERO/save/VZERO.EPFlatenning.PS.LHC11h_000170162_p1_muon_.root");
   
   // Add task
   mgr->AddTask(task);
@@ -66,7 +95,6 @@ void runAnaEPFlatenningProof(Int_t mode = 0, const char *folder = "/alice/data",
   // Connect input/output
   mgr->ConnectInput(task, 0, cinput);
   mgr->ConnectOutput(task, 1, coutput);
-
 
   // Enable debug printouts
   mgr->SetDebugLevel(3);
