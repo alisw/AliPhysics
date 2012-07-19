49c3ac782c95b096617c62f923b483f7a97c209e
dsilverm <dsilverm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-19 15:11:10 +0000

    from Francesco Blanco - skip a few min/max settings

--- EMCAL/AliEMCALQADataMakerRec.cxx
+++ EMCAL/AliEMCALQADataMakerRec.cxx
@@ -477,8 +477,8 @@ void AliEMCALQADataMakerRec::InitRaws()
   Add2RawsList(h15, k2DRatioAmp, expert, image, !saveCorr) ;
 
   TH1F * h16 = new TH1F("hRatioDist", "Amplitude_{current run}/Amplitude_{reference run} ratio distribution", nTot, 0., 2.);
-  h16->SetMinimum(0.1); 
-  h16->SetMaximum(100.);
+  // h16->SetMinimum(0.1); 
+  // h16->SetMaximum(100.);
   gStyle->SetOptStat(0);
   h16->UseCurrentStyle();
   h16->SetDirectory(0);
@@ -498,7 +498,7 @@ void AliEMCALQADataMakerRec::InitRaws()
   Add2RawsList(hL10, kLEDMonRatio, expert, image, !saveCorr) ;
 
   TH1F * hL11 = new TH1F("hMaxMinusMinLEDMonRatioDist", "LEDMon amplitude, Ratio distribution", nTotLEDMon, 0, 2);
-  hL11->SetMinimum(0.1) ;
+  // hL11->SetMinimum(0.1) ;
   gStyle->SetOptStat(0);
   hL11->UseCurrentStyle();
   hL11->SetDirectory(0);
