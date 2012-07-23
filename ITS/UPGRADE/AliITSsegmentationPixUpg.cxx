253d601431daf11cfce2178daa157582149ccc12
shahoian <shahoian@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-23 16:36:17 +0000

    Generic pixel segmentation

---
+++ ITS/UPGRADE/AliITSsegmentationPixUpg.cxx
@@ -0,0 +1,366 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/* $Id: AliITSsegmentationPixUpg.cxx 47180 2011-02-08 09:42:29Z masera $ */
+#include <TGeoManager.h>
+#include <TGeoVolume.h>
+#include <TGeoBBox.h>
+#include "AliITSsegmentationPixUpg.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Segmentation class for pixels                                                                          //
+// Questions to solve: are guardrings needed and do they belong to the sensor or to the module in TGeo    //
+//                     At the moment assume that the local coord syst. is located at bottom left corner   //
+//                     of the ACTIVE matrix. If the guardring to be accounted in the local coords, in     //
+//                     the Z and X conversions one needs to first subtract the  fGuardLft and fGuardBot   //
+//                     from the local Z,X coordinates                                                     //
+//                                                                                                        //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ClassImp(AliITSsegmentationPixUpg)
+
+//_____________________________________________________________________________RS
+AliITSsegmentationPixUpg::AliITSsegmentationPixUpg(int nchips,int ncol,int nrow,
+						   double pitchX,double pitchZ,
+						   double pitchLftC,double pitchRgtC,
+						   double thickness,
+						   double edgL,double edgR,double edgT,double edgB)
+: AliITSsegmentation()
+  ,fGuardLft(edgL)
+  ,fGuardRgt(edgR)
+  ,fGuardTop(edgT)
+  ,fGuardBot(edgB)
+  ,fPitchX(pitchX)
+  ,fPitchZ(pitchZ)
+  ,fPitchZLftCol(pitchLftC<0 ? pitchZ:pitchLftC)
+  ,fPitchZRgtCol(pitchRgtC<0 ? pitchZ:pitchRgtC)
+  ,fChipDZ(0)
+  ,fNChips(nchips)
+  ,fNColPerChip(nchips>0 ? ncol/nchips:0)
+  ,fNRow(nrow)
+  ,fNCol(ncol)
+{
+  // Default constructor, sizes in microns
+  fChipDZ = (fNColPerChip-2)*fPitchZ + fPitchZLftCol + fPitchZRgtCol;
+  SetDetSize( fNRow*fPitchX /*+fGuardTop+fGuardBot*/,
+	      fNChips*fChipDZ /*+fGuardLft+fGuardRgt*/,
+	      thickness);
+  //
+}
+
+//_____________________________________________________________________________RS
+void AliITSsegmentationPixUpg::GetPadIxz(Float_t x,Float_t z,Int_t &ix,Int_t &iz) const 
+{
+  //  Returns pixel coordinates (ix,iz) for given real local coordinates (x,z)
+  //  expects x, z in microns
+  ix = int(x/fPitchX) + 1;     
+  iz = int(Z2Col(z)) + 1;
+  //  
+  if (iz >  fNCol) iz= fNCol;
+  if (ix >  fNRow) ix= fNRow;
+  //
+}
+
+//_____________________________________________________________________________RS
+void AliITSsegmentationPixUpg::GetPadTxz(Float_t &x,Float_t &z) const
+{
+  //  local transformation of real local coordinates (x,z)
+  //  expects x, z in microns
+  x /= fPitchX;
+  z = Z2Col(z);
+  //
+}
+
+//_____________________________________________________________________________RS
+void AliITSsegmentationPixUpg::GetPadCxz(Int_t ix,Int_t iz,Float_t &x,Float_t&z) const
+{
+  // Transform from pixel to real local coordinates
+  // returns x, z in microns
+  x = (ix>0) ? Float_t((ix-0.5)*fPitchX) : Float_t((ix+0.5)*fPitchX);
+  z = Col2Z(iz);
+  //
+}
+
+//_____________________________________________________________________________RS
+Float_t AliITSsegmentationPixUpg::Z2Col(Float_t z) const 
+{
+  // get column number (from 0) from local Z
+  int chip = z/fChipDZ;
+  float col = chip*fNColPerChip;
+  z -= chip*fChipDZ;
+  if (z<fPitchZLftCol) col += z/fPitchZLftCol;
+  else {
+    z = fPitchZLftCol;
+    col += 1;
+    if (z<(fChipDZ-fPitchZRgtCol)) col += 1+z/fPitchZ;
+    else col += 1+(z - (fNColPerChip-2)*fPitchZ)/fPitchZRgtCol;
+  }
+  return col;
+}
+
+//_____________________________________________________________________________RS
+Float_t AliITSsegmentationPixUpg::Col2Z(Int_t col) const 
+{
+  // convert column number (from 0) to Z coordinate
+  int nchip = col/fNColPerChip;
+  col %= fNColPerChip;
+  float z = nchip*fChipDZ;
+  if (!col) z -= fPitchZRgtCol/2;
+  else if (col==1) z += fPitchZLftCol/2;
+  else if (col==fNColPerChip-1) z += fChipDZ - fPitchZRgtCol/2;
+  else    z += fPitchZLftCol + (col-0.5)*fChipDZ;
+  return z;
+  //
+}
+
+//______________________________________________________________________RS
+AliITSsegmentationPixUpg& AliITSsegmentationPixUpg::operator=(const AliITSsegmentationPixUpg &src)
+{
+  // = operator
+  if(this==&src) return *this;
+  AliITSsegmentation::operator=(src);
+  fNCol  = src.fNCol;
+  fNRow  = src.fNRow;
+  fNColPerChip  = src.fNColPerChip;
+  fNChips = src.fNChips;
+  fChipDZ = src.fChipDZ;
+  fPitchZRgtCol = src.fPitchZRgtCol;
+  fPitchZLftCol = src.fPitchZLftCol;
+  fPitchZ = src.fPitchZ;
+  fPitchX = src.fPitchX;
+  //
+  fGuardBot = src.fGuardBot;
+  fGuardTop = src.fGuardTop;
+  fGuardRgt = src.fGuardRgt;
+  fGuardLft = src.fGuardLft;
+  //
+  return *this;
+}
+
+//____________________________________________________________________________RS
+AliITSsegmentationPixUpg::AliITSsegmentationPixUpg(const AliITSsegmentationPixUpg &src) :
+  AliITSsegmentation(src)
+  ,fGuardLft(src.fGuardLft)
+  ,fGuardRgt(src.fGuardRgt)
+  ,fGuardTop(src.fGuardTop)
+  ,fGuardBot(src.fGuardBot)
+  ,fPitchX(src.fPitchX)
+  ,fPitchZ(src.fPitchZ)
+  ,fPitchZLftCol(src.fPitchZLftCol)
+  ,fPitchZRgtCol(src.fPitchZRgtCol)
+  ,fChipDZ(src.fChipDZ)
+  ,fNChips(src.fNChips)
+  ,fNColPerChip(src.fNColPerChip)
+  ,fNRow(src.fNRow)
+  ,fNCol(src.fNCol)  
+{
+}
+
+//____________________________________________________________________________RS
+Float_t AliITSsegmentationPixUpg::Dpx(Int_t ) const 
+{
+  //returs x pixel pitch for a give pixel
+  return fPitchX;
+}
+
+//____________________________________________________________________________RS
+Float_t AliITSsegmentationPixUpg::Dpz(Int_t col) const 
+{
+  // returns z pixel pitch for a given pixel (cols starts from 0)
+  col %= fNColPerChip;
+  if (!col) return fPitchZLftCol;
+  if (col==fNColPerChip-1) return fPitchZRgtCol;
+  return fPitchZ;
+  //
+}
+
+//------------------------------
+void AliITSsegmentationPixUpg::Neighbours(Int_t iX, Int_t iZ, Int_t* nlist, Int_t xlist[8], Int_t zlist[8]) const 
+{
+  // returns the neighbouring pixels for use in Cluster Finders and the like.
+  //
+  *nlist=8;
+  xlist[0]=xlist[1]=iX;
+  xlist[2]=iX-1;
+  xlist[3]=iX+1;
+  zlist[0]=iZ-1;
+  zlist[1]=iZ+1;
+  zlist[2]=zlist[3]=iZ;
+  // Diagonal elements
+  xlist[4]=iX+1;
+  zlist[4]=iZ+1;
+  //  
+  xlist[5]=iX-1;
+  zlist[5]=iZ-1;
+  //
+  xlist[6]=iX-1;
+  zlist[6]=iZ+1;
+  //
+  xlist[7]=iX+1;
+  zlist[7]=iZ-1;
+  //
+}
+
+//______________________________________________________________________
+Bool_t AliITSsegmentationPixUpg::LocalToDet(Float_t x,Float_t z,Int_t &ix,Int_t &iz) const 
+{
+  // Transformation from Geant detector centered local coordinates (cm) to
+  // Pixel cell numbers ix and iz.
+  // Input:
+  //    Float_t   x        detector local coordinate x in cm with respect to
+  //                       the center of the sensitive volume.
+  //    Float_t   z        detector local coordinate z in cm with respect to
+  //                       the center of the sensitive volulme.
+  // Output:
+  //    Int_t    ix        detector x cell coordinate. Has the range 
+  //                       0<=ix<fNRow.
+  //    Int_t    iz        detector z cell coordinate. Has the range 
+  //                       0<=iz<fNCol.
+  // Return:
+  //   kTRUE if point x,z is inside sensitive volume, kFALSE otherwise.
+  //   A value of -1 for ix or iz indecates that this point is outside of the
+  //   detector segmentation as defined.
+  x = x*kCM2MC+0.5*Dx();
+  z = z*kCM2MC+0.5*Dz();
+  ix = iz = -1;
+  if(x<0 || x>Dx()) return kFALSE; // outside x range.
+  if(z<0 || z>Dz()) return kFALSE; // outside z range.
+  ix = int(x/fPitchX);
+  iz = Z2Col(z);
+  return kTRUE; // Found ix and iz, return.
+}
+
+//______________________________________________________________________
+void AliITSsegmentationPixUpg::DetToLocal(Int_t ix,Int_t iz,Float_t &x,Float_t &z) const
+{
+// Transformation from Detector cell coordiantes to Geant detector centerd 
+// local coordinates (cm).
+// Input:
+// Int_t    ix        detector x cell coordinate. Has the range 0<=ix<fNRow.
+// Int_t    iz        detector z cell coordinate. Has the range 0<=iz<fNCol.
+// Output:
+// Float_t   x        detector local coordinate x in cm with respect to the
+//                    center of the sensitive volume.
+// Float_t   z        detector local coordinate z in cm with respect to the
+//                    center of the sensitive volulme.
+// If ix and or iz is outside of the segmentation range a value of -0.5*Dx()
+// or -0.5*Dz() is returned.
+  //
+  x = -0.5/kCM2MC*Dx(); // default value.
+  z = -0.5/kCM2MC*Dz(); // default value.
+  // RS: to check: why we don't do strict check for [0:n)
+  if(ix<0 || ix>=fNRow) return; // outside of detector 
+  if(iz<0 || iz>=fNCol) return; // outside of detctor
+  x += (ix+0.5)*fPitchX*(1./kCM2MC);       // RS: we go to the center of the pad, i.e. + pitch/2, not to the boundary as in SPD
+  z += Col2Z(iz)*(1./kCM2MC); 
+  return; // Found x and z, return.
+}
+
+//______________________________________________________________________
+void AliITSsegmentationPixUpg::CellBoundries(Int_t ix,Int_t iz,Double_t &xl,Double_t &xu,Double_t &zl,Double_t &zu) const
+{
+  // Transformation from Detector cell coordiantes to Geant detector centerd 
+  // local coordinates (cm).
+  // Input:
+  // Int_t    ix        detector x cell coordinate. Has the range 0<=ix<fNRow.
+  // Int_t    iz        detector z cell coordinate. Has the range 0<=iz<fNCol.
+  // Output:
+  // Double_t   xl       detector local coordinate cell lower bounds x in cm
+  //                    with respect to the center of the sensitive volume.
+  // Double_t   xu       detector local coordinate cell upper bounds x in cm 
+  //                    with respect to the center of the sensitive volume.
+  // Double_t   zl       detector local coordinate lower bounds z in cm with
+  //                    respect to the center of the sensitive volulme.
+  // Double_t   zu       detector local coordinate upper bounds z in cm with 
+  //                    respect to the center of the sensitive volulme.
+  // If ix and or iz is outside of the segmentation range a value of -0.5*Dx()
+  // and -0.5*Dx() or -0.5*Dz() and -0.5*Dz() are returned.
+  Float_t x,z;
+  DetToLocal(ix,iz,x,z);
+  //
+  if( ix<0 || ix>=fNRow || iz<0 || iz>=fNCol) {
+    xl = xu = -0.5/kCM2MC*Dx(); // default value.
+    zl = zu = -0.5/kCM2MC*Dz(); // default value.
+    return; // outside of detctor
+  }
+  float zpitchH = Dpz(iz)/2./kCM2MC;
+  float xpitchH = fPitchX/2./kCM2MC;
+  xl -= xpitchH;
+  xu += xpitchH;
+  zl -= zpitchH;
+  zu += zpitchH;
+  return; // Found x and z, return.
+}
+
+//______________________________________________________________________
+Int_t AliITSsegmentationPixUpg::GetChipFromChannel(Int_t, Int_t iz) const 
+{
+  // returns chip number (in range 0-4) starting from channel number
+  if(iz>=fNCol  || iz<0 ){
+    AliWarning("Bad cell number");
+    return -1;
+  }
+  return iz/fNColPerChip;
+}
+
+//______________________________________________________________________
+Int_t AliITSsegmentationPixUpg::GetChipFromLocal(Float_t, Float_t zloc) const 
+{
+  // returns chip number (in range 0-4) starting from local coordinates
+  Int_t ix0,iz;
+  if (!LocalToDet(0,zloc,ix0,iz)) {
+    AliWarning("Bad local coordinate");
+    return -1;
+  } 
+  return GetChipFromChannel(ix0,iz);
+}
+
+//______________________________________________________________________
+Int_t AliITSsegmentationPixUpg::GetChipsInLocalWindow(Int_t* array, Float_t zmin, Float_t zmax, Float_t, Float_t) const 
+{
+  // returns the number of chips containing a road defined by given local coordinate limits
+  //
+  const Float_t kconv = 1./kCM2MC; // converts microns to cm.
+  //
+  if (zmin>zmax) {
+    AliWarning("Bad coordinate limits: zmin>zmax!");
+    return -1;
+  } 
+  //
+  Int_t nChipInW = 0;
+  //
+  Float_t zminDet = -0.5*kconv*Dz();
+  Float_t zmaxDet =  0.5*kconv*Dz();
+  if(zmin<zminDet) zmin=zminDet;
+  if(zmax>zmaxDet) zmax=zmaxDet;
+
+  Int_t n1 = GetChipFromLocal(0,zmin);
+  array[nChipInW] = n1;
+  nChipInW++;
+
+  Int_t n2 = GetChipFromLocal(0,zmax);
+
+  if(n2!=n1){
+    Int_t imin=TMath::Min(n1,n2);
+    Int_t imax=TMath::Max(n1,n2);
+    for(Int_t ichip=imin; ichip<=imax; ichip++){
+      if(ichip==n1) continue;
+      array[nChipInW]=ichip;
+      nChipInW++;
+    }
+  }
+  //
+  return nChipInW;
+}
---
+++ ITS/UPGRADE/AliITSsegmentationPixUpg.h
@@ -0,0 +1,81 @@
+#ifndef ALIITSSEGMENTATIONPIXUPG_H
+#define ALIITSSEGMENTATIONPIXUPG_H
+
+#include "AliITSsegmentation.h"
+
+
+
+// segmentation and response for pixels in ITS upgrade 
+
+class AliITSsegmentationPixUpg :
+public AliITSsegmentation {
+ public:
+  enum {kCM2MC=10000};
+  AliITSsegmentationPixUpg(int nchips=0,int ncol=0,int nrow=0,
+			   double pitchX=0,double pitchZ=0,
+			   double pitchLftC=-1,double pitchRgtC=-1,
+			   double thickness=0,
+			   double edgL=0,double edgR=0,double edgT=0,double edgB=0);
+  
+  AliITSsegmentationPixUpg(Option_t *opt="" );
+  AliITSsegmentationPixUpg(const AliITSsegmentationPixUpg &source);
+  virtual ~AliITSsegmentationPixUpg() {}
+  AliITSsegmentationPixUpg& operator=(const AliITSsegmentationPixUpg &source);
+  
+  virtual void    SetNPads(Int_t, Int_t) {MayNotUse("SetPadSize");}
+  virtual Int_t   GetNPads() const {return fNCol*fNRow;}
+  //
+  virtual void    GetPadIxz(Float_t x,Float_t z,Int_t &ix,Int_t &iz) const;
+  virtual void    GetPadCxz(Int_t ix,Int_t iz,Float_t &x,Float_t &z) const;
+  virtual void    GetPadTxz(Float_t &x ,Float_t &z) const;
+  virtual Bool_t  LocalToDet(Float_t x,Float_t z,Int_t &ix,Int_t &iz) const;
+  virtual void    DetToLocal(Int_t ix,Int_t iz,Float_t &x,Float_t &z) const;
+  virtual void    CellBoundries(Int_t ix,Int_t iz,Double_t &xl,Double_t &xu,
+				Double_t &zl,Double_t &zu) const;
+  //
+  virtual Int_t    GetNumberOfChips() const {return fNChips;}
+  virtual Int_t    GetMaximumChipIndex() const {return fNChips-1;}
+  //
+  virtual Int_t    GetChipFromLocal(Float_t, Float_t zloc) const;
+  virtual Int_t    GetChipsInLocalWindow(Int_t* array, Float_t zmin, Float_t zmax, Float_t, Float_t) const;
+  //
+  virtual Int_t    GetChipFromChannel(Int_t, Int_t iz) const;
+  //
+  virtual Float_t Dpx(Int_t ix=0) const;
+  virtual Float_t Dpz(Int_t iz)   const;
+  //
+  Int_t   GetNRow()               const {return fNRow;}
+  Int_t   GetNCol()               const {return fNCol;}
+  //
+  virtual Int_t                   Npx() const {return GetNRow();}
+  virtual Int_t                   Npz() const {return GetNCol();}
+  //
+  virtual void Neighbours(Int_t iX,Int_t iZ,Int_t* Nlist,Int_t Xlist[10],Int_t Zlist[10]) const;
+  //
+  virtual void PrintDefaultParameters() const {AliWarning("No def. parameters defined as const static data members");}
+  //
+ protected:
+  Float_t Z2Col(Float_t z) const;
+  Float_t Col2Z(Int_t col) const;
+  //
+ protected:
+    Double_t fGuardLft;        // left guard edge
+    Double_t fGuardRgt;        // right guard edge
+    Double_t fGuardTop;        // upper guard edge
+    Double_t fGuardBot;        // bottom guard edge
+    Double_t fPitchX;          // default pitch in X
+    Double_t fPitchZ;          // default pitch in Z
+    Double_t fPitchZLftCol;    // Z pitch of left column of each chip
+    Double_t fPitchZRgtCol;    // Z pitch of right column of each chip
+    Double_t fChipDZ;          // aux: chip size along Z
+    Int_t    fNChips;          // number of chips per module
+    Int_t    fNColPerChip;     // number of columns per chip
+    Int_t    fNRow;            // number of rows
+    Int_t    fNCol;            // number of columns (total)
+    //
+
+  ClassDef(AliITSsegmentationPixUpg,1) //Segmentation class upgrade pixels 
+
+};
+
+#endif
