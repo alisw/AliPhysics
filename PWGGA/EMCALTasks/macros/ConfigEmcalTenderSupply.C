b7b466bd4ec30672ecc6f01eb5c6f167a9416581
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-26 12:11:09 +0000

    cosmetics

--- PWGGA/EMCALTasks/macros/ConfigEmcalTenderSupply.C
+++ PWGGA/EMCALTasks/macros/ConfigEmcalTenderSupply.C
@@ -15,12 +15,12 @@ AliEMCALTenderSupply* ConfigEmcalTenderSupply(
 
   AliEMCALRecParam *params = new AliEMCALRecParam();
   params->SetClusteringThreshold(0.1); // 100 MeV
-  params->SetMinECut(0.05); //50 MeV  
+  params->SetMinECut(0.05);            //50 MeV  
   params->SetW0(4.5);
   if (timeCut) {
-    params->SetTimeCut(1e6);//Open this cut for AODs
-    params->SetTimeMin(-1);//Open this cut for AODs
-    params->SetTimeMax(1e6);//Open this cut for AODs
+    params->SetTimeCut(1e6); //Open this cut for AODs
+    params->SetTimeMin(-1);  //Open this cut for AODs
+    params->SetTimeMax(1e6); //Open this cut for AODs
   }
   EMCALSupply->SetRecParam(params);
 
@@ -63,10 +63,6 @@ AliEMCALTenderSupply* ConfigEmcalTenderSupply(
     EMCALSupply->SwitchOffCalibrateTime();
 
   EMCALSupply->SetMass(0.139);
-  //EMCALSupply->SetStep(5);
-  //EMCALSupply->SwitchOnCutEtaPhiSum(); 
-  //EMCALSupply->SetRCut(0.0025);
-
   EMCALSupply->SwitchOnCutEtaPhiSeparate();
   EMCALSupply->SetEtaCut(0.025);
   EMCALSupply->SetPhiCut(0.05);
