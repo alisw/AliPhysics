0fccbee3390b8e308e70c509e58162b90c96d7b6
miweber <miweber@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-07-30 14:21:40 +0000

    changes in .pkg for chaoticity task + remove couts

--- PWGCF/PWGCFChaoticityLinkDef.h
+++ PWGCF/PWGCFChaoticityLinkDef.h
@@ -4,3 +4,8 @@
 
 #pragma link C++ class AliChaoticity+;
 #pragma link C++ class AliChaoticityEventCollection+;
+#pragma link C++ class AliChaoticityEventStruct+;
+#pragma link C++ class AliChaoticityTrackStruct+;
+#pragma link C++ class AliChaoticityMCStruct+;
+#pragma link C++ class AliChaoticityPairStruct+;
+#pragma link C++ class AliChaoticityNormPairStruct+;
