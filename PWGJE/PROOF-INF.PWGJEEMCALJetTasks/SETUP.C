f5871fc177ab4b1965c7d6b72d1ed246b1310fd7
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-08-06 04:23:06 +0000

    par file

---
+++ PWGJE/PROOF-INF.PWGJEEMCALJetTasks/SETUP.C
@@ -0,0 +1,15 @@
+// $Id$
+
+void SETUP()
+{
+  // Load the library
+  TString ocwd = gSystem->WorkingDirectory();
+  gSystem->Load(ocwd+"/libPWGJEEMCALJetTasks.so");
+
+  // Set the Include paths
+  gSystem->SetIncludePath("-I$ROOTSYS/include -IPWGJEEMCALJetTasks");
+  gROOT->ProcessLine(".include PWGJEEMCALTasks/EMCALJetTasks");
+  
+  // Set our location, so that other packages can find us
+  gSystem->Setenv("PWGJEEMCALJetTasks_INCLUDE", "PWGJEEMCALTasks/EMCALJetTasks");
+}
