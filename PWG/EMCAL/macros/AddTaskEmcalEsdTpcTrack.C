85d04cca1561970d9f512e30c5916ef2e029764c
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-08-07 18:55:35 +0000

    2011 track cuts

--- PWG/EMCAL/macros/AddTaskEmcalEsdTpcTrack.C
+++ PWG/EMCAL/macros/AddTaskEmcalEsdTpcTrack.C
@@ -25,42 +25,20 @@ AliEmcalEsdTpcTrackTask* AddTaskEmcalEsdTpcTrack(
   // Init the task and do settings
   //-------------------------------------------------------
 
+  gROOT->LoadMacro("$ALICE_ROOT/PWGJE/macros/CreateTrackCutsPWGJE.C");
+
   /* hybrid track cuts*/
-  AliESDtrackCuts *cutsp = new AliESDtrackCuts;
-  // TPC
-  TFormula *f1NClustersTPCLinearPtDep = new TFormula("f1NClustersTPCLinearPtDep","70.+30./20.*x");
-  cutsp->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep,20.);
-  cutsp->SetMinNClustersTPC(70);
-  cutsp->SetMaxChi2PerClusterTPC(4);
-  cutsp->SetRequireTPCStandAlone(kTRUE); //cut on NClustersTPC and chi2TPC Iter1
-  cutsp->SetAcceptKinkDaughters(kFALSE);
-  cutsp->SetRequireTPCRefit(kTRUE);
-  cutsp->SetMaxFractionSharedTPCClusters(0.4);
-  // ITS
-  cutsp->SetRequireITSRefit(kTRUE);
-  //accept secondaries
-  cutsp->SetMaxDCAToVertexXY(2.4);
-  cutsp->SetMaxDCAToVertexZ(3.2);
-  cutsp->SetDCAToVertex2D(kTRUE);
-  //reject fakes
-  cutsp->SetMaxChi2PerClusterITS(36);
-  cutsp->SetMaxChi2TPCConstrainedGlobal(36);
-  cutsp->SetRequireSigmaToVertex(kFALSE);
-  cutsp->SetEtaRange(-0.9,0.9);
-  cutsp->SetPtRange(0.15, 1E+15);
-  //tag = "Global tracks jet analysis with ITSrefit and NclsIter1=PtDep, noSPD requirement, no upper pt cut, golden chi2";
-  hybsp = new AliESDtrackCuts(*cutsp);
-  cutsp->SetClusterRequirementITS(AliESDtrackCuts::kSPD, AliESDtrackCuts::kAny);
-  //tag += " + additonal: SPD any requirement";
-  hybsp->SetRequireITSRefit(kFALSE);
-  //tag += " + additional: ITSrefit=kFALSE";
+  AliESDtrackCuts *cutsp = CreateTrackCutsPWGJE(10001007);       //1000 adds SPD any requirement
+  AliESDtrackCuts *hybsp = CreateTrackCutsPWGJE(10041007);       //1004 removes ITSrefit requirement from standard set    
+  hybsp->SetClusterRequirementITS(AliESDtrackCuts::kSPD, AliESDtrackCuts::kOff);
 
   AliEmcalEsdTpcTrackTask *eTask = new AliEmcalEsdTpcTrackTask();
   eTask->SetTrackCuts(cutsp);
   eTask->SetHybridTrackCuts(hybsp);
+
   eTask->SetTracksName(name);
 
-  cout << " *** TPC track to SPD vertex task configured *** " << endl;
+  cout << " *** Hybrid track selector task configured *** " << endl;
 
   //-------------------------------------------------------
   // Final settings, pass to manager and set the containers
@@ -68,8 +46,8 @@ AliEmcalEsdTpcTrackTask* AddTaskEmcalEsdTpcTrack(
   mgr->AddTask(eTask);
   
   // Create containers for input/output
-  AliAnalysisDataContainer *cinput1  = mgr->GetCommonInputContainer()  ;
-  mgr->ConnectInput  (eTask, 0,  cinput1 );
+  AliAnalysisDataContainer *cinput1 = mgr->GetCommonInputContainer();
+  mgr->ConnectInput(eTask, 0, cinput1);
   
   return eTask;
 }
