376eb84285f9d20d99feb311a9a75c8e377eb696
bhippoly <bhippoly@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-08-10 08:30:34 +0000

    first time commited to AliRoot

---
+++ PWGLF/STRANGENESS/Correlations/macros/runProofLambdaOverK0sJets.C
@@ -0,0 +1,68 @@
+//Based on the file $ALICE_ROOT/PWGLF/STRANGENESS/Cascades/macros/runProofCascadePbPb()
+void runProofLambdaOverK0sJets(TString  proofCluster  = "xsanchez@skaf.saske.sk",
+			       TString  alirootVer    = "VO_ALICE@AliRoot::v5-03-33-AN",
+			       TString  rootVer       = "VO_ALICE@ROOT::v5-33-02b", 
+			       TString  path          = "/alice/data/LHC10h_000138624_p2_AOD049",
+			       TString  name          = "LambdaOverK0sRatio", 
+			       Double_t minCen        = 0.,
+			       Double_t maxCen        = 90.,
+			       Double_t ptMinTrig     = 8.,
+			       Double_t ptMaxTrig     = 20.,
+			       Double_t etaMaxTrig    = 0.75,
+			       Bool_t   sepInjec      = kTRUE,
+			       Bool_t   isMC          = kFALSE,
+			       Bool_t   usePID        = kTRUE){
+  
+  Printf("   \nThe parameters of the programm are : \n ");
+  Printf(" \t Analysis mode:\t %s\n \t Centrality:\t %.1lf - %.1lf\n \t Use MC Data?:\t %s\n \t Use PID?:\t %s\n",
+	 "Proof", minCen,maxCen,
+	 (isMC) ? "Yes" : "No",
+	 (usePID) ? "Yes" : "No");
+  
+  // _____________________________________________________ // 
+
+  gEnv->SetValue("XSec.GSI.DelegProxy", "2");
+    
+  TProof::Mgr(proofCluster.Data())->SetROOTVersion(rootVer.Data());
+
+  TString alirootMode = ""; 
+  TString extraLibs;
+  TList *list = new TList();
+  alirootMode="ALIROOT";
+  extraLibs+= "ANALYSIS:OADB:ANALYSISalice:CORRFW";  
+  list->Add(new TNamed("ALIROOT_MODE", alirootMode.Data()));
+  list->Add(new TNamed("ALIROOT_EXTRA_LIBS", extraLibs.Data()));
+   
+  TProof::Reset(proofCluster.Data());
+  TProof::Open(proofCluster.Data(),"workers=40"); 
+  gProof->ClearPackages();
+  gProof->EnablePackage(alirootVer.Data(),list);
+
+  // _____________________________________________________ //
+
+  gSystem->AddIncludePath("-I$ALICE_ROOT/include");
+  
+  AliAnalysisManager *mgr = new AliAnalysisManager("Manager");
+  
+  AliAODInputHandler* aodH = new AliAODInputHandler;
+  mgr->SetInputEventHandler(aodH);
+   
+  //PID
+  gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
+  AliAnalysisTask *pidTask = AddTaskPIDResponse(isMC);
+  //AliAnalysisTask *pidTask = AddTaskPIDResponse(isMC,kTRUE);
+  if(!pidTask) { printf("no PIDtask\n"); return; }
+
+  // My task
+  gProof->Load("AliAnalysisTaskLambdaOverK0sJets.cxx+g");
+  gROOT->LoadMacro("AddTaskLambdaOverK0sJets.C");
+  AliAnalysisTaskLambdaOverK0sJets *task = AddTaskLambdaOverK0sJets(name,minCen,maxCen,ptMinTrig,ptMaxTrig,etaMaxTrig,sepInjec,isMC,usePID);
+  
+  // _____________________________________________________ //
+  
+  if (!mgr->InitAnalysis()) return;
+  mgr->PrintStatus();
+  mgr->StartAnalysis("proof",path); 
+  
+}
+
