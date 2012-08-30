0f2be320b6194cd225a08ca6f96c806e4e2b0874
dlohner <dlohner@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-08-30 10:19:19 +0000

    addtask aod production

---
+++ PWGGA/GammaConv/macros/AddTask_ConversionAODProduction.C
@@ -0,0 +1,52 @@
+AliAnalysisTask *AddTask_ConversionAODProduction(Int_t dataset=1){
+
+    // dataset 0: pp
+    // dataset 1: PbPb
+
+    Bool_t IsHeavyIon=(dataset==1);
+
+    //get the current analysis manager
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    if (!mgr) {
+	Error("AddTask_V0ReaderV1", "No analysis manager found.");
+	return 0;
+    }
+
+  
+    TString analysiscut;
+
+    if(IsHeavyIon){
+	analysiscut="900177009350113211200001000000000";
+    }
+    else{
+	analysiscut="900397209450304221200000002000000";
+    }
+
+    //========= Add V0 Reader to  ANALYSIS manager =====
+
+    AliV0ReaderV1 *fV0Reader=new AliV0ReaderV1("ConvGammaAODProduction");
+    fV0Reader->SetCreateAODs(kTRUE);
+    fV0Reader->SetUseOwnXYZCalculation(kTRUE);
+    // Set AnalysisCut Number
+    AliConversionCuts *fCuts= new AliConversionCuts(analysiscut.Data(),analysiscut.Data());
+    if(fCuts->InitializeCutsFromCutString(analysiscut.Data())){
+	fV0Reader->SetConversionCuts(fCuts);
+    }
+    fV0Reader->Init();
+    mgr->AddTask(fV0Reader);
+
+    AliLog::SetGlobalLogLevel(AliLog::kInfo);
+
+    //================================================
+    //              data containers
+    //================================================
+    //            find input container
+    //below the trunk version
+    AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
+
+    // connect input V0Reader
+    fV0Reader->SelectCollisionCandidates(AliVEvent::kAny);
+    mgr->ConnectInput (fV0Reader,0,cinput);
+
+    return fV0Reader;
+}
