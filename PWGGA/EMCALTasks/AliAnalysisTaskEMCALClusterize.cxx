184e4abcfa8f75b6ea07d7208c1ae919ed0238d9
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-09-05 15:13:04 +0000

    change location of geometry.root file

--- PWGGA/EMCALTasks/AliAnalysisTaskEMCALClusterize.cxx
+++ PWGGA/EMCALTasks/AliAnalysisTaskEMCALClusterize.cxx
@@ -1,4 +1,4 @@
-/**************************************************************************
+ /**************************************************************************
  * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  *                                                                        *
  * Author: The ALICE Off-line Project.                                    *
@@ -1010,7 +1010,7 @@ void AliAnalysisTaskEMCALClusterize::Init()
   // Init geometry, I do not like much to do it like this ...
   if(fImportGeometryFromFile && !gGeoManager) 
   {
-    if (fImportGeometryFilePath == "") fImportGeometryFilePath = "$ALICE_ROOT/PWGGA/EMCALTasks/macros/geometry.root" ; // "$ALICE_ROOT/EVE/alice-data/default_geo.root"
+    if (fImportGeometryFilePath == "") fImportGeometryFilePath = "$ALICE_ROOT/PWGGA/OADB/EMCAL/geometry_2011.root" ; // "$ALICE_ROOT/EVE/alice-data/default_geo.root"
     printf("AliAnalysisTaskEMCALClusterize::Init() - Import %s\n",fImportGeometryFilePath.Data());
     TGeoManager::Import(fImportGeometryFilePath) ; 
   }
