86ca7069dfec7eb215407cf0ea913e45f3696795
gconesab <gconesab@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-10-08 14:03:37 +0000

    correct compilation warning

--- PWGGA/CaloTrackCorrelations/AliAnaPi0EbE.cxx
+++ PWGGA/CaloTrackCorrelations/AliAnaPi0EbE.cxx
@@ -198,7 +198,7 @@ void AliAnaPi0EbE::FillPileUpHistograms(Float_t energy, Float_t time)
   //       GetReader()->IsPileUpFromSPD(),event->IsPileupFromSPDInMultBins(),nVerticesSPD,nVerticesTracks);
   
   Int_t ncont = -1;
-  Int_t z1 = -1, z2 = -1;
+  Float_t z1 = -1, z2 = -1;
   Float_t diamZ = -1;
   for(Int_t iVert=0; iVert<nVerticesSPD;iVert++)
   {
