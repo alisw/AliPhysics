fb29e17dffc2fab1ee672372ab399327f166342d
rbailhac <rbailhac@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-10-08 10:43:08 +0000

    Next lego train

--- PWGHF/hfe/macros/AddTaskHFEtpctofPbPb.C
+++ PWGHF/hfe/macros/AddTaskHFEtpctofPbPb.C
@@ -1,4 +1,4 @@
-AliAnalysisTask *AddTaskHFEtpctofPbPb(Bool_t isMC,Int_t aodfilter=-1, Int_t tpcCls=110,  Int_t tpcClsPID = 70, Double_t tpcClsRatio = 0.6, Double_t tpcClShared = 0.1, Int_t itsCls=4,Double_t itsChi2PerClusters=36.,Int_t itspixelcut=AliHFEextraCuts::kAny, Double_t dcaxy=1.0, Double_t dcaz=2.0, Double_t tofs=3., Double_t ipSig=3.0, Float_t prodlow=0., Float_t prodhigh=100., Bool_t beauty=kTRUE,Bool_t kMCQA = kFALSE, Bool_t kDEStep = kFALSE, Int_t addflag=0,Int_t etacor=0,TString listname=""){
+AliAnalysisTask *AddTaskHFEtpctofPbPb(Bool_t isaod, Bool_t isMC,Int_t aodfilter=-1, Int_t tpcCls=120,  Int_t tpcClsPID = 80, Double_t tpcClsRatio = 0.6, Double_t tpcClShared = 1.1, Int_t itsCls=4,Double_t itsChi2PerClusters=36.,Int_t itspixelcut=AliHFEextraCuts::kBoth, Double_t dcaxy=1.0, Double_t dcaz=2.0, Bool_t usetof=kFALSE, Double_t tofs=3.,Bool_t etacor=kFALSE,TString listname="",Double_t tpceff=0.5, Float_t prodlow=0., Float_t prodhigh=100.,Bool_t kMCQA = kFALSE){
 
   // libraries in case
   gSystem->Load("libANALYSIS.so");
@@ -7,54 +7,62 @@ AliAnalysisTask *AddTaskHFEtpctofPbPb(Bool_t isMC,Int_t aodfilter=-1, Int_t tpcC
 
   //set config file name
   TString configFile("$ALICE_ROOT/PWGHF/hfe/macros/configs/PbPb/ConfigHFEpbpb.C");
-  //TString configFile("/d/alice12/bailhache/AliRootInstallations/11_09_2012/AliRoot/PWGHF/hfe/macros/configs/PbPb/ConfigHFEpbpb.C");
+  //TString configFile("/d/alice12/bailhache/AliRootInstallations/07_10_2012/AliRoot/PWGHF/hfe/macros/configs/PbPb/ConfigHFEpbpb.C");
   TString checkconfig="ConfigHFEpbpb";
   if (!gROOT->GetListOfGlobalFunctions()->FindObject(checkconfig.Data()))
     gROOT->LoadMacro(configFile.Data());
 
   
-  // Name of the directory
-  Int_t itpcClsRatio = (Int_t) tpcClsRatio*10;
-  Int_t itpcClShared = (Int_t) tpcClShared*10;
-  Int_t iitsChi2PerClusters = (Int_t) itsChi2PerClusters*10;
-  Int_t idcaxy = (Int_t) dcaxy*10;
-  Int_t idcaz = (Int_t) dcaz*10;
-  Int_t itofs = (Int_t) tofs*10;
-  Int_t iipSig = (Int_t) ipSig;
-  Int_t iprodlow = (Int_t) prodlow;
-  Int_t iprodhigh = (Int_t) prodhigh*10;
-  Int_t ibeauty = 0;
+  // Name 
+  Int_t itpcClsRatio = (Int_t) (tpcClsRatio*10);
+  Int_t itpcClShared = (Int_t) (tpcClShared*10);
+  Int_t iitsChi2PerClusters = (Int_t) (itsChi2PerClusters*10);
+  Int_t idcaxy = (Int_t) (dcaxy*10);
+  Int_t idcaz = (Int_t) (dcaz*10);
+  Int_t itofs = (Int_t) (tofs*10);
+  Int_t iprodlow = (Int_t) (prodlow*10);
+  Int_t iprodhigh = (Int_t) (prodhigh*10);
+  Int_t itof = 0;
   Int_t iMCQA = 0;
-  Int_t iDEStep = 0;
-  if(beauty) ibeauty = 1;
+  Int_t ietacor = 0;
+  Int_t itpceff = (Int_t) (tpceff*10);
+  if(usetof) itof=kTRUE;
   if(kMCQA) iMCQA = 1;
-  if(kDEStep) iDEStep = 1;
-
-
-  TString appendix(TString::Format("a%dT%dTP%dTR%dTS%dI%dIC%dIp%dDCAx%dz%dTOF%dIP%dprodL%dH%dB%dM%dD%da%de%d",aodfilter,tpcCls,tpcClsPID,itpcClsRatio,itpcClShared,itsCls,iitsChi2PerClusters,itspixelcut,idcaxy,idcaz,itofs,iipSig,iprodlow,iprodhigh,ibeauty,iMCQA,iDEStep,addflag,etacor));
+  if(etacor) ietacor = 1;
+  
+  TString appendix(TString::Format("f%dt%dtp%dtr%dts%di%dic%di%ddcaxy%dz%dtof%dts%de%dtpc%dprodlow%dhigh%dmc%d",aodfilter,tpcCls,tpcClsPID,itpcClsRatio,itpcClShared,itsCls,iitsChi2PerClusters,itspixelcut,idcaxy,idcaz,itof,itofs,ietacor,itpceff,iprodlow,iprodhigh,iMCQA));
   printf("appendix %s\n", appendix.Data());
   
-  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
 
-  // Check the analysis type using the event handlers connected to the analysis manager.
-  //==============================================================================
+  // ESD or AOD
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr->GetInputEventHandler()) {
     printf("AddTaskEventplane", "This task requires an input event handler");
     return NULL;
   }
   TString inputDataType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+  Bool_t isaod = kFALSE;
+  if (inputDataType == "AOD") isaod = kTRUE;
 
+  
+  // TPC cut
+  // 0-5,5-10,10-20,20-30,30-40,40-50,50-60,60-70,70-80,80-90,90-100,one more per history
+  // http://www.physi.uni-heidelberg.de/~pachmay/projects/hfe/pid/tpc/
+  // without eta correction 50%
+  Double_t tpcdEdxcutlow[12] = {0.03,0.15,0.24,0.3,0.39,0.48,0.5,0.51,0.5,0.5,0.5,0.5};
+  // with eta correction 50%
+  // Double_t tpcdEdxcutlow[12] = {-0.01,0.076,0.197,0.26,0.298,0.3,0.3,0.3,0.3,0.3,0.3,0.3};
+  Double_t tpcdEdxcuthigh[12] = {3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0};
+
+
+  // Task
   AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
-  AliAnalysisTaskHFE *task = ConfigHFEpbpb(isMC,appendix,aodfilter,tpcCls,tpcClsPID,tpcClsRatio,tpcClShared,itsCls,itsChi2PerClusters,itspixelcut,dcaxy,dcaz,tofs,ipSig,prodlow,prodhigh,beauty,kMCQA,kDEStep,addflag,0,etacor,listname);  
+  AliAnalysisTaskHFE *task = ConfigHFEpbpb(isaod,isMC,appendix,aodfilter,tpcCls,tpcClsPID,tpcClsRatio,tpcClShared,itsCls,itsChi2PerClusters,itspixelcut,dcaxy,dcaz,usetof,tofs,etacor,listname,tpcdEdxcutlow,tpcdEdxcuthigh,prodlow,prodhigh,kMCQA);  
 
-  if (inputDataType == "AOD"){
-    task->SetFillNoCuts(kTRUE);
-    task->SetApplyCutAOD(kTRUE);
-  }
-
-   
   mgr->AddTask(task);
 
+
+  // Write Output 
   TString containerName = mgr->GetCommonFileName();
   containerName += ":";
   containerName += appendix.Data();
