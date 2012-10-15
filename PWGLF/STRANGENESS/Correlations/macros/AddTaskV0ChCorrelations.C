dbf892569c26af58cc5bb071b7974f9cf2a8944f
mbombara <mbombara@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-10-15 13:48:23 +0000

    AddTask macro required for LEGO train

---
+++ PWGLF/STRANGENESS/Correlations/macros/AddTaskV0ChCorrelations.C
@@ -0,0 +1,47 @@
+// runV0ChCorrelations.C
+//
+// AddTask for AliAnalysisTaskV0ChCorrelations task
+//
+AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const Bool_t bMCtruth=kFALSE)
+{
+  // Creates a V0Ch correlations analysis task and adds it to the analysis manager.
+
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    if (!mgr) {
+      ::Error("AddTaskV0ChCorrelations", "No analysis manager to connect to.");
+      return NULL;
+    }
+
+    // mc event handlerrunEx01.C
+    if(bMCtruth) {
+        AliMCEventHandler* mchandler = new AliMCEventHandler();
+        // Not reading track references
+        mchandler->SetReadTR(kFALSE);
+        mgr->SetMCtruthEventHandler(mchandler);
+    }   
+
+	// create task
+    AliAnalysisTaskV0ChCorrelations* task = new AliAnalysisTaskV0ChCorrelations("V0ChCorrelations_task");
+    mgr->AddTask(task);
+    
+    // Create ONLY the output containers for the data produced by the task.
+    // Get and connect other common input/output containers via the manager as below
+    //==============================================================================
+    TString outputFileName = AliAnalysisManager::GetCommonFileName();
+    outputFileName = "list.grid.v0ch.root";
+
+    // create containers for input/output
+    AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("coutput1", TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName);
+        
+    // connect input/output
+    mgr->ConnectInput(task, 0, cinput);
+    mgr->ConnectOutput(task, 1, coutput1);
+        
+    // Return task pointer at the end
+    return task;
+}
+
