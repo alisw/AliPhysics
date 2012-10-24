f268c26a11decfbeec92524d97f94a2ccdc0f1ec
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-10-24 23:06:50 +0000

    Added script to extract MC corrections from forward_mccorr.root

---
+++ PWGLF/FORWARD/analysis2/corrs/ExtractMCCorr.C
@@ -0,0 +1,208 @@
+/**
+ * Script to draw the energy loss fits 
+ * 
+ * @ingroup pwglf_forward_scripts_corr
+ */
+#ifndef __CINT__
+#include <TFile.h>
+#include <TList.h>
+#include <TError.h>
+#include "AliFMDCorrSecondaryMap.h"
+#include "AliCentralCorrSecondaryMap.h"
+#include "AliForwardCorrectionManager.h"
+#endif 
+
+//____________________________________________________________________
+/** 
+ * Extract the energy loss correction object from file and rename it 
+ * according to the settings 
+ * 
+ * @param fname  File to extract from 
+ * @param sys    Collision system (pp, PbPb)
+ * @param sNN    Center of mass energy (in GeV) per nucleon
+ * @param field  L3 magnetic field (-5,0,5) in kGaus
+ * 
+ * @ingroup pwglf_forward_scripts_corr
+ */
+void
+ExtractMCCorr(const char* fname = "forward_mccorr.root",
+	      UShort_t    sys   = 0, 
+	      UShort_t    sNN   = 900, 
+	      Short_t     field = 5)
+{
+#ifdef __CINT__
+  gROOT->Macro("$ALICE_ROOT/PWGLF/FORWARD/analysis2/scripts/LoadLibs.C");
+#endif
+
+  TFile* file = TFile::Open(fname, "READ");
+  if (!file) {
+    Error("ExtractMCCorr", "Couldn't open %s", fname);
+    return;
+  }
+  if (sys <= 0 || sys > 3 || sNN <= 0) { 
+    TList* forward = static_cast<TList*>(file->Get("ForwardSums"));
+    if (!forward) { 
+      Error("ExtractMCCorr", "Couldn't get forward list from %s", fname);
+      return;
+    }
+    TList* insp = static_cast<TList*>(forward->FindObject("fmdEventInspector"));
+    if (!insp) { 
+      Error("ExtractMCCorr", "Couldn't get event inspector list from %s",fname);
+      return;
+    }
+    TObject* oSys = insp->FindObject("sys");
+    TObject* oSNN = insp->FindObject("sNN");
+    TObject* oFld = insp->FindObject("field");
+    if (oSys) sys   = oSys->GetUniqueID();
+    if (oSNN) sNN   = oSNN->GetUniqueID();
+    if (oFld) field = oFld->GetUniqueID();
+  }
+  if (sys <= 0 || sys > 3) {
+    Error("ExtractMCCorr", "Couldn't get system type (%d), "
+	  "collision energy (%d), "
+	  "and/or field strength (%d)", sys, sNN, field);
+    return;
+  }
+
+  ExtractFMDSecMap(file, sys, sNN, field);
+  ExtractSPDSecMap(file, sys, sNN, field);
+  ExtractSPDAcceptance(file, sys, sNN, field);
+}
+
+//____________________________________________________________________
+/** 
+ * Extract and copy FMD secondary map to file 
+ * 
+ * @param file  Input file
+ * @param sys   Collision system (1:pp, 2:PbPb)
+ * @param sNN   Center of mass energy (GeV) per nucleon
+ * @param field L3 magnetic field
+ *
+ * @ingroup pwglf_forward_scripts_corr
+ */
+void     
+ExtractFMDSecMap(TFile* file, UShort_t sys, UShort_t sNN, Short_t field)
+{
+  TList* forward = static_cast<TList*>(file->Get("ForwardResults"));
+  if (!forward) { 
+    Error("ExtractMCCorr", "Couldn't get forward list from %s", fname);
+    return;
+  }
+  
+  TString n(AliFMDCorrSecondaryMap::Class()->GetName());
+  TObject* fmdCorr = forward->FindObject(n);
+  if (!fmdCorr) { 
+    Error("ExtractMCCorr", "Couldn't get forward correction object %s", 
+	  n.Data());
+    return;
+  }
+
+  AliForwardCorrectionManager& mgr = AliForwardCorrectionManager::Instance();
+  mgr.WriteFile(AliForwardCorrectionManager::kSecondaryMap, 
+		sys, sNN, field, false, fmdCorr, false);
+}
+
+//____________________________________________________________________
+/** 
+ * Extract and copy SPD secondary map to file 
+ * 
+ * @param file  Input file
+ * @param sys   Collision system (1:pp, 2:PbPb)
+ * @param sNN   Center of mass energy (GeV) per nucleon
+ * @param field L3 magnetic field
+ *
+ * @ingroup pwglf_forward_scripts_corr
+ */
+void     
+ExtractSPDSecMap(TFile* file, UShort_t sys, UShort_t sNN, Short_t field)
+{
+  TList* central = static_cast<TList*>(file->Get("CentralResults"));
+  // static_cast<TList*>(file->Get("PWGLFcentralDnDeta/Central"));
+  if (!central) { 
+    Error("ExtractMCCorr", "Couldn't get central list from %s", fname);
+    return;
+  }
+  
+  TString n(AliCentralCorrSecondaryMap::Class()->GetName());
+  TObject* spdCorr  = central->FindObject(n);
+  if (!spdCorr) { 
+    Error("ExtractMCCorr", "Couldn't get central correction object %s", 
+	  n.Data());
+    return;
+  }
+
+
+  AliCentralMultiplicityTask::Manager* mgr = new 
+    AliCentralMultiplicityTask::Manager;
+  // mgr->Dump();
+  // mgr->Print();
+  mgr->WriteFile(0, sys, sNN, field, spdCorr, false);
+}
+//____________________________________________________________________
+/** 
+ * Extract and copy SPD secondary map to file 
+ * 
+ * @param file  Input file
+ * @param sys   Collision system (1:pp, 2:PbPb)
+ * @param sNN   Center of mass energy (GeV) per nucleon
+ * @param field L3 magnetic field
+ *
+ * @ingroup pwglf_forward_scripts_corr
+ */
+void     
+ExtractSPDAcceptance(TFile* file, UShort_t sys, UShort_t sNN, Short_t field)
+{
+  TList* central = static_cast<TList*>(file->Get("CentralResults"));
+  if (!central) { 
+    Error("ExtractMCCorr", "Couldn't get central list from %s", fname);
+    return;
+  }
+  
+  TString n(AliCentralCorrAcceptance::Class()->GetName());
+  TObject* spdCorr  = central->FindObject(n);
+  if (!spdCorr) { 
+    Error("ExtractMCCorr", "Couldn't get central correction object %s", 
+	  n.Data());
+    return;
+  }
+
+
+  AliCentralMultiplicityTask::Manager* mgr = new 
+    AliCentralMultiplicityTask::Manager;
+  // mgr->Dump();
+  // mgr->Print();
+  mgr->WriteFile(1, sys, sNN, field, spdCorr, false);
+}
+    
+  
+//____________________________________________________________________
+/** 
+ * Extract the energy loss correction object from file and rename it 
+ * according to the settings 
+ * 
+ * @param fname  File to extract from 
+ * @param sys    Collision system (pp, PbPb)
+ * @param sNN    Center of mass energy (in GeV) per nucleon
+ * @param field  L3 magnetic field (-5,0,5) in kGaus
+ * 
+ * @ingroup pwglf_forward_scripts_corr
+ */
+void
+ExtractMCCorr(const char* fname, // "forward_mccorr.root"
+	      const char* sys="p-p", 
+	      Float_t     sNN=900, 
+	      Float_t     field=5)
+{
+  gROOT->Macro("$ALICE_ROOT/PWGLF/FORWARD/analysis2/scripts/LoadLibs.C");
+
+  UShort_t uSys   = AliForwardUtil::ParseCollisionSystem(sys);
+  UShort_t usNN   = AliForwardUtil::ParseCenterOfMassEnergy(uSys,sNN);
+  Short_t  sField = AliForwardUtil::ParseMagneticField(field);
+
+  ExtractMCCorr(fname, uSys, usNN, sField);
+}
+
+//____________________________________________________________________
+//
+// EOF
+//
