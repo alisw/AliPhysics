f82db6e55a837fd44c4291c87c8a2faac74382f5
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-10-31 22:32:01 +0000

    added jet embedding from generator tasks

---
+++ PWGJE/EMCALJetTasks/macros/AddTaskJetEmbeddingFromGen.C
@@ -0,0 +1,55 @@
+// $Id: AddTaskJetEmbedding.C 58092 2012-08-05 21:18:57Z loizides $
+
+AliJetEmbeddingFromGenTask* AddTaskJetEmbeddingFromGen(
+  AliGenerator   *genGen,
+  const char     *tracksName   = "GenParticles",
+  const char     *taskName     = "JetEmbeddingFromGenTask",
+  const Double_t  minPt        = 10,
+  const Double_t  maxPt        = 10,
+  const Double_t  minEta       = -0.9,
+  const Double_t  maxEta       = 0.9,
+  const Double_t  minPhi       = 0,
+  const Double_t  maxPhi       = TMath::Pi() * 2,
+  const Bool_t    copyArray    = kFALSE
+)
+{  
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr)
+  {
+    ::Error("AddTaskJetEmbedding", "No analysis manager to connect to.");
+    return NULL;
+  }  
+  
+  // Check the analysis type using the event handlers connected to the analysis manager.
+  //==============================================================================
+  if (!mgr->GetInputEventHandler())
+  {
+    ::Error("AddTaskJetEmbedding", "This task requires an input event handler");
+    return NULL;
+  }
+  
+  //-------------------------------------------------------
+  // Init the task and do settings
+  //-------------------------------------------------------
+
+  AliJetEmbeddingFromGenTask *jetEmb = new AliJetEmbeddingFromGenTask(taskName);
+  jetEmb->SetGen(genGen);
+  jetEmb->SetTracksName(tracksName);
+  jetEmb->SetEtaRange(minEta, maxEta);
+  jetEmb->SetPhiRange(minPhi, maxPhi);
+  jetEmb->SetPtRange(minPt, maxPt);
+  jetEmb->SetCopyArray(copyArray);
+
+  //-------------------------------------------------------
+  // Final settings, pass to manager and set the containers
+  //-------------------------------------------------------
+
+  mgr->AddTask(jetEmb);
+    
+  // Create containers for input/output
+  mgr->ConnectInput (jetEmb, 0, mgr->GetCommonInputContainer() );
+
+  return jetEmb;
+}
