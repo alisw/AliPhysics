e91a96315a63ce51da8794443ba64f2fcc23ce03
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-11-01 14:16:08 +0000

    binning

--- PWGCF/Correlations/Base/AliUEHist.cxx
+++ PWGCF/Correlations/Base/AliUEHist.cxx
@@ -94,14 +94,14 @@ AliUEHist::AliUEHist(const char* reqHist) :
   trackAxisTitle[0] = etaTitle;
   
   // delta eta
-  const Int_t kNDeltaEtaBins = 50;
-  Double_t deltaEtaBins[kNDeltaEtaBins+1] = { -2.5, -2.4, -2.3, -2.2, -2.1, 
+  const Int_t kNDeltaEtaBins = 48;
+  Double_t deltaEtaBins[kNDeltaEtaBins+1] = { -2.4, -2.3, -2.2, -2.1, 
 					      -2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 
 					      -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 
 					      0, 
 					      0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 
 					      1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
-					      2.1, 2.2, 2.3, 2.4, 2.5 };
+					      2.1, 2.2, 2.3, 2.4 };
   
   const Int_t kNDeltaEtaBinsTTR = 40+4;
   Double_t deltaEtaBinsTTR[kNDeltaEtaBinsTTR+1] = { -2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 
