83f164f8d19610a3a58f40e8556674f4788c45b2
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-11-11 07:28:46 +0000

    small mods, minjetpt and AliEmcalJetTask return

--- PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C
@@ -31,11 +31,17 @@ AliEmcalJetTask* AddTaskEmcalJet(
   // Init the task and do settings
   //-------------------------------------------------------
 
+  Double_t minJetPt = 0;
+
   char *algoString;
-  if (algo == 0)
+  if (algo == 0) {
     algoString = "KT";
-  else if (algo == 1)
+    minJetPt = 0.1;
+  } else if (algo == 1) {
     algoString = "AKT";
+    minJetPt = 1.0;
+  }
+
   char *typeString;
   if (type == 0)
     typeString = "Full";
@@ -43,13 +49,16 @@ AliEmcalJetTask* AddTaskEmcalJet(
     typeString = "Charged";
   else if (type == 2)
     typeString = "Neutral";
+
   char radiusString[200];
   sprintf(radiusString,"R0%2.0f",radius*100.0);
+
   char pTString[200];
   if (minTrPt<1.0)
     sprintf(pTString,"pT0%3.0f",minTrPt*1000.0);
   else if (minTrPt>=1.0)
     sprintf(pTString,"pT%4.0f",minTrPt*1000.0);
+
   char ETString[200];
   if (minClPt<1.0)
     sprintf(ETString,"ET0%3.0f",minClPt*1000.0);
@@ -58,13 +67,16 @@ AliEmcalJetTask* AddTaskEmcalJet(
 
   TString name;
   if (type == 0)
-     name = TString(Form("Jet_%s%sR%s_%s_%s_%s_%s",algoString,typeString,radiusString,nTracks,pTString,nClusters,ETString));
-  if (type == 1)
-     name = TString(Form("Jet_%s%sR%s_%s_%s",algoString,typeString,radiusString,nTracks,pTString));
-  if (type == 2)
-     name = TString(Form("Jet_%s%sR%s_%s_%s",algoString,typeString,radiusString,nClusters,ETString));
+    name = TString(Form("Jet_%s%sR%s_%s_%s_%s_%s",
+                        algoString,typeString,radiusString,nTracks,pTString,nClusters,ETString));
+  else if (type == 1)
+    name = TString(Form("Jet_%s%sR%s_%s_%s",
+                        algoString,typeString,radiusString,nTracks,pTString));
+  else if (type == 2)
+    name = TString(Form("Jet_%s%sR%s_%s_%s",
+                        algoString,typeString,radiusString,nClusters,ETString));
  
-  AliAnalysisTask* mgrTask = mgr->GetTask(name.Data());
+  AliEmcalJetTask* mgrTask = mgr->GetTask(name.Data());
   if (mgrTask)
     return mgrTask;  
 
@@ -75,6 +87,7 @@ AliEmcalJetTask* AddTaskEmcalJet(
   jetTask->SetAlgo(algo);
   jetTask->SetMinJetTrackPt(minTrPt);
   jetTask->SetMinJetClusPt(minClPt);
+  jetTask->SetMinJetPt(minJetPt);
   jetTask->SetRadius(radius);
   jetTask->SetType(type);
 
