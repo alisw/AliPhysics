495c487ac918f481430be6700cbd4a281b23a6e9
mivanov <mivanov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-11-13 13:35:01 +0000

    Exmpale part of the code - Adding comments + statis + error of parameters.

--- PWGPP/TPC/AliTPCPerformanceSummary.cxx
+++ PWGPP/TPC/AliTPCPerformanceSummary.cxx
@@ -690,25 +690,25 @@ Int_t AliTPCPerformanceSummary::AnalyzeDCARPhi(const AliPerformanceTPC* pTPC, TT
     // dump values
     //
     (*pcstream)<<"tpcQA"<<
-        "offsetdRA="<< offsetdRA<<
-        "slopedRA="<< slopedRA<<
-        "offsetdRC="<< offsetdRC<<
-        "slopedRC="<<slopedRC<<
-        //
-        "offsetdRAErr="<< offsetdRAErr<<
-        "slopedRAErr="<< slopedRAErr<<
-        "offsetdRCErr="<< offsetdRCErr<<
-        "slopedRCErr="<<slopedRCErr<<
-        //
-        "offsetdRAchi2="<< offsetdRAchi2<<
-        "slopedRAchi2="<< slopedRAchi2<<
-        "offsetdRCchi2="<< offsetdRCchi2<<
-        "slopedRCchi2="<<slopedRCchi2<<
-        //
-        "dcarAP0="<<dcarAP0<<
-        "dcarAP1="<<dcarAP1<<
-        "dcarCP0="<<dcarCP0<<
-        "dcarCP1="<<dcarCP1;
+      "offsetdRA="<< offsetdRA<<                       // mean r-phi distortion fit A side (DCA_rphi=[0]+[1]*tgl(theta)) - parameter [0] offset
+      "slopedRA="<< slopedRA<<                         // mean r-phi distortion fit A side (DCA_rphi=[0]+[1]*tgl(theta)) - parameter [1] slope
+      "offsetdRC="<< offsetdRC<<                       // 
+      "slopedRC="<<slopedRC<<
+      //
+      "offsetdRAErr="<< offsetdRAErr<<
+      "slopedRAErr="<< slopedRAErr<<
+      "offsetdRCErr="<< offsetdRCErr<<
+      "slopedRCErr="<<slopedRCErr<<
+      //
+      "offsetdRAchi2="<< offsetdRAchi2<<
+      "slopedRAchi2="<< slopedRAchi2<<
+      "offsetdRCchi2="<< offsetdRCchi2<<
+      "slopedRCchi2="<<slopedRCchi2<<
+      //
+      "dcarAP0="<<dcarAP0<<
+      "dcarAP1="<<dcarAP1<<
+      "dcarCP0="<<dcarCP0<<
+      "dcarCP1="<<dcarCP1;
         
     return 0;
 }
@@ -786,20 +786,20 @@ Int_t AliTPCPerformanceSummary::AnalyzeDCARPhiPos(const AliPerformanceTPC* pTPC,
     // dump values
     //
     (*pcstream)<<"tpcQA"<<
-        "offsetdRAPos="<< offsetdRAPos<<
-        "slopedRAPos="<< slopedRAPos<<
-        "offsetdRCPos="<< offsetdRCPos<<
-        "slopedRCPos="<<slopedRCPos<<
-        //
-        "offsetdRAErrPos="<< offsetdRAErrPos<<
-        "slopedRAErrPos="<< slopedRAErrPos<<
-        "offsetdRCErrPos="<< offsetdRCErrPos<<
-        "slopedRCErrPos="<<slopedRCErrPos<<
-        //
-        "offsetdRAchi2Pos="<< offsetdRAchi2Pos<<
-        "slopedRAchi2Pos="<< slopedRAchi2Pos<<
-        "offsetdRCchi2Pos="<< offsetdRCchi2Pos<<
-        "slopedRCchi2Pos="<<slopedRCchi2Pos;
+      "offsetdRAPos="<< offsetdRAPos<<
+      "slopedRAPos="<< slopedRAPos<<
+      "offsetdRCPos="<< offsetdRCPos<<
+      "slopedRCPos="<<slopedRCPos<<
+      //
+      "offsetdRAErrPos="<< offsetdRAErrPos<<
+      "slopedRAErrPos="<< slopedRAErrPos<<
+      "offsetdRCErrPos="<< offsetdRCErrPos<<
+      "slopedRCErrPos="<<slopedRCErrPos<<
+      //
+      "offsetdRAchi2Pos="<< offsetdRAchi2Pos<<
+      "slopedRAchi2Pos="<< slopedRAchi2Pos<<
+      "offsetdRCchi2Pos="<< offsetdRCchi2Pos<<
+      "slopedRCchi2Pos="<<slopedRCchi2Pos;
         
     return 0;
 }
@@ -906,6 +906,8 @@ Int_t AliTPCPerformanceSummary::AnalyzeNCL(const AliPerformanceTPC* pTPC, TTreeS
  
     // variables:
     static Double_t meanTPCnclF=0;
+    static Double_t meanTPCnclFStat=0;
+    static Double_t meanTPCnclFErr=0;
     static Double_t rmsTPCnclF=0;
     static Double_t meanTPCChi2=0;
     static Double_t rmsTPCChi2=0;  
@@ -914,6 +916,8 @@ Int_t AliTPCPerformanceSummary::AnalyzeNCL(const AliPerformanceTPC* pTPC, TTreeS
     static Double_t slopeATPCnclFErr=0;
     static Double_t slopeCTPCnclFErr=0;
     static Double_t meanTPCncl=0;
+    static Double_t meanTPCnclStat=0;
+    static Double_t meanTPCnclErr=0;
     static Double_t rmsTPCncl=0;
     static Double_t slopeATPCncl=0;
     static Double_t slopeCTPCncl=0;
@@ -962,7 +966,10 @@ Int_t AliTPCPerformanceSummary::AnalyzeNCL(const AliPerformanceTPC* pTPC, TTreeS
     }
  
     meanTPCncl= his1D->GetMean();
+    meanTPCnclStat= his1D->GetEntries();
+    meanTPCnclErr= his1D->GetMeanError();
     rmsTPCncl= his1D->GetRMS();
+    
     delete his1D;
     
     if (his3D1 && !fgForceTHnSparse) {
@@ -1002,6 +1009,8 @@ Int_t AliTPCPerformanceSummary::AnalyzeNCL(const AliPerformanceTPC* pTPC, TTreeS
     }    
         
     meanTPCnclF= his1D->GetMean();
+    meanTPCnclFStat= his1D->GetEntries();
+    meanTPCnclFErr= his1D->GetMeanError();
     rmsTPCnclF= his1D->GetRMS();
     delete his1D;
     
@@ -1038,7 +1047,9 @@ Int_t AliTPCPerformanceSummary::AnalyzeNCL(const AliPerformanceTPC* pTPC, TTreeS
     // dump results to the tree
     //
     (*pcstream)<<"tpcQA"<<
-      "meanTPCnclF="<<meanTPCnclF <<   
+      "meanTPCnclF="<<meanTPCnclF <<              // mean number  of  ratio findable clusters
+      "meanTPCnclFStat="<<meanTPCnclFStat <<              // mean number  of  ratio findable cluster - statistic used
+      "meanTPCnclFErr="<<meanTPCnclFErr <<              // mean number  of  ratio findable clusters  - errorr
       "rmsTPCnclF="<<rmsTPCnclF <<
       "meanTPCChi2="<<meanTPCChi2 <<
       "rmsTPCChi2="<<rmsTPCChi2 <<
@@ -1047,6 +1058,8 @@ Int_t AliTPCPerformanceSummary::AnalyzeNCL(const AliPerformanceTPC* pTPC, TTreeS
       "slopeATPCnclFErr="<< slopeATPCnclFErr<<
       "slopeCTPCnclFErr="<< slopeCTPCnclFErr<<
       "meanTPCncl="<<meanTPCncl <<
+      "meanTPCnclStat="<<meanTPCnclStat <<              // mean number  of cluster - statistic used
+      "meanTPCnclErr="<<meanTPCnclErr <<              // mean number  of clusters  - errorr
       "rmsTPCncl="<< rmsTPCncl<<
       "slopeATPCncl="<< slopeATPCncl<<
       "slopeCTPCncl="<< slopeCTPCncl<<
@@ -1127,7 +1140,7 @@ Int_t AliTPCPerformanceSummary::AnalyzeDrift(const AliPerformanceTPC* pTPC, TTre
     // dump drift QA values
     //
     (*pcstream)<<"tpcQA"<<
-        "offsetdZA="<< offsetdZA<<
+      "offsetdZA="<< offsetdZA<<   //
         "slopedZA="<< slopedZA<<
         "offsetdZC="<< offsetdZC<<
         "slopedZC="<<slopedZC<<
