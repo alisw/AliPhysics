91d06c07aa4b8ccea9f0335a0aa22b427701620b
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-11-21 16:23:21 +0000

    update from Constantin

--- PWGCF/Correlations/DPhi/FourierDecomposition/AliDhcTask.h
+++ PWGCF/Correlations/DPhi/FourierDecomposition/AliDhcTask.h
@@ -25,10 +25,10 @@ class AliDhcTask : public AliAnalysisTaskSE {
   AliDhcTask() : 
     AliAnalysisTaskSE(), fVerbosity(0), fEtaMax(1), fZVtxMax(10), fPtMin(0.25), fPtMax(15),
     fTrackDepth(0), fPoolSize(0), fTracksName(), fDoWeights(0),
-    fESD(0), fAOD(0), fOutputList(0), fHistPt(0), fHEvt(0), fHTrk(0), fHPtAss(0), 
-    fHPtTrg(0), fHPtTrg_Evt(0), fHCent(0), fHZvtx(0), fNbins(0), fHSs(0), fHMs(0), fIndex(0), 
-    fMeanPtTrg(0), fMeanPtAss(0), fMean2PtTrg(0), fMean2PtAss(0), 
-    fCentrality(99), fZVertex(99), fEsdTrackCutsTPCOnly(0), fPoolMgr(0),
+    fESD(0), fAOD(0), fOutputList(0), fHEvt(0), fHTrk(0), fHPtAss(0), 
+    fHPtTrg(0), fHPtTrg_Evt(0), fHCent(0), fHZvtx(0), fNbins(0), fHSs(0), fHMs(0), 
+    fHPts(0), fIndex(0), 
+    fCentrality(99), fZVertex(99), fEsdTPCOnly(0), fPoolMgr(0),
     fCentMethod("V0M"), fNBdeta(20), fNBdphi(36),
     fBPtT(0x0), fBPtA(0x0), fBCent(0x0), fBZvtx(0x0),
     fMixBCent(0x0), fMixBZvtx(0x0) {}
@@ -71,52 +71,48 @@ class AliDhcTask : public AliAnalysisTaskSE {
   void         Terminate(Option_t *);
 
  private:
-  Int_t        fVerbosity;       //  0 = silence
-  Double_t     fEtaMax;          //  Max |eta| cut (cm)
-  Double_t     fZVtxMax;         //  Max |z| cut (cm)
-  Double_t     fPtMin;           //  Min pt cut
-  Double_t     fPtMax;           //  Max pt cut
-  Int_t        fTrackDepth;      //  #tracks to fill pool
-  Int_t        fPoolSize;        //  Maximum number of events
-  TString      fTracksName;      //  name of track collection
-  Bool_t       fDoWeights;       //  if true weight with 1/N per event 
-  AliESDEvent *fESD;             //! ESD object
-  AliAODEvent *fAOD;             //! AOD object
-  TList       *fOutputList;      //! Output list
-  TH1F        *fHistPt;          //! Pt spectrum
-  TH2         *fHEvt;            //! Cent, vtx, etc.
-  TH2         *fHTrk;            //! Phi, Eta, etc.
-  TH1         *fHPtAss;          //! Pt ass 
-  TH1         *fHPtTrg;          //! Pt trg
-  TH1         *fHPtTrg_Evt;      //! Pt trg per event for weighting
-  TH1         *fHCent;           //! Centrality
-  TH1         *fHZvtx;           //! Zvertex
-  Int_t        fNbins;           //! Number of histogram bins
-  TH2        **fHSs;             //! Same-evt correlations
-  TH2        **fHMs;             //! Diff-evt correlations
-  TFormula    *fIndex;           //! Index for histograms
-  TProfile2D **fMeanPtTrg;       //! Mean pt trig 
-  TProfile2D **fMeanPtAss;       //! Mean pt ass
-  TProfile2D **fMean2PtTrg;      //! RMS pt trig 
-  TProfile2D **fMean2PtAss;      //! RMS pt ass
-  Double_t     fCentrality;      //! V0M for now
-  Double_t     fZVertex;         //! Of current event
-  AliESDtrackCuts   *fEsdTrackCutsTPCOnly; //! Track cuts
-  AliEvtPoolManager *fPoolMgr;             //! Event mixer
-  TString      fCentMethod;      //  centrality selection method
-  Int_t        fNBdeta;          //  no. deta bins
-  Int_t        fNBdphi;          //  no. dphi bins
-  TAxis       *fBPtT;            //  ptt binning
-  TAxis       *fBPtA;            //  pta binning
-  TAxis       *fBCent;           //  centrality binning
-  TAxis       *fBZvtx;           //  zvtx binning
-  TAxis       *fMixBCent;        //  centrality binning for mixing
-  TAxis       *fMixBZvtx;        //  zvtx binning for mixing
+  Int_t              fVerbosity;       //  0 = silence
+  Double_t           fEtaMax;          //  Max |eta| cut (cm)
+  Double_t           fZVtxMax;         //  Max |z| cut (cm)
+  Double_t           fPtMin;           //  Min pt cut
+  Double_t           fPtMax;           //  Max pt cut
+  Int_t              fTrackDepth;      //  #tracks to fill pool
+  Int_t              fPoolSize;        //  Maximum number of events
+  TString            fTracksName;      //  name of track collection
+  Bool_t             fDoWeights;       //  if true weight with 1/N per event 
+  AliESDEvent       *fESD;             //! ESD object
+  AliAODEvent       *fAOD;             //! AOD object
+  TList             *fOutputList;      //! Output list
+  TH2               *fHEvt;            //! Cent vs vtx.
+  TH2               *fHTrk;            //! Phi vs Eta
+  TH1               *fHPtAss;          //! Pt ass 
+  TH1               *fHPtTrg;          //! Pt trg
+  TH1               *fHPtTrg_Evt;      //! Pt trg per event for weighting
+  TH1               *fHCent;           //! Centrality
+  TH1               *fHZvtx;           //! Zvertex
+  Int_t              fNbins;           //! Number of histogram bins
+  TH2              **fHSs;             //! Same-evt correlations
+  TH2              **fHMs;             //! Diff-evt correlations
+  TH1              **fHPts;            //! Pt distributions 
+  TFormula          *fIndex;           //! Index for histograms
+  Double_t           fCentrality;      //! V0M for now
+  Double_t           fZVertex;         //! Of current event
+  AliESDtrackCuts   *fEsdTPCOnly;      //! Track cuts
+  AliEvtPoolManager *fPoolMgr;         //! Event mixer
+  TString            fCentMethod;      //  centrality selection method
+  Int_t              fNBdeta;          //  no. deta bins
+  Int_t              fNBdphi;          //  no. dphi bins
+  TAxis             *fBPtT;            //  ptt binning
+  TAxis             *fBPtA;            //  pta binning
+  TAxis             *fBCent;           //  centrality binning
+  TAxis             *fBZvtx;           //  zvtx binning
+  TAxis             *fMixBCent;        //  centrality binning for mixing
+  TAxis             *fMixBZvtx;        //  zvtx binning for mixing
 
   AliDhcTask(const AliDhcTask&);            // not implemented
   AliDhcTask &operator=(const AliDhcTask&); // not implemented
 
-  ClassDef(AliDhcTask, 2);
+  ClassDef(AliDhcTask, 3);
 };
 
 #endif
