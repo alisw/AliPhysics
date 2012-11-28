d0dbd3717285c3541c6acb71a8294ecd72debb0f
pcrochet <pcrochet@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-11-28 21:13:22 +0000

    1) Some changes to compute and plot background and jpsi evolutions as a function of run number 2)  Also the trigger lists to be considered are now comma separated instead of space separated (Laurent)

--- PWG/muondep/AliAnalysisMuMu.h
+++ PWG/muondep/AliAnalysisMuMu.h
@@ -55,27 +55,49 @@ public:
     };
     
     Result(TRootIOCtor* /*io*/) :
-    TNamed("",""), fTriggerName(), fNofTriggers(), fHC(0x0), fRebin(0), 
+    TNamed("",""),
+    fTriggerName(),
+    fEventSelection(),
+    fPairSelection(),
+    fCentralitySelection(),
+    fNofRuns(),
+    fNofTriggers(),
+    fHC(0x0), fRebin(0),
     fFitTotal(0x0),
     fMinv(0x0), fMinvLS(0), 
     fFitType(0), fMap(0x0)
     {
     }
     
-    Result(const char* name, Int_t ntriggers, AliHistogramCollection* hc, Int_t nrebin=1, UInt_t fitType=(kJpsi | kJpsiPrime)) :
-    TNamed("",""), fTriggerName(name), fNofTriggers(ntriggers), fHC(hc), fRebin(nrebin), 
+    Result(const char* triggerClass,
+           const char* eventSelection,
+           const char* pairSelection,
+           const char* centSelection,
+           Int_t ntriggers, AliHistogramCollection* hc, Int_t nrebin=1, UInt_t fitType=(kJpsi | kJpsiPrime)) :
+    TNamed("",""), fTriggerName(triggerClass),
+    fEventSelection(eventSelection),
+    fPairSelection(pairSelection),
+    fCentralitySelection(centSelection),
+    fNofRuns(1),
+    fNofTriggers(ntriggers), fHC(hc), fRebin(nrebin),
     fFitTotal(0x0),
     fMinv(0x0), fMinvLS(0), 
     fFitType(fitType), fMap(0x0)
     {
       if (hc) Fit(nrebin);
     }
-    
+  
     virtual ~Result();
+  
+    const char* GetName() const { return fTriggerName.Data(); }
     
     AliHistogramCollection* HC() const { return fHC; }
     
-    const char* TriggerClass() const { return fTriggerName.Data(); }
+    const char* TriggerClass() const { return GetName(); }
+    
+    const char* EventSelection() const { return fEventSelection.Data(); }
+    const char* PairSelection() const { return fPairSelection.Data(); }
+    const char* CentralitySelection() const { return fCentralitySelection.Data(); }
     
     Int_t NofTriggers() const { return fNofTriggers; }
     
@@ -146,6 +168,10 @@ public:
     
     UInt_t FitType() const { return fFitType; }
     
+    Int_t NofRuns() const { return fNofRuns; }
+    
+    void SetNofRuns(int n) { fNofRuns=n; }
+    
   private:
     Result(const Result&);
     Result& operator=(const Result&);
@@ -153,6 +179,10 @@ public:
     
   private:
     TString fTriggerName;
+    TString fEventSelection;
+    TString fPairSelection;
+    TString fCentralitySelection;
+    Int_t fNofRuns;
     Int_t fNofTriggers;
     AliHistogramCollection* fHC;
     Int_t fRebin;
@@ -163,17 +193,37 @@ public:
     
     mutable TMap* fMap; // map of results (string,TObjArray) each TObjArray = (value,error) = (TParameter<double>, TParameter<double>)
     
-    ClassDef(AliAnalysisMuMu::Result,5)
+    ClassDef(AliAnalysisMuMu::Result,7)
   };
 
+  /* Basic checks */
+  void BasicCounts(Bool_t detailTrigger=kFALSE,
+                   ULong64_t* totalNmb=0x0,
+                   ULong64_t* totalNmsl=0x0,
+                   ULong64_t* totalNmul=0x0);
 
-  static TObjArray* ComputeBackgroundEvolution(const char* filelist, const char* trigger, 
-                                               const char* outputfile, const char* outputMode="UPDATE");
-
-  void BasicCounts(Bool_t detailTrigger=kFALSE);
+  static void BasicCountsEvolution(const char* filelist, Bool_t detailTrigger=kFALSE);
   
-  void TriggerCountCoverage(const char* triggerList); 
+  void TriggerCountCoverage(const char* triggerList, Bool_t compact=kTRUE);
 
+
+  /** Background evolution functions */
+  static TObjArray* ComputeBackgroundEvolution(const char* filelist, const char* triggerList,
+                                               const char* outputfile="background-evolution.root", const char* outputMode="UPDATE");
+
+  static void PlotBackgroundEvolution(const char* gfile, const char* triggerList);
+  
+
+  /** Fitting */
+  static TMap* ComputeJpsiEvolution(const char* filelist, const char* triggerList,
+                                         const char* outputFile="jpsi-evolution.root", const char* outputMode="UPDATE",
+                                         Bool_t simulation=kFALSE);
+  
+  static void PlotJpsiEvolution(const char* resultFile, const char* triggerList, Bool_t fillBoundaries=kFALSE,
+                                const char* efficiencyFile=0x0);
+
+  ///------
+  
   static void CentralityCheck(const char* filelist);
 
   static TObjArray* CompareJpsiPerCMUUWithBackground(const char* jpsiresults="results.root",
@@ -196,7 +246,6 @@ public:
                            AliCounterCollection& cc,
                            const char* base,
                            const char* selection,
-                           const char* trackcut,
                            const char* paircut,
                            const char* centrality,
                            UInt_t fitType,
@@ -206,11 +255,7 @@ public:
   
   static ULong64_t GetTriggerScalerCount(const char* triggerList, Int_t runNumber);
 
-  static TObjArray* JpsiEvolution(const char* filelist, const char* output="res.root", Bool_t simulation=kFALSE);
-  
   TObjArray* Jpsi(Bool_t simulation=kFALSE);
-
-  static void PlotBackgroundEvolution(const char* gfile, const char* triggerList);
 
   static TObjArray* ReadFileList(const char* filelist);
   
@@ -221,6 +266,20 @@ public:
   void SetDimuonTriggerList(const char* dimuonTriggerList) { fDimuonTriggers = dimuonTriggerList; }
   void SetMuonTriggerList(const char* muonTriggerList) { fMuonTriggers = muonTriggerList; }
   void SetMinbiasTriggerList(const char* minbiasTriggerList) { fMinbiasTriggers = minbiasTriggerList; }
+
+  TString DimuonTriggerList() const { return fDimuonTriggers; }
+  TString MuonTriggerList() const { return fMuonTriggers; }
+  TString MinbiasTriggerList() const { return fMinbiasTriggers; }
+  
+  void SetEventSelectionList(const char* eventSelectionList) { fEventSelectionList = eventSelectionList; }
+  void SetPairSelectionList(const char* pairSelectionList) { fPairSelectionList = pairSelectionList; }
+  
+  static void SetDefaultDimuonTriggerList(const char* dimuonTriggerList) { fgDefaultDimuonTriggers = dimuonTriggerList; }
+  static void SetDefaultMuonTriggerList(const char* muonTriggerList) { fgDefaultMuonTriggers = muonTriggerList; }
+  static void SetDefaultMinbiasTriggerList(const char* minbiasTriggerList) { fgDefaultMinbiasTriggers = minbiasTriggerList; }
+  static void SetDefaultEventSelectionList(const char* eventSelectionList) { fgDefaultEventSelectionList = eventSelectionList; }
+  static void SetDefaultPairSelectionList(const char* pairSelectionList) { fgDefaultPairSelectionList = pairSelectionList; }
+
   
   AliHistogramCollection* HC() const { return fHistogramCollection; }
   AliCounterCollection* CC() const { return fCounterCollection; }
@@ -228,7 +287,9 @@ public:
   static void SetOCDBPath(const char* ocdbPath) { fgOCDBPath = ocdbPath; }
   
   static void SetColorScheme();
-  
+
+  static void DrawFill(Int_t run1, Int_t run2, double ymin, double ymax, const char* label);
+
 private:
 
   static TString ExpandPathName(const char* file);
@@ -239,10 +300,18 @@ private:
   TString fDimuonTriggers; // list of dimuon triggers to consider
   TString fMuonTriggers; // list of single muon triggers to consider
   TString fMinbiasTriggers;   // list of minbias triggers to consider
+  TString fEventSelectionList; // list of event types to consider
+  TString fPairSelectionList; // list of pair cuts to consider
 
   static TString fgOCDBPath; // OCDB to be used (raw:// by default)
   
-  ClassDef(AliAnalysisMuMu,1) // class to analysis results from AliAnalysisTaskMuMuXXX tasks
+  static TString fgDefaultMuonTriggers; // default list of single muon triggers
+  static TString fgDefaultMinbiasTriggers; // default list of MB triggers
+  static TString fgDefaultDimuonTriggers; // default list of dimuon triggers
+  static TString fgDefaultEventSelectionList; // default list of event selections
+  static TString fgDefaultPairSelectionList; // default list of pair selections
+
+  ClassDef(AliAnalysisMuMu,3) // class to analysis results from AliAnalysisTaskMuMuXXX tasks
 };
 
 #endif
