30a67835524e729efbc2fbcc09a2a59001d42401
hqvigsta <hqvigsta@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-12-13 23:17:38 +0000

    Added Tender to PHOSPi0Flow single-task macros.

--- PWGGA/PHOSTasks/PHOS_PbPb/macros/single-task/AddPi0FlowAndDependencies.C
+++ PWGGA/PHOSTasks/PHOS_PbPb/macros/single-task/AddPi0FlowAndDependencies.C
@@ -6,8 +6,8 @@ void AddPi0FlowAndDependencies()
   gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskVZEROEPSelection.C");
   AddTaskVZEROEPSelection();
   
-  // gROOT->LoadMacro("$ALICE_ROOT/PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C");
-  // AddAODPHOSTender();
+  gROOT->LoadMacro("$ALICE_ROOT/PWGGA/PHOSTasks/PHOS_PbPb/AddAODPHOSTender.C");
+  AddAODPHOSTender();
 
   gROOT->LoadMacro("$ALICE_ROOT/PWGGA/PHOSTasks/PHOS_PbPb/AddTaskPHOSPi0Flow.C") ;
   AliAnalysisTaskPi0Flow * task = AddTaskPHOSPi0Flow();
