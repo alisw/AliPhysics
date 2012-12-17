7a949b644f11ff5d4a57c525b24c9eccda0095c3
zconesa <zconesa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2012-12-17 11:55:38 +0000

    Coverity fix

--- PWGHF/vertexingHF/AliRDHFCutsD0toKpi.cxx
+++ PWGHF/vertexingHF/AliRDHFCutsD0toKpi.cxx
@@ -252,12 +252,12 @@ Int_t AliRDHFCutsD0toKpi::IsSelected(TObject* obj,Int_t selectionLevel,AliAODEve
   }
   //PrintAll();
   AliAODRecoDecayHF2Prong* d=(AliAODRecoDecayHF2Prong*)obj;
-  if(!d->HasSelectionBit(AliRDHFCutsD0toKpi::kD0toKpiCuts))return 0;
-
   if(!d){
     cout<<"AliAODRecoDecayHF2Prong null"<<endl;
     return 0;
   }
+
+  if(!d->HasSelectionBit(AliRDHFCutsD0toKpi::kD0toKpiCuts)) return 0;
 
   if(fKeepSignalMC) if(IsSignalMC(d,aod,421)) return 3;
 
