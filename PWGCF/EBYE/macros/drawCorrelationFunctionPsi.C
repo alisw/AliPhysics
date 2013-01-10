e5b200f96c77815c32682f8e8c511e6b5e64b1aa
miweber <miweber@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-01-10 14:57:51 +0000

    Including possibility to use convolution instead of event mixing for correlation functions

--- PWGCF/EBYE/macros/drawCorrelationFunctionPsi.C
+++ PWGCF/EBYE/macros/drawCorrelationFunctionPsi.C
@@ -34,18 +34,46 @@ void drawCorrelationFunctionPsi(const char* filename = "AnalysisResults.root",
   gSystem->Load("libPWGCFebye.so");
 
   //Prepare the objects and return them
+  TList *listQA = NULL;
   TList *list = GetListOfObjects(filename,gCentrality,gBit,gCentralityEstimator,0);
   TList *listShuffled = NULL;
   if(kShowShuffled) listShuffled = GetListOfObjects(filename,gCentrality,gBit,gCentralityEstimator,1);
   TList *listMixed = NULL;
   if(kShowMixed) listMixed = GetListOfObjects(filename,gCentrality,gBit,gCentralityEstimator,2);
 
+  // else  get the QA histograms (for convolution)
+  else{    
+    //Open the file again
+    TFile *f = TFile::Open(filename,"UPDATE");
+    if((!f)||(!f->IsOpen())) {
+      Printf("The file %s is not found.",filename);
+    }
+    
+    TDirectoryFile *dir = dynamic_cast<TDirectoryFile *>(f->Get("PWGCFEbyE.outputBalanceFunctionPsiAnalysis"));
+    if(!dir) {   
+      Printf("The TDirectoryFile is not found.",filename);
+    }
+
+    TString listQAName = "listQAPsi_";
+    
+    listQAName += centralityArray[gCentrality-1];
+    if(gBit > -1) {
+      listQAName += "_Bit"; listQAName += gBit; }
+    if(gCentralityEstimator) {
+      listQAName += "_"; listQAName += gCentralityEstimator;}
+
+    listQA = (TList*)dir->Get(Form("%s",listQAName.Data()));
+    if(!listQA) {
+      Printf("TList QA not found!!!");
+    }
+  }
+
   if(!list) {
     Printf("The TList object was not created");
     return;
   }
   else 
-    draw(list,listShuffled,listMixed,
+    draw(list,listShuffled,listMixed,listQA,
 	 gCentralityEstimator,gCentrality,psiMin,psiMax,
 	 ptTriggerMin,ptTriggerMax,ptAssociatedMin,ptAssociatedMax,normToTrig,rebinEta,rebinPhi);
 }
@@ -209,7 +237,7 @@ TList *GetListOfObjects(const char* filename,
 }
 
 //______________________________________________________//
-void draw(TList *list, TList *listBFShuffled, TList *listBFMixed, 
+void draw(TList *list, TList *listBFShuffled, TList *listBFMixed, TList *listQA,
 	  const char *gCentralityEstimator,
 	  Int_t gCentrality, Double_t psiMin, Double_t psiMax,
 	  Double_t ptTriggerMin, Double_t ptTriggerMax,
@@ -343,6 +371,7 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
     bMixed->SetHistNnn(hNNMixed);
   }
 
+
   TH2D *gHistPN[4];
   TH2D *gHistNP[4];
   TH2D *gHistPP[4];
@@ -353,6 +382,31 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
   TCanvas *cPP[4];
   TCanvas *cNN[4];
   TString histoTitle, pngName;
+
+  // if no mixing then divide by convoluted histograms
+  if(!listBFMixed && listQA){
+    
+    TH2D* fHistPos = (TH2D*)((TH3D*)listQA->FindObject("fHistEtaPhiPos"))->Project3D("xy");
+    fHistPos->GetYaxis()->SetRangeUser(-0.79,0.79);
+    fHistPos->Rebin2D(10,10);
+    
+    TH2D* fHistNeg = (TH2D*)((TH3D*)listQA->FindObject("fHistEtaPhiNeg"))->Project3D("xy");
+    fHistNeg->GetYaxis()->SetRangeUser(-0.79,0.79);
+    fHistNeg->Rebin2D(10,10);
+
+    gHistPN[2] = convolute2D(fHistPos, fHistNeg, "hConvPN");
+    gHistPN[2]->Scale(1./gHistPN[2]->GetBinContent(gHistPN[2]->FindBin(0,0)));
+
+    gHistNP[2] = convolute2D(fHistNeg, fHistPos, "hConvNP");
+    gHistNP[2]->Scale(1./gHistNP[2]->GetBinContent(gHistNP[2]->FindBin(0,0)));
+
+    gHistNN[2] = convolute2D(fHistNeg, fHistNeg, "hConvNN");
+    gHistNN[2]->Scale(1./gHistNN[2]->GetBinContent(gHistNN[2]->FindBin(0,0)));
+
+    gHistPP[2] = convolute2D(fHistPos, fHistPos, "hConvPP");
+    gHistPP[2]->Scale(1./gHistPP[2]->GetBinContent(gHistPP[2]->FindBin(0,0)));
+
+  }
   
   //(+-)
   histoTitle = "(+-) | Centrality: ";
@@ -433,6 +487,65 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
     
     // if normalization to trigger then do not divide Event mixing by number of trigger particles
     gHistPN[2] = bMixed->GetCorrelationFunctionPN(psiMin,psiMax,ptTriggerMin,ptTriggerMax,ptAssociatedMin,ptAssociatedMax);
+    if(rebinEta > 1 || rebinPhi > 1) gHistPN[2]->Rebin2D(rebinEta,rebinPhi);
+    
+    // normalization to 1 at (0,0) --> Jan Fietes method
+    if(normToTrig){
+      Double_t mixedNorm = gHistPN[2]->Integral(gHistPN[2]->GetXaxis()->FindBin(0-10e-5),gHistPN[2]->GetXaxis()->FindBin(0+10e-5),1,gHistPN[2]->GetNbinsX());
+      mixedNorm /= gHistPN[2]->GetNbinsY()*(gHistPN[2]->GetXaxis()->FindBin(0.01) - gHistPN[2]->GetXaxis()->FindBin(-0.01) + 1);
+      gHistPN[2]->Scale(1./mixedNorm);
+    } 
+
+    gHistPN[2]->GetYaxis()->SetTitleOffset(1.5);
+    gHistPN[2]->GetYaxis()->SetTitle("#Delta #varphi (rad)");
+    gHistPN[2]->SetTitle(histoTitle.Data());
+    cPN[2] = new TCanvas("cPN2","",0,200,600,500);
+    cPN[2]->SetFillColor(10); 
+    cPN[2]->SetHighLightColor(10);
+    gHistPN[2]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();    
+    pngName = "DeltaPhiDeltaEtaMixed.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".PositiveNegative.png";
+    //cPN[2]->SaveAs(pngName.Data());
+
+    //Correlation function (+-)
+    gHistPN[3] = dynamic_cast<TH2D *>(gHistPN[0]->Clone());
+    gHistPN[3]->Divide(gHistPN[2]);
+    gHistPN[3]->GetXaxis()->SetRangeUser(-1.5,1.5);
+    gHistPN[3]->GetZaxis()->SetTitle("C_{+-}(#Delta#eta,#Delta#varphi)");
+    cPN[3] = new TCanvas("cPN3","",0,300,600,500);
+    cPN[3]->SetFillColor(10); 
+    cPN[3]->SetHighLightColor(10);
+    gHistPN[3]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();    
+    pngName = "CorrelationFunction.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".PositiveNegative.png";
+    //cPN[3]->SaveAs(pngName.Data());
+  }
+  // if no mixing then divide by convoluted histograms
+  else if(listQA){
+    histoTitle = "(+-) mixed | Centrality: "; 
+    histoTitle += centralityArray[gCentrality-1]; 
+    histoTitle += "%";
+    if((psiMin == -0.5)&&(psiMax == 0.5))
+      histoTitle += " (-7.5^{o} < #varphi - #Psi_{2} < 7.5^{o})"; 
+    else if((psiMin == 0.5)&&(psiMax == 1.5))
+      histoTitle += " (37.5^{o} < #varphi - #Psi_{2} < 52.5^{o})"; 
+    else if((psiMin == 1.5)&&(psiMax == 2.5))
+      histoTitle += " (82.5^{o} < #varphi - #Psi_{2} < 97.5^{o})"; 
+    else 
+      histoTitle += " (0^{o} < #varphi - #Psi_{2} < 180^{o})"; 
+    
     if(rebinEta > 1 || rebinPhi > 1) gHistPN[2]->Rebin2D(rebinEta,rebinPhi);
     
     // normalization to 1 at (0,0) --> Jan Fietes method
@@ -604,7 +717,67 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
     pngName += ".NegativePositive.png";
     //cNP[3]->SaveAs(pngName.Data());
   }
-  
+  // if no mixing then divide by convoluted histograms
+  else if(listQA){
+    histoTitle = "(-+) mixed | Centrality: "; 
+    histoTitle += centralityArray[gCentrality-1]; 
+    histoTitle += "%";
+    if((psiMin == -0.5)&&(psiMax == 0.5))
+      histoTitle += " (-7.5^{o} < #varphi - #Psi_{2} < 7.5^{o})"; 
+    else if((psiMin == 0.5)&&(psiMax == 1.5))
+      histoTitle += " (37.5^{o} < #varphi - #Psi_{2} < 52.5^{o})"; 
+    else if((psiMin == 1.5)&&(psiMax == 2.5))
+      histoTitle += " (82.5^{o} < #varphi - #Psi_{2} < 97.5^{o})"; 
+    else 
+      histoTitle += " (0^{o} < #varphi - #Psi_{2} < 180^{o})"; 
+
+    if(rebinEta > 1 || rebinPhi > 1) gHistNP[2]->Rebin2D(rebinEta,rebinPhi);
+
+    // normalization to 1 at (0,0) --> Jan Fietes method
+    if(normToTrig){
+      Double_t mixedNorm = gHistNP[2]->Integral(gHistNP[2]->GetXaxis()->FindBin(0-10e-5),gHistNP[2]->GetXaxis()->FindBin(0+10e-5),1,gHistNP[2]->GetNbinsX());
+      mixedNorm /= gHistNP[2]->GetNbinsY()*(gHistNP[2]->GetXaxis()->FindBin(0.01) - gHistNP[2]->GetXaxis()->FindBin(-0.01) + 1);
+      gHistNP[2]->Scale(1./mixedNorm);
+    } 
+
+    gHistNP[2]->GetYaxis()->SetTitleOffset(1.5);
+    gHistNP[2]->GetYaxis()->SetTitle("#Delta #varphi (rad)");
+    gHistNP[2]->SetTitle(histoTitle.Data());
+    cNP[2] = new TCanvas("cNP2","",100,200,600,500);
+    cNP[2]->SetFillColor(10); 
+    cNP[2]->SetHighLightColor(10);
+    gHistNP[2]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();
+    pngName = "DeltaPhiDeltaEtaMixed.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".NegativePositive.png";
+    //cNP[2]->SaveAs(pngName.Data());
+
+    //Correlation function (-+)
+    gHistNP[3] = dynamic_cast<TH2D *>(gHistNP[0]->Clone());
+    gHistNP[3]->Divide(gHistNP[2]);
+    gHistNP[3]->GetXaxis()->SetRangeUser(-1.5,1.5);
+    gHistNP[3]->GetZaxis()->SetTitle("C_{-+}(#Delta#eta,#Delta#varphi)");
+    cNP[3] = new TCanvas("cNP3","",100,300,600,500);
+    cNP[3]->SetFillColor(10); 
+    cNP[3]->SetHighLightColor(10);
+    gHistNP[3]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();    
+    pngName = "CorrelationFunction.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".NegativePositive.png";
+    //cNP[3]->SaveAs(pngName.Data());
+  }
+
+
   //(++)
   histoTitle = "(++) | Centrality: "; 
   histoTitle += centralityArray[gCentrality-1]; 
@@ -685,6 +858,65 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
     
     // if normalization to trigger then do not divide Event mixing by number of trigger particles
     gHistPP[2] = bMixed->GetCorrelationFunctionPP(psiMin,psiMax,ptTriggerMin,ptTriggerMax,ptAssociatedMin,ptAssociatedMax);
+    if(rebinEta > 1 || rebinPhi > 1) gHistPP[2]->Rebin2D(rebinEta,rebinPhi);
+
+    // normalization to 1 at (0,0) --> Jan Fietes method
+    if(normToTrig){
+      Double_t mixedNorm = gHistPP[2]->Integral(gHistPP[2]->GetXaxis()->FindBin(0-10e-5),gHistPP[2]->GetXaxis()->FindBin(0+10e-5),1,gHistPP[2]->GetNbinsX());
+      mixedNorm /= gHistPP[2]->GetNbinsY()*(gHistPP[2]->GetXaxis()->FindBin(0.01) - gHistPP[2]->GetXaxis()->FindBin(-0.01) + 1);
+      gHistPP[2]->Scale(1./mixedNorm);
+    } 
+
+    gHistPP[2]->GetYaxis()->SetTitleOffset(1.5);
+    gHistPP[2]->GetYaxis()->SetTitle("#Delta #varphi (rad)");
+    gHistPP[2]->SetTitle(histoTitle.Data());
+    cPP[2] = new TCanvas("cPP2","",200,200,600,500);
+    cPP[2]->SetFillColor(10); 
+    cPP[2]->SetHighLightColor(10);
+    gHistPP[2]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();
+    pngName = "DeltaPhiDeltaEtaMixed.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".PositivePositive.png";
+    //cPP[2]->SaveAs(pngName.Data());
+
+    //Correlation function (++)
+    gHistPP[3] = dynamic_cast<TH2D *>(gHistPP[0]->Clone());
+    gHistPP[3]->Divide(gHistPP[2]);
+    gHistPP[3]->GetXaxis()->SetRangeUser(-1.5,1.5);
+    gHistPP[3]->GetZaxis()->SetTitle("C_{++}(#Delta#eta,#Delta#varphi)");
+    cPP[3] = new TCanvas("cPP3","",200,300,600,500);
+    cPP[3]->SetFillColor(10); 
+    cPP[3]->SetHighLightColor(10);
+    gHistPP[3]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();    
+    pngName = "CorrelationFunction.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".PositivePositive.png";
+    //cPP[3]->SaveAs(pngName.Data());
+  }
+  // if no mixing then divide by convoluted histograms
+  else if(listQA){
+        histoTitle = "(++) mixed | Centrality: "; 
+    histoTitle += centralityArray[gCentrality-1]; 
+    histoTitle += "%";
+    if((psiMin == -0.5)&&(psiMax == 0.5))
+      histoTitle += " (-7.5^{o} < #varphi - #Psi_{2} < 7.5^{o})"; 
+    else if((psiMin == 0.5)&&(psiMax == 1.5))
+      histoTitle += " (37.5^{o} < #varphi - #Psi_{2} < 52.5^{o})"; 
+    else if((psiMin == 1.5)&&(psiMax == 2.5))
+      histoTitle += " (82.5^{o} < #varphi - #Psi_{2} < 97.5^{o})"; 
+    else 
+      histoTitle += " (0^{o} < #varphi - #Psi_{2} < 180^{o})"; 
+    
     if(rebinEta > 1 || rebinPhi > 1) gHistPP[2]->Rebin2D(rebinEta,rebinPhi);
 
     // normalization to 1 at (0,0) --> Jan Fietes method
@@ -856,6 +1088,65 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
     pngName += ".NegativeNegative.png";
     //cNN[3]->SaveAs(pngName.Data());
   }
+  // if no mixing then divide by convoluted histograms
+  else if(listQA){
+        histoTitle = "(--) mixed | Centrality: "; 
+    histoTitle += centralityArray[gCentrality-1]; 
+    histoTitle += "%";
+    if((psiMin == -0.5)&&(psiMax == 0.5))
+      histoTitle += " (-7.5^{o} < #varphi - #Psi_{2} < 7.5^{o})"; 
+    else if((psiMin == 0.5)&&(psiMax == 1.5))
+      histoTitle += " (37.5^{o} < #varphi - #Psi_{2} < 52.5^{o})"; 
+    else if((psiMin == 1.5)&&(psiMax == 2.5))
+      histoTitle += " (82.5^{o} < #varphi - #Psi_{2} < 97.5^{o})"; 
+    else 
+      histoTitle += " (0^{o} < #varphi - #Psi_{2} < 180^{o})"; 
+    
+    if(rebinEta > 1 || rebinPhi > 1) gHistNN[2]->Rebin2D(rebinEta,rebinPhi);
+
+    // normalization to 1 at (0,0) --> Jan Fietes method
+    if(normToTrig){
+      Double_t mixedNorm = gHistNN[2]->Integral(gHistNN[2]->GetXaxis()->FindBin(0-10e-5),gHistNN[2]->GetXaxis()->FindBin(0+10e-5),1,gHistNN[2]->GetNbinsX());
+      mixedNorm /= gHistNN[2]->GetNbinsY()*(gHistNN[2]->GetXaxis()->FindBin(0.01) - gHistNN[2]->GetXaxis()->FindBin(-0.01) + 1);
+      gHistNN[2]->Scale(1./mixedNorm);
+    } 
+
+    gHistNN[2]->GetYaxis()->SetTitleOffset(1.5);
+    gHistNN[2]->GetYaxis()->SetTitle("#Delta #varphi (rad)");
+    gHistNN[2]->SetTitle(histoTitle.Data());
+    cNN[2] = new TCanvas("cNN2","",300,200,600,500);
+    cNN[2]->SetFillColor(10); 
+    cNN[2]->SetHighLightColor(10);
+    gHistNN[2]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();
+    pngName = "DeltaPhiDeltaEtaMixed.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".NegativeNegative.png";
+    //cNN[2]->SaveAs(pngName.Data());
+
+    //Correlation function (--)
+    gHistNN[3] = dynamic_cast<TH2D *>(gHistNN[0]->Clone());
+    gHistNN[3]->Divide(gHistNN[2]);
+    gHistNN[3]->GetXaxis()->SetRangeUser(-1.5,1.5);
+    gHistNN[3]->GetZaxis()->SetTitle("C_{--}(#Delta#eta,#Delta#varphi)");
+    cNN[3] = new TCanvas("cNN3","",300,300,600,500);
+    cNN[3]->SetFillColor(10); 
+    cNN[3]->SetHighLightColor(10);
+    gHistNN[3]->DrawCopy("surf1fb");
+    gPad->SetTheta(30); // default is 30
+    //gPad->SetPhi(130); // default is 30
+    gPad->SetPhi(-60); // default is 30
+    gPad->Update();    
+    pngName = "CorrelationFunction.Centrality"; 
+    pngName += centralityArray[gCentrality-1]; 
+    pngName += ".Psi"; pngName += psiMin; pngName += "To"; pngName += psiMax;
+    pngName += ".NegativeNegative.png";
+    //cNN[3]->SaveAs(pngName.Data());
+  }
 
   //Write to output file
   TString newFileName = "correlationFunction.Centrality";  
@@ -881,7 +1172,7 @@ void draw(TList *list, TList *listBFShuffled, TList *listBFMixed,
     gHistPP[1]->SetName("gHistPPShuffled"); gHistPP[1]->Write();
     gHistNN[1]->SetName("gHistNNShuffled"); gHistNN[1]->Write();
   }
-  if(listBFMixed) {
+  if(listBFMixed || (!listBFMixed&&listQA)) {
     gHistPN[2]->SetName("gHistPNMixed"); gHistPN[2]->Write();
     gHistNP[2]->SetName("gHistNPMixed"); gHistNP[2]->Write();
     gHistPP[2]->SetName("gHistPPMixed"); gHistPP[2]->Write();
@@ -1541,3 +1832,57 @@ void fitCorrelationFunctions(Int_t gCentrality = 1,
   
 
 // }
+
+TH2D *convolute2D(TH2D* h1, TH2D* h2, TString hname){
+  //
+  // this function does the convolution of 2 eta or phi "efficiencies" in a deta or dphi "efficiency"
+  // and returns a new histogram which is normalized to the number of bin combinations
+
+  // histogram definition
+  TH2D *hConv = NULL;
+  hConv = new TH2D(hname.Data(),hname.Data(), h1->GetNbinsX()+1,-2,2,h1->GetNbinsY()+1,-TMath::Pi()/2.,3*TMath::Pi()/2.);
+
+  Double_t x1 = 0.;
+  Double_t x2 = 0.;
+  Double_t y1 = 0.;
+  Double_t y2 = 0.;
+  Double_t z1 = 0.;
+  Double_t z2 = 0.;
+  Double_t dx = 0.;
+  Double_t dy = 0.;
+  Double_t dz = 0.;
+
+
+  // convolution
+  for(Int_t i = 0; i < h1->GetNbinsX(); i ++){
+    cout<<"CONVOLUTING BIN = "<<i<<" of "<<h1->GetNbinsX()<<endl;
+    for(Int_t j = 0; j < h1->GetNbinsY(); j ++){
+      for(Int_t k = 0; k < h2->GetNbinsX(); k ++){
+	for(Int_t l = 0; l < h2->GetNbinsY(); l ++){
+
+	  x1 = (Double_t)h1->GetXaxis()->GetBinCenter(i+1);
+	  y1 = (Double_t)h1->GetYaxis()->GetBinCenter(j+1);
+	  x2 = (Double_t)h2->GetXaxis()->GetBinCenter(k+1);
+	  y2 = (Double_t)h2->GetYaxis()->GetBinCenter(l+1);
+      
+	  z1 = (Double_t)h1->GetBinContent(i+1,j+1);
+	  z2 = (Double_t)h2->GetBinContent(k+1,l+1);
+
+	  dx = x1 - x2;
+	  dy = y1 - y2;
+
+	  if(dx>3./2.*TMath::Pi())  dx = dx - 2.*TMath::Pi();  
+	  if(dx<-1./2.*TMath::Pi()) dx = 2*TMath::Pi() + dx;  
+
+	  dz = z1*z2;
+
+	  hConv->Fill(dy,dx,dz);
+
+	}
+      }
+    }
+  }
+
+  return hConv;
+
+}  
