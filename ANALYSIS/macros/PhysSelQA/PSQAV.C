ecd59275b9f808e6dc6915d350b9de8155b1a703
zconesa <zconesa@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-01-24 20:31:51 +0000

    New input file defined with three columns (run number, fill number, number of events), plus visual separation of the results per fill (Sandro Bjelogrlic)

--- ANALYSIS/macros/PhysSelQA/PSQAV.C
+++ ANALYSIS/macros/PhysSelQA/PSQAV.C
@@ -12,15 +12,20 @@ void PSQAV(){
   Bool_t drawOverPlot    = kTRUE;  // if true, draw to canvas to see
   Bool_t saveOverPlotPdf = kTRUE;  // if true, write to pdf file
   Bool_t saveOverPlotEps = kTRUE;  // if true, write to eps file
+  Bool_t saveOnSameCanvas= kTRUE;  // if true, save v0 bkg on top of accepted histos
   //
   // configuration -- choose the input directory and file
   //
-  QAVobject1->SetROOTInput("../QAoutputPerPeriod/12d_Pass1/LHC12d_PSQA.root");
-  QAVobject1->InitializeSelectedPlots("../QAoutputPerPeriod/12d_Pass1/selectedPlots.list");
-	//QAVobject1->InitializeSelectedPlots("plots.list");
-  QAVobject1->SetOutDirectory("../QAoutputPerPeriod/12d_Pass1/plots");
-  QAVobject1->SetOutPDFName("PlotsLHC12d_pass.pdf");
-  QAVobject1->SetOutEPSName("PlotsLHC12d_pass.eps");
+  // QAVobject1->ImportRunAndFillInfo("LHC12d_list.list");
+  QAVobject1->ImportRunAndFillInfo("RunFillEvent_list.list");
+    
+  QAVobject1->SetROOTInput("~/Analysis/PWGPP_PSQA/QAoutputPerPeriod/pA/13b_ESDs_21Jan/LHC13b_21Jan_PSQA.root");
+  //QAVobject1->InitializeSelectedPlots("~/Analysis/PWGPP_PSQA/QAoutputPerPeriod/pA/13b_ESDs_21Jan/selectedPlots.list");
+  QAVobject1->InitializeSelectedPlots("selectedPlots.list");
+  //QAVobject1->InitializeSelectedPlots("plots.list");
+  QAVobject1->SetOutDirectory("~/Analysis/PWGPP_PSQA/QAoutputPerPeriod/pA/13b_ESDs_21Jan/plots");
+  QAVobject1->SetOutPDFName("Plots13b_ESDs.pdf");
+  QAVobject1->SetOutEPSName("Plots13b_ESDs.eps");
   QAVobject1->SetOverPlotTitle("");
   //
   // settings
@@ -30,10 +35,11 @@ void PSQAV(){
   QAVobject1->SetDrawOverPlot(drawOverPlot);
   QAVobject1->SetSaveOverPlotPDF(saveOverPlotPdf);
   QAVobject1->SetSaveOverPlotEPS(saveOverPlotEps); // if true, write to eps file
+  QAVobject1->SetPlotOnSameCanvas(saveOnSameCanvas);
   QAVobject1->SetUseColorArray(kTRUE);
   //
   QAVobject1->InitializeColorArray("colorArray.list"); // Set the color enums for the overplot
-    /// Use Auto Scale? See README.
+  /// Use Auto Scale? See README.
   QAVobject1->SetScaleAuto(useAutoScale);
   if (QAVobject1->GetScaleAuto()) { 
     QAVobject1->SetScaleAutoDivMin(1.1);
@@ -43,9 +49,9 @@ void PSQAV(){
     QAVobject1->SetScaleManMax(1.1);
   }
   //
-	cout << "Here we are?" << endl;
+  cout << "Here we are?" << endl;
 	
   QAVobject1->PostProcessQA();
-
- cout << "Done?" << endl;
+  
+  cout << "Done?" << endl;
 };
