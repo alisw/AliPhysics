69682be05968d3e344038b111bbb39581426059c
coppedis <coppedis@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-01-28 11:24:52 +0000

    Updating task for p-A

--- PWGPP/ZDC/AliAnalysisTaskZDCpA.h
+++ PWGPP/ZDC/AliAnalysisTaskZDCpA.h
@@ -42,12 +42,14 @@ class AliAnalysisTaskZDCpA : public AliAnalysisTaskSE {
   Bool_t   fIsMCInput;  	// true when input is MC
   //
   TList   *fOutput;		//! list send on output slot 0
+  TH1F    *fhTDCZNC;  		//! TDC ZNC sum
+  TH1F    *fhTDCZNA; 		//! TDC DIFF sum
   TH1F    *fhTDCZNSum;  	//! TDC ZNC sum
   TH1F    *fhTDCZNDiff; 	//! TDC DIFF sum
-  TH1F    *fhZNCSpectrum;	//! ZNC spectra
-  TH1F    *fhZNASpectrum;	//! ZNA spectra
-  TH1F    *fhZPCSpectrum;	//! ZPC spectra
-  TH1F    *fhZPASpectrum;	//! ZPA spectra
+  TH1F    *fhZNCSumQ;	 	//! ZNC sum 4Q
+  TH1F    *fhZNASumQ;		//! ZNA sum 4Q
+  TH1F    *fhZPCSumQ;		//! ZPC sum 4Q
+  TH1F    *fhZPASumQ;		//! ZPA sum 4Q
   TH1F    *fhZEM1Spectrum;	//! ZEM1 spectra
   TH1F    *fhZEM2Spectrum;	//! ZEM2 spectra
   TH1F    *fhZNCpmc;		//! ZNC PMCs
@@ -56,14 +58,12 @@ class AliAnalysisTaskZDCpA : public AliAnalysisTaskSE {
   TH1F    *fhZPApmc;		//! ZPA PMCs
   TH2F    *fhZNCCentroid;       //! ZNC centroid
   TH2F    *fhZNACentroid;       //! ZNA centroid
-  TH1F    *fhZNCemd;	     	//! ZNC sum low gain chain
-  TH1F    *fhZNAemd;	     	//! ZNA sum gain chain
   TH1F    *fhPMCZNCemd;      	//! ZNC PMC low gain chain
   TH1F    *fhPMCZNAemd;      	//! ZNA PMC low gain chain
   TH2F    *fDebunch;            //! TDC sum vs. diff
   
  
-  ClassDef(AliAnalysisTaskZDCpA,1); 
+  ClassDef(AliAnalysisTaskZDCpA,2); 
 
 };
 
