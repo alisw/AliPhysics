3cb073c51f1dc5e50194ccdb56e93261d93e5613
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-02-04 12:27:15 +0000

    complete rework (rosi)

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetSpectra.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetSpectra.h
@@ -3,76 +3,49 @@
 
 // $Id$
 
-class TList;
+
 class TH1F;
 class TH2F;
-class AliESDEvent;
+class THnSparse;
 
-#include "AliAnalysisTaskSE.h"
+#include "AliAnalysisTaskEmcalJet.h"
 
-class AliAnalysisTaskEmcalJetSpectra : public AliAnalysisTaskSE {
+class AliAnalysisTaskEmcalJetSpectra : public AliAnalysisTaskEmcalJet {
  public:
   AliAnalysisTaskEmcalJetSpectra();
   AliAnalysisTaskEmcalJetSpectra(const char *name);
   virtual ~AliAnalysisTaskEmcalJetSpectra() {}
   
-  virtual void           UserCreateOutputObjects();
-  virtual void           UserExec(Option_t *option);
-  virtual void           Terminate(Option_t *);
-
-  virtual void           SetAreaCut(Double_t a)                   { fAreacut    = a; }
-  virtual void           SetJetEta(Double_t emin, Double_t emax)  { fEtamin = emin; fEtamax = emax; }
-  virtual void           SetJetPhi(Double_t pmin, Double_t pmax)  { fPhimin = pmin; fPhimax = pmax; }
-  virtual void           SetJetsName(const char *n)               { fJetsName   = n; }
-  virtual void           SetRhos1Name(const char *n)              { fRhos1Name  = n; }
-  virtual void           SetRhos2Name(const char *n)              { fRhos2Name  = n; }
-  virtual void           SetRhos3Name(const char *n)              { fRhos3Name  = n; }
-  virtual void           SetTracksName(const char *n)             { fTracksName = n; }
   
- protected:
-  virtual Int_t          GetCentBin(Double_t cent) const;
-   
- private:
-  TString                fTracksName;              // name of track collection
-  TString                fJetsName;                // name of jet collection
-  TString                fClustersName;            // name of clusters collection
-  TString                fRhos1Name;               // name of Rho1 array output
-  TString                fRhos2Name;               // name of Rho2 array output
-  TString                fRhos3Name;               // name of Rho2 array output
-  Double_t               fPhimin;                  // phi min
-  Double_t               fPhimax;                  // phi max
-  Double_t               fEtamin;                  // eta min
-  Double_t               fEtamax;                  // eta max
-  Double_t               fAreacut;                 // area cut
+  virtual void           UserCreateOutputObjects();
 
-  AliESDEvent           *fESD;                     //!esd event
-  TList                 *fOutputList;              //!output list
-  TH1F                  *fHistCentrality;          //!centrality
-  TH2F                  *fHistDeltaRho12vsCent;    //!delta rho1 and rho2 vs centrality
-  TH2F                  *fHistDeltaRho13vsCent;    //!delta rho1 and rho3 vs centrality
-  TH2F                  *fHistDeltaRho23vsCent;    //!delta rho2 and rho3 vs centrality
-  TH2F                  *fHistDeltaJetPt12vsCent;  //!delta jet pt1 and pt2 vs centrality 
-  TH2F                  *fHistDeltaJetPt13vsCent;  //!delta jet pt1 and pt3 vs centrality 
-  TH2F                  *fHistDeltaJetPt23vsCent;  //!delta jet pt2 and pt3 vs centrality 
-  TH2F                  *fHistRho1vsCent;          //!rho1 vs centrality
-  TH2F                  *fHistRho2vsCent;          //!rho2 vs centrality
-  TH2F                  *fHistRho3vsCent;          //!rho3 vs centrality
-  TH2F                  *fHistNEFvsPt[6][4];       //!neutral energy fraction vs pt
-  TH2F                  *fHistZvsPt[6][4];         //!z all vs pt
-  TH2F                  *fHistZchvsPt[6][4];       //!z charged vs pt
-  TH2F                  *fHistZemvsPt[6][4];       //!z neutral vs pt
-  TH1F                  *fHistJetPt[6][4];         //!jet pt
-  TH1F                  *fHistJetPt3[6][4];        //!jet pt>3
-  TH1F                  *fHistJetPt5[6][4];        //!jet pt>5
-  TH1F                  *fHistJetPt7[6][4];        //!jet pt>7
-  TH1F                  *fHistJetPt9[6][4];        //!jet pt>9
-  TH2F                  *fHistNconsvsPt[6][4];     //!constituents vs pt
-  TH1F                  *fHistRawJetPt[6];         //!raw jet pt
-  TH2F                  *fHistAreavsRawPt[6];      //!area vs raw pt
+ protected:
+  Bool_t                 Run();
+  virtual Int_t          GetCentBin(Double_t cent) const;
+  Float_t                RelativePhi(Double_t mphi,Double_t vphi) const;
+
+ private:
+  TH2F                  *fHistRhovsCent; //!
+  TH2F                  *fHistNjetvsCent;          //!number of jets versus Centrality
+  TH2F                  *fHistJetPtvsTrackPt[6];//!
+  TH2F                  *fHistRawJetPtvsTrackPt[6];//!
+  TH1F                  *fHistTrackPt[6];//!
+  TH1F                  *fHistEP0[6];//!
+  TH1F                  *fHistEP0A[6];//!
+  TH1F                  *fHistEP0C[6];//!
+  TH2F                  *fHistEPAvsC[6];//!
+  TH2F                  *fHistJetPtvsdEP[6];//!
+  TH2F                  *fHistJetPtvsdEPBias[6];//!
+  TH2F                  *fHistJetPtvsEP[6];//!
+  TH2F                  *fHistJetPtvsEPBias[6];//!
+  TH2F                  *fHistRhovsEP[6]; //!
+
+
+
 
   AliAnalysisTaskEmcalJetSpectra(const AliAnalysisTaskEmcalJetSpectra&); // not implemented
   AliAnalysisTaskEmcalJetSpectra& operator=(const AliAnalysisTaskEmcalJetSpectra&); // not implemented
   
-  ClassDef(AliAnalysisTaskEmcalJetSpectra, 3); // Emcal jet spectra task
+  ClassDef(AliAnalysisTaskEmcalJetSpectra, 4); // Emcal jet spectra task
 };
 #endif
