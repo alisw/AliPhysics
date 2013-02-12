1a51648cdb05ffcbd31143e4bf81104143ca0a45
wiechula <wiechula@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-02-12 22:40:08 +0000

    o use TPC eta correction by default

--- ANALYSIS/macros/AddTaskPIDResponse.C
+++ ANALYSIS/macros/AddTaskPIDResponse.C
@@ -1,7 +1,7 @@
 AliAnalysisTask *AddTaskPIDResponse(Bool_t isMC=kFALSE, Bool_t autoMCesd=kTRUE,
                                     Bool_t tuneOnData=kFALSE, Int_t recoPass=2,
                                     Bool_t cachePID=kFALSE, TString detResponse="",
-                                    Bool_t useTPCEtaCorrection = kFALSE)
+                                    Bool_t useTPCEtaCorrection = kTRUE)
 {
 // Macro to connect a centrality selection task to an existing analysis manager.
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
