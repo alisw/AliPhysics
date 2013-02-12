d67c651a917bc519ed0d59e674c0f01120b430d3
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-02-12 14:42:51 +0000

    Next version of the muon-hadron correlations task.

---
+++ PWGCF/Correlations/DPhi/MuonHadron/AddAnalysisTaskMuonHadronCorrelations.C
@@ -0,0 +1,38 @@
+AliAnalysisTaskMuonHadronCorrelations *AddAnalysisTaskMuonHadronCorrelations(const char *centMethod = "V0M") {
+  
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    printf("Error in adding AnalysisTaskMuonHadronCorrelations: no Analysis Manager found!\n");
+    return NULL;
+  }
+
+  AliAnalysisTaskMuonHadronCorrelations *task = new AliAnalysisTaskMuonHadronCorrelations("AliAnalysisTaskMuonHadronCorrelations");
+
+  // Set analysis cuts   
+  task->SetFilterBitCentralBarrel(7);  // -> 128
+  task->SetMaxEtaCentralBarrel(1.0);
+  task->SetTriggerMatchLevelMuon(1);
+
+  const Int_t nBinCent = 4;
+  Double_t centLimits[nBinCent+1] = {0., 20., 40, 60., 100.};
+  task->SetCentBinning(nBinCent, centLimits);
+
+  task->SetCentMethod(centMethod);
+
+  const Int_t nBinPt = 3;
+  Double_t ptLimits[nBinPt+1] = {0., 1., 2., 4.};
+  task->SetPtBinning(nBinPt, ptLimits);
+
+  mgr->AddTask(task);
+
+  // create output container
+  AliAnalysisDataContainer *output = mgr->CreateContainer("MuonHadronCorrHistos", TList::Class(), AliAnalysisManager::kOutputContainer,
+							  Form("%s:MuonHadronCorrelations_%s", AliAnalysisManager::GetCommonFileName(), centMethod));
+  
+  // finaly connect input and output
+  mgr->ConnectInput(task, 0,  mgr->GetCommonInputContainer());
+  mgr->ConnectOutput(task, 1, output);
+    
+  return task;
+}
+
