fd3b14b70415ab49d22916cb30723a1757b47ee2
dgangadh <dgangadh@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-02-19 07:48:40 +0000

    K0s AddTask Modifications (Matt Steinpreis)

--- PWGCF/FEMTOSCOPY/macros/AddTaskK0Femto.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskK0Femto.C
@@ -11,7 +11,8 @@ AliFemtoK0Analysis *AddTaskK0Femto(bool FieldPositive = kTRUE){
   mgr->AddTask(K0Task);
 
   TString outputFileName = AliAnalysisManager::GetCommonFileName();
-  outputFileName += ":PWGCF.outputK0Analysis.root";
+  if(FieldPositive) outputFileName += ":PWGCF.outputK0Analysis_FieldPos.root";
+  else outputFileName += ":PWGCF.outputK0Analysis_FieldNeg.root";
   AliAnalysisDataContainer *coutputK0 = mgr->CreateContainer("MyList", TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName.Data());
 
   mgr->ConnectInput(K0Task, 0, mgr->GetCommonInputContainer());
