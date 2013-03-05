30ddd47c0a8d1afe3e91bf5a679615db515b44fa
mkrzewic <mkrzewic@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-03-05 12:04:52 +0000

    coverity: null dereference fixed

--- TPC/Base/AliTPCcalibDB.cxx
+++ TPC/Base/AliTPCcalibDB.cxx
@@ -1520,6 +1520,7 @@ void AliTPCcalibDB::UpdateChamberHighVoltageData()
   //
   // initialise graph with active running
   AliCDBEntry *entry = GetCDBEntry("GRP/CTP/Scalers");
+  if (!entry) return;
   entry->SetOwner(kTRUE);
   AliTriggerRunScalers *sca = (AliTriggerRunScalers*)entry->GetObject();
   Int_t nchannels = sca->GetNumClasses(); // number of scaler channels (i.e. trigger classes)
