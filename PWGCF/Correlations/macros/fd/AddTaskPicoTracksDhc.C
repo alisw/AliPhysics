c10dd8389b2e3140aaa7cb1b288719ce19cdbbc2
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-03-14 14:38:17 +0000

    generalize

--- PWGCF/Correlations/macros/fd/AddTaskPicoTracksDhc.C
+++ PWGCF/Correlations/macros/fd/AddTaskPicoTracksDhc.C
@@ -1,7 +1,9 @@
 // $Id$
 
 void AddTaskPicoTracksDhc(
-  TString chNOutTracks = "PicoTracks"
+  TString chNOutTracks   = "PicoTracks"
+  TString period         = "LHC11h",
+  Bool_t includeITSRefit = kFALSE
 ) 
 {
   // Get the analysis manager
@@ -17,17 +19,19 @@ void AddTaskPicoTracksDhc(
   TString chIsESD("ESD");
   
   if (chIsESD.EqualTo(mgr->GetInputEventHandler()->GetDataType())) {
+    TString cuts("Hybrid_");
+    cuts += period;
     Info("AddTaskPicoTracksDhc","adding ESD track selection task ...");
     // ESD Track Cuts
     gROOT->LoadMacro("$ALICE_ROOT/PWG/EMCAL/macros/AddTaskEmcalEsdTpcTrack.C");
-    AliEmcalEsdTpcTrackTask *hybTask = AddTaskEmcalEsdTpcTrack("HybridTracks","Hybrid_LHC11h",kFALSE);
+    AliEmcalEsdTpcTrackTask *hybTask = AddTaskEmcalEsdTpcTrack("HybridTracks", cuts.Data(), includeITSRefit);
     hybTask->SelectCollisionCandidates(AliVEvent::kAny);
     // Pico Tracks
-    pTrackTask = AddTaskEmcalPicoTrackMaker(chNOutTracks.Data(), "HybridTracks", "LHC11h");
+    pTrackTask = AddTaskEmcalPicoTrackMaker(chNOutTracks.Data(), "HybridTracks", period);
     pTrackTask->SelectCollisionCandidates(AliVEvent::kAny);
   } else {
     Info("AddTaskPicoTracksDhc","AOD analysis, adding PicoTrack maker ...");
-    pTrackTask = AddTaskEmcalPicoTrackMaker(chNOutTracks.Data(),"tracks","LHC11h",kFALSE);
+    pTrackTask = AddTaskEmcalPicoTrackMaker(chNOutTracks.Data(),"tracks", period, includeITSRefit);
     pTrackTask->SelectCollisionCandidates(AliVEvent::kAny);
   }
 }
