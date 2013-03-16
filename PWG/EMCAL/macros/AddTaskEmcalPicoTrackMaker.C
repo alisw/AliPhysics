a7c8406ba6b2215b8ab6e6a54272e45a7094ef30
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-03-16 16:29:57 +0000

    allow to set more options

--- PWG/EMCAL/macros/AddTaskEmcalPicoTrackMaker.C
+++ PWG/EMCAL/macros/AddTaskEmcalPicoTrackMaker.C
@@ -5,6 +5,12 @@ AliEmcalPicoTrackMaker* AddTaskEmcalPicoTrackMaker(
   const char *inname       = "tracks",
   const char *runperiod    = "",
   Bool_t      includeNoITS = kTRUE,
+  Double_t ptmin           = 0,
+  Double_t ptmax           = 1000,
+  Double_t etamin          = -10,
+  Double_t etamax          = +10,
+  Double_t phimin          = -10,
+  Double_t phimax          = +10,
   AliESDtrackCuts *cuts    = 0
 )
 {  
@@ -33,21 +39,30 @@ AliEmcalPicoTrackMaker* AddTaskEmcalPicoTrackMaker(
   eTask->SetTracksOutName(name);
   eTask->SetTracksInName(inname);
   eTask->SetIncludeNoITS(includeNoITS);
+  eTask->SetTrackPtLimits(ptmin, ptmax);
+  eTask->SetTrackEtaLimits(etamin, etamax);
+  eTask->SetTrackPhiLimits(phimin, phimax);
+
   TString runPeriod(runperiod);
   runPeriod.ToLower();
   if (runPeriod == "lhc11h") {
     eTask->SetAODfilterBits(256,512); // hybrid tracks for LHC11h
     eTask->SetMC(kFALSE);
-  }
-  else if (runPeriod == "lhc11a") {
+  } else if (runPeriod == "lhc11a") {
     eTask->SetAODfilterBits(256,16); // hybrid tracks for LHC11a
     eTask->SetMC(kFALSE);
-  }
-  else if (runPeriod == "lhc12a15a" || runPeriod == "lhc12a15e") {
+  } else if (runPeriod == "lhc12a15a" || runPeriod == "lhc12a15e") {
     eTask->SetAODfilterBits(256,16); // hybrid tracks for LHC12a15a and LHC12a15e
     eTask->SetMC(kTRUE);
-  }
-  else {
+  } else if (runPeriod.Contains(":")) {
+    TObjArray *arr = runPeriod.Tokenize(":");
+    TString arg1(arr->At(0)->GetName());
+    TString arg2("-1");
+    if (arr->GetEntries()>1)
+      arg2 = arr->At(1)->GetName();
+    eTask->SetAODfilterBits(arg1.Atoi(),arg2.Atoi());
+    delete arr;
+  } else {
     if (runPeriod.IsNull())
       ::Warning("Run period %s not known. It will use IsHybridGlobalConstrainedGlobal.");
   }
