216f325f37f7ea9d0003afe7dab9c4160a7f65b8
mbombara <mbombara@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-03-18 11:37:52 +0000

    Change the name of the output file to default one.

--- PWGLF/STRANGENESS/Correlations/macros/AddTaskV0ChCorrelations.C
+++ PWGLF/STRANGENESS/Correlations/macros/AddTaskV0ChCorrelations.C
@@ -2,8 +2,7 @@
 //
 // AddTask for AliAnalysisTaskV0ChCorrelations task
 //
-AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const char * outfilename,
-														 const Bool_t bMCtruth=kFALSE,
+AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const Bool_t bMCtruth=kTRUE,
 														 Float_t DcaDToPV = 0.5,
 														 Float_t DcaV0D = 0.1
 														)
@@ -20,15 +19,18 @@ AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const char * outfilenam
     }
 
     // mc event handlerrunEx01.C
-    if(bMCtruth) {
-        AliMCEventHandler* mchandler = new AliMCEventHandler();
+    /*
+	if(bMCtruth) {
+		cout << "I am here too! " << endl;
+        AliAODMCEventHandler* mchandler = new AliAODMCEventHandler();
         // Not reading track references
         mchandler->SetReadTR(kFALSE);
         mgr->SetMCtruthEventHandler(mchandler);
     }   
-
+*/
 	// create task
     AliAnalysisTaskV0ChCorrelations* task = new AliAnalysisTaskV0ChCorrelations("V0ChCorrelations_task");
+    task->SetAnalysisMC(bMCtruth);
 	task->SetDcaDToPV(DcaDToPV);
 	task->SetDcaV0D(DcaV0D);
     mgr->AddTask(task);
@@ -37,11 +39,11 @@ AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const char * outfilenam
     // Get and connect other common input/output containers via the manager as below
     //==============================================================================
     TString outputFileName = AliAnalysisManager::GetCommonFileName();
-    outputFileName = "list.grid.v0ch.root";
+    //outputFileName = "list.grid.v0ch.root";
 
     // create containers for input/output
     AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
-    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer(outfilename, TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName);
+    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("coutput1", TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName);
         
     // connect input/output
     mgr->ConnectInput(task, 0, cinput);
