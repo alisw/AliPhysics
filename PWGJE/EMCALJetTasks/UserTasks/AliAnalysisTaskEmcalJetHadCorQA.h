8c956ef4469550aa008f4ddfe24a7d1c53524dad
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-03-25 15:10:43 +0000

    from Rosi

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetHadCorQA.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetHadCorQA.h
@@ -17,6 +17,7 @@ class AliAnalysisTaskEmcalJetHadCorQA : public AliAnalysisTaskEmcalJet {
   
   virtual void           UserCreateOutputObjects();
   void                   SetCalo2Name(const char *n) {fCalo2Name = n;}
+  void                   SetMCName(const char *n) {fMCParticlesName = n;}
 
  protected:
   Bool_t                 Run();
@@ -27,22 +28,30 @@ class AliAnalysisTaskEmcalJetHadCorQA : public AliAnalysisTaskEmcalJet {
  private:
   TString             fCalo2Name;
   TClonesArray       *fCaloClusters2;
+  TString             fMCParticlesName;
+  TClonesArray       *fMCParticles;
   TH2F                  *fHistRhovsCent;           //!rho vs cent
   TH2F                  *fHistNjetvsCent;          //!number of jets versus Centrality
-  TH2F               *fHistNEFvsPt[3];      //!
+  TH2F               *fHistNEFvsPt[3];          //!
   TH2F               *fHistNTMatchvsPt[3];      //!  
   TH2F               *fHistNCMatchvsPt[3];      //!
-  TH2F               *fHistHadCorvsPt[3];      //!
+  TH2F               *fHistHadCorvsPt[3];       //!
+  TH2F               *fHistNconvsPt[3];         //!
+  TH2F               *fHistNtvsPt[3];         //!
+  TH2F               *fHistNcvsPt[3];         //!
   TH2F               *fHistNEFvsPtBias[3];      //!
-  TH2F               *fHistNTMatchvsPtBias[3];      //!  
+  TH2F               *fHistNTMatchvsPtBias[3];  //!  
   TH2F               *fHistNCMatchvsPtBias[3];      //!
   TH2F               *fHistHadCorvsPtBias[3];      //!
+  TH2F               *fHistNconvsPtBias[3];         //!
+  TH2F               *fHistNtvsPtBias[3];         //!
+  TH2F               *fHistNcvsPtBias[3];         //!
   TH3F               *fHistNTMatchvsPtvsNtack0;      //!
 
 
   AliAnalysisTaskEmcalJetHadCorQA(const AliAnalysisTaskEmcalJetHadCorQA&); // not implemented
   AliAnalysisTaskEmcalJetHadCorQA& operator=(const AliAnalysisTaskEmcalJetHadCorQA&); // not implemented
   
-  ClassDef(AliAnalysisTaskEmcalJetHadCorQA, 1); // Emcal jet spectra task
+  ClassDef(AliAnalysisTaskEmcalJetHadCorQA, 2); // Hadronic Correction for jet task
 };
 #endif
