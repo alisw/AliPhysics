bc1bee849cc1b9b49dbc466dae66f6c5b81227a6
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-03-25 15:13:51 +0000

    add new info

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskCLQA.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskCLQA.h
@@ -15,6 +15,7 @@ class TTree;
 #include "AliAnalysisTaskEmcalJet.h"
 
 class AliNtupCumInfo;
+class AliNtupZdcInfo;
 
 class AliAnalysisTaskCLQA : public AliAnalysisTaskEmcalJet {
  public:
@@ -40,22 +41,23 @@ class AliAnalysisTaskCLQA : public AliAnalysisTaskEmcalJet {
   Double_t                    fCumMmin;          // minimum number of tracks for cumulants 
   TTree                      *fNtupCum;          //!ntuple for cumulant analysis
   AliNtupCumInfo             *fNtupCumInfo;      //!object holding cumulant results
+  AliNtupZdcInfo             *fNtupZdcInfo;      //!object holding zdc info
 
  private:
   AliAnalysisTaskCLQA(const AliAnalysisTaskCLQA&);            // not implemented
   AliAnalysisTaskCLQA &operator=(const AliAnalysisTaskCLQA&); // not implemented
 
-  ClassDef(AliAnalysisTaskCLQA, 1) // Constantin's Task
+  ClassDef(AliAnalysisTaskCLQA, 2) // Constantin's Task
 };
 
 class AliNtupCumInfo {
  public:
-    AliNtupCumInfo() : fTrig(0), fRun(0), fVz(0), fIsFEC(0), fIsVSel(0), fIsP(0),
-                       fMall(0), fMall2(0), fPtMaxall(0), fMPtall(0), 
-                       fMPt2all(0), fMPtall2(0), fTSall(0),
-                       fM(0), fQ2abs(0), fQ4abs(0), fQ42re(0),
-                       fPtMax(0), fMPt(0), fMPt2(0), fTS(0), fMV0M(0), 
-                       fCl1(0), fV0M(0), fV0MEq(0), fV0A(0), fV0AEq(0), fZNA(0) {;}
+  AliNtupCumInfo() : fTrig(0), fRun(0), fVz(0), fIsFEC(0), fIsVSel(0), fIsP(0),
+                     fMall(0), fMall2(0), fPtMaxall(0), fMPtall(0), 
+                     fMPt2all(0), fMPtall2(0), fTSall(0),
+                     fM(0), fQ2abs(0), fQ4abs(0), fQ42re(0), fCos2phi(0), fSin2phi(0),
+                     fPtMax(0), fMPt(0), fMPt2(0), fTS(0), fMV0M(0), 
+                     fCl1(0), fV0M(0), fV0MEq(0), fV0A(0), fV0AEq(0), fZNA(0) {;}
   virtual ~AliNtupCumInfo() {;}
 
  public:
@@ -73,9 +75,11 @@ class AliNtupCumInfo {
   Double32_t    fMPtall2;      //[0,0,16] mean pT truncated above 1 GeV/c
   Double32_t    fTSall;        //[0,0,16] transverse sphericity
   Int_t         fM;            // multiplicity (tracks in pT range)
-  Double_t      fQ2abs;        // Q2 absolute
-  Double_t      fQ4abs;        // Q4 absolute
-  Double_t      fQ42re;        // Re(Q2Q*Q*)
+  Double32_t    fQ2abs;        // Q2 absolute
+  Double32_t    fQ4abs;        // Q4 absolute
+  Double32_t    fQ42re;        // Re(Q2Q*Q*)
+  Double32_t    fCos2phi;      // Cos(2phi)
+  Double32_t    fSin2phi;      // Sin(2phi)
   Double32_t    fPtMax;        //[0,0,16] maximum pT
   Double32_t    fMPt;          //[0,0,16] mean pT
   Double32_t    fMPt2;         //[0,0,16] mean pT2
@@ -87,8 +91,24 @@ class AliNtupCumInfo {
   Double32_t    fV0A;          //[0,0,16] class V0A
   Double32_t    fV0AEq;        //[0,0,16] class V0A Eq
   Double32_t    fZNA;          //[0,0,16] class ZNA
- 
-  ClassDef(AliNtupCumInfo,2) // Cumulant storage class
+
+  ClassDef(AliNtupCumInfo,3) // Cumulant storage class
 };
+
+class AliNtupZdcInfo {
+ public:
+   AliNtupZdcInfo() : fZna0(0), fZna1(0), fZna2(0), fZna3(0), fZna4(0) {;}
+   virtual ~AliNtupZdcInfo() {;}
+
+ public:
+  Double32_t    fZna0;         // ZNA energy 0 
+  Double32_t    fZna1;         // ZNA energy 1 
+  Double32_t    fZna2;         // ZNA energy 2 
+  Double32_t    fZna3;         // ZNA energy 3 
+  Double32_t    fZna4;         // ZNA energy 4 
+
+  ClassDef(AliNtupZdcInfo,1) // ZDC storage class
+};
+
 
 #endif
