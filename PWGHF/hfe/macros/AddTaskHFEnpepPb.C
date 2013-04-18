3c4d320ce80011e88d10017f3de70456f1b4f602
ssakai <ssakai@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-04-18 13:38:24 +0000

    updtaes for pPb analysis

--- PWGHF/hfe/macros/AddTaskHFEnpepPb.C
+++ PWGHF/hfe/macros/AddTaskHFEnpepPb.C
@@ -1,15 +1,13 @@
 AliAnalysisTask *AddTaskHFEnpepPb(){
-  // Switches
-  Bool_t kPhotonic                   = kTRUE;
 
-  // Default settings (TOF-TPC 7 TeV pp paper)
-  const int	kDefTPCcl	= 120;
+  // Default settings (TOF-TPC pPb)
+  const int	kDefTPCcl	= 110;
   const int	kDefTPCclPID	=  80;
   const int	kDefITScl	=   4;
   const double	kDefDCAr	=   1.;
   const double	kDefDCAz	=   2.;
-  const double	kDefTPCs	=   0.026;
-  const double	kDefTPCu	=   2.8;
+  const double	kDefTPCs	=   0.0;
+  const double	kDefTPCu	=   3.0;
   const double	kDefTOFs	=   3.;
 
   // Default setting for the associated electron for the NonPhotonic Analysis
@@ -21,6 +19,13 @@ AliAnalysisTask *AddTaskHFEnpepPb(){
   const double	kassDCAz	=   2.0;
   const double	kassTPCSminus	=  -3.0;
   const double	kassTPCSplus	=   3.0;
+
+  enum{
+          kHFEV0A = 1,
+          kHFEV0M = 2,
+          kHFECL1 = 3,
+          kHFEZNA = 4
+  };
 
   //get the current analysis manager
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
@@ -37,36 +42,53 @@ AliAnalysisTask *AddTaskHFEnpepPb(){
 
   //@@ 0 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-  // Remember: (e.g. for 120 clusters) This is only an example to show how to calculate!
-  // effective mean m0=-0.113
-  // width w=1.03
-  // cut --> nsigma*w + m0
-  // e.g. 45% 0.122 sigma --> 0.122*1.03-0.113 = 0.013
-
-  if(kPhotonic)
-  {
-    // Task for pp @ 7 TeV
-    //RegisterTaskNPPID2( MCthere, kDefTPCcl, kDefTPCclPID, kDefITScl, kDefDCAr, kDefDCAz, kDefTPCs, kDefTPCu, kDefTOFs, AliHFEextraCuts::kFirst, kTRUE, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, kassTPCSminus, kassTPCSplus);
-
-    // task for pPb @ 5.023 TeV
-    //RegisterTaskNPPID2( MCthere, 110, 80, 3, 1, 2, 0.045, 2.685, 3, AliHFEextraCuts::kBoth, kTRUE, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, kassTPCSminus, kassTPCSplus);
-
-    //task for pp @ 2.76 TeV
-    RegisterTaskNPPID2( MCthere, 110, 80, 3, 1, 2, -1.35, 2.45, 3, AliHFEextraCuts::kBoth, kTRUE, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, kassTPCSminus, kassTPCSplus);
+  Double_t dEdxlm[12], dEdxhm[12], dEdxaclm[12], dEdxachm[12];
+  for(int icent = 0; icent < 12; icent++){
+    dEdxlm[icent] = kDefTPCs;
+    dEdxhm[icent] = kDefTOFs;
+    dEdxaclm[icent] = kassTPCSminus;
+    dEdxachm[icent] = kassTPCSplus;
   }
+
+  RegisterTaskNPEpPb( MCthere, kDefTPCcl, kDefTPCclPID, kDefITScl, kDefDCAr, kDefDCAz, dEdxlm, dEdxhm, kDefTOFs, AliHFEextraCuts::kBoth, kHFEV0A, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, dEdxaclm, dEdxachm);
+  RegisterTaskNPEpPb( MCthere, kDefTPCcl, kDefTPCclPID, kDefITScl, kDefDCAr, kDefDCAz, dEdxlm, dEdxhm, kDefTOFs, AliHFEextraCuts::kBoth, kHFEV0M, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, dEdxaclm, dEdxachm);
+  RegisterTaskNPEpPb( MCthere, kDefTPCcl, kDefTPCclPID, kDefITScl, kDefDCAr, kDefDCAz, dEdxlm, dEdxhm, kDefTOFs, AliHFEextraCuts::kBoth, kHFECL1, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, dEdxaclm, dEdxachm);
+  RegisterTaskNPEpPb( MCthere, kDefTPCcl, kDefTPCclPID, kDefITScl, kDefDCAr, kDefDCAz, dEdxlm, dEdxhm, kDefTOFs, AliHFEextraCuts::kBoth, kHFEZNA, kassETA, kassITS, kassTPCcl, kassTPCPIDcl, kassDCAr, kassDCAz, dEdxaclm, dEdxachm);
 
   return NULL;
 }
 
 //===============================================================================
 
-AliAnalysisTask *RegisterTaskNPPID2( Bool_t useMC, Int_t tpcCls=120, Int_t tpcClsPID=80, Int_t itsCls=4, Double_t dcaxy=1.0, Double_t dcaz=2.0, Double_t tpcs=-0.0113, Double_t tpcu=3.09, Double_t tofs=3., Int_t itshitpixel=0, Bool_t kNoPhotonic=kFALSE, Double_t assETA=0.8, Int_t assITS=2, Int_t assTPCcl=100, Int_t assTPCPIDcl=80, Double_t assDCAr=1.0, Double_t assDCAz=2.0, Double_t assTPCSminus=-3.0, Double_t assTPCSplus=3.0 )
+AliAnalysisTask *RegisterTaskNPEpPb(Bool_t useMC, 
+               Int_t tpcCls=120, Int_t tpcClsPID=80, 
+               Int_t itsCls=4, Double_t dcaxy=1.0, Double_t dcaz=2.0, 
+               Double_t *tpcdEdxcutlow=NULL, Double_t *tpcdEdxcuthigh=NULL, 
+               Double_t tofs=3., Int_t itshitpixel =AliHFEextraCuts::kBoth, Int_t icent=1,
+               Double_t assETA=0.8, Int_t assITS=2, Int_t assTPCcl=100, 
+               Int_t assTPCPIDcl=80, Double_t assDCAr=1.0, Double_t assDCAz=2.0, 
+               Double_t *assTPCSminus = NULL, Double_t *assTPCSplus=NULL )
 {
+
+  Int_t iassETA  = (Int_t)(assETA*10);
+  Int_t iassDCAr = (Int_t)(assDCAr*10);
+  Int_t iassDCAz = (Int_t)(assDCAz*10);
+  Int_t iassTPCSminus = assTPCSminus ? (Int_t)(assTPCSminus[0]*10) : 0;
+  Int_t iassTPCSplus  = assTPCSplus ? (Int_t)(assTPCSplus[0]*10) : 0;
+
+  if (icent == 2) TString cesti("V0M");
+  else if (icent == 3) TString cesti("CL1");
+  else if (icent == 4) TString cesti("ZNA");
+  else TString cesti("V0A");
+
+  TString appendix(TString::Format("PhotonicEta%dITS%dTPCcl%dTPCPIDcl%dDCAr%dDCAz%dTPCSminus%dTPCSplus%d%s", iassETA, assITS, assTPCcl, assTPCPIDcl, iassDCAr, iassDCAz, iassTPCSminus, iassTPCSplus, cesti.Data()));
+  printf("Add macro appendix %s\n", appendix.Data());
+
   gROOT->LoadMacro("$ALICE_ROOT/PWGHF/hfe/macros/configs/pPb/ConfigHFEnpepPb.C");
 
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
-  AliAnalysisTaskHFE *task = ConfigHFEnpepPb(useMC, tpcCls, tpcClsPID, itsCls, dcaxy, dcaz, tpcs, tpcu, tofs, itshitpixel, kNoPhotonic, assETA, assITS, assTPCcl, assTPCPIDcl, assDCAr, assDCAz, assTPCSminus, assTPCSplus);
+  AliAnalysisTaskHFE *task = ConfigHFEnpepPb(useMC, appendix, tpcCls, tpcClsPID, itsCls, dcaxy, dcaz, tpcdEdxcutlow, tpcdEdxcuthigh, tofs, 0, itshitpixel, icent, assETA, assITS, assTPCcl, assTPCPIDcl, assDCAr, assDCAz, assTPCSminus, assTPCSplus);
   task->SetESDAnalysis();
 
   if (useMC)	task->SetHasMCData(kTRUE);
@@ -74,27 +96,8 @@ AliAnalysisTask *RegisterTaskNPPID2( Bool_t useMC, Int_t tpcCls=120, Int_t tpcCl
 
   task->SelectCollisionCandidates(AliVEvent::kINT7);
 
-  Int_t idcaxy = (Int_t)(dcaxy*10.);
-  Int_t idcaz  = (Int_t)(dcaz*10.);
-  Int_t itpcs  = (Int_t)(tpcs*1000.);
-  Int_t itofs  = (Int_t)(tofs*10.);
-  Int_t ipixelany   = itshitpixel;
-
-  Int_t iNoPhotonic = 0;
-  if(kNoPhotonic) iNoPhotonic = 1;
-
-  Int_t iassETA  = (Int_t)(assETA*10);
-  Int_t iassDCAr = (Int_t)(assDCAr*10);
-  Int_t iassDCAz = (Int_t)(assDCAz*10);
-  Int_t iassTPCSminus = (Int_t)(assTPCSminus*10);
-  Int_t iassTPCSplus  = (Int_t)(assTPCSplus*10);
-
-  TString appendix(TString::Format("Photonic%dEta%dITS%dTPCcl%dTPCPIDcl%dDCAr%dDCAz%dTPCSminus%dTPCSplus%d", iNoPhotonic, iassETA, assITS, assTPCcl, assTPCPIDcl, iassDCAr, iassDCAz, iassTPCSminus, iassTPCSplus));
-  //TString appendix(TString::Format("TPCc%dTPCp%dITS%dDCAr%dz%dTPCs%dTOFs%dpa%dNoPhotonic%d",tpcCls,tpcClsPID,itsCls,idcaxy,idcaz,itpcs,itofs,ipixelany,iNoPhotonic));
-  printf("Add macro appendix %s\n", appendix.Data());
-
   TString containerName = mgr->GetCommonFileName();
-  containerName += ":HFEpPb";
+  containerName += ":HFEtask";
   containerName += appendix.Data();
   printf("container name: %s\n", containerName.Data());
 
--- PWGHF/hfe/macros/configs/pPb/ConfigHFEnpepPb.C
+++ PWGHF/hfe/macros/configs/pPb/ConfigHFEnpepPb.C
@@ -1,36 +1,21 @@
-AliAnalysisTaskHFE* ConfigHFEnpepPb(Bool_t useMC, UChar_t TPCcl=70, UChar_t TPCclPID = 80, UChar_t ITScl=3, Double_t DCAxy=1000., Double_t DCAz=1000., Double_t TPCs=0., Double_t TPCu=3.09, Double_t TOFs=3., Int_t itshitpixel = 0,  Bool_t kNoPhotonic = kFALSE, Double_t assETA=0.8, Int_t assITS=2, Int_t assTPCcl=100, Int_t assTPCPIDcl=80, Double_t assDCAr=1.0, Double_t assDCAz=2.0, Double_t assTPCSminus=-3.0, Double_t assTPCSplus=3.0 )
+AliAnalysisTaskHFE* ConfigHFEnpepPb(Bool_t useMC, TString appendix,
+                UChar_t TPCcl=70, UChar_t TPCclPID = 80, 
+                UChar_t ITScl=3, Double_t DCAxy=1000., Double_t DCAz=1000., 
+                Double_t* tpcdEdxcutlow=NULL, Double_t* tpcdEdxcuthigh=NULL, 
+                Double_t TOFs=3., Int_t TOFmis=0, 
+                Int_t itshitpixel = 0, Int_t icent, 
+                Double_t assETA=0.8, Int_t assITS=2, 
+                Int_t assTPCcl=100, Int_t assTPCPIDcl=80, 
+                Double_t assDCAr=1.0, Double_t assDCAz=2.0, 
+                Double_t *assTPCSminus=NULL, Double_t *assTPCSplus=NULL)
 {
-
-  //
-  // HFE task configuration PID2 (TOF-TPC only!)
-  //
-
-  Bool_t kAnalyseTaggedTracks = kFALSE;
-  Int_t ptbin = 0;
-  Double_t IpSig = 3.;
-  Bool_t prodcut = kFALSE;
-  Bool_t ipOpp = kFALSE;
-  Bool_t mcstr  = kFALSE;
-
-  Int_t iDCAxy = (Int_t)(DCAxy*10.);
-  Int_t iDCAz  = (Int_t)(DCAz*10.);
-  Int_t iTPCs  = (Int_t)(TPCs*1000.);
-  Int_t iTOFs  = (Int_t)(TOFs*10.);
-  Int_t iIpSig = (Int_t)(IpSig*10.);
-  Int_t iIpOpp = 0;
-  Int_t iProdCut = 1;
-  Int_t iMCStr = 0;
-  Int_t iPixelAny = itshitpixel;
-  Int_t iNoPhotonic = 0;
-  if(ipOpp)iIpOpp = 1;
-  if(prodcut) iProdCut = 0;
-  if(mcstr) iMCStr = 1;
-  if(kNoPhotonic) iNoPhotonic = 1;
-
-  TString appendixx(TString::Format("t%di%dr%dz%ds%dt%dpa%dptbin%dNoPhotonic%d",TPCcl,ITScl,iDCAxy,iDCAz,iTPCs,iTOFs,iPixelAny,ptbin,iNoPhotonic));
-  printf("appendixx %s\n", appendixx.Data());
-
-  AliHFEcuts *hfecuts = new AliHFEcuts(Form("hfeCutsPID2%s",appendixx.Data()),"HFE cuts TOF TPC");
+  Bool_t kAnalyseTaggedTracks = kTRUE;
+ 
+  //***************************************//
+  //        Setting up the HFE cuts        //
+  //***************************************//
+  
+  AliHFEcuts *hfecuts = new AliHFEcuts(appendix,"HFE cuts pPb");
   //hfecuts->SetQAOn();
   hfecuts->CreateStandardCuts();
   hfecuts->SetMinNClustersTPC(TPCcl);
@@ -41,56 +26,33 @@ AliAnalysisTaskHFE* ConfigHFEnpepPb(Bool_t useMC, UChar_t TPCcl=70, UChar_t TPCc
   hfecuts->SetCutITSpixel(itshitpixel);
   hfecuts->SetCheckITSLayerStatus(kFALSE);
   hfecuts->SetEtaRange(0.8);
-  Bool_t ipCharge = kFALSE;
-  if(IpSig<0)ipCharge = kTRUE;
-
-  // beauty
-  hfecuts->SetIPcutParam(0,0,0,0,kFALSE,ipCharge,ipOpp);
-  
-  if(TMath::Abs(IpSig)>100&&TMath::Abs(IpSig)<300){
-    hfecuts->SetIPcutParam(0.0064,0.078,-0.56,0,kFALSE,ipCharge,ipOpp); // used Carlo's old parameter (new: 0.011+0.077*exp(-0.65*pt))
-  }
-  else if(TMath::Abs(IpSig)>300&&TMath::Abs(IpSig)<320){
-    hfecuts->SetIPcutParam(0.00978806,0.0860193,-0.567707,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>320&&TMath::Abs(IpSig)<340){
-    hfecuts->SetIPcutParam(0.00359394,0.0707044,-0.569791,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>340&&TMath::Abs(IpSig)<360){
-    hfecuts->SetIPcutParam(0.00807899,0.0818196,-0.563656,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>360&&TMath::Abs(IpSig)<380){
-    hfecuts->SetIPcutParam(0.005207,0.0747452,-0.569437,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>380&&TMath::Abs(IpSig)<400){
-    hfecuts->SetIPcutParam(0.011549,0.0899016,-0.573654,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>400&&TMath::Abs(IpSig)<420){
-    hfecuts->SetIPcutParam(0.00166944,0.066695,-0.556119,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>420&&TMath::Abs(IpSig)<440){
-    hfecuts->SetIPcutParam(0.0129519,0.0918703,-0.557745,0,kFALSE,ipCharge,ipOpp);
-  }
-  else if(TMath::Abs(IpSig)>440&&TMath::Abs(IpSig)<460){
-    hfecuts->SetIPcutParam(0.0000824499,0.0629347,-0.554436,0,kFALSE,ipCharge,ipOpp);
-  }
-
-  if(prodcut) hfecuts->SetProductionVertex(0,100,0,100);
-  else {
-    if((iPixelAny==AliHFEextraCuts::kAny) || (iPixelAny==AliHFEextraCuts::kSecond)) hfecuts->SetProductionVertex(0,7,0,7);
-  }
- 
-  //hfecuts->SetSigmaToVertex(DCAsi);
   hfecuts->SetMaxImpactParam(DCAxy,DCAz);
-  hfecuts->SetTOFPIDStep(kTRUE);
-  //hfecuts->SetQAOn();
   hfecuts->SetUseMixedVertex(kTRUE);
   hfecuts->SetVertexRange(10.);
 
-  AliAnalysisTaskHFE *task = new AliAnalysisTaskHFE(Form("HFEanalysisPID2%s",appendixx.Data()));
+  // TOF settings:
+  Int_t usetof=0;
+  Bool_t kTOFmis=kFALSE;
+  if (TOFs>0.){
+    usetof = 1;
+    printf("CONFIGURATION FILE: TOF is used \n");
+    hfecuts->SetTOFPIDStep(kTRUE);
+    printf("CONFIGURATION FILE: TOF PID step is requested !!!! \n");
+    if (TOFmis>0){
+      kTOFmis = kTRUE;
+      printf("CONFIGURATION FILE: TOF mismatch rejection is set ON \n");
+    }
+  }
+
+  //***************************************//
+  //        Setting up the task            //
+  //***************************************//
+
+  AliAnalysisTaskHFE *task = new AliAnalysisTaskHFE(Form("HFEtask%s",appendix.Data()));
   printf("task %p\n", task);
+  task->SetpPbAnalysis();
   task->SetHFECuts(hfecuts);
-  task->SetRemovePileUp(kTRUE);
+  task->SetRemovePileUp(kFALSE);
   task->GetPIDQAManager()->SetHighResolutionHistos();
 
   // Setttings for pPb
@@ -98,13 +60,12 @@ AliAnalysisTaskHFE* ConfigHFEnpepPb(Bool_t useMC, UChar_t TPCcl=70, UChar_t TPCc
   hfecuts -> SetUseCorrelationVertex();
   hfecuts -> SetSPDVtxResolutionCut();
 
+  //***************************************//
+  //          Variable manager             //
+  //***************************************//
+
   // Define Variables
-  if(ptbin==1){
-    Double_t ptbinning[19] = {0., 0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 2., 2.5, 3., 4., 5., 6., 8., 12., 16., 20.};
-  }
-  else{
-    Double_t ptbinning[36] = {0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.75, 2., 2.25, 2.5, 2.75, 3., 3.5, 4., 4.5, 5., 5.5, 6., 7., 8., 10., 12., 14., 16., 18., 20.};
-  }
+  Double_t ptbinning[36] = {0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.75, 2., 2.25, 2.5, 2.75, 3., 3.5, 4., 4.5, 5., 5.5, 6., 7., 8., 10., 12., 14., 16., 18., 20.};
   Double_t etabinning[17] = {-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
 
   Int_t sizept=(sizeof(ptbinning)/sizeof(double))-1;
@@ -116,102 +77,109 @@ AliAnalysisTaskHFE* ConfigHFEnpepPb(Bool_t useMC, UChar_t TPCcl=70, UChar_t TPCc
   vm->AddVariable("phi",21, -0, 2*TMath::Pi());
   vm->AddVariable("charge");
   vm->AddVariable("source");
-  //vm->AddVariable("centrality");
+  vm->AddVariable("centrality");
 
-  // Background Subtraction ---------------------------------------------------------------------------------
-  if(kNoPhotonic)
-  {
-    AliHFENonPhotonicElectron *backe = new AliHFENonPhotonicElectron(Form("HFEBackGroundSubtractionPID2%s",appendixx.Data()),"Background subtraction");  //appendix
-    //Setting the Cuts for the Associated electron-pool
-/*
-    AliESDtrackCuts *hfeBackgroundCuts = new AliESDtrackCuts();
-    hfeBackgroundCuts->SetAcceptKinkDaughters(kFALSE);
-    hfeBackgroundCuts->SetRequireTPCRefit(kTRUE);
-    hfeBackgroundCuts->SetRequireITSRefit(kTRUE);
-    hfeBackgroundCuts->SetMinNClustersITS(2);
-    hfeBackgroundCuts->SetEtaRange(-0.8,0.8);
-    hfeBackgroundCuts->SetRequireSigmaToVertex(kTRUE);
-    hfeBackgroundCuts->SetMaxChi2PerClusterTPC(4);
-    hfeBackgroundCuts->SetMinNClustersTPC(100);
-    hfeBackgroundCuts->SetPtRange(0.1,1e10);
-*/
-    AliHFEcuts *hfeBackgroundCuts = new AliHFEcuts(Form("HFEBackSub%s",appendixx.Data()),"Background sub Cuts");
-    hfeBackgroundCuts->SetEtaRange(assETA);
-    hfeBackgroundCuts->SetPtRange(0.1,1e10);
+  // Determine the centrality estimator
+  task->SetCentralityEstimator("V0A");
+  if (icent == 2) task->SetCentralityEstimator("V0M");
+  else if (icent == 3) task->SetCentralityEstimator("CL1");
+  else if (icent == 4) task->SetCentralityEstimator("ZNA");
 
-    hfeBackgroundCuts->SetMaxChi2perClusterTPC(4);
-    hfeBackgroundCuts->SetMinNClustersITS(assITS);
-    hfeBackgroundCuts->SetMinNClustersTPC(assTPCcl);
-    hfeBackgroundCuts->SetMinNClustersTPCPID(assTPCPIDcl);
+  // For the moment, remove the part dedicated to the background subtraction.
+  // It will be implemented in a different way, reading it from a root file.
+ 
 
-    //hfeBackgroundCuts->SetMaxImpactParam(assDCAr,assDCAz);
-
-    //hfeBackgroundCuts->SetMinRatioTPCclusters(...);		//TODO needed ???
-    //hfeBackgroundCuts->SetRequireSigmaToVertex();
-    //hfeBackgroundCuts->SetDebugLevel(2);
-    hfeBackgroundCuts->SetQAOn();			        // QA
-
-    AliHFEpid *pidbackground = backe->GetPIDBackground();
-    if(useMC) pidbackground->SetHasMCData(kTRUE);
-    //pidbackground->AddDetector("TOF", 0);
-    pidbackground->AddDetector("TPC", 0);
-    pidbackground->ConfigureTPCasymmetric(0.,20.,assTPCSminus,assTPCSplus);
-    //pidbackground->ConfigureTOF(3.);
-    backe->GetPIDBackgroundQAManager()->SetHighResolutionHistos();
-    backe->SetHFEBackgroundCuts(hfeBackgroundCuts);
-
-    task->SetHFEBackgroundSubtraction(backe);
-
-  }
-  //-----------------------------------------------------------------------------------------------------------
-
-  if(!useMC){
-    // New background model (LHC10d pass2)
-    TF1 *hBackground = new TF1("hadronicBackgroundFunction", "TMath::Exp(([0]/(x**1.5))+[1])", 0., 20.);
-    // These settings assume that the default is a cut on .ge.120 TPC clusters (Sep 27, 2011)
-    hBackground->SetParameter(0, -55.18);
-    hBackground->SetParameter(1, -0.0026);
-    if (TPCcl == 100){
-      hBackground->SetParameter(0, -39.5);
-      hBackground->SetParameter(1, -0.438);
-    } elseif (TPCcl == 140){
-      hBackground->SetParameter(0, -82.11);
-      hBackground->SetParameter(1, 1.138);
-    }
-
-    task->SetBackGroundFactorsFunction(hBackground);
-  }
+  //***************************************//
+  //          Configure the PID            //
+  //***************************************//
 
   // Define PID
   AliHFEpid *pid = task->GetPID();
   if(useMC) pid->SetHasMCData(kTRUE);
-  pid->AddDetector("TOF", 0);
-  pid->AddDetector("TPC", 1);
-  // HERE PUT THE STRAIGHT CUT
-  Double_t params[4];
-  char *cutmodel;
-  if(useMC){
-    // Monte-Carlo needs modelling of the falling mean with momentum at low momentum
-    // for high momentum it is consistent with a flat -0.94
-    cutmodel = "expo(0)+pol1(2)";//[0]*TMath::Exp([1]*x) + [2] + [3]*x";
-    Double_t paramsMC[4] = {-1.00625e-01, -2.09446e+00, -4.71247e-01, 1.80301e-02};
-    for(int ipar = 0; ipar < 4; ipar++) params[ipar] = paramsMC[ipar];
-  } else {
-    // Data is consistent with a flat constant: (Sep 27, 2011)
-    // 100 clusters: mean = -0.076, width = 1.035
-    // 120 clusters: mean = -0.113, width = 1.03
-    // 140 clusters: mean = -0.093, width = 1.004
-    cutmodel = "pol0(0)";
-    params[0] = TPCs;
 
+  if (usetof){
+    pid->AddDetector("TOF", 0);
+    pid->AddDetector("TPC", 1);
+  } else {
+    pid->AddDetector("TPC", 0);
   }
-  pid->ConfigureTOF(TOFs);
-  pid->ConfigureTPCdefaultCut(cutmodel, params, TPCu);
+  
+  // Configure TPC PID
+  if(!useMC){
+    Double_t paramsTPCdEdxcutlow[12] ={0.0, 0.0, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+    if(tpcdEdxcutlow) memcpy(paramsTPCdEdxcutlow,tpcdEdxcutlow,sizeof(paramsTPCdEdxcutlow));
+
+    Double_t paramsTPCdEdxcuthigh[12] ={3.0, 3.0, 3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0};
+    if(tpcdEdxcuthigh) memcpy(paramsTPCdEdxcuthigh,tpcdEdxcuthigh,sizeof(paramsTPCdEdxcuthigh));
+    
+    char *cutmodel;
+    cutmodel="pol0";
+    
+    for(Int_t a=0;a<11;a++)
+    {
+      //   cout << a << " " << paramsTPCdEdxcut[a] << endl;
+      Double_t tpcparamlow[1]={paramsTPCdEdxcutlow[a]};
+      Float_t tpcparamhigh=paramsTPCdEdxcuthigh[a];
+      pid->ConfigureTPCcentralityCut(a,cutmodel,tpcparamlow,tpcparamhigh);
+    }
+  }
+
+  // Configure TOF PID
+  if (usetof){
+    pid->ConfigureTOF(TOFs);
+    AliHFEpidTOF *tofpid = pid->GetDetPID(AliHFEpid::kTOFpid);
+    if (kTOFmis){
+      tofpid->SetRejectTOFmismatch();
+    }
+  }
 
   // To make different upper TOF cut to see contamination effect
   // The below two lines should be removed after this check
   //AliHFEpidTOF *tofpid = pid->GetDetPID(AliHFEpid::kTOFpid);
   //if(TOFs<3.) tofpid->SetTOFnSigmaBand(-3,TOFs); //only to check the assymmetric tof cut
+  
+  //***************************************//
+  //       Configure NPE plugin            //
+  //***************************************//
+
+  AliHFENonPhotonicElectron *backe = new AliHFENonPhotonicElectron(Form("HFEBackGroundSubtractionPID2%s",appendix.Data()),"Background subtraction");  //appendix
+    //Setting the Cuts for the Associated electron-pool
+  AliHFEcuts *hfeBackgroundCuts = new AliHFEcuts(Form("HFEBackSub%s",appendix.Data()),"Background sub Cuts");
+  hfeBackgroundCuts->SetEtaRange(assETA);
+  hfeBackgroundCuts->SetPtRange(0.1,1e10);
+
+  hfeBackgroundCuts->SetMaxChi2perClusterTPC(4);
+  hfeBackgroundCuts->SetMinNClustersITS(assITS);
+  hfeBackgroundCuts->SetMinNClustersTPC(assTPCcl);
+  hfeBackgroundCuts->SetMinNClustersTPCPID(assTPCPIDcl);
+  hfeBackgroundCuts->SetQAOn();			        // QA
+
+  AliHFEpid *pidbackground = backe->GetPIDBackground();
+  if(useMC) pidbackground->SetHasMCData(kTRUE);
+  pidbackground->AddDetector("TPC", 0);
+  Double_t paramsTPCdEdxcutlowAssoc[12] ={-3.0,-3.0,-3.0,-3.0,-3.0,-3.0,-3.0,-3.0,-3.0,-3.0,-3.0,-3.0};
+  if(assTPCSminus) memcpy(paramsTPCdEdxcutlowAssoc,assTPCSminus,sizeof(paramsTPCdEdxcutlowAssoc));
+
+  Double_t paramsTPCdEdxcuthighAssoc[12] ={3.0, 3.0, 3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0};
+  if(assTPCSplus) memcpy(paramsTPCdEdxcuthighAssoc,assTPCSplus,sizeof(paramsTPCdEdxcuthighAssoc));
+    
+  char *cutmodelAssoc;
+  cutmodelAssoc="pol0";
+  for(Int_t a=0;a<11;a++)
+  {
+    //   cout << a << " " << paramsTPCdEdxcut[a] << endl;
+    Double_t tpcparamlow[1]={paramsTPCdEdxcutlowAssoc[a]};
+    Float_t tpcparamhigh=paramsTPCdEdxcuthighAssoc[a];
+    pidbackground->ConfigureTPCcentralityCut(a,cutmodelAssoc,tpcparamlow,tpcparamhigh);
+  }
+  backe->GetPIDBackgroundQAManager()->SetHighResolutionHistos();
+  backe->SetHFEBackgroundCuts(hfeBackgroundCuts);
+
+  task->SetHFEBackgroundSubtraction(backe);
+
+  //***************************************//
+  //          V0 tagged tracks             //
+  //***************************************//
 
   if(kAnalyseTaggedTracks){
     AliHFEcuts *v0trackCuts = new AliHFEcuts("V0trackCuts", "Track Cuts for tagged track Analysis");
@@ -236,9 +204,8 @@ AliAnalysisTaskHFE* ConfigHFEnpepPb(Bool_t useMC, UChar_t TPCcl=70, UChar_t TPCc
   printf("task %p\n", task);
   task->SetQAOn(AliAnalysisTaskHFE::kPIDqa);
   task->SetQAOn(AliAnalysisTaskHFE::kMCqa);
-  if(kNoPhotonic) task->SwitchOnPlugin(AliAnalysisTaskHFE::kNonPhotonicElectron);
+  task->SwitchOnPlugin(AliAnalysisTaskHFE::kNonPhotonicElectron);
   task->SwitchOnPlugin(AliAnalysisTaskHFE::kDEstep);
-  if(useMC && mcstr) task->SetDebugStreaming();
 
   printf("*************************************\n");
   printf("Configuring standard Task:\n");
