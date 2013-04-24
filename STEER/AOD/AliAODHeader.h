9e1fd718013fe9f3bb3fba93b84b7aee65b296d3
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-04-24 09:49:50 +0000

    Class version increased

--- STEER/AOD/AliAODHeader.h
+++ STEER/AOD/AliAODHeader.h
@@ -250,7 +250,7 @@ class AliAODHeader : public AliVHeader {
   Float_t     fT0spread[kT0SpreadSize]; // spread of time distributions: (TOA+T0C/2), T0A, T0C, (T0A-T0C)/2
   TBits   fIRInt2InteractionsMap;  // map of the Int2 events (normally 0TVX) near the event, that's Int2Id-EventId in a -90 to 90 window
   TBits   fIRInt1InteractionsMap;  // map of the Int1 events (normally V0A&V0C) near the event, that's Int1Id-EventId in a -90 to 90 window
-  ClassDef(AliAODHeader, 21);
+  ClassDef(AliAODHeader, 22);
 };
 inline
 void AliAODHeader::SetCentrality(const AliCentrality* cent)      { 
