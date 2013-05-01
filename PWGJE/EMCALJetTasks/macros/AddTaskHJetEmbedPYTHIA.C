42727b57b8a764c93c6cfe67b30f494057cd2c9c
loizides <loizides@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-05-01 21:28:21 +0000

    Fix for Legotrain

--- PWGJE/EMCALJetTasks/macros/AddTaskHJetEmbedPYTHIA.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskHJetEmbedPYTHIA.C
@@ -2,18 +2,18 @@ AliAnalysisTaskHJetEmbed *AddTaskHJetEmbedPYTHIA(const TString period      = "lh
 						 const Bool_t runQA        = kTRUE, 
 						 const Bool_t runHJet      = kTRUE,
 						 const Bool_t runMatch     = kTRUE,
-						 const char *trackName     = "PicoTracks",
-						 const char *clusterName   = "",
-						 const char *mcName        = "MCParticlesSelected",
-						 const UInt_t jetType      = AliEmcalJetTask::kAKT | AliEmcalJetTask::kChargedJet | AliEmcalJetTask::kR040Jet,
-						 const UInt_t jetTypeBkg   = AliEmcalJetTask::kKT | AliEmcalJetTask::kChargedJet | AliEmcalJetTask::kR040Jet,
+						 const TString trackName   = "PicoTracks",
+						 const TString clusterName = "",
+						 const TString mcName      = "MCParticlesSelected",
 						 const Double_t jetRadius  = 0.4,
-						 const Int_t pSel           = AliVEvent::kAnyINT | AliVEvent::kCentral | AliVEvent::kSemiCentral, 
+						 const TString jetArrayPL  = "",
+						 const TString jetArrayDL  = "",
+						 const TString jetArray    = "",
+						 const TString rhoName     = "",
 						 const Double_t minTrkPt   = 0.15, const Double_t maxTrkPt = 1e4,
 						 const Double_t minTrkEta  = -0.9, const Double_t maxTrkEta = 0.9,
-						 const Double_t minTrkPhi  = 0,    const Double_t maxTrkPhi = 2*TMath::Pi(),
-						 const Bool_t   hybridRho  = kTRUE,
-						 TString outfileName       = "")
+						 const Double_t minTrkPhi  = 0,    const Double_t maxTrkPhi = 2*TMath::Pi()
+						 )
 {
 
   // Get the pointer to the existing analysis manager via the static access method.
@@ -32,52 +32,20 @@ AliAnalysisTaskHJetEmbed *AddTaskHJetEmbedPYTHIA(const TString period      = "lh
     ::Error("AddTaskHJetEmbedPYTHIA", "This task requires an input event handler");
     return NULL;
   }
-
-  // Find particle level jets
-  AliEmcalJetTask* jetPLFinderTask =  AddTaskEmcalJet(jetType,mcName,clusterName,minTrkPt,0.30,0.005,jetRadius,"PLJet");
-  jetPLFinderTask->SetEtaRange(minTrkEta,maxTrkEta);
-  jetPLFinderTask->SetPhiRange(minTrkPhi,maxTrkPhi);
-  jetPLFinderTask->SelectCollisionCandidates(pSel);
-
-  // Find detector level jets
-  AliEmcalJetTask* jetDLFinderTask =  AddTaskEmcalJet(jetType,trackName,clusterName,minTrkPt,0.30,0.005,jetRadius,"DLJet");
-  jetDLFinderTask->SetEtaRange(minTrkEta,maxTrkEta);
-  jetDLFinderTask->SetPhiRange(minTrkPhi,maxTrkPhi);
-  jetDLFinderTask->SelectConstituents(TObject::kBitMask, 0); // select tracks with label!=0TObject::kBitMap, 0
-  jetDLFinderTask->SelectCollisionCandidates(pSel);
-
-  // Calculate rho
-  AliEmcalJetTask* jetFinderTaskBkg = AddTaskEmcalJet(jetType, trackName, clusterName,minTrkPt,0.30,0.005,jetRadius,"BkgJet");
-  jetFinderTaskBkg->SetEtaRange(minTrkEta,maxTrkEta);
-  jetFinderTaskBkg->SetPhiRange(minTrkPhi,maxTrkPhi);
-  if(hybridRho) jetFinderTaskBkg->SelectConstituents(0,0);
-  else          jetFinderTaskBkg->SelectConstituents(0,TObject::kBitMask);
-  jetFinderTaskBkg->SelectCollisionCandidates(pSel);
-
-  if(hybridRho) char *rhoName = Form("Hybrid_Rho");
-  else          char *rhoName = Form("Rho");
-  AliAnalysisTaskRho *rhochtask = AddTaskRho(jetFinderTaskBkg->GetName(),trackName, clusterName, rhoName, jetRadius, AliAnalysisTaskEmcal::kTPC, 0.001, 0, 0, 2, kFALSE, rhoName);
-  rhochtask->SetJetEtaLimits(minTrkEta+jetRadius,maxTrkEta-jetRadius);
-  rhochtask->SetJetPhiLimits(minTrkPhi,maxTrkPhi);
-  rhochtask->SelectCollisionCandidates(pSel);
-
-  // Find embedded jets
-  AliEmcalJetTask* jetFinderTask =  AddTaskEmcalJet(jetType,trackName,clusterName,minTrkPt,0.30,0.005,jetRadius,"Jet");
-  jetFinderTask->SetEtaRange(minTrkEta,maxTrkEta);
-  jetFinderTask->SetPhiRange(minTrkPhi,maxTrkPhi);
-  jetFinderTask->SelectConstituents(0,0); 
-  jetFinderTask->SelectCollisionCandidates(pSel);
-
   
   //Analyze
   AliAnalysisTaskHJetEmbed *taskEmbed = new AliAnalysisTaskHJetEmbed("AnaEmbedPYTHIA");
   taskEmbed->SetRunPeriod(period.Data());
-  taskEmbed->SetTrackArrName(trackName);
-  taskEmbed->SetRhoName(rhoName);
-  taskEmbed->SetJetArrName(jetFinderTask->GetName());
-  taskEmbed->SetPLJetArrName(jetPLFinderTask->GetName());
-  taskEmbed->SetDLJetArrName(jetDLFinderTask->GetName());
-  taskEmbed->SetMCParticleArrName(mcName);
+  taskEmbed->SetTrkPtRange(minTrkPt, maxTrkPt);
+  taskEmbed->SetTrkPhiRange(minTrkPhi, maxTrkPhi);
+  taskEmbed->SetTrkEtaRange(minTrkEta, maxTrkEta);
+  taskEmbed->SetRadius(jetRadius);
+  taskEmbed->SetTrackArrName(trackName.Data());
+  taskEmbed->SetMCParticleArrName(mcName.Data());
+  taskEmbed->SetRhoName(rhoName.Data());
+  taskEmbed->SetJetArrName(jetArray.Data());
+  taskEmbed->SetPLJetArrName(jetArrayPL.Data());
+  taskEmbed->SetDLJetArrName(jetArrayDL.Data());
   taskEmbed->SetRunQA(runQA);
   taskEmbed->SetRunHJet(runHJet);
   taskEmbed->SetRunMatch(runMatch);
@@ -85,11 +53,11 @@ AliAnalysisTaskHJetEmbed *AddTaskHJetEmbedPYTHIA(const TString period      = "lh
     taskEmbed->SetCollisionSystem("PbPb");
   else
     taskEmbed->SetCollisionSystem("pp");
-  taskEmbed->SelectCollisionCandidates(pSel);
 
-  mgr->AddTask(taskEmbed);
-  if(outfileName.IsNull()) outfileName = AliAnalysisManager::GetCommonFileName();
-  AliAnalysisDataContainer *coutput = mgr->CreateContainer("EmbedPYTHIA",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s", outfileName.Data()));
+  if(period.Contains("lhc11h",TString::kIgnoreCase))
+    taskEmbed->SelectCollisionCandidates(AliVEvent::kAnyINT | AliVEvent::kCentral | AliVEvent::kSemiCentral);
+
+  AliAnalysisDataContainer *coutput = mgr->CreateContainer("EmbedPYTHIA",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s",  AliAnalysisManager::GetCommonFileName()));
   mgr->ConnectInput(taskEmbed,0,mgr->GetCommonInputContainer());
   mgr->ConnectOutput(taskEmbed,1,coutput);
 
