51eaf57552905b6dd3e6ae2605d3e6bfdda7b07e
lgraczyk <lgraczyk@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-05-09 14:20:37 +0000

    ProtonLambda HBT train macros small update

--- PWGCF/FEMTOSCOPY/macros/Train/ProtonLambdaFemto/fieldMinus/ConfigFemtoAnalysis.C
+++ PWGCF/FEMTOSCOPY/macros/Train/ProtonLambdaFemto/fieldMinus/ConfigFemtoAnalysis.C
@@ -164,6 +164,7 @@ AliFemtoManager* ConfigFemtoAnalysis() {
 	      anetaphitpc[aniter] = new AliFemtoVertexMultAnalysis(10, -10.0, 10.0, 4, multbins[imult], multbins[imult+1]);
 	      anetaphitpc[aniter]->SetNumEventsToMix(10);
 	      anetaphitpc[aniter]->SetMinSizePartCollection(1);
+	      anetaphitpc[aniter]->SetVerboseMode(kFALSE);
 
 	      mecetaphitpc[aniter] = new AliFemtoBasicEventCut();
 	      mecetaphitpc[aniter]->SetEventMult(0,100000);
