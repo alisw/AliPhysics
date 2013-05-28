fffcccc4abd3271ce35c4b528d7f05a1be281001
mkrzewic <mkrzewic@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-05-28 17:21:59 +0000

    from Redmer Bertens: performance updates + QC of jets

--- PWGCF/FLOW/macros/AddTaskJetFlow.C
+++ PWGCF/FLOW/macros/AddTaskJetFlow.C
@@ -38,8 +38,11 @@ void AddTaskJetFlow( TString name       = "name",
         printf(" > Setup default centrality binning with %i bins \n ", cent->GetSize());
     }
     // create the cut objects
-    AliFlowTrackCuts* CutsRP = new AliFlowTrackCuts("CutsRP");
-    CutsRP = CutsRP->GetStandardVZEROOnlyTrackCuts();
+    AliFlowTrackCuts* CutsRP_VZERO = new AliFlowTrackCuts("CutsRP_VZERO");
+    CutsRP_VZERO = CutsRP_VZERO->GetStandardVZEROOnlyTrackCuts();
+    AliFlowTrackCuts* CutsRP_TPC = new AliFlowTrackCuts("CutsRP_TPC");
+    CutsRP_TPC = CutsRP_TPC->GetStandardTPCStandaloneTrackCuts();
+    CutsRP_TPC->SetAODfilterBit(1);
     AliFlowTrackCuts* CutsNull = new AliFlowTrackCuts("CutsNull");
     CutsNull->SetParamType(AliFlowTrackCuts::kGlobal);
     CutsNull->SetEtaRange(+1, -1);
@@ -61,58 +64,55 @@ void AddTaskJetFlow( TString name       = "name",
         else printf(" > added task with name %s and jet collection %s < \n", tempName.Data(), jets.Data());
         task->SetJetCollectionName(jets.Data());
         // pass specific objects and settigns to the task
-        task->SetCutsRP(CutsRP);
+        task->SetCutsRP(CutsRP_TPC, CutsRP_VZERO);
         task->SetCutsNull(CutsNull);
         task->SetMinMaxCentrality(cent->At(i), cent->At(1+i));
         task->SetPtBump(ptbump);
         mgr->AddTask(task);
         mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
         mgr->ConnectOutput(task,1,mgr->CreateContainer(Form("%s_histograms", tempName.Data()), TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
-     
-   // connect flow anaysis task
-        AliAnalysisDataContainer *flowEvent = mgr->CreateContainer(Form("flowEvent_%s", tempName.Data()), AliFlowEventSimple::Class(), AliAnalysisManager::kExchangeContainer);
-        mgr->ConnectOutput(task, 2, flowEvent);
-        TaskJetFlow::AddSPmethod(Form("SP_A_%s", tempName.Data()), -0.8, -0, 0, +0.8, "Qa", 2, flowEvent);
-        TaskJetFlow::AddSPmethod(Form("SP_B_%s", tempName.Data()), -0.8, -0, 0, +0.8, "Qb", 2, flowEvent);
+        // connect flow anaysis task
+        AliAnalysisDataContainer *flowEvent_VZERO = mgr->CreateContainer(Form("flowEvent_VZERO_%s", tempName.Data()), AliFlowEventSimple::Class(), AliAnalysisManager::kExchangeContainer);
+        mgr->ConnectOutput(task, 2, flowEvent_VZERO);
+        AliAnalysisDataContainer *flowEvent_TPC = mgr->CreateContainer(Form("flowEvent_TPC_%s", tempName.Data()), AliFlowEventSimple::Class(), AliAnalysisManager::kExchangeContainer);
+        mgr->ConnectOutput(task, 3, flowEvent_TPC);
+        TaskJetFlow::AddSPmethod(Form("SPVZERO_A_%s", tempName.Data()), "Qa", 2, flowEvent_VZERO);
+        TaskJetFlow::AddSPmethod(Form("SPVZERO_B_%s", tempName.Data()), "Qb", 2, flowEvent_VZERO);
+        TaskJetFlow::AddQCmethod(Form("QC_%s", tempName.Data()), 2, flowEvent_TPC);
     }
 }
 //_____________________________________________________________________________
 namespace TaskJetFlow{ // use unique namespace to avoid problems in TRAIN analysis
-    void AddSPmethod(char *name, double minEtaA, double maxEtaA, double minEtaB, double maxEtaB, char *Qvector, int harmonic, AliAnalysisDataContainer *flowEvent)
+    void AddSPmethod(char *name, char *Qvector, int harmonic, AliAnalysisDataContainer *flowEvent)
     {
-       // add sp task and filter tasks
-       AliFlowTrackSimpleCuts* a = new AliFlowTrackSimpleCuts("a");
-       a->SetEtaMin(minEtaA);
-       a->SetEtaMax(maxEtaA);
-       a->SetMassMin(-999);
-       a->SetMassMax(999);
-       AliFlowTrackSimpleCuts* b = new AliFlowTrackSimpleCuts("b");
-       b->SetEtaMin(minEtaB);
-       b->SetEtaMax(maxEtaB);
-       b->SetMassMin(-999);
-       b->SetMassMax(999);
-       TString fileName = AliAnalysisManager::GetCommonFileName();
-       fileName+=":SP";
-       fileName+="name";
-       AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-       AliAnalysisDataContainer *filteredFlowEvent = mgr->CreateContainer(Form("Filter_%s", name), AliFlowEventSimple::Class(), AliAnalysisManager::kExchangeContainer);
-       AliAnalysisTaskFilterFE *tskFilter(0x0);
-       if(!strcmp("Qa", Qvector)) tskFilter = new AliAnalysisTaskFilterFE(Form("TaskFilter_%s", name), 0x0, a);
-       if(!strcmp("Qb", Qvector)) tskFilter = new AliAnalysisTaskFilterFE(Form("TaskFilter_%s", name), 0x0, b);
-       if(tskFilter) tskFilter->SetSubeventEtaRange(-10, 0, 0, 10);
-       else return;
-       mgr->AddTask(tskFilter);
-       printf( " > Added FILTER TASK < \n ");
-       mgr->ConnectInput(tskFilter, 0, flowEvent);
-       mgr->ConnectOutput(tskFilter, 1, filteredFlowEvent);
-       AliAnalysisDataContainer *outSP = mgr->CreateContainer(name, TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
-       AliAnalysisTaskScalarProduct *tskSP = new AliAnalysisTaskScalarProduct(Form("TaskScalarProduct_%s", name), kFALSE);
-       tskSP->SetApplyCorrectionForNUA(kTRUE);
-       tskSP->SetHarmonic(harmonic);
-       tskSP->SetTotalQvector(Qvector);
-       tskSP->SetBookOnlyBasicCCH(kFALSE);
-       mgr->AddTask(tskSP);
-       mgr->ConnectInput(tskSP, 0, filteredFlowEvent);
-       mgr->ConnectOutput(tskSP, 1, outSP);
+        TString fileName = AliAnalysisManager::GetCommonFileName();
+        fileName+=":SP";
+        AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+        AliAnalysisDataContainer *outSP = mgr->CreateContainer(name, TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
+        AliAnalysisTaskScalarProduct *tskSP = new AliAnalysisTaskScalarProduct(Form("TaskScalarProduct_%s", name), kFALSE);
+        tskSP->SetApplyCorrectionForNUA(kTRUE);
+        tskSP->SetHarmonic(harmonic);
+        if(!strcmp("Qa", Qvector)) tskSP->SetTotalQvector("Qa");
+        if(!strcmp("Qb", Qvector)) tskSP->SetTotalQvector("Qb");
+        tskSP->SetBookOnlyBasicCCH(kFALSE);
+        mgr->AddTask(tskSP);
+        mgr->ConnectInput(tskSP, 0, flowEvent);
+        mgr->ConnectOutput(tskSP, 1, outSP);
     }
+//_____________________________________________________________________________
+    void AddQCmethod(char *name, int harmonic, AliAnalysisDataContainer *flowEvent)
+    {
+       TString fileName = AliAnalysisManager::GetCommonFileName();
+       fileName+=":QC";
+       AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+       AliAnalysisDataContainer *outQC = mgr->CreateContainer(name, TList::Class(), AliAnalysisManager::kOutputContainer, fileName);
+       AliAnalysisTaskQCumulants *tskQC = new AliAnalysisTaskQCumulants(Form("TaskQCumulants_%s", name), kFALSE);
+       tskQC->SetApplyCorrectionForNUA(kTRUE);
+       tskQC->SetHarmonic(harmonic);
+       tskQC->SetBookOnlyBasicCCH(kFALSE);
+       mgr->AddTask(tskQC);
+       mgr->ConnectInput(tskQC, 0, flowEvent);
+       mgr->ConnectOutput(tskQC, 1, outQC);
+    }
+//_____________________________________________________________________________
 }// end of namespace TaskJetFlow
