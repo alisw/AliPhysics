2a9da874a0755d13b8814b24f0b005a57ce23c07
morsch <morsch@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-06-05 15:41:10 +0000

    Updates manso <manso@clermont.in2p3.fr>

--- STRUCT/AliPIPEv4.h
+++ STRUCT/AliPIPEv4.h
@@ -20,13 +20,10 @@ class TGeoVolume;
 class AliPIPEv4 : public AliPIPE {
     
  public:
-    enum constants {kC=6, kAlu=9, kInox=19, kGetter=20, kBe=5, kVac=16,
-	  kAir=15, kAlBe=21, kPA = 22};
+    enum constants {kC=6, kAlu=9, kInox=19, kGetter=20, kBe=5, kVac=16, kAir=15, kAlBe=21, kPA = 22};
 	
   AliPIPEv4();
   AliPIPEv4(const char *name, const char *title);
-  AliPIPEv4(const char *name, const char *title, const Float_t rmax, const Float_t epaisseur);
-  AliPIPEv4(const char *name, const char *title, const Float_t rmax, const Float_t epaisseur, const Float_t z2);
   virtual       ~AliPIPEv4() {};
   virtual void   CreateGeometry();
   virtual void   CreateMaterials();
@@ -38,9 +35,8 @@ class AliPIPEv4 : public AliPIPE {
 
   Float_t   fRmax;       // outer radius of Be beam pipe
   Float_t   fBe;         // width of Be beam pipe
-  Float_t   fZ1;         // beginning of beam pipe z location (A side)
+  Float_t   fZ1;         // beginning of Be beam pipe z location (A side)
   Float_t   fZ2;         // end of Be beam pipe z location (C side)
-  Float_t   fZ3;         // end of Stell beam pipe z location (C side)
  protected:
   ClassDef(AliPIPEv4,1)  //Class for PIPE version using TGeo
 };
