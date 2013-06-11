f8cecce524f41710a4188777177db9a5c04bc596
cholm <cholm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-06-11 11:59:22 +0000

    Always recompile sub-classes - will fail otherwise, because an attempt to load the the previously loaded train class will made

--- PWGLF/FORWARD/trains/Helper.C
+++ PWGLF/FORWARD/trains/Helper.C
@@ -326,7 +326,7 @@ struct Helper
       gSystem->Load("libProofPlayer");
     }
     // (Always) recompile and with debug symbols 
-    gROOT->LoadMacro(Form("%s.C+g",cl.Data()));
+    gROOT->LoadMacro(Form("%s.C++g",cl.Data()));
     Long_t ptr = gROOT->ProcessLine(Form("new %s(\"%s\", %d);", 
 					 cl.Data(), url.GetUrl(), verbose));
     if (verbose < 3) gSystem->RedirectOutput(0);
