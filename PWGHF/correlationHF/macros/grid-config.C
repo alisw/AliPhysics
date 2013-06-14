d007d3b3334c44cd361d16587da607d2c1727436
ploenne <per-ivar.loenne@cern.ch>
2013-06-14 10:53:58 +0200

    Grid run 2013_06_13, pPb, LHC13b - LHC13c. Electron Selection, checking ITS cuts

---
+++ PWGHF/correlationHF/macros/grid-config.C
@@ -0,0 +1,6 @@
+const char* alienAPIVersion="V1.1x";
+const char* alienROOTVersion="v5-34-05";
+const char* alienAliROOTVersion="v5-04-63-AN";
+const char* defaultGridDataDir="/alice/data/2013/LHC13c";
+const char* defaultDataPattern="*/pass1/AOD126/*AliAOD.root";
+const char* defaultFriendDataPattern="./AliAOD.VertexingHF.root";
