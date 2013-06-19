3c24ba030881b9c3669f0c035cae4309905f2457
mbombara <mbombara@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-06-19 11:20:48 +0000

    Task for updates to Analysis Note

--- PWGLF/STRANGENESS/Correlations/macros/AddTaskV0ChCorrelations.C
+++ PWGLF/STRANGENESS/Correlations/macros/AddTaskV0ChCorrelations.C
@@ -1,8 +1,8 @@
-// AddTaskV0ChCorrelations.C
+// runV0ChCorrelations.C
 //
 // AddTask for AliAnalysisTaskV0ChCorrelations task
 //
-AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const Bool_t bMCtruth=kTRUE,
+AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const Bool_t bMCtruth=kFALSE,
 														 Float_t DcaDToPV = 0.1,
 														 Float_t DcaV0D = 1.0
 														)
@@ -18,9 +18,16 @@ AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const Bool_t bMCtruth=k
       return NULL;
     }
 
+    // mc event handlerrunEx01.C
+    if(bMCtruth) {
+        AliMCEventHandler* mchandler = new AliMCEventHandler();
+        // Not reading track references
+        mchandler->SetReadTR(kFALSE);
+        mgr->SetMCtruthEventHandler(mchandler);
+    }   
+
 	// create task
     AliAnalysisTaskV0ChCorrelations* task = new AliAnalysisTaskV0ChCorrelations("V0ChCorrelations_task");
-    task->SetAnalysisMC(bMCtruth);
 	task->SetDcaDToPV(DcaDToPV);
 	task->SetDcaV0D(DcaV0D);
     mgr->AddTask(task);
@@ -29,6 +36,7 @@ AliAnalysisTaskV0ChCorrelations *AddTaskV0ChCorrelations(const Bool_t bMCtruth=k
     // Get and connect other common input/output containers via the manager as below
     //==============================================================================
     TString outputFileName = AliAnalysisManager::GetCommonFileName();
+   // outputFileName = "list.grid.v0ch.root";
 
     // create containers for input/output
     AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
