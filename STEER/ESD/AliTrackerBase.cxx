0d5af5e485b788aed3a07c1cba9c3230c6f16f2d
wiechula <wiechula@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-06-21 10:27:50 +0000

    o add Propagate function with return value for debugging

--- STEER/ESD/AliTrackerBase.cxx
+++ STEER/ESD/AliTrackerBase.cxx
@@ -330,6 +330,78 @@ AliTrackerBase::PropagateTrackTo(AliExternalTrackParam *track, Double_t xToGo,
   return kTRUE;
 }
 
+Int_t AliTrackerBase::PropagateTrackTo2(AliExternalTrackParam *track, Double_t xToGo,
+                                        Double_t mass, Double_t maxStep, Bool_t rotateTo, Double_t maxSnp, Int_t sign, Bool_t addTimeStep, Bool_t correctMaterialBudget){
+  //----------------------------------------------------------------
+  //
+  // Propagates the track to the plane X=xk (cm) using the magnetic field map
+  // and correcting for the crossed material.
+  //
+  // mass     - mass used in propagation - used for energy loss correction
+  // maxStep  - maximal step for propagation
+  //
+  //  Origin: Marian Ivanov,  Marian.Ivanov@cern.ch
+  //
+  //----------------------------------------------------------------
+  const Double_t kEpsilon = 0.00001;
+  Double_t xpos     = track->GetX();
+  Int_t dir         = (xpos<xToGo) ? 1:-1;
+  //
+  while ( (xToGo-xpos)*dir > kEpsilon){
+    Double_t step = dir*TMath::Min(TMath::Abs(xToGo-xpos), maxStep);
+    Double_t x    = xpos+step;
+    Double_t xyz0[3],xyz1[3],param[7];
+    track->GetXYZ(xyz0);   //starting global position
+    
+    Double_t bz=GetBz(xyz0); // getting the local Bz
+    if (!track->GetXYZAt(x,bz,xyz1)) return -1;   // no prolongation
+    xyz1[2]+=kEpsilon; // waiting for bug correction in geo
+    
+    if (maxSnp>0 && TMath::Abs(track->GetSnpAt(x,bz)) >= maxSnp) return -2;
+    if (!track->PropagateTo(x,bz))  return -3;
+    
+    if (correctMaterialBudget){
+      MeanMaterialBudget(xyz0,xyz1,param);
+      Double_t xrho=param[0]*param[4], xx0=param[1];
+      if (sign) {if (sign<0) xrho = -xrho;}  // sign is imposed
+      else { // determine automatically the sign from direction
+        if (dir>0) xrho = -xrho; // outward should be negative
+      }
+      //
+      if (!track->CorrectForMeanMaterial(xx0,xrho,mass)) return -4;
+    }
+    
+    if (rotateTo){
+      track->GetXYZ(xyz1);   // global position
+      Double_t alphan = TMath::ATan2(xyz1[1], xyz1[0]);
+      if (maxSnp>0) {
+        if (TMath::Abs(track->GetSnp()) >= maxSnp) return -5;
+        
+        //
+        Double_t ca=TMath::Cos(alphan-track->GetAlpha()), sa=TMath::Sin(alphan-track->GetAlpha());
+        Double_t sf=track->GetSnp(), cf=TMath::Sqrt((1.-sf)*(1.+sf));
+        Double_t sinNew =  sf*ca - cf*sa;
+        if (TMath::Abs(sinNew) >= maxSnp) return -6;
+        
+      }
+      if (!track->AliExternalTrackParam::Rotate(alphan)) return -7;
+      
+    }
+    xpos = track->GetX();
+    if (addTimeStep && track->IsStartedTimeIntegral()) {
+      if (!rotateTo) track->GetXYZ(xyz1); // if rotateTo==kTRUE, then xyz1 is already extracted
+      Double_t dX=xyz0[0]-xyz1[0],dY=xyz0[1]-xyz1[1],dZ=xyz0[2]-xyz1[2];
+      Double_t d=TMath::Sqrt(dX*dX + dY*dY + dZ*dZ);
+      if (sign) {if (sign>0) d = -d;}  // step sign is imposed, positive means inward direction
+      else { // determine automatically the sign from direction
+        if (dir<0) d = -d;
+      }
+      track->AddTimeStep(d);
+    }
+  }
+  return 1;
+}
+
 Bool_t 
 AliTrackerBase::PropagateTrackToBxByBz(AliExternalTrackParam *track,
 				       Double_t xToGo,Double_t mass, Double_t maxStep, Bool_t rotateTo, Double_t maxSnp,Int_t sign, Bool_t addTimeStep){
