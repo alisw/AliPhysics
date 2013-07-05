a44b65a8f9c8901e22c5b820e67543373563185c
quark <quark@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-07-05 21:27:44 +0000

    Fix enums as reported by coverity

--- EVE/EveBase/AliEveMainWindow.h
+++ EVE/EveBase/AliEveMainWindow.h
@@ -19,67 +19,6 @@ class TGToolBar;
 class AliEveMainWindow;
 class AliEveFileDialog;
 
-// Items IDs for MenuBar
-enum AliEveMainWindow::MENU_FILE
-{
-    MENU_FILE_OPEN,
-    MENU_FILE_OPEN_URL,
-    MENU_FILE_OPEN_CONNECTION,
-    MENU_FILE_EXPORT_VIEWS,
-    MENU_FILE_EXIT
-};
-
-enum AliEveMainWindow::MENU_EDIT
-{
-    MENU_EDIT_UNDO,
-    MENU_EDIT_REDO,
-    MENU_EDIT_CUT,
-    MENU_EDIT_COPY,
-    MENU_EDIT_PASTE,
-    MENU_EDIT_DELETE,
-    MENU_EDIT_PROP
-};
-
-enum AliEveMainWindow::MENU_VIEW
-{
-    MENU_VIEW_TOOLBAR_MAIN,
-    MENU_VIEW_TOOLBAR_NAV, // event navigation toolbar
-    MENU_VIEW_TOOLBAR_PROP, // Properties Sidebar - event info, objects list, etc...
-    MENU_VIEW_TOOLBAR_HIST, // History sidebar
-    MENU_VIEW_RELOAD,
-    MENU_VIEW_ZOOM_IN,
-    MENU_VIEW_ZOOM_OUT,
-    MENU_VIEW_ZOOM_RESET
-};
-
-enum AliEveMainWindow::MENU_GO
-{
-    MENU_GO_NEXT_EVENT,
-    MENU_GO_PREV_EVENT,
-    MENU_GO_FIRST_EVENT,
-    MENU_GO_LAST_EVENT,
-    MENU_GO_PLAY
-};
-
-enum AliEveMainWindow::MENU_HIST
-{
-    MENU_HIST_SHOW_ALL,
-    MENU_HIST_CLEAR_RECENT
-};
-
-enum AliEveMainWindow::MENU_TOOLS
-{
-    MENU_TOOLS_MACROS,
-    MENU_TOOLS_QA
-};
-
-enum AliEveMainWindow::MENU_HELP
-{
-    MENU_HELP_CONTENTS,
-    MENU_HELP_ABOUT
-};
-
-//______________________________________________________________________________
 // AliEveMainWindow
 //
 
@@ -88,6 +27,47 @@ class AliEveMainWindow : public TGMainFrame
 public:
     AliEveMainWindow(const char* title, UInt_t width=800, UInt_t height=600);
     ~AliEveMainWindow();
+// Items IDs for MenuBar
+enum MENU_ITEM_IDS
+{   // File Menu
+    MENU_FILE_OPEN=0,
+    MENU_FILE_OPEN_URL,
+    MENU_FILE_OPEN_CONNECTION,
+    MENU_FILE_EXPORT_VIEWS,
+    MENU_FILE_EXIT,
+    // Edit Menu
+    MENU_EDIT_UNDO,
+    MENU_EDIT_REDO,
+    MENU_EDIT_CUT,
+    MENU_EDIT_COPY,
+    MENU_EDIT_PASTE,
+    MENU_EDIT_DELETE,
+    MENU_EDIT_PROP,
+    // View Menu
+    MENU_VIEW_TOOLBAR_MAIN,
+    MENU_VIEW_TOOLBAR_NAV, // event navigation toolbar
+    MENU_VIEW_TOOLBAR_PROP, // Properties Sidebar - event info, objects list, etc...
+    MENU_VIEW_TOOLBAR_HIST, // History sidebar
+    MENU_VIEW_RELOAD,
+    MENU_VIEW_ZOOM_IN,
+    MENU_VIEW_ZOOM_OUT,
+    MENU_VIEW_ZOOM_RESET,
+    // Go Menu
+    MENU_GO_NEXT_EVENT,
+    MENU_GO_PREV_EVENT,
+    MENU_GO_FIRST_EVENT,
+    MENU_GO_LAST_EVENT,
+    MENU_GO_PLAY,
+    // History Menu
+    MENU_HIST_SHOW_ALL,
+    MENU_HIST_CLEAR_RECENT,
+    // Tools Menu
+    MENU_TOOLS_MACROS,
+    MENU_TOOLS_QA,
+    // Help Menu
+    MENU_HELP_CONTENTS,
+    MENU_HELP_ABOUT
+};
 
 public: // SLOTS
     void onMenuFileItem(UInt_t id);
