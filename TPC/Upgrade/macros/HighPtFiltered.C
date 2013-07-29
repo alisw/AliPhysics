2d430a64ca78b44fd916f7bd160f73ea96f341b2
mivanov <mivanov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-07-29 14:59:20 +0000

    ITS refit efficiency added

--- TPC/Upgrade/macros/HighPtFiltered.C
+++ TPC/Upgrade/macros/HighPtFiltered.C
@@ -11,11 +11,10 @@ void DrawPTResol(){
   TH2 *phis1PtPt[3]={0};
   TH1 *his1PtPtRes[3]={0};
   TObjArray  * fitArray = new TObjArray(3);
-
-
+  //
+  //
   treePt->Draw("abs(esdTrack.fP[4])-1/particle.Pt():1/particle.Pt()>>his1Pt1Pt(20,0,0.5,100,-0.01,0.01)",cutNcl,"colz");
   phis1PtPt[0] = (TH2*)treePt->GetHistogram()->Clone();
-  
   phis1PtPt[0]->FitSlicesY(0,0,-1,0,"QNR",fitArray);
   his1PtPtRes[0]=(TH1*)fitArray->At(2)->Clone();
   //
@@ -23,8 +22,19 @@ void DrawPTResol(){
 
   his1PtPtRes[0]->Draw();
 
-  
-    
+}
 
+
+void DrawMatchingEffiency(){
+  //
+  //
+  //
+  TFile * f = TFile::Open("Filtered.root");
+  TTree * treePt= (TTree*)f->Get("highPt");
+  treePt->SetAlias("ITSrefit","(esdTrack.fFlags&0x4)!=0");
+  //
+  TCut cutNcl = "esdTrack.GetTPCClusterInfo(3,1)>120"; 
+  treePt->Draw("ITSrefit:1/particle.Pt()>>hisMatching(20,0,0.5)",cutNcl,"prof");
+  
 
 }
