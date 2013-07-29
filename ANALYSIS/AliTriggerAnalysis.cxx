b22fe8c1ce557945ca6e49ad4b39625435568af5
akalweit <akalweit@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-07-29 10:37:20 +0000

    Fixing histogram labels.

--- ANALYSIS/AliTriggerAnalysis.cxx
+++ ANALYSIS/AliTriggerAnalysis.cxx
@@ -221,7 +221,7 @@ void AliTriggerAnalysis::EnableHistograms(Bool_t isLowFlux)
   Int_t nBinsY = isLowFlux ? 500  : 1000;
   Float_t xMax = isLowFlux ? 400  : 2999.5;
   Float_t yMax = isLowFlux ? 4000 : 9999.5;
-  fHistSPDClsVsTrk = new TH2F("fHistSPDClsVsTrk", "SPD Clusters vs Tracklets", nBinsX, -0.5, xMax, nBinsY, -0.5, yMax);
+  fHistSPDClsVsTrk = new TH2F("fHistSPDClsVsTrk", "SPD Clusters vs Tracklets; n tracklets; n clusters", nBinsX, -0.5, xMax, nBinsY, -0.5, yMax);
   //
   fHistV0A = new TH1F("fHistV0A", "V0A;leading time (ns);events", 400, -100, 100);
   fHistV0C = new TH1F("fHistV0C", "V0C;leading time (ns);events", 400, -100, 100);
