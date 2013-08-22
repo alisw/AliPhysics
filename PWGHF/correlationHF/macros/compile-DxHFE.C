c70aa8b519f3f2c03350da229f47022fce575844
herdal <hege.erdal@cern.ch>
2013-08-22 10:35:09 +0200

    can compile also in current directory

--- PWGHF/correlationHF/macros/compile-DxHFE.C
+++ PWGHF/correlationHF/macros/compile-DxHFE.C
@@ -12,39 +12,47 @@
 ///
 /// Usage: aliroot -l $ALICE_ROOT/PWGHF/correlationHF/compile-DxHFE.C
 
+void compile_DxHFE(Bool_t bUseLocalDir=kFALSE){
 #if defined(__CINT__) && !defined(__MAKECINT__)
-{
+  {
 
-  //----------- Loading the required libraries ---------//
-  gInterpreter->ExecuteMacro("$ALICE_ROOT/PWGHF/vertexingHF/macros/LoadLibraries.C");
+    //----------- Loading the required libraries ---------//
+    gInterpreter->ExecuteMacro("$ALICE_ROOT/PWGHF/vertexingHF/macros/LoadLibraries.C");
 
-  gSystem->Load("libSTEERBase");
-  gSystem->Load("libESD");
-  gSystem->Load("libAOD");
-  gSystem->Load("libANALYSIS");
-  gSystem->Load("libANALYSISalice");
-  gSystem->Load("libPWGHFhfe");
-  gSystem->Load("libCORRFW");
-  gSystem->AddIncludePath("-I$ROOTSYS/include -I$ALICE_ROOT/include -I$ALICE_ROOT/PWGHF/vertexingHF -I$ALICE_ROOT/PWGHF/base -I$ALICE_ROOT/PWGHF/hfe ");
+    gSystem->Load("libSTEERBase");
+    gSystem->Load("libESD");
+    gSystem->Load("libAOD");
+    gSystem->Load("libANALYSIS");
+    gSystem->Load("libANALYSISalice");
+    gSystem->Load("libPWGHFhfe.so");
+    gSystem->Load("libCORRFW");
+    gSystem->AddIncludePath("-I$ROOTSYS/include -I$ALICE_ROOT/include -I$ALICE_ROOT/PWGHF/vertexingHF -I$ALICE_ROOT/PWGHF/base -I$ALICE_ROOT/PWGHF/hfe ");
 
-  TString dir("$ALICE_ROOT/PWGHF/correlationHF/");
-  gROOT->LoadMacro(dir+"AliHFAssociatedTrackCuts.cxx+");
-  gROOT->LoadMacro(dir+"AliReducedParticle.cxx+");
-  gROOT->LoadMacro(dir+"AliHFCorrelator.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFEParticleSelection.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionD0.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionEl.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFEToolsMC.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionMCD0.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionMCEl.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFECorrelation.cxx+");
-  gROOT->LoadMacro(dir+"AliDxHFECorrelationMC.cxx+");
-  gROOT->LoadMacro(dir+"AliAnalysisTaskDxHFEParticleSelection.cxx+");
-  gROOT->LoadMacro(dir+"AliAnalysisTaskDxHFECorrelation.cxx+");
+    if(bUseLocalDir)
+      TString dir("./");
+    else
+      TString dir("$ALICE_ROOT/PWGHF/correlationHF/");
 
-}
+    gROOT->LoadMacro(dir+"AliSingleTrackEffCuts.cxx+");
+    gROOT->LoadMacro(dir+"AliCFSingleTrackEfficiencyTask.cxx+");
+    gROOT->LoadMacro(dir+"AliHFAssociatedTrackCuts.cxx+");
+    gROOT->LoadMacro(dir+"AliReducedParticle.cxx+");
+    gROOT->LoadMacro(dir+"AliHFCorrelator.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFEParticleSelection.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionD0.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionEl.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFEToolsMC.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionMCD0.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFEParticleSelectionMCEl.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFECorrelation.cxx+");
+    gROOT->LoadMacro(dir+"AliDxHFECorrelationMC.cxx+");
+    gROOT->LoadMacro(dir+"AliAnalysisTaskDxHFEParticleSelection.cxx+");
+    gROOT->LoadMacro(dir+"AliAnalysisTaskDxHFECorrelation.cxx+");
+
+  }
 #elif
-{
-  cerr << "this macro can not be compiled, remove option '+'" << endl;
-}
+  {
+    cerr << "this macro can not be compiled, remove option '+'" << endl;
+  }
 #endif
+}
