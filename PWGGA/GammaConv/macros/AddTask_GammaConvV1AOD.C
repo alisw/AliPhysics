44649c273f381df1d267d536ca0a6d50a98bb532
fbock <fbock@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-08-26 13:58:51 +0000

    removed outdated macros & added macros for running with alienplugin

--- PWGGA/GammaConv/macros/AddTask_GammaConvV1AOD.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvV1AOD.C
@@ -1,5 +1,5 @@
 
-void AddTask_GammaConvV1(TString mode){
+void AddTask_GammaConvV1AOD(TString mode){
 
    gSystem->Load("libCore.so");  
    gSystem->Load("libTree.so");
@@ -28,22 +28,22 @@ void AddTask_GammaConvV1(TString mode){
    Bool_t isMC	= kTRUE;
    Bool_t readTR = kTRUE;
 
-   TString Pattern="*/AliESDs.root";
-   //TString Pattern="*/AliAOD.root";
+//   TString Pattern="*/AliESDs.root";
+   TString Pattern="AOD/*/AliAOD.root";
    TString Prefix ="";
 
 
 
-   
-//   Int_t run_numbers[100] = {137161, 137162, 137165, 137230, 137231, 137232, 137235, 137236, 137243, 137366, 137430, 137431, 137432, 137434, 137439, 137440, 137441, 137443, 137530, 137531, 137539, 137541, 137544, 137546, 137549, 137595, 137608, 137638, 137639, 137685, 137686, 137691, 137692, 137693, 137704, 137718, 137722, 137724, 137751, 137752, 137844, 137848, 138190, 138192, 138197, 138201, 138225, 138275, 138364, 138396, 138438, 138439, 138442, 138469, 138534, 138578, 138579, 138582, 138583, 138621, 138624, 138638, 138652, 138653, 138662, 138666, 138730, 138732, 138837, 138870, 138871, 138872, 139028, 139029, 139036, 139037, 139038, 139104, 139105, 139107, 139173, 139308, 139309, 139310, 139311, 139314, 139328, 139329, 139360, 139437, 139438, 139439, 139465, 139503, 139504, 139505, 139507, 139510};
-   
-   Int_t run_numbers[10] = {137161, 137162, 137165, 137230, 137231, 137232, 137235, 137236, 137243, 137366};
+     Int_t run_numbers[100] = {137161, 137162, 137165, 137230, 137231, 137232, 137235, 137236, 137243, 137366, 137430, 137431, 137432, 137434, 137439, 137440, 137441, 137443, 137530, 137531, 137539, 137541, 137544, 137546, 137549, 137595, 137608, 137638, 137639, 137685, 137686, 137691, 137692, 137693, 137704, 137718, 137722, 137724, 137751, 137752, 137844, 137848, 138190, 138192, 138197, 138201, 138225, 138275, 138364, 138396, 138438, 138439, 138442, 138469, 138534, 138578, 138579, 138582, 138583, 138621,  138638, 138652, 138653, 138662, 138666, 138730, 138732, 138837, 138870, 138871, 138872, 139028, 139029, 139036, 139037, 139038, 139104, 139105, 139107, 139173, 139308, 139309, 139310, 139311, 139314, 139328, 139329, 139360, 139437, 139438, 139439, 139465, 139503, 139504, 139505, 139507, 139510};
+  // run has no AODs's: 138624 (70440 events), 
+  //Int_t  run_numbers[100] = {138638, 138652, 138653, 138662, 138666, 138730, 138732, 138837, 138870, 138871, 138872, 139028, 139029, 139036, 139037, 139038, 139104, 139105, 139107, 139173, 139308, 139309, 139310, 139311, 139314, 139328, 139329, 139360, 139437, 139438, 139439, 139465, 139503, 139504, 139505, 139507, 139510};
+   //Int_t run_numbers[10] = {137161, 137162, 137165, 137230, 137231, 137232, 137235, 137236, 137243, 137366};
    //Int_t run_numbers[10] = {137430, 137431, 137432, 137434, 137439, 137440, 137441, 137443, 137530, 137531};
    //Int_t run_numbers[10] = {137539, 137541, 137544, 137546, 137549, 137595, 137608, 137638, 137639, 137685};
    //Int_t run_numbers[10] = {137686, 137691, 137692, 137693, 137704, 137718, 137722, 137724, 137751, 137752};
    //Int_t run_numbers[10] = {137844, 137848, 138190, 138192, 138197, 138201, 138225, 138275, 138364, 138396};
    //Int_t run_numbers[10] = {138438, 138439, 138442, 138469, 138534, 138578, 138579, 138582, 138583, 138621};
-   //Int_t run_numbers[10] = {138624, 138638, 138652, 138653, 138662, 138666, 138730, 138732, 138837, 138870};
+   //Int_t run_numbers[10] = {138638, 138652, 138653, 138662, 138666, 138730, 138732, 138837, 138870};
    //Int_t run_numbers[10] = {138871, 138872, 139028, 139029, 139036, 139037, 139038, 139104, 139105, 139107};
    //Int_t run_numbers[10] = {139173, 139308, 139309, 139310, 139311, 139314, 139328, 139329, 139360, 139437};
    //Int_t run_numbers[10] = {139438, 139439, 139465, 139503, 139504, 139505, 139507, 139510};
@@ -55,7 +55,7 @@ void AddTask_GammaConvV1(TString mode){
       return 0;
    }
 
-   gROOT->LoadMacro("CreateAlienHandlerNonPT.C");
+   gROOT->LoadMacro("CreateAlienHandlerAOD.C");
    AliAnalysisAlien *alienHandler = CreateAlienHandler(mode);
    if (!alienHandler) return;
 
@@ -70,38 +70,21 @@ void AddTask_GammaConvV1(TString mode){
    alienHandler->SetDataPattern(Pattern.Data());
    alienHandler->SetRunPrefix(Prefix.Data());
    //alienHandler->SetRunRange(137161,139510);
-   for (Int_t i=0; i<10; i++) {
+   for (Int_t i=0; i<100; i++) {
       if (run_numbers[i]==0) break;
       alienHandler->AddRunNumber(run_numbers[i]);
    }
       
-   AliESDInputHandler * handler;
-   handler = new AliESDInputHandler(); mgr->SetInputEventHandler((AliESDInputHandler*)handler);
+   AliAODInputHandler * handler;
+   handler = new AliAODInputHandler(); mgr->SetInputEventHandler((AliAODInputHandler*)handler);
 
-   AliMCEventHandler* mcHandler;
-   if(isMC){
-      mcHandler = new AliMCEventHandler();
-      mgr->SetMCtruthEventHandler(mcHandler);
-      mcHandler->SetReadTR(readTR);
-   }
 
    //========= Add PID Reponse to ANALYSIS manager ====
    if(!(AliPIDResponse*)mgr->GetTask("PIDResponseTask")){
       gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
       AddTaskPIDResponse(isMC);
    }//========= Add PID Reponse to ANALYSIS manager ====
-   
-   gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPhysicsSelection.C");
-   AliPhysicsSelectionTask* physSelTask = AddTaskPhysicsSelection(isMC);
-
-   AliCentralitySelectionTask *taskCentrality;
-   gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskCentrality.C");
-   taskCentrality = AddTaskCentrality();
-   if (isMC){
-      taskCentrality->SetMCInput();
-      Info("AddTask_tender_CentralitySelection", "This task has MC.");
-   }
-   
+      
    gROOT->LoadMacro("$ALICE_ROOT/PWGGA/GammaConv/macros/AddTask_GammaConvV1_PbPb.C");
    
    cout<<"Connecting to Alien..."<<endl;
