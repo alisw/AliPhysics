2f1fda1dbe8f71c9c2f36186459c09e1f0767a07
ploenne <per-ivar.loenne@cern.ch>
2013-08-29 11:36:20 +0200

    Divided help-line in AddTaskDxHFECorrelation into smaller fragments, error about the line being too long

--- PWGHF/correlationHF/macros/AddTaskDxHFECorrelation.C
+++ PWGHF/correlationHF/macros/AddTaskDxHFECorrelation.C
@@ -24,6 +24,7 @@
 #include "TROOT.h"
 #include "AliRDHFCutsD0toKpi.h"
 #include "AliHFEextraCuts.h"
+#include "AliVEvent.h"
 using namespace std;
 #endif
 
@@ -48,27 +49,41 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
 	 << "file=                         - Filename to store output in\n"
 	 << "name=                         - Name of analysis, will correspond to directory inside the file \n"
 	 << "cutname=                      - Filename where information on event pool for event-mixing is stored (if use external file)\n"
+	 << "extraname=                    - extraname for directory and list if you run several tasks at once\n"
 	 << "runD0MassReference            - If you also want to include D2H task for D0selection (for comparison purposes)\n"
 	 << "mc                            - Run on MC\n"
 	 << "usekine                       - To run on kinematical level \n"
+	 << "triggermask=                  - Which triggers to use\n"
 	 << "event-mixing/mixing           - Whether to also run event-mixing (NB! Use AddTaskDxHFECorrelationME.C for eventmixing)\n"
 	 << "useTrackEff                   - If you want to use tracking efficiency (need to attach efficiency maps\n"
-	 << "TrackEffName=                 - The file where the efficiency map is stored\n"
+	 << "TrackEffName=                 - The file where the efficiency map is stored\n";
+    cout << "useD0Eff                      - If you want to use tracking efficiency (need to attach efficiency maps\n"
+	 << "D0EffName=                    - The file where the efficiency map for D0 is stored\n"
 	 << "trigger=D/D0/electron         - Which particle to trigger on \n"
-	 << "\nD0 settings: \n"
+	 << "PbPb/Pb-Pb/system=1           - To use Pb-Pb collision system\n"
+	 << "pPb/p-Pb/system=2/system=p-Pb - To use p-Pb collision system\n"
+	 << "\nD0 SETTINGS: \n"
 	 << "fillD0scheme=both/D0/D0bar    - Which fillsheme to use for D0\n"
-	 << "\nelectron settings: \n"
+	 << "\nELECTRON SETTINGS: \n"
 	 << "useinvmasscut                 - If you want to use invariant mass cut (default is 100MeV/c)\n" 
 	 << "twoselectedinvmasscut         - If you want to use invariant mass selection with stricter cut on partner\n"
 	 << "invmasscut=                   - If you want to specify a different invariant mass cut \n"
-	 << "extraname=                    - extraname for directory and list if you run several tasks at once\n"
+         << "ElSelection=                  - To only store a certain source of electron candidates (hadron/nonHFE/HFE/Onlyc/Onlyb\n"  
+         << "storelastcutstep              - Store the last cutstep (so store all track with final cut info)\n"  
+         << "filterbit=                    - which filterbit to use (default is 0)\n"  
+         << "notusefilterbit               - not use filterbits at all\n";
+    cout << "maxPtCombinedPID=             - Max Pt to use TOF PID, above only use TPC PID\n"
+         << "EMCALPID                      - Use also Emcal for PID, together with TPC\n"  
 	 << "tpcclusters=                  - How many TPC clusters to use on single track cuts for electrons (default=120)\n"
 	 << "itsclusters=                  - How many itsclusters to be used in single track cuts for electrons (default=4) \n"
 	 << "itsreq=                       - (kFirst,kAny,kNone) Which ITSpixel requirement you want to impose\n"
-	 << "elmcreco=                     - (aftertrackcuts/aftertofpid/afterfullpid) Where you want to stop in track selection to look for electrons for mc \n\n";
-
+         << "impactparamcut=               - To set impact parameter cut in radial direction (used to select out all tracks)\n"  
+         << "etacut                        - To set eta cut (used to select out all tracks) \n"  
+	 << "elmcreco=                     - (aftertrackcuts/aftertofpid/afterfullpid) Where you want to stop in track selection to look for electrons for mc \n"
+	 << "elreco=                       - Where you want to stop in track selection to look for electrons for (see code for different selections  \n\n";
     return;
   }
+
   TString ofilename;
   Int_t system=0;
   Bool_t bUseMC=kFALSE;
@@ -82,14 +97,17 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
   Int_t NrTPCclusters=120; // quick fix for problem sending hfe track cut object to addtask
   Int_t NrITSclusters=4;
   Int_t ITSreq=AliHFEextraCuts::kFirst;
+  ULong64_t triggerMask=AliVEvent::kAnyINT;
   Int_t triggerParticle=AliDxHFECorrelation::kD;
   Bool_t bUseMCReco=kFALSE;
   Bool_t bUseKine=kFALSE;
   Bool_t bUseTrackEff=kFALSE;
+  Bool_t bUseEMCAL=kFALSE;
   Bool_t bUseD0Eff=kFALSE;
   TString TrackEffMap="";
   TString D0EffMap="";
   TString extraname="";
+  TString cutFilename="";
 
   cout << endl << "===============================================" << endl;
   cout << "Setting up Correlation task: " << configuration << endl;
@@ -114,74 +132,94 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
 	  if (argument.BeginsWith("file=")) {
 	    argument.ReplaceAll("file=", "");
 	    ofilename=argument;
+	    continue;
 	  }
-	  else if (argument.BeginsWith("name=")) {
+	  if (argument.BeginsWith("name=")) {
 	    argument.ReplaceAll("name=", "");
 	    analysisName=argument;
+	    continue;
 	  }
-	  if (argument.BeginsWith("cutname=")) {
-	    argument.ReplaceAll("cutname=", "");
-	    poolConfigFile=argument;
+	  if (argument.BeginsWith("EMCALPID")) {
+	    bUseEMCAL=kTRUE;
+	    taskOptions+=" EMCALPID";
+	    continue;
 	  }
 	  if (argument.BeginsWith("TrackEffName=")) {
 	    argument.ReplaceAll("TrackEffName=", "");
 	    bUseTrackEff=kTRUE;
 	    TrackEffMap=argument;
 	    taskOptions+=" useTrackEff";
+	    continue;
 	  }
 	  if (argument.BeginsWith("D0EffName=")) {
 	    argument.ReplaceAll("D0EffName=", "");
 	    bUseD0Eff=kTRUE;
 	    D0EffMap=argument;
 	    taskOptions+=" useD0Eff";
+	    continue;
 	  }
 	  if(argument.BeginsWith("useTrackEff")) {
 	    bUseTrackEff=kTRUE;
 	    taskOptions+=" useTrackEff";
-	  }
-	  if(argument.BeginsWith("reducedMode")||argument.BeginsWith("reducedmode") ){
-	    taskOptions+=" reducedMode";
+	    continue;
 	  }
 	  if(argument.BeginsWith("useD0Eff")) {
 	    bUseD0Eff=kTRUE;
 	    taskOptions+=" useD0Eff";
+	    continue;
 	  }
+	  if (argument.BeginsWith("cutname=")) {
+	    argument.ReplaceAll("cutname=", "");
+	    poolConfigFile=argument;
+	    continue;
+	  }
+	  if (argument.BeginsWith("cutFilename=")) { //--------------------//
+	    argument.ReplaceAll("cutFilename=", ""); //   Move this to     //
+	    cutFilename=argument;                    //     cutname?       //
+	    poolConfigFile=argument;                 //--------------------//
+	    continue;
+	  }       
 	  if (argument.BeginsWith("mc")) {
 	    bUseMC=kTRUE;
 	    taskOptions+=" mc";
+	    continue;
 	  }
 	  if(argument.BeginsWith("tpcclusters=")){
 	    argument.ReplaceAll("tpcclusters=", "");
 	    NrTPCclusters=argument.Atoi();
 	    ::Info("AddTaskDxHFEParticleSelection",Form("Setting nr TPC clusters to %d",NrTPCclusters));
+	    continue;
 	  }
 	  if (argument.BeginsWith("usekine") ||argument.BeginsWith("kine")) {
 	    bUseKine=kTRUE;
 	    taskOptions+=" usekine";
+	    continue;
 	  }
 	  if (argument.BeginsWith("event-mixing") ||
 	      argument.BeginsWith("mixing")/*deprecated, to be removed later*/) {
 	    bEventMixing=kTRUE;
 	    taskOptions+=" event-mixing";
-	  }
-	  if(argument.BeginsWith("runmode=")){
-	    taskOptions+=" "+argument;
-	  }
-	  if(argument.BeginsWith("storeseparateorigins")){
-	    taskOptions+=" "+argument;
+	    continue;
 	  }
 	  if (argument.BeginsWith("PbPb") ||
 	      argument.BeginsWith("system=1") ||
 	      argument.BeginsWith("Pb-Pb")) {
 	    system=1;
 	    taskOptions+=" system=Pb-Pb";
+	    continue;
 	  }
-	  if (argument.BeginsWith("fillD0scheme=")){
-	    taskOptions+=" "+argument;
+	  if (argument.BeginsWith("system=p-Pb") ||
+	      argument.BeginsWith("pPb") ||
+	      argument.BeginsWith("p-Pb") ||
+	      argument.BeginsWith("system=2")) {
+	    system=2;
+	    taskOptions+=" system=p-Pb";
+	    continue;
 	  }
 	  if(argument.BeginsWith("elmcreco")){
 	    bUseMCReco=kTRUE;
 	    taskOptions+=" "+argument;
+	    continue;
 	  }
 	  if (argument.BeginsWith("trigger=")) {
 	    taskOptions+=" "+argument;
@@ -189,46 +227,40 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
 	    if (argument.CompareTo("D0")==0) triggerParticle=AliDxHFECorrelation::kD;
 	    else if (argument.CompareTo("D")==0) triggerParticle=AliDxHFECorrelation::kD;
 	    else if (argument.CompareTo("electron")==0) triggerParticle=AliDxHFECorrelation::kElectron;
+	    continue;
 	  }
 	  if (argument.CompareTo("runD0MassReference")==0){
 	    bRunD0MassReference=kTRUE;
-	  }
-	  if(argument.BeginsWith("maxPtCombinedPID="))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("ElSelection="))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("useinvmasscut"))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("twoselectedinvmasscut"))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("invmasscut="))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("impactparamcut"))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("etacut"))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("storelastcutstep"))
-	    taskOptions+=" "+argument;
-	  if(argument.BeginsWith("notusefilterbit")){
-	    taskOptions+=" "+argument;
-	  }
-	  if(argument.BeginsWith("filterbit=")){
-	    taskOptions+=" "+argument;
+	    continue;
 	  }
 	  if(argument.BeginsWith("extraname=")){
 	    argument.ReplaceAll("extraname=", "");
 	    extraname=argument;
+	    continue;
 	  }
 	  if(argument.BeginsWith("itsclusters=")){
 	    argument.ReplaceAll("itsclusters=", "");
 	    NrITSclusters=argument.Atoi();
+	    continue;
 	   }
 	  if(argument.BeginsWith("itsreq=")){
 	    argument.ReplaceAll("itsreq=", "");
 	    if(argument.CompareTo("kFirst")==0) ITSreq=AliHFEextraCuts::kFirst;
 	    else if(argument.CompareTo("kAny")==0) ITSreq=AliHFEextraCuts::kAny;
 	    else if(argument.CompareTo("kNone")==0) ITSreq=AliHFEextraCuts::kNone;
+	    continue;
 	  }
+	  if(argument.BeginsWith("triggermask=")){
+	    argument.ReplaceAll("triggermask=", "");
+	    if(argument.CompareTo("kINT7")==0) {triggerMask=AliVEvent::kINT7; cout<<"kINT7"<<endl;}
+	    else if(argument.CompareTo("kEMC1")==0) {triggerMask=AliVEvent::kEMC1; cout<<"kEMC1"<<endl;}
+	    else if(argument.CompareTo("kEMC7")==0) {triggerMask=AliVEvent::kEMC7; cout<<"kEMC7"<<endl;}
+	    else if(argument.CompareTo("kEMC8")==0) {triggerMask=AliVEvent::kEMC8; cout<<"kEMC8"<<endl;}
+	    else if(argument.CompareTo("allEMCAL")==0) {triggerMask=(AliVEvent::kEMC1|AliVEvent::kEMC7|AliVEvent::kEMC8); cout<<"all"<<endl;}
+	    continue;
+	  }
+	  cout << "Adding argument " << argument << endl;
+	  taskOptions+=" "+argument;
 	  
 	}	
       }
@@ -299,35 +331,65 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
   if (ofilename.IsNull()) ofilename=AliAnalysisManager::GetCommonFileName();
   ofilename+=":"+analysisName;
 
+ if(cutFilename=="")
+    {
   ///______________________________________________________________________
   /// Cuts For D0
 
   AliRDHFCutsD0toKpi* RDHFD0toKpi=new AliRDHFCutsD0toKpi();
   // TODO: we might want to move this to separate functions if more data
   // sets are going to be handled
-  if (system==0) {|
+
+  //p-p
+  if (system==0) {
   RDHFD0toKpi->SetStandardCutsPP2010();
-  } else {
-  // TODO: think about p-Pb
-  RDHFD0toKpi->SetStandardCutsPbPb2011();
 
-  // For centrality 0-10%, add centrality flattening
-  //NB! NEED FOR THE MOMENT THE FILE!
-  TFile *fFlat=TFile::Open("CentrDistrBins005.root","READ");
-  TCanvas *c=fFlat->Get("cintegral");
-  TH1F *hfl=(TH1F*)c->FindObject("hint");
-  RDHFD0toKpi->SetHistoForCentralityFlattening(hfl,0.,10.,0.,0);
-  //  RDHFD0toKpi->SetUseCentrality(AliRDHFCuts::kCentV0M);
-
-  RDHFD0toKpi->SetMinCentrality(0.);// 40.*1.01
-  RDHFD0toKpi->SetMaxCentrality(10.);// 80.*1.01
+  RDHFD0toKpi->SetTriggerMask(triggerMask);
+  RDHFD0toKpi->SetTriggerClass(""); //pPb
   }
+
+  //Pb-Pb
+  else if (system==1) {
+    // TODO: think about p-Pb
+    RDHFD0toKpi->SetStandardCutsPbPb2011();
+    
+    // For centrality 0-10%, add centrality flattening
+    //NB! NEED FOR THE MOMENT THE FILE!
+    TFile *fFlat=TFile::Open("CentrDistrBins005.root","READ");
+    TCanvas *c=fFlat->Get("cintegral");
+    TH1F *hfl=(TH1F*)c->FindObject("hint");
+    RDHFD0toKpi->SetHistoForCentralityFlattening(hfl,0.,10.,0.,0);
+    //  RDHFD0toKpi->SetUseCentrality(AliRDHFCuts::kCentV0M);
+    RDHFD0toKpi->SetMinCentrality(0.);// 40.*1.01
+    RDHFD0toKpi->SetMaxCentrality(10.);// 80.*1.01
+  }
+
+  //p-Pb
+  else if (system==2) {  
+    RDHFD0toKpi->SetStandardCutsPP2010();
+    RDHFD0toKpi->SetTriggerMask(triggerMask); //pPb
+    RDHFD0toKpi->SetTriggerClass(""); //pPb
+  }
+  else {
+    //warning, no system set
+  }
+  
 
   ///______________________________________________________________________
   /// Cuts for HFE
   TString hfeCutsName;
-  if (system==0) hfeCutsName="HFE Standard Cuts";
-  else hfeCutsName="HFE Cuts PbPb";
+  if (system==0){
+    hfeCutsName="HFE Standard Cuts";
+  }
+
+  if (system==1){
+    hfeCutsName="HFE Cuts PbPb";
+  }
+
+  if (system==2){
+    hfeCutsName="HFE Cuts pPb";
+  }
+  
   AliHFEcuts *hfecuts = new AliHFEcuts("hfeCutsTPCTOF", hfeCutsName);
   hfecuts->CreateStandardCuts();
 
@@ -341,10 +403,10 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
   //hfecuts->SetCutITSdrift(AliHFEextraCuts::kAny); 	//Cut on SDD
   //hfecuts->SetCheckITSLayerStatus(kFALSE);
   hfecuts->SetMinNClustersITS(NrITSclusters); //Default = 4
-	
+  
   ///TOF
   hfecuts->SetTOFPIDStep(kTRUE);
-		
+  
   ///Additional Cuts
   hfecuts->SetPtRange(0.30, 10);
   hfecuts->SetMaxImpactParam(1.,2.);
@@ -370,7 +432,8 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
   const int paramSize=4;
   Double_t params[paramSize];
   memset(params, 0, sizeof(Double_t)*paramSize);
-  params[0]=-1.;
+  if(bUseEMCAL)  params[0]=-3.;
+  else params[0]=-1.;
   fPID->ConfigureTPCdefaultCut(NULL, params, 3.);
   fPID->InitializePID();
 
@@ -388,6 +451,60 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
   listHFE->Add(hfecuts);
   listHFE->Add(fPID);
   listHFE->Add(fPIDOnlyTOF);
+  listHFE->Add(fPIDOnlyTPC); 
+    }  // Should this extend further? (default cuts if cutlist-file is not present)
+ else //if there is a cutfile
+   {
+     TFile *filecuts;
+     TString finname="Cutlist.root";
+     filecuts=TFile::Open(finname.Data());
+     TString fRDHFcutsObj="D0toKpiCutsStandard";
+     AliRDHFCutsD0toKpi* RDHFD0toKpi=new AliRDHFCutsD0toKpi();
+     RDHFD0toKpi = (AliRDHFCutsD0toKpi*)filecuts->Get(fRDHFcutsObj.Data());
+     //RDHFD0toKpi->PrintAll();
+     
+     
+     ///______________________________________________________________________
+     /// Cuts for HFE
+     AliHFEcuts *hfecuts = new AliHFEcuts();//("hfeCutsTPCTOF","HFE Standard Cuts");
+     TString fHFEcutsObj="hfeCutsTPCTOF";
+     hfecuts=(AliHFEcuts*)filecuts->Get(fHFEcutsObj.Data());
+     
+     // ________________________________________________________________________
+     // PID for HFE
+     // PID for Only TOF
+     
+     AliHFEpid *fPIDOnlyTOF = new AliHFEpid("hfePidTOF");
+     TString fHFEpidTOFobj="hfePidTOF";
+     fPIDOnlyTOF=(AliHFEpid*)filecuts->Get(fHFEpidTOFobj.Data());
+     
+     // PID object for TPC and TOF combined
+     // Check if PID is set from outside (passed as argument)
+     
+     AliHFEpid* fPID = new AliHFEpid("hfePid");
+     TString fHFEpidobj="hfePid";
+     fPID=(AliHFEpid*)filecuts->Get(fHFEpidobj.Data());
+     
+     AliHFEpid *fPIDOnlyTPC = new AliHFEpid("hfePidTPC");
+     TString fHFEpidTPCobj="hfePidTPC";
+     fPIDOnlyTPC=(AliHFEpid*)filecuts->Get(fHFEpidTPCobj.Data());
+     
+     
+     //=========================================================
+     //Create TList of cut (and pid) objects for D0 or electron
+     TList *listHFE = new TList;
+     /*      if(Particle==AliAnalysisTaskDxHFEParticleSelection::kD0){
+	     Cutlist->SetName("cut objects D0");
+	     Cutlist->Add(RDHFD0toKpi);
+	     }
+	     else if(Particle==AliAnalysisTaskDxHFEParticleSelection::kElectron){
+     */
+     listHFE->SetName("cut objects HFE");
+     listHFE->Add(hfecuts);
+     listHFE->Add(fPID);
+     listHFE->Add(fPIDOnlyTOF);
+     listHFE->Add(fPIDOnlyTPC);
+   }
 
 
   ///______________________________________________________________________
@@ -397,7 +514,8 @@ int AddTaskDxHFECorrelation(TString configuration="", TString analysisName="PWGH
   if (poolConfigFile.IsNull()) {
     // load the default configuration from below if no file is specified
     if (system==0) poolConfiguration=createDefaultPoolConfig(bUseKine);
-    else poolConfiguration=createPbPbPoolConfig();
+   else if (system==1) poolConfiguration=createPbPbPoolConfig();
+   else if (system==2) poolConfiguration=createDefaultPoolConfig();
   } else {
     // load configuration from file, and abort if something goes wrong
     TFile* filePoolConfiguration=TFile::Open(poolConfigFile.Data());
