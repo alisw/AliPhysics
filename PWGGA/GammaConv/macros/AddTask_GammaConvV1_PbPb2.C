f9d41d0543ab770fea277b343ccd6c589c38ef0c
fbock <fbock@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-09-13 14:51:18 +0000

    submitted AddTasks for Dalitz

---
+++ PWGGA/GammaConv/macros/AddTask_GammaConvV1_PbPb2.C
@@ -0,0 +1,179 @@
+void AddTask_GammaConvV1_PbPb2(  Int_t trainConfig = 1,  //change different set of cuts
+                              Bool_t isMC   = kFALSE, //run MC 
+                              Bool_t enableQAMesonTask = kFALSE, //enable QA in AliAnalysisTaskGammaConvV1
+                              Bool_t enableQAPhotonTask = kFALSE, // enable additional QA task
+                              TString fileNameInputForWeighting = "MCSpectraInput.root", // path to file for weigting input
+                              Bool_t doWeighting = kFALSE,  //enable Weighting
+                              TString cutnumberAODBranch = "1000000060084000001500000" 
+                           ) {
+
+   // ================= Load Librariers =================================
+   gSystem->Load("libCore.so");  
+   gSystem->Load("libTree.so");
+   gSystem->Load("libGeom.so");
+   gSystem->Load("libVMC.so");
+   gSystem->Load("libPhysics.so");
+   gSystem->Load("libMinuit");
+   gSystem->Load("libSTEERBase");
+   gSystem->Load("libESD");
+   gSystem->Load("libAOD");
+   gSystem->Load("libANALYSIS");
+   gSystem->Load("libANALYSISalice");  
+   gSystem->Load("libPWGGAGammaConv.so");
+   gSystem->Load("libCDB.so");
+   gSystem->Load("libSTEER.so");
+   gSystem->Load("libSTEERBase.so");
+   gSystem->Load("libTENDER.so");
+   gSystem->Load("libTENDERSupplies.so");
+      
+   // ================== GetAnalysisManager ===============================
+   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+   if (!mgr) {
+      Error(Form("AddTask_GammaConvV1_%i",trainConfig), "No analysis manager found.");
+      return ;
+   }
+
+   // ================== GetInputEventHandler =============================
+   AliVEventHandler *inputHandler=mgr->GetInputEventHandler();
+   
+   //========= Add PID Reponse to ANALYSIS manager ====
+   if(!(AliPIDResponse*)mgr->GetTask("PIDResponseTask")){
+      gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
+      AddTaskPIDResponse(isMC);
+   }
+   
+   //=========  Set Cutnumber for V0Reader ================================
+   TString cutnumber = "100000000008400100150000000"; 
+   AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+   //========= Add V0 Reader to  ANALYSIS manager if not yet existent =====
+   if( !(AliV0ReaderV1*)mgr->GetTask("V0ReaderV1") ){
+      AliV0ReaderV1 *fV0ReaderV1 = new AliV0ReaderV1("V0ReaderV1");
+      
+      fV0ReaderV1->SetUseOwnXYZCalculation(kTRUE);
+      fV0ReaderV1->SetCreateAODs(kFALSE);// AOD Output
+      fV0ReaderV1->SetUseAODConversionPhoton(kTRUE);
+
+      if (!mgr) {
+         Error("AddTask_V0ReaderV1", "No analysis manager found.");
+         return;
+      }
+
+      // Set AnalysisCut Number
+      AliConversionCuts *fCuts=NULL;
+      if(cutnumber!=""){
+         fCuts= new AliConversionCuts(cutnumber.Data(),cutnumber.Data());
+         fCuts->SetPreSelectionCutFlag(kTRUE);
+         if(fCuts->InitializeCutsFromCutString(cutnumber.Data())){
+            fV0ReaderV1->SetConversionCuts(fCuts);
+            fCuts->SetFillCutHistograms("",kTRUE);
+         }
+      }
+      
+      if(inputHandler->IsA()==AliAODInputHandler::Class()){
+      // AOD mode
+         fV0ReaderV1->SetDeltaAODBranchName(Form("GammaConv_%s_gamma",cutnumberAODBranch.Data()));
+      }
+      fV0ReaderV1->Init();
+
+      AliLog::SetGlobalLogLevel(AliLog::kInfo);
+
+      //connect input V0Reader
+      mgr->AddTask(fV0ReaderV1);
+      mgr->ConnectInput(fV0ReaderV1,0,cinput);
+
+   }
+
+   //================================================
+   //========= Add task to the ANALYSIS manager =====
+   //================================================
+   AliAnalysisTaskGammaConvV1 *task=NULL;
+   task= new AliAnalysisTaskGammaConvV1(Form("GammaConvV1_%i",trainConfig));
+   task->SetIsHeavyIon(1);
+   task->SetIsMC(isMC);
+   // Cut Numbers to use in Analysis
+   Int_t numberOfCuts = 1;
+
+   TString *cutarray = new TString[numberOfCuts];
+   TString *mesonCutArray = new TString[numberOfCuts];
+
+   if (trainConfig == 1){ 
+      cutarray[ 0] = "601000104209297002322000000"; mesonCutArray[ 0] = "01522045009000"; 
+   } else if (trainConfig == 2) { 
+      cutarray[ 0] = "612000104209297002322000000"; mesonCutArray[ 0] = "01522045009000"; 
+   } else if (trainConfig == 3) { 
+      cutarray[ 0] = "501000104209297002322000000"; mesonCutArray[ 0] = "01522045009000"; 
+   } else if (trainConfig == 4) { 
+      cutarray[ 0] = "502000104209297002322000000"; mesonCutArray[ 0] = "01522045009000";    
+   } else if (trainConfig == 5) { 
+      cutarray[ 0] = "512000104209297002322000000"; mesonCutArray[ 0] = "01522045009000";    
+   } else if (trainConfig == 6) { 
+      cutarray[ 0] = "524000104209297002322000000"; mesonCutArray[ 0] = "01522045009000";       
+   } else if (trainConfig == 7) {    
+      cutarray[ 0] = "546000104209297002322000000"; mesonCutArray[ 0] = "01522065009000"; 
+   } else if (trainConfig == 8) {    
+      cutarray[ 0] = "548000104209297002322000000"; mesonCutArray[ 0] = "01522065009000";    
+   } else if (trainConfig == 9) {    
+      cutarray[ 0] = "545000104209297002322000000"; mesonCutArray[ 0] = "01522065009000"; 
+   } else if (trainConfig == 10) { 
+      cutarray[ 0] = "556000104209297002322000000"; mesonCutArray[ 0] = "01522065009000";
+   } else if (trainConfig == 11) { 
+      cutarray[ 0] = "568000104209297002322000000"; mesonCutArray[ 0] = "01522065009000";    
+   } else if (trainConfig == 12) { 
+      cutarray[ 0] = "567000104209297002322000000"; mesonCutArray[ 0] = "01522065009000"; 
+   } else if (trainConfig == 13) { 
+      cutarray[ 0] = "578000104209297002322000000"; mesonCutArray[ 0] = "01522065009000"; 
+   } else if (trainConfig == 14) { 
+      cutarray[ 0] = "469000104209297002322000000"; mesonCutArray[ 0] = "01522065009000";
+   } else if (trainConfig == 15) { 
+      cutarray[ 0] = "589000104209297002322000000"; mesonCutArray[ 0] = "01522065009000";    
+   } else {
+      Error(Form("GammaConvV1_%i",trainConfig), "wrong trainConfig variable no cuts have been specified for the configuration");
+      return;
+   }
+
+   TList *ConvCutList = new TList();
+   TList *MesonCutList = new TList();
+
+   TList *HeaderList = new TList();
+   TObjString *Header1 = new TObjString("pi0_1");
+   HeaderList->Add(Header1);
+//    TObjString *Header3 = new TObjString("eta_2");
+//    HeaderList->Add(Header3);
+   
+   ConvCutList->SetOwner(kTRUE);
+   AliConversionCuts **analysisCuts = new AliConversionCuts*[numberOfCuts];
+   MesonCutList->SetOwner(kTRUE);
+   AliConversionMesonCuts **analysisMesonCuts = new AliConversionMesonCuts*[numberOfCuts];
+
+   for(Int_t i = 0; i<numberOfCuts; i++){
+      analysisCuts[i] = new AliConversionCuts();
+      analysisCuts[i]->InitializeCutsFromCutString(cutarray[i].Data());
+      ConvCutList->Add(analysisCuts[i]);
+
+      analysisCuts[i]->SetFillCutHistograms("",kFALSE);
+      analysisMesonCuts[i] = new AliConversionMesonCuts();
+      analysisMesonCuts[i]->InitializeCutsFromCutString(mesonCutArray[i].Data());
+      MesonCutList->Add(analysisMesonCuts[i]);
+      analysisMesonCuts[i]->SetFillCutHistograms("");
+      analysisCuts[i]->SetAcceptedHeader(HeaderList);
+   }
+
+   task->SetConversionCutList(numberOfCuts,ConvCutList);
+   task->SetMesonCutList(numberOfCuts,MesonCutList);
+   task->SetMoveParticleAccordingToVertex(kTRUE);
+   task->SetDoMesonAnalysis(kTRUE);
+   if (enableQAMesonTask) task->SetDoMesonQA(kTRUE); //Attention new switch for Pi0 QA
+   if (enableQAPhotonTask) task->SetDoPhotonQA(kTRUE);  //Attention new switch small for Photon QA
+
+   //connect containers
+   AliAnalysisDataContainer *coutput =
+      mgr->CreateContainer(Form("GammaConvV1_%i",trainConfig), TList::Class(),
+                           AliAnalysisManager::kOutputContainer,Form("GammaConvV1_%i.root",trainConfig));
+
+   mgr->AddTask(task);
+   mgr->ConnectInput(task,0,cinput);
+   mgr->ConnectOutput(task,1,coutput);
+
+   return;
+
+}
