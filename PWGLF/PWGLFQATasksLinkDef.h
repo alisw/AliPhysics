92507fa00b63d433028df30e57536bb9120866a1
delia <delia@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-10-17 20:23:37 +0000

    First preliminary tasks for PWGLF production QA (cont'd)

---
+++ PWGLF/PWGLFQATasksLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+
+#pragma link off all glols;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliAnalysisTaskQAHighPtDeDx+;
+#pragma link C++ class AliQAProdMultistrange+;
+
+#endif
