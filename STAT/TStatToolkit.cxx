d4b3d205bf356d3cb693411b61a86aaa6020b911
mivanov <mivanov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-10-18 08:26:17 +0000

    Status line is created also in case of 1 entry. Outlyer filtering is in that case not active.

--- STAT/TStatToolkit.cxx
+++ STAT/TStatToolkit.cxx
@@ -1425,7 +1425,7 @@ Int_t  TStatToolkit::SetStatusAlias(TTree * tree, const char * expr, const char 
   */
   //
   Int_t entries = tree->Draw(expr,cut,"goff");
-  if (entries<=1){
+  if (entries<1){
     printf("Expression or cut not valid:\t%s\t%s\n", expr, cut);
     return 0;
   }
