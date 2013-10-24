c42be3ec731681bcc1fdb6f76851ef96b1d18acd
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-10-24 17:51:06 +0000

    Root6-enabled test

---
+++ test/gun/aod.C
@@ -0,0 +1,18 @@
+void aod(){
+
+    gSystem->Load("libTree");
+    gSystem->Load("libGeom");
+    gSystem->Load("libPhysics");
+    gSystem->Load("libVMC");
+    gSystem->Load("libSTEERBase");
+    gSystem->Load("libESD");
+    gSystem->Load("libAOD");
+    
+    gSystem->Load("libANALYSIS");
+    gSystem->Load("libANALYSISalice");
+    gSystem->Load("libCORRFW");
+    gSystem->Load("libPWGHFbase");
+    gSystem->Load("libPWGmuon");
+
+    gROOT->Macro("${ALICE_ROOT}/STEER/CreateAODfromESD.C");
+}
