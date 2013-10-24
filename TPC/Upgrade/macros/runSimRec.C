120b3f94399e718c599e93ef1a52707ef984735e
wiechula <wiechula@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-10-24 11:38:10 +0000

    o update

--- TPC/Upgrade/macros/runSimRec.C
+++ TPC/Upgrade/macros/runSimRec.C
@@ -1,6 +1,10 @@
 void runSimRec(Int_t simtype, Int_t SCtype, Int_t nevents, Int_t ntracks, Int_t rate=50, Int_t gas=0)
 {
   //rate is in kHz
+  // recType: 0 = LongT0
+  //          1 = noLongT0 
+  //          2 = LongT0, T0list
+  //          3 = LongT0, Z0list
 
   Int_t recoType=simtype/10;
   recoType%=10;
@@ -13,7 +17,7 @@ void runSimRec(Int_t simtype, Int_t SCtype, Int_t nevents, Int_t ntracks, Int_t 
   TString outputFile="toyMC";
 
   //for simtype also below
-  switch (simtype) {
+  switch (simtype%2) {
     case 0:
       outputFile.Append(Form("_fixed_%dkHz",rate));
       break;
@@ -27,11 +31,11 @@ void runSimRec(Int_t simtype, Int_t SCtype, Int_t nevents, Int_t ntracks, Int_t 
   switch (gas) {
     case 0:
       gasType=AliToyMCEventGeneratorSimple::kNeCO2_9010;
-      outputFile.Appen("_NeCO2");
+      outputFile.Append("_NeCO2");
       break;
     case 1:
       gasType=AliToyMCEventGeneratorSimple::kNeCO2N2_90105;
-      outputFile.Appen("_NeCO2N2");
+      outputFile.Append("_NeCO2N2");
       break;
   }
   
@@ -82,36 +86,52 @@ void runSimRec(Int_t simtype, Int_t SCtype, Int_t nevents, Int_t ntracks, Int_t 
 
   //reconstruction part
   AliToyMCReconstruction rec;
+  rec.SetForceAlpha(kTRUE);
   // rec.SetUseMaterialBudget(kTRUE)
-  rec.SetFillClusterRes(kTRUE);
-  rec.SetLongT0seed(kFALSE);
-
-  if (recoType==0){
-    rec.SetRecoSettings(1,0,AliToyMCReconstruction::kNoCorrection);
-    if (!subRecoType||subRecoType==1) rec.RunReco(outputFile.Data());
-
-    rec.SetRecoSettings(1,1,AliToyMCReconstruction::kIdeal);
-    if (!subRecoType||subRecoType==2) rec.RunReco(outputFile.Data());
-
-    rec.SetRecoSettings(0,1,AliToyMCReconstruction::kIdeal);
-    if (!subRecoType||subRecoType==3) rec.RunReco(outputFile.Data());
-
-    rec.SetRecoSettings(0,1,AliToyMCReconstruction::kAverageEta);
-    if (!subRecoType||subRecoType==4) rec.RunReco(outputFile.Data());
-
-    rec.SetRecoSettings(0,1,AliToyMCReconstruction::kNoCorrection);
-    if (!subRecoType||subRecoType==5) rec.RunReco(outputFile.Data());
-
-    rec.SetRecoSettings(0,0,AliToyMCReconstruction::kNoCorrection);
-    if (!subRecoType||subRecoType==6) rec.RunReco(outputFile.Data());
+  if (recoType==0) {
+    rec.SetFillClusterRes(kTRUE);
+    rec.SetLongT0seed(kTRUE);
+    //rec.SetUseT0list(kTRUE);
+  } else if (recoType==1) {
+    rec.SetFillClusterRes(kTRUE);
+    rec.SetLongT0seed(kFALSE);
+    //rec.SetUseT0list(kTRUE);
+  } else if (recoType==2) {
+    rec.SetFillClusterRes(kTRUE);
+    rec.SetLongT0seed(kTRUE);
+    rec.SetUseT0list(kTRUE);
+  } else if (recoType==3) {
+    rec.SetFillClusterRes(kTRUE);
+    rec.SetLongT0seed(kTRUE);
+    rec.SetUseZ0list(kTRUE);
   }
 
-  if (recoType==1) {
-    rec.SetRecoSettings(0,1,AliToyMCReconstruction::kNoCorrection);
-    if (!subRecoType||subRecoType==1) rec.RunFullTracking(outputFile.Data());
-    
-    rec.SetRecoSettings(0,0,AliToyMCReconstruction::kNoCorrection);
-    if (!subRecoType||subRecoType==2) rec.RunFullTracking(outputFile.Data());
-  }
+//  if (recoType==0){
+  rec.SetRecoSettings(1,0,AliToyMCReconstruction::kNoCorrection);
+  if (!subRecoType||subRecoType==1) rec.RunReco(outputFile.Data());
+
+  rec.SetRecoSettings(1,1,AliToyMCReconstruction::kIdeal);
+  if (!subRecoType||subRecoType==2) rec.RunReco(outputFile.Data());
+
+  rec.SetRecoSettings(0,1,AliToyMCReconstruction::kIdeal);
+  if (!subRecoType||subRecoType==3) rec.RunReco(outputFile.Data());
+
+  rec.SetRecoSettings(0,1,AliToyMCReconstruction::kAverageEta);
+  if (!subRecoType||subRecoType==4) rec.RunReco(outputFile.Data());
+
+  rec.SetRecoSettings(0,1,AliToyMCReconstruction::kNoCorrection);
+  if (!subRecoType||subRecoType==5) rec.RunReco(outputFile.Data());
+
+  rec.SetRecoSettings(0,0,AliToyMCReconstruction::kNoCorrection);
+  if (!subRecoType||subRecoType==6) rec.RunReco(outputFile.Data());
+// }
+
+//  if (recoType==1) {
+//    rec.SetRecoSettings(0,1,AliToyMCReconstruction::kNoCorrection);
+//    if (!subRecoType||subRecoType==1) rec.RunFullTracking(outputFile.Data());
+//    
+//    rec.SetRecoSettings(0,0,AliToyMCReconstruction::kNoCorrection);
+//    if (!subRecoType||subRecoType==2) rec.RunFullTracking(outputFile.Data());
+//  }
   
 }
