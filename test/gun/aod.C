591afc6efe54b18f917481c835ff0e7f0b54769e
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-10-25 08:18:42 +0000

    Removing useless loading of libraries

--- test/gun/aod.C
+++ test/gun/aod.C
@@ -1,13 +1,5 @@
 void aod(){
 
-    gSystem->Load("libTree");
-    gSystem->Load("libGeom");
-    gSystem->Load("libPhysics");
-    gSystem->Load("libVMC");
-    gSystem->Load("libSTEERBase");
-    gSystem->Load("libESD");
-    gSystem->Load("libAOD");
-    
     gSystem->Load("libANALYSIS");
     gSystem->Load("libANALYSISalice");
     gSystem->Load("libCORRFW");
