686a6381a8b45be67b2a6961d05d3d205a47017a
slindal <slindal@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-11-20 16:49:38 +0000

    -update  track cuts -update addtasks -use systematic error matrix

--- PWGGA/GammaConv/macros/AddTask_GammaConvdPhi_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvdPhi_pp.C
@@ -1,7 +1,7 @@
-AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "109000200209297002322000000",
-					 TString pionCut = "01522045009000",
-					 TString photoncut = "",
-					 Bool_t pbpb = kTRUE) {
+AliAnalysisTask *AddTask_GammaConvdPhi_pp(TString v0Cut = "000001100209366300380000000",
+					 TString pionCut = "01631031009000",
+					 Bool_t pbpb = kFALSE) {
+
 
   // standard with task
   printf("========================================================================================\n");
@@ -20,15 +20,15 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   AliAnalysisTaskdPhi *task = new AliAnalysisTaskdPhi((TString("slindalTask_dPhi")+"_" + v0Cut));
 
   ///Axes for histrograms
-  Double_t cptbins[6] = {0.5, 1.0, 1.5, 3.0, 5.0, 10};
-  task->GetAxiscPt().Set(5, cptbins);
+  Double_t cptbins[9] = {0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0, 10};
+  task->GetAxiscPt().Set(8, cptbins);
 
   Double_t tptbins[3] = {3.0, 5.0, 10.0};
   task->GetAxistPt().Set(2, tptbins);
 
   if(pbpb) {
-    Double_t centBins[7] = {0, 5, 10, 30, 50, 60, 90};
-    task->GetAxisCent().Set(6, centBins);
+    Double_t centBins[6] = {0, 5, 10, 30, 50, 60, 90};
+    task->GetAxisCent().Set(5, centBins);
   } else {
     Double_t centBins[2] = {-9999, 9999};
     task->GetAxisCent().Set(1, centBins);
@@ -39,6 +39,7 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
 
   Double_t mbins[17] = {0.07, 0.09, 0.1, 0.11, 0.12, 0.125, 0.1275, 0.13, 0.14, 0.1425, 0.145, 0.15, 0.16, 0.18, 0.2, 0.24, 0.26};
   task->GetAxisPiMass().Set(16, mbins);
+
 
   AliESDtrackCuts * trackCuts = new AliESDtrackCuts();
   TFormula *f1NClustersTPCLinearPtDep = new TFormula("f1NClustersTPCLinearPtDep","70.+30./20.*x");
@@ -62,7 +63,11 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   trackCuts->SetPtRange(cptbins[0], 1000000.0);
 
   trackCuts->SetRequireITSRefit(kFALSE);
-  task->SetTrackFilter(trackCuts);
+
+  AliConversionTrackCuts* tc = new AliConversionTrackCuts();
+  tc->SetEsdTrackCuts(trackCuts);
+  task->SetTrackFilter(tc);
+
 
   ///open side nclusters graph
   AliESDtrackCuts * trackCuts2 = new AliESDtrackCuts();
@@ -87,7 +92,12 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   trackCuts2->SetPtRange(cptbins[0], 1000000.0);
 
   trackCuts2->SetRequireITSRefit(kFALSE);
-  task->AddTrackFilter(trackCuts2, kTRUE);
+  //task->AddTrackFilter(trackCuts2, kTRUE);
+
+  AliConversionTrackCuts tc2 = new AliConversionTrackCuts();
+  tc2->SetEsdTrackCuts(trackCuts2);
+  task->AddTrackFilter(tc2, kTRUE);
+
 
   ///Tight side nclusters graphs
   AliESDtrackCuts * trackCuts3 = new AliESDtrackCuts();
@@ -112,11 +122,16 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   trackCuts3->SetPtRange(cptbins[0], 1000000.0);
 
   trackCuts3->SetRequireITSRefit(kFALSE);
-  task->AddTrackFilter(trackCuts3, kFALSE);
+  //task->AddTrackFilter(trackCuts3, kFALSE);
+
+  AliConversionTrackCuts tc3 = new AliConversionTrackCuts();
+  tc3->SetEsdTrackCuts(trackCuts3);
+  task->AddTrackFilter(tc3, kFALSE);
+
 
 
   ////Tight xyz track cuts
-  AliESDtrackCuts4 * trackCuts4 = new AliESDtrackCuts();
+  AliESDtrackCuts * trackCuts4 = new AliESDtrackCuts();
   TFormula *f1NClustersTPCLinearPtDep = new TFormula("f1NClustersTPCLinearPtDep","70.+30./20.*x");
   trackCuts4->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep, 100);
   trackCuts4->SetMaxChi2PerClusterTPC(4);
@@ -138,11 +153,15 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   trackCuts4->SetPtRange(cptbins[0], 1000000.0);
 
   trackCuts4->SetRequireITSRefit(kFALSE);
-  task->AddTrackFilter(trackCuts4, kFALSE);
+  //task->AddTrackFilter(trackCuts4, kFALSE);
+
+  AliConversionTrackCuts tc4 = new AliConversionTrackCuts();
+  tc4->SetEsdTrackCuts(trackCuts4);
+  task->AddTrackFilter(tc4, kFALSE);
 
 
   ////lose xyz track cuts
-  AliESDtrackCuts5 * trackCuts5 = new AliESDtrackCuts();
+  AliESDtrackCuts * trackCuts5 = new AliESDtrackCuts();
   TFormula *f1NClustersTPCLinearPtDep = new TFormula("f1NClustersTPCLinearPtDep","70.+30./20.*x");
   trackCuts5->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep, 100);
   trackCuts5->SetMaxChi2PerClusterTPC(4);
@@ -164,101 +183,78 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   trackCuts5->SetPtRange(cptbins[0], 1000000.0);
 
   trackCuts5->SetRequireITSRefit(kFALSE);
-  task->AddTrackFilter(trackCuts5);
+  //task->AddTrackFilter(trackCuts5);
 
+  AliConversionTrackCuts tc5 = new AliConversionTrackCuts();
+  tc5->SetEsdTrackCuts(trackCuts5);
+  task->AddTrackFilter(tc5, kTRUE);
 
 
   ///open side shared tpt clusters
-  AliESDtrackCuts * trackCuts2 = new AliESDtrackCuts();
+  AliESDtrackCuts * trackCuts6 = new AliESDtrackCuts();
   TFormula *f1NClustersTPCLinearPtDep = new TFormula("f1NClustersTPCLinearPtDep","70.+30./20.*x");
-  trackCuts2->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep, 100);
-  trackCuts2->SetMaxChi2PerClusterTPC(4);
-  trackCuts2->SetRequireTPCStandAlone(kTRUE);
-  trackCuts2->SetAcceptKinkDaughters(kFALSE);
-  trackCuts2->SetRequireTPCRefit(kTRUE);
-  trackCuts2->SetMaxFractionSharedTPCClusters(0.44);
+  trackCuts6->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep, 100);
+  trackCuts6->SetMaxChi2PerClusterTPC(4);
+  trackCuts6->SetRequireTPCStandAlone(kTRUE);
+  trackCuts6->SetAcceptKinkDaughters(kFALSE);
+  trackCuts6->SetRequireTPCRefit(kTRUE);
+  trackCuts6->SetMaxFractionSharedTPCClusters(0.44);
 
-  trackCuts2->SetMaxDCAToVertexXY(2.4);
-  trackCuts2->SetMaxDCAToVertexZ(3.2);
-  trackCuts2->SetDCAToVertex2D(kTRUE);
+  trackCuts6->SetMaxDCAToVertexXY(2.4);
+  trackCuts6->SetMaxDCAToVertexZ(3.2);
+  trackCuts6->SetDCAToVertex2D(kTRUE);
 
-  trackCuts2->SetMaxChi2PerClusterITS(36);
-  trackCuts2->SetMaxChi2TPCConstrainedGlobal(36);
+  trackCuts6->SetMaxChi2PerClusterITS(36);
+  trackCuts6->SetMaxChi2TPCConstrainedGlobal(36);
 
-  trackCuts2->SetRequireSigmaToVertex(kFALSE);
+  trackCuts6->SetRequireSigmaToVertex(kFALSE);
 
-  trackCuts2->SetEtaRange(-0.9, 0.9);
-  trackCuts2->SetPtRange(cptbins[0], 1000000.0);
+  trackCuts6->SetEtaRange(-0.9, 0.9);
+  trackCuts6->SetPtRange(cptbins[0], 1000000.0);
 
-  trackCuts2->SetRequireITSRefit(kFALSE);
-  task->AddTrackFilter(trackCuts2, kTRUE);
+  trackCuts6->SetRequireITSRefit(kFALSE);
+  //task->AddTrackFilter(trackCuts6, kTRUE);
+
+  AliConversionTrackCuts tc6 = new AliConversionTrackCuts();
+  tc6->SetEsdTrackCuts(trackCuts6);
+  task->AddTrackFilter(tc6, kTRUE);
 
 
   ///tight side shared tpt clusters
-  AliESDtrackCuts * trackCuts2 = new AliESDtrackCuts();
+  AliESDtrackCuts * trackCuts7 = new AliESDtrackCuts();
   TFormula *f1NClustersTPCLinearPtDep = new TFormula("f1NClustersTPCLinearPtDep","70.+30./20.*x");
-  trackCuts2->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep, 100);
-  trackCuts2->SetMaxChi2PerClusterTPC(4);
-  trackCuts2->SetRequireTPCStandAlone(kTRUE);
-  trackCuts2->SetAcceptKinkDaughters(kFALSE);
-  trackCuts2->SetRequireTPCRefit(kTRUE);
-  trackCuts2->SetMaxFractionSharedTPCClusters(0.36);
+  trackCuts7->SetMinNClustersTPCPtDep(f1NClustersTPCLinearPtDep, 100);
+  trackCuts7->SetMaxChi2PerClusterTPC(4);
+  trackCuts7->SetRequireTPCStandAlone(kTRUE);
+  trackCuts7->SetAcceptKinkDaughters(kFALSE);
+  trackCuts7->SetRequireTPCRefit(kTRUE);
+  trackCuts7->SetMaxFractionSharedTPCClusters(0.36);
 
-  trackCuts2->SetMaxDCAToVertexXY(2.4);
-  trackCuts2->SetMaxDCAToVertexZ(3.2);
-  trackCuts2->SetDCAToVertex2D(kTRUE);
+  trackCuts7->SetMaxDCAToVertexXY(2.4);
+  trackCuts7->SetMaxDCAToVertexZ(3.2);
+  trackCuts7->SetDCAToVertex2D(kTRUE);
 
-  trackCuts2->SetMaxChi2PerClusterITS(36);
-  trackCuts2->SetMaxChi2TPCConstrainedGlobal(36);
+  trackCuts7->SetMaxChi2PerClusterITS(36);
+  trackCuts7->SetMaxChi2TPCConstrainedGlobal(36);
 
-  trackCuts2->SetRequireSigmaToVertex(kFALSE);
+  trackCuts7->SetRequireSigmaToVertex(kFALSE);
 
-  trackCuts2->SetEtaRange(-0.9, 0.9);
-  trackCuts2->SetPtRange(cptbins[0], 1000000.0);
+  trackCuts7->SetEtaRange(-0.9, 0.9);
+  trackCuts7->SetPtRange(cptbins[0], 1000000.0);
 
-  trackCuts2->SetRequireITSRefit(kFALSE);
-  task->AddTrackFilter(trackCuts2, kTRUE);
-
-
-
-  ///Pizero cuts
-  pionString =    "01630031009000";
-  TString * picutarr[2][4];
+  trackCuts7->SetRequireITSRefit(kFALSE);
+  //task->AddTrackFilter(trackCuts7, kFALSE);
   
-  picutarr[0][
-
-  
-  
-
-
-  AliConversionMesonCuts * picuts = new AliConversionMesonCuts();
-  picuts->InitializeCutsFromCutString(pionString);
-  task->SetMesonFilter(picuts);
+  AliConversionTrackCuts tc7 = new AliConversionTrackCuts();
+  tc7->SetEsdTrackCuts(trackCuts7);
+  task->AddTrackFilter(tc7, kFALSE);
 
 
-  AliConversionMesonCuts * picuts2 = new AliConversionMesonCuts();
-  picuts2->InitializeCutsFromCutString(pionString2);
-  task->AddMesonFilter(picuts2, kFALSE);
-
-
-  AliConversionMesonCuts * picuts3 = new AliConversionMesonCuts();
-  picuts3->InitializeCutsFromCutString(pionString3);
-  task->AddMesonFilter(picuts3);
-
-
-
- 
   ///Pion cuts
   AliConversionMesonCuts * picuts = new AliConversionMesonCuts("dphi_pioncuts");
   picuts->InitializeCutsFromCutString(pionCut);
   task->SetMesonFilter(picuts);
 
-  if(photoncut.Length() > 0) {
-    ///V0 analysis cuts (applied before pion analysis)
-    AliConversionCuts * gcuts = new AliConversionCuts();
-    gcuts->InitializeCutsFromCutString(photoncut);
-    task->SetPhotonFilter(gcuts);
-  }
 
   //================================================
   //              data containers
@@ -267,7 +263,7 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   //below the trunk version
   AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
   TString baseString("slindal_dPhi_");
-  baseString += v0Cut + "_" + pionCut;
+  baseString += v0Cut + pionCut;
 
   //dumm output container
   AliAnalysisDataContainer *coutput0 =
@@ -279,7 +275,7 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   //define output containers, please use 'username'_'somename'
   AliAnalysisDataContainer *coutput1 = 
 	mgr->CreateContainer(baseString+"me", TList::Class(),
-						 AliAnalysisManager::kOutputContainer,baseString+".root");
+						 AliAnalysisManager::kOutputContainer,baseString+"me.root");
 
   //define output containers, please use 'username'_'somename'
   
@@ -292,11 +288,12 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
   }
    
   //=========  Set Cutnumber for V0Reader ================================
-  //  TString cutnumber = "100000000008400100150000000"; 
+  TString cutnumber = "000000000208400000220000000"; 
+
   //========= Add V0 Reader to  ANALYSIS manager if not yet existent =====
   AliV0ReaderV1 * fV0ReaderV1 = (AliV0ReaderV1*)mgr->GetTask("V0ReaderV1");
-  if( !fV0reader ){
-    AliV0ReaderV1 *fV0ReaderV1 = new AliV0ReaderV1("V0ReaderV1");
+  if( !fV0ReaderV1 ){
+    fV0ReaderV1 = new AliV0ReaderV1("V0ReaderV1");
       
     fV0ReaderV1->SetUseOwnXYZCalculation(kTRUE);
     fV0ReaderV1->SetCreateAODs(kFALSE);// AOD Output
@@ -309,37 +306,80 @@ AliAnalysisTask *AddTask_GammaConvdPhi_PbPb(TString v0Cut = "1090002002092970023
 
     // Set AnalysisCut Number
     AliConversionCuts *fCuts=NULL;
-    if(v0Cut!=""){
-      fCuts= new AliConversionCuts(v0Cut.Data(),v0Cut.Data());
-      if(pbpb) {
-	fCuts->SetPreSelectionCutFlag(kFALSE);
-      } else {
-	fCuts->SetPreSelectionCutFlag(kTRUE);
+    if(cutnumber!=""){
+      fCuts= new AliConversionCuts(cutnumber.Data(),cutnumber.Data());
+      fCuts->SetPreSelectionCutFlag(kTRUE);
       }
-      if(fCuts->InitializeCutsFromCutString(v0Cut)){
+      if(fCuts->InitializeCutsFromCutString(cutnumber)){
 	fV0ReaderV1->SetConversionCuts(fCuts);
-	//fCuts->SetFillCutHistograms("",kTRUE);
       }
-
-
-    }
       
-    fV0ReaderV1->Init();
-    mgr->AddTask(fV0ReaderV1);
-    mgr->ConnectInput(fV0ReaderV1,0,cinput);
-    task->SetV0Reader(fV0ReaderV1);
-    task->SaveReaderHists();
-  } else {
-    ///V0 analysis cuts (applied before pion analysis)
-    AliConversionCuts * v0cuts = new AliConversionCuts();
-    v0cuts->InitializeCutsFromCutString(v0Cut);
-    task->SetV0Reader(fV0ReaderV1);
-    task->SetV0Filter(v0cuts);
+      fV0ReaderV1->Init();
+      mgr->AddTask(fV0ReaderV1);
+      mgr->ConnectInput(fV0ReaderV1,0,cinput);
+      task->SaveReaderHists();
+  }
+ 
+  task->SetV0Reader(fV0ReaderV1);
+
+  AliConversionCuts * v0cuts = new AliConversionCuts();
+  v0cuts->InitializeCutsFromCutString(v0Cut);
+  task->SetV0Filter(v0cuts);
+
+  Int_t numberOfCuts = 7;
+  TString *tcutarray  =new TString[7];
+  TString *wcutarray = new TString[7];
+  
+  //"000001100209366300380000000",
+                // "000001100209366300380000000"; // -4 5 sigma electron line
+  tcutarray[0] = "000001100209266300380000000"; // -3 5 sigma
+  wcutarray[0] = "000001100209166300380000000"; //dedex -5 -5 sigma
+
+             // "000001100209366300380000000"; // 663: 2 sigma low pt (0.25)  0.5 sigma: high pt (3.5)
+  tcutarray[1] = "000001100209386300380000000"; // 863  2                      1 
+  wcutarray[1] = "000001100209356300380000000"; // 563  2                      -10
+
+  tcutarray[2] = "000001100209366300380000000";
+  wcutarray[2] = "000001100209366300380000000";
+
+  tcutarray[3] = "000001100269366300380000000"; // single pt 6: 0.04 tight
+  wcutarray[3] = "000001100249366300380000000"; // single pt 4: 0.075 loose
+
+                            //                 //tpc cluster cut 9 : 0.6 default
+  tcutarray[4] = "000001100206366300380000000"; //6 : > 0.7 tight 
+  wcutarray[4] = "000001100208366300380000000"; //8 : > 0.35 loose
+
+                                   //          // qt max 3: 0.05 default
+  tcutarray[5] = "000001100209366300480000000"; //4 : < 0.03 tight
+  wcutarray[5] = "000001100209366300280000000"; //2 : < 0.07 loose 
+
+                                    //         // chi2 8 default <20
+  tcutarray[6] = "000001100209366300390000000"; //9 : < 15 tight 
+  wcutarray[6] = "000001100209366300320000000"; //2 : < 30 loose
+
+
+  ///Add the tight cuts
+  for(Int_t i = 0; i < numberOfCuts; i++) {
+    AliConversionCuts * v0cuts = new AliConversionCuts("aaa","aaa");
+    v0cuts->SetPreSelectionCutFlag(kTRUE);
+    if(v0cuts->InitializeCutsFromCutString(tcutarray[i].Data())){
+      task->AddV0Filter(v0cuts, kFALSE);
+      v0cuts->SetFillCutHistograms("aaa",kFALSE);
+    }
   }
 
-  task->SelectCollisionCandidates(AliVEvent::kMB|AliVEvent::kCentral|AliVEvent::kSemiCentral);
+  ///Add the tight cuts
+  for(Int_t i = 0; i < numberOfCuts; i++) {
+    AliConversionCuts * v0cuts = new AliConversionCuts("aaa","aaa");
+    v0cuts->SetPreSelectionCutFlag(kTRUE);
+    if(v0cuts->InitializeCutsFromCutString(wcutarray[i].Data())){
+      task->AddV0Filter(v0cuts, kTRUE);
+      v0cuts->SetFillCutHistograms("aaa",kFALSE);
+    }
+  }
 
   mgr->AddTask(task);
+  task->SelectCollisionCandidates(AliVEvent::kMB);
   mgr->ConnectInput  (task,  0, cinput );
   mgr->ConnectOutput (task,  0, coutput0);
   mgr->ConnectOutput (task,  1, coutput1);
