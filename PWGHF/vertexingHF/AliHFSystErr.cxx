dc6a09609a089f58b68444d46a55559e256b1e8f
Zaida Conesa del Valle <zaida.conesa.del.valle@cern.ch>
2013-12-15 20:22:55 +0100

    Update D0 pPb systematics (A.Festanti)

--- PWGHF/vertexingHF/AliHFSystErr.cxx
+++ PWGHF/vertexingHF/AliHFSystErr.cxx
@@ -610,33 +610,31 @@ void AliHFSystErr::InitD0toKpi2013pPb0100(){
   // Raw yield extraction
   fRawYield = new TH1F("fRawYield","fRawYield",24,0,24);
   fRawYield->SetBinContent(1,0.0);
-  fRawYield->SetBinContent(2,0.08);
-  fRawYield->SetBinContent(3,0.02);
-  fRawYield->SetBinContent(4,0.05);
-  for(Int_t i=5;i<=6;i++) fRawYield->SetBinContent(i,0.02);
-  for(Int_t i=7;i<=12;i++) fRawYield->SetBinContent(i,0.05);
-  for(Int_t i=13;i<=24;i++) fRawYield->SetBinContent(i,0.08);
+  fRawYield->SetBinContent(2,0.07);
+  for(Int_t i=3;i<=6;i++) fRawYield->SetBinContent(i,0.02);
+  fRawYield->SetBinContent(7,0.03);
+  fRawYield->SetBinContent(8,0.05);
+  for(Int_t i=9;i<=12;i++) fRawYield->SetBinContent(i,0.02);
+  for(Int_t i=13;i<=16;i++) fRawYield->SetBinContent(i,0.05);
+  for(Int_t i=17;i<=24;i++) fRawYield->SetBinContent(i,0.1);
   
   // Cuts efficiency (from cuts variation)
   fCutsEff = new TH1F("fCutsEff","fCutsEff",24,0,24);
   fCutsEff->SetBinContent(1,0.0);
-  fCutsEff->SetBinContent(2,0.10);
+  fCutsEff->SetBinContent(2,0.08);
   for(Int_t i=3;i<=24;i++) fCutsEff->SetBinContent(i,0.05);
 
   // PID efficiency (from PID/noPID)
   fPIDEff = new TH1F("fPIDEff","fPIDEff",24,0,24);
   fPIDEff->SetBinContent(1,0.0);
-  fPIDEff->SetBinContent(2,0.05);
-  for(Int_t i=3;i<=12;i++) fPIDEff->SetBinContent(i,0.00);
-  for(Int_t i=13;i<=24;i++) fPIDEff->SetBinContent(i,0.05);
+  fPIDEff->SetBinContent(2,0.1);
+  for(Int_t i=3;i<=24;i++) fPIDEff->SetBinContent(i,0.00);
 
   // MC dN/dpt  
   fMCPtShape = new TH1F("fMCPtShape","fMCPtShape",24,0,24);
   fMCPtShape->SetBinContent(1,0.0);
   fMCPtShape->SetBinContent(2,0.02);
-  fMCPtShape->SetBinContent(3,0.02);
-  for(Int_t i=4;i<=6;i++) fMCPtShape->SetBinContent(i,0.0);
-  for(Int_t i=7;i<=24;i++) fMCPtShape->SetBinContent(i,0.02);
+  for(Int_t i=3;i<=24;i++) fMCPtShape->SetBinContent(i,0.0);
 
   // particle-antiparticle
   /*
