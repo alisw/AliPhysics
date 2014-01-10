0504a941094c86a52d4b0e0cb18426a13e37c724
hristov <Peter.Hristov@cern.ch>
2014-01-10 18:37:59 +0100

    Root6 - enabled version

--- test/merge/backgr/sim.C
+++ test/merge/backgr/sim.C
@@ -1,4 +1,16 @@
 void sim(Int_t nev=1) {
+   // libraries required by geant321
+
+    gSystem->Load("liblhapdf");
+    gSystem->Load("libEGPythia6");
+    gSystem->Load("libpythia6");
+    gSystem->Load("libAliPythia6");
+    gSystem->Load("libgeant321");
+    
+    gSystem->Load("libhijing");
+    gSystem->Load("libTHijing");
+
+
   AliSimulation simulator;
   simulator.SetWriteRawData("ALL","raw.root",kTRUE);
 
