a50c1c8f390c050587c6ca8104323da61095d44a
loizides <cloizides@lbl.gov>
2014-01-18 22:17:33 +0100

    Moved Hadroni Correction task to PWG/EMCAL as it does not depend on jets

---
+++ PWG/EMCAL/macros/AddTaskHadCorr.C
@@ -0,0 +1,70 @@
+// $Id$
+
+AliHadCorrTask* AddTaskHadCorr(
+  const char *nTracks        = "EmcalTracks",
+  const char *nClusters      = "EmcalClusters",
+  const char *outClusName    = "CaloClustersCorr",
+  const Double_t hadcorr     = 1,
+  const Double_t minPt       = 0.15,
+  const Double_t phiMatch    = 0.050,
+  const Double_t etaMatch    = 0.025,
+  const Double_t Eexcl       = 0,
+  const Bool_t trackClus     = kTRUE,
+  const Bool_t   histo       = kFALSE,
+  const char *outputname     = "AnalysisResults.root"
+)
+{  
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr)
+  {
+    ::Error("AddTaskHadCorr", "No analysis manager to connect to.");
+    return NULL;
+  }  
+  
+  // Check the analysis type using the event handlers connected to the analysis manager.
+  //==============================================================================
+  if (!mgr->GetInputEventHandler())
+  {
+    ::Error("AddTaskHadCorr", "This task requires an input event handler");
+    return NULL;
+  }
+  
+  //-------------------------------------------------------
+  // Init the task and do settings
+  //-------------------------------------------------------
+
+  TString name(Form("HadCorr_%s", outClusName));
+  AliHadCorrTask *hcor = new AliHadCorrTask(name, histo);
+  hcor->SetOutClusName(outClusName);
+  hcor->SetPhiMatch(phiMatch);
+  hcor->SetEtaMatch(etaMatch);
+  hcor->SetHadCorr(hadcorr);
+  hcor->SetEexcl(Eexcl);
+  hcor->SetTrackClus(trackClus);
+
+  AliParticleContainer *trackCont = hcor->AddParticleContainer(nTracks);  
+  if (trackCont) trackCont->SetParticlePtCut(minPt);
+  AliParticleContainer *clusCont = hcor->AddParticleContainer(nClusters);
+  if (clusCont) clusCont->SetParticlePtCut(minPt);
+
+  //-------------------------------------------------------
+  // Final settings, pass to manager and set the containers
+  //-------------------------------------------------------
+
+  mgr->AddTask(hcor);
+    
+  // Create containers for input/output
+  mgr->ConnectInput (hcor, 0, mgr->GetCommonInputContainer() );
+
+  if (histo) {
+    AliAnalysisDataContainer *cohcor = mgr->CreateContainer(name,
+							    TList::Class(),
+							    AliAnalysisManager::kOutputContainer,
+							    outputname);
+    mgr->ConnectOutput(hcor,1,cohcor);
+  }
+    
+  return hcor;
+}
