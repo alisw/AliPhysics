35cd1329744c7a3dae0378db4b77d69c6a854f25
belikov <iouri.belikov@in2p3.fr>
2014-01-30 16:11:35 +0100

    New geometry from Mario

--- ITS/UPGRADE/ITSUpgradeSimLinkDef.h
+++ ITS/UPGRADE/ITSUpgradeSimLinkDef.h
@@ -20,8 +20,8 @@
 #pragma link C++ class  AliITSU+;
 #pragma link C++ class  AliITSUv0+;
 #pragma link C++ class  AliITSUv0Layer+;
-//#pragma link C++ class  AliITSUv11+;       // obsolete
-//#pragma link C++ class  AliITSUv11Layer+;  // obsolete
+#pragma link C++ class  AliITSUv1+;
+#pragma link C++ class  AliITSUv1Layer+;
 #pragma link C++ class  AliITSUModule+;
 #pragma link C++ class  AliITSUSimuParam+;
 #pragma link C++ class  AliITSUSimulation+;
