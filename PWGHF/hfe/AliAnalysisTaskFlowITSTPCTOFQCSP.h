43bac8d0320490117b695ae5582f38e7b1e33bd6
rbailhac <Raphaelle.Bailhache@cern.ch>
2014-02-03 14:05:28 +0100

    Andrea Dubla's update

--- PWGHF/hfe/AliAnalysisTaskFlowITSTPCTOFQCSP.h
+++ PWGHF/hfe/AliAnalysisTaskFlowITSTPCTOFQCSP.h
@@ -80,55 +80,55 @@ private:
     Bool_t ProcessCutStep(Int_t cutStep, AliVParticle *track);
     
     Bool_t               fDebug; //! enable debug mode
-    AliAODEvent          *fAOD;                     //AOD object
-    TList                *fOutputList;          //output list
-    AliHFEcuts           *fCuts;                 //Cut Collection
-    Bool_t               fIdentifiedAsOutInz;    //Out Of Range in z
-    Bool_t               fPassTheEventCut;       //Pass The Event Cut
-    AliCFManager         *fCFM;                  //!Correction Framework Manager
-    AliHFEpid            *fPID;                  //PID
-    AliHFEpidQAmanager   *fPIDqa;             //! PID QA manager
+    AliAODEvent          *fAOD; //AOD object
+    TList                *fOutputList;//output list
+    AliHFEcuts           *fCuts; //Cut Collection
+    Bool_t               fIdentifiedAsOutInz;//Out Of Range in z
+    Bool_t               fPassTheEventCut;//Pass The Event Cut
+    AliCFManager         *fCFM;//!Correction Framework Manager
+    AliHFEpid            *fPID;//PID
+    AliHFEpidQAmanager   *fPIDqa; //! PID QA manager
     AliFlowTrackCuts     *fCutsRP; // track cuts for reference particles
     AliFlowTrackCuts     *fNullCuts; // dummy cuts for flow event tracks
-    AliFlowEvent         *fFlowEvent; //! flow events Inclusive e
+    AliFlowEvent         *fFlowEvent; // flow events Inclusive e
     const char           *fkCentralityMethod; // method used to determine centrality (V0 by default)
     Double_t             fCentrality; // event centrality for QA
     Double_t             fCentralityMin; // lower bound of cenrality bin
     Double_t             fCentralityMax; // upper bound of centrality bin
-    Double_t             fInvmassCut;           //invariant mass cut value
-    Double_t             fpTCutmin;    //pt cut value min
-    Double_t             fpTCutmax;    //pt cut value max
-    Int_t                fTrigger;          //invariant mass cut value
+    Double_t             fInvmassCut;//invariant mass cut value
+    Double_t             fpTCutmin;//pt cut value min
+    Double_t             fpTCutmax; //pt cut value max
+    Int_t                fTrigger; //invariant mass cut value
     TH1F                 *fPhi; //! QA plot of azimuthal distribution of tracks used for event plane estimation
     TH1F                 *fEta; //! QA plot of eta distribution of tracks used for event plane estimation
     TH1F                 *fVZEROA; //! QA plot vzeroa multiplicity (all tracks in event)
     TH1F                 *fVZEROC; //! QA plot vzeroc multiplicity (all tracks in event)
     TH1F                 *fTPCM; //! QA plot TPC multiplicity (tracks used for event plane estimation)
-    TH1F                 *fNoEvents;            //no of events
-    TH1F                 *fInclusiveElecPt; // Inclusive elec pt
-    TH2F                 *fTPCnsigma;               //TPC n sigma vs p
-    TH2F                 *fTPCnsigmaAft;                //TPC n sigma vs p after HFE pid
-    TH2F                 *fITSnsigmaAft;                //ITS n sigma vs p after HFE pid
-    TH2F                 *fTPCnsigmaVSptAft;   //lodviow
-    TH2F                 *fTOFns;           //track TOFnSigma
-    TH2F                 *fTOFnsAft;                //track TOFnSigma after PID
-    TH2F                 *fTOFBetaAft;
+    TH1F                 *fNoEvents; //!no of events
+    TH1F                 *fInclusiveElecPt; //!Inclusive elec pt
+    TH2F                 *fTPCnsigma;//!TPC n sigma vs p
+    TH2F                 *fTPCnsigmaAft;//!TPC n sigma vs p after HFE pid
+    TH2F                 *fITSnsigmaAft;//!ITS n sigma vs p after HFE pid
+    TH2F                 *fTPCnsigmaVSptAft;//!lodviow
+    TH2F                 *fTOFns;//!track TOFnSigma
+    TH2F                 *fTOFnsAft;//!track TOFnSigma after PID
+    TH2F                 *fTOFBetaAft;//!track TOFnSigma after PID2
     TH1F                 *fCentralityPass; // ! QA histogram of events that pass centrality cut
     TH1F                 *fCentralityNoPass; //! QA histogram of events that do not pass centrality cut
-    TH1F                 *fInvmassLS1; //LS Invmass for all rec par
-    TH1F                 *fInvmassULS1;//ULS Invmass for all rec par
-    TH1F                 *fPhotoElecPt;             //photonic elec pt
-    TH1F                 *fSemiInclElecPt;  //Semi inclusive ele pt
-    TH1F                 *fULSElecPt; //ULS elec Pt
-    TH1F                 *fLSElecPt;// LS elec pt
+    TH1F                 *fInvmassLS1; //!LS Invmass for all rec par
+    TH1F                 *fInvmassULS1;//!ULS Invmass for all rec par
+    TH1F                 *fPhotoElecPt;//!photonic elec pt
+    TH1F                 *fSemiInclElecPt;//!Semi inclusive ele pt
+    TH1F                 *fULSElecPt; //! ULS elec Pt
+    TH1F                 *fLSElecPt;//! LS elec pt
     TH2F                 *fMultCorAfterCuts; //! QA profile global and tpc multiplicity after outlier cut
     TH2F                 *fMultvsCentr; //! QA profile of centralty vs multiplicity
     TProfile             *fSubEventDPhiv2;
-    TH1D                 *EPVzA;//ep v0a
-    TH1D                 *EPVzC;//ep v0c
-    TH1D                 *EPTPC;//ep tpc
+    TH1D                 *EPVzA;//!ep v0a
+    TH1D                 *EPVzC;//!ep v0c
+    TH1D                 *EPTPC;//!ep tpc
     THnSparseF           *fV2Phi;//! v2 analysis of EP-V0
-    TH1D                 *fvertex;//vertex
+    TH1D                 *fvertex;//!vertex
     TH2F                 *fMultCorBeforeCuts; //! QA profile global and tpc multiplicity after outlier cut
     THnSparseF           *fQAPid;             //! v2 analysis of EP-V0
     Double_t             fminITSnsigmaLowpT;  //ID cuts its
@@ -147,18 +147,18 @@ private:
     Int_t                fVz; //vertex range
     Double_t             fOpeningAngleCut; //openingAngle cut value
     Bool_t               fOP_angle; //to shitch on and off the op_angle cut
-    TH1F                 *fOpeningAngleLS;  //opening angle for LS pairs
-    TH1F                 *fOpeningAngleULS; //opening angle for ULS pairs
+    TH1F                 *fOpeningAngleLS;  //!opening angle for LS pairs
+    TH1F                 *fOpeningAngleULS; //!opening angle for ULS pairs
     AliSelectNonHFE      *fNonHFE;//new elienos stuff
     Bool_t               fDCA;//selection PHelectron
-    TH2F                 *fITSnsigma;               //TPC n sigma vs p
-    TH2F                 *fTPCnsigmaAftITSTOF;               //TPC n sigma vs p
+    TH2F                 *fITSnsigma;//!TPC n sigma vs p
+    TH2F                 *fTPCnsigmaAftITSTOF; //!TPC n sigma vs p
     TH2F                 *fTPCnsigmaAftTOF; //TPC n sigma vs p
-    TH2F                 *fITSnsigmaAftTOF;//jbd
-    TH2F                 *fITSvsTOF;               //TPC n sigma vs p
-    TH2F                 *fTPCvsITS;               //TPC n sigma vs p
-    TH2F                 *fTPCvsITSafterTOF; //TPC n sigma vs p
-    TH2F                 *fTPCvsTOF;//jbd
+    TH2F                 *fITSnsigmaAftTOF;//!jbd
+    TH2F                 *fITSvsTOF;//!TPC n sigma vs p
+    TH2F                 *fTPCvsITS;//!TPC n sigma vs p
+    TH2F                 *fTPCvsITSafterTOF;//!TPC n sigma vs p
+    TH2F                 *fTPCvsTOF;//!jbd
     Bool_t               fMultCut;//for mult correlationcut
     TH2F                 *fMultCorAfterCentrBeforeCuts; //! QA profile global and tpc multiplicity after outlier cut
     TH2F                 *fMultCorAfterVZTRKComp;//!after cent comp
@@ -166,13 +166,13 @@ private:
     TH1F                 *fCentralityAfterVZTRK;//!cent chneck2
     TH1F                 *fCentralityAfterCorrCut;//!cent chneck2
     TH2F                 *fMultCorAfterCorrCut;//!after cent comp
-    TH1D                 *EPVz;//v0cep
-    TH1D                 *EPTPCp;//tpcep
-    TH1D                 *EPTPCn;//!tpcep
+    TH1D                 *EPVz;//!v0cep
+    TH1D                 *EPTPCp;//!tpcep
+    TH1D                 *EPTPCn;//!tpcen
     TProfile             *fSubEventDPhiv2new;
     THnSparseF           *fV2Phivzerotot;//! v2 analysis of EP-V0
-    TH1F                 *fHistCentrDistr;// isto for Centr Flat
-    TH1F                 *fCentralityNoPassForFlattening; //! QA histogram of events that do not pass centrality cut for flattening
+    TH1F                 *fHistCentrDistr;//-> isto for Centr Flat
+    TH1F                 *fCentralityNoPassForFlattening; //!QA histogram of events that do not pass centrality cut for flattening
   
     
     AliAnalysisTaskFlowITSTPCTOFQCSP(const AliAnalysisTaskFlowITSTPCTOFQCSP&); // not implemented
