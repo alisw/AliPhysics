bd1ee8e2379f72d7e0eac5537446434e6b80095c
shahoian <ruben.shahoyan@cern.ch>
2014-02-11 18:01:45 +0100

    Macro to reset ITSU names to old versions

---
+++ ITS/UPGRADE/SetITSULegacyPatterns.C
@@ -0,0 +1,14 @@
+void SetITSULegacyPatterns()
+{
+  // set the ITSU patterns to "legacy mode" to be able to read old geometry
+  gSystem->Load("libITSUpgradeBase");
+  AliITSUGeomTGeo::SetITSVolPattern("ITSV");
+  AliITSUGeomTGeo::SetITSLayerPattern("ITSULayer");
+  AliITSUGeomTGeo::SetITSWrapVolPattern("ITSUWrapVol");
+  AliITSUGeomTGeo::SetITSStavePattern("ITSULadder");
+  AliITSUGeomTGeo::SetITSSubStavePattern("");
+  AliITSUGeomTGeo::SetITSModulePattern("");
+  AliITSUGeomTGeo::SetITSChipPattern("ITSUModule");
+  AliITSUGeomTGeo::SetITSSensorPattern("ITSUSensor");
+  //
+}
