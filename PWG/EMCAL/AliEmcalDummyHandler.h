b0bfab013ead6013d9e8d0549346b0ee50c15629
mverweij <marta.verweij@cern.ch>
2014-02-12 13:50:32 +0100

    cosmetics

--- PWG/EMCAL/AliEmcalDummyHandler.h
+++ PWG/EMCAL/AliEmcalDummyHandler.h
@@ -3,8 +3,6 @@
 /* Copyright(c) 1998-2007, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
-/* $Id: AliEmcalDummyHandler.h 58219 2012-08-17 14:18:19Z agheata $ */
-
 //-------------------------------------------------------------------------
 //     Dummy implementation of the input handler e.g. for the case of event-loop steered analysis
 //     Author: M. Verweij
@@ -21,7 +19,6 @@ class AliEmcalDummyHandler : public AliDummyHandler {
 
     void                 SetEvent(AliVEvent *e)  { fEvent = e    ; } 
     AliVEvent           *GetEvent()        const { return fEvent ; }
-
 
  protected:
     AliVEvent    *fEvent;         //! Pointer to the event
