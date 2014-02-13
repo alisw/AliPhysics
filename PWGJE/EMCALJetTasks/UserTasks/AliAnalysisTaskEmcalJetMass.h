31af6640e0950190b0714327157b38286588aa56
mverweij <marta.verweij@cern.ch>
2014-02-13 15:49:38 +0100

    add MC matched jet analysis

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetMass.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetMass.h
@@ -24,14 +24,16 @@ class AliAnalysisTaskEmcalJetMass : public AliAnalysisTaskEmcalJet {
   void                                Terminate(Option_t *option);
 
   //Setters
-  void SetJetContainerBase(Int_t c)                             { fContainerBase = c;}
+  void SetJetContainerBase(Int_t c)                             { fContainerBase = c       ; }
+  void SetMinFractionShared(Double_t f)                         { fMinFractionShared = f   ; }
 
  protected:
   Bool_t                              RetrieveEventObjects();
   Bool_t                              Run();
   Bool_t                              FillHistograms();
 
-  Int_t                               fContainerBase;              // jets to be tagged
+  Int_t                               fContainerBase;              // jets to be analyzed
+  Double_t                            fMinFractionShared;          // only fill histos for jets if shared fraction larger than X
   
  private:
   TH2F            **fh2PtJet1VsLeadPtAllSel;      //!all jets after std selection vs leading track pt
@@ -42,12 +44,14 @@ class AliAnalysisTaskEmcalJetMass : public AliAnalysisTaskEmcalJet {
   TProfile        **fpPtVsMassJet1Tagged;         //!pT vs avg mass of tagged jets
   TH2F            **fh2MassVsAreaJet1All;         //!mass vs area of all jets
   TH2F            **fh2MassVsAreaJet1Tagged;      //!mass vs area of tagged jets
+  TH2F            **fh2MassVsNConstJet1All;       //!mass vs number of constituents of all jets
+  TH2F            **fh2MassVsNConstJet1Tagged;    //!mass vs number of constituents of tagged jets
   TH2F            **fh2EtMassOverEtRSq;           //!Et vs (M/Et*R)^2
 
   AliAnalysisTaskEmcalJetMass(const AliAnalysisTaskEmcalJetMass&);            // not implemented
   AliAnalysisTaskEmcalJetMass &operator=(const AliAnalysisTaskEmcalJetMass&); // not implemented
 
-  ClassDef(AliAnalysisTaskEmcalJetMass, 2)
+  ClassDef(AliAnalysisTaskEmcalJetMass, 3)
 };
 #endif
 
