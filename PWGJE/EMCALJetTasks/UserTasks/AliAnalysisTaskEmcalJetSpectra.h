f370dd7e42045dd8173664419cce3618c6dff093
rreed <rosi.jan.reed@cern.ch>
2014-02-17 09:28:02 -0500

    Adding histograms

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetSpectra.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetSpectra.h
@@ -7,7 +7,7 @@
 class TH1F;
 class TH2F;
 class THnSparse;
-//class AliLocalRhoParameter;
+class AliLocalRhoParameter;
 
 #include "AliAnalysisTaskEmcalJet.h"
 
@@ -17,7 +17,6 @@ class AliAnalysisTaskEmcalJetSpectra : public AliAnalysisTaskEmcalJet {
   AliAnalysisTaskEmcalJetSpectra(const char *name);
   virtual ~AliAnalysisTaskEmcalJetSpectra() {}
  
-  
   
   virtual void           UserCreateOutputObjects();
 
@@ -46,7 +45,32 @@ class AliAnalysisTaskEmcalJetSpectra : public AliAnalysisTaskEmcalJet {
   TH1F                  *fHistCorJetPtfromLocalRho[6]; //!
   TH1F                  *fHistCorJetPtfromGlobalRho[6]; //!
 
+  TH2F                  *fHistGLvsLOCrho; //!         // Global vs Local Rho distribution
+  TH2F                  *fHistRhovsdEPLOC; //! 
+  TH2F                  *fHistRhovsdEPGL; //! 
+  TH2F                  *fHistJetPtvsdEPLOC; //! 
+  TH2F                  *fHistJetPtvsdEPGL; //! 
+  TH2F                  *fHistRhovsEPLOC; //! 
+  TH2F                  *fHistRhovsEPGL; //! 
+  TH2F                  *fHistJetPtvsEPLOC; //!  
+  TH2F                  *fHistJetPtvsEPGL; //! 
+  TH1F                  *fHistCorJetPt;  //!            // (Njets) vs Corrected Jet Pt (local rho)
+  TH1F                  *fHistCorJetPtGL; //!           // (Njets) vs Corrected Jet Pt (global rho)
 
+  TH1F                  *fHistCorJetPtfromLocalRhoIN[6]; //! 
+  TH1F                  *fHistCorJetPtfromLocalRhoOUT[6]; //! 
+  TH1F                  *fHistCorJetPtfromGlobalRhoIN[6]; //! 
+  TH1F                  *fHistCorJetPtfromGlobalRhoOUT[6]; //! 
+
+  TH2F                  *fHistRhodEPcentLOC[6]; //! 
+  TH2F                  *fHistRhodEPcentGL[6]; //! 
+  TH2F                  *fHistCorJetPtdEPcentLOC[6]; //! 
+  TH2F                  *fHistCorJetPtdEPcentGL[6]; //! 
+
+  TH2F                  *fHistRhoEPcentLOC[6]; //! 
+  TH2F                  *fHistRhoEPcentGL[6]; //! 
+  TH2F                  *fHistCorJetPtEPcentLOC[6]; //! 
+  TH2F                  *fHistCorJetPtEPcentGL[6]; //! 
 
 
   AliAnalysisTaskEmcalJetSpectra(const AliAnalysisTaskEmcalJetSpectra&); // not implemented
