01d2db2db6a330680cd949be47f9f64472f8d6e8
mkrzewic <mikolaj.krzewicki@cern.ch>
2014-02-19 09:06:51 +0100

    expert QA code and macros cleaned up, scripts to follow

--- PWGPP/QA/Tracking/ExpertQA/makePeriodPlots.C
+++ PWGPP/QA/Tracking/ExpertQA/makePeriodPlots.C
@@ -1,12 +1,12 @@
-makePeriodPlots( const char *ch, const char *periodName ){
-
-//  gSystem->Load("/hera/alice/tbroeker/testground/trackDump/highPt/AliHighPtTreeAnalysis_C.so");
-  gSystem->Load("/hera/alice/tbroeker/highPt/AliHighPtTreeAnalysis_C.so");
+makePeriodPlots( const char* mergedQAfile, const char *periodName ){
+//what do I do?
+  gSystem->AddIncludePath("-I$ALICE_ROOT/include");
+  //gROOT->LoadMacro("$ALICE_ROOT/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx++");
+  gROOT->LoadMacro("AliHighPtTreeAnalysis.cxx++");
 
   AliHighPtTreeAnalysis *a = new AliHighPtTreeAnalysis();
-    a->ConnectGenericHistos( ch );
+    a->ConnectGenericHistos( mergedQAfile );
     a->SetPeriodName( periodName );
     a->SetMakeFitPerfomancePlots(kTRUE);
     a->RunPeriod();
-
 }
