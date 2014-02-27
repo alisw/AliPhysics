09196bd185881bacaa05265bbfe9e6c134734e7c
mkrzewic <mikolaj.krzewicki@cern.ch>
2014-02-27 16:16:28 +0100

    default (empty) QA script for tracking. The expert QA is going to be in that place

---
+++ PWGPP/QA/detectorQAscripts/TRK.sh
@@ -0,0 +1,18 @@
+#available variables:
+#  $dataType     e.g. data or sim
+#  $year         e.g. 2011
+#  $period       e.g. LHC13g
+#  $runNumer     e.g. 169123
+#  $pass         e.g. cpass1,pass1,passMC
+
+runLevelQA()
+{
+  qaFile=$1
+
+}
+
+periodLevelQA()
+{
+  trendingFile=$1
+
+}
