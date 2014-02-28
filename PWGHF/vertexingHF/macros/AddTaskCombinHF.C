8467fa5d9a07faca7722a862725887f457afd182
fprino <prino@to.infn.it>
2014-03-01 00:32:37 +0100

    Add efficiency histos in the task for combinatorics (Francesco) + updated macro for running this task (AndreaR)

--- PWGHF/vertexingHF/macros/AddTaskCombinHF.C
+++ PWGHF/vertexingHF/macros/AddTaskCombinHF.C
@@ -1,4 +1,4 @@
-AliAnalysisTaskCombinHF *AddTaskCombinHF(Int_t meson=0, Bool_t readMC=kTRUE)
+AliAnalysisTaskCombinHF *AddTaskCombinHF(Int_t meson=0, TString containerStr="",Bool_t readMC=kTRUE, TString cutObjFile="",TString cutObjNam="")
 {
 
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
@@ -8,23 +8,41 @@ AliAnalysisTaskCombinHF *AddTaskCombinHF(Int_t meson=0, Bool_t readMC=kTRUE)
 
   //Analysis Task
 
-  AliRDHFCuts* analysiscuts;
-  if(meson==0) analysiscuts=new AliRDHFCutsD0toKpi();
-  else analysiscuts=new AliRDHFCutsDplustoKpipi();
-  analysiscuts->SetStandardCutsPP2010();
-  
+
+  AliRDHFCuts* analysiscuts=0x0;
+  AliAODPidHF* pid=0x0;
+  if(!cutObjFile.IsNull()){
+    TFile *f=TFile::Open(cutObjFile.Data(),"READ");
+    if(f){
+      analysiscuts=(AliRDHFCuts*)f->Get(cutObjNam.Data());
+      pid=analysiscuts->GetPidHF();
+    }
+  }
+  else {
+    if(meson==0) analysiscuts=new AliRDHFCutsD0toKpi();
+    else analysiscuts=new AliRDHFCutsDplustoKpipi();
+    analysiscuts->SetStandardCutsPP2010();
+    pid=new AliAODPidHF();
+    pid->SetMatch(5);
+    pid->SetTPCnSigmaRangeForPions(-3.,3.);
+    pid->SetTPCnSigmaRangeForKaons(-2.,3.);
+    pid->SetTPCnSigmaRangeForProtons(-3.,3.);
+    pid->SetTOFnSigmaRangeForPions(-3.,3.);
+    pid->SetTOFnSigmaRangeForKaons(-2.,2.);
+    pid->SetTOFnSigmaRangeForProtons(-3.,3.);
+
+  }
+  if(!analysiscuts){
+    Printf("Wrong file or cut object name set");
+    return 0x0;
+  }
+
   AliAnalysisTaskCombinHF *dTask = new AliAnalysisTaskCombinHF(meson,analysiscuts);
   dTask->SetReadMC(readMC);
   dTask->SetDebugLevel(0);
-  AliAODPidHF* pid=new AliAODPidHF();
-  pid->SetMatch(5);
-  pid->SetTPCnSigmaRangeForPions(-3.,3.);
-  pid->SetTPCnSigmaRangeForKaons(-2.,3.);
-  pid->SetTPCnSigmaRangeForProtons(-3.,3.);
-  pid->SetTOFnSigmaRangeForPions(-3.,3.);
-  pid->SetTOFnSigmaRangeForKaons(-2.,2.);
-  pid->SetTOFnSigmaRangeForProtons(-3.,3.);
   dTask->SetPIDHF(pid);
+
+
   mgr->AddTask(dTask);
   
   // Create containers for input/output 
@@ -32,13 +50,13 @@ AliAnalysisTaskCombinHF *AddTaskCombinHF(Int_t meson=0, Bool_t readMC=kTRUE)
   TString mesname="Dzero";
   if(meson==1) mesname="Dplus";
   TString inname = Form("cinput%s",mesname.Data());
-  TString outname = Form("coutput%s",mesname.Data());
-  TString normname = Form("coutput%sNorm",mesname.Data());
+  TString outname = Form("coutput%s%s",mesname.Data(),containerStr.Data());
+  TString normname = Form("coutput%sNorm%s",mesname.Data(),containerStr.Data());
 
   AliAnalysisDataContainer *cinput = mgr->CreateContainer(inname,TChain::Class(),
 							  AliAnalysisManager::kInputContainer);
   TString outputfile = AliAnalysisManager::GetCommonFileName();
-  outputfile += Form(":PWG3_D2H_InvMass%sLowPt",mesname.Data());
+  outputfile += Form(":PWG3_D2H_InvMass%sLowPt%s",mesname.Data(),containerStr.Data());
   
   
   AliAnalysisDataContainer *coutput = mgr->CreateContainer(outname,TList::Class(),
