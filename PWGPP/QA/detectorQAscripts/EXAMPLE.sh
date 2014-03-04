6aad2d8bbc0aad0991edd3802a935f6d717e0282
mkrzewic <mikolaj.krzewicki@cern.ch>
2014-03-04 11:59:38 +0100

    add includeDetectors option, update documentation and example

--- PWGPP/QA/detectorQAscripts/EXAMPLE.sh
+++ PWGPP/QA/detectorQAscripts/EXAMPLE.sh
@@ -16,18 +16,18 @@ runLevelQA()
   #aliroot....
 
   #should produce a file trending.root
-  touch trending.root
+  #if not, a default one will be provided
 }
 
 periodLevelQA()
 {
   #path of the merged period trending.root is provided
   trendingFile=$1 
+
+  #merged trending file in fact present in current dir
   #runs in the production dir: ...../LHCXXx/passX/
   #the running dir contains all the run sub directories
   #named like 000123123 with the outputs of runLevelQA
-
-  
 
   #aliroot...
 }
