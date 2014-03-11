0d1a84dc89c76adcab3983c7d925ed3e8994197e
mfloris <michele.floris@cern.ch>
2014-03-11 11:05:16 +0100

    Improved track cuts, added custom header variables for correlations test

---
+++ PWG/DevNanoAOD/runLocalCorrelations.C
@@ -0,0 +1,116 @@
+#if !defined (__CINT__) || (defined(__MAKECINT__))
+#include <iostream>
+#include "AliAnalysisGrid.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "AliAnalysisManager.h"
+#include "AliAnalysisGrid.h"
+#include "AliVEventHandler.h"
+#include "AliAODInputHandler.h"
+#include "AliAnalysisAlien.h"
+#include "AliMCEventHandler.h"
+#include "AliPhysicsSelectionTask.h"
+#include "TRegexp.h"
+#include "AliTriggerAnalysis.h"
+#include "TChain.h"
+#include "AliAnalysisTaskPIDResponse.h"
+#include "AliAODHandler.h"
+#include "AliAnalysisTaskNanoAODFilter.h"
+#include "AliESEHelpers.h"
+
+#endif
+void LoadLibs();
+
+class AliAnalysisGrid;
+
+
+
+//______________________________________________________________________________
+void runLocalCorrelations(
+			  const int iMCtruth = 2, 
+			  const char * addTaskString = ".x AddTaskNanoAODFilter.C(%d,1)" // 
+			  )
+{
+  LoadLibs();
+  // analysis manager
+  AliAnalysisManager* mgr = new AliAnalysisManager("NanoAOD Filter", "NanoAOD filter for nanoAOD production");
+    
+  AliAODInputHandler* iH = new AliAODInputHandler();
+  mgr->SetInputEventHandler(iH);
+
+  // Define aod output handler
+  AliAODHandler* aodOutputHandler = new AliAODHandler();
+  aodOutputHandler->SetOutputFileName("AliAOD.NanoAOD.root");
+  mgr->SetOutputEventHandler(aodOutputHandler);
+  
+  gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
+  AliAnalysisTaskPIDResponse *taskPID=AddTaskPIDResponse(iMCtruth);
+  taskPID->SetUseTPCEtaCorrection(kTRUE); 
+
+  // create task
+  cout << "Macro: "<< addTaskString << " " << Form(addTaskString, iMCtruth) << endl;
+
+  AliAnalysisTaskNanoAODFilter * task = (AliAnalysisTaskNanoAODFilter*) gROOT->ProcessLine(Form(addTaskString, iMCtruth));
+
+  // Set Track event and vertex cuts here!
+  AliAnalysisNanoAODTrackCuts* trk = new AliAnalysisNanoAODTrackCuts;
+  //  trk->SetBitMask((1 << 4) | (1 << 8)); // hybrid 2010
+  trk->SetBitMask((1 << 9)); // ???
+  trk->SetMaxEta(0.9);
+  trk->SetMinPt(0.5);
+
+  AliAnalysisNanoAODEventCuts* evt = new AliAnalysisNanoAODEventCuts;
+  evt->SetVertexRange(7);
+
+  task->SetTrkCuts(trk);
+  task->SetEvtCuts(evt);
+  task->SetSetter(new AliNanoAODSimpleSetter);
+  task->SetVarList("pt,theta,phi");
+  task->SetVarListHead("cstCentr,cstMagField");
+
+  task->SelectCollisionCandidates(AliVEvent::kMB);
+
+
+  //task->SelectCollisionCandidates(AliVEvent::kMB);// FIXME
+  // enable debug printouts
+  mgr->SetDebugLevel(10);
+  //    mgr->SetNSysInfo(100);
+  if (!mgr->InitAnalysis()) return;
+  mgr->PrintStatus();
+  
+  // start analysis
+  // Always read the same file:
+  TChain * chain = new TChain("aodTree");
+  chain->Add("./AliAOD.root");
+
+  Printf("Starting Analysis....");
+  mgr->StartAnalysis("local", chain,123456789);
+
+}
+
+//______________________________________________________________________________
+
+void LoadLibs() {
+  gSystem->Load("libCore.so");  
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libPhysics.so");
+  gSystem->Load("libVMC");
+  gSystem->Load("libTree");
+  gSystem->Load("libProof");
+  gSystem->Load("libMatrix");
+  gSystem->Load("libMinuit");
+  gSystem->Load("libSTEERBase");
+  gSystem->Load("libESD");
+  gSystem->Load("libAOD");
+  gSystem->Load("libANALYSIS");
+  //  return;
+  gSystem->Load("libOADB");
+  gSystem->Load("libANALYSISalice");
+  gSystem->Load("libTENDER");
+  gSystem->Load("libCORRFW");
+
+  //  gSystem->Load("libNanoAOD.so");
+  gSystem->Load("libPWGLFspectra");
+  gSystem->Load("libPWGDevNanoAOD");
+
+}
