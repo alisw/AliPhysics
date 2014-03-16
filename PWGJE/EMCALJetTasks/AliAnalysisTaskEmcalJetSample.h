1b029861fda737eaecba152d2220f9d6f3c53c6e
mverweij <marta.verweij@cern.ch>
2014-03-16 12:29:15 +0100

    updates for JetSample example task

--- PWGJE/EMCALJetTasks/AliAnalysisTaskEmcalJetSample.h
+++ PWGJE/EMCALJetTasks/AliAnalysisTaskEmcalJetSample.h
@@ -27,13 +27,13 @@ class AliAnalysisTaskEmcalJetSample : public AliAnalysisTaskEmcalJet {
   Bool_t                      Run()              ;
 
   // General histograms
-  TH1                        *fHistTracksPt[4];            //!Track pt spectrum
-  TH1                        *fHistClustersPt[4];          //!Cluster pt spectrum
-  TH1                        *fHistLeadingJetPt[4];        //!Leading jet pt spectrum
-  TH2                        *fHistJetsPhiEta[4];          //!Phi-Eta distribution of jets
-  TH2                        *fHistJetsPtArea[4];          //!Jet pt vs. area
-  TH2                        *fHistJetsPtLeadHad[4];       //!Jet pt vs. leading hadron
-  TH2                        *fHistJetsCorrPtArea[4];      //!Jet pt - bkg vs. area
+  TH1                       **fHistTracksPt;            //!Track pt spectrum
+  TH1                       **fHistClustersPt;          //!Cluster pt spectrum
+  TH1                       **fHistLeadingJetPt;        //!Leading jet pt spectrum
+  TH2                       **fHistJetsPhiEta;          //!Phi-Eta distribution of jets
+  TH2                       **fHistJetsPtArea;          //!Jet pt vs. area
+  TH2                       **fHistJetsPtLeadHad;       //!Jet pt vs. leading hadron
+  TH2                       **fHistJetsCorrPtArea;      //!Jet pt - bkg vs. area
 
   AliJetContainer            *fJetsCont;                   //!Jets
   AliParticleContainer       *fTracksCont;                 //!Tracks
@@ -43,6 +43,6 @@ class AliAnalysisTaskEmcalJetSample : public AliAnalysisTaskEmcalJet {
   AliAnalysisTaskEmcalJetSample(const AliAnalysisTaskEmcalJetSample&);            // not implemented
   AliAnalysisTaskEmcalJetSample &operator=(const AliAnalysisTaskEmcalJetSample&); // not implemented
 
-  ClassDef(AliAnalysisTaskEmcalJetSample, 2) // jet sample analysis task
+  ClassDef(AliAnalysisTaskEmcalJetSample, 3) // jet sample analysis task
 };
 #endif
