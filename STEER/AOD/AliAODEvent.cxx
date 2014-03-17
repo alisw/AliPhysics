e4f7214cd3c0d588c410d6d1e8a0ccfd4ac9103b
mfloris <michele.floris@cern.ch>
2014-03-17 11:03:40 +0100

    Temporary patch for Nano AOD tests in AliAODEvent
    We are storing an AliNanoAODHeader in the Nano AODs, but all other AOD
    classes still assume an AliAODHeader.  Calling methods from
    AliAODHeader on an AliNanoAodHeader produces an undefined behavior. In
    particular, AliAODEvent::ClearStd was calling fHeader->Clear(); Added
    a protection to cast the header to AliVHeader in case it is not a
    standard header.
    If the Nano AOD solution is accepted, fHeader will be of AliVHEader.

--- STEER/AOD/AliAODEvent.cxx
+++ STEER/AOD/AliAODEvent.cxx
@@ -513,8 +513,17 @@ void AliAODEvent::ResetStd(Int_t trkArrSize,
 void AliAODEvent::ClearStd()
 {
   // clears the standard arrays
-  if (fHeader)
-    fHeader        ->Clear();
+  if (fHeader){
+    // FIXME: this if-else patch was introduced by Michele Floris on 17/03/14 to test nano AOD. To be removed.
+    if(fHeader->InheritsFrom("AliAODHeader")){
+      fHeader        ->Clear();
+    }
+    else {
+      AliVHeader * head = 0;
+      head = dynamic_cast<AliVHeader*>(fHeader);
+      if(head) head->Clear();
+    }
+  }
   fTracksConnected = kFALSE;
   if (fTracks)
     fTracks        ->Delete();
