5c0228316a34738395eebf020f292f66aa0cd8e7
alla <Alla.Maevskaya@cern.ch>
2014-03-31 23:49:07 +0400

    T0calib lib added

--- T0/AddTaskT0Analysis.C
+++ T0/AddTaskT0Analysis.C
@@ -6,6 +6,11 @@ AliAnalysisTask  *AddTaskT0Analysis()
   //
 
   cout<<"@@@ AddTaskT0Analysis "<<endl;
+  gSystem->Load("libANALYSIS");
+  gSystem->Load("libANALYSISalice");
+  gSystem->Load("libT0calib"); 
+
+
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
     ::Error("AddTaskT0Analysis", "No analysis manager to connect to.");
@@ -21,7 +26,7 @@ AliAnalysisTask  *AddTaskT0Analysis()
   // Check the analysis type using the event handlers connected to the analysis manager.
   //==============================================================================
   if (!mgr->GetInputEventHandler()) {
-    ::Error("AddTaskT0QA", "This task requires an input event handler");
+    ::Error("AddTaskT0Analysis", "This task requires an input event handler");
     return NULL;
   }
    TString inputDataType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
