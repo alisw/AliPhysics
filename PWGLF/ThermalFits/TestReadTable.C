3f3553bfc5085f098b09e3fd0ba9c326bcb26e28
mfloris <michele.floris@cern.ch>
2014-04-01 15:30:13 +0200

    Added Support for asymmetric normalization uncertainties
    AliParticleYield can now import and export arrays of particles with asymmetric normalization uncertainties.
    The errors are expected in the format +x-y or -y+x (no spaces). Scientific notation and percentage errors are supported.

--- PWGLF/ThermalFits/TestReadTable.C
+++ PWGLF/ThermalFits/TestReadTable.C
@@ -2,26 +2,29 @@ void TestReadTable() {
 
   LoadLibs();
   TClonesArray * arr = AliParticleYield::ReadFromASCIIFile("./PbPb_2760.txt");
-  std::cout << "------------------------------ All Part ------------------------------" << std::endl;
+  std::cout << "" << std::endl;
+  std::cout << "------------------------------ All Part, Saving thermus gile ------------------------------" << std::endl;
   arr->Print();
   AliParticleYield::WriteThermusFile(arr, "thermus.txt" );
 
   // Get it as tree
-  TTree * tree = AliParticleYield::ReadFromASCIIFileAsTree("./PbPb_2760.txt");
+  TTree * tree = AliParticleYield::ReadFromASCIIFileAsTree("./PbPb_7000.txt");
 
   // examples on how to extract sub arrays;
   delete arr;
+  std::cout << "" << std::endl;
   arr =AliParticleYield::GetEntriesMatchingSelection(tree, "fCentr == \"V0M0010\" && fStatus == 0");
   std::cout << "------------------------------ CENTR = 0-10%, Status = 0 ------------------------------" << std::endl;
   arr->Print();
 
   delete arr;
+  std::cout << "" << std::endl;
   arr =AliParticleYield::GetEntriesMatchingSelection(tree, "fCentr == \"V0M0020\" && !IsTypeRatio()");
   std::cout << "------------------------------ CENTR = 0-20%, no ratios ------------------------------" << std::endl;
   arr->Print();
 
-
-						    
+  std::cout << "" << std::endl;
+  std::cout << "------------------------------ Saving ASCII file ------------------------------" << std::endl;						    
 
   AliParticleYield::SaveAsASCIIFile(arr,"pippo.txt");
 
