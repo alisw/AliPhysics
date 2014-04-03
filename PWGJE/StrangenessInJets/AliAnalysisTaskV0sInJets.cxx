1e127e3685e9a561ebf48456e7af6851985c6d3c
mvl <marco.van.leeuwen@cern.ch>
2014-04-03 12:13:40 +0200

    Add copyright notice

--- PWGJE/StrangenessInJets/AliAnalysisTaskV0sInJets.cxx
+++ PWGJE/StrangenessInJets/AliAnalysisTaskV0sInJets.cxx
@@ -1,3 +1,18 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
 // task for analysis of V0s (K0S, (anti-)Lambda) in charged jets
 // Author: Vit Kucera (vit.kucera@cern.ch)
 
