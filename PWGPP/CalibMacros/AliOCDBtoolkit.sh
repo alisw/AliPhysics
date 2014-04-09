a3ef0c67dbc821025d2dbdcffb4b071724b52864
mivanov <marian.ivanov@cern.ch>
2014-04-09 17:11:55 +0200

    script for the OCDB comparison

---
+++ PWGPP/CalibMacros/AliOCDBtoolkit.sh
@@ -0,0 +1,34 @@
+ocdbMakeTable(){
+#
+# create a text file with the OCDB setupt descriptors#
+#
+# Input: 
+#   $1 inputFile name
+#   $2 flag type of the input file
+# Output:
+#   $3 output file name
+}
+
+
+dumpObject(){
+#
+#
+#  Input:
+#    $1 path
+#    $2 obj name 
+#    $3 type of the dump (xml or MI recursive dump )
+#  Output:
+#    #3 output file name   
+}
+
+diffObject(){
+#
+#
+#  Input:
+#    $1 path0
+#    $2 path1
+#    $2 obj name 
+#    $3 type of the dump (xml or MI recursive dump )
+#  Output:
+#    #3 output diff file name   
+}
