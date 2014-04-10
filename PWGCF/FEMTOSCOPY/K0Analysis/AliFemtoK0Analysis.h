babd6747d837480f96477fca1d1ba9841edec579
dgangadh <dhevan.raja.gangadharan@cern.ch>
2014-04-10 02:45:51 +0200

    K0s analysis update (Matt Steinpreis)

--- PWGCF/FEMTOSCOPY/K0Analysis/AliFemtoK0Analysis.h
+++ PWGCF/FEMTOSCOPY/K0Analysis/AliFemtoK0Analysis.h
@@ -30,7 +30,7 @@ class AliESDpid;
 class AliFemtoK0Analysis : public AliAnalysisTaskSE {
  public:
   AliFemtoK0Analysis();
-  AliFemtoK0Analysis(const char *name, bool SignDep = kFALSE, bool FieldPositive = kTRUE, bool OnlineCase = kTRUE, bool MeritCase = kTRUE, bool Case3D = kFALSE, float MinDL = 0.0, int MeritCutChoice = 2, float MinSep = 5.0, bool FlatCent = kFALSE, bool PsiBinning = kFALSE, int NPsiBins = 18);
+  AliFemtoK0Analysis(const char *name, bool SignDep = kFALSE, bool FieldPositive = kTRUE, bool OnlineCase = kTRUE, bool MeritCase = kTRUE, bool Case3D = kFALSE, bool CutCheck = kFALSE, float MinDL = 0.0, int MeritCutChoice = 4, float MinSep = 5.0, bool FlatCent = kFALSE, bool PsiBinning = kFALSE, int NPsiBins = 1);
   virtual ~AliFemtoK0Analysis();
   AliFemtoK0Analysis(const AliFemtoK0Analysis&);
   AliFemtoK0Analysis& operator=(const AliFemtoK0Analysis&);
@@ -62,6 +62,7 @@ class AliFemtoK0Analysis : public AliAnalysisTaskSE {
   bool fOnlineCase;
   bool fMeritCase;
   bool fCase3D;
+  bool fCutCheck;
   float fMinDecayLength;
   int fMeritCutChoice;
   float fMinSep;
