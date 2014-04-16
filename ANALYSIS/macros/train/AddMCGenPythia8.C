ceeca67fd07a780d76ef371aa02ba0eaf6441cbc
miweber <m.weber@cern.ch>
2014-04-16 17:40:38 +0200

    adding library, removing comments

--- ANALYSIS/macros/train/AddMCGenPythia8.C
+++ ANALYSIS/macros/train/AddMCGenPythia8.C
@@ -14,6 +14,7 @@ AliGenerator* AddMCGenPythia8(Float_t e_cms = 2760., Bool_t kCR = kTRUE)
 AliGenerator* CreatePythia8Gen(Float_t e_cms, Bool_t kCR) {
     
    gSystem->Load("libpythia6.so");
+   gSystem->Load("libEGPythia6.so");
    gSystem->Load("libAliPythia6.so");
    gSystem->Load("libpythia8.so");
    gSystem->Load("libAliPythia8.so");
@@ -26,12 +27,6 @@ AliGenerator* CreatePythia8Gen(Float_t e_cms, Bool_t kCR) {
 
   // set process (MB)
   gener->SetProcess(kPyMbDefault);
-  // //   charm, beauty, charm_unforced, beauty_unforced, jpsi, jpsi_chi, mb
-  // if(ptHardMin>0.) {
-  //   gener->SetProcess(kPyJets);
-  //   gener->SetPtHard((float)ptHardMin,(float)ptHardMax);
-  // } else
-  //   gener->SetProcess(kPyMb); // Minimum Bias  
   
   //   Centre of mass energy 
   gener->SetEnergyCMS(e_cms); // in GeV
@@ -41,22 +36,11 @@ AliGenerator* CreatePythia8Gen(Float_t e_cms, Bool_t kCR) {
 
   // color reconnection
   (AliPythia8::Instance())->ReadString("Tune:pp = 5");//CR
-  //gener->UseNewMultipleInteractionsScenario(); // for all Pythia versions >= 6.3
 
   if(kCR)             
     (AliPythia8::Instance())->ReadString("BeamRemnants:reconnectColours = on");
   else
     (AliPythia8::Instance())->ReadString("BeamRemnants:reconnectColours = off");
-
-
-  //   vertex position and smearing 
-  //gener->SetVertexSmear(kPerEvent);
-
-  // structure function
-  // use kCTEQ5l for Perugia tunes 
-  // except for tunes: Perugia * (325, MRSTLO*), Perugia 6 (326, CTEQ6L),  
-  // Perugia 11 M (355, MRST LO**), Perugia 11 C (356, CTEQ6L1) 
-  //gener->SetStrucFunc(kCTEQ5L); 
-   
+  
   return gener;
 }
