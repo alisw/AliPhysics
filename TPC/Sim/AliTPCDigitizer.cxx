056a0dae279d4be1e117a6433e3660a967a9f2f5
mivanov <marian.ivanov@cern.ch>
2014-05-18 22:23:47 +0200

    ATO-17 ATO-34 Modification of code before implementing of Ion tail and Crosstalk Adding pseudo code to implement Change of the nmae of local variale, making code more readable. sec,row ->sector,padRow nrows,ncols ->nTimeBins,nPads rows,col    ->iTimeBin,iPad

--- TPC/Sim/AliTPCDigitizer.cxx
+++ TPC/Sim/AliTPCDigitizer.cxx
@@ -19,14 +19,14 @@
   Class for creating of the sumable digits and digits from MC data
   //
   The input :  ideal signals (Hits->Diffusion->Attachment -Ideal signal)
-  The output:  raw digits
+  The output:  "raw digits"
 
   Effect implemented:
   1. Pad by pad gain map
-  2. Noise map
+  2. Noise map  
   3. The dead channels identified  - zerro noise for corresponding pads
      In this case the outpu equal zerro
- 
+     
 */
 
 
@@ -246,18 +246,18 @@ void AliTPCDigitizer::DigitizeFast(Option_t* option)
     
   for (Int_t segmentID=0; segmentID<param->GetNRowsTotal(); segmentID++) 
    {
-    Int_t sec, row;
-    if (!param->AdjustSectorRow(segmentID,sec,row)) 
+    Int_t sector, padRow;
+    if (!param->AdjustSectorRow(segmentID,sector,padRow)) 
      {
       cerr<<"AliTPC warning: invalid segment ID ! "<<segmentID<<endl;
       continue;
      }
-    AliTPCCalROC * gainROC = gainTPC->GetCalROC(sec);  // pad gains per given sector
-    AliTPCCalROC * noiseROC = noiseTPC->GetCalROC(sec);  // noise per given sector
+    AliTPCCalROC * gainROC = gainTPC->GetCalROC(sector);  // pad gains per given sector
+    AliTPCCalROC * noiseROC = noiseTPC->GetCalROC(sector);  // noise per given sector
     digrow->SetID(segmentID);
 
-    Int_t nrows = 0;
-    Int_t ncols = 0;
+    Int_t nTimeBins = 0;
+    Int_t nPads = 0;
 
     Bool_t digitize = kFALSE;
     for (Int_t i=0;i<nInputs; i++) 
@@ -269,8 +269,8 @@ void AliTPCDigitizer::DigitizeFast(Option_t* option)
       if (gime->TreeS()->GetEntryWithIndex(segmentID,segmentID) >= 0) {
 	digarr[i]->ExpandBuffer();
 	digarr[i]->ExpandTrackBuffer();
-        nrows = digarr[i]->GetNRows();
-        ncols = digarr[i]->GetNCols();
+        nTimeBins = digarr[i]->GetNRows();
+        nPads = digarr[i]->GetNCols();
 	active[i] = kTRUE;
 	if (!GetRegionOfInterest() || (i == 0)) digitize = kTRUE;
       } else {
@@ -280,14 +280,14 @@ void AliTPCDigitizer::DigitizeFast(Option_t* option)
      }   
     if (!digitize) continue;
 
-    digrow->Allocate(nrows,ncols);
+    digrow->Allocate(nTimeBins,nPads);
     digrow->AllocateTrack(3);
 
     Float_t q=0;
     Int_t label[1000]; //stack for 300 events 
     Int_t labptr = 0;
 
-    Int_t nElems = nrows*ncols;     
+    Int_t nElems = nTimeBins*nPads;     
  
     for (Int_t i=0;i<nInputs; i++)
      if (active[i]) { 
@@ -325,12 +325,12 @@ void AliTPCDigitizer::DigitizeFast(Option_t* option)
            ptr[i]++;
          }
         q/=16.;  //conversion factor
-	Float_t gain = gainROC->GetValue(row,elem/nrows);  // get gain for given - pad-row pad
+	Float_t gain = gainROC->GetValue(padRow,elem/nTimeBins);  // get gain for given - pad-row pad
 	//if (gain<0.5){
 	  //printf("problem\n");
 	//}
 	q*= gain;
-	Float_t noisePad = noiseROC->GetValue(row,elem/nrows);
+	Float_t noisePad = noiseROC->GetValue(padRow,elem/nTimeBins);
         //       Float_t noise  = gRandom->Gaus(0,param->GetNoise()*param->GetNoiseNormFac());  
         Float_t noise  = pTPC->GetNoise();
         q+=noise*noisePad;
@@ -357,7 +357,7 @@ void AliTPCDigitizer::DigitizeFast(Option_t* option)
     digrow->CompresBuffer(1,zerosup);
     digrow->CompresTrackBuffer(1);
     tree->Fill();
-    if (fDebug>0) cerr<<sec<<"\t"<<row<<"\n";  
+    if (fDebug>0) cerr<<sector<<"\t"<<padRow<<"\n";  
    } //for (Int_t n=0; n<param->GetNRowsTotal(); n++) 
   
 
@@ -381,10 +381,14 @@ void AliTPCDigitizer::DigitizeFast(Option_t* option)
 //------------------------------------------------------------------------
 void AliTPCDigitizer::DigitizeSave(Option_t* option)
 {
-  
-  // merge input tree's with summable digits
-  //output stored in TreeTPCD
-
+  //
+  // Merge input tree's with summable digits
+  // Output digits stored in TreeTPCD
+  // 
+  // Not active for long time.
+  // Before adding modification (for ion tail calucation and for the crorsstalk) it should be 
+  //  checked one by one with currenlty used AliTPCDigitizer::DigitizeFast
+  //
   TString optionString = option;
   if (!strcmp(optionString.Data(),"deb")) {
     cout<<"AliTPCDigitizer::Digitize: called with option deb "<<endl;
@@ -480,19 +484,19 @@ void AliTPCDigitizer::DigitizeSave(Option_t* option)
     
     }   
     
-    Int_t sec, row;
-    if (!param->AdjustSectorRow(digarr[0]->GetID(),sec,row)) {
+    Int_t sector, padRow;
+    if (!param->AdjustSectorRow(digarr[0]->GetID(),sector,padRow)) {
       cerr<<"AliTPC warning: invalid segment ID ! "<<digarr[0]->GetID()<<endl;
       continue;
     }
 
-    AliTPCCalROC * gainROC = gainTPC->GetCalROC(sec);  // pad gains per given sector
-    AliTPCCalROC * noiseROC = noiseTPC->GetCalROC(sec);  // noise per given sector
+    AliTPCCalROC * gainROC = gainTPC->GetCalROC(sector);  // pad gains per given sector
+    AliTPCCalROC * noiseROC = noiseTPC->GetCalROC(sector);  // noise per given sector
     digrow->SetID(digarr[0]->GetID());
 
-    Int_t nrows = digarr[0]->GetNRows();
-    Int_t ncols = digarr[0]->GetNCols();
-    digrow->Allocate(nrows,ncols);
+    Int_t nTimeBins = digarr[0]->GetNRows();
+    Int_t nPads = digarr[0]->GetNCols();
+    digrow->Allocate(nTimeBins,nPads);
     digrow->AllocateTrack(3);
 
     Float_t q=0;
@@ -501,18 +505,18 @@ void AliTPCDigitizer::DigitizeSave(Option_t* option)
 
     
 
-    for (Int_t rows=0;rows<nrows; rows++){
-      for (Int_t col=0;col<ncols; col++){
+    for (Int_t iTimeBin=0;iTimeBin<nTimeBins; iTimeBin++){   // iTimeBin
+      for (Int_t iPad=0;iPad<nPads; iPad++){    // pad
     
        q=0;
        labptr=0;
        // looop over digits 
         for (Int_t i=0;i<nInputs; i++){ 
-         q  += digarr[i]->GetDigitFast(rows,col);
+         q  += digarr[i]->GetDigitFast(iTimeBin,iPad);
           //q  += *(pdig[i]);
          
           for (Int_t tr=0;tr<3;tr++) {
-           Int_t lab = digarr[i]->GetTrackIDFast(rows,col,tr);
+           Int_t lab = digarr[i]->GetTrackIDFast(iTimeBin,iPad,tr);
            //Int_t lab = ptr[i][tr*nElems];
             if ( (lab > 1) ) {
               label[labptr]=lab+masks[i];
@@ -525,25 +529,32 @@ void AliTPCDigitizer::DigitizeSave(Option_t* option)
         }
        q/=16.;  //conversion factor
        //       Float_t noise  = gRandom->Gaus(0,param->GetNoise()*param->GetNoiseNormFac());  
-       Float_t gain = gainROC->GetValue(row,col);
+       Float_t gain = gainROC->GetValue(padRow,iPad);
        q*= gain;
-       Float_t noisePad = noiseROC->GetValue(row, col);
+       Float_t noisePad = noiseROC->GetValue(padRow, iPad);
 
        Float_t noise  = pTPC->GetNoise();
        q+=noise*noisePad;
+       //
+       // here we can get digits from past and add signal
+       //
+       //
+       //for (Int_t jTimeBin=0; jTimeBin<iTimeBin; jTimeBin++)
+       //  q+=ionTail
+       //
 
         q=TMath::Nint(q);
         if (q > zerosup){ 
          
          if(q >= param->GetADCSat()) q = (Short_t)(param->GetADCSat() - 1);
-         digrow->SetDigitFast((Short_t)q,rows,col);  
+         digrow->SetDigitFast((Short_t)q,iTimeBin,iPad);  
          // *pdig1 =Short_t(q);
          for (Int_t tr=0;tr<3;tr++){
            if (tr<labptr) 
-             ((AliSimDigits*)digrow)->SetTrackIDFast(label[tr],rows,col,tr);
+             ((AliSimDigits*)digrow)->SetTrackIDFast(label[tr],iTimeBin,iPad,tr);
            //ptr1[tr*nElems] = label[tr];
            //else
-             //           ((AliSimDigits*)digrow)->SetTrackIDFast(-1,rows,col,tr);          
+             //           ((AliSimDigits*)digrow)->SetTrackIDFast(-1,iTimeBin,iPad,tr);          
            //  ptr1[tr*nElems] = 1;
          }
        }
@@ -555,7 +566,7 @@ void AliTPCDigitizer::DigitizeSave(Option_t* option)
     digrow->CompresBuffer(1,zerosup);
     digrow->CompresTrackBuffer(1);
     tree->Fill();
-    if (fDebug>0) cerr<<sec<<"\t"<<row<<"\n";  
+    if (fDebug>0) cerr<<sector<<"\t"<<padRow<<"\n";  
   } 
 //  printf("end TPC merging - end -Tree %s\t%p\n",fDigInput->GetInputTreeH(0)->GetName(),fDigInput->GetInputTreeH(0)->GetListOfBranches()->At(3));
   //fDigInput->GetTreeDTPC()->Write(0,TObject::kOverwrite);
