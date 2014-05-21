d86e86e5d85c59a87aa9a253a3135d4ed90ee2be
zconesa <zaida.conesa.del.valle@cern.ch>
2014-05-21 09:53:22 +0200

    Fixes for possible leaks

--- PWGHF/vertexingHF/AliCFVertexingHF2Prong.cxx
+++ PWGHF/vertexingHF/AliCFVertexingHF2Prong.cxx
@@ -174,7 +174,7 @@ Bool_t AliCFVertexingHF2Prong::GetGeneratedValuesFromMCParticle(Double_t* vector
 	else{
 		AliWarning("There are problems!! particle was expected to be either a D0 or a D0bar, check...");
 		delete decay;
-		return vectorMC;
+		return bGenValues;
 	}
 	if (cosThetaStar < -1 || cosThetaStar > 1) {
 		AliWarning("Invalid value for cosine Theta star, returning");
