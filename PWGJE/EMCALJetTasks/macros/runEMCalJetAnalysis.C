e055dfc7a8d806b9d51d90528b3955027bd55cb2
mvl <marco.van.leeuwen@cern.ch>
2014-05-23 17:47:24 +0200

    Fix argument for AddTaskJetPreparation

--- PWGJE/EMCALJetTasks/macros/runEMCalJetAnalysis.C
+++ PWGJE/EMCALJetTasks/macros/runEMCalJetAnalysis.C
@@ -161,7 +161,7 @@ void runEMCalJetAnalysis(
 
   // ################# Now: Call jet preparation macro (picotracks, hadronic corrected caloclusters, ...) 
   gROOT->LoadMacro("$ALICE_ROOT/PWGJE/EMCALJetTasks/macros/AddTaskJetPreparation.C");
-  AddTaskJetPreparation(dataType);
+  AddTaskJetPreparation(periodName);
 
   // ################# Now: Add jet finders+analyzers
   gROOT->LoadMacro("$ALICE_ROOT/PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C");
