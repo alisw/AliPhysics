d24f7c5f13dfaaaa426c41f5d3260874dc9cbeeb
mivanov <marian.ivanov@cern.ch>
2014-05-27 18:20:51 +0200

    ATO-17 Optional usage of debug streamer  configured

--- TPC/Sim/AliTPCDigitizer.h
+++ TPC/Sim/AliTPCDigitizer.h
@@ -6,6 +6,7 @@
 /* $Id$ */
 
 #include "AliDigitizer.h"
+class TTreeSredirector;
 
 class AliDigitizationInput;
 
@@ -23,7 +24,9 @@ class AliTPCDigitizer : public AliDigitizer {
  private: 
     void DigitizeFast(Option_t* option=0); //digitize - using row pointers
     void DigitizeSave(Option_t* option=0); // digitize using controlled arrays   
-    Int_t fDebug;
+    void DigitizeWithTailAndCrossTalk(Option_t* option=0); 
+    Int_t fDebug;                         //
+    TTreeSRedirector *fDebugStreamer;     //!debug streamer
  private:
     ClassDef(AliTPCDigitizer,2)  // MUON merging/digitization
 };    
