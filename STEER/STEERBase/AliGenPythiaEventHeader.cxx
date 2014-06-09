a89be3596a22bd7aee10b0202f81ee86a9be8f06
morsch <andreas.morsch@cern.ch>
2014-06-09 11:40:30 +0200

    Initialisation fixed A. Aiola

--- STEER/STEERBase/AliGenPythiaEventHeader.cxx
+++ STEER/STEERBase/AliGenPythiaEventHeader.cxx
@@ -28,7 +28,8 @@ AliGenPythiaEventHeader::AliGenPythiaEventHeader():
     fYJet(-1.),
     fInMediumLength(0.),
     fImpactParameter(-1),
-    fPtHard(0.) 
+    fPtHard(0.),
+    fXsection(0)
 {
 // Default Constructor
     for (Int_t i = 0; i < 4; i++) fZquench[i] = 0.;
@@ -49,7 +50,8 @@ AliGenPythiaEventHeader::AliGenPythiaEventHeader(const char* name):
     fYJet(-1.),
     fInMediumLength(0.),
     fImpactParameter(-1),
-    fPtHard(0.) 
+    fPtHard(0.),
+    fXsection(0) 
 {
 // Constructor
     for (Int_t i = 0; i < 4; i++) fZquench[i] = 0.;
