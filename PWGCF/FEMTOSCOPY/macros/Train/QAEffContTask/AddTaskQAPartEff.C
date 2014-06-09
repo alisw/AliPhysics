36d7e29a80211b111c67cb83fea080a48bc0838e
majanik <majanik@cern.ch>
2014-06-09 15:22:02 +0200

    ALso the "Add" marco, for this QA/Eff task, for the train.

---
+++ PWGCF/FEMTOSCOPY/macros/Train/QAEffContTask/AddTaskQAPartEff.C
@@ -0,0 +1,28 @@
+AliAnalysisTaskParticleEfficiency *AddTaskQAPartEff(const char* outfilename="AnalysisResults.root")
+{
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTaskPhiCorrelations", "No analysis manager to connect to.");
+    return NULL;
+  }  
+
+  // Create the task and configure it.
+  //===========================================================================
+  AliAnalysisTaskParticleEfficiency* ana = new  AliAnalysisTaskParticleEfficiency("MyTask");
+
+  if (!outfilename)
+    outfilename = AliAnalysisManager::GetCommonFileName();
+
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("MyList", TList::Class(),AliAnalysisManager::kOutputContainer,outfilename);
+
+ mgr->AddTask(ana);
+  
+
+  mgr->ConnectInput  (ana, 0, mgr->GetCommonInputContainer());
+  mgr->ConnectOutput (ana, 1, coutput1 );
+ 
+  return ana;
+
+}
