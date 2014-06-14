10612ed91cf69967b3bc313d1cc7d334ae30ff50
mivanov <marian.ivanov@cern.ch>
2014-06-15 01:08:05 +0200

    ATO-17 - test for dEdx Ion tail and crosttalk simulation (dummy for the moment)

---
+++ test/testdEdx/sim.C
@@ -0,0 +1,35 @@
+void sim(Int_t nev=20) {
+  gSystem->Load("liblhapdf");
+  gSystem->Load("libEGPythia6");
+  gSystem->Load("libpythia6");
+  gSystem->Load("libAliPythia6");
+  gSystem->Load("libhijing");
+  gSystem->Load("libTHijing");
+  gSystem->Load("libgeant321");
+
+  if (gSystem->Getenv("EVENT"))
+   nev = atoi(gSystem->Getenv("EVENT")) ;   
+  
+  AliSimulation simulator;
+  simulator.SetMakeSDigits("ITS TPC TRD TOF PHOS HMPID EMCAL MUON FMD ZDC PMD T0");
+  //simulator.SetMakeDigitsFromHits("ITS TPC");
+  simulator.SetWriteRawData("ALL","raw.root",kTRUE);
+
+  simulator.SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+  simulator.SetSpecificStorage("GRP/GRP/Data",
+			       Form("local://%s",gSystem->pwd()));
+  
+  simulator.SetRunQA("ALL:ALL") ; 
+  
+  simulator.SetQARefDefaultStorage("local://$ALICE_ROOT/QAref") ;
+
+  for (Int_t det = 0 ; det < AliQA::kNDET ; det++) {
+    simulator.SetQACycles((AliQAv1::DETECTORINDEX_t)det, nev+1) ;
+  }
+  
+  TStopwatch timer;
+  timer.Start();
+  simulator.Run(nev);
+  timer.Stop();
+  timer.Print();
+}
