90d169cbce6bd4cdd982c6154bda39c9ccdda436
mkrzewic <mikolaj.krzewicki@cern.ch>
2014-06-22 01:11:46 +0200

    overwrite existing files when unzipping archives

--- PWGPP/QA/scripts/alienSync.sh
+++ PWGPP/QA/scripts/alienSync.sh
@@ -291,8 +291,8 @@ main()
     fi
 
     if [[ $unzipFiles -eq 1 ]]; then
-      echo unzip ${destination} -d ${destinationdir}
-      unzip ${destination} -d ${destinationdir}
+      echo unzip -o ${destination} -d ${destinationdir}
+      unzip -o ${destination} -d ${destinationdir}
     fi
 
     echo
