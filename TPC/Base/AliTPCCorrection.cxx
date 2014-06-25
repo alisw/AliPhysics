e5831a9d5efe4836983a8e2bdb49341f6f55c442
hristov <Peter.Hristov@cern.ch>
2014-06-25 15:49:57 +0400

    Fix for make htmldoc

--- TPC/Base/AliTPCCorrection.cxx
+++ TPC/Base/AliTPCCorrection.cxx
@@ -60,6 +60,7 @@
 //   gROOT->SetStyle("Plain"); gStyle->SetPalette(1);
 //   TCanvas *c2 = new TCanvas("cAliTPCCorrection","cAliTPCCorrection",700,1050);  c2->Divide(2,3);
 //   AliTPCROCVoltError3D roc; // EXAMPLE PLOTS - SEE BELOW
+//   roc.SetROCDataFileName("$ALICE_ROOT/TPC/Calib/maps/TPCROCdzSurvey.root");
 //   roc.SetOmegaTauT1T2(0,1,1); // B=0
 //   Float_t z0 = 1; // at +1 cm -> A side
 //   c2->cd(1); roc.CreateHistoDRinXY(1.,300,300)->Draw("cont4z"); 
