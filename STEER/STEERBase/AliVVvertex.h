4bb0893128ef434fe9e4f6cd526ab9cfe0dfbb7a
sgweber <steffen.georg.weber@cern.ch>
2014-07-01 14:27:59 +0200

    made abstract functions in AliVVvertex non-abstract, becasue AliFlatESDVertex does not implement all of them

--- STEER/STEERBase/AliVVvertex.h
+++ STEER/STEERBase/AliVVvertex.h
@@ -19,14 +19,14 @@ class AliVVvertex
   virtual Double_t GetY() const { return 0; }
   virtual Double_t GetZ() const { return 0; }
   virtual void GetXYZ(Double_t pos[3]) const {if (pos[0]<0) return;}
-  virtual void     GetCovarianceMatrix(Double_t covmatrix[6]) const = 0;
-  virtual Double_t GetChi2perNDF() const = 0;
-  virtual Double_t GetChi2() const = 0;
-  virtual Int_t    GetNDF() const = 0;
-  virtual void     PrintIndices() const = 0;
-  virtual void     Print(Option_t* option = "") const = 0;
-  virtual Int_t    GetBC() const = 0;
-  virtual void Clear(Option_t* /*option*/) = 0;
+  virtual void     GetCovarianceMatrix(Double_t covmatrix[6]) const { return ; }
+  virtual Double_t GetChi2perNDF() const { return 0; }
+  virtual Double_t GetChi2() const { return 0; }
+  virtual Int_t    GetNDF() const{ return 0; }
+  virtual void     PrintIndices() const { return ; }
+  virtual void     Print(Option_t* option = "") const { return ; }
+  virtual Int_t    GetBC() const{ return 0; }
+  virtual void Clear(Option_t* /*option*/) const { return ; }
   virtual Int_t    GetNContributors() const { return 0; }
 
   //  ClassDef(AliVVvertex, 1)   
