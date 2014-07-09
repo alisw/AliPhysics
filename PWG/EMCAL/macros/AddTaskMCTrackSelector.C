32abbeec94a89141211821344298b0dca5854edb
saiola <salvatore.aiola@cern.ch>
2014-07-09 11:08:16 -0400

    Add option to select physical primaries

--- PWG/EMCAL/macros/AddTaskMCTrackSelector.C
+++ PWG/EMCAL/macros/AddTaskMCTrackSelector.C
@@ -1,10 +1,9 @@
-// $Id$
-
 AliEmcalMCTrackSelector* AddTaskMCTrackSelector(
-  const char *outname    = "MCParticles",
+  const char *outname    = "mcparticles",
   Bool_t      nk         = kFALSE,
   Bool_t      ch         = kFALSE,
-  Double_t    etamax     = 1
+  Double_t    etamax     = 1,
+  Bool_t      physPrim   = kTRUE
 )
 {  
   // Get the pointer to the existing analysis manager via the static access method.
@@ -34,6 +33,7 @@ AliEmcalMCTrackSelector* AddTaskMCTrackSelector(
   eTask->SetRejectNK(nk);
   eTask->SetChargedMC(ch);
   eTask->SetEtaMax(etamax);
+  eTask->SetOnlyPhysPrim(physPrim);
 
   //-------------------------------------------------------
   // Final settings, pass to manager and set the containers
