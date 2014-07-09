17c85a94c49d90a2c2bfb86027fa211a210ae186
sgorbuno <Sergey.Gorbunov@cern.ch>
2014-07-09 19:58:29 +0200

    reinitialisation constructor added to AliExternalTrackParam and its base classes

---
+++ STEER/STEERBase/AliVVMisc.h
@@ -0,0 +1,15 @@
+#ifndef ALIVVMISC_H
+#define ALIVVMISC_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               *
+ * Primary Authors : Steffen Weber     */
+
+
+
+enum AliVVConstructorReinitialisationFlag{AliVVReinitialize};
+
+typedef enum AliVVConstructorReinitialisationFlag AliVVConstructorReinitialisationFlag;
+
+
+#endif
