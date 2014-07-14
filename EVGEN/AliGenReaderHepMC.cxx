06274b10937fd564467d2ee1fa7cb5b1cfa96b72
bthorsbr <brian.peter.thorsbro@cern.ch>
2014-07-14 17:26:57 +0200

    Added AliGenHepMCEventHeader class and put it in STEERBase library. Updated AliGenReaderHepMC to create a header for each event.

--- EVGEN/AliGenReaderHepMC.cxx
+++ EVGEN/AliGenReaderHepMC.cxx
@@ -5,8 +5,7 @@
 #include "AliGenReaderHepMC.h"
 #include "AliRun.h"
 #include "AliStack.h"
-
-
+#include "AliGenHepMCEventHeader.h"
 
 
 ClassImp(AliGenReaderHepMC)
@@ -33,9 +32,6 @@ void AliGenReaderHepMC::Init()
    fEventsHandle = new HepMC::IO_GenEvent(fFileName, std::ios::in);
    fParticleArray = new TClonesArray("TParticle");
    fParticleIterator = new TIter(fParticleArray);
-
-
-
 }
 
 Int_t AliGenReaderHepMC::NextEvent()
@@ -44,10 +40,33 @@ Int_t AliGenReaderHepMC::NextEvent()
    if ((fGenEvent = fEventsHandle->read_next_event())) {
       THepMCParser::ParseGenEvent2TCloneArray(fGenEvent,fParticleArray,false);
       fParticleIterator->Reset();
-
-
-      // implement header... somewhere
-
+      THepMCParser::HeavyIonHeader_t heavyIonHeader;
+      THepMCParser::PdfHeader_t pdfHeader;
+      THepMCParser::ParseGenEvent2HeaderStructs(fGenEvent,heavyIonHeader,pdfHeader,true,true);
+      fGenEventHeader = new AliGenHepMCEventHeader(
+            heavyIonHeader.Ncoll_hard,
+            heavyIonHeader.Npart_proj,
+            heavyIonHeader.Npart_targ,
+            heavyIonHeader.Ncoll,
+            heavyIonHeader.spectator_neutrons,
+            heavyIonHeader.spectator_protons,
+            heavyIonHeader.N_Nwounded_collisions,
+            heavyIonHeader.Nwounded_N_collisions,
+            heavyIonHeader.Nwounded_Nwounded_collisions,
+            heavyIonHeader.impact_parameter,
+            heavyIonHeader.event_plane_angle,
+            heavyIonHeader.eccentricity,
+            heavyIonHeader.sigma_inel_NN,
+            pdfHeader.id1,
+            pdfHeader.id2,
+            pdfHeader.pdf_id1,
+            pdfHeader.pdf_id2,
+            pdfHeader.x1,
+            pdfHeader.x2,
+            pdfHeader.scalePDF,
+            pdfHeader.pdf1,
+            pdfHeader.pdf2
+      );
       return fGenEvent->particles_size();
    }
    return 0;
