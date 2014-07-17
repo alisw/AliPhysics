bda6dc841e7e4c13c249b445dac25506ae8f87e5
fbellini <fbellini@cern.ch>
2014-07-17 13:26:06 +0200

    Modified new TOF QA task and macro to include various sets of track cuts + minor changes to histograms axes labels. AddTaskTOFQA macro modified to include selection of trigger mask and track cuts.

--- PWGPP/TOF/AddTaskTOFQA.C
+++ PWGPP/TOF/AddTaskTOFQA.C
@@ -1,7 +1,10 @@
 /*  created by fbellini@cern.ch on 14/09/2010 */
 /*  last modified by fbellini   on 11/11/2011 */
 
-AliAnalysisTaskSE * AddTaskTOFQA(Bool_t flagEnableAdvancedCheck=kFALSE) 
+AliAnalysisTaskSE * AddTaskTOFQA(Bool_t flagEnableAdvancedCheck=kFALSE, 
+				 UInt_t triggerMask = AliVEvent::kAnyINT,
+				 Int_t trackCutSetTOFqa = 0,
+				 TString cutName = "") 
 {
   // Task for checking TOF QA
  
@@ -21,50 +24,47 @@ AliAnalysisTaskSE * AddTaskTOFQA(Bool_t flagEnableAdvancedCheck=kFALSE)
   // Create the task
   AliAnalysisTaskTOFqa *task = new AliAnalysisTaskTOFqa("taskTOFqa");
   task->EnableAdvancedCheck(flagEnableAdvancedCheck);
+  task->SelectCollisionCandidates(triggerMask);
   //AliLog::SetClassDebugLevel("AliAnalysisTaskTOFqa",1);
   mgr->AddTask(task);
 
   /* cuts used for QA in 2010 p-p */
-  /*
-  AliESDtrackCuts* esdTrackCutsLoose2010 = new AliESDtrackCuts("AliESDtrackCuts", "esdTrackCutsLoose2010");
-  esdTrackCutsLoose2010->SetMinNClustersTPC(70); 
-  esdTrackCutsLoose2010->SetMaxChi2PerClusterTPC(3.5); 
-  esdTrackCutsLoose2010->SetMaxCovDiagonalElements(2, 2, 0.5, 0.5, 2);
-  esdTrackCutsLoose2010->SetRequireTPCRefit(kTRUE);
-  esdTrackCutsLoose2010->SetMaxDCAToVertexXY(3.0); 
-  esdTrackCutsLoose2010->SetMaxDCAToVertexZ(3.0); 
-  esdTrackCutsLoose2010->SetRequireSigmaToVertex(kTRUE); 
-  esdTrackCutsLoose2010->SetAcceptKinkDaughters(kFALSE); 
-  esdTrackCutsLoose2010->SetMaxNsigmaToVertex(4.0);
-  */
-  /* standard cuts ITS-TPC 2010 */
-  AliESDtrackCuts* esdTrackCutsStd2010 = new AliESDtrackCuts("AliESDtrackCuts", "Standard2010");
-  // TPC  
-  esdTrackCutsStd2010->SetMinNClustersTPC(70); 
-  esdTrackCutsStd2010->SetMaxChi2PerClusterTPC(4);
-  esdTrackCutsStd2010->SetAcceptKinkDaughters(kFALSE); 
-  esdTrackCutsStd2010->SetRequireTPCRefit(kTRUE);
-  // ITS
-  esdTrackCutsStd2010->SetRequireITSRefit(kTRUE);
-  esdTrackCutsStd2010->SetClusterRequirementITS(AliESDtrackCuts::kSPD,
-					 AliESDtrackCuts::kAny);
-  esdTrackCutsStd2010->SetMaxDCAToVertexXYPtDep("0.0182+0.0350/pt^1.01");//selects primaries
-  esdTrackCutsStd2010->SetMaxDCAToVertexZ(2);
-  esdTrackCutsStd2010->SetDCAToVertex2D(kFALSE);
-  esdTrackCutsStd2010->SetRequireSigmaToVertex(kFALSE);
-
+    //Define track cut set
+  AliESDtrackCuts* esdTrackCuts = new AliESDtrackCuts("AliESDtrackCuts", "TrackCutsTOFqa"); 
   AliAnalysisFilter* trackFilter = new AliAnalysisFilter("trackFilter");
-  trackFilter->AddCuts(esdTrackCutsStd2010);
-  task->SetTrackFilter(trackFilter);
-   
+
+  if ( (trackCutSetTOFqa<0) || (trackCutSetTOFqa>=AliAnalysisTaskTOFqaID::kNCutSetTOFqa) ) trackCutSetTOFqa = 0;
+
+  if ( trackCutSetTOFqa == AliAnalysisTaskTOFqaID::kRun1Cuts ) {
+    //use track cuts used for QA during run1 (before July 2014)
+    esdTrackCuts->SetMinNClustersTPC(70); 
+    esdTrackCuts->SetMaxChi2PerClusterTPC(4);
+    esdTrackCuts->SetAcceptKinkDaughters(kFALSE); 
+    esdTrackCuts->SetRequireTPCRefit(kTRUE);
+    esdTrackCuts->SetRequireITSRefit(kTRUE);
+    esdTrackCuts->SetClusterRequirementITS(AliESDtrackCuts::kSPD,
+					   AliESDtrackCuts::kAny);
+    esdTrackCuts->SetMaxDCAToVertexXYPtDep("0.0182+0.0350/pt^1.01");//selects primaries
+    esdTrackCuts->SetMaxDCAToVertexZ(2);
+    esdTrackCuts->SetDCAToVertex2D(kFALSE);
+    esdTrackCuts->SetRequireSigmaToVertex(kFALSE);
+  } else {
+    if ( trackCutSetTOFqa == AliAnalysisTaskTOFqaID::kStd2010 ) esdTrackCuts->GetStandardITSTPCTrackCuts2010(kTRUE,0);
+    if ( trackCutSetTOFqa == AliAnalysisTaskTOFqaID::kStd2010crossedRows ) esdTrackCuts->GetStandardITSTPCTrackCuts2010(kTRUE,1);
+    if ( trackCutSetTOFqa == AliAnalysisTaskTOFqaID::kStd2011) esdTrackCuts->GetStandardITSTPCTrackCuts2011(kTRUE,0); 
+    if ( trackCutSetTOFqa == AliAnalysisTaskTOFqaID::kStd2011crossedRows ) esdTrackCuts->GetStandardITSTPCTrackCuts2011(kTRUE,1);
+  }
+  
+  trackFilter->AddCuts(esdTrackCuts);
+  task->SetTrackFilter(trackFilter); 
   
   // Create containers for input/output
   AliAnalysisDataContainer *cInputTOFqa = mgr->CreateContainer("cInputTOFqa",TChain::Class(),AliAnalysisManager::kInputContainer);
-  AliAnalysisDataContainer *cGeneralTOFqa = mgr->CreateContainer("cGeneralTOFqa",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
-  AliAnalysisDataContainer *cTimeZeroTOFqa = mgr->CreateContainer("cTimeZeroTOFqa",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
-   AliAnalysisDataContainer *cPIDTOFqa = mgr->CreateContainer("cPIDTOFqa",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
-   AliAnalysisDataContainer *cPosTracksTOFqa = mgr->CreateContainer("cPosTracksTOFqa",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
-   AliAnalysisDataContainer *cNegTracksTOFqa = mgr->CreateContainer("cNegTracksTOFqa",TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
+  AliAnalysisDataContainer *cGeneralTOFqa = mgr->CreateContainer(Form("cGeneralTOFqa%s",cutName.Data()),TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
+  AliAnalysisDataContainer *cTimeZeroTOFqa = mgr->CreateContainer(Form("cTimeZeroTOFqa%s",cutName.Data()),TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
+  AliAnalysisDataContainer *cPIDTOFqa = mgr->CreateContainer(Form("cPIDTOFqa%s",cutName.Data()),TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
+  AliAnalysisDataContainer *cPosTracksTOFqa = mgr->CreateContainer(Form("cPosTracksTOFqa%s",cutName.Data()),TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
+  AliAnalysisDataContainer *cNegTracksTOFqa = mgr->CreateContainer(Form("cNegTracksTOFqa%s",cutName.Data()),TList::Class(),AliAnalysisManager::kOutputContainer,Form("%s:TOF_Performance",mgr->GetCommonFileName()));
 
   // Attach i/o
   mgr->ConnectInput(task, 0,mgr->GetCommonInputContainer());
