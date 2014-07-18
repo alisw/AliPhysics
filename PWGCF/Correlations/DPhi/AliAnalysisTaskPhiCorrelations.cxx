eeba11669b84f3775e965f3eec26b362ec679681
mfloris <michele.floris@cern.ch>
2014-07-18 22:19:55 +0200

    Corrected getters after renaming

--- PWGCF/Correlations/DPhi/AliAnalysisTaskPhiCorrelations.cxx
+++ PWGCF/Correlations/DPhi/AliAnalysisTaskPhiCorrelations.cxx
@@ -1588,7 +1588,7 @@ Bool_t AliAnalysisTaskPhiCorrelations::IsMuEvent(){
   
   if(!fAOD)
     AliFatal("Muon selection only implemented on AOD");//FIXME to be implemented also for ESDs as in AliAnalyseLeadingTrackUE::GetAcceptedPArticles
-  for (Int_t iTrack = 0; iTrack < fAOD->GetNTracks(); iTrack++) {
+  for (Int_t iTrack = 0; iTrack < fAOD->GetNumberOfTracks(); iTrack++) {
     AliAODTrack* track = fAOD->GetTrack(iTrack);
     if (!track->IsMuonTrack()) continue;
     //Float_t dca    = track->DCA();
