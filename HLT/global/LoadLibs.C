6bd29847a8b7d9a53d30cf02e8d8387a9a6757b0
zampolli <chiara.zampolli@cern.ch>
2014-07-29 09:10:39 +0200

    Adding include path

--- HLT/global/LoadLibs.C
+++ HLT/global/LoadLibs.C
@@ -13,6 +13,8 @@
 
 void LoadLibs() {
 
+  gSystem->SetIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_ROOT/ITS -I$ALICE_ROOT/TPC -I$ALICE_ROOT/CONTAINERS -I$ALICE_ROOT/STEER/STEER -I$ALICE_ROOT/STEER/STEERBase -I$ALICE_ROOT/STEER/ESD -I$ALICE_ROOT/STEER/AOD -I$ALICE_ROOT/TRD -I$ALICE_ROOT/macros -I$ALICE_ROOT/ANALYSIS  -I$ALICE_ROOT/OADB -g"); 
+
   gSystem->Load("libHLTbase.so");
   gSystem->Load("libAliHLTUtil.so");
   gSystem->Load("libAliHLTTPC.so");
