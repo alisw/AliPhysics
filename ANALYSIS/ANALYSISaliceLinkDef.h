0c32fd45d3da322d7e00e66b8f17a75da3e4f679
zampolli <chiara.zampolli@cern.ch>
2014-07-31 15:05:03 +0200

    Moving test task to TPC folder

--- ANALYSIS/ANALYSISaliceLinkDef.h
+++ ANALYSIS/ANALYSISaliceLinkDef.h
@@ -50,7 +50,6 @@
 
 #pragma link C++ class AliAnalysisUtils+;
 #pragma link C++ class AliAnalysisTaskBadChunkID+;
-#pragma link C++ class  AliAnalysisTaskPt+;
 
 #ifdef WITHXML
 #pragma link C++ class AliTagAnalysis+;
