b15f037d0f186c4db30348a3c64578b4e9efd16b
sgorbuno <Sergey.Gorbunov@cern.ch>
2014-08-04 10:49:01 +0200

    AliFLatESDTrigger added

---
+++ HLT/global/AliFlatESDTrigger.h
@@ -0,0 +1,97 @@
+#ifndef ALIFLATESDTRIGGER_H
+#define ALIFLATESDTRIGGER_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               *
+ * Primary Authors : Sergey Gorbunov, Jochen Thaeder, Chiara Zampolli     */
+
+/*
+ * See implementation file for documentation
+ */
+
+/*
+*/
+
+#include "Rtypes.h"
+#include <string>
+
+class AliFlatESDTrigger{
+
+ public:
+
+  // --------------------------------------------------------------------------------
+  // -- Constructor / Destructors
+  AliFlatESDTrigger();   
+  ~AliFlatESDTrigger();
+
+  // --------------------------------------------------------------------------------
+  // -- Fill / Set methods
+
+  Int_t SetTriggerClass(  const char *TriggerClassName, Int_t TriggerIndex, ULong64_t MaxSize );
+  
+  // --------------------------------------------------------------------------------
+  // -- Getter methods
+
+  Int_t GetTriggerIndex() const { return fTriggerIndex; } 
+
+  const Char_t *GetTriggerClassName() const { return reinterpret_cast<const Char_t*>( fContent ); } 
+  
+  // --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  --  
+  
+  AliFlatESDTrigger *GetNextTrigger() { return reinterpret_cast<AliFlatESDTrigger*>(fContent+fContentSize); }
+  
+  // --------------------------------------------------------------------------------
+  // -- Size methods
+
+  ULong64_t GetSize()  {return fContent + fContentSize -  reinterpret_cast<Byte_t*>(this) ;}
+    
+ private:
+
+  AliFlatESDTrigger(const AliFlatESDTrigger&);
+  AliFlatESDTrigger& operator=(const AliFlatESDTrigger&);
+
+  // --------------------------------------------------------------------------------
+  // -- Fixed size member objects
+  
+  UInt_t fContentSize;                      // Size of this object
+  Int_t  fTriggerIndex; // trigger index  
+   
+  // --------------------------------------------------------------------------------
+  // -- Variable Size Object
+
+  Byte_t fContent[1];                  // Variale size object, which contains all data
+
+};
+
+
+// _______________________________________________________________________________________________________
+// inline implementation of some methods 
+
+inline AliFlatESDTrigger::AliFlatESDTrigger():
+  fContentSize(1),
+  fTriggerIndex(0)
+{   
+  // Default constructor
+  fContent[0] = '\0';
+}
+
+inline AliFlatESDTrigger::~AliFlatESDTrigger() 
+{
+  // Destructor  
+}
+
+inline Int_t AliFlatESDTrigger::SetTriggerClass(  const char *TriggerClassName, Int_t TriggerIndex, ULong64_t MaxSize )
+{
+  // Set trigger class, returns non-zero when the memory needed exeeeds MaxSize
+
+  size_t len = strlen( TriggerClassName );
+
+  if( ( fContent + len ) > reinterpret_cast<Byte_t*>(this) + MaxSize ) return -1;
+  
+  fTriggerIndex = TriggerIndex;
+  fContentSize = len;
+  strcpy( reinterpret_cast<char*>(fContent), TriggerClassName );
+  return 0;
+}
+
+#endif
