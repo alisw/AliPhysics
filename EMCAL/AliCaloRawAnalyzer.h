e924347c4ad774b45189db7ac3e6ee22aad9f87a
gconesab <gustavo.conesa.balbastre@cern.ch>
2014-08-20 12:22:01 +0200

    remove unnecessary consts, add spaces or change comments

--- EMCAL/AliCaloRawAnalyzer.h
+++ EMCAL/AliCaloRawAnalyzer.h
@@ -43,52 +43,65 @@ public:
   virtual ~AliCaloRawAnalyzer() { ; }
 
   virtual AliCaloFitResults Evaluate( const std::vector<AliCaloBunchInfo> &/*bunchvector*/, 
-  				      const UInt_t /*altrocfg1*/,  const UInt_t /*altrocfg2*/ )  = 0;
+                                      UInt_t /*altrocfg1*/,  UInt_t /*altrocfg2*/ )  = 0;
 
   static void PrintBunches( const std::vector<AliCaloBunchInfo> &bunchvector );
-  static void PrintBunch( const AliCaloBunchInfo &bunch );
+  static void PrintBunch  ( const AliCaloBunchInfo &bunch );
   
   int PreFitEvaluateSamples( const std::vector<AliCaloBunchInfo>  &bunchvector, 
-				     const UInt_t altrocfg1,  const UInt_t altrocfg2, Int_t & index, 
-				     Float_t & maxf, short & maxamp, short & maxampindex, 
-				    Float_t & ped, int & first, int & last, const int acut);
+                             UInt_t altrocfg1, UInt_t altrocfg2, Int_t & index,
+                             Float_t & maxf, short & maxamp, short & maxampindex,
+                             Float_t & ped, int & first, int & last, int acut);
   
-  void SetTimeConstraint(const int min, const int max );
-  void SetVerbose(bool verbose = true){ fVerbose = verbose; };
-  void SetIsZeroSuppressed(const bool iszs = true) { fIsZerosupressed = iszs; } ;
-  void SetAmpCut(const Float_t cut) { fAmpCut = cut ; } ;
-  void SetFitArrayCut(const Int_t cut) { fFitArrayCut = cut ; } ;
-  void SetNsampleCut(const Int_t cut) { fNsampleCut = cut ; } ;
-  void SetOverflowCut(const Int_t cut) { fOverflowCut = cut ; } ;
-  void SetNsamplePed(const Int_t i) { fNsamplePed = i ; } ;
+  void SetTimeConstraint  (int min, int max );
+  void SetVerbose         (bool verbose = true){ fVerbose     = verbose; }
+  void SetIsZeroSuppressed(bool iszs = true)   { fIsZerosupressed = iszs ; }
+  void SetAmpCut     (Float_t cut)             { fAmpCut      = cut ; }
+  void SetFitArrayCut(Int_t cut)               { fFitArrayCut = cut ; }
+  void SetNsampleCut (Int_t cut)               { fNsampleCut  = cut ; }
+  void SetOverflowCut(Int_t cut)               { fOverflowCut = cut ; }
+  void SetNsamplePed (Int_t i)                 { fNsamplePed  = i   ; }
+  void SetL1Phase    (Double_t phase)          { fL1Phase     = phase ; }
 
-  bool GetIsZeroSuppressed() const { return fIsZerosupressed;} ;
-  Float_t GetAmpCut() const { return fAmpCut; } ;
-  Int_t GetFitArrayCut() const { return fFitArrayCut; } ;
-  Int_t GetNsampleCut() const { return fNsampleCut; } ;
-  Int_t GetOverflowCut() const { return fOverflowCut; } ;
-  Int_t GetNsamplePed() const { return fNsamplePed; } ;
+  bool    GetIsZeroSuppressed() const { return fIsZerosupressed;}
+  Float_t GetAmpCut()      const { return fAmpCut      ; }
+  Int_t   GetFitArrayCut() const { return fFitArrayCut ; }
+  Int_t   GetNsampleCut()  const { return fNsampleCut  ; }
+  Int_t   GetOverflowCut() const { return fOverflowCut ; }
+  Int_t   GetNsamplePed()  const { return fNsamplePed  ; }
 
   // access to array info
   Double_t GetReversed(const int i) const { return fReversed[i]; }
-  const char * GetAlgoName() const { return fName;  };
-  const char * GetAlgoAbbr() const { return fNameShort;  };
-  Algo::fitAlgorithm GetAlgo() const { return fAlgo; };
+  const char * GetAlgoName()   const { return fName      ; }
+  const char * GetAlgoAbbr()   const { return fNameShort ; }
+  Algo::fitAlgorithm GetAlgo() const { return fAlgo      ; }
 
   Double_t CalculateChi2(const Double_t amp, const Double_t time,
-			 const Int_t first, const Int_t last,
-			 const Double_t adcErr=1, 
-			 const Double_t tau=2.35) const;
+                         const Int_t first, const Int_t last,
+                         const Double_t adcErr=1,
+                         const Double_t tau=2.35) const;
+  
   void CalculateMeanAndRMS(const Int_t first, const Int_t last,
-			   Double_t & mean, Double_t & rms);
-  void SetL1Phase(const Double_t phase) {fL1Phase = phase;};
-  short Max( const AliCaloBunchInfo *const bunch, int *const maxindex) const;
+                           Double_t & mean, Double_t & rms);
+  
+  short Max( const AliCaloBunchInfo *const bunch, int * maxindex) const;
+  
   UShort_t Max(const UShort_t *data, const int length ) const;
+  
   bool CheckBunchEdgesForMax( const AliCaloBunchInfo *const bunch) const;
+  
   bool IsInTimeRange( const int maxindex, const int maxtime, const int mintime ) const;
-  Float_t  ReverseAndSubtractPed( const AliCaloBunchInfo *bunch, const UInt_t altrocfg1,  const UInt_t altrocfg2, double *outarray ) const;
-  int  SelectBunch( const std::vector<AliCaloBunchInfo> &bunchvector, short *const maxampbin, short *const maxamplitude );
-  void SelectSubarray( const Double_t *date, const int length, const short maxindex, int *const  first, int *const last, const int cut) const;
+  
+  Float_t  ReverseAndSubtractPed( const AliCaloBunchInfo *bunch,
+                                  UInt_t altrocfg1,  UInt_t altrocfg2,
+                                  double *  outarray ) const;
+  
+  int  SelectBunch( const std::vector<AliCaloBunchInfo> &bunchvector,
+                    short * maxampbin, short * maxamplitude );
+  
+  void SelectSubarray( const Double_t *date, int length, short maxindex,
+                       int * first, int * last, int cut) const;
+  
   Float_t EvaluatePedestal(const UShort_t * const data, const int length ) const;
   
   // Used in AliCaloRawAnalyzerFitter
@@ -107,10 +120,13 @@ protected:
   int fOverflowCut; // value when ADC starts to saturate
   int fNsamplePed;   //Number of samples used for pedestal calculation (first in bunch) 
   bool fIsZerosupressed; //Wether or not the data is zeros supressed, by default its assumed that the baseline is also subtracted if set to true
+  
   bool fVerbose;     //Print debug information to std out if set to true
   char fName[256]; // Name of the algorithm
   char fNameShort[256]; // Abbrevation for the name
+  
   Algo::fitAlgorithm fAlgo; // Which algorithm to use
+
   Double_t fL1Phase; // Phase of the ADC sampling clock relative to the LHC clock
   Double_t fAmp;     // The amplitude in entities of ADC counts
   Double_t fTof;     // The amplitude in entities of ADC counts
