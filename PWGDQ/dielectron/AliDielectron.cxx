efa590e36003365ea425073e8cfcb420d5ae8523
jbook <jbook>
2014-08-22 10:59:22 +0200

    remove different handling of SE and ME for AOD wrt. KFusage

--- PWGDQ/dielectron/AliDielectron.cxx
+++ PWGDQ/dielectron/AliDielectron.cxx
@@ -1193,9 +1193,6 @@ void AliDielectron::FillPairArrays(Int_t arr1, Int_t arr2)
 
   AliDielectronPair *candidate=new AliDielectronPair;
   candidate->SetKFUsage(fUseKF);
-  // switch OFF the KF usage in case of AODs && ME (since there is no MoveToSameVertex functionality)
-  Bool_t isESD=(AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler()->IsA()==AliESDInputHandler::Class());
-  if(!isESD && pairIndex>AliDielectron::kEv1MM) candidate->SetKFUsage(kFALSE);
 
   UInt_t selectedMask=(1<<fPairFilter.GetCuts()->GetEntries())-1;
   
@@ -1244,7 +1241,6 @@ void AliDielectron::FillPairArrays(Int_t arr1, Int_t arr2)
       //get a new candidate
       candidate=new AliDielectronPair;
       candidate->SetKFUsage(fUseKF);
-      if(!isESD && pairIndex>AliDielectron::kEv1MM) candidate->SetKFUsage(kFALSE);
     }
   }
   //delete the surplus candidate
