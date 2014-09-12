6ebf39d30028da73be3f8fcb5909bb6a4927f12b
sjena <sjena@cern.ch>
2014-09-12 15:07:12 +0200

    Update to add PidRatio: sjena

--- PWGCF/PWGCFebyeLinkDef.h
+++ PWGCF/PWGCFebyeLinkDef.h
@@ -44,6 +44,13 @@
 #pragma link C++ class AliEbyEPidTaskFastGen+;
 #pragma link C++ class AliEbyEPidTTask+;
 #pragma link C++ class AliEbyEPidTTaskMC+;
+#pragma link C++ class AliEbyEPidRatioBase+;
+#pragma link C++ class AliEbyEPidRatioDCA+;
+#pragma link C++ class AliEbyEPidRatioQA+;
+#pragma link C++ class AliEbyEPidRatioPhy+;
+#pragma link C++ class AliEbyEPidRatioEffCont+;
+#pragma link C++ class AliEbyEPidRatioHelper+;
+#pragma link C++ class AliEbyEPidRatioTask+;
 
 #pragma link C++ class AliAnalysisNetParticleBase+;
 #pragma link C++ class AliAnalysisNetParticleDCA+;
