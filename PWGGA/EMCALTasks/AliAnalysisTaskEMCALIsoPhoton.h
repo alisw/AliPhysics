d21086594bb26b45536ae6f904bd68fa3859b490
mcosenti <mcosenti@cern.ch>
2014-09-15 14:35:59 -0300

    including option to apply a cut in the distance to bad channel of clusters; changing the default values of the histogram limits from -0.25-99.75 GeV to 0.00-100.00 GeV

--- PWGGA/EMCALTasks/AliAnalysisTaskEMCALIsoPhoton.h
+++ PWGGA/EMCALTasks/AliAnalysisTaskEMCALIsoPhoton.h
@@ -75,7 +75,8 @@ class AliAnalysisTaskEMCALIsoPhoton : public AliAnalysisTaskSE {
   void                  SetMinIsoClusE(Double_t emin)           { fMinIsoClusE        = emin;    }
   void                  SetTrCoreRemoval(Bool_t b)              { fTrCoreRem          = b;       }
   void                  SetClusTDiff(Double_t diff)             { fClusTDiff          = diff;    }
-  void                  SetPileUpRejSPD()                       { fPileUpRejSPD       = kTRUE;  }
+  void                  SetPileUpRejSPD()                       { fPileUpRejSPD       = kTRUE;   }
+  void                  SetDistanceToBadCh(Double_t d)          { fDistToBadChan      = d;       }
  protected:
   TObjArray             *fESDClusters;           //!pointer to EMCal clusters
   TObjArray             *fAODClusters;           //!pointer to EMCal clusters
@@ -123,6 +124,7 @@ class AliAnalysisTaskEMCALIsoPhoton : public AliAnalysisTaskSE {
   Bool_t                 fTrCoreRem;             // flag to set the removal of the core in track isolation (true removes it, default)
   Double_t               fClusTDiff;             // variable to hold the time diff between the candidate cluster and the isolation clusters
   Bool_t                 fPileUpRejSPD;          // flag to set pile-up rejection via SPD (multiple vertices)
+  Double_t               fDistToBadChan;         // distance to bad channel
   
  private:
   AliESDEvent *fESD;      //! ESD object
