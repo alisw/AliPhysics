5b17ded1953e4df2479fd075e0219f0872f20ecb
mcosenti <mcosenti@cern.ch>
2014-10-13 16:03:15 -0300

    more fixes in the addtask

--- PWGGA/EMCALTasks/macros/AddTaskEMCALPi0V2ShSh.C
+++ PWGGA/EMCALTasks/macros/AddTaskEMCALPi0V2ShSh.C
@@ -2,50 +2,48 @@
 AliAnalysisTaskEMCALPi0V2ShSh *AddTaskEMCALPi0V2ShSh()
 {
 
-	AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-	
-	if (!mgr)
-	{
-		::Error("AddTaskEMCALPi0V2ShSh", "No analysis manager to connect to.");
-		return NULL;
-  	}  
-  	
-  	if (!mgr->GetInputEventHandler())
-	{
-		::Error("AddTaskEventplane", "This task requires an input event handler");
-		return NULL;
-	}		
-	TString inputDataType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
-
-	Bool_t ismc=kFALSE;
-  	ismc = (mgr->GetMCtruthEventHandler())?kTRUE:kFALSE; 
-  	cout<<"AddTaskEMCALPi0V2ShSh - MC config is: "<<ismc<<endl;
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   
-	if (ismc) return 0;
-	
-	UInt_t physMB = 0;
-	UInt_t physJet = 0;
-	UInt_t physGam = 0;
-	UInt_t physEMC = 0; 
-		
-	TString sGeomName = AliEMCALGeometry::GetDefaultGeometryName();
-	
-	AliAnalysisTaskEMCALPi0V2ShSh *task = new AliAnalysisTaskEMCALPi0V2ShSh("EMCALPi0ShowerShV2");
-
-	task->SelectCollisionCandidates(AliVEvent::kSemiCentral);
-
-  	if(!ismc)
-	{		
-		RequestMemory(task, 250*1024);
-		mgr->AddTask(task);
-
-		AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
-		AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("hist", TList::Class(),AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-			
-		mgr->ConnectInput(task, 0, cinput);
-		mgr->ConnectOutput(task, 1, coutput1);
-		
-  	}
-  	return task;
+  if (!mgr)
+    {
+      ::Error("AddTaskEMCALPi0V2ShSh", "No analysis manager to connect to.");
+      return NULL;
+    }  
+  
+  if (!mgr->GetInputEventHandler())
+    {
+      ::Error("AddTaskEventplane", "This task requires an input event handler");
+      return NULL;
+    }		
+  TString inputDataType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+  
+  Bool_t ismc=kFALSE;
+  ismc = (mgr->GetMCtruthEventHandler())?kTRUE:kFALSE; 
+  cout<<"AddTaskEMCALPi0V2ShSh - MC config is: "<<ismc<<endl;
+  
+  if (ismc) return 0;
+  
+  UInt_t physMB = 0;
+  UInt_t physJet = 0;
+  UInt_t physGam = 0;
+  UInt_t physEMC = 0; 
+  
+  TString sGeomName = AliEMCALGeometry::GetDefaultGeometryName();
+  
+  AliAnalysisTaskEMCALPi0V2ShSh *task = new AliAnalysisTaskEMCALPi0V2ShSh("EMCALPi0ShowerShV2");
+  
+  task->SelectCollisionCandidates(AliVEvent::kSemiCentral);
+  
+  if(!ismc)
+    {		
+      mgr->AddTask(task);
+      
+      AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("hist", TList::Class(),AliAnalysisManager::kOutputContainer, Form("%s",AliAnalysisManager::GetCommonFileName()));
+      
+      mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+      mgr->ConnectOutput(task, 1, coutput1);
+      
+    }
+  return task;
 }
 
