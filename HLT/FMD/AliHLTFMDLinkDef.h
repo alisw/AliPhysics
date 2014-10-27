7d9e09ed17a270bf8e921437fe7b2b98867f9758
agrigora <alina.grigoras@cern.ch>
2014-10-27 11:08:28 +0100

    AliHLTFMD module

--- HLT/FMD/AliHLTFMDLinkDef.h
+++ HLT/FMD/AliHLTFMDLinkDef.h
@@ -1,9 +1,11 @@
-
-#ifdef __CINT__
+#if !defined(__CINT__) && !defined(__CLING__)
+# error Not for compilation
+#else 
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class  AliHLTFMDReconstructionComponent+;
-#pragma link C++ class  AliHLTAgentFMD+;
-#pragma link C++ class  AliHLTFMDReconstructor+;
-#endif
+
+#pragma link C++ class AliHLTFMDReconstructionComponent+;
+#pragma link C++ class AliHLTAgentFMD+;
+
+#endif // __CINT__
