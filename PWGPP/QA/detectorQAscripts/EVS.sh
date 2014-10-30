366cb689697e933cf9d0bf4605fdc9bb221a02b2
ekryshen <evgeny.kryshen@cern.ch>
2014-10-30 15:53:01 +0100

    Physics selection QA added in the QA framework

---
+++ PWGPP/QA/detectorQAscripts/EVS.sh
@@ -0,0 +1,15 @@
+runLevelEventStatQA()
+{
+  eventStatFile=$1
+
+  cp $ALICE_ROOT/PWGPP/EVS/runLevelEventStatQA.C .
+  aliroot -b -q -l "runLevelEventStatQA.C(\"$eventStatFile\",${runNumber})" 
+}
+
+periodLevelQA()
+{
+  trendingFile=$1
+
+  cp $ALICE_ROOT/PWGPP/EVS/periodLevelQA.C .
+  aliroot -b -q -l "periodLevelQA.C(\"trending.root\")"
+}
