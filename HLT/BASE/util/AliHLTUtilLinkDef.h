a64d42f036b28c738c57626abfe523e7ef95c6ed
agrigora <alina.grigoras@cern.ch>
2014-11-04 16:17:16 +0100

    HLT submodules

---
+++ HLT/BASE/util/AliHLTUtilLinkDef.h
@@ -0,0 +1,39 @@
+// Auto generated file - do not edit
+#if !defined(__CINT__) && !defined(__CLING__)
+# error Not for compilation
+#else 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTFilePublisher+;
+#pragma link C++ class AliHLTFileWriter+;
+#pragma link C++ class AliHLTMCEvent+;
+#pragma link C++ class AliHLTJets+;
+#pragma link C++ class AliHLTRootFilePublisherComponent+;
+#pragma link C++ class AliHLTESDMCEventPublisherComponent+;
+#pragma link C++ class AliHLTRootFileWriterComponent+;
+#pragma link C++ class AliHLTRootFileStreamerComponent+;
+#pragma link C++ class AliHLTRootSchemaEvolutionComponent+;
+#pragma link C++ class AliHLTRecoParamComponent+;
+#pragma link C++ class AliHLTLoaderPublisherComponent+;
+#pragma link C++ class AliHLTRawReaderPublisherComponent+;
+#pragma link C++ class AliHLTOUTPublisherComponent+;
+#pragma link C++ class AliHLTEsdCollectorComponent+;
+#pragma link C++ class AliHLTReadoutListDumpComponent+;
+#pragma link C++ class AliHLTDataGenerator+;
+#pragma link C++ class AliHLTBlockFilterComponent+;
+#pragma link C++ class AliHLTMonitoringRelay+;
+#pragma link C++ class AliHLTObjectCompressionComponent+;
+#pragma link C++ class AliHLTCompStatCollector+;
+#pragma link C++ class AliHLTGlobalBarrelTrack+;
+#pragma link C++ class AliHLTAgentUtil+;
+#pragma link C++ class AliHLTESDCaloClusterMaker+;
+#pragma link C++ class AliHLTCaloClusterReader+;
+#pragma link C++ class AliHLTCorruptorComponent+;
+#pragma link C++ class AliHLTESDTrackCuts+;
+
+#endif // __CINT__
+//
+// EOF
+//
