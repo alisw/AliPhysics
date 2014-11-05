5f258abeb8fbdc225dd37236ce27488999b1061b
agrigora <alina.grigoras@cern.ch>
2014-11-05 10:50:55 +0100

    HLTsim

---
+++ HLT/sim/HLTsimLinkDef.h
@@ -0,0 +1,16 @@
+// Auto generated file - do not edit
+#if !defined(__CINT__) && !defined(__CLING__)
+# error Not for compilation
+#else 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTSimulation+;
+#pragma link C++ class AliHLTAgentSim+;
+#pragma link C++ class AliHLTOUTComponent+;
+
+#endif // __CINT__
+//
+// EOF
+//
