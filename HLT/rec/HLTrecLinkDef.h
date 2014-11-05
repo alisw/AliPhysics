883b3ce5f06b198365d974eaee2fa02ac3c76496
agrigora <alina.grigoras@cern.ch>
2014-11-05 10:38:03 +0100

    HLTrec

---
+++ HLT/rec/HLTrecLinkDef.h
@@ -0,0 +1,21 @@
+// Auto generated file - do not edit
+#if !defined(__CINT__) && !defined(__CLING__)
+# error Not for compilation
+#else 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTReconstructor+;
+#pragma link C++ class AliRawReaderHLT+;
+#pragma link C++ class AliHLTOUTHomerCollection+;
+#pragma link C++ class AliHLTEsdManagerImplementation+;
+#pragma link C++ class AliHLTDAQInterfaceImplementation+;
+#pragma link C++ class AliHLTOUTDigitReader+;
+#pragma link C++ class AliHLTMiscImplementation+;
+#pragma link C++ class AliHLTOUTRawReader+;
+
+#endif // __CINT__
+//
+// EOF
+//
