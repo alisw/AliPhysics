f9832a45a9c4a85b0e855697e3933ead2225c3d4
hristov <Peter.Hristov@cern.ch>
2014-11-10 16:47:08 +0100

    Obsolete classes with duplicated names are commented out

--- HLT/ITS/AliHLTITSLinkDef.h
+++ HLT/ITS/AliHLTITSLinkDef.h
@@ -5,8 +5,8 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class AliHLTITStrack+;
-#pragma link C++ class AliHLTITStracker+;
+/* #pragma link C++ class AliHLTITStrack+; */
+/* #pragma link C++ class AliHLTITStracker+; */
 #pragma link C++ class AliHLTITSVertexerZ+;
 #pragma link C++ class AliHLTITSclusterer+;
 #pragma link C++ class AliHLTITSAgent+;
