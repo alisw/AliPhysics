d8e39fa9abb8af80108b54dd6b67fc8ccc22fec6
jniedzie <jeremi.niedziela@cern.ch>
2014-11-11 11:57:19 +0100

    Source variables before starting reco on remote machine.

--- MONITOR/alistoragemanager/setupStorageDatabase.sh
+++ MONITOR/alistoragemanager/setupStorageDatabase.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 HOST="localhost"   # IP of machine on which mysql database is located
-PORT="5055"
-DATABASE="storage"
-USER="storage"
-PASS="storage123"
-TABLE="events"
-STORAGE_PATH="/Users/Jerus/storedFiles"
+PORT="123"
+DATABASE="database"
+USER="user"
+PASS="pass123"
+TABLE="table"
+STORAGE_PATH="/some/path/"
 MAX_SIZE="300000000"
 MAX_OCCUPATION="100"
 REMOVE_PERCENT="50"
 EVENTS_IN_FILE="20"
-EVENT_SERVER="pcald39fix"   # IP of machine running alieventserver
-EVENT_SERVER_USER="edis"
-EVENT_SERVER_PORT="5024"
+EVENT_SERVER="localhost"   # IP of machine running alieventserver
+EVENT_SERVER_USER="user"
+EVENT_SERVER_PORT="124"
 STORAGE_SERVER="localhost"      # IP of machine running alistorage
-STORAGE_SERVER_USER="edis"
-STORAGE_SERVER_PORT="5066"      # server thread communication port
-STORAGE_CLIENT_PORT="5088"      # client thread communication port
-XML_SERVER_PORT="5099"          # server of xml files
-mysql -u root -pdaq -e "create database if not exists $DATABASE;"
-mysql -u root -pdaq -e "grant ALL PRIVILEGES on $DATABASE.* to '$USER'@'$HOST' identified by '$PASS';"
-mysql -u root -pdaq -e "use $DATABASE;"
-mysql -u root -pdaq -e "CREATE TABLE IF NOT EXISTS $DATABASE.$TABLE(\
+STORAGE_SERVER_USER="user"
+STORAGE_SERVER_PORT="125"      # server thread communication port
+STORAGE_CLIENT_PORT="126"      # client thread communication port
+XML_SERVER_PORT="127"          # server of xml files
+mysql -u root -ppassword -e "create database if not exists $DATABASE;"
+mysql -u root -ppassword -e "grant ALL PRIVILEGES on $DATABASE.* to '$USER'@'$HOST' identified by '$PASS';"
+mysql -u root -ppassword -e "use $DATABASE;"
+mysql -u root -ppassword -e "CREATE TABLE IF NOT EXISTS $DATABASE.$TABLE(\
 run_number int(6) NOT NULL,\
 event_number int(6) NOT NULL,\
 system text(7) DEFAULT NULL,\
