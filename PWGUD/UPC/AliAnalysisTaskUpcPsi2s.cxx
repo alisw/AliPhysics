211104a48a475d35f104d7b3ab56a662b581d1b0
mbroz <Michal.Broz@cern.ch>
2014-11-12 13:12:43 +0100

    Fix of CINT1 lumi histogram

--- PWGUD/UPC/AliAnalysisTaskUpcPsi2s.cxx
+++ PWGUD/UPC/AliAnalysisTaskUpcPsi2s.cxx
@@ -514,7 +514,9 @@ void AliAnalysisTaskUpcPsi2s::RunAODtrig()
   if(trigger.Contains("CCUP2-B")) fHistCcup2TriggersPerRun->Fill(fRunNum); //CCUP2 triggers
   
   if(trigger.Contains("CINT1")) fHistCint1TriggersPerRun->Fill(fRunNum); //CINT1 triggers
-  if(trigger.Contains("CINT1") && trigger.Contains("C0TVX")) fHistC0tvxAndCint1TriggersPerRun->Fill(fRunNum); //C0TVX triggers in CINT1 events
+  
+  fL0inputs = aod->GetHeader()->GetL0TriggerInputs();
+  if(trigger.Contains("CINT1") && (fL0inputs & (1 << 3))) fHistC0tvxAndCint1TriggersPerRun->Fill(fRunNum); //0TVX triggers in CINT1 events
   
   if(trigger.Contains("CVLN_B2-B")) fHistCvlnTriggersPerRun->Fill(fRunNum); //CVLN triggers - synchronously downscaled
   if(trigger.Contains("CVLN_R1-B")) fHistCvlnTriggersPerRun->Fill(fRunNum); //CVLN triggers - randomly downscaled
