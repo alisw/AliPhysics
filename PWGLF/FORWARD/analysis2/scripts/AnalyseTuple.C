26ff307fce4c9b1e32cce8315598551ac6dbf099
cholm <Christian.Holm.Christensen@cern.ch>
2014-11-13 14:53:30 +0100

    New selector to analyse tree from AliFMDMCTrackELoss

---
+++ PWGLF/FORWARD/analysis2/scripts/AnalyseTuple.C
@@ -0,0 +1,13 @@
+void
+AnalyseTuple(Bool_t proof=true, Long64_t maxEvents=-1, const char* opt="")
+{
+  const char* fwd = "$(ALICE_ROOT)/PWGLF/FORWARD/analysis2";
+  gSystem->AddIncludePath("-I${ANA_SRC} -I${ALICE_ROOT}/include");
+  gROOT->Macro(Form("%s/scripts/LoadLibs.C",fwd));
+  gROOT->LoadMacro(Form("%s/scripts/TupleSelector.C+%s",fwd,opt));
+
+  if (proof) TupleSelector::Proof(maxEvents, opt);
+  else       TupleSelector::Run(maxEvents);
+}
+
+#endif
