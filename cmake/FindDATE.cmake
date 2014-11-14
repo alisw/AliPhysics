447ce70e3b251d68c185a7a1703dddb4aee6dd14
hristov <Peter.Hristov@cern.ch>
2014-11-14 17:34:14 +0100

    DATE flags

--- cmake/FindDATE.cmake
+++ cmake/FindDATE.cmake
@@ -17,15 +17,16 @@
 # Requires: 
 #       - date-config has to be found in the PATH
 #       - Environment variables
-#               - DATE_ROOT
-#               - DATE_COMMON_DEFS
-#               - DATE_MONITOR_DIR
+#               - DATE_ROOT // try to not use
+#               - DATE_COMMON_DEFS // try to not use
+#               - DATE_MONITOR_DIR //try not to use
 # - DATE_VERSION - DATE version as reported by date-config
 # - DATE_VERSION_MAJOR
 # - DATE_VERSIOM_MINOR
 # - DATE_CONFIG - path to date-config script
 # - DATE_CFLAGS - cflags reported by date-config
 # - DATE_LDFLAGS - ldflags reported by date-config
+# - DATE_LIBS - DATE libs to be linked against to
 # - DATE_MONLIBS - monitorlibs reported by date-config
 
 find_program(DATE_CONFIG date-config)
@@ -44,33 +45,37 @@ if(DATE_CONFIG)
     message(STATUS "DATE version ${DATE_VERSION_MAJOR}.${DATE_VERSION_MINOR} found.")
     
     # Checking if the environment is properly set
-    if(NOT DEFINED ENV{DATE_ROOT})
-        message(FATAL_ERROR "date-config found. Please set DATE_ROOT environment variable")
-    else()
-        set(DATE_ROOT ENV{DATE_ROOT})
-        message(STATUS "DATE_ROOT found ${DATE_ROOT}")
-    endif()
+#    if(NOT DEFINED ENV{DATE_ROOT})
+#        message(FATAL_ERROR "date-config found. Please set DATE_ROOT environment variable")
+#    else()
+#        set(DATE_ROOT ENV{DATE_ROOT})
+#        message(STATUS "DATE_ROOT found ${DATE_ROOT}")
+#    endif()
 
-    if(NOT DEFINED ENV{DATE_COMMON_DEFS})
-        message(FATAL_ERROR "date-config found. Please set DATE_COMMON_DEFS environment variable")
-    else()
-        set(DATE_COMMON_DEFS ENV{DATE_COMMON_DEFS})
-        message(STATUS "DATE_COMMON_DEFS found ${DATE_COMMON_DEFS}")
-    endif()
+#    if(NOT DEFINED ENV{DATE_COMMON_DEFS})
+#        message(FATAL_ERROR "date-config found. Please set DATE_COMMON_DEFS environment variable")
+#    else()
+#        set(DATE_COMMON_DEFS ENV{DATE_COMMON_DEFS})
+#        message(STATUS "DATE_COMMON_DEFS found ${DATE_COMMON_DEFS}")
+#    endif()
 
-    if(NOT DEFINED ENV{DATE_MONITOR_DIR})
-        message(FATAL_ERROR "date-config found. Please set DATE_MONITOR_DIR environment variable")
-    else()
-        set(DATE_MONITOR_DIR ENV{DATE_MONITOR_DIR})
-        message(STATUS "DATE_MONITOR_DIR found ${DATE_MONITOR_DIR}")
-    endif()
+#    if(NOT DEFINED ENV{DATE_MONITOR_DIR})
+#        message(FATAL_ERROR "date-config found. Please set DATE_MONITOR_DIR environment variable")
+#    else()
+#        set(DATE_MONITOR_DIR ENV{DATE_MONITOR_DIR})
+#        message(STATUS "DATE_MONITOR_DIR found ${DATE_MONITOR_DIR}")
+#    endif()
 
     # setting the cflags
     execute_process(COMMAND ${DATE_CONFIG} --cflags OUTPUT_VARIABLE DATE_CFLAGS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE )
     if(error)
         message(FATAL_ERROR "Error retrieving DATE cflags : ${error}")
     endif(error)
-    string(STRIP ${DATE_CFLAGS} DATE_CFLAGS)
+    
+    # If flags not empty we strip them
+    if(DATE_CFLAGS)
+        string(STRIP ${DATE_CFLAGS} DATE_CFLAGS)
+    endif()
     
     set(DATE_CFLAGS "-DALI_DATE ${DATE_CFLAGS}")
 
@@ -79,20 +84,34 @@ if(DATE_CONFIG)
     if(error)
         message(FATAL_ERROR "Error retrieving DATE ldflags : ${error}")
     endif(error)
-    string(STRIP ${DATE_LDFLAGS} DATE_LDFLAGS)
+    
+    # If the flags are not empty we strip them
+    if(DATE_LDFLAGS)
+        string(STRIP ${DATE_LDFLAGS} DATE_LDFLAGS)
+    endif()
+
+    # setting the libs
+    execute_process(COMMAND ${DATE_CONFIG} --libs OUTPUT_VARIABLE DATE_LIBS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE )
+    if(error)
+        message(FATAL_ERROR "Error retrieving DATE libs : ${error}")
+    endif(error)
+    
+    # If the flags are not empty we strip them
+    if(DATE_LIBS)
+        string(STRIP ${DATE_LIBS} DATE_LIBS)
+    endif()
     
     # setting the monlibs
     execute_process(COMMAND ${DATE_CONFIG} --monitorlibs OUTPUT_VARIABLE DATE_MONLIBS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE )
     if(error)
         message(FATAL_ERROR "Error retrieving DATE monitorlibs : ${error}")
     endif(error)
-    string(STRIP ${DATE_MONLIBS} DATE_MONLIBS)
-
-    # Set the proper compilation flags
-    set(CMAKE_C_FLAGS ${DATE_CFLAGS} ${CMAKE_C_FLAGS})
-    set(CMAKE_CXX_FLAGS ${DATE_CFLAGS} ${CMAKE_CXX_FLAGS})
-    set(CMAKE_SHARED_LINKER_FLAGS ${DATE_LDFLAGS} ${CMAKE_SHARED_LINKER_FLAGS})
     
+    # If the flags are not empty we strip them
+    if(DATE_MONLIBS)
+        string(STRIP ${DATE_MONLIBS} DATE_MONLIBS)
+    endif()
+
     set(DATE_FOUND TRUE)
 else()
     message(STATUS "DATE not found")
