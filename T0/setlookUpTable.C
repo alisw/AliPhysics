d3ee09fa981e3ccf95a5daf4d4785888d18b683c
alla <Alla.Maevskaya@cern.ch>
2014-11-21 11:22:47 +0100

    set LookUp table

---
+++ T0/setlookUpTable.C
@@ -0,0 +1,36 @@
+void setlookUpTable()
+{
+  // Writing Lookup table into the Calibration DB
+  // Arguments:
+
+  TString DBFolder;
+  Int_t firstRun   =  200000;
+  Int_t lastRun    = 9999999;
+  Int_t beamPeriod =  1;
+  char* objFormat = "T0 Lookup Table";
+
+  AliT0CalibData *calibda=new AliT0CalibData("T0");
+
+//  calibda->ReadAsciiLookup("lookUpTable.txt");
+  calibda->ReadAsciiLookup("/home/alla/alice/AliRoot/T0/lookup_run2.txt");
+  cout<<" @@@@ TRM "<<calibda->GetNumberOfTRMs()<<endl;
+  //Store calibration data into database
+  // AliCDBManager::Instance()->SetDefaultStorage("local:///scratch/alla/alice/Aug10/TestCDB");
+   AliCDBManager::Instance()->SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+   
+  //  AliCDBManager::Instance()->SetSpecificStorage("T0",DBFolder.Data());
+
+  AliCDBMetaData md;
+  md.SetComment(objFormat);
+  md.SetBeamPeriod(beamPeriod);
+  md.SetResponsible("Alla");
+  TString fPath="T0/Calib/LookUp_Table";
+
+
+  // AliCDBStorage* storage = AliCDBManager::Instance()->GetSpecificStorage("T0");
+  AliCDBStorage* storage = AliCDBManager::Instance()->GetDefaultStorage();
+  if(storage) {
+    AliCDBId id(fPath.Data(),firstRun,lastRun);
+    storage->Put(calibda, id, &md);
+  }
+}
