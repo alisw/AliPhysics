380710ff3ef6618fb95797f2fca26fa31d5351b4
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2014-11-26 17:08:48 -0200

    Lightweight cascade checks -> code implementation

--- PWGLF/PWGLFSTRANGENESSLinkDef.h
+++ PWGLF/PWGLFSTRANGENESSLinkDef.h
@@ -46,4 +46,6 @@
 #pragma link C++ class AliAnalysisTaskAntiHe4+;
 #pragma link C++ class AliAnalysisTaskNucleiv2+;
 #pragma link C++ class AliAnalysisTaskNucleiv2SP+;
+#pragma link C++ class AliLightV0vertexer+;
+#pragma link C++ class AliLightCascadeVertexer+;
 #endif
