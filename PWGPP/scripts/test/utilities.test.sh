e753d43c1f8faecab21967963e60c7fd621fddb7
mkrzewic <mikolaj.krzewicki@cern.ch>
2014-12-09 17:30:40 +0100

    initial (empty) version of the unit test

---
+++ PWGPP/scripts/test/utilities.test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env/ bash
+#/hera/alice/alienQA/alice/sim/2013/LHC13b4/195391/11/QA_merge_archive.zip
+#/hera/alice/alienQA/alice/sim/2013/LHC13b2_fix_1/195351/QA_merge_archive.zip
+#/hera/alice/alienQA/alice/data/2011/LHC11h_2/000169919/ESDs/limited_trigger_set/QA_merge_archive.zip
+return 0
