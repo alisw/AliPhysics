922e51b0bb71a1455cf637ac9f83c879045ff6eb
mfasel <mfasel@lbl.gov>
2014-12-11 14:35:38 -0800

    Set kine cuts

--- PWGJE/EMCALJetTasks/Tracks/AliEMCalTriggerTaskGroup.h
+++ PWGJE/EMCALJetTasks/Tracks/AliEMCalTriggerTaskGroup.h
@@ -12,6 +12,7 @@ namespace EMCalTriggerPtAnalysis {
 class AliEMCalTriggerAnaTriggerDecision;
 class AliEMCalTriggerBinningComponent;
 class AliEMCalTriggerEventSelection;
+class AliEMCalTriggerKineCuts;
 class AliEMCalTriggerTracksAnalysisComponent;
 
 class AliEMCalTriggerTaskGroup : public TNamed {
@@ -23,6 +24,7 @@ public:
   void SetEventSelection(const AliEMCalTriggerEventSelection *sel){ fEventSelection = sel; }
   void SetGlobalBinning(const AliEMCalTriggerBinningComponent *const binning) { fBinning = binning; }
   void SetTriggerDecision(const AliEMCalTriggerAnaTriggerDecision *trigger);
+  void SetKineCuts(const AliEMCalTriggerKineCuts *cuts) { fKineCuts = cuts; }
   void AddAnalysisComponent(AliEMCalTriggerTracksAnalysisComponent * const analysis);
 
   TList * InitialiseAnalysisComponents();
@@ -32,6 +34,7 @@ protected:
   TObjArray                                   *fAnalysisComponents;
   const AliEMCalTriggerEventSelection         *fEventSelection;
   const AliEMCalTriggerBinningComponent       *fBinning;
+  const AliEMCalTriggerKineCuts               *fKineCuts;
 
   ClassDef(AliEMCalTriggerTaskGroup, 1);    // Group of analysis components with common event selection
 };
