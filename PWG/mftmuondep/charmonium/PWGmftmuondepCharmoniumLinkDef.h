183846ee3431bf75f4eede17d589be8c57ee3dc2
agrigora <alina.grigoras@cern.ch>
2014-12-23 18:16:38 +0100

    PWGmftmuondepCharmonium module 	- Root and AliRoot dependencies 	- Macros installation

--- PWG/mftmuondep/charmonium/PWGmftmuondepCharmoniumLinkDef.h
+++ PWG/mftmuondep/charmonium/PWGmftmuondepCharmoniumLinkDef.h
@@ -4,7 +4,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class AliAnalysisTaskDimuonBackground.cxx
+#pragma link C++ class AliAnalysisTaskDimuonBackground++;
 
 #endif
 
