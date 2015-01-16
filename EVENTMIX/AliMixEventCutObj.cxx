0895f77057cbcf4a7985043f3844d4da0606840c
dberzano <dario.berzano@cern.ch>
2015-01-16 12:12:40 +0100

    Split: Tender*, EventMixing out of ANALYSIS

---
+++ EVENTMIX/AliMixEventCutObj.cxx
@@ -0,0 +1,395 @@
+//
+// Class AliMixEventCutObj
+//
+// AliMixEventCutObj object contains information about one cut on for event mixing
+// used by AliMixEventPool class
+//
+// authors:
+//          Martin Vala (martin.vala@cern.ch)
+//
+
+#include "AliLog.h"
+#include "AliESDEvent.h"
+#include "AliAODEvent.h"
+#include "AliMultiplicity.h"
+#include "AliAODVertex.h"
+#include "AliEventplane.h"
+
+#include "AliMixEventCutObj.h"
+
+ClassImp(AliMixEventCutObj)
+
+//_________________________________________________________________________________________________
+AliMixEventCutObj::AliMixEventCutObj(AliMixEventCutObj::EEPAxis_t type, Float_t min, Float_t max, Float_t step, const char *opt) : TObject(),
+   fCutType((Int_t)type),
+   fCutOpt(opt),
+   fCutMin(min),
+   fCutMax(max),
+   fCutStep(step),
+   fCutSmallVal(0),
+   fCurrentVal(min)
+{
+   //
+   // Default constructor
+   //
+   AliDebug(AliLog::kDebug + 5, "<-");
+   if (fCutStep < 1e-5) AliError("fCutStep is too small !!! This cut will not work !!!");
+   AliDebug(AliLog::kDebug + 5, "->");
+}
+
+//_________________________________________________________________________________________________
+AliMixEventCutObj::AliMixEventCutObj(const AliMixEventCutObj &obj) : TObject(obj),
+   fCutType(obj.fCutType),
+   fCutOpt(obj.fCutOpt),
+   fCutMin(obj.fCutMin),
+   fCutMax(obj.fCutMax),
+   fCutStep(obj.fCutStep),
+   fCutSmallVal(obj.fCutSmallVal),
+   fCurrentVal(obj.fCurrentVal)
+{
+   //
+   // Copy constructor
+   //
+   AliDebug(AliLog::kDebug + 5, "<-");
+   AliDebug(AliLog::kDebug + 5, "->");
+}
+
+//_________________________________________________________________________________________________
+AliMixEventCutObj &AliMixEventCutObj::operator=(const AliMixEventCutObj &obj)
+{
+   //
+   // Assigned operator
+   //
+   if (&obj != this) {
+      TObject::operator=(obj);
+      fCutType = obj.fCutType;
+      fCutOpt = obj.fCutOpt;
+      fCutMin = obj.fCutMin;
+      fCutMax = obj.fCutMax;
+      fCutStep = obj.fCutStep;
+      fCutSmallVal = obj.fCutSmallVal;
+      fCurrentVal = obj.fCurrentVal;
+//       fNoMore = obj.fNoMore;
+   }
+   return *this;
+}
+
+
+//_________________________________________________________________________________________________
+void AliMixEventCutObj::Reset()
+{
+   //
+   // Reset cut
+   //
+   AliDebug(AliLog::kDebug + 5, "<-");
+   fCurrentVal = fCutMin - fCutStep;
+   AliDebug(AliLog::kDebug + 5, "->");
+}
+//_________________________________________________________________________________________________
+Bool_t AliMixEventCutObj::HasMore() const
+{
+   //
+   // Return kTRUE when fCurrentVal is in interval of cut range
+   //
+   return ((fCurrentVal + fCutStep) < fCutMax);
+}
+
+//_________________________________________________________________________________________________
+void AliMixEventCutObj::AddStep()
+{
+   //
+   // Adds step
+   //
+   fCurrentVal += fCutStep;
+}
+
+//_________________________________________________________________________________________________
+void AliMixEventCutObj::Print(const Option_t *) const
+{
+   //
+   // Prints cut information
+   //
+   AliInfo(Form("%s %f %f %f", GetCutName(fCutType), fCutMin, fCutMax, fCutStep));
+}
+//_________________________________________________________________________________________________
+void AliMixEventCutObj::PrintCurrentInterval()
+{
+   //
+   // Prints current cut interval information
+   //
+   AliDebug(AliLog::kDebug, Form("%s <%f,%f>", GetCutName(fCutType), GetCurrentMin(), GetCurrentMax()));
+}
+
+//_________________________________________________________________________________________________
+Int_t AliMixEventCutObj::GetNumberOfBins() const
+{
+   //
+   // Returns number of bins
+   //
+   if (fCutStep < 1e-5) return -1;
+   return (Int_t)((fCutMax - fCutMin) / fCutStep);
+}
+
+//_________________________________________________________________________________________________
+Int_t AliMixEventCutObj::GetBinNumber(Float_t num) const
+{
+   //
+   // Returns bin (index) number in current cut.
+   // Returns -1 in case of out of range
+   //
+   Int_t binNum = 0;
+   for (Float_t iCurrent = fCutMin; iCurrent < fCutMax; iCurrent += fCutStep) {
+      binNum++;
+      if ((num >= iCurrent) && (num < iCurrent + fCutStep - fCutSmallVal)) {
+         return binNum;
+      }
+   }
+   return -1;
+}
+
+//_________________________________________________________________________________________________
+Int_t AliMixEventCutObj::GetIndex(AliVEvent *ev)
+{
+   //
+   // Finds bin (index) in current cut from event information.
+   //
+   return GetBinNumber(GetValue(ev));
+}
+
+//_________________________________________________________________________________________________
+Double_t AliMixEventCutObj::GetValue(AliVEvent *ev)
+{
+   //
+   // Returns value from event
+   //
+
+   AliESDEvent *esd = dynamic_cast<AliESDEvent *>(ev);
+   if (esd) return GetValue(esd);
+   AliAODEvent *aod = dynamic_cast<AliAODEvent *>(ev);
+   if (aod) return GetValue(aod);
+
+   AliFatal("Event is not supported in Event Mixing cuts!!!!");
+   return -99999;
+}
+
+//_________________________________________________________________________________________________
+Double_t AliMixEventCutObj::GetValue(AliESDEvent *ev)
+{
+   //
+   // Returns value from esd event
+   //
+
+   const AliMultiplicity *multESD = 0;
+
+   switch (fCutType) {
+      case kMultiplicity:
+         return (Double_t)ev->GetNumberOfTracks();
+      case kZVertex:
+         return ev->GetVertex()->GetZ();
+      case kNumberV0s:
+         return ev->GetNumberOfV0s();
+      case kNumberTracklets:
+         multESD = ev->GetMultiplicity();
+         if (multESD) return multESD->GetNumberOfTracklets();
+         else AliFatal("esd->GetMultiplicity() is null");
+         break;
+      case kCentrality:
+      {
+         AliCentrality *c = ev->GetCentrality();
+         if (!c) AliFatal("esd->GetCentrality() is null");
+         return c->GetCentralityPercentile(fCutOpt.Data());
+      }
+      case kEventPlane:
+      {
+         AliEventplane *evtPlane = ev->GetEventplane();
+         if (!evtPlane) evtPlane = new AliEventplane();
+         Double_t val = evtPlane->GetEventplane("V0",ev,fCutOpt.Atoi());
+         if (!ev->GetEventplane()) delete evtPlane;
+         return val;
+      }
+      case kEventPlaneV0A:
+      {
+         AliEventplane *evtPlane = ev->GetEventplane();
+         if (!evtPlane) evtPlane = new AliEventplane();
+         Double_t val = evtPlane->GetEventplane("V0A",ev,fCutOpt.Atoi());
+         if (!ev->GetEventplane()) delete evtPlane;
+         return val;
+      }
+      case kEventPlaneV0C:
+      {
+         AliEventplane *evtPlane = ev->GetEventplane();
+         if (!evtPlane) evtPlane = new AliEventplane();
+         Double_t val = evtPlane->GetEventplane("V0C",ev,fCutOpt.Atoi());
+         if (!ev->GetEventplane()) delete evtPlane;
+         return val;
+      }
+   }
+
+   AliFatal("Mixing Cut TYPE is not supported for ESD");
+   return -99999;
+
+}
+
+//_________________________________________________________________________________________________
+Double_t AliMixEventCutObj::GetValue(AliAODEvent *ev)
+{
+   //
+   // Returns value from aod event
+   //
+
+   AliAODVertex *v=0;
+   switch (fCutType) {
+      case kMultiplicity:
+         return (Double_t) ev->GetNumberOfTracks();
+      case kZVertex:
+         v = ev->GetVertex(0);
+         if (!v)  return -99999;
+         return ev->GetVertex(0)->GetZ();
+         // if verttex is null return -9999
+         return -99999;
+      case kNumberV0s:
+         return ev->GetNumberOfV0s();
+      case kCentrality:
+      {
+         AliCentrality *c = ev->GetCentrality();
+         if (!c) AliFatal("esd->GetCentrality() is null");
+         return c->GetCentralityPercentile(fCutOpt.Data());
+      }
+      case kEventPlane:
+      {
+         AliEventplane *evtPlane = ev->GetEventplane();
+         if (!evtPlane) evtPlane = new AliEventplane();
+         Double_t val = evtPlane->GetEventplane("V0A",ev,fCutOpt.Atoi());
+         if (!ev->GetEventplane()) delete evtPlane;
+         return val;
+      }
+      case kEventPlaneV0A:
+      {
+         AliEventplane *evtPlane = ev->GetEventplane();
+         if (!evtPlane) evtPlane = new AliEventplane();
+         Double_t val = evtPlane->GetEventplane("V0A",ev,fCutOpt.Atoi());
+         if (!ev->GetEventplane()) delete evtPlane;
+         return val;
+      }
+      case kEventPlaneV0C:
+      {
+         AliEventplane *evtPlane = ev->GetEventplane();
+         if (!evtPlane) evtPlane = new AliEventplane();
+         Double_t val = evtPlane->GetEventplane("V0C",ev,fCutOpt.Atoi());
+         if (!ev->GetEventplane()) delete evtPlane;
+         return val;
+      }
+   }
+
+   AliFatal("Mixing Cut TYPE is not supported for AOD");
+   return -99999;
+}
+
+//_________________________________________________________________________________________________
+const char *AliMixEventCutObj::GetCutName(Int_t index) const
+{
+   //
+   // Retruns name of cut
+   //
+
+   if (index < 0) index = fCutType;
+   switch (index) {
+      case kMultiplicity:
+         return "Multiplicity";
+      case kZVertex:
+         return "ZVertex";
+      case kNumberV0s:
+         return "NumberV0s";
+      case kNumberTracklets:
+         return "NumberTracklets";
+      case kCentrality:
+         return Form("kCentrality[%s]", fCutOpt.Data());
+      case kEventPlane:
+         return Form("EventPlane[%s]", fCutOpt.Data());
+      case kEventPlaneV0A:
+         return Form("EventPlaneV0A[%s]", fCutOpt.Data());
+      case kEventPlaneV0C:
+         return Form("EventPlaneV0A[%s]", fCutOpt.Data());
+   }
+   return "";
+}
+
+//_________________________________________________________________________________________________
+void AliMixEventCutObj::SetCurrentValueToIndex(Int_t index)
+{
+   //
+   // Sets current value to index
+   //
+
+   fCurrentVal = fCutMin;
+   for (Int_t i = 0; i < index-1; i++) AddStep();
+}
+
+//_________________________________________________________________________________________________
+void AliMixEventCutObj::PrintValues(AliVEvent *main, AliVEvent *mix)
+{
+   //
+   // Prints values of both events for current type
+   //
+   AliInfo(Form("name=%s main=%f mix=%f", GetCutName(), GetValue(main), GetValue(mix)));
+}
+
+//_________________________________________________________________________________________________
+Bool_t AliMixEventCutObj::IsValid()
+{
+   //
+   // Check if cut is valid
+   //
+   switch (fCutType) {
+
+      case kCentrality:
+      {
+         if (fCutOpt.IsNull()) {
+            AliError("fCutOpt is null");
+            return kFALSE;
+         }
+         break;
+      }
+      case kEventPlane:
+      {
+         if (fCutOpt.IsNull()) {
+            AliError("fCutOpt is null");
+            return kFALSE;
+         }
+         if (!fCutOpt.IsDigit()) {
+            AliError("fCutOpt is not a digit string");
+            return kFALSE;
+         }
+         break;
+      }
+      case kEventPlaneV0A:
+      {
+         if (fCutOpt.IsNull()) {
+            AliError("fCutOpt is null");
+            return kFALSE;
+         }
+         if (!fCutOpt.IsDigit()) {
+            AliError("fCutOpt is not a digit string");
+            return kFALSE;
+         }
+         break;
+      }
+      case kEventPlaneV0C:
+      {
+         if (fCutOpt.IsNull()) {
+            AliError("fCutOpt is null");
+            return kFALSE;
+         }
+         if (!fCutOpt.IsDigit()) {
+            AliError("fCutOpt is not a digit string");
+            return kFALSE;
+         }
+         break;
+      }
+      default: {
+         break;
+      }
+   }
+
+   return kTRUE;
+}
---
+++ TENDER/TenderSupplies/AliVZEROTenderSupply.cxx
@@ -0,0 +1,263 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//  Recalculate VZERO timing and decision using the tender                   //
+//  (in case the LHC phase drift is updated in OCDB)                         //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#include <TList.h>
+#include <TObjString.h>
+#include <TChain.h>
+#include <TF1.h>
+
+#include <AliLog.h>
+#include <AliESDEvent.h>
+#include <AliESDVZERO.h>
+#include <AliCDBId.h>
+#include <AliCDBManager.h>
+#include <AliCDBEntry.h>
+#include <AliTender.h>
+#include <AliLHCClockPhase.h>
+#include <AliVZEROCalibData.h>
+#include <AliVZEROTriggerMask.h>
+#include <AliVZEROReconstructor.h>
+
+#include "AliVZEROTenderSupply.h"
+
+ClassImp(AliVZEROTenderSupply)
+
+AliVZEROTenderSupply::AliVZEROTenderSupply() :
+  AliTenderSupply(),
+  fCalibData(NULL),
+  fTimeSlewing(NULL),
+  fRecoParam(NULL),
+  fLHCClockPhase(0),
+  fDebug(kFALSE)
+{
+  //
+  // default ctor
+  //
+}
+
+//_____________________________________________________
+AliVZEROTenderSupply::AliVZEROTenderSupply(const char *name, const AliTender *tender) :
+  AliTenderSupply(name,tender),
+  fCalibData(NULL),
+  fTimeSlewing(NULL),
+  fRecoParam(NULL),
+  fLHCClockPhase(0),
+  fDebug(kFALSE)
+{
+  //
+  // named ctor
+  //
+}
+
+//_____________________________________________________
+void AliVZEROTenderSupply::Init()
+{
+  //
+  // Initialise VZERO tender
+  //
+}
+
+//_____________________________________________________
+void AliVZEROTenderSupply::ProcessEvent()
+{
+  //
+  // Reapply the LHC-clock phase drift
+  //
+
+  AliESDEvent *event=fTender->GetEvent();
+  if (!event) return;
+  
+  //load gain correction if run has changed
+  if (fTender->RunChanged()){
+    if (fDebug) printf("AliVZEROTenderSupply::ProcessEvent - Run Changed (%d)\n",fTender->GetRun());
+    GetPhaseCorrection();
+
+    AliCDBEntry *entryGeom = fTender->GetCDBManager()->Get("GRP/Geometry/Data",fTender->GetRun());
+    if (!entryGeom) {
+      AliError("No geometry entry is found");
+      return;
+    } else {
+      if (fDebug) printf("AliVZEROTenderSupply::Used geometry entry: %s\n",entryGeom->GetId().ToString().Data());
+    }
+
+    AliCDBEntry *entryCal = fTender->GetCDBManager()->Get("VZERO/Calib/Data",fTender->GetRun());
+    if (!entryCal) {
+      AliError("No VZERO calibration entry is found");
+      fCalibData = NULL;
+      return;
+    } else {
+      fCalibData = (AliVZEROCalibData*)entryCal->GetObject();
+      if (fDebug) printf("AliVZEROTenderSupply::Used VZERO calibration entry: %s\n",entryCal->GetId().ToString().Data());
+    }
+
+    AliCDBEntry *entrySlew = fTender->GetCDBManager()->Get("VZERO/Calib/TimeSlewing",fTender->GetRun());
+    if (!entrySlew) {
+      AliError("VZERO time slewing function is not found in OCDB !");
+      fTimeSlewing = NULL;
+      return;
+    } else {
+      fTimeSlewing = (TF1*)entrySlew->GetObject();
+      if (fDebug) printf("AliVZEROTenderSupply::Used VZERO time slewing entry: %s\n",entrySlew->GetId().ToString().Data());
+    }
+
+    AliCDBEntry *entryRecoParam = fTender->GetCDBManager()->Get("VZERO/Calib/RecoParam",fTender->GetRun());
+    if (!entryRecoParam) {
+      AliError("VZERO reco-param object is not found in OCDB !");
+      fRecoParam = NULL;
+      return;
+    } else {
+      TObjArray *recoParamArr = (TObjArray*)entryRecoParam->GetObject();
+      if (fDebug) printf("AliVZEROTenderSupply::Used VZERO reco-param entry: %s\n",entryRecoParam->GetId().ToString().Data());
+      fRecoParam = NULL;
+      for(Int_t i = 0; i < recoParamArr->GetEntriesFast(); i++) {
+	AliVZERORecoParam *par = (AliVZERORecoParam*)recoParamArr->At(i);
+	if (!par) continue;
+	if (par->IsDefault()) fRecoParam = par;
+      }
+      if (!fRecoParam) AliError("No default VZERO reco-param object is found in OCDB !");
+    }
+  }
+
+  if (!fCalibData || !fTimeSlewing || !fRecoParam) {
+    AliWarning("VZERO calibration objects not found!");
+    return;
+  }
+  //
+  // correct VZERO time signals and decision
+  //
+  AliESDVZERO *esdVZERO = event->GetVZEROData();
+  if (!esdVZERO) {
+    AliError("No VZERO object is found inside ESD!");
+    return;
+  }
+  if (!esdVZERO->TestBit(AliESDVZERO::kDecisionFilled)) {
+    AliWarning("VZERO offline trigger decisions were not filled in ESD, the tender supply is disabled");
+    return;
+  }
+
+  if (fDebug) printf("LHC-clock phase correction: %f\n",fLHCClockPhase);
+
+  if (fDebug) printf("Original VZERO decision %d (%f ns) and %d (%f ns)\n",
+		     esdVZERO->GetV0ADecision(),esdVZERO->GetV0ATime(),
+		     esdVZERO->GetV0CDecision(),esdVZERO->GetV0CTime());
+  Float_t time[64];
+  for(Int_t i = 0; i < 64; ++i) {
+    time[i] = esdVZERO->GetTime(i);
+    if (time[i] > (AliVZEROReconstructor::kInvalidTime + 1e-6))
+      time[i] += fLHCClockPhase;
+  }
+  esdVZERO->SetTime(time);
+
+  {
+    AliVZEROTriggerMask triggerMask;
+    triggerMask.SetRecoParam(fRecoParam);
+    triggerMask.FillMasks(esdVZERO, fCalibData, fTimeSlewing);
+  }
+  if (fDebug) printf("Modified VZERO decision %d (%f ns) and %d (%f ns)\n",
+		     esdVZERO->GetV0ADecision(),esdVZERO->GetV0ATime(),
+		     esdVZERO->GetV0CDecision(),esdVZERO->GetV0CTime());
+
+}
+
+//_____________________________________________________
+void AliVZEROTenderSupply::GetPhaseCorrection()
+{
+  //
+  // Get Gain splines from OCDB
+  //
+
+  AliInfo("Get LHC-clock phase correction");
+
+  //
+  //find previous entry from the UserInfo
+  //
+  TTree *tree=((TChain*)fTender->GetInputData(0))->GetTree();
+  if (!tree) {
+    AliError("Tree not found in ESDhandler");
+    return;
+  }
+  
+  TList *userInfo=(TList*)tree->GetUserInfo();
+  if (!userInfo) {
+    AliError("No UserInfo found in tree");
+    return;
+  }
+
+  TList *cdbList=(TList*)userInfo->FindObject("cdbList");
+  if (!cdbList) {
+    AliError("No cdbList found in UserInfo");
+    if (AliLog::GetGlobalLogLevel()>=AliLog::kError) userInfo->Print();
+    return;
+  }
+
+  Float_t oldPhase = 0;
+
+  TIter nextCDB(cdbList);
+  TObjString *os=0x0;
+  while ( (os=(TObjString*)nextCDB()) ){
+    if (!(os->GetString().Contains("GRP/Calib/LHCClockPhase"))) continue;
+    AliCDBId *id=AliCDBId::MakeFromString(os->GetString());
+    
+    AliCDBEntry *entry=fTender->GetCDBManager()->Get(*id);
+    if (!entry) {
+      AliError("The previous LHC-clock phase entry is not found");
+      delete id;
+      return;
+    }
+    
+    if (fDebug) printf("AliVZEROTenderSupply::Used old LHC-clock phase entry: %s\n",entry->GetId().ToString().Data());
+    
+    AliLHCClockPhase *phase = (AliLHCClockPhase*)entry->GetObject();
+    if (!phase) {
+      AliError("Phase object is not found in the calibration entry");
+      delete id;
+      return;
+    }
+
+    oldPhase = phase->GetMeanPhase();
+
+    delete id;
+    break;
+  }
+
+  //
+  //new LHC-clock phase entry
+  //
+  Float_t newPhase = 0;
+  AliCDBEntry *entryNew=fTender->GetCDBManager()->Get("GRP/Calib/LHCClockPhase",fTender->GetRun());
+  if (!entryNew) {
+    AliError("No new LHC-clock phase calibration entry is found");
+    return;
+  }
+  if (fDebug) printf("AliVZEROTenderSupply::Used new LHC-clock phase entry: %s\n",entryNew->GetId().ToString().Data());
+  
+  AliLHCClockPhase *phase2 = (AliLHCClockPhase*)entryNew->GetObject();
+  if (!phase2) {
+    AliError("Phase object is not found in the calibration entry");
+    return;
+  }
+
+  newPhase = phase2->GetMeanPhase();
+
+  fLHCClockPhase = newPhase - oldPhase;
+}
