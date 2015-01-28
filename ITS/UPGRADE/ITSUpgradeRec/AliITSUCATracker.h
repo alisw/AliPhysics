7d0ca69de3aac13d9c66d89a8cf10d0a50c01b61
mpuccio <maximiliano.puccio@cern.ch>
2015-01-28 16:00:08 +0100

    Code cleanup and bug fix Fixed valgrind warning concerning conditional jump on undefined values. Now events with low multiplicity - even without any recpoints on some layers - are properly handled.

--- ITS/UPGRADE/ITSUpgradeRec/AliITSUCATracker.h
+++ ITS/UPGRADE/ITSUpgradeRec/AliITSUCATracker.h
@@ -9,7 +9,7 @@
 #ifndef ALIITSUCATRACKER_H
 #define ALIITSUCATRACKER_H
 
-#define _TUNING_
+//#define _TUNING_
 
 #include <vector>
 
@@ -66,7 +66,7 @@ public:
   Bool_t   GetSAonly() const { return fSAonly; }
   void     SetChi2Cut(float cut) { fChi2Cut = cut; }
   void     SetPhiCut(float cut) { fPhiCut = cut; }
-  void     SetSAonly(Bool_t sa=kTRUE) { fSAonly=sa; }
+  void     SetSAonly(Bool_t sa = kTRUE) { fSAonly = sa; }
   void     SetZCut(float cut) { fZCut = cut; }
 
 #ifdef _TUNING_
@@ -92,11 +92,13 @@ public:
 #endif
   //
 protected:
-  bool   CellParams(int l, ClsInfo_t* c1, ClsInfo_t* c2, ClsInfo_t* c3, float &curv, float np[3]);
+  bool   CellParams(int l, ClsInfo_t*__restrict__ c1, ClsInfo_t*__restrict__ c2,
+                    ClsInfo_t*__restrict__ c3, float &curv, float np[3]);
   void   CellsTreeTraversal(vector<AliITSUCARoad> &roads, const int &iD, const int &doubl);
   void   FindTracksCA(int iteration);
   void   MakeCells(int iteration);
-  Bool_t RefitAt(Double_t xx, AliITSUTrackCooked *t, const AliITSUTrackCooked *c);
+  Bool_t RefitAt(Double_t xx, AliITSUTrackCooked*__restrict__ t,
+                 const AliITSUTrackCooked*__restrict__ c);
   void   SetCuts(int it);
   void   SetLabel(AliITSUTrackCooked &t, Float_t wrong);
   
