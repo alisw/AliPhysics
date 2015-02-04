9235534aba8b078185cffbe4e2c25e800c4664a7
malturan <mohammad.al-turany@cern.ch>
2015-02-04 11:50:48 +0100

    add configure command to all builds

--- AliRoot_test.cmake
+++ AliRoot_test.cmake
@@ -20,36 +20,19 @@ Set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 #  Set(CTEST_SVN_UPDATE_OPTIONS "$ENV{REVISION}")
 #EndIf($ENV{ctest_model} MATCHES Continuous)
 
+
+Set (CTEST_CONFIGURE_COMMAND " \"${CMAKE_EXECUTABLE_NAME}\"   \"-DROOTSYS=${CTEST_ROOTSYS_DIRECTORY}\"  \"${CTEST_SOURCE_DIRECTORY}\" ")
+
 SET (BUILD_COMMAND "make")
 SET (CTEST_BUILD_COMMAND "${BUILD_COMMAND} -j$ENV{number_of_processors}")
 
 If($ENV{ctest_model} MATCHES Nightly)
 
-Set (CTEST_CONFIGURE_COMMAND " \"${CMAKE_EXECUTABLE_NAME}\" \"-DCMAKE_BUILD_TYPE=NIGHTLY\" \"-G${CTEST_CMAKE_GENERATOR}\" \"${CTEST_SOURCE_DIRECTORY}\" \"-DROOTSYS=${CTEST_ROOTSYS_DIRECTORY}\"  ")
-
-  # get the information about conflicting or localy modified files
-  # from svn, extract the relavant information about the file name
-  # and put the result in the output variable
-#  execute_process(COMMAND svn stat -u  
-#                  COMMAND grep ^[CM]
-#                  COMMAND cut -c21- 
-#                  OUTPUT_VARIABLE FILELIST
-#                  )
-
-  # create out of the output a cmake list. This step is done to convert the
-  # stream into seperated filenames.
-  # The trick is to exchange an "\n" by an ";" which is the separartor in
-  # a list created by cmake 
-#  STRING(REGEX REPLACE "\n" ";" _result "${FILELIST}")
-
-#  FOREACH(_file ${_result})
-#    STRING(STRIP "${_file}" _file1)
-#    SET (CTEST_NOTES_FILES ${CTEST_NOTES_FILES} "${CTEST_SOURCE_DIRECTORY}/${_file1}")
-#  ENDFOREACH(_file ${_result})
-
+  Set (CTEST_CONFIGURE_COMMAND " \"${CMAKE_EXECUTABLE_NAME}\" \"-DCMAKE_BUILD_TYPE=NIGHTLY\" \"-G${CTEST_CMAKE_GENERATOR}\" \"${CTEST_SOURCE_DIRECTORY}\" \"-DROOTSYS=${CTEST_ROOTSYS_DIRECTORY}\"  ")
   CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
 
 endif($ENV{ctest_model} MATCHES Nightly)
+
 
 configure_file(${CTEST_SOURCE_DIRECTORY}/CTestCustom.cmake
                ${CTEST_BINARY_DIRECTORY}/CTestCustom.cmake
