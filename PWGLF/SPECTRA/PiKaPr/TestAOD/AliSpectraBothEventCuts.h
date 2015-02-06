2c624e8cba63a30da39e50f94ff68b6af1432fb2
mchojnac <Marek.Chojnacki@cern.ch>
2015-02-06 12:21:53 +0100

    fix of the getters

--- PWGLF/SPECTRA/PiKaPr/TestAOD/AliSpectraBothEventCuts.h
+++ PWGLF/SPECTRA/PiKaPr/TestAOD/AliSpectraBothEventCuts.h
@@ -127,8 +127,8 @@ fHistoNChAftSel(0),fHistoQVector(0),fHistoEP(0), fHistoVtxAftSelwithoutZvertexCu
    AliAnalysisUtils* GetAnalysisUtils() const {return fAnalysisUtils; }
 Bool_t GetUseAliPPVsMultUtils() const {return fUseAliPPVsMultUtils;} 
  Int_t  GetNMCProcessType() const { return fNMCProcessType;}
- Bool_t  GetEventMCProcessTypeIncluded(Bool_t flag) const {return flag;}
- Bool_t  GetCheckTypeofVeretx(Bool_t flag) const {return flag;}
+ Bool_t  GetEventMCProcessTypeIncluded() const {return fEventMCProcessTypeIncluded;}
+ Bool_t  GetCheckTypeofVeretx() const {return fchecktypeofveretxbytitle;}
  Int_t  SetVertexSelection(Int_t flag)const {return fvertexselection;} 
  
 
