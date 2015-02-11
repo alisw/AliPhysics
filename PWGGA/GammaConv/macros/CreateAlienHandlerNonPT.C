e607abafd72596a5bb2db831b1b0bc728ba463af
lleardin <lucia.leardini@cern.ch>
2015-02-11 13:23:39 +0100

    added TruePhoton plot vs Pt and DeltaPhi in FillPhotonCombinatorialBackgroundHist

--- PWGGA/GammaConv/macros/CreateAlienHandlerNonPT.C
+++ PWGGA/GammaConv/macros/CreateAlienHandlerNonPT.C
@@ -10,7 +10,7 @@ AliAnalysisGrid* CreateAlienHandler( TString mode )
    plugin->SetAliROOTVersion	("vAN-20150120");
    //plugin->SetProductionMode	(1);
    plugin->SetAdditionalLibs("PWGGAGammaConv.par");
-   plugin->SetAdditionalLibs("libTree.so libVMC.so libGui.so libXMLParser.so libMinuit.so libMinuit2.so libProof.so libGeom.so libPhysics.so libSTEERBase.so libESD.so libAOD.so libOADB.so libANALYSIS.so libANALYSISalice.so libCDB.so libRAWDatabase.so libSTEER.so libCORRFW.so libPWGGAGammaConv.so");
+   plugin->SetAdditionalLibs("libTree.so libVMC.so libGui.so libXMLParser.so libMinuit.so libMinuit2.so libProof.so libGeom.so libPhysics.so libSTEERBase.so libESD.so libAOD.so libOADB.so libANALYSIS.so libANALYSISalice.so libCDB.so libRAWDatabase.so libSTEER.so libCORRFW.so libPWGflowBase.so libPWGflowTasks.so libPWGGAGammaConv.so");
    plugin->SetDefaultOutputs	(kTRUE);
    //plugin->SetOutputFiles("GammaConvV1_4.root GammaConvV1_5.root GammaConvV1_14.root GammaConvV1_15.root");
    plugin->SetOutputToRunNo();
@@ -24,7 +24,7 @@ AliAnalysisGrid* CreateAlienHandler( TString mode )
    plugin->SetKeepLogs();
    plugin->SetExecutableCommand("root -b -q -x");
    plugin->SetPrice		(1);
-   plugin->SetNtestFiles	(2);
+   plugin->SetNtestFiles	(5);
    plugin->SetNrunsPerMaster(1);
    plugin->SetMergeDirName("");
    plugin->SetMaxMergeFiles(20);
