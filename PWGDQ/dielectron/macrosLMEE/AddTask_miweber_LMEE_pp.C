6f9276209520e398715900b1f3af12fcd28281b9
miweber <m.weber@cern.ch>
2015-02-12 11:42:05 +0100

    Revert "adding output file name as task parameter (now correct)"
    This reverts commit 48c26021320db94e5472a7c805d38fefc50d7cfc.

--- PWGDQ/dielectron/macrosLMEE/AddTask_miweber_LMEE_pp.C
+++ PWGDQ/dielectron/macrosLMEE/AddTask_miweber_LMEE_pp.C
@@ -1,4 +1,4 @@
-AliAnalysisTask *AddTask_miweber_LMEE_pp(TString outputFileName = "AnalysisResult.root"){
+AliAnalysisTask *AddTask_miweber_LMEE_pp(){
 
 
   //get the current analysis manager
@@ -66,26 +66,26 @@ AliAnalysisTask *AddTask_miweber_LMEE_pp(TString outputFileName = "AnalysisResul
     mgr->CreateContainer("miweber_LMEE_pp_tree",
                          TTree::Class(),
                          AliAnalysisManager::kExchangeContainer,
-                         outputFileName.Data());
+                         "AnalysisResults.root");
   
   AliAnalysisDataContainer *cOutputHist1 =
     mgr->CreateContainer("miweber_LMEE_pp_out",
                          TList::Class(),
                          AliAnalysisManager::kOutputContainer,
-                         outputFileName.Data());
+                         "AnalysisResults.root");
   
   AliAnalysisDataContainer *cOutputHist2 =
     mgr->CreateContainer("miweber_LMEE_pp_CF",
                          TList::Class(),
                          AliAnalysisManager::kOutputContainer,
-                         outputFileName.Data());
+                         "AnalysisResults.root");
   //                         "miweber_LMEE_pp_CF.root");
   
   AliAnalysisDataContainer *cOutputHist3 =
     mgr->CreateContainer("miweber_EventStat",
                          TH1D::Class(),
                          AliAnalysisManager::kOutputContainer,
-                         outputFileName.Data());
+                         "AnalysisResults.root");
   
   
   mgr->ConnectInput(task,  0, mgr->GetCommonInputContainer());
