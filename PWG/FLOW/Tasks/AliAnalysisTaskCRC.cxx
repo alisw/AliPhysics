f3564c2c7f89dfde1a56060db7277eb667599f5a
jmargutt <jacopo.margutti@cern.ch>
2015-02-16 18:02:54 +0100

    Testing the patch

---
+++ PWG/FLOW/Tasks/AliAnalysisTaskCRC.cxx
@@ -0,0 +1,380 @@
+/*************************************************************************
+ * Copyright(c) 1998-2008, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+/********************************
+ * analysis task for CRC        *
+ *                              *
+ * author: Jacopo Margutti      *
+ *         (margutti@nikhef.nl) *
+ * ******************************/
+
+class TFile;
+class TString;
+class TList;
+class AliAnalysisTaskSE;
+
+#include "Riostream.h"
+#include "AliFlowEventSimple.h"
+#include "AliAnalysisTaskCRC.h"
+#include "AliFlowAnalysisCRC.h"
+
+#include "AliLog.h"
+
+using std::cout;
+using std::endl;
+ClassImp(AliAnalysisTaskCRC)
+
+//================================================================================================================
+
+AliAnalysisTaskCRC::AliAnalysisTaskCRC(const char *name, Bool_t useParticleWeights):
+AliAnalysisTaskSE(name),
+fEvent(NULL),
+fQC(NULL),
+fListHistos(NULL),
+fBookOnlyBasicCCH(kTRUE),
+fFillMultipleControlHistograms(kFALSE),
+fHarmonic(0),
+fApplyCorrectionForNUA(kFALSE),
+fApplyCorrectionForNUAVsM(kFALSE),
+fPropagateErrorAlsoFromNIT(kFALSE),
+fCalculateDiffFlow(kTRUE),
+fCalculate2DDiffFlow(kFALSE),
+fCalculateDiffFlowVsEta(kTRUE),
+fStoreDistributions(kFALSE),
+fCalculateCumulantsVsM(kFALSE),
+fCalculateAllCorrelationsVsM(kFALSE),
+fCalculateMixedHarmonics(kFALSE),
+fCalculateMixedHarmonicsVsM(kFALSE),
+fStoreControlHistograms(kFALSE),
+fMinimumBiasReferenceFlow(kTRUE),
+fForgetAboutCovariances(kFALSE),
+fStoreVarious(kFALSE),
+fExactNoRPs(0),
+fUse2DHistograms(kFALSE),
+fFillProfilesVsMUsingWeights(kTRUE),
+fUseQvectorTerms(kFALSE),
+fnBinsMult(10000),
+fMinMult(0.),
+fMaxMult(10000.),
+fUseParticleWeights(useParticleWeights),
+fUsePhiWeights(kFALSE),
+fUsePtWeights(kFALSE),
+fUseEtaWeights(kFALSE),
+fUseTrackWeights(kFALSE),
+fUsePhiEtaWeights(kFALSE),
+fWeightsList(NULL),
+fMultiplicityWeight(NULL),
+fMultiplicityIs(AliFlowCommonConstants::kRP),
+fnBinsForCorrelations(10000),
+fUseBootstrap(kFALSE),
+fUseBootstrapVsM(kFALSE),
+fnSubsamples(10),
+fCalculateCRC(kFALSE),
+fUseEtaGap(kFALSE),
+fEtaGap(0.),
+fUseNUAforCRC(kFALSE)
+{
+ // constructor
+ AliDebug(2,"AliAnalysisTaskCRC::AliAnalysisTaskCRC(const char *name, Bool_t useParticleWeights)");
+ 
+ // Define input and output slots here
+ // Input slot #0 works with an AliFlowEventSimple
+ DefineInput(0, AliFlowEventSimple::Class());
+ 
+ // Output slot #0 is reserved
+ // Output slot #1 writes into a TList container
+ DefineOutput(1, TList::Class());
+ 
+ // Event weights:
+ fMultiplicityWeight = new TString("combinations");
+ 
+ // b) Initialize default min and max values of correlations:
+ //    (Remark: The default values bellow were chosen for v2=5% and M=500)
+ fMinValueOfCorrelation[0] = -0.015; // <2>_min
+ fMaxValueOfCorrelation[0] = 0.03; // <2>_max
+ fMinValueOfCorrelation[1] = -0.6e-3; // <4>_min
+ fMaxValueOfCorrelation[1] = 0.07; // <4>_max
+ fMinValueOfCorrelation[2] = -0.08e-3; // <6>_min
+ fMaxValueOfCorrelation[2] = 0.015; // <6>_max
+ fMinValueOfCorrelation[3] = -20.e-6; // <8>_min
+ fMaxValueOfCorrelation[3] = 0.003; // <8>_max
+ 
+ // c) Initialize default min and max values of correlation products:
+ //    (Remark: The default values bellow were chosen for v2=5% and M=500)
+ fMinValueOfCorrelationProduct[0] = -15.e-6; // <2><4>_min
+ fMaxValueOfCorrelationProduct[0] = 0.02; // <2><4>_max
+ 
+ // d) Initialize default min and max values of q-vector terms:
+ fMinValueOfQvectorTerms[0] = 0.;
+ fMaxValueOfQvectorTerms[0] = 30.;
+ fMinValueOfQvectorTerms[1] = 0.;
+ fMaxValueOfQvectorTerms[1] = 20.;
+ fMinValueOfQvectorTerms[2] = 0.;
+ fMaxValueOfQvectorTerms[2] = 200.;
+ fMinValueOfQvectorTerms[3] = -30.;
+ fMaxValueOfQvectorTerms[3] = 80.;
+ 
+}
+
+//================================================================================================================
+
+AliAnalysisTaskCRC::AliAnalysisTaskCRC():
+AliAnalysisTaskSE(),
+fEvent(NULL),
+fQC(NULL),
+fListHistos(NULL),
+fBookOnlyBasicCCH(kFALSE),
+fFillMultipleControlHistograms(kFALSE),
+fHarmonic(0),
+fApplyCorrectionForNUA(kFALSE),
+fApplyCorrectionForNUAVsM(kFALSE),
+fPropagateErrorAlsoFromNIT(kFALSE),
+fCalculateDiffFlow(kFALSE),
+fCalculate2DDiffFlow(kFALSE),
+fCalculateDiffFlowVsEta(kTRUE),
+fStoreDistributions(kFALSE),
+fCalculateCumulantsVsM(kFALSE),
+fCalculateAllCorrelationsVsM(kFALSE),
+fCalculateMixedHarmonics(kFALSE),
+fCalculateMixedHarmonicsVsM(kFALSE),
+fStoreControlHistograms(kFALSE),
+fMinimumBiasReferenceFlow(kFALSE),
+fForgetAboutCovariances(kFALSE),
+fStoreVarious(kFALSE),
+fExactNoRPs(0),
+fUse2DHistograms(kFALSE),
+fFillProfilesVsMUsingWeights(kTRUE),
+fUseQvectorTerms(kFALSE),
+fnBinsMult(0),
+fMinMult(0.),
+fMaxMult(0.),
+fUseParticleWeights(kFALSE),
+fUsePhiWeights(kFALSE),
+fUsePtWeights(kFALSE),
+fUseEtaWeights(kFALSE),
+fUseTrackWeights(kFALSE),
+fUsePhiEtaWeights(kFALSE),
+fWeightsList(NULL),
+fMultiplicityWeight(NULL),
+fMultiplicityIs(AliFlowCommonConstants::kRP),
+fnBinsForCorrelations(0),
+fUseBootstrap(kFALSE),
+fUseBootstrapVsM(kFALSE),
+fnSubsamples(10),
+fCalculateCRC(kFALSE),
+fUseEtaGap(kFALSE),
+fEtaGap(0.),
+fUseNUAforCRC(kFALSE)
+{
+ // Dummy constructor
+ AliDebug(2,"AliAnalysisTaskCRC::AliAnalysisTaskCRC()");
+ 
+ // b) Initialize default min and max values of correlations:
+ //    (Remark: The default values bellow were chosen for v2=5% and M=500)
+ fMinValueOfCorrelation[0] = -0.015; // <2>_min
+ fMaxValueOfCorrelation[0] = 0.03; // <2>_max
+ fMinValueOfCorrelation[1] = -0.6e-3; // <4>_min
+ fMaxValueOfCorrelation[1] = 0.07; // <4>_max
+ fMinValueOfCorrelation[2] = -0.08e-3; // <6>_min
+ fMaxValueOfCorrelation[2] = 0.015; // <6>_max
+ fMinValueOfCorrelation[3] = -20.e-6; // <8>_min
+ fMaxValueOfCorrelation[3] = 0.003; // <8>_max
+ 
+ // c) Initialize default min and max values of correlation products:
+ //    (Remark: The default values bellow were chosen for v2=5% and M=500)
+ fMinValueOfCorrelationProduct[0] = -15.e-6; // <2><4>_min
+ fMaxValueOfCorrelationProduct[0] = 0.02; // <2><4>_max
+ 
+ // d) Initialize default min and max values of q-vector terms:
+ fMinValueOfQvectorTerms[0] = 0.;
+ fMaxValueOfQvectorTerms[0] = 30.;
+ fMinValueOfQvectorTerms[1] = 0.;
+ fMaxValueOfQvectorTerms[1] = 20.;
+ fMinValueOfQvectorTerms[2] = 0.;
+ fMaxValueOfQvectorTerms[2] = 200.;
+ fMinValueOfQvectorTerms[3] = -30.;
+ fMaxValueOfQvectorTerms[3] = 80.;
+ 
+}
+
+//================================================================================================================
+
+void AliAnalysisTaskCRC::UserCreateOutputObjects()
+{
+ // Called at every worker node to initialize
+ AliDebug(2,"AliAnalysisTaskCRC::UserCreateOutputObjects()");
+ 
+ // Analyser:
+ fQC = new AliFlowAnalysisCRC();
+ 
+ // Common:
+ fQC->SetBookOnlyBasicCCH(fBookOnlyBasicCCH);
+ fQC->SetFillMultipleControlHistograms(fFillMultipleControlHistograms);
+ fQC->SetHarmonic(fHarmonic);
+ fQC->SetApplyCorrectionForNUA(fApplyCorrectionForNUA);
+ fQC->SetApplyCorrectionForNUAVsM(fApplyCorrectionForNUAVsM);
+ fQC->SetPropagateErrorAlsoFromNIT(fPropagateErrorAlsoFromNIT);
+ fQC->SetCalculateDiffFlow(fCalculateDiffFlow);
+ fQC->SetCalculate2DDiffFlow(fCalculate2DDiffFlow);
+ fQC->SetCalculateDiffFlowVsEta(fCalculateDiffFlowVsEta);
+ fQC->SetStoreDistributions(fStoreDistributions);
+ fQC->SetCalculateCumulantsVsM(fCalculateCumulantsVsM);
+ fQC->SetCalculateAllCorrelationsVsM(fCalculateAllCorrelationsVsM);
+ fQC->SetCalculateMixedHarmonics(fCalculateMixedHarmonics);
+ fQC->SetCalculateMixedHarmonicsVsM(fCalculateMixedHarmonicsVsM);
+ fQC->SetCalculateCRC(fCalculateCRC);
+ fQC->SetUseRPforCRC(fUseRPforCRC);
+ fQC->SetStoreControlHistograms(fStoreControlHistograms);
+ fQC->SetMinimumBiasReferenceFlow(fMinimumBiasReferenceFlow);
+ fQC->SetForgetAboutCovariances(fForgetAboutCovariances);
+ fQC->SetExactNoRPs(fExactNoRPs);
+ fQC->SetUseEtaGap(fUseEtaGap);
+ fQC->SetEtaGap(fEtaGap);
+ fQC->SetNUAforCRC(fUseNUAforCRC);
+ // Multiparticle correlations vs multiplicity:
+ fQC->SetnBinsMult(fnBinsMult);
+ fQC->SetMinMult(fMinMult);
+ fQC->SetMaxMult(fMaxMult);
+ // Particle weights:
+ if(fUseParticleWeights)
+ {
+  // Pass the flags to class:
+  if(fUsePhiWeights){fQC->SetUsePhiWeights(fUsePhiWeights);}
+  if(fUsePtWeights){fQC->SetUsePtWeights(fUsePtWeights);}
+  if(fUseEtaWeights){fQC->SetUseEtaWeights(fUseEtaWeights);}
+  if(fUseTrackWeights){fQC->SetUseTrackWeights(fUseTrackWeights);}
+  if(fUsePhiEtaWeights){fQC->SetUsePhiEtaWeights(fUsePhiEtaWeights);}
+  
+  // Pass the list with weights to class:
+  if(fWeightsList) fQC->SetWeightsList(fWeightsList);
+ }
+ // Event weights:
+ if(!fMultiplicityWeight->Contains("combinations")) // default is "combinations"
+ {
+  fQC->SetMultiplicityWeight(fMultiplicityWeight->Data());
+ }
+ 
+ fQC->SetMultiplicityIs(fMultiplicityIs);
+ fQC->SetnBinsForCorrelations(fnBinsForCorrelations);
+ fQC->SetUse2DHistograms(fUse2DHistograms);
+ fQC->SetFillProfilesVsMUsingWeights(fFillProfilesVsMUsingWeights);
+ fQC->SetUseQvectorTerms(fUseQvectorTerms);
+ 
+ // Store phi distribution for one event to illustrate flow:
+ fQC->SetStoreVarious(fStoreVarious);
+ 
+ // Initialize default min and max values of correlations:
+ for(Int_t ci=0;ci<4;ci++)
+ {
+  fQC->SetMinValueOfCorrelation(ci,fMinValueOfCorrelation[ci]);
+  fQC->SetMaxValueOfCorrelation(ci,fMaxValueOfCorrelation[ci]);
+ }
+ 
+ // Initialize default min and max values of correlation products:
+ for(Int_t cpi=0;cpi<1;cpi++) // TBI hardwired 1
+ {
+  fQC->SetMinValueOfCorrelationProduct(cpi,fMinValueOfCorrelationProduct[cpi]);
+  fQC->SetMaxValueOfCorrelationProduct(cpi,fMaxValueOfCorrelationProduct[cpi]);
+ }
+ 
+ // Initialize default min and max values of Q-vector terms:
+ for(Int_t ci=0;ci<4;ci++)
+ {
+  fQC->SetMinValueOfQvectorTerms(ci,fMinValueOfQvectorTerms[ci]);
+  fQC->SetMaxValueOfQvectorTerms(ci,fMaxValueOfQvectorTerms[ci]);
+ }
+ 
+ // Bootstrap:
+ fQC->SetUseBootstrap(fUseBootstrap);
+ fQC->SetUseBootstrapVsM(fUseBootstrapVsM);
+ fQC->SetnSubsamples(fnSubsamples);
+ 
+ fQC->Init();
+ 
+ if(fQC->GetHistList())
+ {
+  fListHistos = fQC->GetHistList();
+  // fListHistos->Print();
+ } else
+ {
+  Printf("ERROR: Could not retrieve histogram list (QC, Task::UserCreateOutputObjects()) !!!!");
+ }
+ 
+ PostData(1,fListHistos);
+ 
+} // end of void AliAnalysisTaskCRC::UserCreateOutputObjects()
+
+//================================================================================================================
+
+void AliAnalysisTaskCRC::UserExec(Option_t *)
+{
+ // main loop (called for each event)
+ fEvent = dynamic_cast<AliFlowEventSimple*>(GetInputData(0));
+ 
+ // Q-cumulants
+ if(fEvent)
+ {
+  fQC->Make(fEvent);
+ } else
+ {
+  cout<<"WARNING: No input data (QC, Task::UserExec()) !!!!"<<endl;
+  cout<<endl;
+ }
+ 
+ PostData(1,fListHistos);
+}
+
+//================================================================================================================
+
+void AliAnalysisTaskCRC::Terminate(Option_t *)
+{
+ //accessing the merged output list:
+ fListHistos = (TList*)GetOutputData(1);
+ 
+ fQC = new AliFlowAnalysisCRC();
+ 
+ if(fListHistos)
+ {
+  fQC->GetOutputHistograms(fListHistos);
+  fQC->Finish();
+  PostData(1,fListHistos);
+ } else
+ {
+  cout<<" WARNING: histogram list pointer is empty (QC, Task::Terminate()) !!!!"<<endl;
+  cout<<endl;
+ }
+ 
+} // end of void AliAnalysisTaskCRC::Terminate(Option_t *)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PWGCF/FLOW/macros/AddTaskCRC.C
@@ -0,0 +1,227 @@
+void AddTaskCRC(Int_t iHarmonic, Double_t centrMin, Double_t centrMax, Double_t pTMin, Double_t pTMax, Bool_t UseEtaGap = kFALSE, Double_t EtaGap = 0., Bool_t bNUACorr = kFALSE)
+{
+ // load libraries
+ gSystem->Load("libGeom");
+ gSystem->Load("libVMC");
+ gSystem->Load("libXMLIO");
+ gSystem->Load("libPhysics");
+ gSystem->Load("libCore.so");
+ gSystem->Load("libTree.so");
+ gSystem->Load("libSTEERBase");
+ gSystem->Load("libESD");
+ gSystem->Load("libAOD");
+ gSystem->Load("libANALYSIS");
+ gSystem->Load("libANALYSISalice");
+ gSystem->Load("libANALYSIScalib");
+ gSystem->Load("libOADB.so");
+ gSystem->Load("libEventMixing.so");
+ gSystem->Load("libCORRFW.so");
+ gSystem->Load("libPWGTools.so");
+ gSystem->Load("libPWGflowBase.so");
+ gSystem->Load("libPWGflowTasks.so");
+ gSystem->Load("libTPCbase.so");
+ gSystem->Load("libTPCcalib.so");
+ 
+ gROOT->ProcessLine(".include $ALICE_ROOT/include");
+ gROOT->ProcessLine(".include $ALICE_PHYSICS/include");
+ gSystem->AddIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/EMCAL -I$ALICE_ROOT/ANALYSIS -I$ALICE_ROOT/OCDB -I$ALICE_ROOT/STEER/macros -I$ALICE_ROOT/include -I$ALICE_ROOT/ITS -I$ALICE_ROOT/TPC -I$ALICE_ROOT/TRD -I$ALICE_ROOT/macros -I$ALICE_PHYSICS -I$ALICE_PHYSICS/include -I$ALICE_PHYSICS/OADB $ALICE_PHYSICS/OADB/macros -I$ALICE_PHYSICS/PWGGA -I$ALICE_PHYSICS/PWGCF -I$ALICE_PHYSICS/PWGHF -I$ALICE_PHYSICS/TENDER -I$ALICE_PHYSICS/TENDER/Tender -I$ALICE_PHYSICS/TENDER/TenderSupplies -I$ALICE_PHYSICS/PARfiles -I$ALICE_PHYSICS/PWGCF/FLOW/macros -g ");
+ 
+ // the manager is static, so get the existing manager via the static method
+ AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+ if (!mgr) {
+  printf("No analysis manager to connect to!\n");
+  return NULL;
+ }
+ 
+ // just to see if all went well, check if the input event handler has been connected
+ if (!mgr->GetInputEventHandler()) {
+  printf("This task requires an input event handler!\n");
+  return NULL;
+ }
+ 
+ // specify charge/CEA in all names
+ TString suffix = ":CRC";
+ TString EtaGapName = "_";
+ if (!UseEtaGap) { EtaGapName += "FER"; }
+ else            { EtaGapName += Form("0.%i",(Int_t)(EtaGap*10.)); }
+ suffix += EtaGapName;
+ 
+ TString CentrName = "_";
+ CentrName += (Int_t)centrMin;
+ CentrName += "-";
+ CentrName += (Int_t)centrMax;
+ suffix += CentrName;
+ 
+ TString pTName = "_";
+ Int_t rt = (Int_t)(pTMin*10.);
+ Int_t r = (Int_t)(pTMin);
+ pTName += ( pTMin < 1. ? Form("0.%i",rt) : Form("%i.%i",r,rt-r*10));
+ pTName += "-";
+ rt = (Int_t)(pTMax*10.);
+ r = (Int_t)(pTMax);
+ pTName += ( pTMax < 1. ? Form("0.%i",rt) : Form("%i.%i",r,rt-r*10));
+ suffix += pTName;
+ 
+ // specify some variables
+ // Float_t centrMin = 20.;
+ // Float_t centrMax = 30.;
+ 
+ // create instance of the class: because possible qa plots are added in a second output slot,
+ // the flow analysis task must know if you want to save qa plots at the time of class construction
+ Bool_t doQA = kTRUE;
+ TString taskFEname = "FlowEventTask";
+ taskFEname += suffix;
+ // create instance of the class
+ AliAnalysisTaskFlowEvent* taskFE = new AliAnalysisTaskFlowEvent(taskFEname, "", doQA);
+ // add the task to the manager
+ mgr->AddTask(taskFE);
+ // set the trigger selection
+ taskFE->SelectCollisionCandidates(AliVEvent::kMB);
+ 
+ // define the event cuts object
+ AliFlowEventCuts* cutsEvent = new AliFlowEventCuts("EventCuts");
+ // configure some event cuts, starting with centrality
+ cutsEvent->SetCentralityPercentileRange(centrMin,centrMax);
+ // method used for centrality determination
+ cutsEvent->SetCentralityPercentileMethod(AliFlowEventCuts::kV0);
+ cutsEvent->SetRefMultMethod(AliFlowEventCuts::kV0);
+ // vertex-z cut
+ cutsEvent->SetPrimaryVertexZrange(-10.,10.);
+ // enable the qa plots
+ cutsEvent->SetQA(doQA);
+ // explicit multiplicity outlier cut
+ cutsEvent->SetCutTPCmultiplicityOutliers();
+ cutsEvent->SetLHC10h(kTRUE);
+ 
+ // pass these cuts to your flow event task
+ taskFE->SetCutsEvent(cutsEvent);
+ 
+ // create the track cuts for RPs
+ //AliFlowTrackCuts* cutsRP = AliFlowTrackCuts::GetAODTrackCutsForFilterBit(768, "RP cuts");
+ AliFlowTrackCuts* cutsRP = new AliFlowTrackCuts("RP cuts");
+ cutsRP->SetAODfilterBit(768);
+ cutsRP->SetParamType(AliFlowTrackCuts::kAODFilterBit);
+ cutsRP->SetPtRange(pTMin,pTMax);
+ cutsRP->SetEtaRange(-0.8,0.8);
+ //   cutsRP->SetMinNClustersTPC(70);
+ //   cutsRP->SetMinChi2PerClusterTPC(0.2);
+ //   cutsRP->SetMaxChi2PerClusterTPC(4.0);
+ //   cutsRP->SetRequireTPCRefit(kFALSE);
+ //   cutsRP->SetMaxDCAToVertexXY(2.4);
+ //   cutsRP->SetMaxDCAToVertexZ(3.2);
+ //   cutsRP->SetDCAToVertex2D(kTRUE);
+ //   cutsRP->SetAcceptKinkDaughters(kFALSE);
+ //   cutsRP->SetMinimalTPCdedx(10.);
+ cutsRP->SetQA(kTRUE);
+ 
+ taskFE->SetCutsRP(cutsRP);
+ 
+ // create the track cuts for POIs
+ AliFlowTrackCuts* cutsPOI = new AliFlowTrackCuts("POI cuts");
+ cutsPOI->SetAODfilterBit(768);
+ cutsPOI->SetParamType(AliFlowTrackCuts::kAODFilterBit);
+ cutsPOI->SetPtRange(pTMin,pTMax);
+ cutsPOI->SetEtaRange(-0.8,0.8);
+ //   cutsPOI->SetMinNClustersTPC(70);
+ //   cutsPOI->SetMinChi2PerClusterTPC(0.2);
+ //   cutsPOI->SetMaxChi2PerClusterTPC(4.0);
+ //   cutsPOI->SetRequireTPCRefit(kTRUE);
+ //   cutsPOI->SetMaxDCAToVertexXY(2.4);
+ //   cutsPOI->SetMaxDCAToVertexZ(3.2);
+ //   cutsPOI->SetDCAToVertex2D(kTRUE);
+ //   cutsPOI->SetAcceptKinkDaughters(kFALSE);
+ //   cutsPOI->SetMinimalTPCdedx(10.);
+ cutsPOI->SetQA(kTRUE);
+ 
+ //cutsPOI->SetRequireCharge(kTRUE);
+ //cutsPOI->SetMinNsigmaToVertex(4);
+ //cutsPOI->SetRequireSigmaToVertex(kTRUE);
+ //cutsPOI->SetMinNClustersITS(2);
+ //cutsPOI->SetRequireITSRefit(kTRUE);
+ //cutsPOI->SetMaxChi2PerClusterITS(36.);
+ 
+ /*   cutsPOI->SetPID(AliPID::kPion, AliFlowTrackCuts::kTPCTOFNsigma, 0.9);
+  cutsPOI->SetPriors((centrMin+centrMax)*0.5); // set priors and PID as a function of the centrality
+  //cutsPOI->SetAllowTOFmismatchFlag(kTRUE);
+  cutsPOI->SetRequireStrictTOFTPCagreement(kTRUE);
+  cutsPOI->GetBayesianResponse()->ForceOldDedx();*/ // for 2010 data to use old TPC PID Response instead of the official one
+ //example: francesco's tunig TPC Bethe Bloch for data:
+ //cutsPOI->GetESDpid().GetTPCResponse().SetBetheBlochParameters(4.36414e-02,1.75977e+01,1.14385e-08,2.27907e+00,3.36699e+00);
+ //cutsPOI->GetESDpid().GetTPCResponse().SetMip(49);
+ 
+ //  cutsPOI->SetCharge(charge);
+ 
+ taskFE->SetCutsPOI(cutsPOI);
+ 
+ // get the default name of the output file ("AnalysisResults.root")
+ TString file = "AnalysisResults.root";
+ 
+ // get the common input container from the analysis manager
+ AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+ 
+ // create a data container for the output of the flow event task
+ TString taskFECname = "FlowEventContainer";
+ taskFECname += suffix;
+ AliAnalysisDataContainer *coutputFE = mgr->CreateContainer(taskFECname,
+                                                            AliFlowEventSimple::Class(),
+                                                            AliAnalysisManager::kExchangeContainer);
+ // connect the input data to the flow event task
+ mgr->ConnectInput(taskFE,0,cinput);
+ // and connect the output to the flow event task
+ mgr->ConnectOutput(taskFE,1,coutputFE);
+ 
+ // create an additional container for the QA output of the flow event task
+ // the QA histograms will be stored in a sub-folder of the output file called 'QA'
+ if(doQA) {
+  TString taskFEQAname = file;
+  taskFEQAname += ":CutsQA";
+  taskFEQAname += suffix;
+  AliAnalysisDataContainer* coutputFEQA = mgr->CreateContainer(taskFEQAname,
+                                                               TList::Class(),
+                                                               AliAnalysisManager::kOutputContainer,
+                                                               taskFEQAname.Data());
+  // and connect the qa output container to the flow event.
+  // this container will be written to the output file
+  mgr->ConnectOutput(taskFE,2,coutputFEQA);
+ }
+ 
+ Bool_t UseParticleWeights = kFALSE;
+ //TString ParticleWeightsFileName = "ParticleWeights2D_FullLHC10h_2030.root";
+ 
+ // create the flow analysis tasks
+ AliAnalysisTaskCRC *taskQC = new AliAnalysisTaskCRC("AnalysisTask"+suffix, UseParticleWeights);
+ // set thei triggers
+ taskQC->SelectCollisionCandidates(AliVEvent::kMB);
+ // and set the correct harmonic n
+ taskQC->SetHarmonic(iHarmonic);
+ // set standard flow settings
+ taskQC->SetCalculateDiffFlow(kTRUE);
+ taskQC->SetCalculateDiffFlowVsEta(kTRUE);
+ taskQC->SetStoreDistributions(kTRUE);
+ taskQC->SetFillMultipleControlHistograms(kFALSE);
+ //  CRC settings
+ taskQC->SetStoreVarious(kTRUE);
+ taskQC->SetCalculateCRC(kTRUE);
+ taskQC->SetUseEtaGap(UseEtaGap);
+ taskQC->SetEtaGap(EtaGap);
+ taskQC->SetNUAforCRC(bNUACorr);
+ taskQC->SetUseRPforCRC(kFALSE);
+ 
+ // connect the task to the analysis manager
+ mgr->AddTask(taskQC);
+ 
+ // initialize output name
+ TString outputQC = file;
+ outputQC += suffix;
+ // create and connect the output containers
+ AliAnalysisDataContainer *coutputQC = mgr->CreateContainer(outputQC.Data(),
+                                                            TList::Class(),
+                                                            AliAnalysisManager::kOutputContainer,
+                                                            outputQC);
+ // connect the output of the flow event task to the flow analysis task
+ mgr->ConnectInput(taskQC, 0, coutputFE);
+ // and connect the output of the flow analysis task to the output container
+ // which will be written to the output file
+ mgr->ConnectOutput(taskQC, 1, coutputQC);
+ 
+};
