347dc4a57b2a8bae592b79a5f71101bfa10f1360
kharlov <Yuri.Kharlov@cern.ch>
2015-02-18 20:13:47 +0300

    First commit of analysis Sigma0->Lambda+gamma (A.Borissov)

---
+++ PWGGA/Hyperon/PWGGAHyperonLinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// User tasks
+#pragma link C++ class AliAnalysisTaskSigma0+;
+#pragma link C++ class AliAnalysisTaskSigma0Spectra+;
+#pragma link C++ class AliAnalysisTaskSigma0Corr+;
+#pragma link C++ class AliCaloParticle+;
+
+#endif
+
+
+
+
+
+// #pragma link C++ class AliPHOSGeoUtils+;
+// #pragma link C++ class AliPHOSGeometry+;
