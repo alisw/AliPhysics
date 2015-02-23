839bb2d100773d95eb0ff64913ae47f49339981b
mverweij <marta.verweij@cern.ch>
2015-02-23 16:13:28 +0100

    introduce offset for phi cut particles and jets

--- PWGJE/EMCALJetTasks/AliJetContainer.cxx
+++ PWGJE/EMCALJetTasks/AliJetContainer.cxx
@@ -38,6 +38,7 @@ AliJetContainer::AliJetContainer():
   fJetMaxEta(0.9),
   fJetMinPhi(-10),
   fJetMaxPhi(10),
+  fPhiOffset(0.),
   fMaxClusterPt(1000),
   fMaxTrackPt(100),
   fZLeadingEmcCut(10.),
@@ -82,6 +83,7 @@ AliJetContainer::AliJetContainer(const char *name):
   fJetMaxEta(0.9),
   fJetMinPhi(-10),
   fJetMaxPhi(10),
+  fPhiOffset(0.),
   fMaxClusterPt(1000),
   fMaxTrackPt(100),
   fZLeadingEmcCut(10.),
@@ -355,7 +357,7 @@ Bool_t AliJetContainer::AcceptJet(const AliEmcalJet *jet)
     return kFALSE;
   }
 
-  Double_t jetPhi = jet->Phi();
+  Double_t jetPhi = jet->Phi() + fPhiOffset;
   Double_t jetEta = jet->Eta();
    
   // if limits are given in (-pi, pi) range
