0ac7906ade177518f11bcb6b36b8e7ba9073b732
cbianchi <chiara.bianchin@cern.ch>
2015-02-26 14:07:33 +0100

    Fix for use of constant efficiency, add QA histogram fAvgNmiss

--- PWGJE/EMCALJetTasks/AliEmcalJetByJetCorrection.h
+++ PWGJE/EMCALJetTasks/AliEmcalJetByJetCorrection.h
@@ -57,8 +57,9 @@ class AliEmcalJetByJetCorrection : public TNamed
   //book-keeping object filled inside Eval()
   TProfile *fpAppliedEfficiency;               // Control profile efficiency
   TH2F     *fNmissing;                         // number of missing constituents added versus pTjet
+  TH1F     *fAvgNmiss;                         // calculated missing track (average = Nconstituents * (1./eff -1.)), to be compared with the Y projection of fNmissing
   TList    *fListOfOutput;                     // list containing all histograms
  private:
-  ClassDef(AliEmcalJetByJetCorrection, 5) // jet-by-jet correction class
+  ClassDef(AliEmcalJetByJetCorrection, 6) // jet-by-jet correction class
 };
 #endif
