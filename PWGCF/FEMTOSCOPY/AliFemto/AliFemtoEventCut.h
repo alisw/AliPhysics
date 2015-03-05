4d2d70ab26a8c6a5c4d507f015872125a4bb884c
akubera <Andrew.Kubera@cern.ch>
2015-03-04 21:39:33 -0500

    AliFemtoEventCut : Doxygen and had the copy constructor copy the fyAnalysis pointer."

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoEventCut.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoEventCut.h
@@ -1,7 +1,12 @@
-////////////////////////////////////////////////////////////////////////////////
-/// AliFemtoEventCut - the pure virtual base class for the event cut         ///
-/// All event cuts must inherit from this one                                ///
-////////////////////////////////////////////////////////////////////////////////
+///
+/// \class AliFemtoEventCut
+/// \brief The pure virtual base class for the event cut
+///
+/// All event cuts must inherit from this one and implement the ::Pass and
+/// ::Report methods. The ::Clone() function simply returns NULL, so if users
+/// want their cuts to behave as expected, they should also write their own.
+///
+
 #ifndef AliFemtoEventCut_hh
 #define AliFemtoEventCut_hh
 
@@ -17,30 +22,32 @@ class AliFemtoEventCut : public AliFemtoCutMonitorHandler {
 
 public:
 
-  AliFemtoEventCut();                // default constructor. - Users should write their own
-  AliFemtoEventCut(const AliFemtoEventCut& c); // copy constructor
-  virtual ~AliFemtoEventCut(){/* no-op */};       // destructor
-  AliFemtoEventCut& operator=(const AliFemtoEventCut& aCut);
+  AliFemtoEventCut();                          ///< default constructor. - Users should write their own
+  AliFemtoEventCut(const AliFemtoEventCut& c); ///< copy constructor
+  virtual ~AliFemtoEventCut(){/* no-op */};    ///< destructor
+  AliFemtoEventCut& operator=(const AliFemtoEventCut& aCut);  ///< Assignment operator
 
-  virtual bool Pass(const AliFemtoEvent* event) =0;  // true if passes, false if not
+  virtual bool Pass(const AliFemtoEvent* event) =0;    ///< true if event passes, false if not
 
-  virtual AliFemtoString Report() =0;    // user-written method to return string describing cuts
-  virtual AliFemtoEventCut* Clone() { return 0;}
+  virtual AliFemtoString Report() =0;                  ///< A user-written method to return a string describing cuts
+  virtual AliFemtoEventCut* Clone() { return NULL; }   ///< Returns NULL - don't use!
 
-
-  AliFemtoAnalysis* HbtAnalysis(){return fyAnalysis;};
+  AliFemtoAnalysis* HbtAnalysis() { return fyAnalysis; };
   void SetAnalysis(AliFemtoAnalysis* aAnalysis);
 
 protected:
   AliFemtoAnalysis* fyAnalysis;
 
 #ifdef __ROOT__
+  /// \cond CLASSIMP
   ClassDef(AliFemtoEventCut, 0)
+  /// \endcond
 #endif
 };
 
-inline AliFemtoEventCut::AliFemtoEventCut(const AliFemtoEventCut& /* c */) : AliFemtoCutMonitorHandler(), fyAnalysis(0) { }
+inline AliFemtoEventCut::AliFemtoEventCut(const AliFemtoEventCut& c) : AliFemtoCutMonitorHandler(), fyAnalysis(c.fyAnalysis) { }
 inline void AliFemtoEventCut::SetAnalysis(AliFemtoAnalysis* analysis) { fyAnalysis = analysis; }
-inline AliFemtoEventCut::AliFemtoEventCut(): AliFemtoCutMonitorHandler(), fyAnalysis(0){}                // default constructor. - Users should write their own
+inline AliFemtoEventCut::AliFemtoEventCut(): AliFemtoCutMonitorHandler(), fyAnalysis(NULL) { }
 inline AliFemtoEventCut& AliFemtoEventCut::operator=(const AliFemtoEventCut& aCut) { if (this == &aCut) return *this; fyAnalysis = aCut.fyAnalysis; return *this; }
+
 #endif
