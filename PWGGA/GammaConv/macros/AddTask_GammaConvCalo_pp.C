9ba47f17697cf7e35828dfaddc4638b45ac60078
dmuhlhei <d.muehlheim@cern.ch>
2015-03-06 11:49:25 +0100

    fixed preselection cut for AddTask

--- PWGGA/GammaConv/macros/AddTask_GammaConvCalo_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvCalo_pp.C
@@ -52,7 +52,7 @@ void AddTask_GammaConvCalo_pp(  	Int_t 		trainConfig 				= 1,  								//change 
 	Printf("here \n");
 	
 	//=========  Set Cutnumber for V0Reader ================================
-	TString cutnumberPhoton = "00000000600840001001500000";
+    TString cutnumberPhoton = "00000008400100001500000000";
 	TString cutnumberEvent = "0000000";
 	Bool_t doEtaShift = kFALSE;
 	AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
