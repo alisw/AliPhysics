ca58312d3d57c252966a714bf1bc136c9ecdfd4e
fbellini <fbellini@cern.ch>
2015-03-06 09:39:59 +0100

    Bug fix (Anders)

--- PWGLF/RESONANCES/macros/utils/RebinSpectrum.C
+++ PWGLF/RESONANCES/macros/utils/RebinSpectrum.C
@@ -1,6 +1,7 @@
 void RebinSpectrum(TH1** hi,TH1** hf,TF1* f,int n=1,int dofit=0,TH1* ht=0){
   /* Author: Anders G. Knospe, The University of Texas at Austin
      Created: 9 May 2014
+     Last Update: 12 February 2015
 
      This macro rebins histograms, including cases with incompatible bins.
 
@@ -14,7 +15,7 @@ void RebinSpectrum(TH1** hi,TH1** hf,TF1* f,int n=1,int dofit=0,TH1* ht=0){
 
      hi and hf are arrays so that systematic uncertainties can be accounted for.  hi[0] should contain the central values and statistical uncertainties.  The other histograms in hi[*] (if any) should contain the systematic uncertainties (more than one type of systematic uncertainty is allowed).  The output histogram hf[0] will contain the rebinned central values and statistical uncertainties.  The other output histograms hf[*] (if any) will contain the systematic uncertainties.  Going from hi[0] to hf[0], the statistical uncertainties are added in quadrature.  For the other histograms, the systematic uncertainties are added linearly.  It is assumed that all of the histograms in hi and ht have the same binning, and that all of the histograms in hf have the same binning.
 
-     Explanation: The macro rebins from the input histogram to the output histogram.  Let A be a bin in the input histogram and let B be a bin in the output histogram.  If A lies entirely within B, the situation is simple: the contents of bin A will be added to bin B.  If A is split by one (or both) of the edges of B, a fit function is used to determine the fraction of the content of bin A that will be added to bin B.  The fitting may be done externally, or this macro can be used to do the fit over a limited range (generally, the fit will be over A and the two bins adjacent to it).
+     Explanation: The macro rebins from the input histogram to the output histogram.  Let A be a bin in the input histogram and let B be a bin in the output histogram.  If A lies entirely within B, the situation is simple: the contents of bin A will be added to bin B.  If A is split by one (or both) of the edges of B, a fit function is used to determine the fraction of the content of bin A that will be added to bin B.  The fitting may be done externally (set dofit==0), or this macro can be used to do the fit over a limited range (set dofit==1).  If dofit==1, any constraints applied to the fit function should be applied before it is passed to this macro.  Generally, the fit will be over A and the two bins adjacent to it.  Becuase the fit is done over only three bins, you cannot use fit functions with more than three free paramaters.  You should be careful about using functions with three free parameters, since you may end up fitting statistical noise.  For this reason, I use a simple exponential function in the example below.  You will need to judge for yourself whether a three-parameter function is really necessary to describe your distribution.  For the high-pT part of most spectra, an exponential should be sufficient.  Aroung the maximum of the pT distribution, it may be necessary to use a function with three free parameters.  If dofit==0 (i.e, you do the fit yourself), these warnings do no apply and you can use whatever complicated function you want.
 
      Suggested Usage:
 
@@ -71,11 +72,11 @@ void RebinSpectrum(TH1** hi,TH1** hf,TF1* f,int n=1,int dofit=0,TH1* ht=0){
       bi=hi[0]->GetXaxis()->GetBinLowEdge(k+1);
       di=hi[0]->GetXaxis()->GetBinWidth(k);
 
-      if(bi<af+e || ai>bf-e) continue;//bin k of hi completely outside bin l of hf
-      else if(ai>=af-e && bi<=bf+e){
+      if(bi<=af+e || bf<=ai+e) continue;//bin k of hi completely outside bin l of hf
+      else if(af<=ai+e && bi<=bf+e){
 	//bin k of hi completely contained within bin l of hf
 	d=1.;
-      }else if(ai<af-e || bi>bf-e){
+      }else if(ai<=af+e || bf<=bi+e){
 	//bin k of hi is split by the edge(s) of bin l of hf
 	cerr<<"Info in RebinSpectrum(): splitting hi("<<ai<<","<<bi<<") hf("<<af<<","<<bf<<")"<<endl;
 	if(dofit){
@@ -93,13 +94,13 @@ void RebinSpectrum(TH1** hi,TH1** hf,TF1* f,int n=1,int dofit=0,TH1* ht=0){
 	  ht->Fit(f,"NR");
 	}
 
-	if(ai<af-e && bi<bf-e){
+	if(ai<=af+e && bi<=bf+e){
 	  //bin k of hi is split by the low edge of bin l of hf
 	  d=f->Integral(af,bi)/f->Integral(ai,bi);
-	}else if(ai<bf-e && bi>bf+e){
+	}else if(af<=ai+e && bf<=bi+e){
 	  //bin k of hi is split by the high edge of bin l of hf
 	  d=f->Integral(ai,bf)/f->Integral(ai,bi);
-	}else if(ai<af-e && bi>bf+e){
+	}else if(ai<=af+e && bf<=bi+e){
 	  //bin k of hi completely contains bin l of hf
 	  d=f->Integral(af,bf)/f->Integral(ai,bi);
 	}else{
