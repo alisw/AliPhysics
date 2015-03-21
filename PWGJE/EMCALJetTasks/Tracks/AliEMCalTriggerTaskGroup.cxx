6e764c5e9960738358f8e1db2e57b8e9b8ec6f8c
mfasel <markus.fasel@cern.ch>
2015-03-21 03:01:32 +0100

    Fixes in the task group
    - Add copy constructor and assignment operator
    - Delegate copy process to the copy function (for now performing flat
      copies)
    - Implement function setting weight handler, which publishes the weight
      handler also to the analysis components
    - Migrate existing documentation to doxygen

--- PWGJE/EMCALJetTasks/Tracks/AliEMCalTriggerTaskGroup.cxx
+++ PWGJE/EMCALJetTasks/Tracks/AliEMCalTriggerTaskGroup.cxx
@@ -12,13 +12,6 @@
  * about the suitability of this software for any purpose. It is          *
  * provided "as is" without express or implied warranty.                  *
  **************************************************************************/
-/*
- * Group of analysis components with the same event selection
- * Analysis components are initialised via the Initialise function, and executed, if
- * the event is selected, via the function process
- *
- *   Author: Markus Fasel
- */
 #include <THashList.h>
 #include <TList.h>
 #include "AliEMCalTriggerTracksAnalysisComponent.h"
@@ -26,11 +19,15 @@
 
 #include "AliEMCalTriggerTaskGroup.h"
 
+/// \cond CLASSIMP
 ClassImp(EMCalTriggerPtAnalysis::AliEMCalTriggerTaskGroup)
+/// \endcond
 
 namespace EMCalTriggerPtAnalysis {
 
-//______________________________________________________________________________
+/**
+ * Dummy constructor, not to be used
+ */
 AliEMCalTriggerTaskGroup::AliEMCalTriggerTaskGroup() :
     TNamed(),
     fAnalysisComponents(NULL),
@@ -39,12 +36,11 @@ AliEMCalTriggerTaskGroup::AliEMCalTriggerTaskGroup() :
     fKineCuts(NULL),
     fWeightHandler(NULL)
 {
-  /*
-   * Dummy constructor, not to be used
-   */
 }
 
-//______________________________________________________________________________
+/**
+ * Main constructor: to be used by the users
+ */
 AliEMCalTriggerTaskGroup::AliEMCalTriggerTaskGroup(const char* name) :
     TNamed(name, ""),
     fAnalysisComponents(NULL),
@@ -53,29 +49,53 @@ AliEMCalTriggerTaskGroup::AliEMCalTriggerTaskGroup(const char* name) :
     fKineCuts(NULL),
     fWeightHandler(NULL)
 {
-  /*
-   * Main constructor: to be used by the users
-   */
   fAnalysisComponents = new TObjArray();
   fAnalysisComponents->SetOwner();
 }
 
-//______________________________________________________________________________
+/**
+ * Copy constructor. Copy process is delegated to the copy function handling both copy constructor
+ * and assignment operator.
+ * \param ref Reference for the copy
+ */
+AliEMCalTriggerTaskGroup::AliEMCalTriggerTaskGroup(const AliEMCalTriggerTaskGroup& ref):
+    TNamed(ref),
+    fAnalysisComponents(NULL),
+    fEventSelection(NULL),
+    fBinning(NULL),
+    fKineCuts(NULL),
+    fWeightHandler(NULL)
+{
+  ref.Copy(*this);
+}
+
+/**
+ * Copy constructor. Copy process is delegated to the copy function handling both copy constructor
+ * and assignment operator.
+ * \param ref Reference for the copy
+ */
+AliEMCalTriggerTaskGroup& AliEMCalTriggerTaskGroup::operator=(const AliEMCalTriggerTaskGroup& ref) {
+  TNamed::operator=(ref);
+  if(this != &ref){
+    ref.Copy(*this);
+  }
+  return *this;
+}
+
+/**
+ * Destructor, cleaning up objects belonging to the task group (analysis components and event selection)
+ */
 AliEMCalTriggerTaskGroup::~AliEMCalTriggerTaskGroup() {
-  /*
-   * Destructor
-   */
   if(fEventSelection) delete fEventSelection;
   if(fAnalysisComponents) delete fAnalysisComponents;
 }
 
-//______________________________________________________________________________
+/**
+ * Initialise all analysis components. Build a global histlist for the full group
+ *
+ * \return: the global histogram list
+ */
 TList *AliEMCalTriggerTaskGroup::InitialiseAnalysisComponents() {
-  /*
-   * Initialise all analysis components. Build a global histlist for the full group
-   *
-   * @return: the global histogram list
-   */
   TIter compIter(fAnalysisComponents);
   AliEMCalTriggerTracksAnalysisComponent *ana(NULL);
   // Build a global histogram list
@@ -93,13 +113,12 @@ TList *AliEMCalTriggerTaskGroup::InitialiseAnalysisComponents() {
   return histlist;
 }
 
-//______________________________________________________________________________
+/**
+ * Run analysis of the different groups. Apply event selection if requested;
+ *
+ * \param event The combined event data
+ */
 void AliEMCalTriggerTaskGroup::Process(const AliEMCalTriggerEventData* const event) {
-  /*
-   * Run analysis of the different groups. Apply event selection if requested;
-   *
-   * @param event: The combined event data
-   */
   if(fEventSelection && !fEventSelection->IsEventSelected(event)) return;
   TIter compIter(fAnalysisComponents);
   AliEMCalTriggerTracksAnalysisComponent *ana(NULL);
@@ -107,29 +126,58 @@ void AliEMCalTriggerTaskGroup::Process(const AliEMCalTriggerEventData* const eve
     ana->Process(event);
 }
 
-//______________________________________________________________________________
+/**
+ * Add new analysis component to the task group
+ *
+ * \param analysis: the analysis component to be added
+ */
 void AliEMCalTriggerTaskGroup::AddAnalysisComponent(AliEMCalTriggerTracksAnalysisComponent * const analysis) {
-  /*
-   * Add new analysis component to the task group
-   *
-   * @param analysis: the analysis component to be added
-   */
+  if(fWeightHandler) analysis->SetWeightHandler(fWeightHandler);
   fAnalysisComponents->Add(analysis);
 }
 
-//______________________________________________________________________________
+/**
+ * Forward trigger decision to the analysis components
+ *
+ * \param trigger: the trigger decision
+ */
 void EMCalTriggerPtAnalysis::AliEMCalTriggerTaskGroup::SetTriggerDecision(
     const AliEMCalTriggerAnaTriggerDecision* trigger) {
-  /*
-   * Forward trigger decision to the analysis components
-   *
-   * @param trigger: the trigger decision
-   */
   AliEMCalTriggerTracksAnalysisComponent *myana(NULL);
   TIter compIter(fAnalysisComponents);
   while((myana = dynamic_cast<AliEMCalTriggerTracksAnalysisComponent *>(compIter())))
     myana->SetTriggerDecision(trigger);
 }
 
+/**
+ * Set the weight handler to the group. If components are registered, publish the weight handler
+ * to them
+ * \param handler The weight handler for the group
+ */
+void AliEMCalTriggerTaskGroup::SetWeightHandler(const AliEMCalTriggerWeightHandler* handler) {
+  fWeightHandler = handler;
+  for(TIter compIter = TIter(fAnalysisComponents).Begin(); compIter != TIter::End(); ++compIter){
+    AliEMCalTriggerTracksAnalysisComponent *mycomp = static_cast<AliEMCalTriggerTracksAnalysisComponent *>(*compIter);
+    mycomp->SetWeightHandler(fWeightHandler);
+  }
+}
+
+/**
+ * Copy function. Does a flat copy of all elements which are not under control of the task group. Will
+ * make a flat copy of all analysis components as well for the moment.
+ * \param other Target where the information from this object is set to.
+ */
+void AliEMCalTriggerTaskGroup::Copy(TObject& other) const {
+  AliEMCalTriggerTaskGroup &target = static_cast<AliEMCalTriggerTaskGroup &>(other);
+  target.fKineCuts = fKineCuts;
+  target.fBinning = fBinning;
+  target.fWeightHandler = fWeightHandler;
+  target.fEventSelection = new AliEMCalTriggerEventSelection(*fEventSelection);
+  target.fAnalysisComponents = new TObjArray();
+  target.fAnalysisComponents->SetOwner(false);
+  for(TIter compIter = TIter(fAnalysisComponents).Begin(); compIter != TIter::End(); ++compIter){
+    target.fAnalysisComponents->Add(*compIter);
+  }
+}
 
 } /* namespace EMCalTriggerPtAnalysis */
