3c9c8be74368654ebcea623c08b2567b39325925
cholm <Christian.Holm.Christensen@cern.ch>
2015-03-23 14:18:35 +0100

    Added scripts to draw dN/deta results with systematics split out

---
+++ PWGLF/FORWARD/analysis2/dndeta/SysErrorAdder.C
@@ -0,0 +1,364 @@
+#ifndef __CINT__
+# include <TString.h>
+# include <TH1.h>
+# include "GraphSysErr.C"
+# include <TLegend.h>
+# include <TLegendEntry.h>
+# include <TObjString.h>
+#else
+class GraphSysErr;
+class TH1;
+class TLegend;
+#endif
+
+struct SysErrorAdder
+{
+  TString fSys;
+  UShort_t fSNN;
+  TString fTrig;
+  /** 
+   * Constructor 
+   * 
+   * @param sys   System 
+   * @param sNN   Collision energy 
+   * @param trig  Trigger 
+   */
+  SysErrorAdder(const TString& sys,
+		UShort_t       sNN,
+		const TString& trig)
+    : fSys(sys), fSNN(sNN), fTrig(trig)
+  {}
+  virtual ~SysErrorAdder() {}
+  virtual const char* GetTriggerName() const { return "Trigger"; }
+  void ModError(GraphSysErr* gse, Int_t id, Style_t fill, TLegend* l) const
+  {
+    gse->SetSysFillColor(id,gse->GetMarkerColor());
+    gse->SetSysLineColor(id,gse->GetMarkerColor());
+    gse->SetSysLineWidth(id, 1);
+    gse->SetSysFillStyle(id, fill);
+    gse->SetSysOption(id, GraphSysErr::kBox);
+    
+    if (!l) return;
+    TLegendEntry* e = l->AddEntry(Form("sys%02d", id),
+				  gse->GetSysTitle(id), 
+				  "f");
+    e->SetFillStyle(fill);
+    e->SetFillColor(kBlack);
+    e->SetLineColor(kBlack);
+    e->SetLineWidth(1);
+  }
+  /** 
+   * Declare the systematic error from the trigger 
+   * 
+   * @param gse Graph 
+   * 
+   * @return Id of systmatic error or -1
+   */
+  Int_t  MakeTrigger(GraphSysErr* gse, TLegend* l) const
+  {
+    Double_t low = 0,high = 0;
+    GetTrigger(low,high);
+    if (low == 0 && high == 0) return -1;
+    
+    Int_t id = gse->DefineCommon(GetTriggerName(), true, low, high);
+    ModError(gse, id, 3144, l);
+    return id;
+  }
+  /** 
+   * Declare the systematic error from the merging 
+   * 
+   * @param gse Graph 
+   * 
+   * @return Id of systmatic error or -1
+   */
+  Int_t MakeMerging(GraphSysErr* gse, TLegend* l) const
+  {
+    if (GetMerging() <= 0) return -1;
+    Int_t id = gse->DeclarePoint2Point("Merging", true); 
+    ModError(gse, id, 3001, l);
+    return id;
+  }
+  /** 
+   * Declare the systematic error from the denisty calculation
+   * 
+   * @param gse Graph 
+   * 
+   * @return Id of systmatic error or -1
+   */
+  Int_t MakeDensity(GraphSysErr* gse, TLegend* l) const
+  {
+    if (GetDensity() <= 0) return -1;
+    Int_t id = gse->DeclarePoint2Point("Density", true);
+    ModError(gse, id, 3002, l);
+    return id;
+  }
+  /** 
+   * Declare the systematic error from the empirical correction 
+   * 
+   * @param gse Graph 
+   * 
+   * @return Id of systmatic error or -1
+   */
+  virtual Int_t MakeEmpirical(GraphSysErr* gse, TLegend* l) const
+  {
+    if (GetEmpirical() <= 0) return -1;
+    Int_t id = gse->DeclarePoint2Point("Empirical", true); 
+    ModError(gse, id, 3244, l);
+    return id;
+  }
+  /** 
+   * @return The systematic error from merging 
+   */
+  virtual Double_t GetMerging() const { return 0.01; }
+  /** 
+   * @return The systematic error from density calculator 
+   */
+  virtual Double_t GetDensity() const { return 0.01; }
+  /** 
+   * @return The systematic error from empirical correction 
+   */
+  virtual Double_t GetEmpirical() const { return 0.05; }
+  /** 
+   * Get trigger systematic error 
+   * 
+   * @param low   On return, the low error 
+   * @param high  On return, the high error
+   */
+  virtual void GetTrigger(Double_t& low, Double_t& high) const = 0;
+  /** 
+   * Create a graph 
+   * 
+   * @param h histogram 
+   * 
+   * @return Graph 
+   */  
+  virtual GraphSysErr* Make(TH1* h, TLegend* l)
+  {
+    GraphSysErr* gse = new GraphSysErr(10);
+    gse->SetSumLineColor(kRed+2);
+    gse->SetSumLineWidth(2);
+    gse->SetSumTitle("All errors");
+    gse->SetSumOption(GraphSysErr::kHat);
+    gse->SetLineColor(h->GetLineColor());
+    gse->SetMarkerColor(h->GetMarkerColor());
+    gse->SetFillColor(h->GetFillColor());
+    gse->SetMarkerStyle(h->GetMarkerStyle());
+    gse->SetFillStyle(h->GetFillStyle());
+    gse->SetXTitle("#it{#eta}");
+    gse->SetYTitle("1/#it{N} d#it{N}_{ch}/d#it{#eta}");
+		      
+
+    MakeTrigger(gse, l);
+    Int_t idMerge = MakeMerging(gse, l); 
+    Int_t idDens  = MakeDensity(gse, l); 
+    Int_t idEmp   = MakeEmpirical(gse, l);
+
+    Int_t cnt = 0;
+    for (Int_t i = 1; i <= h->GetNbinsX(); i++) {
+      Double_t y  = h->GetBinContent(i);
+      if (y < 1e-6) continue;
+      Double_t ey = h->GetBinError(i);
+      Double_t ex = h->GetXaxis()->GetBinWidth(i)/2;
+      Double_t x  = h->GetXaxis()->GetBinCenter(i);
+
+      gse->SetPoint(cnt, x, y);
+      gse->SetPointError(cnt, ex);
+      gse->SetStatError(cnt, ey);
+
+      if (idMerge>=0) gse->SetSysError(idMerge, cnt, ex, GetMerging());
+      if (idDens >=0) gse->SetSysError(idDens,  cnt, ex, GetDensity());
+      if (idEmp  >=0) gse->SetSysError(idEmp,   cnt, ex, GetEmpirical());
+      cnt++;
+    }
+    return gse;
+  }
+};
+/**
+ * For pp INEL results
+ * 
+ */
+struct INELAdder : public SysErrorAdder
+{
+  Double_t fLow;
+  Double_t fHigh;
+  /** 
+   * Constructor 
+   * 
+   * @param sys Collision system 
+   * @param sNN Collision energy
+   */
+  INELAdder(const TString& sys, UShort_t sNN)
+    : SysErrorAdder(sys, sNN, "INEL"), fLow(0), fHigh(0)
+  {
+    if (fSys.EqualTo("pp", TString::kIgnoreCase)) {
+      switch (fSNN) {
+      case  900:   fLow = 0.001;  fHigh = 0.003; break;
+      case 2760:   fLow = 0.0035; fHigh = 0.006; break;
+      case 7000: 
+      case 8000:   fLow = 0.003;  fHigh = 0.006; break;
+      default: break;
+      }
+    }
+  }
+  /** 
+   * Get trigger systematic error 
+   * 
+   * @param low   On return, the low error 
+   * @param high  On return, the high error
+   */
+  virtual void GetTrigger(Double_t& low, Double_t& high) const
+  {
+    low  = fLow;
+    high = fHigh;
+  }
+};
+/**
+ * For pp INEL results
+ * 
+ */
+struct INELGt0Adder : public SysErrorAdder
+{
+  /** 
+   * Constructor 
+   * 
+   * @param sys Collision system 
+   * @param sNN Collision energy
+   */
+  INELGt0Adder(const TString& sys, UShort_t sNN)
+    : SysErrorAdder(sys, sNN, "INEL>0")
+  {}
+  /** 
+   * Get trigger systematic error 
+   * 
+   * @param low   On return, the low error 
+   * @param high  On return, the high error
+   */
+  virtual void GetTrigger(Double_t& low, Double_t& high) const
+  {
+    low = high = 0;
+  }
+};
+/**
+ * For pp NSD results
+ * 
+ */
+struct NSDAdder : public SysErrorAdder
+{
+  Double_t fValue;
+  /** 
+   * Constructor 
+   * 
+   * @param sys Collision system 
+   * @param sNN Collision energy
+   */
+  NSDAdder(const TString& sys, UShort_t sNN)
+    : SysErrorAdder(sys, sNN, "NSD"), fValue(0)
+  {
+    if (fSys.EqualTo("pp", TString::kIgnoreCase)) {
+      switch (fSNN) {
+      case  900:   fValue = 0.02; break;
+      case 2760:   fValue = 0.03; break;
+      case 7000: 
+      case 8000:   fValue = 0.02; break;
+      default: break;
+      }
+    }
+  }
+  /** 
+   * Get trigger systematic error 
+   * 
+   * @param low   On return, the low error 
+   * @param high  On return, the high error
+   */
+  virtual void GetTrigger(Double_t& low, Double_t& high) const
+  {
+    low = high = fValue;
+  }
+};
+/**
+ * For centrality 
+ * 
+ */
+struct CENTAdder : public SysErrorAdder
+{
+  Double_t fCent;
+  Double_t fValue;
+  Double_t fMin;
+  Double_t fMax;
+  /** 
+   * Constructor 
+   * 
+   * @param sys Collision system 
+   * @param sNN Collision energy
+   * @param Method 
+   */
+  CENTAdder(const TString& sys, UShort_t sNN, const TString& method)
+    : SysErrorAdder(sys, sNN, method), fCent(0), fValue(0)
+  {
+    if (fSys.EqualTo("pPb", TString::kIgnoreCase)) {
+      fMin = 0.02; fMax = 0.04;
+    }
+    else if (fSys.EqualTo("Pbp", TString::kIgnoreCase)) {
+      fMin = 0.04; fMax = 0.06;
+    }
+    else {
+      fMin = 0.01; fMax = 0.02;
+    }
+  }
+  const char* GetTriggerName() const 
+  {
+    static TString n;
+    n = fTrig;
+    n.ReplaceAll("CENT", "Centrality (");
+    n.Append(")");
+    return n.Data();
+  }
+  /** 
+   * Get trigger systematic error 
+   * 
+   * @param low   On return, the low error 
+   * @param high  On return, the high error
+   */
+  virtual void GetTrigger(Double_t& low, Double_t& high) const
+  {
+    low = high = (fCent/100) * (fMax-fMin) + fMin;
+    Printf("Trigger error for centrality = %f -> %f", fCent, low);
+  }
+  /** 
+   * Get centrality 
+   * 
+   * @param h Histogram 
+   * 
+   * @return Centrality 
+   */
+  Double_t GetCentrality(TH1* h) 
+  { 
+    TString name(h->GetName());
+    Int_t idx = name.Index("_cent");
+    name.Remove(0,idx+5);
+    name.ReplaceAll("d", ".");
+    name.ReplaceAll("_", " ");
+    TObjArray* tokens  = name.Tokenize(" ");
+    TObjString* first  = static_cast<TObjString*>(tokens->At(0));
+    TObjString* second = static_cast<TObjString*>(tokens->At(1));
+    Double_t    low    = first->String().Atof();
+    Double_t    high   = second->String().Atof();
+    fCent              = (low+high)/2;
+    Printf("Centrality from %s -> [%f,%f] -> %f", h->GetName(), 
+	   low, high, fCent);
+    return fCent;
+  }
+  
+  /** 
+   * Create a graph 
+   * 
+   * @param h histogram 
+   * 
+   * @return Graph 
+   */  
+  virtual GraphSysErr* Make(TH1* h, TLegend* l)
+  {
+    fCent = GetCentrality(h);
+    return SysErrorAdder::Make(h, l);
+  }
+};
---
+++ PWGLF/FORWARD/analysis2/dndeta/WithSysError.C
@@ -0,0 +1,122 @@
+/** 
+ * 
+ * 
+ * @param system 
+ * @param sNN 
+ * @param trigger 
+ * @param option 
+ * @param rebinned 
+ * @param empirical 
+ */
+void
+WithSysError(const TString&  system, 
+	     UShort_t        sNN, 
+	     const TString&  trigger, 
+	     const Option_t* option="e5",
+	     Bool_t          rebinned=true, 
+	     Bool_t          empirical=true)
+{
+  const char* fwd =
+    gSystem->ExpandPathName("$ALICE_PHYSICS/PWGLF/FORWARD/analysis2");
+  gROOT->SetMacroPath(Form("%s:%s/dndeta:%s/gse", gROOT->GetMacroPath(),
+			   fwd, fwd));
+  gSystem->AddIncludePath(Form("-I%s/dndeta -I%s/gse", fwd,fwd));
+
+  if (!gROOT->GetClass("Drawer"))       gROOT->LoadMacro("Drawer.C+g");
+  if (!gROOT->GetClass("GraphSysErr"))  gROOT->LoadMacro("GraphSysErr.C+g");
+  if (!gROOT->GetClass("SysErrorAdder"))gROOT->LoadMacro("SysErrorAdder.C+g");
+
+
+  TCanvas* c = new TCanvas("C", "C");
+  c->SetTopMargin(0.02);
+  c->SetRightMargin(0.02);
+
+  Double_t    ly1     = 0.7;
+  Double_t    ly2     = 0.9;
+  const char* trigs[] = { trigger.Data(), 0 };
+  const char* exps[]  = { "ALICE", "WIP", 0 };
+  TLegend*    tl      = new TLegend(0.15,ly1,0.3,ly2);
+  tl->SetBorderSize(0);
+  tl->SetFillStyle(0);
+  TObjArray   u;
+  Double_t*   effs    = 0;
+  if (sNN == 8000 && system.EqualTo("pp", TString::kIgnoreCase)) {
+    effs = new Double_t[2];
+    effs[1] = 0;
+    if (trigger.EqualTo("INEL",TString::kIgnoreCase))
+      effs[0] = 0.85;
+    else if (trigger.EqualTo("NSD",TString::kIgnoreCase) ||
+	     trigger.EqualTo("V0AND",TString::kIgnoreCase)) {
+      trigger  = "NSD";
+      trigs[0] = "NSD";
+      effs[0]  = 0.93;
+    }
+  }
+
+  TLegend* el = new TLegend(tl->GetX2(),ly1,tl->GetX2()+.2,ly2);
+  el->SetBorderSize(0);
+  el->SetFillStyle(0);
+  SysErrorAdder* adder = 0;
+  if (trigger.EqualTo("INEL",TString::kIgnoreCase))
+    adder = new INELAdder(system, sNN);
+  else if (trigger.EqualTo("NSD",TString::kIgnoreCase))
+    adder = new NSDAdder(system, sNN);
+  else 
+    adder = new CENTAdder(system, sNN, trigger);
+  
+  TPair* dataOther = Drawer::GetDataOther(tl, u, system, sNN, trigs,
+					  exps, option, rebinned,
+					  empirical, effs);
+  
+  if (!dataOther || !dataOther->Key()) {
+    Error("", "No data found %s", canvas->GetTitle());
+    return;
+  }
+  THStack*     data  = static_cast<THStack*>(dataOther->Key());
+  TMultiGraph* other = static_cast<TMultiGraph*>(dataOther->Value());
+
+  TList* outList = new TList;
+  TIter next(data->GetHists());
+  TH1* h = 0;
+  Bool_t first = true;
+  const char* opt = "stack stat quad split west west";
+  while ((h = static_cast<TH1*>(next()))) {
+    TString n(h->GetName());
+    if (n.Contains("syserror", TString::kIgnoreCase)) continue;
+
+    GraphSysErr* gse = adder->Make(h, (first ? el : 0));
+    gse->SetTitle(""); // Form("%s @ %d (%s)", system.Data(), sNN, trigger.Data()));
+    gse->Draw(Form("%s %s", (first ? "axis" : ""), opt));
+    outList->Add(gse);
+    if (first) { 
+      TMultiGraph* axis = gse->GetMulti();
+      TH1*         frame = axis->GetHistogram();
+      frame->SetMinimum(0);
+      frame->SetMaximum(frame->GetMaximum()*1.3);
+    }
+    // gse->Export(false);
+    first = false;
+    }
+  other->Draw("p");
+  
+  TParameter<int>* um =
+    new TParameter<int>("PWG-LF/GEO - work in progress", 20);
+  um->SetUniqueID(kBlack);
+  u.Add(um);
+
+  TLegend* ul = Drawer::MakeUniqueLegend(.6,.8,.95,.97, u, 1);
+  ul->Draw();
+  tl->Draw();
+  el->Draw();
+
+  TLatex* ltx = Drawer::MakeTitle(.2, .98, system, sNN, trigger);
+  ltx->SetTextAlign(13);
+
+  TString outName(Form("%s_%04d_%s.root", system.Data(), sNN, trigger.Data()));
+  TFile* out = TFile::Open(outName.Data(), "RECREATE");
+  outList->Write();
+  out->Write();
+  
+}
+
+  
