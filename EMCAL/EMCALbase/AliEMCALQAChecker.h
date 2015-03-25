010a9aed79829ee0c21b25b22c9577dff88a12fa
dsilverm <david.silvermyr@gmail.com>
2015-03-25 08:40:45 +0100

    add text also for DCal SMs

--- EMCAL/EMCALbase/AliEMCALQAChecker.h
+++ EMCAL/EMCALbase/AliEMCALQAChecker.h
@@ -92,7 +92,7 @@ private:
   TLine *     fLineRow[fgknSectLines]       ; //! line to distinguish the different SM sectors (0-5, 9-12) 
   TPaveText * fText          ;  //! Information text for the quality of each SM
 	TPaveText * fTextL1[3];       //! Information text for the quality of L1 plots (3 plots in total)
-  ClassDef(AliEMCALQAChecker,4)  // description 
+  ClassDef(AliEMCALQAChecker,5)  // description 
 
 };
 
