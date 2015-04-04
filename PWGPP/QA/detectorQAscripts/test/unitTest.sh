22a31db8f39bc10467d5b68521d222b649903d16
mivanov <marian.ivanov@cern.ch>
2015-04-04 10:35:49 +0200

    PWGPP-41 - unit test for the detector QA (currently for T0)

---
+++ PWGPP/QA/detectorQAscripts/test/unitTest.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#--------------------------------------------------------------------------------------------------
+# Unit test for the detector QA script
+# Currently implemented for the T0
+
+#--------------------------------------------------------------------------------------------------
+# Example to  run the test: 
+#
+# ( source /u/miranov/alicesw/aliphysics/master/src/PWGPP/QA/detectorQAscripts/test/unitTest.sh;  setupUnitTestMI; testQAT0; )
+#
+source $ALICE_PHYSICS/PWGPP/scripts/alilog4bash.sh
+source $ALICE_PHYSICS/PWGPP/scripts/utilities.sh
+#
+# Configuration part:
+#
+setupUnitTestMI(){
+    #
+    #
+    #
+    export unitTestDir="${NOTES}/QA/ATO-102/data/UnitTest"
+    export configFile="${NOTES}/QA/ATO-102/code/runQA-submitting.config"
+    echo /hera/alice/alienQA/alice/data/2013/LHC13e/000196309/pass2/QA_merge_archive.zip  > input.list
+}
+
+testQAT0(){
+#
+# UnitTest T0 
+# To check success number of the output gif files used
+
+    $ALICE_PHYSICS/../src/PWGPP/QA/scripts/runQA.sh inputList="input.list" includeDetectors="T0" configFile="${configFile}"  workingDirectory="${unitTestDir}/T0/" outputDir="${unitTestDir}/T0/" logDirectory="${unitTestDir}/T0/"
+    ngifT0=`find $wdir -iname "*gif" | grep -c gif`
+    [ $ngifT0 = 50 ] && export unitTestT0=OK;
+    [ $ngifT0 != 50 ] && export unitTestT0=FAILED;
+    [ $ngifT0 = 50 ] && alilog_success      "unitTest.sh testQAT0 OK"
+    [ $ngifT0 != 50 ] && alilog_error      "unitTest.sh testQAT0 FAILED"
+}
+
+
+
+
+
