594db48de7ba844911beb85edc992d733860fcef
arossi <andrea.rossi@cern.ch>
2015-04-08 22:48:17 +0200

    New HF di-jet task (Deepa) + inserting in compilations code for b-taggin with displaced vertices.

--- PWGHF/jetsHF/PWGHFjetsHFLinkDef.h
+++ PWGHF/jetsHF/PWGHFjetsHFLinkDef.h
@@ -9,6 +9,13 @@
 #pragma link C++ class AliHFJetsTagging+;
 #pragma link C++ class AliHFJetTaggingIP+;
 #pragma link C++ class AliAnalysisTaskEmcalJetBJetTaggingIP+;
+#pragma link C++ class AliRDHFJetsCuts+;
+#pragma link C++ class AliRDHFJetsCutsVertex+;
+#pragma link C++ class AliHFJetsTaggingVertex+;
+#pragma link C++ class AliHFJetsContainer+;
+#pragma link C++ class AliHFJetsContainerVertex+;
+#pragma link C++ class AliAnalysisTaskSEHFJets+;
+#pragma link C++ class AliAnalysisTaskHFEDiJets+;
 
 
 #endif
