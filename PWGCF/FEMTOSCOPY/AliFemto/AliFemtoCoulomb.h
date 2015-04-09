7c41a6c96d8ef42795ee4380bc2341775e001f70
akubera <Andrew.Kubera@cern.ch>
2015-04-09 02:52:46 -0400

    Updated comments/dox and formatting for AliFemtoCoulomb files

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCoulomb.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCoulomb.h
@@ -1,12 +1,11 @@
-///////////////////////////////////////////////////////////////////////////
-//                                                                       //
-// AliFemtoCoulomb: This is a Coulomb correction class which             //
-//  1. Reads in the dat from a file                                      //  
-//  2. Performs a linear interpolation in R and creates any array of     //
-//     interpolations                                                    //
-//  3. Interpolates in eta and returns the Coulomb correction to user    //
-//                                                                       //
-///////////////////////////////////////////////////////////////////////////
+/// \class AliFemtoCoulomb
+/// \brief Coulomb correction class
+///
+/// This is a Coulomb correction class which:
+/// 1. Reads in the dat from a file
+/// 2. Performs a linear interpolation in R and creates any array of
+///    interpolations
+/// 3. Interpolates in eta and returns the Coulomb correction to user
 
 #ifndef ALIFEMTOCOULOMB_H
 #define ALIFEMTOCOULOMB_H
@@ -40,24 +39,26 @@ public:
   double CoulombCorrect(const AliFemtoPair* pair, const double& radius);
   double CoulombCorrect(const double& mass, const double& charge,
 		        const double& radius, const double& qInv);
-  TH1D* CorrectionHistogram(const double& mass1, const double& mass2, const int& nBins, 
+  TH1D* CorrectionHistogram(const double& mass1, const double& mass2, const int& nBins,
 				    const double& low, const double& high);
 #ifdef __ROOT__
   TH1D* CorrectionHistogram(const TH1D*, const double);
   TH3D* CorrectionHistogram(const TH3D*, const double);
 #endif
 private:
-  double Eta(const AliFemtoPair* pair);          // Calculates eta
-  void CreateLookupTable(const double& radius);  // Creates look-up table
-  const char* fFile;                             // File to interpolate corrections from    
-  double fRadius;                                // Radius from previous iteration
-  double fZ1Z2;                                  // Charge product of particles
-  double fEta[1000];                             // interpolated Coulomb correction table
-  double fCoulomb[1000];                         // interpolated Coulomb correction table
-  int fNLines;                                   // Number of Eta's in lookup-table
+  double Eta(const AliFemtoPair* pair);          ///< Calculates eta
+  void CreateLookupTable(const double& radius);  ///< Creates look-up table
+  const char* fFile;                             ///< File to interpolate corrections from
+  double fRadius;                                ///< Radius from previous iteration
+  double fZ1Z2;                                  ///< Charge product of particles
+  double fEta[1000];                             ///< interpolated Coulomb correction table
+  double fCoulomb[1000];                         ///< interpolated Coulomb correction table
+  int fNLines;                                   ///< Number of Eta's in lookup-table
 
 #ifdef __ROOT__
+  /// \cond CLASSIMP
   ClassDef(AliFemtoCoulomb, 0)
+  /// \endcond
 #endif
 };
 
