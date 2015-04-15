58b3f204dbc04ba3e86e59b6f5739185799cafa2
zconesa <zaida.conesa.del.valle@cern.ch>
2015-04-15 13:54:30 +0200

    AddTask for the instances of the single track efficiency calculation required for the automatic Montecarlo QA trending

---
+++ PWGPP/EvTrkSelection/macros/AddSingleTrackEfficiencyTaskForAutomaticQA.C
@@ -0,0 +1,42 @@
+//
+// Add task to add the instances of the single track efficiency calculation
+//  required for the automatic Montecarlo QA trending. This concerns:
+//   charged particles with filter bit 0 or 4,
+//   and pion/proton/kaon/electron with filter bit 0
+//
+// Authors: Jitendra Kumar, Zaida Conesa del Valle
+//
+
+AliAnalysisTask *AddSingleTrackEfficiencyTaskForAutomaticQA(const Bool_t readAOD = 0, // Flag to read AOD:1 or ESD:0
+                                           ULong64_t triggerMask=AliVEvent::kAnyINT,
+                                           Bool_t useCentrality = kFALSE)
+{
+
+    Info("AliCFSingleTrackEfficiencyTaskForAutomaticQA","Setting up instances");
+    
+    // The AddTask
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/EvTrkSelection/AddSingleTrackEfficiencyTask.C");
+    
+    // Charged particles, filter bit 0
+    AliCFSingleTrackEfficiencyTask * nchFB0 = AddSingleTrackEfficiencyTask(readAOD,"NchFbit0",AliPID::kPion,0,triggerMask,useCentrality);
+    nchFB0->SetFilterType(0);
+    
+    // Charged particles, filter bit 4
+    AliCFSingleTrackEfficiencyTask * nchFB4 = AddSingleTrackEfficiencyTask(readAOD,"NchFbit4",AliPID::kPion,0,triggerMask,useCentrality);
+    nchFB4->SetFilterType(4);
+    
+    // Pions, filter bit 0
+    AliCFSingleTrackEfficiencyTask * pions = AddSingleTrackEfficiencyTask(readAOD,"PionFbit0",AliPID::kPion,211,triggerMask,useCentrality);
+    
+    // Kaons, filter bit 0
+    AliCFSingleTrackEfficiencyTask * kaons = AddSingleTrackEfficiencyTask(readAOD,"KaonFbit0",AliPID::kKaon,321,triggerMask,useCentrality);
+    
+    // Protons, filter bit 0
+    AliCFSingleTrackEfficiencyTask * protons = AddSingleTrackEfficiencyTask(readAOD,"ProtonFbit0",AliPID::kProton,2212,triggerMask,useCentrality);
+    
+    // Electrons, filter bit 0
+    AliCFSingleTrackEfficiencyTask * electrons = AddSingleTrackEfficiencyTask(readAOD,"ElectronFbit0",AliPID::kElectron,11,triggerMask,useCentrality);
+
+    
+    return;
+}
