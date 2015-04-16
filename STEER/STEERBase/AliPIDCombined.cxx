6e92028c6b4a2ba39e2f15303bee734980ad5624
shahoian <ruben.shahoyan@cern.ch>
2015-04-16 10:43:01 +0200

    Initialization of priors settings (F.Noferini)

--- STEER/STEERBase/AliPIDCombined.cxx
+++ STEER/STEERBase/AliPIDCombined.cxx
@@ -48,7 +48,7 @@ AliPIDCombined::AliPIDCombined():
 	TNamed("CombinedPID","CombinedPID"),
 	fDetectorMask(0),
 	fRejectMismatchMask(0x7F),
-	fEnablePriors(kTRUE),
+	fEnablePriors(kFALSE),
 	fSelectedSpecies(AliPID::kSPECIES),
 	fUseDefaultTPCPriors(kFALSE)
 {	
@@ -64,7 +64,7 @@ AliPIDCombined::AliPIDCombined(const TString& name,const TString& title):
 	TNamed(name,title),
 	fDetectorMask(0),
 	fRejectMismatchMask(0x7F),
-	fEnablePriors(kTRUE),
+	fEnablePriors(kFALSE),
 	fSelectedSpecies(AliPID::kSPECIES),
 	fUseDefaultTPCPriors(kFALSE)
 {
