b7543c28ebdf7d7c12c1cafaf4bb1b1e9bec8e84
hristov <Peter.Hristov@cern.ch>
2015-04-20 15:15:00 +0200

    Load libpythia6 before CheckESD.C is called

--- test/ppbench/runtest.sh
+++ test/ppbench/runtest.sh
@@ -18,7 +18,7 @@ aliroot -b -q $1sim.C      2>&1 | tee sim.log
 mv syswatch.log simwatch.log
 aliroot -b -q $1rec.C      2>&1 | tee rec.log
 mv syswatch.log recwatch.log
-aliroot -b -q ${ALICE_ROOT}/STEER/macros/CheckESD.C 2>&1 | tee check.log
+aliroot -b -q check.C 2>&1 | tee check.log
 aliroot -b -q aod.C 2>&1 | tee aod.log
 
 cd recraw
