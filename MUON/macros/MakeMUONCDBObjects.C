8529d969fb7118602044359cc5e448f18a480fcf
rgrosso <Raffaele.Grosso@cern.ch>
2015-05-05 16:24:40 +0200

    Add MUON macro to make CDB objects

---
+++ MUON/macros/MakeMUONCDBObjects.C
@@ -0,0 +1,20 @@
+#include "AliMpCDB.h"
+#include "AliMUONCDB.h"
+#include "MUON/macros/MakeMUONRecoParamArray.C"
+void MakeMUONCDBObjects()
+{
+  AliMpCDB::LoadAll2(); 
+  AliMUONCDB::WriteTrigger();  // MUON/Calib/LocalTriggerBoardMasks MUON/Calib/TriggerDCS MUON/Calib/TriggerEfficiency MUON/Calib/TriggerLut                    
+  AliMUONCDB::WriteTracker();  // MUON/Calib/MappingData MUON/Calib/MappingRunData MUON/Calib/HV MUON/Calib/Pedestals MUON/Calib/Gains MUON/Calib/Capacitances MUON/Calib/OccupancyMap MUON/Calib/RejectList MUON/Calib/Config
+  AliMUONCDB::WriteRegionalTriggerConfig();  //MUON/Calib/RegionalTriggerConfig 
+  AliMUONCDB::WriteGlobalTriggerConfig();    //MUON/Calib/GlobalTriggerCrateConfig
+  MakeMUONRecoParamArray();                  //MUON/Calib/RecoParam 
+}
+
+/* to be done
+MUON/Align/Baseline
+MUON/Align/Data
+MUON/Calib/GlobalTriggerBoardMasks
+MUON/Calib/Neighbours                                  on the TODO list
+MUON/Calib/RegionalTriggerBoardMasks
+*/
