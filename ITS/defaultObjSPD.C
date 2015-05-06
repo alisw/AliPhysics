6646c08a0f6e69c22186324647646cb76c794dd4
masera <massimo.masera@cern.ch>
2015-05-06 16:14:36 +0200

    macro to create SPD default calibration objects for SPD

---
+++ ITS/defaultObjSPD.C
@@ -0,0 +1,16 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include "AliCDBManager.h"
+#include "AliITSOnlineCalibrationSPDhandler.h"
+#include "AliITSTriggerConditions.h"
+#include "AliCDBRunRange.h"
+#endif
+void defaultObjSPD(Int_t runNrStart=0, Int_t runNrEnd=AliCDBRunRange::Infinity(), const Char_t* storage = "local:///tmp/defaultSPD/"){
+AliITSOnlineCalibrationSPDhandler *h = new AliITSOnlineCalibrationSPDhandler();
+h->WriteDeadToDB(runNrStart,runNrEnd,storage);
+h->WriteNoisyToDB(runNrStart,runNrEnd,storage);
+h->WriteSparseDeadToDB(runNrStart,runNrEnd,storage);
+h->ReadPITConditionsFromDB(0,"local://$ALICE_ROOT/OCDB/");
+AliITSTriggerConditions* tr = h->GetTriggerConditions();
+tr->ResetAll();
+h->WritePITConditionsToDB(runNrStart,runNrEnd,storage);
+}
