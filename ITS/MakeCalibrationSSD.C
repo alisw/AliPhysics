a51f0e43616a9a691e9aa79307cf56add74b9ac1
masera <massimo.masera@cern.ch>
2015-05-06 16:05:12 +0200

    updated macro to create the default OCDB for SSD

--- ITS/MakeCalibrationSSD.C
+++ ITS/MakeCalibrationSSD.C
@@ -1,24 +1,29 @@
 void MakeCalibrationSSD(Int_t firstRun=0,Int_t lastRun=999999999 ){
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Macro to generate and store the calibration files for SSD         
+  // Modified: Enrico Fragiacomo - 04/05/2015                          
+  // Generates:                                                        
+  //  1 file with calibration objects (AliITSNoiseSSDv2, AliITSPedestalSSDv2, AliITSGainSSDv2, AliITSBadChannelsSSDv2
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   if(!AliCDBManager::Instance()->IsDefaultStorageSet()) {
-    AliCDBManager::Instance()->SetDefaultStorage("local://$ALICE_ROOT/OCDB/");
+    AliCDBManager::Instance()->SetDefaultStorage("local://OCDB/");
   }
   
   AliCDBMetaData *md1= new AliCDBMetaData(); // metaData describing the object
   md1->SetObjectClassName("AliITSCalibration");
   md1->SetResponsible("Enrico Fragiacomo");
   md1->SetBeamPeriod(0);
-  md1->SetAliRootVersion("trunk090708"); //root version
-
+ 
   AliCDBId idNoiseSSD("ITS/Calib/NoiseSSD",firstRun, lastRun);
   AliCDBId idPedestalSSD("ITS/Calib/PedestalSSD",firstRun, lastRun);
   AliCDBId idGainSSD("ITS/Calib/GainSSD",firstRun, lastRun);
   AliCDBId idBadChannelsSSD("ITS/Calib/BadChannelsSSD",firstRun, lastRun);
   
-  AliITSNoiseSSD *noiseSSD = new AliITSNoiseSSD();
-  AliITSPedestalSSD *pedestalSSD = new AliITSPedestalSSD();
-  AliITSGainSSD *gainSSD = new AliITSGainSSD();
-  AliITSBadChannelsSSD *badchannelsSSD = new AliITSBadChannelsSSD();
+  AliITSNoiseSSDv2 *noiseSSD = new AliITSNoiseSSDv2();
+  AliITSPedestalSSDv2 *pedestalSSD = new AliITSPedestalSSDv2();
+  AliITSGainSSDv2 *gainSSD = new AliITSGainSSDv2();
+  AliITSBadChannelsSSDv2 *badchannelsSSD = new AliITSBadChannelsSSDv2();
 
   for(Int_t i=0; i<1698; i++) {
     for(Int_t j=0; j<768; j++) {
