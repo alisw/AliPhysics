ec68fa289d984b8ff4801f69bd6075c1953f0235
gconesab <gustavo.conesa.balbastre@cern.ch>
2015-05-15 12:03:27 +0200

    Move macros to new directories to try to put some order

--- EMCAL/macros/PeakFinder/MakeEMCALPF.C
+++ EMCAL/macros/PeakFinder/MakeEMCALPF.C
@@ -1,17 +1,10 @@
 void MakeEMCALPF(){
   const char* macroname = "MakeEMCALPF.C";
   TFile *f2 = new TFile("peakfindervectors2.root",  "read" ); 
+
   //  AliCaloPeakFinderVectors *pfv =  (AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors"); 
 
-  //  AliCaloPeakFinderVectors *pfv =  *((AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors")); 
-  // AliCaloPeakFinderVectors pfv =  *((AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors")); 
-  // AliCaloPeakFinderVectors *p = f2->Get("AliCaloPeakFinderVectors");
-  
-  // AliCaloPeakFinderVectors pfv =  *((AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors")); 
-  AliCaloPeakFinderVectors *pfv = (f2->Get("AliCaloPeakFinderVectors"));
-  
-  pfv->PrintVectors();
-  //  p->PrintVectors();
+  AliCaloPeakFinderVectors pfv =  *((AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors")); 
 
   f2->Close();
   {
@@ -23,38 +16,38 @@ void MakeEMCALPF(){
       return;
     }
     
-    
     Info(macroname,"Saving PF objects in CDB storage %s",
 	 Storage.Data());
     AliCDBManager* cdb = AliCDBManager::Instance();
 
+
     AliCDBStorage* storage = cdb->GetStorage(Storage.Data());
-    if(!storage)
-      {
-	Error(macroname,"Unable to open storage %s\n",Storage.Data());
-	return;
-      }
-    
+    if(!storage){
+      Error(macroname,"Unable to open storage %s\n",Storage.Data());
+      return;
+    }
+
     AliCDBMetaData* md = new AliCDBMetaData();
     md->SetResponsible("Per Thomas Hille");
     md->SetComment("Peak-Finder vectors for EMCAL");
     md->SetAliRootVersion(gSystem->Getenv("ARVERSION"));
-    AliCDBId id("EMCAL/Calib/PeakFinder",0,AliCDBRunRange::Infinity());
+    AliCDBId id("EMCAL/Config/PeakFinder",0,AliCDBRunRange::Infinity());
     
-    if ( storage != 0 )
+    // if(pfv == 0)
+    //   {
+    // 	cout << "  ERROR !!!!!!!!!" << endl;
+    //   }
+ 
+    // else
+    
       {
-	Bool_t ret = storage->Put( pfv,id,md);
-	if(ret ==  kFALSE )
-	  {
-	    cout << __FILE__ << ":" << __LINE__ << ": ERROR !!!  storage->Put( pfv,id,md) returned false " << endl;
-	  }
-	else
-	  {
-	    cout << __FILE__ << ":" << __LINE__ << ": INFO  !!!  storage->Put( pfv,id,md) returned TRUE " << endl;
-	  }
-
+	storage->Put( &pfv,id,md);
       }
+
+    //  delete md;
   }
 
+  
+  //
 }
 
