e21bc3711d5b98e2d7c5333c5e109adf29362e39
cblume <Christoph.Blume@cern.ch>
2015-05-18 21:39:59 +0200

    Fix compilation error

--- TRD/TRDbase/AliTRDgeometry.cxx
+++ TRD/TRDbase/AliTRDgeometry.cxx
@@ -3046,7 +3046,7 @@ Bool_t AliTRDgeometry::CreateClusterMatrixArray()
         volPath += vpApp3a;
       };
       if (!gGeoManager->CheckPath(volPath)) {
-        AliInfo(Form("Path not found in geometry: %s",volPath));
+        //AliInfo(Form("Path not found in geometry: %s",volPath.Data()));
 	continue;
       }
 
