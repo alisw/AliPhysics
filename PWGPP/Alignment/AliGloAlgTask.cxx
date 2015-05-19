cc15df63e7fc2f893a41d455760efe4a0d98378f
shahoian <ruben.shahoyan@cern.ch>
2015-05-19 12:25:49 +0200

    global alignment task skeleton, not in compilation

---
+++ PWGPP/Alignment/AliGloAlgTask.cxx
@@ -0,0 +1,136 @@
+/*************************************************************************
+* Copyright(c) 1998-2008, ALICE Experiment at CERN, All rights reserved. *
+*                                                                        *
+* Author: The ALICE Off-line Project.                                    *
+* Contributors are mentioned in the code where appropriate.              *
+*                                                                        *
+* Permission to use, copy, modify and distribute this software and its   *
+* documentation strictly for non-commercial purposes is hereby granted   *
+* without fee, provided that the above copyright notice appears in all   *
+* copies and that both the copyright notice and this permission notice   *
+* appear in the supporting documentation. The authors make no claims     *
+* about the suitability of this software for any purpose. It is          *
+* provided "as is" without express or implied warranty.                  * 
+**************************************************************************/
+
+
+#include <stdio.h>
+#include <TFile.h>
+#include <TChain.h>
+#include <TTree.h>
+#include <TSystem.h>
+#include <TROOT.h>
+#include "AliAnalysisManager.h"
+#include "AliESDInputHandler.h"
+#include "AliGloAlgTask.h"
+#include "AliLog.h"
+#include "AliAlgSteer.h"
+
+ClassImp(AliGloAlgTask)
+
+//________________________________________________________________________
+AliGloAlgTask::AliGloAlgTask(const char *name) 
+  : AliAnalysisTaskSE(name)
+  ,fOutput(0)
+  ,fTrigSel(0)
+  ,fAlgSteer(0)
+  ,fConfMacroName()
+{
+  // Constructor
+  DefineOutput(1, TList::Class());
+  SetConfMacroName();
+  //
+}
+
+//________________________________________________________________________
+AliGloAlgTask::~AliGloAlgTask()
+{
+  // Destructor
+  if (fOutput && !AliAnalysisManager::GetAnalysisManager()->IsProofMode()) {  //RRR
+    printf("Deleteing output\n");
+    delete fOutput;
+    fOutput = 0;
+  }
+  //
+  delete fAlgSteer;
+  //
+}
+
+//________________________________________________________________________
+void AliGloAlgTask::UserCreateOutputObjects() 
+{
+  //
+  fOutput = new TList();
+  fOutput->SetOwner(); 
+  //
+  PostData(1, fOutput);
+  //
+}
+
+//________________________________________________________________________
+void AliGloAlgTask::NotifyRun()
+{
+  // attach tree to alignment object
+  AliAnalysisManager* anMan = AliAnalysisManager::GetAnalysisManager();
+  AliESDInputHandler *handler = (AliESDInputHandler*)anMan->GetInputEventHandler();
+  fAlgSteer->SetESDTree(handler->GetTree());
+}
+
+//________________________________________________________________________
+void AliGloAlgTask::UserExec(Option_t *) 
+{
+  // Main loop
+  //
+  AliAnalysisManager* anMan = AliAnalysisManager::GetAnalysisManager();
+  AliESDInputHandler *handler = (AliESDInputHandler*)anMan->GetInputEventHandler();
+  AliESDEvent* esdEv = handler->GetEvent();
+  AliESDfriend *esdFr = handler->GetESDfriend(); // get the input friend
+  //
+  if(!esdEv) {AliInfo("no ESD"); return;} 
+  if(!esdFr || esdFr->GetNumberOfTracks()<esdEv->GetNumberOfTracks()) {
+    AliDebug(3,"no ESDFriend"); 
+    return;
+  }
+  //  AliInfo(Form("Number of ESD tracks in input = %d ",esdEv->GetNumberOfTracks()));
+  //  AliInfo(Form("Number of tracks in input friends = %d ",esdFr->GetNumberOfTracks()));
+  //
+  fAlgSteer->ProcessEvent(esdEv);
+  //
+}      
+
+//________________________________________________________________________
+void AliGloAlgTask::Terminate(Option_t *) 
+{
+  Printf("Terminating...");
+  fAlgSteer->Terminate();
+  //
+  TH1* hstat;
+  hstat = fAlgSteer->GetHistoDOF();
+  if (hstat) fOutput->Add(hstat);
+  fAlgSteer->DetachHistoDOF();
+  //
+  hstat = fAlgSteer->GetHistoStat();
+  if (hstat) fOutput->Add(hstat);
+  fAlgSteer->DetachHistoStat();
+  //
+}
+
+//_________________________________________________________________________
+void AliGloAlgTask::LocalInit()
+{
+  // init alignment object by running user provided macro containing a function with
+  // the same name as the macro and accepting as an argument a pointer to fAlgSteer
+  AliInfo("Processing");
+  if (fAlgSteer) AliFatal("Something is wrong, alignment was already initialized");
+  //
+  if (fConfMacroName.IsNull() || gSystem->AccessPathName(fConfMacroName.Data(), kFileExists)) {
+    AliFatalF("Cannot find configuration macro %s",fConfMacroName.Data());
+  }
+  //
+  fAlgSteer = new AliAlgSteer();
+  gROOT->ProcessLine(Form(".x %s+g((AliAlgSteer*)%p)",fConfMacroName.Data(),fAlgSteer));
+  //
+  fAlgSteer->InitDOFs();
+  //
+  fAlgSteer->Print();
+}
