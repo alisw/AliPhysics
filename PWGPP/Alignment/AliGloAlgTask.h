f9dec2829c4c3846d4b17b38f8b0dac6c366c23a
shahoian <ruben.shahoyan@cern.ch>
2015-05-23 23:41:06 +0200

    setter for initial params to read

--- PWGPP/Alignment/AliGloAlgTask.h
+++ PWGPP/Alignment/AliGloAlgTask.h
@@ -5,11 +5,6 @@
 #include <TString.h>
 class AliAlgSteer;
 
-/*--------------------------------------------------------
-  Task to produce data for millepede alignment
-  -------------------------------------------------------*/
-
-
 class AliGloAlgTask : public AliAnalysisTaskSE {
  public:
   //
@@ -28,12 +23,16 @@ class AliGloAlgTask : public AliAnalysisTaskSE {
   void        SetConfMacroName(const char* nm="alignConf.C")        {fConfMacroName = nm;}
   const char* GetConfMacroName()                              const {return fConfMacroName.Data();}
   //
+  void        SetIniParFileName(const char* nm=0)                   {fIniParFileName = nm;}
+  const char* GetIniParFileName()                              const {return fIniParFileName.Data();}
+  //
  protected:
   //
   TList*       fOutput;                   // output list send on output slot 1
   UInt_t       fTrigSel;                  // trigger selection
   AliAlgSteer* fAlgSteer;                 // alignment steering
   //
+  TString      fIniParFileName;          // initial parameters file
   TString      fConfMacroName;           // name of alignment configuration macro
   //
  private:    
