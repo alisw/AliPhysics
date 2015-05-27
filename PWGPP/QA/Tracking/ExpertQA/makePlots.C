2bb5e85c03f17c178ae534f0dc406ea9d963719b
mivanov <marian.ivanov@cern.ch>
2015-05-27 18:27:18 +0200

    ATO-102, ATO-191 PWGPP/QA/Tracking/ExpertQA/makePlots.C, PWGPP/QA/Tracking/ExpertQA/test/AliHighPtTreeAnalysis_test.sh - adding test functions PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.h, PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx - using TTreeStream fro trneding instead of palin Branch fill * to be consustent with StandartQA * easier to read the code * easier to document and parse the code

--- PWGPP/QA/Tracking/ExpertQA/makePlots.C
+++ PWGPP/QA/Tracking/ExpertQA/makePlots.C
@@ -3,12 +3,16 @@
 
 */
 
-void makePlots(const char *inputFile)
+void makePlots(const char *inputFile, Int_t mode=0)
 {
+  if (mode==1) {
+    return  testMakeDCArPullFitsMI(inputFile);
+  }
+  
   gSystem->AddIncludePath("-I$ALICE_PHYSICS/include -I$ALICE_ROOT/include");
-  //gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx++");
+  //gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx+");
   if (gROOT->LoadMacro("AliHighPtTreeAnalysis.cxx++")<0){
-    gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx++");
+    gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx+");
   }
   AliHighPtTreeAnalysis *a = new AliHighPtTreeAnalysis();
   a->InitAnalysis(inputFile );
@@ -17,18 +21,38 @@ void makePlots(const char *inputFile)
   
 }
 
+
+
+void testMakeDCArPullFitsMI()(const char *inputFile){
+  //
+  //
+  //
+  gSystem->AddIncludePath("-I$ALICE_PHYSICS/include -I$ALICE_ROOT/include");
+  //gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx+");
+  if (gROOT->LoadMacro("AliHighPtTreeAnalysis.cxx++")<0){
+    gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx+");
+  }
+  AliHighPtTreeAnalysis *a = new AliHighPtTreeAnalysis();
+  a->InitAnalysis("");
+  a->MakeDCArPullFitsMI();
+  (*(a->GetStreamer()))<<"trending"<<"\n";
+
+}
+
+
+
+
 void testQPt(const char *inputFile){
   //
   //
   //
   gSystem->AddIncludePath("-I$ALICE_PHYSICS/include -I$ALICE_ROOT/include");
-  //gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx++");
+  //gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx+");
   if (gROOT->LoadMacro("AliHighPtTreeAnalysis.cxx++")<0){
-    gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx++");
+    gROOT->LoadMacro("$ALICE_PHYSICS/../src/PWGPP/QA/Tracking/ExpertQA/AliHighPtTreeAnalysis.cxx+");
   }
   AliHighPtTreeAnalysis *a = new AliHighPtTreeAnalysis();
   a->InitAnalysis("");
-  MakePowerFit(-1);
-  
+  a->MakePowerFit(-1);
 }
 
