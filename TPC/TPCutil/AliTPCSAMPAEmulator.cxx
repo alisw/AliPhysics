a70d58894f395d2261785e5baacb4137608b8b3a
mivanov <marian.ivanov@cern.ch>
2015-05-28 18:02:03 +0200

    ATO-123, ATO-129 - adding MI impalmentation of MAF filter ()

--- TPC/TPCutil/AliTPCSAMPAEmulator.cxx
+++ TPC/TPCutil/AliTPCSAMPAEmulator.cxx
@@ -44,10 +44,12 @@ ClassImp(AliTPCSAMPAEmulator)
 
 AliTPCSAMPAEmulator::AliTPCSAMPAEmulator() : 
   TNamed(),
-  fBC3SlopeDown(0.2),  // BC3 slope down parameter
-  fBC3SlopeUp(0.1),    // BC3 slope up   parameter
-  fBC3Round(-1)      // Rounding error of BC3 filter
-
+  fBC3SlopeDown(0.2),    // BC3 slope down parameter
+  fBC3SlopeUp(0.1),      // BC3 slope up   parameter
+  fBC3Round(-1),         // Rounding error of BC3 filter
+  //
+  fMAFMIKernelWidth(10), // kernel width for MAF filtering 
+  fMAFMIDiffCut(-1)      // cut on the diff to skip "signal"  
 {
   //
   // Constructor of SAMPA Class
@@ -81,14 +83,14 @@ void AliTPCSAMPAEmulator::SetBC3Parameters(Double_t slopeDown, Double_t slopeUp,
 }
 
 
-Bool_t   AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray){
+Bool_t   AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray, Double_t &baseline){
   //
   // BC3 filter
   //
-  return  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(npoints,dataArray,  fBC3SlopeDown,fBC3SlopeUp,  fBC3Round); 
+  return  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(npoints,dataArray,  fBC3SlopeDown,fBC3SlopeUp,  fBC3Round,baseline); 
 }
 
-Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray, Double_t slopeDown, Double_t slopeUp, Double_t round) {
+Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray, Double_t slopeDown, Double_t slopeUp, Double_t round, Double_t &slopeBaseline) {
   //
   //
   //
@@ -97,21 +99,15 @@ Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataAr
   // https://alice.its.cern.ch/jira/browse/ATO-129
   //    discussed here the root/C code snippet for the slope based filter. The filter is applied as
   //
-  //     data_filtered=data-slopefilterfloat(data);
+  //     data_filtered=data-baseline(data);
   //
   //    The filter is floating point. To simulate the integer behavior of the hardware based filter, define INTFILTER and choose the slopes to be binary compatible (1.0, 0.5, 0.25 etc.).
   // #define SLOPEUP		1.0
   // #define SLOPEDOWN	2.0
 
-  Double_t slopeBaseline=0;
-  Bool_t slopestart=true;
   if (npoints<=1) return kFALSE;
   for (Int_t iTimeBin=0; iTimeBin<npoints; iTimeBin++){
     Double_t data=dataArray[iTimeBin];
-    if (slopestart) {	// initialization, not necessarily needed
-      slopeBaseline=data;	// start value
-      slopestart=false;
-    };
     if (data>slopeBaseline) {
       slopeBaseline+=slopeUp;
       if (slopeBaseline>data) slopeBaseline=data;
@@ -127,3 +123,43 @@ Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataAr
   }
   return kTRUE;
 };
+
+
+Bool_t  AliTPCSAMPAEmulator::MovingAverageFilter(Int_t npoints, Double_t *dataArray, Double_t &baseline){
+  //
+  //
+  //
+  return MovingAverageFilter(npoints,dataArray, fMAFMIKernelWidth, fMAFMIDiffCut,fMAFMIOnlyMinima, baseline);
+}
+
+Bool_t  AliTPCSAMPAEmulator::MovingAverageFilter(Int_t npoints, Double_t *dataArray, Double_t length, Double_t skipDiff,  Bool_t onlyMinima, Double_t &baseline){
+  //
+  // Baseline update formula:
+  //     baseline:=(baseline*length+data)/(length+1);
+  //
+  if (npoints<=1) return kFALSE;
+  for (Int_t iTimeBin=0; iTimeBin<npoints; iTimeBin++){
+    Double_t data=dataArray[iTimeBin];
+    if (skipDiff>0){ // skip data point in case it is assumed to be signal ()too big diff)
+      if ( (TMath::Abs(dataArray[(iTimeBin+npoints-1)%npoints]+baseline-data)>skipDiff) ||
+	   (TMath::Abs(dataArray[(iTimeBin+npoints+1)%npoints]-data)>skipDiff)) {
+	continue;
+      }
+      if ( !(dataArray[(iTimeBin+npoints-1)%npoints]+baseline>data &&TMath::Abs(dataArray[(iTimeBin+npoints+1)%npoints]>data))){
+	continue;  //use only local minima
+      }
+    }
+    baseline=(baseline*length+data)/(length+1);
+    dataArray[iTimeBin]-=baseline;
+  }
+}
+
+
+void AliTPCSAMPAEmulator::SetMAFMIParameters(Double_t  MAFMIKernelWidth,  Double_t  MAFMIDiffCut, Bool_t onlyMinima){
+  //
+  //
+  //
+  fMAFMIKernelWidth=MAFMIKernelWidth;   // kernel width for MAF filtering 
+  fMAFMIDiffCut=MAFMIDiffCut;           // cut on the diff to skip "signal"   
+  fMAFMIOnlyMinima=onlyMinima;          //   
+}
--- TPC/TPCutil/AliTPCSAMPAEmulator.h
+++ TPC/TPCutil/AliTPCSAMPAEmulator.h
@@ -32,12 +32,13 @@ public:
   ~AliTPCSAMPAEmulator();
   //
   //
-  Bool_t  BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray);
-  static Bool_t  BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray, Double_t slopeDown, Double_t slopeUp, Double_t round);
+  Bool_t  BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray,  Double_t &baseline);
+  static Bool_t  BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray, Double_t slopeDown, Double_t slopeUp, Double_t round, Double_t &baseline);
   void SetBC3Parameters(Double_t slopeDown, Double_t slopeUp, Double_t round);
   //
-  // Bool_t  MovingAverageFilter(Int_t npoints, Double_t *dataArray);
-  //Bool_t  MovingAverageFilter(Int_t npoints, Double_t *dataArray, Double_t length, Double_t skipDiff);
+  Bool_t  MovingAverageFilter(Int_t npoints, Double_t *dataArray, Double_t &baseline);
+  static Bool_t  MovingAverageFilter(Int_t npoints, Double_t *dataArray, Double_t length, Double_t skipDiff,  Bool_t onlyMinima, Double_t &baseline); // local maxim need treatment
+  void SetMAFMIParameters(Double_t  MAFMIKernelWidth,  Double_t  MAFMIDiffCut, Bool_t onlyMinima);
 private:
   AliTPCSAMPAEmulator(const AliTPCSAMPAEmulator &sig);
   AliTPCSAMPAEmulator& operator = (const  AliTPCSAMPAEmulator &source);
@@ -48,6 +49,13 @@ public:
   Double_t  fBC3SlopeDown;  // BC3 slope down parameter
   Double_t  fBC3SlopeUp;    // BC3 slope up   parameter
   Double_t  fBC3Round;      // Rounding error of BC3 filter
+  //
+  // Moving average filter parameters (MI) implementation
+  //
+  Double_t  fMAFMIKernelWidth;   // kernel width for MAF filtering 
+  Double_t  fMAFMIDiffCut;       // cut on the diff to skip "signal"
+  Double_t  fMAFMIOnlyMinima;         // use only local minima
+
   ClassDef(AliTPCSAMPAEmulator,1);
 };
 #endif
