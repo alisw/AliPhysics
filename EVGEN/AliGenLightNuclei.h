f6c1a37b3dee9874a521856f5562f26b59c476d1
eserradi <eulogio.serradilla@cern.ch>
2015-06-05 19:08:29 +0200

    fix nucleon momenta wrt CM frame of the cluster for A>2

--- EVGEN/AliGenLightNuclei.h
+++ EVGEN/AliGenLightNuclei.h
@@ -11,6 +11,7 @@
 
 class TParticle;
 class AliStack;
+class TLorentzVector;
 
 class AliGenLightNuclei: public AliGenCocktail
 {
@@ -26,10 +27,7 @@ class AliGenLightNuclei: public AliGenCocktail
 	
 	void SetCoalescenceMomentum(Double_t p0) { fP0 = p0; }
 	
-	void SetDeuterons(Bool_t flag = kTRUE) { fGenDeuterons = flag; }
-	void SetTritons(Bool_t flag = kTRUE)   { fGenTritons = flag; }
-	void SetHe3Nuclei(Bool_t flag = kTRUE) { fGenHe3Nuclei = flag; }
-	void SetHe4Nuclei(Bool_t flag = kTRUE) { fGenHe4Nuclei = flag; }
+	void SetNucleusPdgCode(Int_t pdg) { fPdg = pdg; }
 	
 	enum {kDeuteron=1000010020, kAntiDeuteron=-1000010020, kTriton=1000010030, kAntiTriton=-1000010030, kHe3Nucleus=1000020030, kAntiHe3Nucleus=-1000020030, kAlpha=1000020040, kAntiAlpha=-1000020040 };
 
@@ -40,33 +38,22 @@ class AliGenLightNuclei: public AliGenCocktail
 	AliGenLightNuclei(const AliGenLightNuclei &other);
 	AliGenLightNuclei& operator=(const AliGenLightNuclei &other);
 	
-	Int_t GenerateDeuterons(const TList* protons, const TList* neutrons);
-	Int_t GenerateTritons(const TList* protons, const TList* neutrons);
-	Int_t GenerateHe3Nuclei(const TList* protons, const TList* neutrons);
-	Int_t GenerateHe4Nuclei(const TList* protons, const TList* neutrons);
+	Bool_t Coalescence(const TLorentzVector& p1, const TLorentzVector& p2) const;
+	Bool_t Coalescence(const TLorentzVector& p1, const TLorentzVector& p2, const TLorentzVector& p3) const;
+	Bool_t Coalescence(const TLorentzVector& p1, const TLorentzVector& p2, const TLorentzVector& p3, const TLorentzVector& p4) const;
 	
-	void PushDeuteron(TParticle* parent1, TParticle* parent2);
-	void PushTriton(TParticle* parent1, TParticle* parent2, TParticle* parent3);
-	void PushHe3Nucleus(TParticle* parent1, TParticle* parent2, TParticle* parent3);
-	void PushHe4Nucleus(TParticle* parent1, TParticle* parent2, TParticle* parent3, TParticle* parent4);
+	Int_t GenerateNuclei(Int_t pdg, Double_t mass, const TList* l1, const TList* l2);
+	Int_t GenerateNuclei(Int_t pdg, Double_t mass, const TList* l1, const TList* l2, const TList* l3);
+	Int_t GenerateNuclei(Int_t pdg, Double_t mass, const TList* l1, const TList* l2, const TList* l3, const TList* l4);
 	
 	void PushNucleus(Int_t pdg, Double_t mass, TParticle* parent1, TParticle* parent2, TParticle* parent3=0, TParticle* parent4=0);
 	
-	Bool_t Coalescence(const TParticle* n1, const TParticle* n2) const;
-	TParticle* FindPartner(const TParticle* n0, const TList* nucleons, const TParticle* nx=0) const;
-	
-	Double_t GetS(Double_t p1x, Double_t p1y, Double_t p1z, Double_t m1, Double_t p2x, Double_t p2y, Double_t p2z, Double_t m2) const;
-	Double_t GetPcm(Double_t p1x, Double_t p1y, Double_t p1z, Double_t m1, Double_t p2x, Double_t p2y, Double_t p2z, Double_t m2) const;
-	
  private:
 	
-	Double_t fP0;         // coalescence momentum (radius of the sphere)
-	Bool_t fGenDeuterons; // generate deuterons and anti-deuterons
-	Bool_t fGenTritons;   // generate tritons and anti-tritons
-	Bool_t fGenHe3Nuclei; // generate He3 and anti-He3 nuclei
-	Bool_t fGenHe4Nuclei; // generate He4 and anti-He4 nuclei
+	Double_t fP0; // coalescence momentum (radius of the sphere)
+	Int_t fPdg;   // nucleus PDG code
 	
-	ClassDef(AliGenLightNuclei,1)
+	ClassDef(AliGenLightNuclei,2)
 };
 
 #endif // ALIGENLIGHTNUCLEI_H
