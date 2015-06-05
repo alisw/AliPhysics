c302b7211b9e66994e1cf3d4c594ef740027a0b7
fbellini <fbellini@cern.ch>
2015-06-05 11:40:26 +0200

    First version of the QA task for the Beam-Gas Monitoring, including steering and post-processing macros

---
+++ PWGPP/BeamGasMonitoring/AliAnalysisMBVeto.h
@@ -0,0 +1,98 @@
+#ifndef AliAnalysisMBVeto_h
+#define AliAnalysisMBVeto_h
+
+#include "AliAnalysisTaskSE.h"
+
+
+class AliESDEvent;
+class AliESDfriend;
+class AliAnalysisCuts;
+class TH1D;
+class TH1F;
+class TH2F;
+class TH2D;
+
+class AliAnalysisMBVeto : public AliAnalysisTaskSE {
+ public:
+  AliAnalysisMBVeto(const char *name = "AliAnalysisMBVeto");
+  virtual ~AliAnalysisMBVeto() {}
+  
+  virtual void   ConnectInputData(Option_t *);
+  virtual void   CreateOutputObjects();
+  virtual void   Exec(Option_t *option);
+  virtual void   Terminate(Option_t *);
+
+ // virtual void   Terminate(Option_t *);
+    
+ private: 
+  AliESDEvent *fESD;        //! ESD event
+  AliESDfriend* fESDfriend; //! ESDfriend   
+  TTree *fTreeTrack;        //! tree
+  TList *fList;             //! list
+  
+  Int_t fUseTree;
+
+
+  Int_t runNumber;
+  Int_t ftrigger[kMaxUShort]; 
+  Double_t fvertZ;
+  Double_t fvertX;
+  Double_t fvertY;
+  Double_t fvertTPCZ;
+  Double_t fvertTPCX;
+  Double_t fvertTPCY;
+  Double_t fvertZ2;
+  Double_t fvertX2;
+  Double_t fvertY2;
+  Double_t fvertTPCZ2;
+  Double_t fvertTPCX2;
+  Double_t fvertTPCY2;
+  Float_t fv0a;
+  Float_t fv0c;
+  Float_t fMulta;
+  Float_t fMultc;
+  Float_t fTriCha;
+  Float_t fTriChc;
+  Int_t fbx;
+  Int_t ftime;
+  Int_t fSpdC1;
+  Int_t fSpdC2;
+  Int_t fSpdT;
+  Int_t ntracks;
+  Int_t V0A;
+  Int_t V0C;
+  Int_t V0ABG;
+  Int_t V0CBG;
+  Int_t nV0A;
+  Int_t nV0C;
+  Int_t nV0ABG;
+  Int_t nV0CBG;
+  Int_t VBA;
+  Int_t VBC;
+  Int_t VGA;
+  Int_t VGC;
+  Int_t VTX;
+  Int_t bgID;
+  Int_t t0PileUp;
+  Int_t spdPileUp;
+  Int_t spdPileUpOutOfBunch;
+  Int_t triMask;  
+  Int_t fastORHW;  
+  Int_t SPD1;
+  Int_t SPD2;
+  Int_t SPDHw1;
+  Int_t SPDHw2;  
+  Int_t BGFlagA[kMaxUShort];
+  Int_t BGFlagC[kMaxUShort];
+  Int_t BBFlagA[kMaxUShort];
+  Int_t BBFlagC[kMaxUShort];
+  UShort_t ntr;
+  UShort_t nbunch;
+
+  AliAnalysisMBVeto(const AliAnalysisMBVeto&); // not implemented
+  AliAnalysisMBVeto& operator=(const AliAnalysisMBVeto&); // not implemented
+  
+  ClassDef(AliAnalysisMBVeto, 2);// example of analysis
+};
+
+#endif
