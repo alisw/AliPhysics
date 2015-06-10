c3e53858a95d7d656971d8ee28bf80e78d037e28
belikov <Iouri.Belikov@cern.ch>
2015-06-10 16:14:10 +0200

    Removing the old ITS/UPGRADE code

--- ITS/UPGRADE/ITSUpgradeBase/AliITSUAux.cxx
+++
@@ -1,9 +0,0 @@
-#include "AliITSUAux.h"
-
-//_______________________________________________________________
-void AliITSUAux::PrintBits(ULong64_t patt, Int_t maxBits)
-{
-  // print maxBits of the pattern
-  maxBits = Min(64,maxBits);
-  for (int i=0;i<maxBits;i++) printf("%c",((patt>>i)&0x1) ? '+':'-');
-}
