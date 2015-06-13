62d249dd9cee74f80912460a7294ae7f6be1432f
saiola <salvatore.aiola@cern.ch>
2015-06-13 10:01:47 -0400

    Remove accidental edit in cmake files

--- PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
+++ PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
@@ -78,7 +78,6 @@
 #pragma link C++ class AliAnalysisTaskJetV2+;
 #pragma link C++ class AliAnalysisTaskEmcalJetv2QA+;
 #pragma link C++ class AliAnalysisTaskRhoMassScale+;
-#pragma link C++ class AliEmcalMultTaskEli+;
 #pragma link C++ class AliAnalysisTaskGammaHadron+;
 #pragma link C++ class AliAnalysisTaskSAJF+;
 #pragma link C++ class AliAnalysisTaskSAQA+;
