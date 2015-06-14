2c5d0b8d4ae006f90e11170c43e0a7c97b5f8137
mkrzewic <mikolaj.krzewicki@cern.ch>
2015-06-14 20:49:39 +0200

    formatting: fix indents

--- HLT/rec/makeFullHLTStreamerInfo.C
+++ HLT/rec/makeFullHLTStreamerInfo.C
@@ -3,12 +3,12 @@
 //blame: Mikolaj Krzewicki, mkrzewic@cern.ch
 
 void makeFullHLTStreamerInfo(const char* className = "AliESDEvent",
-                             const char* ocdbSource = "local:///cvmfs/alice.cern.ch/calibration/data/2015/OCDB",
-                             Int_t version = 0,
-                             Int_t firstRun = 0,
-                             Int_t lastRun = AliCDBRunRange::Infinity(),
-                             const char* ocdbTarget = "local://OCDB"
-                             )
+    const char* ocdbSource = "local:///cvmfs/alice.cern.ch/calibration/data/2015/OCDB",
+    Int_t version = 0,
+    Int_t firstRun = 0,
+    Int_t lastRun = AliCDBRunRange::Infinity(),
+    const char* ocdbTarget = "local://OCDB"
+    )
 {
   // Setup the CDB default storage and run number.  
   const char* gkCalibStreamerInfoEntry="HLT/Calib/StreamerInfo";
@@ -31,12 +31,12 @@ void makeFullHLTStreamerInfo(const char* className = "AliESDEvent",
   }
   AliCDBRunRange runRange(firstRun, lastRun);
   AliCDBEntry* pExistingEntry=ocdbSourceStorage->Get(gkCalibStreamerInfoEntry, runRange);  
-  
+
   //get the list of streamers from OCDB
   TObjArray* listOld = NULL;
   if (pExistingEntry) listOld=(TObjArray*)pExistingEntry->GetObject();
   TObjArray* listNew      = new TObjArray;  
-                             
+
   //create a complete list of streamers for selected class
   TClass* pClass = TClass::GetClass(className);
   if (!pClass) {return;}
@@ -46,7 +46,7 @@ void makeFullHLTStreamerInfo(const char* className = "AliESDEvent",
 
   //update the old list with new definitions
   updateStreamerList(listOld, listNew);
-  
+
   //listOld->Print();
 
   // Write the updated object to target OCDB
@@ -70,7 +70,7 @@ void createStreamerInfos(TClass* pClass, TObjArray* listOfStreamerInfos)
     << "version " << pClass->GetClassVersion()
     << endl;
   listOfStreamerInfos->AddAtFree(streamerInfo);
-  
+
   TList* listOfDataMembers = pClass->GetListOfDataMembers();
   if (!listOfDataMembers) {return;}
   TIter nextDataMember(listOfDataMembers);
@@ -79,8 +79,8 @@ void createStreamerInfos(TClass* pClass, TObjArray* listOfStreamerInfos)
   {
     TString memberClassName = member->GetTypeName();
     cout << "  scanning member " << member->GetName()
-         << " of type " << memberClassName 
-         << endl;
+      << " of type " << memberClassName 
+      << endl;
     TClass* memberClass = TClass::GetClass(member->GetTypeName());
     if (!memberClass) {continue;}
     createStreamerInfos(memberClass, listOfStreamerInfos);
@@ -92,14 +92,14 @@ Bool_t HaveStreamersForClass(TClass* pClass, TObjArray* pInfos)
   //check is the class description is already in the list
   TString className = pClass->GetName();
   Version_t classVersion = pClass->GetClassVersion();
-  
+
   TStreamerInfo* pInfo=NULL;
   for (int i=0; i<pInfos->GetEntriesFast(); i++) {
     if (pInfos->At(i)==NULL) continue;
     if (pInfos->At(i)->IsA()!=TStreamerInfo::Class()) {
       cout << "  skipping object " << pInfos->At(i)->GetName() 
-           << " class "          << pInfos->At(i)->Class()->GetName() 
-           << endl;
+        << " class "          << pInfos->At(i)->Class()->GetName() 
+        << endl;
       continue;
     }
     pInfo=dynamic_cast<TStreamerInfo*>(pInfos->At(i));
@@ -110,9 +110,9 @@ Bool_t HaveStreamersForClass(TClass* pClass, TObjArray* pInfos)
 
     if (pInfo->GetClassVersion()==classVersion) {
       cout << "  nothing to be done, class " 
-           << className    << " version " 
-           << classVersion << " already in the list" 
-           << endl;
+        << className    << " version " 
+        << classVersion << " already in the list" 
+        << endl;
       return kTRUE;
     }
   }
@@ -122,26 +122,26 @@ Bool_t HaveStreamersForClass(TClass* pClass, TObjArray* pInfos)
 void updateStreamerList(TObjArray* oldList, TObjArray* newList)
 {
   //update the old list with new/unique entries from new list
-    for (int j=0; j<oldList->GetEntriesFast(); j++)
-    {
-      TStreamerInfo* oldStreamerInfo=dynamic_cast<TStreamerInfo*>(oldList->At(j));
-      if (!oldStreamerInfo) continue;
-      TString oldName = oldStreamerInfo->GetName();
-      int oldVersion = oldStreamerInfo->GetClassVersion();
+  for (int j=0; j<oldList->GetEntriesFast(); j++)
+  {
+    TStreamerInfo* oldStreamerInfo=dynamic_cast<TStreamerInfo*>(oldList->At(j));
+    if (!oldStreamerInfo) continue;
+    TString oldName = oldStreamerInfo->GetName();
+    int oldVersion = oldStreamerInfo->GetClassVersion();
 
-      for (int i=0; i<newList->GetEntriesFast(); i++) {
-        TStreamerInfo* newStreamerInfo=dynamic_cast<TStreamerInfo*>(newList->At(i));
-        if (!newStreamerInfo) continue;
-        TString newName = newStreamerInfo->GetName();
-        int newVersion = newStreamerInfo->GetClassVersion();
-        //update; or not
-        if (! oldName.EqualTo(newName)) continue;
-        if (oldVersion == newVersion) continue;
+    for (int i=0; i<newList->GetEntriesFast(); i++) {
+      TStreamerInfo* newStreamerInfo=dynamic_cast<TStreamerInfo*>(newList->At(i));
+      if (!newStreamerInfo) continue;
+      TString newName = newStreamerInfo->GetName();
+      int newVersion = newStreamerInfo->GetClassVersion();
+      //update; or not
+      if (! oldName.EqualTo(newName)) continue;
+      if (oldVersion == newVersion) continue;
 
-        cout << "adding " << newName << " v" << newVersion
-             << " (old: " << oldName << " v" << oldVersion << ")"<<endl;
-        oldList->AddAtFree(newStreamerInfo);
-      }
+      cout << "adding " << newName << " v" << newVersion
+        << " (old: " << oldName << " v" << oldVersion << ")"<<endl;
+      oldList->AddAtFree(newStreamerInfo);
     }
+  }
   return;
 }
