0f05e8f0715c1a34579ddb612b318460b08dd699
prsnko <Dmitri.Peressounko@cern.ch>
2015-06-23 10:04:35 -0400

    Trigger QA histograms added

--- PWGGA/PHOSTasks/ClusterSelection/AliPHOSTriggerUtils.h
+++ PWGGA/PHOSTasks/ClusterSelection/AliPHOSTriggerUtils.h
@@ -42,10 +42,11 @@ public:
   
   void SetTileOffset(Int_t left=-1, Int_t right=3){fbdrL=left;fbdrR=right ;}
   
+  Bool_t TestBadMap(Int_t mod, Int_t ix,Int_t iz) ; //Check if trigger is in good/active region
+
 protected:
   
   void InitForRun(Int_t run) ; //read trigger bad map for this run from OADB. Should be called once per run
-  Bool_t TestBadMap(Int_t mod, Int_t ix,Int_t iz) ; //Check if trigger is in good/active region
   Int_t FindBranch(Int_t nX, Int_t nZ) ; //Calculate number of PHOS branch
   Double_t TriggerProbabilityLHC13bcdef(Double_t eClu, Int_t module) ; //Parameterization of turn-on curve in LHC13bcdef
   
