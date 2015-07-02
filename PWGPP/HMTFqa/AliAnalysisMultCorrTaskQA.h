a5093330fcf2dd9e086b9bb40a2f0adaf4b1a50d
sjena <sjena@cern.ch>
2015-07-02 17:01:47 +0200

    Bug fix in merging HMTFqa task:Hector

--- PWGPP/HMTFqa/AliAnalysisMultCorrTaskQA.h
+++ PWGPP/HMTFqa/AliAnalysisMultCorrTaskQA.h
@@ -1,3 +1,27 @@
+/**************************************************************************
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+// This is a simple task designed to test AliPPVsMultUtils functionality.
+// --- david.dobrigkeit.chinellato@cern.ch
+// Also has been added analysis for dN/dEta and Multiplicity + Multiplicity correlations with V0 estimators+ V0 sectors
+// ---- hector.bello.martinez@cern.ch
+//
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
 #ifndef AliAnalysisMultCorrTaskQA_H
 #define AliAnalysisMultCorrTaskQA_H
 
@@ -17,8 +41,7 @@ class AliPhysicsSelection;
 class AliESDtrack;
 class AliPPVsMultUtils;
 class AliAnalysisFilter;
-//#include "TString.h"
-//#include "AliESDtrackCuts.h"
+
 #include "AliAnalysisTaskSE.h"
 
 class AliAnalysisMultCorrTaskQA : public AliAnalysisTaskSE {
@@ -30,7 +53,6 @@ public:
     virtual void   UserCreateOutputObjects();
     virtual void   UserExec(Option_t *option);
     virtual void   Terminate(Option_t *);
-    //virtual void   SetCuts(AliESDtrackCuts *); //cuts=0){fCuts = cuts;}
     virtual void   SetCuts(AliESDtrackCuts* cuts=0){fCuts = cuts;}
     virtual void   SetTrackFilterSKE(AliAnalysisFilter* trackF) {fTrackFilterSKE = trackF;}
     
@@ -41,7 +63,7 @@ private:
     // http://root.cern.ch/download/doc/11InputOutput.pdf, page 14
     TList  *fListHist;  //! List of histograms
     AliESDtrackCuts* fCuts; //!
-    AliAnalysisFilter* fTrackFilterSKE; //!    //Added by me
+    AliAnalysisFilter* fTrackFilterSKE; //!    
 
 //===========================================================================================
 //   Histograms
@@ -53,27 +75,17 @@ private:
     TH1D *fHistV0M; //!
     TH1D *fHistV0A; //!
     TH1D *fHistV0C; //!
-    TH1D *fHistV0Aamp; //!
-    TH1D *fHistV0Camp; //!
-    TH1D *fHistV0Mamp; //!
     TH1D *fdNdeta; //!
-    TH1D *fPMult08; //!
-    TH1D *fdNdeta05; //!
-    TH1D *fPMult05; //!
     TH2D *fcorrRef05Ref08; //!
     TH2D *fcorrV0ARef08; //!
     TH2D *fcorrV0CRef08; //!
     TH2D *fcorrV0MRef08; //!
+    TH1D *fHistV0Aamp; //!
+    TH1D *fHistV0Camp; //!
+    TH1D *fHistV0Mamp; //!
     TH2D *fcorrV0AampRef08; //!
     TH2D *fcorrV0CampRef08; //!
     TH2D *fcorrV0MampRef08; //!
-    TProfile *fcorrRef05Ref08pfx; //!
-    TProfile *fcorrV0ARef08pfx; //!
-    TProfile *fcorrV0CRef08pfx; //!
-    TProfile *fcorrV0MRef08pfx; //!
-    TProfile *fcorrV0AampRef08pfx; //!
-    TProfile *fcorrV0CampRef08pfx; //!
-    TProfile *fcorrV0MampRef08pfx; //!
     TH2D *fModulesV0; //!
     
     AliPPVsMultUtils *fPPVsMultUtils;
@@ -81,7 +93,10 @@ private:
     AliAnalysisMultCorrTaskQA(const AliAnalysisMultCorrTaskQA&);            // not implemented
     AliAnalysisMultCorrTaskQA& operator=(const AliAnalysisMultCorrTaskQA&); // not implemented
 
-    ClassDef(AliAnalysisMultCorrTaskQA, 11); //11
+    ClassDef(AliAnalysisMultCorrTaskQA, 10); 
 };
 
 #endif
+
+ // - david.dobrigkeit.chinellato@cern.ch
+ // --hector.bello.martinez@cern.ch
