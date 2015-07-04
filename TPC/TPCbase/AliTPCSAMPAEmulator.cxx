dc9bebf76b3d3bdfc5725d6fd4a8758a0dff1f66
mivanov <marian.ivanov@cern.ch>
2015-07-04 08:49:46 +0200

    ATO-123, ATO-129 - adding filter for zerro suppression + BC3 filter with MI modifications for the max delta cut

--- TPC/TPCbase/AliTPCSAMPAEmulator.cxx
+++ TPC/TPCbase/AliTPCSAMPAEmulator.cxx
@@ -48,7 +48,8 @@ AliTPCSAMPAEmulator::AliTPCSAMPAEmulator() :
   //
   fBC3SlopeDown(0.2),    // BC3 slope down parameter
   fBC3SlopeUp(0.1),      // BC3 slope up   parameter
-  fBC3Round(-1),         // Rounding error of BC3 filter
+  fBC3Round(-1),         // Rounding error of BC3 filter 
+  fBC3DiffCutMI(2.5),      // BC3 cut on the signal difference - for MI implementation
   //
   fMAFMIKernelWidth(10), // kernel width for MAF filtering 
   fMAFMIDiffCut(-1)      // cut on the diff to skip "signal"  
@@ -75,8 +76,15 @@ AliTPCSAMPAEmulator::~AliTPCSAMPAEmulator() {
 
 Bool_t   AliTPCSAMPAEmulator::DigitalFilterFloat(Int_t npoints, Double_t *dataArray,  Double_t &baseline){
   //
+  // Run digittal filter - as configured using fDigitFilterType
+  // In future more sophisticated configuration of filters, pipe  of filters should be implemented
+  //    to be agreed  on interface ( e.g BC3 -> Rounding -> ZerroSpuression )
+  // Current implementation used to make fast studies of the indeusced noise
+  //  
+  // 
   if (fDigitFilterType==0) return BC3SlopeFilterFloat(npoints,dataArray,baseline);
   if (fDigitFilterType==1) return MovingAverageFilter(npoints,dataArray,baseline);
+  if (fDigitFilterType==2) return MovingAverageFilter(npoints,dataArray,baseline);
   //
 }
 
@@ -95,6 +103,13 @@ Bool_t   AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataA
   // BC3 filter
   //
   return  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(npoints,dataArray,  fBC3SlopeDown,fBC3SlopeUp,  fBC3Round,baseline); 
+}
+
+Bool_t   AliTPCSAMPAEmulator::BC3SlopeFilterMI(Int_t npoints, Double_t *dataArray, Double_t &baseline){
+  //
+  // BC3 filter
+  //
+  return  AliTPCSAMPAEmulator::BC3SlopeFilterMI(npoints,dataArray,  fBC3SlopeDown,fBC3SlopeUp,  fBC3Round,baseline,fBC3DiffCutMI); 
 }
 
 Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataArray, Double_t slopeDown, Double_t slopeUp, Double_t round, Double_t &slopeBaseline) {
@@ -133,6 +148,45 @@ Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterFloat(Int_t npoints, Double_t *dataAr
 };
 
 
+Bool_t  AliTPCSAMPAEmulator::BC3SlopeFilterMI(Int_t npoints, Double_t *dataArray, Double_t slopeDown, Double_t slopeUp, Double_t round, Double_t &slopeBaseline, Double_t diffCutMI) {
+  //
+  //
+  //
+  // BC2 filter as should be implemented in SAMPA
+  // from Konstantin description
+  // https://alice.its.cern.ch/jira/browse/ATO-129
+  //    discussed here the root/C code snippet for the slope based filter. The filter is applied as
+  //
+  //     data_filtered=data-baseline(data);
+  //
+  //    The filter is floating point. To simulate the integer behavior of the hardware based filter, define INTFILTER and choose the slopes to be binary compatible (1.0, 0.5, 0.25 etc.).
+  // #define SLOPEUP		1.0
+  // #define SLOPEDOWN	2.0
+
+  if (npoints<=1) return kFALSE;
+  for (Int_t iTimeBin=0; iTimeBin<npoints; iTimeBin++){
+    Double_t data=dataArray[iTimeBin];
+    Double_t maxDiff=TMath::Max( TMath::Abs(dataArray[(iTimeBin+npoints+1)%npoints]-data),  TMath::Abs(dataArray[(iTimeBin+npoints-1)%npoints]-data));
+    if (maxDiff<=diffCutMI){
+      if (data>slopeBaseline) {
+	slopeBaseline+=slopeUp;
+	if (slopeBaseline>data) slopeBaseline=data;
+      } else if (data<slopeBaseline) {
+	slopeBaseline-=slopeDown;
+	if (slopeBaseline<data) slopeBaseline=data;
+      };    
+      if (round>0){
+	//    return round(slopeBaseline);
+	slopeBaseline=TMath::Nint(slopeBaseline*round)/round;
+      }
+    }
+    dataArray[iTimeBin]-=slopeBaseline;
+    dataArray[iTimeBin]=TMath::Nint(dataArray[iTimeBin]);
+  }
+  return kTRUE;
+};
+
+
 Bool_t  AliTPCSAMPAEmulator::MovingAverageFilter(Int_t npoints, Double_t *dataArray, Double_t &baseline){
   //
   //
@@ -159,6 +213,7 @@ Bool_t  AliTPCSAMPAEmulator::MovingAverageFilter(Int_t npoints, Double_t *dataAr
     }
     baseline=(baseline*length+data)/(length+1);
     dataArray[iTimeBin]-=baseline;
+    dataArray[iTimeBin]=TMath::Nint(dataArray[iTimeBin]);
   }
 }
 
@@ -171,3 +226,18 @@ void AliTPCSAMPAEmulator::SetMAFMIParameters(Double_t  MAFMIKernelWidth,  Double
   fMAFMIDiffCut=MAFMIDiffCut;           // cut on the diff to skip "signal"   
   fMAFMIOnlyMinima=onlyMinima;          //   
 }
+
+
+Bool_t  AliTPCSAMPAEmulator::ZeroSuppression(Int_t npoints, Double_t *dataArray, Double_t threshold) {
+  //
+  // Zero suppression - sets all values below threshold to zero.
+  // Useful for evaluating the effect of zero suppression on the results.
+  // pre- and post-samples may be implemented as well.
+  //
+  if (npoints<=1) return kFALSE;
+  for (Int_t iTimeBin=0; iTimeBin<npoints; iTimeBin++){
+    Double_t data=dataArray[iTimeBin];
+    if (data<threshold) dataArray[iTimeBin]=0;
+  }
+  return kTRUE;
+}; 
