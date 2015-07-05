22bc7ad932675ba3e450d988c861b6de647416b2
mkrzewic <mikolaj.krzewicki@cern.ch>
2015-07-05 02:04:55 +0200

    zmqRequester.py: update

--- HLT/global/scripts/zmqRequester.py
+++ HLT/global/scripts/zmqRequester.py
@@ -15,10 +15,18 @@ socket = context.socket(zmq.REQ)
 socket.connect(endpoint)
 
 #send request
+socket.send(request,zmq.SNDMORE)
 socket.send(request)
 print("sent request: "+request)
 
 #receive reply (multipart)
 msg = socket.recv_multipart();
-print "topic: "+str(msg[0])
-print "messagesize: "+str(sys.getsizeof(msg[1]))
+print "_________________________"
+i=0;
+for message in msg:
+  if i==0:
+    print "topic: "+str(message)
+    i=1
+  elif i==1:
+    print "  messagesize: "+str(sys.getsizeof(message))
+    i=0
