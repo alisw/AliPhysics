8c15d33daf62e509945ffbae3c0c4963d9bde3a1
akubera <Andrew.Kubera@cern.ch>
2015-07-09 18:56:23 -0400

    Fixed formatting of AliFemtoV0PairCut with astyle - NO FUNCTIONAL CHANGES

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoV0PairCut.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoV0PairCut.h
@@ -30,17 +30,17 @@
 
 #include "AliFemtoPairCut.h"
 
-class AliFemtoV0PairCut : public AliFemtoPairCut{
+class AliFemtoV0PairCut : public AliFemtoPairCut {
 public:
   AliFemtoV0PairCut();
-  AliFemtoV0PairCut(const AliFemtoV0PairCut& cut);
+  AliFemtoV0PairCut(const AliFemtoV0PairCut &cut);
   virtual ~AliFemtoV0PairCut();
-  AliFemtoV0PairCut& operator=(const AliFemtoV0PairCut& cut);
-  
-  virtual bool Pass(const AliFemtoPair* pair);
+  AliFemtoV0PairCut &operator=(const AliFemtoV0PairCut &cut);
+
+  virtual bool Pass(const AliFemtoPair *pair);
   virtual AliFemtoString Report();
   virtual TList *ListSettings();
-  virtual AliFemtoPairCut* Clone();
+  virtual AliFemtoPairCut *Clone();
   void SetV0Max(Double_t aAliFemtoV0Max);
   Double_t GetAliFemtoV0Max() const;
   void     SetRemoveSameLabel(Bool_t aRemove);
@@ -49,12 +49,12 @@ public:
   void SetDataType(AliFemtoDataType type);
   void SetMinAvgSeparation(int type, double minSep);
 
- protected:
-  long fNPairsPassed;          // Number of pairs consideered that passed the cut 
+protected:
+  long fNPairsPassed;          // Number of pairs consideered that passed the cut
   long fNPairsFailed;          // Number of pairs consideered that failed the cut
   Double_t fV0Max;   // Maximum allowed pair quality
   Double_t fShareFractionMax;  // Maximum allowed share fraction
-  Bool_t   fRemoveSameLabel;   // If 1 pairs with two tracks with the same label will be removed 
+  Bool_t   fRemoveSameLabel;   // If 1 pairs with two tracks with the same label will be removed
 
   AliFemtoDataType fDataType; //Use ESD / AOD / Kinematics.
   Double_t fDTPCMin;          // Minimum allowed pair nominal separation at the entrance to the TPC
@@ -69,7 +69,7 @@ public:
 #endif
 };
 
-inline AliFemtoV0PairCut::AliFemtoV0PairCut(const AliFemtoV0PairCut& c) : 
+inline AliFemtoV0PairCut::AliFemtoV0PairCut(const AliFemtoV0PairCut &c) :
   AliFemtoPairCut(c),
   fNPairsPassed(0),
   fNPairsFailed(0),
@@ -82,10 +82,16 @@ inline AliFemtoV0PairCut::AliFemtoV0PairCut(const AliFemtoV0PairCut& c) :
   fMinAvgSepPosPos(0),
   fMinAvgSepPosNeg(0),
   fMinAvgSepNegPos(0),
-  fMinAvgSepNegNeg(0)		      
+  fMinAvgSepNegNeg(0)
 
-{ /* no-op */ }
+{
+  /* no-op */
+}
 
-inline AliFemtoPairCut* AliFemtoV0PairCut::Clone() { AliFemtoV0PairCut* c = new AliFemtoV0PairCut(*this); return c;}
+inline AliFemtoPairCut *AliFemtoV0PairCut::Clone()
+{
+  AliFemtoV0PairCut *c = new AliFemtoV0PairCut(*this);
+  return c;
+}
 
 #endif
