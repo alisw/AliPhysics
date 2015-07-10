cb4326e081f9948d23a61c46aff9572c2332359e
mfasel <markus.fasel@cern.ch>
2015-07-10 14:07:31 +0200

    Finish doxygen documentation for EMCAL trigger classes

--- PWG/EMCAL/AliEmcalTriggerSetupInfo.cxx
+++ PWG/EMCAL/AliEmcalTriggerSetupInfo.cxx
@@ -1,48 +1,65 @@
-// $Id$
-//
-// Emcal particle trigger class, which can contain either
-//
-// Author: J.Kral
-
+/**************************************************************************
+ * Copyright(c) 1998-2013, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
 #include "AliEmcalTriggerSetupInfo.h"
 #include "AliLog.h"
 
-//_________________________________________________________________________________________________
+/// \cond CLASSIMP
+ClassImp(AliEmcalTriggerSetupInfo)
+/// \endcond
+
+/**
+ * Default constructor
+ */
 AliEmcalTriggerSetupInfo::AliEmcalTriggerSetupInfo() :
   TNamed(),
   fThresholds(),
   fThresholdsSimple()
 {
-  // Default constructor.
   for( int i = 0; i < 4; i++ )
     fThresholds[i] = -1;
   for( int i = 0; i < 4; i++ )
     fThresholdsSimple[i] = -1;
 }
 
-  
-//_________________________________________________________________________________________________
+/**
+ * Copy constructor
+ * @param p Reference for the copy
+ */
 AliEmcalTriggerSetupInfo::AliEmcalTriggerSetupInfo(const AliEmcalTriggerSetupInfo &p) :
   TNamed(p)
 {
-  // Copy constructor.
   for( int i = 0; i < 4; i++ )
     fThresholds[i] = p.fThresholds[i];
   for( int i = 0; i < 4; i++ )
     fThresholdsSimple[i] = p.fThresholdsSimple[i];
 }
 
-//_________________________________________________________________________________________________
+/**
+ * Destructor
+ */
 AliEmcalTriggerSetupInfo::~AliEmcalTriggerSetupInfo()
 {
-  // Destructor.
 }
 
-//_________________________________________________________________________________________________
+/**
+ * Assignment operator
+ * @param p Reference for the assignment
+ * @return This object after assignment
+ */
 AliEmcalTriggerSetupInfo &AliEmcalTriggerSetupInfo::operator=(const AliEmcalTriggerSetupInfo &p)
 {
-  // Assignment operator.
-
   if (this != &p) {
     for( int i = 0; i < 4; i++ )
       fThresholds[i] = p.fThresholds[i];
@@ -53,9 +70,10 @@ AliEmcalTriggerSetupInfo &AliEmcalTriggerSetupInfo::operator=(const AliEmcalTrig
   return *this;
 }
 
-//_________________________________________________________________________________________________
+/**
+ * Cleaning function, set all thresholds to 0
+ */
 void AliEmcalTriggerSetupInfo::Clean(){
-  // cleaner
   for( int i = 0; i < 4; i++ )
     fThresholds[i] = -1;
   for( int i = 0; i < 4; i++ )
