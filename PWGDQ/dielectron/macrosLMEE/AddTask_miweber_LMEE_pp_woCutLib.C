42c71e7fccf88176714c00ba437087395c603ddf
miweber <m.weber@cern.ch>
2015-07-14 15:27:04 +0200

    New Addtask macro: - only one Config file (no cutlib file needed) - to be modified and tested

--- PWGDQ/dielectron/macrosLMEE/AddTask_miweber_LMEE_pp_woCutLib.C
+++ PWGDQ/dielectron/macrosLMEE/AddTask_miweber_LMEE_pp_woCutLib.C
@@ -1,67 +1,59 @@
-AliAnalysisTask *AddTask_miweber_LMEE_pp(TString outputFileName = "AnalysisResult.root", TString directoryBaseName = "miweber_LMEE_pp"){
+AliAnalysisTask *AddTask_miweber_LMEE_pp_woCutLib(Int_t cutDefinition = 1, TString outputFileName = "AnalysisResult.root", TString directoryBaseName = "miweber_LMEE_pp"){
 
 
   //get the current analysis manager
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
-    Error("AddTask_miweber_LMEE_pp", "No analysis manager found.");
+    Error("AddTask_miweber_LMEE_pp_woCutLib", "No analysis manager found.");
     return 0;
   }
 
   Bool_t bESDANA=kFALSE; //Autodetect via InputHandler
 
   TString configBasePath("$ALICE_PHYSICS/PWGDQ/dielectron/macrosLMEE/");
-  TString configFile("Config_miweber_LMEE_pp.C");
-  TString configLMEECutLib("LMEECutLib_miweber.C");
+  TString configFile("Config_miweber_LMEE_pp_woCutLib.C");
   TString configFilePath(configBasePath+configFile);
-  TString configLMEECutLibPath(configBasePath+configLMEECutLib);
   
    
   //load dielectron configuration files
-  if (!gROOT->GetListOfGlobalFunctions()->FindObject(configLMEECutLib.Data()))
-    gROOT->LoadMacro(configLMEECutLibPath.Data());
   if (!gROOT->GetListOfGlobalFunctions()->FindObject(configFile.Data()))
     gROOT->LoadMacro(configFilePath.Data());
-
-  // cut lib
-  LMEECutLib* cutlib = new LMEECutLib();
 
   //Do we have an MC handler?
   Bool_t hasMC=(AliAnalysisManager::GetAnalysisManager()->GetMCtruthEventHandler()!=0x0);
 
   //AOD Usage currently tested with Input handler
   if (mgr->GetInputEventHandler()->IsA()==AliAODInputHandler::Class()){
-    ::Info("AddTask_miweber_LMEE_pp", "no dedicated AOD configuration");
+    ::Info("AddTask_miweber_LMEE_pp_woCutLib", "no dedicated AOD configuration");
   }
   else if (mgr->GetInputEventHandler()->IsA()==AliESDInputHandler::Class()){
-    ::Info("AddTask_miweber_LMEE_pp","switching on ESD specific code");
+    ::Info("AddTask_miweber_LMEE_pp_woCutLib","switching on ESD specific code");
     bESDANA=kTRUE;
   }
   
   //create task and add it to the manager
-  AliAnalysisTaskMultiDielectron *task=new AliAnalysisTaskMultiDielectron("MultiDiEData_miweber_pp");
+  AliAnalysisTaskMultiDielectron *task=new AliAnalysisTaskMultiDielectron("MultiDiEData_miweber_pp_woCutLib");
   if (!hasMC) task->UsePhysicsSelection();
 
   //Add event filter
-  Int_t triggerNames=(AliVEvent::kINT7+AliVEvent::kMB+AliVEvent::kINT8);//pp 2010/2011 Min Bias?
-  task->SetEventFilter(cutlib->GetEventCuts(LMEECutLib::kpp2010All));
+  Int_t triggerNames=(AliVEvent::kINT7+AliVEvent::kMB+AliVEvent::kINT8);//pp 2010/2011 Min Bias, can be set also from outside
   task->SelectCollisionCandidates(triggerNames);
   task->SetTriggerMask(triggerNames);
   task->SetRejectPileup();
   // Note: event cuts are identical for all analysis 'cutDefinition's that run together!
 
+  //Add event filter
+  task->SetEventFilter( GetEventCuts() );
+
   // Add the task to the manager
   mgr->AddTask(task);
   
-  //add dielectron analysis with different cuts to the task
-  for (Int_t i=0; i<nDie; ++i){ //nDie defined in config file
-    //MB
-    AliDielectron *diel_low = Config_miweber_pp(i,hasMC,bESDANA);
-    if(!diel_low)continue;
+  //add dielectron analysis with selected cut to the task
+  AliDielectron *diel_low = Config_miweber_LMEE_pp_woCutLib(cutDefinition,bESDANA);
+  if(diel_low){
     task->AddDielectron(diel_low);
     printf("successfully added AliDielectron: %s\n",diel_low->GetName());
-  }//loop
-
+  }
 
   //create output container
   AliAnalysisDataContainer *coutput1 =
