859a76a5950b54186a1917f9aa612a7cf39ad0b0
arossi <andrea.rossi@cern.ch>
2015-07-15 11:12:35 +0200

    Adding comparison of fit parameters.

--- PWGHF/correlationHF/macros/DoFit.sh
+++ PWGHF/correlationHF/macros/DoFit.sh
@@ -8,6 +8,11 @@ declare -i avopt=$3
 declare inputdir=$4
 declare outputdir=$5
 declare avstring="Weighted"
+declare -i includev2=$6
+declare -i plotv2sep=1
+declare -i v2had=1
+declare -i v2D=1
+
 
 if [ ${avopt} = 1 ]; then
     avstring="Arithmetic"
@@ -44,7 +49,8 @@ elif [ ${system} = 1 ]; then
 SetInputPath("${inputdir}")
 SetOutputPath("${outputdir}")
 SetAverageString("${avstring}")
- Systematics_pPb_lowpthad(${refl})
+Systematics_pPb_lowpthad(${refl})
+SetV2values(${includev2},0.1*((Double_t)v2had),0.1*((Double_t)v2D),${plotv2sep})
 EOF
 
 root -b &>outFitPPbhighpthad.log <<EOF
@@ -53,6 +59,7 @@ SetInputPath("${inputdir}")
 SetOutputPath("${outputdir}")
 SetAverageString("${avstring}")
 Systematics_pPb_highpthad(${refl})
+SetV2values(${includev2},0.1*((Double_t)v2had),0.1*((Double_t)v2D),${plotv2sep})
 EOF
 
 root -b &>outFitPPbintegratedpthad.log <<EOF
@@ -61,5 +68,6 @@ SetInputPath("${inputdir}")
 SetOutputPath("${outputdir}")
 SetAverageString("${avstring}")
 Systematics_pPb_integratedpthad(${refl})
+SetV2values(${includev2},0.1*((Double_t)v2had),0.1*((Double_t)v2D),${plotv2sep})
 EOF
 fi
