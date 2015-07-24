b5aa32179fc0a188352a66db99ba77fefe92d7c7
saiola <salvatore.aiola@cern.ch>
2015-07-24 20:07:18 +0200

    Add option to accept D mesons depending on the originating quark

--- PWGJE/FlavourJetTasks/AliMCHFParticleSelector.h
+++ PWGJE/FlavourJetTasks/AliMCHFParticleSelector.h
@@ -9,15 +9,27 @@ class AliMCHFParticleSelector : public AliEmcalMCTrackSelector {
   AliMCHFParticleSelector(const char *name);
   virtual ~AliMCHFParticleSelector();
 
-  void SetSpecialPDG(Int_t pdg)                         { fSpecialPDG       = pdg  ; }
+  void   SetSpecialPDG(Int_t pdg)          { fSpecialPDG = pdg            ; }
+
+  void   SetRejectQuarkNotFound(Bool_t c)  { fRejectQuarkNotFound = c     ; }
+  Bool_t GetRejectQuarkNotFound() const    { return fRejectQuarkNotFound  ; }
+
+  void   SetRejectDfromB(Bool_t c)         { fRejectDfromB = c            ; }
+  Bool_t GetRejectDfromB() const           { return fRejectDfromB         ; }
+
+  void   SetKeepOnlyDfromB(Bool_t c)       { fKeepOnlyDfromB = c          ; }
+  Bool_t GetKeepOnlyDfromB() const         { return fKeepOnlyDfromB       ; }
   
  protected:
-  Bool_t                    AcceptParticle(AliAODMCParticle* part) const;
+  Bool_t          AcceptParticle(AliAODMCParticle* part) const;
 
-  Bool_t                    IsSpecialPDGDaughter(AliAODMCParticle* part) const;
-  Bool_t                    IsSpecialPDGDaughter(Int_t iPart) const;
+  Bool_t          IsSpecialPDGDaughter(AliAODMCParticle* part) const;
+  Bool_t          IsSpecialPDGDaughter(Int_t iPart) const;
  
-  Int_t                     fSpecialPDG;           // include particles with this PDG code even if they are not primary particles (and exclude their daughters)
+  Int_t           fSpecialPDG;             //  include particles with this PDG code even if they are not primary particles (and exclude their daughters)
+  Bool_t          fRejectQuarkNotFound;    //  reject D mesons for which the original charm or bottom quark could not be found (MC)
+  Bool_t          fRejectDfromB;           //  reject D mesons coming from a B meson decay (MC)
+  Bool_t          fKeepOnlyDfromB;         //  only accept D mesons coming from a B meson decay (MC)
   
  private:
   AliMCHFParticleSelector(const AliMCHFParticleSelector&);            // not implemented
