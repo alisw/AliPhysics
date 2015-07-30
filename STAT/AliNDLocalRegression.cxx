9d3d7ee5b4e494a11f224e9f83a0b2486c64495f
mivanov <marian.ivanov@cern.ch>
2015-07-30 12:31:00 +0200

    ATO-185, ATO-164, ATO-183, AOC-12 - adding otpition for outlier removal. Optional robust nsigma cut. Extending using test to cover the robust option case

--- STAT/AliNDLocalRegression.cxx
+++ STAT/AliNDLocalRegression.cxx
@@ -54,12 +54,16 @@
   author: marian.ivanov@cern.ch
 */
 
+#include <TVectorD.h>
+
 #include "AliNDLocalRegression.h"
 #include "AliLog.h"
 
 #include "THn.h"
 #include "TObjString.h"
 #include "TTreeStream.h"
+#include "AliMathBase.h"
+#include "TMatrixD.h"
 
 ClassImp(AliNDLocalRegression)
 
@@ -70,6 +74,9 @@ TObjArray *AliNDLocalRegression::fgVisualCorrection=0;
 AliNDLocalRegression::AliNDLocalRegression():
   TNamed(),           
   fHistPoints(0),            // ND histogram defining regression granularity
+  fRobustFractionLTS(0),           //   fraction of data used for the robust mean and robust rms estimator (LTS https://en.wikipedia.org/wiki/Least_trimmed_squares)
+  fRobustRMSLTSCut(0),           //  cut on the robust RMS  |value-localmean|<fRobustRMSLTSCut*localRMS
+  fCutType(0),                    //  type of the cut 0- no cut 1-cut localmean=median, 2-cut localmen=rosbut mean 
   fInputTree(0),             // input tree - object is not owner
   fStreamer(0),              // optional streamer 
   fFormulaVal(0),            // value:err  definition formula
@@ -83,8 +90,8 @@ AliNDLocalRegression::AliNDLocalRegression():
   fLocalFitCovar(0),          // local fit covariance matrix
   fBinIndex(0),                  //[fNParameters] working arrays current bin index
   fBinCenter(0),                 //[fNParameters] working current local variables - bin center
-  fBinDelta(0)                  //[fNParameters] working current local variables - bin delta
-
+  fBinDelta(0),                  //[fNParameters] working current local variables - bin delta
+  fLocalRobustStat(0)         // local robust statistic
 {
   if (!fgVisualCorrection) fgVisualCorrection= new TObjArray;
 }
@@ -137,6 +144,16 @@ void AliNDLocalRegression::SetHistogram(THn* histo ){
 
 
 }
+void  AliNDLocalRegression::SetCuts(Double_t nSigma, Double_t robustFraction, Int_t estimator){
+  //
+  //
+  //
+  fRobustFractionLTS=robustFraction;    //  fraction of data used for the robust mean and robust rms estimator (LTS https://en.wikipedia.org/wiki/Least_trimmed_squares)
+  fRobustRMSLTSCut=nSigma;              //  cut on the robust RMS  |value-localmean|<fRobustRMSLTSCut*localRMS
+  fCutType=estimator;                   //  type of the cut 0- no cut 1-cut localmean=median, 2-cut localmen=rosbut mean 
+
+}
+
 
 
 Bool_t AliNDLocalRegression::MakeFit(TTree * tree , const char* formulaVal, const char * formulaVar, const char*selection, const char * formulaKernel, const char * dimensionFormula, Double_t weightCut, Int_t entries){
@@ -155,10 +172,14 @@ Bool_t AliNDLocalRegression::MakeFit(TTree * tree , const char* formulaVal, cons
   //   kernelWidth - : separated list of width of kernel for local fitting
   //   dimenstionFormula - dummy for the moment
   //
+  //Algorithm:
+  //   1.) Check consistency of input data
   //
-  // 1.) Check consistency of input data
-  // 2.) Cache inout data from tree
-  // 3.) Make local fit
+  //   2.) Cache input data from tree to the array of vector TVectorD
+  //
+  //   3.) Calculate robust local mean and robust local RMS in case outlier removal algorithm specified
+  // 
+  //   4.) Make local fit
   //
   //  const Double_t kEpsilon=1e-6;
   if (fHistPoints==NULL){
@@ -228,12 +249,22 @@ Bool_t AliNDLocalRegression::MakeFit(TTree * tree , const char* formulaVal, cons
   Int_t entriesKernel = tree->Draw(formulaKernel,selection,"goffpara",entries);
   for (Int_t ipar=0; ipar<fNParameters; ipar++) kernelArray.AddAt(new TVectorD(entriesVar,tree->GetVal(ipar)),ipar);
   //
-  // 3.) Make local fits
+  //
   //
   Int_t nbins = fHistPoints->GetNbins();
   fBinIndex   = new Int_t[fHistPoints->GetNdimensions()];
   fBinCenter  = new Double_t[fHistPoints->GetNdimensions()];
   fBinDelta   = new Double_t[fHistPoints->GetNdimensions()];
+
+  //
+  // 3.) 
+  //
+  if (fCutType>0 && fRobustRMSLTSCut>0){
+    MakeRobustStatistic(values, errors,  pointArray, kernelArray, weightCut, fRobustFractionLTS);
+  }
+  //
+  // 4.) Make local fits
+  //
   Double_t *binHypFit  = new Double_t[2*fHistPoints->GetNdimensions()];
   //
   TLinearFitter fitter(1+2*fNParameters,TString::Format("hyp%d",2*fNParameters).Data());
@@ -246,6 +277,14 @@ Bool_t AliNDLocalRegression::MakeFit(TTree * tree , const char* formulaVal, cons
     // add fit points    
     for (Int_t ipoint=0; ipoint<entriesVal; ipoint++){
       Double_t weight=1;
+      if (fCutType>0 && fRobustRMSLTSCut>0){
+	if (fCutType==1){
+	  if (TMath::Abs(values[ipoint]-(*fLocalRobustStat)(ibin,0))>fRobustRMSLTSCut*(*fLocalRobustStat)(ibin,2)) continue;
+	}
+	if (fCutType==2){
+	  if (TMath::Abs(values[ipoint]-(*fLocalRobustStat)(ibin,1))>fRobustRMSLTSCut*(*fLocalRobustStat)(ibin,2)) continue;
+	}
+      }
       for (Int_t idim=0; idim<fNParameters; idim++){
 	TVectorD &vecVar=*((TVectorD*)(pointArray.UncheckedAt(idim)));
 	TVectorD &vecKernel=*((TVectorD*)(kernelArray.UncheckedAt(idim)));
@@ -276,8 +315,17 @@ Bool_t AliNDLocalRegression::MakeFit(TTree * tree , const char* formulaVal, cons
     fLocalFitCovar->AddAt(fitCovar,ibin);
     if (fStreamer){
       TVectorD pfBinCenter(fNParameters, fBinCenter);
+      Double_t median=0,mean=0,rms=0;
+      if (fLocalRobustStat){
+	median=(*fLocalRobustStat)(ibin,0);
+	mean=(*fLocalRobustStat)(ibin,1);
+	rms=(*fLocalRobustStat)(ibin,2);
+      }
       (*fStreamer)<<"localFit"<<
 	"ibin="<<ibin<<                // bin index
+	"localMedian="<<median<<
+	"localMean="<<mean<<
+	"localRMS="<<rms<<
 	"nBinPoints="<<nBinPoints<<    // center of the bin
 	"binCenter.="<<&pfBinCenter<<  // 
 	"normRMS="<<normRMS<<          
@@ -288,6 +336,52 @@ Bool_t AliNDLocalRegression::MakeFit(TTree * tree , const char* formulaVal, cons
   }
   return kTRUE;
 }
+
+
+Bool_t  AliNDLocalRegression::MakeRobustStatistic(TVectorD &values,TVectorD &errors,  TObjArray &pointArray,  TObjArray &kernelArray, Double_t weightCut, Double_t robustFraction){
+  //
+  // Calculate robust statistic information
+  //
+  if (robustFraction>0) robustFraction=1;
+
+  Int_t nbins = fHistPoints->GetNbins();    // 
+  Int_t npoints= values.GetNrows();         // number of points for fit
+  if (fLocalRobustStat){
+    delete fLocalRobustStat;
+  }
+  fLocalRobustStat=new TMatrixD(nbins,3);
+
+  TVectorD valueLocal(npoints);
+  for (Int_t ibin=0; ibin<nbins; ibin++){
+    fHistPoints->GetBinContent(ibin,fBinIndex); // 
+    for (Int_t idim=0; idim<fNParameters; idim++){
+      fBinCenter[idim]=fHistPoints->GetAxis(idim)->GetBinCenter(fBinIndex[idim]);
+    }
+    Int_t indexLocal=0;
+    for (Int_t ipoint=0; ipoint<npoints; ipoint++){
+      Double_t weight=1;
+      for (Int_t idim=0; idim<fNParameters; idim++){
+	TVectorD &vecVar=*((TVectorD*)(pointArray.UncheckedAt(idim)));
+	TVectorD &vecKernel=*((TVectorD*)(kernelArray.UncheckedAt(idim)));
+	fBinDelta[idim]=vecVar[ipoint]-fBinCenter[idim];       	
+	weight*=TMath::Gaus(fBinDelta[idim],0,vecKernel[ipoint]);
+	if (weight<weightCut) continue;	
+      }      
+      if (weight<weightCut) continue;
+      valueLocal[indexLocal]=values[ipoint];
+      indexLocal++;
+    }
+    Double_t median=0,meanX=0, rmsX=0;
+    if (indexLocal*robustFraction>3){
+      median=TMath::Median(indexLocal,valueLocal.GetMatrixArray());
+      AliMathBase::EvaluateUni(indexLocal,valueLocal.GetMatrixArray(), meanX,rmsX, indexLocal*robustFraction);
+    }
+    (*fLocalRobustStat)(ibin,0)=median;
+    (*fLocalRobustStat)(ibin,1)=meanX;
+    (*fLocalRobustStat)(ibin,2)=rmsX;
+  }
+}
+
 
 
 Double_t AliNDLocalRegression::Eval(Double_t *point ){
