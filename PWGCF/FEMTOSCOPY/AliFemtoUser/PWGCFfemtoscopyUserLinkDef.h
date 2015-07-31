948c4f2c3a84832bda847afd37bc5c7589cb34e2
majanik <majanik@cern.ch>
2015-07-31 15:43:46 +0200

    AliFemto classes for P. Karczmarczyk (AliFemtoPairCutRadialDistanceAsymmetric) + train config

--- PWGCF/FEMTOSCOPY/AliFemtoUser/PWGCFfemtoscopyUserLinkDef.h
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/PWGCFfemtoscopyUserLinkDef.h
@@ -63,3 +63,4 @@
 #pragma link C++ class AliFemtoCorrFctnGammaMonitorAlpha+;
 #pragma link C++ class AliFemtoCorrFctn3DPRF+;
 #pragma link C++ class AliFemtoPARJGenPythia+;
+#pragma link C++ class AliFemtoPairCutRadialDistanceAsymmetric+;
