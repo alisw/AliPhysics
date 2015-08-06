896c26d003b6153542ecf0167b1493f2a9d0b2ae
mfasel <markus.fasel@cern.ch>
2015-08-06 13:53:52 +0200

    ALIROOT-6222: Add simple test task in order to test reading of the ESD file with branch AliESDFMD disabled

---
+++ PWG/Tools/macros/AddTaskDummy.C
@@ -0,0 +1,7 @@
+void AddTaskDummy() {
+  /// Add dummy task for read check
+  /// Switch off FMD branch for a test
+
+  AliESDInputHandler *inh = dynamic_cast<AliESDInputHandler *>(AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler());
+  inh->SetInactiveBranches("ALIESDFMD"); // Disable FMD branch
+}
