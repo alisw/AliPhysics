4db05576ce97d5040cdfa0d863b6ed649331e597
fbock <friederike.bock@cern.ch>
2015-08-08 16:48:13 +0200

    put correct branch name for FMD

--- PWG/Tools/macros/AddTaskDummy.C
+++ PWG/Tools/macros/AddTaskDummy.C
@@ -2,6 +2,7 @@ void AddTaskDummy() {
   /// Add dummy task for read check
   /// Switch off FMD branch for a test
 
-  AliESDInputHandler *inh = dynamic_cast<AliESDInputHandler *>(AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler());
-  inh->SetInactiveBranches("ALIESDFMD"); // Disable FMD branch
+	AliESDInputHandler *inh = dynamic_cast<AliESDInputHandler *>(AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler());
+	inh->SetInactiveBranches("AliESDFMD"); // Disable FMD branch
+  
 }
