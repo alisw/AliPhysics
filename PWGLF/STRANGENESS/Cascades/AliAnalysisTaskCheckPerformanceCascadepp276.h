ff377f6d5d5de3d06434593f03de351d4c6ca256
dcolella <mimmo@pb-d-128-141-144-142.cern.ch>
2015-08-09 15:03:28 +0200

    Add setting of the topological cuts for revertexers

--- PWGLF/STRANGENESS/Cascades/AliAnalysisTaskCheckPerformanceCascadepp276.h
+++ PWGLF/STRANGENESS/Cascades/AliAnalysisTaskCheckPerformanceCascadepp276.h
@@ -83,7 +83,23 @@ class AliAnalysisTaskCheckPerformanceCascadepp276 : public AliAnalysisTaskSE {
         Bool_t          fApplyAccCut;                   // flag to apply acceptance cuts to MC cascades       
         Float_t         fMinPtCutOnDaughterTracks;      // minimum pt cut on daughter tracks
         Float_t         fEtaCutOnDaughterTracks;        // pseudorapidity cut on daughter tracks 
-        
+        //Setters for the V0 and cascade Vertexer Parameters
+        void SetV0VertexerMaxChisquare           (Double_t lParameter){ fV0Sels[0] = lParameter; }
+        void SetV0VertexerDCAFirstToPV           (Double_t lParameter){ fV0Sels[1] = lParameter; }
+        void SetV0VertexerDCASecondtoPV          (Double_t lParameter){ fV0Sels[2] = lParameter; }
+        void SetV0VertexerDCAV0Daughters         (Double_t lParameter){ fV0Sels[3] = lParameter; }
+        void SetV0VertexerCosinePA               (Double_t lParameter){ fV0Sels[4] = lParameter; }
+        void SetV0VertexerMinRadius              (Double_t lParameter){ fV0Sels[5] = lParameter; }
+        void SetV0VertexerMaxRadius              (Double_t lParameter){ fV0Sels[6] = lParameter; }
+        void SetCascVertexerMaxChisquare         (Double_t lParameter){ fCascSels[0] = lParameter; }
+        void SetCascVertexerMinV0ImpactParameter (Double_t lParameter){ fCascSels[1] = lParameter; }
+        void SetCascVertexerV0MassWindow         (Double_t lParameter){ fCascSels[2] = lParameter; }
+        void SetCascVertexerDCABachToPV          (Double_t lParameter){ fCascSels[3] = lParameter; }
+        void SetCascVertexerDCACascadeDaughters  (Double_t lParameter){ fCascSels[4] = lParameter; }
+        void SetCascVertexerCascadeCosinePA      (Double_t lParameter){ fCascSels[5] = lParameter; }
+        void SetCascVertexerCascadeMinRadius     (Double_t lParameter){ fCascSels[6] = lParameter; }
+         void SetCascVertexerCascadeMaxRadius     (Double_t lParameter){ fCascSels[7] = lParameter; } 
+       
         Double_t        fV0Sels[7];                     // Array to store the 7 values for the different selections V0 related (if fkRerunV0CascVertexers)
         Double_t        fCascSels[8];                   // Array to store the 8 values for the different selections Casc. related (if fkRerunV0CascVertexers)
 	
