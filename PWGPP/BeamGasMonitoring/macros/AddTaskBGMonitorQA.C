d35aed6c3c5c9131423d6c456bf2ec4648350bc4
sjena <sjena@cern.ch>
2015-08-11 18:38:26 +0200

    High M trigger QA:Alexander

---
+++ PWGPP/BeamGasMonitoring/macros/AddTaskBGMonitorQA.C
@@ -0,0 +1,37 @@
+AliAnalysisBGMonitorQA *AddTaskBGMonitorQA(Bool_t UseTree = kFALSE)
+{
+  //
+  //This macro configures the task for the Beam Gas Monitoring QA
+  //==============================================================================
+
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTaskMBVeto", "No analysis manager to connect to.");
+    return NULL;
+  }   
+  
+  if (!mgr->GetInputEventHandler()) {
+    ::Error("AddTaskMBVeto", "This task requires an input event handler");
+    return NULL;
+  }
+    
+  // Create and configure the task
+  AliAnalysisBGMonitorQA *taskMBVeto = new AliAnalysisBGMonitorQA("taskMBVeto");
+  mgr->AddTask(taskMBVeto);
+  
+  // Create containers for input/output
+  AliAnalysisDataContainer *cinput = mgr->CreateContainer("cchain1",TChain::Class(), AliAnalysisManager::kInputContainer);
+  mgr->ConnectInput(taskMBVeto, 0, mgr->GetCommonInputContainer());
+  
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("cOutputH", TList::Class(), AliAnalysisManager::kOutputContainer, Form("%s:BeamGasMon", mgr->GetCommonFileName()));
+  mgr->ConnectOutput(taskMBVeto, 1, coutput1);
+  
+    if(UseTree==kTRUE){
+        AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("cOutputT", TTree::Class(), AliAnalysisManager::kOutputContainer, Form("%s:BeamGasMon", mgr->GetCommonFileName()));
+        mgr->ConnectOutput(taskMBVeto, 2, coutput2);
+    }
+    
+  return taskMBVeto;
+  
+}
+
