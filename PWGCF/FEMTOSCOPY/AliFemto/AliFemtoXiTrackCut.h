a90bac78cdc7ed1b528aa44e662369775eedf6bd
lgraczyk <lgraczyk@cern.ch>
2015-09-02 00:13:00 +0200

    AliFemto: adding Xi cuts to cascade track cut class

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoXiTrackCut.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoXiTrackCut.h
@@ -16,7 +16,7 @@
 class AliFemtoXiTrackCut : public AliFemtoV0TrackCut 
 {
   public:
-  enum XiType {kXiMinus = 0, kXiPlus=1};
+  enum XiType {kXiMinus = 0, kXiPlus=1, kAll = 99, kXiMinusMC = 101, kXiPlusMC=102};
   typedef enum XiType AliFemtoXiType;
 
 
@@ -28,11 +28,45 @@ class AliFemtoXiTrackCut : public AliFemtoV0TrackCut
   virtual AliFemtoString Report();
   virtual TList *ListSettings();
   virtual AliFemtoParticleType Type(){return hbtXi;}
+
+  bool IsPionNSigmaBac(float mom, float nsigmaTPCPi, float nsigmaTOFPi);
+
+  void SetEtaXi(double x);
+  void SetPtXi(double min, double max);
+  void SetEtaBac(double x);
+  void SetPtBac(double min, double max);
+  void SetTPCnclsBac(int x);
+  void SetNdofDaughters(double x);
+  void SetStatusDaughters(unsigned long x);
+  void SetInvariantMassXi(double min, double max);
+  void SetMaxDcaXi(double x);
+  void SetMaxDcaXiBac(double x);
+  void SetMinDcaXiDaughters(double x);
+  void SetMaxCosPointingAngleXi(double max);
+  void SetParticleTypeXi(short x);
  
   
  private:   // here are the quantities I want to cut on...
 
-
+  double fMaxEtaXi;
+  double fMinPtXi;
+  double fMaxPtXi;
+  
+  double fMaxEtaBac;
+  double fMinPtBac;
+  double fMaxPtBac;
+  int fTPCNclsBac;
+  double fNdofBac;
+  unsigned long fStatusBac;
+  double fMaxDcaXi;
+  double fMaxDcaXiBac;
+  double fMinDcaXiDaughters;
+  double fMaxCosPointingAngleXi;
+  double fMaxDecayLengthXi;
+  double fInvMassXiMin;
+  double fInvMassXiMax;
+  short  fParticleTypeXi;
+  
 
 #ifdef __ROOT__ 
   ClassDef(AliFemtoXiTrackCut, 1)
