8d0577f594d45f04df4610147998660f5284ba81
akubera <Andrew.Kubera@cern.ch>
2015-09-01 21:22:59 -0400

    PWGCF-AliFemtoUser-AliFemtoCorrFctn: Changed implementation of average separation calculation to ensure that the values of the TPC points have been set by the reader (not the default -9999). Added documentation explaining how the class works.

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoAvgSepCorrFctn.h
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoAvgSepCorrFctn.h
@@ -2,9 +2,6 @@
 /// \file AliFemtoAvgSepCorrFctn.h
 /// \author M. Janik, L. Graczykowski, Warsaw University of Technology
 ///
-/// \class AliFemtoAvgSepCorrFctn
-/// \brief An average entrance separation correlation function
-///
 
 #ifndef ALIFEMTOAVGSEPCORRFCTN_H
 #define ALIFEMTOAVGSEPCORRFCTN_H
@@ -12,18 +9,77 @@
 #include "TH1D.h"
 #include "AliFemtoCorrFctn.h"
 
+/**
+ * \class AliFemtoAvgSepCorrFctn
+ * \brief A correlation function built from comparing the separation between
+ *        tracks from nominal points in the TPC.
+ *
+ * The AliFemtoAvgSepCorrFctn stores numerator and denomiator histograms
+ * counting the pair frequency of the average separation of the tracks in the
+ * pair, measured in centimeters. This average separation is calculated by
+ * progressing through the "NominalTpcPoints" as reported by the AliFemtoTrack
+ * method NominalTpcPoint(int). The distance between the two tracks at each
+ * point is averaged and this quantity is stored.
+ *
+ * This class enables finding the average separation of multiple "types" of
+ * particle pairs. By using the SetPairType method, you specify which algorithm
+ * to use when calculating average separation and which histograms will be
+ * written upon completion. The options are specified by the enum 'PairType'.
+ * The default value is kTracks, which simply accepts pairs of AliFemtoTracks
+ * and has a simple numerator and denominator histogram. Setting the PairType
+ * to kTrackV0 accepts pairs where the first track is a AliFemtoV0 and the
+ * second is an AliFemtoTrack; there are two sets of histograms produced with
+ * this configuration, between track and the positive & negative daughters of
+ * the V0. The case kV0 accepts pairs of V0 tracks, and produces 4 sets of
+ * histograms, for each combination of positive and negative daughters for both
+ * of the V0s.
+ *
+ * Note: If the track does not pass through the entire TPC, some of the points
+ * are left at their default value (-9999). The averaging algorithm prior to
+ * September 2015 did not check for points which were unset; since then it will
+ * average only the points which have been set. This has the potential of
+ * calculating the average separation from only a few points, which may be
+ * undesireable. The number of misses is very dependent on pT, and thus it is
+ * recommended to take the average separation into account when determining
+ * your pT cut.
+ */
 class AliFemtoAvgSepCorrFctn : public AliFemtoCorrFctn {
 public:
-  enum PairType {kTracks = 0, kTrackV0 = 1, kV0s = 2};
+  /**
+   * Enumeration of the possible pair configuration of AliFemtoParticle types
+   * to expect in the AliFemtoPair pairs given to the AddRealPair and
+   * AddMixedPair methods.
+   *  - kTracks: Both AliFemtoTracks
+   *  - kTrackV0: Track1=AliFemtoV0, Track2=AliFemtoTrack
+   *  - kV0s: Both AliFemtoV0
+   */
+  enum PairType {
+    kTracks = 0,
+    kTrackV0 = 1,
+    kV0s = 2
+  };
   typedef enum PairType AliFemtoPairType;
 
+  /**
+   * Construct with histogram parameters
+   */
   AliFemtoAvgSepCorrFctn(char *title, const int &nbins, const float &Low, const float &High);
+
+  /// Copy Constructor
   AliFemtoAvgSepCorrFctn(const AliFemtoAvgSepCorrFctn &aCorrFctn);
+
+  /// Destructor
   virtual ~AliFemtoAvgSepCorrFctn();
 
+  /// Assignment Operator
   AliFemtoAvgSepCorrFctn &operator=(const AliFemtoAvgSepCorrFctn &aCorrFctn);
 
   virtual AliFemtoString Report();
+
+  /**
+   * Extract the average separation between the two particles in the
+   * AliFemtoPair and store in appropriate histograms.
+   */
   virtual void AddRealPair(AliFemtoPair *aPair);
   virtual void AddMixedPair(AliFemtoPair *aPair);
   virtual void Finish();
@@ -38,26 +94,26 @@ public:
 
 private:
   //2 tracks
-  TH1D *fNumerator;          // numerator - real pairs
-  TH1D *fDenominator;        // denominator - mixed pairs
+  TH1D *fNumerator;          ///< numerator - real pairs
+  TH1D *fDenominator;        ///< denominator - mixed pairs
 
   //track + V0
-  TH1D *fNumeratorPos;          // numerator - real pairs
-  TH1D *fDenominatorPos;        // denominator - mixed pairs
-  TH1D *fNumeratorNeg;          // numerator - real pairs
-  TH1D *fDenominatorNeg;        // denominator - mixed pairs
+  TH1D *fNumeratorPos;       ///< numerator - real pairs
+  TH1D *fDenominatorPos;     ///< denominator - mixed pairs
+  TH1D *fNumeratorNeg;       ///< numerator - real pairs
+  TH1D *fDenominatorNeg;     ///< denominator - mixed pairs
 
   //2 V0s
-  TH1D *fNumeratorPosPos;          // numerator - real pairs
-  TH1D *fDenominatorPosPos;        // denominator - mixed pairs
-  TH1D *fNumeratorPosNeg;          // numerator - real pairs
-  TH1D *fDenominatorPosNeg;        // denominator - mixed pairs
-  TH1D *fNumeratorNegPos;          // numerator - real pairs
-  TH1D *fDenominatorNegPos;        // denominator - mixed pairs
-  TH1D *fNumeratorNegNeg;          // numerator - real pairs
-  TH1D *fDenominatorNegNeg;        // denominator - mixed pairs
+  TH1D *fNumeratorPosPos;    ///< numerator - real pairs
+  TH1D *fDenominatorPosPos;  ///< denominator - mixed pairs
+  TH1D *fNumeratorPosNeg;    ///< numerator - real pairs
+  TH1D *fDenominatorPosNeg;  ///< denominator - mixed pairs
+  TH1D *fNumeratorNegPos;    ///< numerator - real pairs
+  TH1D *fDenominatorNegPos;  ///< denominator - mixed pairs
+  TH1D *fNumeratorNegNeg;    ///< numerator - real pairs
+  TH1D *fDenominatorNegNeg;  ///< denominator - mixed pairs
 
-  TH1D *fRatio;              // ratio - correlation function
+  TH1D *fRatio;              ///< ratio - correlation function
   AliFemtoPairType fPairType;
 
 
@@ -81,4 +137,3 @@ inline  TH1D *AliFemtoAvgSepCorrFctn::Ratio()
 
 
 #endif
-
