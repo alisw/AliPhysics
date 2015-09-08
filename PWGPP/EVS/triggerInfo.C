56693a4986e0e729cbf273bebc2476e6fb46bab4
ekryshen <evgeny.kryshen@cern.ch>
2015-09-08 17:07:22 +0200

    EVS QA macros updated to handle recent runs

--- PWGPP/EVS/triggerInfo.C
+++ PWGPP/EVS/triggerInfo.C
@@ -20,14 +20,18 @@ UInt_t dif(UInt_t stop, UInt_t start){
   return d;
 };
 
-TObjArray GetClasses(Int_t run, TString ocdbStorage, ULong64_t* L0B, ULong64_t* L0A, ULong64_t* L1B, ULong64_t* L1A, ULong64_t* L2B, ULong64_t* L2A){
+TObjArray GetClasses(Int_t run, TString ocdbStorage, TString &partition, TString &activeDetectors, Double_t& run_duration, 
+    ULong64_t* LMB, ULong64_t* LMA, ULong64_t* L0B, ULong64_t* L0A, ULong64_t* L1B, ULong64_t* L1A, ULong64_t* L2B, ULong64_t* L2A){
   AliCDBManager* man = AliCDBManager::Instance();
   man->SetDefaultStorage(ocdbStorage.Data());
   man->SetRun(run);
   // Get scalers 
   AliTriggerConfiguration* cfg = (AliTriggerConfiguration*) man->Get("GRP/CTP/Config")->GetObject();
   if (!cfg) { printf("No GRP/CTP/Config object for run %i\n",run); return TObjArray(); }
-
+  
+  partition = cfg->GetName();
+  activeDetectors = cfg->GetActiveDetectors();
+  
   TObjArray classes = cfg->GetClasses();
   AliTriggerRunScalers* scalers = (AliTriggerRunScalers*) man->Get("GRP/CTP/Scalers")->GetObject();
   if (!scalers) { printf("No GRP/CTP/Scalers object for run %i\n",run); return TObjArray(); }
@@ -46,109 +50,39 @@ TObjArray GetClasses(Int_t run, TString ocdbStorage, ULong64_t* L0B, ULong64_t* 
       const AliTriggerScalers* scaler2 = record2->GetTriggerScalersForClass(classId);
       if (!scaler1) { printf("Null pointer to scalers for class\n"); return TObjArray(); }
       if (!scaler2) { printf("Null pointer to scalers for class\n"); return TObjArray(); }
-      L0B[i] += dif(scaler2->GetLOCB(),scaler1->GetLOCB());
-      L0A[i] += dif(scaler2->GetLOCA(),scaler1->GetLOCA());
-      L1B[i] += dif(scaler2->GetL1CB(),scaler1->GetL1CB());
-      L1A[i] += dif(scaler2->GetL1CA(),scaler1->GetL1CA());
-      L2B[i] += dif(scaler2->GetL2CB(),scaler1->GetL2CB());
-      L2A[i] += dif(scaler2->GetL2CA(),scaler1->GetL2CA());
+      
+      LMB[classId-1] += dif(scaler2->GetLMCB(),scaler1->GetLMCB());
+      LMA[classId-1] += dif(scaler2->GetLMCA(),scaler1->GetLMCA());
+      L0B[classId-1] += dif(scaler2->GetLOCB(),scaler1->GetLOCB());
+      L0A[classId-1] += dif(scaler2->GetLOCA(),scaler1->GetLOCA());
+      L1B[classId-1] += dif(scaler2->GetL1CB(),scaler1->GetL1CB());
+      L1A[classId-1] += dif(scaler2->GetL1CA(),scaler1->GetL1CA());
+      L2B[classId-1] += dif(scaler2->GetL2CB(),scaler1->GetL2CB());
+      L2A[classId-1] += dif(scaler2->GetL2CA(),scaler1->GetL2CA());
     }
-  }
-  return classes;
-}
-
-
-Int_t triggerInfo(Int_t run, TString refClassName, TString ocdbStorage, TString &activeDetectors, Double_t* par){
-  AliCDBManager* man = AliCDBManager::Instance();
-  man->SetDefaultStorage(ocdbStorage.Data());
-  man->SetRun(run);
-  if (run!=189694) { // No GRP/GRP/LHCData for this run
-    AliLHCData* lhc = (AliLHCData*) man->Get("GRP/GRP/LHCData")->GetObject();
-    if (!lhc) { printf("No GRP/GRP/LHCData object for run %i\n",run); return 1; }
-    par[0] = lhc->GetFillNumber();
-  }
-  
-  AliGRPObject* grp = (AliGRPObject*) man->Get("GRP/GRP/Data")->GetObject();
-  printf("%s\n",grp->GetLHCState().Data());
-  if (!grp->GetLHCState().Contains("STABLE") && !grp->GetLHCState().Contains("ADJUST")) return 1;
-  
-  AliTriggerConfiguration* cfg = (AliTriggerConfiguration*) man->Get("GRP/CTP/Config")->GetObject();
-  if (!cfg) { printf("No GRP/CTP/Config object for run %i\n",run); return 1; }
-  printf("%s\n",cfg->GetName());
-  if (!TString(cfg->GetName()).Contains("PHYSICS_1")) return 1;
-  
-  
-  activeDetectors = cfg->GetActiveDetectors().Data();
-
-  // Get scalers 
-  AliTriggerRunScalers* scalers = (AliTriggerRunScalers*) man->Get("GRP/CTP/Scalers")->GetObject();
-  if (!scalers) { printf("No GRP/CTP/Scalers object for run %i\n",run); return 1; }
-  Int_t nEntries = scalers->GetScalersRecords()->GetEntriesFast();
-  printf("Scalers records: %i\n",nEntries);
-  if (nEntries==0) return 5;
-  Double_t run_duration   = 0;
-  ULong64_t l0b      = 0;
-  ULong64_t l0bempty = 0;
-  Int_t classId      = cfg->GetClassIndexFromName(refClassName);
-  Int_t emptyclassid = cfg->GetClassIndexFromName("CBEAMB-ABCE-NOPF-ALL");
-  for (Int_t r=0;r<nEntries-1;r++){
-    // Get SOR and EOR scaler records
-    AliTriggerScalersRecord* record1 = scalers->GetScalersRecord(r);
-    AliTriggerScalersRecord* record2 = scalers->GetScalersRecord(r+1);
-    if (!record1) { printf("Null pointer to scalers record\n"); return 2; }
-    if (!record2) { printf("Null pointer to scalers record\n"); return 2; }
-    const AliTimeStamp*      stamp1  = record1->GetTimeStamp();
-    const AliTimeStamp*      stamp2  = record2->GetTimeStamp();
-    const AliTriggerScalers* scaler1 = record1->GetTriggerScalersForClass(classId);
-    const AliTriggerScalers* scaler2 = record2->GetTriggerScalersForClass(classId);
-    if (!stamp1 ) { printf("Null pointer to start timestamp\n");   return 2; }
-    if (!stamp2 ) { printf("Null pointer to stop timestamp\n");    return 2; }
-    if (!scaler1) { printf("Null pointer to scalers for class %s\n",refClassName.Data()); return 2; }
-    if (!scaler2) { printf("Null pointer to scalers for class %s\n",refClassName.Data()); return 2; }
-//    run_duration += dif(stamp2->GetSeconds(),stamp1->GetSeconds());
-    l0b          += dif(scaler2->GetLOCB()  ,scaler1->GetLOCB());
-    if (emptyclassid<0) continue;
-    const AliTriggerScalers* emptyScaler1 = record1->GetTriggerScalersForClass(emptyclassid);
-    const AliTriggerScalers* emptyScaler2 = record2->GetTriggerScalersForClass(emptyclassid);
-    if (!emptyScaler1) { printf("Null pointer to scalers for empty class\n"); return 2; }
-    if (!emptyScaler2) { printf("Null pointer to scalers for empty class\n");  return 2; }
-    l0bempty+=dif(emptyScaler2->GetLOCB(),emptyScaler1->GetLOCB());
   }
   UInt_t t1 = scalers->GetScalersRecord(0         )->GetTimeStamp()->GetSeconds();
   UInt_t t2 = scalers->GetScalersRecord(nEntries-1)->GetTimeStamp()->GetSeconds();
   run_duration = dif(t2,t1);
-  
-  for (Int_t r=0;r<nEntries-1;r++){
-    // Get SOR and EOR scaler records
-    AliTriggerScalersRecord* record1 = scalers->GetScalersRecord(r);
-    const AliTimeStamp*      stamp1  = record1->GetTimeStamp();
-    Int_t period = stamp1->GetPeriod();
-    Int_t orbit = stamp1->GetOrbit();
-    printf("%5i %5i %5i %ll\n",r,period,orbit);
-  }
+  return classes;
+}
 
-  
-  par[1] = run_duration;
-  par[2] = l0b;
-  if (TMath::Abs(run_duration)<1) return 3;
 
-  // Get number of colliding bunches per orbit
-  Double_t nBCsPerOrbit = -1;
-  Double_t orbitRate = 11245.;
-  if (refClassName.Contains("1B-ABCE-")){
-    nBCsPerOrbit = lhc->GetNInteractingBunchesMeasured();
-    Printf("Number of BCs from LHC data=%i",nBCsPerOrbit);
-    if (nBCsPerOrbit<0 && l0bempty>0) nBCsPerOrbit = Double_t(l0bempty)/orbitRate/run_duration;
-  } else {
-    // Extract number of bunches per orbit
-    AliTriggerClass* cl = cfg->GetTriggerClass(classId);
-    AliTriggerBCMask* mask = cl->GetBCMask();
-    nBCsPerOrbit = mask->GetNUnmaskedBCs();
-  }
-  par[3] = nBCsPerOrbit;
+Int_t triggerInfo(Int_t run, TString ocdbStorage, TString &lhcState, Int_t &fill, Int_t &nBCsPerOrbit){
+  AliCDBManager* man = AliCDBManager::Instance();
+  man->SetDefaultStorage(ocdbStorage.Data());
+  man->SetRun(run);
+
+  AliGRPObject* grp = (AliGRPObject*) man->Get("GRP/GRP/Data")->GetObject();
+  if (!grp) { printf("No GRP/GRP/Data object for run %i\n",run); return 1; }
+  lhcState = grp->GetLHCState();
   
-  Double_t totalBCs = orbitRate*run_duration*nBCsPerOrbit;
-  if (totalBCs<1 || l0b<1) return 4;
-  par[4] = -TMath::Log(1-Double_t(l0b)/totalBCs); // mu
+  if (run==189694) return 1; // No GRP/GRP/LHCData for this run
+  
+  AliLHCData* lhc = (AliLHCData*) man->Get("GRP/GRP/LHCData")->GetObject();
+  if (!lhc) { printf("No GRP/GRP/LHCData object for run %i\n",run); return 1; }
+  fill = lhc->GetFillNumber();
+  nBCsPerOrbit = lhc->GetNInteractingBunchesMeasured();
+
   return 0;
 }
