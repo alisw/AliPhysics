e2360316fe5a23441cb83317e5e3e97f661d4386
lgraczyk <lgraczyk@cern.ch>
2015-09-11 15:54:31 +0200

    AliFemto: correcting beta_t

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoBetaTPairCut.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoBetaTPairCut.h
@@ -1,10 +1,11 @@
-/////////////////////////////////////////////////////////////////////////////
-//                                                                         //
-// AliFemtoBetaTPairCut - a pair cut which selects pairs based on their       //
-// transverse momentum kT                                                  //
-//                                                                         //
-/////////////////////////////////////////////////////////////////////////////
-
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+// AliFemtoBetaTPairCut - a pair cut which selects paris based on their     //
+// betaT value                                                              //
+//                                                                          //
+// Authors: Przemyslaw Karczmarczyk przemyslaw.karczmarczyk@cern.ch         //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
 #ifndef ALIFEMTOBETATPAIRCUT_H
 #define ALIFEMTOBETATPAIRCUT_H
 
@@ -13,7 +14,7 @@
 class AliFemtoBetaTPairCut : public AliFemtoPairCut{
 public:
   AliFemtoBetaTPairCut();
-  AliFemtoBetaTPairCut(double min, double max);
+  AliFemtoBetaTPairCut(double minbetat, double maxbetat, double masspart1, double masspart2);
   AliFemtoBetaTPairCut(const AliFemtoBetaTPairCut& c);
   virtual ~AliFemtoBetaTPairCut();
   AliFemtoBetaTPairCut& operator=(const AliFemtoBetaTPairCut& c);
@@ -22,11 +23,14 @@ public:
   virtual TList *ListSettings();
   AliFemtoPairCut* Clone();
   void SetBetaTRange(double minbetat, double maxbetat);
+  void SetParticleMasses(double masspart1, double masspart2);
   virtual bool Pass(const AliFemtoPair* pair);
 
  protected:
   Double_t fBetaTMin;          // Minimum allowed BetaT
   Double_t fBetaTMax;          // Maximum allowed BetaT
+  Double_t fMassPart1;         // Mass of the first particle in pair [GeV]
+  Double_t fMassPart2;         // Mass of the second particle in pair [GeV]
 
 #ifdef __ROOT__
   ClassDef(AliFemtoBetaTPairCut, 0)
--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCutMonitorPairBetaT.cxx
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCutMonitorPairBetaT.cxx
@@ -17,40 +17,52 @@ AliFemtoCutMonitorPairBetaT::AliFemtoCutMonitorPairBetaT():
   fHistBetaT(0),
   fBinsBetaT(100),
   fMinBetaT(0.0),
-  fMaxBetaT(1.0)
+  fMaxBetaT(1.0),
+  fMassPart1(0.13957018),
+  fMassPart2(0.13957018)
 {
   // Default constructor
   fMinBetaT = 0.0;
   fMaxBetaT = 1.0;
   fBinsBetaT = 100;
   fHistBetaT = new TH1D("BetaT", "BetaT distribution", fBinsBetaT, 0.0, 1.0);
+  fMassPart1 = 0.13957018;
+  fMassPart2 = 0.13957018;
 }
 
-AliFemtoCutMonitorPairBetaT::AliFemtoCutMonitorPairBetaT(const char *aName, const int aBinsBetaT, double aMinBetaT, double aMaxBetaT):
+AliFemtoCutMonitorPairBetaT::AliFemtoCutMonitorPairBetaT(const char *aName, const int aBinsBetaT, double aMinBetaT, double aMaxBetaT, double aMassPart1, double aMassPart2):
   fHistBetaT(0),
   fBinsBetaT(100),
   fMinBetaT(0.0),
-  fMaxBetaT(1.0)
+  fMaxBetaT(1.0),
+  fMassPart1(0.13957018),
+  fMassPart2(0.13957018)
 {
   // Normal constructor
   fBinsBetaT = aBinsBetaT;
   fMinBetaT = aMinBetaT;
   fMaxBetaT = aMaxBetaT;
+  fMassPart1 = aMassPart1;
+  fMassPart2 = aMassPart2;
   char name[200];
   snprintf(name, 200, "BetaT%s", aName);
-  fHistBetaT = new TH1D(name, "BetaT distribution", 100, 0.0, 1.0);
+  fHistBetaT = new TH1D(name, "BetaT distribution", fBinsBetaT, 0.0, 1.0);
 }
 
 AliFemtoCutMonitorPairBetaT::AliFemtoCutMonitorPairBetaT(const AliFemtoCutMonitorPairBetaT &c):
   fHistBetaT(0),
   fBinsBetaT(100),
   fMinBetaT(0.0),
-  fMaxBetaT(1.0)
+  fMaxBetaT(1.0),
+  fMassPart1(0.13957018),
+  fMassPart2(0.13957018)
 {
   // Copy constructor
   fBinsBetaT = c.fBinsBetaT;
   fMinBetaT = c.fMinBetaT;
   fMaxBetaT = c.fMaxBetaT;
+  fMassPart1 = c.fMassPart1;
+  fMassPart2 = c.fMassPart2;
   fHistBetaT = new TH1D(*c.fHistBetaT);
 }
 
@@ -69,6 +81,8 @@ AliFemtoCutMonitorPairBetaT& AliFemtoCutMonitorPairBetaT::operator=(const AliFem
   fBinsBetaT = c.fBinsBetaT;
   fMinBetaT = c.fMinBetaT;
   fMaxBetaT = c.fMaxBetaT;
+  fMassPart1 = c.fMassPart1;
+  fMassPart2 = c.fMassPart2;
   
   return *this;
 }
@@ -89,19 +103,19 @@ void AliFemtoCutMonitorPairBetaT::Fill(const AliFemtoPair* aPair) {
   double pxpair = px1 + px2;
   double pypair = py1 + py2;
   double pTpair = TMath::Sqrt(pxpair*pxpair + pypair*pypair);
-  // Calculate energy of the pair:
+  // Calculate energies of particles:
   double pz1 = aPair->Track1()->Track()->P().z();
   double pz2 = aPair->Track2()->Track()->P().z();
   double pzpair = pz1 + pz2;
   double p1 = TMath::Sqrt(px1*px1 + py1*py1 + pz1*pz1);
   double p2 = TMath::Sqrt(px2*px2 + py2*py2 + pz2*pz2);
-  double m1 = aPair->Track1()->Track()->GetMass();
-  double m2 = aPair->Track2()->Track()->GetMass();
+  double m1 = fMassPart1;
+  double m2 = fMassPart2;
   double e1 = TMath::Sqrt(p1*p1 + m1*m1);
   double e2 = TMath::Sqrt(p2*p2 + m2*m2);
-  double epair = e1 + e2;
   // Calculate transverse mass of the pair:
-  double mTpair = TMath::Sqrt(epair*epair - pzpair*pzpair);
+  double mInvpair_2 = m1*m1 + m2*m2 + 2*(e1*e2 - px1*px2 - py1*py2 - pz1*pz2);
+  double mTpair = TMath::Sqrt(mInvpair_2 + pTpair*pTpair);
   // Calculate betaT:
   double betaT = pTpair / mTpair;
   
