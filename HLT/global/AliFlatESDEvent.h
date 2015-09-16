4f7035e2cf687040885f03b85798ea6a954e7e28
sgorbuno <Sergey.Gorbunov@cern.ch>
2015-09-16 14:34:56 +0200

    memory alignment for the track table in flat ESD event

--- HLT/global/AliFlatESDEvent.h
+++ HLT/global/AliFlatESDEvent.h
@@ -243,7 +243,7 @@ inline const AliFlatESDTrack  *AliFlatESDEvent::GetFlatTrack( Int_t i ) const
 inline Int_t AliFlatESDEvent::SetTracksStart( AliFlatESDTrack* &t, Long64_t* &table, Int_t nTracks, size_t freeMem)
 {
   fNTracks = 0;
-  int memoryAlignment  = static_cast<ULong64_t>(fContent+fContentSize) % 64;
+  size_t memoryAlignment  = reinterpret_cast<size_t>(fContent+fContentSize) % 64;
   if( nTracks*sizeof(Long64_t)  + memoryAlignment> freeMem ) return -1;
   fContentSize += memoryAlignment;
   fTrackTablePointer = fContentSize;  
