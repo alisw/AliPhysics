3244f37eab40aa8f407449a4fb1dd8d40338db62
mkrzewic <mikolaj.krzewicki@cern.ch>
2015-09-18 13:53:55 +0200

    update of the testconfigCalib.C

--- HLT/global/physics/macros/testconfigCalib.C
+++ HLT/global/physics/macros/testconfigCalib.C
@@ -1,57 +1,11 @@
-void testconfigCalib(const char* parent = "GLOBAL-esd-converter", const char* tpcConfig="AddTaskMacro=$ALICE_PHYSICS/PWGPP/CalibMacros/CPass0/AddTaskTPCCalib.C AddTaskArgs=\"TPCCalib:CalibTimeDrift\" WriteAnalysisToFile=1")
+void testconfigCalib(const char* parent = "GLOBAL-flat-esd-converter", const char* tpcConfig="AddTaskMacro=$ALICE_PHYSICS/PWGPP/CalibMacros/CPass0/AddTaskTPCCalib.C AddTaskArgs=\"TPCCalib:CalibTimeDrift\" WriteAnalysisToFile=0 -pushback-period=1000")
 {
-  //load the libraries needed by the calib code
-  gROOT->Macro("$ALICE_PHYSICS/PWGPP/CalibMacros/CPass0/LoadLibraries.C");
   // set up HLT system to enable configuration registration
   AliHLTSystem* pHLT=AliHLTPluginBase::GetInstance();
 
-  //pHLT->LoadComponentLibraries("libANALYSIS.so");  
-  //pHLT->LoadComponentLibraries("libANALYSISalice.so");  
+  AliHLTConfiguration calibTPC("calibTPC" , "HLTAnalysisManagerComponent" , parent , tpcConfig);
+  AliHLTConfiguration vzeroBG("vzeroBG" , "HLTAnalysisManagerComponent" , parent , "AddTaskMacro=$ALICE_PHYSICS/PWGPP/BeamGasMonitoring/macros/AddTaskBGMonitorQA.C AddTaskArgs= -pushback-period=1000");
 
-  /*pHLT->LoadComponentLibraries("libESD.so");
-  pHLT->LoadComponentLibraries("libSTEER.so");  
-  pHLT->LoadComponentLibraries("libSTEERBase.so");  
-  pHLT->LoadComponentLibraries("libAOD.so");  
-  pHLT->LoadComponentLibraries("libANALYSIS.so");  
-  pHLT->LoadComponentLibraries("libANALYSISalice.so");  
+  AliHLTConfiguration rootWriter("RootWriterTPCcalib", "ROOTFileWriter", "vzeroBG calibTPC", "-directory hltoutput -datafile hltoutput.root");
 
-  pHLT->LoadComponentLibraries("libHLTbase.so");
-  pHLT->LoadComponentLibraries("libAliHLTUtil.so");
-  pHLT->LoadComponentLibraries("libAliHLTGlobal.so");*/
-
-  /*pHLT->LoadComponentLibraries("libSTAT.so");
-  pHLT->LoadComponentLibraries("libANALYSISalice.so");
-  pHLT->LoadComponentLibraries("libANALYSIScalib.so");
-  //
-  // detector libraries
-  //
-  pHLT->LoadComponentLibraries("libTPCcalib.so");
-  pHLT->LoadComponentLibraries("libTRDcalib.so");
-  pHLT->LoadComponentLibraries("libT0calib.so");
-  pHLT->LoadComponentLibraries("libTOFcalib.so");
-  //
-  // PWGPP libraries
-  //
-  pHLT->LoadComponentLibraries("libANALYSISalice.so");
-  pHLT->LoadComponentLibraries("libANALYSIScalib.so");
-  pHLT->LoadComponentLibraries("libTENDER.so");
-  pHLT->LoadComponentLibraries("libPWGPP.so");*/
-
-  /*
-    pHLT->LoadComponentLibraries("libAliHLTMUON.so");  
-    pHLT->LoadComponentLibraries("libAliHLTTPC.so");  
-    pHLT->LoadComponentLibraries("libAliHLTTRD.so");  
-  */
-
-
-  // writer configuration
-  AliHLTConfiguration calib("calib" , "HLTAnalysisManagerComponent" , parent , tpcConfig);
-
-  TString writerArg(Form("-directory testDir -datafile test.root"));
-
-  // -- The RootFileWriter 
-  AliHLTConfiguration rootWriter("RootWriter", "ROOTFileWriter", "calib", writerArg.Data() );
-
-  //pHLT->BuildTaskList("RootWriter");
-  //pHLT->PrintTaskList();
 }
