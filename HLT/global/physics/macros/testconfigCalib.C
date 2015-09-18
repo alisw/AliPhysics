8e53f2345015fc4265f654c62f8bb21fd2d0504f
mkrzewic <mikolaj.krzewicki@cern.ch>
2015-09-18 17:44:17 +0200

    can use only one component at a time in emulation

--- HLT/global/physics/macros/testconfigCalib.C
+++ HLT/global/physics/macros/testconfigCalib.C
@@ -1,11 +1,11 @@
-void testconfigCalib(const char* parent = "GLOBAL-flat-esd-converter", const char* tpcConfig="AddTaskMacro=$ALICE_PHYSICS/PWGPP/CalibMacros/CPass0/AddTaskTPCCalib.C AddTaskArgs=\"TPCCalib:CalibTimeDrift\" WriteAnalysisToFile=0 -pushback-period=1000")
+void testconfigCalib(const char* parent = "GLOBAL-flat-esd-converter", const char* tpcConfig="AddTaskMacro=$ALICE_PHYSICS/PWGPP/CalibMacros/CPass0/AddTaskTPCCalib.C(\"TPCCalib:CalibTimeDrift\") WriteAnalysisToFile=0 -pushback-period=1000")
 {
   // set up HLT system to enable configuration registration
   AliHLTSystem* pHLT=AliHLTPluginBase::GetInstance();
 
-  AliHLTConfiguration calibTPC("calibTPC" , "HLTAnalysisManagerComponent" , parent , tpcConfig);
-  AliHLTConfiguration vzeroBG("vzeroBG" , "HLTAnalysisManagerComponent" , parent , "AddTaskMacro=$ALICE_PHYSICS/PWGPP/BeamGasMonitoring/macros/AddTaskBGMonitorQA.C AddTaskArgs= -pushback-period=1000");
+  AliHLTConfiguration calibTPC("calib" , "HLTAnalysisManagerComponent" , parent , tpcConfig);
+  //AliHLTConfiguration vzeroBG("calib" , "HLTAnalysisManagerComponent" , parent , "AddTaskMacro=$ALICE_PHYSICS/PWGPP/BeamGasMonitoring/macros/AddTaskBGMonitorQA.C AddTaskArgs= -pushback-period=1000");
 
-  AliHLTConfiguration rootWriter("RootWriterTPCcalib", "ROOTFileWriter", "vzeroBG calibTPC", "-directory hltoutput -datafile hltoutput.root");
+  AliHLTConfiguration rootWriter("RootWriterTPCcalib", "ROOTFileWriter", "calib", "-directory hltoutput -datafile hltoutput.root");
 
 }
