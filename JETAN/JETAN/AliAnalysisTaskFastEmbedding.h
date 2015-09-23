98414b75cfef95ca95c259408d1cbb86f445acd6
azimmerm <Alice.Zimmermann@cern.ch>
2015-09-23 16:11:26 +0200

    pdg code checks instead of PYTHIA V0 cuts

--- JETAN/JETAN/AliAnalysisTaskFastEmbedding.h
+++ JETAN/JETAN/AliAnalysisTaskFastEmbedding.h
@@ -95,8 +95,6 @@ public:
 
    //AZ
   
-  
- 
    virtual void SetK0Type(Int_t i){ fK0Type = i; }
    Bool_t IsK0InvMass(Double_t mass) const; //La and ALa mass check
    Bool_t IsLaInvMass(Double_t mass) const; //La and ALa mass check
@@ -105,29 +103,10 @@ public:
    virtual void   SetFFRadius(Float_t r = 0.4) { fFFRadius = r; }
 
    Float_t  GetFFRadius() const { return fFFRadius; }
-   Bool_t AcceptBetheBloch(AliAODv0 *v0, AliPIDResponse *PIDResponse, Int_t particletype); //don't use this method for MC Analysis
-   void CalculateInvMass(AliAODv0* v0vtx, Int_t particletype, Double_t& invM, Double_t& trackPt);
    Bool_t DaughterTrackCheck(AliAODv0* v0, Int_t& nnum, Int_t& pnum);
    Bool_t ApplyV0Cuts(AliAODv0* v0, const Int_t type, const Int_t particletype, AliAODVertex* primVertex, AliAODEvent* aod);
    void GetTracksInCone(TList* inputlist, TList* outputlist, const AliAODJet* jet, const Double_t radius, Double_t& sumPt);
-   //cut Setter
 
-  void SetCuttrackPosEta(Double_t posEta){fCutPostrackEta=posEta; Printf("AliAnalysisTaskFastEmbedding:: SetCuttrackPosEta %f",posEta);}
-  void SetCuttrackNegEta(Double_t negEta){fCutNegtrackEta=negEta; Printf("AliAnalysisTaskFastEmbedding:: SetCuttrackNegEta %f",negEta);}
-  void SetCutV0Eta(Double_t v0Eta){fCutEta=v0Eta; Printf("AliAnalysisTaskFastEmbedding:: SetCutV0Eta %f",v0Eta);}
-  void SetCosOfPointingAngle(Double_t cospointAng){fCutV0cosPointAngle=cospointAng; Printf("AliAnalysisTaskFastEmbedding:: SetCosOfPointingAngle %f",cospointAng);}
-  void SetAcceptKinkDaughters(Bool_t isKinkDaughtersAccepted){fKinkDaughters=isKinkDaughtersAccepted; Printf("AliAnalysisTaskFastEmbedding:: SetAcceptKinkDaughters %i", isKinkDaughtersAccepted);}
-  void SetRequireTPCRefit(Bool_t isTPCRefit){fRequireTPCRefit=isTPCRefit; Printf("AliAnalysisTaskFastEmbedding:: SetRequireTPCRefit %i", isTPCRefit);}
-  void SetCutArmenteros(Double_t armenteros){fCutArmenteros=armenteros; Printf("AliAnalysisTaskFastEmbedding:: SetCutArmenteros %f", armenteros);}
-  void SetCutV0DecayMin(Double_t decayMin){fCutV0DecayMin=decayMin; Printf("AliAnalysisTaskFastEmbedding:: SetCutDecayMin %f", decayMin);}
-  void SetCutV0DecayMax(Double_t decayMax){fCutV0DecayMax=decayMax; Printf("AliAnalysisTaskFastEmbedding:: SetCutDecayMax %f", decayMax);}
-  void SetCutV0totMom(Double_t v0totMom){fCutV0totMom=v0totMom; Printf("AliAnalysisTaskFastEmbedding:: SetCutV0totMom %f", v0totMom);}
-  void SetCutDcaV0Daughters(Double_t dcav0daughters){fCutDcaV0Daughters=dcav0daughters; Printf("AliAnalysisTaskFastEmbedding:: SetCutDcaV0Daughters %f", dcav0daughters);}
-  void SetCutDcaPosToPrimVertex(Double_t dcaPosToPrimVertex){fCutDcaPosToPrimVertex=dcaPosToPrimVertex; Printf("AliAnalysisTaskFastEmbedding:: SetCutDcaPosToPrimVertex %f", dcaPosToPrimVertex);}
-  void SetCutDcaNegToPrimVertex(Double_t dcaNegToPrimVertex){fCutDcaNegToPrimVertex=dcaNegToPrimVertex; Printf("AliAnalysisTaskFastEmbedding:: SetCutDcaNegToPrimVertex %f", dcaNegToPrimVertex);}
-  void SetCutV0RadiusMin(Double_t v0RadiusMin){fCutV0RadiusMin=v0RadiusMin; Printf("AliAnalysisTaskFastEmbedding:: SetCutV0RadiusMin %f", v0RadiusMin);}
-  void SetCutV0RadiusMax(Double_t v0RadiusMax){fCutV0RadiusMax=v0RadiusMax; Printf("AliAnalysisTaskFastEmbedding:: SetCutV0RadiusMax %f", v0RadiusMax);}
-  void SetCutBetheBloch(Double_t cutBetheBloch){fCutBetheBloch=cutBetheBloch; Printf("AliAnalysisTaskFastEmbedding:: SetCutBetheBloch %f", cutBetheBloch);}
 
    //
  
@@ -159,26 +138,10 @@ private:
    AliAODMCHeader *mcHeader;    //! mc header
    Double_t fFFRadius;          // jet cone size
 
-  Double_t fCutPostrackEta;
-  Double_t fCutNegtrackEta;
-  Double_t fCutEta;
-  Double_t fCutV0cosPointAngle;
-  Bool_t   fKinkDaughters;
-  Bool_t   fRequireTPCRefit;
-  Double_t fCutArmenteros; 
-  Double_t fCutV0DecayMin;
-  Double_t fCutV0DecayMax;
-  Double_t fCutV0totMom;
-  Double_t fCutDcaV0Daughters;
-  Double_t fCutDcaPosToPrimVertex;
-  Double_t fCutDcaNegToPrimVertex;
-  Double_t fCutV0RadiusMin;
-  Double_t fCutV0RadiusMax;
-  Double_t fCutBetheBloch;
+
   Int_t fK0Type;      
   Int_t fLaType;      
   Int_t fALaType;   
-
 
   TList* fListK0s;
   TList* fListLa;
@@ -298,24 +261,7 @@ private:
    TH1F  *fh1MCTrackN;        //! nb. of MC tracks
    TH1I  *fh1AODfile;         //! used AOD files from AODPathArray
    TH2I  *fh2AODevent;        //! selected events in AODs
-
-   //AZ:
-   TH1F* fh1trackPosEta;
-   TH1F* fh1trackNegEta;
-   TH1F* fh1V0Eta;                    
-   // TH1F* fh1V0totMom;                 
-   TH1F* fh1CosPointAngle;            
-   TH1F* fh1DecayLengthV0;            
-   TH2F* fh2ProperLifetimeK0sVsPtBeforeCut;
-   TH2F* fh2ProperLifetimeK0sVsPtAfterCut;
-   TH1F* fh1V0Radius;                 
-   TH1F* fh1DcaV0Daughters;           
-   TH1F* fh1DcaPosToPrimVertex;       
-   TH1F* fh1DcaNegToPrimVertex;        
-   TH2F* fh2ArmenterosBeforeCuts;     
-   TH2F* fh2ArmenterosAfterCuts;      
-   TH2F* fh2BBLaPos;                  
-   TH2F* fh2BBLaNeg;      
+     
 
    Int_t GetJobID();    // get job id (sub-job id on the GRID)
    Int_t SelectAODfile();
