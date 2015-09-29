f6278a434f006acd5cf3acc97889887d78917b49
fbellini <fbellini@cern.ch>
2015-09-29 12:13:48 +0200

    Added possibility to set a custom cut on the vertex z position in the event selection utilities.

--- PWGLF/RESONANCES/AliRsnCutEventUtils.h
+++ PWGLF/RESONANCES/AliRsnCutEventUtils.h
@@ -24,7 +24,7 @@ class AliRsnCutEventUtils : public AliRsnCut {
   void           SetRemovePileUppA2013(Bool_t doit = kTRUE) {fCheckPileUppA2013 = doit;}
   void           SetRemoveFirstEvtInChunk(Bool_t doit = kTRUE) {fIsRmFirstEvInChunck = doit;}
   void           SetUseMVPlpSelection(Bool_t useMVPlpSelection = kFALSE) { fUseMVPlpSelection = useMVPlpSelection;}
-  void           SetUseVertexSelection2013pA(Bool_t zvtxpA2013 = kTRUE)   {fUseVertexSelection2013pA = zvtxpA2013;}
+  void           SetUseVertexSelection2013pA(Bool_t vtxpA2013 = kTRUE, Double_t maxVtxZ = 10.0) {fUseVertexSelection2013pA = vtxpA2013; fMaxVtxZ = maxVtxZ;}
   Bool_t         IsSelected(TObject *object);
   AliAnalysisUtils* GetAnalysisUtils() { return fUtils; }
   void           SetAnalysisUtils(AliAnalysisUtils* utils){ fUtils = utils; }
@@ -39,10 +39,11 @@ class AliRsnCutEventUtils : public AliRsnCut {
   Int_t               fMinPlpContribMV; // min. n. of MV pile-up contributors
   Int_t               fMinPlpContribSPD; // min. n. of pile-up contributors from SPD
   Bool_t              fUseVertexSelection2013pA;// check and reject vertex of events for pA2013
+  Double_t            fMaxVtxZ;//max selected z_vtx
  
   AliAnalysisUtils  * fUtils; //pointer to the AliAnalysisUtils object
 
-  ClassDef(AliRsnCutEventUtils, 2)
+  ClassDef(AliRsnCutEventUtils, 3)
     
     };
 
