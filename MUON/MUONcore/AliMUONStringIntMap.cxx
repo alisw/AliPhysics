e1a566c1816ac28de48b714ce69d33cd88f05cb4
eulisse <giulio.eulisse@cern.ch>
2015-10-05 15:32:11 +0200

    Reduce memory churn in AliMUONStringIntMap::Get
    As seen in:
    
    https://aliigprof.cern.ch/#/1691/ppbench/igprof.ppbench_REC_MEMORY.sql/rank/65

--- MUON/MUONcore/AliMUONStringIntMap.cxx
+++ MUON/MUONcore/AliMUONStringIntMap.cxx
@@ -121,7 +121,7 @@ Int_t  AliMUONStringIntMap::Get(const TString& first) const
 /// Find the element with specified key (first)
   
   for (Int_t i=0; i<fNofItems; i++) {
-    if ( ((TObjString*)fFirstArray.At(i))->GetString() == first )
+    if ( ((TObjString*)fFirstArray.At(i))->String() == first )
       return fSecondArray.At(i);
   }
   
