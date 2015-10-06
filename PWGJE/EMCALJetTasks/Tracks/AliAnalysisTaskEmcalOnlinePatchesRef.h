d227b1693474ed9c478f4a0ab041a3e37f734ee2
mfasel <markus.fasel@cern.ch>
2015-10-06 17:10:45 +0200

    Bugfix + correlation of patches with event selection

--- PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalOnlinePatchesRef.h
+++ PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalOnlinePatchesRef.h
@@ -31,7 +31,7 @@ private:
   AliAnalysisTaskEmcalOnlinePatchesRef(const AliAnalysisTaskEmcalOnlinePatchesRef &);
   AliAnalysisTaskEmcalOnlinePatchesRef &operator=(const AliAnalysisTaskEmcalOnlinePatchesRef &);
 
-  void FillTriggerPatchHistos(const char *patchtype, const AliEmcalTriggerPatchInfo * const recpatch, Int_t supermodule, Int_t sector);
+  void FillTriggerPatchHistos(const char *patchtype, const AliEmcalTriggerPatchInfo * const recpatch, Int_t supermodule, Int_t sector, Bool_t evsel);
 
   ClassDef(AliAnalysisTaskEmcalOnlinePatchesRef, 1);
 };
