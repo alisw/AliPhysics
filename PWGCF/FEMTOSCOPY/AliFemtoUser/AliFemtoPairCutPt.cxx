b75b236a59bfc0bfdab8270835d43648145e21f8
akubera <andrew.kubera@cern.ch>
2015-10-07 23:34:18 -0400

    PWGCF: AliFemtoPairCutPt - Doxygen & formatting update

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoPairCutPt.cxx
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoPairCutPt.cxx
@@ -1,11 +1,6 @@
-/////////////////////////////////////////////////////////////////////////////////////
-//                                                                                 //
-// AliFemtoPairCutPt - a pair cut which checks if the sum of the transverse        //
-// momenta of two particles fit within given range 		                   //
-// Authors: Malgorzata Janik, Warsaw University of Technology, majanik@cern.ch     //
-//          Lukasz Graczykowski, Warsaw University of Technology, lgraczyk@cern.ch //
-//  	       				                                           //
-/////////////////////////////////////////////////////////////////////////////////////
+///
+/// \file AliFemtoPairCutPt.cxx
+///
 
 #include "AliFemtoPairCutPt.h"
 #include <string>
@@ -13,7 +8,9 @@
 #include <TMath.h>
 
 #ifdef __ROOT__
-ClassImp(AliFemtoPairCutPt)
+  /// \cond CLASSIMP
+  ClassImp(AliFemtoPairCutPt);
+  /// \endcond
 #endif
 
 //__________________
@@ -23,8 +20,7 @@ AliFemtoPairCutPt::AliFemtoPairCutPt():
   fSumPtMax(10000),
   fNPairsFailed(0),
   fNPairsPassed(0)
-{
-
+{ /* no-op */
 }
 //__________________
 AliFemtoPairCutPt::AliFemtoPairCutPt(double lo, double hi):
@@ -33,79 +29,73 @@ AliFemtoPairCutPt::AliFemtoPairCutPt(double lo, double hi):
   fSumPtMax(hi),
   fNPairsFailed(0),
   fNPairsPassed(0)
-{
-  fSumPtMin=lo;
-  fSumPtMax=hi;
+{ /* no-op */
 }
 //__________________
-AliFemtoPairCutPt::AliFemtoPairCutPt(const AliFemtoPairCutPt& c) : 
+AliFemtoPairCutPt::AliFemtoPairCutPt(const AliFemtoPairCutPt& c):
   AliFemtoPairCut(c),
-  fSumPtMin(0),
-  fSumPtMax(0),
+  fSumPtMin(c.fSumPtMin),
+  fSumPtMax(c.fSumPtMax),
   fNPairsFailed(0),
   fNPairsPassed(0)
-{ 
-  fSumPtMin = c.fSumPtMin;
-  fSumPtMax = c.fSumPtMax;
+{ /* no-op */
 }
 AliFemtoPairCutPt& AliFemtoPairCutPt::operator=(const AliFemtoPairCutPt& c)
 {
   if (this != &c) {
+    AliFemtoPairCut::operator=(c);
     fSumPtMin = c.fSumPtMin;
     fSumPtMax = c.fSumPtMax;
   }
 
   return *this;
-
 }
 
 //__________________
-AliFemtoPairCutPt::~AliFemtoPairCutPt(){
-  /* no-op */
+AliFemtoPairCutPt::~AliFemtoPairCutPt()
+{ /* no-op */
 }
 //__________________
-bool AliFemtoPairCutPt::Pass(const AliFemtoPair* pair){
+bool AliFemtoPairCutPt::Pass(const AliFemtoPair* pair)
+{
+  const double pt1 = pair->Track1()->Track()->Pt(),
+               pt2 = pair->Track2()->Track()->Pt();
 
-  bool temp = true;
+  const double pt_sum = pt1 + pt2;
 
-  double pt1 = pair->Track1()->Track()->Pt();
-  double pt2 = pair->Track2()->Track()->Pt();
+  // passes if pt_sum is between fSumPtMin and fSumPtMax
+  const bool temp = (fSumPtMin <= pt_sum && pt_sum <= fSumPtMax);
 
-  double pt_sum = pt1 + pt2;
-
-  if(pt_sum >= fSumPtMin && pt_sum <= fSumPtMax)
-    temp = true;
-  else
-    temp = false;
-
-  if(temp) 
+  if (temp) {
     fNPairsPassed++;
-  else fNPairsFailed++;
-
+  } else {
+    fNPairsFailed++;
+  }
 
   return temp;
-  
 }
 //__________________
-AliFemtoString AliFemtoPairCutPt::Report(){
+AliFemtoString AliFemtoPairCutPt::Report()
+{
   // Prepare a report from the execution
-  string stemp = "AliFemtoPairCutPt Pair Cut\n";  
-  char ctemp[100];
-  stemp += ctemp;
-  snprintf(ctemp,100,"Number of pairs which passed:\t%ld  Number which failed:\t%ld\n",(long int) fNPairsPassed,(long int) fNPairsFailed);
-  stemp += ctemp;
-  AliFemtoString returnThis = stemp;
-  return returnThis;}
+  TString report("AliFemtoPairCutPt Pair Cut\n");
+  report += TString::Format("Number of pairs which passed:\t%ld  Number which failed:\t%ld\n", (long int)fNPairsPassed, (long int)fNPairsFailed);
+
+  return AliFemtoString(report);
+}
 //__________________
 
 TList *AliFemtoPairCutPt::ListSettings()
 {
   // return a list of settings in a writable form
   TList *tListSetttings = new TList();
-  char buf[200];
-  snprintf(buf, 200, "AliFemtoPairCutPt.sumptmin=%f", fSumPtMin);
-  snprintf(buf, 200, "AliFemtoPairCutPr.sumptmax=%f", fSumPtMax);
-  tListSetttings->AddLast(new TObjString(buf));
+
+  tListSetttings->Add(new TObjString(
+    TString::Format("AliFemtoPairCutPt.sumptmin=%f", fSumPtMin)
+  ));
+  tListSetttings->Add(new TObjString(
+    TString::Format("AliFemtoPairCutPr.sumptmax=%f", fSumPtMax)
+  ));
 
   return tListSetttings;
 }
@@ -115,9 +105,7 @@ void AliFemtoPairCutPt::SetMinSumPt(Double_t sumptmin)
   fSumPtMin = sumptmin;
 }
 
- 
 void AliFemtoPairCutPt::SetMaxSumPt(Double_t sumptmax)
 {
   fSumPtMax = sumptmax;
 }
-
