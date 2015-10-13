5a391c39e70949a6db23c743e14082b69e5cb34c
mfasel <markus.fasel@cern.ch>
2015-10-13 11:49:34 +0200

    Prepare EMCAL trigger maker for handling of DCAL triggers
    In order to be able to handle patches from DCAL as well,
    the data container with ADC and amplitudes need to be able
    to handle variable size depending on whether DCAL was available
    or not. Trigger data are no longer cached in a hard coded
    array but in a container with flexible size, called
    AliEmcalTriggerDataGrid. In order to allow for different data
    content, the new class is made a template class which supports
    primitive number data types. The container is allocated in
    ExecOnce. In contrast to the previous implementation the data
    grid is made boundary-safe throwing an OutOfBoundsException
    when inexisting rows or cols are accessed. Furthermore it can
    be dynamically increased, decreasing is not possible.

--- PWG/EMCAL/AliEmcalTriggerMaker.h
+++ PWG/EMCAL/AliEmcalTriggerMaker.h
@@ -32,6 +32,8 @@ class AliAODCaloTrigger;
 class AliVVZERO;
 class THistManager;
 
+template<class T> class AliEmcalTriggerDataGrid;
+
 #include <TSortedList.h>
 #include "AliLog.h"
 #include "AliEmcalTriggerBitConfig.h"
@@ -186,6 +188,7 @@ class AliEmcalTriggerMaker : public AliAnalysisTaskEmcal {
   void SetTriggerThresholdJetHigh  ( Int_t a, Int_t b, Int_t c ) { fThresholdConstants[0][0] = a; fThresholdConstants[0][1] = b; fThresholdConstants[0][2] = c; }
   void SetTriggerThresholdGammaLow ( Int_t a, Int_t b, Int_t c ) { fThresholdConstants[3][0] = a; fThresholdConstants[3][1] = b; fThresholdConstants[3][2] = c; }
   void SetTriggerThresholdGammaHigh( Int_t a, Int_t b, Int_t c ) { fThresholdConstants[3][0] = a; fThresholdConstants[1][1] = b; fThresholdConstants[1][2] = c; }
+  void SetDebugLevel(Int_t debugLevel) { fDebugLevel = debugLevel; }
   void SetV0InName(const char *name) { fV0InName      = name; }
   void AddHotFastor(int col, int row) { fBadChannels.AddChannel(col, row); }
 
@@ -199,56 +202,45 @@ class AliEmcalTriggerMaker : public AliAnalysisTaskEmcal {
    */
   void SetRejectOffAcceptancePatches(Bool_t doReject = kTRUE) { fRejectOffAcceptancePatches = doReject; }
 
-  Bool_t IsEJE(Int_t tBits) const { if( tBits & ( 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetJetLowBit()) | 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetJetHighBit()) | 1 << (fTriggerBitConfig->GetJetLowBit()) | 1 << (fTriggerBitConfig->GetJetHighBit()) )) return kTRUE; else return kFALSE; }
-  Bool_t IsEGA(Int_t tBits) const { if( tBits & ( 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetGammaLowBit()) | 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetGammaHighBit()) | 1 << (fTriggerBitConfig->GetGammaLowBit()) | 1 << (fTriggerBitConfig->GetGammaHighBit()) )) return kTRUE; else return kFALSE; }
-  Bool_t IsLevel0(Int_t tBits) const { if( tBits & (1 << (fTriggerBitConfig->GetLevel0Bit() + fTriggerBitConfig->GetLevel0Bit()) | (1 << fTriggerBitConfig->GetLevel0Bit()))) return kTRUE; return kFALSE; }
+  inline Bool_t IsEJE(Int_t tBits) const;
+  inline Bool_t IsEGA(Int_t tBits) const;
+  inline Bool_t IsLevel0(Int_t tBits) const;
 
  protected:
-  enum{
-	  kPatchCols = 48,
-	  kPatchRows = 64
-  };
+  static const TString                      fgkTriggerTypeNames[5];       ///< Histogram name tags
+  static const int                          kColsEta;                     ///< Number of columns in eta direction
 
-  void                       UserCreateOutputObjects();
-  void                       ExecOnce();
-  Bool_t                     Run();
-  void                       RunSimpleOfflineTrigger();
-  Bool_t                     NextTrigger( Bool_t &isOfflineSimple );
-  AliEmcalTriggerPatchInfo*  ProcessPatch(TriggerMakerTriggerType_t type, Bool_t isOfflineSimple);
-  Bool_t 					           CheckForL0(const AliVCaloTrigger &trg) const;
+  void                                      UserCreateOutputObjects();
+  void                                      ExecOnce();
+  Bool_t                                    Run();
+  void                                      RunSimpleOfflineTrigger();
+  Bool_t                                    NextTrigger( Bool_t &isOfflineSimple );
+  AliEmcalTriggerPatchInfo*                 ProcessPatch(TriggerMakerTriggerType_t type, Bool_t isOfflineSimple);
+  Bool_t 					                          CheckForL0(const AliVCaloTrigger &trg) const;
 
-  AliEmcalTriggerChannelContainer   fBadChannels;       ///< Container of bad channels
-  TString                    fCaloTriggersOutName;      ///< name of output track array
-  TString                    fCaloTriggerSetupOutName;  ///< name of output track array
-  TString                    fV0InName;                 ///< name of output track array
-  TriggerMakerBitConfig_t    fUseTriggerBitConfig;      ///< type of trigger config
-  Int_t                      fThresholdConstants[4][3]; ///< simple offline trigger thresholds constants
-  const AliEmcalTriggerBitConfig  *fTriggerBitConfig;   ///< Trigger bit configuration, aliroot-dependent
-  /// trigger array out
-  TClonesArray              *fCaloTriggersOut;          //!
-  /// trigger setup
-  AliEmcalTriggerSetupInfo  *fCaloTriggerSetupOut;      //!
-  /// simple offline trigger
-  AliAODCaloTrigger         *fSimpleOfflineTriggers;    //!
-  /// V0 object
-  AliVVZERO                 *fV0;                       //!
-  /// TRU Amplitudes (for L0)
-  Float_t                    fPatchAmplitudes[kPatchCols][kPatchRows];  //!
-  /// patch map for simple offline trigger
-  Double_t                   fPatchADCSimple[kPatchCols][kPatchRows];   //!
-  /// ADC values map
-  Int_t                      fPatchADC[kPatchCols][kPatchRows];         //!
-  /// Map needed to store the level0 times
-  Char_t                     fLevel0TimeMap[kPatchCols][kPatchRows];    //!
-  /// trigger counter
-  Int_t                      fITrigger;                 //!
-  Bool_t                     fRunTriggerType[5];        ///< Run patch maker for a given trigger type
-  Bool_t                     fDoQA;                     ///< Fill QA histograms
-  Bool_t                     fRejectOffAcceptancePatches;     ///< Switch for rejection of patches outside the acceptance
-  /// Histograms for QA
-  THistManager              *fQAHistos;                 //!
-  /// Histogram name tags
-  static const TString       fgkTriggerTypeNames[5];    //!
+  AliEmcalTriggerChannelContainer           fBadChannels;                 ///< Container of bad channels
+  TString                                   fCaloTriggersOutName;         ///< name of output track array
+  TString                                   fCaloTriggerSetupOutName;     ///< name of output track array
+  TString                                   fV0InName;                    ///< name of output track array
+  TriggerMakerBitConfig_t                   fUseTriggerBitConfig;         ///< type of trigger config
+  Int_t                                     fThresholdConstants[4][3];    ///< simple offline trigger thresholds constants
+  const AliEmcalTriggerBitConfig            *fTriggerBitConfig;           ///< Trigger bit configuration, aliroot-dependent
+  TClonesArray                              *fCaloTriggersOut;            //!<! trigger array out
+  AliEmcalTriggerSetupInfo                  *fCaloTriggerSetupOut;        //!<! trigger setup
+  AliAODCaloTrigger                         *fSimpleOfflineTriggers;      //!<! simple offline trigger
+  AliVVZERO                                 *fV0;                         //!<! V0 object
+  AliEmcalTriggerDataGrid<float>          *fPatchAmplitudes;            //!<! TRU Amplitudes (for L0)
+  AliEmcalTriggerDataGrid<double>         *fPatchADCSimple;             //!<! patch map for simple offline trigger
+  AliEmcalTriggerDataGrid<int>            *fPatchADC;                   //!<! ADC values map
+  AliEmcalTriggerDataGrid<char>           *fLevel0TimeMap;              //!<! Map needed to store the level0 times
+  Int_t                                     fITrigger;                    //!<! trigger counter
+  Bool_t                                    fRunTriggerType[5];           ///< Run patch maker for a given trigger type
+  Bool_t                                    fDoQA;                        ///< Fill QA histograms
+  Bool_t                                    fRejectOffAcceptancePatches;  ///< Switch for rejection of patches outside the acceptance
+  THistManager                              *fQAHistos;                   //!<! Histograms for QA
+
+  Int_t                                     fDebugLevel;                  ///< Debug lebel;
+
  private:
   AliEmcalTriggerMaker(const AliEmcalTriggerMaker&);            // not implemented
   AliEmcalTriggerMaker &operator=(const AliEmcalTriggerMaker&); // not implemented
@@ -288,4 +280,40 @@ Int_t AliEmcalTriggerMaker::AliEmcalTriggerChannelPosition::Compare(const TObjec
   else if(fCol < refpos->fCol) return -1;
   else return 1;
 }
+
+/**
+ * Check whehter trigger is jet patch trigger according to trigger bits
+ * @param tBits Trigger bits of the fastor
+ * @return True if fastor belongs to a jet patch trigger, false otherwise
+ */
+Bool_t AliEmcalTriggerMaker::IsEJE(Int_t tBits) const {
+  if( tBits & ( 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetJetLowBit()) | 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetJetHighBit()) | 1 << (fTriggerBitConfig->GetJetLowBit()) | 1 << (fTriggerBitConfig->GetJetHighBit()) ))
+    return kTRUE;
+  else
+    return kFALSE;
+}
+
+/**
+ * Check whether trigger is gamma patch trigger according to trigger bits
+ * @param tBits Trigger bits of the fastor
+ * @return True if fastor belongs to a gamma trigger, false otherwise
+ */
+Bool_t AliEmcalTriggerMaker::IsEGA(Int_t tBits) const {
+  if( tBits & ( 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetGammaLowBit()) | 1 << (fTriggerBitConfig->GetTriggerTypesEnd() + fTriggerBitConfig->GetGammaHighBit()) | 1 << (fTriggerBitConfig->GetGammaLowBit()) | 1 << (fTriggerBitConfig->GetGammaHighBit()) ))
+    return kTRUE;
+  else
+    return kFALSE;
+}
+
+/**
+ * Check whether trigger is level0 trigger according to trigger bits
+ * @param tBits Trigger bits of the fastor
+ * @return True if fastor belongs to a level0 trigger, false otherwise
+ */
+Bool_t AliEmcalTriggerMaker::IsLevel0(Int_t tBits) const {
+  if( tBits & (1 << (fTriggerBitConfig->GetLevel0Bit() + fTriggerBitConfig->GetLevel0Bit()) | (1 << fTriggerBitConfig->GetLevel0Bit())))
+    return kTRUE;
+  return kFALSE;
+}
+
 #endif
