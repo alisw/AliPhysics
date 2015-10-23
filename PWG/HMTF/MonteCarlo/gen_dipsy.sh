7995885159a1f967170a3286055021a4a829d7d9
jklein <jochen.klein@cern.ch>
2015-10-23 14:25:10 +0200

    - update script to run DIPSY

--- PWG/HMTF/MonteCarlo/gen_dipsy.sh
+++ PWG/HMTF/MonteCarlo/gen_dipsy.sh
@@ -8,6 +8,9 @@ fi
 
 echo "running in `pwd`, writing HepMC to $1"
 
+# prepare environment
+export PYTHIA8DATA=$PYTHIA_ROOT/share/Pythia8/xmldoc
+
 # force path to requested output
 sed -e "s/^\([[:space:]]*set HepMCFile:Filename\) .*$/\1 $1/" ${ALICE_PHYSICS}/PWG/HMTF/MonteCarlo/DIPSYpp_HepMC.in > thepeg.in
 # setup DIPSY
