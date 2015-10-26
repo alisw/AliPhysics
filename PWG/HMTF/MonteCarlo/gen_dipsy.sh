91f9d5f13e34120461ef724811f82f9362aa603e
jklein <jochen.klein@cern.ch>
2015-10-26 08:57:00 +0100

    - update script to run DIPSY

--- PWG/HMTF/MonteCarlo/gen_dipsy.sh
+++ PWG/HMTF/MonteCarlo/gen_dipsy.sh
@@ -8,12 +8,10 @@ fi
 
 echo "running in `pwd`, writing HepMC to $1"
 
-# prepare environment
-export PYTHIA8DATA=$PYTHIA_ROOT/share/Pythia8/xmldoc
-
 # force path to requested output
 sed -e "s/^\([[:space:]]*set HepMCFile:Filename\) .*$/\1 $1/" ${ALICE_PHYSICS}/PWG/HMTF/MonteCarlo/DIPSYpp_HepMC.in > thepeg.in
 # setup DIPSY
 setupThePEG -r ${THEPEG_ROOT}/lib/ThePEG/ThePEGDefaults.rpo -I ${THEPEG_ROOT}/share/Ariadne thepeg.in > setup.log 2>&1
 # run DIPSY
-runThePEG DIPSYpp.run -N 2147483647 --seed ${ALIEN_PROC_ID:=0}
+PYTHIA8DATA=$PYTHIA_ROOT/share/Pythia8/xmldoc runThePEG DIPSYpp.run -N 2147483647
+# --seed ${ALIEN_PROC_ID:=0}
