d970eb147f0b30cb612277a4f11dca5d70772075
jbook <jbook@pcikf2.ikf.physik.uni-frankfurt.de>
2015-10-29 10:47:38 +0100

    - clean up and restructure of dielectron package

---
+++ PWGDQ/reducedTree/PWGDQreducedTreeLinkDef.h
@@ -0,0 +1,17 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliReducedEvent+;
+#pragma link C++ class AliReducedEventFriend+;
+#pragma link C++ class AliReducedTrack+;
+#pragma link C++ class AliReducedPair+;
+#pragma link C++ class AliReducedFMD+;
+#pragma link C++ class AliReducedCaloCluster+;
+#pragma link C++ class AliAnalysisTaskReducedTree+;
+
+#endif
