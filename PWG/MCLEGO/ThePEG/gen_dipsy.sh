f26bf25901187fd16a9b1b96dc6628df192a9aea
jklein <jochen.klein@cern.ch>
2015-10-29 11:43:38 +0100

    bump ThePEG version for on-the-fly LEGO train

--- PWG/MCLEGO/ThePEG/gen_dipsy.sh
+++ PWG/MCLEGO/ThePEG/gen_dipsy.sh
@@ -10,7 +10,7 @@ echo "running in `pwd`, writing HepMC to $1"
 
 # prepare environment
 source /cvmfs/alice.cern.ch/etc/login.sh
-eval $(alienv printenv ThePEG::v2015-08-11-3)
+eval $(alienv printenv ThePEG::v2015-08-11-4)
 
 # force path to requested output
 sed -e "s/^\([[:space:]]*set HepMCFile:Filename\) .*$/\1 $1/" ${ALICE_PHYSICS}/PWG/MCLEGO/ThePEG/DIPSYpp_HepMC.in > thepeg.in
@@ -19,4 +19,4 @@ sed -e "s/^\([[:space:]]*set HepMCFile:Filename\) .*$/\1 $1/" ${ALICE_PHYSICS}/P
 setupThePEG -r ${THEPEG_ROOT}/lib/ThePEG/ThePEGDefaults.rpo -I ${THEPEG_ROOT}/share/Ariadne thepeg.in > setup.log 2>&1
 
 # run DIPSY
-PYTHIA8DATA=$PYTHIA_ROOT/share/Pythia8/xmldoc runThePEG DIPSYpp.run -N 2147483647 --seed ${ALIEN_PROC_ID:=0}
+runThePEG DIPSYpp.run -N 2147483647 --seed ${ALIEN_PROC_ID:=0}
