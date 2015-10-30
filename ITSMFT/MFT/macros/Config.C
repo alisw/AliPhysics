b8705ffbc9c64887def491b7d995e49c47383600
bogdan <vulpescu@clermont.in2p3.fr>
2015-10-30 16:18:57 +0100

    Change to the correct pythia6 version in Config.C

--- ITSMFT/MFT/macros/Config.C
+++ ITSMFT/MFT/macros/Config.C
@@ -665,7 +665,7 @@ void LoadLibs() {
     gSystem->Load("libAliPythia6");     // ALICE specific implementations
   } 
   else {
-    gSystem->Load("libpythia6.4.21");   // Pythia 6.4
+    gSystem->Load("libpythia6_4_25");   // Pythia 6.4
     gSystem->Load("libAliPythia6");     // ALICE specific implementations	
   }
   
