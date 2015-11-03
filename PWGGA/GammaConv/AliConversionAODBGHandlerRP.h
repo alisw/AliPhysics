0c5d8e82f5c132b0e31865f24fc7396f91760b78
fbock <friederike.bock@cern.ch>
2015-11-03 15:22:14 +0100

    changed indentation to make it compatible with computing rules

--- PWGGA/GammaConv/AliConversionAODBGHandlerRP.h
+++ PWGGA/GammaConv/AliConversionAODBGHandlerRP.h
@@ -16,45 +16,61 @@ typedef vector<AliGammaConversionMultiplicityVector> AliGammaConversionBGVector;
 
 class AliConversionAODBGHandlerRP: public TObject{
 
-	public:
+  public:
 
-		AliConversionAODBGHandlerRP(Bool_t IsHeavyIon=kFALSE,Bool_t UseChargedTrackMult=kTRUE,Int_t NEvents=10);
-		
-		virtual ~AliConversionAODBGHandlerRP();
+    AliConversionAODBGHandlerRP                     ( Bool_t IsHeavyIon=kFALSE,
+                                                      Bool_t UseChargedTrackMult=kTRUE,
+                                                      Int_t NEvents=10 );
+    
+    virtual ~AliConversionAODBGHandlerRP();
 
-		Int_t GetZBinIndex(Double_t z) const;
-		Int_t GetMultiplicityBinIndex(Int_t mult) const;
-		void Initialize();
-		Bool_t FindBins(TObjArray * const eventGammas,AliVEvent *fInputEvent,Int_t &zbin,Int_t &mbin);
-		Bool_t FindBins(TList * const eventGammas,AliVEvent *fInputEvent,Int_t &zbin,Int_t &mbin);
+    Int_t GetZBinIndex                              ( Double_t z ) const;
+    Int_t GetMultiplicityBinIndex                   ( Int_t mult ) const;
+    void Initialize                                 ();
+    Bool_t FindBins                                 ( TObjArray * const eventGammas, 
+                                                      AliVEvent *fInputEvent, 
+                                                      Int_t &zbin,
+                                                      Int_t &mbin);
+    Bool_t FindBins                                 ( TList * const eventGammas, 
+                                                      AliVEvent *fInputEvent,
+                                                      Int_t &zbin,
+                                                      Int_t &mbin );
 
-	
-		AliGammaConversionPhotonVector* GetBGGoodGammas(TObjArray * const eventGammas,AliVEvent *fInputEvent,Int_t event);
-		AliGammaConversionPhotonVector* GetBGGoodGammas(TList * const eventGammas,AliVEvent *fInputEvent,Int_t event);
-		void AddEvent(TObjArray * const eventGammas,AliVEvent *fInputEvent);
-		void AddEvent(TList * const eventGammas,AliVEvent *fInputEvent);
-		Int_t GetNBGEvents()const {return fNEvents;} // Size of the Pool (20)
-		Int_t GetNBGEvents(TObjArray * const eventGammas,AliVEvent *fInputEvent);
-		Int_t GetNBGEvents(TList * const eventGammas,AliVEvent *fInputEvent);
-		Int_t GetNZBins()const{return fNBinsZ;};
-		Int_t GetNMultiplicityBins()const{return fNBinsMultiplicity;};
+  
+    AliGammaConversionPhotonVector* GetBGGoodGammas ( TObjArray * const eventGammas,
+                                                      AliVEvent *fInputEvent,
+                                                      Int_t event );
+    AliGammaConversionPhotonVector* GetBGGoodGammas ( TList * const eventGammas, 
+                                                      AliVEvent *fInputEvent,
+                                                      Int_t event );
+    void AddEvent                                   ( TObjArray * const eventGammas,
+                                                      AliVEvent *fInputEvent );
+    void AddEvent                                   ( TList * const eventGammas, 
+                                                      AliVEvent *fInputEvent );
+    Int_t GetNBGEvents                              ()const                                         { return fNEvents                             ;} // Size of the Pool (20)
+    Int_t GetNBGEvents                              ( TObjArray * const eventGammas,
+                                                      AliVEvent *fInputEvent );
+    Int_t GetNBGEvents                              ( TList * const eventGammas,
+                                                      AliVEvent *fInputEvent );
+    Int_t GetNZBins                                 ()const                                         { return fNBinsZ                              ;}
+    Int_t GetNMultiplicityBins                      ()const                                         { return fNBinsMultiplicity                   ;}
 
-	private:
-		Bool_t fIsHeavyIon;
-		Bool_t fUseChargedTrackMult;
-		Int_t fNEvents;
-		Int_t **fBGEventCounter; //! bg counter
-		Int_t **fNBGEvents;
-		Int_t fNBinsZ; //n z bins
-		Int_t fNBinsMultiplicity; //n bins multiplicity
-		Double_t *fBinLimitsArrayZ;//! bin limits z array
-		Double_t *fBinLimitsArrayMultiplicity;//! bin limit multiplicity array
-		AliGammaConversionBGVector fBGPool; //background events
+  private:
+    Bool_t                      fIsHeavyIon;                      // flag for heavy ion
+    Bool_t                      fUseChargedTrackMult;             // flag for multiplicity switch
+    Int_t                       fNEvents;                         // number of events
+    Int_t**                     fBGEventCounter;                  //! bg counter
+    Int_t**                     fNBGEvents;                       //n BG events
+    Int_t                       fNBinsZ;                          //n z bins
+    Int_t                       fNBinsMultiplicity;               //n bins multiplicity
+    Double_t*                   fBinLimitsArrayZ;                 //! bin limits z array
+    Double_t*                   fBinLimitsArrayMultiplicity;      //! bin limit multiplicity array
+    AliGammaConversionBGVector  fBGPool;                          //background events
 
-		AliConversionAODBGHandlerRP(AliConversionAODBGHandlerRP &original);
-		AliConversionAODBGHandlerRP &operator=(const AliConversionAODBGHandlerRP &ref);
+    AliConversionAODBGHandlerRP(AliConversionAODBGHandlerRP &original);
+    AliConversionAODBGHandlerRP &operator=(const AliConversionAODBGHandlerRP &ref);
 
-	ClassDef(AliConversionAODBGHandlerRP,0);
+  ClassDef(AliConversionAODBGHandlerRP,0);
 
 };
 #endif
