e8095f8e1a49a44f9fa0b90a520f0e70cfad5560
gconesab <Gustavo.Conesa.Balbastre@cern.ch>
2015-11-05 16:56:43 +0100

    add comment on no need to use the time decalibration arrays in this class; remove double declaration of number of columns and rows in different loops; addapt to doxygen

--- EMCAL/EMCALbase/AliEMCALCalibData.cxx
+++ EMCAL/EMCALbase/AliEMCALCalibData.cxx
@@ -13,46 +13,36 @@
  * provided "as is" without express or implied warranty.                  *
  **************************************************************************/
 
-/* $Id$ */
-
-//_________________________________________________________________________
-///*-- Author: Yves Schutz (SUBATECH)
-//           : Aleksei Pavlinov (WSU); Jun 30, 2006 - ALICE numbering scheme
-//           : Add decalibration and time calibration arrays: Jul 21, 2011 (GCB)
-//           : adapted for DCAL by M.L. Wang CCNU & Subatech Oct-18-2012
-///////////////////////////////////////////////////////////////////////////////
-//                                                                           //
-// class for EMCAL calibration                                               //
-//                                                                           //
-///////////////////////////////////////////////////////////////////////////////
-
-#include <TMath.h>
-
 #include "AliEMCALCalibData.h"
 
-ClassImp(AliEMCALCalibData)
+/// \cond CLASSIMP
+ClassImp(AliEMCALCalibData) ;
+/// \endcond
 
-//__________________________________________________________
+///
+/// Default constructor
+//____________________________________
 AliEMCALCalibData::AliEMCALCalibData() : 
 TNamed(), fADCchannelRef(0)
 {
-  // Default constructor
   Reset();
 }
 
-//________________________________________________________________________
+///
+/// Constructor
+//____________________________________________________
 AliEMCALCalibData::AliEMCALCalibData(const char* name) : 
 TNamed(name,name),fADCchannelRef(0)
 {
-  // Constructor
   Reset();
 }
 
-//______________________________________________________________________
+///
+/// Copy constructor
+//____________________________________________________________________
 AliEMCALCalibData::AliEMCALCalibData(const AliEMCALCalibData& calibda) :
 TNamed(calibda), fADCchannelRef(calibda.fADCchannelRef)
 {
-  // copy constructor
   SetName (calibda.GetName());
   SetTitle(calibda.GetName());
   Reset();
@@ -62,11 +52,8 @@ TNamed(calibda), fADCchannelRef(calibda.fADCchannelRef)
   Int_t nRow  = AliEMCALGeoParams::fgkEMCALRows;    //24
   
   for(Int_t supermodule = 0; supermodule < nSMod; supermodule++)
-  {
-    nCol  = AliEMCALGeoParams::fgkEMCALCols;    //48
-    nRow  = AliEMCALGeoParams::fgkEMCALRows;    //24
-    
-    //Init all SM equally, even the channels known to not exist.
+  {    
+    // Init all SM equally, even the channels known to not exist.
     
     for(Int_t column = 0; column<nCol; column++)
     {
@@ -84,10 +71,11 @@ TNamed(calibda), fADCchannelRef(calibda.fADCchannelRef)
   } // SM
 }
 
-//________________________________________________________________
+///
+/// Assignment operator
+//________________________________________________________________________________
 AliEMCALCalibData &AliEMCALCalibData::operator =(const AliEMCALCalibData& calibda)
 {
-  // assignment operator
   SetName (calibda.GetName());
   SetTitle(calibda.GetName());
   Reset();
@@ -100,10 +88,7 @@ AliEMCALCalibData &AliEMCALCalibData::operator =(const AliEMCALCalibData& calibd
   
   for(Int_t supermodule = 0; supermodule < nSMod; supermodule++)
   {
-    nCol  = AliEMCALGeoParams::fgkEMCALCols;    //48
-    nRow  = AliEMCALGeoParams::fgkEMCALRows;    //24
-
-    //Init all SM equally, even the channels known to not exist.
+    // Init all SM equally, even the channels known to not exist.
     
     for(Int_t column = 0; column<nCol; column++)
     {
@@ -123,11 +108,11 @@ AliEMCALCalibData &AliEMCALCalibData::operator =(const AliEMCALCalibData& calibd
   return *this;
 }
 
+///
+/// Set all pedestals to 0 and all ADC channels widths to 1
 //_____________________________
 void AliEMCALCalibData::Reset()
-{
-  // Set all pedestals to 0 and all ADC channels widths to 1
-  
+{  
   fADCchannelRef = 0.0162;	
   
   Int_t nSMod = AliEMCALGeoParams::fgkEMCALModules; 
@@ -136,16 +121,12 @@ void AliEMCALCalibData::Reset()
 
    for (Int_t supermodule=0; supermodule<nSMod; supermodule++)
   {
-   nCol  = AliEMCALGeoParams::fgkEMCALCols;    //48
-   nRow  = AliEMCALGeoParams::fgkEMCALRows;    //24
-    
     //Init all SM equally, even the channels known to not exist.
 
     for (Int_t column=0; column < nCol; column++)
     {
       for (Int_t row = 0; row < nRow; row++)
       {
-       
         SetADCchannel      (supermodule,column,row, fADCchannelRef);
         SetADCchannelOnline(supermodule,column,row, fADCchannelRef);
         SetADCchannelDecal (supermodule,column,row, 1);
@@ -159,21 +140,18 @@ void AliEMCALCalibData::Reset()
   }	
 }
 
+///
+/// Print tables of pedestals and ADC channels widths
+/// options are: "gain", "ped", "online", "decal", "time", "timdecal", "all"
 //____________________________________________________
 void  AliEMCALCalibData::Print(Option_t *option) const
-{
-  // Print tables of pedestals and ADC channels widths
-  // options are: "gain", "ped", "online", "decal", "time", "timdecal", "all"
-  
+{  
   Int_t nSMod = AliEMCALGeoParams::fgkEMCALModules;
   Int_t nCol  = AliEMCALGeoParams::fgkEMCALCols;    //48
   Int_t nRow  = AliEMCALGeoParams::fgkEMCALRows;    //24
   
   for (Int_t supermodule = 0; supermodule < nSMod; supermodule++)
   {
-    nCol  = AliEMCALGeoParams::fgkEMCALCols;    //48
-    nRow  = AliEMCALGeoParams::fgkEMCALRows;    //24
-
     //Init all SM equally, even the channels known to not exist.
 
     printf("============== Supermodule %d\n",supermodule+1);
@@ -207,131 +185,133 @@ void  AliEMCALCalibData::Print(Option_t *option) const
   }
 }
 
-//________________________________________________________________
+///
+/// Set ADC channel witdth values
+/// All indexes start from 0!
+/// Supermodule, column,raw should follow the ALICE convention:
+/// supermodule 0:11, column 0:47, row 0:23
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//________________________________________________________________________________________
 Float_t AliEMCALCalibData::GetADCchannel(Int_t supermodule, Int_t column, Int_t row) const
-{
-  // Set ADC channel witdth values
-  // All indexes start from 0!
-  // Supermodule, column,raw should follow the ALICE convention:
-  // supermodule 0:11, column 0:47, row 0:23
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+{  
   if(supermodule < 12) return fADCchannel    [supermodule]   [column][row];
   else                 return fADCchannelDCAL[supermodule-12][column][row];
 }
 
-//________________________________________________________________
+///
+/// Set ADC channel witdth values, first online calibration parameter
+/// All indexes start from 0!
+/// Supermodule, column,raw should follow the ALICE convention:
+/// supermodule 0:11, column 0:47, row 0:23
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//______________________________________________________________________________________________
 Float_t AliEMCALCalibData::GetADCchannelOnline(Int_t supermodule, Int_t column, Int_t row) const
-{
-  // Set ADC channel witdth values, first online calibration parameter
-  // All indexes start from 0!
-  // Supermodule, column,raw should follow the ALICE convention:
-  // supermodule 0:11, column 0:47, row 0:23
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+{  
   if(supermodule < 12) return fADCchannelOnline    [supermodule]   [column][row];
   else                 return fADCchannelOnlineDCAL[supermodule-12][column][row];
 }
 
-//________________________________________________________________
+///
+/// Set ADC channel decalibration witdth values
+/// All indexes start from 0!
+/// Supermodule, column,raw should follow the ALICE convention:
+/// supermodule 0:11, column 0:47, row 0:23
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//_____________________________________________________________________________________________
 Float_t AliEMCALCalibData::GetADCchannelDecal(Int_t supermodule, Int_t column, Int_t row) const
 {
-  // Set ADC channel decalibration witdth values
-  // All indexes start from 0!
-  // Supermodule, column,raw should follow the ALICE convention:
-  // supermodule 0:11, column 0:47, row 0:23
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
   if(supermodule < 12) return fADCchannelDecal    [supermodule]   [column][row];
   else                 return fADCchannelDecalDCAL[supermodule-12][column][row];
 }
 
-//________________________________________________________________
+///
+/// Get ADC pedestal values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//_________________________________________________________________________________________
 Float_t AliEMCALCalibData::GetADCpedestal(Int_t supermodule, Int_t column, Int_t row) const
-{
-  // Get ADC pedestal values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+{  
   if(supermodule < 12) return fADCpedestal    [supermodule]   [column][row];
   else                 return fADCpedestalDCAL[supermodule-12][column][row];
 }
 
-//________________________________________________________________
-Float_t AliEMCALCalibData::GetTimeChannel(Int_t supermodule, Int_t column, Int_t row, Int_t bc) const
-{
-  // Set channel time values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
-  if(supermodule < 12) return fTimeChannel    [supermodule]   [column][row][bc];
-  else                 return fTimeChannelDCAL[supermodule-12][column][row][bc];
-}
-
-//________________________________________________________________
-Float_t AliEMCALCalibData::GetTimeChannelDecal(Int_t supermodule, Int_t column, Int_t row) const
-{
-  // Set channel time decalibrated values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
-  if(supermodule < 12) return fTimeChannelDecal    [supermodule]   [column][row];
-  else                 return fTimeChannelDecalDCAL[supermodule-12][column][row];
-}
-
-//________________________________________________________________
+///
+/// Set ADC channel width values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//______________________________________________________________________________________________
 void AliEMCALCalibData::SetADCchannel(Int_t supermodule, Int_t column, Int_t row, Float_t value)
-{ 
-  // Set ADC channel width values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+{   
   if(supermodule < 12) fADCchannel    [supermodule]   [column][row] = value;
   else                 fADCchannelDCAL[supermodule-12][column][row] = value;
 }
 
-//________________________________________________________________
+///
+/// Set ADC channel online width values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//____________________________________________________________________________________________________
 void AliEMCALCalibData::SetADCchannelOnline(Int_t supermodule, Int_t column, Int_t row, Float_t value)
-{
-  // Set ADC channel online width values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+{  
   if(supermodule < 12) fADCchannelOnline    [supermodule]   [column][row] = value;
   else                 fADCchannelOnlineDCAL[supermodule-12][column][row] = value;
 }
 
-//________________________________________________________________
+///
+/// Set ADC channel width values, decalibration
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//___________________________________________________________________________________________________
 void AliEMCALCalibData::SetADCchannelDecal(Int_t supermodule, Int_t column, Int_t row, Float_t value)
 { 
-  // Set ADC channel width values, decalibration
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
   if(supermodule < 12) fADCchannelDecal    [supermodule]   [column][row] = value;
   else                 fADCchannelDecalDCAL[supermodule-12][column][row] = value;
 }
 
-//________________________________________________________________
+///
+/// Set ADC pedestal values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//_______________________________________________________________________________________________
 void AliEMCALCalibData::SetADCpedestal(Int_t supermodule, Int_t column, Int_t row, Float_t value)
-{
-  // Set ADC pedestal values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+{  
   if(supermodule < 12) fADCpedestal    [supermodule]   [column][row] = value;
   else                 fADCpedestalDCAL[supermodule-12][column][row] = value;
 }
 
-//________________________________________________________________
-void AliEMCALCalibData::SetTimeChannel(Int_t supermodule, Int_t column, Int_t row, Int_t bc, Float_t value)
+//*** Do not use the following methods use instead AliEMCALCalibTime:
+
+///
+/// Set channel time values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//___________________________________________________________________________________________________
+Float_t AliEMCALCalibData::GetTimeChannel(Int_t supermodule, Int_t column, Int_t row, Int_t bc) const
+{  
+  if(supermodule < 12) return fTimeChannel    [supermodule]   [column][row][bc];
+  else                 return fTimeChannelDCAL[supermodule-12][column][row][bc];
+}
+
+///
+/// Set channel time decalibrated values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//______________________________________________________________________________________________
+Float_t AliEMCALCalibData::GetTimeChannelDecal(Int_t supermodule, Int_t column, Int_t row) const
 {
-  // Set time per channel and bunch crossing values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
+  if(supermodule < 12) return fTimeChannelDecal    [supermodule]   [column][row];
+  else                 return fTimeChannelDecalDCAL[supermodule-12][column][row];
+}
+
+///
+/// Set time per channel and bunch crossing values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//_________________________________________________________________________________________________________
+void AliEMCALCalibData::SetTimeChannel(Int_t supermodule, Int_t column, Int_t row, Int_t bc, Float_t value)
+{  
   if(supermodule < 12) fTimeChannel    [supermodule]   [column][row][bc] = value;
   else                 fTimeChannelDCAL[supermodule-12][column][row][bc] = value;
 }
 
-//________________________________________________________________
+///
+/// Set decalibrated time per channel values
+/// DCal has its own array, starting from SM 12, index 0, same col-row size
+//____________________________________________________________________________________________________
 void AliEMCALCalibData::SetTimeChannelDecal(Int_t supermodule, Int_t column, Int_t row, Float_t value)
 {
-  // Set decalibrated time per channel values
-  // DCal has its own array, starting from SM 12, index 0, same col-row size
-  
   if(supermodule < 12) fTimeChannelDecal    [supermodule]   [column][row] = value;
   else                 fTimeChannelDecalDCAL[supermodule-12][column][row] = value;
 }
