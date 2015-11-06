d996589cbe3cfa3abe8e11ac9ddaf1bb1123bcee
fbock <friederike.bock@cern.ch>
2015-11-06 15:51:23 +0100

    added multiplicity weighting

--- PWGGA/GammaConv/macros/AddTask_GammaConvV1_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvV1_pp.C
@@ -1,16 +1,20 @@
-void AddTask_GammaConvV1_pp(  Int_t   trainConfig                 = 1,                    // change different set of cuts
-                              Int_t   isMC                        = 0,                    // run MC
-                              Int_t   enableQAMesonTask           = 0,                    // enable meson QA in AliAnalysisTaskGammaConvV1
-                              Int_t   enableQAPhotonTask          = 0,                    // enable photon QA in AliAnalysisTaskGammaConvV1
-                              TString fileNameInputForWeighting 	= "MCSpectraInput.root",// path to file for weigting input
-                              TString cutnumberAODBranch          = "000000006008400001001500000",  // cutnumber for AOD branch
-                              TString periodname                  = "LHC12f1x",           // period name
-                              Bool_t  doWeighting                 = kFALSE,               // enables weighting
-                              Bool_t  enableV0findingEffi         = kFALSE,               // enables V0finding efficiency histograms
-                              Bool_t  enableTriggerMimicking      = kFALSE,               // enable trigger mimicking
-                              Bool_t  enableTriggerOverlapRej     = kFALSE,               // enable trigger overlap rejection
-                              Float_t maxFacPtHard                = 3.,                   // maximum factor between hardest jet and ptHard generated
-                              TString periodNameV0Reader          = ""
+void AddTask_GammaConvV1_pp(  Int_t   trainConfig                     = 1,                    // change different set of cuts
+                              Int_t   isMC                            = 0,                    // run MC
+                              Int_t   enableQAMesonTask               = 0,                    // enable meson QA in AliAnalysisTaskGammaConvV1
+                              Int_t   enableQAPhotonTask              = 0,                    // enable photon QA in AliAnalysisTaskGammaConvV1
+                              TString fileNameInputForPartWeighting   = "MCSpectraInput.root",// path to file for weigting input
+                              TString cutnumberAODBranch              = "000000006008400001001500000",  // cutnumber for AOD branch
+                              TString periodname                      = "LHC12f1x",           // period name
+                              Bool_t  doParticleWeighting             = kFALSE,               // enables weighting
+                              Bool_t  enableV0findingEffi             = kFALSE,               // enables V0finding efficiency histograms
+                              Bool_t  enableTriggerMimicking          = kFALSE,               // enable trigger mimicking
+                              Bool_t  enableTriggerOverlapRej         = kFALSE,               // enable trigger overlap rejection
+                              Float_t maxFacPtHard                    = 3.,                   // maximum factor between hardest jet and ptHard generated
+                              TString periodNameV0Reader              = "",
+                              Bool_t  doMultiplicityWeighting         = kFALSE,                  //
+                              TString fileNameInputForMultWeighing    = "Multiplicity.root",    //
+                              TString periodNameAnchor                = ""
+                              
                             ) {
 
   // ================= Load Librariers =================================
@@ -681,9 +685,25 @@ void AddTask_GammaConvV1_pp(  Int_t   trainConfig                 = 1,          
       mcInputNamePi0 = Form("Pi0_%s%s_%s", mcName.Data(), mcNameAdd.Data(), energy.Data() );
       mcInputNameEta = Form("Eta_%s%s_%s", mcName.Data(), mcNameAdd.Data(), energy.Data() );
     }	
-    //		if (doWeighting) analysisEventCuts[i]->SetUseReweightingWithHistogramFromFile(kFALSE, kFALSE, kFALSE, fileNameInputForWeighting, mcInputNamePi0, mcInputNameEta, "",fitNamePi0,fitNameEta);
-    if (doWeighting) analysisEventCuts[i]->SetUseReweightingWithHistogramFromFile(kTRUE, kTRUE, kFALSE, fileNameInputForWeighting, mcInputNamePi0, mcInputNameEta, "",fitNamePi0,fitNameEta);
+    //		if (doParticleWeighting) analysisEventCuts[i]->SetUseReweightingWithHistogramFromFile(kFALSE, kFALSE, kFALSE, fileNameInputForPartWeighting, mcInputNamePi0, mcInputNameEta, "",fitNamePi0,fitNameEta);
+    if (doParticleWeighting) analysisEventCuts[i]->SetUseReweightingWithHistogramFromFile(kTRUE, kTRUE, kFALSE, fileNameInputForPartWeighting, mcInputNamePi0, mcInputNameEta, "",fitNamePi0,fitNameEta);
 
+    TString dataInputMultHisto  = "";
+    TString mcInputMultHisto    = "";
+    TString triggerString   = eventCutArray[i];
+    triggerString           = triggerString(3,2);
+    if (triggerString.CompareTo("03")==0) 
+      triggerString         = "00";
+
+    dataInputMultHisto      = Form("%s_%s", periodNameAnchor.Data(), triggerString.Data());
+    mcInputMultHisto        = Form("%s_%s", periodNameV0Reader.Data(), triggerString.Data());
+   
+    if (doMultiplicityWeighting){
+      cout << "enableling mult weighting" << endl;
+      analysisEventCuts[i]->SetUseWeightMultiplicityFromFile( kTRUE, fileNameInputForMultWeighing, dataInputMultHisto, mcInputMultHisto );
+    }
+
+    
     analysisEventCuts[i]->SetTriggerMimicking(enableTriggerMimicking);
     analysisEventCuts[i]->SetTriggerOverlapRejecion(enableTriggerOverlapRej);
     analysisEventCuts[i]->SetMaxFacPtHard(maxFacPtHard);
