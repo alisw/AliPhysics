alisw/AliPhysics

commit ed8230513c97362ac89732a34fb670f8c87631b9

Protection agains SIGFPE

PWGLF/FORWARD/analysis2/AliForwardUtil.cxx
@@ -558,7 +558,14 @@ void AliForwardUtil::GetEtaPhiFromStrip(Char_t    r,
   Double_t dy  = sy-ipY;
   Double_t rv  = TMath::Sqrt(TMath::Power(dx,2) + TMath::Power(dy,2));
   Double_t the = 2*TMath::ATan(TMath::Exp(-eta));
-  Double_t z   = rs / TMath::Tan(the);
+  Double_t tth = TMath::Tan(the);
+  if (TMath::Abs(tth) < 1e-9) {
+    ::Warning("GetEtaPhiFromStrip",
+	      "eta=%f -> theta=%f tan(theta)=%f invalid (no change)",
+	      eta, theta, tth);
+    return;
+  }
+  Double_t z   = rs / tth;
   // Printf("IP(x,y)=%f,%f S(x,y)=%f,%f D(x,y)=%f,%f R=%f theta=%f tan(theta)=%f z=%f", ipX, ipY, sx, sy, dx, dy, rv, the, TMath::Tan(the), z);
   eta          = -TMath::Log(TMath::Tan(TMath::ATan2(rv,z)/2));
   phi          = TMath::ATan2(dy,dx);

commit a86c44bf3808ff242c1f88b6ace4b4a9b608ee48

PWGLF/FORWARD: fixed variable name
Fixes minor problem introduced in ed8230513c97362ac89732a34fb670f8c87631b9

PWGLF/FORWARD/analysis2/AliForwardUtil.cxx
@@ -562,7 +562,7 @@ void AliForwardUtil::GetEtaPhiFromStrip(Char_t    r,
   if (TMath::Abs(tth) < 1e-9) {
     ::Warning("GetEtaPhiFromStrip",
 	      "eta=%f -> theta=%f tan(theta)=%f invalid (no change)",
-	      eta, theta, tth);
+	      eta, the, tth);
     return;
   }
   Double_t z   = rs / tth;