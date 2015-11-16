4105c6656f80142c484950208241ca98d88b2ec3
gconesab <Gustavo.Conesa.Balbastre@cern.ch>
2015-11-16 14:44:18 +0100

    improve to access files on grid, adapt comments to doxygen

--- EMCAL/macros/SimParamDB/PrintEMCALSimParam.C
+++ EMCAL/macros/SimParamDB/PrintEMCALSimParam.C
@@ -1,13 +1,26 @@
-// Macro to print the values stored in the OCDB with AliEMCALSimParam
-// These parameters are used during simulation
+/// \file PrintEMCALSimParam.C
+/// \brief Print OCDB simulation parameters
+///
+/// Macro to print the values stored in the OCDB with AliEMCALSimParam
+/// These parameters are used during simulation
+///
+/// \author : Gustavo Conesa Balbastre <Gustavo.Conesa.Balbastre@cern.ch>, (LPSC-CNRS)
+///
 
-// Author: Gustavo Conesa (IN2P3-LPSC)
-
-
-void PrintEMCALSimParam(char * file = "$ALICE_ROOT/OCDB/EMCAL/Calib/SimParam/Run0_999999999_v0_s0.root"){
+///
+/// Main method, access local or grid file, print its content.
+///
+/// \param file: string with location of input file
+///
+void PrintEMCALSimParam(TString file = 
+                        "alien:///alice/data/2015/OCDB/EMCAL/Calib/SimParam/Run0_999999999_v3_s0.root"
+//"$ALICE_ROOT/OCDB/EMCAL/Calib/SimParam/Run0_999999999_v0_s0.root"
+                        )
+{
+  if(file.Contains("alien://"))
+    TGrid::Connect("alien://");
   
-  
-  TFile * f = new TFile(file,"READ");
+  TFile * f = TFile::Open(file);
   
   AliCDBEntry * cdb = (AliCDBEntry*) f->Get("AliCDBEntry");    
   AliEMCALSimParam * sparam =  cdb->GetObject();
@@ -16,5 +29,4 @@ void PrintEMCALSimParam(char * file = "$ALICE_ROOT/OCDB/EMCAL/Calib/SimParam/Run
   cout<<"============== "<<sparam->GetName()<<" ==============="<<endl;
   
   sparam->Print("");
-  
 }
