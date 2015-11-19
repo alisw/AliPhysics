808780c5d0cd0474fafd003bc40284d482c5c507
cholm <Christian.Holm.Christensen@cern.ch>
2015-11-19 19:02:49 +0100

    Util geom functions return success/failure flag.  Other code react to this

--- PWGLF/FORWARD/analysis2/AliFMDDensityCalculator.cxx
+++ PWGLF/FORWARD/analysis2/AliFMDDensityCalculator.cxx
@@ -439,14 +439,16 @@ AliFMDDensityCalculator::Calculate(const AliESDFMD&        fmd,
 	  if (fRecalculatePhi) {
 	    // Correct for (x,y) off set of the interaction point 
 	    // AliForwardUtil::GetEtaPhiFromStrip(r,t,eta,phi,ip.X(),ip.Y());
-	    AliForwardUtil::GetEtaPhi(d,r,s,t,ip,eta,phi);
-	    DMSG(fDebug, 10, "IP(x,y,z)=%f,%f,%f Eta=%f -> %f Phi=%f -> %f",
-		 ip.X(), ip.Y(), ip.Z(), oldEta, eta, oldPhi, phi);
-	    if (TMath::Abs(eta) < 1) {
-	      AliWarningF("FMD%d%c[%2d,%3d] (%f,%f,%f) eta=%f phi=%f (was %f)",
+	    if (!AliForwardUtil::GetEtaPhi(d,r,s,t,ip,eta,phi) ||
+		TMath::Abs(eta) < 1) {
+	      AliWarningF("FMD%d%c[%2d,%3d] (%f,%f,%f) eta=%f phi=%f (%f)",
 			  d, r, s, t, ip.X(), ip.Y(), ip.Z(), eta,
 			  phi, oldEta);
-	    }	    
+	      eta = oldEta;
+	      phi = oldPhi;
+	    }
+	    DMSG(fDebug, 10, "IP(x,y,z)=%f,%f,%f Eta=%f -> %f Phi=%f -> %f",
+		 ip.X(), ip.Y(), ip.Z(), oldEta, eta, oldPhi, phi);
 	  }
 	  ADD_TIMER(timer,rePhiTime);
 	  START_TIMER(timer);
