c6f099975409f9f0946054b009c6f1c4fef30671
cholm <Christian.Holm.Christensen@cern.ch>
2015-11-24 01:30:19 +0100

    dN/deta tasks now do empirical correction it self. It also calculates the asymmetry.
    The script DrawdNdeta.C is being deprecated
    
    New script ExtractGSEs.C is the new thing and will be faces in
    instead.  Much better and simpler.
    
    Some updates to handling centralities

---
+++ PWGLF/FORWARD/analysis2/dndeta/ExtractGSEs.C
@@ -0,0 +1,324 @@
+//____________________________________________________________________
+TObject*
+GetObject(TDirectory* d, const char* name, TClass* cls=0, Bool_t verb=true)
+{
+  if (!d) {
+    Error("GetOject", "No directory");
+    return 0;
+  }
+  
+  TObject* o = d->Get(name);
+  if (!o) {
+    if (verb) 
+      Error("GetObject", "No object %s in directory %s", name, d->GetName());
+    return 0;
+  }
+
+  if (!cls) return o;
+
+  if (!o->IsA()->InheritsFrom(cls)) {
+    if (verb) 
+      Error("GetObject", "%s from %s is not a %s, but a %s",
+	    o->GetName(), d->GetName(), cls->GetName(), o->ClassName());
+    return 0;
+  }
+
+  return o;
+}
+
+//____________________________________________________________________
+TObject*
+GetObject(TCollection* d, const char* name, TClass* cls=0, Bool_t verb=true)
+{
+  if (!d) {
+    Error("GetOject", "No collection");
+    return 0;
+  }
+  
+  TObject* o = d->FindObject(name);
+  if (!o) {
+    if (verb)
+      Error("GetObject", "No object %s in collection %s", name, d->GetName());
+    return 0;
+  }
+
+  if (!cls) return o;
+
+  if (!o->IsA()->InheritsFrom(cls)) {
+    if (verb)
+      Error("GetObject", "%s from %s is not a %s, but a %s",
+	    o->GetName(), d->GetName(), cls->GetName(), o->ClassName());
+    return 0;
+  }
+
+  return o;
+}
+//____________________________________________________________________
+TCollection*
+GetCollection(TDirectory* d, const char* name, Bool_t verb=true)
+{
+  return static_cast<TCollection*>(GetObject(d, name, TCollection::Class(), verb));
+}
+//____________________________________________________________________
+TCollection*
+GetCollection(TCollection* d, const char* name, Bool_t verb=true)
+{
+  return static_cast<TCollection*>(GetObject(d, name, TCollection::Class(), verb));
+}
+//____________________________________________________________________
+TH2*
+GetH2(TCollection* c, const char* name, Bool_t verb=true)
+{
+  return static_cast<TH2*>(GetObject(c, name, TH2::Class(), verb));
+}
+//____________________________________________________________________
+TH1*
+GetH1(TCollection* c, const char* name, Bool_t verb=true)
+{
+  return static_cast<TH1*>(GetObject(c, name, TH1::Class(), verb));
+}
+//____________________________________________________________________
+THStack*
+GetStack(TCollection* c, const char* name, Bool_t verb=true)
+{
+  return static_cast<THStack*>(GetObject(c, name, THStack::Class(),verb));
+}
+//____________________________________________________________________
+TAxis*
+GetAxis(TCollection* c, const char* name, Bool_t verb=true)
+{
+  return static_cast<TAxis*>(GetObject(c, name, TAxis::Class(),verb));
+}
+//____________________________________________________________________
+TParameter<double>*
+GetParam(TCollection* c, const char* name, Bool_t verb=false)
+{
+  return static_cast<TParameter<double>*>(GetObject(c, name,
+						    TParameter<double>::Class(),
+						    verb));
+}
+
+//====================================================================
+void
+AddPath(const TString& dir, Bool_t before=true)
+{
+  const char* now = gROOT->GetMacroPath();
+  const char* fst = (before ? dir.Data() : now);
+  const char* lst = (before ? now        : dir.Data());
+  gROOT->SetMacroPath(Form("%s:%s",fst,lst));
+
+  gSystem->AddIncludePath(Form("-I%s",dir.Data()));
+}
+
+//____________________________________________________________________
+void
+AddRelPath(const char* rel, Bool_t first=true)
+{
+  TString alp(Form("$ALICE_PHYSICS/PWGLF/FORWARD/analysis2/%s",rel));
+  AddPath(alp, first);
+  if (gSystem->Getenv("ANA_SRC")) {
+    TString mine(Form("$ANA_SRC/%s", rel));
+    AddPath(mine, first);
+  }
+}
+  
+
+
+//====================================================================
+TH1* Rebin(TH1* h, Int_t rebin, Bool_t cutEdges) const
+{ 
+  if (rebin <= 1) return h;
+
+  Int_t nBins = h->GetNbinsX();
+  if(nBins % rebin != 0) {
+    Warning("Rebin", "Rebin factor %d is not a devisor of current number "
+	    "of bins %d in the histogram %s", rebin, nBins, h->GetName());
+    return;
+  }
+    
+  // Make a copy 
+  TH1* tmp = static_cast<TH1*>(h->Clone("tmp"));
+  tmp->Rebin(rebin);
+  tmp->SetDirectory(0);
+  tmp->Reset();
+  // The new number of bins 
+  Int_t nBinsNew = nBins / rebin;
+  for(Int_t i = 1;i<= nBinsNew; i++) {
+    Double_t content = 0;
+    Double_t sumw    = 0;
+    Double_t wsum    = 0;
+    Int_t    nbins   = 0;
+    for(Int_t j = 1; j<=rebin;j++) {
+      Int_t    bin = (i-1)*rebin + j;
+      Double_t c   =  h->GetBinContent(bin);
+
+      if (c <= 0) continue;
+
+      if ((cutEdges)) {
+	if (h->GetBinContent(bin+1)<=0 || 
+	    h->GetBinContent(bin-1)<=0) {
+	  Warning("Rebin", "removing bin %d=%f of %s (%d=%f,%d=%f)", 
+		  bin, c, h->GetName(), 
+		  bin+1, h->GetBinContent(bin+1), 
+		  bin-1, h->GetBinContent(bin-1));
+	  continue;
+	}	
+      }
+      Double_t e =  h->GetBinError(bin);
+      Double_t w =  1 / (e*e); // 1/c/c
+      content    += c;
+      sumw       += w;
+      wsum       += w * c;
+      nbins++;
+    }
+      
+    if(content > 0 && nbins > 1 ) {
+      tmp->SetBinContent(i, wsum / sumw);
+      tmp->SetBinError(i,1./TMath::Sqrt(sumw));
+    }
+  }
+
+  // Finally, rebin the histogram, and set new content
+  return tmp;
+}
+
+
+
+//____________________________________________________________________
+TObject*
+DoOne(TCollection*   c,
+      const TString& dir,
+      const TString& name,
+      Int_t          rebin,
+      void*          oa,
+      Bool_t         cutEdges=false)
+{
+  SysErrorAdder* adder = reinterpret_cast<SysErrorAdder*>(oa);
+
+  TCollection* sub = GetCollection(c, dir);
+  if (!sub) return 0;
+
+  TH1* hist = GetH1(sub, name);
+  if (!hist) return 0;
+
+  hist = Rebin(hist, rebin, cutEdges);
+  hist->SetMarkerColor(kBlack);
+  hist->SetFillColor(kBlack);
+  hist->SetLineColor(kBlack);
+
+  if (dir.BeginsWith("cent"))
+    hist->SetName(Form("%s_%s",hist->GetName(),dir.Data()));
+  
+  GraphSysErr* gse = adder->Make(hist, 0);
+  if (!gse) return 0;
+
+  gse->SetSumOption(GraphSysErr::kBox);
+  gse->SetSumFillColor(gse->GetMarkerColor());
+  gse->SetSumFillStyle(3002);
+  gse->SetCommonSumOption(GraphSysErr::kBox);
+  gse->SetCommonSumFillColor(gse->GetMarkerColor());
+  gse->SetCommonSumFillStyle(3001);
+
+  return gse;
+}
+  
+//____________________________________________________________________
+TMultiGraph*
+DrawOne(TObject* o, Option_t* opt, Double_t& min, Double_t& max)
+{
+  GraphSysErr* g = static_cast<GraphSysErr*>(o);
+  g->Draw(opt);
+  Double_t mn, mx;
+  g->GetMinMax(opt, mn, mx);
+  min = TMath::Min(min, mn);
+  max = TMath::Max(max, mx);
+  return g->GetMulti();
+}
+  
+
+//____________________________________________________________________
+TList*
+ExtractGSEs(const char* filename="forward_dndeta.root",
+	    Int_t rebin=5, Bool_t cutEdges=false,
+	    const char* name="Forward")
+{
+  if (!gROOT->GetClass("GraphSysErr")) {
+    AddPath("$HOME/GraphSysErr");
+    AddRelPath("gse",false);
+
+    gROOT->LoadMacro("GraphSysErr.C+g");
+  }
+  if (!gROOT->GetClass("SysErrorAdder")) {
+    AddRelPath("dndeta", true);
+    gROOT->LoadMacro("SysErrorAdder.C+g");
+  }
+
+  TFile* file = TFile::Open(filename,"READ");
+  if (!file) {
+    Warning("ExtractGSEs", "Failed to open %s", filename);
+    return 0;
+  }
+
+  TCollection* results = GetCollection(file, Form("%sdNdetaResults",name));
+  if (!results) return 0;
+
+
+  TH1*     emp    = GetH1    (results, "empirical");
+  TObject* osNN   = GetObject(results, "sNN");
+  TObject* osys   = GetObject(results, "sys");
+  TObject* otrg   = GetObject(results, "trigger");
+  TObject* ocentM = GetObject(results, "centEstimator");
+  TAxis*   centA  = GetAxis  (results, "centAxis");
+  TString  sys    = osys->GetTitle();
+  UShort_t sNN    = osNN->GetUniqueID();
+  TString  trg    = (ocentM && centA ? "CENT" : otrg->GetName());
+  TString  mth    = (ocentM && centA ? ocentM->GetTitle() : "");
+  TString  hname  = Form("dndeta%s%s",name,emp?"Emp":"");
+  
+  SysErrorAdder* adder = SysErrorAdder::Create(trg,sys,sNN,mth);
+  if (!adder) {
+    Warning("ExtractGSEs", "Failed to make adder");
+    return 0;
+  }
+
+  TList* ret   = new TList;
+  Bool_t first = true;
+  TH1*   frame = 0;
+  Double_t min = 100000, max = -1000000;
+  if (!centA || !ocentM) {
+    TObject* all = DoOne(results, "all", hname, rebin, adder, cutEdges);
+    if (!all) return 0;
+    ret->Add(all);
+    frame = DrawOne(all, "SUM QUAD AXIS", min, max)->GetHistogram();
+  }
+  else {
+    for (Int_t i = 1; i <= centA->GetNbins(); i++) {
+      Double_t low  = centA->GetBinLowEdge(i);
+      Double_t high = centA->GetBinUpEdge(i);
+      TString  dir;
+      dir.Form("cent%03dd%02d_%03dd%02d",
+	       Int_t(low),  Int_t(low *100)%100,
+	       Int_t(high), Int_t(high*100)%100);
+      TObject* g = DoOne(results, dir, hname, rebin, adder, cutEdges);
+      if (!g) continue;
+      ret->Add(g);
+      if (first)
+	frame = DrawOne(g, "SUM QUAD AXIS", min, max)->GetHistogram();
+      else
+	DrawOne(g, "SUM QUAD", min, max);
+      first = false;
+    }
+  }
+  frame->SetMinimum(0.9*min);
+  frame->SetMaximum(1.1*max);
+
+  TString outName;
+  outName.Form("%s_%05d_%s%s.root",sys.Data(),sNN,trg.Data(),mth.Data());
+  TFile* out = TFile::Open(outName,"RECREATE");
+  ret->Write("container",TObject::kSingleKey);
+  out->Write();
+
+  Info("ExtractGSEs", "Written to %s", outName.Data());
+  return ret;
+}
+  
