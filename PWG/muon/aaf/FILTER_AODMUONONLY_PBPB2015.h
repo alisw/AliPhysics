b38bce28f2e247ae4f95d02d3e9116896a2c9c0e
laphecet <laurent.aphecetche@gmail.com>
2015-11-26 17:32:29 +0100

    New dev filter for PbPb.

---
+++ PWG/muon/aaf/FILTER_AODMUONONLY_PBPB2015.h
@@ -0,0 +1,10 @@
+#ifndef FILTER_AODMUONONLY_PBPB2015_H
+#define FILTER_AODMUONONLY_PBPB2015_H
+
+namespace AAF {
+  
+  void FILTER_AODMUONONLY_PBPB2015(const char* from, const char* to);
+}
+
+#endif
+
