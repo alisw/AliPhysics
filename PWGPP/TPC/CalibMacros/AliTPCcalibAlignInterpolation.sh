926892ef99f4d104d2ad754dc6d1a2637c5591da
mivanov <marian.ivanov@cern.ch>
2015-11-30 15:47:56 +0100

    ATO-108 remove obsolete function form macro

--- PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolation.sh
+++ PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolation.sh
@@ -11,7 +11,7 @@
 makeEnvLocal(){
 #
 #
-# Example usage local 
+# Example usage local at GSI - test farm
 #   
     export isBatch=1
     echo makeEnvLocal
@@ -24,151 +24,17 @@ makeEnvLocal(){
 makeEnvHera(){
 #
 #
-# Example usage GSI farm
+# Example usage GSI big farm
 # 
     export isBatch=1
     echo makeEnvHera
     export batchCommand="qsub -cwd -V -l h_rt=24:0:0,h_rss=4G  "
-    export balice=/hera/alice/miranov/bin/.baliceCalib.sh
-    export incScript=/hera/alice/miranov/alice-tpc-notes/aux/rootlogon.C
+    export balice=/hera/alice/$USER/bin/.baliceCalib.sh
+    export incScript=$NOTES/aux/rootlogon.C
     source $balice
 }
 
 
-
-
-submitRunsFilteringPseudo(){
-    #
-    # OSBOLETE: This is pseudo code as was used for rub based residual maps creation
-    #
-
-    source $ALICE_PHYSICS/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolation.sh
-    #
-    # make run list ls -d */ | sed s_000__ | sed s_"/"__ > run.list
-    #
-    # 0.) Merge data
-    #
-    wdir=`pwd`
-    for a in `cat run.list `; do
-	cd $wdir/000$a;
-        rm ResidualHisto.root
-	hadd -f -k ResidualHisto.root `cat residual.list| sort -r | tail -n 1000`
-	cd $wdir
-    done; 
-    #
-    # 1.) submit histogram creation# ResidualHisto.root=>  ResidualHistograms.root
-    #
-    nTracks=10000000
-    wdir=`pwd`
-    for a in `cat run.list`; do  
-        [ -d $wdir/000$a ]  &&  	cd $wdir/000$a;   
-        [ -d $wdir/$a ] && cd $wdir/$a        
-	rm submit*
-        rm *.log 
-	rm Residual*.root
-	wdir1=`pwd`
-        for ihis in  1 2 4 5 ; do 
-	    echo $a $ihis;
-            mkdir $wdir1/fhis$ihis
-	    rm $wdir1/fhis$ihis/*
-            cd $wdir1/fhis$ihis 
-            rm $wdir1/fhis$ihis/*
-            ln -sf ../residual.list .
-	    echo aliroot -b -q  $ALICE_PHYSICS/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolationMacro.C+\\\(1,$ihis,$nTracks\\\) > submitHisto$ihis.sh
-            echo cp syswatch.log syswatch_His$ihis.log >>submitHisto$ihis.sh
-	    echo aliroot -b -q  $ALICE_PHYSICS/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolationMacro.C+\\\(2,$ihis,0\\\) >> submitHisto$ihis.sh
-            echo cp syswatch.log syswatch_Map$ihis.log >>submitHisto$ihis.sh
-	    chmod a+x submitHisto$ihis.sh
-	    echo qsub -cwd  -V -o submitHisto$ihis.log submitHisto$ihis.sh
-	    $batchCommand  -o submitHisto$ihis.log submitHisto$ihis.sh
-	    cd $wdir1
-	done; 
-	cd $wdir
-    done; 
-
-    #
-    # 2.) Make distortion maps #   ResidualHistograms.root => ResidualMaps.root
-    #
-    wdir=`pwd` 
-    batchCommand="qsub -cwd  -V" 
-    #batchCommand="qsub -cwd  -V -l h_rt=24:0:0,h_rss=4G" 
-    for arun in `cat run.list`; do
-        [ -d $wdir/000$arun ]  &&  	cd $wdir/000$arun;   
-        [ -d $wdir/$arun ] && cd $wdir/$arun;       
-	echo $wdir `pwd`; 	
-        wdir1=`pwd`;
-	rm *Map*{root,sh}
-	for ihis in  1 2 4 5 ; do 
-	    echo $a $ihis;
-            cd $wdir1/his$ihis 
-	    echo aliroot -b -q  $ALICE_PHYSICS/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolationMacro.C+\\\(2,$ihis,2\\\) > submitMap$ihis.sh
-	    echo cp syswatch.log syswatch_Map$ihis.log >>submitMap$ihis.sh	
-            chmod a+x submitMap*.sh
-	    echo qsub -cwd  -V -o submitMap$ihis.log submitMap$ihis.sh
-            $batchCommand -o submitMap$ihis.log submitMap$ihis.sh
-            cd $wdir1;
-         done;
-	cd $wdir
-    done; 
-    #
-    # 3.) Make AliNDLocalRegression fits
-    #  
-    #
-    wdir=`pwd`
-    secStep=2;
-    treeNames=( 'deltaRPhiTPCITSTRDDist'  'deltaRPhiTPCITSTOFDist'  'deltaZTPCITSTOFDist'  'deltaRPhiTPCITS'  'deltaZTPCIITSDist');
-    for arun in `cat run.list`; do
-	[ -d $wdir/000$arun ]  && cd $wdir/000$arun;   
-        [ -d $wdir/$arun ] &&    cd $wdir/$arun ;       
-        mkdir fit;
-        rm fit/*
-        cd fit;
-	for ctype in 0 1 2 3 4; do
-	    ln -sf ../ResidualMapFull_$ctype.root .
-            inputFile=ResidualMapFull_$ctype.root;
-	    for side in  0 1; do
-		for (( sec=0; sec<18; sec+=$secStep )); do 
-		    theta0=$[side-1];
-		    theta1=$[side];
-		    sec0=$sec;
-		    sec1=$[sec+secStep]
-		    echo $arun $theta0 $theta1 $sec0 $sec1
-		    submitScript=submit_${ctype}_${side}_${sec0}		    
-		    echo export inputFile=$inputFile >  ${submitScript}
-		    echo export inputTree=${treeNames[$ctype]} >> ${submitScript}
-		    echo export varTheta0=$theta0       >> ${submitScript}
-		    echo export varTheta1=$theta1       >> ${submitScript}
-		    echo export varSec0=$sec0           >> ${submitScript}
-		    echo export varSec1=$sec1           >> ${submitScript}
-		    echo export runNumber=$arun          >> ${submitScript}
-		    echo source /hera/alice/miranov/bin/.baliceCalib.sh  >> ${submitScript}
-		    echo aliroot -b -q  $ALICE_PHYSICS/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolationMacro.C+\\\(3\\\) >>  ${submitScript}
-                    mv ${submitScript} ${submitScript}.sh
-                    chmod a+x  ${submitScript}.sh    
-		    qsub -b y -cwd  -o ${submitScript}.log -e ${submitScript}.err  `pwd`/${submitScript}.sh              
-		done;
-	    done;
-	done;
-    done 
-    cd $wdir;
-
-
-
-   # cat bz.list0 | sed s_"*"__g | gawk '{print $2" "$3}' > bz.list
-   # cat bz.list | grep "-" | gawk '{print $1}' > runMinus.list
-   # cat bz.list | grep -v "-" | gawk '{print $1}' > runPlus.list
-   # ls -d 000*/| sed s_000__ | sed s_"/"__ > run.listq
-   # find `pwd`/../ -iname "ResidualHistos.root" > residualAll.list 
-
-   runList=run.list
-   inputList=residualAll.list
-   outputList=residual.list
-   touch $outputList
-   for a in `cat $runList`; do
-      cat $inputList | grep $a >> $outputList
-   done;
-
-}  
 
 
 submitTimeDependent(){
@@ -181,8 +47,8 @@ submitTimeDependent(){
     source $ALICE_PHYSICS/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolation.sh
     source $ALICE_PHYSICS/PWGPP/scripts/alilog4bash.sh  
 
-    makeEnvLocal; 
-    #makeEnvHera; 
+    # makeEnvLocal; 
+    # makeEnvHera; 
 
     # cp   $ALICE_PHYSICS/../src/PWGPP/TPC/CalibMacros/AliTPCcalibAlignInterpolationMacro.C AliTPCcalibAlignInterpolationMacro.C 
     #
@@ -328,10 +194,14 @@ submitTimeDependent(){
 submitProfileHis(){
     #
     # This macros I used to submit callgrind/valgrind jobs
+    #    expect inputDir calgrindCommand and calgrindCommand defined
+    #  
+    # GSI SETUP:
     #
-    inputDir=$NOTES/SpaceChargeDistortion/data/ATO-108/alice/data/2015/LHC15l.2011test/000240194/Time1445891400/his1
-    calgrindCommand="/usr/bin/valgrind --tool=callgrind --log-file=cpu.txt   --num-callers=40 -v  --trace-children=yes aliroot "
-    valgrindCommand="/usr/bin/valgrind --leak-check=full --leak-resolution=high --num-callers=40 --error-limit=no --show-reachable=yes  --log-file=xxx.txt -v aliroot"
+    # inputDir=$NOTES/SpaceChargeDistortion/data/ATO-108/alice/data/2015/LHC15l.2011test/000240194/Time1445891400/his1
+    # calgrindCommand="/usr/bin/valgrind --tool=callgrind --log-file=cpu.txt   --num-callers=40 -v  --trace-children=yes aliroot "
+    # valgrindCommand="/usr/bin/valgrind --leak-check=full --leak-resolution=high --num-callers=40 --error-limit=no --show-reachable=yes  --log-file=xxx.txt -v aliroot"
+    #
     wdir=`pwd`
     #
     mkdir $wdir/calgrind/
