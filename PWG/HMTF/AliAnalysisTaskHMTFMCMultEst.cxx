b2ff6e25a8af0bded272dbe1340bc3f4ab91db19
cbourjau <christian.bourjau@cern.ch>
2015-12-02 17:46:14 +0100

    added more sophisticated "global trigger" logic
    Initial this was done by overwriting a  member function pointer. However,
    Root throws errors if a member of a class is of type "member function
    pointer"; TStreamer claims that the type is unknow. Lite seemed to work,
    but I removed this to be sure

--- PWG/HMTF/AliAnalysisTaskHMTFMCMultEst.cxx
+++ PWG/HMTF/AliAnalysisTaskHMTFMCMultEst.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <new>
 
 #include "AliLog.h"
 #include "AliMCEvent.h"
@@ -24,15 +25,15 @@ ClassImp(AliAnalysisTaskHMTFMCMultEst)
 
 //________________________________________________________________________
 AliAnalysisTaskHMTFMCMultEst::AliAnalysisTaskHMTFMCMultEst()
-: AliAnalysisTaskSE(), fMyOut(0), fClassifiers(0), fObservables(0), fGlobalTriggerName(0),
-  fGlobalTriggerMinValue(0), fGlobalTrigger(0)
+: AliAnalysisTaskSE(), fMyOut(0), fClassifiers(0), fObservables(0), fGlobalTrigger(0),
+  fGlobalTriggerClassifiers(0)
 {
 }
 
 //________________________________________________________________________
 AliAnalysisTaskHMTFMCMultEst::AliAnalysisTaskHMTFMCMultEst(const char *name)
-  : AliAnalysisTaskSE(name), fMyOut(0), fClassifiers(0), fObservables(0), fGlobalTriggerName(0),
-    fGlobalTriggerMinValue(0), fGlobalTrigger(0)
+  : AliAnalysisTaskSE(name), fMyOut(0), fClassifiers(0), fObservables(0), fGlobalTrigger(0),
+    fGlobalTriggerClassifiers(0)
 {
   DefineOutput(1, TList::Class());
 }
@@ -50,17 +51,27 @@ void AliAnalysisTaskHMTFMCMultEst::UserCreateOutputObjects()
   // plots between N classifiers. We restrict oursefls to calculating only the correlations of every
   // classifier to a small number of reference classifiers.
   Float_t region2[] = {-0.5, 0.5};  // Use this array to pass two pass two edges to the constructor
-  AliEventClassifierMult* refClassifierEtaLt05 = new AliEventClassifierMult("EtaLt05", "|#eta| < 0.5",
-									    region2, 2, true, true, fMyOut);
+  AliEventClassifierMult* refClassifierEtaLt05 =
+    new AliEventClassifierMult("EtaLt05", "|#eta| < 0.5", region2, 2, true, true, fMyOut);
   fClassifiers.push_back(refClassifierEtaLt05);
+
   region2[0] = -0.8; region2[1] = 0.8; 
   fClassifiers.push_back(new AliEventClassifierMult("EtaLt08", "|#eta| < 0.8", region2, 2, true, true, fMyOut));
+
+  region2[0] = -1.0; region2[1] = 1.0;
+  AliEventClassifierMult* etaLt1 = new AliEventClassifierMult("EtaLt1", "|#eta| < 1.0", region2, 2, true, true, fMyOut);
+  fClassifiers.push_back(etaLt1);
+
   region2[0] = -1.5; region2[1] = 1.5; 
-  fClassifiers.push_back(new AliEventClassifierMult("EtaLt15", "|#eta| < 0.8", region2, 2, true, true, fMyOut));
-  region2[0] = 2.8; region2[1] = 5.1; 
-  fClassifiers.push_back(new AliEventClassifierMult("V0A", "2.8 < #eta < 5.1", region2, 2, true, true, fMyOut));
-  region2[0] = -3.7; region2[1] = -1.7; 
-  fClassifiers.push_back(new AliEventClassifierMult("V0C", "-3.7 < #eta < -1.7", region2, 2, true, true, fMyOut));
+  fClassifiers.push_back(new AliEventClassifierMult("EtaLt15", "|#eta| < 1.5", region2, 2, true, true, fMyOut));
+
+  region2[0] = 2.8; region2[1] = 5.1;
+  AliEventClassifierMult* v0a = new AliEventClassifierMult("V0A", "2.8 < #eta < 5.1", region2, 2, true, true, fMyOut);
+  fClassifiers.push_back(v0a);
+
+  region2[0] = -3.7; region2[1] = -1.7;
+  AliEventClassifierMult* v0c = new AliEventClassifierMult("V0C", "-3.7 < #eta < -1.7", region2, 2, true, true, fMyOut);
+  fClassifiers.push_back(v0c);
 
   region2[0] = -8.7; region2[1] = 8.7; // not inclusive region, not charged!
   fClassifiers.push_back(new AliEventClassifierMult("ZDC", "|#eta| > 8.7", region2, 2, false, false, fMyOut));
@@ -90,21 +101,22 @@ void AliAnalysisTaskHMTFMCMultEst::UserCreateOutputObjects()
   fClassifiers.push_back(refClassifierSphericity);
 
   // Set the global trigger if one was defined for this task
-  if (strcmp(fGlobalTriggerName.Data(), "InelGt0") == 0) {
-    region2[0] = -1.0; region2[1] = 1.0;
-    fGlobalTrigger = new AliEventClassifierMult("EtaLt1", "|#eta| < 1.0",
-						region2, 2, true, true, fMyOut);
-    fGlobalTriggerMinValue = 0.0;
+  // Remember to reset the classifieres used here as well (if new ones are defined just for the trigger)
+  if (fGlobalTrigger == kINEL) {
+    SetupInelAsGlobalTrigger();
+    cout << "INEL global trigger set" << endl;
+  }
+  else if (fGlobalTrigger == kINELGT0) {
+    SetupInelGt0AsGlobalTrigger(etaLt1);
     cout << "Inel > 0 global trigger set" << "\n";
   }
-  else if (strcmp(fGlobalTriggerName.Data(), "SphericityGt09") == 0) {
-    fGlobalTrigger = refClassifierSphericity;
-    fGlobalTriggerMinValue = 0.9;
-    cout << "Sphericity global trigger set" << "\n";
+  else if (fGlobalTrigger == kV0AND) {
+    SetupV0ANDAsGlobalTrigger(v0a, v0c);
+    cout << "V0AND global trigger set" << "\n";
   }
   else
-    cout << "No global trigger set" << endl;
-  
+    AliError("Invalid global trigger name given");
+
   //////////////////////////////////////////
   // Connect classifiers with observables //
   //////////////////////////////////////////
@@ -121,14 +133,13 @@ void AliAnalysisTaskHMTFMCMultEst::UserCreateOutputObjects()
   PostData(1, fMyOut);
 }
 
+
 //________________________________________________________________________
 void AliAnalysisTaskHMTFMCMultEst::UserExec(Option_t *)
 {
   // Reset classifiers before doing anything with this new event
   for (Int_t i = 0; i < fClassifiers.size(); i++) {
     fClassifiers[i]->ResetClassifier();
-    if (fGlobalTrigger)
-      fGlobalTrigger->ResetClassifier();
   }
 
   // Load event
@@ -139,9 +150,10 @@ void AliAnalysisTaskHMTFMCMultEst::UserExec(Option_t *)
   }
   AliStack  *stack = mcEvent->Stack();
 
-  // Either no global trigger is defined or we require it to be fullfilled
-  if (!fGlobalTrigger
-      || (fGlobalTrigger->GetClassifierValue(mcEvent, stack) > fGlobalTriggerMinValue)) {
+  // do we have the right trigger?
+  if (((fGlobalTrigger == kINEL) && IsInel(mcEvent, stack)) ||
+      ((fGlobalTrigger == kINELGT0) && IsInelGt0(mcEvent, stack)) ||
+      ((fGlobalTrigger == kV0AND) && IsV0AND(mcEvent, stack))) {
     for (Int_t i = 0; i < fObservables.size(); i++) {
       fObservables[i]->Fill(mcEvent, stack);
     }
@@ -156,3 +168,45 @@ void AliAnalysisTaskHMTFMCMultEst::Terminate(Option_t *)
 {
   //PostData(2, fRunconditions);
 }
+
+
+
+/////////////////////////////////////////////////////////////
+// Setup the global triggers and their respose when called //
+/////////////////////////////////////////////////////////////
+void AliAnalysisTaskHMTFMCMultEst::SetupInelAsGlobalTrigger() {
+  return;
+}
+
+void AliAnalysisTaskHMTFMCMultEst::SetupInelGt0AsGlobalTrigger(AliEventClassifierBase* etaLt1) {
+  fGlobalTriggerClassifiers.push_back(etaLt1);
+}
+
+void AliAnalysisTaskHMTFMCMultEst::SetupV0ANDAsGlobalTrigger(AliEventClassifierBase* V0A, AliEventClassifierBase* V0C) {
+  fGlobalTriggerClassifiers.push_back(V0A);
+  fGlobalTriggerClassifiers.push_back(V0C);
+}
+
+/*
+  Return true if the current event fulfills the trigger requiremtn
+*/
+Bool_t AliAnalysisTaskHMTFMCMultEst::IsInel(AliMCEvent *event, AliStack *stack) {
+  return kTRUE;
+}
+
+Bool_t AliAnalysisTaskHMTFMCMultEst::IsInelGt0(AliMCEvent *event, AliStack *stack) {
+  if (fGlobalTriggerClassifiers[0]->GetClassifierValue(event, stack) > 0)
+    return kTRUE;
+  else
+    return kFALSE;
+}
+
+Bool_t AliAnalysisTaskHMTFMCMultEst::IsV0AND(AliMCEvent *event, AliStack *stack) {
+  // The tow estimators in the vector are V0A and V0B
+  if ((fGlobalTriggerClassifiers[0]->GetClassifierValue(event, stack) > 0)
+      && (fGlobalTriggerClassifiers[1]->GetClassifierValue(event, stack) > 0))
+    return kTRUE;
+  else
+    return kFALSE;
+}
+
