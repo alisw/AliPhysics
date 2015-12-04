8290b30bdc782b1571ebfd0110c77ae68efc225b
mbroz <Michal.Broz@cern.ch>
2015-12-04 16:59:50 +0100

    Another PM gain curves

--- AD/macros/MakeADPMGainsEntry.C
+++ AD/macros/MakeADPMGainsEntry.C
@@ -21,7 +21,7 @@ void MakeADPMGainsEntry(const char *outputCDB = "local://$ALICE_ROOT/OCDB")
 		    
   Double_t b[16] = {7.147 ,5.681 ,6.794 ,8.848 ,6.503 ,7.394 ,6.568 ,5.928,
   		    6.739 ,6.815 ,6.510 ,6.866 ,6.136 ,6.118 ,6.799 ,6.230};
-  /*/ 
+   
 		    
   //2015 after run  233912 (LHC15h period).
   Double_t a[16] = {1518.0, 1522.1, 1565.3, 1630.1, 1517.8, 1465.8, 1164.0, 1512.2, 
@@ -30,6 +30,22 @@ void MakeADPMGainsEntry(const char *outputCDB = "local://$ALICE_ROOT/OCDB")
 		    
   Double_t b[16] = {7.664, 5.681, 6.794, 8.848, 6.503, 7.394, 5.609, 5.928, 
                     6.739, 6.815, 6.510, 6.866, 6.136, 6.118, 6.799, 6.230};
+		    
+  //2015 after run 238785 		    
+  Double_t a[16] = {1272.8, 1283.7, 1325.5, 1511.5, 1498.4, 1449.6, 1349.2, 1337.8, 
+  	            1348.3, 1476.9, 1243.3, 1403.6, 1337.3, 1347.0, 1376.4, 1487.0};
+  
+  Double_t b[16] = { 6.230, 6.866, 6.799, 6.510, 6.118, 6.815, 6.136, 6.739,
+  		     5.928, 8.848, 6.568, 6.794, 7.394, 5.681, 6.503, 7.147};
+
+   /*/		    
+  //2015 after run 245683
+  Double_t a[16] = {1272.8, 1283.7, 1325.5, 1511.5, 1498.4, 1449.6, 1104.4, 1337.8, 
+  	            1348.3, 1476.9, 1243.3, 1403.6, 1337.3, 1347.0, 1376.4, 1487.0};
+  
+  Double_t b[16] = { 6.230, 6.866, 6.799, 6.510, 6.118, 6.815, 9.027, 6.739,
+  		     5.928, 8.848, 6.568, 6.794, 7.394, 5.681, 6.503, 7.147};
+  
 		    
   TH2F *gains = new TH2F("ADPMGains", "AD PM gain factors", 16, -0.5, 15.5, 2, -0.5, 1.5);
   for(Int_t channel = 0; channel < 16; ++channel) {
@@ -45,7 +61,7 @@ void MakeADPMGainsEntry(const char *outputCDB = "local://$ALICE_ROOT/OCDB")
   md->AddDateToComment();
   md->PrintMetaData();
 
-  AliCDBId id("AD/Calib/PMGains", 233912, AliCDBRunRange::Infinity());
+  AliCDBId id("AD/Calib/PMGains", 245683, AliCDBRunRange::Infinity());
   man->Put(gains, id, md);
 
   delete md;
