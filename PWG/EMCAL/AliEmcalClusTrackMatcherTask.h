4dda107412d6d6e79d92cbccc16b0fff17e0c84a
saiola <salvatore.aiola@cern.ch>
2015-12-08 15:57:30 +0100

    Simplify logic; track propagation is done automatically if needed; uses standard objects (AliVTrack and AliVClusters), AliEmcalParticle used internally (can be posted to the event if requested)

--- PWG/EMCAL/AliEmcalClusTrackMatcherTask.h
+++ PWG/EMCAL/AliEmcalClusTrackMatcherTask.h
@@ -1,8 +1,6 @@
 #ifndef ALIEMCALCLUSTRACKMATCHERTASK_H
 #define ALIEMCALCLUSTRACKMATCHERTASK_H
 
-// $Id$
-
 #include "AliAnalysisTaskEmcal.h"
 
 class AliEmcalClusTrackMatcherTask : public AliAnalysisTaskEmcal {
@@ -11,8 +9,14 @@ class AliEmcalClusTrackMatcherTask : public AliAnalysisTaskEmcal {
   AliEmcalClusTrackMatcherTask(const char *name, Bool_t histo=kFALSE);
   virtual ~AliEmcalClusTrackMatcherTask();
 
-  void          SetMaxDistance(Double_t d)       { fMaxDistance = d; }
-  void          SetModifyObjs(Bool_t b)          { fModifyObjs  = b; }
+  void          SetPropDist(Double_t d)           { fPropDist              = d; }
+  void          SetDoPropagation(Bool_t b)        { fDoPropagation         = b; }
+  void          SetAttemptProp(Bool_t b)          { fAttemptProp           = b; }
+  void          SetAttemptPropMatch(Bool_t b)     { fAttemptPropMatch      = b; }
+  void          SetMaxDistance(Double_t d)        { fMaxDistance           = d; }
+  void          SetAttachEmcalParticles(Bool_t b) { fAttachEmcalParticles  = b; }
+  void          SetUpdateTracks(Bool_t b)         { fUpdateTracks          = b; }
+  void          SetUpdateClusters(Bool_t b)       { fUpdateClusters        = b; }
 
  protected:
   void          ExecOnce();
@@ -20,10 +24,24 @@ class AliEmcalClusTrackMatcherTask : public AliAnalysisTaskEmcal {
   Bool_t        Run();
   void          UserCreateOutputObjects();
 
+  void          GenerateEmcalParticles();
+  void          DoMatching();
+  void          UpdateTracks();
+  void          UpdateClusters();
+  
+  Double_t      fPropDist;              // distance to surface (440cm default)
+  Bool_t        fDoPropagation;         // if true then propagate all hybrid tracks to EMCal surface
+  Bool_t        fAttemptProp;           // if true then attempt to propagate if not done yet
+  Bool_t        fAttemptPropMatch;      // if true then attempt to propagate if not done yet but IsEMCAL is true
   Double_t      fMaxDistance;           // maximum distance to match clusters and tracks
-  Bool_t        fModifyObjs;            // if true then modify original tracks/clusters
-  TClonesArray *fOrigTracks;            //!ptr to original tracks (used if fModifyObjs true)
-  TClonesArray *fOrigClus;              //!ptr to original clusters (used if fModifyObjs true)
+  Bool_t        fAttachEmcalParticles;  // attach emcal particles to the event, so that other tasks can use them
+  Bool_t        fUpdateTracks;          // update tracks with matching info
+  Bool_t        fUpdateClusters;        // update clusters with matching info
+
+  TClonesArray *fEmcalTracks;           //!emcal tracks
+  TClonesArray *fEmcalClusters;         //!emcal clusters
+  Int_t         fNEmcalTracks;          //!number of emcal tracks
+  Int_t         fNEmcalClusters;        //!number of emcal clusters
   TH1          *fHistMatchEtaAll;       //!deta distribution
   TH1          *fHistMatchPhiAll;       //!dphi distribution
   TH1          *fHistMatchEta[8][9][2]; //!deta distribution
@@ -33,6 +51,6 @@ class AliEmcalClusTrackMatcherTask : public AliAnalysisTaskEmcal {
   AliEmcalClusTrackMatcherTask(const AliEmcalClusTrackMatcherTask&);            // not implemented
   AliEmcalClusTrackMatcherTask &operator=(const AliEmcalClusTrackMatcherTask&); // not implemented
 
-  ClassDef(AliEmcalClusTrackMatcherTask, 6) // Cluster-Track matching task
+  ClassDef(AliEmcalClusTrackMatcherTask, 7) // Cluster-Track matching task
 };
 #endif
