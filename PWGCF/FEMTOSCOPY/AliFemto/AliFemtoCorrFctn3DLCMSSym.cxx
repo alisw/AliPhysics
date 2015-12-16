0dc986f304721c4cb352a428702c6c403c6f2751
akubera <andrew.kubera@cern.ch>
2015-12-16 17:08:16 -0500

    PWGCF: AliFemtoCorrFctn3DLCMSSym - 3D histogram axes are given q_{out,side,long} labels. Implemented Clone method and improved copy constructor/assignment. Added doxygen documentation.

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCorrFctn3DLCMSSym.cxx
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCorrFctn3DLCMSSym.cxx
@@ -1,16 +1,12 @@
-///////////////////////////////////////////////////////////////////////////
-//                                                                       //
-// AliFemtoCorrFctn3DLCMSSym: a class to calculate 3D correlation        //
-// for pairs of identical particles.                                     //
-// In analysis the function should be first created in a macro, then     //
-// added to the analysis, and at the end of the macro the procedure to   //
-// write out histograms should be called.                                //
-//                                                                       //
-///////////////////////////////////////////////////////////////////////////
+///
+/// \file AliFemtoCorrFctn3DLCMSSym.cxx
+///
+
 
 #include "AliFemtoCorrFctn3DLCMSSym.h"
+#include "AliFemtoPairCut.h"
 
-#include <cstdio>
+#include <TH3F.h>
 
 #ifdef __ROOT__
   /// \cond CLASSIMP
@@ -19,34 +15,46 @@
 #endif
 
 //____________________________
-AliFemtoCorrFctn3DLCMSSym::AliFemtoCorrFctn3DLCMSSym(char* title, const int& nbins, const float& QHi)
-:
-AliFemtoCorrFctn(),
-  fNumerator(0),
-  fDenominator(0),
-  fNumeratorW(0),
-  fDenominatorW(0),
-  fUseLCMS(1)
+AliFemtoCorrFctn3DLCMSSym::AliFemtoCorrFctn3DLCMSSym(const char* title,
+                                                     const int nbins,
+                                                     const float QHi):
+  AliFemtoCorrFctn()
+  , fNumerator(NULL)
+  , fDenominator(NULL)
+  , fNumeratorW(NULL)
+  , fDenominatorW(NULL)
+  , fUseLCMS(1)
 {
   // Basic constructor
+  TString hist_title = TString::Format("%s; q_{out} (GeV); q_{side} (GeV); q_{long} (GeV)", title);
 
   // set up numerator
-  char tTitNum[101] = "Num";
-  strncat(tTitNum,title, 100);
-  fNumerator = new TH3F(tTitNum,title,nbins,-QHi,QHi,nbins,-QHi,QHi,nbins,-QHi,QHi);
+  fNumerator = new TH3F(TString("Num") + title,
+                        hist_title,
+                        nbins, -QHi, QHi,
+                        nbins, -QHi, QHi,
+                        nbins, -QHi, QHi);
   // set up denominator
-  char tTitDen[101] = "Den";
-  strncat(tTitDen,title, 100);
-  fDenominator = new TH3F(tTitDen,title,nbins,-QHi,QHi,nbins,-QHi,QHi,nbins,-QHi,QHi);
-//Weighted by qinv histos
+  fDenominator = new TH3F(TString("Den") + title,
+                          hist_title,
+                          nbins, -QHi, QHi,
+                          nbins, -QHi, QHi,
+                          nbins, -QHi, QHi);
+
+  // Weighted by qinv histos
   // set up numerator
-  char tTitNumW[101] = "NumWqinv";
-  strncat(tTitNumW,title, 100);
-  fNumeratorW = new TH3F(tTitNumW,title,nbins,-QHi,QHi,nbins,-QHi,QHi,nbins,-QHi,QHi);
+  fNumeratorW = new TH3F(TString("NumWqinv") + title,
+                         hist_title,
+                         nbins, -QHi, QHi,
+                         nbins, -QHi, QHi,
+                         nbins, -QHi, QHi);
+
   // set up denominator
-  char tTitDenW[101] = "DenWqinv";
-  strncat(tTitDenW,title, 100);
-  fDenominatorW = new TH3F(tTitDenW,title,nbins,-QHi,QHi,nbins,-QHi,QHi,nbins,-QHi,QHi);
+  fDenominatorW = new TH3F(TString("DenWqinv") + title,
+                           hist_title,
+                           nbins, -QHi, QHi,
+                           nbins, -QHi, QHi,
+                           nbins, -QHi, QHi);
 
   // to enable error bar calculation...
   fNumerator->Sumw2();
@@ -55,23 +63,23 @@ AliFemtoCorrFctn(),
   fDenominatorW->Sumw2();
 }
 
-AliFemtoCorrFctn3DLCMSSym::AliFemtoCorrFctn3DLCMSSym(const AliFemtoCorrFctn3DLCMSSym& aCorrFctn) :
-  AliFemtoCorrFctn(aCorrFctn),
-  fNumerator(0),
-  fDenominator(0),
-  fNumeratorW(0),
-  fDenominatorW(0),
-  fUseLCMS(1)
+AliFemtoCorrFctn3DLCMSSym::AliFemtoCorrFctn3DLCMSSym(const AliFemtoCorrFctn3DLCMSSym& aCorrFctn):
+  AliFemtoCorrFctn(aCorrFctn)
+  , fNumerator(new TH3F(*aCorrFctn.fNumerator))
+  , fDenominator(new TH3F(*aCorrFctn.fDenominator))
+  , fNumeratorW(new TH3F(*aCorrFctn.fNumeratorW))
+  , fDenominatorW(new TH3F(*aCorrFctn.fDenominatorW))
+  , fUseLCMS(aCorrFctn.fUseLCMS)
 {
   // Copy constructor
-  fNumerator = new TH3F(*aCorrFctn.fNumerator);
-  fDenominator = new TH3F(*aCorrFctn.fDenominator);
-  fNumeratorW = new TH3F(*aCorrFctn.fNumeratorW);
-  fDenominatorW = new TH3F(*aCorrFctn.fDenominatorW);
-  fUseLCMS = aCorrFctn.fUseLCMS;
+  fNumerator->Sumw2();
+  fDenominator->Sumw2();
+  fNumeratorW->Sumw2();
+  fDenominatorW->Sumw2();
 }
 //____________________________
-AliFemtoCorrFctn3DLCMSSym::~AliFemtoCorrFctn3DLCMSSym(){
+AliFemtoCorrFctn3DLCMSSym::~AliFemtoCorrFctn3DLCMSSym()
+{
   // Destructor
   delete fNumerator;
   delete fDenominator;
@@ -85,20 +93,33 @@ AliFemtoCorrFctn3DLCMSSym& AliFemtoCorrFctn3DLCMSSym::operator=(const AliFemtoCo
   if (this == &aCorrFctn)
     return *this;
 
-  if (fNumerator) delete fNumerator;
+  AliFemtoCorrFctn::operator=(aCorrFctn);
+
+  delete fNumerator;
   fNumerator = new TH3F(*aCorrFctn.fNumerator);
-  if (fDenominator) delete fDenominator;
+
+  delete fDenominator;
   fDenominator = new TH3F(*aCorrFctn.fDenominator);
-  if (fNumeratorW) delete fNumeratorW;
+
+  delete fNumeratorW;
   fNumeratorW = new TH3F(*aCorrFctn.fNumeratorW);
-  if (fDenominatorW) delete fDenominatorW;
+
+  delete fDenominatorW;
   fDenominatorW = new TH3F(*aCorrFctn.fDenominatorW);
+
   fUseLCMS = aCorrFctn.fUseLCMS;
+
+  fNumerator->Sumw2();
+  fDenominator->Sumw2();
+  fNumeratorW->Sumw2();
+  fDenominatorW->Sumw2();
+
   return *this;
 }
 
 //_________________________
-void AliFemtoCorrFctn3DLCMSSym::WriteOutHistos(){
+void AliFemtoCorrFctn3DLCMSSym::WriteOutHistos()
+{
   // Write out all histograms to file
   fNumerator->Write();
   fDenominator->Write();
@@ -120,68 +141,78 @@ TList* AliFemtoCorrFctn3DLCMSSym::GetOutputList()
 }
 
 //_________________________
-void AliFemtoCorrFctn3DLCMSSym::Finish(){
+void AliFemtoCorrFctn3DLCMSSym::Finish()
+{
   // here is where we should normalize, fit, etc...
-
 }
 
 //____________________________
-AliFemtoString AliFemtoCorrFctn3DLCMSSym::Report(){
+AliFemtoString AliFemtoCorrFctn3DLCMSSym::Report()
+{
   // Construct the report
-  string stemp = "LCMS Frame Bertsch-Pratt 3D Correlation Function Report:\n";
-  char ctemp[100];
-  snprintf(ctemp , 100, "Number of entries in numerator:\t%E\n",fNumerator->GetEntries());
-  stemp += ctemp;
-  snprintf(ctemp , 100, "Number of entries in denominator:\t%E\n",fDenominator->GetEntries());
-  stemp += ctemp;
+  TString report = "LCMS Frame Bertsch-Pratt 3D Correlation Function Report:\n";
+  report += TString::Format("Number of entries in numerator:\t%E\n", fNumerator->GetEntries());
+  report += TString::Format("Number of entries in denominator:\t%E\n", fDenominator->GetEntries());
 
-  if (fPairCut){
-    snprintf(ctemp , 100, "Here is the PairCut specific to this CorrFctn\n");
-    stemp += ctemp;
-    stemp += fPairCut->Report();
-  }
-  else{
-    snprintf(ctemp , 100, "No PairCut specific to this CorrFctn\n");
-    stemp += ctemp;
+  if (fPairCut) {
+    report += "Here is the PairCut specific to this CorrFctn\n";
+    report += fPairCut->Report();
+  } else {
+    report += "No PairCut specific to this CorrFctn\n";
   }
 
-  //
-  AliFemtoString returnThis = stemp;
-  return returnThis;
+  return AliFemtoString(report);
 }
 //____________________________
-void AliFemtoCorrFctn3DLCMSSym::AddRealPair( AliFemtoPair* pair){
+void AliFemtoCorrFctn3DLCMSSym::AddRealPair(AliFemtoPair* pair)
+{
   // perform operations on real pairs
-  if (fPairCut){
-    if (!(fPairCut->Pass(pair))) return;
+  if (fPairCut && !fPairCut->Pass(pair)) {
+    return;
   }
 
   if (fUseLCMS) {
-    fNumerator->Fill(pair->QOutCMS(),pair->QSideCMS(),pair->QLongCMS());
-    fNumeratorW->Fill(pair->QOutCMS(),pair->QSideCMS(),pair->QLongCMS(),pair->QInv());
+    const Double_t qout = pair->QOutCMS(),
+                  qside = pair->QSideCMS(),
+                  qlong = pair->QLongCMS();
+
+    fNumerator->Fill(qout, qside, qlong, 1.0);
+    fNumeratorW->Fill(qout, qside, qlong, pair->QInv());
   }
   else {
-    fNumerator->Fill(pair->QOutPf(),pair->QSidePf(),pair->QLongPf());
-    fNumeratorW->Fill(pair->QOutPf(),pair->QSidePf(),pair->QLongPf(),pair->QInv());
+    const Double_t qout = pair->QOutPf(),
+                  qside = pair->QSidePf(),
+                  qlong = pair->QLongPf();
+
+    fNumerator->Fill(qout, qside, qlong, 1.0);
+    fNumeratorW->Fill(qout, qside, qlong, pair->QInv());
   }
+
 }
 //____________________________
-void AliFemtoCorrFctn3DLCMSSym::AddMixedPair( AliFemtoPair* pair){
+void AliFemtoCorrFctn3DLCMSSym::AddMixedPair(AliFemtoPair* pair)
+{
   // perform operations on mixed pairs
-  if (fPairCut){
-    if (!(fPairCut->Pass(pair))) return;
+  if (fPairCut && !fPairCut->Pass(pair)) {
+    return;
   }
 
   if (fUseLCMS) {
-    fDenominator->Fill(pair->QOutCMS(),pair->QSideCMS(),pair->QLongCMS(),1.0);
-    fDenominatorW->Fill(pair->QOutCMS(),pair->QSideCMS(),pair->QLongCMS(),pair->QInv());
+    const Double_t qout = pair->QOutCMS(),
+                  qside = pair->QSideCMS(),
+                  qlong = pair->QLongCMS();
+
+    fDenominator->Fill(qout, qside, qlong, 1.0);
+    fDenominatorW->Fill(qout, qside, qlong, pair->QInv());
   }
   else {
-    fDenominator->Fill(pair->QOutPf(),pair->QSidePf(),pair->QLongPf(),1.0);
-    fDenominatorW->Fill(pair->QOutPf(),pair->QSidePf(),pair->QLongPf(),pair->QInv());
+    const Double_t qout = pair->QOutPf(),
+                  qside = pair->QSidePf(),
+                  qlong = pair->QLongPf();
+
+    fDenominator->Fill(qout, qside, qlong, 1.0);
+    fDenominatorW->Fill(qout, qside, qlong, pair->QInv());
   }
-
-
 
 }
 
--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCorrFctn3DLCMSSym.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCorrFctn3DLCMSSym.h
@@ -1,52 +1,61 @@
-///////////////////////////////////////////////////////////////////////////
-//                                                                       //
-// AliFemtoCorrFctn3DLCMSSym: a class to calculate 3D correlation        //
-// for pairs of identical particles vs. Bertsh-Pratt coordinates.        //
-//                                                                       //
-///////////////////////////////////////////////////////////////////////////
+///
+/// \file AliFemtoCorrFctn3DLCMSSym.h
+///
 
 #ifndef ALIFEMTOCORRFCTN3DLCMS_H
 #define ALIFEMTOCORRFCTN3DLCMS_H
 
-#include "AliFemtoCorrFctn.h"
-#include "AliFemtoPairCut.h"
-#include "TH3F.h"
-//#include "TArrayD.h"
+// forward declare classes
+class TH3F;
+class AliFemtoPairCut;
 
+// include headers
+#include "AliFemtoCorrFctn.h"
+
+/// \class AliFemtoCorrFctn3DLCMSSym
+/// \brief A class to calculate 3D correlation functions for pairs of identical
+///        particles vs. Bertsh-Pratt coordinates.
+///
+///
 class AliFemtoCorrFctn3DLCMSSym : public AliFemtoCorrFctn {
 public:
-  AliFemtoCorrFctn3DLCMSSym(char* title, const int& nbins, const float& QHi);
+  /**
+   * Build the correlation function with parameters.
+   * @param title The title with which to give the output
+   * @param nbins The number of bins in each direction of , and q
+   */
+  AliFemtoCorrFctn3DLCMSSym(const char* title, const int nbins, const float QHi);
   AliFemtoCorrFctn3DLCMSSym(const AliFemtoCorrFctn3DLCMSSym& aCorrFctn);
   virtual ~AliFemtoCorrFctn3DLCMSSym();
 
   AliFemtoCorrFctn3DLCMSSym& operator=(const AliFemtoCorrFctn3DLCMSSym& aCorrFctn);
 
   virtual AliFemtoString Report();
-  virtual void AddRealPair( AliFemtoPair* aPair);
-  virtual void AddMixedPair( AliFemtoPair* aPair);
+  virtual void AddRealPair(AliFemtoPair* aPair);
+  virtual void AddMixedPair(AliFemtoPair* aPair);
 
   virtual void Finish();
 
   TH3F* Numerator();
   TH3F* Denominator();
-  TH3F* NumeratorW();//Weighed by qinv
-  TH3F* DenominatorW();
-
+  TH3F* NumeratorW();   ///< Return numerator weighed by qinv
+  TH3F* DenominatorW(); ///< Return denominator weighed by qinv
 
   void WriteOutHistos();
   virtual TList* GetOutputList();
 
   void SetUseLCMS(int);
   int  GetUseLCMS();
+  virtual AliFemtoCorrFctn* Clone();
 
 private:
 
-  TH3F* fNumerator;         // numerator
-  TH3F* fDenominator;       // denominator
-  TH3F* fNumeratorW;         // numerator
-  TH3F* fDenominatorW;       // denominator
+  TH3F* fNumerator;     ///< Numerator
+  TH3F* fDenominator;   ///< Denominator
+  TH3F* fNumeratorW;    ///< Weighted numerator
+  TH3F* fDenominatorW;  ///< Weighted denominator
 
-  int    fUseLCMS;             // 0 - Use PRF, 1 - Use LCMS
+  int    fUseLCMS;      ///< 0 - Use PRF, 1 - Use LCMS
 
 #ifdef __ROOT__
   /// \cond CLASSIMP
@@ -55,8 +64,27 @@ private:
 #endif
 };
 
-inline  TH3F* AliFemtoCorrFctn3DLCMSSym::Numerator(){return fNumerator;}
-inline  TH3F* AliFemtoCorrFctn3DLCMSSym::Denominator(){return fDenominator;}
-inline  TH3F* AliFemtoCorrFctn3DLCMSSym::NumeratorW(){return fNumeratorW;}
-inline  TH3F* AliFemtoCorrFctn3DLCMSSym::DenominatorW(){return fDenominatorW;}
+inline AliFemtoCorrFctn* AliFemtoCorrFctn3DLCMSSym::Clone()
+{
+  // const keeps us out of trouble
+  const AliFemtoCorrFctn3DLCMSSym& self = *this;
+  return new AliFemtoCorrFctn3DLCMSSym(self);
+}
+
+inline  TH3F* AliFemtoCorrFctn3DLCMSSym::Numerator()
+{
+  return fNumerator;
+}
+inline  TH3F* AliFemtoCorrFctn3DLCMSSym::Denominator()
+{
+  return fDenominator;
+}
+inline  TH3F* AliFemtoCorrFctn3DLCMSSym::NumeratorW()
+{
+  return fNumeratorW;
+}
+inline  TH3F* AliFemtoCorrFctn3DLCMSSym::DenominatorW()
+{
+  return fDenominatorW;
+}
 #endif
