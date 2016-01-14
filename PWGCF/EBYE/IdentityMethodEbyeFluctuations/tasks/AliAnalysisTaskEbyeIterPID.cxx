14b743acb414eca5ccbf74c06794ae7bb24a44a4
miweber <m.weber@cern.ch>
2016-01-14 14:29:10 +0100

    Bugfix: mcEvent->fRunFastSimulation (Mesut Arslandok)

--- PWGCF/EBYE/IdentityMethodEbyeFluctuations/tasks/AliAnalysisTaskEbyeIterPID.cxx
+++ PWGCF/EBYE/IdentityMethodEbyeFluctuations/tasks/AliAnalysisTaskEbyeIterPID.cxx
@@ -785,7 +785,7 @@ void AliAnalysisTaskEbyeIterPID::UserExec(Option_t *)
   // corresponding centrality:  0     5    10    20    30     40     50    60      70    80
   AliMCEvent *mcEvent = 0x0;
   if (eventHandler) mcEvent = eventHandler->MCEvent();
-  if(mcEvent){
+  if(fRunFastSimulation){
     //
     // ========================== MC =========================
     //
