c3fc973feea9bf41ca23631704c0e7b03bc8a5a1
akubera <andrew.kubera@cern.ch>
2016-01-18 15:40:26 -0500

    PWGCF: AliFemtoNSigmaFilter - Rewrote documentation and modified code style

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoNSigmaFilter.cxx
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoNSigmaFilter.cxx
@@ -1,48 +1,40 @@
-///////////////////////////////////////////////////////////////////////////
-// AliFemtoNSigmaFilter:                                                 //
-///////////////////////////////////////////////////////////////////////////
+///
+/// \file AliFemtoNSigmaFilter.cxx
+///
 
 
 #include "AliFemtoNSigmaFilter.h"
 
 #ifdef __ROOT__
-ClassImp(AliFemtoNSigmaFilter)
+  /// \cond CLASSIMP
+  ClassImp(AliFemtoNSigmaFilter);
+  /// \endcond
 #endif
 
+#include <TMath.h>
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>  // std::sort
+
+using std::cout;
+using std::cerr;
+using std::endl;
 
 
-
-//________________________________________________________________________________________________________________
-//****************************************************************************************************************
-//________________________________________________________________________________________________________________
-
-
-//________________________________________________________________________________________________________________
 AliFemtoNSigmaFilter::AliFemtoNSigmaFilter():
 
-  fMomMaxPreferTPC(0.5),
+  fMomMaxPreferTPC(0.5)
 
-  fTPCAndTOFCutCollection(0),
-  fTPCCutCollection(0),
-  fTOFCutCollection(0),
+  , fTPCAndTOFCutCollection()
+  , fTPCCutCollection()
+  , fTOFCutCollection()
 
-  fAbsoluteMomMax(0.),
-
-  fImproperConfig(false),
-  fOverrideImproperConfig(false)
-
-
+  , fAbsoluteMomMax(0.0)
+  , fImproperConfig(false)
+  , fOverrideImproperConfig(false)
 {
-
-
-}
-
-
-
-//________________________________________________________________________________________________________________
-AliFemtoNSigmaFilter::~AliFemtoNSigmaFilter()
-{
-  /* noop */
+  // no-op
 }
 
 
@@ -55,42 +47,36 @@ void AliFemtoNSigmaFilter::CheckForOverlap(VecNSigmaCuts &aCollection)
 
   //First, make sure all defined momentum values are positive
   //Also, make sure that pMin < pMax
-  for(unsigned int i=0; i<aCollection.size(); i++)
-  {
-    if(aCollection[i].pMin < 0.) 
-    {
+  for (VecNSigmaCuts::iterator cut = aCollection.begin(); cut != aCollection.end(); ++cut) {
+    if (cut->pMin < 0.0) {
       tImproperConfig = true;
-      cerr << "WARNING:  pMin is set to a negative value!!!!!" << endl;
+      cerr << "W-AliFemtoNSigmaFilter: WARNING - pMin is set to a negative value!!!!!\n";
     }
-    if(aCollection[i].pMax < 0.) 
-    {
+    if (cut->pMax < 0.0) {
       tImproperConfig = true;
-      cerr << "WARNING:  pMax is set to a negative value!!!!!" << endl;
+      cerr << "W-AliFemtoNSigmaFilter: WARNING - pMax is set to a negative value!!!!!\n";
     }
-    if(aCollection[i].pMin > aCollection[i].pMax) 
-    {
+    if (cut->pMin >= cut->pMax) {
       tImproperConfig = true;
-      cerr << "WARNING:  pMin > pMax, making this bin useless!!!!!" << endl;
+      cerr << "W-AliFemtoNSigmaFilter: WARNING - pMin >= pMax, making this bin useless!!!!!" << endl;
     }
   }
 
-  //Now, check for overlapping bins (remembering that sorting has already occurred)
-  for(unsigned int i=1; i<aCollection.size(); i++) //note, starting at i=1, not i=0
-  {
-    if(aCollection[i].pMin < aCollection[i-1].pMax) 
-    {
+  // Now, check for overlapping bins (remembering that sorting has already occurred)
+  for (UInt_t i = 1; i < aCollection.size(); i++) { //note, starting at i=1, not i=0
+    if (aCollection[i].pMin < aCollection[i-1].pMax) {
       tImproperConfig = true;
       cerr << "WARNING:  Overlapping bins created!!!!!" << endl;
-      if(fOverrideImproperConfig)
-      {
-        cerr << "User set fOverrideImproperConfig = true.  Therefore, in the overlap region, the bin with smallest pMin will be used" << endl;
+      if (fOverrideImproperConfig) {
+        cerr << "User set fOverrideImproperConfig = true.  Therefore, in the overlap region, the bin"
+                "with smallest pMin will be used" << endl;
       }
     }
   }
 
-  if(tImproperConfig && !fOverrideImproperConfig) 
-  {
-    cerr << "Improper configuration detected, and user did not set fOverrideImproperConfig=true.  Therefore, all candidates will fail the cut" << endl;
+  if (tImproperConfig && !fOverrideImproperConfig) {
+    cerr << "Improper configuration detected, and user did not set fOverrideImproperConfig=true."
+            "Therefore, all candidates will fail the cut" << endl;
   }
 
   fImproperConfig = tImproperConfig;
@@ -114,126 +100,132 @@ void AliFemtoNSigmaFilter::SortCollection(VecNSigmaCuts &aCollection)
 //________________________________________________________________________________________________________________
 void AliFemtoNSigmaFilter::AddTPCAndTOFCut(double aPMin, double aPMax, double aNSigmaValueTPC, double aNSigmaValueTOF)
 {
-  NSigmaCut tNSigmaCut = {kTPCAndTOF,aPMin,aPMax,aNSigmaValueTPC,aNSigmaValueTOF};
+  NSigmaCut tNSigmaCut = {
+    kTPCAndTOF,
+    aPMin,
+    aPMax,
+    aNSigmaValueTPC,
+    aNSigmaValueTOF
+  };
+
   fTPCAndTOFCutCollection.push_back(tNSigmaCut);
   SortCollection(fTPCAndTOFCutCollection);
 
   //if the maximum pMax in fTPCAndTOFCutCollection is greater than fAbosoluteMomMax, reset fAbosoluteMomMax equal to it
-  if(fTPCAndTOFCutCollection.back().pMax > fAbsoluteMomMax) fAbsoluteMomMax = fTPCAndTOFCutCollection.back().pMax;
+  if (fTPCAndTOFCutCollection.back().pMax > fAbsoluteMomMax) {
+    fAbsoluteMomMax = fTPCAndTOFCutCollection.back().pMax;
+  }
 }
 
 
 //________________________________________________________________________________________________________________
 void AliFemtoNSigmaFilter::AddTPCCut(double aPMin, double aPMax, double aNSigmaValueTPC)
 {
-  NSigmaCut tNSigmaCut = {kTPC,aPMin,aPMax,aNSigmaValueTPC,0.};
+  NSigmaCut tNSigmaCut = {kTPC, aPMin, aPMax, aNSigmaValueTPC, 0.0};
   fTPCCutCollection.push_back(tNSigmaCut);
   SortCollection(fTPCCutCollection);
 
   //if the maximum pMax in fTPCCutCollection is greater than fAbosoluteMomMax, reset fAbosoluteMomMax equal to it
-  if(fTPCCutCollection.back().pMax > fAbsoluteMomMax) fAbsoluteMomMax = fTPCCutCollection.back().pMax;
+  if (fTPCCutCollection.back().pMax > fAbsoluteMomMax) {
+    fAbsoluteMomMax = fTPCCutCollection.back().pMax;
+  }
 }
 
 
 //________________________________________________________________________________________________________________
 void AliFemtoNSigmaFilter::AddTOFCut(double aPMin, double aPMax, double aNSigmaValueTOF)
 {
-  NSigmaCut tNSigmaCut = {kTOF,aPMin,aPMax,0.,aNSigmaValueTOF};
+  NSigmaCut tNSigmaCut = {kTOF, aPMin, aPMax, 0.0, aNSigmaValueTOF};
   fTOFCutCollection.push_back(tNSigmaCut);
   SortCollection(fTOFCutCollection);
 
   //if the maximum pMax in fTOFCutCollection is greater than fAbosoluteMomMax, reset fAbosoluteMomMax equal to it
-  if(fTOFCutCollection.back().pMax > fAbsoluteMomMax) fAbsoluteMomMax = fTOFCutCollection.back().pMax;
+  if (fTOFCutCollection.back().pMax > fAbsoluteMomMax) {
+    fAbsoluteMomMax = fTOFCutCollection.back().pMax;
+  }
 }
 
 
 
-//________________________________________________________________________________________________________________
-int AliFemtoNSigmaFilter::FindBinOfInterest(double aMom, VecNSigmaCuts &aCollection)
+//_______________________________________________________________________________________________________
+int AliFemtoNSigmaFilter::FindBinOfInterest(double aMom, const VecNSigmaCuts &aCollection) const
 {
-  for(unsigned int i=0; i<aCollection.size(); i++)
-  {
-    if( (aCollection[i].pMin <= aMom) && (aCollection[i].pMax > aMom) ) return i;
+  for (unsigned int i = 0; i < aCollection.size(); i++) {
+    if ( (aCollection[i].pMin <= aMom) && (aMom < aCollection[i].pMax) ) {
+      return i;
+    }
   }
 
-  return -1; //implies no cut exists in this collection for this particular momentum value
+  return -1; // implies no cut exists in this collection for this particular momentum value
 }
 
 
-//________________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________
 bool AliFemtoNSigmaFilter::Pass(double aMom, double aNSigmaTPC, double aNSigmaTOF)
 {
-  //If the user configures the AliFemtoNSigmaFilter incorrectly (in the opinion of the authors), all candidates fail
-  //However, if the user finds some novel way of using the filter with a configuration we deem bad, the fOverrideImproperConfig
-  //allows one to negate this check
-  if(fImproperConfig && !fOverrideImproperConfig) return false;
+  // If the user configures the AliFemtoNSigmaFilter incorrectly (in the opinion of the authors), all
+  // candidates fail.
+  // However, if the user finds some novel way of using the filter with a configuration we deem bad, the
+  // fOverrideImproperConfig allows one to negate this check
+  if (fImproperConfig && !fOverrideImproperConfig) {
+    return false;
+  }
 
-  //Cut out any candidates with momentum greater than the range of all of the cut collection
-  if(aMom > fAbsoluteMomMax) return false;
+  // Cut out any candidates with momentum greater than the range of all of the cut collection
+  if (aMom > fAbsoluteMomMax) {
+    return false;
+  }
 
-  //First, check if nsigma values are available (i.e. > -999)
-  bool tExistTPC = true;
-  bool tExistTOF = true;
+  // Check if N-Sigma values are available (i.e. > -999)
+  bool tExistTPC = (aNSigmaTPC > -999),
+       tExistTOF = (aNSigmaTOF > -999);
 
-  if(aNSigmaTPC < -999) tExistTPC = false;
-  if(aNSigmaTOF < -999) tExistTOF = false;
+  // If both TPC and TOF nsigma values do not exist, the cut should fail
+  if (!tExistTPC && !tExistTOF) {
+    return false;
+  }
 
-  //If both TPC and TOF nsigma values do not exist, the cut should fail
-  if(!tExistTPC && !tExistTOF) return false;
-
-  //Next, find which collections need to be considered for this momentum, and which particular momentum bins are needed
-  //If the returned value = -1, this implies no cut exists for this particular momentum value and detector
-  int iTPCAndTOF = FindBinOfInterest(aMom,fTPCAndTOFCutCollection);
-  int iTPC = FindBinOfInterest(aMom,fTPCCutCollection);
-  int iTOF = FindBinOfInterest(aMom,fTOFCutCollection);
+  // Next, find which collections need to be considered for this momentum, and which particular momentum
+  // bins are needed. If -1 is returned, there is no cut for this momentum & detector pair
+  const Int_t iTPCAndTOF = FindBinOfInterest(aMom, fTPCAndTOFCutCollection),
+                    iTPC = FindBinOfInterest(aMom, fTPCCutCollection),
+                    iTOF = FindBinOfInterest(aMom, fTOFCutCollection);
 
   // Check if TPCAndTOF cut exists for this momentum (iTPCAndTOF > -1)
   // and check if nsigma information is available for both (i.e. tExistTPC=true && tExistTOF=true)
   // If so, apply the cuts
-  if(iTPCAndTOF > -1 && tExistTPC && tExistTOF )
-  {
-    if(TMath::Abs(aNSigmaTPC) < fTPCAndTOFCutCollection[iTPCAndTOF].nSigmaValueTPC 
-       && TMath::Abs(aNSigmaTOF) < fTPCAndTOFCutCollection[iTPCAndTOF].nSigmaValueTOF) {return true;}
-    else {return false;}
+  if (iTPCAndTOF > -1 && tExistTPC && tExistTOF) {
+    return (TMath::Abs(aNSigmaTPC) < fTPCAndTOFCutCollection[iTPCAndTOF].nSigmaValueTPC
+         && TMath::Abs(aNSigmaTOF) < fTPCAndTOFCutCollection[iTPCAndTOF].nSigmaValueTOF);
   }
 
-  // If user has implemented separate TPC and TOF nsigma cuts for this momentum value, and both experimental values are valid, there 
-  // exists an ambiguity in which detector to use.  The ambiguity is resolved by the fMomMaxPreferTPC member.
+  // If user has implemented separate TPC and TOF nsigma cuts for this momentum value, and both
+  // experimental values are valid, there exists an ambiguity in which detector to use.  The ambiguity is
+  // resolved by the fMomMaxPreferTPC member.
   // See the documentation in the header file for a more complete description
-  else if(iTPC > -1 && tExistTPC && iTOF > -1 && tExistTOF)
-  {
-    if(aMom <= fMomMaxPreferTPC) //prefer TPC
-    {
-      if(TMath::Abs(aNSigmaTPC) < fTPCCutCollection[iTPC].nSigmaValueTPC) {return true;}
-      else {return false;}
+  else if (iTPC > -1 && tExistTPC && iTOF > -1 && tExistTOF) {
+    if (aMom <= fMomMaxPreferTPC) { //prefer TPC
+      return TMath::Abs(aNSigmaTPC) < fTPCCutCollection[iTPC].nSigmaValueTPC;
     }
-    else //prefer TOF
-    {
-      if(TMath::Abs(aNSigmaTOF) < fTOFCutCollection[iTOF].nSigmaValueTOF) {return true;}
-      else {return false;}
+    else { // prefer TOF
+      return TMath::Abs(aNSigmaTOF) < fTOFCutCollection[iTOF].nSigmaValueTOF;
     }
 
   }
 
   // Check if TPC cut exists for this momentum (iTPC > -1) and if nsigmaTPC information is available
   // If so, apply the cut
-  if(iTPC > -1 && tExistTPC)
-  {
-    if(TMath::Abs(aNSigmaTPC) < fTPCCutCollection[iTPC].nSigmaValueTPC) {return true;}
-    else {return false;}
+  if (iTPC > -1 && tExistTPC) {
+    return TMath::Abs(aNSigmaTPC) < fTPCCutCollection[iTPC].nSigmaValueTPC;
   }
 
   // Check if TOF cut exists for this momentum (iTOF > -1) and if nsigmaTOF information is available
   // If so, apply the cut
-  else if(iTOF > -1 && tExistTOF)
-  {
-    if(TMath::Abs(aNSigmaTOF) < fTOFCutCollection[iTOF].nSigmaValueTOF) {return true;}
-    else {return false;}
+  else if (iTOF > -1 && tExistTOF) {
+    return TMath::Abs(aNSigmaTOF) < fTOFCutCollection[iTOF].nSigmaValueTOF;
   }
-
 
   // If user does not define any nsigma cuts for the particular momentum of interest, return false
   return false;
 
 }
-
