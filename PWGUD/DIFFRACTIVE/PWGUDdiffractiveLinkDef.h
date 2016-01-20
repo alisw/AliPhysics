4687e13764d1287a854785fcb5ce7a85f236ba4e
cmayer <Christoph.Mayer@cern.ch>
2016-01-20 14:06:07 +0100

    added yet another analysis task for double-gap events

--- PWGUD/DIFFRACTIVE/PWGUDdiffractiveLinkDef.h
+++ PWGUD/DIFFRACTIVE/PWGUDdiffractiveLinkDef.h
@@ -17,5 +17,10 @@
 #pragma link C++ class AliCDMesonUtilsStripped+;
 #pragma link C++ class AliCDMesonUtils+;
 #pragma link C++ class AliAnalysisTaskCPQA++;
+#pragma link C++ class AliAnalysisTaskDG++;
+#pragma link C++ class AliAnalysisTaskDG::TreeData++;
+#pragma link C++ class AliAnalysisTaskDG::TrackData++;
+#pragma link C++ class AliAnalysisTaskDG::EventInfo++;
+#pragma link C++ class AliAnalysisTaskDG::ADV0++;
 
 #endif
