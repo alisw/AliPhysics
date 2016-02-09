f548444fe93ea4198df044bb192563766063f8a9
fbock <friederike.bock@cern.ch>
2016-02-09 17:56:15 +0100

    fixed error in Calo_pp Addtask

--- PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
@@ -289,7 +289,7 @@ void AddTask_GammaCalo_pp(  Int_t     trainConfig                   = 1,        
   // LHC13g  
   } else if (trainConfig == 60){  // EMCAL clusters, EMCEGA triggers
     cuts.AddCut("00000113","1111121063032220000","0163103100000050"); 
-    cuts.AddCut("00000013","1111121063032220000","0163103100000050");  without pile-up correction
+    cuts.AddCut("00000013","1111121063032220000","0163103100000050"); // without pile-up correction
     cuts.AddCut("00052013","1111121063032220000","0163103100000050"); // EMC7
     cuts.AddCut("00083013","1111121063032220000","0163103100000050"); // EMCEG1,
     cuts.AddCut("00085013","1111121063032220000","0163103100000050"); // EMCEG2,
