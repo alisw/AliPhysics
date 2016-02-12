00797caf30dfd71cd06d906ce3691400992706b2
shahoian <ruben.shahoyan@cern.ch>
2016-02-13 00:14:43 +0100

    Converted TStatTookit from class to namespace with templated methods
    Most of methods adapted for TVectorF and TVectorD via TVectorT template.
    MakeDistortionMapFast fill write a tree with Float_t branches instead of Double_t

--- STAT/STATLinkDef.h
+++ STAT/STATLinkDef.h
@@ -7,6 +7,8 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ nestedtypedefs;
+
 #pragma link C++ class TKDInterpolatorBase+;
 #pragma link C++ class TKDNodeInfo+;
 #pragma link C++ class TKDNodeInfo::TKDNodeDraw+;
@@ -15,9 +17,37 @@
 #pragma link C++ class TKDSpline+;
 
 #pragma link C++ class AliTMinuitToolkit+;
-#pragma link C++ class TStatToolkit+;
 #pragma link C++ class AliNDLocalRegression+;
 
 #pragma link C++ class AliExternalInfo+;
+
+#pragma link C++ namespace TStatToolkit;
+
+/*
+// RS At the moment is not recognized by the CINT...
+#pragma link C++ function TStatToolkit::GetMinMax(const Double_t*, Long64_t, double &, double &);
+#pragma link C++ function TStatToolkit::GetMinMax(const Float_t*, Long64_t, double &, double &);
+#pragma link C++ function TStatToolkit::GetMinMax(const Int_t*, Long64_t, double &, double &);
+
+#pragma link C++ function TStatToolkit::GetMinMaxMean(const Double_t*, Long64_t, double &, double &, double &);
+#pragma link C++ function TStatToolkit::GetMinMaxMean(const Float_t*, Long64_t, double &, double &, double &);
+#pragma link C++ function TStatToolkit::GetMinMaxMean(const Int_t*, Long64_t, double &, double &, double&);
+
+#pragma link C++ function TStatToolkit::TruncatedMean(const TH1*, TVectorF *,Float_t,Float_t,Bool_t);
+#pragma link C++ function TStatToolkit::TruncatedMean(const TH1*, TVectorD *,Float_t,Float_t,Bool_t);
+
+#pragma link C++ function TStatToolkit::LTMHisto(TH1*,TVectorF &,Float_t);
+#pragma link C++ function TStatToolkit::LTMHisto(TH1*,TVectorD &,Float_t);
+
+#pragma link C++ function TStatToolkit::LTM(TH1*,TVectorF*,Float_t,Bool_t);
+#pragma link C++ function TStatToolkit::LTM(TH1*,TVectorD*,Float_t,Bool_t);
+
+#pragma link C++ function TStatToolkit::FitGaus(TH1*,TVectorF*,TMatrixF*,Float_t,Float_t,Bool_t);
+#pragma link C++ function TStatToolkit::FitGaus(TH1*,TVectorD*,TMatrixD*,Float_t,Float_t,Bool_t);
+
+#pragma link C++ function TStatToolkit::FitGaus(Float_t*,Int_t,Float_t,Float_t,TVectorF*,TMatrixF*,Bool_t);
+#pragma link C++ function TStatToolkit::FitGaus(Float_t*,Int_t,Float_t,Float_t,TVectorD*,TMatrixD*,Bool_t);
+
+*/
 
 #endif
--- STAT/TStatToolkit.cxx
+++ STAT/TStatToolkit.cxx
@@ -22,60 +22,12 @@
 //
 //
 /////////////////////////////////////////////////////////////////////////
-#include "TMath.h"
-#include "Riostream.h"
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TH3.h"
-#include "THnBase.h"
-#include "TF1.h"
-#include "TTree.h"
-#include "TChain.h"
-#include "TObjString.h"
-#include "TLinearFitter.h"
-#include "TGraph2D.h"
-#include "TGraph.h"
-#include "TGraphErrors.h"
-#include "TMultiGraph.h"
-#include "TCanvas.h"
-#include "TLatex.h"
-#include "TCut.h"
-#include "THashList.h"
-#include "TFitResultPtr.h"
-#include "TFitResult.h"
-//
-// includes neccessary for test functions
-//
-#include "TSystem.h"
-#include "TRandom.h"
-#include "TStopwatch.h"
-#include "TTreeStream.h"
-#include "AliSysInfo.h"
 #include "TStopwatch.h"
 #include "TStatToolkit.h"
-#include <limits>
 
 using std::cout;
 using std::cerr;
 using std::endl;
-
- 
-ClassImp(TStatToolkit) // Class implementation to enable ROOT I/O
- 
-TStatToolkit::TStatToolkit() : TObject()
-{
-  //
-  // Default constructor
-  //
-}
-///////////////////////////////////////////////////////////////////////////
-TStatToolkit::~TStatToolkit()
-{
-  //
-  // Destructor
-  // 
-}
-
 
 //_____________________________________________________________________________
 void TStatToolkit::EvaluateUni(Int_t nvectors, Double_t *data, Double_t &mean
@@ -237,78 +189,6 @@ Int_t TStatToolkit::Freq(Int_t n, const Int_t *inlist
 
 }
 
-//___TStatToolkit__________________________________________________________________________
-void TStatToolkit::TruncatedMean(const TH1 * his, TVectorD *param, Float_t down, Float_t up, Bool_t verbose){
-  //
-  //
-  //
-  Int_t nbins    = his->GetNbinsX();
-  Float_t nentries = his->GetEntries();
-  Float_t sum      =0;
-  Float_t mean   = 0;
-  Float_t sigma2 = 0;
-  Float_t ncumul=0;  
-  for (Int_t ibin=1;ibin<nbins; ibin++){
-    ncumul+= his->GetBinContent(ibin);
-    Float_t fraction = Float_t(ncumul)/Float_t(nentries);
-    if (fraction>down && fraction<up){
-      sum+=his->GetBinContent(ibin);
-      mean+=his->GetBinCenter(ibin)*his->GetBinContent(ibin);
-      sigma2+=his->GetBinCenter(ibin)*his->GetBinCenter(ibin)*his->GetBinContent(ibin);      
-    }
-  }
-  mean/=sum;
-  sigma2= TMath::Sqrt(TMath::Abs(sigma2/sum-mean*mean));
-  if (param){
-    (*param)[0] = his->GetMaximum();
-    (*param)[1] = mean;
-    (*param)[2] = sigma2;
-    
-  }
-  if (verbose)  printf("Mean\t%f\t Sigma2\t%f\n", mean,sigma2);
-}
-
-void TStatToolkit::LTM(TH1 * his, TVectorD *param , Float_t fraction,  Bool_t verbose){
-  //
-  // LTM : Trimmed mean on histogram - Modified version for binned data
-  //
-  // Robust statistic to estimate properties of the distribution
-  // See http://en.wikipedia.org/w/index.php?title=Trimmed_estimator&oldid=582847999
-  //
-  // New faster version is under preparation
-  //
-  if (!param) return;
-  (*param)[0]=0;
-  (*param)[1]=0;
-  (*param)[2]=0;  
-  Int_t nbins    = his->GetNbinsX();
-  Int_t nentries = (Int_t)his->GetEntries();
-  if (nentries<=0) return;
-  Double_t *data  = new Double_t[nentries];
-  Int_t npoints=0;
-  for (Int_t ibin=1;ibin<nbins; ibin++){
-    Double_t entriesI = his->GetBinContent(ibin);
-    //Double_t xcenter= his->GetBinCenter(ibin);
-    Double_t x0 =  his->GetXaxis()->GetBinLowEdge(ibin);
-    Double_t w  =  his->GetXaxis()->GetBinWidth(ibin);
-    for (Int_t ic=0; ic<entriesI; ic++){
-      if (npoints<nentries){
-	data[npoints]= x0+w*Double_t((ic+0.5)/entriesI);
-	npoints++;
-      }
-    }
-  }
-  Double_t mean, sigma;  
-  Int_t npoints2=TMath::Min(Int_t(fraction*Float_t(npoints)),npoints-1);
-  npoints2=TMath::Max(Int_t(0.5*Float_t(npoints)),npoints2);
-  TStatToolkit::EvaluateUni(npoints, data, mean,sigma,npoints2);
-  delete [] data;
-  if (verbose)  printf("Mean\t%f\t Sigma2\t%f\n", mean,sigma);if (param){
-    (*param)[0] = his->GetMaximum();
-    (*param)[1] = mean;
-    (*param)[2] = sigma;    
-  }
-}
 
 
 void TStatToolkit::MedianFilter(TH1 * his1D, Int_t nmedian){
@@ -334,359 +214,6 @@ void TStatToolkit::MedianFilter(TH1 * his1D, Int_t nmedian){
   }  
 }
 
-Bool_t TStatToolkit::LTMHisto(TH1 *his1D, TVectorD &params , Float_t fraction){
-  //
-  // LTM : Trimmed mean on histogram - Modified version for binned data
-  // 
-  // Robust statistic to estimate properties of the distribution
-  // To handle binning error special treatment
-  // for definition of unbinned data see:
-  //     http://en.wikipedia.org/w/index.php?title=Trimmed_estimator&oldid=582847999
-  //
-  // Function parameters:
-  //     his1D   - input histogram
-  //     params  - vector with parameters
-  //             - 0 - area
-  //             - 1 - mean
-  //             - 2 - rms 
-  //             - 3 - error estimate of mean
-  //             - 4 - error estimate of RMS
-  //             - 5 - first accepted bin position
-  //             - 6 - last accepted  bin position
-  //
-  Int_t nbins    = his1D->GetNbinsX();
-  Int_t nentries = (Int_t)his1D->GetEntries();
-  const Double_t kEpsilon=0.0000000001;
-
-  if (nentries<=0) return 0;
-  if (fraction>1) fraction=0;
-  if (fraction<0) return 0;
-  TVectorD vectorX(nbins);
-  TVectorD vectorMean(nbins);
-  TVectorD vectorRMS(nbins);
-  Double_t sumCont=0;
-  for (Int_t ibin0=1; ibin0<=nbins; ibin0++) sumCont+=his1D->GetBinContent(ibin0);
-  //
-  Double_t minRMS=his1D->GetRMS()*10000;
-  Int_t maxBin=0;
-  //
-  for (Int_t ibin0=1; ibin0<nbins; ibin0++){
-    Double_t sum0=0, sum1=0, sum2=0;
-    Int_t ibin1=ibin0;
-    for ( ibin1=ibin0; ibin1<nbins; ibin1++){
-      Double_t cont=his1D->GetBinContent(ibin1);
-      Double_t x= his1D->GetBinCenter(ibin1);
-      sum0+=cont;
-      sum1+=cont*x;
-      sum2+=cont*x*x;
-      if ( (ibin0!=ibin1) && sum0>=fraction*sumCont) break;
-    }
-    vectorX[ibin0]=his1D->GetBinCenter(ibin0);
-    if (sum0<fraction*sumCont) continue;
-    //
-    // substract fractions of bin0 and bin1 to keep sum0=fration*sumCont
-    //
-    Double_t diff = sum0-fraction*sumCont;
-    Double_t mean = (sum0>0) ? sum1/sum0:0;
-    //
-    Double_t x0=his1D->GetBinCenter(ibin0);
-    Double_t x1=his1D->GetBinCenter(ibin1);
-    Double_t y0=his1D->GetBinContent(ibin0);
-    Double_t y1=his1D->GetBinContent(ibin1);
-    //
-    Double_t d = y0+y1-diff;    //enties to keep 
-    Double_t w0=0,w1=0;
-    if (y0<=kEpsilon&&y1>kEpsilon){
-      w1=d/y1;
-    } 
-    if (y1<=kEpsilon&&y0>kEpsilon){
-      w0=d/y0;
-    }
-    if (y0>kEpsilon && y1>kEpsilon && x1>x0  ){
-      w0 = (d*(x1-mean))/((x1-x0)*y0);
-      w1 = (d-y0*w0)/y1;
-      //
-      if (w0>1) {w1+=(w0-1)*y0/y1; w0=1;}
-      if (w1>1) {w0+=(w1-1)*y1/y0; w1=1;}
-    }  
-    if ( (x1>x0) &&TMath::Abs(y0*w0+y1*w1-d)>kEpsilon*sum0){
-      printf(" TStatToolkit::LTMHisto error\n");
-    }
-    sum0-=y0+y1;
-    sum1-=y0*x0;
-    sum1-=y1*x1;
-    sum2-=y0*x0*x0;
-    sum2-=y1*x1*x1;
-    //
-    Double_t xx0=his1D->GetXaxis()->GetBinUpEdge(ibin0)-0.5*w0*his1D->GetBinWidth(ibin0);
-    Double_t xx1=his1D->GetXaxis()->GetBinLowEdge(ibin1)+0.5*w1*his1D->GetBinWidth(ibin1);
-    sum0+=y0*w0+y1*w1;
-    sum1+=y0*w0*xx0;
-    sum1+=y1*w1*xx1;
-    sum2+=y0*w0*xx0*xx0;
-    sum2+=y1*w1*xx1*xx1;
-
-    //
-    // choose the bin with smallest rms
-    //
-    if (sum0>0){
-      vectorMean[ibin0]=sum1/sum0;
-      vectorRMS[ibin0]=TMath::Sqrt(TMath::Abs(sum2/sum0-vectorMean[ibin0]*vectorMean[ibin0]));
-      if (vectorRMS[ibin0]<minRMS){
-	minRMS=vectorRMS[ibin0];
-	params[0]=sum0;
-	params[1]=vectorMean[ibin0];
-	params[2]=vectorRMS[ibin0];
-	params[3]=vectorRMS[ibin0]/TMath::Sqrt(sumCont*fraction);
-	params[4]=0; // what is the formula for error of RMS???
-	params[5]=ibin0;
-	params[6]=ibin1;
-	params[7]=his1D->GetBinCenter(ibin0);
-	params[8]=his1D->GetBinCenter(ibin1);
-	maxBin=ibin0;
-      }
-    }else{
-      break;
-    }
-  }
-  return kTRUE;
-}
-
-
-Double_t  TStatToolkit::FitGaus(TH1* his, TVectorD *param, TMatrixD */*matrix*/, Float_t xmin, Float_t xmax, Bool_t verbose){
-  //
-  //  Fit histogram with gaussian function
-  //  
-  //  Prameters:
-  //       return value- chi2 - if negative ( not enough points)
-  //       his        -  input histogram
-  //       param      -  vector with parameters 
-  //       xmin, xmax -  range to fit - if xmin=xmax=0 - the full histogram range used
-  //  Fitting:
-  //  1. Step - make logarithm
-  //  2. Linear  fit (parabola) - more robust - always converge
-  //  3. In case of small statistic bins are averaged
-  //  
-  static TLinearFitter fitter(3,"pol2");
-  TVectorD  par(3);
-  TVectorD  sigma(3);
-  TMatrixD mat(3,3);
-  if (his->GetMaximum()<4) return -1;  
-  if (his->GetEntries()<12) return -1;  
-  if (his->GetRMS()<mat.GetTol()) return -1;
-  Float_t maxEstimate   = his->GetEntries()*his->GetBinWidth(1)/TMath::Sqrt((TMath::TwoPi()*his->GetRMS()));
-  Int_t dsmooth = TMath::Nint(6./TMath::Sqrt(maxEstimate));
-
-  if (maxEstimate<1) return -1;
-  Int_t nbins    = his->GetNbinsX();
-  Int_t npoints=0;
-  //
-
-
-  if (xmin>=xmax){
-    xmin = his->GetXaxis()->GetXmin();
-    xmax = his->GetXaxis()->GetXmax();
-  }
-  for (Int_t iter=0; iter<2; iter++){
-    fitter.ClearPoints();
-    npoints=0;
-    for (Int_t ibin=1;ibin<nbins+1; ibin++){
-      Int_t countB=1;
-      Float_t entriesI =  his->GetBinContent(ibin);
-      for (Int_t delta = -dsmooth; delta<=dsmooth; delta++){
-	if (ibin+delta>1 &&ibin+delta<nbins-1){
-	  entriesI +=  his->GetBinContent(ibin+delta);
-	  countB++;
-	}
-      }
-      entriesI/=countB;
-      Double_t xcenter= his->GetBinCenter(ibin);
-      if (xcenter<xmin || xcenter>xmax) continue;
-      Double_t error=1./TMath::Sqrt(countB);
-      Float_t   cont=2;
-      if (iter>0){
-	if (par[0]+par[1]*xcenter+par[2]*xcenter*xcenter>20) return 0;
-	cont = TMath::Exp(par[0]+par[1]*xcenter+par[2]*xcenter*xcenter);
-	if (cont>1.) error = 1./TMath::Sqrt(cont*Float_t(countB));
-      }
-      if (entriesI>1&&cont>1){
-	fitter.AddPoint(&xcenter,TMath::Log(Float_t(entriesI)),error);
-	npoints++;
-      }
-    }  
-    if (npoints>3){
-      fitter.Eval();
-      fitter.GetParameters(par);
-    }else{
-      break;
-    }
-  }
-  if (npoints<=3){
-    return -1;
-  }
-  fitter.GetParameters(par);
-  fitter.GetCovarianceMatrix(mat);
-  if (TMath::Abs(par[1])<mat.GetTol()) return -1;
-  if (TMath::Abs(par[2])<mat.GetTol()) return -1;
-  Double_t chi2 = fitter.GetChisquare()/Float_t(npoints);
-  //fitter.GetParameters();
-  if (!param)  param  = new TVectorD(3);
-  // if (!matrix) matrix = new TMatrixD(3,3); // Covariance matrix to be implemented
-  (*param)[1] = par[1]/(-2.*par[2]);
-  (*param)[2] = 1./TMath::Sqrt(TMath::Abs(-2.*par[2]));
-  (*param)[0] = TMath::Exp(par[0]+ par[1]* (*param)[1] +  par[2]*(*param)[1]*(*param)[1]);
-  if (verbose){
-    par.Print();
-    mat.Print();
-    param->Print();
-    printf("Chi2=%f\n",chi2);
-    TF1 * f1= new TF1("f1","[0]*exp(-(x-[1])^2/(2*[2]*[2]))",his->GetXaxis()->GetXmin(),his->GetXaxis()->GetXmax());
-    f1->SetParameter(0, (*param)[0]);
-    f1->SetParameter(1, (*param)[1]);
-    f1->SetParameter(2, (*param)[2]);    
-    f1->Draw("same");
-  }
-  return chi2;
-}
-
-Double_t  TStatToolkit::FitGaus(Float_t *arr, Int_t nBins, Float_t xMin, Float_t xMax, TVectorD *param, TMatrixD */*matrix*/, Bool_t verbose){
-  //
-  //  Fit histogram with gaussian function
-  //  
-  //  Prameters:
-  //     nbins: size of the array and number of histogram bins
-  //     xMin, xMax: histogram range
-  //     param: paramters of the fit (0-Constant, 1-Mean, 2-Sigma)
-  //     matrix: covariance matrix -- not implemented yet, pass dummy matrix!!!
-  //
-  //  Return values:
-  //    >0: the chi2 returned by TLinearFitter
-  //    -3: only three points have been used for the calculation - no fitter was used
-  //    -2: only two points have been used for the calculation - center of gravity was uesed for calculation
-  //    -1: only one point has been used for the calculation - center of gravity was uesed for calculation
-  //    -4: invalid result!!
-  //
-  //  Fitting:
-  //  1. Step - make logarithm
-  //  2. Linear  fit (parabola) - more robust - always converge
-  //  
-  static TLinearFitter fitter(3,"pol2");
-  static TMatrixD mat(3,3);
-  static Double_t kTol = mat.GetTol();
-  fitter.StoreData(kFALSE);
-  fitter.ClearPoints();
-  TVectorD  par(3);
-  TVectorD  sigma(3);
-  TMatrixD matA(3,3);
-  TMatrixD b(3,1);
-  Float_t rms = TMath::RMS(nBins,arr);
-  Float_t max = TMath::MaxElement(nBins,arr);
-  Float_t binWidth = (xMax-xMin)/(Float_t)nBins;
-
-  Float_t meanCOG = 0;
-  Float_t rms2COG = 0;
-  Float_t sumCOG  = 0;
-
-  Float_t entries = 0;
-  Int_t nfilled=0;
-
-  for (Int_t i=0; i<nBins; i++){
-      entries+=arr[i];
-      if (arr[i]>0) nfilled++;
-  }
-
-  if (max<4) return -4;
-  if (entries<12) return -4;
-  if (rms<kTol) return -4;
-
-  Int_t npoints=0;
-  //
-
-  //
-  for (Int_t ibin=0;ibin<nBins; ibin++){
-      Float_t entriesI = arr[ibin];
-    if (entriesI>1){
-      Double_t xcenter = xMin+(ibin+0.5)*binWidth;
-      
-      Float_t error    = 1./TMath::Sqrt(entriesI);
-      Float_t val = TMath::Log(Float_t(entriesI));
-      fitter.AddPoint(&xcenter,val,error);
-      if (npoints<3){
-	  matA(npoints,0)=1;
-	  matA(npoints,1)=xcenter;
-	  matA(npoints,2)=xcenter*xcenter;
-	  b(npoints,0)=val;
-	  meanCOG+=xcenter*entriesI;
-	  rms2COG +=xcenter*entriesI*xcenter;
-	  sumCOG +=entriesI;
-      }
-      npoints++;
-    }
-  }
-
-  
-  Double_t chi2 = 0;
-  if (npoints>=3){
-      if ( npoints == 3 ){
-	  //analytic calculation of the parameters for three points
-	  matA.Invert();
-	  TMatrixD res(1,3);
-	  res.Mult(matA,b);
-	  par[0]=res(0,0);
-	  par[1]=res(0,1);
-	  par[2]=res(0,2);
-          chi2 = -3.;
-      } else {
-          // use fitter for more than three points
-	  fitter.Eval();
-	  fitter.GetParameters(par);
-	  fitter.GetCovarianceMatrix(mat);
-	  chi2 = fitter.GetChisquare()/Float_t(npoints);
-      }
-      if (TMath::Abs(par[1])<kTol) return -4;
-      if (TMath::Abs(par[2])<kTol) return -4;
-
-      if (!param)  param  = new TVectorD(3);
-      //if (!matrix) matrix = new TMatrixD(3,3);  // !!!!might be a memory leek. use dummy matrix pointer to call this function! // Covariance matrix to be implemented
-
-      (*param)[1] = par[1]/(-2.*par[2]);
-      (*param)[2] = 1./TMath::Sqrt(TMath::Abs(-2.*par[2]));
-      Double_t lnparam0 = par[0]+ par[1]* (*param)[1] +  par[2]*(*param)[1]*(*param)[1];
-      if ( lnparam0>307 ) return -4;
-      (*param)[0] = TMath::Exp(lnparam0);
-      if (verbose){
-	  par.Print();
-	  mat.Print();
-	  param->Print();
-	  printf("Chi2=%f\n",chi2);
-	  TF1 * f1= new TF1("f1","[0]*exp(-(x-[1])^2/(2*[2]*[2]))",xMin,xMax);
-	  f1->SetParameter(0, (*param)[0]);
-	  f1->SetParameter(1, (*param)[1]);
-	  f1->SetParameter(2, (*param)[2]);
-	  f1->Draw("same");
-      }
-      return chi2;
-  }
-
-  if (npoints == 2){
-      //use center of gravity for 2 points
-      meanCOG/=sumCOG;
-      rms2COG /=sumCOG;
-      (*param)[0] = max;
-      (*param)[1] = meanCOG;
-      (*param)[2] = TMath::Sqrt(TMath::Abs(meanCOG*meanCOG-rms2COG));
-      chi2=-2.;
-  }
-  if ( npoints == 1 ){
-      meanCOG/=sumCOG;
-      (*param)[0] = max;
-      (*param)[1] = meanCOG;
-      (*param)[2] = binWidth/TMath::Sqrt(12);
-      chi2=-1.;
-  }
-  return chi2;
-
-}
 
 
 Float_t TStatToolkit::GetCOG(const Short_t *arr, Int_t nBins, Float_t xMin, Float_t xMax, Float_t *rms, Float_t *sum)
@@ -1422,13 +949,11 @@ TGraphErrors * TStatToolkit::MakeGraphErrors(TTree * tree, const char * expr, co
   // expr   - examp 
   const Int_t entries =  tree->Draw(expr,cut,"goff");
   if (entries<=0) {
-    TStatToolkit t;
-    t.Error("TStatToolkit::MakeGraphError","Empty or Not valid expression (%s) or cut *%s)", expr,cut);
+    ::Error("TStatToolkit::MakeGraphError","Empty or Not valid expression (%s) or cut *%s)", expr,cut);
     return 0;
   }
   if (  tree->GetV2()==0){
-    TStatToolkit t;
-    t.Error("TStatToolkit::MakeGraphError","Not valid expression (%s) ", expr);
+    ::Error("TStatToolkit::MakeGraphError","Not valid expression (%s) ", expr);
     return 0;
   }
   TGraphErrors * graph=0;
@@ -1464,8 +989,7 @@ TGraph * TStatToolkit::MakeGraphSparse(TTree * tree, const char * expr, const ch
   //
   const Int_t entries = tree->Draw(expr,cut,"goff");
   if (entries<=0) {
-    TStatToolkit t;
-    t.Error("TStatToolkit::MakeGraphSparse","Empty or Not valid expression (%s) or cut (%s)", expr, cut);
+    ::Error("TStatToolkit::MakeGraphSparse","Empty or Not valid expression (%s) or cut (%s)", expr, cut);
     return 0;
   }
   //  TGraph * graph = (TGraph*)gPad->GetPrimitive("Graph"); // 2D
@@ -2546,7 +2070,7 @@ void TStatToolkit::MakeDistortionMapFast(THnBase * histo, TTreeSRedirector *pcst
   sw.Start();
   int dimVar=1, dimVarID = axOrd[dimVar];
   //
-  TVectorD  vecLTM(9);
+  TVectorF  vecLTM(9);
   while(1) {
     //
     double dimVarCen = histo->GetAxis(dimVarID)->GetBinCenter(idx[dimVarID]); // center of currently varied bin
@@ -2611,7 +2135,7 @@ void TStatToolkit::MakeDistortionMapFast(THnBase * histo, TTreeSRedirector *pcst
     if (verbose>0) {for (int i=0;i<ndim;i++) printf("%d ",idx[i]); printf(" | central bin fit\n");}
     // 
     // >> ------------- do fit
-    double mean=0,mom2=0,rms=0,m3=0, m4=0, nrm=0,meanG=0,rmsG=0,chi2G=0,maxVal=0,entriesG=0,mean0=0, rms0=0, curt0=0;
+    float mean=0,mom2=0,rms=0,m3=0, m4=0, nrm=0,meanG=0,rmsG=0,chi2G=0,maxVal=0,entriesG=0,mean0=0, rms0=0, curt0=0;
     hfit->Reset();
     for (int ip=tgtNb;ip--;) {
       //grafFit.SetPoint(ip,binX[ip],binY[ip]);
@@ -2632,7 +2156,7 @@ void TStatToolkit::MakeDistortionMapFast(THnBase * histo, TTreeSRedirector *pcst
     
 
     Int_t nbins1D=hfit->GetNbinsX();
-    Double_t binMedian=0;
+    Float_t binMedian=0;
     Double_t limits[2]={hfit->GetBinCenter(1), hfit->GetBinCenter(nbins1D)};
     if (nrm>5) {
       TStatToolkit::LTMHisto(hfit, vecLTM, 0.8); 
@@ -2772,33 +2296,4 @@ void TStatToolkit::MakeDistortionMapFast(THnBase * histo, TTreeSRedirector *pcst
     if (i && (i%prc)==0) printf("Done %d%%\n",int(float(100*i)/prc));
   }
   */
-}
-
-//___________________________________________________________
-void TStatToolkit::GetMinMax(const double* arr, int n, double &minVal, double &maxVal)
-{
-  // find min, max entries in the array in a single loop
-  minVal =  DBL_MAX;
-  maxVal = -DBL_MAX;
-  for (int i=n;i--;) {
-    double val = arr[i];
-    if (val<minVal) minVal = val;
-    if (val>maxVal) maxVal = val;
-  }
-}
-
-//___________________________________________________________
-void TStatToolkit::GetMinMaxMean(const double* arr, int n, double &minVal, double &maxVal, double &meanVal)
-{
-  // find min, max entries in the array in a single loop
-  minVal =  DBL_MAX;
-  maxVal = -DBL_MAX;
-  meanVal = 0;
-  for (int i=n;i--;) {
-    double val = arr[i];
-    if (val<minVal) minVal = val;
-    if (val>maxVal) maxVal = val;
-    meanVal += val;
-  }
-  if (n) meanVal /= n;
 }
