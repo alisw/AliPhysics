d69cc422d47b9231ca2cad53aa2995a736750412
jgronefe <j.gronefeld@cern.ch>
2016-02-15 13:42:36 +0100

    Updating dNdPt-PbPb Task

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTask_dNdPtPbPb_TPCITS.C
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTask_dNdPtPbPb_TPCITS.C
@@ -1,4 +1,4 @@
-void AddTask_dNdPtPbPb_TPCITS(Int_t cutMode =222 , char *mode =""){
+void AddTask_dNdPtPbPb_TPCITS(Int_t cutMode =222 , char *mode ="default"){
   
  
   TString stParticleMode(mode);
@@ -62,7 +62,6 @@ void AddTask_dNdPtPbPb_TPCITS(Int_t cutMode =222 , char *mode =""){
   else if (stParticleMode.Contains("Plus")){fdNdPtAnalysisPbPb->SetParticleMode(AlidNdPtHelper::kPlus);}
   else if (stParticleMode.Contains("Minus")){fdNdPtAnalysisPbPb->SetParticleMode(AlidNdPtHelper::kMinus);}
   else{fdNdPtAnalysisPbPb->SetParticleMode(AlidNdPtHelper::kAllPart);}
-  
   // Change binning
   const Int_t ptNbins = 81;
   Double_t bins[82] = {0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0, 30.0, 32.0, 34.0, 36.0, 40.0, 45.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 180.0, 200.0};
@@ -85,7 +84,11 @@ void AddTask_dNdPtPbPb_TPCITS(Int_t cutMode =222 , char *mode =""){
   mgr->ConnectInput(task, 0, cinput);
 
   TString stContainerName;
-  stContainerName = Form("dNdPt_PbPb_%d%c",cutMode,stParticleMode);
+  stContainerName = Form("dNdPt_PbPb_%d",cutMode);
+  if(!stParticleMode.Contains("default")) {
+    stContainerName = stContainerName + "_" +stParticleMode;
+  }
+    
   AliAnalysisDataContainer *coutput = mgr->CreateContainer(stContainerName,TList::Class(),AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName());
     
   mgr->ConnectOutput(task, 1, coutput);
