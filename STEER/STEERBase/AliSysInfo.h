87248753f25bdc34efdbb046bffdbc29cb3defcd
mivanov <marian.ivanov@cern.ch>
2016-02-17 17:04:50 +0100

    PWGPP-168 - removing old description

--- STEER/STEERBase/AliSysInfo.h
+++ STEER/STEERBase/AliSysInfo.h
@@ -3,15 +3,6 @@
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
-//-------------------------------------------------------------------------
-// This is the class which is to be used during the writing of
-// simulated raw data (DDL files format).
-// It is using the root functionality in order to deal correctly
-// with little/big endian issue. By convention the detector raw
-// data payload is stored always with little endian (this corresponds
-// to the real life situation when the detector data is coming from
-// the hardware).
-//-------------------------------------------------------------------------
 
 #include <TObject.h>
 class TStopwatch;
