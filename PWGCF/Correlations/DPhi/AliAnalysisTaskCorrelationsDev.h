4be1a0fe17c38d77bea04b23aa19f1e888386bc7
jgrosseo <jgrosseo@cern.ch>
2016-02-22 16:45:12 +0100

    possibility to create custom tree

--- PWGCF/Correlations/DPhi/AliAnalysisTaskCorrelationsDev.h
+++ PWGCF/Correlations/DPhi/AliAnalysisTaskCorrelationsDev.h
@@ -16,11 +16,15 @@
 #include "AliAnalysisTaskSE.h"
 #include "TString.h"
 
+class TNtuple;
+
 class AliAnalyseLeadingTrackUE;
 
 class  AliAnalysisTaskCorrelationsDev : public AliAnalysisTaskSE
 {
 public:
+  enum { TreeMaxTracks = 10 };
+  
   AliAnalysisTaskCorrelationsDev(const char* name="AliAnalysisTaskCorrelationsDev");
   virtual           ~AliAnalysisTaskCorrelationsDev();
 
@@ -36,14 +40,17 @@ public:
 
   // track cuts
   void   SetTrackEtaCut( Double_t val )    { fTrackEtaCut = val; }
-  void   SetTrackEtaCutMin( Double_t val )    { fTrackEtaCutMin = val; }
+  void   SetTrackEtaCutMin( Double_t val ) { fTrackEtaCutMin = val; }
   void   SetPtMin(Double_t val)            { fPtMin = val; }
   void   SetFilterBit( UInt_t val )        { fFilterBit = val;  }
   void   SetTrackStatus(UInt_t status)     { fTrackStatus = status; }
 
+  void   SetEventCuts(AliAnalysisCuts* eventCuts) { fEventCuts = eventCuts; }
   void   SetEventSelectionBit( UInt_t val )        { fSelectBit = val;  }
   void   SetCentralityMethod(const char* method, Bool_t unchecked = kFALSE) { fCentralityMethod = method; fUseUncheckedCentrality = kFALSE; }
   void   SetUseNewCentralityFramework(Bool_t flag) { fUseNewCentralityFramework = flag; }
+  
+  void   SetCustomTree(TString eventConfig, TString trackConfig) { fTreeEventConfig = eventConfig.Tokenize(";"); fTreeTrackConfig = trackConfig.Tokenize(";"); }
 
 private:
   AliAnalysisTaskCorrelationsDev(const  AliAnalysisTaskCorrelationsDev &det);
@@ -52,6 +59,7 @@ private:
   // Analysis methods
   void            Initialize(); 			                // initialize some common pointer
   Double_t        GetCentrality(AliVEvent* inputEvent, TObject* mc);
+  Float_t         GetValueInterpreted(TObject* source, const char* command);
 
   AliAnalyseLeadingTrackUE*     fAnalyseUE;      //! points to class containing common analysis algorithms
   
@@ -62,6 +70,7 @@ private:
   TList*              fListOfHistos;    //  Output list of containers
 
   // Event
+  AliAnalysisCuts* fEventCuts;          // additional cuts which have to be passed
   UInt_t     fSelectBit;                // Select events according to AliAnalysisTaskJetServices bit maps
   Double_t   fZVertex;                  // Position of Vertex in Z direction
   TString    fCentralityMethod;         // Method to determine centrality
@@ -74,8 +83,14 @@ private:
   Double_t   fPtMin;                    // Min pT to start correlations
   UInt_t     fFilterBit;                // Select tracks from an specific track cut
   UInt_t     fTrackStatus;              // if non-0, the bits set in this variable are required for each track
+  
+  // custom tree
+  TNtuple*   fOutputTree;               //! custom tree output
+  Float_t*   fOutputContainer;          //! to fill the tuple 
+  TObjArray* fTreeEventConfig;          // list of fields from header kept in tree
+  TObjArray* fTreeTrackConfig;          // list of fields from track kept in tree
 
-  ClassDef(AliAnalysisTaskCorrelationsDev, 1); // Analysis task for correlation development
+  ClassDef(AliAnalysisTaskCorrelationsDev, 2); // Analysis task for correlation development
 };
 
 #endif
