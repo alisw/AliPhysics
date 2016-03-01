6250b841899f96c5d559c8a6f3aedf82bde3663b
mpuccio <maximiliano.puccio@cern.ch>
2016-03-01 16:07:10 +0100

    Modification to speedup the code (Kohei Terasaki)

--- PWGLF/NUCLEX/Exotica/NOmega/AliAnalysisTaskOmegaOmegaOX.h
+++ PWGLF/NUCLEX/Exotica/NOmega/AliAnalysisTaskOmegaOmegaOX.h
@@ -2,7 +2,7 @@
 #define ALIANALYSISTASKOMEGAOMEGAOX_H
 
 	//------------------------------------------------------------------------------------------
-	// version 1.50 (2016/02/22)
+	// version 1.60 (2016/03/01)
 	//------------------------------------------------------------------------------------------
 
 /* $Id$ */ 
@@ -13,17 +13,9 @@
 #include "AliAnalysisTaskSE.h"
 
 class TH1F;
+class AliESDtrack;
 class TClonesArray;
 class AliAODRecoCascade;
-
-/// Missing forward declarations...
-class AliESDtrack;
-class AliESDEvent;
-class AliESDVertex;
-class AliESDtrackCuts;
-class TTree;
-class AliPIDCombined;
-class AliPIDResponse;
 
 class AliAnalysisTaskOmegaOmegaOX : public AliAnalysisTaskSE 
 {
@@ -72,7 +64,6 @@ class AliAnalysisTaskOmegaOmegaOX : public AliAnalysisTaskSE
 	AliAnalysisTaskOmegaOmegaOX& operator=(const AliAnalysisTaskOmegaOmegaOX& source); 
 
 	AliESDtrackCuts  *fESDtrackCutsV0; // basic cut variables for v0's
-//	AliESDv0Cuts     *fESDCutsV0;      // V0 track cuts
 	AliESDtrackCuts  *fESDtrackCuts;   // track cuts
 
 	Bool_t PreTrackCut(AliESDtrack *track);
@@ -80,6 +71,7 @@ class AliAnalysisTaskOmegaOmegaOX : public AliAnalysisTaskSE
 	Double_t InvMassLambdaStar(Double_t MomPos[3],Double_t MomNeg[3],AliESDtrack *pos,AliESDtrack *neg,Double_t v0Return[1]);
 	Double_t GetPaFromPxPyPz(Double_t Momentum[3]);
 	Double_t GetPtFromPxPyPz(Double_t Momentum[3]);
+	Bool_t ReconstructCascade();
 
   Double_t Det(Double_t a00, Double_t a01, Double_t a10, Double_t a11) const;
   Double_t Det(Double_t a00,Double_t a01,Double_t a02,
@@ -96,7 +88,6 @@ class AliAnalysisTaskOmegaOmegaOX : public AliAnalysisTaskSE
 	AliESDEvent *fESDEvent;       // ESD event
 	Bool_t fUseMCInfo;            // Use MC info
 	AliPIDResponse *fPIDResponse; // PID response object
-	AliPIDCombined *fPIDCombined; // combined PID response object
 	Bool_t fIsEventSelected;      // flag for event selected
 	TTree *fParametersTree;       // tree of the cut parameters on output slot 1
 	TTree *fVariablesTree;        // tree of the candidate variables on output slot 2
@@ -106,22 +97,14 @@ class AliAnalysisTaskOmegaOmegaOX : public AliAnalysisTaskSE
 	AliESDVertex *fVtx1;          // reconstructed vertex from 2 tracks (new v0)
 	Double_t fBzkG;               // magnetic field value [kG]
 	Int_t fCentrality;
-	Int_t fCountMatch;
-	Int_t fCountOnlySelf;
-	Int_t fCountOnlyV0;
-	Int_t fCountLambda;
-	Int_t fCountAntiLambda;
-	Int_t fCountLambdaSelf;
-	Int_t fCountAntiLambdaSelf;
-	Int_t fCountMatchLambda;
-	Int_t fCountMatchAntiLambda;
 	Int_t fCountEvent;
 	Int_t fIsDCA;
 	Int_t fNAll;
 
 	// settings for analysis
-	Int_t fRecoTypeDB; // Reconstruction type of DiBaryon (0:All, 1:OmOm, 2:OmXi, 3:XiOm, 4:XiXi)
-	Int_t fLikeSignDB; // Like-sign of DB (0:ALL, 1:OO, 2:(Obar)(Obar))
+	Int_t fRecoTypeDB;   // Reconstruction type of DiBaryon (0:All, 1:OmOm, 2:OmXi, 3:XiOm, 4:XiXi)
+	Int_t fLikeSignDB;   // Like-sign of DB (0:ALL, 1:OO, 2:(Obar)(Obar))
+	Int_t fRecoSelfCasc; // Cascade reconstruction is made by (0:ESD class, 1:by myself)
 
 	// cut parameters for tracks
 	Double_t fReqSigmaTPC;  // TPC PIDcut sigma
