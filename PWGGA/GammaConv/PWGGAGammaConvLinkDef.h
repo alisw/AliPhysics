edaa89e13af3be1ceea042deb8647a921e2cc4af
amarin <a.marin@gsi.de>
2016-03-01 17:48:58 +0100

    Adding Material Task using only Histograms and multiple cuts

--- PWGGA/GammaConv/PWGGAGammaConvLinkDef.h
+++ PWGGA/GammaConv/PWGGAGammaConvLinkDef.h
@@ -30,6 +30,7 @@
 #pragma link C++ class AliAnalysisTaskGammaConvDalitzV1+;
 #pragma link C++ class AliAnalysisTaskConversionQA+;
 #pragma link C++ class AliAnalysisTaskMaterial+;
+#pragma link C++ class AliAnalysisTaskMaterialHistos+;
 #pragma link C++ class AliAnalysisTaskResolution+;
 #pragma link C++ class AliAnalysisTaskGammaCaloDalitzV1+;
 #pragma link C++ class AliAnalysisTaskGammaPureMC+;
