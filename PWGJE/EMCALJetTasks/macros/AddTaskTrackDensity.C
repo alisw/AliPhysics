053f5621832c1b52d7ffb551b6f0dcdbaf6e1914
mfasel <markus.fasel@cern.ch>
2016-03-03 19:21:51 +0100

    Fixes after local tests

--- PWGJE/EMCALJetTasks/macros/AddTaskTrackDensity.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskTrackDensity.C
@@ -1,4 +1,5 @@
 EMCalTriggerPtAnalysis::AliAnalysisTaskTrackDensity *AddTaskTrackDensity(
+    const char *mcparticlecontainername,
     const char *mcjetcontainername,
     const char *suffix
 ) {
@@ -7,20 +8,22 @@ EMCalTriggerPtAnalysis::AliAnalysisTaskTrackDensity *AddTaskTrackDensity(
   EMCalTriggerPtAnalysis::AliAnalysisTaskTrackDensity *densitytask = new EMCalTriggerPtAnalysis::AliAnalysisTaskTrackDensity("densitytask");
   mgr->AddTask(densitytask);
   mgr->ConnectInput(densitytask, 0, mgr->GetCommonInputContainer());
-  mgr->ConnectOutput(task, 1,
+  mgr->ConnectOutput(densitytask, 1,
       mgr->CreateContainer(Form("histos_%s", suffix),
           TList::Class(), AliAnalysisManager::kOutputContainer,
           Form("%s:TrackDensity_%s", mgr->GetCommonFileName(), suffix)));
 
   densitytask->SetIsPythia(true);
 
-  AliMCParticleContainer *partcont = densitytask->AddMCParticleContainer("MCParticlesSelected");
-  densitytask->SetMCParticleContainer("MCParticlesSelected");
+  AliMCParticleContainer *partcont = densitytask->AddMCParticleContainer(mcparticlecontainername);
+  partcont->SetName("MCParticles");
+  densitytask->SetMCParticleContainer("MCParticles");
 
   AliJetContainer *jetcont = densitytask->AddJetContainer(mcjetcontainername, AliJetContainer::kTPC, 0.4);
   jetcont->SetName("MCJetContainer");
   jetcont->SetJetPtCut(20.);
   jetcont->SetJetEtaLimits(-0.5, 0.5);
+  jetcont->ConnectParticleContainer(partcont);
   densitytask->SetMCJetContainer("MCJetContainer");
 
   return densitytask;
