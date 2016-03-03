2132ecda49c3278b262b7931e496bade852f6101
akubera <andrew.kubera@cern.ch>
2016-03-03 15:36:25 -0500

    PWGCF: AddTaskLamLam {jsalzwedel} Fixed parentheses and output directory naming

--- PWGCF/FEMTOSCOPY/macros/AddTaskLamLam.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskLamLam.C
@@ -7,23 +7,29 @@ AliAnalysisV0Lam *AddTaskLamLam(Int_t varCutType, Int_t nominalCutIndex, Bool_t 
     ::Error("AddTaskLamLam", "No analysis manager to connect to.");
     return NULL;
   }
-
+ 
   AliAnalysisV0Lam *myTask = new AliAnalysisV0Lam("LamLamTask",  varCutType, nominalCutIndex, flattenCent);
   if(!myTask) return NULL;
   mgr->AddTask(myTask);
 
   AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+  if (!cinput) {
+    ::Error("AddTaskLamLam", "No common input container.");
+    return NULL;
+  }
 
   TString containerName = "MyListVar";
   containerName += varCutType;
-
+  if(!flattenCent) containerName += "NotFlat";
+ 
   TString outputFileName = AliAnalysisManager::GetCommonFileName();
   outputFileName += ":Results";
-  
-  AliAnalysisDataContainer *coutput = mgr->CreateContainer(containerName, TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName.Data()));
+
+
+  AliAnalysisDataContainer *coutput = mgr->CreateContainer(containerName, TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName.Data());
 
   mgr->ConnectInput(myTask, 0, cinput);	
   mgr->ConnectOutput(myTask, 1, coutput);	
-  
+
   return myTask;
 }
