dafb3a61ad16e8b512c60cab7b868a1bcaf5a0c3
fbellini <fbellini@cern.ch>
2016-03-09 14:49:43 +0100

    Added options for selecting wider y ranges

--- PWGLF/RESONANCES/macros/mini/AddTaskKStarPPB.C
+++ PWGLF/RESONANCES/macros/mini/AddTaskKStarPPB.C
@@ -7,7 +7,9 @@
 ****************************************************************************/
 enum pairYCutSet { kPairDefault,
 		   kNegative,
-		   kCentral
+		   kCentral,
+		   kWide1,
+		   kWide5
                  };
 
 enum eventCutSet { kEvtDefault = 0,
@@ -194,6 +196,13 @@ AliRsnMiniAnalysisTask * AddTaskKStarPPB
     minYlab = -0.765;    maxYlab = -0.165;
   }
 
+  if (pairCutSetID==pairYCutSet::kWide1) { //|y_cm|<1
+    minYlab = -1.465;    maxYlab = 0.535;
+  }
+
+  if (pairCutSetID==pairYCutSet::kWide5) { //|y_cm|<5
+    minYlab = -5.465;    maxYlab = -4.535;
+  }
   //-------------------------------------------
   //mixing settings
   //-------------------------------------------
