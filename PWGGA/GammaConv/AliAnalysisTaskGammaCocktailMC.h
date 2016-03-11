37406d12e6410c0150019854d04d25ad28666da0
msas <mike.sas@cern.ch>
2016-03-11 12:07:06 +0100

    Adding tasks for directphoton cocktail

--- PWGGA/GammaConv/AliAnalysisTaskGammaCocktailMC.h
+++ PWGGA/GammaConv/AliAnalysisTaskGammaCocktailMC.h
@@ -22,23 +22,33 @@ class AliAnalysisTaskGammaCocktailMC : public AliAnalysisTaskSE {
     // additional functions
     void SetLogBinningXTH1(TH1* histoRebin);
     void SetLogBinningXTH2(TH2* histoRebin);
+    void SetMaxY(Double_t maxy){fMaxY = maxy;}
+    Bool_t IsMotherInList(TParticle* mother);
     
   protected:
     AliVEvent*            fInputEvent;                // current event
     AliMCEvent*           fMCEvent;                   // corresponding MC event
     AliStack*             fMCStack;                   // stack belonging to MC event
     TList*                fOutputContainer;           // Output container
+    
+    Int_t*                fParticleList;              // array with particle Pdg values
+    TString*              fParticleListNames;         // array with particle names
+    
     // histograms events
     TH1F*                 fHistNEvents;               // number of events histo
 
     // histograms mesons
-    TH2F*                 fHistPtYPi0;                //! histo for Pi0s
-    TH2F*                 fHistPtYPiPl;               //! histo for Pi+s 
-    TH2F*                 fHistPtYPiMi;               //! histo for Pi-s
-    TH2F*                 fHistPtYEta;                //! histo for Etas
-    TH2F*                 fHistPtYEtaPrim;            //! histo for EtaPrims
-    TH2F*                 fHistPtYOmega;              //! histo for Omegas
+    TH2F*                 fHistPtYGamma;              //! histo for Gammas
+    TH2F**                fHistPtYInput;              //! histo for Input particles
+    TH2F**                fHistPtYGammaSource;        //! histo for Input particles
     Int_t                 fIsMC;                      // MC flag
+    Double_t              fMaxY;                      // Max y
+    
+    TH2F*                 fHistPhiGamma;              //! histo for phi of gamma
+    TH2F**                fHistPhiInput;              //! histo for phi of input particles
+    
+    TH1I*                 fHistPtYInputRest;          //! histo for rest
+    TH1I*                 fHistPtYGammaSourceRest;    //! histo for gamma from rest
 
   private:
     AliAnalysisTaskGammaCocktailMC(const AliAnalysisTaskGammaCocktailMC&); // Prevent copy-construction
