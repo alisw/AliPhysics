2217fd6e50770d9c18771fe2a27e2b22e94862b3
fbock <friederike.bock@cern.ch>
2016-03-13 20:39:57 +0100

    fixed byline

--- PWGGA/GammaConv/AliAnalysisTaskGammaCocktailMC.cxx
+++ PWGGA/GammaConv/AliAnalysisTaskGammaCocktailMC.cxx
@@ -1,17 +1,17 @@
 /**************************************************************************
-* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
-*                                       *
-* Author: Baldo Sahlmueller, Friederike Bock                     *
-* Version 1.0                                 *
-*                                       *
-*                                                                        *
+* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved.  *
+*                                                                         *
+* Author: Friederike Bock, Mike Sas                                       *
+* Version 1.0                                                             *
+*                                                                         *
+*                                                                         *
 * Permission to use, copy, modify and distribute this software and its    *
 * documentation strictly for non-commercial purposes is hereby granted    *
 * without fee, provided that the above copyright notice appears in all    *
 * copies and that both the copyright notice and this permission notice    *
-* appear in the supporting documentation. The authors make no claims    *
-* about the suitability of this software for any purpose. It is      *
-* provided "as is" without express or implied warranty.               *
+* appear in the supporting documentation. The authors make no claims      *
+* about the suitability of this software for any purpose. It is           *
+* provided "as is" without express or implied warranty.                   *
 **************************************************************************/
 
 //////////////////////////////////////////////////////////////////
