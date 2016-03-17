d5a92572eec6bcdf040ce66ead7e0642a5e30054
wiechula <Jens.Wiechula@cern.ch>
2016-03-17 18:43:11 +0100

    add missing ;

--- PWGPP/CalibMacros/CPass1/runCalibTrain.C
+++ PWGPP/CalibMacros/CPass1/runCalibTrain.C
@@ -37,7 +37,7 @@ void runCalibTrain(Int_t runNumber, const char *inFileName = "AliESDs.root", con
   gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/CalibMacros/CPass1/AddTaskT0Analysis.C");
   //  gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/CalibMacros/CPass1/AddTaskMeanVertexCalib.C");
   gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/CalibMacros/CPass1/AddTaskSDDCalib.C"); 
-  gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/AD/AddTaskADCalib.C")
+  gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/AD/AddTaskADCalib.C");
 
   // switch off debug 
   AliLog::SetClassDebugLevel("AliESDEvent",0);
