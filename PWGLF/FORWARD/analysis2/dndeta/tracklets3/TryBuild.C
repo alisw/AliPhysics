fbc498699bb3f3a5ae9b492b28bbc92aac4e764b
cholm <Christian.Holm.Christensen@cern.ch>
2016-03-23 12:08:14 +0100

    Add weights class for when testing build

--- PWGLF/FORWARD/analysis2/dndeta/tracklets3/TryBuild.C
+++ PWGLF/FORWARD/analysis2/dndeta/tracklets3/TryBuild.C
@@ -3,7 +3,9 @@ TryBuild()
 {
   gSystem->AddIncludePath("-I${ALICE_ROOT}/include -I${ALICE_PHYSICS}/include");
   gROOT->LoadMacro("AliAODTracklet.C+g");
+  gROOT->LoadMacro("AliTrackletWeights.C+g");
   gROOT->LoadMacro("AliTrackletAODUtils.C+g");
   gROOT->LoadMacro("AliTrackletAODTask.C+g");
   gROOT->LoadMacro("AliTrackletAODdNdeta.C+g");
+  gROOT->LoadMacro("AliTrackletdNdeta.C+g");
 }
