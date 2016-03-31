c818f54e7d5e7fe241eaa5c89e2cf43a2eac7a57
cholm <Christian.Holm.Christensen@cern.ch>
2016-03-31 10:19:12 +0200

    Do not loop over MC particles, as we already have the info in the primary particle histogram

--- PWGLF/FORWARD/analysis2/AliForwardTriggerBiasCorrection.cxx
+++ PWGLF/FORWARD/analysis2/AliForwardTriggerBiasCorrection.cxx
@@ -90,42 +90,52 @@ Bin::Process(TH1D*              dndetaForward,
 			   mcMult,
 			   mcErr);
   Double_t trMult  = mcMult;
-  Double_t mcIPz   = ipZ;
-  if (true) {
-    // retreive MC particles from event
-    TClonesArray* mcArray =
-      static_cast<TClonesArray*>(aodevent.
-				 FindListObject(AliAODMCParticle::
+  Double_t mcIPz   = mc->GetBinContent(0,0); // IP from MC stored here
+  // The stuff below is redundant.  We've already filled the MC
+  // histogram with the exact same information, and we have the IPz
+  // from MC in the under-flow bin 0,0 of the MC histogram.
+  // Furthermore, we use the information stored in the MC histogram to
+  // form the response matrix, so we should also use the same
+  // information to build the trigger bias correction.
+  //
+  // In fact, this class is really not very useful, since we could
+  // have the same code in the MC class for doing the response matrix.
+  // That would ensure that we have the same number in.
+#if 0
+  // MC particles from event
+  TClonesArray* mcArray =
+    static_cast<TClonesArray*>(aodevent.
+			       FindListObject(AliAODMCParticle::
+					      StdBranchName()));
+  if(!mcArray){
+    AliWarning("No MC array found in AOD. Try making it again.");
+    return;
+  }
+  AliAODMCHeader* header = 
+    static_cast<AliAODMCHeader*>(aodevent.
+				 FindListObject(AliAODMCHeader::
 						StdBranchName()));
-    if(!mcArray){
-      AliWarning("No MC array found in AOD. Try making it again.");
-      return;
+  if (!header) {
+    AliWarning("No header found.");
+    return;
+  }
+  // Track loop: find MC truth multiplicity in selected eta bin - this
+  // is probably redundant
+  trMult           = 0;
+  mcIPz            = header->GetVtxZ();
+  Int_t    ntracks = mcArray->GetEntriesFast();
+  for (Int_t it = 0; it < ntracks; it++) {
+    AliAODMCParticle* particle = (AliAODMCParticle*)mcArray->At(it);
+    if (!particle) {
+      AliError(Form("Could not receive track %d", it));
+      continue;
     }
-    AliAODMCHeader* header = 
-      static_cast<AliAODMCHeader*>(aodevent.
-				   FindListObject(AliAODMCHeader::
-						  StdBranchName()));
-    if (!header) {
-      AliWarning("No header found.");
-      return;
-    }
-    // Track loop: find MC truth multiplicity in selected eta bin - this
-    // is probably redundant
-    trMult           = 0;
-    mcIPz            = header->GetVtxZ();
-    Int_t    ntracks = mcArray->GetEntriesFast();
-    for (Int_t it = 0; it < ntracks; it++) {
-      AliAODMCParticle* particle = (AliAODMCParticle*)mcArray->At(it);
-      if (!particle) {
-	AliError(Form("Could not receive track %d", it));
-	continue;
-      }
     if (!particle->IsPhysicalPrimary()) continue;
     if (particle->Charge() == 0) continue;
     if (particle->Eta() > fEtaLow && particle->Eta() < fEtaHigh-0.0001)
       trMult++;
-    }
   }
+#endif
     
   // fill fMCClass with multiplicity of MC truth NSD events with MC
   // truth |vtxz|<4
