ba68889bfeb334c957d34cabe3e8e22f4e97962c
zconesa <zaida.conesa.del.valle@cern.ch>
2016-04-06 15:23:53 +0200

    New classes to apply selection cuts on cascade candidates (J.Hamon)

---
+++ PWGHF/vertexingHF/AliRDHFCutsDplustoK0spi.h
@@ -0,0 +1,84 @@
+#ifndef ALIRDHFCUTSDPLUSTOK0SPI_H
+#define ALIRDHFCUTSDPLUSTOK0SPI_H
+/* Copyright(c) 1998-2010, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id$ */
+
+/////////////////////////////////////////////////////////////
+///
+/// \class AliRDHFCutsDplustoK0spi
+/// \brief Class for cuts on AOD reconstructed D+->K0S+pi
+///
+/// \author J.Hamon, julien.hamon@cern.ch (IPHC)
+///
+/////////////////////////////////////////////////////////////
+
+
+#include "AliRDHFCuts.h"
+
+class AliAODEvent;
+class AliAODRecoDecayHF;
+class AliESDtrackCuts;
+
+
+class AliRDHFCutsDplustoK0spi : public AliRDHFCuts
+{
+public:
+   
+
+   AliRDHFCutsDplustoK0spi(const char* name="CutsDplustoK0spi");
+   AliRDHFCutsDplustoK0spi(const AliRDHFCutsDplustoK0spi& source);
+   AliRDHFCutsDplustoK0spi& operator=(const AliRDHFCutsDplustoK0spi& source);
+   virtual ~AliRDHFCutsDplustoK0spi();
+
+
+   using   AliRDHFCuts::GetCutVarsForOpt;
+   virtual void GetCutVarsForOpt(AliAODRecoDecayHF* obj, Float_t* vars, Int_t nvars, Int_t*pdgdaughters)
+                           { return GetCutVarsForOpt(obj, vars, nvars, pdgdaughters, 0x0); }
+   virtual void GetCutVarsForOpt(AliAODRecoDecayHF* obj, Float_t* vars, Int_t nvars, Int_t* pdgdaughters, AliAODEvent* aod);
+
+   using   AliRDHFCuts::IsSelected;
+   virtual Int_t IsSelected(TObject* obj, Int_t selectionLevel)
+                           { return IsSelected(obj, selectionLevel, 0); }
+   virtual Int_t IsSelected(TObject* obj, Int_t selectionLevel, AliAODEvent* aod);
+
+   using   AliRDHFCuts::IsSelectedPID;
+   virtual Int_t IsSelectedPID(AliAODRecoDecayHF* obj);
+
+   using   AliRDHFCuts::IsInFiducialAcceptance;
+   virtual Bool_t IsInFiducialAcceptance(Double_t pt, Double_t y) const;
+
+
+   Bool_t AreDtoK0sDaughtersSelected(AliAODRecoDecayHF *rd) const;
+   Int_t  GetV0Type();
+
+
+   void AddTrackCutsV0daughters(AliESDtrackCuts* v0daug)
+                           { delete fV0daughtersCuts; fV0daughtersCuts = new AliESDtrackCuts(*v0daug); }
+   virtual AliESDtrackCuts* GetTrackCutsV0daughters() const
+                           { return fV0daughtersCuts; }
+
+   Float_t GetMassCut(Int_t iPtBin=0) const
+                           { return (GetCuts() ? fCutsRD[GetGlobalIndex(0, iPtBin)] : 1.e6);}
+   Int_t GetExcludedCut()
+                           { return fExcludedCut; }
+   void SetExcludedCut(Int_t excludedCut)
+                           { fExcludedCut = excludedCut; }
+
+
+protected:
+
+
+   Int_t             fExcludedCut;           /// cut to be excluded (-1=none)
+   Float_t           fV0Type;                /// V0 type -- should be defined as in AliRDHFCuts.h
+   AliESDtrackCuts*  fV0daughtersCuts;       /// cuts for v0 daughters (AOD converted to ESD on the fly!)
+
+   
+   /// \cond CLASSIMP
+   ClassDef(AliRDHFCutsDplustoK0spi, 1);   /// class for cuts on AOD reconstructed D+ -> K0S + pi
+   /// \endcond
+   
+};
+
+#endif
