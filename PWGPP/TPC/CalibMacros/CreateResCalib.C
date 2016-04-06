28ae4bcc418e2d3ebc96d27b667059e92e00ef0e
shahoian <ruben.shahoyan@cern.ch>
2016-04-06 17:38:52 +0200

    no need to compile the source code

--- PWGPP/TPC/CalibMacros/CreateResCalib.C
+++ PWGPP/TPC/CalibMacros/CreateResCalib.C
@@ -4,7 +4,7 @@ void CreateResCalib(int run=245231
 	  ,const char * resList="lst.txt"	 
 	  )
 {
-  gROOT->ProcessLine(".L /home/shahoian/tpcCalib/vClass/AliTPCDcalibRes.cxx+g");
+
   AliTPCDcalibRes* clb = new AliTPCDcalibRes(run, tmin, tmax, resList);
   clb->SetOCDBPath("local:///cvmfs/alice.cern.ch/calibration/data/2015/OCDB");
   clb->ProcessFromDeltaTrees();
@@ -16,7 +16,7 @@ void postProcResCalib(int run=245231
 	      ,Long64_t tmax= 9999999999
 	      )
 {
-  gROOT->ProcessLine(".L /home/shahoian/tpcCalib/vClass/AliTPCDcalibRes.cxx+g");
+
   AliTPCDcalibRes* clb = new AliTPCDcalibRes(run, tmin, tmax);
   clb->SetOCDBPath("local:///cvmfs/alice.cern.ch/calibration/data/2015/OCDB");
   clb->ProcessFromStatTree();
