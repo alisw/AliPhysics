4f380177b2d4fdc5ccad836a476b2a772698855b
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-04-07 16:07:02 +0200

    use the helper lib for sending

--- HLT/global/AliHLTZMQsink.cxx
+++ HLT/global/AliHLTZMQsink.cxx
@@ -18,8 +18,15 @@
 #include "TDatime.h"
 #include "TRandom3.h"
 #include <TObject.h>
-#include <TPRegexp.h>
 #include "zmq.h"
+#include "AliZMQhelpers.h"
+#include "AliCDBManager.h"
+#include "AliCDBStorage.h"
+#include "AliCDBEntry.h"
+#include "AliHLTMessage.h"
+#include "TStreamerInfo.h"
+#include "TCollection.h"
+#include "TList.h"
 #include "AliZMQhelpers.h"
 
 using namespace std;
@@ -42,6 +49,8 @@ AliHLTZMQsink::AliHLTZMQsink() :
   , fZMQerrorMsgSkip(100)
   , fSendECSparamString(kFALSE)
   , fECSparamString()
+  , fSendStreamerInfos(kFALSE)
+  , fCDBpattern("^/*[a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+/*$")
 {
   //ctor
 }
@@ -123,7 +132,9 @@ Int_t AliHLTZMQsink::DoInit( Int_t /*argc*/, const Char_t** /*argv*/ )
   }
   
   HLTMessage(Form("socket create ptr %p %s",fZMQout,(rc<0)?zmq_strerror(errno):""));
-  HLTImportant(Form("ZMQ connected to: %s (%s(id %i)) rc %i %s",fZMQoutConfig.Data(),alizmq_socket_name(fZMQsocketType),fZMQsocketType,rc,(rc<0)?zmq_strerror(errno):""));
+  HLTImportant(Form("ZMQ connected to: %s (%s(id %i)) rc %i %s",
+               fZMQoutConfig.Data(),alizmq_socket_name(fZMQsocketType),
+               fZMQsocketType,rc,(rc<0)?zmq_strerror(errno):""));
   
   return retCode;
 }
@@ -151,17 +162,18 @@ int AliHLTZMQsink::DoProcessing( const AliHLTComponentEventData& evtData,
   int requestTopicSize=-1;
   char requestTopic[kAliHLTComponentDataTypeTopicSize];
   memset(requestTopic, '*', kAliHLTComponentDataTypeTopicSize);
-  int requestSize=-1;
-  char request[kAliHLTComponentDataTypeTopicSize];
-  memset(request, '*', kAliHLTComponentDataTypeTopicSize);
 
   int rc = 0;
   Bool_t doSend = kTRUE;
   Bool_t doSendECSparamString = kFALSE;
+  Bool_t doSendStreamerInfos = kFALSE;
+  Bool_t doSendCDB = kFALSE;
+  AliCDBEntry* cdbEntry = NULL;
   
   //cache an ECS param topic
   char ecsParamTopic[kAliHLTComponentDataTypeTopicSize];
   DataType2Topic(kAliHLTDataTypeECSParam, ecsParamTopic);
+  TString requestedCDBpath;
 
   //in case we reply to requests instead of just pushing/publishing
   //we poll for requests
@@ -178,8 +190,11 @@ int AliHLTZMQsink::DoProcessing( const AliHLTComponentEventData& evtData,
       {
         requestTopicSize = zmq_recv (fZMQout, requestTopic, kAliHLTComponentDataTypeTopicSize, 0);
         zmq_getsockopt(fZMQout, ZMQ_RCVMORE, &more, &moreSize);
+        zmq_msg_t requestMsg;
+        int requestSize=-1;
         if (more) {
-          requestSize = zmq_recv(fZMQout, request, kAliHLTComponentDataTypeTopicSize, 0);
+          zmq_msg_init(&requestMsg);
+          requestSize = zmq_msg_recv(&requestMsg, fZMQout, 0);
           zmq_getsockopt(fZMQout, ZMQ_RCVMORE, &more, &moreSize);
         }
         //if request is for ECS params, set the flag
@@ -188,6 +203,40 @@ int AliHLTZMQsink::DoProcessing( const AliHLTComponentEventData& evtData,
         {
           doSendECSparamString = kTRUE;
         }
+        //if request is for streamer infos, set the flag
+        else if (*reinterpret_cast<const AliHLTUInt64_t*>(requestTopic) ==
+                 *reinterpret_cast<const AliHLTUInt64_t*>(kAliHLTDataTypeStreamerInfo.fID))
+        {
+          doSendStreamerInfos = kTRUE;
+        }
+        //if request is for an OCDB object, set the flag
+        else if (requestSize>0 &&
+            *reinterpret_cast<const AliHLTUInt64_t*>(requestTopic) ==
+            *reinterpret_cast<const AliHLTUInt64_t*>(kAliHLTDataTypeCDBEntry.fID))
+        {
+          requestedCDBpath.Append(static_cast<char*>(zmq_msg_data(&requestMsg)),requestSize);
+          
+          //get the CDB entry in a safe way
+          do {
+            if (!requestedCDBpath.Contains(fCDBpattern)) {
+              HLTWarning("malformed CDB path: %s", requestedCDBpath.Data());
+              break;
+            }
+            AliCDBManager* cdbMan = AliCDBManager::Instance();
+            if (!cdbMan) break;
+            AliCDBStorage* cdbStorage = cdbMan->GetDefaultStorage();
+            if (!cdbStorage) break;
+            AliCDBId* cdbId = cdbStorage->GetId(requestedCDBpath.Data(),GetRunNo());
+            if (!cdbId) {
+              HLTWarning("cannot get CDB entry: %s", requestedCDBpath.Data());
+              break;
+            }
+            cdbEntry = cdbMan->Get(requestedCDBpath.Data());
+            if (!cdbEntry) break;
+            doSendCDB = kTRUE;
+          } while (false);
+        }
+        zmq_msg_close(&requestMsg);
       } while (more==1);
     }
     else { doSend = kFALSE; }
@@ -232,6 +281,40 @@ int AliHLTZMQsink::DoProcessing( const AliHLTComponentEventData& evtData,
     }
   }
 
+  //cache streamer infos
+  if (fSendStreamerInfos)
+  {
+    const AliHLTComponentBlockData* inputBlock = NULL;
+    for (int iBlock = 0;
+        iBlock < evtData.fBlockCnt;
+        iBlock++) 
+    {
+      inputBlock = &blocks[iBlock];
+      //cache the streamer info
+      if (*reinterpret_cast<const AliHLTUInt64_t*>(inputBlock->fDataType.fID) ==
+          *reinterpret_cast<const AliHLTUInt64_t*>(kAliHLTDataTypeStreamerInfo.fID))
+      {
+        TObject* obj = NULL;
+        AliHLTUInt32_t firstWord=*((AliHLTUInt32_t*)inputBlock->fPtr);
+        if (firstWord==inputBlock->fSize-sizeof(AliHLTUInt32_t))
+        {
+          HLTDebug("create object from block %d size %d", iBlock, inputBlock->fSize);
+          AliHLTMessage msg(inputBlock->fPtr, inputBlock->fSize);
+          TClass* objclass=msg.GetClass();
+          obj=msg.ReadObject(objclass);
+        }
+        TCollection* coll = dynamic_cast<TCollection*>(obj);
+        if (coll)
+        {
+          UpdateSchema(coll);
+        }
+        //delete the remaining infos and destroy the collection
+        coll->SetOwner(kTRUE);
+        delete coll;
+      } //if kAliHLTDataTypeStreamerInfo
+    } //for iBlock
+  } //dummy scope
+
   if (doSend)
   {
     //set the time of current push
@@ -270,29 +353,53 @@ int AliHLTZMQsink::DoProcessing( const AliHLTComponentEventData& evtData,
     int nSelectedBlocks = selectedBlockIdx.size();
     int nSentBlocks = 0;
 
-    //only send the INFO block if there is something to send
+    aliZMQmsg message;
+
+    //only send the INFO block if there is some data to send
     if (fSendRunNumber && nSelectedBlocks>0)
     {
       string runNumberString = "run=";
       char tmp[34];
       snprintf(tmp,34,"%i",GetRunNo()); 
       runNumberString+=tmp;
-      rc = zmq_send(fZMQout, "INFO", 4, ZMQ_SNDMORE);
-      rc = zmq_send(fZMQout, runNumberString.data(), runNumberString.size(), ZMQ_SNDMORE);
-      if (rc>=0) nSentBlocks++;
+      rc = alizmq_msg_add(&message, "INFO", runNumberString);
+      if (rc<0) {
+        HLTWarning("ZMQ error adding INFO");
+      }
     }
 
     //maybe send the ECS param string
     //once if requested or always if so configured
-    if (fSendECSparamString || doSendECSparamString)
+    if ((fSendECSparamString && nSelectedBlocks>0) || doSendECSparamString)
     {
       AliHLTDataTopic topic = kAliHLTDataTypeECSParam;
-      rc = zmq_send(fZMQout, &topic, sizeof(topic), ZMQ_SNDMORE);
-      int flags = (nSelectedBlocks==0)?0:ZMQ_SNDMORE;
-      rc = zmq_send(fZMQout, fECSparamString.Data(), fECSparamString.Length(), flags);
-      if (rc>=0) nSentBlocks++;
-      HLTMessage("sent ECS params, as per request, rc=%i",rc);
+      rc = alizmq_msg_add(&message, &topic, fECSparamString.Data());
+      if (rc<0) {
+        HLTWarning("ZMQ error adding ECS param string");
+      }
       doSendECSparamString = kFALSE;
+    }
+
+    //send the streamer infos if requested
+    if ((fSendStreamerInfos && nSelectedBlocks>0)|| doSendStreamerInfos)
+    {
+      AliHLTDataTopic topic = kAliHLTDataTypeStreamerInfo;
+      rc = alizmq_msg_add(&message, &topic, GetSchema(), GetCompressionLevel());
+      if (rc<0) {
+        HLTWarning("ZMQ error adding schema infos");
+      }
+      doSendStreamerInfos = kFALSE;
+    }
+
+    //send the CDB entry if requested (on request only)
+    if (doSendCDB && cdbEntry)
+    {
+      AliHLTDataTopic topic = kAliHLTDataTypeCDBEntry;
+      rc = alizmq_msg_add(&message, &topic, cdbEntry, GetCompressionLevel());
+      if (rc<0) {
+        HLTWarning("ZMQ error adding CDB entry %s", requestedCDBpath.Data());
+      }
+      doSendCDB = kFALSE;
     }
 
     //send the selected blocks
@@ -303,40 +410,28 @@ int AliHLTZMQsink::DoProcessing( const AliHLTComponentEventData& evtData,
       inputBlock = &blocks[selectedBlockIdx[iSelectedBlock]];
       AliHLTDataTopic blockTopic = *inputBlock;
 
-      //send:
-      //  first part : AliHLTComponentDataType in string format
-      //  second part: Payload
-      rc = zmq_send(fZMQout, &blockTopic, sizeof(blockTopic), ZMQ_SNDMORE);
-      HLTMessage(Form("send topic rc %i %s",rc,(rc<0)?zmq_strerror(errno):""));
-      int flags = 0;
-      if (fZMQneverBlock) flags = ZMQ_DONTWAIT;
-      if (iSelectedBlock < (selectedBlockIdx.size()-1)) flags = ZMQ_SNDMORE;
-      rc = zmq_send(fZMQout, inputBlock->fPtr, inputBlock->fSize, flags);
-      if (rc<0 && (fNskippedErrorMessages++ >= fZMQerrorMsgSkip))
-      {
-        fNskippedErrorMessages=0;
-        HLTWarning("error sending data frame %s, %s",
-                   blockTopic.Description().c_str(),
-                   zmq_strerror(errno));
-      }
-      else
-      {
-        nSentBlocks++;
+      rc = alizmq_msg_add(&message, &blockTopic, inputBlock->fPtr, inputBlock->fSize);
+      if (rc<0) {
+        HLTWarning("ZMQ error adding block %s", blockTopic.Description().c_str());
       }
       HLTMessage(Form("send data rc %i %s",rc,(rc<0)?zmq_strerror(errno):""));
     }
+
     
     //send an empty message if we really need a reply (ZMQ_REP mode)
     //only in case no blocks were sent
-    if (nSentBlocks == 0 && fZMQsocketType==ZMQ_REP)
+    if (message.size()==0 && fZMQsocketType==ZMQ_REP)
     { 
-      rc = zmq_send(fZMQout, 0, 0, ZMQ_SNDMORE);
-      HLTMessage(Form("send endframe rc %i %s",rc,(rc<0)?zmq_strerror(errno):""));
-      if (rc<0) HLTWarning("error sending dummy REP topic");
-      rc = zmq_send(fZMQout, 0, 0, 0);
-      HLTMessage(Form("send endframe rc %i %s",rc,(rc<0)?zmq_strerror(errno):""));
-      if (rc<0) HLTWarning("error sending dummy REP data");
+      rc = alizmq_msg_add(&message, "", "");
+      if (rc<0) {
+        HLTWarning("ZMQ error adding dummy rep data");
+      }
     }
+    rc = alizmq_msg_send(&message, fZMQout, 0);
+    if (rc<0){ 
+      HLTWarning("ZMQ error sending message: %s", zmq_strerror(errno));
+    }
+    alizmq_msg_close(&message);
   }
 
   outputBlocks.clear();
