fbf07ed562287a647b8deaae3a7928fa8d7dfa1e
atimmins <atimmins@cern.ch>
2016-04-08 17:11:14 -0400

    More pointer protection

--- PWGPP/TPC/AliPerformanceTPC.h
+++ PWGPP/TPC/AliPerformanceTPC.h
@@ -113,6 +113,8 @@ private:
   TFolder *fAnalysisFolder; // folder for analysed histograms
 
   Bool_t fUseHLT; // use HLT ESD
+  double rtime;
+  double revent;
   AliPerformanceTPC(const AliPerformanceTPC&); // not implemented
   AliPerformanceTPC& operator=(const AliPerformanceTPC&); // not implemented
 
