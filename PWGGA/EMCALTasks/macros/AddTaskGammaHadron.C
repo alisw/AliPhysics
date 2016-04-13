d95826d01fde919d32bcadc04ab52ee7fd978f1f
efranzis <eliane.epple@yale.edu>
2016-04-13 00:06:36 -0400

    adapt the ALICE coding convention

--- PWGGA/EMCALTasks/macros/AddTaskGammaHadron.C
+++ PWGGA/EMCALTasks/macros/AddTaskGammaHadron.C
@@ -1,14 +1,15 @@
 // $Id$
 
 AliAnalysisTaskGammaHadron* AddTaskGammaHadron(
-  Bool_t      Input_Gamma_Or_Pi0 = 0,           //gamma analysis=0, pi0 analyis=1
-  Bool_t      Input_SameEventAnalysis = 1,      //same event=1 mixed event =0
-  const char *ntracks            = "Tracks",
-  const char *nclusters          = "CaloClusters",
-  const char *ncells             = "EMCALCells",
-  Double_t    trackptcut         = 0.15,
-  Double_t    clusptcut          = 0.30,
-  const char *taskname           = "AliAnalysisTaskGammaHadron"
+  Bool_t      InputGammaOrPi0        = 0,      //gamma analysis=0, pi0 analyis=1
+  Bool_t      InputSameEventAnalysis = 1,      //same event=1 mixed event =0
+  const char *tracksName             = "Tracks",
+  const char *clustersName           = "CaloClusters",
+  const char *ncells                 = "EMCALCells",   //probably delete this this is nowhere used
+  Double_t    trackptcut             = 0.15,
+  Double_t    clusptcut              = 0.30,
+  const char *taskname               = "AliAnalysisTask",
+  const char *suffix                 = ""
 )
 {  
 	cout<<"in AddTaskGammaHadron.C(...)"<<endl;
@@ -30,22 +31,40 @@ AliAnalysisTaskGammaHadron* AddTaskGammaHadron(
   }
   
   //-------------------------------------------------------
-  // Init the task and do settings
+  // Built the name of the Task together
   //-------------------------------------------------------
-  
-  TString name(taskname);
-  if(strcmp(ntracks,""))
+  TString GammaPi0Name;
+  if(InputGammaOrPi0 == 0)
   {
-    name += "_";
-    name += ntracks;
+	  GammaPi0Name += "GH";
   }
-  if(strcmp(nclusters,""))
+  else
   {
-    name += "_";
-    name += nclusters;
+	  GammaPi0Name += "Pi0H";
+  }
+  TString SameMixName;
+  if(InputSameEventAnalysis == 0)
+  {
+	  SameMixName += "SE";
+  }
+  else
+  {
+	  SameMixName += "ME";
   }
 
-  AliAnalysisTaskGammaHadron* AnalysisTask = new AliAnalysisTaskGammaHadron(Input_Gamma_Or_Pi0,Input_SameEventAnalysis);
+  TString combinedName;
+  combinedName.Form("%s_%s_%s_%s_%s",taskname,(const char*)GammaPi0Name,(const char*)SameMixName,tracksName,clustersName);
+  if(suffix!="")
+  {
+	  combinedName += "_";
+	  combinedName += suffix;
+  }
+
+  cout<<"combinedName: "<<combinedName<<endl;
+  //-------------------------------------------------------
+  // Init the task and do settings
+  //-------------------------------------------------------
+  AliAnalysisTaskGammaHadron* AnalysisTask = new AliAnalysisTaskGammaHadron(InputGammaOrPi0,InputSameEventAnalysis);
 
   //perform some settings
   AnalysisTask->SetCaloCellsName(ncells);
@@ -56,8 +75,8 @@ AliAnalysisTaskGammaHadron* AddTaskGammaHadron(
 
 
   //add particle and cluster container
-  AliParticleContainer *partCont = AnalysisTask->AddParticleContainer(ntracks);
-  AliClusterContainer  *clusCont = AnalysisTask->AddClusterContainer(nclusters);
+  AliParticleContainer *partCont = AnalysisTask->AddParticleContainer(tracksName);
+  AliClusterContainer  *clusCont = AnalysisTask->AddClusterContainer(clustersName);
   if(partCont)
   {
 	  partCont->SetParticlePtCut(trackptcut);
@@ -76,15 +95,13 @@ AliAnalysisTaskGammaHadron* AddTaskGammaHadron(
   //-------------------------------------------------------
   // Final settings, pass to manager and set the containers
   //-------------------------------------------------------
-
   mgr->AddTask(AnalysisTask);
   
   // Create containers for input/output
   AliAnalysisDataContainer *cinput1  = mgr->GetCommonInputContainer()  ;
-  //cout<<"ELI: test1 in task"<<endl;
 
-  TString contName(name);
-  contName += "_histosgrams";
+  TString contName(combinedName);
+  contName += "_histos";
   AliAnalysisDataContainer *coutput1 = mgr->CreateContainer(contName.Data(), 
 							    TList::Class(),AliAnalysisManager::kOutputContainer,
 							    Form("%s", AliAnalysisManager::GetCommonFileName()));
