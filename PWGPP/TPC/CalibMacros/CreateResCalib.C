72ce40ca548b7418fa18fbbbe98dcd51bec1e802
zampolli <Chiara.Zampolli@cern.ch>
2016-04-13 15:29:06 +0200

    Update scripts and macros for TPC SP calibration (see below for details)
    - CreateResCalib.C --> Macro to process the TPC SP distorsion calibration with the approach
    developed by R. Shahoyan. The output is the Cheb parameterization (stored in the class
    AliTPCDcalibRes).
    
    - ProcessOutputCheb.C --> Macro to produce the OCDB entry out of the Cheb param from the macro
    above; it starts from a list of files containing such objects (one per time interval). It
    calls the macro "CreateCorrMapObject.C" inside.
    
    - ProcessTimBinAliTPCcalibAlignInterpolation.sh --> old script to process the TPC SP calibration
    using the procedure developed by M. Ivanov
    
    - createOCDBTPCSPDistCalib.sh --> script to generate the final OCDB. It uses the macro
    ProcessOutputCheb.C.
    
    - defineTimeBinsvDrift.sh --> script to define the time bins (~2400s) and create the vdrift
    calibration. It uses AliTPCcalibAlignInterpolation class
    
    - processTimeBin.sh --> script to process one time bin. It uses CreateResCalib.C

--- PWGPP/TPC/CalibMacros/CreateResCalib.C
+++ PWGPP/TPC/CalibMacros/CreateResCalib.C
@@ -1,12 +1,13 @@
 void CreateResCalib(int run=245231
 	  ,Long64_t tmin=0
 	  ,Long64_t tmax= 9999999999
-	  ,const char * resList="lst.txt"	 
+	  ,const char * resList="lst.txt"
+	  ,const char * cdb="raw://"
 	  )
 {
 
   AliTPCDcalibRes* clb = new AliTPCDcalibRes(run, tmin, tmax, resList);
-  clb->SetOCDBPath("local:///cvmfs/alice.cern.ch/calibration/data/2015/OCDB");
+  clb->SetOCDBPath(cdb);
   clb->ProcessFromDeltaTrees();
   clb->Save();
 }
