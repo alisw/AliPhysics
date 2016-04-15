86a1cad813ec87ae37790c6ac5e9200e779ef94f
lgraczyk <lgraczyk@cern.ch>
2016-04-15 13:44:37 +0200

    AliFemto: change in DEtaDPhi train config

--- PWGCF/FEMTOSCOPY/macros/Train/DEtaDPhi/2015/AnalysisNote1/Lambda/LambdaCorrections/ConfigFemtoAnalysis.C
+++ PWGCF/FEMTOSCOPY/macros/Train/DEtaDPhi/2015/AnalysisNote1/Lambda/LambdaCorrections/ConfigFemtoAnalysis.C
@@ -148,7 +148,7 @@ AliFemtoManager* ConfigFemtoAnalysis(const char* params) {
 	if(runmultdep)	  {runmults[0]=1; runmults[1]=1; runmults[2]=1;	  }
 	int multbins[numOfMultBins+1] = {2, 20, 50,150,2,150};
 	
-	int runch[numOfChTypes] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0};
+	int runch[numOfChTypes] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1};
 	const char *chrgs[numOfChTypes] = { "PP", "aPaP", "PaP", "KpKp", "KmKm", "KpKm", "PIpPIp", "PImPIm", "PIpPIm", "all", "plus", "minus", "mixed", "V0PLlowPt","V0PALlowPt","V0APLlowPt","V0APALlowPt","V0LLlowPt","V0LALlowPt","V0ALALlowPt", "V0PLhighPt","V0PALhighPt","V0APLhighPt","V0APALhighPt","V0LLhighPt","V0LALhighPt","V0ALALhighPt", "V0PL","V0PAL","V0APL","V0APAL","V0LL","V0LAL","V0ALAL" };
 	
 	double ktrng[numOfkTbins+1] = {0.0, 0, 0, 0, 0, 0};
