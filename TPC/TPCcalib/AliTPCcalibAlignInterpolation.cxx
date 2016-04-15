efb583ad8765a161436bb5023da240bc50623ab4
mivanov <marian.ivanov@cern.ch>
2016-04-15 09:18:21 +0200

    ATO-321 -(bug) fix in the sign of delta Z in the OCDB export

--- TPC/TPCcalib/AliTPCcalibAlignInterpolation.cxx
+++ TPC/TPCcalib/AliTPCcalibAlignInterpolation.cxx
@@ -3040,7 +3040,7 @@ void  AliTPCcalibAlignInterpolation::MakeVDriftOCDB(const char *inputFile, Int_t
   atime[0]=graphDRIFTVD->GetX()[0];
   atime[1]=graphDRIFTVD->GetX()[graphDRIFTVD->GetN()-1];
   for (Int_t ipoint=0; ipoint<=1; ipoint++){
-    deltaZ[ipoint]=(*vdriftParam)[1];  // unit OK
+    deltaZ[ipoint]=-(*vdriftParam)[1];  // unit OK
     vdgy[ipoint]=-(*vdriftParam)[2];   // units OK
     t0[ipoint]=-(*vdriftParam)[0];               // t0 to be normzlized to the ms
     t0[ipoint]/=(param->GetDriftV()/1000000.); 
