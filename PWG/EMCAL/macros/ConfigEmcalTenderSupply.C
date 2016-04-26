5b1bbd36d59cdce1936131b8dadb29b71f9a602f
fbock <friederike.bock@cern.ch>
2016-04-26 16:20:54 +0200

    added possibility to set energy difference between cells for start of new cluster in V2 clusterization case

--- PWG/EMCAL/macros/ConfigEmcalTenderSupply.C
+++ PWG/EMCAL/macros/ConfigEmcalTenderSupply.C
@@ -1,26 +1,27 @@
 // $Id$
 
 AliEMCALTenderSupply* ConfigEmcalTenderSupply(
-  const Bool_t distBC         = kFALSE,  //distance to bad channel
-  const Bool_t recalibClus    = kFALSE,  //recalibrate cluster energy
-  const Bool_t recalcClusPos  = kFALSE,  //recalculate cluster position
-  const Bool_t nonLinearCorr  = kFALSE,  //apply non-linearity
-  const Bool_t remExoticCell  = kFALSE,  //remove exotic cells
-  const Bool_t remExoticClus  = kTRUE,   //remove exotic clusters
-  const Bool_t fidRegion      = kFALSE,  //apply fiducial cuts
-  const Bool_t calibEnergy    = kFALSE,  //calibrate energy
-  const Bool_t calibTime      = kFALSE,  //calibrate timing
-  const Bool_t remBC          = kFALSE,  //remove bad channels
-  const UInt_t nonLinFunct    = AliEMCALRecoUtils::kNoCorrection,
-  const Bool_t reclusterize   = kTRUE,   //reclusterize
-  const Float_t seedthresh    = 0.100,   //seed threshold
-  const Float_t cellthresh    = 0.050,   //cell threshold
-  const UInt_t clusterizer    = AliEMCALRecParam::kClusterizerv1,
-  const Bool_t trackMatch     = kTRUE,   //track matching
-  const Bool_t updateCellOnly = kFALSE,  //only change if you run your own clusterizer task
-  const Float_t timeMin       = -1,      //minimum time of physical signal in a cell/digit (s)
-  const Float_t timeMax       = +1,      //maximum time of physical signal in a cell/digit (s)
-  const Float_t timeCut       =  1       //maximum time difference between the digits inside EMC cluster (s)
+  const Bool_t distBC             = kFALSE,     // distance to bad channel
+  const Bool_t recalibClus        = kFALSE,     // recalibrate cluster energy
+  const Bool_t recalcClusPos      = kFALSE,     // recalculate cluster position
+  const Bool_t nonLinearCorr      = kFALSE,     // apply non-linearity
+  const Bool_t remExoticCell      = kFALSE,     // remove exotic cells
+  const Bool_t remExoticClus      = kTRUE,      // remove exotic clusters
+  const Bool_t fidRegion          = kFALSE,     // apply fiducial cuts
+  const Bool_t calibEnergy        = kFALSE,     // calibrate energy
+  const Bool_t calibTime          = kFALSE,     // calibrate timing
+  const Bool_t remBC              = kFALSE,     // remove bad channels
+  const UInt_t nonLinFunct        = AliEMCALRecoUtils::kNoCorrection,
+  const Bool_t reclusterize       = kTRUE,      // reclusterize
+  const Float_t seedthresh        = 0.100,      // seed threshold
+  const Float_t cellthresh        = 0.050,      // cell threshold
+  const UInt_t clusterizer        = AliEMCALRecParam::kClusterizerv1,
+  const Bool_t trackMatch         = kTRUE,      // track matching
+  const Bool_t updateCellOnly     = kFALSE,     // only change if you run your own clusterizer task
+  const Float_t timeMin           = -1,         // minimum time of physical signal in a cell/digit (s)
+  const Float_t timeMax           = +1,         // maximum time of physical signal in a cell/digit (s)
+  const Float_t timeCut           =  1,         // maximum time difference between the digits inside EMC cluster (s)
+  const Float_t diffEAggregation  =  0.         // difference E in aggregation of cells (i.e. stop aggregation if E_{new} > E_{prev} + diffEAggregation)
 )
 {
   AliEMCALTenderSupply *EMCALSupply = new AliEMCALTenderSupply("EMCALtender");  
@@ -38,6 +39,7 @@ AliEMCALTenderSupply* ConfigEmcalTenderSupply(
   params->SetTimeMin(timeMin);
   params->SetTimeMax(timeMax);
   params->SetTimeCut(timeCut);
+  params->SetLocMaxCut(diffEAggregation); // Set minimum energy difference to start new cluster
 
   EMCALSupply->SetRecParam(params);
 
