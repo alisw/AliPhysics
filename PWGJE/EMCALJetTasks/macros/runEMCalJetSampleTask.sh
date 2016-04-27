5c4d3fd791d4aed3c758632fe8715a8e5fe768c6
saiola <salvatore.aiola@cern.ch>
2016-04-27 15:37:16 +0200

    Update the EMCal jet framework sample task
    The new version of this task performs basic QA on tracks, clusters and jets.
    It is used also for benchmark tests when the framework code is updated.

---
+++ PWGJE/EMCALJetTasks/macros/runEMCalJetSampleTask.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	DATATYPE="AOD"
+else
+	DATATYPE="${1}"
+fi
+
+if [ -z "$2" ]; then
+	PERIOD="LHC11h"
+else
+	PERIOD="${2}"
+fi
+
+if [ -z "$3" ]; then
+	FILELIST="fileLists/files_LHC11h_2_AOD145.txt"
+else
+	FILELIST="${3}"
+fi
+
+if [ -z "$4" ]; then
+	NEVENTS="5000"
+else
+	NEVENTS="${4}"
+fi
+
+root -b -q -x $ALICE_PHYSICS/PWGJE/EMCalJetTasks/macros/runEMCalJetSampleTask.C\(\""${DATATYPE}"\",\""${PERIOD}"\",\""${FILELIST}"\","${NEVENTS}"\)
