28919ccd57856ccc45114b7bf3f7047610cd3843
mpuccio <maximiliano.puccio@cern.ch>
2016-04-29 15:58:11 +0200

    Added flow and first coalescence implementation

--- PWGLF/NUCLEX/Nuclei/NucleiKine/AliAnalysisTaskNucleiKine.cxx
+++ PWGLF/NUCLEX/Nuclei/NucleiKine/AliAnalysisTaskNucleiKine.cxx
@@ -11,22 +11,23 @@
 #include "AliInputEventHandler.h"
 #include "AliMCEvent.h"
 
+#include "AliGenEventHeaderTunedPbPb.h"
 
 ClassImp(AliAnalysisTaskNucleiKine)
 
 AliAnalysisTaskNucleiKine::AliAnalysisTaskNucleiKine(const string name):
   AliAnalysisTaskSE(name.data()),
-  fPmax{0.2f},
-  fRmax{1.e4f}, // Disabled for the time being
+  fPotentialShape{nullptr},
   fSpinProb{1.f},
+  fSpatialDistribution{nullptr},
   fOutputList{nullptr},
   fEventCounter{nullptr},
   fPtSpectra{nullptr},
   fPdgCodes {211, -211, 321, -321, 2212, -2212, 2112, -2112, 1000010020, -1000010020}
 
 {
-    DefineInput(0, TChain::Class());
-    DefineOutput(1, TList::Class());
+  DefineInput(0, TChain::Class());
+  DefineOutput(1, TList::Class());
 }
 
 AliAnalysisTaskNucleiKine::~AliAnalysisTaskNucleiKine() {
@@ -39,40 +40,51 @@ void AliAnalysisTaskNucleiKine::UserCreateOutputObjects() {
 
   fEventCounter = new TH1D("fEventCounter",";Centrality (%); Events / 1%",100,0,100);
   fPtSpectra = new TH2D("fPtSpectra",";Particle Species;#it{p}_{T} (GeV/#it{c})",fPdgCodes.size(),-0.5,-0.5 + fPdgCodes.size(),100,0.,10.);
+  fCosine = new TH2D("fCosine",";Particle Species;cos(2(#phi-#Psi_{2}))",fPdgCodes.size(),-0.5,-0.5 + fPdgCodes.size(),100,0.,10.);
+  fPsi2 = new TH1D("fPsi2",";Particle Species;#Psi_{2}",64,0,TMath::TwoPi());
 
   fOutputList->Add(fEventCounter);
-  fPtSpectra->Add(fPtSpectra);
+  fOutputList->Add(fPtSpectra);
+  fOutputList->Add(fCosine);
+  fOutputList->Add(fPsi2);
 
   PostData(1,fOutputList);
 }
 
 void AliAnalysisTaskNucleiKine::UserExec(Option_t*) {
-
+  auto to_0_2pi = [](float &x) { while (x < 0) x+= TMath::TwoPi(); };
   AliInputEventHandler* mcHandler = dynamic_cast<AliInputEventHandler*> (AliAnalysisManager::GetAnalysisManager()->GetMCtruthEventHandler());
   if (!mcHandler) return;
 
   AliMCEvent* mcEvent = mcHandler->MCEvent();
   if (!mcEvent) return;
 
-  AliGenEventHeader *mcHeader = (mcEvent->GenEventHeader());
+  AliGenEventHeaderTunedPbPb *mcHeader = (AliGenEventHeaderTunedPbPb*)mcEvent->GenEventHeader();
   if (!mcHeader) return;
+  float psi2 = mcHeader->GetPsi2();
+  to_0_2pi(psi2);
+  fPsi2->Fill(psi2);
+
+  fEventCounter->Fill(1.);
 
   /// Get the information of all the protons and neutrons to see if they could bind to form a deuteron via coalescence
   /// in this loop also a spatial coordinate is given to all the particles (to be implemented)
   vector<bool> mask(mcEvent->GetNumberOfTracks(),true);
   vector<Particle> protons[2],neutrons[2];
-  for (int iTracks = 0; iTracks < mcEvent->GetNumberOfTracks(); ++iTracks) {
-    AliVParticle* track = mcEvent->GetTrack(iTracks);
-    if (!track) continue;
-    const int pdg = track->PdgCode();
-    if (pdg == fPdgCodes[kNeutron]) {
-      neutrons[0].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.939565*0.939565)+(track->P()*track->P()))}});
-    } else if(pdg == fPdgCodes[kNeutron]) {
-      neutrons[1].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.939565*0.939565)+(track->P()*track->P()))}});
-    } else if(pdg == fPdgCodes[kProton]) {
-      protons[0].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.938272*0.938272)+(track->P()*track->P()))}});
-    } else if(pdg == fPdgCodes[kAntiProton]) {
-      protons[1].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.938272*0.938272)+(track->P()*track->P()))}});
+  if (fPotentialShape) {
+    for (int iTracks = 0; iTracks < mcEvent->GetNumberOfTracks(); ++iTracks) {
+      AliVParticle* track = mcEvent->GetTrack(iTracks);
+      if (!track) continue;
+      const int pdg = track->PdgCode();
+      if (pdg == fPdgCodes[kNeutron]) {
+        neutrons[0].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.939565*0.939565)+(track->P()*track->P()))}});
+      } else if(pdg == fPdgCodes[kNeutron]) {
+        neutrons[1].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.939565*0.939565)+(track->P()*track->P()))}});
+      } else if(pdg == fPdgCodes[kProton]) {
+        protons[0].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.938272*0.938272)+(track->P()*track->P()))}});
+      } else if(pdg == fPdgCodes[kAntiProton]) {
+        protons[1].push_back({{0.f,0.f,0.f,0.f},{track->Px(),track->Py(),track->Pz(),sqrt((0.938272*0.938272)+(track->P()*track->P()))}});
+      }
     }
   }
 
@@ -80,11 +92,16 @@ void AliAnalysisTaskNucleiKine::UserExec(Option_t*) {
   for (int iTracks = 0; iTracks < mcEvent->GetNumberOfTracks(); ++iTracks) {
     AliVParticle* track = mcEvent->GetTrack(iTracks);
     if (!track) continue;
+    if (abs(track->Y()) > 0.5) continue;
     int pdg = track->PdgCode();
+    float phi = track->Phi();
+    to_0_2pi(phi);
     if (mcEvent->IsPhysicalPrimary(iTracks)) {
       for (size_t iParticle = 0; iParticle < fPdgCodes.size(); ++iParticle) {
-        if (pdg == fPdgCodes[iParticle] && mask[iTracks])
+        if (pdg == fPdgCodes[iParticle] && mask[iTracks]) {
           fPtSpectra->Fill(iParticle, track->Pt());
+          fCosine->Fill(iParticle,track->Pt(),cos(2. * (phi - psi2)));
+        }
       }
     }
   }
