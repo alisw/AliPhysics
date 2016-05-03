de3f9ab3cef05ee23a147a8626de17d863dd8a2d
dmuhlhei <d.muehlheim@cern.ch>
2016-05-03 15:21:24 +0200

    ConvEventCuts: remove automatic setting of INT7 trigger + bugfix for MC trigger selection

--- PWGGA/GammaConv/AliConvEventCuts.cxx
+++ PWGGA/GammaConv/AliConvEventCuts.cxx
@@ -1034,8 +1034,8 @@ Bool_t AliConvEventCuts::SetCentralityMax(Int_t maxCentrality)
 
 ///________________________________________________________________________
 Bool_t AliConvEventCuts::SetSelectSpecialTrigger(Int_t selectSpecialTrigger)
-{// Set Cut
-
+{
+  // Set Cut
   switch(selectSpecialTrigger){
   case 0:
     fSpecialTrigger=0; // V0OR
@@ -1101,9 +1101,9 @@ Bool_t AliConvEventCuts::SetSelectSpecialTrigger(Int_t selectSpecialTrigger)
 
 ///________________________________________________________________________
 Bool_t AliConvEventCuts::SetSelectSubTriggerClass(Int_t selectSpecialSubTriggerClass)
-{// Set Cut
-
-  if (fSpecialTrigger == 0){ //OR 
+{
+  // Set Cut
+  if (fSpecialTrigger == 0){ //OR
     switch(selectSpecialSubTriggerClass){
     case 0://with VZERO
       fSpecialTrigger=0;
@@ -2479,6 +2479,8 @@ Bool_t AliConvEventCuts::IsTriggerSelected(AliVEvent *fInputEvent, Bool_t isMC)
   if( fInputHandler->GetEventSelection() || fInputEvent->IsA()==AliAODEvent::Class()) {
   
     TString firedTrigClass = fInputEvent->GetFiredTriggerClasses();
+    // if no trigger has been selected manually, select kAny in case of presel (also important for AOD filtering!)
+    // in other cases select standards depending on system
     if (!fTriggerSelectedManually){
       if (fPreSelCut) fOfflineTriggerMask = AliVEvent::kAny;
       else {     
@@ -2486,25 +2488,21 @@ Bool_t AliConvEventCuts::IsTriggerSelected(AliVEvent *fInputEvent, Bool_t isMC)
             fOfflineTriggerMask = AliVEvent::kMB | AliVEvent::kCentral | AliVEvent::kSemiCentral;    
         } else if (fIsHeavyIon == 2){
             fOfflineTriggerMask = AliVEvent::kINT7;
-        } else if ( fPeriodEnum == kLHC11cg ) {
-          // fixing default trigger to kINT7 as no data with kMB was taken!!!
-          fOfflineTriggerMask = AliVEvent::kINT7;
-  //         cout << "will take kINT7 as trigger mask" << endl; 
         } else {
             fOfflineTriggerMask = AliVEvent::kMB;
         }
       }
     }
-    // Get the actual offline trigger mask for the event and AND it with the
-    // requested mask. If no mask requested select by default the event.
-  //       if (fPreSelCut) cout << "Trigger selected from outside: "<< fTriggerSelectedManually <<"\t Offline Trigger mask for Precut: " << fOfflineTriggerMask << endl;
-  //       else cout << "Trigger selected from outside: "<< fTriggerSelectedManually <<"\t Offline Trigger mask: " << fOfflineTriggerMask << endl;
 
-    if (isMC) fOfflineTriggerMask = AliVEvent::kAny;
+    // in case of MC switch to kAny if no MB/INT7/INT8 has been selected
+    if(isMC){
+      if( fOfflineTriggerMask != AliVEvent::kMB && fOfflineTriggerMask != AliVEvent::kINT7 && fOfflineTriggerMask != AliVEvent::kINT8 ){
+        fOfflineTriggerMask = AliVEvent::kAny;
+      }
+    }
   
     if (fOfflineTriggerMask){
       isSelected = fOfflineTriggerMask & fInputHandler->IsEventSelected(); 
-      
       if (isSelected && !fPreSelCut){
 //         cout << "Special trigger: "<< fSpecialTrigger << " initialized " << fEMCALTrigInitialized << endl;
 //         if (fSpecialTrigger == 5 || fSpecialTrigger == 8 || fSpecialTrigger == 9){ // EMCAL triggers
