alisw/AliPhysics

commit 93a720c8472f649d2855db4103de02b6dc6b8f3f

Fix handling of template in operator<<

PWGJE/EMCALJetTasks/Tracks/AliCutValueRange.h
@@ -22,6 +22,12 @@
  */
 namespace EMCalTriggerPtAnalysis{
 
+template<typename t>
+class AliCutValueRange;
+
+template<typename t>
+std::ostream &operator<<(std::ostream &stream, const AliCutValueRange<t> &cut);
+
 /**
  * \class AliCutValueRange
  * \brief Class containing a range for a value to cut on
@@ -43,7 +49,7 @@ public:
 	 */
 	virtual ~AliCutValueRange() {}
 
-	friend std::ostream &operator<<(std::ostream &stream, const AliCutValueRange<t> &val);
+	friend std::ostream &operator<< <>(std::ostream &stream, const AliCutValueRange<t> &val);
 
 	void SetLimits(t min, t max){
 		fLimits[0] = min;
@@ -116,9 +122,6 @@ private:
 	ClassDef(AliCutValueRange, 1);     // Value range for cuts
 	/// \endcond
 };
-
-template<typename t>
-ostream &operator<<(std::ostream &stream, const AliCutValueRange<t> &cut);
 
 }
 

commit b649c5f78b1bcfa897efc1af4fec55abe27cdbd3

Fix templated operator<< implementation in PWGJE
The problem was introduced with:

  93a720c8472f649d2855db4103de02b6dc6b8f3f
  42435410bd4cf2583d9102db52f82f199de2b372

This fixes library loading. The compiler must know what version of the templated
function to compile every time it is used, therefore the implementation must be
in the header file, i.e. in every place that includes (uses) it.

PWGJE/EMCALJetTasks/Tracks/AliCutValueRange.cxx
@@ -108,12 +108,6 @@ namespace EMCalTriggerPtAnalysis {
 	  stream << "[" << fLimits[0] << "|" << fLimits[1] << "]";
 	}
 
-	template<typename t>
-	std::ostream &operator<<(std::ostream &stream, const AliCutValueRange<t> &val){
-	  val.Print(stream);
-	  return stream;
-	}
-
 	template class AliCutValueRange<int>;
 	template class AliCutValueRange<double>;
 	template class AliCutValueRange<float>;

PWGJE/EMCALJetTasks/Tracks/AliCutValueRange.h
@@ -26,7 +26,10 @@ template<typename t>
 class AliCutValueRange;
 
 template<typename t>
-std::ostream &operator<<(std::ostream &stream, const AliCutValueRange<t> &cut);
+std::ostream &operator<<(std::ostream &stream, const AliCutValueRange<t> &val){
+  val.Print(stream);
+  return stream;
+}
 
 /**
  * \class AliCutValueRange