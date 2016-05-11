622011d02eb6ddcee2cdd70ea765279a4a460a7e
policheh <Boris.Polishchuk@cern.ch>
2016-05-11 18:39:15 +0300

    Trigger QA histograms added.

--- PWGGA/PHOSTasks/CaloCellQA/macros/ExtractPHOSCellQA.C
+++ PWGGA/PHOSTasks/CaloCellQA/macros/ExtractPHOSCellQA.C
@@ -41,6 +41,7 @@ void ExtractPHOSCellQA(const TString QAfilelist="QAresult.list")
   TString oldRootFileName, newRootFileName;
   TFile *oldRootFile, *newRootFile;
   TObjArray *histAnyInt, *histPHI7;
+  TList *histTrigL0, *histTrigL1L, *histTrigL1M, *histTrigL1H;
   Bool_t firstFile = kTRUE;
 
   while (1) {
@@ -55,6 +56,10 @@ void ExtractPHOSCellQA(const TString QAfilelist="QAresult.list")
     oldRootFile = TFile::Open(rootFileName,"read");
     histAnyInt = (TObjArray*)oldRootFile->Get("PHOSCellsQA_AnyInt");
     histPHI7   = (TObjArray*)oldRootFile->Get("PHOSCellsQA_PHI7");
+    histTrigL0 = (TList*)oldRootFile->Get("PHOSTriggerQAResultsL0");
+    histTrigL1L = (TList*)oldRootFile->Get("PHOSTriggerQAResultsL1Low");
+    histTrigL1M = (TList*)oldRootFile->Get("PHOSTriggerQAResultsL1Medium");
+    histTrigL1H = (TList*)oldRootFile->Get("PHOSTriggerQAResultsL1High");
     if (histAnyInt == 0 || histPHI7 == 0) {
       printf(" does not contain PHOSCellQA histograms\n");
       continue;
@@ -74,9 +79,24 @@ void ExtractPHOSCellQA(const TString QAfilelist="QAresult.list")
     newRootFile = TFile::Open(newRootFileName,"recreate");
     histPHI7  ->Write(0,0);
     newRootFile->Close();
-
+        
+    if(histTrigL0 || histTrigL1L || histTrigL1M || histTrigL1H) {
+      newRootFileName = Form("TriggerQA_%s.root",runNum);
+      newRootFile = TFile::Open(newRootFileName,"recreate");
+      if(histTrigL0) histTrigL0    ->Write(histTrigL0->GetName(),TObject::kSingleKey);
+      if(histTrigL1L) histTrigL1L  ->Write(histTrigL1L->GetName(),TObject::kSingleKey);
+      if(histTrigL1M) histTrigL1M  ->Write(histTrigL1M->GetName(),TObject::kSingleKey);
+      if(histTrigL1H) histTrigL1H  ->Write(histTrigL1H->GetName(),TObject::kSingleKey);
+      newRootFile->Close();
+      histTrigL0 ->Clear();
+      histTrigL1L ->Clear();
+      histTrigL1M ->Clear();
+      histTrigL1H ->Clear();
+    }
+    
     histAnyInt->Clear();
     histPHI7  ->Clear();
+    
     oldRootFile  ->Close();
-   }
+  }
 }
