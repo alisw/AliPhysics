df458c71165e6eaac259803908cc6cea8a21b57f
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-05-12 14:34:05 +0200

    Fix memleaks, refactor a little bit to use the V interfaces etc. to run online
    some notes:
    - has to derive from AliAnalysisTask, NOT SE.
    - use of printf is fobidden online
    - AliESDEvent will not work, so only access via AliVEvent interface
      -with a caveat: not all access methods will work - therefore only
       the "blessed" online getters will work (the ones that fill instead of
       returning a pointer).
    - one thing remaining: AliMultiplicity is not supported/implemented yet
      in flat event. I'll push the change to aliroot master, fix will make
      it to aliphysics after new tag.

--- PWGPP/BeamGasMonitoring/AliAnalysisBGMonitorQAHLT.h
+++ PWGPP/BeamGasMonitoring/AliAnalysisBGMonitorQAHLT.h
@@ -1,40 +1,33 @@
 #ifndef AliAnalysisBGMonitorQAHLT_H
 #define AliAnalysisBGMonitorQAHLT_H
-#include "AliAnalysisTaskSE.h"
-class AliESDEvent;
-class AliESDfriend;
-class AliAnalysisCuts;
-class TH1D;
-class TH1F;
-class TH2F;
-class TH2D;
-class AliAnalysisBGMonitorQAHLT : public AliAnalysisTaskSE {
+#include "AliAnalysisTask.h"
+class AliVEvent;
+class AliVfriendEvent;
+
+class AliAnalysisBGMonitorQAHLT : public AliAnalysisTask {
     public:
     AliAnalysisBGMonitorQAHLT();
     AliAnalysisBGMonitorQAHLT(const char* name);
     virtual                            ~AliAnalysisBGMonitorQAHLT();
-    virtual void                    UserCreateOutputObjects();
+    virtual void                    CreateOutputObjects();
     virtual void                    Exec(Option_t* option);
     virtual void                    Terminate(Option_t* option);
-    virtual void                    DrawHist(Int_t* ftrigger, Int_t fSpdT, Int_t fSpdC1, Int_t fSpdC2, Int_t* BBFlagC, Int_t* BBFlagA);
+    virtual void                    DrawHist(Int_t* triggers, Int_t fSpdT, Int_t fSpdClusters, Int_t* BBFlagC, Int_t* BBFlagA);
     // virtual void   Terminate(Option_t *);
     private:
-    AliESDEvent* fESD;        //! ESD event
-    AliESDfriend* fESDfriend; //! ESDfriend
-    TTree *fTreeTrack2;        //! tree
+    AliVEvent* fESD;        //! ESD event
+    AliVfriendEvent* fESDfriend; //! ESDfriend
     TList *fList;             //! list
     Int_t fUseTree;
     Int_t runNumber;
     Int_t ftrigger[kMaxUShort];
-    Int_t fSpdC1;
-    Int_t fSpdC2;
-    Int_t fSpdT;
+    Int_t fSpdClusters;
+    Int_t fSpdTracklets;
     Int_t ntracks;
     Int_t nV0A;
     Int_t nV0C;
     Int_t nV0ABG;
     Int_t nV0CBG;
-    Int_t bgID;
     Int_t BGFlagA[kMaxUShort];
     Int_t BGFlagC[kMaxUShort];
     Int_t BBFlagA[kMaxUShort];
