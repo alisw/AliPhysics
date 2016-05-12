4a319719c323698e69c088c8764a890ee1d311ad
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-05-12 14:34:05 +0200

    Fix memleaks, refactor a little bit to use the V interfaces etc. to run online
    some notes:
    - has to derive from AliAnalysisTask, NOT SE.
    - use of printf is fobidden online
    - AliESDEvent will not work, so only access via AliVEvent interface
      -with a caveat: not all access methods will work - therefore only
       the "blessed" online getters will work (the ones that fill instead of
       returning a pointer).
    - one thing remaining: AliMultiplicity is not supported/implemented yet
      in flat event. I'll push the change to aliroot master, fix will make
      it to aliphysics after new tag.

--- PWGPP/BeamGasMonitoring/AliAnalysisBGMonitorQAHLT.cxx
+++ PWGPP/BeamGasMonitoring/AliAnalysisBGMonitorQAHLT.cxx
@@ -22,45 +22,37 @@
 #include"TCanvas.h"
 #include"TArrayI.h"
 #include "TString.h"
-#include"AliAnalysisTaskSE.h"
+#include"AliAnalysisTask.h"
 #include"AliAnalysisManager.h"
-#include"AliESD.h"
-#include"AliESDEvent.h"
-#include"AliESDfriend.h"
 #include"AliVEvent.h"
-#include"AliESDInputHandler.h"
+#include"AliVfriendEvent.h"
+#include"AliVEventHandler.h"
 #include"AliLog.h"
 #include"AliAnalysisFilter.h"
-#include"AliESDtrackCuts.h"
-#include"AliESDVertex.h"
-#include"AliESDtrack.h"
 #include"AliTriggerAnalysis.h"
 #include"AliAnalysisCuts.h"
 #include"AliMultiplicity.h"
 #include"AliESDVZERO.h"
 #include"AliESDVZEROfriend.h"
 #include"AliESDTZERO.h"
-#include"AliAnalysisUtils.h"
 #include"AliESDAD.h"
 #include"AliESDADfriend.h"
+#include "AliLog.h"
 
 class AliAnalysisBGMonitorQAHLT;
 using namespace std;
 
 ClassImp(AliAnalysisBGMonitorQAHLT)
 
-AliAnalysisBGMonitorQAHLT::AliAnalysisBGMonitorQAHLT() : AliAnalysisTaskSE(),
+AliAnalysisBGMonitorQAHLT::AliAnalysisBGMonitorQAHLT() : AliAnalysisTask(),
 fESD(0x0),
 fESDfriend(0x0),
-fTreeTrack2(0),
 fList(0),
 fUseTree(kFALSE),
 runNumber(0),
-fSpdC1(0),
-fSpdC2(0),
-fSpdT(0),
+fSpdClusters(0),
+fSpdTracklets(0),
 ntracks(0),
-bgID(0),
 ntr(0),
 nbunch(0),
 nV0A(0),
@@ -72,18 +64,15 @@ nV0CBG(0)
 
 //________________________________________________________________________
 AliAnalysisBGMonitorQAHLT::AliAnalysisBGMonitorQAHLT(const char *name):
-AliAnalysisTaskSE(name),
+AliAnalysisTask(name,name),
 fESD(0x0),
 fESDfriend(0x0),
-fTreeTrack2(0),
 fList(0),
 fUseTree(kFALSE),
 runNumber(0),
-fSpdC1(0),
-fSpdC2(0),
-fSpdT(0),
+fSpdClusters(0),
+fSpdTracklets(0),
 ntracks(0),
-bgID(0),
 ntr(0),
 nbunch(0),
 nV0A(0),
@@ -100,16 +89,14 @@ AliAnalysisBGMonitorQAHLT::~AliAnalysisBGMonitorQAHLT()
 {
     // destructor
     if(fList) {
-        delete fList;     // at the end of your task, it is deleted from memory by calling this function
+      fList->Delete();
     }
+    delete fList;     // at the end of your task, it is deleted from memory by calling this function
 }
 
 //________________________________________________________________________
-void AliAnalysisBGMonitorQAHLT::UserCreateOutputObjects()
+void AliAnalysisBGMonitorQAHLT::CreateOutputObjects()
 {
-    fTreeTrack2 = new TTree("TreeTrack","Track Properties2");
-    fTreeTrack2->Branch("runNumber",&runNumber,"runNumber/I"); //run number
-    PostData(0, fTreeTrack2);
     fList = new TList();
     fList->SetOwner(kTRUE);
     TH2F *hTotalTrkVsClsSPID = new TH2F("hTotalTrkVsClsSPID_CINT7","; Spd : total",140,0,140,500,0,500);
@@ -160,115 +147,139 @@ void AliAnalysisBGMonitorQAHLT::UserCreateOutputObjects()
 //________________________________________________________________________
 void AliAnalysisBGMonitorQAHLT::Exec(Option_t *)
 {
-    AliESDInputHandler *esdH = dynamic_cast<AliESDInputHandler*> (AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler());
-    if (esdH) {
-        fESD = (AliESDEvent*) esdH->GetEvent();
-        if(fESD) {
-            fESDfriend = (AliESDfriend*)fESD->FindListObject("AliESDfriend");
-            if (!fESDfriend){
-                AliError("No friend found");
-            }
-        }
-        } else {
-        Printf("ERROR: Could not get ESDInputHandler");
-    }
-    if (!fESD) {
-        Printf("ERROR: fESD not available");
-        return;
-    }
-    runNumber = fESD->GetRunNumber();
-    ntr = 10;
-    nbunch = 21;
-    static AliTriggerAnalysis * triggerAnalysis = new AliTriggerAnalysis();
-    
-    AliVVZERO *vzero = fESD->GetVZEROData();
-   
-    AliAnalysisUtils *utils = new AliAnalysisUtils();
-    bgID = utils->IsSPDClusterVsTrackletBG(fESD);
-   
-    //--- SPD cluster and tracklets
-    const AliMultiplicity* mult = fESD->GetMultiplicity();
-    fSpdC1 = 0;
-    fSpdC2 = 0;
-   
-    fSpdC1 = mult->GetNumberOfITSClusters(0);
-    fSpdC2 = mult->GetNumberOfITSClusters(1);
-    fSpdT = mult->GetNumberOfTracklets();
-    
-    //"online" V0 flags
-    nV0A = 0;
-    nV0ABG = 0;
-    for (Int_t i = 32; i < 64; ++i) {
-        if (vzero->GetBBFlag(i)) nV0A++;
-        if (vzero->GetBGFlag(i)) nV0ABG++;
-    }
-    nV0C = 0;
-    nV0CBG = 0;
-    for (Int_t i = 0; i < 32; ++i) {
-        if (vzero->GetBBFlag(i)) nV0C++;
-        if (vzero->GetBGFlag(i)) nV0CBG++;
-    }
-    memset(BGFlagA, 0, sizeof(Float_t)*nbunch);
-    memset(BBFlagA, 0, sizeof(Float_t)*nbunch);
-    memset(BGFlagC, 0, sizeof(Float_t)*nbunch);
-    memset(BBFlagC, 0, sizeof(Float_t)*nbunch);
-    AliESDVZEROfriend *esdV0friend = fESDfriend->GetVZEROfriend();
-    if(esdV0friend) {
-        for(Int_t j = 0; j < 20; j++){
-            for (Int_t i = 32; i < 64; ++i) {
-                if(esdV0friend->GetBBFlag(i,j)) BBFlagA[j]++;
-                if(esdV0friend->GetBGFlag(i,j)) BGFlagA[j]++;
-            }
-            for (Int_t i = 0; i < 32; ++i) {
-                if(esdV0friend->GetBBFlag(i,j)) BBFlagC[j]++;
-                if(esdV0friend->GetBGFlag(i,j)) BGFlagC[j]++;
-            }
-        }
-        } else {
-        Printf("No esdV0friend available");
-        return;
-    }
-    ntracks = fESD->GetNumberOfTracks(); // number of tracks (no quality cuts)
-    //--- Trigger classes --//
-    memset(ftrigger, 0, sizeof(Float_t)*ntr);
-    if(fESD->IsTriggerClassFired("CINT7-B-NOPF-ALLNOTRD") || fESD->IsTriggerClassFired("CINT7-S-NOPF-ALLNOTRD") || fESD->IsTriggerClassFired("CINT1-B-NOPF-ALLNOTRD") || fESD->IsTriggerClassFired("CINT1-S-NOPF-ALLNOTRD") || fESD->IsTriggerClassFired("CINT7-A-NOPF-CENT") || fESD->IsTriggerClassFired("CINT7-B-NOPF-CENT") || fESD->IsTriggerClassFired("CINT7-C-NOPF-CENT") || fESD->IsTriggerClassFired("CINT7-E-NOPF-CENT")) ftrigger[0] = 1; // CINT7 trigger
-    if(fESD->IsTriggerClassFired("CVHMV0M-A-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMV0M-B-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMV0M-C-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMV0M-E-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMV0M-B-SPD1-CENT") || fESD->IsTriggerClassFired("CVHMV0M-A-NOPF-CENTNOTRD") || fESD->IsTriggerClassFired("CVHMV0M-B-NOPF-CENTNOTRD") || fESD->IsTriggerClassFired("CVHMV0M-C-NOPF-CENTNOTRD") || fESD->IsTriggerClassFired("CVHMV0M-E-NOPF-CENTNOTRD")) ftrigger[1] = 1; // VOM trigger
-    if(fESD->IsTriggerClassFired("CVHMSH2-A-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMSH2-B-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMSH2-C-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMSH2-E-NOPF-CENT") || fESD->IsTriggerClassFired("CVHMSH2-B-NOPF-ALL") || fESD->IsTriggerClassFired("CVHMSH2-B-NOPF-CENTNOTRD")) ftrigger[2] = 1; // SH2 trigger
-    // count total event number (blim)
-    if(ftrigger[0]==1)((TH1F*)fList->FindObject("hNumEvents"))->Fill(1);
-    if(ftrigger[1]==1)((TH1F*)fList->FindObject("hNumEvents"))->Fill(2);
-    if(ftrigger[2]==1)((TH1F*)fList->FindObject("hNumEvents"))->Fill(3);
-    static Bool_t SelGoodEvent;
-    //
-    // List of the information needed to fill the histograms---------------------------------------------------
-    // fSpdT: GetNumberOfTracklets(), Int value
-    // fSpdC1, fSpdC2: GetNumberOfITSClusters(0), Int value
-    // BBFlagA, BBFlagC : esdV0friend->GetBBFlag(i,j), Int array, from 0 to 19
-    //     0 < i < 33: V0C info
-    //   31 < i < 64: V0A info
-    //     0 < j < 20: bunch window timing
+  AliVEventHandler *esdH = AliAnalysisManager::GetAnalysisManager()->GetInputEventHandler();
+  if (!esdH) {
+    AliError("ERROR: Could not get VEventHandler");
+    return;
+  }
+  
+  fESD = esdH->GetEvent();
+  if(!fESD) {
+    AliError("no VEvent!");
+    return;
+  }
+  
+  fESDfriend = fESD->FindFriend();
 
-    if(ftrigger[0]==1 || ftrigger[1]==1 || ftrigger[2]==1) DrawHist(ftrigger, fSpdT, fSpdC1, fSpdC2, BBFlagA, BBFlagC);
+  if (!fESDfriend) {
+    AliError("no VEventFriend!");
+    return;
+  }
 
-    PostData(1, fList);
-    fTreeTrack2->Fill();
-    PostData(0, fTreeTrack2);
+  runNumber = fESD->GetRunNumber();
+  ntr = 10;
+  nbunch = 21;
+
+  AliESDVZERO tmpvzero;
+  fESD->GetVZEROData(tmpvzero);
+  AliESDVZERO* vzero = &tmpvzero;
+
+  //--- SPD cluster and tracklets
+  AliVMultiplicity* mult = fESD->GetMultiplicity();
+  fSpdClusters = 0;
+  fSpdTracklets = 0;
+
+  if (mult) {
+    fSpdClusters = mult->GetNumberOfITSClusters(0) + mult->GetNumberOfITSClusters(1);
+    fSpdTracklets = mult->GetNumberOfTracklets();
+  }
+
+  //"online" V0 flags
+  nV0A = 0;
+  nV0ABG = 0;
+  for (Int_t i = 32; i < 64; ++i) {
+    if (vzero->GetBBFlag(i)) nV0A++;
+    if (vzero->GetBGFlag(i)) nV0ABG++;
+  }
+  nV0C = 0;
+  nV0CBG = 0;
+  for (Int_t i = 0; i < 32; ++i) {
+    if (vzero->GetBBFlag(i)) nV0C++;
+    if (vzero->GetBGFlag(i)) nV0CBG++;
+  }
+  memset(BGFlagA, 0, sizeof(Float_t)*nbunch);
+  memset(BBFlagA, 0, sizeof(Float_t)*nbunch);
+  memset(BGFlagC, 0, sizeof(Float_t)*nbunch);
+  memset(BBFlagC, 0, sizeof(Float_t)*nbunch);
+
+  AliESDVZEROfriend tmpesdV0friend;
+  int haveVZEROfriend = fESDfriend->GetESDVZEROfriend(tmpesdV0friend);
+  AliESDVZEROfriend* esdV0friend = &tmpesdV0friend;
+
+  if(not (haveVZEROfriend<0)) {
+    for(Int_t j = 0; j < 20; j++){
+      for (Int_t i = 32; i < 64; ++i) {
+        if(esdV0friend->GetBBFlag(i,j)) BBFlagA[j]++;
+        if(esdV0friend->GetBGFlag(i,j)) BGFlagA[j]++;
+      }
+      for (Int_t i = 0; i < 32; ++i) {
+        if(esdV0friend->GetBBFlag(i,j)) BBFlagC[j]++;
+        if(esdV0friend->GetBGFlag(i,j)) BGFlagC[j]++;
+      }
+    }
+  } else {
+    AliError("No esdV0friend available");
+    return;
+  }
+  ntracks = fESD->GetNumberOfTracks(); // number of tracks (no quality cuts)
+  //--- Trigger classes --//
+  memset(ftrigger, 0, sizeof(Float_t)*ntr);
+  if(fESD->IsTriggerClassFired("CINT7-B-NOPF-ALLNOTRD") ||
+      fESD->IsTriggerClassFired("CINT7-S-NOPF-ALLNOTRD") ||
+      fESD->IsTriggerClassFired("CINT1-B-NOPF-ALLNOTRD") ||
+      fESD->IsTriggerClassFired("CINT1-S-NOPF-ALLNOTRD") ||
+      fESD->IsTriggerClassFired("CINT7-A-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CINT7-B-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CINT7-C-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CINT7-E-NOPF-CENT")) 
+    ftrigger[0] = 1; // CINT7 trigger
+
+  if(fESD->IsTriggerClassFired("CVHMV0M-A-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMV0M-B-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMV0M-C-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMV0M-E-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMV0M-B-SPD1-CENT") ||
+      fESD->IsTriggerClassFired("CVHMV0M-A-NOPF-CENTNOTRD") ||
+      fESD->IsTriggerClassFired("CVHMV0M-B-NOPF-CENTNOTRD") ||
+      fESD->IsTriggerClassFired("CVHMV0M-C-NOPF-CENTNOTRD") ||
+      fESD->IsTriggerClassFired("CVHMV0M-E-NOPF-CENTNOTRD"))
+    ftrigger[1] = 1; // VOM trigger
+
+  if(fESD->IsTriggerClassFired("CVHMSH2-A-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMSH2-B-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMSH2-C-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMSH2-E-NOPF-CENT") ||
+      fESD->IsTriggerClassFired("CVHMSH2-B-NOPF-ALL") ||
+      fESD->IsTriggerClassFired("CVHMSH2-B-NOPF-CENTNOTRD"))
+    ftrigger[2] = 1; // SH2 trigger
+
+  // count total event number (blim)
+  if(ftrigger[0]==1)((TH1F*)fList->FindObject("hNumEvents"))->Fill(1);
+  if(ftrigger[1]==1)((TH1F*)fList->FindObject("hNumEvents"))->Fill(2);
+  if(ftrigger[2]==1)((TH1F*)fList->FindObject("hNumEvents"))->Fill(3);
+
+  if(ftrigger[0]==1 ||
+     ftrigger[1]==1 ||
+     ftrigger[2]==1)
+    DrawHist(ftrigger, fSpdTracklets, fSpdClusters, BBFlagA, BBFlagC);
+
+  PostData(1, fList);
 }
 //________________________________________________________________________
 void AliAnalysisBGMonitorQAHLT::Terminate(Option_t *)
 {
 }
 //________________________________________________________________________
-void AliAnalysisBGMonitorQAHLT::DrawHist(Int_t* ftrigger, Int_t fSpdT, Int_t fSpdC1, Int_t fSpdC2, Int_t* BBFlagC, Int_t* BBFlagA){
+void AliAnalysisBGMonitorQAHLT::DrawHist(Int_t* triggers, Int_t fSpdTracklets, Int_t fSpdClusters, Int_t* BBFlagC, Int_t* BBFlagA){
 
     TString triggername;
-    if(ftrigger[0]) triggername.Form("CINT7");
-    if(ftrigger[1]) triggername.Form("V0M");
-    if(ftrigger[2]) triggername.Form("SH2");
+    if(triggers[0]) triggername.Form("CINT7");
+    if(triggers[1]) triggername.Form("V0M");
+    if(triggers[2]) triggername.Form("SH2");
 
     Bool_t SelGoodEvent = 0;
-    Printf(Form("%s triggred",triggername.Data()));
-    ((TH1F*)fList->FindObject(Form("hTotalTrkVsClsSPID_%s",triggername.Data())))->Fill(fSpdT, fSpdC1+fSpdC2); // No PF Selection
+    AliInfo(Form("%s triggred",triggername.Data()));
+    ((TH1F*)fList->FindObject(Form("hTotalTrkVsClsSPID_%s",triggername.Data())))->Fill(fSpdTracklets, fSpdClusters); // No PF Selection
         for(Int_t ii=1; ii<33; ii++){
             //___________
             SelGoodEvent = BBFlagA[11]<ii  &  BBFlagA[12]<ii  &  BBFlagA[13]<ii  &  BBFlagA[14]<ii  &  BBFlagA[15]<ii  &  BBFlagA[16]<ii  & BBFlagA[17]<ii; //BB-A 11-17
@@ -278,10 +289,10 @@ void AliAnalysisBGMonitorQAHLT::DrawHist(Int_t* ftrigger, Int_t fSpdT, Int_t fSp
             //___________
             if(SelGoodEvent) {
                 if(ii == 2){
-                    ((TH1F*)fList->FindObject(Form("hTotalTrkVsClsSPID_%s_PF2",triggername.Data())))->Fill(fSpdT, fSpdC1+fSpdC2); // PF = 2 Condition
+                    ((TH1F*)fList->FindObject(Form("hTotalTrkVsClsSPID_%s_PF2",triggername.Data())))->Fill(fSpdTracklets, fSpdClusters); // PF = 2 Condition
                 }
                 if(ii == 10){
-                    ((TH1F*)fList->FindObject(Form("hTotalTrkVsClsSPID_%s_PF10",triggername.Data())))->Fill(fSpdT, fSpdC1+fSpdC2); // PF = 10 Condition
+                    ((TH1F*)fList->FindObject(Form("hTotalTrkVsClsSPID_%s_PF10",triggername.Data())))->Fill(fSpdTracklets, fSpdClusters); // PF = 10 Condition
                 }
             }
         }
