8f631743eda6d61abf85d9d5758e4cf7c8fc2e39
shahoian <ruben.shahoyan@cern.ch>
2016-05-13 16:15:58 +0200

    make max number of tracks argument of the macro

--- PWGPP/TPC/CalibMacros/CreateResCalib.C
+++ PWGPP/TPC/CalibMacros/CreateResCalib.C
@@ -1,13 +1,15 @@
 void CreateResCalib(int run=245231
-	  ,Long64_t tmin=0
-	  ,Long64_t tmax= 9999999999
-	  ,const char * resList="lst.txt"
-	  ,const char * cdb="raw://"
+		    ,Long64_t tmin=0
+		    ,Long64_t tmax= 9999999999
+		    ,const char * resList="lst.txt"
+		    ,const char * cdb="raw://"
+		    ,int maxTracks = 4000000
 	  )
 {
 
   AliTPCDcalibRes* clb = new AliTPCDcalibRes(run, tmin, tmax, resList);
   clb->SetOCDBPath(cdb);
+  clb->SetMaxTracks(maxTracks);
   clb->ProcessFromDeltaTrees();
   clb->Save();
 }
