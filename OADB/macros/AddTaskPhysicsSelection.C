8b92d15202408b326965bbae804e68fd99424baf
ekryshen <evgeny.kryshen@cern.ch>
2016-05-17 17:01:57 +0200

    Error message in case deprecated ComputeBG method is used

--- OADB/macros/AddTaskPhysicsSelection.C
+++ OADB/macros/AddTaskPhysicsSelection.C
@@ -1,4 +1,4 @@
-AliPhysicsSelectionTask* AddTaskPhysicsSelection(Bool_t mCAnalysisFlag = kFALSE, Bool_t deprecatedFlag = kTRUE, UInt_t computeBG = 0, Bool_t useSpecialOutput=kFALSE) 
+AliPhysicsSelectionTask* AddTaskPhysicsSelection(Bool_t mCAnalysisFlag = kFALSE, Bool_t deprecatedFlag = kTRUE, UInt_t deprecatedFlag2 = 0, Bool_t useSpecialOutput=kFALSE) 
 {
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
@@ -38,8 +38,8 @@ AliPhysicsSelectionTask* AddTaskPhysicsSelection(Bool_t mCAnalysisFlag = kFALSE,
   AliPhysicsSelection* physSel = task->GetPhysicsSelection();
   if (mCAnalysisFlag)      
     physSel->SetAnalyzeMC();
-  if (computeBG)
-    physSel->SetComputeBG(computeBG);
+  if (deprecatedFlag2)
+    Error("AddTaskPhysicsSelection", "ComputeBG functionality is deprecated");
 
   if(!deprecatedFlag) {
     Fatal("AddTaskPhysicsSelection", "The BG ID flag is deprecated. Please use the OADB to configure the cuts");
