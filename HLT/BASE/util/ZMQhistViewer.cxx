e5c4e2944a4d2e27c4c89c6457cb6f3a8c86276a
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-05-19 14:04:19 +0200

    Don't pass options to TApplication

--- HLT/BASE/util/ZMQhistViewer.cxx
+++ HLT/BASE/util/ZMQhistViewer.cxx
@@ -3,7 +3,7 @@
 #include "AliZMQMTviewerGUI.h"
 
 int main(int argc, char **argv) {
-   TApplication theApp("App", &argc, argv);
+   TApplication theApp("App", NULL, NULL);
    AliZMQMTviewerGUI viewer(gClient->GetRoot(), 200, 200, argc, argv);
    if (viewer.GetInitStatus()<0) {
      printf("%s",viewer.fUSAGE);
