b24a58347b13015453e677867bf94d027ce1d6a2
shahoian <ruben.shahoyan@cern.ch>
2016-05-20 10:21:36 +0200

    possibility to override useTOFBC by env.var

--- PWGPP/TPC/CalibMacros/CreateResCalib.C
+++ PWGPP/TPC/CalibMacros/CreateResCalib.C
@@ -3,11 +3,21 @@ void CreateResCalib(int run=245231
 		    ,Long64_t tmax= 9999999999
 		    ,const char * resList="lst.txt"
 		    ,const char * cdb="raw://"
-		    ,int maxTracks = 4000000
+		    ,int maxTracks = 5000000
 		    ,Bool_t useTOFBC = kFALSE
 	  )
 {
-
+  TString useTOFBCStr = gSystem->Getenv("useTOFBC");    
+  if (!useTOFBCStr.IsNull()) {
+    useTOFBCStr.ToLower();
+    if      (useTOFBCStr.Contains("true")) useTOFBC = kTRUE;
+    else if (useTOFBCStr.Contains("false")) useTOFBC = kFALSE;
+    else {
+      ::Info("AliTPCcalibAlignInterpolation::FitDrift",
+	     "Cannot interpret Env.var {useTOFBC}=%s as true or false, keep default setting %d",gSystem->Getenv("useTOFBC"),useTOFBC);
+    }
+  }
+  //
   AliTPCDcalibRes* clb = new AliTPCDcalibRes(run, tmin, tmax, resList);
   clb->SetOCDBPath(cdb);
   if (maxTracks>0) clb->SetMaxTracks(maxTracks);
