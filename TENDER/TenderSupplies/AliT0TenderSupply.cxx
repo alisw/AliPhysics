d1eb259687f911368e9034f11da243bd80e7716a
alla <Alla.Maevskaya@cern.ch>
2016-05-29 11:30:25 +0300

    Global offsets for simulation with EPOS

--- TENDER/TenderSupplies/AliT0TenderSupply.cxx
+++ TENDER/TenderSupplies/AliT0TenderSupply.cxx
@@ -74,8 +74,7 @@ void AliT0TenderSupply::Init(){
   //
   fCorrectStartTimeOnAmplSatur = kFALSE;
   fAmplitudeThreshold = 100; //in mips
-
-  Int_t run = fTender->GetRun();
+   Int_t run = fTender->GetRun();
   if (run == 0) return;    // to skip first init, when we don't have yet a run number
   Printf("----------- TZERO Tender ----------------");
 
@@ -85,7 +84,7 @@ void AliT0TenderSupply::Init(){
   // align T0s for LHC10def periods 
  // LHC11h
 
-  if (fTender->GetRun()>=167693 &&  fTender->GetRun()<=170593){
+  //  if (fTender->GetRun()>=167693 &&  fTender->GetRun()<=170593){
     //saturation
     //    fCorrectStartTimeOnAmplSatur = kTRUE;
     // fAmplitudeThreshold = 100; //in mips
@@ -94,20 +93,31 @@ void AliT0TenderSupply::Init(){
     TTree *tree= (TTree*)esdIH->GetTree();
     TFile *file= (TFile*)tree->GetCurrentFile();
     TString fileName(file->GetName());
-    if( fileName.Contains("pass2") )fCorrectMeanTime=kTRUE;
-    Printf("fCorrectMeanTime %i \n", fCorrectMeanTime);
-    AliCDBManager* ocdbMan = AliCDBManager::Instance();
-    ocdbMan->SetRun(fTender->GetRun());    
-    AliCDBEntry *entry = ocdbMan->Get("T0/Calib/TimeAdjust/");
-    if(entry) {
-      AliT0CalibSeasonTimeShift *clb = (AliT0CalibSeasonTimeShift*) entry->GetObject();
-      Float_t *t0means = clb->GetT0Means();
-      for (Int_t i=0;i<4;i++) fTimeOffset[i] = t0means[i];
-    } else {
-      for (Int_t i=0;i<4;i++) fTimeOffset[i] = 0;
-      AliWarning("T0Tender no T0 entry found T0shift set to 0");
+    if (fTender->GetRun()>=167693 &&  fTender->GetRun()<=170593 && 
+	fileName.Contains("pass2") ) {
+	fCorrectMeanTime=kTRUE;
+	AliCDBManager* ocdbMan = AliCDBManager::Instance();
+        ocdbMan->SetRun(fTender->GetRun());    
+        AliCDBEntry *entry = ocdbMan->Get("T0/Calib/TimeAdjust/");
+ //   AliCDBEntry *entry = ocdbMan->Get("T0/Calib/TimeOffsetAOD");
+        if(entry) {
+            AliT0CalibSeasonTimeShift *clb = (AliT0CalibSeasonTimeShift*) entry->GetObject();
+            Float_t *t0means = clb->GetT0Means();
+            for (Int_t i=0;i<4;i++) fTimeOffset[i] = t0means[i];
+       } else {
+            for (Int_t i=0;i<4;i++) fTimeOffset[i] = 0;
+             AliWarning("T0Tender no T0 entry found T0shift set to 0");
+              }
     }
-  }  
+
+ //   TString filename=Form("/tzero/alla/alice/ESDtree/2013/195483/alice_cern.ch_user_a_alla_treeLHC16c_output195483TOFcuts_000_%.3i_AnalysisResults.root",ifile);
+ 	
+    if ( fileName.Contains("LHC16d3") || fileName.Contains("LHC16a2d2") ) {
+   	 fCorrectMeanTime=kTRUE;
+         fTimeOffset[0]=35; fTimeOffset[1]=25; fTimeOffset[2]=40; 
+	 }
+    Printf("fCorrectMeanTime %i \n", fCorrectMeanTime);
+    // }  
 
 }
 
@@ -126,9 +136,9 @@ void AliT0TenderSupply::ProcessEvent(){
      if(fTender->RunChanged()) Init();
     
    if(fTender->RunChanged()){
-      Init();
       if (fTender->GetRun()>=139699&&  fTender->GetRun()<=146860){
-        AliESDInputHandler *esdIH = dynamic_cast<AliESDInputHandler*>  (fTender->GetESDhandler());
+     Init();
+         AliESDInputHandler *esdIH = dynamic_cast<AliESDInputHandler*>  (fTender->GetESDhandler());
         if (esdIH) {
           TTree *tree= (TTree*)esdIH->GetTree();
           TFile *file= (TFile*)tree->GetCurrentFile();
@@ -176,8 +186,11 @@ void AliT0TenderSupply::ProcessEvent(){
       // correct mean time offsets  
       const Double32_t* mean = event->GetT0TOF();
       for(int it0=0; it0<3; it0++){
-	if( mean[it0] < 10000 || (mean[it0]>60000 && mean[it0]<60000 ) )
+	if( mean[it0] < 2000  && mean[it0]>-2000 ) {
 	  event->SetT0TOF(it0, mean[it0] - fTimeOffset[it0]);
+	  printf("T0 %i mean[it0] %f fTimeOffset[it0] %f\n",it0, mean[it0],
+	  fTimeOffset[it0]);
+	  }
      }
     }
     //...........................................
