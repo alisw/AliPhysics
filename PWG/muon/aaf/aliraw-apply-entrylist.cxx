alisw/AliPhysics

commit 6d13160aa48763c80244039c55897c2936960126

RC-5286 Add two small utilities to filter raw data by timestamp

PWG/muon/aaf/aliraw-apply-entrylist.cxx
@@ -1,0 +1,119 @@
+#include "AliRawVEvent.h"
+#include "Riostream.h"
+#include "TFile.h"
+#include "TString.h"
+#include "TTree.h"
+#include <cstdlib>
+#include <sstream>
+
+// TTree* tree = (TTree*) fFile->Get("RAW");
+// if (!tree) {
+//   Error("AliRawReaderRoot", "no raw data tree found");
+//   fIsValid = kFALSE;
+//   return;
+// }
+// fBranch = tree->GetBranch("rawevent");
+// if (!fBranch) {
+//   Error("AliRawReaderRoot", "no raw data branch found");
+//   fIsValid = kFALSE;
+//   return;
+// }
+//
+// fBranch->SetAddress(&fEvent);
+
+Bool_t CopyEntries(const char* filename, const std::vector<int>& entries, TTree*& newtree, TFile* file)
+{
+  std::cout << "CopyEntries from " << filename << std::endl;
+
+  std::unique_ptr<TFile> input(TFile::Open(filename));
+
+  TTree* oldtree = static_cast<TTree*>(input->Get("RAW"));
+
+  if (!oldtree) {
+    std::cout << "Could not get RAW tree from " << filename << std::endl;
+    return kFALSE;
+  }
+  AliRawVEvent* event{ nullptr };
+  TBranch* branch = oldtree->GetBranch("rawevent");
+
+  branch->SetAddress(&event);
+
+  if (!newtree) {
+    file->cd();
+    newtree = oldtree->CloneTree(0);
+  }
+  else {
+    oldtree->CopyAddresses(newtree);
+  }
+
+  for (auto i : entries) {
+    oldtree->GetEntry(i);
+    newtree->Fill();
+  }
+
+  newtree->AutoSave();
+  return kTRUE;
+}
+
+Int_t ApplyEntryList(const char* entryListFileName, const char* outputFileName)
+{
+  /// Copy all the entries found in entryListFileName to a new
+  /// tree in outputFileName
+  /// The format of entryListFileName is :
+  /// filename1
+  /// n1
+  /// id1 id2 ... idn1
+  /// filename2
+  /// n2
+  /// id2_1 id2_2 .... id2_n2
+  /// where n is the number of entries to be copied, and the ids of the entries
+  /// to be copied are on the next line
+
+  std::unique_ptr<TFile> file(TFile::Open(outputFileName, "RECREATE"));
+
+  TTree* newtree = nullptr;
+
+  std::ifstream in(entryListFileName);
+  std::string filename;
+  std::string numberofentries;
+  std::string entries;
+
+  while (std::getline(in, filename)) {
+
+    std::getline(in, numberofentries);
+    std::getline(in, entries);
+
+    std::istringstream s(entries);
+    int x;
+    std::vector<int> v;
+    while (s >> x) {
+      v.push_back(x);
+    }
+
+    CopyEntries(filename.c_str(), v, newtree, file.get());
+  }
+
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cout << "Usage : " << argv[0] << " --entrylist filename.txt --output destination" << std::endl;
+    return 1;
+  }
+
+  TString output{ "" };
+  TString entryList{ "" };
+
+  for (int i = 0; i < argc; ++i) {
+    if (TString(argv[i]) == "--entrylist") {
+      entryList = argv[i + 1];
+    }
+    if (TString(argv[i]) == "--output") {
+      output = argv[i + 1];
+    }
+  }
+
+  return ApplyEntryList(entryList.Data(), output.Data());
+}

PWG/muon/aaf/aliraw-generate-entrylist.cxx
@@ -1,0 +1,92 @@
+#include "AliRawReader.h"
+#include "Riostream.h"
+#include "TDatime.h"
+#include "TString.h"
+#include "TSystem.h"
+
+struct EventInfo {
+  Int_t mEventNumber;
+  Int_t mTimeStamp;
+};
+
+void GenerateEntryListOneChunk(const char* infile, std::ostream& outfile, const TDatime& fromTime,
+                               const TDatime& toTime)
+{
+  std::vector<EventInfo> eventNumbers;
+  int n(0);
+
+  std::unique_ptr<AliRawReader> rawReader(AliRawReader::Create(infile));
+
+  while (rawReader->NextEvent()) {
+    Int_t ts = rawReader->GetTimestamp();
+    TDatime d(ts);
+    if (d <= toTime && d >= fromTime) {
+      EventInfo ei;
+      ei.mEventNumber = n;
+      ei.mTimeStamp = ts;
+      eventNumbers.push_back(ei);
+    }
+    ++n;
+  }
+  if (!eventNumbers.empty()) {
+    std::sort(begin(eventNumbers), end(eventNumbers),
+              [](EventInfo& i, EventInfo& j) -> bool { return i.mTimeStamp < j.mTimeStamp; });
+    outfile << infile << std::endl << eventNumbers.size() << std::endl;
+    for (auto ei : eventNumbers) {
+      outfile << ei.mEventNumber << " ";
+    }
+    outfile << std::endl;
+  }
+}
+
+Int_t GenerateEntryList(const char* filelist, const char* entrylistfilename, const TDatime& fromTime,
+                        const TDatime& toTime)
+{
+
+  std::string line;
+  std::ifstream in(filelist);
+  std::vector<std::string> files;
+
+  while (std::getline(in, line)) {
+    files.push_back(line.c_str());
+  }
+
+  std::ofstream entryListFile(gSystem->ExpandPathName(entrylistfilename));
+
+  for (auto f : files) {
+    GenerateEntryListOneChunk(f.c_str(), entryListFile, fromTime, toTime);
+  }
+
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cout << "Usage : " << argv[0]
+              << " --collection filelist.txt --entrylist filename.txt --from [startTime] --to [endTime]" << std::endl;
+    return 1;
+  }
+
+  TString filelist{ "" };
+  TDatime fromTime{ "1995-1-1 00:00:00" };
+  TDatime toTime{ "2037-1-1 00:00:00" };
+  TString entryList{ "" };
+
+  for (int i = 0; i < argc; ++i) {
+    if (TString(argv[i]) == "--entrylist") {
+      entryList = argv[i + 1];
+    }
+    if (TString(argv[i]) == "--filelist") {
+      filelist = argv[i + 1];
+    }
+    if (TString(argv[i]) == "--from") {
+      fromTime = TDatime(argv[i + 1]);
+    }
+    if (TString(argv[i]) == "--to") {
+      toTime = TDatime(argv[i + 1]);
+    }
+  }
+
+  return GenerateEntryList(filelist.Data(), entryList.Data(), fromTime, toTime);
+}

PWG/muon/aaf/CMakeLists.txt
@@ -15,7 +15,7 @@
 
 #Module
 set(MODULE PWGmuonaaf)
-add_definitions(-D_MODULE_="${MODULE}")
+#add_definitions(-D_MODULE_="${MODULE}")
 
 # Module include folder
 include_directories(${AliPhysics_SOURCE_DIR}/PWG/muon/aaf)
@@ -23,6 +23,7 @@ include_directories(${AliPhysics_SOURCE_DIR}/PWG/muon/aaf)
 # Additional includes - alphabetical order except ROOT
 include_directories(${ROOT_INCLUDE_DIRS}
                     ${AliPhysics_SOURCE_DIR}/PWG/muon
+                    ${AliRoot_SOURCE_DIR}/STEER/STEER
                     )
 
 # Sources - alphabetical order
@@ -51,6 +52,8 @@ add_library_tested(${MODULE} SHARED  ${SRCS} G__${MODULE}.cxx)
 set(EXE aaf-stage-and-filter)
 
 add_executable(${EXE} ${EXE}.cxx)
+add_executable(aliraw-generate-entrylist aliraw-generate-entrylist.cxx)
+add_executable(aliraw-apply-entrylist aliraw-apply-entrylist.cxx)
 
 # Generate the ROOT map
 # Dependecies
@@ -60,6 +63,8 @@ generate_rootmap("${MODULE}" "${LIBDEPS}" "${CMAKE_CURRENT_SOURCE_DIR}/${MODULE}
 # Linking the library
 target_link_libraries(${MODULE} ${LIBDEPS})
 target_link_libraries(${EXE} ${MODULE} Core)
+target_link_libraries(aliraw-generate-entrylist ${MODULE} Core RAWDatarec)
+target_link_libraries(aliraw-apply-entrylist ${MODULE} Core RIO )
 
 # Public include folders that will be propagated to the dependecies
 target_include_directories(${MODULE} PUBLIC ${incdirs})
@@ -77,3 +82,5 @@ install(TARGETS ${MODULE}
 install(FILES ${HDRS} DESTINATION include)
 
 install(TARGETS ${EXE} RUNTIME DESTINATION bin)
+install(TARGETS aliraw-generate-entrylist RUNTIME DESTINATION bin)
+install(TARGETS aliraw-apply-entrylist RUNTIME DESTINATION bin)

commit 77b1647ce43706ba0461cfccdb7f04bb76d4b9ac

PWG/muon/aaf: fix build errors of C++11 features
Introduced in 6d13160aa48763c80244039c55897c2936960126

PWG/muon/aaf/aliraw-apply-entrylist.cxx
@@ -5,6 +5,7 @@
 #include "TTree.h"
 #include <cstdlib>
 #include <sstream>
+#include <memory>
 
 // TTree* tree = (TTree*) fFile->Get("RAW");
 // if (!tree) {

PWG/muon/aaf/aliraw-generate-entrylist.cxx
@@ -3,6 +3,8 @@
 #include "TDatime.h"
 #include "TString.h"
 #include "TSystem.h"
+#include <algorithm>
+#include <memory>
 
 struct EventInfo {
   Int_t mEventNumber;
@@ -30,7 +32,7 @@ void GenerateEntryListOneChunk(const char* infile, std::ostream& outfile, const 
   }
   if (!eventNumbers.empty()) {
     std::sort(begin(eventNumbers), end(eventNumbers),
-              [](EventInfo& i, EventInfo& j) -> bool { return i.mTimeStamp < j.mTimeStamp; });
+              [](const EventInfo& i, const EventInfo& j) -> bool { return i.mTimeStamp < j.mTimeStamp; });
     outfile << infile << std::endl << eventNumbers.size() << std::endl;
     for (auto ei : eventNumbers) {
       outfile << ei.mEventNumber << " ";