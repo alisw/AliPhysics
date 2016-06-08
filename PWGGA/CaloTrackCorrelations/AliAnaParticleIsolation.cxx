aa9458c712a5f22e79b3997b214ed93b378564fc
dlodato <davide.francesco.lodato@cern.ch>
2016-06-08 14:33:18 +0200

    Fix Entries and Filling of 2 histograms

--- PWGGA/CaloTrackCorrelations/AliAnaParticleIsolation.cxx
+++ PWGGA/CaloTrackCorrelations/AliAnaParticleIsolation.cxx
@@ -954,12 +954,17 @@ void AliAnaParticleIsolation::CalculateCaloSignalInCone(AliAODPWG4ParticleCorrel
   coneptsumCluster  = 0;
   
   if( GetIsolationCut()->GetParticleTypeInCone()==AliIsolationCut::kOnlyCharged ) return ;
-  
+
+  Float_t ptTrig = aodParticle->Pt();
+
   // Recover reference arrays with clusters and tracks
   TObjArray * refclusters = aodParticle->GetObjArray(GetAODObjArrayName()+"Clusters");
-  if(!refclusters) return ;
-  
-  Float_t ptTrig = aodParticle->Pt();
+  if(!refclusters){
+    fhConeSumPtCluster ->Fill(ptTrig, coneptsumCluster , GetEventWeight());
+    fhConePtLeadCluster->Fill(ptTrig, coneptLeadCluster, GetEventWeight());
+    
+    return ;
+  }
   
   // Get vertex for cluster momentum calculation
   Double_t vertex[] = {0,0,0} ; //vertex ;
@@ -1089,13 +1094,20 @@ void AliAnaParticleIsolation::CalculateCaloCellSignalInCone(AliAODPWG4ParticleCo
 void AliAnaParticleIsolation::CalculateTrackSignalInCone(AliAODPWG4ParticleCorrelation * aodParticle,
                                                          Float_t & coneptsumTrack, Float_t & coneptLeadTrack)
 {
+  coneptLeadTrack = 0;
+  coneptsumTrack  = 0;
   if( GetIsolationCut()->GetParticleTypeInCone()==AliIsolationCut::kOnlyNeutral ) return ;
   
+  Float_t  ptTrig = aodParticle->Pt();
   // Recover reference arrays with clusters and tracks
   TObjArray * reftracks   = aodParticle->GetObjArray(GetAODObjArrayName()+"Tracks");
-  if(!reftracks) return ;
+  if(!reftracks){
+    fhConeSumPtTrack ->Fill(ptTrig, coneptsumTrack , GetEventWeight());
+    fhConePtLeadTrack->Fill(ptTrig, coneptLeadTrack, GetEventWeight());
+    
+    return ;
+  }
   
-  Float_t  ptTrig = aodParticle->Pt();
   Double_t bz     = GetReader()->GetInputEvent()->GetMagneticField();
   
   Float_t pTtrack       = 0;
