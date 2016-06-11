54ad0986f6881797220fe91b22c06f6b0c09697f
dstocco <diego.stocco@cern.ch>
2016-06-11 16:41:23 +0200

    Bug fix: account for bins with no entries
    Also improve the determination of the Apt/All from data

--- PWG/muon/AliMTRParameterizedResponse.h
+++ PWG/muon/AliMTRParameterizedResponse.h
@@ -30,10 +30,10 @@ class AliMTRParameterizedResponse : public TObject {
     kHptOverLpt
   };
 
-  Bool_t FitResponses();
+  Bool_t FitResponses ( Bool_t buildDataAptOverAllFromGraph = kTRUE, Bool_t fitLowPtIncrease = kFALSE );
 
-  Double_t WeightPerBoard ( Double_t pt, Int_t iboard, Int_t itype, Bool_t isMC, Bool_t useFit );
-  Double_t WeightPerEta ( Double_t pt, Double_t eta, Int_t itype, Bool_t isMC, Bool_t useFit );
+  Double_t WeightPerBoard ( Double_t pt, Int_t iboard, Int_t itype, Bool_t isMC, Bool_t useFit ) const;
+  Double_t WeightPerEta ( Double_t pt, Double_t eta, Int_t itype, Bool_t isMC, Bool_t useFit ) const;
 
   Bool_t SetAptOverAllMC ( TH2* histoApt, TH2* histoAll );
   Bool_t SetHptOverLpt ( TH2* histoHpt, TH2* histoLpt, TH2* histoMCHpt = 0x0, TH2* histoMCLpt = 0x0 );
@@ -45,18 +45,18 @@ class AliMTRParameterizedResponse : public TObject {
                                         Int_t rebin = 3 );
 //                                       const char* outputNameData = "MTRResponseOut", const char* outputNameMC = "MTRResponseOut" const char* identifierData = "out", const char* identifierMC = "out");
 
-  Bool_t ShowResponses ( Int_t itype, Bool_t isMC, Bool_t perBoard );
-  Bool_t CompareResponses ( Int_t itype, Bool_t perBoard );
+  Bool_t ShowResponses ( Int_t itype, Bool_t isMC, Bool_t perBoard ) const;
+  Bool_t CompareResponses ( Int_t itype, Bool_t perBoard ) const;
 
 
  private:
 
-  Double_t GetWeight ( Double_t pt, Int_t ibin, Int_t itype, Bool_t isMC, Bool_t useFit, Bool_t perBoard );
+  Double_t GetWeight ( Double_t pt, Int_t ibin, Int_t itype, Bool_t isMC, Bool_t useFit, Bool_t perBoard ) const;
   Bool_t SetRatio ( TH2* histoNum, TH2* histoDen, Int_t itype, Bool_t isMC, Int_t rebin = 1 );
-  TObjArray* GetResponse ( Int_t itype, Bool_t isMC, Bool_t perBoard, Bool_t warn = kTRUE );
+  TObjArray* GetResponse ( Int_t itype, Bool_t isMC, Bool_t perBoard, Bool_t warn = kTRUE ) const;
   Bool_t AddResponse ( Int_t itype, Bool_t isMC, Bool_t perBoard, TObjArray* resp );
 
-  TString GetStdName ( Int_t itype, Bool_t isMC, Bool_t perBoard );
+  TString GetStdName ( Int_t itype, Bool_t isMC, Bool_t perBoard ) const;
 
   Double_t FitFunctionErf ( Double_t* xVal, Double_t *par );
   Double_t FitFunctionErfApt ( Double_t* xVal, Double_t *par );
