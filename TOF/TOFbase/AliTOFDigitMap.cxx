cb7b5e7ed95b7d832d0268674ea0c096e2016d88
swenzel <sandro.wenzel@cern.ch>
2016-07-07 12:47:34 +0200

    Remove C++11 auto keyword

--- TOF/TOFbase/AliTOFDigitMap.cxx
+++ TOF/TOFbase/AliTOFDigitMap.cxx
@@ -181,7 +181,7 @@ void AliTOFDigitMap::AddDigit(Int_t *vol, Int_t idigit)
   // Assign digit to pad vol
   //
   // 0 means empty pad, we need to shift indeces by 1
-  auto checkedindex = CheckedIndex(vol);
+  Int_t checkedindex = CheckedIndex(vol);
   if (fDigitMap[checkedindex][kMaxDigitsPerPad-1]!=0) {
     AliDebug(1,Form("In the volume (Se%i, Pl%i, St%i, PadR%i, Pad%i) there is not more possibility to add other digits.", vol[0], vol[1], vol[2], vol[4], vol[3]));
     AliDebug(1,Form("Then, the digit number %i will be not inserted in the digit map, i.e. it will be lost.", idigit));
