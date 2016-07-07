c06bc12a5c61c6297622d445f54dea0a168fa79c
vpacik <vojtech.pacik@cern.ch>
2016-07-07 18:49:34 +0200

    V0sInJets:Added track container for V0s filtering & modified AddTask macro for jet finding

--- PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
+++ PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
@@ -103,6 +103,7 @@
 #pragma link C++ namespace JETriggerRejectionAna+;
 #pragma link C++ class JETriggerRejectionAna::AliAnalysisTaskTriggerRejection+;
 #pragma link C++ class AliAnalysisTaskV0sInJetsEmcal+;
+#pragma link C++ class AliTrackContainerV0+;
 #pragma link C++ class AliEmcalPicoTrackFromJetMaker+;
 #pragma link C++ class AliNtupHetInfo+;
 #pragma link C++ class AliNtupCumInfo+;
