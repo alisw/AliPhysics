f36f2683bdc1b9aa3e2765001994e5bfa7b20c0d
dlodato <davide.francesco.lodato@cern.ch>
2016-07-10 12:31:21 +0200

    Switch off Selection of Hybrids via period string with AliEMCALTrackSelectionAOD when running MC

--- PWGGA/EMCALTasks/macros/AddTaskEMCALPhotonIsolation.C
+++ PWGGA/EMCALTasks/macros/AddTaskEMCALPhotonIsolation.C
@@ -103,9 +103,10 @@ AliAnalysisTaskEMCALPhotonIsolation* AddTaskEMCALPhotonIsolation(
     Printf("Error with Hybrids!!");
   tracksForAnalysis->SetName("filterTracksAna");
   tracksForAnalysis->SetFilterHybridTracks(kTRUE);
-  tracksForAnalysis->SetTrackCutsPeriod(periodstr);
+  if(!bIsMC){
+    tracksForAnalysis->SetTrackCutsPeriod(periodstr);
   tracksForAnalysis->SetDefTrackCutsPeriod(periodstr);
-  
+  }
   Printf("Name of Tracks for matching: %s \n Name for Tracks for Isolation: %s",trackCont->GetName(),tracksForAnalysis->GetName());
   
   printf("Task for neutral cluster analysis created and configured, pass it to AnalysisManager\n");
