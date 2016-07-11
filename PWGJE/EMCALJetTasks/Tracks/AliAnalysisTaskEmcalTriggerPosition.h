alisw/AliPhysics

commit 8d175d604a3985373b48b41b457ba6ce8d67ce11

Add simple task comparing the position of recalculate gamma patches and gamma patches found by the STU

PWGJE/EMCALJetTasks/CMakeLists.txt
@@ -60,6 +60,7 @@ set(SRCS
     AliJetRandomizerTask.cxx
     AliJetResponseMaker.cxx
     AliJetTriggerSelectionTask.cxx
+    Tracks/AliAnalysisTaskEmcalTriggerPosition.h
     Tracks/AliAnalysisTaskPtEMCalTrigger.cxx
     Tracks/AliAnalysisTaskPtEMCalTriggerV1.cxx
     Tracks/AliCutValueRange.cxx

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalTriggerPosition.h
@@ -1,0 +1,27 @@
+#ifndef ALIANALYSISTASKEMCALTRIGGERPOSITION_H
+#define ALIANALYSISTASKEMCALTRIGGERPOSITION_H
+
+#include "AliAnalysisTaskSE.h"
+
+class THistManager;
+
+class AliAnalysisTaskEmcalTriggerPosition : public AliAnalysisTaskSE {
+public:
+  AliAnalysisTaskEmcalTriggerPosition();
+  AliAnalysisTaskEmcalTriggerPosition(const char *name);
+  virtual ~AliAnalysisTaskEmcalTriggerPosition();
+
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t *opt);
+
+  void SetEG1Threshold(double threshold) { fThresholdEG1 = threshold; }
+
+protected:
+
+  THistManager                  *fHistos;           //!
+  Double_t                       fThresholdEG1;
+
+  ClassDef(AliAnalysisTaskEmcalTriggerPosition, 1);
+};
+
+#endif /* ALIANALYSISTASKEMCALTRIGGERPOSITION_H_ */

PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
@@ -108,6 +108,7 @@
 #pragma link C++ class AliNtupHetInfo+;
 #pragma link C++ class AliNtupCumInfo+;
 #pragma link C++ class AliNtupZdcInfo+;
+#pragma link C++ class AliAnalysisTaskEmcalTriggerPosition+;
 #pragma link C++ namespace EMCalTriggerPtAnalysis+;
 #pragma link C++ class EMCalTriggerPtAnalysis::AliCutValueRange<float>+;
 #pragma link C++ class EMCalTriggerPtAnalysis::AliCutValueRange<double>+;

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalTriggerPosition.cxx
@@ -1,0 +1,68 @@
+/*
+ * AliAnalysisTaskEmcalTriggerPosition.cxx
+ *
+ *  Created on: 11.07.2016
+ *      Author: markusfasel
+ */
+#include <TClonesArray.h>
+#include <THistManager.h>
+#include <TString.h>
+
+#include "AliVEvent.h"
+#include "AliEMCALTriggerPatchInfo.h"
+
+#include "AliAnalysisTaskEmcalTriggerPosition.h"
+
+ClassImp(AliAnalysisTaskEmcalTriggerPosition)
+
+AliAnalysisTaskEmcalTriggerPosition::AliAnalysisTaskEmcalTriggerPosition():
+  AliAnalysisTaskSE(),
+  fHistos(nullptr),
+  fThresholdEG1(0.)
+{
+
+}
+
+AliAnalysisTaskEmcalTriggerPosition::AliAnalysisTaskEmcalTriggerPosition(const char *name):
+  AliAnalysisTaskSE(name),
+  fHistos(nullptr),
+  fThresholdEG1(0.)
+{
+  DefineOutput(1, TList::Class());
+}
+
+AliAnalysisTaskEmcalTriggerPosition::~AliAnalysisTaskEmcalTriggerPosition() {
+}
+
+void AliAnalysisTaskEmcalTriggerPosition::UserCreateOutputObjects(){
+  fHistos = new THistManager("EG1position");
+
+  fHistos->CreateTH2("posRecalcEG1", "Position of recalc EG1 patches", 48, -0.5, 47.5, 104, -0.5, 103.5);
+  fHistos->CreateTH2("posOnlineEG1", "Position of recalc EG1 patches", 48, -0.5, 47.5, 104, -0.5, 103.5);
+
+  PostData(1, fHistos->GetListOfHistograms());
+}
+
+void AliAnalysisTaskEmcalTriggerPosition::UserExec(Option_t *opt){
+  // Select only EG1 events
+  TString firedtriggers(InputEvent()->GetFiredTriggerClasses());
+  if(!firedtriggers.Contains("EG1")) return;
+  if(!(fInputHandler->IsEventSelected() & AliVEvent::kEMCEGA)) return;
+
+  TClonesArray *triggerpatches = dynamic_cast<TClonesArray *>(InputEvent()->FindListObject("emcalTriggers"));
+
+  AliEMCALTriggerPatchInfo *currentpatch(nullptr);
+  for(TIter patchiter = TIter(triggerpatches).Begin(); patchiter != TIter::End(); ++patchiter){
+    currentpatch = static_cast<AliEMCALTriggerPatchInfo *>(*patchiter);
+    if(currentpatch->GetPatchSize() != 2) continue;
+    // Reject L0 patches
+    if(currentpatch->IsLevel0() || currentpatch->IsLevel0Recalc() || currentpatch->IsLevel0Simple()) continue;
+    if(currentpatch->GetADCAmp() > this->fThresholdEG1)
+      fHistos->FillTH1("posRecalcEG1", currentpatch->GetColStart(), currentpatch->GetRowStart());
+    if(currentpatch->IsGammaHigh())
+      fHistos->FillTH1("posOnlineEG1", currentpatch->GetColStart(), currentpatch->GetRowStart());
+  }
+
+  PostData(1, fHistos->GetListOfHistograms());
+}
+

PWGJE/EMCALJetTasks/macros/AddTaskEmcalTriggerPosition.C
@@ -1,0 +1,15 @@
+AliAnalysisTaskEmcalTriggerPosition *AddTaskEmcalTriggerPosition(){
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+
+  AliAnalysisTaskEmcalTriggerPosition *task = new AliAnalysisTaskEmcalTriggerPosition("triggerpos");
+  mgr->AddTask(task);
+
+  TString outputfile = mgr->GetCommonFileName();
+  outputfile += ":emcaltriggerpos";
+
+  mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+  mgr->ConnectOutput(task, 1, mgr->CreateContainer("EMCALtriggerPosition", TList::Class(), AliAnalysisManager::kOutputContainer, outputfile.Data()));
+  return task;
+}
+
+

commit 65a442964b465e52e282a34ebc9460d634d92569

Fix compilation errors in EmcalTriggerPosition
Introduced along with 8d175d604a3985373b48b41b457ba6ce8d67ce11

PWGJE/EMCALJetTasks/CMakeLists.txt
@@ -60,7 +60,7 @@ set(SRCS
     AliJetRandomizerTask.cxx
     AliJetResponseMaker.cxx
     AliJetTriggerSelectionTask.cxx
-    Tracks/AliAnalysisTaskEmcalTriggerPosition.h
+    Tracks/AliAnalysisTaskEmcalTriggerPosition.cxx
     Tracks/AliAnalysisTaskPtEMCalTrigger.cxx
     Tracks/AliAnalysisTaskPtEMCalTriggerV1.cxx
     Tracks/AliCutValueRange.cxx

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalTriggerPosition.h
@@ -2,6 +2,7 @@
 #define ALIANALYSISTASKEMCALTRIGGERPOSITION_H
 
 #include "AliAnalysisTaskSE.h"
+#include "AliInputEventHandler.h"
 
 class THistManager;
 