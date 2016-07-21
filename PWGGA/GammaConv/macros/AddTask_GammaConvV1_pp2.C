3b1c43b70ddfdf35b271365dc112d39862899f60
dmuhlhei <d.muehlheim@cern.ch>
2016-07-21 11:43:37 +0200

    small fixes for AddTasks

--- PWGGA/GammaConv/macros/AddTask_GammaConvV1_pp2.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvV1_pp2.C
@@ -144,6 +144,7 @@ void AddTask_GammaConvV1_pp2(  Int_t    trainConfig                 = 1,        
       fEventCuts= new AliConvEventCuts(cutnumberEvent.Data(),cutnumberEvent.Data());
       fEventCuts->SetPreSelectionCutFlag(kTRUE);
       fEventCuts->SetV0ReaderName(V0ReaderName);
+      if (periodNameV0Reader.CompareTo("") != 0) fEventCuts->SetPeriodEnum(periodNameV0Reader);
       fEventCuts->SetLightOutput(runLightOutput);
       if(fEventCuts->InitializeCutsFromCutString(cutnumberEvent.Data())){
         fV0ReaderV1->SetEventCuts(fEventCuts);
@@ -158,7 +159,6 @@ void AddTask_GammaConvV1_pp2(  Int_t    trainConfig                 = 1,        
       fCuts->SetPreSelectionCutFlag(kTRUE);
       fCuts->SetIsHeavyIon(isHeavyIon);
       fCuts->SetV0ReaderName(V0ReaderName);
-      if (periodNameV0Reader.CompareTo("") != 0) fEventCuts->SetPeriodEnum(periodNameV0Reader);
       fCuts->SetLightOutput(runLightOutput);
       if(fCuts->InitializeCutsFromCutString(cutnumberPhoton.Data())){
         fV0ReaderV1->SetConversionCuts(fCuts);
