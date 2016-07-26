c800f644727fee00eb918f324e373ed939037160
drohr <drohr@cern.ch>
2016-07-26 16:54:15 +0200

    update cook hcdb script: Create a default huffman table with diffential compression to match HLT settings in data replay

--- HLT/programs/cookOCDB.sh
+++ HLT/programs/cookOCDB.sh
@@ -76,3 +76,7 @@ rm -f $TARGET/HLT/ConfigTPC/TPCDataCompressor/*
 rm -f $TARGET/HLT/ConfigTPC/TPCDataCompressorHuffmanTables/*
 cp $SOURCE//HLT/ConfigTPC/TPCDataCompressor/* $TARGET/HLT/ConfigTPC/TPCDataCompressor
 cp $SOURCE//HLT/ConfigTPC/TPCDataCompressorHuffmanTables/* $TARGET/HLT/ConfigTPC/TPCDataCompressorHuffmanTables
+
+#Create a default huffman table with diffential compression to match HLT settings in data replay
+aliroot -l -q -b $ALICE_SOURCE/HLT/programs/adjustOCDBObject.C"(\"$TARGET/HLT/ConfigTPC/TPCDataCompressorHuffmanTables/Run252209_999999999_v2_s0.root\", \"local://$TARGET\", 0, -1, 1)"
+
