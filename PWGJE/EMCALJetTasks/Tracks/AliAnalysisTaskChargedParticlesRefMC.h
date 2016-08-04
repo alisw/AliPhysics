2124b498a89b566ecdfdedde5600e2f7fd036428
mfasel <markus.fasel@cern.ch>
2016-08-04 14:28:11 +0200

    New task studying charge particle spectra as function of the trigger patch energy
    Instead of pre-defined trigger classes with fixed energy thresholds the energy
    threshold is tunable inside the task. The task is based on AliAnalysisTaskEmcal,
    however it replaces the default event selection by its own event selection.

--- PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskChargedParticlesRefMC.h
+++ PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskChargedParticlesRefMC.h
@@ -4,6 +4,7 @@
  * See cxx source for full Copyright notice                               */
 
 #include "AliAnalysisTaskSE.h"
+#include "AliCutValueRange.h"
 #include <TString.h>
 #include <TCustomBinning.h>
 
@@ -44,18 +45,13 @@ public:
   AliAnalysisTaskChargedParticlesRefMC(const char *name);
   virtual ~AliAnalysisTaskChargedParticlesRefMC();
 
-  void                        UserCreateOutputObjects();
-  void                        UserExec(Option_t *);
-  Bool_t                      UserNotify();
-  void                        Terminate(Option_t *) {}
-
   void                        SetRapidityShift(Double_t yshift) { fYshift = yshift; }
   void                        SetBeamDirection(BeamDirection_t beamdir) { fEtaSign = static_cast<Double_t>(beamdir); }
 
   void                        SetAnalysisUtil(AliAnalysisUtils *util) { fAnalysisUtil = util; }
   void                        SetTrackSelection(AliEmcalTrackSelection * sel) { fTrackCuts = sel; }
-  void                        SetEtaLabCut(double etamin, double etamax) { fEtaLabCut[0] = etamin; fEtaLabCut[1] = etamax; }
-  void                        SetEtaCMSCut(double etamin, double etamax) { fEtaCmsCut[0] = etamin; fEtaCmsCut[1] = etamax; }
+  void                        SetEtaLabCut(double etamin, double etamax) { fEtaLabCut.SetLimits(etamin, etamax); }
+  void                        SetEtaCMSCut(double etamin, double etamax) { fEtaCmsCut.SetLimits(etamin, etamax); }
   void                        SetOutlierCut(double fracpthard = 1.2) { fFracPtHard = fracpthard; }
   void                        SetOfflineTriggerSelection(AliEmcalTriggerOfflineSelection *sel) { fTriggerSelection = sel; }
   void                        SetUsePythiaHard(Bool_t usePythiaHard) { fUsePythiaHard = usePythiaHard; }
@@ -63,6 +59,11 @@ public:
   void                        SetWeightHandler(const AliEMCalTriggerWeightHandler * wh) { fWeightHandler = wh; }
 
 protected:
+  void                        UserCreateOutputObjects();
+  void                        UserExec(Option_t *);
+  Bool_t                      UserNotify();
+  void                        Terminate(Option_t *) {}
+
   void                        FillTrackHistos(const char *eventclass, Double_t weight, Double_t pt, Double_t eta, Double_t etacent, Double_t phi, Bool_t etacut, Bool_t inEmcal, Bool_t hasTRD, const char *pid);
   void                        FillTriggerJetHistograms(Bool_t aftercut, AliGenPythiaEventHeader *const header);
 
@@ -70,8 +71,19 @@ protected:
   Bool_t                      PythiaInfoFromFile(const char* currFile, Float_t &fXsec, Float_t &fTrials, Int_t &pthard) const;
   AliGenPythiaEventHeader    *GetPythiaHeader() const;
   Bool_t                      IsPhysicalPrimary(const AliVParticle *const part, AliMCEvent *const mcevent);
-  Bool_t                      IsOutlier(AliGenPythiaEventHeader * const header) const;
+  Bool_t                      IsOutlierJet(AliGenPythiaEventHeader * const header) const;
+  Bool_t                      IsOutlierParticle(AliGenPythiaEventHeader * const header) const;
 
+private:
+
+  class PtBinning : public TCustomBinning{
+  public:
+    PtBinning();
+    virtual ~PtBinning() {}
+  };
+
+  AliAnalysisTaskChargedParticlesRefMC(const AliAnalysisTaskChargedParticlesRefMC &);
+  AliAnalysisTaskChargedParticlesRefMC &operator=(const AliAnalysisTaskChargedParticlesRefMC &);
 
   AliEmcalTrackSelection                *fTrackCuts;                ///< Standard track selection
   AliAnalysisUtils                      *fAnalysisUtil;             ///< Event selection
@@ -81,33 +93,17 @@ protected:
   const AliEMCalTriggerWeightHandler    *fWeightHandler;            ///< Weight handler (optional)
 
   // Monte-Carlo specific information
-  Bool_t                          fUsePythiaHard;             ///< flag whether using PYTHIA Hard
-  Double_t                        fPtHard;                    ///< event pt hard
-  Int_t                           fPtHardBin;                 ///< event pt hard bin
-  Int_t                           fNTrials;                   ///< event trials
-  Float_t                         fXsection;                  ///< x-section from pythia header
+  Bool_t                                fUsePythiaHard;             ///< flag whether using PYTHIA Hard
+  Double_t                              fPtHard;                    ///< event pt hard
+  Int_t                                 fPtHardBin;                 ///< event pt hard bin
+  Int_t                                 fNTrials;                   ///< event trials
+  Float_t                               fXsection;                  ///< x-section from pythia header
 
-  Double_t                        fYshift;                    ///< Rapidity shift
-  Double_t                        fEtaSign;                   ///< Sign of the eta distribution (swaps when beam directions swap): p-Pb: +1, Pb-p: -1
-  Double_t                        fEtaLabCut[2];              ///< Cut applied in Eta Lab frame
-  Double_t                        fEtaCmsCut[2];              ///< Cut applied in Eta centre-of-mass frame
-  Double_t                        fFracPtHard;                ///< Cut on the maximum fraction of pt hard of any trigger jet
-
-private:
-
-  class OldPtBinning : public TCustomBinning{
-  public:
-    OldPtBinning();
-    virtual ~OldPtBinning() {}
-  };
-  class NewPtBinning : public TCustomBinning{
-  public:
-    NewPtBinning();
-    virtual ~NewPtBinning() {}
-  };
-
-  AliAnalysisTaskChargedParticlesRefMC(const AliAnalysisTaskChargedParticlesRefMC &);
-  AliAnalysisTaskChargedParticlesRefMC &operator=(const AliAnalysisTaskChargedParticlesRefMC &);
+  Double_t                              fYshift;                    ///< Rapidity shift
+  Double_t                              fEtaSign;                   ///< Sign of the eta distribution (swaps when beam directions swap): p-Pb: +1, Pb-p: -1
+  AliCutValueRange<double>              fEtaLabCut;                 ///< Cut applied in Eta Lab frame
+  AliCutValueRange<double>              fEtaCmsCut;                 ///< Cut applied in Eta centre-of-mass frame
+  Double_t                              fFracPtHard;                ///< Cut on the maximum fraction of pt hard of any trigger jet
 
   /// \cond CLASSIMP
   ClassDef(AliAnalysisTaskChargedParticlesRefMC, 1);
