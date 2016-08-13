f542c0afec90c3f6a0ece53c7abc42ee5464fac1
eulisse <giulio.eulisse@cern.ch>
2016-08-13 18:27:16 +0200

    Revert "added standard V0 cuts"
    This reverts commit 94734805ef7daaf80b0d501bdc6215ba9d0d2107.
    
    This does not compile with error:
    
        PWGLF/SPECTRA/IdentifiedHighPt/train/AliAnaTaskV0EffDecomposition.h:53:43: error: 'fMinPt' was not declared in this scope
    
    Most likely cause it's simply missing a `double fMinPt` in the class
    declaration.

--- PWGLF/SPECTRA/IdentifiedHighPt/train/AliAnaTaskV0EffDecomposition.cxx
+++ PWGLF/SPECTRA/IdentifiedHighPt/train/AliAnaTaskV0EffDecomposition.cxx
@@ -1,12 +1,7 @@
 /*CHANGES:
 
  * 18 Jul 2016: changed output filename in AddTask.C
- * 05 Aug 2016: implement standard cuts, added also in .h
 
-
-
-TODO:
-* this is now for Lambda, but to make it general (i.e. for K0s as well), make sure the mass is general, search for "mass" or "1.11"
 
 
 */
@@ -46,20 +41,11 @@ AliAnaTaskV0EffDecomposition::AliAnaTaskV0EffDecomposition():
   fPdgV0(3122),       // default is Lambda
   fPdgPos(2212),      // default is p
   fPdgNeg(-211),      // default is pi-
-  fMinPt(0.1), //cuts added by tuva:
-  fMinPtV0(0.1), 
-  fLowPtFraction(0.01),
-  fMassCut(0.1),
-  fDecayRmax(100.),
-  fDecayRmin(5.0),  
-  fDcaDaugh(1.0),
-  fV0pndca(0.1),
-  fCospt(0.998),//end cuts by tuva
   fListOfObjects(0x0),
   hV0Gen(0x0),
   hV0Rec(0x0),
   hDaughterRec(0x0)
- {
+{
   // Default constructor (should not be used)
 }
 
@@ -77,15 +63,6 @@ AliAnaTaskV0EffDecomposition::AliAnaTaskV0EffDecomposition(const char *name):
   fPdgV0(3122),       // default is Lambda
   fPdgPos(2212),      // default is p
   fPdgNeg(-211),      // default is pi-
-  fMinPt(0.1), //cuts added by tuva:
-  fMinPtV0(0.1), 
-  fLowPtFraction(0.01),
-  fMassCut(0.1),
-  fDecayRmax(100.),
-  fDecayRmin(5.0),  
-  fDcaDaugh(1.0),
-  fV0pndca(0.1),
-  fCospt(0.998),//end cuts by tuva
   fListOfObjects(0x0),
   hV0Gen(0x0),
   hV0Rec(0x0),
@@ -191,46 +168,32 @@ void AliAnaTaskV0EffDecomposition::UserExec(Option_t *)
     
     if (TMath::Abs(zvtx) < fVtxCut) {	
       
-      // Global primary vertex
-      const AliAODVertex *vtx = fAOD->GetPrimaryVertex();
-      if (vtx->GetNContributors() < 2) {
+      
+      // 3) if the centrality is in the correct range
+      Float_t centrality = -10;
+      AliCentrality *centObject = fAOD->GetCentrality();
+      if(centObject)
+	centrality = centObject->GetCentralityPercentile("V0M"); 
+      if((centrality < fMaxCent) && (centrality>=fMinCent)) {
 	
-	// SPD primary vertex
-	const AliAODVertex *vtxSPD = fAOD->GetPrimaryVertexSPD();
-	if (vtxSPD->GetNContributors() < 2) {
+	// as we focus here on Pb-Pb data I decide that the simplest would be
+	// to demand that all 3 are ok before going on
 	
-	// Correlation between global Zvtx and SPD Zvtx
-	fZvtx = vtx->GetZ();
-	Float_t zvSPD = vtxSPD->GetZ();
-	if( TMath::Abs( fZvtx - zvSPD ) < 0.5){
-	  
-	  // 3) if the centrality is in the correct range
-	  Float_t centrality = -10;
-	  AliCentrality *centObject = fAOD->GetCentrality();
-	  if(centObject)
-	    centrality = centObject->GetCentralityPercentile("V0M"); 
-	  if((centrality < fMaxCent) && (centrality>=fMinCent)) {
-	    
-	    // as we focus here on Pb-Pb data I decide that the simplest would be
-	    // to demand that all 3 are ok before going on
-	    
-	    // Fill MC gen histograms
-	    ProcessMCTruthAOD();
-	    
-	    // Fill V0 histograms (loop over V0s)
-	    AnalyzeV0AOD();
-	    
-	    // Fill track pair histograms (loop over tracks)
-	    AnalyzeDaughtersAOD();	
-	    
-	    // In this final step we try to see for each generated V0 how many
-	    // times it was reconstructed as a V0 and/or a track pair
-	    AnalyzeRecMothersAOD();
-	  }
-	}
-	}
+	// Fill MC gen histograms
+	ProcessMCTruthAOD();
+	
+	// Fill V0 histograms (loop over V0s)
+	AnalyzeV0AOD();
+	
+	// Fill track pair histograms (loop over tracks)
+	AnalyzeDaughtersAOD();	
+
+	// In this final step we try to see for each generated V0 how many
+	// times it was reconstructed as a V0 and/or a track pair
+	AnalyzeRecMothersAOD();
       }
     }
+  }
 
   // Post output data.
   PostData(1, fListOfObjects);
@@ -320,17 +283,6 @@ void AliAnaTaskV0EffDecomposition::AnalyzeV0AOD() {
     
     // common part
     
-    //cuts added by tuva
-    if(aodV0->RadiusV0()<fDecayRmin)continue;
-    if(aodV0->RadiusV0()>fDecayRmax)continue;
-    if(TMath::Abs(aodV0->MassLambda()-1.116)>fMassCut)continue;
-    if(TMath::Abs(aodV0->MassAntiLambda()-1.116)>fMassCut)continue;
-    if(aodV0->Pt()<fMinPtV0)continue;
-    if(aodV0->CosPointingAngle(myBestPrimaryVertex)<fCospt)continue;
-    if(aodV0->DcaV0Daughters()>fDcaDaugh)continue;
-    Double_t ct = (aodV0->DecayLength(myBestPrimaryVertex))*(1.115683)/aodV0->P();
-    if(ct>3.0*7.89)continue;
-
     // AliAODTrack (V0 Daughters)
     AliAODVertex* vertex = aodV0->GetSecondaryVtx();
     if (!vertex) {
@@ -357,10 +309,11 @@ void AliAnaTaskV0EffDecomposition::AnalyzeV0AOD() {
       n_track = helpTrack;
     } 
     
-    //cuts added by tuva
-    if(TMath::Abs(p_track->Eta()) > fEtaCut || TMath::Abs(n_track->Eta()) > fEtaCut)continue;
+    // // Eta cut on decay products ?
+    // if(TMath::Abs(p_track->Eta()) > fEtaCut || TMath::Abs(n_track->Eta()) > fEtaCut)
+    //   continue;
     
-AliAODMCParticle* p_mother = ValidateTrack(p_track, fPdgPos);
+    AliAODMCParticle* p_mother = ValidateTrack(p_track, fPdgPos);
     if(!p_mother)
       continue;
     AliAODMCParticle* n_mother = ValidateTrack(n_track, fPdgNeg);
