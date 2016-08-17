609e84852e4039f9d55a430c7cb429d1b8b88177
drohr <drohr@cern.ch>
2016-08-17 17:04:52 +0200

    fix for AliHLTAnalysisManager: disable streaming in the correct process

--- HLT/global/physics/AliHLTAnalysisManagerComponent.cxx
+++ HLT/global/physics/AliHLTAnalysisManagerComponent.cxx
@@ -226,6 +226,18 @@ void* AliHLTAnalysisManagerComponent::AnalysisManagerInit(void*)
   {
     return((void*) -1);
   }
+
+  //this disables streaming of the fProducer and fConsumers data members of
+  //AliAnalysisDataContainer.
+  //It is needed to avoid memory leaks downstream.
+  //Proper fix in the container itself may be too dangerous
+  TClass::GetClass("AliAnalysisDataContainer")->
+    GetDataMember("fProducer")->
+    SetBit(BIT(2),0);
+  TClass::GetClass("AliAnalysisDataContainer")->
+    GetDataMember("fConsumers")->
+    SetBit(BIT(2),0);
+
   return(NULL);
 }
 
@@ -264,17 +276,6 @@ Int_t AliHLTAnalysisManagerComponent::DoInit( Int_t /*argc*/, const Char_t** /*a
     HLTError("could not SetupCTPData(); ENOMEM");
     return -ENOMEM;
   }
-
-  //this disables streaming of the fProducer and fConsumers data members of
-  //AliAnalysisDataContainer.
-  //It is needed to avoid memory leaks downstream.
-  //Proper fix in the container itself may be too dangerous
-  TClass::GetClass("AliAnalysisDataContainer")->
-    GetDataMember("fProducer")->
-    SetBit(BIT(2),0);
-  TClass::GetClass("AliAnalysisDataContainer")->
-    GetDataMember("fConsumers")->
-    SetBit(BIT(2),0);
 
   return 0;
 }
