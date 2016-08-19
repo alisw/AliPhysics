a96c4080c3be1c3ee05ff54f5f5ac632135f3c09
victor <victor.gonzalez@cern.ch>
2016-08-19 11:50:37 +0200

    Support for configuration files in alien

--- PWGPP/EVCHAR/FlowVectorCorrections/QnCorrectionsInterface/macros/AddTaskFlowQnVectorCorrectionsToLegoTrain.C
+++ PWGPP/EVCHAR/FlowVectorCorrections/QnCorrectionsInterface/macros/AddTaskFlowQnVectorCorrectionsToLegoTrain.C
@@ -33,7 +33,10 @@ void AddTaskFlowQnVectorCorrectionsToLegoTrain(const char *configpath = ".") {
   /* strange way of including the header file is for lego train scenarios */
   gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/EVCHAR/FlowVectorCorrections/QnCorrectionsInterface/macros/runAnalysis.H");
   gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/EVCHAR/FlowVectorCorrections/QnCorrectionsInterface/macros/loadRunOptions.C");
-  loadRunOptions(kFALSE, configpath);
+  if (!loadRunOptions(kFALSE, configpath)) {
+    cout << "ERROR: configuration options not loaded. ABORTING!!!" << endl;
+    return;
+  }
 
   gSystem->AddIncludePath("-I$ALICE_PHYSICS/include");
 
