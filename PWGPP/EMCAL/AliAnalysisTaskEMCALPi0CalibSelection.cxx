6b753641bf32e1bd0574d8d04b304c8de648f299
asvauthi <astrid.vauthier@cern.ch>
2016-08-30 13:07:18 +0200

    Add lower energy cut on high M02 clusters for bkg study

--- PWGPP/EMCAL/AliAnalysisTaskEMCALPi0CalibSelection.cxx
+++ PWGPP/EMCAL/AliAnalysisTaskEMCALPi0CalibSelection.cxx
@@ -305,7 +305,8 @@ void  AliAnalysisTaskEMCALPi0CalibSelection::CorrectClusters()
     AliVCluster *c1 = (AliVCluster *) fCaloClustersArr->At(iClu);
       
     Float_t e1i = c1->E();   // cluster energy before correction
-    if      (e1i < fEmin) continue;
+    if(fChangeBkgShape && (((c1->GetM02() > fL0Bkgmin) && (c1->GetM02() < fL0Bkgmax)) && (e1i < fEBkgmin))) continue;
+    else if (!fChangeBkgShape && e1i < fEmin) continue;
     else if (e1i > fEmax) continue;
       
     else if (c1->GetNCells() < fMinNCells)                   continue;
@@ -392,7 +393,8 @@ void AliAnalysisTaskEMCALPi0CalibSelection::FillHistograms()
     
     Float_t e1i = c1->E();   // cluster energy before correction   
     
-    if      (e1i < fEmin) continue;
+    if(fChangeBkgShape && (((c1->GetM02() > fL0Bkgmin) && (c1->GetM02() < fL0Bkgmax)) && (e1i < fEBkgmin))) continue;
+    else if (!fChangeBkgShape && e1i < fEmin) continue;
     else if (e1i > fEmax) continue;
     
     else if (!fRecoUtils->IsGoodCluster(c1,fEMCALGeo,fEMCALCells,bc)) continue;
@@ -486,7 +488,8 @@ void AliAnalysisTaskEMCALPi0CalibSelection::FillHistograms()
       AliAODCaloCluster *c2 = (AliAODCaloCluster *) fCaloClustersArr->At(jClu);
       
       Float_t e2i = c2->E();
-      if      (e2i < fEmin) continue;
+      if(fChangeBkgShape && (((c2->GetM02() > fL0Bkgmin) && (c2->GetM02() < fL0Bkgmax)) && (e2i < fEBkgmin))) continue;
+      else if (!fChangeBkgShape && e2i < fEmin) continue;
       else if (e2i > fEmax) continue;
       
       else if (!fRecoUtils->IsGoodCluster(c2,fEMCALGeo,fEMCALCells,bc))continue;
