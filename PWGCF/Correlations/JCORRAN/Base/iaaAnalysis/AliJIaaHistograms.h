ba30fd8d3fe3e7a88932b45e7bf484014ba64e75
aohlson <alice.ohlson@cern.ch>
2016-09-02 18:15:01 +0900

    reimplementing vz dependent dEta hists (Marton Vargyas)

--- PWGCF/Correlations/JCORRAN/Base/iaaAnalysis/AliJIaaHistograms.h
+++ PWGCF/Correlations/JCORRAN/Base/iaaAnalysis/AliJIaaHistograms.h
@@ -6,7 +6,7 @@
 //===========================================================
 // AliJIaaHistograms.h
 //
-// author: Jussi Viinikainen
+// author: Marton Vargyas
 //===========================================================
 
 #ifndef ALIJIAAHISTOGRAMS_H
@@ -34,25 +34,19 @@ class AliJTrack;
 
 using namespace std;
 
-class AliJIaaHistograms : public AliJHistogramInterface{
+class AliJIaaHistograms : public AliJHistogramInterface
+{
   
 public:
+
   AliJIaaHistograms(AliJCard* cardP); //constructor
   AliJIaaHistograms(const AliJIaaHistograms& obj); // copy constructor
 	virtual ~AliJIaaHistograms();    //destructor
   AliJIaaHistograms& operator=(const AliJIaaHistograms& obj); // equal sign operator
   
-  // create histograms
   void CreateCorrelationHistograms(); // Correlation histogram creation
-  void CreateEventTrackHistos(); // Event track histogram creation
-
   void ReadInclusiveHistos(const char *inclusFileName); // Inclusive histogram reader
-
-  //bool Is2DHistosEnabled(){ return fenable2DHistos; } // Getter for 2D histogram enabling flag
-  //void Set2DHistoCreate(bool isenable) { fenable2DHistos = isenable; } // Set 2D histograms for creation
-  //void SetAcceptanceCorrectionQA(bool isenable) { fEnableAcceptanceQAHistos = isenable; } // Set flag for QA histogram creation
-  
-public:
+  void CreateEventTrackHistos();
 
   //==================================================
   // 1D correlation histograms
@@ -61,7 +55,6 @@ public:
   AliJTH1D fhDphiAssoc;
   AliJTH1D fhDEtaNear;
   AliJTH1D fhDEtaNearM;
-  // JV // One dimensional deltaEta histograms for acceptance correction
   AliJTH1D fhDetaNearMixAcceptance;   // Mixed event uncorrected deltaEta histogram for acceptance correction
 
   //==================================================
@@ -69,7 +62,6 @@ public:
   //==================================================
 
   AliJTH2D fhDphiDetaPta;      // 2D histogram (deltaPhi-deltaEta plane) in [fTyp][fCentralityBin][fZBin][fpttBin][fptaBin] bins
-  //AliJTH2D fhDphiDetaPtaRgap;    // same with 3D near-side definition
   
   //==================================================
   // Trigger and associated particle specra
@@ -105,18 +97,6 @@ public:
   AliJTH1D fhCentr;
   AliJTH1D fhiCentr;
   AliJTH1D fhZVert; // z-vertex distribution
-  
-
-	//===================================================
-	// Acceptance correction QA histograms
-	// Not needed in the actual analysis, only for check
-	//===================================================
-
-	//AliJTH1D fhAcceptanceTraditional;   // Acceptance correction check for traditional 1D triangle
-	//AliJTH2D fhAcceptanceTraditional2D; // Acceptance correction check for traditional deltaEta deltaPhi distribution
-	//AliJTH2D fhAcceptance3DNearSide; // Acceptance correction check for 3D near side deltaEta deltaPhi distribution
-	//AliJTH2D fhAcceptanceTraditional2DZ; // Z-vertex binned acceptance correction check for traditional deltaEta deltaPhi distribution
-	//AliJTH2D fhAcceptance3DNearSideZ; // Z-vertex binned acceptance correction check for 3D near side deltaEta deltaPhi distribution
 
 protected:
 
