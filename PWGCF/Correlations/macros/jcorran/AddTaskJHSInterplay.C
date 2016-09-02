6323c6f31dc190fe4fdd4091d3de348ef3b7cc42
kimb <kimb@macbookdj.dyndns.cern.ch>
2016-09-02 17:33:30 +0200

    new HS Interplay code updated

---
+++ PWGCF/Correlations/macros/jcorran/AddTaskJHSInterplay.C
@@ -0,0 +1,39 @@
+//_____________________________________________________________________
+AliAnalysisTask *AddTaskJHSInterplay(TString taskName, Bool_t ismc, Bool_t iskinematiconly, UInt_t triggSel, TString cardName, TString cardSetting){
+	// Load Custom Configuration and parameters
+	// override values with parameters
+
+	cout<<"### DEGUG Input is "<< taskName << "\t"<< cardName <<"\t"<< cardSetting <<"\t"<<"#########"<<endl;
+	AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+
+	//==== JCORRAN Efficiency TASK
+	AliJHSInterplayTask *jhstask = new AliJHSInterplayTask(taskName.Data());
+	jhstask->SetDebugLevel(0);
+	jhstask->SetDebugMode(0);
+	jhstask->SetIsMC(ismc);
+	jhstask->SetKineOnly(iskinematiconly);
+	cout << jhstask->GetName() << endl;
+
+
+	// === Create AliJCORRAN ====
+	AliJCard *card = new AliJCard(cardName.Data());
+	card->PrintOut();
+	card->ReadLine( cardSetting.Data() );
+	card->ReCompile();
+	card->PrintOut();
+	jhstask->SetCard( card );
+	jhstask->SelectCollisionCandidates( triggSel );  //Apply offline trigger selection by AliPhysicsSelectionTask
+
+	mgr->AddTask((AliAnalysisTask*) jhstask);
+
+	// Create containers for input/output
+	AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
+
+	// Connect input/output
+	mgr->ConnectInput(jhstask, 0, cinput);
+	AliAnalysisDataContainer *jHist = mgr->CreateContainer(Form("%scontainer",jhstask->GetName()),  TDirectory::Class(), AliAnalysisManager::kOutputContainer, Form("%s:%s",AliAnalysisManager::GetCommonFileName(), jhstask->GetName()));
+	mgr->ConnectOutput(jhstask, 1, jHist );
+
+	return jhstask;
+}
+
