33dd18dac4f39c01505f4bfaf50c9d2edc182ab1
iarsene <iarsene@cern.ch>
2016-09-11 23:07:43 +0200

    Adding new cut classes and an analysis task for jpsi2ee analysis

--- PWGDQ/reducedTree/AliReducedEventCut.h
+++ PWGDQ/reducedTree/AliReducedEventCut.h
@@ -1,45 +1,28 @@
-// Class for cutting on event information
+// Class for cutting on ALICE Var manager and other event specific information
 // Author: Ionut-Cristian Arsene (iarsene@cern.ch)
-//   10/09/2015
+//   07/09/2016
 
 #ifndef ALIREDUCEDEVENTCUT_H
 #define ALIREDUCEDEVENTCUT_H
 
-#include "AliReducedInfoCut.h"
+#include "AliReducedVarCut.h"
+#include "AliReducedVarManager.h"
 
 //_________________________________________________________________________
-class AliReducedEventCut : public AliReducedInfoCut {
+class AliReducedEventCut : public AliReducedVarCut {
 
  public:
   AliReducedEventCut();
   AliReducedEventCut(const Char_t* name, const Char_t* title);
   virtual ~AliReducedEventCut();
-  
+
   virtual Bool_t IsSelected(TObject* obj);
-  
-  void SetEventTagMask(ULong64_t mask) {fEventTagMask = mask; fCutOnEventTag=kTRUE;}
-  void SetTriggerMask(ULong64_t mask) {fTriggerMask = mask; fCutOnTriggerMask=kTRUE;}
-  void SetVertexZRange(Float_t min, Float_t max) {fVertexZRange[0]=min; fVertexZRange[1]=max; fCutOnVertexZ=kTRUE;}
-  void SetCentVZERORange(Float_t min, Float_t max) {fCentVZERORange[0]=min; fCentVZERORange[1]=max; fCutOnCentralityVZERO = kTRUE;}
-  void SetCutOnPhysicsSelection(Bool_t cut) {fCutOnPhysicsSelection=cut;}
+  virtual Bool_t IsSelected(TObject* obj, Float_t* values);
   
  protected: 
+      
+  // Cuts on event specific quantities
    
-  ULong64_t fEventTagMask;
-  Bool_t    fCutOnEventTag;
-  
-  ULong64_t fTriggerMask;
-  Bool_t    fCutOnTriggerMask;
-  
-  Float_t   fVertexZRange[2];
-  Bool_t    fCutOnVertexZ;
-  
-  Float_t   fCentVZERORange[2];
-  Bool_t    fCutOnCentralityVZERO;
-  
-  Bool_t    fCutOnPhysicsSelection;
-  
-  
   AliReducedEventCut(const AliReducedEventCut &c);
   AliReducedEventCut& operator= (const AliReducedEventCut &c);
   
