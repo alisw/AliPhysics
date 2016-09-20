70f3989a78830835bce6d42219baf20a3c2497bc
mpuccio <maximiliano.puccio@cern.ch>
2016-09-20 21:50:36 +0200

    Last version of the analysis macros

--- PWGLF/NUCLEX/Nuclei/NucleiPbPb/macros/src/FitModules.cxx
+++ PWGLF/NUCLEX/Nuclei/NucleiPbPb/macros/src/FitModules.cxx
@@ -27,7 +27,7 @@
 #include <RooChebychev.h>
 #include <RooBreitWigner.h>
 #include <RooBernstein.h>
-#include "RooGausDExp.h"
+#include "RooGausExp.h"
 
 
 void FitModule::FitData(TH1* dat,TString &name, TString &title, TString range) {
@@ -35,20 +35,19 @@ void FitModule::FitData(TH1* dat,TString &name, TString &title, TString range) {
   mPlot = mX->frame();
   mPlot->SetTitle(title.Data());
   mPlot->SetName(name.Data());
-  RooFitResult *res = mTemplate->fitTo(data,Extended(),Verbose(kFALSE),PrintLevel(-1),Range(range));
+  for (int i = 2; i--;) RooFitResult *res = mTemplate->fitTo(data,Extended(),Verbose(kFALSE),PrintLevel(-1),Range(range));
   data.plotOn(mPlot,Name("data"));
   mTemplate->plotOn(mPlot,Name("model"),Range(range),NormRange(range));
   mTemplate->plotOn(mPlot,Components(*mBackground),LineStyle(kDashed),Range(range),NormRange(range));
   mTemplate->plotOn(mPlot,Components(*mSignal),LineStyle(kDotted),Range(range),NormRange(range));
-  mTemplate->paramOn(mPlot,Label(Form("#chi^{2}/NDF = %2.4f",mPlot->chiSquare("model","data"))));
+  mChi2 = mPlot->chiSquare("model","data");
+  mTemplate->paramOn(mPlot,Label(Form("#chi^{2}/NDF = %2.4f",mChi2)));
   data.removeSelfFromDir();
 }
 
 FitExpGaus::FitExpGaus(RooRealVar *x) : FitModule(x) {
   mTau = new RooRealVar("mTau","tau bkg",-5.,0.);
   mBackground = new RooExponential("mBackground","Background",*mX,*mTau);
-  mMu = new RooRealVar("mMu","Mu",-0.1,0.1);
-  mSigma = new RooRealVar("mSigma","Sigma",0.01,0.6);
   mSignal = new RooGaussian("mSignal","Signal",*mX,*mMu,*mSigma);
   mTemplate = new RooAddPdf("mTemplate","Template",RooArgList(*mSignal,*mBackground),
                             RooArgList(*mSigCounts,*mBkgCounts));
@@ -68,13 +67,11 @@ FitExpCB::FitExpCB(RooRealVar *x) : FitModule(x) {
   mTau = new RooRealVar("mTau","tau bkg",-5.,0.);
   mBackground = new RooExponential("mBackground","Background",*mX,*mTau);
   mAlpha = new RooRealVar("mAlpha","Alpha",-4.,-1.75);
-  mMu = new RooRealVar("mMu","Mu",-0.1,0.1);
-  mSigma = new RooRealVar("mSigma","Sigma",0.01,0.6);
   mN = new RooRealVar("mN","n",3.,10.);
   mSignal = new RooCBShape("mSignal","Signal",*mX,*mMu,*mSigma,*mAlpha,*mN);
   mTemplate = new RooAddPdf("mTemplate","Template",RooArgList(*mSignal,*mBackground),
                             RooArgList(*mSigCounts,*mBkgCounts));
-  
+
 }
 
 FitExpCB::~FitExpCB() {
@@ -91,17 +88,15 @@ FitExpCB::~FitExpCB() {
 }
 
 FitExpExpCB::FitExpExpCB(RooRealVar *x) : FitModule(x) {
-  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-1.5);
-  mTau1 = new RooRealVar("mTau1","#tau_{1}",-1.5,0.);
+  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-2.);
+  mTau1 = new RooRealVar("mTau1","#tau_{1}",-2.,-0.2);
   mKbkg = new RooRealVar("mKbkg","mKbkg",0.,0.,1.);
   mKbkg->setConstant(false);
   mBkg0 = new RooExponential("mBkg0","background1",*mX,*mTau0);
   mBkg1 = new RooExponential("mBkg1","background2",*mX,*mTau1);
   mBackground = new RooAddPdf("mBackground","Background",RooArgList(*mBkg0,*mBkg1),RooArgList(*mKbkg));
   mAlpha = new RooRealVar("mAlpha","Alpha",-3.,-1.8);
-  mMu = new RooRealVar("mMu","Mu",0.,-0.2,0.2);
   mMu->setConstant(false);
-  mSigma = new RooRealVar("mSigma","Sigma",0.025,0.6);
   mN = new RooRealVar("mN","n",3.,40.);
   mSignal = new RooCBShape("mSignal","Signal",*mX,*mMu,*mSigma,*mAlpha,*mN);
   mTemplate = new RooAddPdf("mTemplate","Template",RooArgList(*mSignal,*mBackground),
@@ -124,16 +119,14 @@ FitExpExpCB::~FitExpExpCB() {
 }
 
 FitExpExpGaus::FitExpExpGaus(RooRealVar *x) : FitModule(x) {
-  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-1.5);
-  mTau1 = new RooRealVar("mTau1","#tau_{1}",-1.5,0.);
+  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-2.);
+  mTau1 = new RooRealVar("mTau1","#tau_{1}",-2.,-0.2);
   mKbkg = new RooRealVar("mKbkg","mKbkg",0.,0.,1.);
   mKbkg->setConstant(false);
   mBkg0 = new RooExponential("mBkg0","background1",*mX,*mTau0);
   mBkg1 = new RooExponential("mBkg1","background2",*mX,*mTau1);
   mBackground = new RooAddPdf("mBackground","Background",RooArgList(*mBkg0,*mBkg1),RooArgList(*mKbkg));
-  mMu = new RooRealVar("mMu","Mu",0.,-0.2,0.2);
   mMu->setConstant(false);
-  mSigma = new RooRealVar("mSigma","Sigma",0.025,0.9);
   mSignal = new RooGaussian("mSignal","Signal",*mX,*mMu,*mSigma);
   mTemplate = new RooAddPdf("mTemplate","Template",RooArgList(*mSignal,*mBackground),
                             RooArgList(*mSigCounts,*mBkgCounts));
@@ -153,32 +146,31 @@ FitExpExpGaus::~FitExpExpGaus() {
 }
 
 FitExpExpTailGaus::FitExpExpTailGaus(RooRealVar *x) : FitModule(x) {
-  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-1.5);
-  mTau1 = new RooRealVar("mTau1","#tau_{1}",-1.5,0.);
+  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-2.);
+  mTau1 = new RooRealVar("mTau1","#tau_{1}",-2.,-0.2);
   mKbkg = new RooRealVar("mKbkg","mKbkg",0.,0.,1.);
   mKbkg->setConstant(false);
   mBkg0 = new RooExponential("mBkg0","background1",*mX,*mTau0);
   mBkg1 = new RooExponential("mBkg1","background2",*mX,*mTau1);
   mBackground = new RooAddPdf("mBackground","Background",RooArgList(*mBkg0,*mBkg1),RooArgList(*mKbkg));
-  mAlpha0 = new RooRealVar("mAlpha0","Alpha0",-1.25,-1.25);
-  mAlpha1 = new RooRealVar("mAlpha1","Alpha1",1.2,5.);
-  mMu = new RooRealVar("mMu","Mu",0.,-0.2,0.2);
+  mAlpha0 = new RooRealVar("mAlpha0","Alpha0",1.6,3.); // tight range based on low pT
+  mAlpha0->setConstant(false);
   mMu->setConstant(false);
-  mSigma = new RooRealVar("mSigma","Sigma",0.025,0.6);
-  mSignal = new RooGausDExp("mSignal","Signal",*mX,*mMu,*mSigma,*mAlpha0,*mAlpha1);
+  mSignal = new RooGausExp("mSignal","Signal",*mX,*mMu,*mSigma,*mAlpha0);
   mTemplate = new RooAddPdf("mTemplate","Template",RooArgList(*mSignal,*mBackground),
                             RooArgList(*mSigCounts,*mBkgCounts));
 }
 
 void FitExpExpTailGaus::SetHighPt(bool flag) {
-  if (flag) {
+  FitModule::SetHighPt(flag);
+  /*if (flag) {
     mAlpha0->setVal(-5.);
     mAlpha0->setConstant(true);
   } else {
     mAlpha0->setConstant(false);
     mAlpha0->setRange(-5.,-1.5);
     mAlpha0->setVal(-1.8);
-  }
+  }*/
 }
 
 
@@ -191,7 +183,43 @@ FitExpExpTailGaus::~FitExpExpTailGaus() {
   delete mSigma;
   delete mSignal;
   delete mAlpha0;
-  delete mAlpha1;
+  delete mBkgCounts;
+  delete mSigCounts;
+  delete mTemplate;
+}
+
+
+FitExpPolTailGaus::FitExpPolTailGaus(RooRealVar *x) : FitModule(x) {
+  mTau0 = new RooRealVar("mTau0","#tau_{0}",-6.5,-2.);
+  mA0 = new RooRealVar("mA0","#a_{0}",-2.,2.);
+ // mA1 = new RooRealVar("mA1","#a_{1}",-20.,1.);
+  mKbkg = new RooRealVar("mKbkg","mKbkg",0.,0.,1.);
+  mKbkg->setConstant(false);
+  mBkg0 = new RooExponential("mBkg0","background1",*mX,*mTau0);
+  mBkg1 = new RooChebychev("mBkg1","background2",*mX,RooArgList(*mA0));
+  mBackground = new RooAddPdf("mBackground","Background",RooArgList(*mBkg0,*mBkg1),RooArgList(*mKbkg));
+  //mAlpha0 = new RooRealVar("mAlpha0","Alpha0",1.6,3.); // tight range based on low pT
+  //mAlpha0->setConstant(false);
+  mMu->setConstant(false);
+  mSignal = new RooGaussian("mSignal","Signal",*mX,*mMu,*mSigma);//,*mAlpha0);
+  mTemplate = new RooAddPdf("mTemplate","Template",RooArgList(*mSignal,*mBackground),
+                            RooArgList(*mSigCounts,*mBkgCounts));
+}
+
+void FitExpPolTailGaus::SetHighPt(bool flag) {
+  FitModule::SetHighPt(flag);
+}
+
+
+FitExpPolTailGaus::~FitExpPolTailGaus() {
+  delete mBackground;
+  delete mTau0;
+  delete mA0;
+  delete mKbkg;
+  delete mMu;
+  delete mSigma;
+  delete mSignal;
+  //delete mAlpha0;
   delete mBkgCounts;
   delete mSigCounts;
   delete mTemplate;
