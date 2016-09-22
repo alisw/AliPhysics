874aeee3f1f0a08d2add72ddfca41b7e3b7899b0
mfasel <markus.fasel@cern.ch>
2016-09-22 15:08:46 +0200

    Add option to get acceptance map from recalc patches (new trigger maker required)

--- PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEGAMonitor.h
+++ PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEGAMonitor.h
@@ -1,11 +1,9 @@
 #ifndef ALIANALYSISTASKEGAMONITOR_H
 #define ALIANALYSISTASKEGAMONITOR_H
 
-class AliAnalysisUtils;
-class AliEMCALGeometry;
 class THistManager;
 
-#include "AliAnalysisTaskSE.h"
+#include "AliAnalysisTaskEmcal.h"
 
 namespace EMCalTriggerPtAnalysis {
 
@@ -13,23 +11,29 @@ namespace EMCalTriggerPtAnalysis {
  * @class AliAnalysisTaskEGAMonitor
  * @brief Simplistic class, checks distribution of the online EGA trigger patches
  */
-class AliAnalysisTaskEGAMonitor : public AliAnalysisTaskSE {
+class AliAnalysisTaskEGAMonitor : public AliAnalysisTaskEmcal {
 public:
   AliAnalysisTaskEGAMonitor();
   AliAnalysisTaskEGAMonitor(const char *name);
   virtual ~AliAnalysisTaskEGAMonitor();
 
+  void SetUseRecalcPatches(Double_t recalcLow, Double_t recalcHigh) {
+    fUseRecalcPatches = kTRUE;
+    fRecalcLow = recalcLow;
+    fRecalcHigh = recalcHigh;
+  }
+
 protected:
   virtual void UserCreateOutputObjects();
-  virtual void UserExec(Option_t *);
-
-  virtual void ExecOnce();
+  virtual bool IsEventSelected();
+  virtual bool Run();
 
 private:
   THistManager                            *fHistos;           //!<!  Histogram manager
-  AliAnalysisUtils                        *fAnalysisUtils;    //!<!  Analysis utils
-  AliEMCALGeometry                        *fGeom;             //!<!  EMCAL geometry (for mapping)
-  Bool_t                                  fLocalInitialized;  //!<!  Local initialization
+
+  Bool_t                                   fUseRecalcPatches; ///< Defined whether to use recalc patches
+  Double_t                                 fRecalcLow;        ///< Low threshold for recalc gamma trigger
+  Double_t                                 fRecalcHigh;       ///< High threshold for recalc gamma trigger
 
   /// \cond CLASSIMP
   ClassDef(AliAnalysisTaskEGAMonitor, 1);
