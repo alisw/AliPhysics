05c4d5cdab59a570c0a3f5619210d3817f5b88e7
dberzano <dario.berzano@cern.ch>
2016-09-28 13:47:55 +0200

    Fix CTest for libMONITOR with system libs

--- test/load_library/LoadLib.C
+++ test/load_library/LoadLib.C
@@ -5,5 +5,10 @@ Int_t LoadLib(const TString &libName)
       libName.Contains("TPHIC"))
     gSystem->Load("libpythia6");
 
+  if (libName.Contains("MONITOR")) {
+    // Might load system-installed libraries (e.g. libdim.so)
+    gSystem->AddDynamicPath("/lib:/lib64:/usr/lib:/usr/lib64");
+  }
+
   return gSystem->Load(libName);
 }
