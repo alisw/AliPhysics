d5d5e1554df8b5add6a26004269776a836deca79
mfasel <markus.fasel@cern.ch>
2016-10-06 19:46:21 +0200

    Use intermediate layer AliAnalysisTaskEmcalTriggerBase to absorb common code
    Intermediate framework class AliAnalysisTaskEmcalTriggerBase inherits from
    AliAnalysisTaskEmcal + adds the event and trigger selection and basic histogram
    handling.

--- PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalClustersRef.h
+++ PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalClustersRef.h
@@ -6,48 +6,33 @@
 #include <string>
 #include <vector>
 
-#include "AliAnalysisTaskEmcal.h"
+#include "AliAnalysisTaskEmcalTriggerBase.h"
 #include "AliCutValueRange.h"
 #include <TCustomBinning.h>
 #include <TString.h>
 
-class AliOADBContainer;
-
-class TArrayD;
 class TClonesArray;
-class THistManager;
-class TList;
-class TObjArray;
-class TString;
 
 namespace EMCalTriggerPtAnalysis {
 
-class AliEmcalTriggerOfflineSelection;
-
-class AliAnalysisTaskEmcalClustersRef : public AliAnalysisTaskEmcal {
+class AliAnalysisTaskEmcalClustersRef : public AliAnalysisTaskEmcalTriggerBase {
 public:
   AliAnalysisTaskEmcalClustersRef();
   AliAnalysisTaskEmcalClustersRef(const char *name);
   virtual ~AliAnalysisTaskEmcalClustersRef();
 
-  void SetOfflineTriggerSelection(AliEmcalTriggerOfflineSelection *sel) { fTriggerSelection = sel; }
   void SetClusterContainer(TString clustercontname) { fNameClusterContainer = clustercontname; }
   void SetCreateTriggerStringFromPatches(Bool_t doUsePatches) { fTriggerStringFromPatches = doUsePatches; }
 
-  void SetRequestAnalysisUtil(Bool_t doRequest) { fRequestAnalysisUtil = doRequest; }
   void SetCentralityRange(double min, double max) { fCentralityRange.SetLimits(min, max); fRequestCentrality = true; }
-  void SetVertexRange(double min, double max) { fVertexRange.SetLimits(min, max); }
-  void SetDownscaleOADB(TString oadbname) { fNameDownscaleOADB = oadbname; }
 
 protected:
-
-  virtual void UserCreateOutputObjects();
+  virtual void CreateUserObjects() {}
+  virtual void CreateUserHistos();
+  virtual bool IsUserEventSelected();
   virtual bool Run();
-  virtual bool IsEventSelected();
-  virtual void ExecOnce();
-  virtual void RunChanged(Int_t runnumber);
+  virtual void UserFillHistosAfterEventSelection();
 
-  Double_t GetTriggerWeight(const TString &triggerclass) const;
   void GetPatchBoundaries(TObject *o, Double_t *boundaries) const;
   bool IsOfflineSimplePatch(TObject *o) const;
   bool SelectDCALPatch(TObject *o) const;
@@ -56,25 +41,14 @@ protected:
   double GetPatchEnergy(TObject *o) const;
 
   void FillClusterHistograms(const TString &triggerclass, double energy, double transversenergy, double eta, double phi, TList *triggerpatches);
-  void FillEventHistograms(const TString &triggerclass, double centrality, double vertexz);
-  TString GetFiredTriggerClassesFromPatches(const TClonesArray* triggerpatches) const;
   void FindPatchesForTrigger(TString triggerclass, const TClonesArray * triggerpatches, TList &foundpatches) const;
   Bool_t CorrelateToTrigger(Double_t etaclust, Double_t phiclust, TList *triggerpatches) const;
 
-  THistManager                        *fHistos;                   //!<! Histogram handler
-  AliEmcalTriggerOfflineSelection     *fTriggerSelection;         ///< EMCAL offline trigger selection tool
-  std::vector<std::string>            fAcceptTriggers;            //!<! Temporary container with list of selected triggers
   TString                             fNameClusterContainer;      ///< Name of the cluster container in the event
 
-  Bool_t                              fRequestAnalysisUtil;       ///< Switch on request for event selection using analysis utils
-  Bool_t                              fTriggerStringFromPatches;  ///< Build trigger string from trigger patches
   AliCutValueRange<double>            fCentralityRange;           ///< Selected centrality range
-  AliCutValueRange<double>            fVertexRange;               ///< Selected vertex range
-  Bool_t                              fRequestCentrality;         ///< Swich on request for centrality range
-
-  TString                             fNameDownscaleOADB;         ///< Name of the downscale OADB container
-  AliOADBContainer                    *fDownscaleOADB;            //!<! Container with downscale factors for different triggers
-  TObjArray                           *fDownscaleFactors;         //!<! Downscalfactors for given run
+  Bool_t                              fRequestCentrality;         ///< Switch on request for centrality range
+  Double_t                            fEventCentrality;           //!<! Current event centrality
 
 private:
 
