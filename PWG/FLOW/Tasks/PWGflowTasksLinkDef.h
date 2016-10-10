904ba1739972174668e5588170d0ffe674ad14c3
rbertens <rbertens@cern.ch>
2016-10-10 09:59:44 +0200

    remove test code for cascades

--- PWG/FLOW/Tasks/PWGflowTasksLinkDef.h
+++ PWG/FLOW/Tasks/PWGflowTasksLinkDef.h
@@ -40,7 +40,6 @@
 #pragma link C++ class AliAnalysisTaskMultiparticleCorrelations+;
 #pragma link C++ class AliAnalysisTaskPIDconfig+;
 #pragma link C++ class AliAnalysisTaskMultiparticleFemtoscopy+;
-#pragma link C++ class AliAnalysisTaskCascadeTester+;
 
 #endif
 
