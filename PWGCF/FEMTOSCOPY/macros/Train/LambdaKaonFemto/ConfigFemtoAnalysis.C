dcdb27238819564805b4b426aa4c7e04f200ffc3
jbuxton <jesse.thomas.buxton@cern.ch>
2016-10-10 09:54:30 -0400

    PWGCF:  Minor change to ...LambdaKaonFemto/ConfigFemtoAnalysis.C

--- PWGCF/FEMTOSCOPY/macros/Train/LambdaKaonFemto/ConfigFemtoAnalysis.C
+++ PWGCF/FEMTOSCOPY/macros/Train/LambdaKaonFemto/ConfigFemtoAnalysis.C
@@ -118,10 +118,8 @@ AliFemtoManager* ConfigFemtoAnalysis(const TString& aParamString="")
     rdr->SetReadV0(1);  //Read V0 information from the AOD and put it into V0Collection
     rdr->SetEPVZERO(kTRUE);  //to get event plane angle from VZERO
     rdr->SetCentralityFlattening(kFALSE);
-//TODO    rdr->SetPrimaryVertexCorrectionTPCPoints(tAnalysisConfig.implementVertexCorrections);
-//    rdr->SetReadMC(tAnalysisConfig.isMCRun);
-    rdr->SetPrimaryVertexCorrectionTPCPoints(true);
-    rdr->SetReadMC(false);
+    rdr->SetPrimaryVertexCorrectionTPCPoints(tAnalysisConfig.implementVertexCorrections);
+    rdr->SetReadMC(tAnalysisConfig.isMCRun);
   tManager->SetEventReader(rdr);
 
 
