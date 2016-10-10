e38618a58acfa5c21e9dafd855bba209f8b583b1
dberzano <dario.berzano@cern.ch>
2016-10-10 19:26:43 +0200

    Fix test library path for Homebrew libraries

--- test/load_library/LoadLib.C
+++ test/load_library/LoadLib.C
@@ -7,7 +7,7 @@ Int_t LoadLib(const TString &libName)
 
   if (libName.Contains("MONITOR")) {
     // Might load system-installed libraries (e.g. libdim.so)
-    gSystem->AddDynamicPath("/lib:/lib64:/usr/lib:/usr/lib64");
+    gSystem->AddDynamicPath("/lib:/lib64:/usr/lib:/usr/lib64:/usr/local/lib:/usr/local/lib64");
   }
 
   return gSystem->Load(libName);
