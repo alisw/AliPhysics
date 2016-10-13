3a012f721b393df0c19e31d73a634ffa11248e93
rhaake <ruediger.haake@cern.ch>
2016-10-14 01:08:21 +0200

    Added matching window

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskChargedJetsHadronCF.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskChargedJetsHadronCF.h
@@ -29,12 +29,13 @@ class AliAnalysisTaskChargedJetsHadronCF : public AliAnalysisTaskEmcalJet {
   void                        Terminate(Option_t *option);
 
   // ######### SETTERS/GETTERS
-  void                        ActivateJetMatching(const char* matchArray, Double_t maxDistance, Double_t minSharedFraction, Double_t matchThreshold, Int_t nLeading)
+  void                        ActivateJetMatching(const char* matchArray, Double_t maxDistance, Double_t minSharedFraction, Double_t matchMinPt, Double_t matchMaxPt, Int_t nLeading)
   {
     fJetMatchingMaxDistance = maxDistance;
     fJetMatchingArrayName = matchArray;
     fJetMatchingMinSharedFraction = minSharedFraction;
-    fJetMatchingThreshold = matchThreshold;
+    fJetMatchingMinPt = matchMinPt;
+    fJetMatchingMaxPt = matchMaxPt;
     fJetMatchingUseOnlyNLeading = nLeading; // currently only 1,2, or 0 (all) possible
   }
 
@@ -82,7 +83,8 @@ class AliAnalysisTaskChargedJetsHadronCF : public AliAnalysisTaskEmcalJet {
   TString                     fJetMatchingArrayName;                    ///< Name of array used to match jets
   Double_t                    fJetMatchingMaxDistance;                  ///< Max distance allowed to accept a matching jet (for embedding)
   Double_t                    fJetMatchingMinSharedFraction;            ///< An embedded jet must carry this pt fraction to be accepted
-  Double_t                    fJetMatchingThreshold;                    ///< Threshold applied on the matchArray jets
+  Double_t                    fJetMatchingMinPt;                        ///< Min pt cut applied on the matchArray jets
+  Double_t                    fJetMatchingMaxPt;                        ///< Max pt cut applied on the matchArray jets
   Int_t                       fJetMatchingUseOnlyNLeading;              ///< Number of matched leading jets that will be used
   std::vector<AliEmcalJet*>   fMatchedJets;                             ///< Jets matched in an event (embedded)
   std::vector<AliEmcalJet*>   fMatchedJetsReference;                    ///< Jets matched in an event (reference)
