6cd1a9cdc0592e1fa4ba1b06745f35e9b097edc4
iarsene <iarsene@cern.ch>
2016-10-17 13:56:13 +0200

    Changed AddTask and Config macros for Zhuo Zhou (zzhou)

--- PWGDQ/dielectron/macrosJPSI/AddTaskJpsi_zzhou_PbPb.C
+++ PWGDQ/dielectron/macrosJPSI/AddTaskJpsi_zzhou_PbPb.C
@@ -1,11 +1,11 @@
 Bool_t isAOD=kTRUE;
 Bool_t hasMC=kFALSE;
 Int_t iPeriod=-1;
-enum { k10b=0, k10c, k10d, k10e, k10f, k10h, k11a, k11d, k11h, k12h, k13b, k13c, k13d, k13e, k13f };
+enum { k10h, k11h };
 
 
 AliAnalysisTask* AddTaskJpsi_zzhou_PbPb(
-					   TString cfg="ConfigJpsi_zzhou_lowpt",
+					   TString cfg="ConfigJpsi_zzhou_lowpt.C",
 				           Bool_t gridconf=kTRUE,
 					   TString prod="",
 					   Bool_t isMC=kFALSE)
@@ -25,11 +25,6 @@ AliAnalysisTask* AddTaskJpsi_zzhou_PbPb(
   Bool_t isAOD=mgr->GetInputEventHandler()->IsA()==AliAODInputHandler::Class();
   if(isAOD) hasMC=isMC;
   
-  //Get the current train configuration
-  //  TString trainConfig=gSystem->Getenv("CONFIG_FILE");
-  //TString list=gSystem->Getenv("LIST");
-  //if( list.IsNull()) list=prod;
-  
   // selected period	   
   if( !prod.CompareTo("LHC10h") ) iPeriod = k10h;
   else if( !prod.CompareTo("LHC11h") ) iPeriod = k11h;
@@ -41,6 +36,37 @@ AliAnalysisTask* AddTaskJpsi_zzhou_PbPb(
   //     list.Contains("LHC12a17") ||
   //     list.Contains("fix")
   //     ) hasMC=kTRUE;
+
+  //set config file name
+  TString configFile("");
+  printf("pwd:        %s \n",gSystem->pwd());
+  if(cfg.IsNull()) cfg="ConfigJpsi_zzhou_lowpt.C";
+
+  // the different paths
+  TString alienPath("alien:///alice/cern.ch/user/z/zzhou/PWGDQ/dielectron/macrosJPSI/");
+  TString alirootPath("$ALICE_PHYSICS/PWGDQ/dielectron/macrosJPSI/");
+
+  // #####  alien config  #####
+  if(gridconf) {
+    if(!gSystem->Exec(Form("alien_cp %s/%s .",alienPath.Data(),cfg.Data()))) {
+      gSystem->Exec(Form("ls -l %s",gSystem->pwd()));
+      configFile=gSystem->pwd();
+    }
+    else {
+      printf("ERROR: couldn't copy file %s/%s from grid \n", alienPath.Data(),cfg.Data() );
+      return;
+    }
+  }
+  
+  // ##### aliroot config #####
+  else if(!gridconf) configFile=alirootPath.Data();
+  // ##### add config to path #####
+  configFile+="/";
+  configFile+=cfg.Data();  
+
+  // print overall configuration
+  printf("config:     %s Grid: %d \n",configFile.Data(),gridconf);
+  printf("#######------------overall configuration----------------#########\n");
   
   //create task and add it to the manager
   AliAnalysisTaskMultiDielectron *task=new AliAnalysisTaskMultiDielectron("Jpsi_zzhouPbPb");
@@ -51,36 +77,43 @@ AliAnalysisTask* AddTaskJpsi_zzhou_PbPb(
   switch(iPeriod) {
   case k11h: task->SetTriggerMask(AliVEvent::kMB+AliVEvent::kCentral+AliVEvent::kSemiCentral); break;
   }
-  mgr->AddTask(task);
-  
+
+
+  // event filter
   AliDielectronEventCuts *eventCuts=new AliDielectronEventCuts("eventCuts","Vertex Track && |vtxZ|<10 && ncontrib>0");
   if(isAOD) eventCuts->SetVertexType(AliDielectronEventCuts::kVtxAny);
   eventCuts->SetRequireVertex();
   eventCuts->SetMinVtxContributors(1);
   eventCuts->SetVertexZ(-10.,10.);
     //  task->SetTriggerOnV0AND();
-  eventCuts->SetCentralityRange(40.,100.);
+  eventCuts->SetCentralityRange(40.,90.);
   task->SetEventFilter(eventCuts);
   task->SetRejectPileup();
 		
-  gROOT->LoadMacro( cfg.Data() );
-  //gROOT->LoadMacro("ConfigJpsi_zzhou_lowpt.C");
-
   // use histogram to correct PID need etaCorr_2010.root from alien
   //TFile *file =TFile::Open("alien:///alice/cern.ch/user/z/zzhou/PWGDQ/dielectron/files/etaCorr_2010.root");
 
+  //load dielectron configuration file (only once)
+  TString checkconfig="ConfigJpsi_zzhou_lowpt";
+  if (!gROOT->GetListOfGlobalFunctions()->FindObject(checkconfig.Data()))
+    gROOT->LoadMacro(configFile.Data());
+  
+  //define default output container
+  TString containerName = "AnalysisResults.root";
+  
   //add dielectron analysis with different cuts to the task
   for (Int_t i=0; i<nDie; ++i){ //nDie defined in config file
     AliDielectron *jpsi=ConfigJpsi_zzhou_lowpt(i);
     if (!jpsi) continue;
     jpsi->SetHasMC(hasMC);
     task->AddDielectron(jpsi);
-  }
+  } //end : loop over configs
   
+  mgr->AddTask(task);
   
   //   task->SetTriggerOnV0AND();
   //   if ( trainConfig=="pp" ) task->SetRejectPileup();
-  
+
   //create output container
   AliAnalysisDataContainer *coutput1 =
     mgr->CreateContainer("jpsi_Default_tree",
@@ -92,19 +125,20 @@ AliAnalysisTask* AddTaskJpsi_zzhou_PbPb(
     mgr->CreateContainer("jpsi_Default_QA",
                          TList::Class(),
                          AliAnalysisManager::kOutputContainer,
-                         "AnalysisResults.root");
+                         containerName.Data());
   
   AliAnalysisDataContainer *cOutputHist2 =
     mgr->CreateContainer("jpsi_Default_CF",
                          TList::Class(),
                          AliAnalysisManager::kOutputContainer,
-                         "AnalysisResults.root");
+                         containerName.Data());
   
   AliAnalysisDataContainer *cOutputHist3 =
     mgr->CreateContainer("jpsi_Default_EventStat",
                          TH1D::Class(),
                          AliAnalysisManager::kOutputContainer,
-                         "AnalysisResults.root");
+                         containerName.Data());
+
   
 	mgr->ConnectInput(task,  0, mgr->GetCommonInputContainer());
 	mgr->ConnectOutput(task, 0, coutput1 );
