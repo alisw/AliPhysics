da6cf8eab6c765e76766e305843b6ea5b53da6fc
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2016-10-18 09:25:45 -0200

    LHC16k period: adjust run range

--- OADB/COMMON/MULTIPLICITY/AliMultSelectionTask.cxx
+++ OADB/COMMON/MULTIPLICITY/AliMultSelectionTask.cxx
@@ -2096,7 +2096,7 @@ TString AliMultSelectionTask::GetPeriodNameByRunNumber() const
     
     //2016
     if ( fCurrentRun >= 256146 && fCurrentRun <= 256420 ) lProductionName = "LHC16j";
-    if ( fCurrentRun >= 256504 && fCurrentRun <= 258197 ) lProductionName = "LHC16k";
+    if ( fCurrentRun >= 256504 && fCurrentRun <= 258537 ) lProductionName = "LHC16k";
     if ( fCurrentRun >= 258883 && fCurrentRun <= 260187 ) lProductionName = "LHC16l";
     
     //Registered Productions : Run 2 Pb-Pb
