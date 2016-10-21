050bacce852af478f0c4e6cb232ac6e5cd64f70f
mfasel <markus.fasel@cern.ch>
2016-10-21 15:50:40 +0200

    Rebase to AliAnalysisTaskEmcalTriggerBase
    - Simplify code
    - Remove code duplication
    - Improve documentation
    - Remove unused functions needed for the AliPhysics -> AliRoot migration

--- PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalPatchesRef.h
+++ PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalPatchesRef.h
@@ -5,12 +5,13 @@
 
 #include <string>
 #include <vector>
-#include "AliAnalysisTaskEmcal.h"
+#include "AliAnalysisTaskEmcalTriggerBase.h"
 #include "AliCutValueRange.h"
 #include <TCustomBinning.h>
 #include <TString.h>
 
 class AliOADBContainer;
+class AliEMCALTriggerPatchInfo;
 class THistManager;
 class TObjArray;
 
@@ -18,59 +19,125 @@ namespace EMCalTriggerPtAnalysis {
 
 class AliEmcalTriggerOfflineSelection;
 
-class AliAnalysisTaskEmcalPatchesRef : public AliAnalysisTaskEmcal {
+/**
+ * @class AliAnalysisTaskEmcalPatchesRef
+ * @brief Analysis of trigger patches in min. bias and triggered events
+ * @author Markus Fasel <markus.fasel@cern.ch>, Lawrence Berkeley National Laboratory
+ * @ingroup PWGJETASKS
+ * @since Aug 31. 2015
+ */
+class AliAnalysisTaskEmcalPatchesRef : public AliAnalysisTaskEmcalTriggerBase {
 public:
+  /**
+   * Dummy (I/O) onstructor
+   */
   AliAnalysisTaskEmcalPatchesRef();
-  AliAnalysisTaskEmcalPatchesRef(const char *name);
-  virtual ~AliAnalysisTaskEmcalPatchesRef();
 
-  void SetOfflineTriggerSelection(AliEmcalTriggerOfflineSelection *sel) { fTriggerSelection = sel; }
-  void SetCreateTriggerStringFromPatches(Bool_t doUsePatches) { fTriggerStringFromPatches = doUsePatches; }
-  void SetRequestAnalysisUtil(bool doUse) { fRequestAnalysisUtil = doUse; }
+  /**
+   * Named constructor
+   * @param[in] name Name of the task
+   */
+  AliAnalysisTaskEmcalPatchesRef(const char *name);
+
+  /**
+   * Destructor
+   */
+  virtual ~AliAnalysisTaskEmcalPatchesRef(){}
+
+  /**
+   * Set centrality selection.
+   * Note: Needs multiplicity task to run in front
+   * @param[in] min Min. value of the centrality interval
+   * @param[in] max Max. value of the centrality interval
+   */
   void SetCentralityRange(double min, double max) { fCentralityRange.SetLimits(min,max); fRequestCentrality = true; }
-  void SetVertexRange(double min, double max) { fVertexRange.SetLimits(min, max); }
-  void SetDownscaleOADB(TString oadbname) { fNameDownscaleOADB = oadbname; }
 
 protected:
-  virtual void UserCreateOutputObjects();
-  virtual void ExecOnce();
-  virtual bool IsEventSelected();
+
+  /**
+   * Task does not need user objects, so the function is not implemented
+   */
+  virtual void CreateUserObjects() {}
+
+  /**
+   * Creating output histograms:
+   * - Patch (calibrated) energy spectrum - separated by patch type - for different trigger classes
+   * - Patch eta-phi map - separated by patch type - for different trigger classes and different min. energies
+   */
+  virtual void CreateUserHistos();
+
+  /**
+   * Handle centrality selection (if requested, and multiplicity information available)
+   * @return True if event is in proper centrality class
+   */
+  virtual bool IsUserEventSelected();
+
+  /**
+   * Run event loop: Loop over all patches, assign them to trigger classes, and
+   * fill relevant histograms
+   * @param Not used
+   */
   virtual bool Run();
-  virtual void RunChanged(Int_t runnumber);
 
-  Double_t GetTriggerWeight(const TString &triggerclass) const;
+  /**
+   * Fill event-based histograms. Monitored are
+   * - Number of events
+   * - Centrality percentile (if available)
+   * - z-position of the primary vertex
+   * In case a downscaling correction is avaiable it is applied to all
+   * histograms as a weight.
+   */
+  virtual void UserFillHistosAfterEventSelection();
 
-  void GetPatchBoundaries(TObject *o, Double_t *boundaries) const;
-  bool IsOfflineSimplePatch(TObject *o) const;
-  bool SelectDCALPatch(TObject *o) const;
-  bool SelectSingleShowerPatch(TObject *o) const;
-  bool SelectJetPatch(TObject *o) const;
-  double GetPatchEnergy(TObject *o) const;
+  /**
+   * Helper function reading the patch boundaries in \f$\eta\f$ and \f$\phi\f$
+   * from a trigger patch
+   * @param[in] patch Patch to check
+   * @param[out] boundaries Patch boundaries
+   */
+  void GetPatchBoundaries(const AliEMCALTriggerPatchInfo *patch, Double_t *boundaries) const;
 
+  /**
+   * Helper function, selecting offline simple single shower (gamma or L0) patches
+   * @param[in] patch Patch to check
+   * @return True if the patch is selected
+   */
+  bool SelectSingleShowerPatch(const AliEMCALTriggerPatchInfo *patch) const;
+
+  /**
+   * Helper function, selecting offline simple jet patches
+   * @param[in] patch Patch to check
+   * @return True if the patch is selected
+   */
+  bool SelectJetPatch(const AliEMCALTriggerPatchInfo  *patch) const;
+
+  /**
+   * Filling patch related histogram. In case a downscaling correction is
+   * available it is applied to the histograms as weight
+   * @param[in] triggerclass Name of the trigger class firing the event
+   * @param[in] patchname Name of the patchtype
+   * @param[in] energy Calibrated energy of the patch
+   * @param[in] eta Patch eta at the geometrical center
+   * @param[in] phi Patch phi at the geometrical center
+   */
   void FillPatchHistograms(TString triggerclass, TString patchname, double energy, double transverseenergy, double eta, double phi, int col, int row);
-  void FillEventHistograms(const TString &triggerclass, double centrality, double vertexz);
-  TString GetFiredTriggerClassesFromPatches(const TClonesArray* triggerpatches) const;
-
-  AliEmcalTriggerOfflineSelection     *fTriggerSelection;         ///< Offline trigger selection tool
-  std::vector<std::string>            fAcceptTriggers;            //!<! Temporary container of selected triggers
-  THistManager                        *fHistos;                   //!<! Histogram handler
-
-  Bool_t                              fRequestAnalysisUtil;       ///< Switch on request for analysis util
-  Bool_t                              fTriggerStringFromPatches;  ///< Switch on building a trigger string based on available trigger patches
 
   AliCutValueRange<double>            fCentralityRange;           ///< Range of accepted event centralities
-  AliCutValueRange<double>            fVertexRange;               ///< Range of the z-position in order to accept a primary vertex
   Bool_t                              fRequestCentrality;         ///< Switch for request of centrality selection
-
-  TString                             fNameDownscaleOADB;         ///< Name of the downscale OADB container
-  AliOADBContainer                    *fDownscaleOADB;            //!<! Container with downscale factors for different triggers
-  TObjArray                           *fDownscaleFactors;         //!<! Downscalefactors for given run
+  Double_t                            fEventCentrality;           //!<1! Event centrality
 
 private:
 
   class EnergyBinning : public TCustomBinning {
   public:
+    /**
+     * Constructor
+     */
     EnergyBinning();
+
+    /**
+     * Destructor
+     */
     virtual ~EnergyBinning() {}
   };
 
