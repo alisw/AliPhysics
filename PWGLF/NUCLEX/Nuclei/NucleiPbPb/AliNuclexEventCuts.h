2b1fdb5605bbe13cb89d6a50f44509cb7a5505f0
mpuccio <maximiliano.puccio@cern.ch>
2016-10-21 15:12:53 +0200

    Avoid hardcoded cuts and standard null cuts constructor
    Also: protect data members that can be accessed in the wrong way (e.g.
    arrays) in order to avoid undefined behaviours.
    Thanks to A.Rossi e F.Prino for the suggestions

--- PWGLF/NUCLEX/Nuclei/NucleiPbPb/AliNuclexEventCuts.h
+++ PWGLF/NUCLEX/Nuclei/NucleiPbPb/AliNuclexEventCuts.h
@@ -14,7 +14,6 @@ class AliNuclexEventCuts : public TNamed {
     AliNuclexEventCuts();
  
     bool   AcceptEvent (AliVEvent *ev, TList* qaList = 0x0);
-    float  GetCentrality (unsigned int estimator = 0) { return fCentPercentiles[estimator]; }
     void AddQAplotsToList(TList *qaList);
     
     void  SetupLHC15o();
@@ -23,33 +22,42 @@ class AliNuclexEventCuts : public TNamed {
     /// for some variables (like the max z vertex position) standard the cuts usually follow some patterns
     /// (e.g. the max vertex z position is always symmetric wrt the nominal beam collision point) thus
     /// is convenient having special setters in this case.
-    void          SetMaxVertexZposition (float max) { fMinVtz = -fabs(max); fMaxVtz = fabs(max); }
-    void          SetCentralityRange(float min, float max) { fMinCentrality = min; fMaxCentrality = max; }
+    float         GetCentrality (unsigned int estimator = 0);
+    string        GetCentralityEstimator (unsigned int estimator = 0);
 
-    bool          fRequireTrackVertex;
-    float         fMinVtz;
-    float         fMaxVtz;
-    float         fMaxDeltaSpdTrackAbsolute;
-    float         fMaxDeltaSpdTrackNsigmaSPD;
-    float         fMaxDeltaSpdTrackNsigmaTrack;
+    void          SetCentralityEstimators (string first = "V0M", string second = "CL0") { fCentEstimators[0] = first; fCentEstimators[1] = second; }
+    void          SetCentralityRange (float min, float max) { fMinCentrality = min; fMaxCentrality = max; }
+    void          SetMaxVertexZposition (float max) { fMinVtz = -fabs(max); fMaxVtz = fabs(max); }
+
+    bool          fRequireTrackVertex;            ///< if true all the events with only the SPD vertex are rejected
+    float         fMinVtz;                        ///< Min z position for the primary vertex
+    float         fMaxVtz;                        ///< Max z position for the primary vertex
+    float         fMaxDeltaSpdTrackAbsolute;      ///< 
+    float         fMaxDeltaSpdTrackNsigmaSPD;     ///<
+    float         fMaxDeltaSpdTrackNsigmaTrack;   ///<
 //    float         fMaxDispersionSPDvertex;
-//    float         fMaxResolutionSPDvertex;
+    float         fMaxResolutionSPDvertex;        ///<
 
     bool          fRejectDAQincomplete;           ///< Reject events that have incomplete information
 
-    int           fRejectPileupSPD;               ///< Reject all the events with SPD pile-up vertices with more than fRejectPileupSPD contributors
+    int           fSPDpileupMinContributors;      ///< Reject all the events with SPD pile-up vertices with more than fRejectPileupSPD contributors
+    double        fSPDpileupMinZdist;             ///<
+		double        fSPDpileupNsigmaZdist;          ///<
+		double        fSPDpileupNsigmaDiamXY;         ///<
+		double        fSPDpileupNsigmaDiamZ;          ///<
 
     int           fCentralityFramework;           ///< 0: skip centrality checks, 1: legacy centrality framework, 2: multiplicity framework
     float         fMinCentrality;                 ///< Minimum centrality to be analised
     float         fMaxCentrality;                 ///< Maximum centrality to be analised
-    string        fCentEstimators[2];             ///< Centrality estimators: the first is used as main estimators, that is correlated with the second to monitor spurious events.
-    float         fCentPercentiles[2];            ///< Centrality percentiles 
     float         fMaxDeltaEstimators;            ///< Maximum difference between two centrality estimators
 
+    bool          fRequireExactTriggerMask;       ///< If true the event selection mask is required to be equal to fTriggerMask
     AliVEvent::EOfflineTriggerTypes fTriggerMask; ///< Trigger mask 
 
     static const string fgkLabels[2];
   private:
+    string        fCentEstimators[2];             ///< Centrality estimators: the first is used as main estimators, that is correlated with the second to monitor spurious events.
+    float         fCentPercentiles[2];            ///< Centrality percentiles 
 
     ClassDef(AliNuclexEventCuts,2)
 };
