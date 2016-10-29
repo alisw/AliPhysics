473a1e6a514c3e9e85847dcd0c803b3dff48670d
mpuccio <maximiliano.puccio@cern.ch>
2016-10-29 15:24:32 +0200

    AliNuclexEventCuts: automatic cut setting and new ui

--- PWGLF/NUCLEX/Nuclei/NucleiPbPb/AliNuclexEventCuts.h
+++ PWGLF/NUCLEX/Nuclei/NucleiPbPb/AliNuclexEventCuts.h
@@ -1,22 +1,27 @@
 #ifndef _AliNuclexEventCuts_h_
 #define _AliNuclexEventCuts_h_
 
-#include <TNamed.h>
+#include <TList.h>
 #include <string>
 using std::string;
 
 #include "AliVEvent.h"
+#include "AliAnalysisUtils.h"
 
 class TList;
+class TH1D;
+class TH1I;
+class TH2D;
 
-class AliNuclexEventCuts : public TNamed {
+class AliNuclexEventCuts : public TList {
   public:
-    AliNuclexEventCuts();
+    AliNuclexEventCuts(bool savePlots = false);
  
-    bool   AcceptEvent (AliVEvent *ev, TList* qaList = 0x0);
-    void AddQAplotsToList(TList *qaList);
-    
-    void  SetupLHC15o();
+    bool   AcceptEvent (AliVEvent *ev);
+    void   AddQAplotsToList(TList *qaList = 0x0);
+    void   SetManualMode (bool man = true) { fManualMode = man; }
+    void   SetupLHC15o();
+    void   SetupRun2pp();
 
     /// While the general philosophy here is to avoid setters and getters (this is not an API we expose)
     /// for some variables (like the max z vertex position) standard the cuts usually follow some patterns
@@ -30,24 +35,28 @@ class AliNuclexEventCuts : public TNamed {
     void          SetCentralityRange (float min, float max) { fMinCentrality = min; fMaxCentrality = max; }
     void          SetMaxVertexZposition (float max) { fMinVtz = -fabs(max); fMaxVtz = fabs(max); }
 
+    AliAnalysisUtils fUtils;                      ///< Analysis utils for the pileup rejection
+
     bool          fRequireTrackVertex;            ///< if true all the events with only the SPD vertex are rejected
     float         fMinVtz;                        ///< Min z position for the primary vertex
     float         fMaxVtz;                        ///< Max z position for the primary vertex
     float         fMaxDeltaSpdTrackAbsolute;      ///< 
     float         fMaxDeltaSpdTrackNsigmaSPD;     ///<
     float         fMaxDeltaSpdTrackNsigmaTrack;   ///<
-//    float         fMaxDispersionSPDvertex;
     float         fMaxResolutionSPDvertex;        ///<
 
     bool          fRejectDAQincomplete;           ///< Reject events that have incomplete information
+    
+    int           fRequiredSolenoidPolarity;      ///< 0: does not require any particular polarity. Positive numbers -> positive B field, negative numbers -> negative B field
 
     int           fSPDpileupMinContributors;      ///< Reject all the events with SPD pile-up vertices with more than fRejectPileupSPD contributors
     double        fSPDpileupMinZdist;             ///<
 		double        fSPDpileupNsigmaZdist;          ///<
 		double        fSPDpileupNsigmaDiamXY;         ///<
 		double        fSPDpileupNsigmaDiamZ;          ///<
+    bool          fTrackletBGcut;                 ///<
 
-    int           fCentralityFramework;           ///< 0: skip centrality checks, 1: legacy centrality framework, 2: multiplicity framework
+    unsigned int  fCentralityFramework;           ///< 0: skip centrality checks, 1: multiplicity framework, 2: legacy centrality framework
     float         fMinCentrality;                 ///< Minimum centrality to be analised
     float         fMaxCentrality;                 ///< Maximum centrality to be analised
     float         fMaxDeltaEstimators;            ///< Maximum difference between two centrality estimators
@@ -55,12 +64,37 @@ class AliNuclexEventCuts : public TNamed {
     bool          fRequireExactTriggerMask;       ///< If true the event selection mask is required to be equal to fTriggerMask
     AliVEvent::EOfflineTriggerTypes fTriggerMask; ///< Trigger mask 
 
-    static const string fgkLabels[2];
+    const string  fkLabels[2];                    ///< Histograms labels (raw/selected)
+
+    enum CutsBin {
+      kNoCuts = 0,
+      kDAQincomplete,
+      kBfield,
+      kTrigger,
+      kVertex,
+      kPileUp,
+      kMultiplicity,
+      kAllCuts
+    };
+
   private:
+    void          AutomaticSetup ();
+  
+    bool          fManualMode;                    ///< if true the cuts are not loaded automatically looking at the run number
+    bool          fSavePlots;                     ///< if true the plots are automatically added to this object
+    int           fCurrentRun;                    ///<
+
     string        fCentEstimators[2];             ///< Centrality estimators: the first is used as main estimators, that is correlated with the second to monitor spurious events.
     float         fCentPercentiles[2];            ///< Centrality percentiles 
     AliVVertex   *fPrimaryVertex;                 //!<! Primary vertex pointer
 
+    TH1I* fCutStats;               //!<! Cuts statistics
+    TH1D* fVtz[2];                 //!<! Vertex z distribution
+    TH1D* fDeltaTrackSPDvtz[2];    //!<! Difference between the vertex computed using SPD and the track vertex
+    TH1D* fCentrality[2];          //!<! Centrality percentile distribution
+    TH2D* fEstimCorrelation[2];    //!<! Correlation between centrality estimators
+    TH2D* fMultCentCorrelation[2]; //!<! Correlation between main centrality estimator and multiplicity
+    
     ClassDef(AliNuclexEventCuts,2)
 };
 
