6e593f39e33690ddaa6e52b580f45cab038a750f
saiola <salvatore.aiola@cern.ch>
2016-11-01 15:25:41 +0100

    Use compiled AddTask static methods (instead of the AddTask interpreted macros)

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetTree.cxx
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskEmcalJetTree.cxx
@@ -16,6 +16,9 @@
 #include "AliLog.h"
 #include "AliAnalysisTaskEmcalJetTree.h"
 
+#include "AliAnalysisManager.h"
+#include "AliVEventHandler.h"
+
 // Definitions of class AliAnalysisTaskEmcalJetTreeBase::AliEmcalJetInfoSummaryPP
 
 /// \cond CLASSIMP
@@ -325,3 +328,120 @@ Bool_t AliAnalysisTaskEmcalJetTree<T>::FillHistograms()
   PostData(2, fTree);
   return kTRUE;
 }
+
+/// Create an instance of this class and add it to the analysis manager
+///
+/// \param ntracks name of the track collection
+/// \param nclusters name of the calorimeter cluster collection
+/// \param trackPtCut minimum transverse momentum of tracks
+/// \param clusECut minimum energy of calorimeter clusters
+/// \param type analysis type (pp, PbPb, etc.)
+/// \param suffix additional suffix that can be added at the end of the task name
+/// \return pointer to the new AliAnalysisTaskEmcalJetTreeBase task
+AliAnalysisTaskEmcalJetTreeBase* AliAnalysisTaskEmcalJetTreeBase::AddTaskEmcalJetTree(TString ntracks, TString nclusters, Double_t trackPtCut, Double_t clusECut, EAnalisysType_t type, TString suffix)
+{
+  // Get the pointer to the existing analysis manager via the static access method
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTaskEmcalJetTree", "No analysis manager to connect to.");
+    return NULL;
+  }
+
+  // Check the analysis type using the event handlers connected to the analysis manager
+  AliVEventHandler* handler = mgr->GetInputEventHandler();
+  if (!handler) {
+    ::Error("AddTaskEmcalJetTree", "This task requires an input event handler");
+    return NULL;
+  }
+
+  EDataType_t dataType = kUnknownDataType;
+
+  if (handler->InheritsFrom("AliESDInputHandler")) {
+    dataType = kESD;
+  }
+  else if (handler->InheritsFrom("AliAODInputHandler")) {
+    dataType = kAOD;
+  }
+
+  // Init the task and do settings
+
+  if (ntracks == "usedefault") {
+    if (dataType == kESD) {
+      ntracks = "Tracks";
+    }
+    else if (dataType == kAOD) {
+      ntracks = "tracks";
+    }
+    else {
+      ntracks = "";
+    }
+  }
+
+  if (nclusters == "usedefault") {
+    if (dataType == kESD) {
+      nclusters = "CaloClusters";
+    }
+    else if (dataType == kAOD) {
+      nclusters = "caloClusters";
+    }
+    else {
+      nclusters = "";
+    }
+  }
+
+  TString name("AliAnalysisTaskEmcalJetTree");
+  if (strcmp(suffix,"")) {
+    name += "_";
+    name += suffix;
+  }
+
+  AliAnalysisTaskEmcalJetTreeBase* jetTask = AliAnalysisTaskEmcalJetTreeBase::CreateInstance(name, type);
+  jetTask->SetVzRange(-10,10);
+  jetTask->SetNeedEmcalGeom(kFALSE);
+
+  if (ntracks == "mcparticles") {
+    AliMCParticleContainer* mcpartCont = jetTask->AddMCParticleContainer(ntracks);
+    mcpartCont->SelectPhysicalPrimaries(kTRUE);
+  }
+  else if (ntracks == "tracks" || ntracks == "Tracks") {
+    AliTrackContainer* trackCont = jetTask->AddTrackContainer(ntracks);
+  }
+  else if (!ntracks.IsNull()) {
+    jetTask->AddParticleContainer(ntracks);
+  }
+
+  AliParticleContainer *partCont = jetTask->GetParticleContainer(0);
+  if (partCont) {
+    partCont->SetParticlePtCut(trackPtCut);
+  }
+
+  AliClusterContainer *clusterCont = jetTask->AddClusterContainer(nclusters);
+  if (clusterCont) {
+    clusterCont->SetClusECut(0.);
+    clusterCont->SetClusPtCut(0.);
+    clusterCont->SetClusHadCorrEnergyCut(clusECut);
+    clusterCont->SetDefaultClusterEnergy(AliVCluster::kHadCorr);
+  }
+
+  // Final settings, pass to manager and set the containers
+  mgr->AddTask(jetTask);
+
+  // Create containers for input/output
+  AliAnalysisDataContainer *cinput1  = mgr->GetCommonInputContainer()  ;
+  TString contname0(name);
+  contname0 += "_jets";
+  TString contname1(name);
+  contname1 += "_histos";
+  AliAnalysisDataContainer *coutput0 = mgr->CreateContainer(contname0.Data(),
+                  TTree::Class(),AliAnalysisManager::kOutputContainer,
+                  Form("%s", AliAnalysisManager::GetCommonFileName()));
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer(contname1.Data(),
+                  TList::Class(),AliAnalysisManager::kOutputContainer,
+                  Form("%s", AliAnalysisManager::GetCommonFileName()));
+  mgr->ConnectInput  (jetTask, 0,  cinput1 );
+  mgr->ConnectOutput (jetTask, 1, coutput1 );
+  mgr->ConnectOutput (jetTask, 2, coutput0 );
+
+  return jetTask;
+}
+
