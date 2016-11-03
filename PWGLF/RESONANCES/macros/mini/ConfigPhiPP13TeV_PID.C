1f907af34f7026a7e16cf6a620c3616efb4d1ece
jisong <jihye.song@cern.ch>
2016-11-03 13:35:24 +0900

    Change in binning for multiplicity-dependent phi analysis in pp at 13 TeV

--- PWGLF/RESONANCES/macros/mini/ConfigPhiPP13TeV_PID.C
+++ PWGLF/RESONANCES/macros/mini/ConfigPhiPP13TeV_PID.C
@@ -42,8 +42,7 @@ Bool_t ConfigPhiPP13TeV_PID
   if(SetCustomQualityCut(trkQualityCut,customQualityCutsID,aodFilterBit)){
     //Set custom quality cuts for systematic checks
     cutSetQ=new AliRsnCutSetDaughterParticle(Form("cutQ_bit%i",aodFilterBit),trkQualityCut,AliRsnCutSetDaughterParticle::kQualityStd2010,AliPID::kKaon,-1.);
-    //////cutSetK=new AliRsnCutSetDaughterParticle(Form("cutK%i_%2.1fsigma",cutKaCandidate, nsigmaKa),trkQualityCut,cutKaCandidate,AliPID::kKaon,nsigmaKaTPC,nsigmaKaTOF);
-    cutSetK=new AliRsnCutSetDaughterParticle(Form("cutK%i_%2.1fsigma",cutKaCandidate, nsigmaKa),trkQualityCut,cutKaCandidate,AliPID::kKaon,nsigmaKaTPC);
+    cutSetK=new AliRsnCutSetDaughterParticle(Form("cutK%i_%2.1fsigma",cutKaCandidate, nsigmaKa),trkQualityCut,cutKaCandidate,AliPID::kKaon,nsigmaKaTPC,nsigmaKaTOF);
   }else{
     //use default quality cuts std 2010 with crossed rows TPC
     Bool_t useCrossedRows = 1;
@@ -122,7 +121,7 @@ Bool_t ConfigPhiPP13TeV_PID
     else out->AddAxis(ptID,200,0.,20.);//default use mother pt
 
     // axis Z: centrality-multiplicity
-    if(!isPP) out->AddAxis(centID,100,0.,100.);
+    if(!isPP || MultBins) out->AddAxis(centID,100,0.,100.);
     else out->AddAxis(centID,161,-0.5,160.5);
     // axis W: pseudorapidity
     // out->AddAxis(etaID, 20, -1.0, 1.0);
@@ -143,7 +142,7 @@ Bool_t ConfigPhiPP13TeV_PID
     outm->SetPairCuts(cutsPair);
     outm->AddAxis(imID,215,0.985,1.2);
     outm->AddAxis(ptID,200,0.,20.);
-    if(!isPP) outm->AddAxis(centID,100,0.,100.);
+    if(!isPP || MultBins) outm->AddAxis(centID,100,0.,100.);
     else outm->AddAxis(centID,161,-0.5,160.5);
     if (polarizationOpt.Contains("J")) outm->AddAxis(ctjmID,21,-1.,1.);
     if (polarizationOpt.Contains("T")) outm->AddAxis(cttmID,21,-1.,1.);
@@ -156,7 +155,7 @@ Bool_t ConfigPhiPP13TeV_PID
     outmf->SetPairCuts(cutsPair);
     outmf->AddAxis(imID,215,0.985,1.2);
     outmf->AddAxis(ptID,300,0.,3.);//fine binning for efficiency weighting
-    if(!isPP) outmf->AddAxis(centID,100,0.,100.);
+    if(!isPP || MultBins) outmf->AddAxis(centID,100,0.,100.);
     else outmf->AddAxis(centID,161,-0.5,160.5);
     if (polarizationOpt.Contains("J")) outmf->AddAxis(ctjmID,21,-1.,1.);
     if (polarizationOpt.Contains("T")) outmf->AddAxis(cttmID,21,-1.,1.);
