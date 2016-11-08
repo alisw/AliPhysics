b5f370c9f69e5cd89d091169082112238c97d95e
saiola <salvatore.aiola@cern.ch>
2016-11-08 15:50:55 +0100

    Add static method to determine beam type for a given run number

--- PWG/EMCAL/EMCALbase/AliAnalysisTaskEmcalLight.cxx
+++ PWG/EMCAL/EMCALbase/AliAnalysisTaskEmcalLight.cxx
@@ -1346,3 +1346,24 @@ Byte_t AliAnalysisTaskEmcalLight::GetTrackType(const AliAODTrack *aodTrack, UInt
 
   return res;
 }
+
+/**
+ * Determine the beam type based on hard-coded run ranges
+ * \param runnumber run number
+ * \return enumeration value corresponding to the beam type
+ */
+AliAnalysisTaskEmcalLight::EBeamType_t AliAnalysisTaskEmcalLight::BeamTypeFromRunNumber(Int_t runnumber)
+{
+  EBeamType_t b = kpp;
+  if ((runnumber >= 136833 && runnumber <= 139517) || // LHC10h Run-1 (Pb-Pb)
+      (runnumber >= 167693 && runnumber <= 170593) || // LHC11h Run-1 (Pb-Pb)
+      (runnumber >= 244824 && runnumber <= 246994)) { // LHC15o Run-2 (Pb-Pb)
+    b = kpA;
+  }
+  else if ((runnumber > 188356 && runnumber <= 188503) ||  // LHC12g Run-1 (p-Pb pilot)
+      (runnumber >= 195164 && runnumber <= 197388) ||      // LHC13b,c,d,e,f Run-1 (p-Pb)
+      (runnumber >= 265115 && runnumber <= 999999)) {      // LHC16 Run-2 (p-Pb)
+    b = kAA;
+  }
+  return b;
+}
