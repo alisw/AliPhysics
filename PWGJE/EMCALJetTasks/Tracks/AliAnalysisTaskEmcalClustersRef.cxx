alisw/AliPhysics

commit 36bc66ecbe3dc7fb4f645b628be90bab21e0aad3

Make several cuts in the event selection setable
Affects
- Trigger bit selection
- Bunch crossing presence
- Analysis Utils

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalTriggerBase.cxx
@@ -40,9 +40,12 @@ namespace EMCalTriggerPtAnalysis {
 AliAnalysisTaskEmcalTriggerBase::AliAnalysisTaskEmcalTriggerBase():
   AliAnalysisTaskEmcal(),
   fTriggerSelection(nullptr),
+  fUseTriggerBits(kTRUE),
+  fRequireBunchCrossing(kTRUE),
   fHistos(nullptr),
   fTriggerStringFromPatches(kFALSE),
   fSelectedTriggers(),
+  fRequireAnalysisUtils(kTRUE),
   fVertexCut(-10., 10.),
   fNameDownscaleOADB(""),
   fDownscaleOADB(nullptr),
@@ -63,9 +66,12 @@ AliAnalysisTaskEmcalTriggerBase::AliAnalysisTaskEmcalTriggerBase():
 AliAnalysisTaskEmcalTriggerBase::AliAnalysisTaskEmcalTriggerBase(const char *name):
   AliAnalysisTaskEmcal(name, true),
   fTriggerSelection(nullptr),
+  fUseTriggerBits(kTRUE),
+  fRequireBunchCrossing(kTRUE),
   fHistos(nullptr),
   fTriggerStringFromPatches(kFALSE),
   fSelectedTriggers(),
+  fRequireAnalysisUtils(kTRUE),
   fVertexCut(-10., 10.),
   fNameDownscaleOADB(""),
   fDownscaleOADB(nullptr),
@@ -91,7 +97,7 @@ AliAnalysisTaskEmcalTriggerBase::~AliAnalysisTaskEmcalTriggerBase() {
 
 void AliAnalysisTaskEmcalTriggerBase::UserCreateOutputObjects() {
   AliAnalysisTaskEmcal::UserCreateOutputObjects();
-  if(!fAliAnalysisUtils) fAliAnalysisUtils = new AliAnalysisUtils;
+  if(fRequireAnalysisUtils && !fAliAnalysisUtils) fAliAnalysisUtils = new AliAnalysisUtils;
 
   fHistos = new THistManager(Form("Histos_%s", GetName()));
 
@@ -113,10 +119,13 @@ Bool_t AliAnalysisTaskEmcalTriggerBase::IsEventSelected(){
   const AliVVertex *vtx = fInputEvent->GetPrimaryVertex();
   //if(!fInputEvent->IsPileupFromSPD(3, 0.8, 3., 2., 5.)) return;         // reject pileup event
   if(vtx->GetNContributors() < 1) return false;
-  if(fInputEvent->IsA() == AliESDEvent::Class() && fAliAnalysisUtils->IsFirstEventInChunk(fInputEvent)) return false;
-  if(fAliAnalysisUtils->IsPileUpEvent(fInputEvent)) return false;       // Apply new vertex cut
 
-  if(!fAliAnalysisUtils->IsVertexSelected2013pA(fInputEvent))return false;       // Apply new vertex cut
+  if(fRequireAnalysisUtils){
+    if(fInputEvent->IsA() == AliESDEvent::Class() && fAliAnalysisUtils->IsFirstEventInChunk(fInputEvent)) return false;
+    if(fAliAnalysisUtils->IsPileUpEvent(fInputEvent)) return false;       // Apply new vertex cut
+    if(!fAliAnalysisUtils->IsVertexSelected2013pA(fInputEvent))return false;       // Apply new vertex cut
+  }
+
   if(!fVertexCut.IsInRange(fVertex[2])) return false;
   if(!IsUserEventSelected()) return false;
 
@@ -143,6 +152,9 @@ void AliAnalysisTaskEmcalTriggerBase::TriggerSelection(){
   } else {
     triggerstring = fInputEvent->GetFiredTriggerClasses();
   }
+
+  if(fRequireBunchCrossing && ! triggerstring.Contains("-B-")) return;
+
   UInt_t selectionstatus = fInputHandler->IsEventSelected();
   Bool_t isMinBias = selectionstatus & AliVEvent::kINT7,
       emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgn];
@@ -152,16 +164,23 @@ void AliAnalysisTaskEmcalTriggerBase::TriggerSelection(){
     // Further cleanup of trigger events can be performed depending on the presence
     // of recalc patches (after masking hot fastors in the trigger maker) above
     // threshold
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgEJ1] = (selectionstatus & AliVEvent::kEMCEJE) && triggerstring.Contains("EJ1");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgEJ2] = (selectionstatus & AliVEvent::kEMCEJE) && triggerstring.Contains("EJ2");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgEG1] = (selectionstatus & AliVEvent::kEMCEGA) && triggerstring.Contains("EG1"),
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgEG2] = (selectionstatus & AliVEvent::kEMCEGA) && triggerstring.Contains("EG2");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgEL0] = (selectionstatus & AliVEvent::kEMC7) && triggerstring.Contains("CEMC7");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgDJ1] = (selectionstatus & AliVEvent::kEMCEJE) && triggerstring.Contains("DJ1");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgDJ2] = (selectionstatus & AliVEvent::kEMCEJE) && triggerstring.Contains("DJ2");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgDG1] = (selectionstatus & AliVEvent::kEMCEGA) && triggerstring.Contains("DG1");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgDG2] = (selectionstatus & AliVEvent::kEMCEGA) && triggerstring.Contains("DG2");
-    emcalTriggers[AliEmcalTriggerOfflineSelection::kTrgDL0] = (selectionstatus & AliVEvent::kEMC7) && triggerstring.Contains("CDMC7");
+    if(fUseTriggerBits){
+      const std::array<ULong_t, AliEmcalTriggerOfflineSelection::kTrgn> kSelectTriggerBits = {
+          AliVEvent::kEMCEJE, AliVEvent::kEMCEJE, AliVEvent::kEMCEGA, AliVEvent::kEMCEGA, AliVEvent::kEMC7,
+          AliVEvent::kEMCEJE, AliVEvent::kEMCEJE, AliVEvent::kEMCEGA, AliVEvent::kEMCEGA, AliVEvent::kEMC7
+      };
+      for(int iclass = 0; iclass < AliEmcalTriggerOfflineSelection::kTrgn; iclass++)
+        emcalTriggers[iclass] &= (selectionstatus & kSelectTriggerBits[iclass]);
+    }
+
+    // Apply cut on the trigger string - this basically discriminates high- and low-threshold
+    // triggers
+    const std::array<TString, AliEmcalTriggerOfflineSelection::kTrgn> kSelectTriggerStrings = {
+        "EJ1", "EJ2", "EG1", "EG2", "CEMC7", "DJ1", "DJ2", "DG1", "DG2", "CDMC7"
+    };
+    for(int iclass = 0; iclass < AliEmcalTriggerOfflineSelection::kTrgn; iclass++)
+      emcalTriggers[iclass] &= triggerstring.Contains(kSelectTriggerStrings[iclass]);
+
     // Online selection / rejection
     if(fRejectNoiseEvents || fSelectNoiseEvents){
       if(fRejectNoiseEvents){

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalTriggerBase.h
@@ -139,6 +139,29 @@ public:
   void SetOnlineTriggerThreshold(const TString &triggerclass, Int_t threshold);
 
   /**
+   * Defining whether to require trigger bits. Attention: Relies on the presence
+   * of the physics selection - to be switched off in case of new data. By default
+   * trigger bits are required.
+   * @param[in] doUse If true trigger bits are required, if false not
+   */
+  void SetUseTriggerBits(Bool_t doUse) { fUseTriggerBits = doUse; }
+
+  /**
+   * Defining whether to require bunch crossing events. To be switch off in case of
+   * noise studies (i.e. beam-empty or empty-empty events). By default bunch crossing
+   * is required
+   * @param[in] doRequire If true bunch-bunch crossing is required.
+   */
+  void SetRequireBunchCrossing(Bool_t doRequire) { fRequireBunchCrossing = doRequire; }
+
+  /**
+   * Define whether cuts in AliAnalysisUtils are used in the event selection. Not to be
+   * applied on new data or data from muon_calo_pass1
+   * @param[in] doRequire If true AliAnalysisUtils are used in addition for the event selection
+   */
+  void SetRequireAnalysisUtils(Bool_t doRequire) { fRequireAnalysisUtils = doRequire; }
+
+  /**
    * Set the name of teh OADB container with trigger acceptance maps. Trigger
    * acceptance maps will be used in the trigger offline selection to mimic
    * the acceptance observed in data. Only useful in simulations and the mimicing
@@ -292,10 +315,13 @@ protected:
 
   THistManager                    *fHistos;                   ///< Task Histogram container
 
+  Bool_t                          fUseTriggerBits;            ///< Switch whether using trigger bits (relies on physics selection)
+  Bool_t                          fRequireBunchCrossing;      ///< Require bunch-bunch events (tag -B- in trigger string)
   AliEmcalTriggerOfflineSelection *fTriggerSelection;         ///< Offline trigger selection
   Bool_t                          fTriggerStringFromPatches;  ///< Do rebuild the trigger string from trigger patches
   std::vector<TString>            fSelectedTriggers;          //!<! Triggers selected for given event
 
+  Bool_t                          fRequireAnalysisUtils;      ///< Switch whether to require event selection in AliAnalysisUtils
   AliCutValueRange<double>        fVertexCut;                 ///< Cut on the z-position of the primary vertex
 
   TString                         fNameDownscaleOADB;         ///< Name of the downscale OADB container

commit 67c10b60021fd350f4d00c488ddc6442a71fb9ce

Fixing operation for trigger check
Bug introduced in 36bc66ecbe3dc7fb4f645b628be90bab21e0aad3:
Operator &= between bool and unsigned int produces undefined
behaviour. Instead, the unsigned int expression needs to be
casted to boolean for the operator to work.

In addition: Allow for different centrality estimators in
AliAnalysisTaskEmcalClustersRef.

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalClustersRef.cxx
@@ -61,6 +61,7 @@ AliAnalysisTaskEmcalClustersRef::AliAnalysisTaskEmcalClustersRef() :
     fCentralityRange(-999., 999.),
     fRequestCentrality(false),
     fEventCentrality(-1),
+    fCentralityEstimator("V0M"),
     fEnergyDefinition(kDefaultEnergy)
 {
 }
@@ -71,6 +72,7 @@ AliAnalysisTaskEmcalClustersRef::AliAnalysisTaskEmcalClustersRef(const char *nam
     fCentralityRange(-999., 999.),
     fRequestCentrality(false),
     fEventCentrality(-1),
+    fCentralityEstimator("V0M"),
     fEnergyDefinition(kDefaultEnergy)
 {
 }
@@ -136,7 +138,7 @@ bool AliAnalysisTaskEmcalClustersRef::IsUserEventSelected(){
       return false;
     }
     if(mult->IsEventSelected()) return false;
-    fEventCentrality = mult->GetEstimator("V0M")->GetPercentile();
+    fEventCentrality = mult->GetEstimator(fCentralityEstimator)->GetPercentile();
     AliDebugStream(1) << GetName() << ": Centrality " <<  fEventCentrality << std::endl;
     if(!fCentralityRange.IsInRange(fEventCentrality)){
       AliDebugStream(1) << GetName() << ": reject centrality: " << fEventCentrality << std::endl;

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalClustersRef.h
@@ -79,6 +79,13 @@ public:
   void SetCentralityRange(double min, double max) { fCentralityRange.SetLimits(min, max); fRequestCentrality = true; }
 
   /**
+   * Set the centrality estimator used to select centrality ranges. By
+   * default V0M will be used
+   * @param[in] centest Name of the centrality estimator
+   */
+  void SetUserCentralityEstimator(TString centest) { fCentralityEstimator = centest; }
+
+  /**
    * Preconfigure task so that it can be used in subwagons
    * @param[in] nClusters Name of the input cluster container
    * @param[in] suffix Suffix of the subwagon
@@ -197,6 +204,7 @@ protected:
   AliCutValueRange<double>            fCentralityRange;           ///< Selected centrality range
   Bool_t                              fRequestCentrality;         ///< Switch on request for centrality range
   Double_t                            fEventCentrality;           //!<! Current event centrality
+  TString                             fCentralityEstimator;       ///< Centrality estimator (default: V0M for PbPb)
 
   EnergyDefinition_t                  fEnergyDefinition;          ///< Energy definition used for a given cluster
 

PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalTriggerBase.cxx
@@ -171,7 +171,7 @@ void AliAnalysisTaskEmcalTriggerBase::TriggerSelection(){
           AliVEvent::kEMCEJE, AliVEvent::kEMCEJE, AliVEvent::kEMCEGA, AliVEvent::kEMCEGA, AliVEvent::kEMC7
       };
       for(int iclass = 0; iclass < AliEmcalTriggerOfflineSelection::kTrgn; iclass++)
-        emcalTriggers[iclass] &= (selectionstatus & kSelectTriggerBits[iclass]);
+        emcalTriggers[iclass] &= bool(selectionstatus & kSelectTriggerBits[iclass]);
     }
 
     // Apply cut on the trigger string - this basically discriminates high- and low-threshold