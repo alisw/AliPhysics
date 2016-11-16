fde9c118987a5018f689a0ea3d9a150b330bf3b2
alla <Alla.Maevskaya@cern.ch>
2016-11-17 01:19:02 +0300

    replace continue with if {}

--- T0/DA/T00Physda.cxx
+++ T0/DA/T00Physda.cxx
@@ -40,13 +40,13 @@ Trigger types used: PHYSICS_EVENT
 
 
 /* Main routine
-      Arguments: 
-      1- monitoring data source
+   Arguments: 
+   1- monitoring data source
 */
 int main(int argc, char **argv) {
-//int main(){
+  //int main(){
   int status;
-
+  
   /* magic line */
   gROOT->GetPluginManager()->AddHandler("TVirtualStreamerInfo",
                                         "*",
@@ -86,13 +86,13 @@ int main(int argc, char **argv) {
     }
   }
   fclose(inp);
-
+  
   if (argc!=2) {
     printf("Wrong number of arguments\n");
     return -1;
   }
   
-
+  
   /* define data source : this is argument 1 */  
   status=monitorSetDataSource( argv[1] );
   if (status!=0) {
@@ -123,8 +123,8 @@ int main(int argc, char **argv) {
     return -1;
   }
   int runNr = atoi(getenv("DATE_RUN_NUMBER"));
-
- // Get the necessary OCDB files from the DAQ detector DB
+  
+  // Get the necessary OCDB files from the DAQ detector DB
   if (gSystem->AccessPathName("localOCDB/T0/Calib/Slewing_Walk/",kFileExists)) {
     if (gSystem->mkdir("localOCDB/T0/Calib/Slewing_Walk/",kTRUE) != 0) {
       printf("Failed to create directory: localOCDB/T0/Calib/Slewing_Walk/");
@@ -136,7 +136,7 @@ int main(int argc, char **argv) {
     printf("Failed to get  file T0/Calib/Slewing_Walk() from DAQdetDB, status=%d\n", status);
     return -1;
   }
- 
+  
   TGraph *gr[24]; TGraph *gramp[24];
   AliCDBManager *man = AliCDBManager::Instance();
   man->SetDefaultStorage("local://localOCDB");
@@ -152,12 +152,12 @@ int main(int argc, char **argv) {
   }
   Int_t chargeQT0[24], chargeQT1[24];
   Float_t adc ,walk, amp;
- 
+  
   // Allocation of histograms - start
-
+  
   TH1F *hCFD1minCFD[24];  
   TH1F *hCFD[24], *hQT1[24], *hPed[24];  
-   
+  
   for(Int_t ic=0; ic<24; ic++) {
     hCFD1minCFD[ic] = new TH1F(Form("CFD1minCFD%d",ic+1),"CFD-CFD",kcbx,kclx,kcmx);
     hCFD[ic] = new TH1F(Form("CFD%d",ic+1),"CFD",kcfdbx,kcfdlx,kcfdhx);
@@ -169,9 +169,9 @@ int main(int argc, char **argv) {
   TH1F *hOrC = new TH1F("hOrC","OrC",kcfdbx,kcfdlx,kcfdhx);
   
    // Allocation of histograms - end
-
-
- Int_t iev=0;
+  
+  
+  Int_t iev=0;
   /* main loop (infinite) */
   for(;;) {
     struct eventHeaderStruct *event;
@@ -182,7 +182,7 @@ int main(int argc, char **argv) {
     
     /* get next event (blocking call until timeout) */
     status=monitorGetEventDynamic((void **)&event);
-     if (status==(int)MON_ERR_EOF) {
+    if (status==(int)MON_ERR_EOF) {
       printf ("End of File detected\n");
       break; /* end of monitoring file has been reached */
     }
@@ -204,11 +204,11 @@ int main(int argc, char **argv) {
       
     case START_OF_RUN:
       break;
-	
+      
     case END_OF_RUN:
       break;
       
-  case PHYSICS_EVENT:
+    case PHYSICS_EVENT:
       //	 	 case CALIBRATION_EVENT: for test
       iev++;
       
@@ -236,75 +236,75 @@ int main(int argc, char **argv) {
 	  }
 	}
       }
-      if (allData[50][0] == 0)     continue;
-      hVertex->Fill(allData[50][0]);
-      hOrA->Fill(allData[51][0]);
-      hOrC->Fill(allData[52][0]);
-      // Fill the histograms
-      walk = adc = amp = -999;
-      for (Int_t in=0; in<12;  in++)
-	{
-	  chargeQT0[in]=allData[2*in+25][0];
-	  chargeQT1[in]=allData[2*in+26][0];
-	}	
-      for (Int_t in=12; in<24;  in++)
-	{
-	  chargeQT0[in]=allData[2*in+57][0];
-	  chargeQT1[in]=allData[2*in+58][0];
-	}
-      Float_t time[24]; 
-      Float_t meanShift[24];
-      for (Int_t ik = 0; ik<24; ik++)
-	{ 	 
-	  if( ( chargeQT0[ik] - chargeQT1[ik])>0)  {
-	    adc = chargeQT0[ik] - chargeQT1[ik];
-	    //	cout<<ik <<"  "<<adc<<endl;
+      if (allData[50][0])   {
+	hVertex->Fill(allData[50][0]);
+	hOrA->Fill(allData[51][0]);
+	hOrC->Fill(allData[52][0]);
+	// Fill the histograms
+	walk = adc = amp = -999;
+	for (Int_t in=0; in<12;  in++)
+	  {
+	    chargeQT0[in]=allData[2*in+25][0];
+	    chargeQT1[in]=allData[2*in+26][0];
+	  }	
+	for (Int_t in=12; in<24;  in++)
+	  {
+	    chargeQT0[in]=allData[2*in+57][0];
+	    chargeQT1[in]=allData[2*in+58][0];
 	  }
-	  if(gr[ik] && adc>0) 
-	    walk = Int_t(gr[ik]->Eval(Double_t(adc) ) );
-	  //	  cout<<ik<<" walk "<<walk<<" "<<allData[ik+1][0]<<endl; 
-	  if(ik<12 && allData[ik+1][0]>0  ){
-	    if( walk >-100) hCFD[ik]->Fill(allData[ik+1][0] - walk);
-	    hQT1[ik]->Fill(chargeQT1[ik]);
-	    if ( allData[knpmtC][0]>0 )
-	      hCFD1minCFD[ik]->Fill(allData[ik+1][0]-allData[knpmtC][0]);
-	  }
-	  
-	  if(ik>11 && allData[ik+45][0]>0 )
-	    {
-	      if( walk >-100) hCFD[ik]->Fill(allData[ik+45][0] - walk);
-	      hQT1[ik]->Fill(chargeQT1[ik]);
-	       if (allData[56+knpmtA][0]>0)
-		 hCFD1minCFD[ik]->Fill(allData[ik+45][0]-allData[56+knpmtA][0]);
+	Float_t time[24]; 
+	Float_t meanShift[24];
+	for (Int_t ik = 0; ik<24; ik++)
+	  { 	 
+	    if( ( chargeQT0[ik] - chargeQT1[ik])>0)  {
+	      adc = chargeQT0[ik] - chargeQT1[ik];
+	      //	cout<<ik <<"  "<<adc<<endl;
 	    }
-	  if(ik<12 && allData[ik+1][0]==0 && adc>0 ) hPed[ik]->Fill(adc);
-	  if(ik>11 && allData[ik+45][0]==0 && adc>0 ) hPed[ik]->Fill(adc);
-
-	}
-	   
-     delete start;
-     start = 0x0;
-     delete reader;
-     reader= 0x0;
-      // End of fill histograms
-     /* free resources */
-    free(event);
-     
+	    if(gr[ik] && adc>0) 
+	      walk = Int_t(gr[ik]->Eval(Double_t(adc) ) );
+	    //	  cout<<ik<<" walk "<<walk<<" "<<allData[ik+1][0]<<endl; 
+	    if(ik<12 && allData[ik+1][0]>0  ){
+	      if( walk >-100) hCFD[ik]->Fill(allData[ik+1][0] - walk);
+	      hQT1[ik]->Fill(chargeQT1[ik]);
+	      if ( allData[knpmtC][0]>0 )
+		hCFD1minCFD[ik]->Fill(allData[ik+1][0]-allData[knpmtC][0]);
+	    }
+	    
+	    if(ik>11 && allData[ik+45][0]>0 )
+	      {
+		if( walk >-100) hCFD[ik]->Fill(allData[ik+45][0] - walk);
+		hQT1[ik]->Fill(chargeQT1[ik]);
+		if (allData[56+knpmtA][0]>0)
+		  hCFD1minCFD[ik]->Fill(allData[ik+45][0]-allData[56+knpmtA][0]);
+	      }
+	    if(ik<12 && allData[ik+1][0]==0 && adc>0 ) hPed[ik]->Fill(adc);
+	    if(ik>11 && allData[ik+45][0]==0 && adc>0 ) hPed[ik]->Fill(adc);
+	    
+	  }
+      }
+      delete start;
+      start = 0x0;
+      delete reader;
+      reader= 0x0;
+      
     }
     
+    // End of fill histograms
+    /* free resources */
+    free(event);
     
     /* exit when last event received, no need to wait for TERM signal */
-    if (eventT==END_OF_RUN) {
+      if (eventT==END_OF_RUN) {
       printf("EOR event detected\n");
       printf("Number of events processed - %i\n ",iev); 	
       break;
-    }
+      } // obsolete 
   }
   printf("After loop, before writing histos\n");
   // write a file with the histograms
-
+  
   TFile hist(FILE_OUT,"RECREATE");
-
+  
   for(Int_t j=0;j<24;j++){
     hCFD1minCFD[j]->SetDirectory(&hist);
     hCFD1minCFD[j]->Write();
