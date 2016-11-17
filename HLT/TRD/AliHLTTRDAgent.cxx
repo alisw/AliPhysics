6aba2024f12ed202800e7fcf0f0152ba2e7db44c
sgorbuno <Sergey.Gorbunov@cern.ch>
2016-11-17 13:37:15 +0100

    fix configuration for TRD tracker

--- HLT/TRD/AliHLTTRDAgent.cxx
+++ HLT/TRD/AliHLTTRDAgent.cxx
@@ -93,7 +93,7 @@ int AliHLTTRDAgent::CreateConfigurations(AliHLTConfigurationHandler* handler,
     rawInput+=publisher;
   }
   handler->CreateConfiguration("TRD-tracklet-reader", "TRDTrackletReader", rawInput.Data(),"");
-  handler->CreateConfiguration("TRD-tracker", "TRDTracker", "TRD-tracklet-reader","");
+  handler->CreateConfiguration("TRD-tracker", "TRDTracker", "TRD-tracklet-reader TPC-globalmerger","-debugOutput");
   return 0;
 }
 
