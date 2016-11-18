06f8b13910d1882582b3f07768a5f63f00cf6be6
ssakai <Shingo.Sakai@lnf.infn.it>
2016-11-18 22:13:16 +0900

    e-h task in pPb updated

--- PWGHF/hfe/macros/AddTaskHFEpACorrelation.C
+++ PWGHF/hfe/macros/AddTaskHFEpACorrelation.C
@@ -1,18 +1,30 @@
 AliAnalysisTaskHFEpACorrelation *AddTaskHFEpACorrelation(
-                                                        
-                                                        Bool_t  isMC                    = kFALSE,
-                                                        Int_t   triggerIndex    = 0,
-                                                        Int_t   configIndex     = 0,
-                                                        Int_t   centralityIndex = 0,
-                                                        Bool_t  isAOD                   = kFALSE,
-                                                        Bool_t isEMCal                  = kFALSE,
-                                                        char * period                   = "b",
-                                                        Int_t EMCalThreshould   = 0,
-                                                        Bool_t ispp = kFALSE,
-                                                        Int_t HadronPtCut = 0,
-                                                        TString ElectronEfficiencyFile = "alien:///alice/cern.ch/user/h/hcorreia/Efficiency/Electron_Tracking.root",
-                                                        TString HadronEfficiencyFile = "alien:///alice/cern.ch/user/h/hcorreia/Efficiency/Hadron_Tracking.root"
-                                                        )
+                                                           Int_t pTBin = 0,
+                                                           Bool_t Correlation = kTRUE,
+                                                           Bool_t ispp = kFALSE,
+                                                           Bool_t isMC = kTRUE,
+                                                           Double_t ElectronDCAxy = 0.25,
+                                                           Double_t ElectronDCAz = 1.0,
+                                                           Double_t HadronDCAxy = 0.25,
+                                                           Double_t HadronDCAz = 1.0,
+                                                           Double_t TPCPIDLow = -0.5,
+                                                           Double_t TPCPIDUp = 3.0,
+                                                           Double_t InvariantMassCut = 0.14,
+                                                           Double_t pTCutPartner = 0.0,
+                                                           Double_t MultiplicityLow = 0.,
+                                                           Double_t MultiplicityUp = 100.,
+                                                           Double_t HadronPtCutLow = 0.3,
+                                                           Double_t HadronPtCutUp = 2.0,
+                                                           Double_t EtaCutLow = -0.8,
+                                                           Double_t EtaCutUp = 0.8,
+                                                           Double_t NonHFEangleCut = 999,
+                                                           Int_t NHitsITS = 4,
+                                                           Int_t SPDLayers = 0,
+                                                           Int_t TPCNCluster = 100,
+                                                           Int_t TPCNClusterPartner = 60,
+                                                           Int_t TPCNClusterPID = 80,
+                                                           TString HadronEfficiencyFile = "alien:///alice/cern.ch/user/h/hcorreia/Efficiency/Hadron_Tracking.root"
+                                                           )
 {
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
     
@@ -31,51 +43,72 @@ AliAnalysisTaskHFEpACorrelation *AddTaskHFEpACorrelation(
     //gROOT->LoadMacro("ConfigEMCalHFEpACorrelation.C");
     
     gROOT->LoadMacro("$ALICE_PHYSICS/PWGHF/hfe/macros/configs/pPb/ConfigHFEpACorrelation.C");
-    AliAnalysisTaskHFEpACorrelation *task = ConfigHFEpACorrelation(isMC,triggerIndex,configIndex,centralityIndex,isAOD,isEMCal,EMCalThreshould,ispp,HadronPtCut);
+    TString taskName = "HFe_h";
+    taskName.Append(Form("%d_%d_%d_%d_%d_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%d_%d_%d_%d_%d",pTBin, Correlation, ispp, isMC,   ElectronDCAxy,ElectronDCAz,HadronDCAxy,HadronDCAz,TPCPIDLow,TPCPIDUp,InvariantMassCut,pTCutPartner, MultiplicityLow, MultiplicityUp, HadronPtCutLow, HadronPtCutUp, EtaCutLow, EtaCutUp, NonHFEangleCut, NHitsITS, SPDLayers, TPCNCluster, TPCNClusterPartner, TPCNClusterPID));
+
+    AliAnalysisTaskHFEpACorrelation *task = ConfigHFEpACorrelation(taskName, Correlation, ispp, isMC,   ElectronDCAxy,ElectronDCAz,HadronDCAxy,HadronDCAz,TPCPIDLow,TPCPIDUp,InvariantMassCut,pTCutPartner, MultiplicityLow, MultiplicityUp, HadronPtCutLow, HadronPtCutUp, EtaCutLow, EtaCutUp, NonHFEangleCut, NHitsITS, SPDLayers, TPCNCluster, TPCNClusterPartner, TPCNClusterPID);
     
     //_______________________
     //Trigger
-    
-    
-    if(!isMC && (period=="d" || period=="e" || period=="f"))
+    if (!isMC)
     {
-        if(triggerIndex == 0) task->SelectCollisionCandidates(AliVEvent::kINT7);
-        if(triggerIndex == 1) task->SelectCollisionCandidates(AliVEvent::kEMC7);
-        if(triggerIndex == 2) task->SelectCollisionCandidates(AliVEvent::kEMCEGA);
-    }
-    else if(!isMC)
-    {
-        if(triggerIndex == 0) task->SelectCollisionCandidates(AliVEvent::kINT7);
-        if(triggerIndex == 1) task->SelectCollisionCandidates(AliVEvent::kEMC7);
-        if(triggerIndex == 2) task->SelectCollisionCandidates(AliVEvent::kEMCEGA);
-        //if(triggerIndex == 3) task->SelectCollisionCandidates(AliVEvent::kEMC8);
-        //if(triggerIndex == 4) task->SelectCollisionCandidates(AliVEvent::kEMCEJE); //Jet Trigger
-    }
-    if(period == "pp")
-    {
-        task->SelectCollisionCandidates(AliVEvent::kMB);
+        if (!ispp)
+            task->SelectCollisionCandidates(AliVEvent::kINT7);
+        else
+            task->SelectCollisionCandidates(AliVEvent::kMB);
     }
     
-    //Seting online efficiency in TH3s
-    TFile *fileE = TFile::Open(ElectronEfficiencyFile.Data());
-    TH3F* ElecEffHisto = (TH3F*) fileE->Get(Form("Eff_Elec_Config%d_C%d",configIndex,centralityIndex));
-    task->SetEfficiencyElectron(ElecEffHisto);
+    if(pTBin ==0)
+    {
+        Float_t pTBinsCorrelation[] = {0.5,0.75,1.0,1.25,1.5,2,2.5,3,4,5,6};
+        task->SetpTBins(11,pTBinsCorrelation);
+    }
+    else if (pTBin ==1)
+    {
+        Float_t pTBinsCorrelation[] = {0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 2, 2.5, 3, 4, 5, 6}
+        task->SetpTBins(12,pTBinsCorrelation);
+    }
+    
+    if(pTBin ==2)
+    {
+        Float_t pTBinsCorrelation[] = {1,2,4,6};
+        task->SetpTBins(4,pTBinsCorrelation);
+    }
+    
+    else if (pTBin ==3)
+    {
+        Float_t pTBinsCorrelation[] = {0.5, 0.7, 0.9, 1.0, 1.2, 1.4, 1.6,1.8, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6}
+        task->SetpTBins(16,pTBinsCorrelation);
+    }
 
     
-    TFile *fileH = TFile::Open(HadronEfficiencyFile.Data());
-    TH3F* HadronEffHisto = (TH3F*) fileH->Get(Form("Eff_Hadron_Config%d_C%d",((Int_t)configIndex/1000)*1000,centralityIndex));
-    task->SetEfficiencyHadron(HadronEffHisto);
+    if(Correlation)
+    {
+    
+        TFile *fileH = TFile::Open(HadronEfficiencyFile.Data());
+        TH3F* HadronEffHisto = (TH3F*) fileH->Get(Form("HadronEff_%1.2f_%1.2f",HadronDCAxy,HadronDCAz));
+        if(HadronEffHisto)
+            task->SetEfficiencyHadron(HadronEffHisto);
+        else
+        {
+            printf("=!=!=!=!=!=!=!=! No Hadron Correction =!=!=!=!=!=!=!=!\n");
+            printf("=!=!=!=!=!=!=!=! No Hadron Correction =!=!=!=!=!=!=!=!\n");
+            printf("=!=!=!=!=!=!=!=! No Hadron Correction =!=!=!=!=!=!=!=!\n");
+            printf("=!=!=!=!=!=!=!=! No Hadron Correction =!=!=!=!=!=!=!=!\n");
+            printf("=!=!=!=!=!=!=!=! Correlation will be useless =!=!=!=!=!=!=!=!\n");
+        }
+    }
     
     
-    mgr->AddTask(task);
+    
     
     TString containerName = mgr->GetCommonFileName();
-    containerName += ":HFE_EMCal_pPb_elienos";
-    containerName += Form("_%d_%d_%d_%d",triggerIndex,configIndex,centralityIndex,EMCalThreshould);
+    containerName += ":HFE_h";
+    containerName += Form("_%d_%d_%d_%d_%d_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%d_%d_%d_%d_%d",pTBin,Correlation, ispp, isMC,   ElectronDCAxy,ElectronDCAz,HadronDCAxy,HadronDCAz,TPCPIDLow,TPCPIDUp,InvariantMassCut,pTCutPartner, MultiplicityLow, MultiplicityUp, HadronPtCutLow, HadronPtCutUp, EtaCutLow, EtaCutUp, NonHFEangleCut, NHitsITS, SPDLayers, TPCNCluster, TPCNClusterPartner, TPCNClusterPID);
     
     //Create containers for input/output
     AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
-    AliAnalysisDataContainer *coutput = mgr->CreateContainer(Form("chist_eh_%d_%d_%d_%d",triggerIndex,configIndex,centralityIndex,EMCalThreshould), TList::Class(),    AliAnalysisManager::kOutputContainer, containerName.Data());
+    AliAnalysisDataContainer *coutput = mgr->CreateContainer( Form("eh_%d_%d_%d_%d_%d_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%1.2f_%d_%d_%d_%d_%d",pTBin,Correlation, ispp, isMC,   ElectronDCAxy,ElectronDCAz,HadronDCAxy,HadronDCAz,TPCPIDLow,TPCPIDUp,InvariantMassCut,pTCutPartner, MultiplicityLow, MultiplicityUp, HadronPtCutLow, HadronPtCutUp, EtaCutLow, EtaCutUp, NonHFEangleCut, NHitsITS, SPDLayers, TPCNCluster, TPCNClusterPartner, TPCNClusterPID), TList::Class(),    AliAnalysisManager::kOutputContainer, containerName.Data());
     
     //Connect input/output
     mgr->ConnectInput(task, 0, cinput);
