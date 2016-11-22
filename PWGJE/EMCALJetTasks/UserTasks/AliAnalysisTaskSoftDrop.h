91c18f0f360d870fb912222b0cdfc988d70fe855
klapidus <kirill.lapidus@cern.ch>
2016-11-22 14:22:51 -0500

    groomed jet radius histo added, jet pt changed to ungroomed

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskSoftDrop.h
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskSoftDrop.h
@@ -45,6 +45,7 @@ class AliAnalysisTaskSoftDrop : public AliAnalysisTaskEmcalJet {
   TH1                        *fNAccJets;                //! number of jets per event
   TH1                        *fhZg;                     //!<! distribution of zg
   TH2                        *fhCorrPtZg;               //!<! distribution of zg, jet pt-diff
+  TH2                        *fhCorrPtRg;               //!<! distribution of groomed jet radius, jet pt-diff
   TH2                        *fhCorrPtPtfrac;           //!<! distribution of ptfrac, jet pt-diff
 
   AliJetContainer            *fJetsCont;                   //!Jets
