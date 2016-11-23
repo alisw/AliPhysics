35fe1887942c4c67713695e3e1d8db3ac5e90d88
dberzano <dario.berzano@cern.ch>
2016-11-23 11:40:11 +0100

    Fix library test for ZeroMQ

--- test/load_library/LoadLib.C
+++ test/load_library/LoadLib.C
@@ -5,9 +5,12 @@ Int_t LoadLib(const TString &libName)
       libName.Contains("TPHIC"))
     gSystem->Load("libpythia6");
 
-  if (libName.Contains("MONITOR")) {
+  if (libName.Contains("MONITOR") ||
+      libName.Contains("Eve") ||
+      libName.Contains("HLT") ||
+      libName.Contains("ConverterCollada")) {
     // Might load system-installed libraries (e.g. libdim.so)
-    gSystem->AddDynamicPath("/lib:/lib64:/usr/lib:/usr/lib64:/usr/local/lib:/usr/local/lib64");
+    gSystem->AddDynamicPath("/lib:/lib64:/usr/lib:/usr/lib64:/usr/local/lib:/usr/local/lib64:/usr/lib/x86_64-linux-gnu");
   }
 
   return gSystem->Load(libName);
