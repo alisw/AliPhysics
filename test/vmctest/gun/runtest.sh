ce8db3a1565239db83905c83ce22f989ed46d1f9
swenzel <sandro.wenzel@cern.ch>
2016-11-25 13:38:03 +0100

    Perform ESD check also in vmctest/gun

--- test/vmctest/gun/runtest.sh
+++ test/vmctest/gun/runtest.sh
@@ -22,6 +22,7 @@ if [ "$RUNG3" = "1" ]; then
   rm -rf *.root *.dat *.log fort* hlt hough raw* recraw/*.root recraw/*.log
   aliroot -b -q  sim.C\($NEVENTS,\""$G3CONFIG"\"\)  2>&1 | tee sim.log
   aliroot -b -q rec.C      2>&1 | tee rec.log
+  aliroot -b -q ${ALICE_ROOT}/STEER/macros/CheckESD.C  2>&1 | tee check.log
   rm -fr $G3OUTDIR
   mkdir $G3OUTDIR
   mv *.root *.log GRP $G3OUTDIR
@@ -32,6 +33,7 @@ if [ "$RUNG4" = "1" ]; then
   rm -rf *.root *.dat *.log fort* hlt hough raw* recraw/*.root recraw/*.log
   aliroot -b -q  sim.C\($NEVENTS,\""$G4CONFIG"\"\)  2>&1 | tee sim.log
   aliroot -b -q rec.C      2>&1 | tee rec.log
+  aliroot -b -q ${ALICE_ROOT}/STEER/macros/CheckESD.C  2>&1 | tee check.log
   rm -fr $G4OUTDIR
   mkdir $G4OUTDIR
   mv *.root *.log *.rndm GRP $G4OUTDIR
