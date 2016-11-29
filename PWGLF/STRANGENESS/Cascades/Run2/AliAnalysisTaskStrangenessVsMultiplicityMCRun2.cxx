26620f6234ce9fbef40b96a3b6c0e91953ecc418
ddobrigk <david.dobrigkeit.chinellato@cern.ch>
2016-11-29 11:18:13 -0200

    Bugfix

--- PWGLF/STRANGENESS/Cascades/Run2/AliAnalysisTaskStrangenessVsMultiplicityMCRun2.cxx
+++ PWGLF/STRANGENESS/Cascades/Run2/AliAnalysisTaskStrangenessVsMultiplicityMCRun2.cxx
@@ -1425,7 +1425,7 @@ void AliAnalysisTaskStrangenessVsMultiplicityMCRun2::UserExec(Option_t *)
                 fTreeVariableLeastRatioCrossedRowsOverFindable > lV0Result->GetCutLeastNumberOfCrossedRowsOverFindable() &&
                 
                 //Check 4: Minimum momentum of baryon daughter
-                ( lV0Result->GetMassHypothesis() != AliV0Result::kK0Short || lBaryonMomentum > lV0Result->GetCutMinBaryonMomentum() ) &&
+                ( lV0Result->GetMassHypothesis() == AliV0Result::kK0Short || lBaryonMomentum > lV0Result->GetCutMinBaryonMomentum() ) &&
                 
                 //Check 5: TPC dEdx selections
                 TMath::Abs(lNegdEdx)<lV0Result->GetCutTPCdEdx() &&
