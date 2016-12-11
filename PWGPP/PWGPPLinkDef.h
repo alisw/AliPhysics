4cf5df1eb787729f08f2fd6556e93460f169e09e
tdietel <tom.dietel@cern.ch>
2016-12-11 23:54:57 +0200

    add class and AddTask macro to filter TRD digits for electron and pion PID reference samples

--- PWGPP/PWGPPLinkDef.h
+++ PWGPP/PWGPPLinkDef.h
@@ -118,6 +118,7 @@
 #pragma link C++ class  AliTRDonlineTrackletQA;
 #pragma link C++ class  AliTRDPIDTree+;
 #pragma link C++ class  AliTRDPIDmatching+;
+#pragma link C++ class  AliTRDdigitsFilter;
 
 // TRD offline calibration classes
 #pragma link C++ class  AliTRDmultiplicity+;
