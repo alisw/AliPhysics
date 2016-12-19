28f88fb395ab546672403dfe055d535f84c2329c
lgraczyk <lgraczyk@cern.ch>
2016-12-19 17:23:33 +0100

    AliFemto: adding for EPOS analysis

---
+++ PWGCF/FEMTOSCOPY/macros/Train/DEtaDPhi/2016/AddMCGen/EPOS/AddMCGenEPOS.C
@@ -0,0 +1,28 @@
+AliGenerator* AddMCGenEPOS()
+{
+  //TString comment = comment.Append(" pp: EPOS-LHC");
+  // //    EPOS LHC
+  // printf("--- LAUNCHING CRMC ---\n");
+  // TString cmd = Form("$CRMC_BASEDIR/bin/crmc -t -c crmc.local.param -f crmceventfifo -o hepmc -p%d -P-%d -n5 -m0", (Int_t)energy / 2, (Int_t)energy / 2);
+  // printf("%s\n", cmd.Data());
+  // printf("----- CRMC PARAM -----\n");
+  // gROOT->ProcessLine(".! cp crmc.param crmc.local.param");
+  // gROOT->ProcessLine(".! sed -ibak 's,BASEDIR,'\"$CRMC_BASEDIR\"',' crmc.local.param");
+  // gROOT->ProcessLine(".! cat crmc.local.param");
+  // printf("----------------------\n");
+  // gROOT->ProcessLine(Form(".! %s &", cmd.Data()));
+
+  gROOT->ProcessLine(".! echo \"pass1\"");
+  gROOT->ProcessLine(".! mkfifo crmceventfifo");
+  gROOT->ProcessLine(".! ls ./");
+  gROOT->ProcessLine(".! echo \"pass2\"");
+  gROOT->ProcessLine(".! sh gen_eposlhc.sh crmceventfifo &");
+  gROOT->ProcessLine(".! echo \"pass3\"");
+
+  AliGenReaderHepMC *reader = new AliGenReaderHepMC();
+  reader->SetFileName("crmceventfifo");
+  AliGenExtFile *gener = new AliGenExtFile(-1);
+  gener->SetReader(reader);
+
+  return gener;
+}
