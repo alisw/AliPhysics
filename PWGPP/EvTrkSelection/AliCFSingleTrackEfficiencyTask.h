75d48803bd6607ab7550d144ec8b4027717df96a
zconesa <zaida.conesa.del.valle@cern.ch>
2016-12-19 15:39:29 +0100

    Added flag to decide to match or skip reconstructed tracks to kinematic tracks in the acceptance

--- PWGPP/EvTrkSelection/AliCFSingleTrackEfficiencyTask.h
+++ PWGPP/EvTrkSelection/AliCFSingleTrackEfficiencyTask.h
@@ -62,6 +62,8 @@ class AliCFSingleTrackEfficiencyTask : public AliAnalysisTaskSE {
   void SetFilterType (Int_t fbittype) { fbit=fbittype; }
   // setter for removal of fake tracks from the calculaltion (negative label at reconstructed level)
   void SetRemoveNegativeLabelTracks(Bool_t flag) { fRemoveNegativeLabelTracks=flag; }
+  // flag to match or skip the matching of reconstructed to kinematic tracks
+  void SetMatchToKinematicTrack(Bool_t flag) { fMatchToKinematicTrack=flag; }
     
   // select trigger event mask
   void SetTriggerMask(ULong64_t mask=0) { fTriggerMask=mask; }
@@ -85,6 +87,8 @@ class AliCFSingleTrackEfficiencyTask : public AliAnalysisTaskSE {
   AliSingleTrackEffCuts *GetSingleTrackEffCuts() { return (AliSingleTrackEffCuts*)fMCCuts; }
   // fake tracks removal flag
   Bool_t GetRemoveNegativeLabelTracks() { return fRemoveNegativeLabelTracks; }
+  // flag to match or skip the matching of reconstructed to kinematic tracks
+  Bool_t GetMatchToKinematicTrack() { return fMatchToKinematicTrack; }
 
 
 
@@ -114,6 +118,7 @@ class AliCFSingleTrackEfficiencyTask : public AliAnalysisTaskSE {
   Bool_t fSetFilterBit; // flag to decide if applying filter-bit selection to tracks
   Int_t  fbit;          // filter-bit selection to tracks
   Bool_t fRemoveNegativeLabelTracks; // flag to remove fake tracks (reconstructed tracks with negative label)
+  Bool_t fMatchToKinematicTrack;  // flag to check if the reconstructed track matches to a kinematic one in the good acceptance
 
   Bool_t fEvalCentrality;        // flag to enable centrality determination
   TString fCentralityEstimator;  // centrality estimator
@@ -122,7 +127,7 @@ class AliCFSingleTrackEfficiencyTask : public AliAnalysisTaskSE {
 
   TH1I  *fHistEventsProcessed;   //! histo for monitoring the number of events processed slot 1
 
-  ClassDef(AliCFSingleTrackEfficiencyTask,4)
+  ClassDef(AliCFSingleTrackEfficiencyTask,5)
 };
 
 #endif
