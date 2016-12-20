26ed84562b924fb4b727af003ba77c13a6c4ccf1
njacazio <nicolo.jacazio@cern.ch>
2016-12-20 14:39:18 +0100

    Restored DCAxy histos

--- PWGLF/SPECTRA/PiKaPr/TOF/PbPb502/task/AliAnalysisTaskTOFSpectra.cxx
+++ PWGLF/SPECTRA/PiKaPr/TOF/PbPb502/task/AliAnalysisTaskTOFSpectra.cxx
@@ -497,7 +497,9 @@ void AliAnalysisTaskTOFSpectra::Init(){//Sets everything to default values
   for(Int_t charge = 0; charge < 2; charge++){//Charge loop Positive/Negative
     for(Int_t species = 0; species < 3; species++){//Species loop
       for(Int_t ptbin = 0; ptbin < kPtBins; ptbin++){//Pt loop
-        hDCAxy[charge][species][ptbin] = 0x0;
+        for(Int_t mult = 0; mult < kEvtMultBins; mult++){//Multiplicity loop
+          hDCAxy[charge][species][ptbin][mult] = 0x0;
+        }
         hDCAxyPrimMC[charge][species][ptbin] = 0x0;
         hDCAxySecStMC[charge][species][ptbin] = 0x0;
         hDCAxySecMatMC[charge][species][ptbin] = 0x0;
@@ -996,13 +998,14 @@ void AliAnalysisTaskTOFSpectra::UserCreateOutputObjects(){
         //*****
         //Info from the DCAxy
         //*****
-        for(Int_t ptbin = 0; ptbin < kPtBins; ptbin++){//Pt loop
-          hDCAxy[charge][species][ptbin] = new TH1F(Form("hDCAxy%s%s_pt%i", pC[charge].Data(), pS[species].Data(), ptbin), Form("DCAxy Distribution of %s %s in pt %i [%.2f,%.2f];DCA_{xy} (cm);Counts", pCharge[charge].Data(), pSpecies[species].Data(), ptbin, fBinPt[ptbin], fBinPt[ptbin+1]), fDCAXYbins, DCAXYbin);
-          hDCAxy[charge][species][ptbin]->Sumw2();
-          fListHist->AddLast(hDCAxy[charge][species][ptbin]);
+        for(Int_t mult = 0; mult < kEvtMultBins; mult++){//Multiplicity loop
+          for(Int_t ptbin = 0; ptbin < kPtBins; ptbin++){//Pt loop
+            hDCAxy[charge][species][ptbin][mult] = new TH1F(Form("hDCAxy%s%s_pt%i_mult%i", pC[charge].Data(), pS[species].Data(), ptbin, mult), Form("DCAxy Distribution of %s %s in pt %i [%.2f,%.2f] and mult %i;DCA_{xy} (cm);Counts", pCharge[charge].Data(), pSpecies[species].Data(), ptbin, fBinPt[ptbin], fBinPt[ptbin+1], mult), fDCAXYbins, DCAXYbin);
+            hDCAxy[charge][species][ptbin][mult]->Sumw2();
+            fListHist->AddLast(hDCAxy[charge][species][ptbin][mult]);
+          }
         }
       }
-      
     }
     
     
@@ -1648,7 +1651,7 @@ void AliAnalysisTaskTOFSpectra::UserExec(Option_t *){
         if(TMath::Abs(fRapidity[species]) >= fRapidityCut) continue;//Rapidity cut
         
         //Pure sample selected via TOF and TPC 2 sigma cut
-        hDCAxy[fSign][species][fBinPtIndex]->Fill(fDCAXY);
+        hDCAxy[fSign][species][fBinPtIndex][fEvtMultBin]->Fill(fDCAXY);
       }
       
       if(fPdgIndex != -999 && TMath::Abs(fRapidity[fPdgIndex]) < fRapidityCut){//MC info If the track is a Pion or a Kaon or a Proton
