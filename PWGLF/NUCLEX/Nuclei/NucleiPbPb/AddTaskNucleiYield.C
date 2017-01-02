90bb2a394ea511cf0b5eb9a47b6d744516de331a
mpuccio <maximiliano.puccio@cern.ch>
2017-01-02 15:53:24 +0100

    Fine tuning of the binning

--- PWGLF/NUCLEX/Nuclei/NucleiPbPb/AddTaskNucleiYield.C
+++ PWGLF/NUCLEX/Nuclei/NucleiPbPb/AddTaskNucleiYield.C
@@ -37,10 +37,10 @@ AliAnalysisTaskNucleiYield* AddTaskNucleiYield(Bool_t isMC = kFALSE,
 
   // Common variables
   Float_t tritonCentBins[7] = {0.f,5.f,10.f,20.f,30.f,40.f,50.f};
-  Float_t deuteronPtBins[27] = {
-    0.4f,0.5f,0.6f,0.7f,0.8f,0.9f,1.0f,1.1f,1.2f,1.4f,
-    1.6f,1.8f,2.0f,2.2f,2.4f,2.6f,2.8f,3.0f,3.2f,3.4f,
-    3.6f,3.8f,4.0f,4.2f,4.4f,5.0f,6.0f
+  float deuteronPtBins[24] = {
+    0.7f,0.8f,0.9f,1.0f,1.1f,1.2f,1.4f,1.6f,1.8f,2.0f,
+    2.2f,2.4f,2.6f,2.8f,3.0f,3.2f,3.4f,3.6f,3.8f,4.0f,
+    4.2f,4.4f,5.0f,6.0f
   };
   Float_t deutBBpar[5] = {4.69637f,7.51827f,0.0183746f,2.60f,2.7f};
   Float_t sigmaBBpar = 0.1f;
@@ -62,9 +62,9 @@ AliAnalysisTaskNucleiYield* AddTaskNucleiYield(Bool_t isMC = kFALSE,
   deu->SetPDG(pdgCode);
   deu->SetIsMC(isMC);
   deu->SetCentBins(6, tritonCentBins);
-  deu->SetPtBins(26,deuteronPtBins);
-  deu->SetDCABins(80,-0.5,0.5);
-  deu->SetDCABins(31,dcabins);
+  deu->SetPtBins(23,deuteronPtBins);
+  //deu->SetDCABins(80,-0.5,0.5);
+  deu->SetDCABins(72,dcabins);
   mgr->AddTask(deu);
   TString output = "AnalysisResults.root";
   AliAnalysisDataContainer *deuCont = mgr->CreateContainer(Form("mpuccio_%s",tskname.Data()),
