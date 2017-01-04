55947383f71e8fa8684ffb5976d376810ec8e653
akubera <andrew.kubera@cern.ch>
2017-01-04 10:59:11 -0500

    PWGCF: AliFemtoManager - Modernized class and improved efficiency of collections

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoManager.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoManager.h
@@ -1,12 +1,8 @@
 ///
 /// \file  AliFemtoManager.h
-/// \class AliFemtoManager
-/// \brief Main class for managing femtoscopic analyses
 ///
-/// The Manager is the top-level object that coordinates activities
-/// and performs event, particle, and pair loops, and checks the
-/// various cuts of the snalyses in its AnalysisCollection
-///
+
+#pragma once
 
 #ifndef ALIFEMTOMANAGER_H
 #define ALIFEMTOMANAGER_H
@@ -14,24 +10,50 @@
 #include "AliFemtoTypes.h"
 #include "AliFemtoAnalysisCollection.h"
 #include "AliFemtoEventWriterCollection.h"
-#include "AliFemtoEvent.h"
 #include "AliFemtoAnalysis.h"
 #include "AliFemtoEventReader.h"
 #include "AliFemtoEventWriter.h"
 
+
+/// \class AliFemtoManager
+/// \brief Main class for managing femtoscopic analyses
+///
+/// The Manager is the top-level object containing an EventReader
+/// (the input), and collections of EventWriters and AliFemtoAnalyses
+/// (the outputs).
+///
+/// A manager object is owned by an AliAnalysistaskFemto object which
+/// calls the `ProcessEvent()` method, which reads an AliFemtoEvent
+/// from the input files, and forwards it to the `ProcessEvent` method
+/// in each output analysis, which is responsible for carrying out the
+/// actual cuts & computation.
+///
+/// AliFemtoManager objects "own" the EventReader, Analyses, and
+/// EventWriters added to them, and is responsible for deleting them
+/// upon its own destruction.
+///
+/// AliFemtoManager objects are not copyable, as the AliFemtoAnalysis
+/// objects they contain have no means of copying/cloning.
+/// Denying copyability by making the copy constructor and assignment
+/// operator private prevents potential dangling pointer (segfault)
+/// errors.
+///
 class AliFemtoManager {
 
 private:
-  AliFemtoAnalysisCollection* fAnalysisCollection;       ///< Collection of analyzes
+  AliFemtoAnalysisCollection fAnalysisCollection;        ///< Collection of analyzes
   AliFemtoEventReader*        fEventReader;              ///< Event reader
-  AliFemtoEventWriterCollection* fEventWriterCollection; ///< Event writer collection
+  AliFemtoEventWriterCollection fEventWriterCollection;  ///< Event writer collection
+
+  /// private to avoid memory bugs
+  AliFemtoManager(const AliFemtoManager& aManager);
+
+  /// private to avoid memory bugs
+  AliFemtoManager& operator=(const AliFemtoManager& aManager);
 
 public:
   AliFemtoManager();
-  AliFemtoManager(const AliFemtoManager& aManager);
   virtual ~AliFemtoManager();
-
-  AliFemtoManager& operator=(const AliFemtoManager& aManager);
 
   // Gets and Sets...
   AliFemtoAnalysisCollection* AnalysisCollection();
@@ -46,24 +68,33 @@ public:
   AliFemtoEventReader* EventReader();
   void SetEventReader(AliFemtoEventReader* r);
 
+  /// Calls `Init()` on all owned EventWriters
+  ///
+  /// Returns 0 for success, 1 for failure.
+  ///
   int Init();
-  int ProcessEvent();   ///< a "0" return value means success - otherwise quit
+
+  /// Reads event from EventReader and passes to each EventWriters'
+  /// `WriteHbtEvent` and each Analyses' `ProcessEvent` methods.
+  ///
+  /// Returns 0 if successful, otherwise the 'Status' of the
+  /// EventReader after failing to return an AliFemtoEvent.
+  ///
+  int ProcessEvent();
+
+  /// Calls `Finish()` on the EventReader, EventWriters, and the Analyses.
   void Finish();
 
-  AliFemtoString Report(); //!<
-#ifdef __ROOT__
-  /// \cond CLASSIMP
-  ClassDef(AliFemtoManager, 0);
-  /// \endcond
-#endif
+  /// Returns combined report of all readers, writers, and analyses.
+  AliFemtoString Report();
 };
 
-inline AliFemtoAnalysisCollection* AliFemtoManager::AnalysisCollection(){return fAnalysisCollection;}
-inline void AliFemtoManager::AddAnalysis(AliFemtoAnalysis* anal){fAnalysisCollection->push_back(anal);}
+inline AliFemtoAnalysisCollection* AliFemtoManager::AnalysisCollection(){return &fAnalysisCollection;}
+inline void AliFemtoManager::AddAnalysis(AliFemtoAnalysis* anal){fAnalysisCollection.push_back(anal);}
 
-inline AliFemtoEventWriterCollection* AliFemtoManager::EventWriterCollection(){return fEventWriterCollection;}
-inline void AliFemtoManager::AddEventWriter(AliFemtoEventWriter* writer){fEventWriterCollection->push_back(writer);}
-inline void AliFemtoManager::SetEventWriter(AliFemtoEventWriter* writer){fEventWriterCollection->push_back(writer);}
+inline AliFemtoEventWriterCollection* AliFemtoManager::EventWriterCollection(){return &fEventWriterCollection;}
+inline void AliFemtoManager::AddEventWriter(AliFemtoEventWriter* writer){fEventWriterCollection.push_back(writer);}
+inline void AliFemtoManager::SetEventWriter(AliFemtoEventWriter* writer){fEventWriterCollection.push_back(writer);}
 
 inline AliFemtoEventReader* AliFemtoManager::EventReader(){return fEventReader;}
 inline void AliFemtoManager::SetEventReader(AliFemtoEventReader* reader){fEventReader = reader;}
