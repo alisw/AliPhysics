7fb923897b1f7ca3fd4706f2c8748506bf7107d3
rehlersi <raymond.ehlers@cern.ch>
2017-03-02 22:40:12 -0500

    EMCal CorrFW: Update sample tasks to use it

--- PWG/EMCAL/macros/runEMCalSampleTask.C
+++ PWG/EMCAL/macros/runEMCalSampleTask.C
@@ -4,6 +4,7 @@ class AliVEvent;
 class AliAnalysisManager;
 class AliPhysicsSelectionTask;
 class AliCentralitySelectionTask;
+class AliEmcalCorrectionTask;
 class AliAnalysisGrid;
 
 void LoadMacros();
@@ -42,7 +43,9 @@ AliAnalysisManager* runEMCalSampleTask(
     iBeamType = AliAnalysisTaskEmcal::kAA;
   }
   else if (sRunPeriod == "lhc12g" || sRunPeriod == "lhc13b" || sRunPeriod == "lhc13c" ||
-      sRunPeriod == "lhc13d" || sRunPeriod == "lhc13e" || sRunPeriod == "lhc13f") {
+      sRunPeriod == "lhc13d" || sRunPeriod == "lhc13e" || sRunPeriod == "lhc13f" ||
+      sRunPeriod == "lhc16q" || sRunPeriod == "lhc16r" || sRunPeriod == "lhc16s" ||
+      sRunPeriod == "lhc16t") {
     iBeamType = AliAnalysisTaskEmcal::kpA;
   }
 
@@ -53,11 +56,9 @@ AliAnalysisManager* runEMCalSampleTask(
 
   Printf("Default track cut period set to: %s", AliTrackContainer::GetDefTrackCutsPeriod().Data());
 
-  const Bool_t   bDoTender            = kTRUE;
-  const Bool_t   bDoHadCorr           = kTRUE;
+  const Bool_t   bDoEmcalCorrections  = kTRUE;
   const Double_t kClusECut            = 0.30;
   const Double_t kTrackPtCut          = 0.15;
-  const Double_t kHadCorrF            = 2.;
 
   enum eDataType { kAod, kEsd };
 
@@ -91,10 +92,10 @@ AliAnalysisManager* runEMCalSampleTask(
   AliAnalysisManager* pMgr = new AliAnalysisManager(cTaskName);
 
   if (iDataType == kAod) {
-    AliAODInputHandler* pAODHandler = AddAODHandler();
+    AliAODInputHandler* pAODHandler = AliAnalysisTaskEmcal::AddAODHandler();
   }
   else {  
-    AliESDInputHandler* pESDHandler = AddESDHandler();
+    AliESDInputHandler* pESDHandler = AliAnalysisTaskEmcal::AddESDHandler();
   }
 
   // Physics selection task
@@ -112,74 +113,17 @@ AliAnalysisManager* runEMCalSampleTask(
   AliTaskCDBconnect *taskCDB = AddTaskCDBconnect();
   taskCDB->SetFallBackToRaw(kTRUE);
 
-  if (bDoTender) {
-    // Only cell energy/time recalibration (and bad channel) is switched on
-    Bool_t   bCalibEnergy    = kTRUE;
-    Bool_t   bCalibTime      = kTRUE;
-    Bool_t   bRemBC          = kTRUE;
-    Bool_t   bUpdateCellOnly = kTRUE;
+  if (bDoEmcalCorrections) {
+    // Configuration of the Correction Task is handled via a YAML file, which is setup below
+    // NOTE: Calling this function is equivalent to the AddTask macro, just the function is defined in the class.
+    //       The AddTask macro still exists for use on the LEGO train
+    AliEmcalCorrectionTask * correctionTask = AliEmcalCorrectionTask::AddTaskEmcalCorrectionTask();
+    correctionTask->SelectCollisionCandidates(kPhysSel);
+    correctionTask->SetForceBeamType(iBeamType);
 
-    // All these parameters are irrelevant for the tender
-    const char *cPass        = 0;
-    Bool_t   bDistBC         = kFALSE;
-    Bool_t   bRecalibClus    = kFALSE;
-    Bool_t   bRecalcClusPos  = kFALSE;
-    Bool_t   bNonLinearCorr  = kFALSE;
-    Bool_t   bRemExoticCell  = kFALSE;
-    Bool_t   bRemExoticClus  = kFALSE;
-    Bool_t   bFidRegion      = kFALSE;
-    UInt_t   iNonLinFunct    = AliEMCALRecoUtils::kNoCorrection;
-    Bool_t   bReclusterize   = kFALSE;
-    Float_t  fSeedThresh     = 0.1;      // 100 MeV
-    Float_t  fCellThresh     = 0.05;     // 50 MeV
-    UInt_t   iClusterizer    = AliEMCALRecParam::kClusterizerv2;
-    Bool_t   bTrackMatch     = kFALSE;
-
-
-    AliAnalysisTaskSE *pTenderTask = AddTaskEMCALTender(bDistBC, bRecalibClus, bRecalcClusPos, bNonLinearCorr, bRemExoticCell, bRemExoticClus,
-        bFidRegion, bCalibEnergy, bCalibTime, bRemBC, iNonLinFunct, bReclusterize, fSeedThresh,
-        fCellThresh, iClusterizer, bTrackMatch, bUpdateCellOnly, 0, 1e6, 1e6, cPass);
-    pTenderTask->SelectCollisionCandidates(kPhysSel);
-
-    // Time cuts are switched off at cell level
-    Float_t  fEMCtimeMin     = -50e-6;
-    Float_t  fEMCtimeMax     =  50e-6;
-    Float_t  fEMCtimeCut     =  1e6;
-
-    AliAnalysisTaskEMCALClusterizeFast *pClusterizerTask = AddTaskClusterizerFast("ClusterizerFast", "", "", iClusterizer,
-        0.05, 0.1, fEMCtimeMin, fEMCtimeMax, fEMCtimeCut,
-        kFALSE, kFALSE, AliAnalysisTaskEMCALClusterizeFast::kFEEData);
-    pClusterizerTask->SelectCollisionCandidates(kPhysSel);
-
-    bRemExoticClus  = kTRUE;
-    iNonLinFunct    = AliEMCALRecoUtils::kBeamTestCorrected;
-
-    AliEmcalClusterMaker *pClusterMakerTask = AddTaskEmcalClusterMaker(iNonLinFunct, bRemExoticClus, "usedefault", "", 0., kTRUE);
-    pClusterMakerTask->GetClusterContainer(0)->SetClusPtCut(0.);
-    pClusterMakerTask->GetClusterContainer(0)->SetClusECut(0.);
-    pClusterMakerTask->SelectCollisionCandidates(kPhysSel);
-  }
-
-  // Cluster-track matcher task
-  AliEmcalClusTrackMatcherTask *pMatcherTask = AddTaskEmcalClusTrackMatcher("usedefault", "usedefault", 0.1, kFALSE, kTRUE, kTRUE, kTRUE);
-  pMatcherTask->SelectCollisionCandidates(kPhysSel);
-  pMatcherTask->GetParticleContainer(0)->SetParticlePtCut(0.15);
-  pMatcherTask->GetClusterContainer(0)->SetClusNonLinCorrEnergyCut(0.15);
-  pMatcherTask->GetClusterContainer(0)->SetClusECut(0.);
-  pMatcherTask->GetClusterContainer(0)->SetClusPtCut(0.);
-
-  if (iDataType == kEsd) {
-    pMatcherTask->SetDoPropagation(kTRUE);
-  }
-
-  if (bDoHadCorr) {
-    // Hadronic correction task
-    AliHadCorrTask *pHadCorrTask = AddTaskHadCorr("usedefault", "usedefault", "",
-        kHadCorrF, 0.15, 0.030, 0.015, 0, kTRUE, kTRUE);
-    pHadCorrTask->SelectCollisionCandidates(kPhysSel);
-    pHadCorrTask->GetClusterContainer(0)->SetClusNonLinCorrEnergyCut(0.15);
-    pHadCorrTask->GetClusterContainer(0)->SetClusECut(0);
-    pHadCorrTask->GetClusterContainer(0)->SetClusPtCut(0.);
+    // Configure and initialize
+    correctionTask->SetUserConfigurationFilename("$ALICE_PHYSICS/PWG/EMCAL/config/EMCalSampleConfig.yaml");
+    correctionTask->Initialize();
   }
 
   // Sample task
@@ -246,16 +190,9 @@ AliAnalysisManager* runEMCalSampleTask(
 void LoadMacros()
 {
   // Aliroot macros
-  gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/train/AddAODHandler.C");
-  gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/train/AddESDHandler.C");
   gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskCentrality.C");
   gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
   gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/PilotTrain/AddTaskCDBconnect.C");
-  gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskEMCALTender.C");
-  gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskClusterizerFast.C");
-  gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskEmcalClusterMaker.C"); 
-  gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskEmcalClusTrackMatcher.C");
-  gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskHadCorr.C");
   gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskEmcalSample.C");
 }
 
