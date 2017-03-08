9ebcf0d17c63f8911f8830a31ad3083dc2b89500
fprino <prino@to.infn.it>
2017-03-08 17:24:53 +0100

    Fix in AddTask macro name

--- PWGPP/EvTrkSelection/macros/AddSingleTrackEfficiencyTaskPbPb.C
+++ PWGPP/EvTrkSelection/macros/AddSingleTrackEfficiencyTaskPbPb.C
@@ -55,7 +55,7 @@ const Float_t centmax_60_100 = 100.;
 
 //PID Threshold
 const Float_t thresholdPID = 0.8;
-AliCFSingleTrackEfficiencyTask *AddSingleTrackEfficiencyTask(const Bool_t readAOD = 0, // Flag to read AOD:1 or ESD:0
+AliCFSingleTrackEfficiencyTask *AddSingleTrackEfficiencyTaskPbPb(const Bool_t readAOD = 0, // Flag to read AOD:1 or ESD:0
 							     TString suffix="default", // suffix for the output directory
 							     AliPID::EParticleType specie=AliPID::kPion, Int_t pdgcode=0, //particle specie
 							     ULong64_t triggerMask=AliVEvent::kAnyINT,
@@ -279,7 +279,7 @@ AliCFSingleTrackEfficiencyTask *AddSingleTrackEfficiencyTask(const Bool_t readAO
   //===============================================================================
   TString type = mgr->GetInputEventHandler()->GetDataType();
   if (!type.Contains("ESD") && !type.Contains("AOD")) {
-    ::Error("AddSingleTrackEfficiencyTask", "AliCFSingleTrackEfficiency task needs the manager to have an ESD or AOD input handler.");
+    ::Error("AddSingleTrackEfficiencyTaskPbPb", "AliCFSingleTrackEfficiency task needs the manager to have an ESD or AOD input handler.");
     return NULL;
   }
   
