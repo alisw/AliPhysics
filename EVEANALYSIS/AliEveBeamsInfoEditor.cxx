8c1139803e5a2fd3b7ca235751b15139162af636
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-03-09 20:44:20 +0100

    Update AliEveBeamsInfoEditor.cxx

--- EVEANALYSIS/AliEveBeamsInfoEditor.cxx
+++ EVEANALYSIS/AliEveBeamsInfoEditor.cxx
@@ -6,7 +6,7 @@
  * See http://aliceinfo.cern.ch/Offline/AliRoot/License.html for          *
  * full copyright notice.                                                 *
  **************************************************************************/
-
+Something which breaks
 #include "AliEveBeamsInfo.h"
 #include "AliEveBeamsInfoEditor.h"
 
