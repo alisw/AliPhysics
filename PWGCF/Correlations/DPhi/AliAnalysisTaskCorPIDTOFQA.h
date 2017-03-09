bc73a92c64c08d21d92124f6897b1a67d98dc7e5
bschaefe <brennan.schaefer@cern.ch>
2017-03-09 13:32:33 +0100

    added TPC/TOF mismatch cut similar to that of Nosferini

--- PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFQA.h
+++ PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFQA.h
@@ -10,8 +10,10 @@
 
 
 class AliAODTrack;
+//class AliEmcalTrackSelection;
 
-
+////namespace BSchaefer_devel{
+    
 class AliAnalysisTaskCorPIDTOFQA : public AliAnalysisTaskSE  
 {
     public:
@@ -26,6 +28,7 @@ class AliAnalysisTaskCorPIDTOFQA : public AliAnalysisTaskSE
 	virtual Double_t        tof_minus_tpion(AliAODTrack *track);
 	virtual Double_t        get_mass_squared(AliAODTrack *track);
 
+
 	Double_t deut_curves[2][2][3];  /* [charge][mean,sigma][par]  */
 	TF1 *fit_deut_curve = new TF1("fit_m_mean",   "[0] + [1]*x + [2]/sqrt(x)",  1.1, 4.4);
 
@@ -36,54 +39,41 @@ class AliAnalysisTaskCorPIDTOFQA : public AliAnalysisTaskSE
 	AliPIDResponse*         fPIDResponse;
 	
 	TH1F*                   fHistPt;
-	
 	TH2F*                   cent_ntracks;
 	TH2F*                   m_squared_pos;
-	TH2F*                   m_squared_pos_cut_T;
-	TH2F*                   m_squared_pos_cut_A;
-	TH2F*                   m_squared_pos_cut_B;
-	TH2F*                   m_squared_pos_cut_V;
+	TH2F*                   m_squared_pos_cut;
 	TH2F*                   m_squared_neg;
-	TH2F*                   m_squared_neg_cut_T;
-	TH2F*                   m_squared_neg_cut_A;
-	TH2F*                   m_squared_neg_cut_B;
-	TH2F*                   m_squared_neg_cut_V;
+	TH2F*                   m_squared_neg_cut;
+	
 	TH2F*                   plength_vs_mom_pos;
 	TH2F*                   plength_vs_mom_neg;
 	TH2F*                   ttof_vs_mom_pos;
 	TH2F*                   ttof_vs_mom_neg;
 	TH2F*                   beta_vs_mom_pos;
 	TH2F*                   beta_vs_mom_neg;
-	TH2F*                   deltat_vs_mom_pos;
-	TH2F*                   deltat_vs_mom_neg;
-	TH2F*                   deut_dphi_T;
-	TH2F*                   deut_dphi_A;
-	TH2F*                   deut_dphi_B;
-	TH2F*                   deut_dphi_V;
-	TH2F*                   deut_dphi_pos_T;
-	TH2F*                   deut_dphi_pos_A;
-	TH2F*                   deut_dphi_pos_B;
-	TH2F*                   deut_dphi_pos_V;
-	TH2F*                   deut_dphi_neg_T;
-	TH2F*                   deut_dphi_neg_A;
-	TH2F*                   deut_dphi_neg_B;
-	TH2F*                   deut_dphi_neg_V;
-	TH1I*                   deuterons_per_event;
-	TH1I*                   deuterons_per_event_pos;
-	TH1I*                   deuterons_per_event_neg;
-	TH1F*                   track_phi;
-	TH1F*                   track_phi_hybrid;
-	TH1F*                   track_eta;
-	TH1F*                   track_eta_hybrid;	
-	TH2F*                   deut_dphi_deta_p0510;
-	TH2F*                   deut_dphi_deta_p1020;
-	TH2F*                   deut_dphi_deta_p2030;
-	TH2F*                   deut_dphi_deta_p3040;
-	TH2F*                   deut_dphi_deta_p4050;
+	TH2F*	                deltat_vs_mom_pos;
+	TH2F*	                deltat_vs_mom_neg;
+	
+	TH2F*                   plength_vs_mom_pos_cut;
+	TH2F*                   plength_vs_mom_neg_cut;
+	TH2F*                   ttof_vs_mom_pos_cut;
+	TH2F*                   ttof_vs_mom_neg_cut;
+	TH2F*                   beta_vs_mom_pos_cut;
+	TH2F*                   beta_vs_mom_neg_cut;
+	TH2F*	                deltat_vs_mom_pos_cut;
+	TH2F*	                deltat_vs_mom_neg_cut;
+
+	TH1F*                   h_dedx_exp_deut;
+	TH1F*                   h_dedx_exp_prot;
+	TH1F*                   h_dedx;
+	TH1F*                   h_TPC_resolution_deut;
+	TH1F*                   h_TPC_resolution_prot;
 	
         AliAnalysisTaskCorPIDTOFQA(const AliAnalysisTaskCorPIDTOFQA&);                        // not implemented
         AliAnalysisTaskCorPIDTOFQA& operator=(const AliAnalysisTaskCorPIDTOFQA&);             // not implemented
+
         ClassDef(AliAnalysisTaskCorPIDTOFQA, 1);
 };
 
+////}  //// namespace
 #endif
