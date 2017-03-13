alisw/AliPhysics

commit e40e9c06e53a8bb444ee8adfc95a3517f4ff0408

Major update for AliAnalysisTaskEmcalLight, AliAnalysisTaskDmesonJets and AliAnalysisTaskDmesonJetsDetectorResponse
AliAnalysisTaskEmcalLight
- Remove fNcentBins and use instead a vector fCentBins to define the centrality bins
- Reorganize the handling of the particle/cluster container using std::map instead of TObjArray
- Reorganize IsEventSelected and remove some uncommon selection (min pt track, min number of tracks etc.)
- Adapt AliAnalysisTaskEmcalJetQA, AliEmcalTriggerQATask, AliAnalysisTaskEmcalJetTree, AliAnalysisTaskEmcalRun2QA, AliAnalysisTaskDmesonJets, AliAnalysisTaskDmesonJetsDetectorResponse

AliAnalysisTaskDmesonJets
- Remove obsolete method void AliAnalysisTaskDmesonJets::AnalysisEngine::FindJets(AliHFJetDefinition& jetDef)
- Allow using multiple track/cluster containers (will be useful when dealing with embedding)

AliAnalysisTaskDmesonJetsDetectorResponse
- Remove option to run jet finding for unreconstructed D mesons

PWGJE/FlavourJetTasks/AliAnalysisTaskDmesonJets.cxx
@@ -684,9 +684,9 @@ AliAnalysisTaskDmesonJets::AnalysisEngine::AnalysisEngine() :
   fCurrentDmesonJetInfo(0),
   fCurrentJetInfo(0),
   fCandidateArray(0),
-  fMCContainer(0),
-  fTrackContainer(0),
-  fClusterContainer(0),
+  fMCContainer(),
+  fTrackContainers(),
+  fClusterContainers(),
   fAodEvent(0),
   fFastJetWrapper(0),
   fHistManager(0)
@@ -728,9 +728,9 @@ AliAnalysisTaskDmesonJets::AnalysisEngine::AnalysisEngine(ECandidateType_t type,
   fCurrentDmesonJetInfo(0),
   fCurrentJetInfo(0),
   fCandidateArray(0),
-  fMCContainer(0),
-  fTrackContainer(0),
-  fClusterContainer(0),
+  fMCContainer(),
+  fTrackContainers(),
+  fClusterContainers(),
   fAodEvent(0),
   fFastJetWrapper(0),
   fHistManager(0)
@@ -770,8 +770,8 @@ AliAnalysisTaskDmesonJets::AnalysisEngine::AnalysisEngine(const AliAnalysisTaskD
   fCurrentJetInfo(0),
   fCandidateArray(source.fCandidateArray),
   fMCContainer(source.fMCContainer),
-  fTrackContainer(source.fTrackContainer),
-  fClusterContainer(source.fClusterContainer),
+  fTrackContainers(source.fTrackContainers),
+  fClusterContainers(source.fClusterContainers),
   fAodEvent(source.fAodEvent),
   fFastJetWrapper(source.fFastJetWrapper),
   fHistManager(source.fHistManager)
@@ -1316,100 +1316,6 @@ void AliAnalysisTaskDmesonJets::AnalysisEngine::RunAnalysis()
   }
 }
 
-/// Finds all inclusive jets according to a certain jet definition.
-/// The jets are stored inside the jet definition object.
-///
-/// \param jetDef Reference to a AliHFJetDefinition object
-void AliAnalysisTaskDmesonJets::AnalysisEngine::FindJets(AliHFJetDefinition& jetDef)
-{
-  fFastJetWrapper->Clear();
-  fFastJetWrapper->SetR(jetDef.fRadius);
-  fFastJetWrapper->SetAlgorithm(AliEmcalJetTask::ConvertToFJAlgo(jetDef.fJetAlgo));
-  fFastJetWrapper->SetRecombScheme(AliEmcalJetTask::ConvertToFJRecoScheme(jetDef.fRecoScheme));
-
-  if (fTrackContainer && jetDef.fJetType != AliJetContainer::kNeutralJet) {
-    fTrackContainer->SetDMesonCandidate(0);
-    AddInputVectors(fTrackContainer, 100);
-  }
-
-  if (fClusterContainer && jetDef.fJetType != AliJetContainer::kChargedJet) {
-    AddInputVectors(fClusterContainer, -100);
-  }
-
-  // run jet finder
-  fFastJetWrapper->Run();
-
-  std::vector<fastjet::PseudoJet> jets_incl = fFastJetWrapper->GetInclusiveJets();
-
-  for (UInt_t ijet = 0; ijet < jets_incl.size(); ++ijet) {
-    std::vector<fastjet::PseudoJet> constituents(fFastJetWrapper->GetJetConstituents(ijet));
-
-    Double_t maxChPt = 0;
-    Double_t maxNePt = 0;
-    Double_t totalNeutralPt = 0;
-
-    for (UInt_t ic = 0; ic < constituents.size(); ++ic) {
-      if (constituents[ic].user_index() >= 100) {
-        if (constituents[ic].pt() > maxChPt) maxChPt = constituents[ic].pt();
-      }
-      else if (constituents[ic].user_index() <= -100) {
-        totalNeutralPt += constituents[ic].pt();
-        if (constituents[ic].pt() > maxNePt) maxChPt = constituents[ic].pt();
-      }
-    }
-
-    jetDef.fJets.push_back(
-        AliJetInfo(jets_incl[ijet].px(), jets_incl[ijet].py(), jets_incl[ijet].pz(), jets_incl[ijet].E(),
-            constituents.size(), totalNeutralPt / jets_incl[ijet].pt(), maxChPt, maxNePt)
-    );
-  }
-}
-
-/// Finds a jet that geometrically matches to a D meson
-///
-/// \param jetDef Reference to a AliHFJetDefinition object
-/// \param dmeson Const reference to a AliDmesonJetInfo object
-/// \param dMax Maximum distance between the D meson and the jet axis
-/// \param applyKinCuts Whether or not kinematic cuts should be applied to the jet
-/// \return Pointer to a matched jet (if found) or a null pointer
-AliAnalysisTaskDmesonJets::AnalysisEngine::jet_distance_pair AliAnalysisTaskDmesonJets::AnalysisEngine::FindJetMacthedToGeneratedDMeson(const AliDmesonJetInfo& dmeson, AliHFJetDefinition& jetDef, Double_t dMax, Bool_t applyKinCuts)
-{
-  if (jetDef.fJets.size() == 0) FindJets(jetDef);
-
-  Double_t d_closest = 999;
-  AliJetInfo* jet_closest = 0;
-  const AliJetInfo* truth_jet = 0;
-  try {
-    truth_jet = &(dmeson.fJets.at(jetDef.GetName()));
-  }
-  catch(...) {
-    return jet_distance_pair(0, 999);
-  }
-
-  for (auto& jet : jetDef.fJets) {
-    Double_t d = truth_jet->GetDistance(jet);
-
-    if (d > dMax) continue;
-    if (d < d_closest) {
-      d_closest = d;
-      jet_closest = &jet;
-    }
-  }
-
-  if (jet_closest && applyKinCuts) {
-    if (!jetDef.IsJetInAcceptance(*jet_closest)) jet_closest = 0;
-  }
-
-  if (jet_closest) {
-    AliDebug(2, Form("Found closest jet (pt=%.3f, eta=%.3f, phi=%.3f) to d meson (pt=%.3f, eta=%.3f, phi=%.3f) with d = %.3f",
-        jet_closest->Pt(), jet_closest->Eta(), jet_closest->Phi_0_2pi(),
-        dmeson.fD.Pt(), dmeson.fD.Eta(), dmeson.fD.Phi_0_2pi(),
-        d_closest));
-  }
-
-  return jet_distance_pair(jet_closest, d_closest);
-}
-
 /// Run a detector level analysis
 void AliAnalysisTaskDmesonJets::AnalysisEngine::RunDetectorLevelAnalysis()
 {
@@ -1466,7 +1372,9 @@ void AliAnalysisTaskDmesonJets::AnalysisEngine::RunDetectorLevelAnalysis()
   fHistManager->FillTH1(hname, "Both", nAccCharm[2]);
 
   hname = TString::Format("%s/fHistNAcceptedDmesonsVsNtracks", GetName());
-  fHistManager->FillTH2(hname, fTrackContainer->GetNAcceptedTracks(), nAccCharm[0]+nAccCharm[1]+nAccCharm[2]);
+  Int_t ntracks = 0;
+  for (auto track_cont : fTrackContainers) ntracks += track_cont->GetNAcceptedTracks();
+  fHistManager->FillTH2(hname, ntracks, nAccCharm[0]+nAccCharm[1]+nAccCharm[2]);
 
   hname = TString::Format("%s/fHistNDmesons", GetName());
   fHistManager->FillTH1(hname, nD);
@@ -1492,23 +1400,30 @@ Bool_t AliAnalysisTaskDmesonJets::AnalysisEngine::FindJet(AliAODRecoDecayHF2Pron
 
   fFastJetWrapper->AddInputVector(DmesonJet.fD.Px(), DmesonJet.fD.Py(), DmesonJet.fD.Pz(), DmesonJet.fD.E(), 0);
 
-  if (fTrackContainer && jetDef.fJetType != AliJetContainer::kNeutralJet) {
-    fTrackContainer->SetDMesonCandidate(Dcand);
-    hname = TString::Format("%s/%s/fHistTrackRejectionReason", GetName(), jetDef.GetName());
-    AddInputVectors(fTrackContainer, 100, static_cast<TH2*>(fHistManager->FindObject(hname)), fTrackEfficiency);
+  if (jetDef.fJetType != AliJetContainer::kNeutralJet) {
+    for (auto track_cont : fTrackContainers) {
+      AliHFTrackContainer* hftrack_cont = dynamic_cast<AliHFTrackContainer*>(hftrack_cont);
+      if (hftrack_cont) hftrack_cont->SetDMesonCandidate(Dcand);
+      hname = TString::Format("%s/%s/fHistTrackRejectionReason", GetName(), jetDef.GetName());
+      AddInputVectors(track_cont, 100, static_cast<TH2*>(fHistManager->FindObject(hname)), fTrackEfficiency);
 
-    hname = TString::Format("%s/%s/fHistDMesonDaughterNotInJet", GetName(), jetDef.GetName());
-    TH1* histDaughterNotInJet = static_cast<TH1*>(fHistManager->FindObject(hname));
-    const TObjArray& daughters = fTrackContainer->GetDaughterList();
-    for (Int_t i = 0; i < daughters.GetEntriesFast(); i++) {
-      AliVParticle* daughter = static_cast<AliVParticle*>(daughters.At(i));
-      if (!fTrackContainer->GetArray()->FindObject(daughter)) histDaughterNotInJet->Fill(daughter->Pt());
+      if (hftrack_cont) {
+        hname = TString::Format("%s/%s/fHistDMesonDaughterNotInJet", GetName(), jetDef.GetName());
+        TH1* histDaughterNotInJet = static_cast<TH1*>(fHistManager->FindObject(hname));
+        const TObjArray& daughters = hftrack_cont->GetDaughterList();
+        for (Int_t i = 0; i < daughters.GetEntriesFast(); i++) {
+          AliVParticle* daughter = static_cast<AliVParticle*>(daughters.At(i));
+          if (!hftrack_cont->GetArray()->FindObject(daughter)) histDaughterNotInJet->Fill(daughter->Pt());
+        }
+      }
     }
   }
 
-  if (fClusterContainer && jetDef.fJetType != AliJetContainer::kChargedJet) {
-    hname = TString::Format("%s/%s/fHistClusterRejectionReason", GetName(), jetDef.GetName());
-    AddInputVectors(fClusterContainer, -100, static_cast<TH2*>(fHistManager->FindObject(hname)));
+  if (jetDef.fJetType != AliJetContainer::kChargedJet) {
+    for (auto clus_cont : fClusterContainers) {
+      hname = TString::Format("%s/%s/fHistClusterRejectionReason", GetName(), jetDef.GetName());
+      AddInputVectors(clus_cont, -100, static_cast<TH2*>(fHistManager->FindObject(hname)));
+    }
   }
 
   // run jet finder
@@ -2154,9 +2069,9 @@ AliAnalysisTaskDmesonJets::AliAnalysisTaskDmesonJets() :
   fApplyKinematicCuts(kTRUE),
   fNOutputTrees(0),
   fTrackEfficiency(0),
+  fMCContainer(0),
   fAodEvent(0),
-  fFastJetWrapper(0),
-  fMCContainer(0)
+  fFastJetWrapper(0)
 {
   SetMakeGeneralHistograms(kTRUE);
 }
@@ -2173,9 +2088,9 @@ AliAnalysisTaskDmesonJets::AliAnalysisTaskDmesonJets(const char* name, Int_t nOu
   fApplyKinematicCuts(kTRUE),
   fNOutputTrees(nOutputTrees),
   fTrackEfficiency(0),
+  fMCContainer(0),
   fAodEvent(0),
-  fFastJetWrapper(0),
-  fMCContainer(0)
+  fFastJetWrapper(0)
 {
   SetMakeGeneralHistograms(kTRUE);
   for (Int_t i = 0; i < nOutputTrees; i++){
@@ -2531,11 +2446,13 @@ void AliAnalysisTaskDmesonJets::ExecOnce()
      //return;
   }
 
-  fMCContainer = dynamic_cast<AliHFAODMCParticleContainer*>(GetParticleContainer(0));
-  if (!fMCContainer) fMCContainer = dynamic_cast<AliHFAODMCParticleContainer*>(GetParticleContainer(1));
-
   TRandom* rnd = 0;
   if (fTrackEfficiency > 0 && fTrackEfficiency < 1) rnd = new TRandom3(0);
+
+  for (auto cont_it : fParticleCollArray) {
+    AliHFAODMCParticleContainer* part_cont = dynamic_cast<AliHFAODMCParticleContainer*>(cont_it.second);
+    if (part_cont) fMCContainer = part_cont;
+  }
 
   for (auto &params : fAnalysisEngines) {
 
@@ -2543,6 +2460,15 @@ void AliAnalysisTaskDmesonJets::ExecOnce()
     params.fFastJetWrapper = fFastJetWrapper;
     params.fTrackEfficiency = fTrackEfficiency;
     params.fRandomGen = rnd;
+
+    params.fMCContainer = fMCContainer;
+
+    for (auto cont_it : fParticleCollArray) {
+      AliTrackContainer* track_cont = dynamic_cast<AliTrackContainer*>(cont_it.second);
+      if (track_cont) params.fTrackContainers.push_back(track_cont);
+    }
+
+    for (auto cont_it : fClusterCollArray) params.fClusterContainers.push_back(cont_it.second);
 
     if (fAodEvent) params.Init(fGeom, fAodEvent->GetRunNumber());
 
@@ -2574,22 +2500,16 @@ void AliAnalysisTaskDmesonJets::ExecOnce()
     }
 
     if (params.fMCMode != kNoMC) {
-      if (!fMCContainer) {
+      if (!params.fMCContainer) {
         ::Error("AliAnalysisTaskDmesonJets::ExecOnce",
             "No MC particle container was provided. Analysis engine '%s' will be disabled!",
             params.GetName());
         params.fInhibit = kTRUE;
       }
-      params.fMCContainer = fMCContainer;
     }
 
     if (params.fMCMode != kMCTruth) {
-      params.fTrackContainer = dynamic_cast<AliHFTrackContainer*>(GetParticleContainer(0));
-      if (!params.fTrackContainer) params.fTrackContainer = dynamic_cast<AliHFTrackContainer*>(GetParticleContainer(1));
-
-      params.fClusterContainer = GetClusterContainer(0);
-
-      if (!params.fTrackContainer && !params.fClusterContainer) {
+      if (params.fTrackContainers.size() == 0 && params.fClusterContainers.size() == 0) {
         ::Error("AliAnalysisTaskDmesonJets::ExecOnce",
             "No track container and no cluster container were provided. Analysis engine '%s' will be disabled!",
             params.GetName());
@@ -2770,7 +2690,7 @@ void AliAnalysisTaskDmesonJets::FillPartonLevelHistograms()
 void AliAnalysisTaskDmesonJets::CalculateMassLimits(Double_t range, Int_t pdg, Int_t nbins, Double_t& minMass, Double_t& maxMass)
 {
   TParticlePDG* part = TDatabasePDG::Instance()->GetParticle(TMath::Abs(pdg));
-  
+
   Double_t mass = part->Mass();
 
   // To make sure that the PDG mass value is not at the edge of a bin
@@ -2950,7 +2870,7 @@ AliAnalysisTaskDmesonJets* AliAnalysisTaskDmesonJets::AddTaskDmesonJets(TString 
     jetTask->AdoptParticleContainer(partCont);
   }
 
-  jetTask->AddClusterContainer(nclusters);
+  jetTask->AddClusterContainer(nclusters.Data());
 
   // Final settings, pass to manager and set the containers
   mgr->AddTask(jetTask);

PWG/EMCAL/EMCALbase/AliAnalysisTaskEmcalLight.h
@@ -21,6 +21,10 @@ class AliAnalysisUtils;
 class AliEMCALTriggerPatchInfo;
 class AliAODTrack;
 
+#include <map>
+#include <set>
+#include <string>
+
 #include "Rtypes.h"
 
 #include "AliParticleContainer.h"
@@ -91,24 +95,16 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   virtual ~AliAnalysisTaskEmcalLight();
 
   // Containers
-  AliParticleContainer       *AddParticleContainer(const char *n);
-  AliTrackContainer          *AddTrackContainer(const char *n);
-  AliMCParticleContainer     *AddMCParticleContainer(const char *n);
-  AliClusterContainer        *AddClusterContainer(const char *n);
-  void                        AdoptParticleContainer(AliParticleContainer* cont)    { fParticleCollArray.Add(cont)                        ; }
-  void                        AdoptTrackContainer(AliTrackContainer* cont)          { AdoptParticleContainer(cont)                        ; }
-  void                        AdoptMCParticleContainer(AliMCParticleContainer* cont){ AdoptParticleContainer(cont)                        ; }
-  void                        AdoptClusterContainer(AliClusterContainer* cont)      { fClusterCollArray.Add(cont)                         ; }
-  AliParticleContainer       *GetParticleContainer(Int_t i=0)         const;
-  AliParticleContainer       *GetParticleContainer(const char* name)  const;
-  AliClusterContainer        *GetClusterContainer(Int_t i=0)          const;
-  AliClusterContainer        *GetClusterContainer(const char* name)   const;
-  AliMCParticleContainer     *GetMCParticleContainer(Int_t i=0)               const { return dynamic_cast<AliMCParticleContainer*>(GetParticleContainer(i))   ; }
-  AliMCParticleContainer     *GetMCParticleContainer(const char* name)        const { return dynamic_cast<AliMCParticleContainer*>(GetParticleContainer(name)); }
-  AliTrackContainer          *GetTrackContainer(Int_t i=0)                    const { return dynamic_cast<AliTrackContainer*>(GetParticleContainer(i))        ; }
-  AliTrackContainer          *GetTrackContainer(const char* name)             const { return dynamic_cast<AliTrackContainer*>(GetParticleContainer(name))     ; }
-  void                        RemoveParticleContainer(Int_t i=0)                    { fParticleCollArray.RemoveAt(i)                      ; } 
-  void                        RemoveClusterContainer(Int_t i=0)                     { fClusterCollArray.RemoveAt(i)                       ; } 
+  AliParticleContainer       *AddParticleContainer(std::string branchName, std::string contName="");
+  AliClusterContainer        *AddClusterContainer(std::string branchName, std::string contName="");
+  void                        AdoptParticleContainer(AliParticleContainer* cont)    { fParticleCollArray.insert(std::pair<std::string, AliParticleContainer*>(cont->GetName(), cont)); }
+  void                        AdoptClusterContainer(AliClusterContainer* cont)      { fClusterCollArray.insert(std::pair<std::string, AliClusterContainer*>(cont->GetName(), cont))  ; }
+  AliParticleContainer       *GetParticleContainer(std::string name)          const;
+  AliClusterContainer        *GetClusterContainer(std::string name)           const;
+  AliMCParticleContainer     *GetMCParticleContainer(std::string name)        const { return dynamic_cast<AliMCParticleContainer*>(GetParticleContainer(name)); }
+  AliTrackContainer          *GetTrackContainer(std::string name)             const { return dynamic_cast<AliTrackContainer*>(GetParticleContainer(name))     ; }
+  void                        RemoveParticleContainer(std::string name)             { fParticleCollArray.erase(name)                      ; }
+  void                        RemoveClusterContainer(std::string name)              { fClusterCollArray.erase(name)                       ; }
 
   // Other input data
   void                        SetCaloCellsName(const char *n)                       { fCaloCellsName     = n                              ; }
@@ -122,21 +118,20 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   void                        SetUseNewCentralityEstimation(Bool_t b)               { fUseNewCentralityEstimation = b                     ; }
   void                        SetMakeGeneralHistograms(Bool_t g)                    { fGeneralHistograms = g                              ; }
   void                        SetNeedEmcalGeom(Bool_t n)                            { fNeedEmcalGeom     = n                              ; }
-  void                        SetNCentBins(Int_t n)                                 { fNcentBins         = n                              ; }
+  void                        SetCentBins(const std::vector<double>& bins)          { fCentBins = std::vector<double>(bins)               ; }
+  Int_t                       GetNCentBins()                                  const { return fCentBins.size() > 1 ? fCentBins.size() - 1 : 1; }
 
   // Event selection
   void                        SetTriggerSelectionBitMap(UInt_t t)                   { fTriggerSelectionBitMap = t                         ; }
   void                        SetCentRange(Double_t min, Double_t max)              { fMinCent           = min  ; fMaxCent     = max      ; }
   void                        SetVzRange(Double_t min, Double_t max)                { fMinVz             = min  ; fMaxVz       = max      ; }
-  void                        SetZvertexDiffValue(Double_t cut)                     { fZvertexDiff       = cut                            ; }
-  void                        SetMinPtTrack(Double_t min)                           { fMinPtTrack        = min                            ; }
-  void                        SetMinNTrack(Int_t min)                               { fMinNTrack         = min                            ; }
-  void                        SetMinPtTrackInEmcal(Double_t min)                    { fMinPtTrackInEmcal = min                            ; }
+  void                        SetZvertexDiffValue(Double_t cut)                     { fMaxVzDiff         = cut                            ; }
+  void                        SetMinNVertCont(Int_t cut)                            { fMinNVertCont      = cut                            ; }
   void                        SetPtHardRange(Double_t min, Double_t max)            { fMinPtHard         = min  ; fMaxPtHard   = max      ; }
-  void                        AddAcceptedTriggerClass(const char* trigClass)        { fAcceptedTriggerClasses.Add(new TObjString(trigClass)); }
-  void                        AddRejectedTriggerClass(const char* trigClass)        { fRejectedTriggerClasses.Add(new TObjString(trigClass)); }
-  void                        ClearAcceptedTriggerClasses()                         { fAcceptedTriggerClasses.Clear()                     ; }
-  void                        ClearRejectedTriggerClasses()                         { fRejectedTriggerClasses.Clear()                     ; }
+  void                        AddAcceptedTriggerClass(const char* trigClass)        { fAcceptedTriggerClasses.insert(trigClass)           ; }
+  void                        AddRejectedTriggerClass(const char* trigClass)        { fRejectedTriggerClasses.insert(trigClass)           ; }
+  void                        ClearAcceptedTriggerClasses()                         { fAcceptedTriggerClasses.clear()                     ; }
+  void                        ClearRejectedTriggerClasses()                         { fRejectedTriggerClasses.clear()                     ; }
   void                        SetMCFilter()                                         { fMCRejectFilter = kTRUE                             ; }
   void                        ResetMCFilter()                                       { fMCRejectFilter = kFALSE                            ; }
   void                        SetJetPtFactor(Float_t f)                             { fPtHardAndJetPtFactor = f                           ; }
@@ -146,7 +141,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   void                        SetTrackPtFactor(Float_t f)                           { fPtHardAndTrackPtFactor = f                         ; }
   Float_t                     TrackPtFactor()                                       { return fPtHardAndTrackPtFactor                      ; }
 
- protected:  
+ protected:
   void                        SetRejectionReasonLabels(TAxis* axis);
   void                        AddObjectToEvent(TObject *obj, Bool_t attempt = kFALSE);
   TClonesArray               *GetArrayFromEvent(const char *name, const char *clname=0);
@@ -179,7 +174,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
    * @return True if event is selected, false otherwise
    */
   virtual Bool_t              Run()                             { return kTRUE                 ; }
-    
+
   // Static utilities
   static void                 GetEtaPhiDiff(const AliVTrack *t, const AliVCluster *v, Double_t &phidiff, Double_t &etadiff);
   static Byte_t               GetTrackType(const AliVTrack *t);
@@ -198,7 +193,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   Bool_t                      fGeneralHistograms;          ///< whether or not it should fill some general histograms
   Bool_t                      fCreateHisto;                ///< whether or not create histograms
   Bool_t                      fNeedEmcalGeom;              ///< whether or not the task needs the emcal geometry
-  Int_t                       fNcentBins;                  ///< how many centrality bins
+  std::vector<double>         fCentBins;                   ///< how many centrality bins
   Bool_t                      fUseNewCentralityEstimation; ///< Use new centrality estimation (for 2015 data)
 
   // Input data
@@ -208,8 +203,10 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   TString                     fCaloTriggerPatchInfoName;   ///< trigger patch info array name
   TString                     fCentEst;                    ///< name of the centrality estimator
 
-  TObjArray                   fParticleCollArray;          ///< particle/track collection array
-  TObjArray                   fClusterCollArray;           ///< cluster collection array
+  std::map<std::string,
+  AliParticleContainer*>      fParticleCollArray;          ///< particle/track collection array
+  std::map<std::string,
+  AliClusterContainer*>       fClusterCollArray;          ///< cluster collection array
 
   // Event selection
   UInt_t                      fTriggerSelectionBitMap;     ///< trigger selection bit map
@@ -217,14 +214,12 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   Double_t                    fMaxCent;                    ///< max centrality for event selection
   Double_t                    fMinVz;                      ///< min vertex for event selection
   Double_t                    fMaxVz;                      ///< max vertex for event selection
-  Double_t                    fZvertexDiff;                ///< upper limit for distance between primary and SPD vertex
-  Double_t                    fMinPtTrack;                 ///< cut on track pt in event selection
-  Int_t                       fMinNTrack;                  ///< minimum nr of tracks in event with pT>fTrackPtCut
-  Double_t                    fMinPtTrackInEmcal;          ///< min pt track in emcal
-  Int_t                       fMinPtHard;                  ///< select minimum pt hard (MC)
-  Int_t                       fMaxPtHard;                  ///< select maximum pt hard (MC)
-  TObjArray                   fAcceptedTriggerClasses;     ///< list of accepted trigger classes
-  TObjArray                   fRejectedTriggerClasses;     ///< list of accepted trigger classes
+  Double_t                    fMaxVzDiff;                  ///< upper limit for distance between primary and SPD vertex
+  Double_t                    fMinNVertCont;               ///< minumum number of vertex contributors
+  Double_t                    fMinPtHard;                  ///< select minimum pt hard (MC)
+  Double_t                    fMaxPtHard;                  ///< select maximum pt hard (MC)
+  std::set<std::string>       fAcceptedTriggerClasses;     ///< list of accepted trigger classes
+  std::set<std::string>       fRejectedTriggerClasses;     ///< list of accepted trigger classes
   Bool_t                      fMCRejectFilter;             ///< enable the filtering of events by tail rejection
   Float_t                     fPtHardAndJetPtFactor;       ///< Factor between ptHard and jet pT to reject/accept event.
   Float_t                     fPtHardAndClusterPtFactor;   ///< Factor between ptHard and cluster pT to reject/accept event.
@@ -250,6 +245,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   TString                     fFiredTriggerClasses;        //!<!trigger classes fired by the current event
   EBeamType_t                 fBeamType;                   //!<!event beam type
   AliGenPythiaEventHeader    *fPythiaHeader;               //!<!event Pythia header
+  Int_t                       fPtHardBin;                  //!<!event pt hard
   Double_t                    fPtHard;                     //!<!event pt hard
   Int_t                       fNTrials;                    //!<!event trials
   Float_t                     fXsection;                   //!<!x-section from pythia header
@@ -274,7 +270,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   AliAnalysisTaskEmcalLight &operator=(const AliAnalysisTaskEmcalLight&); // not implemented
 
   /// \cond CLASSIMP
-  ClassDef(AliAnalysisTaskEmcalLight, 2);
+  ClassDef(AliAnalysisTaskEmcalLight, 3);
   /// \endcond
 };
 
@@ -290,7 +286,7 @@ inline Double_t AliAnalysisTaskEmcalLight::DeltaPhi(Double_t phia, Double_t phib
 {
   Double_t dphi = -999;
   const Double_t tpi = TMath::TwoPi();
-  
+
   if (phia < 0)         phia += tpi;
   else if (phia > tpi) phia -= tpi;
   if (phib < 0)         phib += tpi;
@@ -298,7 +294,7 @@ inline Double_t AliAnalysisTaskEmcalLight::DeltaPhi(Double_t phia, Double_t phib
   dphi = phib - phia;
   if (dphi < rangeMin)      dphi += tpi;
   else if (dphi > rangeMax) dphi -= tpi;
-  
+
   return dphi;
 }
 

PWG/EMCAL/EMCALbase/PWGEMCALbaseLinkDef.h
@@ -24,4 +24,8 @@
 #pragma link C++ class AliMCParticleContainer+;
 #pragma link C++ class AliTrackContainer+;
 #pragma link C++ class AliEmcalList+;
+#pragma link C++ class std::map<std::string, AliParticleContainer*>+;
+#pragma link C++ class std::pair<std::string, AliParticleContainer*>+;
+#pragma link C++ class std::map<std::string, AliClusterContainer*>+;
+#pragma link C++ class std::pair<std::string, AliClusterContainer*>+;
 #endif

commit 6b750517ae8d9569a06c490efb772cd270b1b639

Bug fix for AliAnalysisTaskDmesonJets
The HF track container condition was never tested true

Bug introduced by commits e40e9c06e53a8bb444ee8adfc95a3517f4ff040

PWGJE/FlavourJetTasks/AliAnalysisTaskDmesonJets.cxx
@@ -1381,7 +1381,7 @@ Bool_t AliAnalysisTaskDmesonJets::AnalysisEngine::FindJet(AliAODRecoDecayHF2Pron
 
   if (jetDef.fJetType != AliJetContainer::kNeutralJet) {
     for (auto track_cont : fTrackContainers) {
-      AliHFTrackContainer* hftrack_cont = dynamic_cast<AliHFTrackContainer*>(hftrack_cont);
+      AliHFTrackContainer* hftrack_cont = dynamic_cast<AliHFTrackContainer*>(track_cont);
       if (hftrack_cont) hftrack_cont->SetDMesonCandidate(Dcand);
       hname = TString::Format("%s/%s/fHistTrackRejectionReason", GetName(), jetDef.GetName());
       AddInputVectors(track_cont, 100, static_cast<TH2*>(fHistManager->FindObject(hname)), fTrackEfficiency);