00792150b13cfc63019357723f9b8a47db0098cf
jmargutt <jacopo.margutti@cern.ch>
2017-03-21 11:15:31 +0100

    template student class, part 1

--- PWG/FLOW/Tasks/PWGflowTasksLinkDef.h
+++ PWG/FLOW/Tasks/PWGflowTasksLinkDef.h
@@ -41,6 +41,7 @@
 #pragma link C++ class AliAnalysisTaskMultiparticleCorrelations+;
 #pragma link C++ class AliAnalysisTaskPIDconfig+;
 #pragma link C++ class AliAnalysisTaskMultiparticleFemtoscopy+;
+#pragma link C++ class AliAnalysisTaskForStudents+;
 #pragma link C++ class AliAnalysisTaskVnZDC+;
 #endif
 
