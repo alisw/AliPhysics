845c9e871fad58d21c3f421f8719b353b9b8b045
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2017-04-12 19:15:37 +0200

    doxygenization and cosmetics

--- EMCAL/EMCALbase/AliEMCALLoader.h
+++ EMCAL/EMCALbase/AliEMCALLoader.h
@@ -1,19 +1,25 @@
 #ifndef ALIEMCALLOADER_H
 #define ALIEMCALLOADER_H
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
- * See cxx source for full Copyright notice                               */
+ * See cxx source for full Copyright notice                               */    
 
-//_________________________________________________________________________
+///////////////////////////////////////////////////////////////////////////////
+///
+/// \class AliEMCALLoader
+/// \ingroup EMCALbase
+/// \brief Give access to hits, digits, recpoints arrays and OCDB.
+/// 
 //  The AliEMCALLoader gets the TClonesArray and TObjArray for reading
-//  Hits, Dgits, SDigits and RecPoints. Filling is managed in the GetEvent()
+//  Hits, Digits, SDigits and RecPoints. Filling is managed in the GetEvent()
 //  method. The objects are retrived from  the corresponding folders.  
 //
 //  It also provides acces methods to the calibration and simulation OCDB parameters 
-//
-
-//*-- Author: Yves Schutz (SUBATECH) & Dmitri Peressounko (RRC KI & SUBATECH)
-//    
-
+///
+/// \author Yves Schutz (SUBATECH) 
+/// \author Dmitri Peressounko (RRC KI & SUBATECH)
+/// \author Marco van Leeuwen (LBNL)
+/// \author Gustavo Conesa Balbastre (LPSC-IN2P3-CNRS), 
+//////////////////////////////////////////////////////////////////////////////
 
 // --- ROOT system ---
 #include "TClonesArray.h"
@@ -36,7 +42,8 @@ class AliEMCALDigit ;
 class AliEMCALSDigit ;
 class AliEMCALRecPoint ; 
 
-class AliEMCALLoader : public AliLoader {
+class AliEMCALLoader : public AliLoader 
+{
   
  public:
 
@@ -48,11 +55,11 @@ class AliEMCALLoader : public AliLoader {
 
   virtual Int_t GetEvent();  // Overload to fill TClonesArray
 
-  //Clean arrays methods
-  virtual void    CleanHits() const {GetHitsDataLoader()->Clean();}       
-  virtual void    CleanSDigits() const {GetSDigitsDataLoader()->Clean();}  
-  virtual void    CleanDigits() const {GetDigitsDataLoader()->Clean();}  
-  virtual void    CleanRecPoints() const {GetRecPointsDataLoader()->Clean();}  
+  // Clean arrays methods
+  virtual void    CleanHits     () const { GetHitsDataLoader     ()->Clean() ; }       
+  virtual void    CleanSDigits  () const { GetSDigitsDataLoader  ()->Clean() ; }  
+  virtual void    CleanDigits   () const { GetDigitsDataLoader   ()->Clean() ; }  
+  virtual void    CleanRecPoints() const { GetRecPointsDataLoader()->Clean() ; }  
   
   // Initialize arrays methods
   void MakeSDigitsArray() ;
@@ -79,9 +86,9 @@ class AliEMCALLoader : public AliLoader {
     return 0x0; 
   }
   
-  void   SetDebug(Int_t level) {fDebug = level;} // Set debug level
+  void   SetDebug(Int_t level) { fDebug = level ; } // Set debug level
   
-  //OCDB access methods
+  // OCDB access methods
   
   void  SetCalibData(AliEMCALCalibData* calibda)  { fgCalibData = calibda; }
   AliEMCALCalibData * CalibData();              // to get the energy calibration CDB object
@@ -101,23 +108,25 @@ class AliEMCALLoader : public AliLoader {
  private:
   
   // assignement operator requested by coding convention, but not needed
-  AliEMCALLoader(const AliEMCALLoader &); //Not implemented
+  AliEMCALLoader                    (const AliEMCALLoader &); //Not implemented
   const AliEMCALLoader & operator = (const AliEMCALLoader &); //Not implemented
   
-  static const TString fgkECASDigitsBranchName;   //! Name of branch with ECA SDigits
-  static const TString fgkECADigitsBranchName;    //! Name of branch with ECA Digits
-  static const TString fgkECARecPointsBranchName; //! Name of branch with ECA Reconstructed Points
+  static const TString fgkECASDigitsBranchName;   //!<! Name of branch with ECA SDigits
+  static const TString fgkECADigitsBranchName;    //!<! Name of branch with ECA Digits
+  static const TString fgkECARecPointsBranchName; //!<! Name of branch with ECA Reconstructed Points
   
-  Int_t  fDebug ;             // Debug level
+  Int_t  fDebug ;                             ///<  Debug level
 	
-  static AliEMCALCalibData    * fgCalibData;  //  energy calibration data 
-  static AliEMCALCalibTime    * fgCalibTime;  //  time calibration data 
-  static AliCaloCalibPedestal * fgCaloPed;    //  dead map
-  static AliEMCALSimParam     * fgSimParam;   //  sim param 
-  static AliEMCALRecParam     * fgRecParam;   //  rec param 
+  static AliEMCALCalibData    * fgCalibData;  ///<  Energy calibration data 
+  static AliEMCALCalibTime    * fgCalibTime;  ///<  Time calibration data 
+  static AliCaloCalibPedestal * fgCaloPed;    ///<  Dead map
+  static AliEMCALSimParam     * fgSimParam;   ///<  Sim param 
+  static AliEMCALRecParam     * fgRecParam;   ///<  Rec param 
 
-  ClassDef(AliEMCALLoader,8)  // Algorithm class that provides methods to retrieve objects from a list knowing the index 
-    
+  /// \cond CLASSIMP
+  ClassDef(AliEMCALLoader,8) ; 
+  /// \endcond
+
 };
 
 #endif // AliEMCALLOADER_H
