50b0e2125dd4b61b3741c3f16f11666add613dfc
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2017-04-16 00:43:06 +0200

    merge doxymodules.h files into one in the base directory

--- EMCAL/doxymodules.h
+++ EMCAL/doxymodules.h
@@ -1,5 +1,84 @@
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+///
+/// \file doxymodules.h
+/// \brief The definitions of EMCAL class categories for Doxygen
+///
+
 /**
- * \defgroup EMCAL
+ * \defgroup EMCAL EMCAL
  * \brief EMCAL classes for simulation, reconstruction, calibration and utilities
- * \title EMCAL
+ */
+
+/**
+ * \defgroup EMCALUtils EMCALUtils
+ * \ingroup EMCAL
+ * \brief Various helpers and tools for EMCAL
+ */
+
+/**
+ * \defgroup EMCALbase EMCALbase
+ * \ingroup EMCAL
+ * \brief EMCAL base classes
+ */
+
+/**
+ * \defgroup EMCALraw EMCALraw
+ * \ingroup EMCAL
+ * \brief Classes and tools for EMCAL raw data reconstruction
+ */
+
+/**
+ * \defgroup EMCALsim EMCALsim
+ * \ingroup EMCAL
+ * \brief EMCAL simulation classes
+ */
+
+/**
+ * \defgroup EMCALrec EMCALrec
+ * \ingroup EMCAL
+ * \brief EMCAL reconstruction classes
+ */
+
+/**
+ * \defgroup EMCAL_DA EMCAL_DA
+ * \ingroup EMCAL
+ * \brief EMCAL detector algorithms (DA)
+ */
+
+/**
+ * \defgroup EMCAL_macros EMCAL_macros
+ * \ingroup EMCAL
+ * \brief EMCAL macros for testing, OCDB/OADB, generation etc.
+ */
+
+/**
+ * \defgroup EMCAL_CalibDB EMCAL_CalibDB
+ * \ingroup EMCAL_macros
+ * \brief EMCAL macros for calibration OCDB, setting, print etc.
+ */
+
+/**
+ * \defgroup EMCAL_TestData EMCAL_TestData
+ * \ingroup EMCAL_macros
+ * \brief EMCAL macros for testing, data files, dump main information stored
+ */
+
+/**
+ * \defgroup EMCAL_SimRec EMCAL_SimRec
+ * \ingroup EMCAL_macros
+ * \brief EMCAL macros for testing, simulation and reconstruction
+ */
+
+/**
+ * \defgroup EMCAL_SimRecDB EMCAL_SimRecDB
+ * \ingroup EMCAL_macros
+ * \brief EMCAL macros for generating and printing content of simulation and reconstruction OCDB files
+ */
+
+/**
+ * \defgroup EMCAL_OADB EMCAL_OADB
+ * \ingroup EMCAL_macros
+ * \brief EMCAL macros for testing, generating OADB files
  */
