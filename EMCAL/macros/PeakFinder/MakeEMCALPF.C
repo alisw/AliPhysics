5b4db456febc4154fb52f8f29cf11843e65fc919
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2017-04-16 10:15:30 +0200

    doxygenize

--- EMCAL/macros/PeakFinder/MakeEMCALPF.C
+++ EMCAL/macros/PeakFinder/MakeEMCALPF.C
@@ -1,32 +1,63 @@
-void MakeEMCALPF(){
+///
+/// \file MakeEMCALPF.C
+/// \ingroup EMCAL_SimRecDB
+/// \brief Set OCDB for EMCAL Peak Finder
+///
+/// Create OCDB for Peak Finder vectods
+///
+/// \author Per Thomas Hille <p.t.hille@fys.uio.no>, Yale. 
+///
+
+#if !defined(__CINT__)
+#include <TString.h>
+#include <TStyle.h>
+#include <TFile.h>
+
+#include "AliCDBMetaData.h"
+#include "AliCDBId.h"
+#include "AliCDBEntry.h"
+#include "AliCDBManager.h"
+#include "AliCDBStorage.h"
+
+#include "AliCaloPeakFinderVectors.h"
+#endif
+
+///
+/// Main method
+///
+void MakeEMCALPF()
+{
   const char* macroname = "MakeEMCALPF.C";
+  
   TFile *f2 = new TFile("peakfindervectors2.root",  "read" ); 
-
-  //  AliCaloPeakFinderVectors *pfv =  (AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors"); 
-
+  
+  //AliCaloPeakFinderVectors *pfv =  (AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors"); 
+  
   AliCaloPeakFinderVectors pfv =  *((AliCaloPeakFinderVectors* )f2->GetKey( "AliCaloPeakFinderVectors")); 
-
+  
   f2->Close();
+  
   {
-    //    TString Storage = "local://home/perthi/aliroot-current/OCDB/";
-    TString Storage = "local://OCDB/";  
-
-    if(!Storage.BeginsWith("local://") && !Storage.BeginsWith("alien://")) {
-      Error(macroname ,"STORAGE variable set to %s is not valid. Exiting\n",Storage.Data());
+    //TString Storage = "local://home/perthi/aliroot-current/OCDB/";
+    TString storageName = "local://OCDB/";  
+    
+    if(!storageName.BeginsWith("local://") && !storageName.BeginsWith("alien://")) 
+    {
+      //Error(macroname ,"STORAGE variable set to %s is not valid. Exiting\n",storageName.Data());
       return;
     }
     
-    Info(macroname,"Saving PF objects in CDB storage %s",
-	 Storage.Data());
+    //Info(macroname,"Saving PF objects in CDB storage %s",storageName.Data());
+    
     AliCDBManager* cdb = AliCDBManager::Instance();
-
-
-    AliCDBStorage* storage = cdb->GetStorage(Storage.Data());
-    if(!storage){
-      Error(macroname,"Unable to open storage %s\n",Storage.Data());
+    
+    AliCDBStorage* storage = cdb->GetStorage(storageName.Data());
+    if(!storage)
+    {
+      //Error(macroname,"Unable to open storage %s\n",storageName.Data());
       return;
     }
-
+    
     AliCDBMetaData* md = new AliCDBMetaData();
     md->SetResponsible("Per Thomas Hille");
     md->SetComment("Peak-Finder vectors for EMCAL");
@@ -37,16 +68,16 @@ void MakeEMCALPF(){
     //   {
     // 	cout << "  ERROR !!!!!!!!!" << endl;
     //   }
- 
+    
     // else
     
-      {
-	storage->Put( &pfv,id,md);
-      }
-
+    {
+      storage->Put( &pfv,id,md);
+    }
+    
     //  delete md;
   }
-
+  
   
   //
 }
--- EMCAL/macros/PeakFinder/pfocdbmaker.C
+++ EMCAL/macros/PeakFinder/pfocdbmaker.C
@@ -1,21 +1,46 @@
-#include  "AliCaloRawAnalyzerPeakFinder.h"
-//#include  "AliCaloRawAnalyzerPeakFinder.h"
+///
+/// \file pfocdbmaker.C
+/// \ingroup EMCAL_SimRecDB
+/// \brief Create peak finder vectors
+///
+/// Create peak finder vectors
+///
+/// \author Per Thomas Hille <p.t.hille@fys.uio.no>, Yale. 
+///
 
+#if !defined(__CINT__)
+
+#include "TFile.h"
+
+#include "AliCaloRawAnalyzerPeakFinder.h"
 #include "AliCaloPeakFinderVectors.h"
+
+#include "AliCDBEntry.h"
+
+#endif
+
 #define MAXSTART 3
 #define SAMPLERANGE 15
 #define SHIF 0.5
 
-
+///
+/// Main method
+///
 void pfocdbmaker()
 {
   AliCDBEntry e;
+  
   AliCaloRawAnalyzerPeakFinder *p = new  AliCaloRawAnalyzerPeakFinder();
+  
   e.SetObject(p);
+  
   TFile *f = new TFile("pfvectors.root", "recreate" );
-  //Read in the Peak finder vecors from file
+  
+  // Read in the Peak finder vecors from file
   p->Write();
+  
   //  e.Write();
+  
   f->Close();
 }
 
