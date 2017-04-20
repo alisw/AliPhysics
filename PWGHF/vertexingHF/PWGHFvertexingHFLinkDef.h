b7cadae578a4a82351aba873296740ffdfa616f4
Andrea Dubla <a.dubla@cern.ch>
2017-04-20 17:39:57 +0200

    update cmakelist and linkdef

--- PWGHF/vertexingHF/PWGHFvertexingHFLinkDef.h
+++ PWGHF/vertexingHF/PWGHFvertexingHFLinkDef.h
@@ -84,6 +84,7 @@
 #pragma link C++ class AliEventPlaneResolutionHandler+;
 #pragma link C++ class AliAnalysisTaskSEHFv2+;
 #pragma link C++ class AliAnalysisTaskSEHFvn+;
+#pragma link C++ class AliAnalysisTaskHFv1+;
 #pragma link C++ class AliAnalysisTaskFlowD2H+;
 #pragma link C++ class AliHFAfterBurner+;
 #pragma link C++ class AliAnalysisTaskSELambdacUp+;
