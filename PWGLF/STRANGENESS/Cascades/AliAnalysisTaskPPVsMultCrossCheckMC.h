4f2551f98e5bc2e08d11c9a45f0ab2fdf4998cb5
David Dobrigkeit Chinellato <david.dobrigkeit.chinellato@cern.ch>
2017-05-19 10:22:32 -0300

    Update in PPVsMult X-Check (Fiorella)

--- PWGLF/STRANGENESS/Cascades/AliAnalysisTaskPPVsMultCrossCheckMC.h
+++ PWGLF/STRANGENESS/Cascades/AliAnalysisTaskPPVsMultCrossCheckMC.h
@@ -60,7 +60,15 @@ public:
     void SetAlternateMCSelection ( Bool_t lSet = kTRUE ) { fAlternateMCSelection = lSet; }
     void SetSkipPS ( Bool_t lSet = kTRUE ) { fSkipPS = lSet; } 
     void SetUseRecoVtxZ ( Bool_t lSet = kTRUE ) { fUseRecoVtxZ = lSet; }
-    
+    //Task Configuration: trigger selection 
+    void SetSelectedTriggerClass(AliVEvent::EOfflineTriggerTypes trigType) { fTrigType = trigType;}
+    void SetSelectedTriggerClass(TString trigName) { fkSelectTriggerByName = kTRUE; fTrigName = trigName;}
+
+    void SetUseMultSelection ( Bool_t lUseMultSelection = kTRUE) {
+        fkMultSelection = lUseMultSelection;
+    }
+   
+ 
     Double_t GetV0MAmplitude ( AliESDEvent *lInputEvent ) const;
     
 //---------------------------------------------------------------------------------------
@@ -85,7 +93,12 @@ private:
     Bool_t fAlternateMCSelection;
     Bool_t fSkipPS;
     Bool_t fUseRecoVtxZ;
-    
+    Bool_t fkMultSelection;
+    Bool_t fkSelectTriggerByName;  // to select trigger by name (if it's not availble in AliVEvent)
+  
+    AliVEvent::EOfflineTriggerTypes fTrigType; // trigger type
+    TString   fTrigName; // trigger name (if it's not available in AliVEvent) 
+ 
     //Basic Histograms for counting events as a function of V0M percentiles...
     TH1F *fHistV0M_DataSelection; //!
     TH1F *fHistV0M_MCSelection;   //!
@@ -93,15 +106,46 @@ private:
     TH1F *fHistV0MAmplitude_MCSelection;   //!
     TH1F *fHistV0MTrue_DataSelection; //!
     TH1F *fHistV0MTrue_MCSelection;   //!
-    
+    //// same for Ntrackl08   
+    TH1F *fHistTracklets08Cent_DataSelection; //!
+    TH1F *fHistTracklets08Cent_MCSelection;   //!
+    TH1F *fHistTracklets08_DataSelection; //!
+    TH1F *fHistTracklets08_MCSelection;   //!
+    TH1F *fHistTracklets08True_DataSelection; //!
+    TH1F *fHistTracklets08True_MCSelection;   //!
+    //// same for Ntrackl0815   
+    TH1F *fHistTracklets0815Cent_DataSelection; //!
+    TH1F *fHistTracklets0815Cent_MCSelection;   //!
+    TH1F *fHistTracklets0815_DataSelection; //!
+    TH1F *fHistTracklets0815_MCSelection;   //!
+    TH1F *fHistTracklets0815True_DataSelection; //!
+    TH1F *fHistTracklets0815True_MCSelection;   //!
+ 
     TH2F *fHistV0MVsMidRapidityTrue_DataSelection; //!
     TH2F* fHistV0MAmplitudeVsMidRapidityTrue_DataSelection; //!
     TH2F *fHistV0MTrueVsMidRapidityTrue_DataSelection; //!
-    
+    ////
+    TH2F *fHistTracklets08CentVsMidRapidityTrue_DataSelection; //!
+    TH2F* fHistTracklets08VsMidRapidityTrue_DataSelection; //!
+    TH2F *fHistTracklets08TrueVsMidRapidityTrue_DataSelection; //!
+    ////
+    TH2F *fHistTracklets0815CentVsMidRapidityTrue_DataSelection; //!
+    TH2F* fHistTracklets0815VsMidRapidityTrue_DataSelection; //!
+    TH2F *fHistTracklets0815TrueVsMidRapidityTrue_DataSelection; //!
+       
     TH2F *fHistV0MVsMidRapidityTrue_MCSelection; //!
     TH2F* fHistV0MAmplitudeVsMidRapidityTrue_MCSelection; //!
     TH2F *fHistV0MTrueVsMidRapidityTrue_MCSelection; //!
-    
+    ////
+    TH2F *fHistTracklets08CentVsMidRapidityTrue_MCSelection; //!
+    TH2F* fHistTracklets08VsMidRapidityTrue_MCSelection; //!
+    TH2F *fHistTracklets08TrueVsMidRapidityTrue_MCSelection; //!
+    ////
+    TH2F *fHistTracklets0815CentVsMidRapidityTrue_MCSelection; //!
+    TH2F* fHistTracklets0815VsMidRapidityTrue_MCSelection; //!
+    TH2F *fHistTracklets0815TrueVsMidRapidityTrue_MCSelection; //!
+   
+ 
     //Desired Spectra: pi/K/p/K0/Lambda/Xi/Omega/Phi/K*
     //Data Selection Spectra
     //1-dimensional with transverse momentum (integrated in multiplicity)
@@ -124,7 +168,37 @@ private:
     TH2F *fHistPtVsV0MTrue_DataSelection[9]; //! 9 spectra
     TH2F *fHistPtVsV0MTrue_MCSelection[9];   //! 9 spectra
     
- 
+    ////
+    //2-dimensional with unchecked Tracklets08 percentile...
+    TH2F *fHistPtVsTracklets08Cent_Generated[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets08Cent_DataSelection[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets08Cent_MCSelection[9];   //! 9 spectra
+
+    //2-dimensional with Tracklets08 Amplitudes
+    TH2F *fHistPtVsTracklets08_Generated[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets08_DataSelection[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets08_MCSelection[9];   //! 9 spectra
+
+    //2-dimensional with true generated counts in Tracklets08 acceptance
+    TH2F *fHistPtVsTracklets08True_Generated[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets08True_DataSelection[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets08True_MCSelection[9];   //! 9 spectra
+    /// 
+     //2-dimensional with unchecked Tracklets08 percentile...
+    TH2F *fHistPtVsTracklets0815Cent_Generated[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets0815Cent_DataSelection[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets0815Cent_MCSelection[9];   //! 9 spectra
+
+    //2-dimensional with Tracklets0815 Amplitudes
+    TH2F *fHistPtVsTracklets0815_Generated[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets0815_DataSelection[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets0815_MCSelection[9];   //! 9 spectra
+
+    //2-dimensional with true generated counts in Tracklets0815 acceptance
+    TH2F *fHistPtVsTracklets0815True_Generated[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets0815True_DataSelection[9]; //! 9 spectra
+    TH2F *fHistPtVsTracklets0815True_MCSelection[9];   //! 9 spectra
+
     
     AliAnalysisTaskPPVsMultCrossCheckMC(const AliAnalysisTaskPPVsMultCrossCheckMC&);            // not implemented
     AliAnalysisTaskPPVsMultCrossCheckMC& operator=(const AliAnalysisTaskPPVsMultCrossCheckMC&); // not implemented
--- PWGLF/STRANGENESS/Cascades/macros/AddTaskPPVsMultCrossCheckMC.C
+++ PWGLF/STRANGENESS/Cascades/macros/AddTaskPPVsMultCrossCheckMC.C
@@ -1,39 +1,42 @@
 AliAnalysisTaskPPVsMultCrossCheckMC *AddTaskPPVsMultCrossCheckMC( const TString lMasterJobSessionFlag = "")
 {
-// Creates, configures and attaches to the train a cascades check task.
-   // Get the pointer to the existing analysis manager via the static access method.
-   //==============================================================================
-   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-   if (!mgr) {
-      ::Error("AliAnalysisTaskPPVsMultCrossCheckMC", "No analysis manager to connect to.");
-      return NULL;
-   }   
-
-   // Check the analysis type using the event handlers connected to the analysis manager.
-   //==============================================================================
-   if (!mgr->GetInputEventHandler()) {
-       printf("Caution: No input handler detected!");
-   }   
-   //TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
-
-  // Create and configure the task
-  AliAnalysisTaskPPVsMultCrossCheckMC *taskAuxiliary = new AliAnalysisTaskPPVsMultCrossCheckMC("taskAuxiliary");
-  mgr->AddTask(taskAuxiliary);
-  TString outputFileName = AliAnalysisManager::GetCommonFileName();
-   
-  outputFileName += ":PWGLF_PPVsMultXCheck";
-  if (mgr->GetMCtruthEventHandler()) outputFileName += "_MC";
-   
-  Printf("Set OutputFileName : \n %s\n", outputFileName.Data() );
-
-  AliAnalysisDataContainer *coutputList = mgr->CreateContainer("cList",
-							     TList::Class(),
-							     AliAnalysisManager::kOutputContainer,
-							     outputFileName );
-
-  //Recommendation: Tree as a single output slot
-  mgr->ConnectInput (taskAuxiliary, 0, mgr->GetCommonInputContainer());
-  mgr->ConnectOutput(taskAuxiliary, 1, coutputList);
-  
-  return taskAuxiliary;
+    // Creates, configures and attaches to the train a cascades check task.
+    // Get the pointer to the existing analysis manager via the static access method.
+    //==============================================================================
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    if (!mgr) {
+        ::Error("AliAnalysisTaskPPVsMultCrossCheckMC", "No analysis manager to connect to.");
+        return NULL;
+    }
+    
+    // Check the analysis type using the event handlers connected to the analysis manager.
+    //==============================================================================
+    if (!mgr->GetInputEventHandler()) {
+        printf("Caution: No input handler detected!");
+    }
+    //TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+    
+    // Create and configure the task
+    AliAnalysisTaskPPVsMultCrossCheckMC *taskAuxiliary = new AliAnalysisTaskPPVsMultCrossCheckMC("taskAuxiliary");
+      taskAuxiliary->SetSelectedTriggerClass(AliVEvent::kINT7);
+      taskAuxiliary->SetUseMultSelection(kTRUE);
+      taskAuxiliary->SetSkipPS(kTRUE);
+    mgr->AddTask(taskAuxiliary);
+    TString outputFileName = AliAnalysisManager::GetCommonFileName();
+    
+    outputFileName += ":PWGLF_PPVsMultXCheck";
+    if (mgr->GetMCtruthEventHandler()) outputFileName += "_MC";
+    
+    Printf("Set OutputFileName : \n %s\n", outputFileName.Data() );
+    
+    AliAnalysisDataContainer *coutputList = mgr->CreateContainer("cList",
+                                                                 TList::Class(),
+                                                                 AliAnalysisManager::kOutputContainer,
+                                                                 outputFileName );
+    
+    //Recommendation: Tree as a single output slot
+    mgr->ConnectInput (taskAuxiliary, 0, mgr->GetCommonInputContainer());
+    mgr->ConnectOutput(taskAuxiliary, 1, coutputList);
+    
+    return taskAuxiliary;
 }   
