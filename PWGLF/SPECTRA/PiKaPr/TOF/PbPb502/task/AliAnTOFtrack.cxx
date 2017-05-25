09a52d3507b52d43fccbcb569c01c5a0ab5e6655
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2017-05-25 14:33:38 +0200

    Moved methods to cxx file of AliAnTOFtrack.cxx

--- PWGLF/SPECTRA/PiKaPr/TOF/PbPb502/task/AliAnTOFtrack.cxx
+++ PWGLF/SPECTRA/PiKaPr/TOF/PbPb502/task/AliAnTOFtrack.cxx
@@ -166,3 +166,168 @@ Bool_t AliAnTOFtrack::IsNegative(){
   if(GetMaskBit(fTrkMask, kNegTrk)) return kTRUE;
   return kFALSE;
 }
+
+//________________________________________________________________________
+Double_t AliAnTOFtrack::GetDCA(const Bool_t xy){
+  Double_t u, d;
+  GetBinnedDCA(d, u, xy);
+  return d + (u-d)/2;
+}
+
+////////////////////////
+/////TEST functions/////
+////////////////////////
+
+//________________________________________________________________________
+void AliAnTOFtrack::TestDCAXYBinning(){
+  Int_t dim = static_cast<Int_t>(TMath::Power(2., 8.*sizeof(fDCAXYIndex)));
+  Double_t range = (2.*fDCAXYRange);
+  std::cout<<"fDCAXYIndex has "<<dim<<" possibilities"<<std::endl;
+  std::cout<<"DCAXY range is ["<<-fDCAXYRange<<","<<fDCAXYRange<<"] -> "<<range<<" bin width = "<<range/dim<<std::endl;
+  std::cout<<"Variable dimension is "<<dim<<" while actual bin required are "<<kDCAXYBins<<std::endl;
+  
+  dim = static_cast<Int_t>(TMath::Power(2., 8.*sizeof(fDCAZIndex)));
+  range = (2.*fDCAZRange);
+  std::cout<<"fDCAZIndex has "<<dim<<" possibilities"<<std::endl;
+  std::cout<<"DCAZ range is ["<<-fDCAZRange<<","<<fDCAZRange<<"] -> "<<range<<" bin width = "<<range/dim<<std::endl;
+  std::cout<<"Variable dimension is "<<dim<<" while actual bin required are "<<kDCAZBins<<std::endl;
+  
+  for (Int_t var = 0; var <=20; ++var) {
+    //       Double_t rnd = 2.*fDCAXYRange*gRandom->Rndm()-1;
+    Double_t rnd = 2.*fDCAXYRange*(Double_t)var/20.-fDCAXYRange;
+    ComputeDCABin(rnd, kTRUE);
+    
+    Double_t binlow, binup;
+    GetBinnedDCA(binlow, binup, 1);
+    
+    std::cout<<rnd<<" Index: "<<fDCAXYIndex<<"   ["<<binlow<<" ; "<<binup<<"] --> Diff --> ["<< binlow - rnd <<" ; "<<binup - rnd<<"]"<<std::endl;
+    //       cout<<rnd<<"  "<<TMath::Abs((rnd+fDCAXYRange)*kDCAXYBins/(2*fDCAXYRange))<<" Index: "<<fDCAXYIndex<<"   ["<<binlow<<" ; "<<binup<<"] -->  ["<< binlow - rnd <<" ; "<<binup - rnd<<"]"<<endl;
+  }
+  
+  //     for (Int_t c = 0 ; c <= kDCAXYBins; ++c) cout<<c<<"  "<<-fDCAXYRange+2.*fDCAXYRange*c/kDCAXYBins<<endl;
+  
+}
+
+///////////////////////////
+///DCA Utility functions///
+///////////////////////////
+
+//________________________________________________________________________
+void AliAnTOFtrack::ComputeDCABin(const Double_t dca, const Bool_t xy){
+  if(xy){
+    if(dca > fDCAXYRange){//Overflow
+      fDCAXYIndex = kDCAXYBins+2;
+      return;
+    }
+    else if(dca <= -fDCAXYRange){//Underflow
+      fDCAXYIndex = kDCAXYBins+1;
+      return;
+    }
+    fDCAXYIndex = static_cast<UShort_t>(TMath::Ceil((dca+fDCAXYRange)/((2*fDCAXYRange)/kDCAXYBins)) -1);
+  }
+  else{
+    if(dca > fDCAZRange){//Overflow
+      fDCAZIndex = kDCAZBins+2;
+      return;
+    }
+    else if(dca <= -fDCAZRange){//Underflow
+      fDCAZIndex = kDCAZBins+1;
+      return;
+    }
+    fDCAZIndex = static_cast<UShort_t>(TMath::Ceil((dca+fDCAZRange)/((2*fDCAZRange)/kDCAZBins)) -1);
+  }
+  
+};
+
+//________________________________________________________________________
+void AliAnTOFtrack::ComputeDCABin(const Double_t dcaxy, const Double_t dcaz){
+  
+  ComputeDCABin(dcaxy, (Bool_t) kTRUE);
+  ComputeDCABin(dcaz, (Bool_t) kFALSE);
+};
+
+
+//////////////////////////
+///T0 Utility functions///
+//////////////////////////
+
+//________________________________________________________________________
+Double_t AliAnTOFtrack::GetT0Resolution(const Double_t TOFsigma) const {
+  return TMath::Sqrt(TMath::Power(fTOFExpSigma[0], 2) - TMath::Power(TOFsigma, 2) - TMath::Power(15./GetMomentum(), 2));
+}
+
+////////////////////////
+///T0 Usage functions///
+////////////////////////
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0TOF(){
+  if(GetMaskBit(fTrkMask, kT0_0)) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0A(){
+  if(GetMaskBit(fTrkMask, kT0_1)) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0C(){
+  if(GetMaskBit(fTrkMask, kT0_2)) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0Fill(){
+  if(!GetMaskBit(fTrkMask, kT0_0) && !GetMaskBit(fTrkMask, kT0_1) && !GetMaskBit(fTrkMask, kT0_2)) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0TOF_T0A(){
+  if(IsT0TOF() && IsT0A()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsOrT0TOF_T0A(){
+  if(IsT0TOF() || IsT0A()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0TOF_T0C(){
+  if(IsT0TOF() && IsT0C()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsOrT0TOF_T0C(){
+  if(IsT0TOF() || IsT0C()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0A_T0C(){
+  if(IsT0A() && IsT0C()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsOrT0A_T0C(){
+  if(IsT0A() || IsT0C()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsT0TOF_T0A_T0C(){
+  if(IsT0TOF() && IsT0A() && IsT0C()) return kTRUE;
+  return kFALSE;
+}
+
+//________________________________________________________________________
+Bool_t AliAnTOFtrack::IsOrT0TOF_T0A_T0C(){
+  if(IsT0TOF() || IsT0A() || IsT0C()) return kTRUE;
+  return kFALSE;
+}
