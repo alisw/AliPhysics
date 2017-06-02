a1a6ff5b067dba4db12ac73675e973d70aa81797
Anastasia Barbano <anastasia.maria.barbano@cern.ch>
2017-06-02 09:23:34 +0200

    Increased the class revision in AliAnalysisTrackingUncertaintiesAOT.h

--- PWGPP/EvTrkSelection/AliAnalysisTrackingUncertaintiesAOT.h
+++ PWGPP/EvTrkSelection/AliAnalysisTrackingUncertaintiesAOT.h
@@ -121,7 +121,7 @@ class AliAnalysisTrackingUncertaintiesAOT : public AliAnalysisTaskSE {
   AliAnalysisTrackingUncertaintiesAOT(const AliAnalysisTrackingUncertaintiesAOT&);
   AliAnalysisTrackingUncertaintiesAOT& operator=(const AliAnalysisTrackingUncertaintiesAOT&);
     
-  ClassDef(AliAnalysisTrackingUncertaintiesAOT, 5);
+  ClassDef(AliAnalysisTrackingUncertaintiesAOT, 6);
 };
 
 #endif
