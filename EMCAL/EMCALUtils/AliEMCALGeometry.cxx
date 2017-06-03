alisw/AliPhysics

commit 88429ce92e7c55c8cadb223ade4d6628104f871b

Load EMCal misal matrices only once

EMCAL/EMCALUtils/AliEMCALGeometry.cxx
@@ -1518,12 +1518,14 @@ const TGeoHMatrix * AliEMCALGeometry::GetMatrixForSuperModule(Int_t smod) const
       printf("\t AliEMCALGeometry::SetMisalMatrixes(header->GetEMCALMisalMatrix()) \n") ;
       AliFatal("") ;
     }  
-  }//external matrices
+ } // external matrices
   
   // If gGeoManager exists, take matrix from it
-  if(gGeoManager) return GetMatrixForSuperModuleFromGeoManager(smod);
-  
-  return 0 ;
+  // only once to speed up things
+  if ( gGeoManager && !fkSModuleMatrix[smod] )
+    fkSModuleMatrix[smod] = GetMatrixForSuperModuleFromGeoManager(smod) ;
+
+  return fkSModuleMatrix[smod];
 }
 
 ///
@@ -1736,9 +1738,15 @@ void AliEMCALGeometry::SetMisalMatrix(const TGeoHMatrix * m, Int_t smod)
 {
   fUseExternalMatrices = kTRUE;
 
-  if (smod >= 0 && smod < fEMCGeometry->GetNumberOfSuperModules()){
-    if(!fkSModuleMatrix[smod]) fkSModuleMatrix[smod] = new TGeoHMatrix(*m) ; //Set only if not set yet
-  } else AliFatal(Form("Wrong supermodule index -> %d",smod));
+  if (smod >= 0 && smod < fEMCGeometry->GetNumberOfSuperModules())
+  {
+   if ( !fkSModuleMatrix[smod] )
+      fkSModuleMatrix[smod] = new TGeoHMatrix(*m) ; //Set only if not set yet
+  }
+  else
+  {
+    AliFatal(Form("Wrong supermodule index -> %d",smod));
+  }
 }
 
 ///

EMCAL/EMCALUtils/AliEMCALGeometry.h
@@ -469,7 +469,7 @@ protected:
   Float_t  fZLength;		             ///< Total length in z direction
   Float_t  fSampling;		             ///< Sampling factor
 	
-  TGeoHMatrix* fkSModuleMatrix[AliEMCALGeoParams::fgkEMCALModules] ; ///< Orientations of EMCAL super modules
+  mutable const TGeoHMatrix* fkSModuleMatrix[AliEMCALGeoParams::fgkEMCALModules] ; ///< Orientations of EMCAL super modules
   Bool_t   fUseExternalMatrices;     ///< Use the matrices set in fkSModuleMatrix and not those in the geoManager
 	
 private:

commit e8bef0f07707e11c3080a870264c8bd608e08d0b

Bug fix: EMCal misal matrices were not properly load.
This bug was introduced by commit 88429ce92e7c55c8cadb223ade4d6628104f871b

EMCAL/EMCALUtils/AliEMCALGeometry.cxx
@@ -1501,30 +1501,20 @@ const TGeoHMatrix * AliEMCALGeometry::GetMatrixForSuperModule(Int_t smod) const
 {	
   if(smod < 0 || smod > fEMCGeometry->GetNumberOfSuperModules()) 
     AliFatal(Form("Wrong supermodule index -> %d",smod));
-		
-  // Use matrices set externally
-  if(!gGeoManager)
-  {
-    if(fkSModuleMatrix[smod])
-    {
-      return fkSModuleMatrix[smod] ;
+
+  if (!fkSModuleMatrix[smod]) {
+    if (gGeoManager) {
+      SetMisalMatrix(GetMatrixForSuperModuleFromGeoManager(smod), smod);
     }
-    else
-    {
+    else {
       AliInfo("Stop:");
       printf("\t Can not find EMCAL misalignment matrixes\n") ;
       printf("\t Either import TGeoManager from geometry.root or \n");
       printf("\t read stored matrixes from AliESD Header:  \n") ;   
-      printf("\t AliEMCALGeometry::SetMisalMatrixes(header->GetEMCALMisalMatrix()) \n") ;
-      AliFatal("") ;
+      printf("\t AliEMCALGeometry::GetMatrixForSuperModule(Int_t smod) \n") ;
+      AliFatal("");
     }  
- } // external matrices
-  
-  // If gGeoManager exists, take matrix from it
-  // only once to speed up things
-  if ( gGeoManager && !fkSModuleMatrix[smod] )
-    fkSModuleMatrix[smod] = GetMatrixForSuperModuleFromGeoManager(smod) ;
-
+  }
   return fkSModuleMatrix[smod];
 }
 
@@ -1734,7 +1724,7 @@ void AliEMCALGeometry::RecalculateTowerPosition(Float_t drow, Float_t dcol, cons
 /// Move from header due to coding violations : Dec 2,2011 by PAI
 ///
 //__________________________________________________________________________________________________________________
-void AliEMCALGeometry::SetMisalMatrix(const TGeoHMatrix * m, Int_t smod) 
+void AliEMCALGeometry::SetMisalMatrix(const TGeoHMatrix * m, Int_t smod) const
 {
   if (smod >= 0 && smod < fEMCGeometry->GetNumberOfSuperModules())
   {

EMCAL/EMCALUtils/AliEMCALGeometry.h
@@ -322,7 +322,7 @@ public:
   }
 
   //Method to set shift-rotational matrixes from ESDHeader
-  void SetMisalMatrix(const TGeoHMatrix * m, Int_t smod);
+  void SetMisalMatrix(const TGeoHMatrix * m, Int_t smod) const;
 	
   //Alternate geometry that allows to calculate tower position for different particles and different alignments
   void RecalculateTowerPosition(Float_t drow, Float_t dcol, const Int_t sm, const Float_t depth,