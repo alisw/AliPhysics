6ece8697cbc21d8af3102bb6c9b8aaa01c8b5477
Chiara Oppedisano <chiara.oppedisano@to.infn.it>
2017-06-08 12:53:44 +0200

    Updating beam crossing method applied to slow nucleons

--- EVGEN/AliGenSlowNucleons.h
+++ EVGEN/AliGenSlowNucleons.h
@@ -5,7 +5,7 @@
 
 /* $Id$ */
 //
-//  Generator for slow nucleons in pA interactions. 
+//  Generator for slow nucleons in pA interactions.
 //  Source is modelled by a relativistic Maxwell distributions.
 //  Original code by  Ferenc Sikler  <sikler@rmki.kfki.hu>
 //  This class: andreas.morsch@cern.ch
@@ -36,7 +36,7 @@ public:
     virtual void SetBetaSource(Double_t b1 = 0.05, Double_t b2 = 0.)
 	{fBetaSourceG = b1; fBetaSourceB = b2;}
     //
-    virtual void SetSlowNucleonModel(AliSlowNucleonModel* model) 
+    virtual void SetSlowNucleonModel(AliSlowNucleonModel* model)
 	{fSlowNucleonModel = model;}
     virtual Bool_t NeedsCollisionGeometry() const {return kTRUE;}
     virtual void   SetCollisionGeometry(AliCollisionGeometry* geom)
@@ -54,15 +54,16 @@ public:
     virtual Int_t  GetNGrayProtons()   {return fNgp;}
     virtual Int_t  GetNGrayNeutrons()  {return fNgn;}
     virtual Int_t  GetNBlackProtons()  {return fNbp;}
-    virtual Int_t  GetNBlackNeutrons() {return fNbn;}    
+    virtual Int_t  GetNBlackNeutrons() {return fNbn;}
     //
     virtual void   SetModelSmear(Int_t imode) {fSmearMode=imode;}
-    
+
  protected:
     void     GenerateSlow(Int_t charge, Double_t T, Double_t beta, Float_t* q, Float_t &theta);
     Double_t Maxwell(Double_t m, Double_t p, Double_t t);
     void     Lorentz(Double_t m, Double_t beta, Float_t* q);
-    void     BeamCrossDivergence(Int_t iwhat, Float_t *pLab);;
+    void     BeamDivergence(Float_t *pLab);
+    void     BeamCrossing(Float_t *pLab);
     void     AddAngle(Double_t theta1, Double_t phi1, Double_t theta2,
   	        	Double_t phi2, Double_t *angle);
     void     SetProcessID(Int_t nt, UInt_t process);
@@ -106,10 +107,3 @@ public:
     ClassDef(AliGenSlowNucleons,4) // Slow Nucleon Generator
 };
 #endif
-
-
-
-
-
-
-
