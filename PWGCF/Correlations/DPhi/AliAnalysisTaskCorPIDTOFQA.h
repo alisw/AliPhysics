ffea6b14615e7d6555d0f4c3a3d54c37844d1282
Brennan Schaefer <brennan.schaefer@cern.ch>
2017-06-12 11:45:25 +0200

    changed from proton to unidentified hadron correlations

--- PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFQA.h
+++ PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFQA.h
@@ -29,8 +29,8 @@ class AliAnalysisTaskCorPIDTOFQA : public AliAnalysisTaskSE
 	virtual Double_t        get_mass_squared(AliAODTrack *track);
 
 
-	Double_t hadr_curves[2][2][4];  /* [charge][mean,sigma][par]  */
-	TF1 *fit_hadr_curve = new TF1("fit_m_mean",   "[0] + [1]*x + [2]/sqrt(x) + [3]*x^5",  1.0, 4.0);
+	Double_t prot_curves[2][2][4];  /* [charge][mean,sigma][par]  */
+	TF1 *fit_prot_curve = new TF1("fit_m_mean",   "[0] + [1]*x + [2]/sqrt(x) + [3]*x^5",  1.0, 4.0);
 
     private:
 
@@ -64,15 +64,27 @@ class AliAnalysisTaskCorPIDTOFQA : public AliAnalysisTaskSE
 	TH2F*                   dedx_deltat_neg_cut;    // 24
 	TH3F*                   dedx_p_deltat_pos_cut;  // 25
 	TH3F*                   dedx_p_deltat_neg_cut;  // 26
-	TH2F*                   hadr_dphi_T;            // 27
-	TH2F*                   hadr_dphi_pos_T;        // 28
-	TH2F*                   hadr_dphi_neg_T;        // 29
-	TH1I*                   hadr_per_event;         // 36
-	TH1I*                   hadr_per_event_pos;     // 37
-	TH1I*                   hadr_per_event_neg;	// 38
+	TH2F*                   prot_dphi_T;            // 27
+	TH2F*                   prot_dphi_pos_T;        // 28
+	TH2F*                   prot_dphi_neg_T;        // 29
+	TH2F*                   prot_dphi_A;            // 30
+	TH2F*                   prot_dphi_pos_A;        // 31
+	TH2F*                   prot_dphi_neg_A;	// 32
+	TH2F*                   prot_dphi_B;            // 33
+	TH2F*                   prot_dphi_pos_B;        // 34
+	TH2F*                   prot_dphi_neg_B;        // 35
+	TH1I*                   prot_per_event;         // 36
+	TH1I*                   prot_per_event_pos;     // 37
+	TH1I*                   prot_per_event_neg;	// 38
 	TH2F*                   m2_pos_cut_T;           // 39
+	TH2F*                   m2_pos_cut_A;           // 40
+	TH2F*                   m2_pos_cut_B;           // 41
 	TH2F*                   m2_neg_cut_T;           // 42
-	TH2F*                   dphi_ket_hadr_T;        // 45
+	TH2F*                   m2_neg_cut_A;           // 43
+	TH2F*                   m2_neg_cut_B;           // 44
+	TH2F*                   dphi_ket_prot_T;        // 45
+	TH2F*                   dphi_ket_prot_A;        // 46
+	TH2F*                   dphi_ket_prot_B;        // 47
 	TProfile*               deltat_channel;         // 48
 	
         AliAnalysisTaskCorPIDTOFQA(const AliAnalysisTaskCorPIDTOFQA&);                        // not implemented
