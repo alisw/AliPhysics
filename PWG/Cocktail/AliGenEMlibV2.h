91e15ec7e0942639a5f389d60d66ce738ae99ab1
Lucas Altenkämper <lucas.altenkamper@cern.ch>
2017-06-12 15:29:13 +0200

    PWG/Cocktail: added more particles to cocktail classes

--- PWG/Cocktail/AliGenEMlibV2.h
+++ PWG/Cocktail/AliGenEMlibV2.h
@@ -33,9 +33,10 @@ public:
   
   enum Particle_t{kPizero=0, kEta=1, kRho0=2, kOmega=3, kEtaprime=4, kPhi=5, kJpsi=6,
     kSigma0=7, kK0s=8, kDeltaPlPl=9, kDeltaPl=10, kDeltaMi=11, kDeltaZero=12,
-    kRhoPl=13, kRhoMi=14, kK0star=15, kK0l=16, kLambda=17,
-    kDirectRealGamma=18, kDirectVirtGamma=19};
-  
+    kRhoPl=13, kRhoMi=14, kK0star=15, kK0l=16, kLambda=17, kKPl=18, kKMi=19,
+    kOmegaPl=20, kOmegaMi=21, kXiPl=22, kXiMi=23, kSigmaPl=24, kSigmaMi=25,
+    kDirectRealGamma=26, kDirectVirtGamma=27};
+
   enum CollisionSystem_t {kpp900GeV=0x000, kpp2760GeV=0x64, kpp7TeV=0xC8, kpPb=0x12C, kPbPb=0x190};
   
   enum Centrality_t{ kpp = 0x0, k0005=0x1, k0510=0x2, k1020=0x3, k2030=0x4, k3040=0x5, k4050=0x6, k5060=0x7,
@@ -88,8 +89,8 @@ public:
   static const Double_t fgkV2param[kCentralities][16];                     // parameters of pi v2
   static const Double_t fgkRawPtOfV2Param[kCentralities][10];              // parameters of the raw pt spectrum of v2 analysys
   static const Double_t fgkThermPtParam[kCentralities][2];                 // parameters of thermal gamma pt
-  static const Double_t fgkHM[18];                                         // particle masses
-  static const Double_t fgkMtFactor[3][18];                                // mt scaling factor
+  static const Double_t fgkHM[26];                                         // particle masses
+  static const Double_t fgkMtFactor[3][26];                                // mt scaling factor
 
   // direct gamma
   static Double_t PtPromptRealGamma(const Double_t *px, const Double_t *dummy);
@@ -150,78 +151,126 @@ public:
   static Double_t V2Jpsi(const Double_t *py, const Double_t *dummy);
   
   // Sigma
-  static Int_t    IpSigma(TRandom *ran);
-  static Double_t PtSigma( const Double_t *px, const Double_t *dummy );
-  static Double_t YSigma(const Double_t *py, const Double_t *dummy);
-  static Double_t V2Sigma0( const Double_t *px, const Double_t *dummy );
+  static Int_t    IpSigma0(TRandom *ran);
+  static Double_t PtSigma0(const Double_t *px, const Double_t *dummy);
+  static Double_t YSigma0(const Double_t *py, const Double_t *dummy);
+  static Double_t V2Sigma0(const Double_t *px, const Double_t *dummy);
   
   // K0short
   static Int_t    IpK0short(TRandom *ran);
-  static Double_t PtK0short( const Double_t *px, const Double_t *dummy );
+  static Double_t PtK0short(const Double_t *px, const Double_t *dummy);
   static Double_t YK0short(const Double_t *py, const Double_t *dummy);
-  static Double_t V2K0short( const Double_t *px, const Double_t *dummy );
+  static Double_t V2K0short(const Double_t *px, const Double_t *dummy);
 
   // K0long
   static Int_t    IpK0long(TRandom *ran);
-  static Double_t PtK0long( const Double_t *px, const Double_t *dummy );
+  static Double_t PtK0long(const Double_t *px, const Double_t *dummy);
   static Double_t YK0long(const Double_t *py, const Double_t *dummy);
-  static Double_t V2K0long( const Double_t *px, const Double_t *dummy );
+  static Double_t V2K0long(const Double_t *px, const Double_t *dummy);
 
   // Lambda
   static Int_t    IpLambda(TRandom *ran);
-  static Double_t PtLambda( const Double_t *px, const Double_t *dummy );
+  static Double_t PtLambda(const Double_t *px, const Double_t *dummy);
   static Double_t YLambda(const Double_t *py, const Double_t *dummy);
-  static Double_t V2Lambda( const Double_t *px, const Double_t *dummy );
+  static Double_t V2Lambda(const Double_t *px, const Double_t *dummy);
   
   // Delta++
   static Int_t    IpDeltaPlPl(TRandom *ran);
-  static Double_t PtDeltaPlPl( const Double_t *px, const Double_t *dummy );
+  static Double_t PtDeltaPlPl(const Double_t *px, const Double_t *dummy);
   static Double_t YDeltaPlPl(const Double_t *py, const Double_t *dummy);
-  static Double_t V2DeltaPlPl( const Double_t *px, const Double_t *dummy );
+  static Double_t V2DeltaPlPl(const Double_t *px, const Double_t *dummy);
   
   // Delta+
   static Int_t    IpDeltaPl(TRandom *ran);
-  static Double_t PtDeltaPl( const Double_t *px, const Double_t *dummy );
+  static Double_t PtDeltaPl(const Double_t *px, const Double_t *dummy);
   static Double_t YDeltaPl(const Double_t *py, const Double_t *dummy);
-  static Double_t V2DeltaPl( const Double_t *px, const Double_t *dummy );
+  static Double_t V2DeltaPl(const Double_t *px, const Double_t *dummy);
   
   // Delta-
   static Int_t    IpDeltaMi(TRandom *ran);
-  static Double_t PtDeltaMi( const Double_t *px, const Double_t *dummy );
+  static Double_t PtDeltaMi(const Double_t *px, const Double_t *dummy);
   static Double_t YDeltaMi(const Double_t *py, const Double_t *dummy);
-  static Double_t V2DeltaMi( const Double_t *px, const Double_t *dummy );
+  static Double_t V2DeltaMi(const Double_t *px, const Double_t *dummy);
   
   // Delta0
   static Int_t    IpDeltaZero(TRandom *ran);
-  static Double_t PtDeltaZero( const Double_t *px, const Double_t *dummy );
+  static Double_t PtDeltaZero(const Double_t *px, const Double_t *dummy);
   static Double_t YDeltaZero(const Double_t *py, const Double_t *dummy);
-  static Double_t V2DeltaZero( const Double_t *px, const Double_t *dummy );
+  static Double_t V2DeltaZero(const Double_t *px, const Double_t *dummy);
   
   // Rho+
   static Int_t    IpRhoPl(TRandom *ran);
-  static Double_t PtRhoPl( const Double_t *px, const Double_t *dummy );
+  static Double_t PtRhoPl(const Double_t *px, const Double_t *dummy);
   static Double_t YRhoPl(const Double_t *py, const Double_t *dummy);
-  static Double_t V2RhoPl( const Double_t *px, const Double_t *dummy );
+  static Double_t V2RhoPl(const Double_t *px, const Double_t *dummy);
   
   // Rho-
   static Int_t    IpRhoMi(TRandom *ran);
-  static Double_t PtRhoMi( const Double_t *px, const Double_t *dummy );
+  static Double_t PtRhoMi(const Double_t *px, const Double_t *dummy);
   static Double_t YRhoMi(const Double_t *py, const Double_t *dummy);
-  static Double_t V2RhoMi( const Double_t *px, const Double_t *dummy );
+  static Double_t V2RhoMi(const Double_t *px, const Double_t *dummy);
   
   // K0*
   static Int_t    IpK0star(TRandom *ran);
-  static Double_t PtK0star( const Double_t *px, const Double_t *dummy );
+  static Double_t PtK0star(const Double_t *px, const Double_t *dummy);
   static Double_t YK0star(const Double_t *py, const Double_t *dummy);
-  static Double_t V2K0star( const Double_t *px, const Double_t *dummy );
+  static Double_t V2K0star(const Double_t *px, const Double_t *dummy);
+
+  // K+
+  static Int_t    IpKPl(TRandom *ran);
+  static Double_t PtKPl(const Double_t *px, const Double_t *dummy);
+  static Double_t YKPl(const Double_t *py, const Double_t *dummy);
+  static Double_t V2KPl(const Double_t *px, const Double_t *dummy);
+
+  // K-
+  static Int_t    IpKMi(TRandom *ran);
+  static Double_t PtKMi(const Double_t *px, const Double_t *dummy);
+  static Double_t YKMi(const Double_t *py, const Double_t *dummy);
+  static Double_t V2KMi(const Double_t *px, const Double_t *dummy);
+
+  // Omega+
+  static Int_t    IpOmegaPl(TRandom *ran);
+  static Double_t PtOmegaPl(const Double_t *px, const Double_t *dummy);
+  static Double_t YOmegaPl(const Double_t *py, const Double_t *dummy);
+  static Double_t V2OmegaPl(const Double_t *px, const Double_t *dummy);
+
+  // Omega-
+  static Int_t    IpOmegaMi(TRandom *ran);
+  static Double_t PtOmegaMi(const Double_t *px, const Double_t *dummy);
+  static Double_t YOmegaMi(const Double_t *py, const Double_t *dummy);
+  static Double_t V2OmegaMi(const Double_t *px, const Double_t *dummy);
+
+  // Xi+
+  static Int_t    IpXiPl(TRandom *ran);
+  static Double_t PtXiPl(const Double_t *px, const Double_t *dummy);
+  static Double_t YXiPl(const Double_t *py, const Double_t *dummy);
+  static Double_t V2XiPl(const Double_t *px, const Double_t *dummy);
+
+  // Xi-
+  static Int_t    IpXiMi(TRandom *ran);
+  static Double_t PtXiMi(const Double_t *px, const Double_t *dummy);
+  static Double_t YXiMi(const Double_t *py, const Double_t *dummy);
+  static Double_t V2XiMi(const Double_t *px, const Double_t *dummy);
+
+  // Sigma+
+  static Int_t    IpSigmaPl(TRandom *ran);
+  static Double_t PtSigmaPl(const Double_t *px, const Double_t *dummy);
+  static Double_t YSigmaPl(const Double_t *py, const Double_t *dummy);
+  static Double_t V2SigmaPl(const Double_t *px, const Double_t *dummy);
+
+  // Sigma-
+  static Int_t    IpSigmaMi(TRandom *ran);
+  static Double_t PtSigmaMi(const Double_t *px, const Double_t *dummy);
+  static Double_t YSigmaMi(const Double_t *py, const Double_t *dummy);
+  static Double_t V2SigmaMi(const Double_t *px, const Double_t *dummy);
 
 private:
-  static TF1*     fPtParametrization[18];     // pt paramtrizations
+  static TF1*     fPtParametrization[26];     // pt paramtrizations
   static TF1*     fPtParametrizationProton;   // pt paramtrization
   static TH1D*    fMtFactorHisto;             // mt scaling factors
-  static TH2F*    fPtYDistribution[18];       // pt-y distributions
+  static TH2F*    fPtYDistribution[26];       // pt-y distributions
 
-  ClassDef(AliGenEMlibV2,6);
+  ClassDef(AliGenEMlibV2,7);
 };
 
 #endif
