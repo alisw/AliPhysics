a65c53007c4dc46a23bbe1bc271c4a94a5cc96d0
Salvatore Aiola <salvatore.aiola@cern.ch>
2017-06-16 23:20:36 +0200

    More flexible selection of the centrality estimation: no centrality, old framework, new framework. The centrality estimation is now decoupled from the beam type (previously centrality/multiplicity precentiles were not considered for the pp case)

--- PWG/EMCAL/EMCALbase/AliAnalysisTaskEmcalLight.h
+++ PWG/EMCAL/EMCALbase/AliAnalysisTaskEmcalLight.h
@@ -90,6 +90,16 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
     kpA       = 2  //!< Proton-Nucleus
   };
 
+  /**
+   * @enum ECentralityEstimation_t
+   * @brief Switch for the centrality estimation
+   */
+  enum ECentralityEstimation_t {
+    kNoCentrality  = 0, //!< No centrality estimation
+    kNewCentrality = 1, //!< New centrality estimation (AliMultSelection, see https://twiki.cern.ch/twiki/bin/viewauth/ALICE/AliMultSelectionCalibStatus for calibration status period-by-period)
+    kOldCentrality = 2  //!< Old centrality estimation (AliCentrality, works only on Run-1 PbPb and pPb)
+  };
+
   AliAnalysisTaskEmcalLight();
   AliAnalysisTaskEmcalLight(const char *name, Bool_t histo=kFALSE);
   virtual ~AliAnalysisTaskEmcalLight();
@@ -115,7 +125,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   void                        SetForceBeamType(EBeamType_t f)                       { fForceBeamType     = f                              ; }
 
   // Task configuration
-  void                        SetUseNewCentralityEstimation(Bool_t b)               { fUseNewCentralityEstimation = b                     ; }
+  void                        SetCentralityEstimation(ECentralityEstimation_t b)    { fCentralityEstimation = b                     ; }
   void                        SetMakeGeneralHistograms(Bool_t g)                    { fGeneralHistograms = g                              ; }
   void                        SetNeedEmcalGeom(Bool_t n)                            { fNeedEmcalGeom     = n                              ; }
   void                        SetCentBins(const std::vector<double>& bins)          { fCentBins = std::vector<double>(bins)               ; }
@@ -196,7 +206,7 @@ class AliAnalysisTaskEmcalLight : public AliAnalysisTaskSE {
   Bool_t                      fCreateHisto;                ///< whether or not create histograms
   Bool_t                      fNeedEmcalGeom;              ///< whether or not the task needs the emcal geometry
   std::vector<double>         fCentBins;                   ///< how many centrality bins
-  Bool_t                      fUseNewCentralityEstimation; ///< Use new centrality estimation (for 2015 data)
+  ECentralityEstimation_t     fCentralityEstimation;       ///< Centrality estimation
 
   // Input data
   Bool_t                      fIsPythia;                   ///< if it is a PYTHIA production
