3ef415ba7004d161314b7a99f94a9d720a717237
jgronefe <j.gronefeld@cern.ch>
2017-06-21 15:46:50 +0200

    dNdPt: Add CL1 and CL0 as Mult selectors

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTask_dNdPtPbPb_TPCITS.C
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTask_dNdPtPbPb_TPCITS.C
@@ -53,6 +53,8 @@ void AddTask_dNdPtPbPb_TPCITS(Int_t cutMode =222 , char *controlString ="default
   fdNdPtAnalysisPbPb->SetAnalysisMode(AlidNdPtHelper::kTPCITS);
   if(stControlString.Contains("V0Mminus05")) {fdNdPtAnalysisPbPb->SetCentralityEstimator("V0Mminus05");}
   else if(stControlString.Contains("V0Mplus05")) {fdNdPtAnalysisPbPb->SetCentralityEstimator("V0Mplus05");}
+  else if(stControlString.Contains("CL1")) {fdNdPtAnalysisPbPb->SetCentralityEstimator("CL1");}
+  else if(stControlString.Contains("CL0")) {fdNdPtAnalysisPbPb->SetCentralityEstimator("CL0");}
   else {fdNdPtAnalysisPbPb->SetCentralityEstimator("V0M");}
   if(stEventTrigger.Contains("kINT7")) fdNdPtAnalysisPbPb->SetTriggerMask(AliVEvent::kINT7);
   else if(stEventTrigger.Contains("kMB")) fdNdPtAnalysisPbPb->SetTriggerMask(AliVEvent::kMB);
