67cc726005d10a9735a8a72e588c27b443ed2876
Francesco Prino <prino@to.infn.it>
2017-06-26 22:38:14 +0200

    Fix typo in histo name

--- PWGHF/vertexingHF/AliHFSystErr.cxx
+++ PWGHF/vertexingHF/AliHFSystErr.cxx
@@ -2079,13 +2079,13 @@ void AliHFSystErr::InitDstoKKpi2016pPb0100(){
 
   // PID efficiency
   fPIDEff = new TH1F("fPIDEff","fPIDEff",24,0,24);
-  for(Int_t i=1;  i<=2;  i++) fCutsEff->SetBinContent(i, 1.00); // [0-2]
-  for(Int_t i=3;  i<=4;  i++) fCutsEff->SetBinContent(i, 0.00); // [2-4] - StrongPID
-  for(Int_t i=5;  i<=6;  i++) fCutsEff->SetBinContent(i, 0.00); // [4-6] - StrongPID
-  for(Int_t i=7;  i<=8;  i++) fCutsEff->SetBinContent(i, 0.00); // [6-8] - StrongPID
-  for(Int_t i=9;  i<=12; i++) fCutsEff->SetBinContent(i, 0.00); // [8-12] - StrongPID
-  for(Int_t i=13; i<=16; i++) fCutsEff->SetBinContent(i, 0.09); // [12-16] - ConservativePID
-  for(Int_t i=17; i<=24; i++) fCutsEff->SetBinContent(i, 0.09); // [16-24] - ConservativePID
+  for(Int_t i=1;  i<=2;  i++) fPIDEff->SetBinContent(i, 1.00); // [0-2]
+  for(Int_t i=3;  i<=4;  i++) fPIDEff->SetBinContent(i, 0.00); // [2-4] - StrongPID
+  for(Int_t i=5;  i<=6;  i++) fPIDEff->SetBinContent(i, 0.00); // [4-6] - StrongPID
+  for(Int_t i=7;  i<=8;  i++) fPIDEff->SetBinContent(i, 0.00); // [6-8] - StrongPID
+  for(Int_t i=9;  i<=12; i++) fPIDEff->SetBinContent(i, 0.00); // [8-12] - StrongPID
+  for(Int_t i=13; i<=16; i++) fPIDEff->SetBinContent(i, 0.09); // [12-16] - ConservativePID
+  for(Int_t i=17; i<=24; i++) fPIDEff->SetBinContent(i, 0.09); // [16-24] - ConservativePID
 
   // MC dN/dpt
   fMCPtShape = new TH1F("fMCPtShape","fMCPtShape",24,0,24);
