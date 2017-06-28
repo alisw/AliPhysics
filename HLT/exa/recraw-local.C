6e6285e461a83bd6a4b57a15c53e4f7c1103a81c
drohr <drohr@cern.ch>
2017-06-28 13:37:25 +0200

    Some changes to OCDB setting: in case a snapshot is provided, do not override the GRP with the local copy but use the one from the snapshot. Do not use the local dir when OCDB path is empty, but stick to the default

--- HLT/exa/recraw-local.C
+++ HLT/exa/recraw-local.C
@@ -63,7 +63,8 @@ void recraw_local(const char *filename,
     const char *modules="ALL",
     const char *hltOptions="loglevel=0x7c",
     const char *cdbDrain=NULL,
-    const char* cdbSnapshot=NULL)
+    const char* cdbSnapshot=NULL,
+    const char* qaOptionsOverride=NULL)
 {
   if(!gSystem->AccessPathName("galice.root")){
     cerr << "AliReconstruction on raw data requires to delete galice.root, or run at different place." << endl;
@@ -81,7 +82,7 @@ void recraw_local(const char *filename,
 
   // Set the CDB storage location
   AliCDBManager * man = AliCDBManager::Instance();
-  man->SetDefaultStorage(cdbURI);
+  if (struri.CompareTo("") == 0) man->SetDefaultStorage(cdbURI);
   
   //if found, configure any specific storages
   //same logic as in CPassX and benchmark
@@ -89,7 +90,7 @@ void recraw_local(const char *filename,
     localOCDBaccessConfig();
   }  
 
-  if (struri.BeginsWith("local://")) {
+  if (cdbSnapshot == NULL && struri.BeginsWith("local://")) {
     // set specific storage for GRP entry
     // search in the working directory and one level above, the latter
     // follows the standard simulation setup like e.g. in test/ppbench
@@ -124,6 +125,7 @@ void recraw_local(const char *filename,
   TString qaOptions="HLT TPC";
   if (!strModules.Contains("TPC")) qaOptions.ReplaceAll("TPC", "");
   qaOptions+=":ALL";
+  if (qaOptionsOverride) qaOptions = qaOptionsOverride;
   rec.SetRunQA(qaOptions) ;
   //rec.SetQARefDefaultStorage("local://$ALICE_ROOT/QAref") ;
 
