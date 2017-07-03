3af2f13089f0887578ce0c3fea9164986649ad65
Nur Hussain <nur.hussain@cern.ch>
2017-07-03 18:34:13 +0530

    Kinks: Codes for Pb-Pb analysis from Nur

---
+++ PWGLF/SPECTRA/Kinks/macros/AddTaskKinkPbPbMC.C
@@ -0,0 +1,41 @@
+AliAnalysisTaskKinkPbPbMC* AddTaskKinkPbPbMC(TString lCustomName="",Float_t lRadiusKUp=200.0, Float_t lRadiusKLow= 130.0, Int_t lNCluster=30, Float_t lLowQtValue=0.12, Float_t yRange=0.5)
+   {
+     //pp settings         
+      	AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+      	if (!mgr) 
+        {
+          ::Error("AddTaskKinkPbPbMC", "No analysis manager to connect to.");
+          return NULL;
+        }   
+     // Check the analysis type using the event handlers connected to the analysis manager.
+     //==============================================================================
+     	if (!mgr->GetInputEventHandler()) 
+       	{
+         ::Error("AddTaskKinkPbPbMC", "This task requires an input event handler");
+        	 return NULL;
+      	 }	   
+     
+     	TString type = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
+     	if(type.Contains("AOD"))
+       	{
+         ::Error("AddTaskKinkPbPbMC", "This task requires to run on ESD");
+      	   return NULL;
+       	}
+   
+    	AliAnalysisTaskKinkPbPbMC  *task = new AliAnalysisTaskKinkPbPbMC("AliAnalysisTaskKinkPbPbMC", lRadiusKUp, lRadiusKLow, lNCluster, lLowQtValue, yRange);
+   
+   	task->SetKinkRadius(lRadiusKLow, lRadiusKUp);
+	task->SetNCluster(lNCluster);
+	task->SetLowQtValue(lLowQtValue);
+	task->SetYRange(yRange);
+	mgr->AddTask(task);
+
+	TString outputFileName = AliAnalysisManager::GetCommonFileName();
+        outputFileName += ":PWGLFkinksMC";
+        outputFileName += "_PbPb";
+        AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("KinkPbPbMC", TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName );
+        mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+        mgr->ConnectOutput(task, 1, coutput1);
+        return task;   
+         
+   }
