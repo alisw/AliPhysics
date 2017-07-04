fea5c08e595ff546a4d24d2796633642ef8d42be
drohr <drohr@cern.ch>
2017-07-04 14:04:13 +0200

    add missing new parameter to init function

--- HLT/TPCLib/macros/makeTPCFastTransformOCDBObject.C
+++ HLT/TPCLib/macros/makeTPCFastTransformOCDBObject.C
@@ -95,7 +95,7 @@ void makeTPCFastTransformOCDBObject( const Char_t* cdbUri=NULL,
   
   TStopwatch timer;
   timer.Start();
-  int err = hltTransform.Init( bz, timestamp, 0 );
+  int err = hltTransform.Init( bz, timestamp, 0, 0 );
   timer.Stop();
   cout<<"\n\n Initialisation: "<<timer.CpuTime()<<" / "<<timer.RealTime()<<" sec.\n\n"<<endl;
   if( err!=0 ){
