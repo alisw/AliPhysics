727da2e0594158d25091bc77d9f8ec4b6053fd5f
Fabio Colamaria <fabiocolamaria@cern.ch>
2017-07-07 09:51:54 +0200

    Trivial modification of a comment in a D-h macro

--- PWGHF/correlationHF/macros/ExtractOutput_Dzero.C
+++ PWGHF/correlationHF/macros/ExtractOutput_Dzero.C
@@ -34,7 +34,7 @@ void ExtractOutput(
    Bool_t poolByPool=kTRUE, //kTRUE=pool-by-pool ME correction; kFALSE=merged-pools ME correction (set the options that you used in the online analysis)
    Double_t deltaEtaMin=-1., Double_t deltaEtaMax=1., //deltaEta ranges for correlation distributions  
    Bool_t use2Dmassplots=kFALSE, Double_t mincent=0., Double_t maxcent=100., //***NOTE: ONLY FOR OFFLINE APPROACH*** takes mass plots from 2D massVscent - ***use only if you did a centrality selection in CorrelateOffline.C, and put the same range!!***
-   Bool_t subtractSoftPiME=kTRUE) //***NOTE: ONLY FOR ONLINE APPROACH*** remove likely soft pions (via inv.mass cut) also in ME distributions (for OFFLINE, is done via CorrelateOffline.C)
+   Bool_t subtractSoftPiME=kTRUE) //Removes likely soft pions (via inv.mass cut) also in ME distributions (for OFFLINE, is done via CorrelateOffline.C)
 {
 
   //Create and set the correlation plotter class
