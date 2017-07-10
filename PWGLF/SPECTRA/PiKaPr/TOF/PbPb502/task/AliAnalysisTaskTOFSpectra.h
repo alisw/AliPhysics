4cb06eb6aeb63824e8c11427c34bf9b7f82adc8a
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2017-07-10 15:12:56 +0200

    Added ulterior Histogram for performance
    - Fixed messages in ObtainVertex
    - Fixed PID logic for MC histo

--- PWGLF/SPECTRA/PiKaPr/TOF/PbPb502/task/AliAnalysisTaskTOFSpectra.h
+++ PWGLF/SPECTRA/PiKaPr/TOF/PbPb502/task/AliAnalysisTaskTOFSpectra.h
@@ -734,7 +734,8 @@ private:
   TH2F* hPadDist;                               ///<  Histogram with the Impact Residual X and Residual Z values
   TH2F* hTOFDist;                               ///<  Histogram with the distributions of the TOF strips and sectors
   TH2I* hBeta;                                  ///<  Histogram with the track beta vs the track momentum
-  TH2I* hBetaExpected[kExpSpecies];             ///<  Histogram with the track beta vs the track momentum
+  TH2I* hBetaExpected[kExpSpecies];             ///<  Histogram with the track beta vs the track momentum obtained with the exoected time
+  TH2I* hBetaExpectedTOFPID[kExpSpecies];       ///<  Histogram with the track beta vs the track momentum obtained with the expected time but with the 3sigma TOF PID on the particle hypothesis
   TH2I* hBetaNoMismatch;                        ///<  Histogram with the track beta vs the track momentum with a cut on the maximum number of clusters to reduce the mismatch
   TH2I* hBetaNoMismatchEtaCut;                  ///<  Histogram with the track beta vs the track momentum with a cut on the maximum number of clusters to reduce the mismatch and a cut on the eta range
   TH2I* hBetaNoMismatchEtaCutOut;               ///<  Histogram with the track beta vs the track momentum with a cut on the maximum number of clusters to reduce the mismatch and a lower cut on the eta range
