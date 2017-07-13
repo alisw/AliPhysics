4743c6b8501a5d95579cf3556eb6ecd48a660e92
Brennan Schaefer <brennan.schaefer@cern.ch>
2017-07-13 12:51:40 +0200

    added dphi-deta, correlation radiuses, and multiplicity-centrality relation histos

--- PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFdeut.h
+++ PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFdeut.h
@@ -40,59 +40,67 @@ class AliAnalysisTaskCorPIDTOFdeut : public AliAnalysisTaskSE
 	
 	TH1F*                   fHistPt;                //  1
 	TH2F*                   cent_ntracks;           //  2
+	
 	TH2F*                   m2_pos;                 //  3
 	TH2F*                   m2_neg;                 //  4
 	TH2F*                   beta_p_pos;             //  5
 	TH2F*                   beta_p_neg;             //  6
 	TH2F*	                deltat_p_pos;           //  7
 	TH2F*	                deltat_p_neg;           //  8
-	TH2F*                   dedx_p_pos;             //  9
-	TH2F*                   dedx_p_neg;             // 10
-	TH2F*                   dedx_deltat_pos;        // 11
-	TH2F*                   dedx_deltat_neg;        // 12
-	TH3F*                   dedx_p_deltat_pos;      // 13
-	TH3F*                   dedx_p_deltat_neg;      // 14
-	TH2F*                   m2_pos_cut;             // 15
-	TH2F*                   m2_neg_cut;	        // 16
-	TH2F*                   beta_p_pos_cut;         // 17
-	TH2F*                   beta_p_neg_cut;         // 18
-	TH2F*	                deltat_p_pos_cut;       // 19
-	TH2F*	                deltat_p_neg_cut;       // 20
-	TH2F*                   dedx_p_pos_cut;         // 21
-	TH2F*                   dedx_p_neg_cut;         // 22
-	TH2F*                   dedx_deltat_pos_cut;    // 23
-	TH2F*                   dedx_deltat_neg_cut;    // 24
-	TH3F*                   dedx_p_deltat_pos_cut;  // 25
-	TH3F*                   dedx_p_deltat_neg_cut;  // 26
-	TH2F*                   deut_dphi_T;            // 27
-	TH2F*                   deut_dphi_pos_T;        // 28
-	TH2F*                   deut_dphi_neg_T;        // 29
-//	TH2F*                   deut_dphi_A;            // 30
-//	TH2F*                   deut_dphi_pos_A;        // 31
-//	TH2F*                   deut_dphi_neg_A;	// 32
-//	TH2F*                   deut_dphi_B;            // 33
-//	TH2F*                   deut_dphi_pos_B;        // 34
-//	TH2F*                   deut_dphi_neg_B;        // 35
-	TH1I*                   deut_per_event;         // 36
-	TH1I*                   deut_per_event_pos;     // 37
-	TH1I*                   deut_per_event_neg;	// 38
-	TH2F*                   m2_pos_cut_T;           // 39
-//	TH2F*                   m2_pos_cut_A;           // 40
-//	TH2F*                   m2_pos_cut_B;           // 41
-	TH2F*                   m2_neg_cut_T;           // 42
-//	TH2F*                   m2_neg_cut_A;           // 43
-//	TH2F*                   m2_neg_cut_B;           // 44
-	TH2F*                   dphi_et_deut_T;         // 45
-//	TH2F*                   dphi_et_deut_A;         // 46
-//	TH2F*                   dphi_et_deut_B;         // 47
-	TProfile*               deltat_channel;         // 48
+	TH2F*                   m2_pos_cut;             //  9
+	TH2F*                   m2_neg_cut;	        // 10
+	TH2F*                   beta_p_pos_cut;         // 11
+	TH2F*                   beta_p_neg_cut;         // 12
+	TH2F*	                deltat_p_pos_cut;       // 13
+	TH2F*	                deltat_p_neg_cut;       // 14
+	TH2F*                   m2_pos_cut_T;           // 15
+	TH2F*                   m2_neg_cut_T;           // 16
+	TH1I*                   deut_per_event;         // 17	
+	
+	TH2F*                   dphi_pt_deut_C;         // 18
+	TH2F*                   dphi_kt_deut_C;         // 19
+	TH2F*                   dphi_et_deut_C;         // 20
+	TH2F*                   dphi_en_deut_C;         // 21
 
-	TH1F*                   deut_et_count;          // 49
-	TH1F*                   deut_e_count;           // 50
-	TH2F*                   dphi_e_deut_T;          // 51
-	TH1F*                   deut_pt_count;          // 52
-	TH1F*                   asso_phi_hist;          // 53
-	TH1F*                   deut_phi_hist;          // 54
+	TH1F*                   deut_pt_count_C;        // 22
+	TH1F*                   deut_kt_count_C;        // 23
+	TH1F*                   deut_et_count_C;        // 24
+	TH1F*                   deut_en_count_C;        // 25
+
+	TH2F*                   dphi_pt_deut_P;         // 26
+	TH2F*                   dphi_kt_deut_P;         // 27
+	TH2F*                   dphi_et_deut_P;         // 28
+	TH2F*                   dphi_en_deut_P;         // 29
+
+	TH1F*                   deut_pt_count_P;        // 30
+	TH1F*                   deut_kt_count_P;        // 31
+	TH1F*                   deut_et_count_P;        // 32
+	TH1F*                   deut_en_count_P;        // 33
+	
+	TH1F*                   centrality_dist;        // 34
+	TH1F*                   multiplicity_dist;      // 35
+	TH1F*                   centrality_raw;         // 36
+	TH1F*                   multiplicity_raw;       // 37
+	
+	TH2F*                   deut_dphi_deta_p1_0510; // 38
+	TH2F*                   deut_dphi_deta_p1_1020; // 39
+	TH2F*                   deut_dphi_deta_p1_2030; // 40
+	TH2F*                   deut_dphi_deta_p1_3040; // 41
+	TH2F*                   deut_dphi_deta_p1_4050; // 42
+
+	TH2F*                   deut_dphi_deta_p2_0510; // 43
+	TH2F*                   deut_dphi_deta_p2_1020; // 44
+	TH2F*                   deut_dphi_deta_p2_2030; // 45
+	TH2F*                   deut_dphi_deta_p2_3040; // 46
+	TH2F*                   deut_dphi_deta_p2_4050; // 47
+	
+	TH2F*                   deut_dphi_deta_p3_0510; // 48
+	TH2F*                   deut_dphi_deta_p3_1020; // 49
+	TH2F*                   deut_dphi_deta_p3_2030; // 50
+	TH2F*                   deut_dphi_deta_p3_3040; // 51
+	TH2F*                   deut_dphi_deta_p3_4050; // 52
+	
+	TH2F*                   deut_cor_radius;        // 53
 
 	
         AliAnalysisTaskCorPIDTOFdeut(const AliAnalysisTaskCorPIDTOFdeut&);                        // not implemented
