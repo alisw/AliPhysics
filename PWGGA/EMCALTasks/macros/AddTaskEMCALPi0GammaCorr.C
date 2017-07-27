a6e649a515484c531bfea64cac9afa065bc99b6e
marratia <marratia@cern.ch>
2017-07-26 17:15:57 -0700

    fixing clust-track matching variables; adding option to run over MC (removing trigger requirement)

--- PWGGA/EMCALTasks/macros/AddTaskEMCALPi0GammaCorr.C
+++ PWGGA/EMCALTasks/macros/AddTaskEMCALPi0GammaCorr.C
@@ -4,7 +4,8 @@ AliAnalysisTaskEMCALPi0GammaCorr* AddTaskEMCALPi0GammaCorr(
   UInt_t      evtMixingType          = AliVEvent::kAnyINT,//..use only this type of events to fill your mixed event pool with tracks
   Double_t    trackptcut             = 0.15,              //..
   Double_t    clusptcut              = 0.15,              //..
-  Bool_t      SavePool               = 0,                 //..saves a mixed event pool to the output event
+  Bool_t      SavePool               = kFALSE,                 //..saves a mixed event pool to the output event
+  Bool_t      isMC                   = kFALSE, 
   const char *period                 = "lhc13d", 
   const char *trackName              = "usedefault",
   const char *clusName               = "usedefault",
@@ -24,7 +25,6 @@ AliAnalysisTaskEMCALPi0GammaCorr* AddTaskEMCALPi0GammaCorr(
   TString combinedName;
   combinedName.Form("%s_histos",taskname);
   TString contName(combinedName);
-
   //-------------------------------------------------------
   // Init the task and do settings
   //------------------------------------------------------
@@ -37,9 +37,7 @@ AliAnalysisTaskEMCALPi0GammaCorr* AddTaskEMCALPi0GammaCorr(
   trackCont->SetName("ForCorrelation");
   trackCont->SetFilterHybridTracks(kTRUE); //gives me Hyprid tracks
   const char* periodstr = "LHC13d";
-  //trackCont->SetTrackCutsPeriod(period);
-  //trackCont->SetDefTrackCutsPeriod(period);
-
+ 
   AliTrackContainer* trackContMatching = AnalysisTask->AddTrackContainer("Tracks");
   trackContMatching->SetName("ForMatching");
   trackContMatching->SetTrackFilterType(AliEmcalTrackSelection::kTPCOnlyTracks);  
@@ -47,15 +45,14 @@ AliAnalysisTaskEMCALPi0GammaCorr* AddTaskEMCALPi0GammaCorr(
   //-------------------------------------------------------
   // Add some selection criteria
   //-------------------------------------------------------
-  AnalysisTask->SetOffTrigger(evtTriggerType|evtMixingType); //..select only evets of type evtTriggerType and evtMixingType
+  if(!isMC) AnalysisTask->SetOffTrigger(evtTriggerType|evtMixingType); //..select only evets of type evtTriggerType and evtMixingType
   
     //..for Run1 pPb
   AnalysisTask->SetUseManualEvtCuts(kTRUE);
   AnalysisTask->SetUseAliAnaUtils(kTRUE); //this does automatically some vertex selection and pileup suppression
   AnalysisTask->SetVzRange(-10,10);
-  AnalysisTask->SetCentRange(0,100.0);
+  if(!isMC) AnalysisTask->SetCentRange(0,100.0);
   //..new task for run2
-  //AnalysisTask->SetNCentBins(5);
   AnalysisTask->SetUseNewCentralityEstimation(kFALSE); //maybe this is what is required
   
   AnalysisTask->GetTrackContainer("ForMatching")->SetParticlePtCut(trackptcut);
@@ -74,6 +71,9 @@ AliAnalysisTaskEMCALPi0GammaCorr* AddTaskEMCALPi0GammaCorr(
   AnalysisTask->SetSavePool(SavePool);
   AnalysisTask->SetEvtTriggerType(evtTriggerType);   
   AnalysisTask->SetEvtMixType(evtMixingType);       
+
+  //Whether or not running on MC: 
+  AnalysisTask->SetMC(isMC);
   
   mgr->AddTask(AnalysisTask);
   // Create containers for input/output
@@ -82,9 +82,6 @@ AliAnalysisTaskEMCALPi0GammaCorr* AddTaskEMCALPi0GammaCorr(
 	  	  	  	  	  	  	  	  	  	  	  	  	   AliAnalysisManager::kOutputContainer,
 		  	  	  	  	  	  	  	  	  	  	  	  	   Form("%s", AliAnalysisManager::GetCommonFileName()));
 
-  //AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("MyOuputContainer", TList::Class(), 
-   //                                                         AliAnalysisManager::kOutputContainer, 
-   //                                                         Form("%s", AliAnalysisManager::GetCommonFileName()));
   mgr->ConnectInput  (AnalysisTask, 0,  cinput1 );
   mgr->ConnectOutput (AnalysisTask, 1, coutput1 );
   //mgr->ConnectOutput (AnalysisTask, 2, coutput2 );
