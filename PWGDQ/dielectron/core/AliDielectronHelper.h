4437f76015fd42d7a6b77aef261dd8ed5f064d51
Pascal Dillenseger <pdillens@cern.ch>
2017-08-07 11:29:34 +0200

    PWGDQ added MatchEff TPC ITS using V0C as ep estimator

--- PWGDQ/dielectron/core/AliDielectronHelper.h
+++ PWGDQ/dielectron/core/AliDielectronHelper.h
@@ -30,7 +30,7 @@ TVectorD* MakeArbitraryBinning(const char* bins);
   void     GetMaxPtAndPhi(const AliVEvent *ev, Double_t &ptMax, Double_t &phiOfptMax);
   Int_t    GetNch(const AliMCEvent *ev=0x0, Double_t eta=0.9, Bool_t excludeJpsiDaughters = kFALSE);
   Int_t    GetNacc(const AliVEvent *ev=0x0);
-  Double_t GetITSTPCMatchEff(const AliVEvent *ev=0x0, Double_t *efficiencies = 0x0, Bool_t bEventPlane = kFALSE); // Bools select additional criteria for the tracks relative to the Eventplane angle of the TPC
+  Double_t GetITSTPCMatchEff(const AliVEvent *ev=0x0, Double_t *efficiencies = 0x0, Bool_t bEventPlane = kFALSE, Bool_t useV0Cep = kFALSE ); // Bools select additional criteria for the tracks relative to the Eventplane angle of the TPC
   Int_t    GetNaccTrcklts(const AliVEvent *ev=0x0, Double_t etaRange=1.6);
   Double_t GetNaccTrckltsCorrected(const AliVEvent *event, Double_t uncorrectedNacc, Double_t vtxZ, Int_t type);
 
