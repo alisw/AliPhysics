a52c619136e7e10cf7f0210afb02192d8586b9f7
Rihan Haque <mhaque@cern.ch>
2017-08-09 14:05:06 +0200

    remove obsolute class AliAnalysisTaskVnZDC

--- PWG/FLOW/Tasks/PWGflowTasksLinkDef.h
+++ PWG/FLOW/Tasks/PWGflowTasksLinkDef.h
@@ -42,7 +42,6 @@
 #pragma link C++ class AliAnalysisTaskPIDconfig+;
 #pragma link C++ class AliAnalysisTaskMultiparticleFemtoscopy+;
 #pragma link C++ class AliAnalysisTaskForStudents+;
-#pragma link C++ class AliAnalysisTaskVnZDC+;
 #pragma link C++ class AliAnalysisTaskZDCGainEq+;
 #pragma link C++ class AliAnalysisTaskZDCEP+;
 #pragma link C++ class AliAnalysisTaskPiKpK0Lamba+;
