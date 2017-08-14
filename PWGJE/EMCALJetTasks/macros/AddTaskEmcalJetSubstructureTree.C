c7072b856a72f480d9de44f01e292ca23909a37f
mfasel <markus.fasel@cern.ch>
2017-08-14 18:29:41 +0200

    Additional tree entries, settable recombination scheme
    - Adding energy and groomed jet mass (sim / rec) to the tree
    - Making recombination scheme settable
    - Adding few debug statements

--- PWGJE/EMCALJetTasks/macros/AddTaskEmcalJetSubstructureTree.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskEmcalJetSubstructureTree.C
@@ -1,3 +1,3 @@
-EmcalTriggerJets::AliAnalysisTaskEmcalJetSubstructureTree *AddTaskEmcalJetSubstructureTree(Bool_t isMC, Bool_t isData, Double_t jetradius, const char *trigger) {
-  return EmcalTriggerJets::AliAnalysisTaskEmcalJetSubstructureTree::AddEmcalJetSubstructureTreeMaker(isMC, isData, jetradius, trigger);
+EmcalTriggerJets::AliAnalysisTaskEmcalJetSubstructureTree *AddTaskEmcalJetSubstructureTree(Bool_t isMC, Bool_t isData, Double_t jetradius, AliJetContainer::ERecoScheme_t recoscheme, const char *trigger) {
+  return EmcalTriggerJets::AliAnalysisTaskEmcalJetSubstructureTree::AddEmcalJetSubstructureTreeMaker(isMC, isData, jetradius, recoscheme, trigger);
 }
