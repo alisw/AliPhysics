2427c64a63c71510cb72cbe1c9e2a406bbe888c2
panjinjin <jinjin.pan@hotmail.com>
2017-08-18 00:30:39 -0400

    Update AddTaskPIDBFDptDpt.C

--- PWGCF/Correlations/macros/PIDBFDptDpt/AddTaskPIDBFDptDpt.C
+++ PWGCF/Correlations/macros/PIDBFDptDpt/AddTaskPIDBFDptDpt.C
@@ -24,9 +24,6 @@ AliAnalysisTaskPIDBFDptDpt * AddTaskPIDBFDptDpt
  bool    pidparticle            =  1,   // 0: All Charged Particles;       1: PID particles
  bool    Use_PT_Cut             =  1,   // 0: Use_P_Cut ( TOF lower & higher boundary );       1: Use_PT_Cut
  int    useRapidity             =  1,   // 0: pseudo-rapadity      1: rapidity
- bool    useEventPlane          =  0,   // 0: No      1: Yes
- double  EventPlaneMin          =  -3.1415927/6,
- double  EventPlaneMax          =  3.1415927/6,
  int    CentralityGroup         =  15,   // Diff Cent Groups dealing w/ memory limit & weight file 100M Alien limit
  int    singlesOnly             =  1,   // 0: full correlations    1: singles only
  int    useWeights              =  0,   // 0: no                   1: yes  
@@ -75,7 +72,9 @@ AliAnalysisTaskPIDBFDptDpt * AddTaskPIDBFDptDpt
   Bool_t trigger                = kFALSE;
   Bool_t remove_Tracks_T0       = 1;
   bool   PurePIDinMC            = 0;   // 0: Contamination in MCAODreco;       1: No Contamination in MCAODreco
-   
+  bool    useEventPlane         = 0;   // 0: No      1: Yes
+  double  EventPlaneMin         = -3.1415927/6;
+  double  EventPlaneMax         = 3.1415927/6; 
 
   if      ( System == "PbPb" )                { centralityMethod = 4; trigger = kFALSE; }
   else if ( System == "PbPb_2015_kTRUE" )     { centralityMethod = 4; trigger = kTRUE;  }
