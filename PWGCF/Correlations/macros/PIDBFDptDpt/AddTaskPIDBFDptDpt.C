e625bcc10cf21ac2ed96747b2168da849d698952
panjinjin <jinjin.pan@hotmail.com>
2017-08-23 02:04:40 -0400

    Update AddTaskPIDBFDptDpt.C

--- PWGCF/Correlations/macros/PIDBFDptDpt/AddTaskPIDBFDptDpt.C
+++ PWGCF/Correlations/macros/PIDBFDptDpt/AddTaskPIDBFDptDpt.C
@@ -24,9 +24,6 @@ AliAnalysisTaskPIDBFDptDpt * AddTaskPIDBFDptDpt
  bool    pidparticle            =  1,   // 0: All Charged Particles;       1: PID particles
  bool    Use_PT_Cut             =  0,   // 0: Use_P_Cut ( TOF lower & higher boundary );       1: Use_PT_Cut
  int    useRapidity             =  1,   // 0: pseudo-rapadity      1: rapidity
- bool    useEventPlane          =  1,   // 0: No      1: Yes
- double  EventPlaneMin          =  -3.1415927/6,
- double  EventPlaneMax          =  3.1415927/6,
  int    CentralityGroup         =  9,   // Diff Cent Groups dealing w/ memory limit & weight file 100M Alien limit
  int    singlesOnly             =  1,   // 0: full correlations    1: singles only
  int    useWeights              =  0,   // 0: no                   1: yes  
@@ -47,6 +44,10 @@ AliAnalysisTaskPIDBFDptDpt * AddTaskPIDBFDptDpt
  double dcaXYMax                =  2.4,
  int nCentrality                =  4,
  int particleID                 =  1,   // Pion=0, Kaon=1, Proton=2
+ bool Use_AliHelperPID          =  1,  // 0: Not Use_AliHelperPID       1: Use_AliHelperPID
+ int pidType                    =  2,  // kNSigmaTPC,kNSigmaTOF, kNSigmaTPCTOF
+ Bool_t requestTOFPID           =  1,
+ Bool_t isMC                    =  0,
  double nSigmaCut               =  2.0,
  double ElectronVetoCut         =  1.0,
  double ptMin                   =  0.2, // pt range lower limit cut ( also for pt histos )
@@ -71,7 +72,10 @@ AliAnalysisTaskPIDBFDptDpt * AddTaskPIDBFDptDpt
   Bool_t trigger                = kFALSE;
   Bool_t remove_Tracks_T0       = 1;
   bool   PurePIDinMC            = 0;   // 0: Contamination in MCAODreco;       1: No Contamination in MCAODreco
-   
+  bool    useEventPlane         = 1;   // 0: No      1: Yes
+  double  EventPlaneMin         = -3.1415927/6;
+  double  EventPlaneMax         =  3.1415927/6;
+  
 
   if      ( System == "PbPb" )                { centralityMethod = 4; trigger = kFALSE; }
   else if ( System == "PbPb_2015_kTRUE" )     { centralityMethod = 4; trigger = kTRUE;  }
@@ -383,7 +387,17 @@ AliAnalysisTaskPIDBFDptDpt * AddTaskPIDBFDptDpt
       task->SetPtTOFlowerBoundary( ptTOFlowerMin );
       task->SetElectronNSigmaVetoCut( ElectronVetoCut );
       task->SetfRemoveTracksT0Fill( remove_Tracks_T0 );
-  
+      task->SetUse_AliHelperPID(  Use_AliHelperPID  );
+
+      // assign initial values to AliHelperPID object
+      AliHelperPID* helperpid = new AliHelperPID();
+      helperpid -> SetNSigmaCut( nSigmaCut );
+      helperpid -> SetPIDType( pidType );// kNSigmaTPC,kNSigmaTOF, kNSigmaTPCTOF
+      helperpid -> SetfRequestTOFPID( requestTOFPID );
+      helperpid -> SetfPtTOFPID( ptTOFlowerMin );
+      helperpid -> SetisMC( isMC );
+      task->SetHelperPID( helperpid );
+      
       if(trigger) task -> SelectCollisionCandidates(AliVEvent::kINT7); //pPb, PbPb_2015
       else task -> SelectCollisionCandidates(AliVEvent::kMB); // PbPb & pp
         
