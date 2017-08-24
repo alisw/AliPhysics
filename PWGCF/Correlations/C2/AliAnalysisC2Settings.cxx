af1a8d77f13b0efd726f537b8b07e1734189a140
cbourjau <christian.bourjau@cern.ch>
2017-08-24 11:17:15 +0200

    started using tracks not tracklets
    Also renamed spdtracklet to tracklet, since that is what it is

--- PWGCF/Correlations/C2/AliAnalysisC2Settings.cxx
+++ PWGCF/Correlations/C2/AliAnalysisC2Settings.cxx
@@ -25,7 +25,8 @@ AliAnalysisC2Settings::AliAnalysisC2Settings() :
   fUseFMD(false),
   fUseV0(false),
   fUseSPDclusters(false),
-  fUseSPDtracklets(false)
+  fUseTracklets(false),
+  fUseTracks(false)
 {
   Double_t _ptbins[] = {3.0, 4.0, 6.0, 8.0, 15.0};
   fPtBinEdges = edgeContainer(_ptbins, _ptbins + sizeof(_ptbins) / sizeof(_ptbins[0]));
