dc47486d2bc56da00703041b0c4bcd78c9869d7c
Pascal Dillenseger <pdillens@cern.ch>
2017-09-08 11:48:37 +0200

    PWGDQ EvtVsTrkHist small bug fix for V0C eventplane

--- PWGDQ/dielectron/core/AliDielectron.cxx
+++ PWGDQ/dielectron/core/AliDielectron.cxx
@@ -423,6 +423,8 @@ Bool_t AliDielectron::Process(AliVEvent *ev1, AliVEvent *ev2)
   // set event
   AliDielectronVarManager::SetFillMap(fUsedVars);
   AliDielectronVarManager::SetEvent(ev1);
+  if(fEvtVsTrkHist)  fEvtVsTrkHist->FillHistograms(ev1);
+
   if (fMixing){
     //set mixing bin to event data
     Int_t bin=fMixing->FindBin(AliDielectronVarManager::GetData());
@@ -743,7 +745,6 @@ void AliDielectron::FillHistograms(const AliVEvent *ev, Bool_t pairInfoOnly)
     if (fHistos->GetHistogramList()->FindObject("Event")) {
       fHistos->FillClass("Event", AliDielectronVarManager::kNMaxValues, AliDielectronVarManager::GetData());
     }
-    if(fEvtVsTrkHist)  fEvtVsTrkHist->FillHistograms(ev);
   }
 
   //Fill track information, separately for the track array candidates
