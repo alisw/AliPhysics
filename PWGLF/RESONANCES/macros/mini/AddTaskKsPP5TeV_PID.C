ed3fd0793d9fb4ce0537853e8ad4b4bf4efec739
JihyeSong <Jihye.Song@cern.ch>
2017-09-14 14:03:59 +0900

    Modified to use the AliMultSelection framework for KStar analysis in pp at 5 TeV

--- PWGLF/RESONANCES/macros/mini/AddTaskKsPP5TeV_PID.C
+++ PWGLF/RESONANCES/macros/mini/AddTaskKsPP5TeV_PID.C
@@ -60,6 +60,7 @@ AliRsnMiniAnalysisTask * AddTaskKsPP5TeV_PID
   // if(isMC && (evtCutSetID==eventCutSet::kNoEvtSel || evtCutSetID==eventCutSet::kSpecial3)) triggerMask=AliVEvent::kAny;
   Bool_t      rejectPileUp = kTRUE; //
   Double_t    vtxZcut = 10.0; //cm, default cut on vtx z
+  Int_t MultBins=aodFilterBit/100;
   
   if (evtCutSetID==eventCutSet::kDefaultVtx12){vtxZcut = 12.0;} //cm
 
@@ -70,6 +71,8 @@ AliRsnMiniAnalysisTask * AddTaskKsPP5TeV_PID
   if (evtCutSetID==eventCutSet::kNoPileUpCut){rejectPileUp=kFALSE;}//cm
   
   if(evtCutSetID==eventCutSet::kSpecial2) vtxZcut=1.e6;//off
+
+   if(!isPP || isMC || MultBins) rejectPileUp=kFALSE;
 
   //-------------------------------------------
   //pair cuts
@@ -114,17 +117,20 @@ AliRsnMiniAnalysisTask * AddTaskKsPP5TeV_PID
    if(evtCutSetID!=eventCutSet::kNoEvtSel && evtCutSetID!=eventCutSet::kSpecial3) task->SelectCollisionCandidates(triggerMask); //AOD
 
    
-   if (isPP) 
-     task->UseMultiplicity("QUALITY");
-   else
-     task->UseCentrality("V0M");   
+    if(isPP){
+    if(MultBins==1) task->UseMultiplicity("AliMultSelection_V0M");
+    else if(MultBins==2) task->UseMultiplicity("AliMultSelection_RefMult08");
+    else task->UseMultiplicity("QUALITY");
+    }else task->UseCentrality("V0M");
+
+    
    // set event mixing options
    task->UseContinuousMix();
    //task->UseBinnedMix();
    task->SetNMix(nmix);
    task->SetMaxDiffVz(maxDiffVzMix);
    task->SetMaxDiffMult(maxDiffMultMix);
-   ::Info("AddAnalysisTaskTOFKStar", Form("Event mixing configuration: \n events to mix = %i \n max diff. vtxZ = cm %5.3f \n max diff multi = %5.3f", nmix, maxDiffVzMix, maxDiffMultMix));
+   ::Info("AddTaskKsPP5TeV_PID", Form("Event mixing configuration: \n events to mix = %i \n max diff. vtxZ = cm %5.3f \n max diff multi = %5.3f", nmix, maxDiffVzMix, maxDiffMultMix));
    
    mgr->AddTask(task);
    
@@ -133,62 +139,82 @@ AliRsnMiniAnalysisTask * AddTaskKsPP5TeV_PID
    // - 2nd argument --> |Vz| range
    // - 3rd argument --> minimum required number of contributors to vtx
    // - 4th argument --> tells if TPC stand-alone vertexes must be accepted
-   AliRsnCutPrimaryVertex *cutVertex=0;
-    if (evtCutSetID!=eventCutSet::kSpecial1 && evtCutSetID!=eventCutSet::kNoEvtSel){
-    cutVertex = new AliRsnCutPrimaryVertex("cutVertex", vtxZcut, 0, kFALSE);
-    if (evtCutSetID==eventCutSet::kSpecial3) cutVertex->SetCheckGeneratedVertexZ();
 
-     }//vertex loop
+   AliRsnCutPrimaryVertex* cutVertex=0;
+  if(evtCutSetID!=eventCutSet::kSpecial1 && evtCutSetID!=eventCutSet::kNoEvtSel &&  (!MultBins || fabs(vtxZcut-10.)>1.e-10)){
+    cutVertex=new AliRsnCutPrimaryVertex("cutVertex",vtxZcut,0,kFALSE);
+    if(!MultBins && evtCutSetID!=eventCutSet::kSpecial3){
+    cutVertex->SetCheckZResolutionSPD();
+    cutVertex->SetCheckDispersionSPD();
+    cutVertex->SetCheckZDifferenceSPDTrack();
+    }
+   if (evtCutSetID==eventCutSet::kSpecial3) cutVertex->SetCheckGeneratedVertexZ();
+  }
 
-
-   
-   if (isPP && (!isMC)&&cutVertex) { 
-     cutVertex->SetCheckPileUp(rejectPileUp);   // set the check for pileup  
-     ::Info("AddAnalysisTaskTOFKStar", Form(":::::::::::::::::: Pile-up rejection mode: %s", (rejectPileUp)?"ON":"OFF"));
-     cutVertex->SetCheckZResolutionSPD();//A Khuntia
-     cutVertex->SetCheckDispersionSPD();//A Khuntia
-     cutVertex->SetCheckZDifferenceSPDTrack();//A Khuntia
-   }
-  
 
   
    ///////----------AKhuntia----------//////
    AliRsnCutEventUtils* cutEventUtils=0;
-   cutEventUtils=new AliRsnCutEventUtils("cutEventUtils",kTRUE,rejectPileUp);
-   cutEventUtils->SetCheckIncompleteDAQ();
-   cutEventUtils->SetCheckSPDClusterVsTrackletBG();
+  if(evtCutSetID!=eventCutSet::kNoEvtSel && evtCutSetID!=eventCutSet::kSpecial3){
+    cutEventUtils=new AliRsnCutEventUtils("cutEventUtils",kTRUE,rejectPileUp);
+    if(!MultBins){
+      cutEventUtils->SetCheckIncompleteDAQ();
+      cutEventUtils->SetCheckSPDClusterVsTrackletBG();
+    }else{
+      //cutEventUtils->SetCheckInelGt0SPDtracklets();
+      cutEventUtils->SetRemovePileUppA2013(kFALSE);
+      cutEventUtils->SetCheckAcceptedMultSelection();
+    }
+  }
+
+
+ if(isPP && (!isMC) && cutVertex){ 
+    cutVertex->SetCheckPileUp(rejectPileUp);// set the check for pileup  
+    ::Info("AddTaskKsPP5TeV_PID", Form(":::::::::::::::::: Pile-up rejection mode: %s", (rejectPileUp)?"ON":"OFF"));
+  }
+
+  
    //------------------------------------
    // define and fill cut set for event cut
+  AliRsnCutSet* eventCuts=0;
+  if(cutEventUtils || cutVertex){
+    eventCuts=new AliRsnCutSet("eventCuts",AliRsnTarget::kEvent);
 
-   AliRsnCutSet* eventCuts=0;
-   if(cutVertex){
-   eventCuts = new AliRsnCutSet("eventCuts", AliRsnTarget::kEvent);
-   eventCuts->AddCut(cutVertex);
-   eventCuts->SetCutScheme(Form("%s", cutVertex->GetName()));
-   task->SetEventCuts(eventCuts);
-   }
-   //
+    if(cutEventUtils && cutVertex){
+      eventCuts->AddCut(cutEventUtils);
+      eventCuts->AddCut(cutVertex);
+      eventCuts->SetCutScheme(Form("%s&%s",cutEventUtils->GetName(),cutVertex->GetName()));
+    }else if(cutEventUtils && !cutVertex){
+      eventCuts->AddCut(cutEventUtils);
+      eventCuts->SetCutScheme(Form("%s",cutEventUtils->GetName()));
+    }else if(!cutEventUtils && cutVertex){
+      eventCuts->AddCut(cutVertex);
+      eventCuts->SetCutScheme(Form("%s",cutVertex->GetName()));
+    }
+
+    task->SetEventCuts(eventCuts);
+  }
+
+
    // -- EVENT-ONLY COMPUTATIONS -------------------------------------------------------------------
    //   
    //vertex
-   Int_t vtxID = task->CreateValue(AliRsnMiniValue::kVz, kFALSE);
-   AliRsnMiniOutput *outVtx = task->CreateOutput("eventVtx", "HIST", "EVENT");
-   outVtx->AddAxis(vtxID, 240, -12.0, 12.0);
-   
-   //multiplicity or centrality
-   Int_t multID = task->CreateValue(AliRsnMiniValue::kMult, kFALSE);
-   AliRsnMiniOutput *outMult = task->CreateOutput("eventMult", "HIST", "EVENT");
-   if (isPP) 
-     outMult->AddAxis(multID, 400, 0.0, 400.0);
-   else
-     outMult->AddAxis(multID, 100, 0.0, 100.0);
-   
-   TH2F* hvz=new TH2F("hVzVsCent","", 100, 0., 100., 240, -12.0, 12.0);
-   task->SetEventQAHist("vz",hvz);//plugs this histogram into the fHAEventVz data member
+  Int_t vtxID=task->CreateValue(AliRsnMiniValue::kVz,kFALSE);
+  AliRsnMiniOutput* outVtx=task->CreateOutput("eventVtx","HIST","EVENT");
+  outVtx->AddAxis(vtxID,240,-12.0,12.0);
 
-   TH2F* hmc=new TH2F("MultiVsCent","", 100, 0., 100., 400, 0., 400.);
-   hmc->GetYaxis()->SetTitle("QUALITY");
-   task->SetEventQAHist("multicent",hmc);//plugs this histogram into the fHAEventMultiCent data member
+  //multiplicity or centrality
+  Int_t multID=task->CreateValue(AliRsnMiniValue::kMult,kFALSE);
+  AliRsnMiniOutput* outMult=task->CreateOutput("eventMult","HIST","EVENT");
+  if(isPP && !MultBins) outMult->AddAxis(multID,400,0.5,400.5);
+  else outMult->AddAxis(multID,110,0.,110.);
+
+  TH2F* hvz=new TH2F("hVzVsCent","",110,0.,110., 240,-12.0,12.0);
+  task->SetEventQAHist("vz",hvz);//plugs this histogram into the fHAEventVz data member
+
+  TH2F* hmc=new TH2F("MultiVsCent","", 110,0.,110., 400,0.5,400.5);
+  hmc->GetYaxis()->SetTitle("QUALITY");
+  task->SetEventQAHist("multicent",hmc);//plugs this histogram into the fHAEventMultiCent data member
 
    //
    // -- PAIR CUTS (common to all resonances) ------------------------------------------------------
