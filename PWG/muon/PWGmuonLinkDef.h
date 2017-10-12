f857d747084c9c79b6bce8cd5856a2473f56e413
dstocco <diego.stocco@cern.ch>
2017-10-12 10:14:43 +0200

    Add back classes wrongly removed by 418741b11f6c0d6a0eedd87225677eb11876a5d2

--- PWG/muon/PWGmuonLinkDef.h
+++ PWG/muon/PWGmuonLinkDef.h
@@ -43,6 +43,7 @@
 #pragma link C++ class AliAnalysisTaskFlowSingleMu+;
 #pragma link C++ class AliAnalysisMuonUtility+;
 #pragma link C++ class AliUtilityMuonAncestor+;
+#pragma link C++ class AliUtilityDimuonSource+;
 #pragma link C++ class AliOADBMuonTrackCutsParam+;
 #pragma link C++ class AliAnalysisTaskAOD2MuonAOD+;
 #pragma link C++ class AliAnalysisNonMuonTrackCuts+;
@@ -60,6 +61,7 @@
 #pragma link C++ class AliAnalysisMuMuCutCombination+;
 #pragma link C++ class AliAnalysisMuMuEventCutter+;
 #pragma link C++ class AliAnalysisMuMuSingle+;
+#pragma link C++ class AliAnalysisMuMuTriggerResponse+;
 #pragma link C++ class AliAnalysisMuMuMinv+;
 #pragma link C++ class AliAnalysisMuMuFlowEP+;
 #pragma link C++ class AliAnalysisMuMuFlowSP+;
@@ -69,6 +71,7 @@
 
 #pragma link C++ class AliAnalysisTaskMTRResponse+;
 #pragma link C++ class AliMTRParameterizedResponse+;
+#pragma link C++ class AliAnalysisTaskWeightMTRResponse+;
 
 #pragma link C++ class AliAnalysisMuMuMCGene+;
 
@@ -83,4 +86,3 @@
 #pragma link C++ class AliMuonTrackSmearing+;
 #pragma link C++ class AliTaskMuonTrackSmearing+;
 #endif
-
