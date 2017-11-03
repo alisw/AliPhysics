f499335f7cc99ff687d6ec50674ae76a4770ea2a
Marian Ivanov <marian.ivanov@cern.ch>
2017-11-03 15:30:25 +0100

    ATO-418-Updated test macro

--- STAT/test/testDrawArrayHisto.C
+++ STAT/test/testDrawArrayHisto.C
@@ -1,13 +1,25 @@
 /*
-  wget http://aliqatrkeos.web.cern.ch/aliqatrkeos/performance/alice/data/2015/LHC15o/pass1/LHC15o.pass1.B1.Bin0/performanceHisto.root -o performanceHisto.root
-
-   AliDrawStyle::SetDefaults();
+  AliDrawStyle::SetDefaults();
   AliDrawStyle::ApplyStyle("figTemplate");
+  //
+  .L $AliRoot_SRC/STAT/test/testDrawArrayHisto.C
+  ReadInputData();
+  RegisterStyles();
+  makeP4Report();
 
 
-*/
+  AliDrawStyle::ApplyCssStyle(gPad,"bbStyle");
+  AliDrawStyle::ApplyCssStyle(gPad,"rawStyle");
+
+ */
 
 TObjArray * hisArray=NULL;
+
+void RegisterStyles(){
+  AliDrawStyle::SetCssStyle("rawStyle", AliDrawStyle::ReadCSSFile("$AliRoot_SRC/STAT/Macros/raw.css",0));
+  AliDrawStyle::SetCssStyle("bbStyle", AliDrawStyle::ReadCSSFile("$AliRoot_SRC/STAT/Macros/bb.css",0));
+}
+
 
 void ReadInputData() {
   // read cache file from server
@@ -37,17 +49,6 @@ void makeP4Report(){
   drawExpressionP4+="%Ogridx,gridy;hisDeltaP4_Allv_qPt_tgl(0,400,80,120,0,10)(0,1)(f-grms p);:";
   drawExpressionP4+="%Ogridx,gridy;hisPullP4_Allv_qPt_tgl(0,400,80,120,0,10)(0,1)(f-grms p);:";
   drawExpressionP4+="%Ogridx,gridy;hisDeltaP4_Allv_qPt_tgl(100,300,99,102,0,10)(0)(err);:";
-  drawExpressionP4+="%Ogridx,gridy;hisPullP4_Allv_qPt_tgl(100,300,99,102,0,10)(0)(err);:";
-  TPad * padP4 = AliTreePlayer::DrawHistograms(0,hisArray,drawExpressionP4,keepArray, 1+2+4+8);
-  padP4->SaveAs("deltaP4TPCtoFull.png");
-  padP4->SaveAs("deltaP4TPCtoFull.C");
-}
-
-/// Example P4 report
-void makeP4ReportHisto(){
-  TString drawExpressionP4="";
-  drawExpressionP4="[1,1]:";
-  drawExpressionP4+="%Ogridx,gridy;hisDeltaP4_Allv_qPt_tgl(100,300,0,40,102,0,10)(0)(err);hisDeltaP4_Allv_qPt_tgl(100,300,40,80,102,0,10)(0)(err);hisDeltaP4_Allv_qPt_tgl(100,300,80,10,102,0,10)(0)(err);:";
   drawExpressionP4+="%Ogridx,gridy;hisPullP4_Allv_qPt_tgl(100,300,99,102,0,10)(0)(err);:";
   TPad * padP4 = AliTreePlayer::DrawHistograms(0,hisArray,drawExpressionP4,keepArray, 1+2+4+8);
   padP4->SaveAs("deltaP4TPCtoFull.png");
