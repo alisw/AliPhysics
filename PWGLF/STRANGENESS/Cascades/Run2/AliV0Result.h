16e29bbdcae41305d5470106a75c6f6bc30d0740
David Dobrigkeit Chinellato <david.dobrigkeit.chinellato@cern.ch>
2017-11-14 23:48:17 -0200

    Added proton profile to strangeness histo framework

--- PWGLF/STRANGENESS/Cascades/Run2/AliV0Result.h
+++ PWGLF/STRANGENESS/Cascades/Run2/AliV0Result.h
@@ -148,9 +148,10 @@ public:
     TH3F* GetHistogram       ()       { return fHisto; }
     TH3F* GetHistogramToCopy () const { return fHisto; }
     
-    //Proton Profile - not implemented for V0s so far
-    TProfile* GetProtonProfile       ()       { return 0x0; }
-    TProfile* GetProtonProfileToCopy () const { return 0x0; }
+    //Proton Profile
+    TProfile *GetProtonProfile       ()       { return fProtonProfile; }
+    TProfile *GetProtonProfileToCopy () const { return fProtonProfile; }
+    void InitializeProtonProfile(Long_t lNPtBins, Double_t *lPtBins); //Initialize profile, otherwise not stored
 
     TH3F* GetHistogramFeeddown       ()       { return fHistoFeeddown; }
     TH3F* GetHistogramFeeddownToCopy () const { return fHistoFeeddown; }
@@ -207,7 +208,9 @@ private:
     TH3F *fHisto; //Histogram for storing output with these configurations
     TH3F *fHistoFeeddown; //Feeddown matrix (optional)
     
-    ClassDef(AliV0Result, 16)
+    TProfile *fProtonProfile; //Histogram for bookkeeping proton momenta (optional)
+    
+    ClassDef(AliV0Result, 17)
     // 1 - original implementation
     // 2 - first implementation of MC association (to be adjusted)
     // 3 - Variable binning constructor + re-order variables in main output for convenience
@@ -224,5 +227,6 @@ private:
     //14 - added possibility to select on-the-fly V0 candidates
     //15 - added proton profile (dummy as of now)
     //16 - added configurable AP cut
+    //17 - added real possibility of having proton profiles (re-analysis material)
 };
 #endif
