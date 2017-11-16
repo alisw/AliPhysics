dab05371ab48b5129ca150f886bef1d8e4a8b879
pfecchio <pfecchio@cern.ch>
2017-11-16 11:10:48 +0100

    improvements in TreeAnalysis

--- PWGLF/NUCLEX/Exotica/dStar/AliAnalysisTaskdStar.cxx
+++ PWGLF/NUCLEX/Exotica/dStar/AliAnalysisTaskdStar.cxx
@@ -1,6 +1,6 @@
 #include "AliAnalysisTaskdStar.h"
 
-// pdgcode dstar 900010020
+// dStar pdg code 900010020
 
 // ROOT includes
 #include <TChain.h>
@@ -36,28 +36,34 @@ ClassImp(AliAnalysisTaskdStar);
 AliAnalysisTaskdStar::AliAnalysisTaskdStar(const char* taskname) : AliAnalysisTaskSE(taskname),
 fEventCut(false),
 fList(),
+fRequireYmin(-0.5f),
+fRequireYmax(0.5f),
+fPID(),
 fProduction(),
 fReconstructed(),
 fTotal(),
+fMCDalitzPlot(),
 fTree(nullptr),
+fMCTree(nullptr),
 fDeuteronVector(),
 fPiVector(),
-fRequireYmin(-0.5f),
-fRequireYmax(0.5f),
-fPID()
+fMCDeuteronVector(),
+fMCPiVector()
 {
   fFilterBit = BIT(8);
   DefineInput(0, TChain::Class());
   DefineOutput(1, TList::Class());
   DefineOutput(2, TTree::Class());
+  DefineOutput(3, TTree::Class());
 }
 
 /// Standard destructor
 ///
 AliAnalysisTaskdStar::~AliAnalysisTaskdStar() {
-  if (fList) delete fList;
-  if (fPID) delete fPID;
-  if (fTree) delete fTree;
+  if (fList)   delete fList;
+  if (fPID)    delete fPID;
+  if (fTree)   delete fTree;
+  if (fMCTree) delete fMCTree;
 }
 
 /// This function creates all the histograms and all the objects in general used during the analysis
@@ -72,20 +78,22 @@ void AliAnalysisTaskdStar::UserCreateOutputObjects() {
 
   char   letter[2] = {'a','m'};
   string tpctofMC[3] = {"TPC","TPC_TOF","TPC_(TOF)"};
-  string dStarState = "dStar(2380)";
   float low_mass_limit = 2.2;
   float up_mass_limit = 2.7;
 
-  for (int iC = 0; iC < 2; ++iC){
-    fProduction[iC] = new TH2F(Form("fProduction_%s_%c", dStarState.data(),letter[iC]),";M (GeV/#it{c}^{2});#it{p}_{T} (GeV/#it{c});Counts",50,low_mass_limit,up_mass_limit,20,0,10);
+  for (int iC = 0; iC < 2; ++iC) {
+    fProduction[iC] = new TH2F(Form("fProduction_dStar(2380)_%c",letter[iC]),";M (GeV/#it{c}^{2});#it{p}_{T} (GeV/#it{c});Counts",50,low_mass_limit,up_mass_limit,20,0,10);
     fList->Add(fProduction[iC]);
-    fTotal[iC] = new TH2F(Form("fTotal_%s_%c",dStarState.data(),letter[iC]),";M (GeV/#it{c}^{2});#it{p}_{T} (GeV/#it{c});Counts",50,low_mass_limit,up_mass_limit,20,0,10);
+    fTotal[iC] = new TH2F(Form("fTotal_dStar(2380)_%c",letter[iC]),";M (GeV/#it{c}^{2});#it{p}_{T} (GeV/#it{c});Counts",50,low_mass_limit,up_mass_limit,20,0,10);
     fList->Add(fTotal[iC]);
-    for (int iT = 0; iT < 3; ++iT){
-      fReconstructed[iC][iT] = new TH2F(Form("fRec_%s_%c_ITS_%s",dStarState.data(),letter[iC],tpctofMC[iT].data()),";M (GeV/#it{c}^{2});#it{p}_{T} (GeV/#it{c});Counts",50,low_mass_limit,up_mass_limit,20,0,10);
+    for (int iT = 0; iT < 3; ++iT) {
+      fReconstructed[iC][iT] = new TH2F(Form("fRec_dStar(2380)_%c_ITS_%s",letter[iC],tpctofMC[iT].data()),";M (GeV/#it{c}^{2});#it{p}_{T} (GeV/#it{c});Counts",50,low_mass_limit,up_mass_limit,20,0,10);
       fList->Add(fReconstructed[iC][iT]);
     }
   }
+
+  fMCDalitzPlot = new TH2F("dalitzplotMC_dStar", "Dalitz Plot d*(2380) #rightarrow d #pi^{+} #pi^{-} ;#it{M_{inv}}  #pi^{+} #pi^{-}; #it{M_{inv}} d #pi^{-}", 190, 0.05, 1.0, 800, 4.0, 8.0);
+  fList->Add(fMCDalitzPlot);
 
   AliPDG::AddParticlesToPdgDataBase();
   fEventCut.AddQAplotsToList(fList);
@@ -97,6 +105,13 @@ void AliAnalysisTaskdStar::UserCreateOutputObjects() {
   fTree->Branch("Pi", &fPiVector);
   fTree->SetAutoSave(100000000);
   PostData(2,fTree);
+
+  OpenFile(3);
+  fMCTree = new TTree("dStarMCTree", "MC generated data for dStar background analysis");
+  fMCTree->Branch("MCDeuteron", &fMCDeuteronVector);
+  fMCTree->Branch("MCPi", &fMCPiVector);
+  fMCTree->SetAutoSave(100000000);
+  PostData(3,fMCTree);
 
 }
 
@@ -125,29 +140,97 @@ void AliAnalysisTaskdStar::UserExec(Option_t *) {
   if (!stack)
   ::Fatal("AliAnalysisTaskdStar::UserExec","MC analysis requested on a sample without the MC particle array.");
 
-  /// Making the list of the dstar we want to measure
-  for (int iMC = 0; iMC < stack->GetEntriesFast(); ++iMC){
+  fMCPiVector.clear();
+  fMCPiVector.reserve(800);
+  fMCDeuteronVector.clear();
+  fMCDeuteronVector.reserve(40);
+
+  // need comment
+  for (int iMC = 0; iMC < stack->GetEntriesFast(); ++iMC) {
 
     AliAODMCParticle *part = (AliAODMCParticle*)stack->UncheckedAt(iMC);
     const int pdg = TMath::Abs(part->GetPdgCode());
     const int iC = part->Charge() > 0 ? 1 : 0;
-    if (pdg != 900010020) continue;
-    FourVector_t moth_vec = {0.f,0.f,0.f,0.f}, tmp_vec = {0.f,0.f,0.f,0.f};
 
-    for(int iD=0; iD<3; iD++){
-      const int daughter_id = part->GetDaughter(0)+iD;
-      AliAODMCParticle *daughter_part = (AliAODMCParticle*)stack->At(TMath::Abs(daughter_id));
-      tmp_vec.SetCoordinates(daughter_part->Pt(),daughter_part->Eta(),daughter_part->Phi(),daughter_part->M());
-      moth_vec+=tmp_vec;
+    if (pdg == 211) {
+      FourVector_t tmp_vec = {0.f,0.f,0.f,0.f};
+      tmp_vec.SetCoordinates(part->Pt(),part->Eta(),part->Phi(),part->M());
+      const int m_id = part->GetMother();
+      AliAODMCParticle *m_part = (AliAODMCParticle*)stack->UncheckedAt(m_id);
+      const int m_pdg = TMath::Abs(m_part->GetPdgCode());
+      unsigned char prop = 0u;
+      if (iC != 0)                          prop |= c;
+      if (part->IsPhysicalPrimary())        prop |= p;
+      if (part->IsSecondaryFromMaterial())  prop |= s;
+      daughter_struct daug;
+      daug.mother_pdg = m_pdg;
+      daug.mother_id  = m_id;
+      daug.vec        = tmp_vec;
+      daug.properties = prop;
+      fMCPiVector.push_back(daug);
     }
 
-    fProduction[iC]->Fill(part->M(),part->Pt());
-    if (part->Y() < fRequireYmin || part->Y() > fRequireYmax) continue;
-    fTotal[iC]->Fill(moth_vec.M(),moth_vec.Pt());
+    if (pdg == 1000010020) {
+      FourVector_t tmp_vec = {0.f,0.f,0.f,0.f};
+      tmp_vec.SetCoordinates(part->Pt(),part->Eta(),part->Phi(),part->M());
+      const int m_id = part->GetMother();
+      AliAODMCParticle *m_part = (AliAODMCParticle*)stack->UncheckedAt(m_id);
+      const int m_pdg = TMath::Abs(m_part->GetPdgCode());
+      unsigned char prop = 0u;
+      if (iC != 0)                          prop |= c;
+      if (part->IsPhysicalPrimary())        prop |= p;
+      if (part->IsSecondaryFromMaterial())  prop |= s;
+      daughter_struct daug;
+      daug.mother_pdg = m_pdg;
+      daug.mother_id  = m_id;
+      daug.vec        = tmp_vec;
+      daug.properties = prop;
+      fMCDeuteronVector.push_back(daug);
+    }
+
+    if (pdg == 900010020) {
+      FourVector_t moth_vec = {0.f,0.f,0.f,0.f}, tmp_vec = {0.f,0.f,0.f,0.f};
+      for(int iD=0; iD<3; iD++){
+        const int daughter_id = part->GetDaughter(0)+iD;
+        AliAODMCParticle *daughter_part = (AliAODMCParticle*)stack->At(TMath::Abs(daughter_id));
+        tmp_vec.SetCoordinates(daughter_part->Pt(),daughter_part->Eta(),daughter_part->Phi(),daughter_part->M());
+        moth_vec+=tmp_vec;
+      }
+      fProduction[iC]->Fill(part->M(),part->Pt());
+      if (part->Y() < fRequireYmin || part->Y() > fRequireYmax) continue;
+      fTotal[iC]->Fill(moth_vec.M(),moth_vec.Pt());
+    }
   }
 
-  /// Checking how many dstar in acceptance are reconstructed well
+  // filling MCDalitzPlot
+  for (const auto& mcdeu : fMCDeuteronVector) {
+    FourVector_t deu_vec = {0.f,0.f,0.f,0.f};
+    const unsigned char pdeu = mcdeu.properties;
+    if ((pdeu & c) && mcdeu.mother_pdg == 900010020) {
+      deu_vec = mcdeu.vec;
+      bool check = false;
+      for (const auto& mcpip : fMCPiVector) {
+        FourVector_t pip_vec = {0.f,0.f,0.f,0.f};
+        const unsigned char ppip = mcpip.properties;
+        if ((mcpip.mother_id == mcdeu.mother_id) && (ppip & c)) {
+          pip_vec = mcpip.vec;
+          for (const auto& mcpim : fMCPiVector) {
+            FourVector_t pim_vec = {0.f,0.f,0.f,0.f};
+            const unsigned char ppim = mcpim.properties;
+            if ((mcpim.mother_id == mcdeu.mother_id) && !(ppim & c) && !check) {
+              pim_vec = mcpim.vec;
+              FourVector_t pp = pip_vec + pim_vec;
+              FourVector_t pd = pim_vec + deu_vec;
+              fMCDalitzPlot->Fill(pp.M2(), pd.M2());
+              check = true;
+            }
+          }
+        }
+      }
+    }
+  }
 
+  // Checking how many dstar in acceptance are reconstructed well
   vector<mother_struct> mothers;
   mothers.reserve(40);
 
@@ -172,22 +255,30 @@ void AliAnalysisTaskdStar::UserExec(Option_t *) {
     // add deuterons and pions to the Tree for background analysis (ITS TPC only)
     // if they are under 3 sigmas TPC response
     if (TMath::Abs(fPID->NumberOfSigmas(AliPIDResponse::kTPC, track, AliPID::kDeuteron)) < 3.) {
+      FourVector_t tmp_deu = {(float)track->Pt(), (float)track->Eta(), (float)track->Phi(), (float)track->M(AliAODTrack::kDeuteron)};
+      unsigned char prop = 0u;
+      if (track->Charge() != 0)              prop |= c;
+      if (track->IsPhysicalPrimary())        prop |= p;
+      if (track->IsSecondaryFromMaterial())  prop |= s;
       daughter_struct deu;
       deu.mother_pdg = mum_pdg;
       deu.mother_id  = mother_id;
-      FourVector_t tmp_deu = {(float)track->Pt(), (float)track->Eta(), (float)track->Phi(), (float)track->M(AliAODTrack::kDeuteron)};
-      deu.vec = tmp_deu;
-      deu.charge = (track->Charge() > 0) ? true : false;
+      deu.vec        = tmp_deu;
+      deu.properties = prop;
       fDeuteronVector.push_back(deu);
     }
 
     if (TMath::Abs(fPID->NumberOfSigmas(AliPIDResponse::kTPC, track, AliPID::kPion)) <  3.) {
+      FourVector_t tmp_pi = {(float)track->Pt(), (float)track->Eta(), (float)track->Phi(), (float)track->M(AliAODTrack::kPion)};
+      unsigned char prop = 0u;
+      if (track->Charge() != 0)              prop |= c;
+      if (track->IsPhysicalPrimary())        prop |= p;
+      if (track->IsSecondaryFromMaterial())  prop |= s;
       daughter_struct pi;
       pi.mother_pdg = mum_pdg;
       pi.mother_id  = mother_id;
-      FourVector_t tmp_pi = {(float)track->Pt(), (float)track->Eta(), (float)track->Phi(), (float)track->M(AliAODTrack::kPion)};
-      pi.vec = tmp_pi;
-      pi.charge = (track->Charge() > 0) ? true : false;
+      pi.vec        = tmp_pi;
+      pi.properties = prop;
       fPiVector.push_back(pi);
     }
 
@@ -214,7 +305,7 @@ void AliAnalysisTaskdStar::UserExec(Option_t *) {
 
     // Check wether the track belgons to a pion
     if (pdg == 211 && mum_pdg == 900010020) {
-      if(TMath::Abs(fPID->NumberOfSigmas(AliPIDResponse::kTPC,track,AliPID::kPion))>3.) continue;
+      if (TMath::Abs(fPID->NumberOfSigmas(AliPIDResponse::kTPC,track,AliPID::kPion))>3.) continue;
       FourVector_t tmp_vec = {(float)track->Pt(),(float)track->Eta(),(float)track->Phi(),(float)track->M(AliAODTrack::kPion)};
       auto it = std::find(mothers.begin(),mothers.end(), mother_id);
       if (it == mothers.end()){
@@ -225,7 +316,7 @@ void AliAnalysisTaskdStar::UserExec(Option_t *) {
         tmp_mum.vec = tmp_vec;
         mothers.push_back(tmp_mum);
       }
-      else{
+      else {
         it->n_daughters++;
         it->pi_tof *= AliAnalysisTaskdStar::HasTOF(track) && (TMath::Abs(fPID->NumberOfSigmas(AliPIDResponse::kTOF,track,AliPID::kPion))<3.);
         it->vec+=tmp_vec;
@@ -236,6 +327,7 @@ void AliAnalysisTaskdStar::UserExec(Option_t *) {
 
   // Filling three
   fTree->Fill();
+  fMCTree->Fill();
 
   // Filling histograms
   for (const auto& mum : mothers) {
@@ -246,13 +338,14 @@ void AliAnalysisTaskdStar::UserExec(Option_t *) {
     const float pt_rec = mum.vec.Pt();
     const float mass_rec = mum.vec.M();
     fReconstructed[iC][0]->Fill(mass_rec,pt_rec);
-    if(mum.pi_tof && mum.deuteron_tof) fReconstructed[iC][1]->Fill(mass_rec,pt_rec);
-    if(mum.deuteron_tof) fReconstructed[iC][2]->Fill(mass_rec,pt_rec);
+    if (mum.pi_tof && mum.deuteron_tof) fReconstructed[iC][1]->Fill(mass_rec,pt_rec);
+    if (mum.deuteron_tof) fReconstructed[iC][2]->Fill(mass_rec,pt_rec);
   }
 
   //  Post output data.
   PostData(1,fList);
   PostData(2,fTree);
+  PostData(3,fMCTree);
 }
 
 /// This function checks whether a track has or has not a prolongation in the TOF.
