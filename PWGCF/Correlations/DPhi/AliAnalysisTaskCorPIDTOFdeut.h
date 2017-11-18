6c0d0d92820f3d3efab4d40533d108b32aee10e7
Brennan Schaefer <brennan.schaefer@cern.ch>
2017-11-18 15:27:44 +0100

    using both fine log-binning and coarse nat-binning for m^2

--- PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFdeut.h
+++ PWGCF/Correlations/DPhi/AliAnalysisTaskCorPIDTOFdeut.h
@@ -11,7 +11,7 @@
 
 class AliAODTrack;
 
-namespace BSchaefer_devel{
+//namespace BSchaefer_devel{
     
 class AliAnalysisTaskCorPIDTOFdeut : public AliAnalysisTaskSE  
 {
@@ -150,5 +150,5 @@ class AliAnalysisTaskCorPIDTOFdeut : public AliAnalysisTaskSE
         ClassDef(AliAnalysisTaskCorPIDTOFdeut, 1);
 };
 
-}  //// namespace
+//}  //// namespace
 #endif
