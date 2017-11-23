dd44f6a35d4a68010e864f8a6dce0247b559c3e6
Nicolas Schmidt <nicolas.schmidt@cern.ch>
2017-11-23 15:34:38 +0100

    PWGGA/GammaConv: Added mission classdef increment

--- PWGGA/GammaConv/AliAnalysisTaskGammaCalo.h
+++ PWGGA/GammaConv/AliAnalysisTaskGammaCalo.h
@@ -397,7 +397,7 @@ class AliAnalysisTaskGammaCalo : public AliAnalysisTaskSE {
     AliAnalysisTaskGammaCalo(const AliAnalysisTaskGammaCalo&);                  // Prevent copy-construction
     AliAnalysisTaskGammaCalo &operator=(const AliAnalysisTaskGammaCalo&);       // Prevent assignment
 
-    ClassDef(AliAnalysisTaskGammaCalo, 39);
+    ClassDef(AliAnalysisTaskGammaCalo, 40);
 };
 
 #endif
