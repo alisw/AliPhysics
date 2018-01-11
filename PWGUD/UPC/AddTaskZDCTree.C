6c90cac2b45842ef6397044c66f62f29d1420b12
Uliana Dmitrieva <uliana.dmitrieva@cern.ch>
2018-01-11 16:32:41 +0300

    Output container changed

--- PWGUD/UPC/AddTaskZDCTree.C
+++ PWGUD/UPC/AddTaskZDCTree.C
@@ -25,7 +25,7 @@ AliAnalysisTaskZDCTree* AddTaskZDCTree(TString name = "name")
     // your task needs input: here we connect the manager to your task
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
     // same for the output
-    mgr->ConnectOutput(task,1,mgr->CreateContainer("fZDCTree", TTree::Class(),AliAnalysisManager::kOutputContainer,fileName.Data()));
+    mgr->ConnectOutput(task,1,mgr->CreateContainer("fOutput", TList::Class(),AliAnalysisManager::kOutputContainer,fileName.Data()));
     // in the end, this macro returns a pointer to your task. this will be convenient later on
     // when you will run your analysis in an analysis train on grid
     return task;
