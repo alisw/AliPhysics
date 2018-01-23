21e84d62a273e121dd85dfbeb40f9cb760e13d76
laltenka <lucas.altenkamper@cern.ch>
2018-01-23 17:52:10 +0100

    PWGDQ/reducedTree: updated AliReducedEventCut with exclusion map instead of single bool

--- PWGDQ/reducedTree/AliReducedEventCut.cxx
+++ PWGDQ/reducedTree/AliReducedEventCut.cxx
@@ -20,7 +20,7 @@ ClassImp(AliReducedEventCut)
 AliReducedEventCut::AliReducedEventCut() :
   AliReducedVarCut(),
   fEventTagFilterEnabled(kFALSE),
-  fEventTagFilterExclude(kFALSE),
+  fEventTagFilterExclude(0),
   fEventFilter(0),
   fEventTriggerMaskEnabled(kFALSE), 
   fEventTriggerMask(0),
@@ -38,7 +38,7 @@ AliReducedEventCut::AliReducedEventCut() :
 AliReducedEventCut::AliReducedEventCut(const Char_t* name, const Char_t* title) :
   AliReducedVarCut(name, title),
   fEventTagFilterEnabled(kFALSE),
-  fEventTagFilterExclude(kFALSE),
+  fEventTagFilterExclude(0),
   fEventFilter(0),
   fEventTriggerMaskEnabled(kFALSE), 
   fEventTriggerMask(0),
@@ -82,7 +82,7 @@ Bool_t AliReducedEventCut::IsSelected(TObject* obj, Float_t* values) {
    if(!obj->InheritsFrom(AliReducedBaseEvent::Class())) return kFALSE;
    
    AliReducedBaseEvent* event = (AliReducedBaseEvent*)obj;
-   if (fEventTagFilterEnabled && fEventTagFilterExclude==(Bool_t)(event->EventTag() & fEventFilter)) return kFALSE;
+   if (fEventTagFilterEnabled && ((fEventTagFilterExclude & (event->EventTag() & fEventFilter)) || !(event->EventTag() & fEventFilter))) return kFALSE;
 
    if(fEventTriggerMaskEnabled) {
      if(!obj->InheritsFrom(AliReducedEventInfo::Class())) return kFALSE;
