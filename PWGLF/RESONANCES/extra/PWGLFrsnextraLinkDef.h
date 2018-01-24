40fc63232e826ae90fe68ab449f4430816c8b4a6
Bong-Hwi Lim <bong-hwi.lim@cern.ch>
2018-01-24 09:43:35 +0100

    Add macro and source to list

--- PWGLF/RESONANCES/extra/PWGLFrsnextraLinkDef.h
+++ PWGLF/RESONANCES/extra/PWGLFrsnextraLinkDef.h
@@ -6,8 +6,12 @@
 #pragma link C++ class AliRsnAnalysisPhi7TeV+;
 #pragma link C++ class AliAnalysisTaskSigma1385+;
 #pragma link C++ class AliXiStar+;
+#pragma link C++ class AliXiStarpp13TeV+;
+#pragma link C++ class AliXiStarpp13TeVEventCollection+;
 #pragma link C++ class AliXiStarEventCollection+;
+#pragma link C++ class AliXiStarpp13TeVEventStruct+
 #pragma link C++ class AliXiStarEventStruct+;
+#pragma link C++ class AliXiStarpp13TeVTrackStruct+;
 #pragma link C++ class AliXiStarTrackStruct+;
 #pragma link C++ class AliAnalysisTaskLambdaStar+;
 #pragma link C++ class AliAnalysisTaskSEF01710fromAODtracks+;
