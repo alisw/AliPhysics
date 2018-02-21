cc98de3df0d7417827362ef56ad9010cd9ca4e60
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2018-02-21 15:00:02 +0100

    check the MC production tag name and assign data year and period for some MC productions

--- PWGGA/CaloTrackCorrelations/macros/GetAlienGlobalProductionVariables.C
+++ PWGGA/CaloTrackCorrelations/macros/GetAlienGlobalProductionVariables.C
@@ -68,4 +68,28 @@ void GetAlienGlobalProductionVariables(Bool_t & simulation,
     else if(period.Contains("10")) year = 2010;
   } 
   
+  // Check MC production tag name to match with data year and production name
+  if ( simulation && period=="" )
+  {
+    // 2011 MC productions
+   if ( prodType.Contains("14ka1")  || prodType.Contains("14k1b") || // 7 TeV jet-jet+gamma
+        prodType.Contains("12a15g") || prodType.Contains("13e4")  || // 7 TeV gamma-jet   
+        prodType.Contains("12f2a")  || prodType.Contains("12f2b") || // 7 TeV jet-jet+pi0, jet-jet+hadron
+        prodType.Contains("12a15f") || prodType.Contains("12a15a")|| // 7 TeV and 2.76 jet-jet
+        prodType.Contains("12a17")  || prodType.Contains("14a1")   ) // Pb-Pb LHC11h
+   {
+     year = 2011;
+     period = "LHC11";
+   }
+    
+    // 2012 MC productions
+    if ( prodType.Contains("15h1") || // 8 TeV min bias
+         prodType.Contains("16c2") || // 8 TeV jet-jet   
+         prodType.Contains("17g5")  ) // 8 TeV jet-jet+gamma, gamma-jet
+    {
+      year = 2012;
+      period = "LHC12";
+    }
+  } // Prod MC names
+  
 }
