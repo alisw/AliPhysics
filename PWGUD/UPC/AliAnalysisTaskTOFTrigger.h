66e79e887ca9014c3fa36cacf1892956f44ab3ab
Christoph Mayer <Christoph.Mayer@cern.ch>
2018-02-22 16:50:54 +0100

    delete-trailing-whitespace

--- PWGUD/UPC/AliAnalysisTaskTOFTrigger.h
+++ PWGUD/UPC/AliAnalysisTaskTOFTrigger.h
@@ -25,13 +25,13 @@ class AliAnalysisTaskTOFTrigger : public AliAnalysisTaskSE {
   void SetupParameters(Float_t lowpt,Float_t highpt,Int_t highmult,TString trgcls,Int_t nBCs)
   			{fMaxPt = highpt; fMinPt = lowpt; fMaxMulti = highmult; fTriggerClass = trgcls;fMaxBCs = nBCs;}
   void GetLTMIndex(const Int_t * const detind, Int_t *indexLTM);
-  
+
  private:
-  
+
   TList *fOutputList;		//<
-  
-  AliPIDResponse *fPIDResponse;		
-  AliESDtrackCuts *fTrackCuts; 
+
+  AliPIDResponse *fPIDResponse;
+  AliESDtrackCuts *fTrackCuts;
   AliTOFTriggerMask *fTOFmask;
   TEfficiency *eff_MaxiPadLTM_All;	//!
   TEfficiency *eff_MaxiPadLTM_Mu;	//!
@@ -42,7 +42,7 @@ class AliAnalysisTaskTOFTrigger : public AliAnalysisTaskSE {
   TH2F *hTrackDistribution;		//!
   TH2F *hFiredMaxiPad;			//!
   TH2F *hNotFiredMaxiPad;		//!
-  TH2F *hTrackPadCorrPhi;		//!  
+  TH2F *hTrackPadCorrPhi;		//!
   TH2F *hTrackPadCorrEta;		//!
   TH2F *hNoiseMaxiPad;			//!
   TH1I *hTriggerCounter;		//!
@@ -57,20 +57,20 @@ class AliAnalysisTaskTOFTrigger : public AliAnalysisTaskSE {
   TH1I *hDetIn4;			//!
   TH1F *hPadDistance;			//!
   TH1F *hTrackPt;			//!
-  TH1I *hNMaxiPadIn;			//! 
- 
+  TH1I *hNMaxiPadIn;			//!
+
   Bool_t fGeomLoaded;
   Float_t fMaxPt;
   Float_t fMinPt;
   Int_t fMaxMulti;
   TString fTriggerClass;
   Int_t fMaxBCs;
-   
-  
+
+
   AliAnalysisTaskTOFTrigger(const AliAnalysisTaskTOFTrigger&); //not implemented
   AliAnalysisTaskTOFTrigger& operator =(const AliAnalysisTaskTOFTrigger&); //not implemented
-  
-  ClassDef(AliAnalysisTaskTOFTrigger, 4); 
+
+  ClassDef(AliAnalysisTaskTOFTrigger, 4);
 };
 
 #endif
