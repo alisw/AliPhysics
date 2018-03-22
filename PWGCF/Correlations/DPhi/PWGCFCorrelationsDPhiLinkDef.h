8d01432b0cb4680fd34741b9ab9ec5e71a5acf25
Hyeonjoong Kim <hyeonjoong.kim@cern.ch>
2018-03-22 11:08:32 +0100

    Remove dependencies for DPhi library 	modified:   ../CMakeLists.txt 	modified:   ../DPhi/CMakeLists.txt 	modified:   ../DPhi/PWGCFCorrelationsDPhiLinkDef.h 	renamed:    ../DPhi/EPCorr/AliAnalysisTaskJetLikeCorrelation.cxx -> ../EPCorrelation/AliAnalysisTaskJetLikeCorrelation.cxx 	renamed:    ../DPhi/EPCorr/AliAnalysisTaskJetLikeCorrelation.h -> ../EPCorrelation/AliAnalysisTaskJetLikeCorrelation.h 	new file:   ../EPCorrelation/CMakeLists.txt 	new file:   ../EPCorrelation/PWGCFCorrelationsEPCorrelationLinkDef.h

--- PWGCF/Correlations/DPhi/PWGCFCorrelationsDPhiLinkDef.h
+++ PWGCF/Correlations/DPhi/PWGCFCorrelationsDPhiLinkDef.h
@@ -80,8 +80,4 @@
 #pragma link C++ class AliCSTrackMaps+;
 #pragma link C++ class AliCSTrackSelection+;
 #pragma link C++ class AliDptDptCorrelations+;
-#pragma link C++ class AliExtractedTrack+;
-#pragma link C++ class AliExtractedEvent+;
-#pragma link C++ class AliAnalysisTaskJetLikeCorrelation+;
-#pragma link C++ class MixedParticle+;
 #endif
