ddb8b62c1969c9fc8d8038f61175be0f943650d2
Dong Jo Kim <dong.jo.kim@cern.ch>
2018-03-28 10:30:56 +0300

    only with charged jet

--- PWGCF/Correlations/macros/jcorran/AddTaskBSDiJet.C
+++ PWGCF/Correlations/macros/jcorran/AddTaskBSDiJet.C
@@ -1,42 +1,40 @@
-AliBSDiJetTask * AddAliBSDiJetTask(TString taskname, bool isAA, UInt_t triggMask, Double_t leadingParticlePtMin, AliEmcalJetTask* jetFinderTask, AliEmcalJetTask* jetFinderTaskkt, bool kineon, TString option){
-	AliBSDiJetTask *task = new AliBSDiJetTask(taskname, option.Data());
-	if (!option.Contains("MC")) {
-		task->SelectCollisionCandidates( triggMask ); // WARNING: default is AliVEvent::kEMCEJE. Double check it!
-	}
-	task->SetIsAA( isAA );
-	if (option.Contains("MC")) task->SetIsMC(1);
+#if defined(__CLING__)
+R__ADD_INCLUDE_PATH($ALICE_PHYSICS)
+#include <PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C>
+#endif
 
+AliBSDiJetTask * AddTaskBSDiJet(TString taskname, bool isAA, Double_t leadingParticlePtMin, TString option ){
+
+ AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr)
+  {
+    ::Error("AddTaskJJet", "No analysis manager to connect to.");
+    return NULL;
+  }
+	gROOT->LoadMacro("$ALICE_PHYSICS/PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C");
+	AliEmcalJetTask *jetFinderTask;
+	AliEmcalJetTask *jetFinderTaskkt;
+	TString trackcontname="tracks";
+	jetFinderTask = AddTaskEmcalJet(trackcontname.Data(),"", AliJetContainer::antikt_algorithm,0.4,AliJetContainer::kChargedJet,0.15, 0.300, 0.005, AliJetContainer::pt_scheme, "Jets", 0. );
+	jetFinderTaskkt = AddTaskEmcalJet(trackcontname.Data(),"",AliJetContainer::kt_algorithm,0.2,AliJetContainer::kChargedJet,0.15, 0.300, 0.005, AliJetContainer::pt_scheme, "JetsKt", 0. );
+
+	// Own class
+	AliBSDiJetTask *task = new AliBSDiJetTask(taskname, option);
+	task->SetIsAA( isAA );
 	task->SetLeadingParticlePtMin( leadingParticlePtMin );
 
-	AliJetContainer *jetCont = task->AddJetContainer(jetFinderTask->GetName(),taskname.Contains("FullJet") ? "EMCALfid" : "TPCfid");
+	AliJetContainer *jetCont = task->AddJetContainer(jetFinderTask->GetName(),"TPCfid");
 	AliJetContainer *jetContKt = 0x0;
 	AliParticleContainer *trackCont = 0x0;
 	AliClusterContainer *clusterCont = 0x0;
-	if (kineon) {
-		AliMCParticleContainer *mcCont = task->AddMCParticleContainer("mcparticles");
-	}
-	else {
-		TString trackcontname="tracks";
-		//if (option.Contains("12a15e") || option.Contains("16j5")) trackcontname = "PicoTracksMer";
-		trackCont = task->AddParticleContainer(trackcontname.Data());
-		if (taskname.Contains("FullJet")) clusterCont = task->AddClusterContainer("caloClusters");
-		jetContKt = task->AddJetContainer(jetFinderTaskkt->GetName(),taskname.Contains("FullJet") ? "EMCALfid" : "TPCfid");
-	}
+	trackCont = task->AddParticleContainer(trackcontname.Data());
+	jetCont->ConnectParticleContainer( trackCont );
+	jetContKt->ConnectParticleContainer ( trackCont );
+	jetContKt->SetLeadingHadronType( 0 );
 
-	if (kineon) jetCont->ConnectParticleContainer( mcCont );
-	else {
-		jetCont->ConnectParticleContainer( trackCont );
-		if (taskname.Contains("FullJet")) jetCont->ConnectClusterContainer(clusterCont);
-		jetContKt->ConnectParticleContainer ( trackCont );
-		jetContKt->SetLeadingHadronType( 0 );
-
-	}
 	//jetCont->SetZLeadingCut( 0.98, 0.98 ); // FIXME: Comments me and others
 	cout<<"jetFinderTask->GetRadius() : "<<jetFinderTask->GetRadius()<<endl;
 	if (jetFinderTask->GetRadius() >= 0.4)  jetCont->SetPercAreaCut( 0.6 );
-	if (!taskname.Contains("FullJet")) jetCont->SetLeadingHadronType( 0 ); //leading hadron set to h+-
-	//jetCont->SetJetPtCut( 5 );
-
 	// Create containers for input/output
 	AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
 	AliAnalysisDataContainer *coutput = mgr->CreateContainer(taskname, TList::Class(), AliAnalysisManager::kOutputContainer,Form("%s", AliAnalysisManager::GetCommonFileName()));
