46c2470764bad8ade7f19555432c2f5abf3e8177
Ana Marin <a.marin@gsi.de>
2018-03-29 14:51:02 +0200

    PWGGA: adding Iconfigs for R scan

--- PWGGA/GammaConv/macros/AddTask_GammaConvV1_pp2.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvV1_pp2.C
@@ -269,6 +269,12 @@ void AddTask_GammaConvV1_pp2(   Int_t    trainConfig                 = 1,       
     cuts.AddCut("00010113", "00200009227300008250404000", "0152101500000000"); //old standard cut pp 5 TeV VAND
   } else if (trainConfig == 51){
     cuts.AddCut("00010113", "00200009227300008250404000", "0152103500000000"); //new standard cut pp 5 TeV VAND
+  } else if (trainConfig == 52){
+    cuts.AddCut("00010113", "00a00009227300008250404000", "0152103500000000"); //new standard cut pp 5 TeV VAND
+  } else if (trainConfig == 53){
+    cuts.AddCut("00010113", "00b00009227300008250404000", "0152103500000000"); //new standard cut pp 5 TeV VAND
+  } else if (trainConfig == 54){
+    cuts.AddCut("00010113", "00c00009227300008250404000", "0152103500000000"); //new standard cut pp 5 TeV VAND
 
 
   } else {
