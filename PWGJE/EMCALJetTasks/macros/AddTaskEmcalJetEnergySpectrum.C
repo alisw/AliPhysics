0a91891f6cd7e747d0c34a4514e402a23a10d87c
Markus Fasel <markus.fasel@cern.ch>
2018-04-27 18:18:29 +0200

    [Hotfix] Fix call to static add function

--- PWGJE/EMCALJetTasks/macros/AddTaskEmcalJetEnergySpectrum.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskEmcalJetEnergySpectrum.C
@@ -1,3 +1,3 @@
 EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum *AddTaskJetEnergySpectrum(Bool_t isMC, AliJetContainer::EJetType_t jettype, Double_t radius, const char *trigger, const char *suffix){
-  return EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum::AddTaskEmcalJetEnergySpectrum(isMC, jettype, radius, trigger, suffix);
+  return EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum::AddTaskJetEnergySpectrum(isMC, jettype, radius, trigger, suffix);
 }
