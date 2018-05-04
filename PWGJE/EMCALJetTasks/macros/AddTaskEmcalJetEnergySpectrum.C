fa39255d16549a385e8e15c6b21ae1ef56afc9e5
Markus Fasel <markus.fasel@cern.ch>
2018-05-04 10:26:06 +0200

    Remove suffix from add macro parameters

--- PWGJE/EMCALJetTasks/macros/AddTaskEmcalJetEnergySpectrum.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskEmcalJetEnergySpectrum.C
@@ -1,3 +1,3 @@
-EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum *AddTaskEmcalJetEnergySpectrum(Bool_t isMC, AliJetContainer::EJetType_t jettype, Double_t radius, const char *trigger, const char *suffix){
-  return EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum::AddTaskJetEnergySpectrum(isMC, jettype, radius, trigger, suffix);
+EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum *AddTaskEmcalJetEnergySpectrum(Bool_t isMC, AliJetContainer::EJetType_t jettype, Double_t radius, const char *trigger){
+  return EmcalTriggerJets::AliAnalysisTaskEmcalJetEnergySpectrum::AddTaskJetEnergySpectrum(isMC, jettype, radius, trigger);
 }
