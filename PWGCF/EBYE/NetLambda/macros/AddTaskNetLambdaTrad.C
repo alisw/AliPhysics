49fc745e7c91badcd58b00d7ed62026e3ea9eadc
Ejiro Umaka <enumaka@uh.edu>
2018-05-10 21:19:22 -0500

    Add files via upload

---
+++ PWGCF/EBYE/NetLambda/macros/AddTaskNetLambdaTrad.C
@@ -0,0 +1,37 @@
+AliAnalysisTaskNetLambdaTrad *AddTaskNetLambdaTrad(const char* outputFileName = 0, const char* containerName = "NetLambdaOutput")
+{
+    // Get the pointer to the existing analysis manager via the static access method.
+    //==============================================================================
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    if (!mgr) {
+        ::Error("AddTaskNetLambdaTrad", "No analysis manager to connect to.");
+        return NULL;
+    }
+    
+    // Create the task and configure it.
+    //===========================================================================
+    AliAnalysisTaskNetLambdaTrad* ana = new  AliAnalysisTaskNetLambdaTrad(containerName);
+    
+    // common config,
+    ana->SetDebugLevel(0);
+    
+    //   gSystem->Sleep(3000);
+    
+    mgr->AddTask(ana);
+    
+    // Create ONLY the output containers for the data produced by the task.
+    // Get and connect other common input/output containers via the manager as below
+    //==============================================================================
+    if (!outputFileName)
+        outputFileName = AliAnalysisManager::GetCommonFileName();
+    
+    outputFileName = "NetLambdaMC.root";
+    
+    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer(containerName, TList::Class(),AliAnalysisManager::kOutputContainer,outputFileName);
+    AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("Treevariables", TTree::Class(),AliAnalysisManager::kOutputContainer,Form("%s", outputFileName));
+    
+    mgr->ConnectInput  (ana, 0, mgr->GetCommonInputContainer());
+    mgr->ConnectOutput (ana, 1, coutput1 );
+    mgr->ConnectOutput (ana, 2, coutput2);
+    return ana;
+}
