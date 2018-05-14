2303254cdb74671e0eb8e576428539780cd8b2bb
jokonig <jokonig@cern.ch>
2018-05-14 10:33:12 +0200

    Modified V0Reader cut string to be the same in all AddTasks (calo, conv, convcalo). This should avoid problems when using wagons from different groups within the same train.

--- PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
@@ -148,7 +148,7 @@ void AddTask_GammaCalo_pp(  Int_t     trainConfig                   = 1,        
   Printf("here \n");
 
   //=========  Set Cutnumber for V0Reader ================================
-  TString cutnumberPhoton = "00000008400100001500000000";
+  TString cutnumberPhoton = "00000008400000000100000000";
   TString cutnumberEvent = "00000003";
   Bool_t doEtaShift = kFALSE;
   AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
