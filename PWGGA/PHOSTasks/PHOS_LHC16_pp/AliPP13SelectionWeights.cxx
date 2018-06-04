9cf0a7eb499e5d91d719a21d279f21abba541eb8
Oleksandr Kovalenko <oleksandr.kovalenko@cern.ch>
2018-06-04 20:00:51 +0000

    Update the nonlinearity parametrization for all productions. Add hitogram names to the logs.

--- PWGGA/PHOSTasks/PHOS_LHC16_pp/AliPP13SelectionWeights.cxx
+++ PWGGA/PHOSTasks/PHOS_LHC16_pp/AliPP13SelectionWeights.cxx
@@ -41,10 +41,10 @@ Double_t AliPP13SelectionWeightsSPMC::Weights(Double_t pT, const EventFlags & ef
     Double_t opT = origin->Pt();
 
     // NB: Try generating the yield instead of invariant yield
-	Double_t w = /* opT * */ opT * fW0 / 2. / TMath::Pi();
-	Double_t fraction = (fW2 - 1.) * (fW2 - 2.) / (fW2 * fW1 * (fW2 * fW1 + fW4 * (fW2 - 2.)));
-	Double_t power = TMath::Power(1. + (TMath::Sqrt(opT * opT + fW3 * fW3) - fW4) / (fW2 * fW1), -fW2);
-	return w * fraction * power;
+    Double_t w = /* opT * */ opT * fW0 / 2. / TMath::Pi();
+    Double_t fraction = (fW2 - 1.) * (fW2 - 2.) / (fW2 * fW1 * (fW2 * fW1 + fW4 * (fW2 - 2.)));
+    Double_t power = TMath::Power(1. + (TMath::Sqrt(opT * opT + fW3 * fW3) - fW4) / (fW2 * fW1), -fW2);
+    return w * fraction * power;
 }
 
 //________________________________________________________________
@@ -57,21 +57,23 @@ AliPP13SelectionWeights & AliPP13SelectionWeightsSPMC::SinglePi0()
     // ws.fW1 = 0.28727403800708634;
     // ws.fW2 = 9.9198075195331;
 
-
-    // Debug
-    ws.fW0 = 21.339890553914014;
-    ws.fW1 = 0.08359755308503322;
-    ws.fW2 = 7.334946541612603;
-
     // Weights 0 (new efficiency)
     // ws.fW0 = 0.10325998438001027;
     // ws.fW1 = 0.1710556728057399;
     // ws.fW2 = 8.613628140871766;
 
+    // Debug
+    // ws.fW0 = 21.339890553914014;
+    // ws.fW1 = 0.08359755308503322;
+    // ws.fW2 = 7.334946541612603;
+
+    // The latest iteration
+    //
+    ws.fW0 = 0.2622666606436988 / 0.0119143016137;
+    ws.fW1 = 0.08435275173194286;
+    ws.fW2 = 7.356520553419461;
     ws.fW3 = 0.135;
     ws.fW4 = 0.135;
-
-
 
     // Nonlinearity Naive estimation
     //
@@ -79,14 +81,13 @@ AliPP13SelectionWeights & AliPP13SelectionWeightsSPMC::SinglePi0()
     // ws.fNonSigma = 1.4188237289034245;
     // ws.fNonGlobal = 1.0579663356860527;
 
-     ws.fNonA = -0.014719244288611932;
-     ws.fNonSigma = 2 * 0.8017501954719543;
-     ws.fNonGlobal = 1.050000000000015;
+    ws.fNonA = -0.023207895974126137;
+    ws.fNonSigma = 0.5 * 2.1705074159914495;
+    ws.fNonGlobal = 1.0178019980200619;
 
     // ws.fNonGlobal = 1.0;
     // ws.fNonA = 0;
     // ws.fNonSigma = 1.0579663356860527;
-
     return ws;
 }
 
@@ -101,31 +102,36 @@ AliPP13SelectionWeights & AliPP13SelectionWeightsSPMC::SingleEta()
     ws.fW0 = 0.201;
     ws.fW1 = 0.229;
     ws.fW2 = 7.0;
-
     ws.fW3 = 0.547;
     ws.fW4 = 0.547;
 
-     ws.fNonA = -0.014719244288611932;
-     ws.fNonSigma = 2 * 0.8017501954719543;
-     ws.fNonGlobal = 1.050000000000015;
-     return ws;
+    // Former nonlinearity
+    // ws.fNonA = -0.014719244288611932;
+    // ws.fNonSigma = 2 * 0.8017501954719543;
+    // ws.fNonGlobal = 1.050000000000015;
+
+    // The latest nonlinarity tested on the simples data
+    ws.fNonA = -0.023207895974126137;
+    ws.fNonSigma = 0.5 * 2.1705074159914495;
+    ws.fNonGlobal = 1.0178019980200619;
+    return ws;
 }
 
 
 //________________________________________________________________
 AliPP13SelectionWeights & AliPP13SelectionWeights::Init(Mode m)
 {
-    if(m == kSinglePi0MC)
+    if (m == kSinglePi0MC)
         return AliPP13SelectionWeightsSPMC::SinglePi0();
 
-    if(m == kSingleEtaMC)
+    if (m == kSingleEtaMC)
         return AliPP13SelectionWeightsSPMC::SingleEta();
 
-    if(m == kMC)
+    if (m == kMC)
         return * new AliPP13SelectionWeightsMC();
 
-    if(m == kData)
+    if (m == kData)
         return * new AliPP13SelectionWeightsTOF();
-      
+
     return * new AliPP13SelectionWeights();
 }
