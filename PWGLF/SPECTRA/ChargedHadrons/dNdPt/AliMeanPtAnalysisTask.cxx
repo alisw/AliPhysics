441f5e50619485384d97b93e9d0c0967b7e9d3d9
Mario Kruger <mario.kruger@cern.ch>
2018-06-12 10:52:58 +0200

    AliMeanPt:: added pT resolution histograms

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/AliMeanPtAnalysisTask.cxx
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/AliMeanPtAnalysisTask.cxx
@@ -82,6 +82,7 @@ AliMeanPtAnalysisTask::AliMeanPtAnalysisTask(const char* name) : AliAnalysisTask
   fBinsPt(0),
   fBinsEta(0),
   fBinsZv(0),
+  fBinsPtReso(0),
   //Event-Histograms
   fEventCount(0),
   fHistMCTrackParticle(0),
@@ -90,6 +91,7 @@ AliMeanPtAnalysisTask::AliMeanPtAnalysisTask(const char* name) : AliAnalysisTask
   fHistMCResponseMatTracks(0),
   //Track-Histograms
   fHistTrack(0),
+  fHistRelPtResoFromCov(0),
   fHistMCRecTrack(0),
   fHistMCGenPrimTrack(0),
   fHistMCRecPrimTrack(0),
@@ -97,6 +99,7 @@ AliMeanPtAnalysisTask::AliMeanPtAnalysisTask(const char* name) : AliAnalysisTask
   fHistMCMultPtGenerated(0),
   fHistMCTrackMultGen(0),
   fHistMCPtRes(0),
+  fHistMCRelPtReso(0),
   fHistMCEtaRes(0),
   fHistMCMultRes(0),
   fHistMCParticle(0)
@@ -109,6 +112,15 @@ AliMeanPtAnalysisTask::AliMeanPtAnalysisTask(const char* name) : AliAnalysisTask
   Double_t binsZvDefault[13] = {-30.,-25.,-20.,-15.,-10.,-5.,0.,5.,10.,15.,20.,25.,30.};
   //  Double_t binsPtDefault[69] = {0.,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.2,2.4,2.6,2.8,3.0,3.2,3.4,3.6,3.8,4.0,4.5,5.0,5.5,6.0,6.5,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,18.0,20.0,22.0,24.0,26.0,28.0,30.0,32.0,34.0,36.0,40.0,45.0,50.0};
   //  Double_t binsEtaDefault[31] = {-1.5,-1.4,-1.3,-1.2,-1.1,-1.0,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5};
+
+  Double_t minPtReso = 0.;
+  Double_t maxPtReso = 0.3;
+  Int_t nBinsPtReso = 300;
+  Double_t binsPtReso[301];
+  for (Int_t i = 0; i <= nBinsPtReso; i++) {
+    binsPtReso[i] = minPtReso + i*(maxPtReso-minPtReso)/nBinsPtReso;
+  }
+  SetBinsPtReso(nBinsPtReso, binsPtReso);
 
   SetBinsMult(1,binsMultDefault);
   SetBinsCent(1,binsCentDefault);
@@ -167,6 +179,23 @@ void AliMeanPtAnalysisTask::UserCreateOutputObjects(){
   fHistTrack->GetAxis(3)->SetTitle("Centrality (%)");
   fHistTrack -> Sumw2();
 
+
+  /// relative pT resolution from covariance matrix (global tracks) as a function of pt and centrality
+  Int_t nBinsRelPtReso[3]  = {fBinsPtReso->GetSize()-1,fBinsPt->GetSize()-1, fBinsCent->GetSize()-1};
+  Double_t minRelPtReso[3] = {fBinsPtReso->GetAt(0),fBinsPt->GetAt(0), fBinsCent->GetAt(0)};
+  Double_t maxRelPtReso[3] = {fBinsPtReso->GetAt(fBinsPtReso->GetSize()-1), fBinsPt->GetAt(fBinsPt->GetSize()-1), fBinsCent->GetAt(fBinsCent->GetSize()-1)};
+
+  fHistRelPtResoFromCov = new THnF("fHistRelPtResoFromCov", "Relative pT resolution from covariance matrix", 3, nBinsRelPtReso, minRelPtReso, maxRelPtReso);
+  fHistRelPtResoFromCov -> SetBinEdges(0,fBinsPtReso->GetArray());
+  fHistRelPtResoFromCov -> SetBinEdges(1,fBinsPt->GetArray());
+  fHistRelPtResoFromCov -> SetBinEdges(2,fBinsCent->GetArray());
+  fHistRelPtResoFromCov ->GetAxis(0)->SetTitle("#sigma(#it{p}_{T}) / #it{p}_{T}");
+  fHistRelPtResoFromCov ->GetAxis(1)->SetTitle("#it{p}_{T} (GeV/#it{c})");
+  fHistRelPtResoFromCov ->GetAxis(2)->SetTitle("Centrality (%)");
+  fHistRelPtResoFromCov -> Sumw2();
+
+
+
   if(fIsMC){
 
     // Control histogram showing reconstructed Tracks vs. reconstructed Particles
@@ -212,6 +241,25 @@ void AliMeanPtAnalysisTask::UserCreateOutputObjects(){
     fHistMCPtRes -> SetBinEdges(1,fBinsPt->GetArray());
     fHistMCPtRes->GetAxis(0)->SetTitle("#it{p}_{T}^{track} (GeV/#it{c})");
     fHistMCPtRes->GetAxis(1)->SetTitle("#it{p}_{T}^{particle} (GeV/#it{c})");
+
+
+    /// relative pT resolution from MC as a function of pt generated, reconstructed and centrality
+    Int_t nBinsMCRelPtReso[4]={fBinsPtReso->GetSize()-1,fBinsPt->GetSize()-1, fBinsPt->GetSize()-1, fBinsCent->GetSize()-1};
+    Double_t minMCRelPtReso[4]={fBinsPtReso->GetAt(0),fBinsPt->GetAt(0), fBinsPt->GetAt(0), fBinsCent->GetAt(0)};
+    Double_t maxMCRelPtReso[4]={fBinsPtReso->GetAt(fBinsPtReso->GetSize()-1),fBinsPt->GetAt(fBinsPt->GetSize()-1), fBinsPt->GetAt(fBinsPt->GetSize()-1), fBinsCent->GetAt(fBinsCent->GetSize()-1)};
+
+    fHistMCRelPtReso = new THnF("fHistMCRelPtReso", "Relative pT resolution MC", 4, nBinsMCRelPtReso, minMCRelPtReso, maxMCRelPtReso);
+    fHistMCRelPtReso -> SetBinEdges(0,fBinsPtReso->GetArray());
+    fHistMCRelPtReso -> SetBinEdges(1,fBinsPt->GetArray());
+    fHistMCRelPtReso -> SetBinEdges(2,fBinsPt->GetArray());
+    fHistMCRelPtReso -> SetBinEdges(3,fBinsCent->GetArray());
+    fHistMCRelPtReso ->GetAxis(0)->SetTitle("#Delta(#it{p}_{T}) / #it{p}_{T}");
+    fHistMCRelPtReso ->GetAxis(1)->SetTitle("#it{p}^{MC, gen}_{T} (GeV/#it{c})");
+    fHistMCRelPtReso ->GetAxis(2)->SetTitle("#it{p}^{MC, rec}_{T} (GeV/#it{c})");
+    fHistMCRelPtReso ->GetAxis(3)->SetTitle("Centrality (%)");
+    fHistMCRelPtReso -> Sumw2();
+
+
 
     /// Eta Resolution Histogram
     Int_t nBinsMCEtaEta[2]={fBinsEta->GetSize()-1, fBinsEta->GetSize()-1};
@@ -321,10 +369,12 @@ void AliMeanPtAnalysisTask::UserCreateOutputObjects(){
 
   fOutputList->Add(fHistEvent);
   fOutputList->Add(fHistTrack);
+  fOutputList->Add(fHistRelPtResoFromCov);
 
   if(fIsMC){
     fOutputList->Add(fHistMCTrackParticle);
     fOutputList->Add(fHistMCPtRes);
+    fOutputList->Add(fHistMCRelPtReso);
     fOutputList->Add(fHistMCEtaRes);
 
     fOutputList->Add(fHistMCRecTrack);
@@ -448,6 +498,11 @@ void AliMeanPtAnalysisTask::UserExec(Option_t *){ // Main loop (called for each 
     Double_t trackValues[4] = {track->Pt(), track->Eta(), multAccTracks, centrality};
     fHistTrack->Fill(trackValues);
 
+
+    ///TODO this only works for ESD tracks!!
+    Double_t ptResoValues[3] = {1./TMath::Abs(dynamic_cast<AliESDtrack*>(track)->GetSigned1Pt())*TMath::Sqrt(dynamic_cast<AliESDtrack*>(track)->GetSigma1Pt2()), track->Pt(), centrality};
+    fHistRelPtResoFromCov->Fill(ptResoValues);
+
     /// Find original particle in MC-Stack
     if(fIsMC){
       Int_t mcLabel = TMath::Abs(track->GetLabel()); // negative label means bad quality track
@@ -461,6 +516,9 @@ void AliMeanPtAnalysisTask::UserExec(Option_t *){ // Main loop (called for each 
 
       if(!IsParticleInKinematicRange(mcParticle)) continue;
       multRecPart++;
+
+      Double_t relPtReso[4] = {TMath::Abs(track->Pt() - mcParticle->Pt())/mcParticle->Pt(), mcParticle->Pt(), track->Pt(), centrality};
+      fHistMCRelPtReso->Fill(relPtReso);
 
       if(fIncludeCrosscheckHistos){
         // Analogon to fHistTrack but with MC truth information
