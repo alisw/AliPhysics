176b85a1661078fda18e75df8950908cc124de8e
Youssef El Mard <yelmard@pcikf2.ikf.physik.uni-frankfurt.de>
2018-06-22 14:59:40 +0200

    AlidNdPtUnifiedTask:: remove useless lines for binning of pT. activate SetMaxChi2TPCConstrained(36.)

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTask_yelmardb_Unified.C
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTask_yelmardb_Unified.C
@@ -35,9 +35,7 @@ AlidNdPtUnifiedAnalysisTask* AddTask_yelmardb_Unified()
     // change pt binning
     const Int_t ptNbins = 81;
     Double_t bins[82] = {0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0, 30.0, 32.0, 34.0, 36.0, 40.0, 45.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 180.0, 200.0};
-    Double_t* binsPt = new Double_t[82];
-    for (int i=0; i<82; i++) {binsPt[i] = bins[i];}
-    task->SetBinsPt(ptNbins, binsPt);
+    task->SetBinsPt(81, bins);
 
     //TFile *trkEffFile = TFile::Open("$UNIFIED/macros/trkEffParametrisation.root");
     //if(!trkEffFile){printf("No trk eff file\n");}
@@ -77,7 +75,7 @@ AlidNdPtUnifiedAnalysisTask* AddTask_yelmardb_Unified()
     task->SetSigmaToVertex(kFALSE);
     task->SetDCAtoVertexZ(2.0);
     task->SetDCAtoVertexXYPtDep("0.0182+0.0350/pt^1.01");
-    //task->SetMaxChi2TPCConstrained(36.);
+    task->SetMaxChi2TPCConstrained(36.);
     task->SetMinLenghtInActiveZoneTPC(0);
     task->SetGeometricalCut(kTRUE,3,130,1.5,0.85,0.7); ///if kTRUE comment CrossedRowsTPC cut
    // task->SetMinCrossedRowsTPC(120);
