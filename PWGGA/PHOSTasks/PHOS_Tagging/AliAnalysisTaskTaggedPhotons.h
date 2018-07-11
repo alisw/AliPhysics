f95b6690d36e5b60771620f2c5cf25c62aa9e9e5
Dmitri Peresunko <Dmitri.Peresunko@cern.ch>
2018-07-11 13:10:01 +0300

    Multiple updates in Tagging class

--- PWGGA/PHOSTasks/PHOS_Tagging/AliAnalysisTaskTaggedPhotons.h
+++ PWGGA/PHOSTasks/PHOS_Tagging/AliAnalysisTaskTaggedPhotons.h
@@ -1,5 +1,5 @@
-#ifndef ALIANALYSISTASKTAGGEDPHOTONS_H
-#define ALIANALYSISTASKTAGGEDPHOTONS_H
+#ifndef ALIANALYSISTASKTAGGEDPHOTONSLOCAL_H
+#define ALIANALYSISTASKTAGGEDPHOTONSLOCAL_H
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice     */
 //______________________________________________________________________________
@@ -46,6 +46,10 @@ public:
   void SetPi0WeightParameters(TArrayD * ar) ;
   void SetDistanceToBad(Float_t cut=2.5){fMinBCDistance=cut;}
   void SetTimeCut(Float_t cut=25.e-9){fTimeCut=cut;}
+  void SetCentralityEstimator(Int_t est=1){fCentEstimator=est;}   //Centrality estimator, pPb: 1: V0A/C, 2: V0M, 3: ZNA/C,  4: CL1
+           //for pp: 
+  void SetCentralityWeights(TString filename="MBCentralityWeights.root") ;
+  void SetMultiplicityBins(TArrayI ar){fNCenBin=ar.GetSize() ; fCenBinEdges.Copy(ar);}
 
 protected:
   void    FillMCHistos() ;
@@ -63,15 +67,24 @@ protected:
   Bool_t  TestPID(Int_t iPID, AliCaloPhoton* part) ;
   Double_t PrimaryParticleWeight(AliAODMCParticle * particle) ;
   Int_t   FindPrimary(AliVCluster*, Bool_t&);
+  Double_t TrigCentralityWeight(Double_t x); //Correction for PHOS trigger centrality bias
+  Double_t MBCentralityWeight(Double_t x);   //Correction for Pileup cut centrality bias
+  
   void FillHistogram(const char * key,Double_t x) const ; //Fill 1D histogram witn name key
   void FillHistogram(const char * key,Double_t x, Double_t y) const ; //Fill 2D histogram witn name key
   void FillHistogram(const char * key,Double_t x, Double_t y, Double_t z) const ; //Fill 3D histogram witn name key
-  void FillPIDHistograms(const char * name, const AliCaloPhoton * p) const ;
-  void FillPIDHistograms(const char * name, const AliCaloPhoton * p ,Double_t y) const ;
-  void FillPIDHistograms(const char * name, const AliCaloPhoton * p , const AliCaloPhoton * p2,Double_t y, Bool_t isReal) const ;
+  void FillPIDHistograms(const char * name,  AliCaloPhoton * p) const ;
+  void FillPIDHistograms(const char * name,  AliCaloPhoton * p ,Double_t y) const ;
+  void FillPIDHistograms(const char * name,  AliCaloPhoton * p ,  AliCaloPhoton * p2,Double_t y, Bool_t isReal) const ;
+  Bool_t SelectCentrality(AliVEvent * event) ;
+
 
 private:
 
+  Int_t   fCentEstimator;       //Centrality estimator: 1: V0A/C, 2: V0M, 3: ZNA/C,  4: CL1
+  Int_t   fNCenBin ;     
+  TArrayI fCenBinEdges;   
+  
   AliPHOSGeometry  *fPHOSgeom;      //!PHOS geometry
   THashList *   fOutputContainer ;  //!List of output histograms
   TClonesArray *fStack ;            //!Pointer to MC stack (AOD)
@@ -94,12 +107,15 @@ private:
   //
   Double_t fRP;           //Reaction plane orientation
   Double_t fCentrality;
-  Int_t fCentBin ;
+  Double_t fCentWeight ;  //Weight to correct bias in PHOS triigeered events
+  Int_t  fCentBin ;
+  Int_t  fRunNumber ;      //! current run number
   Bool_t fIsMB ;          //which trigger to use
   Bool_t fIsMC ;          //Is this is MC
   Bool_t fIsFastMC;       //This is fast MC, bypass event checks
-  TH2I * fPHOSBadMap[6] ; 
+  TH2I * fPHOSBadMap[6] ;  
+  TH1F * fCentralityWeights[6]; //Weights to correct centrality non-flatness
     
-  ClassDef(AliAnalysisTaskTaggedPhotons, 5);   // a PHOS photon analysis task 
+  ClassDef(AliAnalysisTaskTaggedPhotons, 3);   // a PHOS photon analysis task 
 };
-#endif // ALIANALYSISTASKTAGGEDPHOTONS_H
+#endif // ALIANALYSISTASKTAGGEDPHOTONSLOCAL_H
