9caaa8a028e4ab36e68bca030b920900fa9d700b
Michael Oliver <michael.oliver@cern.ch>
2018-07-25 17:21:11 -0400

    Revert "Merge branch 'origin/master'"
    This reverts commit 3202b76d1daa8e9b754b158d528084a1fa56dc4d, reversing
    changes made to a9ee4415a579614f720a4a6f50fefc81e9bbd098.

--- PWG/Tools/yaml-cpp/yaml-cpp.pc.cmake
+++ PWG/Tools/yaml-cpp/yaml-cpp.pc.cmake
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
 includedir=${prefix}/@INCLUDE_INSTALL_ROOT_DIR@
 libdir=${exec_prefix}/@LIB_INSTALL_DIR@
-=======
-libdir=@LIB_INSTALL_DIR@
-includedir=@INCLUDE_INSTALL_ROOT_DIR@
->>>>>>> origin/master
 
 Name: Yaml-cpp
 Description: A YAML parser and emitter for C++
