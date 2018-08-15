ac9b862556b24b58aae2d019c15182820063fc65
wioleta rzesa <wrzesa@cern.ch>
2018-08-15 13:29:52 +0200

    Changes in new beta class -deuteros

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCutMonitorParticlePIDBeta.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoCutMonitorParticlePIDBeta.h
@@ -7,61 +7,32 @@
 #ifndef AliFemtoCutMonitorParticlePIDBeta_hh
 #define AliFemtoCutMonitorParticlePIDBeta_hh
 
-class AliFemtoEvent;
 class AliFemtoTrack;
-class AliFemtoV0;
-class AliFemtoKink;
-class AliFemtoPair; // Gael 12/04/02
-class TH1D;
 class TH2D;
 class TList;
 
 #include "AliFemtoString.h"
 #include "AliFemtoParticleCollection.h"
-#include "AliFemtoCutMonitor.h"
+#include "AliFemtoCutMonitorParticlePID.h"
 
-class AliFemtoCutMonitorParticlePIDBeta : public AliFemtoCutMonitor{
+class AliFemtoCutMonitorParticlePIDBeta : public AliFemtoCutMonitorParticlePID{
 
 public:
   AliFemtoCutMonitorParticlePIDBeta();
   AliFemtoCutMonitorParticlePIDBeta(const char *aName, Int_t aTOFParticle, Double_t yTOFTimeMin= -4000.0, Double_t yTOFTimeMax=4000.0);
-    AliFemtoCutMonitorParticlePIDBeta(const AliFemtoCutMonitorParticlePIDBeta &aCut);
+  AliFemtoCutMonitorParticlePIDBeta(const AliFemtoCutMonitorParticlePIDBeta &aCut);
   virtual ~AliFemtoCutMonitorParticlePIDBeta();
-
   AliFemtoCutMonitorParticlePIDBeta& operator=(const AliFemtoCutMonitorParticlePIDBeta& aCut);
 
-  virtual AliFemtoString Report();
-  virtual void Fill(const AliFemtoEvent* aEvent) {AliFemtoCutMonitor::Fill(aEvent);}
   virtual void Fill(const AliFemtoTrack* aTrack);
-  virtual void Fill(const AliFemtoV0* aV0) {AliFemtoCutMonitor::Fill(aV0);}
-  virtual void Fill(const AliFemtoXi* aXi) {AliFemtoCutMonitor::Fill(aXi);}
-  virtual void Fill(const AliFemtoKink* aKink) {AliFemtoCutMonitor::Fill(aKink);}
-  virtual void Fill(const AliFemtoPair* aPair) {AliFemtoCutMonitor::Fill(aPair);}
-  virtual void Fill(const AliFemtoParticleCollection* aCollection) {AliFemtoCutMonitor::Fill(aCollection);}
-  virtual void Fill(const AliFemtoEvent* aEvent,const AliFemtoParticleCollection* aCollection)
-  {AliFemtoCutMonitor::Fill(aEvent, aCollection);}
-  virtual void Fill(const AliFemtoParticleCollection* aCollection1,const AliFemtoParticleCollection* aCollection2) {AliFemtoCutMonitor::Fill(aCollection1, aCollection2);}
- /* void SetTOFParticle(Int_t ipart); */
-
   void Write();
-  void SetUsePt(Bool_t usept){fIfUsePt=usept;}
   virtual TList *GetOutputList();
 
 protected:
-  Int_t fTOFParticle; ///< Select TOF time hypothesis, 0-pion, 1-kaon, 2-proton
-  Bool_t fIfUsePt;    ///< Plot pT instead of p in all momentum histograms
 
-  TH2D *fTPCdEdx;        ///< TPC dEdx information
-  TH2D *fTOFTime;        ///< TOF time
-  TH2D *fTOFNSigma;      ///< TOF NSigma values vs mom
-  TH2D *fTPCNSigma;      ///< TPC NSigma values vs mom
-  TH2D *fTPCTOFNSigma;   ///< TPC^2+ TOF^2 NSigma values vs mom
-  TH2D *fTPCvsTOFNSigma; ///< TPC vs TOF
   TH2D *fBeta;           ///< Beta
   TH2D *fMass;           ///< Mass
 
-  TH1D *fParticleOrigin; ///< particle origin from MC
-  TH1D *fParticleId;     ///< true particle identification from MC
 };
 
 #endif
