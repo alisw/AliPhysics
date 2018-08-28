f2669ca9545df116a5e10c796903dd9355999918
Contantin Loizides <constantin.loizides@cern.ch>
2018-08-28 11:41:18 +0200

    make it possible to run the generate inside the task with several settings of coalescence parameters

--- PWGLF/NUCLEX/Nuclei/NucleiKine/AddTaskNucleiKineCor.C
+++ PWGLF/NUCLEX/Nuclei/NucleiKine/AddTaskNucleiKineCor.C
@@ -1,21 +1,22 @@
-AliAnalysisTaskNucleiKineCor* AddTaskNucleiKineCor(Double_t trigpt=5, TString contname="cor")
+AliAnalysisTaskNucleiKineCor* AddTaskNucleiKineCor(Double_t trigpt=5, Double_t p0=0, TString contname="cor")
 {
   AliAnalysisTaskNucleiKineCor *task = new AliAnalysisTaskNucleiKineCor();
   task->SetPt(trigpt);
+  task->SetP0(p0);
 
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
     cout<<"AliAnalysisTaskNucleiKine: No analysis manager to connect to."<<endl;
     return NULL;
   }
-  task->SetName(Form("task_%s_%.1f",contname.Data(),trigpt));
+  task->SetName(Form("task_%s_%.1f_%.1f",contname.Data(),trigpt,p0));
   mgr->AddTask(task);
 
   // Create containers for input/output
   TString outputFileName = AliAnalysisManager::GetCommonFileName();
 
   AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
-  AliAnalysisDataContainer *coutput = mgr->CreateContainer(Form("output_%s_%.1f",contname.Data(),trigpt), 
+  AliAnalysisDataContainer *coutput = mgr->CreateContainer(Form("output_%s_%.1f_%.1f",contname.Data(),trigpt,p0), 
 							   TList::Class(), AliAnalysisManager::kOutputContainer, outputFileName);
 
   //connect containers
