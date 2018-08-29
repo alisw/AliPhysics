1195e86aae5ff6aeb017ab1ed01b4dc1aeecd0b0
jbuxton <jesse.thomas.buxton@cern.ch>
2018-08-29 10:01:13 -0400

    PWGCF: AliFemtoUser/AliFemtoCutMonitorV0CosPointingAngle: Added very brief statement describing purpose of class

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoCutMonitorV0CosPointingAngle.h
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoCutMonitorV0CosPointingAngle.h
@@ -1,4 +1,7 @@
 /// \file AliFemtoCutMonitorV0CosPointingAngle
+// Purpose is to use 2d fCosPointingAnglewParentInfo with MC data, project out different
+// contributions, and do template fit to experimental data to determine ratio of primary Lambda to
+// Lambdas from secondary sources
 
 #ifndef AliFemtoCutMonitorV0CosPointingAngle_H
 #define AliFemtoCutMonitorV0CosPointingAngle_H
