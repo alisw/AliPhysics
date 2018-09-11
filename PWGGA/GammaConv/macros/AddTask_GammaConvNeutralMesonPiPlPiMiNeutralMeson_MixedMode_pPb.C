0c0465cfdda5937792a36d7bc23095f4cd0e6109
Remco de Boer <r.e.deboer@students.uu.nl>
2018-09-11 15:34:24 +0200

    PWGGA/GammaConv: changed isHeavyIon and cutnumberEvent for pPb

--- PWGGA/GammaConv/macros/AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_MixedMode_pPb.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_MixedMode_pPb.C
@@ -1,7 +1,7 @@
 /**************************************************************************
  * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  *                                                                        *
- * Author: Remco de Boer                                                  *
+ * Author: Remco de Boer, Nicolas Schmidt                                 *
  * Version 1.0                                                            *
  *                                                                        *
  *                                                                        *
@@ -95,7 +95,7 @@ void AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_MixedMode_pPb(
     cout << "INFO: AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_MixedMode_pPb running additionalTrainConfig '" << sAdditionalTrainConfig.Atoi() << "', train config: '" << trainConfig << "'" << endl;
   }
 
-  Int_t isHeavyIon = 0;
+  Int_t isHeavyIon = 2;
   Int_t neutralPionMode = 1;
 
   // ================== GetAnalysisManager ===============================
@@ -123,7 +123,7 @@ void AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_MixedMode_pPb(
         periodNameV0Reader.CompareTo("LHC16P1JJLowB") == 0 || periodNameV0Reader.CompareTo("LHC16P1Pyt8LowB") == 0 )
     cutnumberPhoton         = "00000088400000000100000000";
 
-  TString cutnumberEvent = "00000003";
+  TString cutnumberEvent = "80000003";
   TString PionCuts      = "000000200";            //Electron Cuts
 
 
