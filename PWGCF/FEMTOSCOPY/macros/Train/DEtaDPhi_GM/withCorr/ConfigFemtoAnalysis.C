ac0c13287a2fa24bbd56fc162818a31a2ade260e
Gabriela Moryc <gmoryc@cern.ch>
2018-09-12 15:52:41 +0200

    Changes in config.

--- PWGCF/FEMTOSCOPY/macros/Train/DEtaDPhi_GM/withCorr/ConfigFemtoAnalysis.C
+++ PWGCF/FEMTOSCOPY/macros/Train/DEtaDPhi_GM/withCorr/ConfigFemtoAnalysis.C
@@ -580,7 +580,7 @@ AliFemtoManager* ConfigFemtoAnalysis(const char* params) {
 					if(ifMonitors)//ichg>8)
 					  {
 
-					    if(1){
+					    if(0){
 					      // //V0 monitors (memory leak problems?)
 					      cutPass1V0[aniter] = new AliFemtoCutMonitorV0(Form("cutPass1%stpcM%i", chrgs[ichg], imult));
 					      cutFail1V0[aniter] = new AliFemtoCutMonitorV0(Form("cutFail1%stpcM%i", chrgs[ichg], imult));
