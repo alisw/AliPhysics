cf26d55d7358c7e390d9f34e7eb9eb6b0e2dee40
zampolli <Chiara.Zampolli@cern.ch>
2018-09-14 10:50:46 +0200

    Setting MirrorSE from evn variable

--- TPC/TPCcalib/AliTPCDcalibRes.cxx
+++ TPC/TPCcalib/AliTPCDcalibRes.cxx
@@ -713,7 +713,7 @@ void AliTPCDcalibRes::Init()
   //
   AliCDBManager* man = AliCDBManager::Instance();
   // adding mirroring for OCDB files that will be uploaded (Vdrift)
-  TString mirrorsStr("ALICE::CERN::OCDB,ALICE::FZK::SE,ALICE::LLNL::SE");
+  TString mirrorsStr(gSystem->Getenv("MIRRORSE") ? gSystem->Getenv("MIRRORSE") : "ALICE::CERN::OCDB,ALICE::FZK::SE,ALICE::CNAF::SE");
   AliCDBManager::Instance()->SetMirrorSEs(mirrorsStr.Data());
   printf("List of mirror SEs set to: \"%s\"\n",mirrorsStr.Data());
 
