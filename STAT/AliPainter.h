miranov25/AliRoot

ATO-418: AliDrawStyle, AliPainter, AliParser     AliParser        New clearly names of methods;        Added new functions:          Slice;          Slice2Matrix;        Doxygen documentation wrote;        Tests added;     AliPainter:        Rewrote according with new functions from AliParser;        Bug with numbers in limits was fixed;        Global maps and variables were change to local;     AliDrawStyle:        Bug fixes and optimisations;
Code was installed without error. All tests passed.

STAT/AliPainter.h
@@ -1,26 +1,6 @@
 #ifndef ALIPAINTER_H
 #define ALIPAINTER_H
 
-/*
- gSystem->AddIncludePath("-I$AliRoot_SRC/STAT");
-.L $AliRoot_SRC/STAT/AliPainter.cxx+
-
-TFile::SetCacheFileDir(".");
-TFile *finput = TFile::Open("http://aliqatrkeos.web.cern.ch/aliqatrkeos/performance/AliPainterTest.root","CACHEREAD");
-TTree *tree = (TTree *) finput->Get("hisPtAll");
-hisArray = new TObjArray();
-TList *keys = finput->GetListOfKeys();
-for (Int_t iKey = 0; iKey<keys->GetEntries();iKey++) {
-TObject *o = finput->Get(TString::Format("%s;%d", keys->At(iKey)->GetName(), ((TKey *) keys->At(iKey))->GetCycle()).Data());
-hisArray->AddLast(o);
-}
-TCanvas *canvasQA1 = new TCanvas("canvasQA", "canvasQA", 1200, 800);
-AliPainter::DivideTPad(canvasQA1, "<horizontal>[1]", "Canvas1");
-canvasQA1->cd(1);
-THnBase *hisN = hisArray->FindObject("hisK0DMassQPtTgl");
-AliPainter::DrawHistogram(hisN, "hisName()(1)(name=gaus,option=W)(class=Raw,drawOpt=E)")
-
-*/
 /// \ingroup STAT
 /// \class AliPainter
 /*!
@@ -28,6 +8,17 @@ AliPainter::DrawHistogram(hisN, "hisName()(1)(name=gaus,option=W)(class=Raw,draw
 *  See the documentation in describing of functions.
 * \author  <a href="marian.ivanov@cern.ch">Marian  Ivanov</a> , <a href="boris.rumyantsev@cern.ch">Boris Rumyantsev</a>
 */
+
+
+// TODO: add divFlag for inheritance Mother Class (ClassName) should be add to children (className)=>nameOfObject.class(ClassMother,ClassDaughter) @Boris
+// TODO: perhaps, we should use some aliases? @Marian
+// TODO: may be we should also use TVectorD or TMatrix instead enumeration? @Boris
+// TODO: extend to TH2D, TH3D @Boris
+// TODO: think how to combine  AliPainter::ParseString and AliPainter::ParseOptionString @Boris
+// NOTE: we can provide list of brackets and remove ignoreBrackets from arguments. mb just add flag about do you want to ignore brackets or not.
+// TODO - now we have 2 steps for parsing of ranges option to array of string with simple range values. 1. Initial string into map. 2. Map into array fo strings. First of all we can use vector of vector instead map, and the second we should think how we can avoid this 2 intermediate steps with map. @Boris
+// TODO: add few classes with []
+// TODO: change global maps to arguments of functions
 
 #include <vector>
 #include <map>
@@ -39,41 +30,30 @@ AliPainter::DrawHistogram(hisN, "hisName()(1)(name=gaus,option=W)(class=Raw,draw
 #include "TFormula.h"
 #include "TObject.h"
 #include "TLegend.h"
+#include "AliParser.h"
 
-class AliPainter : public TObject {
+class AliPainter {
   public:
     static TPad *DivideTPad(TPad *pad, const char *division, const char *classID="", const char *style="", Int_t verbose=0);
     static void SetMultiGraphTimeAxis(TMultiGraph *graph, TString option);
     static TPad *SetPadMargin(TPad *cPad, const char *position, const char *wMargin, const char *units, Double_t mValue, Int_t iCol, Int_t nCols);
     static TObjArray *PrepareHistogram(THnBase *hisN, const char *expression, TObjArray *&keepArray, TObjArray *metaData=nullptr, Int_t verbose=0);
     static void DrawHistogram(THnBase *hisN, const char *expression, TPad *pad=nullptr, TObjArray *keepArray=nullptr, TObjArray *metaData=nullptr, Int_t verbose=0);
-    static void DrawHistogram(const TObjArray *histogramArray, const char *expression, TPad *pad=nullptr, TObjArray *keepArray=nullptr, TObjArray *metaData=nullptr, Int_t verbose=0);
+    static void DrawHistogram(TObjArray *histogramArray, const char *expression, TPad *pad=nullptr, TObjArray *keepArray=nullptr, TObjArray *metaData=nullptr, Int_t verbose=0);
     static TPad *GetNextPad(TPad *cPad, TPad *tempPad=nullptr, Int_t verbose=0);
-    typedef std::map<Int_t, std::vector<TString> > axisRangesMap;
-    static std::map<TString, TString> drawValues;
-    static std::map<TString, TString> fitValues;
-    static std::map<TString, TString> genValues;
-    static std::vector<TString> rangesVec;
-    static void FillAll(const char *, Int_t=0);
-    static void ParseRanges(const TString, Int_t=0);
     static void SaveToKeepArray(TObject *, TObjArray *&, Int_t=0);
     static void SaveToKeepArray(TObjArray *, TObjArray *&, Int_t=0);
-    static TObjArray *SetRanges(THnBase *, Int_t=0);
-    static TObject *SetProjections(THnBase *, Int_t=0);
+    static TObjArray *SetRanges(THnBase *, TString, Int_t=0);
+    static TObject *SetProjections(THnBase *, TString, Int_t=0);
 //    static TLegend BuildLegend(THnBase *, TObjArray *, Int_t=0);
     static Double_t *GetDataArray(TObjArray *, Long64_t &, Int_t=0);
-    static void SetLimits(TObjArray *&, Int_t=0);
+    static void SetLimits(TObjArray *&, TString, Int_t=0);
     static Double_t GetStatVal(Double_t *, Long64_t, const TString, Int_t=0);
     template <typename T>
-      static void SetFitter(T *&, Int_t=0);
+      static void SetFitter(T *&, std::map<TString, TString>, Int_t=0);
     template <typename T>
-      static void SetDrawingOptions(T *&, Int_t=0);
-    static std::vector<TString> ParseString(const char *iString,  const char *sep="()", Int_t verbose=0);
-    static std::vector<TString> ParseOptionString(const char *optionsStr, Int_t defSize=0, const char sep=',', const char ignoreBrackets[2]="()", Int_t verbose=0);
-    static void ParsePandasString(const TString optionsStr, std::map<TString, TString> &optMap, const char sep=',', const char ignoreBrackets[2]="[]", Int_t verbose=0);
-    static void RegisterDefaultOptions();
-    static void RangesToMap (TString range , Int_t axisNum, axisRangesMap &result);
-    static void RangesMapToString(Int_t n, axisRangesMap result, std::vector<TString> arr, std::vector<TString> &res);
+      static void SetDrawingOptions(T *&, std::map<TString, TString>, Int_t=0);
     ClassDef(AliPainter,1);
+  private:
 };
 #endif

STAT/AliParser.h
@@ -1,0 +1,34 @@
+#ifndef ALIROOT_ALIPARSER_H
+#define ALIROOT_ALIPARSER_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+* See cxx source for full Copyright notice                               */
+
+/// \ingroup STAT
+/// \class AliParser
+/*!
+* \brief Class provides flexibility static methods for tokenizing strings.
+*  You can find explanation, details and examples in description of each function.
+* \author  <a href="mailto:marian.ivanov@cern.ch">Marian Ivanov</a>, <a href="mailto:boris.rumyantsev@cern.ch">Boris Rumyantsev</a>
+*/
+
+#include "TObject.h"
+#include "TString.h"
+#include "TMatrixD.h"
+#include <map>
+#include <vector>
+
+class AliParser {
+  public:
+    static std::vector<TString> Extract(const char *inputString, const char *startStr, const char *endStr, Int_t verbose=0);
+    static std::vector<TString> Split(const char *inputString, const char delimiter=',', Int_t verbose=0);
+    static std::vector<TString> Separate(const char *inputString, const char brackets[3]="()", Int_t verbose=0);
+    static std::map<TString, TString> Parse(const char *inputString, Int_t verbose=0, std::vector<TString> defKeys = std::vector<TString>{});
+    static  std::vector<Int_t> Slice(const char *inputString);
+    static TMatrixD Slice2Matrix(const char *inputString, Int_t verbose=0);
+  //private:
+    static void Map2Array (std::vector<Double_t> &array, std::map<Int_t, std::vector<Double_t> > iRanges, Int_t *indexes, Int_t cnt);
+    static void FillFloatFlagArray(std::vector<Double_t> &, TString);
+  ClassDef(AliParser,1);
+};
+#endif

alisw/AliPhysics

STAT/AliPainter.h
@@ -1,26 +1,6 @@
 #ifndef ALIPAINTER_H
 #define ALIPAINTER_H
 
-/*
- gSystem->AddIncludePath("-I$AliRoot_SRC/STAT");
-.L $AliRoot_SRC/STAT/AliPainter.cxx+
-
-TFile::SetCacheFileDir(".");
-TFile *finput = TFile::Open("http://aliqatrkeos.web.cern.ch/aliqatrkeos/performance/AliPainterTest.root","CACHEREAD");
-TTree *tree = (TTree *) finput->Get("hisPtAll");
-hisArray = new TObjArray();
-TList *keys = finput->GetListOfKeys();
-for (Int_t iKey = 0; iKey<keys->GetEntries();iKey++) {
-TObject *o = finput->Get(TString::Format("%s;%d", keys->At(iKey)->GetName(), ((TKey *) keys->At(iKey))->GetCycle()).Data());
-hisArray->AddLast(o);
-}
-TCanvas *canvasQA1 = new TCanvas("canvasQA", "canvasQA", 1200, 800);
-AliPainter::DivideTPad(canvasQA1, "<horizontal>[1]", "Canvas1");
-canvasQA1->cd(1);
-THnBase *hisN = hisArray->FindObject("hisK0DMassQPtTgl");
-AliPainter::DrawHistogram(hisN, "hisName()(1)(name=gaus,option=W)(class=Raw,drawOpt=E)")
-
-*/
 /// \ingroup STAT
 /// \class AliPainter
 /*!
@@ -28,6 +8,17 @@ AliPainter::DrawHistogram(hisN, "hisName()(1)(name=gaus,option=W)(class=Raw,draw
 *  See the documentation in describing of functions.
 * \author  <a href="marian.ivanov@cern.ch">Marian  Ivanov</a> , <a href="boris.rumyantsev@cern.ch">Boris Rumyantsev</a>
 */
+
+
+// TODO: add divFlag for inheritance Mother Class (ClassName) should be add to children (className)=>nameOfObject.class(ClassMother,ClassDaughter) @Boris
+// TODO: perhaps, we should use some aliases? @Marian
+// TODO: may be we should also use TVectorD or TMatrix instead enumeration? @Boris
+// TODO: extend to TH2D, TH3D @Boris
+// TODO: think how to combine  AliPainter::ParseString and AliPainter::ParseOptionString @Boris
+// NOTE: we can provide list of brackets and remove ignoreBrackets from arguments. mb just add flag about do you want to ignore brackets or not.
+// TODO - now we have 2 steps for parsing of ranges option to array of string with simple range values. 1. Initial string into map. 2. Map into array fo strings. First of all we can use vector of vector instead map, and the second we should think how we can avoid this 2 intermediate steps with map. @Boris
+// TODO: add few classes with []
+// TODO: change global maps to arguments of functions
 
 #include <vector>
 #include <map>
@@ -39,41 +30,31 @@ AliPainter::DrawHistogram(hisN, "hisName()(1)(name=gaus,option=W)(class=Raw,draw
 #include "TFormula.h"
 #include "TObject.h"
 #include "TLegend.h"
+#include "AliParser.h"
 
-class AliPainter : public TObject {
+class AliPainter {
   public:
     static TPad *DivideTPad(TPad *pad, const char *division, const char *classID="", const char *style="", Int_t verbose=0);
     static void SetMultiGraphTimeAxis(TMultiGraph *graph, TString option);
-    static TPad *SetPadMargin(TPad *cPad, const char *position, const char *wMargin, const char *units, Double_t mValue, Int_t iCol, Int_t nCols);
-    static TObjArray *PrepareHistogram(THnBase *hisN, const char *expression, TObjArray *&keepArray, TObjArray *metaData=nullptr, Int_t verbose=0);
     static void DrawHistogram(THnBase *hisN, const char *expression, TPad *pad=nullptr, TObjArray *keepArray=nullptr, TObjArray *metaData=nullptr, Int_t verbose=0);
-    static void DrawHistogram(const TObjArray *histogramArray, const char *expression, TPad *pad=nullptr, TObjArray *keepArray=nullptr, TObjArray *metaData=nullptr, Int_t verbose=0);
+    static void DrawHistogram(TObjArray *histogramArray, const char *expression, TPad *pad=nullptr, TObjArray *keepArray=nullptr, TObjArray *metaData=nullptr, Int_t verbose=0);
     static TPad *GetNextPad(TPad *cPad, TPad *tempPad=nullptr, Int_t verbose=0);
-    typedef std::map<Int_t, std::vector<TString> > axisRangesMap;
-    static std::map<TString, TString> drawValues;
-    static std::map<TString, TString> fitValues;
-    static std::map<TString, TString> genValues;
-    static std::vector<TString> rangesVec;
-    static void FillAll(const char *, Int_t=0);
-    static void ParseRanges(const TString, Int_t=0);
+  private:
     static void SaveToKeepArray(TObject *, TObjArray *&, Int_t=0);
     static void SaveToKeepArray(TObjArray *, TObjArray *&, Int_t=0);
-    static TObjArray *SetRanges(THnBase *, Int_t=0);
-    static TObject *SetProjections(THnBase *, Int_t=0);
+    static TPad *SetPadMargin(TPad *cPad, const char *position, const char *wMargin, const char *units, Double_t mValue, Int_t iCol, Int_t nCols);
+    static TObjArray *PrepareHistogram(THnBase *hisN, const char *expression, TObjArray *&keepArray, TObjArray *metaData=nullptr, Int_t verbose=0);
+    static TObjArray* SliceHistogram(THnBase *, TString, Int_t=0);
+    static TObject *SetProjections(THnBase *, TString, Int_t=0);
 //    static TLegend BuildLegend(THnBase *, TObjArray *, Int_t=0);
     static Double_t *GetDataArray(TObjArray *, Long64_t &, Int_t=0);
-    static void SetLimits(TObjArray *&, Int_t=0);
+    static void SetLimits(TObjArray *&, TString, Int_t=0);
     static Double_t GetStatVal(Double_t *, Long64_t, const TString, Int_t=0);
     template <typename T>
-      static void SetFitter(T *&, Int_t=0);
+      static void FitHistogram(T *&, std::map<TString, TString>, Int_t=0);
     template <typename T>
-      static void SetDrawingOptions(T *&, Int_t=0);
-    static std::vector<TString> ParseString(const char *iString,  const char *sep="()", Int_t verbose=0);
-    static std::vector<TString> ParseOptionString(const char *optionsStr, Int_t defSize=0, const char sep=',', const char ignoreBrackets[2]="()", Int_t verbose=0);
-    static void ParsePandasString(const TString optionsStr, std::map<TString, TString> &optMap, const char sep=',', const char ignoreBrackets[2]="[]", Int_t verbose=0);
-    static void RegisterDefaultOptions();
-    static void RangesToMap (TString range , Int_t axisNum, axisRangesMap &result);
-    static void RangesMapToString(Int_t n, axisRangesMap result, std::vector<TString> arr, std::vector<TString> &res);
+      static void SetDrawingOptions(T *&, std::map<TString, TString>, Int_t=0);
     ClassDef(AliPainter,1);
+  private:
 };
 #endif

STAT/AliParser.h
@@ -1,0 +1,34 @@
+#ifndef ALIROOT_ALIPARSER_H
+#define ALIROOT_ALIPARSER_H
+
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+* See cxx source for full Copyright notice                               */
+
+/// \ingroup STAT
+/// \class AliParser
+/*!
+* \brief Class provides flexibility static methods for tokenizing strings.
+*  You can find explanation, details and examples in description of each function.
+* \author  <a href="mailto:marian.ivanov@cern.ch">Marian Ivanov</a>, <a href="mailto:boris.rumyantsev@cern.ch">Boris Rumyantsev</a>
+*/
+
+#include "TObject.h"
+#include "TString.h"
+#include "TMatrixD.h"
+#include <map>
+#include <vector>
+
+class AliParser {
+  public:
+    static std::vector<TString> ExtractBetween(const char *inputString, const char *startStr, const char *endStr, Int_t verbose=0);
+    static std::vector<TString> Split(const char *inputString, const char delimiter=',', Int_t verbose=0);
+    static std::vector<TString> ExtractSurroundingBy(const char *inputString, const char begin='(', const char end = ')', Int_t verbose=0);
+    static std::map<TString, TString> Parse(const char *inputString, Int_t verbose=0, std::vector<TString> defKeys = std::vector<TString>{});
+    static  std::vector<Int_t> Slice2IArray(const char *inputString);
+    static TMatrixD Slice2Matrix(const char *inputString, Int_t verbose=0);
+  //private:
+    static void Map2Array (std::vector<Double_t> &array, std::map<Int_t, std::vector<Double_t> > iRanges, Int_t *indexes, Int_t cnt);
+    static void FillFloatFlagArray(std::vector<Double_t> &, TString);
+  ClassDef(AliParser,1);
+};
+#endif