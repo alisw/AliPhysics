dac409cf402454c3c143beab00c4a1e5fa370e5d
Francesco Prino <prino@to.infn.it>
2018-09-21 15:43:49 +0200

    Add options for multiplicity estimators

--- PWGPP/global/AliAnalysisTaskTrackingEffPID.h
+++ PWGPP/global/AliAnalysisTaskTrackingEffPID.h
@@ -46,6 +46,10 @@ public:
   void SetPrimarySelectionOption(Int_t opt){
     fPrimarySelectionOpt=opt;
   }
+  void SetTrackletMultiplicityEstimator(){ fMultEstimator=0;}
+  void SetVertexContribMultiplicityEstimator(){ fMultEstimator=1;}
+  void SetTracksMultiplicityEstimator(){ fMultEstimator=2;}
+
   void SetCollisionSystem(TString collsy){
     collsy.ToLower();
     collsy.ReplaceAll("-","");
@@ -73,6 +77,7 @@ public:
   }
   void SetUseMVPileup(bool flag) {fEventCut.fPileUpCutMV=flag;}
 
+
   AliEventCuts  fEventCut;
 
 
@@ -83,6 +88,7 @@ private:
   bool fUseTrackCutsForAOD;       /// flag to switch off/on fTrackCuts for AOD
   bool fUseGeneratedKine;         /// flag to use the generated pt, eta phi
   int  fPrimarySelectionOpt;      /// 0=no selection, 1=IsPhysicalPrimary, 2= cut on the origin
+  int  fMultEstimator;            /// multiplicity estimator: 0=trackelts, 1=ITS+TPCtracks, 2=primary vertex contributors
   bool fIsAA;                     /// flag to control collision system
   int  fFilterBit;                /// filter-bit selection for AOD tracks
   AliESDtrackCuts* fTrackCuts;                            /// cut object
@@ -97,7 +103,7 @@ private:
 
 
   /// \cond CLASSDEF
-  ClassDef(AliAnalysisTaskTrackingEffPID, 2);
+  ClassDef(AliAnalysisTaskTrackingEffPID, 3);
   /// \endcond
 };
 
