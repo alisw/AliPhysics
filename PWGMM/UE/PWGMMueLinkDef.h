fab273d83670458a13e31e28a94afd7fcef46eab
Antonio Ortiz <aortizve@cern.ch>
2018-09-26 11:53:05 -0500

    Harmonization of the analysis task to follow alice conventions

--- PWGMM/UE/PWGMMueLinkDef.h
+++ PWGMM/UE/PWGMMueLinkDef.h
@@ -1,6 +1,6 @@
 #ifdef __CINT__
 
 //underlying event using transverse spherocity
-#pragma link C++ class AliAnalysisUeSpherocityTask+;
+#pragma link C++ class AliAnalysisTaskUeSpherocity+;
 
 #endif
