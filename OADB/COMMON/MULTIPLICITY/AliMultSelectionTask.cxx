c305ad2f3cdfbc87aa7ce265f151d6ca384f608a
Ionut Arsene <iarsene@cern.ch>
2018-10-10 14:45:19 +0200

    Added OADB object for multiplicity calibration corresponding to period LHC18j

--- OADB/COMMON/MULTIPLICITY/AliMultSelectionTask.cxx
+++ OADB/COMMON/MULTIPLICITY/AliMultSelectionTask.cxx
@@ -2663,6 +2663,7 @@ TString AliMultSelectionTask::GetPeriodNameByRunNumber() const
     if ( fCurrentRun >= 288619 && fCurrentRun <= 288750 ) lProductionName = "LHC18g";
     if ( fCurrentRun >= 288804 && fCurrentRun <= 288806 ) lProductionName = "LHC18h";
     if ( fCurrentRun >= 288861 && fCurrentRun <= 288909 ) lProductionName = "LHC18i";
+    if ( fCurrentRun >= 288943 && fCurrentRun <= 288943 ) lProductionName = "LHC18j";
     
     //Registered Productions : Run 2 Pb-Pb
     if ( fCurrentRun >= 243395 && fCurrentRun <= 243984 ) lProductionName = "LHC15m";
@@ -2751,6 +2752,7 @@ TString AliMultSelectionTask::GetSystemTypeByRunNumber() const
     if ( fCurrentRun >= 288619 && fCurrentRun <= 288750 ) lSystemType = "pp";
     if ( fCurrentRun >= 288804 && fCurrentRun <= 288806 ) lSystemType = "pp";
     if ( fCurrentRun >= 288861 && fCurrentRun <= 288909 ) lSystemType = "pp";
+    if ( fCurrentRun >= 288943 && fCurrentRun <= 288943 ) lSystemType = "pp";
     
     //Registered Productions : Run 2 Pb-Pb
     if ( fCurrentRun >= 243395 && fCurrentRun <= 243984 ) lSystemType = "Pb-Pb";
