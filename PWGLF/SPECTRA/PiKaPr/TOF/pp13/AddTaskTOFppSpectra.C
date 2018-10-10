43c009204f8ec72cd6868c11fd10b0641fd91e83
pranjal <pranjal.sarma@cern.ch>
2018-10-10 16:55:16 +0530

    Modification to set slope

--- PWGLF/SPECTRA/PiKaPr/TOF/pp13/AddTaskTOFppSpectra.C
+++ PWGLF/SPECTRA/PiKaPr/TOF/pp13/AddTaskTOFppSpectra.C
@@ -1,4 +1,4 @@
-AliAnalysisTaskTOFppSpectra *AddTaskTOFppSpectra(const char *PeriodName=NULL, Int_t nTPC_CR=70, Int_t Chi2_TPCcluser=4, Int_t DCAz=2, Int_t DCAxy=7, Int_t value_Sigma=80, Float_t value_Slope=0.0)
+AliAnalysisTaskTOFppSpectra *AddTaskTOFppSpectra(const char *PeriodName=NULL, Int_t nTPC_CR=70, Int_t Chi2_TPCcluser=4, Int_t DCAz=2, Int_t DCAxy=7, Int_t value_Sigma=80, Int_t value_Slope=0)
 {
 // Creates, configures and attaches to the train a cascades check task.
    // Get the pointer to the existing analysis manager via the static access method.
@@ -46,9 +46,9 @@ AliAnalysisTaskTOFppSpectra *AddTaskTOFppSpectra(const char *PeriodName=NULL, In
 
   TString outputFileName = AliAnalysisManager::GetCommonFileName();
    
-	if(value_Slope==0.0)	Double_t slope=0.0125;
-	else if(value_Slope==+1.0)	Double_t slope=0.01375;
-	else if(value_Slope==-1.0)	Double_t slope=0.01125;
+	if (value_Slope==0)	Float_t slope=0.0125;
+	else if (value_Slope==+1)	Float_t slope=0.01375;
+	else if (value_Slope==-1)	Float_t slope=0.01125;
 
   outputFileName += ":Data_PWGLF_pp13VsMult";
   TString OutputListname = Form("fOutputList_CR%i_Chi2TPCcluster%i_DCAz%i_DCAxy%iSigma_PID_Sigma%i_Slope%f",nTPC_CR, Chi2_TPCcluser, DCAz, DCAxy, value_Sigma, slope);
