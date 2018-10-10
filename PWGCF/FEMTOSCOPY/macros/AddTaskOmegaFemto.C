abf99e408de9fea3698ab3b7b0e34b91a7577f82
Andreas Mathis <andreas.mathis@cern.ch>
2018-10-10 20:03:06 +0300

    By default disable delta eta delta phi selection

--- PWGCF/FEMTOSCOPY/macros/AddTaskOmegaFemto.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskOmegaFemto.C
@@ -24,7 +24,7 @@ AliAnalysisTaskSE* AddTaskOmegaFemto(
     bool RunNumberQA=false,//18
     int FilterBit=128,//19
     bool InvMassPairs=false, //20
-    bool DeltaEtaDeltaPhiCut = true)//21
+    bool DeltaEtaDeltaPhiCut = false)//21
 {
   // 1    2     3     4     5     6     7    8    9      10   11     12   13    14    15    16   17
   //true,true,false,false,false,false,false,true,false,false,true,false,true,false,false,false,true
