0fd83745fd3c8b94044390dd2fd20106e331ec46
Marian Ivanov <marian.ivanov@cern.ch>
2018-10-31 12:16:03 +0100

    ATO-452, ATO-15 : adding AliOCDBtoolkit::DumpOCDBFile - from snapshot Int_t  AliOCDBtoolkit::DumpOCDBFile(const char *fileName, const char*objectName, const char *foutput, Bool_t dumpMetaData, TString  printOption){ */// \param fileName         - OCDB snapshot file name .e.g OCDBsim.root or OCDBrec.root */// \param objectName       - OCDB entry name  e.g TPC/Calib/recoParam */// \param foutput          - output file name - e.g TPC_Calib_RecoParam.xml */// \param dumpMetaData     - switch dump also metadata */// \param printOption      - MI, xml, docdb (obejct dump) , pocdb (object print) Example usage: ```` AliOCDBtoolkit::DumpOCDBFile("/lustre/nyx/alice/users/miranov/NOTESData/alice-tpc-notes/JIRA/ALIROOT-7077/test1211_PbPb_MCTail_2_2/OCDB/OCDBrec.root", "TPC/Calib/RecoParam", "TPC_Calib_RecoParamReco.xml",1,"XML") ````
    ATO-452, ATO-15 : adding AliOCDBtoolkit::DumpOCDBFile - from snapshot
    Int_t  AliOCDBtoolkit::DumpOCDBFile(const char *fileName, const char*objectName, const char *foutput, Bool_t dumpMetaData, TString  printOption){
    */// \param fileName         - OCDB snapshot file name .e.g OCDBsim.root or OCDBrec.root
    */// \param objectName       - OCDB entry name  e.g TPC/Calib/recoParam
    */// \param foutput          - output file name - e.g TPC_Calib_RecoParam.xml
    */// \param dumpMetaData     - switch dump also metadata
    */// \param printOption      - MI, xml, docdb (obejct dump) , pocdb (object print)
    Example usage:
    ````
    AliOCDBtoolkit::DumpOCDBFile("/lustre/nyx/alice/users/miranov/NOTESData/alice-tpc-notes/JIRA/ALIROOT-7077/test1211_PbPb_MCTail_2_2/OCDB/OCDBrec.root", "TPC/Calib/RecoParam", "TPC_Calib_RecoParamReco.xml",1,"XML")
    ````

--- STEER/CDB/AliOCDBtoolkit.cxx
+++ STEER/CDB/AliOCDBtoolkit.cxx
@@ -661,6 +661,63 @@ void AliOCDBtoolkit::DumpOCDB(const TMap *cdbMap0, const TList *cdbList0, const 
 //==================================================================================================== 
 
 
+/// \param fileName         - OCDB snapshot file name .e.g OCDBsim.root or OCDBrec.root
+/// \param objectName       - OCDB entry name  e.g TPC/Calib/recoParam
+/// \param foutput          - output file name - e.g TPC_Calib_RecoParam.xml
+/// \param dumpMetaData     - switch dump also metadata
+/// \param printOption      - MI, xml, docdb (obejct dump) , pocdb (object print)
+/// Example usage:
+/// AliOCDBtoolkit::DumpOCDBFile("/lustre/nyx/alice/users/miranov/NOTESData/alice-tpc-notes/JIRA/ALIROOT-7077/test1211_PbPb_MCTail_2_2/OCDB/OCDBrec.root", "TPC/Calib/RecoParam", "TPC_Calib_RecoParamReco.xml",1,"XML")
+///
+Int_t  AliOCDBtoolkit::DumpOCDBFile(const char *fileName, const char*objectName, const char *foutput, Bool_t dumpMetaData, TString  printOption){
+  TString optionString = printOption;
+  optionString.ToLower();
+  TFile * f = TFile::Open(fileName);
+  if (f== nullptr || objectName== nullptr) {
+    ::Error("AliOCDBtoolkit::DumpOCDBFile","File %s does not exist or invalid object name",fileName);
+    return 1;  // return error code one  - file not found
+  }
+  AliCDBEntry *entry = (AliCDBEntry*)f->Get(TString(objectName).ReplaceAll("/","*").Data());
+  if (entry== nullptr){
+    ::Error("AliOCDBtoolkit::DumpOCDBFile","Object %s does not exist in File %s",objectName, fileName);
+    return 1;  // return error code one  - file not found
+  }
+  TObject *obj = entry->GetObject();
+  TFile * fout = TFile::Open(foutput,"new");
+  // print opt ion indicated ()
+
+  if (optionString.Contains("pocdb")){
+    if (dumpMetaData) gROOT->ProcessLine(TString::Format("((TObject*)%p)->Dump(); >%s",entry, foutput).Data());
+    if (!obj) return 4;
+    gROOT->ProcessLine(TString::Format("((TObject*)%p)->Print(\"%s\"); >>%s",obj, printOption.Data(), foutput).Data());
+    return 0;
+  }
+  if (optionString.Contains("docdb")){
+    if (dumpMetaData) gROOT->ProcessLine(TString::Format("((TObject*)%p)->Dump(); >%s",entry, foutput).Data());
+    if (!obj) return 4;
+    gROOT->ProcessLine(TString::Format("((TObject*)%p)->Dump(); >>%s",obj, foutput).Data());
+    return 0;
+  }
+//
+  if (optionString.Contains("mi")){
+    if (dumpMetaData) gROOT->ProcessLine(TString::Format("((TObject*)%p)->Dump(); >%s",entry, foutput).Data());
+    if (!obj) return 4;
+    gROOT->ProcessLine(TString::Format("AliOCDBtoolkit::DumpObjectRecursive((TObject*)%p); >>%s",obj, foutput).Data());
+    return 0;
+  }
+  if (optionString.Contains("xml")){
+    TFile * f = TFile::Open(TString::Format("%s",foutput).Data(),"recreate");
+    if (dumpMetaData) entry->Write("AliCDBEntry");
+    else obj->Write("AliCDBEntry");
+    f->Close();
+    return 0;
+  }
+  ::Error("AliOCDBtoolkit::DumpOCDBFile","Not recognized option %s", optionString.Data());
+  return 2;
+}
+
+
+
 
 
 
