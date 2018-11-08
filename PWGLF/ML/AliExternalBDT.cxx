cb5cdbad5b68958f4ce64b7338914adeaa3e4af9
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2018-11-08 12:36:01 +0100

    Interface for generic BDT models in AliPhysics
    This commit, prepared with @pfecchio, introduces an interface to the
    treelite library. Treelite takes serialised models created by xgboost,
    lightgbm and scikit-learn and turns them in C code.
    The C code can then be compiled into a library that is used for
    inference.
    For the time being the interface allows the user to load a serialised
    model from LightGBM, xgboost or a compiled library generated locally.
    Local tests on a small dataset show that the inference with this
    interface and with xgboost (in python) gives exactly the same results.

---
+++ PWGLF/ML/AliExternalBDT.cxx
@@ -0,0 +1,144 @@
+// Copyright CERN. This software is distributed under the terms of the GNU
+// General Public License v3 (GPL Version 3).
+//
+// See http://www.gnu.org/licenses/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file AliExternalBDT.cxx
+/// \author maximiliano.puccio@cern.ch, pietro.fecchio@cern.ch
+
+#include "AliExternalBDT.h"
+
+#include <cassert>
+#include <stdio.h>
+#include <iostream>
+
+#include <TSystem.h>
+
+namespace {
+  inline bool checkFile (const std::string name) {
+    FILE *file = fopen(name.c_str(), "r");
+    if (file != NULL) {
+      fclose(file);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+AliExternalBDT::AliExternalBDT(std::string name) :
+  fBDTname{name},
+  fModel{},
+  fModelPath{""},
+  fModelName{""},
+  fCompiler{},
+  fPredictor{}
+{
+}
+
+
+bool AliExternalBDT::CompileAndLoadModelLibrary() {
+  std::string path = GetUniquePath();
+  if (checkFile(path + "/main.so")) {
+    std::cout << "Library found: " << path.data() << "/main.so . Loading it!" << std::endl;
+  } else {
+    std::cout << "Starting the model compilation, depending on the model size it can take a while..." << std::endl;
+    gSystem->Exec(Form("gcc -c -O1 -fPIC %s/main.c -o %s/main.o \
+          && gcc -shared %s/main.o -o %s/main.so", path.data(), path.data(), path.data(), path.data()));
+  }
+  return LoadModelLibrary(Form("%s/main.so", path.data()));
+}
+
+bool AliExternalBDT::CreateModelCode() {
+  std::string path = GetUniquePath();
+  if (checkFile(path + "/main.c")) {
+    std::cout << "Code found: " << path.data() << "/main.c . \
+      Remove it or unset/change the AliExternalBDT name to force its regeneration." << std::endl;
+  } else {
+    const int status_comp = TreeliteCompilerCreate("ast_native", &fCompiler);
+    if (status_comp != 0) {
+      std::cerr << "Compiler creation failed." << std::endl;
+      return false;
+    }
+    const int status_gen = TreeliteCompilerGenerateCode(fCompiler, fModel, 1, path.data());
+    if (status_gen != 0) {
+      std::cerr << "Code generation failed." << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+std::string AliExternalBDT::GetUniquePath() {
+  if (fBDTname.empty()) {
+    return std::string(Form("%s_%p",fModelName.data(),this));
+  } else {
+    return std::string(Form("%s_%s",fBDTname.data(), fModelName.data()));
+  }
+}
+
+bool AliExternalBDT::LoadModel(const std::string &path, int type) {
+  if (path.empty()) {
+    std::cout << "Invalid empty model path string" << std::endl;
+    return false;
+  }
+  fModelPath = path;
+  fModelName = fModelPath.substr(fModelPath.find_last_of("\\/")+1,fModelPath.size());
+  int status = 0;
+  switch (type) {
+    case 0:
+      status = TreeliteLoadXGBoostModel(fModelPath.data(), &fModel);
+      break;
+    case 1:
+      status = TreeliteLoadLightGBMModel(fModelPath.data(), &fModel);
+      break;
+    default:
+      std::cerr << "Invalid model type" << std::endl;
+      return false;
+  }
+  if (status != 0) {
+    std::cerr << "Model loading failed" << std::endl;
+    return false;
+  }
+  if (!CreateModelCode()) return false;
+  if (!CompileAndLoadModelLibrary()) return false;
+  return true;
+}
+
+bool AliExternalBDT::LoadXGBoostModel(std::string path) {
+  if (!LoadModel(path, 0)) return false;
+  return true;
+}
+
+bool AliExternalBDT::LoadLightGBMModel(std::string path) {
+  if (!LoadModel(path, 1)) return false;
+  return true;
+}
+
+bool AliExternalBDT::LoadModelLibrary(std::string path) {
+  const int status = TreelitePredictorLoad(path.data(), 1, 1, &fPredictor);
+  if (status != 0) {
+    std::cerr << "Library loading failed" << std::endl;
+    return false;
+  }
+  return true;
+}
+
+double AliExternalBDT::Predict(double *features, int size, bool useRawScore) {
+  std::vector<TreelitePredictorEntry> entries(size);
+  for (size_t iEntry = 0; iEntry < entries.size(); ++iEntry) {
+    entries[iEntry].fvalue = static_cast<float>(features[iEntry]);
+  }
+  size_t out_size{0u};
+  TreelitePredictorQueryResultSizeSingleInst(fPredictor, &out_size);
+  assert(out_size == 1);
+  float output = 0.f;
+  TreelitePredictorPredictInst(fPredictor, entries.data(),
+      static_cast<int>(useRawScore), &output,
+      &out_size);
+  return output;
+}
