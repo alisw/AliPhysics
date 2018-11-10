135e37ad7008c35939e92273b4477178828bebb7
Nicolas Schmidt <nicolas.schmidt@cern.ch>
2018-11-10 15:06:18 +0100

    PWGGA/GammaConv: Added more std cut configs for PCM-EMC pp for EMC studies

--- PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
@@ -879,8 +879,34 @@ void AddTask_GammaCalo_pp(
     cuts.AddCutCalo("00010113","1111124067032220000","01631031000000d0"); // -50ns, 30ns timing cut, NL DExt PCMEMC+BTv3
   } else if (trainConfig == 420){ // EMCAL clusters no NL
     cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
   } else if (trainConfig == 421){ // EMCAL clusters no NonLin
-    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 422){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 423){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 424){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 425){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 426){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, T NL INT7
+  } else if (trainConfig == 427){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 428){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
+  } else if (trainConfig == 429){ // EMCAL clusters no NonLin
+    cuts.AddCutCalo("00010113","11111000a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, no NL INT7
+    cuts.AddCutCalo("00010113","11111060a7032230000","01631031000000d0"); // -12.5ns, 13ns timing cut, TB NL INT7
     // 5 TeV JetJet configs without trackmatching
   } else if (trainConfig == 430){ // EMCAL clusters no NonLin
     cuts.AddCutCalo("00010113","1111100060032220000","01631031000000d0"); // -50ns, 30ns timing cut, no NL INT7
