e7fcfd90ae2135fa99c1c2dc6e7bd30925511a6f
Helena Horstmann <helena.horstmann@cern.ch>
2018-12-18 16:59:30 +0100

    PWGGA/GammaConv: changed eta acceptance in AddTask_GammaCaloIso

--- PWGGA/GammaConv/macros/AddTask_GammaCaloIso_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaCaloIso_pp.C
@@ -150,19 +150,20 @@ void AddTask_GammaCaloIso_pp(
   //*************************** STANDARD EMC EMC MESON CUTS **********************************************
   //******************************************************************************************************
   if (trainConfig == 32){ //Isolation Cut variations, Trackmatching, maxM02
-  cuts.AddCutCalo("00083113","1111100060032230000","0163103100000050"); //with TrackMatching
-  cuts.AddCutCalo("00083113","11111000600322l0000","0163103100000050"); // EMCEG1, with TrackMatcher on
-  cuts.AddCutCalo("00083113","11111000670322l0000","0163103100000050"); // EMCEG1, with TrackMatcher off
-  cuts.AddCutCalo("00083113","1111100067032230000","0163103100000050"); // EMCEG1, with shape cut changed slightly
-  cuts.AddCutCalo("00083113","e1111000600322l0000","0163103100000050"); // EMCEG1, with TrackMatcher off
+  cuts.AddCutCalo("00083113","1221100060032230000","0163103100000050"); //with TrackMatching
+  cuts.AddCutCalo("00083113","12211000600322l0000","0163103100000050"); // EMCEG1, with TrackMatcher on
+  cuts.AddCutCalo("00083113","12211000670322l0000","0163103100000050"); // EMCEG1, with TrackMatcher off
+  cuts.AddCutCalo("00083113","1221100067032230000","0163103100000050"); // EMCEG1, with shape cut changed slightly
+  cuts.AddCutCalo("00083113","e2211000600322l0000","0163103100000050"); // EMCEG1, with TrackMatcher off
   //PhotonIsolation Variations, LHC17i3 MC without non linearity
   } else if (trainConfig == 33){ // without TrackMatching
-  cuts.AddCutCalo("00083113","1111100067032230000","0163103100000050"); //with TrackMatching
-  cuts.AddCutCalo("00083113","1111100060032230000","0163103100000050"); //without TrackMatching
-  cuts.AddCutCalo("00083113","b111100060032230000","0163103100000050"); //without TrackMatching, R=0.1
-  cuts.AddCutCalo("00083113","c111100060032230000","0163103100000050"); //without TrackMatching, R=0.2
-  cuts.AddCutCalo("00083113","d111100060032230000","0163103100000050"); //without TrackMatching, R=0.3
-  cuts.AddCutCalo("00083113","e111100060032230000","0163103100000050"); //without TrackMatching, R=0.4
+  cuts.AddCutCalo("00083113","1221100067032230000","0163103100000050"); //with TrackMatching
+  cuts.AddCutCalo("00083113","1221100060032230000","0163103100000050"); //without TrackMatching
+  cuts.AddCutCalo("00083113","b221100060032230000","0163103100000050"); //without TrackMatching, R=0.1
+  cuts.AddCutCalo("00083113","c221100060032230000","0163103100000050"); //without TrackMatching, R=0.2
+  cuts.AddCutCalo("00083113","d221100060032230000","0163103100000050"); //without TrackMatching, R=0.3
+  cuts.AddCutCalo("00083113","e221100060032230000","0163103100000050"); //without TrackMatching, R=0.4
+
   } else {
     Error(Form("GammaCaloIso_%i",trainConfig), "wrong trainConfig variable no cuts have been specified for the configuration");
     return;
