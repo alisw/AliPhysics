51c75c5bf21f9acfc342f4081e9ace83a75c4d58
Nicolò Jacazio <njacazio@hotmail.com>
2018-12-20 12:59:02 +0100

    Fixed reading macro to work with ROOT v6.14
    - Moved from TDataFrame to RDataFrame

--- RUN3/read.C
+++ RUN3/read.C
@@ -1,17 +1,19 @@
 
-#include "ROOT/TDataFrame.hxx"
+#include "ROOT/RDFHistoModels.hxx"
+#include "ROOT/RDataFrame.hxx"
 #include "TCanvas.h"
 #include "TFile.h"
 
 #include "AliAnalysisTaskAO2Dconverter.h"
 
-using namespace ROOT::Experimental; // TDataFrame's namespace
+#include "AliAnalysisTaskAO2Dconverter.h"
 
 void read()
 {
   const Char_t* name = AliAnalysisTaskAO2Dconverter::TreeName[AliAnalysisTaskAO2Dconverter::kTracks];
-  TDataFrame d(name, "AO2D.root");
-  auto h = d.Define("beta", "fTOFsignal > 0.1 ? fLength/(fTOFsignal * (TMath::C() * 1.e2 / 1.e12)) : -10").Histo2D(::TH2F{ "beta", ";#it{p} (GeV/#it{c});TOF #beta;", 1000, 0.1, 5, 1000, 0, 2 }, "fTPCinnerP", "beta");
+  ROOT::RDataFrame d(name, "AO2D.root");
+  ROOT::RDF::TH2DModel betamodel("beta", ";#it{p} (GeV/#it{c});TOF #beta;", 1000, 0.1, 5, 1000, 0, 2);
+  auto h = d.Define("beta", "fTOFsignal > 0.1 ? fLength/(fTOFsignal * (TMath::C() * 1.e2 / 1.e12)) : -10").Histo2D(betamodel, "fTPCinnerP", "beta");
   new TCanvas();
   gPad->SetLogz();
   gPad->SetLogx();
