db846bde7b5d83b7ef1b1ebc2cfd3f85a5953ea3
Nicolò Jacazio <njacazio@hotmail.com>
2018-12-20 15:01:14 +0100

    Using AliExternalTrackParam to get momentum

--- RUN3/read.C
+++ RUN3/read.C
@@ -6,14 +6,18 @@
 
 #include "AliAnalysisTaskAO2Dconverter.h"
 
-#include "AliAnalysisTaskAO2Dconverter.h"
+#include "AliExternalTrackParam.h"
 
 void read()
 {
   const Char_t* name = AliAnalysisTaskAO2Dconverter::TreeName[AliAnalysisTaskAO2Dconverter::kTracks];
   ROOT::RDataFrame d(name, "AO2D.root");
   ROOT::RDF::TH2DModel betamodel("beta", ";#it{p} (GeV/#it{c});TOF #beta;", 1000, 0.1, 5, 1000, 0, 2);
-  auto h = d.Define("beta", "fTOFsignal > 0.1 ? fLength/(fTOFsignal * (TMath::C() * 1.e2 / 1.e12)) : -10").Histo2D(betamodel, "fTPCinnerP", "beta");
+  auto h = d.Define("param", "std::array<double, 5> p{fY, fZ, fSnp, fTgl, fSigned1Pt}; return p;")
+               .Define("covar", "std::array<double, 15> p{fCYY, fCZY, fCZZ, fCSnpY, fCSnpZ, fCSnpSnp, fCTglY, fCTglZ, fCTglSnp, fCTglTgl, fC1PtY, fC1PtZ, fC1PtSnp, fC1PtTgl, fC1Pt21Pt2}; return p;")
+               .Define("P", "AliExternalTrackParam((double)fX, (double)fAlpha, param.data(), covar.data()).GetP()")
+               .Define("beta", "fTOFsignal > 0.1 ? fLength/(fTOFsignal * (TMath::C() * 1.e2 / 1.e12)) : -10")
+               .Histo2D(betamodel, "P", "beta");
   new TCanvas();
   gPad->SetLogz();
   gPad->SetLogx();
