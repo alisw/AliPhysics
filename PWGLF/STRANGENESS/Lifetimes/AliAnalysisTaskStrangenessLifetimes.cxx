383c73878808f5f4fcc04b6bb3d6cc20d178e32b
Francesco Mazzaschi <fmazzasc@cern.ch>
2019-01-09 13:01:50 +0100

    Fix downscale bug

--- PWGLF/STRANGENESS/Lifetimes/AliAnalysisTaskStrangenessLifetimes.cxx
+++ PWGLF/STRANGENESS/Lifetimes/AliAnalysisTaskStrangenessLifetimes.cxx
@@ -567,7 +567,7 @@ void AliAnalysisTaskStrangenessLifetimes::UserExec(Option_t *) {
         }
       }
       if (fake=true && fDownscale<1){
-         if((gRandom->Rndm())<fDownscale){continue;}
+         if((gRandom->Rndm())>fDownscale){continue;}
       }
       if (isHyperCandidate){
          auto miniHyper = HyperTriton2Body::FillHyperTriton2Body(v0,pTrack,nTrack,nSigmaPosHe3,
