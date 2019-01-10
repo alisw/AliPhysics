5da15eca047cdaac229b9e82a8336e1a92e89dac
Nikola Hardi <nikola.hardi@cern.ch>
2019-01-10 14:05:31 +0100

    Remove some strong dependencies on TAlien - AliAnalysisAlien.cxx

--- ANALYSIS/ANALYSISaliceBase/AliAnalysisAlien.cxx
+++ ANALYSIS/ANALYSISaliceBase/AliAnalysisAlien.cxx
@@ -311,8 +311,8 @@ AliAnalysisAlien::AliAnalysisAlien(const AliAnalysisAlien& other)
                   fTreeName(other.fTreeName)
 {
 // Copy ctor.
-   fGridJDL = (TGridJDL*)gROOT->ProcessLine("new TAlienJDL()");
-   fMergingJDL = (TGridJDL*)gROOT->ProcessLine("new TAlienJDL()");
+   fGridJDL = gGrid->GetJDLGenerator();
+   fMergingJDL = gGrid->GetJDLGenerator();
    fRunRange[0] = other.fRunRange[0];
    fRunRange[1] = other.fRunRange[1];
    if (other.fInputFiles) {
@@ -359,8 +359,8 @@ AliAnalysisAlien &AliAnalysisAlien::operator=(const AliAnalysisAlien& other)
 // Assignment.
    if (this != &other) {
       AliAnalysisGrid::operator=(other);
-      fGridJDL = (TGridJDL*)gROOT->ProcessLine("new TAlienJDL()");
-      fMergingJDL = (TGridJDL*)gROOT->ProcessLine("new TAlienJDL()");
+      fGridJDL = gGrid->GetJDLGenerator();
+      fMergingJDL = gGrid->GetJDLGenerator();
       fPrice                   = other.fPrice;
       fTTL                     = other.fTTL;
       fSplitMaxInputFileNumber = other.fSplitMaxInputFileNumber;
@@ -1295,12 +1295,13 @@ Bool_t AliAnalysisAlien::CreateDataset(const char *pattern)
          }
          if (ncount == gMaxEntries) {
             Info("CreateDataset", "Dataset %s has more than 15K entries. Trying to merge...", file.Data());
-            cadd = (TGridCollection*)gROOT->ProcessLine(Form("new TAlienCollection(\"__tmp%d__%s\", 1000000);",stage,file.Data()));
+            cadd = gGrid->OpenCollection(Form("__tmp%d__%s", stage, file.Data()), 1000000);
             if (!cbase) cbase = cadd;
             else {
 	      // cholm - Avoid using very slow TAlienCollection 
 	      // cbase->Add(cadd);
 	      // cholm - Use AddFast (via interpreter)
+        // TODO: nhardi, Jan 2019
 	      gROOT->ProcessLine(Form("((TAlienCollection*)%p)->AddFast((TGridCollection*)%p)",cbase,cadd));
                delete cadd;
             }   
