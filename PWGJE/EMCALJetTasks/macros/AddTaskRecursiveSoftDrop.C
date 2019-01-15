c996ab2265d76e82bc7b302fd1b5b1b76d92c52c
Harry Andrews <harry.andrews@cern.ch>
2019-01-15 17:09:27 +0100

    fix TaskRecursiveSoftDrop for root6

--- PWGJE/EMCALJetTasks/macros/AddTaskRecursiveSoftDrop.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskRecursiveSoftDrop.C
@@ -5,22 +5,22 @@ AliAnalysisTaskRecursiveSoftDrop* AddTaskRecursiveSoftDrop(
 							   const char * njetsHybridUs,
 							   const char * njetsHybridS,
 							   const Double_t R,
-							   const char * nrhoBase, 
+							   const char * nrhoBase,
 							   const char * ntracksData,
 							   const char * ntracksTrue,
-							   const char * ntracksDet, 
+							   const char * ntracksDet,
 							   const char * ntracksHybridUs,
 							   const char * ntracksHybridS,
-							   const char *type,				      
+							   const char *type,
 							   const char *CentEst,
 							   Int_t       pSel,
 							   TString     trigClass,
 							   TString     kEmcalTriggers,
 							   TString     tag,
-							   AliAnalysisTaskRecursiveSoftDrop::JetShapeSub jetShapeSub,
-							   AliAnalysisTaskRecursiveSoftDrop::JetType fjetType
+							   AliAnalysisTaskRecursiveSoftDrop::JetShapeSub jetShapeSub = AliAnalysisTaskRecursiveSoftDrop::kNoSub,
+							   AliAnalysisTaskRecursiveSoftDrop::JetType fjetType = AliAnalysisTaskRecursiveSoftDrop::kData
 							   ) {
-  
+
   AliAnalysisTaskRecursiveSoftDrop *task = AliAnalysisTaskRecursiveSoftDrop::AddTaskRecursiveSoftDrop(njetsData,njetsTrue,njetsDet,njetsHybridUs,njetsHybridS,R,nrhoBase,ntracksData,ntracksTrue,ntracksDet,ntracksHybridUs,ntracksHybridS,type,CentEst,pSel,trigClass,kEmcalTriggers,tag,jetShapeSub,fjetType);
   return task;
 
