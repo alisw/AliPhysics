faa143b34fdabc31dd07c801c30c14611acaad01
Ionut Arsene <iarsene@cern.ch>
2019-01-30 22:38:34 +0100

    Several small updates. 1) Tree maker: Filling an independent variable for the number of TPCout tracks 2) EventInfo: Added a special data member for the TPCout tracks and added possibility to compute             the VZERO total multiplicity from either the individual channels or from the already integrated data in the trees. 3) VarManager: Added variables for the VZERO mult computed from channels; special per channel VZERO calibration for event plane

--- PWGDQ/reducedTree/AliReducedEventInfo.h
+++ PWGDQ/reducedTree/AliReducedEventInfo.h
@@ -64,6 +64,7 @@ class AliReducedEventInfo : public AliReducedBaseEvent {
   UInt_t    ITSClusters(Int_t layer)          const {return (layer>=1 && layer<=6 ? fITSClusters[layer-1] : 0);}
   Int_t     SPDnSingleClusters()              const {return fSPDnSingle;}
   Int_t     TracksPerTrackingFlag(Int_t flag) const {return (flag>=0 && flag<32 ? fNtracksPerTrackingFlag[flag] : -999);}
+  Int_t     TracksWithTPCout()                const {return fNtracksTPCout;}
   Int_t     Nch16 (Bool_t exclJpsiDau = kFALSE )  const {return (exclJpsiDau? fNch[0] : fNch[1] );}
   Int_t     Nch10 (Bool_t exclJpsiDau = kFALSE )  const {return (exclJpsiDau? fNch[2] : fNch[3] );}
   Int_t     NchV0A(Bool_t exclJpsiDau = kFALSE ) const {return (exclJpsiDau? fNch[4] : fNch[5] );}
@@ -98,9 +99,9 @@ class AliReducedEventInfo : public AliReducedBaseEvent {
   Float_t   MultEstimatorPercentileV0C()   const {return fMultiplicityEstimatorPercentiles[12];}
   
   Float_t   MultChannelVZERO(Int_t channel)   const {return (channel>=0 && channel<=63 ? fVZEROMult[channel] : -999.);}
-  Float_t   MultVZEROA()                      const;
-  Float_t   MultVZEROC()                      const;
-  Float_t   MultVZERO()                       const;
+  Float_t   MultVZEROA(Bool_t fromChannels=kFALSE)                      const;
+  Float_t   MultVZEROC(Bool_t fromChannels=kFALSE)                      const;
+  Float_t   MultVZERO(Bool_t fromChannels=kFALSE)                       const;
   Float_t   MultRingVZEROA(Int_t ring)        const;
   Float_t   MultRingVZEROC(Int_t ring)        const;
   
@@ -191,6 +192,7 @@ class AliReducedEventInfo : public AliReducedBaseEvent {
   UInt_t    fITSClusters[6];        // number of ITS clusters per layer
   Int_t     fSPDnSingle;            // number of clusters in SPD layer 1, not associated to a tracklet on SPD layer 2
   Int_t     fNtracksPerTrackingFlag[32];  // number of tracks for each tracking status bit                
+  Int_t     fNtracksTPCout;          // number of kTPCout tracks in ESDs
   Int_t     fNch[8];                // number of MCtruth charged particles in different eta regions
   Float_t   fVZEROMult[64];         // VZERO multiplicity in all 64 channels
   Float_t   fVZEROTotalMult[2];    // Total VZERO multiplicity
@@ -218,7 +220,7 @@ class AliReducedEventInfo : public AliReducedBaseEvent {
   AliReducedEventInfo& operator= (const AliReducedEventInfo &c);
   AliReducedEventInfo(const AliReducedEventInfo &c);
 
-  ClassDef(AliReducedEventInfo, 11);
+  ClassDef(AliReducedEventInfo, 12);
 };
 
 #endif
