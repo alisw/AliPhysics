4272621a6dd89601ca8574b5366d52a318dd4063
aaroncapon <aaroncapon@cern.ch>
2019-02-04 15:50:26 +0100

    Bug

--- PWGDQ/dielectron/macrosLMEE/Config_acapon.C
+++ PWGDQ/dielectron/macrosLMEE/Config_acapon.C
@@ -741,7 +741,7 @@ TVectorD* GetVector(Int_t var, Bool_t useRun1binning){
       return AliDielectronHelper::MakeArbitraryBinning("0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,"
                                                        "0.10, 0.14, 0.18, 0.22, 0.26, 0.30, 0.34, 0.38, 0.42, 0.46, 0.50");
     case kPtee:
-      return AliDielectronHelper::MakeArbitraryBinning("1.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45,"
+      return AliDielectronHelper::MakeArbitraryBinning("0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45,"
                                                        "0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95,"
                                                        "1.00, 1.20, 1.40, 1.60, 1.80, 2.00, 2.20, 2.40, 2.60, 2.80,"
                                                        "3.00, 3.20, 3.40, 3.60, 3.80, 4.00, 4.20, 4.40, 4.60, 4.80,"
