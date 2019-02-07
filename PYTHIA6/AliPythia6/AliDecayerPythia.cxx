fe7b3ddad03836cd5031323213e5a71291ced46f
Jeremy Wilkinson <jeremy.wilkinson@cern.ch>
2019-02-07 13:54:43 +0100

    split Lc->pK0S mode with forced kaon decay to new option

--- PYTHIA6/AliPythia6/AliDecayerPythia.cxx
+++ PYTHIA6/AliPythia6/AliDecayerPythia.cxx
@@ -591,7 +591,9 @@ void AliDecayerPythia::ForceDecay()
      case kLcpK0S:
         ForceHadronicD(0,0,2);
         break;
-
+     case kLcpK0SBDTsig:
+        ForceHadronicD(0,0,4);
+        break;
     }
 }
 
@@ -912,13 +914,18 @@ void AliDecayerPythia::ForceHadronicD(Int_t optUse4Bodies, Int_t optUseDtoV0, In
     }
     if (optForceLcChannel == 2) { //pK0S
       ForceParticleDecay(4122,prodLcpK0S,multLcpK0S,2,1); //Lc to p + K0
-      ForceParticleDecay(311,310,1);	//K0 -> K0S
-      ForceParticleDecay(310,211,2);	//K0S -> pion x 2
     }
     if(optForceLcChannel == 3) { // Lambda Pi+ Pi0
       ForceParticleDecay(4122, prodLcLambdaPiPlPi0, multLcLambdaPiPlPi0, 3, 1);
     }
-      
+    
+    if(optForceLcChannel == 4) { // pK0S for BDT signal training: force all K0->K0S->pi+pi-
+      ForceParticleDecay(4122,prodLcpK0S,multLcpK0S,2,1); //Lc to p + K0
+      ForceParticleDecay(311,310,1); // K0 -> K0S
+      ForceParticleDecay(310,211,2); // K0S -> pi+ pi-
+    }
+
+  
 
 }
 
