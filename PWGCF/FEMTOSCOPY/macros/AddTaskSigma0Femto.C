9da2c85d2c52affab7a11177bcb22c549a6dd437
Andreas Mathis <andreas.mathis@cern.ch>
2019-02-18 15:52:19 +0100

    Use delta eta delta phi* cut for p-p femto

--- PWGCF/FEMTOSCOPY/macros/AddTaskSigma0Femto.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskSigma0Femto.C
@@ -321,9 +321,11 @@ AliAnalysisTaskSE *AddTaskSigma0Femto(bool isMC = false,
   std::vector<float> kMin;
   std::vector<float> kMax;
   std::vector<int> pairQA;
+  std::vector<bool> closeRejection;
   const int nPairs = (suffix == "0") ? 78 : 36;
   for (int i = 0; i < nPairs; ++i) {
     pairQA.push_back(0);
+    closeRejection.push_back(false);
     if (suffix == "0") {
       NBins.push_back(750);
       kMin.push_back(0.);
@@ -340,9 +342,15 @@ AliAnalysisTaskSE *AddTaskSigma0Femto(bool isMC = false,
     pairQA[0] = 11;   // pp
     pairQA[2] = 14;   // pSigma
     pairQA[12] = 11;  // barp barp
-    pairQA[14] = 14;  // barp barp
+    pairQA[14] = 14;  // barp bSigma
     pairQA[23] = 44;  // Sigma Sigma
     pairQA[33] = 44;  // barSigma barSigma
+
+    closeRejection[0] = true;   // pp
+    closeRejection[12] = true;  // barp barp
+  } else {
+    closeRejection[0] = true;  // pp
+    closeRejection[8] = true;  // barp barp
   }
 
   AliFemtoDreamCollConfig *config =
@@ -405,6 +413,12 @@ AliAnalysisTaskSE *AddTaskSigma0Femto(bool isMC = false,
       std::cout << "You are trying to request the Momentum Resolution without "
                    "MC Info; fix it wont work! \n";
     }
+  }
+
+  if (trigger == "kHighMultV0") {
+    config->SetDeltaEtaMax(0.01);
+    config->SetDeltaPhiMax(0.01);
+    config->SetClosePairRejection(closeRejection);
   }
 
   if (suffix == "0") {
