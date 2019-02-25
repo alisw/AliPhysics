6cfe65e6a0e4f10b68af45663d6443034dd56eb6
bhohlweger <bernhard.hohlweger@cern.ch>
2019-02-25 10:18:32 +0100

    Remove arguments not needed and update SysVars

--- PWGCF/FEMTOSCOPY/macros/AddTaskFemtoDreamSysVar.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskFemtoDreamSysVar.C
@@ -6,15 +6,14 @@ AliAnalysisTaskSE* AddTaskFemtoDreamSysVar(bool isMC = false,
                                                "kInt7",
                                            bool notpp = true,  //1
                                            bool fineBinning = true,  //2
-                                           bool PileUpRej = true,  //3
-                                           bool multBinning = true,  //4
-                                           bool kTBinning = false,  //5
-                                           bool kTCentBinning = false,  //6
-                                           bool mTBinning = false,  //7
-                                           bool eventMixing = true,  //8
-                                           bool phiSpin = true,  //9
-                                           bool minimalBooking = true,  // 10
-                                           const char *swuffix = "")  //11
+                                           bool multBinning = true,  //3
+                                           bool kTBinning = false,  //4
+                                           bool kTCentBinning = false,  //5
+                                           bool mTBinning = false,  //6
+                                           bool eventMixing = true,  //7
+                                           bool phiSpin = true,  //8
+                                           bool minimalBooking = true,  // 9
+                                           const char *swuffix = "")  //10
                                            {
   TString suffix = Form("%s", swuffix);
   bool DCAPlots = false;
@@ -22,7 +21,7 @@ AliAnalysisTaskSE* AddTaskFemtoDreamSysVar(bool isMC = false,
   bool CombSigma = false;
   bool ContributionSplitting = false;
   bool ContributionSplittingDaug = false;
-
+  bool PileUpRej = true;
   // the manager is static, so get the existing manager via the static method
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
 
@@ -278,32 +277,33 @@ AliAnalysisTaskSE* AddTaskFemtoDreamSysVar(bool isMC = false,
   } else if (suffix == "42") {
     CascadeCuts->SetPtRangeXi(0.8, 999.5);
     AntiCascadeCuts->SetPtRangeXi(0.8, 999.5);
+
     //wide wariation
-  } else if (suffix == "43") {
+  } else if (suffix == "101") {
     CascadeCuts->SetXiMassRange(1.368, 0.028);
     AntiCascadeCuts->SetXiMassRange(1.368, 0.028);
-  } else if (suffix == "44") {
+  } else if (suffix == "102") {
     CascadeCuts->SetXiMassRange(1.282, 0.028);
     AntiCascadeCuts->SetXiMassRange(1.282, 0.028);
     //thinner wariation
-  } else if (suffix == "45") {
+  } else if (suffix == "103") {
     CascadeCuts->SetXiMassRange(1.354, 0.014);
     AntiCascadeCuts->SetXiMassRange(1.354, 0.014);
-  } else if (suffix == "46") {
+  } else if (suffix == "104") {
     CascadeCuts->SetXiMassRange(1.296, 0.014);
     AntiCascadeCuts->SetXiMassRange(1.296, 0.014);
     //Further away from the peak
-  } else if (suffix == "47") {
+  } else if (suffix == "105") {
     CascadeCuts->SetXiMassRange(1.368, 0.014);
     AntiCascadeCuts->SetXiMassRange(1.368, 0.014);
-  } else if (suffix == "48") {
+  } else if (suffix == "106") {
     CascadeCuts->SetXiMassRange(1.282, 0.014);
     AntiCascadeCuts->SetXiMassRange(1.282, 0.014);
     //furthest away from the peak
-  } else if (suffix == "49") {
+  } else if (suffix == "107") {
     CascadeCuts->SetXiMassRange(1.382, 0.014);
     AntiCascadeCuts->SetXiMassRange(1.382, 0.014);
-  } else if (suffix == "50") {
+  } else if (suffix == "108") {
     CascadeCuts->SetXiMassRange(1.268, 0.014);
     AntiCascadeCuts->SetXiMassRange(1.268, 0.014);
   }
@@ -484,7 +484,7 @@ AliAnalysisTaskSE* AddTaskFemtoDreamSysVar(bool isMC = false,
     MultBins.push_back(80);
     config->SetMultBins(MultBins);
   }
-  config->SetMultBinning(multBinning);
+  config->SetMultBinning(true);
   config->SetZBins(ZVtxBins);
   config->SetPDGCodes(PDGParticles);
   config->SetNBinsHist(NBins);
@@ -497,6 +497,17 @@ AliAnalysisTaskSE* AddTaskFemtoDreamSysVar(bool isMC = false,
   config->SetmTBinning(mTBinning);
   config->SetUseEventMixing(eventMixing);
   config->SetUsePhiSpinning(phiSpin);
+
+  config->SetDeltaEtaMax(0.01);
+  config->SetDeltaPhiMax(0.01);
+  if (suffix == "43") {
+    config->SetDeltaEtaMax(0.012);
+    config->SetDeltaPhiMax(0.012) ;
+  } else if (suffix == "44") {
+    config->SetDeltaEtaMax(0.014);
+    config->SetDeltaPhiMax(0.014);
+  }
+  config->SetClosePairRejection(config->GetStandardPairRejection());
   if (minimalBooking) {
     config->SetMinimalBookingME(true);
     config->SetMinimalBookingSample(true);
