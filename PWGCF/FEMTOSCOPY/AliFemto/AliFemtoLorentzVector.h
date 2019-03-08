6006e5aa31e67500014a2e75d65739f8e1761d7c
akubera <andrew.kubera@cern.ch>
2019-03-08 03:38:59 -0500

    AliFemtoVectors - Standardize include-guards

--- PWGCF/FEMTOSCOPY/AliFemto/AliFemtoLorentzVector.h
+++ PWGCF/FEMTOSCOPY/AliFemto/AliFemtoLorentzVector.h
@@ -1,72 +1,6 @@
-/***************************************************************************
- *
- * $Id$
- *
- * Author: Brian Lasiuk, Thomas Ullrich, April 1998
- ***************************************************************************
- *
- * Description:
- *
- * Remarks:   Since not all compilers support member templates
- *            we have to specialize the templated member on these
- *            platforms. If member templates are not supported the
- *            ST_NO_MEMBER_TEMPLATES flag has to be set. tu.
- *
- *            In the near future when all compilers can handle member
- *            templates this class should be cleaned up. A lot of
- *            redundant code can be removed as soon as the compilers
- *            are up-to-date. tu
- *
- ***************************************************************************
- *
- * $Log$
- * Revision 1.1.1.1  2007/04/25 15:38:41  panos
- * Importing the HBT code dir
- *
- * Revision 1.1.1.1  2007/03/07 10:14:49  mchojnacki
- * First version on CVS
- *
- * Revision 1.11  2005/09/22 20:09:20  fisyak
- * Make AliFemtoLorentzVector persistent
- *
- * Revision 1.10  2005/07/06 18:49:56  fisyak
- * Replace AliFemtoHelixD, AliFemtoLorentzVectorD,AliFemtoLorentzVectorF,AliFemtoMatrixD,AliFemtoMatrixF,AliFemtoPhysicalHelixD,AliFemtoThreeVectorD,AliFemtoThreeVectorF by templated version
- *
- * Revision 1.9  2005/03/28 06:02:45  perev
- * Defence FPE added
- *
- * Revision 1.8  2003/09/02 17:59:35  perev
- * gcc 3.2 updates + WarnOff
- *
- * Revision 1.7  2003/05/01 19:24:31  ullrich
- * Corrected problem in boost().
- *
- * Revision 1.6  1999/10/15 15:56:36  ullrich
- * Changed output format in operator<<, added operator>>
- *
- * Revision 1.5  1999/06/04 18:01:36  ullrich
- * New operators operator() and operator[] which can be used
- * as lvalues.
- *
- * Revision 1.4  1999/04/14 23:12:07  fisyak
- * Add __CINT__ to handle references
- *
- * Revision 1.3  1999/02/17 11:38:36  ullrich
- * Removed specialization for 'long double'.
- *
- * Revision 1.2  1999/02/14 23:11:42  fisyak
- * Fixes for Rootcint
- *
- * Revision 1.1  1999/01/30 03:59:02  fisyak
- * Root Version of AliFemtoarClassLibrary
- *
- * Revision 1.1  1999/01/23 00:27:52  ullrich
- * Initial Revision
- *
- **************************************************************************/
-/*
-//// General class for a Lorentz four-vector
-***/
+///
+/// \file AliFemtoLorentzVector.h
+///
 
 #ifndef ALIFEMTOLORENTZVECTOR_H
 #define ALIFEMTOLORENTZVECTOR_H
@@ -78,4 +12,3 @@
 //template <typename T> using AliFemtoLorentzVector = AliFmLorentzVector<T>;
 
 #endif
-
