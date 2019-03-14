af679f020ca95241fa619b01554e20adb57c98d8
Stefano Trogolo <strogolo@cern.ch>
2019-03-14 22:46:18 +0100

    Add rejection method for candidate

--- PWGHF/vertexingHF/AliAnalysisTaskSED0Mass.h
+++ PWGHF/vertexingHF/AliAnalysisTaskSED0Mass.h
@@ -63,6 +63,7 @@ class AliAnalysisTaskSED0Mass : public AliAnalysisTaskSE
   void SetFillYHistos(Bool_t flag) {fFillYHist=flag;}
   void SetFillImpactParameterHistos(Bool_t flag) {fFillImpParHist=flag;}
   void SetFillSparses(Bool_t flag) {fFillSparses=flag;}
+  void SetUseRejectionMethod(Bool_t flag=kFALSE, Float_t factor=0.01) {fUseRejectionMethod=flag; fRejectionFactor=factor;}
   void SetSystem(Int_t sys){fSys=sys; if(fSys==1) SetFillVarHists(kFALSE);}
   void SetRejectSDDClusters(Bool_t flag) { fIsRejectSDDClusters=flag; }
   void SetUseSelectionBit(Bool_t flag) { fUseSelectionBit=flag; }
@@ -153,8 +154,10 @@ class AliAnalysisTaskSED0Mass : public AliAnalysisTaskSE
   Double_t *fCandidateVariables;      //!<!  variables to be written to the tree
   Bool_t	fPIDCheck;			/// flag to decide whether to fill "PID = x" bins in fNentrie
   Bool_t    fDrawDetSignal;		/// flag to decide whether to draw the TPC dE/dx and TOF signal before/after PID
-  Bool_t fUseQuarkTagInKine;            // flag for quark/hadron level identification of prompt and feeddown
-  Bool_t fFillSparses;                  // flag to activate THnSparse
+  Bool_t    fUseQuarkTagInKine;            // flag for quark/hadron level identification of prompt and feeddown
+  Bool_t    fFillSparses;                  // flag to activate THnSparse
+  Bool_t    fUseRejectionMethod;           // flag to activate the Rejection method
+  Float_t   fRejectionFactor;              // rejection factor to be used in the rejection method
   THnSparseF *fhStudyImpParSingleTrackSign; //!<! sparse with imp par residual cuts for MC
   THnSparseF *fhStudyImpParSingleTrackCand;  //!<! sparse with imp par residual cuts for Data
   THnSparseF *fhStudyImpParSingleTrackFd;   //!<! sparse with imp par residual cuts for MC
