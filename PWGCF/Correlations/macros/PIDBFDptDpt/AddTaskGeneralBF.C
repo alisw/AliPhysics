e3894c7d04099ffdbb0f2f27e7aef326d567e210
panjinjin <jinjin.pan@hotmail.com>
2019-03-18 09:39:59 -0400

    Update AddTaskGeneralBF.C

--- PWGCF/Correlations/macros/PIDBFDptDpt/AddTaskGeneralBF.C
+++ PWGCF/Correlations/macros/PIDBFDptDpt/AddTaskGeneralBF.C
@@ -290,6 +290,13 @@ AliAnalysisTaskGeneralBF * AddTaskGeneralBF
     minCentrality[4] = 40.;     maxCentrality[4]  = 50.;
     minCentrality[5] = 50.;     maxCentrality[5]  = 60.;
     minCentrality[6] = 60.;     maxCentrality[6]  = 90.; }
+  else if ( CentralityGroup == 35 )
+  { minCentrality[0] = 0;       maxCentrality[0]  = 10.;
+    minCentrality[1] = 10.;     maxCentrality[1]  = 20.;
+    minCentrality[2] = 20.;     maxCentrality[2]  = 30.;
+    minCentrality[3] = 30.;     maxCentrality[3]  = 40.;
+    minCentrality[4] = 40.;     maxCentrality[4]  = 60.;
+    minCentrality[5] = 60.;     maxCentrality[5]  = 90.; }
   else    return 0;
   
   double dedxMin                =  0.0;
