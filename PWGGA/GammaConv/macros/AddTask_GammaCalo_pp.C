0c0c67e5e5c37dd684d92e15106f927c02827ceb
Lizette Lamers <lizette.jacqueline.lamers@cern.ch>
2019-03-22 14:20:37 +0100

    PWGGA/GammaConv Seperating PHOS and EDC in same TrainConfig

--- PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
+++ PWGGA/GammaConv/macros/AddTask_GammaCalo_pp.C
@@ -1441,9 +1441,10 @@ void AddTask_GammaCalo_pp(
     cuts.AddCutCalo("00010113","411771107l032230000","2l631031000000d0"); // Standard EDC
  } else if (trainConfig == 913){ //// Jet QA for EMCAL+DCAL and PHOS
     cuts.AddCutCalo("00010113","411771107l032230000","3l631031000000d0"); // Standard EDC
-    cuts.AddCutCalo("00010113","2446651044012300000","3163103100000010"); // PHOS
  } else if (trainConfig == 914){ //PHOS+JETS
     cuts.AddCutCalo("00010113","2446651044012300000","2163103100000010"); //
+ } else if (trainConfig == 915){ //PHOS+JetQA
+    cuts.AddCutCalo("00010113","2446651044012300000","3163103100000010"); // PHOS QA
 
   } else if (trainConfig == 2000){ // EMCAL+DCAL clusters standard cuts
     cuts.AddCutCalo("00010113","4117900077032230000","01631031000000d0"); // INT7 - NO NL
