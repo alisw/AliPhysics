87a13bd7d5bf0a77e72c8af1ef9b6a6d0c26522b
Jan Fiete Grosse-Oetringhaus <jgrosseo@cern.ch>
2019-04-01 11:29:56 +0200

    updates to nano AOD framework - inclusion of V0 filtering - general syntax for adding fields from MultSelection - basic nsigma PID from TPC and TOF in standard setter code - changes to allow direct filtering from on the fly produced AOD (with ESD filter)

--- PWG/DevNanoAOD/AddTaskNanoAODFilter.C
+++ PWG/DevNanoAOD/AddTaskNanoAODFilter.C
@@ -7,50 +7,42 @@
 #include "AliAnalysisDataContainer.h"
 #include "AliVEventHandler.h"
 #include "AliESEHelpers.h"
-
 #endif
 
 
-AliAnalysisTaskSE * AddTaskNanoAODFilter(Int_t iMC, Bool_t savecuts = 0) {
-  // Adds my task
+AliAnalysisTaskSE* AddTaskNanoAODFilter(Int_t iMC, Bool_t savecuts = 0) 
+{
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
-    ::Error("AddTaskNanoAODESEFilter", "No analysis manager to connect to.");
+    ::Error("AddTaskNanoAODFilter", "No analysis manager to connect to.");
     return NULL;
   }  
   // Check the analysis type using the event handlers connected to the analysis manager.
   //==============================================================================
   if (!mgr->GetInputEventHandler()) {
-    ::Error("AddTaskNanoAODESEFilter", "This task requires an input event handler");
+    ::Error("AddTaskNanoAODFilter", "This task requires an input event handler");
     return NULL;
   }
-  TString inputDataType = mgr->GetInputEventHandler()->GetDataType(); // can be "ESD" or "AOD"
     
   // Configure analysis
   //===========================================================================
-
-  //  gROOT->LoadMacro("AliAnalysisTaskNanoAODESEFilter.cxx+");
-
-  AliAnalysisTaskNanoAODFilter * task = new AliAnalysisTaskNanoAODFilter("TaskEseFilter", savecuts);
+  AliAnalysisTaskNanoAODFilter* task = new AliAnalysisTaskNanoAODFilter("NanoAODFilter", savecuts);
   
   mgr->AddTask(task);
   task->SetMCMode(iMC);
 
   mgr->ConnectInput (task, 0, mgr->GetCommonInputContainer());
-  //  mgr->ConnectOutput(task, 1, mgr->GetCommonOutputContainer());
   
   if(savecuts) {
     AliAnalysisDataContainer *coutputpt1 = mgr->CreateContainer("evtcuts", AliAnalysisCuts::Class(),  AliAnalysisManager::kOutputContainer,"cuts.root");
     AliAnalysisDataContainer *coutputpt2 = mgr->CreateContainer("trkcuts", AliAnalysisCuts::Class(),  AliAnalysisManager::kOutputContainer,"cuts.root");
+    AliAnalysisDataContainer *coutputpt3 = mgr->CreateContainer("v0cuts", AliAnalysisCuts::Class(),  AliAnalysisManager::kOutputContainer,"cuts.root");
     mgr->ConnectOutput(task, 1, coutputpt1);
     mgr->ConnectOutput(task, 2, coutputpt2);
+    mgr->ConnectOutput(task, 3, coutputpt3);
   }
 
-  
-
-
   return task;
-
 }
 
 
