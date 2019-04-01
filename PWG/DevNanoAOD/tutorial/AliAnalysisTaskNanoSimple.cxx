9f349fe9e927f18cf526bce6dbf70f778c2b02fe
Jan Fiete Grosse-Oetringhaus <jgrosseo@cern.ch>
2019-04-01 11:32:03 +0200

    adding nano AOD tutorial files

---
+++ PWG/DevNanoAOD/tutorial/AliAnalysisTaskNanoSimple.cxx
@@ -0,0 +1,105 @@
+#include <TList.h>
+#include <TMath.h>
+
+#include "AliAnalysisTaskNanoSimple.h"
+#include "AliAnalysisManager.h"
+#include "AliInputEventHandler.h"
+#include "AliAODEvent.h"
+#include "AliAODv0.h"
+
+#include "AliNanoAODHeader.h"
+#include "AliNanoAODTrack.h"
+
+#include "AliLog.h"
+#include "AliVParticle.h"
+#include "AliVTrack.h"
+
+ClassImp(AliAnalysisTaskNanoSimple)
+
+//____________________________________________________________________
+AliAnalysisTaskNanoSimple:: AliAnalysisTaskNanoSimple(const char* name):
+AliAnalysisTaskSE(name),
+// general configuration
+fListOfHistos(0x0)
+{
+  // Default constructor
+
+  DefineOutput(1, TList::Class());
+  DefineOutput(2, TTree::Class());
+}
+
+AliAnalysisTaskNanoSimple::~AliAnalysisTaskNanoSimple() 
+{ 
+  // destructor
+  
+  if (fListOfHistos) 
+    delete fListOfHistos;
+}
+
+//____________________________________________________________________
+void  AliAnalysisTaskNanoSimple::UserCreateOutputObjects()
+{
+  // Create the output container
+  
+  // Initialize output list of containers
+  if (fListOfHistos != NULL){
+    delete fListOfHistos;
+    fListOfHistos = NULL;
+  }
+  if (!fListOfHistos){
+    fListOfHistos = new TList();
+    fListOfHistos->SetOwner(kTRUE); 
+  }
+
+  // TODO add output histograms to this list. E.g.
+  //fListOfHistos->Add(new TH2F("multVsMPI", ";n_{MPI};nch_alice;events", 20, -0.5, 19.5, 301, -0.5, 300.5));
+  
+  PostData(1, fListOfHistos);
+}
+
+//____________________________________________________________________
+void  AliAnalysisTaskNanoSimple::UserExec(Option_t */*option*/)
+{
+  // exec (per event)
+
+  if (!fInputEvent)
+    return;
+  
+  // TODO NOTE Access to custom variables. Use static here for caching of index
+  const Int_t kcstNSigmaTPCPi  = AliNanoAODTrackMapping::GetInstance()->GetVarIndex("cstNSigmaTPCPi");
+  const Int_t kcstNSigmaTPCKa  = AliNanoAODTrackMapping::GetInstance()->GetVarIndex("cstNSigmaTPCKa");
+  const Int_t kcstNSigmaTPCPr  = AliNanoAODTrackMapping::GetInstance()->GetVarIndex("cstNSigmaTPCPr");
+  const Int_t kcstNSigmaTOFPi  = AliNanoAODTrackMapping::GetInstance()->GetVarIndex("cstNSigmaTOFPi");
+  const Int_t kcstNSigmaTOFKa  = AliNanoAODTrackMapping::GetInstance()->GetVarIndex("cstNSigmaTOFKa");
+  const Int_t kcstNSigmaTOFPr  = AliNanoAODTrackMapping::GetInstance()->GetVarIndex("cstNSigmaTOFPr");
+
+  const AliVVertex* vertex = fInputEvent->GetPrimaryVertex();
+  if (vertex->GetNContributors() < 1)
+    return;
+  
+  Printf("Vertex = %f", vertex->GetZ());
+  
+  // for custom variables, cast to nano AOD header
+  AliNanoAODHeader* nanoHeader = dynamic_cast<AliNanoAODHeader*>(fInputEvent->GetHeader());
+  const Int_t kRefMult = nanoHeader->GetVarIndex("MultSelection.RefMult08");
+  if (kRefMult != -1)
+    Printf("Ref Mult = %f", nanoHeader->GetVar(kRefMult));
+  
+  unsigned int nTracks = fInputEvent->GetNumberOfTracks();
+  for (unsigned int i = 0; i < nTracks; i++) {
+    AliVTrack* track = (AliVTrack*) fInputEvent->GetTrack(i);
+    Printf("pt = %f", track->Pt());
+    
+    // for custom variables, cast to nano AOD track
+    AliNanoAODTrack* nanoTrack = dynamic_cast<AliNanoAODTrack*>(track);
+    if (nanoTrack && kcstNSigmaTPCPr != -1)
+      Printf("TPC_sigma_proton = %f  TOF_sigma_proton = %f", nanoTrack->GetVar(kcstNSigmaTPCPr), nanoTrack->GetVar(kcstNSigmaTOFPr));
+  }
+  
+  // V0 access - as usual
+  AliAODEvent* aod = dynamic_cast<AliAODEvent*> (fInputEvent);
+  if (aod->GetV0s()) {
+    for (int i = 0; i < aod->GetNumberOfV0s(); i++)
+      Printf("V0 %d: dca = %f", i, aod->GetV0(i)->DcaV0ToPrimVertex());
+  }
+}
---
+++ PWG/DevNanoAOD/tutorial/filterAOD_V0s.C
@@ -0,0 +1,57 @@
+#include "../AddTaskNanoAODFilter.C"
+
+void filterAOD_V0s()
+{
+  AliAnalysisManager* mgr = new AliAnalysisManager("NanoAOD Filter", "NanoAOD filter for nanoAOD production");
+    
+  AliAODInputHandler* iH = new AliAODInputHandler();
+  mgr->SetInputEventHandler(iH);
+
+  // Define aod output handler
+  AliAODHandler* aodOutputHandler = new AliAODHandler();
+  aodOutputHandler->SetOutputFileName("AliAOD.NanoAOD.root");
+  mgr->SetOutputEventHandler(aodOutputHandler);
+  
+  AliAnalysisTaskNanoAODFilter* task = (AliAnalysisTaskNanoAODFilter*) AddTaskNanoAODFilter(0, kFALSE);
+  AliNanoAODSimpleSetter* setter = new AliNanoAODSimpleSetter;
+  task->SetSetter(setter);
+  
+  // Event selection
+  // filter bit
+  task->SelectCollisionCandidates(AliVEvent::kINT7);
+  
+  AliAnalysisNanoAODEventCuts* evtCuts = new AliAnalysisNanoAODEventCuts;
+  evtCuts->SetVertexRange(8);
+  evtCuts->SetCutPileUpMV(kTRUE);
+
+  // Track selection
+  AliAnalysisNanoAODTrackCuts* trkCuts = new AliAnalysisNanoAODTrackCuts;
+  trkCuts->SetBitMask((1 << 8) | (1 << 9)); // hybrid 2011
+  trkCuts->SetMaxEta(0.9);
+  trkCuts->SetMinPt(1.0);
+  
+  // Fields to store
+  // event level
+  // Note: vertices are kept by default
+  task->SetVarListHead("OfflineTrigger,MagField");
+  // track level
+  task->SetVarList("pt,theta,phi");
+
+  task->SetTrkCuts(trkCuts);
+  task->SetEvtCuts(evtCuts);
+  
+  // V0s
+  task->ReplicatorSaveV0s(kTRUE, new AliAnalysisNanoAODV0Cuts);
+
+  mgr->SetDebugLevel(1); // enable debug printouts
+  if (!mgr->InitAnalysis()) 
+    return;
+  mgr->PrintStatus();
+  
+  // Input files
+  TChain * chain = new TChain("aodTree");
+  chain->Add("AliAOD.root");
+
+  Printf("Starting Analysis....");
+  mgr->StartAnalysis("local", chain, 10);
+}
