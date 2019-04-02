ae98b477517f4cae26fbabc894dc09c94618f7e2
Francesco Prino <prino@to.infn.it>
2019-04-03 00:06:18 +0200

    Store cut values in the output

--- PWGPP/global/AliAnalysisTaskTrackingEffPID.cxx
+++ PWGPP/global/AliAnalysisTaskTrackingEffPID.cxx
@@ -73,6 +73,7 @@ AliAnalysisTaskTrackingEffPID::AliAnalysisTaskTrackingEffPID() :
   fFilterBit{4},
   fTrackCuts{0x0},
   fOutputList{0x0},
+  fListCuts{0x0},
   fHistNEvents{0x0}
 {
   // default: use the filter bit 4 cuts
@@ -87,6 +88,7 @@ AliAnalysisTaskTrackingEffPID::AliAnalysisTaskTrackingEffPID() :
 
   DefineInput(0, TChain::Class());
   DefineOutput(1, TList::Class());
+  DefineOutput(2, TList::Class());
 }
 
 /// Standard destructor
@@ -94,6 +96,7 @@ AliAnalysisTaskTrackingEffPID::AliAnalysisTaskTrackingEffPID() :
 AliAnalysisTaskTrackingEffPID::~AliAnalysisTaskTrackingEffPID(){
   if (AliAnalysisManager::GetAnalysisManager()->IsProofMode()) return;
   if (fOutputList) delete fOutputList;
+  if (fListCuts) delete fListCuts;
   if (fTrackCuts) delete fTrackCuts;
 }
 
@@ -175,6 +178,20 @@ void AliAnalysisTaskTrackingEffPID::UserCreateOutputObjects() {
     }
   }
   fEventCut.AddQAplotsToList(fOutputList);
+
+  fListCuts = new TList();
+  fListCuts->SetOwner();
+  if(fTrackCuts){
+    AliESDtrackCuts* ttosave=new AliESDtrackCuts(*fTrackCuts);
+    fListCuts->Add(ttosave);
+    TH1F* hAODCuts=new TH1F("hAODCuts","",2,0.,2.);
+    hAODCuts->GetXaxis()->SetBinLabel(1,"filter bit");
+    hAODCuts->SetBinContent(1,fFilterBit);
+    hAODCuts->GetXaxis()->SetBinLabel(2,"use track cuts for AOD");
+    hAODCuts->SetBinContent(2,fUseTrackCutsForAOD);
+    fListCuts->Add(hAODCuts);
+  }
+  PostData(2, fListCuts);
 
   PostData(1,fOutputList);
 }
