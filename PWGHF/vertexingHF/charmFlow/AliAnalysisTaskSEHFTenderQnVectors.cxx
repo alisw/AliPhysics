03ca6cbfc605f9789d3ff49f8438966cdbcbf10b
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-04-05 09:33:13 +0200

    Add histograms for TPC Q-vector calibration

--- PWGHF/vertexingHF/charmFlow/AliAnalysisTaskSEHFTenderQnVectors.cxx
+++ PWGHF/vertexingHF/charmFlow/AliAnalysisTaskSEHFTenderQnVectors.cxx
@@ -33,6 +33,8 @@ AliAnalysisTaskSEHFTenderQnVectors::AliAnalysisTaskSEHFTenderQnVectors() :
     fOutputList(nullptr),
     fHistNEvents(nullptr),
     fHistCentrality(nullptr),
+    fEnableTPCPhiVsCentrDistr(false),
+    fEnableQvecTPCVsCentrDistr(false),
     fHFQnVecHandler(nullptr),
     fHarmonic(2),
     fCalibType(AliHFQnVectorHandler::kQnCalib),
@@ -40,7 +42,6 @@ AliAnalysisTaskSEHFTenderQnVectors::AliAnalysisTaskSEHFTenderQnVectors() :
     fOADBFileName(""),
     fAOD(nullptr),
     fPrevEventRun(-1),
-    fPhiVsCentrDistr(nullptr),
     fTriggerClass(""),
     fTriggerMask(AliVEvent::kAny)
 {
@@ -51,7 +52,10 @@ AliAnalysisTaskSEHFTenderQnVectors::AliAnalysisTaskSEHFTenderQnVectors() :
         fHistEventPlaneTPC[iDet] = nullptr;
         fHistEventPlaneV0[iDet] = nullptr;
         fSplineListqnPercTPC[iDet] = nullptr;
-        fSplineListqnPercV0[iDet] = nullptr;                       
+        fSplineListqnPercV0[iDet] = nullptr;   
+        fQvecTPCVsCentrDistr[iDet] = nullptr;
+        if(iDet<2)
+            fTPCPhiVsCentrDistr[iDet] = nullptr;                                            
     }
 }
 
@@ -61,6 +65,8 @@ AliAnalysisTaskSEHFTenderQnVectors::AliAnalysisTaskSEHFTenderQnVectors(const cha
     fOutputList(nullptr),
     fHistNEvents(nullptr),
     fHistCentrality(nullptr),
+    fEnableTPCPhiVsCentrDistr(false),
+    fEnableQvecTPCVsCentrDistr(false),
     fHFQnVecHandler(nullptr),
     fHarmonic(harmonic),
     fCalibType(calibType),
@@ -68,7 +74,6 @@ AliAnalysisTaskSEHFTenderQnVectors::AliAnalysisTaskSEHFTenderQnVectors(const cha
     fOADBFileName(oadbFileName),
     fAOD(nullptr),
     fPrevEventRun(-1),
-    fPhiVsCentrDistr(nullptr),
     fTriggerClass(""),
     fTriggerMask(AliVEvent::kAny)
 {
@@ -80,11 +85,15 @@ AliAnalysisTaskSEHFTenderQnVectors::AliAnalysisTaskSEHFTenderQnVectors(const cha
         fHistEventPlaneV0[iDet] = nullptr;
         fSplineListqnPercTPC[iDet] = nullptr;
         fSplineListqnPercV0[iDet] = nullptr;                       
+        fQvecTPCVsCentrDistr[iDet] = nullptr;
+        if(iDet<2)
+            fTPCPhiVsCentrDistr[iDet] = nullptr;                        
     }
-    
+
     DefineInput(0, TChain::Class());
     DefineOutput(1, TList::Class());
-    DefineOutput(2, TList::Class());
+    DefineOutput(2, TH2F::Class());
+    DefineOutput(3, TH2F::Class());
 }
 
 //________________________________________________________________________
@@ -105,7 +114,12 @@ AliAnalysisTaskSEHFTenderQnVectors::~AliAnalysisTaskSEHFTenderQnVectors()
         delete fOutputList;
     }
     if(fHFQnVecHandler) delete fHFQnVecHandler;
-    if(fPhiVsCentrDistr) delete fPhiVsCentrDistr;
+    for(int iDet=0; iDet<3; iDet++) {
+        if(fQvecTPCVsCentrDistr[iDet]) delete fQvecTPCVsCentrDistr[iDet];
+        if(iDet<2) {
+            if(fTPCPhiVsCentrDistr[iDet]) delete fTPCPhiVsCentrDistr[iDet];
+        }
+    }
 }
 
 //________________________________________________________________________
@@ -138,12 +152,36 @@ void AliAnalysisTaskSEHFTenderQnVectors::UserCreateOutputObjects()
         fOutputList->Add(fHistEventPlaneV0[iDet]);
     }
 
-    fPhiVsCentrDistr = new TList();
-    fPhiVsCentrDistr->SetOwner(true);
+    if(fCalibType==AliHFQnVectorHandler::kQnCalib) {
+        if(fEnableTPCPhiVsCentrDistr) {
+            OpenFile(2);
+            fTPCPhiVsCentrDistr[0] = dynamic_cast<TH2F*>(fHFQnVecHandler->GetPhiDistrHistosTPCPosEta()->Clone());
+            OpenFile(3);
+            fTPCPhiVsCentrDistr[1] = dynamic_cast<TH2F*>(fHFQnVecHandler->GetPhiDistrHistosTPCNegEta()->Clone());
+        }
+        if(fEnableQvecTPCVsCentrDistr) {
+            OpenFile(4);
+            fQvecTPCVsCentrDistr[0] = new TH2F("fQvecTPCVsCentrDistr",Form(";centrality (%%);#it{#Q_{%d}^{TPC}}",fHarmonic),10,0,100,100,0,5000);
+            OpenFile(5);
+            fQvecTPCVsCentrDistr[1] = new TH2F("fQvecTPCVsCentrDistrPosEta",Form(";centrality (%%);#it{#Q_{%d}^{TPCPosEta}}",fHarmonic),10,0,100,100,0,5000);
+            OpenFile(6);
+            fQvecTPCVsCentrDistr[2] = new TH2F("fQvecTPCVsCentrDistrNegEta",Form(";centrality (%%);#it{#Q_{%d}^{TPCNegEta}}",fHarmonic),10,0,100,100,0,5000);
+        }
+    }
 
     // post data
     PostData(1, fOutputList);
-    PostData(2, fPhiVsCentrDistr);
+    if(fCalibType==AliHFQnVectorHandler::kQnCalib) {
+        if(fEnableTPCPhiVsCentrDistr) {
+            PostData(2, fTPCPhiVsCentrDistr[0]);
+            PostData(3, fTPCPhiVsCentrDistr[1]);
+        }
+        if(fEnableQvecTPCVsCentrDistr) {
+            PostData(4, fQvecTPCVsCentrDistr[0]);
+            PostData(5, fQvecTPCVsCentrDistr[1]);
+            PostData(6, fQvecTPCVsCentrDistr[2]);
+        }
+    }
 }
 
 //________________________________________________________________________
@@ -216,25 +254,46 @@ void AliAnalysisTaskSEHFTenderQnVectors::UserExec(Option_t */*option*/)
     fHistEventPlaneV0[1]->Fill(PsinV0A);
     fHistEventPlaneV0[2]->Fill(PsinV0C);
 
-    TH2F* hPhiPosEta = fHFQnVecHandler->GetPhiDistrHistosTPCPosEta();
-    TH2F* hPhiNegEta = fHFQnVecHandler->GetPhiDistrHistosTPCNegEta();
+    int runnumber = fAOD->GetRunNumber();
 
-    if(fPrevEventRun!=fAOD->GetRunNumber()) {
-        if(hPhiPosEta) fPhiVsCentrDistr->Add((TH2F*)hPhiPosEta->Clone(Form("%s_%d",hPhiPosEta->GetName(),fAOD->GetRunNumber())));
-        if(hPhiNegEta) fPhiVsCentrDistr->Add((TH2F*)hPhiNegEta->Clone(Form("%s_%d",hPhiNegEta->GetName(),fAOD->GetRunNumber())));
-    }
-    else {
-        TH2F* hPhiPosEtaLast = (TH2F*)fPhiVsCentrDistr->FindObject(Form("%s_%d",hPhiPosEta->GetName(),fAOD->GetRunNumber()));
-        hPhiPosEtaLast->Add(hPhiPosEta);
-        TH2F* hPhiNegEtaLast = (TH2F*)fPhiVsCentrDistr->FindObject(Form("%s_%d",hPhiNegEta->GetName(),fAOD->GetRunNumber()));
-        hPhiNegEtaLast->Add(hPhiNegEta);
-    }
+    if(fCalibType==AliHFQnVectorHandler::kQnCalib) {
+        if(fEnableTPCPhiVsCentrDistr) {
+            TH2F* hPhiPosEta = fHFQnVecHandler->GetPhiDistrHistosTPCPosEta();
+            TH2F* hPhiNegEta = fHFQnVecHandler->GetPhiDistrHistosTPCNegEta();
+            if(runnumber!=fPrevEventRun) {
+                fTPCPhiVsCentrDistr[0]->SetName(Form("%s_%d",hPhiPosEta->GetName(),runnumber));
+                fTPCPhiVsCentrDistr[1]->SetName(Form("%s_%d",hPhiNegEta->GetName(),runnumber));
+            }
 
-    fPrevEventRun = fAOD->GetRunNumber();
+            if(hPhiPosEta) 
+                fTPCPhiVsCentrDistr[0]->Add((TH2F*)hPhiPosEta->Clone());
+            if(hPhiNegEta) 
+                fTPCPhiVsCentrDistr[1]->Add((TH2F*)hPhiNegEta->Clone());
+
+            PostData(2, fTPCPhiVsCentrDistr[0]);    
+            PostData(3, fTPCPhiVsCentrDistr[1]);    
+        }
+        if(fEnableQvecTPCVsCentrDistr) {
+            if(runnumber!=fPrevEventRun) {
+                for(int iDet=0; iDet<3; iDet++)
+                    fQvecTPCVsCentrDistr[iDet]->SetName(Form("%s_%d",fQvecTPCVsCentrDistr[iDet]->GetName(),runnumber));
+            }
+
+            double QnVecFullTPC[2], QnVecPosTPC[2], QnVecNegTPC[2];
+            fHFQnVecHandler->GetUnNormQnVecTPC(QnVecFullTPC, QnVecPosTPC, QnVecNegTPC);
+            fQvecTPCVsCentrDistr[0]->Fill(cent, TMath::Sqrt(QnVecFullTPC[0]*QnVecFullTPC[0]+QnVecFullTPC[1]*QnVecFullTPC[1]));
+            fQvecTPCVsCentrDistr[1]->Fill(cent, TMath::Sqrt(QnVecPosTPC[0]*QnVecPosTPC[0]+QnVecPosTPC[1]*QnVecPosTPC[1]));
+            fQvecTPCVsCentrDistr[2]->Fill(cent, TMath::Sqrt(QnVecNegTPC[0]*QnVecNegTPC[0]+QnVecNegTPC[1]*QnVecNegTPC[1]));
+
+            PostData(4, fQvecTPCVsCentrDistr[0]);
+            PostData(5, fQvecTPCVsCentrDistr[1]);
+            PostData(6, fQvecTPCVsCentrDistr[2]);
+        }
+    }
+    fPrevEventRun = runnumber;
 
     // Post output data
-    PostData(1, fOutputList);
-    PostData(2, fPhiVsCentrDistr);
+    PostData(1, fOutputList);    
 }
 
 //________________________________________________________________________
