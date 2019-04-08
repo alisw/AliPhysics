40bff6a5e70ed646b975a681c1e328b115e50c0e
cterrevo <cristina.terrevoli@cern.ch>
2019-04-08 10:43:20 +0200

    restored to previous version

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTaskDCArStudy.C
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/macros/AddTaskDCArStudy.C
@@ -1,7 +1,3 @@
-#if defined(__CLING__)
-#include "AliAnalysisTaskDCArStudy.h"
-#endif
-
 AliAnalysisTaskDCArStudy* AddTaskDCArStudy() {
   return AliAnalysisTaskDCArStudy::AddTaskDCArStudy("TaskDCArStudy");
 }
