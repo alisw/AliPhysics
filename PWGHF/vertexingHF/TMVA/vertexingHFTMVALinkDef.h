bfb110c29d6bd98848386d465975c902ac0295c8
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2019-04-12 11:08:41 +0200

    Weights from LHC19c2b (semicentral) for BDT (TMVA approach)

--- PWGHF/vertexingHF/TMVA/vertexingHFTMVALinkDef.h
+++ PWGHF/vertexingHF/TMVA/vertexingHFTMVALinkDef.h
@@ -8,5 +8,10 @@
 #pragma link C++ class BDT_DefaultNode+;
 #pragma link C++ class ReadBDT_Default_6_12+;
 #pragma link C++ class ReadBDT_Default_4_6+;
+#pragma link C++ class ReadBDT_Default_LHC19c2b_4_5+;
+#pragma link C++ class ReadBDT_Default_LHC19c2b_5_6+;
+#pragma link C++ class ReadBDT_Default_LHC19c2b_4_6+;
+#pragma link C++ class ReadBDT_Default_LHC19c2b_6_12+;
+#pragma link C++ class ReadBDT_Default_LHC19c2b_12_25+;
 
 #endif
