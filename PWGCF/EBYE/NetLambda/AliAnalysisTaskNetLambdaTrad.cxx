7a87ec3233b7e092a87da729db33cee6aba9ce84
Ejiro Umaka <enumaka@uh.edu>
2019-04-13 13:01:50 -0600

    Update AliAnalysisTaskNetLambdaTrad.cxx

--- PWGCF/EBYE/NetLambda/AliAnalysisTaskNetLambdaTrad.cxx
+++ PWGCF/EBYE/NetLambda/AliAnalysisTaskNetLambdaTrad.cxx
@@ -245,7 +245,7 @@ void AliAnalysisTaskNetLambdaTrad::UserCreateOutputObjects()
     
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     //THNSPARSE BINNING
-    const Int_t dim = 47; //31 pt bins*2 + 1 cent bin
+    const Int_t dim = 47; //23 pt bins*2 + 1 cent bin
     Int_t bin[dim];
     bin[0] = 81;
     for(Int_t ibin = 1; ibin < dim; ibin++) bin[ibin] = 500;
