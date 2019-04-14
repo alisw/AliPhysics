22ef329dc7236cd92ed6853e64db266bdd243803
Bong-Hwi Lim <bonghwilim@gmail.com>
2019-04-15 00:00:12 +0200

    removing sensitive information

--- PWGLF/RESONANCES/PostProcessing/Xi1530/macros_pp13TeV/extract.sh
+++ PWGLF/RESONANCES/PostProcessing/Xi1530/macros_pp13TeV/extract.sh
@@ -30,15 +30,3 @@ echo "========================================="
 root -b -q -x DrawXi1530.C+\($sys\,$multi_start\,$multi_end\,\"$inputoptions\",$optionnumber\) 2>&1
 echo "############## memory after: ##############"
 free -m
-
-
-DATETIME=$(eval date)
-TOKEN=582112631:AAEvRb36PE2lJoplyLoXK3NWAavezCX2f1Q
-CHAT_ID=559725700
-MESSAGE="$DATETIME Job Finished. Options: $1 [$2-$3] $4"
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-
-#OUTPUT="$(ls AnalysisResults_Extracted*)"
-#alien_cp -n $OUTPUT alien:///alice/cern.ch/user/b/blim/Xi1530Extractor/out/$OUTPUT
-
-#curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
