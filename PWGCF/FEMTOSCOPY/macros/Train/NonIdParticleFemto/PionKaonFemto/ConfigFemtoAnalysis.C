2050bdb6a6f7f896a0d5cca4af12979675e47bdd
Ashutosh Kumar Pandey <ashutosh.kumar.pandey@cern.ch>
2019-04-14 19:53:57 +0530

    Modifying Config file for pion-kaon femtoscopy

--- PWGCF/FEMTOSCOPY/macros/Train/NonIdParticleFemto/PionKaonFemto/ConfigFemtoAnalysis.C
+++ PWGCF/FEMTOSCOPY/macros/Train/NonIdParticleFemto/PionKaonFemto/ConfigFemtoAnalysis.C
@@ -206,9 +206,9 @@ AliFemtoManager* ConfigFemtoAnalysis(int runcentrality0, int runcentrality1, int
 	  dtc2etaphitpc[aniter]->SetNsigmaTPCle250(2.0);
 	  dtc2etaphitpc[aniter]->SetNsigmaTPC250_400(2.0);
 	  dtc2etaphitpc[aniter]->SetNsigmaTPC400_450(1.0);
+	  dtc2etaphitpc[aniter]->UseNsigmaTOF450_500(true);
 	  dtc2etaphitpc[aniter]->SetNsigmaTPC450_500(3.0);
 	  dtc2etaphitpc[aniter]->SetNsigmaTOF450_500(2.0);
-	  dtc2etaphitpc[aniter]->UseNsigmaTOF450_500(true);
 	  dtc2etaphitpc[aniter]->SetNsigmaTPCge500(3.0);
 	  dtc2etaphitpc[aniter]->SetNsigmaTOF500_800(2.0);
 	  dtc2etaphitpc[aniter]->SetNsigmaTOF800_1000(1.5);
