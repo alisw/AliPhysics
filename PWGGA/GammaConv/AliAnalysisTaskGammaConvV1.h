b8d1dac3c285e9af919e807d0a80acc6cfae1d2b
Lizette Lamers <lizette.jacqueline.lamers@cern.ch>
2019-04-24 12:07:38 +0200

    Increased ClassDef

--- PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.h
+++ PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.h
@@ -394,7 +394,7 @@ class AliAnalysisTaskGammaConvV1 : public AliAnalysisTaskSE {
 
     AliAnalysisTaskGammaConvV1(const AliAnalysisTaskGammaConvV1&); // Prevent copy-construction
     AliAnalysisTaskGammaConvV1 &operator=(const AliAnalysisTaskGammaConvV1&); // Prevent assignment
-    ClassDef(AliAnalysisTaskGammaConvV1, 44);
+    ClassDef(AliAnalysisTaskGammaConvV1, 45);
 };
 
 #endif
