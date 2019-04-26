9f424e05235540a24af20fe5534c7cc224e6fb49
miranov25 <marian.ivanov@cern.ch>
2019-04-26 07:51:08 +0200

    ATO-465 - change GSI test setup

--- test/configuration/configGSI.sh
+++ test/configuration/configGSI.sh
@@ -6,6 +6,6 @@
 #  Input test data sample description 
 #
 #export TestData_pPb="TO BE FILLED - text file with the list of ESd files"
-export TestData_pPb=/hera/alice/local/UnitTest/testData/testEsdpPb5.list
+export TestData_pPb=$NOTESData/JIRA/ATO-465/testFilter/esd.list
 #export OCDBPath_pPb="TO BE FILLED - ocdb path for the  p-Pb run e.g local:///cvmfs/alice.gsi.de/alice/data/2013/OCDB/"
 export OCDBPath_pPb=local:///cvmfs/alice.cern.ch/calibration/data/2013/OCDB/
