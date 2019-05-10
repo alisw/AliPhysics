aba6c1d462959cfc9065fe98382c37840b95735b
Michael Hartung <michael.hartung@gmx.net>
2019-05-10 14:35:48 +0000

    Added Bethe Bloch parameters for 2017/2018

--- PWGLF/NUCLEX/Hypernuclei/Hyp2Body/AddTask_HypTritEventTree.C
+++ PWGLF/NUCLEX/Hypernuclei/Hyp2Body/AddTask_HypTritEventTree.C
@@ -10,23 +10,23 @@ AliAnalysisTask *AddTask_HypTritEventTree(UInt_t triggerMask = AliVEvent::kINT7,
   //task->SetPeriod(period);
   task->SetBetheSplines(betheSplines);
 
-/* 
   // MC LHC12d3
 //  Double_t he3Params[6] = {1.79043,18.0066,0.00212257,2.24677,4.25945,0.06892};
 //  Double_t tParams[6] = {2.32603,19.2492,30.7943,2.16971,-8.11114, 0.09311};
-// DATA LHC15o
-    if(period == 2015){
+  // DATA LHC15o
+/*if(period == 2015){
     Double_t he3Params[6] = {2.45605,19.8067,-0.774729,1.96279,0.172695, 0.05655};
     Double_t tParams[6] = {2.32603,19.2492,30.7943,2.16971,-8.11114, 0.09311};
   }
-    if(period == 2016){
+  else if(period == 2016){
     Double_t he3Params[6] = {1.69155, 27.4992, 4.00313e-15, 2.48485, 8.31768, 0.05655};
     Double_t tParams[6] = {1.69155, 27.4992, 4.00313e-15, 2.48485, 8.31768, 0.09311};
-*/ 
+  }
+*/
     Double_t he3Params[6];
     Double_t tParams[6];
 
-    if (period ==2017){
+    if (period == 2017){
     he3Params[0] = 3.20025;
     he3Params[1] = 16.4971;
     he3Params[2] = -0.0116571;
