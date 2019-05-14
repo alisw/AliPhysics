374631abf31375caf64f25608b5b0e1281fe3656
Bernhard Hohlweger <hohlweger@mytum.de>
2019-05-14 10:56:01 +0200

    Fixes a bug

--- PWGCF/FEMTOSCOPY/FemtoDream/AliAnalysisTaskNanoXioton.cxx
+++ PWGCF/FEMTOSCOPY/FemtoDream/AliAnalysisTaskNanoXioton.cxx
@@ -210,7 +210,7 @@ void AliAnalysisTaskNanoXioton::UserExec(Option_t *option) {
           < static_cast<TClonesArray *>(aodEvt->GetCascades())->GetEntriesFast();
       ++iCasc) {
     AliAODcascade* casc = aodEvt->GetCascade(iCasc);
-    fCascade->SetCascade(fInputEvent, casc);
+    fCascade->SetCascade(aodEvt, casc);
     if (fXi->isSelected(fCascade)) {
       Xis.push_back(*fCascade);
     }
