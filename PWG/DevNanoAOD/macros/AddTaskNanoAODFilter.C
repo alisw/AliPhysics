eb9bb6a51f2f45f7a255887234cfa9d04144b133
Jan Fiete Grosse-Oetringhaus <jgrosseo@cern.ch>
2019-05-16 14:04:34 +0200

    adding normalisation object to output list of nano producer task

--- PWG/DevNanoAOD/macros/AddTaskNanoAODFilter.C
+++ PWG/DevNanoAOD/macros/AddTaskNanoAODFilter.C
@@ -20,11 +20,11 @@ AliAnalysisTaskSE* AddTaskNanoAODFilter(Int_t iMC, Bool_t savecuts = 0)
   task->SetMCMode(iMC);
 
   mgr->ConnectInput (task, 0, mgr->GetCommonInputContainer());
-  
-  if(savecuts) {
-    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("qa", TList::Class(), AliAnalysisManager::kOutputContainer, Form("%s:%s", AliAnalysisManager::GetCommonFileName(), "NanoAODFilter"));
-    mgr->ConnectOutput(task, 1, coutput1);
-  }
+
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("normalisation", AliNanoFilterNormalisation::Class(), AliAnalysisManager::kOutputContainer, Form("%s:%s", AliAnalysisManager::GetCommonFileName(), "NanoAODFilter"));
+  mgr->ConnectOutput(task, 1, coutput1);
+  AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("qa", TList::Class(), AliAnalysisManager::kOutputContainer, Form("%s:%s", AliAnalysisManager::GetCommonFileName(), "NanoAODFilter"));
+  mgr->ConnectOutput(task, 2, coutput2);
 
   return task;
 }
