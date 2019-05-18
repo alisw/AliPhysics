a9ca2cc36397e8a056757d90479a5fa09cd084b8
laltenka <lucas.altenkamper@cern.ch>
2019-05-18 16:38:25 +0200

    PWGDQ/reducedTree/AliReducedVarManager: added variable for EMCal nSigma electron

--- PWGDQ/reducedTree/AliReducedVarManager.h
+++ PWGDQ/reducedTree/AliReducedVarManager.h
@@ -612,6 +612,7 @@ class AliReducedVarManager : public TObject {
     kEMCALmatchedDeltaPhi,
     kEMCALmatchedDeltaEta,
     kEMCALmatchedDistance,
+    kEMCALmatchedNSigmaElectron,
     kNTrackVars,            // variable to mark end of track vars, introduce new tracks vars before this one
     // Calorimeter cluster variables --------------------------------------
     kEMCALclusterEnergy,        
