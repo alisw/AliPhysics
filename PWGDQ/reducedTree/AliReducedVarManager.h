03e3a131ab0a089d5d6e2256fcfcdcd6e886de20
Xiaozhi Bai <xiaozhi.bai@cern.ch>
2019-07-07 19:36:35 +0200

    add the pt reweight spectra

--- PWGDQ/reducedTree/AliReducedVarManager.h
+++ PWGDQ/reducedTree/AliReducedVarManager.h
@@ -818,7 +818,8 @@ class AliReducedVarManager : public TObject {
   static Bool_t fgOptionRecenterTPCqVec;           //option to do Q vector recentering for TPC
   static Bool_t fgOptionEventRes;                 //option to divide by resolution
 
-  static TH1F* fgpT_reweight_centrality_0_10;  // 
+  static TH1F* fgpT_reweight_centrality_0_10;  // ratio between nature pt shape and gernareted pT shape
+  
   AliReducedVarManager(AliReducedVarManager const&);
   AliReducedVarManager& operator=(AliReducedVarManager const&);  
   
