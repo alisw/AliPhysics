07f2d23042798deab7895e224a329b385a0d2fcf
Omar <omar.vaszquez.rueda@cern.ch>
2019-07-11 15:44:09 +0200

    Including Rt task at the rTPC

--- PWGMM/UE/PWGMMueLinkDef.h
+++ PWGMM/UE/PWGMMueLinkDef.h
@@ -1,7 +1,7 @@
 #ifdef __CINT__
 
 //underlying event using transverse spherocity
-#pragma link C++ class AliAnalysisTaskUeSpherocity+;
 #pragma link C++ class AliAnalysisTaskPPvsRT+;
+#pragma link C++ class AliAnalysisTaskUeSpherocity+;
 
 #endif
