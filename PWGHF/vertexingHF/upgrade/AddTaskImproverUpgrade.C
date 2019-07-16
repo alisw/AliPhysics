5218064b547a800b7611ff4df3a0e8586c0b0716
Dimitra Andreou <dimitra.andreou@cern.ch>
2019-07-16 16:49:46 +0200

    correction in AddImprover for upgrade studies

--- PWGHF/vertexingHF/upgrade/AddTaskImproverUpgrade.C
+++ PWGHF/vertexingHF/upgrade/AddTaskImproverUpgrade.C
@@ -1,15 +1,15 @@
-AliAnalysisTaskSEImproveITSUpgrade *AddTaskImproverUpgrade(Bool_t isRunInVertexing=kFALSE, // set to kTRUE to run during AODvertexingHF creation
+AliAnalysisTaskSEImproveITS3 *AddTaskImproverUpgrade(Bool_t isRunInVertexing=kFALSE, // set to kTRUE to run during AODvertexingHF creation
 					       const char *resfileCurURI="ITSgraphs_NewAll-X0.3-Res4um.root",
                                                const char *resfileUpgURI="cylIB_bp17_B05Tnew.root",
 					       Int_t ndebug=0) {
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
-    ::Error("AddAliAnalysisTaskSEImproveITSUpgrade", "No analysis manager to connect to.");
+    ::Error("AddAliAnalysisTaskSEImproveITS3", "No analysis manager to connect to.");
     return 0;
   }
             
-  AliAnalysisTaskSEImproveITSUpgrade *task
-    =new AliAnalysisTaskSEImproveITSUpgrade("ITSImprover",
+  AliAnalysisTaskSEImproveITS3 *task
+    =new AliAnalysisTaskSEImproveITS3("ITSImprover",
                                      resfileCurURI,
                                      resfileUpgURI,
                                      isRunInVertexing,
