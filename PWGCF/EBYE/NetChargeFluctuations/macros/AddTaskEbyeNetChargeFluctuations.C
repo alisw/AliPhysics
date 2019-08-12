4f1cb91c644b92b4c956dc59361c7d0dfcacc37a
shkhan09 <37208106+shkhan09@users.noreply.github.com>
2019-08-12 23:43:58 +0200

    Update class for systematic studies

--- PWGCF/EBYE/NetChargeFluctuations/macros/AddTaskEbyeNetChargeFluctuations.C
+++ PWGCF/EBYE/NetChargeFluctuations/macros/AddTaskEbyeNetChargeFluctuations.C
@@ -1,36 +1,70 @@
 
-AliAnalysisTaskEbyeNetChargeFluctuations* AddTaskEbyeNetChargeFluctuations(Int_t MCthere)
-{
-    // get the manager via the static access member. since it's static, you don't need
+AliAnalysisTaskEbyeChargeFB* AddTaskEbyeNetChargeFluctuations(
+                                                              TString suffixName = "",
+                                                              Bool_t MCthere = kFALSE,
+                                                    //          Int_t MagPolarity = 1,
+                                                              Int_t zvtxcut = 10,
+                                                              Int_t trackBit = 768,
+                                                              Int_t TPCCrossedRows = 100.,
+                                                              Int_t TPCNclus =80.,
+                                                              Double_t DCAxyCut =2.4,
+                                                              Double_t DCAzCut =3.2
+                                                              
+                                                              )
+
+
+{ // get the manager via the static access member. since it's static, you don't need
     // an instance of the class to call the function
+    
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
     if (!mgr) {
         return 0x0;
     }
     
-    // now we create an instance for task
-    AliAnalysisTaskEbyeNetChargeFluctuations* task = new AliAnalysisTaskEbyeNetChargeFluctuations("TaskEbyE");
-    if(!task) return 0x0;
-  //   task->SelectCollisionCandidates(AliVEvent::kINT7);
-    
-    if(MCthere){
-        task->SetAnalysisType(MCthere);
+    if (!mgr->GetInputEventHandler()) {
+        return 0x0;
     }
+    
+    TString fileName = AliAnalysisManager::GetCommonFileName();
+    fileName += ":MyTask";
+    
+    // now we create an instance for task
+    AliAnalysisTaskEbyeChargeFB* task = new AliAnalysisTaskEbyeChargeFB("");
+    if(!task) return 0x0;
+ 
+//    if(MCthere){
+//        task->SetAnalysisType(MCthere);
+//    }
     
     // add your task to the manager
     mgr->AddTask(task);
+    task->SetAnalysisType(MCthere);
+//    task->SetMagPolarity(MagPolarity);
+    task->Setzvtxcut(zvtxcut);
+    task->SettrackBit(trackBit);
+    task->SetTPCCrossedRows(TPCCrossedRows);
+    task->SetNTPCCluster(TPCNclus);
+    task->SetDCACut(DCAxyCut,DCAzCut);
+    
+    // Create containers for input/output
+    TString finDirname     = "_INT7";
+    TString outBasicname   = "EByE";
+    TString profname       = "coutputProf";
+    
+    finDirname        +=   suffixName.Data();
+    outBasicname      +=   finDirname.Data();
+    profname          +=   finDirname.Data();
     
     // your task needs input: here we connect the manager to your task
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
     
     // same for the output
-    mgr->ConnectOutput(task,1,mgr->CreateContainer("fOutputList", TList::Class(), AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-    mgr->ConnectOutput(task,2,mgr->CreateContainer("fTree", TTree::Class(), AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-    mgr->ConnectOutput(task,3,mgr->CreateContainer("fTreeMCrec", TTree::Class(), AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-    mgr->ConnectOutput(task,4,mgr->CreateContainer("fTreeMCallrec", TTree::Class(), AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-    mgr->ConnectOutput(task,5,mgr->CreateContainer("fTreeMCgen", TTree::Class(), AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-    mgr->ConnectOutput(task,6,mgr->CreateContainer("fTreeMCallgen", TTree::Class(), AliAnalysisManager::kOutputContainer, AliAnalysisManager::GetCommonFileName()));
-    
+    mgr->ConnectOutput(task,1,mgr->CreateContainer(outBasicname, TList::Class(),
+                                                   AliAnalysisManager::kOutputContainer,fileName.Data()));
+    mgr->ConnectOutput(task,2,mgr->CreateContainer("fTree", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,3,mgr->CreateContainer("fTreeMCrec", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,4,mgr->CreateContainer("fTreeMCgen", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+  
     return task;
     
 }
