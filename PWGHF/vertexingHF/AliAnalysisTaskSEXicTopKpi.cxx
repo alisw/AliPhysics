6c8af3447fcdbe72d69b74cfbd7f46b1d19cc1d3
Mattia Faggin <mfaggin@cern.ch>
2019-08-19 12:50:18 +0200

    Remove hard-coded changes for the cutobject in the Init() function

--- PWGHF/vertexingHF/AliAnalysisTaskSEXicTopKpi.cxx
+++ PWGHF/vertexingHF/AliAnalysisTaskSEXicTopKpi.cxx
@@ -318,7 +318,7 @@ void AliAnalysisTaskSEXicTopKpi::Init()
     fCutsXic->SetCuts(13,cutsArrayLctopKpi);
   }
   else{
-    
+    /*
     Double_t sigmaVtxMax[8]={0.09,0.09,0.05,0.035,0.035,0.03,0.03,0.025};
     Double_t sumd02[8]={0.0003,0.0003,0.0002,0.00015,0.00015,0.0001,0.,0.};
     if(fAnalysisType==0 || fAnalysisType==4){// assure mass range is large enough
@@ -335,7 +335,8 @@ void AliAnalysisTaskSEXicTopKpi::Init()
 	cutvalues[10][km]=sumd02[km];
       }
       fCutsXic->SetCuts(nvars,nptbins,cutvalues);
-      Printf("Xic Cuts modified to assure mass window is large enough, current cuts are:");
+      Printf("Xic Cuts modified to assure mass window is large enough, current cuts are:");*/
+      Printf("\n--- Adopted cuts ---");
       fCutsXic->PrintAll();
     }
   }
