c7debe92d96575a3abd8d7bf0de9096c75c02132
miranov25 <marian.ivanov@cern.ch>
2019-08-26 15:27:25 +0200

    add html file name as an input parameter

--- PWGPP/AliRootInteractive.h
+++ PWGPP/AliRootInteractive.h
@@ -6,7 +6,7 @@
 
 
 namespace AliRootInteractive{
-  void treeBokehDrawArray(const char * treeName, const char *query, const char *figureArray,  const char *widgets, const char *options);
+  void treeBokehDrawArray(const char * treeName, const char *query, const char *figureArray,  const char *widgets, const char *options, const char *htmlOut= nullptr);
 
   const char *importBokeh= "from Tools.aliTreePlayer import * \n"
                            "from anytree import * \n"
@@ -16,11 +16,12 @@ namespace AliRootInteractive{
 
 
 
-void AliRootInteractive::treeBokehDrawArray(const char * treeName, const char *query, const char *figureArray,  const char *widgets, const char *options){
+void AliRootInteractive::treeBokehDrawArray(const char * treeName, const char *query, const char *figureArray,  const char *widgets, const char *options, const char *htmlOut){
     //initPython();
     TString x=AliRootInteractive::importBokeh;
-    x = x + "tree=ROOT.gROOT.GetGlobal(\""+treeName+"\")\n" +
-            "fig=bokehDrawSA.fromArray(tree ,'" + query + "'," + figureArray + ",'" + widgets + "'," + options+")";
+    x = x + "tree=ROOT.gROOT.GetGlobal(\""+treeName+"\")\n";
+    if (htmlOut) x=x+"output_file('"+htmlOut +"')\n";
+    x=x+"fig=bokehDrawSA.fromArray(tree ,'" + query + "'," + figureArray + ",'" + widgets + "'," + options+")";
     TPython::Exec(x);
 }
 #endif
