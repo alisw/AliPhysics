ea27f8378cd7ddc21b8b0e6fa2b0ee37ddc2b34d
Kirill Lapidus <kirill.lapidus@cern.ch>
2019-08-28 10:44:11 +0200

    AddTask-s amended

--- PWGJE/EMCALJetTasks/macros/AddTaskSDKLResponse.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskSDKLResponse.C
@@ -1,6 +1,16 @@
-AliAnalysisTaskSDKLResponse* AddTaskSDKLResponse(const char *ntracks, const char *njets1, const char *njets2, const char *nrho, Int_t nCentBins,
-                                                                       Double_t jetradius, Double_t jetptcut, Double_t jetareacut, const char *type, Int_t backgroption,
-                                                                       Int_t leadhadtype, Double_t fractioneventsfortree, const char *taskname)
+AliAnalysisTaskSDKLResponse* AddTaskSDKLResponse(const char *ntracks = "usedefault",
+                                                 const char *njets1 = "Jets1",
+                                                 const char *njets2 = "Jets2",
+                                                 const char *nrho = "Rho",
+                                                 Int_t nCentBins = 1,
+                                                 Double_t jetradius = 0.4,
+                                                 Double_t jetptcut = 1.0,
+                                                 Double_t jetareacut = 0.6,
+                                                 const char *type = "EMCAL",
+                                                 Int_t backgroption = 0,
+                                                 Int_t leadhadtype = 0,
+                                                 Double_t fractioneventsfortree = 1.e-6,
+                                                 const char *taskname = "AliAnalysisTaskSDKLResponse")
 {
   return AliAnalysisTaskSDKLResponse::AddTaskSoftDropResponse(ntracks, njets1, njets2, nrho, nCentBins, jetradius, jetptcut, jetareacut, type, backgroption, leadhadtype, fractioneventsfortree, taskname);
 }
