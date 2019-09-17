dc21c4d353e7983b0d8e7c5d2bab33152bbf2a06
David Horak <david.horak@cern.ch>
2019-09-17 15:40:11 +0200

    cout error fixed

--- PWGUD/UPC/AliAnalysisTaskUpcRho0.cxx
+++ PWGUD/UPC/AliAnalysisTaskUpcRho0.cxx
@@ -134,7 +134,7 @@ void AliAnalysisTaskUpcRho0::LocalInit()
 		if (fEfficiencyFileName.Contains("alien")) fSPDfile = TFile::Open(fEfficiencyFileName.Data());  // private efficiency file
 		else fSPDfile = AliDataFile::OpenOADB(fEfficiencyFileName.Data()); // open OADB effciency file
 		if (!fSPDfile) {
-			cout<<"Efficiency file cannot be open..."<<endl;
+			std::cout<<"Efficiency file cannot be open..."<<endl;
 			return;
 		}
 	}
