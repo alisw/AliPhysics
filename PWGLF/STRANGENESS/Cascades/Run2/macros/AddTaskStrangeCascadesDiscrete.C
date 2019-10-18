01c254c55549e541bb38010ecf4e2ac1cfe66d83
Georgijs Skorodumovs <gskorodu@cern.ch>
2019-10-18 11:04:09 +0000

    Unused parts are removed. New features added.

--- PWGLF/STRANGENESS/Cascades/Run2/macros/AddTaskStrangeCascadesDiscrete.C
+++ PWGLF/STRANGENESS/Cascades/Run2/macros/AddTaskStrangeCascadesDiscrete.C
@@ -1,24 +1,30 @@
-AliAnalysisTaskStrangeCascadesDiscrete *AddTaskStrangeCascadesDiscrete(Bool_t lSaveEventTree = kTRUE,
-                                                                                 Bool_t lSaveV0Tree = kFALSE,
-                                                                                 Bool_t lSaveCascadeTree = kTRUE,
-                                                                                 Bool_t lRunVertexers = kFALSE,
-                                                                                 Bool_t lUseLightVertexer = kFALSE,
-                                                                                 Bool_t lMomentaNeeded = kFALSE, //9 branches of p,pz,pt of daughters
-                                                                                 Bool_t lExtraInfoCascadeKinematics = kTRUE,
-                                                                                 Bool_t lDiscreteSymmetryInfo = kFALSE, //extra branch in cascade tree
-                                                                                 Bool_t lMassesAndBackgroundFromScratch = kFALSE, //extra branch in cascade tree
-                                                                                 Double_t lCascaderMaxChi2 = 33.,
-                                                                                 Double_t lCascaderV0MinImpactParam = 0.05, //0.050 per def
-                                                                                 Double_t lCascaderV0MassWindow = 0.01, //0.010 per def
-                                                                                 Double_t lCascaderBachMinImpactParam = 0.03, //0.03 per def
-                                                                                 Double_t lCascaderMaxDCAV0andBach = 2.0, //2.0 per def
-                                                                                 Double_t lCascaderMinCosAngle = 0.95, //0.95 per def
-                                                                                 Double_t lCascaderMinRadius = 0.4, //0.4 per def
-                                                                                 Double_t lCascaderMaxRadius =100., //100. per def
-                                                                                 TString lExtraOptions = "",
-                                                                                 const TString lMasterJobSessionFlag = "",
-                                                                                 TString lExtraOutputName = ""
-                                                                                 )
+AliAnalysisTaskStrangeCascadesDiscrete *AddTaskStrangeCascadesDiscrete(
+                                                                       Bool_t lRunV0Vertexers = kTRUE,
+                                                                       Bool_t lRunVertexers = kTRUE, //to rerun the cascade vertexers
+                                                                       Bool_t lUseLightVertexer = kFALSE, //use light cascade vertexers
+                                                                       Bool_t lUseOnTheFlyV0Cascading = kFALSE,
+                                                                       Bool_t lguard_CheckTrackQuality = kTRUE,
+                                                                       Bool_t lguard_CheckCascadeQuality = kTRUE,
+                                                                       Bool_t lguard_CheckTPCPID = kTRUE,
+                                                                       Double_t lV0MaxChi2 = 33.,
+                                                                       Double_t lV0minDCAfirst = 0.01,
+                                                                       Double_t lV0minDCAsecond = 0.01,
+                                                                       Double_t lV0maxDCAdaughters = 2.,
+                                                                       Double_t lV0minCosAngle = 0.6,
+                                                                       Double_t lV0minRadius = 0.4,
+                                                                       Double_t lV0maxRadius = 200.,
+                                                                                     
+                                                                       Double_t lCascaderMaxChi2 = 33.,
+                                                                       Double_t lCascaderV0MinImpactParam = 0.05, //0.050 per def
+                                                                       Double_t lCascaderV0MassWindow = 0.008, //0.010 per def
+                                                                       Double_t lCascaderBachMinImpactParam = 0.03, //0.03 per def
+                                                                       Double_t lCascaderMaxDCAV0andBach = 2.0, //2.0 per def
+                                                                       Double_t lCascaderMinCosAngle = 0.95, //0.95 per def
+                                                                       Double_t lCascaderMinRadius = 0.4, //0.4 per def
+                                                                       Double_t lCascaderMaxRadius =100., //100. per def
+                                                                       Float_t sigmaRangeTPC = 3.,
+                                                                       TString lExtraOutputName = ""
+                                                                       )
 {
     
     
@@ -41,26 +47,27 @@ AliAnalysisTaskStrangeCascadesDiscrete *AddTaskStrangeCascadesDiscrete(Bool_t lS
     
     // Create and configure the task
     AliAnalysisTaskStrangeCascadesDiscrete *taskAuxiliary =
-    new AliAnalysisTaskStrangeCascadesDiscrete(lSaveEventTree,
-                                                    lSaveV0Tree,
-                                                    lSaveCascadeTree,
-                                                    lRunVertexers,
-                                                    lUseLightVertexer,
-                                                    lMomentaNeeded,
-                                                    lExtraInfoCascadeKinematics,
-                                                    lDiscreteSymmetryInfo,
-                                                    lMassesAndBackgroundFromScratch, //extra branch in cascade tree
-                                                    lCascaderMaxChi2,
-                                                    lCascaderV0MinImpactParam,
-                                                    lCascaderV0MassWindow,
-                                                    lCascaderBachMinImpactParam,
-                                                    lCascaderMaxDCAV0andBach,
-                                                    lCascaderMinCosAngle,
-                                                    lCascaderMinRadius,
-                                                    lCascaderMaxRadius,
-                                                    "taskAuxiliary",
-                                                    lExtraOptions
-                                                    );  // const*charname =  Form("taskAuxiliary%s",lExtraOutputName.Data())
+    new AliAnalysisTaskStrangeCascadesDiscrete(lRunV0Vertexers,
+                                                      lRunVertexers, lUseLightVertexer,
+                                               lUseOnTheFlyV0Cascading,
+                                               lguard_CheckTrackQuality,
+                                               lguard_CheckCascadeQuality,
+                                               lguard_CheckTPCPID,
+                                                      lV0MaxChi2,
+                                                      lV0minDCAfirst,
+                                                      lV0minDCAsecond,
+                                                      lV0maxDCAdaughters,
+                                                      lV0minCosAngle,
+                                                      lV0minRadius,
+                                                      lV0maxRadius,
+                                               lCascaderMaxChi2,
+                                               lCascaderV0MinImpactParam,
+                                               lCascaderV0MassWindow,
+                                               lCascaderBachMinImpactParam,
+                                               lCascaderMaxDCAV0andBach,
+                                               lCascaderMinCosAngle,
+                                               lCascaderMinRadius,
+                                               lCascaderMaxRadius, sigmaRangeTPC, "taskAuxiliary");  // const*charname =  Form("taskAuxiliary%s",lExtraOutputName.Data())
     
     mgr->AddTask(taskAuxiliary);
     
@@ -68,14 +75,15 @@ AliAnalysisTaskStrangeCascadesDiscrete *AddTaskStrangeCascadesDiscrete(Bool_t lS
     //   TString results = "AnalysisResults.root";
     if (mgr->GetMCtruthEventHandler()) outputFileName += "_MC";
     
-    
-    AliAnalysisDataContainer* coutput = mgr->CreateContainer("skorodum_cascadetreeAsevent", TTree::Class(),
-                                                             AliAnalysisManager::kOutputContainer,outputFileName);
+    AliAnalysisDataContainer* coutput = mgr->CreateContainer("skorodum_cascadetreeAsevent_scratch", TTree::Class(),
+                                                             AliAnalysisManager::kOutputContainer, outputFileName);
     
     mgr->ConnectInput(taskAuxiliary, 0, mgr->GetCommonInputContainer());
     mgr->ConnectOutput(taskAuxiliary, 1, coutput);
+    
     return taskAuxiliary;
     
 }
+
 
 
