731e373920d28c8571da8233d0acf451b30a747d
Simone Ragoni <simone.ragoni@cern.ch>
2019-10-18 10:55:51 +0000

    Add subwagon feature.

--- PWGUD/UPC/AddTaskADin2018.C
+++ PWGUD/UPC/AddTaskADin2018.C
@@ -1,5 +1,10 @@
-AliAnalysisTaskADin2018* AddTaskADin2018(TString name = "name")
+AliAnalysisTaskADin2018* AddTaskADin2018( const char* suffix = "" )
 {
+
+    TString name = "ADcheck";
+    TString combinedName;
+    combinedName.Form("ADcheck_%s", suffix);
+
     // get the manager via the static access member. since it's static, you don't need
     // to create an instance of the class here to call the function
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
@@ -16,7 +21,7 @@ AliAnalysisTaskADin2018* AddTaskADin2018(TString name = "name")
     TString fileName = AliAnalysisManager::GetCommonFileName();
     fileName += ":MyTask";      // create a subfolder in the file
     // now we create an instance of your task
-    AliAnalysisTaskADin2018* task = new AliAnalysisTaskADin2018(name.Data());
+    AliAnalysisTaskADin2018* task = new AliAnalysisTaskADin2018(combinedName.Data());
     if(!task) return 0x0;
     // task->SelectCollisionCandidates(AliVEvent::kAnyINT);     // Physics Selection used by "everybody" but NOT in UPC
     // add your task to the manager
@@ -24,7 +29,7 @@ AliAnalysisTaskADin2018* AddTaskADin2018(TString name = "name")
     // your task needs input: here we connect the manager to your task
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
     // same for the output
-    mgr->ConnectOutput(task,1,mgr->CreateContainer("MyOutputContainer", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,1,mgr->CreateContainer(combinedName.Data(), TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
     // in the end, this macro returns a pointer to your task. this will be convenient later on
     // when you will run your analysis in an analysis train on grid
     return task;
