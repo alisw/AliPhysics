79bbdfad0c3ba756d0378d95968d4b2e77ad4529
Wioleta Rzesa <01110167@pw.edu.pl>
2019-10-22 14:26:37 +0200

    A new class that allows you to set differnet cuts on DCA for different ranges of pt -- to kanon-proton femto analysis.

---
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoWRzTrackCut.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// AliFemtoWRzTrackCut: An extention to AliFemtoMJTrackCut               //
+// The method allows you to set different cut on DCA for different pt ranges //
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef AliFemtoWRzTrackCut_hh
+#define AliFemtoWRzTrackCut_hh
+
+
+#include "AliFemtoMJTrackCut.h"
+
+class AliFemtoWRzTrackCut : public AliFemtoMJTrackCut{
+
+public:
+   AliFemtoWRzTrackCut();
+   AliFemtoWRzTrackCut(const AliFemtoWRzTrackCut &aCut);
+   virtual ~AliFemtoWRzTrackCut();
+   AliFemtoWRzTrackCut& operator =(const AliFemtoWRzTrackCut &aCut);
+   virtual bool Pass(const AliFemtoTrack* aTrack);
+   virtual AliFemtoString Report();
+   virtual TList *ListSettings();
+   void SetDCAcutPtDependence(const float &DCAxy,const float &DCAz,const float& Pt_min, const float& Pt_max);
+   
+private:
+   float fPt_dca[2]; //bounds for transverse momentum to DCA settings
+   float fDCA_ptdependence[2]; //DCA values (cm) in XY and Z plane for given ranges of pt 
+};
+
+inline void AliFemtoWRzTrackCut::SetDCAcutPtDependence(const float &DCAxy, const float &DCAz, const float& Pt_min,const float& Pt_max){ 
+fPt_dca[0]=Pt_min, fPt_dca[1]=Pt_max,fDCA_ptdependence[0]=DCAxy,fDCA_ptdependence[1]=DCAz;
+}
+
+
+#endif
+
