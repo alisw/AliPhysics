fce3a74e63eda5a82ccb6946962e1395bed9588d
Masahiro Takamura <masahiro.takamura@cern.ch>
2019-11-29 18:01:48 +0900

    Jet unfolding threshold changed

--- PWGGA/GammaConv/AliAnalysisTaskGammaCalo.cxx
+++ PWGGA/GammaConv/AliAnalysisTaskGammaCalo.cxx
@@ -5150,15 +5150,15 @@ void AliAnalysisTaskGammaCalo::CalculatePi0Candidates(){
                             Double_t dotproduct = fVectorJetPx.at(i)*pi0cand->Px() + fVectorJetPy.at(i)*pi0cand->Py() + fVectorJetPz.at(i)*pi0cand->Pz();
                             Double_t magn = pow(fVectorJetPx.at(i),2) + pow(fVectorJetPy.at(i),2) + pow(fVectorJetPz.at(i),2);
                             Double_t z = dotproduct/magn;
-                            if(fVectorJetPt.at(i) >= 5){
+                            if(fVectorJetPt.at(i) >= 10){
                               fHistoUnfoldingAsData[fiCut]->Fill(pi0cand->M(),pi0cand->Pt(), tempPi0CandWeight);
                               fHistoUnfoldingAsDataInvMassZ[fiCut]->Fill(pi0cand->M(), z, tempPi0CandWeight);
                             }
-                            if(fVectorJetPt.at(i) < 5 && fTrueVectorJetPt.at(match) >= 5){
+                            if(fVectorJetPt.at(i) < 10 && fTrueVectorJetPt.at(match) >= 10){
                               fHistoUnfoldingMissed[fiCut]->Fill(pi0cand->M(),pi0cand->Pt(), tempPi0CandWeight);
                               fHistoUnfoldingMissedInvMassZ[fiCut]->Fill(pi0cand->M(), z, tempPi0CandWeight);
                             }
-                            if(fVectorJetPt.at(i) >= 5 && fTrueVectorJetPt.at(match) < 5){
+                            if(fVectorJetPt.at(i) >= 10 && fTrueVectorJetPt.at(match) < 10){
                               fHistoUnfoldingReject[fiCut]->Fill(pi0cand->M(),pi0cand->Pt(), tempPi0CandWeight);
                               fHistoUnfoldingRejectInvMassZ[fiCut]->Fill(pi0cand->M(), z, tempPi0CandWeight);
                             }
