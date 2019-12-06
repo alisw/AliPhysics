e60fb27d1ff0ca4dce66d2062860e7dcb1e2c0a5
Stephan Koenigstorfer <stephan.koenigstorfer@cern.ch>
2019-12-06 10:35:21 +0100

    Added Systematic variable to Ntuple

--- PWGLF/NUCLEX/Nuclei/AbsorptionRatios/macros/AddHighMultHe3Task.C
+++ PWGLF/NUCLEX/Nuclei/AbsorptionRatios/macros/AddHighMultHe3Task.C
@@ -24,11 +24,11 @@ AliAnalysisTaskHe3* AddHighMultHe3Task(TString name = "name", ULong64_t triggerM
 	task->SetLowPCut(0.1);
 	task->SetHighPCut(1e30);
 	task->SetEtaCut(0.8);
-	task->SetMinNITSCl(2);
-	task->SetMaxDCAxyPreCut(1.0);
-	task->SetMaxDCAxyFinal(0.5);
-	task->SetMaxDCAz(0.5);
-	//set PID cuts
+	task->SetMinNITSCl(1);
+	task->SetMaxDCAxyPreCut(1.5);
+	task->SetMaxDCAxyFinal(1.5);
+	task->SetMaxDCAz(1.5);
+	//set PID cuts #### Legacy code not used in analysis anymore ###
 	task->SetMaxTPCnSigma(3.0);
 	task->SetUseTOFPidCut(kFALSE);//kTRUE or kFALSE for use of TOF
 	task->SetMaxTOFnSigma(3.0);
