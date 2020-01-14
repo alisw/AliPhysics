634f8b8343aa1a0b49878e073719ce2b8b92d077
Andreas Morsch <andreas.morsch@cern.ch>
2020-01-14 11:20:06 +0100

    updates for fluka2011-3.0

--- TFluka/scripts/runflukageo.sh
+++ TFluka/scripts/runflukageo.sh
@@ -6,8 +6,16 @@ mkdir tmp
 cd tmp
 
 # Link here some special Fluka files needed
-ln -s $FLUPRO/neuxsc-ind_260.bin neuxsc.bin
-ln -s $FLUPRO/random.dat random.dat
+ln -s $FLUPRO/data/neuxsc.bin  .
+ln -s $FLUPRO/data/elasct.bin  .
+ln -s $FLUPRO/data/gxsect.bin  .
+ln -s $FLUPRO/data/neuxsc.bin  .
+ln -s $FLUPRO/data/nuclear.bin .
+ln -s $FLUPRO/data/sigmapi.bin .
+ln -s $FLUPRO/data/brems_fin.bin .
+ln -s $FLUPRO/data/cohff.bin .
+ln -s $FLUPRO/data/fluodt.dat  .
+ln -s $FLUPRO/data/random.dat  .
 # Copy the random seed
 cp $FLUPRO/random.dat old.seed
 
@@ -17,6 +25,7 @@ ln -s fluka.err fort.15
 #Link FlukaConfig.C as Config.C
 ln -fs $ALICE_ROOT/TFluka/macro/FlukaConfig.C Config.C
 ln -fs $ALICE_ROOT/TFluka/input/coreFlukaVmc.inp coreFlukaVmc.inp
+ln -fs $ALICE_ROOT/TFluka/macro/sim.C sim.C
 echo 'Execute at the root prompt:'
 echo 'AliSimulation sim'
 echo 'sim.Run()'
