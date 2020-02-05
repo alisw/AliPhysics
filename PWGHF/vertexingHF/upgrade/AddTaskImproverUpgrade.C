a5946e1db01701847e2dc893daaa3a9ee6d15e47
Matthew Buckland <mattybuckland@gmail.com>
2020-02-05 21:01:01 +0000

    Moved the isImproveDeuteron of the AddTask from the 2nd to 4th argument

--- PWGHF/vertexingHF/upgrade/AddTaskImproverUpgrade.C
+++ PWGHF/vertexingHF/upgrade/AddTaskImproverUpgrade.C
@@ -1,7 +1,7 @@
 AliAnalysisTaskSEImproveITS3 *AddTaskImproverUpgrade(Bool_t isRunInVertexing=kFALSE, // set to kTRUE to run during AODvertexingHF creation
-					       Bool_t isImproveDeuteron=kFALSE,
                                                const char *resfileCurURI="ITSgraphs_NewAll-X0.3-Res4um.root",
                                                const char *resfileUpgURI="cylIB_bp17_B05Tnew.root",
+					       Bool_t isImproveDeuteron=kFALSE,
 					       Int_t ndebug=0) {
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
