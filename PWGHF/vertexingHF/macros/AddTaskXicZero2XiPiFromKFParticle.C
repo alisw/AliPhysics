2b20ce4ef459e69e2d524e334053e82837f1a039
Jianhui Zhu <zjh@mail.ccnu.edu.cn>
2020-02-14 13:08:34 +0000

    “Add class and macro for Xic0-

---
+++ PWGHF/vertexingHF/macros/AddTaskXicZero2XiPiFromKFParticle.C
@@ -0,0 +1,81 @@
+#if !defined (__CINT__) || defined (__CLING__)
+#include "AliAnalysisManager.h"
+#include "AliAnalysisTaskSEXicZero2XiPifromKFP.h"
+#include <TString.h>
+#include <TList.h>
+#endif
+
+AliAnalysisTaskSEXicZero2XiPifromKFP* AddTaskXicZero2XiPiFromKFParticle(TString finname="", Bool_t IsMC=kFALSE)
+{
+    Bool_t writeXic0RecTree = kTRUE;
+    Bool_t writeXic0MCGenTree = kFALSE;
+    if (IsMC) writeXic0MCGenTree = kTRUE;
+
+    // get the manager via the static access member. since it's static, you don't need
+    // an instance of the class to call the function
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    if (!mgr) {
+        ::Error("AddMyTask", "No analysis manager to connect to.");
+        return NULL;
+    }
+
+    // get the input event handler, again via a static method. 
+    // this handler is part of the managing system and feeds events
+    // to your task
+    if (!mgr->GetInputEventHandler()) {
+        return NULL;
+    }
+
+    // check input cut object
+    Bool_t stdCuts = kFALSE;
+    TFile* fileCuts;
+    if ( finname.EqualTo("") ) {
+      stdCuts = kTRUE;
+    } else {
+      fileCuts = TFile::Open(finname.Data());
+      if( !fileCuts || (fileCuts && !fileCuts->IsOpen()) ) {
+        cout << "Input file not found : check your cut object" << endl;
+        return NULL;
+      }
+    }
+
+    AliRDHFCutsKFP *RDHFCutsKFP = new AliRDHFCutsKFP();
+    if (stdCuts) RDHFCutsKFP->SetStandardCutsPP2010();
+    else RDHFCutsKFP = (AliRDHFCutsKFP*)fileCuts->Get("XicZeroAnalysisCuts");
+    RDHFCutsKFP->SetName("XicZeroAnalysisCuts");
+
+    if (!RDHFCutsKFP) {
+      cout << "Specific AliRDHFCutsKFP not found" << endl;
+      return NULL;
+    }
+
+    printf("CREATE TASK\n");
+
+    // by default, a file is open for writing. here, we get the filename
+    TString fileName = AliAnalysisManager::GetCommonFileName();
+    fileName += ":PWGHF_D2H_KFP_";      // create a subfolder in the file
+    // now we create an instance of your task
+    AliAnalysisTaskSEXicZero2XiPifromKFP* task = new AliAnalysisTaskSEXicZero2XiPifromKFP("AliAnalysisTaskSEXicZero2XiPifromKFP", RDHFCutsKFP);
+
+    if(!task) return NULL;
+    task->SetMC(IsMC);
+    task->SetDebugLevel(1);
+    task->SetWriteXic0MCGenTree(writeXic0MCGenTree);
+    task->SetWriteXic0Tree(writeXic0RecTree);
+    // select type of event
+//    task->SelectCollisionCandidates(AliVEvent::kAnyINT); // kAnyINT = kMB | kINT7 | kINT5 | kINT8 | kSPI7
+    // add your task to the manager
+    mgr->AddTask(task);
+    // your task needs input: here we connect the manager to your task
+    mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
+    // same for the output
+    mgr->ConnectOutput(task,1,mgr->CreateContainer("CutsObj", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,2,mgr->CreateContainer("Counter", AliNormalizationCounter::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,3,mgr->CreateContainer("tree_event_char", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,4,mgr->CreateContainer("tree_Xic0", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,5,mgr->CreateContainer("tree_Xic0_MCGen", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+
+    // in the end, this macro returns a pointer to your task. this will be convenient later on
+    // when you will run your analysis in an analysis train on grid
+    return task;
+}
