0a70aab4bdeec0dfd7599c00815d09dfe079e9f3
ilofnes <ingrid.mckibben.lofnes@cern.ch>
2020-02-17 14:08:05 +0000

    Updates for vdm stability check -now full working code

--- PWGMM/Lumi/vdM/VdmStability/AddTask_ilofnes_Vdm.C
+++ PWGMM/Lumi/vdM/VdmStability/AddTask_ilofnes_Vdm.C
@@ -1,21 +1,33 @@
-AliAnalysisTaskV0T0stability* AddTask_ilofnes_Vdm(TString name = "name") {
-    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+AliAnalysisTaskVdmStability* AddTask_ilofnes_Vdm(TString name = "name") {
     
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  
     // resolve the name of the output file
     TString fileName = AliAnalysisManager::GetCommonFileName();
     fileName += ":MyTask";      // create a subfolder in the file
     
     // now we create an instance of your task
     AliAnalysisTaskVdmStability* task = new AliAnalysisTaskVdmStability(name.Data());
-    
+
     // add your task to the manager
     mgr->AddTask(task);
     
+    
+    //Add event filter
+    //define evnt cuts ...
+    //task->SetEventFilter(eventCuts);
+    
+    //Pileup rejection
+    //task->SetRejectPileup(kFALSE);
+
     // connect the manager to your task
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
+
     // same for the output
-    mgr->ConnectOutput(task,1,mgr->CreateContainer("MyOutputContainer", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,1,mgr->CreateContainer("EventStatHist", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,2,mgr->CreateContainer("EventTree", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
     
     // important: return a pointer to your task
     return task;
+    
 }
--- PWGMM/Lumi/vdM/VdmStability/AliAnalysisTaskVdmStability.h
+++ PWGMM/Lumi/vdM/VdmStability/AliAnalysisTaskVdmStability.h
@@ -1,33 +1,88 @@
-#ifndef AliAnalysisTaskVdmStability_H
-#define AliAnalysisTaskVdmStability_H
+/* Copyright(c) 1998-2020, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+// author: I.Lofnes, ingrid.mckibben.lofnes@cern.ch
+// Analysis task for determining the vdm-scan stability
+//
+
+
+#ifndef ALIANALYSISTASKVDMSTABILITY_H
+#define ALIANALYSISTASKVDMSTABILITY_H
+
+#include "TList.h"
 
 #include "AliAnalysisTaskSE.h"
-#include "AliAODEvent.h"
 
-class AliAnalysisTaskVdmStability : public AliAnalysisTaskSE
-{
+
+class AliESDEvent;
+class TH1D;
+class AliAnalysisCuts;
+class AliTriggerAnalysis;
+
+class AliAnalysisTaskVdmStability : public AliAnalysisTaskSE {
+    
 public:
     //Class constructors
     AliAnalysisTaskVdmStability();
     AliAnalysisTaskVdmStability(const char *taskname);
     //class destructor
     virtual         ~AliAnalysisTaskVdmStability();
-    //define output - called once at beginning of runtime
-    virtual void    UserCreateOutputObjects();
     //event loop - called for each event
     virtual void    UserExec(Option_t *);
+    //define output - called once at beginning of runtime
+    virtual void    UserCreateOutputObjects();
     //terminate - called at the end of analysis
     virtual void    Terminate(Option_t *);
-
+    
 private:
-    AliAODEvent*    fAOD;           //!<! Input AOD event
-    TList*          fOutputList;     //!<! output list
-    TH1F*           fHist;           //!<! dummy histogram
+    // bits toggled in the fEventTag data member
+    enum EventTagBits {kAllEvents=0,
+        kSelectedEvents,
+        kV0TimeEvents,
+        kPileupEvents,
+        kGoodZEvents,
+        kV0andPUEvents,
+        kV0andZEvents,
+        kV0andPUandZEvents,
+        kNbinsEvent};
+    
+    //AliESDEvent*    fESD;           //!<! Input ESD event
+    TList          fOutputList;         //! output list
+    
+    UInt_t fRunNumber;                  ///< Run number
+    UInt_t fEventNumber;                ///< Unique number for each event
+    Double_t fVtxZ;                      ///< Vertex Z
+    Bool_t  fIsGoodZ;                   ///< event with good z-vertex
+    Bool_t fSelectPhysics;              ///< Physics selected event
+    Bool_t fIsV0ANDfired;               ///< V0and triggered event
+    Bool_t fIsT0fired;                  ///< T0 triggered event
+    Bool_t fPileupEvent;                ///< pileup event
+    Float_t fTV0A;                      ///< V0 A time info
+    Float_t fTV0C;                      ///< V0 C time info
+    Bool_t fGoodTime;                   ///< event within V0 time window
+    ULong64_t fEventTag;                ///< Event tags
 
+    
+    //AliTriggerAnalysis *fTriggerAnalysis; //! trigger analysis class
+    
+    AliESDEvent* fEvent;                 //! ESD event
+    TH1D *fEventStatV0;                  //! Histogram with event statistics
+    TH1D *fEventStatT0;                  //! Histogram with event statistics
+    TTree* fEventTree;                   //! Event tree
 
-
-    /// \cond CLASSDEF
-    ClassDef(AliAnalysisTaskVdmStability,1);
-    /// \endcond};
+    //std::string fCurrentFile;
+    //Int_t fDirNum;
+    
+    void AddEventTreeVariables(TTree* &tree);
+    //void SetEventNumber();
+    void SetEventVariables();
+    
+    
+    AliAnalysisTaskVdmStability(const AliAnalysisTaskVdmStability &c);
+    AliAnalysisTaskVdmStability& operator= (const AliAnalysisTaskVdmStability &c);
+    
+    ClassDef(AliAnalysisTaskVdmStability,4);
+    
 };
 #endif
+
