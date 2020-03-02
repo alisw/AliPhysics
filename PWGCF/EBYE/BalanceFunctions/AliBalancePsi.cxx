92d6f32904338cd87563bd93649cd82f4e550fde
Zhanna Khabanova <zhannak@nikhef.nl>
2020-03-02 15:53:04 +0100

    Adding the centrality/multiplicity percentile dimension in invariant mass plot for kaon selection

--- PWGCF/EBYE/BalanceFunctions/AliBalancePsi.cxx
+++ PWGCF/EBYE/BalanceFunctions/AliBalancePsi.cxx
@@ -418,12 +418,12 @@ void AliBalancePsi::InitHistograms() {
   fHistConversionafter  = new TH3D("fHistConversionafter","after Conversion cut;#Delta#eta;#Delta#phi;M_{inv}^{2}",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
   fHistPsiMinusPhi      = new TH2D("fHistPsiMinusPhi","",4,-0.5,3.5,100,0,2.*TMath::Pi());
   fHistResonancesBefore = new TH3D("fHistResonancesBefore","before resonance cut;#Delta#eta;#Delta#phi;M_{inv}",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
-  fHistResonancesPhiBeforeUS = new TH2D("fHistResonancesPhiBeforeUS","before phi resonance cut;p_{T} (GeV/c);M_{inv}",200,0,10,500,0.8,1.2);
-  fHistResonancesPhiBeforeLS = new TH2D("fHistResonancesPhiBeforeLS","before phi resonance cut;p_{T} (GeV/c);M_{inv}",200,0,10,500,0.8,1.2);
+  fHistResonancesPhiBeforeUS = new TH3D("fHistResonancesPhiBeforeUS","before phi resonance cut;p_{T} (GeV/c);M_{inv};Mult or Centr percentile",200,0,10,500,0.8,1.2,220,-5,105);
+  fHistResonancesPhiBeforeLS = new TH3D("fHistResonancesPhiBeforeLS","before phi resonance cut;p_{T} (GeV/c);M_{inv};Mult or Centr percentile",200,0,10,500,0.8,1.2,220,-5,105);
   fHistResonancesRho    = new TH3D("fHistResonancesRho","after #rho resonance cut;#Delta#eta;#Delta#phi;M_{inv}",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
   fHistResonancesK0     = new TH3D("fHistResonancesK0","after #rho, K0 resonance cut;#Delta#eta;#Delta#phi;M_{inv}",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
   fHistResonancesLambda = new TH3D("fHistResonancesLambda","after #rho, K0, Lambda resonance cut;#Delta#eta;#Delta#phi;M_{inv}",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
-  fHistResonancesPhi    = new TH2D("fHistResonancesPhi","after phi resonance cut;p_{T} (GeV/c);M_{inv}",200,0,10.0,500,0.8,1.2);
+  fHistResonancesPhi    = new TH3D("fHistResonancesPhi","after phi resonance cut;p_{T} (GeV/c);M_{inv};Mult or Centr percentile",200,0,10.0,500,0.8,1.2,220,-5,105);
   fHistQbefore          = new TH3D("fHistQbefore","before momentum difference cut;#Delta#eta;#Delta#phi;|#Delta p_{T}| (GeV/c)",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
   fHistQafter           = new TH3D("fHistQafter","after momentum difference cut;#Delta#eta;#Delta#phi;|#Delta p_{T}| (GeV/c)",50,-2.0,2.0,50,-TMath::Pi()/2.,3.*TMath::Pi()/2.,300,0,1.5);
 
@@ -640,15 +640,15 @@ void AliBalancePsi::CalculateBalance(Double_t gReactionPlane,
 	vectorDaughter[1].SetPtEtaPhiM(secondPt[j],secondEta[j],secondPhi[j],pKaon.GetMass());
 	vectorMother = vectorDaughter[0] + vectorDaughter[1];
 	  if (charge1 * charge2 > 0)
-	    fHistResonancesPhiBeforeLS->Fill(vectorMother.Pt(),vectorMother.M());
+	    fHistResonancesPhiBeforeLS->Fill(vectorMother.Pt(),vectorMother.M(),trackVariablesSingle[0]);
 	  else if (charge1 * charge2 < 0) {
 	    //if(TMath::Abs(vectorMother.M() - pPhi.GetMass()) <= nSigmaRejection*gWidthForPhiPdg)
 	    //continue;          
-	    fHistResonancesPhiBeforeUS->Fill(vectorMother.Pt(),vectorMother.M());
+	    fHistResonancesPhiBeforeUS->Fill(vectorMother.Pt(),vectorMother.M(),trackVariablesSingle[0]);
 	    //if (((vectorMother.M() - pPhi.GetMass()) < fNSigmaRejectionMin*gWidthForPhiData) || ((vectorMother.M() - pPhi.GetMass()) >= fNSigmaRejectionMax*gWidthForPhiData))
 	    if (((vectorMother.M() - massForPhiData) < fNSigmaRejectionMin*gWidthForPhiData) || ((vectorMother.M() - massForPhiData) >= fNSigmaRejectionMax*gWidthForPhiData))
 	    continue;
-	    fHistResonancesPhi->Fill(vectorMother.Pt(),vectorMother.M());
+	    fHistResonancesPhi->Fill(vectorMother.Pt(),vectorMother.M(),trackVariablesSingle[0]);
 	  }
         }
       }
