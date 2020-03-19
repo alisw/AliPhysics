9d4511e0ee12dbcf3cf23077c007fcc8fdc73360
Dong Jo Kim <dong.jo.kim@cern.ch>
2020-03-19 14:00:13 +0000

    adding xsection for pthard prod

---
+++ PWGCF/Correlations/macros/jcorran/AddTaskJPtHardXection.C
@@ -0,0 +1,24 @@
+//_____________________________________________________________________
+ AliAnalysisTask *AddTaskJPtHardXection(TString taskName = "JPtHardXection"){
+
+     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+     //==== Check the analysis type using the event handlers connected to the analysis mgr	
+     if (!mgr->GetInputEventHandler() ){
+ 	    ::Error("AddTaskFFluc", "This task requires an input event handler" );
+ 	    return NULL;
+     }
+
+     //==== JCORRAN TASK
+     AliJPtHardXectionTask *FFtask = new AliJPtHardXectionTask(taskName.Data());
+     mgr->AddTask((AliAnalysisTask*) FFtask);
+
+     //==== Create containers for input/output
+     AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+     AliAnalysisDataContainer *FFhist = mgr->CreateContainer(Form("%scontainer",FFtask->GetName()),  TDirectory::Class(), AliAnalysisManager::kOutputContainer,  Form("%s:%s",  AliAnalysisManager::GetCommonFileName(), FFtask->GetName()));
+
+     //==== Connect input/output
+     mgr->ConnectInput(FFtask, 0, cinput);
+     mgr->ConnectOutput(FFtask, 1, FFhist);
+
+     return FFtask;
+ }
