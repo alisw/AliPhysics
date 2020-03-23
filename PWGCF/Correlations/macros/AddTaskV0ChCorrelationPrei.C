be741159fa2921c1c67f9cbbe57e14d51c536813
Mustafa Anaam <mustafa.naji.anaam>
2020-03-23 19:31:25 +0800

    V0-h Sys

--- PWGCF/Correlations/macros/AddTaskV0ChCorrelationPrei.C
+++ PWGCF/Correlations/macros/AddTaskV0ChCorrelationPrei.C
@@ -64,18 +64,18 @@ if (!mgr->GetInputEventHandler()) {
   task->SetK0sLifeTimeMax(20);
   task->SetLambdaLifeTimeMin(0);
   task->SetLambdaLifeTimeMax(25);
-  task->SetDCAV0DaughtersMax(0.8);
-  task->SetDCAPostoPrimVertexMink0s(0.1);
-  task->SetDCANegtoPrimVertexMink0s(0.1);
+  task->SetDCAV0DaughtersMax(0.7);
+  task->SetDCAPostoPrimVertexMink0s(0.12);
+  task->SetDCANegtoPrimVertexMink0s(0.12);
   task->SetDCAPostoPrimVertexMinLamb(0.1);
-  task->SetDCANegtoPrimVertexMinLamb(0.25);
-  task->SetDCAPostoPrimVertexMinALamb(0.25);
+  task->SetDCANegtoPrimVertexMinLamb(0.27);
+  task->SetDCAPostoPrimVertexMinALamb(0.27);
   task->SetDCANegtoPrimVertexMinALamb(0.1);
 
 
   task->SetCosPointingAngleMin(0.975);
-  task->SetLambdaCPA(0.995);
-  task->Setk0sCPA(0.98);
+  task->SetLambdaCPA(0.998);
+  task->Setk0sCPA(0.985);
   task->Set2DFiducialMin(5);
   task->SetV0DaughterTrackTPCCluster(70.);
   task->SetNCrossedRowsTPCfindable(0.8);
