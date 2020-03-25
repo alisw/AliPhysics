592d6e1e0cc35635890bef1d9b33fcfb76c0edce
Ejiro Umaka <enumaka@uh.edu>
2020-03-25 00:41:50 -0500

    Update AliAnalysisTaskNetLambdaTrad.cxx

--- PWGCF/EBYE/NetLambda/AliAnalysisTaskNetLambdaTrad.cxx
+++ PWGCF/EBYE/NetLambda/AliAnalysisTaskNetLambdaTrad.cxx
@@ -2,7 +2,7 @@
 // For: Net Lambda fluctuation analysis via traditional method
 // By: Ejiro Naomi Umaka Apr 2018
 // email: ejiro.naomi.umaka@cern.ch
-//Mar22
+//Mar24
 
 #include "AliAnalysisManager.h"
 #include "AliInputEventHandler.h"
@@ -49,6 +49,34 @@ fListHist(0x0),
 fHistEventCounter(0x0),
 fHistCentrality(0x0),
 
+hpVz(0x0),
+hpxy(0x0),
+hetaVO(0x0),
+hpetaVO(0x0),
+hnetaVO(0x0),
+hlmasseta(0x0),
+hamasseta(0x0),
+hprow(0x0),
+hnrow(0x0),
+hposp(0x0),
+hnegp(0x0),
+hpospi(0x0),
+hnegpi(0x0),
+
+lhDCAd(0x0),
+ahDCAd(0x0),
+lhV0rad(0x0),
+ahV0rad(0x0),
+lhCosp(0x0),
+ahCosp(0x0),
+
+lhV0tPV(0x0),
+ahV0tPV(0x0),
+lhntPV(0x0),
+ahntPV(0x0),
+lhptPV(0x0),
+ahptPV(0x0),
+
 f3fHistCentVsInvMassLambda1point0(0x0),
 f3fHistCentVsInvMassAntiLambda1point0(0x0),
 f3fHistCentVsInvMassLambda0point3(0x0),
@@ -57,6 +85,7 @@ f3fHistCentVsInvMassAntiLambda0point3(0x0),
 fCentrality(-1),
 fNptBins(23),
 fEvSel(AliVEvent::kINT7),
+fPtBinNplusNminusCh03(NULL),
 fPtBinNplusNminusCh(NULL)
 
 {
@@ -87,6 +116,12 @@ void AliAnalysisTaskNetLambdaTrad::UserCreateOutputObjects()
     fHistCentrality = new TH1D( "fHistCentrality", "fHistCentrality",100,0,100);
     fListHist->Add(fHistCentrality);
     
+    hpVz = new TH1D( "hpVz", "hpVz",100,-20,20);
+    fListHist->Add(hpVz);
+
+    hpxy = new TH2F("hpxy","hpxy ", 100,-1,1,100,-1,1);
+    fListHist->Add(hpxy);
+
     //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     Double_t CentBins[101] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
     Long_t CentbinNum = sizeof(CentBins)/sizeof(Double_t) - 1;
@@ -103,8 +138,68 @@ void AliAnalysisTaskNetLambdaTrad::UserCreateOutputObjects()
         1.1484,1.1492,1.15,1.1508,1.1516,1.1524,1.1532,1.154,1.1548,1.1556,1.1564,1.1572,1.158,1.1588,1.1596,1.1604};
     Long_t Massbinnumb = sizeof(MassBins)/sizeof(Double_t) - 1;
     
-    //V0 hists//
+    hetaVO = new TH2F("hetaVO","hetaVO", 100, -1, 1, fNptBins, LambdaPtBins);
+    fListHist->Add(hetaVO);
     
+    hpetaVO = new TH2F("hpetaVO","hpetaVO", 100, -1, 1, fNptBins, LambdaPtBins);
+    fListHist->Add(hpetaVO);
+    
+    hnetaVO = new TH2F("hnetaVO","hnetaVO", 100, -1, 1, fNptBins, LambdaPtBins);
+    fListHist->Add(hnetaVO);
+    
+    hlmasseta = new TH2F("hlmasseta","hlmasseta", Massbinnumb, MassBins, 100, -1, 1);
+    fListHist->Add(hlmasseta);
+    
+    hamasseta = new TH2F("hamasseta","hamasseta", Massbinnumb, MassBins, 100, -1, 1);
+    fListHist->Add(hamasseta);
+    
+    hprow = new TH2F("hprow","hprow", 100, 0,10, 1000, 20, 200);
+    fListHist->Add(hprow);
+    
+    hnrow = new TH2F("hnrow","hnrow", 100, 0,10, 1000, 20, 200);
+    fListHist->Add(hnrow);
+    
+    hposp = new TH2F("hposp","hposp", fNptBins, LambdaPtBins, 100, -10, 10);
+    fListHist->Add(hposp);
+    
+    hnegp = new TH2F("hnegp","hnegp", fNptBins, LambdaPtBins, 100, -10, 10);
+    fListHist->Add(hnegp);
+    
+    hpospi = new TH2F("hpospi","hpospi", fNptBins, LambdaPtBins, 100, -10, 10);
+    fListHist->Add(hpospi);
+    
+    hnegpi = new TH2F("hnegpi","hnegpi", fNptBins, LambdaPtBins, 100, -10, 10);
+    fListHist->Add(hnegpi);
+    
+    
+    lhDCAd = new TH2F("lhDCAd","lhDCAd", Massbinnumb, MassBins, 100, 0, 2);
+    fListHist->Add(lhDCAd);
+    ahDCAd = new TH2F("ahDCAd","ahDCAd", Massbinnumb, MassBins, 100, 0, 2);
+    fListHist->Add(ahDCAd);
+    lhV0rad = new TH2F("lhV0rad","lhV0rad", Massbinnumb, MassBins, 200, 0, 199);
+    fListHist->Add(lhV0rad);
+    ahV0rad = new TH2F("ahV0rad","ahV0rad", Massbinnumb, MassBins, 200, 0, 199);
+    fListHist->Add(ahV0rad);
+    lhCosp = new TH2F("lhCosp","lhCosp", Massbinnumb, MassBins, 100, 0, 1);
+    fListHist->Add(lhCosp);
+    ahCosp = new TH2F("ahCosp","ahCosp", Massbinnumb, MassBins, 100, 0, 1);
+    fListHist->Add(ahCosp);
+    lhV0tPV = new TH2F("lhV0tPV","lhV0tPV", Massbinnumb, MassBins, 200, 0, 199);
+    fListHist->Add(lhV0tPV);
+    ahV0tPV = new TH2F("ahV0tPV","ahV0tPV", Massbinnumb, MassBins, 200, 0, 199);
+    fListHist->Add(ahV0tPV);
+    lhntPV = new TH2F("lhntPV","lhntPV", Massbinnumb, MassBins, 100, 0, 99);
+    fListHist->Add(lhntPV);
+    ahntPV = new TH2F("ahntPV","ahntPV", Massbinnumb, MassBins, 100, 0, 99);
+    fListHist->Add(ahntPV);
+    lhptPV = new TH2F("lhptPV","lhptPV", Massbinnumb, MassBins, 100, 0, 99);
+    fListHist->Add(lhptPV);
+    ahptPV = new TH2F("ahptPV","ahptPV", Massbinnumb, MassBins, 100, 0, 99);
+    fListHist->Add(ahptPV);
+
+ 
+    
+    //V0 hists//
     
     f3fHistCentVsInvMassLambda1point0 = new TH3F("f3fHistCentVsInvMassLambda1point0","f3fHistCentVsInvMassLambda1point0",CentbinNum, CentBins, Massbinnumb,MassBins,fNptBins, LambdaPtBins);
     fListHist->Add(f3fHistCentVsInvMassLambda1point0);
@@ -189,14 +284,15 @@ void AliAnalysisTaskNetLambdaTrad::UserExec(Option_t *)
     vVtx[0] = vvertex->GetX();
     vVtx[1] = vvertex->GetY();
     vVtx[2] = vvertex->GetZ();
-    
+    hpxy->Fill(vVtx[0], vVtx[1]);
     if(vVtx[2] < -10. || vVtx[2] > 10.) return;
     
     fCentrality = MultSelection->GetMultiplicityPercentile("V0M");
     
     if( fCentrality < 0 || fCentrality > 80 ) return;
-    if (!fEventCuts.AcceptEvent(fInputEvent)) return;
-    
+    hpVz->Fill(vVtx[2]);
+    if (!fEventCuts.AcceptEvent(fInputEvent)) return; //pileup cut
+   
     fHistEventCounter->Fill(1.5);
     fHistCentrality->Fill(fCentrality);
     
@@ -331,26 +427,62 @@ void AliAnalysisTaskNetLambdaTrad::UserExec(Option_t *)
         negprnsg = fPIDResponse->NumberOfSigmasTPC(esdnTrack, AliPID::kProton);
         pospion  = fPIDResponse->NumberOfSigmasTPC( esdpTrack, AliPID::kPion );
         negpion  = fPIDResponse->NumberOfSigmasTPC( esdnTrack, AliPID::kPion );
+
+        hetaVO->Fill(eta,V0pt);
+        hpetaVO->Fill(peta,V0pt);
+        hnetaVO->Fill(neta,V0pt);
+
+        hprow->Fill(ppt, lPosTrackCrossedRows);
+        hnrow->Fill(npt, lPosTrackCrossedRows);
+        
+        hposp->Fill(V0pt, posprnsg);
+        hnegp->Fill(V0pt, negprnsg);
+        hpospi->Fill(V0pt, pospion);
+        hnegpi->Fill(V0pt, negpion);
+        
+        lhDCAd->Fill(invMassLambda, dcaDaughters);
+        lhV0rad->Fill(invMassLambda, v0Radius);
+        lhCosp->Fill(invMassLambda, cosPointingAngle);
+        lhV0tPV->Fill(invMassLambda, dcaV0ToVertex);
+        lhntPV->Fill(invMassLambda, dcaNegToVertex);
+        lhptPV->Fill(invMassLambda, dcaPosToVertex);
         
         
+        ahDCAd->Fill(invMassAntiLambda, dcaDaughters);
+        ahV0rad->Fill(invMassAntiLambda, v0Radius);
+        ahCosp->Fill(invMassAntiLambda, cosPointingAngle);
+        ahV0tPV->Fill(invMassAntiLambda, dcaV0ToVertex);
+        ahntPV->Fill(invMassAntiLambda, dcaNegToVertex);
+        ahptPV->Fill(invMassAntiLambda, dcaPosToVertex);
+        
+    
+     
         if(TMath::Abs(peta) > 0.8) continue;
         if(TMath::Abs(neta) > 0.8) continue;
         if(dcaDaughters > 0.8) continue;
         if(v0Radius < 5.0) continue;
         if(cosPointingAngle < 0.99) continue;
-      
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+
         Int_t iptbin = GetPtBin(V0pt);
         if( iptbin < 0 || iptbin > fNptBins-1 ) continue;
         
         
         if( ontheflystat == 0 )
         {
+            if(dcaV0ToVertex < 0.25 && dcaNegToVertex > 0.25 && dcaPosToVertex > 0.1 && TMath::Abs(posprnsg) <= 3 && TMath::Abs(negpion) <= 3)
+            {hlmasseta->Fill(invMassLambda,eta);}
+            
+            if(dcaV0ToVertex < 0.25 && dcaNegToVertex > 0.1 && dcaPosToVertex > 0.25 && TMath::Abs(negprnsg) <= 3 && TMath::Abs(pospion) <= 3)
+            {hamasseta->Fill(invMassAntiLambda,eta);}
+            
+
         if(TMath::Abs(eta) < 0.8)
         {
             if(dcaV0ToVertex < 0.05 && dcaNegToVertex > 0.25 && dcaPosToVertex > 0.1 && TMath::Abs(posprnsg) <= 3 && TMath::Abs(negpion) <= 3)
                 {
                 f3fHistCentVsInvMassLambda1point0->Fill(fCentrality,invMassLambda,V0pt);
+
                 if(invMassLambda > 1.11 && invMassLambda < 1.122)
                 {
                     ptChEta1point0[iptbin] += 1;
@@ -366,7 +498,6 @@ void AliAnalysisTaskNetLambdaTrad::UserExec(Option_t *)
                 }
 
         }
-            /////
             
             if(TMath::Abs(eta) < 0.3)
             {
