f3b4dbcf5c6bd482d5da7dc73881ece1213f56a3
Adrian Florin Mechler <adrian.florin.mechler@cern.ch>
2020-03-25 14:58:39 +0100

    going back to use reference in  IsRealPhoton() in header

--- PWGGA/GammaConv/AliAnalysisTaskSigmaPlToProtonPiZero.h
+++ PWGGA/GammaConv/AliAnalysisTaskSigmaPlToProtonPiZero.h
@@ -82,7 +82,7 @@ class AliAnalysisTaskSigmaPlToProtonPiZero : public AliAnalysisTaskSE
     Double_t GetQT(TLorentzVector sigmaVektor, TLorentzVector rekombinatedPi0);
 
     Bool_t IsRealProtonKink(AliESDkink* kink, AliStack* MCstack);
-    Bool_t IsRealPhoton(AliAODConversionPhoton PhotonCandidate, AliMCEvent* fMCEvent);
+    Bool_t IsRealPhoton(AliAODConversionPhoton *PhotonCandidate, AliMCEvent* fMCEvent);
 
     protected:
         AliVEvent *             fEvent;
