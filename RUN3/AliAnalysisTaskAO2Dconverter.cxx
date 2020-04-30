01742a4a2c454019ef3752a58af136dbf0f4b2ab
Valentina Zaccolo <valentina.zaccolo@cern.ch>
2020-04-30 15:10:16 +0200

    Adding tracklets counting to Run2 SPD tracklets to Run3 converter

--- RUN3/AliAnalysisTaskAO2Dconverter.cxx
+++ RUN3/AliAnalysisTaskAO2Dconverter.cxx
@@ -669,11 +669,11 @@ void AliAnalysisTaskAO2Dconverter::UserExec(Option_t *)
     if (fTreeStatus[kTracks]) ntrk_filled++;
   } // end loop on tracks
   vtx.fNentries[kTOF]    = ntofcls_filled;
-  vtx.fNentries[kTracks] = ntrk_filled;
 
   AliMultiplicity *mlt = fESD->GetMultiplicity();
   Int_t Ntracklets = mlt->GetNumberOfTracklets();
 
+  Int_t ntracklet_filled = 0;
   Float_t theta, phi, dphi, dphiS, dist, x, tgl, alpha;
 
   for (Int_t itr = Ntracklets; itr--;) {
@@ -739,8 +739,10 @@ void AliAnalysisTaskAO2Dconverter::UserExec(Option_t *)
       tracks.fLength = NAN;
 
       FillTree(kTracks);
+      if (fTreeStatus[kTracks]) ntracklet_filled++;
     }
   } // end loop on tracklets
+  vtx.fNentries[kTracks] = ntrk_filled + ntracklet_filled; 
   
   //---------------------------------------------------------------------------
   // Calorimeter data
