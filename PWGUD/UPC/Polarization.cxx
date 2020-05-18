fc7d6a6f16c6b8ba6589c628162cd3702984e17d
Amrit Gautam <agautam@cern.ch>
2020-05-18 02:09:33 -0500

    fixed the codes and added additional branches in Polarization.cxx

--- PWGUD/UPC/Polarization.cxx
+++ PWGUD/UPC/Polarization.cxx
@@ -81,7 +81,7 @@ Polarization::Polarization(const char* name) : AliAnalysisTaskSE(name),
     fPiSigma1(0), fTPCcluster1(0), fEta1(0),fTPCcluster2(0), fEta2(0),fHistEtaCut(0),fHistITS(0), fHistChargeCut(0),fHistPtCut(0),fHistPairCut(0),fHistPIDCut(0),
     fHistFilterbitCut(0),fHistTriggerCut(0),fHistCounter(0) ,fDCAxy1(0),fDCAz1(0),fDCAxy2(0),fDCAz2(0),fTriggerClass(0),fdEdX(0),fPd(0),fPp(0),fPtd(0),fZDCdata(0), fZNAenergy(0), fZNCenergy(0),fZDCAtime(0),fZDCCtime(0),fMuSigma0(0),fMuSigma1(0),fRabs1(0), fRabs2(0),fTheta(0),fHelicityTheta(0),fCollinTheta(0),fPhi(0),fHelicityPhi(0),fCollinPhi(0), daughter1(0.,0.,0.,0.), daughter2(0.,0.,0.,0.) , parent(0.,0.,0.,0.)
   {
-    cout<<"constructor"<<endl;
+    
     
     // constructor
     DefineInput(0, TChain::Class());    // define the input of the analysis: in this case we take a 'chain' of events
@@ -147,35 +147,37 @@ void Polarization::UserCreateOutputObjects()
     fTree->Branch("daughter1","TLorentzVector",&daughter1);
     fTree->Branch("daughter2","TLorentzVector",&daughter2);
     fTree->Branch("parent","TLorentzVector",&parent);
+    
+    
     fTree->Branch("fPt", &fPt, "fPt/F");
-    //fTree->Branch("fTheta", &fTheta, "fTheta/F");
-   // fTree->Branch("fHelicityTheta", &fHelicityTheta, "fHelicityTheta/F");
-  //  fTree->Branch("fCollinTheta", &fCollinTheta, "fCollinTheta/F");
+    fTree->Branch("fTheta", &fTheta, "fTheta/F");
+    fTree->Branch("fHelicityTheta", &fHelicityTheta, "fHelicityTheta/F");
+    fTree->Branch("fCollinTheta", &fCollinTheta, "fCollinTheta/F");
     
-  //  fTree->Branch("fPhi", &fPhi, "fPhi/F");
-  //  fTree->Branch("fHelicityPhi", &fHelicityPhi, "fHelicityPhi/F");
-  //  fTree->Branch("fCollinPhi", &fCollinPhi, "fCollinPhi/F");
+    fTree->Branch("fPhi", &fPhi, "fPhi/F");
+    fTree->Branch("fHelicityPhi", &fHelicityPhi, "fHelicityPhi/F");
+    fTree->Branch("fCollinPhi", &fCollinPhi, "fCollinPhi/F");
     
-  //  fTree->Branch("fM", &fM, "fM/F");
-  //  fTree->Branch("fPt0", &fPt0, "fPt0/F");
-  //  fTree->Branch("fPt1", &fPt1, "fPt1/F"); 
-   // fTree->Branch("fKaonSigma0", &fKaonSigma0, "fKaonSigma0/F");
-    //fTree->Branch("fKaonSigma1", &fKaonSigma1, "fKaonSigma1/F");
-  //   fTree->Branch("fPiSigma0", &fPiSigma0, "fPiSigma0/F");
-   // fTree->Branch("fPiSigma1", &fPiSigma1, "fPiSigma1/F");
+    fTree->Branch("fM", &fM, "fM/F");
+    fTree->Branch("fPt0", &fPt0, "fPt0/F");
+    fTree->Branch("fPt1", &fPt1, "fPt1/F"); 
+  //  fTree->Branch("fKaonSigma0", &fKaonSigma0, "fKaonSigma0/F");
+  //  fTree->Branch("fKaonSigma1", &fKaonSigma1, "fKaonSigma1/F");
+  //  fTree->Branch("fPiSigma0", &fPiSigma0, "fPiSigma0/F");
+  //  fTree->Branch("fPiSigma1", &fPiSigma1, "fPiSigma1/F");
   //  fTree->Branch("fMuSigma0", &fMuSigma0, "fMuSigma0/F");
   //  fTree->Branch("fMuSigma1", &fMuSigma1, "fMuSigma1/F");
-    //fTree->Branch("fTPCcluster1", &fTPCcluster1, "fTPCcluster1/F");
+  //  fTree->Branch("fTPCcluster1", &fTPCcluster1, "fTPCcluster1/F");
   //  fTree->Branch("fEta1", &fEta1, "fEta1/F");
   //  fTree->Branch("fTPCcluster2", &fTPCcluster2, "fTPCcluster2/F");
   //  fTree->Branch("fEta2", &fEta2, "fEta2/F");
     fTree->Branch("fDCAxy1", &fDCAxy1, "fDCAxy1/F");
-     fTree->Branch("fDCAxy2", &fDCAxy2, "fDCAxy2/F");
+    fTree->Branch("fDCAxy2", &fDCAxy2, "fDCAxy2/F");
     fTree->Branch("fDCAz1", &fDCAz1, "fDCAz1/F");
     fTree->Branch("fDCAz2", &fDCAz2, "fDCAz2/F");
     fTree->Branch("fTriggerClass", &fTriggerClass,"fTriggerClass/I");
   //  fTree->Branch("fPp", &fPp, "fPp/F");
- //   fTree->Branch("fPt0", &fPd, "fPd/F");
+  //   fTree->Branch("fPt0", &fPd, "fPd/F");
   //  fTree->Branch("fPtd", &fPtd, "fPtd/F");
    // fTree->Branch("fdEdX", &fdEdX, "fdEdX/F");
     fTree->Branch("fZNAenergy", &fZNAenergy, "fZNAenergy/F"); 
@@ -207,9 +209,10 @@ void Polarization::UserExec(Option_t *)
      fHistCounter->Fill(1);
        
      TString trigger = fAOD-> GetFiredTriggerClasses();
+    // cout<<trigger<<endl;
   //   if (trigger.Contains("CCUP8")) return; 
     // cout<< "trigger classes are " << trigger << endl; return;  
-     if (!trigger.Contains("CMUP")) return;
+     if (!trigger.Contains("CMUP")) cout<<"no cmup"<<endl; return;
      if (trigger.Contains("CMUP10")){
          fTriggerClass =10;
          }
@@ -224,7 +227,9 @@ void Polarization::UserExec(Option_t *)
          }
      if  (trigger.Contains("CMUP6")){
          fTriggerClass =6;
-         }    
+         }  
+           
+     cout<<trigger<<endl;     
     
       AliAODZDC *fZDCdata = fAOD->GetZDCData();
       fZNAenergy = fZDCdata->GetZNATowerEnergy()[0];
@@ -262,51 +267,63 @@ void Polarization::UserExec(Option_t *)
       
         if (track->Eta()>-2.5||track->Eta()<-4) continue; //fHistCounter->Fill(4);//continue;
         
+        
+        
         if (17.5>fRabs1||fRabs1>89.5) continue;//fHistCounter->Fill(5);//continue;
+        
+        
+        //debugger to check if the code is working
+        if(17.5>fRabs1) cout<<"rabs cut not working on track 2"<<endl;
+        if(89.5<fRabs1) cout<<"rabs cut not working on track 2"<<endl;
+        if(-2.5<track->Eta()) cout<<"eta cut not working"<<endl;
+        if(-4>track->Eta()) cout<<"eta cut not working on track 2"<<endl;  
           
           
-          
         
         
         
-        
-      //  if(track->GetTPCNcls() < 60)continue;
-       
-   //     fHistP_TPC-> Fill(track->P(),track->GetTPCsignal());
-   
-         // fdEdX =track->GetTPCsignal();
-        //  fPtd  = track->Pt();
-        //  fPd  =  TMath::Sqrt(track->Pt()*track->Pt()+track->Pz()*track->Pz());              
+                  
           for (Int_t j=i+1 ; j<iTracks ; j++) {
               AliAODTrack* track2 = static_cast<AliAODTrack*>(fAOD->GetTrack(j));
               fRabs2 = track2->GetRAtAbsorberEnd();
               
-             /// if(!(track2->TestFilterBit(1<<0))) continue;
+             
             //  fHistCounter->Fill(5);
               if(track2->HasPointOnITSLayer(0) || track2->HasPointOnITSLayer(1)) {GoodTracks2 = kTRUE;}
               else { GoodTracks2 = kFALSE ;}
               if (GoodTracks2 == kFALSE)   fHistCounter-> Fill(6);// continue;
-              if (track2->Pt()>1) continue;
-              //  
+              if (track2->Pt()>1) continue; 
               if (track2->Eta()>-2.5||track2->Eta()<-4) continue;
               if (17.5>fRabs2||fRabs2>89.5) continue;
+             
+             //some debuggers  to make sure the cut is working as I am little concerned they might not work
+              if(17.5>fRabs2) cout<<"rabs cut not working on track 2"<<endl;
+              if(89.5<fRabs2) cout<<"rabs cut not working on track 2"<<endl;
+              if(-2.5<track2->Eta()) cout<<"eta cut not working"<<endl;
+              if(-4>track2->Eta()) cout<<"eta cut not working on track 2"<<endl;
               
            
               fHistCounter->Fill(8);
               PairCounter = PairCounter+1;  
               savetrack1 = track;
               savetrack2 = track2;
-              if (PairCounter>1) break;     
+             
+             // i am braking it to make so that code do not run if pair is more than 1..
+             // it makes code run faster 
+              if (PairCounter<1) break;     
               
-           
+            
             
             
               }
        //end of for loop j tracks*/
-       //fTree ->Fill();
+       
       }//end of for loop i tracks
       //Selecting tracks with only 1 pair of tracks
+     
+     
       if (PairCounter!=1)  return;
+      
       fHistCounter->Fill(9);
      
      
@@ -329,7 +346,7 @@ void Polarization::UserExec(Option_t *)
       fDCAxy2= dca[0];      
       fDCAz2= dca[1];
       delete track2_clone;
-       if(TMath::Abs(dca[1]) > 2) continue;
+      if(TMath::Abs(dca[1]) > 2) return;
       //Charge Cut 
      
       if (savetrack1->Charge() * savetrack2->Charge()>= 0) return;
@@ -344,8 +361,6 @@ void Polarization::UserExec(Option_t *)
       
   
   
-    //if (fKaonSigma0*fKaonSigma0+fKaonSigma1*fKaonSigma1>16) return;
-   //   if ((fPiSigma0*fPiSigma0+fPiSigma1*fPiSigma1)<25) return;
       fHistCounter->Fill(11);
       TLorentzVector d1;
       TLorentzVector d2; 
@@ -364,11 +379,29 @@ void Polarization::UserExec(Option_t *)
       
       TLorentzVector p = d1+d2;
       
+     
       p = parent;
       fPt = p.Pt();
       fM =  p.M(); 
       if (p.Rapidity()>-2.5||p.Rapidity()<-4.0) return;//fHistCounter->Fill(15);  
-      if (fPt>0.25) fHistCounter->Fill(16) return;      
+      if (p.Rapidity()> -2.5) cout<<"this cut is not working"<<endl;
+      if (p.Rapidity()< -4) cout<<"this cut is not working"<<endl;
+      if (fPt>0.25)  return;      
+      
+      
+      fPhi =  p.Phi();
+     
+      fTheta = p.Theta();
+      
+      fHelicityTheta= CosThetaHelicityFrame(d1,d2,p);
+      fCollinTheta= CosThetaCollinsSoper(d1,d2,p);
+      
+      fHelicityPhi= CosPhiHelicityFrame(d1,d2,p);
+      fCollinPhi=  CosPhiCollinsSoper(d1,d2,p);
+      
+      
+      
+      
       
       
       
@@ -377,12 +410,224 @@ void Polarization::UserExec(Option_t *)
      // fEta1 = savetrack1->Eta();
       fTPCcluster2 = savetrack2->GetTPCNcls();
     //  fEta2 = savetrack2->Eta();
+      
+      
       fTree ->Fill();                                // continue until all the tracks are processed
       PostData(1, fOutputList);                           // stream the results the analysis of this event to
       PostData (2,fTree);                            // the output manager which will take care of writing
                                                           // it to a file
   
       }
+      
+      
+
+
+
+Double_t Polarization::CosPhiHelicityFrame(  TLorentzVector muonPositive,
+                                                          TLorentzVector muonNegative,
+                                                          TLorentzVector possibleJPsi )
+{
+  /* - This function computes the helicity phi for the
+     - helicity of the J/Psi.
+     - The idea should be to get back to a reference frame where it
+     - is easier to compute and to define the proper z-axis.
+     -
+   */
+
+  /* - Half of the energy per pair of the colliding nucleons.
+     -
+  */
+  Double_t HalfSqrtSnn   = 2510.;
+  Double_t MassOfLead208 = 193.6823;
+  Double_t MomentumBeam  = TMath::Sqrt( HalfSqrtSnn*HalfSqrtSnn*208*208 - MassOfLead208*MassOfLead208 );
+  /* - Fill the Lorentz vector for projectile and target.
+     - For the moment we do not consider the crossing angle.
+     - Projectile runs towards the MUON arm.
+     -
+   */
+  TLorentzVector pProjCM(0.,0., -MomentumBeam, HalfSqrtSnn*208); // projectile
+  TLorentzVector pTargCM(0.,0.,  MomentumBeam, HalfSqrtSnn*208); // target
+  /* - Translate the dimuon parameters in the dimuon rest frame
+     -
+   */
+  TVector3       beta      = ( -1./possibleJPsi.E() ) * possibleJPsi.Vect();
+  TLorentzVector pMu1Dimu  = muonPositive;
+  TLorentzVector pMu2Dimu  = muonNegative;
+  TLorentzVector pProjDimu = pProjCM;
+  TLorentzVector pTargDimu = pTargCM;
+  pMu1Dimu.Boost(beta);
+  pMu2Dimu.Boost(beta);
+  pProjDimu.Boost(beta);
+  pTargDimu.Boost(beta);
+  //
+  // --- Determine the z axis for the calculation of the polarization angle
+  // (i.e. the direction of the dimuon in the CM system)
+  //
+  TVector3 zaxis = (possibleJPsi.Vect()).Unit();
+  TVector3 yaxis = ((pProjDimu.Vect()).Cross(pTargDimu.Vect())).Unit();
+  TVector3 xaxis = (yaxis.Cross(zaxis)).Unit();
+  //
+  // --- Calculation of the azimuthal angle (Helicity)
+  //
+  Double_t phi = TMath::ATan2((pMu1Dimu.Vect()).Dot(yaxis),(pMu1Dimu.Vect()).Dot(xaxis));
+  return   phi;
+}      
+      
+
+
+Double_t Polarization::CosPhiCollinsSoper( TLorentzVector muonPositive,
+                                                        TLorentzVector muonNegative,
+                                                        TLorentzVector possibleJPsi )
+{
+  /* - This function computes the Collins-Soper PHI for the
+     - helicity of the J/Psi.
+     - The idea should be to get back to a reference frame where it
+     - is easier to compute and to define the proper z-axis.
+     -
+   */
+
+  /* - Half of the energy per pair of the colliding nucleons.
+     -
+   */
+  Double_t HalfSqrtSnn   = 2510.;
+  Double_t MassOfLead208 = 193.6823;
+  Double_t MomentumBeam  = TMath::Sqrt( HalfSqrtSnn*HalfSqrtSnn*208*208 - MassOfLead208*MassOfLead208 );
+  /* - Fill the Lorentz vector for projectile and target.
+     - For the moment we do not consider the crossing angle.
+     - Projectile runs towards the MUON arm.
+     -
+   */
+  TLorentzVector pProjCM(0.,0., -MomentumBeam, HalfSqrtSnn*208); // projectile
+  TLorentzVector pTargCM(0.,0.,  MomentumBeam, HalfSqrtSnn*208); // target
+  /* - Translate the dimuon parameters in the dimuon rest frame
+     -
+   */
+  TVector3       beta      = ( -1./possibleJPsi.E() ) * possibleJPsi.Vect();
+  TLorentzVector pMu1Dimu  = muonPositive;
+  TLorentzVector pMu2Dimu  = muonNegative;
+  TLorentzVector pProjDimu = pProjCM;
+  TLorentzVector pTargDimu = pTargCM;
+  pMu1Dimu.Boost(beta);
+  pMu2Dimu.Boost(beta);
+  pProjDimu.Boost(beta);
+  pTargDimu.Boost(beta);
+  /* - Determine the z axis for the CS angle.
+     -
+   */
+  TVector3 zaxisCS=(((pProjDimu.Vect()).Unit())-((pTargDimu.Vect()).Unit())).Unit();
+  //
+  // --- Determine the CS angle (angle between mu+ and the z axis defined above)
+  //
+  TVector3 yaxisCS=(((pProjDimu.Vect()).Unit()).Cross((pTargDimu.Vect()).Unit())).Unit();
+  TVector3 xaxisCS=(yaxisCS.Cross(zaxisCS)).Unit();
+
+  Double_t phi = TMath::ATan2((pMu1Dimu.Vect()).Dot(yaxisCS),((pMu1Dimu.Vect()).Dot(xaxisCS)));
+  return   phi;
+}
+
+
+
+Double_t Polarization::CosThetaCollinsSoper( TLorentzVector muonPositive,
+                                                          TLorentzVector muonNegative,
+                                                          TLorentzVector possibleJPsi )
+{
+  /* - This function computes the Collins-Soper cos(theta) for the
+     - helicity of the J/Psi.
+     - The idea should be to get back to a reference frame where it
+     - is easier to compute and to define the proper z-axis.
+     -
+   */
+
+  /* - Half of the energy per pair of the colliding nucleons.
+     -
+   */
+  Double_t HalfSqrtSnn   = 2510.;
+  Double_t MassOfLead208 = 193.6823;
+  Double_t MomentumBeam  = TMath::Sqrt( HalfSqrtSnn*HalfSqrtSnn*208*208 - MassOfLead208*MassOfLead208 );
+  /* - Fill the Lorentz vector for projectile and target.
+     - For the moment we do not consider the crossing angle.
+     - Projectile runs towards the MUON arm.
+     -
+   */
+  TLorentzVector pProjCM(0.,0., -MomentumBeam, HalfSqrtSnn*208); // projectile
+  TLorentzVector pTargCM(0.,0.,  MomentumBeam, HalfSqrtSnn*208); // target
+  /* - Translate the dimuon parameters in the dimuon rest frame
+     -
+   */
+  TVector3       beta      = ( -1./possibleJPsi.E() ) * possibleJPsi.Vect();
+  TLorentzVector pMu1Dimu  = muonPositive;
+  TLorentzVector pMu2Dimu  = muonNegative;
+  TLorentzVector pProjDimu = pProjCM;
+  TLorentzVector pTargDimu = pTargCM;
+  pMu1Dimu.Boost(beta);
+  pMu2Dimu.Boost(beta);
+  pProjDimu.Boost(beta);
+  pTargDimu.Boost(beta);
+  /* - Determine the z axis for the CS angle.
+     -
+   */
+  TVector3 zaxisCS=(((pProjDimu.Vect()).Unit())-((pTargDimu.Vect()).Unit())).Unit();
+  /* - Determine the CS angle (angle between mu+ and the z axis defined above)
+     -
+   */
+  Double_t CosThetaCS = zaxisCS.Dot((pMu1Dimu.Vect()).Unit());
+  return   CosThetaCS;
+}
+
+
+Double_t Polarization::CosThetaHelicityFrame( TLorentzVector muonPositive,
+                                                           TLorentzVector muonNegative,
+                                                           TLorentzVector possibleJPsi )
+{
+  /* - This function computes the Helicity cos(theta) for the
+     - helicity of the J/Psi.
+     - The idea should be to get back to a reference frame where it
+     - is easier to compute and to define the proper z-axis.
+     -
+   */
+
+  /* - Half of the energy per pair of the colliding nucleons.
+     -
+   */
+  Double_t HalfSqrtSnn   = 2510.;
+  Double_t MassOfLead208 = 193.6823;
+  Double_t MomentumBeam  = TMath::Sqrt( HalfSqrtSnn*HalfSqrtSnn*208*208 - MassOfLead208*MassOfLead208 );
+  /* - Fill the Lorentz vector for projectile and target.
+     - For the moment we do not consider the crossing angle.
+     - Projectile runs towards the MUON arm.
+     -
+   */
+  TLorentzVector pProjCM(0.,0., -MomentumBeam, HalfSqrtSnn*208); // projectile
+  TLorentzVector pTargCM(0.,0.,  MomentumBeam, HalfSqrtSnn*208); // target
+  /* - Translate the dimuon parameters in the dimuon rest frame
+     -
+   */
+  TVector3       beta      = ( -1./possibleJPsi.E() ) * possibleJPsi.Vect();
+  TLorentzVector pMu1Dimu  = muonPositive;
+  TLorentzVector pMu2Dimu  = muonNegative;
+  TLorentzVector pProjDimu = pProjCM;
+  TLorentzVector pTargDimu = pTargCM;
+  pMu1Dimu.Boost(beta);
+  pMu2Dimu.Boost(beta);
+  pProjDimu.Boost(beta);
+  pTargDimu.Boost(beta);
+  //
+  // --- Determine the z axis for the calculation of the polarization angle
+  // (i.e. the direction of the dimuon in the CM system)
+  //
+  TVector3 zaxis = (possibleJPsi.Vect()).Unit();
+  /* - Determine the He angle (angle between mu+ and the z axis defined above)
+     -
+   */
+  Double_t CosThetaHE = zaxis.Dot((pMu1Dimu.Vect()).Unit());
+  return   CosThetaHE;
+
+}
+
+
+
+
+      
 //_____________________________________________________________________________
 void Polarization::Terminate(Option_t *)
   {
