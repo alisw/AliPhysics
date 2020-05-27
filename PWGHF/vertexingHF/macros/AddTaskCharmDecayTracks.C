f235eade8428dafd3067b554d10332d517798bfa
Francesco Prino <prino@to.infn.it>
2020-05-27 10:16:22 +0200

    Option to select method in the AddTask

--- PWGHF/vertexingHF/macros/AddTaskCharmDecayTracks.C
+++ PWGHF/vertexingHF/macros/AddTaskCharmDecayTracks.C
@@ -1,5 +1,5 @@
 AliAnalysisTaskCharmDecayTracks* AddTaskCharmDecayTracks(Int_t pdgCode=421,
-							 Int_t optMeth=0,
+							 TString optMeth="kine",
 							 Int_t filterMask = 16){
   
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
@@ -11,8 +11,10 @@ AliAnalysisTaskCharmDecayTracks* AddTaskCharmDecayTracks(Int_t pdgCode=421,
   dTask->SetReadMC(kTRUE);
   dTask->SetFilterMask(filterMask);
   dTask->SetSelectedHadron(pdgCode);
-  if(optMeth==0) dTask->SetUseCharmedHadronsFromKine();
-  else dTask->SetUseCandidatesFromDeltaAOD();
+  optMeth.ToLower();
+  if(optMeth.Contains("cand") || optMeth.Contains("delta")) dTask->SetUseCandidatesFromDeltaAOD();
+  else dTask->SetUseCharmedHadronsFromKine();
+
 
   // Create containers for input/output
   TString baseName="CharmDecayTracks";
