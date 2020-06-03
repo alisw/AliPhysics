1ec2f5c32cdf47b204c25bb263464563489d89d4
Patrick Huhn <phuhn@ikf.uni-frankfurt.de>
2020-06-03 16:22:42 +0200

    Charged-particles analysis framework: path bug fix

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/Framework/AliAnalysisTaskUEStudy.h
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/Framework/AliAnalysisTaskUEStudy.h
@@ -1,4 +1,4 @@
-PWGLF/SPECTRA/ChargedHadrons/dNdPt/Framework/AliAnalysisTaskUEStudy.hPWGLF/SPECTRA/ChargedHadrons/dNdPt/Framework/AlidNdPtTools.h/// \class AliAnalysisTaskUEStudy
+/// \class AliAnalysisTaskUEStudy
 /// \brief Task to study efficiency and contamination in MC
 ///
 /// Fills THnSparse to study pt vs. nch in underlying event
@@ -28,17 +28,17 @@ class AliAnalysisTaskUEStudy : public AliAnalysisTaskMKBase
                                 AliAnalysisTaskUEStudy();
                                 AliAnalysisTaskUEStudy(const char *name);
         virtual                 ~AliAnalysisTaskUEStudy();
-        
+
         virtual void            AddOutput();                     //called at the beginning
         virtual Bool_t          IsEventSelected();               //called for each event
-        virtual void            AnaEvent();                      //called once for every event                
+        virtual void            AnaEvent();                      //called once for every event
         virtual void            AnaTrack(Int_t flag = 0);        //called once for every track
         virtual void            AnaParticleMC(Int_t flag = 0);   //called once for every mc particle
-        
-        
+
+
         static AliAnalysisTaskUEStudy* AddTaskUEStudy(const char* name = "TaskUEStudy", const char* outfile = 0);
 
-    protected:    
+    protected:
         Double_t                fPtMax;                     //!<! largest Pt track in the event
         Double_t                fPtMaxPhi;                  //!<! Phi of the largest pt
         Double_t                fMCPtMax;                   //!<! largest Pt particle in the mc event
@@ -48,22 +48,22 @@ class AliAnalysisTaskUEStudy : public AliAnalysisTaskMKBase
         Int_t                   fNTracksAway;               //!<! ntracks in away region
         Int_t                   fMCNChTowards;              //!<! MC nCH in towards region
         Int_t                   fMCNChTransverse;           //!<! MC nCH in transvers region
-        Int_t                   fMCNChAway;                 //!<! MC nCH in away region                   
+        Int_t                   fMCNChAway;                 //!<! MC nCH in away region
         THnSparseF*             fHistUETracks;              //-> underlying event histogram in data
         THnSparseF*             fHistUE;                    //-> underlying event histogram in data
         THnSparseF*             fHistUETracksMC;            //-> underlying event histogram in mc
         THnSparseF*             fHistUEMC;                  //-> underlying event histogram in mc
         THnSparseF*             fHistUEPhiRes;              //-> underlying event resolution phi
-        
 
-        
+
+
     private:
         AliAnalysisTaskUEStudy(const AliAnalysisTaskUEStudy&); // not implemented
         AliAnalysisTaskUEStudy& operator=(const AliAnalysisTaskUEStudy&); // not implemented
-        
-    /// \cond CLASSIMP    
+
+    /// \cond CLASSIMP
         ClassDef(AliAnalysisTaskUEStudy, 2);
-    /// \endcond        
+    /// \endcond
 };
 
 #endif
