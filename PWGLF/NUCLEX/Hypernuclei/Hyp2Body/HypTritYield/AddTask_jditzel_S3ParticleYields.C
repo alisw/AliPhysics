431f5267dc0958e1ea47b290bd86d984dbe6b24a
Janik Ditzel <janik.ditzel@cern.ch>
2020-06-07 12:47:27 +0000

    Fixes for MC.

--- PWGLF/NUCLEX/Hypernuclei/Hyp2Body/HypTritYield/AddTask_jditzel_S3ParticleYields.C
+++ PWGLF/NUCLEX/Hypernuclei/Hyp2Body/HypTritYield/AddTask_jditzel_S3ParticleYields.C
@@ -11,23 +11,24 @@ AliAnalysisTask *AddTask_jditzel_S3ParticleYields() {
 
   TString fileName = AliAnalysisManager::GetCommonFileName();
 
-  AliAnalysisTaskS3ParticleYields* task = new AliAnalysisTaskS3ParticleYields("jditzelTaskS3ParticleYields");   
+  AliAnalysisTaskS3ParticleYields* task = new AliAnalysisTaskS3ParticleYields("jditzelTaskS3ParticleYields");
   if(!task) return 0x0;
 
-  //task->SelectPIDcheckOnly(kFALSE);
-  //task->SetTriggerMask(AliVEvent::kAny);
+  task->SelectPIDcheckOnly(kFALSE);
+  task->SetTriggerMask(AliVEvent::kINT7 | AliVEvent::kTRD | AliVEvent::kHighMultV0 | AliVEvent::kHighMultSPD);
+  task->SelectCollisionCandidates(AliVEvent::kINT7 | AliVEvent::kTRD | AliVEvent::kHighMultV0 | AliVEvent::kHighMultSPD);
 
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
 
-	AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("histogramsheLp", TList::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
-	AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("treep", TTree::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
+    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("histogramsheLp", TList::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
+    AliAnalysisDataContainer *coutput2 = mgr->CreateContainer("treep", TTree::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
     AliAnalysisDataContainer *coutput3 = mgr->CreateContainer("treehe", TTree::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
     AliAnalysisDataContainer *coutput4 = mgr->CreateContainer("treeL", TTree::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
-	AliAnalysisDataContainer *coutput5 = mgr->CreateContainer("treeGenheLp", TTree::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
-	mgr->ConnectOutput(task, 1, coutput1);
-	mgr->ConnectOutput(task, 2, coutput2);
-	mgr->ConnectOutput(task, 3, coutput3);
+    AliAnalysisDataContainer *coutput5 = mgr->CreateContainer("treeGenheLp", TTree::Class(),AliAnalysisManager::kOutputContainer,mgr->GetCommonFileName());
+    mgr->ConnectOutput(task, 1, coutput1);
+    mgr->ConnectOutput(task, 2, coutput2);
+    mgr->ConnectOutput(task, 3, coutput3);
     mgr->ConnectOutput(task, 4, coutput4);
     mgr->ConnectOutput(task, 5, coutput5);
-	return task;
+    return task;
 }
