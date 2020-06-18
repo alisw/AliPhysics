ec30964e353e2365dcdb7cc9ee9135ae33c40680
Katharina Garner <katharina.garner@cern.ch>
2020-06-18 10:47:57 +0000

    AliAnalysisTaskHFJetIPQA: expand definition of track probability

--- PWGJE/FlavourJetTasks/macros/AddTaskHFJetIPQA.C
+++ PWGJE/FlavourJetTasks/macros/AddTaskHFJetIPQA.C
@@ -30,13 +30,8 @@ AliAnalysisTaskHFJetIPQA* AddTaskHFJetIPQA(
                                            const char *nrhoMC               = "RhoMC",
                                            int nTCThresh                      =1,
                                            int iTagSetting              =0,
-                                           TString PathToWeights = 	"alien:///alice/cern.ch/user/k/kgarner/Weights_18_07_18.root",
+                                           int nTrackTypesProb =-1,
                                            TString PathToThresholds = "alien:///alice/cern.ch/user/k/kgarner/ThresholdHists_LHC16JJ_new.root",
-                                          // TString PathToRunwiseCorrectionParameters = "alien:///alice/cern.ch/user/l/lfeldkam/MeanSigmaImpParFactors.root",
-                                          // TString PathToJetProbabilityInput = "/home/katha/Uni/PhD/PhD/LinusCode/Anwendung/data/Binned_ResFct_XYSignificance_pp7TeV.root",
-                                           TString PathToFlukaFactor="alien:///alice/cern.ch/user/k/kgarner/FlukaFactors_18_07_18.root",
-                                           Bool_t GenerateMeanSigmaCorrectionTable=kTRUE,
-                                           Int_t nITSReq=6,
                                            Bool_t useCorrelationTree=kFALSE,
                                            const char* suffix = ""
                                            )
@@ -81,22 +76,6 @@ AliAnalysisTaskHFJetIPQA* AddTaskHFJetIPQA(
         return 0x0;
     }
 
-
-    TFile* fileMCoverDataWeights;
-    if(isMC){
-        if(PathToWeights.EqualTo("") ) {
-        } else {
-            fileMCoverDataWeights=TFile::Open(PathToWeights.Data());
-            if(!fileMCoverDataWeights ||(fileMCoverDataWeights&& !fileMCoverDataWeights->IsOpen())){
-                Printf("%s :: File with modified weights not found",taskname);
-                return 0x0;
-            }
-        }
-    }
-
-    
-    Printf("%s :: File %s successfully loaded, setting up correction factors.",taskname,PathToWeights.Data());
-
     TString name(taskname),combinedName;
     combinedName.Form("%s%s", name.Data(),suffix);
     AliAnalysisTaskHFJetIPQA* jetTask = new AliAnalysisTaskHFJetIPQA(combinedName);
@@ -104,52 +83,7 @@ AliAnalysisTaskHFJetIPQA* AddTaskHFJetIPQA(
     jetTask->SetJetRadius(jetradius);
     jetTask->setTaskName(taskname);
 
-    /*if(isMC && fileMCoverDataWeights){
-        TH1F * h[20] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
-        const char * nampart[20] = {"pi0","eta","etap","rho","phi","omega","k0s","lambda","pi","kaon","proton","D0","Dp","Dsp","Ds","lambdac","bplus","b0","lambdab","bsp"};
-        for(int i = 0;i<20;++i){
-            h[i]=(TH1F*)fileMCoverDataWeights->Get(Form("%s/%s",system,nampart[i]));
-        }
-        Printf("%s :: Reading MC correction factors completed.",taskname);
-        for (int i = 0 ; i<20;++i) if(h[i]==0) return 0x0;
-        jetTask->SetUseMonteCarloWeighingLinus(h[0],h[1],h[2],h[3],h[4],h[5],h[6],h[7],h[8],h[9],h[10],h[11],h[12],h[13],h[14],h[15],h[16],h[17],h[18],h[19]);
-        Printf("%s :: Weights written to analysis task.",taskname);
-        if(fileMCoverDataWeights) fileMCoverDataWeights->Close();
-    }*/
-
-
-
-    // Load and setup Fluka correction factors from file
-    //==============================================================================
-    TFile* fileFlukaCorrection;
-    if(isMC){
-        if( PathToFlukaFactor.EqualTo("") ) {
-        } else {
-            fileFlukaCorrection=TFile::Open(PathToFlukaFactor.Data());
-            if(!fileFlukaCorrection ||(fileFlukaCorrection&& !fileFlukaCorrection->IsOpen())){
-                printf("%s :: File with original weights not found",taskname);
-                return 0x0;
-            }
-        }
-    }
-
-
-    Printf("%s :: File %s successfully loaded, setting up background correction factors.",taskname,PathToFlukaFactor.Data());
-
-    /*if(isMC && fileFlukaCorrection){
-        TGraph *g[4] = {0x0,0x0,0x0,0x0};
-        const char * nampart[4] = {"fGraphOmega","fGraphXi","fK0Star","fPhi"};
-        for(int i = 0;i<4;++i){
-            g[i]=(TGraph*)fileFlukaCorrection->Get(Form("%s/%s",system,nampart[i]));
-        }
-        Printf("%s :: Reading  Fluka factors completed.",taskname);
-        for (int i = 0 ; i<4;++i) if(g[i]==0) return 0x0;
-        jetTask->SetFlukaFactor(g[0],g[1],g[2],g[3]);
-        Printf("%s :: Weights written to analysis task.",taskname);
-        if(fileFlukaCorrection) fileFlukaCorrection->Close();
-    }*/
-
-    jetTask->ReadThresholdHists(PathToThresholds, taskname, nTCThresh, iTagSetting);
+    jetTask->ReadThresholdHists(PathToThresholds, taskname, nTCThresh, iTagSetting, nTrackTypesProb);
 
 
     // Setup input containers
