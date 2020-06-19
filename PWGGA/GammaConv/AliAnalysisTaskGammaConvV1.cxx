633af9762c422e6bda03d7a371979261b1db3c02
Ana Marin <a.marin@gsi.de>
2020-06-19 15:36:21 +0200

    fixing a bug in the fAODMCTrackArray commit, Invmass histogranms were empty for data (obviously ;-()

--- PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.cxx
+++ PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.cxx
@@ -4058,10 +4058,12 @@ void AliAnalysisTaskGammaConvV1::ProcessMCParticles()
 
 //________________________________________________________________________
 void AliAnalysisTaskGammaConvV1::CalculatePi0Candidates(){
-  if(!fAODMCTrackArray) fAODMCTrackArray = dynamic_cast<TClonesArray*>(fInputEvent->FindListObject(AliAODMCParticle::StdBranchName()));
-  if (fAODMCTrackArray == NULL){
-    AliInfo("AODMCTrackArray could not be loaded");
-    return;
+  if (fIsMC>0){
+    if(!fAODMCTrackArray) fAODMCTrackArray = dynamic_cast<TClonesArray*>(fInputEvent->FindListObject(AliAODMCParticle::StdBranchName()));
+    if (fAODMCTrackArray == NULL){
+      AliInfo("AODMCTrackArray could not be loaded");
+      return;
+    }
   }
   // Conversion Gammas
   if(fGammaCandidates->GetEntries()>1){
