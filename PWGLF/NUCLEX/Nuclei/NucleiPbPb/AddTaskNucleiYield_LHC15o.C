de5c5a307aeb27c2c1ca4abab7dce7b42c46e81b
Bong-Hwi Lim <bonghwilim@gmail.com>
2020-07-02 00:25:00 +0200

    Update macro for the tree output

--- PWGLF/NUCLEX/Nuclei/NucleiPbPb/AddTaskNucleiYield_LHC15o.C
+++ PWGLF/NUCLEX/Nuclei/NucleiPbPb/AddTaskNucleiYield_LHC15o.C
@@ -74,8 +74,18 @@ AliAnalysisTaskNucleiYield* AddTaskNucleiYield_LHC15o(Bool_t isMC = kFALSE,
       TList::Class(),
       AliAnalysisManager::kOutputContainer,
       output.Data());
+  AliAnalysisDataContainer *deuCont1 = mgr->CreateContainer(Form("RTree%s",tskname.Data()),
+      TTree::Class(),
+      AliAnalysisManager::kOutputContainer,
+      output.Data());
+  AliAnalysisDataContainer *deuCont2 = mgr->CreateContainer(Form("STree%s",tskname.Data()),
+      TTree::Class(),
+      AliAnalysisManager::kOutputContainer,
+      output.Data());
   mgr->ConnectInput  (deu,  0, mgr->GetCommonInputContainer());
   mgr->ConnectOutput (deu,  1, deuCont);
+  mgr->ConnectOutput (deu,  2, deuCont1);
+  mgr->ConnectOutput (deu,  3, deuCont2);
   return deu;
 }
 
