4bf7e9d728e6a4ad10c928d62b476baa5c99e956
Markus Fasel <markus.fasel@cern.ch>
2020-07-19 12:33:22 +0200

    Fix name of the static add function in add macro

--- PWGJE/EMCALJetTasks/macros/AddTaskEmcalOnlinePatchesRef.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskEmcalOnlinePatchesRef.C
@@ -1,3 +1,3 @@
 PWGJE::EMCALJetTasks::AliAnalysisTaskEmcalOnlinePatchesRef *AddTaskEmcalOnlinePatchesRef(const char *name, const char *suffix){
-  return PWGJE::EMCALJetTasks::AliAnalysisTaskEmcalOnlinePatchesRef::AddTaskEmcalOnlinePatchesRef(name, suffix);
+  return PWGJE::EMCALJetTasks::AliAnalysisTaskEmcalOnlinePatchesRef::AddTaskOnlinePatchesRef(name, suffix);
 }
