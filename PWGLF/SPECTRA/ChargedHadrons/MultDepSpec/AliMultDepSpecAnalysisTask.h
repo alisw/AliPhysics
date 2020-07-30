db4130917c62695164940c8a6ae6f4ac845c59b7
Mario Kruger <mario.kruger@cern.ch>
2020-07-30 17:18:26 +0200

    adjust ue task

--- PWGLF/SPECTRA/ChargedHadrons/MultDepSpec/AliMultDepSpecAnalysisTask.h
+++ PWGLF/SPECTRA/ChargedHadrons/MultDepSpec/AliMultDepSpecAnalysisTask.h
@@ -44,9 +44,9 @@
 class AliMultDepSpecAnalysisTask : public AliAnalysisTaskSE
 {
 public:
-  
+
   // possible axis dimensions
-  enum Dimension : int
+  enum Dimension : unsigned int
   {
     pt_meas = 0,
     pt_true,
@@ -61,16 +61,17 @@ public:
     sigma_pt,
     delta_pt,
     phi,
+    END,
   };
-  
+
   AliMultDepSpecAnalysisTask();
   AliMultDepSpecAnalysisTask(const char *name);
   virtual ~AliMultDepSpecAnalysisTask();
-  
+
   virtual void   UserCreateOutputObjects();
   virtual void   UserExec(Option_t*);
   virtual void   Terminate(Option_t*);
-  
+
   // Setters
   void SetTriggerMask(unsigned int triggermask)  {fTriggerMask = triggermask;}
   void SetIsMC(bool isMC = true){fIsMC = isMC;}
@@ -78,31 +79,31 @@ public:
   void SetUseDataDrivenCorrections(bool useDDC = true){fMCUseDDC = useDDC;}
   void SetUseZDCCut(bool useZDC){fUseZDCCut = useZDC;}
   void SetIncludePeripheralEvents(bool includePeripheralEvents){fIncludePeripheralEvents = includePeripheralEvents;}
-  
-  void SetAxis(Dimension dim, const std::string name, const std::string title, const std::vector<double>& binEdges, int nBins = 0);
-  void SetCuts(Dimension dim, const std::pair<double, double>& cuts) {};
-  
+
+  void SetAxis(unsigned int dim, const std::string name, const std::string title, const std::vector<double>& binEdges, int nBins = 0);
+  void SetCuts(unsigned int dim, const std::pair<double, double>& cuts) {};
+
   // Acceptance cuts -> to be replaced soon
   void SetMinEta(double minEta)   {fMinEta = minEta;}
   void SetMaxEta(double maxEta)   {fMaxEta = maxEta;}
   void SetMinPt(double minPt)     {fMinPt = minPt;}
   void SetMaxPt(double maxPt)     {fMaxPt = maxPt;}
-  
+
   // Configure this object for a train run
   static AliMultDepSpecAnalysisTask* AddTaskMultDepSpec(const std::string& dataSet, int cutModeLow = 100, int cutModeHigh = 119, TString options = "", bool isMC = false);
   void SaveTrainMetadata();
 
   bool SetupTask(std::string dataSet, TString options);
-  bool InitTask(bool isMC, bool isAOD, std::string dataSet, TString options, int cutMode = 100);
+  virtual bool InitTask(bool isMC, bool isAOD, std::string dataSet, TString options, int cutMode = 100);
 
 protected:
 
   virtual void DefineDefaultAxes(int maxMult = 100); // called in AddTask
   virtual void BookHistograms();    // called in UserCreateOutputObjects
-    
+
   virtual bool InitEvent();
   virtual bool InitTrack(AliVTrack* track);
-  
+
   // ugly workaround because template members cannot be virtual
   virtual bool InitParticle(AliMCParticle* particle) {return InitParticleBase(particle);} // called for ESDs
   virtual bool InitParticle(AliAODMCParticle* particle) {return InitParticleBase(particle);} // called for AODs
@@ -112,21 +113,32 @@ protected:
 
   void LoopMeas(bool count = false);
   void LoopTrue(bool count = false);
-  
+
   virtual void FillEventHistos();
   virtual void FillMeasTrackHistos();
   virtual void FillMeasParticleHistos();
   virtual void FillTrueParticleHistos();
-  
+
+  // this must be defined in header so it can be generated for types that are only present in derived classes
   template<typename T>
-  void BookHistogram(Hist::Hist<T>& hist, const std::string& name, const std::vector<Dimension>& axisNames, bool isFillWeigths = false);
-  
+  void BookHistogram(Hist::Hist<T>& histContainer, const std::string& histName, const std::vector<unsigned int>& dimensions, bool isFillWeigths = false)
+  {
+    for(auto& dim : dimensions)
+    {
+      if(fAxes.find(dim) == fAxes.end())
+      {
+        AliFatal(Form("Not all axes for histogram %s were specified properly!", histName.data()));
+        return;
+      }
+      histContainer.AddAxis(fAxes[dim]);
+    }
+    fOutputList->Add(histContainer.GenerateHist(histName));
+  }
+
+
   bool AcceptTrackQuality(AliVTrack* track);
   double GetCentrality(AliVEvent* event);
-  
 
-  
-private:
 
   std::vector<double> GetMultBinEdges(int maxMult);
   std::vector<double> GetMultBinEdges(std::vector<int> multSteps, std::vector<int> multBinWidth);
@@ -144,9 +156,9 @@ private:
   AliEventCuts        fEventCuts;             //!<! Event cuts
   AliESDtrackCuts*    fTrackCuts;             //-> Track cuts
   TRandom3*           fRand;                  //!<! Random generator
-  
+
   std::string         fTrainMetadata;         ///<  metadata of the train run used to generate the output
-  
+
   bool              fIsESD;			              ///< Flag for ESD usage
   bool              fIsMC;                    ///< Flag for MC usage
   bool              fUseZDCCut;               ///< Flag for zdc cut usage
@@ -158,15 +170,15 @@ private:
   double              fMaxEta;        ///< Maximum eta cut
   double              fMinPt;			    ///< Minimum pT cut
   double              fMaxPt;			    ///< Maximum pT cut
-  
+
   // Output Histograms
-  std::map<Dimension, Hist::Axis> fAxes;           ///< Axis definitions used in the histograms
+  std::map<unsigned int, Hist::Axis> fAxes;           ///< Axis definitions used in the histograms
   Hist::Log<TH1D> fHistTrainInfo;                  //!<! Histogram to save train metadata string as bin lable; entries correspond to number of jobs
   Hist::Hist<TH1D> fHistEventSelection;            //!<! Histogram of event selection
   Hist::Hist<TH1D> fHistEvents;                    //!<! Histogram of measured event distribution
   Hist::Hist<THnSparseF> fHistTracks;              //!<! Histogram of measured tracks
   Hist::Hist<THnSparseF> fHistRelPtReso;           //!<! Histogram of relatvie pT resolution from covariance matrix
-  
+
   Hist::Hist<THnSparseF> fHistMCEventEfficiency;   //!<! Histogram of selelcted events vs Nch
   Hist::Hist<THnSparseF> fHistMCRelPtReso;         //!<! Histogram of relative pt resolution from mc
   Hist::Hist<THnSparseF> fHistMCMultCorrelMatrix;  //!<! Histogram of multilicity correlation
@@ -175,42 +187,42 @@ private:
   Hist::Hist<THnSparseF> fHistMCPrimTrue;          //!<! Histogram of generated primaries
   Hist::Hist<THnSparseF> fHistMCPrimMeas;          //!<! Histogram of measured primaries
   Hist::Hist<THnSparseF> fHistMCSecMeas;           //!<! Histogram of measured secondaries
-  
+
   // event related properties
   AliVEvent*          fEvent;			      //!<! Event object
   AliMCEvent*         fMCEvent;         //!<! MC event
   double              fMultMeas;        //!<! measured central barrel track multiplicity
   double              fMultTrue;        //!<! true multiplicity
-  
+
   bool                          fIsFirstEventInJob;          //!<!
   int                           fRunNumber;                  //!<! run number
   unsigned long                 fEventNumber;                //!<! event number
   unsigned int                  fTimeStamp;                  //!<! event time stamp
   double                        fCent;                       //!<! event centrality
   bool                          fIsAcceptedPeripheralEvent;  //!<! event with centrality > 90% that passes the selection criteria
-  
+
   // track related properties
   double                        fPt;                         //!<! track pt
   double                        fEta;                        //!<! track eta
   double                        fPhi;                        //!<! track phi
   double                        fSigmaPt;                    //!<! sigma(pt)/pt
-  
+
   double                        fMCPt;                       //!<! mc pt
   double                        fMCEta;                      //!<! mc eta
   double                        fMCPhi;                      //!<! mc phi
 
   int                           fMCLabel;                    //!<! mc label
-  
+
   bool                          fMCIsChargedPrimary;          //!<! is charged primary?
   bool                          fMCIsChargedSecDecay;         //!<! is charged secondary from decay?
   bool                          fMCIsChargedSecMat;           //!<! is charged secondary from material?
   bool                          fMCIsChargedSecondary;        //!<! is charged secondary?
-  
+
   double                        fMCParticleWeight;            //!<! scaling factor of particle to match data
   double                        fMCSecScaleWeight;            //!<! scaling factor of secondary to match data
   int                           fNRepetitions;                //!<! how often to repeat this particle to match data
   bool                          fUseRandomSeed;               ///<  use a random seed or a deterministic one (default)
-    
+
   /// \cond CLASSIMP
   ClassDef(AliMultDepSpecAnalysisTask, 1); // example of analysis
   /// \endcond
--- PWGLF/SPECTRA/ChargedHadrons/MultDepSpec/AliMultDepSpecAnalysisTaskUE.h
+++ PWGLF/SPECTRA/ChargedHadrons/MultDepSpec/AliMultDepSpecAnalysisTaskUE.h
@@ -9,16 +9,27 @@
 class AliMultDepSpecAnalysisTaskUE : public AliMultDepSpecAnalysisTask
 {
 public:
+
+  // possible axis dimensions
+  enum DimensionUE : unsigned int
+  {
+   pt_lead_meas = Dimension::END,
+   phi_lead_meas,
+   delta_phi_lead,
+   delta_pt_lead,
+  };
+
   AliMultDepSpecAnalysisTaskUE();
   AliMultDepSpecAnalysisTaskUE(const char *name);
   virtual ~AliMultDepSpecAnalysisTaskUE();
-  
-  //virtual void   UserCreateOutputObjects() {AliMultDepSpecAnalysisTask::UserCreateOutputObjects();}
-  //virtual void   UserExec(Option_t*);
-  //virtual void   Terminate(Option_t*);
 
-  AliMultDepSpecAnalysisTaskUE* AddTaskMultDepSpecUE(const std::string& dataSet, int cutModeLow, int cutModeHigh, TString options, bool isMC);
-  
+  // Additional Setters
+  void SetIsUE(bool isUE = true){fIsUE = isUE;}
+
+  static AliMultDepSpecAnalysisTaskUE* AddTaskMultDepSpecUE(const std::string& dataSet, int cutModeLow = 100, int cutModeHigh = 119, TString options = "", bool isMC = false, bool isUE = true);
+
+  virtual bool InitTask(bool isUE, bool isMC, bool isAOD, std::string dataSet, TString options, int cutMode =  100);
+
 protected:
 
   virtual void DefineDefaultAxes(int maxMult = 100); // called in AddTask
@@ -30,17 +41,42 @@ protected:
 
   virtual bool SelectTrack();
   virtual bool SelectParticle();
-    
+
   virtual void FillEventHistos();
   virtual void FillMeasTrackHistos();
   virtual void FillMeasParticleHistos();
   virtual void FillTrueParticleHistos();
-  
+
+  void FindLeadingTrack();
+
 private:
-  
+
   AliMultDepSpecAnalysisTaskUE(const AliMultDepSpecAnalysisTaskUE&); // not implemented
   AliMultDepSpecAnalysisTaskUE& operator=(const AliMultDepSpecAnalysisTaskUE&); // not implemented
-  
+
+  double  fPtLeadMIN; // !<! Minimum leading track pt
+  bool    fIsUE;  // flag for measuring UE or full phi range
+
+  // Additional UE Output histograms
+  Hist::Hist<TH1D> fHistLeadPt;              //!<! Histogram of measured leading track pT
+  Hist::Hist<TH1D> fHistLeadPhi;             //!<! Histogram of measured leading track phi
+
+  Hist::Hist<TH1D> fHistMCResoPtLead;          //!<! Histogram of Difference in Leading Track Pt and corresponding Particle Pt
+  Hist::Hist<TH1D> fHistMCResoPhiLead;         //!<! Histogram of Difference in Leading Track Phi and corresponding Particle Phi
+  Hist::Hist<TH1D> fHistDiffToMCPtLead;        //!<! Histogram of Difference in Leading Track Pt and Leading Particle Pt
+  Hist::Hist<TH1D> fHistDiffToMCPhiLead;       //!<! Histogram of Difference in Leading Track Phi and Leading Particle Phi
+
+  Hist::Hist<TH2D> fHistPlateau;         //!<! Histogram for deriving Plateau Plot
+
+  // Additional event related properties
+  double                        fPtLead;                     //!<! Leading track pT
+  double                        fPhiLead;                    //!<! Leading track phi
+
+  double                        fMCPtLead;                   //!<! Leading MC particle pT
+  double                        fMCPhiLead;                  //!<! Leading MC particle phi
+  double                        fMCPtOfLead;                 //!<! To leading MC track corresponding particle pT
+  double                        fMCPhiOfLead;                //!<! To leading MC track corresponding particle phi
+
   /// \cond CLASSIMP
   ClassDef(AliMultDepSpecAnalysisTaskUE, 1); // example of analysis
   /// \endcond
