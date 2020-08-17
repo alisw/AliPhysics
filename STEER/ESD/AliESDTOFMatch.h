2d49bdfb74736fe353beb5548082403197f5ff7c
miranov25 <marian.ivanov@cern.ch>
2020-08-17 06:05:04 +0200

    PWGPP-426 - adding TPC-TRD-TOF infromation for ML - TOF cleaning

--- STEER/ESD/AliESDTOFMatch.h
+++ STEER/ESD/AliESDTOFMatch.h
@@ -17,6 +17,14 @@
 class AliESDTOFMatch : public AliVTOFMatch
 {
  public:
+  enum {
+    kActive=0x1,
+    kDeadZone=0x2,
+    kGainOK=0x4,
+    kDriftOK=0x8,
+    kHVROCOK=0x10,
+    kHVDriftOK=0x20
+  };
   AliESDTOFMatch();
   AliESDTOFMatch(Int_t i,Double_t inttimes[AliPID::kSPECIESC],Double_t dx,Double_t dy,Double_t dz,Double_t l);
   AliESDTOFMatch(AliESDTOFMatch &source);
@@ -38,15 +46,32 @@ class AliESDTOFMatch : public AliVTOFMatch
   virtual Int_t GetTrackIndex()           const {return GetUniqueID();}
   virtual void  SetTrackIndex(Int_t id)         {SetUniqueID(id);}
   void Print(const Option_t *opt=0) const;
-  
+  //
+  Short_t GetX0Layer(Int_t layer){ return fX0Layer[layer];}
+  Short_t GetTRDstatus(Int_t layer){ return fTRDStatus[layer];}
+  Short_t GetTRDncls(Int_t layer){ return fTRDncls[layer];}
+  Double_t GetChi2TPCTOFS(){return fChi2TPCTOFS;}
+  Double_t GetChi2TPCTrDTOFS(){return fChi2TPCTRDTOFS;}
+  void SetX0Layer(Int_t layer,Float_t value){ fX0Layer[layer]=value;}
+  void SetTRDstatus(Int_t layer,Short_t value){ fTRDStatus[layer]=value;}
+  void SetTRDncls(Int_t layer,Short_t value){ fTRDncls[layer]=value;}
+  void SetChi2TPCTOFS(Float_t value){fChi2TPCTOFS=value;}
+  void SetChi2TPCTrDTOFS(Float_t value){fChi2TPCTRDTOFS=value;}
+
+  //
  protected:
   Double32_t fDx;                // DeltaX residual
   Double32_t fDy;                //! DeltaY residual
   Double32_t fDz;                // DeltaZ residual
   Double32_t fTrackLength;       // track Length
   Double32_t fIntegratedTimes[AliPID::kSPECIESC]; // int timex
+  Double32_t fX0Layer[6];             // [0,0,8]  X0 vector estimator
+  Short_t    fTRDStatus[6];      // TRD chamber status (active, dead zone, gain status, vdrift status, HV status)
+  Short_t    fTRDncls[6];        // [0,60] - number of TRD clusters in road
+  Double32_t fChi2TPCTOFS;       // [0,0,8] - TPC-TOF      sqrt(chi2)
+  Double32_t fChi2TPCTRDTOFS;    // [0,0,8] - TPC-TRD-TOF  sqrt(chi2)
   //
-  ClassDef(AliESDTOFMatch, 1) // TOF matchable hit
+  ClassDef(AliESDTOFMatch, 2) // TOF matchable hit
     //
 };
 #endif
