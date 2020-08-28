95619020163deecd5b0f0ef5d7ad8aba3a44ab76
Manuel Colocci <mcolocci@cern.ch>
2020-08-28 14:50:13 +0200

    Triton case added

--- PWGLF/NUCLEX/Utils/ChunkFilter/AliAnalysisTaskFilterHe3.h
+++ PWGLF/NUCLEX/Utils/ChunkFilter/AliAnalysisTaskFilterHe3.h
@@ -27,16 +27,19 @@ public:
   virtual void Terminate(Option_t *option);
 
   void SetUseMultTaskCentrality(Bool_t useMultTaskCentrality = kTRUE) { fUseMultTaskCentrality = useMultTaskCentrality; };
-
+  void SetParticleType(AliPID::EParticleType particleType = AliPID::kHe3) {ParticleType = particleType; };
+  
   // Setters for some cuts
-  void SetMinNsig3He(Double_t opt) { fMinNSigma3He = opt; }
-  void SetMaxNsig3He(Double_t opt) { fMaxNSigma3He = opt; }
+  void SetMinNsig(Double_t opt) { fMinNSigma = opt; }
+  void SetMaxNsig(Double_t opt) { fMaxNSigma = opt; }
   void SetNclsTPC(Double_t opt) { fMinNclsTPC = opt; }
   void SetPtotPosMin(Double_t opt) { fMinPtotPos = opt; }
   void SetPtotNegMin(Double_t opt) { fMinPtotNeg = opt; }
   void SetPtotPosMax(Double_t opt) { fMaxPtotPos = opt; }
   void SetPtotNegMax(Double_t opt) { fMaxPtotNeg = opt; }
   void SetFillSecifTOF(Bool_t opt) { fillSecifTOF = opt; }
+  void SetMinMass(Double_t opt) { fMinMass = opt; }
+  void SetMaxMass(Double_t opt) { fMaxMass = opt; }
 
 private:
   AliESDEvent *fESD;                     //! input event
@@ -47,30 +50,33 @@ private:
   AliPIDResponse *fPIDResponse;          //! pid response object
   AliMultSelection *fMultSel;            //! centrality and multiplicity selection
   Bool_t fUseMultTaskCentrality;         // flag: true if centrality should be taken from AliMultSelectionTask, false if traditional method should be used
+  AliPID::EParticleType ParticleType;    // to select He3 or triton
   //
   Int_t fEventIdFile; //! event id in file
   TString fFileName;  //! chunk file name
 
   // Cut params
-  Double_t fMinNSigma3He = -4.0;
-  Double_t fMaxNSigma3He = 10.0;
+  Double_t fMinNSigma = -4.0;
+  Double_t fMaxNSigma = 10.0;
   Double_t fMinNclsTPC = 50;
   Double_t fMinPtotPos = 1.5;
   Double_t fMinPtotNeg = 0.5;
   Double_t fMaxPtotPos = 20.0;
   Double_t fMaxPtotNeg = 20.0;
   Bool_t fillSecifTOF = kTRUE;
-
+  Double_t fMinMass = 1.0;            
+  Double_t fMaxMass = 2.3;            
+  
   //
   // histograms
   //
-  TH1F *fHistZv;                //! z-Vertex distribution
-  TH2F *fHistdEdxData;          //! PID histogram dEdx all particles
-  TH2F *fHistTof;               //! PID histogram TOF all particles
-  TH3F *fHistdEdxDeuteronParam; //! PID-QA histogram for deuteron Bethe-Bloch parameterisation
-  TH3F *fHistdEdxHe3Param;      //! PID-QA histogram for he3 Bethe-Bloch parameterisation
-  TH3F *fHistdEdxTritonParam;   //! PID-QA histogram for triton Bethe-Bloch parameterisation
-  TH1F *fHistCent;              //! centrality histogram
+  TH1F *fHistZv;                   //! z-Vertex distribution
+  TH2F *fHistdEdxData;             //! PID histogram dEdx all particles
+  TH2F *fHistTof;                  //! PID histogram TOF all particles
+  TH3F *fHistdEdxDeuteronParam[2]; //! PID-QA histogram for deuteron Bethe-Bloch parameterisation
+  TH3F *fHistdEdxHe3Param[2];      //! PID-QA histogram for he3 Bethe-Bloch parameterisation
+  TH3F *fHistdEdxTritonParam[2];   //! PID-QA histogram for triton Bethe-Bloch parameterisation
+  TH1F *fHistCent;                 //! centrality histogram
   //
   //
   // Bethe-Bloch parameterisations (hard-coded for the time being)
