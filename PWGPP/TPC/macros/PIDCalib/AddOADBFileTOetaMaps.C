e2a45382344df85a597c15c8ef1f9bac7747244c
wiechula <Jens.Wiechula@cern.ch>
2020-08-28 09:26:17 +0200

    Helper macros for spline file and eta map creation

---
+++ PWGPP/TPC/macros/PIDCalib/AddOADBFileTOetaMaps.C
@@ -0,0 +1,37 @@
+void AddObjects(AliOADBContainer& from, AliOADBContainer& to);
+
+void AddOADBFileTOetaMaps(const TString inputFile, const TString fileToUpdate = "$ALICE_PHYSICS_SRC/OADB/COMMON/PID/data/TPCetaMaps.root")
+{
+  TFile* fin = TFile::Open(inputFile);
+  TIter nextKey(fin->GetListOfKeys());
+
+  TFile* fupdate = TFile::Open(fileToUpdate, "update");
+  TList* keysUpdata = fupdate->GetListOfKeys();
+
+  for ( auto kin : *fin->GetListOfKeys() ) {
+    AliOADBContainer* contin = (AliOADBContainer*)fin->Get(kin->GetName());
+
+    if ( keysUpdata->FindObject(kin->GetName()) ) {
+      AliOADBContainer* cout = (AliOADBContainer*)fupdate->Get(kin->GetName());
+      AddObjects(*contin, *cout);
+      cout->Write(0, TObject::kOverwrite);
+    }
+    else {
+      contin->Write();
+    }
+  }
+
+  fupdate->Close();
+}
+
+void AddObjects(AliOADBContainer& from, AliOADBContainer& to)
+{
+  for (int i = 0; i < from.GetNumberOfEntries(); ++i) {
+    const int lower = from.LowerLimit(i);
+    const int upper = from.UpperLimit(i);
+    const char* pass = from.GetPassNameByIndex(i)->GetName();
+    TObject* o = from.GetObjectByIndex(i);
+
+    to.AppendObject(o, lower, upper, pass);
+  }
+}
