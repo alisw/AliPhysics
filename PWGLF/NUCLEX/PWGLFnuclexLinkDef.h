4eed80a1f439947d0a672ebf01b812746d9a3894
Alberto Caliva <alberto.caliva@cern.ch>
2020-11-15 11:19:00 +0100

    Nuclei vs. UE

--- PWGLF/NUCLEX/PWGLFnuclexLinkDef.h
+++ PWGLF/NUCLEX/PWGLFnuclexLinkDef.h
@@ -5,8 +5,6 @@
 #pragma link off all functions;
 
 /// Exotica
-/// * Dibaryons
-#pragma link C++ class AliAnalysisTaskDibaryons+;
 /// * Hdibaryon
 #pragma link C++ class AliAnalysisTaskHdibaryonLPpi+;
 /// * LambdaN
@@ -70,7 +68,7 @@
 #pragma link C++ class AliAnalysisTaskNucleiv2SP+;
 #pragma link C++ class AliAnalysisTaskNucleiv2pPb+;
 #pragma link C++ class AliAnalysisTaskAllPtcv2+;
-#pragma link C++ class AliAnalysishDEventCollection+;
+#pragma link C++ class AliAnalysishDEventCollection+;  
 #pragma link C++ class AliReconstructed2pcFirst+;
 #pragma link C++ class AliReconstructed2pcSecond+;
 #pragma link C++ class AliAnalysishDEvent+;
@@ -82,6 +80,10 @@
 /// * NucleiKine
 #pragma link C++ class AliAnalysisTaskNucleiKine+;
 #pragma link C++ class AliAnalysisTaskNucleiKineCor+;
+
+/// *Nuclei_UE
+#pragma link C++ class AliAnalysisTaskDeuteronsRT+;
+
 /// Hypernuclei
 /// * Hyp2body
 #pragma link C++ class AliAnalysisTaskHelium3Pi+;
@@ -94,22 +96,12 @@
 #pragma link C++ class AliAnalysisTaskS3ParticleYields+;
 #pragma link C++ class AliAnalysisTaskHe3EffTree+;
 #pragma link C++ class AliAnalysisTaskHypCrossCheck+;
+#pragma link C++ class AliAnalysisTaskHyperTriton2He3piML+;
 #pragma link C++ class AliAnalysisTaskHypV0s+;
 #pragma link C++ class AliAnalysisTaskReducedTreeHypertritonBindingEnergy+;
-
-/// * Hyp3body
-#pragma link C++ class AliAnalysisTaskFindableHypertriton3+;
-
-/// * KF2Body
-#pragma link C++ class AliAnalysisTaskHypertritonKFTree+;
-
-/// ROOT6 tasks
-#ifdef __CLING__
 #pragma link C++ class RHyperTritonHe3pi+;
-#pragma link C++ class RHyperTritonHe3piFull+;
 #pragma link C++ class SHyperTritonHe3pi+;
 #pragma link C++ class std::vector<RHyperTritonHe3pi>+;
-#pragma link C++ class std::vector<RHyperTritonHe3piFull>+;
 #pragma link C++ class std::vector<SHyperTritonHe3pi>+;
 #pragma link C++ class RCollision+;
 #pragma link C++ class RTracklet+;
@@ -118,8 +110,16 @@
 #pragma link C++ class SGenericTracklet+;
 #pragma link C++ class std::vector<SGenericV0>+;
 #pragma link C++ class std::vector<SGenericTracklet>+;
+
+/// * Hyp3body
+#pragma link C++ class AliAnalysisTaskFindableHypertriton3+;
+
+/// * KF2Body
+#pragma link C++ class AliAnalysisTaskHypertritonKFTree+;
+
+/// Hyp3body Selector
+#ifdef __CLING__
 #pragma link C++ class AliSelectorFindableHyperTriton3Body+;
-#pragma link C++ class AliAnalysisTaskHyperTriton2He3piML+;
 #pragma link C++ class RHyperTriton+;
 #pragma link C++ class RHyperTriton3KF+;
 #pragma link C++ class RHyperTriton3O2+;
