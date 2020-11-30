993014e20a48b7de7032314ec95432e4e2974174
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2020-11-30 12:49:44 +0100

    fix: changing counter type to double

--- PWGCF/Correlations/Base/AliUEHist.cxx
+++ PWGCF/Correlations/Base/AliUEHist.cxx
@@ -1044,7 +1044,7 @@ TH2* AliUEHist::GetSumOfRatios2(AliUEHist* mixed, AliUEHist::CFStep step, AliUEH
   TH2* eventMixedAll = 0;
   TH2* eventMixedAllStep6 = 0;
   
-  Long64_t totalEvents = 0;
+  Double_t totalEvents = 0; // a counter that takes into account non-unitary weights, thus it is a double
   Int_t nCorrelationFunctions = 0;
   
   GetHistsZVtxMult(step, region, ptLeadMin, ptLeadMax, &trackSameAll, &eventSameAll);
