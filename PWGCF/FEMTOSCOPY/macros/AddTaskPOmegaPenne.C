5ed79f58a85f9f7d40393e12e08ed8583590430d
magicep0t <holladiewaldfee2@gmail.com>
2020-12-15 16:16:34 +0100

    MC origin binning for three paircleaners

--- PWGCF/FEMTOSCOPY/macros/AddTaskPOmegaPenne.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskPOmegaPenne.C
@@ -42,8 +42,8 @@ AliAnalysisTaskPOmegaPenne *AddTaskPOmegaPenne(bool isMC = true,
     // Lambda Cuts
     //
     AliFemtoDreamv0Cuts *v0Cuts = AliFemtoDreamv0Cuts::LambdaCuts(isMC, true, false);
-    v0Cuts->SetCutInvMass(0.050);
-    v0Cuts->SetAxisInvMassPlots(600, 0.95, 1.25);
+    // v0Cuts->SetCutInvMass(0.050);
+    // v0Cuts->SetAxisInvMassPlots(600, 0.95, 1.25);
 
     AliFemtoDreamTrackCuts *Posv0Daug = AliFemtoDreamTrackCuts::DecayProtonCuts(isMC, true, false);
     AliFemtoDreamTrackCuts *Negv0Daug = AliFemtoDreamTrackCuts::DecayPionCuts(isMC, true, false);
@@ -56,8 +56,8 @@ AliAnalysisTaskPOmegaPenne *AddTaskPOmegaPenne(bool isMC = true,
 
 
     AliFemtoDreamv0Cuts *Antiv0Cuts = AliFemtoDreamv0Cuts::LambdaCuts(isMC, true, false);           // V0 cuts - keep Lambda
-    Antiv0Cuts->SetCutInvMass(0.050);
-    Antiv0Cuts->SetAxisInvMassPlots(600, 0.95, 1.25);
+    // Antiv0Cuts->SetCutInvMass(0.050);
+    // Antiv0Cuts->SetAxisInvMassPlots(600, 0.95, 1.25);
 
     AliFemtoDreamTrackCuts *PosAntiv0Daug = AliFemtoDreamTrackCuts::DecayPionCuts(isMC, true, false);
     PosAntiv0Daug->SetCutCharge(1);
