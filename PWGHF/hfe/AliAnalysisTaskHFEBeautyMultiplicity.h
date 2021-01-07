66e1be9f6e7ca516d7ce62c2e653ca21b3a454eb
Shunya Chiba <schiba@cern.ch>
2021-01-07 08:36:40 +0000

    Update b->e Mult in pPb

--- PWGHF/hfe/AliAnalysisTaskHFEBeautyMultiplicity.h
+++ PWGHF/hfe/AliAnalysisTaskHFEBeautyMultiplicity.h
@@ -171,7 +171,8 @@ class AliAnalysisTaskHFEBeautyMultiplicity : public AliAnalysisTaskSE
         Int_t               NpureMC;
         Int_t               Nch;
 	Bool_t		    iBevt;
-	TH1F*		    fNDB;
+	TH1F*		    fNoB;
+	TH1F*		    fNoD;
     
         TH1F*               fCheckEtaMC;
         TH2F*               fHistMCorg_Pi0;
@@ -194,7 +195,28 @@ class AliAnalysisTaskHFEBeautyMultiplicity : public AliAnalysisTaskSE
 
 	TH2F*		    fDCAxy_MC_ele;
 	TH2F*		    fDCAxy_MC_Phot;
-    
+    	
+	TH1F*		    fHistPt_B_TrkCut0;
+	TH1F*		    fHistPt_B_TrkCut1;
+	TH1F*		    fHistPt_B_TrkCut2;
+	TH1F*		    fHistPt_B_TrkCut3;
+	TH1F*		    fHistPt_B_TrkCut4;
+	TH1F*		    fHistPt_B_TrkCut5;
+	TH1F*		    fHistPt_B_TrkCut6;
+	TH1F*		    fHistPt_B_TrkCut7;
+	TH1F*		    fHistPt_B_TrkCut8;
+	TH1F*		    fHistPt_B_TrkCut9;
+
+	TH1F*		    fHistPt_D_TrkCut0;
+	TH1F*		    fHistPt_D_TrkCut1;
+	TH1F*		    fHistPt_D_TrkCut2;
+	TH1F*		    fHistPt_D_TrkCut3;
+	TH1F*		    fHistPt_D_TrkCut4;
+	TH1F*		    fHistPt_D_TrkCut5;
+	TH1F*		    fHistPt_D_TrkCut6;
+	TH1F*		    fHistPt_D_TrkCut7;
+	TH1F*		    fHistPt_D_TrkCut8;
+	TH1F*		    fHistPt_D_TrkCut9;
     
     
     
