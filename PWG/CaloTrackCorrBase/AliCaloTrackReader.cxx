6bb960c89c46448da8718c8c81e47a0ab80ababd
Gustavo Conesa Balbastre <Gustavo.Conesa.Balbastre@cern.ch>
2021-01-27 09:36:31 +0100

    finer time histo binning

--- PWG/CaloTrackCorrBase/AliCaloTrackReader.cxx
+++ PWG/CaloTrackCorrBase/AliCaloTrackReader.cxx
@@ -1211,7 +1211,7 @@ TList * AliCaloTrackReader::GetCreateControlHistograms()
     }
     
     fhEMCALClusterTimeE  = new TH2F 
-    ("hEMCALReaderTimeE","#it{time}_{cluster} vs #it{E}_{cluster} after cuts", 250,0,250,1201,-1201,1201);
+    ("hEMCALReaderTimeE","#it{time}_{cluster} vs #it{E}_{cluster} after cuts", 125,0,250,1001,-1001,2002);
     fhEMCALClusterTimeE->SetXTitle("#it{E}_{cluster} (GeV)");
     if ( fHistoPtDependent )
        fhEMCALClusterTimeE->SetXTitle("#it{p}_{T} (GeV/#it{c})");
