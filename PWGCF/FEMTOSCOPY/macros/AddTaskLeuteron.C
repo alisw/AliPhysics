da959f4e5be80f23a52054fb70b7b622db9b26e2
Michael Jung <m.jung@cern.ch>
2021-02-17 12:50:19 +0000

    open deuteron nSigma cut

--- PWGCF/FEMTOSCOPY/macros/AddTaskLeuteron.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskLeuteron.C
@@ -148,7 +148,7 @@ AliAnalysisTaskSE *AddTaskLeuteron(
   TrackCuts3->SetDCAVtxXY(0.1);
   TrackCuts3->SetCutSharedCls(true);
   TrackCuts3->SetCutTPCCrossedRows(true,70,0.83);
-  TrackCuts3->SetPID(AliPID::kDeuteron,thresholdTOF,3.0);
+  TrackCuts3->SetPID(AliPID::kDeuteron,thresholdTOF,60.0);
   TrackCuts3->SetRejLowPtPionsTOF(true);
   TrackCuts3->SetCutSmallestSig(true);
   TrackCuts3->SetMinimalBooking(false);
@@ -180,7 +180,7 @@ AliAnalysisTaskSE *AddTaskLeuteron(
   TrackCuts4->SetDCAVtxXY(0.1);
   TrackCuts4->SetCutSharedCls(true);
   TrackCuts4->SetCutTPCCrossedRows(true,70,0.83);
-  TrackCuts4->SetPID(AliPID::kDeuteron,thresholdTOF,3.0);
+  TrackCuts4->SetPID(AliPID::kDeuteron,thresholdTOF,60.0);
   TrackCuts4->SetRejLowPtPionsTOF(true);
   TrackCuts4->SetCutSmallestSig(true);
   TrackCuts4->SetMinimalBooking(false);
