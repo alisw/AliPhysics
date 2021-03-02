03bdd17e6ef2b545ab40d7641788651600dbd5ab
Shunya Chiba <schiba@cern.ch>
2021-03-02 03:17:21 +0000

    Update b->e in Multi in pPb 5TeV

--- PWGHF/hfe/macros/AddTaskHFEBeautyMultiplicity.C
+++ PWGHF/hfe/macros/AddTaskHFEBeautyMultiplicity.C
@@ -8,22 +8,13 @@ class AliAnalysisDataContainer;
 
 AliAnalysisTask* AddTaskHFEBeautyMultiplicity(
 	TString name = "name",
-	TString estimatorFilename = "alien:///alice/cern.ch/user/s/schiba/Mult_pPb16qt/estimatorArg.root",
-	Double_t nref = 30.1
+	Double_t nref = 30.1,
+	Double_t minNtrklet = 0,
+	DOuble_t maxNtrklet = 9999,
+	TString estimatorFilename = "alien:///alice/cern.ch/user/s/schiba/Mult_pPb16qt/estimatorArg.root"
 
 	)
 {
-
-
-	TFile* fEstimator = TFile::Open(estimatorFilename.Data());
-	if(!fEstimator){
-		AliFatal("File with multiplicity estimator not found\n");
-		return;
-	}
-
-	TProfile* multEstimatorAvgMB;
-	multEstimatorAvgMB = (TProfile*)(fEstimator->Get("mean_Trklet"))->Clone("multEstimatorAvgMB");
-
 
     // get the manager via the static access member. since it's static, you don't need
     // an instance of the class to call the function
@@ -31,30 +22,68 @@ AliAnalysisTask* AddTaskHFEBeautyMultiplicity(
     if (!mgr) {
         return 0x0;
     }
+
     // get the input event handler, again via a static method. 
     // this handler is part of the managing system and feeds events
     // to your task
     if (!mgr->GetInputEventHandler()) {
         return 0x0;
     }
+
+
+
     // by default, a file is open for writing. here, we get the filename
     TString fileName = AliAnalysisManager::GetCommonFileName();
     fileName += ":MyTask";      // create a subfolder in the file
-    // now we create an instance of your task
+    
 
+    //---- my task
     AliAnalysisTaskHFEBeautyMultiplicity* task = new AliAnalysisTaskHFEBeautyMultiplicity(name.Data());
-    task->SetMultiProfileLHC16qt(multEstimatorAvgMB);
     task->SetNref(nref);
+    task->SetNtrkletMin(minNtrklet);
+    task->SetNtrkletMax(maxNtrklet);
+
+
+
+
+    //---- Get Estimator 
+    TFile* fEstimator = TFile::Open(estimatorFilename.Data());
+    if(!fEstimator){
+		AliFatal("File with multiplicity estimator not found!\n");
+		return;
+    }
+
+    TProfile* multiEstimatorAvgMB[2];
+    multiEstimatorAvgMB[0] = (TProfile*)(fEstimator->Get("mean_Trklet"))->Clone("multiEstimatorAvgMB_Data");	// Get Estimator file (Data)
+    multiEstimatorAvgMB[1] = (TProfile*)(fEstimator->Get("mean_Trklet_MC"))->Clone("multiEstimatorAvgMB_MC");	// Get Estimator file (MC)
+
+    task->SetMultiProfileLHC16qt(multiEstimatorAvgMB[0]);
+    task->SetMultiProfileLHC16qt_MC(multiEstimatorAvgMB[1]);
+
+
+
+/*
+    //---- Get weight for N tracklet
+    TH1D* WeightNtrklet = (TH1D*)fEstimator->Get("weightNtrklet")->Clone("WeightNtrklet");
+    if(!WeightNtrklet){
+	    AliFatal("Multiplicity estimator for weight not found! Please check your estimator file.\n");
+	    return;
+    }
+    task->SetWeightNtrklet(WeightNtrklet);
+*/
+
 
 
     if(!task) return 0x0;
+
     // add your task to the manager
     mgr->AddTask(task);
     // your task needs input: here we connect the manager to your task
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
     // same for the output
-    mgr->ConnectOutput(task,1,mgr->CreateContainer("MyOutputContainer", TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task,1,mgr->CreateContainer(name, TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
     // in the end, this macro returns a pointer to your task. this will be convenient later on
     // when you will run your analysis in an analysis train on grid
+
     return task;
 }
