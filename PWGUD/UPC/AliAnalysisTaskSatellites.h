38d1170f0aa341e4e2a2ecd8e54875bdb4648ac6
Michal Broz <Michal.Broz@cern.ch>
2021-03-07 10:47:38 -0500

    Update of Satellite task

--- PWGUD/UPC/AliAnalysisTaskSatellites.h
+++ PWGUD/UPC/AliAnalysisTaskSatellites.h
@@ -35,8 +35,8 @@ class AliAnalysisTaskSatellites : public AliAnalysisTaskSE {
   TH1I *hTriggerClassesCounter;
   TH1I *hTriggerInputsCounter;
   TH1I *hSatellitesCounter;
-  Bool_t fTrgClassCINTZAC, fTrgInputV0M, fIsSatellite;
-  Bool_t fTrgClassC0V0M, fTrgInputVBA, fTrgInputVBC, fTrgInputZAC;
+  Bool_t fTrgClassCINTZAC, fTrgInputV0M, fTrgInputTVX, fTrgInputV0alt, fIsSatellite;
+  Bool_t fTrgClassC0V0M, fTrgClassC0TVX, fTrgClassCV0L, fTrgInputVBA, fTrgInputVBC, fTrgInputZAC;
   Int_t fRunNumber;
   UInt_t fL0inputs, fL1inputs, fTimeStamp;
   Float_t fZNATDCm[4], fZNCTDCm[4];
