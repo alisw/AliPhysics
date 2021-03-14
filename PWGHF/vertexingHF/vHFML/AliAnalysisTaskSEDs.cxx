e5a71eea44c9a9eeef5d9156829ef7adad8357ff
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2021-03-14 17:59:02 +0100

    Fix axis names

--- PWGHF/vertexingHF/vHFML/AliAnalysisTaskSEDs.cxx
+++ PWGHF/vertexingHF/vHFML/AliAnalysisTaskSEDs.cxx
@@ -1821,7 +1821,7 @@ void AliAnalysisTaskSEDs::CreateCutVarsAndEffSparses()
       xminReco = {xminReco[0], xminReco[1], xminReco[13], xminReco[14], xminReco[15]};
       xmaxReco = {xmaxReco[0], xmaxReco[1], xmaxReco[13], xmaxReco[14], xmaxReco[15]};
 
-      axisMC = {axisMC[0], axisMC[1], axisMC[2], axisMC[3], axisMC[14], axisMC[15], axisMC[16]};
+      axisMC = {axisMC[0], axisMC[1], axisMC[2], axisMC[3], axisMC[15], axisMC[16], axisMC[17]};
       nBinsRecoMC = {nBinsRecoMC[0], nBinsRecoMC[1], nBinsRecoMC[2], nBinsRecoMC[3], nBinsRecoMC[15], nBinsRecoMC[16], nBinsRecoMC[17]};
       xminRecoMC = {xminRecoMC[0], xminRecoMC[1], xminRecoMC[2], xminRecoMC[3], xminRecoMC[15], xminRecoMC[16], xminRecoMC[17]};
       xmaxRecoMC = {xmaxRecoMC[0], xmaxRecoMC[1], xmaxRecoMC[2], xmaxRecoMC[3], xmaxRecoMC[15], xmaxRecoMC[16], xmaxRecoMC[17]};
