4170e1f5eea24b7be1e9418f0cab827e7e167209
Andreas Mathis <andreas.mathis@cern.ch>
2021-04-01 10:47:50 +0200

    [p-d femto] Check with global tracks for protons

--- PWGCF/FEMTOSCOPY/macros/AddTaskCharmingFemto.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskCharmingFemto.C
@@ -50,12 +50,12 @@ AliAnalysisTaskSE *AddTaskCharmingFemto(
   // Track Cuts
   AliFemtoDreamTrackCuts *TrackCuts = AliFemtoDreamTrackCuts::PrimProtonCuts(
       isMC, true, false, false);
-  TrackCuts->SetFilterBit(128);
+  TrackCuts->SetFilterBit(96);
   TrackCuts->SetCutCharge(1);
 
   AliFemtoDreamTrackCuts *AntiTrackCuts =
       AliFemtoDreamTrackCuts::PrimProtonCuts(isMC, true, false, false);
-  AntiTrackCuts->SetFilterBit(128);
+  AntiTrackCuts->SetFilterBit(96);
   AntiTrackCuts->SetCutCharge(-1);
 
   TrackCuts->SetMinimalBooking(suffix != "0");
