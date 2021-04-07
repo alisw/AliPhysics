e67e77ed0bb7edcc391057ccbecc76c1a58e26fc
siyu.tang@cern.ch <siyu.tang@cern.ch>
2021-04-07 20:30:08 +0000

    Add task to analysis the MC jet v2

--- PWGCF/Correlations/DPhi/PWGCFCorrelationsDPhiLinkDef.h
+++ PWGCF/Correlations/DPhi/PWGCFCorrelationsDPhiLinkDef.h
@@ -90,6 +90,7 @@
 #pragma link C++ class AliAssociatedTrackYSLEGOMC+;
 #pragma link C++ class AliMixTrackYSLEGOMC+;
 #pragma link C++ class AliAssociatedVZEROYSLEGOMC+;
+#pragma link C++ class AliAnalysisTaskSEpPbCorrelationsJetV2Kine+;
 #pragma link C++ class AliAnalysisTaskSEpPbCorrelationsJetV2+;
 #pragma link C++ class AliTrigAssoPairST+;
 #pragma link C++ class AliAnalysisTaskSEPbPbCorrelationsJetV2+;
