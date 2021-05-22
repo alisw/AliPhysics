d23df92853c61ad769cc87783d4b0655accb68f6
Dmitri Peresunko <Dmitri.Peresunko@cern.ch>
2021-05-22 19:30:21 +0300

    Emin dependent peak parameterization added

--- PWGGA/PHOSTasks/PHOS_Tagging/AliAnalysisTaskTaggedPhotons.h
+++ PWGGA/PHOSTasks/PHOS_Tagging/AliAnalysisTaskTaggedPhotons.h
@@ -76,7 +76,7 @@ protected:
   Int_t   GetFiducialArea(const Float_t * pos)const ;                           //what kind of fiducial area hit the photon
   Int_t   IsSameParent(const AliCaloPhoton *p1, const AliCaloPhoton *p2,Int_t &iGrandPa) const; //Check MC genealogy; return PDG of parent
   Bool_t  IsGoodChannel(Int_t mod, Int_t ix, Int_t iz) ;
-  Double_t  InPi0Band(Double_t m, Double_t pt)const; //Check if invariant mass is within pi0 peak
+  void    InPi0Band(Double_t m, Double_t pt, Double_t *nsimga)const; //Check if invariant mass is within pi0 peak
   Bool_t  TestDisp(Double_t l0, Double_t l1, Double_t e)const  ;
   Bool_t  TestTOF(Double_t /*t*/,Double_t /*en*/)const{return kTRUE;} 
   Bool_t  TestCharged(Double_t dr,Double_t en)const ;
