8e8da71709d7733f9fc66fe8cc41843836e49d91
Vytautas Vislavicius <vytautas.vislavicius@cern.ch>
2021-06-01 16:28:27 +0200

    MeanPtV2: adding a setter to bypass trigger & event cut check for MC

--- PWGCF/FLOW/GF/AliAnalysisTaskMeanPtV2Corr.cxx
+++ PWGCF/FLOW/GF/AliAnalysisTaskMeanPtV2Corr.cxx
@@ -46,6 +46,7 @@ AliAnalysisTaskMeanPtV2Corr::AliAnalysisTaskMeanPtV2Corr():
   fCentEst(0),
   fExtendV0MAcceptance(kTRUE),
   fIsMC(kFALSE),
+  fBypassTriggerAndEvetCuts(kFALSE),
   fMCEvent(0),
   fUseRecoNchForMC(kTRUE),
   fRndm(0),
@@ -107,6 +108,7 @@ AliAnalysisTaskMeanPtV2Corr::AliAnalysisTaskMeanPtV2Corr(const char *name, Bool_
   fCentEst(0),
   fExtendV0MAcceptance(kTRUE),
   fIsMC(IsMC),
+  fBypassTriggerAndEvetCuts(kFALSE),
   fMCEvent(0),
   fUseRecoNchForMC(kTRUE),
   fNBootstrapProfiles(10),
@@ -663,7 +665,8 @@ void AliAnalysisTaskMeanPtV2Corr::UserExec(Option_t*) {
   }
   AliMultSelection *lMultSel = (AliMultSelection*)fInputEvent->FindListObject("MultSelection");
   Double_t l_Cent = lMultSel->GetMultiplicityPercentile(fCentEst->Data());
-  if(!CheckTrigger(l_Cent)) return;
+  if(!fBypassTriggerAndEvetCuts)
+    if(!CheckTrigger(l_Cent)) return;
   Double_t vtxXYZ[] = {0.,0.,0.};
   if(!AcceptAOD(fAOD, vtxXYZ)) return;
   Double_t vz = fAOD->GetPrimaryVertex()->GetZ();
@@ -699,7 +702,7 @@ Bool_t AliAnalysisTaskMeanPtV2Corr::CheckTrigger(Double_t lCent) {
   return kTRUE;
 };
 Bool_t AliAnalysisTaskMeanPtV2Corr::AcceptAOD(AliAODEvent *inEv, Double_t *lvtxXYZ) {
-  if(!fEventCuts.AcceptEvent(inEv)) return 0;
+  if(!fBypassTriggerAndEvetCuts) if(!fEventCuts.AcceptEvent(inEv)) return 0;
   const AliAODVertex* vtx = dynamic_cast<const AliAODVertex*>(inEv->GetPrimaryVertex());
   if(!vtx || vtx->GetNContributors() < 1)
     return kFALSE;
