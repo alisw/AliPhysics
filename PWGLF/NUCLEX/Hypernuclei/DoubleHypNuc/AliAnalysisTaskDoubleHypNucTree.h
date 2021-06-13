80542a9e99d78c831add7e38f6088b0b53646e9c
Janik Ditzel <janik.ditzel@cern.ch>
2021-06-13 15:38:50 +0200

    New analysis structure in order to reduce combinatorics.

--- PWGLF/NUCLEX/Hypernuclei/DoubleHypNuc/AliAnalysisTaskDoubleHypNucTree.h
+++ PWGLF/NUCLEX/Hypernuclei/DoubleHypNuc/AliAnalysisTaskDoubleHypNucTree.h
@@ -24,6 +24,7 @@ class AliESDtrack;
 #include "AliPID.h"
 #include "AliVertexerTracks.h"
 #include "AliMCParticle.h"
+#include "AliESDEvent.h"
 
 
 class AliAnalysisTaskDoubleHypNucTree : public AliAnalysisTaskSE {
@@ -66,7 +67,7 @@ enum PdgCodeType_t {
   static const Int_t fgkPdgCode[];
 
  private:
-  //--General--
+  //-- General variables --
   AliESDInputHandler    *fInputHandler;        
   AliESDpid             *fPID;                 
   AliESDEvent           *fESDevent;     
@@ -77,7 +78,9 @@ enum PdgCodeType_t {
   AliESDtrackCuts       *trackCutsStrong;
   AliESDtrackCuts       *trackCutsSoft;
   TTree                 *fTree;
+  TTree                 *gTree;
   TTree                 *fTreeGen;
+  TTree                 *gTreeGen;
   TList                 *fHistogramList; 
   TH2F                  *fHistdEdx;            
   TH1F                  *fHistNumEvents;       
@@ -89,17 +92,48 @@ enum PdgCodeType_t {
   Bool_t                fPIDCheckOnly;  
   Int_t                 fMCtrue; 
   UInt_t		fTriggerMask; 
-  Int_t                 fvariante;
-  //--Trigger--
+  Int_t                 fvariante; 
+  //-- Trigger --
   Int_t                 MB, HMV0, HMSPD, HNU, HQU;
-  //--Tracks-- 
+  //-- Tracks -- 
   AliESDtrack           *track1;
   AliESDtrack           *track2;
   AliESDtrack           *track3;
   AliESDtrack           *track4;
   Double_t              ptot1, sign1, ptot2, sign2, ptot3, sign3, ptot4, sign4;
   Bool_t                He3Pos1, He3Neg1, pPos2, pNeg2, piPos3, piNeg3, piPos4, piNeg4;
-  //--Vertex Reco--
+  //-- Full track arrays --
+  std::vector < int >   He3PosArray;
+  std::vector < int >   PPosArray;
+  std::vector < int >   PiPosArray;
+  std::vector < int >   PiPosSecArray;
+  std::vector < int >   He3NegArray;
+  std::vector < int >   PNegArray;
+  std::vector < int >   PiNegArray;
+  std::vector < int >   PiNegSecArray;
+  //-- Reduced track arrays --
+  std::vector < int >   He3PosArrayAcc;
+  std::vector < int >   PPosArrayAcc;
+  std::vector < int >   PiPosArrayAcc;
+  std::vector < int >   He3NegArrayAcc;
+  std::vector < int >   PNegArrayAcc;
+  std::vector < int >   PiNegArrayAcc;
+  //-- Array index counter --
+  Int_t                 He3PosCounter;
+  Int_t                 He3NegCounter;
+  Int_t                 PPosCounter;
+  Int_t                 PNegCounter;
+  Int_t                 PiPosCounter;
+  Int_t                 PiNegCounter;
+  Int_t                 PiPosSecCounter;
+  Int_t                 PiNegSecCounter;
+  Int_t                 He3PosCounterAcc;
+  Int_t                 He3NegCounterAcc;
+  Int_t                 PPosCounterAcc;
+  Int_t                 PNegCounterAcc;
+  Int_t                 PiPosCounterAcc;
+  Int_t                 PiNegCounterAcc;
+  //-- Vertex Reco --
   AliESDVertex          *primVertex;
   const AliESDVertex    *vertex;
   AliESDVertex          *secVertex;
@@ -122,7 +156,7 @@ enum PdgCodeType_t {
   Double_t              xyz[3];
   Double_t              dd[3];  
   Short_t               sign; 
-  //--MC--
+  //-- MC --
   Int_t                 noCombinatoricsMC;
   Int_t                 lessCombinatoricsMC;
   Int_t                 stackN;
@@ -152,7 +186,7 @@ enum PdgCodeType_t {
   AliMCParticle         *ParticleGrandMother2;
   AliMCParticle         *ParticleGrandMother3;
   AliMCParticle         *ParticleGrandMother4;
-  //--Particle--
+  //-- Particle --
   TLorentzVector        *sublorentzsum;
   TLorentzVector        *sublorentzsum2;
   TLorentzVector        *particle1;
@@ -164,7 +198,7 @@ enum PdgCodeType_t {
   TVector3              *h;
   Double_t              xthiss; 
   Double_t              xpp; 
-  //saved in Tree
+  //-- saved in Tree --
   Int_t		        fPeriod; 
   Int_t                 fMagneticField;
   Int_t                 fCentrality;
@@ -181,7 +215,7 @@ enum PdgCodeType_t {
   Float_t               fEDaughter3, fpDaughter3, fptDaughter3, fpxDaughter3, fpyDaughter3, fpzDaughter3, fyDaughter3, fdEdxDaughter3, fdEdxSigmaDaughter3, fDcaDaughter3, fDcaDaughter3o, fDcaSecDaughter3, fDcazDaughter3, fChi2Daughter3, fEtaDaughter3, fPhiDaughter3, fGeoLengthDaughter3, fTOFSignalDaughter3;
   Float_t               fEDaughter4, fpDaughter4, fptDaughter4, fpxDaughter4, fpyDaughter4, fpzDaughter4, fyDaughter4, fDcaDaughter4, fDcazDaughter4, fDcaSecDaughter4;
   Float_t               fSigmaYXDaughter, fSigmaXYZDaughter, fSigmaZDaughter, fSigmaYXDaughter1, fSigmaXYZDaughter1, fSigmaZDaughter1, fSigmaYXDaughter2, fSigmaXYZDaughter2, fSigmaZDaughter2, fSigmaYXDaughter3, fSigmaXYZDaughter3, fSigmaZDaughter3, fSigmaYXDaughter4, fSigmaXYZDaughter4, fSigmaZDaughter4, fPtUncertDaughter, fPtUncertDaughter1, fPtUncertDaughter2, fPtUncertDaughter3, fPtUncertDaughter4;
-  Int_t               fTPCRefitDaughter, fITSRefitDaughter, fTPCRefitDaughter1, fITSRefitDaughter1, fTPCRefitDaughter2, fITSRefitDaughter2, fTPCRefitDaughter3, fITSRefitDaughter3;
+  Int_t                 fTPCRefitDaughter, fITSRefitDaughter, fTPCRefitDaughter1, fITSRefitDaughter1, fTPCRefitDaughter2, fITSRefitDaughter2, fTPCRefitDaughter3, fITSRefitDaughter3;
   Float_t               fImParDaughter, fImPar2Daughter, fImParDaughter1, fImPar2Daughter1, fImParDaughter2, fImPar2Daughter2, fImParDaughter3, fImPar2Daughter3, fImParDaughter4, fImPar2Daughter4, fImParzDaughter, fImParz2Daughter, fImParzDaughter1, fImParz2Daughter1, fImParzDaughter2, fImParz2Daughter2, fImParzDaughter3, fImParz2Daughter3, fImParzDaughter4, fImParz2Daughter4;
   Float_t               fdEdxSigmaPion, fdEdxSigmaDeuteron, fdEdxSigmaTriton, fdEdxSigmaAlpha;
   Float_t               fDCA2B, fDCA2Bo, fDCA3B1, fDCA3B2, fDCA3B3, fPA, fSubPA, fSubPA2, fSubPA3, fSubPA4, fDecAngle, farmalpha, farmpt;
@@ -189,36 +223,49 @@ enum PdgCodeType_t {
   Float_t               fmMother, fmMother2, fEMother, fpxMother, fpyMother, fpzMother, fptMother, fpMother, fyMother, fctMother;
   Float_t               fmSubMother, fESubMother, fpxSubMother, fpySubMother, fpzSubMother, fptSubMother, fpSubMother, fySubMother, fctSubMother;
   Float_t               fthetaP, fthetaN;
-  
-
-  Bool_t TriggerSelection();
-
-  Float_t GetInvPtDevFromBC(Int_t b, Int_t c);
-  
-  Double_t Bethe(const AliESDtrack& track, Double_t mass, Int_t charge, Double_t* params);
-  
-  Double_t GeoLength(const AliESDtrack& track);
-  
-  Double_t GetTOFSignal(const AliESDtrack& track);
-  
+  //-- Functions --
+  //-- only PID check --
   void dEdxCheck();
-  
-  void AnalysisLoop();
+  //-- init track arrays --
+  void InitArrays();
+  //-- main function for track finding --
+  void FindTracks();
+  //-- pos 4LHe reco --
+  void CalcPosDaughterHypNuc();
+  //-- neg 4LHe reco --
+  void CalcNegDaughterHypNuc();
+  //-- pos 4LLH reco --
+  void CalcPosMotherHypNuc();
+  //-- neg 4LLH reco
+  void CalcNegMotherHypNuc();
+  //-- function for the cascade vertex reco --
   void CreateSecVertex();
-  void SetDaughterInformation();
-
+  //-- filling track information into tree --
+  void SetDaughterInformation(TString Mother);
+  //-- set Bethe_Bloch Params by runnumber --
   void SetBetheBlochParams(Int_t runNumber);
-
+  //-- Main MC function --
   void MCGenerated();                                      
-  
-  void MCFourBodyDecay (Int_t stackN, AliMCParticle *tparticleMother, Long_t PDGMother, Long_t PDGFirstDaughter, Long_t PDGSecondDaughter, Long_t PDGThirdDaughter, Long_t PDGFourthDaughter, Double_t massFirstDaughter, Double_t massSecondDaughter, Double_t massThirdDaughter, Double_t massFourthDaughter); 
-  
+  //-- gen 4LLH --
+  void MCFourBodyDecay (Int_t stackN, AliMCParticle *tparticleMother, Long_t PDGMother, Long_t PDGFirstDaughter, Long_t PDGSecondDaughter, Long_t PDGThirdDaughter, Long_t PDGFourthDaughter, Double_t massFirstDaughter, Double_t massSecondDaughter, Double_t massThirdDaughter, Double_t massFourthDaughter);
+  //-- gen 4LHe --
+  void MCThreeBodyDecay (Int_t stackN, AliMCParticle *tparticleMother, Long_t PDGMother, Long_t PDGFirstDaughter, Long_t PDGSecondDaughter, Long_t PDGThirdDaughter, Double_t massFirstDaughter, Double_t massSecondDaughter, Double_t massThirdDaughter); 
+  //-- stack label calculation --
   Int_t GetLabel(Int_t labelFirstMother, Int_t particlePdgCode, Int_t motherparticlePdgCode);
   Int_t GetLabel(Int_t labelFirstMother, Int_t particlePdgCode);
-
+  //-- trigger selection and simulation --
+  Bool_t TriggerSelection();
+  //-- see AliExternalTrackParams --
+  Float_t GetInvPtDevFromBC(Int_t b, Int_t c);
+  //-- clac of dEdx --
+  Double_t Bethe(const AliESDtrack& track, Double_t mass, Int_t charge, Double_t* params);
+  //
+  Double_t GeoLength(const AliESDtrack& track);
+  //
+  Double_t GetTOFSignal(const AliESDtrack& track);
+  //
   void ResetVals(TString mode);
-
-
+  //
   AliAnalysisTaskDoubleHypNucTree(const AliAnalysisTaskDoubleHypNucTree&);
   AliAnalysisTaskDoubleHypNucTree &operator=(const AliAnalysisTaskDoubleHypNucTree&);
 
