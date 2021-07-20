ddb8f29972fc36033b39e50c06101ee7eb56fa66
Joshua Koenig <joshua.konig@cern.ch>
2021-07-20 12:47:43 +0200

    PWGGA/GammaConv: Added M02 vs E histogram to calibration task and added train configs

--- PWGGA/GammaConv/AliAnalysisTaskConvCaloCalibration.h
+++ PWGGA/GammaConv/AliAnalysisTaskConvCaloCalibration.h
@@ -210,6 +210,8 @@ protected:
   TH1F***                 fHistoClusGammaERxSM;                               //!array of histos with cluster E in 0.1 < M02 < 0.3 for each SM
   TH1F**                  fHistoClusGammaERxNCellCrit;                        //!array of histos with cluster E in 0.1 < M02 < 0.3 and NCell > 4
   TH1F***                 fHistoClusGammaERxNCellCritSM;                      //!array of histos with cluster E in 0.1 < M02 < 0.3 and NCell > 4 for each SM
+  TH2F**                  fHistoEVsM02;                                       //!array of histos with cluster E vs M02
+  TH2F**                  fHistoEVsM02NCell4;                                 //!array of histos with cluster E vs M02 for NCell > 4
   TH1F**                  fHistoMotherInvMassRejected;                        //! array of histos with invariant mass pairs which were rejected
   TH1F**                  fHistoNEvents;                                      //! array of histos with event information
   TH1F**                  fHistoNEventsWOWeight;                              //! array of histos with event information without event weights
