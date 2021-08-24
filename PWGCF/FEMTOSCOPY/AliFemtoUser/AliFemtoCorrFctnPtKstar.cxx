8d5951277e2862f3fed2c461773cff6380a0cdf3
dbauri <63589684+dbauri@users.noreply.github.com>
2021-08-24 18:54:17 +0530

    Adding histogram for pair pT vs. kstarr

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoCorrFctnPtKstar.cxx
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoCorrFctnPtKstar.cxx
@@ -28,7 +28,11 @@ AliFemtoCorrFctn(),
 
   fPtKstar2part = new TH2D(Form("PtvsKstar2part%s",title),"Pt vs kstar (part 2)",200,0.0,2.0, 200, 0.0, 4.0);
   fPtKstarDen2part = new TH2D(Form("PtvsKstarDen2part%s",title),"Pt vs kstar in mixed events (part 2)",200,0.0,2.0, 200, 0.0, 4.0);
-}
+
+  fPairPtKstar = new TH2D(Form("PairPtvsKstar%s",title),"Pair Pt vs kstar (part 1)",100,0.0,0.5, 300, 0.0, 6.0);
+  fPairPtKstarDen = new TH2D(Form("PairPtvsKstarDen%s",title),"Pair Pt vs kstar in mixed events (part 1)",100,0.0,0.5, 300, 0.0, 6.0);
+
+  }
 
 //____________________________
 AliFemtoCorrFctnPtKstar::AliFemtoCorrFctnPtKstar(const AliFemtoCorrFctnPtKstar& aCorrFctn) :
@@ -36,7 +40,9 @@ AliFemtoCorrFctnPtKstar::AliFemtoCorrFctnPtKstar(const AliFemtoCorrFctnPtKstar& 
    fPtKstar(0),
   fPtKstarDen(0),
   fPtKstar2part(0),
-  fPtKstarDen2part(0)
+  fPtKstarDen2part(0),
+  fPairPtKstar(0),
+  fPairPtKstarDen(0)
 {
   // copy constructor
   if (fPtKstar) delete fPtKstar;
@@ -52,6 +58,8 @@ AliFemtoCorrFctnPtKstar::~AliFemtoCorrFctnPtKstar(){
     delete  fPtKstarDen;
     delete  fPtKstar2part;
     delete  fPtKstarDen2part;
+    delete  fPairPtKstar;
+    delete  fPairPtKstarDen;
 }
 //_________________________
 AliFemtoCorrFctnPtKstar& AliFemtoCorrFctnPtKstar::operator=(const AliFemtoCorrFctnPtKstar& aCorrFctn)
@@ -71,6 +79,12 @@ AliFemtoCorrFctnPtKstar& AliFemtoCorrFctnPtKstar::operator=(const AliFemtoCorrFc
   
   if (fPtKstarDen2part) delete fPtKstarDen2part;
   fPtKstarDen2part = new TH2D(*aCorrFctn.fPtKstarDen2part);
+  
+  if (fPairPtKstar) delete fPairPtKstar;
+  fPairPtKstar = new TH2D(*aCorrFctn.fPairPtKstar);
+  
+  if (fPairPtKstarDen) delete fPairPtKstarDen;
+  fPairPtKstarDen = new TH2D(*aCorrFctn.fPairPtKstarDen);
   
   return *this;
 }
@@ -97,6 +111,8 @@ AliFemtoString AliFemtoCorrFctnPtKstar::Report(){
 void AliFemtoCorrFctnPtKstar::AddRealPair( AliFemtoPair* pair){
 
  double tKStar = fabs(pair->KStar());
+ double tPairPt = fabs(pair->KT());
+  
  double px = pair->Track1()->Track()->P().x();
  double py = pair->Track1()->Track()->P().y();
  double pT = TMath::Hypot(px, py);
@@ -108,15 +124,20 @@ void AliFemtoCorrFctnPtKstar::AddRealPair( AliFemtoPair* pair){
  double pT2 = TMath::Hypot(px2, py2);
  fPtKstar2part->Fill(tKStar,pT2);
 
+ fPairPtKstar->Fill(tKStar,tPairPt);
+
 }
 //____________________________
 void AliFemtoCorrFctnPtKstar::AddMixedPair( AliFemtoPair* pair){
  double tKStar = fabs(pair->KStar());
+ double tPairPt = fabs(pair->KT());  
+  
  double px = pair->Track1()->Track()->P().x();
  double py = pair->Track1()->Track()->P().y();
  double pT = TMath::Hypot(px, py);
  
  fPtKstarDen->Fill(tKStar,pT);
+ fPairPtKstarDen->Fill(tKStar,tPairPt);  
  
  double px2 = pair->Track2()->Track()->P().x();
  double py2 = pair->Track2()->Track()->P().y();
@@ -133,6 +154,9 @@ void AliFemtoCorrFctnPtKstar::WriteHistos()
   fPtKstarDen->Write();
   fPtKstar2part->Write();
   fPtKstarDen2part->Write();
+  fPairPtKstar->Write();
+  fPairPtKstarDen->Write();
+
 }
 
 TList* AliFemtoCorrFctnPtKstar::GetOutputList()
@@ -144,6 +168,8 @@ TList* AliFemtoCorrFctnPtKstar::GetOutputList()
   tOutputList->Add(fPtKstarDen);
   tOutputList->Add(fPtKstar2part);
   tOutputList->Add(fPtKstarDen2part);
+  tOutputList->Add(fPairPtKstar);
+  tOutputList->Add(fPairPtKstarDen);
 
   return tOutputList;
 }
