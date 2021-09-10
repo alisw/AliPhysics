7a3a0eb290432a8b323fe4caa2f89eb5b11cd74d
Luuk Vermunt <luuk.vermunt@cern.ch>
2021-09-10 08:35:07 +0000

    Update Lc PbPb syst

--- PWGHF/vertexingHF/AliHFSystErr.cxx
+++ PWGHF/vertexingHF/AliHFSystErr.cxx
@@ -9029,22 +9029,22 @@ void AliHFSystErr::InitLctopK0S2018PbPb010ML() {
   fNorm = new TH1F("fNorm","fNorm",24,0.,24.);
   for(Int_t i=1;i<=24;i++) fNorm->SetBinContent(i,0.04); // TAA and pp norm
 
-  // NOT FINAL (preliminary values, usual track syst + 6% additional from GeoCut correction)
   // Tracking efficiency
   fTrackingEff = new TH1F("fTrackingEff","fTrackingEff",24,0.,24.);
   fTrackingEff->SetBinContent(1,0.00);
-  for(Int_t i=2;i<=6;i++) fTrackingEff->SetBinContent(i,0.125);
-  for(Int_t i=7;i<=8;i++) fTrackingEff->SetBinContent(i,0.14);
-  for(Int_t i=9;i<=12;i++) fTrackingEff->SetBinContent(i,0.15);
-  for(Int_t i=13;i<=24;i++) fTrackingEff->SetBinContent(i,0.145);
+  fTrackingEff->SetBinContent(2,0.11);
+  for(Int_t i=3;i<=12;i++) fTrackingEff->SetBinContent(i,0.10);
+  for(Int_t i=13;i<=24;i++) fTrackingEff->SetBinContent(i,0.09);
 
   // Raw yield extraction
   fRawYield = new TH1F("fRawYield","fRawYield",24,0.,24.);
   fRawYield->SetBinContent(1,0.00);
   fRawYield->SetBinContent(2,0.11);
-  for(Int_t i=3;i<=6;i++) fRawYield->SetBinContent(i,0.10);
-  for(Int_t i=7;i<=12;i++) fRawYield->SetBinContent(i,0.07);
-  for(Int_t i=13;i<=24;i++) fRawYield->SetBinContent(i,0.10);
+  for(Int_t i=3;i<=4;i++) fRawYield->SetBinContent(i,0.12);
+  for(Int_t i=5;i<=6;i++) fRawYield->SetBinContent(i,0.11);
+  for(Int_t i=7;i<=8;i++) fRawYield->SetBinContent(i,0.08);
+  for(Int_t i=9;i<=12;i++) fRawYield->SetBinContent(i,0.11);
+  for(Int_t i=13;i<=24;i++) fRawYield->SetBinContent(i,0.15);
 
   fCutsEff = new TH1F("fCutsEff","fCutsEff",24,0.,24.);
   fCutsEff->SetBinContent(1,0.00);
@@ -9081,22 +9081,23 @@ void AliHFSystErr::InitLctopK0S2018PbPb3050ML() {
   fNorm = new TH1F("fNorm","fNorm",24,0.,24.);
   for(Int_t i=1;i<=24;i++) fNorm->SetBinContent(i,0.04); // TAA and pp norm
 
-  // NOT FINAL (preliminary values, usual track syst + 4% additional from GeoCut correction)
   // Tracking efficiency
   fTrackingEff = new TH1F("fTrackingEff","fTrackingEff",24,0.,24.);
   fTrackingEff->SetBinContent(1,0.00);
-  for(Int_t i=2;i<=4;i++) fTrackingEff->SetBinContent(i,0.10);
-  for(Int_t i=5;i<=6;i++) fTrackingEff->SetBinContent(i,0.11);
-  for(Int_t i=7;i<=8;i++) fTrackingEff->SetBinContent(i,0.11);
-  for(Int_t i=9;i<=12;i++) fTrackingEff->SetBinContent(i,0.11);
-  for(Int_t i=13;i<=24;i++) fTrackingEff->SetBinContent(i,0.10);
+  fTrackingEff->SetBinContent(2,0.12);
+  for(Int_t i=3;i<=4;i++) fTrackingEff->SetBinContent(i,0.10);
+  for(Int_t i=5;i<=6;i++) fTrackingEff->SetBinContent(i,0.09);
+  for(Int_t i=7;i<=24;i++) fTrackingEff->SetBinContent(i,0.08);
 
   // Raw yield extraction
   fRawYield = new TH1F("fRawYield","fRawYield",24,0.,24.);
   fRawYield->SetBinContent(1,0.00);
-  fRawYield->SetBinContent(2,0.10);
-  for(Int_t i=3;i<=6;i++) fRawYield->SetBinContent(i,0.06);
-  for(Int_t i=7;i<=24;i++) fRawYield->SetBinContent(i,0.11);
+  fRawYield->SetBinContent(2,0.12);
+  for(Int_t i=3;i<=4;i++) fRawYield->SetBinContent(i,0.09);
+  for(Int_t i=5;i<=6;i++) fRawYield->SetBinContent(i,0.07);
+  for(Int_t i=7;i<=8;i++) fRawYield->SetBinContent(i,0.09);
+  for(Int_t i=9;i<=12;i++) fRawYield->SetBinContent(i,0.07);
+  for(Int_t i=13;i<=24;i++) fRawYield->SetBinContent(i,0.12);
 
   fCutsEff = new TH1F("fCutsEff","fCutsEff",24,0.,24.);
   fCutsEff->SetBinContent(1,0.00);
