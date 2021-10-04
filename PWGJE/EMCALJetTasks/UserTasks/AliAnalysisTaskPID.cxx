80dc66a93d45796ce635bbfdbfbb9730cd8fd2b1
Mario Krüger <mario.kruger@cern.ch>
2021-10-04 14:30:27 +0200

    fix wrong constexpr

--- PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskPID.cxx
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskPID.cxx
@@ -1278,7 +1278,7 @@ void AliAnalysisTaskPID::UserCreateOutputObjects()
     const Int_t histNumbers = 3;
     
     constexpr Int_t nBinsY[histNumbers+1] = {8, 4, 11, 15};
-    constexpr Float_t twopi = 2.0 * TMath::Pi();
+    const Float_t twopi = 2.0 * TMath::Pi();
     
     Double_t binsPrime[nBinsPrime+1];
     for (Int_t i=0;i<=nBinsPrime;i++) {
