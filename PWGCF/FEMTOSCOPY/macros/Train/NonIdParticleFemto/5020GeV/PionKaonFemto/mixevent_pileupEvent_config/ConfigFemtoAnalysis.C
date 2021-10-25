5338b213c65b186244bd6375de59e50ac4509e70
prchakra <47203359+prchakra@users.noreply.github.com>
2021-10-25 23:13:51 +0530

    Adding 2 sets of kT bins

--- PWGCF/FEMTOSCOPY/macros/Train/NonIdParticleFemto/5020GeV/PionKaonFemto/mixevent_pileupEvent_config/ConfigFemtoAnalysis.C
+++ PWGCF/FEMTOSCOPY/macros/Train/NonIdParticleFemto/5020GeV/PionKaonFemto/mixevent_pileupEvent_config/ConfigFemtoAnalysis.C
@@ -60,7 +60,7 @@
 #endif
 
 //_
-AliFemtoManager* ConfigFemtoAnalysis(int runcentrality0, int runcentrality1, int runcentrality2, int runcentrality3, int runcentrality4,int runcentrality5, int runcentrality6, double Vz, int Vz_bin, double eta, double dcaxy, double dcaz, int ncls, int pid_default, int pid_loose, int pid_strict,  int runSHCorrFctn, int runNonIdCorrFctn, int paircutantigammaon, int paircutmergedfractionon, double distance, double fraction1, int runDPhiStarKStarMergedFraction, int runDPhiStarKStarAverageMergedPointsFraction, int runDPhiStarDEta, int turnOnMonitors, int turnOnBetaTMonitor, int runbetatdep, int runbetatylm, int runbetatnonid, int lmax, int no_mix_event, int ispileup, int trackpileup, double pTmin, double pTmax) {
+AliFemtoManager* ConfigFemtoAnalysis(int runcentrality0, int runcentrality1, int runcentrality2, int runcentrality3, int runcentrality4,int runcentrality5, int runcentrality6, double Vz, int Vz_bin, double eta, double dcaxy, double dcaz, int ncls, int pid_default, int pid_loose, int pid_strict,  int runSHCorrFctn, int runNonIdCorrFctn, int paircutantigammaon, int paircutmergedfractionon, double distance, double fraction1, int runDPhiStarKStarMergedFraction, int runDPhiStarKStarAverageMergedPointsFraction, int runDPhiStarDEta, int turnOnMonitors, int turnOnBetaTMonitor, int runbetatdep, int runbetatylm, int runbetatnonid, int lmax, int no_mix_event, int ispileup, int trackpileup, double pTmin, double pTmax, int kt1, int kt2) {
 
 
   double PionMass = 0.13957018;//0.13956995;
@@ -78,7 +78,10 @@ AliFemtoManager* ConfigFemtoAnalysis(int runcentrality0, int runcentrality1, int
   const char *chrgs[numOfChTypes] = { "PIpKp", "PImKm", "PIpKm","PImKp"};
 
   //int runktdep = 1;
-  double ktrng[numOfkTbins + 1] = {0.5, 0.8, 1.1, 1.4, 1.7, 2.0, 2.5, 3.0};
+  if(kt1==1){
+  double ktrng[numOfkTbins + 1] = {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 1.0, 1.5};}		
+  if(kt2==1){
+  double ktrng[numOfkTbins + 1] = {0.5, 0.8, 1.1, 1.4, 1.7, 2.0, 2.5, 3.0};}
   
   int runshlcms = 0;// 0:PRF(PAP), 1:LCMS(PP,APAP)
 
