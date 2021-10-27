bd0931e447d3e7b299aaabd1b7fb05b8749fe1d8
TakuyaKumaoka <takuya.kumaoka@cern.ch>
2021-10-27 20:11:03 +0200

    Request to include a new task for measure raw jet spectam

---
+++ PWGJE/EMCALJetTasks/UserTasks/AliAnalysisTaskRawJetWithEP.cxx
@@ -0,0 +1,526 @@
+/**************************************************************************
+ * Copyright(c) 1998-2016, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
+
+#include <TClonesArray.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TH3.h>
+#include <TGrid.h>
+#include <TList.h>
+#include <THnSparse.h>
+
+#include <AliAnalysisManager.h>
+#include <AliVEventHandler.h>
+#include <AliVCluster.h>
+#include <AliVParticle.h>
+#include <AliLog.h>
+
+#include "AliTLorentzVector.h"
+#include "AliEmcalJet.h"
+#include "AliRhoParameter.h"
+#include "AliJetContainer.h"
+#include "AliParticleContainer.h"
+#include "AliClusterContainer.h"
+
+#include "AliAnalysisTaskRawJetWithEP.h"
+
+/// \cond CLASSIMP
+ClassImp(AliAnalysisTaskRawJetWithEP);
+/// \endcond
+
+/**
+ * Default constructor. Needed by ROOT I/O
+ */
+AliAnalysisTaskRawJetWithEP::AliAnalysisTaskRawJetWithEP() : 
+  AliAnalysisTaskEmcalJet(),
+  fHistManager()
+{
+}
+
+/**
+ * Standard constructor. Should be used by the user.
+ *
+ * @param[in] name Name of the task
+ */
+AliAnalysisTaskRawJetWithEP::AliAnalysisTaskRawJetWithEP(const char *name) : 
+  AliAnalysisTaskEmcalJet(name, kTRUE),
+  fHistManager(name)
+{
+  SetMakeGeneralHistograms(kTRUE);
+}
+
+/**
+ * Destructor
+ */
+AliAnalysisTaskRawJetWithEP::~AliAnalysisTaskRawJetWithEP()
+{
+}
+
+/**
+ * Performing run-independent initialization.
+ * Here the histograms should be instantiated.
+ */
+void AliAnalysisTaskRawJetWithEP::UserCreateOutputObjects()
+{
+  AliAnalysisTaskEmcalJet::UserCreateOutputObjects();
+  AllocateTrackHistograms();
+  AllocateJetHistograms();
+
+  TIter next(fHistManager.GetListOfHistograms());
+  TObject* obj = 0;
+  while ((obj = next())) {
+    fOutput->Add(obj);
+  }
+  PostData(1, fOutput); // Post data for ALL output slots > 0 here.
+}
+
+/*
+ * This function allocates the histograms for basic tracking QA.
+ * A set of histograms (pT, eta, phi, difference between kinematic properties
+ * at the vertex and at the EMCal surface, number of tracks) is allocated
+ * per each particle container and per each centrality bin.
+ */
+void AliAnalysisTaskRawJetWithEP::AllocateTrackHistograms()
+{
+  TString histname;
+  TString histtitle;
+  TString groupname;
+  AliParticleContainer* partCont = 0;
+  TIter next(&fParticleCollArray);
+  while ((partCont = static_cast<AliParticleContainer*>(next()))) {
+    groupname = partCont->GetName();
+    // Protect against creating the histograms twice
+    if (fHistManager.FindObject(groupname)) {
+      AliWarning(TString::Format("%s: Found groupname %s in hist manager. The track containers will be filled into the same histograms.", GetName(), groupname.Data()));
+      continue;
+    }
+    fHistManager.CreateHistoGroup(groupname);
+    for (Int_t cent = 0; cent < fNcentBins; cent++) {
+      histname = TString::Format("%s/histTrackPt_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{p}_{T,track} (GeV/#it{c});counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins / 2, fMinBinPt, fMaxBinPt / 2);
+
+      histname = TString::Format("%s/histTrackPhi_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{#phi}_{track};counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins / 2, 0, TMath::TwoPi());
+
+      histname = TString::Format("%s/histTrackEta_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{#eta}_{track};counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins / 6, -1, 1);
+
+      if (TClass(partCont->GetClassName()).InheritsFrom("AliVTrack")) {
+        histname = TString::Format("%s/fHistDeltaEtaPt_%d", groupname.Data(), cent);
+        histtitle = TString::Format("%s;#it{p}_{T,track}^{vertex} (GeV/#it{c});#it{#eta}_{track}^{vertex} - #it{#eta}_{track}^{EMCal};counts", histname.Data());
+        fHistManager.CreateTH2(histname, histtitle, fNbins / 2, fMinBinPt, fMaxBinPt, 50, -0.5, 0.5);
+
+        histname = TString::Format("%s/fHistDeltaPhiPt_%d", groupname.Data(), cent);
+        histtitle = TString::Format("%s;#it{p}_{T,track}^{vertex} (GeV/#it{c});#it{#phi}_{track}^{vertex} - #it{#phi}_{track}^{EMCal};counts", histname.Data());
+        fHistManager.CreateTH2(histname, histtitle, fNbins / 2, fMinBinPt, fMaxBinPt, 200, -2, 2);
+
+        histname = TString::Format("%s/fHistDeltaPtvsPt_%d", groupname.Data(), cent);
+        histtitle = TString::Format("%s;#it{p}_{T,track}^{vertex} (GeV/#it{c});#it{p}_{T,track}^{vertex} - #it{p}_{T,track}^{EMCal} (GeV/#it{c});counts", histname.Data());
+        fHistManager.CreateTH2(histname, histtitle, fNbins / 2, fMinBinPt, fMaxBinPt, fNbins / 2, -fMaxBinPt/2, fMaxBinPt/2);
+
+        histname = TString::Format("%s/fHistEoverPvsP_%d", groupname.Data(), cent);
+        histtitle = TString::Format("%s;#it{P}_{track} (GeV/#it{c});#it{E}_{cluster} / #it{P}_{track} #it{c};counts", histname.Data());
+        fHistManager.CreateTH2(histname, histtitle, fNbins / 2, fMinBinPt, fMaxBinPt, fNbins / 2, 0, 4);
+      }
+
+      histname = TString::Format("%s/histNTracks_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;number of tracks;events", histname.Data());
+      if (fForceBeamType != kpp) {
+        fHistManager.CreateTH1(histname, histtitle, 500, 0, 5000);
+      }
+      else {
+        fHistManager.CreateTH1(histname, histtitle, 200, 0, 200);
+      }
+    }
+  }
+
+  histname = "fHistSumNTracks";
+  histtitle = TString::Format("%s;Sum of n tracks;events", histname.Data());
+  if (fForceBeamType != kpp) {
+    fHistManager.CreateTH1(histname, histtitle, 500, 0, 5000);
+  }
+  else {
+    fHistManager.CreateTH1(histname, histtitle, 200, 0, 200);
+  }
+}
+
+/*
+ * This function allocates the histograms for basic jet QA.
+ * A set of histograms (pT, eta, phi, area, number of jets, corrected pT) is allocated
+ * per each jet container and per each centrality bin.
+ */
+void AliAnalysisTaskRawJetWithEP::AllocateJetHistograms()
+{
+  TString histname;
+  TString histtitle;
+  TString groupname;
+  AliJetContainer* jetCont = 0;
+  TIter next(&fJetCollArray);
+  while ((jetCont = static_cast<AliJetContainer*>(next()))) {
+    groupname = jetCont->GetName();
+    // Protect against creating the histograms twice
+    if (fHistManager.FindObject(groupname)) {
+      AliWarning(TString::Format("%s: Found groupname %s in hist manager. The jet containers will be filled into the same histograms.", GetName(), groupname.Data()));
+      continue;
+    }
+    fHistManager.CreateHistoGroup(groupname);
+
+    // A vs. pT
+    if (fForceBeamType == kAA) {
+      histname = TString::Format("%s/hRhoVsCent", groupname.Data());
+      histtitle = histname + ";Centrality (%);#rho (GeV/#it{c});counts";
+    	fHistManager.CreateTH2(histname, histtitle.Data(), 50, 0, 100, 100, 0, 500);
+
+      histname = TString::Format("%s/CentVsPtVsPtdence", groupname.Data());
+      histtitle = histname + ";Centrality (%);#it{p}_{T}^{corr} (GeV/#it{c});#it{A}_{jet}/#pi#it{R}^{2}";
+      // fHistManager.CreateTH3(histname, histtitle.Data(), 10, 0, 100, 300, 0, 250, 75, 0, 3);
+    }
+
+    for (Int_t cent = 0; cent < fNcentBins; cent++) {
+      histname = TString::Format("%s/histJetPt_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{p}_{T,jet} (GeV/#it{c});counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins, fMinBinPt, fMaxBinPt);
+
+      histname = TString::Format("%s/histJetArea_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{A}_{jet};counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins / 2, 0, 3);
+
+      histname = TString::Format("%s/histJetPhi_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{#phi}_{jet};counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins / 2, 0, TMath::TwoPi());
+
+      histname = TString::Format("%s/histJetEta_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;#it{#eta}_{jet};counts", histname.Data());
+      fHistManager.CreateTH1(histname, histtitle, fNbins / 6, -1, 1);
+
+      histname = TString::Format("%s/histNJets_%d", groupname.Data(), cent);
+      histtitle = TString::Format("%s;number of jets;events", histname.Data());
+      if (fForceBeamType != kpp) {
+        fHistManager.CreateTH1(histname, histtitle, 500, 0, 500);
+      }
+      else {
+        fHistManager.CreateTH1(histname, histtitle, 100, 0, 100);
+      }
+
+      if (!jetCont->GetRhoName().IsNull()) {
+        histname = TString::Format("%s/histJetCorrPt_%d", groupname.Data(), cent);
+        histtitle = TString::Format("%s;#it{p}_{T,jet}^{corr} (GeV/#it{c});counts", histname.Data());
+        fHistManager.CreateTH1(histname, histtitle, fNbins, -fMaxBinPt / 2, fMaxBinPt / 2);
+      }
+    }
+  }
+}
+
+/**
+ * The body of this function should contain instructions to fill the output histograms.
+ * This function is called inside the event loop, after the function Run() has been
+ * executed successfully (i.e. it returned kTRUE).
+ * @return Always kTRUE
+ */
+Bool_t AliAnalysisTaskRawJetWithEP::FillHistograms()
+{
+  DoJetLoop();
+  DoTrackLoop();
+
+  return kTRUE;
+}
+
+/**
+ * This function performs a loop over the reconstructed jets
+ * in the current event and fills the relevant histograms.
+ */
+void AliAnalysisTaskRawJetWithEP::DoJetLoop()
+{
+  TString histname;
+  TString groupname;
+  AliJetContainer* jetCont = 0;
+  TIter next(&fJetCollArray);
+
+
+  while ((jetCont = static_cast<AliJetContainer*>(next()))) {
+    groupname = jetCont->GetName();
+    UInt_t count = 0;
+    Double_t jetR = jetCont->GetJetRadius();
+
+    Double_t rhoVal = 0;
+    if (jetCont->GetRhoParameter()) {
+      rhoVal = jetCont->GetRhoVal();
+      histname = TString::Format("%s/hRhoVsCent", groupname.Data());
+      fHistManager.FillTH2(histname.Data(), fCent, rhoVal);
+    }
+
+    for(auto jet : jetCont->accepted()) {
+      if (!jet) continue;
+      count++;
+      histname = TString::Format("%s/histJetPt_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, jet->Pt());
+
+      histname = TString::Format("%s/histJetArea_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, jet->Area());
+
+      histname = TString::Format("%s/histJetPhi_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, jet->Phi());
+
+      histname = TString::Format("%s/histJetEta_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, jet->Eta());
+
+      if (jetCont->GetRhoParameter()) {
+        Float_t corrPt = jet->Pt() - jetCont->GetRhoVal() * jet->Area();
+        histname = TString::Format("%s/histJetCorrPt_%d", groupname.Data(), fCentBin);
+        fHistManager.FillTH1(histname, corrPt);
+
+        // fHistManager.FillTH3(histname.Data(), fCentBin, corrPt, \
+        1.0*jet->Area()/(1.0*TMath::Pi()*pow(jetR,2)));
+      }
+    }
+    histname = TString::Format("%s/histNJets_%d", groupname.Data(), fCentBin);
+    fHistManager.FillTH1(histname, count);
+  }
+}
+
+/**
+ * This function performs a loop over the reconstructed tracks
+ * in the current event and fills the relevant histograms.
+ */
+void AliAnalysisTaskRawJetWithEP::DoTrackLoop()
+{
+  AliClusterContainer* clusCont = GetClusterContainer(0);
+
+  TString histname;
+  TString groupname;
+  UInt_t sumAcceptedTracks = 0;
+  AliParticleContainer* partCont = 0;
+  TIter next(&fParticleCollArray);
+  while ((partCont = static_cast<AliParticleContainer*>(next()))) {
+    groupname = partCont->GetName();
+    UInt_t count = 0;
+    for(auto part : partCont->accepted()) {
+      if (!part) continue;
+      count++;
+
+      histname = TString::Format("%s/histTrackPt_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, part->Pt());
+
+      histname = TString::Format("%s/histTrackPhi_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, part->Phi());
+
+      histname = TString::Format("%s/histTrackEta_%d", groupname.Data(), fCentBin);
+      fHistManager.FillTH1(histname, part->Eta());
+
+      if (partCont->GetLoadedClass()->InheritsFrom("AliVTrack")) {
+        const AliVTrack* track = static_cast<const AliVTrack*>(part);
+
+        histname = TString::Format("%s/fHistDeltaEtaPt_%d", groupname.Data(), fCentBin);
+        fHistManager.FillTH1(histname, track->Pt(), track->Eta() - track->GetTrackEtaOnEMCal());
+
+        histname = TString::Format("%s/fHistDeltaPhiPt_%d", groupname.Data(), fCentBin);
+        fHistManager.FillTH1(histname, track->Pt(), track->Phi() - track->GetTrackPhiOnEMCal());
+
+        histname = TString::Format("%s/fHistDeltaPtvsPt_%d", groupname.Data(), fCentBin);
+        fHistManager.FillTH1(histname, track->Pt(), track->Pt() - track->GetTrackPtOnEMCal());
+
+        if (clusCont) {
+          Int_t iCluster = track->GetEMCALcluster();
+          if (iCluster >= 0) {
+            AliVCluster* cluster = clusCont->GetAcceptCluster(iCluster);
+            if (cluster) {
+              histname = TString::Format("%s/fHistEoverPvsP_%d", groupname.Data(), fCentBin);
+              fHistManager.FillTH2(histname, track->P(), cluster->GetNonLinCorrEnergy() / track->P());
+            }
+          }
+        }
+      }
+    }
+    sumAcceptedTracks += count;
+
+    histname = TString::Format("%s/histNTracks_%d", groupname.Data(), fCentBin);
+    fHistManager.FillTH1(histname, count);
+  }
+
+  histname = "fHistSumNTracks";
+  fHistManager.FillTH1(histname, sumAcceptedTracks);
+}
+
+
+/**
+ * This function is executed automatically for the first event.
+ * Some extra initialization can be performed here.
+ */
+void AliAnalysisTaskRawJetWithEP::ExecOnce()
+{
+  AliAnalysisTaskEmcalJet::ExecOnce();
+}
+
+/**
+ * Run analysis code here, if needed.
+ * It will be executed before FillHistograms().
+ * If this function return kFALSE, FillHistograms() will *not*
+ * be executed for the current event
+ * @return Always kTRUE
+ */
+Bool_t AliAnalysisTaskRawJetWithEP::Run()
+{
+  return kTRUE;
+}
+
+/**
+ * This function is called once at the end of the analysis.
+ */
+void AliAnalysisTaskRawJetWithEP::Terminate(Option_t *) 
+{
+}
+
+/**
+ * This function adds the task to the analysis manager. Often, this function is called
+ * by an AddTask C macro. However, by compiling the code, it ensures that we do not
+ * have to deal with difficulties caused by CINT.
+ */
+AliAnalysisTaskRawJetWithEP * AliAnalysisTaskRawJetWithEP::AddTaskRawJetWithEP(
+  const char *ntracks,
+  const char *nclusters,
+  const char* ncells,
+  const char *suffix)
+{
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr)
+  {
+    ::Error("AddTaskRawJetWithEP", "No analysis manager to connect to.");
+    return 0;
+  }
+
+  // Check the analysis type using the event handlers connected to the analysis manager.
+  //==============================================================================
+  AliVEventHandler* handler = mgr->GetInputEventHandler();
+  if (!handler)
+  {
+    ::Error("AddTaskRawJetWithEP", "This task requires an input event handler");
+    return 0;
+  }
+
+  enum EDataType_t {
+    kUnknown,
+    kESD,
+    kAOD
+  };
+
+  EDataType_t dataType = kUnknown;
+
+  if (handler->InheritsFrom("AliESDInputHandler")) {
+    dataType = kESD;
+  }
+  else if (handler->InheritsFrom("AliAODInputHandler")) {
+    dataType = kAOD;
+  }
+
+  //-------------------------------------------------------
+  // Init the task and do settings
+  //-------------------------------------------------------
+
+  TString trackName(ntracks);
+  TString clusName(nclusters);
+  TString cellName(ncells);
+
+  if (trackName == "usedefault") {
+    if (dataType == kESD) {
+      trackName = "Tracks";
+    }
+    else if (dataType == kAOD) {
+      trackName = "tracks";
+    }
+    else {
+      trackName = "";
+    }
+  }
+
+  if (clusName == "usedefault") {
+    if (dataType == kESD) {
+      clusName = "CaloClusters";
+    }
+    else if (dataType == kAOD) {
+      clusName = "caloClusters";
+    }
+    else {
+      clusName = "";
+    }
+  }
+
+  if (cellName == "usedefault") {
+    if (dataType == kESD) {
+      cellName = "EMCALCells";
+    }
+    else if (dataType == kAOD) {
+      cellName = "emcalCells";
+    }
+    else {
+      cellName = "";
+    }
+  }
+
+  TString name("AliAnalysisTaskRawJetWithEP");
+  if (!trackName.IsNull()) {
+    name += "_";
+    name += trackName;
+  }
+  if (!clusName.IsNull()) {
+    name += "_";
+    name += clusName;
+  }
+  if (!cellName.IsNull()) {
+    name += "_";
+    name += cellName;
+  }
+  if (strcmp(suffix,"") != 0) {
+    name += "_";
+    name += suffix;
+  }
+
+  AliAnalysisTaskRawJetWithEP* rawJetTask = new AliAnalysisTaskRawJetWithEP(name);
+  rawJetTask->SetCaloCellsName(cellName);
+  rawJetTask->SetVzRange(-10,10);
+
+  if (trackName == "mcparticles") {
+    rawJetTask->AddMCParticleContainer(trackName);
+  }
+  else if (trackName == "tracks" || trackName == "Tracks") {
+    rawJetTask->AddTrackContainer(trackName);
+  }
+  else if (!trackName.IsNull()) {
+    rawJetTask->AddParticleContainer(trackName);
+  }
+  rawJetTask->AddClusterContainer(clusName);
+
+  //-------------------------------------------------------
+  // Final settings, pass to manager and set the containers
+  //-------------------------------------------------------
+
+  mgr->AddTask(rawJetTask);
+
+  // Create containers for input/output
+  AliAnalysisDataContainer *cinput1  = mgr->GetCommonInputContainer()  ;
+  TString contname(name);
+  contname += "_histos";
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer(contname.Data(),
+      TList::Class(),AliAnalysisManager::kOutputContainer,
+      Form("%s", AliAnalysisManager::GetCommonFileName()));
+  mgr->ConnectInput  (rawJetTask, 0,  cinput1 );
+  mgr->ConnectOutput (rawJetTask, 1, coutput1 );
+
+  return rawJetTask;
+}
