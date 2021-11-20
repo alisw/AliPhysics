f28478ec7b443077e3566f156cc2645938eb84a0
vipetrov <93945835+vipetrov@users.noreply.github.com>
2021-11-20 14:57:11 +0300

    Update PWGCFebyeLinkDef.h

--- PWGCF/EBYE/PWGCFebyeLinkDef.h
+++ PWGCF/EBYE/PWGCFebyeLinkDef.h
@@ -111,7 +111,6 @@
 #pragma link C++ class TrackInfoCorr+;
 #pragma link C++ class AliAnalysisTaskFluctMCTEPOS+;
 
-#pragma link C++ class AliAnalysisTaskParticleYieldRatioCorrelations+;
 #pragma link C++ class AliAnalysisTaskParticleYieldRatioCorrelationsEfficiency+;
 
 #endif
