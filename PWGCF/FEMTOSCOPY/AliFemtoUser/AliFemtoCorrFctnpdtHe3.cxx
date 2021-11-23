079689a238befadf611e703225a2df491ab1f5b7
df <df@localhost.localdomain>
2021-11-22 23:03:57 -0800

    dowang add velocity gate CorrFcn

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoCorrFctnpdtHe3.cxx
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliFemtoCorrFctnpdtHe3.cxx
@@ -3,6 +3,9 @@
 ///
 
 #include "AliFemtoCorrFctnpdtHe3.h"
+const float ProtonMass = 0.9383;
+const float DeuteronMass= 2.225;
+const float TritonMass = 2.8089;
 const float He3Mass = 2.8084;
 /*
 AliFemtoCorrFctnpdtHe3::AliFemtoCorrFctnpdtHe3():
@@ -21,10 +24,16 @@ AliFemtoCorrFctnpdtHe3::AliFemtoCorrFctnpdtHe3(const char* title,
     fNbinsKStar(nbins),
     fKStarLow(KStarLo),
     fKStarHigh(KStarHi),
+    fP1Mass(0.1),
+    fP2Mass(0.1),
     fNumerator(nullptr),
     fDenominator(nullptr),
     fQANumerator(3, nullptr),
-    fQADenominator(3, nullptr)
+    fQADenominator(3, nullptr),
+    fP1EarlierP2Num(nullptr),
+    fP1EarlierP2Dum(nullptr),
+    fP2EarlierP1Num(nullptr),
+    fP2EarlierP1Dum(nullptr)
 {
     
     fNumerator      = new TH1D(TString::Format("Num%s", fTitle.Data()), "fNumerator", nbins, KStarLo, KStarHi);
@@ -46,6 +55,15 @@ AliFemtoCorrFctnpdtHe3::AliFemtoCorrFctnpdtHe3(const char* title,
 
     }
 
+    fP1EarlierP2Num = new TH1D(TString::Format("p1Ep2Num_%s", fTitle.Data()), " ", nbins, KStarLo, KStarHi);
+    fP1EarlierP2Dum = new TH1D(TString::Format("p1Ep2Dum_%s", fTitle.Data()), " ", nbins, KStarLo, KStarHi);;
+    fP2EarlierP1Num = new TH1D(TString::Format("p2Ep1Num_%s", fTitle.Data()), " ", nbins, KStarLo, KStarHi);;
+    fP2EarlierP1Dum = new TH1D(TString::Format("p2Ep1Dum_%s", fTitle.Data()), " ", nbins, KStarLo, KStarHi);;
+
+    fP1EarlierP2Num->Sumw2();
+    fP1EarlierP2Dum->Sumw2();
+    fP2EarlierP1Num->Sumw2();
+    fP2EarlierP1Dum->Sumw2();
 
 
 }
@@ -55,11 +73,17 @@ AliFemtoCorrFctnpdtHe3::AliFemtoCorrFctnpdtHe3(const AliFemtoCorrFctnpdtHe3& aCo
     fTitle(aCorrFctn.fTitle),
     fNbinsKStar(aCorrFctn.fNbinsKStar),
     fKStarLow(aCorrFctn.fKStarLow),
-    fKStarHigh(aCorrFctn.fKStarHigh),
+    fKStarHigh(aCorrFctn.fKStarHigh),    
+    fP1Mass(aCorrFctn.fP1Mass),
+    fP2Mass(aCorrFctn.fP2Mass),
     fNumerator(aCorrFctn.fNumerator ? new TH1D(*aCorrFctn.fNumerator) : nullptr),
     fDenominator(aCorrFctn.fDenominator ? new TH1D(*aCorrFctn.fDenominator) : nullptr),
     fQANumerator(3, nullptr),
-    fQADenominator(3, nullptr)
+    fQADenominator(3, nullptr),
+    fP1EarlierP2Num(aCorrFctn.fP1EarlierP2Num),
+    fP1EarlierP2Dum(aCorrFctn.fP1EarlierP2Dum),
+    fP2EarlierP1Num(aCorrFctn.fP2EarlierP1Num),
+    fP2EarlierP1Dum(aCorrFctn.fP2EarlierP1Dum)
 {
     
     // copy constructor
@@ -80,6 +104,11 @@ AliFemtoCorrFctnpdtHe3::~AliFemtoCorrFctnpdtHe3()
         delete fQADenominator[ihist];
        
     }
+    delete fP1EarlierP2Num;
+    delete fP1EarlierP2Dum;
+    delete fP2EarlierP1Num;
+    delete fP2EarlierP1Dum;
+
     
 }
 AliFemtoCorrFctnpdtHe3& AliFemtoCorrFctnpdtHe3::operator=(const AliFemtoCorrFctnpdtHe3& aCorrFctn)
@@ -95,11 +124,13 @@ AliFemtoCorrFctnpdtHe3& AliFemtoCorrFctnpdtHe3::operator=(const AliFemtoCorrFctn
     fNbinsKStar = aCorrFctn.fNbinsKStar;
     fKStarLow = aCorrFctn.fKStarLow;
     fKStarHigh = aCorrFctn.fKStarHigh;
+    fP1Mass = aCorrFctn.fP1Mass;
+    fP2Mass = aCorrFctn.fP2Mass;
 
     if(fNumerator) delete fNumerator;
-    fNumerator = new TH1D(*aCorrFctn.fNumerator);
+        fNumerator = new TH1D(*aCorrFctn.fNumerator);
     if(fDenominator) delete fDenominator;
-    fDenominator = new TH1D(*aCorrFctn.fDenominator);
+        fDenominator = new TH1D(*aCorrFctn.fDenominator);
 
     if (fQANumerator.size() == aCorrFctn.fQANumerator.size()) {
         // simple assignments if same number of histograms
@@ -125,6 +156,16 @@ AliFemtoCorrFctnpdtHe3& AliFemtoCorrFctnpdtHe3::operator=(const AliFemtoCorrFctn
             fQADenominator[ihist]   = new TH1D(*aCorrFctn.fQADenominator[ihist]);
         }
     }
+
+
+    if(fP1EarlierP2Num) delete fP1EarlierP2Num;
+        fP1EarlierP2Num = new TH1D(*aCorrFctn.fP1EarlierP2Num);
+    if(fP1EarlierP2Dum) delete fP1EarlierP2Dum;
+        fP1EarlierP2Dum = new TH1D(*aCorrFctn.fP1EarlierP2Dum);
+    if(fP2EarlierP1Num) delete fP2EarlierP1Num;
+        fP2EarlierP1Num = new TH1D(*aCorrFctn.fP2EarlierP1Num);
+    if(fP2EarlierP1Dum) delete fP2EarlierP1Dum;
+        fP2EarlierP1Dum = new TH1D(*aCorrFctn.fP2EarlierP1Dum);
 
     return *this;
 
@@ -152,6 +193,13 @@ TList* AliFemtoCorrFctnpdtHe3::GetOutputList()
         tOutputList->Add(fQADenominator[ihist]);
 
     }
+
+    tOutputList->Add(fP1EarlierP2Num);
+    tOutputList->Add(fP1EarlierP2Dum);
+    tOutputList->Add(fP2EarlierP1Num);
+    tOutputList->Add(fP2EarlierP1Dum);
+    
+
     return tOutputList;
 }
 void AliFemtoCorrFctnpdtHe3::Finish()
@@ -167,6 +215,11 @@ void AliFemtoCorrFctnpdtHe3::Write()
         fQANumerator[ihist]->Write();
         fQADenominator[ihist]->Write();
     }
+
+    fP1EarlierP2Num->Write();
+    fP1EarlierP2Dum->Write();
+    fP2EarlierP1Num->Write();
+    fP2EarlierP1Dum->Write();
 
 }
 void AliFemtoCorrFctnpdtHe3::AddRealPair(AliFemtoPair* aPair)
@@ -195,6 +248,18 @@ void AliFemtoCorrFctnpdtHe3::AddRealPair(AliFemtoPair* aPair)
     if(p1pT > 0.3) fQANumerator[1]->Fill(tKStar);
     if(p1pT > 0.4) fQANumerator[2]->Fill(tKStar);
   
+
+    int VelLabel = ReVelocityGate(aPair);
+    if(VelLabel == 1){
+        fP1EarlierP2Num->Fill(tKStar);
+    }
+    else if(VelLabel == 2){
+        fP2EarlierP1Num->Fill(tKStar);
+    }
+    else if(VelLabel == 3){
+        return;
+    }
+ 
 }
 
 void AliFemtoCorrFctnpdtHe3::AddMixedPair(AliFemtoPair* aPair)
@@ -212,7 +277,7 @@ void AliFemtoCorrFctnpdtHe3::AddMixedPair(AliFemtoPair* aPair)
     // add true pair
     if (fPairCut && !fPairCut->Pass(fPair)) {
         return;
-    }
+    } 
 
     double tKStar = fabs(fPair->KStar());
     fDenominator->Fill(tKStar);
@@ -223,6 +288,17 @@ void AliFemtoCorrFctnpdtHe3::AddMixedPair(AliFemtoPair* aPair)
     if(p1pT > 0.4) fQADenominator[2]->Fill(tKStar);
 
 
+    int VelLabel = ReVelocityGate(aPair);
+    if(VelLabel == 1){
+        fP1EarlierP2Dum->Fill(tKStar);
+    }
+    else if(VelLabel == 2){
+        fP2EarlierP1Dum->Fill(tKStar);
+    }
+    else if(VelLabel == 3){
+        return;
+    }
+    
 
 }
 void AliFemtoCorrFctnpdtHe3::SetIsHe3Pair(int isOrNot)
@@ -252,6 +328,39 @@ AliFemtoPair * AliFemtoCorrFctnpdtHe3::ChangeP2Mom(AliFemtoPair* aPair)
     fPair->SetTrack2(tPart2);
 
     return fPair;
+}
+int AliFemtoCorrFctnpdtHe3::ReVelocityGate(AliFemtoPair* aPair){
+    //\ 1: p1 faster than p2
+    //\ 2: p2 faster than p1
+    //\ 3: bad
+    AliFemtoParticle *tPart1 = new AliFemtoParticle(*aPair->Track1());
+    AliFemtoLorentzVector tFourMom1 = AliFemtoLorentzVector(tPart1->FourMomentum());
+    float TotalP = tFourMom1.px() * tFourMom1.px() + tFourMom1.py() * tFourMom1.py() + tFourMom1.pz() * tFourMom1.pz();
+    float TotalE = TMath::Sqrt( fP1Mass * fP1Mass + TotalP);
+    float P1velocity = TotalP/TotalE;
+
+    //
+    
+    AliFemtoParticle *tPart2 = new AliFemtoParticle(*aPair->Track2());
+    AliFemtoLorentzVector tFourMom2 = AliFemtoLorentzVector(tPart2->FourMomentum());
+    TotalP = tFourMom2.px() * tFourMom2.px() + tFourMom2.py() * tFourMom2.py() + tFourMom2.pz() * tFourMom2.pz();
+    TotalE = TMath::Sqrt( fP2Mass * fP2Mass + TotalP);
+    float P2velocity = TotalP/TotalE;
+
+    int ReLabel = 3;
+    if(P1velocity > P2velocity){
+        ReLabel = 1;
+    }
+    if(P1velocity < P2velocity){
+        ReLabel = 2;
+    }
+
+    return ReLabel;
+}
+void AliFemtoCorrFctnpdtHe3::SetP1AndP2Mass(float p1Mass,float p2Mass){
+
+    fP1Mass = p1Mass;
+    fP2Mass = p2Mass;
 }
 
 
