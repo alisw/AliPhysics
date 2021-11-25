304a355f2264ad395b2c32c85268ff498ff28ccb
Prottay Das <prottay.das@niser.ac.in>
2021-11-25 11:09:45 +0530

    checking commit

--- PWGCF/FLOW/macros/AddTaskCMWPU.C
+++ PWGCF/FLOW/macros/AddTaskCMWPU.C
@@ -9,7 +9,7 @@ void AddTaskCMWPU(Int_t gFilterBit = 768, Double_t fPtMin=0.2, Double_t fPtMax=1
 {
   // standard with task
   printf("===================================================================================\n");
-  printf("\n                PID: Initialising AliAnalysisTaskCMWPU                             \n");
+  printf("\n                PID: Initialising AliAnalysisTaskCMWPU                           \n");
   printf("===================================================================================\n");
 
 
