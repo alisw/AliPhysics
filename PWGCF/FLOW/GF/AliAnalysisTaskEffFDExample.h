d0b7b5669635e24a4818cc6de7b497ecc98e8edc
vvislavi <vytautas.vislavicius@cern.ch>
2021-12-10 14:22:40 +0100

    AliEffFDContainer now also includes pi/K/p PID. Next, check how FB512 works

--- PWGCF/FLOW/GF/AliAnalysisTaskEffFDExample.h
+++ PWGCF/FLOW/GF/AliAnalysisTaskEffFDExample.h
@@ -15,6 +15,8 @@
 #include "TString.h"
 #include "AliESDtrackCuts.h"
 #include "AliESDEvent.h"
+#include "AliPIDCombined.h"
+#include "AliPIDResponse.h"
 
 
 class TList;
@@ -44,6 +46,9 @@ class AliAnalysisTaskEffFDExample : public AliAnalysisTaskSE {
   void AddTrackCut(AliESDtrackCuts* incut) { l_CreateTCList(); fTCtoAdd->Add(incut); };
   void AddTrackCut(Int_t fb) { fFBtoAdd+=fb; };
   void SetUseGenPt(Bool_t newval) { fUseGenPt = newval; };
+  void EnablePID(Bool_t newval) {fAddPID = newval; };
+  void SetBayesianPIDProbs(std::vector<Double_t> probs) {fBayesPIDProbs.clear(); for(auto i: probs) fBayesPIDProbs.push_back(i); };
+  void SetVertexCut(Double_t newval) {fVtxZCut = newval; };
  protected:
   AliEventCuts fEventCuts;
  private:
@@ -52,7 +57,12 @@ class AliAnalysisTaskEffFDExample : public AliAnalysisTaskSE {
   Bool_t fIsMC;
   Bool_t fUseGenPt;
   AliMCEvent *fMCEvent; //! MC event
+  Bool_t fAddPID;
+  AliPIDResponse *fPIDResponse; //! for PID
+  AliPIDCombined *fBayesPID; //! for PID
+  std::vector<Double_t> fBayesPIDProbs;
   UInt_t fTriggerType;
+  Double_t fVtxZCut;
   AliEffFDContainer *fEfFd;
   Double_t fEtaMin;
   Double_t fEtaMax;
