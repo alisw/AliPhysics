488d23c8777982ad80296d589660c28df34a9e08
Swati Saha <swatisaha067@gmail.com>
2021-12-17 15:24:11 +0530

    check code commit

--- PWGCF/FLOW/macros/AddTaskCMWPU.C
+++ PWGCF/FLOW/macros/AddTaskCMWPU.C
@@ -12,7 +12,7 @@ void AddTaskCMWPU(Int_t gFilterBit = 768, Double_t fPtMin=0.2, Double_t fPtMax=1
   printf("\n                PID: Initialising AliAnalysisTaskCMWPU                           \n");
   printf("===================================================================================\n");
   printf("************************************************************************************\n");
-  printf("############checks##############################");
+  printf("############checking##############################");
   
 
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
