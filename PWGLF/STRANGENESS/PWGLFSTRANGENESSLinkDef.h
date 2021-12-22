d4dd12bf47ec8631031ec9c9341e81c436349c65
Romain Schotter <romain.schotter@cern.ch>
2021-12-22 01:55:20 +0100

    Add new classes to facilitate event-by-event analysis

--- PWGLF/STRANGENESS/PWGLFSTRANGENESSLinkDef.h
+++ PWGLF/STRANGENESS/PWGLFSTRANGENESSLinkDef.h
@@ -71,7 +71,16 @@
 #pragma link C++ class AliAnalysisTaskStrVsMult_BumpStudies+;
 #pragma link C++ class AliAnalysisTaskStrVsMult_PileUpStudies+;
 #pragma link C++ class AliAnalysisTaskStrVsMult_SelectionStudies+;
+#pragma link C++ class AliEventContainer+;
+#pragma link C++ class AliCascadeContainer+;
+#pragma link C++ class AliV0Container+;
+#pragma link C++ class AliTrackInfoContainer+;
 #pragma link C++ class AliAnalysisTaskStrangeCascadesTriggerAODRun2+;
+#pragma link C++ class AliMCEventContainer+;
+#pragma link C++ class AliMCCascadeContainer+;
+#pragma link C++ class AliMCV0Container+;
+#pragma link C++ class AliMCTrackInfoContainer+;
+#pragma link C++ class AliMCGenParticleContainer+;
 #pragma link C++ class AliAnalysisTaskStrangeCascadesTriggerAODRun2MC+;
 #pragma link C++ class AliAnalysisTaskV0multspec+;
 #pragma link C++ class AliAnalysisTaskXiNucleusInteraction+;
