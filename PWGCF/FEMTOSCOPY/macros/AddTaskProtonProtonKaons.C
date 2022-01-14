66c093fd1058b8b3eb59089326f385e05d8ce43e
Raffaele Del Grande <raffaele.del.grande@cern.ch>
2022-01-14 12:32:16 +0100

    Added Mixing control variable in add task

--- PWGCF/FEMTOSCOPY/macros/AddTaskProtonProtonKaons.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskProtonProtonKaons.C
@@ -15,7 +15,7 @@ AliAnalysisTaskSE *AddTaskProtonProtonKaons(int trigger = 0, bool fullBlastQA = 
                                      bool isMC = false, bool isNano = true, bool triggerOn = false, int MixingDepth = 30,
                                      float Q3Limit = 0.6, float Q3LimitSample = 3.0,float Q3LimitSample2 = 3.0, float Q3LimitFraction = 0.5, float Q3LimitSampleFraction = 0.01, float Q3LimitSampleFraction2 = 0.01,
                                      const char *cutVariation = "0", bool turnoffClosePairRejectionCompletely = false, bool ClosePairRejectionForAll = "false",
-                                     const char *triggerVariation = "0", bool UseSphericityCut = false, bool DoOnlyThreeBody = false, bool RunOfficialTwoBody=false, int KaonCut = 1, bool DoTwoPrimary = false) {
+                                     const char *triggerVariation = "0", bool UseSphericityCut = false, bool DoOnlyThreeBody = false, bool RunOfficialTwoBody=false, int KaonCut = 1, bool DoTwoPrimary = false, bool StandardMixing = false) {
 
 
   TString suffix = TString::Format("%s", cutVariation);
@@ -342,6 +342,7 @@ AliAnalysisTaskSE *AddTaskProtonProtonKaons(int trigger = 0, bool fullBlastQA = 
     taskNano->SetDoOnlyThreeBody(DoOnlyThreeBody);
     taskNano->SetRunOfficialTwoBody(RunOfficialTwoBody);
     taskNano->SetDoTwoPrimary(DoTwoPrimary);
+    taskNano->SetStandardMixing(StandardMixing);
 
     mgr->AddTask(taskNano);
 
