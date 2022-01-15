c8bcb6be3be9700d16e87d85a57b7864ce3ab3df
vipetrov <93945835+vipetrov@users.noreply.github.com>
2022-01-15 20:13:00 +0300

    Add suffix for subwagons feature

--- PWGCF/EBYE/ParticleYieldRatioCorrelations/macros/AddTaskParticleYieldRatioCorrelations.C
+++ PWGCF/EBYE/ParticleYieldRatioCorrelations/macros/AddTaskParticleYieldRatioCorrelations.C
@@ -1,5 +1,5 @@
 AliAnalysisTaskParticleYieldRatioCorrelations *AddTaskParticleYieldRatioCorrelations(
-    TString name = "name", int iTask = 0, TString InputFileName = "")
+    TString name = "name", int iTask = 0, TString InputFileName = "", const char* suffix = "")
 {
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
     if (!mgr)
@@ -13,6 +13,10 @@ AliAnalysisTaskParticleYieldRatioCorrelations *AddTaskParticleYieldRatioCorrelat
     TString fileName = AliAnalysisManager::GetCommonFileName();
     fileName += ":output_";
     fileName += name;
+    TString SuffixAdd;
+    SuffixAdd.Form("MyTask%s", suffix);
+    fileName += "_";
+    fileName += SuffixAdd;
     AliAnalysisTaskParticleYieldRatioCorrelations *task = new AliAnalysisTaskParticleYieldRatioCorrelations(name.Data());
     if (!task)
         return 0x0;
