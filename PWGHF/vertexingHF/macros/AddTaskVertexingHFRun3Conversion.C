519eee537746b21eeb254abb23984a2dae26ad12
Francesco Prino <prino@to.infn.it>
2022-01-24 09:59:36 +0100

    Fix indendations

--- PWGHF/vertexingHF/macros/AddTaskVertexingHFRun3Conversion.C
+++ PWGHF/vertexingHF/macros/AddTaskVertexingHFRun3Conversion.C
@@ -36,23 +36,23 @@ AliAnalysisTaskSEVertexingHFRun3Conversion *AddTaskVertexingHFRun3Conversion(TSt
     if(collSyst=="PbPb" || collSyst=="XeXe"){
       configPWG3d2h="$ALICE_PHYSICS/PWGHF/vertexingHF/ConfigVertexingHF_Pb_AllCent_NoLS_PIDLc_PtDepSel_LooseIP.C";
       if(runnumber<0){
-	::Error("AddTaskVertexingHF","LPMRunNumber (needed for Pb-Pb or Xe-Xe) not available and custom config not passed in the arguments");
-	return NULL;
+        ::Error("AddTaskVertexingHF","LPMRunNumber (needed for Pb-Pb or Xe-Xe) not available and custom config not passed in the arguments");
+        return NULL;
       }
       if(prodType.IsNull()){
-	::Error("AddTaskVertexingHF","LPMProductionType (needed for Pb-Pb or Xe-Xe) not available and custom config not passed in the arguments");
-	return NULL;
+        ::Error("AddTaskVertexingHF","LPMProductionType (needed for Pb-Pb or Xe-Xe) not available and custom config not passed in the arguments");
+        return NULL;
       }
       if(runnumber>=295424)configPWG3d2h="$ALICE_PHYSICS/PWGHF/vertexingHF/ConfigVertexingHF_Pb_AllCent_NoLS_PIDLc_PtDepSel_LcMinpt1_DsMinPt15_2018opt.C";
       if (prodType=="MC"){
-	TString prodTag="";
-	if(gSystem->Getenv("LPMProductionTag")) prodTag=gSystem->Getenv("LPMProductionTag");
-	if(prodTag.IsNull()){
-	  ::Error("AddTaskVertexingHF","LPMProductionTag (needed for MCs of Pb-Pb or Xe-Xe) not available and custom config not passed in the arguments");
-	  return NULL;
-	}
-	if(prodTag=="LHC20g2a_2" || prodTag=="LHC20g11a2" || prodTag=="LHC20j5a1" || prodTag=="LHC20j5a2" || prodTag=="LHC20k3a" || prodTag=="LHC20l5a") configPWG3d2h="$ALICE_PHYSICS/PWGHF/vertexingHF/ConfigVertexingHF_Pb_AllCent_NoLS_PIDLc_PtDepSel_LcMinpt1_DsMinPt15_2018opt_Central.C";
-	else if(prodTag=="LHC20g2b_2" || prodTag=="LHC20g11b2" || prodTag=="LHC20j5b1" || prodTag=="LHC20j5b2" || prodTag=="LHC20k3b" || prodTag=="LHC20l5b") configPWG3d2h="$ALICE_PHYSICS/PWGHF/vertexingHF/ConfigVertexingHF_Pb_AllCent_NoLS_PIDLc_PtDepSel_LcMinpt1_DsMinPt15_2018opt_SemiCentral.C";
+        TString prodTag="";
+        if(gSystem->Getenv("LPMProductionTag")) prodTag=gSystem->Getenv("LPMProductionTag");
+        if(prodTag.IsNull()){
+          ::Error("AddTaskVertexingHF","LPMProductionTag (needed for MCs of Pb-Pb or Xe-Xe) not available and custom config not passed in the arguments");
+          return NULL;
+        }
+        if(prodTag=="LHC20g2a_2" || prodTag=="LHC20g11a2" || prodTag=="LHC20j5a1" || prodTag=="LHC20j5a2" || prodTag=="LHC20k3a" || prodTag=="LHC20l5a") configPWG3d2h="$ALICE_PHYSICS/PWGHF/vertexingHF/ConfigVertexingHF_Pb_AllCent_NoLS_PIDLc_PtDepSel_LcMinpt1_DsMinPt15_2018opt_Central.C";
+        else if(prodTag=="LHC20g2b_2" || prodTag=="LHC20g11b2" || prodTag=="LHC20j5b1" || prodTag=="LHC20j5b2" || prodTag=="LHC20k3b" || prodTag=="LHC20l5b") configPWG3d2h="$ALICE_PHYSICS/PWGHF/vertexingHF/ConfigVertexingHF_Pb_AllCent_NoLS_PIDLc_PtDepSel_LcMinpt1_DsMinPt15_2018opt_SemiCentral.C";
       }
     }
     Printf("HF config file that will be used (and copied to ConfigVertexingHF.C in the local directory) is: %s", configPWG3d2h.Data());
@@ -75,28 +75,28 @@ AliAnalysisTaskSEVertexingHFRun3Conversion *AddTaskVertexingHFRun3Conversion(TSt
   AliAnalysisDataContainer *coutputListOfCuts = mgr->CreateContainer("ListOfCuts",TList::Class(),AliAnalysisManager::kOutputContainer,outputfile.Data()); //cuts
   
   AliAnalysisDataContainer *coutputD0 = mgr->CreateContainer("D0CandidateTree",
-							     TTree::Class(),
-							     AliAnalysisManager::kOutputContainer,
-							     outputfile.Data());
+                                                             TTree::Class(),
+                                                             AliAnalysisManager::kOutputContainer,
+                                                             outputfile.Data());
   coutputD0->SetSpecialOutput();
   
   AliAnalysisDataContainer *coutput3p = mgr->CreateContainer("Charm3pCandidateTree",
-							     TTree::Class(),
-							     AliAnalysisManager::kOutputContainer,
-							     outputfile.Data());
+                                                             TTree::Class(),
+                                                             AliAnalysisManager::kOutputContainer,
+                                                             outputfile.Data());
   
   coutput3p->SetSpecialOutput();
   
   AliAnalysisDataContainer *coutputDst = mgr->CreateContainer("DstarCandidateTree",
-							      TTree::Class(),
-							      AliAnalysisManager::kOutputContainer,
-							      outputfile.Data());
+                                                              TTree::Class(),
+                                                              AliAnalysisManager::kOutputContainer,
+                                                              outputfile.Data());
   coutputDst->SetSpecialOutput();
 
   AliAnalysisDataContainer *coutputCasc = mgr->CreateContainer("LcV0bachCandidateTree",
-							       TTree::Class(),
-							       AliAnalysisManager::kOutputContainer,
-							       outputfile.Data());
+                                                               TTree::Class(),
+                                                               AliAnalysisManager::kOutputContainer,
+                                                               outputfile.Data());
   coutputCasc->SetSpecialOutput();
 
 
