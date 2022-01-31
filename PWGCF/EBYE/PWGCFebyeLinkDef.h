aa14eced3d275b402daf7854983b816723297cf2
rihanphys@gmail.com <rihanphys@gmail.com>
2022-01-31 17:38:01 +0100

    Added SignedBF with MC (on the fly)

--- PWGCF/EBYE/PWGCFebyeLinkDef.h
+++ PWGCF/EBYE/PWGCFebyeLinkDef.h
@@ -13,6 +13,7 @@
 #pragma link C++ class AliAnalysisTaskBF+;
 #pragma link C++ class AliAnalysisTaskBFPsi+;
 #pragma link C++ class AliAnalysisTaskSignedBF+;
+#pragma link C++ class AliAnalysisTaskSignedBFMC+;
 #pragma link C++ class AliAnalysisTaskEventMixingBF+;
 #pragma link C++ class AliAnalysisTaskTriggeredBF+;
 #pragma link C++ class AliAnalysisTaskEfficiencyBF+;
