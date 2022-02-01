98d426601a0948f1ba94027e41dfaad83524af25
grundd <david.grund@cern.ch>
2022-02-01 12:36:08 +0100

    Analysis macro CentralJpsi updated

--- PWGUD/UPC/AddTaskCentralJpsi_DG.C
+++ PWGUD/UPC/AddTaskCentralJpsi_DG.C
@@ -4,26 +4,27 @@ AliAnalysisTaskCentralJpsi_DG *AddTaskCentralJpsi_DG()
     // get the current analysis manager
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
     if (!mgr) {
+        Error("AddTaskCentralJpsi_DG", "No analysis manager found.");
         return 0;
     }
     // check the analysis type using the event handlers connected to the analysis manager
     if (!mgr->GetInputEventHandler()) {
+        Error("AddTaskCentralJpsi_DG", "This task requires an input event handler");
         return 0;
     }
-    // get the name of the file that is created
-    TString fileName = AliAnalysisManager::GetCommonFileName();
-    // create a subfolder in the file
-    fileName += ":AnalysisOutput";      
-    // now we create an instance of your task
-    AliAnalysisTaskCentralJpsi_DG* task = new AliAnalysisTaskCentralJpsi_DG(name.Data());   
-    if(!task) return 0;
-    // if MC
     Bool_t isMC = kFALSE;
     if(mgr->GetMCtruthEventHandler()) isMC = kTRUE;
+
+    // create task
+    AliAnalysisTaskCentralJpsi_DG* task = new AliAnalysisTaskCentralJpsi_DG(name.Data());
     task->SetIsMC(isMC);
     // add task to the manager
     mgr->AddTask(task);
-    // connect the manager to the task
+
+    // get the name of the file that is created and create subfolder
+    TString fileName = AliAnalysisManager::GetCommonFileName();
+    fileName += ":AnalysisOutput";      
+
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
     // same for the output
     mgr->ConnectOutput(task,1,mgr->CreateContainer("fTreeJpsi", TTree::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
