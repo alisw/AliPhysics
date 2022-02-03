5dbcc7f8e0ef2e2c4548ceb3cd1482812ca99053
Fabio Colamaria <fabiocolamaria@cern.ch>
2022-02-03 10:08:12 +0100

    Compiling new classes only with ROOT6

--- PWGHF/correlationHF/PWGHFcorrelationHFLinkDef.h
+++ PWGHF/correlationHF/PWGHFcorrelationHFLinkDef.h
@@ -38,6 +38,8 @@
 #pragma link C++ class AliHFCorrFitSystematics+;
 #pragma link C++ class AliDhCorrelationExtraction+;
 #pragma link C++ class AliAnalysisHFEHCorrOnFlySim+;
+
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,0,0)
 #pragma link C++ class AliHFMLResponseLambdactopKpi+;
 #pragma link C++ class AliAnalysisTaskSELambdacCorrelations+;
 
