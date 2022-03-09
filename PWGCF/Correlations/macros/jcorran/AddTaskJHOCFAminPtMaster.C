fa8154f74195ee7dc88b64b982adfe18d5192a61
Cindy <cindy.mordasini@cern.ch>
2022-03-09 12:51:04 +0100

    (PWG-CF) Add support for corrected NUA maps, and additional trials

--- PWGCF/Correlations/macros/jcorran/AddTaskJHOCFAminPtMaster.C
+++ PWGCF/Correlations/macros/jcorran/AddTaskJHOCFAminPtMaster.C
@@ -15,29 +15,59 @@ AliAnalysisTask *AddTaskJHOCFAminPtMaster(TString taskName = "JHOCFAminPtMaster_
 
   TString configName;   // Configuration name corresponding to the cutConfig.
   switch(cutConfig) { // Hardcoded names to prevent typo in phi weights files.
-  case 0 :  // Default: global.
-    configName = "global";
+  case 0 :    // Default selection.     // V0M + |zVtx < 8| + (pileup > 15000)
+    configName = "default";   // + global tracks 96 + (NTPC < 70) + (chi2 in [0.1, 4]).
     break;
-  case 1 :  // Syst: hybrid.
+  case 1 :    // Syst: global changed to hybrid.
     configName = "hybrid";
     break;
-  case 2 :  // Syst: nqq. TBI
-    configName = "nqq";
-    break;
-  case 3 :  // Syst: pileup.
-    configName = "pileup";
-    break;
-  case 4 :  // Syst: SPD.
+  case 2 :    // Syst: V0M changed to SPD clusters.
     configName = "SPD";
     break;
-  case 5 :  // Syst: subA. TBI
-    configName = "subA";
+  case 3 :    // Syst: (pileup > 15000) changed to (no pileup cut).
+    configName = "noPileup";
     break;
-  case 6 :  // Syst: ztx < 10.
-    configName = "zvtx";
+  case 4 :    // Syst: (pileup > 15000) changed to (pileup > 10000).
+    configName = "pileup10";
     break;
-  case 7 :  // Syst: pqq. TBI.
+  case 5 :    // Syst: |zVtx < 8| changed to |zVtx < 10|.
+    configName = "zvtx10";
+    break;
+  case 6 :    // Syst: |zVtx < 8| changed to |zVtx < 9|.
+    configName = "zvtx9";
+    break;
+  case 7 :    // Syst: |zVtx < 8| changed to |zVtx < 7|.
+    configName = "zvtx7";
+    break;
+  case 8 :    // Syst: (NTPC > 70) changed to (NTPC > 80).
+    configName = "NTPC80";
+    break;
+  case 9 :    // Syst: (NTPC > 70) changed to (NTPC > 90).
+    configName = "NTPC90";
+    break;
+  case 10 :    // Syst: (NTPC > 70) changed to (NTPC > 100).
+    configName = "NTPC100";
+    break;
+  case 11 :    // Syst: (chi2 in [0.1, 4]) changed to (chi2 < 4).
+    configName = "chi2def";
+    break;
+  case 12 :    // Syst: (chi2 in [0.1, 4]) changed to (chi2 < 2.5).
+    configName = "chi2tight";
+    break;
+  case 13 :     // Syst: (DCAz < 2cm - default in global) changed to (DCAz < 1cm).
+    configName = "DCAz1";
+    break;
+  case 14 :     // Syst: (DCAz < 2cm - default in global) changed to (DCAz < 0.5cm).
+    configName = "DCAz05";
+    break;
+  case 15 :     // Syst: (all charges) changed to (negative charges only).
+    configName = "nqq";
+    break;
+  case 16 :     // Syst: (all charges) changed to (positive charges only).
     configName = "pqq";
+    break;
+  case 17 :     // Syst: subA. TBI
+    configName = "subA";
     break;
   default :
     std::cout << "ERROR: Invalid configuration index." << std::endl;
@@ -81,9 +111,14 @@ AliAnalysisTask *AddTaskJHOCFAminPtMaster(TString taskName = "JHOCFAminPtMaster_
     MAPfilenames = Form("%sPhiWeights_LHC%s_Error_pt02_s_%s.root",
       MAPdirname.Data(), sCorrection[period].Data(), configName.Data());
     break;
-  case 1:   // 1; Fine binning, minPt = 0.2 for all.
-    MAPfilenames = Form("%sPhiWeights_LHC%s_Error_finerBins_pt02_s_%s.root",
-      MAPdirname.Data(), sCorrection[period].Data(), configName.Data());
+  case 1:   // 1; Fine binning, minPt = 0.2 for all. --> Default.
+    if (cutConfig == 0) {
+      MAPfilenames = Form("%sPhiWeights_LHC%s_Error_finerBins_Default_s_%s.root",
+        MAPdirname.Data(), sCorrection[period].Data(), configName.Data());
+    } else {
+      MAPfilenames = Form("%sPhiWeights_LHC%s_Error_finerBins_s_%s.root",
+        MAPdirname.Data(), sCorrection[period].Data(), configName.Data());
+    }
     break;
   default:
     std::cout << "ERROR: Invalid configuration index. Skipping this element."
@@ -112,14 +147,7 @@ AliAnalysisTask *AddTaskJHOCFAminPtMaster(TString taskName = "JHOCFAminPtMaster_
     std::cout << "Setting the catalyst: " << fJCatalyst[i]->GetJCatalystTaskName() << std::endl;
     fJCatalyst[i]->SetSaveAllQA(saveQA);
 
-  // Set the correct flags to use.
-    if (strcmp(configName.Data(), "pileup") != 0) {
-      fJCatalyst[i]->AddFlags(AliJCatalystTask::FLUC_CUT_OUTLIERS);
-      fJCatalyst[i]->SetESDpileupCuts(ESDpileup, slope, intercept, saveQApileup);
-    }
-    if (period == lhc18q || period == lhc18r) {fJCatalyst[i]->AddFlags(AliJCatalystTask::FLUC_CENT_FLATTENING);}
-
-  // Set the trigger and centrality selection.
+    /// Trigger and centrality selection.
     fJCatalyst[i]->SelectCollisionCandidates(selEvt);
     fJCatalyst[i]->SetCentrality(0.,5.,10.,20.,30.,40.,50.,60.,70.,80.,-10.,-10.,-10.,-10.,-10.,-10.,-10.);
     fJCatalyst[i]->SetInitializeCentralityArray();
@@ -129,17 +157,55 @@ AliAnalysisTask *AddTaskJHOCFAminPtMaster(TString taskName = "JHOCFAminPtMaster_
       fJCatalyst[i]->SetCentDetName("V0M");
     }
 
-  // Set the filtering and kinematic cuts.
-    if (strcmp(configName.Data(), "global") == 0) {
-      fJCatalyst[i]->SetTestFilterBit(globalCut);
-    } else {  // Default: Hybrid tracks.
-      fJCatalyst[i]->SetTestFilterBit(hybridCut);
+    /// Event selection: pileup cuts and Zvtx.
+    if (strcmp(configName.Data(), "noPileup") != 0) {   // Set flag only if we cut on pileup.
+      fJCatalyst[i]->AddFlags(AliJCatalystTask::FLUC_CUT_OUTLIERS);
+      if (strcmp(configName.Data(), "pileup10") == 0) {fJCatalyst[i]->SetESDpileupCuts(true, slope, 10000, saveQApileup);}
+      else {fJCatalyst[i]->SetESDpileupCuts(ESDpileup, slope, intercept, saveQApileup);}
     }
-  
-    if (strcmp(configName.Data(), "zvtx") == 0) {    
+    if (period == lhc18q || period == lhc18r) {fJCatalyst[i]->AddFlags(AliJCatalystTask::FLUC_CENT_FLATTENING);}    
+
+    if (strcmp(configName.Data(), "zvtx10") == 0) {    
       fJCatalyst[i]->SetZVertexCut(10.0);
+    } else if (strcmp(configName.Data(), "zvtx9") == 0) {
+      fJCatalyst[i]->SetZVertexCut(9.0);
+    } else if (strcmp(configName.Data(), "zvtx7") == 0) {
+      fJCatalyst[i]->SetZVertexCut(7.0);
     } else {  // Default value for JCorran analyses in Run 2.
       fJCatalyst[i]->SetZVertexCut(8.0);
+    }
+
+    /// Filtering, kinematic and detector cuts.
+    if (strcmp(configName.Data(), "hybrid") == 0) {
+      fJCatalyst[i]->SetTestFilterBit(hybridCut);
+    } else {  // Default: global tracks.
+      fJCatalyst[i]->SetTestFilterBit(globalCut);
+    }
+
+    if (strcmp(configName.Data(), "NTPC80") == 0) {    
+      fJCatalyst[i]->SetNumTPCClusters(80);
+    } else if (strcmp(configName.Data(), "NTPC90") == 0) {
+      fJCatalyst[i]->SetNumTPCClusters(90);
+    } else if (strcmp(configName.Data(), "NTPC100") == 0) {
+      fJCatalyst[i]->SetNumTPCClusters(100);
+    } else {  // Default value for JCorran analyses in Run 2.
+      fJCatalyst[i]->SetNumTPCClusters(70);
+    }
+
+    if (strcmp(configName.Data(), "chi2def") == 0) {    
+      fJCatalyst[i]->SetChi2Cuts(0.0, 4.0);
+    } else if (strcmp(configName.Data(), "chi2tight") == 0) {
+      fJCatalyst[i]->SetChi2Cuts(0.0, 2.5);
+    } else {  // Default value for JCorran analyses in Run 2.
+      fJCatalyst[i]->SetChi2Cuts(0.1, 4.0);
+    }
+
+    if (strcmp(configName.Data(), "DCAz1") == 0) {    
+      fJCatalyst[i]->SetDCAzCut(1.0);
+    } else if (strcmp(configName.Data(), "DCAz05") == 0) {
+      fJCatalyst[i]->SetDCAzCut(0.5);
+    } else {  // Default value for JCorran analyses in Run 2.
+      fJCatalyst[i]->SetDCAzCut(2.0);
     }
 
     if (strcmp(configName.Data(), "nqq") == 0) {
@@ -147,6 +213,8 @@ AliAnalysisTask *AddTaskJHOCFAminPtMaster(TString taskName = "JHOCFAminPtMaster_
     } else if (strcmp(configName.Data(), "pqq") == 0) {
       fJCatalyst[i]->SetParticleCharge(1);
     }   // Default: charge = 0 to accept all charges.
+
+    // TBA: subA systematics.
 
     fJCatalyst[i]->SetPtRange(configMinPt[i], ptMax);
     fJCatalyst[i]->SetEtaRange(-0.8, 0.8);
