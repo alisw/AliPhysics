8db32cd88cab9d4d77401e09b2e4b81c6b09a4bf
Markus Fasel <markus.fasel@cern.ch>
2022-03-21 18:34:15 +0100

    Add option to cut separately on charged and neutral particle pt
    Cut implemented in
    AliMCParticleContainer as it is relevant
    only in case of simulation. The title string is only modified in case a cut
    is set, in order to keep backward comp
    for existing tasks.

--- PWG/EMCAL/EMCALbase/AliMCParticleContainer.h
+++ PWG/EMCAL/EMCALbase/AliMCParticleContainer.h
@@ -224,6 +224,18 @@ class AliMCParticleContainer : public AliParticleContainer {
   void                        SetMCFlag(UInt_t m)                             { fMCFlag          = m ; }
 
   /**
+   * @brief Set min. pt cut applied on charged particles
+   * @param minpt Min. pt (in GeV/c)
+   */
+  void                        SetMinPtCharged(Double_t minpt)                 { fMinPtCharged = minpt; }
+
+  /**
+   * @brief Set min. pt cut applied on neutral particles
+   * @param minpt Min. pt (in GeV/c)
+   */
+  void                        SetMinPtNeutral(Double_t minpt)                 { fMinPtNeutral = minpt; }
+
+  /**
    * @brief Require particle to be a physical primary particle
    * @param s If true only physical primary particles are selected 
    */
@@ -274,13 +286,15 @@ class AliMCParticleContainer : public AliParticleContainer {
   virtual TString             GetDefaultArrayName(const AliVEvent * const ev) const { return "mcparticles"; }
 
   UInt_t                      fMCFlag;                        ///< select MC particles with flags
+  Double_t                    fMinPtCharged;                  ///< min. charged particle pt
+  Double_t                    fMinPtNeutral;                  ///< min. neutral particle pt
 
  private:
   AliMCParticleContainer(const AliMCParticleContainer& obj); // copy constructor
   AliMCParticleContainer& operator=(const AliMCParticleContainer& other); // assignment
 
   /// \cond CLASSIMP
-  ClassDef(AliMCParticleContainer,1);
+  ClassDef(AliMCParticleContainer,2);
   /// \endcond
 };
 
