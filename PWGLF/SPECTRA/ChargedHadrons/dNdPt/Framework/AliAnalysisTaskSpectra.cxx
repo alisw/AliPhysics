6028605bc3ae7811deb83f11cf573557bdad35c2
Patrick Huhn <phuhn@cern.ch>
2022-03-29 23:57:17 +0200

    charged particles: changed PCC from TParticle to MC label

--- PWGLF/SPECTRA/ChargedHadrons/dNdPt/Framework/AliAnalysisTaskSpectra.cxx
+++ PWGLF/SPECTRA/ChargedHadrons/dNdPt/Framework/AliAnalysisTaskSpectra.cxx
@@ -28,26 +28,26 @@ ClassImp(AliAnalysisTaskSpectra)
 /// \endcond
 //_____________________________________________________________________________
 
-AliAnalysisTaskSpectra::AliAnalysisTaskSpectra()
+AliAnalysisTaskSpectra::AliAnalysisTaskSpectra() 
     : AliAnalysisTaskMKBase()
     , fHistEffCont(0)
-    , fHistTrack(0)
+    , fHistTrack(0)   
     , fHistEvent(0)
 {
     // default contructor
-    AliAnalysisTaskMKBase::fNeedEventVertex = kTRUE;
+ 
 }
 
 //_____________________________________________________________________________
 
-AliAnalysisTaskSpectra::AliAnalysisTaskSpectra(const char* name)
+AliAnalysisTaskSpectra::AliAnalysisTaskSpectra(const char* name) 
     : AliAnalysisTaskMKBase(name)
     , fHistEffCont(0)
     , fHistTrack(0)
     , fHistEvent(0)
 {
     // constructor
-    AliAnalysisTaskMKBase::fNeedEventVertex = kTRUE;
+
 }
 
 //_____________________________________________________________________________
@@ -60,29 +60,29 @@ AliAnalysisTaskSpectra::~AliAnalysisTaskSpectra()
 //_____________________________________________________________________________
 
 void AliAnalysisTaskSpectra::AddOutput()
-{
+{        
     AddAxis("cent");
     AddAxis("nAcc","mult6kcoarse");
     AddAxis("MCpT","pt");
-    AddAxis("MCQ",3,-1.5,1.5);
+    AddAxis("MCQ",3,-1.5,1.5);        
     AddAxis("MCpid",10,-0.5,9.5);  // 0=e, 1=mu, 2=pi, 3=K, 4=p, 6=sigmaP, 7=sigmaM, 8=xi, 9=omega, 5=other
-    AddAxis("MCinfo",4,-0.5,3.5);  // 0=prim, 1=decay 2=material, 3=genprim
+    AddAxis("MCinfo",4,-0.5,3.5);  // 0=prim, 1=decay 2=material, 3=genprim    
     fHistEffCont = CreateHist("fHistEffCont");
     fOutputList->Add(fHistEffCont);
-
+    
     AddAxis("cent");
-    AddAxis("nAcc","mult6kcoarse");
-    AddAxis("pt");
-    AddAxis("Q",3,-1.5,1.5);
+    AddAxis("nAcc","mult6kcoarse"); 
+    AddAxis("pt");         
+    AddAxis("Q",3,-1.5,1.5);        
     fHistTrack = CreateHist("fHistTrack");
     fOutputList->Add(fHistTrack);
-
-    AddAxis("cent");
+        
+    AddAxis("cent");    
     AddAxis("nAcc","mult6kfine");
     AddAxis("zV",8,-20,20);
     fHistEvent = CreateHist("fHistEvent");
-    fOutputList->Add(fHistEvent);
-
+    fOutputList->Add(fHistEvent);    
+    
 }
 
 //_____________________________________________________________________________
@@ -96,12 +96,12 @@ Bool_t AliAnalysisTaskSpectra::IsEventSelected()
 
 void AliAnalysisTaskSpectra::AnaEvent()
 {
-
+   
    LoopOverAllTracks();
    if (fIsMC) LoopOverAllParticles();
-
+   
    FillHist(fHistEvent, fMultPercentileV0M, fNTracksAcc, fZv);
-
+   
 }
 
 //_____________________________________________________________________________
@@ -109,7 +109,7 @@ void AliAnalysisTaskSpectra::AnaEvent()
 void AliAnalysisTaskSpectra::AnaTrack(Int_t flag)
 {
     if (!fAcceptTrackM) return;
-
+    
     FillHist(fHistTrack, fMultPercentileV0M, fNTracksAcc, fPt, fChargeSign);
 }
 
@@ -117,36 +117,33 @@ void AliAnalysisTaskSpectra::AnaTrack(Int_t flag)
 
 void AliAnalysisTaskSpectra::AnaTrackMC(Int_t flag)
 {
-    if(fMCPileUpTrack) return; // reject pileup tracks
     if (!fAcceptTrackM) return;
-
+     
     if (fMCParticleType==AlidNdPtTools::kOther) { Log("RecTrack.PDG.",fMCPDGCode); }
     if (TMath::Abs(fMCQ > 1)) { Log("RecTrack.Q>1.PDG.",fMCPDGCode); }
-
-    FillHist(fHistEffCont, fMultPercentileV0M, fNTracksAcc, fMCPt, fMCChargeSign, fMCParticleType, fMCProdcutionType);
+    
+    FillHist(fHistEffCont, fMultPercentileV0M, fNTracksAcc, fMCPt, fMCChargeSign, fMCParticleType, fMCProdcutionType); 
 
 }
 
 //_____________________________________________________________________________
 
 void AliAnalysisTaskSpectra::AnaParticleMC(Int_t flag)
-{
-    if(fMCPileUpTrack) return; // reject pileup tracks
-
-    if (!fMCisPrim) return;
-    if (!fMCIsCharged) return;
-    if (TMath::Abs(fMCEta) > 0.8) return;
-
+{            
+    if (!fMCisPrim) return;    
+    if (!fMCIsCharged) return;    
+    if (TMath::Abs(fMCEta) > 0.8) return;    
+    
     if (fMCParticleType==AlidNdPtTools::kOther) { Log("GenPrim.PDG.",fMCPDGCode); }
     if (TMath::Abs(fMCQ > 1)) { Log("GenPrim.Q>1.PDG.",fMCPDGCode); }
-
-    FillHist(fHistEffCont, fMultPercentileV0M, fNTracksAcc, fMCPt, fMCChargeSign, fMCParticleType, 3);
+    
+    FillHist(fHistEffCont, fMultPercentileV0M, fNTracksAcc, fMCPt, fMCChargeSign, fMCParticleType, 3);     
 
 }
 
 //_____________________________________________________________________________
 
-AliAnalysisTaskSpectra* AliAnalysisTaskSpectra::AddTaskSpectra(const char* name, const char* outfile)
+AliAnalysisTaskSpectra* AliAnalysisTaskSpectra::AddTaskSpectra(const char* name, const char* outfile) 
 {
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
     if (!mgr) {
@@ -160,25 +157,25 @@ AliAnalysisTaskSpectra* AliAnalysisTaskSpectra::AddTaskSpectra(const char* name,
         ::Error("AddTaskSpectra", "This task requires an input event handler");
         return NULL;
     }
-
+    
     // Setup output file
     //===========================================================================
-    TString fileName = AliAnalysisManager::GetCommonFileName();
+    TString fileName = AliAnalysisManager::GetCommonFileName();        
     fileName += ":";
     fileName += name;  // create a subfolder in the file
     if (outfile) { // if a finename is given, use that one
-        fileName = TString(outfile);
+        fileName = TString(outfile);        
     }
-
+    
 
     // create the task
     //===========================================================================
-    AliAnalysisTaskSpectra *task = new AliAnalysisTaskSpectra(name);
+    AliAnalysisTaskSpectra *task = new AliAnalysisTaskSpectra(name);  
     if (!task) { return 0; }
-
+    
     // configure the task
     //===========================================================================
-    task->SelectCollisionCandidates(AliVEvent::kAnyINT);
+    task->SelectCollisionCandidates(AliVEvent::kAnyINT);    
     task->SetESDtrackCutsM(AlidNdPtTools::CreateESDtrackCuts("defaultEta08"));
 //     task->SetESDtrackCuts(0,AlidNdPtTools::CreateESDtrackCuts("defaultEta08"));
     task->SetNeedEventMult(kTRUE);
@@ -189,6 +186,6 @@ AliAnalysisTaskSpectra* AliAnalysisTaskSpectra::AddTaskSpectra(const char* name,
     mgr->AddTask(task);
     mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
     mgr->ConnectOutput(task,1,mgr->CreateContainer(name, TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
-
+    
   return task;
 }
