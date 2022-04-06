8a4d066b5102bfc638df0d5451bdbd2f9e007590
jikim1290 <jikim1290@gmail.com>
2022-04-06 17:38:42 +0900

    updates

--- PWGLF/RESONANCES/PostProcessing/f0980/AliAnalysisTaskInclusivef0f2.h
+++ PWGLF/RESONANCES/PostProcessing/f0980/AliAnalysisTaskInclusivef0f2.h
@@ -14,7 +14,8 @@
 #include "AliPID.h"
 #include "AliStack.h"
 #include "AliEventCuts.h"
-
+#include "AliAnalysisUtils.h"
+#include "AliQnCorrectionsFillEventTask.h"
 
 class AliMultSelection;
 class AliVMultiplicity;
@@ -113,6 +114,7 @@ class AliAnalysisTaskInclusivef0f2 : public AliAnalysisTaskSE{
         TAxis                           binPtGen; //!
 	TAxis                           binType; //!
         TAxis                           binMass; //!
+	TAxis				binEP; //!
 	TAxis				binPtTrack;
 	TAxis				binPhiTrack;
 	TAxis				binEtaTrack;
@@ -133,10 +135,8 @@ class AliAnalysisTaskInclusivef0f2 : public AliAnalysisTaskSE{
         Int_t                           zbin = -1 ;
 	Int_t				trkbin = -1;
 	Int_t				pidbin = -1;
+	Double_t			fEP_v0 = -999.;
 
-	std::vector<Double_t>		EffpT;
-	Int_t fEff_npT_step = 140;
-	Double_t fEff_pT_max = 14.0;
 
         ClassDef(AliAnalysisTaskInclusivef0f2, 1);
 };
