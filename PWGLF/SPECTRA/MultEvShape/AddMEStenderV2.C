636a6f9df02972ed5b7106e7d6318f840aa48ec4
Amelia Lindner <53506792+alindner14@users.noreply.github.com>
2022-04-06 17:38:58 +0300

    Revert "reconfigured task"

--- PWGLF/SPECTRA/MultEvShape/AddMEStenderV2.C
+++ PWGLF/SPECTRA/MultEvShape/AddMEStenderV2.C
@@ -1,4 +1,4 @@
-#if !defined(__CINT__) || defined(__MAKECINT__)
+#if ! defined (__CINT__) || defined (__MAKECINT__)
 #include <TTree.h>
 #include <TError.h>
 #include <AliLog.h>
@@ -13,48 +13,50 @@ AliMEStenderV2 *AddMEStenderV2(Bool_t mc, Int_t configuration = 1)
 {
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
 
-  AliMEStenderV2 *tender = new AliMEStenderV2((char *)"MEStenderV2");
+  AliMEStenderV2 *tender = new AliMEStenderV2((char*)"MEStenderV2");
   mgr->AddTask(tender);
 
   // task set-up
   tender->SetMCdata(mc);
-  switch (configuration)
-  {
-  case 0:
-    tender->ConfigTask(AliMEStenderV2::AliMESconfigTender::k7TeV,                        // event cuts
-                       AliMEStenderV2::AliMESconfigTender::kStandardITSTPCTrackCuts2010, // track cuts
-                       AliMEStenderV2::AliMESconfigTender::kIterative);                  // PID priors
-    break;
-  case 1:
-    tender->ConfigTask(AliMEStenderV2::AliMESconfigTender::k13TeV,                       // event cuts
-                       AliMEStenderV2::AliMESconfigTender::kStandardITSTPCTrackCuts2011, // track cuts
-                       AliMEStenderV2::AliMESconfigTender::kNoPP);                       // PID priors
-    break;
-  default:
-    printf("Configuration not defined\n");
-    break;
-  }
-  tender->SetPriors(); // always call this after ConfigTask !!
+  tender->SetDebugLevel(1);
+	switch (configuration) {
+		case 0:
+	 		tender->ConfigTask( AliMEStenderV2::AliMESconfigTender::k7TeV,      // event cuts
+					  AliMEStenderV2::AliMESconfigTender::kStandardITSTPCTrackCuts2010, // track cuts
+					  AliMEStenderV2::AliMESconfigTender::kIterative);                  // PID priors
+  		break;
+		case 1:
+			tender->ConfigTask( AliMEStenderV2::AliMESconfigTender::k13TeV,       // event cuts
+					AliMEStenderV2::AliMESconfigTender::kStandardITSTPCTrackCuts2011, // track cuts
+					AliMEStenderV2::AliMESconfigTender::kNoPP);                  // PID priors
+			break;
+		default: printf("Configuration not defined\n");
+			break;
+	}
+	tender->SetPriors();  // always call this after ConfigTask !!
 
   // connect input
-  mgr->ConnectInput(tender, 0, mgr->GetCommonInputContainer());
+  mgr->ConnectInput (tender, 0, mgr->GetCommonInputContainer());
 
   // create output containers
   AliAnalysisDataContainer *co[AliMEStenderV2::kNcontainers] = {NULL};
-  co[0] = mgr->CreateContainer("tenderQA", TList::Class(), AliAnalysisManager::kOutputContainer, Form("%s:MES", mgr->GetCommonFileName()));
+  co[0]                          = mgr->CreateContainer("tenderQA", TList::Class(), AliAnalysisManager::kOutputContainer, Form("%s:MES", mgr->GetCommonFileName()));
   co[AliMEStenderV2::kEventInfo] = mgr->CreateContainer("MESeventInfo", AliMESeventInfo::Class(), AliAnalysisManager::kExchangeContainer);
-  co[AliMEStenderV2::kTracks] = mgr->CreateContainer("MEStracks", TObjArray::Class(), AliAnalysisManager::kExchangeContainer);
-  co[AliMEStenderV2::kTree] = mgr->CreateContainer("MESdata/rec", TTree::Class(), AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName());
-  if (mc)
-  {
-    co[AliMEStenderV2::kMCeventInfo] = mgr->CreateContainer("MESMCeventInfo", AliMESeventInfo::Class(), AliAnalysisManager::kExchangeContainer);
-    co[AliMEStenderV2::kMCtracks] = mgr->CreateContainer("MESMCtracks", TObjArray::Class(), AliAnalysisManager::kExchangeContainer);
+  co[AliMEStenderV2::kTracks]    = mgr->CreateContainer("MEStracks", TObjArray::Class(), AliAnalysisManager::kExchangeContainer);
+  co[AliMEStenderV2::kTree] = mgr->CreateContainer("MEStree", TTree::Class(), AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName());
+  if(mc){
+	  co[AliMEStenderV2::kMCeventInfo] = mgr->CreateContainer("MESMCeventInfo", AliMESeventInfo::Class(), AliAnalysisManager::kExchangeContainer);
+	  co[AliMEStenderV2::kMCtracks]    = mgr->CreateContainer("MESMCtracks", TObjArray::Class(), AliAnalysisManager::kExchangeContainer);
+    co[AliMEStenderV2::kTreeMC] = mgr->CreateContainer("MESMCtree", TTree::Class(), AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName());
+    co[AliMEStenderV2::kTreeGen] = mgr->CreateContainer("MESGen", TTree::Class(), AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName());
+    co[AliMEStenderV2::kTreeMiss] = mgr->CreateContainer("MESMiss", TTree::Class(), AliAnalysisManager::kOutputContainer, mgr->GetCommonFileName());
   }
 
   // connect output
-  for (Int_t ios(0); ios < AliMEStenderV2::kNcontainers; ios++)
-    if (co[ios])
-      mgr->ConnectOutput(tender, ios + 1, co[ios]);
+  for(Int_t ios(0);ios<AliMEStenderV2::kNcontainers;ios++)
+	  if(co[ios]) mgr->ConnectOutput(tender, ios+1, co[ios]);
+
 
   return tender;
+
 }
