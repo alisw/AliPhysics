a5c385decf9e9d0044fc24815e73284abd0af17d
Hikari Murakami <26593188+hmurakam@users.noreply.github.com>
2022-04-26 18:33:02 +0900

    Fix for alien_cp

--- PWGDQ/dielectron/LMEE/AliAnalysisTaskLMeeCocktailMC.cxx
+++ PWGDQ/dielectron/LMEE/AliAnalysisTaskLMeeCocktailMC.cxx
@@ -1143,7 +1143,8 @@ void AliAnalysisTaskLMeeCocktailMC::SetEffFileName(TString name)
   // Get Efficiency
   if(fFileNameEff.Contains("alien")){
     // file is copied from alien path to local directory
-    gSystem->Exec(Form("alien_cp %s .", fFileNameEff.Data()));
+    //gSystem->Exec(Form("alien_cp %s .", fFileNameEff.Data()));
+    TFile::Cp(fFileNameEff, TString::Format("file:%s", gSystem->BaseName(fFileNameEff)));
 
     // obtain ROOT file name only and local directory
     TObjArray* Strings = fFileNameEff.Tokenize("/");
@@ -1178,7 +1179,8 @@ void AliAnalysisTaskLMeeCocktailMC::SetResFileName(TString name)
   if(fResolType == 2) {
     if(fResolDataSetName.Contains("alien")){
       // file is copied from alien path to local directory
-      gSystem->Exec(Form("alien_cp %s .", fResolDataSetName.Data()));
+      //gSystem->Exec(Form("alien_cp %s .", fResolDataSetName.Data()));
+      TFile::Cp(fResolDataSetName, TString::Format("file:%s", gSystem->BaseName(fResolDataSetName)));
 
       // obtain ROOT file name only and local directory
       TObjArray* Strings = fResolDataSetName.Tokenize("/");
