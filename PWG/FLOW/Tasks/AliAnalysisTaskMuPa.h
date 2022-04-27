190a667d4bc261eb1e8cc6771119ff637816d41c
abilandz <Ante.Bilandzic@cern.ch>
2022-04-27 13:32:20 +0200

    ClassDef

--- PWG/FLOW/Tasks/AliAnalysisTaskMuPa.h
+++ PWG/FLOW/Tasks/AliAnalysisTaskMuPa.h
@@ -808,7 +808,7 @@ class AliAnalysisTaskMuPa : public AliAnalysisTaskSE{
   Bool_t fPrintEventInfo;            // print event medatata (for AOD: fRun, fBunchCross, fOrbit, fPeriod). Enabled indirectly via task->PrintEventInfo()
  
   // Increase this counter in each new version:
-  ClassDef(AliAnalysisTaskMuPa,30);
+  ClassDef(AliAnalysisTaskMuPa,31);
 
 };
 
