36462a278ed82f41763dd7fb39dd51b5f1cea9b7
Wioleta Rzesa <wrzesa@cern.ch>
2022-05-12 09:57:20 +0200

    changes in my efficiecny task

--- PWGCF/FEMTOSCOPY/macros/AddTaskEfficiencyWRZ.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskEfficiencyWRZ.C
@@ -6,7 +6,7 @@
 //
 //=============================================================================
 
-AddTaskEfficiencyWRZ *AddTaskEfficiency(TString containerName="femtolist")
+AliAnalysisTaskParticleEffWRZ *AddTaskEfficiencyWRZ(TString containerName="femtolist")
 {
   // A. Get the pointer to the existing analysis manager via the static access method.
   //==============================================================================
