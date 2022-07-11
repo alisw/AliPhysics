0e160b0517094eb9dee13e869279f7fee1418433
abmodak <67369858+abmodak@users.noreply.github.com>
2022-07-11 13:34:03 +0530

    Update PWGMMMultLinkDef.h

--- PWGMM/Mult/PWGMMMultLinkDef.h
+++ PWGMM/Mult/PWGMMMultLinkDef.h
@@ -6,6 +6,7 @@
 #pragma link C++ class AliAnalysisPseudoRapidityDensity+;
 #pragma link C++ class AliPMDpPbAnalysisTaskData+;
 #pragma link C++ class AliPMDpPbAnalysisTaskSim+;
+#pragma link C++ class AliPMDppAnalysisTaskSim+;
 
 #endif /* __CINT__ */
 
