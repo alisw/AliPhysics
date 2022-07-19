83b5e0c3938a21134c378fc2c7d2d8f709974128
absonner <anna.onnerstad@cern.ch>
2022-07-19 15:22:39 +0300

    change zvtx trial to <9cm

--- PWGCF/Correlations/macros/jcorran/AddTaskJFFlucJCMAPsMaster.C
+++ PWGCF/Correlations/macros/jcorran/AddTaskJFFlucJCMAPsMaster.C
@@ -52,8 +52,8 @@ AliAnalysisTask *AddTaskJFFlucJCMAPsMaster(TString taskName = "JFFlucJCMAP_Run2_
     case 4 :    // Syst: (pileup > 15000) changed to (pileup > 10000).
       configNames.push_back("pileup10");
       break;
-    case 5 :    // Syst: |zVtx < 8| changed to |zVtx < 10|.
-      configNames.push_back("zvtx10");
+    case 5 :    // Syst: |zVtx < 8| changed to |zVtx < 9|.
+      configNames.push_back("zvtx9");
       break;
     case 6 :    // Syst: |zVtx < 8| changed to |zVtx < 6|.
       configNames.push_back("zvtx6");
@@ -164,8 +164,8 @@ AliAnalysisTask *AddTaskJFFlucJCMAPsMaster(TString taskName = "JFFlucJCMAP_Run2_
     }
     if (period == lhc18q || period == lhc18r) {fJCatalyst[i]->AddFlags(AliJCatalystTask::FLUC_CENT_FLATTENING);}    
 
-    if (strcmp(configNames[i].Data(), "zvtx10") == 0) {    
-      fJCatalyst[i]->SetZVertexCut(10.0);
+    if (strcmp(configNames[i].Data(), "zvtx9") == 0) {    
+      fJCatalyst[i]->SetZVertexCut(9.0);
     } else if (strcmp(configNames[i].Data(), "zvtx6") == 0) {
       fJCatalyst[i]->SetZVertexCut(6.0);
     } else if (strcmp(configNames[i].Data(), "zvtx7") == 0) {
