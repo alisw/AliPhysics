537f8c3212f66c7c8e612db501de15245ce8274e
Swati Saha <swatisaha067@gmail.com>
2022-07-21 18:38:33 +0530

    MC code for High statistics HIJING data

--- PWGCF/EBYE/HMPTF/macros/AddTaskResonanceVsMultiplicity_MCnoESD.C
+++ PWGCF/EBYE/HMPTF/macros/AddTaskResonanceVsMultiplicity_MCnoESD.C
@@ -1,5 +1,5 @@
 
-AliAnalysisTaskResonanceVsMultiplicityROOT6 *AddTaskResonanceVsMultiplicityROOT6(Int_t fCentralityMin=0, Int_t fCentralityMax=90,/* TString sTrigger="kINT7"*/ Double_t fVzMax=10, Double_t fdcaxy=0.1, Double_t fdcaz=1, Double_t fchi2tpc=2.5, Double_t fchi2its=36, Double_t fnCrossedRows=70, TString OutFileName = "_default", Double_t fEta=0.8)
+AliAnalysisTaskResonanceVsMultiplicity_MCnoESD *AddTaskResonanceVsMultiplicity_MCnoESD(Int_t fCentralityMin=0, Int_t fCentralityMax=90,/* TString sTrigger="kINT7"*/ Double_t fVzMax=10, Double_t fdcaxy=0.1, Double_t fdcaz=1, Double_t fchi2tpc=2.5, Double_t fchi2its=36, Double_t fnCrossedRows=70, TString OutFileName = "_default", Double_t fEta=0.8)
 {
   // standard with task
   printf("===================================================================================\n");
@@ -13,16 +13,15 @@ AliAnalysisTaskResonanceVsMultiplicityROOT6 *AddTaskResonanceVsMultiplicityROOT6
 
 
   TString list1OutName = outfileName;        // common outfile filename
-  list1OutName        += ":Results";         // This directory contains result histograms
+  list1OutName        += ":ResultsMC";         // This directory contains result histograms
 
   Int_t gCentMin = fCentralityMin;
   Int_t gCentMax = fCentralityMax;
 
   TString TaskMeanpt;
   TaskMeanpt.Form("gTaskMeanpt%d_%d_%s", gCentMin, gCentMax, " ");
-  //gROOT->LoadMacro("AliAnalysisTaskResonanceVsMultiplicity.cxx++g");                                                           
-  gInterpreter->ProcessLine(".x AliAnalysisTaskResonanceVsMultiplicityROOT6.cxx++g");
-  AliAnalysisTaskResonanceVsMultiplicityROOT6 *task_Mpt = new AliAnalysisTaskResonanceVsMultiplicityROOT6(TaskMeanpt);
+                                                
+  AliAnalysisTaskResonanceVsMultiplicity_MCnoESD *task_Mpt = new AliAnalysisTaskResonanceVsMultiplicity_MCnoESD(TaskMeanpt);
 
   /*
   ///-------> Analysis Object Created, now pass the arguments
@@ -50,22 +49,22 @@ AliAnalysisTaskResonanceVsMultiplicityROOT6 *AddTaskResonanceVsMultiplicityROOT6
   
   // ///Event cuts:
   
-  task_Mpt->SetVzRangeMax(10.0);
+  // task_Mpt->SetVzRangeMax(fVzMax);
        
 
   // //Track cuts:
-  task_Mpt->SetDCAXYRangeMax(fdcaxy);
-  task_Mpt->SetDCAZRangeMax(fdcaz);
-  task_Mpt->SetMaxChi2PerTPCClusterRange(fchi2tpc);
-  task_Mpt->SetMaxChi2PerITSClusterRange(fchi2its);
-  task_Mpt->SetMinNCrossedRowsTPCRange(fnCrossedRows);
-  task_Mpt->SetEtaCut(fEta);
+  // task_Mpt->SetDCAXYRangeMax(fdcaxy);
+  // task_Mpt->SetDCAZRangeMax(fdcaz);
+  // task_Mpt->SetMaxChi2PerTPCClusterRange(fchi2tpc);
+  // task_Mpt->SetMaxChi2PerITSClusterRange(fchi2its);
+  // task_Mpt->SetMinNCrossedRowsTPCRange(fnCrossedRows);
+  // task_Mpt->SetEtaCut(fEta);
   
 
-  TString OutTreeName;
-  OutTreeName = "fTreeEvent";
-  OutTreeName += OutFileName;
-  task_Mpt->SetTreeName(OutTreeName);
+  // TString OutTreeName;
+  // OutTreeName += "fTreeEvent";
+  // OutTreeName += OutFileName;
+  // task_Mpt->SetTreeName(OutTreeName);
  
   
   mgr->AddTask(task_Mpt);                        // connect the task to the analysis manager
@@ -74,7 +73,7 @@ AliAnalysisTaskResonanceVsMultiplicityROOT6 *AddTaskResonanceVsMultiplicityROOT6
 
   AliAnalysisDataContainer  *cOutPut1;
   AliAnalysisDataContainer  *cOutPut2;
-  AliAnalysisDataContainer  *cOutPut3;
+  //AliAnalysisDataContainer  *cOutPut3;
   TString                  sMyOutName1;
   TString                  sMyOutName2;
   TString                  sMyOutName3;
@@ -88,11 +87,10 @@ AliAnalysisTaskResonanceVsMultiplicityROOT6 *AddTaskResonanceVsMultiplicityROOT6
   
   cOutPut1 = (AliAnalysisDataContainer *) mgr->CreateContainer(sMyOutName1,TTree::Class(),AliAnalysisManager::kOutputContainer,list1OutName.Data());
   cOutPut2 = (AliAnalysisDataContainer *) mgr->CreateContainer(sMyOutName2,TList::Class(),AliAnalysisManager::kOutputContainer,list1OutName.Data());
-  cOutPut3 = (AliAnalysisDataContainer *) mgr->CreateContainer(sMyOutName3,TList::Class(),AliAnalysisManager::kOutputContainer,list1OutName.Data());
+  //cOutPut3 = (AliAnalysisDataContainer *) mgr->CreateContainer(sMyOutName3,TList::Class(),AliAnalysisManager::kOutputContainer,list1OutName.Data());
   
   mgr->ConnectOutput(task_Mpt, 1, cOutPut1);
   mgr->ConnectOutput(task_Mpt, 2, cOutPut2);
-  mgr->ConnectOutput(task_Mpt, 3, cOutPut3);
   
 
   return task_Mpt;
--- PWGCF/EBYE/PWGCFebyeLinkDef.h
+++ PWGCF/EBYE/PWGCFebyeLinkDef.h
@@ -125,6 +125,7 @@
 #pragma link C++ class AliAnalysisTaskResonanceVsMultiplicityMC+;
 #pragma link C++ class AliAnalysisTaskResonanceVsMultiplicity2+;
 #pragma link C++ class AliAnalysisTaskResonanceVsMultiplicityROOT6+;
+#pragma link C++ class AliAnalysisTaskResonanceVsMultiplicity_MCnoESD+;
 
 #pragma link C++ class AliAnalysisTaskNFMs+;
 #endif
