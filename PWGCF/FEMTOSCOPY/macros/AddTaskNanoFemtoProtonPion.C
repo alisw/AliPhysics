8c676470ef79b5b68f70500a52908d56ee699b8e
mlesch <marcel.lesch@cern.ch>
2022-07-22 14:25:49 +0200

    Changes in set up of CPR histograms

--- PWGCF/FEMTOSCOPY/macros/AddTaskNanoFemtoProtonPion.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskNanoFemtoProtonPion.C
@@ -47,58 +47,55 @@ AliAnalysisTaskSE* AddTaskNanoFemtoProtonPion(bool isMC = false,//1
     TrackCutsAntiPion = AliFemtoDreamTrackCuts::PrimPionCuts(isMC, true, false, false);
     TrackCutsAntiPion->SetFilterBit(96);
     TrackCutsAntiPion->SetCutCharge(-1);
-
-
   } else {
+    TrackCutsPion = new AliFemtoDreamTrackCuts();
+    TrackCutsPion->SetIsMonteCarlo(isMC);
+    TrackCutsPion->SetCutCharge(1);
+    TrackCutsPion->SetPtRange(0.14, 4.0);
+    TrackCutsPion->SetEtaRange(-0.8, 0.8);
+    TrackCutsPion->SetNClsTPC(80);
+    // Not mention in AN oder Indico
+    TrackCutsPion->SetDCAReCalculation(true);//Get the dca from the PropagateToVetex
+    TrackCutsPion->SetFilterBit(128);//96); // Filterbit 5+6
+    TrackCutsPion->SetDCAVtxZ(0.3);
+    TrackCutsPion->SetDCAVtxXY(0.3);
+    // Cut on avrg. separation in TPC: <Dr> < 12 cm (10 cm, 3 cm); Share quality < 1.0; share fraction < 0.05
 
-	TrackCutsPion = new AliFemtoDreamTrackCuts();
-	TrackCutsPion->SetIsMonteCarlo(isMC);
-	TrackCutsPion->SetCutCharge(1);
-	TrackCutsPion->SetPtRange(0.14, 4.0);
-	TrackCutsPion->SetEtaRange(-0.8, 0.8);
-	TrackCutsPion->SetNClsTPC(80);
-	// Not mention in AN oder Indico
-	TrackCutsPion->SetDCAReCalculation(true);//Get the dca from the PropagateToVetex
-	TrackCutsPion->SetFilterBit(128);//96); // Filterbit 5+6
-	TrackCutsPion->SetDCAVtxZ(0.3);
-	TrackCutsPion->SetDCAVtxXY(0.3);
-	// Cut on avrg. separation in TPC: <Dr> < 12 cm (10 cm, 3 cm); Share quality < 1.0; share fraction < 0.05
+    // FOR NOW OF BEACUSE OF MAX's INFORMATION
+    //TrackCutsPion->SetCutSharedCls(true);
 
-	// FOR NOW OF BEACUSE OF MAX's INFORMATION
-	//TrackCutsPion->SetCutSharedCls(true);
-
-	TrackCutsPion->SetNClsTPC(80); // In Indico + additional Chi²/NDF <4
-	TrackCutsPion->SetPID(AliPID::kPion, 0.5);
-	TrackCutsPion->SetRejLowPtPionsTOF(false);
-	TrackCutsPion->SetMinimalBooking(false);
-	//this checks if the sigma of the wanted hypothesis is the smallest, and if
-	//another particle has a smaller sigma, the track is rejected.
-	// Not mention in AN oder Indico
-	//TrackCutsPion->SetCutSmallestSig(true);
-	TrackCutsPion->SetPlotDCADist(true);
+    TrackCutsPion->SetNClsTPC(80); // In Indico + additional Chi²/NDF <4
+    TrackCutsPion->SetPID(AliPID::kPion, 0.5);
+    TrackCutsPion->SetRejLowPtPionsTOF(false);
+    TrackCutsPion->SetMinimalBooking(false);
+    //this checks if the sigma of the wanted hypothesis is the smallest, and if
+    //another particle has a smaller sigma, the track is rejected.
+    // Not mention in AN oder Indico
+    //TrackCutsPion->SetCutSmallestSig(true);
+    TrackCutsPion->SetPlotDCADist(true);
 
 
-	TrackCutsAntiPion = new AliFemtoDreamTrackCuts();
-	TrackCutsAntiPion->SetIsMonteCarlo(isMC);
-	TrackCutsAntiPion->SetCutCharge(-1);
-	TrackCutsAntiPion->SetPtRange(0.14, 4.0);
-	TrackCutsAntiPion->SetEtaRange(-0.8, 0.8);
-	TrackCutsAntiPion->SetNClsTPC(80);
-	TrackCutsAntiPion->SetDCAReCalculation(true);
+    TrackCutsAntiPion = new AliFemtoDreamTrackCuts();
+    TrackCutsAntiPion->SetIsMonteCarlo(isMC);
+    TrackCutsAntiPion->SetCutCharge(-1);
+    TrackCutsAntiPion->SetPtRange(0.14, 4.0);
+    TrackCutsAntiPion->SetEtaRange(-0.8, 0.8);
+    TrackCutsAntiPion->SetNClsTPC(80);
+    TrackCutsAntiPion->SetDCAReCalculation(true);
 
-	// FOR NOW OF BEACUSE OF MAX's INFORMATION
-	//TrackCutsAntiPion->SetCutSharedCls(true);}
+    // FOR NOW OF BEACUSE OF MAX's INFORMATION
+    //TrackCutsAntiPion->SetCutSharedCls(true);}
 
-	TrackCutsAntiPion->SetNClsTPC(80);
-	TrackCutsAntiPion->SetPID(AliPID::kPion, 0.5);
-	TrackCutsAntiPion->SetRejLowPtPionsTOF(false);
-	TrackCutsAntiPion->SetMinimalBooking(false);
-	//TrackCutsAntiPion->SetCutSmallestSig(true);
-	TrackCutsAntiPion->SetPlotDCADist(true);
+    TrackCutsAntiPion->SetNClsTPC(80);
+    TrackCutsAntiPion->SetPID(AliPID::kPion, 0.5);
+    TrackCutsAntiPion->SetRejLowPtPionsTOF(false);
+    TrackCutsAntiPion->SetMinimalBooking(false);
+    //TrackCutsAntiPion->SetCutSmallestSig(true);
+    TrackCutsAntiPion->SetPlotDCADist(true);
 
-	TrackCutsAntiPion->SetFilterBit(128);//96);
-	TrackCutsAntiPion->SetDCAVtxZ(0.3);
-	TrackCutsAntiPion->SetDCAVtxXY(0.3);
+    TrackCutsAntiPion->SetFilterBit(128);//96);
+    TrackCutsAntiPion->SetDCAVtxZ(0.3);
+    TrackCutsAntiPion->SetDCAVtxXY(0.3);
   }
 
   //Proton and AntiProton cuts
@@ -116,9 +113,9 @@ AliAnalysisTaskSE* AddTaskNanoFemtoProtonPion(bool isMC = false,//1
   //Set-up output ------------------------------------------------------------------------
   std::vector<int> PDGParticles;
   PDGParticles.push_back(2212); 
-  PDGParticles.push_back(2212); 
+  PDGParticles.push_back(-2212); 
   PDGParticles.push_back(211); 
-  PDGParticles.push_back(211); 
+  PDGParticles.push_back(-211); 
 
   std::vector<bool> closeRejection;
   std::vector<float> mTBins;
@@ -215,7 +212,6 @@ AliAnalysisTaskSE* AddTaskNanoFemtoProtonPion(bool isMC = false,//1
   MultBins.push_back(96);
   MultBins.push_back(100);
 
-
   AliFemtoDreamCollConfig *config = new AliFemtoDreamCollConfig("Femto", "Femto", false);
   config->SetZBins(ZVtxBins);
   config->SetMultBins(MultBins);
@@ -242,12 +238,12 @@ AliAnalysisTaskSE* AddTaskNanoFemtoProtonPion(bool isMC = false,//1
     }
   }
   if (fullBlastQA) {
-   // config->SetkTBinning(true);
+    // config->SetkTBinning(true);
     config->SetPtQA(true);
     config->SetdPhidEtaPlots(true);
-  }
-
-  if (!fullBlastQA) {
+    config->SetdPhidEtaPlotsSmallK(true);
+    config->SetPhiEtaBinnign(true);
+  } else {
     evtCuts->SetMinimalBooking(true);
     TrackCutsProton->SetMinimalBooking(true);
     TrackCutsAntiProton->SetMinimalBooking(true);
@@ -256,7 +252,6 @@ AliAnalysisTaskSE* AddTaskNanoFemtoProtonPion(bool isMC = false,//1
     config->SetMinimalBookingME(true);
     config->SetMinimalBookingSample(true);
   }
-
 
   AliAnalysisTaskNanoFemtoProtonPion *task =
   new AliAnalysisTaskNanoFemtoProtonPion("FemtoDreamDefault", isMC);
