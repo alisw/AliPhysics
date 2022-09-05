703941e0436d6299685ed28958ed556c2b32875a
ryoka tokumoto <rtokumot@cern.ch>
2022-09-06 00:19:32 +0900

    train test for PbPb

--- PWGLF/NUCLEX/Exotica/Dibaryons/AddTaskMSDibaryons.C
+++ PWGLF/NUCLEX/Exotica/Dibaryons/AddTaskMSDibaryons.C
@@ -2,7 +2,7 @@
 
 AliAnalysisTaskMSDibaryons *AddTaskMSDibaryons(
 		const TString taskname = "MSDibaryons",
-		const UInt_t trigger = AliVEvent::kINT7 | AliVEvent::kCentral | AliVEvent::kSemiCentral,
+		const UInt_t trigger = AliVEvent::kINT7 | AliVEvent::kCentral | AliVEvent::kSemiCentral,	
 		const TString estimator = "V0M",
 		const Float_t cenmin = 0,
 		const Float_t cenmax = 90
@@ -20,6 +20,7 @@ AliAnalysisTaskMSDibaryons *AddTaskMSDibaryons(
 	//================================================================
 	AliAnalysisTaskMSDibaryons *task=new AliAnalysisTaskMSDibaryons(taskname.Data());
 	task->SetTrigger(trigger);
+	//task->SelectCollisionCandidates(trigger);
 	mgr->AddTask(task);
 
 	TString outlistname = Form("outlist_msdibaryons_%02d%02d",int(cenmin),int(cenmax));
