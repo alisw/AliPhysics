4f4085b58a4b96194c2d5fd7b70409e9fb7d1dd7
Raffaele Del Grande <raffaele.del.grande@cern.ch>
2022-09-07 15:29:17 +0200

    Added Q3 vs Pk Histo

--- PWGCF/FEMTOSCOPY/macros/AddTaskProtonProtonKaons.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskProtonProtonKaons.C
@@ -15,7 +15,7 @@ AliAnalysisTaskSE *AddTaskProtonProtonKaons(int trigger = 0, bool fullBlastQA = 
                                      bool isMC = false, bool isNano = true, bool triggerOn = false, int MixingDepth = 30,
                                      float Q3Limit = 0.6, float Q3LimitSample = 3.0,float Q3LimitSample2 = 3.0, float Q3LimitFraction = 0.5, float Q3LimitSampleFraction = 0.01, float Q3LimitSampleFraction2 = 0.01,
                                      const char *cutVariation = "0", bool turnoffClosePairRejectionCompletely = false, bool ClosePairRejectionForAll = "false",
-                                     const char *triggerVariation = "0", bool RunPlotPt = true, bool RunPlotQ3Vsq = false, bool UseSphericityCut = false, bool DoOnlyThreeBody = false, bool RunOfficialTwoBody=false, int KaonCut = 1, bool DoTwoPrimary = false, bool StandardMixing = false, bool DoKinematicPlots = false, bool RunPlotMult = true, bool RunCorrDeltaPhi = true, bool RunPlotPhiTheta = false) {
+                                     const char *triggerVariation = "0", bool RunPlotPt = true, bool RunPlotQ3Vsq = false, bool UseSphericityCut = false, bool DoOnlyThreeBody = false, bool RunOfficialTwoBody=false, int KaonCut = 1, bool DoTwoPrimary = false, bool StandardMixing = false, bool DoKinematicPlots = false, bool RunPlotMult = true, bool RunCorrDeltaPhi = true, bool RunPlotPhiTheta = false, bool RunPlotP1 = false) {
 
 
   TString suffix = TString::Format("%s", cutVariation);
@@ -346,6 +346,7 @@ AliAnalysisTaskSE *AddTaskProtonProtonKaons(int trigger = 0, bool fullBlastQA = 
     taskNano->SetCleanWithLambdas(false);
     taskNano->SetDoOnlyThreeBody(DoOnlyThreeBody);
     taskNano->SetRunOfficialTwoBody(RunOfficialTwoBody);
+    taskNano->SetRunPlotOtherHistos(false);
     //taskNano->SetDoTwoPrimary(DoTwoPrimary);
     taskNano->SetStandardMixing(StandardMixing);
     taskNano->SetRunPlotQ3Vsq(RunPlotQ3Vsq);
@@ -361,6 +362,7 @@ AliAnalysisTaskSE *AddTaskProtonProtonKaons(int trigger = 0, bool fullBlastQA = 
     taskNano->SetRunPlotMult(RunPlotMult);
     taskNano->SetRunCorrDeltaPhi(RunCorrDeltaPhi);
     taskNano->SetRunPlotPhiTheta(RunPlotPhiTheta);
+    taskNano->SetPlotP1(RunPlotP1);
     if (isMC) taskNano->SetPlotsMC(true);
 
     mgr->AddTask(taskNano);
