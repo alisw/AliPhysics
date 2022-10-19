7bec5a2b9ea592712b9e6809811bcf9038bcdd15
Mingrui Zhao on ubuntu ihep <mingrui.zhao@mail.labz0.org>
2022-10-19 02:30:44 -0400

    try to eliminate the zig zaging problem

--- PWGCF/FLOW/GF/AliAnalysisTaskCorrForNonlinearFlow.cxx
+++ PWGCF/FLOW/GF/AliAnalysisTaskCorrForNonlinearFlow.cxx
@@ -524,7 +524,7 @@ void AliAnalysisTaskCorrForNonlinearFlow::UserCreateOutputObjects() {
 	Double_t binning_deta_tpctpc[37] = {-1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0,  0.1,  0.2,  0.3,  0.4,  0.5, 0.6,  0.7,  0.8,  0.9,  1.0,  1.1,  1.2,  1.3,  1.4,  1.5, 1.6, 1.7, 1.8};
 	Double_t binning_deta_tpcfmd[43]={-6.,-5.8, -5.6, -5.4, -5.2, -5.0, -4.8, -4.6, -4.4, -4.2, -4., -3.8, -3.6, -3.4, -3.2, -3., -2.8, -2.6, -2.4, -2.2, -2., -1.8, -1.6, -1.4, -1.2, -1., -0.8, 1., 1.2, 1.4, 1.6, 1.8, 2. , 2.2, 2.4, 2.6, 2.8, 3., 3.2, 3.4, 3.6, 3.8, 4.};
 	std::vector<Double_t>   fPtBinsTrigCharged = {0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.25,1.5,1.75,2.0,2.25,2.5,3.0,3.5,4.0,5.0,6.0,8.0,10.0};    // I don't want to set the things outside
-	std::vector<Double_t>   fzVtxBins = {-10.0, 0, 10.0};
+	std::vector<Double_t>   fzVtxBins = {-10.0, -8.0, -6.0, -4.0, -2.0, 0, 2.0, 4.0, 6.0, 8.0, 10.0};
 	Int_t sizeEta = 0;
 	if (anaType.EqualTo("TPCTPC")) sizeEta = 36;
 	else if (anaType.EqualTo("TPCFMD")) sizeEta = 42;
@@ -553,6 +553,7 @@ void AliAnalysisTaskCorrForNonlinearFlow::UserCreateOutputObjects() {
 	// mixing
 	fPoolMaxNEvents = 2000;
 	fPoolMinNTracks = 50000;
+    fMinEventsToMix = 5;
 	int fNCentBins = fCentBins.size() - 1;
 	int fNzVtxBins = fzVtxBins.size() - 1;
 	fPoolMgr = new AliEventPoolManager(fPoolMaxNEvents, fPoolMinNTracks, fNCentBins, fCentBins.data(), fNzVtxBins, fzVtxBins.data());
@@ -953,7 +954,7 @@ void AliAnalysisTaskCorrForNonlinearFlow::FillCorrelationsMixed() {
 		return;
 	}
 
-	if (pool->IsReady()) {
+    if(pool->IsReady() || pool->NTracksInPool() > fPoolMinNTracks ||  pool->GetCurrentNEvents() > fMinEventsToMix) {
 		int nMix = pool->GetCurrentNEvents();
 
 
