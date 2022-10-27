8c9bd8971f199833e1ff4fc1f73555b25d2a3263
Daniel Battistini <daniel.battistini@cern.ch>
2022-10-27 13:06:27 +0200

    Add close pair rejection histos

--- PWGCF/FEMTOSCOPY/macros/AddTaskAnyCharmingFemto.C
+++ PWGCF/FEMTOSCOPY/macros/AddTaskAnyCharmingFemto.C
@@ -464,6 +464,7 @@ if (!isMC) {
 
   config->SetmTBinning((suffix == "0" && fullBlastQA));
   config->SetPtQA((suffix == "0" && fullBlastQA));
+  config->SetPhiEtaBinnign(suffix == "0" && fullBlastQA);
   config->SetMassQA((suffix == "0" && fullBlastQA));
   config->SetPDGCodes(PDGParticles);
   config->SetNBinsHist(NBins);
