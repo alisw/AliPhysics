c2cb609fb96e530930809b6cb8e12c4bc40e4914
aschmier <aschmier@vols.utk.edu>
2022-11-29 11:28:19 -0500

    PWGJE/EMCALJetTasks SVtaskMCFilter - Adding functionality for clusters. Should not impact existing wagon configs.

--- PWGJE/EMCALJetTasks/macros/AddTaskSVtaskMCFilter.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskSVtaskMCFilter.C
@@ -1,8 +1,8 @@
-AliAnalysisTaskSVtaskMCFilter* AddTaskSVtaskMCFilter(const char* trkcontname   = "tracks", const char* outtrk = "mytracks", Bool_t fFilterTracks = kTRUE){
+AliAnalysisTaskSVtaskMCFilter* AddTaskSVtaskMCFilter(const char* trkcontname   = "tracks", const char* outtrk = "mytracks", Bool_t fFilterTracks = kTRUE, const char* clscontname = "caloClusters", const char* outcls = "myclusters", Bool_t fFilterClusters = kFALSE){
 
    //Steering macro for filter of detector level MC tracks. Removes EPOS part of the event
 
-	
-   return  AliAnalysisTaskSVtaskMCFilter::AddTaskSVtaskMCFilter(trkcontname, outtrk, fFilterTracks);
+
+   return  AliAnalysisTaskSVtaskMCFilter::AddTaskSVtaskMCFilter(trkcontname, outtrk, fFilterTracks, clscontname, outcls, fFilterClusters);
 
 }
