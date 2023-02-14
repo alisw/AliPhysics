dcefd165e7b290f85ac15652b6b95020d5d6cada
imsurajprasad786 <suraj.prasad@cern.ch>
2023-02-14 14:07:37 +0530

    Physics selection and event cuts added

--- PWGMM/UE/FlatenicityLambdaK0s/AliAnalysisTaskFlatenicityLambdaK0s.h
+++ PWGMM/UE/FlatenicityLambdaK0s/AliAnalysisTaskFlatenicityLambdaK0s.h
@@ -4,61 +4,65 @@
 
 #ifndef AliAnalysisTaskFlatenicityLambdaK0s_H
 #define AliAnalysisTaskFlatenicityLambdaK0s_H
-
+#include "AliEventCuts.h"
 #include "AliAnalysisTaskSE.h"
-
-class AliAnalysisTaskFlatenicityLambdaK0s : public AliAnalysisTaskSE
-{
+class AliESDtrackCuts;
+class AliESDAD;
+class AliESDEvent;
+class AliAnalysisTaskFlatenicityLambdaK0s : public AliAnalysisTaskSE {
 public:
-	AliAnalysisTaskFlatenicityLambdaK0s();
-	AliAnalysisTaskFlatenicityLambdaK0s(const char *name);
-	virtual ~AliAnalysisTaskFlatenicityLambdaK0s();
+  AliAnalysisTaskFlatenicityLambdaK0s();
+  AliAnalysisTaskFlatenicityLambdaK0s(const char *name);
+  virtual ~AliAnalysisTaskFlatenicityLambdaK0s();
 
-	virtual void UserCreateOutputObjects();
-	virtual void UserExec(Option_t *option);
-	virtual void Terminate(Option_t *option);
-	Float_t GetDCAz(AliESDtrack *lTrack);
-	Float_t GetFlatenicityV0();
-	void CheckChargeV0(AliESDv0 *v0);
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t *option);
+  virtual void Terminate(Option_t *option);
+  Float_t GetDCAz(AliESDtrack *lTrack);
+  Float_t GetFlatenicityV0();
+  void CheckChargeV0(AliESDv0 *v0);
+  AliEventCuts fEventCuts;
 
 private:
-	AliESDEvent *fESD;	//! input event
-	TList *fOutputList; //! output list
-	AliPIDResponse *fPIDResponse;
-	AliESDpid *fESDpid;
-	TH1F *hinvmassK0s;		  //! dummy histogram
-	TH1F *hinvmassLambda;	  //! dummy histogram
-	TH1F *hinvmassAntiLambda; //! dummy histogram
-	TH1F *hflat;			  //! dummy histogram
+  AliESDEvent *fESD;  //! input event
+  TList *fOutputList; //! output list
+  AliPIDResponse *fPIDResponse;
+  AliESDpid *fESDpid;
+  TH1F *hinvmassK0s;        //! dummy histogram
+  TH1F *hinvmassLambda;     //! dummy histogram
+  TH1F *hinvmassAntiLambda; //! dummy histogram
+  TH1F *hflat;              //! dummy histogram
 
-	Float_t invmK0s;
-	Float_t invpK0s;
-	Float_t invptK0s;
-	Float_t invyK0s;
-	Float_t flatenicityK0s;
+  Float_t invmK0s;
+  Float_t invpK0s;
+  Float_t invptK0s;
+  Float_t invyK0s;
+  Float_t flatenicityK0s;
 
-	TTree *treeK0s;
+  TTree *treeK0s;
 
-	Float_t invmLambda;
-	Float_t invpLambda;
-	Float_t invptLambda;
-	Float_t invyLambda;
-	Float_t flatenicityLambda;
+  Float_t invmLambda;
+  Float_t invpLambda;
+  Float_t invptLambda;
+  Float_t invyLambda;
+  Float_t flatenicityLambda;
 
-	TTree *treeLambda;
+  TTree *treeLambda;
 
-	Float_t invmAntiLambda;
-	Float_t invpAntiLambda;
-	Float_t invptAntiLambda;
-	Float_t invyAntiLambda;
-	Float_t flatenicityAntiLambda;
+  Float_t invmAntiLambda;
+  Float_t invpAntiLambda;
+  Float_t invptAntiLambda;
+  Float_t invyAntiLambda;
+  Float_t flatenicityAntiLambda;
 
-	TTree *treeAntiLambda;
+  TTree *treeAntiLambda;
 
-	AliAnalysisTaskFlatenicityLambdaK0s(const AliAnalysisTaskFlatenicityLambdaK0s &);			 // not implemented
-	AliAnalysisTaskFlatenicityLambdaK0s &operator=(const AliAnalysisTaskFlatenicityLambdaK0s &); // not implemented
+  AliAnalysisTaskFlatenicityLambdaK0s(
+      const AliAnalysisTaskFlatenicityLambdaK0s &); // not implemented
+  AliAnalysisTaskFlatenicityLambdaK0s &
+  operator=(const AliAnalysisTaskFlatenicityLambdaK0s &); // not implemented
 
-	ClassDef(AliAnalysisTaskFlatenicityLambdaK0s, 1);
+  ClassDef(AliAnalysisTaskFlatenicityLambdaK0s, 1);
 };
 
 #endif
