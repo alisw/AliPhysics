c7c8ee79095de964284bce1f97d4466094530472
Mikkel Petersen <qkf986@alumni.ku.dk>
2023-03-20 13:58:11 +0100

    Initial commit

--- PWGCF/FLOW/GF/PWGCFFLOWGFLinkDef.h
+++ PWGCF/FLOW/GF/PWGCFFLOWGFLinkDef.h
@@ -8,6 +8,7 @@
 #pragma link C++ class AliAnalysisTaskCorrForFlow+;
 #pragma link C++ class AliAnalysisTaskCorrForFlowFMD+;
 #pragma link C++ class AliAnalysisTaskCorrForFlowMaster+;
+#pragma link C++ class AliAnalysisTaskCorrForFlowEta+;
 #pragma link C++ class AliAnalysisTaskDeform+;
 #pragma link C++ class AliAnalysisTaskGFWPIDFlow+;
 #pragma link C++ class AliAnalysisTaskFlowExample+;
