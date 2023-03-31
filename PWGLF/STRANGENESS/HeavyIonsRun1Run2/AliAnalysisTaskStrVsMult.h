f4bd00078f068c87c9c8c6bca633d849b0ea8e5d
Alessandro Balbino <abalbino@cern.ch>
2023-03-31 15:11:55 +0200

    Add new Armenteros-Podolanski selection for K0s

--- PWGLF/STRANGENESS/HeavyIonsRun1Run2/AliAnalysisTaskStrVsMult.h
+++ PWGLF/STRANGENESS/HeavyIonsRun1Run2/AliAnalysisTaskStrVsMult.h
@@ -18,7 +18,7 @@ class AliAnalysisTaskStrVsMult : public AliAnalysisTaskSE {
     virtual ~AliAnalysisTaskStrVsMult();
 
     // enum and names.
-    enum cutnumb_V0{kV0_DcaV0Daught, kV0_DcaPosToPV, kV0_DcaNegToPV, kV0_V0CosPA, kV0_V0Rad, kV0_MaxV0Rad, kV0_y, kV0_etaDaugh, kV0_LeastCRaws, kV0_LeastCRawsOvF, kV0_TrackLengthCut, kV0_MaxChi2perCls, kV0_NSigPID, kV0_PropLifetK0s, kV0_PropLifetLam, kV0_ITSTOFtracks, kV0cutsnum};
+    enum cutnumb_V0{kV0_DcaV0Daught, kV0_DcaPosToPV, kV0_DcaNegToPV, kV0_V0CosPA, kV0_V0Rad, kV0_MaxV0Rad, kV0_y, kV0_etaDaugh, kV0_LeastCRaws, kV0_LeastCRawsOvF, kV0_TrackLengthCut, kV0_MaxChi2perCls, kV0_NSigPID, kV0_PropLifetK0s, kV0_PropLifetLam, kV0_ITSTOFtracks, kV0_ArmenterosK0s, kV0cutsnum};
     enum cutnumb_Casc{kCasc_DcaCascDaught, kCasc_CascCosPA, kCasc_CascRad, kCasc_NSigPID, kCasc_LeastCRaws, kCasc_LeastCRawsOvF, kCasc_TrackLengthCut, kCasc_MaxChi2perCls, kCasc_InvMassLam, kCasc_DcaV0Daught, kCasc_V0CosPA, kCasc_DcaV0ToPV, kCasc_DcaBachToPV, kCasc_ITSTOFtracks, kCasc_y, kCasc_etaDaugh, kCasc_PropLifetXi, kCasc_PropLifetOm, kCasc_CompetingXiMass, kCasc_V0Rad, kCasc_MaxV0Rad, kCasc_DcaMesToPV, kCasc_DcaBarToPV, kCasc_BacBarCosPA, kCasccutsnum};
     enum particles{kK0s, kLam, kXi, kOm, knumpart}; 
     enum signedparticles{kk0s, klam, kalam, kxip, kxim, komp, komm, ksignednumpart}; 
@@ -122,6 +122,8 @@ class AliAnalysisTaskStrVsMult : public AliAnalysisTaskSE {
     ULong64_t fV0_NegTrackStatus;                             //!
     ULong64_t fV0_PosTrackStatus;                             //!
     double fV0_kinkidx;                                       //!
+    double fV0_AlphaArm;                                      //!
+    double fV0_pTArm;                                         //!
 
     //variables for Cascade analysis
     double fCasc_DcaCascDaught;                               //!
@@ -204,8 +206,8 @@ class AliAnalysisTaskStrVsMult : public AliAnalysisTaskSE {
     AliAnalysisTaskStrVsMult(const AliAnalysisTaskStrVsMult&);            // not implemented
     AliAnalysisTaskStrVsMult& operator=(const AliAnalysisTaskStrVsMult&); // not implemented
 
-    ClassDef(AliAnalysisTaskStrVsMult, 19); 
-    //version 19: add setters for TrackLength cut
+    ClassDef(AliAnalysisTaskStrVsMult, 20); 
+    //version 20: add Armenteros-Podolanski cut for K0s
 };
 
 #endif
