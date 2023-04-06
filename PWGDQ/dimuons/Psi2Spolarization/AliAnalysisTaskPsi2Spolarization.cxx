31aa3859064be796e9bf6f51590deae4ca25cc24
dushmanta97 <119102437+dushmanta97@users.noreply.github.com>
2023-04-06 15:15:53 +0530

    Update AliAnalysisTaskPsi2Spolarization.cxx

--- PWGDQ/dimuons/Psi2Spolarization/AliAnalysisTaskPsi2Spolarization.cxx
+++ PWGDQ/dimuons/Psi2Spolarization/AliAnalysisTaskPsi2Spolarization.cxx
@@ -172,9 +172,9 @@ void AliAnalysisTaskPsi2Spolarization::UserExec(Option_t *)
  
 
   UInt_t fSelectMask= fInputHandler->IsEventSelected();
-  Bool_t isINT7selected = fSelectMask& AliVEvent::kINT7;
+  Bool_t iskMuonUnlikeLowPt7selected = fSelectMask& AliVEvent::kMuonUnlikeLowPt7;
 
-    if (isINT7selected) return;
+    if (!iskMuonUnlikeLowPt7selected) return;
     
   AliVVertex* vertex = NULL;
   AliAODVertex* vertexSPD = NULL;
