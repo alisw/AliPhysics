fd5ad93ba8d7be1ea5d357e9819d20e42bbc977e
mwaelde <merle.luisa.waelde@cern.ch>
2023-04-11 14:11:30 +0200

    Update for cut variations

--- PWGDQ/dielectron/LMEE/AliAnalysisTaskOmegaDielectron_AccEff.h
+++ PWGDQ/dielectron/LMEE/AliAnalysisTaskOmegaDielectron_AccEff.h
@@ -51,93 +51,102 @@ using std::string;
 using std::vector;
 using std::array;
 
+class AliAnalysisFilter;
 class AliPIDResponse;
 
+
 class AliAnalysisTaskOmegaDielectron_AccEff : public AliAnalysisTaskSE {
-  public:
-    AliAnalysisTaskOmegaDielectron_AccEff();
-    AliAnalysisTaskOmegaDielectron_AccEff(const char *name);
-    virtual ~AliAnalysisTaskOmegaDielectron_AccEff();
+public:
+  AliAnalysisTaskOmegaDielectron_AccEff();
+  AliAnalysisTaskOmegaDielectron_AccEff(const char *name);
+  virtual ~AliAnalysisTaskOmegaDielectron_AccEff();
 
-    // static AliAnalysisTaskOmegaDielectron_AccEff* AddTaskMultDepSpec(TString controlstring, Int_t cutModeLow = 100, Int_t cutModeHigh = 121);
-    virtual void   UserCreateOutputObjects();
-    virtual void   UserExec(Option_t* option);
-    virtual void   Terminate(Option_t*);
+  // static AliAnalysisTaskOmegaDielectron_AccEff* AddTaskMultDepSpec(TString controlstring, Int_t cutModeLow = 100, Int_t cutModeHigh = 121);
+  virtual void   UserCreateOutputObjects();
+  virtual void   UserExec(Option_t* option);
+  virtual void   Terminate(Option_t*);
 
-  private:
+  // Track cuts setter
+  void   AddTrackCut(AliAnalysisFilter* filter) {fFilter_TrackCuts.push_back(filter);}
+  void   AddPIDCut(AliAnalysisFilter* filter_PID) {fFilter_PID.push_back(filter_PID);}
 
-    TList*              fOutputList;		  //!<! Output list
-    AliVEvent*          fEvent;			      //!<! Event object
-    AliMCEvent*         fMCEvent;         //!<! MC event
-    AliDielectronEventCuts  *fEventCuts;       //!<! Event cuts
-    AliDielectronEventCuts  *fEventCuts_VertexZ;       //!<! Event cuts
-    AliAnalysisFilter * fFilter_TrackCuts;    //!<! Analysis Filter
-    AliAnalysisFilter * fFilter_PID;          //!<! Analysis Filter
-    AliPIDResponse*     fPIDResponse;           //!<! Analysis Filter
+private:
 
-    // Acceptance cuts for tracks
-    Double_t              fMinEta;        ///< Minimum eta cut
-    Double_t              fMaxEta;        ///< Maximum eta cut
-    Double_t              fMinPt;			    ///< Minimum pT cut
-    Double_t              fMaxPt;			    ///< Maximum pT cut
+  TList*              fOutputList;		  //!<! Output list
+  TList*              fCutVariList;		  //!<! Output list
+  AliVEvent*          fEvent;			      //!<! Event object
+  AliMCEvent*         fMCEvent;         //!<! MC event
+  AliDielectronEventCuts  *fEventCuts;       //!<! Event cuts
+  AliDielectronEventCuts  *fEventCuts_VertexZ;       //!<! Event cuts
+  std::vector<AliAnalysisFilter*> fFilter_TrackCuts;  //!<! Analysis Filter
+  std::vector<AliAnalysisFilter*> fFilter_PID;    //!<! Analysis Filter
+  AliPIDResponse*     fPIDResponse;           //!<! Analysis Filter
 
-    // pdg codes:
-    Int_t                felectron_pdg;   //!
-    Int_t                fpositron_pdg;   //!
-    Int_t                fmother_pdg;     //!
+  // Acceptance cuts for tracks
+  Double_t              fMinEta;        ///< Minimum eta cut
+  Double_t              fMaxEta;        ///< Maximum eta cut
+  Double_t              fMinPt;			    ///< Minimum pT cut
+  Double_t              fMaxPt;			    ///< Maximum pT cut
 
-    //storage vectors:
-    vector<AliAODTrack *>   v_elec_true_omega;              //! array of strings containing the electron track from a true omega dielectron decay
-    vector<AliAODTrack *>   v_posi_true_omega;              //! array of strings containing the positron track from a true omega dielectron decay
-    vector<AliMCParticle *> v_elec_true_omega_MCPart;       //! array of strings containing the electron MCParticle from a true omega dielectron decay
-    vector<AliMCParticle *> v_posi_true_omega_MCPart;       //! array of strings containing the positron MCParticle from a true omega dielectron decay
-    vector<Int_t>           v_elec_motherID_true_omega;     //! array of strings containing the mother id of the electron
-    vector<Int_t>           v_posi_motherID_true_omega;     //! array of strings containing the mother id of the positron
+  // pdg codes:
+  Int_t                felectron_pdg;   //!
+  Int_t                fpositron_pdg;   //!
+  Int_t                fmother_pdg;     //!
 
-
-    // Output Histograms
-    TH2D* fHistVertex;                            //!<! Histogram for event counting
-
-    TH1F* fHist_MC_Omegas_Rapidity;                //!<! Histogram for event counting
-    TH3D* fHist_MC_Omegas_gen;                     //!<! Histogram of generated primaries
-    TH3D* fHist_MC_Omegas_gen_DaughtersinAcc;      //!<! Histogram of generated primaries
-
-    TH3D* fHist_MC_elec_gen;                 //!<! Histogram of generated primaries
-    TH3D* fHist_MC_posi_gen;                 //!<! Histogram of generated primaries
-    TH3D* fHist_MC_elec_gen_inAcc;                 //!<! Histogram of generated primaries
-    TH3D* fHist_MC_posi_gen_inAcc;                 //!<! Histogram of generated primaries
-
-    TH3D* fHist_elec_rec_inAcc;                 //!<! Histogram of generated primaries
-    TH3D* fHist_elec_rec_inAcc_Track;                 //!<! Histogram of generated primaries
-    TH3D* fHist_elec_rec_inAcc_Track_PID;                 //!<! Histogram of generated primaries
-    TH3D* fHist_posi_rec_inAcc;                 //!<! Histogram of generated primaries
-    TH3D* fHist_posi_rec_inAcc_Track;                 //!<! Histogram of generated primaries
-    TH3D* fHist_posi_rec_inAcc_Track_PID;                 //!<! Histogram of generated primaries
-
-    TH3D* fHist_MC_Omegas_withoutCuts;              //!<! Histogram of generated primaries
-    TH3D* fHist_MC_Omegas_TrackCuts;               //!<! Histogram of generated primaries
-    TH3D* fHist_MC_Omegas_TrackPID;                //!<! Histogram of generated primaries
-    TH3D* fHist_Rec_Omegas_withoutCuts;              //!<! Histogram of generated primaries
-    TH3D* fHist_Rec_Omegas_TrackCuts;              //!<! Histogram of generated primaries
-    TH3D* fHist_Rec_Omegas_TrackPID;               //!<! Histogram of generated primaries
+  //storage vectors:
+  vector<AliAODTrack *>   v_elec_true_omega;              //! array of strings containing the electron track from a true omega dielectron decay
+  vector<AliAODTrack *>   v_posi_true_omega;              //! array of strings containing the positron track from a true omega dielectron decay
+  vector<AliMCParticle *> v_elec_true_omega_MCPart;       //! array of strings containing the electron MCParticle from a true omega dielectron decay
+  vector<AliMCParticle *> v_posi_true_omega_MCPart;       //! array of strings containing the positron MCParticle from a true omega dielectron decay
+  vector<Int_t>           v_elec_motherID_true_omega;     //! array of strings containing the mother id of the electron
+  vector<Int_t>           v_posi_motherID_true_omega;     //! array of strings containing the mother id of the positron
 
 
-    void    InitCuts();       //!<! initialize the cuts
-    void    SetPIDResponse(AliPIDResponse *fPIDRespIn)        {fPIDResponse = fPIDRespIn;} //!<! pid response
+  // Output Histograms
+  TH2D* fHistVertex;                            //!<! Histogram for event counting
 
-    AliAnalysisCuts *SetPIDcuts();            //!<! Pid Cuts
-    AliAnalysisCuts *SetupTrackCuts();        //!<! Track cuts
+  TH1F* fHist_MC_Omegas_Rapidity;                //!<! Histogram for event counting
+  TH3D* fHist_MC_Omegas_gen_DaughtersinAcc;      //!<! Histogram of generated primaries
+
+  TH3D* fHist_MC_elec_gen;                 //!<! Histogram of generated primaries
+  TH3D* fHist_MC_posi_gen;                 //!<! Histogram of generated primaries
+  TH3D* fHist_MC_elec_gen_inAcc;                 //!<! Histogram of generated primaries
+  TH3D* fHist_MC_posi_gen_inAcc;                  //!<! Histogram of generated primaries
+  TH3D* fHist_MC_Omegas_withoutCuts;              //!<! Histogram of generated primaries
+
+  TH3D* fHist_Rec_Omegas_withoutCuts;              //!<! Histogram of generated primaries
 
 
-    Bool_t AcceptKinematics(AliVParticle* particle);                            //!<! Accept kinematic
-    Bool_t CheckDielectronDecay(AliMCParticle *particle, Bool_t checkacc);      //!<! Check if particle has e+e- as daughters , with bool for die kinematic acceptance check for the daughters
 
-    AliAnalysisTaskOmegaDielectron_AccEff(const AliAnalysisTaskOmegaDielectron_AccEff&); // not implemented
-    AliAnalysisTaskOmegaDielectron_AccEff& operator=(const AliAnalysisTaskOmegaDielectron_AccEff&); // not implemented
+  std::vector<TH3D*> fHist_MC_Omegas_gen;                     //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_elec_rec_inAcc;                 //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_elec_rec_inAcc_Track;                 //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_elec_rec_inAcc_Track_PID;                 //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_posi_rec_inAcc;                 //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_posi_rec_inAcc_Track;                 //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_posi_rec_inAcc_Track_PID;                 //!<! Histogram of generated primaries
 
-    /// \cond CLASSIMP
-    ClassDef(AliAnalysisTaskOmegaDielectron_AccEff, 1); // example of analysis
-    /// \endcond
+  // std::vector<TH3D*> fHist_MC_Omegas_withoutCuts;              //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_MC_Omegas_TrackCuts;               //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_MC_Omegas_TrackPID;                //!<! Histogram of generated primaries
+  // std::vector<TH3D*> fHist_Rec_Omegas_withoutCuts;              //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_Rec_Omegas_TrackCuts;              //!<! Histogram of generated primaries
+  std::vector<TH3D*> fHist_Rec_Omegas_TrackPID;               //!<! Histogram of generated primaries
+
+
+  void    SetPIDResponse(AliPIDResponse *fPIDRespIn)        {fPIDResponse = fPIDRespIn;} //!<! pid response
+
+
+
+  Bool_t AcceptKinematics(AliVParticle* particle);                            //!<! Accept kinematic
+  Bool_t CheckDielectronDecay(AliMCParticle *particle, Bool_t checkacc);      //!<! Check if particle has e+e- as daughters , with bool for die kinematic acceptance check for the daughters
+
+  AliAnalysisTaskOmegaDielectron_AccEff(const AliAnalysisTaskOmegaDielectron_AccEff&); // not implemented
+  AliAnalysisTaskOmegaDielectron_AccEff& operator=(const AliAnalysisTaskOmegaDielectron_AccEff&); // not implemented
+
+  /// \cond CLASSIMP
+  ClassDef(AliAnalysisTaskOmegaDielectron_AccEff, 1); // example of analysis
+  /// \endcond
 };
 
 #endif
