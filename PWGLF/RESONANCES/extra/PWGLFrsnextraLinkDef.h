b90da97549f00e1021af6af8d56bef2fa9b6b07f
Prottay Das <prottay.das@niser.ac.in>
2023-05-05 20:25:34 +0530

    changed class names

--- PWGLF/RESONANCES/extra/PWGLFrsnextraLinkDef.h
+++ PWGLF/RESONANCES/extra/PWGLFrsnextraLinkDef.h
@@ -25,7 +25,7 @@
 #pragma link C++ class AliAnalysisTaskEffMatrix+;
 #pragma link C++ class AliAnalysisTaskPhiSA+;
 #pragma link C++ class AliAnalysisTaskPhiSAR+;
-#pragma link C++ class Aliresonance+;
+#pragma link C++ class AliAnalysisTaskfn+;
 #pragma link C++ class AliCompactTrack+;
 #pragma link C++ class AliAnalysisTaskPhiCount+;
 #pragma link C++ class AliAnalysisTask_Phi_MC+;
