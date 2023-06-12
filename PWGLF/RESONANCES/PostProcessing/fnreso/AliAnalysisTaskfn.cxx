b89ca7d8d7a2524a7dd43ea52d71eccf8f7fc348
Prottay Das <prottay.das@niser.ac.in>
2023-06-12 17:18:03 +0530

    added equal sign

--- PWGLF/RESONANCES/PostProcessing/fnreso/AliAnalysisTaskfn.cxx
+++ PWGLF/RESONANCES/PostProcessing/fnreso/AliAnalysisTaskfn.cxx
@@ -352,7 +352,7 @@ void AliAnalysisTaskfn::UserExec(Option_t *)
 
 	  motherf1 = pion.particle + kks0.particle;
 
-	  if (TMath::Abs(motherf1.Rapidity())>0.5)                                                                                         
+	  if (TMath::Abs(motherf1.Rapidity())>=0.5)                                                                                         
           continue;       
 
 	  if (motherf1.M() > 2.0)
@@ -415,7 +415,7 @@ void AliAnalysisTaskfn::UserExec(Option_t *)
 		 pionmixenergy = TMath::Sqrt(pionmass * pionmass + piontrackmix->Px() * piontrackmix->Px() + piontrackmix->Py() * piontrackmix->Py() + piontrackmix->Pz() * piontrackmix->Pz());
 		 motherf1mix.SetPxPyPzE(piontrackmix->Px()+kks0.particle.Px(), piontrackmix->Py()+kks0.particle.Py(), piontrackmix->Pz()+kks0.particle.Pz(), pionmixenergy+kks0.particle.E());
 
-		 if (TMath::Abs(motherf1mix.Rapidity())>0.5)                                         
+		 if (TMath::Abs(motherf1mix.Rapidity())>=0.5)                                         
 		   continue;
 		 
 		 if (motherf1mix.M() > 2.0)
