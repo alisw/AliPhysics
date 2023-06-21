de03f69974da22139b18810ef3a48b141f52c86c
Salman K Malik <salman.khurshid.malik@cern.ch>
2023-06-22 01:35:17 +0200

    intermittencyfixMmax

--- PWGCF/EBYE/macros/AddTaskNFactorialMoments.C
+++ PWGCF/EBYE/macros/AddTaskNFactorialMoments.C
@@ -1,18 +1,19 @@
 #include "AliAnalysisManager.h"
 #include "AliAnalysisTaskNFactorialMoments.h"
 
-AliAnalysisTaskNFactorialMoments *AddTaskNFactorialMoments(
-    TString year = "2015", bool fSelfAffAnalysis = kFALSE, double pt1 = 0.4,
-    double pt2 = 0.6, double pt3 = 0.4, double pt4 = 1.0, double pt5 = 0.6,
-    double pt6 = 0.8, double pt7 = 0.6, double pt8 = 0.9, bool fPileup = kFALSE,
-    double fEtaMin = -0.8, double fEtaMax = 0.8, int fCentMin = 0,
-    int fCentMax = 10, int fBit = 128, double fDeta = 0.02, double fDphi = 0.02,
-    bool fTwoTrack = kFALSE, double fSharedFraction = 0.05,
-    bool fSharity = kFALSE, double fVxMax = 0.3, double fVyMax = 0.4,
-    double fVzMax = 10.0, bool fRejectElectrons = kFALSE,
-    double fDCAXYRangeMax = 0.0, double fDCAZRangeMax = 0.0,
-    double fITSClusterCut = 0.0, double fTPCClusterCut = 0.0,
-    double fnTPCrossedrows = 0.0, double fSharedCls = 0.0, double fSharedRows = 0.0, double fFindableCls = 0.0, int Mmax = 123, bool fIsMC = kTRUE) {
+AliAnalysisTaskNFactorialMoments* AddTaskNFactorialMoments(
+  TString year = "2015", bool fSelfAffAnalysis = kFALSE, double pt1 = 0.4,
+  double pt2 = 0.6, double pt3 = 0.4, double pt4 = 1.0, double pt5 = 0.6,
+  double pt6 = 0.8, double pt7 = 0.6, double pt8 = 0.9, bool fPileup = kFALSE,
+  double fEtaMin = -0.8, double fEtaMax = 0.8, int fCentMin = 0,
+  int fCentMax = 10, int fBit = 128, double fDeta = 0.02, double fDphi = 0.02,
+  bool fTwoTrack = kFALSE, double fSharedFraction = 0.05,
+  bool fSharity = kFALSE, double fVxMax = 0.3, double fVyMax = 0.4,
+  double fVzMax = 10.0, bool fRejectElectrons = kFALSE,
+  double fDCAXYRangeMax = 0.0, double fDCAZRangeMax = 0.0,
+  double fITSClusterCut = 0.0, double fTPCClusterCut = 0.0,
+  double fnTPCrossedrows = 0.0, double fSharedCls = 0.0, double fSharedRows = 0.0, double fFindableCls = 0.0, int Mmax = 123, bool fIsMC = kTRUE)
+{
 
   // year = "2010" or "2015"
   // fSelfAffAnalysis for self-affine analysis
@@ -20,7 +21,7 @@ AliAnalysisTaskNFactorialMoments *AddTaskNFactorialMoments(
   // cuts on DCA, ITS/TPC clusters, ncrssed rows work only if they have non-zero
   // values Particle Indentification uses only TPC for now TODO: add TOF
 
-  double ptarr[8] = {pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8};
+  double ptarr[8] = { pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8 };
   TArrayD ptarrD(8, ptarr);
   int fNumberofPtBins = ptarrD.GetSize() / 2;
 
@@ -28,9 +29,9 @@ AliAnalysisTaskNFactorialMoments *AddTaskNFactorialMoments(
   int Nbinsarr[40]; // used in case of selfaffine analysis
   for (size_t nMB = 0; nMB < 40; nMB++) {
     if (Mmax == 123)
-    Mbinsarr[nMB] = 3 * (nMB + 2);
+      Mbinsarr[nMB] = 3 * (nMB + 2);
     if (Mmax == 82)
-    Mbinsarr[nMB] = 2 * (nMB + 2);
+      Mbinsarr[nMB] = 2 * (nMB + 2);
   }
   if (fSelfAffAnalysis) {
     for (int nMB = 0; nMB < 40; nMB++) {
@@ -42,14 +43,14 @@ AliAnalysisTaskNFactorialMoments *AddTaskNFactorialMoments(
   TArrayI Mbins(40, Mbinsarr);
   TArrayI Nbins(40, Nbinsarr);
 
-  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  AliAnalysisManager* mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
     return 0x0;
   }
 
   // creating an instance for tasknfms
-  AliAnalysisTaskNFactorialMoments *tasknfms =
-      new AliAnalysisTaskNFactorialMoments("tasknfms");
+  AliAnalysisTaskNFactorialMoments* tasknfms =
+    new AliAnalysisTaskNFactorialMoments("tasknfms");
   if (!tasknfms)
     return 0x0;
 
@@ -78,33 +79,33 @@ AliAnalysisTaskNFactorialMoments *AddTaskNFactorialMoments(
 
   // input for tasknfmss and connecting containers to it
   int nout = (fNumberofPtBins) + 3 + fIsMC * ((fNumberofPtBins)-1);
-  AliAnalysisDataContainer *coutputlist[nout];
+  AliAnalysisDataContainer* coutputlist[nout];
 
   coutputlist[0] = mgr->CreateContainer(
-      "outputlist", TList::Class(), AliAnalysisManager::kOutputContainer,
-      AliAnalysisManager::GetCommonFileName());
+    "outputlist", TList::Class(), AliAnalysisManager::kOutputContainer,
+    AliAnalysisManager::GetCommonFileName());
 
-  char *name = "ntplist";
+  char* name = "ntplist";
   if (fIsMC)
     name = "ntplistRe";
   for (int nbins = 0; nbins < fNumberofPtBins; nbins++) {
     coutputlist[nbins + 1] =
-        mgr->CreateContainer(Form("%s%d", name, nbins + 1), TList::Class(),
-                             AliAnalysisManager::kOutputContainer,
-                             AliAnalysisManager::GetCommonFileName());
+      mgr->CreateContainer(Form("%s%d", name, nbins + 1), TList::Class(),
+                           AliAnalysisManager::kOutputContainer,
+                           AliAnalysisManager::GetCommonFileName());
     if (fIsMC)
       coutputlist[nbins + fNumberofPtBins + 1] =
-          mgr->CreateContainer(Form("ntplistGen%d", nbins + 1), TList::Class(),
-                               AliAnalysisManager::kOutputContainer,
-                               AliAnalysisManager::GetCommonFileName());
+        mgr->CreateContainer(Form("ntplistGen%d", nbins + 1), TList::Class(),
+                             AliAnalysisManager::kOutputContainer,
+                             AliAnalysisManager::GetCommonFileName());
   }
   if (!fIsMC)
     coutputlist[nout - 2] = mgr->CreateContainer(
-        "QAlist", TList::Class(), AliAnalysisManager::kOutputContainer,
-        AliAnalysisManager::GetCommonFileName());
-  coutputlist[nout - 1] = mgr->CreateContainer(
-      "QAlist2", TList::Class(), AliAnalysisManager::kOutputContainer,
+      "QAlist", TList::Class(), AliAnalysisManager::kOutputContainer,
       AliAnalysisManager::GetCommonFileName());
+  coutputlist[nout - 1] = mgr->CreateContainer(
+    "QAlist2", TList::Class(), AliAnalysisManager::kOutputContainer,
+    AliAnalysisManager::GetCommonFileName());
 
   mgr->ConnectInput(tasknfms, 0, mgr->GetCommonInputContainer());
   for (int i = 0; i < nout; i++) {
