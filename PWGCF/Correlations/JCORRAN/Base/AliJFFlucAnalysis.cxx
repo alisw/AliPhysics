d2093d6d4286def80bf5264d6b2647f9bff234e6
Maxim Virta <maxim.m.o.virta@jyu.fi>
2023-07-24 12:45:42 +0300

    (PWGCF): TH3D declaration changed, new binning added

--- PWGCF/Correlations/JCORRAN/Base/AliJFFlucAnalysis.cxx
+++ PWGCF/Correlations/JCORRAN/Base/AliJFFlucAnalysis.cxx
@@ -228,7 +228,7 @@ void AliJFFlucAnalysis::UserCreateOutputObjects(){
 			<< fHistCentBin
 			<< "END";
 		fh_phietaz
-			<< TH3D("h_phietaz","h_phietaz",60,-TMath::Pi(),TMath::Pi(),40,-2.0,2.0,8,-10.0,10.0)
+			<< TH3D("h_phietaz","h_phietaz",400,-TMath::Pi(),TMath::Pi(),40,-1.0,1.0,8,-20.0,20.0)
 			<< fHistCentBin
 			<< "END";
 	}
