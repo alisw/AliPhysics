35107afddca35513d5947b7158adf6756f62b863
Friederike Bock <friederike.bock@cern.ch>
2023-08-30 09:48:37 -0400

    PWGGA/GammaConv: added single cuts for PbPb PCM omega

--- PWGGA/GammaConv/macros/AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_ConvMode_PbPb.C
+++ PWGGA/GammaConv/macros/AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_ConvMode_PbPb.C
@@ -177,7 +177,18 @@ void AddTask_GammaConvNeutralMesonPiPlPiMiNeutralMeson_ConvMode_PbPb(
     cuts.AddCutHeavyMesonPCM("11310a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 10-30% MB
     cuts.AddCutHeavyMesonPCM("13530a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 30-50% Triggeres
     cuts.AddCutHeavyMesonPCM("15910a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 50-90% MB
- 
+
+    }else if (trainConfig == 102){ // Standard 5 TeV omega INT7 cutstring in pPb -> First look into PbPb
+    cuts.AddCutHeavyMesonPCM("10910a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 0-90% MB
+    }else if (trainConfig == 103){ // Standard 5 TeV omega INT7 cutstring in pPb -> First look into PbPb
+    cuts.AddCutHeavyMesonPCM("10130a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 0-10% Triggered
+    }else if (trainConfig == 104){ // Standard 5 TeV omega INT7 cutstring in pPb -> First look into PbPb
+    cuts.AddCutHeavyMesonPCM("11310a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 10-30% MB
+    }else if (trainConfig == 105){ // Standard 5 TeV omega INT7 cutstring in pPb -> First look into PbPb
+    cuts.AddCutHeavyMesonPCM("13530a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 30-50% Triggeres
+    }else if (trainConfig == 106){ // Standard 5 TeV omega INT7 cutstring in pPb -> First look into PbPb
+    cuts.AddCutHeavyMesonPCM("15910a13","0dm00009f9730000dge0404000","32c51079a","0000003z00000000","0400503000000000"); // 50-90% MB
+  
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   //                                          ETA PRIME MESON
