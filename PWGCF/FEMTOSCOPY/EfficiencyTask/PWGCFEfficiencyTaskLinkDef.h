d988538b4ff27aca3d87eb6597375b3a4d0dcdd8
Daniel Rodak <49811184+AsFastAsSnail@users.noreply.github.com>
2023-10-02 12:12:14 +0200

    Add files via upload

--- PWGCF/FEMTOSCOPY/EfficiencyTask/PWGCFEfficiencyTaskLinkDef.h
+++ PWGCF/FEMTOSCOPY/EfficiencyTask/PWGCFEfficiencyTaskLinkDef.h
@@ -3,6 +3,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class AliAnalysisTaskParticleEff+;
-#pragma link C++ AliAnalysisTaskParticleEffWithDoubleCounting+;
+#pragma link C++ class AliAnalysisTaskParticleEffWithDoubleCounting+;
 #pragma link C++ class AliAnalysisTaskParticleEffpbpb5020+;
 #pragma link C++ class AliAnalysisTaskParticleEfficiencyMezonPhiDR+;
