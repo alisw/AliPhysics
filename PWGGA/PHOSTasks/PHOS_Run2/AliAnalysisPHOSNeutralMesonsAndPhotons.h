a5f182d391d6b331c7f7280052318b205ebb081b
Vladislav Kuskov <vladislav.kuskov@cern.ch>
2024-04-08 14:15:50 +0200

    PWGGA/PHOSTasks: new task for neutral meson and photon analyses

---
+++ PWGGA/PHOSTasks/PHOS_Run2/AliAnalysisPHOSNeutralMesonsAndPhotons.h
@@ -0,0 +1,230 @@
+#ifndef AliAnalysisPHOSNeutralMesonsAndPhotons_cxx
+#define AliAnalysisPHOSNeutralMesonsAndPhotons_cxx
+
+#include "AliCaloPhoton.h"
+#include "AliAODCaloCluster.h"
+#include "AliPHOSGeometry.h"
+#include "AliMCEvent.h"
+#include "AliAODMCParticle.h"
+#include "AliCaloTriggerMimicHelper.h"
+#include "AliAnalysisUtils.h"
+#include "AliMCAnalysisUtils.h"
+
+// Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+// See cxx source for full Copyright notice
+
+// Analysis task for neutral meson and inclusive/direct photon measurements (AODs only)
+// + supproting QA studies
+// Author: Vladislav Kuskov
+// since march 2024
+
+#include "AliAnalysisTaskSE.h"
+
+class AliAnalysisPHOSNeutralMesonsAndPhotons : public AliAnalysisTaskSE
+{
+ public:
+  AliAnalysisPHOSNeutralMesonsAndPhotons(const char* name = "AliAnalysisPHOSNeutralMesonsAndPhotons");
+  virtual ~AliAnalysisPHOSNeutralMesonsAndPhotons();
+
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t* option);
+  virtual void Terminate(Option_t*);
+
+  void SetMBTrigger(Bool_t isMB) { fIsMBTriggerAnalysis = isMB; };
+  void SetPHOSTrigger(Bool_t isPHS, AliCaloTriggerMimicHelper::phosTriggerType PHSTrigType)
+  {
+    fIsPHOSTriggerAnalysis = isPHS;
+    fPHOSTrigType = PHSTrigType;
+  };
+  void SetMC(Bool_t isMC) { fIsMC = isMC; };
+  void SetCoreEnergyFlag(Bool_t useCoreE) { fUseCoreEnergy = useCoreE; };
+  void SetClusterCuts(Bool_t useCoreE, Double_t Emin, Double_t DispCut, Double_t CPVCut, Double_t TOFCut)
+  {
+    fUseCoreEnergy = useCoreE;
+    fEminCut = Emin;
+    fDispSigma = DispCut;
+    fCPVSigma = CPVCut;
+    fTOFCut = TOFCut;
+  };
+  void SetQAStudy(Bool_t DoClustQA, Bool_t DoCellQA)
+  {
+    fDoClustQA = DoClustQA;
+    fDoCellQA = DoCellQA;
+  };
+  void SetCentralityStudy(Bool_t doCentralityStudy, TString CentEstim, Float_t CentMin, Float_t CentMax)
+  {
+    fDoCentralityStudy = doCentralityStudy;
+    fCentralityEstimator = CentEstim;
+    fCentralityMin = CentMin;
+    fCentralityMax = CentMax;
+  }
+
+  void SetTOFEfficiency(TF1* tofeff) { fUserTOFEff = tofeff; };
+  void SetUserNonlinearityFunction(TF1* nonlin) { fUserNonlinFunc = nonlin; };
+
+  void SetNEventsForMixing(Int_t NMix) { fNMixEvents = NMix; };
+
+ private:
+  AliAnalysisPHOSNeutralMesonsAndPhotons(const AliAnalysisPHOSNeutralMesonsAndPhotons&);
+  AliAnalysisPHOSNeutralMesonsAndPhotons& operator=(const AliAnalysisPHOSNeutralMesonsAndPhotons&);
+
+  void FillHistogram(const char* key, Double_t x) const;                         // Fill 1D histogram witn name key
+  void FillHistogram(const char* key, Double_t x, Double_t y) const;             // Fill 2D histogram witn name key
+  void FillHistogram(const char* key, Double_t x, Double_t y, Double_t z) const; // Fill 3D histogram witn name key
+
+  void SelectCentrality();
+  void InitPHOSGeometry();
+
+  void ProcessCaloPhotons();
+  void FillMgg();
+  void EstimatePIDCutEfficiency();
+  void EstimateTOFCutEfficiency();
+
+  void ProcessMCParticles();
+  AliAODMCParticle* FindMCPrimaryMother(AliAODMCParticle* particle);
+  AliAODMCParticle* FindMCPrimaryMother(Int_t primlb);
+  Int_t FindMCPrimaryMotherLabel(Int_t primlb);
+  Bool_t InPHOSAcc(AliAODMCParticle* particle);
+  Bool_t IsPrimaryMCParticle(AliAODMCParticle* particle);
+  Bool_t IsPrimaryMCParticle3D(AliAODMCParticle* particle);
+
+  TF1* GetNonlinFunction() { return fUserNonlinFunc; };
+
+ private:
+  THashList* fOutputContainer; //! final histogram container
+
+  AliMCEvent* fMCEvent;
+  AliAODEvent* fEvent; //! Current event
+
+  Int_t fRunNumber;
+
+  TClonesArray* fMCStack;
+
+  TClonesArray* fCaloPhotonsPHOS; //! List of selected photons in current event
+  TClonesArray* fCaloPhotonsMix;  //! List of selected photons in mixed events
+
+  TList* fCaloPhotonsPHOSList;
+  TList* fPHOSEvents[10][10]; //! Previous events for mixing
+
+  AliAnalysisUtils*                          fUtils;
+  AliPHOSGeometry*                           fPHOSGeo;
+  AliAODCaloCells*                           fAODCells;
+  AliCaloTriggerMimicHelper*                 fCaloTriggerMimicHelper;
+  AliCaloTriggerMimicHelper::phosTriggerType fPHOSTrigType;
+
+  Bool_t fIsMC;
+  Bool_t fIsPHOSTriggerAnalysis;
+  Bool_t fIsMBTriggerAnalysis;
+  Bool_t fDoClustQA;
+  Bool_t fDoCellQA;
+
+  Int_t fNMixEvents;
+
+  Double_t fVertex[3];
+  Int_t fZvtx;
+
+  Bool_t  fDoCentralityStudy;
+  TString fCentralityEstimator;
+  AliMultSelection* fMultSelection;
+  Float_t  fCentrality;
+  Int_t    fNCenBin;
+  Int_t    fCentBin;
+  TArrayI  fCenBinEdges;
+  Double_t fCentralityMin;
+  Double_t fCentralityMax;
+
+  Bool_t fDoNonlinCorr;
+  TF1*   fUserNonlinFunc;
+  Bool_t fDoTOFEffCorr;
+  TF1*   fUserTOFEff;
+
+  Bool_t   fUseCoreEnergy;
+  Double_t fEminCut;
+  Double_t fDispSigma;
+  Double_t fCPVSigma;
+  Double_t fTOFCut;
+
+  TH1F* fHistInfo;
+  TH1F* fHistSelectEvent;
+  TH1F* fHistVertexZ;
+
+  TH1F* fHistCentMain;
+  TH2F* fHistCentV0MvsCL0;
+  TH2F* fHistCentV0MvsCL1;
+  TH2F* fHistCentV0MvsV0C;
+  TH2F* fHistCentCL0vsCL1;
+  TH2F* fHistCentZNAvsZNC;
+
+  TH2I* fPHOSBadMap[6];
+
+  TH2F* fHistTOFClust;
+  TH2F* fHistTOFClustMod[4];
+  TH2F* fHistCaloPhotonTOFvsE;
+  TH2F* fHistCaloPhotonTOFvsEMod[4];
+
+  TH2F* fHistClustTOFvsDDL;
+  TH2F* fHistClustTOFvsDDLEnCut;
+
+  TH2F* fHistClustMultVsCentrality;
+  TH2F* fHistClustMultVsCentralityMod[4];
+
+  TH1F* fHistCaloPhotonPt[4];
+  TH2F* fHistM02vsPt;
+  TH2F* fHistM20vsPt;
+
+  TH1F* fHistClustFullE;
+  TH1F* fHistClustCoreE;
+  TH1F* fHistCaloPhotonPtMod[4];
+
+  TH2F* fHistNonlinTest;
+
+  TH1F* fHistClustFullEMod[4];
+  TH1F* fHistClustCoreEMod[4];
+
+  TH2F* fHistMgg[4];
+  TH2F* fHistMixMgg[4];
+
+  TH2F* fHistMggTOFCutEffBase;
+  TH2F* fHistMixMggTOFCutEffBase;
+  TH2F* fHistMggTOFCutEffProbe;
+  TH2F* fHistMixMggTOFCutEffProbe;
+
+  TH2F* fHistMggCutEff[4];
+  TH2F* fHistMixMggCutEff[4];
+
+  TH2F* fHistMggCutEffMod[4][4];
+  TH2F* fHistMixMggCutEffMod[4][4];
+
+  TH2F* fHistMggMod[4];
+  TH2F* fHistMixMggMod[4];
+  TH2F* fHistMggPhIDCutMod[4];
+  TH2F* fHistMixMggPhIDCutMod[4];
+
+  TH2F* fHistTruePi0MggVsRecPt;
+  TH2F* fHistTruePi0MggVsTruePt;
+  TH2F* fHistTrueEtaMggVsRecPt;
+  TH2F* fHistTrueEtaMggVsTruePt;
+
+  TH2F* fHistMCPartIDvsPt;
+  TH2F* fHistMCCaloPartIDvsPt[4];
+
+  TH1F* fHistPrimPi0Pt;
+  TH1F* fHistPrimPi0InAccPt;
+  TH1F* fHistPrimPi0BothPhInAccPt;
+  TH1F* fHistPrimPi0OnePhInAccPt;
+  TH2F* fHistPrimPi0PtvsPhInAccPt;
+
+  TH1F* fHistPrimEtaPt;
+  TH1F* fHistPrimEtaInAccPt;
+  TH1F* fHistPrimEtaBothPhInAccPt;
+  TH1F* fHistPrimEtaOnePhInAccPt;
+  TH2F* fHistPrimEtaPtvsPhInAccPt;
+
+  TH1F* fHistPrimGammaPt;
+  TH1F* fHistPrimGammaInAccPt;
+
+  // Histograms for analysis
+
+  ClassDef(AliAnalysisPHOSNeutralMesonsAndPhotons, 1);
+};
+#endif
