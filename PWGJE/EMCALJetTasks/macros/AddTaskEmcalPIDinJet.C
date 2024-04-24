61b4a3c07b6275ccc0d117c7c0f3469872b1b218
Shingo Sakai <shingo.sakai@cern.ch>
2024-04-24 11:33:20 +0900

    fixed bug in the task

--- PWGJE/EMCALJetTasks/macros/AddTaskEmcalPIDinJet.C
+++ PWGJE/EMCALJetTasks/macros/AddTaskEmcalPIDinJet.C
@@ -1,27 +1,27 @@
 AliAnalysisTaskEmcalPIDinJet* AddTaskEmcalPIDinJet(
+  TString name = "name",
   const char *ntracks            = "usedefault",
   const char *nclusters          = "usedefault",
   const char* ncells             = "usedefault",
-  const char *suffix             = ""
+  const char *suffix             = "",
+  Bool_t iBeamType_PbPb          = kTRUE          
 )
 {
 
    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr)
   {  
-    ::Error("AddTaskHFjetTagHFE", "No analysis manager to connect to.");
     return 0;
   }   
 
   AliVEventHandler* handler = mgr->GetInputEventHandler();
   if (!handler)
   {
-    ::Error("AddTaskHFjetTagHFE", "This task requires an input event handler");
     return 0;
   }
 
-  AliAnalysisTaskEmcalPIDinJet *jetTask = 0;
-  jetTask = AddTaskEmcalJet2023("usedefault", "usedefault", "usedefault", "");
+  AliAnalysisTaskEmcalPIDinJet *jetTask = new AliAnalysisTaskEmcalPIDinJet(name.Data());
+  //jetTask = AddTaskEmcalJetPIDinJet("usedefault", "usedefault", "usedefault", "");
   jetTask->GetClusterContainer(0)->SetClusECut(0.);
   jetTask->GetClusterContainer(0)->SetClusPtCut(0.);
   jetTask->GetClusterContainer(0)->SetClusNonLinCorrEnergyCut(0.);
@@ -29,16 +29,16 @@ AliAnalysisTaskEmcalPIDinJet* AddTaskEmcalPIDinJet(
   jetTask->GetClusterContainer(0)->SetDefaultClusterEnergy(AliVCluster::kHadCorr);
   jetTask->GetParticleContainer(0)->SetParticlePtCut(0.15);;
   jetTask->SetHistoBins(300, 0, 300);
-  jetTask->SelectCollisionCandidates(kPhysSel);
+  jetTask->SelectCollisionCandidates(AliVEvent::kCentral);
 
-  AliJetContainer* jetCont02 = sampleTask->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.2, AliEmcalJet::kTPCfid, "Jet");
-  AliJetContainer* jetCont03 = sampleTask->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.3, AliEmcalJet::kTPCfid, "Jet");
-  AliJetContainer* jetCont04 = sampleTask->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.4, AliEmcalJet::kTPCfid, "Jet");  
+  AliJetContainer* jetCont02 = jetTask->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.2, AliEmcalJet::kTPCfid, "Jet");
+  AliJetContainer* jetCont03 = jetTask->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.3, AliEmcalJet::kTPCfid, "Jet");
+  AliJetContainer* jetCont04 = jetTask->AddJetContainer(AliJetContainer::kChargedJet, AliJetContainer::antikt_algorithm, AliJetContainer::pt_scheme, 0.4, AliEmcalJet::kTPCfid, "Jet");  
  
   jetCont02->SetPercAreaCut(0.6);
   jetCont03->SetPercAreaCut(0.6);
   jetCont04->SetPercAreaCut(0.6);
-  if (iBeamType != AliAnalysisTaskEmcal::kpp) {
+  if (iBeamType_PbPb) {
        jetCont02->SetRhoName("Rho");
        jetCont03->SetRhoName("Rho");
        jetCont04->SetRhoName("Rho");
@@ -50,6 +50,7 @@ AliAnalysisTaskEmcalPIDinJet* AddTaskEmcalPIDinJet(
   mgr->AddTask(jetTask);
     
   // Create containers for input/output
+ /*
   AliAnalysisDataContainer *cinput1  = mgr->GetCommonInputContainer()  ;
   TString contname(name);
   contname += "_histos";
@@ -58,7 +59,18 @@ AliAnalysisTaskEmcalPIDinJet* AddTaskEmcalPIDinJet(
                                                             Form("%s", AliAnalysisManager::GetCommonFileName()));
   mgr->ConnectInput  (jetTask, 0,  cinput1 );
   mgr->ConnectOutput (jetTask, 1, coutput1 );
+  */
+
+    TString containerName = mgr->GetCommonFileName();
+    containerName += ":PWGJE_PIDinJet";
+    TString SubcontainerName = Form("PIDinJet");
+    SubcontainerName += name;
+    AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
+    AliAnalysisDataContainer *coutput1 = mgr->CreateContainer(SubcontainerName, TList::Class(),AliAnalysisManager::kOutputContainer, containerName.Data());
+    mgr->ConnectInput(task, 0, cinput);
+    mgr->ConnectOutput(task, 1, coutput1); 
   
+
   return jetTask;
 } 
 
