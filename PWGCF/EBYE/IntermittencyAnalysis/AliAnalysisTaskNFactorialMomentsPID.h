6f2dae2be837431398b7e0a11a9c80821907efff
Salman Malik <salman.khurshid.malik@cern.ch>
2024-05-10 15:38:40 +0530

    codesforProtonAnalysis

---
+++ PWGCF/EBYE/IntermittencyAnalysis/AliAnalysisTaskNFactorialMomentsPID.h
@@ -0,0 +1,214 @@
+#ifndef AliAnalysisTaskNFactorialMomentsPID_H
+#define AliAnalysisTaskNFactorialMomentsPID_H
+
+#include "AliAODMCHeader.h"
+#include "AliAODMCParticle.h"
+#include "AliAnalysisTaskSE.h"
+#include "AliEventCuts.h"
+#include "AliPID.h"
+#include "AliPIDResponse.h"
+#include "AliPIDCombined.h"
+#include "TNtuple.h"
+#include "TH3F.h"
+
+const Int_t mMBins = 40, mDim = 2, mQs = 6, mPtmax = 5;
+
+class TH1F;
+class TH1D;
+class TH2D;
+class TH2F;
+class TList;
+class TNtuple;
+class TString;
+class TList;
+class AliAODEvent;
+class AliMCEvent;
+class AliPIDResponse;
+class AliPIDCombined;
+
+class AliAnalysisTaskNFactorialMomentsPID : public AliAnalysisTaskSE
+{
+ public:
+  AliAnalysisTaskNFactorialMomentsPID();
+  AliAnalysisTaskNFactorialMomentsPID(const char* name);
+  virtual ~AliAnalysisTaskNFactorialMomentsPID();
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t* option);
+  virtual void Terminate(Option_t* option);
+
+  // Analysis Setters
+  void SetVtxCut(Double_t fVx, Double_t fVy, Double_t fVz)
+  {
+    this->fVxMax = fVx;
+    this->fVyMax = fVy;
+    this->fVzMax = fVz;
+  }
+  void SetIsMC(Bool_t isMC, TString gen)
+  {
+    this->flagMC = isMC;
+    this->fGenName = gen;
+  }
+  void SetCentLim(Int_t f_minCent, Int_t f_maxCent)
+  {
+    this->minCent = f_minCent;
+    this->maxCent = f_maxCent;
+  }
+  void SetEta(Double_t f_minEta, Double_t f_maxEta)
+  {
+    this->minEta = f_minEta;
+    this->maxEta = f_maxEta;
+  }
+  void Setfbit(Int_t fb) { this->filterBit = fb; }
+  void SetYear(TString year) { this->fYear = year; }
+  void SetPSbinning(TArrayI MB, TArrayI NB, Int_t Mm)
+  {
+    this->mMBin2 = MB;
+    this->mNBin2 = NB;
+    this->Mmax = Mm;
+  }
+  void SetPtArray(TArrayD f_ptArray, Int_t nbins)
+  {
+    this->ptarray = f_ptArray;
+    this->mPtBins = nbins;
+  }
+  void SetRejectPileup(Bool_t RejectPileup) { this->flagPileup = RejectPileup; }
+  void SetBfield(Int_t bf) { this->fBfield = bf; }
+  void SetDCAXYRangeMax(Double_t dcaxy) { this->fDCAxyMax = dcaxy; }
+  void SetDCAZRangeMax(Double_t dcaz) { this->fDCAzMax = dcaz; }
+  void SetITSClusterCut(Double_t itscut) { this->fITSCls = itscut; }
+  void SetTPCClusterCut(Double_t tpccut) { this->fTPCCls = tpccut; }
+  void SetnTPCrossedrows(Double_t tpcrowcut) { this->fTPCRows = tpcrowcut; }
+  void SetSelfAffine(Bool_t selfaffine) { this->flagSelfAff = selfaffine; }
+  void SetSharedCls(Double_t fSharedCls, Double_t fSharedrows, Double_t fFindable)
+  {
+    this->fSharedClsMax = fSharedCls;
+    this->fSharedRowsMax = fSharedrows;
+    this->fFindableClsMin = fFindable;
+  }
+
+ protected:
+  AliEventCuts fEventCuts;
+  void FillTrackInfo();
+  void FillMCTrackInfo();
+  void GetPtBin(Double_t);
+  void CalculateNFMs(TH2D* h1[mPtmax][mMBins], Bool_t mcGen);
+  void DataPosting();
+  void ResetHistograms();
+
+ private:
+  // Event related (Data and MC)
+  AliAODEvent* fAOD;    // AOD object
+  AliMCEvent* fMCEvent; // MC event
+  Bool_t flagMC;        // Flag for MC
+  Bool_t flagPileup;    // Flag for pile-up rejection
+  TString fGenName;     // Generator name
+  TString fYear;        // Year of data taking
+  Int_t counter;
+  Bool_t flagSelfAff;
+  Double_t fITSCls, fTPCCls, fTPCRows;
+  Int_t fBfield;
+  Int_t mPtBins;
+
+  // Output lists
+  TList* fHistList;
+  TList* fNtupleList[mPtmax];
+  TList* fNtupleListCorr[mPtmax];
+  TList* fNtupleListGen[mPtmax];
+  TList* fNtupleListBin1; // Ntuple to store Fqes
+  TList* fNtupleListBin2; // Ntuple to store Fqes
+  TList* fNtupleListBin3; // Ntuple to store Fqes
+  TList* fNtupleListBin4;
+
+  TList* fWCNtupleListBin1; // Ntuple to store Fqes
+  TList* fWCNtupleListBin2; // Ntuple to store Fqes
+  TList* fWCNtupleListBin3; // Ntuple to store Fqes
+  TList* fWCNtupleListBin4; // Ntuple to store Fqes
+
+  // Objects retrieved from the input handler
+  AliPIDResponse* fPIDResponse;
+  AliAODMCHeader* mcHeader;
+
+  // Tuples for Output
+  TNtuple* fntpMBin[mPtmax][mMBins];     //! Tuples for output
+  TNtuple* fntpMBinCorr[mPtmax][mMBins]; //! Tuples for output
+
+  TNtuple* fntpMBinGen[mPtmax][mMBins]; //! Tuples for output
+
+  TNtuple* fWCntpMBin1[mMBins]; //!
+  TNtuple* fWCntpMBin2[mMBins]; //!
+  TNtuple* fWCntpMBin3[mMBins]; //!
+  TNtuple* fWCntpMBin4[mMBins]; //!
+
+  // Filter Bit
+  Int_t filterBit;
+
+  // Variables for analysis
+  Double_t fVxMax, fVyMax, fVzMax, minCent, maxCent, minEta, maxEta;
+  Double_t fSharedClsMax, fSharedRowsMax, fFindableClsMin;
+  float fDCAxyMax, fDCAzMax;
+  Int_t Mmax;
+  Int_t ptbin1, ptbin2, ptbin3, ptbin4;
+  TArrayD ptarray;
+  TArrayI mMBin2, mNBin2;
+  std::vector<Bool_t> ptbin; // vector of Bool_tean for pt bin
+
+  Double_t mfield;
+  Double_t nsigmaTPC[5];
+  Double_t nsigmaTOF[5];
+  AliPIDCombined* fPIDCombined;
+
+  // Histograms
+  TH2D* fHistQAPID[13];
+  TH1D* fHistPDG[2];
+  TH1F* fHistnTPCcls;
+  TH1F* fHistnTPCcrossedrows;
+  TH1F* fHistnITScls;
+  TH1F* fHistnchi2ITScls;
+  TH1F* fHistnchi2TPCcls;
+  TH1F* fHistDCAxy;
+  TH1F* fHistDCAz;
+  TH2F* fHistDCAxypT;
+  TH2F* fHistDCAzpT;
+  TH1F* fHistNShCls[2];
+  TH2F* fHistNShClsFra[2];
+  TH2F* fHistNShClsFravspt;
+  TH2F* fHistNFoundClsFra[2];
+  TH1F* fHistNFcls[2];
+  // For Data and also for Generated MC (if flagMC = true)
+  TH1F* fPtBin[mPtmax];             //! Pt spectrum
+  TH1F* fEtaBin[mPtmax];            //! Eta spectrum
+  TH1F* fPhiBin[mPtmax];            //! Phi spectrum
+  TH1F* fMultBin[mPtmax];           //! Histogram to register event multiplicity
+  TH2D* fEtaPhiBin[mPtmax][mMBins]; //! Eta-Phi for 4 different bins  distribution
+
+  TH1F* fPtBinGen[mPtmax];             //! Pt spectrum
+  TH1F* fEtaBinGen[mPtmax];            //! Eta spectrum
+  TH1F* fPhiBinGen[mPtmax];            //! Phi spectrum
+  TH1F* fMultBinGen[mPtmax];           //! Histogram to register event multiplicity
+  TH2D* fEtaPhiBinGen[mPtmax][mMBins]; //! Eta-Phi for 4 different bins  distribution
+
+  TH2D* fhMapEtaPhiBin1M[mMBins]; //!
+  TH2D* fhMapEtaPhiBin2M[mMBins]; //!
+  TH2D* fhMapEtaPhiBin3M[mMBins]; //!
+  TH2D* fhMapEtaPhiBin4M[mMBins]; //!
+
+  // QA hists
+  TH1F* fHistQAVx;     //!
+  TH1F* fHistQAVy;     //!
+  TH1F* fHistQAVz;     //!
+  TH1D* fEventCounter; //! Histogram to track events etc
+  TH1D* fTrackCounter; //! Histogram to track tracks etc
+  TH1F* fHistQACent;   //! Histogram for centrality Distribution
+  TH1F* fHistQAEta[2]; //!
+  TH1F* fHistQAPhi[2]; //!
+  Bool_t zeromult;
+
+  AliAnalysisTaskNFactorialMomentsPID(
+    const AliAnalysisTaskNFactorialMomentsPID&); // not implemented
+  AliAnalysisTaskNFactorialMomentsPID&
+    operator=(const AliAnalysisTaskNFactorialMomentsPID&); // not implemented
+
+  ClassDef(AliAnalysisTaskNFactorialMomentsPID, 1);
+};
+
+#endif
---
+++ PWGCF/EBYE/macros/AddTaskNFactorialMomentsPID.C
@@ -0,0 +1,150 @@
+#include "AliAnalysisManager.h"
+#include "AliAnalysisTaskNFactorialMomentsPID.h"
+
+AliAnalysisTaskNFactorialMomentsPID* AddTaskNFactorialMomentsPID(Int_t nPt = 4, Double_t pt1 = 0.2, Double_t pt2 = 2.0, Double_t pt3 = 0.4, Double_t pt4 = 1.0, Double_t pt5 = 0.4, Double_t pt6 = 2.0, Double_t pt7 = 0.4, Double_t pt8 = 5.0, Double_t pt9 = 0.0, Double_t pt10 = 0.0, Int_t Mmax = 82, Bool_t fIsMC = kFALSE, const char* suffix = "")
+{
+
+  // year = "2010" or "2015"
+  // Initialize variables
+  TString year = "2015";
+  Bool_t fPileup = kTRUE;
+  Double_t fEtaMin = -0.8;
+  Double_t fEtaMax = 0.8;
+  Int_t fCentMin = 0;
+  Int_t fCentMax = 5;
+  Int_t fBit = 768;
+  Double_t fVxMax = 0.3;
+  Double_t fVyMax = 0.4;
+  Double_t fVzMax = 10.0;
+  Double_t fDCAXYRangeMax = 0.0;
+  Double_t fDCAZRangeMax = 0.0;
+  Double_t fITSClusterCut = 0.0;
+  Double_t fTPCClusterCut = 0.0;
+  Double_t fnTPCrossedrows = 0.0;
+  Double_t fSharedCls = 0.0;
+  Double_t fSharedRows = 0.0;
+  Double_t fFindableCls = 0.0;
+  Int_t bField = 0;
+  Bool_t fSelfAffAnalysis = kFALSE;
+  Double_t nSigmaCutPr = 3.0;
+
+  std::vector<Double_t> ptVector;
+  if (pt1 != 0.0)
+    ptVector.push_back(pt1);
+  if (pt2 != 0.0)
+    ptVector.push_back(pt2);
+  if (pt3 != 0.0)
+    ptVector.push_back(pt3);
+  if (pt4 != 0.0)
+    ptVector.push_back(pt4);
+  if (pt5 != 0.0)
+    ptVector.push_back(pt5);
+  if (pt6 != 0.0)
+    ptVector.push_back(pt6);
+  if (pt7 != 0.0)
+    ptVector.push_back(pt7);
+  if (pt8 != 0.0)
+    ptVector.push_back(pt8);
+  if (pt9 != 0.0)
+    ptVector.push_back(pt9);
+  if (pt10 != 0.0)
+    ptVector.push_back(pt10);
+
+  Int_t fNumberofPtBins = ptVector.size() / 2;
+  std::cout << "Number of pt bins: " << fNumberofPtBins << std::endl;
+
+  if (fNumberofPtBins != nPt) {
+    std::cout << "Number of pt bins is not equal to the number of pt values" << std::endl;
+    return 0x0;
+  }
+
+  Double_t ptarr[nPt * 2];
+  std::copy(ptVector.begin(), ptVector.end(), ptarr);
+  TArrayD ptarrD(nPt * 2, ptarr);
+
+  Int_t Mbinsarr[40];
+  Int_t Nbinsarr[40]; // used in case of selfaffine analysis
+  for (size_t nMB = 0; nMB < 40; nMB++) {
+    if (Mmax == 123)
+      Mbinsarr[nMB] = 3 * (nMB + 2);
+    if (Mmax == 82)
+      Mbinsarr[nMB] = 2 * (nMB + 2);
+  }
+  if (fSelfAffAnalysis) {
+    for (Int_t nMB = 0; nMB < 40; nMB++) {
+      Mbinsarr[nMB] = nMB + 1;
+      Nbinsarr[nMB] = 4 * (nMB + 1);
+    }
+  }
+
+  TArrayI Mbins(40, Mbinsarr);
+  TArrayI Nbins(40, Nbinsarr);
+
+  AliAnalysisManager* mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    return 0x0;
+  }
+
+  // creating an instance for taskFactorialMoments
+  AliAnalysisTaskNFactorialMomentsPID* taskFactorialMoments =
+    new AliAnalysisTaskNFactorialMomentsPID(suffix);
+  if (!taskFactorialMoments)
+    return 0x0;
+
+  TString gen = "Hijing";
+
+  taskFactorialMoments->SetIsMC(fIsMC, gen);
+  taskFactorialMoments->SetYear(year);
+  taskFactorialMoments->SetPSbinning(Mbins, Nbins, Mmax);
+  taskFactorialMoments->SetRejectPileup(fPileup);
+  taskFactorialMoments->SetEta(fEtaMin, fEtaMax);
+  taskFactorialMoments->SetCentLim(fCentMin, fCentMax);
+  taskFactorialMoments->SetPtArray(ptarrD, nPt);
+  taskFactorialMoments->Setfbit(fBit);
+  taskFactorialMoments->SetBfield(bField);
+  taskFactorialMoments->SetVtxCut(fVxMax, fVyMax, fVzMax);
+  taskFactorialMoments->SetDCAXYRangeMax(fDCAXYRangeMax);   // 0.1
+  taskFactorialMoments->SetDCAZRangeMax(fDCAZRangeMax);     // 1
+  taskFactorialMoments->SetITSClusterCut(fITSClusterCut);   // chi2 per ITS 36
+  taskFactorialMoments->SetTPCClusterCut(fTPCClusterCut);   // chi2 per TPC 4
+  taskFactorialMoments->SetnTPCrossedrows(fnTPCrossedrows); // 70
+  taskFactorialMoments->SetSelfAffine(fSelfAffAnalysis);
+  taskFactorialMoments->SetSharedCls(fSharedCls, fSharedRows, fFindableCls);
+
+  // add your taskFactorialMoments to the manager
+  mgr->AddTask(taskFactorialMoments);
+
+  // input for taskFactorialMoments and connecting containers to it
+  Int_t nout = (fNumberofPtBins) + 1 + fIsMC * (fNumberofPtBins);
+  AliAnalysisDataContainer* coutputlist[nout];
+
+  TString str;
+  if (fIsMC)
+    str = Form("list_MC_%dpt_%d-%dcent_FB%d_%s_%s", fNumberofPtBins, fCentMin, fCentMax, fBit, year.Data(), suffix);
+  else
+    str = Form("list_DATA_%dpt_%d-%dcent_FB%d_%s_%s", fNumberofPtBins, fCentMin, fCentMax, fBit, year.Data(), suffix);
+  coutputlist[0] = mgr->CreateContainer(
+    str, TList::Class(), AliAnalysisManager::kOutputContainer,
+    AliAnalysisManager::GetCommonFileName());
+
+  char* name = "ntplist";
+  if (fIsMC)
+    name = "ntplistRe";
+  for (Int_t nbins = 0; nbins < fNumberofPtBins; nbins++) {
+    coutputlist[nbins + 1] =
+      mgr->CreateContainer(Form("%s%d%s", name, nbins + 1, suffix), TList::Class(),
+                           AliAnalysisManager::kOutputContainer,
+                           AliAnalysisManager::GetCommonFileName());
+    if (fIsMC)
+      coutputlist[nbins + fNumberofPtBins + 1] =
+        mgr->CreateContainer(Form("ntplistGen%d%s", nbins + 1, suffix), TList::Class(),
+                             AliAnalysisManager::kOutputContainer,
+                             AliAnalysisManager::GetCommonFileName());
+  }
+
+  mgr->ConnectInput(taskFactorialMoments, 0, mgr->GetCommonInputContainer());
+  for (Int_t i = 0; i < nout; i++) {
+    mgr->ConnectOutput(taskFactorialMoments, i + 1, coutputlist[i]);
+  }
+  return taskFactorialMoments;
+}
