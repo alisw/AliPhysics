0b4a79c7c4049d5f9c155992877103b478f30601
dsarkar <60989535+dsarkaralice@users.noreply.github.com>
2024-05-10 15:16:47 +0200

    use pos and neg rap tracks for Nch estimation

--- PWGCF/FLOW/GF/AliAnalysisTaskCorrForFlowFMD.cxx
+++ PWGCF/FLOW/GF/AliAnalysisTaskCorrForFlowFMD.cxx
@@ -49,6 +49,8 @@ AliAnalysisTaskCorrForFlowFMD::AliAnalysisTaskCorrForFlowFMD() : AliAnalysisTask
     fshiftphi_PHI(kFALSE),
     fshiftrap_PHI(kFALSE),
     fUseNch_reco(kFALSE),
+    fUseNch_posrap_tracks(kFALSE),
+    fUseNch_negrap_tracks(kFALSE),
     fUseNch_truth(kFALSE),
     fUseNchfor_eventmixing(kFALSE),
     fUseFMDtrkfor_eventmixing(kFALSE),
@@ -179,6 +181,8 @@ AliAnalysisTaskCorrForFlowFMD::AliAnalysisTaskCorrForFlowFMD(const char* name, B
     fshiftphi_PHI(kFALSE),
     fshiftrap_PHI(kFALSE),
     fUseNch_reco(kFALSE),
+    fUseNch_posrap_tracks(kFALSE),
+    fUseNch_negrap_tracks(kFALSE),
     fUseNch_truth(kFALSE),										     
     fUseNchfor_eventmixing(kFALSE),
     fUseFMDtrkfor_eventmixing(kFALSE),
@@ -1819,6 +1823,9 @@ Bool_t AliAnalysisTaskCorrForFlowFMD::PrepareTPCTracks(){
   if(!fTracksAss || !fTracksTrig[0] || !fhTrigTracks[0]) {AliError("Cannot prepare TPC tracks!"); return kFALSE; }
 
   Double_t fNofTracks_reco = 0;
+  Double_t fNofTracks_reco_posrap = 0;
+  Double_t fNofTracks_reco_negrap = 0;
+	
   Double_t binscont[3] = {fPVz, fSampleIndex, 0.};
 
   TObjArray* fTracksJets = nullptr;
@@ -1838,6 +1845,10 @@ Bool_t AliAnalysisTaskCorrForFlowFMD::PrepareTPCTracks(){
 	       if(trkEff < 0.001) continue;
 	      }
 	      fNofTracks_reco += 1.0/trkEff;
+      if(track->Eta() > 0.01)  fNofTracks_reco_posrap += 1.0/trkEff;
+      if(track->Eta() < -0.01) fNofTracks_reco_negrap += 1.0/trkEff;
+
+	      
         if(fAnalType == eFMDAFMDC || fIsTPCgen) continue;
         if(fAnalType == eTPCTPC) fTracksAss->Add((AliAODTrack*)track); 
       }
@@ -1893,6 +1904,9 @@ Bool_t AliAnalysisTaskCorrForFlowFMD::PrepareTPCTracks(){
 
   if(fUseNch_reco){
     fNofTracks = fNofTracks_reco;
+  if(fUseNch_posrap_tracks)     fNofTracks = fNofTracks_reco_posrap;
+  if(fUseNch_negrap_tracks)     fNofTracks = fNofTracks_reco_negrap;
+	  
     if(fNofTracks < fNchMin || fNofTracks > fNchMax) { return kFALSE; }
     fhEventCounter->Fill("Nch cut ok ",1);
   }
