da8b340bc83534545d4d2af1d49078fe88ffc40e
Lars <146946151+ljoergen@users.noreply.github.com>
2024-05-23 18:34:06 +0200

    Linked new class in PWGJEEMCALJetTasksLinkDef.h

--- PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
+++ PWGJE/EMCALJetTasks/PWGJEEMCALJetTasksLinkDef.h
@@ -341,6 +341,7 @@
 #pragma link C++ class AliAnalysisTaskEmcalJetCDF+; 
 #pragma link C++ class AliAnalysisTaskEmcalPIDinJet+;
 #pragma link C++ class AliAnalysisTaskEmcalJetValidation+;
+#pragma link C++ class AliAnalysisTaskJetAngCorrelations+;
 #pragma link C++ namespace PWGJE::EMCALJetTasks::AliAnalysisTaskEmcalJetCDF_NS;
 #ifdef WITH_ROOUNFOLD
 // Classes which need direct access to both Fastjet and RooUnfold objects
