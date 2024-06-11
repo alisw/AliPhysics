21a2896cbf00e93752ed38e37d3771d09268ab6e
Daniela Ruggiano <daniela.ruggiano@cern.ch>
2024-06-11 23:53:52 +0200

    efficiency for systematic checks

--- PWGCF/FEMTOSCOPY/AliFemtoUser/AliAnalysisEffTaskExclusivePID.h
+++ PWGCF/FEMTOSCOPY/AliFemtoUser/AliAnalysisEffTaskExclusivePID.h
@@ -1,8 +1,6 @@
 #ifndef ALIANALYSISEFFTASKEXCLUSIVEPID
 #define ALIANALYSISEFFTASKEXCLUSIVEPID
 
-
-
 #define MULTBINS 1
 #define PARTTYPES 6
 
@@ -23,32 +21,10 @@ class AliAnalysisEffTaskExclusivePID :public AliAnalysisTaskSE{
   enum PidMethod {kNSigma=0, kNSigmaNoDoubleCounting=1, kExclusivePID=2, kExclusivePIDDiffRejection=3};
   typedef enum PidMethod PidMethod;
 
-AliAnalysisEffTaskExclusivePID();
-/*AliAnalysisEffTaskFilterBit768() :  AliAnalysisTaskSE(), centrality(0), fHistoList(0),  fMassInvLambdaPass(0),fMassInvAntiLambdaPass(0), fMassInvLambdaFail(0), fMassInvAntiLambdaFail(0),fYLambda(0),fPtLambda(0), fYAntiLambda(0),fPtAntiLambda(0), fCutsLambda(0), fCutsAntiLambda(0), fTruePtLambdaMC(0), fRecPtLambdaMC(0), fTruePtAntiLambdaMC(0),fRecPtAntiLambdaMC(0), fMassInvXimPass(0),fMassInvXipPass(0), fMassInvXimFail(0), fMassInvXipFail(0),fYXim(0),fPtXim(0), fYXip(0),fPtXip(0), fCutsXim(0), fCutsXip(0), recoParticleArrayXi(0), fTruePtXimMC(0), fRecPtXimMC(0), fTruePtXipMC(0),fRecPtXipMC(0), fDCAtoPrimVtx(0), fIfAliEventCuts(kFALSE), fFB(768), fPidMethod(kNSigma), fEstEventMult(kRefMult), fIfXiAnalysis(kFALSE), fpidResponse(0), fAODpidUtil(0), fEventCuts(0), fTrackPileUpRemoval(kFALSE), fV0PileUpRemoval(kFALSE)
 
-    {
+ AliAnalysisEffTaskExclusivePID();
 
-      for(Int_t i = 0; i < MULTBINS*PARTTYPES; i++)  {
-	for(Int_t chg=0;chg<2;chg++){
-	  fGeneratedMCPrimaries[i][chg] = NULL;
-	  fMCPrimariesThatAreReconstructed[i][chg] = NULL;
-	  fGeneratedMCPrimaries4D[i][chg] = NULL;
-	  fMCPrimariesThatAreReconstructed4D[i][chg] = NULL;
-	  fMCPrimariesThatAreReconstructedNoNsigma[i][chg] = NULL;
-	  fReconstructedAfterCuts[i][chg] = NULL;
-	  fReconstructedNotPrimaries[i][chg] = NULL;
-	  fReconstructedPrimaries[i][chg] = NULL;
-	  fContamination[i][chg] = NULL;
-	}
-      }
-
-      for ( Int_t i = 0; i < 11; i++) {
-	fHistQA[i] = NULL;
-	if(i<3) fHistQA2D[i] = NULL;
-      }
-    }
-*/
-  AliAnalysisEffTaskExclusivePID(TString name, int pidMethod=2, int filterbit=96); // default constructor
+  AliAnalysisEffTaskExclusivePID(TString name, int pidMethod=2, int filterbit=128); // default constructor
   virtual ~AliAnalysisEffTaskExclusivePID(); // default destructor
   virtual void UserCreateOutputObjects(); // user create output objects
   virtual void UserExec(Option_t *option); // user exec
@@ -78,7 +54,7 @@ AliAnalysisEffTaskExclusivePID();
   TList *fHistoList; // histo list
   //TClonesArray *arrayMC;
   TH1F *fHistEv[4];
-
+  TH1F *fHistP[4];
   TH1F *fHistQA[11];
   TH2F *fHistQA2D[3];
   TH2F *fHistQAPID[5][PARTTYPES][2];
@@ -88,6 +64,7 @@ AliAnalysisEffTaskExclusivePID();
   TH2F *fOriginLambdas[5][2];
   TH2F *fHistQAXi[2];
   TH2F *fOriginXi[5][2];
+  
 
   //TObjArray *recoParticleArray;
   TH2F *fGeneratedMCPrimaries[MULTBINS*PARTTYPES][2];
@@ -185,5 +162,4 @@ AliAnalysisEffTaskExclusivePID();
   
 
 };
-
 #endif /* ALIANALYSISTASKPARTICLEEFFICIENCYDY */
