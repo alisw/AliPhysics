2071a83e443da9a76e5bcc11d12eddcbbb59d56d
bushali <bushra.ali@cern.ch>
2024-08-02 13:06:52 +0200

    Task improved for p_T ranges

--- PWGCF/EBYE/MeanPtFluctuations/pp13TeV_MB/AliAnalysisMCMeanPt.h
+++ PWGCF/EBYE/MeanPtFluctuations/pp13TeV_MB/AliAnalysisMCMeanPt.h
@@ -26,113 +26,119 @@ class TObjArray;
 #include "TRandom.h"
 #include "TTreeStream.h"
 
-
 class AliAnalysisMCMeanPt : public AliAnalysisTaskSE
 {
 public:
-    AliAnalysisMCMeanPt();
-    AliAnalysisMCMeanPt(const char *name);
-    virtual                 ~AliAnalysisMCMeanPt();
+	AliAnalysisMCMeanPt();
+	AliAnalysisMCMeanPt(const char *name);
+	virtual ~AliAnalysisMCMeanPt();
 
-    virtual void            UserCreateOutputObjects();
-    virtual void            UserExec(Option_t* option);
-    virtual void            Terminate(Option_t* option);
-    AliEventCuts 	        fEventCuts;                   // Event cuts
+	virtual void UserCreateOutputObjects();
+	virtual void UserExec(Option_t *option);
+	virtual void Terminate(Option_t *option);
+	AliEventCuts fEventCuts; //! Event cuts
 
-//===added for systematics===========
-    void                    SetMaxTPCCluster(int MaxTPCclus){fCutTPCMaxCls	=MaxTPCclus;}
-    void                    SetNTPCCluster(int TPCNclus)	{fCutTPCNCls	=TPCNclus;}
-    void                    Setzvtxcut(float zvtxcut1, float zvtxcut2)			{fzvtxcutmin		=zvtxcut1; fzvtxcutmax		=zvtxcut2;};
-    void                    SettrackBit(int trackBit)		{ftrackBit		=trackBit;};
-
+	//===added for systematics===========
+	void SetMaxTPCCluster(int MaxTPCclus) { fCutTPCMaxCls = MaxTPCclus; }
+	void SetNTPCCluster(int TPCNclus) { fCutTPCNCls = TPCNclus; }
+	void Setzvtxcut(float zvtxcut1, float zvtxcut2)
+	{
+		fzvtxcutmin = zvtxcut1;
+		fzvtxcutmax = zvtxcut2;
+	};
+	void SettrackBit(int trackBit) { ftrackBit = trackBit; };
+	void Setptrange(float pt_min, float pt_max)
+	{
+		ptmin = pt_min;
+		ptmax = pt_max;
+	}
 
 private:
-    	Bool_t AcceptTrack(AliAODTrack* aodtrack) const;
+	Bool_t AcceptTrack(AliAODTrack *aodtrack) const;
 
-	    int                     fAnalysisType;
+	int fAnalysisType;
 
-//===============
-    float                   fzvtxcutmin;
-     float                   fzvtxcutmax;
-   int                   ftrackBit;
-    int                   fCutTPCMaxCls;
-    int                   fCutTPCNCls;
-//===============
+	//===============
+	float fzvtxcutmin;
+	float fzvtxcutmax;
+	int ftrackBit;
+	int fCutTPCMaxCls;
+	int fCutTPCNCls;
+	float ptmin;
+	float ptmax;
+	//===============
 
-        AliAODEvent*            fAOD;           //! input event
-        TList*                  fOutputList;    //! output list
-        TClonesArray*			AODMCTrackArray;
+	AliAODEvent *fAOD;	//! input event
+	TList *fOutputList; //! output list
+	TClonesArray *AODMCTrackArray;
 
-		TTree					*fTreeFB768rec;//!
-		TTree					*fTreeFB768gen;//!
-		float 		        	pt[5000];//!
-		float 		        	phi[5000];//!
-		float 		        	eta[5000];//!
-		float 					cent;//!
-		float					field;//!
-		int 					charge[5000];//!
-		int						TPCrows[500];//!
-		int						nevt;//!
-		int						ns;//!
-		int						isample;//!
-        int						fRunNumber;//!
-		int 					Vz;//!
+	TTree *fTreeFB768rec; //!
+	TTree *fTreeFB768gen; //!
+	float pt[5000];		  //!
+	float phi[5000];	  //!
+	float eta[5000];	  //!
+	float cent;			  //!
+	float field;		  //!
+	int charge[5000];	  //!
+	int TPCrows[500];	  //!
+	int nevt;			  //!
+	int ns;				  //!
+	int isample;		  //!
+	int fRunNumber;		  //!
+	int Vz;				  //!
 
+	TH1I *fEventCount; //!
+	TH1F *fCentMC;	   //!
+	TH1F *fHistPtgen;  //!
+	TH1F *fHistPtrec;  //!
 
-   		TH1I					*fEventCount;//!
-        TH1F                    *fCentMC;//!
-		TH1F					*fHistPtgen;//!
-		TH1F					*fHistPtrec;//!
+	THnSparse *fTHnfcentetaptphi;	 //!
+	THnSparse *fTHnfcentetaptphigen; //!
+	THnSparse *fTHnfnch;			 //!
+	THnSparse *fTHnfnchP;			 //!
 
-		THnSparse				*fTHnfcentetaptphi;//!
-		THnSparse				*fTHnfcentetaptphigen;//!
-		THnSparse 				*fTHnfnch;//!
-		THnSparse 				*fTHnfnchP;//!
+	TH1D *fHistVx;	  //! Vx hist
+	TH1D *fHistVy;	  //! Vy hist
+	TH1D *fHistVz;	  //! Vz hist
+	TH1D *fHistVzcut; //! Vz hist
 
-   		TH1D                    *fHistVx; //! Vx hist
-    	TH1D                    *fHistVy; //! Vy hist
-    	TH1D                    *fHistVz; //! Vz hist
-    	TH1D                    *fHistVzcut; //! Vz hist
+	TH2D *hsec_wd;	//!
+	TH2D *hsec_mat; //!
+	TH2D *hsec;		//!
+	TH2D *hrec;		//!
+	TH2D *hprim;	//!
+	TH2D *hgen;		//!//!
+	TH2D *hprimgen; //!
 
-		TH2D					*hsec_wd;//!
-		TH2D					*hsec_mat;//!
-		TH2D					*hsec;//!
-		TH2D					*hrec;//!
-		TH2D           			*hprim;//!
-		TH2D					*hgen;//!//!
-		TH2D           			*hprimgen;//!
+	TH1F *fHistEta_rec; //!
+	TH1F *fHistPt_rec;	//!
+	TH1F *fHistPhi_rec; //!
+	TH1F *fHistEta_gen; //!
+	TH1F *fHistPt_gen;	//!
+	TH1F *fHistPhi_gen; //!
 
-        TH1F                    *fHistEta_rec;//!
-        TH1F                    *fHistPt_rec;//!
-        TH1F                    *fHistPhi_rec;//!
-		TH1F                    *fHistEta_gen;//!
-        TH1F                    *fHistPt_gen;//!
-        TH1F                    *fHistPhi_gen;//!
+	TH2D *hScale_gen;		 //!
+	TH2D *hTracks_gen;		 //!
+	TH2D *hTracksq_gen;		 //!
+	TH2D *hTrackavgpt_gen;	 //!
+	TH2D *hTrackpair_gen;	 //!
+	TH2D *hTrackavgptsq_gen; //!
 
-		TH2D 					*hScale_gen;//!
-		TH2D 					*hTracks_gen;//!
-		TH2D 					*hTracksq_gen;//!
-		TH2D					*hTrackavgpt_gen;//!
-		TH2D					*hTrackpair_gen;//!
-		TH2D					*hTrackavgptsq_gen;//!
+	TH2D *hScale_rec;		 //!
+	TH2D *hTracks_rec;		 //!
+	TH2D *hTracksq_rec;		 //!
+	TH2D *hTrackavgpt_rec;	 //!
+	TH2D *hTrackpair_rec;	 //!
+	TH2D *hTrackavgptsq_rec; //!
 
-		TH2D 					*hScale_rec;//!
-		TH2D 					*hTracks_rec;//!
-		TH2D 					*hTracksq_rec;//!
-		TH2D					*hTrackavgpt_rec;//!
-		TH2D					*hTrackpair_rec;//!
-		TH2D					*hTrackavgptsq_rec;//!
+	TH2D *hTracksumpt_rec;	   //!
+	TH2D *hTracksumptiptj_rec; //!
+	TH2D *hTracksumpt_gen;	   //!
+	TH2D *hTracksumptiptj_gen; //!
 
-		TH2D					*hTracksumpt_rec;//!
-		TH2D					*hTracksumptiptj_rec;//!
-		TH2D					*hTracksumpt_gen;//!
-		TH2D					*hTracksumptiptj_gen;//!
-
-
-    AliAnalysisMCMeanPt(const AliAnalysisMCMeanPt&);
-    AliAnalysisMCMeanPt& operator=(const AliAnalysisMCMeanPt&);
-    ClassDef(AliAnalysisMCMeanPt, 1);
+	AliAnalysisMCMeanPt(const AliAnalysisMCMeanPt &);
+	AliAnalysisMCMeanPt &operator=(const AliAnalysisMCMeanPt &);
+	ClassDef(AliAnalysisMCMeanPt, 1);
 };
-
 
 #endif
--- PWGCF/EBYE/macros/AddMCMeanPt.C
+++ PWGCF/EBYE/macros/AddMCMeanPt.C
@@ -1,34 +1,43 @@
-AliAnalysisMCMeanPt* AddMCMeanPt(float zvtxcut1     = -10, float zvtxcut2     = 10, int trackBit    = 768, int MaxTPCclus  = 70., TString suffixName = "")
+AliAnalysisMCMeanPt *AddMCMeanPt(
+    int trackBit = 768,
+    float zvtxcut1 = -10,
+    float zvtxcut2 = 10,
+    float pt_min = 0.15,
+    float pt_max = 1.0,
+    int MaxTPCclus = 70.,
+    TString period = "def",
+    TString suffixName = "a")
 {
     // an instance of the class to call the function
     AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
-    if (!mgr) {
+    if (!mgr)
+    {
         return 0x0;
     }
 
- 	TString TaskMCMeanPt;
-  	TaskMCMeanPt.Form("Taskfor_%d_%s", trackBit, suffixName.Data());
-  	AliAnalysisMCMeanPt *task = new AliAnalysisMCMeanPt(TaskMCMeanPt);
-    	if(!task) return 0x0;
+    TString TaskMCMeanPt;
+    TaskMCMeanPt.Form("Taskfor_%d_%s", trackBit, suffixName.Data());
+    AliAnalysisMCMeanPt *task = new AliAnalysisMCMeanPt(TaskMCMeanPt);
+    if (!task)
+        return 0x0;
 
     task->Setzvtxcut(zvtxcut1, zvtxcut2);
     task->SettrackBit(trackBit);
     task->SetMaxTPCCluster(MaxTPCclus);
-    //task->SetNTPCCluster(TPCNclus);
-    mgr->AddTask(task);
-    
-    TString finDirname      =  suffixName.Data();
+    task->Setptrange(pt_min, pt_max);
 
-    TString fileName = mgr->GetCommonFileName(); 
+    mgr->AddTask(task);
+
+    TString finDirname = suffixName.Data();
+    TString fileName = mgr->GetCommonFileName();
 
     printf("container name: %s\n", fileName.Data());
     printf("file name: %s\n", finDirname.Data());
 
-    mgr->ConnectInput(task,0,mgr->GetCommonInputContainer());
+    mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
 
     // same for the output
-    mgr->ConnectOutput(task,1,mgr->CreateContainer(Form("%s_fb%d_vz%3.2f_tpc%d_fOutputList",finDirname.Data(), trackBit, zvtxcut2, MaxTPCclus), TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
+    mgr->ConnectOutput(task, 1, mgr->CreateContainer(Form("fList_%s_MC_FB%d_%s", period.Data(), trackBit, suffixName.Data()), TList::Class(), AliAnalysisManager::kOutputContainer, fileName.Data()));
 
     return task;
-
 }
