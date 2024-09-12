138cfa332de60ef04c7a5030b095ea48bcf72593
Markus Fasel <markus.fasel@cern.ch>
2024-09-12 13:39:47 +0200

    Fill histograms for different hybrid track types

--- PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalQoverPtShift.cxx
+++ PWGJE/EMCALJetTasks/Tracks/AliAnalysisTaskEmcalQoverPtShift.cxx
@@ -84,9 +84,17 @@ void AliAnalysisTaskEmcalQoverPtShift::UserCreateOutputObjects() {
   std::array<std::string, 2> chargetypes = {{"pos", "neg"}};
   for(auto charge : chargetypes) {
     fHistos->CreateTH2(Form("fHistShift%s", charge.data()), Form("Pt-shift for charge %s; p_{t}^{orig} (GeV/c); p_{t}^{shift}", charge.data()), 300, 0., 300, 300, 0., 300.);  
+    fHistos->CreateTH2(Form("fHistShift%sGlobal", charge.data()), Form("Pt-shift for charge %s (glob. tracks); p_{t}^{orig} (GeV/c); p_{t}^{shift}", charge.data()), 300, 0., 300, 300, 0., 300.);  
+    fHistos->CreateTH2(Form("fHistShift%sComplementary", charge.data()), Form("Pt-shift for charge %s (comp. tracks); p_{t}^{orig} (GeV/c); p_{t}^{shift}", charge.data()), 300, 0., 300, 300, 0., 300.);  
     fHistos->CreateTH2(Form("fHistScanShift%s", charge.data()), Form("%s charged particle spectrum as function of q/pt shift; q/pt shift; pt", charge.data()), fQoverPtShiftScanBins, fQoverPtShiftScanMin, fQoverPtShiftScanMax, 300, 0., 300);
+    fHistos->CreateTH2(Form("fHistScanShift%sGlobal", charge.data()), Form("%s charged particle spectrum as function of q/pt shift (glob. tracks); q/pt shift; pt", charge.data()), fQoverPtShiftScanBins, fQoverPtShiftScanMin, fQoverPtShiftScanMax, 300, 0., 300);
+    fHistos->CreateTH2(Form("fHistScanShift%sComplementary", charge.data()), Form("%s charged particle spectrum as function of q/pt shift (comp. tracks); q/pt shift; pt", charge.data()), fQoverPtShiftScanBins, fQoverPtShiftScanMin, fQoverPtShiftScanMax, 300, 0., 300);
     fHistos->CreateTH2(Form("fHistShift%sEMCAL", charge.data()), Form("Pt-shift for charge %s (EMCAL acceptance); p_{t}^{orig} (GeV/c); p_{t}^{shift}", charge.data()), 300, 0., 300, 300, 0., 300.);  
+    fHistos->CreateTH2(Form("fHistShift%sGlobalEMCAL", charge.data()), Form("Pt-shift for charge %s (glob. tracks, EMCAL acceptance); p_{t}^{orig} (GeV/c); p_{t}^{shift}", charge.data()), 300, 0., 300, 300, 0., 300.);  
+    fHistos->CreateTH2(Form("fHistShift%sComplementaryEMCAL", charge.data()), Form("Pt-shift for charge %s (comp. tracks, EMCAL acceptance); p_{t}^{orig} (GeV/c); p_{t}^{shift}", charge.data()), 300, 0., 300, 300, 0., 300.);  
     fHistos->CreateTH2(Form("fHistScanShift%sEMCAL", charge.data()), Form("%s charged particle spectrum as function of q/pt shift (EMCAL acceptance); q/pt shift; pt", charge.data()), fQoverPtShiftScanBins, fQoverPtShiftScanMin, fQoverPtShiftScanMax, 300, 0., 300);
+    fHistos->CreateTH2(Form("fHistScanShift%sGlobalEMCAL", charge.data()), Form("%s charged particle spectrum as function of q/pt shift (glob. tracks, EMCAL acceptance); q/pt shift; pt", charge.data()), fQoverPtShiftScanBins, fQoverPtShiftScanMin, fQoverPtShiftScanMax, 300, 0., 300);
+    fHistos->CreateTH2(Form("fHistScanShift%sComplementaryEMCAL", charge.data()), Form("%s charged particle spectrum as function of q/pt shift (comp. tracks, EMCAL acceptance); q/pt shift; pt", charge.data()), fQoverPtShiftScanBins, fQoverPtShiftScanMin, fQoverPtShiftScanMax, 300, 0., 300);
   }
 
   fQoverPtShiftScanBinCenters.clear();
@@ -110,24 +118,64 @@ Bool_t AliAnalysisTaskEmcalQoverPtShift::Run() {
   TH2 *histScanPos = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftpos")),
       *histScanNeg = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftneg")),
       *histScanPosEMCAL = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftposEMCAL")),
-      *histScanNegEMCAL = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftnegEMCAL"));
-
-  for(auto trk : tracks->accepted()) {
+      *histScanNegEMCAL = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftnegEMCAL")),
+      *histScanPosGlobal = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftposGlobal")),
+      *histScanNegGlobal = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftnegGlobal")),
+      *histScanPosEMCALGlobal = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftposGlobalEMCAL")),
+      *histScanNegEMCALGlobal = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftnegGlobalEMCAL")),
+      *histScanPosComplementary = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftposComplementary")),
+      *histScanNegComplementary = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftnegComplementary")),
+      *histScanPosEMCALComplementary = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftposComplementaryEMCAL")),
+      *histScanNegEMCALComplementary = static_cast<TH2 *>(fHistos->GetListOfHistograms()->FindObject("fHistScanShiftnegComplementaryEMCAL"));
+  auto trackiter = tracks->accepted();
+  for(auto trkit = trackiter.begin(); trkit != trackiter.end(); ++trkit) {
+    auto trk = *trkit;
+    auto tracktype = tracks->GetTrackType(trkit.current_index());
+    bool globaltrack = tracktype == 0, 
+         comptrack = tracktype == 1;
     Double_t chargeval = trk->Charge() > 0 ? 1 : -1;
     std::string chargestring = chargeval > 0 ? "pos" : "neg";
     bool emcalacceptance = std::abs(trk->Eta()) < 0.7 && trk->Phi() > fGeom->GetArm1PhiMin() && trk->Phi() < fGeom->GetEMCALPhiMax();
     Double_t ptorig = abs(trk->Pt());
     Double_t ptshift = getShiftedPt(ptorig, fQOverPtShift, chargeval);
     fHistos->FillTH2(Form("fHistShift%s", chargestring.data()), ptorig, ptshift);
+    if(globaltrack) {
+      fHistos->FillTH2(Form("fHistShift%sGlobal", chargestring.data()), ptorig, ptshift);
+    }
+    if(comptrack) {
+      fHistos->FillTH2(Form("fHistShift%sComplementary", chargestring.data()), ptorig, ptshift);
+    }
     if(emcalacceptance) {
       fHistos->FillTH2(Form("fHistShift%sEMCAL", chargestring.data()), ptorig, ptshift);
+      if(globaltrack) {
+        fHistos->FillTH2(Form("fHistShift%sGlobalEMCAL", chargestring.data()), ptorig, ptshift);
+      }
+      if(comptrack) {
+        fHistos->FillTH2(Form("fHistShift%sComplementaryEMCAL", chargestring.data()), ptorig, ptshift);
+      }
     }
     auto scanhist = chargeval > 0 ? histScanPos : histScanNeg,
-         scanhistEMCAL = chargeval > 0 ? histScanPosEMCAL : histScanNegEMCAL;
+         scanhistEMCAL = chargeval > 0 ? histScanPosEMCAL : histScanNegEMCAL,
+         scanhistGlobal = chargeval > 0 ? histScanPosGlobal : histScanNegGlobal,
+         scanhistComplementary = chargeval > 0 ? histScanPosComplementary : histScanNegComplementary,
+         scanhistEMCALGlobal = chargeval > 0 ? histScanPosEMCALGlobal : histScanNegEMCALGlobal,
+         scanhistEMCALComplementary = chargeval > 0 ? histScanPosEMCALComplementary : histScanNegEMCALComplementary;
     for(auto currentshift : fQoverPtShiftScanBinCenters) {
       scanhist->Fill(currentshift, getShiftedPt(ptorig, currentshift, chargeval));
+      if(globaltrack) {
+        scanhistGlobal->Fill(currentshift, getShiftedPt(ptorig, currentshift, chargeval));
+      }
+      if(comptrack) {
+        scanhistComplementary->Fill(currentshift, getShiftedPt(ptorig, currentshift, chargeval));
+      }
       if(emcalacceptance) {
         scanhistEMCAL->Fill(currentshift, getShiftedPt(ptorig, currentshift, chargeval));
+        if(globaltrack) {
+          scanhistEMCALGlobal->Fill(currentshift, getShiftedPt(ptorig, currentshift, chargeval));
+        }
+        if(comptrack) {
+          scanhistEMCALComplementary->Fill(currentshift, getShiftedPt(ptorig, currentshift, chargeval));
+        }
       }
     }
   }
