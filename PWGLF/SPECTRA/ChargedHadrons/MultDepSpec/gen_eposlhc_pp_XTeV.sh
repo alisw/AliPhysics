af4a8c84ce46bca8f8e3410ecc3569a77c8d37bb
Mario Krüger <mario.kruger@cern.ch>
2024-10-07 13:03:08 +0200

    fix unit

--- PWGLF/SPECTRA/ChargedHadrons/MultDepSpec/gen_eposlhc_pp_XTeV.sh
+++ PWGLF/SPECTRA/ChargedHadrons/MultDepSpec/gen_eposlhc_pp_XTeV.sh
@@ -2,12 +2,12 @@
 set -e
 
 if [[ $# -lt 2 ]]; then
-    echo "You must specify at least where to write output to as first parameter and the center-of-mass energy in TeV as second parameter."
+    echo "You must specify at least where to write output to as first parameter and the center-of-mass energy in GeV as second parameter."
     exit -1
 fi
-beamErg=$(($2 * 1000 / 2))
+beamErg=$(($2 / 2))
 
-echo "running in `pwd`, writing HepMC to $1, pp collisions with center-of-mass energy $2 TeV"
+echo "running in `pwd`, writing HepMC to $1, pp collisions with center-of-mass energy $2 GeV"
 
 # prepare environment
 source /cvmfs/alice.cern.ch/etc/login.sh
