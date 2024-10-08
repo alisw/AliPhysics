0adad93bea1b1db05882288b411bed77f4a4a946
Abhishek Nath <aabhishek.naath@gmail.com>
2024-10-08 04:16:23 +0200

    ML4PCM: TPC dEdX in XGBTree

--- PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.cxx
+++ PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.cxx
@@ -2926,7 +2926,7 @@ void AliAnalysisTaskGammaConvV1::ProcessPhotonCandidates()
       fDeDx_ITS_EPos_PhotonML = pidResonse->NumberOfSigmasITS(EPosTrack,AliPID::kElectron);
       fTOF_EPos_PhotonML = pidResonse->NumberOfSigmasTOF(EPosTrack,AliPID::kElectron);
       if (fIsMC)  {fDeDx_TPC_EPos_PhotonML = pidResonse->NumberOfSigmasTPC(EPosTrack,AliPID::kElectron);
-      } else  {fDeDx_TPC_EPos_PhotonML =  fiPhotonCut->GetCorrectedElectronTPCResponse(EPosTrack->Charge(),pidResonse->NumberOfSigmasTPC(ENegTrack,AliPID::kElectron), EPosTrack->P(), EPosTrack->Eta(), EPosTrack->GetTPCNcls(),PhotonCandidate->GetConversionRadius() ); }
+      } else  {fDeDx_TPC_EPos_PhotonML =  fiPhotonCut->GetCorrectedElectronTPCResponse(EPosTrack->Charge(),pidResonse->NumberOfSigmasTPC(EPosTrack,AliPID::kElectron), EPosTrack->P(), EPosTrack->Eta(), EPosTrack->GetTPCNcls(),PhotonCandidate->GetConversionRadius() ); }
 
       fDeDx_PiP_ITS_PhotonML =  pidResonse->NumberOfSigmasITS(EPosTrack,AliPID::kPion);
       fDeDx_PiP_TPC_PhotonML =  pidResonse->NumberOfSigmasTPC(EPosTrack,AliPID::kPion);
--- PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.h
+++ PWGGA/GammaConv/AliAnalysisTaskGammaConvV1.h
@@ -516,7 +516,7 @@ class AliAnalysisTaskGammaConvV1 : public AliAnalysisTaskSE {
 
     AliAnalysisTaskGammaConvV1(const AliAnalysisTaskGammaConvV1&); // Prevent copy-construction
     AliAnalysisTaskGammaConvV1 &operator=(const AliAnalysisTaskGammaConvV1&); // Prevent assignment
-    ClassDef(AliAnalysisTaskGammaConvV1, 66);
+    ClassDef(AliAnalysisTaskGammaConvV1, 67);
 };
 
 #endif
