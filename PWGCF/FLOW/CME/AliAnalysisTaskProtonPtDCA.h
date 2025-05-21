bedd0db1493cc53291be8f9f00adc07eb12a81d6
ChunzhengLab <chunzheng.wang@icloud.com>
2025-05-21 09:33:49 +0200

    add feeddown study for cve analysis

---
+++ PWGCF/FLOW/CME/AliAnalysisTaskProtonPtDCA.h
@@ -0,0 +1,111 @@
+#ifndef AliAnalysisTaskProtonPtDCA_cxx
+#define AliAnalysisTaskProtonPtDCA_cxx
+#include <TH3.h>
+#include <memory>
+#include "AliAnalysisTaskSE.h"
+#include "AliAODTrack.h"
+#include "AliEventCuts.h"
+#include "AliPIDResponse.h"
+#include "AliMultSelection.h"
+#include "TList.h"
+#include "TProfile.h"
+
+class AliAnalysisTaskProtonPtDCA : public AliAnalysisTaskSE
+{
+ public:
+  AliAnalysisTaskProtonPtDCA();
+  AliAnalysisTaskProtonPtDCA(const char* name);
+  virtual ~AliAnalysisTaskProtonPtDCA();
+
+  virtual void UserCreateOutputObjects();
+  virtual void UserExec(Option_t* option);
+  virtual void Terminate(Option_t*);
+
+ private:
+  ////////////////////////
+  // Procedural function
+  ////////////////////////
+  bool LoopTracks();
+
+  ////////////////////////
+  // Functional function
+  ////////////////////////
+  // Pile-up
+  bool RejectEvtTFFit(float centSPD0);
+  // Track
+  bool AcceptAODTrack(AliAODTrack* track);
+  bool CheckPIDofParticle(AliAODTrack* ftrack, int pidToCheck);
+  // Plane
+  // Get DCA
+  bool GetDCA(float &dcaxy, float &dcaz, AliAODTrack* track);
+
+  //////////////////////
+  // Switch           //
+  //////////////////////
+  bool isNarrowDcaCuts768;
+  bool isProtonCustomizedDCACut;
+  bool isTightPileUp;
+
+  //////////////////////
+  // Cuts and options //
+  //////////////////////
+  // Global
+  TString fTrigger; //
+  TString fPeriod;  // period
+  // Event
+  float fVzCut;      // vz cut
+  // Track
+  int fFilterBit;          // AOD filter bit selection
+  int fNclsCut;            // ncls cut for all tracks
+  float fChi2Max;          // upper limmit for chi2
+  float fChi2Min;          // lower limmit for chi2
+  // PID
+  float fNSigmaTPC;
+  float fNSigmaRMS;
+
+  ///////////////////The following files are from the data//////////////////////////////////
+  /////////////
+  // Handles //
+  /////////////
+  AliAODEvent* fAOD;            // aod Event
+  AliPIDResponse* fPIDResponse; // PID Handler
+  AliMultSelection* fMultSel;
+
+  ////////////////////////////////
+  // Global Variables from data //
+  ////////////////////////////////
+  std::array<double, 3> fVertex;
+  int fRunNum;       // runnumber
+  int fOldRunNum;
+  int fRunNumBin;    // runnumer bin; 10:139510...; 11:170387...; 15HIR:246994...
+  float fCent;
+
+  ///////////////////The following files are read from external sources////////////////////
+
+  ////////////////////////
+  // Pile up Function
+  ////////////////////////
+  std::unique_ptr<TF1> fSPDCutPU;
+  std::unique_ptr<TF1> fV0CutPU;
+  std::unique_ptr<TF1> fCenCutLowPU;
+  std::unique_ptr<TF1> fCenCutHighPU;
+  std::unique_ptr<TF1> fMultCutPU;
+
+  ///////////////////The following files will be saved//////////////////////////////////
+  //////////////
+  // QA Plots //
+  //////////////
+  TList* fQAList;
+  //Proton QA
+  TH2D* h2ProtonPtDcaXY;
+  TH2D* h2ProtonPtDcaZ;
+  TH2D* h2AntiProtonPtDcaXY;
+  TH2D* h2AntiProtonPtDcaZ;
+
+  AliAnalysisTaskProtonPtDCA(const AliAnalysisTaskProtonPtDCA&);
+  AliAnalysisTaskProtonPtDCA& operator=(const AliAnalysisTaskProtonPtDCA&);
+
+  ClassDef(AliAnalysisTaskProtonPtDCA, 1);
+};
+
+#endif
